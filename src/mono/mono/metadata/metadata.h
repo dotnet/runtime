87124674fc53b245cc6768b6da9b366e73ee12fd
Miguel de Icaza <miguel@gnome.org>
2001-07-30 00:23:41 +0000

    Mono 0.5
    svn path=/trunk/mono/; revision=342
    
    
    Commit migrated from https://github.com/mono/mono/commit/637f7a92201461a411a225f40240b5e7cad88049

--- src/mono/mono/metadata/metadata.h
+++ src/mono/mono/metadata/metadata.h
@@ -5,7 +5,7 @@
 #include <glib.h>
 
 #include <mono/metadata/blob.h>
-#include <mono/metadata/row_indexes.h>
+#include <mono/metadata/row-indexes.h>
 
 #ifdef __GNUC__
 #define MONO_ZERO_LEN_ARRAY 0
