69885751d8da859ea0dc32af4aae039690aaa09e
Paolo Molaro <lupus@oddwiz.org>
2001-08-26 17:14:15 +0000

    Expand stream.cs to cover read and write testing. I should work now, with an updated corlib.
    svn path=/trunk/mono/; revision=631
    
    
    Commit migrated from https://github.com/mono/mono/commit/2f45d0163d29b461d79d0da51c5b533d7aff61f1

--- src/mono/mono/tests/stream.cs
+++ src/mono/mono/tests/stream.cs
@@ -4,9 +4,19 @@ using System.IO;
 public class Test {
 
 	public static int Main () {
-		FileStream s = new FileStream ("/tmp/stest.dat", FileMode.Create);
-		
-		
+		byte[] buf = new byte [20];
+		int i;
+		FileStream s = new FileStream ("stest.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+		for (i=0; i < 20; ++i)
+			buf [i] = 65;
+		s.Write (buf, 0, 20);
+		s.Position = 0;
+		for (i=0; i < 20; ++i)
+			buf [i] = 66;
+		s.Read (buf, 0, 20);
+		for (i=0; i < 20; ++i)
+			if (buf [i] != 65)
+				return 1;
 		
 		return 0;
 	}
