7e3dbf32a5a537523b8fd86610481656523227be
Paolo Molaro <lupus@oddwiz.org>
2001-09-07 12:52:44 +0000

    Add delegate test.
    svn path=/trunk/mono/; revision=744
    
    
    Commit migrated from https://github.com/mono/mono/commit/dd833782a94632ebd51e488176f27ed019d7cb53

---
+++ src/mono/mono/tests/delegate.cs
@@ -0,0 +1,34 @@
+using System;
+namespace Bah {
+class Test {
+	delegate void SimpleDelegate ();
+	delegate string NotSimpleDelegate (int a);
+	
+	public int data;
+	
+	static void F () {
+		Console.WriteLine ("Test.F from delegate");
+	}
+	public static string G (int a) {
+		return "G got: " + a.ToString ();
+	}
+	public string H (int a) {
+		return "H got: " + a.ToString () + " and " + data.ToString ();
+	}
+	public Test () {
+		data = 5;
+	}
+	static int Main () {
+		Test test = new Test ();
+		SimpleDelegate d = new SimpleDelegate (F);
+		NotSimpleDelegate d2 = new NotSimpleDelegate (G);
+		NotSimpleDelegate d3 = new NotSimpleDelegate (test.H);
+		d ();
+#if NOTYETWORKING
+		Console.WriteLine (d2 (2));
+		Console.WriteLine (d3 (3));
+#endif
+		return 0;
+	}
+}
+}
