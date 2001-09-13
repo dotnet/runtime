6256d1722de7631c13cc8ea93b07c25664024cef
Dietmar Maurer <dietmar@mono-cvs.ximian.com>
2001-09-13 10:24:11 +0000

    *** empty log message ***
    svn path=/trunk/mono/; revision=804
    
    
    Commit migrated from https://github.com/mono/mono/commit/9179a068781684ec83cb238e2107c6989128170e

---
+++ src/mono/mono/jit/regset.h
@@ -0,0 +1,31 @@
+#ifndef _MONO_JIT_REGSET_H_
+#define _MONO_JIT_REGSET_H_
+
+#include <glib.h>
+
+typedef struct {
+	int max_regs;
+	guint32 free_mask;
+	guint32 used_mask;
+	guint32 reserved_mask;
+} MonoRegSet;
+
+MonoRegSet *
+mono_regset_new         (int max_regs);
+
+void
+mono_regset_free        (MonoRegSet *rs);
+
+int
+mono_regset_alloc_reg   (MonoRegSet *rs, int regnum);
+
+void
+mono_regset_free_reg    (MonoRegSet *rs, int regnum);
+
+void
+mono_regset_reserve_reg (MonoRegSet *rs, int regnum);
+
+gboolean
+mono_regset_reg_used    (MonoRegSet *rs, int regnum);
+
+#endif
