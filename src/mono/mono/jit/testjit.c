b86c69b37fa5238dd9f6d4360ba667a940882555
Dietmar Maurer <dietmar@mono-cvs.ximian.com>
2001-09-18 09:01:31 +0000

    2001-09-18  Dietmar Maurer  <dietmar@ximian.com>
    	* x86.brg: added some rules to optimize code
    
    	* testjit.c (get_address): use a binary search to get the address
    	(main): added an option to debug the jit (-d)
    
    svn path=/trunk/mono/; revision=866
    
    
    Commit migrated from https://github.com/mono/mono/commit/a6ec683fbb45f2ae89f75145c3d2559d67cf6183

--- src/mono/mono/jit/testjit.c
+++ src/mono/mono/jit/testjit.c
@@ -93,37 +93,33 @@ tree_emit (guint8 *cstart, guint8 **buf, MBTree *tree)
 }
 
 static gint32
-tree_get_address (MBTree *tree, gint32 vaddr)
+get_address (GPtrArray *forest, guint32 cli_addr, gint base, gint len)
 {
-	MBTree *t1, *first = NULL;
+	gint32 ind, pos;
+	MBTree *t1;
 
-	/* find the first instruction in the block */
-	for (t1 = tree; t1; t1 = t1->left) {
-		if (t1->vaddr >= 0)
-			first = t1;
+	ind = (len / 2);
+	pos = base + ind;
+
+	/* skip trees with cli_addr == -1 */
+	while ((t1 = (MBTree *) g_ptr_array_index (forest, pos)) &&
+	       t1->cli_addr == -1 && ind) {
+		ind--;
+		pos--;
 	}
 
-	if (first && first->vaddr == vaddr) {
-		return first->addr;
+	if (t1->cli_addr == cli_addr)
+		return t1->first_addr;
+
+	if (len <= 1)
+		return -1;
+
+	if (t1->cli_addr > cli_addr) {
+		return get_address (forest, cli_addr, base, ind);
+	} else {
+		ind = (len / 2);		
+		return get_address (forest, cli_addr, base + ind, len - ind);
 	}
-
-	return -1;
-}
-
-static gint32
-get_address (GPtrArray *forest, guint32 vaddr)
-{
-	gint32 i, a;
-
-	for (i = 0; i < forest->len; i++) {
-		MBTree *t1 = (MBTree *) g_ptr_array_index (forest, i);
-
-		if ((a = tree_get_address (t1, vaddr)) >= 0)
-			return a;
-	}
-
-	g_error ("address 0x%x not found", vaddr);
-	return 0;
 }
 
 static void
@@ -137,9 +133,13 @@ compute_branches (guint8 *cstart, GPtrArray *forest, guint32 epilog)
 		MBTree *t1 = (MBTree *) g_ptr_array_index (forest, i);
 
 		if (t1->is_jump) {
-			if (t1->data.i >= 0)
-				addr = get_address (forest, t1->data.i);
-			else
+			if (t1->data.i >= 0) {
+				addr = get_address (forest, t1->data.i, 0, forest->len);
+				if (addr == -1)
+					g_error ("address 0x%x not found",
+						 t1->data.i);
+
+			} else
 				addr = epilog;
 
 			t1->data.i = addr - t1->addr;
@@ -160,6 +160,11 @@ ctree_new (int op, MonoTypeEnum type, MBTree *left, MBTree *right)
 	t->type = type;
 	t->reg = -1;
 	t->is_jump = 0;
+	t->last_instr = 0;
+	t->cli_addr = -1;
+	t->addr = 0;
+	t->first_addr = 0;
+	t->emit = NULL;
 	return t;
 }
 
@@ -201,7 +206,11 @@ print_forest (GPtrArray *forest)
 
 	for (i = 0; i < top; i++) {
 		MBTree *t = (MBTree *) g_ptr_array_index (forest, i);
-		printf ("IL%04x:", t->vaddr);
+		if (t->cli_addr >= 0)
+			printf ("IL%04x:", t->cli_addr);
+		else
+			printf ("ILXXXX:");
+
 		print_tree (t);
 		printf ("\n");
 	}
@@ -236,6 +245,7 @@ forest_emit (guint8 *cstart, guint8 **buf, GPtrArray *forest)
 	
 	for (i = 0; i < top; i++) {
 		MBTree *t1 = (MBTree *) g_ptr_array_index (forest, i);
+		t1->first_addr = *buf - cstart;
 		tree_emit (cstart, buf, t1);
 	}
 }
@@ -331,9 +341,8 @@ create_jit_trampoline (MonoMethod *method)
 }
 
 
-#define ADD_TREE(t)     do { t->vaddr = vaddr; g_ptr_array_add (forest, (t)); } while (0)
-
-#define PUSH_TREE(t)    do { t->vaddr = vaddr; *sp = t; sp++; } while (0)
+#define ADD_TREE(t)     do { g_ptr_array_add (forest, (t)); } while (0)
+#define PUSH_TREE(t)    do { *sp = t; sp++; } while (0)
 
 #define LOCAL_POS(n)    (locals_offsets [(n)])
 #define LOCAL_TYPE(n)   ((header)->locals [(n)])
@@ -404,8 +413,7 @@ mono_compile_method (MonoMethod *method)
 	forest = g_ptr_array_new ();
 
 	while (ip < end) {
-		guint32 vaddr = ip - header->code;
-		printf ("VADDR %0x\n", vaddr);
+		guint32 cli_addr = ip - header->code;
 		switch (*ip) {
 
 		case CEE_CALL: {
@@ -413,7 +421,8 @@ mono_compile_method (MonoMethod *method)
 			MonoMethod *cm;
 			guint32 token, nargs;
 			int i;
-			
+			gint32 fa;
+
 			++ip;
 			token = read32 (ip);
 			ip += 4;
@@ -422,13 +431,25 @@ mono_compile_method (MonoMethod *method)
 			csig = cm->signature;
 			g_assert (csig->call_convention == MONO_CALL_DEFAULT);
 			
-			nargs = csig->param_count + csig->hasthis;
+			if ((nargs = csig->param_count + csig->hasthis)) {
 
-			for (i = 0; i < nargs; i++) {
-				sp--;
-				t1 = ctree_new (MB_TERM_ARG, 0, *sp, NULL);
-				ADD_TREE (t1);
-			}
+				fa = sp [-nargs]->cli_addr;
+
+				for (i = 0; i < nargs; i++) {
+					sp--;
+					t1 = ctree_new (MB_TERM_ARG, 0, *sp, NULL);
+				
+					if (i)
+						t1->cli_addr = -1;
+					else
+						t1->cli_addr = fa;
+					
+					ADD_TREE (t1);
+				}
+
+				fa = -1;
+			} else
+				fa = cli_addr;
 			
 			cm->addr = create_jit_trampoline (cm);
 			
@@ -442,6 +463,7 @@ mono_compile_method (MonoMethod *method)
 				local_offset += align - 1;
 				local_offset &= ~(align - 1);
 				local_offset += size;
+				t2->cli_addr = fa;
 				ADD_TREE (t2);
 				t1 = ctree_new_leaf (MB_TERM_LDLOC, t2->type);
 				t1->data.i = t2->data.i;
@@ -449,23 +471,26 @@ mono_compile_method (MonoMethod *method)
 			} else {
 				t1 = ctree_new_leaf (MB_TERM_CALL, MONO_TYPE_VOID);
 				t1->data.p = cm;
+				t1->cli_addr = fa;
 				ADD_TREE (t1);
 			}
 			break;
 		}
 		case CEE_LDC_I4_S: 
 			++ip;
-			t1 = ctree_new_leaf (MB_TERM_CONST, MONO_TYPE_I4);
+			t1 = ctree_new_leaf (MB_TERM_CONST_I4, MONO_TYPE_I4);
 			t1->data.i = *ip;
 			++ip;
+			t1->cli_addr = cli_addr;
 			PUSH_TREE (t1);
 			break;
 
 		case CEE_LDC_I4: 
 			++ip;
-			t1 = ctree_new_leaf (MB_TERM_CONST, MONO_TYPE_I4);
+			t1 = ctree_new_leaf (MB_TERM_CONST_I4, MONO_TYPE_I4);
 			t1->data.i = read32 (ip);
 			ip += 4;
+			t1->cli_addr = cli_addr;
 			PUSH_TREE (t1);
 			break;
 
@@ -479,9 +504,10 @@ mono_compile_method (MonoMethod *method)
 		case CEE_LDC_I4_6:
 		case CEE_LDC_I4_7:
 		case CEE_LDC_I4_8:
-			t1 = ctree_new_leaf (MB_TERM_CONST, MONO_TYPE_I4);
+			t1 = ctree_new_leaf (MB_TERM_CONST_I4, MONO_TYPE_I4);
 			t1->data.i = (*ip) - CEE_LDC_I4_0;
 			++ip;
+			t1->cli_addr = cli_addr;
 			PUSH_TREE (t1);
 			break;
 
@@ -494,6 +520,7 @@ mono_compile_method (MonoMethod *method)
 
 			t1 = ctree_new_leaf (MB_TERM_LDLOC, LOCAL_TYPE (n)->type);
 			t1->data.i = LOCAL_POS (n);
+			t1->cli_addr = cli_addr;
 			PUSH_TREE (t1);
 			break;
 		}
@@ -502,10 +529,13 @@ mono_compile_method (MonoMethod *method)
 
 			t1 = ctree_new_leaf (MB_TERM_LDLOC, LOCAL_TYPE (*ip)->type);
 			t1->data.i = LOCAL_POS (*ip);
+			t1->cli_addr = cli_addr;
 			++ip;
+
 			PUSH_TREE (t1);
 			break;
 		}
+
 		case CEE_STLOC_0:
 		case CEE_STLOC_1:
 		case CEE_STLOC_2:
@@ -516,7 +546,7 @@ mono_compile_method (MonoMethod *method)
 
 			t1 = ctree_new (MB_TERM_STLOC, LOCAL_TYPE (n)->type, *sp, NULL);
 			t1->data.i = LOCAL_POS (n);
-
+			t1->cli_addr = sp [0]->cli_addr;
 			ADD_TREE (t1);			
 			break;
 		}
@@ -526,6 +556,7 @@ mono_compile_method (MonoMethod *method)
 
 			t1 = ctree_new (MB_TERM_STLOC, LOCAL_TYPE (*ip)->type, *sp, NULL);
 			t1->data.i = LOCAL_POS (*ip);
+			t1->cli_addr = sp [0]->cli_addr;
 			++ip;
 
 			ADD_TREE (t1);			
@@ -535,6 +566,7 @@ mono_compile_method (MonoMethod *method)
 			++ip;
 			sp -= 2;
 			t1 = ctree_new (MB_TERM_ADD, 0, sp [0], sp [1]);
+			t1->cli_addr = sp [0]->cli_addr;
 			PUSH_TREE (t1);
 			break;
 
@@ -542,6 +574,7 @@ mono_compile_method (MonoMethod *method)
 			++ip;
 			sp -= 2;
 			t1 = ctree_new (MB_TERM_SUB, 0, sp [0], sp [1]);
+			t1->cli_addr = sp [0]->cli_addr;
 			PUSH_TREE (t1);
 			break;
 
@@ -549,13 +582,15 @@ mono_compile_method (MonoMethod *method)
 			++ip;
 			sp -= 2;
 			t1 = ctree_new (MB_TERM_MUL, 0, sp [0], sp [1]);
+			t1->cli_addr = sp [0]->cli_addr;
 			PUSH_TREE (t1);
 			break;
 
 		case CEE_BR_S: 
 			++ip;
 			t1 = ctree_new_leaf (MB_TERM_BR, 0);
-			t1->data.i = vaddr + 2 + (signed char) *ip;
+			t1->data.i = cli_addr + 2 + (signed char) *ip;
+			t1->cli_addr = cli_addr;
 			ADD_TREE (t1);
 			++ip;
 			break;
@@ -563,7 +598,8 @@ mono_compile_method (MonoMethod *method)
 		case CEE_BR:
 			++ip;
 			t1 = ctree_new_leaf (MB_TERM_BR, 0);
-			t1->data.i = vaddr + 5 + (gint32) read32(ip);
+			t1->data.i = cli_addr + 5 + (gint32) read32(ip);
+			t1->cli_addr = cli_addr;
 			ADD_TREE (t1);
 			ip += 4;
 			break;
@@ -575,10 +611,11 @@ mono_compile_method (MonoMethod *method)
 			sp -= 2;
 			t1 = ctree_new (MB_TERM_BLT, 0, sp [0], sp [1]);
 			if (near_jump) 
-				t1->data.i = vaddr + 2 + (signed char) *ip;
+				t1->data.i = cli_addr + 2 + (signed char) *ip;
 			else 
-				t1->data.i = vaddr + 5 + (gint32) read32 (ip);
+				t1->data.i = cli_addr + 5 + (gint32) read32 (ip);
 
+			t1->cli_addr = sp [0]->cli_addr;
 			ADD_TREE (t1);
 			ip += near_jump ? 1: 4;		
 			break;
@@ -590,10 +627,11 @@ mono_compile_method (MonoMethod *method)
 			sp -= 2;
 			t1 = ctree_new (MB_TERM_BEQ, 0, sp [0], sp [1]);
 			if (near_jump)
-				t1->data.i = vaddr + 2 + (signed char) *ip;
+				t1->data.i = cli_addr + 2 + (signed char) *ip;
 			else 
-				t1->data.i = vaddr + 5 + (gint32) read32 (ip);
+				t1->data.i = cli_addr + 5 + (gint32) read32 (ip);
 
+			t1->cli_addr = sp [0]->cli_addr;
 			ADD_TREE (t1);
 			ip += near_jump ? 1: 4;		
 			break;
@@ -605,29 +643,35 @@ mono_compile_method (MonoMethod *method)
 			sp -= 2;
 			t1 = ctree_new (MB_TERM_BGE, 0, sp [0], sp [1]);
 			if (near_jump)
-				t1->data.i = vaddr + 2 + (signed char) *ip;
+				t1->data.i = cli_addr + 2 + (signed char) *ip;
 			else 
-				t1->data.i = vaddr + 5 + (gint32) read32 (ip);
+				t1->data.i = cli_addr + 5 + (gint32) read32 (ip);
 
+			t1->cli_addr = sp [0]->cli_addr;
 			ADD_TREE (t1);
 			ip += near_jump ? 1: 4;		
 			break;
 		}
 		case CEE_RET:
+			ip++;
+
 			if (signature->ret->type != MONO_TYPE_VOID) {
 				--sp;
 				t1 = ctree_new (MB_TERM_RETV, 0, *sp, NULL);
-			} else
+				t1->cli_addr = sp [0]->cli_addr;
+			} else {
 				t1 = ctree_new (MB_TERM_RET, 0, NULL, NULL);
-				
+				t1->cli_addr = cli_addr;
+			}
+
 			t1->data.i = -1;
+			t1->last_instr = (ip == end);
 
 			ADD_TREE (t1);
 
 			if (sp > stack)
 				g_warning ("more values on stack: %d", sp - stack);
 
-			ip++;
 			break;
 
 		case CEE_LDARG_0:
@@ -638,6 +682,7 @@ mono_compile_method (MonoMethod *method)
 			++ip;
 			t1 = ctree_new_leaf (MB_TERM_LDARG, ARG_TYPE (n)->type);
 			t1->data.i = ARG_POS (n);
+			t1->cli_addr = cli_addr;
 			PUSH_TREE (t1);
 			break;
 		}
@@ -652,6 +697,7 @@ mono_compile_method (MonoMethod *method)
 				ip += 4;
 				t1 = ctree_new_leaf (MB_TERM_LDARG, ARG_TYPE (n)->type);
 				t1->data.i = ARG_POS (n);
+				t1->cli_addr = cli_addr;
 				PUSH_TREE (t1);
 				break;
 			}
@@ -729,11 +775,12 @@ static void
 usage (char *name)
 {
 	fprintf (stderr,
-		 "%s %s, the Mono ECMA CLI JITTER, (C) 2001 Ximian, Inc.\n\n"
+		 "%s %s, the Mono ECMA CLI JIT Compiler, (C) 2001 Ximian, Inc.\n\n"
 		 "Usage is: %s [options] executable args...\n", name,  VERSION, name);
 	fprintf (stderr,
 		 "Valid Options are:\n"
-		 "--help\n");
+		 "-d         debug the jit, show disassembler output.\n"
+		 "--help     print this help message\n");
 	exit (1);
 }
 
@@ -743,14 +790,17 @@ main (int argc, char *argv [])
 	MonoAssembly *assembly;
 	int retval = 0, i;
 	char *file;
+	gboolean testjit = FALSE;
 
 	if (argc < 2)
 		usage (argv [0]);
 
-	for (i = 1; argv [i][0] == '-'; i++){
-		if (strcmp (argv [i], "--help") == 0)
+	for (i = 1; i < argc - 1 && argv [i][0] == '-'; i++){
+		if (!strcmp (argv [i], "--help")) {
 			usage (argv [0]);
-		else
+		} else if (!strcmp (argv [i], "-d")) {
+			testjit = TRUE;
+		} else
 			usage (argv [0]);
 	}
 	
@@ -765,13 +815,14 @@ main (int argc, char *argv [])
 		exit (1);
 	}
 
-
-	retval = ves_exec (assembly, argc, argv);
-	//jit_assembly (assembly);
+	if (testjit) {
+		jit_assembly (assembly);
+	} else {
+		retval = ves_exec (assembly, argc, argv);
+		printf ("RESULT: %d\n", retval);
+	}
 
 	mono_assembly_close (assembly);
-
-	printf ("RESULT: %d\n", retval);
 
 	return retval;
 }
