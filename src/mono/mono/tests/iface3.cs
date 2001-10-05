2ac0b4366c915ac70a863951fbeb626201b1dbcd
Dietmar Maurer <dietmar@mono-cvs.ximian.com>
2001-10-05 05:03:42 +0000

    more tests
    svn path=/trunk/mono/; revision=1088
    
    
    Commit migrated from https://github.com/mono/mono/commit/f9f86d8b2b3fcad8b89ccf00f6824cdd43713c14

---
+++ src/mono/mono/tests/iface3.cs
@@ -0,0 +1,44 @@
+using System;
+
+public interface ICommon {
+	int DoIt ();
+}
+
+public class Base : ICommon {
+	int ICommon.DoIt () { return 1; }
+	public virtual int DoIt () { return 2; }
+}
+
+public class Derived : Base, ICommon {
+	int ICommon.DoIt () { return 3; }
+	public new virtual int DoIt () { return 4; }
+}
+
+public class ReallyDerived : Derived {
+	public override int DoIt () { return 5; }
+}
+
+public class Test {
+
+	static int Main () {
+		ReallyDerived r1 = new ReallyDerived ();
+		Derived       r2 = r1;
+		Base          r3 = r1;
+		ICommon       r4 = r1;
+		Object        r5 = r1;
+
+		if (r1.DoIt() != 5)
+			return 1;
+		
+		if (r2.DoIt() != 5)
+			return 2;
+		
+		if (r3.DoIt() != 2)
+			return 3;
+		
+		if (r4.DoIt() != 3)
+			return 4;
+		
+		return 0;
+	}
+}
