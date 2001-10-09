6ab8ef83f318f3bccfe33d84f4558f857fad61f8
Paolo Molaro <lupus@oddwiz.org>
2001-10-09 10:28:29 +0000

    Missing file: this is what you get working on holidays :-(
    svn path=/trunk/mono/; revision=1124
    
    
    Commit migrated from https://github.com/mono/mono/commit/596a86e61b4db0e77957c7e68f12efcbff65ee7f

---
+++ src/mono/mono/metadata/mono-endian.c
@@ -0,0 +1,78 @@
+#include "mono-endian.h"
+
+#if NO_UNALIGNED_ACCESS
+
+typedef struct {
+	char c [2];
+	guint16 i;
+} mono_rint16;
+
+typedef struct {
+	char c [4];
+	guint32 i;
+} mono_rint32;
+
+typedef struct {
+	char c [8];
+	guint64 i;
+} mono_rint64;
+
+guint16 
+mono_read16 (const unsigned char *x)
+{
+	mono_rint16 r;
+#if G_BYTE_ORDER != G_LITTLE_ENDIAN
+	r.c [0] = x [0];
+	r.c [1] = x [1];
+#else
+	r.c [1] = x [0];
+	r.c [0] = x [1];
+#endif
+	return r.i;
+}
+
+guint32 
+mono_read32 (const unsigned char *x)
+{
+	mono_rint32 r;
+#if G_BYTE_ORDER != G_LITTLE_ENDIAN
+	r.c [0] = x [0];
+	r.c [1] = x [1];
+	r.c [2] = x [2];
+	r.c [3] = x [3];
+#else
+	r.c [3] = x [0];
+	r.c [2] = x [1];
+	r.c [1] = x [2];
+	r.c [0] = x [3];
+#endif
+	return r.i;
+}
+
+guint64 
+mono_read64 (const unsigned char *x)
+{
+	mono_rint64 r;
+#if G_BYTE_ORDER != G_LITTLE_ENDIAN
+	r.c [0] = x [0];
+	r.c [1] = x [1];
+	r.c [2] = x [2];
+	r.c [3] = x [3];
+	r.c [4] = x [4];
+	r.c [5] = x [5];
+	r.c [6] = x [6];
+	r.c [7] = x [7];
+#else
+	r.c [7] = x [0];
+	r.c [6] = x [1];
+	r.c [5] = x [2];
+	r.c [4] = x [3];
+	r.c [3] = x [4];
+	r.c [2] = x [5];
+	r.c [1] = x [6];
+	r.c [0] = x [7];
+#endif
+	return r.i;
+}
+
+#endif
