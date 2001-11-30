777393ea2cd7a03c0082f2b68ed32b395813fb81
Radek Doulik <rodo@mono-cvs.ximian.com>
2001-11-30 11:09:16 +0000

    reverted my last changes, fixed unaligned access to make it work
    2001-11-30  Radek Doulik  <rodo@ximian.com>
    
    	* mono-endian.h: reverted last change
    	added function prototypes
    
    	* Makefile.am (libmetadata_a_SOURCES): reverted my last change and
    	add mono-endian.c back
    
    	* mono-endian.c: returned back, as Paolo pointed out, it's needed
    	for unaligned access, I've mistaked it with endianess. I am
    	sorry.
    	(mono_read16): fix reverted endianess
    	(mono_read64): ditto
    	(mono_read32): ditto
    
    svn path=/trunk/mono/; revision=1491
    
    
    Commit migrated from https://github.com/mono/mono/commit/5324a1c3f055e9fd89536a8f8b42d60c839bb84a

--- src/mono/mono/metadata/mono-endian.h
+++ src/mono/mono/metadata/mono-endian.h
@@ -13,9 +13,23 @@ typedef union {
 	double fval;
 } mono_rdouble;
 
+#if NO_UNALIGNED_ACCESS
+
+guint16 mono_read16 (const unsigned char *x);
+guint32 mono_read32 (const unsigned char *x);
+guint64 mono_read64 (const unsigned char *x);
+
+#define read16(x) (mono_read16 ((x)))
+#define read32(x) (mono_read32 ((x)))
+#define read64(x) (mono_read64 ((x)))
+
+#else
+
 #define read16(x) GUINT16_FROM_LE (*((guint16 *) (x)))
 #define read32(x) GUINT32_FROM_LE (*((guint32 *) (x)))
 #define read64(x) GUINT64_FROM_LE (*((guint64 *) (x)))
+
+#endif
 
 #define readr4(x,dest)	\
 	do {	\
