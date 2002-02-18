50e233af0dd4395ffc439a062848ae95e92cb022
Radek Doulik <rodo@mono-cvs.ximian.com>
2002-02-18 22:53:53 +0000

    added pinvoked method with 10 int arguments
    svn path=/trunk/mono/; revision=2484
    
    
    Commit migrated from https://github.com/mono/mono/commit/89da80f5bf1f16793f1cb29874631aac065e124b

--- src/mono/mono/tests/pinvoke.cs
+++ src/mono/mono/tests/pinvoke.cs
@@ -6,6 +6,10 @@ public class Test {
 	[DllImport("cygwin1.dll", EntryPoint="puts", CharSet=CharSet.Ansi)]
 	public static extern int puts (string name);
 
+	[DllImport (".libs/libtest.so", EntryPoint="mono_test_many_int_arguments")]
+	public static extern int mono_test_many_int_arguments (int a, int b, int c, int d, int e,
+							       int f, int g, int h, int i, int j);
+
 	public static int Main () {
 		puts ("A simple Test for PInvoke");
 		
@@ -13,9 +17,9 @@ public class Test {
 			return 1;
 		if (Math.Acos (1) != 0)
 			return 1;
+		if (mono_test_many_int_arguments (1, 1, 1, 1, 1, 1, 1, 1, 1, 1) != 10)
+			return 1;
 		
 		return 0;
 	}
 }
-
-
