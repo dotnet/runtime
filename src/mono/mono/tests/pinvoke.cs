7516e55e59fe2543afe9bbd448e9193e23cf1457
Radek Doulik <rodo@mono-cvs.ximian.com>
2002-02-19 20:53:40 +0000

    added more pinvoke test methods
    svn path=/trunk/mono/; revision=2525
    
    
    Commit migrated from https://github.com/mono/mono/commit/875af536b1a3e5aa1f6bc46d244e8e54b847fa2f

--- src/mono/mono/tests/pinvoke.cs
+++ src/mono/mono/tests/pinvoke.cs
@@ -6,9 +6,15 @@ public class Test {
 	[DllImport("cygwin1.dll", EntryPoint="puts", CharSet=CharSet.Ansi)]
 	public static extern int puts (string name);
 
-	[DllImport (".libs/libtest.so", EntryPoint="mono_test_many_int_arguments")]
+	[DllImport ("libtest.so", EntryPoint="mono_test_many_int_arguments")]
 	public static extern int mono_test_many_int_arguments (int a, int b, int c, int d, int e,
 							       int f, int g, int h, int i, int j);
+	[DllImport ("libtest.so", EntryPoint="mono_test_many_short_arguments")]
+	public static extern int mono_test_many_short_arguments (short a, short b, short c, short d, short e,
+								 short f, short g, short h, short i, short j);
+	[DllImport ("libtest.so", EntryPoint="mono_test_many_byte_arguments")]
+	public static extern int mono_test_many_byte_arguments (byte a, byte b, byte c, byte d, byte e,
+								byte f, byte g, byte h, byte i, byte j);
 
 	public static int Main () {
 		puts ("A simple Test for PInvoke");
@@ -18,6 +24,10 @@ public class Test {
 		if (Math.Acos (1) != 0)
 			return 1;
 		if (mono_test_many_int_arguments (1, 1, 1, 1, 1, 1, 1, 1, 1, 1) != 10)
+			return 1;
+		if (mono_test_many_short_arguments (1, 2, 3, 4, 5, 6, 7, 8, 9, 10) != 55)
+			return 1;
+		if (mono_test_many_byte_arguments (1, 2, 3, 4, 5, 6, 7, 8, 9, 10) != 55)
 			return 1;
 		
 		return 0;
