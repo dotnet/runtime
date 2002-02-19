7efe7723d941b1e9f67c320483df707570f439dc
Radek Doulik <rodo@mono-cvs.ximian.com>
2002-02-19 20:51:37 +0000

    2002-02-19  Radek Doulik  <rodo@ximian.com>
    	* mono/tests/libtest.c (mono_test_many_short_arguments): new test
    	method
    	(mono_test_many_byte_arguments): ditto
    
    svn path=/trunk/mono/; revision=2524
    
    
    Commit migrated from https://github.com/mono/mono/commit/21d2218caecd8ca19bbb681eaa0679c3125765b5

--- src/mono/mono/tests/libtest.c
+++ src/mono/mono/tests/libtest.c
@@ -1,5 +1,24 @@
+int mono_test_many_int_arguments (int a, int b, int c, int d, int e,
+				  int f, int g, int h, int i, int j);
+short mono_test_many_short_arguments (short a, short b, short c, short d, short e,
+				      short f, short g, short h, short i, short j);
+char mono_test_many_char_arguments (char a, char b, char c, char d, char e,
+				    char f, char g, char h, char i, char j);
+
 int
 mono_test_many_int_arguments (int a, int b, int c, int d, int e, int f, int g, int h, int i, int j)
+{
+	return a + b + c + d + e + f + g + h + i + j;
+}
+
+short
+mono_test_many_short_arguments (short a, short b, short c, short d, short e, short f, short g, short h, short i, short j)
+{
+	return a + b + c + d + e + f + g + h + i + j;
+}
+
+char
+mono_test_many_byte_arguments (char a, char b, char c, char d, char e, char f, char g, char h, char i, char j)
 {
 	return a + b + c + d + e + f + g + h + i + j;
 }
