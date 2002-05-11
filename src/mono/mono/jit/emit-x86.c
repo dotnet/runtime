ccb86412ce4b1dfe3314c1cc3926f13519d33340
Dietmar Maurer <dietmar@mono-cvs.ximian.com>
2002-05-11 04:25:13 +0000

    2002-05-11  Dietmar Maurer  <dietmar@ximian.com>
    	* emit-x86.c (arch_emit_prologue): check if live_in_set != NULL
    
    svn path=/trunk/mono/; revision=4517
    
    
    Commit migrated from https://github.com/mono/mono/commit/cc5deacb8412585a31500986e0e2f6f14962035f

--- src/mono/mono/jit/emit-x86.c
+++ src/mono/mono/jit/emit-x86.c
@@ -324,10 +324,14 @@ arch_emit_prologue (MonoFlowGraph *cfg)
 
 	/* initialize local vars */
 	if (header->num_locals) {
-		gboolean unassigned_locals;
+		gboolean unassigned_locals = TRUE;
 
-		i = mono_bitset_find_first (cfg->bblocks [0].live_in_set, cfg->locals_start_index - 1);
-		unassigned_locals = (i >= 0 && i < cfg->locals_start_index + header->num_locals);
+		if (cfg->bblocks [0].live_in_set) {
+			i = mono_bitset_find_first (cfg->bblocks [0].live_in_set, 
+						    cfg->locals_start_index - 1);
+			unassigned_locals = (i >= 0 && i < cfg->locals_start_index + 
+					     header->num_locals);
+		}
 
 		if (unassigned_locals && header->init_locals) {
 			MonoVarInfo *vi = &VARINFO (cfg, cfg->locals_start_index + header->num_locals - 1);
