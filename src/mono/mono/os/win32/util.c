e0b327a3d2c16410291b69b84065db38300b7cfa
Chris J. Breisch <cjbreisch@mono-cvs.ximian.com>
2002-05-19 11:47:02 +0000

    Dummy version so the build will complete.
    svn path=/trunk/mono/; revision=4759
    
    
    Commit migrated from https://github.com/mono/mono/commit/342c6c1d450d7418510be58dbe320a94bbfc1550

---
+++ src/mono/mono/os/win32/util.c
@@ -0,0 +1,25 @@
+/*
+ * util.c: Simple runtime tools for the Win32 platform
+ *
+ * Author:
+ *   Miguel de Icaza
+ *
+ * (C) 2002 Ximian, Inc. (http://www.ximian.com)
+ */
+#include <config.h>
+#include <mono/os/util.h>
+
+/*
+ * mono_set_rootdir:
+ * @vm_filename: The pathname of the code invoking us (argv [0])
+ *
+ * Informs the runtime of the root directory for the Mono installation,
+ * the vm_file
+ */
+void
+mono_set_rootdir (const char *vm_filename)
+{
+}
+
+ 
+
