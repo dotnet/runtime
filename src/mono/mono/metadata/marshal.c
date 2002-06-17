8a103899053a37a99e6999731ab85f332034dea8
Paolo Molaro <lupus@oddwiz.org>
2002-06-17 11:44:35 +0000

    Mon Jun 17 17:29:40 CEST 2002 Paolo Molaro <lupus@ximian.com>
    	* marshal.h, marshal.c: some support code to handle complex marshaling.
    
    svn path=/trunk/mono/; revision=5309
    
    
    Commit migrated from https://github.com/mono/mono/commit/5e47407100198161467b4d8757fa5d7e155e63f0

--- src/mono/mono/metadata/marshal.c
+++ src/mono/mono/metadata/marshal.c
@@ -11,7 +11,8 @@
 #include "config.h"
 #include "object.h"
 #include "loader.h"
-#include "marshal.h"
+#include "metadata/marshal.h"
+#include "metadata/tabledefs.h"
 
 /* FIXME: on win32 we should probably use GlobalAlloc(). */
 void*
@@ -46,5 +47,137 @@ mono_marshal_string_array (MonoArray *array)
 		result [i] = s ? mono_string_to_utf8 (s): NULL;
 	}
 	return result;
+}
+
+gint32
+mono_marshal_type_size (MonoMarshalNative type, int count, gint32 *align)
+{
+	switch (type) {
+	case MONO_NATIVE_BOOLEAN:
+		*align = 4;
+		return 4;
+	case MONO_NATIVE_I1:
+	case MONO_NATIVE_U1:
+		*align = 1;
+		return 1;
+	case MONO_NATIVE_I2:
+	case MONO_NATIVE_U2:
+		*align = 2;
+		return 2;
+	case MONO_NATIVE_I4:
+	case MONO_NATIVE_U4:
+		*align = 4;
+		return 4;
+	case MONO_NATIVE_I8:
+	case MONO_NATIVE_U8:
+		*align = 8;
+		return 8;
+	case MONO_NATIVE_R4:
+		*align = 4;
+		return 4;
+	case MONO_NATIVE_R8:
+		*align = 8;
+		return 8;
+	case MONO_NATIVE_CURRENCY:
+	case MONO_NATIVE_BSTR:
+	case MONO_NATIVE_LPSTR:
+	case MONO_NATIVE_LPWSTR:
+	case MONO_NATIVE_LPTSTR:
+	case MONO_NATIVE_BYVALTSTR:
+	case MONO_NATIVE_IUNKNOWN:
+	case MONO_NATIVE_IDISPATCH:
+	case MONO_NATIVE_STRUCT:
+	case MONO_NATIVE_INTERFACE:
+	case MONO_NATIVE_SAFEARRAY:
+	case MONO_NATIVE_BYVALARRAY:
+	case MONO_NATIVE_INT:
+	case MONO_NATIVE_UINT:
+	case MONO_NATIVE_VBBYREFSTR:
+	case MONO_NATIVE_ANSIBSTR:
+	case MONO_NATIVE_TBSTR:
+	case MONO_NATIVE_VARIANTBOOL:
+	case MONO_NATIVE_FUNC:
+	case MONO_NATIVE_ASANY:
+	case MONO_NATIVE_ARRAY:
+	case MONO_NATIVE_LPSTRUCT:
+	case MONO_NATIVE_CUSTOM:
+	case MONO_NATIVE_ERROR:
+	default:
+		break;
+	}
+	g_assert_not_reached ();
+	return 0;
+}
+
+guint32
+mono_type_to_unmanaged (MonoType *type) {
+	int t = type->type;
+	if (type->byref)
+		return MONO_NATIVE_UINT;
+
+handle_enum:
+	switch (t) {
+	case MONO_TYPE_BOOLEAN: return MONO_NATIVE_BOOLEAN;
+	case MONO_TYPE_CHAR: return MONO_NATIVE_U2;
+	case MONO_TYPE_I1: return MONO_NATIVE_I1;
+	case MONO_TYPE_U1: return MONO_NATIVE_U1;
+	case MONO_TYPE_I2: return MONO_NATIVE_I2;
+	case MONO_TYPE_U2: return MONO_NATIVE_U2;
+	case MONO_TYPE_I4: return MONO_NATIVE_I4;
+	case MONO_TYPE_U4: return MONO_NATIVE_U4;
+	case MONO_TYPE_I8: return MONO_NATIVE_I8;
+	case MONO_TYPE_U8: return MONO_NATIVE_U8;
+	case MONO_TYPE_R4: return MONO_NATIVE_R4;
+	case MONO_TYPE_R8: return MONO_NATIVE_R8;
+	/* the default may change according to the platform... */
+	case MONO_TYPE_STRING: return MONO_NATIVE_LPSTR; 
+	case MONO_TYPE_PTR: return MONO_NATIVE_UINT;
+	case MONO_TYPE_VALUETYPE: /*FIXME*/
+		if (type->data.klass->enumtype) {
+			t = type->data.klass->enum_basetype->type;
+			goto handle_enum;
+		}
+		return MONO_NATIVE_STRUCT;
+	case MONO_TYPE_ARRAY: return MONO_NATIVE_ARRAY;
+	case MONO_TYPE_I: return MONO_NATIVE_INT;
+	case MONO_TYPE_U: return MONO_NATIVE_UINT;
+	case MONO_TYPE_FNPTR: return MONO_NATIVE_FUNC;
+	case MONO_TYPE_OBJECT: return MONO_NATIVE_ASANY; /* ?? */
+	case MONO_TYPE_SZARRAY: return MONO_NATIVE_ARRAY;
+	case MONO_TYPE_CLASS: 
+		/* FIXME : we need to handle ArrayList and StringBuilder here, probably */
+	case MONO_TYPE_TYPEDBYREF:
+	default:
+		g_error ("type 0x%02x not handled in marshal", t);
+	}
+	return MONO_NATIVE_MAX;
+}
+
+void
+mono_marshal_load_type_info (MonoClass* klass)
+{
+	int i, j, count;
+	MonoMarshalType *info;
+
+	if (klass->marshal_info)
+		return;
+
+	for (i = 0; i < klass->field.count; ++i) {
+		if (klass->fields [i].type->attrs & FIELD_ATTRIBUTE_STATIC)
+			continue;
+		/* handle fields in embedded valuetypes. */
+		count++;
+	}
+
+	klass->marshal_info = info = g_malloc0 (sizeof (MonoMarshalType) + sizeof (MonoMarshalField) * count);
+	info->num_fields = count;
+	
+	for (j = i = 0; i < klass->field.count; ++i) {
+		if (klass->fields [i].type->attrs & FIELD_ATTRIBUTE_STATIC)
+			continue;
+		/* handle fields in embedded valuetypes. */
+		j++;
+	}
+
 }
 
--- src/mono/mono/metadata/marshal.h
+++ src/mono/mono/metadata/marshal.h
@@ -12,8 +12,11 @@
 #ifndef __MONO_MARSHAL_H__
 #define __MONO_MARSHAL_H__
 
+#include <mono/metadata/class.h>
+#include <mono/metadata/object.h>
+
 /* ECMA lamespec: the old spec had more info... */
-enum {
+typedef enum {
 	MONO_NATIVE_BOOLEAN = 0x02, /* 4 bytes, 0 is false, != 0 is true */
 	MONO_NATIVE_I1 = 0x03,
 	MONO_NATIVE_U1 = 0x04,
@@ -25,14 +28,51 @@ enum {
 	MONO_NATIVE_U8 = 0x0a,
 	MONO_NATIVE_R4 = 0x0b,
 	MONO_NATIVE_R8 = 0x0c,
-	MONO_NATIVE_LPSTR = 0x14,
+	MONO_NATIVE_CURRENCY = 0x0f,
+	MONO_NATIVE_BSTR = 0x13,
+	MONO_NATIVE_LPSTR = 0x14, /* char* */
+	MONO_NATIVE_LPWSTR = 0x15, /* gunichar2* */
+	MONO_NATIVE_LPTSTR = 0x16,
+	MONO_NATIVE_BYVALTSTR = 0x17,
+	MONO_NATIVE_IUNKNOWN = 0x19,
+	MONO_NATIVE_IDISPATCH = 0x1a,
+	MONO_NATIVE_STRUCT = 0x1b,
+	MONO_NATIVE_INTERFACE = 0x1c,
+	MONO_NATIVE_SAFEARRAY = 0x1d,
+	MONO_NATIVE_BYVALARRAY = 0x1e,
 	MONO_NATIVE_INT   = 0x1f,
 	MONO_NATIVE_UINT  = 0x20,
+	MONO_NATIVE_VBBYREFSTR  = 0x22,
+	MONO_NATIVE_ANSIBSTR  = 0x23,
+	MONO_NATIVE_TBSTR  = 0x24,
+	MONO_NATIVE_VARIANTBOOL  = 0x25,
 	MONO_NATIVE_FUNC  = 0x26,
-	MONO_NATIVE_ARRAY = 0x2a
+	MONO_NATIVE_ASANY = 0x28,
+	MONO_NATIVE_ARRAY = 0x2a,
+	MONO_NATIVE_LPSTRUCT = 0x2b,
+	MONO_NATIVE_CUSTOM = 0x2c,
+	MONO_NATIVE_ERROR = 0x2d,
+	MONO_NATIVE_MAX = 0x50 /* no info */
 } MonoMarshalNative;
 
+typedef struct {
+	MonoClassField *field;
+	guint32 offset;
+	guint32 utype;
+	guint32 conv;
+	guint32 count;
+} MonoMarshalField;
+
+struct MonoMarshalType {
+	guint32 unmanaged_size;
+	guint32 num_fields;
+	MonoMarshalField fields [MONO_ZERO_LEN_ARRAY];
+};
+
 void* mono_marshal_string_array (MonoArray *array);
+
+guint32 mono_type_to_unmanaged      (MonoType *type);
+void    mono_marshal_load_type_info (MonoClass* klass);
 
 void* mono_marshal_alloc   (gpointer size);
 void  mono_marshal_free    (gpointer ptr);
