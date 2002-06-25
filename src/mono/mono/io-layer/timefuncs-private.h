238c868bdbc39fcabd8af5f26eabc3c35f13570f
Dick Porter <dick@acm.org>
2002-06-25 12:29:33 +0000

    2002-06-25  Dick Porter  <dick@ximian.com>
    	* versioninfo.h: PE resource decoding
    
    	* unicode.c (_wapi_unicode_to_utf8): g_utf16_to_utf8 doesnt need
    	to be told the string length
    
    	* io.c: Removed the ACTUALLY_DO_UNICODE option.  Fixed some leaks.
    	Moved _wapi_time_t_to_filetime and the WapiFileTime typedef to
    	timefuncs.c
    
    2002-06-25  Dick Porter  <dick@ximian.com>
    
    	* image.c (do_mono_image_open): Set ref_count to 1, so that bad
    	things dont happen when the image is closed.
    	(mono_image_lookup_resource): Walk the resource section looking
    	for a particular entry
    
    	* cil-coff.h: PE resource section decoding
    
    svn path=/trunk/mono/; revision=5449
    
    
    Commit migrated from https://github.com/mono/mono/commit/d7961b8491ad2ffeba517de9a804d3703710faef

---
+++ src/mono/mono/io-layer/timefuncs-private.h
@@ -0,0 +1,18 @@
+/*
+ * timefuncs-private.h:  performance timer and other time private functions
+ *
+ * Author:
+ *	Dick Porter (dick@ximian.com)
+ *
+ * (C) 2002 Ximian, Inc.
+ */
+
+#ifndef _WAPI_TIMEFUNCS_PRIVATE_H_
+#define _WAPI_TIMEFUNCS_PRIVATE_H_
+
+#include <config.h>
+#include <glib.h>
+
+extern void _wapi_time_t_to_filetime (time_t timeval, WapiFileTime *filetime);
+
+#endif /* _WAPI_TIMEFUNCS_PRIVATE_H_ */
