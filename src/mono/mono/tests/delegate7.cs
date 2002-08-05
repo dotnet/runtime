900289f936dcfd2f019c2586e7fbf4e95c2ca0d0
Dietmar Maurer <dietmar@mono-cvs.ximian.com>
2002-08-05 16:45:03 +0000

    new test
    svn path=/trunk/mono/; revision=6434
    
    
    Commit migrated from https://github.com/mono/mono/commit/ef2bd985e549382859b904ce57ae3d9d2fa69f52

---
+++ src/mono/mono/tests/delegate7.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Runtime.InteropServices;
+
+class Test {
+	delegate void SimpleDelegate ();
+
+	public static int v = 0;
+	
+	static void F1 () {
+		v += 1;
+		Console.WriteLine ("Test.F1");
+	}
+	static void F2 () {
+		v += 2;
+		Console.WriteLine ("Test.F2");
+	}
+	static void F3 () {
+		v += 4;
+		Console.WriteLine ("Test.F3");
+	}
+
+	static int Main () {
+		SimpleDelegate t;
+		SimpleDelegate d1 = new SimpleDelegate (F1);
+		SimpleDelegate d2 = new SimpleDelegate (F2);
+		SimpleDelegate d3 = new SimpleDelegate (F3);
+
+		SimpleDelegate d12 = d1 + d2;
+		SimpleDelegate d13 = d1 + d3;
+		SimpleDelegate d23 = d2 + d3;
+		SimpleDelegate d123 = d1 + d2 + d3;
+
+		v = 0;
+		t = d123 - d13;
+		t ();
+		if (v != 7)
+			return 1;
+		
+		v = 0;
+		t = d123 - d12;
+		t ();
+		if (v != 4)
+			return 1;
+		
+		v = 0;
+		t = d123 - d23;
+		t ();
+		if (v != 1)
+			return 1;
+		
+		
+		return 0;
+	}
+}
