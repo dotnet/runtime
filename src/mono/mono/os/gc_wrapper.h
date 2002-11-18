fdfcb0c7bb01ffa68fd6b2effe80047c233b88a0
Paolo Molaro <lupus@oddwiz.org>
2002-11-18 15:48:05 +0000

    Mon Nov 18 16:40:34 CET 2002 Paolo Molaro <lupus@ximian.com>
    	* configure.in, mono/os/gc_wrapper.h: check that the gc headers have
    	been installed.
    
    svn path=/trunk/mono/; revision=9059
    
    
    Commit migrated from https://github.com/mono/mono/commit/5802708086b578d77b4912cea4e79d07b6fa989f

--- src/mono/mono/os/gc_wrapper.h
+++ src/mono/mono/os/gc_wrapper.h
@@ -1,8 +1,17 @@
 #include <config.h>
 
+#ifdef HAVE_BOEHM_GC
+
 #ifdef	HAVE_GC_GC_H
 #include <gc/gc.h>
-#endif
+#else
+
 #ifdef	HAVE_GC_H
 #include <gc.h>
+#else
+#error have boehm GC without headers, you probably need to install them by hand
+#endif
+
+#endif
+
 #endif
