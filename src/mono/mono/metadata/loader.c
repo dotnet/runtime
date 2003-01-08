a3c6f5485cf991407e619c2cf0dc77e0da93e9e1
Paolo Molaro <lupus@oddwiz.org>
2003-01-08 19:14:35 +0000

    Wed Jan 8 20:11:46 CET 2003 Paolo Molaro <lupus@ximian.com>
    	* loader.c: fixed off by one error in loaded parameter names.
    
    svn path=/trunk/mono/; revision=10288
    
    
    Commit migrated from https://github.com/mono/mono/commit/cec5ae1ae90ee3f6ae728443d7a67519d0a93279

--- src/mono/mono/metadata/loader.c
+++ src/mono/mono/metadata/loader.c
@@ -587,7 +587,7 @@ mono_method_get_param_names (MonoMethod *method, const char **names)
 			if (idx + 1 < methodt->rows)
 				lastp = mono_metadata_decode_row_col (methodt, idx + 1, MONO_METHOD_PARAMLIST);
 			else
-				lastp = paramt->rows;
+				lastp = paramt->rows + 1;
 			for (i = param_index; i < lastp; ++i) {
 				mono_metadata_decode_row (paramt, i -1, cols, MONO_PARAM_SIZE);
 				if (cols [MONO_PARAM_SEQUENCE]) /* skip return param spec */
