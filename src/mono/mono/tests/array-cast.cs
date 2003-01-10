6f6dae77b438429ce061afc5331db85fae1a95d5
Dietmar Maurer <dietmar@mono-cvs.ximian.com>
2003-01-10 14:57:05 +0000

    also test isinst
    svn path=/trunk/mono/; revision=10332
    
    
    Commit migrated from https://github.com/mono/mono/commit/ce0aeca153d03cd4750c830176529b0d07e3ce67

--- src/mono/mono/tests/array-cast.cs
+++ src/mono/mono/tests/array-cast.cs
@@ -5,7 +5,13 @@ namespace Test {
 		public static int Main () {
 			Attribute[] attr_array = new Attribute [1];
 			object obj = (object) attr_array;
-			object[] obj_array = (object[]) obj;
+			object [] obj_array = (object[]) obj;
+
+			obj_array = obj as object[];
+
+			if (obj_array == null)
+				return 1;
+			
 			return 0;
 		}
 	}
