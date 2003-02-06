a7bbb2c9b9595a79db03dc740151b0ecf8a8d472
Patrik Torstensson <totte@mono-cvs.ximian.com>
2003-02-06 13:25:47 +0000

    2002-02-06	Patrik Torstensson
    	* binwritter.cs : Tests binary writter and checks that correct data has been added to the array
    
    svn path=/trunk/mono/; revision=11278
    
    
    Commit migrated from https://github.com/mono/mono/commit/304a621cd557246c6c1496d763c65d240b97ece6

--- src/mono/mono/tests/binwritter.cs
+++ src/mono/mono/tests/binwritter.cs
@@ -12,10 +12,24 @@ public class BinaryWrTest {
 
 		byte [] arr = mr.ToArray();
 
+		Console.Write ("Array (should be: 1 1 0 0 0 ff ff ff ff): ");
+
 		for (int a = 0; a != arr.Length; a++)
 			Console.Write(arr[a].ToString("x") + " ");		
 
 		Console.WriteLine();
+
+		if (arr.Length != 9)
+			return 4;
+
+		if (arr[0] != 1) 
+			return 1;
+
+		if (arr[1] != 1 && arr[2] != 0 && arr[3] != 0 && arr[4] != 0)
+			return 2;
+
+		if (arr[5] != 0xff && arr[6] != 0xff && arr[7] != 0xff && arr[8] != 0xff)
+			return 3;
 	
 		Console.WriteLine("test-ok");
 
