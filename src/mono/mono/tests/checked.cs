386e69abeb539ef3fc2e57dadaac907fa04927d8
Patrik Torstensson <totte@mono-cvs.ximian.com>
2003-02-14 12:54:56 +0000

    2003-02-14	Patrik Torstensson
    	* checked.cs: Added checks for unsigned check keyword
    
    svn path=/trunk/mono/; revision=11580
    
    
    Commit migrated from https://github.com/mono/mono/commit/2bcd3194b5a4feacc46ee529c3810c58ea4901c0

--- src/mono/mono/tests/checked.cs
+++ src/mono/mono/tests/checked.cs
@@ -10,6 +10,7 @@ namespace test {
 		public static int Main () {
 			float a  = 1e20f;
 			int i = 0;
+			uint ui = 0;
 
 			bool exception = false;
 
@@ -42,6 +43,7 @@ namespace test {
 				return -2;
 			}
 
+
 			if (i != 100000)
 				return -3;
 		
@@ -59,10 +61,43 @@ namespace test {
 			}
 			catch (Exception) {
 			}			
+			exception = false;
+
+
+			a  = -1e30f;
+			try {
+				uint b = (uint) a;
+				checked {
+					ui = (uint)a;
+				}
+
+				Console.WriteLine("No Exception");
+			} 
+			catch (OverflowException) {
+				exception = true;
+			}
+			catch (Exception) {
+			}
+
 
 			if (!exception)
 				return -4;
 
+			a  = 1e5f;
+			try {
+				uint b = (uint) a;
+				checked {
+					ui = (uint)a;
+				}
+			} 
+			catch (Exception) {
+				return -5;
+			}
+
+			if (ui != 100000)
+				return -6;
+			
+			
 			Console.WriteLine("test-ok");
 
 			return 0;
