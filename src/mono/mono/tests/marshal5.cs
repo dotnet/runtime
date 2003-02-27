a022c2c9afa60a43f3ba7755910088860ce7396b
Dietmar Maurer <dietmar@mono-cvs.ximian.com>
2003-02-27 11:52:55 +0000

    new test
    svn path=/trunk/mono/; revision=12024
    
    
    Commit migrated from https://github.com/mono/mono/commit/63f894376bf03cdeafe7b32d28d9a34ba1c81380

---
+++ src/mono/mono/tests/marshal5.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Runtime.InteropServices;
+
+public class Test 
+{
+	[DllImport ("libtest.so", EntryPoint="mono_test_byvalstr_gen")]
+	public static extern IntPtr mono_test_byvalstr_gen();
+
+	[DllImport ("libtest.so", EntryPoint="mono_test_byvalstr_check")]
+	public static extern int mono_test_byvalstr_check(IntPtr data, string correctString);
+	
+	[StructLayout (LayoutKind.Sequential)]
+	public struct ByValStrStruct 
+	{
+		[MarshalAs(UnmanagedType.ByValTStr, SizeConst=100)]
+		public string a;
+	}
+	
+	public unsafe static int Main () 
+	{
+		string testString = "A small string";
+
+		IntPtr udata = mono_test_byvalstr_gen();
+
+		ByValStrStruct data = new ByValStrStruct();
+		data.a = testString;
+
+		Marshal.StructureToPtr(data, udata, false);
+
+		return mono_test_byvalstr_check(udata, testString);
+	}
+}
+
