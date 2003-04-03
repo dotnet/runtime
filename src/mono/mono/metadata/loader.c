22cf0e0114c8e12cfadab40518db9e16f01b2be7
Miguel de Icaza <miguel@gnome.org>
2003-04-03 03:36:49 +0000

    2003-04-02  Miguel de Icaza  <miguel@ximian.com>
    	* loader.c (mono_lookup_internal_call): Report the corlib that is
    	out of sync.
    
    svn path=/trunk/mono/; revision=13053
    
    
    Commit migrated from https://github.com/mono/mono/commit/b0e252192a943f9fd79d43022402ec9ddf0c4af5

--- src/mono/mono/metadata/loader.c
+++ src/mono/mono/metadata/loader.c
@@ -92,6 +92,7 @@ mono_lookup_internal_call (MonoMethod *method)
 		if (!(res = g_hash_table_lookup (icall_hash, name))) {
 			g_warning ("cant resolve internal call to \"%s\" (tested without signature also)", name);
 			g_print ("\nYour mono runtime and corlib are out of sync.\n");
+			g_print ("Corlib is: %s", method->klass->image->name);
 			g_print ("When you update one from cvs you need to update, compile and install\nthe other too.\n");
 			g_print ("Do not report this as a bug unless you're sure you have updated correctly:\nyou probably have a broken mono install.\n");
 			g_print ("If you see other errors or faults after this message they are probably related\n");
