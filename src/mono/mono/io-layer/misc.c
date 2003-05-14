cc62a100456a5d0277830a19f0b6076ad368160d
Dick Porter <dick@acm.org>
2003-05-14 09:25:15 +0000

    2003-05-12  Dick Porter  <dick@ximian.com>
    	* misc.c (_wapi_calc_timeout): Cope with overflowing tv_nsec.
    	Patch from Martin Dvorak <mdvorak@ninell.cz>.
    
    svn path=/trunk/mono/; revision=14568
    
    
    Commit migrated from https://github.com/mono/mono/commit/95b0d6ae35778693dd2bd52a707576da2ec471b2

--- src/mono/mono/io-layer/misc.c
+++ src/mono/mono/io-layer/misc.c
@@ -19,9 +19,9 @@ void _wapi_calc_timeout(struct timespec *timeout, guint32 ms)
 	struct timeval now;
 	div_t divvy;
 		
-	divvy=div((int)ms, 1000);
 	gettimeofday(&now, NULL);
+	divvy=div(now.tv_usec+1000*ms, 1000000);
 		
 	timeout->tv_sec=now.tv_sec+divvy.quot;
-	timeout->tv_nsec=(now.tv_usec+(divvy.rem*1000))*1000;
+	timeout->tv_nsec=divvy.rem*1000;
 }
