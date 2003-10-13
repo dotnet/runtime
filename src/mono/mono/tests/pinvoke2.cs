e1c9dca429f87235bbbf59133607adeb90182658
Zoltan Varga <vargaz@gmail.com>
2003-10-13 15:19:08 +0000

    2003-10-13  Zoltan Varga  <vargaz@freemail.hu>
    	* pinvoke2.cs pinvoke3.cs: Merge tests 4-8 into these files +
    	convert tests so they run with the test driver used by mini.
    
    	* pinvoke3.cs libtest.c: Add tests for class arguments and return
    	values.
    
    	* pinvoke4.cs pinvoke5.cs pinvoke6.cs pinvoke7.cs pinvoke8.cs: Removed.
    
    svn path=/trunk/mono/; revision=18967
    
    
    Commit migrated from https://github.com/mono/mono/commit/66a505879d8108e376bc398ea9ff45b9055ac613

--- src/mono/mono/tests/pinvoke2.cs
+++ src/mono/mono/tests/pinvoke2.cs
@@ -1,12 +1,11 @@
 using System;
+using System.Text;
 using System.Runtime.InteropServices;
 
-public class Test {
+public class Tests {
 
 	public static int delegate_test (int a)
 	{
-		Console.WriteLine ("Delegate: " + a);
-		
 		if (a == 2)
 			return 0;
 
@@ -33,6 +32,10 @@ public class Test {
 		public long h;
 	}
 
+	[StructLayout (LayoutKind.Sequential, Size=0)]
+	public struct EmptyStruct {
+	}
+
 	/* sparcv9 has complex conventions when passing structs with doubles in them 
 	   by value, some simple tests for them */
 	[StructLayout (LayoutKind.Sequential)]
@@ -47,8 +50,27 @@ public class Test {
 		public double y;
 	}
 
+	[StructLayout (LayoutKind.Sequential)]
+	public class SimpleClass {
+		public bool a;
+		public bool b;
+		public bool c;
+		public string d;
+		public byte e;
+		public double f;
+		public byte g;
+		public long h;
+	}
+
+	[StructLayout (LayoutKind.Sequential)]
+	public class EmptyClass {
+	}
+
 	[DllImport ("libtest", EntryPoint="mono_test_marshal_char")]
 	public static extern int mono_test_marshal_char (char a1);
+
+	[DllImport ("libtest", EntryPoint="mono_test_marshal_bool_byref")]
+	public static extern int mono_test_marshal_bool_byref (int a, ref bool b, int c);
 
 	[DllImport ("libtest", EntryPoint="mono_test_marshal_array")]
 	public static extern int mono_test_marshal_array (int [] a1);
@@ -68,31 +90,54 @@ public class Test {
 	[DllImport ("libtest", EntryPoint="mono_test_marshal_mixed_point")]
 	public static extern int mono_test_marshal_mixed_point (MixedPoint p);
 
+	[DllImport ("libtest", EntryPoint="mono_test_empty_struct")]
+	public static extern int mono_test_empty_struct (int a, EmptyStruct es, int b);
+
 	[DllImport ("libtest", EntryPoint="mono_test_marshal_struct_array")]
 	public static extern int mono_test_marshal_struct_array (SimpleStruct2[] ss);
+
+	[DllImport ("libtest", EntryPoint="mono_test_marshal_class")]
+	public static extern SimpleClass mono_test_marshal_class (int i, int j, int k, SimpleClass ss, int l);
+
+	[DllImport ("libtest", EntryPoint="mono_test_marshal_byref_class")]
+	public static extern int mono_test_marshal_byref_class (ref SimpleClass ss);
 
 	[DllImport ("libtest", EntryPoint="mono_test_marshal_delegate")]
 	public static extern int mono_test_marshal_delegate (SimpleDelegate d);
 
+	[DllImport ("libtest", EntryPoint="mono_test_return_vtype")]
+	public static extern SimpleStruct mono_test_return_vtype (IntPtr i);
+
+	[DllImport ("libtest", EntryPoint="mono_test_marshal_stringbuilder")]
+	public static extern void mono_test_marshal_stringbuilder (StringBuilder sb, int len);
+
 	public delegate int SimpleDelegate (int a);
 
 	public static int Main () {
-		if (mono_test_marshal_char ('a') != 0)
-			return 1;
+		return TestDriver.RunTests (typeof (Tests));
+	}
 
+	static int test_0_marshal_char () {
+		return mono_test_marshal_char ('a');
+	}
+
+	static int test_1225_marshal_array () {
 		int [] a1 = new int [50];
 		for (int i = 0; i < 50; i++)
 			a1 [i] = i;
 
-		if (mono_test_marshal_array (a1) != 1225)
-			return 2;
+		return mono_test_marshal_array (a1);
+	}
 
+	static int test_0_marshal_struct () {
 		SimpleStruct ss = new  SimpleStruct ();
 		ss.b = true;
 		ss.d = "TEST";
-		if (mono_test_marshal_struct (ss) != 0)
-			return 3;
+		
+		return mono_test_marshal_struct (ss);
+	}
 
+	static int test_0_marshal_struct2 () {
 		SimpleStruct2 ss2 = new  SimpleStruct2 ();
 		ss2.b = true;
 		ss2.d = "TEST";
@@ -101,11 +146,34 @@ public class Test {
 		ss2.g = 42;
 		ss2.h = 123L;
 
-		if (mono_test_marshal_struct2 (ss2) != 0)
-			return 4;
+		return mono_test_marshal_struct2 (ss2);
+	}
 
+	static int test_0_marshal_struct3 () {
+		SimpleStruct2 ss2 = new  SimpleStruct2 ();
+		ss2.b = true;
+		ss2.d = "TEST";
+		ss2.e = 99;
+		ss2.f = 1.5;
+		ss2.g = 42;
+		ss2.h = 123L;
+
+		return mono_test_marshal_struct2_2 (10, 11, 12, ss2);
+	}
+
+	static int test_0_marshal_empty_struct () {
+		EmptyStruct es = new EmptyStruct ();
+
+		if (mono_test_empty_struct (1, es, 2) != 0)
+			return 1;
+		
+		return 0;
+	}
+
+	static int test_0_marshal_struct_array () {
 		SimpleStruct2[] ss_arr = new SimpleStruct2 [2];
 
+		SimpleStruct2 ss2 = new SimpleStruct2 ();
 		ss2.b = true;
 		ss2.d = "TEST";
 		ss2.e = 99;
@@ -124,29 +192,103 @@ public class Test {
 
 		ss_arr [1] = ss2;
 
-		if (mono_test_marshal_struct_array (ss_arr) != 0)
-			return 5;
-		
-		if (mono_test_marshal_struct2_2 (10, 11, 12, ss2) != 0)
-			return 6;
-		
+		return mono_test_marshal_struct_array (ss_arr);
+	}
+
+	/* Test classes as arguments and return values */
+	static int test_0_marshal_class () {
+		SimpleClass ss = new  SimpleClass ();
+		ss.b = true;
+		ss.d = "TEST";
+		ss.e = 99;
+		ss.f = 1.5;
+		ss.g = 42;
+		ss.h = 123L;
+
+		SimpleClass res = mono_test_marshal_class (10, 11, 12, ss, 14);
+		if (res == null)
+			return 1;
+		if  (! (res.a == ss.a && res.b == ss.b && res.c == ss.c && 
+				res.d == ss.d && res.e == ss.e && res.f == ss.f &&
+				res.g == ss.g && res.h == ss.h))
+			return 2;
+
+		/* Test null arguments and results */
+		res = mono_test_marshal_class (10, 11, 12, null, 14);
+		if (res != null)
+			return 3;
+
+		return 0;
+	}
+
+	static int test_0_marshal_byref_class () {
+		SimpleClass ss = new  SimpleClass ();
+		ss.b = true;
+		ss.d = "TEST";
+		ss.e = 99;
+		ss.f = 1.5;
+		ss.g = 42;
+		ss.h = 123L;
+
+		int res = mono_test_marshal_byref_class (ref ss);
+		if (ss.d != "TEST-RES")
+			return 1;
+
+		return 0;
+	}
+
+	static int test_0_marshal_delegate () {
 		SimpleDelegate d = new SimpleDelegate (delegate_test);
 
-		if (mono_test_marshal_delegate (d) != 0)
-			return 7;
+		return mono_test_marshal_delegate (d);
+	}
 
+	static int test_0_marshal_point () {
 		Point pt = new Point();
 		pt.x = 1.25;
 		pt.y = 3.5;
-		if (mono_test_marshal_point(pt) != 0)
-			return 8;
+		
+		return mono_test_marshal_point(pt);
+	}
 
+	static int test_0_marshal_mixed_point () {
 		MixedPoint mpt = new MixedPoint();
 		mpt.x = 5;
 		mpt.y = 6.75;
-		if (mono_test_marshal_mixed_point(mpt) != 0)
-			return 9;
+		
+		return mono_test_marshal_mixed_point(mpt);
+	}
 
+	static int test_0_marshal_bool_byref () {
+		bool b = true;
+		if (mono_test_marshal_bool_byref (99, ref b, 100) != 1)
+			return 1;
+		b = false;
+		if (mono_test_marshal_bool_byref (99, ref b, 100) != 0)
+			return 12;
+		if (b != true)
+			return 13;
+
+		return 0;
+	}
+
+	static int test_0_return_vtype () {
+		SimpleStruct ss = mono_test_return_vtype (new IntPtr (5));
+
+		if (!ss.a && ss.b && !ss.c && ss.d == "TEST")
+			return 0;
+		
+		return 1;
+	}
+
+	static int test_0_marshal_stringbuilder () {
+		StringBuilder sb = new StringBuilder(255);
+		mono_test_marshal_stringbuilder (sb, sb.Capacity);
+		String res = sb.ToString();
+
+		if (res != "This is my message.  Isn't it nice?")
+			return 1;  
+		
 		return 0;
 	}
 }
--- src/mono/mono/tests/pinvoke3.cs
+++ src/mono/mono/tests/pinvoke3.cs
@@ -1,7 +1,13 @@
+//
+// pinvoke3.cs:
+//
+//  Tests for native->managed marshalling
+//
+
 using System;
 using System.Runtime.InteropServices;
 
-public class Test {
+public class Tests {
 
 	[StructLayout (LayoutKind.Sequential)]
 	public struct SimpleStruct {
@@ -11,32 +17,162 @@ public class Test {
 		public string d;
 	}
 
-	public static int delegate_test (SimpleStruct ss)
+	[StructLayout (LayoutKind.Sequential)]
+	public class SimpleClass {
+		public bool a;
+		public bool b;
+		public bool c;
+		public string d;
+	}
+
+	public static SimpleStruct delegate_test_struct (SimpleStruct ss)
 	{
-		Console.WriteLine ("delegate called");
-		Console.WriteLine ("A: " + ss.a);
-		Console.WriteLine ("B: " + ss.b);
-		Console.WriteLine ("C: " + ss.c);
-		Console.WriteLine ("D: " + ss.d);
-		
-		if (!ss.a && ss.b && !ss.c && ss.d == "TEST")
+		SimpleStruct res;
+
+		res.a = !ss.a;
+		res.b = !ss.b;
+		res.c = !ss.c;
+		res.d = ss.d + "-RES";
+
+		return res;
+	}
+
+	public static int delegate_test_struct_byref (int a, ref SimpleStruct ss, int b)
+	{
+		if (a == 1 && b == 2 && ss.a && !ss.b && ss.c && ss.d == "TEST2")
 			return 0;
-		
+
 		return 1;
 	}
+
+	public static SimpleClass delegate_test_class (SimpleClass ss)
+	{
+		if (ss == null)
+			return null;
+
+		if (! (!ss.a && ss.b && !ss.c && ss.d == "TEST"))
+			return null;
+
+		SimpleClass res = ss;
+
+		return res;
+	}
+
+	public static int delegate_test_class_byref (ref SimpleClass ss)
+	{
+		if (ss == null)
+			return -1;
+
+		if (!ss.a && ss.b && !ss.c && ss.d == "TEST") {
+			ss.a = true;
+			ss.b = false;
+			ss.c = true;
+			ss.d = "RES";
+
+			return 0;
+		}
+
+		return 1;
+	}
+
+	public static int delegate_test_class_out (out SimpleClass ss)
+	{
+		ss = new SimpleClass ();
+		ss.a = true;
+		ss.b = false;
+		ss.c = true;
+		ss.d = "RES";
+
+		return 0;
+	}
+
+	[DllImport ("libtest", EntryPoint="mono_test_ref_vtype")]
+	public static extern int mono_test_ref_vtype (int a, ref SimpleStruct ss, int b, TestDelegate d);
 	
 	[DllImport ("libtest", EntryPoint="mono_test_marshal_delegate2")]
 	public static extern int mono_test_marshal_delegate2 (SimpleDelegate2 d);
 
-	public delegate int SimpleDelegate2 (SimpleStruct ss);
+	[DllImport ("libtest", EntryPoint="mono_test_marshal_delegate4")]
+	public static extern int mono_test_marshal_delegate4 (SimpleDelegate4 d);
+
+	[DllImport ("libtest", EntryPoint="mono_test_marshal_delegate5")]
+	public static extern int mono_test_marshal_delegate5 (SimpleDelegate5 d);
+
+	[DllImport ("libtest", EntryPoint="mono_test_marshal_delegate6")]
+	public static extern int mono_test_marshal_delegate6 (SimpleDelegate5 d);
+
+	[DllImport ("libtest", EntryPoint="mono_test_marshal_delegate7")]
+	public static extern int mono_test_marshal_delegate7 (SimpleDelegate7 d);
+
+	public delegate int TestDelegate (int a, ref SimpleStruct ss, int b);
+
+	public delegate SimpleStruct SimpleDelegate2 (SimpleStruct ss);
+
+	public delegate SimpleClass SimpleDelegate4 (SimpleClass ss);
+
+	public delegate int SimpleDelegate5 (ref SimpleClass ss);
+
+	public delegate int SimpleDelegate7 (out SimpleClass ss);
 
 	public static int Main () {
-		
-		SimpleDelegate2 d = new SimpleDelegate2 (delegate_test);
+		return TestDriver.RunTests (typeof (Tests));
+	}
 
-		if (mono_test_marshal_delegate2 (d) != 0)
+	/* Test structures as arguments and return values of delegates */
+	static int test_0_marshal_struct_delegate () {
+		SimpleDelegate2 d = new SimpleDelegate2 (delegate_test_struct);
+
+		return mono_test_marshal_delegate2 (d);
+	}
+
+	/* Test structures as byref arguments of delegates */
+	static int test_0_marshal_byref_struct_delegate () {
+		SimpleStruct ss = new SimpleStruct ();
+		TestDelegate d = new TestDelegate (delegate_test_struct_byref);
+		
+		ss.b = true;
+		ss.d = "TEST1";
+
+		if (mono_test_ref_vtype (1, ref ss, 2, d) != 0)
 			return 1;
 		
+		if (! (ss.a && !ss.b && ss.c && ss.d == "TEST2"))
+			return 2;
+		
 		return 0;
+	}
+
+	/* Test classes as arguments and return values of delegates */
+	static int test_0_marshal_class_delegate () {
+		SimpleDelegate4 d = new SimpleDelegate4 (delegate_test_class);
+
+		return mono_test_marshal_delegate4 (d);
+	}
+
+	/* Test classes as byref arguments of delegates */
+	static int test_0_marshal_byref_class_delegate () {
+		SimpleDelegate5 d = new SimpleDelegate5 (delegate_test_class_byref);
+
+		return mono_test_marshal_delegate5 (d);
+	}
+
+	/* Test classes as out arguments of delegates */
+	static int test_0_marshal_out_class_delegate () {
+		SimpleDelegate7 d = new SimpleDelegate7 (delegate_test_class_out);
+
+		return mono_test_marshal_delegate7 (d);
+	}
+
+	/* Test that the delegate wrapper correctly catches null byref arguments */
+	static int test_0_marshal_byref_class_delegate_null () {
+		SimpleDelegate5 d = new SimpleDelegate5 (delegate_test_class_byref);
+		
+		try {
+			mono_test_marshal_delegate6 (d);
+			return 1;
+		}
+		catch (ArgumentNullException ex) {
+			return 0;
+		}
 	}
 }
