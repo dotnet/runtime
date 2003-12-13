0e7bf2aa9154e02a4c554e9be9375ef29edadff9
Zoltan Varga <vargaz@gmail.com>
2003-12-13 12:10:33 +0000

    2003-12-13  Zoltan Varga  <vargaz@freemail.hu>
    	appdomain.c (MONO_CORLIB_VERSION): Bump version number.
    
    svn path=/trunk/mono/; revision=21101
    
    
    Commit migrated from https://github.com/mono/mono/commit/ce18b11814abdc3de197b413a43ac5f2aab2b337

--- src/mono/mono/metadata/appdomain.c
+++ src/mono/mono/metadata/appdomain.c
@@ -25,7 +25,7 @@
 #include <mono/metadata/gc-internal.h>
 #include <mono/metadata/marshal.h>
 
-#define MONO_CORLIB_VERSION 1
+#define MONO_CORLIB_VERSION 2
 
 CRITICAL_SECTION mono_delegate_section;
 
