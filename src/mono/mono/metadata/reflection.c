1eaef05da8a13665c1f7ae04bbe6d00590f9b193
Bernie Solomon <bernard@mono-cvs.ximian.com>
2003-12-15 20:25:35 +0000

    2003-12-15  Bernie Solomon  <bernard@ugsolutions.com>
    	* reflection.c: move definition of swap_with_size
    	to before its first call
    
    svn path=/trunk/mono/; revision=21203
    
    
    Commit migrated from https://github.com/mono/mono/commit/dca10eb3590464e82834f8c23a503e42eb2efb70

--- src/mono/mono/metadata/reflection.c
+++ src/mono/mono/metadata/reflection.c
@@ -308,6 +308,51 @@ add_to_blob_cached (MonoDynamicImage *assembly, char *b1, int s1, char *b2, int 
 	return idx;
 }
 
+/*
+ * Copy len * nelem bytes from val to dest, swapping bytes to LE if necessary.
+ * dest may be misaligned.
+ */
+static void
+swap_with_size (char *dest, const char* val, int len, int nelem) {
+#if G_BYTE_ORDER != G_LITTLE_ENDIAN
+	int elem;
+
+	for (elem = 0; elem < nelem; ++elem) {
+		switch (len) {
+		case 1:
+			*dest = *val;
+			break;
+		case 2:
+			dest [0] = val [1];
+			dest [1] = val [0];
+			break;
+		case 4:
+			dest [0] = val [3];
+			dest [1] = val [2];
+			dest [2] = val [1];
+			dest [3] = val [0];
+			break;
+		case 8:
+			dest [0] = val [7];
+			dest [1] = val [6];
+			dest [2] = val [5];
+			dest [3] = val [4];
+			dest [4] = val [3];
+			dest [5] = val [2];
+			dest [6] = val [1];
+			dest [7] = val [0];
+			break;
+		default:
+			g_assert_not_reached ();
+		}
+		dest += len;
+		val += len;
+	}
+#else
+	memcpy (dest, val, len * nelem);
+#endif
+}
+
 static guint32
 add_mono_string_to_blob_cached (MonoDynamicImage *assembly, MonoString *str)
 {
@@ -1258,51 +1303,6 @@ field_encode_signature (MonoDynamicImage *assembly, MonoReflectionFieldBuilder *
 	idx = add_to_blob_cached (assembly, blob_size, b-blob_size, buf, p-buf);
 	g_free (buf);
 	return idx;
-}
-
-/*
- * Copy len * nelem bytes from val to dest, swapping bytes to LE if necessary.
- * dest may be misaligned.
- */
-static void
-swap_with_size (char *dest, const char* val, int len, int nelem) {
-#if G_BYTE_ORDER != G_LITTLE_ENDIAN
-	int elem;
-
-	for (elem = 0; elem < nelem; ++elem) {
-		switch (len) {
-		case 1:
-			*dest = *val;
-			break;
-		case 2:
-			dest [0] = val [1];
-			dest [1] = val [0];
-			break;
-		case 4:
-			dest [0] = val [3];
-			dest [1] = val [2];
-			dest [2] = val [1];
-			dest [3] = val [0];
-			break;
-		case 8:
-			dest [0] = val [7];
-			dest [1] = val [6];
-			dest [2] = val [5];
-			dest [3] = val [4];
-			dest [4] = val [3];
-			dest [5] = val [2];
-			dest [6] = val [1];
-			dest [7] = val [0];
-			break;
-		default:
-			g_assert_not_reached ();
-		}
-		dest += len;
-		val += len;
-	}
-#else
-	memcpy (dest, val, len * nelem);
-#endif
 }
 
 static guint32
