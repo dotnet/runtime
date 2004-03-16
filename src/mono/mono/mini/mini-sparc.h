1d2f44c7351da91271e47f44ed15941c90d77433
Bernie Solomon <bernard@mono-cvs.ximian.com>
2004-03-16 00:31:20 +0000

    2004-03-15  Bernie Solomon  <bernard@ugsolutions.com>
    	* mini-sparc.h: implement __builtin_frame_address
    	and __builtin_return_address for Sun C compiler
    
    svn path=/trunk/mono/; revision=24088
    
    
    Commit migrated from https://github.com/mono/mono/commit/8873be05accac2b81a81a07edfb8050a0f56f2f3

--- src/mono/mono/mini/mini-sparc.h
+++ src/mono/mono/mini/mini-sparc.h
@@ -36,6 +36,39 @@ typedef struct MonoCompileArch {
 #define MONO_ARCH_EMULATE_LCONV_TO_R8_UN 1
 #define MONO_ARCH_EMULATE_FREM 1
 
+#ifndef __GNUC__
+/* assume Sun compiler if not GCC */
+static void * __builtin_return_address(int depth)
+{
+	asm("ta      3");
+	asm("tst     %i0");
+	asm("be      retAddr_End");
+	asm("mov     %fp, %l0");
+	asm("retAddr_Start:");
+	asm("sub     %i0, 1, %i0");
+	asm("tst     %i0");
+	asm("bne     retAddr_Start");
+	asm("ld      [%l0+56], %l0");
+	asm("retAddr_End:");
+	asm("ld      [%l0+60], %i0");
+}
+
+static void * __builtin_frame_address(int depth)
+{
+	asm("ta      3");
+	asm("tst     %i0");
+	asm("be      frameAddr_End");
+	asm("mov     %fp, %l0");
+	asm("frameAddr_Start:");
+	asm("sub     %i0, 1, %i0");
+	asm("tst     %i0");
+	asm("bne     frameAddr_Start");
+	asm("ld      [%l0+56], %l0");
+	asm("frameAddr_End:");
+	asm("ld      [%l0+56], %i0");
+}
+#endif
+
 gboolean mono_sparc_is_virtual_call (guint32 *code);
 
 gpointer* mono_sparc_get_vcall_slot_addr (guint32 *code, guint32 *fp);
