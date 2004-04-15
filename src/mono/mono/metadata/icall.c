1d66e76bbead95ab95a12ec900b3598f7e173a85
Bernie Solomon <bernard@mono-cvs.ximian.com>
2004-04-15 18:08:12 +0000

    2004-04-15  Bernie Solomon  <bernard@ugsolutions.com>
    	* icall.c: #include mono-config.h
    
    svn path=/trunk/mono/; revision=25562
    
    
    Commit migrated from https://github.com/mono/mono/commit/ac827bc84a27bbe700d31e1249408e413aa79ca5

--- src/mono/mono/metadata/icall.c
+++ src/mono/mono/metadata/icall.c
@@ -47,6 +47,7 @@
 #include <mono/metadata/filewatcher.h>
 #include <mono/metadata/char-conversions.h>
 #include <mono/metadata/security.h>
+#include <mono/metadata/mono-config.h>
 #include <mono/io-layer/io-layer.h>
 #include <mono/utils/strtod.h>
 #include <mono/utils/monobitset.h>
