298f0ae6130e0dc5319a29264e24c1b753800d0b
Sebastien Pouliot <sebastien@ximian.com>
2004-05-05 18:18:58 +0000

    2004-05-05  Sebastien Pouliot  <sebastien@ximian.com>
    	* environment.c|h: Added icall ves_icall_System_Environment_
    	GetOSVersionString to get the current OS version as a string.
    	* icall.c: Registred icall.
    
    svn path=/trunk/mono/; revision=26776
    
    
    Commit migrated from https://github.com/mono/mono/commit/219613bdad76f6352c1c93552a4530c036678bbe

--- src/mono/mono/metadata/environment.h
+++ src/mono/mono/metadata/environment.h
@@ -13,4 +13,6 @@
 extern gint32 mono_environment_exitcode_get (void);
 extern void mono_environment_exitcode_set (gint32 value);
 
+extern MonoString* ves_icall_System_Environment_GetOSVersionString (void);
+
 #endif /* _MONO_METADATA_ENVIRONMENT_H_ */
