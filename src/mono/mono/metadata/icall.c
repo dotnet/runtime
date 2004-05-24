fc11c573fdeaa6f49c63fe69814621de43ce3971
Sebastien Pouliot <sebastien@ximian.com>
2004-05-24 14:32:56 +0000

    2004-05-24  Sebastien Pouliot  <sebastien@ximian.com>
    	* icall.c: Added define for CSIDL_FLAG_CREATE (0x8000) in case it is
    	missing in the windows build (like older cygwin include files).
    
    svn path=/trunk/mono/; revision=27968
    
    
    Commit migrated from https://github.com/mono/mono/commit/18f3b62bdf2cbeb490bcee571e2ffa7968ee55c9

--- src/mono/mono/metadata/icall.c
+++ src/mono/mono/metadata/icall.c
@@ -4387,6 +4387,10 @@ static MonoString*
 ves_icall_System_Environment_GetWindowsFolderPath (int folder)
 {
 #if defined (PLATFORM_WIN32)
+	#ifndef CSIDL_FLAG_CREATE
+		#define CSIDL_FLAG_CREATE	0x8000
+	#endif
+
 	WCHAR path [MAX_PATH];
 	/* Create directory if no existing */
 	if (SUCCEEDED (SHGetFolderPathW (NULL, folder | CSIDL_FLAG_CREATE, NULL, 0, path))) {
