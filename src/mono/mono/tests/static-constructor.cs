b7c5487fd97c3c60c12cc53e112626e4bc64bc8e
Zoltan Varga <vargaz@gmail.com>
2004-05-28 13:27:01 +0000

    2004-05-28  Zoltan Varga  <vargaz@freemail.hu>
    	* static-constructor.cs: Add new regression test for bug mono/mono#59193.
    
    svn path=/trunk/mono/; revision=28347
    
    
    Commit migrated from https://github.com/mono/mono/commit/49066dff5dd7896d8d3bed704b8dce5a9b1c551b

--- src/mono/mono/tests/static-constructor.cs
+++ src/mono/mono/tests/static-constructor.cs
@@ -1,9 +1,22 @@
 
-public class GenRandom {
+using System;
+using System.Reflection;
+
+struct A {
+	public A (int i) {
+	}
+}
+
+struct B {
+	public B (int i) {
+	}
+}
+
+public class Tests {
     	static int last = 42;
     	static int burp;
 
-	static GenRandom () {
+	static Tests () {
 		/* 
 		 * This is really at test of the compiler: it should init
 		 * last before getting here.
@@ -18,6 +31,14 @@ public class GenRandom {
 			return 1;
 		if (burp != 4)
 			return 1;
+
+		// Regression test for bug #59193 (shared runtime wrappers)
+		ConstructorInfo con1 = typeof (A).GetConstructor (BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic, null, new Type [1] { typeof (int) }, null);
+		ConstructorInfo con2 = typeof (B).GetConstructor (BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic, null, new Type [1] { typeof (int) }, null);
+
+		con1.Invoke (null, new Object [] { 0 });
+		con2.Invoke (null, new Object [] { 0 });
+
 		return 0;
 	}
 }
