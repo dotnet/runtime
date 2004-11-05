e8a179667f6b02cf767ec012f4027733b8549b45
Zoltan Varga <vargaz@gmail.com>
2004-11-05 15:18:12 +0000

    2004-11-05  Zoltan Varga  <vargaz@freemail.hu>
    	* loader.cs: Add test for loading dynamic assemblies using
    	Assembly.LoadWithPartialName ().
    
    svn path=/trunk/mono/; revision=35697
    
    
    Commit migrated from https://github.com/mono/mono/commit/63a9de9d4b6a3ff33f4ea79f3c4f8f1f2cc4d58b

--- src/mono/mono/tests/loader.cs
+++ src/mono/mono/tests/loader.cs
@@ -6,6 +6,7 @@
 
 using System;
 using System.Reflection;
+using System.Reflection.Emit;
 
 public class Tests {
 
@@ -20,6 +21,25 @@ public class Tests {
 			return 1;
 		else
 			return 0;
+	}
+
+	public static int test_0_load_dynamic ()
+	{
+		// Check that dynamic assemblies are not loaded by Assembly.Load
+        AssemblyName an = new AssemblyName();
+        an.Name = "NOT.EXISTS";
+
+        AssemblyBuilder ab = 
+            AppDomain.CurrentDomain.DefineDynamicAssembly(an,
+                                                     AssemblyBuilderAccess.RunAndSave);
+
+        ModuleBuilder mb = ab.DefineDynamicModule("NOT.EXISTS");
+
+		Assembly b = Assembly.LoadWithPartialName ("NOT.EXISTS");
+		if (b == null)
+			return 0;
+		else
+			return 1;
 	}
 }
 
