a96e2c2b41876715b7f2a092b41557694dc3f301
Zoltan Varga <vargaz@gmail.com>
2004-11-15 12:42:28 +0000

    Merge from HEAD.
    svn path=/branches/mono-1-0/mono/; revision=36133
    
    
    Commit migrated from https://github.com/mono/mono/commit/3003049245579d648b4c752ea5c23bd5e6fa3f40

--- src/mono/mono/metadata/icall.c
+++ src/mono/mono/metadata/icall.c
@@ -1564,7 +1564,7 @@ ves_icall_type_isprimitive (MonoReflectionType *type)
 {
 	MONO_ARCH_SAVE_REGS;
 
-	return (!type->type->byref && (type->type->type >= MONO_TYPE_BOOLEAN) && (type->type->type <= MONO_TYPE_R8));
+	return (!type->type->byref && (((type->type->type >= MONO_TYPE_BOOLEAN) && (type->type->type <= MONO_TYPE_R8)) || (type->type->type == MONO_TYPE_I) || (type->type->type == MONO_TYPE_U)));
 }
 
 static MonoBoolean
