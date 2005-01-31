5dc7ac873b069b47916961e9faaf1ea52a7d19f6
Miguel de Icaza <miguel@gnome.org>
2005-01-31 23:57:28 +0000

    Moved the old Mono JIT
    svn path=/trunk/mono/; revision=39879
    
    
    Commit migrated from https://github.com/mono/mono/commit/be0379593e6bf28ad9a2d8bde2d6dc17479d3126

--- src/mono/mono/jit/debug-jit.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MONO_JIT_DEBUG_JIT_H__
-#define __MONO_JIT_DEBUG_JIT_H__
-
-#include <mono/jit/codegen.h>
-
-void           mono_debug_jit_add_method (MonoFlowGraph *cfg);
-
-#endif /* __MONO_JIT_DEBUG_JIT_H__ */
