e8cdfd685d6b6ee63ee88e1b5976f23062ac4392
Paolo Molaro <lupus@oddwiz.org>
2005-02-11 22:07:13 +0000

    Fix arg to invalidated_delegate_trampoline.
    svn path=/trunk/mono/; revision=40500
    
    
    Commit migrated from https://github.com/mono/mono/commit/bd19f6e54602c83dba3e9306a7c3e6c374f9fd8a

--- src/mono/mono/mini/mini.c
+++ src/mono/mono/mini/mini.c
@@ -9237,11 +9237,11 @@ mono_jit_compile_method (MonoMethod *method)
 }
 
 static void
-invalidated_delegate_trampoline (MonoClass *klass)
+invalidated_delegate_trampoline (char *desc)
 {
 	g_error ("Unmanaged code called delegate of type %s which was already garbage collected.\n"
 		 "See http://www.go-mono.com/delegate.html for an explanation and ways to fix this.",
-		 mono_type_full_name (&klass->byval_arg));
+		 desc);
 }
 
 /*
