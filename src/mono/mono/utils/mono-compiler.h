032b4cb809ad1cf4f65b4b47ad50456dc986f8e9
Paolo Molaro <lupus@oddwiz.org>
2005-02-24 13:59:53 +0000

    Thu Feb 24 15:18:05 CET 2005 Paolo Molaro <lupus@ximian.com>
    	* Makefile.am, mono-compiler.h: added header to deal with 
    	compiler-specific bugs and features.
    
    
    svn path=/trunk/mono/; revision=41154
    
    
    Commit migrated from https://github.com/mono/mono/commit/8d8dc33b38c1976c7756602d0b948612e187ea20

---
+++ src/mono/mono/utils/mono-compiler.h
@@ -0,0 +1,29 @@
+#ifndef __UTILS_MONO_COMPILER_H__
+#define __UTILS_MONO_COMPILER_H__
+/*
+ * This file includes macros used in the runtime to encapsulate different
+ * compiler behaviours.
+ */
+#include <config.h>
+
+#ifdef HAVE_KW_THREAD
+#if HAVE_TLS_MODEL_ATTR
+#define MONO_TLS_FAST __attribute__((tls_model("local-exec")))
+#else
+#define MONO_TLS_FAST 
+#endif
+
+#if defined(__GNUC__) && defined(__i386__)
+#define MONO_THREAD_VAR_OFFSET(var,offset) __asm ("jmp 1f; .section writetext, \"awx\"; 1: movl $" #var "@ntpoff, %0; jmp 2f; .previous; 2:" : "=r" (offset));
+#else
+#define MONO_THREAD_VAR_OFFSET(var,offset) (offset) = -1
+#endif
+
+#else /* no HAVE_KW_THREAD */
+
+#define MONO_THREAD_VAR_OFFSET(var,offset) (offset) = -1
+
+#endif
+
+#endif /* __UTILS_MONO_COMPILER_H__*/
+
