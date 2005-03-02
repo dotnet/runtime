d55c19f4a728e23b4a707e099dc9e27503872660
Sebastien Pouliot <sebastien@ximian.com>
2005-03-02 21:29:56 +0000

    svn path=/trunk/mono/; revision=41375
    Commit migrated from https://github.com/mono/mono/commit/ba814f392d53d6d27af58f7a169379733ca00d92

---
+++ src/mono/mono/tests/cas/inheritance/refload1.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Reflection;
+using System.Security;
+using System.Security.Permissions;
+
+[assembly: SecurityPermission (SecurityAction.RequestRefuse, ControlPrincipal=true)]
+
+public class Program {
+
+	static int Main (string[] args)
+	{
+		try {
+			string filename = "library1a";
+			Assembly a = Assembly.Load (filename);
+			if (a == null) {
+				Console.WriteLine ("*1* Couldn't load assembly '{0}'.", filename);
+				return 1;
+			} else {
+				Console.WriteLine ("*0* Assembly '{0}' loaded.", filename);
+				return 0;
+			}
+		}
+		catch (SecurityException se) {
+			Console.WriteLine ("*2* Unexpected SecurityException\n{0}", se);
+			return 2;
+		}
+		catch (Exception e) {
+			Console.WriteLine ("*3* Unexpected Exception\n{0}", e);
+			return 3;
+		}
+	}
+}
