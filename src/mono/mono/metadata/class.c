470e32b272ce3e5c5618b1c01c7640843bcebff6
Martin Baulig <martin@novell.com>
2005-03-10 04:32:26 +0000

    2005-03-10  Martin Baulig  <martin@ximian.com>
    	* class.c (mono_bounded_array_class_get): Initialize `eclass' if
    	it's a generic instance.
    
    
    svn path=/trunk/mono/; revision=41629
    
    
    Commit migrated from https://github.com/mono/mono/commit/6a91d42fbf7653faa9d9ed105dac1afebc0b2250

--- src/mono/mono/metadata/class.c
+++ src/mono/mono/metadata/class.c
@@ -2632,6 +2632,8 @@ mono_bounded_array_class_get (MonoClass *eclass, guint32 rank, gboolean bounded)
 	class->instance_size = mono_class_instance_size (class->parent);
 	class->class_size = 0;
 	mono_class_setup_supertypes (class);
+	if (eclass->generic_class)
+		mono_class_init (eclass);
 	if (!eclass->size_inited)
 		class_compute_field_layout (eclass);
 	class->has_references = MONO_TYPE_IS_REFERENCE (&eclass->byval_arg) || eclass->has_references? TRUE: FALSE;
