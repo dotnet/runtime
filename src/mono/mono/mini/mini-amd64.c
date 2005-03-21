ea381c89a2da97201713f0c2e9bac1ab9d105c57
Zoltan Varga <vargaz@gmail.com>
2005-03-21 19:00:42 +0000

    2005-03-21  Zoltan Varga  <vargaz@freemail.hu>
    	* mini-amd64.c (mono_arch_output_basic_block): Add inline versions of
    	some fp functions in sse2 mode.
    
    svn path=/trunk/mono/; revision=42055
    
    
    Commit migrated from https://github.com/mono/mono/commit/71fe4a042410cedd8ec11ba321b37856d3870956

--- src/mono/mono/mini/mini-amd64.c
+++ src/mono/mono/mini/mini-amd64.c
@@ -1260,6 +1260,14 @@ if (ins->flags & MONO_INST_BRLABEL) { \
 	amd64_fnstsw (code); \
 } while (0); 
 
+#define EMIT_SSE2_FPFUNC(code, op, dreg, sreg1) do { \
+    amd64_movsd_membase_reg (code, AMD64_RSP, -8, (sreg1)); \
+	amd64_fld_membase (code, AMD64_RSP, -8, TRUE); \
+	amd64_ ##op (code); \
+	amd64_fst_membase (code, AMD64_RSP, -8, TRUE, TRUE); \
+	amd64_movsd_reg_membase (code, (dreg), AMD64_RSP, -8); \
+} while (0);
+
 static guint8*
 emit_call (MonoCompile *cfg, guint8 *code, guint32 patch_type, gconstpointer data)
 {
@@ -4340,23 +4348,31 @@ mono_arch_output_basic_block (MonoCompile *cfg, MonoBasicBlock *bb)
 				amd64_fchs (code);
 			break;		
 		case OP_SIN:
-			if (use_sse2)
-				g_assert_not_reached ();
-			amd64_fsin (code);
-			amd64_fldz (code);
-			amd64_fp_op_reg (code, X86_FADD, 1, TRUE);
+			if (use_sse2) {
+				EMIT_SSE2_FPFUNC (code, fsin, ins->dreg, ins->sreg1);
+			}
+			else {
+				amd64_fsin (code);
+				amd64_fldz (code);
+				amd64_fp_op_reg (code, X86_FADD, 1, TRUE);
+			}
 			break;		
 		case OP_COS:
-			if (use_sse2)
-				g_assert_not_reached ();
-			amd64_fcos (code);
-			amd64_fldz (code);
-			amd64_fp_op_reg (code, X86_FADD, 1, TRUE);
+			if (use_sse2) {
+				EMIT_SSE2_FPFUNC (code, fcos, ins->dreg, ins->sreg1);
+			}
+			else {
+				amd64_fcos (code);
+				amd64_fldz (code);
+				amd64_fp_op_reg (code, X86_FADD, 1, TRUE);
+			}
 			break;		
 		case OP_ABS:
-			if (use_sse2)
-				g_assert_not_reached ();
-			amd64_fabs (code);
+			if (use_sse2) {
+				EMIT_SSE2_FPFUNC (code, fabs, ins->dreg, ins->sreg1);
+			}
+			else
+				amd64_fabs (code);
 			break;		
 		case OP_TAN: {
 			/* 
@@ -4404,9 +4420,11 @@ mono_arch_output_basic_block (MonoCompile *cfg, MonoBasicBlock *bb)
 			amd64_fp_op_reg (code, X86_FADD, 1, TRUE);
 			break;		
 		case OP_SQRT:
-			if (use_sse2)
-				g_assert_not_reached ();
-			amd64_fsqrt (code);
+			if (use_sse2) {
+				EMIT_SSE2_FPFUNC (code, fsqrt, ins->dreg, ins->sreg1);
+			}
+			else
+				amd64_fsqrt (code);
 			break;		
 		case OP_X86_FPOP:
 			if (!use_sse2)
@@ -5977,9 +5995,6 @@ mono_arch_get_inst_for_method (MonoCompile *cfg, MonoMethod *cmethod, MonoMethod
 {
 	MonoInst *ins = NULL;
 
-	if (use_sse2)
-		return NULL;
-
 	if (cmethod->klass == mono_defaults.math_class) {
 		if (strcmp (cmethod->name, "Sin") == 0) {
 			MONO_INST_NEW (cfg, ins, OP_SIN);
@@ -5988,9 +6003,13 @@ mono_arch_get_inst_for_method (MonoCompile *cfg, MonoMethod *cmethod, MonoMethod
 			MONO_INST_NEW (cfg, ins, OP_COS);
 			ins->inst_i0 = args [0];
 		} else if (strcmp (cmethod->name, "Tan") == 0) {
+			if (use_sse2)
+				return ins;
 			MONO_INST_NEW (cfg, ins, OP_TAN);
 			ins->inst_i0 = args [0];
 		} else if (strcmp (cmethod->name, "Atan") == 0) {
+			if (use_sse2)
+				return ins;
 			MONO_INST_NEW (cfg, ins, OP_ATAN);
 			ins->inst_i0 = args [0];
 		} else if (strcmp (cmethod->name, "Sqrt") == 0) {
