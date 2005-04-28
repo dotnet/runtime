416770051490fa571799b7e44def1e584e45866a
Zoltan Varga <vargaz@gmail.com>
2005-04-28 19:49:34 +0000

    2005-04-28  Zoltan Varga  <vargaz@freemail.hu>
    	* mini.c (mini_cleanup): Skip the freeing of the runtime structures
    	for now, since some threads might still be running.
    
    svn path=/trunk/mono/; revision=43743
    
    
    Commit migrated from https://github.com/mono/mono/commit/856c2373807ddc9dba681fc5f3b383b73e87b53c

--- src/mono/mono/mini/mini.c
+++ src/mono/mono/mini/mini.c
@@ -9629,10 +9629,18 @@ SIG_HANDLER_SIGNATURE (sigusr1_signal_handler)
 {
 	gboolean running_managed;
 	MonoException *exc;
+	void *ji;
 	
-	GET_CONTEXT
+	GET_CONTEXT;
 
-	running_managed = (mono_jit_info_table_find (mono_domain_get (), mono_arch_ip_from_context(ctx)) != NULL);
+	/*
+	 * FIXME:
+	 * This is an async signal, so the code below must not call anything which
+	 * is not async safe. That includes the pthread locking functions. If we
+	 * know that we interrupted managed code, then locking is safe.
+	 */
+	ji = mono_jit_info_table_find (mono_domain_get (), mono_arch_ip_from_context(ctx));
+	running_managed = ji != NULL;
 	
 	exc = mono_thread_request_interruption (running_managed); 
 	if (!exc) return;
@@ -10080,6 +10088,19 @@ mini_cleanup (MonoDomain *domain)
 	 * and mono_runtime_cleanup will wait for other threads to finish).
 	 */
 	mono_domain_finalize (domain, 2000);
+
+	/*
+	 * Since the runtime does not suspend/abort running threads when exiting,
+	 * freeing up JIT structures could cause crashes in those threads. So skip 
+	 * that for the moment.
+	 */
+
+	/* 
+	 * This is not safe but people running profilers hopefully do not
+	 * use threads.
+	 */
+	mono_profiler_shutdown ();
+	return;
 
 	mono_runtime_cleanup (domain);
 
