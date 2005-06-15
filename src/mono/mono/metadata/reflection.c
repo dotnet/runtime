097032662680f9edf12e3faca2a999e41512477c
Martin Baulig <martin@novell.com>
2005-06-15 17:37:04 +0000

    2005-06-15  Martin Baulig  <martin@ximian.com>
    	* reflection.c (mono_image_get_inflated_method_token): Encode
    	correct klass; fixes mono/mono#75260.
    
    
    svn path=/trunk/mono/; revision=46056
    
    
    Commit migrated from https://github.com/mono/mono/commit/8006ba2fe3113dd9929d69a64759428896279f83

--- src/mono/mono/metadata/reflection.c
+++ src/mono/mono/metadata/reflection.c
@@ -2508,6 +2508,8 @@ mono_image_get_inflated_method_token (MonoDynamicImage *assembly, MonoMethod *m)
 	MonoMethodInflated *imethod = (MonoMethodInflated *) m;
 	guint32 sig, token;
 
+	m = mono_get_inflated_method (m);
+
 	sig = method_encode_signature (assembly, mono_method_signature (imethod->declaring));
 	token = mono_image_get_memberref_token (
 		assembly, &m->klass->byval_arg, m->name, sig);
