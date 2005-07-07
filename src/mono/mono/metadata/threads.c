e0aeb887f0cc07c38cf8f1b4b8bcefb03c5a5d44
Paolo Molaro <lupus@oddwiz.org>
2005-07-07 18:00:41 +0000

    Thu Jul 7 19:59:31 CEST 2005 Paolo Molaro <lupus@ximian.com>
    	* threads.c: try to better gues an available RT signal (bug mono/mono#75387).
    
    
    svn path=/trunk/mono/; revision=47059
    
    
    Commit migrated from https://github.com/mono/mono/commit/1aa86589bb2c49453d8515ffce3e8b116a59d5b0

--- src/mono/mono/metadata/threads.c
+++ src/mono/mono/metadata/threads.c
@@ -1386,6 +1386,20 @@ mono_thread_get_abort_signal (void)
 #ifndef	SIGRTMIN
 	return SIGUSR1;
 #else
+	static int abort_signum = -1;
+	int i;
+	if (abort_signum != -1)
+		return abort_signum;
+	/* we try to avoid SIGRTMIN and any one that might have been set already, see bug #75387 */
+	for (i = SIGRTMIN + 1; i < SIGRTMAX; ++i) {
+		struct sigaction sinfo;
+		sigaction (i, NULL, &sinfo);
+		if (sinfo.sa_handler == SIG_DFL && (void*)sinfo.sa_sigaction == (void*)SIG_DFL) {
+			abort_signum = i;
+			return i;
+		}
+	}
+	/* fallback to the old way */
 	return SIGRTMIN;
 #endif
 #endif /*defined (__MINGW32__) || defined (_MSC_VER) */
