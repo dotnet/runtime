d4537006ed393a53c42830d4e292ca4e98222be2
Jb Evain <jbevain@gmail.com>
2005-07-21 22:07:16 +0000

    2005-07-21  Jb Evain  <jbevain@gmail.com>
    	* reflection.c (method_encode_clauses/class): Handle filters clauses.
    	Fixes mono/mono#75010.
    
    svn path=/trunk/mono/; revision=47537
    
    
    Commit migrated from https://github.com/mono/mono/commit/2211cfa196d8a92efd4ea09f6afbab8ae32c3b6e

--- src/mono/mono/metadata/reflection.c
+++ src/mono/mono/metadata/reflection.c
@@ -849,8 +849,10 @@ method_encode_clauses (MonoDynamicImage *assembly, MonoReflectionILGen *ilgen, g
 			if (ex_block->extype) {
 				clause->data.catch_class = mono_class_from_mono_type (ex_block->extype->type);
 			} else {
-				/* FIXME: handle filters */
-				clause->data.filter_offset = 0;
+				if (ex_block->type == MONO_EXCEPTION_CLAUSE_FILTER)
+					clause->data.filter_offset = ex_block->filter_offset;
+				else
+					clause->data.filter_offset = 0;
 			}
 			finally_start = ex_block->start + ex_block->len;
 
@@ -998,8 +1000,10 @@ fat_header:
 					if (ex_block->extype) {
 						val = mono_metadata_token_from_dor (mono_image_typedef_or_ref (assembly, ex_block->extype->type));
 					} else {
-						/* FIXME: handle filters */
-						val = 0;
+						if (ex_block->type == MONO_EXCEPTION_CLAUSE_FILTER)
+							val = ex_block->filter_offset;
+						else
+							val = 0;
 					}
 					val = GUINT32_TO_LE (val);
 					mono_image_add_stream_data (&assembly->code, (char*)&val, sizeof (guint32));
