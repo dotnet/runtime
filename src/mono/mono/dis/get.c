284027cc28e1def1c2d510500e3a98e08c3d63b7
Ankit Jain <radical@corewars.org>
2005-08-01 15:17:10 +0000

    * get.c (field_flags_map): Use C comments instead of C#.
    svn path=/trunk/mono/; revision=47892
    
    
    Commit migrated from https://github.com/mono/mono/commit/bb881a44b52ee288cd8f1437a03b2fd48de19cf6

--- src/mono/mono/dis/get.c
+++ src/mono/mono/dis/get.c
@@ -1396,7 +1396,7 @@ static dis_map_t field_flags_map [] = {
 	{ FIELD_ATTRIBUTE_SPECIAL_NAME,        "specialname " },
 	{ FIELD_ATTRIBUTE_PINVOKE_IMPL,        "FIXME:pinvokeimpl " },
 	{ FIELD_ATTRIBUTE_RT_SPECIAL_NAME,        "rtspecialname " },
-	//{ FIELD_ATTRIBUTE_HAS_FIELD_MARSHAL,        "hasfieldmarshal " },
+	/*{ FIELD_ATTRIBUTE_HAS_FIELD_MARSHAL,        "hasfieldmarshal " },*/
 	{ 0, NULL }
 };
 
