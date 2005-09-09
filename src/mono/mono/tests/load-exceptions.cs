97db6ad0588b6f2e403b0ef6b506c3371b307b02
Zoltan Varga <vargaz@gmail.com>
2005-09-09 14:01:30 +0000

    2005-09-09  Zoltan Varga  <vargaz@gmail.com>
    	* load-exceptions.cs t-missing.cs load-missing.il: New files. Tests
    	for handling type load exceptions.
    
    	* Makefile.am: Add type loading tests.
    
    svn path=/trunk/mono/; revision=49802
    
    
    Commit migrated from https://github.com/mono/mono/commit/1de932cffb04b4e950c1dd51d7693ea9c1fb4335

---
+++ src/mono/mono/tests/load-exceptions.cs
@@ -0,0 +1,219 @@
+//
+// load-exceptions.cs: Tests for loading missing types/methods/fields from IL
+//
+
+using System;
+
+class Miss1 : Missing.Foo1 {
+}
+
+public class Tests : LoadMissing {
+
+	public delegate void TestDel ();
+
+	internal static int check_type_load (TestDel d) {
+		try {
+			d ();
+		}
+		catch (TypeLoadException ex) {
+			//Console.WriteLine (ex.TypeName);
+			//Console.WriteLine (ex);
+			return 0;
+		}
+
+		return 1;
+	}
+
+	internal static int check_missing_method (TestDel d) {
+		try {
+			d ();
+		}
+		catch (MissingMethodException ex) {
+			//Console.WriteLine (ex);
+			return 0;
+		}
+
+		return 1;
+	}
+
+	internal static int check_missing_field (TestDel d) {
+		try {
+			d ();
+		}
+		catch (MissingFieldException ex) {
+			//Console.WriteLine (ex);
+			return 0;
+		}
+
+		return 1;
+	}
+
+	//
+	// Base instructions
+	//
+
+	public static int test_0_call () {
+		return check_missing_method (new TestDel (missing_call));
+	}
+
+	public static int test_0_jmp () {
+		return check_missing_method (new TestDel (missing_jmp));
+	}
+
+	public static int test_0_ldftn () {
+		return check_missing_method (new TestDel (missing_ldftn));
+	}
+
+	//
+	// Object model instructions
+	//
+
+	public static int test_0_box () {
+		// Thrown earlier
+		return 0;
+	}
+
+	public static int test_0_callvirt () {
+		return check_missing_method (new TestDel (missing_callvirt));
+	}
+
+	public static int test_0_castclass () {
+		return check_type_load (new TestDel (missing_castclass));
+	}
+
+	public static int test_0_cpobj () {
+		return check_type_load (new TestDel (missing_cpobj));
+	}
+
+	public static int test_0_initobj () {
+		return check_type_load (new TestDel (missing_initobj));
+	}
+
+	public static int test_0_isinst () {
+		return check_type_load (new TestDel (missing_isinst));
+	}
+
+	public static int test_0_ldelem () {
+		// Thrown earlier
+		return 0;
+	}
+
+	public static int test_0_ldelema () {
+		// Thrown earlier
+		return 0;
+	}
+
+	public static int test_0_ldfld () {
+		return check_missing_field (new TestDel (missing_ldfld));
+	}		
+
+	public static int test_0_ldflda () {
+		return check_missing_field (new TestDel (missing_ldflda));
+	}		
+
+	public static int test_0_ldobj () {
+		// Thrown earlier
+		return 0;
+	}
+
+	public static int test_0_ldsfld () {
+		return check_missing_field (new TestDel (missing_ldsfld));
+	}		
+
+	public static int test_0_ldsflda () {
+		return check_missing_field (new TestDel (missing_ldsflda));
+	}		
+
+	public static int test_0_ldtoken_type () {
+		return check_type_load (new TestDel (missing_ldtoken_type));
+	}
+
+	public static int test_0_ldtoken_method () {
+		return check_missing_method (new TestDel (missing_ldtoken_method));
+	}
+
+	public static int test_0_ldtoken_field () {
+		return check_missing_field (new TestDel (missing_ldtoken_field));
+	}
+
+	public static int test_0_ldvirtftn () {
+		return check_missing_method (new TestDel (missing_ldvirtftn));
+	}
+
+	public static int test_0_mkrefany () {
+		// Thrown earlier
+		return 0;
+	}
+
+	public static int test_0_newarr () {
+		return check_type_load (new TestDel (missing_newarr));
+	}		
+
+	public static int test_0_newobj () {
+		return check_missing_method (new TestDel (missing_newobj));
+	}		
+
+	public static int test_0_refanyval () {
+		return check_type_load (new TestDel (missing_refanyval));
+	}
+
+	public static int test_0_sizeof () {
+		return check_type_load (new TestDel (missing_sizeof));
+	}
+
+	public static int test_0_stelem () {
+		// Thrown earlier
+		return 0;
+	}
+
+	public static int test_0_stfld () {
+		return check_missing_field (new TestDel (missing_stfld));
+	}
+
+	public static int test_0_stobj () {
+		// Thrown earlier
+		return 0;
+	}
+
+	public static int test_0_stsfld () {
+		return check_missing_field (new TestDel (missing_stsfld));
+	}
+
+	public static int test_0_unbox () {
+		return check_type_load (new TestDel (missing_unbox));
+	}
+
+	public static int test_0_unbox_any () {
+		return check_type_load (new TestDel (missing_unbox_any));
+	}
+
+	//
+	// Missing classes referenced from metadata
+	//
+
+	// FIXME: These do not work yet
+#if FALSE
+	public static int test_0_missing_local () {
+		try {
+			missing_local ();
+		}
+		catch (TypeLoadException ex) {
+		}
+
+		/* MS.NET doesn't throw an exception if a local is not found */
+		return 0;
+	}
+
+	public static void missing_parent () {
+		new Miss1 ();
+	}
+
+	public static int test_0_missing_parent () {
+		return check_type_load (new TestDel (missing_parent));
+	}
+#endif
+
+	public static int Main () {
+		return TestDriver.RunTests (typeof (Tests));
+	}
+}
