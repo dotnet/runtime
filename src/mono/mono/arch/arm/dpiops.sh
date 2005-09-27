2bfac144801f32c73ffd3de1e8ba9aeb87ca97eb
Raja R Harinath <harinath@hurrynot.org>
2005-09-27 09:09:41 +0000

    * arm/dpiops.sh, arm/fpaops.h: Output to stdout. * arm/Makefile.am (arm_dpimacros.h, arm_fpamacros.h): Update.  Fix for srcdir != builddir.
    svn path=/trunk/mono/; revision=50833
    
    
    Commit migrated from https://github.com/mono/mono/commit/f6eb9cb62509f3556cf1fa54c854f79f727b8876

--- src/mono/mono/arch/arm/dpiops.sh
+++ src/mono/mono/arch/arm/dpiops.sh
@@ -3,30 +3,28 @@
 OPCODES="AND EOR SUB RSB ADD ADC SBC RSC ORR BIC"
 CMP_OPCODES="TST TEQ CMP CMN"
 MOV_OPCODES="MOV MVN"
-OUTFILE=arm_dpimacros.h
 
 # $1: opcode list
 # $2: template
 function gen() {
 	for i in $1; do
-		sed "s/<Op>/$i/g" $2.th >> $OUTFILE
+		sed "s/<Op>/$i/g" $2.th
 	done
 }
 
 
 
-echo -e "/* Macros for DPI ops, auto-generated from template */\n" > $OUTFILE
+echo -e "/* Macros for DPI ops, auto-generated from template */\n"
 
-echo -e "\n/* mov/mvn */\n" >>  $OUTFILE
+echo -e "\n/* mov/mvn */\n"
 gen "$MOV_OPCODES" mov_macros
 
-echo -e "\n/* DPIs, arithmetic and logical */\n" >>  $OUTFILE
+echo -e "\n/* DPIs, arithmetic and logical */\n"
 gen "$OPCODES" dpi_macros
 
-echo -e "\n\n" >> $OUTFILE
+echo -e "\n\n"
 
-echo -e "\n/* DPIs, comparison */\n" >>  $OUTFILE
+echo -e "\n/* DPIs, comparison */\n"
 gen "$CMP_OPCODES" cmp_macros
 
-echo -e "\n/* end generated */\n" >> $OUTFILE
-
+echo -e "\n/* end generated */\n"
--- src/mono/mono/arch/arm/fpaops.sh
+++ src/mono/mono/arch/arm/fpaops.sh
@@ -2,25 +2,23 @@
 
 DYADIC="ADF MUF SUF RSF DVF RDF POW RPW RMF FML FDV FRD POL"
 MONADIC="MVF MNF ABS RND SQT LOG EXP SIN COS TAN ASN ACS ATN URD NRM"
-OUTFILE=arm_fpamacros.h
 
 # $1: opcode list
 # $2: template
 function gen() {
 	for i in $1; do
-		sed "s/<Op>/$i/g" $2.th >> $OUTFILE
+		sed "s/<Op>/$i/g" $2.th
 	done
 }
 
-echo -e "/* Macros for FPA ops, auto-generated from template */\n" > $OUTFILE
+echo -e "/* Macros for FPA ops, auto-generated from template */\n"
 
-echo -e "\n/* dyadic */\n" >>  $OUTFILE
+echo -e "\n/* dyadic */\n"
 gen "$DYADIC" fpa_macros
 
-echo -e "\n/* monadic */\n" >>  $OUTFILE
+echo -e "\n/* monadic */\n"
 gen "$MONADIC" fpam_macros
 
 echo -e "\n\n" >> $OUTFILE
 
-echo -e "\n/* end generated */\n" >> $OUTFILE
-
+echo -e "\n/* end generated */\n"
