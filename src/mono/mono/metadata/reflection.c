eea2bed8675db8aeadb20d0d2b1e3f4d348c282b
Zoltan Varga <vargaz@gmail.com>
2005-10-26 17:28:20 +0000

    2005-10-26  Zoltan Varga  <vargaz@gmail.com>
    	* reflection.c (module_add_cattrs): Save custom attributes for global methods
    	and fields as well.
    
    svn path=/trunk/mono/; revision=52250
    
    
    Commit migrated from https://github.com/mono/mono/commit/9f8a7b1212227c98ab982eb46d14285b3e41104c

--- src/mono/mono/metadata/reflection.c
+++ src/mono/mono/metadata/reflection.c
@@ -3010,17 +3010,31 @@ type_add_cattrs (MonoDynamicImage *assembly, MonoReflectionTypeBuilder *tb) {
 }
 
 static void
-module_add_cattrs (MonoDynamicImage *assembly, MonoReflectionModuleBuilder *mb) {
+module_add_cattrs (MonoDynamicImage *assembly, MonoReflectionModuleBuilder *moduleb)
+{
 	int i;
 	
-	mono_image_add_cattrs (assembly, mb->table_idx, MONO_CUSTOM_ATTR_MODULE, mb->cattrs);
+	mono_image_add_cattrs (assembly, moduleb->table_idx, MONO_CUSTOM_ATTR_MODULE, moduleb->cattrs);
+
+	if (moduleb->global_methods) {
+		for (i = 0; i < mono_array_length (moduleb->global_methods); ++i) {
+			MonoReflectionMethodBuilder* mb = mono_array_get (moduleb->global_methods, MonoReflectionMethodBuilder*, i);
+			mono_image_add_cattrs (assembly, mb->table_idx, MONO_CUSTOM_ATTR_METHODDEF, mb->cattrs);
+			params_add_cattrs (assembly, mb->pinfo);
+		}
+	}
+
+	if (moduleb->global_fields) {
+		for (i = 0; i < mono_array_length (moduleb->global_fields); ++i) {
+			MonoReflectionFieldBuilder *fb = mono_array_get (moduleb->global_fields, MonoReflectionFieldBuilder*, i);
+			mono_image_add_cattrs (assembly, fb->table_idx, MONO_CUSTOM_ATTR_FIELDDEF, fb->cattrs);
+		}
+	}
 	
-	/* no types in the module */
-	if (!mb->types)
-		return;
-	
-	for (i = 0; i < mb->num_types; ++i)
-		type_add_cattrs (assembly, mono_array_get (mb->types, MonoReflectionTypeBuilder*, i));
+	if (moduleb->types) {
+		for (i = 0; i < moduleb->num_types; ++i)
+			type_add_cattrs (assembly, mono_array_get (moduleb->types, MonoReflectionTypeBuilder*, i));
+	}
 }
 
 static void
