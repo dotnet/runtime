093cf55d1c1b8ef1faa07681a38a60fc50ecf909
Zoltan Varga <vargaz@gmail.com>
2005-12-12 19:14:17 +0000

    2005-12-12  Zoltan Varga  <vargaz@gmail.com>
    	* generics.2.cs: Add valuetype tests.
    
    svn path=/trunk/mono/; revision=54251
    
    
    Commit migrated from https://github.com/mono/mono/commit/f2094d27e7fb47a46029c83fae653fe93285133d

--- src/mono/mono/mini/generics.2.cs
+++ src/mono/mono/mini/generics.2.cs
@@ -2,6 +2,14 @@ using System;
 
 class Tests {
 
+	struct TestStruct {
+		public int i;
+
+		public TestStruct (int i) {
+			this.i = i;
+		}
+	}
+
 	static int Main ()
 	{
 		return TestDriver.RunTests (typeof (Tests));
@@ -31,6 +39,32 @@ class Tests {
 	{
 		object o = 1;
 		return (o is int?) ? 1 : 0;
+	}
+	
+	public static int test_1_nullable_unbox_vtype ()
+	{
+		return Unbox<TestStruct?> (new TestStruct (1)).Value.i;
+	}
+
+	public static int test_1_nullable_unbox_null_vtype ()
+	{
+		return Unbox<int?> (null).HasValue ? 0 : 1;
+	}
+
+	public static int test_1_nullable_box_vtype ()
+	{
+		return ((TestStruct)(Box<TestStruct?> (new TestStruct (1)))).i;
+	}
+
+	public static int test_1_nullable_box_null_vtype ()
+	{
+		return Box<TestStruct?> (null) == null ? 1 : 0;
+	}
+
+	public static int test_1_isinst_nullable_vtype ()
+	{
+		object o = new TestStruct (1);
+		return (o is TestStruct?) ? 1 : 0;
 	}
 
 	static object Box<T> (T t)
