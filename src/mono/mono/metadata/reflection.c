152d9eedb3db09c611370390a758ffd7806af587
Zoltan Varga <vargaz@gmail.com>
2005-12-13 14:09:58 +0000

    2005-12-13  Zoltan Varga  <vargaz@gmail.com>
    	* reflection.c (custom_attr_visible): Really fix this.
    
    svn path=/trunk/mono/; revision=54280
    
    
    Commit migrated from https://github.com/mono/mono/commit/4efe5f7789a09682f1923370aca4a8b480d132d2

--- src/mono/mono/metadata/reflection.c
+++ src/mono/mono/metadata/reflection.c
@@ -1048,10 +1048,14 @@ static gboolean
 custom_attr_visible (MonoImage *image, MonoReflectionCustomAttr *cattr)
 {
 	/* FIXME: Need to do more checks */
-	if (cattr->ctor->method && (cattr->ctor->method->klass->image != image) && !((cattr->ctor->method->klass->flags & TYPE_ATTRIBUTE_VISIBILITY_MASK) & (TYPE_ATTRIBUTE_PUBLIC|TYPE_ATTRIBUTE_NESTED_PUBLIC)))
-		return FALSE;
-	else
-		return TRUE;
+	if (cattr->ctor->method && (cattr->ctor->method->klass->image != image)) {
+		int visibility = cattr->ctor->method->klass->flags & TYPE_ATTRIBUTE_VISIBILITY_MASK;
+
+		if ((visibility != TYPE_ATTRIBUTE_PUBLIC) && (visibility != TYPE_ATTRIBUTE_NESTED_PUBLIC))
+			return FALSE;
+	}
+
+	return TRUE;
 }
 
 static MonoCustomAttrInfo*
