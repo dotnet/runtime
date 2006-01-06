99c6433077e75f4f4a536f9b075da62c115e6217
Neale Ferguson <neale@mono-cvs.ximian.com>
2006-01-06 02:40:59 +0000

    * mini-s390x.c (emit_float_to_int): Correct r8 to unsigned int algorithm.
    svn path=/trunk/mono/; revision=55126
    
    
    Commit migrated from https://github.com/mono/mono/commit/f6413fb5db842ea249c0bc214db987eb4341f019

--- src/mono/mono/mini/mini-s390x.c
+++ src/mono/mono/mini/mini-s390x.c
@@ -2332,9 +2332,9 @@ emit_float_to_int (MonoCompile *cfg, guchar *code, int dreg, int sreg, int size,
 		s390_llong  (code, 0x41e0000000000000);
 		s390_llong  (code, 0x41f0000000000000);
 		s390_ldr    (code, s390_f15, sreg);
-		s390_cdb    (code, s390_f15, 0, s390_r13, 0);
+		s390_cdb    (code, s390_f15, 0, s390_r13, 4);
 		s390_jl     (code, 0); CODEPTR (code, o[0]);
-		s390_sdb    (code, s390_f15, 0, s390_r13, 8);
+		s390_sdb    (code, s390_f15, 0, s390_r13, 12);
 		s390_cfdbr  (code, dreg, 7, s390_f15);
 		s390_j      (code, 4);
 		PTRSLOT (code, o[0]);
