7033c46d51ef769665b93c2bd53979a1379b4aab
Zoltan Varga <vargaz@gmail.com>
2006-01-17 15:23:53 +0000

    2006-01-17  Zoltan Varga  <vargaz@gmail.com>
    	* mini.h: Move mono_bitset_test_fast macro to monobitset.h where it belongs.
    
    svn path=/trunk/mono/; revision=55681
    
    
    Commit migrated from https://github.com/mono/mono/commit/c06ee790ff408975e6d33706557cc984d9154983

--- src/mono/mono/mini/mini.h
+++ src/mono/mono/mini/mini.h
@@ -40,12 +40,6 @@
 #define MONO_AOT_FILE_VERSION "25"
 
 #if 0
-#define mono_bitset_test_fast(set,n) (((guint32*)set)[2+(n)/32] & (1 << ((n) % 32)))
-#else
-#define mono_bitset_test_fast(set,n) mono_bitset_test(set,n)
-#endif
-
-#if 0
 #define mono_bitset_foreach_bit(set,b,n) \
 	for (b = 0; b < n; b++)\
 		if (mono_bitset_test_fast(set,b))
