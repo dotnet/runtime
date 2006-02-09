d51607d09b257ae7ebb190b3529cd9d2af7851cc
Zoltan Varga <vargaz@gmail.com>
2006-02-09 14:39:38 +0000

    2006-02-09  Zoltan Varga  <vargaz@gmail.com>
    	* generics.2.cs: Add tests for nullable reference types.
    
    svn path=/trunk/mono/; revision=56704
    
    
    Commit migrated from https://github.com/mono/mono/commit/4127629e4f43b5299eb8214a629fb0d233fa4bfd

--- src/mono/mono/mini/generics.2.cs
+++ src/mono/mono/mini/generics.2.cs
@@ -61,10 +61,30 @@ class Tests {
 		return Box<TestStruct?> (null) == null ? 1 : 0;
 	}
 
-	public static int test_1_isinst_nullable_vtype ()
+	public static int test_1_nullable_unbox_reftype ()
 	{
-		object o = new TestStruct (1);
-		return (o is TestStruct?) ? 1 : 0;
+		return Unbox<String?> ("ABC").Value == "ABC" ? 1 : 0;
+	}
+
+	public static int test_1_nullable_unbox_null_reftype ()
+	{
+		return Unbox<String?> (null).HasValue ? 0 : 1;
+	}
+
+	public static int test_1_nullable_box_reftype ()
+	{
+		return ((String)(Box<String?> ("ABC"))) == "ABC" ? 1 : 0;
+	}
+
+	public static int test_1_nullable_box_null_reftype ()
+	{
+		return Box<String?> (null) == null ? 1 : 0;
+	}
+
+	public static int test_1_isinst_nullable_reftype ()
+	{
+		object o = "ABC";
+		return (o is String?) ? 1 : 0;
 	}
 
 	public static void stelem_any<T> (T[] arr, T elem) {
