efa2600c482c23f9b2d65f8e20b557619b428c01
Zoltan Varga <vargaz@gmail.com>
2006-03-01 21:02:26 +0000

    2006-03-01  Zoltan Varga  <vargaz@gmail.com>
    	* class.c (mono_class_create_from_typedef): Mark classes using CharSet.Auto as unicode on
    	windows. Fixes mono/mono#77683.
    
    svn path=/trunk/mono/; revision=57486
    
    
    Commit migrated from https://github.com/mono/mono/commit/04160ef32ea0f2268fe51c3ce15dbfea5dd426a8

--- src/mono/mono/metadata/class.c
+++ src/mono/mono/metadata/class.c
@@ -2710,10 +2710,11 @@ mono_class_create_from_typedef (MonoImage *image, guint32 type_token)
 
 	if ((class->flags & TYPE_ATTRIBUTE_STRING_FORMAT_MASK) == TYPE_ATTRIBUTE_UNICODE_CLASS)
 		class->unicode = 1;
-	/* fixme: maybe we must set this on windows 
+
+#if PLATFORM_WIN32
 	if ((class->flags & TYPE_ATTRIBUTE_STRING_FORMAT_MASK) == TYPE_ATTRIBUTE_AUTO_CLASS)
 		class->unicode = 1;
-	*/
+#endif
 
 	class->cast_class = class->element_class = class;
 
