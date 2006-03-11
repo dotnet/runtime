2759ee1c3fe6a169301d726cf3f52e05eb378659
Zoltan Varga <vargaz@gmail.com>
2006-03-11 15:28:16 +0000

    2006-03-11  Zoltan Varga  <vargaz@gmail.com>
    	* icall.c (mono_register_jit_icall_wrapper): Register the argument, not
    	info->wrapper.
    
    svn path=/trunk/mono/; revision=57825
    
    
    Commit migrated from https://github.com/mono/mono/commit/9625c1abde149657b33a4341dfb52e34d62d285d

--- src/mono/mono/metadata/icall.c
+++ src/mono/mono/metadata/icall.c
@@ -7582,7 +7582,7 @@ void
 mono_register_jit_icall_wrapper (MonoJitICallInfo *info, gconstpointer wrapper)
 {
 	mono_loader_lock ();
-	g_hash_table_insert (jit_icall_hash_addr, (gpointer)info->wrapper, info);	
+	g_hash_table_insert (jit_icall_hash_addr, wrapper, info);
 	mono_loader_unlock ();
 }
 
