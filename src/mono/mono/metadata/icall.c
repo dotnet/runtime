b57def78733bb1668ab548a1def621a909dbc17b
Zoltan Varga <vargaz@gmail.com>
2006-03-11 15:30:02 +0000

    2006-03-11  Zoltan Varga  <vargaz@gmail.com>
    	* icall.c (mono_register_jit_icall_wrapper): Fix a warning.
    
    svn path=/trunk/mono/; revision=57826
    
    
    Commit migrated from https://github.com/mono/mono/commit/adf488d9aac315ab767b41c25d26ac9eb450a516

--- src/mono/mono/metadata/icall.c
+++ src/mono/mono/metadata/icall.c
@@ -7582,7 +7582,7 @@ void
 mono_register_jit_icall_wrapper (MonoJitICallInfo *info, gconstpointer wrapper)
 {
 	mono_loader_lock ();
-	g_hash_table_insert (jit_icall_hash_addr, wrapper, info);
+	g_hash_table_insert (jit_icall_hash_addr, (gpointer)wrapper, info);
 	mono_loader_unlock ();
 }
 
