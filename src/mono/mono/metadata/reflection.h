d3809b2b133427bf9fde9a5d61f0157e89d1068c
Miguel de Icaza <miguel@gnome.org>
2006-05-01 20:35:48 +0000

    Revert this one
    svn path=/trunk/mono/; revision=60148
    
    
    Commit migrated from https://github.com/mono/mono/commit/07a24dd900fde81c6939de8f1da8e963e09f3661

--- src/mono/mono/metadata/reflection.h
+++ src/mono/mono/metadata/reflection.h
@@ -65,7 +65,7 @@ MonoReflectionEvent*    mono_event_get_object    (MonoDomain *domain, MonoClass 
 MonoArray* mono_param_get_objects  (MonoDomain *domain, MonoMethod *method);
 MonoReflectionMethodBody* mono_method_body_get_object (MonoDomain *domain, MonoMethod *method);
 MonoObject* mono_get_dbnull_object (MonoDomain *domain);
-
+MonoArray*  mono_reflection_get_custom_attrs_by_type (MonoObject *obj, MonoClass *attr_klass);
 MonoArray*  mono_reflection_get_custom_attrs (MonoObject *obj);
 MonoArray*  mono_reflection_get_custom_attrs_data (MonoObject *obj);
 MonoArray*  mono_reflection_get_custom_attrs_blob (MonoReflectionAssembly *assembly, MonoObject *ctor, MonoArray *ctorArgs, MonoArray *properties, MonoArray *porpValues, MonoArray *fields, MonoArray* fieldValues);
