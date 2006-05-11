b45aa020cc6b96ea32f6a81bed20afe81e39c0ab
Zoltan Varga <vargaz@gmail.com>
2006-05-11 11:36:01 +0000

    2006-05-11  Zoltan Varga  <vargaz@gmail.com>
    	* reflection.c (mono_image_basic_init): Fix a small memory leak.
    
    svn path=/trunk/mono/; revision=60588
    
    
    Commit migrated from https://github.com/mono/mono/commit/1b94c7a0b1736a70236a51d2c06bcced6df4e547

--- src/mono/mono/metadata/reflection.c
+++ src/mono/mono/metadata/reflection.c
@@ -4433,19 +4433,21 @@ mono_image_basic_init (MonoReflectionAssemblyBuilder *assemblyb)
 		assembly->assembly.aname.culture = g_strdup ("");
 
         if (assemblyb->version) {
-                char **version = g_strsplit (mono_string_to_utf8 (assemblyb->version), ".", 4);
-                char **parts = version;
-                assembly->assembly.aname.major = atoi (*parts++);
-                assembly->assembly.aname.minor = atoi (*parts++);
-                assembly->assembly.aname.build = *parts != NULL ? atoi (*parts++) : 0;
-                assembly->assembly.aname.revision = *parts != NULL ? atoi (*parts) : 0;
+			char *vstr = mono_string_to_utf8 (assemblyb->version);
+			char **version = g_strsplit (vstr, ".", 4);
+			char **parts = version;
+			assembly->assembly.aname.major = atoi (*parts++);
+			assembly->assembly.aname.minor = atoi (*parts++);
+			assembly->assembly.aname.build = *parts != NULL ? atoi (*parts++) : 0;
+			assembly->assembly.aname.revision = *parts != NULL ? atoi (*parts) : 0;
 
-                g_strfreev (version);
+			g_strfreev (version);
+			g_free (vstr);
         } else {
-                assembly->assembly.aname.major = 0;
-                assembly->assembly.aname.minor = 0;
-                assembly->assembly.aname.build = 0;
-                assembly->assembly.aname.revision = 0;
+			assembly->assembly.aname.major = 0;
+			assembly->assembly.aname.minor = 0;
+			assembly->assembly.aname.build = 0;
+			assembly->assembly.aname.revision = 0;
         }
 
 	assembly->run = assemblyb->access != 2;
