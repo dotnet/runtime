189386c785126347afe6b27b26f98dcf8e39f365
Miguel de Icaza <miguel@gnome.org>
2006-06-08 17:33:58 +0000

    Revert until Martin is online to debug
    svn path=/trunk/mono/; revision=61578
    
    
    Commit migrated from https://github.com/mono/mono/commit/ca92689761c1d732efd27266b6f2e981dabcef17

--- src/mono/mono/metadata/debug-mono-symfile.c
+++ src/mono/mono/metadata/debug-mono-symfile.c
@@ -199,7 +199,7 @@ mono_debug_symfile_lookup_location (MonoDebugMethodInfo *minfo, guint32 offset)
 	MonoSymbolFile *symfile;
 	gchar *source_file = NULL;
 	const char *ptr;
-	int count, i;
+	int i;
 
 	if ((symfile = minfo->handle->symfile) == NULL)
 		return NULL;
@@ -216,13 +216,12 @@ mono_debug_symfile_lookup_location (MonoDebugMethodInfo *minfo, guint32 offset)
 
 	ptr = symfile->raw_contents + read32(&(minfo->entry->_line_number_table_offset));
 
-	count = read32(&(minfo->entry->_num_line_numbers));
-	lne = ((MonoSymbolFileLineNumberEntry *) ptr) + count;
+	lne = (MonoSymbolFileLineNumberEntry *) ptr;
 
-	for (i = count - 1; i >= 0; i--, lne--) {
+	for (i = 0; i < read32(&(minfo->entry->_num_line_numbers)); i++, lne++) {
 		MonoDebugSourceLocation *location;
 
-		if (read32(&(lne->_offset)) > offset)
+		if (read32(&(lne->_offset)) < offset)
 			continue;
 
 		location = g_new0 (MonoDebugSourceLocation, 1);
