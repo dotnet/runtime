5504869acbde20e03aef9911ed09838a1ba8e73c
Zoltan Varga <vargaz@gmail.com>
2006-06-10 13:25:10 +0000

    2006-06-10  Zoltan Varga  <vargaz@gmail.com>
    	* interlocked-2.2.cs: Make the return values positive.
    
    svn path=/trunk/mono/; revision=61613
    
    
    Commit migrated from https://github.com/mono/mono/commit/0052660efed7acb4c642cf8d950bb9eb63e5c531

--- src/mono/mono/tests/interlocked-2.2.cs
+++ src/mono/mono/tests/interlocked-2.2.cs
@@ -6,8 +6,6 @@ public class InterlockTest
 	public int test;
 	public int ltest;
 
-	static int s_test;
-	
 	public static int Main() {
 		int a,b;
 		long la, lb;
@@ -18,33 +16,33 @@ public class InterlockTest
 		it.test = 2;
 		int c = Interlocked.Add (ref it.test, 1);
 		if (c != 3)
-			return -1;
+			return 1;
 
 		if (it.test != 3)
-			return -2;
+			return 2;
 
 		a = 1;
 		b = Interlocked.Add (ref a, 1);
 		if (a != 2)
-			return -3;
+			return 3;
 		if (b != 2)
-			return -4;
+			return 4;
 
 		/* long */
 		it.ltest = 2;
 		int lc = Interlocked.Add (ref it.ltest, 1);
 		if (lc != 3)
-			return -5;
+			return 5;
 
 		if (it.ltest != 3)
-			return -6;
+			return 6;
 
 		la = 1;
 		lb = Interlocked.Add (ref la, 1);
 		if (la != 2)
-			return -7;
+			return 7;
 		if (lb != 2)
-			return -8;
+			return 8;
 
 		Console.WriteLine ("done!");
 
