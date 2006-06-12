1c54798441bc927b3a55286a28be9d480e5d44a9
Jonathan Chambers <joncham@gmail.com>
2006-06-12 00:53:44 +0000

    Fix for building mono in VS, and implement reading/writing USER/MACHINE level environment variables.
    svn path=/trunk/mono/; revision=61621
    
    
    Commit migrated from https://github.com/mono/mono/commit/2d67b06422c60cb69b694dfc3e134514dda033be

--- src/mono/mono/metadata/icall.c
+++ src/mono/mono/metadata/icall.c
@@ -5622,23 +5622,43 @@ ves_icall_System_Environment_GetEnvironmentVariableNames (void)
 static void
 ves_icall_System_Environment_InternalSetEnvironmentVariable (MonoString *name, MonoString *value)
 {
+#ifdef PLATFORM_WIN32
+	gunichar2 *utf16_name, *utf16_value;
+#else
 	gchar *utf8_name, *utf8_value;
+#endif
 
 	MONO_ARCH_SAVE_REGS;
+	
+#ifdef PLATFORM_WIN32
+	utf16_name = mono_string_to_utf16 (name);
+	if ((value == NULL) || (mono_string_length (value) == 0) || (mono_string_chars (value)[0] == 0)) {
+		SetEnvironmentVariable (utf16_name, NULL);
+		g_free (utf16_name);
+		return;
+	}
 
+	utf16_value = mono_string_to_utf16 (value);
+
+	SetEnvironmentVariable (utf16_name, utf16_value);
+
+	g_free (utf16_name);
+	g_free (utf16_value);
+#else
 	utf8_name = mono_string_to_utf8 (name);	/* FIXME: this should be ascii */
 
 	if ((value == NULL) || (mono_string_length (value) == 0) || (mono_string_chars (value)[0] == 0)) {
 		g_unsetenv (utf8_name);
+		g_free (utf8_name);
 		return;
 	}
 
 	utf8_value = mono_string_to_utf8 (value);
-
 	g_setenv (utf8_name, utf8_value, TRUE);
 
 	g_free (utf8_name);
 	g_free (utf8_value);
+#endif
 }
 
 /*
