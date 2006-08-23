b9bec9b775778257750bbc1b4ff390a407b4f9c0
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2006-08-23 17:37:53 +0000

    2006-08-23 Gonzalo Paniagua Javier <gonzalo@ximian.com>
    	* mono-stdlib.[ch]: mkstemp implementation.
    
    
    svn path=/trunk/mono/; revision=64257
    
    
    Commit migrated from https://github.com/mono/mono/commit/b0bba1ec55d4f8771a84dd34d133151ec7fb7915

---
+++ src/mono/mono/utils/mono-stdlib.c
@@ -0,0 +1,60 @@
+/*
+ * mono-stdlib.c: stdlib replacement functions.
+ * 
+ * Authors:
+ * 	Gonzalo Paniagua Javier (gonzalo@novell.com)
+ *
+ * (C) 2006 Novell, Inc.  http://www.novell.com
+ *
+ */
+#include <config.h>
+#include <glib.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "mono-stdlib.h"
+
+#ifndef HAVE_MKSTEMP
+#ifndef O_BINARY
+#define O_BINARY	0
+#endif
+
+int
+mono_mkstemp (char *templ)
+{
+	int ret;
+	int count = 27; /* Windows doc. */
+	char *t;
+	int len;
+
+	len = strlen (templ);
+	do {
+		t = mktemp (templ);
+		if (t == NULL) {
+			errno = EINVAL;
+			return -1;
+		}
+
+		if (*templ == '\0') {
+			return -1;
+		}
+
+			
+		ret = open (templ, O_RDWR | O_BINARY | O_CREAT | O_EXCL, 0600);
+		if (ret == -1 && errno != EEXIST) {
+			return -1;
+		}
+
+		if (ret == -1) {
+			memcpy (templ + len - 6, "XXXXXX", 6);
+		}
+
+	} while (count-- > 0);
+
+	return ret;
+}
+#endif
+
