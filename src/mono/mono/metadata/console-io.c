63a4ac237c79af8bf18b6fc76c02a47b79d27ddf
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2006-09-19 18:27:12 +0000

    2006-09-19 Gonzalo Paniagua Javier <gonzalo@ximian.com>
    	* console-io.[ch]: trigger the ConsoleCancelEvent and retrieve
    	characters that need special handling.
    
    
    svn path=/trunk/mono/; revision=65679
    
    
    Commit migrated from https://github.com/mono/mono/commit/f346556361d0e92b8d47ac8e785cd3c21b7bde38

--- src/mono/mono/metadata/console-io.c
+++ src/mono/mono/metadata/console-io.c
@@ -16,6 +16,12 @@
 #include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <mono/metadata/appdomain.h>
+#include <mono/metadata/object-internals.h>
+#include <mono/metadata/class-internals.h>
+#include <mono/metadata/domain-internals.h>
+#include <mono/metadata/metadata.h>
+#include <mono/metadata/threadpool.h>
 /* On solaris, curses.h must come before both termios.h and term.h */
 #ifdef HAVE_CURSES_H
 #include <curses.h>
@@ -186,25 +192,92 @@ tty_teardown (void)
 
 	tcflush (STDIN_FILENO, TCIFLUSH);
 	tcsetattr (STDIN_FILENO, TCSANOW, &initial_attr);
+	set_property (ECHO, TRUE);
 	setup_finished = FALSE;
 }
 
+static void
+do_console_cancel_event (void)
+{
+	static MonoClassField *cancel_handler_field;
+	MonoDomain *domain = mono_domain_get ();
+	MonoClass *klass;
+	MonoDelegate *load_value;
+	MonoMethod *method;
+	MonoMethodMessage *msg;
+	MonoMethod *im;
+
+	if (!domain->domain)
+		return;
+
+	klass = mono_class_from_name (mono_defaults.corlib, "System", "Console");
+	if (klass == NULL)
+		return;
+
+	if (cancel_handler_field == NULL) {
+		cancel_handler_field = mono_class_get_field_from_name (klass, "cancel_handler");
+		g_assert (cancel_handler_field);
+	}
+
+	mono_field_static_get_value (mono_class_vtable (domain, klass), cancel_handler_field, &load_value);
+	if (load_value == NULL)
+		return;
+
+	klass = load_value->object.vtable->klass;
+	method = mono_class_get_method_from_name (klass, "BeginInvoke", -1);
+	g_assert (method != NULL);
+	im = mono_get_delegate_invoke (method->klass);
+	msg = mono_method_call_message_new (method, NULL, im, NULL, NULL);
+	mono_thread_pool_add ((MonoObject *) load_value, msg, NULL, NULL);
+}
+
+static void (*prev_int_handler)(int);
+static gboolean in_sigint;
+static void
+sigint_handler (int signo)
+{
+	MONO_ARCH_SAVE_REGS;
+
+	if (in_sigint)
+		return;
+
+	in_sigint = TRUE;
+	do_console_cancel_event ();
+	if (prev_int_handler != SIG_DFL && prev_int_handler != NULL)
+		prev_int_handler (signo);
+	in_sigint = FALSE;
+}
+
 MonoBoolean
-ves_icall_System_ConsoleDriver_TtySetup (MonoString *teardown)
+ves_icall_System_ConsoleDriver_TtySetup (MonoString *teardown, char *verase, char *vsusp, char*intr)
 {
 	struct termios attr;
 	
 	MONO_ARCH_SAVE_REGS;
 
+
+	*verase = '\0';
+	*vsusp = '\0';
+	*intr = '\0';
 	if (tcgetattr (STDIN_FILENO, &initial_attr) == -1)
 		return FALSE;
 
+	prev_int_handler = signal (SIGINT, sigint_handler);
+	/* TODO: handle SIGTSTP - Ctrl-Z */
 	attr = initial_attr;
+	/* TODO: Perhaps ISIG too? Or we intercept signals from unmanaged code */
 	attr.c_lflag &= ~ICANON;
 	attr.c_cc [VMIN] = 1;
 	attr.c_cc [VTIME] = 0;
 	if (tcsetattr (STDIN_FILENO, TCSANOW, &attr) == -1)
 		return FALSE;
+
+	*verase = initial_attr.c_cc [VERASE];
+	*vsusp = initial_attr.c_cc [VSUSP];
+	*intr = initial_attr.c_cc [VINTR];
+	/* If initialized from another appdomain... */
+	if (setup_finished)
+		return TRUE;
 
 	setup_finished = TRUE;
 	if (!atexit_called) {
@@ -223,6 +296,8 @@ ves_icall_System_ConsoleDriver_GetTtySize (HANDLE handle, gint32 *width, gint32 
 #ifdef TIOCGWINSZ
 	struct winsize ws;
 	int res;
+
+	MONO_ARCH_SAVE_REGS;
 
 	res = ioctl (GPOINTER_TO_INT (handle), TIOCGWINSZ, &ws);
 
