5ba55e5fe151cbfdff4831128b3d6523d843473f
Paolo Molaro <lupus@oddwiz.org>
2006-10-09 17:28:01 +0000

    Mon Oct 9 19:27:26 CEST 2006 Paolo Molaro <lupus@ximian.com>
    	* loader.c: check for NULL before initializing.
    
    
    svn path=/trunk/mono/; revision=66475
    
    
    Commit migrated from https://github.com/mono/mono/commit/eae1dfb8de35a98b27babef432372bd6bfa0efb7

--- src/mono/mono/metadata/loader.c
+++ src/mono/mono/metadata/loader.c
@@ -403,9 +403,9 @@ mono_field_from_token (MonoImage *image, guint32 token, MonoClass **retklass,
 		if (!type)
 			return NULL;
 		k = mono_class_get (image, MONO_TOKEN_TYPE_DEF | type);
-		mono_class_init (k);
 		if (!k)
 			return NULL;
+		mono_class_init (k);
 		if (retklass)
 			*retklass = k;
 		field = mono_class_get_field (k, token);
