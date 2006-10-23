11e31e62a7b80ad98d7cd9b52d4e7f0044d33197
Zoltan Varga <vargaz@gmail.com>
2006-10-23 10:35:08 +0000

    2006-10-23  Zoltan Varga  <vargaz@gmail.com>
    	* reflection.c (mono_method_get_object): Cache the MonoMethod class and its
    	friends.
    
    svn path=/trunk/mono/; revision=66897
    
    
    Commit migrated from https://github.com/mono/mono/commit/4f1d9cb0c9faf665fc94a816765dd10010d20d43

--- src/mono/mono/metadata/reflection.c
+++ src/mono/mono/metadata/reflection.c
@@ -5556,6 +5556,10 @@ mono_method_get_object (MonoDomain *domain, MonoMethod *method, MonoClass *refcl
 	 * We use the same C representation for methods and constructors, but the type 
 	 * name in C# is different.
 	 */
+	static MonoClass *System_Reflection_MonoMethod = NULL;
+	static MonoClass *System_Reflection_MonoCMethod = NULL;
+	static MonoClass *System_Reflection_MonoGenericMethod = NULL;
+	static MonoClass *System_Reflection_MonoGenericCMethod = NULL;
 	const char *cname;
 	MonoClass *klass;
 	MonoReflectionMethod *ret;
@@ -5566,12 +5570,15 @@ mono_method_get_object (MonoDomain *domain, MonoMethod *method, MonoClass *refcl
 		method = mono_get_inflated_method (method);
 		refclass = method->klass;
 		CHECK_OBJECT (MonoReflectionMethod *, method, refclass);
-		if ((*method->name == '.') && (!strcmp (method->name, ".ctor") || !strcmp (method->name, ".cctor")))
-			cname = "MonoGenericCMethod";
-		else
-			cname = "MonoGenericMethod";
-		klass = mono_class_from_name (mono_defaults.corlib, "System.Reflection", cname);
-
+		if ((*method->name == '.') && (!strcmp (method->name, ".ctor") || !strcmp (method->name, ".cctor"))) {
+			if (!System_Reflection_MonoGenericCMethod)
+				System_Reflection_MonoGenericCMethod = mono_class_from_name (mono_defaults.corlib, "System.Reflection", "MonoGenericCMethod");
+			klass = System_Reflection_MonoGenericCMethod;
+		} else {
+			if (!System_Reflection_MonoGenericMethod)
+				System_Reflection_MonoGenericMethod = mono_class_from_name (mono_defaults.corlib, "System.Reflection", "MonoGenericMethod");
+			klass = System_Reflection_MonoGenericMethod;
+		}
 		gret = (MonoReflectionGenericMethod*)mono_object_new (domain, klass);
 		gret->method.method = method;
 		MONO_OBJECT_SETREF (gret, method.name, mono_string_new (domain, method->name));
@@ -5583,12 +5590,16 @@ mono_method_get_object (MonoDomain *domain, MonoMethod *method, MonoClass *refcl
 		refclass = method->klass;
 
 	CHECK_OBJECT (MonoReflectionMethod *, method, refclass);
-	if (*method->name == '.' && (strcmp (method->name, ".ctor") == 0 || strcmp (method->name, ".cctor") == 0))
-		cname = "MonoCMethod";
-	else
-		cname = "MonoMethod";
-	klass = mono_class_from_name (mono_defaults.corlib, "System.Reflection", cname);
-
+	if (*method->name == '.' && (strcmp (method->name, ".ctor") == 0 || strcmp (method->name, ".cctor") == 0)) {
+		if (!System_Reflection_MonoCMethod)
+			System_Reflection_MonoCMethod = mono_class_from_name (mono_defaults.corlib, "System.Reflection", "MonoCMethod");
+		klass = System_Reflection_MonoCMethod;
+	}
+	else {
+		if (!System_Reflection_MonoMethod)
+			System_Reflection_MonoMethod = mono_class_from_name (mono_defaults.corlib, "System.Reflection", "MonoMethod");
+		klass = System_Reflection_MonoMethod;
+	}
 	ret = (MonoReflectionMethod*)mono_object_new (domain, klass);
 	ret->method = method;
 	MONO_OBJECT_SETREF (ret, name, mono_string_new (domain, method->name));
