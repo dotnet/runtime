f0d7acee3d32650f4ef40eba51d222fbe63e3027
Paolo Molaro <lupus@oddwiz.org>
2006-11-07 10:31:07 +0000

    Test case for bug mono/mono#79744.
    svn path=/trunk/mono/; revision=67445
    
    
    Commit migrated from https://github.com/mono/mono/commit/9d4c27fc77cd47718938746b16392c87190357f8

---
+++ src/mono/mono/tests/typeload-unaligned.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Runtime.InteropServices;
+
+public class Z {
+    ~Z() {
+        Console.WriteLine("Hello, world!");
+    }
+}
+
+[StructLayout(LayoutKind.Explicit)]
+public struct X {
+    [FieldOffset(0)] public short a;
+    [FieldOffset(2)] public Z z; // Unaligned reference
+}
+
+class Y {
+    static X test() {
+        X x = new X();
+        x.z = new Z();
+        return x;
+    }
+
+    static void test2(X x) {
+        Console.WriteLine("Object: " + x);
+    }
+
+    static int Main() {
+    	try {
+	        X t1 = test();
+        	System.GC.Collect();
+	        System.GC.Collect();
+        	System.GC.WaitForPendingFinalizers();
+	        test2(t1);
+	} catch (TypeLoadException e) {
+		Console.WriteLine ("got correct exception: {0}", e);
+		return 0;
+	}
+	return 1;
+    }
+}
