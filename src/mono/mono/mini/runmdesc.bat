d0b6febdacf38f74fc93a745bbb5f21e08494dec
Miguel de Icaza <miguel@gnome.org>
2006-12-01 21:47:31 +0000

    Add a step to run genmdesc from visual studio
    svn path=/trunk/mono/; revision=68855
    
    
    Commit migrated from https://github.com/mono/mono/commit/be1778076f1c28dac71e907d69dfac085c72f065

---
+++ src/mono/mono/mini/runmdesc.bat
@@ -0,0 +1,7 @@
+@echo off
+rem This runs genmdesc on the x86 files when called on Visual Studio
+echo Running genmdesc on the x86 files...
+cd mono\mini
+..\..\VSDependancies\genmdesc___win32_debug\genmdesc cpu-x86.md cpu-x86.h x86_desc
+echo done
+
