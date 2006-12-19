d90c2acf03d1825702eacd6c35c524b5b612dfad
Paolo Molaro <lupus@oddwiz.org>
2006-12-19 15:22:17 +0000

    Tue Dec 19 16:18:16 CET 2006 Paolo Molaro <lupus@ximian.com>
    	* mono-mlist.h, mono-mlist.c, Makefile.am: linked list
    	implementation for managed objects.
    
    
    svn path=/trunk/mono/; revision=69744
    
    
    Commit migrated from https://github.com/mono/mono/commit/6caebbd4c737e5755cd92a03808aabaa06c6d32c

---
+++ src/mono/mono/metadata/mono-mlist.c
@@ -0,0 +1,201 @@
+/*
+ * mono-mlist.c: Managed object list implementation
+ *
+ * Author:
+ *   Paolo Molaro (lupus@ximian.com)
+ *
+ * (C) 2006 Novell, Inc.
+ */
+
+#include "mono/metadata/mono-mlist.h"
+#include "mono/metadata/appdomain.h"
+#include "mono/metadata/class-internals.h"
+
+/* matches the System.MonoListItem objcet*/
+struct _MonoMList {
+	MonoObject object;
+	MonoMList *next;
+	MonoObject *data;
+};
+
+/* 
+ * note: we only allocate in the root domain: this lists are
+ * not exposed to managed code
+ */
+static MonoVTable *monolist_item_vtable = NULL;
+
+/**
+ * mono_mlist_alloc:
+ * @data: object to use as data
+ *
+ * Allocates a new managed list node with @data as the contents.
+ * A managed list node also represents a singly-linked list.
+ * Managed lists are garbage collected, so there is no free routine
+ * and the user is required to keep references to the managed list
+ * to prevent it from being garbage collected.
+ */
+MonoMList*
+mono_mlist_alloc (MonoObject *data)
+{
+	MonoMList* res;
+	if (!monolist_item_vtable) {
+		MonoClass *klass = mono_class_from_name (mono_defaults.corlib, "System", "MonoListItem");
+		monolist_item_vtable = mono_class_vtable (mono_get_root_domain (), klass);
+	}
+	res = (MonoMList*)mono_object_new_fast (monolist_item_vtable);
+	MONO_OBJECT_SETREF (res, data, data);
+	return res;
+}
+
+/**
+ * mono_mlist_get_data:
+ * @list: the managed list node
+ *
+ * Get the object stored in the list node @list.
+ */
+MonoObject*
+mono_mlist_get_data (MonoMList* list)
+{
+	return list->data;
+}
+
+/**
+ * mono_mlist_set_data:
+ * @list: the managed list node
+ *
+ * Set the object content in the list node @list.
+ */
+void
+mono_mlist_set_data (MonoMList* list, MonoObject *data)
+{
+	MONO_OBJECT_SETREF (list, data, data);
+}
+
+/**
+ * mono_mlist_length:
+ * @list: the managed list
+ *
+ * Get the number of items in the list @list.
+ * Since managed lists are singly-linked, this operation takes O(n) time.
+ */
+int
+mono_mlist_length (MonoMList* list)
+{
+	int len = 0;
+	while (list) {
+		list = list->next;
+		++len;
+	}
+	return len;
+}
+
+/**
+ * mono_mlist_next:
+ * @list: the managed list node
+ *
+ * Returns the next managed list node starting from @list.
+ */
+MonoMList*
+mono_mlist_next (MonoMList* list)
+{
+	return list->next;
+}
+
+/**
+ * mono_mlist_last:
+ * @list: the managed list node
+ *
+ * Returns the last managed list node in list @list.
+ * Since managed lists are singly-linked, this operation takes O(n) time.
+ */
+MonoMList*
+mono_mlist_last (MonoMList* list)
+{
+	if (list) {
+		while (list->next)
+			list = list->next;
+		return list;
+	}
+	return NULL;
+}
+
+/**
+ * mono_mlist_prepend:
+ * @list: the managed list
+ * @data: the object to add to the list
+ *
+ * Allocate a new list node with @data as content and prepend it
+ * to the list @list. @list can be NULL.
+ */
+MonoMList*
+mono_mlist_prepend (MonoMList* list, MonoObject *data)
+{
+	MonoMList* res = mono_mlist_alloc (data);
+	if (list)
+		MONO_OBJECT_SETREF (res, next, list);
+	return res;
+}
+
+/**
+ * mono_mlist_append:
+ * @list: the managed list
+ * @data: the object to add to the list
+ *
+ * Allocate a new list node with @data as content and append it
+ * to the list @list. @list can be NULL.
+ * Since managed lists are singly-linked, this operation takes O(n) time.
+ */
+MonoMList*
+mono_mlist_append (MonoMList* list, MonoObject *data)
+{
+	MonoMList* res = mono_mlist_alloc (data);
+	if (list) {
+		MonoMList* last = mono_mlist_last (list);
+		MONO_OBJECT_SETREF (last, next, res);
+		return list;
+	} else {
+		return res;
+	}
+}
+
+static MonoMList*
+find_prev (MonoMList* list, MonoMList *item)
+{
+	MonoMList* prev = NULL;
+	while (list) {
+		if (list == item)
+			break;
+		prev = list;
+		list = list->next;
+	}
+	return prev;
+}
+
+/**
+ * mono_mlist_remove_item:
+ * @list: the managed list
+ * @data: the object to add to the list
+ *
+ * Remove the list node @item from the managed list @list.
+ * Since managed lists are singly-linked, this operation can take O(n) time.
+ */
+MonoMList*
+mono_mlist_remove_item (MonoMList* list, MonoMList *item)
+{
+	MonoMList* prev;
+	if (list == item) {
+		list = item->next;
+		item->next = NULL;
+		return list;
+	}
+	prev = find_prev (list, item);
+	if (prev) {
+		MONO_OBJECT_SETREF (prev, next, item->next);
+		item->next = NULL;
+		return list;
+	} else {
+		/* not found */
+		return list;
+	}
+}
+
---
+++ src/mono/mono/metadata/mono-mlist.h
@@ -0,0 +1,23 @@
+#ifndef __MONO_METADATA_MONO_MLIST_H__
+#define __MONO_METADATA_MONO_MLIST_H__
+
+/*
+ * mono-mlist.h: Managed object list implementation
+ */
+
+#include <mono/metadata/object.h>
+
+typedef struct _MonoMList MonoMList;
+
+MonoMList*  mono_mlist_alloc       (MonoObject *data);
+MonoObject* mono_mlist_get_data    (MonoMList* list);
+void        mono_mlist_set_data    (MonoMList* list, MonoObject *data);
+int         mono_mlist_length      (MonoMList* list);
+MonoMList*  mono_mlist_next        (MonoMList* list);
+MonoMList*  mono_mlist_last        (MonoMList* list);
+MonoMList*  mono_mlist_prepend     (MonoMList* list, MonoObject *data);
+MonoMList*  mono_mlist_append      (MonoMList* list, MonoObject *data);
+MonoMList*  mono_mlist_remove_item (MonoMList* list, MonoMList *item);
+
+#endif /* __MONO_METADATA_MONO_MLIST_H__ */
+
