dd18f8bef861cc3a51dd6eb654e7091ec127c296
Martin Baulig <martin@novell.com>
2006-12-22 13:59:25 +0000

    Use .uleb128 instead of .long to make it work on x86_64.
    svn path=/trunk/mono/; revision=69936
    
    
    Commit migrated from https://github.com/mono/mono/commit/1a3437b67d952f7ac58b8a5c4377dd00fcfd6311

--- src/mono/mono/mini/mdb-debug-info.s
+++ src/mono/mono/mini/mdb-debug-info.s
@@ -2,4 +2,4 @@
 .global		MONO_DEBUGGER__debugger_info
 .global		MONO_DEBUGGER__debugger_info_ptr
 MONO_DEBUGGER__debugger_info_ptr:
-		.long	MONO_DEBUGGER__debugger_info
+		.uleb128	MONO_DEBUGGER__debugger_info
