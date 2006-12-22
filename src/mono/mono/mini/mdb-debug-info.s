7b756bb2f3fd9b49051f6e74c39f3f56c4a62107
Martin Baulig <martin@novell.com>
2006-12-22 13:39:28 +0000

    Making it readonly doesn't work.
    svn path=/trunk/mono/; revision=69935
    
    
    Commit migrated from https://github.com/mono/mono/commit/ee059807febd4aeb57f0111d648c687af9841ddc

--- src/mono/mono/mini/mdb-debug-info.s
+++ src/mono/mono/mini/mdb-debug-info.s
@@ -1,4 +1,5 @@
-.section	.mdb_debug_info, "a", @progbits
+.section	.mdb_debug_info, "aw", @progbits
+.global		MONO_DEBUGGER__debugger_info
 .global		MONO_DEBUGGER__debugger_info_ptr
 MONO_DEBUGGER__debugger_info_ptr:
 		.long	MONO_DEBUGGER__debugger_info
