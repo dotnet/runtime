d598f7b06d0610a4e2cab5f806e16f940b2204f9
Martin Baulig <martin@novell.com>
2006-12-22 17:48:03 +0000

    Small fix.
    svn path=/trunk/mono/; revision=69953
    
    
    Commit migrated from https://github.com/mono/mono/commit/4eaecfb03ed5330463ba6b3df6b28108cda47ad2

--- src/mono/mono/mini/mdb-debug-info32.s
+++ src/mono/mono/mini/mdb-debug-info32.s
@@ -1,5 +1,6 @@
-.section	.mdb_debug_info, "aw", @progbits
+.text
 .global		MONO_DEBUGGER__debugger_info
+.section	.mdb_debug_info, "aw", @progbits
 .global		MONO_DEBUGGER__debugger_info_ptr
 MONO_DEBUGGER__debugger_info_ptr:
 		.long	MONO_DEBUGGER__debugger_info
