8751a4c638e118331bbb36ba8100ea04ae2f43a1
Paolo Molaro <lupus@oddwiz.org>
2007-01-16 14:33:47 +0000

    Tue Jan 16 15:32:53 CET 2007 Paolo Molaro <lupus@ximian.com>
    	* icall.c: optimized InitializeArray() on bigendian.
    
    
    svn path=/trunk/mono/; revision=71135
    
    
    Commit migrated from https://github.com/mono/mono/commit/a79e4bcf96cace114d960b26bd4fc00e6e57f85b

--- src/mono/mono/metadata/icall.c
+++ src/mono/mono/metadata/icall.c
@@ -692,27 +692,17 @@ ves_icall_System_Runtime_CompilerServices_RuntimeHelpers_InitializeArray (MonoAr
 {
 	MonoClass *klass = array->obj.vtable->klass;
 	guint32 size = mono_array_element_size (klass);
-	int i;
 
-	MONO_ARCH_SAVE_REGS;
-
-	if (array->bounds == NULL)
-		size *= array->max_length;
-	else
-		for (i = 0; i < klass->rank; ++i) 
-			size *= array->bounds [i].length;
-
-	memcpy (mono_array_addr (array, char, 0), field_handle->data, size);
+	size *= array->max_length;
 
 #if G_BYTE_ORDER != G_LITTLE_ENDIAN
 #define SWAP(n) {\
-	gint i; \
-	guint ## n tmp; \
 	guint ## n *data = (guint ## n *) mono_array_addr (array, char, 0); \
+	guint ## n *src = (guint ## n *) field_handle->data; \
+	guint ## n *end = (guint ## n *)((char*)src + size); \
 \
-	for (i = 0; i < size; i += n/8, data++) { \
-		tmp = read ## n (data); \
-		*data = tmp; \
+	for (; src < end; data++, src++) { \
+		*data = read ## n (src); \
 	} \
 }
 
@@ -734,8 +724,12 @@ ves_icall_System_Runtime_CompilerServices_RuntimeHelpers_InitializeArray (MonoAr
 	case MONO_TYPE_R8:
 		SWAP (64);
 		break;
+	default:
+		memcpy (mono_array_addr (array, char, 0), field_handle->data, size);
+		break;
 	}
 #else
+	memcpy (mono_array_addr (array, char, 0), field_handle->data, size);
 #ifdef ARM_FPU_FPA
 	if (klass->element_class->byval_arg.type == MONO_TYPE_R8) {
 		gint i;
