d23e9ae896dc47f04a0c332f991c4c5ed16318e0
Zoltan Varga <vargaz@gmail.com>
2007-01-24 13:31:58 +0000

    2007-01-24  Zoltan Varga  <vargaz@gmail.com>
    	* mini-sparc.h (MONO_ARCH_FRAME_ALIGNMENT): Fix sparc build.
    
    svn path=/trunk/mono/; revision=71590
    
    
    Commit migrated from https://github.com/mono/mono/commit/09e9bb9f3a95a4f14191b466e800637ea31cdc4a

--- src/mono/mono/mini/mini-sparc.h
+++ src/mono/mono/mini/mini-sparc.h
@@ -50,8 +50,11 @@
 #define MONO_ARCH_INST_REGPAIR_REG2(desc,hreg1) (((desc == 'l') ? sparc_o0 : (desc == 'L' ? (hreg1 + 1) : -1)))
 #endif
 
-
-#define MONO_ARCH_FRAME_ALIGNMENT (sizeof (gpointer) * 2)
+#if SIZEOF_VOID_P == 8
+#define MONO_ARCH_FRAME_ALIGNMENT 16
+#else
+#define MONO_ARCH_FRAME_ALIGNMENT 8
+#endif
 
 #define MONO_ARCH_CODE_ALIGNMENT 32
 
