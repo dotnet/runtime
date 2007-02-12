6a7a9314bd9f0d978d71ffc555b1e7a9856d2859
Paolo Molaro <lupus@oddwiz.org>
2007-02-12 14:51:12 +0000

    Mon Feb 12 15:50:24 CET 2007 Paolo Molaro <lupus@ximian.com>
    	* shared.c: exit if semaphores can't be created (bug mono/mono#80616).
    
    
    svn path=/trunk/mono/; revision=72661
    
    
    Commit migrated from https://github.com/mono/mono/commit/70d26cab9b4fd0bab9d00d94cafca01df1b6daae

--- src/mono/mono/io-layer/shared.c
+++ src/mono/mono/io-layer/shared.c
@@ -330,10 +330,10 @@ again:
 		while ((_wapi_sem_id = semget (key, _WAPI_SHARED_SEM_COUNT,
 					       IPC_CREAT | IPC_EXCL | 0600)) == -1) {
 			if (errno == ENOMEM) {
-				g_critical ("%s: semget error: %s", __func__,
+				g_error ("%s: semget error: %s", __func__,
 					    g_strerror (errno));
 			} else if (errno == ENOSPC) {
-				g_critical ("%s: semget error: %s.  Try deleting some semaphores with ipcs and ipcrm", __func__, g_strerror (errno));
+				g_error ("%s: semget error: %s.  Try deleting some semaphores with ipcs and ipcrm\nor increase the maximum number of semaphore in the system.", __func__, g_strerror (errno));
 			} else if (errno != EEXIST) {
 				if (retries > 3)
 					g_warning ("%s: semget error: %s key 0x%x - trying again", __func__,
