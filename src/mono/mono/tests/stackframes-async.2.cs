b0cf9f8ece3e8f694bc373c0d96624d3227d2347
Paolo Molaro <lupus@oddwiz.org>
2007-02-14 17:40:04 +0000

    Test case for bugs mono/mono#80724 and mono/mono#79717.
    svn path=/trunk/mono/; revision=72847
    
    
    Commit migrated from https://github.com/mono/mono/commit/5764a415547d575f2b77de4aab87771f2ef695f8

---
+++ src/mono/mono/tests/stackframes-async.2.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Net;
+using System.Diagnostics;
+
+namespace stacktracetest
+{
+	class MainClass
+	{
+		static int frame_count = 0;
+		public static int Main(string[] args)
+		{
+			AsyncCallback cback = new AsyncCallback(ResolveCallback);
+			IAsyncResult res = Dns.BeginGetHostEntry("localhost", cback, null);
+			System.Threading.Thread.Sleep(2000);
+			/*
+			 * seems to be broken
+			while (!res.IsCompleted) {
+				System.Threading.Thread.Sleep(20);
+			};
+			IPHostEntry ip = Dns.EndGetHostEntry (res);
+			Console.WriteLine (ip);*/
+			if (frame_count > 1)
+				return 0;
+			return 1;
+		}
+		
+		public static void ResolveCallback(IAsyncResult ar)
+		{
+		    Console.WriteLine("ResolveCallback()");
+		    StackTrace st = new StackTrace();
+		    frame_count = st.FrameCount;
+	            for(int i = 0; i < st.FrameCount; i++) {
+	                StackFrame sf = st.GetFrame(i);
+        	        Console.WriteLine("method: {0}", sf.GetMethod());
+	            }
+        	    Console.WriteLine("ResolveCallback() complete");
+		}
+	}
+}
