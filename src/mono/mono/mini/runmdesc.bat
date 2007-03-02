610f475e4be49174dcf67484a8f8ab929d3a1b32
Jonathan Chambers <joncham@gmail.com>
2007-03-02 19:17:41 +0000

    Set executable flag on bat files so they will run on Windows. Also, set PATH variable in bat files so glib dlls will be found when run.
    svn path=/trunk/mono/; revision=73649
    
    
    Commit migrated from https://github.com/mono/mono/commit/d82761b65111606d33e5f0f7081e2bd3bb650bf4

--- src/mono/mono/mini/runmdesc.bat
+++ src/mono/mono/mini/runmdesc.bat
@@ -2,6 +2,7 @@
 rem This runs genmdesc on the x86 files when called on Visual Studio
 echo Running genmdesc on the x86 files...
 cd mono\mini
+set PATH=%PATH%;..\..\VSDependancies\lib
 ..\..\VSDependancies\genmdesc___win32_debug\genmdesc cpu-x86.md cpu-x86.h x86_desc
 echo done
 
--- src/mono/mono/monoburg/runburg.bat
+++ src/mono/mono/monoburg/runburg.bat
@@ -2,6 +2,7 @@
 rem This runs Monoburg on the various x86 files when called on Visual Studio
 echo Running Monoburg on the x86 inssel.brg files...
 cd ..\mini
+set PATH=%PATH%;..\..\VSDependancies\lib
 ..\..\VSDependancies\temp\monoburg\Debug\monoburg -c 1 -p -e inssel.brg inssel-float.brg inssel-long32.brg inssel-x86.brg -d inssel.h -s inssel.c
 echo done
 
