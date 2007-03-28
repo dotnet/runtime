13988160deb25ac46cc42a60eb1aca8498a91e45
Jb Evain <jbevain@gmail.com>
2007-03-28 13:14:14 +0000

    2007-03-28  Jb Evain  <jbevain@gmail.com>
    	* class.c (mono_type_get_name_recurse): Complete the
    	fix for the creation of assembly qualified names for
    	pointer types. Fixes mono/mono#81208.
    
    svn path=/trunk/mono/; revision=75070
    
    
    Commit migrated from https://github.com/mono/mono/commit/9df96c3fb6e27171b2a24fb3a6ed67792f136413

--- src/mono/mono/metadata/class.c
+++ src/mono/mono/metadata/class.c
@@ -254,7 +254,7 @@ mono_type_get_name_recurse (MonoType *type, GString *str, gboolean is_recursed,
 			type->data.type, str, FALSE, nested_format);
 		g_string_append_c (str, '*');
 		if (format == MONO_TYPE_NAME_FORMAT_ASSEMBLY_QUALIFIED)
-			_mono_type_get_assembly_name (type->data.type->data.klass, str);
+			_mono_type_get_assembly_name (mono_class_from_mono_type (type->data.type), str);
 		break;
 	}
 	case MONO_TYPE_VAR:
