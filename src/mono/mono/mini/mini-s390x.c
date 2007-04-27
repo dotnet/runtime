4dcd7ace5d1dd615ee89c43d263713dc293682f8
Wade Berrier <wade@mono-cvs.ximian.com>
2007-04-27 19:39:05 +0000

    * mini-s390x.c: Remove redeclaration of CompRelation and CompType (defined in mini.h) to fix build on s390x.
    svn path=/trunk/mono/; revision=76402
    
    
    Commit migrated from https://github.com/mono/mono/commit/61e7560e67b472351615b7cdba8f09621cd57a5d

--- src/mono/mono/mini/mini-s390x.c
+++ src/mono/mono/mini/mini-s390x.c
@@ -202,25 +202,6 @@ typedef struct {
 	      retStruct;
 } size_data;	
 
-typedef enum {
-	CMP_EQ,
-	CMP_NE,
-	CMP_LE,
-	CMP_GE,
-	CMP_LT,
-	CMP_GT,
-	CMP_LE_UN,
-	CMP_GE_UN,
-	CMP_LT_UN,
-	CMP_GT_UN
-} CompRelation;
-
-typedef enum {
-	CMP_TYPE_L,
-	CMP_TYPE_I,
-	CMP_TYPE_F
-} CompType;
-
 /*------------------------------------------------------------------*/
 /* Used by the instrument_emit_epilog 		                    */
 /*------------------------------------------------------------------*/
