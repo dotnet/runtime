5dccfc6d20ae121d79f3928a03fca54bdc16e8d9
Jonathan Chambers <joncham@gmail.com>
2007-04-28 01:50:11 +0000

    2007-04-27  Jonathan Chambers <joncham@gmail.com>
    	* src/eglib-config.hw: Forgot to add for msvc build.
    	* src/vasprintf: Forgot to add for msvc build.
    
    svn path=/trunk/mono/; revision=76424
    
    
    Commit migrated from https://github.com/mono/mono/commit/643adb6a4ccb415b32aa36eb54af922fbf1eb295

---
+++ src/mono/eglib/src/vasprintf.c
@@ -0,0 +1,30 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int vasprintf(char **ret, const char *fmt, va_list ap)
+{
+char *buf;
+int len;
+size_t buflen;
+va_list ap2;
+
+#ifdef _MSC_VER
+ap2 = ap;
+#else
+va_copy(ap2, ap);
+#endif
+
+len = vsnprintf(NULL, 0, fmt, ap2);
+
+if (len > 0 && (buf = malloc((buflen = (size_t) (len + 1)))) != NULL) {
+len = vsnprintf(buf, buflen, fmt, ap);
+*ret = buf;
+} else {
+*ret = NULL;
+len = -1;
+}
+
+va_end(ap2);
+return len;
+}
