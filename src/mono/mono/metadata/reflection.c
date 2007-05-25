cb89d7d3fb28da23e5550f327835c52d8ba99fb0
Sebastien Pouliot <sebastien@ximian.com>
2007-05-25 13:34:32 +0000

    2007-05-25  Sebastien Pouliot  <sebastien@ximian.com>
    	* reflection.c: HasSecurity can be present even if no specially 
    	encoded (CAS) attributes are available (e.g. SuppressUnmanagedCode
    	SecurityAttribute). Fix CAS regression tests on buildbot.
    
    
    svn path=/trunk/mono/; revision=77946
    
    
    Commit migrated from https://github.com/mono/mono/commit/0ae3c722de0d33e3ea40d3fec76e695d896d7df1

--- src/mono/mono/metadata/reflection.c
+++ src/mono/mono/metadata/reflection.c
@@ -9831,11 +9831,16 @@ const static guint32 declsec_flags_map[] = {
 static guint32
 mono_declsec_get_flags (MonoImage *image, guint32 token)
 {
-	guint32 index = mono_metadata_declsec_from_index (image, token);
+	int index = mono_metadata_declsec_from_index (image, token);
 	MonoTableInfo *t = &image->tables [MONO_TABLE_DECLSECURITY];
 	guint32 result = 0;
 	guint32 action;
 	int i;
+
+	/* HasSecurity can be present for other, not specially encoded, attributes,
+	   e.g. SuppressUnmanagedCodeSecurityAttribute */
+	if (index < 0)
+		return 0;
 
 	for (i = index; i < t->rows; i++) {
 		guint32 cols [MONO_DECL_SECURITY_SIZE];
