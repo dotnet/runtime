9b79580cbc359cbeddc59c62dacdcae07690509e
Paolo Molaro <lupus@oddwiz.org>
2007-07-05 18:01:50 +0000

    Thu Jul 5 20:12:52 CEST 2007 Paolo Molaro <lupus@ximian.com>
    	* mini-ppc.c: fixed memory corruption for localloc(0)
    	(bug mono/mono#81852).
    
    
    svn path=/trunk/mono/; revision=81413
    
    
    Commit migrated from https://github.com/mono/mono/commit/837e74fec41a6a26a0595e1fd1aeaa4c68a524e6

--- src/mono/mono/mini/mini-ppc.c
+++ src/mono/mono/mini/mini-ppc.c
@@ -2560,8 +2560,12 @@ mono_arch_output_basic_block (MonoCompile *cfg, MonoBasicBlock *bb)
 			ppc_rlwinm (code, ppc_r11, ppc_r11, 0, 0, 27);
 			/* use ctr to store the number of words to 0 if needed */
 			if (ins->flags & MONO_INST_INIT) {
-				/* we zero 4 bytes at a time */
-				ppc_addi (code, ppc_r0, ins->sreg1, 3);
+				/* we zero 4 bytes at a time:
+				 * we add 7 instead of 3 so that we set the counter to
+				 * at least 1, otherwise the bdnz instruction will make
+				 * it negative and iterate billions of times.
+				 */
+				ppc_addi (code, ppc_r0, ins->sreg1, 7);
 				ppc_srawi (code, ppc_r0, ppc_r0, 2);
 				ppc_mtctr (code, ppc_r0);
 			}
@@ -2571,7 +2575,10 @@ mono_arch_output_basic_block (MonoCompile *cfg, MonoBasicBlock *bb)
 			
 			if (ins->flags & MONO_INST_INIT) {
 				/* adjust the dest reg by -4 so we can use stwu */
-				ppc_addi (code, ins->dreg, ppc_sp, (area_offset - 4));
+				/* we actually adjust -8 because we let the loop
+				 * run at least once
+				 */
+				ppc_addi (code, ins->dreg, ppc_sp, (area_offset - 8));
 				ppc_li (code, ppc_r11, 0);
 				zero_loop_start = code;
 				ppc_stwu (code, ppc_r11, 4, ins->dreg);
