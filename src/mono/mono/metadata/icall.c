35186134f4929442f5662c749b0d38b17529ad98
Zoltan Varga <vargaz@gmail.com>
2007-07-15 14:19:57 +0000

    2007-07-15  Zoltan Varga  <vargaz@gmail.com>
    	* icall.c (ves_icall_System_Reflection_Module_ResolveSignature): New icall.
    
    svn path=/trunk/mono/; revision=82009
    
    
    Commit migrated from https://github.com/mono/mono/commit/01335d30f9f160cf0854df605d32f10e033cd6b1

--- src/mono/mono/metadata/icall.c
+++ src/mono/mono/metadata/icall.c
@@ -5127,6 +5127,38 @@ ves_icall_System_Reflection_Module_ResolveMemberToken (MonoImage *image, guint32
 	return NULL;
 }
 
+static MonoArray*
+ves_icall_System_Reflection_Module_ResolveSignature (MonoImage *image, guint32 token, MonoResolveTokenError *error)
+{
+	int table = mono_metadata_token_table (token);
+	int idx = mono_metadata_token_index (token);
+	MonoTableInfo *tables = image->tables;
+	guint32 sig, len;
+	const char *ptr;
+	MonoArray *res;
+
+	*error = ResolveTokenError_OutOfRange;
+
+	/* FIXME: Support other tables ? */
+	if (table != MONO_TABLE_STANDALONESIG)
+		return NULL;
+
+	if (image->dynamic)
+		return NULL;
+
+	if ((idx == 0) || (idx > tables [MONO_TABLE_STANDALONESIG].rows))
+		return NULL;
+
+	sig = mono_metadata_decode_row_col (&tables [MONO_TABLE_STANDALONESIG], idx - 1, 0);
+
+	ptr = mono_metadata_blob_heap (image, sig);
+	len = mono_metadata_decode_blob_size (ptr, &ptr);
+
+	res = mono_array_new (mono_domain_get (), mono_defaults.byte_class, len);
+	memcpy (mono_array_addr (res, guint8, 0), ptr, len);
+	return res;
+}
+
 static MonoReflectionType*
 ves_icall_ModuleBuilder_create_modified_type (MonoReflectionTypeBuilder *tb, MonoString *smodifiers)
 {
