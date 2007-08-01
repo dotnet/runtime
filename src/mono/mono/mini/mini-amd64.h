d4e9967b1283a6b8246c8390a1a252b22224ba24
Zoltan Varga <vargaz@gmail.com>
2007-08-01 10:05:35 +0000

    2007-08-01  Zoltan Varga  <vargaz@gmail.com>
    	* mini-amd64.h (MONO_INIT_CONTEXT_FROM_FUNC): Avoid using 
    	__builtin_frame_address () since it is broken on older gcc versions.
    
    svn path=/trunk/mono/; revision=83157
    
    
    Commit migrated from https://github.com/mono/mono/commit/cb8307694ffc7c8d3299e1a59a8a07cd776f81f2

--- src/mono/mono/mini/mini-amd64.h
+++ src/mono/mono/mini/mini-amd64.h
@@ -182,11 +182,17 @@ typedef struct {
 
 #else
 
+/* 
+ * __builtin_frame_address () is broken on some older gcc versions in the presence of
+ * frame pointer elimination, see bug #82095.
+ */
 #define MONO_INIT_CONTEXT_FROM_FUNC(ctx,start_func) do {	\
+        int tmp; \
+        guint64 stackptr = &tmp; \
 		mono_arch_flush_register_windows ();	\
 		MONO_CONTEXT_SET_IP ((ctx), (start_func));	\
-		MONO_CONTEXT_SET_BP ((ctx), __builtin_frame_address (0));	\
-		MONO_CONTEXT_SET_SP ((ctx), __builtin_frame_address (0));	\
+		MONO_CONTEXT_SET_BP ((ctx), stackptr);	\
+		MONO_CONTEXT_SET_SP ((ctx), stackptr);	\
 	} while (0)
 
 #endif
