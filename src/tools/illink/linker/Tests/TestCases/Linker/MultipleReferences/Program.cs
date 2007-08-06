de77212001483401e57f698451e9c36ce1cb47bf
Jb Evain <jbevain@gmail.com>
2007-08-06 12:32:57 +0000

    update tests
    svn path=/trunk/mcs/; revision=83530
    
    
    Commit migrated from https://github.com/dotnet/linker/commit/0ace7e2cd5b4dd4fe424bbf51420177f2845c440

--- src/tools/illink/linker/Tests/TestCases/Linker/MultipleReferences/Program.cs
+++ src/tools/illink/linker/Tests/TestCases/Linker/MultipleReferences/Program.cs
@@ -4,12 +4,18 @@ public class Program {
 
 	public static int Main ()
 	{
+		Program p = new Program ();
+		p.Run ();
+
+		return 0;
+	}
+
+	void Run ()
+	{
 		Foo f = new Foo (new Bar ());
 		f.UseBar ();
 
 		Baz b = new Baz ();
 		b.Chain (f);
-
-		return 0;
 	}
 }
--- src/tools/illink/linker/Tests/TestCases/Linker/VirtualCall/Program.cs
+++ src/tools/illink/linker/Tests/TestCases/Linker/VirtualCall/Program.cs
@@ -4,6 +4,12 @@ public class Program {
 
 	public static int Main ()
 	{
+		Program p = new Program ();
+		return p.Run ();
+	}
+
+	int Run ()
+	{
 		Library lib = new PowerFulLibrary ();
 		return lib.Shebang ();
 	}
