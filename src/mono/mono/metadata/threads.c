b4a2d314bd428c9cb7fda3e64850acb9baa803d2
Zoltan Varga <vargaz@gmail.com>
2007-08-07 20:10:20 +0000

    2007-08-07  Zoltan Varga  <vargaz@gmail.com>
    	* threads.c (free_thread_static_data_helper): Avoid a crash if
    	thread->static_data is not yet set.
    
    svn path=/trunk/mono/; revision=83614
    
    
    Commit migrated from https://github.com/mono/mono/commit/a52db5122f338de91e190668738d330957303e87

--- src/mono/mono/metadata/threads.c
+++ src/mono/mono/metadata/threads.c
@@ -2945,7 +2945,7 @@ free_thread_static_data_helper (gpointer key, gpointer value, gpointer user)
 	int idx = (data->offset >> 24) - 1;
 	char *ptr;
 
-	if (!thread->static_data [idx])
+	if (!thread->static_data || !thread->static_data [idx])
 		return;
 	ptr = ((char*) thread->static_data [idx]) + (data->offset & 0xffffff);
 	memset (ptr, 0, data->size);
