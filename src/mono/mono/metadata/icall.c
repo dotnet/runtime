b0651ebea6c8ecf1d047ff5d8d64d1da06063915
Zoltan Varga <vargaz@gmail.com>
2007-08-11 15:41:28 +0000

    2007-08-11  Zoltan Varga  <vargaz@gmail.com>
    	* icall.c (ves_icall_System_Array_SetGenericValueImpl): New icall.
    
    svn path=/trunk/mono/; revision=83865
    
    
    Commit migrated from https://github.com/mono/mono/commit/9813eb75e9daca9c7cdf77e5f0246f88ef27510a

--- src/mono/mono/metadata/icall.c
+++ src/mono/mono/metadata/icall.c
@@ -694,6 +694,25 @@ ves_icall_System_Array_GetGenericValueImpl (MonoObject *this, guint32 pos, gpoin
 }
 
 static void
+ves_icall_System_Array_SetGenericValueImpl (MonoObject *this, guint32 pos, gpointer value)
+{
+	MonoClass *ac;
+	MonoArray *ao;
+	gint32 esize;
+	gpointer *ea;
+
+	MONO_ARCH_SAVE_REGS;
+
+	ao = (MonoArray *)this;
+	ac = (MonoClass *)ao->obj.vtable->klass;
+
+	esize = mono_array_element_size (ac);
+	ea = (gpointer*)((char*)ao->vector + (pos * esize));
+
+	memcpy (ea, value, esize);
+}
+
+static void
 ves_icall_System_Runtime_CompilerServices_RuntimeHelpers_InitializeArray (MonoArray *array, MonoClassField *field_handle)
 {
 	MonoClass *klass = array->obj.vtable->klass;
