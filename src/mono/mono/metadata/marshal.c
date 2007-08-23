ca837f9f1a0dfe4c5986994cb0d615a1f36c4220
Robert Jordan <robertj@gmx.net>
2007-08-23 19:53:38 +0000

    2007-08-23  Robert Jordan  <robertj@gmx.net>
    	* marshal.c (Marshal_ReAllocHGlobal) : Fix GlobalReAlloc's flags.
    	Fixes mono/mono#82499.
    
    
    svn path=/trunk/mono/; revision=84747
    
    
    Commit migrated from https://github.com/mono/mono/commit/1216a69ea1a18335f6af7cb09ec77db8f179072b

--- src/mono/mono/metadata/marshal.c
+++ src/mono/mono/metadata/marshal.c
@@ -10711,7 +10711,7 @@ ves_icall_System_Runtime_InteropServices_Marshal_ReAllocHGlobal (gpointer ptr, i
 	}
 
 #ifdef PLATFORM_WIN32
-	res = GlobalReAlloc (ptr, (gulong)size, 0);
+	res = GlobalReAlloc (ptr, (gulong)size, GMEM_MOVEABLE);
 #else
 	res = g_try_realloc (ptr, (gulong)size);
 #endif
