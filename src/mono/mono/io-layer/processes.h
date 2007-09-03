fc721083463e3af3c18b89a8c366f30962b0ae47
Atsushi Eno <atsushieno@gmail.com>
2007-09-03 09:19:42 +0000

    2007-09-03  Atsushi Enomoto  <atsushi@ximian.com>
    	* processes.h : sigh, I really hate fixing them on windows :(
    
    
    svn path=/trunk/mono/; revision=85214
    
    
    Commit migrated from https://github.com/mono/mono/commit/726a7e16370f78d9f48f8463391c5a22f472260c

--- src/mono/mono/io-layer/processes.h
+++ src/mono/mono/io-layer/processes.h
@@ -173,7 +173,7 @@ extern gboolean CreateProcess (const gunichar2 *appname,
 			       WapiProcessInformation *process_info);
 extern gboolean CreateProcessWithLogonW (const gunichar2 *username,
 					 const gunichar2 *domain,
-					 const gunichar2 *password,
+					 const gpointer password,
 					 const guint32 logonFlags,
 					 const gunichar2 *appname,
 					 const gunichar2 *cmdline,
