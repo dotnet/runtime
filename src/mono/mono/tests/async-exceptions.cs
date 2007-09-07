4bfc19fefdf1d6de76e509ee7f7340dfc752a3d1
Zoltan Varga <vargaz@gmail.com>
2007-09-07 17:55:02 +0000

    2007-09-07  Zoltan Varga  <vargaz@gmail.com>
    	* async-exceptions.cs Makefile.am: Add a test for frames with a frame pointer.
    
    svn path=/trunk/mono/; revision=85491
    
    
    Commit migrated from https://github.com/mono/mono/commit/21bb01cdf7d2ee85ef28e1ab9d240f04fc246792

--- src/mono/mono/tests/async-exceptions.cs
+++ src/mono/mono/tests/async-exceptions.cs
@@ -7,16 +7,31 @@ using System;
 public class Tests {
 
 	// Async exceptions will be raised while executing this method
+	// On amd64, this doesn't have a frame pointer
 	static void foo () {
 		for (int i = 0; i < 10; ++i)
 			;
+	}
+
+	// This does have a frame pointer
+	static void bar () {
+		for (int i = 0; i < 10; ++i)
+			;
+
+		try {
+		} catch {
+		}
 	}
 
 	public static int Main (String[] args) {
 		try {
 			foo ();
 		} catch (NullReferenceException ex) {
-			return 0;
+		}
+
+		try {
+			bar ();
+		} catch (NullReferenceException ex) {
 		}
 
 		return 0;
