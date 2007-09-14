4994c627c38997ba12c2ced26162647d442c69fb
Paolo Molaro <lupus@oddwiz.org>
2007-09-14 17:12:24 +0000

    Fri Sep 14 19:36:37 CEST 2007 Paolo Molaro <lupus@ximian.com>
    	* icall-def.h: redirect all the string ctor to the managed
    	CreateString () methods.
    	* string-icalls.c, string-icalls.h: removed dead code for string
    	ctors and icalls.
    
    
    svn path=/trunk/mono/; revision=85828
    
    
    Commit migrated from https://github.com/mono/mono/commit/24b95802b676f863b80517689ca02852ade28059

--- src/mono/mono/metadata/string-icalls.h
+++ src/mono/mono/metadata/string-icalls.h
@@ -14,44 +14,11 @@
 #include <mono/metadata/object.h>
 #include "mono/utils/mono-compiler.h"
 
-MonoString *
-ves_icall_System_String_ctor_charp (gpointer dummy, gunichar2 *value) MONO_INTERNAL;
-
-MonoString *
-ves_icall_System_String_ctor_char_int (gpointer dummy, gunichar2 value, gint32 count) MONO_INTERNAL;
-
-MonoString *
-ves_icall_System_String_ctor_charp_int_int (gpointer dummy, gunichar2 *value, gint32 sindex, gint32 length) MONO_INTERNAL;
-
-MonoString *
-ves_icall_System_String_ctor_sbytep (gpointer dummy, gint8 *value) MONO_INTERNAL;
-
-MonoString *
-ves_icall_System_String_ctor_sbytep_int_int (gpointer dummy, gint8 *value, gint32 sindex, gint32 length) MONO_INTERNAL;
-
-MonoString *
-ves_icall_System_String_ctor_chara (gpointer dummy, MonoArray *value) MONO_INTERNAL;
-
-MonoString *
-ves_icall_System_String_ctor_chara_int_int (gpointer dummy, MonoArray *value,  gint32 sindex, gint32 length) MONO_INTERNAL;
-
-MonoString *
-ves_icall_System_String_ctor_encoding (gpointer dummy, gint8 *value, gint32 sindex, gint32 length, MonoObject *enc) MONO_INTERNAL;
-
 void
 ves_icall_System_String_ctor_RedirectToCreateString (void) MONO_INTERNAL;
 
 MonoString * 
 ves_icall_System_String_InternalJoin (MonoString *separator, MonoArray * value, gint32 sindex, gint32 count) MONO_INTERNAL;
-
-MonoString * 
-ves_icall_System_String_InternalInsert (MonoString *me, gint32 sindex, MonoString *value) MONO_INTERNAL;
-
-MonoString * 
-ves_icall_System_String_InternalReplace_Char (MonoString *me, gunichar2 oldChar, gunichar2 newChar) MONO_INTERNAL;
-
-MonoString * 
-ves_icall_System_String_InternalRemove (MonoString *me, gint32 sindex, gint32 count) MONO_INTERNAL;
 
 void
 ves_icall_System_String_InternalCopyTo (MonoString *me, gint32 sindex, MonoArray *dest, gint32 dindex, gint32 count) MONO_INTERNAL;
@@ -61,15 +28,6 @@ ves_icall_System_String_InternalSplit (MonoString *me, MonoArray *separator, gin
 
 MonoString * 
 ves_icall_System_String_InternalTrim (MonoString *me, MonoArray *chars, gint32 typ) MONO_INTERNAL;
-
-gint32 
-ves_icall_System_String_InternalIndexOfAny (MonoString *me, MonoArray *arr, gint32 sindex, gint32 count) MONO_INTERNAL;
-
-gint32 
-ves_icall_System_String_InternalLastIndexOf_Char (MonoString *me, gunichar2 value, gint32 sindex, gint32 count) MONO_INTERNAL;
-
-gint32 
-ves_icall_System_String_InternalLastIndexOf_Str (MonoString *me, MonoString *value, gint32 sindex, gint32 count) MONO_INTERNAL;
 
 gint32 
 ves_icall_System_String_InternalLastIndexOfAny (MonoString *me, MonoArray *anyOf, gint32 sindex, gint32 count) MONO_INTERNAL;
@@ -98,13 +56,7 @@ ves_icall_System_String_InternalIntern (MonoString *str) MONO_INTERNAL;
 MonoString * 
 ves_icall_System_String_InternalIsInterned (MonoString *str) MONO_INTERNAL;
 
-gint32
-ves_icall_System_String_GetHashCode (MonoString *me) MONO_INTERNAL;
-
 gunichar2 
 ves_icall_System_String_get_Chars (MonoString *me, gint32 idx) MONO_INTERNAL;
-
-void
-ves_icall_System_String_InternalCharCopy (gunichar2 *src, gunichar2 *dest, gint32 count) MONO_INTERNAL;
 
 #endif /* _MONO_CLI_STRING_ICALLS_H_ */
