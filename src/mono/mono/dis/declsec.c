e4b5af4599eebc3805db9da5ac77915654c37eab
Jb Evain <jbevain@gmail.com>
2007-10-02 10:35:58 +0000

    2007-10-02  Jb Evain  <jbevain@novell.com>
    	* declsec.c (declsec_20_write_type): support for bools.
    
    
    svn path=/trunk/mono/; revision=86736
    
    
    Commit migrated from https://github.com/mono/mono/commit/cc30e425a61f34c902f80145cc0874c4f81ad19f

--- src/mono/mono/dis/declsec.c
+++ src/mono/mono/dis/declsec.c
@@ -56,6 +56,9 @@ static gboolean
 declsec_20_write_type (GString *str, char type)
 {
 	switch (type) {
+	case MONO_TYPE_BOOLEAN:
+		g_string_append (str, "bool");
+		break;
 	case MONO_TYPE_SZARRAY:
 		g_string_append (str, "[]");
 		break;
