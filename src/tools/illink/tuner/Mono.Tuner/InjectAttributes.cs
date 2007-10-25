71730a866ca7d90b859ced5de6ace8c0d6550766
Jb Evain <jbevain@gmail.com>
2007-10-25 20:25:38 +0000

    2007-10-25  Jb Evain  <jbevain@novell.com>
    	* Mono.Tuner/InjectAttributes.cs: infrastructure for
    	the attribute injector.
    
    
    svn path=/trunk/mcs/; revision=88225
    
    
    Commit migrated from https://github.com/dotnet/linker/commit/416d307dd415131efaf8c2845ffe5b430404bd3b

---
+++ src/tools/illink/tuner/Mono.Tuner/InjectAttributes.cs
@@ -0,0 +1,98 @@
+//
+// InjectAttributes.cs
+//
+// Author:
+//   Jb Evain (jbevain@novell.com)
+//
+// (C) 2007 Novell, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Collections;
+
+using System.Xml.XPath;
+
+using Mono.Linker;
+using Mono.Linker.Steps;
+
+using Mono.Cecil;
+
+namespace Mono.Tuner {
+
+	public class InjectAttributes : BaseStep, IXApiVisitor {
+
+		ICustomAttributeProvider _provider;
+
+		protected override void ProcessAssembly (AssemblyDefinition assembly)
+		{
+			XPathDocument xapi = XApiService.GetApiInfoByAssemblyName (assembly.Name.Name);
+			if (xapi == null)
+				return;
+
+			XApiReader reader = new XApiReader (xapi, this);
+			reader.Process (Context);
+		}
+
+		public void OnAttribute (XPathNavigator nav)
+		{
+		}
+
+		public void OnAssembly (XPathNavigator nav, AssemblyDefinition assembly)
+		{
+			_provider = assembly;
+		}
+
+		public void OnClass (XPathNavigator nav, TypeDefinition type)
+		{
+			_provider = type;
+		}
+
+		public void OnInterface (XPathNavigator nav, TypeDefinition type)
+		{
+		}
+
+		public void OnField (XPathNavigator nav, FieldDefinition field)
+		{
+			_provider = field;
+		}
+
+		public void OnMethod (XPathNavigator nav, MethodDefinition method)
+		{
+			_provider = method;
+		}
+
+		public void OnConstructor (XPathNavigator nav, MethodDefinition method)
+		{
+			_provider = method;
+		}
+
+		public void OnProperty (XPathNavigator nav, PropertyDefinition property)
+		{
+			_provider = property;
+		}
+
+		public void OnEvent (XPathNavigator nav, EventDefinition evt)
+		{
+			_provider = evt;
+		}
+	}
+}
