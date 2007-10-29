d01ca44d68bc9e8005b0ace3b402a89a67f49f8d
Rodrigo Kumpera <kumpera@gmail.com>
2007-10-29 20:42:06 +0000

    2007-10-29 Rodrigo Kumpera  <rkumpera@novell.com>
    	* make_imt_test.sh: added, this script generates
    	an interface with 2000 methods, an implementation and
    	test code that calls all methods. This is enough to
    	stress the IMT machinery, as thunks should have more
    	than 100 methods with the current IMT table size.
     
    	* Makefile.am: added the generated imt test and
    	associated rules to use the script to create it.
    
    
    svn path=/trunk/mono/; revision=88408
    
    
    Commit migrated from https://github.com/mono/mono/commit/995589b75c66e2e0b3396560eee430d6480998c5

---
+++ src/mono/mono/tests/make_imt_test.sh
@@ -0,0 +1,101 @@
+#! /bin/bash
+
+LOW=2000
+HIGH=2000
+
+function create_iface () {
+	COUNT=$1
+	echo "public interface Iface_$COUNT {"
+	for i in `seq 1 $COUNT`;
+	do
+		echo "	int Method_$i (int a, int b, int c, int d);"
+	done 
+	echo "}"
+	echo
+}
+
+
+function create_impl () {
+	COUNT=$1
+	echo "public class Impl_$COUNT : Iface_$COUNT {"
+	for i in `seq 1 $COUNT`;
+	do
+		echo "	public virtual int Method_$i (int a, int b, int c, int d) { return a - b - c - d + ${i}; }"
+	done 
+	echo "}"
+	echo
+}
+
+
+function create_static_part () {
+	IFACE=$1
+	echo "	static Iface_$IFACE var_$IFACE = new Impl_$IFACE ();"
+	echo "	static int Test_$IFACE () {
+		int res = 0;
+		int r;
+	"
+
+	for i in `seq 1 $IFACE`;
+	do	
+		echo "		if ((r = var_${IFACE}.Method_$i (10,5,3,2)) != ${i}) {
+			Console.WriteLine(\"iface $IFACE method $i returned {0}\", r);
+			res = 1;
+		}"
+
+	done
+
+	echo "		return res;
+	}"
+}
+
+
+function test_iface () {
+	IFACE=$1
+	echo "		res |= Test_$IFACE ();"
+}
+
+
+####Part that split the output
+
+echo "using System;
+
+"
+
+for i in `seq $LOW $HIGH`;
+do
+	create_iface $i
+	create_impl $i
+done
+
+
+
+echo "
+public class Driver
+{
+"
+
+
+for i in `seq $LOW $HIGH`;
+do
+	create_static_part $i
+done
+
+
+echo "
+	public static int Main ()
+	{
+		int res = 0;"
+
+
+for i in `seq $LOW $HIGH`;
+do
+	test_iface  $i
+done
+
+
+echo "		return res;
+	}
+}"
+
+
+
