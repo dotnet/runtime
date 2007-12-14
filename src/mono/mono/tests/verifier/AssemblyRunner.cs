5e560d37bbe8b5f74d4472d2cc7293a6c8e62698
Rodrigo Kumpera <kumpera@gmail.com>
2007-12-14 14:36:00 +0000

    2007-12-14 Rodrigo Kumpera <rkumpera@novell.com>
    	* Makefile: add support to check strict tests.
    
    	* AssemblyRunner.cs: start using peverify as part of the
    	process. Support for strict tests.
    
    
    svn path=/trunk/mono/; revision=91287
    
    
    Commit migrated from https://github.com/mono/mono/commit/6b30d8422c0cc6d64f745f8d785b8199c8e86a64

--- src/mono/mono/tests/verifier/AssemblyRunner.cs
+++ src/mono/mono/tests/verifier/AssemblyRunner.cs
@@ -37,6 +37,14 @@ using System.Threading;
 
 namespace AssemblyRunner {
 
+	public enum RunResult {
+		valid,
+		unverifiable,
+		invalid,
+		strict,
+		none
+	}
+
 	public class Runner {
 	    private static string program = "\"%COMSPEC%\"";
 
@@ -62,6 +70,21 @@ namespace AssemblyRunner {
 			return stderr;
 	    }
 
+		static int ExecuteAndFetchReturnCode (String command) {
+			ProcessStartInfo psi = new ProcessStartInfo (Environment.ExpandEnvironmentVariables (program), "/c " + command);
+			psi.CreateNoWindow = true;
+			psi.UseShellExecute = false;
+			psi.RedirectStandardError = true;
+
+		    Process activeProcess = Process.Start (psi);
+
+	        activeProcess.WaitForExit();
+			int res = activeProcess.ExitCode;
+			activeProcess.Close ();
+
+			return res;
+	    }
+	    
 		static AppDomain NewDomain () {
 			PolicyStatement statement = new PolicyStatement(new PermissionSet(PermissionState.None),PolicyStatementAttribute.Nothing);
 			PermissionSet ps = new PermissionSet(PermissionState.None);
@@ -97,40 +120,106 @@ namespace AssemblyRunner {
 			return domain;
 		}
 
-		static void executeTest (String assembly, String path) {
-			String op = assembly.Substring (0, assembly.IndexOf ("_"));
+		static RunResult testWithAppDomain (String path, String assemblyName) {
 			AppDomain domain = NewDomain ();
 			try {
 				domain.ExecuteAssembly (path);
-				if (!op.Equals ("valid"))
-					Console.WriteLine ("Test returned valid: "+assembly);
-			} catch (InvalidProgramException ipe) {
-				if (!op.Equals ("invalid"))
-					Console.WriteLine ("Test returned invalid: "+assembly);
-			} catch (FileLoadException ve) {
-				if (!op.Equals ("invalid"))
-					Console.WriteLine ("Test returned invalid: "+assembly);
-			} catch (VerificationException ve) {
-				RecheckUnverifiableResult (path, assembly, op);
+				return RunResult.valid;
+			} catch (InvalidProgramException) {
+				return RunResult.invalid;
+			} catch (FileLoadException) {
+				return RunResult.invalid;
+			} catch (VerificationException) {
+				return RunResult.unverifiable;
 			} catch (TypeInitializationException ve) {
 				if (ve.InnerException is VerificationException)
-					RecheckUnverifiableResult (path, assembly, op);
-				else
-					Console.WriteLine ("Warning: test thrown exception: "+assembly);
-			} catch (MissingMemberException ve) {
-				if (!op.Equals ("invalid"))
-					Console.WriteLine ("Test returned invalid: "+assembly);
-			} catch (MemberAccessException ve) {
-				if (!op.Equals ("unverifiable"))
-					Console.WriteLine ("Test returned unverifiable: "+assembly);
-			} catch (TypeLoadException ve) {
-				if (!op.Equals ("unverifiable"))
-					Console.WriteLine ("Test returned unverifiable: "+assembly);
+					return RunResult.unverifiable;
+				Console.WriteLine ("Warning: test {0} thrown exception: {1} ", assemblyName, ve.InnerException);
+				return RunResult.invalid;
+			} catch (MissingMemberException) {
+				return RunResult.invalid;
+			} catch (MemberAccessException) {
+				return RunResult.unverifiable;
+			} catch (TypeLoadException) {
+				return RunResult.unverifiable;
 			} catch (Exception e) {
-					Console.WriteLine ("Warning: test thrown exception: "+assembly);
+				Console.WriteLine ("Warning: test {0} thrown exception {1}", assemblyName, e);
+				return RunResult.valid;
 			} finally {
 				AppDomain.Unload (domain);
 			}
+		}
+
+		/*
+		 * This test with runtime is usefull to assert if the code is unverifiable but not invalid.
+		 * This test should be used to diagnose if it's the case of code that was reported as invalid but actually is unverifiable.
+		 */
+		static RunResult testWithRuntime (String path) {
+			String stderr = ExecuteAndFetchStderr (path);
+			bool invalid = stderr.IndexOf ("InvalidProgramException") >= 0 || stderr.IndexOf ("FileLoadException") >= 0;
+
+			return invalid ? RunResult.invalid : RunResult.valid;
+		}
+
+		/*
+		 * This test can only assert if the code is unverifiable or not. Use it
+		 * to check if it's the case for a strict check or the code is verifiable.
+		 */
+		static RunResult testWithPeverify (String path) {
+			if (ExecuteAndFetchReturnCode ("peverify "+path) == 0)
+				return RunResult.valid;
+			return RunResult.unverifiable;
+		}
+
+		static RunResult decide (RunResult ad, RunResult rt, RunResult pv, String testName) {
+			if (ad == RunResult.valid) {
+				if (rt != RunResult.valid) {
+					Console.WriteLine ("Warning: test {0} returned valid under AD but {1} under runtime. PV said {2}, using runtime choice", testName, rt, pv);
+					return rt;
+				}
+				if (pv != RunResult.valid)
+					return RunResult.strict;
+				return RunResult.valid;
+			}
+			
+			if (ad == RunResult.unverifiable) {
+				//the rt test cannot complain about unverifiable
+
+				if (pv == RunResult.valid)
+					Console.WriteLine ("Warning: test {0} returned unverifiable under AD but {1} under PV, using AD choice", testName, pv);
+
+				if (rt == RunResult.invalid) {
+					Console.WriteLine ("Warning: test {0} returned unverifiable under AD but {1} under runtime. PV said {2}, using runtime choice", testName, rt, pv);
+					return rt;
+				}
+
+				return RunResult.unverifiable;
+			}
+
+			if (ad == RunResult.invalid) {
+				//in some cases the runtime throws exceptions meant for invalid code but the code is only unverifiable
+				//we double check that by checking if rt returns ok
+				
+				if (pv == RunResult.valid)
+					Console.WriteLine ("Warning: test {0} returned invalid under AD but {1} under PV, using AD choice", testName, pv);
+
+				if (rt == RunResult.valid)
+					return RunResult.unverifiable;
+
+				return RunResult.invalid;
+			}
+			Console.WriteLine ("ERROR: test {0} returned an unknown result under ad {1}, runtime said {2} and PV {3} -- FIXME --", testName, ad, rt, pv);
+			return RunResult.none;
+		}
+		
+		static void executeTest (String path, String assemblyName, RunResult expected) {
+			RunResult ad = testWithAppDomain (path, assemblyName);
+			RunResult rt = testWithRuntime (path);
+			RunResult pv = testWithPeverify (path);
+			
+			RunResult veredict = decide (ad, rt, pv, assemblyName);
+			if (veredict != expected)
+				Console.WriteLine ("ERROR: test {0} expected {1} but got {2}", assemblyName, expected, veredict);
 		}
 
 		/*
@@ -154,9 +243,20 @@ namespace AssemblyRunner {
 			DirectoryInfo dir = new DirectoryInfo (dirName);
 			foreach (FileInfo file in dir.GetFiles ()) {
 				try {
-				if (file.Name.EndsWith (".exe") && (file.Name.StartsWith ("valid_") || file.Name.StartsWith ("unverifiable_") || file.Name.StartsWith ("invalid_")))
-					executeTest (file.Name, file.FullName);
+					RunResult rr = RunResult.none;
+					if (file.Name.StartsWith ("strict_"))
+						rr = RunResult.strict;
+					else if (file.Name.StartsWith ("valid_"))
+						rr = RunResult.valid;
+					else if (file.Name.StartsWith ("unverifiable_"))
+						rr = RunResult.unverifiable;
+					else if (file.Name.StartsWith ("invalid_"))
+						rr = RunResult.invalid;
+						
+					if (file.Name.EndsWith (".exe") && rr != RunResult.none)
+						executeTest (file.FullName, file.Name, rr);
 				} catch (Exception e) {
+					Console.WriteLine ("Warning: test {0} thrown exception {1}", file.FullName, e);
 				}
 			}
 		}
