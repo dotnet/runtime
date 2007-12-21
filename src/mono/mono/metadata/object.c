23fe877fd33aa9c33d1917624ec33927fccb5851
Zoltan Varga <vargaz@gmail.com>
2007-12-21 17:41:03 +0000

    2007-12-21  Zoltan Varga  <vargaz@gmail.com>
    	* object.c (mono_class_compute_gc_descriptor): Remove more unused icall
    	registrations. Fixes mono/mono#348193.
    
    svn path=/trunk/mono/; revision=91778
    
    
    Commit migrated from https://github.com/mono/mono/commit/92fdb1ecdb3bce1282b30b4028b0618e13e8f230

--- src/mono/mono/metadata/object.c
+++ src/mono/mono/metadata/object.c
@@ -871,9 +871,10 @@ mono_class_compute_gc_descriptor (MonoClass *class)
 		mono_register_jit_icall (GC_local_gcj_malloc, "GC_local_gcj_malloc", mono_create_icall_signature ("object int ptr"), FALSE);
 		mono_register_jit_icall (GC_local_gcj_fast_malloc, "GC_local_gcj_fast_malloc", mono_create_icall_signature ("object int ptr"), FALSE);
 #endif
-#endif
 		mono_register_jit_icall (GC_gcj_malloc, "GC_gcj_malloc", mono_create_icall_signature ("object int ptr"), FALSE);
 		mono_register_jit_icall (GC_gcj_fast_malloc, "GC_gcj_fast_malloc", mono_create_icall_signature ("object int ptr"), FALSE);
+#endif
+
 #endif
 		gcj_inited = TRUE;
 		mono_loader_unlock ();
