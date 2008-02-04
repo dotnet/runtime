d205352794a36115f272772eb2ec03d5d0f19cb6
Massimiliano Mantione <massi@mono-cvs.ximian.com>
2008-02-04 16:39:31 +0000

    * iface4.cs: Also added another "level" of inheritance...
    svn path=/trunk/mono/; revision=94756
    
    
    Commit migrated from https://github.com/mono/mono/commit/15ff2041745c65bb876f60c84ec6ffff84543b62

--- src/mono/mono/tests/iface4.cs
+++ src/mono/mono/tests/iface4.cs
@@ -6,10 +6,15 @@ public interface IVehicle {
 	int Turn ();
 }
 
+public interface IWalker {
+	int Walk ();
+}
+
 public class Base : IVehicle {
 	int IVehicle.Start () { return 1; }
 	public int Stop () { return 2; }
 	public virtual int Turn () { return 3; }
+	public int Walk () { return 1; }
 }
 
 public class Derived1 : Base {
@@ -28,11 +33,15 @@ public class Derived2 : Base, IVehicle {
 	public override int Turn () { return 8; }
 }
 
+public class Derived3 : Derived1, IWalker {
+}
+
 public class Test {
 
 	static int Main () {
 		Derived1 d1 = new Derived1 ();
 		Derived2 d2 = new Derived2 ();
+		Derived3 d3 = new Derived3 ();
 		Base b1 = d1;
 		Base b2 = d2;
 		Base rb = new Base ();
@@ -60,6 +69,9 @@ public class Test {
 
 		if (((IVehicle)d2).Stop () != 6)
 			return 8;
+
+		if (d3.Walk () != 1)
+			return 9;
 
 		//Console.WriteLine ("TEST {0}", ((IVehicle)b2).Turn ());
 		return 0;
