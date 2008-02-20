2df365d85fbe6a084c33f991f6dd4715d0aecba1
Zoltan Varga <vargaz@gmail.com>
2008-02-20 20:45:20 +0000

    2008-02-20  Zoltan Varga  <vargaz@gmail.com>
    	* icall.c (ves_icall_Type_GetPacking): Handle dynamic types.
    
    svn path=/trunk/mono/; revision=96314
    
    
    Commit migrated from https://github.com/mono/mono/commit/699d92bd405a652363a3e513ba80593d7de4f6dc

--- src/mono/mono/metadata/icall.c
+++ src/mono/mono/metadata/icall.c
@@ -1932,9 +1932,13 @@ ves_icall_Type_GetPacking (MonoReflectionType *type, guint32 *packing, guint32 *
 {
 	MonoClass *klass = mono_class_from_mono_type (type->type);
 
-	g_assert (!klass->image->dynamic);
-
-	mono_metadata_packing_from_typedef (klass->image, klass->type_token, packing, size);
+	if (klass->image->dynamic) {
+		MonoReflectionTypeBuilder *tb = (MonoReflectionTypeBuilder*)type;
+		*packing = tb->packing_size;
+		*size = tb->class_size;
+	} else {
+		mono_metadata_packing_from_typedef (klass->image, klass->type_token, packing, size);
+	}
 }
 
 static MonoReflectionType*
