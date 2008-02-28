f12c6ab38212ef86044bb69039225a5144ac629e
Rodrigo Kumpera <kumpera@gmail.com>
2008-02-28 16:18:19 +0000

    2008-02-28 Rodrigo Kumpera  <rkumpera@novell.com> 	* verify.c (do_mkrefany): Verify the mkrefany opcode.
    	* verify.c (mono_method_verify): Use do_mkrefany.
    
    
    svn path=/trunk/mono/; revision=96898
    
    
    Commit migrated from https://github.com/mono/mono/commit/606437c6674f1ae3e4341fc767bd36bdae9a832b

--- src/mono/mono/metadata/verify.c
+++ src/mono/mono/metadata/verify.c
@@ -3809,6 +3809,33 @@ do_refanyval (VerifyContext *ctx, int token)
 	set_stack_value (ctx, stack_push (ctx), type, TRUE);
 }
 
+static void
+do_mkrefany (VerifyContext *ctx, int token)
+{
+	ILStackDesc *top;
+	MonoType *type;
+	if (!check_underflow (ctx, 1))
+		return;
+
+	if (!(type = get_boxable_mono_type (ctx, token, "refanyval")))
+		return;
+
+	top = stack_pop (ctx);
+
+	if (!stack_slot_is_managed_pointer (top)) {
+		CODE_NOT_VERIFIABLE (ctx, g_strdup_printf ("Expected a managed pointer for mkrefany, but found %s at 0x%04x", stack_slot_get_name (top), ctx->ip_offset));
+	}else {
+		MonoType *stack_type = mono_type_get_type_byval (top->type);
+		if (MONO_TYPE_IS_REFERENCE (type) && !mono_metadata_type_equal (type, stack_type))
+			CODE_NOT_VERIFIABLE (ctx, g_strdup_printf ("Type not compatible for mkrefany at 0x%04x", ctx->ip_offset));
+			
+		if (!MONO_TYPE_IS_REFERENCE (type) && !verify_type_compatibility_full (ctx, type, stack_type, TRUE))
+			CODE_NOT_VERIFIABLE (ctx, g_strdup_printf ("Type not compatible for mkrefany at 0x%04x", ctx->ip_offset));
+	}
+
+	set_stack_value (ctx, stack_push (ctx), &mono_defaults.typed_reference_class->byval_arg, FALSE);
+}
+
 /*
  * merge_stacks:
  * Merge the stacks and perform compat checks. The merge check if types of @from are mergeable with type of @to 
@@ -4744,12 +4771,12 @@ mono_method_verify (MonoMethod *method, int level)
 		case CEE_UNUSED25:
 			++ip; /* warn, error ? */
 			break;
+
 		case CEE_MKREFANY:
-			if (!check_underflow (&ctx, 1))
-				break;
-			token = read32 (ip + 1);
+			do_mkrefany (&ctx,  read32 (ip + 1));
 			ip += 5;
 			break;
+
 		case CEE_UNUSED59:
 		case CEE_UNUSED60:
 		case CEE_UNUSED61:
