31b0ded8078d60389599ce5e8da9c78d2c79a3b0
Paolo Molaro <lupus@oddwiz.org>
2008-03-06 18:16:57 +0000

    Update to test store as well.
    svn path=/trunk/mono/; revision=97602
    
    
    Commit migrated from https://github.com/mono/mono/commit/a308e20ba0ded9ae2093e00d5be7bfaf2ec48b70

--- src/mono/mono/tests/marshal-valuetypes.cs
+++ src/mono/mono/tests/marshal-valuetypes.cs
@@ -3,6 +3,7 @@ using System;
 public class Test: MarshalByRefObject
 {
         public DateTime Stamp = new DateTime (1968, 1, 2);
+	public double perc = 5.4;
 
         static int Main ()
         {
@@ -10,6 +11,9 @@ public class Test: MarshalByRefObject
                 Test t = (Test) d.CreateInstanceAndUnwrap (typeof (Test).Assembly.FullName, typeof (Test).FullName);
 		if (t.Stamp != new DateTime (1968, 1, 2))
 			return 1;
+		t.perc = 7.2;
+		if (t.perc != 7.2)
+			return 2;
 		return 0;
         }
 }
