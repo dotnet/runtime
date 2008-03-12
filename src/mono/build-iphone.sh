b34a6013ca9900c15b6b54d4a917d6df04c9e696
Geoff Norton <grompf@sublimeintervention.com>
2008-03-12 17:14:41 +0000

    In .: 2008-03-12  Geoff Norton  <gnorton@novell.com>
    	* build-iphone.sh: A autogen/make wrapper to define everything to build
    	mono for the iphone.
    	* configure.in: Add support for arm-apple-darwin (iPhone).
    
    
    svn path=/trunk/mono/; revision=98068
    
    
    Commit migrated from https://github.com/mono/mono/commit/d94aec877f043881a89770a36a5e7ffec52cdbbb

---
+++ src/mono/build-iphone.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+ASPEN_ROOT=/Developer/Platforms/Aspen.platform/Developer
+ASPEN_SDK=$ASPEN_ROOT/SDKs/Aspen1.2.sdk/
+
+export PATH=$ASPEN_ROOT/usr/bin:$PATH
+
+export C_INCLUDE_PATH="$ASPEN_SDK/usr/lib/gcc/arm-apple-darwin9/4.0.1/include:$ASPEN_SDK/usr/include"
+export CC=arm-apple-darwin9-gcc-4.0.1
+export CXX=arm-apple-darwin9-g++-4.0.1
+export CPP="cpp-4.0 -nostdinc -U__i386__ -D__arm__"
+export CXXPP="cpp-4.0 -nostdinc -U__i386__ -D__arm__"
+export LD=$CC
+export LDFLAGS=-Wl,-syslibroot,$ASPEN_SDK
+
+export mono_cv_uscore=yes
+export cv_mono_sizeof_sunpath=104
+export ac_cv_func_posix_getpwuid_r=yes
+export ac_cv_func_backtrace_symbols=no
+
+pushd eglib 
+./autogen.sh --host=arm-apple-darwin9
+popd
+
+./autogen.sh --disable-mcs-build --host=arm-apple-darwin9 --disable-shared-handles --with-tls=pthread --with-sigaltstack=no --with-glib=embedded --with-gc=none $@
+
+curl -o mono/mini/inssel.c http://primates.ximian.com/~gnorton/iphone/inssel.c
+curl -o mono/mini/inssel.h http://primates.ximian.com/~gnorton/iphone/inssel.h
+
+make
+
+# We dont use monoburg since I nicely provided the inssel's pregenerated above.
+pushd mono/monoburg
+$CC -o monoburg ./monoburg.c parser.c -I../../eglib/src -I../../eglib/src -I. -I../..  -Wl,-syslibroot,/Developer/Platforms/Aspen.platform/Developer/SDKs/Aspen1.2.sdk/ -pthread -L../../eglib/src/.libs -leglib -lm
+touch sample.c
+popd
+
+make
