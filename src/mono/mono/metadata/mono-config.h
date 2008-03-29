1f566bc06c291b5edb01f45facec566b0962538a
Zoltan Varga <vargaz@gmail.com>
2008-03-29 15:39:46 +0000

    2008-03-29  Zoltan Varga  <vargaz@gmail.com>
    	* mono-config.h: Add missing G_BEGIN_DECLS/G_END_DECLS. Fixes mono/mono#375188.
    
    svn path=/trunk/mono/; revision=99291
    
    
    Commit migrated from https://github.com/mono/mono/commit/2b8224957fc9cec5f8068fbfb729609fd26bab05

--- src/mono/mono/metadata/mono-config.h
+++ src/mono/mono/metadata/mono-config.h
@@ -8,6 +8,8 @@
 #ifndef __MONO_METADATA_CONFIG_H__
 #define __MONO_METADATA_CONFIG_H__
 
+G_BEGIN_DECLS
+
 const char* mono_get_config_dir (void);
 void        mono_set_config_dir (const char *dir);
 
@@ -18,6 +20,8 @@ void mono_config_for_assembly (MonoImage *assembly);
 void mono_config_parse_memory (const char *buffer);
 
 const char* mono_config_string_for_assembly_file (const char *filename);
+
+G_END_DECLS
 
 #endif /* __MONO_METADATA_CONFIG_H__ */
 
