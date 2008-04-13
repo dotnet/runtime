6a27c28d9e988b8770146d9e009cbddd1d809f23
Zoltan Varga <vargaz@gmail.com>
2008-04-13 22:16:09 +0000

    2008-04-14  Zoltan Varga  <vargaz@gmail.com>
    	* reflection.c (mono_custom_attrs_construct): Fix crash when the cattr type
    	isn't finished yet. Fixes mono/mono#363447.
    
    svn path=/trunk/mono/; revision=100610
    
    
    Commit migrated from https://github.com/mono/mono/commit/37d85c886b95a49b280575a1e39c78ff77af351e

--- src/mono/mono/metadata/reflection.c
+++ src/mono/mono/metadata/reflection.c
@@ -7390,6 +7390,10 @@ mono_custom_attrs_construct (MonoCustomAttrInfo *cinfo)
 
 	result = mono_array_new (mono_domain_get (), mono_defaults.attribute_class, cinfo->num_attrs);
 	for (i = 0; i < cinfo->num_attrs; ++i) {
+		if (!cinfo->attrs [i].ctor)
+			/* The cattr type is not finished yet */
+			/* We should include the type name but cinfo doesn't contain it */
+			mono_raise_exception (mono_get_exception_type_load (NULL, NULL));
 		attr = create_custom_attr (cinfo->image, cinfo->attrs [i].ctor, cinfo->attrs [i].data, cinfo->attrs [i].data_size);
 		mono_array_setref (result, i, attr);
 	}
