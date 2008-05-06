49023bd6837678727fabfe521768bb0ae2e13e29
Zoltan Varga <vargaz@gmail.com>
2008-05-06 22:29:00 +0000

    2008-05-07  Zoltan Varga  <vargaz@gmail.com>
    	* reflection.c: Special case generic type definitions as well. Fixes mono/mono#383444.
    
    svn path=/trunk/mono/; revision=102675
    
    
    Commit migrated from https://github.com/mono/mono/commit/945a8ed2b684ea2ea9abf6ed557fe013e441a0a2

--- src/mono/mono/metadata/reflection.c
+++ src/mono/mono/metadata/reflection.c
@@ -1582,7 +1582,8 @@ type_get_qualified_name (MonoType *type, MonoAssembly *ass) {
 		return mono_type_get_name_full (type, MONO_TYPE_NAME_FORMAT_REFLECTION);
 	ta = klass->image->assembly;
 	if (ta->dynamic || (ta == ass)) {
-		if (klass->generic_class)
+		if (klass->generic_class || klass->generic_container)
+			/* For generic type definitions, we want T, while REFLECTION returns T<K> */
 			return mono_type_get_name_full (type, MONO_TYPE_NAME_FORMAT_FULL_NAME);
 		else
 			return mono_type_get_name_full (type, MONO_TYPE_NAME_FORMAT_REFLECTION);
