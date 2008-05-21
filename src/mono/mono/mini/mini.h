bcfd317083fd00e390d4bed0a358b3e00c09b25e
Zoltan Varga <vargaz@gmail.com>
2008-05-21 13:25:20 +0000

    2008-05-21  Zoltan Varga  <vargaz@gmail.com>
    	* mini.h: Remove the declaration of mono_aot_init_vtable ().
    
    svn path=/trunk/mono/; revision=103684
    
    
    Commit migrated from https://github.com/mono/mono/commit/6296815db6ce1a542ba524af10c7fdc756036898

--- src/mono/mono/mini/mini.h
+++ src/mono/mono/mini/mini.h
@@ -1120,7 +1120,6 @@ gpointer  mono_aot_get_method               (MonoDomain *domain,
 gpointer  mono_aot_get_method_from_token    (MonoDomain *domain, MonoImage *image, guint32 token) MONO_INTERNAL;
 gboolean  mono_aot_is_got_entry             (guint8 *code, guint8 *addr) MONO_INTERNAL;
 guint8*   mono_aot_get_plt_entry            (guint8 *code) MONO_INTERNAL;
-gboolean  mono_aot_init_vtable              (MonoVTable *vtable) MONO_INTERNAL;
 gboolean  mono_aot_get_cached_class_info    (MonoClass *klass, MonoCachedClassInfo *res) MONO_INTERNAL;
 gboolean  mono_aot_get_class_from_name      (MonoImage *image, const char *name_space, const char *name, MonoClass **klass) MONO_INTERNAL;
 MonoJitInfo* mono_aot_find_jit_info         (MonoDomain *domain, MonoImage *image, gpointer addr) MONO_INTERNAL;
