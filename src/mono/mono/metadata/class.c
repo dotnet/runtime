dbe71a97c9d11a604e2813d9f7d6d62b3f396acd
Zoltan Varga <vargaz@gmail.com>
2008-05-24 15:49:05 +0000

    2008-05-24  Zoltan Varga  <vargaz@gmail.com>
    	* class.c (setup_interface_offsets): Use sizeof (guint16) when allocating the
    	interface_offsets_packed table.
    
    svn path=/trunk/mono/; revision=103989
    
    
    Commit migrated from https://github.com/mono/mono/commit/b63dbb57940933b188d0d45d31fa1f21b1411efe

--- src/mono/mono/metadata/class.c
+++ src/mono/mono/metadata/class.c
@@ -2259,7 +2259,7 @@ setup_interface_offsets (MonoClass *class, int cur_slot)
 	} else {
 		class->interface_offsets_count = interface_offsets_count;
 		class->interfaces_packed = mono_mempool_alloc (class->image->mempool, sizeof (MonoClass*) * interface_offsets_count);
-		class->interface_offsets_packed = mono_mempool_alloc (class->image->mempool, sizeof (int) * interface_offsets_count);
+		class->interface_offsets_packed = mono_mempool_alloc (class->image->mempool, sizeof (guint16) * interface_offsets_count);
 		class->interface_bitmap = mono_mempool_alloc0 (class->image->mempool, (sizeof (guint8) * ((max_iid + 1) >> 3)) + (((max_iid + 1) & 7)? 1 :0));
 		for (interface_offsets_count = 0, i = 0; i <= max_iid; i++) {
 			if (interface_offsets_full [i] != -1) {
