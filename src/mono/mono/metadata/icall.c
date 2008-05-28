30c80b3b594292de3d6800ea2f0450057dd76cda
Rodrigo Kumpera <kumpera@gmail.com>
2008-05-28 17:08:10 +0000

    2008-05-28 Rodrigo Kumpera  <rkumpera@novell.com>
    	* icall.c: Added ves_icall_System_Array_GetLongLength and
    	ves_icall_System_Array_CreateInstanceImpl64. Both allow handling big
    	arrays.
    
    	* icall.h: Export both new functions.
    
    	Based on patch by Luis F. Ortiz.
    	Contributed under X11 license.
    	http://lists.ximian.com/pipermail/mono-devel-list/2008-May/027995.html
    
    
    svn path=/trunk/mono/; revision=104298
    
    
    Commit migrated from https://github.com/mono/mono/commit/cd3fbf7fca3b29e6037b5facffb43e7f9ed5c917

--- src/mono/mono/metadata/icall.c
+++ src/mono/mono/metadata/icall.c
@@ -547,6 +547,50 @@ ves_icall_System_Array_CreateInstanceImpl (MonoReflectionType *type, MonoArray *
 	return array;
 }
 
+static MonoArray *
+ves_icall_System_Array_CreateInstanceImpl64 (MonoReflectionType *type, MonoArray *lengths, MonoArray *bounds)
+{
+	MonoClass *aklass;
+	MonoArray *array;
+	mono_array_size_t *sizes, i;
+	gboolean bounded = FALSE;
+
+	MONO_ARCH_SAVE_REGS;
+
+	MONO_CHECK_ARG_NULL (type);
+	MONO_CHECK_ARG_NULL (lengths);
+
+	MONO_CHECK_ARG (lengths, mono_array_length (lengths) > 0);
+	if (bounds)
+		MONO_CHECK_ARG (bounds, mono_array_length (lengths) == mono_array_length (bounds));
+
+	for (i = 0; i < mono_array_length (lengths); i++) 
+		if ((mono_array_get (lengths, gint64, i) < 0) ||
+		    (mono_array_get (lengths, gint64, i) > MONO_ARRAY_MAX_INDEX))
+			mono_raise_exception (mono_get_exception_argument_out_of_range (NULL));
+
+	if (bounds && (mono_array_length (bounds) == 1) && (mono_array_get (bounds, gint64, 0) != 0))
+		/* vectors are not the same as one dimensional arrays with no-zero bounds */
+		bounded = TRUE;
+	else
+		bounded = FALSE;
+
+	aklass = mono_bounded_array_class_get (mono_class_from_mono_type (type->type), mono_array_length (lengths), bounded);
+
+	sizes = alloca (aklass->rank * sizeof(mono_array_size_t) * 2);
+	for (i = 0; i < aklass->rank; ++i) {
+		sizes [i] = mono_array_get (lengths, guint64, i);
+		if (bounds)
+			sizes [i + aklass->rank] = (mono_array_size_t) mono_array_get (bounds, guint64, i);
+		else
+			sizes [i + aklass->rank] = 0;
+	}
+
+	array = mono_array_new_full (mono_object_domain (type), aklass, sizes, sizes + aklass->rank);
+
+	return array;
+}
+
 static gint32 
 ves_icall_System_Array_GetRank (MonoObject *this)
 {
@@ -576,6 +620,22 @@ ves_icall_System_Array_GetLength (MonoArray *this, gint32 dimension)
 	        mono_raise_exception (mono_get_exception_overflow ());
 #endif
 	return length;
+}
+
+static gint64
+ves_icall_System_Array_GetLongLength (MonoArray *this, gint32 dimension)
+{
+	gint32 rank = ((MonoObject *)this)->vtable->klass->rank;
+
+	MONO_ARCH_SAVE_REGS;
+
+	if ((dimension < 0) || (dimension >= rank))
+		mono_raise_exception (mono_get_exception_index_out_of_range ());
+	
+	if (this->bounds == NULL)
+ 		return this->max_length;
+ 	
+ 	return this->bounds [dimension].length;
 }
 
 static gint32
