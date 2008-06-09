24eac07ece9a97e93c73b1b2ba0d45cd2aba9bae
Martin Baulig <martin@novell.com>
2008-06-09 13:15:20 +0000

    2008-06-09  Martin Baulig  <martin@ximian.com>
    	* debug-mono-symfile.c (load_symfile): Check the major version
    	first; if it's wrong, don't print the minor version in the error message.
    
    
    svn path=/trunk/mono/; revision=105305
    
    
    Commit migrated from https://github.com/mono/mono/commit/896c52a702e97579bfa801278b2c42bf2038badd

--- src/mono/mono/metadata/debug-mono-symfile.c
+++ src/mono/mono/metadata/debug-mono-symfile.c
@@ -80,7 +80,14 @@ load_symfile (MonoDebugHandle *handle, MonoSymbolFile *symfile, gboolean in_the_
 	minor = read32(ptr);
 	ptr += sizeof(guint32);
 
-	if ((major != MONO_SYMBOL_FILE_MAJOR_VERSION) || (minor != MONO_SYMBOL_FILE_MINOR_VERSION)) {
+	if (major != MONO_SYMBOL_FILE_MAJOR_VERSION) {
+		if (!in_the_debugger)
+			g_warning ("Symbol file %s has incorrect version (expected %d, got %d)",
+				   symfile->filename, MONO_SYMBOL_FILE_MAJOR_VERSION, major);
+		return FALSE;
+	}
+
+	if (minor != MONO_SYMBOL_FILE_MINOR_VERSION) {
 		if (!in_the_debugger)
 			g_warning ("Symbol file %s has incorrect version "
 				   "(expected %d.%d, got %d.%d)", symfile->filename,
