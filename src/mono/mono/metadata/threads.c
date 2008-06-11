02003bc234ab44d22c6f6b0c03b1937e5ec3aafa
Zoltan Varga <vargaz@gmail.com>
2008-06-11 21:23:38 +0000

    2008-06-11  Zoltan Varga  <vargaz@gmail.com>
    	* threads.c (mono_thread_get_stack_bounds): Align the stack in all cases.
    
    svn path=/trunk/mono/; revision=105651
    
    
    Commit migrated from https://github.com/mono/mono/commit/d3e93d791afa1a11abd0916dce606d51e085b1ea

--- src/mono/mono/metadata/threads.c
+++ src/mono/mono/metadata/threads.c
@@ -754,6 +754,7 @@ mono_thread_get_stack_bounds (guint8 **staddr, size_t *stsize)
 #if defined(HAVE_PTHREAD_GET_STACKSIZE_NP) && defined(HAVE_PTHREAD_GET_STACKADDR_NP)
 	*staddr = (guint8*)pthread_get_stackaddr_np (pthread_self ());
 	*stsize = pthread_get_stacksize_np (pthread_self ());
+	*staddr = (guint8*)((gssize)*staddr & ~(mono_pagesize () - 1));
 	return;
 	/* FIXME: simplify the mess below */
 #elif !defined(PLATFORM_WIN32)
@@ -762,31 +763,31 @@ mono_thread_get_stack_bounds (guint8 **staddr, size_t *stsize)
 
 	pthread_attr_init (&attr);
 #ifdef HAVE_PTHREAD_GETATTR_NP
-		pthread_getattr_np (pthread_self(), &attr);
+	pthread_getattr_np (pthread_self(), &attr);
 #else
 #ifdef HAVE_PTHREAD_ATTR_GET_NP
-		pthread_attr_get_np (pthread_self(), &attr);
+	pthread_attr_get_np (pthread_self(), &attr);
 #elif defined(sun)
-		*staddr = NULL;
-		pthread_attr_getstacksize (&attr, &stsize);
+	*staddr = NULL;
+	pthread_attr_getstacksize (&attr, &stsize);
 #else
-		*staddr = NULL;
-		*stsize = 0;
-		return;
+	*staddr = NULL;
+	*stsize = 0;
+	return;
 #endif
 #endif
 
 #ifndef sun
-		pthread_attr_getstack (&attr, (void**)staddr, stsize);
-		if (*staddr)
-			g_assert ((current > *staddr) && (current < *staddr + *stsize));
+	pthread_attr_getstack (&attr, (void**)staddr, stsize);
+	if (*staddr)
+		g_assert ((current > *staddr) && (current < *staddr + *stsize));
 #endif
 
-		pthread_attr_destroy (&attr); 
+	pthread_attr_destroy (&attr); 
 #endif
 
-		/* When running under emacs, sometimes staddr is not aligned to a page size */
-		*staddr = (guint8*)((gssize)*staddr & ~(mono_pagesize () - 1));
+	/* When running under emacs, sometimes staddr is not aligned to a page size */
+	*staddr = (guint8*)((gssize)*staddr & ~(mono_pagesize () - 1));
 }	
 
 MonoThread *
