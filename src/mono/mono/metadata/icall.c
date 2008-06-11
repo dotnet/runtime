9282ceea8568ee8b710a3347343522300e6b34c0
Rodrigo Kumpera <kumpera@gmail.com>
2008-06-11 20:11:37 +0000

    2008-06-10 Rodrigo Kumpera  <rkumpera@novell.com>
    	* icall.c (ves_icall_System_Reflection_AssemblyName_ParseName): Release
    	memory on failure.
    
    
    
    svn path=/trunk/mono/; revision=105644
    
    
    Commit migrated from https://github.com/mono/mono/commit/8be1349c23a82a901170384ce9401fee6d225c6e

--- src/mono/mono/metadata/icall.c
+++ src/mono/mono/metadata/icall.c
@@ -5154,9 +5154,13 @@ ves_icall_System_Reflection_AssemblyName_ParseName (MonoReflectionAssemblyName *
 	gboolean is_version_defined;
 	gboolean is_token_defined;
 
+	aname.public_key = NULL;
 	val = mono_string_to_utf8 (assname);
-	if (!mono_assembly_name_parse_full (val, &aname, TRUE, &is_version_defined, &is_token_defined))
+	if (!mono_assembly_name_parse_full (val, &aname, TRUE, &is_version_defined, &is_token_defined)) {
+		g_free ((guint8*) aname.public_key);
+		g_free (val);
 		return FALSE;
+	}
 	
 	fill_reflection_assembly_name (domain, name, &aname, "", is_version_defined,
 		FALSE, is_token_defined);
