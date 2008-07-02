0e5c49c9ae6a59f934a948e7ad24986591c71e57
Dick Porter <dick@acm.org>
2008-07-02 14:30:01 +0000

    Add a comment regarding potential future issues with 64 bit systems and pids > 16 bits wide
    svn path=/trunk/mono/; revision=107062
    
    
    Commit migrated from https://github.com/mono/mono/commit/ed7695c870acf95149168f169bcf21e088b1da36

--- src/mono/mono/io-layer/process-private.h
+++ src/mono/mono/io-layer/process-private.h
@@ -17,8 +17,9 @@
 #define _WAPI_PROCESS_CURRENT (gpointer)0xFFFFFFFF
 
 /* This marks a system process that we don't have a handle on */
+/* FIXME: cope with pids > 16bit */
 #define _WAPI_PROCESS_UNHANDLED_PID_MASK 0x7FFF
-#define _WAPI_PROCESS_UNHANDLED (-1 & ~_WAPI_PROCESS_UNHANDLED_PID_MASK) /*0xFFFF8000*/
+#define _WAPI_PROCESS_UNHANDLED (-1 & ~_WAPI_PROCESS_UNHANDLED_PID_MASK)
 
 extern gpointer _wapi_process_duplicate (void);
 
