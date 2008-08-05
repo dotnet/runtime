4a97c8daed27260cbffdbe5efee0543390055959
Andreas Färber <afaerber@mono-cvs.ximian.com>
2008-08-05 11:37:46 +0000

    2008-08-05  Andreas Faerber  <andreas.faerber@web.de>
    	* autogen.sh: Suppress arguments warning for NOCONFIGURE.
    
    In eglib:
    2008-08-05  Andreas Faerber  <andreas.faerber@web.de>
    
    	* autogen.sh: Suppress arguments warning for NOCONFIGURE.
    
    In libgc:
    2008-08-05  Andreas Faerber  <andreas.faerber@web.de>
    
    	* autogen.sh: Suppress arguments warning for NOCONFIGURE.
    
    
    This commit is licensed under the MIT X11 license.
    
    svn path=/trunk/mono/; revision=109666
    
    
    Commit migrated from https://github.com/mono/mono/commit/115fc0608b4322042314e934aac6e89e0c9ad2b9

--- src/mono/eglib/autogen.sh
+++ src/mono/eglib/autogen.sh
@@ -82,7 +82,7 @@ if test "$DIE" -eq 1; then
   exit 1
 fi
 
-if test -z "$*"; then
+if test x$NOCONFIGURE = x && test -z "$*"; then
   echo "**Warning**: I am going to run \`configure' with no arguments."
   echo "If you wish to pass any to it, please specify them on the"
   echo \`$0\'" command line."
