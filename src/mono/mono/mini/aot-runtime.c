c15906e48671a4876050f0937340695e6679c488
Zoltan Varga <vargaz@gmail.com>
2008-09-06 03:03:46 +0000

    2008-09-06  Zoltan Varga  <vargaz@gmail.com>
    	* aot-runtime.c (mono_aot_get_method): Add another cache mapping wrapper
    	methods to their code to avoid computing the full name of wrappers and
    	doing a lookup in a string hash table.
    
    svn path=/trunk/mono/; revision=112413
    
    
    Commit migrated from https://github.com/mono/mono/commit/14bedce48a3bff17cfbb97b3f4a02f273fb6a618

--- src/mono/mono/mini/aot-runtime.c
+++ src/mono/mono/mini/aot-runtime.c
@@ -77,6 +77,8 @@ typedef struct MonoAotModule {
 	GHashTable *wrappers;
 	/* Maps wrapper names to their method index */
 	GHashTable *wrappers_by_name;
+	/* Maps wrapper methods to their code */
+	GHashTable *wrappers_to_code;
 	MonoAssemblyName *image_names;
 	char **image_guids;
 	MonoAssembly *assembly;
@@ -1832,10 +1834,20 @@ mono_aot_get_method (MonoDomain *domain, MonoMethod *method)
 		if (!method->wrapper_type || !aot_module->wrapper_info)
 			return NULL;
 
+		mono_aot_lock ();
+
+		/* Avoid doing a hash table lookup using strings if possible */
+		if (!aot_module->wrappers_to_code)
+			aot_module->wrappers_to_code = g_hash_table_new (mono_aligned_addr_hash, NULL);
+		code = g_hash_table_lookup (aot_module->wrappers_to_code, method);
+		if (code) {
+			mono_aot_unlock ();
+			return code;
+		}
+
 		/* Try to find the wrapper among the wrapper info */
 		full_name = mono_method_full_name (method, TRUE);
 
-		mono_aot_lock ();
 		/* Create a hash table to avoid repeated linear searches */
 		if (!aot_module->wrappers_by_name) {
 			aot_module->wrappers_by_name = g_hash_table_new (g_str_hash, g_str_equal);
@@ -2013,6 +2025,9 @@ mono_aot_load_method (MonoDomain *domain, MonoAotModule *aot_module, MonoMethod 
 	aot_module->methods_loaded [method_index / 32] |= 1 << (method_index % 32);
 
 	init_plt (aot_module);
+
+	if (method->wrapper_type)
+		g_hash_table_insert (aot_module->wrappers_to_code, method, code);
 
 	mono_aot_unlock ();
 
