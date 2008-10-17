9e055557bb2e2c9249d44233fd4641bcbf3d8a04
Zoltan Varga <vargaz@gmail.com>
2008-10-17 17:01:54 +0000

    Fix the OP_IREM_IMM optimization.
    svn path=/trunk/mono/; revision=116260
    
    
    Commit migrated from https://github.com/mono/mono/commit/16a4b4fbc623648764ddde1d41d1ba5df10e1e93

--- src/mono/mono/mini/mini-amd64.c
+++ src/mono/mono/mini/mini-amd64.c
@@ -2619,7 +2619,7 @@ mono_arch_lowering_pass (MonoCompile *cfg, MonoBasicBlock *bb)
 			break;
 		case OP_IREM_IMM:
 			/* Keep the opcode if we can implement it efficiently */
-			if (!((ins->inst_imm > 0) && mono_is_power_of_two (ins->inst_imm)))
+			if (!((ins->inst_imm > 0) && (mono_is_power_of_two (ins->inst_imm) != -1)))
 				mono_decompose_op_imm (cfg, bb, ins);
 			break;
 		case OP_COMPARE_IMM:
