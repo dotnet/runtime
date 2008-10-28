b4d14fb62be1be5fb9f5c384ec297d0900073a0c
Zoltan Varga <vargaz@gmail.com>
2008-10-28 16:51:35 +0000

    Fix the previous change.
    svn path=/trunk/mono/; revision=117278
    
    
    Commit migrated from https://github.com/mono/mono/commit/0770e71505fb3d67ed2d20216a924f55ea2926c9

--- src/mono/mono/mini/mini.c
+++ src/mono/mono/mini/mini.c
@@ -10459,14 +10459,15 @@ mono_allocate_stack_slots_full (MonoCompile *cfg, gboolean backward, guint32 *st
 			 * efficient copying (and to work around the fact that OP_MEMCPY
 			 * and OP_MEMSET ignores alignment).
 			 */
-			if (MONO_TYPE_ISSTRUCT (t))
+			if (MONO_TYPE_ISSTRUCT (t)) {
 				align = sizeof (gpointer);
+				/* 
+				 * Align the size too so the code generated for passing vtypes in
+				 * registers doesn't overwrite random locals.
+				 */
+				size = (size + (align - 1)) & ~(align -1);
+			}
 
-			/* 
-			 * Align the size too so the code generated for passing vtypes in
-			 * registers doesn't overwrite random locals.
-			 */
-			size = (size + (align - 1)) & ~(align -1);
 			if (backward) {
 				offset += size;
 				offset += align - 1;
