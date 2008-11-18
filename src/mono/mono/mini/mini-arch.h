8db7b88e655f601fe3061465ae42cf8fde3cba00
Mark Probst <mark.probst@gmail.com>
2008-11-18 14:23:05 +0000

    2008-11-18  Mark Probst  <mark.probst@gmail.com>
    	* Makefile.am: Changes to build PPC64.
    
    	* mini-arch.h: Include mini-ppc64.h on PPC64.
    
    svn path=/trunk/mono/; revision=119158
    
    
    Commit migrated from https://github.com/mono/mono/commit/41ad8cd9ecd5d365396b8f68218aae8cfef17571

--- src/mono/mono/mini/mini-arch.h
+++ src/mono/mono/mini/mini-arch.h
@@ -5,7 +5,9 @@
 #include "mini-x86.h"
 #elif defined(__x86_64__)
 #include "mini-amd64.h"
-#elif defined(__ppc__) || defined(__powerpc__) || defined(__ppc64__)
+#elif defined(__mono_ppc64__)
+#include "mini-ppc64.h"
+#elif defined(__mono_ppc__)
 #include "mini-ppc.h"
 #elif defined(__sparc__) || defined(sparc)
 #include "mini-sparc.h"
