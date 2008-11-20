f72b3fffc8a75f41a1ccea9f17967de0e21c286e
Zoltan Varga <vargaz@gmail.com>
2008-11-20 00:22:29 +0000

    2008-11-20  Zoltan Varga  <vargaz@gmail.com>
    	Backport of r119398.
    	
    	* pinvoke2.cs: Add a test for passing/receiving generic struct instances.
    
    svn path=/branches/mono-2-2/mono/; revision=119400
    
    
    Commit migrated from https://github.com/mono/mono/commit/bc76b7d785cee26d30a7ba11bc2bf75d52afd46b

--- src/mono/mono/tests/pinvoke2.cs
+++ src/mono/mono/tests/pinvoke2.cs
@@ -1126,6 +1126,37 @@ public class Tests {
 		return 0;
 	}
 
+	/*
+	 * Generic structures
+	 */
+
+	[StructLayout(LayoutKind.Sequential)]
+	public struct Amd64Struct1Gen<T> {
+		public T i;
+		public T j;
+		public T k;
+		public T l;
+	}
+	
+	[DllImport ("libtest", EntryPoint="mono_test_marshal_amd64_pass_return_struct1")]
+	public static extern Amd64Struct1Gen<int> mono_test_marshal_amd64_pass_return_struct1_gen (Amd64Struct1Gen<int> s);
+
+	public static int test_0_amd64_struct1_gen () {
+		Amd64Struct1Gen<int> s = new Amd64Struct1Gen<int> ();
+		s.i = 5;
+		s.j = -5;
+		s.k = 0xffffff;
+		s.l = 0xfffffff;
+
+		Amd64Struct1Gen<int> s2 = mono_test_marshal_amd64_pass_return_struct1_gen (s);
+
+		return ((s2.i == 6) && (s2.j == -4) && (s2.k == 0x1000000) && (s2.l == 0x10000000)) ? 0 : 1;
+	}
+
+	/*
+	 * Other tests
+	 */
+
 	public static int test_0_marshal_byval_class () {
 		SimpleObj obj0 = new SimpleObj ();
 		obj0.str = "T1";
