15820362b2909bd683e7e39280f5c7bfd56a2fc0
Mark Probst <mark.probst@gmail.com>
2008-11-27 16:05:55 +0000

    2008-11-27  Mark Probst  <mark.probst@gmail.com>
    	* marshal.c (mono_string_builder_to_utf8): Fixed a wrong
    	invocation to g_utf16_to_utf8().
    
    svn path=/trunk/mono/; revision=120164
    
    
    Commit migrated from https://github.com/mono/mono/commit/0f5df744cc3f88a9a9023d9269e3fa3da36c6f39

--- src/mono/mono/metadata/marshal.c
+++ src/mono/mono/metadata/marshal.c
@@ -1022,8 +1022,7 @@ gpointer
 mono_string_builder_to_utf8 (MonoStringBuilder *sb)
 {
 	GError *error = NULL;
-	glong *res;
-	gchar *tmp;
+	gchar *tmp, *res = NULL;
 
 	if (!sb)
 		return NULL;
@@ -1037,14 +1036,12 @@ mono_string_builder_to_utf8 (MonoStringBuilder *sb)
 		sb->cached_str = NULL;
 	}
 
-	res = mono_marshal_alloc (mono_stringbuilder_capacity (sb) + 1);
-
-	tmp = g_utf16_to_utf8 (mono_string_chars (sb->str), sb->length, NULL, res, &error);
+	tmp = g_utf16_to_utf8 (mono_string_chars (sb->str), sb->length, NULL, NULL, &error);
 	if (error) {
 		g_error_free (error);
-		mono_marshal_free (res);
 		mono_raise_exception (mono_get_exception_execution_engine ("Failed to convert StringBuilder from utf16 to utf8"));
 	} else {
+		res = mono_marshal_alloc (mono_stringbuilder_capacity (sb) + 1);
 		memcpy (res, tmp, sb->length + 1);
 		g_free (tmp);
 	}
