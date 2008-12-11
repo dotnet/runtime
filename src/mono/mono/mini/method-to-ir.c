6f14359701f946b9b7252f0aea37eb052cc5bfcf
Mark Probst <mark.probst@gmail.com>
2008-12-11 14:55:41 +0000

    2008-12-11  Mark Probst  <mark.probst@gmail.com>
    	* metadata/generic-sharing.c: Look for constraints in all type
    	arguments, not just the first one.
    
    2008-12-11  Mark Probst  <mark.probst@gmail.com>
    
    	* method-to-ir.c: Removed an unnecessary assertion.
    
    2008-12-11  Mark Probst  <mark.probst@gmail.com>
    
    	* generic-marshalbyref.2.cs: Test for calls to marshal-by-ref
    	objects.
    
    	* Makefile.am: Test added.
    
    svn path=/trunk/mono/; revision=121292
    
    
    Commit migrated from https://github.com/mono/mono/commit/e7b134c70ad835f237a3a13869aae451d0f7ea14

--- src/mono/mono/mini/method-to-ir.c
+++ src/mono/mono/mini/method-to-ir.c
@@ -7316,8 +7316,6 @@ mono_method_to_ir (MonoCompile *cfg, MonoMethod *method, MonoBasicBlock *start_b
 							!mono_class_generic_sharing_enabled (cmethod->klass))) {
 					MonoInst *cmethod_addr;
 
-					g_assert (!callvirt_this_arg);
-
 					cmethod_addr = emit_get_rgctx_method (cfg, context_used,
 						cmethod, MONO_RGCTX_INFO_GENERIC_METHOD_CODE);
 
