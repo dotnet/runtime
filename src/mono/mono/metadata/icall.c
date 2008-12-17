d4c5939b4ffb5ad995b654c856a005209a5262b1
Miguel de Icaza <miguel@gnome.org>
2008-12-17 18:14:40 +0000

    Backport 2008-12-17  Miguel de Icaza  <miguel@novell.com>
    	* icall.c (ves_icall_System_Environment_get_Platform): For
    	compatibility reasons for existing client code we will keep
    	returning 4 for a while.   
    
    	For how long will depend on the documentation being updated, and
    	for us to give client code a chance to be updated.
    
    	This reverts the original decison on mono/mono#433108 since we did not
    	catch roughly 33 instances of the broken code in our own source
    	code base, we did not catch failures on the buildbots, and QA did
    	not bring this as a problem.
    
    	Only today I found some customer's code breaking due to our own
    	class libraries not being fully updated and tracked it down to
    	this change.  I am reverting it because if we could not even get
    	our story straight in our own code base, how can we hope that our
    	end user code be fixed?
    
    	As of this morning, our Wiki page that documents how to detect
    	Unix had not been fixed.    
    
    svn path=/branches/mono-2-2/mono/; revision=121716
    
    
    Commit migrated from https://github.com/mono/mono/commit/0a5a292db49fc04386074d2b14cd80233e4c4af5

--- src/mono/mono/metadata/icall.c
+++ src/mono/mono/metadata/icall.c
@@ -6308,7 +6308,14 @@ ves_icall_System_Environment_get_Platform (void)
 	/* OSX */
 	if (mono_framework_version () < 2)
 		return 128;
-	return 6;
+
+	//
+	// For compatibility with our client code, this will be 4 for a while.
+	// We will eventually move to 6 to match .NET, but it requires all client
+	// code to be updated and the documentation everywhere to be updated 
+	// first.
+	//
+	return 4;
 #else
 	/* Unix */
 	if (mono_framework_version () < 2)
