eeca4b099464545d98d7a3b3d2231b8a896ad186
Rodrigo Kumpera <kumpera@gmail.com>
2009-01-06 22:05:02 +0000

    2009-01-06 Rodrigo Kumpera  <rkumpera@novell.com>
    	* verify.c (stack_slot_is_complex_type_not_reference_type): Check if the type
    	is a boxed complex as well.
    
    	Fixes part of mono/mono#463294.
    
    svn path=/trunk/mono/; revision=122579
    
    
    Commit migrated from https://github.com/mono/mono/commit/b47ecaf7e82462fcbfd33b9fbe15df27485f1916

--- src/mono/mono/metadata/verify.c
+++ src/mono/mono/metadata/verify.c
@@ -3022,7 +3022,7 @@ do_boolean_branch_op (VerifyContext *ctx, int delta)
 static gboolean
 stack_slot_is_complex_type_not_reference_type (ILStackDesc *slot)
 {
-	return stack_slot_get_type (slot) == TYPE_COMPLEX && !MONO_TYPE_IS_REFERENCE (slot->type);
+	return stack_slot_get_type (slot) == TYPE_COMPLEX && !MONO_TYPE_IS_REFERENCE (slot->type) && !stack_slot_is_boxed_value (slot);
 }
 
 static void
