f8ca833da05be76f48c7211daf4b67aeb212275f
Rodrigo Kumpera <kumpera@gmail.com>
2009-01-09 13:36:31 +0000

    2009-01-09 Rodrigo Kumpera  <rkumpera@novell.com>
    	* gc.c (mono_gc_init): Fix the comments about deadlock on windows.
    
    svn path=/trunk/mono/; revision=122884
    
    
    Commit migrated from https://github.com/mono/mono/commit/0ad0431a93b2ec3af0a554687b44ce2bf3d24ae5

--- src/mono/mono/metadata/gc.c
+++ src/mono/mono/metadata/gc.c
@@ -992,14 +992,13 @@ mono_gc_init (void)
 	mono_thread_create (mono_domain_get (), finalizer_thread, NULL);
 
 	/*
-	 * Waiting for a new thread would result in a deadlock when the runtime is
-	 * initialized from _CorDllMain that is called while the OS loader lock is
-	 * held by LoadLibrary. Avoiding waiting for the finalizer thread being
-	 * created should not cause any issues on Windows.
-	 */
-	/*
 	 * Wait until the finalizer thread sets gc_thread since its value is needed
 	 * by mono_thread_attach ()
+	 *
+	 * FIXME: Eliminate this as to avoid some deadlocks on windows. 
+	 * Waiting for a new thread should result in a deadlock when the runtime is
+	 * initialized from _CorDllMain that is called while the OS loader lock is
+	 * held by LoadLibrary.
 	 */
 	WaitForSingleObjectEx (thread_started_event, INFINITE, FALSE);
 }
