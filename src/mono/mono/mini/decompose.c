1c6c9f658f2d105b047884d6ac8cdef4674a7406
Rodrigo Kumpera <kumpera@gmail.com>
2009-01-17 20:15:59 +0000

    In .: 2009-01-17  Rodrigo Kumpera  <rkumpera@novell.com> 	* decompose.c (mono_decompose_vtype_opts): Fix the decomposition 	of OP_VCALL_* ops for 8 bytes vtypes on 32 bits archs and platorms 	that pass them on a register pair.
    	This affects windows, OSX and FreeBSD. The mono/tests/handleref.exe
    	test was crashing due to that.
    
    
    svn path=/trunk/mono/; revision=123706
    
    
    Commit migrated from https://github.com/mono/mono/commit/41ac3284697e854f7173696db9105e613e7c8636

--- src/mono/mono/mini/decompose.c
+++ src/mono/mono/mini/decompose.c
@@ -1198,7 +1198,28 @@ mono_decompose_vtype_opts (MonoCompile *cfg)
 							MONO_EMIT_NEW_STORE_MEMBASE (cfg, OP_STOREI4_MEMBASE_REG, dest->dreg, 0, call2->inst.dreg);
 							break;
 						case 8:
+#if SIZEOF_REGISTER == 4
+							/*
+							FIXME It would be nice to fix the operding of OP_CALL to make it possible to use numbering voodoo
+							FIXME It would be even nicer to be able to leverage the long decompose stuff.
+							*/
+							switch (call2->inst.opcode) {
+							case OP_CALL:
+								call2->inst.opcode = OP_LCALL;
+								break;
+							case OP_CALL_REG:
+								call2->inst.opcode = OP_LCALL_REG;
+								break;
+							case OP_CALL_MEMBASE:
+								call2->inst.opcode = OP_LCALL_MEMBASE;
+								break;
+							}
+							call2->inst.dreg = alloc_lreg (cfg);
+							MONO_EMIT_NEW_STORE_MEMBASE (cfg, OP_STOREI4_MEMBASE_REG, dest->dreg, MINI_MS_WORD_OFFSET, call2->inst.dreg + 2);
+							MONO_EMIT_NEW_STORE_MEMBASE (cfg, OP_STOREI4_MEMBASE_REG, dest->dreg, MINI_LS_WORD_OFFSET, call2->inst.dreg + 1);
+#else
 							MONO_EMIT_NEW_STORE_MEMBASE (cfg, OP_STOREI8_MEMBASE_REG, dest->dreg, 0, call2->inst.dreg);
+#endif
 							break;
 						default:
 							/* This assumes the vtype is sizeof (gpointer) long */
