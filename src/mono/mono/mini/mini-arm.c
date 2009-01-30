700823a2a723151e9cea48acdf560f379754365c
Rodrigo Kumpera <kumpera@gmail.com>
2009-01-30 12:48:22 +0000

    2009-01-30  Rodrigo Kumpera  <rkumpera@novell.com>
    	* mini-arm.c (mono_arch_output_basic_block): Emit two cond ops for OP_FBGE
    	as FPA requires it.
    
    svn path=/trunk/mono/; revision=125078
    
    
    Commit migrated from https://github.com/mono/mono/commit/e1334e19e725fbc7e79aa2e60ea41d0bdd86f1c1

--- src/mono/mono/mini/mini-arm.c
+++ src/mono/mono/mini/mini-arm.c
@@ -3465,6 +3465,10 @@ mono_arch_output_basic_block (MonoCompile *cfg, MonoBasicBlock *bb)
 			g_assert_not_reached ();
 			break;
 		case OP_FBGE:
+			/* FIXME does VFP requires both conds?
+			 * FPA requires EQ even thou the docs suggests that just CS is enough
+			 */
+			EMIT_COND_BRANCH_FLAGS (ins, ARMCOND_EQ);
 			EMIT_COND_BRANCH_FLAGS (ins, ARMCOND_CS);
 			break;
 		case OP_FBGE_UN:
