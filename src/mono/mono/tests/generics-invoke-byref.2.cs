c55dd75b6615bdf29d12bf39b6bdbe4d66d6164d
Zoltan Varga <vargaz@gmail.com>
2009-02-02 15:04:25 +0000

    2009-02-02  Zoltan Varga  <vargaz@gmail.com>
    	* generics-invoke-byref.2.cs: Add a test for mono/mono#471259.
    
    svn path=/trunk/mono/; revision=125352
    
    
    Commit migrated from https://github.com/mono/mono/commit/7461e395f76967bbb9121363d09d0f372bb2bc64

--- src/mono/mono/tests/generics-invoke-byref.2.cs
+++ src/mono/mono/tests/generics-invoke-byref.2.cs
@@ -5,23 +5,48 @@ namespace TestConsole
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-	  List<string> str = null;
+			List<string> str = null;
 	  
-	  object[] methodArgs = new object[] { str };
+			object[] methodArgs = new object[] { str };
 	  
-	  Program p = new Program();
-	  p.GetType().GetMethod("TestMethod").Invoke(p, methodArgs);
+			Program p = new Program();
+			p.GetType().GetMethod("TestMethod").Invoke(p, methodArgs);
+
+			/* Byref nullable tests */
+			object[] a = new object [1];
+			int? i = 5;
+			object o = i;
+			a [0] = o;
+			typeof (Program).GetMethod ("TestMethodNullable").Invoke (p, a);
+			if ((int)a [0] != 6)
+				return 1;
+			if ((int)o != 5)
+				return 2;
+
+			a [0] = null;
+			typeof (Program).GetMethod ("TestMethodNullable").Invoke (p, a);
+			if ((int)a [0] != 0)
+				return 3;
+
+			return 0;
         }
       
-	public Program()
-	{
-	}
+		public Program()
+		{
+		}
 
 	public void TestMethod(ref List<string> strArg)
 	{
 	  strArg = new List<string>();
 	}
+
+		public void TestMethodNullable (ref int? x) {
+			if (x != null)
+				x ++;
+			else
+				x = 0;
+		}
     }
 }
