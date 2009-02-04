abcad9b5878c85f43063aea25d8a1e6da429a799
Zoltan Varga <vargaz@gmail.com>
2009-02-04 19:07:52 +0000

    2009-02-04  Zoltan Varga  <vargaz@gmail.com>
    	* metadata.c (inflated_method_in_image): Avoid calling 
    	mono_method_signature () if the method does not already have a signature.
    
    svn path=/trunk/mono/; revision=125724
    
    
    Commit migrated from https://github.com/mono/mono/commit/ba879305257a0a489d30d3cc40e10e20d120e873

--- src/mono/mono/metadata/metadata.c
+++ src/mono/mono/metadata/metadata.c
@@ -2200,7 +2200,7 @@ inflated_method_in_image (gpointer key, gpointer value, gpointer data)
 	// https://bugzilla.novell.com/show_bug.cgi?id=458168
 	return method->declaring->klass->image == image ||
 		(method->context.class_inst && ginst_in_image (method->context.class_inst, image)) ||
-		(method->context.method_inst && ginst_in_image (method->context.method_inst, image)) || signature_in_image (mono_method_signature ((MonoMethod*)method), image);
+		(method->context.method_inst && ginst_in_image (method->context.method_inst, image)) || (((MonoMethod*)method)->signature && signature_in_image (mono_method_signature ((MonoMethod*)method), image));
 }
 
 static gboolean
