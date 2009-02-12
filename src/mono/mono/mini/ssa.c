390f341f3f41213585a055e98641f2a5bfdc4ddb
Zoltan Varga <vargaz@gmail.com>
2009-02-12 00:17:45 +0000

    2009-02-12  Zoltan Varga  <vargaz@gmail.com>
    	* ssa.c (fold_ins): Use MONO_IS_JUMP_TABLE () and MONO_JUMP_TABLE_FROM_INS ()
    	for processing jump tables. Fixes mono/mono#473787.
    
    svn path=/trunk/mono/; revision=126668
    
    
    Commit migrated from https://github.com/mono/mono/commit/3d0d54794b664c4db01cdb1e5263cac45ce5d6b4

--- src/mono/mono/mini/ssa.c
+++ src/mono/mono/mini/ssa.c
@@ -983,9 +983,9 @@ visit_inst (MonoCompile *cfg, MonoBasicBlock *bb, MonoInst *ins, GList **cvars, 
 			}
 		}
 
-		if (ins->opcode == OP_JUMP_TABLE) {
+		if (MONO_IS_JUMP_TABLE (ins)) {
 			int i;
-			MonoJumpInfoBBTable *table = ins->inst_p0;
+			MonoJumpInfoBBTable *table = MONO_JUMP_TABLE_FROM_INS (ins);
 
 			if (ins->next->opcode != OP_PADD) {
 				/* The PADD was optimized away */
@@ -1093,9 +1093,9 @@ fold_ins (MonoCompile *cfg, MonoBasicBlock *bb, MonoInst *ins, MonoInst **carray
 			}
 		}
 
-		if (ins->opcode == OP_JUMP_TABLE) {
+		if (MONO_IS_JUMP_TABLE (ins)) {
 			int i;
-			MonoJumpInfoBBTable *table = ins->inst_p0;
+			MonoJumpInfoBBTable *table = MONO_JUMP_TABLE_FROM_INS (ins);
 
 			if (ins->next->opcode != OP_PADD) {
 				/* The PADD was optimized away */
