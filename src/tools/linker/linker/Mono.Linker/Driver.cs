1c96eae094b3c4c848de77a236ad2e0b85e45310
Jb Evain <jbevain@gmail.com>
2009-02-18 18:53:36 +0000

    2009-02-18  Jb Evain  <jbevain@novell.com>
    	* Mono.Linker/XApiService.cs: delete, unused.
    	* Mono.Linker/Driver.cs: don't use the uneeded XApiService anymore.
    
    svn path=/trunk/mcs/; revision=127306
    
    
    Commit migrated from https://github.com/dotnet/linker/commit/da2803ab3c7495bc0c59161c9e60cc378ea8d6e6

--- src/tools/linker/linker/Mono.Linker/Driver.cs
+++ src/tools/linker/linker/Mono.Linker/Driver.cs
@@ -135,7 +135,7 @@ namespace Mono.Linker {
 					break;
 				case 'i':
 					foreach (string file in GetFiles (GetParam ()))
-						p.PrependStep (new ResolveFromXApiStep (XApiService.GetApiInfoFromFile (file)));
+						p.PrependStep (new ResolveFromXApiStep (new XPathDocument (file)));
 					resolver = true;
 					break;
 				case 'l':
