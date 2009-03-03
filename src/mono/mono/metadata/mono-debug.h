76fcd7ba9687e1f8b4bf8e2fa4b1d13efc936688
Martin Baulig <martin@novell.com>
2009-03-03 18:41:34 +0000

    Bump MONO_DEBUGGER_MAJOR_VERSION to 81 because of the `MonoGenericInst' changes.
    svn path=/trunk/mono/; revision=128461
    
    
    Commit migrated from https://github.com/mono/mono/commit/86ff08cde95b00916817559b82b986cb9e984bbd

--- src/mono/mono/metadata/mono-debug.h
+++ src/mono/mono/metadata/mono-debug.h
@@ -128,7 +128,7 @@ struct _MonoDebugVarInfo {
 	MonoType *type;
 };
 
-#define MONO_DEBUGGER_MAJOR_VERSION			80
+#define MONO_DEBUGGER_MAJOR_VERSION			81
 #define MONO_DEBUGGER_MINOR_VERSION			0
 #define MONO_DEBUGGER_MAGIC				0x7aff65af4253d427ULL
 
