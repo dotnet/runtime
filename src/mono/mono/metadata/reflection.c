d9144fcd53091b55b46696c0f69c36d9e7cd5c53
Zoltan Varga <vargaz@gmail.com>
2009-03-09 10:37:22 +0000

    2009-03-09  Zoltan Varga  <vargaz@gmail.com>
    	* reflection.c (mono_custom_attrs_has_attr): Handle interfaces. Fixes
    	mono/mono#483247.
    
    svn path=/trunk/mono/; revision=128864
    
    
    Commit migrated from https://github.com/mono/mono/commit/76b6f7043c32785f118b16b75365e5c81b81f395

--- src/mono/mono/metadata/reflection.c
+++ src/mono/mono/metadata/reflection.c
@@ -8237,7 +8237,7 @@ mono_custom_attrs_has_attr (MonoCustomAttrInfo *ainfo, MonoClass *attr_klass)
 	MonoClass *klass;
 	for (i = 0; i < ainfo->num_attrs; ++i) {
 		klass = ainfo->attrs [i].ctor->klass;
-		if (mono_class_has_parent (klass, attr_klass))
+		if (mono_class_has_parent (klass, attr_klass) || (MONO_CLASS_IS_INTERFACE (attr_klass) && mono_class_is_assignable_from (attr_klass, klass)))
 			return TRUE;
 	}
 	return FALSE;
