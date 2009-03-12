02d62c7bfa8eaf24caf23db19491703a2290526d
Zoltan Varga <vargaz@gmail.com>
2009-03-12 17:55:03 +0000

    2009-03-12  Zoltan Varga  <vargaz@gmail.com>
    	* icall.c: Allocate the memory used by the mono_ptr_array macros using
    	mono_gc_alloc_fixed () since it contains GC refs.
    
    svn path=/trunk/mono/; revision=129166
    
    
    Commit migrated from https://github.com/mono/mono/commit/9d8de658a51f07f6378f0b53aa056d96315f02fd

--- src/mono/mono/metadata/icall.c
+++ src/mono/mono/metadata/icall.c
@@ -98,23 +98,19 @@ typedef struct {
 #define mono_ptr_array_init(ARRAY, INITIAL_SIZE) do {\
 	(ARRAY).size = 0; \
 	(ARRAY).capacity = MAX (INITIAL_SIZE, MONO_PTR_ARRAY_MAX_ON_STACK); \
-	(ARRAY).data = INITIAL_SIZE > MONO_PTR_ARRAY_MAX_ON_STACK ? g_new (void*, INITIAL_SIZE) : g_newa (void*, MONO_PTR_ARRAY_MAX_ON_STACK); \
+	(ARRAY).data = INITIAL_SIZE > MONO_PTR_ARRAY_MAX_ON_STACK ? mono_gc_alloc_fixed (sizeof (void*) * INITIAL_SIZE, NULL) : g_newa (void*, MONO_PTR_ARRAY_MAX_ON_STACK); \
 } while (0)
 
 #define mono_ptr_array_destroy(ARRAY) do {\
 	if ((ARRAY).capacity > MONO_PTR_ARRAY_MAX_ON_STACK) \
-		g_free ((ARRAY).data); \
+		mono_gc_free_fixed ((ARRAY).data); \
 } while (0)
 
 #define mono_ptr_array_append(ARRAY, VALUE) do { \
 	if ((ARRAY).size >= (ARRAY).capacity) {\
-		if ((ARRAY).capacity == MONO_PTR_ARRAY_MAX_ON_STACK) { \
-			void *__tmp = g_new (void*, (ARRAY).capacity * 2); \
-			memcpy (__tmp, (ARRAY).data, (ARRAY).capacity * sizeof (void*));\
-			(ARRAY).data = __tmp;\
-		} else {\
-			(ARRAY).data = g_renew (void*, (ARRAY).data, (ARRAY).capacity * 2); \
-		} \
+		void *__tmp = mono_gc_alloc_fixed (sizeof (void*) * (ARRAY).capacity * 2, NULL); \
+		memcpy (__tmp, (ARRAY).data, (ARRAY).capacity * sizeof (void*)); \
+		(ARRAY).data = __tmp;											\
 		(ARRAY).capacity *= 2;\
 	}\
 	((ARRAY).data [(ARRAY).size++] = VALUE); \
