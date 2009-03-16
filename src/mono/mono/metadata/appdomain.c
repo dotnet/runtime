a895c12cbe176be5973f197f636bc28a0c6c581d
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2009-03-16 14:24:17 +0000

    update corlib version
    svn path=/trunk/mono/; revision=129446
    
    
    Commit migrated from https://github.com/mono/mono/commit/a79db829304e619a283bb4c6f6272034af711a87

--- src/mono/mono/metadata/appdomain.c
+++ src/mono/mono/metadata/appdomain.c
@@ -68,7 +68,7 @@
  * Changes which are already detected at runtime, like the addition
  * of icalls, do not require an increment.
  */
-#define MONO_CORLIB_VERSION 71
+#define MONO_CORLIB_VERSION 72
 
 typedef struct
 {
