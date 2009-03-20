180ed7fc9ebfb19af168ff226ff847d6468d2425
Zoltan Varga <vargaz@gmail.com>
2009-03-20 21:31:14 +0000

    2009-03-20  Zoltan Varga  <vargaz@gmail.com>
    	* method-to-ir.c (mono_find_block_region): Handle try clauses nested
    	inside finally clauses correctly. Fixes mono/mono#485721.
    
    	* mini.c (mono_find_spvar_for_region): This needs to handle try regions
    	after the change above.
    
    	* exceptions.cs: Add a test.
    
    svn path=/trunk/mono/; revision=129927
    
    
    Commit migrated from https://github.com/mono/mono/commit/2b6498793d7f95bcef2c9e24d444f2cfc3620946

--- src/mono/mono/mini/exceptions.cs
+++ src/mono/mono/mini/exceptions.cs
@@ -2447,5 +2447,34 @@ class Tests {
 
 		return (i == 1) ? 0 : 1;
 	}		
+
+	// bug #485721
+	public static int test_0_try_inside_finally_cmov_opt () {
+		bool Reconect = false;
+
+		object o = new object ();
+
+		try {
+		}
+		catch (Exception ExCon) {
+			if (o != null)
+				Reconect = true;
+
+			try {
+			}
+			catch (Exception Last) {
+			}
+		}
+		finally {
+			if (Reconect == true) {
+				try {
+				}
+				catch (Exception ex) {
+				}
+			}
+		}
+
+		return 0;
+	}
 }
 
