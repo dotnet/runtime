da0edef7ce418ea38d85de898c688cbd89dbe8e2
Rodrigo Kumpera <kumpera@gmail.com>
2009-04-06 22:54:34 +0000

    2009-04-06 Rodrigo Kumpera  <rkumpera@novell.com>
    	* verify.c (do_throw): Don't allow an unboxed generic param ar argument.
    
    	Fixes mono/mono#487738.
    
    svn path=/trunk/mono/; revision=131174
    
    
    Commit migrated from https://github.com/mono/mono/commit/1db0ab83ff533e23c71b6694f22a35fb59891c6f

--- src/mono/mono/metadata/verify.c
+++ src/mono/mono/metadata/verify.c
@@ -4243,6 +4243,11 @@ do_throw (VerifyContext *ctx)
 	if (!stack_slot_is_null_literal (exception) && !(stack_slot_get_type (exception) == TYPE_COMPLEX && !mono_class_from_mono_type (exception->type)->valuetype))
 		CODE_NOT_VERIFIABLE (ctx, g_strdup_printf ("Invalid type on stack for throw, expected reference type at 0x%04x", ctx->ip_offset));
 
+	if (mono_type_is_generic_argument (exception->type) && !stack_slot_is_boxed_value (exception)) {
+		char *name = mono_type_full_name (exception->type);
+		CODE_NOT_VERIFIABLE (ctx, g_strdup_printf ("Invalid type on stack for throw, expected reference type but found unboxed %s  at 0x%04x ", name, ctx->ip_offset));
+		g_free (name);
+	}
 	/*The stack is left empty after a throw*/
 	ctx->eval.size = 0;
 }
