92b1767a6650395d0807611092a4004a438667a7
Atsushi Eno <atsushieno@gmail.com>
2009-04-17 19:10:09 +0000

    2009-04-17  Atsushi Enomoto  <atsushi@ximian.com>
    	* mini-exceptions.c : add thread id to EXCEPTION trace message.
    
    
    svn path=/trunk/mono/; revision=132034
    
    
    Commit migrated from https://github.com/mono/mono/commit/021f8cf328f036211b344006d0fa4adfedc2438e

--- src/mono/mono/mini/mini-exceptions.c
+++ src/mono/mono/mini/mini-exceptions.c
@@ -921,7 +921,7 @@ mono_handle_exception_internal (MonoContext *ctx, gpointer obj, gpointer origina
 	if (!test_only) {
 		MonoContext ctx_cp = *ctx;
 		if (mono_trace_is_enabled ())
-			g_print ("EXCEPTION handling: %s\n", mono_object_class (obj)->name);
+			g_print ("[%p:] EXCEPTION handling: %s\n", (void*)GetCurrentThreadId (), mono_object_class (obj)->name);
 		mono_profiler_exception_thrown (obj);
 		if (!mono_handle_exception_internal (&ctx_cp, obj, original_ip, TRUE, &first_filter_idx)) {
 			if (mono_break_on_exc)
