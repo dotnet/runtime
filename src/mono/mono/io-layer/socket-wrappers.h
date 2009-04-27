77663e912d6187c22a0aea2603fd2d1a59866be5
Miguel de Icaza <miguel@gnome.org>
2009-04-27 23:07:17 +0000

    Only include the headers if available
    svn path=/trunk/mono/; revision=132795
    
    
    Commit migrated from https://github.com/mono/mono/commit/ccf50c87c5ed515a012b490cdd09db921e040ff9

--- src/mono/mono/io-layer/socket-wrappers.h
+++ src/mono/mono/io-layer/socket-wrappers.h
@@ -6,8 +6,12 @@
  */
 
 #include <config.h>
-#include <sys/select.h>
-#include <sys/time.h>
+#ifdef HAVE_SYS_SELECT_H
+#  include <sys/select.h>
+#endif
+#ifdef HAVE_SYS_TIME_H
+#  include <sys/time.h>
+#endif
 
 #ifndef HAVE_SOCKLEN_T
 #define socklen_t int
