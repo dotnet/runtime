e22e485d4112ed7d032e7f3f54ec04042d3c67d6
Miguel de Icaza <miguel@gnome.org>
2009-05-01 22:58:11 +0000

    Copy
    svn path=/trunk/mono/; revision=133335
    
    
    Commit migrated from https://github.com/mono/mono/commit/81d51f61283d1f4eae6c1d9b669b37c6508ea68e

---
+++ src/mono/msvc05/setup-solution.bat
@@ -0,0 +1,24 @@
+@echo off
+mkdir scripts\net_1_1_bootstrap > nul 2> nul
+mkdir scripts\net_1_1 > nul 2> nul
+mkdir scripts\net_2_0 > nul 2> nul
+mkdir scripts\net_2_1 > nul 2> nul
+
+csc -debug -out:scripts\monowrap.exe scripts\monowrap.cs 
+copy scripts\monowrap.exe scripts\net_1_1_bootstrap\csc.exe
+copy scripts\monowrap.pdb scripts\net_1_1_bootstrap\csc.pdb
+
+copy scripts\monowrap.exe scripts\net_1_1\csc.exe
+copy scripts\monowrap.pdb scripts\net_1_1\csc.pdb
+
+copy scripts\monowrap.exe scripts\net_2_0\csc.exe
+copy scripts\monowrap.pdb scripts\net_2_0\csc.pdb
+
+copy scripts\monowrap.exe scripts\net_2_1\csc.exe
+copy scripts\monowrap.pdb scripts\net_2_1\csc.pdb
+
+echo Setup complete, you can now use build the solution
+
+csc -r:System.Xml.Linq -debug -out:scripts\genproj.exe scripts\genproj.cs
+cd scripts
+genproj.exe
