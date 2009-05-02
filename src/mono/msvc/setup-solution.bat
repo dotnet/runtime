caf950d7b3f01be8c8b79641b9dd81096e5bf0f1
Miguel de Icaza <miguel@gnome.org>
2009-05-02 00:42:51 +0000

    update
    svn path=/trunk/mono/; revision=133346
    
    
    Commit migrated from https://github.com/mono/mono/commit/c3769a3b6b1442a008579d326cd25cb401d5b242

--- src/mono/msvc/setup-solution.bat
+++ src/mono/msvc/setup-solution.bat
@@ -1,23 +1,5 @@
 @echo off
-mkdir scripts\net_1_1_bootstrap > nul 2> nul
-mkdir scripts\net_1_1 > nul 2> nul
-mkdir scripts\net_2_0 > nul 2> nul
-mkdir scripts\net_2_1 > nul 2> nul
-
 csc -debug -out:scripts\monowrap.exe scripts\monowrap.cs 
-copy scripts\monowrap.exe scripts\net_1_1_bootstrap\csc.exe
-copy scripts\monowrap.pdb scripts\net_1_1_bootstrap\csc.pdb
-
-copy scripts\monowrap.exe scripts\net_1_1\csc.exe
-copy scripts\monowrap.pdb scripts\net_1_1\csc.pdb
-
-copy scripts\monowrap.exe scripts\net_2_0\csc.exe
-copy scripts\monowrap.pdb scripts\net_2_0\csc.pdb
-
-copy scripts\monowrap.exe scripts\net_2_1\csc.exe
-copy scripts\monowrap.pdb scripts\net_2_1\csc.pdb
-
-
 csc -debug -out:scripts\genproj.exe scripts\genproj.cs
 cd scripts
 genproj.exe
