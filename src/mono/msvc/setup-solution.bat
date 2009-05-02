72e235e915b61d84b7680f56a8b82e684a76f815
Miguel de Icaza <miguel@gnome.org>
2009-05-02 05:53:05 +0000

    Update
    svn path=/trunk/mono/; revision=133370
    
    
    Commit migrated from https://github.com/mono/mono/commit/42d6af035c70c1ea0601f4007df3d8e47950cf42

--- src/mono/msvc/setup-solution.bat
+++ src/mono/msvc/setup-solution.bat
@@ -5,4 +5,7 @@ csc -debug -out:scripts\prepare.exe scripts\prepare.cs
 cd scripts
 genproj.exe
 cd ..
-echo Setup complete, you can now use build the solution
+echo Setup complete, you need at least a mcs\class\lib\basic directory with
+echo mcs.exe  mscorlib.dll  System.dll  System.Xml.dll
+echo to bootstrap
+
