07b4dc9f9f069cb2a5c0d90c178c5702c4695352
Rodrigo Kumpera <kumpera@gmail.com>
2009-05-26 22:48:01 +0000

    2009-05-26 Rodrigo Kumpera  <rkumpera@novell.com>
    	* icall.c (ves_icall_Type_GetMethodsByName): Virtual methods should be
    	discarded based on their most specific definition so we set the method_slots
    	array before checking if the method is acceptable or not.
    
    	Fixes mono/mono#506757.
    
    svn path=/trunk/mono/; revision=134810
    
    
    Commit migrated from https://github.com/mono/mono/commit/8a33d428ca3f5e727fe5cc89c5e21ef6225c2897

--- src/mono/mono/metadata/icall.c
+++ src/mono/mono/metadata/icall.c
@@ -3714,6 +3714,7 @@ ves_icall_Type_GetMethodsByName (MonoReflectionType *type, MonoString *name, gui
 	gpointer iter;
 	MonoObject *member;
 	int i, len, match, nslots;
+	/*FIXME, use MonoBitSet*/
 	guint32 method_slots_default [8];
 	guint32 *method_slots;
 	gchar *mname = NULL;
@@ -3775,6 +3776,13 @@ handle_parent:
 	iter = NULL;
 	while ((method = mono_class_get_methods (klass, &iter))) {
 		match = 0;
+		if (method->slot != -1) {
+			g_assert (method->slot < nslots);
+			if (method_slots [method->slot >> 5] & (1 << (method->slot & 0x1f)))
+				continue;
+			method_slots [method->slot >> 5] |= 1 << (method->slot & 0x1f);
+		}
+
 		if (method->name [0] == '.' && (strcmp (method->name, ".ctor") == 0 || strcmp (method->name, ".cctor") == 0))
 			continue;
 		if ((method->flags & METHOD_ATTRIBUTE_MEMBER_ACCESS_MASK) == METHOD_ATTRIBUTE_PUBLIC) {
@@ -3804,12 +3812,6 @@ handle_parent:
 		}
 		
 		match = 0;
-		if (method->slot != -1) {
-			g_assert (method->slot < nslots);
-			if (method_slots [method->slot >> 5] & (1 << (method->slot & 0x1f)))
-				continue;
-			method_slots [method->slot >> 5] |= 1 << (method->slot & 0x1f);
-		}
 		
 		member = (MonoObject*)mono_method_get_object (domain, method, refklass);
 		
