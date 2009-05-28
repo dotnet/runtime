e97a12149bce9775df4ace38503d31ed8580cd79
Martin Baulig <martin@novell.com>
2009-05-28 04:35:13 +0000

    Add missing 'MONO_DEBUGGER_SUPPORTED' conditional.
    svn path=/trunk/mono/; revision=134925
    
    
    Commit migrated from https://github.com/mono/mono/commit/1ac0c473fb0fc78b03d6bb7df9de979f967ab29a

--- src/mono/mono/mini/mini.c
+++ src/mono/mono/mini/mini.c
@@ -4697,8 +4697,10 @@ mini_init (const char *filename, const char *runtime_version)
 	if (!default_opt_set)
 		default_opt = mono_parse_default_optimizations (NULL);
 
+#ifdef MONO_DEBUGGER_SUPPORTED
 	if (mono_debug_using_mono_debugger ())
 		mono_debugger_init ();
+#endif
 
 	InitializeCriticalSection (&jit_mutex);
 
