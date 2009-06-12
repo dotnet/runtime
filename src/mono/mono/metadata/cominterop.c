0057df50fb461e7f5b223eec56263964030a412b
Robert Jordan <robertj@gmx.net>
2009-06-12 20:41:58 +0000

    2009-06-12  Robert Jordan  <robertj@gmx.net>
    	* cominterop.c (cominterop_get_ccw): Increment mspec's SizeParamIndex
    	because "this" of the managed signature has become an
    	ordinary parameter in the unmanaged signature.
    
    
    svn path=/trunk/mono/; revision=136029
    
    
    Commit migrated from https://github.com/mono/mono/commit/c7ab76d04d86e70fadc97186cb9cd67e72122bf1

--- src/mono/mono/metadata/cominterop.c
+++ src/mono/mono/metadata/cominterop.c
@@ -1917,6 +1917,12 @@ cominterop_get_ccw (MonoObject* object, MonoClass* itf)
 						mspecs[mspec_index] = g_new0 (MonoMarshalSpec, 1);
 						mspecs[mspec_index]->native = MONO_NATIVE_VARIANTBOOL;
 					}
+				} else {
+					/* increase SizeParamIndex since we've added a param */
+					if (sig_adjusted->params[param_index]->type == MONO_TYPE_ARRAY ||
+					    sig_adjusted->params[param_index]->type == MONO_TYPE_SZARRAY)
+						if (mspecs[mspec_index]->data.array_data.param_num != -1)
+							mspecs[mspec_index]->data.array_data.param_num++;
 				}
 			}
 
