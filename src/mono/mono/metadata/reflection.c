be63249e43400c2e8c8a308acc4bafe97d131766
Paolo Molaro <lupus@oddwiz.org>
2009-06-25 13:51:47 +0000

    Thu Jun 25 15:48:09 CEST 2009 Paolo Molaro <lupus@ximian.com>
    	* reflection.c: in mono_method_clear_object () really ensure all the
    	objects are removed.
    
    
    svn path=/trunk/mono/; revision=136834
    
    
    Commit migrated from https://github.com/mono/mono/commit/3e2f55010e56744e1e018a9709bb83ba74652361

--- src/mono/mono/metadata/reflection.c
+++ src/mono/mono/metadata/reflection.c
@@ -6285,11 +6285,21 @@ mono_method_get_object (MonoDomain *domain, MonoMethod *method, MonoClass *refcl
 void
 mono_method_clear_object (MonoDomain *domain, MonoMethod *method)
 {
+	MonoClass *klass;
 	g_assert (method->dynamic);
 
-	clear_cached_object (domain, method, method->klass);
+	klass = method->klass;
+	while (klass) {
+		clear_cached_object (domain, method, klass);
+		klass = klass->parent;
+	}
 	/* Added by mono_param_get_objects () */
 	clear_cached_object (domain, &(method->signature), NULL);
+	klass = method->klass;
+	while (klass) {
+		clear_cached_object (domain, &(method->signature), klass);
+		klass = klass->parent;
+	}
 }
 
 /*
