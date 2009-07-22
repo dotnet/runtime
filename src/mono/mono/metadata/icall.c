4a04072685ddee6e8f46dd4fd172d2a528e73007
Mark Probst <mark.probst@gmail.com>
2009-07-22 12:37:53 +0000

    2009-07-22  Mark Probst  <mark.probst@gmail.com>
            * icall.c (ves_icall_System_Array_SetGenericValueImpl): Use a
            write barrier if necessary.
    
    svn path=/trunk/mono/; revision=138397
    
    
    Commit migrated from https://github.com/mono/mono/commit/3a489a0589114ac253de3bf4d3a98ad724a9f0c0

--- src/mono/mono/metadata/icall.c
+++ src/mono/mono/metadata/icall.c
@@ -810,7 +810,7 @@ ves_icall_System_Array_GetGenericValueImpl (MonoObject *this, guint32 pos, gpoin
 static void
 ves_icall_System_Array_SetGenericValueImpl (MonoObject *this, guint32 pos, gpointer value)
 {
-	MonoClass *ac;
+	MonoClass *ac, *ec;
 	MonoArray *ao;
 	gint32 esize;
 	gpointer *ea;
@@ -819,11 +819,20 @@ ves_icall_System_Array_SetGenericValueImpl (MonoObject *this, guint32 pos, gpoin
 
 	ao = (MonoArray *)this;
 	ac = (MonoClass *)ao->obj.vtable->klass;
+	ec = ac->element_class;
 
 	esize = mono_array_element_size (ac);
 	ea = (gpointer*)((char*)ao->vector + (pos * esize));
 
-	memcpy (ea, value, esize);
+	if (MONO_TYPE_IS_REFERENCE (&ec->byval_arg)) {
+		g_assert (esize == sizeof (gpointer));
+		mono_gc_wbarrier_generic_store (ea, *(gpointer*)value);
+	} else if (ec->has_references) {
+		g_assert (ec->inited);
+		mono_gc_wbarrier_value_copy (ea, value, 1, ec);
+	} else {
+		memcpy (ea, value, esize);
+	}
 }
 
 static void
