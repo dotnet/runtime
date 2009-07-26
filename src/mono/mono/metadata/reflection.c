dfdc180cddb647776ffef52e62514391adccc9cb
Miguel de Icaza <miguel@gnome.org>
2009-07-26 20:56:22 +0000

    2009-07-26  Miguel de Icaza  <miguel@novell.com>
    	* reflection.c (mono_reflection_sighelper_get_signature_local):
    	Only dereference the assembly if it has been set.    Fixes a
    	crasher exposed by mono/mono#525328
    
    svn path=/trunk/mono/; revision=138701
    
    
    Commit migrated from https://github.com/mono/mono/commit/ae14a1802ddd7cd5275cff758a2eb8cd2fd371e8

--- src/mono/mono/metadata/reflection.c
+++ src/mono/mono/metadata/reflection.c
@@ -10626,7 +10626,8 @@ mono_reflection_initialize_generic_parameter (MonoReflectionGenericParam *gparam
 MonoArray *
 mono_reflection_sighelper_get_signature_local (MonoReflectionSigHelper *sig)
 {
-	MonoDynamicImage *assembly = sig->module->dynamic_image;
+	MonoReflectionModuleBuilder *module = sig->module;
+	MonoDynamicImage *assembly = module != NULL ? module->dynamic_image : NULL;
 	guint32 na = sig->arguments ? mono_array_length (sig->arguments) : 0;
 	guint32 buflen, i;
 	MonoArray *result;
@@ -10638,9 +10639,11 @@ mono_reflection_sighelper_get_signature_local (MonoReflectionSigHelper *sig)
 
 	sigbuffer_add_value (&buf, 0x07);
 	sigbuffer_add_value (&buf, na);
-	for (i = 0; i < na; ++i) {
-		MonoReflectionType *type = mono_array_get (sig->arguments, MonoReflectionType*, i);
-		encode_reflection_type (assembly, type, &buf);
+	if (assembly != NULL){
+		for (i = 0; i < na; ++i) {
+			MonoReflectionType *type = mono_array_get (sig->arguments, MonoReflectionType*, i);
+			encode_reflection_type (assembly, type, &buf);
+		}
 	}
 
 	buflen = buf.p - buf.buf;
