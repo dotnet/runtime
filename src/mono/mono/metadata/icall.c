32200deea8617a719f5cf493e00505b373c4231a
Rodrigo Kumpera <kumpera@gmail.com>
2009-07-29 00:41:57 +0000

    2009-07-28 Rodrigo Kumpera  <rkumpera@novell.com>
    	* icall.c (ves_icall_Type_make_array_type): Handle the new encoding
    	schema for vectors and one dimension SZARRAY.
    
    svn path=/trunk/mono/; revision=138905
    
    
    Commit migrated from https://github.com/mono/mono/commit/9b235bf40954c9cabef8c4e0fb08b18748dc8881

--- src/mono/mono/metadata/icall.c
+++ src/mono/mono/metadata/icall.c
@@ -5981,7 +5981,10 @@ ves_icall_Type_make_array_type (MonoReflectionType *type, int rank)
 	MONO_ARCH_SAVE_REGS;
 
 	klass = mono_class_from_mono_type (type->type);
-	aklass = mono_array_class_get (klass, rank);
+	if (rank == 0) //single dimentional array
+		aklass = mono_array_class_get (klass, 1);
+	else
+		aklass = mono_bounded_array_class_get (klass, rank, TRUE);
 
 	return mono_type_get_object (mono_object_domain (type), &aklass->byval_arg);
 }
