0977e46f13bc87f82582a1436d03f9aa724f5419
Zoltan Varga <vargaz@gmail.com>
2009-07-30 11:12:35 +0000

    2009-07-30  Zoltan Varga  <vargaz@gmail.com>
    	* icall.c (ves_icall_System_Reflection_FieldInfo_internal_from_handle_type): Fix the second argument, its a MonoType* not a MonoClass*. Check whenever
    	the field belongs to the type. Fixes mono/mono#525733.
    
    svn path=/trunk/mono/; revision=139052
    
    
    Commit migrated from https://github.com/mono/mono/commit/ffaaace6012408827c5e981f8f4144c53f28cae1

--- src/mono/mono/metadata/icall.c
+++ src/mono/mono/metadata/icall.c
@@ -1588,16 +1588,32 @@ ves_icall_System_Reflection_FieldInfo_GetUnmanagedMarshal (MonoReflectionField *
 }
 
 static MonoReflectionField*
-ves_icall_System_Reflection_FieldInfo_internal_from_handle_type (MonoClassField *handle, MonoClass *klass)
+ves_icall_System_Reflection_FieldInfo_internal_from_handle_type (MonoClassField *handle, MonoType *type)
 {
+	gboolean found = FALSE;
+	MonoClass *klass;
+	MonoClass *k;
+
 	g_assert (handle);
 
-	if (!klass)
+	if (!type) {
 		klass = handle->parent;
+	} else {
+		klass = mono_class_from_mono_type (type);
 
-	/* FIXME: check that handle is a field of klass or of a parent: return null
-	 * and throw the exception in managed code.
-	 */
+		/* Check that the field belongs to the class */
+		for (k = klass; k; k = k->parent) {
+			if (k == handle->parent) {
+				found = TRUE;
+				break;
+			}
+		}
+
+		if (!found)
+			/* The managed code will throw the exception */
+			return NULL;
+	}
+
 	return mono_field_get_object (mono_domain_get (), klass, handle);
 }
 
