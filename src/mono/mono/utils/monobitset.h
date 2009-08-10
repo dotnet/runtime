9a98bcd48460be8fa6c2ad593476f8f7c2099b69
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2009-08-10 21:40:30 +0000

    2009-08-10 Gonzalo Paniagua Javier <gonzalo@novell.com>
    	* monobitset.h: add comment.
    
    
    svn path=/trunk/mono/; revision=139674
    
    
    Commit migrated from https://github.com/mono/mono/commit/6602b6d7285b510b41ec2377c1bfc6065617d887

--- src/mono/mono/utils/monobitset.h
+++ src/mono/mono/utils/monobitset.h
@@ -3,6 +3,11 @@
 
 #include <glib.h>
 
+/*
+ * When embedding, you have to define MONO_ZERO_LEN_ARRAY before including any
+ * other Mono header file if you use a different compiler from the one used to
+ * build Mono.
+ */
 #ifndef MONO_ZERO_LEN_ARRAY
 #ifdef __GNUC__
 #define MONO_ZERO_LEN_ARRAY 0
