58829cba4ffc67b577301dd4015466517609354a
Rodrigo Kumpera <kumpera@gmail.com>
2009-08-21 00:01:31 +0000

    2009-08-20  Rodrigo Kumpera  <rkumpera@novell.com>
    	* metadata-verify.c (verify_assembly_table): Accept 0x10 as a valid
    	bit for assembly flags. MS is ok with it but there is no spec anywhere
    	on its mean
    
    svn path=/trunk/mono/; revision=140392
    
    
    Commit migrated from https://github.com/mono/mono/commit/c7bb5af6fcf8d1e380a8ec440c4b535c912713e0

--- src/mono/mono/metadata/metadata-verify.c
+++ src/mono/mono/metadata/metadata-verify.c
@@ -2936,7 +2936,7 @@ verify_fieldrva_table (VerifyContext *ctx)
 	}
 }
 
-#define INVALID_ASSEMBLY_FLAGS_BITS ~((1 << 0) | (1 << 8) | (1 << 14) | (1 << 15))
+#define INVALID_ASSEMBLY_FLAGS_BITS ~((1 << 0) | (1 << 4) | (1 << 8) | (1 << 14) | (1 << 15))
 static void
 verify_assembly_table (VerifyContext *ctx)
 {
