980574cfb48ff4fce817e50cb12c9d13af8e517c
Zoltan Varga <vargaz@gmail.com>
2009-10-05 14:39:58 +0000

    2009-10-05  Zoltan Varga  <vargaz@gmail.com>
    	* domain.c (mono_init_internal): Print a useful error message when encountering
    	an old mscorlib, instead of crashing. Fixes mono/mono#544307.
    
    svn path=/trunk/mono/; revision=143409
    
    
    Commit migrated from https://github.com/mono/mono/commit/3290dd62ac612193525e1fe25d0101545cc9dfac

--- src/mono/mono/metadata/domain.c
+++ src/mono/mono/metadata/domain.c
@@ -1483,7 +1483,13 @@ mono_init_internal (const char *filename, const char *exe_filename, const char *
 
 	mono_defaults.internal_thread_class = mono_class_from_name (
                 mono_defaults.corlib, "System.Threading", "InternalThread");
-	g_assert (mono_defaults.internal_thread_class != 0);
+	if (!mono_defaults.internal_thread_class) {
+		/* This can happen with an old mscorlib */
+		fprintf (stderr, "Corlib too old for this runtime.\n");
+		fprintf (stderr, "Loaded from: %s\n",
+				 mono_defaults.corlib? mono_image_get_filename (mono_defaults.corlib): "unknown");
+		exit (1);
+	}
 
 	mono_defaults.appdomain_class = mono_class_from_name (
                 mono_defaults.corlib, "System", "AppDomain");
