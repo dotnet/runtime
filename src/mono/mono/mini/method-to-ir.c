6d2cdd74407401b63ec4eb3e2c244b93d9708dca
Mark Probst <mark.probst@gmail.com>
2009-10-14 10:23:42 +0000

    2009-10-14  Mark Probst  <mark.probst@gmail.com>
            * method-to-ir.c (mono_method_to_ir): Don't use a managed array
            allocator in shared generic code for open classes, because we
            can't get those classes' vtables.  We need to make managed
            allocators not depend on the vtable at compile-time to solve this.
    
    svn path=/trunk/mono/; revision=144087
    
    
    Commit migrated from https://github.com/mono/mono/commit/1333d8c73a47863ed1dae9aea40e2b5d1bb6af0c

--- src/mono/mono/mini/method-to-ir.c
+++ src/mono/mono/mini/method-to-ir.c
@@ -8436,8 +8436,19 @@ mono_method_to_ir (MonoCompile *cfg, MonoMethod *method, MonoBasicBlock *start_b
 			if (context_used) {
 				MonoInst *args [3];
 				MonoClass *array_class = mono_array_class_get (klass, 1);
+				/* FIXME: we cannot get a managed
+				   allocator because we can't get the
+				   open generic class's vtable.  We
+				   have the same problem in
+				   handle_alloc_from_inst().  This
+				   needs to be solved so that we can
+				   have managed allocs of shared
+				   generic classes. */
+				/*
 				MonoVTable *array_class_vtable = mono_class_vtable (cfg->domain, array_class);
 				MonoMethod *managed_alloc = mono_gc_get_managed_array_allocator (array_class_vtable, 1);
+				*/
+				MonoMethod *managed_alloc = NULL;
 
 				/* FIXME: Decompose later to help abcrem */
 
