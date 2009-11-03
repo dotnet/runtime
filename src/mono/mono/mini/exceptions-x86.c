eff55d9ef90315bc9e13ba70cdae92125ff2051a
Jonathan Chambers <joncham@gmail.com>
2009-11-03 02:20:47 +0000

    2009-11-02 Jonathan Chambers <joncham@novell.com>
    	* exceptions-x86.c (win32_handle_stack_overflow): Fix parameters
    	to mono_arch_find_jit_info_ext.
    	
    	Code contributed under MIT/X11 license.
    
    svn path=/trunk/mono/; revision=145250
    
    
    Commit migrated from https://github.com/mono/mono/commit/f0b27a6fe04a2c1a11ca5e4282bbaa8bb726c424

--- src/mono/mono/mini/exceptions-x86.c
+++ src/mono/mono/mini/exceptions-x86.c
@@ -133,7 +133,7 @@ win32_handle_stack_overflow (EXCEPTION_POINTERS* ep, struct sigcontext *sctx)
 	do {
 		MonoContext new_ctx;
 
-		mono_arch_find_jit_info_ext (domain, jit_tls, &rji, &rji, &ctx, &new_ctx, &lmf, &frame);
+		mono_arch_find_jit_info_ext (domain, jit_tls, &rji, &ctx, &new_ctx, &lmf, &frame);
 		if (!frame.ji) {
 			g_warning ("Exception inside function without unwind info");
 			g_assert_not_reached ();
