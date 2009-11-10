c1662fa9de37e44231eeca72897b93a176df4e44
Rodrigo Kumpera <kumpera@gmail.com>
2009-11-10 02:05:53 +0000

    2009-11-09  Rodrigo Kumpera  <rkumpera@novell.com>
    	* reflection.c (mono_image_module_basic_init): Handle exceptions correctly.
    
    svn path=/trunk/mono/; revision=145798
    
    
    Commit migrated from https://github.com/mono/mono/commit/518d039a04e5a591374146235b9f211ee0f875e0

--- src/mono/mono/metadata/reflection.c
+++ src/mono/mono/metadata/reflection.c
@@ -5919,16 +5919,24 @@ mono_image_module_basic_init (MonoReflectionModuleBuilder *moduleb)
 	MonoDynamicImage *image = moduleb->dynamic_image;
 	MonoReflectionAssemblyBuilder *ab = moduleb->assemblyb;
 	if (!image) {
+		MonoError error;
 		int module_count;
 		MonoImage **new_modules;
 		MonoImage *ass;
+		char *name, *fqname;
 		/*
 		 * FIXME: we already created an image in mono_image_basic_init (), but
 		 * we don't know which module it belongs to, since that is only 
 		 * determined at assembly save time.
 		 */
 		/*image = (MonoDynamicImage*)ab->dynamic_assembly->assembly.image; */
-		image = create_dynamic_mono_image (ab->dynamic_assembly, mono_string_to_utf8 (ab->name), mono_string_to_utf8 (moduleb->module.fqname));
+		name = mono_string_to_utf8 (ab->name);
+		fqname = mono_string_to_utf8_checked (moduleb->module.fqname, &error);
+		if (!mono_error_ok (&error)) {
+			g_free (name);
+			mono_error_raise_exception (&error);
+		}
+		image = create_dynamic_mono_image (ab->dynamic_assembly, name, fqname);
 
 		moduleb->module.image = &image->image;
 		moduleb->dynamic_image = image;
