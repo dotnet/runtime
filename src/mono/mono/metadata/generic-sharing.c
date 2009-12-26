462d4e8f590dea4082076924f6497ea21979446f
Zoltan Varga <vargaz@gmail.com>
2009-12-26 04:28:17 +0000

    2009-12-26  Zoltan Varga  <vargaz@gmail.com>
    	* generic-sharing.c (instantiate_other_info): Avoid creating jump trampolines since
    	they cannot be patched. Partly fixes mono/mono#564408.
    
    svn path=/trunk/mono/; revision=148899
    
    
    Commit migrated from https://github.com/mono/mono/commit/787d6e9b3f571fbf6d05da284a4eb68326b80a74

--- src/mono/mono/metadata/generic-sharing.c
+++ src/mono/mono/metadata/generic-sharing.c
@@ -795,12 +795,12 @@ instantiate_other_info (MonoDomain *domain, MonoRuntimeGenericContextOtherInfoTe
 	case MONO_RGCTX_INFO_METHOD:
 		return data;
 	case MONO_RGCTX_INFO_GENERIC_METHOD_CODE:
-		return mono_create_ftnptr (mono_domain_get (),
-				mono_runtime_create_jump_trampoline (mono_domain_get (), data, TRUE));
+		/*
+		 * We can't create a jump trampoline here, as it cannot be patched.
+		 */
+		return mono_create_ftnptr (mono_domain_get (), mono_compile_method (data));
 	case MONO_RGCTX_INFO_REMOTING_INVOKE_WITH_CHECK:
-		return mono_create_ftnptr (mono_domain_get (),
-				mono_runtime_create_jump_trampoline (mono_domain_get (),
-						mono_marshal_get_remoting_invoke_with_check (data), TRUE));
+		return mono_create_ftnptr (mono_domain_get (), mono_compile_method (mono_marshal_get_remoting_invoke_with_check (data)));
 	case MONO_RGCTX_INFO_METHOD_DELEGATE_CODE:
 		return mono_domain_alloc0 (domain, sizeof (gpointer));
 	case MONO_RGCTX_INFO_CLASS_FIELD:
