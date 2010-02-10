93b126b8123ad64a33db4e2908bee5086179b276
Rodrigo Kumpera <kumpera@gmail.com>
2010-02-10 15:03:50 +0000

    2010-02-09  Rodrigo Kumpera  <rkumpera@novell.com>
    	* reflection.c (mono_image_get_property_info): Encode the default value of a property.
    
    svn path=/trunk/mono/; revision=151229
    
    
    Commit migrated from https://github.com/mono/mono/commit/801b9c7aa4231e305d2b643f8b53fb4a3761d9b7

--- src/mono/mono/metadata/reflection.c
+++ src/mono/mono/metadata/reflection.c
@@ -1985,6 +1985,7 @@ mono_image_get_property_info (MonoReflectionPropertyBuilder *pb, MonoDynamicImag
 	 * PROPERTY    (rows already preallocated in _get_type_info ())
 	 * METHOD      (method info already done with the generic method code)
 	 * METHODSEMANTICS
+	 * CONSTANT
 	 */
 	table = &assembly->tables [MONO_TABLE_PROPERTY];
 	pb->table_idx = table->next_idx ++;
@@ -2014,6 +2015,17 @@ mono_image_get_property_info (MonoReflectionPropertyBuilder *pb, MonoDynamicImag
 		values [MONO_METHOD_SEMA_SEMANTICS] = METHOD_SEMANTIC_SETTER;
 		values [MONO_METHOD_SEMA_METHOD] = pb->set_method->table_idx;
 		values [MONO_METHOD_SEMA_ASSOCIATION] = (pb->table_idx << MONO_HAS_SEMANTICS_BITS) | MONO_HAS_SEMANTICS_PROPERTY;
+	}
+	if (pb->attrs & PROPERTY_ATTRIBUTE_HAS_DEFAULT) {
+		guint32 field_type = 0;
+		table = &assembly->tables [MONO_TABLE_CONSTANT];
+		table->rows ++;
+		alloc_table (table, table->rows);
+		values = table->values + table->rows * MONO_CONSTANT_SIZE;
+		values [MONO_CONSTANT_PARENT] = MONO_HASCONSTANT_PROPERTY | (pb->table_idx << MONO_HASCONSTANT_BITS);
+		values [MONO_CONSTANT_VALUE] = encode_constant (assembly, pb->def_value, &field_type);
+		values [MONO_CONSTANT_TYPE] = field_type;
+		values [MONO_CONSTANT_PADDING] = 0;
 	}
 }
 
