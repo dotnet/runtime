858f88974c816115493c1ddbde5577763c1457b7
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2010-02-16 01:50:14 +0000

    update year
    svn path=/trunk/mono/; revision=151765
    
    
    Commit migrated from https://github.com/mono/mono/commit/0f3d148ab8fcd90b005562b297eff4ad972e3785

--- src/mono/mono/mini/driver.c
+++ src/mono/mono/mini/driver.c
@@ -1335,7 +1335,7 @@ mono_main (int argc, char* argv[])
 			mini_verbose++;
 		} else if (strcmp (argv [i], "--version") == 0 || strcmp (argv [i], "-V") == 0) {
 			char *build = mono_get_runtime_build_info ();
-			g_print ("Mono JIT compiler version %s\nCopyright (C) 2002-2009 Novell, Inc and Contributors. www.mono-project.com\n", build);
+			g_print ("Mono JIT compiler version %s\nCopyright (C) 2002-2010 Novell, Inc and Contributors. www.mono-project.com\n", build);
 			g_free (build);
 			g_print (info);
 			if (mini_verbose) {
