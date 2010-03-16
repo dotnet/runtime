3b61480d26bc697739c88cdb548dd1e9d64e075e
Rodrigo Kumpera <kumpera@gmail.com>
2010-03-16 15:16:25 +0000

    2010-03-16 Rodrigo Kumpera  <rkumpera@novell.com>
    	 * mini-x86.c (mono_arch_output_basic_block): Record try block holes.
    
    svn path=/trunk/mono/; revision=153687
    
    
    Commit migrated from https://github.com/mono/mono/commit/e762ecad98cb3405c366da74fcfecc1db296f491

--- src/mono/mono/mini/mini-x86.c
+++ src/mono/mono/mini/mini-x86.c
@@ -2981,6 +2981,7 @@ mono_arch_output_basic_block (MonoCompile *cfg, MonoBasicBlock *bb)
 			x86_alu_reg_imm (code, X86_SUB, X86_ESP, MONO_ARCH_FRAME_ALIGNMENT - 4);
 			mono_add_patch_info (cfg, code - cfg->native_code, MONO_PATCH_INFO_BB, ins->inst_target_bb);
 			x86_call_imm (code, 0);
+			mono_cfg_add_try_hole (cfg, ins->inst_eh_block, code, bb);
 			x86_alu_reg_imm (code, X86_ADD, X86_ESP, MONO_ARCH_FRAME_ALIGNMENT - 4);
 			break;
 		case OP_START_HANDLER: {
