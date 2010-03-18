c8a0b40152e8ac6e82470078df29ec3de9110334
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2010-03-18 13:55:15 +0000

    fixup nsec for absolute time too
    svn path=/trunk/mono/; revision=153838
    
    
    Commit migrated from https://github.com/mono/mono/commit/a63e10a75c7baf7a3066b6d2ba77041a6f1ddbad

--- src/mono/mono/utils/mono-semaphore.c
+++ src/mono/mono/utils/mono-semaphore.c
@@ -9,6 +9,9 @@
 
 #include <config.h>
 #include "utils/mono-semaphore.h"
+#ifdef HAVE_SYS_TIME_H
+#include <sys/time.h>
+#endif
 
 #if defined(HAVE_SEMAPHORE_H) || defined(USE_MACH_SEMA)
 /* sem_* or semaphore_* functions in use */
@@ -23,16 +26,24 @@
 gboolean
 mono_sem_timedwait (MonoSemType *sem, guint32 timeout_ms)
 {
-	TIMESPEC tv;
+	TIMESPEC ts;
+	struct timeval t;
 
 #ifndef USE_MACH_SEMA
 	if (timeout_ms == 0)
 		return (!sem_trywait (sem));
 #endif
 
-	tv.tv_sec = time (NULL) + timeout_ms / 1000;
-	tv.tv_nsec = (timeout_ms % 1000) * 1000000;
-	return (!WAIT_BLOCK (sem, &tv));
+	t.tv_sec = 0;
+	t.tv_usec = 0;
+	gettimeofday (&t, NULL);
+	ts.tv_sec = timeout_ms / 1000 + t.tv_sec;
+	ts.tv_nsec = (timeout_ms % 1000) * 1000000 + t.tv_usec * 1000;
+	while (ts.tv_nsec > 1000000000) {
+		ts.tv_nsec -= 1000000000;
+		ts.tv_sec++;
+	}
+	return (!WAIT_BLOCK (sem, &ts));
 }
 
 #else
