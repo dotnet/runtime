1231dcb0d3fe8f4ad35828cfc8adfca0317824ac
Zoltan Varga <vargaz@gmail.com>
2010-03-19 12:09:14 +0000

    Fix the build.
    svn path=/trunk/mono/; revision=153889
    
    
    Commit migrated from https://github.com/mono/mono/commit/fa1475a28c41139c0ad5234cae3a100eddcaf6d1

--- src/mono/mono/mini/aot-runtime.c
+++ src/mono/mono/mini/aot-runtime.c
@@ -3018,7 +3018,7 @@ mono_aot_get_method (MonoDomain *domain, MonoMethod *method)
 
 		if (method_index == 0xffffff && method->is_inflated && mono_method_is_generic_sharable_impl_full (method, FALSE, TRUE)) {
 			/* Partial sharing */
-			method_index = find_extra_method (mini_get_shared_method_to_register (method), &amodule);
+			method_index = find_extra_method (mini_get_shared_method (method), &amodule);
 		}
 
 		if (method_index == 0xffffff) {
