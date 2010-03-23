863896c1df28d61b30aa30d57a853f5436155a2b
Rodrigo Kumpera <kumpera@gmail.com>
2010-03-23 01:29:57 +0000

    2010-03-22 Rodrigo Kumpera  <rkumpera@novell.com>
    	* threads.c (ves_icall_System_Threading_Thread_ResetAbort):
    	Check MonoInternalThread's ::state instead of ::abort_exc
    	since the later can be lazily created.
    
    	This is specially problematic when running a finally block
    	under AbortRequested state. ResetAbort must work, but the
    	abort_exc object has not been created because interruption
    	has not began.
    
    svn path=/trunk/mono/; revision=154031
    
    
    Commit migrated from https://github.com/mono/mono/commit/69736898252ad6fea167a7755d3e6f90a2a1c8b0

--- src/mono/mono/metadata/threads.c
+++ src/mono/mono/metadata/threads.c
@@ -2321,28 +2321,26 @@ void
 ves_icall_System_Threading_Thread_ResetAbort (void)
 {
 	MonoInternalThread *thread = mono_thread_internal_current ();
+	gboolean was_aborting;
 
 	ensure_synch_cs_set (thread);
 	
 	EnterCriticalSection (thread->synch_cs);
-
+	was_aborting = thread->state & ThreadState_AbortRequested;
 	thread->state &= ~ThreadState_AbortRequested;
-	
-	if (!thread->abort_exc) {
-		const char *msg = "Unable to reset abort because no abort was requested";
-		LeaveCriticalSection (thread->synch_cs);
-		mono_raise_exception (mono_get_exception_thread_state (msg));
-	} else {
-		thread->abort_exc = NULL;
-		if (thread->abort_state_handle) {
-			mono_gchandle_free (thread->abort_state_handle);
-			/* This is actually not necessary - the handle
-			   only counts if the exception is set */
-			thread->abort_state_handle = 0;
-		}
-	}
-	
 	LeaveCriticalSection (thread->synch_cs);
+
+	if (!was_aborting) {
+		const char *msg = "Unable to reset abort because no abort was requested";
+		mono_raise_exception (mono_get_exception_thread_state (msg));
+	}
+	thread->abort_exc = NULL;
+	if (thread->abort_state_handle) {
+		mono_gchandle_free (thread->abort_state_handle);
+		/* This is actually not necessary - the handle
+		   only counts if the exception is set */
+		thread->abort_state_handle = 0;
+	}
 }
 
 void
