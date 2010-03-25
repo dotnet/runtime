27383f8dd96feef30e825165f9180990339a7f76
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2010-03-25 20:43:22 +0000

    2010-03-25 Gonzalo Paniagua Javier <gonzalo@novell.com>
    	* mono-semaphore.[hc]: make sure the semaphore calls are restarted if
    	interrupted. Return 0 on success and -1 on failure on windows.
    
    
    svn path=/trunk/mono/; revision=154219
    
    
    Commit migrated from https://github.com/mono/mono/commit/d44b73d72ab443e7cabc7443ab89958dfe2bc373

--- src/mono/mono/utils/mono-semaphore.c
+++ src/mono/mono/utils/mono-semaphore.c
@@ -8,6 +8,7 @@
  */
 
 #include <config.h>
+#include <errno.h>
 #include "utils/mono-semaphore.h"
 #ifdef HAVE_SYS_TIME_H
 #include <sys/time.h>
@@ -23,21 +24,20 @@
 #    define WAIT_BLOCK(a,b) sem_timedwait (a, b)
 #  endif
 
-gboolean
+int
 mono_sem_timedwait (MonoSemType *sem, guint32 timeout_ms)
 {
-	TIMESPEC ts;
+	TIMESPEC ts, copy;
 	struct timeval t;
+	int res;
 
 #ifndef USE_MACH_SEMA
 	if (timeout_ms == 0)
 		return (!sem_trywait (sem));
 #endif
 	if (timeout_ms == (guint32) 0xFFFFFFFF)
-		return MONO_SEM_WAIT (sem);
+		return mono_sem_wait (sem);
 
-	t.tv_sec = 0;
-	t.tv_usec = 0;
 	gettimeofday (&t, NULL);
 	ts.tv_sec = timeout_ms / 1000 + t.tv_sec;
 	ts.tv_nsec = (timeout_ms % 1000) * 1000000 + t.tv_usec * 1000;
@@ -45,16 +45,61 @@ mono_sem_timedwait (MonoSemType *sem, guint32 timeout_ms)
 		ts.tv_nsec -= 1000000000;
 		ts.tv_sec++;
 	}
-	return (!WAIT_BLOCK (sem, &ts));
+	copy = ts;
+	while ((res = WAIT_BLOCK (sem, &ts) == -1) && errno == EINTR)
+		ts = copy;
+	return res;
+}
+
+int
+mono_sem_wait (MonoSemType *sem)
+{
+	int res;
+#ifndef USE_MACH_SEMA
+	while ((res = sem_wait (sem) == -1) && errno == EINTR);
+#else
+	while ((res = semaphore_wait (*sem) == -1) && errno == EINTR);
+#endif
+	return res;
+}
+
+int
+mono_sem_post (MonoSemType *sem)
+{
+	int res;
+#ifndef USE_MACH_SEMA
+	while ((res = sem_post (sem) == -1) && errno == EINTR);
+#else
+	while ((res = semaphore_signal (*sem) == -1) && errno == EINTR);
+#endif
+	return res;
 }
 
 #else
 /* Windows or io-layer functions in use */
-gboolean
-mono_sem_timedwait (MonoSemType *sem, guint32 timeout_ms)
+int
+mono_sem_wait (MonoSemType *sem)
 {
-	return WaitForSingleObjectEx (*sem, timeout_ms, TRUE);
+	return mono_sem_timedwait (sem, INFINITE);
 }
 
+int
+mono_sem_timedwait (MonoSemType *sem, guint32 timeout_ms)
+{
+	gboolean res;
+
+	res = WaitForSingleObjectEx (*sem, timeout_ms, FALSE);
+	if (!res)
+		return -1;
+	return 0;
+}
+
+int
+mono_sem_post (MonoSemType *sem)
+{
+	if (!ReleaseSemaphore (*sem, 1, NULL))
+		return -1;
+	return 0;
+}
 #endif
 
--- src/mono/mono/utils/mono-semaphore.h
+++ src/mono/mono/utils/mono-semaphore.h
@@ -26,22 +26,22 @@
 #    include <mach/semaphore.h>
 typedef semaphore_t MonoSemType;
 #    define MONO_SEM_INIT(addr,value) semaphore_create (current_task (), (addr), SYNC_POLICY_FIFO, (value))
-#    define MONO_SEM_WAIT(sem) semaphore_wait (*(sem))
-#    define MONO_SEM_POST(sem) semaphore_signal (*(sem))
+#    define MONO_SEM_WAIT(sem) mono_sem_wait ((sem))
+#    define MONO_SEM_POST(sem) mono_sem_post (*(sem))
 #    define MONO_SEM_DESTROY(sem) semaphore_destroy (current_task (), *(sem))
 #  else
 typedef sem_t MonoSemType;
 #    define MONO_SEM_INIT(addr,value) sem_init ((addr), 0, (value))
-#    define MONO_SEM_WAIT(sem) sem_wait ((sem))
-#    define MONO_SEM_POST(sem) sem_post ((sem))
+#    define MONO_SEM_WAIT(sem) mono_sem_wait ((sem))
+#    define MONO_SEM_POST(sem) mono_sem_post ((sem))
 #    define MONO_SEM_DESTROY(sem) sem_destroy ((sem))
 #  endif
 #elif defined(HOST_WIN32) || defined(_WAPI_SEMAPHORES_H)
 #  define MONO_HAS_SEMAPHORES
 typedef HANDLE MonoSemType;
 #    define MONO_SEM_INIT(addr,initial) do {*(addr) = CreateSemaphore ( NULL,(initial),0x7FFFFFFF,NULL);} while(0)
-#    define MONO_SEM_WAIT(sem) (!WaitForSingleObjectEx (*(sem),INFINITE, TRUE))
-#    define MONO_SEM_POST(sem) (!(ReleaseSemaphore (*(sem),1,NULL)))
+#    define MONO_SEM_WAIT(sem) mono_sem_wait (sem)
+#    define MONO_SEM_POST(sem) mono_sem_post (sem)
 #    define MONO_SEM_DESTROY(sem) CloseHandle (*(sem))
 #endif
 
@@ -49,7 +49,9 @@ typedef HANDLE MonoSemType;
 
 G_BEGIN_DECLS
 
-gboolean mono_sem_timedwait (MonoSemType *sem, guint32 timeout_ms);
+int mono_sem_wait (MonoSemType *sem);
+int mono_sem_timedwait (MonoSemType *sem, guint32 timeout_ms);
+int mono_sem_post (MonoSemType *sem);
 
 G_END_DECLS
 #endif /* _MONO_SEMAPHORE_H_ */
