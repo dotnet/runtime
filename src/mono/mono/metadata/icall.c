8ed252e30d3e483c07fd153643abdd72570cc0e3
Zoltan Varga <vargaz@gmail.com>
2010-03-30 03:26:59 +0000

    2010-03-30  Zoltan Varga  <vargaz@gmail.com>
    	* icall.c (ves_icall_System_Enum_get_underlying_type): Don't crash on
    	unfinished/broken typebuilders.
    
    svn path=/trunk/mono/; revision=154417
    
    
    Commit migrated from https://github.com/mono/mono/commit/a4808ee095913cfc016938ac703109b484f2024c

--- src/mono/mono/metadata/icall.c
+++ src/mono/mono/metadata/icall.c
@@ -3124,9 +3124,16 @@ ves_icall_System_Enum_get_value (MonoObject *this)
 static MonoReflectionType *
 ves_icall_System_Enum_get_underlying_type (MonoReflectionType *type)
 {
+	MonoType *etype;
+
 	MONO_ARCH_SAVE_REGS;
 
-	return mono_type_get_object (mono_object_domain (type), mono_class_enum_basetype (mono_class_from_mono_type (type->type)));
+	etype = mono_class_enum_basetype (mono_class_from_mono_type (type->type));
+	if (!etype)
+		/* MS throws this for typebuilders */
+		mono_raise_exception (mono_get_exception_argument ("Type must be a type provided by the runtime.", "enumType"));
+
+	return mono_type_get_object (mono_object_domain (type), etype);
 }
 
 static int
