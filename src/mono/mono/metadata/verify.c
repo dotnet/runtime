90a20c80e3cd7b4486494d06dba777244afbe3f7
Rodrigo Kumpera <kumpera@gmail.com>
2010-04-13 22:59:23 +0000

    2010-04-13 Rodrigo Kumpera  <rkumpera@novell.com>
    	* verify.c (mono_verifier_is_enabled_for_method): Handle
    	assembly less images.
    
    	* verify.c (mono_verifier_is_class_full_trust): Ditto.
    
    svn path=/trunk/mono/; revision=155341
    
    
    Commit migrated from https://github.com/mono/mono/commit/af244b4827fd7291e0ca7a06bba171617bb9a116

--- src/mono/mono/metadata/verify.c
+++ src/mono/mono/metadata/verify.c
@@ -5588,7 +5588,7 @@ mono_verifier_is_enabled_for_method (MonoMethod *method)
 gboolean
 mono_verifier_is_enabled_for_class (MonoClass *klass)
 {
-	return verify_all || (verifier_mode > MONO_VERIFIER_MODE_OFF && !klass->image->assembly->in_gac && klass->image != mono_defaults.corlib);
+	return verify_all || (verifier_mode > MONO_VERIFIER_MODE_OFF && !(klass->image->assembly && klass->image->assembly->in_gac) && klass->image != mono_defaults.corlib);
 }
 
 gboolean
@@ -5616,7 +5616,7 @@ mono_verifier_is_class_full_trust (MonoClass *klass)
 {
 	/* under CoreCLR code is trusted if it is part of the "platform" otherwise all code inside the GAC is trusted */
 	gboolean trusted_location = (mono_security_get_mode () != MONO_SECURITY_MODE_CORE_CLR) ? 
-		klass->image->assembly->in_gac : mono_security_core_clr_is_platform_image (klass->image);
+		(klass->image->assembly && klass->image->assembly->in_gac) : mono_security_core_clr_is_platform_image (klass->image);
 
 	if (verify_all && verifier_mode == MONO_VERIFIER_MODE_OFF)
 		return trusted_location || klass->image == mono_defaults.corlib;
