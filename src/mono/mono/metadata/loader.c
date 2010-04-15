d029460a9f1cff007df5752453702d332c2acccc
Rodrigo Kumpera <kumpera@gmail.com>
2010-04-15 18:30:58 +0000

    2010-04-15 Rodrigo Kumpera  <rkumpera@novell.com>
    	* loader.c (inflate_generic_signature): Add _checked variant
    	and move this function to use it.
    
    svn path=/trunk/mono/; revision=155527
    
    
    Commit migrated from https://github.com/mono/mono/commit/a947192eb608e10650640ca1e6f6f11c38fb4ad1

--- src/mono/mono/metadata/loader.c
+++ src/mono/mono/metadata/loader.c
@@ -705,22 +705,28 @@ find_method (MonoClass *in_class, MonoClass *ic, const char* name, MonoMethodSig
 }
 
 static MonoMethodSignature*
-inflate_generic_signature (MonoImage *image, MonoMethodSignature *sig, MonoGenericContext *context)
+inflate_generic_signature_checked (MonoImage *image, MonoMethodSignature *sig, MonoGenericContext *context, MonoError *error)
 {
 	MonoMethodSignature *res;
 	gboolean is_open;
 	int i;
 
+	mono_error_init (error);
 	if (!context)
 		return sig;
 
 	res = g_malloc0 (MONO_SIZEOF_METHOD_SIGNATURE + ((gint32)sig->param_count) * sizeof (MonoType*));
 	res->param_count = sig->param_count;
 	res->sentinelpos = -1;
-	res->ret = mono_class_inflate_generic_type (sig->ret, context);
+	res->ret = mono_class_inflate_generic_type_checked (sig->ret, context, error);
+	if (!mono_error_ok (error))
+		goto fail;
 	is_open = mono_class_is_open_constructed_type (res->ret);
 	for (i = 0; i < sig->param_count; ++i) {
-		res->params [i] = mono_class_inflate_generic_type (sig->params [i], context);
+		res->params [i] = mono_class_inflate_generic_type_checked (sig->params [i], context, error);
+		if (!mono_error_ok (error))
+			goto fail;
+
 		if (!is_open)
 			is_open = mono_class_is_open_constructed_type (res->params [i]);
 	}
@@ -732,6 +738,25 @@ inflate_generic_signature (MonoImage *image, MonoMethodSignature *sig, MonoGener
 	res->sentinelpos = sig->sentinelpos;
 	res->has_type_parameters = is_open;
 	res->is_inflated = 1;
+	return res;
+
+fail:
+	if (res->ret)
+		mono_metadata_free_type (res->ret);
+	for (i = 0; i < sig->param_count; ++i) {
+		if (res->params [i])
+			mono_metadata_free_type (res->params [i]);
+	}
+	g_free (res);
+	return NULL;
+}
+
+static MonoMethodSignature*
+inflate_generic_signature (MonoImage *image, MonoMethodSignature *sig, MonoGenericContext *context)
+{
+	MonoError error;
+	MonoMethodSignature *res = inflate_generic_signature_checked (image, sig, context, &error);
+	g_assert (mono_error_ok (&error)); /*FIXME move callers to use _checked version*/
 	return res;
 }
 
