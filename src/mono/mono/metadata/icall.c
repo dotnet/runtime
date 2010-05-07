4cbf7e9a76482f3d4b32a4986b0c5ba048808e38
Rodrigo Kumpera <kumpera@gmail.com>
2010-05-07 22:41:43 +0000

    2010-05-07 Rodrigo Kumpera  <rkumpera@novell.com>
    	* icall-def.h
    	* icall.c: Delete some unused icalls.
    
    svn path=/trunk/mono/; revision=156926
    
    
    Commit migrated from https://github.com/mono/mono/commit/079db735345ac60bda8ac04537e66c0dd4ca1641

--- src/mono/mono/metadata/icall.c
+++ src/mono/mono/metadata/icall.c
@@ -2276,22 +2276,6 @@ ves_icall_MonoType_get_DeclaringType (MonoReflectionType *type)
 	return class ? mono_type_get_object (domain, &class->byval_arg) : NULL;
 }
 
-static MonoReflectionType*
-ves_icall_MonoType_get_UnderlyingSystemType (MonoReflectionType *type)
-{
-	MonoDomain *domain = mono_domain_get (); 
-	MonoClass *class = mono_class_from_mono_type (type->type);
-
-	MONO_ARCH_SAVE_REGS;
-
-	if (class->enumtype && mono_class_enum_basetype (class)) /* types that are modified typebuilders may not have enum_basetype set */
-		return mono_type_get_object (domain, mono_class_enum_basetype (class));
-	else if (class->element_class)
-		return mono_type_get_object (domain, &class->element_class->byval_arg);
-	else
-		return NULL;
-}
-
 static MonoString*
 ves_icall_MonoType_get_Name (MonoReflectionType *type)
 {
