8ba58c6cf1eea4922673bc9be4ddb5ee8de595a5
Rodrigo Kumpera <kumpera@gmail.com>
2010-05-13 01:39:10 +0000

    2010-05-12 Rodrigo Kumpera  <rkumpera@novell.com>
    	* icall.c: Switch to mono-ptr-array.h.
    
    svn path=/trunk/mono/; revision=157287
    
    
    Commit migrated from https://github.com/mono/mono/commit/038a5182ec33112d3ec388efae0afc7efb0978c6

--- src/mono/mono/metadata/icall.c
+++ src/mono/mono/metadata/icall.c
@@ -67,6 +67,7 @@
 #include <mono/metadata/security-core-clr.h>
 #include <mono/metadata/mono-perfcounters.h>
 #include <mono/metadata/mono-debug.h>
+#include <mono/metadata/mono-ptr-array.h>
 #include <mono/io-layer/io-layer.h>
 #include <mono/utils/strtod.h>
 #include <mono/utils/monobitset.h>
@@ -88,49 +89,6 @@ static MonoReflectionAssembly* ves_icall_System_Reflection_Assembly_GetCallingAs
 
 static MonoArray*
 type_array_from_modifiers (MonoImage *image, MonoType *type, int optional);
-
-/* This is an implementation of a growable pointer array that avoids doing memory allocations for small sizes.
- * It works by allocating an initial small array on stack and only going to malloc'd memory if needed. 
- */
-typedef struct {
-	void **data;
-	int size;
-	int capacity;
-} MonoPtrArray;
-
-#define MONO_PTR_ARRAY_MAX_ON_STACK (16)
-
-#define mono_ptr_array_init(ARRAY, INITIAL_SIZE) do {\
-	(ARRAY).size = 0; \
-	(ARRAY).capacity = MAX (INITIAL_SIZE, MONO_PTR_ARRAY_MAX_ON_STACK); \
-	(ARRAY).data = INITIAL_SIZE > MONO_PTR_ARRAY_MAX_ON_STACK ? mono_gc_alloc_fixed (sizeof (void*) * INITIAL_SIZE, NULL) : g_newa (void*, MONO_PTR_ARRAY_MAX_ON_STACK); \
-} while (0)
-
-#define mono_ptr_array_destroy(ARRAY) do {\
-	if ((ARRAY).capacity > MONO_PTR_ARRAY_MAX_ON_STACK) \
-		mono_gc_free_fixed ((ARRAY).data); \
-} while (0)
-
-#define mono_ptr_array_append(ARRAY, VALUE) do { \
-	if ((ARRAY).size >= (ARRAY).capacity) {\
-		void *__tmp = mono_gc_alloc_fixed (sizeof (void*) * (ARRAY).capacity * 2, NULL); \
-		memcpy (__tmp, (ARRAY).data, (ARRAY).capacity * sizeof (void*)); \
-		if ((ARRAY).capacity > MONO_PTR_ARRAY_MAX_ON_STACK)	\
-			mono_gc_free_fixed ((ARRAY).data);	\
-		(ARRAY).data = __tmp;											\
-		(ARRAY).capacity *= 2;\
-	}\
-	((ARRAY).data [(ARRAY).size++] = VALUE); \
-} while (0)
-
-#define mono_ptr_array_set(ARRAY, IDX, VALUE) do { \
-	((ARRAY).data [(IDX)] = VALUE); \
-} while (0)
-	
-#define mono_ptr_array_get(ARRAY, IDX) ((ARRAY).data [(IDX)])
-
-#define mono_ptr_array_size(ARRAY) ((ARRAY).size)
-
 
 static inline MonoBoolean
 is_generic_parameter (MonoType *type)
