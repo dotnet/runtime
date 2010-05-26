0df634842b995fe7508e49d476721a7e2532dcd0
Jb Evain <jbevain@gmail.com>
2010-05-26 12:23:20 +0000

    fix integration tests
    svn path=/trunk/mcs/; revision=157958
    
    
    Commit migrated from https://github.com/dotnet/linker/commit/36360b37b5e162c309b6ec9bc5a78eda41c28177

--- src/tools/illink/linker/Tests/Mono.Linker.Tests/IntegrationTestFixture.cs
+++ src/tools/illink/linker/Tests/Mono.Linker.Tests/IntegrationTestFixture.cs
@@ -69,6 +69,7 @@ namespace Mono.Linker.Tests {
 			Pipeline p = new Pipeline ();
 			p.AppendStep (new LoadReferencesStep ());
 			p.AppendStep (new BlacklistStep ());
+			p.AppendStep (new TypeMapStep ());
 			p.AppendStep (new MarkStep ());
 			p.AppendStep (new SweepStep ());
 			p.AppendStep (new CleanStep ());
