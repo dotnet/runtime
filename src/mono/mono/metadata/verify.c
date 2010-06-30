20a8d3ae90551efaf575183da4def1d379e5b14a
Rodrigo Kumpera <kumpera@gmail.com>
2010-06-30 17:36:51 +0000

    2010-06-30 Rodrigo Kumpera  <rkumpera@novell.com>
    	* verify.c (do_store_static_field): Improve error
    	message.
    
    svn path=/trunk/mono/; revision=159742
    
    
    Commit migrated from https://github.com/mono/mono/commit/584f3ea026378b9e415b7a20dfbf7dfa31c77133

--- src/mono/mono/metadata/verify.c
+++ src/mono/mono/metadata/verify.c
@@ -3043,8 +3043,14 @@ do_store_static_field (VerifyContext *ctx, int token) {
 	if (!IS_SKIP_VISIBILITY (ctx) && !mono_method_can_access_field_full (ctx->method, field, NULL))
 		CODE_NOT_VERIFIABLE2 (ctx, g_strdup_printf ("Type at stack is not accessible at 0x%04x", ctx->ip_offset), MONO_EXCEPTION_FIELD_ACCESS);
 
-	if (!verify_stack_type_compatibility (ctx, field->type, value))
-		CODE_NOT_VERIFIABLE (ctx, g_strdup_printf ("Incompatible type %s in static field store at 0x%04x", stack_slot_get_name (value), ctx->ip_offset));	
+	if (!verify_stack_type_compatibility (ctx, field->type, value)) {
+		char *stack_name = stack_slot_full_name (value);
+		char *field_name = mono_type_full_name (field->type);
+		CODE_NOT_VERIFIABLE (ctx, g_strdup_printf ("Incompatible type in static field store expected '%s' but found '%s' at 0x%04x",
+				field_name, stack_name, ctx->ip_offset));
+		g_free (field_name);
+		g_free (stack_name);
+	}
 }
 
 static gboolean
