df993c074a590e06bc6be0f9bd6976da23be6633
Rodrigo Kumpera <kumpera@gmail.com>
2010-07-08 00:42:11 +0000

    2010-07-08 Rodrigo Kumpera  <rkumpera@novell.com>
    	* verify.c (do_stobj): Improve error message.
    
    svn path=/trunk/mono/; revision=160030
    
    
    Commit migrated from https://github.com/mono/mono/commit/f1242f032b3c31489309fa0c2d48b35aabad2cde

--- src/mono/mono/metadata/verify.c
+++ src/mono/mono/metadata/verify.c
@@ -3373,8 +3373,13 @@ do_stobj (VerifyContext *ctx, int token)
 	if (stack_slot_is_boxed_value (src) && !MONO_TYPE_IS_REFERENCE (src->type) && !MONO_TYPE_IS_REFERENCE (type))
 		CODE_NOT_VERIFIABLE (ctx, g_strdup_printf ("Cannot use stobj with a boxed source value that is not a reference type at 0x%04x", ctx->ip_offset));
 
-	if (!verify_stack_type_compatibility (ctx, type, src))
-		CODE_NOT_VERIFIABLE (ctx, g_strdup_printf ("Token and source types of stobj don't match at 0x%04x", ctx->ip_offset));
+	if (!verify_stack_type_compatibility (ctx, type, src)) {
+		char *type_name = mono_type_full_name (type);
+		char *src_name = stack_slot_full_name (src);
+		CODE_NOT_VERIFIABLE (ctx, g_strdup_printf ("Token '%s' and source '%s' of stobj don't match ' at 0x%04x", type_name, src_name, ctx->ip_offset));
+		g_free (type_name);
+		g_free (src_name);
+	}
 
 	if (!verify_type_compatibility (ctx, mono_type_get_type_byval (dest->type), type))
 		CODE_NOT_VERIFIABLE (ctx, g_strdup_printf ("Destination and token types of stobj don't match at 0x%04x", ctx->ip_offset));
