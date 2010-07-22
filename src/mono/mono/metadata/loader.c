8fab03b84cff72dbfc1bce5a442320f21a63096e
Zoltan Varga <vargaz@gmail.com>
2010-07-23 01:54:53 +0200

    2010-07-23  Zoltan Varga  <vargaz@gmail.com>
    	* loader.c (mono_loader_lock_if_inited): New helper function which can be called
    	during startup before the loader lock is initialized.
    	(mono_loader_unlock_if_inited): Ditto.
    
    
    Commit migrated from https://github.com/mono/mono/commit/6143425b511fa2dd8dfccd79603d3648ab5f6b9c

--- src/mono/mono/metadata/loader.c
+++ src/mono/mono/metadata/loader.c
@@ -54,6 +54,7 @@ MonoDefaults mono_defaults;
  * domain lock.
  */
 static CRITICAL_SECTION loader_mutex;
+static gboolean loader_lock_inited;
 
 /* Statistics */
 static guint32 inflated_signatures_size;
@@ -77,6 +78,7 @@ mono_loader_init ()
 
 	if (!inited) {
 		InitializeCriticalSection (&loader_mutex);
+		loader_lock_inited = TRUE;
 
 		loader_error_thread_id = TlsAlloc ();
 		loader_lock_nest_id = TlsAlloc ();
@@ -97,6 +99,7 @@ mono_loader_cleanup (void)
 	TlsFree (loader_lock_nest_id);
 
 	/*DeleteCriticalSection (&loader_mutex);*/
+	loader_lock_inited = FALSE;
 }
 
 /*
@@ -2088,6 +2091,26 @@ mono_loader_lock_is_owned_by_self (void)
 	g_assert (loader_lock_track_ownership);
 
 	return GPOINTER_TO_UINT (TlsGetValue (loader_lock_nest_id)) > 0;
+}
+
+/*
+ * mono_loader_lock_if_inited:
+ *
+ *   Acquire the loader lock if it has been initialized, no-op otherwise. This can
+ * be used in runtime initialization code which can be executed before mono_loader_init ().
+ */
+void
+mono_loader_lock_if_inited (void)
+{
+	if (loader_lock_inited)
+		mono_loader_lock ();
+}
+
+void
+mono_loader_unlock_if_inited (void)
+{
+	if (loader_lock_inited)
+		mono_loader_unlock ();
 }
 
 /**
