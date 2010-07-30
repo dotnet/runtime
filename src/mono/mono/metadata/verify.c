bed723f5a0a0caae2c6ceea9020e304d76936859
Rodrigo Kumpera <kumpera@gmail.com>
2010-07-30 16:53:26 -0300

    Verify the token for ldtoken.
    	* verify.c (do_load_token): Verify table and index
    	of the source token.
    
    	Fixes mono/mono#560348
    
    
    Commit migrated from https://github.com/mono/mono/commit/da58ce5fd34c83d5ab039434baf37cad046b51f1

--- src/mono/mono/metadata/verify.c
+++ src/mono/mono/metadata/verify.c
@@ -3318,6 +3318,25 @@ do_load_token (VerifyContext *ctx, int token)
 	MonoClass *handle_class;
 	if (!check_overflow (ctx))
 		return;
+
+	switch (token & 0xff000000) {
+	case MONO_TOKEN_TYPE_DEF:
+	case MONO_TOKEN_TYPE_REF:
+	case MONO_TOKEN_TYPE_SPEC:
+	case MONO_TOKEN_FIELD_DEF:
+	case MONO_TOKEN_METHOD_DEF:
+	case MONO_TOKEN_METHOD_SPEC:
+	case MONO_TOKEN_MEMBER_REF:
+		if (!token_bounds_check (ctx->image, token)) {
+			ADD_VERIFY_ERROR (ctx, g_strdup_printf ("Table index out of range 0x%x for token %x for ldtoken at 0x%04x", mono_metadata_token_index (token), token, ctx->ip_offset));
+			return;
+		}
+		break;
+	default:
+		ADD_VERIFY_ERROR (ctx, g_strdup_printf ("Invalid table 0x%x for token 0x%x for ldtoken at 0x%04x", mono_metadata_token_table (token), token, ctx->ip_offset));
+		return;
+	}
+
 	handle = mono_ldtoken (ctx->image, token, &handle_class, ctx->generic_context);
 	if (!handle) {
 		ADD_VERIFY_ERROR (ctx, g_strdup_printf ("Invalid token 0x%x for ldtoken at 0x%04x", token, ctx->ip_offset));
