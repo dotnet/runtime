0d808dc8024a94e0780761a7fabccd77c121203a
Rodrigo Kumpera <kumpera@gmail.com>
2010-08-04 16:30:45 -0300

    Improve error message for signature load failure.
    	* loader.c (mono_method_signature): Print type and
    	method name in case of signature parsing error.
    
    
    Commit migrated from https://github.com/mono/mono/commit/2bc36b61df69eccb90554c85ee5bb0f631961034

--- src/mono/mono/metadata/loader.c
+++ src/mono/mono/metadata/loader.c
@@ -2277,7 +2277,9 @@ mono_method_signature (MonoMethod *m)
 
 	sig = mono_method_signature_checked (m, &error);
 	if (!sig) {
-		g_warning ("Could not load signature due to: %s", mono_error_get_message (&error));
+		char *type_name = mono_type_get_full_name (m->klass);
+		g_warning ("Could not load signature of %s:%s due to: %s", type_name, m->name, mono_error_get_message (&error));
+		g_free (type_name);
 		mono_error_cleanup (&error);
 	}
 
