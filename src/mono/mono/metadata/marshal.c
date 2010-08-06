b21b8265169f11d154908c94182bafaafeafc9bc
Jb Evain <jbevain@gmail.com>
2010-08-06 18:23:49 +0200

    [fix] mono/mono#475962: delegates to instance methods closed over null
    * marshal.c (mono_marshal_get_delegate_invoke): add support for
    delegates pointing to instance methods that are closed over null.
    Managed side of the feature is in 2ec0ab59fe4aecf5507a81846a66de.
    
    
    Commit migrated from https://github.com/mono/mono/commit/81d23f19f756f083d8203e1c8d60a1caf51ddc47

--- src/mono/mono/metadata/marshal.c
+++ src/mono/mono/metadata/marshal.c
@@ -3765,6 +3765,7 @@ mono_marshal_get_delegate_invoke (MonoMethod *method, MonoDelegate *del)
 	MonoMethod *target_method = NULL;
 	MonoClass *target_class = NULL;
 	gboolean callvirt = FALSE;
+	gboolean closed_over_null = FALSE;
 
 	/*
 	 * If the delegate target is null, and the target method is not static, a virtual 
@@ -3790,6 +3791,9 @@ mono_marshal_get_delegate_invoke (MonoMethod *method, MonoDelegate *del)
 		  !strcmp (method->name, "Invoke"));
 		
 	sig = mono_signature_no_pinvoke (method);
+
+	if (callvirt)
+		closed_over_null = sig->param_count == mono_method_signature (del->method)->param_count;
 
 	if (callvirt) {
 		/* We need to cache the signature+method pair */
@@ -3888,11 +3892,18 @@ mono_marshal_get_delegate_invoke (MonoMethod *method, MonoDelegate *del)
 	mono_mb_patch_branch (mb, pos0);
 
 	if (callvirt) {
-		mono_mb_emit_ldarg (mb, 1);
-		mono_mb_emit_op (mb, CEE_CASTCLASS, target_class);
-		for (i = 1; i < sig->param_count; ++i)
-			mono_mb_emit_ldarg (mb, i + 1);
-		mono_mb_emit_op (mb, CEE_CALLVIRT, target_method);
+		if (!closed_over_null) {
+			mono_mb_emit_ldarg (mb, 1);
+			mono_mb_emit_op (mb, CEE_CASTCLASS, target_class);
+			for (i = 1; i < sig->param_count; ++i)
+				mono_mb_emit_ldarg (mb, i + 1);
+			mono_mb_emit_op (mb, CEE_CALLVIRT, target_method);
+		} else {
+			mono_mb_emit_byte (mb, CEE_LDNULL);
+			for (i = 0; i < sig->param_count; ++i)
+				mono_mb_emit_ldarg (mb, i + 1);
+			mono_mb_emit_op (mb, CEE_CALL, target_method);
+		}
 	} else {
 		for (i = 0; i < sig->param_count; ++i)
 			mono_mb_emit_ldarg (mb, i + 1);
