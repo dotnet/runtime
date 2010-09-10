1560b4c2cc15c5b69d15f7399ede8e3633c316f7
Miguel de Icaza <miguel@gnome.org>
2010-09-10 17:01:48 -0400

    [Embedded] Add new fallback API to allow hooking up to the P/Invoke lookup system
    This patch allows a developer to register a fallback for P/Invoke when
    embedding Mono.   Typically this is done when you statically link all of your
    libraries in some embedded systems and you need to dynamically resolve symbol
    names.
    
    There is a *sample* API included (but not compiled into Mono itself) called
    mono-embed.c that shows a layman use of the API.   A full implementation would
    mandate sorted tables, and would use a bsearch or a hashtable instead of having
    linear searches.
    
    
    Commit migrated from https://github.com/mono/mono/commit/6df4b5ac5e19997ddf1d3183ec777977186c5560

--- src/mono/mono/utils/mono-dl.h
+++ src/mono/mono/utils/mono-dl.h
@@ -2,12 +2,7 @@
 #define __MONO_UTILS_DL_H__
 
 #include "mono/utils/mono-compiler.h"
-
-enum {
-	MONO_DL_LAZY  = 1,
-	MONO_DL_LOCAL = 2,
-	MONO_DL_MASK  = 3
-};
+#include "mono/utils/mono-dl-fallback.h"
 
 typedef struct _MonoDl MonoDl;
 
@@ -16,7 +11,6 @@ char*       mono_dl_symbol     (MonoDl *module, const char *name, void **symbol)
 void        mono_dl_close      (MonoDl *module) MONO_INTERNAL;
 
 char*       mono_dl_build_path (const char *directory, const char *name, void **iter) MONO_INTERNAL;
-
 
 #endif /* __MONO_UTILS_DL_H__ */
 
