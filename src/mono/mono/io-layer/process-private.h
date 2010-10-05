0336968ee5882855eca02ad7fe253a6b7ec52aa9
Mikayla Hutchinson <mhutchinson@novell.com>
2010-10-05 13:38:52 -0400

    [io-layer] Cope with pids > 16 bits
    Fixes an issue in Process where "pseudo-handles" for processes with
    pids > 16 bits would overflow and start getting treated as normal
    handles. This would frequently segfault MonoDevelop on MacOS.
    
    
    Commit migrated from https://github.com/mono/mono/commit/1084c49a948692899720b39ac204a6501ed8b3e2

--- src/mono/mono/io-layer/process-private.h
+++ src/mono/mono/io-layer/process-private.h
@@ -17,12 +17,8 @@
 #define _WAPI_PROCESS_CURRENT (gpointer)0xFFFFFFFF
 
 /* This marks a system process that we don't have a handle on */
-/* FIXME: cope with pids > 16bit */
-#if defined(__HAIKU__)
+/* FIXME: cope with pids > 31bit? */
 #define _WAPI_PROCESS_UNHANDLED_PID_MASK 0x7FFFFFFF
-#else
-#define _WAPI_PROCESS_UNHANDLED_PID_MASK 0x7FFF
-#endif
 #define _WAPI_PROCESS_UNHANDLED (-1 & ~_WAPI_PROCESS_UNHANDLED_PID_MASK)
 
 extern gpointer _wapi_process_duplicate (void);
