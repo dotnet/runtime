4e611076ef6d56f23b1bf8f25053a957848db0b3
Geoff Norton <grompf@sublimeintervention.com>
2010-10-05 15:58:09 -0400

    Fix the build on windows
    Commit migrated from https://github.com/mono/mono/commit/2b638808bf4e24c8f46643038fed8cb95ff46045

--- src/mono/mono/metadata/process.c
+++ src/mono/mono/metadata/process.c
@@ -65,7 +65,11 @@ void ves_icall_System_Diagnostics_Process_Process_free_internal (MonoObject *thi
 	g_message ("%s: Closing process %p, handle %p", __func__, this, process);
 #endif
 
+#if TARGET_WIN32
+	CloseHandle (process);
+#else
 	CloseProcess (process);
+#endif
 }
 
 #define STASH_SYS_ASS(this) \
