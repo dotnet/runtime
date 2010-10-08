5848ff146eac83ae9a98b782d37cfa3640d62e40
Zoltan Varga <vargaz@gmail.com>
2010-10-08 11:47:13 +0200

    Fix a warning.
    Commit migrated from https://github.com/mono/mono/commit/c919b7fa72d934e6f6fcd291b1b630d109165f9f

--- src/mono/eglib/src/goutput.c
+++ src/mono/eglib/src/goutput.c
@@ -24,6 +24,9 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <glib.h>
