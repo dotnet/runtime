6caeb9c646c5fb76f7ff10e1c4be454c64bb7d8d
Zoltan Varga <vargaz@gmail.com>
2010-10-23 23:41:56 +0200

    Extract the default profiler into its own file.
    Commit migrated from https://github.com/mono/mono/commit/fd036a2b56515e6ac6c5026b3874e776aeb1ebed

---
+++ src/mono/mono/metadata/profiler-default.h
@@ -0,0 +1,9 @@
+#ifndef __MONO_PROFILER_DEFAULT_H__
+#define __MONO_PROFILER_DEFAULT_H__
+
+#include "mono/utils/mono-compiler.h"
+
+void
+mono_profiler_install_simple (const char *desc) MONO_INTERNAL;
+
+#endif
