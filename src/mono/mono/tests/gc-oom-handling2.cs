948ae7244b23516a725549fc11faf7c43d798968
Rodrigo Kumpera <kumpera@gmail.com>
2010-11-05 20:01:53 -0200

    Add test for string interning OOM.
    Commit migrated from https://github.com/mono/mono/commit/b5e3da7e58d948d1d6473a517e1ebf54459518b8

--- src/mono/mono/tests/gc-oom-handling2.cs
+++ src/mono/mono/tests/gc-oom-handling2.cs
@@ -2,18 +2,27 @@ using System;
 using System.Collections.Generic;
 
 class Driver {
+	static void DumpStuff () {
+		Console.WriteLine ("CWL under OOM - should not print {0}", 99);
+		Console.WriteLine ("CWL under OOM - should not print {0}{1}", 22, 44.4);
+	}
+
 	static int Main () {
 		Console.WriteLine ("start");
 		var r = new Random (123456);
 		var l = new List<object> ();
 		try {
-			for (int i = 0; i < 40000; ++i) {
+			for (int i = 0; i < 400000; ++i) {
 				var foo = new byte[r.Next () % 4000];
 				l.Add (foo);
 			}
 			Console.WriteLine ("done");
 			return -1;
-		} catch (Exception e) {
+		} catch (Exception) {
+			try {
+				DumpStuff ();
+			} catch (Exception) {}
+
 			l.Clear ();
 			l = null;
 			Console.WriteLine ("OOM done");
