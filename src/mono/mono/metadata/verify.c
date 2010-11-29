d4cf952891b16e840b8f3dd1a7a85f2b01027695
Rodrigo Kumpera <kumpera@gmail.com>
2010-11-29 19:58:08 -0200

    Don't allow zero index tokens on IL streams.
    	* verify.c (token_bounds_check): Check if token index is zero.
    
    	Fixes mono/mono#651682
    
    
    Commit migrated from https://github.com/mono/mono/commit/698a6247565bc8dc5a38cc21f7084cb5a33bfda1

--- src/mono/mono/metadata/verify.c
+++ src/mono/mono/metadata/verify.c
@@ -366,7 +366,7 @@ token_bounds_check (MonoImage *image, guint32 token)
 {
 	if (image->dynamic)
 		return mono_reflection_is_valid_dynamic_token ((MonoDynamicImage*)image, token);
-	return image->tables [mono_metadata_token_table (token)].rows >= mono_metadata_token_index (token);
+	return image->tables [mono_metadata_token_table (token)].rows >= mono_metadata_token_index (token) && mono_metadata_token_index (token) > 0;
 }
 
 static MonoType *
