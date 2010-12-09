bb1d9b088f0cf1761e8fc7674662ad1420989cae
Rodrigo Kumpera <kumpera@gmail.com>
2010-12-09 17:59:31 -0200

    Add a few more undocumented bits to allowed AssemblyFlags
    	Fixes mono/mono#657516
    
    
    Commit migrated from https://github.com/mono/mono/commit/ed8e4e2cb9139d10c611f83e23b702585ea9c08a

--- src/mono/mono/metadata/metadata-verify.c
+++ src/mono/mono/metadata/metadata-verify.c
@@ -3369,7 +3369,7 @@ verify_fieldrva_table (VerifyContext *ctx)
 	}
 }
 
-#define INVALID_ASSEMBLY_FLAGS_BITS ~((1 << 0) | (1 << 4) | (1 << 8) | (1 << 14) | (1 << 15))
+#define INVALID_ASSEMBLY_FLAGS_BITS ~((1 << 0) | (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7) | (1 << 8) | (1 << 14) | (1 << 15))
 static void
 verify_assembly_table (VerifyContext *ctx)
 {
