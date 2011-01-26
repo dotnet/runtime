57339c63f1556d5472ce2ec164fd4adf07aaf99c
Geoff Norton <grompf@sublimeintervention.com>
2011-01-26 12:11:30 -0500

    Ensure we properly align the stack before resuming from breakpoint signal handlers
    Commit migrated from https://github.com/mono/mono/commit/567512bd8f44bc0a17642c3ed26a59d32bd3fda6

--- src/mono/mono/mini/exceptions-x86.c
+++ src/mono/mono/mini/exceptions-x86.c
@@ -1295,3 +1295,18 @@ mono_tasklets_arch_restore (void)
 }
 #endif
 
+/*
+ * mono_arch_setup_resume_sighandler_ctx:
+ *
+ *   Setup CTX so execution continues at FUNC.
+ */
+void
+mono_arch_setup_resume_sighandler_ctx (MonoContext *ctx, gpointer func)
+{
+	int align = (((gint32)MONO_CONTEXT_GET_SP (ctx)) % MONO_ARCH_FRAME_ALIGNMENT + 4);
+
+	if (align != 0)
+		MONO_CONTEXT_SET_SP (ctx, MONO_CONTEXT_GET_SP (ctx) - align);
+
+	MONO_CONTEXT_SET_IP (ctx, func);
+}
