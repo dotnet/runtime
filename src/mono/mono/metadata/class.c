b6969628f811ff41e5d2ab9fbe5bdc00a771ceac
Rodrigo Kumpera <kumpera@gmail.com>
2011-02-09 16:30:05 +0100

    Handle assignment check of variant type and its gtd.
    	* clas.c (mono_class_is_variant_compatible): Return
    	FALSE if checking a variant type and its gtd instead
    	of crashing.
    
    	Fixes mono/mono#668506
    
    
    Commit migrated from https://github.com/mono/mono/commit/bae9eb37ca65d42cb6ca5509343f8b0d93f2107a

--- src/mono/mono/metadata/class.c
+++ src/mono/mono/metadata/class.c
@@ -7201,10 +7201,11 @@ mono_class_is_variant_compatible (MonoClass *klass, MonoClass *oklass)
 	int j;
 	MonoType **klass_argv, **oklass_argv;
 	MonoClass *klass_gtd = mono_class_get_generic_type_definition (klass);
+	MonoClass *oklass_gtd = mono_class_get_generic_type_definition (oklass);
 	MonoGenericContainer *container = klass_gtd->generic_container;
 
 	/*Viable candidates are instances of the same generic interface*/
-	if (mono_class_get_generic_type_definition (oklass) != klass_gtd)
+	if (mono_class_get_generic_type_definition (oklass) != klass_gtd || oklass == klass_gtd)
 		return FALSE;
 
 	klass_argv = &klass->generic_class->context.class_inst->type_argv [0];
@@ -7344,7 +7345,7 @@ mono_class_is_variant_compatible_slow (MonoClass *klass, MonoClass *oklass)
 	MonoGenericContainer *container = klass_gtd->generic_container;
 
 	/*Viable candidates are instances of the same generic interface*/
-	if (mono_class_get_generic_type_definition (oklass) != klass_gtd)
+	if (mono_class_get_generic_type_definition (oklass) != klass_gtd || oklass == klass_gtd)
 		return FALSE;
 
 	klass_argv = &klass->generic_class->context.class_inst->type_argv [0];
