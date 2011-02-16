865481164c4c1935c08a9f0f19ca445e2a88aa14
Gonzalo Paniagua Javier <gonzalo.mono@gmail.com>
2011-02-15 19:58:08 -0500

    Remove a bunch of warnings.
    Commit migrated from https://github.com/mono/mono/commit/59fcb4f8d3636af1844a79c5bb4771da2c984fe9

--- src/mono/mono/metadata/sgen-gc.h
+++ src/mono/mono/metadata/sgen-gc.h
@@ -776,6 +776,7 @@ gboolean mono_sgen_object_is_live (void *obj) MONO_INTERNAL;
 
 gboolean mono_sgen_need_bridge_processing (void) MONO_INTERNAL;
 void mono_sgen_bridge_processing (int num_objs, MonoObject **objs) MONO_INTERNAL;
+void mono_sgen_register_test_bridge_callbacks (void) MONO_INTERNAL;
 
 enum {
 	SPACE_MAJOR,
