2e06a3f2d4c62efd9a1e0249cf52d9e39606d296
Mark Sciabica <msciabica@itracs.com>
2011-03-18 17:59:28 +0100

    Fix exception handling on windows x64.
    Commit migrated from https://github.com/mono/mono/commit/e52cd5993b181f8046bb6b56635ac80d682c921a

--- src/mono/mono/mini/exceptions-amd64.c
+++ src/mono/mono/mini/exceptions-amd64.c
@@ -420,13 +420,19 @@ get_throw_trampoline (MonoTrampInfo **info, gboolean rethrow, gboolean corlib, g
 	guint8 *code;
 	MonoJumpInfo *ji = NULL;
 	GSList *unwind_ops = NULL;
-	int i, stack_size, arg_offsets [16], regs_offset;
+	int i, stack_size, arg_offsets [16], regs_offset, dummy_stack_space;
 	const guint kMaxCodeSize = NACL_SIZE (256, 512);
+
+#ifdef TARGET_WIN32
+	dummy_stack_space = 6 * sizeof(mgreg_t);	/* Windows expects stack space allocated for all 6 dummy args. */
+#else
+	dummy_stack_space = 0;
+#endif
 
 	start = code = mono_global_codeman_reserve (kMaxCodeSize);
 
 	/* The stack is unaligned on entry */
-	stack_size = 192 + 8;
+	stack_size = 192 + 8 + dummy_stack_space;
 
 	code = start;
 
@@ -443,11 +449,11 @@ get_throw_trampoline (MonoTrampInfo **info, gboolean rethrow, gboolean corlib, g
 	 * the stack by passing 6 dummy values in registers.
 	 */
 
-	arg_offsets [0] = 0;
-	arg_offsets [1] = sizeof(mgreg_t);
-	arg_offsets [2] = sizeof(mgreg_t) * 2;
-	arg_offsets [3] = sizeof(mgreg_t) * 3;
-	regs_offset = sizeof(mgreg_t) * 4;
+	arg_offsets [0] = dummy_stack_space + 0;
+	arg_offsets [1] = dummy_stack_space + sizeof(mgreg_t);
+	arg_offsets [2] = dummy_stack_space + sizeof(mgreg_t) * 2;
+	arg_offsets [3] = dummy_stack_space + sizeof(mgreg_t) * 3;
+	regs_offset = dummy_stack_space + sizeof(mgreg_t) * 4;
 
 	/* Save registers */
 	for (i = 0; i < AMD64_NREG; ++i)
