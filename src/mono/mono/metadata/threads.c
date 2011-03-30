318719ffb5896d8f28449d977f5f31ae0ad0a3a4
Zoltan Varga <vargaz@gmail.com>
2011-03-30 05:05:06 +0200

    Call SleepEx in a loop in the Thread.Sleep () icall, to avoid a race when the sleep is interrupted. Fixes mono/mono#683519.
    Commit migrated from https://github.com/mono/mono/commit/ae6588e9650acadd92aca87822ef52df7f6e5d36

--- src/mono/mono/metadata/threads.c
+++ src/mono/mono/metadata/threads.c
@@ -1076,15 +1076,23 @@ void ves_icall_System_Threading_Thread_Sleep_internal(gint32 ms)
 
 	mono_thread_current_check_pending_interrupt ();
 	
-	mono_thread_set_state (thread, ThreadState_WaitSleepJoin);
+	while (TRUE) {
+		mono_thread_set_state (thread, ThreadState_WaitSleepJoin);
 	
-	res = SleepEx(ms,TRUE);
+		res = SleepEx(ms,TRUE);
 	
-	mono_thread_clr_state (thread, ThreadState_WaitSleepJoin);
+		mono_thread_clr_state (thread, ThreadState_WaitSleepJoin);
 
-	if (res == WAIT_IO_COMPLETION) { /* we might have been interrupted */
-		MonoException* exc = mono_thread_execute_interruption (thread);
-		if (exc) mono_raise_exception (exc);
+		if (res == WAIT_IO_COMPLETION) { /* we might have been interrupted */
+			MonoException* exc = mono_thread_execute_interruption (thread);
+			if (exc) {
+				mono_raise_exception (exc);
+			} else {
+				// FIXME: !INFINITE
+				if (ms != INFINITE)
+					break;
+			}
+		}
 	}
 }
 
