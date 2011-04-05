d5ea0167f47332ab512a2ceb5e3569bd018eed79
Rodrigo Kumpera <kumpera@gmail.com>
2011-04-04 23:05:45 -0300

    Add implementation of a lock free linked list set.
    	* mono-linked-list-set.c: Implementation based
    	on M. Michael paper.
    
    
    Commit migrated from https://github.com/mono/mono/commit/de47c136db9c5128f0ace6aa6eea1c4eb5226526

---
+++ src/mono/mono/utils/mono-linked-list-set.c
@@ -0,0 +1,168 @@
+/*
+ * mono-split-ordered-list.c: A lock-free split ordered list.
+ *
+ * Author:
+ *	Rodrigo Kumpera (kumpera@gmail.com)
+ *
+ * (C) 2011 Novell, Inc
+ *
+ * This is an implementation of Maged Michael's lock-free linked-list set.
+ * For more details see:
+ *	"High Performance Dynamic Lock-Free Hash Tables and List-Based Sets"
+ *  Maged M. Michael 2002
+ *
+ *  http://www.research.ibm.com/people/m/michael/spaa-2002.pdf
+ */
+
+#include <mono/utils/mono-linked-list-set.h>
+
+/*atomics.*/
+#include <mono/io-layer/io-layer.h>
+
+static inline gpointer
+mask (gpointer n, uintptr_t bit)
+{
+	return (gpointer)(((uintptr_t)n) | bit);
+}
+
+gpointer
+get_hazardous_pointer_with_mask (gpointer volatile *pp, MonoThreadHazardPointers *hp, int hazard_index)
+{
+	gpointer p;
+
+	for (;;) {
+		/* Get the pointer */
+		p = *pp;
+		/* If we don't have hazard pointers just return the
+		   pointer. */
+		if (!hp)
+			return p;
+		/* Make it hazardous */
+		mono_hazard_pointer_set (hp, hazard_index, mono_lls_pointer_unmask (p));
+		/* Check that it's still the same.  If not, try
+		   again. */
+		if (*pp != p) {
+			mono_hazard_pointer_clear (hp, hazard_index);
+			continue;
+		}
+		break;
+	}
+
+	return p;
+}
+
+/*
+Initialize @list and will use @free_node_func to release memory.
+*/
+void
+mono_lls_init (MonoLinkedListSet *list, void (*free_node_func)(void *))
+{
+	list->head = NULL;
+	list->free_node_func = free_node_func;
+}
+
+/*
+Search @list for element with key @key.
+The nodes next, cur and prev are returned in @hp.
+Returns true if a node with key @key was found.
+This function cannot be called from a signal nor within interrupt context*.
+XXX A variant that works within interrupted is possible if needed.
+
+* interrupt context is when the current thread is reposible for another thread
+been suspended at an arbritary point. This is a limitation of our SMR implementation.
+*/
+gboolean
+mono_lls_find (MonoLinkedListSet *list, MonoThreadHazardPointers *hp, uintptr_t key)
+{
+	MonoLinkedListSetNode *cur, *next;
+	MonoLinkedListSetNode **prev;
+	uintptr_t cur_key;
+
+try_again:
+	prev = &list->head;
+	mono_hazard_pointer_set (hp, 2, prev);
+
+	cur = mono_lls_pointer_unmask (get_hazardous_pointer ((gpointer*)prev, hp, 1));
+
+	while (1) {
+		if (cur == NULL)
+			return FALSE;
+		next = get_hazardous_pointer_with_mask ((gpointer*)&cur->next, hp, 0);
+		cur_key = cur->key;
+
+		if (*prev != cur)
+			goto try_again;
+
+		if (!mono_lls_pointer_get_mark (next)) {
+			if (cur_key >= key)
+				return cur_key == key;
+
+			prev = &cur->next;
+			mono_hazard_pointer_set (hp, 2, cur);
+		} else {
+			next = mono_lls_pointer_unmask (next);
+			if (InterlockedCompareExchangePointer ((volatile gpointer*)prev, next, cur) == next)
+				mono_thread_hazardous_free_or_queue (cur, list->free_node_func);
+			else
+				goto try_again;
+		}
+		cur = mono_lls_pointer_unmask (next);
+		mono_hazard_pointer_set (hp, 1, cur);
+	}
+}
+
+/*
+Insert @value into @list.
+The nodes value, cur and prev are returned in @hp.
+Return true if @value was inserted by this call. If it returns FALSE, it's the caller
+resposibility to release memory.
+This function cannot be called from a signal nor with the world stopped.
+*/
+gboolean
+mono_lls_insert (MonoLinkedListSet *list, MonoThreadHazardPointers *hp, MonoLinkedListSetNode *value)
+{
+	MonoLinkedListSetNode *cur, **prev;
+	/*We must do a store barrier before inserting 
+	to make sure all values in @node are globally visible.*/
+	mono_memory_barrier ();
+
+	while (1) {
+		if (mono_lls_find (list, hp, value->key))
+			return FALSE;
+		cur = mono_hazard_pointer_get_val (hp, 1);
+		prev = mono_hazard_pointer_get_val (hp, 2);
+
+		value->next = cur;
+		mono_hazard_pointer_set (hp, 0, value);
+		if (InterlockedCompareExchangePointer ((volatile gpointer*)prev, value, cur) == cur)
+			return TRUE;
+	}
+}
+
+/*
+Search @list for element with key @key.
+The nodes next, cur and prev are returned in @hp
+Returns true if @value was removed by this call.
+This function cannot be called from a signal nor with the world stopped.
+*/
+gboolean
+mono_lls_remove (MonoLinkedListSet *list, MonoThreadHazardPointers *hp, MonoLinkedListSetNode *value)
+{
+	MonoLinkedListSetNode *cur, **prev, *next;
+	while (1) {
+		if (!mono_lls_find (list, hp, value->key))
+			return FALSE;
+
+		next = mono_hazard_pointer_get_val (hp, 0);
+		cur = mono_hazard_pointer_get_val (hp, 1);
+		prev = mono_hazard_pointer_get_val (hp, 2);
+
+		if (InterlockedCompareExchangePointer ((volatile gpointer*)&cur->next, mask (next, 1), next) != next)
+			continue;
+		if (InterlockedCompareExchangePointer ((volatile gpointer*)prev, next, cur) == cur)
+			mono_thread_hazardous_free_or_queue (value, list->free_node_func);
+		else
+			mono_lls_find (list, hp, value->key);
+		return TRUE;
+	}
+}
---
+++ src/mono/mono/utils/mono-linked-list-set.h
@@ -0,0 +1,99 @@
+/*
+ * mono-linked-list-set.h: A lock-free split ordered list.
+ *
+ * Author:
+ *	Rodrigo Kumpera (kumpera@gmail.com)
+ *
+ * (C) 2011 Novell, Inc
+ */
+
+#ifndef __MONO_SPLIT_ORDERED_LIST_H__
+#define __MONO_SPLIT_ORDERED_LIST_H__
+
+#include <mono/utils/hazard-pointer.h>
+
+typedef struct _MonoLinkedListSetNode MonoLinkedListSetNode;
+
+struct _MonoLinkedListSetNode {
+	MonoLinkedListSetNode *next;
+	uintptr_t key;
+};
+
+typedef struct {
+	MonoLinkedListSetNode *head;
+	void (*free_node_func)(void *);
+} MonoLinkedListSet;
+
+
+static inline gpointer
+mono_lls_pointer_unmask (gpointer p)
+{
+	return (gpointer)((uintptr_t)p & ~(uintptr_t)0x3);
+}
+
+static inline uintptr_t
+mono_lls_pointer_get_mark (gpointer n)
+{
+	return (uintptr_t)n & 0x1;
+}
+
+/*
+Those are low level operations. prev, cur, next are returned in the hazard pointer table.
+You must manually clean the hazard pointer table after using them.
+*/
+
+void
+mono_lls_init (MonoLinkedListSet *list, void (*free_node_func)(void *));
+
+gboolean
+mono_lls_find (MonoLinkedListSet *list, MonoThreadHazardPointers *hp, uintptr_t key) MONO_INTERNAL;
+
+gboolean
+mono_lls_insert (MonoLinkedListSet *list, MonoThreadHazardPointers *hp, MonoLinkedListSetNode *value) MONO_INTERNAL;
+
+gboolean
+mono_lls_remove (MonoLinkedListSet *list, MonoThreadHazardPointers *hp, MonoLinkedListSetNode *value) MONO_INTERNAL;
+
+gpointer
+get_hazardous_pointer_with_mask (gpointer volatile *pp, MonoThreadHazardPointers *hp, int hazard_index) MONO_INTERNAL;
+
+/*
+Requires the world to be stoped
+*/
+#define MONO_LLS_FOREACH(list, element) {\
+	MonoLinkedListSetNode *__cur;	\
+	for (__cur = list->head; __cur; __cur = mono_lls_pointer_unmask (__cur->next))	\
+		if (!mono_lls_pointer_get_mark (__cur->next)) {	\
+			element = (typeof(element))__cur;	\
+
+#define MONO_LLS_END_FOREACH }}
+
+static inline MonoLinkedListSetNode*
+mono_lls_info_step (MonoLinkedListSetNode *val, MonoThreadHazardPointers *hp)
+{
+	val = mono_lls_pointer_unmask (val);
+	mono_hazard_pointer_set (hp, 1, val);
+	return val;
+}
+
+/*
+Provides snapshot iteration
+*/
+#define MONO_LLS_FOREACH_SAFE(list, element) {\
+	MonoThreadHazardPointers *__hp = mono_hazard_pointer_get ();	\
+	MonoLinkedListSetNode *__cur, *__next;	\
+	for (__cur = mono_lls_pointer_unmask (get_hazardous_pointer ((gpointer volatile*)&list->head, __hp, 1)); \
+		__cur;	\
+		__cur = mono_lls_info_step (__next, __hp)) {	\
+		__next = get_hazardous_pointer_with_mask ((gpointer volatile*)&__cur->next, __hp, 0);	\
+		if (!mono_lls_pointer_get_mark (__next)) {	\
+			element = (typeof(element))__cur;
+
+#define MONO_LLS_END_FOREACH_SAFE \
+		} \
+	}	\
+	mono_hazard_pointer_clear (__hp, 0); \
+	mono_hazard_pointer_clear (__hp, 1); \
+}
+
+#endif /* __MONO_SPLIT_ORDERED_LIST_H__ */
