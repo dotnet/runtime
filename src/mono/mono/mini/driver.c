677833e086fbc3f5d6b99eec1b0f121e5c8b13e4
Zoltan Varga <vargaz@gmail.com>
2011-04-07 01:29:49 +0200

    Disable SMP on arm until mono/mono#683409 is fixed.
    Commit migrated from https://github.com/mono/mono/commit/29550902eec1ab9dca1e64f51829b1032577748c

--- src/mono/mono/mini/driver.c
+++ src/mono/mono/mini/driver.c
@@ -1363,6 +1363,11 @@ mono_main (int argc, char* argv[])
 
 	if (getenv ("MONO_NO_SMP"))
 		mono_set_use_smp (FALSE);
+
+#ifdef TARGET_ARM
+	// #683409
+	mono_set_use_smp (FALSE);
+#endif
 	
 	if (!g_thread_supported ())
 		g_thread_init (NULL);
