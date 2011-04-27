0728bbddf143ea6b41d7de75fb7934a5ecbdf276
Jeffrey Stedfast <fejj@gnome.org>
2011-04-27 09:12:47 -0400

    Silence gcc warning about vasprintf()'s retval not being used
    Commit migrated from https://github.com/mono/mono/commit/54cb2ebe9d38e1c9f1dd1512f72988c8a2295c3f

--- src/mono/eglib/src/goutput.c
+++ src/mono/eglib/src/goutput.c
@@ -113,7 +113,9 @@ g_logv (const gchar *log_domain, GLogLevelFlags log_level, const gchar *format, 
 {
 	char *msg;
 	
-	vasprintf (&msg, format, args);
+	if (vasprintf (&msg, format, args) < 0)
+		return;
+	
 #if PLATFORM_ANDROID
 	__android_log_print (to_android_priority (log_level), 
 		/* TODO: provide a proper app name */
