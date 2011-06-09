2261516f042bb1a422d0422688e3b8a3312106cc
Sebastien Pouliot <sebastien@xamarin.com>
2011-06-09 10:30:07 -0400

    Refactor MonoMac's MarkNSObjects substep so it can be reused in other products
    Commit migrated from https://github.com/dotnet/linker/commit/cd93001f91141a6d8d9ca62d007fc6dcf695d1da

--- src/tools/linker/tuner/MonoMac.Tuner/MarkNSObjects.cs
+++ src/tools/linker/tuner/MonoMac.Tuner/MarkNSObjects.cs
@@ -1,10 +1,12 @@
 //
-// MarkNSObbjects.cs
+// MarkNSObjects.cs
 //
-// Author:
-//   Jb Evain (jbevain@novell.com)
+// Authors:
+//	Jb Evain (jbevain@novell.com)
+//	Sebastien Pouliot  <sebastien@xamarin.com>
 //
 // (C) 2009 Novell, Inc.
+// Copyright (C) 2011 Xamarin, Inc.
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
@@ -27,12 +29,6 @@
 //
 
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-
-using Mono.Linker;
-using Mono.Linker.Steps;
 
 using Mono.Tuner;
 
@@ -40,13 +36,10 @@ using Mono.Cecil;
 
 namespace MonoMac.Tuner {
 
-	public class MarkNSObjects : BaseSubStep {
+	public class MarkNSObjects : MarkNSObjectsBase {
 
-		const string ExportAttribute = "MonoMac.Foundation.ExportAttribute";
-		const string MonoMacAssembly = "MonoMac";
-
-		public override SubStepTargets Targets {
-			get { return SubStepTargets.Type; }
+		protected override string ExportAttribute {
+			get { return "MonoMac.Foundation.ExportAttribute"; }
 		}
 
 		public override void ProcessType (TypeDefinition type)
@@ -54,97 +47,7 @@ namespace MonoMac.Tuner {
 			if (!type.IsNSObject () || !type.IsNativeObject ())
 				return;
 
-			if (!IsMonoMacType (type)) {
-				Annotations.Mark (type);
-				Annotations.SetPreserve (type, TypePreserve.All);
-			} else
-				PreserveMonoMacType (type);
-		}
-
-		void PreserveMonoMacType (TypeDefinition type)
-		{
-			PreserveIntPtrConstructor (type);
-			PreserveExportedMethods (type);
-		}
-
-		void PreserveExportedMethods (TypeDefinition type)
-		{
-			if (!type.HasMethods)
-				return;
-
-			foreach (var method in type.GetMethods ()) {
-				if (!IsExportedMethod (method))
-					continue;
-
-				if (!IsOverridenInUserCode (method))
-					continue;
-
-				PreserveMethod (type, method);
-			}
-		}
-
-		bool IsOverridenInUserCode (MethodDefinition method)
-		{
-			if (!method.IsVirtual)
-				return false;
-
-			var overrides = Annotations.GetOverrides (method);
-			if (overrides == null || overrides.Count == 0)
-				return false;
-
-			foreach (MethodDefinition @override in overrides)
-				if (!IsMonoMacMethod (@override))
-					return true;
-
-			return false;
-		}
-
-		static bool IsExportedMethod (MethodDefinition method)
-		{
-			return HasExportAttribute (method);
-		}
-
-		static bool HasExportAttribute (ICustomAttributeProvider provider)
-		{
-			if (!provider.HasCustomAttributes)
-				return false;
-
-			foreach (CustomAttribute attribute in provider.CustomAttributes)
-				if (attribute.AttributeType.FullName == ExportAttribute)
-					return true;
-
-			return false;
-		}
-
-		void PreserveIntPtrConstructor (TypeDefinition type)
-		{
-			if (!type.HasMethods)
-				return;
-
-			foreach (MethodDefinition constructor in type.GetConstructors ()) {
-				if (!constructor.HasParameters)
-					continue;
-
-				if (constructor.Parameters.Count != 1 || constructor.Parameters [0].ParameterType.FullName != "System.IntPtr")
-					continue;
-
-				PreserveMethod (type, constructor);
-			}
-		}
-
-		void PreserveMethod (TypeDefinition type, MethodDefinition method)
-		{
-			Annotations.AddPreservedMethod (type, method);
-		}
-
-		static bool IsMonoMacMethod (MethodDefinition method)
-		{
-			return IsMonoMacType (method.DeclaringType);
-		}
-
-		static bool IsMonoMacType (TypeDefinition type)
-		{
-			return type.Module.Assembly.Name.Name == MonoMacAssembly;
+			base.ProcessType (type);
 		}
 	}
 }
