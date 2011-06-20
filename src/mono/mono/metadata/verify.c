2e56522867673515be2dd3e396bff1a138e028f5
Rodrigo Kumpera <kumpera@gmail.com>
2011-06-20 12:22:32 -0300

    Cleanup code a bit by reducing spurious nesting.
    Commit migrated from https://github.com/mono/mono/commit/0e2adfb0a2bbb14e74d43c27c8e85752b8d69e3d

--- src/mono/mono/metadata/verify.c
+++ src/mono/mono/metadata/verify.c
@@ -2831,19 +2831,21 @@ store_local (VerifyContext *ctx, guint32 arg)
 	}
 
 	/*TODO verify definite assigment */		
-	if (check_underflow (ctx, 1)) {
-		value = stack_pop(ctx);
-		if (!verify_stack_type_compatibility (ctx, ctx->locals [arg], value)) {
-			char *expected = mono_type_full_name (ctx->locals [arg]);
-			char *found = stack_slot_full_name (value);
-			CODE_NOT_VERIFIABLE (ctx, g_strdup_printf ("Incompatible type '%s' on stack cannot be stored to local %d with type '%s' at 0x%04x",
-					found,
-					arg,
-					expected,
-					ctx->ip_offset));
-			g_free (expected);
-			g_free (found);	
-		}
+	if (!check_underflow (ctx, 1))
+		return;
+
+	value = stack_pop (ctx);
+
+	if (!verify_stack_type_compatibility (ctx, ctx->locals [arg], value)) {
+		char *expected = mono_type_full_name (ctx->locals [arg]);
+		char *found = stack_slot_full_name (value);
+		CODE_NOT_VERIFIABLE (ctx, g_strdup_printf ("Incompatible type '%s' on stack cannot be stored to local %d with type '%s' at 0x%04x",
+				found,
+				arg,
+				expected,
+				ctx->ip_offset));
+		g_free (expected);
+		g_free (found);	
 	}
 }
 
