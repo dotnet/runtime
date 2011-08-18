8e4383c10d8e2bd4a602f6e7a0a98f928b39673a
Tak <levi@unity3d.com>
2011-08-18 13:13:50 +0200

    NULL global interface bitset on deletion. Fixes runtime crash on shutdown.
    MIT/X11 License
    
    
    Commit migrated from https://github.com/mono/mono/commit/75e22916512ed34d51cabd98646911cfc4343a96

--- src/mono/mono/metadata/class.c
+++ src/mono/mono/metadata/class.c
@@ -2407,7 +2407,7 @@ mono_unload_interface_ids (MonoBitSet *bitset)
 void
 mono_unload_interface_id (MonoClass *class)
 {
-	if (class->interface_id) {
+	if (global_interface_bitset && class->interface_id) {
 		mono_loader_lock ();
 		mono_bitset_clear (global_interface_bitset, class->interface_id);
 		mono_loader_unlock ();
@@ -9021,6 +9021,7 @@ mono_classes_cleanup (void)
 {
 	if (global_interface_bitset)
 		mono_bitset_free (global_interface_bitset);
+	global_interface_bitset = NULL;
 }
 
 /**
