6750d33f6a1ad0a52281c813b0dac103b8304d58
Zoltan Varga <vargaz@gmail.com>
2011-10-22 15:46:53 +0200

    Add a stopwatch structure/functions for runtime internal use.
    Commit migrated from https://github.com/mono/mono/commit/c0ce64afca7f85a47cc813da9dbc5a63a94e8fee

--- src/mono/mono/utils/mono-time.h
+++ src/mono/mono/utils/mono-time.h
@@ -13,5 +13,35 @@ gint64  mono_100ns_ticks     (void) MONO_INTERNAL;
 /* Returns the number of 100ns ticks since 1/1/1, UTC timezone */
 gint64  mono_100ns_datetime  (void) MONO_INTERNAL;
 
+/* Stopwatch class for internal runtime use */
+typedef struct {
+	gint32 start, stop;
+} MonoStopwatch;
+
+static inline void
+mono_stopwatch_start (MonoStopwatch *w)
+{
+	w->start = mono_100ns_ticks ();
+	w->stop = 0;
+}
+
+static inline void
+mono_stopwatch_stop (MonoStopwatch *w)
+{
+	w->stop = mono_100ns_ticks ();
+}
+
+static inline guint64
+mono_stopwatch_elapsed (MonoStopwatch *w)
+{
+	return (w->stop - w->start) / 10;
+}
+
+static inline guint64
+mono_stopwatch_elapsed_ms (MonoStopwatch *w)
+{
+	return (mono_stopwatch_elapsed (w) + 500) / 1000;
+}
+
 #endif /* __UTILS_MONO_TIME_H__ */
 
