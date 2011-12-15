9f0f1c57d8b9b6e6775419e9a4fd5f9d49d32f9e
Csaba Halasz <Csaba.Halasz@gmail.com>
2011-12-15 20:22:17 +0100

    Expose the fallback mmap implementation as mono_file_(un)map_fileio so that it can be used if mmap fails at runtime
    Commit migrated from https://github.com/mono/mono/commit/7db21936c7127234ea6bac9ceee6343c2eefa59d

--- src/mono/mono/utils/mono-mmap.h
+++ src/mono/mono/utils/mono-mmap.h
@@ -35,6 +35,10 @@ void* mono_valloc_aligned (size_t length, size_t alignment, int flags);
 int   mono_vfree      (void *addr, size_t length);
 void* mono_file_map   (size_t length, int flags, int fd, guint64 offset, void **ret_handle);
 int   mono_file_unmap (void *addr, void *handle);
+#ifndef HOST_WIN32
+void* mono_file_map_fileio   (size_t length, int flags, int fd, guint64 offset, void **ret_handle);
+int   mono_file_unmap_fileio (void *addr, void *handle);
+#endif
 int   mono_mprotect   (void *addr, size_t length, int flags);
 
 void* mono_shared_area         (void);
