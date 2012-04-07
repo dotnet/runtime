edb8bb92666e486036bd8c2099c12516e1aebe3a
Zoltan Varga <vargaz@gmail.com>
2012-04-07 06:04:10 +0200

    Fix the domains of the objects returned by AssemblyMirror.GetAssemblyObject ()/TypeMirror.GetTypeObject (). Avoid caching empty strings in AppDomainMirror.FriendlyName. Fixes mono/mono#4223.
    Commit migrated from https://github.com/mono/mono/commit/c14f1026f9f2bac57bb3f352e99204fc5e047d71

--- src/mono/mono/mini/debugger-agent.c
+++ src/mono/mono/mini/debugger-agent.c
@@ -6780,7 +6780,7 @@ assembly_commands (int command, guint8 *p, guint8 *end, Buffer *buf)
 		break;
 	}
 	case CMD_ASSEMBLY_GET_OBJECT: {
-		MonoObject *o = (MonoObject*)mono_assembly_get_object (mono_domain_get (), ass);
+		MonoObject *o = (MonoObject*)mono_assembly_get_object (domain, ass);
 		buffer_add_objid (buf, o);
 		break;
 	}
@@ -7264,7 +7264,7 @@ type_commands_internal (int command, MonoClass *klass, MonoDomain *domain, guint
 		break;
 	}
 	case CMD_TYPE_GET_OBJECT: {
-		MonoObject *o = (MonoObject*)mono_type_get_object (mono_domain_get (), &klass->byval_arg);
+		MonoObject *o = (MonoObject*)mono_type_get_object (domain, &klass->byval_arg);
 		buffer_add_objid (buf, o);
 		break;
 	}
