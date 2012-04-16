3d5ac26f3c3a549bd9e52ad2de9c8e6d0c15cc48
Zoltan Varga <vargaz@gmail.com>
2012-04-16 14:22:31 +0200

    Revert "Disable wrapper generation code when DISABLE_JIT is defined."
    This reverts commit mono/mono@a6583c60c0c3814ca02bf2569c7a5e48b46377a7.
    
    This doesn't work since emit_marshal () is used to compute the signatures of the wrappers themselves through the 'conv_arg_type' argument.
    
    
    Commit migrated from https://github.com/mono/mono/commit/0628864fe967ba9df19d41f5240ba67b23d311ba

--- src/mono/mono/metadata/marshal.c
+++ src/mono/mono/metadata/marshal.c
@@ -8066,10 +8066,6 @@ emit_marshal (EmitMarshalContext *m, int argnum, MonoType *t,
 	/* Ensure that we have marshalling info for this param */
 	mono_marshal_load_type_info (mono_class_from_mono_type (t));
 
-#ifdef DISABLE_JIT
-	/* Not JIT support, no need to generate correct IL */
-	return conv_arg;
-#else
 	if (spec && spec->native == MONO_NATIVE_CUSTOM)
 		return emit_marshal_custom (m, argnum, t, spec, conv_arg, conv_arg_type, action);
 
@@ -8134,7 +8130,6 @@ emit_marshal (EmitMarshalContext *m, int argnum, MonoType *t,
 		else
 			return emit_marshal_object (m, argnum, t, spec, conv_arg, conv_arg_type, action);
 	}
-#endif
 
 	return conv_arg;
 }
