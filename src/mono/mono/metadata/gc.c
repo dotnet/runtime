33dd78d8cf27f15cdb96e5036b861085cb33a609
alexrp <xtzgzorex@gmail.com>
2012-04-25 15:57:40 +0200

    Replicate .NET behavior for exceptions in the finalizer thread.
    On .NET, when an exception occurs in the finalizer thread, it is
    printed, the AppDomain.UnhandledException event is raised, and
    the runtime is aborted (exact same thing as what happens when an
    exception is thrown in a thread pool thread).
    
    We now do the same.
    
    Note that this is a breaking change; previously, we just silently
    swallowed exceptions that occurred in the finalizer thread. Given
    this, a test had to be adjusted (and renamed). An extra test has
    been added to verify that the UnhandledException event is raised
    correctly.
    
    
    Commit migrated from https://github.com/mono/mono/commit/59883a039925be9ff3e478d265aa7be896fbda79

--- src/mono/mono/metadata/gc.c
+++ src/mono/mono/metadata/gc.c
@@ -24,6 +24,7 @@
 #include <mono/metadata/metadata-internals.h>
 #include <mono/metadata/mono-mlist.h>
 #include <mono/metadata/threadpool.h>
+#include <mono/metadata/threadpool-internals.h>
 #include <mono/metadata/threads-types.h>
 #include <mono/utils/mono-logger-internal.h>
 #include <mono/metadata/gc-internal.h>
@@ -225,9 +226,8 @@ mono_gc_run_finalize (void *obj, void *data)
 
 	runtime_invoke (o, NULL, &exc, NULL);
 
-	if (exc) {
-		/* fixme: do something useful */
-	}
+	if (exc)
+		mono_internal_thread_unhandled_exception (exc);
 
 	mono_domain_set_internal (caller_domain);
 }
--- src/mono/mono/metadata/threadpool-internals.h
+++ src/mono/mono/metadata/threadpool-internals.h
@@ -3,5 +3,6 @@
 
 void mono_thread_pool_remove_socket (int sock) MONO_INTERNAL;
 gboolean mono_thread_pool_is_queue_array (MonoArray *o) MONO_INTERNAL;
+void mono_internal_thread_unhandled_exception (MonoObject* exc) MONO_INTERNAL;
 
 #endif
