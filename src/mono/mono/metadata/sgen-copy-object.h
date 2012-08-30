965614114016f7ef30f02c713f8d05cb06733b31
Jonathan Chambers <joncham@gmail.com>
2012-08-30 19:55:49 -0400

    Only use labels as values if compiling with gcc.
    Commit migrated from https://github.com/mono/mono/commit/62b5d803d23c0b67238e457516813b92f2c3cca0

--- src/mono/mono/metadata/sgen-copy-object.h
+++ src/mono/mono/metadata/sgen-copy-object.h
@@ -37,12 +37,15 @@ extern long long stat_slots_allocated_in_vain;
 static inline void
 par_copy_object_no_checks (char *destination, MonoVTable *vt, void *obj, mword objsize, SgenGrayQueue *queue)
 {
+#ifdef __GNUC__
 	static const void *copy_labels [] = { &&LAB_0, &&LAB_1, &&LAB_2, &&LAB_3, &&LAB_4, &&LAB_5, &&LAB_6, &&LAB_7, &&LAB_8 };
+#endif
 
 	DEBUG (9, g_assert (vt->klass->inited));
 	DEBUG (9, fprintf (gc_debug_file, " (to %p, %s size: %lu)\n", destination, ((MonoObject*)obj)->vtable->klass->name, (unsigned long)objsize));
 	binary_protocol_copy (obj, destination, vt, objsize);
-
+	
+#ifdef __GNUC__
 	if (objsize <= sizeof (gpointer) * 8) {
 		mword *dest = (mword*)destination;
 		goto *copy_labels [objsize / sizeof (gpointer)];
@@ -68,6 +71,9 @@ par_copy_object_no_checks (char *destination, MonoVTable *vt, void *obj, mword o
 		/*can't trust memcpy doing word copies */
 		mono_gc_memmove (destination + sizeof (mword), (char*)obj + sizeof (mword), objsize - sizeof (mword));
 	}
+#else
+		mono_gc_memmove (destination + sizeof (mword), (char*)obj + sizeof (mword), objsize - sizeof (mword));
+#endif
 	/* adjust array->bounds */
 	DEBUG (9, g_assert (vt->gc_descr));
 	if (G_UNLIKELY (vt->rank && ((MonoArray*)obj)->bounds)) {
