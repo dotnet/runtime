e6eef42c8f344e26d2747b07f0ccf10f1c27b289
Zoltan Varga <vargaz@gmail.com>
2012-11-01 18:12:25 +0100

    Disable LLVM+AOT for methods with clauses on ARM, because some tests still fail.
    Commit migrated from https://github.com/mono/mono/commit/d3891886acbeae52773adcdefefe93e058916647

--- src/mono/mono/mini/mini-llvm.c
+++ src/mono/mono/mini/mini-llvm.c
@@ -2158,15 +2158,8 @@ process_bb (EmitContext *ctx, MonoBasicBlock *bb)
 			 * http://llvm.org/bugs/show_bug.cgi?id=6102
 			 */
 			//LLVM_FAILURE (ctx, "aot+clauses");
-#ifdef TARGET_MACH
-			/*
-			 * llc asserts for this code when targeting darwin:
-			 * try {
-			 *   throw new Exception ();
-			 * } catch (Exception ex) {
-			 *   Console.WriteLine (ex);
-			 * }
-			 */
+#ifdef TARGET_ARM
+			// test_0_invalid_unbox_arrays () fails
 			LLVM_FAILURE (ctx, "aot+clauses");
 #endif
 		} else {
