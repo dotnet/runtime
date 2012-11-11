f2a4ebadf2e0227d95c32f7d7b464f0de15d0100
Marek Safar <marek.safar@gmail.com>
2012-11-11 18:28:38 +0100

    Add System.Runtime.Serialization to runtime remapping. Fixes mono/mono#8037
    Commit migrated from https://github.com/mono/mono/commit/e3b9881e5707953bd37fb3ed0dbeab93e6603a5e

--- src/mono/mono/metadata/assembly.c
+++ src/mono/mono/metadata/assembly.c
@@ -114,6 +114,7 @@ static const AssemblyVersionMap framework_assemblies [] = {
 	{"System.Management", 0},
 	{"System.Messaging", 0},
 	{"System.Runtime.Remoting", 0},
+	{"System.Runtime.Serialization", 2},	
 	{"System.Runtime.Serialization.Formatters.Soap", 0},
 	{"System.Security", 0},
 	{"System.ServiceProcess", 0},
