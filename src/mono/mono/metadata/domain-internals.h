a775d9ad4f4a0d2ebc0c83f8980bf83066bdba0e
Pablo Ruiz Garcia <pablo.ruiz@gmail.com>
2012-11-27 18:53:58 +0100

    Fixed xamarin-bug mono/mono#8565: System.Runtime.Serialization assembly remapping bug when targeting 2/3/3.5 profile.
    The problem is related to a fix for issues mono/mono#8037 & mono/mono#6528
    (e3b9881) which seems to be breaking shadow copy
    with programs targeting .net35 and referencing System.Runtime.Serialization.dll
    
    The fix consists on adding a new version_set entry to each supported_runtime
    at mono/metadata/domain.c so I we cope with the case of assemblies which must
    be remapped to v4.0 on net4/net45, but which should be remapped to v3.0 (instead
    of v3.5) when targeting net2 or net3x.
    
    See: https://bugzilla.xamarin.com/show_bug.cgi?id=8565
    
    License: This patch is under the MIT/X11 license.
    
    
    Commit migrated from https://github.com/mono/mono/commit/05828c83c84e1464e8d8dfcdb55d0a227016818f

--- src/mono/mono/metadata/domain-internals.h
+++ src/mono/mono/metadata/domain-internals.h
@@ -393,7 +393,7 @@ typedef struct  {
 typedef struct  {
 	const char runtime_version [12];
 	const char framework_version [4];
-	const AssemblyVersionSet version_sets [3];
+	const AssemblyVersionSet version_sets [4];
 } MonoRuntimeInfo;
 
 #define mono_domain_lock(domain) mono_locks_acquire(&(domain)->lock, DomainLock)
