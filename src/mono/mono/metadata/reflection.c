5fdc79dd688be85cd8431a567ca72202d3fd7780
Zoltan Varga <vargaz@gmail.com>
2013-01-14 22:14:55 +0100

    Remove some special case code from mono_image_get_fieldref_token () which no longer seems to be needed. Fixes mono/mono#9531.
    Commit migrated from https://github.com/mono/mono/commit/56275bc16bf2ab007ae7f943ffe746dadde27dc1

--- src/mono/mono/metadata/reflection.c
+++ src/mono/mono/metadata/reflection.c
@@ -2750,15 +2750,10 @@ mono_image_get_fieldref_token (MonoDynamicImage *assembly, MonoObject *f, MonoCl
 	if (token)
 		return token;
 
-	if (field->parent->generic_class && field->parent->generic_class->container_class && field->parent->generic_class->container_class->fields) {
-		int index = field - field->parent->fields;
-		type = field->parent->generic_class->container_class->fields [index].type;
-	} else {
-		if (is_field_on_inst (field))
-			type = get_field_on_inst_generic_type (field);
-		else
-			type = mono_field_get_type (field);
-	}
+	if (is_field_on_inst (field))
+		type = get_field_on_inst_generic_type (field);
+	else
+		type = mono_field_get_type (field);
 	token = mono_image_get_memberref_token (assembly, &field->parent->byval_arg,
 											mono_field_get_name (field),
 											fieldref_encode_signature (assembly, field->parent->image, type));
