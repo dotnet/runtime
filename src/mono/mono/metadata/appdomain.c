3489d6ff588ab30e4b83d56d9257b9b006bb3149
Rodrigo Kumpera <kumpera@gmail.com>
2013-02-04 13:51:28 -0500

    Make sure assembly ordering matches MS by always appending. Fixes mono/mono#10004.
    Commit migrated from https://github.com/mono/mono/commit/a736dee9e1511a0d3ff87ff14fe46459c03dd73b

--- src/mono/mono/metadata/appdomain.c
+++ src/mono/mono/metadata/appdomain.c
@@ -997,7 +997,7 @@ add_assemblies_to_domain (MonoDomain *domain, MonoAssembly *ass, GHashTable *ht)
 	if (!g_hash_table_lookup (ht, ass)) {
 		mono_assembly_addref (ass);
 		g_hash_table_insert (ht, ass, ass);
-		domain->domain_assemblies = g_slist_prepend (domain->domain_assemblies, ass);
+		domain->domain_assemblies = g_slist_append (domain->domain_assemblies, ass);
 		mono_trace (G_LOG_LEVEL_INFO, MONO_TRACE_ASSEMBLY, "Assembly %s[%p] added to domain %s, ref_count=%d", ass->aname.name, ass, domain->friendly_name, ass->ref_count);
 	}
 
