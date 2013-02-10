cc1a642e1a7247d0743abdad1b87413f5a4be0bc
Jack Pappas <jackp@server.fake>
2013-02-09 19:38:08 -0500

    Swap the condition -- instead of checking for OS X and assuming anything else uses the Linux behavior, check for Linux and let other platforms use the default POSIX behavior. This allows sigaltstack to be used on BSD variants. This change has been tested (with the standard test suite) on FreeBSD 9.1 (i386, amd64) and Ubuntu 12.04.1 (i386).
    Commit migrated from https://github.com/mono/mono/commit/cc991feac75d6957495b5d1b24924b367b7ed18e

--- src/mono/mono/mini/mini-exceptions.c
+++ src/mono/mono/mini/mini-exceptions.c
@@ -2002,10 +2002,11 @@ mono_setup_altstack (MonoJitTlsData *tls)
 
 	sa.ss_sp = tls->signal_stack;
 	sa.ss_size = MONO_ARCH_SIGNAL_STACK_SIZE;
-#if __APPLE__
-	sa.ss_flags = 0;
+#if __linux__
+  sa.ss_flags = SS_ONSTACK;
 #else
-	sa.ss_flags = SS_ONSTACK;
+  	/* Mac OS X, BSD */
+	sa.ss_flags = 0;
 #endif
 	g_assert (sigaltstack (&sa, NULL) == 0);
 
