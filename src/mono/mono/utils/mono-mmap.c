57882ca1e8a42102c06bf22dd6f2d5bb8906343c
Rodrigo Kumpera <kumpera@gmail.com>
2013-02-12 16:31:15 -0500

    Use the fast shared_area lookup code everywhere. The targets that don't need it don't compile it in.
    Commit migrated from https://github.com/mono/mono/commit/7d1c207c62df4f736e2b2019c52ba3540c839f0f

--- src/mono/mono/utils/mono-mmap.c
+++ src/mono/mono/utils/mono-mmap.c
@@ -490,7 +490,6 @@ mono_shared_area_instances_slow (void **array, int count, gboolean cleanup)
 	return j;
 }
 
-#if (defined (__MACH__) && defined (TARGET_ARM))
 static int
 mono_shared_area_instances_helper (void **array, int count, gboolean cleanup)
 {
@@ -523,9 +522,6 @@ mono_shared_area_instances_helper (void **array, int count, gboolean cleanup)
 	g_dir_close (dir);
 	return i;
 }
-#else
-#define mono_shared_area_instances_helper mono_shared_area_instances_slow
-#endif
 
 void*
 mono_shared_area (void)
