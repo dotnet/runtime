4be2dee5cd8f52913ba0d35e0a3d9b0fe49a22ec
Zoltan Varga <vargaz@gmail.com>
2013-02-25 23:12:38 +0100

    Fix the DISABLE_JIT build.
    Commit migrated from https://github.com/mono/mono/commit/84ec6ef7a0fc5c57dbf0a3a91c4aa02eb878196b

--- src/mono/mono/mini/tramp-arm.c
+++ src/mono/mono/mini/tramp-arm.c
@@ -1038,6 +1038,15 @@ mono_arch_get_gsharedvt_arg_trampoline (MonoDomain *domain, gpointer arg, gpoint
 	return start;
 }
 
+#else
+
+gpointer
+mono_arch_get_gsharedvt_arg_trampoline (MonoDomain *domain, gpointer arg, gpointer addr)
+{
+	g_assert_not_reached ();
+	return NULL;
+}
+
 #endif
 
 #if defined(MONOTOUCH) || defined(MONO_EXTENSIONS)
