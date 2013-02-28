c365c6337cc63ab085d7dd88bdb505d1565bb499
Zoltan Varga <vargaz@gmail.com>
2013-02-28 23:27:22 +0100

    Fix MONO_CONTEXT_GET_CURRENT () clobbering a register before using it on ARM.
    Commit migrated from https://github.com/mono/mono/commit/dc94b16c6e3346c41db76c9e6d887a0d6da7f998

--- src/mono/mono/utils/mono-context.h
+++ src/mono/mono/utils/mono-context.h
@@ -238,8 +238,8 @@ typedef struct {
 	__asm__ __volatile__(			\
 		"push {r0}\n"				\
 		"push {r1}\n"				\
-		"mov r1, r0\n"				\
 		"mov r0, %0\n"				\
+		"ldr r1, [sp, 4]\n"			\
 		"str r1, [r0]!\n"			\
 		"pop {r1}\n"				\
 		"str r1, [r0]!\n"			\
