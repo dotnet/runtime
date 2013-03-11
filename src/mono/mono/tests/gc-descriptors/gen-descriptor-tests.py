2172c6e50d7392b527af3f9b968ad8abbcda7ba2
Mark Probst <mark.probst@gmail.com>
2013-03-11 10:43:50 -0700

    [tests] In GC descriptor tests script, optionally generate huge nested if.
    Commit migrated from https://github.com/mono/mono/commit/06b2ab0a7c76ada115bd860490a4d38f5ad48a5e

--- src/mono/mono/tests/gc-descriptors/gen-descriptor-tests.py
+++ src/mono/mono/tests/gc-descriptors/gen-descriptor-tests.py
@@ -4,6 +4,7 @@ from optparse import OptionParser
 
 parser = OptionParser ()
 parser.add_option ("--switch", action = "store_true", dest = "switch")
+parser.add_option ("--one-method-if", action = "store_true", dest = "one_method_if")
 
 (options, args) = parser.parse_args ()
 
@@ -69,22 +70,32 @@ def gen_new (name):
     print ("  b = new %s ();" % name)
     print ("b.Fill (refs); return b;")
 
-def gen_binary_search (left, right):
-    name = search_method_name (left, right)
-    print ("public static Filler %s (int which, object[] refs, bool wrap) {" % name)
+def gen_binary_search_body (left, right, one_method):
     if left + 1 >= right:
         gen_new (names [left])
     else:
         mid = (left + right) // 2
         print ("if (which < %d) {" % mid)
-        print ("return %s (which, refs, wrap);" % search_method_name (left, mid))
+        if one_method:
+            gen_binary_search_body (left, mid, one_method)
+        else:
+            print ("return %s (which, refs, wrap);" % search_method_name (left, mid))
         print ("} else {")
-        print ("return %s (which, refs, wrap);" % search_method_name (mid, right))
+        if one_method:
+            gen_binary_search_body (mid, right, one_method)
+        else:
+            print ("return %s (which, refs, wrap);" % search_method_name (mid, right))
         print ("}")
+
+def gen_binary_search (left, right, one_method):
+    name = search_method_name (left, right)
+    print ("public static Filler %s (int which, object[] refs, bool wrap) {" % name)
+    gen_binary_search_body (left, right, one_method)
     print ("}")
-    if left + 1 < right:
-        gen_binary_search (left, mid)
-        gen_binary_search (mid, right)
+    if not one_method and left + 1 < right:
+        mid = (left + right) // 2
+        gen_binary_search (left, mid, one_method)
+        gen_binary_search (mid, right, one_method)
     return name
 
 print ("public class Bitmaps {")
@@ -99,7 +110,7 @@ if options.switch:
     print ("    }")
     print ("  }")
 else:
-    method_name = gen_binary_search (0, len (names))
+    method_name = gen_binary_search (0, len (names), options.one_method_if)
     print ("  public static Filler MakeAndFill (int which, object[] refs, bool wrap) {")
     print ("    if (which >= %d) return null;" % len (names))
     print ("    return %s (which, refs, wrap);" % method_name)
