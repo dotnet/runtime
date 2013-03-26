c6c47f4e2baa27e4ab74ebf0d3185890e0dd1fef
Zoltan Varga <vargaz@gmail.com>
2013-03-26 20:15:06 +0100

    Export a few more symbols for LLVM.
    Commit migrated from https://github.com/mono/mono/commit/f85afc4d5534b94373866507e0f8af61fd83c5c1

--- src/mono/mono/utils/mono-dl.h
+++ src/mono/mono/utils/mono-dl.h
@@ -6,8 +6,8 @@
 
 typedef struct _MonoDl MonoDl;
 
-MonoDl*     mono_dl_open       (const char *name, int flags, char **error_msg) MONO_INTERNAL;
-char*       mono_dl_symbol     (MonoDl *module, const char *name, void **symbol) MONO_INTERNAL;
+MonoDl*     mono_dl_open       (const char *name, int flags, char **error_msg) MONO_LLVM_INTERNAL;
+char*       mono_dl_symbol     (MonoDl *module, const char *name, void **symbol) MONO_LLVM_INTERNAL;
 void        mono_dl_close      (MonoDl *module) MONO_INTERNAL;
 
 char*       mono_dl_build_path (const char *directory, const char *name, void **iter) MONO_INTERNAL;
