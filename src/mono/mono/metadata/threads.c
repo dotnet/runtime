7fa649949583df44cf4e023ac8fd1ac457e9e686
Rodrigo Kumpera <kumpera@gmail.com>
2013-04-25 13:59:18 -0400

    Don't emit thread event twice when explicitly detaching a thread.
    Commit migrated from https://github.com/mono/mono/commit/65aa1e504853612485c8ff1a2a4024488f1d86c1

--- src/mono/mono/metadata/threads.c
+++ src/mono/mono/metadata/threads.c
@@ -994,8 +994,6 @@ mono_thread_detach (MonoThread *thread)
 	g_return_if_fail (thread != NULL);
 
 	THREAD_DEBUG (g_message ("%s: mono_thread_detach for %p (%"G_GSIZE_FORMAT")", __func__, thread, (gsize)thread->internal_thread->tid));
-	
-	mono_profiler_thread_end (thread->internal_thread->tid);
 
 	thread_cleanup (thread->internal_thread);
 
