f95109fc13fbaa5bfa61437cdcee1b0d4b09bda4
Mark Probst <mark.probst@gmail.com>
2013-08-09 13:24:18 -0700

    Fix mono/mono#13604 - STW handshake/thread shutdown race condition.
    Commit migrated from https://github.com/mono/mono/commit/2c45af25e2a027d749feef771a83a3c9c731f4aa

--- src/mono/mono/metadata/sgen-os-posix.c
+++ src/mono/mono/metadata/sgen-os-posix.c
@@ -57,7 +57,14 @@ suspend_thread (SgenThreadInfo *info, void *context)
 #endif
 	gpointer stack_start;
 
-	g_assert (info->doing_handshake);
+	/*
+	 * It's possible that a dying thread is parked via
+	 * sgen_park_current_thread_if_doing_handshake(), and, while parked, STW tries to
+	 * suspend it again.  In that case doing_handshake will not be set anymore, and the
+	 * "nested" suspend must be ignored.
+	 */
+	if (!info->doing_handshake)
+		return;
 
 	info->stopped_domain = mono_domain_get ();
 	info->stopped_ip = context ? (gpointer) ARCH_SIGCTX_IP (context) : NULL;
