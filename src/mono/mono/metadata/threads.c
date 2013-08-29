54583cf29a4593a643047ab74e3f872b9d15139b
Rodrigo Kumpera <kumpera@gmail.com>
2013-08-29 18:59:17 -0400

    [runtime]Use InterlockedCompareExchange64 instead of local hack.
    Commit migrated from https://github.com/mono/mono/commit/5d903d67621e50fd50f6146dc4db550d927c1717

--- src/mono/mono/metadata/threads.c
+++ src/mono/mono/metadata/threads.c
@@ -1994,19 +1994,7 @@ ves_icall_System_Threading_Interlocked_CompareExchange_Double (gdouble *location
 gint64 
 ves_icall_System_Threading_Interlocked_CompareExchange_Long (gint64 *location, gint64 value, gint64 comparand)
 {
-#if SIZEOF_VOID_P == 8
-	return (gint64)InterlockedCompareExchangePointer((gpointer *) location, (gpointer)value, (gpointer)comparand);
-#else
-	gint64 old;
-
-	mono_interlocked_lock ();
-	old = *location;
-	if (old == comparand)
-		*location = value;
-	mono_interlocked_unlock ();
-	
-	return old;
-#endif
+	return InterlockedCompareExchange64 (location, value, comparand);
 }
 
 MonoObject*
