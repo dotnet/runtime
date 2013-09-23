79ccf3aab3c5ab47583a142416bea08c69fb29ae
Rodrigo Kumpera <kumpera@gmail.com>
2013-09-23 19:14:05 -0400

    Fix the windows build.
    Commit migrated from https://github.com/mono/mono/commit/3734c725a4f18b23f9979641f4ce771a4ed21dc0

--- src/mono/mono/metadata/sgen-os-win32.c
+++ src/mono/mono/metadata/sgen-os-win32.c
@@ -107,15 +107,9 @@ sgen_thread_handshake (BOOL suspend)
 		if (info->gc_disabled)
 			continue;
 		if (suspend) {
-			g_assert (!info->doing_handshake);
-			info->doing_handshake = TRUE;
-
 			if (!sgen_suspend_thread (info))
 				continue;
 		} else {
-			g_assert (info->doing_handshake);
-			info->doing_handshake = FALSE;
-
 			if (!sgen_resume_thread (info))
 				continue;
 		}
