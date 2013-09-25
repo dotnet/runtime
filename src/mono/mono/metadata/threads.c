bb18b57106a0cb2a1f2512d65ed94d4a618910d7
Alex Rønne Petersen <alexrp@xamarin.com>
2013-09-25 20:31:18 +0200

    Use InterlockedAdd64 () for Interlocked.Add (long&, long).
    Commit migrated from https://github.com/mono/mono/commit/421e1beea55d26d767dbd857e2f2276e3c6c2bee

--- src/mono/mono/metadata/threads.c
+++ src/mono/mono/metadata/threads.c
@@ -1955,14 +1955,7 @@ ves_icall_System_Threading_Interlocked_Add_Long (gint64 *location, gint64 value)
 	mono_raise_exception (mono_get_exception_not_implemented (NULL));
 	return 0;
 #else
-	gint64 orig;
-
-	mono_interlocked_lock ();
-	orig = *location;
-	*location = orig + value;
-	mono_interlocked_unlock ();
-
-	return orig + value;
+	return InterlockedAdd64 (location, value);
 #endif
 }
 
