de7bcc4da50cc868aba144a4eba1e7e1e574a02e
Alex Rønne Petersen <alexrp@xamarin.com>
2013-09-25 20:19:18 +0200

    Use InterlockedRead64 () for Interlocked.Read (long&).
    Commit migrated from https://github.com/mono/mono/commit/ab90040305d003b6c06c842e788e682495117514

--- src/mono/mono/metadata/threads.c
+++ src/mono/mono/metadata/threads.c
@@ -2023,18 +2023,7 @@ ves_icall_System_Threading_Interlocked_Add_Long (gint64 *location, gint64 value)
 gint64 
 ves_icall_System_Threading_Interlocked_Read_Long (gint64 *location)
 {
-#if SIZEOF_VOID_P == 8
-	/* 64 bit reads are already atomic */
-	return *location;
-#else
-	gint64 res;
-
-	mono_interlocked_lock ();
-	res = *location;
-	mono_interlocked_unlock ();
-
-	return res;
-#endif
+	return InterlockedRead64 (location);
 }
 
 void
