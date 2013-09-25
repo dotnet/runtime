5b5886c7f9bd876d9388061f9d63f6c4d8055446
Alex Rønne Petersen <alexrp@xamarin.com>
2013-09-25 20:23:36 +0200

    Use InterlockedExchange64 () for Interlocked.Exchange (double&, double).
    Commit migrated from https://github.com/mono/mono/commit/d785b33891091153ced9e632129badc7262efca9

--- src/mono/mono/metadata/threads.c
+++ src/mono/mono/metadata/threads.c
@@ -1839,27 +1839,12 @@ ves_icall_System_Threading_Interlocked_Exchange_Long (gint64 *location, gint64 v
 gdouble 
 ves_icall_System_Threading_Interlocked_Exchange_Double (gdouble *location, gdouble value)
 {
-#if SIZEOF_VOID_P == 8
 	LongDoubleUnion val, ret;
 
 	val.fval = value;
-	ret.ival = (gint64)InterlockedExchangePointer((gpointer *) location, (gpointer)val.ival);
+	ret.ival = (gint64)InterlockedExchange64((gint64 *) location, val.ival);
 
 	return ret.fval;
-#else
-	gdouble res;
-
-	/* 
-	 * According to MSDN, this function is only atomic with regards to the 
-	 * other Interlocked functions on 32 bit platforms.
-	 */
-	mono_interlocked_lock ();
-	res = *location;
-	*location = value;
-	mono_interlocked_unlock ();
-
-	return res;
-#endif
 }
 
 gint32 ves_icall_System_Threading_Interlocked_CompareExchange_Int(gint32 *location, gint32 value, gint32 comparand)
