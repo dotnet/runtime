c2ecc64430dd3bc56331382192b43bbc258bc65e
Zoltan Varga <vargaz@gmail.com>
2013-10-18 18:01:44 +0200

    [runtime] Add a missing file.
    Commit migrated from https://github.com/mono/mono/commit/c94acdd08549f5807d8e66d63f227faa5dc6ccc7

---
+++ src/mono/mono/utils/mono-tls.c
@@ -0,0 +1,35 @@
+/*
+ * mono-tls.c: Low-level TLS support
+ *
+ * Copyright 2013 Xamarin, Inc (http://www.xamarin.com)
+ */
+
+#include "mono-tls.h"
+
+static int tls_offsets [TLS_KEY_NUM];
+static gboolean tls_offset_set [TLS_KEY_NUM];
+
+/*
+ * mono_tls_key_get_offset:
+ *
+ *   Return the TLS offset used by the TLS var identified by KEY, previously initialized by a call to
+ * mono_tls_key_set_offset (). Return -1 if the offset is not known.
+ */
+int
+mono_tls_key_get_offset (MonoTlsKey key)
+{
+	g_assert (tls_offset_set [key]);
+	return tls_offsets [key];
+}
+
+/*
+ * mono_tls_key_set_offset:
+ *
+ *   Set the TLS offset used by the TLS var identified by KEY.
+ */
+void
+mono_tls_key_set_offset (MonoTlsKey key, int offset)
+{
+	tls_offsets [key] = offset;
+	tls_offset_set [key] = TRUE;
+}
