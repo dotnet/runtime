c1c460a4f8c1d987e18e26aba542ccaf9b80f4e6
Rodrigo Kumpera <kumpera@gmail.com>
2013-11-04 10:55:29 -0500

    [build]Enable passing arguments to the extension module.
    The build argument --enable-extension-module now takes a comma separated list
    of arguments that are passed down. Passing no arguments triggers the old behavior.
    
    
    Commit migrated from https://github.com/mono/mono/commit/1ba345570fd761c9afb18b1130991f394846ba69

--- src/mono/autogen.sh
+++ src/mono/autogen.sh
@@ -107,15 +107,19 @@ fi
 # Plug in the extension module
 #
 has_ext_mod=false
+ext_mod_args=''
 for PARAM; do
-    if test "$PARAM" = "--enable-extension-module" ; then
-		has_ext_mod=true
-	fi
+    if [[ $PARAM =~ "--enable-extension-module" ]] ; then
+        has_ext_mod=true
+        if [[ $PARAM =~ "=" ]] ; then
+            ext_mod_args=`echo $PARAM | cut -d= -f2`
+        fi
+    fi
 done
 
 if test x$has_ext_mod = xtrue; then
 	pushd ../mono-extensions/scripts
-	sh ./prepare-repo.sh || exit 1
+	sh ./prepare-repo.sh $ext_mod_args || exit 1
 	popd
 else
 	cat mono/mini/Makefile.am.in > mono/mini/Makefile.am
