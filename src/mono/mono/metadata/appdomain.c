aa0b7af82f0cf6b6147265c1e707c5cc9766650b
Marek Safar <marek.safar@gmail.com>
2013-12-06 09:50:21 +0100

    Bump mscorlib version for monolite to work correctly
    Commit migrated from https://github.com/mono/mono/commit/c8d91bc505446f3d017f238250e6de608b603110

--- src/mono/mono/metadata/appdomain.c
+++ src/mono/mono/metadata/appdomain.c
@@ -77,7 +77,7 @@
  * Changes which are already detected at runtime, like the addition
  * of icalls, do not require an increment.
  */
-#define MONO_CORLIB_VERSION 110
+#define MONO_CORLIB_VERSION 111
 
 typedef struct
 {
