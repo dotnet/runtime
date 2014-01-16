7d563cd8abf011b25e0fa2e403b9baf370d60e44
Zoltan Varga <vargaz@gmail.com>
2014-01-16 03:37:21 +0100

    Fix the build.
    Commit migrated from https://github.com/mono/mono/commit/8af5af5763a71bc46fa2bb08cf877ff489ead9ae

--- src/mono/mono/mini/mini.c
+++ src/mono/mono/mini/mini.c
@@ -7858,6 +7858,22 @@ mono_arch_get_gsharedvt_call_info (gpointer addr, MonoMethodSignature *normal_si
 
 #endif
 
+#ifndef MONO_NATIVE_TYPES
+
+MonoType*
+mini_native_type_replace_type (MonoType *type)
+{
+	return type;
+}
+
+MonoInst*
+mono_emit_native_types_intrinsics (MonoCompile *cfg, MonoMethod *cmethod, MonoMethodSignature *fsig, MonoInst **args)
+{
+	return NULL;
+}
+
+#endif
+
 #ifdef USE_JUMP_TABLES
 #define DEFAULT_JUMPTABLE_CHUNK_ELEMENTS 128
 
