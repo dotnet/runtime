715e17737bebd5a269300c821f7a6dd012805916
Miguel de Icaza <miguel@gnome.org>
2014-02-20 19:12:08 -0500

    [style] comment style updated
    Commit migrated from https://github.com/mono/mono/commit/22a41030911a49cf23d4242400e9a879e96dfe17

--- src/mono/mono/utils/mono-threads-mach-helper.c
+++ src/mono/mono/utils/mono-threads-mach-helper.c
@@ -4,7 +4,7 @@
  * Author:
  *	Rodrigo Kumpera (kumpera@gmail.com)
  *
- * (C) 2014 Xamarin
+ * (C) 2014 Xamarin Inc
  */
 
 #include "config.h"
@@ -16,9 +16,9 @@
 #include <mono/utils/mono-compiler.h>
 
 /*
-We cannot include mono-threads.h as this includes io-layer internal types
-which conflicts with objc.
-Hence the hack here.
+ * We cannot include mono-threads.h as this includes io-layer internal types
+ * which conflicts with objc.
+ * Hence the hack here.
 */
 void mono_threads_init_dead_letter (void) MONO_INTERNAL;
 void mono_threads_install_dead_letter (void) MONO_INTERNAL;
@@ -29,24 +29,27 @@ static SEL dealloc, release, currentThread, threadDictionary, init, alloc, objec
 static id mono_dead_letter_key;
 
 /*
-Our Mach bindings have a problem in that they might need to attach the runtime after
-the the user tls keys have been destroyed.
-
-This happens when a bound object is retained by NSThread, which is released very late in the
-TLS cleanup process.
-
-At that point, attaching the runtime leaves us in no position to detach it later using TLS destructors
-as pthread is done with user keys. This leaves us with a dead thread registered, which can cause
-all sorts of terrible problems.
-
-The tipical crash is when another thread is created at the exact same address of the previous one, cause
-thread registration to abort due to duplicate entries.
-
-So what do we do here?
-
-Experimentation showns that threadDictionary is destroied after the problematic keys, so we add our dead letter
-object as an aditional way to be notified of thread death.
-*/
+ * Our Mach bindings have a problem in that they might need to attach
+ * the runtime after the the user tls keys have been destroyed.
+ *
+ * This happens when a bound object is retained by NSThread, which is
+ * released very late in the TLS cleanup process.
+ *
+ * At that point, attaching the runtime leaves us in no position to
+ * detach it later using TLS destructors as pthread is done with user
+ * keys. This leaves us with a dead thread registered, which can cause
+ * all sorts of terrible problems.
+ *
+ * The tipical crash is when another thread is created at the exact
+ * same address of the previous one, cause thread registration to abort
+ * due to duplicate entries.
+ *
+ * So what do we do here?
+ * 
+ * Experimentation showns that threadDictionary is destroied after the
+ * problematic keys, so we add our dead letter object as an aditional
+ * way to be notified of thread death.
+ */
 static void
 mono_dead_letter_dealloc (id self, SEL _cmd)
 {
@@ -92,12 +95,12 @@ mono_threads_init_dead_letter (void)
 	setObjectForKey = sel_registerName ("setObject:forKey:");
 	objectForKey = sel_registerName ("objectForKey:");
 
-	//define the dead letter class
+	// define the dead letter class
 	mono_dead_letter_class = objc_allocateClassPair (nsobject, "MonoDeadLetter", 0);
 	class_addMethod (mono_dead_letter_class, dealloc, (IMP)mono_dead_letter_dealloc, "v@:");
 	objc_registerClassPair (mono_dead_letter_class);
 
-	//create the dict key
+	// create the dict key
 	id pool = objc_msgSend (objc_msgSend (nsautoreleasepool, alloc), init);
 	mono_dead_letter_key = objc_msgSend (nsstring, stringWithUTF8String, "mono-dead-letter");
 	objc_msgSend (mono_dead_letter_key, retain);
