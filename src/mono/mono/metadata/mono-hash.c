dac2e18a3e7fe367524c5abbc7c6641d8fdd978f
Zoltan Varga <vargaz@gmail.com>
2014-03-10 21:06:36 -0400

    Fix a warning.
    Commit migrated from https://github.com/mono/mono/commit/d65d91435a231811d5d378fcddb096950f02f8f2

--- src/mono/mono/metadata/mono-hash.c
+++ src/mono/mono/metadata/mono-hash.c
@@ -65,14 +65,6 @@ struct _MonoGHashTable {
 	MonoGHashGCType gc_type;
 };
 
-static const int prime_tbl[] = {
-	11, 19, 37, 73, 109, 163, 251, 367, 557, 823, 1237,
-	1861, 2777, 4177, 6247, 9371, 14057, 21089, 31627,
-	47431, 71143, 106721, 160073, 240101, 360163,
-	540217, 810343, 1215497, 1823231, 2734867, 4102283,
-	6153409, 9230113, 13845163
-};
-
 #ifdef HAVE_SGEN_GC
 static void *table_hash_descr = NULL;
 
