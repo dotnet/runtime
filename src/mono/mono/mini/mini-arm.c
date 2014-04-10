b50b920bcbdaa885f8fd4198747504601d51f183
Alex Rønne Petersen <alexrp@xamarin.com>
2014-04-10 23:24:16 +0200

    ARM: Fix epilogue instrumentation to handle MONO_TYPE_{GENERICINST,R4,R8} correctly.
    Commit migrated from https://github.com/mono/mono/commit/1afa622ba446ca7f67f24d446b938826bd568574

--- src/mono/mono/mini/mini-arm.c
+++ src/mono/mono/mini/mini-arm.c
@@ -2926,7 +2926,8 @@ enum {
 	SAVE_STRUCT,
 	SAVE_ONE,
 	SAVE_TWO,
-	SAVE_FP
+	SAVE_ONE_FP,
+	SAVE_TWO_FP
 };
 
 void*
@@ -2961,9 +2962,23 @@ mono_arch_instrument_epilog_full (MonoCompile *cfg, void *func, void *p, gboolea
 		save_mode = SAVE_TWO;
 		break;
 	case MONO_TYPE_R4:
-	case MONO_TYPE_R8:
-		save_mode = SAVE_FP;
+		if (IS_HARD_FLOAT)
+			save_mode = SAVE_ONE_FP;
+		else
+			save_mode = SAVE_ONE;
 		break;
+	case MONO_TYPE_R8:
+		if (IS_HARD_FLOAT)
+			save_mode = SAVE_TWO_FP;
+		else
+			save_mode = SAVE_TWO;
+		break;
+	case MONO_TYPE_GENERICINST:
+		if (!mono_type_generic_inst_is_valuetype (rtype)) {
+			save_mode = SAVE_ONE;
+			break;
+		}
+		/* Fall through */
 	case MONO_TYPE_VALUETYPE:
 		save_mode = SAVE_STRUCT;
 		break;
@@ -2987,10 +3002,16 @@ mono_arch_instrument_epilog_full (MonoCompile *cfg, void *func, void *p, gboolea
 			ARM_MOV_REG_REG (code, ARMREG_R1, ARMREG_R0);
 		}
 		break;
-	case SAVE_FP:
-		/* FIXME: what reg?  */
+	case SAVE_ONE_FP:
+		ARM_FSTS (code, ARM_VFP_F0, cfg->frame_reg, save_offset);
 		if (enable_arguments) {
-			/* FIXME: what reg?  */
+			ARM_FMRS (code, ARMREG_R1, ARM_VFP_F0);
+		}
+		break;
+	case SAVE_TWO_FP:
+		ARM_FSTD (code, ARM_VFP_D0, cfg->frame_reg, save_offset);
+		if (enable_arguments) {
+			ARM_FMDRR (code, ARMREG_R1, ARMREG_R2, ARM_VFP_D0);
 		}
 		break;
 	case SAVE_STRUCT:
@@ -3016,8 +3037,11 @@ mono_arch_instrument_epilog_full (MonoCompile *cfg, void *func, void *p, gboolea
 	case SAVE_ONE:
 		ARM_LDR_IMM (code, ARMREG_R0, cfg->frame_reg, save_offset);
 		break;
-	case SAVE_FP:
-		/* FIXME */
+	case SAVE_ONE_FP:
+		ARM_FLDS (code, ARM_VFP_F0, cfg->frame_reg, save_offset);
+		break;
+	case SAVE_TWO_FP:
+		ARM_FLDD (code, ARM_VFP_D0, cfg->frame_reg, save_offset);
 		break;
 	case SAVE_NONE:
 	default:
