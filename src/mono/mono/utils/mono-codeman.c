ed0d6efd2aca6284b7e8ee04b550c5da7f34d71a
Rodrigo Kumpera <kumpera@gmail.com>
2014-04-15 17:53:26 -0400

    [windows] Win64 requires 16 bytes alignment too.
    Commit migrated from https://github.com/mono/mono/commit/f15f527aff44982a70135442f713505ef63944d1

--- src/mono/mono/utils/mono-codeman.c
+++ src/mono/mono/utils/mono-codeman.c
@@ -42,7 +42,7 @@ static gulong dynamic_code_frees_count;
 
 #define MIN_PAGES 16
 
-#if defined(__ia64__) || defined(__x86_64__)
+#if defined(__ia64__) || defined(__x86_64__) || defined (_WIN64)
 /*
  * We require 16 byte alignment on amd64 so the fp literals embedded in the code are 
  * properly aligned for SSE2.
