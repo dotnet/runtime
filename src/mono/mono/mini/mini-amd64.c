c8a58443a6ccfad4ca1f0bd3371e625dfea43cb0
Rodrigo Kumpera <kumpera@gmail.com>
2014-04-16 11:34:14 -0400

    [win64] Fix stack probing and disable omit_fp. Fixes iltests.
    Stack probing on windows must be done a page a time so we must always force it to happen on windows.
    Disable omit_fp on win64 for now as it makes the tail call tests fail.
    
    
    Commit migrated from https://github.com/mono/mono/commit/d2108392db296befe5fb8a4938f2bdf98c1853d3

--- src/mono/mono/mini/mini-amd64.c
+++ src/mono/mono/mini/mini-amd64.c
@@ -1428,6 +1428,10 @@ mono_arch_compute_omit_fp (MonoCompile *cfg)
 	cfg->arch.omit_fp = FALSE;
 #endif
 
+#ifdef HOST_WIN32
+	cfg->arch.omit_fp = FALSE;
+#endif
+
 	if (cfg->disable_omit_fp)
 		cfg->arch.omit_fp = FALSE;
 
@@ -3417,7 +3421,9 @@ mono_emit_stack_alloc (MonoCompile *cfg, guchar *code, MonoInst* tree)
 	int sreg = tree->sreg1;
 	int need_touch = FALSE;
 
-#if defined(HOST_WIN32) || defined(MONO_ARCH_SIGSEGV_ON_ALTSTACK)
+#if defined(HOST_WIN32)
+		need_touch = TRUE;
+#elif defined(MONO_ARCH_SIGSEGV_ON_ALTSTACK)
 	if (!tree->flags & MONO_INST_INIT)
 		need_touch = TRUE;
 #endif
