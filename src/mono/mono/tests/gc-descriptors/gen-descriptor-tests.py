7661204da3b95a208d0ad08918a8a495229ef2e4
Alex Rønne Petersen <alexrp@xamarin.com>
2014-04-16 12:46:48 +0200

    Make all Python scripts Python 3-friendly.
    Commit migrated from https://github.com/mono/mono/commit/a7c60da137619ba7dd168f645e6108add9bdd7bc

--- src/mono/mono/tests/gc-descriptors/gen-descriptor-tests.py
+++ src/mono/mono/tests/gc-descriptors/gen-descriptor-tests.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
 from optparse import OptionParser
+import sys
 
 parser = OptionParser ()
 parser.add_option ("--switch", action = "store_true", dest = "switch")
@@ -11,14 +13,15 @@ parser.add_option ("--one-method-if", action = "store_true", dest = "one_method_
 def print_file (file_name):
     f = open (file_name, "r")
     for line in f:
-        print (line),
+        sys.stdout.write (line + " ")
+        sys.stdout.flush ()
     f.close ()
 
 print_file ("descriptor-tests-prefix.cs")
 
 print ("public struct NoRef1 { int x; }")
 for i in range (1, 17):
-    print ("public struct NoRef%d { NoRef%d a, b; }" % (1 << i, 1 << (i-1)))
+    print ("public struct NoRef{0} {{ NoRef{1} a, b; }}".format (1 << i, 1 << (i-1)))
 
 print ("")
 
@@ -29,30 +32,30 @@ max_bitmap = 257
 
 for offset in range (0, max_offset, 19):
     for bitmap in range (0, max_bitmap):
-        name = "Bitmap%dSkip%d" % (bitmap, offset)
+        name = "Bitmap{0}Skip{1}".format (bitmap, offset)
         names.append (name)
-        print ("public struct %s : Filler {" % name)
+        print ("public struct {0} : Filler {{".format (name))
         for i in range (0, 16):
             bit = 1 << i
             if offset & bit:
-                print ("  NoRef%d skip%d;" % (bit, bit))
+                print ("  NoRef{0} skip{1};".format (bit, bit))
         for i in range (0, 9):
             bit = 1 << i
             if bitmap & bit:
-                print ("  object ref%d;" % i)
+                print ("  object ref{0};".format (i))
             else:
-                print ("  int bit%d;" % i)
+                print ("  int bit{0};".format (i))
         print ("  public void Fill (object[] refs) {")
         for i in range (0, 9):
             bit = 1 << i
             if bitmap & bit:
-                print ("    ref%d = refs [%d];" % (i, i))
+                print ("    ref{0} = refs [{1}];".format (i, i))
         print ("  }")
         print ("}")
-        print ("public class %sWrapper : Filler {" % name)
-        print ("  %s[] a;" % name)
-        print ("  public %sWrapper () {" % name)
-        print ("    a = new %s [1];" % name)
+        print ("public class {0}Wrapper : Filler {{".format (name))
+        print ("  {0}[] a;".format (name))
+        print ("  public {0}Wrapper () {{".format (name))
+        print ("    a = new {0} [1];".format (name))
         print ("  }")
         print ("  public void Fill (object[] refs) {")
         print ("    a [0].Fill (refs);")
@@ -60,14 +63,14 @@ for offset in range (0, max_offset, 19):
         print ("}\n")
 
 def search_method_name (left, right):
-    return "MakeAndFillL%dR%d" % (left, right)
+    return "MakeAndFillL{0}R{1}".format (left, right)
 
 def gen_new (name):
     print ("Filler b;")
     print ("if (wrap)")
-    print ("  b = new %sWrapper ();" % name)
+    print ("  b = new {0}Wrapper ();".format (name))
     print ("else")
-    print ("  b = new %s ();" % name)
+    print ("  b = new {0} ();".format (name))
     print ("b.Fill (refs); return b;")
 
 def gen_binary_search_body (left, right, one_method):
@@ -75,21 +78,21 @@ def gen_binary_search_body (left, right, one_method):
         gen_new (names [left])
     else:
         mid = (left + right) // 2
-        print ("if (which < %d) {" % mid)
+        print ("if (which < {0}) {{".format (mid))
         if one_method:
             gen_binary_search_body (left, mid, one_method)
         else:
-            print ("return %s (which, refs, wrap);" % search_method_name (left, mid))
+            print ("return {0} (which, refs, wrap);".format (search_method_name (left, mid)))
         print ("} else {")
         if one_method:
             gen_binary_search_body (mid, right, one_method)
         else:
-            print ("return %s (which, refs, wrap);" % search_method_name (mid, right))
+            print ("return {0} (which, refs, wrap);".format (search_method_name (mid, right)))
         print ("}")
 
 def gen_binary_search (left, right, one_method):
     name = search_method_name (left, right)
-    print ("public static Filler %s (int which, object[] refs, bool wrap) {" % name)
+    print ("public static Filler {0} (int which, object[] refs, bool wrap) {{".format (name))
     gen_binary_search_body (left, right, one_method)
     print ("}")
     if not one_method and left + 1 < right:
@@ -103,7 +106,7 @@ if options.switch:
     print ("  public static Filler MakeAndFill (int which, object[] refs, bool wrap) {")
     print ("    switch (which) {")
     for i in range (0, len (names)):
-        print ("      case %d: {" % i)
+        print ("      case {0}: {{".format (i))
         gen_new (names [i])
         print ("}")
     print ("      default: return null;")
@@ -112,10 +115,10 @@ if options.switch:
 else:
     method_name = gen_binary_search (0, len (names), options.one_method_if)
     print ("  public static Filler MakeAndFill (int which, object[] refs, bool wrap) {")
-    print ("    if (which >= %d) return null;" % len (names))
-    print ("    return %s (which, refs, wrap);" % method_name)
+    print ("    if (which >= {0}) return null;".format (len (names)))
+    print ("    return {0} (which, refs, wrap);".format (method_name))
     print ("  }")
-print ("  public const int NumWhich = %d;" % len (names))
+print ("  public const int NumWhich = {0};".format (len (names)))
 print ("}")
 
 print ("")
