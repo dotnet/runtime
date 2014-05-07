41a51c87aeed119caa4771b5c93123cac257a7a4
Zoltan Varga <vargaz@gmail.com>
2014-05-07 14:50:20 +0200

    Fix the build.
    Commit migrated from https://github.com/mono/mono/commit/1e4e2bd449fb146b8cabfef8b6ff2418ecb2e921

--- src/mono/mono/mini/method-to-ir.c
+++ src/mono/mono/mini/method-to-ir.c
@@ -6898,7 +6898,7 @@ mono_method_to_ir (MonoCompile *cfg, MonoMethod *method, MonoBasicBlock *start_b
 			int *il_offsets;
 			int *line_numbers;
 
-			mono_debug_symfile_get_line_numbers_full (minfo, NULL, NULL, &n_il_offsets, &il_offsets, &line_numbers, NULL, NULL);
+			mono_debug_symfile_get_line_numbers_full (minfo, NULL, NULL, &n_il_offsets, &il_offsets, &line_numbers, NULL, NULL, NULL, NULL);
 			seq_point_locs = mono_bitset_mem_new (mono_mempool_alloc0 (cfg->mempool, mono_bitset_alloc_size (header->code_size, 0)), header->code_size, 0);
 			seq_point_set_locs = mono_bitset_mem_new (mono_mempool_alloc0 (cfg->mempool, mono_bitset_alloc_size (header->code_size, 0)), header->code_size, 0);
 			sym_seq_points = TRUE;
