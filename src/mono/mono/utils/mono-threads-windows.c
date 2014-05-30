e7b35a79b3b5514e7b5271a383352ef02a718ab7
Rodrigo Kumpera <kumpera@gmail.com>
2014-05-30 14:11:46 -0400

    fix win thread size calc.
    Commit migrated from https://github.com/mono/mono/commit/3fd79bd70e9949cf38e890b38319d6fb084e5ee8

--- src/mono/mono/utils/mono-threads-windows.c
+++ src/mono/mono/utils/mono-threads-windows.c
@@ -190,6 +190,7 @@ __readfsdword (unsigned long offset)
 void
 mono_threads_core_get_stack_bounds (guint8 **staddr, size_t *stsize)
 {
+	MEMORY_BASIC_INFORMATION meminfo;
 #ifdef TARGET_AMD64
 	/* win7 apis */
 	NT_TIB* tib = (NT_TIB*)NtCurrentTeb();
@@ -201,9 +202,17 @@ mono_threads_core_get_stack_bounds (guint8 **staddr, size_t *stsize)
 	guint8 *stackTop = (guint8*)*(int*)((char*)tib + 4);
 	guint8 *stackBottom = (guint8*)*(int*)((char*)tib + 8);
 #endif
+	/*
+	Windows stacks are expanded on demand, one page at time. The TIB reports
+	only the currently allocated amount.
+	VirtualQuery will return the actual limit for the bottom, which is what we want.
+	*/
+	if (VirtualQuery (&meminfo, &meminfo, sizeof (meminfo)) == sizeof (meminfo))
+		stackBottom = MIN (stackBottom, (guint8*)meminfo.AllocationBase);
 
 	*staddr = stackBottom;
 	*stsize = stackTop - stackBottom;
+
 }
 
 gboolean
