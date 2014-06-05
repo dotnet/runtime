d29099c2e2e1e9ea27b49924e49fd33b50adf4b2
Joao Matos <joao.matos@xamarin.com>
2014-06-05 15:39:40 +0100

    Fixed warning in MSVC: 'different types for formal and actual parameter'.
    Commit migrated from https://github.com/mono/mono/commit/623e43dcd6f28da4e486ea50e4120925039bae34

--- src/mono/mono/metadata/icall.c
+++ src/mono/mono/metadata/icall.c
@@ -6731,7 +6731,7 @@ ICALL_EXPORT void
 ves_icall_System_Environment_BroadcastSettingChange (void)
 {
 #ifdef HOST_WIN32
-	SendMessageTimeout (HWND_BROADCAST, WM_SETTINGCHANGE, NULL, L"Environment", SMTO_ABORTIFHUNG, 2000, 0);
+	SendMessageTimeout (HWND_BROADCAST, WM_SETTINGCHANGE, (WPARAM)NULL, (LPARAM)L"Environment", SMTO_ABORTIFHUNG, 2000, 0);
 #endif
 }
 
