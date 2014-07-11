91c542a0fee8824889d6e15a12017dd7f059f909
Zoltan Varga <vargaz@gmail.com>
2014-07-11 21:20:56 +0200

    Fix a warning.
    Commit migrated from https://github.com/mono/mono/commit/f9016dc075c0a8d958a268f6268c438248d9316b

--- src/mono/mono/mini/mini-cross-helpers.c
+++ src/mono/mono/mini/mini-cross-helpers.c
@@ -3,6 +3,9 @@
 #ifdef ENABLE_EXTENSION_MODULE
 #include "../../../mono-extensions/mono/mini/mini-cross-helpers.c"
 #else
+
+void mono_cross_helpers_run (void);
+
 void
 mono_cross_helpers_run (void)
 {
