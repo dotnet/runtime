63c8ac80533617f0797c3744fb97cabef0be3c20
Mark Probst <mark.probst@gmail.com>
2014-08-29 14:58:12 -0700

    [sgen] Also prefetch the (potential) block header.
    When we scan a small object in the major heap we have to read the
    block header of its block.  It makes sense to prefetch that as well,
    in addition to the object itself.
    
    
    Commit migrated from https://github.com/mono/mono/commit/32d7a0a24cfc2ded841c771a94c588c839e1e7be

--- src/mono/mono/metadata/sgen-gray.h
+++ src/mono/mono/metadata/sgen-gray.h
@@ -237,6 +237,9 @@ GRAY_OBJECT_DEQUEUE (SgenGrayQueue *queue, char** obj, mword *desc)
 #endif
 }
 
+#define GRAY_MS_BLOCK_SIZE	(16*1024)
+#define GRAY_MS_BLOCK_DATA_FOR_OBJ(o)	((char*)((mword)(o) & ~(mword)(GRAY_MS_BLOCK_SIZE - 1)))
+
 static inline void
 sgen_gray_object_dequeue_fast (SgenGrayQueue *queue, char** obj, mword *desc) {
 	GrayQueueEntry *cursor = queue->prefetch_cursor;
@@ -248,6 +251,7 @@ sgen_gray_object_dequeue_fast (SgenGrayQueue *queue, char** obj, mword *desc) {
 #else
 	GRAY_OBJECT_DEQUEUE (queue, &cursor->obj, NULL);
 #endif
+	PREFETCH (GRAY_MS_BLOCK_DATA_FOR_OBJ (cursor->obj));
 	PREFETCH (cursor->obj);
 	++cursor;
 	if (cursor == end)
