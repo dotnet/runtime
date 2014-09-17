a762af188945c74f790dbea5ca9aed32bb3f2bc0
Marcos Henrich <marcos.henrich@xamarin.com>
2014-09-17 19:48:43 +0100

    Added to StackFrame a field for the method address. Incremented MONO_CORLIB_VERSION.
    Commit migrated from https://github.com/mono/mono/commit/20523739f8c605e6eb5268f119718df2456781f8

--- src/mono/mono/mini/mini-exceptions.c
+++ src/mono/mono/mini/mini-exceptions.c
@@ -690,6 +690,7 @@ ves_icall_get_trace (MonoException *exc, gint32 skip, MonoBoolean need_file_info
 		}
 		else
 			MONO_OBJECT_SETREF (sf, method, mono_method_get_object (domain, method, NULL));
+		sf->method_address = (gint64) ji->code_start;
 		sf->native_offset = (char *)ip - (char *)ji->code_start;
 
 		/*
