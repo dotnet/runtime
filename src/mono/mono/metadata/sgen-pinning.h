5a7954cd9e1e60c673ed560c7a57a7d493a0fb71
Mark Probst <mark.probst@gmail.com>
2014-10-29 16:36:02 -0700

    [sgen] Don't store pin queue addresses, only indexes.  Fixes mono/mono#24084.
    The pin queue can get relocated as a side-effect of resizing when late
    pinning happens, so we can't count on pointers into it to remain valid.
    
    
    Commit migrated from https://github.com/mono/mono/commit/5eec4d0c848adfd73174016b2e681329aa6a5840

--- src/mono/mono/metadata/sgen-pinning.h
+++ src/mono/mono/metadata/sgen-pinning.h
@@ -38,6 +38,11 @@ void sgen_pinning_trim_queue_to_section (GCMemSection *section) MONO_INTERNAL;
 
 void sgen_dump_pin_queue (void) MONO_INTERNAL;
 
+gboolean sgen_find_optimized_pin_queue_area (void *start, void *end, size_t *first_out, size_t *last_out) MONO_INTERNAL;
+void sgen_find_section_pin_queue_start_end (GCMemSection *section) MONO_INTERNAL;
+void** sgen_pinning_get_entry (size_t index) MONO_INTERNAL;
+void sgen_pin_objects_in_section (GCMemSection *section, ScanCopyContext ctx) MONO_INTERNAL;
+
 /* Pinning stats */
 
 void sgen_pin_stats_register_address (char *addr, int pin_type) MONO_INTERNAL;
