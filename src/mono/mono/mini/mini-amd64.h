bedbc594f395e97f524680311a3d6ab1a5a0e7b0
Zoltan Varga <vargaz@gmail.com>
2014-11-07 15:33:48 -0500

    [amd64] Make xmm6:xmm15 callee saved on winx64 as required by the ABI.
    Commit migrated from https://github.com/mono/mono/commit/e19f539121e0b78b778aa8cf5711ccc1876eb443

--- src/mono/mono/mini/mini-amd64.h
+++ src/mono/mono/mini/mini-amd64.h
@@ -123,8 +123,14 @@ struct sigcontext {
 #define MONO_ARCH_FP_RETURN_REG AMD64_XMM0
 
 /* xmm15 is reserved for use by some opcodes */
+#ifdef TARGET_WIN32
+#define MONO_ARCH_CALLEE_FREGS 0x3f
+/* xmm6:xmm15 */
+#define MONO_ARCH_CALLEE_SAVED_FREGS (0x7fff - 0x3f)
+#else
 #define MONO_ARCH_CALLEE_FREGS 0x7fff
 #define MONO_ARCH_CALLEE_SAVED_FREGS 0
+#endif
 
 #define MONO_MAX_XREGS MONO_MAX_FREGS
 
