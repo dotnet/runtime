7ebeb1c61a14bada9f563b994a71ef19cf7d2cb5
Jo Shields <jo.shields@xamarin.com>
2014-11-25 12:23:20 +0000

    [msvc] Fix MONO_INTERNAL undefined in Boehm
    Commit migrated from https://github.com/mono/mono/commit/9f6f6a9ccef9e65adc281b5c0609f79d1dce6fcf

--- src/mono/mono/metadata/sgen-pointer-queue.c
+++ src/mono/mono/metadata/sgen-pointer-queue.c
@@ -17,6 +17,7 @@
  * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
+#include "utils/mono-compiler.h"
 #include "metadata/sgen-gc.h"
 #include "metadata/sgen-pointer-queue.h"
 
