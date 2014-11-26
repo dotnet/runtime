6dbbbafc13cfc35d1bdf036d7e621c90b1a4d26a
Jo Shields <jo.shields@xamarin.com>
2014-11-26 10:46:17 +0000

    [msvc] duplicate mono/mono@1d6b8a75fa31d60e9ddbc0b9eddbf05ed3fb6c12 for eglib
    Commit migrated from https://github.com/mono/mono/commit/9bdd77969a8f9057f3b46994135d39adb3ef216f

--- src/mono/msvc/winsetup.bat
+++ src/mono/msvc/winsetup.bat
@@ -1,7 +1,9 @@
 @echo off
 cd ..
 if exist config.h if not exist cygconfig.h copy config.h cygconfig.h
+if exist eglib\config.h if not exist eglib\cygconfig.h copy eglib\config.h eglib\cygconfig.h
 copy winconfig.h config.h
+copy eglib\winconfig.h eglib\config.h
 goto end
 :error
 echo fatal error: the VSDepenancies directory was not found in the "mono" directory
