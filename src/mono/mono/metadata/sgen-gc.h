76db66e53449d6c7dbaa7545343a8a2edf3e242d
Alexis Christoforides <alexis@thenull.net>
2014-12-05 15:48:21 -0500

    [runtime] Replace offsetof with G_STRUCT_OFFSET on commit's @mono/mono@b0cff959e47c3d9955e6675364bf71404cd74bac changes
    Commit migrated from https://github.com/mono/mono/commit/fdf9cf74a6a29604145d857fe5b4216066d085bf

--- src/mono/mono/metadata/sgen-gc.h
+++ src/mono/mono/metadata/sgen-gc.h
@@ -765,7 +765,7 @@ slow_object_get_size (MonoVTable *vtable, MonoObject* o)
 	 * mono_array_length_fast not using the object's vtable.
 	 */
 	if (klass == mono_defaults.string_class) {
-		return offsetof (MonoString, chars) + 2 * mono_string_length_fast ((MonoString*) o) + 2;
+		return G_STRUCT_OFFSET (MonoString, chars) + 2 * mono_string_length_fast ((MonoString*) o) + 2;
 	} else if (klass->rank) {
 		MonoArray *array = (MonoArray*)o;
 		size_t size = sizeof (MonoArray) + klass->sizes.element_size * mono_array_length_fast (array);
@@ -818,7 +818,7 @@ sgen_par_object_get_size (MonoVTable *vtable, MonoObject* o)
 		SGEN_ASSERT (9, size >= sizeof (MonoObject), "Run length object size to small");
 		return size;
 	} else if (descr == SGEN_DESC_STRING) {
-		return offsetof (MonoString, chars) + 2 * mono_string_length_fast ((MonoString*) o) + 2;
+		return G_STRUCT_OFFSET (MonoString, chars) + 2 * mono_string_length_fast ((MonoString*) o) + 2;
 	} else if (type == DESC_TYPE_VECTOR) {
 		int element_size = ((descr) >> VECTOR_ELSIZE_SHIFT) & MAX_ELEMENT_SIZE;
 		MonoArray *array = (MonoArray*)o;
--- src/mono/mono/metadata/string-icalls.c
+++ src/mono/mono/metadata/string-icalls.c
@@ -65,7 +65,7 @@ ves_icall_System_String_GetLOSLimit (void)
 {
 	int limit = mono_gc_get_los_limit ();
 
-	return (limit - 2 - offsetof (MonoString, chars)) / 2;
+	return (limit - 2 - G_STRUCT_OFFSET (MonoString, chars)) / 2;
 }
 
 void
