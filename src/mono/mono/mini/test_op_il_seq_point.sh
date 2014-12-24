73b0ebabfbdaeacf71ab5f4c58e92972c655d575
Marcos Henrich <marcos.henrich@xamarin.com>
2014-12-24 13:37:45 +0000

    [runtime] Improved test_op_il_seq_point.sh test_op_il_seq_point.sh now compares methods generated native code.
    Commit migrated from https://github.com/mono/mono/commit/6719c94da501450e44a703763101b23221c5c7bf

--- src/mono/mono/mini/test_op_il_seq_point.sh
+++ src/mono/mono/mini/test_op_il_seq_point.sh
@@ -2,35 +2,62 @@
 
  TESTFILE=$1
 
-get_code_length () {
-    ./mono -v $1 | grep 'code length' | sed 's/0x[0-9a-f]*/0x/g' | sed -E 's/Method (\(.*\) )?//g' | sort
+get_methods () {
+    ./mono -v -v $1 | grep '^Method \|^000' | sed 's/emitted[^()]*//' | sed 's/0x[0-9a-fA-F]*/0x0/g' | awk -v RS='' '{gsub(/\n000/, "000"); print}' | sort
 }
 
-sdiff_code_length () {
-    sdiff -s -w 1000 <(echo "$(get_code_length $1)") <(echo "$(MONO_DEBUG=gen-compact-seq-points  get_code_length $1)")
+get_method () {
+	MONO_VERBOSE_METHOD="$2" ./mono $1 | grep '^Method \|^000' | sed 's/emitted[^()]*//' | sed 's/0x[0-9a-fA-F]*/0x0/g'
+}
+
+sdiff_methods () {
+    sdiff -s -w 1000 <(echo "$(get_methods $1)") <(echo "$(MONO_DEBUG=gen-compact-seq-points  get_methods $1)")
+}
+
+diff_method () {
+	diff <(echo "$(get_method $1 $2)") <(echo "$(MONO_DEBUG=gen-compact-seq-points  get_method $1 $2)")
+}
+
+get_method_name () {
+	echo $1 | sed -E 's/Method (\([^)]*\) )?([^ ]*).*/\2/g'
+}
+
+get_method_length () {
+	echo $1 | sed 's/.*code length \([0-9]*\).*/\1/'
 }
 
 DIFF_FILE=mktemp
 
-echo "$(sdiff_code_length $TESTFILE)" > $DIFF_FILE
+echo "$(sdiff_methods $TESTFILE)" > $DIFF_FILE
 
 CHANGES=0
+METHOD=""
+MIN_SIZE=10000
 
 while read line; do
 	if [ "$line" != "" ]; then
+		echo $line | sed 's/000.*//g'
 		CHANGES=$((CHANGES+1))
-	    echo $line
+		SIZE=$(get_method_length "$line")
+		if [[ SIZE -lt MIN_SIZE ]]; then
+			MIN_SIZE=$SIZE
+			METHOD="$line"
+		fi
 	fi
 done < $DIFF_FILE
 
 if [ $CHANGES != 0 ]
 then
+	METHOD_NAME=$(get_method_name "$METHOD")
+
+	echo "$(diff_method $TESTFILE $METHOD_NAME)"
+
 	echo ''
 	echo "Detected OP_IL_SEQ_POINT incompatibility on $TESTFILE"
-	echo "  $CHANGES methods differ in size when sequence points are enabled."
+	echo "  $CHANGES methods differ when sequence points are enabled."
 	echo '  This is probably caused by a runtime optimization that is not handling OP_IL_SEQ_POINT'
-	echo '  More details can be obtained by comparing the output of the following commands:'
-	echo "    MONO_VERBOSE_METHOD=testname mono/mini/mono $TESTFILE"
-	echo "    MONO_DEBUG=gen-compact-seq-points #MONO_VERBOSE_METHOD=testname mono/mini/mono $TESTFILE"
+	echo '  Differences can be obtained by comparing the output of the following commands:'
+	echo "    MONO_VERBOSE_METHOD=\"$METHOD_NAME\" mono/mini/mono mono/mini/$TESTFILE"
+	echo "    MONO_DEBUG=gen-compact-seq-points MONO_VERBOSE_METHOD=\"$METHOD_NAME\" mono/mini/mono mono/mini/$TESTFILE"
 	exit 1
 fi
