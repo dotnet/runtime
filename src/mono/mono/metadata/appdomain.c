a2a06cddad42cc7e9fc7f6d89fcbcbabd9236808
Marcos Henrich <marcos.henrich@xamarin.com>
2015-01-23 15:44:22 +0000

    [corlib] Changed some MonoIO methods to use SafeHandle.
    Commit migrated from https://github.com/mono/mono/commit/753229c72dd9f2b63beb097700e350164b8807c2

--- src/mono/mono/metadata/appdomain.c
+++ src/mono/mono/metadata/appdomain.c
@@ -78,7 +78,7 @@
  * Changes which are already detected at runtime, like the addition
  * of icalls, do not require an increment.
  */
-#define MONO_CORLIB_VERSION 114
+#define MONO_CORLIB_VERSION 115
 
 typedef struct
 {
