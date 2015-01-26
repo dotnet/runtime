4766071f64c923835e1996920d0c42ca69399286
João Matos <joao@tritao.eu>
2015-01-26 14:34:45 +0000

    [runtime] Fixed the iOS build.
    Failure caused by https://github.com/mono/mono/commit/mono/mono@2f1b464beb14a1b6b6099a03fc332493293bef35.
    
    ```
    ../../../../../mono/mono/utils/mono-proclib.c:104:8: error: use of undeclared identifier 'errno'
                            if (errno != ENOMEM)
                                ^
    ../../../../../mono/mono/utils/mono-proclib.c:104:17: error: use of undeclared identifier 'ENOMEM'
                            if (errno != ENOMEM)
    ```
    
    
    Commit migrated from https://github.com/mono/mono/commit/20eb41f97a6dcfeb4b738e0efdc9ffc1b9239c05

--- src/mono/mono/utils/mono-proclib.c
+++ src/mono/mono/utils/mono-proclib.c
@@ -20,6 +20,7 @@
 #endif
 
 #if defined(__APPLE__) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
+#include <sys/errno.h>
 #include <sys/param.h>
 #include <sys/types.h>
 #include <sys/sysctl.h>
