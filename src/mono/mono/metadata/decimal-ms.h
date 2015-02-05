043af99ca6f0af6dcdc724b343a46ddbbf46f316
Miguel de Icaza <miguel@gnome.org>
2015-02-04 21:45:03 -0500

    [decimal] Microsoft's unmanaged code for decimal support
    This is a port of the CoreCLR Decimal support to C and to the Mono
    coding conventions.  In the process of porting the code, I removed a
    bunch of COM-isms and some Windows-isms that were just workaround with
    compatibility typedefs and replaced it instead with stdint.h data
    types as well as Mono assertions.
    
    There are still a few methods missing, and the managed library is not
    plugged yet (both constructors likely need to be altered to be managed
    proxies to the init method).
    
    
    Commit migrated from https://github.com/mono/mono/commit/5b2bb8c9017ab563d7ec819cf249b209c770423e

---
+++ src/mono/mono/metadata/decimal-ms.h
@@ -0,0 +1,58 @@
+#ifndef __MONO_DECIMAL_MS_H__
+#define __MONO_DECIMAL_MS_H__
+typedef struct tagDEC {
+    // Decimal.cs treats the first two shorts as one long
+    // And they seriable the data so we need to little endian
+    // seriliazation
+    // The wReserved overlaps with Variant's vt member
+#if G_BYTE_ORDER != G_LITTLE_ENDIAN
+    union {
+        struct {
+            int8_t sign;
+            int8_t scale;
+        } u;
+        uint16_t signscale;
+    } u;
+    uint16_t wReserved;
+#else
+    uint16_t wReserved;
+    union {
+        struct {
+            int8_t scale;
+            int8_t sign;
+        } u;
+        uint16_t signscale;
+    } u;
+#endif
+    uint32_t Hi32;
+    union {
+        struct {
+            uint32_t Lo32;
+            uint32_t Mid32;
+        } v;
+        uint64_t Lo64;
+    } v;
+} MonoDecimal;
+
+typedef enum {
+	MONO_DECIMAL_CMP_EQ,
+	MONO_DECIMAL_CMP_LT,
+	MONO_DECIMAL_CMP_GT
+} MonoDecimalCompareResult;
+	
+MonoDecimalCompareResult
+        mono_decimal_compare (MonoDecimal *left, MonoDecimal *right) MONO_INTERNAL;
+
+void    mono_decimal_init_single   (MonoDecimal *_this, float value) MONO_INTERNAL;
+void    mono_decimal_init_double   (MonoDecimal *_this, double value) MONO_INTERNAL;
+void    mono_decimal_floor         (MonoDecimal *d) MONO_INTERNAL;
+int32_t mono_decimal_get_hash_code (MonoDecimal *d) MONO_INTERNAL;
+void    mono_decimal_multiply      (MonoDecimal *d1, MonoDecimal *d2) MONO_INTERNAL;
+void    mono_decimal_round         (MonoDecimal *d, int32_t decimals) MONO_INTERNAL;
+void    mono_decimal_tocurrency    (MonoDecimal *decimal) MONO_INTERNAL;
+double  mono_decimal_to_double     (MonoDecimal d) MONO_INTERNAL;
+int32_t mono_decimal_to_int32      (MonoDecimal d) MONO_INTERNAL;
+float   mono_decimal_to_float      (MonoDecimal d) MONO_INTERNAL;
+void    mono_decimal_truncate      (MonoDecimal *d) MONO_INTERNAL;
+
+#endif
