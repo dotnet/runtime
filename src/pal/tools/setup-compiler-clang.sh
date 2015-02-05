2e63a8004caf9ab9d85a7dee9867c8bd84eb7882
Pat Gavlin <pagavlin@microsoft.com>
2015-02-04 16:59:45 -0800

    Build fixes for clang3.7 and systems without stropt.h.
    - Fix setup-compiler-clang to detect binaries outside of /usr/bin
    - Fix ambiguously-sized cmp instructions when comparing g_GCShadow and 0
    - Remove references to stropt.h. None of the defiinitions in this header
      are used by the product, and this header is not present on certain
      systems.

--- src/pal/tools/setup-compiler-clang.sh
+++ src/pal/tools/setup-compiler-clang.sh
@@ -3,5 +3,5 @@
 # This file sets the environment to be used for building with clang.
 #
 
-export CC=/usr/bin/clang
-export CXX=/usr/bin/clang++
+export CC=$(which clang)
+export CXX=$(which clang++)
