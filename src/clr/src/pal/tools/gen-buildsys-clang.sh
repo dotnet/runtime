ac373f6fad012a564e60e1bfc072681858f3c178
Kasper F. Brandt <poizan@poizan.dk>
2015-02-12 16:59:43 +0100

    Fix build on paths with spaces and with llvm path with spaces. Fixes dotnet/coreclr#216
    Commit migrated from https://github.com/dotnet/coreclr/commit/91f7e0e26ee014df27ea34860b0adc324d1ef33e

--- src/clr/src/pal/tools/gen-buildsys-clang.sh
+++ src/clr/src/pal/tools/gen-buildsys-clang.sh
@@ -12,7 +12,7 @@ then
   exit 1
 fi
 
-. $1/src/pal/tools/setup-compiler-clang.sh
+. "$1/src/pal/tools/setup-compiler-clang.sh"
 
 # Possible build types are DEBUG, RELEASE, RELWITHDEBINFO, MINSIZEREL.
 # Default to DEBUG
@@ -21,7 +21,7 @@ then
   echo "Defaulting to DEBUG build."
   buildtype="DEBUG"
 else
-  buildtype=$2
+  buildtype="$2"
 fi
 
 OS=`uname`
@@ -42,35 +42,35 @@ fi
 
 desired_llvm_version=3.5
 locate_llvm_exec() {
-  if which $llvm_prefix$1-$desired_llvm_version > /dev/null 2>&1
+  if which "$llvm_prefix$1-$desired_llvm_version" > /dev/null 2>&1
   then
-    echo $(which $llvm_prefix$1-$desired_llvm_version)
-  elif which $1 > /dev/null 2>&1
+    echo "$(which $llvm_prefix$1-$desired_llvm_version)"
+  elif which "$1" > /dev/null 2>&1
   then
-    echo $(which $1)
+    echo "$(which $1)"
   else
     exit 1
   fi
 }
-llvm_ar=$(locate_llvm_exec ar)
+llvm_ar="$(locate_llvm_exec ar)"
 [[ $? -eq 0 ]] || { echo "Unable to locate llvm-ar"; exit 1; }
-llvm_link=$(locate_llvm_exec link)
+llvm_link="$(locate_llvm_exec link)"
 [[ $? -eq 0 ]] || { echo "Unable to locate llvm-link"; exit 1; }
-llvm_nm=$(locate_llvm_exec nm)
+llvm_nm="$(locate_llvm_exec nm)"
 [[ $? -eq 0 ]] || { echo "Unable to locate llvm-nm"; exit 1; }
-llvm_ranlib=$(locate_llvm_exec ranlib)
+llvm_ranlib="$(locate_llvm_exec ranlib)"
 [[ $? -eq 0 ]] || { echo "Unable to locate llvm-ranlib"; exit 1; }
 if [ $OS = "Linux" ]; then
-  llvm_objdump=$(locate_llvm_exec objdump)
+  llvm_objdump="$(locate_llvm_exec objdump)"
   [[ $? -eq 0 ]] || { echo "Unable to locate llvm-objdump"; exit 1; }
 fi
 
-cmake -DCMAKE_USER_MAKE_RULES_OVERRIDE=$1/src/pal/tools/clang-compiler-override.txt \
-  -DCMAKE_AR=$llvm_ar \
-  -DCMAKE_LINKER=$llvm_link \
-  -DCMAKE_NM=$llvm_nm \
-  -DCMAKE_OBJDUMP=$llvm_objdump \
-  -DCMAKE_RANLIB=$llvm_ranlib \
-  -DCMAKE_BUILD_TYPE=$buildtype \
-  $1
+cmake "-DCMAKE_USER_MAKE_RULES_OVERRIDE=$1/src/pal/tools/clang-compiler-override.txt" \
+  "-DCMAKE_AR=$llvm_ar" \
+  "-DCMAKE_LINKER=$llvm_link" \
+  "-DCMAKE_NM=$llvm_nm" \
+  "-DCMAKE_OBJDUMP=$llvm_objdump" \
+  "-DCMAKE_RANLIB=$llvm_ranlib" \
+  "-DCMAKE_BUILD_TYPE=$buildtype" \
+  "$1"
 
