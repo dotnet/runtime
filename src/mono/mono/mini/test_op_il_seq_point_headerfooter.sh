4b1d77edf86fbb898b4d2a3e26e9d0931fa2c1cc
Jo Shields <jo.shields@xamarin.com>
2015-02-18 18:29:01 +0000

    Just avoid BSD sed altogether.
    Commit migrated from https://github.com/mono/mono/commit/9f5a7ae769d7c0293f404c14c08f1db9c5df0f2a

--- src/mono/mono/mini/test_op_il_seq_point_headerfooter.sh
+++ src/mono/mono/mini/test_op_il_seq_point_headerfooter.sh
@@ -23,14 +23,18 @@ echo "    </results>" >> TestResults_op_il_seq_point.xml
 echo "  </test-suite>" >> TestResults_op_il_seq_point.xml
 echo "</test-results>" >> TestResults_op_il_seq_point.xml
 
-sed -i .bak -e "1i\ \ \ \ \ \ \ \ \ \ \ \ <results>" TestResults_op_il_seq_point.xml
-sed -i .bak -e "1i\ \ \ \ \ \ \ \ \ \ <test-suite name=\"op_il_seq_point\" success=\"${PASS}\" time=\"0\" asserts=\"0\">" TestResults_op_il_seq_point.xml
-sed -i .bak -e "1i\ \ \ \ \ \ \ \ <results>" TestResults_op_il_seq_point.xml
-sed -i .bak -e "1i\ \ \ \ \ \ <test-suite name=\"MonoTests\" success=\"${PASS}\" time=\"0\" asserts=\"0\">" TestResults_op_il_seq_point.xml
-sed -i .bak -e "1i\ \ \ \ <results>" TestResults_op_il_seq_point.xml
-sed -i .bak -e "1i\ \ <test-suite name=\"op_il_seq_point-tests.dummy\" success=\"${PASS}\" time=\"0\" asserts=\"0\">" TestResults_op_il_seq_point.xml
-sed -i .bak -e "1i\ \ <culture-i .baknfo current-culture=\"${MYLOCALE}\" current-uiculture=\"${MYLOCALE}\" />" TestResults_op_il_seq_point.xml
-sed -i .bak -e "1i\ \ <environment nunit-version=\"2.4.8.0\" clr-version=\"4.0.30319.17020\" os-version=\"Unix ${MYUNAME}\" platform=\"Unix\" cwd=\"${PWD}\" machine-name=\"${MYHOSTNAME}\" user=\"${USER}\" user-domain=\"${MYFQDN}\" />" TestResults_op_il_seq_point.xml
-sed -i .bak -e "1i<test-results name=\"regression-tests.dummy\" total=\"${TOTAL}\" failures=\"${FAILURES}\" not-run=\"0\" date=\"${MYDATE}\" time=\"${MYTIME}\">" TestResults_op_il_seq_point.xml
-sed -i .bak -e "1i<!--This file represents the results of running a test suite-->" TestResults_op_il_seq_point.xml
-sed -i .bak -e "1i<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"no\"?>" TestResults_op_il_seq_point.xml
+echo "<?xml version=\"1.0\" encoding=\"utf-8\" standalone=\"no\"?>" > TestResults_op_il_seq_point.xml.header
+echo "<!--This file represents the results of running a test suite-->" >> TestResults_op_il_seq_point.xml.header
+echo "<test-results name=\"regression-tests.dummy\" total=\"${TOTAL}\" failures=\"${FAILURES}\" not-run=\"0\" date=\"${MYDATE}\" time=\"${MYTIME}\">" >> TestResults_op_il_seq_point.xml.header
+echo "  <environment nunit-version=\"2.4.8.0\" clr-version=\"4.0.30319.17020\" os-version=\"Unix ${MYUNAME}\" platform=\"Unix\" cwd=\"${PWD}\" machine-name=\"${MYHOSTNAME}\" user=\"${USER}\" user-domain=\"${MYFQDN}\" />" >> TestResults_op_il_seq_point.xml.header
+echo "  <culture-info current-culture=\"${MYLOCALE}\" current-uiculture=\"${MYLOCALE}\" />" >> TestResults_op_il_seq_point.xml.header
+echo "  <test-suite name=\"op_il_seq_point-tests.dummy\" success=\"${PASS}\" time=\"0\" asserts=\"0\">" >> TestResults_op_il_seq_point.xml.header
+echo "    <results>" >> TestResults_op_il_seq_point.xml.header
+echo "      <test-suite name=\"MonoTests\" success=\"${PASS}\" time=\"0\" asserts=\"0\">" >> TestResults_op_il_seq_point.xml.header
+echo "        <results>" >> TestResults_op_il_seq_point.xml.header
+echo "          <test-suite name=\"op_il_seq_point\" success=\"${PASS}\" time=\"0\" asserts=\"0\">" >> TestResults_op_il_seq_point.xml.header
+echo "            <results>" >> TestResults_op_il_seq_point.xml.header
+
+cat TestResults_op_il_seq_point.xml.header TestResults_op_il_seq_point.xml > TestResults_op_il_seq_point.xml.new
+mv TestResults_op_il_seq_point.xml.new TestResults_op_il_seq_point.xml
+rm -f TestResults_op_il_seq_point.xml.header
