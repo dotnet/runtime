2f56ddec6bb86600b8a6ff1970959484888c13f9
Neale Ferguson <neale@sinenomine.net>
2015-04-07 12:15:43 -0400

    decimal-ms.c: - Fix handling of lo64 on big endian systems (fixes 28672)
    mono-endian.h:
    - Add optimization for s390x read64/read32/read16 operations
    
    exceptions-s390x.c:
    - Add profiling support for exception handler
    
    
    Commit migrated from https://github.com/mono/mono/commit/f6294778fe4f5a90bd7b7b592bd8fa359d299e1e

--- src/mono/mono/mini/exceptions-s390x.c
+++ src/mono/mono/mini/exceptions-s390x.c
@@ -210,6 +210,9 @@ mono_arch_get_call_filter (MonoTrampInfo **info, gboolean aot)
 
 	g_assert ((code - start) < SZ_THROW); 
 
+	mono_arch_flush_icache(start, code - start);
+	mono_profiler_code_buffer_new (start, code - start, MONO_PROFILER_CODE_BUFFER_EXCEPTION_HANDLING, NULL);
+
 	if (info)
 		*info = mono_tramp_info_create ("call_filter",
 						start, code - start, ji,
@@ -356,6 +359,9 @@ mono_arch_get_throw_exception_generic (int size, MonoTrampInfo **info,
 	/* we should never reach this breakpoint */
 	s390_break (code);
 	g_assert ((code - start) < size);
+
+	mono_arch_flush_icache (start, code - start);
+	mono_profiler_code_buffer_new (start, code - start, MONO_PROFILER_CODE_BUFFER_EXCEPTION_HANDLING, NULL);
 
 	if (info)
 		*info = mono_tramp_info_create (corlib ? "throw_corlib_exception" 
