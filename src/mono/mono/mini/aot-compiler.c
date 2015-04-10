719b6827c5ec7f8627f86a21ed327622c4154736
Zoltan Varga <vargaz@gmail.com>
2015-04-10 11:12:58 -0400

    [aot] Avoid duplicate plt symbols for plt entries for synchronized methods. Fixes mono/mono#28961.
    Commit migrated from https://github.com/mono/mono/commit/2a96a023512a937dd16ca23ef6f3ff2b62ab3560

--- src/mono/mono/mini/aot-compiler.c
+++ src/mono/mono/mini/aot-compiler.c
@@ -3093,6 +3093,8 @@ static MonoPltEntry*
 get_plt_entry (MonoAotCompile *acfg, MonoJumpInfo *patch_info)
 {
 	MonoPltEntry *res;
+	gboolean synchronized = FALSE;
+	static int synchronized_symbol_idx;
 
 	if (!is_plt_patch (patch_info))
 		return NULL;
@@ -3108,6 +3110,7 @@ get_plt_entry (MonoAotCompile *acfg, MonoJumpInfo *patch_info)
 		 * wrapper.
 		 */
 		res = NULL;
+		synchronized = TRUE;
 	}
 
 	if (!res) {
@@ -3121,6 +3124,13 @@ get_plt_entry (MonoAotCompile *acfg, MonoJumpInfo *patch_info)
 		res->symbol = get_plt_symbol (acfg, res->plt_offset, patch_info);
 		if (acfg->aot_opts.write_symbols)
 			res->debug_sym = get_plt_entry_debug_sym (acfg, res->ji, acfg->plt_entry_debug_sym_cache);
+		if (synchronized) {
+			/* Avoid duplicate symbols because we don't cache */
+			res->symbol = g_strdup_printf ("%s_%d", res->symbol, synchronized_symbol_idx);
+			if (res->debug_sym)
+				res->debug_sym = g_strdup_printf ("%s_%d", res->debug_sym, synchronized_symbol_idx);
+			synchronized_symbol_idx ++;
+		}
 		if (res->debug_sym)
 			res->llvm_symbol = g_strdup_printf ("%s_%s_llvm", res->symbol, res->debug_sym);
 		else
