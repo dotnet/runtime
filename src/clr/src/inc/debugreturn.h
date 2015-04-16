1d0e073ba8d81f172804d8d2182e0b841dc2d892
Jan Vorlicek <janvorli@microsoft.com>
2015-04-16 23:29:48 +0200

    Fix dangling else warnings
    This change fixes all dangling else warnings and removes the
    compiler flag that was disabling their reporting.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/d067efd6c54ff66cca85b96eefbcdcc3bad4ecb4

--- src/clr/src/inc/debugreturn.h
+++ src/clr/src/inc/debugreturn.h
@@ -93,7 +93,7 @@ typedef __SafeToReturn __ReturnOK;
 // The call is dead, and does not appear in the generated code, even in a checked
 // build.  (And, in fastchecked, there is no penalty at all.)
 //
-#define return if (0 && __ReturnOK::safe_to_return()) { } else return
+#define return for (;1;__ReturnOK::safe_to_return()) return
 
 #define DEBUG_ASSURE_NO_RETURN_BEGIN(arg) { typedef __YouCannotUseAReturnStatementHere __ReturnOK; if (0 && __ReturnOK::used()) { } else {
 #define DEBUG_ASSURE_NO_RETURN_END(arg)   } }
