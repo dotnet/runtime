905f814385984fd657b36835e4d4fe9d728e21d0
Dick van den Brink <d_vandenbrink@outlook.com>
2015-04-26 19:36:42 +0200

    Allow building with VS2015 installed
    Previous it was required to have VS2013 but now VS2015 can also be used. The default is still 2013 though.
    On a machine with both version installed it is possible to force VS2013/VS2015 with a parameter.
    
    Note: Still some work has to be done to make compilation successfull on VS2015.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/c574d1c69ca8a9cc9222e42ca66eedcd6af3e9de

--- src/clr/src/pal/tools/gen-buildsys-win.bat
+++ src/clr/src/pal/tools/gen-buildsys-win.bat
@@ -5,7 +5,7 @@ rem This file invokes cmake and generates the build system for windows.
 set argC=0
 for %%x in (%*) do Set /A argC+=1
 
-if NOT %argC%==1 GOTO :USAGE
+if NOT %argC%==2 GOTO :USAGE
 if %1=="/?" GOTO :USAGE
 
 setlocal
@@ -15,20 +15,24 @@ set "basePath=%basePath:"=%"
 :: remove trailing slash
 if %basePath:~-1%==\ set "basePath=%basePath:~0,-1%"
 
+set __VSString=12 2013
+if /i "%2" == "vs2015" (set __VSString=14 2015)
+
 if defined CMakePath goto DoGen
 
 :: Eval the output from probe-win1.ps1
 for /f "delims=" %%a in ('powershell -NoProfile -ExecutionPolicy RemoteSigned "& .\probe-win.ps1"') do %%a
 
 :DoGen
-"%CMakePath%" "-DCMAKE_USER_MAKE_RULES_OVERRIDE=%basePath%\windows-compiler-override.txt" -G "Visual Studio 12 2013 Win64" %1
+"%CMakePath%" "-DCMAKE_USER_MAKE_RULES_OVERRIDE=%basePath%\windows-compiler-override.txt" -G "Visual Studio %__VSString% Win64" %1
 endlocal
 GOTO :DONE
 
 :USAGE
   echo "Usage..."
-  echo "gen-buildsys-win.bat <path to top level CMakeLists.txt>"
+  echo "gen-buildsys-win.bat <path to top level CMakeLists.txt> <VSVersion>"
   echo "Specify the path to the top level CMake file - <ProjectK>/src/NDP"
+  echo "Specify the VSVersion to be used - VS2013 or VS2015"
   EXIT /B 1
 
 :DONE
