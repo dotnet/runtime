3bbc34c803c7b7690049ceefe2accb8b623eed5b
Alex Rønne Petersen <alexrp@xamarin.com>
2015-04-28 16:18:05 +0200

    [utils] Add MONO_BITSET_FOREACH macro.
    Commit migrated from https://github.com/mono/mono/commit/e03d67b7652af2a4c6c5b51bb5fa3bbae2250de3

--- src/mono/mono/utils/monobitset.h
+++ src/mono/mono/utils/monobitset.h
@@ -38,12 +38,27 @@ enum {
 
 /* Fast access to bits which depends on the implementation of the bitset */
 #define mono_bitset_test_fast(set,n) ((set)->data [(n)/MONO_BITSET_BITS_PER_CHUNK] & ((gsize)1 << ((n) % MONO_BITSET_BITS_PER_CHUNK)))
-#define mono_bitset_test_fast(set,n) ((set)->data [(n)/MONO_BITSET_BITS_PER_CHUNK] & ((gsize)1 << ((n) % MONO_BITSET_BITS_PER_CHUNK)))
 #define mono_bitset_set_fast(set,n) do { (set)->data [(n)/MONO_BITSET_BITS_PER_CHUNK] |= ((gsize)1 << ((n) % MONO_BITSET_BITS_PER_CHUNK)); } while (0)
 #define mono_bitset_clear_fast(set,n) do { (set)->data [(n)/MONO_BITSET_BITS_PER_CHUNK] &= ~((gsize)1 << ((n) % MONO_BITSET_BITS_PER_CHUNK)); } while (0)
 #define mono_bitset_get_fast(set,n) ((set)->data[(n)])
 
 #define mono_bitset_copyto_fast(src,dest) do { memcpy (&(dest)->data, &(src)->data, (dest)->size / 8); } while (0)
+
+#define MONO_BITSET_FOREACH(set,idx,/*stmt*/...) \
+	do \
+	{ \
+		MonoBitSet *set__ = (set); \
+		for (int i__ = 0; i__ < set__->size / MONO_BITSET_BITS_PER_CHUNK; i__++) { \
+			if (set__->data [i__]) { \
+				for (int j__ = 0; j__ < MONO_BITSET_BITS_PER_CHUNK; j__++) { \
+					if (set__->data [i__] & ((gsize) 1 << j__)) { \
+						guint idx = j__ + i__ * MONO_BITSET_BITS_PER_CHUNK; \
+						__VA_ARGS__; \
+					} \
+				} \
+			} \
+		} \
+	} while (0)
 
 #define mono_bitset_union_fast(dest,src) do { \
     MonoBitSet *tmp_src = (src); \
