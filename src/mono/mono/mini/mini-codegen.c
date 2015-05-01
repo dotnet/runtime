450ec88c3b7d501b9366b626c2721e62c678b613
Zoltan Varga <vargaz@gmail.com>
2015-05-01 01:33:00 -0400

    [jit] Merge regalloc.c into mini-codegen.c.
    Commit migrated from https://github.com/mono/mono/commit/a30442a7de01086c7cba7b2c72babc9c4bd1e2d5

--- src/mono/mono/mini/mini-codegen.c
+++ src/mono/mono/mini/mini-codegen.c
@@ -2773,4 +2773,23 @@ mini_type_is_hfa (MonoType *t, int *out_nfields, int *out_esize)
 	return TRUE;
 }
 
+MonoRegState*
+mono_regstate_new (void)
+{
+	MonoRegState* rs = g_new0 (MonoRegState, 1);
+
+	rs->next_vreg = MAX (MONO_MAX_IREGS, MONO_MAX_FREGS);
+#ifdef MONO_ARCH_NEED_SIMD_BANK
+	rs->next_vreg = MAX (rs->next_vreg, MONO_MAX_XREGS);
+#endif
+
+	return rs;
+}
+
+void
+mono_regstate_free (MonoRegState *rs) {
+	g_free (rs->vassign);
+	g_free (rs);
+}
+
 #endif /* DISABLE_JIT */
