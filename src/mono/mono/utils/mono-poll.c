bda9c7b9a773938623d99b4ab309238e55dc59b0
João Matos <joao@tritao.eu>
2015-05-06 15:03:58 +0100

    [runtime] Fixed warning by including <winsock2.h> before <windows.h>.
    Commit migrated from https://github.com/mono/mono/commit/03fa9201026bf9c30b63b74d8216d9e621489575

--- src/mono/mono/utils/mono-poll.c
+++ src/mono/mono/utils/mono-poll.c
@@ -1,3 +1,8 @@
+#ifdef HOST_WIN32
+/* For select */
+#include <winsock2.h>
+#endif
+
 #include "mono-poll.h"
 #include <errno.h>
 
@@ -19,11 +24,6 @@ mono_poll (mono_pollfd *ufds, unsigned int nfds, int timeout)
 	return poll (ufds, nfds, timeout);
 }
 #else
-
-#ifdef HOST_WIN32
-/* For select */
-#include <winsock2.h>
-#endif
 
 int
 mono_poll (mono_pollfd *ufds, unsigned int nfds, int timeout)
