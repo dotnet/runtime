9a3e68a370f0d0e8b9cc765cc86e1c33656dee57
João Matos <joao@tritao.eu>
2015-05-06 15:02:32 +0100

    [runtime] Fixed warning by only using #pragma warning on MSVC.
    Commit migrated from https://github.com/mono/mono/commit/44127d6c4cab78e6823d9ec6449f799d0af0e0a3

--- src/mono/mono/utils/dlmalloc.c
+++ src/mono/mono/utils/dlmalloc.c
@@ -1164,7 +1164,7 @@ int mspace_mallopt(int, int);
 
 /*------------------------------ internal #includes ---------------------- */
 
-#ifdef WIN32
+#ifdef _MSC_VER
 #pragma warning( disable : 4146 ) /* no "unsigned" warnings */
 #endif /* WIN32 */
 
