dc061636a77f85af99250d3d0b6cb3e2f691563c
João Matos <joao@tritao.eu>
2015-05-14 15:46:37 +0100

    [aot] Fixed linker invocation when cross-compiling to Android.
    Fixes an Android AOT regression introduced by https://github.com/mono/mono/commit/mono/mono@b41ed5db82ad82f0540eef70d821318836fba107.
    
    
    Commit migrated from https://github.com/mono/mono/commit/75175b2f261262b9369b1e1c505a7b5be193a331

--- src/mono/mono/mini/aot-compiler.c
+++ src/mono/mono/mini/aot-compiler.c
@@ -8844,7 +8844,7 @@ compile_asm (MonoAotCompile *acfg)
 #define LD_NAME "gcc -shared --dll"
 #elif defined(TARGET_X86) && defined(TARGET_MACH) && !defined(__native_client_codegen__)
 #define LD_NAME "clang -m32 -dynamiclib"
-#elif defined(TARGET_ARM)
+#elif defined(TARGET_ARM) && !defined(TARGET_ANDROID)
 #define LD_NAME "gcc --shared"
 #endif
 
