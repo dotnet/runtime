6e2b9927f7d449e19243f27058f137b945ebb924
Pat Gavlin <pagavlin@microsoft.com>
2015-05-26 17:41:38 -0700

    Import JIT tests.
    This imports the remainder of the tests under JIT/jit64 that are able
    to target CoreCLR.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/b1d65ae5f855952aab37c39923ca5d5138ea09db

---
+++ src/coreclr/tests/src/JIT/jit64/opt/osr/osr001.cs
@@ -0,0 +1,55 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+public class TestCase
+{
+    private const int ARRAY_MULTIPLIER = 3;
+    private const int ARRAY_UNITSIZE = 10;
+    private const int ARRAY_SIZE = ARRAY_UNITSIZE * ARRAY_MULTIPLIER;
+
+    public static int Main(String[] real_args)
+    {
+        int rc = 0;
+        int[] array = new int[ARRAY_SIZE];
+        int j;
+        int i = ARRAY_MULTIPLIER - 1;
+
+        while (i >= 0)
+        {
+            for (j = (i * ARRAY_UNITSIZE); j < ((i + 1) * ARRAY_UNITSIZE); j++)
+            {
+                array[j] = j + i;
+            }
+            i--;
+        }
+
+        // Check for values of array elements
+        int nErrors = ARRAY_SIZE;
+        for (int k = 0; k < ARRAY_SIZE; k++)
+        {
+            if (array[k] != (k + k / ARRAY_UNITSIZE))
+            {
+                Console.WriteLine("[k = {0}]\texpected = {1}\tactual={2}", k, k + (k / ARRAY_UNITSIZE), array[k]);
+            }
+            else
+            {
+                nErrors--;
+            }
+        }
+        if (nErrors == 0)
+        {
+            Console.WriteLine("Passed");
+            rc = 100;
+        }
+        else
+        {
+            Console.WriteLine("Failed");
+            rc = 1;
+        }
+
+        return rc;
+    }
+}
+
