d3dc86ed63224f4cd02cd69db4c24a3ce57c3f28
Zoltan Varga <vargaz@gmail.com>
2015-05-30 11:18:07 -0400

    Revert "[amd64] Add back a dummy argument to the throw trampolines to fix unaligned stack problems."
    This reverts commit mono/mono@d262c343d13f2a1c6e586663bca86f8e207797aa.
    
    Revert this as it breaks on linux.
    
    
    Commit migrated from https://github.com/mono/mono/commit/4bd2d86add1c349d51f6806d9bcb6ffa0d7ba71b

--- src/mono/mono/mini/mini-amd64.h
+++ src/mono/mono/mini/mini-amd64.h
@@ -379,12 +379,12 @@ mono_amd64_patch (unsigned char* code, gpointer target);
 void
 mono_amd64_throw_exception (guint64 dummy1, guint64 dummy2, guint64 dummy3, guint64 dummy4,
 							guint64 dummy5, guint64 dummy6,
-							MonoContext *mctx, MonoObject *exc, gboolean rethrow, guint64 dummy7);
+							MonoContext *mctx, MonoObject *exc, gboolean rethrow);
 
 void
 mono_amd64_throw_corlib_exception (guint64 dummy1, guint64 dummy2, guint64 dummy3, guint64 dummy4,
 								   guint64 dummy5, guint64 dummy6,
-								   MonoContext *mctx, guint32 ex_token_index, gint64 pc_offset, guint64 dummy7);
+								   MonoContext *mctx, guint32 ex_token_index, gint64 pc_offset);
 
 guint64
 mono_amd64_get_original_ip (void);
