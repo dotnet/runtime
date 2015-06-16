2c0fc6f04c89badfb98dfc8f9e5dd607c06472a7
Jan Kotas <jkotas@microsoft.com>
2015-06-15 20:03:00 -0700

    Allow AssemblyLoadContext.GetAssemblyName for native images on CoreCLR
    Commit migrated from https://github.com/dotnet/coreclr/commit/78f849b124d688128a9895221dbfa589eac5de51

--- src/coreclr/src/vm/assemblyname.cpp
+++ src/coreclr/src/vm/assemblyname.cpp
@@ -63,9 +63,17 @@ FCIMPL1(Object*, AssemblyNameNative::GetFileInformation, StringObject* filenameU
 
     EX_TRY
     {
+#ifdef FEATURE_CORECLR
+        // Allow AssemblyLoadContext.GetAssemblyName for native images on CoreCLR
+        if (pImage->HasNTHeaders() && pImage->HasCorHeader() && pImage->HasNativeHeader())
+            pImage->VerifyIsNIAssembly();
+        else
+            pImage->VerifyIsAssembly();
+#else
         pImage->VerifyIsAssembly();
+#endif
     }
-    EX_CATCH              
+    EX_CATCH
     {
         Exception *ex = GET_EXCEPTION();
         EEFileLoadException::Throw(sFileName,ex->GetHR(),ex);
