f7721a58a41535ffc963b387dcb40e030196028c
Matt Ellis <matell@microsoft.com>
2015-06-18 12:03:11 -0700

    Use System.Globalization.Native for casing
    This is a fairly straight forward change to start using
    System.Globalization.Native (and hence ICU) for casing support.  There
    are a few caveats due to how the work is being staged.
    
     - There are some fast paths in mscorlib that do simple ASCII casing if
       the input string is ASCII and we know the locale does not do special
       casing for ascii characters.  This is detected by doing a case
       insensitive string comparision between "ABC..XYZ" and "abc...xyz".
       However, since we don't have real collation support yet, this check
       would always return true and hence we would never actually go to
       TextInfo to do casing.  For now, this code is just disabled so we
       always go to TextInfo
    
     - Some of the dummy collation implementations used the ASCII casing
       routines on TextInfo.  Since they are no longer needed for TextInfo
       we move them to CompareInfo.  The alternative would be to do proper
       casing via TextInfo, but since all the collation code is throw away
       it doesn't seem prudent to spend time moving them to TextInfo.
    
     - Detection on if we should do turkish casing is based on the locale
       name, when we have collation support we should likely detect this by
       doing a case insenstive string comparision between i and LATIN
       CAPTIAL I WITH DOT ABOVE.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/71fab326cb589002ed6fa37a60a4a8da6949fe24

--- src/coreclr/src/mscorlib/corefx/System/Globalization/TextInfo.cs
+++ src/coreclr/src/mscorlib/corefx/System/Globalization/TextInfo.cs
@@ -288,6 +288,14 @@ namespace System.Globalization
         {
             get
             {
+#if PLATFORM_UNIX
+                // UNIXTODO: This hack can be removed once collation works and the code after this correctly returns "false".
+                if (m_needsTurkishCasing)
+                {
+                    return false;
+                }
+#endif
+
                 if (m_IsAsciiCasingSameAsInvariant == null)
                 {
                     m_IsAsciiCasingSameAsInvariant = CultureInfo.GetCultureInfo(m_textInfoName).CompareInfo.Compare("abcdefghijklmnopqrstuvwxyz",
