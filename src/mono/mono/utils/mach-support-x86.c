301d643872c76c610f0303049908c14f774c054a
Toxa <toxa@toxas-iMac.local>
2015-07-10 08:32:54 +0300

    Add TLS offset for pre-release OS X 10.11 on x86
    Commit migrated from https://github.com/mono/mono/commit/82091b3332a9a5438eef57b0fa5f4eb97d153a0c

--- src/mono/mono/utils/mach-support-x86.c
+++ src/mono/mono/utils/mach-support-x86.c
@@ -21,6 +21,7 @@
 /* All OSX versions up to 10.8 */
 #define TLS_VECTOR_OFFSET_CATS 0x48
 #define TLS_VECTOR_OFFSET_10_9 0xb0
+#define TLS_VECTOR_OFFSET_10_11 0x100
 
 static int tls_vector_offset;
 
@@ -127,6 +128,10 @@ mono_mach_init (pthread_key_t key)
 	tls_vector_offset = TLS_VECTOR_OFFSET_10_9;
 	if (mono_mach_arch_get_tls_value_from_thread (pthread_self (), key) == canary)
 		goto ok;
+    
+    tls_vector_offset = TLS_VECTOR_OFFSET_10_11;
+    if (mono_mach_arch_get_tls_value_from_thread (pthread_self (), key) == canary)
+        goto ok;
 
 	g_error ("could not discover the mach TLS offset");
 ok:
