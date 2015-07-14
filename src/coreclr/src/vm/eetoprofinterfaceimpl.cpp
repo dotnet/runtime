5a6be5aed31110004abbde8d58b0b8026a3fa7de
sergey-raevskiy <ccik@inbox.ru>
2015-07-15 00:52:55 +0300

    Update documentation links
    Commit migrated from https://github.com/dotnet/coreclr/commit/2ac7242889cf704886282b4a7d8f70b5432311d1

--- src/coreclr/src/vm/eetoprofinterfaceimpl.cpp
+++ src/coreclr/src/vm/eetoprofinterfaceimpl.cpp
@@ -17,7 +17,7 @@
 // PLEASE READ!
 //
 // There are strict rules for how to implement ICorProfilerCallback* wrappers.  Please read
-// https://github.com/dotnet/coreclr/blob/master/Documentation/profilability.md
+// https://github.com/dotnet/coreclr/blob/master/Documentation/botr/profilability.md
 // to understand the rules and why they exist.
 //
 // As a reminder, here is a short summary of your responsibilities.  Every PUBLIC
