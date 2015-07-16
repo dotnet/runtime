123ea51fc27eda4bbf7c570a051a30f444aa0892
Matt Ellis <matell@microsoft.com>
2015-07-16 12:48:17 -0700

    Add Normalization wrappers on top of ICU
    This change introduces System.Globalization.Native.so which will be
    the libary that we use to wrap all ICU functionality needed by the
    System.Globalization.* namespaces.
    
    The initial commit also adds wrappers around the string normalization
    functions which are exposed by the System.Globalization.Extensions
    contract.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/4d199ab379d4364a6202a6bfcd5e79ede77e624d

---
+++ src/clr/src/corefx/System.Globalization.Native/normalization.cpp
@@ -0,0 +1,84 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+#include <stdint.h>
+#include <uchar.h>
+#include <unicode/unorm2.h>
+
+/*
+ * These values should be kept in sync with System.Text.NormalizationForm
+ */
+enum class NormalizationForm : int32_t {
+    C = 0x1,
+    D = 0x2,
+    KC = 0x5,
+    KD = 0x6
+};
+
+const UNormalizer2* GetNormalizerForForm(NormalizationForm normalizationForm, UErrorCode* pErrorCode)
+{
+    switch(normalizationForm)
+    {
+    case NormalizationForm::C:
+        return unorm2_getNFCInstance(pErrorCode);
+    case NormalizationForm::D:
+        return unorm2_getNFDInstance(pErrorCode);
+    case NormalizationForm::KC:
+        return unorm2_getNFKCInstance(pErrorCode);
+    case NormalizationForm::KD:
+        return unorm2_getNFKDInstance(pErrorCode);
+    }
+
+    *pErrorCode = U_ILLEGAL_ARGUMENT_ERROR;
+    return nullptr;
+}
+
+/*
+Function:
+IsNormalized
+
+Used by System.StringNormalizationExtensions.IsNormalized to detect if a string is in a certain
+Unicode Normalization Form.
+
+Return values:
+0: lpStr is not normalized.
+1: lpStr is normalized.
+-1: internal error during normalization.
+*/
+extern "C" int32_t IsNormalized(NormalizationForm normalizationForm, const UChar* lpStr, int32_t cwStrLength)
+{
+    UErrorCode err = U_ZERO_ERROR;
+    const UNormalizer2* pNormalizer = GetNormalizerForForm(normalizationForm, &err);
+    UBool isNormalized = unorm2_isNormalized(pNormalizer, lpStr, cwStrLength, &err);
+
+    if (U_SUCCESS(err))
+    {
+        return isNormalized == TRUE ? 1 : 0;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+/*
+Function:
+NormalizeString
+
+Used by System.StringNormalizationExtensions.Normalize to normalize a string into a certain
+Unicode Normalization Form.
+
+Return values:
+0: internal error during normalization.
+>0: the length of the normalized string (not counting the null terminator).
+*/
+extern "C" int32_t NormalizeString(NormalizationForm normalizationForm, const UChar* lpSrc, int32_t cwSrcLength, UChar* lpDst, int32_t cwDstLength)
+{
+    UErrorCode err = U_ZERO_ERROR;
+    const UNormalizer2* pNormalizer = GetNormalizerForForm(normalizationForm, &err);
+    int32_t normalizedLen = unorm2_normalize(pNormalizer, lpSrc, cwSrcLength, lpDst, cwDstLength, &err);
+
+    return (U_SUCCESS(err) || (err == U_BUFFER_OVERFLOW_ERROR)) ? normalizedLen : 0;
+}
