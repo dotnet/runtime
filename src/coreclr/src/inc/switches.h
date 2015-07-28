6311e72e885652e838428edcddd9339657a31c27
Geoff Norton <grompf@gmail.com>
2015-07-28 15:40:23 +0000

    [aarch64] Enable the interpreter on linux as well
    Commit migrated from https://github.com/dotnet/coreclr/commit/8c4e60054ddb42298f3eebaf20c970d665474ae3

--- src/coreclr/src/inc/switches.h
+++ src/coreclr/src/inc/switches.h
@@ -262,7 +262,7 @@
 #define FEATURE_STACK_SAMPLING
 #endif // defined (ALLOW_SXS_JIT)
 
-#if defined(_TARGET_ARM64_) && !defined(FEATURE_CORECLR)
+#if defined(_TARGET_ARM64_)
 #define FEATURE_INTERPRETER
 #endif // defined(_TARGET_ARM64_)
 
