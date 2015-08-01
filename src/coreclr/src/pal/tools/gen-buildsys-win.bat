943f694fc108d37edadddd1e4c19847d4b93a6ed
Mike Danes <onemihaid@hotmail.com>
2015-08-01 11:04:16 +0300

    Enable the Windows x86 build This allows building the Windows x86 version of CoreCLR by using "x86" for the BuildArch parameter of build.cmd. Note that CMAKE_SYSTEM_PROCESSOR is no longer used in Windows builds to set IS_64BIT_BUILD. This change is enough to get CoreCLR to build but more changes are required for it to actually run correctly. In particular, the JIT compiler support for x86 is pretty limited at this point.
    Commit migrated from https://github.com/dotnet/coreclr/commit/8bb8ed66e4e54270e18e68c37515f21730c82cea

--- src/coreclr/src/pal/tools/gen-buildsys-win.bat
+++ src/coreclr/src/pal/tools/gen-buildsys-win.bat
@@ -5,7 +5,7 @@ rem This file invokes cmake and generates the build system for windows.
 set argC=0
 for %%x in (%*) do Set /A argC+=1
 
-if NOT %argC%==2 GOTO :USAGE
+if NOT %argC%==3 GOTO :USAGE
 if %1=="/?" GOTO :USAGE
 
 setlocal
@@ -17,6 +17,7 @@ if %basePath:~-1%==\ set "basePath=%basePath:~0,-1%"
 
 set __VSString=12 2013
 if /i "%2" == "vs2015" (set __VSString=14 2015)
+if /i "%3" == "x64" (set __VSString=%__VSString% Win64)
 
 if defined CMakePath goto DoGen
 
@@ -24,7 +25,7 @@ if defined CMakePath goto DoGen
 for /f "delims=" %%a in ('powershell -NoProfile -ExecutionPolicy RemoteSigned "& .\probe-win.ps1"') do %%a
 
 :DoGen
-"%CMakePath%" "-DCMAKE_USER_MAKE_RULES_OVERRIDE=%basePath%\windows-compiler-override.txt" -G "Visual Studio %__VSString% Win64" %1
+"%CMakePath%" "-DCMAKE_USER_MAKE_RULES_OVERRIDE=%basePath%\windows-compiler-override.txt" "-DCLR_CMAKE_TARGET_ARCH=%3" -G "Visual Studio %__VSString%" %1
 endlocal
 GOTO :DONE
 
