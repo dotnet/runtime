14a1279f26ef261b0745c3c58bf14ec37bfb3354
Zoltan Varga <vargaz@gmail.com>
2015-08-02 00:13:49 -0400

    [runtime] Clean up configure.ac a bit.
    Commit migrated from https://github.com/mono/mono/commit/d37df4b0c64f75e7de39a13ec614c7c2719a0491

--- src/mono/mono/metadata/process.c
+++ src/mono/mono/metadata/process.c
@@ -663,10 +663,7 @@ MonoBoolean ves_icall_System_Diagnostics_Process_ShellExecuteEx_internal (MonoPr
 	} else {
 		process_info->process_handle = shellex.hProcess;
 		process_info->thread_handle = NULL;
-		/* It appears that there's no way to get the pid from a
-		 * process handle before windows xp.  Really.
-		 */
-#if defined(HAVE_GETPROCESSID) && !defined(MONO_CROSS_COMPILE)
+#if !defined(MONO_CROSS_COMPILE)
 		process_info->pid = GetProcessId (shellex.hProcess);
 #else
 		process_info->pid = 0;
