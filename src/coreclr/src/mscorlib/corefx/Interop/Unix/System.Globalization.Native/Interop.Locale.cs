61df2085a3abb3e7faebdef467321efa9be92df8
Steve Harter <sharter@microsoft.com>
2015-08-04 17:53:29 -0500

    address code review comments
    Commit migrated from https://github.com/dotnet/coreclr/commit/f05fa7f22424c636bc5b813e417654a5a45f6773

--- src/coreclr/src/mscorlib/corefx/Interop/Unix/System.Globalization.Native/Interop.Locale.cs
+++ src/coreclr/src/mscorlib/corefx/Interop/Unix/System.Globalization.Native/Interop.Locale.cs
@@ -10,12 +10,15 @@ internal static partial class Interop
     internal static partial class GlobalizationInterop
     {
         [DllImport(Libraries.GlobalizationInterop, CharSet = CharSet.Unicode)]
+        [return: MarshalAs(UnmanagedType.Bool)]
         internal unsafe static extern bool GetLocaleName(string localeName, StringBuilder value, int valueLength);
 
         [DllImport(Libraries.GlobalizationInterop, CharSet = CharSet.Unicode)]
+        [return: MarshalAs(UnmanagedType.Bool)]
         internal unsafe static extern bool GetLocaleInfoString(string localeName, uint localeStringData, StringBuilder value, int valueLength);
 
         [DllImport(Libraries.GlobalizationInterop, CharSet = CharSet.Unicode)]
+        [return: MarshalAs(UnmanagedType.Bool)]
         internal unsafe static extern bool GetLocaleInfoInt(string localeName, uint localeNumberData, ref int value);
     }
 }
