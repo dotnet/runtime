7c102f1ff7e3c49bd7d2f8f972ca3dcca3f5b48e
Aditya Mandaleeka <adityam@microsoft.com>
2015-08-07 19:23:47 -0700

    Remove temporary ifdef and refactor/simplify

--- src/debug/daccess/dacdbiimplstackwalk.cpp
+++ src/debug/daccess/dacdbiimplstackwalk.cpp
@@ -1303,7 +1303,7 @@ bool DacDbiInterfaceImpl::IsThreadSuspendedOrHijacked(VMPTR_Thread vmThread)
         return true;
     }
 
-#if defined(FEATURE_HIJACK) || defined(FEATURE_UNIX_GC_REDIRECT_HIJACK)
+#ifdef FEATURE_HIJACK
     if ((ts & Thread::TS_Hijacked) != 0)
     {
         return true;
