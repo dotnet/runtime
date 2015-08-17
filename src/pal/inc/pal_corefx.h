512ef05db7419352c4630bb2fcf6c5dca94fce4b
Nick Guerrera <nicholg@microsoft.com>
2015-08-16 21:54:49 -0700

    Remove GetFileInformation stat wrapper from PAL
    It has been replaced by stat shims in corefx System.Native.
    
    Reverts commits:
    * 79ebe5605a94d559d2996997aec3cbcf8a4504c3.
    * 0119283384f1bac80da8bb1736453c87d4f8e689.

--- src/pal/inc/pal_corefx.h
+++ src/pal/inc/pal_corefx.h
@@ -45,35 +45,6 @@ ForkAndExecProcess(
            int* stdoutFd,
            int* stderrFd);
 
-struct fileinfo {
-    int32_t  flags;   /* flags for testing if some members are present */
-    int32_t  mode;    /* protection */
-    int32_t  uid;     /* user ID of owner */
-    int32_t  gid;     /* group ID of owner */
-    int64_t  size;    /* total size, in bytes */
-    int64_t  atime;   /* time of last access */
-    int64_t  mtime;   /* time of last modification */
-    int64_t  ctime;   /* time of last status change */
-    int64_t  btime;   /* time the file was created (birthtime) */
-};
-
-#define FILEINFO_FLAGS_NONE 0x0
-#define FILEINFO_FLAGS_HAS_BTIME 0x1
-
-PALIMPORT
-int
-PALAPI
-GetFileInformationFromPath(
-    const char* path,
-    struct fileinfo* buf);
-
-PALIMPORT
-int
-PALAPI
-GetFileInformationFromFd(
-    int fd,
-    struct fileinfo* buf);
-
 #ifdef  __cplusplus
 } // extern "C"
 #endif
