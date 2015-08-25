ca6957e7f38ad75490925c919fa6a00f2a767d22
Eric Erhardt <eerhardt@microsoft.com>
2015-08-25 16:55:30 -0500

    Implement CalendarData on Linux.
    This is the first round of CalendarData implementation on Linux using ICU
    for the information.  It contains Month Names, Day Names, DateTime format
    patterns, etc.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/e78ea838781d45b1f16d555a1be5950c05da7bfb

---
+++ src/coreclr/src/mscorlib/corefx/Interop/Unix/System.Globalization.Native/Interop.Calendar.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Text;
+
+internal static partial class Interop
+{
+    internal static partial class GlobalizationInterop
+    {
+        internal delegate void EnumCalendarInfoCallback(
+           [MarshalAs(UnmanagedType.LPWStr)] string calendarString,
+           IntPtr context);
+
+        [DllImport(Libraries.GlobalizationInterop, CharSet = CharSet.Unicode)]
+        internal static extern CalendarDataResult GetCalendarInfo(string localeName, CalendarId calendarId, CalendarDataType calendarDataType, StringBuilder result, int resultCapacity);
+
+        [DllImport(Libraries.GlobalizationInterop, CharSet = CharSet.Unicode)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool EnumCalendarInfo(EnumCalendarInfoCallback callback, string localeName, CalendarId calendarId, CalendarDataType calendarDataType, IntPtr context);
+    }
+}
