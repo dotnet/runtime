b6c039a092322c74b96ffd9ef3dce533e493cccf
Eric Erhardt <eerhardt@microsoft.com>
2015-09-02 18:18:20 -0500

    Implement Japanese Era information.
    Commit migrated from https://github.com/dotnet/coreclr/commit/75cb790ade44b67e2a3c7b01bf0968b822014d65

--- src/clr/src/mscorlib/corefx/Interop/Unix/System.Globalization.Native/Interop.Calendar.cs
+++ src/clr/src/mscorlib/corefx/Interop/Unix/System.Globalization.Native/Interop.Calendar.cs
@@ -23,5 +23,11 @@ internal static partial class Interop
         [DllImport(Libraries.GlobalizationInterop, CharSet = CharSet.Unicode)]
         [return: MarshalAs(UnmanagedType.Bool)]
         internal static extern bool EnumCalendarInfo(EnumCalendarInfoCallback callback, string localeName, CalendarId calendarId, CalendarDataType calendarDataType, IntPtr context);
+
+        [DllImport(Libraries.GlobalizationInterop)]
+        internal static extern int GetLatestJapaneseEra();
+
+        [DllImport(Libraries.GlobalizationInterop)]
+        internal static extern bool GetJapaneseEraStartDate(int era, out int startYear, out int startMonth, out int startDay);
     }
 }
