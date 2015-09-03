dd599efbf33f8adb9b8206c348cc96a95daa2738
Alex Rønne Petersen <alexrp@xamarin.com>
2015-09-04 00:30:46 +0200

    [runtime] Optimize mono_class_field_is_special_static () a bit.
    We can avoid loading custom attributes by checking field->offset == -1. This
    won't always be the case for a special static field at all stages during class
    loading but it'll speed up the most frequent cases.
    
    
    Commit migrated from https://github.com/mono/mono/commit/0686c213583a70bde97c8d04e0748e47416bfaf9

--- src/mono/mono/metadata/object.c
+++ src/mono/mono/metadata/object.c
@@ -2419,6 +2419,8 @@ mono_class_field_is_special_static (MonoClassField *field)
 	if (mono_field_is_deleted (field))
 		return FALSE;
 	if (!(field->type->attrs & FIELD_ATTRIBUTE_LITERAL)) {
+		if (field->offset == -1)
+			return TRUE;
 		if (field_is_special_static (field->parent, field) != SPECIAL_STATIC_NONE)
 			return TRUE;
 	}
