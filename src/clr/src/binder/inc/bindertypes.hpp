39094e4896e7defe8472dc86c48d2b29221665d8
Lakshmi Priya Sekar <lasekar@microsoft.com>
2015-09-17 13:00:36 -0700

    Use SString type for path allocations in binder.
    Commit migrated from https://github.com/dotnet/coreclr/commit/b8282e0b7359e1abbcfdf4574621118b4f61d377

--- src/clr/src/binder/inc/bindertypes.hpp
+++ src/clr/src/binder/inc/bindertypes.hpp
@@ -32,7 +32,7 @@ class PEAssembly;
 
 namespace BINDER_SPACE
 {
-    typedef InlineSString<MAX_LONGPATH + 1> PathString;
+    typedef InlineSString<512> PathString;
 
     class AssemblyVersion;
     class AssemblyName;
