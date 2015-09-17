1ff94fee07fb93852a7761a1ce5f62d072887565
Eric Erhardt <eerhardt@microsoft.com>
2015-09-17 10:50:37 -0500

    Use 'readlink' of the /etc/localtime file to determine the TimeZoneInfo.Local.Id on Linux.
    Fix https://github.com/dotnet/corefx/issues/2489.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/cf5669690fbd4fe08ecf264783380667ceb25c27

---
+++ src/coreclr/src/corefx/System.Globalization.Native/timeZoneInfo.cpp
@@ -0,0 +1,24 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+#include <stdint.h>
+#include <unistd.h>
+
+/*
+Function:
+ReadLink
+
+Gets the symlink value for the path.
+*/
+extern "C" int32_t ReadLink(const char* path, char* result, size_t resultCapacity)
+{
+	ssize_t r = readlink(path, result, resultCapacity - 1); // subtract one to make room for the NULL character
+
+	if (r < 1 || r >= resultCapacity)
+		return false;
+
+	result[r] = '\0';
+	return true;
+}
