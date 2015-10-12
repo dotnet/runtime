e961858aec90f4318af4429cfab169305ca67d5c
Zoltan Varga <vargaz@gmail.com>
2015-10-12 14:34:35 -0400

    Revert "[runtime] Don't allocate domain-level static slots for special static fields."
    This reverts commit mono/mono@d2efa789ac432bbcc92483d8f6e0b17c3b23efff.
    
    Revert this as it breaks on monodroid.
    
    
    Commit migrated from https://github.com/mono/mono/commit/390a7583967211555bfa3e65bba9814d5518dcf9

--- src/mono/mono/metadata/object.c
+++ src/mono/mono/metadata/object.c
@@ -725,8 +725,8 @@ compute_class_bitmap (MonoClass *class, gsize *bitmap, int size, int offset, int
 			if (field->type->byref)
 				break;
 
-			// Special static fields do not have a domain-level static slot
-			if (static_fields && mono_class_field_is_special_static (field))
+			if (static_fields && field->offset == -1)
+				/* special static */
 				continue;
 
 			pos = field->offset / sizeof (gpointer);
