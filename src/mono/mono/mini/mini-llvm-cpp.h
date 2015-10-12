504ce52f7f16e6d8221ec4f83e33f627745dae3c
Zoltan Varga <vargaz@gmail.com>
2015-10-12 12:15:12 -0400

    Fix the cross compiler build by not including c headers into mini-llvm-cpp.h.
    Commit migrated from https://github.com/mono/mono/commit/d037168755e35d51ab9794d6eed859aec6b90e5d

--- src/mono/mono/mini/mini-llvm-cpp.h
+++ src/mono/mono/mini/mini-llvm-cpp.h
@@ -15,9 +15,6 @@
 #include "llvm-c/Core.h"
 #include "llvm-c/ExecutionEngine.h"
 
-#include "../metadata/object.h"
-#include "../metadata/domain-internals.h"
-
 #include <unwind.h>
 
 G_BEGIN_DECLS
@@ -98,33 +95,6 @@ mono_llvm_create_constant_data_array (const uint8_t *data, int len);
 void
 mono_llvm_set_is_constant (LLVMValueRef global_var);
 
-void
-mono_llvm_cpp_throw_exception (void);
-
-void
-mono_llvm_rethrow_exception (MonoObject *ex);
-
-void
-mono_llvm_throw_exception (MonoObject *ex);
-
-void
-mono_llvm_throw_corlib_exception (guint32 ex_token_index);
-
-void
-mono_llvm_resume_exception (void);
-
-gint32
-mono_llvm_match_exception (MonoJitInfo *jinfo, guint32 region_start, guint32 region_end);
-
-void 
-mono_llvm_clear_exception (void);
-
-MonoObject *
-mono_llvm_load_exception (void);
-
-void
-mono_llvm_reset_exception (void);
-
 _Unwind_Reason_Code 
 mono_debug_personality (int a, _Unwind_Action b,
 	uint64_t c, struct _Unwind_Exception *d, struct _Unwind_Context *e);
@@ -136,7 +106,7 @@ void
 default_mono_llvm_unhandled_exception (void);
 
 void
-mono_llvm_raise_exception (MonoException *e);
+mono_llvm_cpp_throw_exception (void);
 
 G_END_DECLS
 
