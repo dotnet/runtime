e25afd20e63219179383a35d9952dc773c4fc1c0
Rama Krishnan Raghupathy <ramarag@microsoft.com>
2015-10-19 18:22:07 -0700

    addressing desktop build failure
    Code which does not have FEATURE_EVENT_TRACE defined is using artifacts from it
    
    [tfs-changeset: 1539531]
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/586b884dc1b8fb7487a6c25cec327627ba75f411

--- src/coreclr/src/inc/eventtracebase.h
+++ src/coreclr/src/inc/eventtracebase.h
@@ -828,11 +828,6 @@ namespace ETW
 };
 
 
-#if defined(FEATURE_EVENT_TRACE) && !defined(FEATURE_PAL)
-//
-// The ONE and only ONE global instantiation of this class
-//
-extern ETW::CEtwTracer *  g_pEtwTracer;
 #define ETW_IS_TRACE_ON(level) ( FALSE ) // for fusion which is eventually going to get removed
 #define ETW_IS_FLAG_ON(flag) ( FALSE ) // for fusion which is eventually going to get removed
 
@@ -845,6 +840,12 @@ extern ETW::CEtwTracer *  g_pEtwTracer;
 #define ETWLoaderLoadTypeNotAvailable 0 // Static or Dynamic Load is only valid at LoaderPhaseStart and LoaderPhaseEnd events - for other events, 0 indicates "not available"
 #define ETWLoaderStaticLoad 0 // Static reference load
 #define ETWLoaderDynamicLoad 1 // Dynamic assembly load
+
+#if defined(FEATURE_EVENT_TRACE) && !defined(FEATURE_PAL)
+//
+// The ONE and only ONE global instantiation of this class
+//
+extern ETW::CEtwTracer *  g_pEtwTracer;
 
 //
 // Special Handling of Startup events
