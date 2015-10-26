2a32520076e0a6eaa9114e7c91ffc6ddacb722fe
Ludovic Henry <ludovic@xamarin.com>
2015-10-26 16:35:14 +0000

    [tests] Add timeout before failing
    Commit migrated from https://github.com/mono/mono/commit/a92a2ea39243a3090169093749032e97c64d03d3

--- src/mono/mono/tests/unhandled-exception-3.cs
+++ src/mono/mono/tests/unhandled-exception-3.cs
@@ -19,6 +19,10 @@ class Driver
 		if (!mre.WaitOne (5000))
 			Environment.Exit (2);
 
+		/* Give a chance to the threadpool thread to finish executing the exception
+		 * unwinding after the finally, before we exit with status 0 on the current thread */
+		Thread.Sleep (1000);
+
 		Environment.Exit (0);
 	}
 }
