54762a3658f0165b69de611746e7968c56fc2133
Zoltan Varga <vargaz@gmail.com>
2015-10-25 22:17:01 -0400

    Add another missing file.
    Commit migrated from https://github.com/mono/mono/commit/53a79d1b6bbcef7f83bb1321b20f040cc280d5ed

---
+++ src/mono/mono/mini/llvm-runtime.h
@@ -0,0 +1,27 @@
+/*
+ * llvm-runtime.h: Runtime support for llvm generated code
+ *
+ * Authors:
+ *   Zoltan Varga (vargaz@gmail.com)
+ *
+ * (C) 2015 Xamarin, Inc.
+ */
+
+#ifndef __MONO_LLVM_RUNTIME_H__
+#define __MONO_LLVM_RUNTIME_H__
+
+#include <glib.h>
+
+#include <unwind.h>
+
+G_BEGIN_DECLS
+
+void
+mono_llvm_cpp_throw_exception (void);
+
+G_END_DECLS
+
+#endif /* __MONO_LLVM_RUNTIME_H__ */
+ 
+
+
