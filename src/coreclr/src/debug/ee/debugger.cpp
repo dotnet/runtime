7d9c332d614412ee1b104cccb5050f054be1bbfb
Eugene <Eugene.Zemtsov@microsoft.com>
2015-11-05 14:42:54 -0800

    Selective VirtualProtect in debugger heap for Realloc
    Commit migrated from https://github.com/dotnet/coreclr/commit/6d2035bb2589aae0d3f363b3a08ec76297e38dbe

--- src/coreclr/src/debug/ee/debugger.cpp
+++ src/coreclr/src/debug/ee/debugger.cpp
@@ -16802,14 +16802,17 @@ void *DebuggerHeap::Realloc(void *pMem, DWORD newSize, DWORD oldSize)
 
 
 #ifdef FEATURE_PAL
-    // We don't have executable heap in PAL, but we still need to allocate 
-    // executable memory, that's why have change protection level for 
-    // each allocation. 
-    // UNIXTODO: We need to look how JIT solves this problem.
-    DWORD unusedFlags;
-    if (!VirtualProtect(ret, newSize, PAGE_EXECUTE_READWRITE, &unusedFlags))
-    {
-        _ASSERTE(!"VirtualProtect failed to make this memory executable");
+    if (m_fExecutable)
+    {    
+        // We don't have executable heap in PAL, but we still need to allocate 
+        // executable memory, that's why have change protection level for 
+        // each allocation. 
+        // UNIXTODO: We need to look how JIT solves this problem.
+        DWORD unusedFlags;
+        if (!VirtualProtect(ret, newSize, PAGE_EXECUTE_READWRITE, &unusedFlags))
+        {
+            _ASSERTE(!"VirtualProtect failed to make this memory executable");
+        }
     }
 #endif // FEATURE_PAL    
 
