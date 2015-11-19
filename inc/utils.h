85ed3401c3eba23d28ae10d9bd68f5dd444a1088
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2015-11-19 09:33:43 -0800

    add default values for DOTNET_HOME in corehost
    fixes #234

--- inc/utils.h
+++ inc/utils.h
@@ -11,5 +11,6 @@ pal::string_t get_executable(const pal::string_t& filename);
 pal::string_t get_directory(const pal::string_t& path);
 pal::string_t get_filename(const pal::string_t& path);
 void append_path(pal::string_t& path1, const pal::char_t* path2);
+bool coreclr_exists_in_dir(const pal::string_t& candidate);
 
 #endif
