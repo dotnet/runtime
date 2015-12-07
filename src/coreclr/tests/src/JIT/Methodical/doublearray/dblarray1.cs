a51d73f4346da85844f40fff47aa97b6ed578722
Pat Gavlin <pagavlin@microsoft.com>
2015-12-07 12:07:12 -0800

    Import additional JIT tests.
    Most of these tests were missed in the first round because they rely on
    TypedReference.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/d070ca1f4e643c1cb9dc7d750f2476f2aa77efc3

---
+++ src/coreclr/tests/src/JIT/Methodical/doublearray/dblarray1.cs
@@ -0,0 +1,113 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+// Goal: Test arrays of doubles are allocated on large object heap and therefore 8 byte aligned
+// Assumptions:
+// 1) large object heap is always 8 byte aligned
+// 2) double array greater than 1000 elements is on large object heap
+// 3) non-double array greater than 1000 elements but less than 85K is NOT on large object heap
+// 4) new arrays allocated in large object heap is of generation 2
+// 5) new arrays NOT allocated in large object heap is of generation 0 
+// 6) the threshold can be set by registry key DoubleArrayToLargeObjectHeap
+
+// Variation on array length
+
+using System;
+internal class DblArray1
+{
+    private static int s_LOH_GEN = 0;
+    public static void f0()
+    {
+        double[] arr = new double[1];
+        if (GC.GetGeneration(arr) != 0)
+        {
+            Console.WriteLine("Generation {0}", GC.GetGeneration(arr));
+            throw new Exception();
+        }
+    }
+
+    public static void f1()
+    {
+        double[] arr = new double[99];
+        if (GC.GetGeneration(arr) != 0)
+        {
+            Console.WriteLine("Generation {0}", GC.GetGeneration(arr));
+            throw new Exception();
+        }
+    }
+
+    public static void f2()
+    {
+        double[] arr = new double[100];
+        if (GC.GetGeneration(arr) != 0)
+        {
+            Console.WriteLine("Generation {0}", GC.GetGeneration(arr));
+            throw new Exception();
+        }
+    }
+
+    public static void f3()
+    {
+        double[] arr = new double[999];
+        if (GC.GetGeneration(arr) != 0)
+        {
+            Console.WriteLine("Generation {0}", GC.GetGeneration(arr));
+            throw new Exception();
+        }
+    }
+
+    public static void f4()
+    {
+        double[] arr = new double[1000];
+        if (GC.GetGeneration(arr) != s_LOH_GEN)
+        {
+            Console.WriteLine("Generation {0}", GC.GetGeneration(arr));
+            throw new Exception();
+        }
+    }
+
+    public static void f5()
+    {
+        double[] arr = new double[1001];
+        if (GC.GetGeneration(arr) != s_LOH_GEN)
+        {
+            Console.WriteLine("Generation {0}", GC.GetGeneration(arr));
+            throw new Exception();
+        }
+    }
+
+    public static int Main()
+    {
+        if (Environment.GetEnvironmentVariable("PROCESSOR_ARCHITECTURE") == "x86")
+        {
+            s_LOH_GEN = 2;
+        }
+        try
+        {
+            f0();
+            f1();
+            f2();
+            f3();
+            f4();
+            f5();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            Console.WriteLine(e.StackTrace);
+            Console.WriteLine("FAILED");
+            Console.WriteLine();
+            Console.WriteLine(@"// Goal: Test arrays of doubles are allocated on large object heap and therefore 8 byte aligned
+// Assumptions:
+// 1) large object heap is always 8 byte aligned
+// 2) double array greater than 1000 elements is on large object heap
+// 3) non-double array greater than 1000 elements but less than 85K is NOT on large object heap
+// 4) new arrays allocated in large object heap is of generation 2
+// 5) new arrays NOT allocated in large object heap is of generation 0 ");
+
+            return -1;
+        }
+        Console.WriteLine("PASSED");
+        return 100;
+    }
+}
