3533d99a47c7c55a3ec1943f15e7c063251ad7c1
Ludovic Henry <ludovic@xamarin.com>
2015-12-08 20:49:27 +0000

    [threadpool] Fix get min and available threads icalls
    Fix bug mono/mono#36414
    
    
    Commit migrated from https://github.com/mono/mono/commit/780fbf4a57c1732e136d7ff80e61c6468e587df6

--- src/mono/mono/metadata/threadpool-ms.c
+++ src/mono/mono/metadata/threadpool-ms.c
@@ -1421,12 +1421,16 @@ mono_threadpool_ms_resume (void)
 void
 ves_icall_System_Threading_ThreadPool_GetAvailableThreadsNative (gint32 *worker_threads, gint32 *completion_port_threads)
 {
+	ThreadPoolCounter counter;
+
 	if (!worker_threads || !completion_port_threads)
 		return;
 
 	mono_lazy_initialize (&status, initialize);
 
-	*worker_threads = threadpool->limit_worker_max;
+	counter.as_gint64 = COUNTER_READ ();
+
+	*worker_threads = counter._.max_working - counter._.active;
 	*completion_port_threads = threadpool->limit_io_max;
 }
 
@@ -1464,8 +1468,8 @@ ves_icall_System_Threading_ThreadPool_SetMinThreadsNative (gint32 worker_threads
 	if (completion_port_threads <= 0 || completion_port_threads > threadpool->limit_io_max)
 		return FALSE;
 
-	threadpool->limit_worker_max = worker_threads;
-	threadpool->limit_io_max = completion_port_threads;
+	threadpool->limit_worker_min = worker_threads;
+	threadpool->limit_io_min = completion_port_threads;
 
 	return TRUE;
 }
