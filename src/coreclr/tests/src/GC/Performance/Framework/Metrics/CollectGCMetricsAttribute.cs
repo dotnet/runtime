b03b21807c0e8b76ce859f671df9fcfa55178d28
Sean Gillespie <segilles@microsoft.com>
2015-12-08 15:42:43 -0800

    initial commit of GC performance test framework
    Some style changes to conform to CoreFX's csharp style guide. Fix the readme to not refer to paths local to my machine
    
    switch timeout to not accept 0 as a valid timeout
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/2cb1a6b2538bee201a9b5c82d8b673e8dd55efa4

---
+++ src/coreclr/tests/src/GC/Performance/Framework/Metrics/CollectGCMetricsAttribute.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Xunit.Performance.Sdk;
+using System;
+
+namespace GCPerfTestFramework.Metrics
+{
+    /// <summary>
+    /// This attribute marks a xunit-performance test artifact as requiring GC metrics. When this attribute adorns
+    /// a test artifact, xunit-performance creates an instance of <see cref="GCMetricDiscoverer"/> and uses
+    /// it to populate the list of metrics provided for that artifact.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Assembly)]
+#if WINDOWS
+    [PerformanceMetricDiscoverer("GCPerfTestFramework.Metrics.GCMetricDiscoverer", "Framework")]
+#endif
+    public class CollectGCMetricsAttribute : 
+        Attribute
+#if WINDOWS
+        , IPerformanceMetricAttribute
+#endif
+    {
+    }
+}
