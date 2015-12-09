45fa80a4d4d548e1ac20dbba267f9737adea1d62
Andy Ayers <andya@microsoft.com>
2015-12-08 16:03:19 -0800

    Add SIMD tests
    SIMD correctness tests.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/c370ca3e4a0e8d0e255077c309cad2fefd618be2

---
+++ src/clr/tests/src/JIT/SIMD/VectorArgs.cs
@@ -0,0 +1,66 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using System;
+using System.Numerics;
+
+internal class Color
+{
+    private Vector<float> _rgb;
+
+    public Color(float r, float g, float b)
+    {
+        float[] temp = new float[Vector<float>.Count];
+        temp[0] = r; temp[1] = g; temp[2] = b;
+        _rgb = new Vector<float>(temp);
+    }
+
+    public Color(Vector<float> _rgb)
+    { this._rgb = _rgb; }
+
+    public Color Change(float f)
+    {
+        Vector<float> t = new Vector<float>(f);
+        // t[3] = 0;
+        return new Color(t * _rgb);
+    }
+
+    public Vector<float> RGB { get { return _rgb; } }
+}
+
+internal partial class VectorTest
+{
+    private static int VectorArgs()
+    {
+        const int Pass = 100;
+        const int Fail = -1;
+
+        float[] temp = new float[Vector<float>.Count];
+        for (int i = 0; i < Vector<float>.Count; i++)
+        {
+            temp[i] = 3 - i;
+        }
+        Vector<float> rgb = new Vector<float>(temp);
+
+        float x = 2f;
+        Color c1 = new Color(rgb);
+        Color c2 = c1.Change(x);
+
+        for (int i = 0; i < Vector<float>.Count; i++)
+        {
+            // Round to integer for comparison.
+            if (((int)c2.RGB[i]) != (3 - i) * x)
+            {
+                return Fail;
+            }
+        }
+
+        return Pass;
+    }
+
+    private static int Main()
+    {
+        return VectorArgs();
+    }
+}
