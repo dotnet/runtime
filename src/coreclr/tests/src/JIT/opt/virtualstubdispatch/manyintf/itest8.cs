c7195772b3717a80d911418b5be2d227db2c0a3e
Hongtao Yu <Hongtao.Yu@microsoft.com>
2015-12-09 15:27:40 -0800

    Porting tests that have references
    jit\directed\perffix\primitivevt\callconv2_cs_d.csproj
    jit\directed\perffix\primitivevt\callconv2_cs_do.csproj
    jit\directed\perffix\primitivevt\callconv2_cs_r.csproj
    jit\directed\perffix\primitivevt\callconv2_cs_ro.csproj
    jit\methodical\cctor\xassem\xprecise1_cs_d.csproj
    jit\methodical\cctor\xassem\xprecise1_cs_do.csproj
    jit\methodical\cctor\xassem\xprecise1_cs_r.csproj
    jit\methodical\cctor\xassem\xprecise1_cs_ro.csproj
    jit\methodical\cctor\xassem\xprecise1b_cs_d.csproj
    jit\methodical\cctor\xassem\xprecise1b_cs_do.csproj
    jit\methodical\cctor\xassem\xprecise1b_cs_r.csproj
    jit\methodical\cctor\xassem\xprecise1b_cs_ro.csproj
    jit\methodical\cctor\xassem\xprecise2_cs_d.csproj
    jit\methodical\cctor\xassem\xprecise2_cs_do.csproj
    jit\methodical\cctor\xassem\xprecise2_cs_r.csproj
    jit\methodical\cctor\xassem\xprecise2_cs_ro.csproj
    jit\methodical\cctor\xassem\xprecise4_cs_d.csproj
    jit\methodical\cctor\xassem\xprecise4_cs_do.csproj
    jit\methodical\cctor\xassem\xprecise4_cs_r.csproj
    jit\methodical\cctor\xassem\xprecise4_cs_ro.csproj
    jit\regression\clr-x86-jit\v2.0-beta2\b423721\b423721.csproj
    
    Remove testing against Type.GetMethod(string) which is not in coreCLR.
    
    jit\jit64\eh\finallyexec\nestedTryRegionsWithSameOffset1.csproj
    jit\jit64\eh\finallyexec\nestedTryRegionsWithSameOffset1_o.csproj
    jit\jit64\eh\finallyexec\nestedTryRegionsWithSameOffset2.csproj
    jit\jit64\eh\finallyexec\nestedTryRegionsWithSameOffset2_o.csproj
    jit\jit64\eh\finallyexec\nestedTryRegionsWithSameOffset3.csproj
    jit\jit64\eh\finallyexec\nestedTryRegionsWithSameOffset3_o.csproj
    jit\jit64\eh\finallyexec\nonlocalgotoinatryblockinahandler.csproj
    jit\jit64\eh\leaves\nonlocalexitfromnestedcatch.csproj
    jit\methodical\boxing\xlang\_dbgsin_cs_cs.csproj
    jit\methodical\boxing\xlang\_odbgsin_cs_cs.csproj
    jit\methodical\boxing\xlang\_orelsin_cs_cs.csproj
    jit\methodical\boxing\xlang\_relsin_cs_cs.csproj
    jit\methodical\eh\basics\multihandler_d.csproj
    jit\methodical\eh\basics\multihandler_do.csproj
    jit\methodical\eh\basics\multihandler_r.csproj
    jit\methodical\eh\basics\multihandler_ro.csproj
    jit\methodical\eh\basics\throwincatch_d.csproj
    jit\methodical\eh\basics\throwincatch_do.csproj
    jit\methodical\eh\basics\throwincatch_r.csproj
    jit\methodical\eh\basics\throwincatch_ro.csproj
    jit\methodical\eh\basics\throwinclassconstructor_d.csproj
    jit\methodical\eh\basics\throwinclassconstructor_do.csproj
    jit\methodical\eh\basics\throwinclassconstructor_r.csproj
    jit\methodical\eh\basics\throwinclassconstructor_ro.csproj
    Porting jit\methodical\eh\basics
    Porting jit\methodical\eh\finallyexec
    Porting jit\methodical\eh\generics
    Porting jit\methodical\eh\interactions
    Porting jit\methodical\eh\leaves
    
    Porting jit\methodical\eh\rethrow
    Porting jit\methodical\eh\nested\general
    Porting jit\methodical\eh\nested\nonlocalexit
    Porting jit\methodical\eh\regress
    Porting more tests
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/1a49621bae8f5388a6f1aaa3ed9f8c944e67985f

---
+++ src/coreclr/tests/src/JIT/opt/virtualstubdispatch/manyintf/itest8.cs
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace VirtFunc
+{
+    public interface ITest8
+    {
+        int f8a();
+        int f8b(int a);
+        decimal f8c();
+        string f8d();
+    }
+}
