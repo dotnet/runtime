82e65d1dc0ce3c7d9dba83af122f7d1fd3e3c17c
Vlad Brezae <brezaevlad@gmail.com>
2015-12-10 04:49:47 +0200

    [jit] Optimize signed division by constant
    Commit migrated from https://github.com/mono/mono/commit/3bc3787974ae185937e929590ba435d3fdb03cb8

--- src/mono/mono/mini/local-propagation.c
+++ src/mono/mono/mini/local-propagation.c
@@ -49,6 +49,11 @@ struct magic_unsigned {
 	int shift;
 };
 
+struct magic_signed {
+	gint32 magic_number;
+	int shift;
+};
+
 /* http://www.hackersdelight.org/hdcodetxt/magicu.c.txt */
 static struct magic_unsigned
 compute_magic_unsigned (guint32 divisor) {
@@ -92,6 +97,49 @@ compute_magic_unsigned (guint32 divisor) {
 	magu.magic_number = q2 + 1;
 	magu.shift = p - 32;
 	return magu;
+}
+
+/* http://www.hackersdelight.org/hdcodetxt/magic.c.txt */
+static struct magic_signed
+compute_magic_signed (gint32 divisor) {
+	int p;
+	guint32 ad, anc, delta, q1, r1, q2, r2, t;
+	const guint32 two31 = 0x80000000;
+	struct magic_signed mag;
+
+	ad = abs (divisor);
+	t = two31 + ((unsigned)divisor >> 31);
+	anc = t - 1 - t % ad;
+	p = 31;
+	q1 = two31 / anc;
+	r1 = two31 - q1 * anc;
+	q2 = two31 / ad;
+	r2 = two31 - q2 * ad;
+	do {
+		p++;
+		q1 *= 2;
+		r1 *= 2;
+		if (r1 >= anc) {
+			q1++;
+			r1 -= anc;
+		}
+
+		q2 *= 2;
+		r2 *= 2;
+
+		if (r2 >= ad) {
+			q2++;
+			r2 -= ad;
+		}
+
+		delta = ad - r2;
+	} while (q1 < delta || (q1 == delta && r1 == 0));
+
+	mag.magic_number = q2 + 1;
+	if (divisor < 0)
+		mag.magic_number = -mag.magic_number;
+	mag.shift = p - 32;
+	return mag;
 }
 #endif
 
@@ -139,6 +187,55 @@ mono_strength_reduction_division (MonoCompile *cfg, MonoInst *ins)
 			} else {
 				MONO_EMIT_NEW_BIALU_IMM (cfg, OP_LSHR_UN_IMM, ins->dreg, tmp_regl, 32 + mag.shift);
 			}
+			break;
+		}
+		case OP_IDIV_IMM: {
+			guint32 tmp_regl, dividend_reg;
+			struct magic_signed mag;
+			int power2 = mono_is_power_of_two (ins->inst_imm);
+
+			/* The decomposition doesn't handle exception throwing */
+			if (ins->inst_imm == 0 || ins->inst_imm == -1)
+				break;
+			allocated_vregs = TRUE;
+			if (power2 == 1) {
+				guint32 r1 = alloc_ireg (cfg);
+				MONO_EMIT_NEW_BIALU_IMM (cfg, OP_ISHR_UN_IMM, r1, ins->sreg1, 31);
+				MONO_EMIT_NEW_BIALU (cfg, OP_IADD, r1, r1, ins->sreg1);
+				MONO_EMIT_NEW_BIALU_IMM (cfg, OP_ISHR_IMM, ins->dreg, r1, 1);
+				break;
+			} else if (power2 > 0 && power2 < 31) {
+				guint32 r1 = alloc_ireg (cfg);
+				MONO_EMIT_NEW_BIALU_IMM (cfg, OP_ISHR_IMM, r1, ins->sreg1, 31);
+				MONO_EMIT_NEW_BIALU_IMM (cfg, OP_ISHR_UN_IMM, r1, r1, (32 - power2));
+				MONO_EMIT_NEW_BIALU (cfg, OP_IADD, r1, r1, ins->sreg1);
+				MONO_EMIT_NEW_BIALU_IMM (cfg, OP_ISHR_IMM, ins->dreg, r1, power2);
+				break;
+			}
+
+			/*
+			 * Replacement of signed division with multiplication,
+			 * shifts and additions Hacker's Delight, chapter 10-6.
+			 */
+			mag = compute_magic_signed (ins->inst_imm);
+			tmp_regl = alloc_lreg (cfg);
+			dividend_reg = alloc_lreg (cfg);
+			MONO_EMIT_NEW_I8CONST (cfg, tmp_regl, mag.magic_number);
+			MONO_EMIT_NEW_UNALU (cfg, OP_SEXT_I4, dividend_reg, ins->sreg1);
+			MONO_EMIT_NEW_BIALU (cfg, OP_LMUL, tmp_regl, dividend_reg, tmp_regl);
+			if ((ins->inst_imm > 0 && mag.magic_number < 0) || (ins->inst_imm < 0 && mag.magic_number > 0)) {
+				MONO_EMIT_NEW_BIALU_IMM (cfg, OP_LSHR_IMM, tmp_regl, tmp_regl, 32);
+				if (ins->inst_imm > 0 && mag.magic_number < 0) {
+					MONO_EMIT_NEW_BIALU (cfg, OP_LADD, tmp_regl, tmp_regl, dividend_reg);
+				} else if (ins->inst_imm < 0 && mag.magic_number > 0) {
+					MONO_EMIT_NEW_BIALU (cfg, OP_LSUB, tmp_regl, tmp_regl, dividend_reg);
+				}
+				MONO_EMIT_NEW_BIALU_IMM (cfg, OP_LSHR_IMM, tmp_regl, tmp_regl, mag.shift);
+			} else {
+				MONO_EMIT_NEW_BIALU_IMM (cfg, OP_LSHR_IMM, tmp_regl, tmp_regl, 32 + mag.shift);
+			}
+			MONO_EMIT_NEW_BIALU_IMM (cfg, OP_LSHR_UN_IMM, ins->dreg, tmp_regl, SIZEOF_REGISTER * 8 - 1);
+			MONO_EMIT_NEW_BIALU (cfg, OP_LADD, ins->dreg, ins->dreg, tmp_regl);
 			break;
 		}
 	}
@@ -213,32 +310,9 @@ mono_strength_reduction_ins (MonoCompile *cfg, MonoInst *ins, const char **spec)
 		}
 		break;
 	}
-	case OP_IDIV_UN_IMM: {
-		allocated_vregs = mono_strength_reduction_division (cfg, ins);
-		break;
-	}
+	case OP_IDIV_UN_IMM:
 	case OP_IDIV_IMM: {
-		int c = ins->inst_imm;
-		int power2 = mono_is_power_of_two (c);
-
-		if (power2 == 1) {
-			int r1 = mono_alloc_ireg (cfg);
-
-			MONO_EMIT_NEW_BIALU_IMM (cfg, OP_ISHR_UN_IMM, r1, ins->sreg1, 31);
-			MONO_EMIT_NEW_BIALU (cfg, OP_IADD, r1, r1, ins->sreg1);
-			MONO_EMIT_NEW_BIALU_IMM (cfg, OP_ISHR_IMM, ins->dreg, r1, 1);
-
-			allocated_vregs = TRUE;
-		} else if (power2 > 0 && power2 < 31) {
-			int r1 = mono_alloc_ireg (cfg);
-
-			MONO_EMIT_NEW_BIALU_IMM (cfg, OP_ISHR_IMM, r1, ins->sreg1, 31);
-			MONO_EMIT_NEW_BIALU_IMM (cfg, OP_ISHR_UN_IMM, r1, r1, (32 - power2));
-			MONO_EMIT_NEW_BIALU (cfg, OP_IADD, r1, r1, ins->sreg1);
-			MONO_EMIT_NEW_BIALU_IMM (cfg, OP_ISHR_IMM, ins->dreg, r1, power2);
-
-			allocated_vregs = TRUE;
-		}
+		allocated_vregs = mono_strength_reduction_division (cfg, ins);
 		break;
 	}
 #if SIZEOF_REGISTER == 8
