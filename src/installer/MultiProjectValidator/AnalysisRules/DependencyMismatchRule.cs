14028fbe8109aba68a0a0facabd7312066703c5b
Bryan <brthor@microsoft.com>
2015-12-10 19:01:40 -0800

    Multi Project Validator A tool which searches recursively for project.json files, runs a set of analyses and reports on the result.
    For CLI we have only one rule currently, that Dependencies
    between projects must be equivalent to avoid stomping.
    
    
    Commit migrated from https://github.com/dotnet/core-setup/commit/6b2583d793f864a295bdf697d9c41e3e72671bb8

---
+++ src/installer/MultiProjectValidator/AnalysisRules/DependencyMismatchRule.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.DotNet.ProjectModel;
+
+namespace MultiProjectValidator.AnalysisRules
+{
+    public class DependencyMismatchRule : IAnalysisRule
+    {
+        public AnalysisResult Evaluate(List<ProjectContext> projectContexts)
+        {
+            var targetGroupedContexts = GroupContextsByTarget(projectContexts);
+
+            var failureMessages = EvaluateTargetContextGroups(targetGroupedContexts);
+            var pass = failureMessages.Count == 0;
+
+            var result = new AnalysisResult(failureMessages, pass);
+            return result;
+        }
+
+        private List<string> EvaluateTargetContextGroups(Dictionary<string, List<ProjectContext>> targetGroupedContexts)
+        {
+            var failureMessages = new List<string>();
+
+            foreach (var target in targetGroupedContexts.Keys)
+            {
+                var targetContexts = targetGroupedContexts[target];
+
+                failureMessages.AddRange(EvaluateTargetContextGroup(targetContexts));
+            }
+
+            return failureMessages;
+        }
+
+        private List<string> EvaluateTargetContextGroup(List<ProjectContext> targetContexts)
+        {
+            var failedMessages = new List<string>();
+            var assemblyVersionMap = new Dictionary<string, string>();
+
+            foreach(var context in targetContexts)
+            {
+                var libraries = context.LibraryManager.GetLibraries();
+
+                foreach(var library in libraries)
+                {
+                    var name = library.Identity.Name;
+                    var version = library.Identity.Version.ToString();
+
+                    if (assemblyVersionMap.ContainsKey(name))
+                    {
+                        var existingVersion = assemblyVersionMap[name];
+                        if (!string.Equals(existingVersion, version, StringComparison.OrdinalIgnoreCase))
+                        {
+                            string message = 
+                                $"Dependency mismatch in {context.ProjectFile.ProjectFilePath} for dependency {name}. Versions {version}, {existingVersion}";
+
+                            failedMessages.Add(message);
+                        }
+                    }
+                    else
+                    {
+                        assemblyVersionMap[name] = version;
+                    }
+                }
+            }
+
+            return failedMessages;
+        }
+
+        private Dictionary<string, List<ProjectContext>> GroupContextsByTarget(List<ProjectContext> projectContexts)
+        {
+            var targetContextMap = new Dictionary<string, List<ProjectContext>>();
+            foreach (var context in projectContexts)
+            {
+                var target = context.TargetFramework + context.RuntimeIdentifier;
+
+                if (targetContextMap.ContainsKey(target))
+                {
+                    targetContextMap[target].Add(context);
+                }
+                else
+                {
+                    targetContextMap[target] = new List<ProjectContext>()
+                    {
+                        context
+                    };
+                }
+            }
+
+            return targetContextMap;
+        }
+    }
+}
---
+++ src/installer/MultiProjectValidator/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Microsoft.DotNet.ProjectModel;
+using System.Linq;
+
+namespace MultiProjectValidator
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            var rootPath = ParseAndValidateArgs(args);
+
+            List<ProjectContext> projects = null;
+            try
+            {
+                projects = ProjectLoader.Load(rootPath);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Failed to load projects from path: " + rootPath);
+                Exit(1);
+            }
+
+            var analyzer = ProjectAnalyzer.Create(projects);
+            var analysisResults = analyzer.DoAnalysis();
+            var failed = analysisResults.Where((a) => !a.Passed).Any();
+
+            PrintAnalysisResults(analysisResults);
+
+            return failed ? 1 : 0;
+        }
+
+        private static void PrintAnalysisResults(List<AnalysisResult> analysisResults)
+        {
+            Console.WriteLine("Project Validation Results");
+
+            var failedCount = analysisResults.Where((a) => !a.Passed).Count();
+            var passedCount = analysisResults.Where((a) => a.Passed).Count();
+
+            Console.WriteLine($"{passedCount} Successful Analysis Rules");
+            Console.WriteLine($"{failedCount} Failed Analysis Rules");
+
+            if(failedCount != 0)
+            {
+                Console.WriteLine("Failure Messages");
+
+                foreach(var result in analysisResults)
+                {
+                    if (!result.Passed)
+                    {
+                        foreach(var message in result.Messages)
+                        {
+                            Console.WriteLine(message);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static bool AnyAnalysisFailed(List<AnalysisResult> analysisResults)
+        {
+            foreach (var result in analysisResults)
+            {
+                if (!result.Passed)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static string ParseAndValidateArgs(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                PrintHelp();
+                Exit(1);
+            }
+
+            string rootPath = args[0];
+
+            if (!Directory.Exists(rootPath))
+            {
+                Console.WriteLine("Root Directory does not exist: " + rootPath);
+                Exit(1);
+            }
+
+            return rootPath;
+        }
+
+        private static void Exit(int code)
+        {
+            Environment.Exit(code);
+        }
+        
+        private static void PrintHelp()
+        {
+            var help = @"
+Multi-Project Validator
+
+Description:
+This tool recursively searches for project.json's from the given root path.
+It then applies a set of analysis rules, determines whether they pass/fail
+and then sets exit code to reflect.
+
+Usage:
+pjvalidate [root path of recursive search]";
+
+            Console.WriteLine(help);
+        }
+    }
+}
