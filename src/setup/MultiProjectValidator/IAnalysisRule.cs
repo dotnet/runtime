9ceb2db685d0497ce3e7402237239febdbe6ef94
Bryan <brthor@microsoft.com>
2015-12-10 19:01:40 -0800

    Multi Project Validator A tool which searches recursively for project.json files, runs a set of analyses and reports on the result.
    For CLI we have only one rule currently, that Dependencies
    between projects must be equivalent to avoid stomping.
    
    
    Commit migrated from https://github.com/dotnet/core-setup/commit/6b2583d793f864a295bdf697d9c41e3e72671bb8

---
+++ src/setup/MultiProjectValidator/IAnalysisRule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.DotNet.ProjectModel;
+
+namespace MultiProjectValidator
+{
+    public interface IAnalysisRule
+    {
+        AnalysisResult Evaluate(List<ProjectContext> projectContexts);
+    }
+}
