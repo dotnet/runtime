7d952ff9d9e54bfefd6631cffa6aefc42e9cc7fd
Mike McLaughlin <mikem@microsoft.com>
2015-12-15 16:42:20 -0800

    Add debugger launch to dbgshim for xplat.
    Add the RegisterForRuntimeStartup/UnregisterForRuntimeStartup to dbghim. Executes
    the callback when the coreclr runtime starts in the specified process. The callback
    is passed the proper ICorDebug instance for the version of the runtime or an error if
    something fails. This API works for launch and attach (and even the attach scenario
    if the runtime hasn't been loaded yet) equally on both xplat and Windows. The callback
    is always called on a separate thread. This API returns immediately.
    
    The callback is invoke when the coreclr runtime module is loaded during early
    initialization. The runtime is blocked during initialization until the callback
    returns.
    
    HRESULT
    RegisterForRuntimeStartup(
        __in DWORD dwProcessId,
        __in PSTARTUP_CALLBACK pfnCallback,
        __in PVOID parameter,
        __out PVOID *ppUnregisterToken)
    
    HRESULT
    UnregisterForRuntimeStartup(
        __in PVOID pUnregisterToken)
    
    Most of the work is done for xplat in the PAL_RegisterForRuntimeStartup and
    PAL_UnregisterForRuntimeStartup. On Windows, the APIs are implemented on top
    of the old dbgshim ones.
    
    Added reference counting to DbgTransportSession so the cleanup can be done after
    the transport worker and the main code is finished.
    
    Fix a hang in OSX initializing multiple PALs in the debugging test dbg, dbgshim
    and mscordaccore by not calling FILEInitStdHandles() from PAL_InitializeDLL.
    
    Fixed a minor EnumerateCLRs bug in an error path. A ThrowHR instead of returning
    the HRESULT.
    
    Better pipe file/dbg transport cleanup. Now also call the dbg transport connection
    abort for an unhandled native exception. Added PROCAbort to replace most calls to
    abort(). The shutdown handler is called in PROCAbort().
    
    Cleanup debugger transport pipes on CTRL-C termination.
    
    Cleanup process code; remove now useless CProcSharedData.
    
    Added "PROCESS" PAL trace type.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/dac4a490f66ef501fb8d7b246ed3c21a8bcfa485

--- src/clr/src/dlls/dbgshim/dbgshim.h
+++ src/clr/src/dlls/dbgshim/dbgshim.h
@@ -9,28 +9,52 @@
 
 #include <windows.h>
 
-EXTERN_C HRESULT GetStartupNotificationEvent(DWORD debuggeePID,
-                                    __out HANDLE* phStartupEvent);
+typedef VOID (*PSTARTUP_CALLBACK)(IUnknown *pCordb, PVOID parameter, HRESULT hr);
 
-EXTERN_C HRESULT CloseCLREnumeration(HANDLE* pHandleArray, LPWSTR* pStringArray, DWORD dwArrayLength);
+EXTERN_C HRESULT
+RegisterForRuntimeStartup(
+    __in DWORD dwProcessId,
+    __in PSTARTUP_CALLBACK pfnCallback,
+    __in PVOID parameter,
+    __out PVOID *ppUnregisterToken);
 
-EXTERN_C HRESULT EnumerateCLRs(DWORD debuggeePID, 
-                      __out HANDLE** ppHandleArrayOut,
-                      __out LPWSTR** ppStringArrayOut,
-                      __out DWORD* pdwArrayLengthOut);
+EXTERN_C HRESULT
+UnregisterForRuntimeStartup(
+    __in PVOID pUnregisterToken);
 
-EXTERN_C HRESULT CreateVersionStringFromModule(DWORD pidDebuggee,
-                                      LPCWSTR szModuleName,
-                                      __out_ecount_part(cchBuffer, *pdwLength) LPWSTR pBuffer,
-                                      DWORD cchBuffer,
-                                      __out DWORD* pdwLength);
+EXTERN_C HRESULT 
+GetStartupNotificationEvent(
+    __in DWORD debuggeePID,
+    __out HANDLE* phStartupEvent);
 
-EXTERN_C HRESULT CreateDebuggingInterfaceFromVersionEx(
-    int iDebuggerVersion,
-    LPCWSTR szDebuggeeVersion,
-    IUnknown ** ppCordb);
+EXTERN_C HRESULT 
+EnumerateCLRs(DWORD debuggeePID, 
+    __out HANDLE** ppHandleArrayOut,
+    __out LPWSTR** ppStringArrayOut,
+    __out DWORD* pdwArrayLengthOut);
 
-EXTERN_C HRESULT CreateDebuggingInterfaceFromVersion(
-    LPCWSTR szDebuggeeVersion, 
-    IUnknown ** ppCordb);
+EXTERN_C HRESULT 
+CloseCLREnumeration(
+    __in HANDLE* pHandleArray,
+    __in LPWSTR* pStringArray,
+    __in DWORD dwArrayLength);
+
+EXTERN_C HRESULT 
+CreateVersionStringFromModule(
+    __in DWORD pidDebuggee,
+    __in LPCWSTR szModuleName,
+    __out_ecount_part(cchBuffer, *pdwLength) LPWSTR pBuffer,
+    __in DWORD cchBuffer,
+    __out DWORD* pdwLength);
+
+EXTERN_C HRESULT 
+CreateDebuggingInterfaceFromVersionEx(
+    __in int iDebuggerVersion,
+    __in LPCWSTR szDebuggeeVersion,
+    __out IUnknown ** ppCordb);
+
+EXTERN_C HRESULT 
+CreateDebuggingInterfaceFromVersion(
+    __in LPCWSTR szDebuggeeVersion, 
+    __out IUnknown ** ppCordb);
 
