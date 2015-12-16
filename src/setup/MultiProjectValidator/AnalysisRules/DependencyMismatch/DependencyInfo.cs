98bb0eb15051c8e677c72f26e4efa9fa8d7169aa
Bryan <brthor@microsoft.com>
2015-12-15 18:09:08 -0800

    Change Output format to match PR feedback
    Don't use Environment.Exit()
    
    PR Feedback, Unecessary Usings and Immutable types
    
    
    Commit migrated from https://github.com/dotnet/core-setup/commit/8d04d95ebd77d03369305e3ec3d44520d9201b60

---
+++ src/setup/MultiProjectValidator/AnalysisRules/DependencyMismatch/DependencyInfo.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.DotNet.ProjectModel;
+
+namespace ProjectSanity.AnalysisRules.DependencyMismatch
+{
+    internal class DependencyInfo
+    {
+        public static DependencyInfo Create(ProjectContext context, LibraryDescription library)
+        {
+            return new DependencyInfo
+            {
+                ProjectPath = context.ProjectFile.ProjectFilePath,
+                Version = library.Identity.Version.ToString(),
+                Name = library.Identity.Name
+            };
+        }
+
+        public string ProjectPath { get; private set; }
+        public string Version { get; private set; }
+        public string Name { get; private set; }
+    }
+}
--- src/setup/MultiProjectValidator/ProjectLoader.cs
+++ src/setup/MultiProjectValidator/ProjectLoader.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.DotNet.ProjectModel;
 
 namespace MultiProjectValidator
