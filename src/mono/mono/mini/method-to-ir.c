67c7c6245638d7608c517da63dae18b5542ad79f
Zoltan Varga <vargaz@gmail.com>
2015-12-17 01:20:13 -0500

    [jit] Fix the support for gshared types in mini_emit_initobj (), previously we were treating them as reference types, leading to stack overwrites. Fixes mono/mono#37079.
    Commit migrated from https://github.com/mono/mono/commit/b8285895e7331af43ad10136c98a15c74b4b1d83

--- src/mono/mono/mini/method-to-ir.c
+++ src/mono/mono/mini/method-to-ir.c
@@ -3469,6 +3469,8 @@ mini_emit_initobj (MonoCompile *cfg, MonoInst *dest, const guchar *ip, MonoClass
 		return;
 	}
 
+	klass = mono_class_from_mono_type (mini_get_underlying_type (&klass->byval_arg));
+
 	n = mono_class_value_size (klass, &align);
 
 	if (n <= sizeof (gpointer) * 8) {
