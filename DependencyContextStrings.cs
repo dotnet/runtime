836740a86c8511366ad2234e30b71ec2b7c70e76
Pavel Krymets <pavel@krymets.com>
2015-12-17 15:04:18 -0800

    Add dependency model api

---
+++ DependencyContextStrings.cs
@@ -0,0 +1,23 @@
+namespace Microsoft.Extensions.DependencyModel
+{
+    internal class DependencyContextStrings
+    {
+        internal const char VersionSeperator = '/';
+
+        internal const string CompileTimeAssembliesKey = "compile";
+
+        internal const string RunTimeAssembliesKey = "runtime";
+
+        internal const string LibrariesPropertyName = "libraries";
+
+        internal const string TargetsPropertyName = "targets";
+
+        internal const string DependenciesPropertyName = "dependencies";
+
+        internal const string Sha512PropertyName = "sha512";
+
+        internal const string TypePropertyName = "type";
+
+        internal const string ServiceablePropertyName = "serviceable";
+    }
+}
