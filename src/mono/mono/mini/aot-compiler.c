7b6f0a079ec6c980d5d788b7df1a00e84489e7ac
Zoltan Varga <vargaz@gmail.com>
2015-12-18 01:38:23 -0500

    [runtime] Remove some debug printfs.
    Commit migrated from https://github.com/mono/mono/commit/3cfbb97b1740091d2be166d63eeaa29971ba27ce

--- src/mono/mono/mini/aot-compiler.c
+++ src/mono/mono/mini/aot-compiler.c
@@ -3574,7 +3574,6 @@ add_wrappers (MonoAotCompile *acfg)
 				for (i = 0; i < 16; ++i) {
 					sig = mini_get_gsharedvt_out_sig_wrapper_signature ((variants & 1) > 0, (variants & 2) > 0, i);
 					add_extra_method (acfg, mono_marshal_get_runtime_invoke_for_sig (sig));
-					printf ("X: %s\n", mono_method_full_name (mono_marshal_get_runtime_invoke_for_sig (sig), 1));
 
 					g_free (sig);
 				}
