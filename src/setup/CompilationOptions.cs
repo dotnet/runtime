6b7a041ec2b2c9547cc5e3b34e53d9b24cc03b0b
Pavel Krymets <pavel@krymets.com>
2015-12-21 10:36:20 -0800

    Add compilation options to dependency context
    Commit migrated from https://github.com/dotnet/core-setup/commit/e38fb7509fdbae62c7615827061aba7e6af8ee5a

---
+++ src/setup/CompilationOptions.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace Microsoft.Extensions.DependencyModel
+{
+    public class CompilationOptions
+    {
+        public IEnumerable<string> Defines { get; }
+
+        public string LanguageVersion { get; }
+
+        public string Platform { get; }
+
+        public bool? AllowUnsafe { get; }
+
+        public bool? WarningsAsErrors { get; }
+
+        public bool? Optimize { get; }
+
+        public string KeyFile { get; }
+
+        public bool? DelaySign { get; }
+
+        public bool? PublicSign { get; }
+
+        public bool? EmitEntryPoint { get; }
+
+        public CompilationOptions(IEnumerable<string> defines,
+            string languageVersion,
+            string platform,
+            bool? allowUnsafe,
+            bool? warningsAsErrors,
+            bool? optimize,
+            string keyFile,
+            bool? delaySign,
+            bool? publicSign,
+            bool? emitEntryPoint)
+        {
+            Defines = defines;
+            LanguageVersion = languageVersion;
+            Platform = platform;
+            AllowUnsafe = allowUnsafe;
+            WarningsAsErrors = warningsAsErrors;
+            Optimize = optimize;
+            KeyFile = keyFile;
+            DelaySign = delaySign;
+            PublicSign = publicSign;
+            EmitEntryPoint = emitEntryPoint;
+        }
+    }
+}
