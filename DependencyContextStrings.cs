e38fb7509fdbae62c7615827061aba7e6af8ee5a
Pavel Krymets <pavel@krymets.com>
2015-12-21 10:36:20 -0800

    Add compilation options to dependency context

--- DependencyContextStrings.cs
+++ DependencyContextStrings.cs
@@ -19,5 +19,27 @@ namespace Microsoft.Extensions.DependencyModel
         internal const string TypePropertyName = "type";
 
         internal const string ServiceablePropertyName = "serviceable";
+
+        internal const string CompilationOptionsPropertName = "compilationOptions";
+
+        internal const string DefinesPropertyName = "defines";
+
+        internal const string LanguageVersionPropertyName = "languageVersion";
+
+        internal const string PlatformPropertyName = "platform";
+
+        internal const string AllowUnsafePropertyName = "allowUnsafe";
+
+        internal const string WarningsAsErrorsPropertyName = "warningsAsErrors";
+
+        internal const string OptimizePropertyName = "optimize";
+
+        internal const string KeyFilePropertyName = "keyFile";
+
+        internal const string DelaySignPropertyName = "delaySign";
+
+        internal const string PublicSignPropertyName = "publicSign";
+
+        internal const string EmitEntryPointPropertyName = "emitEntryPoint";
     }
 }
