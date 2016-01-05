1e2cb743851c9eb3c87dc071f81e8aa125ca1a4b
Pat Gavlin <pagavlin@microsoft.com>
2016-01-04 16:58:07 -0800

    Port more JIT tests.
    Most of these tests were blocked on unnecessary usage
    of SerializableAttribute and Environment.ExitCode.

---
+++ tests/src/JIT/Generics/Instantiation/delegates/Delegate021.cs
@@ -0,0 +1,42 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Threading;
+
+internal delegate T GenDelegate<T>(T p1, out T p2);
+
+internal interface IFoo
+{
+    U Function<U>(U i, out U j);
+}
+
+internal struct Foo : IFoo
+{
+    public U Function<U>(U i, out U j)
+    {
+        j = i;
+        return i;
+    }
+}
+
+internal class Test
+{
+    public static int Main()
+    {
+        int i, j;
+        IFoo inst = new Foo();
+        GenDelegate<int> MyDelegate = new GenDelegate<int>(inst.Function<int>);
+        i = MyDelegate(10, out j);
+
+        if ((i != 10) || (j != 10))
+        {
+            Console.WriteLine("Failed Sync Invokation");
+            return 1;
+        }
+
+        Console.WriteLine("Test Passes");
+        return 100;
+    }
+}
+
