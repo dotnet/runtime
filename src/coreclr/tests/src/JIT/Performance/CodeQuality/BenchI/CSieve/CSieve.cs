f479ebfe606d36fd5994c718f5f01e145985ffc1
Andy Ayers <andya@microsoft.com>
2016-01-06 18:31:45 -0800

    Add more integer benchmarks
    The remainder of our simple integer benchmarks: AddArray2, Array1, BenchE, CSieve, Permutate, Puzzle, and TreeInsert.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/8c887eb84af27fdc45086feb739f3e9125847554

---
+++ src/coreclr/tests/src/JIT/Performance/CodeQuality/BenchI/CSieve/CSieve.cs
@@ -0,0 +1,81 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+// Based on Eratosthenes Sieve Prime Number Program in C, Byte Magazine, January 1983.
+
+using Microsoft.Xunit.Performance;
+using System;
+using System.Runtime.CompilerServices;
+using Xunit;
+
+[assembly: OptimizeForBenchmarks]
+[assembly: MeasureInstructionsRetired]
+
+public static class CSieve
+{
+
+#if DEBUG
+    public const int Iterations = 1;
+#else
+    public const int Iterations = 200;
+#endif
+
+    const int Size = 8190;
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    static bool Bench() {
+        bool[] flags = new bool[Size + 1];
+        int count = 0;
+        for (int iter = 1; iter <= Iterations; iter++)
+        {
+            count = 0;
+
+            // Initially, assume all are prime
+            for (int i = 0; i <= Size; i++)
+            {
+                flags[i] = true;
+            }
+
+            // Refine
+            for (int i = 2; i <= Size; i++)
+            {
+                if (flags[i])
+                {
+                    // Found a prime
+                    for (int k = i + i; k <= Size; k += i)
+                    {
+                        // Cancel its multiples
+                        flags[k] = false;
+                    }
+                    count++;
+                }
+            }
+        }
+
+        return (count == 1027);
+    }
+
+    [Benchmark]
+    public static void Test() {
+        foreach (var iteration in Benchmark.Iterations) {
+            using (iteration.StartMeasurement()) {
+                for (int i = 0; i < Iterations; i++) {
+                    Bench();
+                }
+            }
+        }
+    }
+
+    static bool TestBase() {
+        bool result = true;
+        for (int i = 0; i < Iterations; i++) {
+            result &= Bench();
+        }
+        return result;
+    }
+    
+    public static int Main() {
+        bool result = TestBase();
+        return (result ? 100 : -1);
+    }
+}
