414ab4ee1a6f31ae63f166de2b9d4d0af640574f
Andy Ayers <andya@microsoft.com>
2016-01-07 23:13:23 -0800

    Add a subset of benchmark game benchmarks
    Add tests that are not multithreaded and don't rely on large input files. These are adapted for xunit-performance.
    
    Add license and readme.

---
+++ tests/src/JIT/Performance/CodeQuality/BenchmarksGame/binarytrees/binarytrees.csharp.cs
@@ -0,0 +1,148 @@
+/* The Computer Language Benchmarks Game
+   http://benchmarksgame.alioth.debian.org/ 
+
+   contributed by Marek Safar  
+
+   modified for use with xunit-performance
+*/
+
+using Microsoft.Xunit.Performance;
+using System;
+using System.Runtime.CompilerServices;
+using Xunit;
+
+[assembly: OptimizeForBenchmarks]
+[assembly: MeasureInstructionsRetired]
+
+public class BinaryTrees
+{
+    private const int minDepth = 4;
+    private const int Iterations = 1;
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private static bool Bench(bool verbose = false)
+    {
+        int n = 16;
+        int maxDepth = Math.Max(minDepth + 2, n);
+        int stretchDepth = maxDepth + 1;
+        int t = 0;
+
+        int check = (TreeNode.bottomUpTree(0, stretchDepth)).itemCheck();
+        if (verbose)
+        {
+            Console.WriteLine("stretch tree of depth {0}\t check: {1}", stretchDepth, check);
+        }
+        t += check;
+
+        TreeNode longLivedTree = TreeNode.bottomUpTree(0, maxDepth);
+
+        for (int depth = minDepth; depth <= maxDepth; depth += 2)
+        {
+            int iterations = 1 << (maxDepth - depth + minDepth);
+
+            check = 0;
+            for (int i = 1; i <= iterations; i++)
+            {
+                check += (TreeNode.bottomUpTree(i, depth)).itemCheck();
+                check += (TreeNode.bottomUpTree(-i, depth)).itemCheck();
+            }
+
+            if (verbose)
+            {
+                Console.WriteLine("{0}\t trees of depth {1}\t check: {2}",
+                    iterations * 2, depth, check);
+            }
+
+            t += check;
+        }
+
+        if (verbose)
+        {
+            Console.WriteLine("long lived tree of depth {0}\t check: {1}",
+                maxDepth, longLivedTree.itemCheck());
+        }
+
+        t += check;
+
+        return (t == -174785);
+    }
+
+    private struct TreeNode
+    {
+        private class Next
+        {
+            public TreeNode left, right;
+        }
+
+        private Next _next;
+        private int _item;
+
+        private TreeNode(int item)
+        {
+            _item = item;
+            _next = null;
+        }
+
+        internal static TreeNode bottomUpTree(int item, int depth)
+        {
+            if (depth > 0)
+            {
+                return new TreeNode(
+                     bottomUpTree(2 * item - 1, depth - 1)
+                   , bottomUpTree(2 * item, depth - 1)
+                   , item
+                   );
+            }
+            else
+            {
+                return new TreeNode(item);
+            }
+        }
+
+        private TreeNode(TreeNode left, TreeNode right, int item)
+        {
+            _next = new Next();
+            _next.left = left;
+            _next.right = right;
+            _item = item;
+        }
+
+        internal int itemCheck()
+        {
+            // if necessary deallocate here
+            if (_next == null) return _item;
+            else return _item + _next.left.itemCheck() - _next.right.itemCheck();
+        }
+    }
+
+    [Benchmark]
+    public static void Test()
+    {
+        foreach (var iteration in Benchmark.Iterations)
+        {
+            using (iteration.StartMeasurement())
+            {
+                for (int i = 0; i < Iterations; i++)
+                {
+                    Bench();
+                }
+            }
+        }
+    }
+
+    private static bool TestBase()
+    {
+        bool result = true;
+        for (int i = 0; i < Iterations; i++)
+        {
+            result &= Bench(true);
+        }
+        return result;
+    }
+
+    public static int Main()
+    {
+        bool result = TestBase();
+        return (result ? 100 : -1);
+    }
+}
---
+++ tests/src/JIT/Performance/CodeQuality/BenchmarksGame/fastaredux/fastaredux.csharp.cs
@@ -0,0 +1,181 @@
+/* The Computer Language Benchmarks Game
+   http://benchmarksgame.alioth.debian.org/
+
+   contributed by Robert F. Tobler
+   optimized based on java & C# by Enotus, Isaac Gouy, and Alp Toker
+
+   modified for use with xunit-performance
+*/
+
+using Microsoft.Xunit.Performance;
+using System;
+using System.IO;
+using System.Text;
+
+[assembly: OptimizeForBenchmarks]
+[assembly: MeasureInstructionsRetired]
+
+public static class FastaRedux
+{
+#if DEBUG
+    private const int Iterations = 1;
+#else
+    const int Iterations = 5;
+#endif
+
+    public static int Main(string[] args)
+    {
+        AccumulateAndScale(s_homoSapiens);
+        AccumulateAndScale(s_IUB);
+        int n = args.Length > 0 ? Int32.Parse(args[0]) : 2500;
+        using (Stream s = Console.OpenStandardOutput())
+        {
+            s.WriteRepeatFasta("ONE", "Homo sapiens alu", Encoding.ASCII.GetBytes(s_ALU), n * 2);
+            s.WriteRandomFasta("TWO", "IUB ambiguity codes", s_IUB, n * 3);
+            s.WriteRandomFasta("THREE", "Homo sapiens frequency", s_homoSapiens, n * 5);
+        }
+        return 100;
+    }
+
+    [Benchmark]
+    public static void Bench()
+    {
+        int n = 2500000;
+        AccumulateAndScale(s_homoSapiens);
+        AccumulateAndScale(s_IUB);
+        foreach (var iteration in Benchmark.Iterations)
+        {
+            using (iteration.StartMeasurement())
+            {
+                using (Stream s = Stream.Null)
+                {
+                    for (int i = 0; i < Iterations; i++)
+                    {
+                        s.WriteRepeatFasta("ONE", "Homo sapiens alu", Encoding.ASCII.GetBytes(s_ALU), n * 2);
+                        s.WriteRandomFasta("TWO", "IUB ambiguity codes", s_IUB, n * 3);
+                        s.WriteRandomFasta("THREE", "Homo sapiens frequency", s_homoSapiens, n * 5);
+                    }
+                }
+            }
+        }
+    }
+
+    private const int LINE_LEN = 60;
+    private const int BUF_LEN = 64 * 1024;
+    private const byte LF = (byte)'\n';
+
+    private const int LOOKUP_LEN = 1024;
+    private const double LOOKUP_SCALE = LOOKUP_LEN - 1;
+
+    private static readonly string s_ALU =
+        "GGCCGGGCGCGGTGGCTCACGCCTGTAATCCCAGCACTTTGG" +
+        "GAGGCCGAGGCGGGCGGATCACCTGAGGTCAGGAGTTCGAGA" +
+        "CCAGCCTGGCCAACATGGTGAAACCCCGTCTCTACTAAAAAT" +
+        "ACAAAAATTAGCCGGGCGTGGTGGCGCGCGCCTGTAATCCCA" +
+        "GCTACTCGGGAGGCTGAGGCAGGAGAATCGCTTGAACCCGGG" +
+        "AGGCGGAGGTTGCAGTGAGCCGAGATCGCGCCACTGCACTCC" +
+        "AGCCTGGGCGACAGAGCGAGACTCCGTCTCAAAAA";
+
+    private struct Freq
+    {
+        public double P;
+        public byte C;
+
+        public Freq(char c, double p) { C = (byte)c; P = p; }
+    }
+
+    private static Freq[] s_IUB = {
+      new Freq('a', 0.27), new Freq('c', 0.12), new Freq('g', 0.12),
+      new Freq('t', 0.27), new Freq('B', 0.02), new Freq('D', 0.02),
+      new Freq('H', 0.02), new Freq('K', 0.02), new Freq('M', 0.02),
+      new Freq('N', 0.02), new Freq('R', 0.02), new Freq('S', 0.02),
+      new Freq('V', 0.02), new Freq('W', 0.02), new Freq('Y', 0.02),
+    };
+
+    private static Freq[] s_homoSapiens = {
+      new Freq ('a', 0.3029549426680), new Freq ('c', 0.1979883004921),
+      new Freq ('g', 0.1975473066391), new Freq ('t', 0.3015094502008),
+    };
+
+    private static void AccumulateAndScale(Freq[] a)
+    {
+        double cp = 0.0;
+        for (int i = 0; i < a.Length; i++)
+            a[i].P = (cp += a[i].P) * LOOKUP_SCALE;
+        a[a.Length - 1].P = LOOKUP_SCALE;
+    }
+
+    private static byte[] s_buf = new byte[BUF_LEN];
+
+    private static int WriteDesc(this byte[] buf, string id, string desc)
+    {
+        var ds = Encoding.ASCII.GetBytes(">" + id + " " + desc + "\n");
+        for (int i = 0; i < ds.Length; i++) buf[i] = ds[i];
+        return BUF_LEN - ds.Length;
+    }
+
+    private static int Min(int a, int b) { return a < b ? a : b; }
+
+    private static void WriteRepeatFasta(
+          this Stream s, string id, string desc, byte[] alu, int nr)
+    {
+        int alen = alu.Length;
+        int ar = alen, br = s_buf.WriteDesc(id, desc), lr = LINE_LEN;
+        while (nr > 0)
+        {
+            int r = Min(Min(nr, lr), Min(ar, br));
+            for (int ai = alen - ar, bi = BUF_LEN - br, be = bi + r;
+                bi < be; bi++, ai++)
+                s_buf[bi] = alu[ai];
+            nr -= r; lr -= r; br -= r; ar -= r;
+            if (ar == 0) ar = alen;
+            if (br == 0) { s.Write(s_buf, 0, BUF_LEN); br = BUF_LEN; }
+            if (lr == 0) { s_buf[BUF_LEN - (br--)] = LF; lr = LINE_LEN; }
+            if (br == 0) { s.Write(s_buf, 0, BUF_LEN); br = BUF_LEN; }
+        }
+        if (lr < LINE_LEN) s_buf[BUF_LEN - (br--)] = LF;
+        if (br < BUF_LEN) s.Write(s_buf, 0, BUF_LEN - br);
+    }
+
+    private static Freq[] s_lookup = new Freq[LOOKUP_LEN];
+
+    private static void CreateLookup(Freq[] fr)
+    {
+        for (int i = 0, j = 0; i < LOOKUP_LEN; i++)
+        {
+            while (fr[j].P < i) j++;
+            s_lookup[i] = fr[j];
+        }
+    }
+
+    private const int IM = 139968;
+    private const int IA = 3877;
+    private const int IC = 29573;
+    private const double SCALE = LOOKUP_SCALE / IM;
+
+    private static int s_last = 42;
+
+    private static void WriteRandomFasta(
+          this Stream s, string id, string desc, Freq[] fr, int nr)
+    {
+        CreateLookup(fr);
+        int br = s_buf.WriteDesc(id, desc), lr = LINE_LEN;
+        while (nr > 0)
+        {
+            int r = Min(Min(nr, lr), br);
+            for (int bi = BUF_LEN - br, be = bi + r; bi < be; bi++)
+            {
+                double p = SCALE * (s_last = (s_last * IA + IC) % IM);
+                int ai = (int)p; if (s_lookup[ai].P < p) ai++;
+                s_buf[bi] = s_lookup[ai].C;
+            }
+            nr -= r; lr -= r; br -= r;
+            if (br == 0) { s.Write(s_buf, 0, BUF_LEN); br = BUF_LEN; }
+            if (lr == 0) { s_buf[BUF_LEN - (br--)] = LF; lr = LINE_LEN; }
+            if (br == 0) { s.Write(s_buf, 0, BUF_LEN); br = BUF_LEN; }
+        }
+        if (lr < LINE_LEN) s_buf[BUF_LEN - (br--)] = LF;
+        if (br < BUF_LEN) s.Write(s_buf, 0, BUF_LEN - br);
+    }
+}
+
