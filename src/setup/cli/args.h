8710e7d6141308770e17ab0c8ebdfc69bac21d24
PiotrP <piotrp@microsoft.com>
2016-01-11 13:10:55 -0800

    PR Feedback
    Commit migrated from https://github.com/dotnet/core-setup/commit/010581b98d9401b00b0b6a08c860230fd04302b1

--- src/setup/cli/args.h
+++ src/setup/cli/args.h
@@ -8,7 +8,7 @@
 #include "pal.h"
 #include "trace.h"
 
-static const pal::string_t s_depsArgPrefix = _X("--corehost-depsfile:");
+static const pal::string_t s_depsArgPrefix = _X("--depsfile:");
 
 struct arguments_t
 {
