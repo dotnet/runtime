27aa23e3b53eb88c0edac89142429a8d70103626
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2016-01-12 16:36:31 -0800

    switch from DNX to cross-plat NuGet!
    Commit migrated from https://github.com/dotnet/core-setup/commit/0d7f03b8055e3b376796bef2fd4c0232129fee53

--- src/setup/common/pal.windows.cpp
+++ src/setup/common/pal.windows.cpp
@@ -75,7 +75,7 @@ bool pal::get_default_packages_directory(string_t* recv)
     {
         return false;
     }
-    append_path(&*recv, _X(".dnx"));
+    append_path(&*recv, _X(".nuget"));
     append_path(&*recv, _X("packages"));
     return true;
 }
