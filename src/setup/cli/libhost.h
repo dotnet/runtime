90851c1a7edcc230c50d67ab80dc61c6b20439ac
schellap <schellap@microsoft.com>
2016-01-13 21:35:31 -0800

    Fix host name, debuggability and embed CRT
    Commit migrated from https://github.com/dotnet/core-setup/commit/e540b6f55ce6e4b0429faa2e6721e28f7cb0afe8

--- src/setup/cli/libhost.h
+++ src/setup/cli/libhost.h
@@ -1,4 +1,4 @@
 // Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#define LIBHOST_NAME MAKE_LIBNAME("clihost")
+#define LIBHOST_NAME MAKE_LIBNAME("hostpolicy")
