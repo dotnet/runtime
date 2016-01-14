4aaa0015c0646f04b2617b406ed762506e28ef1f
Rahul Kumar <rahku@microsoft.com>
2016-01-14 12:02:03 -0800

    Add OSS license headers to all new files in github
    [tfs-changeset: 1565108]
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/ff05597e5a09e0b5a25adb45c68c5deb392bdd03

--- src/clr/src/ToolBox/PdbTypeMatch/regs.h
+++ src/clr/src/ToolBox/PdbTypeMatch/regs.h
@@ -1,3 +1,8 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
 extern const wchar_t * const rgRegX86[];
 extern const wchar_t * const rgRegAMD64[];
 extern const wchar_t * const rgRegMips[];
