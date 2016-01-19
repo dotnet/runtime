0bdc1be3ef94d8f18384b70a245b9fdf3d355381
Vlad Brezae <brezaevlad@gmail.com>
2016-01-19 19:43:04 +0200

    [sgen] Don't prefetch unused memory when scanning the card table
    When doing the mod union scan, we use the block's cardtable (instead of the global one), which we also cannot prefetch, since that would mean dereferencing the block info.
    
    
    Commit migrated from https://github.com/mono/mono/commit/32ec26d7f48ecf417b9076c0105970c96588b905

--- src/mono/mono/sgen/sgen-marksweep.c
+++ src/mono/mono/sgen/sgen-marksweep.c
@@ -2326,10 +2326,12 @@ major_scan_card_table (gboolean mod_union, ScanCopyContext ctx)
 		int prefetch_index = __index + 6;
 		if (prefetch_index < allocated_blocks.next_slot) {
 			MSBlockInfo *prefetch_block = BLOCK_UNTAG (allocated_blocks.data [prefetch_index]);
-			guint8 *prefetch_cards = sgen_card_table_get_card_scan_address ((mword)MS_BLOCK_FOR_BLOCK_INFO (prefetch_block));
 			PREFETCH_READ (prefetch_block);
-			PREFETCH_WRITE (prefetch_cards);
-			PREFETCH_WRITE (prefetch_cards + 32);
+			if (!mod_union) {
+				guint8 *prefetch_cards = sgen_card_table_get_card_scan_address ((mword)MS_BLOCK_FOR_BLOCK_INFO (prefetch_block));
+				PREFETCH_WRITE (prefetch_cards);
+				PREFETCH_WRITE (prefetch_cards + 32);
+			}
                 }
 #endif
 
