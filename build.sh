5998f75a480eca0651051374a4e6c712bae830f8
Kamil Rytarowski <n54@gmx.com>
2016-01-21 20:03:31 +0100

    NetBSD: Add support for retrieving the number of available CPUs
    $ uname
    NetBSD
    $ getconf NPROCESSORS_ONLN
    2
    $ getconf _NPROCESSORS_ONLN
    getconf: _NPROCESSORS_ONLN: unknown variable

--- build.sh
+++ build.sh
@@ -129,6 +129,8 @@ build_coreclr()
     # processors available to a single process.
     if [ `uname` = "FreeBSD" ]; then
         NumProc=`sysctl hw.ncpu | awk '{ print $2+1 }'`
+    elif [ `uname` = "NetBSD" ]; then
+        NumProc=$(($(getconf NPROCESSORS_ONLN)+1))
     else
         NumProc=$(($(getconf _NPROCESSORS_ONLN)+1))
     fi
