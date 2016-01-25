37fc52b1320574e19d1cbfe1553d8ff0b34e05d7
Rahul Kumar <rahku@microsoft.com>
2016-01-25 14:50:20 -0800

    add helix run support to coreclr repo
    Commit migrated from https://github.com/dotnet/coreclr/commit/a8741647cfa038e67a644237bd0806b78fb77b46

--- src/clr/build.cmd
+++ src/clr/build.cmd
@@ -176,7 +176,8 @@ echo %__MsgPrefix%Commencing CoreCLR Repo build
 set "__BinDir=%__RootBinDir%\Product\%__BuildOS%.%__BuildArch%.%__BuildType%"
 set "__IntermediatesDir=%__RootBinDir%\obj\%__BuildOS%.%__BuildArch%.%__BuildType%"
 set "__PackagesBinDir=%__BinDir%\.nuget"
-set "__TestBinDir=%__RootBinDir%\tests\%__BuildOS%.%__BuildArch%.%__BuildType%"
+set "__TestRootDir=%__RootBinDir%\tests"
+set "__TestBinDir=%__TestRootDir%\%__BuildOS%.%__BuildArch%.%__BuildType%"
 set "__TestIntermediatesDir=%__RootBinDir%\tests\obj\%__BuildOS%.%__BuildArch%.%__BuildType%"
 
 :: Use this variable to locate dynamically generated files; the actual location though will be different.
