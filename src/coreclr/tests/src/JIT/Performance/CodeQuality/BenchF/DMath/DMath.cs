763fcd8e2de01157c5948857ca77a24d78b75ca1
Chris McKinsey <chrismck@microsoft.com>
2016-02-01 11:35:32 -0800

    Add 15 more Benchstone floating-point (BenchF) benchmarks in Xunit format. Update all BenchF sources with CodeFormatter tool.
    Commit migrated from https://github.com/dotnet/coreclr/commit/830f80b067dbccbe477c34186473fb096e71f784

--- src/coreclr/tests/src/JIT/Performance/CodeQuality/BenchF/DMath/DMath.cs
+++ src/coreclr/tests/src/JIT/Performance/CodeQuality/BenchF/DMath/DMath.cs
@@ -13,89 +13,99 @@ using Xunit;
 
 public static class DMath
 {
-
 #if DEBUG
     public const int Iterations = 1;
 #else
     public const int Iterations = 100000;
 #endif
 
-    const double Deg2Rad = 57.29577951;
-    static volatile object VolatileObject;
+    private const double Deg2Rad = 57.29577951;
+    private static volatile object s_volatileObject;
 
-    static void Escape(object obj) {
-        VolatileObject = obj;
+    private static void Escape(object obj)
+    {
+        s_volatileObject = obj;
     }
 
-    static double Fact(double n)
+    private static double Fact(double n)
     {
-       double res;
-       res = 1.0;
-       while (n > 0.0) {
-          res *= n;
-          n -= 1.0;
-       }
+        double res;
+        res = 1.0;
+        while (n > 0.0)
+        {
+            res *= n;
+            n -= 1.0;
+        }
 
-       return res;
+        return res;
     }
 
-    static double Power(double n, double p)
+    private static double Power(double n, double p)
     {
-       double res;
-       res = 1.0;
-       while (p > 0.0) {
-          res *= n;
-          p -= 1.0;
-       }
+        double res;
+        res = 1.0;
+        while (p > 0.0)
+        {
+            res *= n;
+            p -= 1.0;
+        }
 
-       return res;
+        return res;
     }
 
     [MethodImpl(MethodImplOptions.NoInlining)]
-    static bool Bench(int loop)
+    private static bool Bench(int loop)
     {
-       double[] sines = new double[91];
-       double angle, radians, sine, worksine, temp, k;
-       double diff;
+        double[] sines = new double[91];
+        double angle, radians, sine, worksine, temp, k;
+        double diff;
 
-       for (int iter = 1; iter <= loop; iter++) {
-           for (angle = 0.0; angle <= 90.0; angle += 1.0) {
-               radians = angle / Deg2Rad;
-               k = 0.0;
-               worksine = 0.0;
-               do {
-                   sine = worksine;
-                   temp = (2.0 * k) + 1.0;
-                   worksine += (Power(-1.0, k) / Fact(temp)) * Power(radians, temp);
-                   k += 1.0;
-                   diff = Math.Abs(sine - worksine);
-               } while (diff > 1E-8);
+        for (int iter = 1; iter <= loop; iter++)
+        {
+            for (angle = 0.0; angle <= 90.0; angle += 1.0)
+            {
+                radians = angle / Deg2Rad;
+                k = 0.0;
+                worksine = 0.0;
+                do
+                {
+                    sine = worksine;
+                    temp = (2.0 * k) + 1.0;
+                    worksine += (Power(-1.0, k) / Fact(temp)) * Power(radians, temp);
+                    k += 1.0;
+                    diff = Math.Abs(sine - worksine);
+                } while (diff > 1E-8);
 
-               sines[(int)angle] = worksine;
-           }
-       }
+                sines[(int)angle] = worksine;
+            }
+        }
 
-       // Escape sines array so that its elements appear live-out
-       Escape(sines);
+        // Escape sines array so that its elements appear live-out
+        Escape(sines);
 
-       return true;
+        return true;
     }
 
     [Benchmark]
-    public static void Test() {
-        foreach (var iteration in Benchmark.Iterations) {
-            using (iteration.StartMeasurement()) {
+    public static void Test()
+    {
+        foreach (var iteration in Benchmark.Iterations)
+        {
+            using (iteration.StartMeasurement())
+            {
                 Bench(Iterations);
             }
         }
     }
 
-    static bool TestBase() {
+    private static bool TestBase()
+    {
         bool result = Bench(Iterations);
         return result;
     }
 
-    public static int Main() {
+    public static int Main()
+    {
         bool result = TestBase();
         return (result ? 100 : -1);
     }
