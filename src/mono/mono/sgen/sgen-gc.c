98c777161dc2e9a6922c0018b201e96d8c77cf27
Vlad Brezae <brezaevlad@gmail.com>
2016-02-01 18:14:46 +0200

    [sgen] Remove fixed FIXME comment
    Commit migrated from https://github.com/mono/mono/commit/5f06b81c0d61468a83dcf447483b86ff64b5f550

--- src/mono/mono/sgen/sgen-gc.c
+++ src/mono/mono/sgen/sgen-gc.c
@@ -1549,11 +1549,6 @@ collect_nursery (SgenGrayQueue *unpin_queue, gboolean finish_up_concurrent_mark)
 	SGEN_LOG (2, "Finding pinned pointers: %zd in %ld usecs", sgen_get_pinned_count (), TV_ELAPSED (btv, atv));
 	SGEN_LOG (4, "Start scan with %zd pinned objects", sgen_get_pinned_count ());
 
-	/*
-	 * FIXME: When we finish a concurrent collection we do a nursery collection first,
-	 * as part of which we scan the card table.  Then, later, we scan the mod union
-	 * cardtable.  We should only have to do one.
-	 */
 	sj = (ScanJob*)sgen_thread_pool_job_alloc ("scan remset", job_remembered_set_scan, sizeof (ScanJob));
 	sj->ops = object_ops;
 	sgen_workers_enqueue_job (&sj->job, FALSE);
