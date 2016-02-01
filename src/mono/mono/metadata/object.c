6fb8b51c4dec33cd9bb368a7daa136e61edbd616
Ludovic Henry <ludovic@xamarin.com>
2016-02-01 13:11:44 +0000

    [coop] Ensure we exit blocking mode when invoking managed code
    mono_runtime_invoke might be called by P/Invoked code, which is in blocking mode. Not switching back to running can lead to a crash when hitting a safepoint.
    
    
    Commit migrated from https://github.com/mono/mono/commit/0c0ab39407654f9f41f9e790d85b386bc627c8c3

--- src/mono/mono/metadata/object.c
+++ src/mono/mono/metadata/object.c
@@ -2904,7 +2904,11 @@ mono_runtime_invoke (MonoMethod *method, void *obj, void **params, MonoObject **
 	if (mono_profiler_get_events () & MONO_PROFILE_METHOD_EVENTS)
 		mono_profiler_method_start_invoke (method);
 
+	MONO_PREPARE_RESET_BLOCKING;
+
 	result = do_runtime_invoke (method, obj, params, exc);
+
+	MONO_FINISH_RESET_BLOCKING;
 
 	if (mono_profiler_get_events () & MONO_PROFILE_METHOD_EVENTS)
 		mono_profiler_method_end_invoke (method);
