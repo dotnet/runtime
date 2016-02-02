c8df018ab7ffe635af08089a2605bf95516f5d93
Kamil Rytarowski <n54@gmx.com>
2016-02-02 02:52:32 +0100

    Fix build on NetBSD: cast ptr to function to ptr to data

--- src/utilcode/delayloadhelpers.cpp
+++ src/utilcode/delayloadhelpers.cpp
@@ -93,7 +93,7 @@ namespace DelayLoad
             HMODULE hMod = nullptr;
             IfFailRet(m_pModule->GetValue(&hMod));
 
-            LPVOID pvFunc = ::GetProcAddress(hMod, m_szFunctionName);
+            LPVOID pvFunc = reinterpret_cast<LPVOID>(::GetProcAddress(hMod, m_szFunctionName));
             hr = (pvFunc == nullptr) ? HRESULT_FROM_GetLastError() : S_OK;
             
             {   // Lock scope
@@ -112,4 +112,3 @@ namespace DelayLoad
         return m_hr;
     }
 }
-
