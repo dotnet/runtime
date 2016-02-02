2fa8c4b671cb40920d457e43c3915cea2d26ce8f
Kamil Rytarowski <n54@gmx.com>
2016-02-02 02:58:56 +0100

    Fix build on NetBSD: Introduce check for pthread_getcpuclockid(3)
    This function is part of the optional part of POSIX.
    
    It's available in:
    - FreeBSD,
    - AIX,
    - QNX,
    - Linux.
    
    It's missing in NetBSD.
    
    According to Jan Kotas (Microsoft) this function is used for undocumented
    resource monitoring feature (FEATURE_APPDOMAIN_RESOURCE_MONITORING) and
    unlikely anybody is using this today.
    
    Solution suggested in #2947 "NetBSD doesn't implement pthread_getcpuclockid(3)"

--- src/pal/src/thread/thread.cpp
+++ src/pal/src/thread/thread.cpp
@@ -1413,7 +1413,9 @@ CorUnix::GetThreadTimesInternal(
 
     pTargetThread->Lock(pThread);
 
+#if HAVE_PTHREAD_GETCPUCLOCKID
     if (pthread_getcpuclockid(pTargetThread->GetPThreadSelf(), &cid) != 0)
+#endif
     {
         ASSERT("Unable to get clock from thread\n", hThread);
         SetLastError(ERROR_INTERNAL_ERROR);
