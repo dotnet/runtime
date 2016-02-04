e541fe5a2a1e73a75ceb021a6d7adf38bf66f238
Kamil Rytarowski <n54@gmx.com>
2016-02-04 20:07:04 +0100

    Fix build on NetBSD: Walkaround compiler strictness regarding const conversion
    This commit walks-around over-cautious Clang/LLVM alerts like:
    
    /tmp/pkgsrc-tmp/wip/coreclr-git/work/coreclr/src/pal/tests/palsuite/c_runtime/sscanf/test4/test4.c:32:33: error: implicit conversion from
                    'int' to 'short' changes value from 65535 to -1 [-Werror,-Wconstant-conversion]
     DoShortNumTest("-1", "%hd", 65535);
                    ^~~~~~~~~~~~~~~~~~~
    
    This closes dotnet/coreclr#2968 "implicit conversion from 'int' to 'short' changes value from 65535 to -1"
    
    Thanks Jan Kotas (Microsoft) for pointers.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/5f38e169674d3970bc97d61b5a14b28027ebc0cb

--- src/coreclr/src/pal/tests/palsuite/c_runtime/sscanf/test5/test5.c
+++ src/coreclr/src/pal/tests/palsuite/c_runtime/sscanf/test5/test5.c
@@ -18,6 +18,8 @@
 
 int __cdecl main(int argc, char *argv[])
 {
+    int n65535 = 65535; /* Walkaround compiler strictness */
+
     if (PAL_Initialize(argc, argv))
     {
         return FAIL;
@@ -28,7 +30,7 @@ int __cdecl main(int argc, char *argv[])
     DoNumTest("-1", "%i", -1);
     DoNumTest("0x1234", "%i", 0x1234);
     DoNumTest("012", "%i", 10);
-    DoShortNumTest("-1", "%hi", 65535);
+    DoShortNumTest("-1", "%hi", n65535);
     DoShortNumTest("65536", "%hi", 0);
     DoNumTest("-1", "%li", -1);
     DoNumTest("65536", "%li", 65536);
--- src/coreclr/src/pal/tests/palsuite/c_runtime/swscanf/test7/test7.c
+++ src/coreclr/src/pal/tests/palsuite/c_runtime/swscanf/test7/test7.c
@@ -19,6 +19,7 @@
 
 int __cdecl main(int argc, char *argv[])
 {
+    int n65535 = 65535; /* Walkaround compiler strictness */
 
     if (PAL_Initialize(argc, argv))
     {
@@ -30,7 +31,7 @@ int __cdecl main(int argc, char *argv[])
     DoNumTest(convert("-1"), convert("%x"), -1);
     DoNumTest(convert("0x1234"), convert("%x"), 0x1234);
     DoNumTest(convert("012"), convert("%x"), 0x12);
-    DoShortNumTest(convert("-1"), convert("%hx"), 65535);
+    DoShortNumTest(convert("-1"), convert("%hx"), n65535);
     DoShortNumTest(convert("10000"), convert("%hx"), 0);
     DoNumTest(convert("-1"), convert("%lx"), -1);
     DoNumTest(convert("10000"), convert("%lx"), 65536);
