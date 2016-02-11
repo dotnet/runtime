61c5ac28b65af3c603b8d1d78e505e75444368e7
Pavel Krymets <pavel@krymets.com>
2016-02-10 20:13:56 -0800

    Refactor DependencyModel, make it ready for tests
    Commit migrated from https://github.com/dotnet/core-setup/commit/2aa18b5ccb5fcf46deda0cd55a725a64cb577964

---
+++ src/setup/FileWrapper.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.IO;
+
+namespace Microsoft.Extensions.EnvironmentAbstractions
+{
+    internal class FileWrapper: IFile
+    {
+        public bool Exists(string path)
+        {
+            return File.Exists(path);
+        }
+
+        public string ReadAllText(string path)
+        {
+            return File.ReadAllText(path);
+        }
+    }
+}
---
+++ src/setup/IEnvironment.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Extensions.EnvironmentAbstractions
+{
+    internal interface IEnvironment
+    {
+        string GetEnvironmentVariable(string name);
+    }
+}
