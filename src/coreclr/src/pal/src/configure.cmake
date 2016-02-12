9622aef523f713650960b3b8c42a667407eb3726
Kamil Rytarowski <n54@gmx.com>
2016-02-12 03:46:46 +0100

    Fix build issue on NetBSD: Include <stdlib.h> for alloca(3)
    Header <alloca.h> isn't needed on other supported systems either.
    
    /tmp/pkgsrc-tmp/wip/coreclr-git/work/coreclr/src/pal/src/file/directory.cpp:605:
    warning: Warning: reference to the libc supplied alloca(3); this most likely will
    not work. Please use the compiler provided version of alloca(3), by supplying the
    appropriate compiler flags (e.g. not -std=c89).
    
    $ uname -a
    NetBSD chieftec 7.99.25 NetBSD 7.99.25 (GENERIC) dotnet/coreclr#0: Fri Dec 25 20:51:06 UTC 2015  root@chieftec:/tmp/netbsd-tmp/sys/arch/amd64/compile/GENERIC amd64
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/3639bd23882a45f2248bc6296340dc76a0e21f33

--- src/coreclr/src/pal/src/configure.cmake
+++ src/coreclr/src/pal/src/configure.cmake
@@ -19,7 +19,6 @@ endif()
 list(APPEND CMAKE_REQUIRED_DEFINITIONS -D_FILE_OFFSET_BITS=64)
 
 check_include_files(ieeefp.h HAVE_IEEEFP_H)
-check_include_files(alloca.h HAVE_ALLOCA_H)
 check_include_files(sys/vmparam.h HAVE_SYS_VMPARAM_H)
 check_include_files(mach/vm_types.h HAVE_MACH_VM_TYPES_H)
 check_include_files(mach/vm_param.h HAVE_MACH_VM_PARAM_H)
