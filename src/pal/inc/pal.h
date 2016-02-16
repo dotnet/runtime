6952d90ad3edfc461ecbe081f121419f4072326c
Kamil Rytarowski <n54@gmx.com>
2016-02-16 22:55:13 +0100

    Fix NetBSD build issue: Use compiler version of alloca(3) when possible
    The documentation of alloca(3) says:
    
         o   The alloca() function should be supplied by the compiler because the
             compiler is allowed to make assumptions about the stack and frame
             pointers.  The libc alloca() implementation cannot account for those
             assumptions.  While there is a machine dependent implementation of
             alloca() in libc, its use is discouraged and in most cases it will
             not work.  Using this implementation will produce linker warnings.
    
    This is true for CoreCLR - alloca(3) was breaking 30 PAL tests.
    
    Enforce on this operating system a compiler version.
    
    Thanks Jan Kotas (Microsoft) for inspiration what may go wrong.
    Thanks Jan Vorli (Microsoft) for useful suggestions in order to fix it.

--- src/pal/inc/pal.h
+++ src/pal/inc/pal.h
@@ -6340,11 +6340,6 @@ PALIMPORT char * __cdecl _strdup(const char *);
 #endif //_MSC_VER
 
 #if defined(__GNUC__) && defined(PLATFORM_UNIX)
-// we set -fno-builtin on the command line. This requires that if
-// we use alloca, with either have to call __builtin_alloca, or
-// ensure that the alloca call doesn't happen in code which is
-// modifying the stack (such as "memset (alloca(x), y, z)"
-
 #define alloca  __builtin_alloca
 #endif // __GNUC__
 
