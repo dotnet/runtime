afe7b3f7dd2c4802fc95d2edcd4cf97a2a88ac2e
Tarek Mahmoud Sayed <tarekms@microsoft.com>
2016-02-18 14:19:09 -0800

    Small refactoring in DateTime comparing methods
    Commit migrated from https://github.com/dotnet/coreclr/commit/0148107cff7f462a601d91a8a119208b11a832a4

--- src/coreclr/src/mscorlib/src/System/DateTime.cs
+++ src/coreclr/src/mscorlib/src/System/DateTime.cs
@@ -487,20 +487,12 @@ namespace System {
             if (!(value is DateTime)) {
                 throw new ArgumentException(Environment.GetResourceString("Arg_MustBeDateTime"));
             }
-    
-            long valueTicks = ((DateTime)value).InternalTicks;
-            long ticks = InternalTicks;
-            if (ticks > valueTicks) return 1;
-            if (ticks < valueTicks) return -1;
-            return 0;
+            
+            return Compare(this, (DateTime)value);
         }
 
         public int CompareTo(DateTime value) {
-            long valueTicks = value.InternalTicks;
-            long ticks = InternalTicks;
-            if (ticks > valueTicks) return 1;
-            if (ticks < valueTicks) return -1;
-            return 0;
+            return Compare(this, value);
         }
     
         // Returns the tick count corresponding to the given year, month, and day.
