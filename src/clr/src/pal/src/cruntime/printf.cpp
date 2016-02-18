6c3a35f0a3423c0d625775aeb217dc0cfa109ab4
Kamil Rytarowski <n54@gmx.com>
2016-02-18 23:41:56 +0100

    Fix issue unveiled on NetBSD: Add PAL__vsnprintf shadow in PAL
    NetBSD's libc internally uses a function named _vsnprintf. We need to mask
    PAL's version of this function to remove clash with the system. With it
    snprintf(3) calls were jumping out of libc and landing back to PAL.
    
    It was unveiled with many PAL tests.
    
    Detailed documentation about _symbols in the NetBSD libc is documented
    in the NetBSD sources in a file: src/lib/libc/README
    
    Retire unneeded _vsnprintf().
    
    Thanks Jan Vorli (Microsoft) for help with debugging.
    Thanks Jan Kotas (Microsoft) for suggesting the right solution.
    
    Fix dotnet/coreclr#3199
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/fd1f866f28c71570a7ef80dd1159a5cd755f8bf3

--- src/clr/src/pal/src/cruntime/printf.cpp
+++ src/clr/src/pal/src/cruntime/printf.cpp
@@ -295,7 +295,7 @@ wsprintfA(
     ENTRY("wsprintfA (buffer=%p, format=%p (%s))\n", buffer, format, format);
 
     va_start(ap, format);
-    Length = PAL__vsnprintf(buffer, 1024, format, ap);
+    Length = InternalVsnprintf(CorUnix::InternalGetCurrentThread(), buffer, 1024, format, ap);
     va_end(ap);
 
     LOGEXIT("wsprintfA returns int %d\n", Length);
@@ -354,7 +354,7 @@ _snprintf(
           buffer, (unsigned long) count, format, format);
 
     va_start(ap, format);
-    Length = PAL__vsnprintf(buffer, count, format, ap);
+    Length = InternalVsnprintf(CorUnix::InternalGetCurrentThread(), buffer, count, format, ap);
     va_end(ap);
 
     LOGEXIT("_snprintf returns int %d\n", Length);
@@ -1529,7 +1529,7 @@ PAL_sprintf(
     ENTRY("PAL_sprintf (buffer=%p, format=%p (%s))\n", buffer, format, format);
 
     va_start(ap, format);
-    Length = PAL__vsnprintf(buffer, 0x7fffffff, format, ap);
+    Length = InternalVsnprintf(CorUnix::InternalGetCurrentThread(), buffer, 0x7fffffff, format, ap);
     va_end(ap);
 
     LOGEXIT("PAL_sprintf returns int %d\n", Length);
@@ -1613,42 +1613,13 @@ PAL_vsprintf(char *buffer,
     ENTRY("PAL_vsprintf (buffer=%p, format=%p (%s), argptr=%p)\n", 
           buffer, format, format, argptr);
 
-    Length = PAL__vsnprintf(buffer, 0x7fffffff, format, argptr);
+    Length = InternalVsnprintf(CorUnix::InternalGetCurrentThread(), buffer, 0x7fffffff, format, argptr);
 
     LOGEXIT("PAL_vsprintf returns int %d\n", Length);
     PERF_EXIT(vsprintf);
 
     return Length;
 }
-
-
-/*++
-Function:
-  _vsnprintf
-
-See MSDN doc.
---*/
-int 
-__cdecl 
-_vsnprintf(char *buffer, 
-           size_t count, 
-           const char *format, 
-           va_list argptr)
-{
-    LONG Length;
-
-    PERF_ENTRY(_vsnprintf);
-    ENTRY("_vsnprintf (buffer=%p, count=%d, format=%p (%s), argptr=%p)\n", 
-          buffer, count, format, format, argptr);
-
-    Length = PAL__vsnprintf(buffer, count, format, argptr);
-
-    LOGEXIT("_vsnprintf returns int %d\n", Length);
-    PERF_EXIT(_vsnprintf);
-
-    return Length;
-}
-
 
 
 /*++
@@ -1785,4 +1756,3 @@ static int SscanfFloatCheckExponent(LPCSTR buff, LPCSTR floatFmt,
     return ret;
 }
 #endif // SSCANF_CANNOT_HANDLE_MISSING_EXPONENT
-
--- src/clr/src/pal/src/cruntime/printfcpp.cpp
+++ src/clr/src/pal/src/cruntime/printfcpp.cpp
@@ -1068,7 +1068,18 @@ Parameters:
 
 int __cdecl PAL__vsnprintf(LPSTR Buffer, size_t Count, LPCSTR Format, va_list ap)
 {
-    return CoreVsnprintf(InternalGetCurrentThread(), Buffer, Count, Format, ap);
+    LONG Length;
+
+    PERF_ENTRY(PAL__vsnprintf);
+    ENTRY("PAL__vsnprintf (buffer=%p, count=%d, format=%p (%s), argptr=%p)\n",
+          Buffer, Count, Format, Format, ap);
+
+    Length = CoreVsnprintf(InternalGetCurrentThread(), Buffer, Count, Format, ap);
+
+    LOGEXIT("PAL__vsnprintf returns int %d\n", Length);
+    PERF_EXIT(PAL__vsnprintf);
+
+    return Length;
 }
 
 /*******************************************************************************
