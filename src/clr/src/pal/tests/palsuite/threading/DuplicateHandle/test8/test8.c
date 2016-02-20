261934fb7e23680444bdd8f8029a921ccc4caf9e
Kamil Rytarowski <n54@gmx.com>
2016-02-21 00:41:04 +0100

    Disable not relevant tests for NetBSD: SCHED_OTHER may not be reassignable
    This addresses the following issues:
    
    threading/GetCurrentThread/test1/paltest_getcurrentthread_test1. Exit code: 1
    threading/GetCurrentThread/test2/paltest_getcurrentthread_test2. Exit code: 1
    threading/GetProcessTimes/test2/paltest_getprocesstimes_test2. Exit code: 1
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/c341d9b33e5b827e48f52671a21bf3dde89fd5c8

--- src/clr/src/pal/tests/palsuite/threading/DuplicateHandle/test8/test8.c
+++ src/clr/src/pal/tests/palsuite/threading/DuplicateHandle/test8/test8.c
@@ -36,6 +36,13 @@ int __cdecl main(int argc, char* argv[])
     {
         return (FAIL);
     }
+
+#if !HAVE_SCHED_OTHER_ASSIGNABLE
+    /* Defining thread priority for SCHED_OTHER is implementation defined.
+       Some platforms like NetBSD cannot reassign it as they are dynamic.
+    */
+    printf("paltest_duplicatehandle_test8 has been disabled on this platform\n");
+#else
     
     /* Create a thread.*/
     hThread = CreateThread(NULL,            /* SD*/
@@ -143,6 +150,9 @@ int __cdecl main(int argc, char* argv[])
     /* Clean-up thread and Terminate the PAL.*/
     CloseHandle(hThread);
     CloseHandle(hDupThread);
+
+#endif
+
     PAL_Terminate();
     return PASS;
 }
--- src/clr/src/pal/tests/palsuite/threading/GetCurrentThread/test2/test2.c
+++ src/clr/src/pal/tests/palsuite/threading/GetCurrentThread/test2/test2.c
@@ -90,7 +90,12 @@ INT __cdecl main( INT argc, CHAR **argv )
         return( FAIL );
     }
 
-
+#if !HAVE_SCHED_OTHER_ASSIGNABLE
+    /* Defining thread priority for SCHED_OTHER is implementation defined.
+       Some platforms like NetBSD cannot reassign it as they are dynamic.
+    */
+    printf("paltest_getcurrentthread_test1 has been disabled on this platform\n");
+#else
     /* Create multiple threads. */
     hThread = CreateThread(    NULL,         /* no security attributes    */
                                0,            /* use default stack size    */
@@ -132,10 +137,8 @@ INT __cdecl main( INT argc, CHAR **argv )
         Fail( "FAIL:Unexpected thread priority %d returned, expected %d\n",
                 g_priority, THREAD_PRIORITY_TIME_CRITICAL );
     }
-    
-    
+#endif    
 
     PAL_Terminate();
     return PASS;
 }
-
