d5fdbda7593b468370ea30e27aa220644c8b0c11
Kamil Rytarowski <n54@gmx.com>
2016-02-21 00:41:04 +0100

    Disable not relevant tests for NetBSD: SCHED_OTHER may not be reassignable
    This addresses the following issues:
    
    threading/GetCurrentThread/test1/paltest_getcurrentthread_test1. Exit code: 1
    threading/GetCurrentThread/test2/paltest_getcurrentthread_test2. Exit code: 1
    threading/GetProcessTimes/test2/paltest_getprocesstimes_test2. Exit code: 1
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/c341d9b33e5b827e48f52671a21bf3dde89fd5c8

--- src/coreclr/src/pal/tests/palsuite/threading/GetCurrentThread/test1/thread.c
+++ src/coreclr/src/pal/tests/palsuite/threading/GetCurrentThread/test1/thread.c
@@ -30,7 +30,13 @@ int __cdecl main( int argc, char **argv )
     {
         return ( FAIL );
     }
-   
+
+#if !HAVE_SCHED_OTHER_ASSIGNABLE
+    /* Defining thread priority for SCHED_OTHER is implementation defined.
+       Some platforms like NetBSD cannot reassign it as they are dynamic.
+    */
+    printf("paltest_getcurrentthread_test1 has been disabled on this platform\n");
+#else   
     hThread = GetCurrentThread();
     
     nPriority = GetThreadPriority(hThread);
@@ -79,6 +85,7 @@ int __cdecl main( int argc, char **argv )
 		    "function failed.  Failing test.\n");
 	}
     }
+#endif
 
     PAL_Terminate();
     return ( PASS );    
