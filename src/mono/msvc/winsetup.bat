5be6c2c468177a08f1b8905fc88cc0444b58a4b9
Alexander Köplinger <alex.koeplinger@outlook.com>
2016-02-23 17:03:11 +0100

    [msvc] %PATH% is sometimes empty in Jenkins, so "powershell" doesn't run
    Try using a system-standard default variable which points to the required folder instead.
    
    
    Commit migrated from https://github.com/mono/mono/commit/22cc7b350e13473429e08060f5f573fed7525dd8

--- src/mono/msvc/winsetup.bat
+++ src/mono/msvc/winsetup.bat
@@ -4,7 +4,7 @@ if exist config.h if not exist cygconfig.h copy config.h cygconfig.h
 if exist eglib\config.h if not exist eglib\cygconfig.h copy eglib\config.h eglib\cygconfig.h
 copy winconfig.h config.h
 copy eglib\winconfig.h eglib\config.h
-powershell -Command "(Get-Content config.h) -replace '#MONO_VERSION#', (Select-String -path configure.ac -pattern 'AC_INIT\(mono, \[(.*)\]').Matches[0].Groups[1].Value | Set-Content config.h"
+%windir%\system32\WindowsPowerShell\v1.0\powershell.exe -Command "(Get-Content config.h) -replace '#MONO_VERSION#', (Select-String -path configure.ac -pattern 'AC_INIT\(mono, \[(.*)\]').Matches[0].Groups[1].Value | Set-Content config.h"
 goto end
 :error
 echo fatal error: the VSDepenancies directory was not found in the "mono" directory
