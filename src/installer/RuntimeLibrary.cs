ed2fc77904ae7197cde161dd4371c83b867c69cc
Pavel Krymets <pavel@krymets.com>
2016-02-25 11:00:48 -0800

    Add support for json deps file
    Commit migrated from https://github.com/dotnet/core-setup/commit/26938c3ef5dac412e11462d273df869ec77d4b2e

--- src/installer/RuntimeLibrary.cs
+++ src/installer/RuntimeLibrary.cs
@@ -8,12 +8,39 @@ namespace Microsoft.Extensions.DependencyModel
 {
     public class RuntimeLibrary : Library
     {
-        public RuntimeLibrary(string libraryType, string packageName, string version, string hash, string[] assemblies, Dependency[] dependencies, bool serviceable)
+        public RuntimeLibrary(
+            string libraryType,
+            string packageName,
+            string version,
+            string hash,
+            string[] assemblies,
+            RuntimeTarget[] subTargets,
+            Dependency[] dependencies,
+            bool serviceable)
             : base(libraryType, packageName, version, hash, dependencies, serviceable)
         {
             Assemblies = assemblies.Select(path => new RuntimeAssembly(path)).ToArray();
+            SubTargets = subTargets;
         }
 
         public IReadOnlyList<RuntimeAssembly> Assemblies { get; }
+
+        public IReadOnlyList<RuntimeTarget> SubTargets { get; }
+    }
+
+    public class RuntimeTarget
+    {
+        public RuntimeTarget(string runtime, IReadOnlyList<RuntimeAssembly> assemblies, IReadOnlyList<string> nativeLibraries)
+        {
+            Runtime = runtime;
+            Assemblies = assemblies;
+            NativeLibraries = nativeLibraries;
+        }
+
+        public string Runtime { get; }
+
+        public IReadOnlyList<RuntimeAssembly> Assemblies { get; }
+
+        public IReadOnlyList<string> NativeLibraries { get; }
     }
 }
