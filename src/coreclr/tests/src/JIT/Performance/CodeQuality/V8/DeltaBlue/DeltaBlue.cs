f6df1c4be5869bf8f9303f4d33f7c699493492cf
Sean Gillespie <sean.william.g@gmail.com>
2016-02-26 22:37:01 -0800

    Fix license header for test files
    Commit migrated from https://github.com/dotnet/coreclr/commit/3ac97ffe15312ead86cb90220c0f945820abfa2e

--- src/coreclr/tests/src/JIT/Performance/CodeQuality/V8/DeltaBlue/DeltaBlue.cs
+++ src/coreclr/tests/src/JIT/Performance/CodeQuality/V8/DeltaBlue/DeltaBlue.cs
@@ -1,3 +1,6 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 /*
   This is a Java implemention of the DeltaBlue algorithm described in:
     "The DeltaBlue Algorithm: An Incremental Constraint Hierarchy Solver"
