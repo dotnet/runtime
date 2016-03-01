b9c76811598feccdfd150182943b8f07f9d116c5
Pavel Krymets <pavel@krymets.com>
2016-03-01 15:11:52 -0800

    Add more tests and some reading logic
    Commit migrated from https://github.com/dotnet/core-setup/commit/b386eb7c28578d3d3b5f98e97a88651fd5dc7092

---
+++ src/installer/DependencyContextJsonReaderTest.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Microsoft.Extensions.DependencyModel.Tests
+{
+    public class DependencyContextJsonReaderTest
+    {
+        private DependencyContext Read(string text)
+        {
+            using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(text)))
+            {
+                return new DependencyContextJsonReader().Read(stream);
+            }
+        }
+
+        [Fact]
+        public void ReadsRuntimeTargetInfo()
+        {
+            var context = Read(
+@"{
+    ""runtimeTarget"": {
+        ""portable"": false,
+        ""name"": "".NETStandardApp,Version=v1.5/osx.10.10-x64"",
+    },
+    ""targets"": {
+        "".NETStandardApp,Version=v1.5"": {},
+        "".NETStandardApp,Version=v1.5/osx.10.10-x64"": {},
+    }
+}");
+            context.IsPortable.Should().BeFalse();
+            context.Target.Should().Be(".NETStandardApp,Version=v1.5");
+            context.Runtime.Should().Be("osx.10.10-x64");
+        }
+
+        [Fact]
+        public void DefaultsToPortable()
+        {
+            var context = Read(
+@"{
+}");
+            context.IsPortable.Should().BeTrue();
+        }
+
+        [Fact]
+        public void ReadsMainTarget()
+        {
+            var context = Read(
+@"{
+    ""targets"": {
+        "".NETStandardApp,Version=v1.5"": {}
+    }
+}");
+            context.Target.Should().Be(".NETStandardApp,Version=v1.5");
+        }
+
+        [Fact]
+        public void ReadsRuntimeGraph()
+        {
+            var context = Read(
+@"{
+    ""runtimes"": {
+        "".NETStandardApp,Version=v1.5"": {
+            ""osx.10.10-x64"": [ ],
+            ""osx.10.11-x64"": [ ""osx"" ],
+            ""rhel.7-x64"": [ ""linux-x64"", ""unix"" ]
+        }
+    }
+}");
+            context.RuntimeGraph.Should().Contain(p => p.Key == "osx.10.10-x64").Which
+                .Value.Should().BeEquivalentTo();
+
+            context.RuntimeGraph.Should().Contain(p => p.Key == "osx.10.11-x64").Which
+                .Value.Should().BeEquivalentTo("osx");
+
+            context.RuntimeGraph.Should().Contain(p => p.Key == "rhel.7-x64").Which
+                .Value.Should().BeEquivalentTo("linux-x64", "unix");
+        }
+
+        [Fact]
+        public void ReadsCompilationTarget()
+        {
+            var context = Read(
+@"{
+    ""targets"": {
+        "".NETStandardApp,Version=v1.5"": {
+            ""MyApp/1.0.1"": {
+                ""dependencies"": {
+                    ""AspNet.Mvc"": ""1.0.0""
+                },
+                ""compile"": {
+                    ""MyApp.dll"": { }
+                }
+            },
+            ""System.Banana/1.0.0"": {
+                ""dependencies"": {
+                    ""System.Foo"": ""1.0.0""
+                },
+                ""compile"": {
+                    ""ref/dotnet5.4/System.Banana.dll"": { }
+                }
+            }
+        }
+    },
+    ""libraries"":{
+        ""MyApp/1.0.1"": {
+            ""type"": ""project"",
+            ""serviceable"": true,
+            ""sha512"": ""HASH-MyApp""
+        },
+        ""System.Banana/1.0.0"": {
+            ""type"": ""package"",
+            ""serviceable"": false,
+            ""sha512"": ""HASH-System.Banana""
+        },
+    }
+}");
+            context.CompileLibraries.Should().HaveCount(2);
+            var project = context.CompileLibraries.Should().Contain(l => l.PackageName == "MyApp").Subject;
+            project.Version.Should().Be("1.0.1");
+            project.Assemblies.Should().BeEquivalentTo("MyApp.dll");
+            project.Hash.Should().Be("HASH-MyApp");
+            project.LibraryType.Should().Be("project");
+            project.Serviceable.Should().Be(true);
+            project.Hash.Should().BeEquivalentTo("HASH-MyApp");
+
+
+            var package = context.CompileLibraries.Should().Contain(l => l.PackageName == "System.Banana").Subject;
+            package.Version.Should().Be("1.0.0");
+            package.Assemblies.Should().BeEquivalentTo("ref/dotnet5.4/System.Banana.dll");
+            package.Hash.Should().Be("HASH-System.Banana");
+            package.LibraryType.Should().Be("package");
+            package.Serviceable.Should().Be(false);
+        }
+    }
+}
---
+++ src/installer/JsonAssetions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using FluentAssertions.Primitives;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.Extensions.DependencyModel.Tests
+{
+    public static class JsonAssertionExtensions
+    {
+        public static JsonAssetions Should(this JToken jToken)
+        {
+            return new JsonAssetions(jToken);
+        }
+    }
+
+    public class JsonAssetions: ReferenceTypeAssertions<JToken, JsonAssetions>
+    {
+        public JsonAssetions(JToken token)
+        {
+            Subject = token;
+        }
+
+        protected override string Context => nameof(JToken);
+
+        public AndWhichConstraint<JsonAssetions, JToken> HaveProperty(string expected)
+        {
+            var token = Subject[expected];
+            Execute.Assertion
+                .ForCondition(token != null)
+                .FailWith($"Expected {Subject} to have property '" + expected + "'");
+
+            return new AndWhichConstraint<JsonAssetions, JToken>(this, token);
+        }
+
+        public AndConstraint<JsonAssetions> NotHaveProperty(string expected)
+        {
+            var token = Subject[expected];
+            Execute.Assertion
+                .ForCondition(token == null)
+                .FailWith($"Expected {Subject} not to have property '" + expected + "'");
+
+            return new AndConstraint<JsonAssetions>(this);
+        }
+    }
+}
