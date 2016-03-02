07278237ec9470d8885fca486bed899bb2ce0c81
Andy Ayers <andya@microsoft.com>
2016-03-02 15:21:50 -0800

    Inline refactoring: convert hints into observations
    This change updates the inlining code to use observations in place of
    the InlineHints and hint-like things (eg "HasSimd"). A number of new
    observations were added in support of this.
    
    The `compInlineeHints` member of the compiler object was removed as
    the same information is now tracked by the inline policy. The policy
    also now contains most of the weights and combining logic used to
    compute the profitabiliy boost for the candidate.
    
    There is one subtle and tricky aspect to the change. For the most part
    the hints were ignored during the prejit-root analysis, but the mostly
    load-store hint was propagated and influenced the inlines done when
    crossgenning mscorlib. See #3482 for more on this particular quirk.
    
    I've preserved this behavior by "passing" the prejit inline result
    into `fgFindJumpTargets`. In actuality the result is passed by
    temporarily setting the `compInlineResult` compiler member variable.
    Then, in `fgFindJumpTargets`, the load-store observation is the only
    one not guarded by `compIsForInlining`. In a subsequent change I'll redo
    the guards at other observation sites and put the policy aspects of these
    observations into the policy object.
    
    This addresses another piece of the work outlined in #3371.

--- src/jit/inlinepolicy.cpp
+++ src/jit/inlinepolicy.cpp
@@ -44,18 +44,6 @@ void LegacyPolicy::noteCandidate(InlineObservation obs)
     InlineImpact impact = inlGetImpact(obs);
     assert(impact == InlineImpact::INFORMATION);
 
-    switch (obs)
-    {
-    case InlineObservation::CALLEE_IS_FORCE_INLINE:
-        {
-            inlIsForceInline = true;
-            break;
-        }
-
-    default:
-        break;
-    }
-
     switch (inlDecision)
     {
     case InlineDecision::UNDECIDED:
@@ -69,6 +57,9 @@ void LegacyPolicy::noteCandidate(InlineObservation obs)
         assert(!"Unexpected inlDecision");
         unreached();
     }
+
+    // Now fall through to the general handling.
+    note(obs);
 }
 
 //------------------------------------------------------------------------
@@ -95,7 +86,55 @@ void LegacyPolicy::note(InlineObservation obs)
     // reported via noteFatal.
     assert(impact != InlineImpact::FATAL);
 
-    noteInternal(obs, impact);
+    // Handle most information here
+    bool isInformation = (impact == InlineImpact::INFORMATION);
+    bool propagate = !isInformation;
+
+    if (isInformation)
+    {
+        switch (obs)
+        {
+        case InlineObservation::CALLEE_IS_FORCE_INLINE:
+            inlIsForceInline = true;
+            break;
+        case InlineObservation::CALLEE_IS_INSTANCE_CTOR:
+            inlIsInstanceCtor = true;
+            break;
+        case InlineObservation::CALLEE_CLASS_PROMOTABLE:
+            inlIsFromPromotableValueClass = true;
+            break;
+        case InlineObservation::CALLEE_HAS_SIMD:
+            inlHasSimd = true;
+            break;
+        case InlineObservation::CALLEE_LOOKS_LIKE_WRAPPER:
+            inlLooksLikeWrapperMethod = true;
+            break;
+        case InlineObservation::CALLEE_ARG_FEEDS_CONSTANT_TEST:
+            inlArgFeedsConstantTest = true;
+            break;
+        case InlineObservation::CALLEE_ARG_FEEDS_RANGE_CHECK:
+            inlArgFeedsRangeCheck = true;
+            break;
+        case InlineObservation::CALLEE_IS_MOSTLY_LOAD_STORE:
+            inlMethodIsMostlyLoadStore = true;
+            break;
+        case InlineObservation::CALLEE_HAS_SWITCH:
+            // Pass this one on, it should cause inlining to fail.
+            propagate = true;
+            break;
+        case InlineObservation::CALLSITE_CONSTANT_ARG_FEEDS_TEST:
+            inlConstantFeedsConstantTest = true;
+            break;
+        default:
+            // Ignore the remainder for now
+            break;
+        }
+    }
+
+    if (propagate)
+    {
+        noteInternal(obs);
+    }
 }
 
 //------------------------------------------------------------------------
@@ -106,13 +145,10 @@ void LegacyPolicy::note(InlineObservation obs)
 
 void LegacyPolicy::noteFatal(InlineObservation obs)
 {
-    // Check the impact
-    InlineImpact impact = inlGetImpact(obs);
-
     // As a safeguard, all fatal impact must be
     // reported via noteFatal.
-    assert(impact == InlineImpact::FATAL);
-    noteInternal(obs, impact);
+    assert(inlGetImpact(obs) == InlineImpact::FATAL);
+    noteInternal(obs);
     assert(inlDecisionIsFailure(inlDecision));
 }
 
@@ -192,17 +228,11 @@ void LegacyPolicy::noteDouble(InlineObservation obs, double value)
 //
 // Arguments:
 //    obs      - the current obsevation
-//    impact   - impact of the current observation
 
-void LegacyPolicy::noteInternal(InlineObservation obs, InlineImpact impact)
+void LegacyPolicy::noteInternal(InlineObservation obs)
 {
-    // Ignore INFORMATION for now, since policy
-    // is still embedded at the observation sites.
-    if (impact == InlineImpact::INFORMATION)
-    {
-        return;
-    }
-
+    // Note any INFORMATION that reaches here will now cause failure.
+    // Non-fatal INFORMATION observations must be handled higher up.
     InlineTarget target = inlGetTarget(obs);
 
     if (target == InlineTarget::CALLEE)
@@ -271,4 +301,76 @@ void LegacyPolicy::setNever(InlineObservation obs)
         assert(!"Unexpected inlDecision");
         unreached();
     }
+}
+
+//------------------------------------------------------------------------
+// determineMultiplier: determine benefit multiplier for this inline
+//
+// Notes: uses the accumulated set of observations to compute a
+// profitability boost for the inline candidate.
+
+double LegacyPolicy::determineMultiplier()
+{
+    double multiplier = 0;
+
+    // Bump up the multiplier for instance constructors
+
+    if (inlIsInstanceCtor)
+    {
+        multiplier += 1.5;
+        JITDUMP("\nmultiplier in instance constructors increased to %g.", multiplier);
+    }
+
+    // Bump up the multiplier for methods in promotable struct
+
+    if (inlIsFromPromotableValueClass)
+    {
+        multiplier += 3;
+        JITDUMP("\nmultiplier in methods of promotable struct increased to %g.", multiplier);
+    }
+
+#ifdef FEATURE_SIMD
+
+    if (inlHasSimd)
+    {
+        static ConfigDWORD fJitInlineSIMDMultiplier;
+        int simdMultiplier = fJitInlineSIMDMultiplier.val(CLRConfig::INTERNAL_JitInlineSIMDMultiplier);
+
+        multiplier += simdMultiplier;
+        JITDUMP("\nInline candidate has SIMD type args, locals or return value.  Multiplier increased to %g.", multiplier);
+    }
+
+#endif // FEATURE_SIMD
+
+    if (inlLooksLikeWrapperMethod)
+    {
+        multiplier += 1.0;
+        JITDUMP("\nInline candidate looks like a wrapper method.  Multiplier increased to %g.", multiplier);
+    }
+
+    if (inlArgFeedsConstantTest)
+    {
+        multiplier += 1.0;
+        JITDUMP("\nInline candidate has an arg that feeds a constant test.  Multiplier increased to %g.", multiplier);
+    }
+
+    if (inlMethodIsMostlyLoadStore)
+    {
+        multiplier += 3.0;
+        JITDUMP("\nInline candidate is mostly loads and stores.  Multiplier increased to %g.", multiplier);
+    }
+
+    if (inlArgFeedsRangeCheck)
+    {
+        multiplier += 0.5;
+        JITDUMP("\nInline candidate has arg that feeds range check.  Multiplier increased to %g.", multiplier);
+    }
+
+    if (inlConstantFeedsConstantTest)
+    {
+        multiplier += 3.0;
+        JITDUMP("\nInline candidate has const arg that feeds a conditional.  Multiplier increased to %g.", multiplier);
+    }
+
+    return multiplier;
 }
