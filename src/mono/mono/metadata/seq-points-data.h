388b88173d3da0f471b856c4441cec5d2e4cea6e
Marcos Henrich <marcos.henrich@xamarin.com>
2016-03-02 10:29:05 +0000

    [mono-symbolicate] Created SeqPointInfo.cs
    SeqPointInfo is a c# implementation of the debug data decoding process
    that is done natively by the runtime.
    
    
    Commit migrated from https://github.com/mono/mono/commit/11aa99476bb844d36dcbd7278423b1b96db39fb3

--- src/mono/mono/metadata/seq-points-data.h
+++ src/mono/mono/metadata/seq-points-data.h
@@ -111,7 +111,7 @@ mono_seq_point_data_add (SeqPointData *data, guint32 methodToken, guint32 method
 gboolean
 mono_seq_point_data_get (SeqPointData *data, guint32 methodToken, guint32 methodIndex, MonoSeqPointInfo** info);
 
-gboolean
+MONO_API gboolean
 mono_seq_point_data_get_il_offset (char *path, guint32 methodToken, guint32 methodIndex, guint32 native_offset, guint32 *il_offset);
 
 #endif /* __MONO_SEQ_POINTS_DATA_H__ */
