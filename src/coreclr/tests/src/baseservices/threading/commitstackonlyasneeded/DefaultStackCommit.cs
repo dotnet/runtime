d60bdd8a4d7d84e36cd4dc2e7b631887a3123108
Aditya Mandaleeka <adityam@microsoft.com>
2016-03-03 12:24:52 -0800

    Open source tests from baseservices/threading.
    Commit migrated from https://github.com/dotnet/coreclr/commit/316e43433443447d345c7a48b9deb5cdd7946144

---
+++ src/coreclr/tests/src/baseservices/threading/commitstackonlyasneeded/DefaultStackCommit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+// using System.Configuration;
+using System.Runtime.InteropServices;
+
+namespace StackCommitTest
+{
+    class DefaultStackCommit
+    {
+        static int Main(string[] args)
+        {
+            int result = 1;
+            bool commitEnabled = false;
+
+            if (Utility.RunTest(commitEnabled))
+            {
+                result = 100;
+            }
+
+            Console.WriteLine(result == 100 ? "Success!" : "FAILED!");
+            return result;
+        }
+    }
+}
