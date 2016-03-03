769bea0081325f4cc5ce4fa5f269bb310c88a0d2
schellap <schellap@microsoft.com>
2016-03-03 01:46:06 -0800

    Fix host case comparison

--- common/utils.h
+++ common/utils.h
@@ -6,8 +6,8 @@
 
 #include "pal.h"
 
-bool ends_with(const pal::string_t& value, const pal::string_t& suffix);
-bool starts_with(const pal::string_t& value, const pal::string_t& prefix);
+bool ends_with(const pal::string_t& value, const pal::string_t& suffix, bool match_case);
+bool starts_with(const pal::string_t& value, const pal::string_t& prefix, bool match_case);
 pal::string_t get_executable(const pal::string_t& filename);
 pal::string_t get_directory(const pal::string_t& path);
 pal::string_t get_filename(const pal::string_t& path);
