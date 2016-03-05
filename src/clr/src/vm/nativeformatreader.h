199d4427a65d10584b7adf9cec108030dfec099d
Fadi Hanna <fadim@microsoft.com>
2016-03-04 17:26:07 -0800

    Title: Hashtable of types for R2R modules Description: This change replaces the hashtable of types that we dynamically build at module load time with a statically built hashtable at crossgen time, for R2R modules. Typedef tokens and exported type tokens are stored in that hashtable, hashed by their full type name.
    Changes include:
    - Adding support for NativeFormat hashtables (NativeFormat hashtable reader and writer + supporting functionalities)
    - Changes to prevent the allocation and building of m_pAvailableClasses and m_pAvailableClassesCaseIns for a R2R module
    - Logic in crossgen to build the hashtable of types, using the NativeFormat (table saved to a new section)
    - Refactoring the type lookup functions to return results in a HashedTypeEntry data structure, which supports token based results, as well as EEClassHashEntry_t* based results
    - Changes/Cleanup to the ClassLoader::GetClassValue to support the new lookup type
    - Fixed small bugs in the std implementation
    - Fallback to old hashtable lookup algorithm supported for:
    	1) Case insensitive type loading
    	2) R2R image built with older version of crossgen (not having static hashtable)
    
    Tested following scenarios:
    	- Token based lookups
    	- Type name based lookups (Type.GetType API), including case insensitive scenarios
    	- Nested types
    	- Forwarded types (and forwarded nested types), up to 2 levels of forwarding
    	- Old R2R image created with previous version of crossgen
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/30631742c9e4468264c39060cc9533bd9c3a6e3c

--- src/clr/src/vm/nativeformatreader.h
+++ src/clr/src/vm/nativeformatreader.h
@@ -20,6 +20,9 @@
 
 namespace NativeFormat
 {
+    class NativeReader;
+    typedef DPTR(NativeReader) PTR_NativeReader;
+
     class NativeReader
     {
         PTR_BYTE _base;
@@ -42,6 +45,13 @@ namespace NativeFormat
         {
             _ASSERTE(false);
             ThrowHR(COR_E_BADIMAGEFORMAT);
+        }
+
+        uint EnsureOffsetInRange(uint offset, uint lookAhead)
+        {
+            if ((int)offset < 0 || offset + lookAhead >= _size)
+                ThrowBadImageFormatException();
+            return offset;
         }
 
         byte ReadUInt8(uint offset)
@@ -119,11 +129,167 @@ namespace NativeFormat
 
             return offset;
         }
+
+        int DecodeSigned(uint offset, int * pValue)
+        {
+            if (offset >= _size)
+                ThrowBadImageFormatException();
+
+            int val = *(_base + offset);
+            if ((val & 1) == 0)
+            {
+                *pValue = val >> 1;
+                offset += 1;
+            }
+            else if ((val & 2) == 0)
+            {
+                if (offset + 1 >= _size)
+                    ThrowBadImageFormatException();
+                *pValue = (val >> 2) |
+                    (((int)*(_base + offset + 1)) << 6);
+                offset += 2;
+            }
+            else if ((val & 4) == 0)
+            {
+                if (offset + 2 >= _size)
+                    ThrowBadImageFormatException();
+                *pValue = (val >> 3) |
+                    (((int)*(_base + offset + 1)) << 5) |
+                    (((int)*(_base + offset + 2)) << 13);
+                offset += 3;
+            }
+            else if ((val & 8) == 0)
+            {
+                if (offset + 3 >= _size)
+                    ThrowBadImageFormatException();
+                *pValue = (val >> 4) |
+                    (((int)*(_base + offset + 1)) << 4) |
+                    (((int)*(_base + offset + 2)) << 12) |
+                    (((int)*(_base + offset + 3)) << 20);
+                offset += 4;
+            }
+            else if ((val & 16) == 0)
+            {
+                *pValue = (int)ReadUInt32(offset + 1);
+                offset += 5;
+            }
+            else
+            {
+                ThrowBadImageFormatException();
+            }
+
+            return offset;
+        }
+
+        uint SkipInteger(uint offset)
+        {
+            EnsureOffsetInRange(offset, 0);
+
+            PTR_BYTE data = (_base + offset);
+            if ((*data & 1) == 0)
+            {
+                return offset + 1;
+            }
+            else if ((*data & 2) == 0)
+            {
+                return offset + 2;
+            }
+            else if ((*data & 4) == 0)
+            {
+                return offset + 3;
+            }
+            else if ((*data & 8) == 0)
+            {
+                return offset + 4;
+            }
+            else if ((*data & 16) == 0)
+            {
+                return offset + 5;
+            }
+            else if ((*data & 32) == 0)
+            {
+                return offset + 9;
+            }
+            else
+            {
+                ThrowBadImageFormatException();
+                return offset;
+            }
+        }
     };
 
+    class NativeParser
+    {
+        PTR_NativeReader _pReader;
+        uint _offset;
+
+    public:
+        NativeParser()
+            : _pReader(PTR_NULL), _offset(0)
+        {
+        }
+        
+        NativeParser(NativeReader * pReader, uint offset)
+        {
+            _pReader = dac_cast<PTR_NativeReader>(pReader);
+            _offset = offset;
+        }
+
+        NativeReader * GetNativeReader() { return _pReader; }
+
+        uint GetOffset() { return _offset; }
+        void SetOffset(uint value) { _offset = value; }
+
+        void ThrowBadImageFormatException()
+        {
+            _pReader->ThrowBadImageFormatException();
+        }
+
+        byte GetUInt8()
+        {
+            byte val = _pReader->ReadUInt8(_offset);
+            _offset += 1;
+            return val;
+        }
+
+        uint GetUnsigned()
+        {
+            uint value;
+            _offset = _pReader->DecodeUnsigned(_offset, &value);
+            return value;
+        }
+
+        int GetSigned()
+        {
+            int value;
+            _offset = _pReader->DecodeSigned(_offset, &value);
+            return value;
+        }
+
+        uint GetRelativeOffset()
+        {
+            uint pos = _offset;
+
+            int delta;
+            _offset = _pReader->DecodeSigned(_offset, &delta);
+
+            return pos + (uint)delta;
+        }
+
+        void SkipInteger()
+        {
+            _offset = _pReader->SkipInteger(_offset);
+        }
+
+        NativeParser GetParserFromRelativeOffset()
+        {
+            return NativeParser(_pReader, GetRelativeOffset());
+        }
+    };
+    
     class NativeArray
     {
-        NativeReader * _pReader;
+        PTR_NativeReader _pReader;
         uint _baseOffset;
         uint _nElements;
         byte _entryIndexSize;
@@ -132,7 +298,7 @@ namespace NativeFormat
 
     public:
         NativeArray()
-            : _pReader(NULL)
+            : _pReader(PTR_NULL), _nElements(0)
         {
         }
 
@@ -207,6 +373,128 @@ namespace NativeFormat
 
             *pOffset = offset;
             return true;
+        }
+    };
+
+    class NativeHashtable
+    {
+        PTR_NativeReader _pReader;
+        uint _baseOffset;
+        uint _bucketMask;
+        byte _entryIndexSize;
+
+        NativeParser GetParserForBucket(uint bucket, uint * pEndOffset)
+        {
+            uint start, end;
+
+            if (_entryIndexSize == 0)
+            {
+                uint bucketOffset = _baseOffset + bucket;
+                start = _pReader->ReadUInt8(bucketOffset);
+                end = _pReader->ReadUInt8(bucketOffset + 1);
+            }
+            else if (_entryIndexSize == 1)
+            {
+                uint bucketOffset = _baseOffset + 2 * bucket;
+                start = _pReader->ReadUInt16(bucketOffset);
+                end = _pReader->ReadUInt16(bucketOffset + 2);
+            }
+            else
+            {
+                uint bucketOffset = _baseOffset + 4 * bucket;
+                start = _pReader->ReadUInt32(bucketOffset);
+                end = _pReader->ReadUInt32(bucketOffset + 4);
+            }
+
+            *pEndOffset = end + _baseOffset;
+            return NativeParser(_pReader, _baseOffset + start);
+        }
+
+    public:
+        NativeHashtable() : _pReader(PTR_NULL), _baseOffset(0), _bucketMask(0), _entryIndexSize(0)
+        {
+        }
+        
+        NativeHashtable(NativeParser& parser)
+        {
+            uint header = parser.GetUInt8();
+
+            _pReader = dac_cast<PTR_NativeReader>(parser.GetNativeReader());
+            _baseOffset = parser.GetOffset();
+
+            int numberOfBucketsShift = (int)(header >> 2);
+            if (numberOfBucketsShift > 31)
+                _pReader->ThrowBadImageFormatException();
+            _bucketMask = (uint)((1 << numberOfBucketsShift) - 1);
+
+            byte entryIndexSize = (byte)(header & 3);
+            if (entryIndexSize > 2)
+                _pReader->ThrowBadImageFormatException();
+            _entryIndexSize = entryIndexSize;
+        }
+
+        bool IsNull() { return _pReader == NULL; }
+
+        //
+        // The enumerator does not conform to the regular C# enumerator pattern to avoid paying 
+        // its performance penalty (allocation, multiple calls per iteration)
+        //
+        class Enumerator
+        {
+            NativeParser _parser;
+            uint _endOffset;
+            byte _lowHashcode;
+
+        public:
+            Enumerator(NativeParser parser, uint endOffset, byte lowHashcode)
+            {
+                _parser = parser;
+                _endOffset = endOffset;
+                _lowHashcode = lowHashcode;
+            }
+
+            bool GetNext(NativeParser& entryParser)
+            {
+                while (_parser.GetOffset() < _endOffset)
+                {
+                    byte lowHashcode = _parser.GetUInt8();
+
+                    if (lowHashcode == _lowHashcode)
+                    {
+                        entryParser = _parser.GetParserFromRelativeOffset();
+                        return true;
+                    }
+
+                    // The entries are sorted by hashcode within the bucket. It allows us to terminate the lookup prematurely.
+                    if (lowHashcode > _lowHashcode)
+                    {
+                        _endOffset = _parser.GetOffset(); // Ensure that extra call to GetNext returns null parser again
+                        break;
+                    }
+
+                    _parser.SkipInteger();
+                }
+
+                return false;
+            }
+        };
+
+        // The recommended code pattern to perform lookup is: 
+        //
+        //  NativeHashtable::Enumerator lookup = hashtable.Lookup(dwHashCode);
+        //  NativeParser entryParser;
+        //  while (lookup.GetNext(entryParser))
+        //  {
+        //      ... read entry using entryParser ...
+        //  }
+        //
+        Enumerator Lookup(int hashcode)
+        {
+            uint endOffset;
+            uint bucket = ((uint)hashcode >> 8) & _bucketMask;
+            NativeParser parser = GetParserForBucket(bucket, &endOffset);
+
+            return Enumerator(parser, endOffset, (byte)hashcode);
         }
     };
 }
