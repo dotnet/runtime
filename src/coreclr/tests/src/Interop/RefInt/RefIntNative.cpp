4a681c1989287fd426244bd4abdf3dd85d27bfad
tijoytom <tijoytk@microsoft.com>
2016-03-07 15:34:38 -0800

    Improve interop test coverage Updating license headers Delete duplicate test
    Commit migrated from https://github.com/dotnet/coreclr/commit/aad12ef45b670933a504edad0a5610eeb1c97da3

--- src/coreclr/tests/src/Interop/RefInt/RefIntNative.cpp
+++ src/coreclr/tests/src/Interop/RefInt/RefIntNative.cpp
@@ -1,3 +1,7 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 #include <xplatform.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,7 +10,7 @@ const int iManaged = 10;
 const int iNative = 11;
 
 
-extern "C"  BOOL _cdecl MarshalRefInt_Cdcel(int * pint)
+extern "C"  BOOL DLL_EXPORT _cdecl MarshalRefInt_Cdcel(int * pint)
 {
 	//Check the Input	
 	if(iManaged != *pint)
@@ -18,7 +22,7 @@ extern "C"  BOOL _cdecl MarshalRefInt_Cdcel(int * pint)
 	return TRUE;
 }
 
-extern "C"  BOOL __stdcall MarshalRefInt_Stdcall(int * pint)
+extern "C"  BOOL DLL_EXPORT __stdcall MarshalRefInt_Stdcall(int * pint)
 {
 	//Check the Input
 	if(iManaged != *pint)
@@ -31,7 +35,7 @@ extern "C"  BOOL __stdcall MarshalRefInt_Stdcall(int * pint)
 }
 
 typedef BOOL (_cdecl *Cdeclcaller)(int* pint);
-extern "C"  BOOL __stdcall DoCallBack_MarshalRefInt_Cdecl(Cdeclcaller caller)
+extern "C"  BOOL DLL_EXPORT __stdcall DoCallBack_MarshalRefInt_Cdecl(Cdeclcaller caller)
 {
 	//Check the Input
 	int itemp = iNative;
@@ -49,7 +53,7 @@ extern "C"  BOOL __stdcall DoCallBack_MarshalRefInt_Cdecl(Cdeclcaller caller)
 }
 
 typedef BOOL (__stdcall *Stdcallcaller)(int* pint);
-extern "C"  BOOL __stdcall DoCallBack_MarshalRefInt_Stdcall(Stdcallcaller caller)
+extern "C"  BOOL DLL_EXPORT __stdcall DoCallBack_MarshalRefInt_Stdcall(Stdcallcaller caller)
 {
 	//Check the Input
 	int itemp = iNative;
@@ -67,14 +71,14 @@ extern "C"  BOOL __stdcall DoCallBack_MarshalRefInt_Stdcall(Stdcallcaller caller
 }
 
 typedef BOOL (_cdecl * DelegatePInvokeCdecl)(int * pint);
-extern "C" DelegatePInvokeCdecl MarshalRefInt_DelegatePInvoke_Cdecl()
+extern "C" DLL_EXPORT DelegatePInvokeCdecl MarshalRefInt_DelegatePInvoke_Cdecl()
 {
 	return MarshalRefInt_Cdcel;
 }
 
 
 typedef BOOL (__stdcall * DelegatePInvokeStdcall)(int *pint);
-extern "C" DelegatePInvokeStdcall __stdcall MarshalRefInt_DelegatePInvoke_StdCall()
+extern "C" DLL_EXPORT DelegatePInvokeStdcall __stdcall MarshalRefInt_DelegatePInvoke_StdCall()
 {
 	return MarshalRefInt_Stdcall;
 }
