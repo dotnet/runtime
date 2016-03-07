291bd303a14aa57b8a9778654750804249eae4bb
tijoytom <tijoytk@microsoft.com>
2016-03-07 15:34:38 -0800

    Improve interop test coverage Updating license headers Delete duplicate test
    Commit migrated from https://github.com/dotnet/coreclr/commit/aad12ef45b670933a504edad0a5610eeb1c97da3

--- src/coreclr/tests/src/Interop/common/types.h
+++ src/coreclr/tests/src/Interop/common/types.h
@@ -1,4 +1,6 @@
-
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 
 #ifndef _INTEROP_TYPES__H
 #define _INTEROP_TYPES__H
