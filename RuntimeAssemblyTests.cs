78c85525b4831e1cf6a86cebf2c2d74648183c6c
Pavel Krymets <pavel@krymets.com>
2016-03-08 16:46:50 -0800

    Generate actual deps.json file

---
+++ RuntimeAssemblyTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Microsoft.Extensions.DependencyModel.Tests
+{
+    public class RuntimeAssemblyTests
+    {
+        [Fact]
+        public void UsesFileNameAsAssemblyNameInCreate()
+        {
+            var assembly = RuntimeAssembly.Create("path/to/System.Collections.dll");
+            assembly.Name.Name.Should().Be("System.Collections");
+        }
+
+        [Fact]
+        public void TrimsDotNiFromDllNames()
+        {
+            var assembly = RuntimeAssembly.Create("path/to/System.Collections.ni.dll");
+            assembly.Name.Name.Should().Be("System.Collections");
+        }
+    }
+}
