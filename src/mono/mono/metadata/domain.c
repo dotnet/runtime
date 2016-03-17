9907b6201f3e13ac01efbeaf05a3b949269cd08a
Zoltan Varga <vargaz@gmail.com>
2016-03-17 12:06:02 +0100

    [runtime] Cleanup the debug info module before the images module since ppdb debug info depends on images. Fixes mono/mono#39715.
    Commit migrated from https://github.com/mono/mono/commit/3736375ac24304ac7aa49c05df84572f5224b8d1

--- src/mono/mono/metadata/domain.c
+++ src/mono/mono/metadata/domain.c
@@ -886,8 +886,8 @@ mono_cleanup (void)
 	mono_loader_cleanup ();
 	mono_classes_cleanup ();
 	mono_assemblies_cleanup ();
-	mono_images_cleanup ();
 	mono_debug_cleanup ();
+	mono_images_cleanup ();
 	mono_metadata_cleanup ();
 
 	mono_native_tls_free (appdomain_thread_id);
