be94e039394b3d8741780f2591ce234b0f45e00a
Senthil <schellap@microsoft.com>
2016-03-23 18:00:52 -0700

    Logging and case format fix
    Commit migrated from https://github.com/dotnet/core-setup/commit/9d0a34048d8e55c8434424b7c5e1e88c6d21b66b

--- src/installer/cli/hostpolicy.cpp
+++ src/installer/cli/hostpolicy.cpp
@@ -205,11 +205,29 @@ SHARED_API int corehost_main(const int argc, const pal::char_t* argv[])
 
     assert(g_init);
 
+    if (trace::is_enabled())
+    {
+        trace::info(_X("--- Invoked policy main = {"));
+        for (int i = 0; i < argc; ++i)
+        {
+            trace::info(_X("%s"), argv[i]);
+        }
+        trace::info(_X("}"));
+
+        trace::info(_X("Host mode: %d"), g_init->host_mode());
+        trace::info(_X("Deps file: %s"), g_init->deps_file().c_str());
+        trace::info(_X("Probe dir: %s"), g_init->probe_dir().c_str());
+    }
+
     // Take care of arguments
     arguments_t args;
     if (!parse_arguments(g_init->deps_file(), g_init->probe_dir(), g_init->host_mode(), argc, argv, &args))
     {
         return StatusCode::LibHostInvalidArgs;
+    }
+    if (trace::is_enabled())
+    {
+        args.print();
     }
 
     if (g_init->runtime_config())
