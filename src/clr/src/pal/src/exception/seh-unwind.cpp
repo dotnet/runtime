0ca133d13990d06a8b63b5cf6d978fec2579a686
MyungJoo Ham <myungjoo.ham@samsung.com>
2016-03-25 10:12:37 +0900

    Fix ARM/Unwind: explicitly mark the end of stack
    Libunwind-arm unw_step returns 0 with unchanged PC
    if it hits the bottom of the stack.
    
    Thus, seh-unwind.cpp::PAL_VirtualUnwind should
    explicitly mark that it has reached the bottom
    as it does with ARM64, FreeBSD, or APPLE for
    ARM/Linux.
    
    Fix dotnet/coreclr#3859
    
    Signed-off-by: MyungJoo Ham <myungjoo.ham@samsung.com>
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/95cad43adb26410a31adaa8f30908f07876bb742

--- src/clr/src/pal/src/exception/seh-unwind.cpp
+++ src/clr/src/pal/src/exception/seh-unwind.cpp
@@ -226,7 +226,7 @@ BOOL PAL_VirtualUnwind(CONTEXT *context, KNONVOLATILE_CONTEXT_POINTERS *contextP
     unw_context_t unwContext;
     unw_cursor_t cursor;
 
-#if defined(__APPLE__) || defined(__FreeBSD__) || defined(_ARM64_)
+#if defined(__APPLE__) || defined(__FreeBSD__) || defined(_ARM64_) || defined(_ARM_)
     DWORD64 curPc;
 #endif
 
@@ -262,7 +262,7 @@ BOOL PAL_VirtualUnwind(CONTEXT *context, KNONVOLATILE_CONTEXT_POINTERS *contextP
     WinContextToUnwindCursor(context, &cursor);
 #endif
 
-#if defined(__APPLE__) || defined(__FreeBSD__) || defined(_ARM64_)
+#if defined(__APPLE__) || defined(__FreeBSD__) || defined(_ARM64_) || defined(_ARM_)
     // OSX and FreeBSD appear to do two different things when unwinding
     // 1: If it reaches where it cannot unwind anymore, say a 
     // managed frame.  It wil return 0, but also update the $pc
@@ -295,7 +295,7 @@ BOOL PAL_VirtualUnwind(CONTEXT *context, KNONVOLATILE_CONTEXT_POINTERS *contextP
     // Update the passed in windows context to reflect the unwind
     //
     UnwindContextToWinContext(&cursor, context);
-#if defined(__APPLE__) || defined(__FreeBSD__) || defined(_ARM64_)
+#if defined(__APPLE__) || defined(__FreeBSD__) || defined(_ARM64_) || defined(_ARM_)
     if (st == 0 && CONTEXTGetPC(context) == curPc)
     {
         CONTEXTSetPC(context, 0);
