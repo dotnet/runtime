c4a0d8b96606e3eae5d17e5bc6df65535e80813d
schellap <schellap@microsoft.com>
2016-03-29 01:58:51 -0700

    Revert "Merge pull request dotnet/core-setup#2080 from schellap/nupkg2"
    This reverts commit dotnet/core-setup@a6bf3b416b2a926e33db946a4c377a37e08f6f54, reversing
    changes made to dotnet/core-setup@b0e457559d7626775461ab96092816aa5982d028.
    
    
    Commit migrated from https://github.com/dotnet/core-setup/commit/2a4c3ad9a1a29994f90945edcbb257b3f3d38077

--- src/setup/cli/hostpolicy.cpp
+++ src/setup/cli/hostpolicy.cpp
@@ -207,11 +207,7 @@ SHARED_API int corehost_main(const int argc, const pal::char_t* argv[])
 
     if (trace::is_enabled())
     {
-        trace::info(_X("--- Invoked policy [%s/%s/%s] main = {"),
-            _STRINGIFY(HOST_POLICY_PKG_NAME),
-            _STRINGIFY(HOST_POLICY_PKG_VER),
-            _STRINGIFY(HOST_POLICY_PKG_REL_DIR));
-
+        trace::info(_X("--- Invoked policy main = {"));
         for (int i = 0; i < argc; ++i)
         {
             trace::info(_X("%s"), argv[i]);
