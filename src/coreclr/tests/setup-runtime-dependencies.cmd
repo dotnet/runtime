0fc07379152fb390a158386812f6272dd191ef61
Sejong OH <sejooh@microsoft.com>
2016-03-30 18:23:15 -0700

    Fix tests/setup-runtime-dependencies.sh as per dotnet cli changes
    Commit migrated from https://github.com/dotnet/coreclr/commit/7bbd985be6eb703a2e6f3822acae68c7305dd325

--- src/coreclr/tests/setup-runtime-dependencies.cmd
+++ src/coreclr/tests/setup-runtime-dependencies.cmd
@@ -12,6 +12,7 @@ REM ===
 REM =========================================================================================
 
 set __OutputDir=
+set __Arch= 
 
 :Arg_Loop
 if "%1" == "" goto ArgsDone
@@ -23,6 +24,7 @@ if /i "%1" == "-h"    goto Usage
 if /i "%1" == "/help" goto Usage
 if /i "%1" == "-help" goto Usage
 
+if /i "%1" == "/arch"             (set __Arch=%2&shift&shift&goto Arg_Loop) 
 if /i "%1" == "/outputdir"        (set __OutputDir=%2&shift&shift&goto Arg_Loop)
 
 echo Invalid command-line argument: %1
@@ -31,7 +33,7 @@ goto Usage
 :ArgsDone
 
 if not defined __OutputDir goto Usage
-
+if not defined __Arch goto Usage 
 
 REM =========================================================================================
 REM ===
@@ -40,7 +42,7 @@ REM ===
 REM =========================================================================================
 
 set __DotNetToolDir=%__ThisScriptPath%..\Tools
-set __DotNetCmd=%__DotNetToolDir%\dotnetcli\bin\dotnet.exe
+set __DotNetCmd=%__DotNetToolDir%\dotnetcli\dotnet.exe 
 set __PackageDir=%__ThisScriptPath%..\Packages
 set __TmpDir=%Temp%\coreclr_gcstress_%RANDOM%
 
@@ -87,6 +89,12 @@ set DOTNETCMD="%__DotNetCmd%" restore "%__JasonFilePath%" --source https://dotne
 echo %DOTNETCMD%
 call %DOTNETCMD%
 if errorlevel 1 goto Fail
+REM Get downloaded dll path
+FOR /F "delims=" %%i IN ('dir %__PackageDir%\coredistools.dll /b/s ^| findstr /R "runtime.win[0-9]*-%__Arch%"') DO set __LibPath=%%i
+if not exist "%__LibPath%" (
+    echo Failed to locate the downloaded library: %__LibPath%
+    goto Fail
+)
 
 REM Get downloaded dll path
 FOR /F "delims=" %%i IN ('dir %__PackageDir%\coredistools.dll /b/s') DO set __LibPath=%%i
@@ -123,6 +131,6 @@ echo.
 echo Download coredistool for GC stress testing
 echo.
 echo Usage:
-echo     %__ThisScriptShort% /outputdir ^<coredistools_lib_install_path^>
+echo     %__ThisScriptShort% /arch ^<TargetArch^> /outputdir ^<coredistools_lib_install_path^>
 echo.
 exit /b 1
