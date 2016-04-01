86662d7dea8fc6ca4e5195a23f51f0c231b98cdf
Zoltan Varga <vargaz@gmail.com>
2016-04-01 16:12:23 -0400

    [runtime] Make a copy of the types passed to mono_mb_add_local () earlier since they could be transient.
    Commit migrated from https://github.com/mono/mono/commit/db5af7fe7ea69fd816ef8abfbaa3faea828a61db

--- src/mono/mono/metadata/method-builder.c
+++ src/mono/mono/metadata/method-builder.c
@@ -98,6 +98,12 @@ void
 mono_mb_free (MonoMethodBuilder *mb)
 {
 #ifndef DISABLE_JIT
+	GList *l;
+
+	for (l = mb->locals_list; l; l = l->next) {
+		/* Allocated in mono_mb_add_local () */
+		g_free (l->data);
+	}
 	g_list_free (mb->locals_list);
 	if (!mb->dynamic) {
 		g_free (mb->method);
@@ -150,7 +156,7 @@ mono_mb_create_method (MonoMethodBuilder *mb, MonoMethodSignature *signature, in
 		header->code = mb->code;
 
 		for (i = 0, l = mb->locals_list; l; l = l->next, i++) {
-			header->locals [i] = mono_metadata_type_dup (NULL, (MonoType*)l->data);
+			header->locals [i] = (MonoType*)l->data;
 		}
 	} else
 #endif
@@ -174,10 +180,14 @@ mono_mb_create_method (MonoMethodBuilder *mb, MonoMethodSignature *signature, in
 		memcpy ((char*)header->code, mb->code, mb->pos);
 
 		for (i = 0, l = mb->locals_list; l; l = l->next, i++) {
-			header->locals [i] = mono_metadata_type_dup (NULL, (MonoType*)l->data);
+			header->locals [i] = (MonoType*)l->data;
 		}
 #endif
 	}
+
+	/* Free the locals list so mono_mb_free () doesn't free the types twice */
+	g_list_free (mb->locals_list);
+	mb->locals_list = NULL;
 
 	method->signature = signature;
 	if (!signature->hasthis)
@@ -270,12 +280,19 @@ int
 mono_mb_add_local (MonoMethodBuilder *mb, MonoType *type)
 {
 	int res;
+	MonoType *t;
+
+	/*
+	 * Have to make a copy early since type might be sig->ret,
+	 * which is transient, see mono_metadata_signature_dup_internal_with_padding ().
+	 */
+	t = mono_metadata_type_dup (NULL, type);
 
 	g_assert (mb != NULL);
 	g_assert (type != NULL);
 
 	res = mb->locals;
-	mb->locals_list = g_list_append (mb->locals_list, type);
+	mb->locals_list = g_list_append (mb->locals_list, t);
 	mb->locals++;
 
 	return res;
