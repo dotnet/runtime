2b750fc6ef9ec235cfee70d21959fec9de08881c
Pavel Krymets <pavel@krymets.com>
2016-04-01 21:52:08 -0700

    Do not trim refs

--- DependencyContextExtensions.cs
+++ DependencyContextExtensions.cs
@@ -12,41 +12,101 @@ namespace Microsoft.Extensions.DependencyModel
 
         public static IEnumerable<string> GetDefaultNativeAssets(this DependencyContext self)
         {
+            if (self == null)
+            {
+                throw new ArgumentNullException(nameof(self));
+            }
             return self.RuntimeLibraries.SelectMany(library => library.GetDefaultNativeAssets(self));
         }
 
         public static IEnumerable<string> GetRuntimeNativeAssets(this DependencyContext self, string runtimeIdentifier)
         {
+            if (self == null)
+            {
+                throw new ArgumentNullException(nameof(self));
+            }
+            if (runtimeIdentifier == null)
+            {
+                throw new ArgumentNullException(nameof(runtimeIdentifier));
+            }
             return self.RuntimeLibraries.SelectMany(library => library.GetRuntimeNativeAssets(self, runtimeIdentifier));
         }
 
         public static IEnumerable<string> GetDefaultNativeAssets(this RuntimeLibrary self, DependencyContext context)
         {
+            if (self == null)
+            {
+                throw new ArgumentNullException(nameof(self));
+            }
             return ResolveAssets(context, string.Empty, self.NativeLibraryGroups);
         }
 
         public static IEnumerable<string> GetRuntimeNativeAssets(this RuntimeLibrary self, DependencyContext context, string runtimeIdentifier)
         {
+            if (self == null)
+            {
+                throw new ArgumentNullException(nameof(self));
+            }
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            if (runtimeIdentifier == null)
+            {
+                throw new ArgumentNullException(nameof(runtimeIdentifier));
+            }
             return ResolveAssets(context, runtimeIdentifier, self.NativeLibraryGroups);
         }
 
         public static IEnumerable<AssemblyName> GetDefaultAssemblyNames(this DependencyContext self)
         {
+            if (self == null)
+            {
+                throw new ArgumentNullException(nameof(self));
+            }
             return self.RuntimeLibraries.SelectMany(library => library.GetDefaultAssemblyNames(self));
         }
 
         public static IEnumerable<AssemblyName> GetRuntimeAssemblyNames(this DependencyContext self, string runtimeIdentifier)
         {
+            if (self == null)
+            {
+                throw new ArgumentNullException(nameof(self));
+            }
+            if (runtimeIdentifier == null)
+            {
+                throw new ArgumentNullException(nameof(runtimeIdentifier));
+            }
             return self.RuntimeLibraries.SelectMany(library => library.GetRuntimeAssemblyNames(self, runtimeIdentifier));
         }
 
         public static IEnumerable<AssemblyName> GetDefaultAssemblyNames(this RuntimeLibrary self, DependencyContext context)
         {
+            if (self == null)
+            {
+                throw new ArgumentNullException(nameof(self));
+            }
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
             return ResolveAssets(context, string.Empty, self.RuntimeAssemblyGroups).Select(GetAssemblyName);
         }
 
         public static IEnumerable<AssemblyName> GetRuntimeAssemblyNames(this RuntimeLibrary self, DependencyContext context, string runtimeIdentifier)
         {
+            if (self == null)
+            {
+                throw new ArgumentNullException(nameof(self));
+            }
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            if (runtimeIdentifier == null)
+            {
+                throw new ArgumentNullException(nameof(runtimeIdentifier));
+            }
             return ResolveAssets(context, runtimeIdentifier, self.RuntimeAssemblyGroups).Select(GetAssemblyName);
         }
 
