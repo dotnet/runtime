9f614dcae19747f37edee1c78241a7542588b18f
Aleksey Kliger <aleksey@xamarin.com>
2016-04-05 15:24:23 -0400

    [runtime] Fix bug in get_caller_no_reflection
    In order to find the user code that called some corlib API
    that calls an icall, we write:
    	dest = m = mono_method_get_last_managed ();
    	mono_stack_walk_no_il (get_caller_no_reflection, &dest);
    	if (!dest) dest = m;
    
    The get_caller_no_reflection walk works in two stages: in the first
    stage we look for the target method (the initial value of dest) when we
    find the target we set dest to NULL and then the second stage continues
    walking upward past the reflection APIs.
    
    However if the target method is itself in System.Reflection, the
    old implementation would skip over it even in the first stage and we'd
    never find the target method and exhaust the whole stack.
    
    The fix is to move the check for the target method before the check for
    the Reflection API methods.
    
    
    Commit migrated from https://github.com/mono/mono/commit/9be882ab60747e5f26acd4734df103affb6beb59

--- src/mono/mono/metadata/icall.c
+++ src/mono/mono/metadata/icall.c
@@ -1308,13 +1308,14 @@ get_caller_no_reflection (MonoMethod *m, gint32 no, gint32 ilo, gboolean managed
 	if (m->wrapper_type != MONO_WRAPPER_NONE)
 		return FALSE;
 
-	if (m->klass->image == mono_defaults.corlib && !strcmp (m->klass->name_space, "System.Reflection"))
-		return FALSE;
-
 	if (m == *dest) {
 		*dest = NULL;
 		return FALSE;
 	}
+
+	if (m->klass->image == mono_defaults.corlib && !strcmp (m->klass->name_space, "System.Reflection"))
+		return FALSE;
+
 	if (!(*dest)) {
 		*dest = m;
 		return TRUE;
