a05cc6bbc8c7f93b393168304533f2a537285668
Alex Rønne Petersen <alexrp@xamarin.com>
2016-04-08 03:20:11 +0200

    [threads] Don't consider MONO_THREAD_FLAG_NAME_SET when setting thread name at thread startup.
    If we did consider this, an internal runtime thread that had its name set by a
    mono_thread_set_name_internal (..., FALSE /*managed*/, ...) call prior to
    starting would not have its native name set nor its name reported to the
    profiler.
    
    
    Commit migrated from https://github.com/mono/mono/commit/92664ed7aba572feed8b6f4bba4b9f40db275cad

--- src/mono/mono/metadata/threads.c
+++ src/mono/mono/metadata/threads.c
@@ -727,12 +727,13 @@ static guint32 WINAPI start_wrapper_internal(void *data)
 	mono_profiler_thread_start (tid);
 
 	/* if the name was set before starting, we didn't invoke the profiler callback */
-	if (internal->name && (internal->flags & MONO_THREAD_FLAG_NAME_SET)) {
+	if (internal->name) {
 		char *tname = g_utf16_to_utf8 (internal->name, internal->name_len, NULL, NULL, NULL);
 		mono_profiler_thread_name (internal->tid, tname);
 		mono_native_thread_set_name (MONO_UINT_TO_NATIVE_THREAD_ID (internal->tid), tname);
 		g_free (tname);
 	}
+
 	/* start_func is set only for unmanaged start functions */
 	if (start_func) {
 		start_func (start_arg);
