6b4320b16364eb59c1164c9a5b90d3e3046da72f
Jan Kotas <jkotas@microsoft.com>
2016-04-08 18:34:40 -1000

    Revert "ARM64 Work Item 3817, 3524 - Struct16 decomposition"

--- src/jit/compiler.h
+++ src/jit/compiler.h
@@ -8823,9 +8823,6 @@ public:
     void fgMorphSystemVStructArgs(GenTreeCall* call, bool hasStructArgument);
 #endif // defined(FEATURE_UNIX_AMD64_STRUCT_PASSING)
 
-    void          fgMorphMultiregStructArgs(GenTreeCall* call);
-    GenTreePtr    fgMorphMultiregStructArg (GenTreePtr   arg);
-
 }; // end of class Compiler
 
 // Inline methods of CompAllocator.
