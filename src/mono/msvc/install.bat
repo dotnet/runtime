8725b7003a201b6bd12d91dee89e468f19f349ee
lateralusX <lateralusx.github@gmail.com>
2016-04-12 14:14:29 +0200

    Separate build tree and target suffix support.
    * Enabled build target suffix in order to lable a build (primarly for sgen).
    * Enabled separate build tree.
    * Dropped all sgen configurations, replaced by properties and target suffix.
    * Fixed pdb naming of intermediate pdb files.
    
    
    Commit migrated from https://github.com/mono/mono/commit/b1c604eee2bf5f80c14d4708424a1606894bb5b9

--- src/mono/msvc/install.bat
+++ src/mono/msvc/install.bat
@@ -1,42 +1,65 @@
 @ECHO off
 
-IF "" == "%1" (
+SET PLATFORM=%1
+SET CONFIG=%2
+SET BUILD_DIR=%3
+SET INSTALL_DIR=%4
+
+IF "" == "%PLATFORM%" (
 	ECHO Error: No platform parameter set.
 	GOTO ON_ERROR
 
 	)
-IF "" == "%2" (
+IF "" == "%CONFIG%" (
 	ECHO Error: No configuration parameter set.
 	GOTO ON_ERROR
 )
 
-IF "" == "%3" (
-	ECHO Error: No MONO_INSTALLATION_PREFIX parameter set.
+IF "" == "%BUILD_DIR%" (
+	ECHO Error: No MONO_BUILD_DIR_PREFIX parameter set.
 	GOTO ON_ERROR
 )
 
-IF NOT EXIST %3 (
-	ECHO Error: '%3', directory doesn't eixst.
+IF "" == "%INSTALL_DIR%" (
+	ECHO Error: No MONO_INSTALLATION_DIR_PREFIX parameter set.
 	GOTO ON_ERROR
 )
 
-SET PACKAGE_DIR=.\package\%1\%2
+IF "\" == "%BUILD_DIR:~-1%" (
+	SET BUILD_DIR=%BUILD_DIR:~0,-1%
+)
+
+IF "\" == "%INSTALL_DIR:~-1%" (
+	SET INSTALL_DIR=%INSTALL_DIR:~0,-1%
+)
+
+IF NOT EXIST %BUILD_DIR% (
+	ECHO Error: '%BUILD_DIR%', directory doesn't eixst.
+	GOTO ON_ERROR
+)
+
+IF NOT EXIST %INSTALL_DIR% (
+	ECHO Error: '%INSTALL_DIR%', directory doesn't eixst.
+	GOTO ON_ERROR
+)
+
+SET PACKAGE_DIR=%BUILD_DIR%\package\%PLATFORM%\%CONFIG%
 
 IF NOT EXIST %PACKAGE_DIR% (
 	ECHO Error: '%PACKAGE_DIR%' directory unavailable.
 	GOTO ON_ERROR
 )
 
-ECHO Installing mono build %1 %2 into %3 ...
+ECHO Installing mono build %PLATFORM% %CONFIG% from %BUILD_DIR% into %INSTALL_DIR% ...
 
-xcopy %PACKAGE_DIR%\*.* %3 /s /e /q /y > nul
+xcopy %PACKAGE_DIR%\*.* %INSTALL_DIR% /s /e /q /y > nul
 
-ECHO Installing of mono build %1 %2 into %3 DONE. 
+ECHO Installing of mono build %PLATFORM% %CONFIG% from %BUILD_DIR% into %INSTALL_DIR% DONE. 
 
 EXIT /b 0
 
 :ON_ERROR
-	ECHO "install.bat [win32|x64] [Debug|Release] [MONO_INSTALLATION_PREFIX]"
+	ECHO "install.bat [win32|x64] [Debug|Release] [MONO_BUILD_DIR_PREFIX] [MONO_INSTALLATION_DIR_PREFIX]"
 	EXIT /b 1
 
 @ECHO on
--- src/mono/msvc/package.bat
+++ src/mono/msvc/package.bat
@@ -1,48 +1,66 @@
 @ECHO off
 
-IF "" == "%1" (
+SET PLATFORM=%1
+SET CONFIG=%2
+SET BUILD_DIR=%3
+
+IF "" == "%PLATFORM%" (
 	ECHO Error: No platform parameter set.
 	GOTO ON_ERROR
 )
 	
-IF "" == "%2" (
+IF "" == "%CONFIG%" (
 	ECHO Error: No configuration parameter set.
 	GOTO ON_ERROR
 )
 
-IF NOT EXIST .\%1\lib\%2 (
-	ECHO Error: No lib directory available for %1 %2. Any build availalbe for platform, configuration pair?
+IF "" == "%BUILD_DIR%" (
+	ECHO Error: No MONO_BUILD_DIR_PREFIX parameter set.
 	GOTO ON_ERROR
 )
 
-IF NOT EXIST .\%1\bin\%2 (
-	ECHO Error: No bin directory available for %1 %2. Any build availalbe for platform, configuration pair?
+IF NOT EXIST %BUILD_DIR% (
+	ECHO Error: '%BUILD_DIR%', directory doesn't eixst.
 	GOTO ON_ERROR
 )
 
-SET PACKAGE_DIR=.\package\%1\%2
+IF "\" == "%BUILD_DIR:~-1%" (
+	SET BUILD_DIR=%BUILD_DIR:~0,-1%
+)
 
-ECHO Packaging mono build %1 %2 into '%PACKAGE_DIR%' ...
+IF NOT EXIST %BUILD_DIR%\%PLATFORM%\lib\%CONFIG% (
+	ECHO Error: No lib directory available for %PLATFORM% %CONFIG% at '%BUILD_DIR%'. Any build availalbe for platform, configuration pair?
+	GOTO ON_ERROR
+)
+
+IF NOT EXIST %BUILD_DIR%\%PLATFORM%\bin\%CONFIG% (
+	ECHO Error: No bin directory available for %PLATFORM% %CONFIG% at '%BUILD_DIR%'. Any build availalbe for platform, configuration pair?
+	GOTO ON_ERROR
+)
+
+SET PACKAGE_DIR=%BUILD_DIR%\package\%PLATFORM%\%CONFIG%
+
+ECHO Packaging mono build %PLATFORM% %CONFIG% into '%PACKAGE_DIR%' ...
 
 IF EXIST %PACKAGE_DIR% rmdir %PACKAGE_DIR% /s /q
 mkdir %PACKAGE_DIR%
 mkdir %PACKAGE_DIR%\include\mono-2.0
 xcopy .\include\*.* %PACKAGE_DIR%\include\mono-2.0\ /s /e /q /y > nul
 
-xcopy .\%1\lib\%2\*.lib %PACKAGE_DIR%\lib\ /s /e /q /y > nul
-xcopy .\%1\lib\%2\*.pdb %PACKAGE_DIR%\lib\ /s /e /q /y > nul
+xcopy %BUILD_DIR%\%PLATFORM%\lib\%CONFIG%\*.lib %PACKAGE_DIR%\lib\ /s /e /q /y > nul
+xcopy %BUILD_DIR%\%PLATFORM%\lib\%CONFIG%\*.pdb %PACKAGE_DIR%\lib\ /s /e /q /y > nul
 
-xcopy .\%1\bin\%2\*.exe %PACKAGE_DIR%\bin\ /s /e /q /y > nul
-xcopy .\%1\bin\%2\*.dll %PACKAGE_DIR%\bin\ /s /e /q /y > nul
-xcopy .\%1\bin\%2\*.pdb %PACKAGE_DIR%\bin\ /s /e /q /y > nul
-xcopy .\%1\bin\%2\*.lib %PACKAGE_DIR%\bin\ /s /e /q /y > nul
+xcopy %BUILD_DIR%\%PLATFORM%\bin\%CONFIG%\*.exe %PACKAGE_DIR%\bin\ /s /e /q /y > nul
+xcopy %BUILD_DIR%\%PLATFORM%\bin\%CONFIG%\*.dll %PACKAGE_DIR%\bin\ /s /e /q /y > nul
+xcopy %BUILD_DIR%\%PLATFORM%\bin\%CONFIG%\*.pdb %PACKAGE_DIR%\bin\ /s /e /q /y > nul
+xcopy %BUILD_DIR%\%PLATFORM%\bin\%CONFIG%\*.lib %PACKAGE_DIR%\bin\ /s /e /q /y > nul
 
-ECHO Packaging of mono build %1 %2 into '%PACKAGE_DIR%' DONE. 
+ECHO Packaging of mono build %PLATFORM% %CONFIG% into '%PACKAGE_DIR%' DONE. 
 
 EXIT /b 0
 
 :ON_ERROR
-	ECHO "package.bat [win32|x64] [Debug|Release]"
+	ECHO "package.bat [win32|x64] [Debug|Release] [MONO_BUILD_DIR_PREFIX]"
 	EXIT /b 1
 
 @ECHO on
