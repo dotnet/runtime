f73c697aa6d486832c8ea63c767ab407368497c7
Andi McClure <andi.mcclure@xamarin.com>
2016-04-14 16:26:39 -0400

    Bump corlib version number following PRmono/mono#2875
    Commit migrated from https://github.com/mono/mono/commit/a3f8e503293bd5cf6bef7695c4356aa70f374ba2

--- src/mono/mono/metadata/appdomain.c
+++ src/mono/mono/metadata/appdomain.c
@@ -81,7 +81,7 @@
  * Changes which are already detected at runtime, like the addition
  * of icalls, do not require an increment.
  */
-#define MONO_CORLIB_VERSION 144
+#define MONO_CORLIB_VERSION 145
 
 typedef struct
 {
