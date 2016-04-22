61ce7c3d42a9d4f4f5d520618065fc2af8ef5e7e
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2016-04-22 15:01:56 -0700

    Memory usage improvements in build (dotnet/core-setup#2626)
    * Use a WorkspaceContext in dotnet-build to cache project data across
    multiple compilations in a single build action
    * Dramatically reduce string and object duplication by introducing a
    "Symbol Table" that shares instances of NuGetVersion, NuGetFramework,
    VersionRange and string across multiple lock-file parses
    
    Test Results:
    * Testing was done by compiling Microsoft.AspNetCore.Mvc (and it's
    dependencies) and taking memory snapshots after each compilation in
    dotMemory
    * We used to allocate ~3MB and deallocate ~2.5MB on EACH compilation in
    a single build action. This has been reduced to ~120KB
    allocated/deallocated
    * After introducing WorkspaceContext, total memory usage spiked from 6MB
    across the whole build action to about 13MB, introducing the symbol
    table dropped it back to about 5-6MB.
    
    Commit migrated from https://github.com/dotnet/core-setup/commit/b69621cc0ff99dfabfbc3eb1fe184d97a3de0438

--- src/installer/Resolution/DotNetReferenceAssembliesPathResolver.cs
+++ src/installer/Resolution/DotNetReferenceAssembliesPathResolver.cs
@@ -11,7 +11,7 @@ namespace Microsoft.Extensions.DependencyModel.Resolution
     public class DotNetReferenceAssembliesPathResolver
     {
         public static readonly string DotNetReferenceAssembliesPathEnv = "DOTNET_REFERENCE_ASSEMBLIES_PATH";
-        
+
         internal static string Resolve(IEnvironment envirnment, IFileSystem fileSystem, IRuntimeEnvironment runtimeEnvironment)
         {
             var path = envirnment.GetEnvironmentVariable(DotNetReferenceAssembliesPathEnv);
@@ -19,40 +19,40 @@ namespace Microsoft.Extensions.DependencyModel.Resolution
             {
                 return path;
             }
-            
+
             return GetDefaultDotNetReferenceAssembliesPath(fileSystem, runtimeEnvironment);
         }
-        
+
         public static string Resolve()
         {
             return Resolve(EnvironmentWrapper.Default, FileSystemWrapper.Default, PlatformServices.Default.Runtime);
         }
-                
+
         private static string GetDefaultDotNetReferenceAssembliesPath(IFileSystem fileSystem, IRuntimeEnvironment runtimeEnvironment)
-        {            
+        {
             var os = runtimeEnvironment.OperatingSystemPlatform;
-            
+
             if (os == Platform.Windows)
             {
                 return null;
             }
-            
-            if (os == Platform.Darwin && 
+
+            if (os == Platform.Darwin &&
                 fileSystem.Directory.Exists("/Library/Frameworks/Mono.framework/Versions/Current/lib/mono/xbuild-frameworks"))
             {
                 return "/Library/Frameworks/Mono.framework/Versions/Current/lib/mono/xbuild-frameworks";
             }
-            
+
             if (fileSystem.Directory.Exists("/usr/local/lib/mono/xbuild-frameworks"))
             {
                 return "/usr/local/lib/mono/xbuild-frameworks";
             }
-            
+
             if (fileSystem.Directory.Exists("/usr/lib/mono/xbuild-frameworks"))
             {
                 return "/usr/lib/mono/xbuild-frameworks";
             }
-            
+
             return null;
         }
     }
