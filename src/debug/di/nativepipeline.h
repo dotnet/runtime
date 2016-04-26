9607acec3f53c4d7ac41b60d8a82affaee07832b
Mike McLaughlin <mikem@microsoft.com>
2016-04-26 16:57:31 -0700

    Fix issue #4298 "SIGSEGV_libcoreclr.so!Debugger::GetArgCount"
    The fix is to remove the call to TerminateDebugger in the EE shutdown
    path. The reason was to clean up the transport pipe files but that
    still happens in coreclr_uninitialize called by the host.
    
    Also added code to clean up the transport named pipes and semaphores
    on the debugger side when it detects that the target process has
    terminated before it sends the ExitProcess notification.
    
    Plumbed the cleanup call from dbi's ExitProcess code through the
    native pipe line to the transport and then to pipe code.
    
    Add PAL_CleanupTargetProcess for the "continue" named semaphore cleanup.
    
    Found and fixed a minor race in dbgshim register runtime startup.

--- src/debug/di/nativepipeline.h
+++ src/debug/di/nativepipeline.h
@@ -167,6 +167,14 @@ public:
     {
         return S_FALSE;
     } 
+
+#ifdef FEATURE_PAL
+    // Used by debugger side (RS) to cleanup the target (LS) named pipes 
+    // and semaphores when the debugger detects the debuggee process  exited.
+    virtual void CleanupTargetProcess()
+    {
+    }
+#endif
 };
 
 //
