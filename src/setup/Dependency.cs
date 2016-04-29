670c4ea554d77313dfb6e94ace3dc648c7db5150
Pranav K <prkrishn@hotmail.com>
2016-04-29 12:11:27 -0700

    Copy FileSystemGlobbing and HashCodeCombiner sources
    Commit migrated from https://github.com/dotnet/core-setup/commit/7dbafce3c393bd432fe6eb62cd512e07160de96a

--- src/setup/Dependency.cs
+++ src/setup/Dependency.cs
@@ -2,7 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using Microsoft.Extensions.Internal;
+using Microsoft.DotNet.InternalAbstractions;
 
 namespace Microsoft.Extensions.DependencyModel
 {
