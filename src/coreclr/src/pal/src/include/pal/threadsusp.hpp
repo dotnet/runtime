6bb4e90c832031bdb9a0bc4c2c60e051932e8463
Jan Vorlicek <janvorli@microsoft.com>
2016-05-09 21:09:55 +0200

    Let all signals be unblocked on Unix (dotnet/coreclr#4863)
    This change removes blocking of all of the 17 signals that we were
    blocking. I have looked at the default behavior of their handlers
    and we should be fine with it.
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/2edd715867b82ba73c9db4192986104ffc6a936d

--- src/coreclr/src/pal/src/include/pal/threadsusp.hpp
+++ src/coreclr/src/pal/src/include/pal/threadsusp.hpp
@@ -360,11 +360,6 @@ namespace CorUnix
                 DWORD *pdwSuspendCount
             );
 
-#if !HAVE_MACH_EXCEPTIONS
-            static 
-            VOID InitializeSignalSets();
-#endif // !HAVE_MACH_EXCEPTIONS
-
             VOID InitializeSuspensionLock();
 
             void SetBlockingPipe(
