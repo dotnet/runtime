5f7fec846b948d7f1b01a128988b2f8b7ef4a1a2
Senthil <schellap@microsoft.com>
2016-05-11 20:58:01 -0700

    Help message for dotnet
    Commit migrated from https://github.com/dotnet/core-setup/commit/c9716a578bebabc2dd849e1d5abf079e4bf05e74

--- src/installer/cli/setup.cmake
+++ src/installer/cli/setup.cmake
@@ -70,6 +70,12 @@ else()
     add_definitions(-DHOST_POLICY_PKG_VER="${CLI_CMAKE_HOST_POLICY_VER}")
 endif()
 
+if("${CLI_CMAKE_HOST_FXR_VER}" STREQUAL "")
+    message(FATAL_ERROR "Host FXR version is not specified")
+else()
+    add_definitions(-DHOST_FXR_PKG_VER="${CLI_CMAKE_HOST_FXR_VER}")
+endif()
+
 if("${CLI_CMAKE_PKG_RID}" STREQUAL "")
     message(FATAL_ERROR "A minimum supported package rid is not specified (ex: win7-x86 or ubuntu.14.04-x64, osx.10.10-x64, rhel.7-x64)")
 else()
