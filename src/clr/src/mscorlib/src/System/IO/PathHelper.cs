e505ec192f9ef0128a908fe0ae0249198adee3ac
Alex Ghiondea <ghiondea.alexandru@microsoft.com>
2016-05-19 12:17:41 -0700

    Checking in on behalf of JKuhne
    Fixing build break in K caused by the long file path work.
    
    [tfs-changeset: 1606374]
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/9acb5d6f68f7e0590e8be2542efdc8cc241dcec0

--- src/clr/src/mscorlib/src/System/IO/PathHelper.cs
+++ src/clr/src/mscorlib/src/System/IO/PathHelper.cs
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
+
+#if FEATURE_PATHCOMPAT
 using System;
 using System.Collections;
 using System.Text;
@@ -443,3 +445,4 @@ namespace System.IO {
 
     }
 }
+#endif // FEATURE_PATHCOMPAT
