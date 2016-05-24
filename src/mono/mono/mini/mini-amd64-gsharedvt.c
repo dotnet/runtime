25a140bbab5f7a8095cce876f8dd0863cf5c3655
Zoltan Varga <vargaz@gmail.com>
2016-05-24 15:35:25 -0400

    [amd64] Add back an assert removed by the previous change.
    Commit migrated from https://github.com/mono/mono/commit/2fb2f261e2014388e68eecbeaae1c26c58191707

--- src/mono/mono/mini/mini-amd64-gsharedvt.c
+++ src/mono/mono/mini/mini-amd64-gsharedvt.c
@@ -428,9 +428,25 @@ mono_arch_get_gsharedvt_call_info (gpointer addr, MonoMethodSignature *normal_si
 				case MONO_TYPE_U4:
 					info->ret_marshal = GSHAREDVT_RET_U4;
 					break;
-				default:
+				case MONO_TYPE_I:
+				case MONO_TYPE_U:
+				case MONO_TYPE_PTR:
+				case MONO_TYPE_FNPTR:
+				case MONO_TYPE_CLASS:
+				case MONO_TYPE_OBJECT:
+				case MONO_TYPE_SZARRAY:
+				case MONO_TYPE_ARRAY:
+				case MONO_TYPE_STRING:
+				case MONO_TYPE_U8:
+				case MONO_TYPE_I8:
 					info->ret_marshal = GSHAREDVT_RET_I8;
 					break;
+				case MONO_TYPE_GENERICINST:
+					g_assert (!mono_type_generic_inst_is_valuetype (ret));
+					info->ret_marshal = GSHAREDVT_RET_I8;
+					break;
+				default:
+					g_error ("Gsharedvt can't handle dst type [%d]", (int)sig->ret->type);
 				}
 			}
 			break;
