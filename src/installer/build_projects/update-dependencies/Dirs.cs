d9f9bf0d4e12a76eedac889a95322afe59148f37
Eric Erhardt <eric.erhardt@microsoft.com>
2016-05-25 15:30:38 -0500

    Add update-dependencies to core-setup. (dotnet/core-setup#10)
    Also - add the ability to specify environment variables on the command line during our build.  This allows us to use VSO secret variables.
    
    Commit migrated from https://github.com/dotnet/core-setup/commit/b6775bf32d741394a06d3f8aed56ab5f172df531

---
+++ src/installer/build_projects/update-dependencies/Dirs.cs
@@ -0,0 +1,12 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.IO;
+
+namespace Microsoft.DotNet.Scripts
+{
+    public static class Dirs
+    {
+        public static readonly string RepoRoot = Directory.GetCurrentDirectory();
+    }
+}
