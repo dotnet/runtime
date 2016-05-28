3889783dd233a3b1c506f19bba1d6c0f525f8519
Matt Ellis <matell@microsoft.com>
2016-05-27 22:29:49 -0700

    Update badge filename generation

--- build_projects/shared-build-targets-utils/Utils/Monikers.cs
+++ build_projects/shared-build-targets-utils/Utils/Monikers.cs
@@ -30,6 +30,21 @@ namespace Microsoft.DotNet.Cli.Build
             }
         }
 
+        public static string GetBadgeMoniker()
+        {
+            switch (RuntimeEnvironment.GetRuntimeIdentifier())
+            {
+                case "ubuntu.16.04-x64":
+                     return "Ubuntu_16_04_x64";
+                case "fedora.23-x64":
+                     return "Fedora_23_x64";
+                case "opensuse.13.2-x64":
+                     return "openSUSE_13_2_x64";
+            }
+
+            return $"{CurrentPlatform.Current}_{CurrentArchitecture.Current}";
+        }
+
         public static string GetDebianPackageName(BuildTargetContext c)
         {
             var channel = c.BuildContext.Get<string>("Channel").ToLower();
