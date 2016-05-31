3c35cd36762b6e5705ba9b24de367e11422d769b
Bryan Thornbury <brthor@microsoft.com>
2016-05-31 14:01:27 -0700

    Add CLI_NUGET_FEED_URL and CLI_NUGET_API_KEY to the build docker container environment variable list for dotnet-deb-tool publishing
    Commit migrated from https://github.com/dotnet/core-setup/commit/4ca1a91d231c9e7aa2cd4f89bcc34b06eb1b1ad7

--- src/setup/scripts/dockerrun.sh
+++ src/setup/scripts/dockerrun.sh
@@ -113,6 +113,8 @@ docker run $INTERACTIVE -t --rm --sig-proxy=true \
     -e NUGET_FEED_URL \
     -e NUGET_API_KEY \
     -e GITHUB_PASSWORD \
+    -e CLI_NUGET_FEED_URL \
+    -e CLI_NUGET_API_KEY \
     $DOTNET_BUILD_CONTAINER_TAG \
     $BUILD_COMMAND "$@"
 
