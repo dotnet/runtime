8a847009ccd9e0dc054da4672aa184a7030ceb8e
Bryan Thornbury <brthor@microsoft.com>
2016-06-02 16:02:24 -0700

    base test system, not working
    Commit migrated from https://github.com/dotnet/core-setup/commit/f711be1d7151f9bc1e04f559cec67c25bb61384a

---
+++ src/installer/TestAssets/TestProjects/PortableApp/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PortableApp
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
