b8de1e7b82752dfe5842c7097bda8f44ce067e80
Bryan Thornbury <brthor@microsoft.com>
2016-06-02 16:02:24 -0700

    base test system, not working

---
+++ TestAssets/TestProjects/StandaloneApp/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace StandaloneApp
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
