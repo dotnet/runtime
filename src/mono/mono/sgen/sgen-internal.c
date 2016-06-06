c0bacef15cabe5b9b93900523bd062be31a8811f
Vlad Brezae <brezaevlad@gmail.com>
2016-06-06 03:54:25 +0300

    [sgen] Add missing memory type description
    Commit migrated from https://github.com/mono/mono/commit/c9d37d36860856832bf11d1bb11c8d62d13937b1

--- src/mono/mono/sgen/sgen-internal.c
+++ src/mono/mono/sgen/sgen-internal.c
@@ -139,6 +139,7 @@ description_for_type (int type)
 	case INTERNAL_MEM_CARDTABLE_MOD_UNION: return "cardtable-mod-union";
 	case INTERNAL_MEM_BINARY_PROTOCOL: return "binary-protocol";
 	case INTERNAL_MEM_TEMPORARY: return "temporary";
+	case INTERNAL_MEM_LOG_ENTRY: return "log-entry";
 	default: {
 		const char *description = sgen_client_description_for_internal_mem_type (type);
 		SGEN_ASSERT (0, description, "Unknown internal mem type");
