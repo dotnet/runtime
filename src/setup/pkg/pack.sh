b351fc4da8b9a59bc0f0b28ac7b2db3126fb1b10
Wes Haggard <Wes.Haggard@microsoft.com>
2016-06-07 13:15:00 -0700

    Switch to generating version.props instead of using env variables
    This has the outer build system generate a verison.props so
    the package builds can consume the versions from there instead
    of using env variables or a crazy list of arguments.
    
    This has the added bonus that once you build once from the root
    you can easily rebuild the just the packages without a full build
    by running pack.cmd/sh directly.
    
    
    Commit migrated from https://github.com/dotnet/core-setup/commit/08886f6cc467a12e6ad9b51e70eb35f60f25a83a

--- src/setup/pkg/pack.sh
+++ src/setup/pkg/pack.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-usage()
-{
-   echo "Usage: ${BASH_SOURCE[0]} --arch x64/x86/arm --hostbindir path-to-binaries" --hostver --fxrver --policyver --build --vertag
-   exit 1
-}
-
 init_distro_name()
 {
     if [ ! -e /etc/os-release ]; then
@@ -13,7 +7,7 @@ init_distro_name()
         export __distro_rid=""
     else
         source /etc/os-release
-        export __distro_rid="$ID.$VERSION_ID-$__build_arch"
+        export __distro_rid="$ID.$VERSION_ID-x64"
     fi
 }
 
@@ -29,69 +23,7 @@ done
 
 # initialize variables
 __project_dir="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-__build_arch=
-__dotnet_host_bin_dir=
 __distro_rid=
-__host_ver=
-__fxr_ver=
-__policy_ver=
-__build_major=
-__build_minor=
-__version_tag=
-
-# parse arguments
-while [ "$1" != "" ]; do
-        lowerI="$(echo $1 | awk '{print tolower($0)}')"
-        case $lowerI in
-        -h|--help)
-            usage
-            exit 1
-            ;;
-        --arch)
-            shift
-            __build_arch=$1
-            ;;
-        --hostbindir)
-            shift
-            __dotnet_host_bin_dir=$1
-            ;;
-        --hostver)
-            shift
-            __host_ver=$1
-            ;;
-        --fxrver)
-            shift
-            __fxr_ver=$1
-            ;;
-        --policyver)
-            shift
-            __policy_ver=$1
-            ;;
-        --build-major)
-            shift
-            __build_major=$1
-            ;;
-        --build-minor)
-            shift
-            __build_minor=$1
-            ;;
-        --vertag)
-            shift
-            __version_tag=$1
-            ;;
-        *)
-        echo "Unknown argument to pack.sh $1"; exit 1
-    esac
-    shift
-done
-
-# validate args
-if [ -z $__dotnet_host_bin_dir ]; then
-    usage
-fi
-if [ -z $__build_arch ]; then
-    usage
-fi
 
 # setup msbuild
 "$__project_dir/init-tools.sh"
@@ -116,10 +48,8 @@ else
     init_distro_name
 fi
 
-__common_parameters="/p:Platform=$__build_arch /p:DotNetHostBinDir=$__dotnet_host_bin_dir /p:$__targets_param /p:DistroRid=$__distro_rid /p:HostVersion=$__host_ver /p:HostResolverVersion=$__fxr_ver /p:HostPolicyVersion=$__policy_ver /p:BuildNumberMajor=$__build_major /p:BuildNumberMinor=$__build_minor /p:PreReleaseLabel=$__version_tag /p:CLIBuildVersion=$__build_major /verbosity:minimal"
+__common_parameters="/p:$__targets_param /p:DistroRid=$__distro_rid /verbosity:minimal"
 
 $__corerun $__msbuild $__project_dir/projects/packages.builds $__common_parameters || exit 1
-
-cp -rf "$__project_dir/bin/packages" "$__dotnet_host_bin_dir"
 
 exit 0
