58255fafdd32ba29609b8afbdf23c2882e3a0cf6
Zoltan Varga <vargaz@gmail.com>
2016-06-07 04:08:41 -0400

    [runtime] Call mono_class_init () for classes in mono_class_is_subclass_of (). Fixes mono/mono#41466.
    Commit migrated from https://github.com/mono/mono/commit/97634ecfae6b2b7ec4b1efd0e1a1fdcd23e69224

--- src/mono/mono/metadata/class.c
+++ src/mono/mono/metadata/class.c
@@ -8022,7 +8022,9 @@ gboolean
 mono_class_is_subclass_of (MonoClass *klass, MonoClass *klassc, 
 			   gboolean check_interfaces)
 {
-/*FIXME test for interfaces with variant generic arguments*/
+	/* FIXME test for interfaces with variant generic arguments */
+	mono_class_init (klass);
+	mono_class_init (klassc);
 	
 	if (check_interfaces && MONO_CLASS_IS_INTERFACE (klassc) && !MONO_CLASS_IS_INTERFACE (klass)) {
 		if (MONO_CLASS_IMPLEMENTS_INTERFACE (klass, klassc->interface_id))
