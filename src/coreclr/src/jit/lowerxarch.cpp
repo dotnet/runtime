cb19a69c78f0ec0a2b4a80362bc1fa595441bea4
Bruce Forstall <brucefo@microsoft.com>
2016-06-28 14:56:50 -0700

    Address P/Invoke inlining code review feedback and cleanup
    Address some code review feedback from dotnet/coreclr#5939. Also, do a little cleanup.
    Specifically:
    1. Make many GenTreeCall accessors 'const'.
    2. HasNonStandardArgs() and GetNonStandardArgCount() are used for a specific purpose in the fast
    tailcall implementation. However, the fgMorphArgs() "non-standard args" mechanism is now used for
    other purposes, so the names of these didn't match their intent. Also, the logic had decayed compared
    to the fgMorphArgs logic it was mimicing. I renamed them to add the word "Added", and wrote some
    comments to be more explicit about their use, as well as indicate in fgMorphArgs() that they need
    to be kept in sync.
    3. Added several GenTreeCall::IsHelperCall() accessors, and replace some code with them.
    4. Added RBM_INIT_PINVOKE_FRAME_TRASH define to remove an `#ifdef`.
    5. Removed an assert loop in LowerFastTailCall() since it's no longer accurate given the new users of `isNonStandard`.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/d415a00bd954f7f9306c38c4e4fbfe34ccfa7242

--- src/coreclr/src/jit/lowerxarch.cpp
+++ src/coreclr/src/jit/lowerxarch.cpp
@@ -1073,7 +1073,7 @@ Lowering::TreeNodeInfoInitCall(GenTreeCall* call)
 
     // Set destination candidates for return value of the call.
 #ifdef _TARGET_X86_
-    if ((call->gtCallType == CT_HELPER) && (call->gtCallMethHnd == compiler->eeFindHelper(CORINFO_HELP_INIT_PINVOKE_FRAME)))
+    if (call->IsHelperCall(compiler, CORINFO_HELP_INIT_PINVOKE_FRAME))
     {
         // The x86 CORINFO_HELP_INIT_PINVOKE_FRAME helper uses a custom calling convention that returns with
         // TCB in REG_PINVOKE_TCB. AMD64/ARM64 use the standard calling convention. fgMorphCall() sets the
