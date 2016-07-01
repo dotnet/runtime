3ba98d51b4f22eed785c851767140fc0d3e239c8
Zoltan Varga <vargaz@gmail.com>
2016-06-30 21:15:05 -0400

    [tests] Probe for a different type than System.IO.Pipes.PipeOptions which is not part of the mobile_static profile. Fixes mono/mono#41955.
    Commit migrated from https://github.com/mono/mono/commit/fe991525f6c64f9f4f5c3e1715df0fd3aceb54cd

--- src/mono/mono/tests/bug-30085.cs
+++ src/mono/mono/tests/bug-30085.cs
@@ -18,7 +18,7 @@ class Program
 	static void ProbeCorlib ()
 	{
 		Type good = System.Type.GetType("System.Nullable`1[[System.Int32, mscorlib]]"); 
-		Type bad = System.Type.GetType("System.Nullable`1[[System.IO.Pipes.PipeOptions, System.Core, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089]]");
+		Type bad = System.Type.GetType("System.Nullable`1[[System.IO.MemoryMappedFiles.MemoryMappedFile, System.Core, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089]]");
 
 		if (good.Assembly.FullName.Split (',') [0] != "mscorlib")
 			throw new Exception ("Wrong assembly name");
