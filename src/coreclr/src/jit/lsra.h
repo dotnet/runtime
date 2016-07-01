d428201c32e84c7c6c22c28202e421f9bb688d01
sivarv <sivarv@microsoft.com>
2016-07-01 11:41:02 -0700

    Consider spilled lcl var as contained memory operands for codegen purpose.
    Commit migrated from https://github.com/dotnet/coreclr/commit/d92b629cdb742c42ef7b6a35f3ba55104539c9bd

--- src/coreclr/src/jit/lsra.h
+++ src/coreclr/src/jit/lsra.h
@@ -538,6 +538,22 @@ private:
     LsraSpill                   getLsraSpill()                  { return (LsraSpill) (lsraStressMask & LSRA_SPILL_MASK); }
     bool                        spillAlways()                   { return getLsraSpill() == LSRA_SPILL_ALWAYS; }
 
+    // This controls whether RefPositions that require a register optionally should
+    // be allocated a reg at all.
+    enum LsraRegOptionalControl     { LSRA_REG_OPTIONAL_DEFAULT          = 0,
+                                      LSRA_REG_OPTIONAL_NO_ALLOC         = 0x1000,
+                                      LSRA_REG_OPTIONAL_MASK             = 0x1000 };
+
+    LsraRegOptionalControl      getLsraRegOptionalControl()            
+    { 
+        return (LsraRegOptionalControl) (lsraStressMask & LSRA_REG_OPTIONAL_MASK); 
+    }
+
+    bool                        regOptionalNoAlloc()     
+    { 
+        return getLsraRegOptionalControl() == LSRA_REG_OPTIONAL_NO_ALLOC;
+    }
+
     // Dump support
     void            lsraDumpIntervals(const char* msg);
     void            dumpRefPositions(const char *msg);
@@ -743,6 +759,8 @@ private:
 
     void associateRefPosWithRegister(RefPosition *rp);
 
+    unsigned getWeight(RefPosition* refPos);
+
     /*****************************************************************************
      * Register management
      ****************************************************************************/
@@ -750,7 +768,7 @@ private:
     regNumber tryAllocateFreeReg(Interval *current, RefPosition *refPosition);
     RegRecord* findBestPhysicalReg(RegisterType regType, LsraLocation endLocation,
                                   regMaskTP candidates, regMaskTP preferences);
-    regNumber allocateBusyReg(Interval *current, RefPosition *refPosition);
+    regNumber allocateBusyReg(Interval* current, RefPosition* refPosition, bool allocationOptional);
     regNumber assignCopyReg(RefPosition * refPosition);
 
     void checkAndAssignInterval(RegRecord * regRec, Interval * interval);
@@ -1326,15 +1344,24 @@ public:
     LsraLocation    nodeLocation;
     regMaskTP       registerAssignment;
 
-    regNumber       assignedReg() { return genRegNumFromMask(registerAssignment); }
+    regNumber       assignedReg() { 
+        if (registerAssignment == RBM_NONE)
+        {
+            return REG_NA;
+        }
+
+        return genRegNumFromMask(registerAssignment); 
+    }
 
     RefType         refType;
 
-    bool            RequiresRegister()
+    bool            IsActualRef()
     {
-        return (refType == RefTypeDef || refType == RefTypeUse
+        return (refType == RefTypeDef || 
+                refType == RefTypeUse
 #if FEATURE_PARTIAL_SIMD_CALLEE_SAVE
-               || refType == RefTypeUpperVectorSaveDef || refType == RefTypeUpperVectorSaveUse
+                || refType == RefTypeUpperVectorSaveDef 
+                || refType == RefTypeUpperVectorSaveUse
 #endif // FEATURE_PARTIAL_SIMD_CALLEE_SAVE
                );
     }
@@ -1353,6 +1380,23 @@ public:
     }
 
     unsigned        getMultiRegIdx() { return multiRegIdx;  }
+
+    // Returns true if codegen has indicated that the tree node
+    // referred to by RefPosition can be treated as a contained
+    // memory operand if no register was allocated.
+    bool           IsRegOptional()
+    {
+        // TODO-CQ: Right now if a ref position is marked as
+        // copyreg or movereg, then it is always allocated a
+        // register, though it is marked as reg optional.
+        // This is an implementation limitation that needs to 
+        // be addressed.
+        return (refType == RefTypeUse) &&
+               !copyReg &&
+               !moveReg &&
+               (treeNode != nullptr) &&
+               treeNode->IsRegOptional();
+    }  
 
     // Last Use - this may be true for multiple RefPositions in the same Interval
     bool            lastUse      : 1;
