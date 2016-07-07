5cc1902777221e0dac49a548f9710169ceea9fd9
Tijoy Tom Kalathiparambil <tijoytk@microsoft.com>
2016-07-07 15:03:01 -0700

    MarshalAs(UnManaged.ByValArray) overflow. For non-blittable embedded array in structs we ignored the SizeConst and wrote past the buffer when number of elementsin the arrayis greater than SizeConst.Fix is to truncate the array at SizeConst MarshalAs(UnManaged.ByValTStr) Very subtle case when the SizeConst == Number of bytes required to marshal , we write the null one past the buffer.This happens only on  machine with non-english (multi-byte) locale as default. Fix is to check the number of bytes required and truncate the correctly leaving space for the terminating null.
    Commit migrated from https://github.com/dotnet/coreclr/commit/8d51b631b3f582a093a108acfa28c2390fe85f41

---
+++ src/clr/tests/src/Interop/MarshalAPI/MarshalStructure/MarshalStructure.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        VerifyByValBoolArray();
+        VerifyByValArrayInStruct();
+        VerfiyByValDateArray();
+        return 100;
+    }
+    
+    static void VerifyByValBoolArray()
+    {
+        var structure1 = new StructWithBoolArray()
+        {
+            array = new bool[]
+            {
+                true,true,true,true
+            }
+        };
+
+        int size = Marshal.SizeOf(structure1);
+        IntPtr memory = Marshal.AllocHGlobal(size + sizeof(Int32));
+
+        try
+        {
+            Marshal.WriteInt32(memory, size, 0xFF);
+            Marshal.StructureToPtr(structure1, memory, false);
+
+            if (Marshal.ReadInt32(memory, size) != 0xFF)
+                throw new Exception("Marshal.StructureToPtr buffer overwritten...");
+        }
+        finally
+        {
+            Marshal.FreeHGlobal(memory);
+        }
+    }
+
+    static void VerifyByValArrayInStruct()
+    {
+        // equal
+        var structure1 = new StructWithByValArray()
+        {
+            array = new StructWithIntField[]
+            {
+                new StructWithIntField { value = 1 },
+                new StructWithIntField { value = 2 },
+                new StructWithIntField { value = 3 },
+                new StructWithIntField { value = 4 },
+                new StructWithIntField { value = 5 }
+            }
+        };
+        int size = Marshal.SizeOf(structure1);
+        IntPtr memory = Marshal.AllocHGlobal(size);
+        try
+        {
+            Marshal.StructureToPtr(structure1, memory, false);
+        }
+        finally
+        {
+            Marshal.FreeHGlobal(memory);
+        }
+
+        // underflow
+        var structure2 = new StructWithByValArray()
+        {
+            array = new StructWithIntField[]
+         {
+                new StructWithIntField { value = 1 },
+                new StructWithIntField { value = 2 },
+                new StructWithIntField { value = 3 },
+                new StructWithIntField { value = 4 }
+         }
+        };
+        bool expectedException = false;
+        size = Marshal.SizeOf(structure2);
+        memory = Marshal.AllocHGlobal(size);
+        try
+        {
+            Marshal.StructureToPtr(structure2, memory, false);
+        }
+        catch (ArgumentException)
+        {
+            expectedException = true;
+        }
+        finally
+        {
+            Marshal.FreeHGlobal(memory);
+        }
+        if (!expectedException)
+            throw new Exception("Expected ArgumentException");
+
+        // overflow
+        var structure3 = new StructWithByValArray()
+        {
+            array = new StructWithIntField[]
+         {
+                new StructWithIntField { value = 1 },
+                new StructWithIntField { value = 2 },
+                new StructWithIntField { value = 3 },
+                new StructWithIntField { value = 4 },
+                new StructWithIntField { value = 5 },
+                new StructWithIntField { value = 6 }
+         }
+        };
+
+        size = Marshal.SizeOf(structure3);
+        memory = Marshal.AllocHGlobal(size);
+        try
+        {
+            Marshal.StructureToPtr(structure3, memory, false);
+        }
+        finally
+        {
+            Marshal.FreeHGlobal(memory);
+        }
+    }
+    
+    static void VerfiyByValDateArray()
+    {
+        var structure1 = new StructWithDateArray()
+        {
+           array = new DateTime[]
+           {
+                DateTime.Now, DateTime.Now , DateTime.Now, DateTime.Now, DateTime.Now, DateTime.Now , DateTime.Now, DateTime.Now
+           }
+        };
+
+        int size = Marshal.SizeOf(structure1);
+        IntPtr memory = Marshal.AllocHGlobal(size);
+        try
+        {
+            Marshal.StructureToPtr(structure1, memory, false);
+        }
+        finally
+        {
+            Marshal.FreeHGlobal(memory);
+        }
+    }
+
+    public struct StructWithIntField
+    {
+        public int value;
+    }
+
+    public struct StructWithByValArray
+    {
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 5)]
+        public StructWithIntField[] array;
+    }
+
+    public struct StructWithBoolArray
+    {
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+        public bool[] array;
+    }
+
+    public struct StructWithDateArray
+    {
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+        public DateTime[] array;
+    }
+}
