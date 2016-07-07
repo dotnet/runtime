aae323011a1c9b957a407ea13b8bfcfb18f3f675
Jb Evain <jb@evain.net>
2016-07-07 13:07:58 -0700

    [runtime] Fix the CLI_FLAGS_PREFERRED32BIT value
    Commit migrated from https://github.com/mono/mono/commit/2a8146f2876410216412ef8580452e35f961f2e8

--- src/mono/mono/metadata/cil-coff.h
+++ src/mono/mono/metadata/cil-coff.h
@@ -295,8 +295,8 @@ typedef struct {
 #define CLI_FLAGS_ILONLY         0x01
 #define CLI_FLAGS_32BITREQUIRED  0x02
 #define CLI_FLAGS_STRONGNAMESIGNED 0x8
-#define CLI_FLAGS_PREFERRED32BIT  0x10
 #define CLI_FLAGS_TRACKDEBUGDATA 0x00010000
+#define CLI_FLAGS_PREFERRED32BIT 0x00020000
 	guint32        ch_flags;
 
 	guint32        ch_entry_point;
