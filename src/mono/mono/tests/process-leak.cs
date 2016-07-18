c0e0036f7c7c54670d3c409c3ca8d2823d9c8aa0
Zoltan Varga <vargaz@gmail.com>
2016-07-18 11:44:45 -0400

    [tests] Reduce a test iteration in process-leak.exe so it doesn't take minutes to run.
    Commit migrated from https://github.com/mono/mono/commit/0e97e079fa6f90c2bf864d315a2e2c8bf8ca7410

--- src/mono/mono/tests/process-leak.cs
+++ src/mono/mono/tests/process-leak.cs
@@ -27,7 +27,7 @@ namespace ToManyOpenHandles
 
 		private static void RunStuffMode()
 		{
-			for (int i = 0; i < 1000; i++)
+			for (int i = 0; i < 100; i++)
 			{
 				Execute(Assembly.GetExecutingAssembly().Location, i.ToString());
 			}
