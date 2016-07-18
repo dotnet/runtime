24066e1427bbb6be7e95449302040daf2fcddfd7
Evgeny Pavlov <e.pavlov@samsung.com>
2016-07-18 15:19:26 +0300

    Create coreclr delegate inside coreclr_initialize

--- src/coreclr/hosts/inc/coreclrhost.h
+++ src/coreclr/hosts/inc/coreclrhost.h
@@ -47,21 +47,6 @@ CORECLR_HOSTING_API(coreclr_execute_assembly,
 
 #undef CORECLR_HOSTING_API
 
-struct SequencePointInfo
-{
-    int lineNumber, ilOffset;
-    char16_t* fileName;
-};
-
-struct MethodDebugInfo
-{
-    SequencePointInfo* points;
-    int size;
-};
-
-typedef int (*GetInfoForMethodDelegate)(const char*, unsigned int, MethodDebugInfo& methodDebugInfo);
-extern GetInfoForMethodDelegate getInfoForMethodDelegate;
-
 #endif // __CORECLR_HOST_H__
 
 
