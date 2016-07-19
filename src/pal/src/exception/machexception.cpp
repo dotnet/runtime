9585fc244705b3a06c1f99dc6d60b9c9583ec3ed
Aditya Mandaleeka <adityam@microsoft.com>
2016-07-18 19:39:05 -0700

    Save and restore ymm registers in signal handlers.
    - Modified the CONTEXT structure for storing the upper 16 bytes of ymm registers
    - Upon start of signal handler, ymmh data is copied from the native context to the CONTEXT structure, and a new flag is set to indicate that it has ymmh data
    - Upon calling RtlRestoreContext, the new flag is checked, and ymmh data is restored into registers from the CONTEXT structure
    - This change fixes only the Linux side for now.

--- src/pal/src/exception/machexception.cpp
+++ src/pal/src/exception/machexception.cpp
@@ -14,12 +14,14 @@ Abstract:
 
 --*/
 
+#include "pal/dbgmsg.h"
+SET_DEFAULT_DEBUG_CHANNEL(EXCEPT); // some headers have code with asserts, so do this first
+
 #include "pal/thread.hpp"
 #include "pal/seh.hpp"
 #include "pal/palinternal.h"
 #if HAVE_MACH_EXCEPTIONS
 #include "machexception.h"
-#include "pal/dbgmsg.h"
 #include "pal/critsect.h"
 #include "pal/debug.h"
 #include "pal/init.h"
@@ -41,8 +43,6 @@ Abstract:
 #include <mach-o/loader.h>
 
 using namespace CorUnix;
-
-SET_DEFAULT_DEBUG_CHANNEL(EXCEPT);
 
 // The port we use to handle exceptions and to set the thread context
 mach_port_t s_ExceptionPort;
