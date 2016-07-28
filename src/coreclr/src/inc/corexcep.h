46fcf2b0b47749c35a27bed4f04997a394ddd2b8
Aditya Mandaleeka <adityamandaleeka@users.noreply.github.com>
2016-07-27 21:29:36 -0700

    Fix a couple of paths related to exception codes. (dotnet/coreclr#6497)
    Commit migrated from https://github.com/dotnet/coreclr/commit/f30a8de1dc0190dedded08a6f03951ecc1a17604

--- src/coreclr/src/inc/corexcep.h
+++ src/coreclr/src/inc/corexcep.h
@@ -14,7 +14,7 @@
 
 // All COM+ exceptions are expressed as a RaiseException with this exception
 // code.  If you change this value, you must also change
-// bcl\src\system\Exception.cs's _COMPlusExceptionCode value.
+// mscorlib\src\system\Exception.cs's _COMPlusExceptionCode value.
 
 #define EXCEPTION_MSVC    0xe06d7363    // 0xe0000000 | 'msc'
 
