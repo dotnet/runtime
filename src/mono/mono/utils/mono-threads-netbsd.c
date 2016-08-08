328599ff630c2cfc99360229d446f79fd320abf4
Kamil Rytarowski <krytarowski@users.noreply.github.com>
2016-08-08 23:21:38 +0200

    Introduce mono-threads-netbsd.c (mono/mono#3374)
    Commit migrated from https://github.com/mono/mono/commit/5fe8bfae7de7168a5061269b8fcf1afed0c7c0a0

---
+++ src/mono/mono/utils/mono-threads-netbsd.c
@@ -0,0 +1,23 @@
+#include <config.h>
+
+#if defined(__NetBSD__)
+
+#include <mono/utils/mono-threads.h>
+#include <pthread.h>
+
+void
+mono_threads_platform_get_stack_bounds (guint8 **staddr, size_t *stsize)
+{
+	pthread_attr_t attr;
+
+	*staddr = NULL;
+	*stsize = (size_t)-1;
+
+	pthread_attr_init (&attr);
+	pthread_attr_get_np (pthread_self (), &attr);
+
+	pthread_attr_getstack (&attr, (void**)staddr, stsize);
+	pthread_attr_destroy (&attr);
+}
+
+#endif
