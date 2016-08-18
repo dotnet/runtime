97464a7c614cea28c9f526fe82c68d1a676ac964
Stephen Toub <stoub@microsoft.com>
2016-08-18 14:14:02 -0400

    Remove TypeForwardedFrom attributes for System and System.Core
    These assemblies don't exist in .NET Core.  Having these attributes on serializable types causes deserialization for such types to fail, as it attempts to load the assembly from which they were forwarded.

--- src/mscorlib/src/System/Action.cs
+++ src/mscorlib/src/System/Action.cs
@@ -12,65 +12,47 @@ namespace System {
     // Action/Func delegates first shipped with .NET Framework 3.5 in System.Core.dll as part of LINQ
     // These were type forwarded to mscorlib.dll in .NET Framework 4.0 and in Silverlight 5.0
 
-#if FEATURE_CORECLR
-    [TypeForwardedFrom("System.Core, Version=2.0.5.0, Culture=Neutral, PublicKeyToken=7cec85d7bea7798e")]
-#else
+#if !FEATURE_CORECLR
     [TypeForwardedFrom("System.Core, Version=3.5.0.0, Culture=Neutral, PublicKeyToken=b77a5c561934e089")]
 #endif
     public delegate void Action();
 
-#if FEATURE_CORECLR
-    [TypeForwardedFrom("System.Core, Version=2.0.5.0, Culture=Neutral, PublicKeyToken=7cec85d7bea7798e")]
-#else
+#if !FEATURE_CORECLR
     [TypeForwardedFrom("System.Core, Version=3.5.0.0, Culture=Neutral, PublicKeyToken=b77a5c561934e089")]
 #endif
     public delegate void Action<in T1,in T2>(T1 arg1, T2 arg2);
 
-#if FEATURE_CORECLR
-    [TypeForwardedFrom("System.Core, Version=2.0.5.0, Culture=Neutral, PublicKeyToken=7cec85d7bea7798e")]
-#else
+#if !FEATURE_CORECLR
     [TypeForwardedFrom("System.Core, Version=3.5.0.0, Culture=Neutral, PublicKeyToken=b77a5c561934e089")]
 #endif
     public delegate void Action<in T1,in T2,in T3>(T1 arg1, T2 arg2, T3 arg3);
 
-#if FEATURE_CORECLR
-    [TypeForwardedFrom("System.Core, Version=2.0.5.0, Culture=Neutral, PublicKeyToken=7cec85d7bea7798e")]
-#else
+#if !FEATURE_CORECLR
     [TypeForwardedFrom("System.Core, Version=3.5.0.0, Culture=Neutral, PublicKeyToken=b77a5c561934e089")]
 #endif
     public delegate void Action<in T1,in T2,in T3,in T4>(T1 arg1, T2 arg2, T3 arg3, T4 arg4);
 
-#if FEATURE_CORECLR
-    [TypeForwardedFrom("System.Core, Version=2.0.5.0, Culture=Neutral, PublicKeyToken=7cec85d7bea7798e")]
-#else
+#if !FEATURE_CORECLR
     [TypeForwardedFrom("System.Core, Version=3.5.0.0, Culture=Neutral, PublicKeyToken=b77a5c561934e089")]
 #endif
     public delegate TResult Func<out TResult>();
 
-#if FEATURE_CORECLR
-    [TypeForwardedFrom("System.Core, Version=2.0.5.0, Culture=Neutral, PublicKeyToken=7cec85d7bea7798e")]
-#else
+#if !FEATURE_CORECLR
     [TypeForwardedFrom("System.Core, Version=3.5.0.0, Culture=Neutral, PublicKeyToken=b77a5c561934e089")]
 #endif
     public delegate TResult Func<in T, out TResult>(T arg);
 
-#if FEATURE_CORECLR
-    [TypeForwardedFrom("System.Core, Version=2.0.5.0, Culture=Neutral, PublicKeyToken=7cec85d7bea7798e")]
-#else
+#if !FEATURE_CORECLR
     [TypeForwardedFrom("System.Core, Version=3.5.0.0, Culture=Neutral, PublicKeyToken=b77a5c561934e089")]
 #endif
     public delegate TResult Func<in T1, in T2, out TResult>(T1 arg1, T2 arg2);
 
-#if FEATURE_CORECLR
-    [TypeForwardedFrom("System.Core, Version=2.0.5.0, Culture=Neutral, PublicKeyToken=7cec85d7bea7798e")]
-#else
+#if !FEATURE_CORECLR
     [TypeForwardedFrom("System.Core, Version=3.5.0.0, Culture=Neutral, PublicKeyToken=b77a5c561934e089")]
 #endif
     public delegate TResult Func<in T1, in T2, in T3, out TResult>(T1 arg1, T2 arg2, T3 arg3);
 
-#if FEATURE_CORECLR
-    [TypeForwardedFrom("System.Core, Version=2.0.5.0, Culture=Neutral, PublicKeyToken=7cec85d7bea7798e")]
-#else
+#if !FEATURE_CORECLR
     [TypeForwardedFrom("System.Core, Version=3.5.0.0, Culture=Neutral, PublicKeyToken=b77a5c561934e089")]
 #endif
     public delegate TResult Func<in T1, in T2, in T3, in T4, out TResult>(T1 arg1, T2 arg2, T3 arg3, T4 arg4);
