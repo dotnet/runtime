03216cd997efb590fea7621065cbf07ea8772a15
Stephen Toub <stoub@microsoft.com>
2016-08-18 14:14:02 -0400

    Remove TypeForwardedFrom attributes for System and System.Core
    These assemblies don't exist in .NET Core.  Having these attributes on serializable types causes deserialization for such types to fail, as it attempts to load the assembly from which they were forwarded.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/97464a7c614cea28c9f526fe82c68d1a676ac964

--- src/coreclr/src/mscorlib/src/System/Threading/LockRecursionException.cs
+++ src/coreclr/src/mscorlib/src/System/Threading/LockRecursionException.cs
@@ -20,7 +20,9 @@ namespace System.Threading
 
     [Serializable]
     [System.Security.Permissions.HostProtection(MayLeakOnAbort = true)]
+#if !FEATURE_CORECLR
     [TypeForwardedFrom("System.Core, Version=3.5.0.0, Culture=Neutral, PublicKeyToken=b77a5c561934e089")]
+#endif
     public class LockRecursionException : System.Exception
     {
         public LockRecursionException() { }
