046b9b9e6214228030dfe521838bfd771e46412b
Bruce Forstall <brucefo@microsoft.com>
2016-08-19 15:56:04 -0700

    Open source SuperPMI
    OVERVIEW
    ========
    
    This directory contains the SuperPMI tool used for testing the .NET
    just-in-time (JIT) compiler.
    
    SuperPMI has two uses:
    1. Verification that a JIT code change doesn't cause any asserts.
    2. Finding test code where two JIT compilers generate different code, or verifying
    that the two compilers generate the same code.
    
    Case dotnet/coreclr#1 is useful for doing quick regression checking when making a source
    code change to the JIT compiler. The process is: (a) make a JIT source code
    change, (b) run that newly built JIT through a SuperPMI run to verify no
    asserts have been introduced.
    
    Case dotnet/coreclr#2 is useful for generating assembly language diffs, to help analyze the
    impact of a JIT code change.
    
    SuperPMI works in two phases: collection and playback. In the collection phase,
    the system is configured to collect SuperPMI data. Then, run any set of .NET managed
    programs. When these managed programs invoke the JIT compiler, SuperPMI gathers and
    captures all information passed between the JIT and its .NET host. In the
    playback phase, SuperPMI loads the JIT directly, and causes it to compile all
    the functions that it previously compiled, but using the collected data to
    provide answers to various questions that the JIT needs to ask. The .NET
    execution engine (EE) is not invoked at all.
    
    TOOLS
    ==========
    
    There are two native executable tools: superpmi and mcs. There is a .NET Core
    C# program that is built as part of the coreclr repo tests build called
    superpmicollect.exe.
    
    All will show a help screen if passed -?.
    
    COLLECTION
    ==========
    
    Set the following environment variables:
    
    SuperPMIShimLogPath=<full path to an empty temporary directory>
    SuperPMIShimPath=<full path to clrjit.dll, the "standalone" JIT>
    COMPlus_AltJit=*
    COMPlus_AltJitName=superpmi-shim-collector.dll
    
    (On Linux, use libclrjit.so and libsuperpmi-shim-collector.so. On Mac,
    use libclrjit.dylib and libsuperpmi-shim-collector.dylib.)
    
    Then, run some managed programs. When done running programs, un-set these variables.
    
    Now, you will have a large number of .mc files. Merge these using the mcs
    tool:
    
    mcs -merge base.mch *.mc
    
    One benefit of SuperPMI is the ability to remove duplicated compilations, so
    on replay only unique functions are compiled. Use the following to create a
    "unique" set of functions:
    
    mcs -removeDup -thin base.mch unique.mch
    
    Note that -thin is not required. However, it will delete all the compilation
    result collected during the collection phase, which makes the resulting MCH
    file smaller. Those compilation results are not required for playback.
    
    Use the superpmicollect.exe tool to automate and simplify this process.
    
    PLAYBACK
    ========
    
    Once you have a merged, de-duplicated MCH collection, you can play it back
    using:
    
    superpmi unique.mch clrjit.dll
    
    You can do this much faster by utilizing all the processors on your machine,
    and replaying in parallel, using:
    
    superpmi -p unique.mch clrjit.dll
    
    REMAINING WORK
    =============
    
    The basic of assembly diffing are there, using the "coredistools"
    package. The open source build needs to be altered to use this package
    to wire up the correct build steps.
    
    [tfs-changeset: 1623347]
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/d85eb92698be624b181ae9685f9cceb09524f75f

---
+++ src/clr/src/ToolBox/superpmi/mcs/verbsmarty.cpp
@@ -0,0 +1,96 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+#include "standardpch.h"
+#include "verbsmarty.h"
+#include "simpletimer.h"
+#include "methodcontext.h"
+#include "methodcontextiterator.h"
+
+//
+// Constructs a new verbSmarty.
+//
+// Arguments:
+//    hFile  - A handle to the output file that we are writing the Smarty Test IDs
+//
+// Assumptions:
+//    hFile refers to an open and writeable file handle.
+//
+verbSmarty::verbSmarty(HANDLE hFile)
+{
+    m_hFile=hFile;
+}
+
+//
+// Dumps the Smarty TestID to file
+//
+// Arguments:
+//    testID    - Smarty Test ID
+//
+void verbSmarty::DumpTestInfo(int testID)
+{
+    #define bufflen 4096
+    DWORD bytesWritten;
+
+    char buff[bufflen];
+    int buff_offset = 0;
+    ZeroMemory(buff, bufflen * sizeof(char));
+
+    buff_offset+=sprintf_s(&buff[buff_offset], bufflen-buff_offset, "%i\r\n", testID);
+    WriteFile(m_hFile, buff, buff_offset * sizeof(char), &bytesWritten, nullptr);
+}
+
+
+int verbSmarty::DoWork(const char *nameOfInput, const char *nameOfOutput, int indexCount, const int *indexes)
+{
+    LogVerbose("Reading from '%s' reading Smarty ID for the Mc Indexes and writing into '%s'", nameOfInput, nameOfOutput);
+
+    MethodContextIterator mci(indexCount, indexes);
+    if (!mci.Initialize(nameOfInput))
+        return -1;
+
+    int savedCount = 0;
+
+    HANDLE hFileOut = CreateFileA(nameOfOutput, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL|FILE_FLAG_SEQUENTIAL_SCAN, NULL);
+    if(hFileOut == INVALID_HANDLE_VALUE)
+    {
+        LogError("Failed to open input 1 '%s'. GetLastError()=%u", nameOfOutput, GetLastError());
+        return -1;
+    }
+
+    verbSmarty *verbList = new verbSmarty(hFileOut);
+
+    //TODO-Cleanup: look to use toc for this
+    while (mci.MoveNext())
+    {
+        MethodContext* mc = mci.Current();
+
+        int testID = mc->repGetTestID();
+        if (testID != -1)
+        {
+            //write to the file
+            verbList->DumpTestInfo(testID);
+        }
+        else
+        {
+            LogError("Smarty ID not found for '%s'", mc->cr->repProcessName());
+        }
+    }
+
+    delete verbList;
+
+    if (!CloseHandle(hFileOut))
+    {
+        LogError("2nd CloseHandle failed. GetLastError()=%u", GetLastError());
+        return -1;
+    }
+
+    LogInfo("Loaded %d, Saved %d", mci.MethodContextNumber(), savedCount);
+
+    if (!mci.Destroy())
+        return -1;
+
+    return 0;
+}
---
+++ src/clr/src/ToolBox/superpmi/superpmi-shared/spmiutil.cpp
@@ -0,0 +1,109 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+//----------------------------------------------------------
+// SPMIUtil.cpp - General utility functions
+//----------------------------------------------------------
+
+#include "standardpch.h"
+#include "logging.h"
+#include "spmiutil.h"
+
+bool breakOnDebugBreakorAV = false;
+
+void DebugBreakorAV(int val)
+{
+    if (IsDebuggerPresent())
+    {
+        if (val == 0)
+            __debugbreak();
+        if (breakOnDebugBreakorAV)
+            __debugbreak();
+    } 
+
+    int exception_code = EXCEPTIONCODE_DebugBreakorAV + val;
+    // assert((EXCEPTIONCODE_DebugBreakorAV <= exception_code) && (exception_code < EXCEPTIONCODE_DebugBreakorAV_MAX))
+    LogException(exception_code, "DebugBreak or AV Exception %d", val);
+}
+
+char* GetEnvironmentVariableWithDefaultA(const char* envVarName, const char* defaultValue)
+{
+    char* retString = nullptr;
+
+    // Figure out how much space we need to allocate
+    DWORD dwRetVal = ::GetEnvironmentVariableA(envVarName, nullptr, 0);
+    if (dwRetVal != 0)
+    {
+        retString = new char[dwRetVal];
+        dwRetVal = ::GetEnvironmentVariableA(envVarName, retString, dwRetVal);
+    }
+    else
+    {
+        if (defaultValue != nullptr)
+        {
+            dwRetVal = (DWORD)strlen(defaultValue) + 1; // add one for null terminator
+            retString = new char[dwRetVal];
+            memcpy_s(retString, dwRetVal, defaultValue, dwRetVal);
+        }
+    }
+
+    return retString;
+}
+
+WCHAR* GetEnvironmentVariableWithDefaultW(const WCHAR* envVarName, const WCHAR* defaultValue)
+{
+    WCHAR* retString = nullptr;
+
+    // Figure out how much space we need to allocate
+    DWORD dwRetVal = ::GetEnvironmentVariableW(envVarName, nullptr, 0);
+    if (dwRetVal != 0)
+    {
+        retString = new WCHAR[dwRetVal];
+        dwRetVal = ::GetEnvironmentVariableW(envVarName, retString, dwRetVal);
+    }
+    else
+    {
+        if (defaultValue != nullptr)
+        {
+            dwRetVal = (DWORD)wcslen(defaultValue) + 1; // add one for null terminator
+            retString = new WCHAR[dwRetVal];
+            memcpy_s(retString, dwRetVal * sizeof(WCHAR), defaultValue, dwRetVal * sizeof(WCHAR));
+        }
+    }
+
+    return retString;
+}
+
+#ifdef FEATURE_PAL
+// For some reason, the PAL doesn't have GetCommandLineA(). So write it.
+LPSTR GetCommandLineA()
+{
+    LPSTR pCmdLine = nullptr;
+    LPWSTR pwCmdLine = GetCommandLineW();
+
+    if (pwCmdLine != nullptr)
+    {
+        // Convert to ASCII
+
+        int n = WideCharToMultiByte(CP_ACP, 0, pwCmdLine, -1, nullptr, 0, nullptr, nullptr);
+        if (n == 0)
+        {
+            LogError("MultiByteToWideChar failed %d", GetLastError());
+            return nullptr;
+        }
+
+        pCmdLine = new char[n];
+
+        int n2 = WideCharToMultiByte(CP_ACP, 0, pwCmdLine, -1, pCmdLine, n, nullptr, nullptr);
+        if ((n2 == 0) || (n2 != n))
+        {
+            LogError("MultiByteToWideChar failed %d", GetLastError());
+            return nullptr;
+        }
+    }
+
+    return pCmdLine;
+}
+#endif // FEATURE_PAL
