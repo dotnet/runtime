d85eb92698be624b181ae9685f9cceb09524f75f
Bruce Forstall <brucefo@microsoft.com>
2016-08-19 15:56:04 -0700

    Open source SuperPMI
    OVERVIEW
    ========
    
    This directory contains the SuperPMI tool used for testing the .NET
    just-in-time (JIT) compiler.
    
    SuperPMI has two uses:
    1. Verification that a JIT code change doesn't cause any asserts.
    2. Finding test code where two JIT compilers generate different code, or verifying
    that the two compilers generate the same code.
    
    Case #1 is useful for doing quick regression checking when making a source
    code change to the JIT compiler. The process is: (a) make a JIT source code
    change, (b) run that newly built JIT through a SuperPMI run to verify no
    asserts have been introduced.
    
    Case #2 is useful for generating assembly language diffs, to help analyze the
    impact of a JIT code change.
    
    SuperPMI works in two phases: collection and playback. In the collection phase,
    the system is configured to collect SuperPMI data. Then, run any set of .NET managed
    programs. When these managed programs invoke the JIT compiler, SuperPMI gathers and
    captures all information passed between the JIT and its .NET host. In the
    playback phase, SuperPMI loads the JIT directly, and causes it to compile all
    the functions that it previously compiled, but using the collected data to
    provide answers to various questions that the JIT needs to ask. The .NET
    execution engine (EE) is not invoked at all.
    
    TOOLS
    ==========
    
    There are two native executable tools: superpmi and mcs. There is a .NET Core
    C# program that is built as part of the coreclr repo tests build called
    superpmicollect.exe.
    
    All will show a help screen if passed -?.
    
    COLLECTION
    ==========
    
    Set the following environment variables:
    
    SuperPMIShimLogPath=<full path to an empty temporary directory>
    SuperPMIShimPath=<full path to clrjit.dll, the "standalone" JIT>
    COMPlus_AltJit=*
    COMPlus_AltJitName=superpmi-shim-collector.dll
    
    (On Linux, use libclrjit.so and libsuperpmi-shim-collector.so. On Mac,
    use libclrjit.dylib and libsuperpmi-shim-collector.dylib.)
    
    Then, run some managed programs. When done running programs, un-set these variables.
    
    Now, you will have a large number of .mc files. Merge these using the mcs
    tool:
    
    mcs -merge base.mch *.mc
    
    One benefit of SuperPMI is the ability to remove duplicated compilations, so
    on replay only unique functions are compiled. Use the following to create a
    "unique" set of functions:
    
    mcs -removeDup -thin base.mch unique.mch
    
    Note that -thin is not required. However, it will delete all the compilation
    result collected during the collection phase, which makes the resulting MCH
    file smaller. Those compilation results are not required for playback.
    
    Use the superpmicollect.exe tool to automate and simplify this process.
    
    PLAYBACK
    ========
    
    Once you have a merged, de-duplicated MCH collection, you can play it back
    using:
    
    superpmi unique.mch clrjit.dll
    
    You can do this much faster by utilizing all the processors on your machine,
    and replaying in parallel, using:
    
    superpmi -p unique.mch clrjit.dll
    
    REMAINING WORK
    =============
    
    The basic of assembly diffing are there, using the "coredistools"
    package. The open source build needs to be altered to use this package
    to wire up the correct build steps.
    
    [tfs-changeset: 1623347]

---
+++ src/ToolBox/superpmi/superpmi-shared/spmiutil.h
@@ -0,0 +1,25 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+//----------------------------------------------------------
+// SPMIUtil.h - General utility functions
+//----------------------------------------------------------
+#ifndef _SPMIUtil
+#define _SPMIUtil
+
+#include "methodcontext.h"
+
+extern bool breakOnDebugBreakorAV;
+
+extern void DebugBreakorAV(int val); //Global(ish) error handler
+
+extern char* GetEnvironmentVariableWithDefaultA(const char* envVarName, const char* defaultValue = nullptr);
+
+extern WCHAR* GetEnvironmentVariableWithDefaultW(const WCHAR* envVarName, const WCHAR* defaultValue = nullptr);
+
+#ifdef FEATURE_PAL
+extern LPSTR GetCommandLineA();
+#endif // FEATURE_PAL
+
+#endif // !_SPMIUtil
---
+++ tests/src/JIT/superpmi/collect_alltests.cmd
@@ -0,0 +1,36 @@
+@echo off
+setlocal
+
+REM Do a .NET Core SuperPMI collection across all tests in the coreclr repo.
+
+REM Set the repo root.
+set _root=d:\src\coreclr
+
+REM Set the build flavor.
+set _flavor=Windows_NT.x64.Debug
+
+REM Everything else in this script is parameterized using the above two variables.
+
+if not exist %_root% echo Error: %_root% not found&goto :eof
+
+REM Where to put the resulting MCH file?
+set _mch=%_root%\bin\tests\alltests_win.mch
+
+set _testbuild=%_root%\bin\tests\%_flavor%
+if not exist %_testbuild% echo Error: %_testbuild% not found&goto :eof
+if not exist %_testbuild%\JIT\superpmi\superpmicollect\superpmicollect.exe echo Error: superpmicollect.exe not found&goto :eof
+
+set _collect_script=%_root%\tests\src\JIT\superpmi\collect_runtest.cmd
+if not exist %_collect_script% echo Error: %_collect_script% not found&goto :eof
+
+if not exist %_root%\tests\runtest.cmd echo Error: %_root%\tests\runtest.cmd not found&goto :eof
+
+if not defined CORE_ROOT echo ERROR: set CORE_ROOT before running this script&goto :eof
+if not exist %CORE_ROOT% echo Error: CORE_ROOT (%CORE_ROOT%) not found&goto :eof
+if not exist %CORE_ROOT%\coreclr.dll echo Error: coreclr.dll (%CORE_ROOT%\coreclr.dll) not found&goto :eof
+if not exist %CORE_ROOT%\corerun.exe echo Error: corerun.exe (%CORE_ROOT%\corerun.exe) not found&goto :eof
+
+REM Do the collection!
+pushd %_testbuild%
+%core_root%\corerun.exe %_testbuild%\JIT\superpmi\superpmicollect\superpmicollect.exe -mch %_mch% -run %_collect_script% %_root%\tests\runtest.cmd
+popd
