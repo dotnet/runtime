337ef1ee1c02ced6fe0baebfecdd8bd2ee4c4385
Aleksey Kliger (λgeek) <akliger@gmail.com>
2016-08-22 12:28:18 -0400

    [sre] Fix s390x build (mono/mono#3427)
    Followup for mono/mono@6be008c409153a82e80394b98c93564e2cdb2309 to fix the s390x build.
    
    Commit migrated from https://github.com/mono/mono/commit/faa06c572d54ac19f2fb79034321c46b8e30aba8

--- src/mono/mono/metadata/sre-encode.c
+++ src/mono/mono/metadata/sre-encode.c
@@ -36,6 +36,12 @@ static guint32 mono_image_typedef_or_ref (MonoDynamicImage *assembly, MonoType *
 
 #define mono_type_array_get_and_resolve(array, index, error) mono_reflection_type_get_handle ((MonoReflectionType*)mono_array_get (array, gpointer, index), error)
 
+static guint32
+mono_image_add_stream_data (MonoDynamicStream *stream, const char *data, guint32 len)
+{
+	return mono_dynstream_add_data (stream, data, len);
+}
+
 static void
 alloc_table (MonoDynamicTable *table, guint nrows)
 {
@@ -437,6 +443,52 @@ mono_dynimage_encode_locals (MonoDynamicImage *assembly, MonoReflectionILGen *il
 	return idx;
 }
 
+
+/*
+ * Copy len * nelem bytes from val to dest, swapping bytes to LE if necessary.
+ * dest may be misaligned.
+ */
+static void
+swap_with_size (char *dest, const char* val, int len, int nelem) {
+	MONO_REQ_GC_NEUTRAL_MODE;
+#if G_BYTE_ORDER != G_LITTLE_ENDIAN
+	int elem;
+
+	for (elem = 0; elem < nelem; ++elem) {
+		switch (len) {
+		case 1:
+			*dest = *val;
+			break;
+		case 2:
+			dest [0] = val [1];
+			dest [1] = val [0];
+			break;
+		case 4:
+			dest [0] = val [3];
+			dest [1] = val [2];
+			dest [2] = val [1];
+			dest [3] = val [0];
+			break;
+		case 8:
+			dest [0] = val [7];
+			dest [1] = val [6];
+			dest [2] = val [5];
+			dest [3] = val [4];
+			dest [4] = val [3];
+			dest [5] = val [2];
+			dest [6] = val [1];
+			dest [7] = val [0];
+			break;
+		default:
+			g_assert_not_reached ();
+		}
+		dest += len;
+		val += len;
+	}
+#else
+	memcpy (dest, val, len * nelem);
+#endif
+}
 
 
 guint32
