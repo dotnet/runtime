028100e370fd13177c629e7e77e2f0c5b6c2f252
Aleksey Kliger <aleksey@xamarin.com>
2016-08-22 14:07:59 -0400

    [s390x] Fix compilation of sre-save.c
    Commit migrated from https://github.com/mono/mono/commit/9aa7728ec605da5f31826a432b65c3abcfd40819

--- src/mono/mono/metadata/sre-save.c
+++ src/mono/mono/metadata/sre-save.c
@@ -101,6 +101,52 @@ find_index_in_table (MonoDynamicImage *assembly, int table_idx, int col, guint32
 	return 0;
 }
 
+/*
+ * Copy len * nelem bytes from val to dest, swapping bytes to LE if necessary.
+ * dest may be misaligned.
+ */
+static void
+swap_with_size (char *dest, const char* val, int len, int nelem) {
+	MONO_REQ_GC_NEUTRAL_MODE;
+#if G_BYTE_ORDER != G_LITTLE_ENDIAN
+	int elem;
+
+	for (elem = 0; elem < nelem; ++elem) {
+		switch (len) {
+		case 1:
+			*dest = *val;
+			break;
+		case 2:
+			dest [0] = val [1];
+			dest [1] = val [0];
+			break;
+		case 4:
+			dest [0] = val [3];
+			dest [1] = val [2];
+			dest [2] = val [1];
+			dest [3] = val [0];
+			break;
+		case 8:
+			dest [0] = val [7];
+			dest [1] = val [6];
+			dest [2] = val [5];
+			dest [3] = val [4];
+			dest [4] = val [3];
+			dest [5] = val [2];
+			dest [6] = val [1];
+			dest [7] = val [0];
+			break;
+		default:
+			g_assert_not_reached ();
+		}
+		dest += len;
+		val += len;
+	}
+#else
+	memcpy (dest, val, len * nelem);
+#endif
+}
+
 static guint32
 add_mono_string_to_blob_cached (MonoDynamicImage *assembly, MonoString *str)
 {
