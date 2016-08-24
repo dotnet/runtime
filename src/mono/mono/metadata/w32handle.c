cb32004223397fb16fbebb74d883fa45862c4edd
Ludovic Henry <ludovic@xamarin.com>
2016-08-24 15:06:26 +0200

    [w32handle] Remove unused header includes
    Commit migrated from https://github.com/mono/mono/commit/d49734aa5a7da8d4a0dffd55b9123511541b61d7

--- src/mono/mono/metadata/w32handle.c
+++ src/mono/mono/metadata/w32handle.c
@@ -15,30 +15,6 @@
 #if !defined(HOST_WIN32)
 
 #include <glib.h>
-#include <pthread.h>
-#include <errno.h>
-#include <unistd.h>
-#ifdef HAVE_SIGNAL_H
-#include <signal.h>
-#endif
-#include <string.h>
-#include <sys/types.h>
-#ifdef HAVE_SYS_SOCKET_H
-#  include <sys/socket.h>
-#endif
-#ifdef HAVE_SYS_UN_H
-#  include <sys/un.h>
-#endif
-#ifdef HAVE_SYS_MMAN_H
-#  include <sys/mman.h>
-#endif
-#ifdef HAVE_DIRENT_H
-#  include <dirent.h>
-#endif
-#include <sys/stat.h>
-#ifdef HAVE_SYS_RESOURCE_H
-#  include <sys/resource.h>
-#endif
 
 #include "w32handle.h"
 
