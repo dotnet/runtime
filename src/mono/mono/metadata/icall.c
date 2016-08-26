353068c6bc3d9ff93616bb4da15de8442799a8c6
Josh Peterson <petersonjm1@gmail.com>
2016-08-26 14:54:27 -0400

    Throw an ArgumentException when a delegate is bound to an open generic method.
    * This change was made in Unity's old version of Mono at: https://github.com/Unity-Technologies/mono/commit/mono/mono@9e27edaec9a91a75d6ebf0d21654dedcab74653d
    
    
    Commit migrated from https://github.com/mono/mono/commit/eaaf9b498f7579063442491d45cb4f4ed86d8c28

--- src/mono/mono/metadata/icall.c
+++ src/mono/mono/metadata/icall.c
@@ -6212,6 +6212,7 @@ ves_icall_System_Delegate_CreateDelegate_internal (MonoReflectionType *type, Mon
 	MonoObject *delegate;
 	gpointer func;
 	MonoMethod *method = info->method;
+	MonoMethodSignature *sig = mono_method_signature(method);
 
 	mono_class_init_checked (delegate_class, &error);
 	if (mono_error_set_pending_exception (&error))
@@ -6234,6 +6235,11 @@ ves_icall_System_Delegate_CreateDelegate_internal (MonoReflectionType *type, Mon
 				mono_error_cleanup (&error);
 			return NULL;
 		}
+	}
+
+	if (sig->generic_param_count && method->wrapper_type == MONO_WRAPPER_NONE) {
+		if (!method->is_inflated)
+			mono_raise_exception(mono_get_exception_argument("method", " Cannot bind to the target method because its signature differs from that of the delegate type"));
 	}
 
 	delegate = mono_object_new_checked (mono_object_domain (type), delegate_class, &error);
