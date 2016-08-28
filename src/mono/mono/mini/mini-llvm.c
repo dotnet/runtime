f53b69c1aa8c1ff781f66327b519963071cfed04
Alex Rønne Petersen <alexrp@xamarin.com>
2016-08-28 16:52:21 +0200

    [llvm] Fix atomic opcodes on ARM64 to emit correct fences.
    This is a workaround. It's needed for the same reason that we need the fix for
    Clang's atomic intrinsics in mono/utils/atomic.h.
    
    
    Commit migrated from https://github.com/mono/mono/commit/062ccc0d7e8505a6707ce9835243b43e4ef077b5

--- src/mono/mono/mini/mini-llvm.c
+++ src/mono/mono/mini/mini-llvm.c
@@ -5375,6 +5375,17 @@ process_bb (EmitContext *ctx, MonoBasicBlock *bb)
 			values [ins->dreg] = LLVMBuildSelect (builder, v, lhs, rhs, dname);
 			break;
 		}
+
+/*
+ * See the ARM64 comment in mono/utils/atomic.h for an explanation of why this
+ * hack is necessary (for now).
+ */
+#ifdef TARGET_ARM64
+#define ARM64_ATOMIC_FENCE_FIX mono_llvm_build_fence (builder, LLVM_BARRIER_SEQ)
+#else
+#define ARM64_ATOMIC_FENCE_FIX
+#endif
+
 		case OP_ATOMIC_EXCHANGE_I4:
 		case OP_ATOMIC_EXCHANGE_I8: {
 			LLVMValueRef args [2];
@@ -5390,7 +5401,9 @@ process_bb (EmitContext *ctx, MonoBasicBlock *bb)
 			args [0] = convert (ctx, lhs, LLVMPointerType (t, 0));
 			args [1] = convert (ctx, rhs, t);
 
+			ARM64_ATOMIC_FENCE_FIX;
 			values [ins->dreg] = mono_llvm_build_atomic_rmw (builder, LLVM_ATOMICRMW_OP_XCHG, args [0], args [1]);
+			ARM64_ATOMIC_FENCE_FIX;
 			break;
 		}
 		case OP_ATOMIC_ADD_I4:
@@ -5407,7 +5420,9 @@ process_bb (EmitContext *ctx, MonoBasicBlock *bb)
 
 			args [0] = convert (ctx, lhs, LLVMPointerType (t, 0));
 			args [1] = convert (ctx, rhs, t);
+			ARM64_ATOMIC_FENCE_FIX;
 			values [ins->dreg] = LLVMBuildAdd (builder, mono_llvm_build_atomic_rmw (builder, LLVM_ATOMICRMW_OP_ADD, args [0], args [1]), args [1], dname);
+			ARM64_ATOMIC_FENCE_FIX;
 			break;
 		}
 		case OP_ATOMIC_CAS_I4:
@@ -5425,7 +5440,9 @@ process_bb (EmitContext *ctx, MonoBasicBlock *bb)
 			args [1] = convert (ctx, values [ins->sreg3], t);
 			/* new value */
 			args [2] = convert (ctx, values [ins->sreg2], t);
+			ARM64_ATOMIC_FENCE_FIX;
 			val = mono_llvm_build_cmpxchg (builder, args [0], args [1], args [2]);
+			ARM64_ATOMIC_FENCE_FIX;
 			/* cmpxchg returns a pair */
 			values [ins->dreg] = LLVMBuildExtractValue (builder, val, 0, "");
 			break;
@@ -5466,7 +5483,9 @@ process_bb (EmitContext *ctx, MonoBasicBlock *bb)
 
 			addr = convert (ctx, addr, LLVMPointerType (t, 0));
 
+			ARM64_ATOMIC_FENCE_FIX;
 			values [ins->dreg] = emit_load_general (ctx, bb, &builder, size, addr, lhs, dname, is_volatile, barrier);
+			ARM64_ATOMIC_FENCE_FIX;
 
 			if (sext)
 				values [ins->dreg] = LLVMBuildSExt (builder, values [ins->dreg], LLVMInt32Type (), dname);
@@ -5514,7 +5533,9 @@ process_bb (EmitContext *ctx, MonoBasicBlock *bb)
 			addr = LLVMBuildGEP (builder, convert (ctx, base, LLVMPointerType (t, 0)), &index, 1, "");
 			value = convert (ctx, values [ins->sreg1], t);
 
+			ARM64_ATOMIC_FENCE_FIX;
 			emit_store_general (ctx, bb, &builder, size, value, addr, base, is_volatile, barrier);
+			ARM64_ATOMIC_FENCE_FIX;
 			break;
 		}
 		case OP_RELAXED_NOP: {
