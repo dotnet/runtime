21c6fb72ad0e926cc7849cbb0fc511ca3a9d3924
Ben Adams <thundercat@illyriad.co.uk>
2016-08-30 03:23:53 +0100

    Monitor.TryEnter asm timeout != 0 before spin (dotnet/coreclr#6951)
    * JIT_MonTryEnter_Slow timeout != 0 before spin
    * JIT_MonTryEnter_InlineGetThread timeout != 0 before spin
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/4f60a817e3ea408a176c74ec0f0ea5e9f151dd61

--- src/clr/src/vm/amd64/JitHelpers_Slow.asm
+++ src/clr/src/vm/amd64/JitHelpers_Slow.asm
@@ -1452,6 +1452,10 @@ NESTED_ENTRY JIT_MonTryEnter_Slow, _TEXT
         ret
 
     PrepareToWaitThinLock:
+        ; Return failure if timeout is zero
+        test    rsi, rsi
+        jz     TimeoutZero
+
         ; If we are on an MP system, we try spinning for a certain number of iterations
         cmp     dword ptr [g_SystemInfo + OFFSETOF__g_SystemInfo__dwNumberOfProcessors], 1
         jle     FramedLockHelper
@@ -1553,9 +1557,13 @@ endif
         ret
 
     PrepareToWait:
+        ; Return failure if timeout is zero
+        test    rsi, rsi
+        jz      TimeoutZero
+
         ; If we are on an MP system, we try spinning for a certain number of iterations
         cmp     dword ptr [g_SystemInfo + OFFSETOF__g_SystemInfo__dwNumberOfProcessors], 1
-        jle     WouldBlock
+        jle     Block
     
         ; Exponential backoff; delay by approximately 2*r10d clock cycles
         mov     eax, r10d
@@ -1570,15 +1578,11 @@ endif
         cmp     r10d, dword ptr [g_SpinConstants + OFFSETOF__g_SpinConstants__dwMaximumDuration]
         jle     RetrySyncBlock
 
-        ; We would need to block to enter the section. Return failure if
-        ; timeout is zero, else call the farmed helper to do the blocking
-        ; form of TryEnter.
-    WouldBlock:
-        test    rsi, rsi
-        jnz     Block
+        jmp     Block
 
+    TimeoutZero:
         ; Return FALSE
-        mov		byte ptr [r8], 0
+        mov     byte ptr [r8], 0
         add     rsp, MON_ENTER_STACK_SIZE
         pop     rsi
         ret
