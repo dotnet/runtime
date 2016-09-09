93820c9d5f2876670d71a2d715f2fc45ecbfe232
Eric Mellino <erme@microsoft.com>
2016-09-09 19:52:19 +0000

    Update DependencyVersions.cs

--- build_projects/shared-build-targets-utils/DependencyVersions.cs
+++ build_projects/shared-build-targets-utils/DependencyVersions.cs
@@ -7,8 +7,8 @@ namespace Microsoft.DotNet.Cli.Build
 {
     public class DependencyVersions
     {
-        public static readonly string CoreCLRVersion = "1.0.2";
-        public static readonly string JitVersion = "1.0.2";
+        public static readonly string CoreCLRVersion = "1.1.0-beta-24509-04";
+        public static readonly string JitVersion = "1.1.0-beta-24509-04";
         public static readonly string CoreCLRVersion_Latest = "1.1.0-beta-24415-01";
         public static readonly string JitVersion_Latest = "1.1.0-beta-24415-01";
     }
