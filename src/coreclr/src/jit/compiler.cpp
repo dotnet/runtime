83567175040b5f7b7b2d08aa37d6d5f2fdd66fb9
Peter Kukol <pkukol@users.noreply.github.com>
2016-09-12 22:35:54 -0600

    Clean up COMPlus_JitMemStats / MEASURE_MEM_ALLOC (dotnet/coreclr#7119)
    Clean up COMPlus_JitMemStats / MEASURE_MEM_ALLOC (all of it is off by default for non-DEBUG builds).
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/f718e6ac78184a2ac3c0d89ba10c99bd50390a32

--- src/coreclr/src/jit/compiler.cpp
+++ src/coreclr/src/jit/compiler.cpp
@@ -1101,7 +1101,9 @@ size_t genFlowNodeCnt;
 #ifdef DEBUG
 /* static */
 unsigned Compiler::s_compMethodsCount = 0; // to produce unique label names
+#endif
 
+#if MEASURE_MEM_ALLOC
 /* static */
 bool Compiler::s_dspMemStats = false;
 #endif
@@ -1367,11 +1369,7 @@ void Compiler::compShutdown()
 
 #if MEASURE_MEM_ALLOC
 
-#ifdef DEBUG
-    // Under debug, we only dump memory stats when the COMPlus_* variable is defined.
-    // Under non-debug, we don't have the COMPlus_* variable, and we always dump it.
     if (s_dspMemStats)
-#endif
     {
         fprintf(fout, "\nAll allocations:\n");
         s_aggMemStats.Print(jitstdout);
@@ -3004,7 +3002,6 @@ void Compiler::compInitOptions(CORJIT_FLAGS* jitFlags)
     opts.dspGCtbls             = false;
     opts.disAsm2               = false;
     opts.dspUnwind             = false;
-    s_dspMemStats              = false;
     opts.compLongAddress       = false;
     opts.compJitELTHookEnabled = false;
 
@@ -3096,11 +3093,6 @@ void Compiler::compInitOptions(CORJIT_FLAGS* jitFlags)
             opts.dspDiffable = true;
         }
 
-        if (JitConfig.DisplayMemStats() != 0)
-        {
-            s_dspMemStats = true;
-        }
-
         if (JitConfig.JitLongAddress() != 0)
         {
             opts.compLongAddress = true;
@@ -3183,6 +3175,10 @@ void Compiler::compInitOptions(CORJIT_FLAGS* jitFlags)
     }
     opts.compStackCheckOnRet  = (dwJitStackChecks & DWORD(STACK_CHECK_ON_RETURN)) != 0;
     opts.compStackCheckOnCall = (dwJitStackChecks & DWORD(STACK_CHECK_ON_CALL)) != 0;
+#endif
+
+#if MEASURE_MEM_ALLOC
+    s_dspMemStats = (JitConfig.DisplayMemStats() != 0);
 #endif
 
 #ifdef PROFILING_SUPPORTED
