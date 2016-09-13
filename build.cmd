0f012af53aafe2d93f51c1b95a01672d9ab9086e
Ian Hays <ianha@microsoft.com>
2016-09-12 17:21:21 -0700

    Use latest packages and fix arm64 build
    Fixes the arm64 build by adding a vcvarsall.bat call to set the path variables we need. Also updates the package versions to the latest from coreclr and corefx.

--- build.cmd
+++ build.cmd
@@ -3,5 +3,22 @@
 REM Copyright (c) .NET Foundation and contributors. All rights reserved.
 REM Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+:Arg_Loop
+:: Since the native build requires some configuration information before msbuild is called, we have to do some manual args parsing
+if [%1] == [] goto :InvokeBuild
+if /i [%1] == [-TargetArch] if /i [%2] == [arm64] ( 
+    if not defined VS140COMNTOOLS (
+        echo Error: Visual Studio 2015 required  
+        echo        Please see https://github.com/dotnet/corefx/blob/master/Documentation/project-docs/developer-guide.md for build instructions.
+        exit /b 1
+    )
+    call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x86_amd64
+    goto :InvokeBuild
+)
+
+shift
+goto :Arg_Loop
+
+:InvokeBuild
 powershell -NoProfile -NoLogo -Command "%~dp0build_projects\dotnet-host-build\build.ps1 %*; exit $LastExitCode;"
 if %errorlevel% neq 0 exit /b %errorlevel%
