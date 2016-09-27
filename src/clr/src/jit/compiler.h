cb1a35da9af3d87e4a6ffa91c925d8b85b55b09d
Carol Eidt <carol.eidt@microsoft.com>
2016-09-27 15:19:18 -0700

    Enable optimization of structs
    Remove many of the restrictions on structs that were added to preserve behavior of the old IR form.
    Change the init block representation to not require changing the parent when a copy block is changed to an init.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/c4b661d90ed2921d37b95c8f2a2d6922b2771f92

--- src/clr/src/jit/compiler.h
+++ src/clr/src/jit/compiler.h
@@ -2323,7 +2323,8 @@ public:
         DNER_VMNeedsStackAddr,
         DNER_LiveInOutOfHandler,
         DNER_LiveAcrossUnmanagedCall,
-        DNER_BlockOp, // Is read or written via a block operation that explicitly takes the address.
+        DNER_BlockOp,     // Is read or written via a block operation that explicitly takes the address.
+        DNER_IsStructArg, // Is a struct passed as an argument in a way that requires a stack location.
 #ifdef JIT32_GCENCODER
         DNER_PinningRef,
 #endif
@@ -4561,6 +4562,7 @@ private:
     GenTreePtr fgMorphGetStructAddr(GenTreePtr* pTree, CORINFO_CLASS_HANDLE clsHnd, bool isRValue = false);
     GenTreePtr fgMorphBlkNode(GenTreePtr tree, bool isDest);
     GenTreePtr fgMorphBlockOperand(GenTreePtr tree, var_types asgType, unsigned blockWidth, bool isDest);
+    void fgMorphUnsafeBlk(GenTreeObj* obj);
     GenTreePtr fgMorphCopyBlock(GenTreePtr tree);
     GenTreePtr fgMorphForRegisterFP(GenTreePtr tree);
     GenTreePtr fgMorphSmpOp(GenTreePtr tree, MorphAddrContext* mac = nullptr);
