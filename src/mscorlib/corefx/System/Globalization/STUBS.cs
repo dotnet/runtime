a8468d2c9f70d211d8d05c66eaceb49adb699439
Tarekm Mahmoud Sayed <tarekms@microsoft.com>
2016-09-29 16:48:47 -0700

    Enable added Calendar APIS on Linux

--- src/mscorlib/corefx/System/Globalization/STUBS.cs
+++ src/mscorlib/corefx/System/Globalization/STUBS.cs
@@ -1,24 +1,5 @@
 namespace System.Globalization
 {
-    public abstract partial class Calendar : System.ICloneable
-    {
-        [System.Runtime.InteropServices.ComVisibleAttribute(false)]
-        public virtual int GetLeapMonth(int year) { throw new NotImplementedException(); }
-    }
-
-    [System.Runtime.InteropServices.ComVisible(true)]
-    public enum CalendarAlgorithmType
-    {
-        Unknown = 0,            // This is the default value to return in the Calendar base class.
-        SolarCalendar = 1,      // Solar-base calendar, such as GregorianCalendar, jaoaneseCalendar, JulianCalendar, etc.
-                                // Solar calendars are based on the solar year and seasons.
-        LunarCalendar = 2,      // Lunar-based calendar, such as Hijri and UmAlQuraCalendar.
-                                // Lunar calendars are based on the path of the moon.  The seasons are not accurately represented.
-        LunisolarCalendar = 3   // Lunisolar-based calendar which use leap month rule, such as HebrewCalendar and Asian Lunisolar calendars.
-                                // Lunisolar calendars are based on the cycle of the moon, but consider the seasons as a secondary consideration,
-                                // so they align with the seasons as well as lunar events.
-    }
-
     public partial class CompareInfo : System.Runtime.Serialization.IDeserializationCallback
     {
         public int LCID { get { throw new NotImplementedException(); } }
