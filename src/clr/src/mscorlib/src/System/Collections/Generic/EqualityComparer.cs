28ad6eae93a01c9c57941217dac729c03ca135f5
James Ko <jamesqko@gmail.com>
2016-09-30 00:47:41 -0400

    Fix change to GenericEqualityComparer.Equals from dotnet/coreclr#5804 (dotnet/coreclr#7419)
    Commit migrated from https://github.com/dotnet/coreclr/commit/37d0ac835c757266eb5224110f4914af987302dd

--- src/clr/src/mscorlib/src/System/Collections/Generic/EqualityComparer.cs
+++ src/clr/src/mscorlib/src/System/Collections/Generic/EqualityComparer.cs
@@ -177,11 +177,13 @@ namespace System.Collections.Generic
         }
 
         // Equals method for the comparer itself.
+        // If in the future this type is made sealed, change the is check to obj != null && GetType() == obj.GetType().
         public override bool Equals(Object obj) =>
-            obj != null && GetType() == obj.GetType();
+            obj is GenericEqualityComparer<T>;
 
+        // If in the future this type is made sealed, change typeof(...) to GetType().
         public override int GetHashCode() =>
-            GetType().GetHashCode();
+            typeof(GenericEqualityComparer<T>).GetHashCode();
     }
 
     [Serializable]
