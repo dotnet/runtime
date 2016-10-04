51c07a6a452c51fd17238c39226bc29b28de0a6a
Rodrigo Kumpera <kumpera@gmail.com>
2016-10-04 15:53:36 -0700

    [runtime] Don't do logging from a signal handler. It's not signal safe.
    I found this while trying to MONO_LOG_LEVEL=debug an process way bug and the removed log statements
    would hang mono in a matter of seconds.
    
    
    Commit migrated from https://github.com/mono/mono/commit/d60084ccdc65b56b1022d62035c07c0dc57faaa4

--- src/mono/mono/io-layer/processes.c
+++ src/mono/mono/io-layer/processes.c
@@ -2685,8 +2685,6 @@ MONO_SIGNAL_HANDLER_FUNC (static, mono_sigchld_signal_handler, (int _dummy, sigi
 	int pid;
 	struct MonoProcess *p;
 
-	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "SIG CHILD handler for pid: %i\n", info->si_pid);
-
 	do {
 		do {
 			pid = waitpid (-1, &status, WNOHANG);
@@ -2694,8 +2692,6 @@ MONO_SIGNAL_HANDLER_FUNC (static, mono_sigchld_signal_handler, (int _dummy, sigi
 
 		if (pid <= 0)
 			break;
-
-		MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "child ended: %i", pid);
 
 		/*
 		 * This can run concurrently with the code in the rest of this module.
@@ -2714,8 +2710,6 @@ MONO_SIGNAL_HANDLER_FUNC (static, mono_sigchld_signal_handler, (int _dummy, sigi
 			p->freeable = TRUE;
 		}
 	} while (1);
-
-	MONO_TRACE (G_LOG_LEVEL_DEBUG, MONO_TRACE_IO_LAYER, "SIG CHILD handler: done looping.");
 }
 
 #endif
