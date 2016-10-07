1079a6e76f284c48247e6e62809832411dec4f8a
William Godbe <williamgodbe@Wills-MacBook-Pro-3.local>
2016-10-07 15:23:26 -0700

    Use new version of CLI so we can get working CLI for OpenSUSE
    Commit migrated from https://github.com/dotnet/core-setup/commit/b94b04ba4a27a2220967b5ebeea26f4984dc8abe

--- src/installer/build_projects/dotnet-host-build/build.sh
+++ src/installer/build_projects/dotnet-host-build/build.sh
@@ -94,7 +94,7 @@ done < "$REPOROOT/branchinfo.txt"
 [ -d "$DOTNET_INSTALL_DIR" ] || mkdir -p $DOTNET_INSTALL_DIR
 
 DOTNET_INSTALL_SCRIPT_URL="https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.sh"
-curl -sSL "$DOTNET_INSTALL_SCRIPT_URL" | bash /dev/stdin --channel Sdk --version 1.0.0-preview3-003223 --verbose
+curl -sSL "$DOTNET_INSTALL_SCRIPT_URL" | bash /dev/stdin --version 1.0.0-preview3-003223-3 --verbose
 
 # Put stage 0 on the PATH (for this shell only)
 PATH="$DOTNET_INSTALL_DIR:$PATH"
