d2f8bbb786c54ca28c3acbcfc42dd092d6dab736
Ivan Baravy <i.baravy@samsung.com>
2016-10-25 20:32:15 +0400

    Add more lldbplugin/libsosplugin tests (dotnet/coreclr#5606)
    * Add more libsosplugin tests, update framework
    
    + Introduce test.assert* functions in testutils.py
    + Print test results as a table with failed assertions highlighted
    + PEP8!
    
    * Add more lldbplugin/libsosplugin tests
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/f6b0056c819b07ae053d3112299ade0990144d00

---
+++ src/clr/src/ToolBox/SOS/tests/t_cmd_bpmd_module_function_iloffset.py
@@ -0,0 +1,43 @@
+import lldb
+import re
+import testutils as test
+
+# bpmd <module name> <managed function name> [<il offset>]
+
+
+def runScenario(assembly, debugger, target):
+    process = target.GetProcess()
+    res = lldb.SBCommandReturnObject()
+    ci = debugger.GetCommandInterpreter()
+
+    # Run debugger, wait until libcoreclr is loaded,
+    # set breakpoint at Test.Main and stop there
+    test.stop_in_main(debugger, assembly)
+
+    ci.HandleCommand("bpmd " + assembly + " Test.UnlikelyInlined 66", res)
+    out_msg = res.GetOutput()
+    err_msg = res.GetError()
+    print(out_msg)
+    print(err_msg)
+    # Interpreter must have this command and able to run it
+    test.assertTrue(res.Succeeded())
+
+    # Output is not empty
+    # Should be at least 'Adding pending breakpoints...'
+    test.assertTrue(len(out_msg) > 0)
+
+    # Error message is empty
+    test.assertTrue(len(err_msg) == 0)
+
+    process.Continue()
+    # Process must be stopped at UnlinkelyInlined
+    test.assertEqual(process.GetState(), lldb.eStateStopped)
+
+    # The reason of this stop must be a breakpoint
+    test.assertEqual(process.GetSelectedThread().GetStopReason(),
+                     lldb.eStopReasonBreakpoint)
+
+    #
+
+    # Delete all breakpoints, continue current process and checks its exit code
+    test.exit_lldb(debugger, assembly)
