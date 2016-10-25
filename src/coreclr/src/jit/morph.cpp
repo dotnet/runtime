32f09268f9afd607263d9f75c83ebd4aaa994640
Sergey Andreenko <seandree@microsoft.com>
2016-10-25 14:54:52 -0700

    fix VSO 278365
    don't convert (int) ((double)Round(double))  to  (int)Round(double) on
    RyuJit/x86
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/6413f4d162bc7e8c2f3911e1d5d0892367195b92

--- src/coreclr/src/jit/morph.cpp
+++ src/coreclr/src/jit/morph.cpp
@@ -204,6 +204,9 @@ GenTreePtr Compiler::fgMorphCast(GenTreePtr tree)
                 {
                     case TYP_INT:
 #ifdef _TARGET_X86_ // there is no rounding convert to integer instruction on ARM or x64 so skip this
+#ifdef LEGACY_BACKEND
+                        // the RyuJIT backend does not use the x87 FPU and therefore
+                        // does not support folding the cast conv.i4(round.d(d))
                         if ((oper->gtOper == GT_INTRINSIC) &&
                             (oper->gtIntrinsic.gtIntrinsicId == CORINFO_INTRINSIC_Round))
                         {
@@ -212,7 +215,9 @@ GenTreePtr Compiler::fgMorphCast(GenTreePtr tree)
                             return fgMorphTree(oper);
                         }
                         // if SSE2 is not enabled, we need the helper
-                        else if (!opts.compCanUseSSE2)
+                        else
+#endif // LEGACY_BACKEND
+                            if (!opts.compCanUseSSE2)
                         {
                             return fgMorphCastIntoHelper(tree, CORINFO_HELP_DBL2INT, oper);
                         }
