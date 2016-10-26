431fd753ff0396e7b42dfeb6aac3d9964e13852d
Gaurav Khanna <gkhanna@microsoft.com>
2016-10-26 15:48:36 -0700

    Enable WinArm32 CI support

--- build.cmd
+++ build.cmd
@@ -1,4 +1,5 @@
 @echo off
+setlocal EnableDelayedExpansion
 
 REM Copyright (c) .NET Foundation and contributors. All rights reserved.
 REM Licensed under the MIT license. See LICENSE file in the project root for full license information.
@@ -6,13 +7,23 @@ REM Licensed under the MIT license. See LICENSE file in the project root for ful
 :Arg_Loop
 :: Since the native build requires some configuration information before msbuild is called, we have to do some manual args parsing
 if [%1] == [] goto :InvokeBuild
-if /i [%1] == [-TargetArch] if /i [%2] == [arm64] ( 
+if /i [%1] == [-TargetArch]  ( 
+    
+    if /i [%2] == [arm64] (
+        set __BuildArch=x86_amd64
+    )
+
+    if /i [%2] == [arm] (
+        set __BuildArch=x86_arm
+    )
+
     if not defined VS140COMNTOOLS (
         echo Error: Visual Studio 2015 required  
         echo        Please see https://github.com/dotnet/corefx/blob/master/Documentation/project-docs/developer-guide.md for build instructions.
         exit /b 1
     )
-    call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" x86_amd64
+
+    call "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" !__BuildArch!
     goto :InvokeBuild
 )
 
