c1c3ef03b754ca64f25b9f5a3007e4836268b168
Stephen Toub <stoub@microsoft.com>
2016-11-20 17:25:47 -0500

    Improve AsyncLocal throughput and memory usage
    ExecutionContext stores an immutable dictionary used to hold the values of all AsyncLocal instances associated with that context.  Reading an AsyncLocal.Value looks up a value in the dictionary, keyed by the AsyncLocal instance.  Writing an AsyncLocal.Value creates a new dictionary with the contents of the old one and then sets the new pair into it. For small numbers of AsyncLocal instances, this adds a lot of overhead.
    
    This commit adds a new internal IAsyncLocalValueMap interface that's used instead of Dictionary.  It then provides several concrete implementations of that interface, for 1 pair, 2 pairs, 3 pairs, up to 16 pairs, and then up to any number of pairs.  The first three implementations are optimized for those sizes, while the one for up to 16 uses an array, and the one for beyond that is just a Dictionary.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/29b13d32fc84d040e0771bd391a4d99503a0d7bc

--- src/clr/src/mscorlib/src/System/Threading/AsyncLocal.cs
+++ src/clr/src/mscorlib/src/System/Threading/AsyncLocal.cs
@@ -113,4 +113,242 @@ namespace System.Threading
             ThreadContextChanged = contextChanged;
         }
     }
+
+    //
+    // Interface used to store an IAsyncLocal => object mapping in ExecutionContext.
+    // Implementations are specialized based on the number of elements in the immutable
+    // map in order to minimize memory consumption and look-up times.
+    //
+    interface IAsyncLocalValueMap
+    {
+        bool TryGetValue(IAsyncLocal key, out object value);
+        IAsyncLocalValueMap Set(IAsyncLocal key, object value);
+    }
+
+    //
+    // Utility functions for getting/creating instances of IAsyncLocalValueMap
+    //
+    internal static class AsyncLocalValueMap
+    {
+        public static IAsyncLocalValueMap Empty { get; } = new EmptyAsyncLocalValueMap();
+
+        public static IAsyncLocalValueMap Create(IAsyncLocal key, object value) => new OneElementAsyncLocalValueMap(key, value);
+
+        // Instance without any key/value pairs.  Used as a singleton/
+        private sealed class EmptyAsyncLocalValueMap : IAsyncLocalValueMap
+        {
+            public IAsyncLocalValueMap Set(IAsyncLocal key, object value) => new OneElementAsyncLocalValueMap(key, value);
+
+            public bool TryGetValue(IAsyncLocal key, out object value)
+            {
+                value = null;
+                return false;
+            }
+        }
+
+        // Instance with one key/value pair.
+        private sealed class OneElementAsyncLocalValueMap : IAsyncLocalValueMap
+        {
+            private readonly IAsyncLocal _key1;
+            private readonly object _value1;
+
+            public OneElementAsyncLocalValueMap(IAsyncLocal key, object value)
+            {
+                _key1 = key; _value1 = value;
+            }
+
+            public IAsyncLocalValueMap Set(IAsyncLocal key, object value)
+            {
+                return ReferenceEquals(key, _key1) ?
+                    new OneElementAsyncLocalValueMap(key, value) :
+                    (IAsyncLocalValueMap)new TwoElementAsyncLocalValueMap(_key1, _value1, key, value);
+            }
+
+            public bool TryGetValue(IAsyncLocal key, out object value)
+            {
+                if (ReferenceEquals(key, _key1))
+                {
+                    value = _value1;
+                    return true;
+                }
+                else
+                {
+                    value = null;
+                    return false;
+                }
+            }
+        }
+
+        // Instance with two key/value pairs.
+        private sealed class TwoElementAsyncLocalValueMap : IAsyncLocalValueMap
+        {
+            private readonly IAsyncLocal _key1, _key2;
+            private readonly object _value1, _value2;
+
+            public TwoElementAsyncLocalValueMap(IAsyncLocal key1, object value1, IAsyncLocal key2, object value2)
+            {
+                _key1 = key1; _value1 = value1;
+                _key2 = key2; _value2 = value2;
+            }
+
+            public IAsyncLocalValueMap Set(IAsyncLocal key, object value)
+            {
+                return
+                    ReferenceEquals(key, _key1) ? new TwoElementAsyncLocalValueMap(key, value, _key2, _value2) :
+                    ReferenceEquals(key, _key2) ? new TwoElementAsyncLocalValueMap(_key1, _value1, key, value) :
+                    (IAsyncLocalValueMap)new ThreeElementAsyncLocalValueMap(_key1, _value1, _key2, _value2, key, value);
+            }
+
+            public bool TryGetValue(IAsyncLocal key, out object value)
+            {
+                if (ReferenceEquals(key, _key1))
+                {
+                    value = _value1;
+                    return true;
+                }
+                else if (ReferenceEquals(key, _key2))
+                {
+                    value = _value2;
+                    return true;
+                }
+                else
+                {
+                    value = null;
+                    return false;
+                }
+            }
+        }
+
+        // Instance with three key/value pairs.
+        private sealed class ThreeElementAsyncLocalValueMap : IAsyncLocalValueMap
+        {
+            private readonly IAsyncLocal _key1, _key2, _key3;
+            private readonly object _value1, _value2, _value3;
+
+            public ThreeElementAsyncLocalValueMap(IAsyncLocal key1, object value1, IAsyncLocal key2, object value2, IAsyncLocal key3, object value3)
+            {
+                _key1 = key1; _value1 = value1;
+                _key2 = key2; _value2 = value2;
+                _key3 = key3; _value3 = value3;
+            }
+
+            public IAsyncLocalValueMap Set(IAsyncLocal key, object value)
+            {
+                if (ReferenceEquals(key, _key1)) return new ThreeElementAsyncLocalValueMap(key, value, _key2, _value2, _key3, _value3);
+                if (ReferenceEquals(key, _key2)) return new ThreeElementAsyncLocalValueMap(_key1, _value1, key, value, _key3, _value3);
+                if (ReferenceEquals(key, _key3)) return new ThreeElementAsyncLocalValueMap(_key1, _value1, _key2, _value2, key, value);
+
+                var multi = new MultiElementAsyncLocalValueMap(4);
+                multi.UnsafeStore(0, _key1, _value1);
+                multi.UnsafeStore(1, _key2, _value2);
+                multi.UnsafeStore(2, _key3, _value3);
+                multi.UnsafeStore(3, key, value);
+                return multi;
+            }
+
+            public bool TryGetValue(IAsyncLocal key, out object value)
+            {
+                if (ReferenceEquals(key, _key1))
+                {
+                    value = _value1;
+                    return true;
+                }
+                else if (ReferenceEquals(key, _key2))
+                {
+                    value = _value2;
+                    return true;
+                }
+                else if (ReferenceEquals(key, _key3))
+                {
+                    value = _value3;
+                    return true;
+                }
+                else
+                {
+                    value = null;
+                    return false;
+                }
+            }
+        }
+
+        // Instance with up to 16 key/value pairs.
+        private sealed class MultiElementAsyncLocalValueMap : IAsyncLocalValueMap
+        {
+            private const int MaxMultiElements = 16;
+            private readonly KeyValuePair<IAsyncLocal, object>[] _keyValues;
+
+            internal MultiElementAsyncLocalValueMap(int count)
+            {
+                Debug.Assert(count <= MaxMultiElements);
+                _keyValues = new KeyValuePair<IAsyncLocal, object>[count];
+            }
+
+            internal void UnsafeStore(int index, IAsyncLocal key, object value)
+            {
+                Debug.Assert(index < _keyValues.Length);
+                _keyValues[index] = new KeyValuePair<IAsyncLocal, object>(key, value);
+            }
+
+            public IAsyncLocalValueMap Set(IAsyncLocal key, object value)
+            {
+                for (int i = 0; i < _keyValues.Length; i++)
+                {
+                    if (ReferenceEquals(key, _keyValues[i].Key))
+                    {
+                        var multi = new MultiElementAsyncLocalValueMap(_keyValues.Length);
+                        Array.Copy(_keyValues, 0, multi._keyValues, 0, _keyValues.Length);
+                        multi._keyValues[i] = new KeyValuePair<IAsyncLocal, object>(key, value);
+                        return multi;
+                    }
+                }
+
+                if (_keyValues.Length < MaxMultiElements)
+                {
+                    var multi = new MultiElementAsyncLocalValueMap(_keyValues.Length + 1);
+                    Array.Copy(_keyValues, 0, multi._keyValues, 0, _keyValues.Length);
+                    multi._keyValues[_keyValues.Length] = new KeyValuePair<IAsyncLocal, object>(key, value);
+                    return multi;
+                }
+
+                var many = new ManyElementAsyncLocalValueMap(MaxMultiElements + 1);
+                foreach (KeyValuePair<IAsyncLocal, object> pair in _keyValues)
+                {
+                    many[pair.Key] = pair.Value;
+                }
+                many[key] = value;
+                return many;
+            }
+
+            public bool TryGetValue(IAsyncLocal key, out object value)
+            {
+                foreach (KeyValuePair<IAsyncLocal, object> pair in _keyValues)
+                {
+                    if (ReferenceEquals(key, pair.Key))
+                    {
+                        value = pair.Value;
+                        return true;
+                    }
+                }
+                value = null;
+                return false;
+            }
+        }
+
+        // Instance with any number of key/value pairs.
+        private sealed class ManyElementAsyncLocalValueMap : Dictionary<IAsyncLocal, object>, IAsyncLocalValueMap
+        {
+            public ManyElementAsyncLocalValueMap(int capacity) : base(capacity) { }
+
+            public IAsyncLocalValueMap Set(IAsyncLocal key, object value)
+            {
+                var map = new ManyElementAsyncLocalValueMap(Count);
+                foreach (KeyValuePair<IAsyncLocal,object> pair in this)
+                {
+                    map[pair.Key] = pair.Value;
+                }
+                map[key] = value;
+                return map;
+            }
+        }
+    }
 }
