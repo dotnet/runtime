2c4543601b0a0a67c216252f928da11e73847d1b
Bruce Forstall <brucefo@microsoft.com>
2016-11-22 16:42:57 -0800

    Make RyuJIT/x86 the default x86 JIT
    JIT32 becomes compatjit.dll and RyuJIT LEGACY_BACKEND becomes legacyjit.dll
    (and is an altjit).
    
    If JIT32 is not being built, then RyuJIT LEGACY_BACKEND becomes compatjit.dll
    and is a normal jit (not an altjit).
    
    Both clrjit.dll and compatjit.dll are added to the JIT NuGet package.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/aad84026d7c5dc10b877bec263f4456a31584772

--- src/coreclr/tests/x86/ryujit_x86_testenv.cmd
+++ src/coreclr/tests/x86/ryujit_x86_testenv.cmd
@@ -1,51 +1,5 @@
-@REM -------------------------------------------------------------------------
-@REM 
-@REM  This script provides x86 Ryujit test environment settings
+@REM This is no longer needed, except the CI system, during the conversion of RyuJIT/x86
+@REM to the default, still looks for it for the old "ryujit" tasks. Leave it here, and the
+@REM "ryujit" tasks will just run the default JIT (now RyuJIT).
 @REM
-@REM -------------------------------------------------------------------------
-
-set COMPLUS_AltJit=*
-set COMPLUS_AltJitName=protojit.dll
-set COMPLUS_NoGuiOnAssert=1
-set COMPLUS_AltJitAssertOnNYI=1
-
-@REM -------------------------------------------------------------------------
-@REM A JitFuncInfoLogFile is a per-function record of which functions were
-@REM compiled, and what asserts and NYI were hit.
-@REM
-@REM If you wish to collect FuncInfo, choose one of the following collection
-@REM methods to use, by uncommenting the appropriate option.
-
-@REM Option 1: collect a single FuncInfoLogFile for all tests.
-@REM TO DO: set a single, fully-qualified pathname here.
-@REM ==== Uncomment below
-@REM set COMPLUS_JitFuncInfoLogFile=%TEMP%\JitFuncInfoLogFile.txt
-@REM ==== Uncomment above
-
-@REM Option #2: collect one FuncInfoLogFile per test, and put it in
-@REM the same directory as the test. Note that each tests lives in
-@REM its own directory, and the current directory is set to the unique
-@REM test directory before this script is invoked.
-@REM ==== Uncomment below
-@REM set __TestDir=%CD%
-@REM if %__TestDir:~-1%==\ set __TestDir=%__TestDir:~0,-1%
-@REM set COMPLUS_JitFuncInfoLogFile=%__TestDir%\FuncInfo.txt
-@REM ==== Uncomment above
-
-@REM Option #3: collect one FuncInfoLogFile per test, and put all of
-@REM them in a separate directory tree rooted at %__FuncInfoRootDir%.
-@REM If that is set globally already, then use it. Otherwise, use a
-@REM default set here. The directory tree will mirror the test binary tree.
-@REM Note that the current directory is set to the unique test directory
-@REM before this script is invoked.
-@REM ==== Uncomment below
-@REM if not defined __FuncInfoRootDir set __FuncInfoRootDir=c:\FuncInfos
-@REM set __TestDir=%CD%
-@REM if %__TestDir:~-1%==\ set __TestDir=%__TestDir:~0,-1%
-@REM if %__TestDir:~1,1%==: set __TestDir=%__TestDir:~3%
-@REM set __FuncInfoDir=%__FuncInfoRootDir%\%__TestDir%
-@REM if not exist %__FuncInfoDir% mkdir %__FuncInfoDir%
-@REM set COMPLUS_JitFuncInfoLogFile=%__FuncInfoDir%\FuncInfo.txt
-@REM ==== Uncomment above
-
-@REM -------------------------------------------------------------------------
+@REM After the .NET CI is updated, this can be deleted.
