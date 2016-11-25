cb25e09f555807146c7eec597872a0200d766bc3
Stephen Toub <stoub@microsoft.com>
2016-11-24 23:39:15 -0500

    Make GetCurrentProcessorNumber an FCall
    Commit migrated from https://github.com/dotnet/coreclr/commit/9a7bddb80ae678b42c694f0ff55041284a7069b8

--- src/clr/src/mscorlib/corefx/System/Buffers/TlsOverPerCoreLockedStacksArrayPool.Windows.cs
+++ src/clr/src/mscorlib/corefx/System/Buffers/TlsOverPerCoreLockedStacksArrayPool.Windows.cs
@@ -14,7 +14,7 @@ namespace System.Buffers
         private static int ExecutionId
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get { return GetCurrentProcessorNumber(); }
+            get { return CurrentProcessorNumber; }
         }
     }
 }
