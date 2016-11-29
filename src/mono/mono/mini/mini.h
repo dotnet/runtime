6f70cb90c3903e88c60b03c4a13ede13400ccf5c
Zoltan Varga <vargaz@gmail.com>
2016-11-28 20:58:45 -0500

    [aot] Bump aot file version after mono/mono@c690636c01df73badb1e24c146da8492dbeabade.
    Commit migrated from https://github.com/mono/mono/commit/4f738f8b32901425b35dec2e2b553811e7d70ea2

--- src/mono/mono/mini/mini.h
+++ src/mono/mono/mini/mini.h
@@ -125,7 +125,7 @@
 #endif
 
 /* Version number of the AOT file format */
-#define MONO_AOT_FILE_VERSION 137
+#define MONO_AOT_FILE_VERSION 138
 
 //TODO: This is x86/amd64 specific.
 #define mono_simd_shuffle_mask(a,b,c,d) ((a) | ((b) << 2) | ((c) << 4) | ((d) << 6))
