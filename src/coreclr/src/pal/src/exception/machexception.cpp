64a774ac6c32386b26bf2295b3c00cbbf6f47a98
John Chen <jochen@microsoft.com>
2016-11-29 19:50:09 -0800

    Capture YMM registers on OSX (dotnet/coreclr#8365)
    Also fixed checking of context flags on OSX (issue dotnet/coreclr#8116)
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/85c0a5641d60aabe028a9e4b4ef07a7b58a2181b

--- src/coreclr/src/pal/src/exception/machexception.cpp
+++ src/coreclr/src/pal/src/exception/machexception.cpp
@@ -1518,6 +1518,7 @@ ActivationHandler(CONTEXT* context)
 
 extern "C" void ActivationHandlerWrapper();
 extern "C" int ActivationHandlerReturnOffset;
+extern "C" unsigned int XmmYmmStateSupport();
 
 /*++
 Function :
@@ -1581,6 +1582,12 @@ InjectActivationInternal(CPalThread* pThread)
                 // after the activation function returns.
                 CONTEXT *pContext = (CONTEXT *)contextAddress;
                 pContext->ContextFlags = CONTEXT_FULL | CONTEXT_SEGMENTS;
+#ifdef XSTATE_SUPPORTED
+                if (XmmYmmStateSupport() == 1)
+                {
+                    pContext->ContextFlags |= CONTEXT_XSTATE;
+                }
+#endif
                 MachRet = CONTEXT_GetThreadContextFromPort(threadPort, pContext);
                 _ASSERT_MSG(MachRet == KERN_SUCCESS, "CONTEXT_GetThreadContextFromPort\n");
 
