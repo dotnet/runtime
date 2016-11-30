82c43831c84cfe1b37080a5cede4636c2548c000
Jonghyun Park <parjong@gmail.com>
2016-12-01 05:09:36 +0900

    Disable EHWatsonBucketTracker for non-window platforms (dotnet/coreclr#8373)
    Commit migrated from https://github.com/dotnet/coreclr/commit/883f856ee614f48ab698c627b893c055cd1fb585

--- src/clr/src/vm/exinfo.h
+++ src/clr/src/vm/exinfo.h
@@ -79,6 +79,7 @@ public:
     //
     void* m_StackAddress; // A pseudo or real stack location for this record.
 
+#ifndef FEATURE_PAL
 private:
     EHWatsonBucketTracker m_WatsonBucketTracker;
 public:
@@ -87,6 +88,7 @@ public:
         LIMITED_METHOD_CONTRACT;
         return PTR_EHWatsonBucketTracker(PTR_HOST_MEMBER_TADDR(ExInfo, this, m_WatsonBucketTracker));
     }
+#endif
 
 #ifdef FEATURE_CORRUPTING_EXCEPTIONS
 private:
