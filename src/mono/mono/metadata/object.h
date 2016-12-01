03170bb7d17b69785561bacf9a3af0358714ef76
Mike Voorhees <mrvoorhe@users.noreply.github.com>
2016-12-01 14:53:44 -0500

    Add embedding API to expose empty string instance (mono/mono#4067)
    * Reuse a single empty string instance
    
    * Undo string empty change to mono_string_new_size_checked and add new embedding api for string empty instead
    
    * Add & use mono_string_empty_checked to fix build error on Linux
    
    * Revert "Add & use mono_string_empty_checked to fix build error on Linux"
    
    This reverts commit mono/mono@47cafae6573115333fa04776c21e5ddc49fe4b40.
    
    * Remove MONO_RT_EXTERNAL_ONLY to fix build error on linux
    
    
    Commit migrated from https://github.com/mono/mono/commit/b6d1320f3ea61422db4cf379abc3f6a93ddadeeb

--- src/mono/mono/metadata/object.h
+++ src/mono/mono/metadata/object.h
@@ -112,6 +112,13 @@ mono_array_addr_with_size   (MonoArray *array, int size, uintptr_t idx);
 MONO_API uintptr_t
 mono_array_length           (MonoArray *array);
 
+MONO_API MonoString*
+mono_string_empty	      (MonoDomain *domain);
+
+MONO_RT_EXTERNAL_ONLY
+MONO_API MonoString*
+mono_string_empty_wrapper   ();
+
 MONO_RT_EXTERNAL_ONLY
 MONO_API MonoString*
 mono_string_new_utf16	    (MonoDomain *domain, const mono_unichar2 *text, int32_t len);
