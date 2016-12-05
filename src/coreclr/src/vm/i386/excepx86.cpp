551f84a56a0d2e19aa534c1f9dcec392b49f51f9
Jonghyun Park <parjong@gmail.com>
2016-12-06 05:06:49 +0900

    [x86/Linux] Fix dangling CLR_ImpersonateLoggedOnUser reference (dotnet/coreclr#8435)
    src/vm/securityprincipal.cpp is not included in x86/Linux build, and
    thus all the reference to the functions in it will be dangling. (i.e.
    COMPrincipal::CLR_ImpersonateLoggedOnUser).
    
    This commit hides COMPrincipal for non-Windows platforms, and marks
    COMPlusThrowCallbackHelper as NYI.
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/46483e11a314a167eecc557ef3f246dcc95bee99

--- src/coreclr/src/vm/i386/excepx86.cpp
+++ src/coreclr/src/vm/i386/excepx86.cpp
@@ -2235,6 +2235,7 @@ int COMPlusThrowCallbackHelper(IJitManager *pJitManager,
     int iFilt = 0;
     BOOL impersonating = FALSE;
 
+#ifndef FEATURE_PAL
     EX_TRY
     {
         GCPROTECT_BEGIN (throwable);
@@ -2285,6 +2286,10 @@ int COMPlusThrowCallbackHelper(IJitManager *pJitManager,
     EX_END_CATCH(SwallowAllExceptions)
 
     return iFilt;
+#else   // FEATURE_PAL
+    PORTABILITY_ASSERT("COMPlusThrowCallbackHelper");
+    return EXCEPTION_CONTINUE_SEARCH;
+#endif  // FEATURE_PAL
 }
 
 //******************************************************************************
