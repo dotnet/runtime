0d96e71f256296ad371b64d6ebc65767247ef35b
SaeHie Park <saehie.park@gmail.com>
2016-12-09 18:29:16 +0900

    [x86/Linux] Fix getcpuid calling convention (dotnet/coreclr#8552)
    Fix getcpuid(), getextcpuid() with STDCALL
    Fix xmmYmmStateSupport() with STDCALL
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/685de9132d453e08bc4ddb0c66f30d59fb4603b3

--- src/clr/src/vm/i386/unixstubs.cpp
+++ src/clr/src/vm/i386/unixstubs.cpp
@@ -41,49 +41,6 @@ extern "C"
         PORTABILITY_ASSERT("Implement for PAL");
     }
 
-    DWORD getcpuid(DWORD arg, unsigned char result[16])
-    {
-        DWORD eax;
-        __asm("  xor %%ecx, %%ecx\n" \
-              "  cpuid\n" \
-              "  mov %%eax, 0(%[result])\n" \
-              "  mov %%ebx, 4(%[result])\n" \
-              "  mov %%ecx, 8(%[result])\n" \
-              "  mov %%edx, 12(%[result])\n" \
-            : "=a"(eax) /*output in eax*/\
-            : "a"(arg), [result]"r"(result) /*inputs - arg in eax, result in any register*/\
-            : "eax", "rbx", "ecx", "edx", "memory" /* registers that are clobbered, *result is clobbered */
-          );
-        return eax;
-    }
-    
-    DWORD getextcpuid(DWORD arg1, DWORD arg2, unsigned char result[16])
-    {
-        DWORD eax;
-        __asm("  cpuid\n" \
-              "  mov %%eax, 0(%[result])\n" \
-              "  mov %%ebx, 4(%[result])\n" \
-              "  mov %%ecx, 8(%[result])\n" \
-              "  mov %%edx, 12(%[result])\n" \
-            : "=a"(eax) /*output in eax*/\
-            : "c"(arg1), "a"(arg2), [result]"r"(result) /*inputs - arg1 in ecx, arg2 in eax, result in any register*/\
-            : "eax", "rbx", "ecx", "edx", "memory" /* registers that are clobbered, *result is clobbered */
-          );
-        return eax;
-    }
-
-    DWORD xmmYmmStateSupport()
-    {
-        DWORD eax;
-        __asm("  xgetbv\n" \
-            : "=a"(eax) /*output in eax*/\
-            : "c"(0) /*inputs - 0 in ecx*/\
-            : "eax", "edx" /* registers that are clobbered*/
-          );
-        // check OS has enabled both XMM and YMM state support
-        return ((eax & 0x06) == 0x06) ? 1 : 0;
-    }
-    
     void STDCALL JIT_ProfilerEnterLeaveTailcallStub(UINT_PTR ProfilerHandle)
     {
     }
