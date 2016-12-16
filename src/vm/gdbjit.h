93187ae57cdd970bc2eaa8a71c7b7162fcecb440
Igor Kulaychuk <igor.kulaychuk@gmail.com>
2016-12-16 16:05:25 +0300

    [Linux][GDB-JIT] Add try/catch blocks to methods in DWARF (#8650)
    * Add try/catch blocks to DWARF info
    
    Use info about exception handling from IL and map it to
    native code as try/catch blocks in DWARF.
    
    * Improve locals naming convention consistency in gdbjit
    
    * Drop pointer to line info from FunctionMember after it is dumped

--- src/vm/gdbjit.h
+++ src/vm/gdbjit.h
@@ -391,7 +391,7 @@ private:
     static bool BuildStringTableSection(MemBuf& strTab);
     static bool BuildDebugStrings(MemBuf& buf, PTK_TypeInfoMap pTypeMap);
     static bool BuildDebugAbbrev(MemBuf& buf);
-    static bool BuildDebugInfo(MemBuf& buf, PTK_TypeInfoMap pTypeMap);
+    static bool BuildDebugInfo(MemBuf& buf, PTK_TypeInfoMap pTypeMap, SymbolsInfo* lines, unsigned nlines);
     static bool BuildDebugPub(MemBuf& buf, const char* name, uint32_t size, uint32_t dieOffset);
     static bool BuildLineTable(MemBuf& buf, PCODE startAddr, TADDR codeSize, SymbolsInfo* lines, unsigned nlines);
     static bool BuildFileTable(MemBuf& buf, SymbolsInfo* lines, unsigned nlines);
@@ -423,6 +423,8 @@ public:
           m_num_vars(num_args + num_locals),
           m_entry_offset(0),
           vars(new VarDebugInfo[m_num_vars]),
+          lines(NULL),
+          nlines(0),
           m_linkage_name_offset(0),
           dumped(false)
     {
@@ -443,6 +445,7 @@ public:
 
     void DumpStrings(char* ptr, int& offset) override;
     void DumpDebugInfo(char* ptr, int& offset) override;
+    void DumpTryCatchDebugInfo(char* ptr, int& offset);
     HRESULT GetLocalsDebugInfo(NotifyGdb::PTK_TypeInfoMap pTypeMap,
                            LocalsInfo& locals,
                            int startNativeOffset);
@@ -460,12 +463,16 @@ public:
     uint16_t m_num_vars;
     int m_entry_offset;
     VarDebugInfo* vars;
+    SymbolsInfo* lines;
+    unsigned nlines;
     int m_linkage_name_offset;
 private:
     int GetArgsAndLocalsLen();
     void MangleName(char *buf, int &buf_offset, const char *name);
     void DumpMangledNamespaceAndMethod(char *buf, int &offset, const char *nspace, const char *mname);
     void DumpLinkageName(char* ptr, int& offset);
+    bool GetBlockInNativeCode(int blockILOffset, int blockILLen, TADDR *startOffset, TADDR *endOffset);
+    void DumpTryCatchBlock(char* ptr, int& offset, int ilOffset, int ilLen, int abbrev);
     BOOL dumped;
 };
 #endif // #ifndef __GDBJIT_H__
