e775af208363312fb0e6badd91991da02d5f9c1c
Jiyoung Yun <jy910.yun@samsung.com>
2016-12-28 14:28:28 +0900

    Disable GenerateDebs target for building ubuntu arm.
    By using BuidlArchitectures attribute, the GenerateDebs target is disabled.
    And below enumerations are also added:
      BuildArchitecture.arm
      BuildArchitecture.arm64
    
    Signed-off-by: Jiyoung Yun <jy910.yun@samsung.com>
    
    
    Commit migrated from https://github.com/dotnet/core-setup/commit/8e9aa5bf4edce831509d163e252185cd2571eb74

--- src/installer/build_projects/Microsoft.DotNet.Cli.Build.Framework/Enumerations/BuildArchitecture.cs
+++ src/installer/build_projects/Microsoft.DotNet.Cli.Build.Framework/Enumerations/BuildArchitecture.cs
@@ -3,6 +3,8 @@ namespace Microsoft.DotNet.Cli.Build.Framework
     public enum BuildArchitecture
     {
         x86 = 1,
-        x64 = 2
+        x64 = 2,
+        arm = 3,
+        arm64 = 4
     }
 }
