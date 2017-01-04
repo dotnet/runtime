146a7c976296e45ff3cc7efdb377a50f049af586
Marek Safar <marek.safar@gmail.com>
2017-01-04 13:46:29 +0100

    Revert "Bump corlib version"
    This reverts commit mono/mono@acf7d8597142b558a3ec83c952b456ae81841eff.
    
    
    Commit migrated from https://github.com/mono/mono/commit/b7f6f7976e14fc68de286eeab0ce8f472e14598a

--- src/mono/mono/metadata/appdomain.c
+++ src/mono/mono/metadata/appdomain.c
@@ -86,7 +86,7 @@
  * Changes which are already detected at runtime, like the addition
  * of icalls, do not require an increment.
  */
-#define MONO_CORLIB_VERSION 165
+#define MONO_CORLIB_VERSION 164
 
 typedef struct
 {
