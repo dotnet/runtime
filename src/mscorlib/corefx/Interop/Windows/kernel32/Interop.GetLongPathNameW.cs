b2c6ab752c7a71b726695f783a5a0381196cc1fe
John Chen (CLR) <jochen@microsoft.com>
2017-01-09 12:19:14 -0800

    Enable PInvoke analyzer for S.P.CoreLib.dll

--- src/mscorlib/corefx/Interop/Windows/kernel32/Interop.GetLongPathNameW.cs
+++ src/mscorlib/corefx/Interop/Windows/kernel32/Interop.GetLongPathNameW.cs
@@ -7,12 +7,12 @@ using System.Runtime.InteropServices;
 
 partial class Interop
 {
-    partial class mincore
+    partial class Kernel32
     {
         /// <summary>
         /// WARNING: This method does not implicitly handle long paths. Use GetFullPath/PathHelper.
         /// </summary>
-        [DllImport(Libraries.CoreFile_L1, SetLastError = true, CharSet = CharSet.Unicode, BestFitMapping = false, ExactSpelling = true)]
+        [DllImport(Libraries.Kernel32, SetLastError = true, CharSet = CharSet.Unicode, BestFitMapping = false, ExactSpelling = true)]
         internal static extern uint GetLongPathNameW(char[] lpszShortPath, char[] lpszLongPath, uint cchBuffer);
     }
 }
