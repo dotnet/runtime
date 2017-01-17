dd72470021b0a40469f33a6a573840d6d20fc52d
Hyeongseok Oh <hseok82.oh@samsung.com>
2017-01-17 21:41:58 +0900

    [ARM32/Linx] cross-architecture build: restrict build project list (dotnet/coreclr#8918)
    * [ARM32/Linx] cross-architecture build: restrict build project list
    
    Restrict build project for ARM32/Linux cross-architecture
    We can build these projects with PR dotnet/coreclr#8866 and dotnet/coreclr#8917. (release build)
    - crossgen
    - clrjit
    - protojit
    
    * modify crosscomponents.cmake
    
    Add list items when not Linux, instead remove in Linux
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/69c55380c1c611388b8c9e3668f41ed099bdd5f1

--- src/clr/crosscomponents.cmake
+++ src/clr/crosscomponents.cmake
@@ -1,10 +1,15 @@
 add_definitions(-DCROSS_COMPILE)
 
-set (CLR_CROSS_COMPONENTS_LIST  
-  crossgen   
-  mscordaccore   
-  mscordbi   
-  sos
-  clrjit
-  protojit
-)  
+set (CLR_CROSS_COMPONENTS_LIST
+    crossgen   
+    clrjit
+    protojit
+)
+
+if(NOT CLR_CMAKE_PLATFORM_LINUX)
+    list (APPEND CLR_CROSS_COMPONENTS_LIST
+        mscordaccore
+        mscordbi
+        sos
+    )
+endif()
