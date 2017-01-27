06fd8622e368ae23edc0055ce5a0c4ac5075fb9a
Rama Krishnan Raghupathy <ramarag@microsoft.com>
2017-01-26 18:56:05 -0800

    removing match_hash logic
    Commit migrated from https://github.com/dotnet/core-setup/commit/8d2969489eed71571ccc1555c690b67171b275e1

--- src/installer/corehost/cli/args.h
+++ src/installer/corehost/cli/args.h
@@ -13,7 +13,6 @@
 struct probe_config_t
 {
     pal::string_t probe_dir;
-    bool match_hash;
     bool patch_roll_fwd;
     bool prerelease_roll_fwd;
     const deps_json_t* probe_deps_json;
@@ -25,61 +24,47 @@ struct probe_config_t
 
     void print() const
     {
-        trace::verbose(_X("probe_config_t: probe=[%s] match-hash=[%d] deps-json=[%p] deps-dir-probe=[%d]"),
-            probe_dir.c_str(), match_hash, probe_deps_json, probe_publish_dir);
+        trace::verbose(_X("probe_config_t: probe=[%s]  deps-json=[%p] deps-dir-probe=[%d]"),
+            probe_dir.c_str(), probe_deps_json, probe_publish_dir);
     }
 
     probe_config_t(
         const pal::string_t& probe_dir,
-        bool match_hash,
         const deps_json_t* probe_deps_json,
         bool only_serviceable_assets,
         bool only_runtime_assets,
         bool probe_publish_dir)
         : probe_dir(probe_dir)
-        , match_hash(match_hash)
         , probe_deps_json(probe_deps_json)
         , only_serviceable_assets(only_serviceable_assets)
         , only_runtime_assets(only_runtime_assets)
         , probe_publish_dir(probe_publish_dir)
     {
-        // Will not do hash match when probing a deps json.
-        assert(!match_hash || probe_deps_json == nullptr);
     }
 
     static probe_config_t svc_ni(const pal::string_t& dir)
     {
-        return probe_config_t(dir, false, nullptr, true, true, false);
+        return probe_config_t(dir, nullptr, true, true, false);
     }
 
     static probe_config_t svc(const pal::string_t& dir)
     {
-        return probe_config_t(dir, false, nullptr, true, false, false);
-    }
-
-    static probe_config_t cache_ni(const pal::string_t& dir)
-    {
-        return probe_config_t(dir, true, nullptr, false, true, false);
-    }
-    
-    static probe_config_t cache(const pal::string_t& dir)
-    {
-        return probe_config_t(dir, true, nullptr, false, false, false);
+        return probe_config_t(dir, nullptr, true, false, false);
     }
 
     static probe_config_t fx(const pal::string_t& dir, const deps_json_t* deps)
     {
-        return probe_config_t(dir, false, deps, false, false, false);
+        return probe_config_t(dir, deps, false, false, false);
     }
 
     static probe_config_t lookup(const pal::string_t& dir)
     {
-        return probe_config_t(dir, false, nullptr, false, false, false);
+        return probe_config_t(dir, nullptr, false, false, false);
     }
 
     static probe_config_t published_deps_dir()
     {
-        return probe_config_t(_X(""), false, nullptr, false, false, true);
+        return probe_config_t(_X(""), nullptr, false, false, true);
     }
 };
 
