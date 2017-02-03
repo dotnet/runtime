70d49d953b33a07eb85f7f82cafac6101401a484
jashook <jashoo@microsoft.com>
2017-02-02 16:48:27 -0800

    Update lst_creator.
    Generally refactoring, make it a little more robust and easier to use.
    In addition, expand its functionality to update the lstFile.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/0bbb3fbecae00c57bb4488f85bf052eedb13b86f

--- src/clr/tests/scripts/arm64_post_build.py
+++ src/clr/tests/scripts/arm64_post_build.py
@@ -34,7 +34,7 @@ from collections import defaultdict
 
 g_arm64ci_path = os.path.join(os.environ["USERPROFILE"], "bin")
 g_dotnet_url = "https://go.microsoft.com/fwlink/?LinkID=831469"
-g_test_url = "https://clrjit.blob.core.windows.net/arm64ci/CoreCLRTests-2c0a2c05ba82460a8d8a4b1e2d98e908e59d5d54.zip"
+g_test_url = "https://clrjit.blob.core.windows.net/arm64ci/CoreCLRTests-28d04376fe54aea392d75d478bd468f14d134e67.zip"
 g_x64_client_url = "https://clrjit.blob.core.windows.net/arm64ci/x64_client.zip"
 
 ################################################################################
--- src/clr/tests/scripts/lst_creator.py
+++ src/clr/tests/scripts/lst_creator.py
@@ -7,227 +7,376 @@
 ##
 # Title               :lst_creator.py
 #
-# Script to create a working list file from the test overlay directory. This 
+# Script to create a working list file from the test overlay directory. This
 # will be used by smarty to run tests.
 #
 ################################################################################
 
+import argparse
+import datetime
 import os
-import os.path
+import re
 import sys
 
-################################################################################
-# Globals
-################################################################################
-
-g_debug = False
-g_name = ""
-g_old_list_file = ""
-
-def print_debug(str):
-    if g_debug is True:
-        print str
+from collections import defaultdict
 
 ################################################################################
-# Requires the OSS test overlay directory to be built and passed.
-#
-# find_tests
+# Argument Parser
 ################################################################################
 
-def find_tests(base_dir, cat = None, dir = None):
-    # Begin walking this folder recursively.
-    # Look for any file that ends with .cmd
-    # we will be returning a list of these files.
+DESCRIPTION = """Python script to help create/update the arm64 lstFile
+              """
 
-    subdir_list = []
-    cmd_list = []
+PARSER = argparse.ArgumentParser(description=DESCRIPTION)
 
-    def traverse_dir(dir, cat, cat_dir, add_cat = False):
-        if os.path.basename(dir) == cat_dir:
-            add_cat = True
+PARSER.add_argument("--test", dest="testing", action="store_true", default=False)
+PARSER.add_argument("-lst_file", dest="old_list_file", nargs='?', default=None)
+PARSER.add_argument("-test_dir", dest="test_dir", nargs='?', default=None)
+PARSER.add_argument("-commit_hash", dest="commit_hash", nargs='?', default=None)
+PARSER.add_argument("-failures_csv", dest="failures_csv", nargs='?', default=None)
+ARGS = PARSER.parse_args(sys.argv[1:])
 
-        dir = os.path.abspath(dir)
+################################################################################
+# Helper Functions
+################################################################################
 
-        for filename in os.listdir(dir):
-            print_debug(filename)
+def create_list_file(file_name, metadata):
+    """ Create a lstFile given a set of metadata input
 
-            filename = os.path.join(dir, filename)
-            print_debug("Full Path: " + filename)
+    Args:
+        file_name (str): Location to write the lstFile
+        metadata ({ str: { str: str } }): Dictionary mapping test name to
+                                        : a dictionary of key/value
+                                        : attributes.
 
-            if os.path.isfile(filename) and filename.split(".")[-1] == "cmd":
-                if add_cat is True:
-                    cmd_list.append((filename, cat))
-                else:
-                    cmd_list.append((filename, g_name))
+    """
 
-            elif os.path.isdir(filename):
-                traverse_dir(filename, cat, cat_dir, add_cat)
+    current_time = datetime.datetime.now()
+    current_time_str = current_time.strftime("%d-%b-%Y %H:%M:%S%z")
 
-    traverse_dir(base_dir, cat, dir)
+    metadata = [metadata[item] for item in metadata]
+    metadata = sorted(metadata, key=lambda item: item[1])
 
-    return cmd_list
+    new_metadata = [item for item in metadata if item[1] == -1]
+    old_metadata = [item for item in metadata if item[1] != -1]
+
+    with open(file_name, "w") as file_handle:
+        file_handle.write("## This list file has been produced automatically. Any changes\n")
+        file_handle.write("## are subject to being overwritten when reproducing this file.\n")
+        file_handle.write("## \n")
+        file_handle.write("## Last Updated: %s\n" % current_time_str)
+        file_handle.write("## Commit: %s\n" % ARGS.commit_hash)
+        file_handle.write("## \n")
+
+        order = ["RelativePath", "WorkingDir", "Expected", 
+                "MaxAllowedDurationSeconds", "Categories", "HostStyle"]
+
+        def write_metadata(data, count=None):
+            for item in data:
+                test_name = item[0]["RelativePath"]
+                if item[1] != -1:
+                    count = item[1]
+
+                item = item[0]
+
+                # Get the test name.
+                title = "[%s_%d]" % (test_name.split("\\")[-1], count)
+                count += 1
+
+                file_handle.write("%s\n" % title)
+
+                attribute_str = ""
+                for key in order:
+                    attribute_str += "%s=%s\n" % (key, item[key])
+
+                file_handle.write(attribute_str + "\n")
+
+        write_metadata(old_metadata)
+        write_metadata(new_metadata, old_metadata[-1][1] + 1)
+
+def create_metadata(tests):
+    """ Given a set of tests create the metadata around them
+
+    Args:
+        tests ([str]): List of tests for which to determine metadata
+
+    Returns:
+        test_metadata ({ str: { str: str } }): Dictionary mapping test name to
+                                             : a dictionary of key/value
+                                             : attributes.
+
+    """
+    test_metadata = defaultdict(lambda: None)
+
+    failures_csv = ARGS.failures_csv
+
+    failure_information = defaultdict(lambda: None)
+
+    if failures_csv is not None:
+        lines = []
+        assert(os.path.isfile(failures_csv))
+
+        with open(failures_csv, "r") as file_handle:
+            lines = file_handle.readlines()
+        
+        try:
+            for line in lines:
+                split = line.split(",")
+                relative_path = split[0].replace("/", "\\")
+                category = split[1]
+
+                failure_information[relative_path] = category.strip()
+        except:
+            raise Exception("Error. CSV format expects: relativepath,category")
+
+    for test in tests:
+        working_directory = os.path.dirname(test).replace("/", "\\")
+
+        # Make sure the tests use the windows \ seperator.
+        relative_path = test.replace("/", "\\")
+        max_duration = "600"
+        categories = "EXPECTED_PASS"
+        expected = "0"
+        host_style = "0"
+
+        metadata = defaultdict(lambda: None)
+        metadata["RelativePath"] = relative_path
+        metadata["WorkingDir"] = working_directory
+        metadata["MaxAllowedDurationSeconds"] = max_duration
+        metadata["HostStyle"] = host_style
+        metadata["Expected"] = expected
+        metadata["Categories"] = categories
+
+        if failure_information[relative_path] is not None:
+            metadata["Categories"] = failure_information[relative_path]
+
+        test_metadata[relative_path] = metadata
+
+    return test_metadata
+
+def get_all_tests(base_dir):
+    """ Find all of the tests in the enlistment
+
+    Args:
+        base_dir (str): Directory to start traversing from
+
+    Returns:
+        test_list ([str]): List of the tests. Note this is defined to be every
+                         : cmd file under the base_dir.
+
+    Note:
+        To find the tests correctly you must build the tests correctly and 
+        pass that directory. This method will NOT check to make sure that 
+        this has been done correctly.
+
+        This is a recursive method.
+
+    """
+
+    def get_all_tests_helper(working_dir):
+        """ Helper function to recursively get all tests.
+        """
+
+        assert os.path.isdir(working_dir)
+
+        items = os.listdir(working_dir)
+        items = [os.path.join(working_dir, item) for item in items]
+        dirs = [item for item in items if os.path.isdir(item)]
+        tests = [item for item in items if ".cmd" in item]
+
+
+        for item in dirs:
+            tests += get_all_tests_helper(item)
+
+        return tests
+
+    # Recursively get all of the tests in the directory.
+    tests = get_all_tests_helper(base_dir)
+
+    # Find the correct base directory for the tests.
+    common_prefix = os.path.commonprefix(tests)
+
+    if common_prefix is not None:
+        tests = [test.replace(common_prefix, "") for test in tests]
+
+    return tests
+
+def log(message):
+    """ Log a debug message. This is to be used when the --test option is passed
+    """
+
+    if ARGS.testing is True:
+        print message
+
+def parse_lst_file(lst_file):
+    """Parse a lstFile given.
+
+    Args:
+        lst_file(str): location of the lstFile
+
+    Returns:
+        test_metadata (defaultdict(lambda: None)): Key is test name.
+
+    """
+
+    assert os.path.isfile(lst_file)
+
+    contents = None
+    with open(lst_file) as file_handle:
+        contents = file_handle.read()
+
+    split = re.split("\[(.*?)\]", contents)
+
+    unique_name = None
+    test_metadata = defaultdict(lambda: None)
+    for item in split:
+        if len(item) == 0 or item[0] == "#":
+            continue
+
+        if unique_name is None:
+            unique_name = item
+        else:
+            index = int(unique_name.split("_")[-1])
+            metadata = defaultdict(lambda: None)
+
+            attributes = item.split(os.linesep)
+            for attribute in attributes:
+                # Skip the removed new lines.
+                if len(attribute) == 0:
+                    continue
+
+                pair = attribute.split("=")
+                key = pair[0].strip()
+                value = pair[1].strip()
+
+                metadata[key] = value
+
+            # Relative path is unique, while the test name alone is not.
+            unique_name = metadata["RelativePath"]
+            test_metadata[unique_name] = (metadata, index)
+            unique_name = None
+
+    return test_metadata
 
 ################################################################################
 # Main
 ################################################################################
 
+def main(args):
+    """ Main method
+    Args:
+        args ([str]): the arugments passed to the program.
+
+    """
+
+    # Assign all of the passed variables.
+    test_dir = args.test_dir
+    old_list_file = args.old_list_file
+    commit_hash = args.commit_hash
+
+    if commit_hash is None:
+        print "Error please provide a commit hash."
+        sys.exit(1)
+
+    if test_dir is None or not os.path.isdir(test_dir):
+        print "Error the test directory passed is not a valid directory."
+        sys.exit(1)
+
+    tests = get_all_tests(test_dir)
+    print "Found %d tests in the test directory." % (len(tests))
+    print
+
+    old_test_metadata = None
+    # If we are updating an old lstFile. Get all of the tests from that
+    # lstFile and their metadata.
+    if old_list_file is not None:
+        old_test_metadata = parse_lst_file(old_list_file)
+
+        print "Found %d tests in the old lstFile." % (len(old_test_metadata))
+
+    test_metadata = create_metadata(tests)
+
+    if old_test_metadata is not None:
+        # If the new information has been changed, we will need to update
+        # the lstFile.
+
+        new_test_count = 0
+        update_count = 0
+        for test_name in test_metadata:
+            new_metadata = test_metadata[test_name]
+            old_metadata = old_test_metadata[test_name]
+
+            attributes = None
+            if old_test_metadata[test_name] is None:
+                new_test_count += 1
+                new_metadata["Categories"] += ";NEW"
+                old_test_metadata[test_name] = (new_metadata, -1)
+
+            else:
+                index = old_metadata[1]
+                old_metadata = old_metadata[0]
+                attributes = set(old_metadata.keys() + new_metadata.keys())
+
+                # Make sure we go through all attributes of both sets.
+                # If an attribute exists in one set but not the other it will
+                # be None. If the new metadata has a new attribute, write this
+                # into the old metadata. If the old metadata has an attribute
+                # that does not exist in the new set. Do not remove it.
+
+                overwritten = False
+
+                for attribute in attributes:
+                    if attribute == "MaxAllowedDurationSeconds":
+                            continue
+                    if attribute == "Categories":
+                        new_split = new_metadata["Categories"].split(";")
+                        old_split = old_metadata["Categories"].split(";")
+
+                        if "NEW" in old_split:
+                            old_split.remove("NEW")
+
+                        # If an old test is marked as a failing test. Make
+                        # sure that we carry that information along.
+                        if "EXPECTED_PASS" in new_split and "EXPECTED_FAIL" in old_split:
+                            new_split.remove("EXPECTED_PASS")
+
+                        # If it used to be marked as pass but it is now failing. Make sure
+                        # we remove the old category.
+                        elif "EXPECTED_FAIL" in new_split and "EXPECTED_PASS" in old_split:
+                            old_split.remove("EXPECTED_PASS")
+
+                        joined_categories = set(old_split + new_split)
+
+                        overwritten = True
+                        old_metadata[attribute] = ";".join(joined_categories)
+                        old_test_metadata[test_name] = (old_metadata, index)
+
+                    elif new_metadata[attribute] != old_metadata[attribute]:
+                            # If the old information is not the same as the new
+                            # information, keep the new information. overwrite the old
+                            # metadata.
+                            if new_metadata[attribute] is not None:
+                                overwritten = True
+                                old_metadata[attribute] = new_metadata[attribute]
+
+                                old_test_metadata[test_name] = (old_metadata, index)
+
+                    if overwritten:
+                        update_count += 1
+
+        print "Added %d tests." % new_test_count
+        print "Finished join. %d tests updated." % update_count
+
+        test_metadata = old_test_metadata
+
+    # Overwrite the old file if provided, else use the generic name Tests.lst
+    lst_file = "Tests.lst" if old_list_file is None else old_list_file
+
+    # Write out the new lstFile
+    create_list_file(lst_file, test_metadata)
+
+################################################################################
+################################################################################
+
 if __name__ == "__main__":
-    print "Starting lst_creator"
-    print "- - - - - - - - - - - - - - - - - - - - - - - - - - - -"
-    print
-
-    if len(sys.argv) < 4:
-        print "Error, incorrect number of arguments."
-        print "Ex usage: python lst_creator <root_oss_test_dir> <lst file name> <optional cat name> <old list file location>"
-        print "Tests must be built!"
-        exit(1)
-
-    if not os.path.isdir(sys.argv[1]):
-        print "Error argument passed is not a valid directory."
-        exit(1)
-
-    g_name = sys.argv[3]
-
-    cat, dirname = None, None
-    
-    if len(sys.argv) > 4:
-        if sys.argv[4] == "-D":
-            cat = sys.argv[5]
-            dirname = sys.argv[6]
-
-        elif sys.argv[4] != "-D":
-            g_old_list_file = sys.argv[4]
-
-            if not os.path.isfile(g_old_list_file):
-
-                print "Error, old list file must be valid."
-                exit(1)
-
-    cmd_list = find_tests(sys.argv[1], cat, dirname)
-
-    print "Found " + str(len(cmd_list)) + " tests to add."
-    print
-
-    if g_old_list_file is not "":
-        print "Updating the old list file"
-
-    else:
-        print "Creating the lst file."
-
-    unique_output = dict()
-    largest_value = 0
-
-    # If there was an old list file passed. Parse it for all the old tests.
-
-    if g_old_list_file is not "":
-        old_list_file_lines = []
-
-        with open(sys.argv[4]) as lst_file_handle:
-
-            old_list_file_lines = lst_file_handle.readlines()
-
-        for line in old_list_file_lines:
-            split_line = line.split("[")
-
-            # We only need the test names
-            # which come in as [ testname_number ]
-            if len(split_line) == 1:
-                continue
-
-            # This is a test name, start splitting
-
-            split_line = split_line[1].split("]")
-            split_line = split_line[0].split("_")
-
-            if largest_value < int(split_line[-1]):
-                largest_value = int(split_line[-1])
-
-            test_name = "_".join(split_line[:-1])
-
-            if len(test_name.split("exe")) == 1:
-                # Error, name is not an exe.
-                print "Error"
-
-                sys.exit(1)
-
-            unique_output[test_name] = True
-
-        print str(len(unique_output)) + " tests found in the old lstFile."
-
-    output = []
-
-    repeat_count = 0
-    count = largest_value
-
-    for line in cmd_list:
-        path, cat = line[0], line[1]
-
-        # get the relative path
-        prefix = os.path.commonprefix([path, sys.argv[1]])
-        rel_path = os.path.relpath(path, prefix)
-
-        cmd_contents = None
-        with open(path) as cmd_file_handle:
-            cmd_contents = cmd_file_handle.readlines()
-
-        expected_exit_code_line = None
-
-        for cmd_line in cmd_contents:
-            if cmd_line.find("CLRTestExpectedExitCode") != -1:
-                expected_exit_code_line = cmd_line
-                break
-
-        if expected_exit_code_line is None:
-            print "Error, cmd file missing contents. Skipping, however, the test suite was not built correctly."
-            print path
-            continue
-
-        expected = expected_exit_code_line[expected_exit_code_line.find("CLRTestExpectedExitCode") + (len("CLRTestExpectedExitCode") + 1):].strip()
-        max_allowed_duration = 600
-        categories = cat
-        build_type = "CoreSys"
-        relative_path = rel_path[:rel_path.find("cmd")] + "exe"
-        working_dir = os.path.dirname(rel_path)
-        test_name = os.path.basename(relative_path)
-
-        try:
-            if unique_output[test_name] == True:
-                repeat_count += 1
-
-                continue
-
-        except:
-            output.append("[" + test_name + "_" + str(count) + "]" + "\n")
-
-            count = count + 1
-
-            output.append("RelativePath=" + os.path.relpath(relative_path) + "\n")
-            output.append("WorkingDir=" + os.path.relpath(working_dir) + "\n")
-            output.append("Expected=" + expected + "\n")
-            output.append("MaxAllowedDurationSeconds=" + str(max_allowed_duration) + "\n")
-            output.append("Categories=" + categories + "\n")
-            output.append("HostStyle=Any")
-            output.append("\n")
-
-    print
-    print "Writing out lst file."
-
-    if repeat_count > 0:
-        print "Found " + str(repeat_count) + " old tests."
-    
-        # If we found repeats then we open file to append not write.
-
-        with open(g_old_list_file, 'a') as list_file_handle:
-            list_file_handle.write("\n")
-
-            for line in output:
-                list_file_handle.write(line)
-
-
-    else:
-        with open(sys.argv[2], 'w') as list_file_handle:
-
-            list_file_handle.write("##=== Test Definitions ===============================\n")
-
-            for line in output:
-                list_file_handle.write(line)
+    main(ARGS)
