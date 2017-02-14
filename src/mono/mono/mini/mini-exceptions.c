ec071cb6b97bed85cab99a4b85c24632ab41e144
mathieubourgeois <mathieu.bourgeois@gameloft.com>
2017-02-13 21:53:05 -0500

    Fix when() clause cutting stack traces when filtering doesn't succeed (fix for 46661) (mono/mono#4328)
    * * [exceptions] When encountering a filter (when) clause, don't setup the stack trace before we know if the filter is going to work. Otherwise, any exception that isn't blocked by the when clause will have its stack trace cut at the point of the parent of the function containing the when clause
    * Fixes https://bugzilla.xamarin.com/show_bug.cgi?id=46661
    
    * + Added test case for bug 46661
    
    
    Commit migrated from https://github.com/mono/mono/commit/516e1cf79367c22caea1ae897511ab763a616a09

--- src/mono/mono/mini/mini-exceptions.c
+++ src/mono/mono/mini/mini-exceptions.c
@@ -1589,18 +1589,9 @@ mono_handle_exception_internal_first_pass (MonoContext *ctx, MonoObject *obj, gi
 					ex_obj = obj;
 
 				if (ei->flags == MONO_EXCEPTION_CLAUSE_FILTER) {
-					gboolean is_user_frame = method->wrapper_type == MONO_WRAPPER_NONE || method->wrapper_type == MONO_WRAPPER_DYNAMIC_METHOD;
 #ifndef DISABLE_PERFCOUNTERS
 					mono_perfcounters->exceptions_filters++;
 #endif
-					/*
-					Here's the thing, if this is a filter clause done by a wrapper like runtime invoke, we don't want to
-					trim the stackframe since if it returns FALSE we lose information.
-
-					FIXME Not 100% sure if it's a good idea even with user clauses.
-					*/
-					if (is_user_frame)
-						setup_stack_trace (mono_ex, dynamic_methods, &trace_ips);
 
 #ifndef MONO_CROSS_COMPILE
 #ifdef MONO_CONTEXT_SET_LLVM_EXC_REG
@@ -1637,8 +1628,7 @@ mono_handle_exception_internal_first_pass (MonoContext *ctx, MonoObject *obj, gi
 					filter_idx ++;
 
 					if (filtered) {
-						if (!is_user_frame)
-							setup_stack_trace (mono_ex, dynamic_methods, &trace_ips);
+						setup_stack_trace (mono_ex, dynamic_methods, &trace_ips);
 						g_slist_free (dynamic_methods);
 						/* mono_debugger_agent_handle_exception () needs this */
 						mini_set_abort_threshold (ctx);
