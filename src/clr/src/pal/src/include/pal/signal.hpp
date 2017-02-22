0d6a40704c56a6d3f079228b2d94bc6f4b0524ad
Jan Vorlicek <janvorli@microsoft.com>
2017-02-22 23:18:01 +0100

    Improve stack overflow reporting (dotnet/coreclr#9650)
    * Improve stack overflow reporting
    
    This change modifies the SIGSEGV handling to use an alternate stack so that
    we can safely detect and report stack overflow even in case when we are
    really out of stack. Before, we were able to detect stack overflow and report
    it only when JIT inserted stack probes (for functions with frames larger than
    4kB) and so there was still space on the stack to run the sigsegv handler.
    It brings in some additional complexity, since we need to switch to the original
    stack of the thread once we figure out the sigsegv is not a stack overflow and
    if we return from the hardware exception handler, we need to switch back to the
    alternate stack before returning from the sigsegv handler.
    Also, the alternate stack is created per thread and so we need to correctly destroy
    it when a thread terminates and also install it on foreign threads that enter PAL.
    This also requires creating fake stack frames to enable the libunwind to walk
    the stack from the exception handler to the sigsegv location.
    
    * Fix stack unwinding in CallDescrWorkerInternal
    
    While testing the change to enable stack overflow handling, I've noticed that
    the PROLOG_SAVE_REG_PAIR and PROLOG_SAVE_REG_PAIR_INDEXED macros are missing
    .cfi_def_cfa_register fp. That resulted in inability to unwind through the
    CallDescrWorkerInternal, since this function dynamically allocates stack slots
    and so the default sp based frame doesn't work.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/f193024a5ef8f277f08bed3721e2e8e730aabdb9

---
+++ src/clr/src/pal/src/include/pal/signal.hpp
@@ -0,0 +1,141 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+/*++
+
+
+
+Module Name:
+
+    include/pal/signal.hpp
+
+Abstract:
+    Private signal handling utilities for SEH
+
+
+
+--*/
+
+#ifndef _PAL_SIGNAL_HPP_
+#define _PAL_SIGNAL_HPP_
+
+#if !HAVE_MACH_EXCEPTIONS
+
+struct SignalHandlerWorkerReturnPoint;
+
+/*++
+Function :
+    CallSignalHandlerWrapperX
+
+    These functions are never called, only a fake stack frame will be setup to have a return
+    address set to SignalHandlerWorkerReturnX during SIGSEGV handling.
+    It enables the unwinder to unwind stack from the handling code to the actual failure site.
+
+    There are four variants of this function based on what stack alignment needs to be done
+    to ensure properly aligned stack pointer at the call site of the signal_handler_worker.
+
+Parameters :
+    none
+
+    (no return value)
+--*/
+extern "C" void CallSignalHandlerWrapper0();
+extern "C" void CallSignalHandlerWrapper4();
+extern "C" void CallSignalHandlerWrapper8();
+extern "C" void CallSignalHandlerWrapper12();
+
+// Offset of the return address from the signal_handler_worker in the CallSignalHandlerWrapperX 
+// relative to the start of the function.
+// There are four offsets matching the stack alignments as described in the function header above.
+extern "C" int SignalHandlerWorkerReturnOffset0;
+extern "C" int SignalHandlerWorkerReturnOffset4;
+extern "C" int SignalHandlerWorkerReturnOffset8;
+extern "C" int SignalHandlerWorkerReturnOffset12;
+
+/*++
+Function :
+    signal_handler_worker
+
+    Handles signal on the original stack where the signal occured. 
+    Invoked via setcontext.
+
+Parameters :
+    POSIX signal handler parameter list ("man sigaction" for details)
+    returnPoint - context to which the function returns if the common_signal_handler returns
+
+    (no return value)
+--*/
+extern "C" void signal_handler_worker(int code, siginfo_t *siginfo, void *context, SignalHandlerWorkerReturnPoint* returnPoint);
+
+/*++
+Function :
+    ExecuteHandlerOnOriginalStack
+
+    Executes signal_handler_worker on the original stack where the signal occured.
+    It installs fake stack frame to enable stack unwinding to the signal source location.
+
+Parameters :
+    POSIX signal handler parameter list ("man sigaction" for details)
+    returnPoint - context to which the function returns if the common_signal_handler returns
+
+    (no return value)
+--*/
+void ExecuteHandlerOnOriginalStack(int code, siginfo_t *siginfo, void *context, SignalHandlerWorkerReturnPoint* returnPoint);
+
+/*++
+Function :
+    EnsureSignalAlternateStack
+
+    Ensure that alternate stack for signal handling is allocated for the current thread
+
+Parameters :
+    None
+
+Return :
+    TRUE in case of a success, FALSE otherwise
+--*/
+BOOL EnsureSignalAlternateStack();
+
+/*++
+Function :
+    FreeSignalAlternateStack
+
+    Free alternate stack for signal handling
+
+Parameters :
+    None
+
+Return :
+    None
+--*/
+void FreeSignalAlternateStack();
+
+/*++
+Function :
+    SEHInitializeSignals
+
+    Set-up signal handlers to catch signals and translate them to exceptions
+
+Parameters :
+    flags: PAL initialization flags
+
+Return :
+    TRUE in case of a success, FALSE otherwise
+--*/
+BOOL SEHInitializeSignals(DWORD flags);
+
+/*++
+Function :
+    SEHCleanupSignals
+
+    Restore default signal handlers
+
+    (no parameters, no return value)
+--*/
+void SEHCleanupSignals();
+
+#endif // !HAVE_MACH_EXCEPTIONS
+
+#endif /* _PAL_SIGNAL_HPP_ */
+
