cc7e209404b98e2db56d977de4c7dbf30e5fe6e9
Caio Cesar Saldanha Maia Orejuela Kinelski <t-cakine@microsoft.com>
2017-03-01 14:44:52 -0800

    Custom deps feature implemented.
    Commit migrated from https://github.com/dotnet/core-setup/commit/1d3afa57d9349463e4775e543ffc77cf82c451a1

--- src/installer/corehost/cli/deps_resolver.h
+++ src/installer/corehost/cli/deps_resolver.h
@@ -49,6 +49,8 @@ public:
             m_deps = std::unique_ptr<deps_json_t>(new deps_json_t(false, m_deps_file));
         }
 
+        resolve_additional_deps(init);
+
         setup_additional_probes(args.probe_paths);
         setup_probe_config(init, args);
     }
@@ -92,6 +94,9 @@ public:
     void init_known_entry_path(
         const deps_entry_t& entry,
         const pal::string_t& path);
+
+    void resolve_additional_deps(
+        const hostpolicy_init_t& init);
 
     const pal::string_t& get_fx_deps_file() const
     {
@@ -171,6 +176,9 @@ private:
 
     // The filepath for the app deps
     pal::string_t m_deps_file;
+
+    // The filepaths for the app custom deps
+    std::vector<pal::string_t> m_additional_deps_files;
     
     // The filepath for the fx deps
     pal::string_t m_fx_deps_file;
@@ -180,6 +188,9 @@ private:
 
     // Deps files for the app
     std::unique_ptr<deps_json_t>  m_deps;
+
+    // Custom deps files for the app
+    std::vector< std::unique_ptr<deps_json_t> > m_additional_deps;
 
     // Various probe configurations.
     std::vector<probe_config_t> m_probes;
