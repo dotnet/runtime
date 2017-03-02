709b76879961a9dc7161ffa12a90e7a2b10034eb
Jan Kotas <jkotas@microsoft.com>
2017-03-01 17:28:42 -0800

    Cleanup (dotnet/coreclr#9892)
    * Delete unused PdbTypeMatch tool
    * Delete unused GetFileVersion.dll binary
    * Delete bad comments
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/c666f11b70bf12ef2e4568e71c4547e8b2176911

--- src/coreclr/src/ToolBox/PdbTypeMatch/stdafx.cpp
+++
@@ -1,9 +0,0 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file
