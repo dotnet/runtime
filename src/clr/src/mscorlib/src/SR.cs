738a5862b3c5c6a4ce0b67ccc1f3e25dc15496fa
Alex Perovich <alperovi@microsoft.com>
2017-03-02 20:11:47 -0600

    Merge shared changes from corert and use C# shared project to enable VS editing (dotnet/coreclr#9922)
    Commit migrated from https://github.com/dotnet/coreclr/commit/684784fdb929f2c7d0173a317cb6e3ac6dc29d1a

--- src/clr/src/mscorlib/src/SR.cs
+++ src/clr/src/mscorlib/src/SR.cs
@@ -835,4 +835,7 @@ internal static class SR
     {
         get { return Environment.GetResourceString("Arg_ArrayPlusOffTooSmall"); }
     }
+
+    internal static string PlatformNotSupported_OSXFileLocking =>
+        Environment.GetResourceString("PlatformNotSupported_OSXFileLocking");
 }
