88fe72339d517a49b74a85e617496fd1cbc8e391
Drew Scoggins <anscoggi@microsoft.com>
2017-03-03 14:51:37 -0800

    Update CoreCLR to use xunit perf api
    This updates the CoreCLR performance runs to use the new xunit
    performance api.  This will unblock several people who were wanting to
    move tests forward from netstandard1.4 which is were we have been locked
    because of using the old desktop runner.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/02749bf56c408d62e2687a8f208b8b2e674df7eb

---
+++ src/coreclr/tests/src/Common/PerfHarness/PerfHarness.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+using Microsoft.Xunit.Performance.Api;
+
+public class PerfHarness
+{
+    public static void Main(string[] args)
+    {
+        string assemblyName = args[0];
+        using (XunitPerformanceHarness harness = new XunitPerformanceHarness(args))
+        {
+            harness.RunBenchmarks(assemblyName);
+        }
+    }
+}
