5d3fc768a03ef6ff84b58c7e7a5c58be7ee121ab
Mikhail Skvortcov <m.skvortcov@partner.samsung.com>
2017-03-03 15:20:04 +0300

    RyuJIT/ARM32: enable codegen for long nodes.
    Commit migrated from https://github.com/dotnet/coreclr/commit/0d02a5a1e7c44db3d06c159a641a597b13b23178

--- src/coreclr/src/jit/gentree.h
+++ src/coreclr/src/jit/gentree.h
@@ -3398,7 +3398,7 @@ struct GenTreeCall final : public GenTree
     //
     bool HasMultiRegRetVal() const
     {
-#if defined(_TARGET_X86_) && !defined(LEGACY_BACKEND)
+#if (defined(_TARGET_X86_) || defined(_TARGET_ARM_)) && !defined(LEGACY_BACKEND)
         // LEGACY_BACKEND does not use multi reg returns for calls with long return types
         return varTypeIsLong(gtType);
 #elif FEATURE_MULTIREG_RET
