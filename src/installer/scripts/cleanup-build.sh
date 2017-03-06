ca3deefd4689b38271173fa731b6a295005ef65a
Ravi Eda <raeda@microsoft.com>
2017-03-06 16:24:10 -0600

    Introduce cleanup-build.sh that will restore BuildTools package, and executes cleanup-docker.sh.
    Commit migrated from https://github.com/dotnet/core-setup/commit/251d9b56f8b6d2e3421fe375370cc13c7288379c

---
+++ src/installer/scripts/cleanup-build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Restores BuildTools package, and runs cleanup-docker.sh
+
+# Get repository root.
+scriptpath=$(cd "$(dirname "$0")"; pwd -P)
+repoRoot=$(cd "$scriptpath/.."; pwd -P)
+
+# Restore BuildTools package.
+"$repoRoot"/pkg/init-tools.sh
+
+# Run the script to cleanup Docker containers and images.
+"$repoRoot"/pkg/Tools/scripts/docker/cleanup-docker.sh
