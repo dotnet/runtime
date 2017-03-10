c6c0457b7ecf157344df135d7fe1bc71428db42c
Aleksey Kliger <aleksey@xamarin.com>
2017-03-10 10:50:51 -0500

    [mono/tests] Add strong named assembly loading tests
    These tests place strong named assemblies on the MONO_PATH and into
    MONO_GAC_PREFIX in various configurations and check that when a strong assembly
    name  is specified: mono will ignore non-strong named assemblies that come
    earlier in the search paths; mono will ignore strong named assemblies that have
    the right filename and come earlier in the search path but have an incorrect
    strong name.
    
    
    Commit migrated from https://github.com/mono/mono/commit/5a06ca6c04cdc2e0f869b0794a7537a7cbe40ff1

---
+++ src/mono/mono/tests/testing_gac/v2/gactestlib.cs
@@ -0,0 +1,15 @@
+
+[assembly: System.Reflection.AssemblyVersion ("1.1.0.0")]
+
+public class X
+{
+	public static void N1 ()
+	{
+		
+	}
+
+	// In the "v2" version, let's make this method missing.
+	// public static void N2 ()
+	// {
+	// }
+}
