7a1cbc505a3a23bf5402f186ad3c2fad267eff9a
Alexander Soldatov <soldatov.a@samsung.com>
2017-03-14 16:34:39 +0300

    Added Array tests to CodeGenBringUpTests
    Commit migrated from https://github.com/dotnet/coreclr/commit/f734e77607bdf9b39049216948fc6ce243c69852

---
+++ src/coreclr/tests/src/JIT/CodeGenBringUpTests/Array4.cs
@@ -0,0 +1,25 @@
+    // Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+//
+
+using System;
+using System.Runtime.CompilerServices;
+
+public class BringUpTest
+{
+    const int Pass = 100;
+    const int Fail = -1;
+
+    [MethodImplAttribute(MethodImplOptions.NoInlining)]
+    static int Array4(int i) {
+        int[] a = {1, 2, 3, 4};
+        return a[i];
+    }
+
+    static int Main()
+    {
+        if (Array4(1) != 2) return Fail;
+        return Pass;
+    }
+}
