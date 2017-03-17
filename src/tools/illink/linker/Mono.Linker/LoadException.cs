628e7aab7ae0670218202595825f4bed5264d8fa
Mike Voorhees <michaelv@unity3d.com>
2017-03-17 14:57:41 -0400

    Improve exception message when exception
    is thrown while loading references
    
    
    Commit migrated from https://github.com/dotnet/linker/commit/96b2425b2505656a601d3595254b1bf36c6b31e8

---
+++ src/tools/illink/linker/Mono.Linker/LoadException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Mono.Linker
+{
+	public class LoadException : Exception
+	{
+		public LoadException (string message)
+			: base (message)
+		{
+		}
+
+		public LoadException (string message, Exception innerException)
+			: base (message, innerException)
+		{
+		}
+	}
+}
