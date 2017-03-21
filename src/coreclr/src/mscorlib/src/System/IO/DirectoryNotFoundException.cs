91490ac9c1d5b49a86b6824bf289b07744ad394d
Jan Kotas <jkotas@microsoft.com>
2017-03-21 16:24:52 -0700

    Revert "Switch coreclr corelib to use resx (dotnet/coreclr#10268)"
    This reverts commit dotnet/coreclr@12ef04c22b07f1e4ec5a63009298e4537a2c8e1a.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/6d32a5152ef43752c72ff8dbbe9fbc635a55df13

--- src/coreclr/src/mscorlib/src/System/IO/DirectoryNotFoundException.cs
+++ src/coreclr/src/mscorlib/src/System/IO/DirectoryNotFoundException.cs
@@ -28,7 +28,7 @@ namespace System.IO
     public class DirectoryNotFoundException : IOException
     {
         public DirectoryNotFoundException()
-            : base(SR.Arg_DirectoryNotFoundException)
+            : base(Environment.GetResourceString("Arg_DirectoryNotFoundException"))
         {
             SetErrorCode(__HResults.COR_E_DIRECTORYNOTFOUND);
         }
