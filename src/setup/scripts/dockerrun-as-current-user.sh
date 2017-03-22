e0eb1bc934c3d2291632553e89cc60f8b2fcc8ba
Davis Goodin <dagood@users.noreply.github.com>
2017-03-22 15:35:32 -0500

    Run CrossBuild as agent user to avoid creating undeletable files (dotnet/core-setup#1817)
    * Add generic script to run Docker as current user
    
    * Use custom HOME if HOME is set but doesn't exist
    
    * Run CrossBuild as agent user
    
    
    Commit migrated from https://github.com/dotnet/core-setup/commit/104b04de5a898c513f92bcaeb7fc659d88fc45e4

--- src/setup/scripts/dockerrun-as-current-user.sh
+++ src/setup/scripts/dockerrun-as-current-user.sh
@@ -6,4 +6,12 @@
 
 set -e
 
-docker run -u="$(id -u):$(id -g)" "$@"
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ "$SOURCE" != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+"$DIR/docker-as-current-user.sh" run "$@"
