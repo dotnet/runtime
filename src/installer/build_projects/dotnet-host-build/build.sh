eb34ab86cb013b539ec5395a899120e44b2388a2
Eric Erhardt <eric.erhardt@microsoft.com>
2017-03-22 09:56:42 -0500

    Update dotnet-install script to 'dotnet/cli/master'.
    Commit migrated from https://github.com/dotnet/core-setup/commit/bfddd4112d554e1e57ae2fd0bed32e1084cd4cd7

--- src/installer/build_projects/dotnet-host-build/build.sh
+++ src/installer/build_projects/dotnet-host-build/build.sh
@@ -141,7 +141,7 @@ done < "$REPOROOT/branchinfo.txt"
 [ -z "$DOTNET_INSTALL_DIR" ] && export DOTNET_INSTALL_DIR=$REPOROOT/.dotnet_stage0/$(uname)
 [ -d "$DOTNET_INSTALL_DIR" ] || mkdir -p $DOTNET_INSTALL_DIR
 
-DOTNET_INSTALL_SCRIPT_URL="https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.sh"
+DOTNET_INSTALL_SCRIPT_URL="https://raw.githubusercontent.com/dotnet/cli/master/scripts/obtain/dotnet-install.sh"
 curl -sSL "$DOTNET_INSTALL_SCRIPT_URL" | bash /dev/stdin --version 1.0.0-preview3-003886 --verbose
 
 # Put stage 0 on the PATH (for this shell only)
