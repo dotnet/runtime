c33110faf5abcd840498445557000e0d0b4b4457
Gaurav Khanna <gkhanna@microsoft.com>
2017-03-23 09:25:32 -0700

    Build Core-Setup for OSX 10.12
    Commit migrated from https://github.com/dotnet/core-setup/commit/e46c30e1cce9f54f2b4280e1244dd93345d2d374

--- src/setup/build_projects/shared-build-targets-utils/DependencyVersions.cs
+++ src/setup/build_projects/shared-build-targets-utils/DependencyVersions.cs
@@ -7,7 +7,7 @@ namespace Microsoft.DotNet.Cli.Build
 {
     public class DependencyVersions
     {
-        public static readonly string CoreCLRVersion = "2.0.0-beta-25121-02";
-        public static readonly string JitVersion = "2.0.0-beta-25121-02";
+        public static readonly string CoreCLRVersion = "2.0.0-beta-25123-03";
+        public static readonly string JitVersion = "2.0.0-beta-25123-03";
     }
 }
