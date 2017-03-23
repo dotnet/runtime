4517c45741f43fde626ab72f1d28d607921846c4
Atsushi Kanamori <AtsushiKan@users.noreply.github.com>
2017-03-23 11:49:42 -0700

    Migrate the Reflection serialization holders to the shared partition. (dotnet/coreclr#10429)
    * String => string, SR.GetResourceString() => SR.
    
    * Copy over CoreRT files verbatim.
    
    * Get things building and running again.
    
    * Don't bring CoreRT's visibility sins into CoreCLR
    
    * String => string, Object => object
    
    * De-"m_" the field names.
    
    * Move the files to the shared directory.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/0b9b922e7a4197e478adb354189eab508a475410

---
+++ src/clr/src/mscorlib/src/System/Reflection/MemberSerializationStringGenerator.cs
@@ -0,0 +1,39 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Text;
+using System.Reflection;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace System
+{
+    internal static class MemberSerializationStringGenerator
+    {
+        //
+        // Generate the "Signature2" binary serialization string for PropertyInfos
+        //
+        // Because the string is effectively a file format for serialized Reflection objects, it must be exactly correct. If missing
+        // metadata prevents generating the string, this method throws a MissingMetadata exception.
+        // 
+        public static string SerializationToString(this PropertyInfo property) => ((RuntimePropertyInfo)property).SerializationToString();
+
+        //
+        // Generate the "Signature2" binary serialization string for ConstructorInfos
+        //
+        // Because the string is effectively a file format for serialized Reflection objects, it must be exactly correct. If missing
+        // metadata prevents generating the string, this method throws a MissingMetadata exception.
+        // 
+        public static string SerializationToString(this ConstructorInfo constructor) => ((RuntimeConstructorInfo)constructor).SerializationToString();
+
+        //
+        // Generate the "Signature2" binary serialization string for MethodInfos
+        //
+        // Because the string is effectively a file format for serialized Reflection objects, it must be exactly correct. If missing
+        // metadata prevents generating the string, this method throws a MissingMetadata exception.
+        // 
+        public static string SerializationToString(this MethodInfo method) => ((RuntimeMethodInfo)method).SerializationToString();
+    }
+}
