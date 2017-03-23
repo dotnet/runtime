3b4382036ae086065f559a4a0139d95bca9a7ecc
Aditya Mandaleeka <adityam@microsoft.com>
2017-03-23 16:23:05 -0700

    Create a GCHandleTable interface with Init/Shutdown.
    Commit migrated from https://github.com/dotnet/coreclr/commit/8d02c0786825a27d67fb2af150f751dbda360bef

--- src/clr/src/gc/gchandletable.cpp
+++ src/clr/src/gc/gchandletable.cpp
@@ -1,23 +1,24 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
-
-
+// 
 
 #include "common.h"
-
 #include "gcenv.h"
-
-#include "gc.h"
-#include "gcscan.h"
 #include "gchandletableimpl.h"
+#include "objecthandle.h"
 
-#ifdef SERVER_GC
-#undef SERVER_GC
-#endif
-
-namespace WKS { 
-#include "gcimpl.h"
-#include "gcee.cpp"
+IGCHandleTable* CreateGCHandleTable()
+{
+    return new(nothrow) GCHandleTable();
 }
 
+bool GCHandleTable::Initialize()
+{
+    return Ref_Initialize();
+}
+
+void GCHandleTable::Shutdown()
+{
+    Ref_Shutdown();
+}
