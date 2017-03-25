040573b4386a7e219ce39d4503b2ec75791e0785
mikedn <onemihaid@hotmail.com>
2017-03-25 16:45:10 +0200

    Some BitVecOps improvements (dotnet/coreclr#10437)
    * Avoid virtual calls when allocating bitvectors
    
    * Use specialized BitVecOps when possible
    
    * Avoid unnecessary bitvector copying
    
    * Cleanup CSE_DataFlow
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/6d5ce09e13a3ff43d592cc67c04e026a12cce008

--- src/coreclr/src/jit/codegencommon.cpp
+++ src/coreclr/src/jit/codegencommon.cpp
@@ -1100,9 +1100,9 @@ void Compiler::compChangeLife(VARSET_VALARG_TP newLife DEBUGARG(GenTreePtr tree)
     /* Can't simultaneously become live and dead at the same time */
 
     // (deadSet UNION bornSet) != EMPTY
-    noway_assert(!VarSetOps::IsEmpty(this, VarSetOps::Union(this, deadSet, bornSet)));
+    noway_assert(!VarSetOps::IsEmptyUnion(this, deadSet, bornSet));
     // (deadSet INTERSECTION bornSet) == EMPTY
-    noway_assert(VarSetOps::IsEmpty(this, VarSetOps::Intersection(this, deadSet, bornSet)));
+    noway_assert(VarSetOps::IsEmptyIntersection(this, deadSet, bornSet));
 
 #ifdef LEGACY_BACKEND
     // In the LEGACY_BACKEND case, we only consider variables that are fully enregisterd
