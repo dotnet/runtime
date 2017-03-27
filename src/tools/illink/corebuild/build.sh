8ae7adfc6a1dd902dc65a5f3b211058cf60b79d1
Sven Boemer <sbomer@gmail.com>
2017-03-27 09:28:47 -0700

    Remove outdated comments in build scripts
    Commit migrated from https://github.com/dotnet/linker/commit/ae59acd7b4fe0706fa201d1088b0885b97d52793

--- src/tools/illink/corebuild/build.sh
+++ src/tools/illink/corebuild/build.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-# build.sh will bootstrap the cli and ultimately call "dotnet build".
-# If no configuration is specified, the default configuration will be
-# set to netcore_Debug (see config.json).
+# build.sh will bootstrap the cli and ultimately call "dotnet build"..
 
 working_tree_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 $working_tree_root/dotnet.sh build ../linker/Mono.Linker.csproj -c netcore_Debug $@
