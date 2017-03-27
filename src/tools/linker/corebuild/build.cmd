16f0bc1393e1a3241e36481a236703f2a67beab8
Sven Boemer <sbomer@gmail.com>
2017-03-27 09:28:47 -0700

    Remove outdated comments in build scripts
    Commit migrated from https://github.com/dotnet/linker/commit/ae59acd7b4fe0706fa201d1088b0885b97d52793

--- src/tools/linker/corebuild/build.cmd
+++ src/tools/linker/corebuild/build.cmd
@@ -1,8 +1,6 @@
 @if not defined _echo @echo off
 
-REM build.cmd will bootstrap the cli and ultimately call "dotnet build".
-REM If no configuration is specified, the default configuration will be
-REM set to netcore_Debug (see config.json).
+REM build.cmd will bootstrap the cli and ultimately call "dotnet build"..
 
 @call dotnet.cmd build ..\linker\Mono.Linker.csproj -c netcore_Debug %*
 @exit /b %ERRORLEVEL%
