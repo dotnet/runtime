def03cc858e599b45c165fa592a3e5b0de272e22
smile21prc <xiwe@microsoft.com>
2017-03-29 11:52:25 -0700

    Re-enable multi-queue for coreclr (#10541)
    * Add multiqueue Support for coreclr.
    
    Add multiqueue Support for coreclr.
    
    * Upload build results to ONE linux container
    
    Upload build results to ONE linux container
    
    * Fix helixpublish.proj
    
    Fix helixpublish.proj
    
    * Adding more logs.
    
    Adding more logs.
    
    * Revert logging changes.
    
    Revert logging changes.
    
    * Use build version containing support of "+" delimiter
    
    Use build version containing support of "+" delimiter
    
    * Fix indention.
    
    Fix indention.
    
    * Update to use latest tools.
    
    Update to use latest tools.
    
    * Use an older tool as latest has a wired error on "Run build.sh" step.
    
    Error details:
    2017-03-28T23:30:13.2659430Z Running:
    /root/coreclr/Tools/dotnetcli/dotnet /root/coreclr/Tools/run.exe
    /root/coreclr/config.json build -Project=/root/coreclr/build.proj
    -MsBuildLog=/flp:Verbosity=normal;LogFile=/root/coreclr/bin/Logs/System.Private.CoreLib_Checked.log
    -BuildTarget -__IntermediatesDir=/root/coreclr/bin/obj/Linux.x64.Checked
    -__RootBinDir=/root/coreclr/bin -BuildNugetPackage=false
    -UseSharedCompilation=false -BuildArch=x64 -BuildType=Checked
    -BuildOS=Linux
    -OptimizationDataDir="/root/coreclr/packages/optimization.Linux-x64.IBC.CoreCLR//data/"
    -EnableProfileGuidedOptimization=true -skiprestore
    -OfficialBuildId=20170329-01 -- /p:ConfigurationGroup=Release
    /flp:v=diag
    2017-03-28T23:30:13.6855470Z Running: /root/coreclr/Tools/msbuild.sh
    /nologo /verbosity:minimal /clp:Summary /maxcpucount
    /l:BinClashLogger,Tools/net46/Microsoft.DotNet.Build.Tasks.dll;LogFile=binclash.log
    /p:RestoreDuringBuild=false  /root/coreclr/build.proj
    /p:__BuildType=Checked /p:__BuildArch=x64 /p:__BuildOS=Linux
    /p:__RootBinDir=/root/coreclr/bin
    /p:__IntermediatesDir=/root/coreclr/bin/obj/Linux.x64.Checked
    /p:OfficialBuildId=20170329-01 /p:EnableProfileGuidedOptimization=true
    /p:OptimizationDataDir="/root/coreclr/packages/optimization.Linux-x64.IBC.CoreCLR//data/"
    
    /flp:Verbosity=normal;LogFile=/root/coreclr/bin/Logs/System.Private.CoreLib_Checked.log
    /p:BuildNugetPackage=false /t:Build /p:UseSharedCompilation=false
    /p:ConfigurationGroup=Release /flp:v=diag
    2017-03-28T23:30:15.5843800Z
    /root/coreclr/Tools/Microsoft.CSharp.CurrentVersion.targets(321,5):
    error MSB4019: The imported project
    "/root/coreclr/Tools/Microsoft.Net.Compilers/2.0.0-rc/tools/Microsoft.CSharp.Core.targets"
    was not found. Confirm that the path in the <Import> declaration is
    correct, and that the file exists on disk.
    [/root/coreclr/src/ToolBox/SOS/NETCore/SOS.NETCore.csproj]
    2017-03-28T23:30:16.6459770Z
    /root/coreclr/Tools/Microsoft.CSharp.CurrentVersion.targets(321,5):
    error MSB4019: The imported project
    "/root/coreclr/Tools/Microsoft.Net.Compilers/2.0.0-rc/tools/Microsoft.CSharp.Core.targets"
    was not found. Confirm that the path in the <Import> declaration is
    correct, and that the file exists on disk.
    [/root/coreclr/src/mscorlib/System.Private.CoreLib.csproj]
    
    * New builds have errors, revert to use older builds.
    
    New builds have errors, revert to use older builds.

--- publish-packages.sh
+++ publish-packages.sh
@@ -10,6 +10,8 @@ usage()
     echo "   -BuildArch=\"Architecture\""
     echo "To specify the name of the container to publish into, use the following property:"
     echo "   -Container=\"container name\""
+    echo "To specify the OS you're building for, use the following property:"
+    echo "   -DistroRiD=\"RID\""	
     echo "Configuration can be Release, Checked, or Debug"
     echo "Architecture can be x64, x86, arm, or arm64"
     exit 1
@@ -18,7 +20,7 @@ usage()
 working_tree_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 $working_tree_root/run.sh publish-packages -BuildOS $*
-$working_tree_root/run.sh publish-packages -BuildOS -distroRid -PublishTestNativeBins $*
+$working_tree_root/run.sh publish-packages -BuildOS -PublishTestNativeBins $*
 if [ $? -ne 0 ]
 then
     echo "ERROR: An error occurred while publishing packages; see $working_tree_root/publish-packages.log for more details. There may have been networking problems, so please try again in a few minutes."
