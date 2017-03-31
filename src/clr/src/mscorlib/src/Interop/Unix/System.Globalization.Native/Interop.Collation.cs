6a5a6350b9a7dbd7260f8c3236bb861bebcb18a3
Jan Kotas <jkotas@microsoft.com>
2017-03-31 11:45:17 -0700

    Fix build breaks
    Commit migrated from https://github.com/dotnet/coreclr/commit/9b1504c1baccdebe5e3df9dba3d8faaf548a4b2e

--- src/clr/src/mscorlib/src/Interop/Unix/System.Globalization.Native/Interop.Collation.cs
+++ src/clr/src/mscorlib/src/Interop/Unix/System.Globalization.Native/Interop.Collation.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Diagnostics;
 using System.Globalization;
 using System.Runtime.InteropServices;
 using System.Security;
