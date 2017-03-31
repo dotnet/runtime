c00172036a2b79412b5f274910d0d9bb73674ac5
Sven Boemer <sbomer@gmail.com>
2017-03-31 09:33:57 -0700

    Have build scripts use linker.sln
    Also make build scripts resilient to caller's working directory.
    
    
    Commit migrated from https://github.com/dotnet/linker/commit/77b99e0b58fd2f3bd0714d6676fbe6711147b48d

--- src/tools/linker/corebuild/restore.cmd
+++ src/tools/linker/corebuild/restore.cmd
@@ -1,18 +1,7 @@
 @if not defined _echo @echo off
 
-REM restore.cmd will bootstrap the cli and ultimately call "dotnet
-REM restore". The configuration specified here is just a workaround to
-REM set the correct conditional properties for the netcore/netstandard
-REM restore in the .csproj files. It doesn't matter whether we choose
-REM Debug or Release here, as the same assets will be restored in either
-REM case.
+REM restore.sh will bootstrap the cli and ultimately call "dotnet
+REM restore". Dependencies of the linker will get restored as well.
 
-REM Normally, "dotnet restore" will also restore referenced
-REM projects. However, because we are using the old .csproj format,
-REM restore will not correctly restore project references, and so we
-REM need to explicitly restore Mono.Cecil as well as Mono.Linker.
-
-@call dotnet.cmd restore ..\linker\Mono.Linker.csproj /p:Configuration=netcore_Debug %*
-@call dotnet.cmd restore ..\cecil\Mono.Cecil.csproj /p:Configuration=netstandard_Debug %*
-@call dotnet.cmd restore ..\cecil\symbols\pdb\Mono.Cecil.Pdb.csproj /p:Configuration=netstandard_Debug %*
+@call %~dp0dotnet.cmd restore %dp0linker.sln %*
 @exit /b %ERRORLEVEL%
--- src/tools/linker/corebuild/restore.sh
+++ src/tools/linker/corebuild/restore.sh
@@ -1,18 +1,7 @@
 #!/usr/bin/env bash
 
 # restore.sh will bootstrap the cli and ultimately call "dotnet
-# restore". The configuration specified here is just a workaround to
-# set the correct conditional properties for the netcore/netstandard
-# restore in the .csproj files. It doesn't matter whether we choose
-# Debug or Release here, as the same assets will be restored in either
-# case.
-
-# Normally, "dotnet restore" will also restore referenced
-# projects. However, because we are using the old .csproj format,
-# restore will not correctly restore project references, and so we
-# need to explicitly restore Mono.Cecil as well as Mono.Linker.
+# restore". Dependencies of the linker will get restored as well.
 
 working_tree_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-$working_tree_root/dotnet.sh restore ../linker/Mono.Linker.csproj /p:Configuration=netcore_Debug $@
-$working_tree_root/dotnet.sh restore ../cecil/Mono.Cecil.csproj /p:Configuration=netstandard_Debug $@
-$working_tree_root/dotnet.sh restore ../cecil/symbols/pdb/Mono.Cecil.Pdb.csproj /p:Configuration=netstandard_Debug $@
+$working_tree_root/dotnet.sh restore $working_tree_root/linker.sln $@
