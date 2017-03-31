965774b868699a7c1551e26e9fa7f21095244e9d
Jan Kotas <jkotas@microsoft.com>
2017-03-31 13:33:53 -0700

    Change the test hook from interface to delegate
    Commit migrated from https://github.com/dotnet/coreclr/commit/6168b219764d53f6a1821c1f6459fe93b69c3da4

--- src/coreclr/src/mscorlib/src/System/Diagnostics/Debug.Windows.cs
+++ src/coreclr/src/mscorlib/src/System/Diagnostics/Debug.Windows.cs
@@ -2,77 +2,60 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Security;
-
 namespace System.Diagnostics
 {
-    // Intentionally excluding visibility so it defaults to internal except for
-    // the one public version in System.Diagnostics.Debug which defines
-    // another version of this partial class with the public visibility 
-    static partial class Debug
+    public static partial class Debug
     {
         private static string NewLine => "\r\n";
 
-        // internal and not read only so that the tests can swap this out.
-        internal static IDebugLogger s_logger = new WindowsDebugLogger();
-
-        // --------------
-        // PAL ENDS HERE
-        // --------------
-
-        internal sealed class WindowsDebugLogger : IDebugLogger
+        private static void ShowAssertDialog(string stackTrace, string message, string detailMessage)
         {
-            [SecuritySafeCritical]
-            public void ShowAssertDialog(string stackTrace, string message, string detailMessage)
+            if (Debugger.IsAttached)
             {
-                if (Debugger.IsAttached)
+                Debugger.Break();
+            }
+            else
+            {
+                // TODO: #3708 Determine if/how to put up a dialog instead.
+                throw new DebugAssertException(message, detailMessage, stackTrace);
+            }
+        }
+
+        private static void WriteCore(string message)
+        {
+            // really huge messages mess up both VS and dbmon, so we chop it up into 
+            // reasonable chunks if it's too big. This is the number of characters 
+            // that OutputDebugstring chunks at.
+            const int WriteChunkLength = 4091;
+
+            // We don't want output from multiple threads to be interleaved.
+            lock (s_ForLock)
+            {
+                if (message == null || message.Length <= WriteChunkLength)
                 {
-                    Debugger.Break();
+                    WriteToDebugger(message);
                 }
                 else
                 {
-                    // TODO: #3708 Determine if/how to put up a dialog instead.
-                    throw new DebugAssertException(message, detailMessage, stackTrace);
+                    int offset;
+                    for (offset = 0; offset < message.Length - WriteChunkLength; offset += WriteChunkLength)
+                    {
+                        WriteToDebugger(message.Substring(offset, WriteChunkLength));
+                    }
+                    WriteToDebugger(message.Substring(offset));
                 }
             }
+        }
 
-            public void WriteCore(string message)
+        private static void WriteToDebugger(string message)
+        {
+            if (Debugger.IsLogging())
             {
-                // really huge messages mess up both VS and dbmon, so we chop it up into 
-                // reasonable chunks if it's too big. This is the number of characters 
-                // that OutputDebugstring chunks at.
-                const int WriteChunkLength = 4091;
-
-                // We don't want output from multiple threads to be interleaved.
-                lock (s_ForLock)
-                {
-                    if (message == null || message.Length <= WriteChunkLength)
-                    {
-                        WriteToDebugger(message);
-                    }
-                    else
-                    {
-                        int offset;
-                        for (offset = 0; offset < message.Length - WriteChunkLength; offset += WriteChunkLength)
-                        {
-                            WriteToDebugger(message.Substring(offset, WriteChunkLength));
-                        }
-                        WriteToDebugger(message.Substring(offset));
-                    }
-                }
+                Debugger.Log(0, null, message);
             }
-
-            [System.Security.SecuritySafeCritical]
-            private static void WriteToDebugger(string message)
+            else
             {
-                if (Debugger.IsLogging())
-                {
-                    Debugger.Log(0, null, message);
-                }
-                else
-                {
-                    Interop.Kernel32.OutputDebugString(message ?? string.Empty);
-                }
+                Interop.Kernel32.OutputDebugString(message ?? string.Empty);
             }
         }
     }
