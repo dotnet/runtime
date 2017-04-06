03ebd8d4f06e7098929aa5bf52c823db37249119
Atsushi Kanamori <AtsushiKan@users.noreply.github.com>
2017-04-06 07:45:58 -0700

    Clean up and prepare FileLoadException.cs for move to Shared Partition. (dotnet/coreclr#10764)
    * String => string
    
    * Replace fields with autoprops.
    
    * Turn Type.FullName call to Type.ToString() in ToString().
    
    Avoids MissingMetadataExceptions on CoreRT.
    
    Assuming metadata exists, ToString() and FullName()
    still generates the same string except in the weird
    case where an app subclasses FileLoadException
    with a generic exception type - in which case,
    the ToString() version is actually more readable.
    
    * Get rid of useless try-catch around FusionLog calls.
    
    FusionLog is now just a normal property and an answer
    to a trivia question - it won't throw SecurityException.
    
    * Get rid of low-value SetMessageField() middleman.
    
    * Split off the stuff we don't want to share.
    
    * The complementary carwash...
    
    * Don't conditionalize FusionLog serialization.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/82b8a644a560e4c688ba6d59ac5fb86bd24b68ad

---
+++ src/coreclr/src/mscorlib/src/System/IO/FileLoadException.CoreCLR.cs
@@ -0,0 +1,43 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using System.Security;
+
+namespace System.IO
+{
+    public partial class FileLoadException
+    {
+        // Do not delete: this is invoked from native code.
+        private FileLoadException(string fileName, string fusionLog, int hResult)
+            : base(null)
+        {
+            SetErrorCode(hResult);
+            FileName = fileName;
+            FusionLog = fusionLog;
+            _message = FormatFileLoadExceptionMessage(FileName, HResult);
+        }
+
+        internal static string FormatFileLoadExceptionMessage(string fileName, int hResult)
+        {
+            string format = null;
+            GetFileLoadExceptionMessage(hResult, JitHelpers.GetStringHandleOnStack(ref format));
+
+            string message = null;
+            GetMessageForHR(hResult, JitHelpers.GetStringHandleOnStack(ref message));
+
+            return string.Format(CultureInfo.CurrentCulture, format, fileName, message);
+        }
+
+        [DllImport(JitHelpers.QCall, CharSet = CharSet.Unicode)]
+        [SuppressUnmanagedCodeSecurity]
+        private static extern void GetFileLoadExceptionMessage(int hResult, StringHandleOnStack retString);
+
+        [DllImport(JitHelpers.QCall, CharSet = CharSet.Unicode)]
+        [SuppressUnmanagedCodeSecurity]
+        private static extern void GetMessageForHR(int hresult, StringHandleOnStack retString);
+    }
+}
