d86521e84140078303bb8d031e95e235180dbe7b
Sean Gillespie <sean@swgillespie.me>
2017-04-05 22:20:35 -0700

    [Local GC] Fix a bad merge (dotnet/coreclr#10748)
    Commit migrated from https://github.com/dotnet/coreclr/commit/f9d0333d8955c11a2e8f495f0a12d6384fb95541

--- src/coreclr/src/gc/env/gcenv.ee.h
+++ src/coreclr/src/gc/env/gcenv.ee.h
@@ -72,7 +72,6 @@ public:
     static void HandleFatalError(unsigned int exitCode);
     static bool ShouldFinalizeObjectForUnload(AppDomain* pDomain, Object* obj);
     static bool ForceFullGCToBeBlocking();
-    static bool ShouldElevateForAppDomainCleanup();
     static bool EagerFinalized(Object* obj);
 };
 
