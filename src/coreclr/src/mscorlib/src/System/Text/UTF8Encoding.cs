9e929be731344385433aa4851b867f5de6f96dcb
Ben Adams <thundercat@illyriad.co.uk>
2017-04-07 03:05:18 +0100

    Devirtualizable default encoding (dotnet/coreclr#10783)
    Commit migrated from https://github.com/dotnet/coreclr/commit/97daa74ccbb052878135dec2e11ddac6b0219566

--- src/coreclr/src/mscorlib/src/System/Text/UTF8Encoding.cs
+++ src/coreclr/src/mscorlib/src/System/Text/UTF8Encoding.cs
@@ -57,13 +57,12 @@ namespace System.Text
         [Serializable]
         internal sealed class UTF8EncodingSealed : UTF8Encoding
         {
-            public UTF8EncodingSealed() : base(encoderShouldEmitUTF8Identifier: true) { }
             public UTF8EncodingSealed(bool encoderShouldEmitUTF8Identifier) : base(encoderShouldEmitUTF8Identifier) { }
         }
 
         // Used by Encoding.UTF8 for lazy initialization
         // The initialization code will not be run until a static member of the class is referenced
-        internal static readonly UTF8EncodingSealed s_default = new UTF8EncodingSealed();
+        internal static readonly UTF8EncodingSealed s_default = new UTF8EncodingSealed(encoderShouldEmitUTF8Identifier: true);
 
         // Yes, the idea of emitting U+FEFF as a UTF-8 identifier has made it into
         // the standard.
