9dd9febe93230a221c5569d10ef02cf4a63ed8f7
Eric Erhardt <eric.erhardt@microsoft.com>
2017-04-11 10:00:13 -0500

    Allow additional ICompilationAssemblyResolvers
    This allows for callers to use their own compilation resolvers along with the default resolvers shipped in the DependencyModel.
    
    Fix dotnet/core-setup#625
    
    
    Commit migrated from https://github.com/dotnet/core-setup/commit/84338605b408a996657d19bf92417bde8933b6f0

---
+++ src/installer/test/Microsoft.Extensions.DependencyModel.Tests/CompilationLibraryTests.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using FluentAssertions;
+using Microsoft.Extensions.DependencyModel.Resolution;
+using Moq;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Microsoft.Extensions.DependencyModel.Tests
+{
+    public class CompilationLibraryTests
+    {
+        [Fact]
+        public void ResolveReferencePathsAcceptsAdditionalResolvers()
+        {
+            var fail = new Mock<ICompilationAssemblyResolver>();
+            var success = new Mock<ICompilationAssemblyResolver>();
+            success.Setup(r => r.TryResolveAssemblyPaths(It.IsAny<CompilationLibrary>(), It.IsAny<List<string>>()))
+                .Callback((CompilationLibrary l, List<string> a) =>
+                {
+                    a.Add("Assembly");
+                })
+                .Returns(true);
+
+            var failTwo = new Mock<ICompilationAssemblyResolver>();
+
+            var resolvers = new[]
+            {
+                fail.Object,
+                success.Object,
+                failTwo.Object
+            };
+
+            var library = TestLibraryFactory.Create();
+
+            var result = library.ResolveReferencePaths(resolvers);
+
+            result.ShouldBeEquivalentTo(new[] { "Assembly" });
+
+            fail.Verify(r => r.TryResolveAssemblyPaths(It.IsAny<CompilationLibrary>(), It.IsAny<List<string>>()),
+                Times.Once());
+            success.Verify(r => r.TryResolveAssemblyPaths(It.IsAny<CompilationLibrary>(), It.IsAny<List<string>>()),
+                Times.Once());
+            failTwo.Verify(r => r.TryResolveAssemblyPaths(It.IsAny<CompilationLibrary>(), It.IsAny<List<string>>()),
+                Times.Never());
+        }
+    }
+}
