9a46949027b68bb4d7b1a9a5b4de9d95715ed71b
Jan Kotas <jkotas@microsoft.com>
2017-04-11 17:44:40 -0700

    Move files to shared CoreLib partition (dotnet/coreclr#10885)
    Commit migrated from https://github.com/dotnet/coreclr/commit/c85c19baef5edf867c3a9a217cbe235a892d0c1e

--- src/coreclr/src/mscorlib/shared/System/Threading/SendOrPostCallback.cs
+++ src/coreclr/src/mscorlib/shared/System/Threading/SendOrPostCallback.cs
@@ -1,14 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
-/*============================================================
-**
-**
-**
-** Purpose: Represents a method to be called when a message is to be dispatched to a synchronization context. 
-**
-** 
-===========================================================*/
 
 namespace System.Threading
 {
