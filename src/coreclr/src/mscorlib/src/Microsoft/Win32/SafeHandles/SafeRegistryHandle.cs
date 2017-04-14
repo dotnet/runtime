135ac4d4176a20469fa86df508666fb525ed61ef
danmosemsft <danmose@microsoft.com>
2017-04-14 15:53:33 -0700

    Revert "Strip out unused reg code. (dotnet/coreclr#10741)"
    This reverts commit dotnet/coreclr@ed4f594abf41a71b126152bb8755051d0831e12d.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/368b35097bb12ef8ec5df64103303c7488d82dd7

--- src/coreclr/src/mscorlib/src/Microsoft/Win32/SafeHandles/SafeRegistryHandle.cs
+++ src/coreclr/src/mscorlib/src/Microsoft/Win32/SafeHandles/SafeRegistryHandle.cs
@@ -2,9 +2,19 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+//
+//
+//
+// Implements Microsoft.Win32.SafeHandles.SafeRegistryHandle
+//
+// ======================================================================================
+
 using System;
 using System.Security;
 using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.Versioning;
 
 namespace Microsoft.Win32.SafeHandles
 {
