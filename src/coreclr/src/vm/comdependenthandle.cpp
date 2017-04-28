fa3d1179726aa4266d750524f0b9dd5ebe011296
Jan Kotas <jkotas@microsoft.com>
2017-04-27 18:28:34 -0700

    Avoid unnecessary out arguments in DependentHandle FCalls
    Make the signatures closer to what they are in CoreRT
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/f8b5fc2b225d4cad2d38388e99dd8578a426924c

--- src/coreclr/src/vm/comdependenthandle.cpp
+++ src/coreclr/src/vm/comdependenthandle.cpp
@@ -17,22 +17,22 @@
 
 
 
-FCIMPL3(VOID, DependentHandle::nInitialize, Object *_primary, Object *_secondary, OBJECTHANDLE *outHandle)
+FCIMPL2(OBJECTHANDLE, DependentHandle::nInitialize, Object *_primary, Object *_secondary)
 {
     FCALL_CONTRACT;
 
-    _ASSERTE(outHandle != NULL && *outHandle == NULL);  // Multiple initializations disallowed 
-
     OBJECTREF primary(_primary);
     OBJECTREF secondary(_secondary);
+    OBJECTHANDLE result = NULL;
 
-    HELPER_METHOD_FRAME_BEGIN_NOPOLL();
+    HELPER_METHOD_FRAME_BEGIN_RET_NOPOLL();
     
     // Create the handle.
-    *outHandle = GetAppDomain()->CreateDependentHandle(primary, secondary);
+    result = GetAppDomain()->CreateDependentHandle(primary, secondary);
 
     HELPER_METHOD_FRAME_END_POLL();
 
+    return result;
 }
 FCIMPLEND
 
@@ -55,22 +55,22 @@ FCIMPLEND
 
 
 
-FCIMPL2(VOID, DependentHandle::nGetPrimary, OBJECTHANDLE handle, Object **outPrimary)
+FCIMPL1(Object*, DependentHandle::nGetPrimary, OBJECTHANDLE handle)
 {
     FCALL_CONTRACT;
-    _ASSERTE(handle != NULL && outPrimary != NULL);
-    *outPrimary = OBJECTREFToObject(ObjectFromHandle(handle));
+    FCUnique(0x54);
+    _ASSERTE(handle != NULL);
+    return OBJECTREFToObject(ObjectFromHandle(handle));
 }
 FCIMPLEND
 
 
 
-FCIMPL3(VOID, DependentHandle::nGetPrimaryAndSecondary, OBJECTHANDLE handle, Object **outPrimary, Object **outSecondary)
+FCIMPL2(Object*, DependentHandle::nGetPrimaryAndSecondary, OBJECTHANDLE handle, Object **outSecondary)
 {
     FCALL_CONTRACT;
-    _ASSERTE(handle != NULL && outPrimary != NULL && outSecondary != NULL);
-    *outPrimary = OBJECTREFToObject(ObjectFromHandle(handle));
     *outSecondary = OBJECTREFToObject(GetDependentHandleSecondary(handle));
+    return OBJECTREFToObject(ObjectFromHandle(handle));
 }
 FCIMPLEND
 
