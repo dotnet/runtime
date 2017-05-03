2f3a31f0e475dc2fb91012fcaa0c082f6e74fe8a
Gaurav Khanna <gkhanna@microsoft.com>
2017-05-03 16:06:22 -0700

    Add CoreLib explicitly to TPA list (#2272)

--- src/corehost/cli/hostpolicy.cpp
+++ src/corehost/cli/hostpolicy.cpp
@@ -49,6 +49,22 @@ int run(const arguments_t& args)
         return StatusCode::CoreClrResolveFailure;
     }
 
+    // Get path in which CoreCLR is present.
+    pal::string_t clr_dir = get_directory(clr_path);
+
+    // System.Private.CoreLib.dll is expected to be next to CoreCLR.dll - add its path to the TPA list.
+    pal::string_t corelib_path = clr_dir;
+    append_path(&corelib_path, CORELIB_NAME);
+
+    // Append CoreLib path
+    if (probe_paths.tpa.back() != PATH_SEPARATOR)
+    {
+        probe_paths.tpa.push_back(PATH_SEPARATOR);
+    }
+
+    probe_paths.tpa.append(corelib_path);
+    probe_paths.tpa.push_back(PATH_SEPARATOR);
+
     pal::string_t clrjit_path = probe_paths.clrjit;
     if (clrjit_path.empty())
     {
@@ -143,7 +159,6 @@ int run(const arguments_t& args)
     assert(property_keys.size() == property_values.size());
 
     // Bind CoreCLR
-    pal::string_t clr_dir = get_directory(clr_path);
     trace::verbose(_X("CoreCLR path = '%s', CoreCLR dir = '%s'"), clr_path.c_str(), clr_dir.c_str());
     if (!coreclr::bind(clr_dir))
     {
