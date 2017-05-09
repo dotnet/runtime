76876bb3d652b80a83cdeba42d6b754f34c632bb
Mike Voorhees <michaelv@unity3d.com>
2017-05-09 17:49:18 -0400

    Add a test for copying of core assemblies
    Commit migrated from https://github.com/dotnet/linker/commit/8c5a33bb25974f786460ac739d923555b7478342

---
+++ src/tools/linker/linker/Tests/Mono.Linker.Tests.Cases/CoreLink/CopyOfCoreLibrariesKeepsUnusedTypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
+using Mono.Linker.Tests.Cases.Expectations.Metadata;
+
+namespace Mono.Linker.Tests.Cases.CoreLink
+{
+	[CoreLink ("copy")]
+	[KeptAssembly ("mscorlib.dll")]
+	// These types are normally removed when the core libraries are linked
+	[KeptTypeInAssembly ("mscorlib.dll", typeof (ConsoleKeyInfo))]
+	[KeptTypeInAssembly ("mscorlib.dll", typeof (System.Collections.ObjectModel.KeyedCollection<,>))]
+	class CopyOfCoreLibrariesKeepsUnusedTypes
+	{
+		public static void Main()
+		{
+		}
+	}
+}
