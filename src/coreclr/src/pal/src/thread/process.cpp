c9a5c256ea6bd4afc4dd06c749e4d1a9b139daf2
Jan Vorlicek <janvorli@microsoft.com>
2017-05-09 11:03:32 +0200

    Enable build on old Linux (dotnet/coreclr#11414)
    This change enables building coreclr on old Linux distros that
    don't have PR_SET_PTRACER and don't have _xstate struct in the
    standard sigcontext.h header.
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/3437a820fdc94caa3d1775bcee802b056f3adce2

--- src/coreclr/src/pal/src/thread/process.cpp
+++ src/coreclr/src/pal/src/thread/process.cpp
@@ -2981,7 +2981,7 @@ PROCAbort()
     // Do any shutdown cleanup before aborting or creating a core dump
     PROCNotifyProcessShutdown();
 
-#if HAVE_PRCTL_H
+#if HAVE_PRCTL_H && HAVE_PR_SET_PTRACER
     // If enabled, launch the create minidump utility and wait until it completes
     if (g_argvCreateDump[0] != nullptr)
     {
@@ -3018,7 +3018,7 @@ PROCAbort()
             }
         }
     }
-#endif // HAVE_PRCTL_H
+#endif // HAVE_PRCTL_H && HAVE_PR_SET_PTRACER
     // Abort the process after waiting for the core dump to complete
     abort();
 }
