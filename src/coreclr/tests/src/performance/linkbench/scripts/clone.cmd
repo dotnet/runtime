42e1a4112c815547238a6e520a3de9b83d1424da
Swaroop Sridhar <swaroops@microsoft.com>
2017-05-10 15:25:53 -0700

    This change has the following improvements to LinkBench
    1. Move the testing to the latest version of ILLink package
       (for all benchmarks except for Roslyn, which is still on netcoreapp1.1)
    2. Support for LinkBench to run individual benchmarks in the suite --
       helps local testing.
    3. Support for running without clean, cloning, build etc --
       for local runs during continuous development.
    4. Support to obtain the correct version(s) of DotNet CLI required to run
       the benchmarks
    5. Get-Crossgen.ps1 is required to crossgen MusicStore binaries (before/after linking).
       Since it was recently dropped from JitBench repo, it is added to linkBench as an asset.
    6. Add the --perf:runid parameter to generate output file names suitable
       to upload the results to BenchView.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/406166785e168d840b711783e9806b155376a930

--- src/coreclr/tests/src/performance/linkbench/scripts/clone.cmd
+++ src/coreclr/tests/src/performance/linkbench/scripts/clone.cmd
@@ -1,30 +1,68 @@
 @echo off
 
-rmdir /s /q LinkBench
-
 set ROOT=%cd%\LinkBench
-mkdir LinkBench 2> nul
-pushd %ROOT%
+set EXITCODE=0
 
+if not exist %LinkBenchRoot% mkdir %LinkBenchRoot% 
+pushd %LinkBenchRoot%
+
+if not exist .dotnet call :DotNet
+if defined __test_HelloWorld call :HelloWorld
+if defined __test_WebAPI call :WebAPI
+if defined __test_MusicStore call :MusicStore
+if defined __test_MusicStore_R2R call :MusicStore_R2R 
+if defined __test_CoreFx call :CoreFx
+if defined __test_Roslyn call :Roslyn
+
+popd
+exit /b %EXITCODE%
+
+:DotNet
+REM Roslyn needs SDK 1.0.0, other benchmarks need SDK 2.0.0
+mkdir .dotnet
+cd .dotnet
+mkdir 1.0.0
+mkdir 2.0.0
+powershell -noprofile -executionPolicy RemoteSigned wget  https://raw.githubusercontent.com/dotnet/cli/master/scripts/obtain/dotnet-install.ps1 -OutFile dotnet-install.ps1
+powershell -noprofile -executionPolicy RemoteSigned -file dotnet-install.ps1 -InstallDir 1.0.0
+powershell -noprofile -executionPolicy RemoteSigned -file dotnet-install.ps1 -Channel master -InstallDir 2.0.0
+if not exist %__dotnet1% set EXITCODE=1
+if not exist %__dotnet2% set EXITCODE=1
+cd ..
+exit /b 
+
+:HelloWorld
 mkdir HelloWorld
 cd HelloWorld
 dotnet new console
-if errorlevel 1 exit /b 1
+if errorlevel 1 set EXITCODE=1
 cd ..
+exit /b 
 
+:WebAPI
 mkdir WebAPI
 cd WebAPI
 dotnet new webapi
-if errorlevel 1 exit /b 1
+if errorlevel 1 set EXITCODE=1
 cd ..
+exit /b
 
+:MusicStore
 git clone https://github.com/aspnet/JitBench -b dev
-if errorlevel 1 exit /b 1
+if errorlevel 1 set EXITCODE=1
+exit /b
 
+:MusicStore_R2R
+REM MusicStore_R2R requires a previous MusicStore run.
+REM No Additional Setup
+exit /b
+
+:CoreFx
 git clone http://github.com/dotnet/corefx
-if errorlevel 1 exit /b 1
+if errorlevel 1 set EXITCODE=1
+exit /b
 
+:Roslyn
 git clone https://github.com/dotnet/roslyn.git
-if errorlevel 1 exit /b 1
-
-popd
+if errorlevel 1 set EXITCODE=1
+exit /b
