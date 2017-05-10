d59b4df76d662fa7e9e09fda295094434ab74c42
Mike Voorhees <michaelv@unity3d.com>
2017-05-10 19:39:12 -0400

    Add tests for ComImport cases
    Commit migrated from https://github.com/dotnet/linker/commit/cbb4fe809fab5684ecb5302ae1f01a28db418f42

---
+++ src/tools/illink/linker/Tests/Mono.Linker.Tests.Cases/Interop/InternalCalls/Com/FieldsOfReturnTypeAreRemoved.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
+
+namespace Mono.Linker.Tests.Cases.Interop.InternalCalls.Com {
+	class FieldsOfReturnTypeAreRemoved {
+		public static void Main ()
+		{
+			var a = SomeMethod ();
+		}
+
+		[Kept]
+		[ComImport]
+		[Guid ("D7BB1889-3AB7-4681-A115-60CA9158FECA")]
+		class A {
+			private int field;
+		}
+
+		[Kept]
+		[MethodImpl (MethodImplOptions.InternalCall)]
+		static extern A SomeMethod ();
+	}
+}
---
+++ src/tools/illink/linker/Tests/Mono.Linker.Tests.Cases/Interop/PInvoke/Com/FieldsOfThisAreRemoved.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
+
+namespace Mono.Linker.Tests.Cases.Interop.PInvoke.Com {
+	class FieldsOfThisAreRemoved {
+		public static void Main ()
+		{
+			new A ().SomeMethod ();
+		}
+
+		[Kept]
+		[KeptMember (".ctor()")]
+		[ComImport]
+		[Guid ("D7BB1889-3AB7-4681-A115-60CA9158FECA")]
+		class A {
+			private int field;
+
+			[Kept]
+			[MethodImpl (MethodImplOptions.InternalCall)]
+			public extern void SomeMethod ();
+		}
+	}
+}
