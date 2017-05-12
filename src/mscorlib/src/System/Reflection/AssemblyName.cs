0c4845c9a327446ad6f5acb41b34dff6c1f489d8
Atsushi Kanamori <atsushik@microsoft.com>
2017-05-12 08:22:52 -0700

    Delete non-shared version and switch over to shared version.

--- src/mscorlib/src/System/Reflection/AssemblyName.cs
+++ src/mscorlib/src/System/Reflection/AssemblyName.cs
@@ -19,7 +19,6 @@ namespace System.Reflection
     using System;
     using System.IO;
     using System.Configuration.Assemblies;
-    using System.Reflection.Runtime.Assemblies;
     using System.Runtime.CompilerServices;
     using CultureInfo = System.Globalization.CultureInfo;
     using System.Runtime.Serialization;
@@ -282,7 +281,9 @@ namespace System.Reflection
             {
                 if (this.Name == null)
                     return string.Empty;
-                return AssemblyNameHelpers.ComputeDisplayName(this);
+                // Do not call GetPublicKeyToken() here - that latches the result into AssemblyName which isn't a side effect we want.
+                byte[] pkt = _PublicKeyToken ?? nGetPublicKeyToken();
+                return AssemblyNameFormatter.ComputeDisplayName(Name, Version, CultureName, pkt, Flags, ContentType);
             }
         }
 
