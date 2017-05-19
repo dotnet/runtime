5ad6f92123519d16f24135eaef902c80560b7634
Andy Ayers <andya@microsoft.com>
2017-05-19 12:40:15 -0700

    Modify iteration behavior some benchmarks
    This change updates the iteration behavior for the following benchmarks, so that
    they generally run for about 1 second per iteration:
    * Inline/InlineGCStruct
    * Inline/NoThrowInline
    * Span/IndexerBench/Indexer4
    * Functions/MathTests/*
    
    It also removes the length 1, 10, and 1000 variants of the Span/SpanBench
    tests, leaving just the length 100 variants. Analysis has shown that there
    is no need to test varying lengths here. Iteration counts for these tests
    have been left as is and will be fixed subsequently.
    
    Partially addresses dotnet/coreclr#11654.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/c92d35ba6171640fb6ebb34c0f45ebfd8be4182f

--- src/coreclr/tests/src/JIT/Performance/CodeQuality/Math/Functions/Double/SqrtDouble.cs
+++ src/coreclr/tests/src/JIT/Performance/CodeQuality/Math/Functions/Double/SqrtDouble.cs
@@ -14,14 +14,17 @@ namespace Functions
         private const double sqrtDoubleDelta = 0.0006283185307180;
         private const double sqrtDoubleExpectedResult = 5909.0605337797215;
 
-        [Benchmark]
+        [Benchmark(InnerIterationCount = SqrtDoubleIterations)]
         public static void SqrtDoubleBenchmark()
         {
             foreach (var iteration in Benchmark.Iterations)
             {
                 using (iteration.StartMeasurement())
                 {
-                    SqrtDoubleTest();
+                    for (int i = 0; i < Benchmark.InnerIterationCount; i++)
+                    {
+                        SqrtDoubleTest();
+                    }
                 }
             }
         }
