8e62961b8e282c5807a2fdbc320b4fb6bb65f277
Marcin Cieślak <saper@saper.info>
2017-05-25 01:09:05 +0200

    sysctl takes 3 name components (mono/mono#4883)
    Adapted from:
    
    https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=212259
    
    https://github.com/mono/mono/pull/3480
    
    https://svnweb.freebsd.org/ports?view=revision&revision=423356
    
    Commit migrated from https://github.com/mono/mono/commit/c96819eb2561e5b4f2bcd26ec7b6b2e67ee4e361

--- src/mono/mono/utils/mono-proclib.c
+++ src/mono/mono/utils/mono-proclib.c
@@ -131,11 +131,11 @@ mono_process_list (int *size)
 		mib [2] = KERN_PROC_ALL;
 		mib [3] = 0;
 
-		res = sysctl (mib, 4, NULL, &data_len, NULL, 0);
+		res = sysctl (mib, 3, NULL, &data_len, NULL, 0);
 		if (res)
 			return NULL;
 		processes = (struct kinfo_proc *) g_malloc (data_len);
-		res = sysctl (mib, 4, processes, &data_len, NULL, 0);
+		res = sysctl (mib, 3, processes, &data_len, NULL, 0);
 		if (res < 0) {
 			g_free (processes);
 			if (errno != ENOMEM)
