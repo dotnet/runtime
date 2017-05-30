cf4c4b1b78e60c37b0dc27c346323f58d1f5e76c
Mike Voorhees <michaelv@unity3d.com>
2017-05-30 17:29:32 -0400

    Test framework extensibility improvements
    Commit migrated from https://github.com/dotnet/linker/commit/65fd2098ffb981b3162da4399ae07b90306c5270

--- src/tools/linker/linker/Tests/TestCasesRunner/TestRunner.cs
+++ src/tools/linker/linker/Tests/TestCasesRunner/TestRunner.cs
@@ -41,10 +41,10 @@ namespace Mono.Linker.Tests.TestCasesRunner {
 			var sourceFiles = sandbox.SourceFiles.Select(s => s.ToString()).ToArray();
 
 			var references = metadataProvider.GetReferencedAssemblies(sandbox.InputDirectory);
-			var inputAssemblyPath = compiler.CompileTestIn (sandbox.InputDirectory, sourceFiles, references, null);
+			var inputAssemblyPath = compiler.CompileTestIn (sandbox.InputDirectory, "test.exe", sourceFiles, references, null);
 
 			references = metadataProvider.GetReferencedAssemblies(sandbox.ExpectationsDirectory);
-			var expectationsAssemblyPath = compiler.CompileTestIn (sandbox.ExpectationsDirectory, sourceFiles, references, new [] { "INCLUDE_EXPECTATIONS" });
+			var expectationsAssemblyPath = compiler.CompileTestIn (sandbox.ExpectationsDirectory, "test.exe", sourceFiles, references, new [] { "INCLUDE_EXPECTATIONS" });
 			return new ManagedCompilationResult (inputAssemblyPath, expectationsAssemblyPath);
 		}
 
