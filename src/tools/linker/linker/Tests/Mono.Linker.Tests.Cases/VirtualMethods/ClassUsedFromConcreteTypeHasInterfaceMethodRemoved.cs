7f1ff8c0771257cdba59919c3dff162bb5cbf08b
Mike Voorhees <michaelv@unity3d.com>
2017-05-31 10:00:43 -0400

    Add some tests for situation where interface method is removed
    Commit migrated from https://github.com/dotnet/linker/commit/392310da518785c1ed177bbd118780ef89aaa946

---
+++ src/tools/linker/linker/Tests/Mono.Linker.Tests.Cases/VirtualMethods/ClassUsedFromConcreteTypeHasInterfaceMethodRemoved.cs
@@ -0,0 +1,26 @@
+﻿using Mono.Linker.Tests.Cases.Expectations.Assertions;
+
+namespace Mono.Linker.Tests.Cases.VirtualMethods
+{
+	class ClassUsedFromConcreteTypeHasInterfaceMethodRemoved {
+		public static void Main ()
+		{
+			A a = new A ();
+			a.Foo ();
+		}
+
+		[Kept]
+		[KeptInterface (typeof (IFoo))]
+		struct A : IFoo {
+			[Kept]
+			public void Foo ()
+			{
+			}
+		}
+
+		[Kept]
+		public interface IFoo {
+			void Foo ();
+		}
+	}
+}
---
+++ src/tools/linker/linker/Tests/Mono.Linker.Tests.Cases/VirtualMethods/ClassUsedFromInterfaceHasInterfaceMethodKept.cs
@@ -0,0 +1,27 @@
+﻿using Mono.Linker.Tests.Cases.Expectations.Assertions;
+
+namespace Mono.Linker.Tests.Cases.VirtualMethods
+{
+	class ClassUsedFromInterfaceHasInterfaceMethodKept {
+		public static void Main ()
+		{
+			IFoo a = new A ();
+			a.Foo ();
+		}
+
+		[Kept]
+		[KeptInterface (typeof (IFoo))]
+		struct A : IFoo {
+			[Kept]
+			public void Foo ()
+			{
+			}
+		}
+
+		[Kept]
+		public interface IFoo {
+			[Kept]
+			void Foo ();
+		}
+	}
+}
