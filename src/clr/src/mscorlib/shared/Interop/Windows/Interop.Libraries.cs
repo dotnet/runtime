cc8dcad085de7b330fd8b4200a3f5c1f2d60b3a2
Jan Kotas <jkotas@microsoft.com>
2017-05-30 23:22:43 -0700

    Merge pull request dotnet/corertdotnet/coreclr#3745 from dotnet/nmirror
    Merge nmirror to master
    
    Signed-off-by: dotnet-bot <dotnet-bot@microsoft.com>
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/23126aa5b7d6dc7b58301df7d0af1ccdf805224d

--- src/clr/src/mscorlib/shared/Interop/Windows/Interop.Libraries.cs
+++ src/clr/src/mscorlib/shared/Interop/Windows/Interop.Libraries.cs
@@ -10,5 +10,6 @@ internal static partial class Interop
         internal const string Crypt32 = "crypt32.dll";
         internal const string Kernel32 = "kernel32.dll";
         internal const string OleAut32 = "oleaut32.dll";
+        internal const string User32 = "user32.dll";
     }
 }
