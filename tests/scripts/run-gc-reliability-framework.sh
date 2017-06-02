392c4e10e4bbdb16d9e3a0d231433fdc15bca0fb
Sean Gillespie <segilles@microsoft.com>
2017-06-02 10:17:53 -0700

    Return 0 on success from GC Reliability Framework scripts (#11990)
    * Return 0 on success from GC Reliability Framework scripts
    
    * Don't save standard out, Jenkins already does this

--- tests/scripts/run-gc-reliability-framework.sh
+++ tests/scripts/run-gc-reliability-framework.sh
@@ -2,5 +2,19 @@
 
 export CORE_ROOT=`pwd`/bin/tests/Windows_NT.$1.$2/Tests/coreoverlay
 FRAMEWORK_DIR=`pwd`/bin/tests/Windows_NT.$1.$2/GC/Stress/Framework/ReliabilityFramework
-$CORE_ROOT/corerun $FRAMEWORK_DIR/ReliabilityFramework.exe $FRAMEWORK_DIR/testmix_gc.config | tee stdout.txt
+$CORE_ROOT/corerun $FRAMEWORK_DIR/ReliabilityFramework.exe $FRAMEWORK_DIR/testmix_gc.config
+EXIT_CODE=$?
+if [ $EXIT_CODE -eq 100 ]
+then
+    echo "ReliabilityFramework successful"
+    exit 0
+fi
 
+if [ $EXIT_CODE -eq 99 ]
+then
+    echo "ReliabilityFramework test failed, some tests failed"
+    exit 1
+fi
+
+echo "ReliabilityFramework returned a strange exit code $EXIT_CODE, perhaps some config is wrong?"
+exit 1
