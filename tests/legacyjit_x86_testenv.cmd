dfa46929e97aa6e090ad06ea9192b1814291135e
Roman Artemev <rartemev@microsoft.com>
2017-06-12 16:42:50 -0700

    Fix legacyjit CI testing

---
+++ tests/legacyjit_x86_testenv.cmd
@@ -0,0 +1,11 @@
+@REM -------------------------------------------------------------------------
+@REM 
+@REM  This script provides x86 Legacy JIT test environment settings
+@REM
+@REM -------------------------------------------------------------------------
+
+set COMPLUS_AltJit=*
+set COMPLUS_AltJitNgen=*
+set COMPLUS_AltJitName=legacyjit.dll
+set COMPLUS_NoGuiOnAssert=1
+set COMPLUS_AltJitAssertOnNYI=1
