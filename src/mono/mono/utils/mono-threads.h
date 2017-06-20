ea858aefb17a0dfbcba11fdb5eccb1e6cb1b8a05
Alex Rønne Petersen <alexrp@xamarin.com>
2017-06-20 02:49:26 +0200

    [utils/threads] Export small ID functions as part of the API.
    These will be used in the log profiler.
    
    Also, make mono_thread_info_register_small_id () only allocate a new small ID
    if one isn't already allocated for the current thread. This is necessary so
    that we can allocate a small ID for a thread and not have it be overwritten
    if/when that thread is later attached probably (which also results in this
    function being called).
    
    
    Commit migrated from https://github.com/mono/mono/commit/bdfa3ef2cafa954f76a405ce3dc9d6ce39f46d42

--- src/mono/mono/utils/mono-threads.h
+++ src/mono/mono/utils/mono-threads.h
@@ -314,7 +314,7 @@ mono_thread_info_runtime_init (MonoThreadInfoRuntimeCallbacks *callbacks);
 MonoThreadInfoRuntimeCallbacks *
 mono_threads_get_runtime_callbacks (void);
 
-int
+MONO_API int
 mono_thread_info_register_small_id (void);
 
 THREAD_INFO_TYPE *
@@ -341,7 +341,7 @@ mono_thread_info_current (void);
 THREAD_INFO_TYPE*
 mono_thread_info_current_unchecked (void);
 
-int
+MONO_API int
 mono_thread_info_get_small_id (void);
 
 MonoLinkedListSet*
