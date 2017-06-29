9ef69df8350abeb0a36f907bc38b850cbe51e717
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2017-06-28 23:58:34 -0700

    Delete test that tests pinning of System.Object (dotnet/coreclr#12537)
    See dotnet/corefxdotnet/coreclr#21673 for motivation. This test fails on CoreRT.
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/98d19c577c4fdeade912eceef4575e984ec3ce19

--- src/coreclr/tests/src/GC/Features/Pinning/PinningOther/PinnedObject.cs
+++ src/coreclr/tests/src/GC/Features/Pinning/PinningOther/PinnedObject.cs
@@ -3,7 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 // Tests Pinning of objects
-// Cannot pin objects or array of objects
+// Cannot pin array of objects
 
 using System;
 using System.Runtime.InteropServices;
@@ -13,10 +13,6 @@ public class Test
     public static int Main()
     {
         Object[] arr = new Object[100];
-        Object obj = new Object();
-        int exceptionCount = 0;
-
-        Console.WriteLine("This test should throw 2 exceptions");
 
         try
         {
@@ -25,21 +21,6 @@ public class Test
         catch (Exception e)
         {
             Console.WriteLine("Caught: {0}", e);
-            exceptionCount++;
-        }
-
-        try
-        {
-            GCHandle handle2 = GCUtil.Alloc(obj, GCHandleType.Pinned);
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("Caught: {0}", e);
-            exceptionCount++;
-        }
-
-        if (exceptionCount == 2)
-        {
             Console.WriteLine("Test passed");
             return 100;
         }
