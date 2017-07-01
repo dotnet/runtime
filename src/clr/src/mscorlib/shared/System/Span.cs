d998376bbae5cd33f84b9bc98d73fe3295e11649
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2017-06-30 20:04:18 -0700

    Merge pull request dotnet/corertdotnet/coreclr#4046 from dotnet/nmirror
    Merge nmirror to master
    
    Signed-off-by: dotnet-bot <dotnet-bot@microsoft.com>
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/bfc3141f97a9f9f0d095f3d2e99a7c02ce869ab0

--- src/clr/src/mscorlib/shared/System/Span.cs
+++ src/clr/src/mscorlib/shared/System/Span.cs
@@ -22,6 +22,7 @@ namespace System
     /// Span represents a contiguous region of arbitrary memory. Unlike arrays, it can point to either managed
     /// or native memory, or to memory allocated on the stack. It is type- and memory-safe.
     /// </summary>
+    [IsByRefLike]
     public struct Span<T>
     {
         /// <summary>A byref or a native ptr.</summary>
