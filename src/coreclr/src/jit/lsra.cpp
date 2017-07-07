d76b3c83bb019d328bd0c4a140b53eb1e4e68c89
Hyung-Kyu Choi <hk0110.choi@samsung.com>
2017-07-07 14:20:10 +0900

    [RyuJIT/ARM32] Apply review feedback
    Rename vairable and rewrite code to use less #ifdef
    
    Signed-off-by: Hyung-Kyu Choi <hk0110.choi@samsung.com>
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/0acd5abc5725353833f9d6fb14a6fd4290a6c1d4

--- src/coreclr/src/jit/lsra.cpp
+++ src/coreclr/src/jit/lsra.cpp
@@ -5750,17 +5750,14 @@ regNumber LinearScan::tryAllocateFreeReg(Interval* currentInterval, RefPosition*
     {
         if (intervalToUnassign != nullptr)
         {
+            RegRecord* physRegToUnassign = availablePhysRegInterval;
 #ifdef _TARGET_ARM_
-            RegRecord* unassignedPhysReg = availablePhysRegInterval;
             // We should unassign a double register if availablePhysRegInterval is part of the double register
             if (availablePhysRegInterval->assignedInterval->registerType == TYP_DOUBLE &&
                 !genIsValidDoubleReg(availablePhysRegInterval->regNum))
-                unassignedPhysReg = findAnotherHalfRegRec(availablePhysRegInterval);
-
-            unassignPhysReg(unassignedPhysReg, intervalToUnassign->recentRefPosition);
-#else
-            unassignPhysReg(availablePhysRegInterval, intervalToUnassign->recentRefPosition);
+                physRegToUnassign = findAnotherHalfRegRec(availablePhysRegInterval);
 #endif
+            unassignPhysReg(physRegToUnassign, intervalToUnassign->recentRefPosition);
             if (bestScore & VALUE_AVAILABLE)
             {
                 assert(intervalToUnassign->isConstant);
@@ -5770,11 +5767,7 @@ regNumber LinearScan::tryAllocateFreeReg(Interval* currentInterval, RefPosition*
             // the next ref, remember it.
             else if ((bestScore & UNASSIGNED) != 0 && intervalToUnassign != nullptr)
             {
-#ifdef _TARGET_ARM_
-                updatePreviousInterval(unassignedPhysReg, intervalToUnassign, intervalToUnassign->registerType);
-#else
-                updatePreviousInterval(availablePhysRegInterval, intervalToUnassign, intervalToUnassign->registerType);
-#endif
+                updatePreviousInterval(physRegToUnassign, intervalToUnassign, intervalToUnassign->registerType);
             }
         }
         else
