d6d673e8fe982ca6c9a1ef457ae8bf3724e82303
Andi McClure <andi.mcclure@xamarin.com>
2017-07-10 17:01:12 -0400

    Do not major_free_swept_blocks on Orbis. Also, the existing major_free_swept_blocks exemption for Windows is more appropriate for Windows *hosts* than Windows *targets*.
    Commit migrated from https://github.com/mono/mono/commit/f75c72dd2da11dbcf6d8fc128a2c834658c57f09

--- src/mono/mono/sgen/sgen-marksweep.c
+++ src/mono/mono/sgen/sgen-marksweep.c
@@ -2137,7 +2137,7 @@ major_free_swept_blocks (size_t section_reserve)
 {
 	SGEN_ASSERT (0, sweep_state == SWEEP_STATE_SWEPT, "Sweeping must have finished before freeing blocks");
 
-#ifdef TARGET_WIN32
+#if defined(HOST_WIN32) || defined(HOST_ORBIS)
 		/*
 		 * sgen_free_os_memory () asserts in mono_vfree () because windows doesn't like freeing the middle of
 		 * a VirtualAlloc ()-ed block.
