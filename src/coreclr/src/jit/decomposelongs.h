c6701949040953d5713419ccebb2823a3358bcf8
Pat Gavlin <pagavlin@microsoft.com>
2017-07-13 12:59:29 -0700

    Fix VSO 462269.
    When decomposing a long compare on 32-bit platforms, the operands to the
    decomposed compare must be sign- or zero-extended appropriately.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/61dc4c01f046812b1341eecaccdce951f11d02b8

--- src/coreclr/src/jit/decomposelongs.h
+++ src/coreclr/src/jit/decomposelongs.h
@@ -61,6 +61,7 @@ private:
     // Helper functions
     GenTree* FinalizeDecomposition(LIR::Use& use, GenTree* loResult, GenTree* hiResult, GenTree* insertResultAfter);
     GenTree* RepresentOpAsLocalVar(GenTree* op, GenTree* user, GenTree** edge);
+    GenTree* EnsureIntSized(GenTree* node, bool signExtend);
 
     GenTree* StoreNodeToVar(LIR::Use& use);
     static genTreeOps GetHiOper(genTreeOps oper);
