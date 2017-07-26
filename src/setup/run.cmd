84c97ddf290b27b1858b901d5b4bbdff43c9ea35
Wes Haggard <Wes.Haggard@microsoft.com>
2017-07-26 09:52:03 -0700

    Enable building this repo with VS2017
    Commit migrated from https://github.com/dotnet/core-setup/commit/9a20b39067a7fcb979c7421e97c081f8544ae187

--- src/setup/run.cmd
+++ src/setup/run.cmd
@@ -1,15 +1,20 @@
 @if "%_echo%" neq "on" echo off
 setlocal
 
-if not defined VisualStudioVersion (
-  if defined VS140COMNTOOLS (
-    call "%VS140COMNTOOLS%\VsDevCmd.bat"
-    goto :Run
-  )
-  echo Error: Visual Studio 2015 required.
-  echo        Please see https://github.com/dotnet/core-setup/blob/master/Documentation/building/windows-instructions.md for build instructions.
+if defined VisualStudioVersion goto :Run
+
+set _VSWHERE="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+if exist %_VSWHERE% (
+  for /f "usebackq tokens=*" %%i in (`%_VSWHERE% -latest -property installationPath`) do set _VSCOMNTOOLS=%%i\Common7\Tools
+)
+if not exist "%_VSCOMNTOOLS%" set _VSCOMNTOOLS=%VS140COMNTOOLS%
+if not exist "%_VSCOMNTOOLS%" (
+  echo Error: Visual Studio 2015 or 2017 required.
+  echo        Please see https://github.com/dotnet/corefx/blob/master/Documentation/project-docs/developer-guide.md for build instructions.
   exit /b 1
 )
+
+call "%_VSCOMNTOOLS%\VsDevCmd.bat"
 
 :Run
 :: We do not want to run the first-time experience.
