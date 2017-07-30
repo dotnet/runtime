168936a7042ff850db6bd92c5428a3ae848d8ba5
Xiang Fan <xiangfan@microsoft.com>
2017-07-30 09:18:23 -0700

    Remove extra qualification in member function declaration (dotnet/coreclr#13104)
    Fix issues mentioned in https://github.com/dotnet/coreclr/issues/12799
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/760313a0bb72a2bf057ecdde1a36ab54cf50c58d

--- src/clr/src/vm/assemblynative.hpp
+++ src/clr/src/vm/assemblynative.hpp
@@ -92,7 +92,7 @@ public:
     void QCALLTYPE GetType(QCall::AssemblyHandle pAssembly, LPCWSTR wszName, BOOL bThrowOnError, BOOL bIgnoreCase, QCall::ObjectHandleOnStack retType, QCall::ObjectHandleOnStack keepAlive);
 
     static
-    void QCALLTYPE AssemblyNative::GetForwardedType(QCall::AssemblyHandle pAssembly, mdToken mdtExternalType, QCall::ObjectHandleOnStack retType);
+    void QCALLTYPE GetForwardedType(QCall::AssemblyHandle pAssembly, mdToken mdtExternalType, QCall::ObjectHandleOnStack retType);
 
     static 
     INT32 QCALLTYPE GetManifestResourceInfo(QCall::AssemblyHandle pAssembly, LPCWSTR wszName, QCall::ObjectHandleOnStack retAssembly, QCall::StringHandleOnStack retFileName, QCall::StackCrawlMarkHandle stackMark);
