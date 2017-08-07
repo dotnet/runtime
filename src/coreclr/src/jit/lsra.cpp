e71a2e2bed0857cc78960d2459d2b283ff150a09
Hanjoung Lee <hanjoung.lee@samsung.com>
2017-08-07 11:35:21 +0900

    [RyuJIT/armel] Fix ifdef condition and Formatting Error
    Commit migrated from https://github.com/dotnet/coreclr/commit/99e4b01180b7a52a419a3a92c9f7b436489f52b3

--- src/coreclr/src/jit/lsra.cpp
+++ src/coreclr/src/jit/lsra.cpp
@@ -8916,15 +8916,15 @@ void LinearScan::updateMaxSpill(RefPosition* refPosition)
                     ReturnTypeDesc* retTypeDesc = treeNode->AsCall()->GetReturnTypeDesc();
                     typ                         = retTypeDesc->GetReturnRegType(refPosition->getMultiRegIdx());
                 }
-#ifdef _TARGET_ARM_
+#ifdef ARM_SOFTFP
                 else if (treeNode->OperIsPutArgReg())
                 {
                     // For double arg regs, the type is changed to long since they must be passed via `r0-r3`.
                     // However when they get spilled, they should be treated as separated int registers.
                     var_types typNode = treeNode->TypeGet();
-                    typ = (typNode == TYP_LONG) ? TYP_INT : typNode;
+                    typ               = (typNode == TYP_LONG) ? TYP_INT : typNode;
                 }
-#endif // _TARGET_ARM_
+#endif // ARM_SOFTFP
                 else
                 {
                     typ = treeNode->TypeGet();
