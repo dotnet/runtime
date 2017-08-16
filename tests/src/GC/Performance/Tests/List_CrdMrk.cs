724c11739a588312937b0b95d37322f290a6977d
Shiming Ge (Pactera Technologies) <v-shige@microsoft.com>
2017-08-16 00:52:03 -0700

    add MS license

--- tests/src/GC/Performance/Tests/List_CrdMrk.cs
+++ tests/src/GC/Performance/Tests/List_CrdMrk.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
 using System.Collections.Generic;
 
 //Try to allocate in such a way that card marking path is hit often
@@ -69,7 +73,7 @@ namespace List_CrdMrk
                         if (current2.oRef != null)
                         {
                             current2.oRef = null;
-                        }
+                        }2
                         current2 = current.next;
                     }
 
