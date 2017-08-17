84c5943a454da85c303c6f1422e9d072fd448357
Mike Voorhees <michaelv@unity3d.com>
2017-08-17 13:23:13 -0400

    Testing support for adding the KeepTypeForwarderOnlyAssemblies
    KeepTypeForwarder arg handling fix.
    
    
    Commit migrated from https://github.com/dotnet/linker/commit/a8d0bbe7e6503dd1798a99b785d18aaaaea34d28

---
+++ src/tools/illink/linker/Tests/Mono.Linker.Tests.Cases.Expectations/Metadata/KeepTypeForwarderOnlyAssembliesAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mono.Linker.Tests.Cases.Expectations.Metadata {
+	[AttributeUsage (AttributeTargets.Class)]
+	public sealed class KeepTypeForwarderOnlyAssembliesAttribute : BaseMetadataAttribute {
+		public KeepTypeForwarderOnlyAssembliesAttribute (string value)
+		{
+			if (string.IsNullOrEmpty (value))
+				throw new ArgumentException ("Value cannot be null or empty.", nameof (value));
+		}
+	}
+}
