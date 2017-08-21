aa5d2a7cf4de472faaa99a4daaccb41f3e8e0b43
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2017-08-21 15:45:57 -0700

    Use CreateFile2FromApp if available (dotnet/coreclr#13410)
    * Use CreateFile2FromApp if available
    
    This is needed for F5 UAP brokering scenarios.
    
    Remove check for handle type. Overzealous, doesn't apply to UAP scenarios.
    
    * Address feedback
    
    - Use CreateFile2 if Win8 or higher
    - Always use CreateFile2FromApp if Appx
    - Make DisableMediaInsertionPrompt a struct
    - Push CreateFile2FromApp down to avoid JIT
    
    * More feedback
    
    Always use FromApp for WinRT.
    
    * Conditionalize IsWindows8OrAbove check
    
    * Always use FromApp if in AppX
    
    * Address more feedback
    
    - Fix Unix build by moving LoadString to shared
    - Break out CREATEFILE2 struct
    - Condition CreateFile2FromApp usage
    
    * Move include
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/41a2b788cb77668d397e372df40c5215bf61bfa7

--- src/coreclr/src/mscorlib/shared/System/IO/FileStream.WinRT.cs
+++ src/coreclr/src/mscorlib/shared/System/IO/FileStream.WinRT.cs
@@ -9,11 +9,18 @@ namespace System.IO
 {
     public partial class FileStream : Stream
     {
+#if !CORECLR
         private unsafe SafeFileHandle OpenHandle(FileMode mode, FileShare share, FileOptions options)
+        {
+            return CreateFile2OpenHandle(mode, share, options);
+        }
+#endif
+
+        private unsafe SafeFileHandle CreateFile2OpenHandle(FileMode mode, FileShare share, FileOptions options)
         {
             Interop.Kernel32.SECURITY_ATTRIBUTES secAttrs = GetSecAttrs(share);
 
-            int fAccess =
+            int access =
                 ((_access & FileAccess.Read) == FileAccess.Read ? GENERIC_READ : 0) |
                 ((_access & FileAccess.Write) == FileAccess.Write ? GENERIC_WRITE : 0);
 
@@ -30,31 +37,15 @@ namespace System.IO
             parameters.dwFileFlags = (uint)options;
             parameters.lpSecurityAttributes = &secAttrs;
 
-            SafeFileHandle fileHandle = Interop.Kernel32.CreateFile2(
-                lpFileName: _path,
-                dwDesiredAccess: fAccess,
-                dwShareMode: share,
-                dwCreationDisposition: mode,
-                pCreateExParams: &parameters);
-
-            fileHandle.IsAsync = _useAsyncIO;
-
-            if (fileHandle.IsInvalid)
+            using (DisableMediaInsertionPrompt.Create())
             {
-                // Return a meaningful exception with the full path.
-
-                // NT5 oddity - when trying to open "C:\" as a Win32FileStream,
-                // we usually get ERROR_PATH_NOT_FOUND from the OS.  We should
-                // probably be consistent w/ every other directory.
-                int errorCode = Marshal.GetLastWin32Error();
-
-                if (errorCode == Interop.Errors.ERROR_PATH_NOT_FOUND && _path.Length == PathInternal.GetRootLength(_path))
-                    errorCode = Interop.Errors.ERROR_ACCESS_DENIED;
-
-                throw Win32Marshal.GetExceptionForWin32Error(errorCode, _path);
+                return ValidateFileHandle(Interop.FileApiInterop.CreateFile2FromApp(
+                    lpFileName: _path,
+                    dwDesiredAccess: access,
+                    dwShareMode: share,
+                    dwCreationDisposition: mode,
+                    pCreateExParams: ref parameters));
             }
-
-            return fileHandle;
         }
     }
 }
