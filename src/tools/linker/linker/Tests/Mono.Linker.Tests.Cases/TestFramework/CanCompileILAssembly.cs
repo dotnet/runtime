b75d04611baac833565934f4574d0bb09b2c610f
Marek Safar <marek.safar@gmail.com>
2017-08-21 16:27:33 +0200

    Fixes failing IL test
    Commit migrated from https://github.com/dotnet/linker/commit/51804fad6fe911cfa9f219a2ad16e07e15e0fb4f

--- src/tools/linker/linker/Tests/Mono.Linker.Tests.Cases/TestFramework/CanCompileILAssembly.cs
+++ src/tools/linker/linker/Tests/Mono.Linker.Tests.Cases/TestFramework/CanCompileILAssembly.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using Mono.Linker.Tests.Cases.Expectations.Assertions;
 using Mono.Linker.Tests.Cases.Expectations.Metadata;
-using Mono.Linker.Tests.Cases.TestFramework.Dependencies;
 
-namespace Mono.Linker.Tests.Cases.TestFramework {
-	[SetupCompileBefore ("ILAssembly.dll", new[] { "Dependencies/ILAssemblySample.il" })]
-	[KeptMemberInAssembly ("ILAssembly.dll", typeof (ILAssemblySample), "GiveMeAValue()")]
-	public class CanCompileILAssembly {
+namespace Mono.Linker.Tests.Cases.TestFramework
+{
+	[Define ("IL_ASSEMBLY_AVAILABLE")]
+	[SetupCompileBefore ("ILAssembly.dll", new [] { "Dependencies/ILAssemblySample.il" })]
+	[KeptMemberInAssembly ("ILAssembly.dll", "Mono.Linker.Tests.Cases.TestFramework.Dependencies.ILAssemblySample", "GiveMeAValue()")]
+	public class CanCompileILAssembly
+	{
 		static void Main ()
 		{
-			Console.WriteLine (new ILAssemblySample ().GiveMeAValue ());
+#if IL_ASSEMBLY_AVAILABLE
+			Console.WriteLine (new Mono.Linker.Tests.Cases.TestFramework.Dependencies.ILAssemblySample ().GiveMeAValue ());
+#endif
 		}
 	}
 }
