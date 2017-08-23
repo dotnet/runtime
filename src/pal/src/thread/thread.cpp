5f2d1ec0e50a3aceddcc75172021cea10677b354
Jan Vorlicek <janvorli@microsoft.com>
2017-08-23 19:03:16 +0200

    Enable overriding default stack size on Unix (#13517)
    Alpine Linux has a very small default stack size limit, about 80kB.
    This is not enough for running coreclr apps.
    
    This change enables overriding the default stack size using the
    COMPlus_DefaultStackSize env variable. For Alpine, it also sets
    the default stack size to the same value we use for Windows, which
    is 1.5MB.

--- src/pal/src/thread/thread.cpp
+++ src/pal/src/thread/thread.cpp
@@ -636,15 +636,21 @@ CorUnix::InternalCreateThread(
 
     fAttributesInitialized = TRUE;
 
+    if (alignedStackSize == 0)
+    {
+        // The thread is to be created with default stack size. Use the default stack size
+        // override that was determined during the PAL initialization.
+        alignedStackSize = g_defaultStackSize;
+    }
+
     /* adjust the stack size if necessary */
     if (alignedStackSize != 0)
     {
 #ifdef PTHREAD_STACK_MIN
-        const size_t MinStackSize = PTHREAD_STACK_MIN;
+        size_t MinStackSize = ALIGN_UP(PTHREAD_STACK_MIN, GetVirtualPageSize());
 #else // !PTHREAD_STACK_MIN
-        const size_t MinStackSize = 64 * 1024; // this value is typically accepted by pthread_attr_setstacksize()
+        size_t MinStackSize = 64 * 1024; // this value is typically accepted by pthread_attr_setstacksize()
 #endif // PTHREAD_STACK_MIN
-        _ASSERTE(IS_ALIGNED(MinStackSize, GetVirtualPageSize()));
         if (alignedStackSize < MinStackSize)
         {
             // Adjust the stack size to a minimum value that is likely to be accepted by pthread_attr_setstacksize(). If this
