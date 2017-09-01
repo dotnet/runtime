93534f1237907931874dbe6ad0fce83e2da33385
Pankaj Gode <pgode.qdt@qualcommdatacenter.com>
2017-09-01 21:12:26 +0530

    [ARM64/Windows] Add JIT_Stelem_Ref helper (dotnet/coreclr#13687)
    * [ARM64/Windows] Add JIT_Stelem_Ref helper
    
    * [ARM64/Windows] Modified JIT_Stelem_Ref helper to save and restore x0-x2 instead of x0-x8
    
    * [ARM64/Windows] using EPILOG_BRANCH for correct unwind info
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/a741f5f1f4eae22fa2ee42541a9b6851e01cee73

--- src/coreclr/src/vm/arm64/cgencpu.h
+++ src/coreclr/src/vm/arm64/cgencpu.h
@@ -79,8 +79,6 @@ typedef INT64 StackElemType;
 // !! This expression assumes STACK_ELEM_SIZE is a power of 2.
 #define StackElemSize(parmSize) (((parmSize) + STACK_ELEM_SIZE - 1) & ~((ULONG)(STACK_ELEM_SIZE - 1)))
 
-
-#ifdef FEATURE_PAL // TODO-ARM64-WINDOWS Add JIT_Stelem_Ref support
 //
 // JIT HELPERS.
 //
@@ -88,8 +86,6 @@ typedef INT64 StackElemType;
 //
 // optimized static helpers
 #define JIT_Stelem_Ref                      JIT_Stelem_Ref
-#endif
-
 
 //**********************************************************************
 // Frames
