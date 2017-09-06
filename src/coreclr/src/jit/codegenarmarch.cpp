46da66a785ca85a0a6a7ed2e93997a7c2e18004e
sergey ignatov <sergign60@mail.ru>
2017-09-06 04:37:55 +0300

    Fix for CoreRT issue https://github.com/dotnet/corert/issues/3672 (ar… (dotnet/coreclr#13777)
    * Fix for CoreRT issue https://github.com/dotnet/corert/issues/3672 (armel tizen)
    
    * Added assertion for accessType
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/25b6eef888e07f7dd0b0fea873554e786a952bf7

--- src/coreclr/src/jit/codegenarmarch.cpp
+++ src/coreclr/src/jit/codegenarmarch.cpp
@@ -2103,16 +2103,26 @@ void CodeGen::genCallInstruction(GenTreeCall* call)
         void* addr = nullptr;
         if (callType == CT_HELPER)
         {
-            // Direct call to a helper method.
-            CorInfoHelpFunc helperNum = compiler->eeGetHelperNum(methHnd);
-            noway_assert(helperNum != CORINFO_HELP_UNDEF);
-
-            void* pAddr = nullptr;
-            addr        = compiler->compGetHelperFtn(helperNum, (void**)&pAddr);
-
-            if (addr == nullptr)
+// Direct call to a helper method.
+#ifdef FEATURE_READYTORUN_COMPILER
+            if (call->gtEntryPoint.addr != NULL)
             {
-                addr = pAddr;
+                addr = call->gtEntryPoint.addr;
+                assert(call->gtEntryPoint.accessType == IAT_VALUE);
+            }
+            else
+#endif // FEATURE_READYTORUN_COMPILER
+            {
+                CorInfoHelpFunc helperNum = compiler->eeGetHelperNum(methHnd);
+                noway_assert(helperNum != CORINFO_HELP_UNDEF);
+
+                void* pAddr = nullptr;
+                addr        = compiler->compGetHelperFtn(helperNum, (void**)&pAddr);
+
+                if (addr == nullptr)
+                {
+                    addr = pAddr;
+                }
             }
         }
         else
