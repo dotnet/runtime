f02d0b398cc2a0a99817694c5be45068f01ee7fd
Viktor Hofer <viktor.hofer@outlook.com>
2017-09-08 01:01:50 +0200

    PR feedback
    Commit migrated from https://github.com/dotnet/coreclr/commit/f2706ae32e1198e7c5f0cfecb4d83da38802bb16

--- src/coreclr/src/mscorlib/shared/System/UnitySerializationHolder.cs
+++ src/coreclr/src/mscorlib/shared/System/UnitySerializationHolder.cs
@@ -4,26 +4,21 @@
 
 using System.Runtime.Serialization;
 using System.Reflection;
-using System.Diagnostics.Contracts;
 
 namespace System
 {
-    // Holds classes (Empty, Null, Missing) for which we guarantee that there is only ever one instance of.
+    /// <summary>
+    /// Holds Null class for which we guarantee that there is only ever one instance of.
+    /// This only exists for backwarts compatibility with 
+    /// </summary>
 #if CORECLR
     internal
 #else
     public  // On CoreRT, this must be public because of the Reflection.Core/CoreLib divide and the need to whitelist past the ReflectionBlock.
 #endif
-    class UnitySerializationHolder : ISerializable, IObjectReference
+    sealed class UnitySerializationHolder : ISerializable, IObjectReference
     {
         internal const int NullUnity = 0x0002;
-        const string DataName = "Data";
-        const string UnityTypeName = "UnityType";
-        const string AssemblyNameName = "AssemblyName";
-
-        private readonly string _data;
-        private readonly string _assemblyName;
-        private int _unityType;
 
         public static void GetUnitySerializationInfo(SerializationInfo info, int unityType, string data, Assembly assembly)
         {
@@ -33,9 +28,9 @@ namespace System
             // types.)
 
             info.SetType(typeof(UnitySerializationHolder));
-            info.AddValue(DataName, data, typeof(string));
-            info.AddValue(UnityTypeName, unityType);
-            info.AddValue(AssemblyNameName, assembly?.FullName ?? string.Empty);
+            info.AddValue("Data", data, typeof(string));
+            info.AddValue("UnityType", unityType);
+            info.AddValue("AssemblyName", assembly?.FullName ?? string.Empty);
         }
 
         public UnitySerializationHolder(SerializationInfo info, StreamingContext context)
@@ -44,15 +39,16 @@ namespace System
             {
                 throw new ArgumentNullException(nameof(info));
             }
-
-            _unityType = info.GetInt32(UnityTypeName);
-            _data = info.GetString(DataName);
-            _assemblyName = info.GetString(AssemblyNameName);
         }
 
-        public virtual void GetObjectData(SerializationInfo info, StreamingContext context) =>
+        public void GetObjectData(SerializationInfo info, StreamingContext context) =>
             throw new NotSupportedException(SR.NotSupported_UnitySerHolder);
 
-        public virtual object GetRealObject(StreamingContext context) => DBNull.Value;
+        public object GetRealObject(StreamingContext context)
+        {
+            // We are always returning the same DBNull instance and ignoring serialization input.
+
+            return DBNull.Value;
+        }
     }
 }
