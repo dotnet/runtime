881962b76d04b7493e3f9c36c0a2e2be8f4105bb
Rodrigo Kumpera <kumpera@gmail.com>
2017-09-12 10:39:38 -0700

    [verifier] Add test for structs.
    Commit migrated from https://github.com/mono/mono/commit/41be53b107c3e0d0a3d0edaa9cac627083a32be8

--- src/mono/mono/tests/verifier/valid_ref_return.cs
+++ src/mono/mono/tests/verifier/valid_ref_return.cs
@@ -1,5 +1,10 @@
 using System;
 
+struct Point {
+    public int x;
+    public int y;
+}
+
 class Foo {
 	static int X = 10;
 	static int[] Arr = new int[1];
@@ -52,4 +57,21 @@ class Foo {
 	ref int ReturnFunc () {
 		return ref ReturnStatic ();
 	}
+
+    Point mp;
+
+    ref int Pick (bool b, ref Point p) {
+        if (b)
+            return ref p.x;
+        else
+            return ref p.y;
+    }
+
+    void F (bool b) {
+        Point lp = new Point {x = 3, y = 3};
+        ref int z = ref Pick (b, ref lp);
+        z = 4;
+        ref int z2 = ref Pick (b, ref mp);
+        z2 = 5;
+    }
 }
