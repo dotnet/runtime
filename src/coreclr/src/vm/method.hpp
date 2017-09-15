930cd320ebfb2ad58d0eff4ffaef35da418f027d
noahfalk <noahfalk@microsoft.com>
2017-09-14 19:46:07 -0700

    Disable tiered compilation for dynamic methods
    Fixing an oversight from earlier, tiered compilation isn't designed to handle dynamic methods yet because of their limited lifetime. There may be other issues but a basic one would be having the method get collected while its MethodDesc is in the tiered compilation queue which results in use after free memory issues.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/613cd2b251ea86d1091c0e239173d3f4f595e8a5

--- src/coreclr/src/vm/method.hpp
+++ src/coreclr/src/vm/method.hpp
@@ -1244,7 +1244,8 @@ public:
             !IsEnCMethod() &&
             HasNativeCodeSlot() &&
             !IsUnboxingStub() &&
-            !IsInstantiatingStub();
+            !IsInstantiatingStub() &&
+            !IsDynamicMethod();
 
         // We should add an exclusion for modules with debuggable code gen flags
 
