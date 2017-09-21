4ba90e5a40aa338e0e28e62e0926bb122d395876
Alexander Kyte <alexmkyte@gmail.com>
2017-09-21 19:33:47 -0400

    [runtime] Bring jemalloc to mono
    Jemalloc is known to have much better performance than system memory
    allocators, and is safe in concurrent/re-entrant situations.
    
    To use, provide the following options to configure:
    
    --with-jemalloc to enable optionally. To use at runtime,
    export MONO_USE_JEMALLOC to the environment.
    
    --with-jemalloc-always to enable and make the default
    
    --with-jemalloc-assert to enable with assertions to catch memory
    unsafety. It adds overhead, but an overhead that's much less than
    valgrind or asan.
    
    If you export
    MONO_JEMALLOC_CONF="stats_print:true"
    
    You will see detailed memory statistics for mono at exit.
    
    Now note that jemalloc is used through an explicit registration, so
    C libraries called through pinvoke calls will not use jemalloc unless
    they configure themselves to do so separately.
    
    To understand the build work, I'm statically linking jemalloc.a into
    the compilation unit for mono/utils/mono-jemalloc.c, which is
    responsible for registering it with mono at startup. No further
    copying out of the submodule is necessary at installation time.
    
    
    Commit migrated from https://github.com/mono/mono/commit/27d68cef080d5d21090a66bb49b2478cd901c889

--- src/mono/mono/mini/mini.h
+++ src/mono/mono/mini/mini.h
@@ -35,6 +35,7 @@
 #include <mono/utils/mono-threads-coop.h>
 #include <mono/utils/mono-tls.h>
 #include <mono/utils/atomic.h>
+#include <mono/utils/mono-jemalloc.h>
 #include <mono/utils/mono-conc-hashtable.h>
 #include <mono/utils/mono-signal-handler.h>
 
