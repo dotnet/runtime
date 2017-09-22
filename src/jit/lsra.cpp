d4bec111d70761857fcebd122cb36eb8bc383ef6
Steve MacLean <sdmaclea.qdt@qualcommdatacenter.com>
2017-09-22 12:07:43 -0400

    [Arm64] Add GT_JCMP node
    Create new node type GT_JCMP to represent a
    fused Relop + JTrue which does not set flags
    
    Add lowering code to create GT_JCMP when
    Arm64 could use cbz, cbnz, tbz, or tbnz

--- src/jit/lsra.cpp
+++ src/jit/lsra.cpp
@@ -3554,7 +3554,7 @@ static int ComputeOperandDstCount(GenTree* operand)
         // If an operand has no destination registers but does have source registers, it must be a store
         // or a compare.
         assert(operand->OperIsStore() || operand->OperIsBlkOp() || operand->OperIsPutArgStk() ||
-               operand->OperIsCompare() || operand->OperIs(GT_CMP) || operand->IsSIMDEqualityOrInequality());
+               operand->OperIsCompare() || operand->OperIs(GT_CMP, GT_JCMP) || operand->IsSIMDEqualityOrInequality());
         return 0;
     }
     else if (!operand->OperIsFieldListHead() && (operand->OperIsStore() || operand->TypeGet() == TYP_VOID))
