354fc65ba6520b4e5f4ae0a4a2b33bf6c27ca801
Hanjoung Lee <hanjoung.lee@samsung.com>
2017-09-25 20:50:27 +0900

    [RyuJIT/armarch] Put arguments with GT_BITCAST
    Put arguments with GT_BITCAST instead of GT_COPY for arm32/arm64
    
    Fix dotnet/coreclr#14008
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/0603f5b9ab96b20f72d006ab78a9b0c608c034df

--- src/coreclr/src/jit/gtlist.h
+++ src/coreclr/src/jit/gtlist.h
@@ -64,7 +64,11 @@ GTNODE(CMPXCHG          , GenTreeCmpXchg     ,0,GTK_SPECIAL)
 GTNODE(MEMORYBARRIER    , GenTree            ,0,GTK_LEAF|GTK_NOVALUE)
 
 GTNODE(CAST             , GenTreeCast        ,0,GTK_UNOP|GTK_EXOP)      // conversion to another type
+#if !defined(LEGACY_BACKEND) && defined(_TARGET_ARM_)
+GTNODE(BITCAST          , GenTreeMultiRegOp  ,0,GTK_UNOP)               // reinterpretation of bits as another type
+#else
 GTNODE(BITCAST          , GenTreeUnOp        ,0,GTK_UNOP)               // reinterpretation of bits as another type
+#endif
 GTNODE(CKFINITE         , GenTreeOp          ,0,GTK_UNOP|GTK_NOCONTAIN) // Check for NaN
 GTNODE(LCLHEAP          , GenTreeOp          ,0,GTK_UNOP|GTK_NOCONTAIN) // alloca()
 GTNODE(JMP              , GenTreeVal         ,0,GTK_LEAF|GTK_NOVALUE)   // Jump to another function
