c05d97c076f45e3aac0862ee9642fc415547be5c
Sven Boemer <sbomer@gmail.com>
2017-09-26 08:50:15 -0700

    Add native trimming logic
    This adds logic to scan managed dependencies for module references to
    native libraries. The logic is taken from
    Microsoft.Packaging.Tools.Trimming (https://github.com/dotnet/standard/blob/dotnet/linker@9a99f024343e6a36d15e7073890c07daa81606fc/Microsoft.Packaging.Tools.Trimming/docs/trimming.md).
    
    As part of this change, we also stop rewriting the publish dependency
    file, instead specifying the excluded managed and native assets as an
    input to the target in the SDK that generates the deps file in the
    first place.
    
    
    Commit migrated from https://github.com/dotnet/linker/commit/65a0ade30b70c4fe3fadde2c70a1d28560902e24

---
+++ src/tools/illink/corebuild/integration/ILLink.Tasks/ComputeRemovedAssemblies.cs
@@ -0,0 +1,45 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.Build.Utilities;
+using Microsoft.Build.Framework;
+
+namespace ILLink.Tasks
+{
+	public class ComputeRemovedAssemblies : Task
+	{
+		/// <summary>
+		///   The paths to the inputs to the linker.
+		/// </summary>
+		[Required]
+		public ITaskItem[] InputAssemblies { get; set; }
+
+		/// <summary>
+		///   The paths to the linked assemblies.
+		/// </summary>
+		[Required]
+		public ITaskItem[] KeptAssemblies { get; set; }
+
+		/// <summary>
+		///   The set of assemblies in the inputs that weren't kept by
+		///   the linker. These items include the full metadata from
+		///   the input assemblies, and only the filenames of the
+		///   inputs are used to determine which assemblies were
+		///   removed.
+		/// </summary>
+		[Output]
+		public ITaskItem[] RemovedAssemblies { get; set; }
+
+		public override bool Execute()
+		{
+			var keptAssemblyNames = new HashSet<string> (
+				KeptAssemblies.Select(i => Path.GetFileName(i.ItemSpec))
+			);
+			RemovedAssemblies = InputAssemblies.Where(i =>
+				!keptAssemblyNames.Contains(Path.GetFileName(i.ItemSpec))
+			).ToArray();
+			return true;
+		}
+	}
+}
---
+++ src/tools/illink/corebuild/integration/ILLink.Tasks/FindNativeDeps.cs
@@ -0,0 +1,109 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Build.Utilities;
+using Microsoft.Build.Framework;
+using System.Reflection.PortableExecutable;
+using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ILLink.Tasks
+{
+	public class FindNativeDeps : Task
+	{
+		/// <summary>
+		///   The managed assemblies to scan for references to native
+		///   dependencies.
+		/// </summary>
+		[Required]
+		public ITaskItem[] ManagedAssemblyPaths { get; set; }
+
+		/// <summary>
+		///   The set of native dependencies to keep even if they
+		///   aren't found to be referenced by a managed assembly..
+		/// </summary>
+		public ITaskItem[] NativeDepsToKeep { get; set; }
+
+		/// <summary>
+		///   The paths to the available native dependencies. We
+		///   expect that all references found point to existing
+		///   native files.
+		/// </summary>
+		[Required]
+		public ITaskItem[] NativeDepsPaths { get; set; }
+
+		/// <summary>
+		///   The set of native dependencies to keep, including those
+		///   found in the analysis, and those explicitly marked keep
+		///   by NativeDepsToKeep. This includes metadata from the
+		///   input NativeDepsToKeep.
+		/// </summary>
+		[Output]
+		public ITaskItem[] KeptNativeDepsPaths { get; set; }
+
+		public override bool Execute()
+		{
+			var allNative = new Dictionary<string, ITaskItem> ();
+			foreach (var n in NativeDepsPaths)
+			{
+				var fileName = Path.GetFileName(n.ItemSpec);
+				if (!allNative.ContainsKey(fileName))
+				{
+					allNative.Add(fileName, n);
+				}
+			}
+			var keptNative = new List<ITaskItem> ();
+			var managedAssemblies = ManagedAssemblyPaths.Select (i => i.ItemSpec).ToArray();
+			foreach (string managedAssembly in managedAssemblies)
+			{
+				using (var peReader = new PEReader(new FileStream(managedAssembly, FileMode.Open, FileAccess.Read, FileShare.Read)))
+				{
+					if (peReader.HasMetadata)
+					{
+						var reader = peReader.GetMetadataReader();
+						for (int i = 1, count = reader.GetTableRowCount(TableIndex.ModuleRef); i <= count; i++)
+						{
+							var moduleRef = reader.GetModuleReference(MetadataTokens.ModuleReferenceHandle(i));
+							var moduleName = reader.GetString(moduleRef.Name);
+
+							var moduleRefCandidates = new[] { moduleName, moduleName + ".dll", moduleName + ".so", moduleName + ".dylib" };
+
+							ITaskItem referencedNativeFile = null;
+							foreach (string moduleRefCandidate in moduleRefCandidates)
+							{
+								if (allNative.TryGetValue (moduleRefCandidate, out referencedNativeFile))
+								{
+									break;
+								}
+							}
+
+							if (referencedNativeFile != null)
+							{
+								keptNative.Add(referencedNativeFile);
+							}
+							else
+							{
+								// DLLImport that wasn't satisfied
+								Log.LogMessage(MessageImportance.High, "unsatisfied DLLImport: " + managedAssembly + " -> " + moduleName);
+							}
+						}
+					}
+				}
+			}
+
+			var nativeWhitelist = new HashSet<string> (NativeDepsToKeep.Select(i => i.ItemSpec));
+			foreach (var n in NativeDepsToKeep)
+			{
+				ITaskItem nativeFile = null;
+				if (allNative.TryGetValue (n.ItemSpec, out nativeFile))
+				{
+					keptNative.Add(nativeFile);
+				}
+			}
+			KeptNativeDepsPaths = keptNative.ToArray();
+			return true;
+		}
+	}
+}
