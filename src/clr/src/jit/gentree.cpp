cdf96611691aa56b9e6919e05a4941309cc0441f
Andy Ayers <andya@microsoft.com>
2017-09-28 07:56:24 -0700

    JIT: have gtClone handle long constants (dotnet/coreclr#14201)
    The `Enum.HasFlag` opt needs to clone the enum values, and they
    can be long constants. Make sure `gtClone` handles this case.
    
    Closes dotnet/coreclr#14149.
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/ec160a4bed52ac7c6aff3a87c0846c1da228936a

--- src/clr/src/jit/gentree.cpp
+++ src/clr/src/jit/gentree.cpp
@@ -7161,6 +7161,10 @@ GenTreePtr Compiler::gtClone(GenTree* tree, bool complexOK)
             }
             break;
 
+        case GT_CNS_LNG:
+            copy = gtNewLconNode(tree->gtLngCon.gtLconVal);
+            break;
+
         case GT_LCL_VAR:
             // Remember that the LclVar node has been cloned. The flag will be set
             // on 'copy' as well.
@@ -12834,6 +12838,7 @@ GenTree* Compiler::gtOptimizeEnumHasFlag(GenTree* thisOp, GenTree* flagOp)
     if (thisVal->IsIntegralConst())
     {
         thisValOpt = gtClone(thisVal);
+        assert(thisValOpt != nullptr);
     }
     else
     {
@@ -12846,8 +12851,10 @@ GenTree* Compiler::gtOptimizeEnumHasFlag(GenTree* thisOp, GenTree* flagOp)
 
     if (flagVal->IsIntegralConst())
     {
-        flagValOpt     = gtClone(flagVal);
+        flagValOpt = gtClone(flagVal);
+        assert(flagValOpt != nullptr);
         flagValOptCopy = gtClone(flagVal);
+        assert(flagValOptCopy != nullptr);
     }
     else
     {
