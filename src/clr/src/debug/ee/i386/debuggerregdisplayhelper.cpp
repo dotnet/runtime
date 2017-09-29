de64584e51c52334eece41f455f1cf53005c2e85
Konstantin Baladurin <k.baladurin@partner.samsung.com>
2017-09-29 20:15:35 +0300

    [x86/Linux] Fix CopyREGDISPLAY for WIN64EXCEPTIONS (dotnet/coreclr#14254)
    When WIN64EXCEPTIONS is defined, fields pCurrentContextPointers,
    pCallerContextPointers, pCurrentContext and pCallerContext of the
    REGDISPLAY are used. So we need to fix their values after coping.
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/2eed20ce511fa25085742d4aaffc29a3939e84dd

--- src/clr/src/debug/ee/i386/debuggerregdisplayhelper.cpp
+++ src/clr/src/debug/ee/i386/debuggerregdisplayhelper.cpp
@@ -15,4 +15,28 @@
 void CopyREGDISPLAY(REGDISPLAY* pDst, REGDISPLAY* pSrc)
 {
     *pDst = *pSrc;
+
+#ifdef WIN64EXCEPTIONS
+    if (pSrc->pCurrentContextPointers == &(pSrc->ctxPtrsOne))
+    {
+        pDst->pCurrentContextPointers = &(pDst->ctxPtrsOne);
+        pDst->pCallerContextPointers  = &(pDst->ctxPtrsTwo);
+    }
+    else
+    {
+        pDst->pCurrentContextPointers = &(pDst->ctxPtrsTwo);
+        pDst->pCallerContextPointers  = &(pDst->ctxPtrsOne);
+    }
+
+    if (pSrc->pCurrentContext == &(pSrc->ctxOne))
+    {
+        pDst->pCurrentContext = &(pDst->ctxOne);
+        pDst->pCallerContext  = &(pDst->ctxTwo);
+    }
+    else
+    {
+        pDst->pCurrentContext = &(pDst->ctxTwo);
+        pDst->pCallerContext  = &(pDst->ctxOne);
+    }
+#endif
 }
