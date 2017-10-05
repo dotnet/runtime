35996d5d76c178bc4ba9ca157bf818946cdc3922
Stephen Toub <stoub@microsoft.com>
2017-10-05 17:23:41 -0400

    Address perf issues
    Workaround regressions from the span switch and also fix a few other issues that popped while profiling.
    
    - Specialize Compare{String} methods to take one span and one string
    - Ensure frequently called methods get inlined
    - Avoid unnecessary writes to a ref
    - Avoid unnecessary Math.Pow calls
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/92408358a76eb9b908b9e9ce317f2386617f64d1

--- src/coreclr/src/mscorlib/src/System/Globalization/CompareInfo.Unix.cs
+++ src/coreclr/src/mscorlib/src/System/Globalization/CompareInfo.Unix.cs
@@ -143,6 +143,23 @@ namespace System.Globalization
             return Interop.GlobalizationInterop.CompareStringOrdinalIgnoreCase(string1, count1, string2, count2);
         }
 
+        // TODO https://github.com/dotnet/coreclr/issues/13827:
+        // This method shouldn't be necessary, as we should be able to just use the overload
+        // that takes two spans.  But due to this issue, that's adding significant overhead.
+        private unsafe int CompareString(ReadOnlySpan<char> string1, string string2, CompareOptions options)
+        {
+            Debug.Assert(!_invariantMode);
+
+            Debug.Assert(string2 != null);
+            Debug.Assert((options & (CompareOptions.Ordinal | CompareOptions.OrdinalIgnoreCase)) == 0);
+
+            fixed (char* pString1 = &string1.DangerousGetPinnableReference())
+            fixed (char* pString2 = &string2.GetRawStringData())
+            {
+                return Interop.GlobalizationInterop.CompareString(_sortHandle, pString1, string1.Length, pString2, string2.Length, options);
+            }
+        }
+
         private unsafe int CompareString(ReadOnlySpan<char> string1, ReadOnlySpan<char> string2, CompareOptions options)
         {
             Debug.Assert(!_invariantMode);
