7ee6359ec5e3a481d816c10fb001d1b84f368d73
Viktor Hofer <viktor.hofer@outlook.com>
2017-10-05 16:55:25 +0200

    Make more exceptions serializable, fix typeforwards and add one missing serialization field
    Commit migrated from https://github.com/dotnet/coreclr/commit/50da5688dcfbd5f8e15bdcd3cb96d7553595be1a

--- src/coreclr/src/mscorlib/shared/System/Threading/SemaphoreFullException.cs
+++ src/coreclr/src/mscorlib/shared/System/Threading/SemaphoreFullException.cs
@@ -8,7 +8,7 @@ using System.Runtime.Serialization;
 namespace System.Threading
 {
     [Serializable]
-    [System.Runtime.CompilerServices.TypeForwardedFrom("mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089")]
+    [System.Runtime.CompilerServices.TypeForwardedFrom("System, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089")]
     public class SemaphoreFullException : SystemException
     {
         public SemaphoreFullException() : base(SR.Threading_SemaphoreFullException)
