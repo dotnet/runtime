b7307ba700e6e125100f9c0b273d4739fac3723a
Stephen Toub <stoub@microsoft.com>
2017-10-05 17:47:49 -0400

    Add DateTime{Offset} span-based {Try}Parse{Exact} overloads
    Commit migrated from https://github.com/dotnet/coreclr/commit/f87bf901f58edff70a0a3c30fd3c7a1f2965949f

--- src/coreclr/src/mscorlib/shared/System/Globalization/DateTimeFormatInfo.cs
+++ src/coreclr/src/mscorlib/shared/System/Globalization/DateTimeFormatInfo.cs
@@ -454,35 +454,12 @@ namespace System.Globalization
             }
         }
 
-
-        public static DateTimeFormatInfo GetInstance(IFormatProvider provider)
-        {
-            // Fast case for a regular CultureInfo
-            DateTimeFormatInfo info;
-            CultureInfo cultureProvider = provider as CultureInfo;
-            if (cultureProvider != null && !cultureProvider._isInherited)
-            {
-                return cultureProvider.DateTimeFormat;
-            }
-            // Fast case for a DTFI;
-            info = provider as DateTimeFormatInfo;
-            if (info != null)
-            {
-                return info;
-            }
-            // Wasn't cultureInfo or DTFI, do it the slower way
-            if (provider != null)
-            {
-                info = provider.GetFormat(typeof(DateTimeFormatInfo)) as DateTimeFormatInfo;
-                if (info != null)
-                {
-                    return info;
-                }
-            }
-            // Couldn't get anything, just use currentInfo as fallback
-            return CurrentInfo;
-        }
-
+        public static DateTimeFormatInfo GetInstance(IFormatProvider provider) =>
+            provider == null ? CurrentInfo :
+            provider is CultureInfo cultureProvider && !cultureProvider._isInherited ? cultureProvider.DateTimeFormat :
+            provider is DateTimeFormatInfo info ? info :
+            provider.GetFormat(typeof(DateTimeFormatInfo)) is DateTimeFormatInfo info2 ? info2 :
+            CurrentInfo; // Couldn't get anything, just use currentInfo as fallback
 
         public Object GetFormat(Type formatType)
         {
