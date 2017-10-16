8c348368cb518879d9de329c8fbdba32f658070a
Viktor Hofer <viktor.hofer@outlook.com>
2017-10-16 20:29:40 +0200

    Adjusting field name changes in native code

--- src/vm/mscorlib.h
+++ src/vm/mscorlib.h
@@ -1420,9 +1420,9 @@ DEFINE_CLASS(LOADERALLOCATORSCOUT,      Reflection,             LoaderAllocatorS
 DEFINE_CLASS(CONTRACTEXCEPTION,     CodeContracts,  ContractException)
 
 DEFINE_CLASS_U(CodeContracts,       ContractException,          ContractExceptionObject)
-DEFINE_FIELD_U(_Kind,               ContractExceptionObject,    _Kind)
-DEFINE_FIELD_U(_UserMessage,        ContractExceptionObject,    _UserMessage)
-DEFINE_FIELD_U(_Condition,          ContractExceptionObject,    _Condition)
+DEFINE_FIELD_U(_kind,               ContractExceptionObject,    _Kind)
+DEFINE_FIELD_U(_userMessage,        ContractExceptionObject,    _UserMessage)
+DEFINE_FIELD_U(_condition,          ContractExceptionObject,    _Condition)
 
 #ifdef FEATURE_COMINTEROP
 DEFINE_CLASS(ASYNC_TRACING_EVENT_ARGS,       WindowsFoundationDiag,         TracingStatusChangedEventArgs)
