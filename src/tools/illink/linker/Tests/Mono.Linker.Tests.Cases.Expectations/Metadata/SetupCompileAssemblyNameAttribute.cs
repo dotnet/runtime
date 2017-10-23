07f7e55dfc961ca8e356ef6db91207c528f04a60
Mike Voorhees <michaelv@unity3d.com>
2017-10-23 10:24:45 -0400

    Add ability to customize the name of the output assembly in tests
    Commit migrated from https://github.com/dotnet/linker/commit/75f6151c817268ee236139573e5117859e7925f8

---
+++ src/tools/illink/linker/Tests/Mono.Linker.Tests.Cases.Expectations/Metadata/SetupCompileAssemblyNameAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mono.Linker.Tests.Cases.Expectations.Metadata {
+	[AttributeUsage (AttributeTargets.Class, AllowMultiple = false)]
+	public class SetupCompileAssemblyNameAttribute : BaseMetadataAttribute {
+		public SetupCompileAssemblyNameAttribute (string outputName)
+		{
+			if (string.IsNullOrEmpty (outputName))
+				throw new ArgumentNullException (nameof (outputName));
+		}
+	}
+}
