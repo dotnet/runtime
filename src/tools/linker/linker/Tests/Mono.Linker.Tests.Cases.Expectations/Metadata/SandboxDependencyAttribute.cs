250662aae174f3f060a4109fdf824d6621540cef
Mike Voorhees <michaelv@unity3d.com>
2017-10-24 10:19:47 -0400

    Reframe alternate dest as file name only
    Rather than relative path.  The sandboxing code isn't really setup to
    allow files being placed in arbitrary sub directories.
    
    
    Commit migrated from https://github.com/dotnet/linker/commit/c2b6b730408b197db481c6fec752d7f7212c7b94

--- src/tools/linker/linker/Tests/Mono.Linker.Tests.Cases.Expectations/Metadata/SandboxDependencyAttribute.cs
+++ src/tools/linker/linker/Tests/Mono.Linker.Tests.Cases.Expectations/Metadata/SandboxDependencyAttribute.cs
@@ -4,7 +4,7 @@ namespace Mono.Linker.Tests.Cases.Expectations.Metadata {
 	[AttributeUsage (AttributeTargets.Class)]
 	public class SandboxDependencyAttribute : BaseMetadataAttribute {
 
-		public SandboxDependencyAttribute (string relativePathToFile, string relativeDestinationPathToFile = null)
+		public SandboxDependencyAttribute (string relativePathToFile, string destinationFileName = null)
 		{
 			if (string.IsNullOrEmpty (relativePathToFile))
 				throw new ArgumentException ("Value cannot be null or empty.", nameof (relativePathToFile));
