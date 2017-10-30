786a4fdd71d98b82414a639b4f0094d8bab93cf2
Steve Harter <steveharter@users.noreply.github.com>
2017-10-30 12:44:53 -0500

    Support shared hosts other than dotnet.exe (#3356)
    Use argv[0] to calculate own_dir. Only the path portion of argv[0] is used; the file part (expected to be dotnet.exe) is not used in any way other to verify the file exists.

--- src/corehost/cli/args.cpp
+++ src/corehost/cli/args.cpp
@@ -61,8 +61,20 @@ bool parse_arguments(
     const int argc, const pal::char_t* argv[], arguments_t* arg_out)
 {
     arguments_t& args = *arg_out;
+
+    // Try to use argv[0] as own_path to allow for hosts located elsewhere
+    if (argc >= 1)
+    {
+        args.own_path = argv[0];
+        if (!args.own_path.empty() && (!pal::realpath(&args.own_path) || !pal::file_exists(args.own_path)))
+        {
+            trace::warning(_X("Failed to resolve argv[0] as path [%s]. Using location of current executable instead."), args.own_path.c_str());
+            args.own_path.clear();
+        }
+    }
+
     // Get the full name of the application
-    if (!pal::get_own_executable_path(&args.own_path) || !pal::realpath(&args.own_path))
+    if (args.own_path.empty() && (!pal::get_own_executable_path(&args.own_path) || !pal::realpath(&args.own_path)))
     {
         trace::error(_X("Failed to resolve full path of the current executable [%s]"), args.own_path.c_str());
         return false;
