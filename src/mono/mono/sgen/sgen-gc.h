5d5244a8d91d21aa49ad4fe2ece393da29d175b7
Rodrigo Kumpera <kumpera@gmail.com>
2017-11-02 15:25:03 -0700

    [wasm] Workaround for emcc 1.37.22 that miscompiles the inline assembly.
    Commit migrated from https://github.com/mono/mono/commit/675b1904558cd15b8b4ebb71a08b15ec1382aa3a

--- src/mono/mono/sgen/sgen-gc.h
+++ src/mono/mono/sgen/sgen-gc.h
@@ -1138,11 +1138,11 @@ gboolean nursery_canaries_enabled (void);
 static inline void
 sgen_dummy_use (gpointer v)
 {
-#if defined(__GNUC__)
-	__asm__ volatile ("" : "=r"(v) : "r"(v));
-#elif defined(_MSC_VER)
+#if defined(_MSC_VER) || defined(HOST_WASM)
 	static volatile gpointer ptr;
 	ptr = v;
+#elif defined(__GNUC__)
+	__asm__ volatile ("" : "=r"(v) : "r"(v));
 #else
 #error "Implement sgen_dummy_use for your compiler"
 #endif
