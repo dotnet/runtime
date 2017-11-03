67133c7eb8f1c002aceeff844a06760e43bab6a2
Eugene Rozenfeld <erozen@microsoft.com>
2017-11-03 16:05:14 -0700

    Add a switch to specify action on user assemblies.
    Add -u switch to specify action on user (non-core) assemblies similar
    to -c switch for core assemblies.
    
    
    Commit migrated from https://github.com/dotnet/linker/commit/4d2362d8083e3c71560ba1225d468a0080aeea27

--- src/tools/linker/linker/Mono.Linker/Driver.cs
+++ src/tools/linker/linker/Mono.Linker/Driver.cs
@@ -120,6 +120,9 @@ namespace Mono.Linker {
 					case 'c':
 						context.CoreAction = ParseAssemblyAction (GetParam ());
 						break;
+					case 'u':
+						context.UserAction = ParseAssemblyAction (GetParam ());
+						break;
 					case 'p':
 						AssemblyAction action = ParseAssemblyAction (GetParam ());
 						context.Actions [GetParam ()] = action;
@@ -275,6 +278,7 @@ namespace Mono.Linker {
 		{
 			LinkContext context = new LinkContext (pipeline);
 			context.CoreAction = AssemblyAction.Skip;
+			context.UserAction = AssemblyAction.Link;
 			context.OutputDirectory = "output";
 			return context;
 		}
@@ -294,6 +298,7 @@ namespace Mono.Linker {
 			Console.WriteLine ("   --version   Print the version number of the {0}", _linker);
 			Console.WriteLine ("   -out        Specify the output directory, default to `output'");
 			Console.WriteLine ("   -c          Action on the core assemblies, skip, copy or link, default to skip");
+			Console.WriteLine("    -u          Action on the user assemblies, skip, copy or link, default to link");
 			Console.WriteLine ("   -p          Action per assembly");
 			Console.WriteLine ("   -s          Add a new step to the pipeline.");
 			Console.WriteLine ("   -t          Keep assemblies in which only type forwarders are referenced.");
