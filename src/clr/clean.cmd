9ae72120a9e65d1024b30e5edd37f6ce4f891cec
Jacek Blaszczynski <biosciencenow@outlook.com>
2017-11-08 17:57:57 +0100

    Update clean.sh script to reflect changes introduced in clean.cmd, unify messages and functionality
    Commit migrated from https://github.com/dotnet/coreclr/commit/3bc27d58fe180282c582b16d15f70137a9667748

--- src/clr/clean.cmd
+++ src/clr/clean.cmd
@@ -1,6 +1,9 @@
 @if not defined _echo @echo off
 setlocal EnableDelayedExpansion
 
+set NO_DASHES_ARG=%1
+if /I [%NO_DASHES_ARG:-=%] == [?] goto Usage
+if /I [%NO_DASHES_ARG:-=%] == [h] goto Usage
 
 :: Check if VBCSCompiler.exe is running
 tasklist /fi "imagename eq VBCSCompiler.exe" |find ":" > nul
@@ -13,7 +16,6 @@ if errorlevel 1 (
 :: Strip all dashes off the argument and use invariant
 :: compare to match as many versions of "all" that we can
 :: All other argument validation happens inside Run.exe
-set NO_DASHES_ARG=%1
 if not defined NO_DASHES_ARG goto no_args
 if /I [%NO_DASHES_ARG:-=%] == [all] (
   echo Cleaning entire working directory ...
@@ -28,12 +30,13 @@ exit /b %ERRORLEVEL%
 
 :Usage
 echo.
+echo Usage: clean [-b] [-p] [-c] [-all]
 echo Repository cleaning script.
 echo Options:
-echo     -b     - Cleans the bin directory
-echo     -p     - Cleans the packages directory
-echo     -c     - Deletes the user-local nuget package cache.
-echo     -all   - Cleans everything and restores repository to pristine state
+echo     -b     - Delete the binary output directory.
+echo     -p     - Delete the repo-local NuGet package directory.
+echo     -c     - Deletes the user-local NuGet package cache.
+echo     -all   - Cleans repository and restores it to pristine state.
 echo.
-echo If no option is specified then clean.cmd -b is implied.
+echo ^If no option is specified then "clean -b" is implied.
 exit /b
