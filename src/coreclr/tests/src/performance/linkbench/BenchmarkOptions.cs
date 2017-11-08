41f8f56d2277147ce21bcaa052e938f5bb8e30f9
José Rivero <jorive@microsoft.com>
2017-11-08 14:09:14 -0800

    Small number of bug fixes. (dotnet/coreclr#14900)
    - Added stability prefix to the scenario benchmark (JitBench)
    - Specify output directory to the `run-xunit-perf.cmd` script and avoid the extra step to xcopy files to the archive folder.
    - Added a command line parser class to the illink scenario, and changed its behavior where it used to fail when a new command line option passed to xUnit was not recognized.
    - Save the output log of the tests into the sandbox-logs folder.
    - Updating the label of the machine pool used by the illink scenario
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/d0dfd53eb9849f48ecf75418e0ffd2943ffe5012

---
+++ src/coreclr/tests/src/performance/linkbench/BenchmarkOptions.cs
@@ -0,0 +1,121 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using Microsoft.Xunit.Performance.Api;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace LinkBench
+{
+    internal sealed class BenchmarkOptions
+    {
+        [Option("nosetup", Default = true, Required = false, HelpText = "Do not clone and fixup benchmark repositories.")]
+        public bool DoSetup { get; set; } = true;
+
+        [Option("nobuild", Default = true, Required = false, HelpText = "Do not build and link benchmarks.")]
+        public bool DoBuild { get; set; } = true;
+
+        [Option("benchmarks", Required = false, Separator = ',',
+            HelpText = "Any of: HelloWorld, WebAPI, MusicStore, MusicStore_R2R, CoreFX, Roslyn. Default is to run all the above benchmarks.")]
+        public IEnumerable<string> BenchmarkNames
+        {
+            get => _benchmarkNames;
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("Missing benchmark names.");
+
+                if (value.Count() == 0)
+                {
+                    _benchmarkNames = ValidBenchmarkNames;
+                    return;
+                }
+
+                var setDifference = value
+                    .Except(ValidBenchmarkNames, StringComparer.OrdinalIgnoreCase);
+                if (setDifference.Count() != 0)
+                    throw new ArgumentException($"Invalid Benchmark name(s) specified: {string.Join(", ", setDifference)}");
+                _benchmarkNames = value;
+            }
+        }
+
+        private IEnumerable<string> ValidBenchmarkNames => LinkBench.Benchmarks.Select(benchmark => benchmark.Name);
+
+        public static BenchmarkOptions Parse(string[] args)
+        {
+            using (var parser = new Parser((settings) => {
+                settings.CaseInsensitiveEnumValues = true;
+                settings.CaseSensitive = false;
+                settings.HelpWriter = new StringWriter();
+                settings.IgnoreUnknownArguments = true;
+            }))
+            {
+                BenchmarkOptions options = null;
+                parser.ParseArguments<BenchmarkOptions>(args)
+                    .WithParsed(parsed => options = parsed)
+                    .WithNotParsed(errors => {
+                        foreach (var error in errors)
+                        {
+                            switch (error.Tag)
+                            {
+                                case ErrorType.MissingValueOptionError:
+                                    throw new ArgumentException(
+                                            $"Missing value option for command line argument '{(error as MissingValueOptionError).NameInfo.NameText}'");
+                                case ErrorType.HelpRequestedError:
+                                    Console.WriteLine(Usage());
+                                    Environment.Exit(0);
+                                    break;
+                                case ErrorType.VersionRequestedError:
+                                    Console.WriteLine(new AssemblyName(typeof(BenchmarkOptions).GetTypeInfo().Assembly.FullName).Version);
+                                    Environment.Exit(0);
+                                    break;
+                                case ErrorType.BadFormatTokenError:
+                                case ErrorType.UnknownOptionError:
+                                case ErrorType.MissingRequiredOptionError:
+                                    throw new ArgumentException(
+                                            $"Missing required  command line argument '{(error as MissingRequiredOptionError).NameInfo.NameText}'");
+                                case ErrorType.MutuallyExclusiveSetError:
+                                case ErrorType.BadFormatConversionError:
+                                case ErrorType.SequenceOutOfRangeError:
+                                case ErrorType.RepeatedOptionError:
+                                case ErrorType.NoVerbSelectedError:
+                                case ErrorType.BadVerbSelectedError:
+                                case ErrorType.HelpVerbRequestedError:
+                                    break;
+                            }
+                        }
+                    });
+                return options;
+            }
+        }
+
+        public static string Usage()
+        {
+            var parser = new Parser((parserSettings) => {
+                parserSettings.CaseInsensitiveEnumValues = true;
+                parserSettings.CaseSensitive = false;
+                parserSettings.EnableDashDash = true;
+                parserSettings.HelpWriter = new StringWriter();
+                parserSettings.IgnoreUnknownArguments = true;
+            });
+
+            var helpTextString = new HelpText {
+                AddDashesToOption = true,
+                AddEnumValuesToHelpText = true,
+                AdditionalNewLineAfterOption = false,
+                Heading = "LinkBenchHarness",
+                MaximumDisplayWidth = 80,
+            }.AddOptions(parser.ParseArguments<BenchmarkOptions>(new string[] { "--help" })).ToString();
+
+            var sb = new StringBuilder(helpTextString);
+            sb.AppendLine();
+            sb.AppendLine(XunitPerformanceHarness.Usage());
+            return sb.ToString();
+        }
+
+        private IEnumerable<string> _benchmarkNames;
+    }
+}
