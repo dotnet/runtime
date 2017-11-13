fcb588b13fa06f69f4deefac93c024996412923b
Mike Danes <onemihaid@hotmail.com>
2017-11-13 22:36:50 +0200

    Eliminate duplicate SSA number bookkeeping
    During SSA construction SsaRenameState keeps a definition count for each variable in an array. But each variable has a lvPerSsaData array that does almost the same thing, count the definitions. "Almost" because lvPerSsaData is a JitExpandArray that tracks only the array size and not the number of elements actually stored in the array.
    
    Replace JitExpandArray with purposely designed "array" that is in charge with allocating new SSA numbers  and handles their intricacies - RESERVED_SSA_NUM, UNINIT_SSA_NUM and FIRST_SSA_NUM.
    
    This also allows the removal of the allocator from the array. Allocating new SSA numbers happens only during SSA construction and it's reasonable to pass the allocator to AllocSsaNum rather than increasing the size of PerSsaArray and LclVarDsc.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/72a1d0ee7c9f5989edc9f449df24b5b87a5844ce

--- src/coreclr/src/jit/lclvars.cpp
+++ src/coreclr/src/jit/lclvars.cpp
@@ -221,7 +221,7 @@ void Compiler::lvaInitTypeRef()
     memset(lvaTable, 0, tableSize);
     for (unsigned i = 0; i < lvaTableCnt; i++)
     {
-        new (&lvaTable[i], jitstd::placement_t()) LclVarDsc(this); // call the constructor.
+        new (&lvaTable[i], jitstd::placement_t()) LclVarDsc(); // call the constructor.
     }
 
     //-------------------------------------------------------------------------
