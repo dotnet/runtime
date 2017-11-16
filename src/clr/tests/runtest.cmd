8aa60de8b3ece992bdff9e25d19a731b9cb7cd5b
Bruce Forstall <brucefo@microsoft.com>
2017-11-16 15:20:57 -0800

    Clean up netci.groovy (dotnet/coreclr#15030)
    * Clean up netci.groovy
    
    1. Don't repeat entire list of JIT stress and R2R scenarios over and over;
    use functions to check for scenario groups.
    2. Merge handling of R2R baseline and stress scenarios.
    3. Add function to determine if a scenario is a JIT stress scenario.
    4. Fix JIT stress mode display name generator to handle CoreFx tests
    as well, preserving the way they are currently displayed.
    5. Fix R2R display name generator to handle baseline R2R scenario.
    6. Fix bug where x86 R2R baseline job wasn't created.
    7. Fix bugs where triggering some jobs, such as R2R jobs, would trigger lots
    of other jobs with insufficient trigger strings. Thus, more trigger strings
    now have architecture + scenario in them. E.g., "Innerloop" jobs now much
    be triggered using a phrase containing "Innerloop".
    8. Simplify code that creates builds, and test run commands.
    9. Fix mis-casing of COMPlus_JITMinOpts in R2R stress list.
    10. Expand list of valid ARM Windows scenarios to match total list of
    scenarios, but with some scenarios commented out. Add the
    jitstress2 + jitstressregs to the acceptable list. Add note about the
    many scenarios in this list that are not in the full scenario list. Add
    function to determine if a scenario is in this list.
    11. Support crossgen of frameworks using altjit
    12. Don't support 'r2r' scenario on arm/armlb/arm64 Windows.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/793391ad1b0d4cdbbb1a17688f2863e2624fd526

--- src/clr/tests/runtest.cmd
+++ src/clr/tests/runtest.cmd
@@ -66,6 +66,8 @@ set __AgainstPackages=
 set __JitDisasm=
 set __IlasmRoundTrip=
 set __CollectDumps=
+set __DoCrossgen=
+set __CrossgenAltJit=
 
 :Arg_Loop
 if "%1" == "" goto ArgsDone
@@ -93,6 +95,7 @@ if /i "%1" == "TestEnv"               (set __TestEnv=%2&shift&shift&goto Arg_Loo
 if /i "%1" == "AgainstPackages"       (set __AgainstPackages=1&shift&goto Arg_Loop)
 if /i "%1" == "sequential"            (set __Sequential=1&shift&goto Arg_Loop)
 if /i "%1" == "crossgen"              (set __DoCrossgen=1&shift&goto Arg_Loop)
+if /i "%1" == "crossgenaltjit"        (set __DoCrossgen=1&set __CrossgenAltJit=%2&shift&shift&goto Arg_Loop)
 if /i "%1" == "longgc"                (set __LongGCTests=1&shift&goto Arg_Loop)
 if /i "%1" == "gcsimulator"           (set __GCSimulatorTests=1&shift&goto Arg_Loop)
 if /i "%1" == "jitstress"             (set COMPlus_JitStress=%2&shift&shift&goto Arg_Loop)
@@ -214,10 +217,7 @@ if "%__PerfTests%"=="true" goto RunPerfTests
 
 call :ResolveDependecies
 
-if not defined __DoCrossgen goto :SkipPrecompileFX
-call :PrecompileFX
-
-:SkipPrecompileFX
+if defined __DoCrossgen call :PrecompileFX
 
 if  defined __GenerateLayoutOnly (
     REM Delete the unecessary mscorlib.ni file.
@@ -353,7 +353,22 @@ echo %__MsgPrefix%Successfully precompiled and generated dasm for %2
 exit /b 0
 
 :PrecompileFX
+setlocal
+
+if defined __CrossgenAltJit (
+    REM Set altjit flags for the crossgen run. Note that this entire crossgen section is within a setlocal/endlocal scope,
+    REM so we don't need to save or unset these afterwards.
+    echo %__MsgPrefix%Setting altjit environment variables for %__CrossgenAltJit%.
+    set COMPlus_AltJit=*
+    set COMPlus_AltJitNgen=*
+    set COMPlus_AltJitName=%__CrossgenAltJit%
+    set COMPlus_AltJitAssertOnNYI=1
+    set COMPlus_NoGuiOnAssert=1
+    set COMPlus_ContinueOnAssert=0
+)
+
 for %%F in (%CORE_ROOT%\*.dll) do call :PrecompileAssembly "%CORE_ROOT%" "%%F" %%~nF%%~xF
+endlocal
 exit /b 0
 
 :msbuild
@@ -466,6 +481,7 @@ echo AgainstPackages           - This indicates that we are running tests that w
 echo GenerateLayoutOnly        - If specified will not run the tests and will only create the Runtime Dependency Layout
 echo sequential                - Run tests sequentially (no parallelism).
 echo crossgen                  - Precompile ^(crossgen^) the managed assemblies in CORE_ROOT before running the tests.
+echo crossgenaltjit ^<altjit^>   - Precompile ^(crossgen^) the managed assemblies in CORE_ROOT before running the tests, using the given altjit.
 echo link ^<ILlink^>             - Runs the tests after linking via the IL linker ^<ILlink^>.
 echo RunCrossgenTests          - Runs ReadytoRun tests
 echo jitstress ^<n^>             - Runs the tests with COMPlus_JitStress=n
