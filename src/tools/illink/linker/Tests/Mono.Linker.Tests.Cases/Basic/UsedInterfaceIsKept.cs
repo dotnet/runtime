9cbc4065e17b6cf560ef546f9f504c30d612456e
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-11-22 07:29:17 +0100

    Add test to ensure interfaces aren't removed even if not used.
    Commit migrated from https://github.com/dotnet/linker/commit/404f7316f92964bf66d87fdae68a05bd704fd1d9

---
+++ src/tools/illink/linker/Tests/Mono.Linker.Tests.Cases/Basic/UsedInterfaceIsKept.cs
@@ -0,0 +1,24 @@
+﻿using Mono.Linker.Tests.Cases.Expectations.Assertions;
+
+namespace Mono.Linker.Tests.Cases.Basic
+{
+	class UsedInterfaceIsKept
+	{
+		public static void Main ()
+		{
+			A a = new A ();
+		}
+
+		[Kept]
+		[KeptInterface (typeof (I))]
+		[KeptMember (".ctor()")]
+		class A : I
+		{
+		}
+
+		[Kept]
+		interface I
+		{ 
+		}
+	}
+}
