195f56059c1df6a8d546a7dc35f1843b2a34b9a6
Stephen Toub <stoub@microsoft.com>
2017-11-28 22:22:50 -0500

    Address PR feedback
    - Simplify FCALL signatures
    - Simplify mantissa logic copied from corert
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/6bc104ed6d3f3a8e9e584fc92de844845e8d4b06

--- src/coreclr/src/classlibnative/bcltype/number.h
+++ src/coreclr/src/classlibnative/bcltype/number.h
@@ -31,9 +31,9 @@ struct NUMBER {
 class COMNumber
 {
 public:
-    static FCDECL3(void, DoubleToNumberFC, double value, int precision, BYTE* number);
-    static FCDECL2(void, NumberToDoubleFC, BYTE* number, double* value);
-    static FCDECL2(FC_BOOL_RET, NumberBufferToDecimal, BYTE* number, DECIMAL* value);
+    static FCDECL3(void, DoubleToNumberFC, double value, int precision, NUMBER* number);
+    static FCDECL1(double, NumberToDoubleFC, NUMBER* number);
+    static FCDECL2(FC_BOOL_RET, NumberBufferToDecimal, NUMBER* number, DECIMAL* value);
     
     static wchar_t* Int32ToDecChars(__in wchar_t* p, unsigned int value, int digits);
 };
