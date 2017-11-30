610ba10c7c0c79b002ad4bbbcfa550c96935c399
Mike Danes <onemihaid@hotmail.com>
2017-11-30 23:47:03 +0200

    Remove useless IndirectAssignmentAnnotation
    This annotation is supposed to be used by SSA and VN but that does not actually happen because fgMorphCopyBlock also marks the destination local as address exposed.
    
    Also, even if the local wasn't address exposed there are other parts of the JIT that should probably use this annotation but currently they do not:
    * Copy propagation attempts to mirror SSA renaming but it ignores IndirectAssignmentAnnotation.
    * Liveness also doesn't pay attention to IndirectAssignmentAnnotation. A comment in fgMorphCopyBlock even gives liveness as the reason why the destination local is address exposed.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/026abe216b39550ab6acb80ecb8927904e194886

--- src/clr/src/jit/compiler.cpp
+++ src/clr/src/jit/compiler.cpp
@@ -2070,7 +2070,6 @@ void Compiler::compInit(ArenaAllocator* pAlloc, InlineInfo* inlineInfo)
 
     vnStore               = nullptr;
     m_opAsgnVarDefSsaNums = nullptr;
-    m_indirAssignMap      = nullptr;
     fgSsaPassesCompleted  = 0;
     fgVNPassesCompleted   = 0;
 
