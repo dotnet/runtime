5ffcbae7187fdc527fd2b16a1c44f190e96de444
Unknown <michaelv@unity3d.com>
2017-12-05 10:57:25 -0500

    Add test to verify the default user assembly action
    Commit migrated from https://github.com/dotnet/linker/commit/2ba517aee2fb69c5d6fcc4b3595ca448a09fca67

---
+++ src/tools/illink/linker/Tests/Mono.Linker.Tests.Cases/References/Dependencies/UserAssembliesAreLinkedByDefault_Library1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Mono.Linker.Tests.Cases.References.Dependencies {
+	public class UserAssembliesAreLinkedByDefault_Library1 {
+		public void UsedMethod ()
+		{
+			Console.WriteLine ("Used");
+		}
+
+		public void UnusedMethod ()
+		{
+			Console.WriteLine ("NotUsed");
+		}
+	}
+}
