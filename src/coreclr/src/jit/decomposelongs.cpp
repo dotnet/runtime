5092da3db4ce3e48460148c17befd50174b0a9c5
Sergey Andreenko <seandree@microsoft.com>
2017-12-11 13:48:55 -0800

    [RyuJit][x86] another forgotten case with unused flag in decomposition.
    rotate nodes are rare, so it why we did not see it.
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/c903f7bffdf606b728cab45e4fe14530d82d2ac8

--- src/coreclr/src/jit/decomposelongs.cpp
+++ src/coreclr/src/jit/decomposelongs.cpp
@@ -1553,6 +1553,11 @@ GenTree* DecomposeLongs::DecomposeRotate(LIR::Use& use)
         gtLong->gtOp.gtOp1 = loResult;
         gtLong->gtOp.gtOp2 = hiResult;
 
+        if (tree->IsUnusedValue())
+        {
+            gtLong->SetUnusedValue();
+        }
+
         GenTree* next = tree->gtNext;
         // Remove tree and don't do anything else.
         Range().Remove(tree);
