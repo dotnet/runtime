5e22e3c1dd2641ed935c57becc4418f316c29e86
Unknown <michaelv@unity3d.com>
2017-12-12 11:27:49 -0500

    Add test to verify pattern matched type in xml
    Commit migrated from https://github.com/dotnet/linker/commit/77d8b63c13e0610fe24d1b915cfa9512ff2c9e4b

---
+++ src/tools/illink/linker/Tests/Mono.Linker.Tests.Cases/LinkXml/CanPreserveTypesUsingRegex.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
+
+namespace Mono.Linker.Tests.Cases.LinkXml
+{
+	[KeptMember (".ctor()")]
+	class CanPreserveTypesUsingRegex {
+		public static void Main () {
+		}
+
+		[Kept]
+		void UnusedHelper () {
+		}
+
+		[Kept]
+		[KeptMember (".ctor()")]
+		class Bar {
+		}
+	}
+}
