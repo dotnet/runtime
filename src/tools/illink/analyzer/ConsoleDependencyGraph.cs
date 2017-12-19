afbaa0b158edfe3e672b2ced5e3237b618263efb
Marek Safar <marek.safar@gmail.com>
2017-12-19 10:41:23 +0100

    Moves linker analyzer to linker repo (from Mono repo)
    Commit migrated from https://github.com/dotnet/linker/commit/2e5c599841bf8bdf22299c663233d272ca80ff60

---
+++ src/tools/illink/analyzer/ConsoleDependencyGraph.cs
@@ -0,0 +1,176 @@
+﻿//
+// ConsoleDependencyGraph.cs: text output related code for dependency graph
+//
+// Author:
+//   Radek Doulik (rodo@xamarin.com)
+//
+// Copyright 2015 Xamarin Inc (http://www.xamarin.com).
+//
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using LinkerAnalyzer.Core;
+
+namespace LinkerAnalyzer
+{
+	public class ConsoleDependencyGraph : DependencyGraph
+	{
+		public bool Tree = false;
+		public bool FlatDeps;
+
+		public void ShowDependencies (string raw, List<VertexData> verticesList, string searchString)
+		{
+			VertexData vertex = Vertex (raw);
+			if (vertex == null) {
+				Regex regex = new Regex (searchString);
+				int count = 0;
+
+				foreach (var v in verticesList) {
+					if (regex.Match (v.value) != Match.Empty) {
+						ShowDependencies (v);
+						count++;
+					}
+				}
+
+				if (count == 0)
+					Console.WriteLine ("\nUnable to find vertex: {0}", raw);
+				else
+					Console.WriteLine ("\nFound {0} matches", count);
+			} else
+				ShowDependencies (vertex);
+		}
+
+		void ShowFlatDependencies (VertexData vertex)
+		{
+			bool first = true;
+			var flatDeps = GetAllDependencies (vertex);
+
+			Console.WriteLine ();
+
+			foreach (var d in flatDeps) {
+				if (first) {
+					Console.WriteLine ($"Distance | {d.Item1.value} [total deps: {flatDeps.Count}]");
+					Line ();
+					first = false;
+					continue;
+				}
+				Console.WriteLine ($"{string.Format ("{0,8}", d.Item2)} | {d.Item1.value}");
+			}
+		}
+
+		public void ShowDependencies (VertexData vertex)
+		{
+			if (FlatDeps) {
+				ShowFlatDependencies (vertex);
+
+				return;
+			}
+
+			Header ("{0} dependencies", vertex.value);
+			if (vertex.parentIndexes == null) {
+				Console.WriteLine ("Root dependency");
+			} else {
+				int i = 0;
+				foreach (int index in vertex.parentIndexes) {
+					Console.WriteLine ("Dependency #{0}", ++i);
+					Console.WriteLine ("\t{0}", vertex.value);
+					var childVertex = Vertex (index);
+					Console.WriteLine ("\t| {0}{1}", childVertex.value, childVertex.DepsCount);
+					while (childVertex.parentIndexes != null) {
+						childVertex = Vertex (childVertex.parentIndexes [0]);
+						Console.WriteLine ("\t| {0}{1}", childVertex.value, childVertex.DepsCount);
+					}
+					if (Tree)
+						break;
+				}
+			}
+		}
+
+		public void ShowAllDependencies ()
+		{
+			Header ("All dependencies");
+			Console.WriteLine ("Types count: {0}", vertices.Count);
+			foreach (var vertex in vertices)
+				ShowDependencies (vertex);
+		}
+
+		public void ShowTypesDependencies ()
+		{
+			Header ("All types dependencies");
+			Console.WriteLine ("Deps count: {0}", Types.Count);
+			foreach (var type in Types)
+				ShowDependencies (type);
+		}
+
+		string Tabs (string key)
+		{
+			int count = Math.Max (1, 2 - key.Length / 8);
+
+			if (count == 1)
+				return "\t";
+			else
+				return "\t\t";
+		}
+
+		public void ShowStat (bool verbose = false)
+		{
+			Header ("Statistics");
+			if (verbose) {
+				foreach (var key in counts.Keys)
+					Console.WriteLine ("Vertex type:\t{0}{1}count:{2}", key, Tabs (key), counts [key]);
+			} else {
+				Console.WriteLine ("Assemblies:\t{0}", counts ["Assembly"]);
+				Console.WriteLine ("Modules:\t{0}", counts ["Module"]);
+				Console.WriteLine ("Types:\t\t{0}", counts ["TypeDef"]);
+				Console.WriteLine ("Fields:\t\t{0}", counts ["Field"]);
+				Console.WriteLine ("Methods:\t{0}", counts ["Method"]);
+			}
+
+			Console.WriteLine ();
+			Console.WriteLine ("Total vertices: {0}", vertices.Count);
+		}
+
+		public void ShowRoots ()
+		{
+			Header ("Root vertices");
+
+			int count = 0;
+			foreach (var vertex in vertices) {
+				if (vertex.parentIndexes == null) {
+					Console.WriteLine ("{0}", vertex.value);
+					count++;
+				}
+			}
+
+			Console.WriteLine ();
+			Console.WriteLine ("Total root vertices: {0}", count);
+		}
+
+		public void ShowRawDependencies (string raw)
+		{
+			Header ("Raw dependencies: '{0}'", raw);
+			ShowDependencies (raw, vertices, raw);
+		}
+
+		public void ShowTypeDependencies (string raw)
+		{
+			Header ("Type dependencies: '{0}'", raw);
+			ShowDependencies ("TypeDef:" + raw, Types, raw);
+		}
+
+		static readonly string line = new string ('-', 72);
+
+		void Line ()
+		{
+			Console.Write (line);
+			Console.WriteLine ();
+		}
+
+		void Header (string header, params object[] values)
+		{
+			string formatted = string.Format (header, values);
+			Console.WriteLine ();
+			Console.WriteLine ($"--- {formatted} {new string ('-', Math.Max (3, 67 - formatted.Length))}");
+		}
+	}
+}
---
+++ src/tools/illink/analyzer/LinkerAnalyzerCore/DependencyGraph.cs
@@ -0,0 +1,141 @@
+﻿//
+// DependencyGraph.cs: linker dependencies graph
+//
+// Author:
+//   Radek Doulik (rodo@xamarin.com)
+//
+// Copyright 2015 Xamarin Inc (http://www.xamarin.com).
+//
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Xml;
+
+namespace LinkerAnalyzer.Core
+{
+	public class VertexData {
+		public string value;
+		public List<int> parentIndexes;
+		public int index;
+
+		public string DepsCount {
+			get {
+				if (parentIndexes == null || parentIndexes.Count < 1)
+					return "";
+				return string.Format (" [{0} deps]", parentIndexes.Count);
+			}
+		}
+	};
+
+	public class DependencyGraph
+	{
+		protected List<VertexData> vertices = new List<VertexData> ();
+		public List<VertexData> Types = new List<VertexData> ();
+		Dictionary<string, int> indexes = new Dictionary<string, int> ();
+		protected Dictionary<string, int> counts = new Dictionary<string, int> ();
+
+		public void Load (string filename)
+		{
+			Console.WriteLine ("Loading dependency tree from: {0}", filename);
+
+			try {
+				using (var fileStream = File.OpenRead (filename))
+				using (var zipStream = new GZipStream (fileStream, CompressionMode.Decompress)) {
+					Load (zipStream);
+				}
+			} catch (Exception) {
+				Console.WriteLine ("Unable to open and read the dependecies.");
+				Environment.Exit (1);
+			}
+		}
+
+		void Load (GZipStream zipStream) {
+			using (XmlReader reader = XmlReader.Create (zipStream)) {
+				while (reader.Read ()) {
+					switch (reader.NodeType) {
+					case XmlNodeType.Element:
+						//Console.WriteLine (reader.Name);
+						if (reader.Name == "edge" && reader.IsStartElement ()) {
+							string b = reader.GetAttribute ("b");
+							string e = reader.GetAttribute ("e");
+							//Console.WriteLine ("edge value " + b + "  -->  " + e);
+
+							if (e != b) {
+								VertexData begin = Vertex (b, true);
+								VertexData end = Vertex (e, true);
+
+								if (end.parentIndexes == null)
+									end.parentIndexes = new List<int> ();
+								if (!end.parentIndexes.Contains (begin.index)) {
+									end.parentIndexes.Add (begin.index);
+									//Console.WriteLine (" end parent index: {0}", end.parentIndexes);
+								}
+							}
+						}
+						break;
+					default:
+						//Console.WriteLine ("node: " + reader.NodeType);
+						break;
+					}
+				}
+			}
+		}
+
+		public VertexData Vertex (string vertexName, bool create = false)
+		{
+			VertexData vertex;
+
+			try {
+				vertex = vertices [indexes [vertexName]];
+			} catch (KeyNotFoundException) {
+				if (create) {
+					int index = vertices.Count;
+					vertex = new VertexData () { value = vertexName, index = index };
+					vertices.Add (vertex);
+					indexes.Add (vertexName, index);
+					string prefix = vertexName.Substring (0, vertexName.IndexOf (':'));
+					if (counts.ContainsKey (prefix))
+						counts [prefix]++;
+					else
+						counts [prefix] = 1;
+					//Console.WriteLine ("prefix " + prefix + " count " + counts[prefix]);
+					if (prefix == "TypeDef") {
+						Types.Add (vertex);
+					}
+				} else
+					return null;
+			}
+
+			return vertex;
+		}
+
+		public VertexData Vertex (int index)
+		{
+			return vertices [index];
+		}
+
+		IEnumerable<Tuple<VertexData, int>> AddDependencies (VertexData vertex, HashSet<int> reachedVertices, int depth)
+		{
+			reachedVertices.Add (vertex.index);
+			yield return new Tuple<VertexData, int> (vertex, depth);
+
+			if (vertex.parentIndexes == null)
+				yield break;
+
+			foreach (var pi in vertex.parentIndexes) {
+				var parent = Vertex (pi);
+				if (reachedVertices.Contains (parent.index))
+					continue;
+
+				foreach (var d in AddDependencies (parent, reachedVertices, depth + 1))
+					yield return d;
+			}
+		}
+
+		public List<Tuple<VertexData, int>> GetAllDependencies (VertexData vertex)
+		{
+			return new List<Tuple<VertexData, int>> (AddDependencies (vertex, new HashSet<int> (), 0));
+		}
+	}
+}
