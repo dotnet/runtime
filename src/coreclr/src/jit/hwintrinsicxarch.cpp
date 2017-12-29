8c408212ec79cb17fc694e7198cb5248656ed9d9
Tanner Gooding <tagoo@outlook.com>
2017-12-28 17:55:50 -0800

    Adding support for the SSE compare eq, gt, ge, lt, le, ne, ord, and unord scalar intrinsics
    Commit migrated from https://github.com/dotnet/coreclr/commit/eaf9aefffcda0d588147c2a244182d4c34c83a0b

--- src/coreclr/src/jit/hwintrinsicxarch.cpp
+++ src/coreclr/src/jit/hwintrinsicxarch.cpp
@@ -525,17 +525,29 @@ GenTree* Compiler::impSSEIntrinsic(NamedIntrinsic        intrinsic,
         case NI_SSE_And:
         case NI_SSE_AndNot:
         case NI_SSE_CompareEqual:
+        case NI_SSE_CompareEqualScalar:
         case NI_SSE_CompareGreaterThan:
+        case NI_SSE_CompareGreaterThanScalar:
         case NI_SSE_CompareGreaterThanOrEqual:
+        case NI_SSE_CompareGreaterThanOrEqualScalar:
         case NI_SSE_CompareLessThan:
+        case NI_SSE_CompareLessThanScalar:
         case NI_SSE_CompareLessThanOrEqual:
+        case NI_SSE_CompareLessThanOrEqualScalar:
         case NI_SSE_CompareNotEqual:
+        case NI_SSE_CompareNotEqualScalar:
         case NI_SSE_CompareNotGreaterThan:
+        case NI_SSE_CompareNotGreaterThanScalar:
         case NI_SSE_CompareNotGreaterThanOrEqual:
+        case NI_SSE_CompareNotGreaterThanOrEqualScalar:
         case NI_SSE_CompareNotLessThan:
+        case NI_SSE_CompareNotLessThanScalar:
         case NI_SSE_CompareNotLessThanOrEqual:
+        case NI_SSE_CompareNotLessThanOrEqualScalar:
         case NI_SSE_CompareOrdered:
+        case NI_SSE_CompareOrderedScalar:
         case NI_SSE_CompareUnordered:
+        case NI_SSE_CompareUnorderedScalar:
         case NI_SSE_Divide:
         case NI_SSE_DivideScalar:
         case NI_SSE_Max:
