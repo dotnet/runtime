ae61c45864f03eca591d53f8a6d9075e6fb8544d
Koundinya Veluri <kouvel@users.noreply.github.com>
2018-01-04 17:34:35 -0800

    Change monitor test (dotnet/coreclr#15727)
    Relevant to https://github.com/dotnet/coreclr/issues/15592:
    - Not sure what is actually happening, guessing there is an exception during TryEnter() and the exception from Exit() from the finally block is hiding the actual exception
    - Moved TryEnter() to before the try block
    - Fixed pass/fail condition for some similar monitor tests
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/b5a755ef274fa182cd17317208be4cb4dc3896de

--- src/coreclr/tests/src/baseservices/threading/generics/Monitor/TryEnter06.cs
+++ src/coreclr/tests/src/baseservices/threading/generics/Monitor/TryEnter06.cs
@@ -44,7 +44,7 @@ struct Gen<T>
 			if(myHelper.Error == true || myHelper2.Error == true)
 				break;
 		}
-		Test.Eval(!(myHelper.Error && myHelper2.Error));
+		Test.Eval(!(myHelper.Error || myHelper2.Error));
 		
 	}
 
