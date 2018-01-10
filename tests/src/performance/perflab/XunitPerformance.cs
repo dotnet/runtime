75385b1fa21f14076f05ca1045fc487d09337064
Andy Ayers <andya@microsoft.com>
2018-01-09 16:25:22 -0800

    Update perflab tests to execute in a fixed order
    Goal is to pin class init overhead on one test consistently. Also we were
    not picking up the "optimize for benchmark" attribute and so likely risking
    having tests execute concurrently.

---
+++ tests/src/performance/perflab/XunitPerformance.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Xunit.Performance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+[assembly: OptimizeForBenchmarks]
+
+// Constrain tests to execute in a predetermined order
+[assembly: TestCaseOrderer("PerfLabTests.DisplayNameTestCaseOrderer", "PerfLab")]
+
+namespace PerfLabTests
+{
+
+    public class DisplayNameTestCaseOrderer : ITestCaseOrderer
+    {
+        public IEnumerable<TTestCase> OrderTestCases<TTestCase>(IEnumerable<TTestCase> testCases)
+        where TTestCase : ITestCase
+        => testCases.OrderBy(test => test.DisplayName);
+    }
+
+}
