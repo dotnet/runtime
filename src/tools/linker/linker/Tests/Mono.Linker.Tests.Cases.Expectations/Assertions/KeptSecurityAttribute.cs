158bd91158bd9c432e081a455df96b4214e32153
Unknown <michaelv@unity3d.com>
2018-01-11 13:30:52 -0500

    Implement support for asserting security attributes in tests
    Commit migrated from https://github.com/dotnet/linker/commit/a2ada3d869ed54fa8e7e40d9072fb5b4e6aa2b7f

---
+++ src/tools/linker/linker/Tests/Mono.Linker.Tests.Cases.Expectations/Assertions/KeptSecurityAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Mono.Linker.Tests.Cases.Expectations.Assertions {
+	[AttributeUsage (AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Assembly | AttributeTargets.Method, AllowMultiple = true, Inherited = false)]
+	public class KeptSecurityAttribute : KeptAttribute {
+		public KeptSecurityAttribute (string attributeName)
+		{
+			if (string.IsNullOrEmpty (attributeName))
+				throw new ArgumentException ("Value cannot be null or empty.", nameof (attributeName));
+		}
+
+		public KeptSecurityAttribute (Type type)
+		{
+			if (type == null)
+				throw new ArgumentNullException (nameof (type));
+		}
+	}
+}
---
+++ src/tools/linker/linker/Tests/Mono.Linker.Tests.Cases/Attributes/SecurityAttributesOnUsedMethodAreKept.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Permissions;
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
+
+namespace Mono.Linker.Tests.Cases.Attributes {
+	class SecurityAttributesOnUsedMethodAreKept {
+		static void Main ()
+		{
+			new Foo ().Method ();
+		}
+
+		[Kept]
+		[KeptMember (".ctor()")]
+		class Foo {
+			[SecurityPermission (SecurityAction.LinkDemand)]
+			[Kept]
+			[KeptSecurity (typeof (SecurityPermissionAttribute))]
+			public void Method ()
+			{
+			}
+		}
+	}
+}
