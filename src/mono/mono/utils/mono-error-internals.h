501a22343ed9dd559bed6a3f8bad52dac717aae1
Jay Krell <jaykrell@microsoft.com>
2018-01-14 01:37:34 -0800

    Support for removing ampersand on MonoError variables and other preparation for MonoError cleanup (specific use-cases instead of one error_init).
    Commit migrated from https://github.com/mono/mono/commit/111786f94431638f5e932c8b7fae51c2ace5fd6b

--- src/mono/mono/utils/mono-error-internals.h
+++ src/mono/mono/utils/mono-error-internals.h
@@ -43,11 +43,71 @@ struct _MonoErrorBoxed {
 	MonoImage *image;
 };
 
-// Initial version for easier to read history.
-#define ERROR_DECL(name) \
-	MonoError name
+/*
+Historically MonoError initialization was deferred, but always had to occur,
+	even in success paths, as cleanup could be done unconditionally.
+	This was confusing.
 
-#define error_init(error) ((void)((error)->init = 0))
+ERROR_DECL (error)
+	This is the overwhelmingly common case.
+	Declare and initialize a local variable, named "error",
+	pointing to an initialized MonoError (named "error_value",
+	using token pasting).
+
+ERROR_DECL_VALUE (foo)
+	Declare and initialize a local variable, named "foo";
+	no pointer is produced for it.
+
+MONO_API_ERROR_INIT
+	This is used for MonoError in/out parameter on a public interface,
+	which must be presumed uninitialized. These are often
+	marked with MONO_API, MONO_RT_EXTERNAL_ONLY, MONO_PROFILER_API, etc.
+	Tnis includes functions called from dis, profiler, pedump, and driver.
+	dis, profiler, and pedump make sense, these are actually external and
+	uninitialized. Driver less so.
+
+error_init
+	Initialize a MonoError. These are historical and usually
+	but not always redundant, and should be reduced/eliminated.
+	All the non-redundant ones should be renamed and all the redundant
+	ones removed.
+
+error_init_reuse
+	This indicates an error has been cleaned up and will be reused.
+	Consider also changing mono_error_cleanup to call error_init_internal,
+	and then remove these.
+
+error_init_internal
+	Rare cases without a better name.
+	For example, setting up an icall frame, or initializing member data.
+
+new0, calloc, static
+	A zeroed MonoError is valid and initialized.
+	Zeroing an entire MonoError is overkill, unless it is near other
+	bulk zeroing.
+
+All initialization is actually bottlenecked to error_init_internal.
+Different names indicate different scenarios, but the same code.
+*/
+#define ERROR_DECL_VALUE(x) 		MonoError x; error_init_internal (&x)
+#define ERROR_DECL(x) 			ERROR_DECL_VALUE (x##_value); MonoError * const x = &x##_value
+#define error_init_internal(error) 	((void)((error)->init = 0))
+#define MONO_API_ERROR_INIT(error) 	error_init_internal (error)
+#define error_init_reuse(error) 	error_init_internal (error)
+
+// Historical deferred initialization was called error_init.
+
+// possible bug detection that did not work
+//#define error_init(error) (is_ok (error))
+
+// FIXME Eventually all error_init should be removed, however it is prudent
+// to leave them in for now, at least most of them, while we sort out
+// the few that are needed and to experiment with adding them back in bulk,
+// i.e. in an entire source file. Some are obviously not needed.
+//#define error_init(error) // nothing
+#define error_init(error) error_init_internal (error)
+// Function for experimentation, should go away.
+//void error_init(MonoError*);
 
 #define is_ok(error) ((error)->error_code == MONO_ERROR_NONE)
 
