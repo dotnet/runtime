9891451941747155f3f375ee46f00f55670d5ff5
Marek Safar <marek.safar@gmail.com>
2018-01-16 17:00:27 +0100

    Add missing checks for property variants when looking for property backing field.
    Fixes dotnet/linker#213
    
    
    Commit migrated from https://github.com/dotnet/linker/commit/8998e8c7798e8fecd93866aacd50e7746a83d31c

---
+++ src/tools/illink/linker/Tests/Mono.Linker.Tests.Cases/LinkXml/PreserveBackingFieldWhenPropertyIsKept.cs
@@ -0,0 +1,18 @@
+﻿using Mono.Linker.Tests.Cases.Expectations.Assertions;
+
+namespace Mono.Linker.Tests.Cases.LinkXml
+{
+	abstract class PreserveBackingFieldWhenPropertyIsKept
+	{
+		public static void Main ()
+		{
+			Prop = 1;
+		}
+
+		public abstract int Base { set; }
+
+		[Kept]
+		[KeptBackingField]
+		public static int Prop { get; [Kept] set; }		
+	}
+}
