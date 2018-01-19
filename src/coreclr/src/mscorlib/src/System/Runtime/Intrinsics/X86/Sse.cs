ef523093274890c8ea57f62484bfcec20034b19d
Tanner Gooding <tagoo@outlook.com>
2018-01-18 22:31:02 -0800

    Fix the naming of the arguments for LoadHigh and LoadLow
    Commit migrated from https://github.com/dotnet/coreclr/commit/1a6bad1534b1a3fb6054e094f3ead1d4e0273be2

--- src/coreclr/src/mscorlib/src/System/Runtime/Intrinsics/X86/Sse.cs
+++ src/coreclr/src/mscorlib/src/System/Runtime/Intrinsics/X86/Sse.cs
@@ -274,12 +274,12 @@ namespace System.Runtime.Intrinsics.X86
         /// <summary>
         /// __m128 _mm_loadh_pi (__m128 a, __m64 const* mem_addr)
         /// </summary>
-        public static unsafe Vector128<float> LoadHigh(Vector128<float> value, float* address) => LoadHigh(value, address);
+        public static unsafe Vector128<float> LoadHigh(Vector128<float> lower, float* address) => LoadHigh(lower, address);
 
         /// <summary>
         /// __m128 _mm_loadl_pi (__m128 a, __m64 const* mem_addr)
         /// </summary>
-        public static unsafe Vector128<float> LoadLow(Vector128<float> value, float* address) => LoadLow(value, address);
+        public static unsafe Vector128<float> LoadLow(Vector128<float> upper, float* address) => LoadLow(upper, address);
 
         /// <summary>
         /// __m128 _mm_max_ps (__m128 a,  __m128 b)
