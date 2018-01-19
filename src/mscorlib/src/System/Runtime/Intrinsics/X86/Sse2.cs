0f11e645d2ca028658884464ef7122dbdcd2b08b
Tanner Gooding <tagoo@outlook.com>
2018-01-18 21:34:48 -0800

    Adding the missing Sse2.MoveMask(Vector128<byte>) intrinsic

--- src/mscorlib/src/System/Runtime/Intrinsics/X86/Sse2.cs
+++ src/mscorlib/src/System/Runtime/Intrinsics/X86/Sse2.cs
@@ -696,6 +696,10 @@ namespace System.Runtime.Intrinsics.X86
         /// </summary>
         public static int MoveMask(Vector128<sbyte> value) => MoveMask(value);
         /// <summary>
+        /// int _mm_movemask_epi8 (__m128i a)
+        /// </summary>
+        public static int MoveMask(Vector128<byte> value) => MoveMask(value);
+        /// <summary>
         /// int _mm_movemask_pd (__m128d a)
         /// </summary>
         public static int MoveMask(Vector128<double> value) => MoveMask(value);
