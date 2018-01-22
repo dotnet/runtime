1b6d0e941f18cd59fb79aa155c80ce4b349b73d8
Bernhard Urban <lewurm@gmail.com>
2018-01-22 20:46:47 +0100

    [offsetdumper] remove InterpMethodArguments
    Every user of this tool is using architectures that are implementing
    `CallContext`.
    
    
    Commit migrated from https://github.com/mono/mono/commit/236a8d6dc097df46aa7e70d5707b180e6ec6459b

--- src/mono/mono/metadata/object-offsets.h
+++ src/mono/mono/metadata/object-offsets.h
@@ -279,12 +279,6 @@ DECL_OFFSET(SeqPointInfo, ss_tramp_addr)
 DECL_OFFSET(SeqPointInfo, bp_addrs)
 #endif
 
-DECL_OFFSET(InterpMethodArguments, ilen)
-DECL_OFFSET(InterpMethodArguments, iargs)
-DECL_OFFSET(InterpMethodArguments, flen)
-DECL_OFFSET(InterpMethodArguments, fargs)
-DECL_OFFSET(InterpMethodArguments, retval)
-DECL_OFFSET(InterpMethodArguments, is_float_ret)
 #if defined(TARGET_AMD64) || defined(TARGET_ARM) || defined(TARGET_ARM64)
 DECL_OFFSET(CallContext, gregs)
 DECL_OFFSET(CallContext, fregs)
