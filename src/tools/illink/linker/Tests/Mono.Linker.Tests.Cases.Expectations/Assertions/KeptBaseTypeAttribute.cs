6cd6b6c76eb4c37216048f52626e1f0bfc60d20e
Michael Voorhees <michaelv@unity3d.com>
2018-01-24 16:31:06 -0500

    Add some enum test cases
    Commit migrated from https://github.com/dotnet/linker/commit/b82b57c6742485b2eb530d3d3135a418d3bc36c3

--- src/tools/illink/linker/Tests/Mono.Linker.Tests.Cases.Expectations/Assertions/KeptBaseTypeAttribute.cs
+++ src/tools/illink/linker/Tests/Mono.Linker.Tests.Cases.Expectations/Assertions/KeptBaseTypeAttribute.cs
@@ -2,7 +2,7 @@
 
 namespace Mono.Linker.Tests.Cases.Expectations.Assertions
 {
-	[AttributeUsage (AttributeTargets.Class | AttributeTargets.Delegate, AllowMultiple = false, Inherited = false)]
+	[AttributeUsage (AttributeTargets.Class | AttributeTargets.Delegate | AttributeTargets.Enum, AllowMultiple = false, Inherited = false)]
 	public sealed class KeptBaseTypeAttribute : KeptAttribute
 	{
 		public KeptBaseTypeAttribute (Type baseType)
