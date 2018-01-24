97e143c2fe0fc602b394ce937c18e654ce892387
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2018-01-24 01:10:52 +0100

    Do not devirtualize shared default interface methods (dotnet/coreclr#15979)
    The result of the devirtualization for a method on a generic type should be an instantiating stub, but this doesn't seem to work right.
    
    Fixing that is a perf issue (that can be triaged/punted - dotnet/coreclr#15977). This commit is a correctness fix to avoid bad codegen for that case.
    
    Resolves dotnet/coreclr#15591.
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/3211c255a54b2956b345ba85e7be371548264e7a

--- src/coreclr/src/vm/jitinterface.cpp
+++ src/coreclr/src/vm/jitinterface.cpp
@@ -8870,6 +8870,14 @@ CORINFO_METHOD_HANDLE CEEInfo::resolveVirtualMethodHelper(CORINFO_METHOD_HANDLE 
         {
             return nullptr;
         }
+
+        // If we devirtualized into a default interface method on a generic type, we should actually return an
+        // instantiating stub but this is not happening.
+        // Making this work is tracked by https://github.com/dotnet/coreclr/issues/15977
+        if (pDevirtMD->GetMethodTable()->IsInterface() && pDevirtMD->HasClassInstantiation())
+        {
+            return nullptr;
+        }
     }
     else
     {
