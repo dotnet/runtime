fe272d23b8fb4a9e5b0320afe8407585de5d08b9
Jacek Blaszczynski <biosciencenow@outlook.com>
2018-01-26 16:59:54 +0100

    Implement simple Sse2 hardware intrinsics
    Commit migrated from https://github.com/dotnet/coreclr/commit/e535ca8d6b104319b4cd91bd765a172fcde648ae

---
+++ src/clr/tests/src/JIT/HardwareIntrinsics/X86/Sse2/CompareNotLessThan.cs
@@ -0,0 +1,55 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+//
+
+using System;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
+
+namespace IntelHardwareIntrinsicTest
+{
+    internal static partial class Program
+    {
+        private const int Pass = 100;
+        private const int Fail = 0;
+
+        static unsafe int Main(string[] args)
+        {
+            int testResult = Pass;
+            int testsCount = 21;
+            string methodUnderTestName = nameof(Sse2.CompareNotLessThan);
+
+            if (Sse2.IsSupported)
+            {
+                using (var doubleTable = TestTableSse2<double>.Create(testsCount))
+                {
+                    for (int i = 0; i < testsCount; i++)
+                    {
+                        (Vector128<double>, Vector128<double>, Vector128<double>) value = doubleTable[i];
+                        var result = Sse2.CompareNotLessThan(value.Item1, value.Item2);
+                        doubleTable.SetOutArray(result);
+                    }
+
+                    CheckMethod<double> checkDouble = (double x, double y, double z, ref double a) =>
+                    {
+                        a = !(x < y) ? BitConverter.Int64BitsToDouble(-1) : 0;
+                        return BitConverter.DoubleToInt64Bits(a) == BitConverter.DoubleToInt64Bits(z);
+                    };
+
+                    if (!doubleTable.CheckResult(checkDouble))
+                    {
+                        PrintError(doubleTable, methodUnderTestName, "(x, y, z, ref a) => (a = !(x < y) ? double.NaN : 0) == z", checkDouble);
+                        testResult = Fail;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Sse2.IsSupported: {Sse2.IsSupported}, skipped tests of {typeof(Sse2)}.{methodUnderTestName}");
+            }
+
+            return testResult;
+        }
+    }
+}
