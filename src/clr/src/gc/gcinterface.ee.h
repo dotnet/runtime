1a0553707f2b6a9cb23c41d76f7ada9e8a1d84ff
Sean Gillespie <segilles@microsoft.com>
2018-01-25 18:17:57 -0800

    [Local GC] FEATURE_EVENT_TRACE 3/n: Defining and Firing Dynamic Events (dotnet/coreclr#16000)
    Commit migrated from https://github.com/dotnet/coreclr/commit/145cfe19f9e4f3e8a84b75a3048fe98fffb59a68

--- src/clr/src/gc/gcinterface.ee.h
+++ src/clr/src/gc/gcinterface.ee.h
@@ -16,7 +16,15 @@
 // to the EE. ([LOCALGC TODO dynamic event implementation])
 class IGCToCLREventSink
 {
-    /* [LOCALGC TODO] This will be filled with events as they get ported */
+public:
+    // Fires a dynamic event with the given event name and payload. Dynamic
+    // events are not known to the EE and are fired as an unschematized event
+    // to the underlying eventing implementation.
+    virtual
+    void FireDynamicEvent(
+        const char* eventName,
+        void* payload,
+        uint32_t payloadSize) = 0;
 };
 
 // This interface provides the interface that the GC will use to speak to the rest
