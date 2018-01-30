52d1532a239809aa6e34378e72e5b89b84abecc9
Michael Voorhees <michaelv@unity3d.com>
2018-01-30 10:22:39 -0500

    Fix mdb symbol handling.
    All that was needed was to reference Mono.Cecil.Mdb so that the assembly would be built
    
    
    Commit migrated from https://github.com/dotnet/linker/commit/6955856d62981f560a5669e0cc4bf01fce4776aa

--- src/tools/linker/linker/Tests/Mono.Linker.Tests.Cases/Symbols/AssemlbyWithDefaultSymbolsAndSymbolLinkingEnabled.cs
+++ src/tools/linker/linker/Tests/Mono.Linker.Tests.Cases/Symbols/AssemlbyWithDefaultSymbolsAndSymbolLinkingEnabled.cs
@@ -4,7 +4,6 @@ using Mono.Linker.Tests.Cases.Expectations.Metadata;
 [assembly: KeptAttributeAttribute (typeof (System.Diagnostics.DebuggableAttribute))]
 
 namespace Mono.Linker.Tests.Cases.Symbols {
-	[IgnoreTestCase ("Will fix in follow on PR.  Fails on OSX due to mbd symbol linking being broken")]
 	[SetupCompileArgument ("/debug:full")]
 	[SetupLinkerLinkSymbols ("true")]
 	[KeptSymbols ("test.exe")]
