dae615391a1a5695840765dddb55769dbe448c56
Rakesh Singh <raksingh@microsoft.com>
2018-02-01 20:19:23 -0800

    Add sample snaps for .NET Core based applications (dotnet/core-setup#3660)
    * add base snaps
    
    * Add sample snaps for .NET Core based applications. These are examples of self-contained snaps
    
    
    Commit migrated from https://github.com/dotnet/core-setup/commit/31d7bde17f4b28e774ba81b588914242dfc7977a

---
+++ src/installer/pkg/packaging/snaps/dotnet-hello/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet.hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
