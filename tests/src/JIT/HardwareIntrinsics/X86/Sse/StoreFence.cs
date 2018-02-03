7d7202846ef4e00c912054edf3bf0eca420f0a6d
Tanner Gooding <tagoo@outlook.com>
2018-02-03 09:30:11 -0800

    Adding tests for the StoreFence/Prefetch* APIs and the new Sse scalar overloads.

---
+++ tests/src/JIT/HardwareIntrinsics/X86/Sse/StoreFence.cs
@@ -0,0 +1,38 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+//
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
+using System.Runtime.Intrinsics;
+
+namespace IntelHardwareIntrinsicTest
+{
+    class Program
+    {
+        const int Pass = 100;
+        const int Fail = 0;
+
+        static unsafe int Main(string[] args)
+        {
+            int testResult = Pass;
+
+            if (Sse.IsSupported)
+            {
+                try
+                {
+                    Sse.StoreFence();
+                }
+                catch
+                {
+                    testResult = Fail;
+                }
+            }
+
+            return testResult;
+        }
+    }
+}
