009e911ce2bddaff90f4758df166e96b1c90af33
Koundinya Veluri <kouvel@users.noreply.github.com>
2018-02-02 17:55:39 -0800

    Fix GetILToNativeMapping3 to return mappings for the specified code start address (dotnet/coreclr#16146)
    Fix GetILToNativeMapping3 to return mappings for the specified code start address
    
    Fix for https://github.com/dotnet/coreclr/issues/16145 in master:
    - Previously, it was getting or creating the current entry point's corresponding DebugJitInfo and determining that the specified code address is not within the code range
    - Fixed to get or create a DebugJitInfo corresponding to the specified code address and return that
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/7801a4f047c6e2cd68c5b10b9de232f32a6bdb26

--- src/coreclr/src/debug/ee/debugger.h
+++ src/coreclr/src/debug/ee/debugger.h
@@ -1010,7 +1010,7 @@ public:
     DebuggerJitInfo * FindJitInfo(MethodDesc * pMD, TADDR addrNativeStartAddr);
 
     // Creating the Jit-infos.
-    DebuggerJitInfo *FindOrCreateInitAndAddJitInfo(MethodDesc* fd);
+    DebuggerJitInfo *FindOrCreateInitAndAddJitInfo(MethodDesc* fd, TADDR startAddr);
     DebuggerJitInfo *CreateInitAndAddJitInfo(MethodDesc* fd, TADDR startAddr, BOOL* jitInfoWasCreated);
 
 
