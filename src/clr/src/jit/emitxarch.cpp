09f6598074a9606b71cc9deca4a30bfece334ec5
Carol Eidt <carol.eidt@microsoft.com>
2018-02-06 09:53:00 -0800

    Disable prefetch instructions for LEGACY_BACKEND
    Commit migrated from https://github.com/dotnet/coreclr/commit/38663d0869a6221ad2478248150d6efa885e15a2

--- src/clr/src/jit/emitxarch.cpp
+++ src/clr/src/jit/emitxarch.cpp
@@ -2029,9 +2029,7 @@ UNATIVE_OFFSET emitter::emitInsSizeAM(instrDesc* id, code_t code)
 
         assert((attrSize == EA_4BYTE) || (attrSize == EA_PTRSIZE)    // Only for x64
                || (attrSize == EA_16BYTE) || (attrSize == EA_32BYTE) // only for x64
-               || (ins == INS_movzx) || (ins == INS_movsx) || (ins == INS_prefetcht0) ||
-               (ins == INS_prefetcht1)                                  // the prefetch instructions are always 3 bytes
-               || (ins == INS_prefetcht2) || (ins == INS_prefetchnta)); // and have part of their modr/m byte hardcoded
+               || (ins == INS_movzx) || (ins == INS_movsx) || isPrefetch(ins));
         size = 3;
     }
     else
