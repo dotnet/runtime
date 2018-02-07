ff1da90da40388afa547833722009ca7422c94d2
Andy Ayers <andya@microsoft.com>
2018-02-06 16:05:00 -0800

    Add test case for contstructor devirtualization example (#16239)
    See #16198 for discussion.

---
+++ tests/src/JIT/opt/Devirtualization/constructor.cs
@@ -0,0 +1,40 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Runtime.CompilerServices;
+
+class Test 
+{
+    static string s;
+
+    public override string ToString() 
+    {
+        return "Test";
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public Test() 
+    {
+        s = ToString();   // cannot be devirtualized
+    }
+    
+    static int Main() 
+    {
+        new Child();
+        return (s == "Child" ? 100 : 0);
+    }
+}
+
+class Child : Test 
+{
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public Child() { }
+
+    public override string ToString() 
+    {
+        return "Child";
+    }
+}
