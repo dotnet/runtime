3a7b5f6cbb7a024c2cdf0ab9546ca7d2e593760b
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2018-02-14 11:16:09 +0100

    Handle ELEMENT_TYPE_PTR in ILCodeStream::EmitLDIND/EmitSTIND (#16381)
    Fixes #16371.

---
+++ tests/src/Regressions/coreclr/16371/ptrarray.cs
@@ -0,0 +1,25 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+unsafe class Program
+{
+    static int*[,] s_mdArray;
+
+    static void Init(ref int* elem)
+    {
+        elem = (int*)98;
+    }
+
+    static Program()
+    {
+        s_mdArray = new int*[2,2];
+        Init(ref s_mdArray[0, 0]);
+        s_mdArray[1, 1] = (int*)2;
+    }
+
+    static int Main()
+    {
+        return (int)s_mdArray[0, 0] + (int)s_mdArray[1, 1];
+    }
+}
