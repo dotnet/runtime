f524ff88ff1e2f6c44dbe6144ec65647b71ea38c
Vlad Brezae <brezaevlad@gmail.com>
2018-02-21 01:58:45 +0200

    [tests] Avoid false pinning in weak-fields test
    Commit migrated from https://github.com/mono/mono/commit/a32ca3e2c2892f7f5f1146a14747627ead58e161

--- src/mono/mono/tests/weak-fields.cs
+++ src/mono/mono/tests/weak-fields.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading;
+using MonoTests.Helpers;
 
 [AttributeUsage(AttributeTargets.Field)]
 public sealed class Weak2Attribute : Attribute
@@ -32,35 +33,33 @@ public class Tests
 
 	public static int Main (String[] args) {
 		var t = new Tests ();
-		var thread = new Thread (delegate () {
-				t.Obj = new Finalizable ();
-				t.Obj2 = new Finalizable ();
-				t.Obj3 = new Finalizable ();
-				t.Obj4 = retain = new Finalizable ();
-				retain.a = 0x1029458;
+		FinalizerHelpers.PerformNoPinAction (delegate () {
+				FinalizerHelpers.PerformNoPinAction (delegate () {
+						t.Obj = new Finalizable ();
+						t.Obj2 = new Finalizable ();
+						t.Obj3 = new Finalizable ();
+						t.Obj4 = retain = new Finalizable ();
+						retain.a = 0x1029458;
+					});
+				GC.Collect (0);
+				GC.Collect ();
+				GC.WaitForPendingFinalizers ();
+				if (t.Obj != null)
+					Environment.Exit (1);
+				if (t.Obj2 != null)
+					Environment.Exit (2);
+				if (t.Obj3 == null)
+					Environment.Exit (3);
+				//overflow the nursery, make sure we fill it
+				for (int i = 0; i < 1000 * 1000 * 10; ++i)
+					new OneField ();
+
+				if (retain.a != 0x1029458)
+					Environment.Exit (4);
+
+				retain = null;
 			});
-		thread.Start ();
-		thread.Join ();
-		GC.Collect (0);
 		GC.Collect ();
-		GC.WaitForPendingFinalizers ();
-		GC.WaitForPendingFinalizers ();
-		if (t.Obj != null)
-			return 1;
-		if (t.Obj2 != null)
-			return 2;
-		if (t.Obj3 == null)
-			return 3;
-		//overflow the nursery, make sure we fill it
-		for (int i = 0; i < 1000 * 1000 * 10; ++i)
-			new OneField ();
-
-		if (retain.a != 0x1029458)
-			return 4;
-
-		retain = null;
-		GC.Collect ();
-		GC.WaitForPendingFinalizers ();
 		GC.WaitForPendingFinalizers ();
 		if (t.Obj4 != null)
 			return 5;
