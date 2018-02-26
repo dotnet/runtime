d6ac9873066334cc82ab4ed45d86ea21d524b26e
Eugene Rozenfeld <erozen@microsoft.com>
2018-02-26 12:55:20 -0800

    Add ClearInitLocalsAssemblies parameter for ClearInitLocalsStep.
    Modified ClearInitLocalsStep to accept ClearInitLocalsAssemblies
    custom parameter that can be specified via -m. The parameter
    is a comma-separated list of assembly simple names that the the
    step should be applied to. If the parameter is not specified ClearInitLocals
    runs on all assemblies.
    
    ClearInitLocals and ClearInitLocalsAssemblies are also now exposed as
    ILLinkTask properties.
    
    
    Commit migrated from https://github.com/dotnet/linker/commit/6a540a86d4f827ca0b9f429d66e8519bb414bc90

--- src/tools/illink/corebuild/integration/ILLink.CustomSteps/ClearInitLocals.cs
+++ src/tools/illink/corebuild/integration/ILLink.CustomSteps/ClearInitLocals.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 using Mono.Linker;
 using Mono.Linker.Steps;
@@ -8,17 +9,41 @@ namespace ILLink.CustomSteps
 {
 	public class ClearInitLocalsStep : BaseStep
 	{
-		protected override void ProcessAssembly(AssemblyDefinition assembly)
+		HashSet<string> _assemblies;
+
+		protected override void Process ()
 		{
+			string parameterName = "ClearInitLocalsAssemblies";
+
+			if (Context.HasParameter (parameterName)) {
+				string parameter = Context.GetParameter (parameterName);
+				_assemblies = new HashSet<string> (parameter.Split(','), StringComparer.OrdinalIgnoreCase);
+			}
+		}
+
+		protected override void ProcessAssembly (AssemblyDefinition assembly)
+		{
+			if ((_assemblies != null) && (!_assemblies.Contains (assembly.Name.Name))) {
+				return;
+			}
+
+			bool changed = false;
+
 			foreach (ModuleDefinition module in assembly.Modules) {
 				foreach (TypeDefinition type in module.Types) {
 					foreach (MethodDefinition method in type.Methods) {
 						if (method.Body != null) {
-							method.Body.InitLocals = false;
+							if (method.Body.InitLocals) {
+								method.Body.InitLocals = false;
+								changed = true;
+							}
 						}
 					}
 				}
 			}
+
+			if (changed && (Annotations.GetAction (assembly) == AssemblyAction.Copy))
+					Annotations.SetAction (assembly, AssemblyAction.Save);
 		}
 	}
 }
