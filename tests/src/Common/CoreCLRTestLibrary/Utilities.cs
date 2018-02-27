c41b6f0828ff0b59acb4ffc6a6aec4354bc013ff
Jan Kotas <jkotas@microsoft.com>
2018-02-27 06:41:45 -0800

    Delete left-over globalization CoreCLR tests (#16571)

--- tests/src/Common/CoreCLRTestLibrary/Utilities.cs
+++ tests/src/Common/CoreCLRTestLibrary/Utilities.cs
@@ -56,22 +56,6 @@ namespace TestLibrary
             }
         }
 
-        public static bool IsBigEndian
-        {
-            get
-            {
-                return EndianessChecker.IsBigEndian();
-            }
-        }
-
-        public static bool IsLittleEndian
-        {
-            get
-            {
-                return EndianessChecker.IsLittleEndian();
-            }
-        }
-
         public static bool IsWindows
         {
             [SecuritySafeCritical]
@@ -79,53 +63,6 @@ namespace TestLibrary
             {
                 return Path.DirectorySeparatorChar == '\\';
             }
-        }
-
-        public static bool IsVista
-        {
-            get
-            {
-                return false;
-            }
-        }
-
-        public static bool IsVistaOrLater
-        {
-            get
-            {
-                return true;
-            }
-        }
-
-        public static bool IsWin2K
-        {
-            get
-            {
-                return false;
-            }
-        }
-
-        public static bool IsWin7
-        {
-            get
-            {
-                return false;
-            }
-        }
-
-        public static bool IsWin7OrLater
-        {
-            get
-            {
-                return true; //Win8P is always win8+
-            }
-        }
-
-        public static bool PlatformSpecificComparer(object Actual, object XPExpected, object VistaExpected, object MACPPCExpected, object MACX86Expected)
-        {
-            if (!IsWindows) return (Actual.Equals(IsBigEndian ? MACPPCExpected : MACX86Expected));
-            if (IsVista) return (Actual.Equals(VistaExpected));
-            return Actual.Equals(XPExpected);
         }
 
         // return whether or not the OS is a 64 bit OS
@@ -150,48 +87,6 @@ namespace TestLibrary
                 sb.Remove(sb.Length - 2, 2);
 
             return sb.ToString();
-        }
-
-        public static byte[] TrimBytes(byte[] input)
-        {
-            int outputLength = input.Length;
-            int pos = 0;
-            while (pos < input.Length && input[pos] == 0) { outputLength--; pos++; }
-            int newStart = pos;
-            pos = input.Length - 1;
-            while (input[pos] == 0) { outputLength--; pos--; }
-
-            byte[] output = new byte[outputLength];
-            for (int i = 0; i < outputLength; i++)
-            {
-                output[i] = input[i + newStart];
-            }
-
-            return output;
-        }
-
-        public static bool CompareBytes(byte[] arr1, byte[] arr2)
-        {
-            if (arr1 == null) return (arr2 == null);
-            if (arr2 == null) return false;
-
-            if (arr1.Length != arr2.Length) return false;
-
-            for (int i = 0; i < arr1.Length; i++) if (arr1[i] != arr2[i]) return false;
-
-            return true;
-        }
-
-        public static bool CompareChars(char[] arr1, char[] arr2)
-        {
-            if (arr1 == null) return (arr2 == null);
-            if (arr2 == null) return false;
-
-            if (arr1.Length != arr2.Length) return false;
-
-            for (int i = 0; i < arr1.Length; i++) if (arr1[i] != arr2[i]) return false;
-
-            return true;
         }
 
         // Given a string, display the unicode characters in hex format, optionally displaying each 
@@ -230,19 +125,6 @@ namespace TestLibrary
             return ((int)char1).ToString("X4");
         }
 
-        public static bool IsHighSurrogate(char c)
-        {
-            return ((c >= HIGH_SURROGATE_START) && (c <= HIGH_SURROGATE_END));
-        }
-        public static bool IsLowSurrogate(char c)
-        {
-            return ((c >= LOW_SURROGATE_START) && (c <= LOW_SURROGATE_END));
-        }
-
-        public static bool CompareCurrentCulture(String culture)
-        {
-            return (String.Compare(System.Globalization.CultureInfo.CurrentCulture.Name, culture, StringComparison.CurrentCultureIgnoreCase) == 0);
-        }
         public static CultureInfo CurrentCulture
         {
             get { return System.Globalization.CultureInfo.CurrentCulture; }
@@ -252,36 +134,4 @@ namespace TestLibrary
             }
         }
     }
-}
-
-public static class HelperExtensions {
-   public static bool IsAssignableFrom(this Type t1, Type t2) {
-      return t1.GetTypeInfo().IsAssignableFrom(t2.GetTypeInfo());
-   }
-
-   public static String ToLongDateString(this DateTime dt)
-   {
-       return String.Format("{0:D}", dt);
-   }
-
-   public static String ToLongTimeString(this DateTime dt)
-   {
-       return String.Format("{0:T}", dt);
-   }
-
-
-   public static String ToShortDateString(this DateTime dt)
-   {
-       return String.Format("{0:d}", dt);
-   }
-
-   public static String ToShortTimeString(this DateTime dt)
-   {
-       return String.Format("{0:t}", dt);
-   }
-
-   public static UnicodeCategory GetUnicodeCategory(this Char c)
-   {
-       return CharUnicodeInfo.GetUnicodeCategory(c);
-   }
 }
