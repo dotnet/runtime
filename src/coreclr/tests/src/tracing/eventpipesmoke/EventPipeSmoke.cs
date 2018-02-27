c20916edb1cda4385fea2fcaaedbff698e16ee53
Adam Sitnik <adam.sitnik@gmail.com>
2018-02-27 21:25:35 +0100

    Event pipe guid in metadata (dotnet/coreclr#16316), fixes dotnet/coreclr#16105
    * some refactor to remove dumplicated code in tests and make them fit 1 screen
    
    * write the failing test for no activity id
    
    * use 17, the "free slot" of TypeCode enum to represent Guid in event pipe metadata
    
    * remove the test which would not pass until new trace event is released and Event Pipe enabled for Windows
    
    * added comment to TypeCode about using the value 17
    
    * post code review fixes (thanks @justinvp)
    
    * post code review fixes (thanks @brianrob)
    
    * refactor the rest of event pipe tests to use NetPerfFile helper
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/5fa7b84ff85a153133352aba9362def0aae571bc

--- src/coreclr/tests/src/tracing/eventpipesmoke/EventPipeSmoke.cs
+++ src/coreclr/tests/src/tracing/eventpipesmoke/EventPipeSmoke.cs
@@ -12,52 +12,30 @@ namespace Tracing.Tests
 
         static int Main(string[] args)
         {
-            bool keepOutput = false;
-            // Use the first arg as an output filename if there is one
-            string outputFilename = null;
-            if (args.Length >= 1) {
-                outputFilename = args[0];
-                keepOutput = true;
-            }
-            else {
-                outputFilename = System.IO.Path.GetTempPath() + Guid.NewGuid().ToString() + ".netperf";
-            }
-
-            Console.WriteLine("\tStart: Enable tracing.");
-            TraceControl.EnableDefault(outputFilename);
-            Console.WriteLine("\tEnd: Enable tracing.\n");
-
-            Console.WriteLine("\tStart: Allocation.");
-            // Allocate for allocIterations iterations.
-            for(int i=0; i<allocIterations; i++)
+            using (var netPerfFile = NetPerfFile.Create(args))
             {
-                GC.KeepAlive(new object());
+                Console.WriteLine("\tStart: Enable tracing.");
+                TraceControl.EnableDefault(netPerfFile.Path);
+                Console.WriteLine("\tEnd: Enable tracing.\n");
+
+                Console.WriteLine("\tStart: Allocation.");
+                // Allocate for allocIterations iterations.
+                for (int i = 0; i < allocIterations; i++)
+                {
+                    GC.KeepAlive(new object());
+                }
+                Console.WriteLine("\tEnd: Allocation.\n");
+
+                Console.WriteLine("\tStart: Disable tracing.");
+                TraceControl.Disable();
+                Console.WriteLine("\tEnd: Disable tracing.\n");
+
+                FileInfo outputMeta = new FileInfo(netPerfFile.Path);
+                Console.WriteLine("\tExpecting at least {0} bytes of data", trivialSize);
+                Console.WriteLine("\tCreated {0} bytes of data", outputMeta.Length);
+
+                return outputMeta.Length > trivialSize ? 100 : 0;
             }
-            Console.WriteLine("\tEnd: Allocation.\n");
-
-            Console.WriteLine("\tStart: Disable tracing.");
-            TraceControl.Disable();
-            Console.WriteLine("\tEnd: Disable tracing.\n");
-
-            FileInfo outputMeta = new FileInfo(outputFilename);
-            Console.WriteLine("\tExpecting at least {0} bytes of data", trivialSize);
-            Console.WriteLine("\tCreated {0} bytes of data", outputMeta.Length);
-
-            bool pass = false;
-            if (outputMeta.Length > trivialSize){
-                pass = true;
-            }
-
-            if (keepOutput)
-            {
-                Console.WriteLine(String.Format("\tOutput file: {0}", outputFilename));
-            }
-            else
-            {
-                System.IO.File.Delete(outputFilename);
-            }
-
-            return pass ? 100 : 0;
         }
     }
 }
