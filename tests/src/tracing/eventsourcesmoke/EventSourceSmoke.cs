5fa7b84ff85a153133352aba9362def0aae571bc
Adam Sitnik <adam.sitnik@gmail.com>
2018-02-27 21:25:35 +0100

    Event pipe guid in metadata (#16316), fixes #16105
    * some refactor to remove dumplicated code in tests and make them fit 1 screen
    
    * write the failing test for no activity id
    
    * use 17, the "free slot" of TypeCode enum to represent Guid in event pipe metadata
    
    * remove the test which would not pass until new trace event is released and Event Pipe enabled for Windows
    
    * added comment to TypeCode about using the value 17
    
    * post code review fixes (thanks @justinvp)
    
    * post code review fixes (thanks @brianrob)
    
    * refactor the rest of event pipe tests to use NetPerfFile helper

--- tests/src/tracing/eventsourcesmoke/EventSourceSmoke.cs
+++ tests/src/tracing/eventsourcesmoke/EventSourceSmoke.cs
@@ -44,59 +44,37 @@ namespace Tracing.Tests
 
         static int Main(string[] args)
         {
-            bool keepOutput = false;
-            // Use the first arg as an output filename if there is one
-            string outputFilename = null;
-            if (args.Length >= 1) {
-                outputFilename = args[0];
-                keepOutput = true;
-            }
-            else {
-                outputFilename = System.IO.Path.GetTempPath() + Guid.NewGuid().ToString() + ".netperf";
-            }
-
-            SimpleEventSource eventSource = new SimpleEventSource();
-
-            Console.WriteLine("\tStart: Enable tracing.");
-            TraceControl.Enable(GetConfig(eventSource, outputFilename));
-            Console.WriteLine("\tEnd: Enable tracing.\n");
-
-            Console.WriteLine("\tStart: Messaging.");
-            // Send messages
-            // Use random numbers and addition as a simple, human readble checksum
-            Random generator = new Random();
-            for(int i=0; i<messageIterations; i++)
+            using (var netPerfFile = NetPerfFile.Create(args))
             {
-                int x = generator.Next(1,1000);
-                int y = generator.Next(1,1000);
-                string formula = String.Format("{0} + {1} = {2}", x, y, x+y);
-                
-                eventSource.MathResult(x, y, x+y, formula);
+                SimpleEventSource eventSource = new SimpleEventSource();
+
+                Console.WriteLine("\tStart: Enable tracing.");
+                TraceControl.Enable(GetConfig(eventSource, netPerfFile.Path));
+                Console.WriteLine("\tEnd: Enable tracing.\n");
+
+                Console.WriteLine("\tStart: Messaging.");
+                // Send messages
+                // Use random numbers and addition as a simple, human readble checksum
+                Random generator = new Random();
+                for (int i = 0; i < messageIterations; i++)
+                {
+                    int x = generator.Next(1, 1000);
+                    int y = generator.Next(1, 1000);
+                    string formula = String.Format("{0} + {1} = {2}", x, y, x + y);
+
+                    eventSource.MathResult(x, y, x + y, formula);
+                }
+                Console.WriteLine("\tEnd: Messaging.\n");
+
+                Console.WriteLine("\tStart: Disable tracing.");
+                TraceControl.Disable();
+                Console.WriteLine("\tEnd: Disable tracing.\n");
+
+                FileInfo outputMeta = new FileInfo(netPerfFile.Path);
+                Console.WriteLine("\tCreated {0} bytes of data", outputMeta.Length);
+
+                return outputMeta.Length > trivialSize ? 100 : -1;
             }
-            Console.WriteLine("\tEnd: Messaging.\n");
-
-            Console.WriteLine("\tStart: Disable tracing.");
-            TraceControl.Disable();
-            Console.WriteLine("\tEnd: Disable tracing.\n");
-
-            FileInfo outputMeta = new FileInfo(outputFilename);
-            Console.WriteLine("\tCreated {0} bytes of data", outputMeta.Length);
-
-            bool pass = false;
-            if (outputMeta.Length > trivialSize){
-                pass = true;
-            }
-
-            if (keepOutput)
-            {
-                Console.WriteLine(String.Format("\tOutput file: {0}", outputFilename));
-            }
-            else
-            {
-                System.IO.File.Delete(outputFilename);
-            }
-
-            return pass ? 100 : -1;
         }
     }
 }
--- tests/src/tracing/tracevalidation/inducedgc/InducedGC.cs
+++ tests/src/tracing/tracevalidation/inducedgc/InducedGC.cs
@@ -13,25 +13,10 @@ namespace Tracing.Tests
 
         public static int Main(string[] args)
         {
-            bool pass = true;
-            bool keepOutput = false;
-
-            // Use the first arg as an output filename if there is one.
-            string outputFilename = null;
-            if (args.Length >= 1)
-            {
-                outputFilename = args[0];
-                keepOutput = true;
-            }
-            else
-            {
-                outputFilename = System.IO.Path.GetTempPath() + Guid.NewGuid().ToString() + ".netperf";
-            }
-
-            try
+            using (var netPerfFile = NetPerfFile.Create(args))
             {
                 Console.WriteLine("\tStart: Enable tracing.");
-                TraceControl.EnableDefault(outputFilename);
+                TraceControl.EnableDefault(netPerfFile.Path);
                 Console.WriteLine("\tEnd: Enable tracing.\n");
 
                 Console.WriteLine("\tStart: Generate some events.");
@@ -48,7 +33,7 @@ namespace Tracing.Tests
                 Console.WriteLine("\tStart: Process the trace file.");
                 int matchingEventCount = 0;
                 int nonMatchingEventCount = 0;
-                using (var trace = TraceEventDispatcher.GetDispatcherFromFileName(outputFilename))
+                using (var trace = TraceEventDispatcher.GetDispatcherFromFileName(netPerfFile.Path))
                 {
                     string gcReasonInduced = GCReason.Induced.ToString();
                     string providerName = "Microsoft-Windows-DotNETRuntime";
@@ -74,16 +59,6 @@ namespace Tracing.Tests
 
                 Assert.Equal(nameof(matchingEventCount), InducedGCIterations, matchingEventCount);
                 Assert.Equal(nameof(nonMatchingEventCount), nonMatchingEventCount, 0);
-            }
-            finally {
-                if (keepOutput)
-                {
-                    Console.WriteLine("\n\tOutput file: {0}", outputFilename);
-                }
-                else
-                {
-                    System.IO.File.Delete(outputFilename);
-                }
             }
 
             return 100;
