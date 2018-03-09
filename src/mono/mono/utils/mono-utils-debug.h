183302120e3a6dc2675345fd5d26d079b166405c
jaykrell <jay.krell@cornell.edu>
2018-03-09 11:12:27 -0800

    gboolean g_is_usermode_native_debugger_present (void). (mono/mono#7488)
    * gboolean g_is_usermode_native_debugger_present (void).
    i.e. Win32 IsDebuggerPresent which is just call mov mov ret.
    Other slower methods on other systems if possible and unintrusive (OSX).
    This is very useful as in:
       if (g_is_usermode_native_debugger_present ()) G_BREAKPOINT
    "native" meaning "not managed"
    "usermode" meaning "not kernel"
    Both other kinds exist and are detected differently.
    
    * Move new debugging utility from glib to mono/utils.
    
    * Redo it with CoreCLR as basis. https://github.com/dotnet/coreclr/blob/master/src/pal/src/init/pal.cpp
    
    * Link to a specific stable revision, not master.
    https://raw.githubusercontent.com/dotnet/coreclr/mono/mono@f1c9dac3e2db2397e01cd2da3e8aaa4f81a80013/src/pal/src/init/pal.cpp
    
    * Use a clearer form of boolean normalization.
    
    * Update Visual Studio project files.
    
    * Fix include path, remove unnecessary boolean canonicalization, spell out words, swap int/boolean.
    
    * Header got missed in the move from glib.
    
    
    Commit migrated from https://github.com/mono/mono/commit/f24a6d622be00233185e9a2164b893cf61f0c39b

---
+++ src/mono/mono/utils/mono-utils-debug.h
@@ -0,0 +1,12 @@
+/* mono-utils-debug.h
+ *
+ * Copyright 2018 Microsoft
+ * Licensed under the MIT license. See LICENSE file in the project root for full license information.
+*/
+
+G_BEGIN_DECLS
+
+gboolean
+mono_is_usermode_native_debugger_present (void);
+
+G_END_DECLS
