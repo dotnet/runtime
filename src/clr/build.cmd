73e6759ebfdf99864ec0f9ea087ad550096c6314
Wes Haggard <Wes.Haggard@microsoft.com>
2018-03-21 16:40:34 -0700

    Fix RestoreSources for optdata packages
    Pass unprocessed args to restore optdata step so it
    can override the sources when doing restore
    
    Handle prority as an unprocessed arg for the sync alias
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/46f003fe2276e319f86d06b934aab9d57461a9f6

--- src/clr/build.cmd
+++ src/clr/build.cmd
@@ -320,7 +320,7 @@ REM ============================================================================
 
 if %__RestoreOptData% EQU 1 if %__BuildTypeRelease% EQU 1 (
     echo %__MsgPrefix%Restoring the OptimizationData Package
-    @call %__ProjectDir%\run.cmd sync -optdata
+    @call %__ProjectDir%\run.cmd sync -optdata %__UnprocessedBuildArgs%
     if not !errorlevel! == 0 (
         echo %__MsgPrefix%Error: Failed to restore the optimization data package.
         exit /b 1
