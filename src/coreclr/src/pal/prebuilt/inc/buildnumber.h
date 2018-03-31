58709fda35abb3e9d4771422270ca221f2a92a0e
Eric StJohn <ericstj@microsoft.com>
2018-03-30 18:37:06 -0700

    Fix ilasm COR header (dotnet/coreclr#17357)
    Ilasm was emitting the wrong metadata version in the COR header.
    
    This created a difference when examining the metadata diff between
    desktop and core.
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/2023be14efa54af8b339d36499018a8743b32a29

--- src/coreclr/src/pal/prebuilt/inc/buildnumber.h
+++ src/coreclr/src/pal/prebuilt/inc/buildnumber.h
@@ -2,33 +2,28 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#if 0
-/**** Generated Based on d:\ProjectK\src\InternalApis\Version\buildnumber.settings.targets
-Bizarely enough, you can't put comments in this file (without the #if) -
-because ndp\clr\src\Tools\applaunch preprocesses it into a perl script *****/
-#endif
 #define ProjectNMajorVersion   1
 #define ProjectNMinorVersion   0
 #define ProjectNMajorVersion_A "1"
 #define ProjectNMinorVersion_A "0"
-#define BuildNumberMajor       22220
+#define BuildNumberMajor       30319
 #define BuildNumberMinor       0
-#define BuildNumberMajor_A     "22220"
+#define BuildNumberMajor_A     "30319"
 #define BuildNumberMinor_A     "00"
-#define BuildNumbers_A         "22220.00"
-#define BuildNumbers_T         TEXT("22220.00")
+#define BuildNumbers_A         "30319.00"
+#define BuildNumbers_T         TEXT("30319.00")
 
-#define NDPBuildNumberMajor   22220
+#define NDPBuildNumberMajor   30319
 #define NDPBuildNumberMinor   0
-#define NDPBuildNumbers_A     "22220.00"
-#define NDPBuildNumbers_T     TEXT("22220.00")
+#define NDPBuildNumbers_A     "30319.00"
+#define NDPBuildNumbers_T     TEXT("30319.00")
 
 #define NDPFileVersionMinor   5
-#define NDPFileVersionBuild   22220
+#define NDPFileVersionBuild   30319
 #define NDPFileVersionRevision   0
 
 #define NDPFileVersionMinor_A     "5"
-#define NDPFileVersionBuild_A     "22220"
+#define NDPFileVersionBuild_A     "30319"
 #define NDPFileVersionRevision_A     "00"
 #define PROJECTN_VER_FILEVERSION_STR   ProjectNMajorVersion_A "." NDPFileVersionMinor_A "." NDPFileVersionBuild_A "." NDPFileVersionRevision_A 
 #define PROJECTN_VER_LEGALCOPYRIGHT_LOGO_STR "Copyright (c) Microsoft Corporation.  All rights reserved."
