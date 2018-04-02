b0fcb963c6557f72794587f74d914b6c0f0936f7
Maoni Stephens <Maoni0@users.noreply.github.com>
2018-04-02 11:10:48 -0700

    Added a new API to provide memory information that GC records. This (dotnet/coreclr#17326)
    is to be used by BCL for deciding when to trim memory usage in pooling code
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/2583ce936776a0eac31df904e41d5119840c203b

--- src/coreclr/src/vm/comutilnative.cpp
+++ src/coreclr/src/vm/comutilnative.cpp
@@ -922,6 +922,18 @@ UINT64   GCInterface::m_remPressure[NEW_PRESSURE_COUNT] = {0, 0, 0, 0};   // his
 // (m_iteration % NEW_PRESSURE_COUNT) is used as an index into m_addPressure and m_remPressure
 UINT     GCInterface::m_iteration = 0;
 
+FCIMPL5(void, GCInterface::GetMemoryInfo, UINT32* highMemLoadThreshold, UINT64* totalPhysicalMem, UINT32* lastRecordedMemLoad, size_t* lastRecordedHeapSize, size_t* lastRecordedFragmentation)
+{
+    FCALL_CONTRACT;
+
+    FC_GC_POLL_NOT_NEEDED();
+    
+    return GCHeapUtilities::GetGCHeap()->GetMemoryInfo(highMemLoadThreshold, totalPhysicalMem, 
+                                                       lastRecordedMemLoad, 
+                                                       lastRecordedHeapSize, lastRecordedFragmentation);
+}
+FCIMPLEND
+
 FCIMPL0(int, GCInterface::GetGcLatencyMode)
 {
     FCALL_CONTRACT;
