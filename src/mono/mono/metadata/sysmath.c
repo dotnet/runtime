254b3925ae847b166f91e88552c4c656f47cbaf8
Miguel de Icaza <miguel@gnome.org>
2018-04-05 08:37:46 -0400

    [System.MathF] Bring support for the single-precission Math operations to Mono (mono/mono#7941)
    This pull request brings support to Mono's class libraries for the new `System.MathF` type which includes operations for single-floating point.   
    
    This is needed to run some new applications like:
    
    https://github.com/aras-p/ToyPathTracer
    
    With this change, it is possible to run this unmodified.  With this support, and the `-O=float32` option, it is possible to improve the performance from 6.3Mrays/sec to 7.8Mrays/sec on a Mac.
    
    Oddly, this seems to be faster than .NET Core 2.1.4, which is at 4.2Mrays/sec.
    
    * Bump API snapshot submodule
    
    
    Commit migrated from https://github.com/mono/mono/commit/52df0f2b00b89e28f313b1d803e87f5868e8da95

--- src/mono/mono/metadata/sysmath.c
+++ src/mono/mono/metadata/sysmath.c
@@ -299,8 +299,8 @@ ves_icall_System_Math_Abs_double (gdouble v)
 	return fabs (v);
 }
 
-gfloat
-ves_icall_System_Math_Abs_single (gfloat v)
+float
+ves_icall_System_Math_Abs_single (float v)
 {
 	return fabsf (v);
 }
@@ -316,3 +316,145 @@ ves_icall_System_Math_SplitFractionDouble (gdouble *v)
 {
 	return modf (*v, v);
 }
+
+float
+ves_icall_System_MathF_Acos (float x)
+{
+	return acosf (x);
+}
+
+float
+ves_icall_System_MathF_Acosh (float x)
+{
+	return acoshf (x);
+}
+
+float
+ves_icall_System_MathF_Asin (float x)
+{
+	return asinf (x);
+}
+
+float
+ves_icall_System_MathF_Asinh  (float x)
+{
+	return asinhf (x);
+}
+
+float
+ves_icall_System_MathF_Atan  (float x)
+{
+	return atan (x);
+}
+
+float
+ves_icall_System_MathF_Atan2 (float x, float y)
+{
+	return atan2f (x, y);
+}
+
+float
+ves_icall_System_MathF_Atanh (float x)
+{
+	return atanhf (x);
+}
+
+float
+ves_icall_System_MathF_Cbrt (float x)
+{
+	return cbrtf (x);
+}
+
+float
+ves_icall_System_MathF_Ceiling (float x)
+{
+	return ceilf(x);
+}
+
+float
+ves_icall_System_MathF_Cos (float x)
+{
+	return cosf (x);
+}
+
+float
+ves_icall_System_MathF_Cosh (float x)
+{
+	return coshf (x);
+}
+
+float
+ves_icall_System_MathF_Exp (float x)
+{
+	return expf (x);
+}
+
+float
+ves_icall_System_MathF_Floor (float x)
+{
+	return floorf (x);
+}
+
+float
+ves_icall_System_MathF_Log (float x)
+{
+	return logf (x);
+}
+
+float
+ves_icall_System_MathF_Log10 (float x)
+{
+	return log10f (x);
+}
+
+float
+ves_icall_System_MathF_Pow (float x, float y)
+{
+	return powf (x, y);
+}
+
+float
+ves_icall_System_MathF_Sin (float x)
+{
+	return sinf (x);
+}
+
+float
+ves_icall_System_MathF_Sinh (float x)
+{
+	return sinh (x);
+}
+
+float
+ves_icall_System_MathF_Sqrt (float x)
+{
+	return sqrtf (x);
+}
+
+float
+ves_icall_System_MathF_Tan (float x)
+{
+	return tanf (x);
+}
+
+float
+ves_icall_System_MathF_Tanh (float x)
+{
+	return tanh (x);
+}
+
+float
+ves_icall_System_MathF_FMod (float x, float y)
+{
+	return fmodf (x, y);
+}
+
+float
+ves_icall_System_MathF_ModF (float x, float *d)
+{
+	float f;
+	if (d == NULL)
+		d = &f;
+	return modff (x, d);
+}
+
