22b5f4c728c5139c8c794c2f99a27fc7e7db72c5
Bernhard Urban <bernhard.urban@xamarin.com>
2018-04-11 13:55:24 +0200

    [console-unix] retry `tcsetattr` if it was interrupted by signal (mono/mono#8138)
    [console-unix] retry `tcsetattr` if it was interrupted by signal
    
    if configuring the terminal failed, we still try to continue (see
    mono/mono@a3ce2f008bdb2fa5108034f1687fc7f87c92a0b8 ) and therefore succedings
    `read`s can fail.
    
    Fixes https://bugzilla.xamarin.com/show_bug.cgi?id=26983
    
    
    
    Commit migrated from https://github.com/mono/mono/commit/5128cce2dcd097aa4e7b86dd3b56e833c15a9ff0

--- src/mono/mono/metadata/console-unix.c
+++ src/mono/mono/metadata/console-unix.c
@@ -463,7 +463,14 @@ ves_icall_System_ConsoleDriver_TtySetup (MonoStringHandle keypad, MonoStringHand
 	/* Disable C-y being used as a suspend character on OSX */
 	mono_attr.c_cc [VDSUSP] = 255;
 #endif
-	if (tcsetattr (STDIN_FILENO, TCSANOW, &mono_attr) == -1)
+	gint ret;
+	do {
+		MONO_ENTER_GC_SAFE;
+		ret = tcsetattr (STDIN_FILENO, TCSANOW, &mono_attr);
+		MONO_EXIT_GC_SAFE;
+	} while (ret == -1 && errno == EINTR);
+
+	if (ret == -1)
 		return FALSE;
 
 	uint32_t h;
