f9a92757413250fb6ef5675ddab8376231d575ab
Brian Sullivan <briansul@microsoft.com>
2018-04-11 16:15:57 -0700

    Fixed checks for Avx/Avx2.InsertVector128 to check the type of the second arg Added test case JIT\HardwareIntrinsics\X86\Regression\GitHub_17435

---
+++ tests/src/JIT/HardwareIntrinsics/X86/Regression/GitHub_17435/GitHub_17435.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
+using System.Runtime.Intrinsics;
+
+namespace GitHub_17435
+{
+    class Program
+    {
+        const int Pass = 100;
+        const int Fail = 0;
+
+        static unsafe int Main(string[] args)
+        {
+
+            if (Sse2.IsSupported)
+            {
+                (uint a, uint b) = Program.Repro();
+                if ((a !=3) || (b != 6))
+                {
+                    Console.WriteLine($"FAILED {a}, {b}");
+                    return Fail;
+                }
+                else
+                {
+                    Console.WriteLine("Passed");
+                    return Pass;
+                }
+            }
+            else
+            {
+                Console.WriteLine("SSE2 not supported");
+                return Pass;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public unsafe static (uint, uint) Repro()
+        {
+            uint* a = stackalloc uint[4];
+            a[0] = 1;
+            a[1] = 1;
+            a[2] = 1;
+            a[3] = 1;
+            
+            // Here we force populate the registers
+            var b = a[0];
+            var h = a[3];
+            var c = a[1];
+            
+            // We operate the values in xmm0
+            Vector128<uint> v = Sse2.LoadVector128(a);
+            v = Sse2.Add(v, v);
+            // We send to the memory the modified values
+            Sse2.Store(a, v);
+            
+            // We return both sums (from registers and from memory)
+            return (b + h + c, a[0]+a[1]+a[3]);
+        }
+    }
+}
