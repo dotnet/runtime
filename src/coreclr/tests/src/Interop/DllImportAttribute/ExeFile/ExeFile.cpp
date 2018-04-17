211f34a989498ee634c8ec00104789df395ece37
Luqun Lou <luqunl@users.noreply.github.com>
2018-04-16 22:10:29 -0700

    Enable support DllImport a native assembly whose name contains '.' (dotnet/coreclr#17505)
    Commit migrated from https://github.com/dotnet/coreclr/commit/9d45401c6056a5b9c92d036e25c34ad34c03385e

---
+++ src/coreclr/tests/src/Interop/DllImportAttribute/ExeFile/ExeFile.cpp
@@ -0,0 +1,21 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <xplatform.h>
+
+#pragma warning( push )
+#pragma warning( disable : 4996)
+
+extern "C" DLL_EXPORT int _cdecl Sum(int a, int b)
+{
+    return a + b;
+}
+
+extern "C" int _cdecl main(int argc,  char **argv)
+{
+    return 0;
+}
