801882177f3c71b1c2b99ed581d62ad7c4fd18e5
Brian Robbins <brianrob@microsoft.com>
2018-04-24 19:11:48 -0700

    Patch LTTng ust-tracepoint.h on ARM/Trusty when building crossrootfs. (#17762)

--- cross/build-rootfs.sh
+++ cross/build-rootfs.sh
@@ -155,6 +155,7 @@ if [[ -n $__LinuxCodeName ]]; then
     if [[ "$__BuildArch" == "arm" && "$__LinuxCodeName" == "trusty" ]]; then
         pushd $__RootfsDir
         patch -p1 < $__CrossDir/$__BuildArch/trusty.patch
+        patch -p1 < $__CrossDir/$__BuildArch/trusty-lttng-2.4.patch
         popd
     fi
 elif [ "$__Tizen" == "tizen" ]; then
