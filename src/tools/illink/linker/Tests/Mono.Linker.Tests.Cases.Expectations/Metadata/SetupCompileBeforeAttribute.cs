70fe716a1930f1c2e8e9f723b7df361b4d097d5d
Michael Voorhees <michaelv@unity3d.com>
2018-04-25 13:35:59 -0400

    Test Framework, Support locating files by type
    Support defining files by the type they are in instead of relative path strings.  This mechanism is more robust to refactoring or doesn't require `../` adjustments when putting tests into sub directories
    
    Note, I purposefully did not add a new overload accepting types for SetupCompileAfterAttribute.  Because of the way that attribute works, it's easy to end up with typeof(InAnAssemblyThatIsNotReferencedIBeforeCompilingTheTest)  which leads to confusing errors.  I don't think this is a big deal, SetupCompileAfter is only used in niche cases
    
    Commit migrated from https://github.com/dotnet/linker/commit/1a5597ef129cd401679ed03b916c24ef4563d081

--- src/tools/illink/linker/Tests/Mono.Linker.Tests.Cases.Expectations/Metadata/SetupCompileBeforeAttribute.cs
+++ src/tools/illink/linker/Tests/Mono.Linker.Tests.Cases.Expectations/Metadata/SetupCompileBeforeAttribute.cs
@@ -14,5 +14,14 @@ namespace Mono.Linker.Tests.Cases.Expectations.Metadata {
 			if (string.IsNullOrEmpty (outputName))
 				throw new ArgumentException ("Value cannot be null or empty.", nameof (outputName));
 		}
+		
+		public SetupCompileBeforeAttribute (string outputName, Type[] typesToIncludeSourceFor, string[] references = null, string[] defines = null, string[] resources = null, string additionalArguments = null, string compilerToUse = null, bool addAsReference = true)
+		{
+			if (typesToIncludeSourceFor == null)
+				throw new ArgumentNullException (nameof (typesToIncludeSourceFor));
+
+			if (string.IsNullOrEmpty (outputName))
+				throw new ArgumentException ("Value cannot be null or empty.", nameof (outputName));
+		}
 	}
 }
