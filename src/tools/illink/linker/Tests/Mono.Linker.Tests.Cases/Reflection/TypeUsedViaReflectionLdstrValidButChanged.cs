dbc160cde2b70d66762bd4dd02cb0783a6564ded
Unknown <michaelv@unity3d.com>
2018-05-10 22:36:11 -0400

    Harden TypeNameParsing to avoid crashes
    * Fix crash processing class libs due to type name portion of found ldstr being empty.  Ex: `, System`
    
    * Fix crash that could have happened when closing `]` was missing.  Ex : `A]`
    
    * Fix infinite loop that could have happened when `[]` were out of order.  Ex: `A][`
    
    * Add tests for some other odd scenarios to make sure they behave as expected
    
    Commit migrated from https://github.com/dotnet/linker/commit/0aaf72e7d4880c900b9b13f7b562673dbb7c7714

---
+++ src/tools/illink/linker/Tests/Mono.Linker.Tests.Cases/Reflection/TypeUsedViaReflectionLdstrValidButChanged.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
+
+namespace Mono.Linker.Tests.Cases.Reflection {
+	/// <summary>
+	/// We don't know if `typeName` will be changed or not.  If we error on the side of caution and preserve something
+	/// that we found, I don't think that's a big deal
+	/// </summary>
+	public class TypeUsedViaReflectionLdstrValidButChanged {
+		public static void Main ()
+		{
+			var replace = "Mono.Linker";
+			var with = "Blah.Blah";
+			var typeName = "Mono.Linker.Tests.Cases.Reflection.TypeUsedViaReflectionLdstrValidButChanged+Full, test";
+			var typeKept = Type.GetType (typeName.Replace (replace, with), false);
+		}
+
+		[Kept]
+		public class Full { }
+	}
+}
