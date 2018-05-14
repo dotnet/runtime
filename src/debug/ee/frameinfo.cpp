ed9c82a956116f78fdaef781d99c3a22b01bfd27
David Wrighton <davidwr@microsoft.com>
2018-05-14 16:20:12 -0700

    Fix multicast delegate step from delegate (#17990)
    - Replace incorrect FEATURE_STUBS_AS_IL define check with FEATURE_MULTICASTSTUB_AS_IL
      - Allows step from one delegate to the next and step out from multicast delegate to function correctly.
    - Add null check as the active frame's methoddesc isn't always non-null
      - In scenarios such as FuncEval, we also run through this code, and the md may be null.
    - Fix unwind from StubHelpers::MulticastDebuggerTraceHelper function on amd64

--- src/debug/ee/frameinfo.cpp
+++ src/debug/ee/frameinfo.cpp
@@ -1563,7 +1563,7 @@ StackWalkAction DebuggerWalkStackProc(CrawlFrame *pCF, void *data)
     // The only exception is dynamic methods.  We want to report them when SIS is turned on.
     if ((md != NULL) && md->IsILStub() && pCF->IsFrameless())
     {
-#ifdef FEATURE_STUBS_AS_IL
+#ifdef FEATURE_MULTICASTSTUB_AS_IL
         if(md->AsDynamicMethodDesc()->IsMulticastStub())
         {
             use = true;
