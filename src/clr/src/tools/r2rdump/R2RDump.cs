e568a9cda018736d2db980235898bc4aa4d94c4b
acmyu <amycmyu@gmail.com>
2018-05-14 10:23:07 -0700

    R2RDump - Print ReadyToRun image headers (dotnet/coreclr#17942)
    * R2RDump - Outputing ReadyToRun header from PE image
    
    * R2RDump - Comments, follow coding guidelines, use enum for section types and flags
    
    * R2RDump - Use utf8 encoding, rva from ManagedNativeHeader, return exit code
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/cf7489c07eafed047b776a8219d701215fbc6afe

---
+++ src/clr/src/tools/r2rdump/R2RDump.cs
@@ -0,0 +1,45 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+
+namespace R2RDump
+{
+    class R2RDump
+    {
+        public static int Main(string[] args)
+        {
+            try
+            {
+                if (args.Length < 1)
+                {
+                    throw new System.ArgumentException("File name must be passed as argument");
+                }
+
+                R2RReader r2r = new R2RReader(args[0]);
+
+                if (r2r.IsR2R)
+                {
+                    Console.WriteLine($"Filename: {r2r.Filename}");
+                    Console.WriteLine($"Machine: {r2r.Machine}");
+                    Console.WriteLine($"ImageBase: 0x{r2r.ImageBase:X8}");
+
+                    Console.WriteLine("============== R2R Header ==============");
+                    Console.WriteLine(r2r.R2RHeader.ToString());
+                    for (int i = 0; i < r2r.R2RHeader.NumberOfSections; i++)
+                    {
+                        Console.WriteLine("------------------");
+                        Console.WriteLine(r2r.R2RHeader.Sections[i].ToString());
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.ToString());
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
---
+++ src/clr/src/tools/r2rdump/R2RSection.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace R2RDump
+{
+    struct R2RSection
+    {
+        public enum SectionType
+        {
+            READYTORUN_SECTION_COMPILER_IDENTIFIER = 100,
+            READYTORUN_SECTION_IMPORT_SECTIONS = 101,
+            READYTORUN_SECTION_RUNTIME_FUNCTIONS = 102,
+            READYTORUN_SECTION_METHODDEF_ENTRYPOINTS = 103,
+            READYTORUN_SECTION_EXCEPTION_INFO = 104,
+            READYTORUN_SECTION_DEBUG_INFO = 105,
+            READYTORUN_SECTION_DELAYLOAD_METHODCALL_THUNKS = 106,
+            READYTORUN_SECTION_AVAILABLE_TYPES = 108,
+            READYTORUN_SECTION_INSTANCE_METHOD_ENTRYPOINTS = 109,
+            READYTORUN_SECTION_INLINING_INFO = 110,
+            READYTORUN_SECTION_PROFILEDATA_INFO = 111
+        }
+
+        /// <summary>
+        /// The ReadyToRun section type
+        /// </summary>
+        public SectionType Type { get; }
+
+        /// <summary>
+        /// The RVA to the section
+        /// </summary>
+        public uint RelativeVirtualAddress { get; }
+
+        /// <summary>
+        /// The size of the section
+        /// </summary>
+        public uint Size { get; }
+
+        public R2RSection(int type, uint rva, uint size)
+        {
+            if (Enum.IsDefined(typeof(SectionType), type)) { 
+                Type = (SectionType)type;
+            }
+            else
+            {
+                throw new System.BadImageFormatException("Invalid ReadyToRun section type");
+            }
+            RelativeVirtualAddress = rva;
+            Size = size;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat($"Type:  {Enum.GetName(typeof(SectionType), Type)} ({Type:D})\n");
+            sb.AppendFormat($"RelativeVirtualAddress: 0x{RelativeVirtualAddress:X8}\n");
+            sb.AppendFormat($"Size: {Size} bytes\n");
+            return sb.ToString();
+        }
+    }
+}
