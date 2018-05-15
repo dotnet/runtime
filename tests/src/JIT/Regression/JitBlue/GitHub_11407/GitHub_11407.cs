a7f129cce3ee1ccbc119d771ae517f7e4f18b782
Carol Eidt <carol.eidt@microsoft.com>
2018-05-15 13:57:09 -0700

    Add tests for struct-related issues

---
+++ tests/src/JIT/Regression/JitBlue/GitHub_11407/GitHub_11407.cs
@@ -0,0 +1,28 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+// This test has two effectively identical initializations of an
+// array of byte vs. an array of structs containing a single byte field.
+// They should generate the same code.
+
+using System;
+using System.Runtime.CompilerServices;
+
+class GitHub_11407
+{
+    struct foo { public byte b1, b2, b3, b4; }
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    static foo getfoo() { return new foo(); }
+
+    static int Main()
+    {
+        int returnVal = 100;
+        foo myFoo = getfoo();
+        if (myFoo.b1 != 0 || myFoo.b2 != 0 || myFoo.b3 != 0 || myFoo.b4 != 0)
+        {
+            returnVal = -1;
+        }
+        return returnVal;
+    }
+}
