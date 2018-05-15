a60b85b4d505438f800173dd9183c466fff7fb64
Carol Eidt <carol.eidt@microsoft.com>
2018-05-15 13:57:09 -0700

    Add tests for struct-related issues
    Commit migrated from https://github.com/dotnet/coreclr/commit/a7f129cce3ee1ccbc119d771ae517f7e4f18b782

---
+++ src/clr/tests/src/JIT/Regression/JitBlue/GitHub_5556/GitHub_5556.cs
@@ -0,0 +1,52 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+// This test has two effectively identical methods, one of which copies
+// its input paramter to a local, allowing it to be promoted.
+// The JIT should be able to generate identical code for these.
+
+using System;
+using System.Runtime.CompilerServices;
+
+class GitHub_5556
+{
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static long SpanAsParam(Span<long> span)
+    {
+        long value = 0;
+        for (int i = 0; i < span.Length; i++)
+        {
+            value = span[i];
+        }
+        return value;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static long SpanWithLocalCopy(Span<long> span)
+    {
+        var spanLocal = span;
+        long value = 0;
+        for (int i = 0; i < spanLocal.Length; i++)
+        {
+            value = spanLocal[i];
+        }
+        return value;
+    }
+
+    static int Main()
+    {
+        long[] a = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        Span<long> mySpan = new Span<long>(a);
+        int returnVal = 100;
+        if (SpanAsParam(mySpan) != 9)
+        {
+            returnVal = -1;
+        }
+        if (SpanWithLocalCopy(mySpan) != 9)
+        {
+            returnVal = -1;
+        }
+        return returnVal;
+    }
+}
