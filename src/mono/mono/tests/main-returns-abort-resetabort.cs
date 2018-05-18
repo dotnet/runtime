2715b81af26bb4e0600ec26355a974a186f8e4c0
Jay Krell <jay.krell@cornell.edu>
2018-05-18 14:23:21 -0700

    Disambiguate test output. (mono/mono#8755)
    Commit migrated from https://github.com/mono/mono/commit/21a4dfa09464e370835b4ef24b96afe8096c41e4

--- src/mono/mono/tests/main-returns-abort-resetabort.cs
+++ src/mono/mono/tests/main-returns-abort-resetabort.cs
@@ -14,48 +14,48 @@ public class foo {
 
 	public static void thread() {
 		try {
-			Console.WriteLine("Thread running");
+			Console.WriteLine("Thread running 1");
 			Thread.Sleep(500);
 		} catch(ThreadAbortException) {
 			Thread.ResetAbort();
-			Console.WriteLine("Abort reset!");
+			Console.WriteLine("Abort reset! 1");
 		} finally {
-			Console.WriteLine("ThreadAbortException finally");
+			Console.WriteLine("ThreadAbortException finally 1");
 		}
 		try {
-			Console.WriteLine("Thread running");
+			Console.WriteLine("Thread running 2");
 			Thread.Sleep(500);
 		} catch(ThreadAbortException) {
 			Thread.ResetAbort();
-			Console.WriteLine("Abort reset!");
+			Console.WriteLine("Abort reset! 2");
 		} finally {
-			Console.WriteLine("ThreadAbortException finally");
+			Console.WriteLine("ThreadAbortException finally 2");
 		}
 		try {
-			Console.WriteLine("Thread running");
+			Console.WriteLine("Thread running 3");
 			Thread.Sleep(500);
 		} catch(ThreadAbortException) {
 			Thread.ResetAbort();
-			Console.WriteLine("Abort reset!");
+			Console.WriteLine("Abort reset! 3");
 		} finally {
-			Console.WriteLine("ThreadAbortException finally");
+			Console.WriteLine("ThreadAbortException finally 3");
 		}
 		try {
-			Console.WriteLine("Thread running");
+			Console.WriteLine("Thread running 4");
 			Thread.Sleep(500);
 		} catch(ThreadAbortException) {
 			Thread.ResetAbort();
-			Console.WriteLine("Abort reset!");
+			Console.WriteLine("Abort reset! 4");
 		} finally {
-			Console.WriteLine("ThreadAbortException finally");
+			Console.WriteLine("ThreadAbortException finally 4");
 		}
 		try {
-			Console.WriteLine("Thread running");
+			Console.WriteLine("Thread running 5");
 		} catch(ThreadAbortException) {
 			Thread.ResetAbort();
-			Console.WriteLine("Abort reset!");
+			Console.WriteLine("Abort reset! 5");
 		} finally {
-			Console.WriteLine("ThreadAbortException finally");
+			Console.WriteLine("ThreadAbortException finally 5");
 		}
 	}
 }
