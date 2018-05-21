e8f4d4704239c1ba2398b3d5767d5da0469d5bb6
Gleb Balykov <g.balykov@samsung.com>
2018-05-21 18:33:40 +0300

    Update Tizen CI docker image (dotnet/coreclr#17814)
    * Update Tizen CI docker image
    
    * Revert "Remove Tizen armel automatic PR triggered jobs"
    
    This reverts commit dotnet/coreclr@462e2b59c67b0b6d25985a7284616539f348cbfc.
    
    * Update Tizen CI docker image in netci
    
    * Update links required for generation of Tizen rootfs
    
    * Remove -rebuild from arm32 CI docker build
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/338474d63adf4a896e39b96fb0a4a2c7dd7a7985

--- src/coreclr/cross/armel/tizen-fetch.sh
+++ src/coreclr/cross/armel/tizen-fetch.sh
@@ -51,7 +51,7 @@ if [ ! -d $TMPDIR ]; then
 	mkdir -p $TMPDIR 
 fi
 
-TIZEN_URL=http://download.tizen.org/releases/daily/tizen
+TIZEN_URL=http://download.tizen.org/snapshots/tizen
 BUILD_XML=build.xml
 REPOMD_XML=repomd.xml
 PRIMARY_XML=primary.xml
@@ -157,20 +157,15 @@ fetch_tizen_pkgs()
 Inform "Initialize arm 4.0-base"
 fetch_tizen_pkgs_init arm 4.0-base
 Inform "fetch common packages"
-fetch_tizen_pkgs armv7l gcc glibc glibc-devel
+fetch_tizen_pkgs armv7l gcc glibc glibc-devel libicu libicu-devel
 fetch_tizen_pkgs noarch linux-glibc-devel
 Inform "fetch coreclr packages"
-fetch_tizen_pkgs armv7l lldb lldb-devel libgcc libstdc++ libstdc++-devel libunwind libunwind-devel
+fetch_tizen_pkgs armv7l lldb lldb-devel libgcc libstdc++ libstdc++-devel libunwind libunwind-devel tizen-release lttng-ust-devel lttng-ust userspace-rcu-devel userspace-rcu
 Inform "fetch corefx packages"
-fetch_tizen_pkgs armv7l libcom_err libcom_err-devel zlib zlib-devel libopenssl libopenssl-devel
+fetch_tizen_pkgs armv7l libcom_err libcom_err-devel zlib zlib-devel libopenssl libopenssl-devel krb5 krb5-devel libcurl libcurl-devel
 
 Inform "Initialize standard 4.0-unified"
 fetch_tizen_pkgs_init standard 4.0-unified
-Inform "fetch common packages"
-fetch_tizen_pkgs armv7l libicu-devel
-Inform "fetch coreclr packages"
-fetch_tizen_pkgs armv7l tizen-release lttng-ust-devel lttng-ust userspace-rcu-devel userspace-rcu
 Inform "fetch corefx packages"
-fetch_tizen_pkgs armv7l gssdp gssdp-devel krb5 krb5-devel libcurl libcurl-devel
-
+fetch_tizen_pkgs armv7l gssdp gssdp-devel
 
