0bc68257a0d8cd734123f4ac9df5af17766ac763
Maryam Ariyan <maryam.ariyan@microsoft.com>
2018-05-22 11:26:07 -0700

    Move InsufficientMemory, OutOfMemory and ThreadInterrupted exceptions (dotnet/coreclr#18049)
    * Move following exceptions to shared:
    
        - InsufficientMemory,
        - OutOfMemory
        - ThreadInterrupted
    
    Related to: dotnet/coreclrdotnet/coreclr#17904
    
    Reduced diff in RegistryKey visible between coreclr and corert
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/32ab54d345a4ab2eb572fe3ba061b6e0d1b31088

--- src/coreclr/src/System.Private.CoreLib/shared/System/InsufficientMemoryException.cs
+++ src/coreclr/src/System.Private.CoreLib/shared/System/InsufficientMemoryException.cs
@@ -2,32 +2,30 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-/*=============================================================================
-**
-**
-**
-** Purpose: The exception class for running out of memory
-** but most likely in a non-fatal way that shouldn't 
-** be affected by escalation policy.  Use this for cases
-** like MemoryFailPoint or a TryAllocate method, where you 
-** expect OOM's with no shared state corruption and you
-** want to recover from these errors.
-**
-**
-=============================================================================*/
-
-
 using System;
 using System.Runtime.Serialization;
 
 namespace System
 {
+    /// <summary>
+    /// Purpose: The exception class for running out of memory
+    /// but most likely in a non-fatal way that shouldn't 
+    /// be affected by escalation policy.  Use this for cases
+    /// like MemoryFailPoint or a TryAllocate method, where you 
+    /// expect OOM's with no shared state corruption and you
+    /// want to recover from these errors.
+    /// </summary>
     [Serializable]
     [System.Runtime.CompilerServices.TypeForwardedFrom("mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089")]
     public sealed class InsufficientMemoryException : OutOfMemoryException
     {
-        public InsufficientMemoryException()
-            : base(GetMessageFromNativeResources(ExceptionMessageKind.OutOfMemory))
+        public InsufficientMemoryException() : base(
+#if CORECLR
+            GetMessageFromNativeResources(ExceptionMessageKind.OutOfMemory)
+#else
+            SR.Arg_OutOfMemoryException
+#endif
+            )
         {
             HResult = HResults.COR_E_INSUFFICIENTMEMORY;
         }
