8cc7e09108b8ebac9afe2b97fc451716edee2866
Andy Ayers <andya@microsoft.com>
2018-05-31 12:25:22 -0700

    Add test case for optimized box vector assert (dotnet/coreclr#18163)
    Test case for issue dotnet/coreclr#18043 was left out of dotnet/coreclr#18046. Adding it now.
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/2ff2c6e4337eb2dfcb149b6649befa9f82ccf372

---
+++ src/clr/tests/src/jit/Regression/JitBlue/GitHub_18043/GitHub_18043.cs
@@ -0,0 +1,26 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+// GitHub18043: ensure dead box optimizations treat Vector<float> as a struct type.
+
+class X
+{
+    public static int VT()
+    {
+        Vector<float> f = new Vector<float>(4);
+        Vector<float>[] a = new Vector<float>[10];
+        a[5] = f;
+        return Array.IndexOf(a, f);
+    }
+
+    public static int Main()
+    {
+        int r1 = VT();
+        return (r1 == 5 ? 100 : 0);
+    }
+}
