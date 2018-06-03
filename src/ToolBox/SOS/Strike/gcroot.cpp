9791a661155d08f85f862bfc37eff3cd8d7b25cc
John Doe <github.john.doe@outlook.com>
2018-06-02 19:03:47 -0700

    Typo (#18261)
    * accomodate -> accommodate
    
    * accross -> across
    
    * adddress -> address
    
    * bookeeping -> bookkeeping
    
    * arbitary -> arbitrary
    
    * calulating -> calculating
    
    * fromClas -> fromClass
    
    * comand -> command
    
    * comming -> coming
    
    * comparisions -> comparisons
    
    * configuiration -> configuration
    
    * contraints -> constraints
    
    * datastructures -> data structures
    
    * defintely -> definitely
    
    * defered -> deferred
    
    * diassemble -> disassemble
    
    * Emptry -> Empty
    
    * everytime -> every time
    
    * fragmetation -> fragmentation
    
    * instaces -> instances
    
    * interpretting -> interpreting
    
    * intialize -> initialize
    
    * loction -> location

--- src/ToolBox/SOS/Strike/gcroot.cpp
+++ src/ToolBox/SOS/Strike/gcroot.cpp
@@ -516,7 +516,7 @@ int GCRootImpl::PrintRootsInOlderGen()
 
         if (!analyzeData.heap_analyze_success)
         {
-            ExtOut("Failed to gather needed data, possibly due to memory contraints in the debuggee.\n");
+            ExtOut("Failed to gather needed data, possibly due to memory constraints in the debuggee.\n");
             ExtOut("To try again re-issue the !FindRoots -gen <N> command.\n");
             return 0;
         }
@@ -558,7 +558,7 @@ int GCRootImpl::PrintRootsInOlderGen()
 
             if (!analyzeData.heap_analyze_success)
             {
-                ExtOut("Failed to gather needed data, possibly due to memory contraints in the debuggee.\n");
+                ExtOut("Failed to gather needed data, possibly due to memory constraints in the debuggee.\n");
                 ExtOut("To try again re-issue the !FindRoots -gen <N> command.\n");
                 continue;
             }
@@ -1663,7 +1663,7 @@ BOOL FindSegment(const DacpGcHeapDetails &heap, DacpHeapSegmentData &seg, CLRDAT
 BOOL VerifyObject(const DacpGcHeapDetails &heap, DWORD_PTR objAddr, DWORD_PTR MTAddr, size_t objSize, BOOL bVerifyMember)
 {
     // This is only used by the other VerifyObject function if bVerifyMember is true,
-    // so we only intialize it if we need it for verifying object members.
+    // so we only initialize it if we need it for verifying object members.
     DacpHeapSegmentData seg;
 
     if (bVerifyMember)
@@ -2161,7 +2161,7 @@ void HeapTraverser::PrintRefs(size_t obj, size_t methodTable, size_t size)
     
     // TODO: pass info to callback having to lookup the MethodTableInfo again
     MethodTableInfo* info = g_special_mtCache.Lookup((DWORD_PTR)methodTable);
-    _ASSERTE(info->IsInitialized());    // This is the second pass, so we should be intialized
+    _ASSERTE(info->IsInitialized());    // This is the second pass, so we should be initialized
 
     if (!info->bContainsPointers)
         return;
