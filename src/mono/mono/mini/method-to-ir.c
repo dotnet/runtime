42aab72043f06f658b60b6ba4ceef7ac1327a260
Rodrigo Kumpera <kumpera@users.noreply.github.com>
2018-06-07 13:58:23 -0700

    [mini] Remove unneeded restriction on fast delegate creation for context-using generic delegate under gshared. (mono/mono#8987)
    The only reason for the restriction was that handle_alloc previously would not handle generic instances.
    
    This speeded up the following microbenchmark by 19x (3.38s x 0.17s):
    
    ```
    class Driver {
    	static T Identity<T> (T t) {
    		return t;
    	}
    
    	[MethodImpl(MethodImplOptions.NoInlining)]
    	static Func<T,T> MakeDelegate<T> () {
    		return new Func<T, T> (Identity);
    	}
    
    	static void Main () {
    		object o = new object ();
    		for (int i = 0; i < 400 * 1000; ++i) {
    			MakeDelegate<object>()(o);
    		}
    	}
    }
    ```
    
    Commit migrated from https://github.com/mono/mono/commit/0c9719c496307070e19c21ce3317b3109a3370b2

--- src/mono/mono/mini/method-to-ir.c
+++ src/mono/mono/mini/method-to-ir.c
@@ -11463,8 +11463,7 @@ mono_ldptr:
 						}
 					}
 
-					/* FIXME: SGEN support */
-					if (invoke_context_used == 0 || cfg->llvm_only) {
+					if ((invoke_context_used == 0 || !cfg->gsharedvt) || cfg->llvm_only) {
 						if (cfg->verbose_level > 3)
 							g_print ("converting (in B%d: stack: %d) %s", cfg->cbb->block_num, (int)(sp - stack_start), mono_disasm_code_one (NULL, method, ip + 6, NULL));
 						if ((handle_ins = handle_delegate_ctor (cfg, ctor_method->klass, target_ins, cmethod, context_used, FALSE))) {
@@ -11525,8 +11524,7 @@ mono_ldptr:
 					if (mono_security_core_clr_enabled ())
 						ensure_method_is_allowed_to_call_method (cfg, method, ctor_method);
 
-					/* FIXME: SGEN support */
-					if (invoke_context_used == 0 || cfg->llvm_only) {
+					if ((invoke_context_used == 0 || !cfg->gsharedvt) || cfg->llvm_only) {
 						if (cfg->verbose_level > 3)
 							g_print ("converting (in B%d: stack: %d) %s", cfg->cbb->block_num, (int)(sp - stack_start), mono_disasm_code_one (NULL, method, ip + 6, NULL));
 						if ((handle_ins = handle_delegate_ctor (cfg, ctor_method->klass, target_ins, cmethod, context_used, is_virtual))) {
