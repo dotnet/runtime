a5539e28e6d7f05788ed6537373a9619b1e8e1bc
Aleksey Kliger <alklig@microsoft.com>
2018-06-13 18:18:35 -0400

    Revert "[mini] Remove unneeded restriction on fast delegate creation for context-using generic delegate under gshared. (mono/mono#8987)"
    This reverts commit mono/mono@0c9719c496307070e19c21ce3317b3109a3370b2.
    
    
    Commit migrated from https://github.com/mono/mono/commit/2bedfdee2c161b956591748c6f417cfb6629d1ae

--- src/mono/mono/mini/method-to-ir.c
+++ src/mono/mono/mini/method-to-ir.c
@@ -11511,7 +11511,8 @@ mono_ldptr:
 						}
 					}
 
-					if ((invoke_context_used == 0 || !cfg->gsharedvt) || cfg->llvm_only) {
+					/* FIXME: SGEN support */
+					if (invoke_context_used == 0 || cfg->llvm_only) {
 						if (cfg->verbose_level > 3)
 							g_print ("converting (in B%d: stack: %d) %s", cfg->cbb->block_num, (int)(sp - stack_start), mono_disasm_code_one (NULL, method, ip + 6, NULL));
 						if ((handle_ins = handle_delegate_ctor (cfg, ctor_method->klass, target_ins, cmethod, context_used, FALSE))) {
@@ -11572,7 +11573,8 @@ mono_ldptr:
 					if (mono_security_core_clr_enabled ())
 						ensure_method_is_allowed_to_call_method (cfg, method, ctor_method);
 
-					if ((invoke_context_used == 0 || !cfg->gsharedvt) || cfg->llvm_only) {
+					/* FIXME: SGEN support */
+					if (invoke_context_used == 0 || cfg->llvm_only) {
 						if (cfg->verbose_level > 3)
 							g_print ("converting (in B%d: stack: %d) %s", cfg->cbb->block_num, (int)(sp - stack_start), mono_disasm_code_one (NULL, method, ip + 6, NULL));
 						if ((handle_ins = handle_delegate_ctor (cfg, ctor_method->klass, target_ins, cmethod, context_used, is_virtual))) {
