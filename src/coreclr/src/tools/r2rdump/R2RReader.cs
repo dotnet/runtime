3061b39e7013d792eef1c81b02d0badf6a9a8998
Amy Yu <amycmyu@gmail.com>
2018-06-15 17:37:10 -0700

    Add missing registers enum
    Commit migrated from https://github.com/dotnet/coreclr/commit/377718dffb619796c4abb2751b934c03e4ab7db8

--- src/coreclr/src/tools/r2rdump/R2RReader.cs
+++ src/coreclr/src/tools/r2rdump/R2RReader.cs
@@ -12,6 +12,26 @@ using System.Text;
 
 namespace R2RDump
 {
+    public enum Amd64Registers
+    {
+        EAX = 0,
+        ECX = 1,
+        EDX = 2,
+        EBX = 3,
+        ESP = 4,
+        EBP = 5,
+        ESI = 6,
+        EDI = 7,
+        E8 = 8,
+        E9 = 9,
+        E10 = 10,
+        E11 = 11,
+        E12 = 12,
+        E13 = 13,
+        E14 = 14,
+        E15 = 15,
+    }
+
     class R2RReader
     {
         private readonly PEReader _peReader;
