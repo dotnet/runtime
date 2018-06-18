fabefabf17822d69f548d6a084dc2ff75e3669ec
Tomas Weinfurt <tweinfurt@yahoo.com>
2018-06-18 12:54:23 -0700

    temporarily disable pthread based named mutexes on FreeBSD (dotnet/coreclr#18480)
    Commit migrated from https://github.com/dotnet/coreclr/commit/ddca735a59327dedc681900e7d484491b769b7d6

--- src/coreclr/src/pal/src/include/pal/mutex.hpp
+++ src/coreclr/src/pal/src/include/pal/mutex.hpp
@@ -71,7 +71,7 @@ DWORD SPINLOCKTryAcquire (LONG * lock);
 
 // Temporarily disabling usage of pthread process-shared mutexes on ARM/ARM64 due to functional issues that cannot easily be
 // detected with code due to hangs. See https://github.com/dotnet/coreclr/issues/5456.
-#if HAVE_FULLY_FEATURED_PTHREAD_MUTEXES && HAVE_FUNCTIONAL_PTHREAD_ROBUST_MUTEXES && !(defined(_ARM_) || defined(_ARM64_))
+#if HAVE_FULLY_FEATURED_PTHREAD_MUTEXES && HAVE_FUNCTIONAL_PTHREAD_ROBUST_MUTEXES && !(defined(_ARM_) || defined(_ARM64_) || defined(__FreeBSD__))
     #define NAMED_MUTEX_USE_PTHREAD_MUTEX 1
 #else
     #define NAMED_MUTEX_USE_PTHREAD_MUTEX 0
