57e9204da87febf1115233b8eb40ef81d2d3b034
Tanner Gooding <tagoo@outlook.com>
2018-06-30 11:28:47 -0700

    Removing unnecessary `try/catch` blocks from the ExtractScalar and InsertScalar HWIntrinsic test templates
    Commit migrated from https://github.com/dotnet/coreclr/commit/1613245a1e5d844825ed67dc73698ffe2e3129fa

--- src/coreclr/tests/src/JIT/HardwareIntrinsics/X86/Sse41/Insert.Byte.1.cs
+++ src/coreclr/tests/src/JIT/HardwareIntrinsics/X86/Sse41/Insert.Byte.1.cs
@@ -22,9 +22,7 @@ namespace JIT.HardwareIntrinsics.X86
         private static void InsertByte1()
         {
             var test = new SimpleUnaryOpTest__InsertByte1();
-            
-            try
-            {
+
             if (test.IsSupported)
             {
                 // Validates basic functionality works, using Unsafe.Read
@@ -77,11 +75,6 @@ namespace JIT.HardwareIntrinsics.X86
                 // Validates we throw on unsupported hardware
                 test.RunUnsupportedScenario();
             }
-            }
-            catch (PlatformNotSupportedException)
-            {
-                test.Succeeded = true;
-            }
 
             if (!test.Succeeded)
             {
@@ -126,7 +119,7 @@ namespace JIT.HardwareIntrinsics.X86
             _dataTable = new SimpleUnaryOpTest__DataTable<Byte, Byte>(_data, new Byte[RetElementCount], LargestVectorSize);
         }
 
-        public bool IsSupported => Sse41.IsSupported;
+        public bool IsSupported => Sse41.IsSupported && (Environment.Is64BitProcess || ((typeof(Byte) != typeof(long)) && (typeof(Byte) != typeof(ulong))));
 
         public bool Succeeded { get; set; }
 
