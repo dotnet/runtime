7d1aac997b61428a3b33e1066e11e789372b75e0
Aleksey Kliger <alklig@microsoft.com>
2018-07-03 14:08:52 -0400

    [merp] Add suspend policy to native state summary
    Commit migrated from https://github.com/mono/mono/commit/9f340f50d72e455668a635824ef59307ce290c9e

--- src/mono/mono/utils/mono-state.c
+++ src/mono/mono/utils/mono-state.c
@@ -12,6 +12,7 @@
 #include <glib.h>
 #include <mono/utils/json.h>
 #include <mono/utils/mono-state.h>
+#include <mono/utils/mono-threads-coop.h>
 #include <mono/metadata/object-internals.h>
 
 #ifdef TARGET_OSX
@@ -342,11 +343,17 @@ mono_native_state_add_version (JsonWriter *writer)
 	mono_json_writer_object_key(writer, "llvm_support");
 #ifdef MONO_ARCH_LLVM_SUPPORTED
 #ifdef ENABLE_LLVM
-	mono_json_writer_printf (writer, "\"%d\"\n", LLVM_API_VERSION);
+	mono_json_writer_printf (writer, "\"%d\",\n", LLVM_API_VERSION);
 #else
-	mono_json_writer_printf (writer, "\"disabled\"\n");
+	mono_json_writer_printf (writer, "\"disabled\",\n");
 #endif
 #endif
+
+	const char *susp_policy = mono_threads_suspend_policy_name ();
+	mono_json_writer_indent (writer);
+	mono_json_writer_object_key (writer, "suspend");
+	mono_json_writer_printf (writer, "\"%s\"\n", susp_policy);
+
 
 	mono_json_writer_indent_pop (writer);
 	mono_json_writer_indent (writer);
