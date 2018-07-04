b50e4d6e65f1efb37bffd435c09539cd0f483bc6
Luqun Lou <luqunl@users.noreply.github.com>
2018-07-03 18:30:33 -0700

    Delete InternalsVisibleTo attributes from S.P.CoreLib (dotnet/coreclr#18697)
    Commit migrated from https://github.com/dotnet/coreclr/commit/c5dce0a3c0c5046aacde93c7ee2e35371d15ad6f

--- src/coreclr/src/System.Private.CoreLib/src/mscorlib.Friends.cs
+++
@@ -1,8 +0,0 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
-
-using System.Runtime.CompilerServices;
-
-// Depends on things like WindowsRuntimeImportAttribute
-[assembly: InternalsVisibleTo("System.Runtime.WindowsRuntime, PublicKey=00000000000000000400000000000000", AllInternalsVisible = false)]
