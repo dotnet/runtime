11bb697f337a558a908f70b7cc0bd87d950a76b2
Tanner Gooding <tagoo@outlook.com>
2018-07-05 23:36:30 -0700

    Adding tests for the AndNot, ExtractLowestSetBit, GetMaskUpToLowestSetBit, and ResetLowestSetBit x86 HWIntrinsics
    Commit migrated from https://github.com/dotnet/coreclr/commit/a1a58001acecb3779f06087d93470bcf515c2cf6

---
+++ src/coreclr/tests/src/JIT/HardwareIntrinsics/X86/Bmi1/AndNot.UInt32.cs
@@ -0,0 +1,178 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+/******************************************************************************
+ * This file is auto-generated from a template file by the GenerateTests.csx  *
+ * script in tests\src\JIT\HardwareIntrinsics\X86\Shared. In order to make    *
+ * changes, please update the corresponding template and run according to the *
+ * directions listed in the file.                                             *
+ ******************************************************************************/
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
+
+namespace JIT.HardwareIntrinsics.X86
+{
+    public static partial class Program
+    {
+        private static void AndNotUInt32()
+        {
+            var test = new ScalarBinaryOpTest__AndNotUInt32();
+
+            if (test.IsSupported)
+            {
+                // Validates basic functionality works, using Unsafe.ReadUnaligned
+                test.RunBasicScenario_UnsafeRead();
+
+                // Validates calling via reflection works, using Unsafe.ReadUnaligned
+                test.RunReflectionScenario_UnsafeRead();
+
+                // Validates passing a static member works
+                test.RunClsVarScenario();
+
+                // Validates passing a local works, using Unsafe.ReadUnaligned
+                test.RunLclVarScenario_UnsafeRead();
+
+                // Validates passing the field of a local works
+                test.RunLclFldScenario();
+
+                // Validates passing an instance member works
+                test.RunFldScenario();
+            }
+            else
+            {
+                // Validates we throw on unsupported hardware
+                test.RunUnsupportedScenario();
+            }
+
+            if (!test.Succeeded)
+            {
+                throw new Exception("One or more scenarios did not complete as expected.");
+            }
+        }
+    }
+
+    public sealed unsafe class ScalarBinaryOpTest__AndNotUInt32
+    {
+        private static UInt32 _data1;
+        private static UInt32 _data2;
+
+        private static UInt32 _clsVar1;
+        private static UInt32 _clsVar2;
+
+        private UInt32 _fld1;
+        private UInt32 _fld2;
+
+        static ScalarBinaryOpTest__AndNotUInt32()
+        {
+            var random = new Random();
+            _clsVar1 = (uint)(random.Next(0, int.MaxValue));
+            _clsVar2 = (uint)(random.Next(0, int.MaxValue));
+        }
+
+        public ScalarBinaryOpTest__AndNotUInt32()
+        {
+            Succeeded = true;
+
+            var random = new Random();
+            
+            _fld1 = (uint)(random.Next(0, int.MaxValue));
+            _fld2 = (uint)(random.Next(0, int.MaxValue));
+
+            _data1 = (uint)(random.Next(0, int.MaxValue));
+            _data2 = (uint)(random.Next(0, int.MaxValue));
+        }
+
+        public bool IsSupported => Bmi1.IsSupported && (Environment.Is64BitProcess || ((typeof(UInt32) != typeof(long)) && (typeof(UInt32) != typeof(ulong))));
+
+        public bool Succeeded { get; set; }
+
+        public void RunBasicScenario_UnsafeRead()
+        {
+            var result = Bmi1.AndNot(
+                Unsafe.ReadUnaligned<UInt32>(ref Unsafe.As<UInt32, byte>(ref _data1)),
+                Unsafe.ReadUnaligned<UInt32>(ref Unsafe.As<UInt32, byte>(ref _data2))
+            );
+
+            ValidateResult(_data1, _data2, result);
+        }
+
+        public void RunReflectionScenario_UnsafeRead()
+        {
+            var result = typeof(Bmi1).GetMethod(nameof(Bmi1.AndNot), new Type[] { typeof(UInt32), typeof(UInt32) })
+                                     .Invoke(null, new object[] {
+                                        Unsafe.ReadUnaligned<UInt32>(ref Unsafe.As<UInt32, byte>(ref _data1)),
+                                        Unsafe.ReadUnaligned<UInt32>(ref Unsafe.As<UInt32, byte>(ref _data2))
+                                     });
+
+            ValidateResult(_data1, _data2, (UInt32)result);
+        }
+
+        public void RunClsVarScenario()
+        {
+            var result = Bmi1.AndNot(
+                _clsVar1,
+                _clsVar2
+            );
+
+            ValidateResult(_clsVar1, _clsVar2, result);
+        }
+
+        public void RunLclVarScenario_UnsafeRead()
+        {
+            var data1 = Unsafe.ReadUnaligned<UInt32>(ref Unsafe.As<UInt32, byte>(ref _data1));
+            var data2 = Unsafe.ReadUnaligned<UInt32>(ref Unsafe.As<UInt32, byte>(ref _data2));
+            var result = Bmi1.AndNot(data1, data2);
+
+            ValidateResult(data1, data2, result);
+        }
+
+        public void RunLclFldScenario()
+        {
+            var test = new ScalarBinaryOpTest__AndNotUInt32();
+            var result = Bmi1.AndNot(test._fld1, test._fld2);
+
+            ValidateResult(test._fld1, test._fld2, result);
+        }
+
+        public void RunFldScenario()
+        {
+            var result = Bmi1.AndNot(_fld1, _fld2);
+            ValidateResult(_fld1, _fld2, result);
+        }
+
+        public void RunUnsupportedScenario()
+        {
+            Succeeded = false;
+
+            try
+            {
+                RunBasicScenario_UnsafeRead();
+            }
+            catch (PlatformNotSupportedException)
+            {
+                Succeeded = true;
+            }
+        }
+
+        private void ValidateResult(UInt32 left, UInt32 right, UInt32 result, [CallerMemberName] string method = "")
+        {
+            var isUnexpectedResult = false;
+
+            isUnexpectedResult = ((~left & right) != result);
+
+            if (isUnexpectedResult)
+            {
+                Console.WriteLine($"{nameof(Bmi1)}.{nameof(Bmi1.AndNot)}<UInt32>(UInt32, UInt32): AndNot failed:");
+                Console.WriteLine($"    left: {left}");
+                Console.WriteLine($"   right: {right}");
+                Console.WriteLine($"  result: {result}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ src/coreclr/tests/src/JIT/HardwareIntrinsics/X86/Bmi1/AndNot.UInt64.cs
@@ -0,0 +1,178 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+/******************************************************************************
+ * This file is auto-generated from a template file by the GenerateTests.csx  *
+ * script in tests\src\JIT\HardwareIntrinsics\X86\Shared. In order to make    *
+ * changes, please update the corresponding template and run according to the *
+ * directions listed in the file.                                             *
+ ******************************************************************************/
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
+
+namespace JIT.HardwareIntrinsics.X86
+{
+    public static partial class Program
+    {
+        private static void AndNotUInt64()
+        {
+            var test = new ScalarBinaryOpTest__AndNotUInt64();
+
+            if (test.IsSupported)
+            {
+                // Validates basic functionality works, using Unsafe.ReadUnaligned
+                test.RunBasicScenario_UnsafeRead();
+
+                // Validates calling via reflection works, using Unsafe.ReadUnaligned
+                test.RunReflectionScenario_UnsafeRead();
+
+                // Validates passing a static member works
+                test.RunClsVarScenario();
+
+                // Validates passing a local works, using Unsafe.ReadUnaligned
+                test.RunLclVarScenario_UnsafeRead();
+
+                // Validates passing the field of a local works
+                test.RunLclFldScenario();
+
+                // Validates passing an instance member works
+                test.RunFldScenario();
+            }
+            else
+            {
+                // Validates we throw on unsupported hardware
+                test.RunUnsupportedScenario();
+            }
+
+            if (!test.Succeeded)
+            {
+                throw new Exception("One or more scenarios did not complete as expected.");
+            }
+        }
+    }
+
+    public sealed unsafe class ScalarBinaryOpTest__AndNotUInt64
+    {
+        private static UInt64 _data1;
+        private static UInt64 _data2;
+
+        private static UInt64 _clsVar1;
+        private static UInt64 _clsVar2;
+
+        private UInt64 _fld1;
+        private UInt64 _fld2;
+
+        static ScalarBinaryOpTest__AndNotUInt64()
+        {
+            var random = new Random();
+            _clsVar1 = (ulong)(random.Next(0, int.MaxValue));
+            _clsVar2 = (ulong)(random.Next(0, int.MaxValue));
+        }
+
+        public ScalarBinaryOpTest__AndNotUInt64()
+        {
+            Succeeded = true;
+
+            var random = new Random();
+            
+            _fld1 = (ulong)(random.Next(0, int.MaxValue));
+            _fld2 = (ulong)(random.Next(0, int.MaxValue));
+
+            _data1 = (ulong)(random.Next(0, int.MaxValue));
+            _data2 = (ulong)(random.Next(0, int.MaxValue));
+        }
+
+        public bool IsSupported => Bmi1.IsSupported && (Environment.Is64BitProcess || ((typeof(UInt64) != typeof(long)) && (typeof(UInt64) != typeof(ulong))));
+
+        public bool Succeeded { get; set; }
+
+        public void RunBasicScenario_UnsafeRead()
+        {
+            var result = Bmi1.AndNot(
+                Unsafe.ReadUnaligned<UInt64>(ref Unsafe.As<UInt64, byte>(ref _data1)),
+                Unsafe.ReadUnaligned<UInt64>(ref Unsafe.As<UInt64, byte>(ref _data2))
+            );
+
+            ValidateResult(_data1, _data2, result);
+        }
+
+        public void RunReflectionScenario_UnsafeRead()
+        {
+            var result = typeof(Bmi1).GetMethod(nameof(Bmi1.AndNot), new Type[] { typeof(UInt64), typeof(UInt64) })
+                                     .Invoke(null, new object[] {
+                                        Unsafe.ReadUnaligned<UInt64>(ref Unsafe.As<UInt64, byte>(ref _data1)),
+                                        Unsafe.ReadUnaligned<UInt64>(ref Unsafe.As<UInt64, byte>(ref _data2))
+                                     });
+
+            ValidateResult(_data1, _data2, (UInt64)result);
+        }
+
+        public void RunClsVarScenario()
+        {
+            var result = Bmi1.AndNot(
+                _clsVar1,
+                _clsVar2
+            );
+
+            ValidateResult(_clsVar1, _clsVar2, result);
+        }
+
+        public void RunLclVarScenario_UnsafeRead()
+        {
+            var data1 = Unsafe.ReadUnaligned<UInt64>(ref Unsafe.As<UInt64, byte>(ref _data1));
+            var data2 = Unsafe.ReadUnaligned<UInt64>(ref Unsafe.As<UInt64, byte>(ref _data2));
+            var result = Bmi1.AndNot(data1, data2);
+
+            ValidateResult(data1, data2, result);
+        }
+
+        public void RunLclFldScenario()
+        {
+            var test = new ScalarBinaryOpTest__AndNotUInt64();
+            var result = Bmi1.AndNot(test._fld1, test._fld2);
+
+            ValidateResult(test._fld1, test._fld2, result);
+        }
+
+        public void RunFldScenario()
+        {
+            var result = Bmi1.AndNot(_fld1, _fld2);
+            ValidateResult(_fld1, _fld2, result);
+        }
+
+        public void RunUnsupportedScenario()
+        {
+            Succeeded = false;
+
+            try
+            {
+                RunBasicScenario_UnsafeRead();
+            }
+            catch (PlatformNotSupportedException)
+            {
+                Succeeded = true;
+            }
+        }
+
+        private void ValidateResult(UInt64 left, UInt64 right, UInt64 result, [CallerMemberName] string method = "")
+        {
+            var isUnexpectedResult = false;
+
+            isUnexpectedResult = ((~left & right) != result);
+
+            if (isUnexpectedResult)
+            {
+                Console.WriteLine($"{nameof(Bmi1)}.{nameof(Bmi1.AndNot)}<UInt64>(UInt64, UInt64): AndNot failed:");
+                Console.WriteLine($"    left: {left}");
+                Console.WriteLine($"   right: {right}");
+                Console.WriteLine($"  result: {result}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
