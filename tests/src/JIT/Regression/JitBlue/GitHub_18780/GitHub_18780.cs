c06c61ab4d8c8d1b212fc7e9a9a130c63d47ed44
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2018-07-19 00:57:17 +0200

    Properly type morphed NEG nodes (#18837)
    * Properly type optimized NEG nodes
    
    When the JIT was morphing trees like '-1 * expr', it would turn the
    multiplication into a NEG node with the same type as its right operand.
    This is a problem when the right operand was a small type like TYP_UBYTE
    because the NEG node always produces a widened result. This could cause
    problems when the negation was fed into something that depended on the
    type, such as a cast to TYP_UBYTE: here the JIT would conclude that the
    cast could be dropped, and end up producing a widened result.
    
    The solution is to give the tree the actual type of the NEG node.
    
    Also add a test for this case and for a similar case of '0 - expr',
    which already had a fix.
    
    Fix #18780
    
    * Address PR feedback
    
    * Clarify comment

---
+++ tests/src/JIT/Regression/JitBlue/GitHub_18780/GitHub_18780.cs
@@ -0,0 +1,46 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Runtime.CompilerServices;
+
+public class GitHub_18780
+{
+    public static int Main()
+    {
+        bool ok = true;
+        ok &= M1(0);
+        ok &= M2();
+        ok &= M3();
+        return ok ? 100 : -1;
+    }
+
+    // The multiplication by uint.MaxValue was optimized to a NEG
+    // which was typed as the second operand, giving a byte NEG node.
+    // With x86/ARM32 RyuJIT the cast to ulong would then treat vr13
+    // as a byte instead of a uint and zero extend from 8 bits.
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    static bool M1(byte arg2)
+    {
+        byte vr23 = arg2++;
+        return (ulong)(uint.MaxValue * arg2) == uint.MaxValue;
+    }
+
+    // Like above, the -1 multiplication was turned into a byte NEG node.
+    // The byte cast was then removed, but since the NEG byte node still
+    // produces a wide result this meant (byte)val was -1.
+    static byte s_1 = 1;
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    static bool M2()
+    {
+        return (byte)(-1 * s_1) == 255;
+    }
+
+    // Exactly the same as above, but this tests the optimization for
+    // transforming (0 - expr) into -expr.
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    static bool M3()
+    {
+        return (byte)(0 - s_1) == 255;
+    }
+}
