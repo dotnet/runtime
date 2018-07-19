166e5f0628b12f200eff5ca36c31f6d1a9205a6a
Aaron Robinson <arobins@microsoft.com>
2018-07-18 17:52:02 -0700

    Basic .NET client tests (dotnet/coreclr#18843)
    - Add support for a testing primitive marshalling with a native COM server
    and .NET client. This uses RegFree COM for activation.
    - Remove ClassicCOM tests
    - Bring back tests for activation via reflection
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/f37214ab432e192178ada2d202328f5fd8884cff

---
+++ src/coreclr/tests/src/Interop/COM/NativeServer/ErrorMarshalTesting.h
@@ -0,0 +1,32 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#pragma once
+
+#include "Servers.h"
+
+class DECLSPEC_UUID("71CF5C45-106C-4B32-B418-43A463C6041F") ErrorMarshalTesting : public UnknownImpl, public IErrorMarshalTesting
+{
+public: // IErrorMarshalTesting
+    DEF_RAWFUNC(Throw_HResult)(
+        /*[in]*/ long hresultToReturn)
+    {
+        return HRESULT{ hresultToReturn };
+    }
+    long __stdcall Return_As_HResult(
+        /*[in]*/ long hresultToReturn)
+    {
+        return hresultToReturn;
+    }
+
+public: // IUnknown
+    STDMETHOD(QueryInterface)(
+        /* [in] */ REFIID riid,
+        /* [iid_is][out] */ _COM_Outptr_ void __RPC_FAR *__RPC_FAR *ppvObject)
+    {
+        return DoQueryInterface<ErrorMarshalTesting, IErrorMarshalTesting>(this, riid, ppvObject);
+    }
+
+    DEFINE_REF_COUNTING();
+};
