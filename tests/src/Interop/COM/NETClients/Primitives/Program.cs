f37214ab432e192178ada2d202328f5fd8884cff
Aaron Robinson <arobins@microsoft.com>
2018-07-18 17:52:02 -0700

    Basic .NET client tests (#18843)
    - Add support for a testing primitive marshalling with a native COM server
    and .NET client. This uses RegFree COM for activation.
    - Remove ClassicCOM tests
    - Bring back tests for activation via reflection

---
+++ tests/src/Interop/COM/NETClients/Primitives/Program.cs
@@ -0,0 +1,29 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace NetClient
+{
+    using System;
+
+    class Program
+    {
+        static int Main(string[] doNotUse)
+        {
+            try
+            {
+                new NumericTests().Run();
+                new ArrayTests().Run();
+                new StringTests().Run();
+                new ErrorTests().Run();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Test Failure: {e.Message}\n{e.StackTrace}");
+                return 101;
+            }
+
+            return 100;
+        }
+    }
+}
