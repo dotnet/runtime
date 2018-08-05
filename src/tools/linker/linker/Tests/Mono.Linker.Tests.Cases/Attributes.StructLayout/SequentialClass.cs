76f2128e029863d7a7a3169fd640108e8f32e276
Marek Safar <marek.safar@gmail.com>
2018-08-05 21:31:18 +0200

    Add more StructLayout tests
    Commit migrated from https://github.com/dotnet/linker/commit/5648356e4eabb55a39461e4d5d9a9bfcf42b2497

---
+++ src/tools/linker/linker/Tests/Mono.Linker.Tests.Cases/Attributes.StructLayout/SequentialClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
+
+namespace Mono.Linker.Tests.Cases.Attributes.StructLayout
+{
+	[StructLayout (LayoutKind.Sequential)]
+	[KeptMember (".ctor()")]
+	class SequentialClassData
+	{
+		[Kept]
+		public int never_used;
+		[Kept]
+		public int used;
+	}
+
+	public class SequentialClass
+	{
+		public static void Main ()
+		{
+			var c = new SequentialClassData ();
+			c.used = 1;
+			if (Marshal.SizeOf (c) != 8)
+				throw new ApplicationException ();
+		}
+	}
+}
--- src/tools/linker/linker/Tests/Mono.Linker.Tests.Cases/Attributes.StructLayout/UnusedTypeWithSequentialLayoutIsRemoved.cs
+++ src/tools/linker/linker/Tests/Mono.Linker.Tests.Cases/Attributes.StructLayout/UnusedTypeWithSequentialLayoutIsRemoved.cs
@@ -1,7 +1,8 @@
 ﻿using System.Runtime.InteropServices;
 using Mono.Linker.Tests.Cases.Expectations.Assertions;
 
-namespace Mono.Linker.Tests.Cases.Interop {
+namespace Mono.Linker.Tests.Cases.Attributes.StructLayout
+{
 	class UnusedTypeWithSequentialLayoutIsRemoved {
 		static void Main ()
 		{
