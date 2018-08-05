3d6135f273f32431512fb19f99eb93ca603e2d0c
Marek Safar <marek.safar@gmail.com>
2018-08-05 21:31:18 +0200

    Add more StructLayout tests
    Commit migrated from https://github.com/dotnet/linker/commit/5648356e4eabb55a39461e4d5d9a9bfcf42b2497

---
+++ src/tools/illink/linker/Tests/Mono.Linker.Tests.Cases/Attributes.StructLayout/ExplicitClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
+
+namespace Mono.Linker.Tests.Cases.Attributes.StructLayout {
+	[StructLayout (LayoutKind.Explicit)]
+	[KeptMember (".ctor()")]
+	class ExplicitClassData {
+		[FieldOffset (0)]
+		[Kept] // the linker could remove this
+		public int never_used;
+		[FieldOffset (4)]
+		[Kept]
+		public int used;
+		[FieldOffset (8)]
+		[Kept]
+		public int never_ever_used;
+	}
+
+	public class ExplicitClass
+	{
+		public static void Main ()
+		{
+			var c = new ExplicitClassData ();
+			c.used = 1;
+		}
+	}
+}
