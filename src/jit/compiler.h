aa1d28a7b24f896df4a389db5f8fa984e84764b7
Andy Ayers <andya@microsoft.com>
2018-08-06 18:55:56 -0700

    JIT: refactor ref count computation into a reusable utility method (#19240)
    Extract out the basic ref count computation into a method that we
    can conceptually call later on if we want to recompute counts.
    
    Move one existing RCS_EARLY count for promoted fields of register
    args into this recomputation since losing this count bump causes
    quite a few diffs.
    
    The hope is to eventually call this method again later in the jit
    phase pipeline and then possibly get rid of all the (non-early)
    incremental count maintenance we do currently.
    
    Part of #18969

--- src/jit/compiler.h
+++ src/jit/compiler.h
@@ -2790,9 +2790,9 @@ public:
     void lvaSortByRefCount();
     void lvaDumpRefCounts();
 
-    void lvaMarkLocalVars(BasicBlock* block);
-
     void lvaMarkLocalVars(); // Local variable ref-counting
+    void lvaComputeRefCounts(bool isRecompute, bool setSlotNumbers);
+    void lvaMarkLocalVars(BasicBlock* block, bool isRecompute);
 
     void lvaAllocOutgoingArgSpaceVar(); // Set up lvaOutgoingArgSpaceVar
 
@@ -2972,7 +2972,7 @@ public:
 protected:
     //---------------- Local variable ref-counting ----------------------------
 
-    void lvaMarkLclRefs(GenTree* tree, BasicBlock* block, GenTreeStmt* stmt);
+    void lvaMarkLclRefs(GenTree* tree, BasicBlock* block, GenTreeStmt* stmt, bool isRecompute);
     bool IsDominatedByExceptionalEntry(BasicBlock* block);
     void SetVolatileHint(LclVarDsc* varDsc);
 
