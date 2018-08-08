908f5f9653efcf9fde3d925086625d22f62c838b
Michael Voorhees <michaelv@unity3d.com>
2018-08-08 09:20:09 -0400

    Fix custom attributes on method return not being removed
    Commit migrated from https://github.com/dotnet/linker/commit/422c2192d7aa0386112ee642ca67e291414f0790

---
+++ src/tools/illink/linker/Tests/Mono.Linker.Tests.Cases/Attributes/OnlyKeepUsed/UnusedAttributeOnReturnTypeIsRemoved.cs
@@ -0,0 +1,17 @@
+﻿using Mono.Linker.Tests.Cases.Expectations.Assertions;
+using Mono.Linker.Tests.Cases.Expectations.Metadata;
+
+namespace Mono.Linker.Tests.Cases.Attributes.OnlyKeepUsed {
+	[SetupLinkerArgument ("--used-attrs-only", "true")]
+	[Define ("IL_ASSEMBLY_AVAILABLE")]
+	[SetupCompileBefore ("library.dll", new [] { "Dependencies/AssemblyWithUnusedAttributeOnReturnParameterDefinition.il" })]
+	[RemovedTypeInAssembly ("library.dll", "Mono.Linker.Tests.Cases.Attributes.OnlyKeepUsed.Dependencies.AssemblyWithUnusedAttributeOnReturnParameterDefinition/FooAttribute")]
+	public class UnusedAttributeOnReturnTypeIsRemoved {
+		static void Main ()
+		{
+#if IL_ASSEMBLY_AVAILABLE
+			var tmp = new Mono.Linker.Tests.Cases.Attributes.OnlyKeepUsed.Dependencies.AssemblyWithUnusedAttributeOnReturnParameterDefinition ().Method (1);
+#endif
+		}
+	}
+}
