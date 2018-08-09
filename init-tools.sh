db01a9371e3a12adc122a34ea974fa959ea63f6f
Tanner Gooding <tagoo@outlook.com>
2018-08-08 18:03:30 -0700

    Updating init-tools to be inline with the CoreFX/CoreCLR copy

--- init-tools.sh
+++ init-tools.sh
@@ -1,27 +1,28 @@
 #!/usr/bin/env bash
 
 __scriptpath=$(cd "$(dirname "$0")"; pwd -P)
-__init_tools_log=$__scriptpath/init-tools.log
-__PACKAGES_DIR=$__scriptpath/packages
-__TOOLRUNTIME_DIR=$__scriptpath/Tools
-__DOTNET_PATH=$__TOOLRUNTIME_DIR/dotnetcli
-__DOTNET_CMD=$__DOTNET_PATH/dotnet
-if [ -z "$__BUILDTOOLS_SOURCE" ]; then __BUILDTOOLS_SOURCE=https://dotnet.myget.org/F/dotnet-buildtools/api/v3/index.json; fi
+__init_tools_log="$__scriptpath/init-tools.log"
+__PACKAGES_DIR="$__scriptpath/packages"
+__TOOLRUNTIME_DIR="$__scriptpath/Tools"
+__DOTNET_PATH="$__TOOLRUNTIME_DIR/dotnetcli"
+__DOTNET_CMD="$__DOTNET_PATH/dotnet"
+if [ -z "${__BUILDTOOLS_SOURCE:-}" ]; then __BUILDTOOLS_SOURCE=https://dotnet.myget.org/F/dotnet-buildtools/api/v3/index.json; fi
 export __BUILDTOOLS_USE_CSPROJ=true
-__BUILD_TOOLS_PACKAGE_VERSION=$(cat $__scriptpath/BuildToolsVersion.txt)
-__DOTNET_TOOLS_VERSION=$(cat $__scriptpath/DotnetCLIVersion.txt)
-__BUILD_TOOLS_PATH=$__PACKAGES_DIR/microsoft.dotnet.buildtools/$__BUILD_TOOLS_PACKAGE_VERSION/lib
-__INIT_TOOLS_RESTORE_PROJECT=$__scriptpath/init-tools.msbuild
-__BUILD_TOOLS_SEMAPHORE=$__TOOLRUNTIME_DIR/$__BUILD_TOOLS_PACKAGE_VERSION/init-tools.complete
+__BUILD_TOOLS_PACKAGE_VERSION=$(cat "$__scriptpath/BuildToolsVersion.txt" | sed 's/\r$//') # remove CR if mounted repo on Windows drive
+__DOTNET_TOOLS_VERSION=$(cat "$__scriptpath/DotnetCLIVersion.txt" | sed 's/\r$//') # remove CR if mounted repo on Windows drive
+__ILASM_VERSION=$(cat "$__scriptpath/tools-local/ILAsmVersion.txt" | sed 's/\r$//') # remove CR if mounted repo on Windows drive
+__BUILD_TOOLS_PATH="$__PACKAGES_DIR/microsoft.dotnet.buildtools/$__BUILD_TOOLS_PACKAGE_VERSION/lib"
+__INIT_TOOLS_RESTORE_PROJECT="$__scriptpath/init-tools.msbuild"
+__BUILD_TOOLS_SEMAPHORE="$__TOOLRUNTIME_DIR/$__BUILD_TOOLS_PACKAGE_VERSION/init-tools.complete"
 
-if [ -e $__BUILD_TOOLS_SEMAPHORE ]; then
+if [ -e "$__BUILD_TOOLS_SEMAPHORE" ]; then
     echo "Tools are already initialized"
     return #return instead of exit because this script is inlined in other scripts which we don't want to exit
 fi
 
-if [ -e $__TOOLRUNTIME_DIR ]; then rm -rf -- $__TOOLRUNTIME_DIR; fi
+if [ -e "$__TOOLRUNTIME_DIR" ]; then rm -rf -- "$__TOOLRUNTIME_DIR"; fi
 
-if [ -d "$DotNetBuildToolsDir" ]; then
+if [ -d "${DotNetBuildToolsDir:-}" ]; then
     echo "Using tools from '$DotNetBuildToolsDir'."
     ln -s "$DotNetBuildToolsDir" "$__TOOLRUNTIME_DIR"
 
@@ -31,11 +32,11 @@ if [ -d "$DotNetBuildToolsDir" ]; then
     fi
 
     echo "Done initializing tools."
-    mkdir -p "$(dirname "$__BUILD_TOOLS_SEMAPHORE")" && touch $__BUILD_TOOLS_SEMAPHORE
+    mkdir -p "$(dirname "$__BUILD_TOOLS_SEMAPHORE")" && touch "$__BUILD_TOOLS_SEMAPHORE"
     return #return instead of exit because this script is inlined in other scripts which we don't want to exit
 fi
 
-echo "Running: $__scriptpath/init-tools.sh" > $__init_tools_log
+echo "Running: $__scriptpath/init-tools.sh" > "$__init_tools_log"
 
 display_error_message()
 {
@@ -64,8 +65,8 @@ execute_with_retry() {
     return 0
 }
 
-if [ ! -e $__DOTNET_PATH ]; then
-    if [ -z "$__DOTNET_PKG" ]; then
+if [ ! -e "$__DOTNET_PATH" ]; then
+    if [ -z "${__DOTNET_PKG:-}" ]; then
         if [ "$(uname -m | grep "i[3456]86")" = "i686" ]; then
             echo "Warning: build not supported on 32 bit Unix"
         fi
@@ -101,6 +102,12 @@ if [ ! -e $__DOTNET_PATH ]; then
                         __PKG_RID=rhel.6
                     fi
                 fi
+                OSArch=$(uname -m)
+                if [ $OSArch == 'armv7l' ];then
+                    __PKG_ARCH=arm
+                elif [ $OSArch == 'aarch64' ]; then
+                    __PKG_ARCH=arm64
+                fi
 
                 ;;
 
@@ -109,9 +116,9 @@ if [ ! -e $__DOTNET_PATH ]; then
                 OS=Linux
                 __PKG_RID=linux
                 ;;
-      esac
-    __PKG_RID=$__PKG_RID-$__PKG_ARCH
-    __DOTNET_PKG=dotnet-sdk-${__DOTNET_TOOLS_VERSION}-$__PKG_RID
+        esac
+        __PKG_RID=$__PKG_RID-$__PKG_ARCH
+        __DOTNET_PKG=dotnet-sdk-${__DOTNET_TOOLS_VERSION}-$__PKG_RID
     fi
     mkdir -p "$__DOTNET_PATH"
 
@@ -119,7 +126,7 @@ if [ ! -e $__DOTNET_PATH ]; then
     __DOTNET_LOCATION="https://dotnetcli.azureedge.net/dotnet/Sdk/${__DOTNET_TOOLS_VERSION}/${__DOTNET_PKG}.tar.gz"
 
     install_dotnet_cli() {
-        if [[ "$DotNetBootstrapCliTarPath" = "" ]]; then
+        if [[ -z "${DotNetBootstrapCliTarPath-}" ]]; then
             echo "Installing '${__DOTNET_LOCATION}' to '$__DOTNET_PATH/dotnet.tar'"
             rm -rf -- "$__DOTNET_PATH/*"
             # curl has HTTPS CA trust-issues less often than wget, so lets try that first.
@@ -132,18 +139,18 @@ if [ ! -e $__DOTNET_PATH ]; then
             echo "Copying '$DotNetBootstrapCliTarPath' to '$__DOTNET_PATH/dotnet.tar'"
             cp $DotNetBootstrapCliTarPath $__DOTNET_PATH/dotnet.tar
         fi
-        cd $__DOTNET_PATH
-        tar -xf $__DOTNET_PATH/dotnet.tar
+        cd "$__DOTNET_PATH"
+        tar -xf "$__DOTNET_PATH/dotnet.tar"
     }
     execute_with_retry install_dotnet_cli >> "$__init_tools_log" 2>&1
 
-    cd $__scriptpath
+    cd "$__scriptpath"
 fi
 
-if [ ! -e $__BUILD_TOOLS_PATH ]; then
+if [ ! -e "$__BUILD_TOOLS_PATH" ]; then
     echo "Restoring BuildTools version $__BUILD_TOOLS_PACKAGE_VERSION..."
-    echo "Running: $__DOTNET_CMD restore \"$__INIT_TOOLS_RESTORE_PROJECT\" --no-cache --packages $__PACKAGES_DIR --source $__BUILDTOOLS_SOURCE /p:BuildToolsPackageVersion=$__BUILD_TOOLS_PACKAGE_VERSION" >> $__init_tools_log
-    $__DOTNET_CMD restore "$__INIT_TOOLS_RESTORE_PROJECT" --no-cache --packages $__PACKAGES_DIR --source $__BUILDTOOLS_SOURCE /p:BuildToolsPackageVersion=$__BUILD_TOOLS_PACKAGE_VERSION >> $__init_tools_log
+    echo "Running: $__DOTNET_CMD restore \"$__INIT_TOOLS_RESTORE_PROJECT\" --no-cache --packages $__PACKAGES_DIR --source $__BUILDTOOLS_SOURCE /p:BuildToolsPackageVersion=$__BUILD_TOOLS_PACKAGE_VERSION /p:ToolsDir=$__TOOLRUNTIME_DIR" >> "$__init_tools_log"
+    "$__DOTNET_CMD" restore "$__INIT_TOOLS_RESTORE_PROJECT" --no-cache --packages "$__PACKAGES_DIR" --source "$__BUILDTOOLS_SOURCE" /p:BuildToolsPackageVersion=$__BUILD_TOOLS_PACKAGE_VERSION /p:ToolsDir="$__TOOLRUNTIME_DIR" >> "$__init_tools_log"
     if [ ! -e "$__BUILD_TOOLS_PATH/init-tools.sh" ]; then
         echo "ERROR: Could not restore build tools correctly." 1>&2
         display_error_message
@@ -151,11 +158,11 @@ if [ ! -e $__BUILD_TOOLS_PATH ]; then
 fi
 
 echo "Initializing BuildTools..."
-echo "Running: $__BUILD_TOOLS_PATH/init-tools.sh $__scriptpath $__DOTNET_CMD $__TOOLRUNTIME_DIR $__PACKAGES_DIR" >> $__init_tools_log
+echo "Running: $__BUILD_TOOLS_PATH/init-tools.sh $__scriptpath $__DOTNET_CMD $__TOOLRUNTIME_DIR $__PACKAGES_DIR" >> "$__init_tools_log"
 
 # Executables restored with .NET Core 2.0 do not have executable permission flags. https://github.com/NuGet/Home/issues/4424
-chmod +x $__BUILD_TOOLS_PATH/init-tools.sh
-$__BUILD_TOOLS_PATH/init-tools.sh $__scriptpath $__DOTNET_CMD $__TOOLRUNTIME_DIR $__PACKAGES_DIR >> $__init_tools_log
+chmod +x "$__BUILD_TOOLS_PATH/init-tools.sh"
+"$__BUILD_TOOLS_PATH/init-tools.sh" "$__scriptpath" "$__DOTNET_CMD" "$__TOOLRUNTIME_DIR" "$__PACKAGES_DIR" >> "$__init_tools_log"
 if [ "$?" != "0" ]; then
     echo "ERROR: An error occurred when trying to initialize the tools." 1>&2
     display_error_message
@@ -164,12 +171,12 @@ fi
 
 echo "Making all .sh files executable under Tools."
 # Executables restored with .NET Core 2.0 do not have executable permission flags. https://github.com/NuGet/Home/issues/4424
-ls $__scriptpath/Tools/*.sh | xargs chmod +x
-ls $__scriptpath/Tools/scripts/docker/*.sh | xargs chmod +x
+ls "$__scriptpath/Tools/"*.sh | xargs chmod +x
+ls "$__scriptpath/Tools/scripts/docker/"*.sh | xargs chmod +x
 
-Tools/crossgen.sh $__scriptpath/Tools $__PKG_RID
+"$__scriptpath/Tools/crossgen.sh" "$__scriptpath/Tools" $__PKG_RID
 
-mkdir -p "$(dirname "$__BUILD_TOOLS_SEMAPHORE")" && touch $__BUILD_TOOLS_SEMAPHORE
+mkdir -p "$(dirname "$__BUILD_TOOLS_SEMAPHORE")" && touch "$__BUILD_TOOLS_SEMAPHORE"
 
 echo "Done initializing tools."
 
