904464dd8a990eaef42a9e525d2d0d9739ef3383
Sergey Ignatov <sergign60@mail.ru>
2018-08-09 11:52:10 +0300

    [CoreRT armel]Fix for unwinding support for ARM is not fully implemented dotnet/coreclr#5874
    Commit migrated from https://github.com/dotnet/coreclr/commit/f4eaea5265b90bdf782bbd62d1cbc246f62a7a75

--- src/coreclr/src/jit/unwindarm.cpp
+++ src/coreclr/src/jit/unwindarm.cpp
@@ -194,6 +194,9 @@ void Compiler::unwindBegProlog()
     if (generateCFIUnwindCodes())
     {
         unwindBegPrologCFI();
+#if defined(_TARGET_ARM_)
+        unwindCfiEpilogFormed = false;
+#endif
         return;
     }
 #endif // _TARGET_UNIX_
@@ -235,6 +238,11 @@ void Compiler::unwindBegEpilog()
 void Compiler::unwindEndEpilog()
 {
     assert(compGeneratingEpilog);
+#if defined(_TARGET_UNIX_)
+#if defined(_TARGET_ARM_)
+    unwindCfiEpilogFormed = true;
+#endif
+#endif // _TARGET_UNIX_
 }
 
 #if defined(_TARGET_ARM_)
