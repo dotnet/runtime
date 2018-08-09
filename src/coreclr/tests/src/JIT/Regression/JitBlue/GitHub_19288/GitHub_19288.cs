7b113c8d1a1120894c78ceca404c4af5485c84e7
Carol Eidt <carol.eidt@microsoft.com>
2018-08-09 16:31:11 -0700

    Fix non-lclVar odd-byte struct passing
    When a non-power-of-2 struct is passed, we can only use an IND if the arg is a local.
    
    Fix dotnet/coreclr#19288
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/74047ae1244062ef383de15163ac026d8b9f549a

---
+++ src/coreclr/tests/src/JIT/Regression/JitBlue/GitHub_19288/GitHub_19288.cs
@@ -0,0 +1,60 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+//
+// This test exhibits a case where the JIT was passing a 3-byte struct
+// by loading 4-bytes, which is not always safe.
+
+using System;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+public class GitHub_19288
+{
+    static int returnVal = 100;
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct PixelData
+    {
+        public byte blue;
+        public byte green;
+        public byte red;
+    }
+
+    public unsafe class MyClass
+    {
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        unsafe void CheckPointer(PixelData p)
+        {
+            if (&p == null)
+            {
+                Console.WriteLine("FAIL");
+                returnVal = -1;
+            }
+        }
+
+        unsafe int DoStuff()
+        {
+            IntPtr pBase = Marshal.AllocCoTaskMem(0x40000 * 3);
+            PixelData* foo = (PixelData*)(pBase + 511 * (512 * sizeof(PixelData)) + 511 * sizeof(PixelData));
+
+            CheckPointer(*foo);
+
+            return 0;
+        }
+
+        static int Main()
+        {
+            try
+            {
+                new MyClass().DoStuff();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed with exception: " + e.Message);
+                returnVal = -1;
+            }
+            return returnVal;
+        }
+    }
+}
