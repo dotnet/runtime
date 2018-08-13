fc7c89b1fab95223b1b5b959046abd9453e0da55
Koundinya Veluri <kouvel@users.noreply.github.com>
2018-08-12 18:35:48 -0700

    Fix a couple of apartment state issues (dotnet/coreclr#19384)
    * Fix a couple of apartment state issues
    
    Fix for https://github.com/dotnet/coreclr/issues/17822
    - The apartment state now defaults to MTA for the main thread along with a CoInitialize
    - Calling `Thread.SetApartmentState` with STA now fails as expected (different behavior from previous netcore, same behavior as netfx)
    
    Fix for https://github.com/dotnet/coreclr/issues/17787
    - `WaitHandle.WaitAll` for multiple handles is not supported on an STA thread due to issues described in https://github.com/dotnet/coreclr/issues/17787#issuecomment-385117537
    - It now throws `NotSupportedException` as expected (different behavior from previous netcore, same behavior as netfx)
    
    Fix for https://github.com/dotnet/coreclr/issues/19225
    
    * Temporarily exclude invalid CoreFX test
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/3a56afe6de4113c83b66a5d3a467b8accfaafa1f

--- src/coreclr/src/vm/appdomain.cpp
+++ src/coreclr/src/vm/appdomain.cpp
@@ -3074,8 +3074,9 @@ void SystemDomain::SetThreadAptState (Thread::ApartmentState state)
         Thread::ApartmentState pState = pThread->SetApartment(Thread::AS_InSTA, TRUE);
         _ASSERTE(pState == Thread::AS_InSTA);
     }
-    else if (state == Thread::AS_InMTA)
+    else
     {
+        // If an apartment state was not explicitly requested, default to MTA
         Thread::ApartmentState pState = pThread->SetApartment(Thread::AS_InMTA, TRUE);
         _ASSERTE(pState == Thread::AS_InMTA);
     }
