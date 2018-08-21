304c755e35fb9d2432a2b803d5136b48c6f8285f
Brian Sullivan <briansul@microsoft.com>
2018-08-20 17:32:13 -0700

    Define FMT_VN as "$%x" and use it uniformly in the codebase
    We use a unique prefix character when printing value numbers in dumps:  i.e.  $1c0
    This define is used with string concatenation to put this in printf format strings
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/904ac5f6484831b94ca01d195e2e68bd0c435a8a

--- src/clr/src/jit/rangecheck.h
+++ src/clr/src/jit/rangecheck.h
@@ -191,7 +191,7 @@ struct Limit
                 return "Dependent";
 
             case keBinOpArray:
-                sprintf_s(buf, size, "VN%04X + %d", vn, cns);
+                sprintf_s(buf, size, FMT_VN " + %d", vn, cns);
                 return buf;
 
             case keConstant:
