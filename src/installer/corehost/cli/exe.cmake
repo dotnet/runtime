cdf9b67ffad3b53eabf2b5e21c0ef95a78e4eb8a
Aaron Robinson <arobins@microsoft.com>
2018-08-22 08:47:59 -0700

    Moving hostpolicy to sub-directory under dll (dotnet/core-setup#4483)
    * Moving hostpolicy to sub-directory under dll
    
    * Remove inconsistent property
    
    * Remove 'exe' and 'dll' directories.
    
    
    Commit migrated from https://github.com/dotnet/core-setup/commit/2ef0b64810530961f492c33d37fc7509128e0a9b

--- src/installer/corehost/cli/exe.cmake
+++ src/installer/corehost/cli/exe.cmake
@@ -11,36 +11,36 @@ else()
     add_compile_options(-fvisibility=hidden)
 endif()
 
-include(../../setup.cmake)
+include(../setup.cmake)
 
 # Include directories
 if(WIN32)
     include_directories("${CLI_CMAKE_RESOURCE_DIR}/${DOTNET_HOST_EXE_NAME}")
 endif()
-include_directories(../../../)
-include_directories(../../../common)
 include_directories(../../)
-include_directories(../../fxr)
+include_directories(../../common)
+include_directories(../)
+include_directories(../fxr)
 
 # CMake does not recommend using globbing since it messes with the freshness checks
 list(APPEND SOURCES
-    ../../../corehost.cpp
-    ../../../common/trace.cpp
-    ../../../common/utils.cpp)
+    ../../corehost.cpp
+    ../../common/trace.cpp
+    ../../common/utils.cpp)
 
 if(WIN32)
     list(APPEND SOURCES
-        ../../../common/pal.windows.cpp
-        ../../../common/longfile.windows.cpp)
+        ../../common/pal.windows.cpp
+        ../../common/longfile.windows.cpp)
 else()
     list(APPEND SOURCES
-        ../../../common/pal.unix.cpp
+        ../../common/pal.unix.cpp
         ${VERSION_FILE_PATH})
 endif()
 
 set(RESOURCES)
 if(WIN32 AND NOT SKIP_VERSIONING)
-    list(APPEND RESOURCES ../../native.rc)
+    list(APPEND RESOURCES ../native.rc)
 endif()
 
 add_executable(${DOTNET_HOST_EXE_NAME} ${SOURCES} ${RESOURCES})
