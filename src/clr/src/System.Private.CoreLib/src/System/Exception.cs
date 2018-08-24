eba8be56429818af1c1c706a5b4d84686f0d21b8
Santiago Fernandez Madero <safern@microsoft.com>
2018-08-24 12:30:27 -0700

    Remove not necessary type forwarded from attributes when type comes from mscorlib in netfx
    Commit migrated from https://github.com/dotnet/coreclr/commit/8bc0e5e39a3805fa707f366f2a1189e2b25d43b6

--- src/clr/src/System.Private.CoreLib/src/System/Exception.cs
+++ src/clr/src/System.Private.CoreLib/src/System/Exception.cs
@@ -27,7 +27,6 @@ namespace System
     using System.Globalization;
 
     [Serializable]
-    [System.Runtime.CompilerServices.TypeForwardedFrom("mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089")]
     public class Exception : ISerializable
     {
         private void Init()
