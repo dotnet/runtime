78c4514a8e990c5de9b55b468d8bcf122d303733
Sergey Andreenko <seandree@microsoft.com>
2018-08-24 22:12:25 -0700

    Fix long spmi file names/clean few functions. (dotnet/coreclr#19656)
    * delete extern functions
    
    * extract LoadRealJitLib func
    
    * extract getResultFileName
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/37e8cc75c96160a9f2fa27535e593e397a707640

--- src/coreclr/src/ToolBox/superpmi/superpmi-shared/spmiutil.cpp
+++ src/coreclr/src/ToolBox/superpmi/superpmi-shared/spmiutil.cpp
@@ -11,7 +11,17 @@
 #include "logging.h"
 #include "spmiutil.h"
 
-bool breakOnDebugBreakorAV = false;
+static bool breakOnDebugBreakorAV = false;
+
+bool BreakOnDebugBreakorAV()
+{
+    return breakOnDebugBreakorAV;
+}
+
+void SetBreakOnDebugBreakOrAV(bool value)
+{
+    breakOnDebugBreakorAV = value;
+}
 
 void DebugBreakorAV(int val)
 {
@@ -19,7 +29,7 @@ void DebugBreakorAV(int val)
     {
         if (val == 0)
             __debugbreak();
-        if (breakOnDebugBreakorAV)
+        if (BreakOnDebugBreakorAV())
             __debugbreak();
     }
 
@@ -107,3 +117,111 @@ LPSTR GetCommandLineA()
     return pCmdLine;
 }
 #endif // FEATURE_PAL
+
+bool LoadRealJitLib(HMODULE& jitLib, WCHAR* jitLibPath)
+{
+    // Load Library
+    if (jitLib == NULL)
+    {
+        if (jitLibPath == nullptr)
+        {
+            LogError("LoadRealJitLib - No real jit path");
+            return false;
+        }
+        jitLib = ::LoadLibraryW(jitLibPath);
+        if (jitLib == NULL)
+        {
+            LogError("LoadRealJitLib - LoadLibrary failed to load '%ws' (0x%08x)", jitLibPath, ::GetLastError());
+            return false;
+        }
+    }
+    return true;
+}
+
+void cleanupExecutableName(WCHAR* executableName)
+{
+    WCHAR* quote = nullptr;
+
+    // If there are any quotes in the file name convert them to spaces.
+    while ((quote = wcsstr(executableName, W("\""))) != nullptr)
+    {
+        *quote = W(' ');
+    }
+
+    // Remove any illegal or annoying characters from the file name by converting them to underscores.
+    while ((quote = wcspbrk(executableName, W("=<>:\"/\\|?! *.,"))) != nullptr)
+    {
+        *quote = W('_');
+    }
+}
+
+void generateRandomSuffix(WCHAR* buffer, size_t length)
+{
+    unsigned randNumber = 0;
+#ifdef FEATURE_PAL
+    PAL_Random(&randNumber, sizeof(randNumber));
+#else  // !FEATURE_PAL
+    rand_s(&randNumber);
+#endif // !FEATURE_PAL
+
+    swprintf_s(buffer, length, W("%08X"), randNumber);
+}
+
+// All lengths in this function exclude the terminal NULL.
+WCHAR* getResultFileName(const WCHAR* folderPath, WCHAR* executableName, const WCHAR* extension)
+{
+    cleanupExecutableName(executableName);
+
+    size_t executableNameLength = wcslen(executableName);
+    size_t extensionLength      = wcslen(extension);
+    size_t folderPathLength     = wcslen(folderPath);
+
+    size_t dataFileNameLength = folderPathLength + 1 + executableNameLength + 1 + extensionLength;
+
+    const size_t maxAcceptablePathLength =
+        MAX_PATH - 50; // subtract 50 because excel doesn't like paths longer then 230.
+
+    if (dataFileNameLength > maxAcceptablePathLength)
+    {
+        // The path name is too long; creating the file will fail. This can happen because we use the command line,
+        // which for ngen includes lots of environment variables, for example.
+        // Shorten the executable file name and add a random string to it to avoid collisions.
+
+        const size_t randStringLength = 8;
+
+        size_t lengthToBeDeleted = (dataFileNameLength - maxAcceptablePathLength) + randStringLength;
+
+        if (executableNameLength <= lengthToBeDeleted)
+        {
+            LogError("getResultFileName - path to the output file is too long '%ws\\%ws.%ws(%d)'", folderPath,
+                     executableName, extension, dataFileNameLength);
+            return nullptr;
+        }
+
+        executableNameLength -= lengthToBeDeleted;
+        executableName[executableNameLength] = 0;
+
+        executableNameLength += randStringLength;
+        WCHAR randNumberString[randStringLength + 1];
+        generateRandomSuffix(randNumberString, randStringLength + 1);
+        wcsncat_s(executableName, executableNameLength + 1, randNumberString, randStringLength);
+
+        executableNameLength = wcslen(executableName);
+
+        dataFileNameLength = folderPathLength + 1 + executableNameLength + 1 + extensionLength;
+        if (dataFileNameLength > maxAcceptablePathLength)
+        {
+            LogError("getResultFileName - were not able to short the result file name.", folderPath, executableName,
+                     extension, dataFileNameLength);
+            return nullptr;
+        }
+    }
+
+    WCHAR* dataFileName = new WCHAR[dataFileNameLength + 1];
+    dataFileName[0]     = 0;
+    wcsncat_s(dataFileName, dataFileNameLength + 1, folderPath, folderPathLength);
+    wcsncat_s(dataFileName, dataFileNameLength + 1, DIRECTORY_SEPARATOR_STR_W, 1);
+    wcsncat_s(dataFileName, dataFileNameLength + 1, executableName, executableNameLength);
+    wcsncat_s(dataFileName, dataFileNameLength + 1, extension, extensionLength);
+    return dataFileName;
+}
