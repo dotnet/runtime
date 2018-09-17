d4b071d74948eda801dde239ebc806181339fd77
Egor Bogatov <egorbo@gmail.com>
2018-09-17 20:00:52 +0300

    fix resolvemember crash
    Commit migrated from https://github.com/mono/mono/commit/5a55842715ac89b9206afa809383e693d96707a2

--- src/mono/mono/metadata/icall.c
+++ src/mono/mono/metadata/icall.c
@@ -5989,7 +5989,12 @@ mono_memberref_is_method (MonoImage *image, guint32 token)
 	if (!image_is_dynamic (image)) {
 		guint32 cols [MONO_MEMBERREF_SIZE];
 		const char *sig;
-		mono_metadata_decode_row (&image->tables [MONO_TABLE_MEMBERREF], mono_metadata_token_index (token) - 1, cols, MONO_MEMBERREF_SIZE);
+		const MonoTableInfo *table = &image->tables [MONO_TABLE_MEMBERREF];
+		int idx = mono_metadata_token_index (token) - 1;
+		if (idx < 0 || table->rows <= idx) {
+			return FALSE;
+		}
+		mono_metadata_decode_row (table, idx, cols, MONO_MEMBERREF_SIZE);
 		sig = mono_metadata_blob_heap (image, cols [MONO_MEMBERREF_SIGNATURE]);
 		mono_metadata_decode_blob_size (sig, &sig);
 		return (*sig != 0x6);
