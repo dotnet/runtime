4b7c2fec53ebd47acf468051276c0503b779b09b
Jarret Shook <jashoo@microsoft.com>
2018-10-11 15:23:17 -0700

    Do not restore or initialize buildtools for x86/arm64 (dotnet/coreclr#20370)
    * Do not restore or initialize buildtools for x86/arm64
    
    * Remove string to download cli correctly
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/907c013a7f5bf6ef4e37519ca27b7ea6fb998153

--- src/coreclr/init-tools.cmd
+++ src/coreclr/init-tools.cmd
@@ -55,7 +55,7 @@ if /i "%PROCESSOR_ARCHITECTURE%" == "arm" (
 )
 
 if /i "%PROCESSOR_ARCHITECTURE%" == "amd64" (
-  set _Arch="x64"
+  set _Arch=x64
   goto ArchSet
 )
 
@@ -65,7 +65,7 @@ REM
 REM TODO: consume native arm64 toolset, blocked by official arm64 windows cli
 REM     : release. See https://github.com/dotnet/coreclr/issues/19614 for more
 REM     : information
-set _Arch="x86"
+set _Arch=x86
 
 echo "init-tools.cmd: Setting arch to %_Arch% for build tools"
 
@@ -85,6 +85,11 @@ if NOT exist "%DOTNET_LOCAL_PATH%" (
 
 :afterdotnetrestore
 
+REM We do not need the build tools for arm64/x86
+if /i "%_Arch%" == "x86" (
+  goto :EOF
+)
+
 if exist "%BUILD_TOOLS_PATH%" goto :afterbuildtoolsrestore
 echo Restoring BuildTools version %BUILDTOOLS_VERSION%...
 echo Running: "%DOTNET_CMD%" restore "%INIT_TOOLS_RESTORE_PROJECT%" --no-cache --packages "%PACKAGES_DIR%" --source "%BUILDTOOLS_SOURCE%" /p:BuildToolsPackageVersion=%BUILDTOOLS_VERSION% /p:ToolsDir=%TOOLRUNTIME_DIR% >> "%INIT_TOOLS_LOG%"
@@ -95,11 +100,6 @@ if NOT exist "%BUILD_TOOLS_PATH%\init-tools.cmd" (
 )
 
 :afterbuildtoolsrestore
-
-REM We do not need the build tools for arm64
-if /i "%PROCESSOR_ARCHITECTURE%" == "arm64" (
-  goto :EOF
-)
 
 :: Ask init-tools to also restore ILAsm
 set /p ILASMCOMPILER_VERSION=< "%~dp0ILAsmVersion.txt"
