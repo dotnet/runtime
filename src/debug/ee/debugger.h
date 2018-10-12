72eb917675fb960c9a6fb8678fdabb686d2be75b
Chuck Ries <chuckr@microsoft.com>
2018-10-12 16:28:36 -0700

    Ensure BeforeGC and AfterGC events are always sent in pairs
    This latches the m_isGarbageCollectionEventsEnabled in BeforeGarbageCollection
    to ensure that if we send a BeforeGC event we will send the corresponding
    AfterGC event. It also ensure we will not send an AfterGC event if we did not
    send the corresponding BeforeGC event.

--- src/debug/ee/debugger.h
+++ src/debug/ee/debugger.h
@@ -2959,6 +2959,9 @@ public:
 #endif
     BOOL m_isBlockedOnGarbageCollectionEvent;
     BOOL m_isGarbageCollectionEventsEnabled;
+    // this latches m_isGarbageCollectionEventsEnabled in BeforeGarbageCollection so we can 
+    // guarantee the corresponding AfterGC event is sent even if the events are disabled during GC.
+    BOOL m_isGarbageCollectionEventsEnabledLatch;
 private:
     HANDLE GetGarbageCollectionBlockerEvent() { return  GetLazyData()->m_garbageCollectionBlockerEvent; }
 
