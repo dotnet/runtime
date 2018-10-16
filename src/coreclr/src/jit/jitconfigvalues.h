b9a5b075884c536bd96d7378e13a89892f76948c
Eugene Rozenfeld <erozen@microsoft.com>
2018-10-15 18:40:35 -0700

    Add JitObjectStackAllocation config option.
    Commit migrated from https://github.com/dotnet/coreclr/commit/9a5e5810bc22f6d9f4c181069ee94e059a81d14c

--- src/coreclr/src/jit/jitconfigvalues.h
+++ src/coreclr/src/jit/jitconfigvalues.h
@@ -347,6 +347,7 @@ CONFIG_STRING(JitInlineReplayFile, W("JitInlineReplayFile"))
 #endif // defined(DEBUG) || defined(INLINE_DATA)
 
 CONFIG_INTEGER(JitInlinePolicyModel, W("JitInlinePolicyModel"), 0)
+CONFIG_INTEGER(JitObjectStackAllocation, W("JitObjectStackAllocation"), 0)
 
 CONFIG_INTEGER(JitEECallTimingInfo, W("JitEECallTimingInfo"), 0)
 
