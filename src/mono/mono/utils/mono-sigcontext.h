7905f0e56f4905888241c7502a0d9834a529c624
Bernhard Urban <bernhard.urban@xamarin.com>
2018-10-17 01:49:47 +0200

    [ppc] fix UCONTEXT_REG macros for FreeBSD (mono/mono#11128)
    Contributed by @lenoil98. Fixes https://github.com/mono/mono/issues/11020
    
    Commit migrated from https://github.com/mono/mono/commit/965aebf500d80daee246f2f0818f33c588fdf047

--- src/mono/mono/utils/mono-sigcontext.h
+++ src/mono/mono/utils/mono-sigcontext.h
@@ -363,10 +363,10 @@ typedef struct ucontext {
 #elif defined(__FreeBSD__)
 	typedef ucontext_t os_ucontext;
 
-	#define UCONTEXT_REG_Rn(ctx, n)   ((ctx)->uc_mcontext.mc_gpr [(n)])
-	#define UCONTEXT_REG_FPRn(ctx, n) ((ctx)->uc_mcontext.mc_fpreg [(n)])
-	#define UCONTEXT_REG_NIP(ctx)     ((ctx)->uc_mcontext.mc_srr0)
-	#define UCONTEXT_REG_LNK(ctx)     ((ctx)->uc_mcontext.mc_lr)
+	#define UCONTEXT_REG_Rn(ctx, n)   (((os_ucontext*)(ctx))->uc_mcontext.mc_gpr [(n)])
+	#define UCONTEXT_REG_FPRn(ctx, n) (((os_ucontext*)(ctx))->uc_mcontext.mc_fpreg [(n)])
+	#define UCONTEXT_REG_NIP(ctx)     (((os_ucontext*)(ctx))->uc_mcontext.mc_srr0)
+	#define UCONTEXT_REG_LNK(ctx)     (((os_ucontext*)(ctx))->uc_mcontext.mc_lr)
 #elif defined(_AIX)
 	typedef ucontext_t os_ucontext;
 
