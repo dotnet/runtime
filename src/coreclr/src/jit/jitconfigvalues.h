b8ea37f88e1ec80bd4f349e6f62104175a028cb9
Bruce Forstall <brucefo@microsoft.com>
2018-10-17 20:52:39 -0700

    Remove unnecessary interpreter fallback (dotnet/coreclr#20470)
    This was added for arm64 bring-up. Remove it, and the associated TODO.
    
    Fixes dotnet/coreclr#19696
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/c29b1261a367816a6802df4fc0e4939d047ee919

--- src/coreclr/src/jit/jitconfigvalues.h
+++ src/coreclr/src/jit/jitconfigvalues.h
@@ -28,8 +28,6 @@ CONFIG_INTEGER(DisplayLsraStats, W("JitLsraStats"), 0)       // Display JIT Line
 CONFIG_INTEGER(DumpJittedMethods, W("DumpJittedMethods"), 0) // Prints all jitted methods to the console
 CONFIG_INTEGER(EnablePCRelAddr, W("JitEnablePCRelAddr"), 1)  // Whether absolute addr be encoded as PC-rel offset by
                                                              // RyuJIT where possible
-CONFIG_INTEGER(InterpreterFallback, W("InterpreterFallback"), 0) // Fallback to the interpreter when the JIT compiler
-                                                                 // fails
 CONFIG_INTEGER(JitAssertOnMaxRAPasses, W("JitAssertOnMaxRAPasses"), 0)
 CONFIG_INTEGER(JitBreakEmitOutputInstr, W("JitBreakEmitOutputInstr"), -1)
 CONFIG_INTEGER(JitBreakMorphTree, W("JitBreakMorphTree"), 0xffffffff)
