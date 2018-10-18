577aafcdc7122649cb4c74c617aef597d194ac05
Zeng Jiang <v-jiazen@microsoft.com>
2018-10-19 01:19:12 +0800

    Add PInvoke/Primitives/Int tests (#19304)
    * Add PInvoke/Primitives/Int tests
    
    * Clean up Int tests to pass xplat.

---
+++ tests/src/Interop/PInvoke/Primitives/Int/PInvokeIntNative.cpp
@@ -0,0 +1,115 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#include <stdio.h>
+#include <xplatform.h>
+
+int intManaged = 1000;
+int intNative = 2000;
+int intReturn = 3000;
+int intErrReturn = 4000;
+
+extern "C" DLL_EXPORT int STDMETHODCALLTYPE Marshal_In(/*[in]*/int intValue)
+{
+    //Check the input
+    if(intValue != intManaged)
+    {
+        printf("Error in Function Marshal_In(Native Client)\n");
+
+        //Expected
+        printf("Expected:%u\n", intManaged);
+
+        //Actual
+        printf("Actual:%u\n",intValue);
+
+        //Return the error value instead if verification failed
+        return intErrReturn;
+    }
+
+    return intReturn;
+}
+
+extern "C" DLL_EXPORT int STDMETHODCALLTYPE Marshal_InOut(/*[In,Out]*/int intValue)
+{
+    //Check the input
+    if(intValue != intManaged)
+    {
+        printf("Error in Function Marshal_InOut(Native Client)\n");
+
+        //Expected
+        printf("Expected:%u\n", intManaged);
+
+        //Actual
+        printf("Actual:%u\n",intValue);
+
+        //Return the error value instead if verification failed
+        return intErrReturn;
+    }
+
+    //In-Place Change
+    intValue = intNative;
+
+    //Return
+    return intReturn;
+}
+
+extern "C" DLL_EXPORT int STDMETHODCALLTYPE Marshal_Out(/*[Out]*/int intValue)
+{
+    intValue = intNative;
+
+    //Return
+    return intReturn;
+}
+
+extern "C" DLL_EXPORT int STDMETHODCALLTYPE MarshalPointer_In(/*[in]*/int *pintValue)
+{
+    //Check the input
+    if(*pintValue != intManaged)
+    {
+        printf("Error in Function MarshalPointer_In(Native Client)\n");
+
+        //Expected
+        printf("Expected:%u\n", intManaged);
+
+        //Actual
+        printf("Actual:%u\n",*pintValue);
+
+        //Return the error value instead if verification failed
+        return intErrReturn;
+    }
+    
+    return intReturn;
+}
+
+extern "C" DLL_EXPORT int STDMETHODCALLTYPE MarshalPointer_InOut(/*[in,out]*/int *pintValue)
+{
+    //Check the input
+    if(*pintValue != intManaged)
+    {
+        printf("Error in Function MarshalPointer_InOut(Native Client)\n");
+
+        //Expected
+        printf("Expected:%u\n", intManaged);
+
+        //Actual
+        printf("Actual:%u\n",*pintValue);
+
+        //Return the error value instead if verification failed
+        return intErrReturn;
+    }
+
+    //In-Place Change
+    *pintValue = intNative;
+
+    //Return
+    return intReturn;
+}
+
+extern "C" DLL_EXPORT int STDMETHODCALLTYPE MarshalPointer_Out(/*[out]*/ int *pintValue)
+{
+    *pintValue = intNative;
+
+    //Return
+    return intReturn;
+}
