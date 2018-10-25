1bc5a438d82d4b50a249d987112305cedb5117cf
Brian Robbins <brianrob@microsoft.com>
2018-10-25 12:58:56 -0700

    Remove debug-only file writers. (dotnet/coreclr#20612)
    Commit migrated from https://github.com/dotnet/coreclr/commit/6dd4270daf3ee49a08adedaf71a087de32c810a2

--- src/coreclr/src/vm/eventpipe.h
+++ src/coreclr/src/vm/eventpipe.h
@@ -329,10 +329,6 @@ class EventPipe
         static EventPipeFile *s_pFile;
         static EventPipeEventSource *s_pEventSource;
         static LPCWSTR s_pCommandLine;
-#ifdef _DEBUG
-        static EventPipeFile *s_pSyncFile;
-        static EventPipeJsonFile *s_pJsonFile;
-#endif // _DEBUG
         const static DWORD FileSwitchTimerPeriodMS = 1000;
         static HANDLE s_fileSwitchTimerHandle;
         static ULONGLONG s_lastFileSwitchTime;
