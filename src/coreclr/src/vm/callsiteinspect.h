fbccbcb336f472f9718bb937db0f827fbbfab3c5
Austin Wise <AustinWise@gmail.com>
2018-10-26 09:35:45 -0700

    Add missing include guard in header file. (dotnet/coreclr#20635)
    Commit migrated from https://github.com/dotnet/coreclr/commit/62b78b03f4ad51cbced41792dbc144dea3ef7e22

--- src/coreclr/src/vm/callsiteinspect.h
+++ src/coreclr/src/vm/callsiteinspect.h
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#pragma once
+
 struct CallsiteDetails
 {
     // The signature of the current call
