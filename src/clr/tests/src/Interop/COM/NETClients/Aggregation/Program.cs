93cfdd0126132d97db54f4ac582adc31d2a90212
Aaron Robinson <arobins@microsoft.com>
2018-10-25 17:05:53 -0700

    Add a runtime check for Windows.Nano to all COM related tests.
    Commit migrated from https://github.com/dotnet/coreclr/commit/529b22a5ff59abae958dabc34961b20ea06f3185

--- src/clr/tests/src/Interop/COM/NETClients/Aggregation/Program.cs
+++ src/clr/tests/src/Interop/COM/NETClients/Aggregation/Program.cs
@@ -29,6 +29,12 @@ namespace NetClient
 
         static int Main(string[] doNotUse)
         {
+            // RegFree COM is not supported on Windows Nano
+            if (Utilities.IsWindowsNanoServer)
+            {
+                return 100;
+            }
+
             try
             {
                 ValidateNativeOuter();
