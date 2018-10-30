626def86af589734999a0e62d3a83c2acb9de54a
Andy Ayers <andya@microsoft.com>
2018-10-30 11:24:37 -0700

    JIT: Fix call flag propagation for GenTreeArrElem (#20660)
    Closes #20651.
    
    Also fix up some "near miss" cases for GenTreeField and GenTreeBoundsCheck,
    where we get lucky and the importer currently splits trees with temps so the
    currently ignored child nodes have no interesting side effects.
    
    Revise GenTreeField a bit to pull more of the initialization work into the
    constructor. Add a missing R2R field propagation for field nodes in GtClone
    (evidently also never hit in practice).

---
+++ tests/src/JIT/Regression/JitBlue/GitHub_20651/GitHub_20651.cs
@@ -0,0 +1,25 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+
+class X
+{
+    static string s = "hello, world";
+
+    static string[,] G()
+    {
+        string[,] strings = new string[3,3];
+        strings[0,0] = s;
+        return strings;
+    }
+
+    // Ensure GTF_CALL flag is propagated to MD array accessor
+    public static int Main()
+    {
+        int c = G()[0,0].GetHashCode();
+        int v = s.GetHashCode();
+        return c == v ? 100 : -1;
+    }
+}
