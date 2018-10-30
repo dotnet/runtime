47cd30cd377d38e77a8e384f94bacc31d6b397e1
Michael Voorhees <michaelv@unity3d.com>
2018-10-30 14:31:37 -0400

    Compile ExplicitStaticCtor with /optimize+
    This eliminates a source of instability in the test where it could fail if compiled with a compiler that put a `nop` into the cctor
    
    Commit migrated from https://github.com/dotnet/linker/commit/4f70d9ab234c7748bb324e6bc7be892a7fd94587

--- src/tools/illink/linker/Tests/Mono.Linker.Tests.Cases/Statics/ExplicitStaticCtor.cs
+++ src/tools/illink/linker/Tests/Mono.Linker.Tests.Cases/Statics/ExplicitStaticCtor.cs
@@ -1,9 +1,11 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using Mono.Linker.Tests.Cases.Expectations.Assertions;
+using Mono.Linker.Tests.Cases.Expectations.Metadata;
 
 namespace Mono.Linker.Tests.Cases.Statics
 {
+	[SetupCompileArgument ("/optimize+")]
 	public class ExplicitStaticCtor
 	{
 		public static void Main ()
