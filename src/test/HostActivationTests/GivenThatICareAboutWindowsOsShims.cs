16fc8e9125638f24527091a573d9dbe546a1f609
Vitek Karas <vitek.karas@microsoft.com>
2018-10-31 01:24:01 -0700

    Add os supported versions to host (#4700)
    Add a manifest to dotnet.exe with supported versions of Windows.
    Also remove any manifest from apphost.exe as it will get the one from the app when it's used.

---
+++ src/test/HostActivationTests/GivenThatICareAboutWindowsOsShims.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Xunit;
+
+namespace Microsoft.DotNet.CoreSetup.Test.HostActivation.WindowsOsShims
+{
+    public class GivenThatICareAboutWindowsOsShims : IClassFixture<GivenThatICareAboutWindowsOsShims.SharedTestState>
+    {
+        private SharedTestState sharedTestState;
+
+        public GivenThatICareAboutWindowsOsShims(SharedTestState fixture)
+        {
+            sharedTestState = fixture;
+        }
+
+        [Fact]
+        public void MuxerRunsPortableAppWithoutWindowsOsShims()
+        {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                // Manifests are only supported on Windows OSes.
+                return;
+            }
+
+            TestProjectFixture portableAppFixture = sharedTestState.PortableTestWindowsOsShimsAppFixture.Copy();
+
+            portableAppFixture.BuiltDotnet.Exec(portableAppFixture.TestProject.AppDll)
+                .CaptureStdErr()
+                .CaptureStdOut()
+                .Execute()
+                .Should().Pass()
+                .And.HaveStdOutContaining("Reported OS version is newer or equal to the true OS version - no shims.");
+        }
+
+        // Testing the standalone version (apphost) would require to make a copy of the entire SDK
+        // and overwrite the apphost.exe in it. Currently this is just too expensive for one test (160MB of data).
+
+        public class SharedTestState : IDisposable
+        {
+            private static RepoDirectoriesProvider RepoDirectories { get; set; }
+
+            public TestProjectFixture PortableTestWindowsOsShimsAppFixture { get; set; }
+
+            public SharedTestState()
+            {
+                RepoDirectories = new RepoDirectoriesProvider();
+
+                PortableTestWindowsOsShimsAppFixture = new TestProjectFixture("TestWindowsOsShimsApp", RepoDirectories)
+                    .EnsureRestored(RepoDirectories.CorehostPackages)
+                    .PublishProject();
+            }
+
+            public void Dispose()
+            {
+                PortableTestWindowsOsShimsAppFixture.Dispose();
+            }
+        }
+    }
+}
