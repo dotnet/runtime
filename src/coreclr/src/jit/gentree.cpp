0b5c3929077512f99d4290b9364b8ae19d6e56ca
Eugene Rozenfeld <erozen@microsoft.com>
2018-11-09 16:53:49 -0800

    Fix for bug 20499.
    When the jit is copying a struct-typed return to the return local in a synchronous method on arm64,
    it ends up invoking an importer utility outside the importer, where impTreeLast is not set.
    
    The call sequence is
    fgMorphBlocks --> gtNewTempAssign --> impAssignStruct --> impAssignStructPtr --> impAppendTree
    
    When impAssignStructPtr sees GT_COMMA src nodes, it unwraps them and inserts additional statements.
    The fix is to pass an insertion point statement through this call chain to prevent impAssignStruct
    and impAssignStructPtr from calling impAppendTree outside of the importer.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/73f84776b9db118f855672724388cb1def3f2906

--- src/coreclr/src/jit/gentree.cpp
+++ src/coreclr/src/jit/gentree.cpp
@@ -14235,8 +14235,11 @@ DONE:
 // gtNewTempAssign: Create an assignment of the given value to a temp.
 //
 // Arguments:
-//    tmp - local number for a compiler temp
-//    val - value to assign to the temp
+//    tmp         - local number for a compiler temp
+//    val         - value to assign to the temp
+//    pAfterStmt  - statement to insert any additional statements after
+//    ilOffset    - il offset for new statements
+//    block       - block to insert any additional statements in
 //
 // Return Value:
 //    Normally a new assignment node.
@@ -14248,9 +14251,9 @@ DONE:
 //    May update the type of the temp, if it was previously unknown.
 //
 //    May set compFloatingPointUsed.
-//
 
-GenTree* Compiler::gtNewTempAssign(unsigned tmp, GenTree* val)
+GenTree* Compiler::gtNewTempAssign(
+    unsigned tmp, GenTree* val, GenTree** pAfterStmt, IL_OFFSETX ilOffset, BasicBlock* block)
 {
     // Self-assignment is a nop.
     if (val->OperGet() == GT_LCL_VAR && val->gtLclVarCommon.gtLclNum == tmp)
@@ -14346,7 +14349,7 @@ GenTree* Compiler::gtNewTempAssign(unsigned tmp, GenTree* val)
         }
         dest->gtFlags |= GTF_DONT_CSE;
         valx->gtFlags |= GTF_DONT_CSE;
-        asg = impAssignStruct(dest, val, structHnd, (unsigned)CHECK_SPILL_NONE);
+        asg = impAssignStruct(dest, val, structHnd, (unsigned)CHECK_SPILL_NONE, pAfterStmt, ilOffset, block);
     }
     else
     {
