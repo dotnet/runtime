63d5169b4003d7eae54aea59f95874040528c450
Alexander Kyte <alkyte@microsoft.com>
2018-11-09 22:16:39 -0500

    [runtime] Make sure lldb/gdb dump happens when MERP fails
    Commit migrated from https://github.com/mono/mono/commit/f2d5baf6cb3eb6273721d42e1ace451e3e77803a

--- src/mono/mono/mini/mini-posix.c
+++ src/mono/mono/mini/mini-posix.c
@@ -976,10 +976,11 @@ dump_native_stacktrace (const char *signal, void *ctx)
 
 			if (!leave) {
 				mono_sigctx_to_monoctx (ctx, &mctx);
-				// Do before forking
-				if (!mono_threads_summarize (&mctx, &output, &hashes, FALSE, TRUE, NULL, 0))
-					g_assert_not_reached ();
+				// Returns success, so leave if !success
+				leave = !mono_threads_summarize (&mctx, &output, &hashes, FALSE, TRUE, NULL, 0);
+			}
 
+			if (!leave) {
 				// Wait for the other threads to clean up and exit their handlers
 				// We can't lock / wait indefinitely, in case one of these threads got stuck somehow
 				// while dumping. 
@@ -1024,12 +1025,11 @@ dump_native_stacktrace (const char *signal, void *ctx)
 			if (pid == 0) {
 				if (!ctx) {
 					mono_runtime_printf_err ("\nMust always pass non-null context when using merp.\n");
-					exit (1);
+				} else if (output) {
+					mono_merp_invoke (crashed_pid, signal, output, &hashes);
+				} else {
+					mono_runtime_printf_err ("\nMerp dump step not run, no dump created.\n");
 				}
-
-				mono_merp_invoke (crashed_pid, signal, output, &hashes);
-
-				exit (1);
 			}
 		}
 #endif
