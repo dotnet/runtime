26e4e2783bbf69639c177efaf6cee03a8e08504d
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2018-11-14 15:18:23 -0800

    Add test for marshalling ArgIterators on Windows (dotnet/coreclr#20888)
    * Add (windows-only) tests for __arglist and ArgIterator marshalling.
    
    * Add explicit calling convention notation to pinvokes.
    
    * Add license header
    
    * Update VarargsTest.cs
    
    * Fix calling conventions.
    
    * PR Feedback
    
    * Add explicit calling convention.
    
    * Update VarargsNative.cpp
    
    * Don't reference System.Private.Corelib directly
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/30c530c65bb54a2c7e5697d115454136c35ee418

---
+++ src/clr/tests/src/Interop/PInvoke/Varargs/VarargsNative.cpp
@@ -0,0 +1,19 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#include <xplatform.h>
+#include <stdarg.h>
+
+extern "C" DLL_EXPORT void __cdecl TestVarArgs(LPWSTR formattedString, SIZE_T bufferSize, LPCWSTR format, ...)
+{
+    va_list args;
+    va_start(args, format);
+
+    vswprintf_s(formattedString, bufferSize, format, args);
+}
+
+extern "C" DLL_EXPORT void STDMETHODCALLTYPE TestArgIterator(LPWSTR formattedString, SIZE_T bufferSize, LPCWSTR format, va_list args)
+{
+    vswprintf_s(formattedString, bufferSize, format, args);
+}
