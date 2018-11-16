57ae190cef45cee76cd8e940913e518cac4a4d68
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2018-11-15 17:14:35 -0800

    Add tests for marshalling as VARIANT_BOOL or VARIANT (dotnet/coreclr#20856)
    * Add native side impl for VARIANT_BOOL test.
    
    * More bool native code.
    
    * Add VariantBool marshalling tests.
    
    * Add infrastructure for tests for object -> VARIANT marshalling.
    
    * Add variant tests for all built-in types.
    
    * Clean up Variant tests.
    
    * Add test for passing custom structure as object.
    
    * Add test for VARIANT_BOOL byref.
    
    * Added byref tests for VARIANT marshalling.
    
    * Add field marshalling tests for VARIANT.
    
    * Move ByValue tests to not have two sources of truth.
    
    * Make managed code the one source of truth.
    
    * Clean up formatting/unused methods.
    
    * Clean up some whitespace issues.
    
    * Add tests for BStrWrapper and CurrencyWrapper marshalling.
    
    * Add UnknownWrapper tests.
    
    * Add a test for VariantWrapper and PInvoke.
    
    * PR Feedback.
    
    * Remove ToString.
    
    * Fix visibility
    
    * Fix MSBuild import path.
    
    * Clean up Variant tests.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/a08a66902cdfb8e713c1e11f56c3aea9ca6ece6c

--- src/coreclr/tests/src/Interop/PrimitiveMarshalling/Bool/BoolTest.cs
+++ src/coreclr/tests/src/Interop/PrimitiveMarshalling/Bool/BoolTest.cs
@@ -9,6 +9,9 @@ using System.Text;
 
 class Test
 {
+    const bool boolManaged = true;
+    const bool boolNative = false;
+
     static void ReportFailure(string describe, bool expect, bool actual)
     {
         throw new Exception(" === Fail: " + describe + "\n\tExpected:" + expect + "\n\tActual:" + actual);        
@@ -16,12 +19,9 @@ class Test
     
     public static int Main(string[] args)
     {
-        const bool boolManaged = true;
-        const bool boolNative = false;
-        
         //Test Method1
         bool boolValue1 = boolManaged;
-        bool boolValueRet1 = NativeMethods.Marshal_In(boolValue1);
+        bool boolValueRet1 = BoolNative.Marshal_In(boolValue1);
         if (!boolValueRet1)
         {
             ReportFailure("Method Marshal_In[Managed Side],The return value is wrong", true, boolValueRet1);
@@ -29,7 +29,7 @@ class Test
 
         //TestMethod2
         bool boolValue2 = boolManaged;
-        bool boolValueRet2 = NativeMethods.Marshal_InOut(boolValue2);
+        bool boolValueRet2 = BoolNative.Marshal_InOut(boolValue2);
         if (!boolValueRet2)
         {
             ReportFailure("Method Marshal_InOut[Managed Side],The return value is wrong", true, boolValueRet2);
@@ -41,7 +41,7 @@ class Test
 
         //TestMethod3
         bool boolValue3 = boolManaged;
-        bool boolValueRet3 = NativeMethods.Marshal_Out(boolValue3);
+        bool boolValueRet3 = BoolNative.Marshal_Out(boolValue3);
         if (!boolValueRet3)
         {
             ReportFailure("Method Marshal_Out[Managed Side],The return value is wrong", true, boolValueRet3);
@@ -53,7 +53,7 @@ class Test
 
         //TestMethod4
         bool boolValue4 = boolManaged;
-        bool boolValueRet4 = NativeMethods.MarshalPointer_In(ref boolValue4);
+        bool boolValueRet4 = BoolNative.MarshalPointer_In(ref boolValue4);
         if (!boolValueRet4)
         {
             ReportFailure("Method MarshalPointer_In[Managed Side],The return value is wrong", true, boolValueRet4);
@@ -65,7 +65,7 @@ class Test
 
         //TestMethod5
         bool boolValue5 = boolManaged;
-        bool boolValueRet5 = NativeMethods.MarshalPointer_InOut(ref boolValue5);
+        bool boolValueRet5 = BoolNative.MarshalPointer_InOut(ref boolValue5);
         if (!boolValueRet5)
         {
             ReportFailure("Method MarshalPointer_InOut[Managed Side],The return value is wrong", true, boolValueRet5);
@@ -77,7 +77,7 @@ class Test
 
         //TestMethod6
         bool boolValue6 = boolManaged;
-        bool boolValueRet6 = NativeMethods.MarshalPointer_Out(out boolValue6);
+        bool boolValueRet6 = BoolNative.MarshalPointer_Out(out boolValue6);
         if (!boolValueRet6)
         {
             ReportFailure("Method Marshal_Out[Managed Side],The return value is wrong", true, boolValueRet6);
@@ -89,18 +89,18 @@ class Test
 
         //Test Method7
         bool boolValue7 = boolManaged;
-        bool boolValueRet7 = NativeMethods.Marshal_As_In(boolValue7);
+        bool boolValueRet7 = BoolNative.Marshal_As_In(boolValue7);
         if (!boolValueRet7)
         {
-            ReportFailure("Method Marshal_As_In[Managed Side],The return value is wrong", true, boolValueRet1);
+            ReportFailure("Method Marshal_As_In[Managed Side],The return value is wrong", true, boolValueRet7);
         }
 
         //TestMethod8
         bool boolValue8 = boolManaged;
-        bool boolValueRet8 = NativeMethods.Marshal_As_InOut(boolValue8);
+        bool boolValueRet8 = BoolNative.Marshal_As_InOut(boolValue8);
         if (!boolValueRet8)
         {
-            ReportFailure("Method Marshal_As_InOut[Managed Side],The return value is wrong", true, boolValueRet2);
+            ReportFailure("Method Marshal_As_InOut[Managed Side],The return value is wrong", true, boolValueRet8);
         }
         if (boolValue8 != boolManaged)
         {
@@ -109,15 +109,83 @@ class Test
 
         //TestMethod9
         bool boolValue9 = boolManaged;
-        bool boolValueRet9 = NativeMethods.Marshal_As_Out(boolValue9);
+        bool boolValueRet9 = BoolNative.Marshal_As_Out(boolValue9);
         if (!boolValueRet9)
         {
-            ReportFailure("Method Marshal_As_Out[Managed Side],The return value is wrong", true, boolValueRet3);
+            ReportFailure("Method Marshal_As_Out[Managed Side],The return value is wrong", true, boolValueRet9);
         }
         if (boolValue9 != boolManaged)
         {
-            ReportFailure("Method Marshal_As_Out[Managed Side],The parameter value is changed", boolManaged, boolValue3);
+            ReportFailure("Method Marshal_As_Out[Managed Side],The parameter value is changed", boolManaged, boolValue9);
         }
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            TestVariantBool();
+        }
+        
         return 100;
+    }
+
+    private static void TestVariantBool()
+    {
+        
+        if (!BoolNative.Marshal_ByValue_Variant(true, true))
+        {
+            ReportFailure("Method Marshal_ByValue_Variant[Managed Side], The return value is wrong", true, true);
+        }
+        
+        if (!BoolNative.Marshal_ByValue_Variant(false, false))
+        {
+            ReportFailure("Method Marshal_ByValue_Variant[Managed Side], The return value is wrong", false, false);
+        }
+
+        bool boolValue10 = boolManaged;
+        bool boolValueRet10 = BoolNative.Marshal_Ref_Variant(ref boolValue10);
+
+        if (!boolValueRet10)
+        {
+            ReportFailure("Method Marshal_Ref_Variant[Managed Side], The return value is wrong.", true, boolValueRet10);
+        }
+        if (boolValue10 != boolNative)
+        {
+            ReportFailure("Method Marshal_Ref_Variant[Managed Side],The passed value is wrong", boolNative, boolValue10);
+        } 
+        
+        var trueStruct = new BoolNative.ContainsVariantBool
+        {
+            value = true
+        };
+
+        var falseStruct = new BoolNative.ContainsVariantBool
+        {
+            value = false
+        };
+        
+        if (!BoolNative.Marshal_ByValue_Struct_Variant(trueStruct, true))
+        {
+            ReportFailure("Method Marshal_ByValue_Variant[Managed Side], The return value is wrong", true, true);
+        }
+        
+        if (!BoolNative.Marshal_ByValue_Struct_Variant(falseStruct, false))
+        {
+            ReportFailure("Method Marshal_ByValue_Variant[Managed Side], The return value is wrong", false, false);
+        }
+
+        var boolValue11 = new BoolNative.ContainsVariantBool
+        {
+            value = boolManaged
+        };
+        
+        bool boolValueRet11 = BoolNative.Marshal_Ref_Struct_Variant(ref boolValue11);
+
+        if (!boolValueRet11)
+        {
+            ReportFailure("Method Marshal_Ref_Variant[Managed Side], The return value is wrong.", true, boolValueRet11);
+        }
+        if (boolValue11.value != boolNative)
+        {
+            ReportFailure("Method Marshal_Ref_Variant[Managed Side],The passed value is wrong", boolNative, boolValue11.value);
+        }
     }
 }
--- src/coreclr/tests/src/Interop/PrimitiveMarshalling/Bool/NativeMethodDef.cs
+++ src/coreclr/tests/src/Interop/PrimitiveMarshalling/Bool/NativeMethodDef.cs
@@ -7,41 +7,69 @@ using System.Runtime.InteropServices;
 using System.Reflection;
 using System.Text;
 
-public class NativeMethods
+public class BoolNative
 {
-
-    public const string NativeSharedBinaryName = "BoolNative";
-
-    [DllImport(NativeSharedBinaryName, CallingConvention = CallingConvention.StdCall)]
+    [DllImport(nameof(BoolNative))]
     public static extern bool Marshal_In([In]bool boolValue);
 
-    [DllImport(NativeSharedBinaryName, CallingConvention = CallingConvention.StdCall)]
+    [DllImport(nameof(BoolNative))]
     public static extern bool Marshal_InOut([In, Out]bool boolValue);
 
-    [DllImport(NativeSharedBinaryName, CallingConvention = CallingConvention.StdCall)]
+    [DllImport(nameof(BoolNative))]
     public static extern bool Marshal_Out([Out]bool boolValue);
 
-    [DllImport(NativeSharedBinaryName, CallingConvention = CallingConvention.StdCall)]
+    [DllImport(nameof(BoolNative))]
     public static extern bool MarshalPointer_In([In]ref bool pboolValue);
 
-    [DllImport(NativeSharedBinaryName, CallingConvention = CallingConvention.StdCall)]
+    [DllImport(nameof(BoolNative))]
     public static extern bool MarshalPointer_InOut(ref bool pboolValue);
 
-    [DllImport(NativeSharedBinaryName, CallingConvention = CallingConvention.StdCall)]
+    [DllImport(nameof(BoolNative))]
     public static extern bool MarshalPointer_Out(out bool pboolValue);
 
-    [DllImport(NativeSharedBinaryName, CallingConvention = CallingConvention.StdCall)]
+    [DllImport(nameof(BoolNative))]
     [return: MarshalAs(UnmanagedType.U1)]
     public static extern bool Marshal_As_In(
       [In, MarshalAs(UnmanagedType.U1)]bool boolValue);
 
-    [DllImport(NativeSharedBinaryName, CallingConvention = CallingConvention.StdCall)]
+    [DllImport(nameof(BoolNative))]
     [return: MarshalAs(UnmanagedType.U1)]
     public static extern bool Marshal_As_InOut(
       [In, Out, MarshalAs(UnmanagedType.U1)]bool boolValue);
 
-    [DllImport(NativeSharedBinaryName, CallingConvention = CallingConvention.StdCall)]
+    [DllImport(nameof(BoolNative))]
     [return: MarshalAs(UnmanagedType.U1)]
     public static extern bool Marshal_As_Out(
       [Out, MarshalAs(UnmanagedType.U1)]bool boolValue);
+
+#pragma warning disable CS0612, CS0618
+    public struct ContainsVariantBool
+    {
+        [MarshalAs(UnmanagedType.VariantBool)]
+        public bool value;
+    }
+
+    [DllImport(nameof(BoolNative))]
+    [return: MarshalAs(UnmanagedType.U1)]
+    public static extern bool Marshal_ByValue_Variant(
+        [MarshalAs(UnmanagedType.VariantBool)] bool value,
+        [MarshalAs(UnmanagedType.U1)] bool expected);
+
+    [DllImport(nameof(BoolNative))]
+    [return: MarshalAs(UnmanagedType.U1)]
+    public static extern bool Marshal_Ref_Variant(
+        [MarshalAs(UnmanagedType.VariantBool)] ref bool value);
+    
+    [DllImport(nameof(BoolNative))]
+    [return: MarshalAs(UnmanagedType.U1)]
+    public static extern bool Marshal_ByValue_Struct_Variant(
+        ContainsVariantBool value,
+        [MarshalAs(UnmanagedType.U1)] bool expected);
+
+    [DllImport(nameof(BoolNative))]
+    [return: MarshalAs(UnmanagedType.U1)]
+    public static extern bool Marshal_Ref_Struct_Variant(ref ContainsVariantBool value);
+
+#pragma warning restore CS0612, CS0618
+
 }
