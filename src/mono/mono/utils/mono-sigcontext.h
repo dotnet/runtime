aabf27add6e406fc8cc58bf3e829904f18e73d38
myfreeweb <greg@unrelenting.technology>
2018-11-19 14:25:31 +0300

    Add support for FreeBSD/aarch64 (mono/mono#11638)
    Add support for FreeBSD/aarch64
    
    (I also needed the mono/mono#7017 workaround and a boringssl patch for auxval stuff, but this is the important part.)
    
    
    Commit migrated from https://github.com/mono/mono/commit/b60b7249631c08a109238b07eefc501b58d52794

--- src/mono/mono/utils/mono-sigcontext.h
+++ src/mono/mono/utils/mono-sigcontext.h
@@ -467,6 +467,13 @@ typedef struct ucontext {
 	#define UCONTEXT_REG_SP(ctx) (((ucontext64_t*)(ctx))->uc_mcontext64->__ss.__sp)
 	#define UCONTEXT_REG_R0(ctx) (((ucontext64_t*)(ctx))->uc_mcontext64->__ss.__x [ARMREG_R0])
 	#define UCONTEXT_GREGS(ctx) (&(((ucontext64_t*)(ctx))->uc_mcontext64->__ss.__x))
+#elif defined(__FreeBSD__)
+#include <ucontext.h>
+	/* https://lists.freebsd.org/pipermail/freebsd-arm/2017-February/015611.html */
+	#define UCONTEXT_REG_PC(ctx) (((ucontext_t*)(ctx))->uc_mcontext.mc_gpregs.gp_elr)
+	#define UCONTEXT_REG_SP(ctx) (((ucontext_t*)(ctx))->uc_mcontext.mc_gpregs.gp_sp)
+	#define UCONTEXT_REG_R0(ctx) (((ucontext_t*)(ctx))->uc_mcontext.mc_gpregs.gp_x [ARMREG_R0])
+	#define UCONTEXT_GREGS(ctx) (&(((ucontext_t*)(ctx))->uc_mcontext.mc_gpregs.gp_x))
 #else
 #include <ucontext.h>
 	#define UCONTEXT_REG_PC(ctx) (((ucontext_t*)(ctx))->uc_mcontext.pc)
