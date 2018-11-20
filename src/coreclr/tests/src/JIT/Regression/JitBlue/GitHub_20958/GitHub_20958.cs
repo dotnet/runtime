4f3c7d6703001f920ee702ec96a6c6e214964a44
Carol Eidt <carol.eidt@microsoft.com>
2018-11-19 19:43:03 -0800

    Add asserts for the types of the Span indexers. (dotnet/coreclr#21037)
    Also, add a test for dotnet/coreclr#20958
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/398c3bc97d5297df9773284a03e425f90c40bcf0

---
+++ src/coreclr/tests/src/JIT/Regression/JitBlue/GitHub_20958/GitHub_20958.cs
@@ -0,0 +1,61 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+
+// This test was extracted from the Indexer tests in the corefx System.Memory.Tests.
+// The JIT was trying to expand the form of indexer that takes a range, but was not
+// correctly expanding it, as it was expecting only the scalar index form.
+
+public class GitHub_20958
+{
+
+    public static int IndexerWithRangeTest()
+    {
+        int returnVal = 100;
+   
+        ReadOnlySpan<char> span = "Hello".AsSpan();
+        ReadOnlySpan<char> sliced = span[Range.Create(new Index(1, fromEnd: false), new Index(1, fromEnd: true))];
+        if (span.Slice(1, 3) != sliced)
+        {
+            returnVal = -1;
+        }
+        try
+        {
+            ReadOnlySpan<char> s = "Hello".AsSpan()[Range.Create(new Index(1, fromEnd: true), new Index(1, fromEnd: false))];
+            returnVal = -1;
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+        }
+        catch (Exception e)
+        {
+            returnVal = -1;
+        }
+        Span<char> span1 = new Span<char>(new char[] { 'H', 'e', 'l', 'l', 'o' });
+        Span<char> sliced1 = span1[Range.Create(new Index(2, fromEnd: false), new Index(1, fromEnd: true))];
+        if (span1.Slice(2, 2) != sliced1)
+        {
+            returnVal = -1;
+        }
+        try
+        {
+            Span<char> s = new Span<char>(new char[] { 'H', 'i' })[Range.Create(new Index(0, fromEnd: true), new Index(1, fromEnd: false))];
+            returnVal = -1;
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+        }
+        catch (Exception e)
+        {
+            returnVal = -1;
+        }
+        return returnVal;
+    }
+
+    public static int Main()
+    {
+        return IndexerWithRangeTest();
+    }
+}
