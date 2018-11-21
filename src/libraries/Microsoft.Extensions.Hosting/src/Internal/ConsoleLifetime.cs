4b5bf32c2a1936e92900ad07ab31a3a35688b0fd
Chris Ross (ASP.NET) <chrross@microsoft.com>
2018-11-21 11:26:25 -0800

    Hosting/dotnet/Extensions#1584 Replace Console.WriteLine with Logging in generic host
    Commit migrated from https://github.com/dotnet/Extensions/commit/c2351bb4664c89ef40004989d2c58988ffb79445

--- src/libraries/Microsoft.Extensions.Hosting/src/Internal/ConsoleLifetime.cs
+++ src/libraries/Microsoft.Extensions.Hosting/src/Internal/ConsoleLifetime.cs
@@ -1,9 +1,11 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Options;
 
 namespace Microsoft.Extensions.Hosting.Internal
@@ -16,10 +18,14 @@ namespace Microsoft.Extensions.Hosting.Internal
         private readonly ManualResetEvent _shutdownBlock = new ManualResetEvent(false);
 
         public ConsoleLifetime(IOptions<ConsoleLifetimeOptions> options, IHostingEnvironment environment, IApplicationLifetime applicationLifetime)
+            : this(options, environment, applicationLifetime, NullLoggerFactory.Instance) { }
+
+        public ConsoleLifetime(IOptions<ConsoleLifetimeOptions> options, IHostingEnvironment environment, IApplicationLifetime applicationLifetime, ILoggerFactory loggerFactory)
         {
             Options = options?.Value ?? throw new ArgumentNullException(nameof(options));
             Environment = environment ?? throw new ArgumentNullException(nameof(environment));
             ApplicationLifetime = applicationLifetime ?? throw new ArgumentNullException(nameof(applicationLifetime));
+            Logger = loggerFactory.CreateLogger("Microsoft.Extensions.Hosting.Host");
         }
 
         private ConsoleLifetimeOptions Options { get; }
@@ -28,15 +34,17 @@ namespace Microsoft.Extensions.Hosting.Internal
 
         private IApplicationLifetime ApplicationLifetime { get; }
 
+        private ILogger Logger { get; }
+
         public Task WaitForStartAsync(CancellationToken cancellationToken)
         {
             if (!Options.SuppressStatusMessages)
             {
                 ApplicationLifetime.ApplicationStarted.Register(() =>
                 {
-                    Console.WriteLine("Application started. Press Ctrl+C to shut down.");
-                    Console.WriteLine($"Hosting environment: {Environment.EnvironmentName}");
-                    Console.WriteLine($"Content root path: {Environment.ContentRootPath}");
+                    Logger.LogInformation("Application started. Press Ctrl+C to shut down.");
+                    Logger.LogInformation("Hosting environment: {envName}", Environment.EnvironmentName);
+                    Logger.LogInformation("Content root path: {contentRoot}", Environment.ContentRootPath);
                 });
             }
 
