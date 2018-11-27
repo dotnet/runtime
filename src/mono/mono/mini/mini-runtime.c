22f0ac834a1ebd59a777fbf64447ac0dc953b502
Vlad Brezae <brezaevlad@gmail.com>
2018-11-28 00:42:03 +0200

    [runtime] Check for error to prevent overwrites (mono/mono#11812)
    Fixes crash in corlib suite on fullaotinterp
    
    Commit migrated from https://github.com/mono/mono/commit/51b977699f9aecc4a6d21370c6255054a83f8c14

--- src/mono/mono/mini/mini-runtime.c
+++ src/mono/mono/mini/mini-runtime.c
@@ -2415,11 +2415,19 @@ lookup_start:
 	}
 #endif
 
-	if (!code)
+	if (!code) {
 		code = compile_special (method, target_domain, error);
 
-	if (!jit_only && !code && mono_aot_only && mono_use_interpreter && method->wrapper_type != MONO_WRAPPER_OTHER)
+		if (!mono_error_ok (error))
+			return NULL;
+	}
+
+	if (!jit_only && !code && mono_aot_only && mono_use_interpreter && method->wrapper_type != MONO_WRAPPER_OTHER) {
 		code = mini_get_interp_callbacks ()->create_method_pointer (method, TRUE, error);
+
+		if (!mono_error_ok (error))
+			return NULL;
+	}
 
 	if (!code) {
 		if (mono_class_is_open_constructed_type (m_class_get_byval_arg (method->klass))) {
