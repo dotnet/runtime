40bf810b5cb83ba45008f9f8c12a4e3d46eb6832
Andy Ayers <andya@microsoft.com>
2018-11-29 00:32:55 -0800

    Track single def locals in importer (#21251)
    * Defer setting lvSingleDef until lvaMarkLocalVars
    
    Since this information isn't used early on in the jit, defer setting this bit
    until later.
    
    * Track single-def locals in the importer
    
    and use this information to enable type updates during late devirtualization.
    This gets some cases where the exact type materializes late (perhaps via a
    chain of copies).

--- src/jit/compiler.h
+++ src/jit/compiler.h
@@ -359,8 +359,11 @@ public:
 #if OPT_BOOL_OPS
     unsigned char lvIsBoolean : 1; // set if variable is boolean
 #endif
+    unsigned char lvSingleDef : 1; // variable has a single def
+                                   // before lvaMarkLocalVars: identifies ref type locals that can get type updates
+                                   // after lvaMarkLocalVars: identifies locals that are suitable for optAddCopies
+
 #if ASSERTION_PROP
-    unsigned char lvSingleDef : 1;    // variable has a single def
     unsigned char lvDisqualify : 1;   // variable is no longer OK for add copy optimization
     unsigned char lvVolatileHint : 1; // hint for AssertionProp
 #endif
