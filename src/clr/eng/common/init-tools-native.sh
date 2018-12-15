e0c38cd6165cafb289d01432ffb21807456d51a5
Sven Boemer <sbomer@gmail.com>
2018-12-14 17:03:40 -0800

    Publish packages in Azure DevOps official build (dotnet/coreclr#21536)
    This enables coreclr participation in dependency flow.
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/483135b05e853290d6890f9f5f72aa0bb96aca23

--- src/clr/eng/common/init-tools-native.sh
+++ src/clr/eng/common/init-tools-native.sh
@@ -10,7 +10,7 @@ force=false
 download_retries=5
 retry_wait_time_seconds=30
 global_json_file="${scriptroot}/../../global.json"
-declare -A native_assets
+declare -a native_assets
 
 . $scriptroot/native/common-library.sh
 
