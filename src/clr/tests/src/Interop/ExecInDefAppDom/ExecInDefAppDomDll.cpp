063c1f11cdeb6ec0910375189a9778c6c6fe4ccb
Steve MacLean <stmaclea@microsoft.com>
2018-12-17 21:23:20 -0500

    Do not use CorHost2::m_fStarted to gate execution (dotnet/coreclr#20953)
    * ExecuteInDefaultAppDomain w/o Start
    
    Do not require Start() call before
    calling ExecuteInDefaultAppDomain()
    
    * Remove unused variable
    * Move flags as needed to CorHost2
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/f6d403292c17f7024509c59593bcab05ee9379f1

--- src/clr/tests/src/Interop/ExecInDefAppDom/ExecInDefAppDomDll.cpp
+++ src/clr/tests/src/Interop/ExecInDefAppDom/ExecInDefAppDomDll.cpp
@@ -53,12 +53,9 @@ CallExecuteInDefaultAppDomain(LPCWSTR pwzAssemblyPath,
     if (!host)
         return E_FAIL;
 
-    if(host->Start())
-        return E_FAIL;
-
     auto result = host->ExecuteInDefaultAppDomain(pwzAssemblyPath, pwzTypeName, pwzMethodName, pwzArgument, pReturnValue);
 
-    host->Stop();
+    host->Release();
 
     return result;
 }
