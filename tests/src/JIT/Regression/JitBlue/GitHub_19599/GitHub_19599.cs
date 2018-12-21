3e216841102ab82a6f4b276aadd8558d78819c12
mikedn <onemihaid@hotmail.com>
2018-12-21 02:10:27 +0200

    Don't morph volatile IND(ADDR(LCL_VAR)) (#20843)
    Besides the fact that volatile indirections aren't supposed to be removed doing so in this case results in incorrect code being generated.
    
    The GT_IND node has GTF_DONT_CSE set and this gets copied to the GT_LCL_VAR node. Later this prevents the insertion of a normalization cast because GTF_DONT_CSE on a GT_LCL_VAR node is assumed to mean that the variable address is being taken.

---
+++ tests/src/JIT/Regression/JitBlue/GitHub_19599/GitHub_19599.cs
@@ -0,0 +1,30 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+struct S0
+{
+    public byte F0;
+}
+
+class Program
+{
+    static S0 s_2;
+    static long s_5;
+
+    static int Main()
+    {
+        s_2.F0 = 128;
+        M7(s_2);
+        return (s_5 == 128) ? 100 : -1;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    static void M7(S0 arg0)
+    {
+        s_5 = Volatile.Read(ref arg0.F0);
+    }
+}
