29ce17a47b6bfd97db26d17817e040c36c809d15
Zoltan Varga <vargaz@gmail.com>
2018-12-20 23:57:51 -0500

    [jit] Emit a OP_NOT_NULL in MONO_EMIT_NULL_CHECK. (mono/mono#12078)
    Commit migrated from https://github.com/mono/mono/commit/7846f99f35fec44c2af2ca86d93762ca81586c9c

--- src/mono/mono/mini/ir-emit.h
+++ src/mono/mono/mini/ir-emit.h
@@ -889,6 +889,7 @@ static int ccount = 0;
 		} else {			\
 			MONO_EMIT_NEW_IMPLICIT_EXCEPTION_LOAD_STORE (cfg);						\
 		}																\
+		MONO_EMIT_NEW_UNALU (cfg, OP_NOT_NULL, -1, reg);				\
 	} while (0)
 
 #define MONO_EMIT_NEW_CHECK_THIS(cfg, sreg) do { \
@@ -898,8 +899,8 @@ static int ccount = 0;
 		} else {											\
 			MONO_EMIT_NEW_UNALU (cfg, OP_CHECK_THIS, -1, sreg);			\
 			MONO_EMIT_NEW_IMPLICIT_EXCEPTION_LOAD_STORE (cfg);			\
+			MONO_EMIT_NEW_UNALU (cfg, OP_NOT_NULL, -1, sreg);			\
 		}																\
-		MONO_EMIT_NEW_UNALU (cfg, OP_NOT_NULL, -1, sreg);				\
 	} while (0)
 
 #define NEW_LOAD_MEMBASE_FLAGS(cfg,dest,op,dr,base,offset,ins_flags) do {	\
