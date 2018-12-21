d3bba92e18d7330459eeb8d4b17463d7da1b0c75
Marek Safar <marek.safar@gmail.com>
2018-12-21 20:15:08 +0100

    Moves ByReference to shared CoreLib (dotnet/coreclr#21633)
    Commit migrated from https://github.com/dotnet/coreclr/commit/cc68e4af8388c7b85984d1fc769e90ddba14f3e5

--- src/libraries/System.Private.CoreLib/src/System/ByReference.cs
+++ src/libraries/System.Private.CoreLib/src/System/ByReference.cs
@@ -13,24 +13,29 @@ namespace System
     [NonVersionable]
     internal readonly ref struct ByReference<T>
     {
-        private readonly IntPtr _value;
+        // CS0169: The private field '{blah}' is never used
+#pragma warning disable 169
+         private readonly IntPtr _value;
+#pragma warning restore
 
+        [Intrinsic]
         public ByReference(ref T value)
         {
             // Implemented as a JIT intrinsic - This default implementation is for 
             // completeness and to provide a concrete error if called via reflection
             // or if intrinsic is missed.
-            throw new System.PlatformNotSupportedException();
+            throw new PlatformNotSupportedException();
         }
 
         public ref T Value
         {
+            [Intrinsic]
             get
             {
                 // Implemented as a JIT intrinsic - This default implementation is for 
                 // completeness and to provide a concrete error if called via reflection
                 // or if the intrinsic is missed.
-                throw new System.PlatformNotSupportedException();
+                throw new PlatformNotSupportedException();
             }
         }
     }
