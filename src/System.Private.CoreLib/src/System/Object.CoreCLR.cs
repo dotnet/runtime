302dda27ee4ea80cc879c62cb40ce23fcc0de39f
Jan Kotas <jkotas@microsoft.com>
2018-12-21 16:07:22 -0800

    Cleanup to match conventions

---
+++ src/System.Private.CoreLib/src/System/Object.CoreCLR.cs
@@ -0,0 +1,25 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace System
+{
+    [ClassInterface(ClassInterfaceType.AutoDispatch)]
+    [ComVisible(true)]
+    public partial class Object
+    {
+        // Returns a Type object which represent this object instance.
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        public extern Type GetType();
+
+        // Returns a new object instance that is a memberwise copy of this 
+        // object.  This is always a shallow copy of the instance. The method is protected
+        // so that other object may only call this method on themselves.  It is intended to
+        // support the ICloneable interface.
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        protected extern object MemberwiseClone();
+    }
+}
