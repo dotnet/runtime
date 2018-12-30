d516f4753c5526ddd52de78033c25016cf81cd30
Marek Safar <marek.safar@gmail.com>
2018-12-30 08:38:06 +0100

    Moves generic comparers to shared CoreLib (dotnet/coreclr#21649)
    * Moves generic comparer to shared CoreLib
    
    * Keep Index/LastIndexOf optimizations in CoreCLR only
    
    * Moved runtime-specific enum helper to runtime-specific partial type
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/3b5782fa0aa4086ee72904b6052f21cba693bcf7

---
+++ src/coreclr/src/System.Private.CoreLib/src/System/Collections/Generic/Comparer.CoreCLR.cs
@@ -0,0 +1,26 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+
+using System.Runtime;
+using System.Runtime.CompilerServices;
+
+namespace System.Collections.Generic
+{
+    [TypeDependencyAttribute("System.Collections.Generic.ObjectComparer`1")]
+    public abstract partial class Comparer<T> : IComparer, IComparer<T>
+    {
+        // To minimize generic instantiation overhead of creating the comparer per type, we keep the generic portion of the code as small
+        // as possible and define most of the creation logic in a non-generic class.
+        public static Comparer<T> Default { get; } = (Comparer<T>)ComparerHelpers.CreateDefaultComparer(typeof(T));
+    }
+
+    internal sealed partial class EnumComparer<T> : Comparer<T> where T : struct, Enum
+    {
+        public override int Compare(T x, T y)
+        {
+            return System.Runtime.CompilerServices.JitHelpers.EnumCompareTo(x, y);
+        }
+    }
+}
