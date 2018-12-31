e132ec17decf1cd3bc0a441380a17fc7dfeb9a99
David Fowler <davidfowl@gmail.com>
2018-12-31 19:24:30 -0400

    Made changes to ChangeToken.OnChange (dotnet/Extensions#869)
    - Introduced ChangeTokenRegistration to reduce allocations and managed the the registration to the underlying change token.
    - Support dispose working if the token fires more than once
    
    
    Commit migrated from https://github.com/dotnet/Extensions/commit/b6d7d4c9402ba3ed347799f71d5db5d26d95190d

--- src/libraries/Microsoft.Extensions.Primitives/tests/ChangeTokenTest.cs
+++ src/libraries/Microsoft.Extensions.Primitives/tests/ChangeTokenTest.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using Xunit;
 
@@ -109,7 +110,7 @@ namespace Microsoft.Extensions.Primitives
             {
                 // AsyncLocal not set, when run on clean context
                 // A suppressed flow runs in current context, rather than restoring the captured context
-                Assert.Equal(0, ((AsyncLocal<int>) al).Value);
+                Assert.Equal(0, ((AsyncLocal<int>)al).Value);
                 executed = true;
             }, asyncLocal);
 
@@ -122,6 +123,193 @@ namespace Microsoft.Extensions.Primitives
             // AsyncLocal should still be set
             Assert.Equal(1, asyncLocal.Value);
             Assert.True(executed);
+        }
+
+        [Fact]
+        public void DisposingChangeTokenRegistrationDoesNotRaiseConsumerCallback()
+        {
+            var provider = new ResettableChangeTokenProvider();
+            var count = 0;
+            var reg = ChangeToken.OnChange(provider.GetChangeToken, () =>
+            {
+                count++;
+            });
+
+            for (int i = 0; i < 5; i++)
+            {
+                provider.Changed();
+            }
+
+            Assert.Equal(5, count);
+
+            reg.Dispose();
+
+            for (int i = 0; i < 5; i++)
+            {
+                provider.Changed();
+            }
+
+            Assert.Equal(5, count);
+        }
+
+        [Fact]
+        public void DisposingChangeTokenRegistrationDoesNotRaiseConsumerCallbackStateOverload()
+        {
+            var provider = new ResettableChangeTokenProvider();
+            var count = 0;
+            var reg = ChangeToken.OnChange<object>(provider.GetChangeToken, state =>
+            {
+                count++;
+            },
+            null);
+
+            for (int i = 0; i < 5; i++)
+            {
+                provider.Changed();
+            }
+
+            Assert.Equal(5, count);
+
+            reg.Dispose();
+
+            for (int i = 0; i < 5; i++)
+            {
+                provider.Changed();
+            }
+
+            Assert.Equal(5, count);
+        }
+
+        [Fact]
+        public void DisposingChangeTokenRegistrationDuringCallbackWorks()
+        {
+            var provider = new ResettableChangeTokenProvider();
+            var count = 0;
+
+            IDisposable reg = null;
+
+            reg = ChangeToken.OnChange<object>(provider.GetChangeToken, state =>
+            {
+                count++;
+                reg.Dispose();
+            },
+            null);
+
+            provider.Changed();
+
+            Assert.Equal(1, count);
+
+            provider.Changed();
+
+            Assert.Equal(1, count);
+        }
+
+        [Fact]
+        public void DoubleDisposeDisposesOnce()
+        {
+            var provider = new TrackableChangeTokenProvider();
+            var count = 0;
+
+            IDisposable reg = null;
+
+            reg = ChangeToken.OnChange<object>(provider.GetChangeToken, state =>
+            {
+                count++;
+                reg.Dispose();
+            },
+            null);
+
+            provider.Changed();
+
+            Assert.Equal(1, count);
+            Assert.Equal(1, provider.RegistrationCalls);
+            Assert.Equal(1, provider.DisposeCalls);
+
+            reg.Dispose();
+
+            provider.Changed();
+
+            Assert.Equal(1, count);
+            Assert.Equal(2, provider.RegistrationCalls);
+            Assert.Equal(2, provider.DisposeCalls);
+        }
+
+        public class TrackableChangeTokenProvider
+        {
+            private TrackableChangeToken _cts = new TrackableChangeToken();
+
+            public int RegistrationCalls { get; set; }
+            public int DisposeCalls { get; set; }
+
+            public IChangeToken GetChangeToken() => _cts;
+
+            public void Changed()
+            {
+                var previous = _cts;
+                _cts = new TrackableChangeToken();
+                previous.Execute();
+
+                RegistrationCalls += previous.RegistrationCalls;
+                DisposeCalls += previous.DisposeCalls;
+            }
+        }
+
+        public class TrackableChangeToken : IChangeToken
+        {
+            private CancellationTokenSource _cts = new CancellationTokenSource();
+
+            public int RegistrationCalls { get; set; }
+            public int DisposeCalls { get; set; }
+
+            public bool HasChanged => _cts.IsCancellationRequested;
+
+            public bool ActiveChangeCallbacks => true;
+
+            public void Execute()
+            {
+                _cts.Cancel();
+            }
+
+            public IDisposable RegisterChangeCallback(Action<object> callback, object state)
+            {
+                var registration = _cts.Token.Register(callback, state);
+                RegistrationCalls++;
+
+                return new DisposableAction(() =>
+                {
+                    DisposeCalls++;
+                    registration.Dispose();
+                });
+            }
+
+            private class DisposableAction : IDisposable
+            {
+                private Action _action;
+
+                public DisposableAction(Action action)
+                {
+                    _action = action;
+                }
+
+                public void Dispose()
+                {
+                    _action?.Invoke();
+                }
+            }
+        }
+
+        public class ResettableChangeTokenProvider
+        {
+            private CancellationTokenSource _cts = new CancellationTokenSource();
+
+            public IChangeToken GetChangeToken() => new CancellationChangeToken(_cts.Token);
+
+            public void Changed()
+            {
+                var previous = _cts;
+                _cts = new CancellationTokenSource();
+                previous.Cancel();
+            }
         }
     }
 }
