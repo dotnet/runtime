ccaf35e1e0dc6bbfb86e4fe6b8c24fe8fe346e55
Tanner Gooding <tagoo@outlook.com>
2019-01-07 16:57:34 -0800

    Fixing ContainCheckHWIntrinsic to ensure that scalar integer operands are the appropriate size (#21641)
    * Fixing ContainCheckHWIntrinsic to ensure that scalar integer operands are the appropriate size
    
    * Adding a regression test for issue 21625
    
    * Fixing IsContainableHWIntrinsicOp to use the containing node type (rather than the simd base type) for Scalar intrinsics
    
    * Fixing the containment check for `Sse41.Insert(V128<float>, V128<float>, byte)`
    
    * Cleaning up the isContainableHWIntrinsicOp logic in lowerxarch.cpp
    
    * Restrict containment to genActualType(baseType)
    
    * Formatting
    
    * Removing some comments and simplifying the supportsContainment checks for various HWIntrinsics that take a scalar operand
    
    * Applying formatting patch

---
+++ tests/src/JIT/Regression/JitBlue/GitHub_21625/GitHub_21625.cs
@@ -0,0 +1,52 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
+
+namespace GitHub_21625
+{
+    public class test
+    {
+        public static Vector128<ushort> CreateScalar(ushort value)
+        {
+            if (Sse2.IsSupported)
+            {
+                return Sse2.ConvertScalarToVector128UInt32(value).AsUInt16();
+            }
+ 
+            return SoftwareFallback(value);
+ 
+            Vector128<ushort> SoftwareFallback(ushort x)
+            {
+                var result = Vector128<ushort>.Zero;
+                Unsafe.WriteUnaligned(ref Unsafe.As<Vector128<ushort>, byte>(ref result), value);
+                return result;
+            }
+        }
+
+        static int Main()
+        {
+            ushort value = TestLibrary.Generator.GetUInt16();
+            Vector128<ushort> result = CreateScalar(value);
+
+            if (result.GetElement(0) != value)
+            {
+                return 0;
+            }
+
+            for (int i = 1; i < Vector128<ushort>.Count; i++)
+            {
+                if (result.GetElement(i) != 0)
+                {
+                    return 0;
+                }
+            }
+
+            return 100;
+        }
+    }
+}
