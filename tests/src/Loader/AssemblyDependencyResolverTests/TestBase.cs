dbf50d82754a894ba53781dbb839198a46605c9f
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2019-01-09 16:02:36 -0800

    Implement AssemblyDependencyResolver (#21896)
    * Implementation of ComponentDependencyResolver
    
    PInvokes into hostpolicy.dll (which should live next to the runtime and thus always be reachable).
    If the PInvoke fails (missing hostpolicy.dll) we will fail for now.
    
    Adds tests for the API into CoreCLR repo. The main reason is that with corerun
    we can easily mock the hostpolicy.dll since there's none to start with.
    Writing the same tests in CoreFX or any other place which starts the runtime through
    hostpolicy would require test-only functionality to exist in either the class itself
    or in the hostpolicy.
    
    * Fix test project file to work outside of VS
    
    * Better test cleanup to not leave artifacts on disk.
    
    * CDR native resolution tests
    
     Add native resolution tests
    
    * Implements detailed error reporting for ComponentDependencyResolver.
    
    Registers error writer with the hostpolicy to receive detailed errors. Uses that in the exception.
    
    Modifications to the mock and the tests to be able to verify the functionality.
    
    * Revert overly eager cleanup
    
    * Change public API surface naming to match the approved API surface.
    
    * Fix nits.
    
    * Fix renames.

---
+++ tests/src/Loader/AssemblyDependencyResolverTests/TestBase.cs
@@ -0,0 +1,102 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace AssemblyDependencyResolverTests
+{
+    class TestBase
+    {
+        protected string TestBasePath { get; private set; }
+        protected string BinaryBasePath { get; private set; }
+        protected string CoreRoot { get; private set; }
+
+        protected virtual void Initialize()
+        {
+        }
+
+        protected virtual void Cleanup()
+        {
+        }
+
+        public static int RunTests(params Type[] testTypes)
+        {
+            int result = 100;
+            foreach (Type testType in testTypes)
+            {
+                int testResult = RunTestsForType(testType);
+                if (testResult != 100)
+                {
+                    result = testResult;
+                }
+            }
+
+            return result;
+        }
+
+        private static int RunTestsForType(Type testType)
+        {
+            string testBasePath = Path.GetDirectoryName(testType.Assembly.Location);
+
+            TestBase runner = (TestBase)Activator.CreateInstance(testType);
+            runner.TestBasePath = testBasePath;
+            runner.BinaryBasePath = Path.GetDirectoryName(testBasePath);
+            runner.CoreRoot = GetCoreRoot();
+
+            try
+            {
+                runner.Initialize();
+
+                runner.RunTestsForInstance(runner);
+                return runner._retValue;
+            }
+            finally
+            {
+                runner.Cleanup();
+            }
+        }
+
+        private int _retValue = 100;
+        private void RunSingleTest(Action test, string testName = null)
+        {
+            testName = testName ?? test.Method.Name;
+
+            try
+            {
+                Console.WriteLine($"{testName} Start");
+                test();
+                Console.WriteLine($"{testName} PASSED.");
+            }
+            catch (Exception exe)
+            {
+                Console.WriteLine($"{testName} FAILED:");
+                Console.WriteLine(exe.ToString());
+                _retValue = -1;
+            }
+        }
+
+        private void RunTestsForInstance(object testClass)
+        {
+            foreach (MethodInfo m in testClass.GetType()
+                .GetMethods(BindingFlags.Instance | BindingFlags.Public)
+                .Where(m => m.Name.StartsWith("Test") && m.GetParameters().Length == 0))
+            {
+                RunSingleTest(() => m.Invoke(testClass, new object[0]), m.Name);
+            }
+        }
+
+        private static string GetCoreRoot()
+        {
+            string value = Environment.GetEnvironmentVariable("CORE_ROOT");
+            if (value == null)
+            {
+                value = Directory.GetCurrentDirectory();
+            }
+
+            return value;
+        }
+    }
+}
