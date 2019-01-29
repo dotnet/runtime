09b5d391139e336ca5076226da842f4f36d4023b
Vitek Karas <vitek.karas@microsoft.com>
2019-01-29 01:10:56 -0800

    Fix lookup of coreclr.dll in .deps.json to not rely on relative paths (dotnet/core-setup#5050)
    The existing code expects the .deps.json record for coreclr.dll (and clrjit.dll) to have at least one directory separator, since that was always the case up until now.
    With recent changes to targeting packs, these records are just file names, and the above logic breaks.
    
    Fix the logic by lookup at the resolved absolute path, which is guaranteed to have a directory separator before the library name (we need the separator to match whole file names).
    
    Added a test which validates the new scenario.
    
    Commit migrated from https://github.com/dotnet/core-setup/commit/988132f2a89decd7203d665d26df53bc1c8f9eec

--- src/installer/test/TestUtils/RuntimeInformationExtensions.cs
+++ src/installer/test/TestUtils/RuntimeInformationExtensions.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Threading.Tasks;
+﻿using System.Runtime.InteropServices;
 
 namespace Microsoft.DotNet.CoreSetup.Test
 {
