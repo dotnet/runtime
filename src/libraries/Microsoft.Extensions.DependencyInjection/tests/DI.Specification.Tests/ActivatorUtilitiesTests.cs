5a99e9b530c6d2d0af30cb39440184026e0c9586
Ryan Brandenburg <rybrande@microsoft.com>
2019-01-29 13:26:53 -0800

    Use Arcade (dotnet/Extensions#586)
    Use arcade
    
    
    Commit migrated from https://github.com/dotnet/Extensions/commit/f0458995c625dba132a74733e041b97c0e10e025

--- src/libraries/Microsoft.Extensions.DependencyInjection/tests/DI.Specification.Tests/ActivatorUtilitiesTests.cs
+++ src/libraries/Microsoft.Extensions.DependencyInjection/tests/DI.Specification.Tests/ActivatorUtilitiesTests.cs
@@ -111,9 +111,9 @@ namespace Microsoft.Extensions.DependencyInjection.Specification
             // Assert
             Assert.NotNull(anotherClass);
             Assert.Equal(ConsoleColor.DarkGreen, anotherClass.Color);
-            Assert.Equal(null, anotherClass.ColorNull);
+            Assert.Null(anotherClass.ColorNull);
             Assert.Equal(12, anotherClass.Integer);
-            Assert.Equal(null, anotherClass.IntegerNull);
+            Assert.Null(anotherClass.IntegerNull);
         }
 
         [Theory]
@@ -248,7 +248,7 @@ namespace Microsoft.Extensions.DependencyInjection.Specification
         public void TypeActivatorThrowsOnMultipleMarkedCtors(CreateInstanceFunc createFunc)
         {
             // Act
-            var exception =  Assert.Throws<InvalidOperationException>(() => CreateInstance<ClassWithMultipleMarkedCtors>(createFunc, null, "hello"));
+            var exception = Assert.Throws<InvalidOperationException>(() => CreateInstance<ClassWithMultipleMarkedCtors>(createFunc, null, "hello"));
 
             // Assert
             Assert.Equal("Multiple constructors were marked with ActivatorUtilitiesConstructorAttribute.", exception.Message);
@@ -259,7 +259,7 @@ namespace Microsoft.Extensions.DependencyInjection.Specification
         public void TypeActivatorThrowsWhenMarkedCtorDoesntAcceptArguments(CreateInstanceFunc createFunc)
         {
             // Act
-            var exception =  Assert.Throws<InvalidOperationException>(() => CreateInstance<ClassWithAmbiguousCtorsAndAttribute>(createFunc, null, 0, "hello"));
+            var exception = Assert.Throws<InvalidOperationException>(() => CreateInstance<ClassWithAmbiguousCtorsAndAttribute>(createFunc, null, 0, "hello"));
 
             // Assert
             Assert.Equal("Constructor marked with ActivatorUtilitiesConstructorAttribute does not accept all given argument types.", exception.Message);
