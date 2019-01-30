d38567fb0b63ad49600339fece50206f2f3bc3f0
Jonathan Chambers <joncham@gmail.com>
2019-01-30 13:57:03 -0500

    Implement marshaling for generic struct fields (mono/mono#12660)
    Commit migrated from https://github.com/mono/mono/commit/115c1965757ad16733315a8230df72c2baeabf32

--- src/mono/mono/metadata/marshal-ilgen.c
+++ src/mono/mono/metadata/marshal-ilgen.c
@@ -1028,12 +1028,20 @@ emit_struct_conv_full (MonoMethodBuilder *mb, MonoClass *klass, gboolean to_obje
 					mono_mb_emit_byte (mb, mono_type_to_stind (ftype));
 				}
 				break;
+			case MONO_TYPE_GENERICINST:
+				if (!mono_type_generic_inst_is_valuetype (ftype)) {
+					char *msg = g_strdup_printf ("Generic type %s cannot be marshaled as field in a struct.",
+						mono_type_full_name (ftype));
+					mono_mb_emit_exception_marshal_directive (mb, msg);
+					break;
+				}
+				/* fall through */
 			case MONO_TYPE_VALUETYPE: {
 				int src_var, dst_var;
 				MonoType *etype;
 				int len;
 
-				if (m_class_is_enumtype (ftype->data.klass)) {
+				if (t == MONO_TYPE_VALUETYPE && m_class_is_enumtype (ftype->data.klass)) {
 					ftype = mono_class_enum_basetype_internal (ftype->data.klass);
 					goto handle_enum;
 				}
@@ -1052,7 +1060,7 @@ emit_struct_conv_full (MonoMethodBuilder *mb, MonoClass *klass, gboolean to_obje
 				if (get_fixed_buffer_attr (info->fields [i].field, &etype, &len)) {
 					emit_fixed_buf_conv (mb, ftype, etype, len, to_object, &usize);
 				} else {
-					emit_struct_conv (mb, ftype->data.klass, to_object);
+					emit_struct_conv (mb, mono_class_from_mono_type_internal (ftype), to_object);
 				}
 
 				/* restore the old src pointer */
