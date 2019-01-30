50180a7c6af23529c2cc0343d9c6097fa7074269
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2019-01-30 21:16:13 +0100

    Fix CorError header file generation (dotnet/coreclr#22294)
    The checked in batch script was only usable if you have a Razzle environment and even then it didn't update the right files.
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/6a43dd765ea6341310fde3f83bb41ff88d94b435

--- src/coreclr/src/pal/prebuilt/corerror/makecorerror.bat
+++ src/coreclr/src/pal/prebuilt/corerror/makecorerror.bat
@@ -4,24 +4,8 @@ REM The .NET Foundation licenses this file to you under the MIT license.
 REM See the LICENSE file in the project root for more information.
 setlocal
 
-if "%_NTROOT%" == "" goto LUsage
+csc ..\..\..\inc\genheaders.cs
 
-set MANAGED_TOOLS_PATH=%MANAGED_TOOLS_ROOT%\%MANAGED_TOOLS_VERSION%
-set CORERROR_PATH=%_NTROOT%\ndp\clr\src\inc
+genheaders.exe ..\..\..\inc\corerror.xml ..\inc\corerror.h mscorurt.rc
 
-%MANAGED_TOOLS_PATH%\genheaders.exe %CORERROR_PATH%\corerror.xml ..\inc\corerror.h mscorurt.rc
-
-goto LExit
-
-:LUsage
-
-echo.
-echo makecorerror.bat
-echo.
-echo    Builds corerror.h for PALRT
-echo.
-echo    Should be run inside razzle environment, depends on %%_NTROOT%% environment variable.
-echo.
-
-:LExit
-
+del genheaders.exe
