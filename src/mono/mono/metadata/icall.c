e2b8015676313c267cc36d89eb882285f7500701
Bernhard Urban <lewurm@gmail.com>
2019-01-31 17:02:17 +0100

    [arm/ios] workaround for faulty vcmp.f64 insn
    Fixes https://github.com/mono/mono/issues/11965
    
    
    Commit migrated from https://github.com/mono/mono/commit/719117f9d702312db4ebdfd2b5fe551f7825cb16

--- src/mono/mono/metadata/icall.c
+++ src/mono/mono/metadata/icall.c
@@ -7840,6 +7840,18 @@ ves_icall_System_Runtime_InteropServices_RuntimeInformation_get_RuntimeArchitect
 int
 ves_icall_Interop_Sys_DoubleToString(double value, char *format, char *buffer, int bufferLength)
 {
+#if defined(TARGET_ARM)
+	/* workaround for faulty vcmp.f64 implementation on some 32bit ARM CPUs */
+	guint64 bits = *(guint64 *) &value;
+	if (bits == 0x1) { /* 4.9406564584124654E-324 */
+		g_assert (!strcmp (format, "%.40e"));
+		return snprintf (buffer, bufferLength, "%s", "4.9406564584124654417656879286822137236506e-324");
+	} else if (bits == 0x4) { /* 2E-323 */
+		g_assert (!strcmp (format, "%.40e"));
+		return snprintf (buffer, bufferLength, "%s", "1.9762625833649861767062751714728854894602e-323");
+	}
+#endif
+
 	return snprintf(buffer, bufferLength, format, value);
 }
 
