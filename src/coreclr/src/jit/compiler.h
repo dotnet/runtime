441a1f5331cf5f3c74c0cfdfca3ef095945e21a5
Carol Eidt <carol.eidt@microsoft.com>
2019-02-04 21:49:25 -0800

    Extract addressing mode analysis from gtSetEvalOrder (dotnet/coreclr#22387)
    * Extract addressing mode analysis from gtSetEvalOrder
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/8362be7ec4a8f4ca88ed151ace9a81d6f916508b

--- src/coreclr/src/jit/compiler.h
+++ src/coreclr/src/jit/compiler.h
@@ -2555,6 +2555,12 @@ public:
     // Returns true iff the secondNode can be swapped with firstNode.
     bool gtCanSwapOrder(GenTree* firstNode, GenTree* secondNode);
 
+    // Given an address expression, compute its costs and addressing mode opportunities,
+    // and mark addressing mode candidates as GTF_DONT_CSE.
+    // TODO-Throughput - Consider actually instantiating these early, to avoid
+    // having to re-run the algorithm that looks for them (might also improve CQ).
+    bool gtMarkAddrMode(GenTree* addr, int* costEx, int* costSz, var_types type);
+
     unsigned gtSetEvalOrder(GenTree* tree);
 
     void gtSetStmtInfo(GenTree* stmt);
