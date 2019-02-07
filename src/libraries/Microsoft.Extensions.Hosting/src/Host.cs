813d42307d6c238c876028117b598f9cf8c746a9
Chris Ross <Tratcher@Outlook.com>
2019-02-07 12:01:54 -0800

    Use the DOTNET_ env var prefix dotnet/Extensions#4150 (dotnet/Extensions#1084)
    Commit migrated from https://github.com/dotnet/Extensions/commit/40dc8fac8adfabbdc62dc55a51f37709bfbe84a6

--- src/libraries/Microsoft.Extensions.Hosting/src/Host.cs
+++ src/libraries/Microsoft.Extensions.Hosting/src/Host.cs
@@ -20,7 +20,7 @@ namespace Microsoft.Extensions.Hosting
             builder.UseContentRoot(Directory.GetCurrentDirectory());
             builder.ConfigureHostConfiguration(config =>
             {
-                config.AddEnvironmentVariables(); // TODO: Prefix?
+                config.AddEnvironmentVariables("DOTNET_");
                 if (args != null)
                 {
                     config.AddCommandLine(args);
