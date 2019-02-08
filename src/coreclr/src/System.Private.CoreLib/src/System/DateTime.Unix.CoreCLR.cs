09acf2ad80b23bd2053790daa527155169f74984
Marek Safar <marek.safar@gmail.com>
2019-02-08 04:45:44 +0100

    Move DateTime for Unix to shared partition (dotnet/coreclr#22383)
    * Move DateTime for Unix to shared partition
    
    * Keep CoreCLR specific implementation
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/4c480a5f8e1e12d83f089891231c8eadbb5762e1

--- src/coreclr/src/System.Private.CoreLib/src/System/DateTime.Unix.CoreCLR.cs
+++ src/coreclr/src/System.Private.CoreLib/src/System/DateTime.Unix.CoreCLR.cs
@@ -8,8 +8,6 @@ namespace System
 {
     public readonly partial struct DateTime
     {
-        internal const bool s_systemSupportsLeapSeconds = false;
-
         public static DateTime UtcNow
         {
             get
@@ -18,13 +16,7 @@ namespace System
             }
         }
 
-        internal static DateTime FromFileTimeLeapSecondsAware(long fileTime) => default(DateTime);
-        internal static long ToFileTimeLeapSecondsAware(long ticks) => default(long);
-
-        // IsValidTimeWithLeapSeconds is not expected to be called at all for now on non-Windows platforms
-        internal static bool IsValidTimeWithLeapSeconds(int year, int month, int day, int hour, int minute, int second, DateTimeKind kind) => false;
-
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        internal static extern long GetSystemTimeAsFileTime();
+        private static extern long GetSystemTimeAsFileTime();
     }
 }
