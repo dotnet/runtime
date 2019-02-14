e36e9ba7d948a1693d3e9d5e48cd49bfb1de588f
Juan Hoyos <juan.hoyos@microsoft.com>
2019-02-13 18:14:35 -0800

    Use dependency flow for core-setup and corefx dependencies (dotnet/coreclr#22544)
    * Remove old use of signing package used to work around SPC signing now that arcade has bubbled up the version.
    * Move dependency update for CoreFX and Core-setup dependencies to BAR/Darc/Maestro++ flow
    * Update property references in tests.
    * Version bump for CoreFX and Core-Setup dependencies.
    * Update test Github_20958 to use revised System.Range constructor as surface area changed with dotnet/coreclr#22331.
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/ebaa5ddf432ed4dd120007e0bdc13717943147f3

--- src/clr/tests/src/JIT/Regression/JitBlue/GitHub_20958/GitHub_20958.cs
+++ src/clr/tests/src/JIT/Regression/JitBlue/GitHub_20958/GitHub_20958.cs
@@ -16,14 +16,14 @@ public class GitHub_20958
         int returnVal = 100;
 
         ReadOnlySpan<char> span = "Hello".AsSpan();
-        ReadOnlySpan<char> sliced = span[Range.Create(new Index(1, fromEnd: false), new Index(1, fromEnd: true))];
+        ReadOnlySpan<char> sliced = span[new Range(new Index(1, fromEnd: false), new Index(1, fromEnd: true))];
         if (span.Slice(1, 3) != sliced)
         {
             returnVal = -1;
         }
         try
         {
-            ReadOnlySpan<char> s = "Hello".AsSpan()[Range.Create(new Index(1, fromEnd: true), new Index(1, fromEnd: false))];
+            ReadOnlySpan<char> s = "Hello".AsSpan()[new Range(new Index(1, fromEnd: true), new Index(1, fromEnd: false))];
             returnVal = -1;
         }
         catch (ArgumentOutOfRangeException)
@@ -34,14 +34,14 @@ public class GitHub_20958
             returnVal = -1;
         }
         Span<char> span1 = new Span<char>(new char[] { 'H', 'e', 'l', 'l', 'o' });
-        Span<char> sliced1 = span1[Range.Create(new Index(2, fromEnd: false), new Index(1, fromEnd: true))];
+        Span<char> sliced1 = span1[new Range(new Index(2, fromEnd: false), new Index(1, fromEnd: true))];
         if (span1.Slice(2, 2) != sliced1)
         {
             returnVal = -1;
         }
         try
         {
-            Span<char> s = new Span<char>(new char[] { 'H', 'i' })[Range.Create(new Index(0, fromEnd: true), new Index(1, fromEnd: false))];
+            Span<char> s = new Span<char>(new char[] { 'H', 'i' })[new Range(new Index(0, fromEnd: true), new Index(1, fromEnd: false))];
             returnVal = -1;
         }
         catch (ArgumentOutOfRangeException)
