4ec7d7c2059adf7b7a2d5bfae7b2aeb5e1a13780
Swaroop Sridhar <Swaroop.Sridhar@microsoft.com>
2019-02-17 13:27:41 -0800

    Fix the build of Loader/NativeLib test (dotnet/coreclr#22648)
    The native test assets were not build correctly on Unix platforms.
    The native library exports were generated as private symbols by clang.
    
    This change fixes the export declaration so that the global symbols are
    correctly generated for exported symbols.
    
    Fixes dotnet/coreclr#22549
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/390d002ac1ff919fcaae047eb09217ddeac78779

--- src/coreclr/tests/src/Loader/NativeLibs/FromNativePaths_lib.cpp
+++ src/coreclr/tests/src/Loader/NativeLibs/FromNativePaths_lib.cpp
@@ -1,7 +1,13 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-extern "C" bool NativeFunc()
+#if defined(__clang__)
+#define EXPORT_API extern "C" __attribute__((visibility("default")))
+#elif defined(_MSC_VER)
+#define EXPORT_API extern "C" __declspec(dllexport)
+#endif 
+
+EXPORT_API bool NativeFunc()
 {
     return true;
 }
