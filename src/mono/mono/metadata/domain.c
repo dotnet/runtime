defe4b6ee8638f3eeff45efcb4d7677b69cf8443
Zoltan Varga <vargaz@gmail.com>
2019-02-18 04:20:45 -0500

    [netcore] EH work (mono/mono#13041)
    * [netcore] Switch to Debugger class from corlib.
    
    * [netcore] Implement exception stack trace support.
    
    * Address review comments.
    
    * [netcore] Create a netcore specific version of Exception based on the refsource version.
    
    
    Commit migrated from https://github.com/mono/mono/commit/77644b833e0d461c3b18314736e53ac19bd1d866

--- src/mono/mono/metadata/domain.c
+++ src/mono/mono/metadata/domain.c
@@ -741,8 +741,13 @@ mono_init_internal (const char *filename, const char *exe_filename, const char *
 	mono_defaults.method_info_class = mono_class_load_from_name (
 		mono_defaults.corlib, "System.Reflection", "MethodInfo");
 
+#ifdef ENABLE_NETCORE
+	mono_defaults.stack_frame_class = mono_class_load_from_name (
+	        mono_defaults.corlib, "System.Diagnostics", "MonoStackFrame");
+#else
 	mono_defaults.stack_frame_class = mono_class_load_from_name (
 	        mono_defaults.corlib, "System.Diagnostics", "StackFrame");
+#endif
 
 	mono_defaults.marshal_class = mono_class_load_from_name (
 	        mono_defaults.corlib, "System.Runtime.InteropServices", "Marshal");
