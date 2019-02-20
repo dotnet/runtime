9ef91bfc6a23b96f8cc6fc05451ca65a949cca14
Mike McLaughlin <mikem@microsoft.com>
2019-02-19 23:16:25 -0800

    Fix CLRDataCreateInstance export. This was breaking SOS. (dotnet/coreclr#22701)
    Fix CLRDataCreateInstance export. This was breaking SOS.
    
    Added the right cmake magic so the function is exported properly.
    
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/e0c2cc567cb7b5072eb71b0ff788da2ffde63a7d

--- src/coreclr/src/debug/daccess/dacdbiimpl.h
+++ src/coreclr/src/debug/daccess/dacdbiimpl.h
@@ -12,9 +12,16 @@
 #ifndef _DACDBI_IMPL_H_
 #define _DACDBI_IMPL_H_
 
-// Prototype for creation function
+// Prototype for creation functions
+
+DLLEXPORT 
 STDAPI
-DLLEXPORT
+CLRDataCreateInstance(REFIID iid,
+    ICLRDataTarget * pLegacyTarget,
+    void ** iface);
+
+DLLEXPORT 
+STDAPI
 DacDbiInterfaceInstance(
     ICorDebugDataTarget * pTarget,
     CORDB_ADDRESS baseAddress,
