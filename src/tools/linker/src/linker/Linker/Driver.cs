04c7ce95f2862d1aa5ec83d00f13f9264cbd3c18
Unknown <michaelv@unity3d.com>
2019-02-20 13:08:56 -0500

    Add an option to disable override removal
    Commit migrated from https://github.com/dotnet/linker/commit/8cd60396669885e5ef1727bf5a43ab9584ef3264

--- src/tools/linker/src/linker/Linker/Driver.cs
+++ src/tools/linker/src/linker/Linker/Driver.cs
@@ -361,6 +361,9 @@ namespace Mono.Linker {
 						case "beforefieldinit":
 							context.DisabledOptimizations |= CodeOptimizations.BeforeFieldInit;
 							break;
+						case "overrideremoval":
+							context.DisabledOptimizations |= CodeOptimizations.OverrideRemoval;
+							break;
 						}
 					}
 				}
@@ -519,6 +522,7 @@ namespace Mono.Linker {
 			Console.WriteLine ("  --custom-step <name>      Add a custom step to the pipeline");
 			Console.WriteLine ("  --disable-opt <name>      Disable one of the default optimizations");
 			Console.WriteLine ("                              beforefieldinit: Unused static fields are removed if there is no static ctor");
+			Console.WriteLine ("                              overrideremoval: Overrides of virtual methods on types that are never instantiated are removed");
 			Console.WriteLine ("  --exclude-feature <name>  Any code which has a feature <name> in linked assemblies will be removed");
 			Console.WriteLine ("                              com: Support for COM Interop");
 			Console.WriteLine ("                              etw: Event Tracing for Windows");
