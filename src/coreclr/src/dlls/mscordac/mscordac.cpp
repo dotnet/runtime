cbc3d5203bb13b0e0e12fc4d15bcba734659af96
Mike McLaughlin <mikem@microsoft.com>
2019-02-19 23:16:25 -0800

    Fix CLRDataCreateInstance export. This was breaking SOS. (dotnet/coreclr#22701)
    Fix CLRDataCreateInstance export. This was breaking SOS.
    
    Added the right cmake magic so the function is exported properly.
    
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/e0c2cc567cb7b5072eb71b0ff788da2ffde63a7d

--- src/coreclr/src/dlls/mscordac/mscordac.cpp
+++ src/coreclr/src/dlls/mscordac/mscordac.cpp
@@ -1,21 +1,3 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
-
-#ifdef FEATURE_PAL
-
-#include <clrdata.h>
-
-//
-// This dummy reference to CLRDataCreateInstance prevents the LLVM toolchain from optimizing this important export out.
-//
-#ifdef __GNUC__
-__attribute__((used))
-#endif // __GNUC__
-void
-DummyReferenceToExportedAPI()
-{
-    CLRDataCreateInstance(IID_ICLRDataTarget, NULL, NULL);
-}
-
-#endif // FEATURE_PAL
