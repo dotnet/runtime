2ad02f4920710d3f35840ff44180e91cd05925bf
Jan Vorlicek <janvorli@microsoft.com>
2019-02-25 17:56:56 +0100

    Use function pointer term in the coreclr_create_delegate doc
    Commit migrated from https://github.com/dotnet/coreclr/commit/ab8bc11531fd7fb6d05aee0b0bf670beab38da41

--- src/clr/src/coreclr/hosts/inc/coreclrhost.h
+++ src/clr/src/coreclr/hosts/inc/coreclrhost.h
@@ -77,7 +77,7 @@ CORECLR_HOSTING_API(coreclr_shutdown_2,
             int* latchedExitCode);
 
 //
-// Create a native callable delegate for a managed method.
+// Create a native callable function pointer for a managed method.
 //
 // Parameters:
 //  hostHandle              - Handle of the host
@@ -85,7 +85,7 @@ CORECLR_HOSTING_API(coreclr_shutdown_2,
 //  entryPointAssemblyName  - Name of the assembly which holds the custom entry point
 //  entryPointTypeName      - Name of the type which holds the custom entry point
 //  entryPointMethodName    - Name of the method which is the custom entry point
-//  delegate                - Output parameter, the function stores a pointer to the delegate at the specified address
+//  delegate                - Output parameter, the function stores a native callable function pointer to the delegate at the specified address
 //
 // Returns:
 //  HRESULT indicating status of the operation. S_OK if the assembly was successfully executed
--- src/clr/src/dlls/mscoree/unixinterface.cpp
+++ src/clr/src/dlls/mscoree/unixinterface.cpp
@@ -333,7 +333,7 @@ int coreclr_shutdown_2(
 }
 
 //
-// Create a native callable delegate for a managed method.
+// Create a native callable function pointer for a managed method.
 //
 // Parameters:
 //  hostHandle              - Handle of the host
@@ -341,7 +341,7 @@ int coreclr_shutdown_2(
 //  entryPointAssemblyName  - Name of the assembly which holds the custom entry point
 //  entryPointTypeName      - Name of the type which holds the custom entry point
 //  entryPointMethodName    - Name of the method which is the custom entry point
-//  delegate                - Output parameter, the function stores a pointer to the delegate at the specified address
+//  delegate                - Output parameter, the function stores a native callable function pointer to the delegate at the specified address
 //
 // Returns:
 //  HRESULT indicating status of the operation. S_OK if the assembly was successfully executed
