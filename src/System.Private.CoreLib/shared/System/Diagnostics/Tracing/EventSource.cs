63b850917dd28c7ab08a51954c8799ca5440ea4e
Stephen Toub <stoub@microsoft.com>
2019-02-27 08:43:03 -0500

    Fix BitOperations using in standalone EventSource build
    Signed-off-by: dotnet-bot <dotnet-bot@microsoft.com>

--- src/System.Private.CoreLib/shared/System/Diagnostics/Tracing/EventSource.cs
+++ src/System.Private.CoreLib/shared/System/Diagnostics/Tracing/EventSource.cs
@@ -185,7 +185,7 @@ using Microsoft.Win32;
 
 #if ES_BUILD_STANDALONE
 using EventDescriptor = Microsoft.Diagnostics.Tracing.EventDescriptor;
-using BitOps = Microsoft.Diagnostics.Tracing.Internal.BitOps;
+using BitOperations = Microsoft.Diagnostics.Tracing.Internal.BitOperations;
 #else
 using System.Threading.Tasks;
 #endif
