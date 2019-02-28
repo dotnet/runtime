8eebf242fa1d05042e51b87ee562e03ce32ee8c4
Hao Kung <HaoK@users.noreply.github.com>
2019-02-28 15:57:34 -0800

    Switch Configuration.Json to use System.Json.Text and add backcompat NewtonsoftJson package
    Commit migrated from https://github.com/dotnet/Extensions/commit/5b7d7d730464a399f17e83bc406c314586608dfa

--- src/libraries/Microsoft.Extensions.Configuration.Json/tests/ConfigurationProviderJsonTest.cs
+++ src/libraries/Microsoft.Extensions.Configuration.Json/tests/ConfigurationProviderJsonTest.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using Microsoft.Extensions.Configuration.Json;
 using Microsoft.Extensions.Configuration.Test;
+using Newtonsoft.Json.Linq;
 
 namespace Microsoft.Extensions.Configuration
 {
@@ -20,7 +21,7 @@ namespace Microsoft.Extensions.Configuration
         protected override (IConfigurationProvider Provider, Action Initializer) LoadThroughProvider(TestSection testConfig)
         {
             var jsonBuilder = new StringBuilder();
-            SectionToJson(jsonBuilder, testConfig);
+            SectionToJson(jsonBuilder, testConfig, includeComma: false);
 
             var provider = new JsonConfigurationProvider(
                 new JsonConfigurationSource
@@ -29,30 +30,37 @@ namespace Microsoft.Extensions.Configuration
                 });
 
             var json = jsonBuilder.ToString();
-
+            json = JObject.Parse(json).ToString(); // standardize the json (removing trailing commas)
             return (provider, () => provider.Load(TestStreamHelpers.StringToStream(json)));
         }
 
-        private void SectionToJson(StringBuilder jsonBuilder, TestSection section)
+        private void SectionToJson(StringBuilder jsonBuilder, TestSection section, bool includeComma = true)
         {
-            string ValueToJson(object value) => value == null ? "null" : $"'{value}'";
+            string ValueToJson(object value) => value == null ? "null" : $"\"{value}\"";
 
             jsonBuilder.AppendLine("{");
 
             foreach (var tuple in section.Values)
             {
                 jsonBuilder.AppendLine(tuple.Value.AsArray != null
-                    ? $"'{tuple.Key}': [{string.Join(", ", tuple.Value.AsArray.Select(ValueToJson))}],"
-                    : $"'{tuple.Key}': {ValueToJson(tuple.Value.AsString)},");
+                    ? $"\"{tuple.Key}\": [{string.Join(", ", tuple.Value.AsArray.Select(ValueToJson))}],"
+                    : $"\"{tuple.Key}\": {ValueToJson(tuple.Value.AsString)},");
             }
 
             foreach (var tuple in section.Sections)
             {
-                jsonBuilder.Append($"'{tuple.Key}': ");
+                jsonBuilder.Append($"\"{tuple.Key}\": ");
                 SectionToJson(jsonBuilder, tuple.Section);
             }
 
-            jsonBuilder.AppendLine("},");
+            if (includeComma)
+            {
+                jsonBuilder.AppendLine("},");
+            }
+            else
+            {
+                jsonBuilder.AppendLine("}");
+            }
         }
     }
 }
