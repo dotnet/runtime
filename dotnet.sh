ff53a99fb9e4c6e479a60c09a7014bfb72af4936
Jan Kotas <jkotas@microsoft.com>
2019-03-02 08:49:52 -0800

    Disable DOTNET_MULTILEVEL_LOOKUP in the build scripts (#22972)
    Fixes #22971

--- dotnet.sh
+++ dotnet.sh
@@ -2,6 +2,12 @@
 
 working_tree_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Don't resolve runtime, shared framework, or SDK from other locations to ensure build determinism
+export DOTNET_MULTILEVEL_LOOKUP=0
+
+# Disable first run since we want to control all package sources
+export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
+
 echo "Running init-tools.sh"
 source $working_tree_root/init-tools.sh
 
