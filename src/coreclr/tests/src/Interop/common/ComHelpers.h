6031b754e0dd5212f4902a13f0689a587737229c
Aaron Robinson <arobins@microsoft.com>
2019-03-01 22:53:42 -0800

    Support IClassFactory2 - LicenseManager use in COM activation (dotnet/coreclr#22846)
    * Implement IClassFactory2
    
    * Add test support for IClassFactory2
    
    * Add support for testing RCW activation via IClassFactory2
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/9e66e140f09b32694b32b98c34d8948bdf3608a3

--- src/coreclr/tests/src/Interop/common/ComHelpers.h
+++ src/coreclr/tests/src/Interop/common/ComHelpers.h
@@ -231,3 +231,103 @@ public: // IUnknown
 
     DEFINE_REF_COUNTING();
 };
+
+// Templated class factory
+// Supplied type must have the following properties to use this template:
+//  1) Have a static method with the following signature:
+//    - HRESULT RequestLicKey(BSTR *key);
+//  2) Have a constructor that takes an optional BSTR value as the key
+template<typename T>
+class ClassFactoryLicense : public UnknownImpl, public IClassFactory2
+{
+public: // static
+    static HRESULT Create(_In_ REFIID riid, _Outptr_ LPVOID FAR* ppv)
+    {
+        try
+        {
+            auto cf = new ClassFactoryLicense();
+            HRESULT hr = cf->QueryInterface(riid, ppv);
+            cf->Release();
+            return hr;
+        }
+        catch (const std::bad_alloc&)
+        {
+            return E_OUTOFMEMORY;
+        }
+    }
+
+public: // IClassFactory
+    STDMETHOD(CreateInstance)(
+        _In_opt_  IUnknown *pUnkOuter,
+        _In_  REFIID riid,
+        _COM_Outptr_  void **ppvObject)
+    {
+        return CreateInstanceLic(pUnkOuter, nullptr, riid, nullptr, ppvObject);
+    }
+
+    STDMETHOD(LockServer)(/* [in] */ BOOL fLock)
+    {
+        assert(false && "Not impl");
+        return E_NOTIMPL;
+    }
+
+public: // IClassFactory2
+    STDMETHOD(GetLicInfo)(
+        /* [out][in] */ __RPC__inout LICINFO *pLicInfo)
+    {
+        // The CLR does not call this function and as such,
+        // returns an error. Note that this is explicitly illegal
+        // in a proper implementation of IClassFactory2.
+        return E_UNEXPECTED;
+    }
+
+    STDMETHOD(RequestLicKey)(
+        /* [in] */ DWORD dwReserved,
+        /* [out] */ __RPC__deref_out_opt BSTR *pBstrKey)
+    {
+        if (dwReserved != 0)
+            return E_UNEXPECTED;
+
+        return T::RequestLicKey(pBstrKey);
+    }
+
+    STDMETHOD(CreateInstanceLic)(
+        /* [annotation][in] */ _In_opt_  IUnknown *pUnkOuter,
+        /* [annotation][in] */ _Reserved_  IUnknown *pUnkReserved,
+        /* [annotation][in] */ __RPC__in  REFIID riid,
+        /* [annotation][in] */ __RPC__in  BSTR bstrKey,
+        /* [annotation][iid_is][out] */ __RPC__deref_out_opt  PVOID *ppvObj)
+    {
+        if (pUnkOuter != nullptr)
+            return CLASS_E_NOAGGREGATION;
+
+        if (pUnkReserved != nullptr)
+            return E_UNEXPECTED;
+
+        try
+        {
+            auto ti = new T(bstrKey);
+            HRESULT hr = ti->QueryInterface(riid, ppvObj);
+            ti->Release();
+            return hr;
+        }
+        catch (HRESULT hr)
+        {
+            return hr;
+        }
+        catch (const std::bad_alloc&)
+        {
+            return E_OUTOFMEMORY;
+        }
+    }
+
+public: // IUnknown
+    STDMETHOD(QueryInterface)(
+        /* [in] */ REFIID riid,
+        /* [iid_is][out] */ _COM_Outptr_ void __RPC_FAR *__RPC_FAR *ppvObject)
+    {
+        return DoQueryInterface(riid, ppvObject, static_cast<IClassFactory *>(this), static_cast<IClassFactory2 *>(this));
+    }
+
+    DEFINE_REF_COUNTING();
+};
