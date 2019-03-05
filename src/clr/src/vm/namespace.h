82708e7a805c5dec063f4a331f1473d9536b4682
Aaron Robinson <arobins@microsoft.com>
2019-03-04 18:58:56 -0800

    Remove loose dependency in runtime on System.ComponentModel. (dotnet/coreclr#23010)
    Remove loose dependency in runtime on System.ComponentModel.
    Disable test to sync with CoreFX
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/88ada237f7765b7edd39967adab8ea2ba25ea16c

--- src/clr/src/vm/namespace.h
+++ src/clr/src/vm/namespace.h
@@ -28,6 +28,7 @@
 #define g_CollectionsGenericNS g_SystemNS ".Collections.Generic"
 
 #define g_InteropServicesNS g_SystemNS ".Runtime.InteropServices"
+#define g_InternalInteropServicesNS  "Internal.Runtime.InteropServices"
 #define g_ReflectionNS      g_SystemNS ".Reflection"
 #define g_ReflectionEmitNS  g_ReflectionNS ".Emit"
 
