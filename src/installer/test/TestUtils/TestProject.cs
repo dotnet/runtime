670c45f6e8bd20324350511ac0c2d53ec0a82b7b
Vitek Karas <vitek.karas@microsoft.com>
2019-03-06 12:49:48 -0800

    Introduce TestArtifact and some refactoring. (dotnet/core-setup#5352)
    Commit migrated from https://github.com/dotnet/core-setup/commit/dc843c54682ef408062de0212706b4930853522f

--- src/installer/test/TestUtils/TestProject.cs
+++ src/installer/test/TestUtils/TestProject.cs
@@ -1,32 +1,48 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 
 namespace Microsoft.DotNet.CoreSetup.Test
 {
-    public class TestProject : IDisposable
+    public class TestProject : TestArtifact
     {
-        public string ProjectDirectory { get; }
-        public string ProjectName { get; }
+        public string ProjectDirectory { get => Location; }
+        public string ProjectName { get => Name; }
 
+        public string AssemblyName { get; private set; }
         public string OutputDirectory { get; set; }
-        public string AssemblyName { get; set; }
-        public string ProjectFile { get; set; }
-        public string ProjectAssetsJson { get; set; }
-        public string RuntimeConfigJson { get; set; }
-        public string RuntimeDevConfigJson { get; set; }
-        public string DepsJson { get; set; }
-        public string AppDll { get; set; }
-        public string AppExe { get; set; }
-        public string HostPolicyDll { get; set; }
-        public string HostFxrDll { get; set; }
+        public string ProjectFile { get; private set; }
+        public string ProjectAssetsJson { get; private set; }
+        public string RuntimeConfigJson { get => BuiltApp?.RuntimeConfigJson; }
+        public string RuntimeDevConfigJson { get => BuiltApp?.RuntimeDevConfigJson; }
+        public string DepsJson { get => BuiltApp?.DepsJson; }
+        public string AppDll { get => BuiltApp?.AppDll; }
+        public string AppExe { get => BuiltApp?.AppExe; }
+        public string HostPolicyDll { get => BuiltApp?.HostPolicyDll; }
+        public string HostFxrDll { get => BuiltApp?.HostFxrDll; }
+
+        public TestApp BuiltApp { get; private set; }
 
         public TestProject(
             string projectDirectory,
             string outputDirectory = null,
             string assemblyName = null)
+            : base(projectDirectory)
         {
-            ProjectDirectory = projectDirectory;
-            ProjectName = Path.GetFileName(ProjectDirectory);
+            Initialize(outputDirectory, assemblyName);
+        }
+
+        public TestProject(TestProject source)
+            : base(source)
+        {
+            Initialize(null, source.AssemblyName);
+        }
+
+        public TestProject Copy()
+        {
+            return new TestProject(this);
+        }
+
+        private void Initialize(string outputDirectory, string assemblyName)
+        {
             AssemblyName = assemblyName ?? ProjectName;
             ProjectFile = Path.Combine(ProjectDirectory, $"{ProjectName}.csproj");
             ProjectAssetsJson = Path.Combine(ProjectDirectory, "obj", "project.assets.json");
@@ -38,28 +54,9 @@ namespace Microsoft.DotNet.CoreSetup.Test
             }
         }
 
-        public void Dispose()
-        {
-            if (!PreserveTestRuns())
-            {
-                Directory.Delete(ProjectDirectory, true);
-            }
-        }
-
-        public void CopyProjectFiles(string directory)
-        {
-            CopyRecursive(ProjectDirectory, directory, overwrite: true);
-        }
-
         public void LoadOutputFiles()
         {
-            AppDll = Path.Combine(OutputDirectory, $"{AssemblyName}.dll");
-            AppExe = Path.Combine(OutputDirectory, RuntimeInformationExtensions.GetExeFileNameForCurrentPlatform(AssemblyName));
-            DepsJson = Path.Combine(OutputDirectory, $"{AssemblyName}.deps.json");
-            RuntimeConfigJson = Path.Combine(OutputDirectory, $"{AssemblyName}.runtimeconfig.json");
-            RuntimeDevConfigJson = Path.Combine(OutputDirectory, $"{AssemblyName}.runtimeconfig.dev.json");
-            HostPolicyDll = Path.Combine(OutputDirectory, RuntimeInformationExtensions.GetSharedLibraryFileNameForCurrentPlatform("hostpolicy"));
-            HostFxrDll = Path.Combine(OutputDirectory, RuntimeInformationExtensions.GetSharedLibraryFileNameForCurrentPlatform("hostfxr"));
+            BuiltApp = new TestApp(OutputDirectory, AssemblyName);
         }
 
         public bool IsRestored()
@@ -70,35 +67,6 @@ namespace Microsoft.DotNet.CoreSetup.Test
             }
 
             return File.Exists(ProjectAssetsJson);
-        }
-
-        private void CopyRecursive(string sourceDirectory, string destinationDirectory, bool overwrite = false)
-        {
-            if ( ! Directory.Exists(destinationDirectory))
-            {
-                Directory.CreateDirectory(destinationDirectory);
-            }
-
-            foreach (var dir in Directory.EnumerateDirectories(sourceDirectory))
-            {
-                CopyRecursive(dir, Path.Combine(destinationDirectory, Path.GetFileName(dir)), overwrite);
-            }
-
-            foreach (var file in Directory.EnumerateFiles(sourceDirectory))
-            {
-                var dest = Path.Combine(destinationDirectory, Path.GetFileName(file));
-                if (!File.Exists(dest) || overwrite)
-                {
-                    // We say overwrite true, because we only get here if the file didn't exist (thus it doesn't matter) or we
-                    // wanted to overwrite :)
-                    File.Copy(file, dest, overwrite: true);
-                }
-            }
-        }
-
-        public static bool PreserveTestRuns()
-        {
-            return Environment.GetEnvironmentVariable("PRESERVE_TEST_RUNS") == "1";
         }
     }
 }
