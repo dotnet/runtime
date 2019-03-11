d32b4bd98d137872d08872d7008b360ff08b3e95
Adeel <adeelbm@outlook.com>
2019-03-11 02:38:32 -0700

    Remove conditions around CMAKE_REQUIRED_LIBRARIES
    Commit migrated from https://github.com/dotnet/coreclr/commit/107b9424d7dd56c1902b09bb1c67533215190ea8

--- src/coreclr/src/pal/src/configure.cmake
+++ src/coreclr/src/pal/src/configure.cmake
@@ -42,9 +42,7 @@ if(NOT CMAKE_SYSTEM_NAME STREQUAL Darwin)
   check_include_files("libintl.h" HAVE_LIBINTL_H)
 endif()
 
-if(NOT CMAKE_SYSTEM_NAME STREQUAL FreeBSD AND NOT CMAKE_SYSTEM_NAME STREQUAL NetBSD)
-  set(CMAKE_REQUIRED_LIBRARIES ${CMAKE_DL_LIBS})
-endif()
+set(CMAKE_REQUIRED_LIBRARIES ${CMAKE_DL_LIBS})
 
 check_cxx_source_compiles("
 #include <lttng/tracepoint.h>
@@ -52,9 +50,7 @@ int main(int argc, char **argv) {
   return 0;
 }" HAVE_LTTNG_TRACEPOINT_H)
 
-if(NOT CMAKE_SYSTEM_NAME STREQUAL FreeBSD AND NOT CMAKE_SYSTEM_NAME STREQUAL NetBSD)
-  set(CMAKE_REQUIRED_LIBRARIES)
-endif()
+set(CMAKE_REQUIRED_LIBRARIES)
 
 check_include_files(sys/sysctl.h HAVE_SYS_SYSCTL_H)
 check_include_files(gnu/lib-names.h HAVE_GNU_LIBNAMES_H)
