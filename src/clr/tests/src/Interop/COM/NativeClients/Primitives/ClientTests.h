e7c3aa04d049cbabe7c49ff1907c9273b30e71d0
Aaron Robinson <arobins@microsoft.com>
2019-03-12 20:34:45 -0700

    Default Interface COM testing project (dotnet/coreclr#23195)
    * Add tests for native COM client consuming managed COM server that
      consumes interfaces with default implementation.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/b364ef6c7872d90d75063570f25495286b87acfd

--- src/clr/tests/src/Interop/COM/NativeClients/Primitives/ClientTests.h
+++ src/clr/tests/src/Interop/COM/NativeClients/Primitives/ClientTests.h
@@ -12,58 +12,6 @@
 #define THROW_IF_FAILED(exp) { hr = exp; if (FAILED(hr)) { ::printf("FAILURE: 0x%08x = %s\n", hr, #exp); throw hr; } }
 #define THROW_FAIL_IF_FALSE(exp) { if (!(exp)) { ::printf("FALSE: %s\n", #exp); throw E_FAIL; } }
 
-template<typename T>
-struct ComSmartPtr
-{
-    ComSmartPtr()
-        : p{}
-    { }
-
-    ComSmartPtr(_In_ const ComSmartPtr &) = delete;
-    ComSmartPtr(_Inout_ ComSmartPtr &&) = delete;
-
-    ComSmartPtr& operator=(_In_ const ComSmartPtr &) = delete;
-    ComSmartPtr& operator=(_Inout_ ComSmartPtr &&) = delete;
-
-    ~ComSmartPtr()
-    {
-        if (p != nullptr)
-            p->Release();
-    }
-
-    operator T*()
-    {
-        return p;
-    }
-
-    T** operator&()
-    {
-        return &p;
-    }
-
-    T* operator->()
-    {
-        return p;
-    }
-
-    void Attach(_In_opt_ T *t)
-    {
-        if (p != nullptr)
-            p->Release();
-
-        p = t;
-    }
-
-    T *Detach()
-    {
-        T *tmp = p;
-        p = nullptr;
-        return tmp;
-    }
-
-    T *p;
-};
-
 void Run_NumericTests();
 void Run_ArrayTests();
 void Run_StringTests();
