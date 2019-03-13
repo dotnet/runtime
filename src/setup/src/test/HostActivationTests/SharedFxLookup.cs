1e265a3340abb6b8f13f657afcec028a1987e47d
Vitek Karas <vitek.karas@microsoft.com>
2019-03-13 16:37:29 -0700

    Backfill license headers (dotnet/core-setup#5469)
    Commit migrated from https://github.com/dotnet/core-setup/commit/b944f5ba22e7b4733e6437cb57d65e425cedac3d

--- src/setup/src/test/HostActivationTests/SharedFxLookup.cs
+++ src/setup/src/test/HostActivationTests/SharedFxLookup.cs
@@ -1,4 +1,7 @@
-﻿using Newtonsoft.Json.Linq;
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
