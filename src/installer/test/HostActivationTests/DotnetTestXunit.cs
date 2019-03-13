885ed27fee392ce2618f98e3ef6287b22100fe7f
Vitek Karas <vitek.karas@microsoft.com>
2019-03-13 16:37:29 -0700

    Backfill license headers (dotnet/core-setup#5469)
    Commit migrated from https://github.com/dotnet/core-setup/commit/b944f5ba22e7b4733e6437cb57d65e425cedac3d

--- src/installer/test/HostActivationTests/DotnetTestXunit.cs
+++ src/installer/test/HostActivationTests/DotnetTestXunit.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.DotNet.CoreSetup.Test;
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.DotNet.CoreSetup.Test;
 using System;
 using System.IO;
 using Xunit;
