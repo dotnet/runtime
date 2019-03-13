b944f5ba22e7b4733e6437cb57d65e425cedac3d
Vitek Karas <vitek.karas@microsoft.com>
2019-03-13 16:37:29 -0700

    Backfill license headers (#5469)

--- src/test/TestUtils/DotNetCli.cs
+++ src/test/TestUtils/DotNetCli.cs
@@ -1,4 +1,7 @@
-﻿using System.IO;
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.IO;
 using System.Linq;
 using Microsoft.DotNet.Cli.Build.Framework;
 using Microsoft.DotNet.CoreSetup.Test;
