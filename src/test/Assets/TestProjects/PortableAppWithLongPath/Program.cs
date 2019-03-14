aa0913a7b96bc77935f48d85efe2f67434945c84
Vitek Karas <vitek.karas@microsoft.com>
2019-03-14 10:29:32 -0700

    Use tests folder for test artifacts (#5478)

--- src/test/Assets/TestProjects/PortableAppWithLongPath/Program.cs
+++ src/test/Assets/TestProjects/PortableAppWithLongPath/Program.cs
@@ -12,9 +12,10 @@ namespace PortableAppWithLongPath
             Console.WriteLine(string.Join(Environment.NewLine, args));
 
             // Create a path that is longer than MAX_PATH
-            DirectoryInfo newDir = Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "longPath"));
+            DirectoryInfo newDir = Directory.CreateDirectory(Path.Combine(args[0], "longPath"));
             string longPath = Path.Combine(newDir.FullName, new string('x', 255));
 
+            Console.WriteLine($"Trying to create `{longPath}`");
             bool success = CreateDirectoryW(longPath, IntPtr.Zero);
             Console.WriteLine($"CreateDirectoryW with long path {(success ? "succeeded" : $"failed with {Marshal.GetLastWin32Error()}" )}");
 
