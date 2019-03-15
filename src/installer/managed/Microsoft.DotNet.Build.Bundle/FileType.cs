9a500ecac2ec993836989dd84a5f6fb70a855e64
Swaroop Sridhar <Swaroop.Sridhar@microsoft.com>
2019-03-14 20:10:53 -0700

    Implement Single-file Bundler (dotnet/core-setup#5286)
    * Implement Single-file Bundler
    
    This change implements the single-file bundler, according to this
    [design document](https://github.com/dotnet/designs/blob/master/accepted/single-file/design.md)
    
    The change also adds a test-case that:
    * Bundles the contents of a self-contained console app publish directory along with all sub-directories
    * Extracts the contents to a new location
    * Runs the extracted app to verify successful execution
    
    TBD:
    * Add more tests tests once the host-changes to process the bundle are implemented.
    * Localization of output strings.
    
    Issue: https://github.com/dotnet/coreclr/issues/20287
    
    
    Commit migrated from https://github.com/dotnet/core-setup/commit/1bcd5d5ec790b9d6ade0ff8619bd0acfe6e50269

---
+++ src/installer/managed/Microsoft.DotNet.Build.Bundle/FileType.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.DotNet.Build.Bundle
+{
+    /// <summary>
+    /// FileType: Identifies the type of file embedded into the bundle.
+    /// 
+    /// The bundler differentiates a few kinds of files via the manifest,
+    /// with respect to the way in which they'll be used by the runtime.
+    ///
+    /// - Runtime Configuration files: Processed directly from memory
+    /// - Assemblies: Loaded directly from memory.
+    ///               Currently, these are only pure-managed assemblies.
+    ///               Future versions should include R2R assemblies here.
+    /// - Other files: Files that will be extracted out to disk. 
+    ///                These include native binaries.
+    /// </summary>
+    public enum FileType : byte
+    {
+        Application,        // Represents the main app, also an assembly
+        Assembly,           // IL Assemblies, which will be processed from bundle
+        Ready2Run,          // R2R assemblies, currently unused, spilled to disk.
+        DepsJson,           // Configuration file, processed from bundle
+        RuntimeConfigJson,  // Configuration file, processed from bundle
+        Extract            // Files spilled to disk by the host
+    };
+}
+
---
+++ src/installer/test/Microsoft.DotNet.Build.Bundle.Tests/BundleAndExtract.cs
@@ -0,0 +1,107 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.IO;
+using Xunit;
+using Microsoft.DotNet.Cli.Build.Framework;
+
+namespace Microsoft.DotNet.CoreSetup.Test.BundleTests.BundleExtract
+{
+    public class BundleAndExtract : IClassFixture<BundleAndExtract.SharedTestState>
+    {
+        private SharedTestState sharedTestState;
+
+        public BundleAndExtract(BundleAndExtract.SharedTestState fixture)
+        {
+            sharedTestState = fixture;
+        }
+
+        [Fact]
+        public void Test()
+        {
+            var fixture = sharedTestState.TestFixture
+                .Copy();
+
+            var dotnet = fixture.SdkDotnet;
+            var appDll = fixture.TestProject.AppDll;
+
+            // Run the App normally
+            dotnet.Exec(appDll)
+                .CaptureStdErr()
+                .CaptureStdOut()
+                .Execute()
+                .Should()
+                .Pass()
+                .And
+                .HaveStdOutContaining("Wow! We now say hello to the big world and you.");
+
+
+            // Create a directory for bundle/extraction output.
+            // This directory shouldn't be within TestProject.OutputDirectory, since the bundler
+            // will (attempt to) embed all files below the TestProject.OutputDirectory tree into one file.
+            string singleFileDir = Path.Combine(fixture.TestProject.ProjectDirectory, "oneExe");
+            Directory.CreateDirectory(singleFileDir);
+
+            // Bundle to a single-file
+            string hostName = Path.GetFileName(fixture.TestProject.AppExe);
+            string bundleDll = Path.Combine(sharedTestState.RepoDirectories.Artifacts,
+                                            "Microsoft.DotNet.Build.Bundle",
+                                            "netcoreapp2.0",
+                                            "Microsoft.DotNet.Build.Bundle.dll");
+            string[] bundleArgs = { "--source", fixture.TestProject.OutputDirectory,
+                                    "--apphost", hostName,
+                                    "--output", singleFileDir };
+
+            dotnet.Exec(bundleDll, bundleArgs)
+                .CaptureStdErr()
+                .CaptureStdOut()
+                .Execute()
+                .Should()
+                .Pass();
+
+            // Extract the contents
+            string singleFile = Path.Combine(singleFileDir, hostName);
+            string[] extractArgs = { "--extract", singleFile,
+                                     "--output", singleFileDir };
+
+            dotnet.Exec(bundleDll, extractArgs)
+                .CaptureStdErr()
+                .CaptureStdOut()
+                .Execute()
+                .Should()
+                .Pass();
+
+            // Run the extracted app
+            Command.Create(singleFile)
+                .CaptureStdErr()
+                .CaptureStdOut()
+                .Execute()
+                .Should()
+                .Pass()
+                .And
+                .HaveStdOutContaining("Wow! We now say hello to the big world and you.");
+        }
+
+        public class SharedTestState : IDisposable
+        {
+            public TestProjectFixture TestFixture { get; set; }
+            public RepoDirectoriesProvider RepoDirectories { get; set; }
+
+            public SharedTestState()
+            {
+                RepoDirectories = new RepoDirectoriesProvider();
+
+                TestFixture = new TestProjectFixture("StandaloneAppWithSubDirs", RepoDirectories);
+                TestFixture
+                    .EnsureRestoredForRid(TestFixture.CurrentRid, RepoDirectories.CorehostPackages)
+                    .PublishProject(runtime: TestFixture.CurrentRid);
+            }
+
+            public void Dispose()
+            {
+                TestFixture.Dispose();
+            }
+        }
+    }
+}
