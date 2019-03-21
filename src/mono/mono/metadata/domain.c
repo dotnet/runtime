4e8375425816010c22e02d4736400e49ac159297
Egor Bogatov <egorbo@gmail.com>
2019-03-21 12:29:43 +0200

    [netcore] Implement missing bits in ThreadPool (mono/mono#13566)
    * implement missing bits in ThreadPool
    
    * remove [SecurityCritical]
    
    
    Commit migrated from https://github.com/mono/mono/commit/36a62bbf8a62b1325fbbcb2d97858dda89836609

--- src/mono/mono/metadata/domain.c
+++ src/mono/mono/metadata/domain.c
@@ -786,7 +786,6 @@ mono_init_internal (const char *filename, const char *exe_filename, const char *
 	mono_defaults.generic_ienumerator_class = mono_class_load_from_name (
 	        mono_defaults.corlib, "System.Collections.Generic", "IEnumerator`1");
 
-#ifndef ENABLE_NETCORE
 	ERROR_DECL (error);
 
 	MonoClass *threadpool_wait_callback_class = mono_class_load_from_name (
@@ -795,7 +794,6 @@ mono_init_internal (const char *filename, const char *exe_filename, const char *
 	mono_defaults.threadpool_perform_wait_callback_method = mono_class_get_method_from_name_checked (
 		threadpool_wait_callback_class, "PerformWaitCallback", 0, 0, error);
 	mono_error_assert_ok (error);
-#endif
 
 	domain->friendly_name = g_path_get_basename (filename);
 
