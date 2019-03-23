c1b6e882ab969fb9bfd9fd9e5c6912b419b56042
Vitek Karas <vitek.karas@microsoft.com>
2019-03-23 04:41:45 -0700

    Generalize test settings for framework resolution tests (dotnet/core-setup#5524)
    Add ability to easily apply settings (roll forward and so on) in a dynamic way (picking location via variable).
    This simplifies testing of settings combinations.
    
    
    
    Commit migrated from https://github.com/dotnet/core-setup/commit/7d83e1fa27bb2ef3e3623f37370023446a3d3dbe

---
+++ src/setup/src/test/HostActivationTests/FrameworkResolution/FrameworkResolutionBase.Settings.cs
@@ -0,0 +1,73 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+
+namespace Microsoft.DotNet.CoreSetup.Test.HostActivation.FrameworkResolution
+{
+    public partial class FrameworkResolutionBase
+    {
+        public enum SettingLocation
+        {
+            CommandLine,
+            Environment,
+            RuntimeOptions,
+            FrameworkReference
+        }
+
+        public static Func<TestSettings, TestSettings> RollForwardOnNoCandidateFxSetting(
+            SettingLocation location,
+            int? value,
+            string frameworkReferenceName = MicrosoftNETCoreApp)
+        {
+            if (!value.HasValue)
+            {
+                return testSettings => testSettings;
+            }
+
+            switch (location)
+            {
+                case SettingLocation.Environment:
+                    return testSettings => testSettings.WithEnvironment(Constants.RollForwardOnNoCandidateFxSetting.EnvironmentVariable, value.ToString());
+                case SettingLocation.CommandLine:
+                    return testSettings => testSettings.WithCommandLine(Constants.RollForwardOnNoCandidateFxSetting.CommandLineArgument, value.ToString());
+                case SettingLocation.RuntimeOptions:
+                    return testSettings => testSettings.WithRuntimeConfigCustomizer(rc => rc.WithRollForwardOnNoCandidateFx(value));
+                case SettingLocation.FrameworkReference:
+                    return testSettings => testSettings.WithRuntimeConfigCustomizer(rc =>
+                    {
+                        rc.GetFramework(frameworkReferenceName).WithRollForwardOnNoCandidateFx(value);
+                        return rc;
+                    });
+                default:
+                    throw new Exception($"RollForwardOnNoCandidateFx doesn't support setting location {location}.");
+            }
+        }
+
+        public static Func<TestSettings, TestSettings> ApplyPatchesSetting(
+            SettingLocation location,
+            bool? value,
+            string frameworkReferenceName = MicrosoftNETCoreApp)
+        {
+            if (!value.HasValue)
+            {
+                return testSettings => testSettings;
+            }
+
+            switch (location)
+            {
+                case SettingLocation.RuntimeOptions:
+                    return testSettings => testSettings.WithRuntimeConfigCustomizer(rc => rc.WithApplyPatches(value));
+                case SettingLocation.FrameworkReference:
+                    return testSettings => testSettings.WithRuntimeConfigCustomizer(rc =>
+                    {
+                        rc.GetFramework(frameworkReferenceName).WithApplyPatches(value);
+                        return rc;
+                    });
+                default:
+                    throw new Exception($"ApplyPatches doesn't support setting location {location}.");
+            }
+        }
+    }
+}
--- src/setup/src/test/HostActivationTests/FrameworkResolution/FrameworkResolutionBase.cs
+++ src/setup/src/test/HostActivationTests/FrameworkResolution/FrameworkResolutionBase.cs
@@ -11,7 +11,7 @@ using System.Linq;
 
 namespace Microsoft.DotNet.CoreSetup.Test.HostActivation.FrameworkResolution
 {
-    public abstract class FrameworkResolutionBase
+    public abstract partial class FrameworkResolutionBase
     {
         protected const string MicrosoftNETCoreApp = "Microsoft.NETCore.App";
 
@@ -20,30 +20,82 @@ namespace Microsoft.DotNet.CoreSetup.Test.HostActivation.FrameworkResolution
             TestApp app,
             Func<RuntimeConfig, RuntimeConfig> runtimeConfig,
             Action<CommandResult> resultAction,
-            string[] environment = null,
+            IDictionary<string, string> environment = null,
             string[] commandLine = null,
             bool multiLevelLookup = false)
         {
-            runtimeConfig(RuntimeConfig.Path(app.RuntimeConfigJson)).Save();
-
-            IEnumerable<string> args = commandLine ?? Enumerable.Empty<string>();
-            args = args.Concat(new string[] { app.AppDll });
-
-            IDictionary<string, string> environmentVariables = null;
-            if (environment != null)
-            {
-                environmentVariables = new Dictionary<string, string>();
-                foreach (string v in environment)
+            RunTest(
+                dotnet,
+                app,
+                new TestSettings()
                 {
-                    string[] s = v.Split('=');
-                    environmentVariables.Add(s[0], s[1]);
+                    RuntimeConfigCustomizer = runtimeConfig,
+                    Environment = environment,
+                    CommandLine = commandLine
+                },
+                resultAction,
+                multiLevelLookup);
+        }
+
+        public class TestSettings
+        {
+            public Func<RuntimeConfig, RuntimeConfig> RuntimeConfigCustomizer { get; set; }
+            public IDictionary<string, string> Environment { get; set; }
+            public IEnumerable<string> CommandLine { get; set; }
+
+            public TestSettings WithRuntimeConfigCustomizer(Func<RuntimeConfig, RuntimeConfig> customizer)
+            {
+                Func<RuntimeConfig, RuntimeConfig> previousCustomizer = RuntimeConfigCustomizer;
+                if (previousCustomizer == null)
+                {
+                    RuntimeConfigCustomizer = customizer;
                 }
+                else
+                {
+                    RuntimeConfigCustomizer = runtimeConfig => customizer(previousCustomizer(runtimeConfig));
+                }
+
+                return this;
             }
 
-            CommandResult result = dotnet.Exec(args.First(), args.Skip(1).ToArray())
+            public TestSettings WithEnvironment(string key, string value)
+            {
+                Environment = Environment == null ? 
+                    new Dictionary<string, string>() { { key, value } } : 
+                    new Dictionary<string, string>(Environment.Append(new KeyValuePair<string, string>(key, value)));
+                return this;
+            }
+
+            public TestSettings WithCommandLine(params string[] args)
+            {
+                CommandLine = CommandLine == null ? args : CommandLine.Concat(args);
+                return this;
+            }
+
+            public TestSettings With(Func<TestSettings, TestSettings> modifier)
+            {
+                return modifier(this);
+            }
+        }
+
+        protected void RunTest(
+            DotNetCli dotnet,
+            TestApp app,
+            TestSettings settings,
+            Action<CommandResult> resultAction,
+            bool multiLevelLookup = false)
+        {
+            if (settings.RuntimeConfigCustomizer != null)
+            {
+                settings.RuntimeConfigCustomizer(RuntimeConfig.Path(app.RuntimeConfigJson)).Save();
+            }
+
+            settings.WithCommandLine(app.AppDll);
+
+            CommandResult result = dotnet.Exec(settings.CommandLine.First(), settings.CommandLine.Skip(1).ToArray())
                 .EnvironmentVariable("COREHOST_TRACE", "1")
                 .EnvironmentVariable("DOTNET_MULTILEVEL_LOOKUP", multiLevelLookup ? "1" : "0")
-                .Environment(environmentVariables)
+                .Environment(settings.Environment)
                 .CaptureStdOut()
                 .CaptureStdErr()
                 .Execute();
