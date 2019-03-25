4a8c8dd0260ad36538275e631243f9e9e3e9e0c4
dschinde <dschindelman@gmail.com>
2019-03-25 11:32:27 -0500

    Added XML doc comments (dotnet/Extensions#1304)
    * Added XML doc comments for exceptions to StringSegment.
    
    * Use <see cref...> and <paramref> where appropriate in Microsoft.Extensions.Primitives
    
    * Correct documentation for Microsoft.Extensions.Options.ConfigureOptions.Configure(TOptions)
    
    * Use <see cref...> and <paramref> where appropriate
    
    * Fix doc comment on ConfigurationChangeTokenSource
    
    
    Commit migrated from https://github.com/dotnet/Extensions/commit/730c5d93b2e3f2dc0f045c90e79ad9443ea169da

--- src/libraries/Microsoft.Extensions.Configuration/src/ChainedConfigurationSource.cs
+++ src/libraries/Microsoft.Extensions.Configuration/src/ChainedConfigurationSource.cs
@@ -4,7 +4,7 @@
 namespace Microsoft.Extensions.Configuration
 {
     /// <summary>
-    /// Represents a chained IConfiguration as an <see cref="IConfigurationSource"/>.
+    /// Represents a chained <see cref="IConfiguration"/> as an <see cref="IConfigurationSource"/>.
     /// </summary>
     public class ChainedConfigurationSource : IConfigurationSource
     {
