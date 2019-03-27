37c0e856ba279e204a2d96cf14d61a6227f9d37d
Brian Sullivan <briansul@microsoft.com>
2019-03-27 13:23:30 -0700

    Fix for issue dotnet/coreclr#23441 Added test case GitHub_23411
    Commit migrated from https://github.com/dotnet/coreclr/commit/cc04782de8f5a8f6eff0f8ff9e790c90edb3c68d

--- src/coreclr/src/jit/flowgraph.cpp
+++ src/coreclr/src/jit/flowgraph.cpp
@@ -1458,7 +1458,7 @@ void Compiler::fgChangeSwitchBlock(BasicBlock* oldSwitchBlock, BasicBlock* newSw
         // update those for the new block.
         if (m_switchDescMap->Lookup(oldSwitchBlock, &uniqueSuccSet))
         {
-            m_switchDescMap->Set(newSwitchBlock, uniqueSuccSet);
+            m_switchDescMap->Set(newSwitchBlock, uniqueSuccSet, BlockToSwitchDescMap::Overwrite);
         }
         else
         {
