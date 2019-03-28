eda66dab7d9e1d33840cd2860b571cdd3ae7cfe7
Egor Bogatov <egorbo@gmail.com>
2019-03-28 19:00:34 +0200

    [netcore] Improve xtest command (mono/mono#13703)
    The python script now also extracts downloaded zips and `make xtest-%` is now able to consume these artifacts.
    E.g.
    ```
    make xtest-System.Collections.Tests COREFX_ROOT=/prj/corefx-repo
    ```
    We still need COREFX_ROOT because of two missing files:
    `CoreFx.Private.TestUtilities.dll` and `xunit.console.deps.json`
    maybe I should just add them to our repo?
    
    Also this PR fixes `RemoteExecutorConsoleApp` problems for `make xtest-%`
    
    Commit migrated from https://github.com/mono/mono/commit/2522547cd5ba592e21fdba2e94a9feeb20037daf

--- src/mono/netcore/dl-test-assets.py
+++ src/mono/netcore/dl-test-assets.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
 import sys
+import os
 import subprocess
 import xml.etree.ElementTree as ET
+import zipfile
 
 if len(sys.argv) < 4:
     print("Usage: dl-test-assets.py <path to assets.xml> <base url> <output dir>")
@@ -17,9 +19,15 @@ root = tree.getroot()
 for elem in root:
     if elem.tag != "Blob":
         continue
-    print elem.attrib ["Id"]
-    res = subprocess.call (["wget", "-N", "-P", outdir, base_url + "/" + elem.attrib ["Id"]])
+    id = elem.attrib ["Id"]
+    filename = os.path.basename(id)
+    # System.IO.Compression.* have a lot of test files (>100mb)
+    if "System.IO.Compression" in filename:
+        continue
+    print filename
+    res = subprocess.call (["wget", "-N", "-P", outdir, base_url + "/" + id])
     if res != 0:
-        print ("Download failed.")
+        print ("Download failed for " + id)
         sys.exit (1)
-
+    with zipfile.ZipFile(outdir + "/" + filename) as zf:
+        zf.extractall(outdir + "/extracted/" + filename[:-4])
