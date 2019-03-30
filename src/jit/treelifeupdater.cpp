5ed0190aede95ffa6fb19460bb296cca6d1729af
Brian Bohe <brianbohe@gmail.com>
2019-03-29 17:23:35 -0700

    Moving VariableLiveRanges classes outside Compiler class
    Signed-off-by: Brian Bohe <brianbohe@gmail.com>

--- src/jit/treelifeupdater.cpp
+++ src/jit/treelifeupdater.cpp
@@ -248,7 +248,7 @@ void TreeLifeUpdater<ForCodeGen>::UpdateLifeVar(GenTree* tree)
 
 #ifdef USING_VARIABLE_LIVE_RANGE
             // For each of the LclVarDsc that are reporting change, variable or fields
-            compiler->getVariableLiveKeeper()->siStartOrCloseVariableLiveRanges(varDeltaSet, isBorn, isDying);
+            compiler->codeGen->getVariableLiveKeeper()->siStartOrCloseVariableLiveRanges(varDeltaSet, isBorn, isDying);
 #endif // USING_VARIABLE_LIVE_RANGE
 
 #ifdef USING_SCOPE_INFO
