afd165cb8c4b544a731c9c508b7095a9472fb698
Jan Kotas <jkotas@microsoft.com>
2019-04-02 14:41:49 -0700

    Avoid unnecessary SetLastError on PInvokes (dotnet/corefxdotnet/coreclr#36544)
    These console PInvokes are used in nearly every app. Omitting unnecessary SetLastError
    will save us from generating marshalling stub for them.
    
    Signed-off-by: dotnet-bot <dotnet-bot@microsoft.com>
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/aab338efb65808acc1ff9b7f95bd3dd5f0a6a3be

--- src/clr/src/System.Private.CoreLib/shared/Interop/Windows/Kernel32/Interop.HandleTypes.cs
+++ src/clr/src/System.Private.CoreLib/shared/Interop/Windows/Kernel32/Interop.HandleTypes.cs
@@ -2,14 +2,15 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Runtime.InteropServices;
-
 internal partial class Interop
 {
     internal partial class Kernel32
     {
-        [DllImport(Libraries.Kernel32)]
-        internal static extern IntPtr GetStdHandle(int nStdHandle);  // param is NOT a handle, but it returns one!
+        internal partial class HandleTypes
+        {
+            internal const int STD_INPUT_HANDLE = -10;
+            internal const int STD_OUTPUT_HANDLE = -11;
+            internal const int STD_ERROR_HANDLE = -12;
+        }
     }
 }
