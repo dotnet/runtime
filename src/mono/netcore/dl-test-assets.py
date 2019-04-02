adb8473866ffa8445ff3982eb38ad80cea83a136
Filip Navara <filip.navara@gmail.com>
2019-04-02 17:49:20 +0200

    [netcore] Cleanup + test fixes (mono/mono#13795)
    Avoid using `wget` since it's not installed on macOS by default.
    
    Commit migrated from https://github.com/mono/mono/commit/02be9aeba324089a46a10a1cecddf40826e71f64

--- src/mono/netcore/dl-test-assets.py
+++ src/mono/netcore/dl-test-assets.py
@@ -5,6 +5,8 @@ import os
 import subprocess
 import xml.etree.ElementTree as ET
 import zipfile
+import urllib
+import multiprocessing
 
 if len(sys.argv) < 4:
     print("Usage: dl-test-assets.py <path to assets.xml> <base url> <output dir>")
@@ -12,22 +14,29 @@ if len(sys.argv) < 4:
 
 infile_name = sys.argv [1]
 base_url = sys.argv [2]
-outdir = sys.argv [3];
+outdir = sys.argv [3]
 tree = ET.parse(infile_name)
 root = tree.getroot()
 
-for elem in root:
+if not os.path.exists(outdir):
+    os.makedirs(outdir)
+
+def downloadAsset(elem):
     if elem.tag != "Blob":
-        continue
+        return
     id = elem.attrib ["Id"]
     filename = os.path.basename(id)
-    # System.IO.Compression.* have a lot of test files (>100mb)
-    if "System.IO.Compression" in filename:
-        continue
-    print filename
-    res = subprocess.call (["wget", "-N", "-P", outdir, base_url + "/" + id])
-    if res != 0:
+    print "Downloading " + filename
+    try:
+        name, hdrs = urllib.urlretrieve(base_url + "/" + id, outdir + "/" + filename)
+    except IOError, e:
         print ("Download failed for " + id)
         sys.exit (1)
+    print "Extracting " + filename
     with zipfile.ZipFile(outdir + "/" + filename) as zf:
         zf.extractall(outdir + "/extracted/" + filename[:-4])
+
+pool = multiprocessing.Pool(multiprocessing.cpu_count())
+results = pool.map(downloadAsset, root)
+pool.close()
+pool.join()
