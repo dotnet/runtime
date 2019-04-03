38ea801c7ca623760c5f81bf558e037d684c3050
Sven Boemer <sbomer@gmail.com>
2019-04-03 09:25:05 -0700

    Enable native PDB copy on Windows for .NET Core
    Commit migrated from https://github.com/dotnet/linker/commit/dddf6631bb12698a4ed8f672e02d34892cbd72ce

--- src/tools/linker/test/Mono.Linker.Tests.Cases/Symbols/ReferenceWithPdbCopyActionAndSymbolLinkingEnabled.cs
+++ src/tools/linker/test/Mono.Linker.Tests.Cases/Symbols/ReferenceWithPdbCopyActionAndSymbolLinkingEnabled.cs
@@ -3,8 +3,12 @@ using Mono.Linker.Tests.Cases.Expectations.Metadata;
 using Mono.Linker.Tests.Cases.Symbols.Dependencies;
 
 namespace Mono.Linker.Tests.Cases.Symbols {
-#if NETCOREAPP
-	[IgnoreTestCase ("Needs investigation")]
+#if NETCOREAPP && !WIN32
+	// .NET Core type forwarders cause the assembly action to be
+	// changed from "copy" to "save" (to remove references to removed
+	// typeforwarders). However, saving the native PDB is only
+	// supported on windows.
+	[IgnoreTestCase ("Only supported on Windows on .NET Core.")]
 #endif
 	[Reference ("Dependencies/LibraryWithPdb/LibraryWithPdb.dll")]
 	[ReferenceDependency ("Dependencies/LibraryWithPdb/LibraryWithPdb.pdb")]
