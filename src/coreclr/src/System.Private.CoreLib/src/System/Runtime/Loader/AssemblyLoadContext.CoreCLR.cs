3aa8af7eb4f722a8d1604b3fd78ff073abd283c9
Steve MacLean <stmaclea@microsoft.com>
2019-04-11 00:20:39 -0400

    Contextual reflection (dotnet/coreclr#23740)
    * Add ContextualReflection APIs
    
    Add ContextualReflection APIs approved in dotnet/corefxdotnet/coreclr#36236
    Fix issue dotnet/coreclr#22213
    
    * SetParentAssembly even when IsCollectible()
    * ContextualReflection tests
    
    * PR Feedback
    * Add more usage tests
    
    Add using statement tests
    Add bad usage tests including Assert.Throws<>
    
    * Only initialize on set
    * Add XML API comments
    * Unify VerifyIsolation
    * Fix unused expectedAssembly
    * Remove ContextualReflectionScope throw
    * Clean up TestResolveMissingAssembly et. al
    * Remove unused QCall::AppDomainHandle
    * Remove AppDomainBaseObject
    * Pass AssemblyLoadContext as managed object to native
    * Fix AssemblyLoadContextBaseObject packing
    
    * AssemblyLoadContext backing stores
    
    Use explicit backing stores for events and properties
    
    * Remove StaticAsyncLocalCurrentContextualReflectionContext
    * Remove PermissionSetObject
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/206f9680303134731862a35338618f2cbd225651

--- src/coreclr/src/System.Private.CoreLib/src/System/Runtime/Loader/AssemblyLoadContext.CoreCLR.cs
+++ src/coreclr/src/System.Private.CoreLib/src/System/Runtime/Loader/AssemblyLoadContext.CoreCLR.cs
@@ -104,7 +104,7 @@ namespace System.Runtime.Loader
         {
             Assembly resolvedAssembly = null;
 
-            Func<AssemblyLoadContext, AssemblyName, Assembly> assemblyResolveHandler = Resolving;
+            Func<AssemblyLoadContext, AssemblyName, Assembly> assemblyResolveHandler = _resolving;
 
             if (assemblyResolveHandler != null)
             {
@@ -200,7 +200,7 @@ namespace System.Runtime.Loader
         {
             IntPtr resolvedDll = IntPtr.Zero;
 
-            Func<Assembly, string, IntPtr> dllResolveHandler = ResolvingUnmanagedDll;
+            Func<Assembly, string, IntPtr> dllResolveHandler = _resolvingUnmanagedDll;
 
             if (dllResolveHandler != null)
             {
