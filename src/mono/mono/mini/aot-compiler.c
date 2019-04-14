2146bfe433d41e698ecccd13b3e7a12b80663047
Jay Krell <jay.krell@cornell.edu>
2019-04-14 12:58:17 -0700

    Remove redundant check of llvm_acfg->aot_opts.direct_icalls. (mono/mono#14028)
    Commit migrated from https://github.com/mono/mono/commit/cfdcf29478ff3f3aeb634ed525fe00744020f845

--- src/mono/mono/mini/aot-compiler.c
+++ src/mono/mono/mini/aot-compiler.c
@@ -9355,7 +9355,7 @@ mono_aot_get_direct_call_symbol (MonoJumpInfoType type, gconstpointer data)
 		} else if (type == MONO_PATCH_INFO_JIT_ICALL) {
 			MonoJitICallInfo *info = mono_find_jit_icall_by_name ((const char*)data);
 			const char *name = mono_lookup_jit_icall_symbol ((const char*)data);
-			if (name && llvm_acfg->aot_opts.direct_icalls && info->func == info->wrapper)
+			if (name && info->func == info->wrapper)
 				sym = name;
 		}
 		if (sym)
