53b05964863f1669571d4b325026ae53b15a08f7
Sven Boemer <sbomer@gmail.com>
2019-04-15 13:13:27 -0700

    Remove jenkins jobs and supporting scripts
    This also adds a status badge for the .NET Core build.
    
    
    Commit migrated from https://github.com/dotnet/linker/commit/20363020e8d45d6721a9e5ad6ca36b32a3a20ce6

--- src/tools/linker/corebuild/integration/build.ps1
+++
@@ -1,9 +0,0 @@
-$tasksFolder="~\.nuget\packages\illink.tasks"
-If (Test-Path $tasksFolder) {
-  Remove-Item -r $tasksFolder
-}
-
-$dotNetTool = Join-Path $PSScriptRoot "..\..\eng\dotnet.ps1"
-# create integration packages
-& $dotNetTool restore (Join-Path $PSScriptRoot ".." ".." "illink.sln")
-& $dotNetTool pack (Join-Path $PSScriptRoot ".." ".." "illink.sln")
