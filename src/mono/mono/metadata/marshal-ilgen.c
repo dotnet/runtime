6d22ebbf82a5384e84724d2c0a4f5d34ef1c2a53
Zoltan Varga <vargaz@gmail.com>
2019-04-23 12:05:50 -0400

    [netcore] Throw the correct exception for byref returns returning null, .net core throws a nullref. (mono/mono#14128)
    Commit migrated from https://github.com/mono/mono/commit/c1c7a739cc5e83690350cf209e4c48e6b1e6cdf0

--- src/mono/mono/metadata/marshal-ilgen.c
+++ src/mono/mono/metadata/marshal-ilgen.c
@@ -1485,7 +1485,16 @@ handle_enum:
 	}
 
 	if (sig->ret->byref) {
+		int pos;
+
 		/* perform indirect load and return by value */
+#ifdef ENABLE_NETCORE
+		mono_mb_emit_byte (mb, CEE_DUP);
+		pos = mono_mb_emit_branch (mb, CEE_BRTRUE);
+		mono_mb_emit_exception_full (mb, "Mono", "NullByRefReturnException", NULL);
+		mono_mb_patch_branch (mb, pos);
+#endif
+
 		int ldind_op;
 		MonoType* ret_byval = m_class_get_byval_arg (mono_class_from_mono_type_internal (sig->ret));
 		g_assert (!ret_byval->byref);
