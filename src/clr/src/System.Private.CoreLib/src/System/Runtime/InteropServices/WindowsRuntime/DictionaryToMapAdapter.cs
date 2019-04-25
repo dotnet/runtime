c21941de3949d6df4179bec67e603ddc4b44c942
Anirudh Agnihotry <anirudhagnihotry098@gmail.com>
2019-04-25 11:47:22 -0700

    Nullable System.Collections.EmptyReadOnlyDictionaryInternal, System.Collections.ObjectModel.* (dotnet/coreclr#24221)
    * annotating collections
    
    * annotated collection.cs and readonlycollection.cs
    
    * adding tkey constraint and comments
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/ebb717431b79c6d6c83a32983b5819a13eb62cef

--- src/clr/src/System.Private.CoreLib/src/System/Runtime/InteropServices/WindowsRuntime/DictionaryToMapAdapter.cs
+++ src/clr/src/System.Private.CoreLib/src/System/Runtime/InteropServices/WindowsRuntime/DictionaryToMapAdapter.cs
@@ -26,7 +26,7 @@ namespace System.Runtime.InteropServices.WindowsRuntime
         }
 
         // V Lookup(K key)
-        internal V Lookup<K, V>(K key)
+        internal V Lookup<K, V>(K key) where K : object
         {
             IDictionary<K, V> _this = Unsafe.As<IDictionary<K, V>>(this);
             V value;
@@ -44,21 +44,21 @@ namespace System.Runtime.InteropServices.WindowsRuntime
         }
 
         // uint Size { get }
-        internal uint Size<K, V>()
+        internal uint Size<K, V>() where K : object
         {
             IDictionary<K, V> _this = Unsafe.As<IDictionary<K, V>>(this);
             return (uint)_this.Count;
         }
 
         // bool HasKey(K key)
-        internal bool HasKey<K, V>(K key)
+        internal bool HasKey<K, V>(K key) where K : object
         {
             IDictionary<K, V> _this = Unsafe.As<IDictionary<K, V>>(this);
             return _this.ContainsKey(key);
         }
 
         // IMapView<K, V> GetView()
-        internal IReadOnlyDictionary<K, V> GetView<K, V>()
+        internal IReadOnlyDictionary<K, V> GetView<K, V>() where K : object
         {
             IDictionary<K, V> _this = Unsafe.As<IDictionary<K, V>>(this);
             Debug.Assert(_this != null);
@@ -73,7 +73,7 @@ namespace System.Runtime.InteropServices.WindowsRuntime
         }
 
         // bool Insert(K key, V value)
-        internal bool Insert<K, V>(K key, V value)
+        internal bool Insert<K, V>(K key, V value) where K : object
         {
             IDictionary<K, V> _this = Unsafe.As<IDictionary<K, V>>(this);
             bool replacing = _this.ContainsKey(key);
@@ -82,7 +82,7 @@ namespace System.Runtime.InteropServices.WindowsRuntime
         }
 
         // void Remove(K key)
-        internal void Remove<K, V>(K key)
+        internal void Remove<K, V>(K key) where K : object
         {
             IDictionary<K, V> _this = Unsafe.As<IDictionary<K, V>>(this);
             bool removed = _this.Remove(key);
@@ -97,7 +97,7 @@ namespace System.Runtime.InteropServices.WindowsRuntime
         }
 
         // void Clear()
-        internal void Clear<K, V>()
+        internal void Clear<K, V>() where K : object
         {
             IDictionary<K, V> _this = Unsafe.As<IDictionary<K, V>>(this);
             _this.Clear();
