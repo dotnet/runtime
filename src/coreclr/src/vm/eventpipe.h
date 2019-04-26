9c200fc3b92017983aad8d2889a4e412fd45ba45
Brian Sullivan <briansul@microsoft.com>
2019-04-26 14:08:01 -0700

    Revert "Fix for IBC profile data on Linux - Build the full command line for FEATURE_PAL"
    This reverts commit dotnet/coreclr@08422af33fa2026953c2e720cecf000ed4345a32.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/1a30173f2fb4e6cf427dfbd962ab4f80a36583a3

--- src/coreclr/src/vm/eventpipe.h
+++ src/coreclr/src/vm/eventpipe.h
@@ -330,6 +330,9 @@ public:
     // Get the managed call stack for the specified thread.
     static bool WalkManagedStackForThread(Thread *pThread, StackContents &stackContents);
 
+    // Save the command line for the current process.
+    static void SaveCommandLine(LPCWSTR pwzAssemblyPath, int argc, LPCWSTR *argv);
+
     // Get next event.
     static EventPipeEventInstance *GetNextEvent();
 
@@ -396,6 +399,7 @@ private:
     static EventPipeBufferManager *s_pBufferManager;
     static EventPipeFile *s_pFile;
     static EventPipeEventSource *s_pEventSource;
+    static LPCWSTR s_pCommandLine;
     static HANDLE s_fileSwitchTimerHandle;
     static ULONGLONG s_lastFlushSwitchTime;
 };
