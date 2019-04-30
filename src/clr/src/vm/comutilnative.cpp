6c1c4eb053484244014d44bc35918a805afaf126
Jan Kotas <jkotas@microsoft.com>
2019-04-29 17:38:04 -0700

    Workaround memset alignment sensitivity (dotnet/coreclr#24302)
    * Workaround memset alignment sensitivity
    
    memset is up to 2x slower on misaligned block on some types of hardware. The problem is uneven performance of "rep stosb"
    used to implement the memset in some cases. The exact matrix on when it is slower and by how much is very complex.
    
    This change workarounds the issue by aligning the memory block before it is passed to memset and filling in the potential misaligned
    part manually. This workaround will regress performance by a few percent (<10%) in some cases, but we will gain up to 2x improvement
    in other cases.
    
    Fixes dotnet/coreclr#24300
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/3661584ffcdeac6f35fa9e2485796a482ebbf7b3

--- src/clr/src/vm/comutilnative.cpp
+++ src/clr/src/vm/comutilnative.cpp
@@ -815,6 +815,28 @@ void QCALLTYPE MemoryNative::Clear(void *dst, size_t length)
 {
     QCALL_CONTRACT;
 
+#if defined(_X86_) || defined(_AMD64_)
+    if (length > 0x100)
+    {
+        // memset ends up calling rep stosb if the hardware claims to support it efficiently. rep stosb is up to 2x slower
+        // on misaligned blocks. Workaround this issue by aligning the blocks passed to memset upfront.
+
+        *(uint64_t*)dst = 0;
+        *((uint64_t*)dst + 1) = 0;
+        *((uint64_t*)dst + 2) = 0;
+        *((uint64_t*)dst + 3) = 0;
+
+        void* end = (uint8_t*)dst + length;
+        *((uint64_t*)end - 1) = 0;
+        *((uint64_t*)end - 2) = 0;
+        *((uint64_t*)end - 3) = 0;
+        *((uint64_t*)end - 4) = 0;
+
+        dst = ALIGN_UP((uint8_t*)dst + 1, 32);
+        length = ALIGN_DOWN((uint8_t*)end - 1, 32) - (uint8_t*)dst;
+    }
+#endif
+
     memset(dst, 0, length);
 }
 
