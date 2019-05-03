40aeca5ec6b0344367ad887e94cd62fefed17dbd
Vitek Karas <vitek.karas@microsoft.com>
2019-05-03 13:52:39 -0700

    Add global registered install location to Linux and macOS (#6124)
    The product now reads the /etc/dotnet/install_location file and use its first line as the path to the install location.
    
    Adds test-only env. variables to be able to test the behavior around picking global install location.
    
    Changes windows-only tests to run on Linux/macOS as well now.
    
    Added some tests.
    Improved the test infra around global registered location.

--- src/test/HostActivationTests/Constants.cs
+++ src/test/HostActivationTests/Constants.cs
@@ -44,8 +44,10 @@ namespace Microsoft.DotNet.CoreSetup.Test.HostActivation
 
         public static class TestOnlyEnvironmentVariables
         {
+            public const string DefaultInstallPath = "_DOTNET_TEST_DEFAULT_INSTALL_PATH";
             public const string RegistryPath = "_DOTNET_TEST_REGISTRY_PATH";
             public const string GloballyRegisteredPath = "_DOTNET_TEST_GLOBALLY_REGISTERED_PATH";
+            public const string InstallLocationFilePath = "_DOTNET_TEST_INSTALL_LOCATION_FILE_PATH";
         }
     }
 }
