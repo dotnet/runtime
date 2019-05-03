360d41f4ff93b6507863cb324a247fd7380b4f79
Andrew Au <andrewau@microsoft.com>
2019-05-03 15:43:43 -0700

    AutoTrace - launch dotnet trace to capture trace as soon as the runtime is available to trace

---
+++ src/vm/autotrace.h
@@ -0,0 +1,11 @@
+#ifndef __AUTO_TRACE_H__
+#define __AUTO_TRACE_H__
+#ifdef FEATURE_AUTO_TRACE
+
+void auto_trace_init();
+void auto_trace_launch();
+void auto_trace_wait();
+void auto_trace_signal();
+
+#endif // FEATURE_AUTO_TRACE
+#endif // __AUTO_TRACE_H__
