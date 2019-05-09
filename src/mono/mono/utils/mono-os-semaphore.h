73078628ff45fb7cdbe7d3c6b58532a5ba416653
lateralusX <lateralusx.github@gmail.com>
2019-05-09 18:33:00 +0200

    Add log profiler support to Mono MSVC build.
    Several fixes to the log profiler in order to work on Windows.
    
    * Add new dynamic library project to build log profiler on Windows MSVC.
    * Fix dependecy between log profiler and Mono.
    * Rewrite command pipe logic on Windows since it's not supported to select
      on both sockets and pipes.
    * Several additional socket fixes needed for log profiler to work on Windows.
    
    
    Commit migrated from https://github.com/mono/mono/commit/2a703e72ff9001c9c872f1629ce0842e061a1c8a

--- src/mono/mono/utils/mono-os-semaphore.h
+++ src/mono/mono/utils/mono-os-semaphore.h
@@ -280,6 +280,8 @@ mono_os_sem_post (MonoSemType *sem)
 
 #else
 
+#include <mono/utils/mono-compiler.h>
+
 typedef HANDLE MonoSemType;
 
 static inline void
@@ -305,30 +307,8 @@ mono_os_sem_destroy (MonoSemType *sem)
 		g_error ("%s: CloseHandle failed with error %d", __func__, GetLastError ());
 }
 
-static inline MonoSemTimedwaitRet
-mono_os_sem_timedwait (MonoSemType *sem, guint32 timeout_ms, MonoSemFlags flags)
-{
-	BOOL res;
-
-retry:
-	res = mono_win32_wait_for_single_object_ex (*sem, timeout_ms, flags & MONO_SEM_FLAGS_ALERTABLE);
-	if (G_UNLIKELY (res != WAIT_OBJECT_0 && res != WAIT_IO_COMPLETION && res != WAIT_TIMEOUT))
-		g_error ("%s: mono_win32_wait_for_single_object_ex failed with error %d", __func__, GetLastError ());
-
-	if (res == WAIT_IO_COMPLETION && !(flags & MONO_SEM_FLAGS_ALERTABLE))
-		goto retry;
-
-	switch (res) {
-	case WAIT_OBJECT_0:
-		return MONO_SEM_TIMEDWAIT_RET_SUCCESS;
-	case WAIT_IO_COMPLETION:
-		return MONO_SEM_TIMEDWAIT_RET_ALERTED;
-	case WAIT_TIMEOUT:
-		return MONO_SEM_TIMEDWAIT_RET_TIMEDOUT;
-	default:
-		g_assert_not_reached ();
-	}
-}
+MONO_PROFILER_API MonoSemTimedwaitRet
+mono_os_sem_timedwait (MonoSemType *sem, guint32 timeout_ms, MonoSemFlags flags);
 
 static inline int
 mono_os_sem_wait (MonoSemType *sem, MonoSemFlags flags)
