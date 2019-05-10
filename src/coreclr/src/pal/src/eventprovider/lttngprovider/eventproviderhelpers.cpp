54d3b8438a7a4921104e954abe325ba2bcd5ce6f
Jeremy Koritzinsky <jekoritz@microsoft.com>
2019-05-10 16:24:42 -0700

    Move EventProvider native layout to be driven by CMake configure (dotnet/coreclr#24478)
    * Generate eventpipe implementation as part of CMake configure.
    
    * Generate Etw provider as part of CMake configure.
    
    * First pass porting over lttng provider to cmake.
    
    * Fix up CMake Lttng provider generation.
    
    * Move Lttng provider into CMake tree.
    
    * Move dummy event provider to CMake
    
    * Move genEventing into the CMake tree.
    
    * Remove extraneous logging and unused python locator.
    
    * Clean up build.sh
    
    * Clean up genEventingTests.py
    
    * Add dependencies to enable more incremental builds (providers not fully incremental).
    
    * Convert to custom command and targets instead of at configure time.
    
    * Get each eventing target to incrementally build.
    
    * Fix incremental builds
    
    * Add missing dependencies on eventing headers.
    
    * PR Feedback. Mark all generated files as generated
    
    * Clean up eventprovider test CMakeLists
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/e985b20260631c98a09f08cb67e93d690a6ffd0b

---
+++ src/coreclr/src/pal/src/eventprovider/lttngprovider/eventproviderhelpers.cpp
@@ -0,0 +1,81 @@
+#include "palrt.h"
+#include "pal.h"
+#include "stdlib.h"
+#include "pal_mstypes.h"
+#include "pal_error.h"
+#include <new>
+#include <memory.h>
+
+#define wcslen PAL_wcslen
+
+bool ResizeBuffer(char *&buffer, size_t& size, size_t currLen, size_t newSize, bool &fixedBuffer)
+{
+    newSize = (size_t)(newSize * 1.5);
+    _ASSERTE(newSize > size); // check for overflow
+
+    if (newSize < 32)
+        newSize = 32;
+
+    // We can't use coreclr includes here so we use std::nothrow
+    // rather than the coreclr version
+    char *newBuffer = new (std::nothrow) char[newSize];
+
+    if (newBuffer == NULL)
+        return false;
+
+    memcpy(newBuffer, buffer, currLen);
+
+    if (!fixedBuffer)
+        delete[] buffer;
+
+    buffer = newBuffer;
+    size = newSize;
+    fixedBuffer = false;
+
+    return true;
+}
+
+bool WriteToBuffer(const BYTE *src, size_t len, char *&buffer, size_t& offset, size_t& size, bool &fixedBuffer)
+{
+    if(!src) return true;
+    if (offset + len > size)
+    {
+        if (!ResizeBuffer(buffer, size, offset, size + len, fixedBuffer))
+            return false;
+    }
+
+    memcpy(buffer + offset, src, len);
+    offset += len;
+    return true;
+}
+
+bool WriteToBuffer(PCWSTR str, char *&buffer, size_t& offset, size_t& size, bool &fixedBuffer)
+{
+    if(!str) return true;
+    size_t byteCount = (wcslen(str) + 1) * sizeof(*str);
+
+    if (offset + byteCount > size)
+    {
+        if (!ResizeBuffer(buffer, size, offset, size + byteCount, fixedBuffer))
+            return false;
+    }
+
+    memcpy(buffer + offset, str, byteCount);
+    offset += byteCount;
+    return true;
+}
+
+bool WriteToBuffer(const char *str, char *&buffer, size_t& offset, size_t& size, bool &fixedBuffer)
+{
+    if(!str) return true;
+    size_t len = strlen(str) + 1;
+    if (offset + len > size)
+    {
+        if (!ResizeBuffer(buffer, size, offset, size + len, fixedBuffer))
+            return false;
+    }
+
+    memcpy(buffer + offset, str, len);
+    offset += len;
+    return true;
+}
