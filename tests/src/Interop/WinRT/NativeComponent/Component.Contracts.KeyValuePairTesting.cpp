6a2c6a4735bcec975ff94ea016ea44f7d592b918
Jeremy Koritzinsky <jekoritz@microsoft.com>
2019-05-16 17:40:23 -0700

    Add Managed->Native tests for our WinRT primitives (#23529)
    * First pass generating the contracts for the WinRT tests. Managed ones are manually written, Native are generated via C++/WinRT.
    
    * Don't overwrite the public implementation files when we run cppwinrt
    
    * Don't output a MIDL-processed header.
    
    * Add default constructor for BindingViewModel.
    
    * Partial implementation of most of the native winrt component.
    
    * Finish implementation of native winrt component.
    
    * Get native component building correctly (cppwinrt doesn't include wrappers for the "Windows::UI::Xaml::Interop::IBindable*" collection types.
    
    * Add WinRT primitive marshalling tests.
    
    * Add testing for projected types used for binding.
    
    * Add license headers to native files.
    
    * Disable WinRT tests on non-WinRT platforms (detection copied from CoreFX).
    
    * Use WINDOWS_SDK_VERSION variable in all locations.
    
    * Use Windows SDK version determined by CMake in WinRT build.
    
    * Resolve WinMDs via globs so the build can roll between different Windows SDK versions that have required APIs seamlessly.
    
    * Add logging of cppwinrt version.
    
    * Try to construct path to cppwinrt when finding it
    
    * Just directly construct the cppwinrt path.
    
    * Remove -prefix flag from cppwinrt invocation
    
    * PR feedback.
    
    * Fix syntax in BindableVectorWrapper.
    
    * Disable winrt binding test on Nano Server.
    
    * Add enum testing. Clean up WinRT tests to hopefully build on CI (or at least fail at a later point).
    
    * Add some more logging to try to determine why an older SDK version is being picked.
    
    * Try to define CMAKE_SYSTEM_VERSION and see if that selects the correct SDK version (it seems to work on the build.cmd script)
    
    * Clean up WinRT CMake now that it builds on AzDO CI.
    
    * Disable WinRT binding test on pre-Win10V1809 systems.

---
+++ tests/src/Interop/WinRT/NativeComponent/Component.Contracts.KeyValuePairTesting.cpp
@@ -0,0 +1,48 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+#include "pch.h"
+#include "Component.Contracts.KeyValuePairTesting.h"
+#include <utility>
+#include <vector>
+
+template<typename K, typename V>
+struct pair_wrapper : winrt::implements<pair_wrapper<K, V>, winrt::Windows::Foundation::Collections::IKeyValuePair<K, V>>
+{
+    pair_wrapper(const K& key, const V& value)
+        :key{ key },
+        value{ value }
+    {}
+
+    K Key()
+    {
+        return key;
+    }
+
+    V Value()
+    {
+        return value;
+    }
+
+private:
+    K key;
+    V value;
+};
+
+namespace winrt::Component::Contracts::implementation
+{
+    Windows::Foundation::Collections::IKeyValuePair<int32_t, int32_t> KeyValuePairTesting::MakeSimplePair(int32_t key, int32_t value)
+    {
+        return make<pair_wrapper<int32_t, int32_t>>(key, value);
+    }
+
+    Windows::Foundation::Collections::IKeyValuePair<hstring, hstring> KeyValuePairTesting::MakeMarshaledPair(hstring const& key, hstring const& value)
+    {
+        return make<pair_wrapper<hstring, hstring>>(key, value);
+    }
+
+    Windows::Foundation::Collections::IKeyValuePair<int32_t, Windows::Foundation::Collections::IIterable<int32_t>> KeyValuePairTesting::MakeProjectedPair(int32_t key, array_view<int32_t const> values)
+    {
+        return pair_wrapper<int32_t, Windows::Foundation::Collections::IIterable<int32_t>>{key, winrt::single_threaded_vector(std::vector<int32_t>(values.begin(), values.end()))};
+    }
+}
---
+++ tests/src/Interop/WinRT/NativeComponent/Component.Contracts.UriTesting.h
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+#pragma once
+
+#include "Component/Contracts/UriTesting.g.h"
+
+namespace winrt::Component::Contracts::implementation
+{
+    struct UriTesting : UriTestingT<UriTesting>
+    {
+        UriTesting() = default;
+
+        hstring GetFromUri(Windows::Foundation::Uri const& uri);
+        Windows::Foundation::Uri CreateUriFromString(hstring const& uri);
+    };
+}
+
+namespace winrt::Component::Contracts::factory_implementation
+{
+    struct UriTesting : UriTestingT<UriTesting, implementation::UriTesting>
+    {
+    };
+}
