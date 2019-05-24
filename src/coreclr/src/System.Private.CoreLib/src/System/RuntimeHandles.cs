7b2057a513b3ae5ec23c0845c45feb287377cf18
Santiago Fernandez Madero <safern@microsoft.com>
2019-05-24 11:45:29 -0700

    Fix nullable annotations in RuntimeTypeHandle operators
    Commit migrated from https://github.com/dotnet/coreclr/commit/7a60974599a3ca78e354b391afc23b56af294008

--- src/coreclr/src/System.Private.CoreLib/src/System/RuntimeHandles.cs
+++ src/coreclr/src/System.Private.CoreLib/src/System/RuntimeHandles.cs
@@ -70,11 +70,11 @@ namespace System
 
         public static bool operator ==(RuntimeTypeHandle left, object right) { return left.Equals(right); }
 
-        public static bool operator ==(object left, RuntimeTypeHandle right) { return right.Equals(left); }
+        public static bool operator ==(object? left, RuntimeTypeHandle right) { return right.Equals(left); }
 
         public static bool operator !=(RuntimeTypeHandle left, object right) { return !left.Equals(right); }
 
-        public static bool operator !=(object left, RuntimeTypeHandle right) { return !right.Equals(left); }
+        public static bool operator !=(object? left, RuntimeTypeHandle right) { return !right.Equals(left); }
 
 
         // This is the RuntimeType for the type
