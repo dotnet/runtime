668eb5f2ef40d412409e93852af23d268a1f642b
Tamar Christina <48126768+TamarChristinaArm@users.noreply.github.com>
2019-05-25 00:26:18 +0100

    Arm64: Take more bounds checks into consideration when inlining immediates (dotnet/coreclr#24762)
    * Arm64: Take GT_ARR_BOUNDS_CHECK to consideration for inlining immediate in cmp
    
    * Arm64: Add SIMD and HW_INTRINSIC to IsContainableImmed
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/c6b3b61ae4d9ec1f928f02d4272135bca8d75f52

--- src/coreclr/src/jit/lowerarmarch.cpp
+++ src/coreclr/src/jit/lowerarmarch.cpp
@@ -92,6 +92,9 @@ bool Lowering::IsContainableImmed(GenTree* parentNode, GenTree* childNode)
             case GT_LE:
             case GT_GE:
             case GT_GT:
+            case GT_ARR_BOUNDS_CHECK:
+            case GT_SIMD_CHK:
+            case GT_HW_INTRINSIC_CHK:
                 return emitter::emitIns_valid_imm_for_cmp(immVal, size);
             case GT_AND:
             case GT_OR:
