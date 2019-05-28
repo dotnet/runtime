3489e56c875c6144c729b51063300c7d60b1ae31
Jan Vorlicek <janvorli@microsoft.com>
2019-05-28 18:06:38 +0200

    Fix initial thread affinity on Linux (#24801)
    * Fix initial thread affinity on Linux
    
    On Linux, a new thread inherits the affinity mask of the thread
    that created the new thread. This is a problem for background GC
    threads that are created by one of the server GC threads that are
    affinitized to single core.
    This change adds resetting each new thread affinity to match the
    current process affinity.
    In addition to that, I've also fixed the extraction of the CPU count
    that was using PID 0. While the doc says that 0 represents current process,
    it in fact means current thread.
    And as a small bonus, I've added caching of the value returned by
    the PAL_GetLogicalCpuCountFromOS, since it cannot change during runtime.

--- src/pal/src/configure.cmake
+++ src/pal/src/configure.cmake
@@ -101,6 +101,7 @@ check_library_exists(${PTHREAD_LIBRARY} pthread_getattr_np "" HAVE_PTHREAD_GETAT
 check_library_exists(${PTHREAD_LIBRARY} pthread_getcpuclockid "" HAVE_PTHREAD_GETCPUCLOCKID)
 check_library_exists(${PTHREAD_LIBRARY} pthread_sigqueue "" HAVE_PTHREAD_SIGQUEUE)
 check_library_exists(${PTHREAD_LIBRARY} pthread_getaffinity_np "" HAVE_PTHREAD_GETAFFINITY_NP)
+check_library_exists(${PTHREAD_LIBRARY} pthread_attr_setaffinity_np "" HAVE_PTHREAD_ATTR_SETAFFINITY_NP)
 
 check_function_exists(sigreturn HAVE_SIGRETURN)
 check_function_exists(_thread_sys_sigreturn HAVE__THREAD_SYS_SIGRETURN)
