7fbd4f96e6119b9176301eba6d62ee69f8ad3b1f
Vitek Karas <vitek.karas@microsoft.com>
2019-05-28 21:43:54 -0700

    Improve apphost missing fxr error message with self registered location (dotnet/core-setup#6546)
    With the recent changes apphost now looks for shared framework in 3 potential places: the default global location, the globally registered location and the DOTNET_ROOT path.
    The error message didn't mention the globally registered location, so add that.
    
    Required a bit of refactoring in the pals to avoid code duplication.
    
    Commit migrated from https://github.com/dotnet/core-setup/commit/832f8250b5986028594c0db47b4e1cb62915c15b

--- src/setup/src/corehost/common/pal.unix.cpp
+++ src/setup/src/corehost/common/pal.unix.cpp
@@ -299,6 +299,20 @@ bool pal::get_global_dotnet_dirs(std::vector<pal::string_t>* recv)
     return false;
 }
 
+bool pal::get_dotnet_self_registered_config_location(pal::string_t* recv)
+{
+    *recv = _X("/etc/dotnet/install_location");
+
+    //  ***Used only for testing***
+    pal::string_t environment_install_location_override;
+    if (test_only_getenv(_X("_DOTNET_TEST_INSTALL_LOCATION_FILE_PATH"), &environment_install_location_override))
+    {
+        *recv = environment_install_location_override;
+    }
+
+    return true;
+}
+
 bool pal::get_dotnet_self_registered_dir(pal::string_t* recv)
 {
     recv->clear();
@@ -312,13 +326,10 @@ bool pal::get_dotnet_self_registered_dir(pal::string_t* recv)
     }
     //  ***************************
 
-    pal::string_t install_location_file_path = _X("/etc/dotnet/install_location");
-
-    //  ***Used only for testing***
-    pal::string_t environment_install_location_override;
-    if (test_only_getenv(_X("_DOTNET_TEST_INSTALL_LOCATION_FILE_PATH"), &environment_install_location_override))
+    pal::string_t install_location_file_path;
+    if (!get_dotnet_self_registered_config_location(&install_location_file_path))
     {
-        install_location_file_path = environment_install_location_override;
+        return false;
     }
     //  ***************************
 
