82b6123a71426aa80b87eb8f345cf6bca80b7678
Jan Vorlicek <janvorli@microsoft.com>
2019-05-30 22:07:06 +0200

    Add build-tools.cmd (dotnet/coreclr#24817)
    * Add build-tools.cmd
    
    This script builds selected tools in the src/tools directory (for the
    time being, it builds just the runincontext tool). This is a preparation
    step to enable nightly runs with unloadability tests. The
    build-tools.cmd should build helper tools needed during test runs.
    
    * Remove the build-tools.cmd
    
    * Build the tools as part of the managed runtime build.
    * Update the R2RDump project to put binaries into the bin/Product/...
    
    * Update runincontext.cmd/sh scripts
    
    Reflect the new location of the runincontext binary
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/7e17986ff71a70ba328895e0b99e1de83d1bf5b5

--- src/coreclr/tests/scripts/runincontext.sh
+++ src/coreclr/tests/scripts/runincontext.sh
@@ -14,4 +14,4 @@
 # 3. - n. Additional arguments that were passed to the test .sh
 
 export CORE_LIBRARIES=$1
-$_DebuggerFullPath "$CORE_ROOT/corerun" "$CORE_ROOT/../../runincontext/runincontext/runincontext.exe" $RunInContextExtraArgs /referencespath:$CORE_ROOT/ $1$2 $3 $4 $5 $6 $7 $8 $9
+$_DebuggerFullPath "$CORE_ROOT/corerun" "$CORE_ROOT/runincontext.dll" $RunInContextExtraArgs /referencespath:$CORE_ROOT/ $1$2 $3 $4 $5 $6 $7 $8 $9
