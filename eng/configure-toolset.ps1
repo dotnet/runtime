a9509fc30f9351f0339395b109135347cdf84ef9
Juan Hoyos <juan.hoyos@microsoft.com>
2019-05-31 16:04:38 -0700

    Add workaround for native tool installation CI hang (#24899)

--- eng/configure-toolset.ps1
+++ eng/configure-toolset.ps1
@@ -7,3 +7,6 @@ $script:useInstalledDotNetCli = $false
 # Always use the local repo packages directory instead of
 # the user's NuGet cache
 $script:useGlobalNuGetCache = $false
+
+# Working around issue https://github.com/dotnet/arcade/issues/2673
+$script:DisableNativeToolsetInstalls = $true
