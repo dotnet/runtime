986aa1eae242cd6d8bb99e551b8c5e344903ada0
Gleb Balykov <g.balykov@samsung.com>
2019-06-03 19:29:39 +0300

    Fix armel tests build (dotnet/coreclr#24678)
    * Fix build-tests script for cross build and fix tests for armel
    
    * Handle case of -skipcrossgen for cross builds
    
    * Fix build error in case of cross builds when ROOTFS_DIR is not passed explicitly
    
    * Fix portable build parameter setup for armel tests build
    
    * Move force of non-portable build for armel from build.sh/build-test.sh to init-distro-rid.sh
    
    * Fix priority1 tests build for armel
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/2e28c8114d58870ebc1ae8aac51b4539d2314452

--- src/coreclr/build.sh
+++ src/coreclr/build.sh
@@ -441,12 +441,18 @@ build_CoreLib()
         exit 1
     fi
 
+    local __CoreLibILDir=$__BinDir/IL
+
     if [ $__SkipCrossgen == 1 ]; then
         echo "Skipping generating native image"
+
+        if [ $__CrossBuild == 1 ]; then
+            # Crossgen not performed, so treat the IL version as the final version
+            cp $__CoreLibILDir/System.Private.CoreLib.dll $__BinDir/System.Private.CoreLib.dll
+        fi
+
         return
     fi
-
-    local __CoreLibILDir=$__BinDir/IL
 
     # The cross build generates a crossgen with the target architecture.
     if [ $__CrossBuild == 0 ]; then
@@ -986,15 +992,6 @@ if [[ $__ClangMajorVersion == 0 && $__ClangMinorVersion == 0 ]]; then
     fi
 fi
 
-if [[ "$__BuildArch" == "armel" ]]; then
-    # Armel cross build is Tizen specific and does not support Portable RID build
-    __PortableBuild=0
-fi
-
-if [ $__PortableBuild == 0 ]; then
-    __CommonMSBuildArgs="$__CommonMSBuildArgs /p:PortableBuild=false"
-fi
-
 # Set dependent variables
 __LogsDir="$__RootBinDir/Logs"
 __MsbuildDebugLogsDir="$__LogsDir/MsbuildDebugLogs"
@@ -1012,8 +1009,20 @@ if [ $__CrossBuild == 1 ]; then
 fi
 __CrossGenCoreLibLog="$__LogsDir/CrossgenCoreLib_$__BuildOS.$__BuildArch.$__BuildType.log"
 
+# Configure environment if we are doing a cross compile.
+if [ $__CrossBuild == 1 ]; then
+    export CROSSCOMPILE=1
+    if ! [[ -n "$ROOTFS_DIR" ]]; then
+        export ROOTFS_DIR="$__ProjectRoot/cross/rootfs/$__BuildArch"
+    fi
+fi
+
 # init the target distro name
 initTargetDistroRid
+
+if [ $__PortableBuild == 0 ]; then
+    __CommonMSBuildArgs="$__CommonMSBuildArgs /p:PortableBuild=false"
+fi
 
 # Init if MSBuild for .NET Core is supported for this platform
 isMSBuildOnNETCoreSupported
@@ -1031,14 +1040,6 @@ fi
 # Specify path to be set for CMAKE_INSTALL_PREFIX.
 # This is where all built CoreClr libraries will copied to.
 export __CMakeBinDir="$__BinDir"
-
-# Configure environment if we are doing a cross compile.
-if [ $__CrossBuild == 1 ]; then
-    export CROSSCOMPILE=1
-    if ! [[ -n "$ROOTFS_DIR" ]]; then
-        export ROOTFS_DIR="$__ProjectRoot/cross/rootfs/$__BuildArch"
-    fi
-fi
 
 # Make the directories necessary for build if they don't exist
 setup_dirs
