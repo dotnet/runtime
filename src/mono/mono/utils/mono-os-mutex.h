a51810a075de33b5f5602bb6ffdb2afa959b41ce
Aleksey Kliger <alklig@microsoft.com>
2019-06-04 18:22:10 -0400

    [runtime] Autoconf check for pthread_mutexattr_setprotocol
    Android doesn't always declare that function.
    
    
    Commit migrated from https://github.com/mono/mono/commit/be92baf12d33c95b46075f48f69c0274ae8301d9

--- src/mono/mono/utils/mono-os-mutex.h
+++ src/mono/mono/utils/mono-os-mutex.h
@@ -57,7 +57,7 @@ mono_os_mutex_init_type (mono_mutex_t *mutex, int type)
 	if (G_UNLIKELY (res != 0))
 		g_error ("%s: pthread_mutexattr_settype failed with \"%s\" (%d)", __func__, g_strerror (res), res);
 
-#ifdef PTHREAD_PRIO_INHERIT
+#if defined (PTHREAD_PRIO_INHERIT) && HAVE_DECL_PTHREAD_MUTEXATTR_SETPROTOCOL
 	/* use PTHREAD_PRIO_INHERIT if possible */
 	res = pthread_mutexattr_setprotocol (&attr, PTHREAD_PRIO_INHERIT);
 	if (G_UNLIKELY (res != 0 && res != ENOTSUP))
