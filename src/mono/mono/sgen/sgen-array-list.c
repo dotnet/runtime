cb1434ff0f8a54dd29fab728b90c6b6d720d1662
Rich Lander <rlander@microsoft.com>
2019-06-03 21:23:38 -0700

    Update license to match directory
    Commit migrated from https://github.com/mono/mono/commit/f0ef7aa4dfe494b885d344f9a83fc322f609fb70

--- src/mono/mono/sgen/sgen-array-list.c
+++ src/mono/mono/sgen/sgen-array-list.c
@@ -4,18 +4,7 @@
  *
  * Copyright (C) 2016 Xamarin Inc
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License 2.0 as published by the Free Software Foundation;
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
- *
- * You should have received a copy of the GNU Library General Public
- * License 2.0 along with this library; if not, write to the Free
- * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * Licensed under the MIT license. See LICENSE file in the project root for full license information.
  */
 
 #ifdef HAVE_SGEN_GC
