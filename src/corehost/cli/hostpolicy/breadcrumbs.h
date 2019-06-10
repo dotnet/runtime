300aa77605171e5913c36423cfbe43a61d840318
Steve MacLean <Steve.MacLean@Microsoft.com>
2019-06-10 17:40:16 -0400

    Revise breadcrumb_writer_t ownership
    Make breadcrumb_writer_t own m_files
    Manage breadcrumb_writer_t with a shared pointer
    Prevent premature destruction by adding a shared pointer for the background thread

--- src/corehost/cli/hostpolicy/breadcrumbs.h
+++ src/corehost/cli/hostpolicy/breadcrumbs.h
@@ -10,21 +10,18 @@
 class breadcrumb_writer_t
 {
 public:
-    breadcrumb_writer_t(bool enabled, const std::unordered_set<pal::string_t> &files);
-    ~breadcrumb_writer_t();
-
-    void begin_write();
+    breadcrumb_writer_t(std::unordered_set<pal::string_t> &files);
+    static std::shared_ptr<breadcrumb_writer_t> begin_write(std::unordered_set<pal::string_t> &files);
+    void end_write();
 
 private:
     void write_callback();
-    bool end_write();
     static void write_worker_callback(breadcrumb_writer_t* p_this);
 
+    std::shared_ptr<breadcrumb_writer_t> m_threads_instance;
     pal::string_t m_breadcrumb_store;
     std::thread m_thread;
-    const std::unordered_set<pal::string_t> &m_files;
-    bool m_enabled;
-    volatile bool m_status;
+    std::unordered_set<pal::string_t> m_files;
 };
 
 #endif // __BREADCRUMBS_H__
