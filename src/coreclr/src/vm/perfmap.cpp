b0daee58d37d64369adebd3350ee1e5683032b1b
Koundinya Veluri <kouvel@users.noreply.github.com>
2019-06-10 23:27:02 -0700

    Add optimization tiers to the Linux perf maps for perfcollect (dotnet/coreclr#24967)
    Add optimization tiers to the Linux perf maps for perfcollect
    
    Fixes https://github.com/dotnet/coreclr/issues/23222:
    - It looks like module unloads are currently not taken into account. Once they would be taken into account, Although we have method JIT events from `lttng` with the code address and optimization tier, samples can only be associated with method JIT events by associating the time range when the module is loaded with times of samples, and the event times from `lttng` would not necessarily correspond with times from samples taken by `perf`.
    - Updated to include the optimization tier in the perf map for each jitted or R2R method code address
    - Refactored common code between eventtrace and perfmap for getting jit tiers
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/d5906ecce405c8ff9476a1ea3d9a297bf5d9991c

--- src/coreclr/src/vm/perfmap.cpp
+++ src/coreclr/src/vm/perfmap.cpp
@@ -23,6 +23,7 @@
 #endif
 
 PerfMap * PerfMap::s_Current = nullptr;
+bool PerfMap::s_ShowOptimizationTiers = false;
 
 // Initialize the map for the process - called from EEStartupHelper.
 void PerfMap::Initialize()
@@ -43,6 +44,11 @@ void PerfMap::Initialize()
         if (signalNum > 0)
         {
             PAL_IgnoreProfileSignal(signalNum);
+        }
+
+        if (CLRConfig::GetConfigValue(CLRConfig::EXTERNAL_PerfMapShowOptimizationTiers) != 0)
+        {
+            s_ShowOptimizationTiers = true;
         }
     }
 }
@@ -156,7 +162,7 @@ void PerfMap::WriteLine(SString& line)
 }
 
 // Log a method to the map.
-void PerfMap::LogMethod(MethodDesc * pMethod, PCODE pCode, size_t codeSize)
+void PerfMap::LogMethod(MethodDesc * pMethod, PCODE pCode, size_t codeSize, const char *optimizationTier)
 {
     CONTRACTL{
         THROWS;
@@ -183,7 +189,15 @@ void PerfMap::LogMethod(MethodDesc * pMethod, PCODE pCode, size_t codeSize)
         // Build the map file line.
         StackScratchBuffer scratch;
         SString line;
-        line.Printf(FMT_CODE_ADDR " %x %s\n", pCode, codeSize, fullMethodSignature.GetANSI(scratch));
+        line.Printf(FMT_CODE_ADDR " %x %s", pCode, codeSize, fullMethodSignature.GetANSI(scratch));
+        if (optimizationTier != nullptr && s_ShowOptimizationTiers)
+        {
+            line.AppendPrintf("[%s]\n", optimizationTier);
+        }
+        else
+        {
+            line.Append(W('\n'));
+        }
 
         // Write the line.
         WriteLine(line);
@@ -229,14 +243,24 @@ void PerfMap::LogImage(PEFile * pFile)
 
 
 // Log a method to the map.
-void PerfMap::LogJITCompiledMethod(MethodDesc * pMethod, PCODE pCode, size_t codeSize)
+void PerfMap::LogJITCompiledMethod(MethodDesc * pMethod, PCODE pCode, size_t codeSize, PrepareCodeConfig *pConfig)
 {
     LIMITED_METHOD_CONTRACT;
 
-    if (s_Current != nullptr)
+    if (s_Current == nullptr)
     {
-        s_Current->LogMethod(pMethod, pCode, codeSize);
+        return;
     }
+
+    const char *optimizationTier = nullptr;
+#ifndef CROSSGEN_COMPILE
+    if (s_ShowOptimizationTiers)
+    {
+        optimizationTier = PrepareCodeConfig::GetJitOptimizationTierStr(pConfig, pMethod);
+    }
+#endif // CROSSGEN_COMPILE
+
+    s_Current->LogMethod(pMethod, pCode, codeSize, optimizationTier);
 }
 
 // Log a set of stub to the map.
@@ -333,7 +357,7 @@ void NativeImagePerfMap::LogDataForModule(Module * pModule)
             MethodDesc *hotDesc = mi.GetMethodDesc();
             hotDesc->CheckRestore();
 
-            LogPreCompiledMethod(hotDesc, mi.GetMethodStartAddress(), baseAddr);
+            LogPreCompiledMethod(hotDesc, mi.GetMethodStartAddress(), baseAddr, nullptr);
         }
         return;
     }
@@ -344,12 +368,12 @@ void NativeImagePerfMap::LogDataForModule(Module * pModule)
     {
         MethodDesc* hotDesc = mi.GetMethodDesc();
 
-        LogPreCompiledMethod(hotDesc, mi.GetMethodStartAddress(), baseAddr);
+        LogPreCompiledMethod(hotDesc, mi.GetMethodStartAddress(), baseAddr, "ReadyToRun");
     }
 }
 
 // Log a pre-compiled method to the perfmap.
-void NativeImagePerfMap::LogPreCompiledMethod(MethodDesc * pMethod, PCODE pCode, SIZE_T baseAddr)
+void NativeImagePerfMap::LogPreCompiledMethod(MethodDesc * pMethod, PCODE pCode, SIZE_T baseAddr, const char *optimizationTier)
 {
     STANDARD_VM_CONTRACT;
 
@@ -364,12 +388,12 @@ void NativeImagePerfMap::LogPreCompiledMethod(MethodDesc * pMethod, PCODE pCode,
     // Emit an entry for each section if it is used.
     if (methodRegionInfo.hotSize > 0)
     {
-        LogMethod(pMethod, (PCODE)methodRegionInfo.hotStartAddress - baseAddr, methodRegionInfo.hotSize);
+        LogMethod(pMethod, (PCODE)methodRegionInfo.hotStartAddress - baseAddr, methodRegionInfo.hotSize, optimizationTier);
     }
 
     if (methodRegionInfo.coldSize > 0)
     {
-        LogMethod(pMethod, (PCODE)methodRegionInfo.coldStartAddress - baseAddr, methodRegionInfo.coldSize);
+        LogMethod(pMethod, (PCODE)methodRegionInfo.coldStartAddress - baseAddr, methodRegionInfo.coldSize, optimizationTier);
     }
 }
 
