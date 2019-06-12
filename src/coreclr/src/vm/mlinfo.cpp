bcfa7513d6ac22e154bb7904cca10f92a40fa0ec
Jeremy Koritzinsky <jekoritz@microsoft.com>
2019-06-12 10:16:42 -0700

    Allocate handles for custom marshalers in ALCs instead of in the AppDomain. (dotnet/coreclr#25105)
    Commit migrated from https://github.com/dotnet/coreclr/commit/a8b4c8f67a2d56587c56930b5483335f7ddab3a2

--- src/coreclr/src/vm/mlinfo.cpp
+++ src/coreclr/src/vm/mlinfo.cpp
@@ -4277,9 +4277,8 @@ VOID MarshalInfo::MarshalTypeToString(SString& strMarshalType, BOOL fSizeIsSpeci
     {
         GCX_COOP();
         
-        OBJECTHANDLE objHandle = m_pCMHelper->GetCustomMarshalerInfo()->GetCustomMarshaler();
+        OBJECTREF pObjRef = m_pCMHelper->GetCustomMarshalerInfo()->GetCustomMarshaler();
         {
-            OBJECTREF pObjRef = ObjectFromHandle(objHandle);
             DefineFullyQualifiedNameForClassW();
 
             strMarshalType.Printf(W("custom marshaler (%s)"),
