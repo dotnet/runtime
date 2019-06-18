85d49105ce2667763326c5bf5520553a447fa541
Fadi Hanna <fadim@microsoft.com>
2019-06-17 19:36:24 -0700

    Allow PEImageLayout to be loadable for R2R images loaded from streams. (dotnet/coreclr#25159)
    * Allow PEImageLayout to be loadable for R2R images loaded from streams.
    
    * Add regression test coverage
    
    * Change CheckILOnly to return true for R2R images (similar to the ILOnly API)
    
    * Early out for R2R images
    
    * Remove R2R check
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/2b85852af5e4e46973af07ce36c4d2f2a0e30616

--- src/coreclr/tests/src/readytorun/tests/main.cs
+++ src/coreclr/tests/src/readytorun/tests/main.cs
@@ -425,6 +425,16 @@ class Program
             Assert.AreEqual(value[i], (byte)(9 - i));
     }
 
+    static void TestLoadR2RImageFromByteArray()
+    {
+        Assembly assembly1 = typeof(Program).Assembly;
+        
+        byte[] array = File.ReadAllBytes(assembly1.Location);
+        Assembly assembly2 = Assembly.Load(array);
+        
+        Assert.AreEqual(assembly2.FullName, assembly1.FullName);
+    }
+
     static void RunAllTests()
     {
         TestVirtualMethodCalls();
@@ -479,6 +489,8 @@ class Program
         GenericLdtokenFieldsTest();
 
         RVAFieldTest();
+        
+        TestLoadR2RImageFromByteArray();
     }
 
     static int Main()
