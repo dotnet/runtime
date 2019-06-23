248f087b8db718880c82ccd95286c133f2c36ec8
Herman Eldering <HermanEldering@users.noreply.github.com>
2019-06-23 04:52:42 +0200

    ArgumentException: use system-supplied message if message is null (#25115)
    Enable using a system-supplied message while also providing an argument/parameter name by passing in a null message. Made to match the behavior of FileNotFoundException.

--- src/System.Private.CoreLib/shared/System/ArgumentException.cs
+++ src/System.Private.CoreLib/shared/System/ArgumentException.cs
@@ -78,6 +78,8 @@ namespace System
         {
             get
             {
+                SetMessageField();
+                
                 string s = base.Message;
                 if (!string.IsNullOrEmpty(_paramName))
                 {
@@ -88,6 +90,14 @@ namespace System
             }
         }
 
+        private void SetMessageField()
+        {
+            if (_message == null && HResult == System.HResults.COR_E_ARGUMENT) 
+            {
+                _message = SR.Arg_ArgumentException;
+            }
+        }
+        
         public virtual string? ParamName
         {
             get { return _paramName; }
