d813dd23eccac5a322ed76acf832dfbefeead937
Ganbarukamo41 <ganbarukamo@gmail.com>
2019-06-25 04:44:50 +0900

    Make Vector*.ToString() Culture invariant (dotnet/coreclr#25343)
    * Remove IFormattable
    * Hardcode characters
    * Add tests
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/d505b49a285f6cd7776230fb7f0d73d3ac20b73a

---
+++ src/clr/tests/src/JIT/HardwareIntrinsics/General/Vector64_1/ToString.Double.cs
@@ -0,0 +1,50 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+/******************************************************************************
+ * This file is auto-generated from a template file by the GenerateTests.csx  *
+ * script in tests\src\JIT\HardwareIntrinsics\General\Shared. In order to make    *
+ * changes, please update the corresponding template and run according to the *
+ * directions listed in the file.                                             *
+ ******************************************************************************/
+
+using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics;
+
+namespace JIT.HardwareIntrinsics.General
+{
+    public static partial class Program
+    {
+        private unsafe static void ToStringDouble()
+        {
+            int size = Unsafe.SizeOf<Vector64<Double>>() / sizeof(Double);
+            Double[] values = new Double[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                values[i] = TestLibrary.Generator.GetDouble();
+            }
+            
+            Vector64<Double> vector = Vector64.Create(values[0]);
+            string actual = vector.ToString();
+
+            string expected = '<' + string.Join(", ", values.Select(x => x.ToString("G", System.Globalization.CultureInfo.InvariantCulture))) + '>';
+
+            bool succeeded = string.Equals(expected, actual, StringComparison.Ordinal);
+
+            if (!succeeded)
+            {
+                TestLibrary.TestFramework.LogInformation($"Vector64DoubleToString: Vector64<Double>.ToString() returned an unexpected result.");
+                TestLibrary.TestFramework.LogInformation($"Expected: {expected}");
+                TestLibrary.TestFramework.LogInformation($"Actual: {actual}");
+                TestLibrary.TestFramework.LogInformation(string.Empty);
+
+                throw new Exception("One or more scenarios did not complete as expected.");
+            }
+        }
+    }
+}
---
+++ src/clr/tests/src/JIT/HardwareIntrinsics/General/Vector64_1/ToString.SByte.cs
@@ -0,0 +1,50 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+/******************************************************************************
+ * This file is auto-generated from a template file by the GenerateTests.csx  *
+ * script in tests\src\JIT\HardwareIntrinsics\General\Shared. In order to make    *
+ * changes, please update the corresponding template and run according to the *
+ * directions listed in the file.                                             *
+ ******************************************************************************/
+
+using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics;
+
+namespace JIT.HardwareIntrinsics.General
+{
+    public static partial class Program
+    {
+        private unsafe static void ToStringSByte()
+        {
+            int size = Unsafe.SizeOf<Vector64<SByte>>() / sizeof(SByte);
+            SByte[] values = new SByte[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                values[i] = TestLibrary.Generator.GetSByte();
+            }
+            
+            Vector64<SByte> vector = Vector64.Create(values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7]);
+            string actual = vector.ToString();
+
+            string expected = '<' + string.Join(", ", values.Select(x => x.ToString("G", System.Globalization.CultureInfo.InvariantCulture))) + '>';
+
+            bool succeeded = string.Equals(expected, actual, StringComparison.Ordinal);
+
+            if (!succeeded)
+            {
+                TestLibrary.TestFramework.LogInformation($"Vector64SByteToString: Vector64<SByte>.ToString() returned an unexpected result.");
+                TestLibrary.TestFramework.LogInformation($"Expected: {expected}");
+                TestLibrary.TestFramework.LogInformation($"Actual: {actual}");
+                TestLibrary.TestFramework.LogInformation(string.Empty);
+
+                throw new Exception("One or more scenarios did not complete as expected.");
+            }
+        }
+    }
+}
