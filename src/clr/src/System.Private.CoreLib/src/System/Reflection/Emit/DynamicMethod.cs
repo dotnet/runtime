5ef6499caa92fe2a96368001fc12de7c6eec8f2f
Santiago Fernandez Madero <safern@microsoft.com>
2019-06-24 19:11:32 -0500

    Address last round of nullable feedback - S.Resources.ResourceManager, System.Reflection.Emit and ILGeneration (dotnet/coreclr#25318)
    * Address nullable feedback for System.Resources.ResourceManager
    
    * Address nullable feedback for System.Reflection.Emit.ILGeneration
    
    * Address nullable feedback for System.Reflection.Emit
    
    * PR Feedback and clean-up redundant casts
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/560467709747d75f5897e0d9cf441c91ac4e65a8

--- src/clr/src/System.Private.CoreLib/src/System/Reflection/Emit/DynamicMethod.cs
+++ src/clr/src/System.Private.CoreLib/src/System/Reflection/Emit/DynamicMethod.cs
@@ -49,8 +49,8 @@ namespace System.Reflection.Emit
         //
 
         public DynamicMethod(string name,
-                             Type returnType,
-                             Type[] parameterTypes)
+                             Type? returnType,
+                             Type[]? parameterTypes)
         {
             Init(name,
                 MethodAttributes.Public | MethodAttributes.Static,
@@ -64,8 +64,8 @@ namespace System.Reflection.Emit
         }
 
         public DynamicMethod(string name,
-                             Type returnType,
-                             Type[] parameterTypes,
+                             Type? returnType,
+                             Type[]? parameterTypes,
                              bool restrictedSkipVisibility)
         {
             Init(name,
@@ -80,8 +80,8 @@ namespace System.Reflection.Emit
         }
 
         public DynamicMethod(string name,
-                             Type returnType,
-                             Type[] parameterTypes,
+                             Type? returnType,
+                             Type[]? parameterTypes,
                              Module m)
         {
             if (m == null)
@@ -99,8 +99,8 @@ namespace System.Reflection.Emit
         }
 
         public DynamicMethod(string name,
-                             Type returnType,
-                             Type[] parameterTypes,
+                             Type? returnType,
+                             Type[]? parameterTypes,
                              Module m,
                              bool skipVisibility)
         {
@@ -121,8 +121,8 @@ namespace System.Reflection.Emit
         public DynamicMethod(string name,
                              MethodAttributes attributes,
                              CallingConventions callingConvention,
-                             Type returnType,
-                             Type[] parameterTypes,
+                             Type? returnType,
+                             Type[]? parameterTypes,
                              Module m,
                              bool skipVisibility)
         {
@@ -141,8 +141,8 @@ namespace System.Reflection.Emit
         }
 
         public DynamicMethod(string name,
-                             Type returnType,
-                             Type[] parameterTypes,
+                             Type? returnType,
+                             Type[]? parameterTypes,
                              Type owner)
         {
             if (owner == null)
@@ -160,8 +160,8 @@ namespace System.Reflection.Emit
         }
 
         public DynamicMethod(string name,
-                             Type returnType,
-                             Type[] parameterTypes,
+                             Type? returnType,
+                             Type[]? parameterTypes,
                              Type owner,
                              bool skipVisibility)
         {
@@ -182,8 +182,8 @@ namespace System.Reflection.Emit
         public DynamicMethod(string name,
                              MethodAttributes attributes,
                              CallingConventions callingConvention,
-                             Type returnType,
-                             Type[] parameterTypes,
+                             Type? returnType,
+                             Type[]? parameterTypes,
                              Type owner,
                              bool skipVisibility)
         {
