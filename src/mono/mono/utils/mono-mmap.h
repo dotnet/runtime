4fdf15bf79dfb0d96bb0796831d6f7f8b57c4f24
Aleksey Kliger (λgeek) <alklig@microsoft.com>
2019-06-26 11:10:03 -0400

    [sdks] dynamically link libmonosgen to mono-sgen for monodroid host builds (mono/mono#15410)
    * [configure] Cosmetic AC_MSG_WARN fix
    
    * [runtime] Mark mono_file_map_error as MONO_API, always
    
    Fixed the `--with-static_mono=no` build on non-Win32
    
    * [sdks] dynamically link libmonosgen to mono-sgen for monodroid host builds
    
    
    Commit migrated from https://github.com/mono/mono/commit/927c608184b86b6c03958ba9aecec02340cd0661

--- src/mono/mono/utils/mono-mmap.h
+++ src/mono/mono/utils/mono-mmap.h
@@ -64,11 +64,7 @@ MONO_API void* mono_file_map   (size_t length, int flags, int fd, guint64 offset
 // Last two parameters are optional.
 // This is mono_file_map but with optionally returning an error message.
 // See https://github.com/mono/mono/issues/8225.
-#if defined (HOST_WIN32)
 MONO_API
-#elif defined (__cplusplus)
-G_EXTERN_C
-#endif
 void*
 mono_file_map_error (size_t length, int flags, int fd, guint64 offset, void **ret_handle, const char *filepath, char **error_message);
 MONO_API int   mono_file_unmap (void *addr, void *handle);
