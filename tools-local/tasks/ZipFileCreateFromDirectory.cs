52a5513a7cd4d11fbd8d89ca8fd6f4fe07a4efd0
Davis Goodin <dagood@microsoft.com>
2019-06-26 09:51:35 -0500

    Remove+sort usings, add headers for build tasks

--- tools-local/tasks/ZipFileCreateFromDirectory.cs
+++ tools-local/tasks/ZipFileCreateFromDirectory.cs
@@ -2,12 +2,12 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
 using System;
 using System.IO;
 using System.IO.Compression;
 using System.Text.RegularExpressions;
-using Microsoft.Build.Framework;
-using Microsoft.Build.Utilities;
 
 namespace Microsoft.DotNet.Build.Tasks
 {
