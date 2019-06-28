851bfbb001bb4ef946d0576895f2fe8a13db6bbe
Kahbazi <A.Kahbazi@gmail.com>
2019-06-28 17:06:39 +0430

    Dispose EventLog in WindowsEventLog (dotnet/Extensions#1865)
    * Dispose EventLog in WindowsEventLog
    
    
    Commit migrated from https://github.com/dotnet/Extensions/commit/92ed06dcb775d8f7a62cfcb17018ec42a2d23910

--- src/libraries/Microsoft.Extensions.Logging.EventLog/src/EventLogLoggerProvider.cs
+++ src/libraries/Microsoft.Extensions.Logging.EventLog/src/EventLogLoggerProvider.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using Microsoft.Extensions.Options;
 
 namespace Microsoft.Extensions.Logging.EventLog
@@ -49,6 +50,10 @@ namespace Microsoft.Extensions.Logging.EventLog
 
         public void Dispose()
         {
+            if (_settings.EventLog is WindowsEventLog windowsEventLog)
+            {
+                windowsEventLog.DiagnosticsEventLog.Dispose();
+            }
         }
 
         public void SetScopeProvider(IExternalScopeProvider scopeProvider)
