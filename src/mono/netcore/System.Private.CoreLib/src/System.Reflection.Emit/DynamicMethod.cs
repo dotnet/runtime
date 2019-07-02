5afb3cad3a5d842b24b8065a0a83c20493a17fdb
Steve Pfister <steveisok@users.noreply.github.com>
2019-07-01 21:12:06 -0400

    [netcore] Remove IsByRef check on Reflection.Emit.DynamicMethod
    Fixes https://github.com/mono/mono/issues/15324
    
    Removed check on the return type being by ref.
    
    Commit migrated from https://github.com/mono/mono/commit/d39c84d45f20fac4884ca5e4a3074f1518e8ca8b

--- src/mono/netcore/System.Private.CoreLib/src/System.Reflection.Emit/DynamicMethod.cs
+++ src/mono/netcore/System.Private.CoreLib/src/System.Reflection.Emit/DynamicMethod.cs
@@ -109,9 +109,7 @@ namespace System.Reflection.Emit {
 			if (owner == null && typeOwner)
 				throw new ArgumentNullException (nameof (owner));
 			if ((m == null) && !anonHosted)
-				throw new ArgumentNullException ("m");
-			if (returnType.IsByRef)
-				throw new ArgumentException ("Return type can't be a byref type", "returnType");
+				throw new ArgumentNullException ("m");			
 			if (parameterTypes != null) {
 				for (int i = 0; i < parameterTypes.Length; ++i)
 					if (parameterTypes [i] == null)
