e611ece7a0313320b96eeea376a97219f901a8e1
Davis Goodin <dagood@microsoft.com>
2019-07-12 15:51:35 -0500

    Add simple sfx packaging tests

---
+++ src/test/Microsoft.DotNet.CoreSetup.Packaging.Tests/NETStandardTests.cs
@@ -0,0 +1,28 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.DotNet.CoreSetup.Test;
+using Xunit;
+
+namespace Microsoft.DotNet.CoreSetup.Packaging.Tests
+{
+    public class NETStandardTests
+    {
+        private readonly RepoDirectoriesProvider dirs = new RepoDirectoriesProvider();
+
+        [Fact]
+        public void NETStandardTargetingPackIsValid()
+        {
+            using (var tester = NuGetArtifactTester.Open(
+                dirs,
+                "NETStandard.Library.Ref"))
+            {
+                tester.HasOnlyTheseDataFiles(
+                    "data/FrameworkList.xml");
+
+                tester.IsTargetingPack();
+            }
+        }
+    }
+}
