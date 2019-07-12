c61caeeea31d45ded9f4950299d091c89de1a398
Davis Goodin <dagood@microsoft.com>
2019-07-12 15:51:35 -0500

    Add simple sfx packaging tests
    Commit migrated from https://github.com/dotnet/core-setup/commit/e611ece7a0313320b96eeea376a97219f901a8e1

---
+++ src/installer/test/Microsoft.DotNet.CoreSetup.Packaging.Tests/NETCoreTests.cs
@@ -0,0 +1,54 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.DotNet.CoreSetup.Test;
+using Xunit;
+
+namespace Microsoft.DotNet.CoreSetup.Packaging.Tests
+{
+    public class NETCoreTests
+    {
+        private readonly RepoDirectoriesProvider dirs = new RepoDirectoriesProvider();
+
+        [Fact]
+        public void NETCoreTargetingPackIsValid()
+        {
+            using (var tester = NuGetArtifactTester.Open(
+                dirs,
+                "Microsoft.NETCore.App.Ref"))
+            {
+                tester.HasOnlyTheseDataFiles(
+                    "data/FrameworkList.xml",
+                    "data/PlatformManifest.txt");
+
+                tester.IsTargetingPack();
+                tester.HasGoodPlatformManifest();
+            }
+        }
+
+        [Fact]
+        public void NETCoreAppHostPackIsValid()
+        {
+            using (var tester = NuGetArtifactTester.Open(
+                dirs,
+                "Microsoft.NETCore.App.Host",
+                $"Microsoft.NETCore.App.Host.{dirs.BuildRID}"))
+            {
+                tester.IsAppHostPack();
+            }
+        }
+
+        [Fact]
+        public void NETCoreRuntimePackIsValid()
+        {
+            using (var tester = NuGetArtifactTester.Open(
+                dirs,
+                "Microsoft.NETCore.App.Runtime",
+                $"Microsoft.NETCore.App.Runtime.{dirs.BuildRID}"))
+            {
+                tester.IsRuntimePack();
+            }
+        }
+    }
+}
