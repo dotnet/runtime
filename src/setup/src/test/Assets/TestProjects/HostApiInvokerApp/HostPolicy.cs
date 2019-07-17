06cc879e9adda4c8dd1f78527d30afa5cbd95910
Vitek Karas <vitek.karas@microsoft.com>
2019-07-17 03:49:01 -0700

    Test component dependency resolution via native host tests (dotnet/core-setup#6919)
    Change component dependency resolution tests to not use managed test app which requires full framework to run (and is slow). Instead implement component dependency resolution into native host test app and change all tests to use that.
    
    This makes the tests much faster and it also allows for more flexible tests (it's easy to mock shared frameworks now).
    
    Notable refactorings:
    
    Implement error writer redirector in the native tests - this is necessary to correctly handle multi-threaded error redirection. Using C++ templates to get multiple distinct function pointers (for each thread separately)
    Split hostfxr exports into its own class which can be reused by multiple tests
    Introduce hostpolicy exports
    Implement case class for component dependency resolution tests with helpers and such
    Added test which validates per-assembly version resolution behavior
    
    
    
    Commit migrated from https://github.com/dotnet/core-setup/commit/2870b217650af9f49ffa1b835b998d58e25638c8

--- src/setup/src/test/Assets/TestProjects/HostApiInvokerApp/HostPolicy.cs
+++ src/setup/src/test/Assets/TestProjects/HostApiInvokerApp/HostPolicy.cs
@@ -32,108 +32,6 @@ namespace HostApiInvokerApp
                 corehost_error_writer_fn error_writer);
         }
 
-        static void Test_corehost_resolve_component_dependencies_internal(string prefix, string assemblyPath)
-        {
-            StringBuilder errorBuilder = new StringBuilder();
-
-            hostpolicy.corehost_set_error_writer((message) =>
-            {
-                errorBuilder.AppendLine(message);
-            });
-
-            string assemblies = null;
-            string nativeSearchPaths = null;
-            string resourceSearcPaths = null;
-            int rc;
-            try
-            {
-                rc = hostpolicy.corehost_resolve_component_dependencies(
-                    assemblyPath,
-                    (assembly_paths, native_search_paths, resource_search_paths) => 
-                    {
-                        assemblies = assembly_paths;
-                        nativeSearchPaths = native_search_paths;
-                        resourceSearcPaths = resource_search_paths;
-                    });
-            }
-            finally
-            {
-                hostpolicy.corehost_set_error_writer(null);
-            }
-
-            if (assemblies != null)
-            {
-                // Sort the assemblies since in the native code we store it in a hash table
-                // which gives random order. The native code always adds the separator at the end
-                // so mimic that behavior as well.
-                assemblies = string.Join(Path.PathSeparator, assemblies.Split(Path.PathSeparator, StringSplitOptions.RemoveEmptyEntries).OrderBy(a => a)) + Path.PathSeparator;
-            }
-
-            if (rc == 0)
-            {
-                Console.WriteLine($"{prefix}corehost_resolve_component_dependencies:Success");
-                Console.WriteLine($"{prefix}corehost_resolve_component_dependencies assemblies:[{assemblies}]");
-                Console.WriteLine($"{prefix}corehost_resolve_component_dependencies native_search_paths:[{nativeSearchPaths}]");
-                Console.WriteLine($"{prefix}corehost_resolve_component_dependencies resource_search_paths:[{resourceSearcPaths}]");
-            }
-            else
-            {
-                Console.WriteLine($"{prefix}corehost_resolve_component_dependencies:Fail[0x{rc.ToString("X8")}]");
-            }
-
-            if (errorBuilder.Length > 0)
-            {
-                IEnumerable<string> errorLines = errorBuilder.ToString().Split(new string[] { Environment.NewLine }, StringSplitOptions.None)
-                    .Select(line => prefix + line);
-                Console.WriteLine($"{prefix}corehost reported errors:{Environment.NewLine}{string.Join(Environment.NewLine, errorLines)}");
-            }
-        }
-
-        static void Test_corehost_resolve_component_dependencies(string[] args)
-        {
-            if (args.Length != 2)
-            {
-                throw new ArgumentException("Invalid number of arguments passed");
-            }
-
-            Test_corehost_resolve_component_dependencies_internal("", args[1]);
-        }
-
-        static void Test_corehost_resolve_component_dependencies_multithreaded(string[] args)
-        {
-            if (args.Length != 3)
-            {
-                throw new ArgumentException("Invalid number of arguments passed");
-            }
-
-            Func<string, Thread> createThread = (string assemblyPath) =>
-            {
-                return new Thread(() =>
-                {
-                    Test_corehost_resolve_component_dependencies_internal(
-                        Path.GetFileNameWithoutExtension(assemblyPath) + ": ",
-                        assemblyPath
-                    );
-                });
-            };
-
-            Thread t1 = createThread(args[1]);
-            Thread t2 = createThread(args[2]);
-
-            t1.Start();
-            t2.Start();
-
-            if (!t1.Join(TimeSpan.FromSeconds(30)))
-            {
-                throw new ApplicationException("Thread 1 didn't finish in time.");
-            }
-
-            if (!t2.Join(TimeSpan.FromSeconds(30)))
-            {
-                throw new ApplicationException("Thread 1 didn't finish in time.");
-            }
-        }
-
         static void Test_corehost_set_error_writer(string[] args)
         {
             hostpolicy.corehost_error_writer_fn writer1 = (message) => { Console.WriteLine(nameof(writer1)); };
@@ -164,12 +62,6 @@ namespace HostApiInvokerApp
         {
             switch (apiToTest)
             {
-                case nameof(hostpolicy.corehost_resolve_component_dependencies):
-                    Test_corehost_resolve_component_dependencies(args);
-                    break;
-                case nameof(hostpolicy.corehost_resolve_component_dependencies) + "_multithreaded":
-                    Test_corehost_resolve_component_dependencies_multithreaded(args);
-                    break;
                 case nameof(Test_corehost_set_error_writer):
                     Test_corehost_set_error_writer(args);
                     break;
