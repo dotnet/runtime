a8f25bb613bff282b3aa59ceb8c6d605279a5d6a
Zoltan Varga <vargaz@gmail.com>
2019-07-21 15:56:44 -0400

    Fix the netcore build.
    Commit migrated from https://github.com/mono/mono/commit/fe015de078a72aa3d180297acee63c1395b5e2a5

--- src/mono/mono/metadata/icall-def-netcore.h
+++ src/mono/mono/metadata/icall-def-netcore.h
@@ -540,5 +540,5 @@ HANDLES(TYPEDR_1, "InternalMakeTypedReference", ves_icall_System_TypedReference_
 HANDLES(TYPEDR_2, "InternalToObject", ves_icall_System_TypedReference_ToObject, MonoObject, 1, (MonoTypedRef_ptr))
 
 ICALL_TYPE(VALUET, "System.ValueType", VALUET_1)
-ICALL(VALUET_1, "InternalEquals", ves_icall_System_ValueType_Equals)
-ICALL(VALUET_2, "InternalGetHashCode", ves_icall_System_ValueType_InternalGetHashCode)
+HANDLES(VALUET_1, "InternalEquals", ves_icall_System_ValueType_Equals, MonoBoolean, 3, (MonoObject, MonoObject, MonoArrayOut))
+HANDLES(VALUET_2, "InternalGetHashCode", ves_icall_System_ValueType_InternalGetHashCode, gint32, 2, (MonoObject, MonoArrayOut))
