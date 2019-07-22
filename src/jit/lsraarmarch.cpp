2a8529c6130f6c70488e8bace26404b319eed00f
mikedn <onemihaid@hotmail.com>
2019-07-23 00:32:45 +0300

    Remove GTF_REVERSE_OPS checks from LSRA (#22489)
    GTF_REVERSE_OPS is cleared during rationalization.

--- src/jit/lsraarmarch.cpp
+++ src/jit/lsraarmarch.cpp
@@ -685,20 +685,16 @@ int LinearScan::BuildBlockStore(GenTreeBlk* blkNode)
         buildInternalIntRegisterDefForNode(blkNode, blkSizeRegMask);
     }
 
-    if (!dstAddr->isContained() && !blkNode->IsReverseOp())
+    if (!dstAddr->isContained())
     {
         srcCount++;
         BuildUse(dstAddr, dstAddrRegMask);
     }
+
     if ((srcAddrOrFill != nullptr) && !srcAddrOrFill->isContained())
     {
         srcCount++;
         BuildUse(srcAddrOrFill, sourceRegMask);
-    }
-    if (!dstAddr->isContained() && blkNode->IsReverseOp())
-    {
-        srcCount++;
-        BuildUse(dstAddr, dstAddrRegMask);
     }
 
     if (blkNode->OperIs(GT_STORE_DYN_BLK))
