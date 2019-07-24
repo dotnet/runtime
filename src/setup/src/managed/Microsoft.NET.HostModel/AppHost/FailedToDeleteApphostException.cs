1b957902962cbce22cd5bbfcaf1235ab4e0f0d6f
William Li <wul@microsoft.com>
2019-07-24 15:24:04 -0700

    Delete output apphost upon failure of the CreateAppHost task.
    Port https://github.com/dotnet/sdk/pull/3427 by @peterhuene with my edit
    
    This commit deletes the output apphost when the CreateAppHost task fails from
    an exception.
    
    Partially fixes dotnet/core-setup#2989
    
    -------------
    * deletes the output apphost when the `CreateAppHost` task fails from
    an exception.
    
    * aggreates the Exception for future logging from deleting the failed apphost
    
    
    Commit migrated from https://github.com/dotnet/core-setup/commit/7f0eb2869f2f37a899cdd02b7bbc4bd78e4e7038

---
+++ src/setup/src/managed/Microsoft.NET.HostModel/AppHost/FailedToDeleteApphostException.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+
+namespace Microsoft.NET.HostModel.AppHost
+{
+    /// <summary>
+    /// Failed to delete apphost when trying to delete incomplete appphost
+    /// </summary>
+    public class FailedToDeleteApphostException : AppHostUpdateException
+    {
+        public string ExceptionMessage { get; }
+        public FailedToDeleteApphostException(string exceptionMessage)
+        {
+            ExceptionMessage = exceptionMessage;
+        }
+    }
+}
+
