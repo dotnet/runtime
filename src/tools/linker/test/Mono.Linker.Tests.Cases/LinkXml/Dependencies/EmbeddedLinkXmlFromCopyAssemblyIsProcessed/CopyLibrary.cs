f56bb1cdef770427d198ed169118300b97bd998a
Sven Boemer <sbomer@gmail.com>
2019-07-29 07:58:21 -0700

    Respect embedded xml files from copy assemblies (dotnet/linker#675)
    * Respect embedded xml files from copy assemblies
    
    * Add testcase
    
    * Remove reference from CopyLibrary -> Library
    
    
    Commit migrated from https://github.com/dotnet/linker/commit/18ff3f49475c3fc3fafa1d17161b1525eea182d8

---
+++ src/tools/linker/test/Mono.Linker.Tests.Cases/LinkXml/Dependencies/EmbeddedLinkXmlFromCopyAssemblyIsProcessed/CopyLibrary.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Mono.Linker.Tests.Cases.LinkXml.Dependencies.EmbeddedLinkXmlFromCopyAssemblyIsProcessed
+{
+	public class CopyLibrary {
+		public void Method ()
+		{
+		}
+	}
+}
