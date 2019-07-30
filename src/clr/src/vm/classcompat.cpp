328f03759947e42925091328c2d3687798d90117
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2019-07-30 17:11:17 +0200

    Allow default interface methods for crossgen /createpdb (dotnet/coreclr#25839)
    The existing code was assuming we're doing fragile crossgen and blocked loading the type.
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/39f207ad8babe677029ddc2c72714a9959da2475

--- src/clr/src/vm/classcompat.cpp
+++ src/clr/src/vm/classcompat.cpp
@@ -2596,7 +2596,7 @@ VOID    MethodTableBuilder::EnumerateClassMethods()
         if (fIsClassInterface
 #if defined(FEATURE_DEFAULT_INTERFACES)
             // Only fragile crossgen wasn't upgraded to deal with default interface methods.
-            && !IsReadyToRunCompilation()
+            && !IsReadyToRunCompilation() && !IsNgenPDBCompilationProcess()
 #endif
             )
         {
