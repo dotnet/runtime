30baed934edacfa86f0563a8d9e81dda027be18a
Jan Kotas <jkotas@microsoft.com>
2019-08-06 13:48:15 +0200

    Revert "Fix issue with out-of-scope GCFrame in Frame list (dotnet/coreclr#25795)" (dotnet/coreclr#26029)
    This reverts commit dotnet/coreclr@09cb367557c580466b6de7582f1e53c9af526c9b.
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/f3dd26cb712030151c68c0c391ea0e4fbe9e2155

--- src/coreclr/src/vm/frames.h
+++ src/coreclr/src/vm/frames.h
@@ -2530,11 +2530,7 @@ private:
     BOOL          m_MaybeInterior;
 
     // Keep as last entry in class
-    DEFINE_VTABLE_GETTER(GCFrame)
-
-#if !defined(DACCESS_COMPILE) && !defined(CROSSGEN_COMPILE)
-    ~GCFrame();
-#endif
+    DEFINE_VTABLE_GETTER_AND_DTOR(GCFrame)
 };
 
 #ifdef FEATURE_INTERPRETER
@@ -3264,16 +3260,11 @@ public:
             *m_pShadowSP |= ICodeManager::SHADOW_SP_FILTER_DONE;
         }
     }
-
-#ifndef CROSSGEN_COMPILE
-    ~ExceptionFilterFrame();
-#endif
-
 #endif
 
 private:
     // Keep as last entry in class
-    DEFINE_VTABLE_GETTER_AND_CTOR(ExceptionFilterFrame)
+    DEFINE_VTABLE_GETTER_AND_CTOR_AND_DTOR(ExceptionFilterFrame)
 };
 
 #ifdef _DEBUG
@@ -3591,7 +3582,7 @@ public:
 
 #define GCPROTECT_END()                                                 \
                 DEBUG_ASSURE_NO_RETURN_END(GCPROTECT) }                 \
-                } while(0)
+                __gcframe.Pop(); } while(0)
 
 
 #else // #ifndef DACCESS_COMPILE
