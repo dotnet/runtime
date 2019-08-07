8685c4f4d74d0823e625d08b5d936cfcdd526d7a
Marek Safar <marek.safar@gmail.com>
2019-08-07 14:29:11 +0200

    Handle simple local variables reflection calls
    Commit migrated from https://github.com/dotnet/linker/commit/78d64331cc0dfd28d5133e6824ea2e0ba51cb8e7

---
+++ src/tools/linker/test/Mono.Linker.Tests.Cases/Reflection/ExpressionCallStringAndLocals.cs
@@ -0,0 +1,24 @@
+﻿using Mono.Linker.Tests.Cases.Expectations.Assertions;
+using System.Linq.Expressions;
+using System;
+using Mono.Linker.Tests.Cases.Expectations.Metadata;
+
+namespace Mono.Linker.Tests.Cases.Reflection {
+	[SetupCompileArgument ("/r:System.Core.dll")]
+	public class ExpressionCallStringAndLocals {
+		public static void Main ()
+		{
+			var t1 = typeof (ExpressionCallStringAndLocals);
+			var t2 = t1;
+
+			var expr = Expression.Call (t2, "OnlyCalledViaExpression", Type.EmptyTypes);
+			Console.WriteLine (expr.Method);
+		}
+
+		[Kept]
+		private static int OnlyCalledViaExpression ()
+		{
+			return 42;
+		}
+	}
+}
