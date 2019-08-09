8eb721ccbbdbee3697e4b52af8ff4eda070a1de2
Mike McLaughlin <mikem@microsoft.com>
2019-08-09 10:24:06 -0700

    ARM32 out of proc unwind support (dotnet/coreclr#26082)
    ARM32 out of proc unwind support
    
    Removed the old dwarf unwind info decoding and use libunwind
    for both dwarf and arm exidx.
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/e8f08ff064b406d0ab0f36217abe6328ea61c081

---
+++ src/coreclr/src/pal/src/libunwind/src/oop/_OOP_internal.h
@@ -0,0 +1,25 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#ifndef _OOP_internal_h
+#define _OOP_internal_h
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#ifdef HAVE_SYS_TYPES_H
+#include <sys/types.h>
+#endif
+
+#include <errno.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "libunwind_i.h"
+#include "dwarf-eh.h"
+#include "dwarf_i.h"
+
+#endif /* _OOP_internal_h */
