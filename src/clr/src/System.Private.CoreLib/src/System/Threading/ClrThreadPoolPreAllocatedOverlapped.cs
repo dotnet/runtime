2b3885385c70bb06da2010ac231a6ab5ef43b38f
Stephen Toub <stoub@microsoft.com>
2019-08-16 10:04:30 -0400

    Fix IDE0060 (unused parameter)
    Commit migrated from https://github.com/dotnet/coreclr/commit/4510509e4d17102670b0a3271aaf68f571a5753b

--- src/clr/src/System.Private.CoreLib/src/System/Threading/ClrThreadPoolPreAllocatedOverlapped.cs
+++ src/clr/src/System.Private.CoreLib/src/System/Threading/ClrThreadPoolPreAllocatedOverlapped.cs
@@ -58,7 +58,7 @@ namespace System.Threading
 
         internal bool AddRef()
         {
-            return _lifetime.AddRef(this);
+            return _lifetime.AddRef();
         }
 
         internal void Release()
