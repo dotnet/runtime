5206770153b565d064ebb8802dee9f30b8793474
Ryan Lucia <rylucia@microsoft.com>
2019-08-20 10:45:03 -0400

    Move AssemblyLoadContext VM-invoked static resolve methods to shared (dotnet/coreclr#26248)
    Shared between CoreCLR and Mono. Mono began using a copy of these methods with https://github.com/mono/mono/pull/16256/
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/d305063523370cfe3b728898df9b4e67acd6e3cb

--- src/coreclr/src/System.Private.CoreLib/src/System/Runtime/Loader/AssemblyLoadContext.CoreCLR.cs
+++ src/coreclr/src/System.Private.CoreLib/src/System/Runtime/Loader/AssemblyLoadContext.CoreCLR.cs
@@ -188,48 +188,6 @@ namespace System.Runtime.Loader
             InternalStartProfile(profile, _nativeAssemblyLoadContext);
         }
 
-        // This method is called by the VM.
-        private static void OnAssemblyLoad(RuntimeAssembly assembly)
-        {
-            AssemblyLoad?.Invoke(AppDomain.CurrentDomain, new AssemblyLoadEventArgs(assembly));
-        }
-
-        // This method is called by the VM.
-        private static RuntimeAssembly? OnResourceResolve(RuntimeAssembly assembly, string resourceName)
-        {
-            return InvokeResolveEvent(ResourceResolve, assembly, resourceName);
-        }
-
-        // This method is called by the VM
-        private static RuntimeAssembly? OnTypeResolve(RuntimeAssembly assembly, string typeName)
-        {
-            return InvokeResolveEvent(TypeResolve, assembly, typeName);
-        }
-
-        // This method is called by the VM.
-        private static RuntimeAssembly? OnAssemblyResolve(RuntimeAssembly assembly, string assemblyFullName)
-        {
-            return InvokeResolveEvent(AssemblyResolve, assembly, assemblyFullName);
-        }
-
-        private static RuntimeAssembly? InvokeResolveEvent(ResolveEventHandler? eventHandler, RuntimeAssembly assembly, string name)
-        {
-            if (eventHandler == null)
-                return null;
-
-            var args = new ResolveEventArgs(name, assembly);
-
-            foreach (ResolveEventHandler handler in eventHandler.GetInvocationList())
-            {
-                Assembly? asm = handler(AppDomain.CurrentDomain, args);
-                RuntimeAssembly? ret = GetRuntimeAssembly(asm);
-                if (ret != null)
-                    return ret;
-            }
-
-            return null;
-        }
-
         private static RuntimeAssembly? GetRuntimeAssembly(Assembly? asm)
         {
             return
