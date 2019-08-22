1b3048575bdb9236f6c6d8d7952485e0cc0360c3
Jarret Shook <jashoo@microsoft.com>
2019-08-22 10:10:03 -0700

    Remove native test build from build.sh (dotnet/coreclr#26176)
    * Remove native test build from build.sh
    
    * Remove skiptests from unix documentation and yml
    
    * Add back skiptests for winodws
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/42e189be4fd1fbb68a13345074d726b4cf86d195

--- src/clr/build.sh
+++ src/clr/build.sh
@@ -25,7 +25,7 @@ export PYTHON
 
 usage()
 {
-    echo "Usage: $0 [BuildArch] [BuildType] [-verbose] [-coverage] [-cross] [-gccx.y] [-clangx.y] [-ninja] [-configureonly] [-skipconfigure] [-skipnative] [-skipcrossarchnative] [-skipmanaged] [-skipmscorlib] [-skiptests] [-stripsymbols] [-ignorewarnings] [-cmakeargs] [-bindir]"
+    echo "Usage: $0 [BuildArch] [BuildType] [-verbose] [-coverage] [-cross] [-gccx.y] [-clangx.y] [-ninja] [-configureonly] [-skipconfigure] [-skipnative] [-skipcrossarchnative] [-skipmanaged] [-skipmscorlib] [-stripsymbols] [-ignorewarnings] [-cmakeargs] [-bindir]"
     echo "BuildArch can be: -x64, -x86, -arm, -armel, -arm64"
     echo "BuildType can be: -debug, -checked, -release"
     echo "-coverage - optional argument to enable code coverage build (currently supported only for Linux and OSX)."
@@ -43,7 +43,6 @@ usage()
     echo "-skipcrossarchnative - do not build cross-architecture native components."
     echo "-skipmanaged - do not build managed components."
     echo "-skipmscorlib - do not build mscorlib.dll."
-    echo "-skiptests - skip the tests in the 'tests' subdirectory."
     echo "-skipnuget - skip building nuget packages."
     echo "-skiprestoreoptdata - skip restoring optimization data used by profile-based optimizations."
     echo "-skipcrossgen - skip native image generation"
@@ -641,7 +640,6 @@ __SkipRestoreOptData=0
 __SkipCrossgen=0
 __CrossgenOnly=0
 __PartialNgen=0
-__SkipTests=0
 __CrossBuild=0
 __ClangMajorVersion=0
 __ClangMinorVersion=0
@@ -893,10 +891,6 @@ while :; do
             __PartialNgen=1
             ;;
 
-        skiptests|-skiptests)
-            __SkipTests=1
-            ;;
-
         skipnuget|-skipnuget|skipbuildpackages|-skipbuildpackages)
             __SkipNuget=1
             ;;
@@ -1071,12 +1065,6 @@ generate_event_logging
 
 # Build the coreclr (native) components.
 __ExtraCmakeArgs="-DCLR_CMAKE_TARGET_OS=$__BuildOS -DCLR_CMAKE_PACKAGES_DIR=$__PackagesDir -DCLR_CMAKE_PGO_INSTRUMENT=$__PgoInstrument -DCLR_CMAKE_OPTDATA_VERSION=$__PgoOptDataVersion -DCLR_CMAKE_PGO_OPTIMIZE=$__PgoOptimize"
-
-# [TODO] Remove this when the `build-test.sh` script properly builds and deploys test assets.
-if [ $__SkipTests != 1 ]; then
-    echo "Adding CMake flags to build native tests for $__BuildOS.$__BuildArch.$__BuildType"
-    __ExtraCmakeArgs="$__ExtraCmakeArgs -DCLR_CMAKE_BUILD_TESTS=ON"
-fi
 
 build_native $__SkipCoreCLR "$__BuildArch" "$__IntermediatesDir" "$__ExtraCmakeArgs" "CoreCLR component"
 
