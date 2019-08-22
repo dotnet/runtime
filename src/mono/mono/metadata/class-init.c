0c377455bbb52e4b841e461ea30f5c4ae521f210
Zoltan Varga <vargaz@gmail.com>
2019-08-22 02:01:17 -0400

    [llvm] Mark Vector128/Vector256 types as SIMD.
    These are not currently supported, but marking them as simd helps llvm eliminate dead variables with these types, since
    XZERO is easier to eliminate than VZERO.
    
    
    Commit migrated from https://github.com/mono/mono/commit/798af6613796fcbe11cf20a33a889705483197a7

--- src/mono/mono/metadata/class-init.c
+++ src/mono/mono/metadata/class-init.c
@@ -808,7 +808,8 @@ mono_class_create_generic_inst (MonoGenericClass *gclass)
 			klass->simd_type = 1;
 	}
 #ifdef ENABLE_NETCORE
-	if (mono_is_corlib_image (gklass->image) && !strcmp (gklass->name, "Vector`1")) {
+	if (mono_is_corlib_image (gklass->image) &&
+		(!strcmp (gklass->name, "Vector`1") || !strcmp (gklass->name, "Vector128`1") || !strcmp (gklass->name, "Vector256`1"))) {
 		MonoType *etype = gclass->context.class_inst->type_argv [0];
 		if (mono_type_is_primitive (etype) && etype->type != MONO_TYPE_CHAR && etype->type != MONO_TYPE_BOOLEAN)
 			klass->simd_type = 1;
