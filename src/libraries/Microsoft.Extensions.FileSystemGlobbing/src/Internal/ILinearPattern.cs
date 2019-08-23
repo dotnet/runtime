9fc548bc4178b4a09c3233d34352138dc0fbef29
Kevin Pilch <kevinpi@microsoft.com>
2019-08-23 13:58:33 -0700

    Add XML Docs for pubternal types
    Commit migrated from https://github.com/dotnet/Extensions/commit/033ea04bab004a477acd6f023af171ee853965b8

--- src/libraries/Microsoft.Extensions.FileSystemGlobbing/src/Internal/ILinearPattern.cs
+++ src/libraries/Microsoft.Extensions.FileSystemGlobbing/src/Internal/ILinearPattern.cs
@@ -5,6 +5,10 @@ using System.Collections.Generic;
 
 namespace Microsoft.Extensions.FileSystemGlobbing.Internal
 {
+    /// <summary>
+    /// This API supports infrastructure and is not intended to be used
+    /// directly from your code. This API may change or be removed in future releases.
+    /// </summary>
     public interface ILinearPattern : IPattern
     {
         IList<IPathSegment> Segments { get; }
