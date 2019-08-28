db8842511cd8c7109265a8dca8679953822ee3ae
Jay Krell <jaykrell@microsoft.com>
2019-08-28 06:42:44 -0700

    Fix assert in x86_patch regarding second byte of Jcc instructions being 0x80-0x8F vs. 0x70-0x8F (6byte conditional branch with 32bit offset). (mono/mono#16471)
    Commit migrated from https://github.com/mono/mono/commit/c80a1eca636c82014f8fbb9873e5391cddffa9a9

--- src/mono/mono/arch/x86/x86-codegen.h
+++ src/mono/mono/arch/x86/x86-codegen.h
@@ -365,8 +365,8 @@ typedef union {
 		int disp, size = 0;	\
 		switch (*(unsigned char*)(ins)) {	\
 		case 0xe8: case 0xe9: ++size; break; /* call, jump32 */	\
-		case 0x0f: if (!(*pos >= 0x70 && *pos <= 0x8f)) assert (0);	\
-		   ++size; ++pos; break; /* prefix for 32-bit disp */	\
+		case 0x0f: if (!(*pos >= 0x80 && *pos <= 0x8f)) assert (0);	\
+		   ++size; ++pos; break; /* prefix for conditional jumps with 32-bit disp */ \
 		case 0xe0: case 0xe1: case 0xe2: /* loop */	\
 		case 0xeb: /* jump8 */	\
 		/* conditional jump opcodes */	\
