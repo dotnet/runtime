8bac7044b92991254e5d792749b8ad028b658b53
Tlakaelel Axayakatl Ceja <tlakaelel.ceja@microsoft.com>
2019-09-09 05:13:15 -0700

    Improve Error Experience in the linker (dotnet/linker#719)
    * src/ILLink.Tasks/LinkTask.cs – Increase Standard Error output logging
    importance so default verbosity is able to print it
    
    src/linker/Linker/AssemblyResolver.cs – Include a warning message for
    suspicious series of common punctuation characters in assembly names,
    although supported they are likely typos in assembly names. Warning is
    only printed upon failure.
    
    src/linker/Linker/LinkContext.cs – skip assembly if it’s null (means is
    an unresolved assembly)
    src/linker/Linker.Steps/ResolveFromXmlStep.cs – skip assembly if it’s
    null (means is an unresolved assembly)
    
    * - src/linker/Linker.Steps/ResolveFromXmlStep.cs
      add space to match repo convention
    
    -src/linker/Linker/AssemblyResolver.cs
      delete invalid chars check
      add "skipping this assembly" message
    
    * Update src/linker/Linker/LinkContext.cs to match mono repo conventions
    
    Co-Authored-By: Sven Boemer <sbomer@gmail.com>
    
    * Update AssemblyResolver.cs
    
    
    Commit migrated from https://github.com/dotnet/linker/commit/9a525c6fc83af84c001576c0e445a71dbc61990a

--- src/tools/linker/src/linker/Linker/AssemblyResolver.cs
+++ src/tools/linker/src/linker/Linker/AssemblyResolver.cs
@@ -121,8 +121,7 @@ namespace Mono.Linker {
 				} catch (AssemblyResolutionException) {
 					if (!_ignoreUnresolved)
 						throw;
-
-					_context.LogMessage ($"warning: unresolved assembly {name.Name}");
+					_context.LogMessage ($"Ignoring unresolved assembly '{name.Name}'.");
 					if (_unresolvedAssemblies == null)
 						_unresolvedAssemblies = new HashSet<string> ();
 					_unresolvedAssemblies.Add (name.Name);
