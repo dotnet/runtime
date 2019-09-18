092df323b51f7c7be8f3837fc22eb2a6f0275328
Anubhav Srivastava <SrivastavaAnubhav@users.noreply.github.com>
2019-09-18 13:31:51 -0700

    Add performance measurement to crossgen2/SuperILC (dotnet/coreclr#26742)
    Crossgen2 changes
    - Add PerfEventSource classes to log performance-related events (average time taken to compile, loading time, graph processing time, emitting time, number of nodes added to graph, and number of methods JITed)
    
    SuperILC changes
    - Add MeasurePerf flag to measure performance (with 2 warmup runs and averaged over 5 real runs)
    - Add PerfEventSourceListener class to listen to PerfEventSource events from crossgen2 and record statistics.
    - Add InputFileSearchString flag to select an input file to compile (instead of requiring a folder)
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/8cb1ac8f47c8247e3944b44f10b0ce68594e071b

---
+++ src/coreclr/src/tools/crossgen2/ILCompiler.DependencyAnalysisFramework/PerfEventSource.cs
@@ -0,0 +1,31 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Diagnostics.Tracing;
+
+/// <summary>
+/// Performance events releated to the dependency graph.
+/// </summary>
+namespace ILCompiler.DependencyAnalysisFramework
+{
+    // The event IDs here must not collide with the ones used by ReadyToRunPerfEventSource.cs
+    [EventSource(Name = "Microsoft-ILCompiler-Perf")]
+    class PerfEventSource : EventSource
+    {
+        [Event(1001, Level = EventLevel.Informational)]
+        public void GraphProcessingStart() { WriteEvent(1001); }
+        [Event(1002, Level = EventLevel.Informational)]
+        public void GraphProcessingStop() { WriteEvent(1002); }
+
+        [Event(1003, Level = EventLevel.Informational)]
+        public void DependencyAnalysisStart() { WriteEvent(1003); }
+        [Event(1004, Level = EventLevel.Informational)]
+        public void DependencyAnalysisStop() { WriteEvent(1004); }
+
+        [Event(1005, Level = EventLevel.Informational)]
+        public void AddedNodeToMarkStack() { WriteEvent(1005); }
+
+        public static PerfEventSource Log = new PerfEventSource();
+    }
+}
