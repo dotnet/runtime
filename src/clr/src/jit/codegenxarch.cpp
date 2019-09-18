772783833a86409233a4ed7d2d31c0a25a70a94a
Brian Bohe <brianbohe@gmail.com>
2019-09-18 14:13:38 -0300

    Adding comments (dotnet/coreclr#23327)
    * Adding comments to psiScope functions declarations
    
    * Adding comments on declaration/definition Compiler::compUpdateLife
    
    * Editing CodeGen::genProduceReg header comments
    
    * Editting var Compiler::lvaCount comment
    
    * Adding comments to Compiler::compChangeLife function
    
    * Adding comments when changing variable liveness
    
    * Adding comments in genSetScopeInfo()
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/728740987f4afcb8f55fe89a309471f01c62ef06

--- src/clr/src/jit/codegenxarch.cpp
+++ src/clr/src/jit/codegenxarch.cpp
@@ -4639,6 +4639,7 @@ void CodeGen::genCodeForStoreLclFld(GenTreeLclFld* tree)
     genConsumeRegs(op1);
     getEmitter()->emitInsBinary(ins_Store(targetType), emitTypeSize(tree), tree, op1);
 
+    // Updating variable liveness after instruction was emitted
     genUpdateLife(tree);
 }
 
