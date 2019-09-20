5e5cb0729eff37dcc2f639d7a68098b1750f6629
Jeremy Koritzinsky <jekoritz@microsoft.com>
2019-09-20 15:47:10 -0700

    Move ComHost generation from dotnet/sdk to Microsoft.NET.HostModel (dotnet/core-setup#8293)
    * Copy files over from dotnet/sdk.
    
    * Update JSON code for ComHost to use System.Text.Json.
    
    * Rename bundle tests folder to also make sense for other tests for Microsoft.NET.HostModel.
    
    * Add tests for Clsidmap generation.
    
    * Add RegFreeComManifest tests.
    
    * PR Feedback.
    
    
    Commit migrated from https://github.com/dotnet/core-setup/commit/7544cb6e71d3dc34c9b7e0a20a25a8bccd8e3f6e

---
+++ src/installer/test/Microsoft.NET.HostModel.Tests/Microsoft.NET.HostModel.ComHost.Tests/TestDirectory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace Microsoft.NET.HostModel.ComHost.Tests
+{
+    internal class TestDirectory : IDisposable
+    {
+        public string Path { get; private set; }
+
+        private TestDirectory(string path)
+        {
+            Path = path;
+            Directory.CreateDirectory(path);
+        }
+
+        public static TestDirectory Create([CallerMemberName] string callingMethod = "")
+        {
+            string path = System.IO.Path.Combine(
+                System.IO.Path.GetTempPath(),
+                "dotNetSdkUnitTest_" + callingMethod + (Guid.NewGuid().ToString().Substring(0, 8)));
+            return new TestDirectory(path);
+        }
+
+        public void Dispose()
+        {
+            if (Directory.Exists(Path))
+            {
+                Directory.Delete(Path, true);
+            }
+        }
+    }
+}
