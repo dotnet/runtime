df1301be33c232a66841a3781883f08f67b4e43f
Sinan Kaya <41809318+franksinankaya@users.noreply.github.com>
2019-09-23 19:23:57 -0400

    Replace gtSsaNum with GetSsaNum (dotnet/coreclr#26836)
    * find ./ -type f -exec sed -i -e 's/\<gtSsaNum\>/GetSsaNum()/g' {} \;
    
    * format patch
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/eeea730d09d5a7eeaf8c5bb036319ebec85819ea

--- src/coreclr/src/jit/compiler.hpp
+++ src/coreclr/src/jit/compiler.hpp
@@ -4180,7 +4180,7 @@ unsigned Compiler::GetSsaNumForLocalVarDef(GenTree* lcl)
     }
     else
     {
-        return lcl->AsLclVarCommon()->gtSsaNum;
+        return lcl->AsLclVarCommon()->GetSsaNum();
     }
 }
 
