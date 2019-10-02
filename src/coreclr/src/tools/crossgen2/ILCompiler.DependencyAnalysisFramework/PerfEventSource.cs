3b3764d2500b82861cb865bc7c69a2951cee8f21
Anubhav Srivastava <SrivastavaAnubhav@users.noreply.github.com>
2019-10-02 14:04:51 -0700

    Change PerfEventSource to be able to use 'using'. (dotnet/coreclr#26981)
    * Change and reorganize PerfEventSource to be able to use using.
    
    
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/4e680b3d82effacd72d8394eee9cf01b501b9207

--- src/coreclr/src/tools/crossgen2/ILCompiler.DependencyAnalysisFramework/PerfEventSource.cs
+++ src/coreclr/src/tools/crossgen2/ILCompiler.DependencyAnalysisFramework/PerfEventSource.cs
@@ -2,30 +2,67 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Diagnostics.Tracing;
 
 /// <summary>
-/// Performance events releated to the dependency graph.
+/// Performance events specific to the dependency graph.
 /// </summary>
 namespace ILCompiler.DependencyAnalysisFramework
 {
-    // The event IDs here must not collide with the ones used by ReadyToRunPerfEventSource.cs
     [EventSource(Name = "Microsoft-ILCompiler-Perf")]
-    class PerfEventSource : EventSource
+    public class PerfEventSource : EventSource
     {
+        private PerfEventSource() { }
+
+        public static PerfEventSource Log = new PerfEventSource();
+
+        public struct StartStopEvents : IDisposable
+        {
+            private Action _stopAction;
+
+            private StartStopEvents(Action stopAction)
+            {
+                _stopAction = stopAction;
+            }
+
+            public void Dispose()
+            {
+                _stopAction?.Invoke();
+            }
+
+            public static StartStopEvents GraphProcessingEvents()
+            {
+                if (!Log.IsEnabled())
+                    return new StartStopEvents();
+
+                Log.GraphProcessingStart();
+                return new StartStopEvents(Log.GraphProcessingStop);
+            }
+
+            public static StartStopEvents DependencyAnalysisEvents()
+            {
+                if (!Log.IsEnabled())
+                    return new StartStopEvents();
+
+                Log.DependencyAnalysisStart();
+                return new StartStopEvents(Log.DependencyAnalysisStop);
+            }
+        }
+
+        // The event IDs here must not collide with the ones used by ReadyToRunPerfEventSource.cs
         [Event(1001, Level = EventLevel.Informational)]
-        public void GraphProcessingStart() { WriteEvent(1001); }
+        private void GraphProcessingStart() { WriteEvent(1001); }
         [Event(1002, Level = EventLevel.Informational)]
-        public void GraphProcessingStop() { WriteEvent(1002); }
+        private void GraphProcessingStop() { WriteEvent(1002); }
 
         [Event(1003, Level = EventLevel.Informational)]
-        public void DependencyAnalysisStart() { WriteEvent(1003); }
+        private void DependencyAnalysisStart() { WriteEvent(1003); }
         [Event(1004, Level = EventLevel.Informational)]
-        public void DependencyAnalysisStop() { WriteEvent(1004); }
+        private void DependencyAnalysisStop() { WriteEvent(1004); }
 
         [Event(1005, Level = EventLevel.Informational)]
         public void AddedNodeToMarkStack() { WriteEvent(1005); }
-
-        public static PerfEventSource Log = new PerfEventSource();
     }
+
 }
