82ed48cfbfc478c71890d60f993037d1208fb3c4
Sinan Kaya <41809318+franksinankaya@users.noreply.github.com>
2019-10-04 15:23:39 -0400

    Rename lvOtherReg (#27031)
    * find src/jit -type f -exec sed -i -e 's/->lvOtherReg/->GetOtherReg()/g' {} \;
    
    * Format branch

--- src/jit/scopeinfo.cpp
+++ src/jit/scopeinfo.cpp
@@ -381,11 +381,11 @@ void CodeGenInterface::siVarLoc::siFillRegisterVarLoc(
 #if !CPU_HAS_FP_SUPPORT
         case TYP_DOUBLE:
 #endif
-            if (varDsc->lvOtherReg != REG_STK)
+            if (varDsc->GetOtherReg() != REG_STK)
             {
                 this->vlType            = VLT_REG_REG;
                 this->vlRegReg.vlrrReg1 = varDsc->GetRegNum();
-                this->vlRegReg.vlrrReg2 = varDsc->lvOtherReg;
+                this->vlRegReg.vlrrReg2 = varDsc->GetOtherReg();
             }
             else
             {
@@ -1802,7 +1802,7 @@ void CodeGen::psiMoveToReg(unsigned varNum, regNumber reg, regNumber otherReg)
         // Grab the assigned registers.
 
         reg      = compiler->lvaTable[varNum].GetRegNum();
-        otherReg = compiler->lvaTable[varNum].lvOtherReg;
+        otherReg = compiler->lvaTable[varNum].GetOtherReg();
     }
 
     psiScope* scope;
