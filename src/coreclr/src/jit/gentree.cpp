47ccf26d547d3706ef37df149ee4d13ba1dcfc9e
Sinan Kaya <41809318+franksinankaya@users.noreply.github.com>
2019-10-07 13:38:39 -0400

    Rename isSplit (dotnet/coreclr#27011)
    * __declspec(property(get = getIsSplit, put = setIsSplit)) bool isSplit;
    
    * Format patch
    
    * SetSplit
    
    * GetSplit
    
    * Forgotten conversions
    
    * Add more forgotten
    
    * Rename agein
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/601a4ae2a1a17e43c2f09952e51e16176090258a

--- src/coreclr/src/jit/gentree.cpp
+++ src/coreclr/src/jit/gentree.cpp
@@ -11339,7 +11339,7 @@ void Compiler::gtGetArgMsg(
         else
         {
 #ifdef _TARGET_ARM_
-            if (curArgTabEntry->isSplit)
+            if (curArgTabEntry->IsSplit())
             {
                 regNumber firstReg = curArgTabEntry->regNum;
                 if (listCount == -1)
@@ -11469,7 +11469,7 @@ void Compiler::gtGetLateArgMsg(
             sprintf_s(bufp, bufLength, "this in %s%c", compRegVarName(argReg), 0);
         }
 #ifdef _TARGET_ARM_
-        else if (curArgTabEntry->isSplit)
+        else if (curArgTabEntry->IsSplit())
         {
             regNumber firstReg = curArgTabEntry->regNum;
             unsigned  argNum   = curArgTabEntry->argNum;
