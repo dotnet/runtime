8df2e7b38211e0a2b6bad706a473f181ec1cb3fb
Sinan Kaya <41809318+franksinankaya@users.noreply.github.com>
2019-10-08 16:09:24 -0700

    Rename genAlignLoops (#27030)
    * find src/jit -type f -exec sed -i -e 's/.*declspec.*genAlignLoops.*//g' {} \;
    
    * Go back to doAlignLoops
    
    * Rename to ShouldAlignLoops

--- src/jit/codegeninterface.h
+++ src/jit/codegeninterface.h
@@ -75,12 +75,12 @@ public:
     //-------------------------------------------------------------------------
     //  The following property indicates whether to align loops.
     //  (Used to avoid effects of loop alignment when diagnosing perf issues.)
-    __declspec(property(get = doAlignLoops, put = setAlignLoops)) bool genAlignLoops;
-    bool doAlignLoops()
+
+    bool ShouldAlignLoops()
     {
         return m_genAlignLoops;
     }
-    void setAlignLoops(bool value)
+    void SetAlignLoops(bool value)
     {
         m_genAlignLoops = value;
     }
