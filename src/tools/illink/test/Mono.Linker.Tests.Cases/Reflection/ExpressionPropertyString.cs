624e54e69b5a86c6e0d4b330f30ea10e62ac20d0
Michael Voorhees <michaelv@unity3d.com>
2019-10-11 11:14:05 -0400

    Test fixes
    Tests should use the [Reference] attribute to specify additional references.  This is the established pattern for doing this.  Not using it was causing problems with our test framework
    
    ExpressionPropertyString needs to use Roslyn to compile.  Explicitly request roslyn so that the test works when ran on Windows w/ .NET framework where the built in CodeDomProvider doesn't allow for properties that only define `{get}`
    
    Update `RuntimeReflectionExtensionsCalls` and `ExpressionPropertyString` to work with or without unreachable bodies enabled.
    
    Commit migrated from https://github.com/dotnet/linker/commit/fa8d848cfc405cc66c22f2dab486ccd86ef49965

--- src/tools/illink/test/Mono.Linker.Tests.Cases/Reflection/ExpressionPropertyString.cs
+++ src/tools/illink/test/Mono.Linker.Tests.Cases/Reflection/ExpressionPropertyString.cs
@@ -4,25 +4,34 @@ using Mono.Linker.Tests.Cases.Expectations.Metadata;
 
 namespace Mono.Linker.Tests.Cases.Reflection
 {
-	[SetupCompileArgument ("/r:System.Core.dll")]
+	// Explicitly use roslyn to try and get a compiler that supports defining `TestOnlyStatic2` without a setter
+	[SetupCSharpCompilerToUse ("csc")]
+	[Reference ("System.Core.dll")]
 	public class ExpressionPropertyString
 	{
 		public static void Main ()
 		{
-			var e1 = Expression.Property (null, typeof (ExpressionPropertyString), "TestOnlyStatic1");
-			var e2 = Expression.Property (null, typeof (ExpressionPropertyString), "TestOnlyStatic2");
+			// So that this test works with or without unreachable bodies
+			new Foo ();
+			
+			var e1 = Expression.Property (null, typeof (Foo), "TestOnlyStatic1");
+			var e2 = Expression.Property (null, typeof (Foo), "TestOnlyStatic2");
 
-			var e3 = Expression.Property (Expression.Parameter (typeof(int), "somename"), typeof (ExpressionPropertyString), "TestName1");
+			var e3 = Expression.Property (Expression.Parameter (typeof(int), "somename"), typeof (Foo), "TestName1");
 		}
 
-		[Kept]
-		[KeptBackingField]
-		private static int TestOnlyStatic1 { [Kept] get; [Kept] set; }
+		[KeptMember (".ctor()")]
+		class Foo
+		{
+			[Kept]
+			[KeptBackingField]
+			private static int TestOnlyStatic1 { [Kept] get; [Kept] set; }
 
-		private int TestOnlyStatic2 { get; }
+			private int TestOnlyStatic2 { get; }
 
-		[Kept]
-		[KeptBackingField]
-		private int TestName1 { [Kept] get; }
+			[Kept]
+			[KeptBackingField]
+			private int TestName1 { [Kept] get; }
+		}
 	}
 }
