7e1125c0339dff5747162babe73ed09409f18ba0
Tanner Gooding <tagoo@outlook.com>
2019-10-17 19:31:25 -0700

    Improve codegen for IsNan (#27272)
    * Updating double.IsNaN and float.IsNaN to just `return x != x`
    
    * Updating x86 codegen to optimize `x != x` for floating-point
    
    * Changing the isnan optimization to only check the operand assigned registers

--- src/jit/codegenlinear.cpp
+++ src/jit/codegenlinear.cpp
@@ -2304,12 +2304,26 @@ void CodeGen::genCodeForJumpTrue(GenTreeOp* jtrue)
     assert(compiler->compCurBB->bbJumpKind == BBJ_COND);
     assert(jtrue->OperIs(GT_JTRUE));
 
-    GenCondition condition = GenCondition::FromRelop(jtrue->gtGetOp1());
+    GenTreeOp*   relop     = jtrue->gtGetOp1()->AsOp();
+    GenCondition condition = GenCondition::FromRelop(relop);
 
     if (condition.PreferSwap())
     {
         condition = GenCondition::Swap(condition);
     }
+
+#if defined(_TARGET_XARCH_)
+    if ((condition.GetCode() == GenCondition::FNEU) &&
+        (relop->gtGetOp1()->GetRegNum() == relop->gtGetOp2()->GetRegNum()))
+    {
+        // For floating point, `x != x` is a common way of
+        // checking for NaN. So, in the case where both
+        // operands are the same, we can optimize codegen
+        // to only do a single check.
+
+        condition = GenCondition(GenCondition::P);
+    }
+#endif
 
     inst_JCC(condition, compiler->compCurBB->bbJumpDest);
 }
