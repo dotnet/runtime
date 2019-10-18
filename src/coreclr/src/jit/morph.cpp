5abb6dfa816cf7c0a58f89334c969f2bdd995cd0
Sinan Kaya <41809318+franksinankaya@users.noreply.github.com>
2019-10-18 11:01:08 -0700

    Replace gtIndex. with AsIndex()-> (dotnet/coreclr#27257)
    * find src/jit -type f -exec sed -i -e 's/gtIndex\./AsIndex()\./g' {} \;
    
    * Format patch
    
    
    Commit migrated from https://github.com/dotnet/coreclr/commit/f2cbf9100e5ace69557eea679a9685e09a491bd6

--- src/coreclr/src/jit/morph.cpp
+++ src/coreclr/src/jit/morph.cpp
@@ -5346,8 +5346,8 @@ GenTree* Compiler::fgMorphArrayIndex(GenTree* tree)
     GenTreeIndex* asIndex = tree->AsIndex();
 
     var_types            elemTyp        = tree->TypeGet();
-    unsigned             elemSize       = tree->gtIndex.gtIndElemSize;
-    CORINFO_CLASS_HANDLE elemStructType = tree->gtIndex.gtStructElemClass;
+    unsigned             elemSize       = tree->AsIndex()->gtIndElemSize;
+    CORINFO_CLASS_HANDLE elemStructType = tree->AsIndex()->gtStructElemClass;
 
     noway_assert(elemTyp != TYP_STRUCT || elemStructType != nullptr);
 
