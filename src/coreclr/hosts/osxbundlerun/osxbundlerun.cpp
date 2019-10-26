9a60a6e7e8622adfb1848a7af7d42207f29ec3c4
pi1024e <49824824+pi1024e@users.noreply.github.com>
2019-10-26 10:56:03 -0400

    Removing unneeded function calls and empty bodies (#27420)

--- src/coreclr/hosts/osxbundlerun/osxbundlerun.cpp
+++ src/coreclr/hosts/osxbundlerun/osxbundlerun.cpp
@@ -26,7 +26,7 @@
 #include <unistd.h>
 #include <sys/stat.h>
 
-int corerun(const int argc, const char* argv[])
+int main(const int argc, const char* argv[])
 {
     // Make sure we have a full path for argv[0].
     std::string argv0AbsolutePath;
@@ -82,9 +82,4 @@ int corerun(const int argc, const char* argv[])
                             managedAssemblyArgv);
 
     return exitCode;
-}
-
-int main(const int argc, const char* argv[])
-{
-    return corerun(argc, argv);
 }
