6730feb28308d5bba5b9b4f4680b8f21d444089b
Marek Safar <marek.safar@gmail.com>
2019-10-30 00:13:31 +0100

    Expand handling of TypeConverterAttribute on all elements
    Fixes dotnet/linker#800
    
    
    Commit migrated from https://github.com/dotnet/linker/commit/1bfc6af94b12ef182505db9e83aa66c0cd1d5e8f

---
+++ src/tools/linker/test/Mono.Linker.Tests.Cases/ComponentModel/TypeConverterOnMembers.cs
@@ -0,0 +1,75 @@
+using System;
+using System.ComponentModel;
+using System.Globalization;
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
+using Mono.Linker.Tests.Cases.Expectations.Metadata;
+
+namespace Mono.Linker.Tests.Cases.ComponentModel
+{
+	[Kept]
+	class OnProperty
+	{
+		[Kept]
+		public OnProperty ()
+		{
+		}
+
+		[TypeConverter (typeof (Custom1))]
+
+		[Kept]
+		[KeptAttributeAttribute (typeof (TypeConverterAttribute))]
+		[KeptBackingField]
+		public string Foo { [Kept] get; set; }
+
+		[Kept]
+		[KeptBaseType (typeof (TypeConverter))]
+		class Custom1 : TypeConverter
+		{
+			[Kept]			
+			public Custom1 (Type type)
+			{
+			}
+		}
+	}
+
+	[Kept]
+	class OnField
+	{
+		[Kept]
+		public OnField ()
+		{
+		}
+
+		[TypeConverter (typeof (Custom2))]
+
+		[Kept]
+		[KeptAttributeAttribute (typeof (TypeConverterAttribute))]
+		public object Field;
+
+		[Kept]
+		[KeptBaseType (typeof (TypeConverter))]
+		class Custom2 : TypeConverter
+		{
+			[Kept]
+			public Custom2 ()
+			{
+			}
+
+			[Kept]
+			public override object ConvertFrom (ITypeDescriptorContext context, CultureInfo culture, object value)
+			{
+				return "test";
+			}
+		}
+	}
+
+	[Reference ("System.dll")]
+	public class TypeConverterOnMembers
+	{		
+		public static void Main ()
+		{
+			var r1 = new OnProperty ().Foo;
+			var r2 = new OnField ().Field;
+		}
+	}
+}
