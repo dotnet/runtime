f98688bc231839e210e0f1e598e7ea62663aeaf2
Andrew Au <andrewau@microsoft.com>
2019-11-21 17:02:12 -0800

    Adding missed files

---
+++ src/coreclr/src/tools/r2rdump/IAssemblyResolver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace R2RDump
+{
+    public interface IAssemblyResolver
+    {
+        string FindAssembly(string name, string filename);
+        // TODO (refactoring) - signature formatting options should be independent of assembly resolver
+        bool Naked { get; }
+        bool SignatureBinary { get; }
+        bool InlineSignatureBinary { get; }
+    }
+}
