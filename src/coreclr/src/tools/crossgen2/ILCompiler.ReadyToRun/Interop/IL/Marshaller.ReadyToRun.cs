abe369abdf5c1449f337bf193645402ccb24389c
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2019-11-24 23:24:24 +0100

    Update Marshaller.ReadyToRun.cs

--- src/coreclr/src/tools/crossgen2/ILCompiler.ReadyToRun/Interop/IL/Marshaller.ReadyToRun.cs
+++ src/coreclr/src/tools/crossgen2/ILCompiler.ReadyToRun/Interop/IL/Marshaller.ReadyToRun.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Debug = System.Diagnostics.Debug;
+
 namespace Internal.TypeSystem.Interop
 {
     partial class Marshaller
