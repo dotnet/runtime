7e9a4a4ebad539181ff66dfd1606ed788f020829
Stephen Toub <stoub@microsoft.com>
2019-11-24 17:15:11 -0500

    Address some IDEXXX analysis warnings

--- src/libraries/System.Private.CoreLib/src/System/TimeZoneInfo.cs
+++ src/libraries/System.Private.CoreLib/src/System/TimeZoneInfo.cs
@@ -5,7 +5,6 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Runtime.Serialization;
 using System.Threading;
