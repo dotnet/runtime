df81dbfc42e94757533e956e26c97ac6ac1dacf9
John <johnkellyoxford@gmail.com>
2019-11-26 18:36:44 +0000

    Add sequential layout to match [U]Int32

--- src/libraries/System.Private.CoreLib/src/System/UIntPtr.cs
+++ src/libraries/System.Private.CoreLib/src/System/UIntPtr.cs
@@ -18,6 +18,7 @@ namespace System
 {
     [Serializable]
     [CLSCompliant(false)]
+    [StructLayout(LayoutKind.Sequential)]
     [System.Runtime.CompilerServices.TypeForwardedFrom("mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089")]
     public readonly struct UIntPtr : IEquatable<UIntPtr>, ISerializable
     {
