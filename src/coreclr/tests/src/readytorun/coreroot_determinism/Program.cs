9510b57a5826120d901918bf6958548ef74e7448
Anubhav Srivastava <t-ansri@microsoft.com>
2019-12-02 12:12:14 -0800

    Check files exist before diff'ing them. Remove output directory if it exists before creating a new one.

--- src/coreclr/tests/src/readytorun/coreroot_determinism/Program.cs
+++ src/coreclr/tests/src/readytorun/coreroot_determinism/Program.cs
@@ -3,8 +3,10 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
 
 internal class Program
@@ -17,14 +19,27 @@ internal class Program
     public static int CompareDLLs(string folder1, string folder2)
     {
         int result = 100;
-        foreach (string filepath1 in Directory.EnumerateFiles(folder1, "*.dll"))
+
+        // Check for files that failed compilation with one of the seeds but not the other
+        HashSet<string> uniqueFilenames = new HashSet<string>(Directory.GetFiles(folder1, "*.dll").Select(Path.GetFileName));
+        uniqueFilenames.SymmetricExceptWith(Directory.GetFiles(folder2, "*.dll").Select(Path.GetFileName));
+        foreach (string uniqueFilename in uniqueFilenames)
         {
-            byte[] file1 = File.ReadAllBytes(filepath1);
-            byte[] file2 = File.ReadAllBytes(Path.Combine(folder2, Path.GetFileName(filepath1)));
+            Console.WriteLine($"{uniqueFilename} was found in only one of the output folders.");
+            result = 1;
+        }
+
+        foreach (string filename in Directory.GetFiles(folder1, "*.dll").Select(Path.GetFileName))
+        {
+            if (uniqueFilenames.Contains(filename))
+                continue;
+
+            byte[] file1 = File.ReadAllBytes(Path.Combine(folder1, Path.GetFileName(filename)));
+            byte[] file2 = File.ReadAllBytes(Path.Combine(folder2, Path.GetFileName(filename)));
 
             if (file1.Length != file2.Length)
             {
-                Console.WriteLine(filepath1);
+                Console.WriteLine(filename);
                 Console.WriteLine($"Expected ReadyToRun'd files to be identical but they have different sizes ({file1.Length} and {file2.Length})");
                 result = 1;
             }
@@ -33,7 +48,7 @@ internal class Program
             {
                 if (file1[i] != file2[i] && !IsTimestampByte(i))
                 {
-                    Console.WriteLine(filepath1);
+                    Console.WriteLine(filename);
                     Console.WriteLine($"Difference at non-timestamp byte {i}");
                     result = 1;
                 }
@@ -51,8 +66,12 @@ internal class Program
         string coreRootPath = Environment.GetEnvironmentVariable("Core_Root");
         string superIlcPath = Path.Combine(coreRootPath, "ReadyToRun.SuperIlc", OSExeSuffix("ReadyToRun.SuperIlc"));
 
-
+        Console.WriteLine($"================================== Compiling with seed {seed} ==================================");
         Environment.SetEnvironmentVariable("CoreRT_DeterminismSeed", seed.ToString());
+        if (Directory.Exists(outDir))
+        {
+            Directory.Delete(outDir, true);
+        }
         Directory.CreateDirectory(outDir);
         ProcessStartInfo processStartInfo = new ProcessStartInfo(superIlcPath, $"compile-directory -cr {coreRootPath} -in {coreRootPath} --nojit --noexe --large-bubble --release --nocleanup -out {outDir}");
         Process.Start(processStartInfo).WaitForExit();
