38f470959d3a765df6b63d6037f1a3060eed9b76
Viktor Hofer <viktor.hofer@microsoft.com>
2019-12-09 12:36:34 +0100

    Invoke dotnet SDK in the calling script
    Add the powershell script back and invoke the SDK in the corresponding
    root script.

--- dotnet.cmd
+++ dotnet.cmd
@@ -1,2 +1,25 @@
 @echo off
-powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0dotnet.ps1""" %*"
+SETLOCAL enabledelayedexpansion
+
+if not "%~1%" == "dotnetPath" (
+  powershell -ExecutionPolicy ByPass -NoProfile -Command "& { . '%~dp0dotnet.ps1'; Get-DotNetPath %* }"
+  goto:eof
+)
+
+:: Clear the 'Platform' env variable for this session, as it's a per-project setting within the build, and
+:: misleading value (such as 'MCD' in HP PCs) may lead to build breakage (issue: #69).
+set Platform=
+
+:: Don't resolve runtime, shared framework, or SDK from other locations to ensure build determinism
+set DOTNET_MULTILEVEL_LOOKUP=0
+
+:: Disable first run since we want to control all package sources
+set DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
+
+set dotnetPath="%~2\dotnet.exe"
+: Remove first two arguments
+set params=%*
+set params=!params:%1 =!
+set params=!params:%2=!
+
+call "%dotnetPath%"!params!
--- dotnet.ps1
+++ dotnet.ps1
@@ -1,16 +1,23 @@
+function Get-DotNetPath() {
+  Invoke-Expression "& `"$PSScriptRoot\dotnet.cmd`" dotnetPath $dotnetPath $args"
+}
+
 . $PSScriptRoot\eng\common\tools.ps1
-
-# Clear the 'Platform' env variable for this session, as it's a per-project setting within the build, and
-# misleading value (such as 'MCD' in HP PCs) may lead to build breakage (issue: #69).
-$env:Platform=
-
-# Don't resolve runtime, shared framework, or SDK from other locations to ensure build determinism
-$env:DOTNET_MULTILEVEL_LOOKUP=0
-
-# Disable first run since we want to control all package sources
-$env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
-
 $dotnetPath=InitializeDotNetCli($true)
 
-Invoke-Expression "& $dotnetPath\dotnet.exe $args"
-exit $lastExitCode
+# Invoke dotnet.exe if script hasn't been dot-sourced
+if ($MyInvocation.InvocationName -ne ".") {
+  # Clear the 'Platform' env variable for this session, as it's a per-project setting within the build, and
+  # misleading value (such as 'MCD' in HP PCs) may lead to build breakage (issue: #69).
+  $env:Platform=
+
+  # Don't resolve runtime, shared framework, or SDK from other locations to ensure build determinism
+  $env:DOTNET_MULTILEVEL_LOOKUP=0
+
+  # Disable first run since we want to control all package sources
+  $env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
+
+  Invoke-Expression "& $dotnetPath\dotnet.exe $args"
+
+  exit $lastExitCode
+}
