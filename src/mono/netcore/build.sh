b2a7a37cc71cd6b9c10ad6907eddc19202cadc4e
Egor Bogatov <egorbo@gmail.com>
2019-12-09 16:55:44 +0300

    [netcore] Enable more tests for interpreter and llvm on CI (mono/mono#17957)
    * Enable more tests
    
    * fix build?
    
    * forward XUNIT_MONO_ENV_OPTIONS
    
    * increase timeout
    
    * ignore some tests for interpreter
    
    * ignore more tests
    
    * move interp specific issues to a separate rsp
    
    * fix CI
    
    * fix CI
    
    * undo build.sh chmod
    
    * revert chmod for xunit-summary-py
    
    * improve xunit summary
    
    * Ignore the whole System.Net.Tests namespace
    
    * Ignore System.Text.Json
    
    * disable the whole BinaryFormatterTests
    
    * Disable System.Net.Http.Functional.Tests
    
    
    Commit migrated from https://github.com/mono/mono/commit/2befdcfa52c76584d750092ced62c1ea8bc570f8

--- src/mono/netcore/build.sh
+++ src/mono/netcore/build.sh
@@ -23,6 +23,7 @@ usage()
   echo "Actions:"
   echo "  --pack                     Package build outputs into NuGet packages"
   echo "  --test                     Run all unit tests in the solution (short: -t)"
+  echo "  --interpreter              Run tests with interpreter"
   echo "  --rebuild                  Run ../.autogen.sh"
   echo "  --llvm                     Enable LLVM support"
   echo "  --skipnative               Do not build runtime"
@@ -36,6 +37,8 @@ usage()
 
 pack=false
 configuration='Debug'
+test_mono_flags=''
+test_xunit_flags=''
 properties=''
 force_rebuild=false
 test=false
@@ -63,6 +66,10 @@ while [[ $# > 0 ]]; do
     -test|-t)
       test=true
       ;;
+    -interpreter)
+      test_mono_flags="$test_mono_flags --interpreter"
+      test_xunit_flags="$test_xunit_flags @../../../../CoreFX.issues_interpreter.rsp -parallel none"
+      ;;
     -rebuild)
       force_rebuild=true
       ;;
@@ -74,6 +81,7 @@ while [[ $# > 0 ]]; do
       ;;
     -llvm)
       llvm=true
+      test_mono_flags="$test_mono_flags --llvm"
       ;;
     -ci)
       ci=true
@@ -144,8 +152,8 @@ fi
 if [ "$test" = "true" ]; then
   make update-tests-corefx || (Write-PipelineTelemetryError -c "tests-download" -e 1 "Error downloading tests" && exit 1)
   if [ "$ci" = "true" ]; then
-    make run-tests-corefx USE_TIMEOUT=1 || (Write-PipelineTelemetryError -c "tests" -e 1 "Error running tests" && exit 1)
+    make run-tests-corefx XUNIT_MONO_ENV_OPTIONS="$test_mono_flags" XUNIT_ARGS="$test_xunit_flags" USE_TIMEOUT=1 || (Write-PipelineTelemetryError -c "tests" -e 1 "Error running tests" && exit 1)
   else
-    make run-tests-corefx
+    make run-tests-corefx XUNIT_MONO_ENV_OPTIONS="$test_mono_flags" XUNIT_ARGS="$test_xunit_flags"
   fi
 fi
--- src/mono/netcore/xunit-summary.py
+++ src/mono/netcore/xunit-summary.py
@@ -11,7 +11,7 @@ if len(sys.argv) < 1:
     sys.exit(1)
 test_dir = sys.argv [1]
 
-class TestResults():
+class AssemblyTestResults():
     def __init__(self, name, total, passed, failed, skipped, errors, time):
         self.name = name
         self.total = total
@@ -20,9 +20,14 @@ class TestResults():
         self.skipped = skipped
         self.time = time
 
-print("")
+class TestInfo():
+    def __init__(self, name, time):
+        self.name = name
+        self.time = time
 
-tests = []
+test_assemblies = []
+test_items = []
+
 for testfile in glob.glob(test_dir + "/*-xunit.xml"):
     assemblies = ET.parse(testfile).getroot()
     for assembly in assemblies:
@@ -30,30 +35,39 @@ for testfile in glob.glob(test_dir + "/*-xunit.xml"):
         if test_name is None:
             print("WARNING: %s has no tests!" % ntpath.basename(testfile))
             continue
-        tests.append(TestResults(test_name, 
+        test_assemblies.append(AssemblyTestResults(test_name, 
             int(assembly.attrib["total"]), 
             int(assembly.attrib["passed"]), 
             int(assembly.attrib["failed"]), 
             int(assembly.attrib["skipped"]), 
             int(assembly.attrib["errors"]), 
             float(assembly.attrib["time"])))
+        for collection in assembly.iter("collection"):
+            for test in collection.iter("test"):
+                test_items.append(TestInfo(test.attrib["name"], 
+                    float(test.attrib["time"])))
 
-# sort by name
-tests.sort(key=lambda item: item.name)
+test_assemblies.sort(key=lambda item: (item.failed, item.name), reverse=True)
+test_items.sort(key=lambda item: (item.time), reverse=True)
 
 print("")
+print("")
 print("=" * 105)
-for t in tests:
+for t in test_assemblies:
     #if t.failed > 0: # uncomment to list only test suits with failures
         print("{0:<60}  Total:{1:<6}  Failed:{2:<6}  Time:{3} sec".format(t.name, t.total, t.failed, round(t.time, 1)))
 print("=" * 105)
-
 print("")
-print("Total test suites:    %d" % len(tests))
-print("Total tests run:      %d" % sum(x.total for x in tests))
-print("Total tests passed:   %d" % sum(x.passed for x in tests))
-print("Total tests failed:   %d" % sum(x.failed for x in tests))
-print("Total tests skipped:  %d" % sum(x.skipped for x in tests))
-print("Total duration:       %d min" % (sum(x.time for x in tests) / 60))
+print("Total test suites:    %d" % len(test_assemblies))
+print("Total tests run:      %d" % sum(x.total for x in test_assemblies))
+print("Total tests passed:   %d" % sum(x.passed for x in test_assemblies))
+print("Total tests failed:   %d" % sum(x.failed for x in test_assemblies))
+print("Total tests skipped:  %d" % sum(x.skipped for x in test_assemblies))
+print("Total duration:       %d min" % (sum(x.time for x in test_assemblies) / 60))
 print("")
-
+print("")
+print("Top 20 slowest tests:")
+print("=" * 105)
+for t in test_items[:20]:
+    print("{0:<89}  Time:{1} sec".format(t.name[:88], round(t.time, 1)))
+print("")
