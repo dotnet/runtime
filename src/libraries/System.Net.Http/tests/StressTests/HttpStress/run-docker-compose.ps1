269f7c639a409d8e8205531491ca645179600129
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2019-12-10 16:25:40 +0000

    fix docker tag argument passing

--- src/libraries/System.Net.Http/tests/StressTests/HttpStress/run-docker-compose.ps1
+++ src/libraries/System.Net.Http/tests/StressTests/HttpStress/run-docker-compose.ps1
@@ -7,12 +7,12 @@ Param(
     [switch][Alias('w')]$useWindowsContainers, # Use windows containers, if available
     [switch][Alias('b')]$buildCurrentLibraries, # Drives the stress test using libraries built from current source
     [switch][Alias('o')]$buildOnly, # Build, but do not run the stress app
-    [string][Alias('t')]$sdkImageName = "dotnet-sdk-libraries-current", # Name of the sdk image name, if built from source.
+    [string][Alias('t')]$sdkImageName, # Name of the sdk image name, if built from source.
     [string]$clientStressArgs = "",
     [string]$serverStressArgs = ""
 )
 
-$REPO_ROOT_DIR=$(git -C "$PSScriptRoot" rev-parse --show-toplevel)
+$REPO_ROOT_DIR = $(git -C "$PSScriptRoot" rev-parse --show-toplevel)
 $COMPOSE_FILE = "$PSScriptRoot/docker-compose.yml"
 
 # Build runtime libraries and place in a docker image
@@ -33,7 +33,7 @@ if ($buildCurrentLibraries)
 # Dockerize the stress app using docker-compose
 
 $BUILD_ARGS = ""
-if ($buildCurrentLibraries)
+if (![string]::IsNullOrEmpty($sdkImageName))
 {
     $BUILD_ARGS += " --build-arg SDK_BASE_IMAGE=$sdkImageName"
 }
