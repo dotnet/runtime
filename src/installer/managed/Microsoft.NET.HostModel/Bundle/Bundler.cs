226036b88388e786724d6d905b14eb11a70e8d0c
Felipe Martínez <pipernene@gmail.com>
2019-12-10 20:00:55 +0100

    Simplify code

--- src/installer/managed/Microsoft.NET.HostModel/Bundle/Bundler.cs
+++ src/installer/managed/Microsoft.NET.HostModel/Bundle/Bundler.cs
@@ -165,7 +165,7 @@ namespace Microsoft.NET.HostModel.Bundle
             if (duplicateGroups.Any())
             {
                 throw new ArgumentException("Invalid input specification: Found multiple entries with the same BundleRelativePath:"
-                    + string.Join("\n", duplicateGroups.SelectMany(o => o).Select(o => "  " + o.ToString())));
+                    + string.Join("\n", duplicateGroups.SelectMany(o => o)));
             }
 
             string bundlePath = Path.Combine(OutputDir, HostName);
