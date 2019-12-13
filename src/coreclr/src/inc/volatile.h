c6bb671301545a37a96f6ecb24e9d215475939f6
Sinan Kaya <sinan.kaya@microsoft.com>
2019-12-13 06:02:30 +0000

    inc/volatile

--- src/coreclr/src/inc/volatile.h
+++ src/coreclr/src/inc/volatile.h
@@ -76,7 +76,7 @@
 #if defined(__GNUC__)
 #if defined(_ARM_) || defined(_ARM64_)
 // This is functionally equivalent to the MemoryBarrier() macro used on ARM on Windows.
-#define VOLATILE_MEMORY_BARRIER() asm volatile ("dmb ish" : : : "memory")
+#define VOLATILE_MEMORY_BARRIER() __sync_synchronize()
 #else
 //
 // For GCC, we prevent reordering by the compiler by inserting the following after a volatile
