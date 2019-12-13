a0e070459bc28d3abbbdec89ea8dee3507c8aec7
Viktor Hofer <viktor.hofer@microsoft.com>
2019-12-13 14:28:43 +0100

    Revert another file change mistake

--- src/coreclr/tests/src/performance/Scenario/JitBench/Utilities/FileTasks.cs
+++ src/coreclr/tests/src/performance/Scenario/JitBench/Utilities/FileTasks.cs
@@ -15,8 +15,7 @@ namespace JitBench
     {
         public async static Task DownloadAndUnzip(string remotePath, string localExpandedDirPath, ITestOutputHelper output, bool deleteTempFiles=true)
         {
-            string tempFileNameBase = Guid.NewGuid().ToString();
-            string tempDownloadPath = Path.Combine(Path.GetTempPath(), tempFileNameBase + Path.GetExtension(remotePath));
+            string tempDownloadPath = Path.Combine(Path.GetTempPath(), Path.GetFileName(remotePath));
             Download(remotePath, tempDownloadPath, output);
             await Unzip(tempDownloadPath, localExpandedDirPath, output, true);
         }
@@ -51,8 +50,7 @@ namespace JitBench
                 bool deleteTar = deleteZippedFiles;
                 if(tempTarPath == null)
                 {
-                    string tempFileNameBase = Guid.NewGuid().ToString();
-                    tempTarPath = Path.Combine(Path.GetTempPath(), tempFileNameBase + ".tar");
+                    tempTarPath = Path.Combine(Path.GetTempPath(), zipPath.Substring(0, zipPath.Length - ".gz".Length));
                     deleteTar = true;
                 }
                 await UnGZip(zipPath, tempTarPath, output);
@@ -178,6 +176,9 @@ namespace JitBench
                 }
                 try
                 {
+                    // On some systems, directories/files created programmatically are created with attributes
+                    // that prevent them from being deleted. Set those attributes to be normal
+                    SetAttributesNormal(path);
                     Directory.Delete(path, true);
                     return;
                 }
@@ -191,6 +192,18 @@ namespace JitBench
                 }
                 // if something has a transient lock on the file waiting may resolve the issue
                 Thread.Sleep((i+1) * 10);
+            }
+        }
+
+        public static void SetAttributesNormal(string path)
+        {
+            foreach (var subDir in Directory.GetDirectories(path))
+            {
+                SetAttributesNormal(subDir);
+            }
+            foreach (var file in Directory.GetFiles(path))
+            {
+                File.SetAttributes(file, FileAttributes.Normal);
             }
         }
 
@@ -210,6 +223,37 @@ namespace JitBench
                 try
                 {
                     Directory.Move(sourceDirName, destDirName);
+                    return;
+                }
+                catch (IOException e) when (i < retries - 1)
+                {
+                    output.WriteLine($"    Attempt #{i + 1} failed: {e.Message}");
+                }
+                catch (UnauthorizedAccessException e) when (i < retries - 1)
+                {
+                    output.WriteLine($"    Attempt #{i + 1} failed: {e.Message}");
+                }
+                // if something has a transient lock on the file waiting may resolve the issue
+                Thread.Sleep((i + 1) * 10);
+            }
+        }
+
+        public static void MoveFile(string sourceFileName, string destFileName, ITestOutputHelper output)
+        {
+            if (output != null)
+            {
+                output.WriteLine("Moving " + sourceFileName + " -> " + destFileName);
+            }
+            int retries = 10;
+            for (int i = 0; i < retries; i++)
+            {
+                if (!File.Exists(sourceFileName) && File.Exists(destFileName))
+                {
+                    return;
+                }
+                try
+                {
+                    File.Move(sourceFileName, destFileName);
                     return;
                 }
                 catch (IOException e) when (i < retries - 1)
