9678864ea13e07984bff68ba95f45a06e465d2ea
Alexander Nikolaev <alnikola@microsoft.com>
2019-12-17 13:45:41 +0100

    Allow is allowed in trailer

--- src/libraries/System.Net.Http/src/System/Net/Http/Headers/KnownHeaders.cs
+++ src/libraries/System.Net.Http/src/System/Net/Http/Headers/KnownHeaders.cs
@@ -24,7 +24,7 @@ namespace System.Net.Http.Headers
         public static readonly KnownHeader AccessControlExposeHeaders = new KnownHeader("Access-Control-Expose-Headers");
         public static readonly KnownHeader AccessControlMaxAge = new KnownHeader("Access-Control-Max-Age");
         public static readonly KnownHeader Age = new KnownHeader("Age", HttpHeaderType.Response | HttpHeaderType.NonTrailing, TimeSpanHeaderParser.Parser, null, StaticTable.Age);
-        public static readonly KnownHeader Allow = new KnownHeader("Allow", HttpHeaderType.Content | HttpHeaderType.NonTrailing, GenericHeaderParser.TokenListParser, null, StaticTable.Allow);
+        public static readonly KnownHeader Allow = new KnownHeader("Allow", HttpHeaderType.Content, GenericHeaderParser.TokenListParser, null, StaticTable.Allow);
         public static readonly KnownHeader AltSvc = new KnownHeader("Alt-Svc");
         public static readonly KnownHeader Authorization = new KnownHeader("Authorization", HttpHeaderType.Request | HttpHeaderType.NonTrailing, GenericHeaderParser.SingleValueAuthenticationParser, null, StaticTable.Authorization);
         public static readonly KnownHeader CacheControl = new KnownHeader("Cache-Control", HttpHeaderType.General | HttpHeaderType.NonTrailing, CacheControlHeaderParser.Parser, null, StaticTable.CacheControl);
