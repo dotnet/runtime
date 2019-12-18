11963746084d52f8e0798edeefb97cbbf942bd87
Fadi Hanna <fadim@microsoft.com>
2019-12-17 17:32:25 -0800

    Fix to array validation in type loadability checks

--- src/coreclr/src/tools/Common/Compiler/CompilerTypeSystemContext.Validation.cs
+++ src/coreclr/src/tools/Common/Compiler/CompilerTypeSystemContext.Validation.cs
@@ -76,11 +76,14 @@ namespace ILCompiler
                         ThrowHelper.ThrowTypeLoadException(ExceptionStringID.ClassLoadGeneral, type);
                     }
 
-                    LayoutInt elementSize = parameterType.GetElementSize();
-                    if (!elementSize.IsIndeterminate && elementSize.AsInt >= ushort.MaxValue)
+                    if (!parameterType.IsRuntimeDeterminedSubtype)
                     {
-                        // Element size over 64k can't be encoded in the GCDesc
-                        ThrowHelper.ThrowTypeLoadException(ExceptionStringID.ClassLoadValueClassTooLarge, parameterType);
+                        LayoutInt elementSize = parameterType.GetElementSize();
+                        if (!elementSize.IsIndeterminate && elementSize.AsInt >= ushort.MaxValue)
+                        {
+                            // Element size over 64k can't be encoded in the GCDesc
+                            ThrowHelper.ThrowTypeLoadException(ExceptionStringID.ClassLoadValueClassTooLarge, parameterType);
+                        }
                     }
 
                     if (((ArrayType)parameterizedType).Rank > 32)
