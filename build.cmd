ea33a834b894e772c7866b3029e30f49273ab4e2
Santiago Fernandez Madero <safern@microsoft.com>
2019-12-18 17:19:52 -0800

    Fix .cmd scripts help arguments

--- build.cmd
+++ build.cmd
@@ -1,15 +1,7 @@
 @echo off
 
-if "%~1"=="-h" goto help
-if "%~1"=="-help" goto help
-if "%~1"=="-?" goto help
-if "%~1"=="/?" goto help
-
 powershell -ExecutionPolicy ByPass -NoProfile -File "%~dp0eng\build.ps1" %*
 goto end
-
-:help
-powershell -ExecutionPolicy ByPass -NoProfile -Command "& { . '%~dp0eng\build.ps1'; Get-Help }"
 
 :end
 exit /b %ERRORLEVEL%
