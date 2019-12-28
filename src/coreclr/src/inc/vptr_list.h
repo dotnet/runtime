0266fa6a7d04d3e7c15be888a7e1be2dc275236d
Steve MacLean <Steve.MacLean@Microsoft.com>
2019-12-28 14:02:18 -0500

    TARGET_UNIX vptr_list.h

--- src/coreclr/src/inc/vptr_list.h
+++ src/coreclr/src/inc/vptr_list.h
@@ -46,9 +46,9 @@ VPTR_CLASS(PEImageLayout)
 VPTR_CLASS(RawImageLayout)
 VPTR_CLASS(ConvertedImageLayout)
 VPTR_CLASS(MappedImageLayout)
-#if !defined(CROSSGEN_COMPILE) && !defined(FEATURE_PAL)
+#if !defined(CROSSGEN_COMPILE) && !defined(TARGET_UNIX)
 VPTR_CLASS(LoadedImageLayout)
-#endif // !CROSSGEN_COMPILE && !FEATURE_PAL
+#endif // !CROSSGEN_COMPILE && !TARGET_UNIX
 VPTR_CLASS(FlatImageLayout)
 #ifdef FEATURE_COMINTEROP
 VPTR_CLASS(ComMethodFrame)
