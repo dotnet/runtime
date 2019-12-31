3453a57e297936175f5ddaf22f0da13e5b8ca50e
Egor Chesakov <Egor.Chesakov@microsoft.com>
2019-12-31 11:16:27 -0800

    Add generated tests

--- src/coreclr/tests/src/JIT/HardwareIntrinsics/Arm/AdvSimd.Arm64/Program.AdvSimd.Arm64.cs
+++ src/coreclr/tests/src/JIT/HardwareIntrinsics/Arm/AdvSimd.Arm64/Program.AdvSimd.Arm64.cs
@@ -14,6 +14,10 @@ namespace JIT.HardwareIntrinsics.Arm
             TestList = new Dictionary<string, Action>() {
                 ["Abs.Vector128.Double"] = Abs_Vector128_Double,
                 ["Abs.Vector128.UInt64"] = Abs_Vector128_UInt64,
+                ["AbsoluteCompareGreaterThan.Vector128.Double"] = AbsoluteCompareGreaterThan_Vector128_Double,
+                ["AbsoluteCompareGreaterThanOrEqual.Vector128.Double"] = AbsoluteCompareGreaterThanOrEqual_Vector128_Double,
+                ["AbsoluteCompareLessThan.Vector128.Double"] = AbsoluteCompareLessThan_Vector128_Double,
+                ["AbsoluteCompareLessThanOrEqual.Vector128.Double"] = AbsoluteCompareLessThanOrEqual_Vector128_Double,
                 ["ReverseElementBits.Vector128.Byte"] = ReverseElementBits_Vector128_Byte,
                 ["ReverseElementBits.Vector128.SByte"] = ReverseElementBits_Vector128_SByte,
                 ["ReverseElementBits.Vector64.Byte"] = ReverseElementBits_Vector64_Byte,
