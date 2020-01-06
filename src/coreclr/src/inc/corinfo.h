63782c92fe5f19f07ea7319ff632883b5f077b1d
vsadov <vsadov@microsoft.com>
2020-01-06 11:04:44 -0800

    Updating `JitEEVersionIdentifier` GIUD. A follow up for removing ArrayTypeDesc.

--- src/coreclr/src/inc/corinfo.h
+++ src/coreclr/src/inc/corinfo.h
@@ -217,11 +217,11 @@ TODO: Talk about initializing strutures before use
 #endif
 #endif
 
-SELECTANY const GUID JITEEVersionIdentifier = { /* aec2498a-ca70-408e-903e-1e6d84e90bd2 */
-    0xaec2498a,
-    0xca70,
-    0x408e,
-    {0x90, 0x3e, 0x1e, 0x6d, 0x84, 0xe9, 0x0b, 0xd2}
+SELECTANY const GUID JITEEVersionIdentifier = { /* abcf830c-56d1-4b33-a8ec-5063bb5495f1 */
+    0xabcf830c,
+    0x56d1,
+    0x4b33,
+    {0xa8, 0xec, 0x50, 0x63, 0xbb, 0x54, 0x95, 0xf1}
 };
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
