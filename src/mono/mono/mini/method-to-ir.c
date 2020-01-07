7afe4dd655fb48056d125c3194bb23dcb945ca46
EgorBo <egorbo@gmail.com>
2020-01-07 15:18:51 +0300

    Address Zoltan's feedback (enable for AOT)
    Commit migrated from https://github.com/mono/mono/commit/e6c0cdae0ca94955c3c1e7083b3eb906a5824c3e

--- src/mono/mono/mini/method-to-ir.c
+++ src/mono/mono/mini/method-to-ir.c
@@ -8949,11 +8949,14 @@ calli_end:
 				MonoMethod* gettype_method = mini_get_method (cfg, method, gettype_token, NULL, generic_context);
 				if (!strcmp (gettype_method->name, "GetType") && gettype_method->klass == mono_defaults.object_class) {
 					mono_class_init_internal(klass);
-					MonoType *klass_type = m_class_get_byval_arg (klass);
-					if (!mono_class_is_open_constructed_type (klass_type) && !cfg->compile_aot) { // FIXME: implement for AOT
-						// Emit `typeof(T)`
-						MonoReflectionType* reflection_type = mono_type_get_object_checked (cfg->domain, klass_type, cfg->error);
-						EMIT_NEW_PCONST (cfg, ins, reflection_type);
+					if (mono_class_get_checked (m_class_get_image (klass), m_class_get_type_token (klass), error) == klass) {
+						if (cfg->compile_aot) {
+							EMIT_NEW_TYPE_FROM_HANDLE_CONST (cfg, ins, m_class_get_image (klass), m_class_get_type_token (klass), generic_context);
+						} else {
+							MonoType *klass_type = m_class_get_byval_arg (klass);
+							MonoReflectionType* reflection_type = mono_type_get_object_checked (cfg->domain, klass_type, cfg->error);
+							EMIT_NEW_PCONST (cfg, ins, reflection_type);
+						}
 						ins->type = STACK_OBJ;
 						ins->klass = mono_defaults.systemtype_class;
 						*sp++ = ins;					
