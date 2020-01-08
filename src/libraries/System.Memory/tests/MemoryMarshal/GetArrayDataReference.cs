8e048f8b5e5b1b4df24b50206ed099c42025bc74
Levi Broderick <levib@microsoft.com>
2020-01-07 17:29:00 -0800

    Add missing libraries files

--- src/libraries/System.Memory/tests/MemoryMarshal/GetArrayDataReference.cs
+++ src/libraries/System.Memory/tests/MemoryMarshal/GetArrayDataReference.cs
@@ -11,37 +11,37 @@ namespace System.SpanTests
     public static partial class MemoryMarshalTests
     {
         [Fact]
-        public static void GetRawArrayData_NullInput_ThrowsNullRef()
+        public static void GetArrayDataReference_NullInput_ThrowsNullRef()
         {
-            Assert.Throws<NullReferenceException>(() => MemoryMarshal.GetRawArrayData((object[])null));
+            Assert.Throws<NullReferenceException>(() => MemoryMarshal.GetArrayDataReference((object[])null));
         }
 
         [Fact]
-        public static void GetRawArrayData_NonEmptyInput_ReturnsRefToFirstElement()
+        public static void GetArrayDataReference_NonEmptyInput_ReturnsRefToFirstElement()
         {
             int[] theArray = new int[] { 10, 20, 30 };
-            Assert.True(Unsafe.AreSame(ref theArray[0], ref MemoryMarshal.GetRawArrayData(theArray)));
+            Assert.True(Unsafe.AreSame(ref theArray[0], ref MemoryMarshal.GetArrayDataReference(theArray)));
         }
 
         [Fact]
-        public static unsafe void GetRawArrayData_EmptyInput_ReturnsRefToWhereFirstElementWouldBe()
+        public static unsafe void GetArrayDataReference_EmptyInput_ReturnsRefToWhereFirstElementWouldBe()
         {
             int[] theArray = new int[0];
 
-            ref int theRef = ref MemoryMarshal.GetRawArrayData(theArray);
+            ref int theRef = ref MemoryMarshal.GetArrayDataReference(theArray);
 
             Assert.True(Unsafe.AsPointer(ref theRef) != null);
             Assert.True(Unsafe.AreSame(ref theRef, ref MemoryMarshal.GetReference(theArray.AsSpan())));
         }
 
         [Fact]
-        public static void GetRawArrayData_IgnoresArrayVarianceChecks()
+        public static void GetArrayDataReference_IgnoresArrayVarianceChecks()
         {
             string[] strArr = new string[] { "Hello" };
 
-            // 'ref object' instead of 'ref string' because GetRawArrayData skips array variance checks.
+            // 'ref object' instead of 'ref string' because GetArrayDataReference skips array variance checks.
             // We can deref it but we must not write to it unless we know the value being written is also a string.
-            ref object refObj = ref MemoryMarshal.GetRawArrayData<object>(strArr);
+            ref object refObj = ref MemoryMarshal.GetArrayDataReference<object>(strArr);
 
             Assert.True(Unsafe.AreSame(ref refObj, ref Unsafe.As<string, object>(ref strArr[0])));
         }
