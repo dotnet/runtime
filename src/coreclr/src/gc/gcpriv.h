243b7e11c64fef69686884c1719cded4c67d6df3
Andrew Au <andrewau@microsoft.com>
2020-01-10 19:19:57 -0800

    Disable TRACE_GC

--- src/coreclr/src/gc/gcpriv.h
+++ src/coreclr/src/gc/gcpriv.h
@@ -145,8 +145,8 @@ inline void FATAL_GC_ERROR()
 
 //#define STRESS_PINNING    //Stress pinning by pinning randomly
 
-#define TRACE_GC          //debug trace gc operation
-#define SIMPLE_DPRINTF
+//#define TRACE_GC          //debug trace gc operation
+//#define SIMPLE_DPRINTF
 
 //#define TIME_GC           //time allocation and garbage collection
 //#define TIME_WRITE_WATCH  //time GetWriteWatch and ResetWriteWatch calls
