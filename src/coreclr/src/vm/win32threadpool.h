69cdf2a2402c6c545ef30986cc2eeb2ac7d986b0
Steve MacLean <Steve.MacLean@Microsoft.com>
2020-01-16 16:37:03 -0500

    Remove GetRecycleMemoryInfo from DAC builds

--- src/coreclr/src/vm/win32threadpool.h
+++ src/coreclr/src/vm/win32threadpool.h
@@ -733,6 +733,7 @@ public:
             return pRecycledListPerProcessor != NULL;
         }
 
+#ifndef DACCESS_COMPILE
     	FORCEINLINE RecycledListInfo& GetRecycleMemoryInfo( enum MemType memType )
         {
             LIMITED_METHOD_CONTRACT;
@@ -756,6 +757,7 @@ public:
 #endif // !TARGET_UNIX
             return pRecycledListPerProcessor[processorNumber][memType];
     	}
+#endif // DACCESS_COMPILE
     };
 
 #define GATE_THREAD_STATUS_NOT_RUNNING         0 // There is no gate thread
