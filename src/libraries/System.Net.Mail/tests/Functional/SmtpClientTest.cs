2fdc85da69f58bf22d91750ac6135d069c958436
Santiago Fernandez Madero <safern@microsoft.com>
2020-01-15 17:12:37 -0800

    Use a single pipeline with runtime conditioned jobs in PRs (#1473)
    * Use a single pipeline with runtime conditioned jobs in PRs
    
    * PR Feedback
    
    * Disable long running tests checked runtime

--- src/libraries/System.Net.Mail/tests/Functional/SmtpClientTest.cs
+++ src/libraries/System.Net.Mail/tests/Functional/SmtpClientTest.cs
@@ -358,6 +358,7 @@ namespace System.Net.Mail.Tests
 
         [Fact]
         [PlatformSpecific(TestPlatforms.Windows)] // NTLM support required, see https://github.com/dotnet/corefx/issues/28961
+        [SkipOnCoreClr("System.Net.Tests are flaky and/or long running: https://github.com/dotnet/runtime/issues/131")]
         public async Task TestCredentialsCopyInAsyncContext()
         {
             using var server = new LoopbackSmtpServer();
