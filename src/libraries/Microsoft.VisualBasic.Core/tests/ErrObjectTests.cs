d464af7857107d034c276ec0d40e25aab74d9bd0
Maxim Lipnin <v-maxlip@microsoft.com>
2020-01-23 07:55:52 -0300

    [xunit tests][mono] Remove disabled Microsoft.VisualBasic.Core.Tests tests from rsp file.

--- src/libraries/Microsoft.VisualBasic.Core/tests/ErrObjectTests.cs
+++ src/libraries/Microsoft.VisualBasic.Core/tests/ErrObjectTests.cs
@@ -11,6 +11,7 @@ namespace Microsoft.VisualBasic.Tests
     public class ErrObjectTests
     {
         [Fact]
+        [ActiveIssue("https://github.com/mono/mono/issues/14854", TargetFrameworkMonikers.Mono)]
         public void Clear()
         {
             ProjectData.ClearProjectError();
