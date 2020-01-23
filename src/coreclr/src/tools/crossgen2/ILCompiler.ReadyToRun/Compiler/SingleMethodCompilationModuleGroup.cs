fd7a5e8cf1748b7aefa90dd8e6966af2cbb4fe8b
David Wrighton <davidwr@microsoft.com>
2020-01-22 16:27:27 -0800

    Refactor compilation module group (#1889)
    - Our CompilationModule groups used for single method compilation and general compilation had diverged substantially, reducing the effectiveness of single method compilation substantially
    - The profiler guided optimization logic also needs to use the VersionWithCode logic to identify which methods are interesting to consider, this will happen in a future change
    - The VersionsWith logic in CompilationModuleGroup did not correctly model versionable logic.
      - Methods with generic parameters were not handled correctly. Method generic parameters did not contribute to the VersionsWith logic.
      - The newer ideas in our recent work on upcoming enhancements to crossgen2 driven compilation enhancements are aligned with this effort
      - Add support for the concept of considering instantiations over primitive types to be legal even if the primitive types aren't part of the version bubble. (Only if there are no constraints in the generic)
    
    - Also a bonus determinism fix. In cases where MemberRef's were being used in combination with ENCODE_FIELD_SIG_OwnerType, we could possibly emit multiple different choices for the token emitting in a signature.

--- src/coreclr/src/tools/crossgen2/ILCompiler.ReadyToRun/Compiler/SingleMethodCompilationModuleGroup.cs
+++ src/coreclr/src/tools/crossgen2/ILCompiler.ReadyToRun/Compiler/SingleMethodCompilationModuleGroup.cs
@@ -2,12 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Diagnostics;
 using System.Collections.Generic;
 
 using Internal.TypeSystem;
-using Internal.TypeSystem.Ecma;
-using ILCompiler.DependencyAnalysis.ReadyToRun;
 
 namespace ILCompiler
 {
@@ -15,12 +12,20 @@ namespace ILCompiler
     /// A compilation group that only contains a single method. Useful for development purposes when investigating
     /// code generation issues.
     /// </summary>
-    public class SingleMethodCompilationModuleGroup : CompilationModuleGroup
+    public class SingleMethodCompilationModuleGroup : ReadyToRunCompilationModuleGroupBase
     {
         private MethodDesc _method;
-        private Dictionary<TypeDesc, ModuleToken> _typeRefsInCompilationModuleSet;
 
-        public SingleMethodCompilationModuleGroup(MethodDesc method)
+        public SingleMethodCompilationModuleGroup(
+            TypeSystemContext context,
+            IEnumerable<ModuleDesc> compilationModuleSet,
+            IEnumerable<ModuleDesc> versionBubbleModuleSet,
+            bool compileGenericDependenciesFromVersionBubbleModuleSet,
+            MethodDesc method) :
+                base(context,
+                     compilationModuleSet,
+                     versionBubbleModuleSet,
+                     compileGenericDependenciesFromVersionBubbleModuleSet)
         {
             _method = method;
         }
@@ -30,42 +35,10 @@ namespace ILCompiler
             return method == _method;
         }
 
-        public override bool ContainsType(TypeDesc type)
+        public override void ApplyProfilerGuidedCompilationRestriction(ProfileDataManager profileGuidedCompileRestriction)
         {
-            return type == _method.OwningType;
-        }
-
-        public override bool VersionsWithModule(ModuleDesc module)
-        {
-            return ((EcmaMethod)_method.GetTypicalMethodDefinition()).Module == module;
-        }
-
-        public override bool GeneratesPInvoke(MethodDesc method)
-        {
-            return true;
-        }
-
-        public override bool TryGetModuleTokenForExternalType(TypeDesc type, out ModuleToken token)
-        {
-            Debug.Assert(!VersionsWithType(type));
-
-            if (_typeRefsInCompilationModuleSet == null)
-            {
-                _typeRefsInCompilationModuleSet = new Dictionary<TypeDesc, ModuleToken>();
-
-                EcmaModule ecmaModule = ((EcmaMethod)_method.GetTypicalMethodDefinition()).Module;
-                foreach (var typeRefHandle in ecmaModule.MetadataReader.TypeReferences)
-                {
-                    try
-                    {
-                        TypeDesc typeFromTypeRef = ecmaModule.GetType(typeRefHandle);
-                        _typeRefsInCompilationModuleSet[typeFromTypeRef] = new ModuleToken(ecmaModule, typeRefHandle);
-                    }
-                    catch (TypeSystemException) { }
-                }
-            }
-
-            return _typeRefsInCompilationModuleSet.TryGetValue(type, out token);
+            // Profiler guided restrictions are ignored for single method compilation
+            return;
         }
     }
 }
