53e8fea8f28481b0ce07185e48402ec5c9802577
Jeremy Koritzinsky <jekoritz@microsoft.com>
2020-01-22 16:22:09 -0800

    Don't block non-blittable pointer types in P/Invokes. (#1866)
    * Don't block non-blittable pointer types in P/Invokes.
    
    Don't block non-blittable pointer types in P/Invokes. If you're using pointers in interop you're already using unsafe code so we're going to assume you know what you're doing. C# won't let you form a pointer to a reference, so there's no risk of passing an object reference directly to native unless you're writing IL directly.
    
    Fixes dotnet/coreclr#27800
    
    * Delete unused resources.
    
    * Apply fix to managed type system as well.
    
    * Add positive test for nonblittable pointers. Remove negative tests that were in GenericTest.

--- src/coreclr/src/tools/Common/TypeSystem/Interop/IL/MarshalHelpers.cs
+++ src/coreclr/src/tools/Common/TypeSystem/Interop/IL/MarshalHelpers.cs
@@ -11,8 +11,8 @@ namespace Internal.TypeSystem.Interop
 {
     public static partial class MarshalHelpers
     {
-        internal static TypeDesc GetNativeTypeFromMarshallerKind(TypeDesc type, 
-                MarshallerKind kind, 
+        internal static TypeDesc GetNativeTypeFromMarshallerKind(TypeDesc type,
+                MarshallerKind kind,
                 MarshallerKind elementMarshallerKind,
 #if !READYTORUN
                 InteropStateManager interopStateManager,
@@ -121,7 +121,7 @@ namespace Internal.TypeSystem.Interop
 #if !READYTORUN
                             interopStateManager,
 #endif
-                            marshalAs, 
+                            marshalAs,
                             isArrayElement: true);
 
                         return elementNativeType.MakePointerType();
@@ -438,22 +438,6 @@ namespace Internal.TypeSystem.Interop
             }
             else if (type.IsPointer)
             {
-#if READYTORUN
-                TypeDesc parameterType = ((PointerType)type).ParameterType;
-
-                if ((!parameterType.IsEnum
-                    && !parameterType.IsPrimitive
-                    && !MarshalUtils.IsBlittableType(parameterType))
-                    || parameterType.IsGCPointer)
-                {
-                    // Pointers cannot reference marshaled structures.  Use ByRef instead.
-                    return MarshallerKind.Invalid;
-                }
-#else
-                // Do not bother enforcing the above artificial restriction
-                // See https://github.com/dotnet/coreclr/issues/27800
-#endif
-
                 if (nativeType == NativeTypeKind.Default)
                     return MarshallerKind.BlittableValue;
                 else
