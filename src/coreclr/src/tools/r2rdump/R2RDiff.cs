bb498a44266c908dfbfb3d6af25ceb847cd7d09c
Tomas <trylek@microsoft.com>
2020-01-25 02:25:26 +0100

    Minor R2RDump facelift w.r.t. diffing dump
    Based on offline chat with JanV I added initial provisions to simplify
    diffing Crossgen1- and 2-compiled SPC. In essence, the --diff option
    newly ends up emitting two extra files with the ".r2rdump" extension
    representing the methods common to the left and right R2R file. This
    is formerly untrodden territory, I'm pioneering R2RDump usability for
    this purpose.
    
    Thanks
    
    Tomas

--- src/coreclr/src/tools/r2rdump/R2RDiff.cs
+++ src/coreclr/src/tools/r2rdump/R2RDiff.cs
@@ -18,30 +18,30 @@ namespace R2RDump
     class R2RDiff
     {
         /// <summary>
-        /// Left R2R image for the diff.
+        /// Left dumper to use for the diff
         /// </summary>
-        private readonly ReadyToRunReader _leftFile;
+        private readonly Dumper _leftDumper;
 
         /// <summary>
-        /// Right R2R image for the diff.
+        /// Right dumper to use for the diff
         /// </summary>
-        private readonly ReadyToRunReader _rightFile;
+        private readonly Dumper _rightDumper;
 
         /// <summary>
-        /// Text writer to receive diff output.
+        /// Text writer to use for common output
         /// </summary>
         private readonly TextWriter _writer;
 
         /// <summary>
         /// Store the left and right file and output writer.
         /// </summary>
-        /// <param name="leftFile">Left R2R file</param>
-        /// <param name="rightFile">Right R2R file</param>
-        /// <param name="writer">Output writer to receive the diff</param>
-        public R2RDiff(ReadyToRunReader leftFile, ReadyToRunReader rightFile, TextWriter writer)
+        /// <param name="leftDumper">Dumper to use for the left diff output</param>
+        /// <param name="rightDumper">Dumper to use for the right diff output</param>
+        /// <param name="writer">Writer to use for output common to left / right side</param>
+        public R2RDiff(Dumper leftDumper, Dumper rightDumper, TextWriter writer)
         {
-            _leftFile = leftFile;
-            _rightFile = rightFile;
+            _leftDumper = leftDumper;
+            _rightDumper = rightDumper;
             _writer = writer;
         }
 
@@ -54,6 +54,11 @@ namespace R2RDump
             DiffPESections();
             DiffR2RSections();
             DiffR2RMethods();
+
+            HashSet<string> commonMethods = new HashSet<string>(_leftDumper.Reader.Methods.Select(method => method.SignatureString)
+                .Intersect(_rightDumper.Reader.Methods.Select(method => method.SignatureString)));
+            DumpCommonMethods(_leftDumper, commonMethods);
+            DumpCommonMethods(_rightDumper, commonMethods);
         }
 
         /// <summary>
@@ -61,8 +66,8 @@ namespace R2RDump
         /// </summary>
         private void DiffTitle()
         {
-            _writer.WriteLine($@"Left file:  {_leftFile.Filename} ({_leftFile.Image.Length} B)");
-            _writer.WriteLine($@"Right file: {_rightFile.Filename} ({_rightFile.Image.Length} B)");
+            _writer.WriteLine($@"Left file:  {_leftDumper.Reader.Filename} ({_leftDumper.Reader.Image.Length} B)");
+            _writer.WriteLine($@"Right file: {_rightDumper.Reader.Filename} ({_rightDumper.Reader.Image.Length} B)");
             _writer.WriteLine();
         }
 
@@ -71,7 +76,7 @@ namespace R2RDump
         /// </summary>
         private void DiffPESections()
         {
-            ShowDiff(GetPESectionMap(_leftFile), GetPESectionMap(_rightFile), "PE sections");
+            ShowDiff(GetPESectionMap(_leftDumper.Reader), GetPESectionMap(_rightDumper.Reader), "PE sections");
         }
 
         /// <summary>
@@ -79,7 +84,7 @@ namespace R2RDump
         /// </summary>
         private void DiffR2RSections()
         {
-            ShowDiff(GetR2RSectionMap(_leftFile), GetR2RSectionMap(_rightFile), "R2R sections");
+            ShowDiff(GetR2RSectionMap(_leftDumper.Reader), GetR2RSectionMap(_rightDumper.Reader), "R2R sections");
         }
 
         /// <summary>
@@ -87,7 +92,7 @@ namespace R2RDump
         /// </summary>
         private void DiffR2RMethods()
         {
-            ShowDiff(GetR2RMethodMap(_leftFile), GetR2RMethodMap(_rightFile), "R2R methods");
+            ShowDiff(GetR2RMethodMap(_leftDumper.Reader), GetR2RMethodMap(_rightDumper.Reader), "R2R methods");
         }
 
         /// <summary>
@@ -203,6 +208,25 @@ namespace R2RDump
             }
 
             return methodMap;
+        }
+
+        /// <summary>
+        /// Dump the subset of methods common to both sides of the diff to the given dumper.
+        /// </summary>
+        /// <param name="dumper">Output dumper to use</param>
+        /// <param name="signatureFilter">Set of common signatures of methods to dump</param>
+        private void DumpCommonMethods(Dumper dumper, HashSet<string> signatureFilter)
+        {
+            IEnumerable<ReadyToRunMethod> filteredMethods = dumper
+                .Reader
+                .Methods
+                .Where(method => signatureFilter.Contains(method.SignatureString))
+                .OrderBy(method => method.SignatureString);
+
+            foreach (ReadyToRunMethod method in filteredMethods)
+            {
+                dumper.DumpMethod(method);
+            }
         }
     }
 }
