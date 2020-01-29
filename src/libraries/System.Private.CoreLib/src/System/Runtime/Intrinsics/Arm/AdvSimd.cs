06f870972fcc71de8f4aa8da7011937770232b12
Egor Chesakov <Egor.Chesakov@microsoft.com>
2020-01-29 15:19:43 -0800

    Update "AndNot" in AdvSimd.cs AdvSimd.PlatformNotSupported.cs

--- src/libraries/System.Private.CoreLib/src/System/Runtime/Intrinsics/Arm/AdvSimd.cs
+++ src/libraries/System.Private.CoreLib/src/System/Runtime/Intrinsics/Arm/AdvSimd.cs
@@ -1602,50 +1602,50 @@ namespace System.Runtime.Intrinsics.Arm
         /// <summary>
         /// uint8x8_t vbic_u8 (uint8x8_t a, uint8x8_t b)
         ///   A32: VBIC Dd, Dn, Dm
-        ///   A64: BIC Vd, Vn, Vm
+        ///   A64: BIC Vd.8B, Vn.8B, Vm.8B
         /// </summary>
         public static Vector64<byte> AndNot(Vector64<byte> left, Vector64<byte> right) => AndNot(left, right);
 
-        // /// <summary>
-        // /// float64x1_t vbic_f64 (float64x1_t a, float64x1_t b)
-        // ///   A32: VBIC Dd, Dn, Dm
-        // ///   A64: BIC Vd, Vn, Vm
-        // /// The above native signature does not exist. We provide this additional overload for consistency with the other scalar APIs.
-        // /// </summary>
-        // public static Vector64<double> AndNot(Vector64<double> left, Vector64<double> right) => AndNot(left, right);
+        /// <summary>
+        /// float64x1_t vbic_f64 (float64x1_t a, float64x1_t b)
+        ///   A32: VBIC Dd, Dn, Dm
+        ///   A64: BIC Vd.8B, Vn.8B, Vm.8B
+        /// The above native signature does not exist. We provide this additional overload for consistency with the other scalar APIs.
+        /// </summary>
+        public static Vector64<double> AndNot(Vector64<double> left, Vector64<double> right) => AndNot(left, right);
 
         /// <summary>
         /// int16x4_t vbic_s16 (int16x4_t a, int16x4_t b)
         ///   A32: VBIC Dd, Dn, Dm
-        ///   A64: BIC Vd, Vn, Vm
+        ///   A64: BIC Vd.8B, Vn.8B, Vm.8B
         /// </summary>
         public static Vector64<short> AndNot(Vector64<short> left, Vector64<short> right) => AndNot(left, right);
 
         /// <summary>
-        /// int32x2_t vbic_s32(int32x2_t a, int32x2_t b)
+        /// int32x2_t vbic_s32 (int32x2_t a, int32x2_t b)
         ///   A32: VBIC Dd, Dn, Dm
-        ///   A64: BIC Vd, Vn, Vm
+        ///   A64: BIC Vd.8B, Vn.8B, Vm.8B
         /// </summary>
         public static Vector64<int> AndNot(Vector64<int> left, Vector64<int> right) => AndNot(left, right);
 
-        // /// <summary>
-        // /// int64x1_t vbic_s64 (int64x1_t a, int64x1_t b)
-        // ///   A32: VBIC Dd, Dn, Dm
-        // ///   A64: BIC Vd, Vn, Vm
-        // /// </summary>
-        // public static Vector64<long> AndNot(Vector64<long> left, Vector64<long> right) => AndNot(left, right);
+        /// <summary>
+        /// int64x1_t vbic_s64 (int64x1_t a, int64x1_t b)
+        ///   A32: VBIC Dd, Dn, Dm
+        ///   A64: BIC Vd.8B, Vn.8B, Vm.8B
+        /// </summary>
+        public static Vector64<long> AndNot(Vector64<long> left, Vector64<long> right) => AndNot(left, right);
 
         /// <summary>
         /// int8x8_t vbic_s8 (int8x8_t a, int8x8_t b)
         ///   A32: VBIC Dd, Dn, Dm
-        ///   A64: BIC Vd, Vn, Vm
+        ///   A64: BIC Vd.8B, Vn.8B, Vm.8B
         /// </summary>
         public static Vector64<sbyte> AndNot(Vector64<sbyte> left, Vector64<sbyte> right) => AndNot(left, right);
 
         /// <summary>
         /// float32x2_t vbic_f32 (float32x2_t a, float32x2_t b)
         ///   A32: VBIC Dd, Dn, Dm
-        ///   A64: BIC Vd, Vn, Vm
+        ///   A64: BIC Vd.8B, Vn.8B, Vm.8B
         /// The above native signature does not exist. We provide this additional overload for consistency with the other scalar APIs.
         /// </summary>
         public static Vector64<float> AndNot(Vector64<float> left, Vector64<float> right) => AndNot(left, right);
@@ -1653,93 +1653,93 @@ namespace System.Runtime.Intrinsics.Arm
         /// <summary>
         /// uint16x4_t vbic_u16 (uint16x4_t a, uint16x4_t b)
         ///   A32: VBIC Dd, Dn, Dm
-        ///   A64: BIC Vd, Vn, Vm
+        ///   A64: BIC Vd.8B, Vn.8B, Vm.8B
         /// </summary>
         public static Vector64<ushort> AndNot(Vector64<ushort> left, Vector64<ushort> right) => AndNot(left, right);
 
         /// <summary>
         /// uint32x2_t vbic_u32 (uint32x2_t a, uint32x2_t b)
         ///   A32: VBIC Dd, Dn, Dm
-        ///   A64: BIC Vd, Vn, Vm
+        ///   A64: BIC Vd.8B, Vn.8B, Vm.8B
         /// </summary>
         public static Vector64<uint> AndNot(Vector64<uint> left, Vector64<uint> right) => AndNot(left, right);
 
-        // /// <summary>
-        // /// uint64x1_t vbic_u64 (uint64x1_t a, uint64x1_t b)
-        // ///   A32: VBIC Dd, Dn, Dm
-        // ///   A64: BIC Vd, Vn, Vm
-        // /// </summary>
-        // public static Vector64<ulong> AndNot(Vector64<ulong> left, Vector64<ulong> right) => AndNot(left, right);
+        /// <summary>
+        /// uint64x1_t vbic_u64 (uint64x1_t a, uint64x1_t b)
+        ///   A32: VBIC Dd, Dn, Dm
+        ///   A64: BIC Vd.8B, Vn.8B, Vm.8B
+        /// </summary>
+        public static Vector64<ulong> AndNot(Vector64<ulong> left, Vector64<ulong> right) => AndNot(left, right);
 
         /// <summary>
-        /// uint8x16_t vbic_u8 (uint8x16_t a, uint8x16_t b)
-        ///   A32: VBIC Dd, Dn, Dm
-        ///   A64: BIC Vd, Vn, Vm
+        /// uint8x16_t vbicq_u8 (uint8x16_t a, uint8x16_t b)
+        ///   A32: VBIC Qd, Qn, Qm
+        ///   A64: BIC Vd.16B, Vn.16B, Vm.16B
         /// </summary>
         public static Vector128<byte> AndNot(Vector128<byte> left, Vector128<byte> right) => AndNot(left, right);
 
         /// <summary>
-        /// float64x2_t vbic_f64 (float64x2_t a, float64x2_t b)
-        ///   A32: VBIC Dd, Dn, Dm
-        ///   A64: BIC Vd, Vn, Vm
+        /// float64x2_t vbicq_f64 (float64x2_t a, float64x2_t b)
+        ///   A32: VBIC Qd, Qn, Qm
+        ///   A64: BIC Vd.16B, Vn.16B, Vm.16B
         /// The above native signature does not exist. We provide this additional overload for consistency with the other scalar APIs.
         /// </summary>
         public static Vector128<double> AndNot(Vector128<double> left, Vector128<double> right) => AndNot(left, right);
 
         /// <summary>
-        /// int16x8_t vbic_s16 (int16x8_t a, int16x8_t b)
-        ///   A32: VBIC Dd, Dn, Dm
-        ///   A64: BIC Vd, Vn, Vm
+        /// int16x8_t vbicq_s16 (int16x8_t a, int16x8_t b)
+        ///   A32: VBIC Qd, Qn, Qm
+        ///   A64: BIC Vd.16B, Vn.16B, Vm.16B
         /// </summary>
         public static Vector128<short> AndNot(Vector128<short> left, Vector128<short> right) => AndNot(left, right);
 
         /// <summary>
-        /// int32x4_t vbic_s32(int32x4_t a, int32x4_t b)
-        ///   A32: VBIC Dd, Dn, Dm
-        ///   A64: BIC Vd, Vn, Vm
+        /// int32x4_t vbicq_s32 (int32x4_t a, int32x4_t b)
+        ///   A32: VBIC Qd, Qn, Qm
+        ///   A64: BIC Vd.16B, Vn.16B, Vm.16B
         /// </summary>
         public static Vector128<int> AndNot(Vector128<int> left, Vector128<int> right) => AndNot(left, right);
 
         /// <summary>
-        /// int64x2_t vbic_s64 (int64x2_t a, int64x2_t b)
-        ///   A32: VBIC Dd, Dn, Dm
-        ///   A64: BIC Vd, Vn, Vm
+        /// int64x2_t vbicq_s64 (int64x2_t a, int64x2_t b)
+        ///   A32: VBIC Qd, Qn, Qm
+        ///   A64: BIC Vd.16B, Vn.16B, Vm.16B
         /// </summary>
         public static Vector128<long> AndNot(Vector128<long> left, Vector128<long> right) => AndNot(left, right);
 
         /// <summary>
-        /// int8x16_t vbic_s8 (int8x16_t a, int8x16_t b)
-        ///   A32: VBIC Dd, Dn, Dm
-        ///   A64: BIC Vd, Vn, Vm
+        /// int8x16_t vbicq_s8 (int8x16_t a, int8x16_t b)
+        ///   A32: VBIC Qd, Qn, Qm
+        ///   A64: BIC Vd.16B, Vn.16B, Vm.16B
         /// </summary>
         public static Vector128<sbyte> AndNot(Vector128<sbyte> left, Vector128<sbyte> right) => AndNot(left, right);
 
         /// <summary>
-        /// float32x4_t vbic_f32 (float32x4_t a, float32x4_t b)
-        ///   A32: VBIC Dd, Dn, Dm
-        ///   A64: BIC Vd, Vn, Vm
+        /// float32x4_t vbicq_f32 (float32x4_t a, float32x4_t b)
+        ///   A32: VBIC Qd, Qn, Qm
+        ///   A64: BIC Vd.16B, Vn.16B, Vm.16B
         /// The above native signature does not exist. We provide this additional overload for consistency with the other scalar APIs.
         /// </summary>
         public static Vector128<float> AndNot(Vector128<float> left, Vector128<float> right) => AndNot(left, right);
 
         /// <summary>
-        /// uint16x8_t vbic_u16 (uint16x8_t a, uint16x8_t b)
-        ///   A32: VBIC Dd, Dn, Dm
-        ///   A64: BIC Vd, Vn, Vm
+        /// uint16x8_t vbicq_u16 (uint16x8_t a, uint16x8_t b)
+        ///   A32: VBIC Qd, Qn, Qm
+        ///   A64: BIC Vd.16B, Vn.16B, Vm.16B
         /// </summary>
         public static Vector128<ushort> AndNot(Vector128<ushort> left, Vector128<ushort> right) => AndNot(left, right);
 
         /// <summary>
-        /// uint32x4_t vbic_u32 (uint32x4_t a, uint32x4_t b)
-        ///   A32: VBIC Dd, Dn, Dm
-        ///   A64: BIC Vd, Vn, Vm
+        /// uint32x4_t vbicq_u32 (uint32x4_t a, uint32x4_t b)
+        ///   A32: VBIC Qd, Qn, Qm
+        ///   A64: BIC Vd.16B, Vn.16B, Vm.16B
         /// </summary>
         public static Vector128<uint> AndNot(Vector128<uint> left, Vector128<uint> right) => AndNot(left, right);
 
         /// <summary>
-        /// uint64x2_t vbic_u64 (uint64x2_t a, uint64x2_t b)
-        ///   A32: VBIC Dd, Dn, Dm
-        ///   A64: BIC Vd, Vn, Vm
+        /// uint64x2_t vbicq_u64 (uint64x2_t a, uint64x2_t b)
+        ///   A32: VBIC Qd, Qn, Qm
+        ///   A64: BIC Vd.16B, Vn.16B, Vm.16B
         /// </summary>
         public static Vector128<ulong> AndNot(Vector128<ulong> left, Vector128<ulong> right) => AndNot(left, right);
 
