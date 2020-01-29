099a5124458b6ce4a11e3c4f6159ec4c3bcd9700
Jan Kotas <jkotas@microsoft.com>
2020-01-29 10:02:22 -0800

    Merging small assemblies (part 1/N) (#2189)
    * Delete unnecessary ClsCompliant
    
    * Merge System.Diagnostics.Tools\src into CoreLib
    
    * Merge System.Security.Pricipal\src into CoreLib
    
    * Delete System.IO.UnmanagedMemoryStream\ref\System.IO.UnmanagedMemoryStream.cs
    
    * Delete System.Reflection\ref\System.Reflection.cs
    
    * Delete  System.Threading.Timer\src\ApiCompatBaseline.uapaot.txt
    
    * Delete System.Buffers\src\Resources\Strings.resx
    
    * Merge System.Buffers\ref into System.Runtime
    
    * Merge System.Diagnostics.Debug\ref into System.Runtime
    
    * Delete *\MatchingRefApiCompatBaseline.txt
    
    * Merge System.Diagnostics.Tools into System.Runtime\ref
    
    * Merge System.Resources.ResourceManager into System.Runtime\ref
    
    * Cleanup System.IO.* project references
    
    * Cleanup System.Reflection project references
    
    * Cleanup System.Runtime.Handles project references
    
    * Merge System.Threading.Thread\src into CoreLib
    
    * Merge System.Threading.Timer\ref into System.Runtime
    
    * Merge System.Threading.Tasks\ref into System.Runtime

--- src/libraries/System.Threading.Timer/ref/System.Threading.Timer.Forwards.cs
+++ src/libraries/System.Threading.Timer/ref/System.Threading.Timer.Forwards.cs
@@ -4,3 +4,6 @@
 // ------------------------------------------------------------------------------
 // Changes to this file must follow the http://aka.ms/api-review process.
 // ------------------------------------------------------------------------------
+
+[assembly: System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Threading.Timer))]
+[assembly: System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Threading.TimerCallback))]
