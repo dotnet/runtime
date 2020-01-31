7eb48332a9c7cc1ba65e998bc74d6c2bf52233a1
Mateo Torres Ruiz <matorre@microsoft.com>
2020-01-31 14:47:57 -0800

    Verify for yet another method
    Commit migrated from https://github.com/dotnet/linker/commit/36a6887620a61b0b4d4e6d65b51f56cee9a35071

--- src/tools/illink/test/Mono.Linker.Tests.Cases/PreserveDependencies/PreserveDependencyMemberSignatureWildcard.cs
+++ src/tools/illink/test/Mono.Linker.Tests.Cases/PreserveDependencies/PreserveDependencyMemberSignatureWildcard.cs
@@ -5,6 +5,7 @@ using Mono.Linker.Tests.Cases.Expectations.Metadata;
 namespace Mono.Linker.Tests.Cases.PreserveDependencies
 {
 	[KeptMemberInAssembly ("library.dll", "Mono.Linker.Tests.Cases.PreserveDependencies.Dependencies.PreserveDependencyMethodInAssemblyLibrary", ".ctor()")]
+	[KeptMemberInAssembly ("library.dll", "Mono.Linker.Tests.Cases.PreserveDependencies.Dependencies.PreserveDependencyMethodInAssemblyLibrary", "Foo()")]
 	[SetupCompileBefore ("library.dll", new [] { "Dependencies/PreserveDependencyMethodInAssemblyLibrary.cs" })]
 	public class PreserveDependencyMemberSignatureWildcard
 	{
