f4facb25544cfd6f0a87efb9d24067562343ac34
Andrew Au <andrewau@microsoft.com>
2020-01-31 17:11:02 -0800

    Introduce ThreadSuspend::SysIsSuspended()

--- src/coreclr/src/vm/weakreferencenative.cpp
+++ src/coreclr/src/vm/weakreferencenative.cpp
@@ -500,9 +500,8 @@ void FinalizeWeakReference(Object * obj)
     // We need to revisit the implementation of FinalizeWeakReference() in case of concurrent ScanForFinalization
     // ThreadSuspend::g_pSuspensionThread is set before SuspendRuntime is called, during that time interval, 
     // there could be managed code running calling WeakReferenceNative::SetTarget() and missed the synchronization.
-    bool isRuntimeSuspended = (ThreadSuspend::GetSuspensionThread() != nullptr);
 
-    OBJECTHANDLE handle = isRuntimeSuspended ? pThis->m_Handle.LoadWithoutBarrier() : AcquireWeakHandleSpinLock(pThis);
+    OBJECTHANDLE handle = ThreadSuspend::SysIsSuspended() ? pThis->m_Handle.LoadWithoutBarrier() : AcquireWeakHandleSpinLock(pThis);
     OBJECTHANDLE handleToDestroy = NULL;
     bool isWeakWinRTHandle = false;
 
