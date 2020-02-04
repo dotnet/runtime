fdf7e086006d357e0cfbdaec113bc386b815a1a3
Marek Safar <marek.safar@gmail.com>
2020-02-04 18:42:49 +0100

    Clean up more illink build projects
    Commit migrated from https://github.com/dotnet/linker/commit/3635d662ae832ec66755937dd087374e8b07c6c9

--- src/tools/illink/test/Mono.Linker.Tests.Cases/Symbols/ReferenceWithMdbAndSymbolLinkingEnabled.cs
+++ src/tools/illink/test/Mono.Linker.Tests.Cases/Symbols/ReferenceWithMdbAndSymbolLinkingEnabled.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#if !NETCOREAPP
+
+using System;
 using Mono.Linker.Tests.Cases.Expectations.Assertions;
 using Mono.Linker.Tests.Cases.Expectations.Metadata;
 using Mono.Linker.Tests.Cases.Symbols.Dependencies;
@@ -31,3 +33,5 @@ namespace Mono.Linker.Tests.Cases.Symbols {
 		}
 	}
 }
+
+#endif
