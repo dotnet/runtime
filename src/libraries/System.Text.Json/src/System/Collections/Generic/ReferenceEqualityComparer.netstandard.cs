16ba551952269e4395423d54e7a464be321f9295
Levi Broderick <levib@microsoft.com>
2020-02-04 13:58:12 -0800

    Add public ReferenceEqualityComparer API - Also update libraries to use this in lieu of custom comparers where appropriate

---
+++ src/libraries/System.Text.Json/src/System/Collections/Generic/ReferenceEqualityComparer.netstandard.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Runtime.CompilerServices;
+
+namespace System.Collections.Generic
+{
+    /// <summary>
+    /// Passed to the <see cref="DefaultReferenceResolver._objectToReferenceIdMap"/> meant for serialization.
+    /// It forces the dictionary to do a ReferenceEquals comparison when comparing the TKey object.
+    /// </summary>
+    internal sealed class ReferenceEqualityComparer : IEqualityComparer<object>, IEqualityComparer
+    {
+        private ReferenceEqualityComparer() { }
+
+        public static ReferenceEqualityComparer Instance { get; } = new ReferenceEqualityComparer();
+
+        public new bool Equals(object? x, object? y) => ReferenceEquals(x, y);
+
+        public int GetHashCode(object obj) => RuntimeHelpers.GetHashCode(obj);
+    }
+}
