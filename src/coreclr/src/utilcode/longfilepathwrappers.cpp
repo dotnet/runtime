799fb8c804ca4c06b149b1225a5b6993f6acbb74
Steve MacLean <Steve.MacLean@Microsoft.com>
2020-02-04 18:53:24 -0500

    Configure long file path wrappers based on host

--- src/coreclr/src/utilcode/longfilepathwrappers.cpp
+++ src/coreclr/src/utilcode/longfilepathwrappers.cpp
@@ -11,14 +11,14 @@
 class LongFile
 {
 private:
-#ifndef TARGET_UNIX
+#ifndef HOST_UNIX
         static const WCHAR* ExtendedPrefix;
         static const WCHAR* DevicePathPrefix;
         static const WCHAR* UNCPathPrefix;
         static const WCHAR* UNCExtendedPathPrefix;
         static const WCHAR VolumeSeparatorChar;
 		#define UNCPATHPREFIX W("\\\\")
-#endif //TARGET_UNIX
+#endif //HOST_UNIX
         static const WCHAR DirectorySeparatorChar;
         static const WCHAR AltDirectorySeparatorChar;
 public:
@@ -31,7 +31,7 @@ public:
 
         static HRESULT NormalizePath(SString& path);
 
-#ifndef TARGET_UNIX
+#ifndef HOST_UNIX
         static void NormalizeDirectorySeparators(SString& path);
 #endif
 };
@@ -60,11 +60,11 @@ LoadLibraryExWrapper(
 
         if (LongFile::IsPathNotFullyQualified(path) || SUCCEEDED(LongFile::NormalizePath(path)))
         {
-#ifndef TARGET_UNIX
+#ifndef HOST_UNIX
             //Adding the assert to ensure relative paths which are not just filenames are not used for LoadLibrary Calls
             _ASSERTE(!LongFile::IsPathNotFullyQualified(path) || !LongFile::ContainsDirectorySeparator(path));
             LongFile::NormalizeDirectorySeparators(path);
-#endif //TARGET_UNIX
+#endif //HOST_UNIX
 
             ret = LoadLibraryExW(path.GetUnicode(), hFile, dwFlags);
         }
@@ -657,7 +657,7 @@ DWORD WINAPI GetEnvironmentVariableWrapper(
 }
 
 
-#ifndef TARGET_UNIX
+#ifndef HOST_UNIX
 
 BOOL
 CopyFileExWrapper(
@@ -764,10 +764,10 @@ FindFirstFileExWrapper(
 
     return ret;
 }
-#endif //!TARGET_UNIX
+#endif //!HOST_UNIX
 
 
-#ifndef TARGET_UNIX
+#ifndef HOST_UNIX
 
 #if ! defined(DACCESS_COMPILE) && !defined(SELF_NO_HOST)
 extern HINSTANCE            g_pMSCorEE;
@@ -822,13 +822,13 @@ BOOL PAL_GetPALDirectoryWrapper(SString& pbuffer)
     return retval;
 }
 
-#endif // TARGET_UNIX
+#endif // HOST_UNIX
 
 
 //Implementation of LongFile Helpers
 const WCHAR LongFile::DirectorySeparatorChar = W('\\');
 const WCHAR LongFile::AltDirectorySeparatorChar = W('/');
-#ifndef TARGET_UNIX
+#ifndef HOST_UNIX
 const WCHAR LongFile::VolumeSeparatorChar = W(':');
 const WCHAR* LongFile::ExtendedPrefix = W("\\\\?\\");
 const WCHAR* LongFile::DevicePathPrefix = W("\\\\.\\");
@@ -1003,7 +1003,7 @@ HRESULT LongFile::NormalizePath(SString & path)
 {
     return S_OK;
 }
-#endif //TARGET_UNIX
+#endif //HOST_UNIX
 
 BOOL LongFile::ContainsDirectorySeparator(SString & path)
 {
