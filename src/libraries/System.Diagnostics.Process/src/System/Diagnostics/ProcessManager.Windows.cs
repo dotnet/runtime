bf5d983f09a89d3eab1f528cd178a8223f548f4e
NextTurn <45985406+NextTurn@users.noreply.github.com>
2020-02-07 00:00:00 +0800

    Remove unnecessary using directives

--- src/libraries/System.Diagnostics.Process/src/System/Diagnostics/ProcessManager.Windows.cs
+++ src/libraries/System.Diagnostics.Process/src/System/Diagnostics/ProcessManager.Windows.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
 using System.Runtime.InteropServices;
-using System.Threading;
 using Microsoft.Win32.SafeHandles;
 
 using static Interop.Advapi32;
