7bd369de40c5b558e30e7e99bce8823429024b93
Maryam Ariyan <maryam.ariyan@microsoft.com>
2020-02-06 14:11:44 -0800

    Update header licensing - Rmv multiple blank lines

--- src/libraries/Microsoft.Extensions.Primitives/src/StringValues.cs
+++ src/libraries/Microsoft.Extensions.Primitives/src/StringValues.cs
@@ -1,5 +1,6 @@
-// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 
 using System;
 using System.Collections;
@@ -620,7 +621,6 @@ namespace Microsoft.Extensions.Primitives
         /// <param name="other">The string array to compare to this instance.</param>
         /// <returns><c>true</c> if the value of <paramref name="other"/> is the same as this instance; otherwise, <c>false</c>.</returns>
         public bool Equals(string[] other) => Equals(this, new StringValues(other));
-
 
         /// <inheritdoc cref="Equals(StringValues, string)" />
         public static bool operator ==(StringValues left, string right) => Equals(left, new StringValues(right));
