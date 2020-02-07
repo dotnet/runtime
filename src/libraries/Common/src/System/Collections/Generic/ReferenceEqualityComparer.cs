c26711669fe20d004714bf6663100d7850f79360
Levi Broderick <levib@microsoft.com>
2020-02-07 13:28:32 -0800

    Move ReferenceEqualityComparer to common folder

--- src/libraries/Common/src/System/Collections/Generic/ReferenceEqualityComparer.cs
+++ src/libraries/Common/src/System/Collections/Generic/ReferenceEqualityComparer.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#nullable enable
+
 using System.Runtime.CompilerServices;
 
 namespace System.Collections.Generic
@@ -14,10 +16,10 @@ namespace System.Collections.Generic
     /// The <see cref="ReferenceEqualityComparer"/> type cannot be instantiated. Instead, use the <see cref="Instance"/> property
     /// to access the singleton instance of this type.
     /// </remarks>
-#if INTERNAL_REFERENCE_EQUALITY_COMPARER
-    internal
-#else
+#if SYSTEM_PRIVATE_CORELIB
     public
+#else
+    internal
 #endif
     sealed class ReferenceEqualityComparer : IEqualityComparer<object?>, IEqualityComparer
     {
