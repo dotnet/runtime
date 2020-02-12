ca6adaac796a19d70e4361a9b2a8d090c2f27637
Julien Couvreur <jcouv@microsoft.com>
2020-02-11 19:06:16 -0800

    Enable nullability on one file

--- src/libraries/Common/src/System/Collections/Generic/ReferenceEqualityComparer.cs
+++ src/libraries/Common/src/System/Collections/Generic/ReferenceEqualityComparer.cs
@@ -4,6 +4,7 @@
 
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
+#nullable enable
 
 namespace System.Collections.Generic
 {
@@ -16,7 +17,7 @@ namespace System.Collections.Generic
         {
         }
 
-        public bool Equals([AllowNull] T x, [AllowNull] T y)
+        public bool Equals(T? x, T? y)
         {
             return ReferenceEquals(x, y);
         }
