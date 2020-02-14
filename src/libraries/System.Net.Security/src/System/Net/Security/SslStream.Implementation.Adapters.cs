f5874b08b53665ce950b76b628deb63abecaee85
Tomas Weinfurt <tweinfurt@yahoo.com>
2020-02-14 15:09:47 -0800

    fix buffer handling in Tls handshake (#32267)
    * fix buffer handling in Tls handshake
    
    * feedback from review

--- src/libraries/System.Net.Security/src/System/Net/Security/SslStream.Implementation.Adapters.cs
+++ src/libraries/System.Net.Security/src/System/Net/Security/SslStream.Implementation.Adapters.cs
@@ -11,7 +11,7 @@ namespace System.Net.Security
     {
         private interface ISslIOAdapter
         {
-            ValueTask<int> ReadAsync(byte[] buffer, int offset, int count);
+            ValueTask<int> ReadAsync(Memory<byte> buffer);
             ValueTask<int> ReadLockAsync(Memory<byte> buffer);
             Task WriteLockAsync();
             ValueTask WriteAsync(byte[] buffer, int offset, int count);
@@ -29,7 +29,7 @@ namespace System.Net.Security
                 _sslStream = sslStream;
             }
 
-            public ValueTask<int> ReadAsync(byte[] buffer, int offset, int count) => _sslStream.InnerStream.ReadAsync(new Memory<byte>(buffer, offset, count), _cancellationToken);
+            public ValueTask<int> ReadAsync(Memory<byte> buffer) => _sslStream.InnerStream.ReadAsync(buffer, _cancellationToken);
 
             public ValueTask<int> ReadLockAsync(Memory<byte> buffer) => _sslStream.CheckEnqueueReadAsync(buffer);
 
@@ -46,7 +46,7 @@ namespace System.Net.Security
 
             public SyncSslIOAdapter(SslStream sslStream) => _sslStream = sslStream;
 
-            public ValueTask<int> ReadAsync(byte[] buffer, int offset, int count) => new ValueTask<int>(_sslStream.InnerStream.Read(buffer, offset, count));
+            public ValueTask<int> ReadAsync(Memory<byte> buffer) => new ValueTask<int>(_sslStream.InnerStream.Read(buffer.Span));
 
             public ValueTask<int> ReadLockAsync(Memory<byte> buffer) => new ValueTask<int>(_sslStream.CheckEnqueueRead(buffer));
 
