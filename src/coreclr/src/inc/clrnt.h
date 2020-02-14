0f3f8e1930c28b67f29990126bc2e8527e959a2e
Steve MacLean <Steve.MacLean@microsoft.com>
2020-02-14 01:28:57 -0500

    Fix arm cross OS DAC compilation (#31903)
    * Fix arm cross OS DAC compilation
    
    * Simplify #if logic
    
    Prefer !HOST_LINUX -> HOST_WINDOWS
    Use consistent control logic
    
    * Remove crosscomp.h RUNTIME_FUNCTION
    
    Clean up crosscomp.h to consistently use T_RUNTIME_FUNCTION
    
    * Remove UNWIND_HISTORY_TABLE

--- src/coreclr/src/inc/clrnt.h
+++ src/coreclr/src/inc/clrnt.h
@@ -945,7 +945,7 @@ RtlUnwindEx (
     __in_opt PEXCEPTION_RECORD ExceptionRecord,
     __in PVOID ReturnValue,
     __in PT_CONTEXT ContextRecord,
-    __in_opt PUNWIND_HISTORY_TABLE HistoryTable
+    __in_opt PVOID HistoryTable
     );
 
 EXTERN_C
@@ -956,7 +956,7 @@ RtlVirtualUnwind (
     __in DWORD HandlerType,
     __in DWORD ImageBase,
     __in DWORD ControlPc,
-    __in PRUNTIME_FUNCTION FunctionEntry,
+    __in PT_RUNTIME_FUNCTION FunctionEntry,
     __inout PT_CONTEXT ContextRecord,
     __out PVOID *HandlerData,
     __out PDWORD EstablisherFrame,
