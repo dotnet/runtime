dfa5d039140035636e2c88aa7934f80d9c9d2aa6
Vladimir Sadov <vsadov@microsoft.com>
2020-02-16 15:42:01 -0800

    GC polling in unboxing JIT helpers (#32353)
    * GC polling in unboxing JIT helpers
    
    * further tuning the type check for enums

--- src/coreclr/src/vm/jitinterface.h
+++ src/coreclr/src/vm/jitinterface.h
@@ -246,6 +246,7 @@ extern "C" FCDECL2(VOID, JIT_WriteBarrierEnsureNonHeapTarget, Object **dst, Obje
 
 extern "C" FCDECL2(Object*, ChkCastAny_NoCacheLookup, CORINFO_CLASS_HANDLE type, Object* obj);
 extern "C" FCDECL2(Object*, IsInstanceOfAny_NoCacheLookup, CORINFO_CLASS_HANDLE type, Object* obj);
+extern "C" FCDECL2(LPVOID, Unbox_Helper, CORINFO_CLASS_HANDLE type, Object* obj);
 
 extern "C" FCDECL1(void, JIT_InternalThrow, unsigned exceptNum);
 extern "C" FCDECL1(void*, JIT_InternalThrowFromHelper, unsigned exceptNum);
