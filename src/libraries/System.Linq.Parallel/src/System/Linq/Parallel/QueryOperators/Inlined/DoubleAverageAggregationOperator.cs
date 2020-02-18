a235ffc9281475c482de009b788769cd4d959716
Stephen Toub <stoub@microsoft.com>
2020-02-17 22:36:56 -0500

    Use CancellationToken.ThrowIfCancellationRequested directly where applicable

--- src/libraries/System.Linq.Parallel/src/System/Linq/Parallel/QueryOperators/Inlined/DoubleAverageAggregationOperator.cs
+++ src/libraries/System.Linq.Parallel/src/System/Linq/Parallel/QueryOperators/Inlined/DoubleAverageAggregationOperator.cs
@@ -120,7 +120,7 @@ namespace System.Linq.Parallel
                     do
                     {
                         if ((i++ & CancellationState.POLL_INTERVAL) == 0)
-                            CancellationState.ThrowIfCanceled(_cancellationToken);
+                            _cancellationToken.ThrowIfCancellationRequested();;
 
                         checked
                         {
