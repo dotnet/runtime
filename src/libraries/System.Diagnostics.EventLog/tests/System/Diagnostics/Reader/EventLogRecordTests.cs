48a222cbbbee6c39d622a9daee86f8f185263d5f
Viktor Hofer <viktor.hofer@microsoft.com>
2020-02-19 00:01:57 +0100

    Move EventLog tests to not run on CI (#32501)
    * Move EventLog tests to not run on CI
    
    We are hitting Win32 API issues when calling specific EventLog features.
    As the risk of changes in EventLog is low we decided to turn off some
    tests in CI and run them manually before a release.

--- src/libraries/System.Diagnostics.EventLog/tests/System/Diagnostics/Reader/EventLogRecordTests.cs
+++ src/libraries/System.Diagnostics.EventLog/tests/System/Diagnostics/Reader/EventLogRecordTests.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
 using System.Security.Principal;
 using Xunit;
+using Microsoft.DotNet.XUnitExtensions;
 
 namespace System.Diagnostics.Tests
 {
@@ -29,6 +30,7 @@ namespace System.Diagnostics.Tests
         }
 
         [ActiveIssue("https://github.com/dotnet/corefx/issues/34547")]
+        [Trait(XunitConstants.Category, XunitConstants.IgnoreForCI)] // Unreliable Win32 API call
         [ConditionalTheory(typeof(Helpers), nameof(Helpers.SupportsEventLogs))]
         [SkipOnTargetFramework(TargetFrameworkMonikers.NetFramework)]
         [InlineData("System")]
