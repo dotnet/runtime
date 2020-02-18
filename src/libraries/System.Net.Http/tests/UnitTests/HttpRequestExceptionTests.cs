023fb4e9478e4dfaaa9e324a1ddecf48f0841fe8
Yaakov Smith <yaakov.smith@wisetechglobal.com>
2020-02-18 15:25:01 +1100

    Remove unused usings from test code.

--- src/libraries/System.Net.Http/tests/UnitTests/HttpRequestExceptionTests.cs
+++ src/libraries/System.Net.Http/tests/UnitTests/HttpRequestExceptionTests.cs
@@ -2,12 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Net.Http;
-using Microsoft.DotNet.RemoteExecutor;
 using Xunit;
-using Xunit.Abstractions;
 
 namespace System.Net.Http.Tests
 {
