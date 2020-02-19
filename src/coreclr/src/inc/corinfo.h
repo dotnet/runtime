65cdf4265fa345aa9518afe73fb8f7519935c45f
Jan Kotas <jkotas@microsoft.com>
2020-02-18 20:17:56 -0800

    Fix build break

--- src/coreclr/src/inc/corinfo.h
+++ src/coreclr/src/inc/corinfo.h
@@ -1528,7 +1528,6 @@ enum CorInfoIsAccessAllowedResult
 {
     CORINFO_ACCESS_ALLOWED = 0,           // Call allowed
     CORINFO_ACCESS_ILLEGAL = 1,           // Call not allowed
-    CORINFO_ACCESS_RUNTIME_CHECK = 2,     // Ask at runtime whether to allow the call or not
 };
 
 
