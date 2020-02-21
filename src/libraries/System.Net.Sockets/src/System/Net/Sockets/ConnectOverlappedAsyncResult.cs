b9f793671c949c48684e147dcbc52d1690c866f7
Eric Erhardt <eric.erhardt@microsoft.com>
2020-02-20 19:53:33 -0600

    Remove inline #nullable lines

--- src/libraries/System.Net.Sockets/src/System/Net/Sockets/ConnectOverlappedAsyncResult.cs
+++ src/libraries/System.Net.Sockets/src/System/Net/Sockets/ConnectOverlappedAsyncResult.cs
@@ -1,7 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
-#nullable enable
+
 namespace System.Net.Sockets
 {
     // ConnectOverlappedAsyncResult - used to take care of storage for async Socket BeginConnect call.
--- src/libraries/System.Net.Sockets/src/System/Net/Sockets/UnixDomainSocketEndPoint.Windows.cs
+++ src/libraries/System.Net.Sockets/src/System/Net/Sockets/UnixDomainSocketEndPoint.Windows.cs
@@ -1,7 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
-#nullable enable
+
 namespace System.Net.Sockets
 {
     /// <summary>Represents a Unix Domain Socket endpoint as a path.</summary>
