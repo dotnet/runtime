d1a96f3131a5172377f6dacfbee36278b0c562db
jaykrell <jaykrell@users.noreply.github.com>
2020-02-23 19:19:46 +0000

    [interp] Use alloca for InterpFrame.

--- src/mono/mono/mini/interp-stubs.c
+++ src/mono/mono/mini/interp-stubs.c
@@ -71,13 +71,6 @@ stub_frame_get_res (MonoInterpFrameHandle frame)
 	return NULL;
 }
 
-static gpointer
-stub_frame_get_native_stack_addr (MonoInterpFrameHandle frame)
-{
-	g_assert_not_reached ();
-	return NULL;
-}
-
 static void
 stub_start_single_stepping (void)
 {
