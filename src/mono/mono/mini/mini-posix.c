ba76c377203bc852c6e2c28f4f4a24c7ae71ea1e
monojenkins <jo.shields+jenkins@xamarin.com>
2020-02-24 14:56:07 -0500

    [merp] Increase buffer size for state dump (#32134)
    Addresses https://github.com/mono/mono/issues/18796
    
    Co-authored-by: Alexis Christoforides <alexis@thenull.net>

--- src/mono/mono/mini/mini-posix.c
+++ src/mono/mono/mini/mini-posix.c
@@ -1030,7 +1030,7 @@ dump_native_stacktrace (const char *signal, MonoContext *mctx)
 			mono_summarize_timeline_start ();
 			mono_summarize_toggle_assertions (TRUE);
 
-			int mono_max_summary_len = 500000;
+			int mono_max_summary_len = 10000000;
 			int mono_state_tmp_file_tag = 1;
 			mono_state_alloc_mem (&merp_mem, mono_state_tmp_file_tag, mono_max_summary_len * sizeof (gchar));
 
