3ec191bdce87d8e89a482ba1e6420f81c798c034
Sam Harwell <Sam.Harwell@microsoft.com>
2020-02-26 10:27:22 -0800

    Normalize all file headers to the expected Apache 2.0 license
    Commit migrated from https://github.com/dotnet/Extensions/commit/cec6e75f0ce3145c76acd4c8a8c14093696a03ba

--- src/libraries/Microsoft.Extensions.DependencyInjection/tests/Fakes/StructService.cs
+++ src/libraries/Microsoft.Extensions.DependencyInjection/tests/Fakes/StructService.cs
@@ -1,4 +1,7 @@
-﻿namespace Microsoft.Extensions.DependencyInjection.Fakes
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Microsoft.Extensions.DependencyInjection.Fakes
 {
     public struct StructService
     {
