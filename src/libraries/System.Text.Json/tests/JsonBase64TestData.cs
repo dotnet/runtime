3b4df302b88e0f8f1449b31fe909b33181f9cc2a
Alan Pinkert <alan.pinkert@gmail.com>
2020-02-29 17:00:35 -0500

    moved base64 test data in class, added test case

---
+++ src/libraries/System.Text.Json/tests/JsonBase64TestData.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+
+namespace System.Text.Json.Tests
+{
+    internal class JsonBase64TestData
+    {
+        public static IEnumerable<object[]> ValidBase64Tests()
+        {
+            yield return new object[] { "\"ABC=\"" };
+            yield return new object[] { "\"AB+D\"" };
+            yield return new object[] { "\"ABCD\"" };
+            yield return new object[] { "\"ABC/\"" };
+            yield return new object[] { "\"++++\"" };
+            yield return new object[] { GenerateRandomValidLargeString() };
+        }
+
+        public static IEnumerable<object[]> InvalidBase64Tests()
+        {
+            yield return new object[] { "\"ABC===\"" };
+            yield return new object[] { "\"ABC\"" };
+            yield return new object[] { "\"ABC!\"" };
+            yield return new object[] { GenerateRandomInvalidLargeString(true) };
+            yield return new object[] { GenerateRandomInvalidLargeString(false) };
+        }
+
+        private static string GenerateRandomValidLargeString()
+        {
+            var random = new Random(42);
+            var charArray = new char[502];
+            charArray[0] = '"';
+            for (int i = 1; i < charArray.Length; i++)
+            {
+                charArray[i] = (char)random.Next('A', 'Z'); // ASCII values (between 65 and 90) that constitute valid base 64 string.
+            }
+            charArray[charArray.Length - 1] = '"';
+            var jsonString = new string(charArray);
+            return jsonString;
+        }
+
+        private static string GenerateRandomInvalidLargeString(bool includeEscapedCharacter)
+        {
+            var random = new Random(42);
+            var charArray = new char[500];
+            charArray[0] = '"';
+            for (int i = 1; i < charArray.Length; i++)
+            {
+                charArray[i] = (char)random.Next('?', '\\'); // ASCII values (between 63 and 91) that don't need to be escaped.
+            }
+
+            if (includeEscapedCharacter)
+            {
+                charArray[256] = '\\';
+                charArray[257] = '"';
+            }
+
+            charArray[charArray.Length - 1] = '"';
+            var jsonString = new string(charArray);
+            return jsonString;
+        }
+    }
+}
