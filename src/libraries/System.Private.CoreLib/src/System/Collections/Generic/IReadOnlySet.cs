db9067e380cfc01bf13c212f13f53734b2e9f87d
jjjlalonde@gmail.com <jjjlalonde@gmail.com>
2020-03-01 15:57:11 -0500

    Add lang words to comments and fix spelling

--- src/libraries/System.Private.CoreLib/src/System/Collections/Generic/IReadOnlySet.cs
+++ src/libraries/System.Private.CoreLib/src/System/Collections/Generic/IReadOnlySet.cs
@@ -14,49 +14,49 @@ namespace System.Collections.Generic
         /// Determines if the set contains a specific item
         /// </summary>
         /// <param name="item">The item to check if the set contains.</param>
-        /// <returns>true if found; otherwise false.</returns>
+        /// <returns><see langword="true" /> if found; otherwise <see langword="false" />.</returns>
         bool Contains(T item);
         /// <summary>
         /// Determines whether the current set is a proper (strict) subset of a specified collection.
         /// </summary>
         /// <param name="other">The collection to compare to the current set.</param>
-        /// <returns>true if the current set is a proper subset of other; otherwise false</returns>
-        /// <exception cref="ArgumentNullException">other is null.</exception>
+        /// <returns><see langword="true" /> if the current set is a proper subset of other; otherwise <see langword="false" />.</returns>
+        /// <exception cref="ArgumentNullException">other is <see langword="null" />.</exception>
         bool IsProperSubsetOf(IEnumerable<T> other);
         /// <summary>
         /// Determines whether the current set is a proper (strict) superset of a specified collection.
         /// </summary>
         /// <param name="other">The collection to compare to the current set.</param>
-        /// <returns>true if the collection is a proper superset of other; otherwise false.</returns>
-        /// <exception cref="ArgumentNullException">other is null.</exception>
+        /// <returns><see langword="true" /> if the collection is a proper superset of other; otherwise <see langword="false" />.</returns>
+        /// <exception cref="ArgumentNullException">other is <see langword="null" />.</exception>
         bool IsProperSupersetOf(IEnumerable<T> other);
         /// <summary>
         /// Determine whether the current set is a subset of a specified collection.
         /// </summary>
         /// <param name="other">The collection to compare to the current set.</param>
-        /// <returns>true if the current set is a subset of other; otherwise false.</returns>
-        /// <exception cref="ArgumentNullException">other is null.</exception>
+        /// <returns><see langword="true" /> if the current set is a subset of other; otherwise <see langword="false" />.</returns>
+        /// <exception cref="ArgumentNullException">other is <see langword="null" />.</exception>
         bool IsSubsetOf(IEnumerable<T> other);
         /// <summary>
         /// Determine whether the current set is a super set of a specified collection.
         /// </summary>
         /// <param name="other">The collection to compare to the current set</param>
-        /// <returns>true if the current set is a subset of other; otherwise false.</returns>
-        /// <exception cref="ArgumentNullException">other is null.</exception>
+        /// <returns><see langword="true" /> if the current set is a subset of other; otherwise <see langword="false" />.</returns>
+        /// <exception cref="ArgumentNullException">other is <see langword="null" />.</exception>
         bool IsSupersetOf(IEnumerable<T> other);
         /// <summary>
         /// Determines whether the current set overlaps with the specified collection.
         /// </summary>
         /// <param name="other">The collection to compare to the current set.</param>
-        /// <returns>ture if the currnet set and other share at least one common eleemnt; otherwise, false.</returns>
-        /// <exception cref="ArgumentNullException">other is null.</exception>
+        /// <returns><see langword="true" />if the current set and other share at least one common element; otherwise, <see langword="false" />.</returns>
+        /// <exception cref="ArgumentNullException">other is <see langword="null" />.</exception>
         bool Overlaps(IEnumerable<T> other);
         /// <summary>
         /// Determines whether the current set and the specified collection contain the same elements.
         /// </summary>
         /// <param name="other">The collection to compare to the current set.</param>
-        /// <returns>true if the current set is equal to other; otherwise, false.</returns>
-        /// <exception cref="ArgumentNullException">other is null.</exception>
+        /// <returns><see langword="true" /> if the current set is equal to other; otherwise, <see langword="false" />.</returns>
+        /// <exception cref="ArgumentNullException">other is <see langword="null" />.</exception>
         bool SetEquals(IEnumerable<T> other);
     }
 }
