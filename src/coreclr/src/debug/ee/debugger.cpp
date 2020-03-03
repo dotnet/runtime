fadd39791c4d12cd6999b3c0bc6cbbaf33413e04
Jan Kotas <jkotas@microsoft.com>
2020-03-02 17:04:21 -0800

    Delete some more

--- src/coreclr/src/debug/ee/debugger.cpp
+++ src/coreclr/src/debug/ee/debugger.cpp
@@ -9434,64 +9434,6 @@ void Debugger::SendCreateAppDomainEvent(AppDomain * pRuntimeAppDomain)
 }
 
 
-
-
-//
-// SendExitAppDomainEvent is called when an app domain is destroyed.
-//
-void Debugger::SendExitAppDomainEvent(AppDomain* pRuntimeAppDomain)
-{
-    CONTRACTL
-    {
-        MAY_DO_HELPER_THREAD_DUTY_THROWS_CONTRACT;
-        MAY_DO_HELPER_THREAD_DUTY_GC_TRIGGERS_CONTRACT;
-    }
-    CONTRACTL_END;
-
-    if (CORDBUnrecoverableError(this))
-        return;
-
-    LOG((LF_CORDB, LL_INFO100, "D::EAD: Exit AppDomain 0x%08x.\n",
-        pRuntimeAppDomain));
-
-    STRESS_LOG2(LF_CORDB, LL_INFO10000, "D::EAD: AppDomain exit:%#08x, %#08x\n",
-            pRuntimeAppDomain, CORDebuggerAttached());
-
-    Thread *thread = g_pEEInterface->GetThread();
-    // Prevent other Runtime threads from handling events.
-    SENDIPCEVENT_BEGIN(this, thread);
-
-    if (CORDebuggerAttached())
-    {
-        // Send the exit appdomain event to the Right Side.
-        DebuggerIPCEvent* ipce = m_pRCThread->GetIPCEventSendBuffer();
-        InitIPCEvent(ipce,
-                     DB_IPCE_EXIT_APP_DOMAIN,
-                     thread,
-                     pRuntimeAppDomain);
-        m_pRCThread->SendIPCEvent();
-
-        // Delete any left over modules for this appdomain.
-        // Note that we're doing this under the lock.
-        if (m_pModules != NULL)
-        {
-            DebuggerDataLockHolder ch(this);
-            m_pModules->RemoveModules(pRuntimeAppDomain);
-        }
-
-        // Stop all Runtime threads
-        TrapAllRuntimeThreads();
-    }
-    else
-    {
-        LOG((LF_CORDB,LL_INFO1000, "D::EAD: Skipping SendIPCEvent because RS detached."));
-    }
-
-    SENDIPCEVENT_END;
-}
-
-
-
 //
 // LoadAssembly is called when a new Assembly gets loaded.
 //
@@ -14906,11 +14848,9 @@ ErrExit:
     // UnLock the list
     m_pAppDomainCB->Unlock();
 
-    // send event to debugger if one is attached
-    if (CORDebuggerAttached())
-    {
-        SendExitAppDomainEvent(pAppDomain);
-    }
+    //
+    // The Debugger expects to never get an unload event for the default AppDomain.
+    //
 
     return hr;
 }
