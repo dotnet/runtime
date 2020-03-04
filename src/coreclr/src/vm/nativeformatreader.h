36582f4834318f9a3cd64ecfeb084bf988b780a9
NextTurn <45985406+NextTurn@users.noreply.github.com>
2020-03-04 13:48:35 +0800

    Revert "Check __GNUC__"
    This reverts commit c3ed7430b4ecd426161925d054284c6b471df156.

--- src/coreclr/src/vm/nativeformatreader.h
+++ src/coreclr/src/vm/nativeformatreader.h
@@ -667,7 +667,7 @@ namespace NativeFormat
         }
 
         bool
-#if defined(USE_INTEL_INTRINSICS_FOR_CUCKOO_FILTER) && defined(__GNUC__)
+#ifdef USE_INTEL_INTRINSICS_FOR_CUCKOO_FILTER
         __attribute__((__target__("sse2")))
 #endif
         MayExist(UInt32 hashcode, UInt16 fingerprint)
