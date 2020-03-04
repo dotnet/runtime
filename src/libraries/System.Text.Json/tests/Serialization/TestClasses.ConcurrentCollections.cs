b5de9f0d76732c3e462228d7f7c4bc209ba123e8
Alan Pinkert <alan.pinkert@gmail.com>
2020-03-03 21:12:40 -0500

    added tests for concurrent collection private constructors

---
+++ src/libraries/System.Text.Json/tests/Serialization/TestClasses.ConcurrentCollections.cs
@@ -0,0 +1,18 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Concurrent;
+
+namespace System.Text.Json.Serialization.Tests
+{
+    public class GenericConcurrentQueuePrivateConstructor<T> : ConcurrentQueue<T>
+    {
+        private GenericConcurrentQueuePrivateConstructor() { }
+    }
+
+    public class GenericConcurrentStackPrivateConstructor<T> : ConcurrentStack<T>
+    {
+        private GenericConcurrentStackPrivateConstructor() { }
+    }
+}
