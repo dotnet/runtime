7b2252f0f29b61d9cce53ffba97c3af67c0181b6
EgorBo <egorbo@gmail.com>
2020-03-05 17:12:40 +0300

    Add more test cases

--- src/coreclr/tests/src/JIT/opt/InstructionCombining/CondToBranchlessExpr.cs
+++ src/coreclr/tests/src/JIT/opt/InstructionCombining/CondToBranchlessExpr.cs
@@ -21,30 +21,35 @@ public class Program
             .OrderBy(m => m.Name)
             .ToArray();
 
-        int[] testInput = Enumerable.Range(-100, 100)
-            .Concat(new [] {int.MaxValue - 1, int.MaxValue})
+        long[] testInput = Enumerable.Range(-256, 256)
+            .Select(i => (long)i)
+            .Concat(new [] { (long)int.MaxValue - 1, int.MaxValue, long.MaxValue - 1, long.MaxValue })
             .ToArray();
 
         for (int i = 0; i < allTestsWithOpt.Length; i++)
         {
             for (int j = 0; j < testInput.Length; j++)
             {
-                var invokeArgs = new object[] { testInput[j] };
-                int actual = (int)allTestsWithOpt[i].Invoke(testsWithOpt, invokeArgs);
-                int expected = (int)allTestsWithoutOpt[i].Invoke(testsWithoutOpt, invokeArgs);
+                object[] invokeArgs;
+                Type argType = allTestsWithOpt[i].GetParameters()[0].ParameterType;
+                if (argType == typeof(int))
+                    invokeArgs = new object[] {unchecked((int) testInput[j])};
+                else
+                    invokeArgs = new object[] {testInput[j]};
 
-                if (actual != expected)
+                object actualBoxed = allTestsWithOpt[i].Invoke(testsWithOpt, invokeArgs);
+                object expectedBoxed = allTestsWithoutOpt[i].Invoke(testsWithoutOpt, invokeArgs);
+
+                if (!actualBoxed.Equals(expectedBoxed))
                 {
-                    Console.WriteLine($"{allTestsWithOpt[i].Name}: {actual} != {expected}");
+                    Console.WriteLine($"{allTestsWithOpt[i].Name}: {actualBoxed} != {expectedBoxed}");
                     returnCode++;
                 }
             }
         }
-
         return returnCode;
     }
 }
-
 
 public class Tests
 {
@@ -62,7 +67,7 @@ public class Tests
     public int Test9(int a) => a == 42 ? Cns(4) : Cns(2);
     public int Test10(int a) => a == 42 ? Cns(4) : Cns(6);
     public int Test11(int a) => a == 42 ? Cns(int.MaxValue) : Cns(int.MaxValue - 1);
-    public int Test12(int a) => a == 42 ? Cns(int.MaxValue) : Cns(int.MaxValue);
+    public int Test12(int a) => a == 42 ? Cns(int.MaxValue) : Cns(int.MinValue);
     public int Test13(int a) => a == 42 ? Cns(int.MaxValue - 1) : Cns(int.MaxValue);
 
     public int Test14(int a) => a != 42 ? Cns(0) : Cns(1);
@@ -76,7 +81,7 @@ public class Tests
     public int Test22(int a) => a != 42 ? Cns(4) : Cns(2);
     public int Test23(int a) => a != 42 ? Cns(4) : Cns(6);
     public int Test24(int a) => a != 42 ? Cns(int.MaxValue) : Cns(int.MaxValue - 1);
-    public int Test25(int a) => a != 42 ? Cns(int.MaxValue) : Cns(int.MaxValue);
+    public int Test25(int a) => a != 42 ? Cns(int.MaxValue) : Cns(int.MinValue);
     public int Test26(int a) => a != 42 ? Cns(int.MaxValue - 1) : Cns(int.MaxValue);
 
     public int Test27(int a) => a > 42 ? Cns(0) : Cns(1);
@@ -90,7 +95,7 @@ public class Tests
     public int Test35(int a) => a > 42 ? Cns(4) : Cns(2);
     public int Test36(int a) => a > 42 ? Cns(4) : Cns(6);
     public int Test37(int a) => a > 42 ? Cns(int.MaxValue) : Cns(int.MaxValue - 1);
-    public int Test38(int a) => a > 42 ? Cns(int.MaxValue) : Cns(int.MaxValue);
+    public int Test38(int a) => a > 42 ? Cns(int.MaxValue) : Cns(int.MinValue);
     public int Test39(int a) => a > 42 ? Cns(int.MaxValue - 1) : Cns(int.MaxValue);
 
     // GT_TEST_/test instruction
@@ -105,7 +110,7 @@ public class Tests
     public int Test48(int a) => a == 0 ? Cns(4) : Cns(2);
     public int Test49(int a) => a == 0 ? Cns(4) : Cns(6);
     public int Test50(int a) => a == 0 ? Cns(int.MaxValue) : Cns(int.MaxValue - 1);
-    public int Test51(int a) => a == 0 ? Cns(int.MaxValue) : Cns(int.MaxValue);
+    public int Test51(int a) => a == 0 ? Cns(int.MaxValue) : Cns(int.MinValue);
     public int Test52(int a) => a == 0 ? Cns(int.MaxValue - 1) : Cns(int.MaxValue);
 
     public int Test53(int a) => a != 0 ? Cns(0) : Cns(1);
@@ -119,8 +124,47 @@ public class Tests
     public int Test61(int a) => a != 0 ? Cns(4) : Cns(2);
     public int Test62(int a) => a != 0 ? Cns(4) : Cns(6);
     public int Test63(int a) => a != 0 ? Cns(int.MaxValue) : Cns(int.MaxValue - 1);
-    public int Test64(int a) => a != 0 ? Cns(int.MaxValue) : Cns(int.MaxValue);
+    public int Test64(int a) => a != 0 ? Cns(int.MaxValue) : Cns(int.MinValue);
     public int Test65(int a) => a != 0 ? Cns(int.MaxValue - 1) : Cns(int.MaxValue);
+
+    public long Test66(long a) => a != 0 ? Cns(0) : Cns(1);
+    public long Test67(long a) => a != 0 ? Cns(0) : Cns(0);
+    public long Test68(long a) => a != 0 ? Cns(1) : Cns(0);
+    public long Test69(long a) => a != 0 ? Cns(0) : Cns(-1);
+    public long Test70(long a) => a != 0 ? Cns(-1) : Cns(0);
+    public long Test71(long a) => a != 0 ? Cns(1) : Cns(2);
+    public long Test72(long a) => a != 0 ? Cns(2) : Cns(2);
+    public long Test73(long a) => a != 0 ? Cns(3) : Cns(2);
+    public long Test74(long a) => a != 0 ? Cns(4) : Cns(2);
+    public long Test75(long a) => a != 0 ? Cns(4) : Cns(6);
+    public long Test76(long a) => a != 0 ? Cns(long.MaxValue) : Cns(long.MaxValue - 1);
+    public long Test77(long a) => a != 0 ? Cns(long.MaxValue) : Cns(long.MinValue);
+    public long Test78(long a) => a != 0 ? Cns(long.MaxValue - 1) : Cns(long.MaxValue);
+
+    public bool Test79(long a)
+    {
+        if (a == 0)
+            return Cns(true);
+        return Cns(false);
+    }
+    public bool Test80(long a)
+    {
+        if (a == 42)
+            return Cns(false);
+        return Cns(true);
+    }
+    public int Test81(long a)
+    {
+        if (a != 42)
+            return Cns(1);
+        return Cns(0);
+    }
+    public int Test82(long a)
+    {
+        if (a > 42)
+            return Cns(0);
+        return Cns(1);
+    }
 }
 
 public class TestsWithoutOptimization
@@ -139,7 +183,7 @@ public class TestsWithoutOptimization
     public int Test9(int a) => a == 42 ? Var(4) : Var(2);
     public int Test10(int a) => a == 42 ? Var(4) : Var(6);
     public int Test11(int a) => a == 42 ? Var(int.MaxValue) : Var(int.MaxValue - 1);
-    public int Test12(int a) => a == 42 ? Var(int.MaxValue) : Var(int.MaxValue);
+    public int Test12(int a) => a == 42 ? Var(int.MaxValue) : Var(int.MinValue);
     public int Test13(int a) => a == 42 ? Var(int.MaxValue - 1) : Var(int.MaxValue);
 
     public int Test14(int a) => a != 42 ? Var(0) : Var(1);
@@ -153,7 +197,7 @@ public class TestsWithoutOptimization
     public int Test22(int a) => a != 42 ? Var(4) : Var(2);
     public int Test23(int a) => a != 42 ? Var(4) : Var(6);
     public int Test24(int a) => a != 42 ? Var(int.MaxValue) : Var(int.MaxValue - 1);
-    public int Test25(int a) => a != 42 ? Var(int.MaxValue) : Var(int.MaxValue);
+    public int Test25(int a) => a != 42 ? Var(int.MaxValue) : Var(int.MinValue);
     public int Test26(int a) => a != 42 ? Var(int.MaxValue - 1) : Var(int.MaxValue);
 
     public int Test27(int a) => a > 42 ? Var(0) : Var(1);
@@ -167,7 +211,7 @@ public class TestsWithoutOptimization
     public int Test35(int a) => a > 42 ? Var(4) : Var(2);
     public int Test36(int a) => a > 42 ? Var(4) : Var(6);
     public int Test37(int a) => a > 42 ? Var(int.MaxValue) : Var(int.MaxValue - 1);
-    public int Test38(int a) => a > 42 ? Var(int.MaxValue) : Var(int.MaxValue);
+    public int Test38(int a) => a > 42 ? Var(int.MaxValue) : Var(int.MinValue);
     public int Test39(int a) => a > 42 ? Var(int.MaxValue - 1) : Var(int.MaxValue);
 
     // GT_TEST_/test instruction
@@ -182,7 +226,7 @@ public class TestsWithoutOptimization
     public int Test48(int a) => a == 0 ? Var(4) : Var(2);
     public int Test49(int a) => a == 0 ? Var(4) : Var(6);
     public int Test50(int a) => a == 0 ? Var(int.MaxValue) : Var(int.MaxValue - 1);
-    public int Test51(int a) => a == 0 ? Var(int.MaxValue) : Var(int.MaxValue);
+    public int Test51(int a) => a == 0 ? Var(int.MaxValue) : Var(int.MinValue);
     public int Test52(int a) => a == 0 ? Var(int.MaxValue - 1) : Var(int.MaxValue);
 
     public int Test53(int a) => a != 0 ? Var(0) : Var(1);
@@ -196,6 +240,45 @@ public class TestsWithoutOptimization
     public int Test61(int a) => a != 0 ? Var(4) : Var(2);
     public int Test62(int a) => a != 0 ? Var(4) : Var(6);
     public int Test63(int a) => a != 0 ? Var(int.MaxValue) : Var(int.MaxValue - 1);
-    public int Test64(int a) => a != 0 ? Var(int.MaxValue) : Var(int.MaxValue);
+    public int Test64(int a) => a != 0 ? Var(int.MaxValue) : Var(int.MinValue);
     public int Test65(int a) => a != 0 ? Var(int.MaxValue - 1) : Var(int.MaxValue);
+
+    public long Test66(long a) => a != 0 ? Var(0) : Var(1);
+    public long Test67(long a) => a != 0 ? Var(0) : Var(0);
+    public long Test68(long a) => a != 0 ? Var(1) : Var(0);
+    public long Test69(long a) => a != 0 ? Var(0) : Var(-1);
+    public long Test70(long a) => a != 0 ? Var(-1) : Var(0);
+    public long Test71(long a) => a != 0 ? Var(1) : Var(2);
+    public long Test72(long a) => a != 0 ? Var(2) : Var(2);
+    public long Test73(long a) => a != 0 ? Var(3) : Var(2);
+    public long Test74(long a) => a != 0 ? Var(4) : Var(2);
+    public long Test75(long a) => a != 0 ? Var(4) : Var(6);
+    public long Test76(long a) => a != 0 ? Var(long.MaxValue) : Var(long.MaxValue - 1);
+    public long Test77(long a) => a != 0 ? Var(long.MaxValue) : Var(long.MinValue);
+    public long Test78(long a) => a != 0 ? Var(long.MaxValue - 1) : Var(long.MaxValue);
+
+    public bool Test79(long a)
+    {
+        if (a == 0)
+            return Var(true);
+        return Var(false);
+    }
+    public bool Test80(long a)
+    {
+        if (a == 42)
+            return Var(false);
+        return Var(true);
+    }
+    public int Test81(long a)
+    {
+        if (a != 42)
+            return Var(1);
+        return Var(0);
+    }
+    public int Test82(long a)
+    {
+        if (a > 42)
+            return Var(0);
+        return Var(1);
+    }
 }
