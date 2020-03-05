5f9b655926d714966dda9ee4b35ea96321c06272
EgorBo <egorbo@gmail.com>
2020-03-06 02:10:03 +0300

    Remove Padding.cs dup

--- src/libraries/Common/src/Internal/Padding.cs
+++ src/libraries/Common/src/Internal/Padding.cs
@@ -10,7 +10,11 @@ namespace Internal
     internal static class PaddingHelpers
     {
         /// <summary>A size greater than or equal to the size of the most common CPU cache lines.</summary>
+#if TARGET_ARM64
         internal const int CACHE_LINE_SIZE = 128;
+#else
+        internal const int CACHE_LINE_SIZE = 64;
+#endif
     }
 
     /// <summary>Padding structure used to minimize false sharing</summary>
