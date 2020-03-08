a5dd1380fc9f905abde2f4bfd0e0be489c25bf2c
Marek Safar <marek.safar@gmail.com>
2020-03-08 10:06:02 +0100

    When doing local variables analysis don't assume they are always… (dotnet/linker#978)
    * When doing local variables analysis don't assume they are always assigned.
    
    Fixes dotnet/linker#927
    
    * Add test
    
    Commit migrated from https://github.com/dotnet/linker/commit/bd4b7ab7707690e27858dc62896fc5a0e1306be7

---
+++ src/tools/illink/test/Mono.Linker.Tests.Cases/UnreachableBlock/UninitializedLocals.cs
@@ -0,0 +1,19 @@
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
+using Mono.Linker.Tests.Cases.Expectations.Metadata;
+
+namespace Mono.Linker.Tests.Cases.UnreachableBlock
+{
+	[Define ("IL_ASSEMBLY_AVAILABLE")]
+	[SetupCompileBefore ("library.dll", new [] { "Dependencies/LocalsWithoutStore.il" })]
+	[SetupLinkerArgument ("--enable-opt", "ipconstprop")]
+	[SkipPeVerify]
+	public class UninitializedLocals
+	{
+		public static void Main()
+		{
+#if IL_ASSEMBLY_AVAILABLE
+			Mono.Linker.Tests.Cases.UnreachableBlock.Dependencies.ClassA.Method_1 ();
+#endif
+		}
+	}
+}
