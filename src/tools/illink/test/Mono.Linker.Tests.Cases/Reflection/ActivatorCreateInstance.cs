bad61349ebcb71a231ebff2391147db805d59749
Michael Voorhees <michaelv@unity3d.com>
2020-03-11 11:13:28 -0400

    Fixes to reflection tests when running on windows
    Need to explicitly use roslyn in order for these tests to pass on windows.  Otherwise they fail complaining about the nameof() usages in attributes.
    
    Commit migrated from https://github.com/dotnet/linker/commit/9e030cd22f14f4304c2ccd34b13aea0dae978257

--- src/tools/illink/test/Mono.Linker.Tests.Cases/Reflection/ActivatorCreateInstance.cs
+++ src/tools/illink/test/Mono.Linker.Tests.Cases/Reflection/ActivatorCreateInstance.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Reflection;
 using Mono.Linker.Tests.Cases.Expectations.Assertions;
+using Mono.Linker.Tests.Cases.Expectations.Metadata;
 
 namespace Mono.Linker.Tests.Cases.Reflection
 {
+	[SetupCSharpCompilerToUse ("csc")]
 	public class ActivatorCreateInstance
 	{
 		[UnrecognizedReflectionAccessPattern(
