b8ff8f16022e65e9d4253c8e0c97eab5ff5c47a7
Steve MacLean <Steve.MacLean@Microsoft.com>
2020-03-11 15:15:11 -0400

    Fix typo

--- src/coreclr/src/inc/daccess.h
+++ src/coreclr/src/inc/daccess.h
@@ -2460,7 +2460,7 @@ typedef DPTR(PTR_PCODE) PTR_PTR_PCODE;
 
 // For cross compilation, controlling type layout is important
 // We add a simple macro here which defines DAC_ALIGNAS to the C++11 alignas operator
-// This helps force the algnment of the next member
+// This helps force the alignment of the next member
 // For most cross compilation cases the layout of types simply works
 // There are a few cases (where this macro is helpful) which are not consistent accross platforms:
 // - Base class whose size is padded to its align size.  On Linux the gcc/clang
