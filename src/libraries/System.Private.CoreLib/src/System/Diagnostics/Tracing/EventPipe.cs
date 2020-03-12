70bfd2ac9b844701c361b90b90ef399ae6fec483
Johan Lorensson <lateralusx.github@gmail.com>
2020-03-12 12:35:50 +0100

    Share managed EventSource/EventPipe implementation between CoreCLR and Mono. (#33425)
    First changes needed in order to start sharing managed EventSource/EventPipe
    code between CoreCLR and Mono runtime. Sharable code has been moved into
    S.P.C library project and split into runtime specific source files when
    needed, kept within each runtime specific S.P.C project files.
    
    Mono runtime has been extended with a set of icalls needed by
    EventPipeInternal, just to make sure EventPipe can be initialized,
    current Mono runtime implementation returns dummy values.
    
    Current change also enables ETW provider support on Mono Windows runtime.
    
    NOTE, this is just initial changes needed in order to share managed
    EventSource/EventPipe code between runtimes. Future changes will incrementally
    add EventPipe native code into Mono runtime.

--- src/libraries/System.Private.CoreLib/src/System/Diagnostics/Tracing/EventPipe.cs
+++ src/libraries/System.Private.CoreLib/src/System/Diagnostics/Tracing/EventPipe.cs
@@ -206,7 +206,7 @@ namespace System.Diagnostics.Tracing
         }
     }
 
-    internal static class EventPipeInternal
+    internal static partial class EventPipeInternal
     {
         private unsafe struct EventPipeProviderConfigurationNative
         {
@@ -235,17 +235,6 @@ namespace System.Diagnostics.Tracing
                 }
             }
         }
-
-        //
-        // These PInvokes are used by the configuration APIs to interact with EventPipe.
-        //
-        [DllImport(RuntimeHelpers.QCall, CharSet = CharSet.Unicode)]
-        private static unsafe extern ulong Enable(
-            char* outputFile,
-            EventPipeSerializationFormat format,
-            uint circularBufferSizeInMB,
-            EventPipeProviderConfigurationNative* providers,
-            uint numProviders);
 
         internal static unsafe ulong Enable(
             string? outputFile,
@@ -282,43 +271,6 @@ namespace System.Diagnostics.Tracing
                 }
             }
         }
-
-        [DllImport(RuntimeHelpers.QCall, CharSet = CharSet.Unicode)]
-        internal static extern void Disable(ulong sessionID);
-
-        //
-        // These PInvokes are used by EventSource to interact with the EventPipe.
-        //
-        [DllImport(RuntimeHelpers.QCall, CharSet = CharSet.Unicode)]
-        internal static extern IntPtr CreateProvider(string providerName, Interop.Advapi32.EtwEnableCallback callbackFunc);
-
-        [DllImport(RuntimeHelpers.QCall, CharSet = CharSet.Unicode)]
-        internal static extern unsafe IntPtr DefineEvent(IntPtr provHandle, uint eventID, long keywords, uint eventVersion, uint level, void* pMetadata, uint metadataLength);
-
-        [DllImport(RuntimeHelpers.QCall, CharSet = CharSet.Unicode)]
-        internal static extern IntPtr GetProvider(string providerName);
-
-        [DllImport(RuntimeHelpers.QCall, CharSet = CharSet.Unicode)]
-        internal static extern void DeleteProvider(IntPtr provHandle);
-
-        [DllImport(RuntimeHelpers.QCall, CharSet = CharSet.Unicode)]
-        internal static extern int EventActivityIdControl(uint controlCode, ref Guid activityId);
-
-        [DllImport(RuntimeHelpers.QCall, CharSet = CharSet.Unicode)]
-        internal static extern unsafe void WriteEventData(IntPtr eventHandle, EventProvider.EventData* pEventData, uint dataCount, Guid* activityId, Guid* relatedActivityId);
-
-
-        //
-        // These PInvokes are used as part of the EventPipeEventDispatcher.
-        //
-        [DllImport(RuntimeHelpers.QCall, CharSet = CharSet.Unicode)]
-        internal static extern unsafe bool GetSessionInfo(ulong sessionID, EventPipeSessionInfo* pSessionInfo);
-
-        [DllImport(RuntimeHelpers.QCall, CharSet = CharSet.Unicode)]
-        internal static extern unsafe bool GetNextEvent(ulong sessionID, EventPipeEventInstanceData* pInstance);
-
-        [DllImport(RuntimeHelpers.QCall, CharSet = CharSet.Unicode)]
-        internal static extern unsafe IntPtr GetWaitHandle(ulong sessionID);
     }
 }
 
