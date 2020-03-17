bb5613a882cc686c634bdf9766f55c12676e845b
Jan Kotas <jkotas@microsoft.com>
2020-03-16 21:34:49 -0700

    Disable warnings-as-errors for release branches
    Fixes #33627

--- src/coreclr/clrdefinitions.cmake
+++ src/coreclr/clrdefinitions.cmake
@@ -1,9 +1,5 @@
 include(clrfeatures.cmake)
 
-# If set, indicates that this is not an officially supported release
-# Keep in sync with IsPrerelease in dir.props
-set(PRERELEASE 1)
-
 # Features we're currently flighting, but don't intend to ship in officially supported releases
 if (PRERELEASE)
   add_definitions(-DFEATURE_UTF8STRING)
