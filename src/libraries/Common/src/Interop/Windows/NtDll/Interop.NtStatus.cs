fd0959f27e2f378f61704230cb1c703bf4c9e9be
carlossanlop <calope@microsoft.com>
2020-03-17 16:51:33 -0700

    Remove hidden character causing build failure. It showed up after the file's encoding was changed to UTF8.

--- src/libraries/Common/src/Interop/Windows/NtDll/Interop.NtStatus.cs
+++ src/libraries/Common/src/Interop/Windows/NtDll/Interop.NtStatus.cs
@@ -11,7 +11,7 @@ internal static partial class Interop
         internal const uint STATUS_SOME_NOT_MAPPED        = 0x00000107;
         internal const uint STATUS_NO_MORE_FILES          = 0x80000006;
         internal const uint STATUS_INVALID_PARAMETER      = 0xC000000D;
-        internal const uint STATUS_FILE_NOT_FOUND         = 0xC000000F‬;
+        internal const uint STATUS_FILE_NOT_FOUND         = 0xC000000F;
         internal const uint STATUS_NO_MEMORY              = 0xC0000017;
         internal const uint STATUS_ACCESS_DENIED          = 0xC0000022;
         internal const uint STATUS_OBJECT_NAME_NOT_FOUND  = 0xC0000034;
