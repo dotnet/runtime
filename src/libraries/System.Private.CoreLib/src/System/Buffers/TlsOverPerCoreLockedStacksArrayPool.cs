5024e5b100bcbc90ee8286ffe453ae8ec71a4fd6
vsadov <vsadov@microsoft.com>
2020-03-18 14:41:11 -0700

    use pinned object heap in array pool.

--- src/libraries/System.Private.CoreLib/src/System/Buffers/TlsOverPerCoreLockedStacksArrayPool.cs
+++ src/libraries/System.Private.CoreLib/src/System/Buffers/TlsOverPerCoreLockedStacksArrayPool.cs
@@ -135,13 +135,13 @@ namespace System.Buffers
                 }
 
                 // No buffer available.  Allocate a new buffer with a size corresponding to the appropriate bucket.
-                buffer = GC.AllocateUninitializedArray<T>(_bucketArraySizes[bucketIndex]);
+                buffer = GC.AllocateUninitializedArray<T>(_bucketArraySizes[bucketIndex], pinned: !RuntimeHelpers.IsReferenceOrContainsReferences<T>());
             }
             else
             {
                 // The request was for a size too large for the pool.  Allocate an array of exactly the requested length.
                 // When it's returned to the pool, we'll simply throw it away.
-                buffer = GC.AllocateUninitializedArray<T>(minimumLength);
+                buffer = GC.AllocateUninitializedArray<T>(minimumLength, pinned: !RuntimeHelpers.IsReferenceOrContainsReferences<T>());
             }
 
             if (log.IsEnabled())
