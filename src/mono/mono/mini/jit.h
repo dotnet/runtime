92290b154d1d053352c7662e449d33bbf9081e2c
Aleksey Kliger <alklig@microsoft.com>
2020-03-18 16:30:33 -0400

    [mini] Move mono_install_load_aot_data_hook to a public header
    It's already a MONO_API and it is used, for example, by Xamarin.iOS, but it
    wasn't in a public header.
    
    Mark it external only.  There are no uses of it inside the runtiem

--- src/mono/mono/mini/jit.h
+++ src/mono/mono/mini/jit.h
@@ -109,9 +109,19 @@ mono_set_use_llvm (mono_bool use_llvm);
 MONO_API MONO_RT_EXTERNAL_ONLY void
 mono_aot_register_module (void **aot_info);
 
+/* These are used to load the AOT data for aot images compiled with MONO_AOT_FILE_FLAG_SEPARATE_DATA */
+/*
+ * Return the AOT data for ASSEMBLY. SIZE is the size of the data. OUT_HANDLE should be set to a handle which is later
+ * passed to the free function.
+ */
+typedef unsigned char* (*MonoLoadAotDataFunc)          (MonoAssembly *assembly, int size, void* user_data, void **out_handle);
+/* Not yet used */
+typedef void  (*MonoFreeAotDataFunc)          (MonoAssembly *assembly, int size, void* user_data, void *handle);
+MONO_API MONO_RT_EXTERNAL_ONLY void
+mono_install_load_aot_data_hook (MonoLoadAotDataFunc load_func, MonoFreeAotDataFunc free_func, void* user_data);
+
 MONO_API MONO_RT_EXTERNAL_ONLY
 MonoDomain* mono_jit_thread_attach (MonoDomain *domain);
-
 
 MONO_END_DECLS
 
