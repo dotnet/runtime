d271fd381c6b8b77ce93f9e9262f3cd151579b14
Günther Foidl <gue@korporal.at>
2020-03-19 10:00:53 +0100

    Added comment for workaround
    Cf. https://github.com/dotnet/runtime/pull/33476#issuecomment-600937760

--- src/libraries/System.Private.CoreLib/src/System/BitConverter.cs
+++ src/libraries/System.Private.CoreLib/src/System/BitConverter.cs
@@ -451,6 +451,7 @@ namespace System
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static unsafe long DoubleToInt64Bits(double value)
         {
+            // Workaround for https://github.com/dotnet/runtime/issues/11413
             if (Sse2.X64.IsSupported)
             {
                 Vector128<long> vec = Vector128.CreateScalarUnsafe(value).AsInt64();
@@ -463,6 +464,7 @@ namespace System
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static unsafe double Int64BitsToDouble(long value)
         {
+            // Workaround for https://github.com/dotnet/runtime/issues/11413
             if (Sse2.X64.IsSupported)
             {
                 Vector128<double> vec = Vector128.CreateScalarUnsafe(value).AsDouble();
@@ -475,6 +477,7 @@ namespace System
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static unsafe int SingleToInt32Bits(float value)
         {
+            // Workaround for https://github.com/dotnet/runtime/issues/11413
             if (Sse2.IsSupported)
             {
                 Vector128<int> vec = Vector128.CreateScalarUnsafe(value).AsInt32();
@@ -487,6 +490,7 @@ namespace System
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static unsafe float Int32BitsToSingle(int value)
         {
+            // Workaround for https://github.com/dotnet/runtime/issues/11413
             if (Sse2.IsSupported)
             {
                 Vector128<float> vec = Vector128.CreateScalarUnsafe(value).AsSingle();
