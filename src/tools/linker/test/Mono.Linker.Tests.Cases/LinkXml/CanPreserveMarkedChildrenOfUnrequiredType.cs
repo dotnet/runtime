ae26498697f4ee5732fdc209d1f638ae84a5b27a
Mateo Torres-Ruiz <mateoatr@users.noreply.github.com>
2020-03-20 16:56:35 -0700

    Preserve marked children of XML descriptor (dotnet/linker#1015)
    * Mark children specified in xml descriptor regardless of the `required` attribute value
    
    * Call MarkChildren once
    
    * Check that unmarked children are removed
    
    * Remove redundant modifier
    
    Commit migrated from https://github.com/dotnet/linker/commit/17a028b82d62e63368e1d2261d5cc0c262c5f7b3

---
+++ src/tools/linker/test/Mono.Linker.Tests.Cases/LinkXml/CanPreserveMarkedChildrenOfUnrequiredType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
+using Mono.Linker.Tests.Cases.Expectations.Metadata;
+using Mono.Linker.Tests.Cases.LinkXml.Dependencies;
+
+namespace Mono.Linker.Tests.Cases.LinkXml
+{
+	[SetupCompileBefore ("Library.dll", new [] { "Dependencies/CanPreserveMarkedChildrenOfUnrequiredType_Library.cs" })]
+	[KeptMemberInAssembly ("Library.dll", typeof (CanPreserveMarkedChildrenOfUnrequiredType_Library), "Field1", "Method1()", "Property1")]
+	[RemovedMemberInAssembly ("Library.dll", typeof (CanPreserveMarkedChildrenOfUnrequiredType_Library), "Field2", "Method2()", "Property2")]
+
+	class CanPreserveMarkedChildrenOfUnrequiredType
+	{
+		public static void Main () {
+			// Mark the type by calling its ctor.
+			new CanPreserveMarkedChildrenOfUnrequiredType_Library ();
+		}
+	}
+}
