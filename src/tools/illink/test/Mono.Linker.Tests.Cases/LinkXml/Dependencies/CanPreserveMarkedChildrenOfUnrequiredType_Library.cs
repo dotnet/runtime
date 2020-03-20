ee57d2172efbcdc5b62f57c1a07477b064bc50b1
Mateo Torres-Ruiz <mateoatr@users.noreply.github.com>
2020-03-20 16:56:35 -0700

    Preserve marked children of XML descriptor (dotnet/linker#1015)
    * Mark children specified in xml descriptor regardless of the `required` attribute value
    
    * Call MarkChildren once
    
    * Check that unmarked children are removed
    
    * Remove redundant modifier
    
    Commit migrated from https://github.com/dotnet/linker/commit/17a028b82d62e63368e1d2261d5cc0c262c5f7b3

---
+++ src/tools/illink/test/Mono.Linker.Tests.Cases/LinkXml/Dependencies/CanPreserveMarkedChildrenOfUnrequiredType_Library.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Mono.Linker.Tests.Cases.LinkXml.Dependencies
+{
+	public class CanPreserveMarkedChildrenOfUnrequiredType_Library
+	{
+		public int Field1;
+		
+		public int Field2;
+		
+		public void Method1 () { }
+		
+		public void Method2 () { }
+		
+		public int Property1 {
+			get {
+				return Field1;
+			}
+			set {
+				Field1 = value;
+			}
+		}
+		
+		public int Property2 {
+			get {
+				return Field2;
+			}
+			set {
+				Field2 = value;
+			}
+		}
+	}
+}
