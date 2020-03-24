d4d36fda8edf8f3cecfd918f29f939c01487e33b
vitek-karas <vitek.karas@microsoft.com>
2020-03-24 15:27:04 -0700

    Propagate data flow annotations across method calls
    Implements propagation of data flow annotations from/to method parameters and from/to method return value.
    
    
    Commit migrated from https://github.com/dotnet/linker/commit/c9bb0397399724317db476b5cc5f5da0e0ea4f7c

--- src/tools/illink/test/Mono.Linker.Tests.Cases/Reflection/ActivatorCreateInstance.cs
+++ src/tools/illink/test/Mono.Linker.Tests.Cases/Reflection/ActivatorCreateInstance.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using Mono.Linker.Tests.Cases.Expectations.Assertions;
 using Mono.Linker.Tests.Cases.Expectations.Metadata;
 
 namespace Mono.Linker.Tests.Cases.Reflection
 {
 	[SetupCSharpCompilerToUse ("csc")]
+	[KeptMember(".ctor()")]
 	public class ActivatorCreateInstance
 	{
 		[UnrecognizedReflectionAccessPattern(
@@ -18,6 +20,26 @@ namespace Mono.Linker.Tests.Cases.Reflection
 			Activator.CreateInstance (typeof (Test4), new object [] { 1, "ss" });
 			Activator.CreateInstance ("test", "Mono.Linker.Tests.Cases.Reflection.ActivatorCreateInstance+Test5");
 			Activator.CreateInstance<Test1> ();
+
+			var p = new ActivatorCreateInstance ();
+
+			// Direct call to static method with a parameter
+			FromParameterOnStaticMethod (typeof (FromParameterOnStaticMethodType));
+
+			// Value comes from multiple sources - each must be marked appropriately
+			Type fromParameterOnStaticMethodType = p == null ?
+				typeof (FromParameterOnStaticMethodTypeA) :
+				typeof (FromParameterOnStaticMethodTypeB);
+			FromParameterOnStaticMethod (fromParameterOnStaticMethodType);
+
+			// Direct call to instance method with a parameter
+			p.FromParameterOnInstanceMethod (typeof (FromParameterOnInstanceMethodType));
+
+			// All constructors required
+			FromParameterWithConstructors (typeof (FromParameterWithConstructorsType));
+
+			// Public contructors required
+			FromParameterWithPublicConstructors (typeof (FromParameterWithPublicConstructorsType));
 		}
 
 		[Kept]
@@ -85,6 +107,101 @@ namespace Mono.Linker.Tests.Cases.Reflection
 			public Test5 (int i, object o)
 			{
 			}
+		}
+
+		[Kept]
+		private static void FromParameterOnStaticMethod (
+			[DynamicallyAccessedMembers(DynamicallyAccessedMemberKinds.DefaultConstructor)]
+			[KeptAttributeAttribute(typeof(DynamicallyAccessedMembersAttribute))]
+			Type type)
+		{
+			Activator.CreateInstance (type);
+		}
+
+		[Kept]
+		class FromParameterOnStaticMethodType
+		{
+			[Kept]
+			public FromParameterOnStaticMethodType () { }
+			public FromParameterOnStaticMethodType (int arg) { }
+		}
+
+		[Kept]
+		class FromParameterOnStaticMethodTypeA
+		{
+			[Kept]
+			public FromParameterOnStaticMethodTypeA () { }
+			public FromParameterOnStaticMethodTypeA (int arg) { }
+		}
+
+		[Kept]
+		class FromParameterOnStaticMethodTypeB
+		{
+			[Kept]
+			public FromParameterOnStaticMethodTypeB () { }
+			public FromParameterOnStaticMethodTypeB (int arg) { }
+		}
+
+		[Kept]
+		private void FromParameterOnInstanceMethod (
+			[DynamicallyAccessedMembers(DynamicallyAccessedMemberKinds.DefaultConstructor)]
+			[KeptAttributeAttribute(typeof(DynamicallyAccessedMembersAttribute))]
+			Type type)
+		{
+			Activator.CreateInstance (type);
+		}
+
+		[Kept]
+		class FromParameterOnInstanceMethodType
+		{
+			[Kept]
+			public FromParameterOnInstanceMethodType () { }
+			public FromParameterOnInstanceMethodType (int arg) { }
+		}
+
+		[Kept]
+		private static void FromParameterWithConstructors (
+			[DynamicallyAccessedMembers(DynamicallyAccessedMemberKinds.Constructors)]
+			[KeptAttributeAttribute(typeof(DynamicallyAccessedMembersAttribute))]
+			Type type)
+
+		{
+			// All ctors are required by the parameter, so all cases should work
+			Activator.CreateInstance (type);
+			Activator.CreateInstance (type, new object [] { 1 });
+			Activator.CreateInstance (type, BindingFlags.NonPublic, new object [] { 1, 2 });
+		}
+
+		[Kept]
+		class FromParameterWithConstructorsType
+		{
+			[Kept]
+			public FromParameterWithConstructorsType () { }
+			[Kept]
+			public FromParameterWithConstructorsType (int arg) { }
+			[Kept]
+			private FromParameterWithConstructorsType (int arg, int arg2) { }
+		}
+
+		[Kept]
+		private static void FromParameterWithPublicConstructors (
+			[DynamicallyAccessedMembers(DynamicallyAccessedMemberKinds.PublicConstructors)]
+			[KeptAttributeAttribute(typeof(DynamicallyAccessedMembersAttribute))]
+			Type type)
+		{
+			// Only public ctors and default ctor are required, so only those cases should work
+			Activator.CreateInstance (type);
+			Activator.CreateInstance (type, new object [] { 1 });
+		}
+
+		[Kept]
+		class FromParameterWithPublicConstructorsType
+		{
+			[Kept]
+			public FromParameterWithPublicConstructorsType () { }
+			[Kept]
+			public FromParameterWithPublicConstructorsType (int arg) { }
+			private FromParameterWithPublicConstructorsType (int arg, int arg2) { }
 		}
 	}
 }
