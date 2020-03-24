66724a324169879e6c398baf0bdac707210a5874
Alexander Köplinger <alex.koeplinger@outlook.com>
2020-03-24 19:13:15 +0100

    PR feedback

--- src/libraries/Native/Unix/System.Globalization.Native/pal_icushim_internal.h
+++ src/libraries/Native/Unix/System.Globalization.Native/pal_icushim_internal.h
@@ -13,31 +13,31 @@
 #include "config.h"
 
 #if defined(TARGET_ANDROID)
-    #include "pal_icushim_internal_android.h"
+#include "pal_icushim_internal_android.h"
 #else
 
-    #define U_DISABLE_RENAMING 1
+#define U_DISABLE_RENAMING 1
 
-    // All ICU headers need to be included here so that all function prototypes are
-    // available before the function pointers are declared below.
-    #include <unicode/ucurr.h>
-    #include <unicode/ucal.h>
-    #include <unicode/uchar.h>
-    #include <unicode/ucol.h>
-    #include <unicode/udat.h>
-    #include <unicode/udatpg.h>
-    #include <unicode/uenum.h>
-    #include <unicode/uidna.h>
-    #include <unicode/uldnames.h>
-    #include <unicode/ulocdata.h>
-    #include <unicode/unorm2.h>
-    #include <unicode/unum.h>
-    #include <unicode/ures.h>
-    #include <unicode/usearch.h>
-    #include <unicode/utf16.h>
-    #include <unicode/utypes.h>
-    #include <unicode/urename.h>
-    #include <unicode/ustring.h>
+// All ICU headers need to be included here so that all function prototypes are
+// available before the function pointers are declared below.
+#include <unicode/ucurr.h>
+#include <unicode/ucal.h>
+#include <unicode/uchar.h>
+#include <unicode/ucol.h>
+#include <unicode/udat.h>
+#include <unicode/udatpg.h>
+#include <unicode/uenum.h>
+#include <unicode/uidna.h>
+#include <unicode/uldnames.h>
+#include <unicode/ulocdata.h>
+#include <unicode/unorm2.h>
+#include <unicode/unum.h>
+#include <unicode/ures.h>
+#include <unicode/usearch.h>
+#include <unicode/utf16.h>
+#include <unicode/utypes.h>
+#include <unicode/urename.h>
+#include <unicode/ustring.h>
 
 #endif
 
