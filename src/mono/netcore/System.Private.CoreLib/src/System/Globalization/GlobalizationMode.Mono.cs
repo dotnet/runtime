3dc2ad8f32350f0b3f3f9184a8078240ba5662e7
Stephen Toub <stoub@microsoft.com>
2020-03-24 22:16:01 -0400

    Enable analyzers for mono/netcore

--- src/mono/netcore/System.Private.CoreLib/src/System/Globalization/GlobalizationMode.Mono.cs
+++ src/mono/netcore/System.Private.CoreLib/src/System/Globalization/GlobalizationMode.Mono.cs
@@ -5,16 +5,16 @@ using System.Runtime.CompilerServices;
 
 namespace System.Globalization
 {
-	internal static partial class GlobalizationMode
-	{
-		internal static bool Invariant { get; } = GetGlobalizationInvariantMode ();
+    internal static partial class GlobalizationMode
+    {
+        internal static bool Invariant { get; } = GetGlobalizationInvariantMode();
 
-		static bool GetInvariantSwitchValue ()
-		{
-			var val = Environment.GetEnvironmentVariable ("DOTNET_SYSTEM_GLOBALIZATION_INVARIANT");
-			if (val != null)
-				return Boolean.IsTrueStringIgnoreCase (val) || val.Equals ("1");
-			return false;
-		}
-	}
+        private static bool GetInvariantSwitchValue()
+        {
+            var val = Environment.GetEnvironmentVariable("DOTNET_SYSTEM_GLOBALIZATION_INVARIANT");
+            if (val != null)
+                return bool.IsTrueStringIgnoreCase(val) || val.Equals("1");
+            return false;
+        }
+    }
 }
--- src/mono/netcore/System.Private.CoreLib/src/System/Reflection/Emit/DynamicILInfo.cs
+++ src/mono/netcore/System.Private.CoreLib/src/System/Reflection/Emit/DynamicILInfo.cs
@@ -1,5 +1,3 @@
-#nullable disable
-
 //
 // System.Reflection.Emit/DynamicILInfo.cs
 //
@@ -15,10 +13,10 @@
 // distribute, sublicense, and/or sell copies of the Software, and to
 // permit persons to whom the Software is furnished to do so, subject to
 // the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be
 // included in all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -28,106 +26,125 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
+#nullable disable
 #if MONO_FEATURE_SRE
 using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-namespace System.Reflection.Emit {
+namespace System.Reflection.Emit
+{
 
-	[ComVisible (true)]
-	public class DynamicILInfo {
+    [ComVisible(true)]
+    public class DynamicILInfo
+    {
 
-		DynamicMethod method;
+        private DynamicMethod method;
 
-		internal DynamicILInfo ()
-		{
-		}
+        internal DynamicILInfo()
+        {
+        }
 
-		internal DynamicILInfo (DynamicMethod method)
-		{
-			this.method = method;
-		}
+        internal DynamicILInfo(DynamicMethod method)
+        {
+            this.method = method;
+        }
 
-		public DynamicMethod DynamicMethod { 
-			get {
-				return method;
-			}
-		}
+        public DynamicMethod DynamicMethod
+        {
+            get
+            {
+                return method;
+            }
+        }
 
-		// FIXME:
-		public int GetTokenFor (byte[] signature) {
-			throw new NotImplementedException ();
-		}
+        // FIXME:
+        public int GetTokenFor(byte[] signature)
+        {
+            throw new NotImplementedException();
+        }
 
-		public int GetTokenFor (DynamicMethod method) {
-			return this.method.GetILGenerator ().TokenGenerator.GetToken (method, false);
-		}
+        public int GetTokenFor(DynamicMethod method)
+        {
+            return this.method.GetILGenerator().TokenGenerator.GetToken(method, false);
+        }
 
-		public int GetTokenFor (RuntimeFieldHandle field) {
-			return this.method.GetILGenerator ().TokenGenerator.GetToken (FieldInfo.GetFieldFromHandle (field), false);
-		}
+        public int GetTokenFor(RuntimeFieldHandle field)
+        {
+            return this.method.GetILGenerator().TokenGenerator.GetToken(FieldInfo.GetFieldFromHandle(field), false);
+        }
 
-		public int GetTokenFor (RuntimeMethodHandle method) {
-			MethodBase mi = MethodBase.GetMethodFromHandle (method);
-			return this.method.GetILGenerator ().TokenGenerator.GetToken (mi, false);
-		}
+        public int GetTokenFor(RuntimeMethodHandle method)
+        {
+            MethodBase mi = MethodBase.GetMethodFromHandle(method);
+            return this.method.GetILGenerator().TokenGenerator.GetToken(mi, false);
+        }
 
-		public int GetTokenFor (RuntimeTypeHandle type) {
-			Type t = Type.GetTypeFromHandle (type);
-			return this.method.GetILGenerator ().TokenGenerator.GetToken (t, false);
-		}
+        public int GetTokenFor(RuntimeTypeHandle type)
+        {
+            Type t = Type.GetTypeFromHandle(type);
+            return this.method.GetILGenerator().TokenGenerator.GetToken(t, false);
+        }
 
-		public int GetTokenFor (string literal) {
-			return method.GetILGenerator ().TokenGenerator.GetToken (literal);
-		}
+        public int GetTokenFor(string literal)
+        {
+            return method.GetILGenerator().TokenGenerator.GetToken(literal);
+        }
 
-		// FIXME:
-		public int GetTokenFor (RuntimeMethodHandle method, RuntimeTypeHandle contextType) {
-			throw new NotImplementedException ();
-		}
+        // FIXME:
+        public int GetTokenFor(RuntimeMethodHandle method, RuntimeTypeHandle contextType)
+        {
+            throw new NotImplementedException();
+        }
 
-		// FIXME:
-		public int GetTokenFor (RuntimeFieldHandle field, RuntimeTypeHandle contextType) {
-			throw new NotImplementedException ();
-		}
+        // FIXME:
+        public int GetTokenFor(RuntimeFieldHandle field, RuntimeTypeHandle contextType)
+        {
+            throw new NotImplementedException();
+        }
 
-		public void SetCode (byte[] code, int maxStackSize) {
-			if (code == null)
-				throw new ArgumentNullException ("code");
-			method.GetILGenerator ().SetCode (code, maxStackSize);
-		}
+        public void SetCode(byte[] code, int maxStackSize)
+        {
+            if (code == null)
+                throw new ArgumentNullException(nameof(code));
+            method.GetILGenerator().SetCode(code, maxStackSize);
+        }
 
-		[CLSCompliantAttribute(false)] 
-		public unsafe void SetCode (byte* code, int codeSize, int maxStackSize) {
-			if (code == null)
-				throw new ArgumentNullException ("code");
-			method.GetILGenerator ().SetCode (code, codeSize, maxStackSize);
-		}
+        [CLSCompliantAttribute(false)]
+        public unsafe void SetCode(byte* code, int codeSize, int maxStackSize)
+        {
+            if (code == null)
+                throw new ArgumentNullException(nameof(code));
+            method.GetILGenerator().SetCode(code, codeSize, maxStackSize);
+        }
 
-		// FIXME:
-		public void SetExceptions (byte[] exceptions) {
-			throw new NotImplementedException ();
-		}
+        // FIXME:
+        public void SetExceptions(byte[] exceptions)
+        {
+            throw new NotImplementedException();
+        }
 
-		// FIXME:
-		[CLSCompliantAttribute(false)] 
-		public unsafe void SetExceptions (byte* exceptions, int exceptionsSize) {
-			throw new NotImplementedException ();
-		}
+        // FIXME:
+        [CLSCompliantAttribute(false)]
+        public unsafe void SetExceptions(byte* exceptions, int exceptionsSize)
+        {
+            throw new NotImplementedException();
+        }
 
-		// FIXME:
-		public void SetLocalSignature (byte[] localSignature) {
-			throw new NotImplementedException ();
-		}
+        // FIXME:
+        public void SetLocalSignature(byte[] localSignature)
+        {
+            throw new NotImplementedException();
+        }
 
-		[CLSCompliantAttribute(false)] 
-		public unsafe void SetLocalSignature (byte* localSignature, int signatureSize) {
-			byte[] b = new byte [signatureSize];
-			for (int i = 0; i < signatureSize; ++i)
-				b [i] = localSignature [i];
-		}
-	}
+        [CLSCompliantAttribute(false)]
+        public unsafe void SetLocalSignature(byte* localSignature, int signatureSize)
+        {
+            byte[] b = new byte[signatureSize];
+            for (int i = 0; i < signatureSize; ++i)
+                b[i] = localSignature[i];
+        }
+    }
 }
 
 #endif
