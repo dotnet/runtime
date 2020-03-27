665940dda51a5188ca42014bd3b9846a63cf7648
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2020-03-27 11:11:24 +0100

    [feature/dataflow] Implement FlowAnnotations helper class (dotnet/linker#1024)
    * `IFlowAnnotationSource` lets us abstract away the source of the annotations. We expect to have an attribute-based one and JSON-based one (the JSON based one mostly for internal purposes right now - it probably won't be public).
    * The `FlowAnnotations` class deals with caching and propagating the attributes from properties to the accessor methods and backing fields. The cache is per-type because the properties make this more complicated. Being per-type is also more memory efficient because we don't have to keep track of all methods/fields that we looked at and found out they're not interesting (the majority of cases). Not interesting things are not tracked and that's cached at the type level.
    
    Vitek is working on the thing that consumes this. I already tested it a bit with Vitek's thing.
    
    Commit migrated from https://github.com/dotnet/linker/commit/1ff7856eea9ed7d4bd890ff7ace72e0d9decce99

---
+++ src/tools/illink/src/linker/Linker/FieldDefinitionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Mono.Cecil;
+
+namespace Mono.Linker
+{
+	static class FieldDefinitionExtensions
+	{
+		public static bool IsCompilerGenerated(this FieldDefinition field)
+		{
+			if (!field.HasCustomAttributes)
+				return false;
+
+			foreach (var ca in field.CustomAttributes) {
+				var caType = ca.AttributeType;
+				if (caType.Name == "CompilerGeneratedAttribute" && caType.Namespace == "System.Runtime.CompilerServices")
+					return true;
+			}
+
+			return false;
+		}
+	}
+}
