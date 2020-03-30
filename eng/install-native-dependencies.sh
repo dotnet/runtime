a0eda1f3dce6f970def07099baf2db2bb730a7f6
Joseph Shields <directhex@apebox.org>
2020-03-30 13:44:15 -0400

    Revert "Install some extra dependencies post-hoc"
    This reverts commit e7e9b063c62635bd35c28374138a568dfe21d077.

--- eng/install-native-dependencies.sh
+++ eng/install-native-dependencies.sh
@@ -9,15 +9,6 @@ if [ "$1" = "Linux" ]; then
     if [ "$?" != "0" ]; then
         exit 1;
     fi
-elif [ "$1" = "Android" ]; then
-    sudo apt update
-    if [ "$?" != "0" ]; then
-       exit 1;
-    fi
-    sudo apt install cmake llvm-3.9 clang-3.9 lldb-3.9 liblldb-3.9-dev libunwind8 libunwind8-dev gettext libicu-dev liblttng-ust-dev libcurl4-openssl-dev libssl-dev libkrb5-dev libnuma-dev autoconf automake libtool build-essential
-    if [ "$?" != "0" ]; then
-        exit 1;
-    fi
 elif [ "$1" = "OSX" ]; then
     brew update
     brew upgrade
