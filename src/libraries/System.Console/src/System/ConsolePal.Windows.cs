69473a886f07256110aba84fb04585464521db6a
Tom Deseyn <tom.deseyn@gmail.com>
2020-03-31 17:05:24 +0200

    Cleanup unused 'using'

--- src/libraries/System.Console/src/System/ConsolePal.Windows.cs
+++ src/libraries/System.Console/src/System/ConsolePal.Windows.cs
@@ -3,7 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
