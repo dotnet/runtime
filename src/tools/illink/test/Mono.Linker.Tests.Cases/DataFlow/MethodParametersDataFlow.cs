dotnet/linker

commit 061873b5ed58a4e96d46002157467006f9327a75

[feature/dataflow] Much improved error messages when data flow fails (#1037)
Much improved error messages when data flow fails

src/linker/Linker.Steps/MarkStep.cs
@@ -3528,7 +3528,7 @@ namespace Mono.Linker.Steps {
 					if (requiredMemberKinds != 0) {
 						var reflectionContext = new ReflectionPatternContext (_markStep._context, methodBody.Method, methodBody.Method, 0);
 						reflectionContext.AnalyzingPattern ();
-						RequireDynamicallyAccessedMembers (ref reflectionContext, requiredMemberKinds, MethodReturnValue);
+						RequireDynamicallyAccessedMembers (ref reflectionContext, requiredMemberKinds, MethodReturnValue, methodBody.Method.MethodReturnType);
 					}
 				}
 			}
@@ -3600,7 +3600,7 @@ namespace Mono.Linker.Steps {
 						case "CreateInstance" when !calledMethod.ContainsGenericParameter
 							&& calledMethod.DeclaringType.Name == "Activator"
 							&& calledMethod.Parameters.Count >= 1
-							&& calledMethod.Parameters[0].ParameterType.MetadataType != MetadataType.String: {
+							&& calledMethod.Parameters [0].ParameterType.MetadataType != MetadataType.String: {
 
 								var parameters = calledMethod.Parameters;
 
@@ -3648,7 +3648,7 @@ namespace Mono.Linker.Steps {
 								}
 
 								// Go over all types we've seen
-								foreach (var value in methodParams[0].UniqueValues ()) {
+								foreach (var value in methodParams [0].UniqueValues ()) {
 									if (value is SystemTypeValue systemTypeValue) {
 										// Special case known type values as we can do better by applying exact binding flags and parameter count.
 										MarkMethodsFromReflectionCall (ref reflectionContext, systemTypeValue.TypeRepresented, ".ctor", bindingFlags, ctorParameterCount);
@@ -3659,7 +3659,7 @@ namespace Mono.Linker.Steps {
 											: ((bindingFlags & BindingFlags.NonPublic) == 0)
 												? DynamicallyAccessedMemberKinds.PublicConstructors
 												: DynamicallyAccessedMemberKinds.Constructors;
-										RequireDynamicallyAccessedMembers (ref reflectionContext, requiredMemberKinds, value);
+										RequireDynamicallyAccessedMembers (ref reflectionContext, requiredMemberKinds, value, calledMethod.Parameters [0]);
 									}
 								}
 							}
@@ -3669,8 +3669,10 @@ namespace Mono.Linker.Steps {
 								reflectionContext.AnalyzingPattern ();
 								for (int parameterIndex = 0; parameterIndex < methodParams.Count; parameterIndex ++) {
 									var requiredMemberKinds = _flowAnnotations.GetParameterAnnotation (calledMethodDefinition, parameterIndex);
-									if (requiredMemberKinds != 0)
-										RequireDynamicallyAccessedMembers (ref reflectionContext, requiredMemberKinds, methodParams [parameterIndex]);
+									if (requiredMemberKinds != 0) {
+										var targetParameter = calledMethodDefinition.Parameters [parameterIndex - (calledMethodDefinition.HasImplicitThis () ? 1 : 0)];
+										RequireDynamicallyAccessedMembers (ref reflectionContext, requiredMemberKinds, methodParams [parameterIndex], targetParameter);
+									}
 								}
 
 								reflectionContext.RecordHandledPattern ();
@@ -3719,12 +3721,16 @@ namespace Mono.Linker.Steps {
 				return true;
 			}
 
-			void RequireDynamicallyAccessedMembers(ref ReflectionPatternContext reflectionContext, DynamicallyAccessedMemberKinds requiredMemberKinds, ValueNode value)
+			void RequireDynamicallyAccessedMembers (ref ReflectionPatternContext reflectionContext, DynamicallyAccessedMemberKinds requiredMemberKinds, ValueNode value, IMetadataTokenProvider targetContext)
 			{
 				foreach (var uniqueValue in value.UniqueValues ()) {
 					if (uniqueValue is LeafValueWithDynamicallyAccessedMemberNode valueWithDynamicallyAccessedMember) {
 						if (!valueWithDynamicallyAccessedMember.DynamicallyAccessedMemberKinds.HasFlag (requiredMemberKinds)) {
-							reflectionContext.RecordUnrecognizedPattern ($"Value from {valueWithDynamicallyAccessedMember.SourceContext} doesn't have enough dynamically accessed members required by {reflectionContext.MethodCalled}.");
+							reflectionContext.RecordUnrecognizedPattern ($"The {GetValueDescriptionForErrorMessage (valueWithDynamicallyAccessedMember)} " +
+								$"with dynamically accessed member kinds '{GetDynamicallyAccessedMemberKindsDescription (valueWithDynamicallyAccessedMember.DynamicallyAccessedMemberKinds)}' " +
+								$"is passed into the {GetMetadataTokenDescriptionForErrorMessage (targetContext)} " +
+								$"which requires dynamically accessed member kinds `{GetDynamicallyAccessedMemberKindsDescription (requiredMemberKinds)}`. " +
+								$"To fix this add DynamicallyAccessedMembersAttribute to it and specify at least these member kinds '{GetDynamicallyAccessedMemberKindsDescription (requiredMemberKinds)}'.");
 						} else {
 							reflectionContext.RecordHandledPattern ();
 						}
@@ -3743,7 +3749,10 @@ namespace Mono.Linker.Steps {
 					} else if (uniqueValue == NullValue.Instance) {
 						// Ignore - probably unreachable path as it would fail at runtime anyway.
 					} else {
-						reflectionContext.RecordUnrecognizedPattern ($"Value comes from unsupported source.");
+						reflectionContext.RecordUnrecognizedPattern ($"A {GetValueDescriptionForErrorMessage (uniqueValue)} " +
+							$"is passed into the {GetMetadataTokenDescriptionForErrorMessage (targetContext)} " +
+							$"which requires dynamically accessed member kinds `{GetDynamicallyAccessedMemberKindsDescription (requiredMemberKinds)}`. " +
+							$"It's not possible to guarantee that these requirements are met by the application.");
 					}
 				}
 
@@ -3784,6 +3793,82 @@ namespace Mono.Linker.Steps {
 					bool publicOnly = (bindingFlags & (BindingFlags.Public | BindingFlags.NonPublic)) == BindingFlags.Public;
 					reflectionContext.RecordUnrecognizedPattern ($"Reflection call '{reflectionContext.MethodCalled.FullName}' inside '{reflectionContext.MethodCalling.FullName}' could not resolve {(publicOnly ? "public" : "")} method `{name}` on type `{declaringType.FullName}`.");
 				}
+			}
+
+			string GetValueDescriptionForErrorMessage (ValueNode value)
+			{
+				switch (value) {
+					case MethodParameterValue methodParameterValue: {
+							if (methodParameterValue.SourceContext is MethodDefinition method) {
+								int declaredParameterIndex = methodParameterValue.ParameterIndex - (method.HasImplicitThis () ? 1 : 0);
+								if (declaredParameterIndex >= 0 && declaredParameterIndex < method.Parameters.Count)
+									return GetMetadataTokenDescriptionForErrorMessage (method.Parameters [declaredParameterIndex]);
+							}
+
+							return $"method prameter #{methodParameterValue.ParameterIndex}";
+						}
+
+					case MethodReturnValue methodReturnValue: {
+							if (methodReturnValue.SourceContext is MethodDefinition method) {
+								return GetMetadataTokenDescriptionForErrorMessage (method.MethodReturnType);
+							}
+
+							return "method return value";
+						}
+
+					default:
+						return $"value from unknown source";
+				}
+			}
+
+			string GetMetadataTokenDescriptionForErrorMessage (IMetadataTokenProvider targetContext)
+			{
+				switch (targetContext) {
+					case ParameterDefinition parameterDefinition: return $"parameter '{parameterDefinition.Name}' of method '{parameterDefinition.Method}'";
+					case MethodReturnType methodReturnType: return $"return value of method '{methodReturnType.Method}'";
+					default: return targetContext.ToString ();
+				};
+			}
+
+			string GetDynamicallyAccessedMemberKindsDescription (DynamicallyAccessedMemberKinds memberKinds)
+			{
+				var results = new List<DynamicallyAccessedMemberKinds> ();
+				if (memberKinds.HasFlag (DynamicallyAccessedMemberKinds.Constructors))
+					results.Add (DynamicallyAccessedMemberKinds.Constructors);
+				else if (memberKinds.HasFlag (DynamicallyAccessedMemberKinds.PublicConstructors))
+					results.Add (DynamicallyAccessedMemberKinds.PublicConstructors);
+				else if (memberKinds.HasFlag (DynamicallyAccessedMemberKinds.DefaultConstructor))
+					results.Add (DynamicallyAccessedMemberKinds.DefaultConstructor);
+				
+				if (memberKinds.HasFlag (DynamicallyAccessedMemberKinds.Methods))
+					results.Add (DynamicallyAccessedMemberKinds.Methods);
+				else if (memberKinds.HasFlag (DynamicallyAccessedMemberKinds.PublicMethods))
+					results.Add (DynamicallyAccessedMemberKinds.PublicMethods);
+				
+				if (memberKinds.HasFlag (DynamicallyAccessedMemberKinds.Properties))
+					results.Add (DynamicallyAccessedMemberKinds.Properties);
+				else if (memberKinds.HasFlag (DynamicallyAccessedMemberKinds.PublicProperties))
+					results.Add (DynamicallyAccessedMemberKinds.PublicProperties);
+				
+				if (memberKinds.HasFlag (DynamicallyAccessedMemberKinds.Fields))
+					results.Add (DynamicallyAccessedMemberKinds.Fields);
+				else if (memberKinds.HasFlag (DynamicallyAccessedMemberKinds.PublicFields))
+					results.Add (DynamicallyAccessedMemberKinds.PublicFields);
+				
+				if (memberKinds.HasFlag (DynamicallyAccessedMemberKinds.Events))
+					results.Add (DynamicallyAccessedMemberKinds.Events);
+				else if (memberKinds.HasFlag (DynamicallyAccessedMemberKinds.PublicEvents))
+					results.Add (DynamicallyAccessedMemberKinds.PublicEvents);
+				
+				if (memberKinds.HasFlag (DynamicallyAccessedMemberKinds.NestedTypes))
+					results.Add (DynamicallyAccessedMemberKinds.NestedTypes);
+				else if (memberKinds.HasFlag (DynamicallyAccessedMemberKinds.PublicNestedTypes))
+					results.Add (DynamicallyAccessedMemberKinds.PublicNestedTypes);
+
+				if (results.Count == 0)
+					return "None";
+
+				return string.Join (" | ", results.Select (r => r.ToString ()));
 			}
 		}
 	}

test/Mono.Linker.Tests.Cases/DataFlow/MethodParametersDataFlow.cs
@@ -32,7 +32,13 @@ namespace Mono.Linker.Tests.Cases.DataFlow
 		}
 
 		[Kept]
-		[UnrecognizedReflectionAccessPattern (typeof (MethodParametersDataFlow), nameof (RequirePublicConstructors), new Type [] { typeof (Type) })]
+		// Validate the error message when annotated parameter is passed to another annotated parameter
+		[UnrecognizedReflectionAccessPattern (typeof (MethodParametersDataFlow), nameof (RequirePublicConstructors), new Type [] { typeof (Type) },
+			"The parameter 'type' of method 'System.Void Mono.Linker.Tests.Cases.DataFlow.MethodParametersDataFlow::DefaultConstructorParameter(System.Type)' " +
+			"with dynamically accessed member kinds 'DefaultConstructor' is passed into the parameter 'type' " +
+			"of method 'System.Void Mono.Linker.Tests.Cases.DataFlow.MethodParametersDataFlow::RequirePublicConstructors(System.Type)' " +
+			"which requires dynamically accessed member kinds `PublicConstructors`. " +
+			"To fix this add DynamicallyAccessedMembersAttribute to it and specify at least these member kinds 'PublicConstructors'.")]
 		[UnrecognizedReflectionAccessPattern (typeof (MethodParametersDataFlow), nameof (RequireConstructors), new Type [] { typeof (Type) })]
 		private static void DefaultConstructorParameter (
 			[DynamicallyAccessedMembers(DynamicallyAccessedMemberKinds.DefaultConstructor)]
@@ -110,18 +116,31 @@ namespace Mono.Linker.Tests.Cases.DataFlow
 			RequirePublicConstructors (t);
 		}
 
-		[UnrecognizedReflectionAccessPattern (typeof (MethodParametersDataFlow), nameof (RequireDefaultConstructor), new Type [] { typeof (Type) })]
+		// Validate the error message for the case of unannotated method return value passed to an annotated parameter.
+		[UnrecognizedReflectionAccessPattern (typeof (MethodParametersDataFlow), nameof (RequireDefaultConstructor), new Type [] { typeof (Type) },
+			"The parameter 'type' of method 'System.Void Mono.Linker.Tests.Cases.DataFlow.MethodParametersDataFlow::NoAnnotation(System.Type)' " +
+			"with dynamically accessed member kinds 'None' is passed into " +
+			"the parameter 'type' of method 'System.Void Mono.Linker.Tests.Cases.DataFlow.MethodParametersDataFlow::RequireDefaultConstructor(System.Type)' " +
+			"which requires dynamically accessed member kinds `DefaultConstructor`. " +
+			"To fix this add DynamicallyAccessedMembersAttribute to it and specify at least these member kinds 'DefaultConstructor'.")]
 		[Kept]
 		private void NoAnnotation (Type type)
 		{
-			RequireDefaultConstructor (this.GetType ());
+			RequireDefaultConstructor (type);
 		}
 
-		[UnrecognizedReflectionAccessPattern (typeof (MethodParametersDataFlow), nameof (RequireDefaultConstructor), new Type [] { typeof (Type) })]
+		// Validate error message when untracable value is passed to an annotated parameter.
+		[UnrecognizedReflectionAccessPattern (typeof (MethodParametersDataFlow), nameof (RequireDefaultConstructor), new Type [] { typeof (Type) },
+			"A value from unknown source is passed " +
+			"into the parameter 'type' of method 'System.Void Mono.Linker.Tests.Cases.DataFlow.MethodParametersDataFlow::RequireDefaultConstructor(System.Type)' " +
+			"which requires dynamically accessed member kinds `DefaultConstructor`. " +
+			"It's not possible to guarantee that these requirements are met by the application.")]
 		[Kept]
 		private void UnknownValue ()
 		{
-			RequireDefaultConstructor (this.GetType ());
+			var array = new object [1];
+			array [0] = this.GetType ();
+			RequireDefaultConstructor ((Type)array [0]);
 		}
 
 		[RecognizedReflectionAccessPattern]

test/Mono.Linker.Tests.Cases/DataFlow/MethodReturnParameterDataFlow.cs
@@ -94,7 +94,13 @@ namespace Mono.Linker.Tests.Cases.DataFlow
 			return null;
 		}
 
-		[UnrecognizedReflectionAccessPattern (typeof (MethodReturnParameterDataFlow), nameof (ReturnPublicConstructorsFailure), new Type [] { typeof (Type) })]
+		// Validate error message when insufficiently annotated value is returned from a method
+		[UnrecognizedReflectionAccessPattern (typeof (MethodReturnParameterDataFlow), nameof (ReturnPublicConstructorsFailure), new Type [] { typeof (Type) },
+			"The parameter 'defaultConstructorType' of method 'System.Type Mono.Linker.Tests.Cases.DataFlow.MethodReturnParameterDataFlow::ReturnPublicConstructorsFailure(System.Type)' " +
+			"with dynamically accessed member kinds 'DefaultConstructor' is " +
+			"passed into the return value of method 'System.Type Mono.Linker.Tests.Cases.DataFlow.MethodReturnParameterDataFlow::ReturnPublicConstructorsFailure(System.Type)' " +
+			"which requires dynamically accessed member kinds `PublicConstructors`. " +
+			"To fix this add DynamicallyAccessedMembersAttribute to it and specify at least these member kinds 'PublicConstructors'.")]
 		[Kept]
 		[return: DynamicallyAccessedMembers (DynamicallyAccessedMemberKinds.PublicConstructors)]
 		[return: KeptAttributeAttribute (typeof (DynamicallyAccessedMembersAttribute))]

dotnet/runtime

commit 0df8cf2a976ac995f5e371d3e0cdc33396719b15

[feature/dataflow] Much improved error messages when data flow fails (dotnet/linker#1037)
Much improved error messages when data flow fails

Commit migrated from https://github.com/dotnet/linker/commit/061873b5ed58a4e96d46002157467006f9327a75

src/tools/illink/src/linker/Linker.Steps/MarkStep.cs
@@ -3528,7 +3528,7 @@ namespace Mono.Linker.Steps {
 					if (requiredMemberKinds != 0) {
 						var reflectionContext = new ReflectionPatternContext (_markStep._context, methodBody.Method, methodBody.Method, 0);
 						reflectionContext.AnalyzingPattern ();
-						RequireDynamicallyAccessedMembers (ref reflectionContext, requiredMemberKinds, MethodReturnValue);
+						RequireDynamicallyAccessedMembers (ref reflectionContext, requiredMemberKinds, MethodReturnValue, methodBody.Method.MethodReturnType);
 					}
 				}
 			}
@@ -3600,7 +3600,7 @@ namespace Mono.Linker.Steps {
 						case "CreateInstance" when !calledMethod.ContainsGenericParameter
 							&& calledMethod.DeclaringType.Name == "Activator"
 							&& calledMethod.Parameters.Count >= 1
-							&& calledMethod.Parameters[0].ParameterType.MetadataType != MetadataType.String: {
+							&& calledMethod.Parameters [0].ParameterType.MetadataType != MetadataType.String: {
 
 								var parameters = calledMethod.Parameters;
 
@@ -3648,7 +3648,7 @@ namespace Mono.Linker.Steps {
 								}
 
 								// Go over all types we've seen
-								foreach (var value in methodParams[0].UniqueValues ()) {
+								foreach (var value in methodParams [0].UniqueValues ()) {
 									if (value is SystemTypeValue systemTypeValue) {
 										// Special case known type values as we can do better by applying exact binding flags and parameter count.
 										MarkMethodsFromReflectionCall (ref reflectionContext, systemTypeValue.TypeRepresented, ".ctor", bindingFlags, ctorParameterCount);
@@ -3659,7 +3659,7 @@ namespace Mono.Linker.Steps {
 											: ((bindingFlags & BindingFlags.NonPublic) == 0)
 												? DynamicallyAccessedMemberKinds.PublicConstructors
 												: DynamicallyAccessedMemberKinds.Constructors;
-										RequireDynamicallyAccessedMembers (ref reflectionContext, requiredMemberKinds, value);
+										RequireDynamicallyAccessedMembers (ref reflectionContext, requiredMemberKinds, value, calledMethod.Parameters [0]);
 									}
 								}
 							}
@@ -3669,8 +3669,10 @@ namespace Mono.Linker.Steps {
 								reflectionContext.AnalyzingPattern ();
 								for (int parameterIndex = 0; parameterIndex < methodParams.Count; parameterIndex ++) {
 									var requiredMemberKinds = _flowAnnotations.GetParameterAnnotation (calledMethodDefinition, parameterIndex);
-									if (requiredMemberKinds != 0)
-										RequireDynamicallyAccessedMembers (ref reflectionContext, requiredMemberKinds, methodParams [parameterIndex]);
+									if (requiredMemberKinds != 0) {
+										var targetParameter = calledMethodDefinition.Parameters [parameterIndex - (calledMethodDefinition.HasImplicitThis () ? 1 : 0)];
+										RequireDynamicallyAccessedMembers (ref reflectionContext, requiredMemberKinds, methodParams [parameterIndex], targetParameter);
+									}
 								}
 
 								reflectionContext.RecordHandledPattern ();
@@ -3719,12 +3721,16 @@ namespace Mono.Linker.Steps {
 				return true;
 			}
 
-			void RequireDynamicallyAccessedMembers(ref ReflectionPatternContext reflectionContext, DynamicallyAccessedMemberKinds requiredMemberKinds, ValueNode value)
+			void RequireDynamicallyAccessedMembers (ref ReflectionPatternContext reflectionContext, DynamicallyAccessedMemberKinds requiredMemberKinds, ValueNode value, IMetadataTokenProvider targetContext)
 			{
 				foreach (var uniqueValue in value.UniqueValues ()) {
 					if (uniqueValue is LeafValueWithDynamicallyAccessedMemberNode valueWithDynamicallyAccessedMember) {
 						if (!valueWithDynamicallyAccessedMember.DynamicallyAccessedMemberKinds.HasFlag (requiredMemberKinds)) {
-							reflectionContext.RecordUnrecognizedPattern ($"Value from {valueWithDynamicallyAccessedMember.SourceContext} doesn't have enough dynamically accessed members required by {reflectionContext.MethodCalled}.");
+							reflectionContext.RecordUnrecognizedPattern ($"The {GetValueDescriptionForErrorMessage (valueWithDynamicallyAccessedMember)} " +
+								$"with dynamically accessed member kinds '{GetDynamicallyAccessedMemberKindsDescription (valueWithDynamicallyAccessedMember.DynamicallyAccessedMemberKinds)}' " +
+								$"is passed into the {GetMetadataTokenDescriptionForErrorMessage (targetContext)} " +
+								$"which requires dynamically accessed member kinds `{GetDynamicallyAccessedMemberKindsDescription (requiredMemberKinds)}`. " +
+								$"To fix this add DynamicallyAccessedMembersAttribute to it and specify at least these member kinds '{GetDynamicallyAccessedMemberKindsDescription (requiredMemberKinds)}'.");
 						} else {
 							reflectionContext.RecordHandledPattern ();
 						}
@@ -3743,7 +3749,10 @@ namespace Mono.Linker.Steps {
 					} else if (uniqueValue == NullValue.Instance) {
 						// Ignore - probably unreachable path as it would fail at runtime anyway.
 					} else {
-						reflectionContext.RecordUnrecognizedPattern ($"Value comes from unsupported source.");
+						reflectionContext.RecordUnrecognizedPattern ($"A {GetValueDescriptionForErrorMessage (uniqueValue)} " +
+							$"is passed into the {GetMetadataTokenDescriptionForErrorMessage (targetContext)} " +
+							$"which requires dynamically accessed member kinds `{GetDynamicallyAccessedMemberKindsDescription (requiredMemberKinds)}`. " +
+							$"It's not possible to guarantee that these requirements are met by the application.");
 					}
 				}
 
@@ -3784,6 +3793,82 @@ namespace Mono.Linker.Steps {
 					bool publicOnly = (bindingFlags & (BindingFlags.Public | BindingFlags.NonPublic)) == BindingFlags.Public;
 					reflectionContext.RecordUnrecognizedPattern ($"Reflection call '{reflectionContext.MethodCalled.FullName}' inside '{reflectionContext.MethodCalling.FullName}' could not resolve {(publicOnly ? "public" : "")} method `{name}` on type `{declaringType.FullName}`.");
 				}
+			}
+
+			string GetValueDescriptionForErrorMessage (ValueNode value)
+			{
+				switch (value) {
+					case MethodParameterValue methodParameterValue: {
+							if (methodParameterValue.SourceContext is MethodDefinition method) {
+								int declaredParameterIndex = methodParameterValue.ParameterIndex - (method.HasImplicitThis () ? 1 : 0);
+								if (declaredParameterIndex >= 0 && declaredParameterIndex < method.Parameters.Count)
+									return GetMetadataTokenDescriptionForErrorMessage (method.Parameters [declaredParameterIndex]);
+							}
+
+							return $"method prameter #{methodParameterValue.ParameterIndex}";
+						}
+
+					case MethodReturnValue methodReturnValue: {
+							if (methodReturnValue.SourceContext is MethodDefinition method) {
+								return GetMetadataTokenDescriptionForErrorMessage (method.MethodReturnType);
+							}
+
+							return "method return value";
+						}
+
+					default:
+						return $"value from unknown source";
+				}
+			}
+
+			string GetMetadataTokenDescriptionForErrorMessage (IMetadataTokenProvider targetContext)
+			{
+				switch (targetContext) {
+					case ParameterDefinition parameterDefinition: return $"parameter '{parameterDefinition.Name}' of method '{parameterDefinition.Method}'";
+					case MethodReturnType methodReturnType: return $"return value of method '{methodReturnType.Method}'";
+					default: return targetContext.ToString ();
+				};
+			}
+
+			string GetDynamicallyAccessedMemberKindsDescription (DynamicallyAccessedMemberKinds memberKinds)
+			{
+				var results = new List<DynamicallyAccessedMemberKinds> ();
+				if (memberKinds.HasFlag (DynamicallyAccessedMemberKinds.Constructors))
+					results.Add (DynamicallyAccessedMemberKinds.Constructors);
+				else if (memberKinds.HasFlag (DynamicallyAccessedMemberKinds.PublicConstructors))
+					results.Add (DynamicallyAccessedMemberKinds.PublicConstructors);
+				else if (memberKinds.HasFlag (DynamicallyAccessedMemberKinds.DefaultConstructor))
+					results.Add (DynamicallyAccessedMemberKinds.DefaultConstructor);
+				
+				if (memberKinds.HasFlag (DynamicallyAccessedMemberKinds.Methods))
+					results.Add (DynamicallyAccessedMemberKinds.Methods);
+				else if (memberKinds.HasFlag (DynamicallyAccessedMemberKinds.PublicMethods))
+					results.Add (DynamicallyAccessedMemberKinds.PublicMethods);
+				
+				if (memberKinds.HasFlag (DynamicallyAccessedMemberKinds.Properties))
+					results.Add (DynamicallyAccessedMemberKinds.Properties);
+				else if (memberKinds.HasFlag (DynamicallyAccessedMemberKinds.PublicProperties))
+					results.Add (DynamicallyAccessedMemberKinds.PublicProperties);
+				
+				if (memberKinds.HasFlag (DynamicallyAccessedMemberKinds.Fields))
+					results.Add (DynamicallyAccessedMemberKinds.Fields);
+				else if (memberKinds.HasFlag (DynamicallyAccessedMemberKinds.PublicFields))
+					results.Add (DynamicallyAccessedMemberKinds.PublicFields);
+				
+				if (memberKinds.HasFlag (DynamicallyAccessedMemberKinds.Events))
+					results.Add (DynamicallyAccessedMemberKinds.Events);
+				else if (memberKinds.HasFlag (DynamicallyAccessedMemberKinds.PublicEvents))
+					results.Add (DynamicallyAccessedMemberKinds.PublicEvents);
+				
+				if (memberKinds.HasFlag (DynamicallyAccessedMemberKinds.NestedTypes))
+					results.Add (DynamicallyAccessedMemberKinds.NestedTypes);
+				else if (memberKinds.HasFlag (DynamicallyAccessedMemberKinds.PublicNestedTypes))
+					results.Add (DynamicallyAccessedMemberKinds.PublicNestedTypes);
+
+				if (results.Count == 0)
+					return "None";
+
+				return string.Join (" | ", results.Select (r => r.ToString ()));
 			}
 		}
 	}

src/tools/illink/test/Mono.Linker.Tests.Cases/DataFlow/MethodParametersDataFlow.cs
@@ -32,7 +32,13 @@ namespace Mono.Linker.Tests.Cases.DataFlow
 		}
 
 		[Kept]
-		[UnrecognizedReflectionAccessPattern (typeof (MethodParametersDataFlow), nameof (RequirePublicConstructors), new Type [] { typeof (Type) })]
+		// Validate the error message when annotated parameter is passed to another annotated parameter
+		[UnrecognizedReflectionAccessPattern (typeof (MethodParametersDataFlow), nameof (RequirePublicConstructors), new Type [] { typeof (Type) },
+			"The parameter 'type' of method 'System.Void Mono.Linker.Tests.Cases.DataFlow.MethodParametersDataFlow::DefaultConstructorParameter(System.Type)' " +
+			"with dynamically accessed member kinds 'DefaultConstructor' is passed into the parameter 'type' " +
+			"of method 'System.Void Mono.Linker.Tests.Cases.DataFlow.MethodParametersDataFlow::RequirePublicConstructors(System.Type)' " +
+			"which requires dynamically accessed member kinds `PublicConstructors`. " +
+			"To fix this add DynamicallyAccessedMembersAttribute to it and specify at least these member kinds 'PublicConstructors'.")]
 		[UnrecognizedReflectionAccessPattern (typeof (MethodParametersDataFlow), nameof (RequireConstructors), new Type [] { typeof (Type) })]
 		private static void DefaultConstructorParameter (
 			[DynamicallyAccessedMembers(DynamicallyAccessedMemberKinds.DefaultConstructor)]
@@ -110,18 +116,31 @@ namespace Mono.Linker.Tests.Cases.DataFlow
 			RequirePublicConstructors (t);
 		}
 
-		[UnrecognizedReflectionAccessPattern (typeof (MethodParametersDataFlow), nameof (RequireDefaultConstructor), new Type [] { typeof (Type) })]
+		// Validate the error message for the case of unannotated method return value passed to an annotated parameter.
+		[UnrecognizedReflectionAccessPattern (typeof (MethodParametersDataFlow), nameof (RequireDefaultConstructor), new Type [] { typeof (Type) },
+			"The parameter 'type' of method 'System.Void Mono.Linker.Tests.Cases.DataFlow.MethodParametersDataFlow::NoAnnotation(System.Type)' " +
+			"with dynamically accessed member kinds 'None' is passed into " +
+			"the parameter 'type' of method 'System.Void Mono.Linker.Tests.Cases.DataFlow.MethodParametersDataFlow::RequireDefaultConstructor(System.Type)' " +
+			"which requires dynamically accessed member kinds `DefaultConstructor`. " +
+			"To fix this add DynamicallyAccessedMembersAttribute to it and specify at least these member kinds 'DefaultConstructor'.")]
 		[Kept]
 		private void NoAnnotation (Type type)
 		{
-			RequireDefaultConstructor (this.GetType ());
+			RequireDefaultConstructor (type);
 		}
 
-		[UnrecognizedReflectionAccessPattern (typeof (MethodParametersDataFlow), nameof (RequireDefaultConstructor), new Type [] { typeof (Type) })]
+		// Validate error message when untracable value is passed to an annotated parameter.
+		[UnrecognizedReflectionAccessPattern (typeof (MethodParametersDataFlow), nameof (RequireDefaultConstructor), new Type [] { typeof (Type) },
+			"A value from unknown source is passed " +
+			"into the parameter 'type' of method 'System.Void Mono.Linker.Tests.Cases.DataFlow.MethodParametersDataFlow::RequireDefaultConstructor(System.Type)' " +
+			"which requires dynamically accessed member kinds `DefaultConstructor`. " +
+			"It's not possible to guarantee that these requirements are met by the application.")]
 		[Kept]
 		private void UnknownValue ()
 		{
-			RequireDefaultConstructor (this.GetType ());
+			var array = new object [1];
+			array [0] = this.GetType ();
+			RequireDefaultConstructor ((Type)array [0]);
 		}
 
 		[RecognizedReflectionAccessPattern]

src/tools/illink/test/Mono.Linker.Tests.Cases/DataFlow/MethodReturnParameterDataFlow.cs
@@ -94,7 +94,13 @@ namespace Mono.Linker.Tests.Cases.DataFlow
 			return null;
 		}
 
-		[UnrecognizedReflectionAccessPattern (typeof (MethodReturnParameterDataFlow), nameof (ReturnPublicConstructorsFailure), new Type [] { typeof (Type) })]
+		// Validate error message when insufficiently annotated value is returned from a method
+		[UnrecognizedReflectionAccessPattern (typeof (MethodReturnParameterDataFlow), nameof (ReturnPublicConstructorsFailure), new Type [] { typeof (Type) },
+			"The parameter 'defaultConstructorType' of method 'System.Type Mono.Linker.Tests.Cases.DataFlow.MethodReturnParameterDataFlow::ReturnPublicConstructorsFailure(System.Type)' " +
+			"with dynamically accessed member kinds 'DefaultConstructor' is " +
+			"passed into the return value of method 'System.Type Mono.Linker.Tests.Cases.DataFlow.MethodReturnParameterDataFlow::ReturnPublicConstructorsFailure(System.Type)' " +
+			"which requires dynamically accessed member kinds `PublicConstructors`. " +
+			"To fix this add DynamicallyAccessedMembersAttribute to it and specify at least these member kinds 'PublicConstructors'.")]
 		[Kept]
 		[return: DynamicallyAccessedMembers (DynamicallyAccessedMemberKinds.PublicConstructors)]
 		[return: KeptAttributeAttribute (typeof (DynamicallyAccessedMembersAttribute))]