23f1d00c03187ff4d36af46ac12447433d3c99ec
John Salem <josalem@microsoft.com>
2020-04-01 12:13:42 -0700

    Add initial test harness and sample tests

---
+++ src/coreclr/tests/src/tracing/eventpipe/common/Reverse.cs
@@ -0,0 +1,121 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace Tracing.Tests.Common
+{
+    /**
+     * ==ADVERTISE PROTOCOL==
+     * Before standard IPC Protocol communication can occur on a client-mode connection
+     * the runtime must advertise itself over the connection.  ALL SUBSEQUENT COMMUNICATION 
+     * IS STANDARD DIAGNOSTICS IPC PROTOCOL COMMUNICATION.
+     * 
+     * The flow for Advertise is a one-way burst of 32 bytes consisting of
+     * 8 bytes  - "ADVR_V1\0" (ASCII chars + null byte)
+     * 16 bytes - CLR Instance Cookie (little-endian)
+     * 8 bytes  - PID (little-endian)
+     */
+
+    public class IpcAdvertise
+    {
+        public static int Size_V1 => 32;
+        public static byte[] Magic_V1 => System.Text.Encoding.ASCII.GetBytes("ADVR_V1" + '\0');
+        public static int MagicSize_V1 => 8;
+
+        public byte[] Magic = Magic_V1;
+        public UInt64 ProcessId;
+        public Guid RuntimeInstanceCookie;
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <returns> (pid, clrInstanceId) </returns>
+        public static IpcAdvertise Parse(Stream stream)
+        {
+            var binaryReader = new BinaryReader(stream);
+            var advertise = new IpcAdvertise()
+            {
+                Magic = binaryReader.ReadBytes(Magic_V1.Length),
+                RuntimeInstanceCookie = new Guid(binaryReader.ReadBytes(16)),
+                ProcessId = binaryReader.ReadUInt64()
+            };
+
+            for (int i = 0; i < Magic_V1.Length; i++)
+                if (advertise.Magic[i] != Magic_V1[i])
+                    throw new Exception("Invalid advertise message from client connection");
+
+            // FUTURE: switch on incoming magic and change if version ever increments
+            return advertise;
+        }
+
+        override public string ToString()
+        {
+            return $"{{ Magic={Magic}; ClrInstanceId={RuntimeInstanceCookie}; ProcessId={ProcessId}; }}";
+        }
+    }
+    public static class ReverseServer
+    {
+        public static string MakeServerAddress()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return Path.GetRandomFileName();
+            }
+            else
+            {
+                return Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            }
+        }
+
+        // Creates the server, listens, and closes the server
+        public static async Task<IpcAdvertise> CreateServerAndReceiveAdvertisement(string serverAddress)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                using var serverStream = new NamedPipeServerStream(serverAddress);
+                Logger.logger.Log("Waiting for connection");
+                await serverStream.WaitForConnectionAsync();
+                Logger.logger.Log("Got a connection");
+                IpcAdvertise advertise = IpcAdvertise.Parse(serverStream);
+                serverStream.Disconnect();
+                return advertise;
+            }
+            else
+            {
+                if (File.Exists(serverAddress))
+                    File.Delete(serverAddress);
+                var remoteEP = new UnixDomainSocketEndPoint(serverAddress);
+
+                using var socket = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.Unspecified);
+                socket.Bind(remoteEP);
+                socket.Listen(255);
+                socket.LingerState.Enabled = false;
+                Logger.logger.Log("Waiting for connection");
+                using Socket clientSocket = await socket.AcceptAsync();
+                Logger.logger.Log("Got a connection");
+                using var socketStream = new NetworkStream(clientSocket);
+                IpcAdvertise advertise = IpcAdvertise.Parse(socketStream);
+                try
+                {
+                    socket.Shutdown(SocketShutdown.Both);
+                }
+                finally
+                {
+                    clientSocket.Close();
+                    socket.Close();
+                    if (File.Exists(serverAddress))
+                        File.Delete(serverAddress);
+                }
+
+                return advertise;
+            }
+        }
+    }
+}
---
+++ src/coreclr/tests/src/tracing/eventpipe/reverse/reverse.cs
@@ -0,0 +1,108 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Diagnostics.Tracing;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using Microsoft.Diagnostics.Tools.RuntimeClient;
+using Tracing.Tests.Common;
+
+namespace Tracing.Tests.ReverseValidation
+{
+    public class ReverseValidation
+    {
+        public static async Task RunSubprocess(string serverName, Func<Task> beforeExecution = null, Func<Task> duringExecution = null, Func<Task> afterExecution = null)
+        {
+            using (var process = new Process())
+            {
+                if (beforeExecution != null)
+                    await beforeExecution();
+
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.Environment.Add("DOTNET_DiagnosticsMonitorAddress", serverName);
+                process.StartInfo.FileName = Process.GetCurrentProcess().MainModule.FileName;
+                process.StartInfo.Arguments = Assembly.GetExecutingAssembly().CodeBase + " 0";
+                Logger.logger.Log($"running sub-process: {process.StartInfo.FileName} {process.StartInfo.Arguments}");
+                bool fSuccess = process.Start();
+                Logger.logger.Log($"subprocess started: {fSuccess}");
+
+                if (duringExecution != null)
+                    await duringExecution();
+
+                process.Kill();
+
+                if (afterExecution != null)
+                    await afterExecution();
+            }
+        }
+        public static async Task<bool> TEST_RuntimeIsResilientToServerClosing()
+        {
+            string serverName = ReverseServer.MakeServerAddress();
+            Logger.logger.Log($"Server name is '{serverName}'");
+            await RunSubprocess(
+                serverName: serverName,
+                duringExecution: async () =>
+                {
+                    var ad1 = await ReverseServer.CreateServerAndReceiveAdvertisement(serverName);
+                    Logger.logger.Log(ad1.ToString());
+                    var ad2 = await ReverseServer.CreateServerAndReceiveAdvertisement(serverName);
+                    Logger.logger.Log(ad2.ToString());
+                    var ad3 = await ReverseServer.CreateServerAndReceiveAdvertisement(serverName);
+                    Logger.logger.Log(ad3.ToString());
+                    var ad4 = await ReverseServer.CreateServerAndReceiveAdvertisement(serverName);
+                    Logger.logger.Log(ad4.ToString());
+                }
+            );
+
+            return true;
+        }
+
+        public static async Task<bool> TEST_RuntimeConnectsToExistingServer()
+        {
+            string serverName = ReverseServer.MakeServerAddress();
+            Task<IpcAdvertise> advertiseTask = ReverseServer.CreateServerAndReceiveAdvertisement(serverName);
+            Logger.logger.Log($"Server name is `{serverName}`");
+            await RunSubprocess(
+                serverName: serverName,
+                duringExecution: async () => 
+                {
+                    IpcAdvertise advertise = await advertiseTask;
+                    Logger.logger.Log(advertise.ToString());
+                }
+            );
+
+            return true;
+        }
+
+
+        public static async Task<int> Main(string[] args)
+        {
+            if (args.Length >= 1)
+            {
+                await Task.Delay(-1); // will be killed in test
+                return 1;
+            }
+
+            bool fSuccess = true;
+            IEnumerable<MethodInfo> tests = typeof(ReverseValidation).GetMethods().Where(mi => mi.Name.StartsWith("TEST_"));
+            foreach (var test in tests)
+            {
+                Logger.logger.Log($"Running test: {test.Name}");
+                bool result = await (Task<bool>)test.Invoke(null, new object[] {});
+                fSuccess &= result;
+                Logger.logger.Log($"Test passed: {result}");
+
+            }
+            return fSuccess ? 100 : -1;
+        }
+    }
+}
