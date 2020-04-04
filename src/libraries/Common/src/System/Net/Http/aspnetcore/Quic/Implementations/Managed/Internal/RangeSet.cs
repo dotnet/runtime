28f46823e1846c2e8047d8b736e1940b4bcee796
Radek Zikmund <r.zikmund.rz@gmail.com>
2020-04-04 18:56:08 +0200

    Add Remove functions to the RangeSet

--- src/libraries/Common/src/System/Net/Http/aspnetcore/Quic/Implementations/Managed/Internal/RangeSet.cs
+++ src/libraries/Common/src/System/Net/Http/aspnetcore/Quic/Implementations/Managed/Internal/RangeSet.cs
@@ -1,14 +1,16 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net.Quic.Implementations.Managed.Internal.Frames;
 using System.Net.Security;
+using System.Numerics;
 
 namespace System.Net.Quic.Implementations.Managed.Internal
 {
     /// <summary>
     ///     Container for storing numbers with efficient range queries and insertions. The values are maintained in a list of ranges.
     /// </summary>
-    internal class RangeSet
+    internal class RangeSet : IEnumerable<RangeSet.Range>
     {
         private List<Range> _ranges;
 
@@ -17,10 +19,34 @@ namespace System.Net.Quic.Implementations.Managed.Internal
             internal ulong Start;
             internal ulong End;
 
-            public Range(ulong start, ulong end)
+            internal ulong Length => End - Start + 1;
+
+            internal Range(ulong start, ulong end)
             {
                 Start = start;
                 End = end;
+            }
+
+            internal bool Includes(Range other)
+            {
+                return Includes(other.Start, other.End);
+            }
+
+            internal bool Contains(ulong value)
+            {
+                return Start <= value && value <= End;
+            }
+
+            internal bool IsSubset(ulong start, ulong end)
+            {
+                Debug.Assert(start <= end);
+                return start <= Start && End <= end;
+            }
+
+            internal bool Includes(ulong start, ulong end)
+            {
+                Debug.Assert(start <= end);
+                return Start <= start && end <= End;
             }
 
             public override string ToString() => $"[{Start}..{End}]";
@@ -31,11 +57,20 @@ namespace System.Net.Quic.Implementations.Managed.Internal
             _ranges = new List<Range>();
         }
 
+        /// <summary>
+        ///     Adds given value to the set. Equivalent to adding range [value, value].
+        /// </summary>
+        /// <param name="value">Value to be added.</param>
         internal void Add(ulong value)
         {
             Add(value, value);
         }
 
+        /// <summary>
+        ///     Adds given range of values to the set, both bounds are inclusive.
+        /// </summary>
+        /// <param name="start">Minimum value to add.</param>
+        /// <param name="end">Maximum value to add.</param>
         internal void Add(ulong start, ulong end)
         {
             Debug.Assert(start <= end);
@@ -46,37 +81,33 @@ namespace System.Net.Quic.Implementations.Managed.Internal
                 return;
             }
 
-            // do binary search and find first preceding range
-            int index = 0;
-            int delta = _ranges.Count / 2;
-            while (delta > 0)
+            int removeCount = 0;
+
+            // check the range to the left, if exists
+            int index = IndexOfPrevious(start);
+            if (index >= 0)
             {
-                if (_ranges[index + delta].Start < start)
-                {
-                    index += delta;
-                }
-
-                delta /= 2;
-            }
-
-            var range = _ranges[index];
-
-            if (start < range.Start)
-            {
-                // if overlaps with preceding range, merge them
-                if (end >= range.Start)
+                var range = _ranges[index];
+                // it overlaps with or touches the preceding range, merge them
+                if (start <= range.End + 1)
                 {
                     start = Math.Min(start, range.Start);
                     end = Math.Max(end, range.End);
 
-                    _ranges.RemoveAt(index);
+                    removeCount = 1;
+                }
+                else // keep the range
+                {
+                    index++;
                 }
             }
 
+            index = Math.Max(0, index);
+
             // check if following ranges overlap with new one
-            while (index < _ranges.Count)
+            while (index + removeCount < _ranges.Count)
             {
-                range = _ranges[index];
+                var range = _ranges[index + removeCount];
 
                 if (range.Start - 1 > end || range.End + 1 < start)
                 {
@@ -84,25 +115,148 @@ namespace System.Net.Quic.Implementations.Managed.Internal
                     break;
                 }
 
-                if (start <= range.Start && end >= range.End)
+                if (range.IsSubset(start, end))
                 {
                     // subset of new range, we can remove this range
-                    _ranges.RemoveAt(index);
+                    removeCount++;
                     continue;
                 }
 
-                // overlap with new range, remove
+                // overlap with the new range, merge it into the new one
                 start = Math.Min(start, range.Start);
                 end = Math.Max(end, range.End);
 
-                _ranges.RemoveAt(index);
+                removeCount++;
             }
 
-            // finally, add the new range
-            if (start > range.Start)
-                index++;
-            _ranges.Insert(index, new Range(start, end));
+            if (removeCount == 0)
+            {
+                // add the new range
+                _ranges.Insert(index, new Range(start, end));
+            }
+            else
+            {
+                // remove all except one range, which will be reused
+                _ranges.RemoveRange(index, removeCount - 1);
+                _ranges[index].Start = start;
+                _ranges[index].End = end;
+            }
+        }
 
+        /// <summary>
+        ///     Finds index of the largest range with start lesser than or equal to given value. The range at returned index does not necessarily contain the given value.
+        /// </summary>
+        /// <param name="value">The value to be searched for.</param>
+        /// <returns>Index of the last range started before the value or -1 if no such range exists.</returns>
+        private int IndexOfPrevious(ulong value)
+        {
+            if (_ranges[0].Start > value)
+                return -1;
+
+            // do binary search
+            int hi = _ranges.Count - 1;
+            int lo = 0;
+            while (lo <= hi)
+            {
+                int median = lo + (hi - lo) / 2;
+                int cmp = _ranges[median].Start.CompareTo(value);
+                if (cmp == 0) return median;
+                if (cmp < 0) lo = median + 1;
+                else hi = median - 1;
+            }
+
+            return lo - 1;
+        }
+
+        /// <summary>
+        ///     Checks if the given value is present in the set.
+        /// </summary>
+        /// <param name="value">The value to check.</param>
+        /// <returns></returns>
+        internal bool Contains(ulong value)
+        {
+            return Includes(value, value);
+        }
+
+        /// <summary>
+        ///     Checks if the given range of values is present in the set.
+        /// </summary>
+        /// <param name="start">Start of the range.</param>
+        /// <param name="end">Inclusive end of the range.</param>
+        /// <returns></returns>
+        internal bool Includes(ulong start, ulong end)
+        {
+            Debug.Assert(start <= end);
+
+            // empty set does not intersect with anything
+            if (_ranges.Count == 0) return false;
+            return _ranges[IndexOfPrevious(start)].Includes(start, end);
+        }
+
+        /// <summary>
+        ///     Removes given value from the set.
+        /// </summary>
+        /// <param name="value"></param>
+        internal void Remove(ulong value)
+        {
+            Remove(value, value);
+        }
+
+        /// <summary>
+        ///     Removes given inclusive range of value from the set.
+        /// </summary>
+        /// <param name="start">Start of the range to remove.</param>
+        /// <param name="end">End of the range to remove.</param>
+        internal void Remove(ulong start, ulong end)
+        {
+            Debug.Assert(start <= end);
+            if (_ranges.Count == 0) return;
+
+            int removeCount = 0;
+            int index = IndexOfPrevious(start);
+            if (index >= 0)
+            {
+                var range = _ranges[index];
+                Debug.Assert(range.Start <= start);
+                if (range.IsSubset(start, end))
+                {
+                    // remove completely
+                    removeCount++;
+                }
+                else if (end < range.End)
+                {
+                    // split the range
+                    _ranges.Insert(index, new Range(end + 1, range.End));
+                    range.End = start - 1;
+                    return;
+                }
+                else // reduce the range if needed and keep it
+                {
+                    range.End = Math.Min(range.End, start - 1);
+                    index++;
+                }
+            }
+
+            index = Math.Max(0, index);
+
+            // check all following ranges
+            while (index + removeCount < _ranges.Count)
+            {
+                var range = _ranges[index + removeCount];
+                Debug.Assert(start < range.Start);
+
+                if (end < range.End)
+                {
+                    // possible only partial overlap, adjust the range
+                    range.Start = Math.Max(range.Start, end);
+                    break;
+                }
+
+                // proper subset, remove entire range
+                removeCount++;
+            }
+
+            _ranges.RemoveRange(index, removeCount);
         }
 
         internal ulong GetMin()
@@ -118,5 +272,9 @@ namespace System.Net.Quic.Implementations.Managed.Internal
         internal Range this[int index] => _ranges[index];
 
         internal int Count => _ranges.Count;
+
+        public IEnumerator<Range> GetEnumerator() => _ranges.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
--- src/libraries/System.Net.Http/tests/UnitTests/Quic/RangeSetTest.cs
+++ src/libraries/System.Net.Http/tests/UnitTests/Quic/RangeSetTest.cs
@@ -20,13 +20,13 @@ namespace System.Net.Quic.Tests
         [Fact]
         public void AddMany()
         {
-            _rangeSet.Add(0);
             _rangeSet.Add(3);
+            _rangeSet.Add(0);
             _rangeSet.Add(6);
 
             Assert.Equal(3, _rangeSet.Count);
-            Assert.Equal(0u , _rangeSet.GetMin());
-            Assert.Equal(6u , _rangeSet.GetMax());
+            Assert.Equal(0u, _rangeSet.GetMin());
+            Assert.Equal(6u, _rangeSet.GetMax());
         }
 
         [Theory]
@@ -48,7 +48,7 @@ namespace System.Net.Quic.Tests
         [Theory]
         [InlineData(2, 4)]
         [InlineData(4, 2)]
-        public void InsertNonContiguousAbove(ulong first, ulong second)
+        public void InsertNonContiguous(ulong first, ulong second)
         {
             _rangeSet.Add(first);
             _rangeSet.Add(second);
@@ -65,8 +65,24 @@ namespace System.Net.Quic.Tests
             _rangeSet.Add(0, 5);
 
             Assert.Equal(1, _rangeSet.Count);
-            Assert.Equal(0u , _rangeSet.GetMin());
-            Assert.Equal(5u , _rangeSet.GetMax());
+            Assert.Equal(0u, _rangeSet.GetMin());
+            Assert.Equal(5u, _rangeSet.GetMax());
+        }
+
+        [Fact]
+        public void ExpandRangeInMiddle()
+        {
+            _rangeSet.Add(1, 2);
+            _rangeSet.Add(5, 9);
+            _rangeSet.Add(14, 15);
+
+            // enlarge middle range
+            _rangeSet.Add(4, 10);
+
+            Assert.Equal(3, _rangeSet.Count);
+            Assert.True(_rangeSet.Includes(4, 10));
+            Assert.False(_rangeSet.Contains(3));
+            Assert.False(_rangeSet.Includes(11, 11));
         }
 
         [Theory]
@@ -82,8 +98,61 @@ namespace System.Net.Quic.Tests
             _rangeSet.Add(start, end);
 
             Assert.Equal(1, _rangeSet.Count);
-            Assert.Equal(1u , _rangeSet.GetMin());
-            Assert.Equal(6u , _rangeSet.GetMax());
+            Assert.Equal(1u, _rangeSet.GetMin());
+            Assert.Equal(6u, _rangeSet.GetMax());
+        }
+
+        [Fact]
+        public void Includes()
+        {
+            _rangeSet.Add(1, 3);
+            _rangeSet.Add(5, 6);
+
+            Assert.True(_rangeSet.Includes(2, 2));
+            Assert.True(_rangeSet.Includes(1, 3));
+            Assert.True(_rangeSet.Includes(5, 6));
+
+            Assert.False(_rangeSet.Includes(1, 6));
+        }
+
+        [Fact]
+        public void RemoveEntireRange()
+        {
+            _rangeSet.Add(1, 5);
+            _rangeSet.Add(7, 9);
+            _rangeSet.Add(11, 15);
+            _rangeSet.Add(20, 21);
+
+            _rangeSet.Remove(7, 9);
+            _rangeSet.Remove(19, 22);
+
+            Assert.Equal(2, _rangeSet.Count);
+            Assert.Equal(1u, _rangeSet.GetMin());
+            Assert.Equal(15u, _rangeSet.GetMax());
+        }
+
+        [Fact]
+        public void RemoveSplitsRange()
+        {
+            _rangeSet.Add(1, 5);
+
+            _rangeSet.Remove(3);
+
+            Assert.Equal(2, _rangeSet.Count);
+        }
+
+        [Theory]
+        [InlineData(1, 4)]
+        [InlineData(11, 15)]
+        public void RemoveNonExistingAfter(ulong start, ulong end)
+        {
+            _rangeSet.Add(5, 10);
+
+            _rangeSet.Remove(start, end);
+
+            var range = Assert.Single(_rangeSet);
+            Assert.Equal(5u, range.Start);
+            Assert.Equal(10u, range.End);
         }
     }
 }
