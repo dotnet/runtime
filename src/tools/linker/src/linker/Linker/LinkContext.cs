858d3d779aa2870768404677e7e3d29d6d43fffd
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2020-04-04 11:30:56 +0200

    [feature/dataflow] Add support for reading dataflow annotation from JSON (dotnet/linker#1060)
    Commit migrated from https://github.com/dotnet/linker/commit/63b50f268ce2ac21618f82d5e65f247231e4ca32

--- src/tools/linker/src/linker/Linker/LinkContext.cs
+++ src/tools/linker/src/linker/Linker/LinkContext.cs
@@ -120,9 +120,12 @@ namespace Mono.Linker {
 
 		public List<string> Substitutions { get; private set; }
 
+		public List<string> DataflowAnnotations { get; private set; }
+
 		public List<PInvokeInfo> PInvokes { get; private set; }
 
 		public string PInvokesListFile;
+
 
 		public System.Collections.IDictionary Actions {
 			get { return _actions; }
@@ -222,6 +225,19 @@ namespace Mono.Linker {
 				return;
 
 			Substitutions.Add (file);
+		}
+
+		public void AddDataflowAnnotationsFile (string file)
+		{
+			if (DataflowAnnotations == null) {
+				DataflowAnnotations = new List<string> { file };
+				return;
+			}
+
+			if (DataflowAnnotations.Contains (file))
+				return;
+
+			DataflowAnnotations.Add (file);
 		}
 
 		public TypeDefinition GetType (string fullName)
