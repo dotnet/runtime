610b50a2584e0e39f230bf96b012fbb727f1af98
EgorBo <egorbo@gmail.com>
2020-04-05 15:26:29 +0300

    Add iosAppBuilderTasks project

---
+++ src/mono/IosAppBuillderTasks/AotCompiler.cs
@@ -0,0 +1,131 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class AotCompiler
+{
+    /// <summary>
+    /// Precompile all assemblies in parallel
+    /// </summary>
+    public static void PrecompileLibraries(
+        string crossCompiler,
+        string binDir,
+        string[] libsToPrecompile,
+        IDictionary<string, string> envVariables,
+        bool optimize)
+    {
+        Parallel.ForEach(libsToPrecompile,
+            new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount  },
+            lib => PrecompileLibrary(crossCompiler, binDir, lib, envVariables, optimize));
+    }
+
+    private static void PrecompileLibrary(
+        string crossCompiler,
+        string binDir,
+        string libToPrecompile,
+        IDictionary<string, string> envVariables,
+        bool optimize)
+    {
+        Utils.LogInfo($"[AOT] {libToPrecompile}");
+
+        var crossArgs = new StringBuilder();
+        crossArgs
+            .Append(" -O=gsharedvt,float32")
+            .Append(" --nollvm")
+            .Append(" --debug");
+
+        string libName = Path.GetFileNameWithoutExtension(libToPrecompile);
+        var aotArgs = new StringBuilder();
+        aotArgs
+            .Append("mtriple=arm64-ios,")
+            .Append("static,")
+            .Append("asmonly,")
+            .Append("direct-icalls,")
+            .Append("no-direct-calls,")
+            .Append("dwarfdebug,")
+            .Append("outfile=").Append(Path.Combine(binDir, libName + ".dll.s,"))
+            .Append("data-outfile=").Append(Path.Combine(binDir, libName + ".aotdata,"))
+            .Append("full,");
+
+        // TODO: enable LLVM
+
+        crossArgs
+            .Append(" --aot=").Append(aotArgs).Append(" ")
+            .Append(libToPrecompile);
+
+        Utils.RunProcess(crossCompiler, crossArgs.ToString(), envVariables, binDir);
+
+        var clangArgs = new StringBuilder();
+        if (optimize)
+        {
+            clangArgs.Append(" -O2");
+        }
+        clangArgs
+            .Append(" -isysroot ").Append(Xcode.Sysroot)
+            .Append(" -miphoneos-version-min=10.1")
+            .Append(" -arch arm64")
+            .Append(" -c ").Append(Path.Combine(binDir, libName)).Append(".dll.s")
+            .Append(" -o ").Append(Path.Combine(binDir, libName)).Append(".dll.o");
+
+        Utils.RunProcess("clang", clangArgs.ToString(), workingDir: binDir);
+    }
+
+    public static void GenerateLinkAllFile(string[] objFiles, string outputFile)
+    {
+        //  Generates 'modules.m' in order to register all managed libraries
+        //
+        //
+        // extern void *mono_aot_module_Lib1_info;
+        // extern void *mono_aot_module_Lib2_info;
+        // ...
+        //
+        // void mono_ios_register_modules (void)
+        // {
+        //     mono_aot_register_module (mono_aot_module_Lib1_info);
+        //     mono_aot_register_module (mono_aot_module_Lib2_info);
+        //     ...
+        // }
+
+        Utils.LogInfo("Generating 'modules.m'...");
+
+        var lsDecl = new StringBuilder();
+        lsDecl
+            .AppendLine("#include <mono/jit/jit.h>")
+            .AppendLine("#include <TargetConditionals.h>")
+            .AppendLine()
+            .AppendLine("#if TARGET_OS_IPHONE && !TARGET_IPHONE_SIMULATOR")
+            .AppendLine();
+
+        var lsUsage = new StringBuilder();
+        lsUsage
+            .AppendLine("void mono_ios_register_modules (void)")
+            .AppendLine("{");
+        foreach (string objFile in objFiles)
+        {
+            string symbol = "mono_aot_module_" +
+                            Path.GetFileName(objFile)
+                                .Replace(".dll.o", "")
+                                .Replace(".", "_")
+                                .Replace("-", "_") + "_info";
+
+            lsDecl.Append("extern void *").Append(symbol).Append(';').AppendLine();
+            lsUsage.Append("\tmono_aot_register_module (").Append(symbol).Append(");").AppendLine();
+        }
+        lsDecl
+            .AppendLine()
+            .Append(lsUsage)
+            .AppendLine("}")
+            .AppendLine()
+            .AppendLine("#endif")
+            .AppendLine();
+
+        File.WriteAllText(outputFile, lsDecl.ToString());
+        Utils.LogInfo($"Saved to {outputFile}.");
+    }
+}
---
+++ src/mono/IosAppBuillderTasks/IosAppBuilderTask.cs
@@ -0,0 +1,128 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+public class IosAppBuilderTask : Task
+{
+    /// <summary>
+    /// Path to arm64 AOT cross-compiler (mono-aot-cross)
+    /// It's not used for x64 (Simulator)
+    /// </summary>
+    public string? CrossCompiler { get; set; }
+
+    /// <summary>
+    /// ProjectName is used as an app name, bundleId and xcode project name
+    /// </summary>
+    [Required]
+    public string ProjectName { get; set; } = ""!;
+
+    /// <summary>
+    /// Target directory with *dll and other content to be AOT'd and/or bundled
+    /// </summary>
+    [Required]
+    public string AppDir { get; set; } = ""!;
+    /// <summary>
+    /// Path to Mono public headers (*.h)
+    /// </summary>
+    [Required]
+    public string MonoInclude { get; set; } = ""!;
+
+    /// <summary>
+    /// This library will be used as an entry-point (e.g. TestRunner.dll)
+    /// </summary>
+    [Required]
+    public string EntryPointLib { get; set; } = ""!;
+
+    /// <summary>
+    /// Path to a custom main.m with custom UI
+    /// A default one is used if it's not set
+    /// </summary>
+    public string? NativeMainSource { get; set; }
+
+    /// <summary>
+    /// Produce optimized binaries (e.g. use -O2 in AOT)
+    /// </summary>
+    public bool Optimized { get; set; }
+
+    /// <summary>
+    /// Target arch, can be "arm64" (device) or "x64" (simulator) at the moment
+    /// </summary>
+    [Required]
+    public string Arch { get; set; } = ""!;
+
+    /// <summary>
+    /// DEVELOPER_TEAM provisioning, needed for arm64 builds.
+    /// </summary>
+    public string? DevTeamProvisioning { get; set; }
+
+    /// <summary>
+    /// Path to *.app bundle
+    /// </summary>
+    [Output]
+    public string AppBundlePath { get; set; } = ""!;
+
+    public override bool Execute()
+    {
+        Utils.Logger = Log;
+        bool isDevice = Arch.Equals("arm64", StringComparison.InvariantCultureIgnoreCase);
+        if (isDevice && string.IsNullOrEmpty(CrossCompiler))
+        {
+            throw new ArgumentException("arm64 arch requires CrossCompiler");
+        }
+
+        var stopWatch = Stopwatch.StartNew();
+
+        // see https://github.com/dotnet/runtime/issues/34448
+        // Can be exposed as an input argument if needed
+        string[] excludes = {"System.Runtime.WindowsRuntime.dll"};
+        string[] libsToAot = Directory.GetFiles(AppDir, "*.dll")
+            .Where(f => !excludes.Contains(Path.GetFileName(f)))
+            .ToArray();
+
+        string binDir = Path.Combine(AppDir, $"bin-{ProjectName}-{Arch}");
+        Directory.CreateDirectory(binDir);
+
+        // run AOT compilation only for devices
+        if (Arch == "arm64")
+        {
+            if (string.IsNullOrEmpty(CrossCompiler))
+                throw new InvalidOperationException("cross-compiler is not set");
+
+            AotCompiler.PrecompileLibraries(CrossCompiler, binDir, libsToAot,
+                new Dictionary<string, string>
+                {
+                    {"DOTNET_SYSTEM_GLOBALIZATION_INVARIANT", "1"},
+                    {"MONO_PATH", AppDir},
+                },
+                Optimized);
+        }
+
+        // generate modules.m
+        AotCompiler.GenerateLinkAllFile(
+            Directory.GetFiles(binDir, "*.dll.o"),
+            Path.Combine(binDir, "modules.m"));
+
+        // generate xcode project
+        Xcode.GenerateXCode(ProjectName, EntryPointLib, AppDir, binDir, MonoInclude, NativeMainSource);
+
+        // build app
+        string appBundle = Xcode.BuildAppBundle(
+            Path.Combine(binDir, ProjectName, ProjectName + ".xcodeproj"),
+            Arch, Optimized, DevTeamProvisioning);
+
+        stopWatch.Stop();
+        Debug.Assert(Directory.Exists(appBundle)); // it's actually a directory
+        Log.LogMessage(MessageImportance.High, $"App: {appBundle} in {stopWatch.Elapsed.TotalSeconds:F1} s.");
+
+        AppBundlePath = appBundle;
+        return true;
+    }
+}
