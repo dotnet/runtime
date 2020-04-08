e016d63e8c34f99654536ac5652cfe8f96f95470
Omair Majid <omajid@redhat.com>
2020-04-07 20:00:22 -0400

    Sync cgroup v2 in libraries with coreclr
    This commit brings in two changes from coreclr to libraries:
    
    1. https://github.com/dotnet/runtime/pull/980
    
       "Fix named cgroup handling in docker"
    
       This fixes getting cgroup information for named cgroups inside containers.
    
    2. https://github.com/dotnet/runtime/pull/34334
    
       "Add cgroup v2 support to coreclr"
    
       This is essentially the same change pushed to corefx (now libraries)
       to add cgroupv2 support, but this newer coreclr change has one major
       difference: it determines whether the system is using cgroup v1 or
       cgroup v2 once, and then explicitly uses that (only). This avoids
       issues on systems where both cgroup v1 and v2 are enabled, (but only
       one is being used by default).

--- src/libraries/System.Runtime.InteropServices.RuntimeInformation/tests/DescriptionNameTests.cs
+++ src/libraries/System.Runtime.InteropServices.RuntimeInformation/tests/DescriptionNameTests.cs
@@ -41,6 +41,7 @@ namespace System.Runtime.InteropServices.RuntimeInformationTests
 
             Console.WriteLine($"### CURRENT DIRECTORY: {Environment.CurrentDirectory}");
 
+            Console.WriteLine($"### CGROUPS VERSION: {Interop.cgroups.s_cgroupVersion}");
             string cgroupsLocation = Interop.cgroups.s_cgroupMemoryLimitPath;
             if (cgroupsLocation != null)
             {
