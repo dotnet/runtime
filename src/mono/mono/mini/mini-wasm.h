929ab4d34962a6cd07fafe9ce77ef25a5935f202
lewing <lewing@users.noreply.github.com>
2020-04-08 23:32:05 +0000

    Wasm debug log level
    enable_debugging was already an int, with this change log_level
        will remain at 1 by default but passing -1 will disable the log
        spew

--- src/mono/mono/mini/mini-wasm.h
+++ src/mono/mono/mini/mini-wasm.h
@@ -104,7 +104,7 @@ typedef struct {
 void mono_wasm_debugger_init (void);
 
 // sdks/wasm/driver.c is C and uses this
-G_EXTERN_C void mono_wasm_enable_debugging (void);
+G_EXTERN_C void mono_wasm_enable_debugging (int log_level);
 
 void mono_wasm_breakpoint_hit (void);
 void mono_wasm_set_timeout (int timeout, int id);
