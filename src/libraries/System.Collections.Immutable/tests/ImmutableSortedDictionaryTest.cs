f004abe248da79dac220f72427b88f6456daa1c2
Lennart Brüggemann <lbrueggemann@outlook.com>
2020-04-10 14:35:59 +0200

    Include key when throwing KeyNotFoundException in indexer (#34759)
    * Include key when throwing KeyNotFoundException in indexer
    
    * Add quotes around replacement markers to be consistent with other strings

--- src/libraries/System.Collections.Immutable/tests/ImmutableSortedDictionaryTest.cs
+++ src/libraries/System.Collections.Immutable/tests/ImmutableSortedDictionaryTest.cs
@@ -506,6 +506,15 @@ namespace System.Collections.Immutable.Tests
             Assert.Throws<KeyNotFoundException>(() => dictionary.ValueRef("c"));
         }
 
+        [Fact]
+        public void Indexer_KeyNotFoundException_ContainsKeyInMessage()
+        {
+            var map = ImmutableSortedDictionary.Create<string, string>()
+                .Add("a", "1").Add("b", "2");
+            var exception = Assert.Throws<KeyNotFoundException>(() => map["c"]);
+            Assert.Contains("'c'", exception.Message);
+        }
+
         protected override IImmutableDictionary<TKey, TValue> Empty<TKey, TValue>()
         {
             return ImmutableSortedDictionaryTest.Empty<TKey, TValue>();
