56c217f076728317fcb9ca81ce9f14cf0e4acbaf
Santiago Fernandez Madero <safern@microsoft.com>
2020-04-10 19:27:04 -0700

    Add support to pass subset without arg specifier in Unix

--- eng/build.sh
+++ eng/build.sh
@@ -89,9 +89,19 @@ source $scriptroot/native/init-os-and-arch.sh
 # Check if an action is passed in
 declare -a actions=("b" "build" "r" "restore" "rebuild" "testnobuild" "sign" "publish" "clean")
 actInt=($(comm -12 <(printf '%s\n' "${actions[@]/#/-}" | sort) <(printf '%s\n' "${@/#--/-}" | sort)))
+i=0
 
 while [[ $# > 0 ]]; do
   opt="$(echo "${1/#--/-}" | awk '{print tolower($0)}')"
+
+  if [[ $i -eq 0 && $opt =~ ^[a-zA-Z\.\+]+ ]]; then
+    arguments="$arguments /p:Subset=$1"
+    shift 1
+    continue
+  fi
+
+  i=$(( i + 1 ))
+
   case "$opt" in
      -help|-h)
       usage
