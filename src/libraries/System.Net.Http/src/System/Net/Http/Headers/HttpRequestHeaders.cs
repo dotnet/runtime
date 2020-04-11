c572c0f09a8d243c34d46ef6244fe44f469086ce
Stephen Toub <stoub@microsoft.com>
2020-04-11 14:21:37 -0400

    Several allocation reductions in SocketsHttpHandler (#34724)
    * Remove state machine allocation for SendWithNtConnectionAuthAsync
    
    Just manually inline the tiny helper that's only used from one call site.
    
    * Remove unnecessary HeaderStoreItemInfo allocations
    
    For the common case of a raw string, let the dictionary store the raw string directly rather than always wrapping it in a HeaderStoreItemInfo.
    
    * Cache Date and Server response header values
    
    For Server, we expect it to be the same for every request on the same connection.
    
    For Date, if we're making lots of requests in a high-throughput fashion, we expect many responses on the same connection to have the same value.
    
    In both cases, we compare the received value against the last one received, and if it's the same, reuse the same string.

--- src/libraries/System.Net.Http/src/System/Net/Http/Headers/HttpRequestHeaders.cs
+++ src/libraries/System.Net.Http/src/System/Net/Http/Headers/HttpRequestHeaders.cs
@@ -266,6 +266,11 @@ namespace System.Net.Http.Headers
         {
         }
 
+        internal HttpRequestHeaders(bool forceHeaderStoreItems)
+            : base(HttpHeaderType.General | HttpHeaderType.Request | HttpHeaderType.Custom, HttpHeaderType.Response, forceHeaderStoreItems)
+        {
+        }
+
         internal override void AddHeaders(HttpHeaders sourceHeaders)
         {
             base.AddHeaders(sourceHeaders);
