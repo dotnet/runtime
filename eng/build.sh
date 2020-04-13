c1566cca41a915d0ce1c8c30eebf969cb80aadc5
Santiago Fernandez Madero <safern@microsoft.com>
2020-04-13 12:22:34 -0700

    Add support to pass subset without arg specifier in Unix (#34844)
    * Add support to pass subset without arg specifier in Unix
    
    * PR Feedback
    
    * Fix regex expression to match whole word

--- eng/build.sh
+++ eng/build.sh
@@ -89,9 +89,19 @@ source $scriptroot/native/init-os-and-arch.sh
 # Check if an action is passed in
 declare -a actions=("b" "build" "r" "restore" "rebuild" "testnobuild" "sign" "publish" "clean")
 actInt=($(comm -12 <(printf '%s\n' "${actions[@]/#/-}" | sort) <(printf '%s\n' "${@/#--/-}" | sort)))
+firstArgumentChecked=0
 
 while [[ $# > 0 ]]; do
   opt="$(echo "${1/#--/-}" | awk '{print tolower($0)}')"
+
+  if [[ $firstArgumentChecked -eq 0 && $opt =~ ^[a-zA-Z.+]+$ ]]; then
+    arguments="$arguments /p:Subset=$1"
+    shift 1
+    continue
+  fi
+
+  firstArgumentChecked=1
+
   case "$opt" in
      -help|-h)
       usage
