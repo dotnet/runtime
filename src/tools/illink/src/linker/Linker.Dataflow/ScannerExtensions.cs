13783c0c3271b30d1c83f7f434e7f6108cdb0459
Vitek Karas <vitek.karas@microsoft.com>
2020-04-14 13:35:33 -0700

    Add license headers to newly introduced dataflow files (dotnet/linker#1094)
    Commit migrated from https://github.com/dotnet/linker/commit/958731e4c16884c1ce78abcf37774ef860ddde1b

--- src/tools/illink/src/linker/Linker.Dataflow/ScannerExtensions.cs
+++ src/tools/illink/src/linker/Linker.Dataflow/ScannerExtensions.cs
@@ -1,7 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 using Mono.Cecil;
 using Mono.Cecil.Cil;
+using System;
+using System.Collections.Generic;
 
 namespace Mono.Linker.Dataflow
 {
