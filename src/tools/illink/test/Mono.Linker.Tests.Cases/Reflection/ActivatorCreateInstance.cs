9597f2d659eb7f20c336dac40f896bf08628acf1
Vitek Karas <vitek.karas@microsoft.com>
2020-04-14 16:42:55 -0700

    [feature/dataflow] Implement pattern recognition for the rest of CreateInstance overloads (dotnet/linker#1095)
    This implements it for Activator.CreateInstance* with strings, AppDomain.CreateInstance* and Assembly.CreateInstance.
    
    The implementation is basically just copying the existing behavior. Didn't try to improve it much yet.
    
    Added tests.
    
    Commit migrated from https://github.com/dotnet/linker/commit/4d85c5767d520ff0dcfc39e03efd7f3cfa0b7973

--- src/tools/illink/test/Mono.Linker.Tests.Cases/Reflection/ActivatorCreateInstance.cs
+++ src/tools/illink/test/Mono.Linker.Tests.Cases/Reflection/ActivatorCreateInstance.cs
@@ -11,16 +11,12 @@ namespace Mono.Linker.Tests.Cases.Reflection
 	[KeptMember(".ctor()")]
 	public class ActivatorCreateInstance
 	{
-		[UnrecognizedReflectionAccessPattern(
-			typeof(Activator), nameof(Activator.CreateInstance) + "<T>", new Type[0])]
 		public static void Main ()
 		{
 			Activator.CreateInstance (typeof (Test1));
 			Activator.CreateInstance (typeof (Test2), true);
 			Activator.CreateInstance (typeof (Test3), BindingFlags.NonPublic | BindingFlags.Instance, null, null, null);
 			Activator.CreateInstance (typeof (Test4), new object [] { 1, "ss" });
-			Activator.CreateInstance ("test", "Mono.Linker.Tests.Cases.Reflection.ActivatorCreateInstance+Test5");
-			Activator.CreateInstance<Test1> ();
 
 			var p = new ActivatorCreateInstance ();
 
@@ -41,6 +37,16 @@ namespace Mono.Linker.Tests.Cases.Reflection
 
 			// Public contructors required
 			FromParameterWithPublicConstructors (typeof (FromParameterWithPublicConstructorsType));
+
+			WithAssemblyName ();
+			WithAssemblyPath ();
+
+			AppDomainCreateInstance ();
+
+			UnsupportedCreateInstance ();
+
+			TestCreateInstanceOfTWithNewConstraint<TestCreateInstanceOfTWithNewConstraintType> ();
+			TestCreateInstanceOfTWithNoConstraint<TestCreateInstanceOfTWithNoConstraintType> ();
 		}
 
 		[Kept]
@@ -87,20 +93,6 @@ namespace Mono.Linker.Tests.Cases.Reflection
 		{
 			[Kept]
 			public Test4 (int i, object o)
-			{
-			}
-		}
-
-
-		[Kept]
-		class Test5
-		{
-			[Kept]
-			public Test5 ()
-			{
-			}
-
-			public Test5 (int i, object o)
 			{
 			}
 		}
@@ -207,6 +199,213 @@ namespace Mono.Linker.Tests.Cases.Reflection
 			[Kept]
 			public FromParameterWithPublicConstructorsType (int arg) { }
 			private FromParameterWithPublicConstructorsType (int arg, int arg2) { }
+		}
+
+		[Kept]
+		class WithAssemblyNameParameterless1
+		{
+			[Kept]
+			public WithAssemblyNameParameterless1 ()
+			{
+			}
+
+			public WithAssemblyNameParameterless1 (int i, object o)
+			{
+			}
+		}
+
+		[Kept]
+		class WithAssemblyNameParameterless2
+		{
+			[Kept]
+			public WithAssemblyNameParameterless2 ()
+			{
+			}
+
+			public WithAssemblyNameParameterless2 (int i, object o)
+			{
+			}
+		}
+
+		[Kept]
+		class WithAssemblyNamePublicOnly
+		{
+			[Kept]
+			public WithAssemblyNamePublicOnly ()
+			{
+			}
+
+			[Kept]
+			public WithAssemblyNamePublicOnly (int i, object o)
+			{
+			}
+
+			private WithAssemblyNamePublicOnly (int i, object o, int j)
+			{
+			}
+		}
+
+		[Kept]
+		class WithAssemblyNamePrivateOnly
+		{
+			public WithAssemblyNamePrivateOnly ()
+			{
+			}
+
+			[Kept]
+			private WithAssemblyNamePrivateOnly (int i, object o, int j)
+			{
+			}
+		}
+
+		[Kept]
+		private static void WithAssemblyName ()
+		{
+			Activator.CreateInstance ("test", "Mono.Linker.Tests.Cases.Reflection.ActivatorCreateInstance+WithAssemblyNameParameterless1");
+			Activator.CreateInstance ("test", "Mono.Linker.Tests.Cases.Reflection.ActivatorCreateInstance+WithAssemblyNameParameterless2", new object [] { });
+			Activator.CreateInstance ("test", "Mono.Linker.Tests.Cases.Reflection.ActivatorCreateInstance+WithAssemblyNamePublicOnly", false, BindingFlags.Public, null, new object [] { }, null, new object [] { });
+			Activator.CreateInstance ("test", "Mono.Linker.Tests.Cases.Reflection.ActivatorCreateInstance+WithAssemblyNamePrivateOnly", false, BindingFlags.NonPublic, null, new object [] { }, null, new object [] { });
+
+			WithNullAssemblyName ();
+			WithNonExistingAssemblyName ();
+			WithAssemblyAndUnknownTypeName ();
+			WithAssemblyAndNonExistingTypeName ();
+		}
+
+		[Kept]
+		[UnrecognizedReflectionAccessPattern (typeof (Activator), nameof (Activator.CreateInstance), new Type [] { typeof (string), typeof (string) })]
+		private static void WithNullAssemblyName ()
+		{
+			Activator.CreateInstance (null, "Mono.Linker.Tests.Cases.Reflection.ActivatorCreateInstance+WithAssemblyNameParameterless1");
+		}
+
+		[Kept]
+		[UnrecognizedReflectionAccessPattern (typeof (Activator), nameof (Activator.CreateInstance), new Type [] { typeof (string), typeof (string) })]
+		private static void WithNonExistingAssemblyName ()
+		{
+			Activator.CreateInstance ("NonExisting", "Mono.Linker.Tests.Cases.Reflection.ActivatorCreateInstance+WithAssemblyNameParameterless1");
+		}
+
+		[Kept]
+		private static string _typeNameField;
+
+		[Kept]
+		[UnrecognizedReflectionAccessPattern (typeof (Activator), nameof (Activator.CreateInstance), new Type [] { typeof (string), typeof (string), typeof (object []) })]
+		private static void WithAssemblyAndUnknownTypeName ()
+		{
+			Activator.CreateInstance ("test", _typeNameField, new object [] { });
+		}
+
+		[Kept]
+		[UnrecognizedReflectionAccessPattern (typeof (Activator), nameof (Activator.CreateInstance), new Type [] { typeof (string), typeof (string), typeof (object []) })]
+		private static void WithAssemblyAndNonExistingTypeName ()
+		{
+			Activator.CreateInstance ("test", "NonExisting", new object [] { });
+		}
+
+		[Kept]
+		class WithAssemblyPathParameterless
+		{
+			[Kept]
+			public WithAssemblyPathParameterless ()
+			{
+			}
+
+			public WithAssemblyPathParameterless (int i, object o)
+			{
+			}
+		}
+
+		[Kept]
+		private static void WithAssemblyPath ()
+		{
+			Activator.CreateInstanceFrom ("test", "Mono.Linker.Tests.Cases.Reflection.ActivatorCreateInstance+WithAssemblyPathParameterless");
+		}
+
+		[Kept]
+		class AppDomainCreateInstanceType
+		{
+			[Kept]
+			public AppDomainCreateInstanceType ()
+			{
+			}
+		}
+
+		[Kept]
+		[RecognizedReflectionAccessPattern]
+		private static void AppDomainCreateInstance ()
+		{
+			// Just a basic test that these are all recognized, we're not testing that it marks correctly as it has the exact same implementation
+			// as the above tested Activator.CreateInstance overloads
+			AppDomain.CurrentDomain.CreateInstance ("test", "Mono.Linker.Tests.Cases.Reflection.ActivatorCreateInstance+AppDomainCreateInstanceType");
+			AppDomain.CurrentDomain.CreateInstance ("test", "Mono.Linker.Tests.Cases.Reflection.ActivatorCreateInstance+AppDomainCreateInstanceType", new object [] { });
+			AppDomain.CurrentDomain.CreateInstance ("test", "Mono.Linker.Tests.Cases.Reflection.ActivatorCreateInstance+AppDomainCreateInstanceType", false, BindingFlags.Public, null, null, null, null);
+
+			AppDomain.CurrentDomain.CreateInstanceAndUnwrap ("test", "Mono.Linker.Tests.Cases.Reflection.ActivatorCreateInstance+AppDomainCreateInstanceType");
+			AppDomain.CurrentDomain.CreateInstanceAndUnwrap ("test", "Mono.Linker.Tests.Cases.Reflection.ActivatorCreateInstance+AppDomainCreateInstanceType", new object [] { });
+			AppDomain.CurrentDomain.CreateInstanceAndUnwrap ("test", "Mono.Linker.Tests.Cases.Reflection.ActivatorCreateInstance+AppDomainCreateInstanceType", false, BindingFlags.Public, null, null, null, null);
+
+			AppDomain.CurrentDomain.CreateInstanceFrom ("test", "Mono.Linker.Tests.Cases.Reflection.ActivatorCreateInstance+AppDomainCreateInstanceType");
+			AppDomain.CurrentDomain.CreateInstanceFrom ("test", "Mono.Linker.Tests.Cases.Reflection.ActivatorCreateInstance+AppDomainCreateInstanceType", new object [] { });
+			AppDomain.CurrentDomain.CreateInstanceFrom ("test", "Mono.Linker.Tests.Cases.Reflection.ActivatorCreateInstance+AppDomainCreateInstanceType", false, BindingFlags.Public, null, null, null, null);
+
+			AppDomain.CurrentDomain.CreateInstanceFromAndUnwrap ("test", "Mono.Linker.Tests.Cases.Reflection.ActivatorCreateInstance+AppDomainCreateInstanceType");
+			AppDomain.CurrentDomain.CreateInstanceFromAndUnwrap ("test", "Mono.Linker.Tests.Cases.Reflection.ActivatorCreateInstance+AppDomainCreateInstanceType", new object [] { });
+			AppDomain.CurrentDomain.CreateInstanceFromAndUnwrap ("test", "Mono.Linker.Tests.Cases.Reflection.ActivatorCreateInstance+AppDomainCreateInstanceType", false, BindingFlags.Public, null, null, null, null);
+		}
+
+		[Kept]
+		[UnrecognizedReflectionAccessPattern (typeof (Activator), nameof (Activator.CreateInstance) + "<T>", new Type [0])]
+		[UnrecognizedReflectionAccessPattern (typeof (Assembly), nameof (Assembly.CreateInstance), new Type [] { typeof (string) })]
+		[UnrecognizedReflectionAccessPattern (typeof (Assembly), nameof (Assembly.CreateInstance), new Type [] { typeof (string), typeof (bool) })]
+		[UnrecognizedReflectionAccessPattern (typeof (Assembly), nameof (Assembly.CreateInstance), new Type [] { typeof (string), typeof (bool), typeof (BindingFlags), typeof (Binder), typeof (object []), typeof (CultureInfo), typeof (object []) })]
+		private static void UnsupportedCreateInstance ()
+		{
+			Activator.CreateInstance<Test1> ();
+
+			typeof (ActivatorCreateInstance).Assembly.CreateInstance ("NonExistent");
+			typeof (ActivatorCreateInstance).Assembly.CreateInstance ("NonExistent", ignoreCase: false);
+			typeof (ActivatorCreateInstance).Assembly.CreateInstance ("NonExistent", false, BindingFlags.Public, null, new object [] { }, null, new object [] { });
+
+		}
+
+		[Kept]
+		class TestCreateInstanceOfTWithNewConstraintType
+		{
+			[Kept]
+			public TestCreateInstanceOfTWithNewConstraintType ()
+			{
+			}
+
+			public TestCreateInstanceOfTWithNewConstraintType (int i)
+			{
+			}
+		}
+
+		[Kept]
+		[RecognizedReflectionAccessPattern]
+		private static void TestCreateInstanceOfTWithNewConstraint<T> () where T : new()
+		{
+			Activator.CreateInstance<T> ();
+		}
+
+		[Kept]
+		class TestCreateInstanceOfTWithNoConstraintType
+		{
+			public TestCreateInstanceOfTWithNoConstraintType ()
+			{
+			}
+
+			public TestCreateInstanceOfTWithNoConstraintType (int i)
+			{
+			}
+		}
+
+		[Kept]
+		[UnrecognizedReflectionAccessPattern (typeof (Activator), nameof (Activator.CreateInstance) + "<T>", new Type [0])]
+		private static void TestCreateInstanceOfTWithNoConstraint<T> ()
+		{
+			Activator.CreateInstance<T> ();
 		}
 	}
 }
