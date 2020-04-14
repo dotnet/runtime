dotnet/runtime

commit 6e83eafe49ce3c55e9f68ae669f24957e5b482f8

[runtime] Try to load assemblies from bundles on netcore before trying other methods, the ALC might not be initialized yet. (#34877)

src/mono/mono/metadata/assembly.c
@@ -4806,9 +4806,7 @@ mono_assembly_request_byname (MonoAssemblyName *aname, const MonoAssemblyByNameR
 		result = prevent_reference_assembly_from_running (result, refonly);
 	}
 #else
-	result = netcore_load_reference (aname, req->request.alc, req->requesting_assembly, !req->no_postload_search);
-
-	if (!result && bundles != NULL) {
+	if (bundles != NULL) {
 		MonoImageOpenStatus status;
 		MonoImage *image;
 		image = mono_assembly_open_from_bundle (req->request.alc, aname->name, &status, FALSE);
@@ -4819,6 +4817,8 @@ mono_assembly_request_byname (MonoAssemblyName *aname, const MonoAssemblyByNameR
 		if (image)
 			result = mono_assembly_request_load_from (image, aname->name, &req->request, &status);
 	}
+	if (!result)
+		result = netcore_load_reference (aname, req->request.alc, req->requesting_assembly, !req->no_postload_search);
 #endif
 	return result;
 }

commit 1998434262472626eb301a86d06473319af8fd8d

Fix an uninitialized variable error introduced by #34877.

src/mono/mono/metadata/assembly.c
@@ -4806,6 +4806,7 @@ mono_assembly_request_byname (MonoAssemblyName *aname, const MonoAssemblyByNameR
 		result = prevent_reference_assembly_from_running (result, refonly);
 	}
 #else
+	result = NULL;
 	if (bundles != NULL) {
 		MonoImageOpenStatus status;
 		MonoImage *image;