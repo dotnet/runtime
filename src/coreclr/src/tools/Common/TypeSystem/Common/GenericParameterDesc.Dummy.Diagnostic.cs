f604973fd99f28ec575a208b58b9801867d86201
Andy Gocke <andy@commentout.net>
2020-04-13 17:51:16 -0700

    Add ILVerify from the corert repo
    No significant source modifications were made, only moving the code and any missing
    dependencies.

---
+++ src/coreclr/src/tools/Common/TypeSystem/Common/GenericParameterDesc.Dummy.Diagnostic.cs
@@ -0,0 +1,15 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Internal.TypeSystem
+{
+    // Dummy implementation of diagnostic names that just forwards to Name
+    public abstract partial class GenericParameterDesc
+    {
+        /// <summary>
+        /// Gets the name of the generic parameter as defined in the metadata. This must not throw
+        /// </summary>
+        public string DiagnosticName => Name;
+    }
+}
---
+++ src/coreclr/src/tools/ILVerification/README.md
@@ -0,0 +1,3 @@
+# ILVerification
+
+The ILVerification library is part of the ILVerify project. See details under [src/ILVerify/](https://github.com/dotnet/corert/tree/master/src/ILVerify).
