789ba645c2864b2dd506564abcabad171f388843
Mateo Torres-Ruiz <mateoatr@users.noreply.github.com>
2020-04-14 15:14:57 -0700

    [feature/dataflow] Refactor Expression.* (dotnet/linker#1100)
    * Update tests for ExpressionField/Property
    Refactor Expression.* pattern matching
    
    * Update ExpressionCallString tests
    
    * Use binding flags for marking properties on Expression.Property
    
    Commit migrated from https://github.com/dotnet/linker/commit/8e6fc7ef896f3ab40979ee2f39aaa783f4756009

--- src/tools/linker/test/Mono.Linker.Tests.Cases/Reflection/ExpressionCallString.cs
+++ src/tools/linker/test/Mono.Linker.Tests.Cases/Reflection/ExpressionCallString.cs
@@ -3,6 +3,7 @@ using System.Linq.Expressions;
 using System;
 using System.Linq;
 using Mono.Linker.Tests.Cases.Expectations.Metadata;
+using System.Runtime.CompilerServices;
 
 namespace Mono.Linker.Tests.Cases.Reflection
 {
@@ -12,187 +13,140 @@ namespace Mono.Linker.Tests.Cases.Reflection
 	{
 		public static void Main ()
 		{
-			Branch_SystemTypeValueNode_KnownStringValue ();
-			Branch_SystemTypeValueNode_NullValueNode ();
-			Branch_NullValueNode ();
-			Branch_MethodParameterValueNode (typeof (ExpressionCallString), "Foo");
-			Branch_UnrecognizedPatterns ();
-			// TODO
-			TestPublicOnBase();
-			TestProtectedOnBase();
+			Expression.Call (typeof (Foo), "PublicStaticMethod", Type.EmptyTypes);
+			Expression.Call (typeof (Foo), "PublicNonStaticMethod", Type.EmptyTypes);
+			Expression.Call (typeof (Foo), "ProtectedStaticMethod", Type.EmptyTypes);
+			Expression.Call (typeof (Foo), "ProtectedNonStaticMethod", Type.EmptyTypes);
+			Expression.Call (typeof (Foo), "PrivateStaticMethod", Type.EmptyTypes);
+			Expression.Call (typeof (Foo), "PrivateNonStaticMethod", Type.EmptyTypes);
+
+			Expression.Call (typeof (Derived), "PublicOnBase", Type.EmptyTypes);
+			Expression.Call (typeof (Derived), "ProtectedOnBase", Type.EmptyTypes);
+			Expression.Call (typeof (Derived), "PrivateOnBase", Type.EmptyTypes);
+
+			// Keep all methods on type Bar
+			Expression.Call (typeof (Bar), GetUnknownString (), Type.EmptyTypes);
+
+			TestUnknownType.Test ();
 		}
 
 		[Kept]
-		static void Branch_SystemTypeValueNode_KnownStringValue ()
+		static string GetUnknownString ()
 		{
-			TestByName (0);
-			TestByName (1);
-			TestByType (0);
-			TestByType (1);
-			TestByNameWithParameters ();
-			TestNonExistingName ();
+			return "unknownstring";
 		}
 
 		[Kept]
-		static void Branch_SystemTypeValueNode_NullValueNode ()
-		{
-			Expression.Call (typeof (ExpressionCallString), null, Type.EmptyTypes);
-		}
-
-		[Kept]
-		static void Branch_NullValueNode ()
-		{
-			Expression.Call ((Type)null, "OnlyCalledViaExpression", Type.EmptyTypes);
-		}
-
-		[Kept]
-		static void Branch_MethodParameterValueNode (Type T, string s)
-		{
-			TestNonExistingTypeParameter (T);
-			TestNonExistingNameParameter (s);
-		}
-
-		#region RecognizedReflectionAccessPatterns
-		[UnrecognizedReflectionAccessPattern ( // Expression.Call (Type, null, Type []);
-			typeof (Expression), nameof (Expression.Call), new Type [] { typeof (Type), typeof (string), typeof (Type []), typeof (Expression []) })]
-		[RecognizedReflectionAccessPattern (
-			typeof (Expression), nameof (Expression.Call), new Type [] { typeof (Type), typeof (string), typeof (Type []), typeof (Expression []) },
-			typeof (ExpressionCallString), nameof (A), new Type [0])]
-		[RecognizedReflectionAccessPattern (
-			typeof (Expression), nameof (Expression.Call), new Type [] { typeof (Type), typeof (string), typeof (Type []), typeof (Expression []) },
-			typeof (ExpressionCallString), nameof (B), new Type [0])]
-		[Kept]
-		static void TestByName (int i)
-		{
-			string MethodName = null;
-			switch (i) {
-				case 0:
-					MethodName = "A";
-					break;
-				case 1:
-					MethodName = "B";
-					break;
-				default:
-					break;
-			}
-
-			Expression.Call (typeof (ExpressionCallString), MethodName, null);
-		}
-
-		[RecognizedReflectionAccessPattern (
-			typeof (Expression), nameof (Expression.Call), new Type [] { typeof (Type), typeof (string), typeof (Type []), typeof (Expression []) },
-			typeof (C), "Foo", new Type [0])]
-		[RecognizedReflectionAccessPattern (
-			typeof (Expression), nameof (Expression.Call), new Type [] { typeof (Type), typeof (string), typeof (Type []), typeof (Expression []) },
-			typeof (D), "Foo", new Type [0])]
-		[Kept]
-		static void TestByType (int i)
-		{
-			Type T = (Type)null;
-			switch (i) {
-				case 0:
-					T = typeof (C);
-					break;
-				case 1:
-					T = typeof (D);
-					break;
-				default:
-					break;
-			}
-
-			Expression.Call (T, "Foo", Type.EmptyTypes);
-		}
-
-		[RecognizedReflectionAccessPattern (
-			typeof (Expression), nameof (Expression.Call), new Type [] { typeof (Type), typeof (string), typeof (Type []), typeof (Expression []) },
-			typeof (ExpressionCallString), nameof (Count) + "<T>", new string [] { "T" })]
-		[Kept]
-		static void TestByNameWithParameters ()
-		{
-			IQueryable source = null;
-			Expression.Call (typeof (ExpressionCallString), "Count", new Type [] { source.ElementType }, source.Expression);
-		}
-		#endregion
-
-		#region UnrecognizedReflectionAccessPatterns
-		[UnrecognizedReflectionAccessPattern (
-			typeof (Expression), nameof (Expression.Call), new Type [] { typeof (Type), typeof (string), typeof (Type []), typeof (Expression []) })]
-		[Kept]
-		static void Branch_UnrecognizedPatterns ()
-		{
-			Expression.Call (FindType (), "OnlyCalledViaExpression", Type.EmptyTypes);
-		}
-
-		[Kept]
-		static void TestNonExistingName ()
-		{
-			Expression.Call (typeof (ExpressionCallString), "NonExisting", Type.EmptyTypes);
-		}
-
-		[UnrecognizedReflectionAccessPattern (
-			typeof (Expression), nameof (Expression.Call), new Type [] { typeof (Type), typeof (string), typeof (Type []), typeof (Expression []) })]
-		[Kept]
-		static void TestNonExistingTypeParameter (Type T)
-		{
-			Expression.Call (T, "Foo", Type.EmptyTypes);
-		}
-
-		[UnrecognizedReflectionAccessPattern (
-			typeof (Expression), nameof (Expression.Call), new Type [] { typeof (Type), typeof (string), typeof (Type []), typeof (Expression []) })]
-		[Kept]
-		static void TestNonExistingNameParameter (string s)
-		{
-			Expression.Call (typeof (ExpressionCallString), s, Type.EmptyTypes);
-		}
-		#endregion
-
-		#region Helpers
-		[Kept]
-		static void TestPublicOnBase ()
-		{
-			Expression.Call (typeof (ADerived), "PublicOnBase", Type.EmptyTypes);
-		}
-
-		[Kept]
-		static void TestProtectedOnBase ()
-		{
-			Expression.Call (typeof (ADerived), "ProtectedOnBase", Type.EmptyTypes);
-		}
-
-		[Kept]
-		static Type FindType ()
-		{
-			return typeof (ExpressionCallString);
-		}
-
-		[Kept]
-		static void A () { }
-
-		[Kept]
-		static void B () { }
-
-		[Kept]
-		class C
+		class TestUnknownType
 		{
 			[Kept]
-			static void Foo () { }
+			public static void PublicMethod ()
+			{
+			}
+
+			[Kept]
+			protected static void ProtectedMethod ()
+			{
+			}
+
+			[Kept]
+			private static void PrivateMethod ()
+			{
+			}
+
+			[Kept]
+			[UnrecognizedReflectionAccessPattern (typeof (Expression), nameof (Expression.Call),
+				new Type [] { typeof (Type), typeof (string), typeof (Type []), typeof (Expression []) })]
+			public static void Test ()
+			{
+				// Keep all methods of the type that made the call
+				Expression.Call (GetUnknownType (), "This string will not be reached", Type.EmptyTypes);
+				// UnrecognizedReflectionAccessPattern
+				Expression.Call (TriggerUnrecognizedPattern (), "This string will not be reached", Type.EmptyTypes);
+			}
+
+			[Kept]
+			[return: KeptAttributeAttribute (typeof (DynamicallyAccessedMembersAttribute))]
+			[return: DynamicallyAccessedMembers (DynamicallyAccessedMemberKinds.Methods)]
+			static Type GetUnknownType ()
+			{
+				return typeof (TestUnknownType);
+			}
+
+			[Kept]
+			static Type TriggerUnrecognizedPattern ()
+			{
+				return typeof (TestUnknownType);
+			}
 		}
 
+
 		[Kept]
-		class D
+		class Foo
 		{
 			[Kept]
-			static void Foo () { }
+			public static void PublicStaticMethod ()
+			{
+			}
+
+			public void PublicNonStaticMethod ()
+			{
+			}
+
+			[Kept]
+			protected static void ProtectedStaticMethod ()
+			{
+			}
+			protected void ProtectedNonStaticMethod ()
+			{
+			}
+
+			[Kept]
+			private static void PrivateStaticMethod ()
+			{
+			}
+
+			private void PrivateNonStaticMethod ()
+			{
+			}
 		}
 
 		[Kept]
-		protected static T Count<T> (T t)
+		class Bar
 		{
-			return default (T);
+			[Kept]
+			public static void PublicStaticMethod ()
+			{
+			}
+
+			[Kept]
+			public void PublicNonStaticMethod ()
+			{
+			}
+
+			[Kept]
+			protected static void ProtectedStaticMethod ()
+			{
+			}
+
+			[Kept]
+			protected void ProtectedNonStaticMethod ()
+			{
+			}
+
+			[Kept]
+			private static void PrivateStaticMethod ()
+			{
+			}
+
+			[Kept]
+			private void PrivateNonStaticMethod ()
+			{
+			}
 		}
 
 		[Kept]
-		class ABase
+		class Base
 		{
 			[Kept]
 			public static void PublicOnBase ()
@@ -203,13 +157,16 @@ namespace Mono.Linker.Tests.Cases.Reflection
 			protected static void ProtectedOnBase ()
 			{
 			}
+
+			private static void PrivateOnBase ()
+			{
+			}
 		}
 
 		[Kept]
-		[KeptBaseType (typeof (ABase))]
-		class ADerived : ABase
+		[KeptBaseType (typeof (Base))]
+		class Derived : Base
 		{
 		}
-		#endregion
 	}
 }
