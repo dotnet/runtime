c26cf8b6fb23e0b9dfb45a5b0902b62546a1dfdb
EgorBo <egorbo@gmail.com>
2020-04-16 17:42:45 +0300

    Disable data-outfile again

--- src/mono/msbuild/AppleAppBuilder/AotCompiler.cs
+++ src/mono/msbuild/AppleAppBuilder/AotCompiler.cs
@@ -54,7 +54,8 @@ internal class AotCompiler
             .Append("dwarfdebug,")
             .Append("outfile=").Append(Path.Combine(binDir, libName + ".dll.s,"))
             .Append("msym-dir=").Append(Path.Combine(binDir, "Msym,"))
-            .Append("data-outfile=").Append(Path.Combine(binDir, libName + ".aotdata,"))
+            // TODO: enable aotdata
+            //.Append("data-outfile=").Append(Path.Combine(binDir, libName + ".aotdata,"))
             //  TODO: enable direct-pinvokes (to get rid of -force_loads)
             //.Append("direct-pinvoke,")
             .Append("full,");
