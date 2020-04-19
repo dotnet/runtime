d57ed4ebf59dd234289130af48302957f63d47a4
Robert Garrison <robert.garrison91@gmail.com>
2020-04-19 13:03:53 -0400

    Adding IsTransient property as a boolean to show consumer that the operation may succeed if tried again.

--- src/libraries/System.Data.Common/src/System/Data/Common/DbException.cs
+++ src/libraries/System.Data.Common/src/System/Data/Common/DbException.cs
@@ -8,6 +8,11 @@ namespace System.Data.Common
     [System.Runtime.CompilerServices.TypeForwardedFrom("System.Data, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089")]
     public abstract class DbException : System.Runtime.InteropServices.ExternalException
     {
+        /// <summary>
+        /// When overriden in a derived class return true in all cases where a simple retry of the operation without any other change may be successful.
+        /// </summary>
+        public bool IsTransient { get; protected set; } = false;
+
         protected DbException() : base() { }
 
         protected DbException(string message) : base(message) { }
