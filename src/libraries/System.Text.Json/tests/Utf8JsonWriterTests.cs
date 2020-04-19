5a47b9f80a057f85edd8a1005d863785863adc20
Marcus Turewicz <24448509+marcusturewicz@users.noreply.github.com>
2020-04-19 11:49:12 +1000

    Bumping CI

--- src/libraries/System.Text.Json/tests/Utf8JsonWriterTests.cs
+++ src/libraries/System.Text.Json/tests/Utf8JsonWriterTests.cs
@@ -7730,7 +7730,7 @@ namespace System.Text.Json.Tests
                     i++;
                     sb.Append("\\u003c");
                 }
-                // Remove ".0"
+                // Convert ".0" to ""
                 else if (json[i] == '.' && json[i+1] == '0')
                 {
                     i+=2;
