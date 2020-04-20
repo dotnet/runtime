a3668fef73fc4258a36627eb365367297e107165
Ryan Lucia <rylucia@microsoft.com>
2020-04-20 01:22:21 -0400

    Add embedding API function get the default ALC

--- src/mono/mono/metadata/mono-private-unstable.h
+++ src/mono/mono/metadata/mono-private-unstable.h
@@ -24,4 +24,7 @@ typedef MonoAssembly * (*MonoAssemblyPreLoadFuncV3) (MonoAssemblyLoadContextGCHa
 MONO_API MONO_RT_EXTERNAL_ONLY void
 mono_install_assembly_preload_hook_v3 (MonoAssemblyPreLoadFuncV3 func, gpointer user_data, gboolean append);
 
+MONO_API MONO_RT_EXTERNAL_ONLY MonoAssemblyLoadContextGCHandle
+mono_alc_get_default_gchandle (void);
+
 #endif /*__MONO_METADATA_MONO_PRIVATE_UNSTABLE_H__*/
