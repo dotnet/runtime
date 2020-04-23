27fb9438a73c5c7cefbcd7c810dbd1933c320fd2
fanyang-mono <yangfan@microsoft.com>
2020-04-23 16:33:03 -0400

    Change header file name one more time

--- src/mono/mono/utils/mono-time.h
+++ src/mono/mono/utils/mono-time.h
@@ -33,7 +33,7 @@ gint64 mono_100ns_datetime_from_timeval (struct timeval tv);
 #include <mach/clock.h>
 typedef clock_serv_t mono_clock_id_t;
 #elif defined(__linux__)
-#include <time.h>
+#include <sys/types.h>
 typedef clockid_t mono_clock_id_t;
 #else
 typedef void* mono_clock_id_t;
