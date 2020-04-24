0ac3bfbb6edbb818e340c03da93c2f4912fb3742
Egor Chesakov <Egor.Chesakov@microsoft.com>
2020-04-24 13:51:36 -0700

    Add FusedSubtractHalving in AdvSimd.cs AdvSimd.PlatformNotSupported.cs

--- src/libraries/System.Private.CoreLib/src/System/Runtime/Intrinsics/Arm/AdvSimd.cs
+++ src/libraries/System.Private.CoreLib/src/System/Runtime/Intrinsics/Arm/AdvSimd.cs
@@ -4616,6 +4616,90 @@ namespace System.Runtime.Intrinsics.Arm
         public static Vector64<float> FusedMultiplySubtractNegatedScalar(Vector64<float> acc, Vector64<float> left, Vector64<float> right) => FusedMultiplySubtractNegatedScalar(acc, left, right);
 
         /// <summary>
+        /// uint8x8_t vhsub_u8 (uint8x8_t a, uint8x8_t b)
+        ///   A32: VHSUB.U8 Dd, Dn, Dm
+        ///   A64: UHSUB Vd.8B, Vn.8B, Vm.8B
+        /// </summary>
+        public static Vector64<byte> FusedSubtractHalving(Vector64<byte> left, Vector64<byte> right) => FusedSubtractHalving(left, right);
+
+        /// <summary>
+        /// int16x4_t vhsub_s16 (int16x4_t a, int16x4_t b)
+        ///   A32: VHSUB.S16 Dd, Dn, Dm
+        ///   A64: SHSUB Vd.4H, Vn.4H, Vm.4H
+        /// </summary>
+        public static Vector64<short> FusedSubtractHalving(Vector64<short> left, Vector64<short> right) => FusedSubtractHalving(left, right);
+
+        /// <summary>
+        /// int32x2_t vhsub_s32 (int32x2_t a, int32x2_t b)
+        ///   A32: VHSUB.S32 Dd, Dn, Dm
+        ///   A64: SHSUB Vd.2S, Vn.2S, Vm.2S
+        /// </summary>
+        public static Vector64<int> FusedSubtractHalving(Vector64<int> left, Vector64<int> right) => FusedSubtractHalving(left, right);
+
+        /// <summary>
+        /// int8x8_t vhsub_s8 (int8x8_t a, int8x8_t b)
+        ///   A32: VHSUB.S8 Dd, Dn, Dm
+        ///   A64: SHSUB Vd.8B, Vn.8B, Vm.8B
+        /// </summary>
+        public static Vector64<sbyte> FusedSubtractHalving(Vector64<sbyte> left, Vector64<sbyte> right) => FusedSubtractHalving(left, right);
+
+        /// <summary>
+        /// uint16x4_t vhsub_u16 (uint16x4_t a, uint16x4_t b)
+        ///   A32: VHSUB.U16 Dd, Dn, Dm
+        ///   A64: UHSUB Vd.4H, Vn.4H, Vm.4H
+        /// </summary>
+        public static Vector64<ushort> FusedSubtractHalving(Vector64<ushort> left, Vector64<ushort> right) => FusedSubtractHalving(left, right);
+
+        /// <summary>
+        /// uint32x2_t vhsub_u32 (uint32x2_t a, uint32x2_t b)
+        ///   A32: VHSUB.U32 Dd, Dn, Dm
+        ///   A64: UHSUB Vd.2S, Vn.2S, Vm.2S
+        /// </summary>
+        public static Vector64<uint> FusedSubtractHalving(Vector64<uint> left, Vector64<uint> right) => FusedSubtractHalving(left, right);
+
+        /// <summary>
+        /// uint8x16_t vhsubq_u8 (uint8x16_t a, uint8x16_t b)
+        ///   A32: VHSUB.U8 Qd, Qn, Qm
+        ///   A64: UHSUB Vd.16B, Vn.16B, Vm.16B
+        /// </summary>
+        public static Vector128<byte> FusedSubtractHalving(Vector128<byte> left, Vector128<byte> right) => FusedSubtractHalving(left, right);
+
+        /// <summary>
+        /// int16x8_t vhsubq_s16 (int16x8_t a, int16x8_t b)
+        ///   A32: VHSUB.S16 Qd, Qn, Qm
+        ///   A64: SHSUB Vd.8H, Vn.8H, Vm.8H
+        /// </summary>
+        public static Vector128<short> FusedSubtractHalving(Vector128<short> left, Vector128<short> right) => FusedSubtractHalving(left, right);
+
+        /// <summary>
+        /// int32x4_t vhsubq_s32 (int32x4_t a, int32x4_t b)
+        ///   A32: VHSUB.S32 Qd, Qn, Qm
+        ///   A64: SHSUB Vd.4S, Vn.4S, Vm.4S
+        /// </summary>
+        public static Vector128<int> FusedSubtractHalving(Vector128<int> left, Vector128<int> right) => FusedSubtractHalving(left, right);
+
+        /// <summary>
+        /// int8x16_t vhsubq_s8 (int8x16_t a, int8x16_t b)
+        ///   A32: VHSUB.S8 Qd, Qn, Qm
+        ///   A64: SHSUB Vd.16B, Vn.16B, Vm.16B
+        /// </summary>
+        public static Vector128<sbyte> FusedSubtractHalving(Vector128<sbyte> left, Vector128<sbyte> right) => FusedSubtractHalving(left, right);
+
+        /// <summary>
+        /// uint16x8_t vhsubq_u16 (uint16x8_t a, uint16x8_t b)
+        ///   A32: VHSUB.U16 Qd, Qn, Qm
+        ///   A64: UHSUB Vd.8H, Vn.8H, Vm.8H
+        /// </summary>
+        public static Vector128<ushort> FusedSubtractHalving(Vector128<ushort> left, Vector128<ushort> right) => FusedSubtractHalving(left, right);
+
+        /// <summary>
+        /// uint32x4_t vhsubq_u32 (uint32x4_t a, uint32x4_t b)
+        ///   A32: VHSUB.U32 Qd, Qn, Qm
+        ///   A64: UHSUB Vd.4S, Vn.4S, Vm.4S
+        /// </summary>
+        public static Vector128<uint> FusedSubtractHalving(Vector128<uint> left, Vector128<uint> right) => FusedSubtractHalving(left, right);
+
+        /// <summary>
         /// uint8x8_t vset_lane_u8 (uint8_t a, uint8x8_t v, const int lane)
         ///   A32: VMOV.8 Dd[lane], Rt
         ///   A64: INS Vd.B[lane], Wn
