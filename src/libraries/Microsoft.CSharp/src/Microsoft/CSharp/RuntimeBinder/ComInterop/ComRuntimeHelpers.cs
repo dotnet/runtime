28ff1a9add32511c0efcbb7d78b9bb50459690b8
Stephen Toub <stoub@microsoft.com>
2020-04-24 17:39:01 -0400

    Emit shorter variants in ILGenerator.Emit(OpCode, int)
    The Ldloc, Stloc, Ldloca, Ldc_I4, Ldarg, Ldarga, and Starg opcodes all have shorter variants that take up less space in the IL instruction stream.  ILGenerator.Emit(OpCode, LocalBuilder) already special cases Ldloc, Stloc, and Ldloca to automatically translate those into their shorter forms where applicable, but similar logic doesn't exist in Emit(OpCode, int) for Ldc_I4, Ldarg, Ldarga, and Starg.  Instead, various other libraries higher in the stack that use reflection emit either end up doing all the special-casing with their own helper routines to do the shrinking, or they just forego it and end up with larger IL than is necessary.
    
    This PR just moves the logic down into Emit(OpCode, int) such that all uses can benefit, and removes the special-casing duplication from the other libraries.

--- src/libraries/Microsoft.CSharp/src/Microsoft/CSharp/RuntimeBinder/ComInterop/ComRuntimeHelpers.cs
+++ src/libraries/Microsoft.CSharp/src/Microsoft/CSharp/RuntimeBinder/ComInterop/ComRuntimeHelpers.cs
@@ -342,32 +342,7 @@ namespace Microsoft.CSharp.RuntimeBinder.ComInterop
         private static void EmitLoadArg(ILGenerator il, int index)
         {
             Requires.Condition(index >= 0, nameof(index));
-
-            switch (index)
-            {
-                case 0:
-                    il.Emit(OpCodes.Ldarg_0);
-                    break;
-                case 1:
-                    il.Emit(OpCodes.Ldarg_1);
-                    break;
-                case 2:
-                    il.Emit(OpCodes.Ldarg_2);
-                    break;
-                case 3:
-                    il.Emit(OpCodes.Ldarg_3);
-                    break;
-                default:
-                    if (index <= byte.MaxValue)
-                    {
-                        il.Emit(OpCodes.Ldarg_S, (byte)index);
-                    }
-                    else
-                    {
-                        il.Emit(OpCodes.Ldarg, index);
-                    }
-                    break;
-            }
+            il.Emit(OpCodes.Ldarg, index);
         }
 
         private static readonly object s_lock = new object();
