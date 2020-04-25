4564703bb3d3f349ccc55197f6294bdbf34a40a4
Aaron Robinson <arobins@microsoft.com>
2020-04-24 22:56:59 -0700

    Stop building hostmisc as a static lib. (#35431)
    * Stop building hostmisc as a static lib.
    
    This broke the nethost from being consumable as a static lib.

--- src/installer/corehost/cli/hostmisc/configure.cmake
+++ src/installer/corehost/cli/hostmisc/configure.cmake
@@ -2,4 +2,4 @@ include(CheckStructHasMember)
 
 check_struct_has_member("struct dirent" d_type dirent.h HAVE_DIRENT_D_TYPE)
 
-configure_file(${CMAKE_CURRENT_SOURCE_DIR}/config.h.in ${CMAKE_CURRENT_BINARY_DIR}/config.h)
+configure_file(${CMAKE_CURRENT_LIST_DIR}/config.h.in ${CMAKE_CURRENT_BINARY_DIR}/config.h)
