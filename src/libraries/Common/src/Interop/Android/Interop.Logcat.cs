24623f2a3f2d3b27eafeac5e37da0f63e628a22a
Egor Bogatov <egorbo@gmail.com>
2020-04-26 15:59:53 +0300

    Update Interop.Logcat.cs

--- src/libraries/Common/src/Interop/Android/Interop.Logcat.cs
+++ src/libraries/Common/src/Interop/Android/Interop.Logcat.cs
@@ -12,19 +12,20 @@ internal static partial class Interop
         [DllImport(Libraries.Liblog)]
         private static extern void __android_log_print(LogLevel level, string? tag, string format, string args, IntPtr ptr);
 
-        internal static void AndroidLogPrint(LogLevel level, string? tag, string message) => __android_log_print(level, tag, "%s", message, IntPtr.Zero);
-    }
+        internal static void AndroidLogPrint(LogLevel level, string? tag, string message) =>
+            __android_log_print(level, tag, "%s", message, IntPtr.Zero);
 
-    internal enum LogLevel
-    {
-        Unknown = 0x00,
-        Default = 0x01,
-        Verbose = 0x02,
-        Debug   = 0x03,
-        Info    = 0x04,
-        Warn    = 0x05,
-        Error   = 0x06,
-        Fatal   = 0x07,
-        Silent  = 0x08
+        internal enum LogLevel
+        {
+            Unknown = 0x00,
+            Default = 0x01,
+            Verbose = 0x02,
+            Debug   = 0x03,
+            Info    = 0x04,
+            Warn    = 0x05,
+            Error   = 0x06,
+            Fatal   = 0x07,
+            Silent  = 0x08
+        }
     }
 }
