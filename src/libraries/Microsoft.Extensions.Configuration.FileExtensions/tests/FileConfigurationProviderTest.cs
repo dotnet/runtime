fefbfb2156a466b33790d275b40de34a08ce4bca
Cédric Luthi <cedric.luthi@gmail.com>
2020-04-28 19:06:18 +0200

    Add test for InvalidDataException when load fails

--- src/libraries/Microsoft.Extensions.Configuration.FileExtensions/tests/FileConfigurationProviderTest.cs
+++ src/libraries/Microsoft.Extensions.Configuration.FileExtensions/tests/FileConfigurationProviderTest.cs
@@ -3,11 +3,9 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-using System.Collections.Generic;
 using System.IO;
 using Microsoft.Extensions.Configuration.Test;
 using Microsoft.Extensions.FileProviders;
-using Microsoft.Extensions.Primitives;
 using Moq;
 using Xunit;
 
@@ -35,14 +33,55 @@ namespace Microsoft.Extensions.Configuration.FileExtensions.Test
             Assert.Empty(changeToken.Callbacks);
         }
 
+        [Theory]
+        [InlineData(@"C:\path\to\configuration.txt")]
+        [InlineData(@"/path/to/configuration.txt")]
+        public void ProviderThrowsInvalidDataExceptionWhenLoadFails(string physicalPath)
+        {
+            var fileProviderMock = new Mock<IFileProvider>();
+            fileProviderMock.Setup(fp => fp.Watch(It.IsAny<string>())).Returns(new ConfigurationRootTest.ChangeToken());
+            fileProviderMock.Setup(fp => fp.GetFileInfo(It.IsAny<string>())).Returns(new FileInfoImpl(physicalPath));
+
+            var source = new FileConfigurationSourceImpl
+            {
+                FileProvider = fileProviderMock.Object,
+                ReloadOnChange = true,
+            };
+            var exceptionOnLoad = new Exception("An error occured");
+            var provider = new FileConfigurationProviderImpl(source, exceptionOnLoad);
+
+            var exception = Assert.Throws<InvalidDataException>(() => provider.Load());
+            Assert.Contains($"'{physicalPath}'", exception.Message);
+            Assert.Equal(exceptionOnLoad, exception.InnerException);
+        }
+
+        public class FileInfoImpl : IFileInfo
+        {
+            public FileInfoImpl(string physicalPath) => PhysicalPath = physicalPath;
+            public Stream CreateReadStream() => new MemoryStream();
+            public bool Exists => true;
+            public bool IsDirectory => false;
+            public DateTimeOffset LastModified => default;
+            public long Length => default;
+            public string Name => default;
+            public string PhysicalPath { get; }
+        }
+
         public class FileConfigurationProviderImpl : FileConfigurationProvider
         {
-            public FileConfigurationProviderImpl(FileConfigurationSource source)
+            private readonly Exception _exceptionOnLoad;
+
+            public FileConfigurationProviderImpl(FileConfigurationSource source, Exception exceptionOnLoad = null)
                 : base(source)
-            { }
+            {
+                _exceptionOnLoad = exceptionOnLoad;
+            }
 
             public override void Load(Stream stream)
-            { }
+            {
+                if (_exceptionOnLoad != null)
+                    throw _exceptionOnLoad;
+            }
         }
 
         public class FileConfigurationSourceImpl : FileConfigurationSource
