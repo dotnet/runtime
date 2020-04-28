66be61fa7460bee8d07003dcd04c00103d7c43d3
Adeel <adeelbm@outlook.com>
2020-04-28 16:34:33 +0300

    Provide dummy runtime_version.h when skipped With `-skipgenerateversion`, all MSBuild invocations are skipped. In that case, we generate a dummy `version.c` file. Since `runtime_version.h` is added as a required header, generate its dummy variant in the same context.

--- eng/native/build-commons.sh
+++ eng/native/build-commons.sh
@@ -82,6 +82,7 @@ build_native()
         buildTool="make"
     fi
 
+    runtimeVersionHeaderFile="$intermediatesDir/../runtime_version.h"
     if [[ "$__SkipConfigure" == 0 ]]; then
         # if msbuild is not supported, then set __SkipGenerateVersion to 1
         if [[ "$__IsMSBuildOnNETCoreSupported" == 0 ]]; then __SkipGenerateVersion=1; fi
@@ -95,7 +96,7 @@ build_native()
         if [[ "$__SkipGenerateVersion" == 0 ]]; then
             "$__RepoRootDir/eng/common/msbuild.sh" /clp:nosummary "$__ArcadeScriptArgs" "$__RepoRootDir"/eng/empty.csproj \
                                                    /p:NativeVersionFile="$__versionSourceFile" \
-                                                   /p:RuntimeVersionFile="$intermediatesDir/../runtime_version.h" \
+                                                   /p:RuntimeVersionFile="$runtimeVersionHeaderFile" \
                                                    /t:GenerateRuntimeVersionFile /restore \
                                                    $__CommonMSBuildArgs $__binlogArg $__UnprocessedBuildArgs
             local exit_code="$?"
@@ -104,12 +105,24 @@ build_native()
                 exit "$exit_code"
             fi
         else
-            # Generate the dummy version.c, but only if it didn't exist to make sure we don't trigger unnecessary rebuild
+            # Generate the dummy version.c and runtime_version.h, but only if they didn't exist to make sure we don't trigger unnecessary rebuild
             __versionSourceLine="static char sccsid[] __attribute__((used)) = \"@(#)No version information produced\";"
             if [[ -e "$__versionSourceFile" ]]; then
                 read existingVersionSourceLine < "$__versionSourceFile"
             fi
             if [[ "$__versionSourceLine" != "$existingVersionSourceLine" ]]; then
+                cat << EOF > $runtimeVersionHeaderFile
+#define RuntimeAssemblyMajorVersion 0
+#define RuntimeAssemblyMinorVersion 0
+#define RuntimeFileMajorVersion 0
+#define RuntimeFileMinorVersion 0
+#define RuntimeFileBuildVersion 0
+#define RuntimeFileRevisionVersion 0
+#define RuntimeProductMajorVersion 0
+#define RuntimeProductMinorVersion 0
+#define RuntimeProductPatchVersion 0
+#define RuntimeProductVersion
+EOF
                 echo "$__versionSourceLine" > "$__versionSourceFile"
             fi
         fi
