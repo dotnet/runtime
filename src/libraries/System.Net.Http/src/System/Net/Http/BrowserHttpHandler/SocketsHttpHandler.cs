ced64372fa599279c3d12c6ebb8ba8c4ba1825fe
Kenneth Pouncey <kjpou@pt.lu>
2020-04-29 10:26:36 +0200

    Replace SocketsHttpHandler build for Browser.
    - Throws `PlatformNotSupportedException` for properties and methods of the HttpMessageHandler abstract implementation.

--- src/libraries/System.Net.Http/src/System/Net/Http/BrowserHttpHandler/SocketsHttpHandler.cs
+++ src/libraries/System.Net.Http/src/System/Net/Http/BrowserHttpHandler/SocketsHttpHandler.cs
@@ -12,26 +12,6 @@ namespace System.Net.Http
 {
     public sealed class SocketsHttpHandler : HttpMessageHandler
     {
-        private HttpMessageHandler? _handler;
-        private bool _disposed;
-
-        private void CheckDisposed()
-        {
-            if (_disposed)
-            {
-                throw new ObjectDisposedException(nameof(SocketsHttpHandler));
-            }
-        }
-
-        private void CheckDisposedOrStarted()
-        {
-            CheckDisposed();
-            if (_handler != null)
-            {
-                throw new InvalidOperationException(SR.net_http_operation_started);
-            }
-        }
-
         public bool UseCookies
         {
             get => throw new PlatformNotSupportedException("Property UseCookies is not supported.");
@@ -150,89 +130,10 @@ namespace System.Net.Http
 
         public IDictionary<string, object?> Properties => throw new PlatformNotSupportedException("Property Properties is not supported.");
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && !_disposed)
-            {
-                _disposed = true;
-                _handler?.Dispose();
-                _handler = null;
-            }
-
-            base.Dispose(disposing);
-        }
-
-        private HttpMessageHandler SetupHandlerChain()
-        {
-
-            HttpMessageHandler handler = new BrowserHttpMessageHandler();
-
-            // Ensure a single handler is used for all requests.
-            if (Interlocked.CompareExchange(ref _handler, handler, null) != null)
-            {
-                handler.Dispose();
-            }
-
-            return _handler;
-        }
-
         protected internal override Task<HttpResponseMessage> SendAsync(
             HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            CheckDisposed();
-            HttpMessageHandler handler = _handler ?? SetupHandlerChain();
-
-            Exception? error = ValidateAndNormalizeRequest(request);
-            if (error != null)
-            {
-                return Task.FromException<HttpResponseMessage>(error);
-            }
-
-            return handler.SendAsync(request, cancellationToken);
-        }
-
-        private Exception? ValidateAndNormalizeRequest(HttpRequestMessage request)
-        {
-            if (request.Version.Major == 0)
-            {
-                return new NotSupportedException(SR.net_http_unsupported_version);
-            }
-
-            // Add headers to define content transfer, if not present
-            if (request.HasHeaders && request.Headers.TransferEncodingChunked.GetValueOrDefault())
-            {
-                if (request.Content == null)
-                {
-                    return new HttpRequestException(SR.net_http_client_execution_error,
-                        new InvalidOperationException(SR.net_http_chunked_not_allowed_with_empty_content));
-                }
-
-                // Since the user explicitly set TransferEncodingChunked to true, we need to remove
-                // the Content-Length header if present, as sending both is invalid.
-                request.Content.Headers.ContentLength = null;
-            }
-            else if (request.Content != null && request.Content.Headers.ContentLength == null)
-            {
-                // We have content, but neither Transfer-Encoding nor Content-Length is set.
-                request.Headers.TransferEncodingChunked = true;
-            }
-
-            if (request.Version.Minor == 0 && request.Version.Major == 1 && request.HasHeaders)
-            {
-                // HTTP 1.0 does not support chunking
-                if (request.Headers.TransferEncodingChunked == true)
-                {
-                    return new NotSupportedException(SR.net_http_unsupported_chunking);
-                }
-
-                // HTTP 1.0 does not support Expect: 100-continue; just disable it.
-                if (request.Headers.ExpectContinue == true)
-                {
-                    request.Headers.ExpectContinue = false;
-                }
-            }
-
-            return null;
+            throw new PlatformNotSupportedException("Method SendAsync is not supported.");
         }
     }
 }
