5372b09ac1415f2e43300c06023f9e1fc66cc7da
John <johnkellyoxford@gmail.com>
2020-04-30 10:32:05 +0100

    Change helper method to only accept float/double

--- src/libraries/System.Private.CoreLib/src/System/Numerics/VectorMath.cs
+++ src/libraries/System.Private.CoreLib/src/System/Numerics/VectorMath.cs
@@ -31,21 +31,46 @@ namespace System.Numerics
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Vector128<T> ConditionalSelectBitwise<T>(Vector128<T> selector, Vector128<T> ifTrue, Vector128<T> ifFalse)
-            where T : struct
+        public static Vector128<float> ConditionalSelectBitwise(Vector128<float> selector, Vector128<float> ifTrue, Vector128<float> ifFalse)
         {
             Debug.Assert(Sse.IsSupported || AdvSimd.IsSupported);
 
             if (Sse.IsSupported)
             {
                 return Sse.Or(
-                            Sse.And(ifTrue.AsSingle(), selector.AsSingle()),
-                            Sse.AndNot(selector.AsSingle(), ifFalse.AsSingle())
-                        ).As<float, T>();
+                            Sse.And(ifTrue, selector),
+                            Sse.AndNot(selector, ifFalse)
+                        );
             }
             else if (AdvSimd.IsSupported)
             {
-                return AdvSimd.BitwiseSelect(selector.AsByte(), ifTrue.AsByte(), ifFalse.AsByte()).As<byte, T>();
+                return AdvSimd.BitwiseSelect(selector.AsByte(), ifTrue.AsByte(), ifFalse.AsByte()).As<byte, float>();
+            }
+
+            return default;
+        }
+
+        public static Vector128<double> ConditionalSelectBitwise(Vector128<double> selector, Vector128<double> ifTrue, Vector128<double> ifFalse)
+        {
+            Debug.Assert(Sse.IsSupported || AdvSimd.IsSupported);
+
+            if (Sse2.IsSupported)
+            {
+                return Sse2.Or(
+                            Sse2.And(ifTrue, selector),
+                            Sse2.AndNot(selector, ifFalse))
+                    );
+            }
+            else if (Sse.IsSupported)
+            {
+                return Sse.Or(
+                            Sse.And(ifTrue.AsSingle(), selector.AsSingle()),
+                            Sse.AndNot(selector.AsSingle(), ifFalse.AsSingle())
+                        ).As<float, double>();
+            }
+            else if (AdvSimd.IsSupported)
+            {
+                return AdvSimd.BitwiseSelect(selector.AsByte(), ifTrue.AsByte(), ifFalse.AsByte()).As<byte, double>();
             }
 
             return default;
