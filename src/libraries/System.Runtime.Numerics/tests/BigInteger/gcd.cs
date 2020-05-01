036c535a5eec298176ab8fc33b89c12d7e108998
sakno <roman.sakno@gmail.com>
2020-05-01 13:27:02 +0300

    Simple test for debugging

--- src/libraries/System.Runtime.Numerics/tests/BigInteger/gcd.cs
+++ src/libraries/System.Runtime.Numerics/tests/BigInteger/gcd.cs
@@ -71,10 +71,12 @@ namespace System.Numerics.Tests
         {
             var tempByteArray1 = new byte[] { 255, 255, 255, 255, 255, 255, 255, 255, 1 };
             var tempByteArray2 = new byte[] { 255, 255, 255, 255, 255, 255, 255, 255, 1 };
-            
+
+            Console.WriteLine("Correct GCD");
             var result = MyBigIntImp.GCD(new Collections.Generic.List<byte>(tempByteArray1), new Collections.Generic.List<byte>(tempByteArray2)).ToArray();
             Console.WriteLine(Print(result));
-            
+
+            Console.WriteLine("Incorrect GCD");
             result = BigInteger.GreatestCommonDivisor(new BigInteger(tempByteArray1), new BigInteger(tempByteArray2)).ToByteArray();
 
             Console.WriteLine(Print(result));
