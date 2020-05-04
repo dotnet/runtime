169ce1a39fe0aa5543a5aec3c0f17c38b35b290d
Sergey Andreenko <seandree@microsoft.com>
2020-05-04 02:07:06 -0700

    Disable for Win64 by default.
    There are 2 issues that prevent it from being enbaled by default. They are causing significant asm regressions.

--- src/coreclr/src/jit/jitconfigvalues.h
+++ src/coreclr/src/jit/jitconfigvalues.h
@@ -438,7 +438,7 @@ CONFIG_INTEGER(JitSaveFpLrWithCalleeSavedRegisters, W("JitSaveFpLrWithCalleeSave
 #endif // DEBUG
 
 #if !FEATURE_MULTIREG_RET
-CONFIG_INTEGER(JitDoOldStructRetyping, W("JitDoOldStructRetyping"), 0) // Allow Jit to retype structs as primitive types
+CONFIG_INTEGER(JitDoOldStructRetyping, W("JitDoOldStructRetyping"), 1) // Allow Jit to retype structs as primitive types
                                                                        // when possible.
 #else                                                                  // FEATURE_MULTIREG_RET
 CONFIG_INTEGER(JitDoOldStructRetyping, W("JitDoOldStructRetyping"), 1) // Allow Jit to retype structs as primitive types
