1b723c6a81200ba7d4f708c7377f2f986ac056f1
Vitek Karas <vitek.karas@microsoft.com>
2020-05-06 12:42:03 -0700

    Fix build and improve auto-formatting (dotnet/linker#1172)
    Fixes command line build on windows in the analyzer project (latest Arcade has conflicts when building net core and net desktop projects at the same time. since we don't need desktop I removed it)
    
    Fixes editorconfig to correctly specify LF as EOL character so that it works on Windows.
    
    Added running the lint (auto-formatter) in command line (lint.sh/lint.cmd). CI now runs this command in a `--dry-run` mode so using it before submitting a PR should guarantee correct formatting.
    
    Commit migrated from https://github.com/dotnet/linker/commit/c0f1fb57748bbebef039becfb56e24208be95e62

---
+++ src/tools/linker/lint.cmd
@@ -0,0 +1,2 @@
+@echo off
+powershell -ExecutionPolicy ByPass -NoProfile -command "Set-Location %~dp0; & """%~dp0eng\dotnet.ps1""" ""tool run dotnet-format --verbosity diagnostic -f . --exclude src/analyzer,src/tuner,external %*"""
