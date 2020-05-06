f6e5219b13f4bb9d6c1c9f80fd6ca6c594c6c7f8
Vladimir Sadov <vsadov@microsoft.com>
2020-05-05 18:05:50 -0700

    Build an apphost with hostfxr and hostpolicy linked in (#35368)
    * hostfxr: Build most of hostfxr as a static library
    
    This is part of the work to create an apphost that bundles both hostfxr
    and hostpolicy.  The main distinction between the static and shared
    versions of hostfxr is that the static version contains a hostpolicy
    resolver that references the hostpolicy symbols directly rather than
    loading them from a DLL.
    
    * hostpolicy: Build as a static library
    
    This change is part of the work to enable an apphost that bundles both
    hostfxr and hostpolicy.  There's no distinction between hostpolicy
    that's built as a shared library and as a static library: the shared
    library is built by linking an empty object file with the static
    library.
    
    * corehost: Allow linking of hostfxr and hostpolicy with apphost
    
    Provide a hostfxr_iface class, that abstracts how the hostfxr functions called
    by the early stage in the hosting layer is resolved.
    
    * dotnet: Teach the muxer binary about hostfxr_iface
    
    * apphost: Teach apphost about hostfxr_iface
    
    This provides two implementations of hostfxr_iface: one for the static
    apphost, which bundles hostfxr and hostpolicy, and another for the
    conventional apphost, which loads them dynamically on startup.
    
    * Add exports for hostfxr and policy
    
    * Exports for unix
    
    * EXPORTS_LINKER_OPTION
    
    * use generateversionscript.awk from ENG
    
    * Move fxr files out of static
    
    * Fixes for Linux
    
    * Fix for   win-x86
    
    * move HEADERS next to SOURCES similarly to other files.
    
    * PR feedback (simplifying hostpolicy_resolver::try_get_dir for static host)
    
    * Publish static_apphost to Microsoft.NETCore.App.Host
    
    * bind to entry points without probing, when in a static host.
    
    * Add a test case
    
    * renamed hostfxr_iface --> hostfxr_resolver_t
    
    * renamed shared --> standalone
    
    * rename static_apphost --> singlefilehost
    
    * Signing exclusions for singlefilehost
    
    * switched StaticHost tst to a different asset (mostly a copy of StandaloneApp)
    
    Co-authored-by: Leandro Pereira <leandro.pereira@microsoft.com>
    Co-authored-by: Swaroop Sridhar <swaroop.sridhar@microsoft.com>

--- src/installer/corehost/cli/exe.cmake
+++ src/installer/corehost/cli/exe.cmake
@@ -18,6 +18,9 @@ list(APPEND SOURCES
     ${CMAKE_CURRENT_LIST_DIR}/fxr_resolver.cpp
     ${CMAKE_CURRENT_LIST_DIR}/../corehost.cpp
 )
+list(APPEND HEADERS
+    ${CMAKE_CURRENT_LIST_DIR}/../hostfxr_resolver_t.h
+)
 
 add_executable(${DOTNET_PROJECT_NAME} ${SOURCES} ${RESOURCES})
 
