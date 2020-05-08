4818f60dfc7deb01c0ea1e452623297943ef31ad
Kenneth Pouncey <kjpou@pt.lu>
2020-05-08 09:44:54 +0200

    Modify the underlying sockets field name.
    - address review comments.

--- src/libraries/System.Net.Http/src/System/Net/Http/HttpClientHandler.cs
+++ src/libraries/System.Net.Http/src/System/Net/Http/HttpClientHandler.cs
@@ -14,9 +14,9 @@ namespace System.Net.Http
     public partial class HttpClientHandler : HttpMessageHandler
     {
 #if TARGETS_BROWSER
-        private readonly BrowserHttpHandler _socketsHttpHandler;
+        private readonly BrowserHttpHandler _underlyingHandler;
 #else
-        private readonly SocketsHttpHandler _socketsHttpHandler;
+        private readonly SocketsHttpHandler _underlyingHandler;
 #endif
         private readonly DiagnosticsHandler _diagnosticsHandler;
         private ClientCertificateOption _clientCertificateOptions;
@@ -24,11 +24,11 @@ namespace System.Net.Http
         public HttpClientHandler()
         {
 #if TARGETS_BROWSER
-            _socketsHttpHandler = new BrowserHttpHandler();
+            _underlyingHandler = new BrowserHttpHandler();
 #else
-            _socketsHttpHandler = new SocketsHttpHandler();
+            _underlyingHandler = new SocketsHttpHandler();
 #endif
-            _diagnosticsHandler = new DiagnosticsHandler(_socketsHttpHandler);
+            _diagnosticsHandler = new DiagnosticsHandler(_underlyingHandler);
             ClientCertificateOptions = ClientCertificateOption.Manual;
         }
 
@@ -37,7 +37,7 @@ namespace System.Net.Http
             if (disposing && !_disposed)
             {
                 _disposed = true;
-                _socketsHttpHandler.Dispose();
+                _underlyingHandler.Dispose();
             }
 
             base.Dispose(disposing);
@@ -49,13 +49,13 @@ namespace System.Net.Http
 
         public bool UseCookies
         {
-            get => _socketsHttpHandler.UseCookies;
-            set => _socketsHttpHandler.UseCookies = value;
+            get => _underlyingHandler.UseCookies;
+            set => _underlyingHandler.UseCookies = value;
         }
 
         public CookieContainer CookieContainer
         {
-            get => _socketsHttpHandler.CookieContainer;
+            get => _underlyingHandler.CookieContainer;
             set
             {
                 if (value == null)
@@ -63,57 +63,57 @@ namespace System.Net.Http
                     throw new ArgumentNullException(nameof(value));
                 }
 
-                _socketsHttpHandler.CookieContainer = value;
+                _underlyingHandler.CookieContainer = value;
             }
         }
 
         public DecompressionMethods AutomaticDecompression
         {
-            get => _socketsHttpHandler.AutomaticDecompression;
-            set => _socketsHttpHandler.AutomaticDecompression = value;
+            get => _underlyingHandler.AutomaticDecompression;
+            set => _underlyingHandler.AutomaticDecompression = value;
         }
 
         public bool UseProxy
         {
-            get => _socketsHttpHandler.UseProxy;
-            set => _socketsHttpHandler.UseProxy = value;
+            get => _underlyingHandler.UseProxy;
+            set => _underlyingHandler.UseProxy = value;
         }
 
         public IWebProxy? Proxy
         {
-            get => _socketsHttpHandler.Proxy;
-            set => _socketsHttpHandler.Proxy = value;
+            get => _underlyingHandler.Proxy;
+            set => _underlyingHandler.Proxy = value;
         }
 
         public ICredentials? DefaultProxyCredentials
         {
-            get => _socketsHttpHandler.DefaultProxyCredentials;
-            set => _socketsHttpHandler.DefaultProxyCredentials = value;
+            get => _underlyingHandler.DefaultProxyCredentials;
+            set => _underlyingHandler.DefaultProxyCredentials = value;
         }
 
         public bool PreAuthenticate
         {
-            get => _socketsHttpHandler.PreAuthenticate;
-            set => _socketsHttpHandler.PreAuthenticate = value;
+            get => _underlyingHandler.PreAuthenticate;
+            set => _underlyingHandler.PreAuthenticate = value;
         }
 
         public bool UseDefaultCredentials
         {
             // SocketsHttpHandler doesn't have a separate UseDefaultCredentials property.  There
             // is just a Credentials property.  So, we need to map the behavior.
-            get => _socketsHttpHandler.Credentials == CredentialCache.DefaultCredentials;
+            get => _underlyingHandler.Credentials == CredentialCache.DefaultCredentials;
             set
             {
                 if (value)
                 {
-                    _socketsHttpHandler.Credentials = CredentialCache.DefaultCredentials;
+                    _underlyingHandler.Credentials = CredentialCache.DefaultCredentials;
                 }
                 else
                 {
-                    if (_socketsHttpHandler.Credentials == CredentialCache.DefaultCredentials)
+                    if (_underlyingHandler.Credentials == CredentialCache.DefaultCredentials)
                     {
                         // Only clear out the Credentials property if it was a DefaultCredentials.
-                        _socketsHttpHandler.Credentials = null;
+                        _underlyingHandler.Credentials = null;
                     }
                 }
             }
@@ -121,32 +121,32 @@ namespace System.Net.Http
 
         public ICredentials? Credentials
         {
-            get => _socketsHttpHandler.Credentials;
-            set => _socketsHttpHandler.Credentials = value;
+            get => _underlyingHandler.Credentials;
+            set => _underlyingHandler.Credentials = value;
         }
 
         public bool AllowAutoRedirect
         {
-            get => _socketsHttpHandler.AllowAutoRedirect;
-            set => _socketsHttpHandler.AllowAutoRedirect = value;
+            get => _underlyingHandler.AllowAutoRedirect;
+            set => _underlyingHandler.AllowAutoRedirect = value;
         }
 
         public int MaxAutomaticRedirections
         {
-            get => _socketsHttpHandler.MaxAutomaticRedirections;
-            set => _socketsHttpHandler.MaxAutomaticRedirections = value;
+            get => _underlyingHandler.MaxAutomaticRedirections;
+            set => _underlyingHandler.MaxAutomaticRedirections = value;
         }
 
         public int MaxConnectionsPerServer
         {
-            get => _socketsHttpHandler.MaxConnectionsPerServer;
-            set => _socketsHttpHandler.MaxConnectionsPerServer = value;
+            get => _underlyingHandler.MaxConnectionsPerServer;
+            set => _underlyingHandler.MaxConnectionsPerServer = value;
         }
 
         public int MaxResponseHeadersLength
         {
-            get => _socketsHttpHandler.MaxResponseHeadersLength;
-            set => _socketsHttpHandler.MaxResponseHeadersLength = value;
+            get => _underlyingHandler.MaxResponseHeadersLength;
+            set => _underlyingHandler.MaxResponseHeadersLength = value;
         }
 
         public ClientCertificateOption ClientCertificateOptions
@@ -159,13 +159,13 @@ namespace System.Net.Http
                     case ClientCertificateOption.Manual:
                         ThrowForModifiedManagedSslOptionsIfStarted();
                         _clientCertificateOptions = value;
-                        _socketsHttpHandler.SslOptions.LocalCertificateSelectionCallback = (sender, targetHost, localCertificates, remoteCertificate, acceptableIssuers) => CertificateHelper.GetEligibleClientCertificate(ClientCertificates)!;
+                        _underlyingHandler.SslOptions.LocalCertificateSelectionCallback = (sender, targetHost, localCertificates, remoteCertificate, acceptableIssuers) => CertificateHelper.GetEligibleClientCertificate(ClientCertificates)!;
                         break;
 
                     case ClientCertificateOption.Automatic:
                         ThrowForModifiedManagedSslOptionsIfStarted();
                         _clientCertificateOptions = value;
-                        _socketsHttpHandler.SslOptions.LocalCertificateSelectionCallback = (sender, targetHost, localCertificates, remoteCertificate, acceptableIssuers) => CertificateHelper.GetEligibleClientCertificate()!;
+                        _underlyingHandler.SslOptions.LocalCertificateSelectionCallback = (sender, targetHost, localCertificates, remoteCertificate, acceptableIssuers) => CertificateHelper.GetEligibleClientCertificate()!;
                         break;
 
                     default:
@@ -183,18 +183,18 @@ namespace System.Net.Http
                     throw new InvalidOperationException(SR.Format(SR.net_http_invalid_enable_first, nameof(ClientCertificateOptions), nameof(ClientCertificateOption.Manual)));
                 }
 
-                return _socketsHttpHandler.SslOptions.ClientCertificates ??
-                    (_socketsHttpHandler.SslOptions.ClientCertificates = new X509CertificateCollection());
+                return _underlyingHandler.SslOptions.ClientCertificates ??
+                    (_underlyingHandler.SslOptions.ClientCertificates = new X509CertificateCollection());
             }
         }
 
         public Func<HttpRequestMessage, X509Certificate2?, X509Chain?, SslPolicyErrors, bool>? ServerCertificateCustomValidationCallback
         {
-            get => (_socketsHttpHandler.SslOptions.RemoteCertificateValidationCallback?.Target as ConnectHelper.CertificateCallbackMapper)?.FromHttpClientHandler;
+            get => (_underlyingHandler.SslOptions.RemoteCertificateValidationCallback?.Target as ConnectHelper.CertificateCallbackMapper)?.FromHttpClientHandler;
             set
             {
                 ThrowForModifiedManagedSslOptionsIfStarted();
-                _socketsHttpHandler.SslOptions.RemoteCertificateValidationCallback = value != null ?
+                _underlyingHandler.SslOptions.RemoteCertificateValidationCallback = value != null ?
                     new ConnectHelper.CertificateCallbackMapper(value).ForSocketsHttpHandler :
                     null;
             }
@@ -202,32 +202,32 @@ namespace System.Net.Http
 
         public bool CheckCertificateRevocationList
         {
-            get => _socketsHttpHandler.SslOptions.CertificateRevocationCheckMode == X509RevocationMode.Online;
+            get => _underlyingHandler.SslOptions.CertificateRevocationCheckMode == X509RevocationMode.Online;
             set
             {
                 ThrowForModifiedManagedSslOptionsIfStarted();
-                _socketsHttpHandler.SslOptions.CertificateRevocationCheckMode = value ? X509RevocationMode.Online : X509RevocationMode.NoCheck;
+                _underlyingHandler.SslOptions.CertificateRevocationCheckMode = value ? X509RevocationMode.Online : X509RevocationMode.NoCheck;
             }
         }
 
         public SslProtocols SslProtocols
         {
-            get => _socketsHttpHandler.SslOptions.EnabledSslProtocols;
+            get => _underlyingHandler.SslOptions.EnabledSslProtocols;
             set
             {
                 ThrowForModifiedManagedSslOptionsIfStarted();
-                _socketsHttpHandler.SslOptions.EnabledSslProtocols = value;
+                _underlyingHandler.SslOptions.EnabledSslProtocols = value;
             }
         }
 
-        public IDictionary<string, object?> Properties => _socketsHttpHandler.Properties;
+        public IDictionary<string, object?> Properties => _underlyingHandler.Properties;
 
         protected internal override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
             CancellationToken cancellationToken)
         {
             return DiagnosticsHandler.IsEnabled() ?
                 _diagnosticsHandler.SendAsync(request, cancellationToken) :
-                _socketsHttpHandler.SendAsync(request, cancellationToken);
+                _underlyingHandler.SendAsync(request, cancellationToken);
         }
 
         public static Func<HttpRequestMessage, X509Certificate2, X509Chain, SslPolicyErrors, bool> DangerousAcceptAnyServerCertificateValidator { get; } = delegate { return true; };
@@ -236,7 +236,7 @@ namespace System.Net.Http
         {
             // Hack to trigger an InvalidOperationException if a property that's stored on
             // SslOptions is changed, since SslOptions itself does not do any such checks.
-            _socketsHttpHandler.SslOptions = _socketsHttpHandler.SslOptions;
+            _underlyingHandler.SslOptions = _underlyingHandler.SslOptions;
         }
     }
 }
