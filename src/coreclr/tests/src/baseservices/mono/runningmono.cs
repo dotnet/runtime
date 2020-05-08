ea24d6927a77478f7d370aaf52f8c9551450033a
Nathan Ricci <naricc@microsoft.com>
2020-05-08 12:55:26 -0400

    Fixed spacing/formatting.

--- src/coreclr/tests/src/baseservices/mono/runningmono.cs
+++ src/coreclr/tests/src/baseservices/mono/runningmono.cs
@@ -5,21 +5,21 @@ using System;
 
 namespace TestRunningMono
 {
-   class Program
-   {
+    class Program
+    {
         public static int Main(string[] args)
         {
-            const int Pass = 100, Fail = 1;
-            bool isMono = typeof(object).Assembly.GetType("Mono.RuntimeStructs") != null;
-	
-	    if (isMono)
-            {
-		return Pass;
-	    }
-            else
-            {
-		return Fail;
-            }
+             const int Pass = 100, Fail = 1;
+             bool isMono = typeof(object).Assembly.GetType("Mono.RuntimeStructs") != null;
+
+             if(isMono)
+             {
+                 return Pass;
+             }
+             else
+             {
+                 return Fail;
+             }
         }
    }
 }
