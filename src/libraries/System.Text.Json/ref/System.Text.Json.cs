4aa943df8cd385f38320e4d31a79eba7e943794b
Nikiforov Alexey <nikiforovalekcey@gmail.com>
2020-05-13 03:13:16 +0300

    Add ctor to JsonSerializerOptions that takes serializer defaults (#36073)
    * add ctor to JsonSerializerOptions that takes serializer defaults
    
    * add out of range handling; enhance xml documentation
    
    * test serializer option construction via serializer defaults
    
    * adjust PredefinedSerializerOptions tests

--- src/libraries/System.Text.Json/ref/System.Text.Json.cs
+++ src/libraries/System.Text.Json/ref/System.Text.Json.cs
@@ -209,6 +209,7 @@ namespace System.Text.Json
     {
         public JsonSerializerOptions() { }
         public JsonSerializerOptions(System.Text.Json.JsonSerializerOptions options) { }
+        public JsonSerializerOptions(System.Text.Json.JsonSerializerDefaults defaults) { }
         public bool AllowTrailingCommas { get { throw null; } set { } }
         public System.Collections.Generic.IList<System.Text.Json.Serialization.JsonConverter> Converters { get { throw null; } }
         public int DefaultBufferSize { get { throw null; } set { } }
@@ -223,6 +224,11 @@ namespace System.Text.Json
         public System.Text.Json.Serialization.ReferenceHandling ReferenceHandling { get { throw null; } set { } }
         public bool WriteIndented { get { throw null; } set { } }
         public System.Text.Json.Serialization.JsonConverter GetConverter(System.Type typeToConvert) { throw null; }
+    }
+    public enum JsonSerializerDefaults
+    {
+        General = 0,
+        Web = 1,
     }
     public enum JsonTokenType : byte
     {
