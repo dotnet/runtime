a70d713d13e368f2a4b127c264ac179800860f09
Kunal Pathak <Kunal.Pathak@microsoft.com>
2020-05-14 11:20:37 -0700

    one missing formatting

--- src/coreclr/src/jit/hwintrinsic.cpp
+++ src/coreclr/src/jit/hwintrinsic.cpp
@@ -718,7 +718,8 @@ GenTree* Compiler::impHWIntrinsic(NamedIntrinsic        intrinsic,
     }
 
     // Immediately return if the category is other than scalar/special and this is not a supported base type.
-    if ((category != HW_Category_Special) && (category != HW_Category_Scalar) && !isSupportedBaseType(intrinsic, baseType))
+    if ((category != HW_Category_Special) && (category != HW_Category_Scalar) &&
+        !isSupportedBaseType(intrinsic, baseType))
     {
         return nullptr;
     }
