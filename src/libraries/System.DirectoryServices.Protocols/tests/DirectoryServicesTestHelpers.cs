c3325d13e608beaf8b2896af9f984b981448f961
Jose Perez Rodriguez <joperezr@microsoft.com>
2020-05-18 14:09:03 -0700

    Adding OSX support for System.DirectoryServices.Protocols

--- src/libraries/System.DirectoryServices.Protocols/tests/DirectoryServicesTestHelpers.cs
+++ src/libraries/System.DirectoryServices.Protocols/tests/DirectoryServicesTestHelpers.cs
@@ -16,6 +16,9 @@ namespace System.DirectoryServices.Protocols.Tests
         // Cache the check once we have performed it once
         private static bool? _isLibLdapInstalled = null;
 
+        /// <summary>
+        /// Returns true if able to PInvoke into Linux or OSX, false otherwise
+        /// </summary>
         public static bool IsLibLdapInstalled
         {
             get
@@ -24,26 +27,43 @@ namespace System.DirectoryServices.Protocols.Tests
                 {
                     try
                     {
-                        // Attempt PInvoking into libldap
-                        IntPtr handle = ber_alloc(1);
-                        ber_free(handle, 1);
+                        // Attempt PInvoking into libldap on Linux
+                        IntPtr handle = ber_alloc_Linux(1);
+                        ber_free_Linux(handle, 1);
                         _isLibLdapInstalled = true;
                     }
                     catch (Exception)
                     {
-                        _isLibLdapInstalled = false;
+                        try
+                        {
+                            // Attempt PInvoking into libldap on OSX
+                            IntPtr handle = ber_alloc_OSX(1);
+                            ber_free_OSX(handle, 1);
+                            _isLibLdapInstalled = true;
+                        }
+                        catch (Exception)
+                        {
+                            _isLibLdapInstalled = false;
+                        }
                     }
                 }
                 return _isLibLdapInstalled.Value;
             }
         }
 
-        internal const string OpenLdap = "libldap-2.4.so.2";
+        internal const string OpenLdapLinux = "libldap-2.4.so.2";
+        internal const string OpenLdapOSX = "libldap";
 
-        [DllImport(OpenLdap, EntryPoint = "ber_alloc_t", CharSet = CharSet.Ansi)]
-        internal static extern IntPtr ber_alloc(int option);
+        [DllImport(OpenLdapLinux, EntryPoint = "ber_alloc_t", CharSet = CharSet.Ansi)]
+        internal static extern IntPtr ber_alloc_Linux(int option);
 
-        [DllImport(OpenLdap, EntryPoint = "ber_free", CharSet = CharSet.Ansi)]
-        public static extern IntPtr ber_free([In] IntPtr berelement, int option);
+        [DllImport(OpenLdapLinux, EntryPoint = "ber_free", CharSet = CharSet.Ansi)]
+        public static extern IntPtr ber_free_Linux([In] IntPtr berelement, int option);
+
+        [DllImport(OpenLdapOSX, EntryPoint = "ber_alloc_t", CharSet = CharSet.Ansi)]
+        internal static extern IntPtr ber_alloc_OSX(int option);
+
+        [DllImport(OpenLdapOSX, EntryPoint = "ber_free", CharSet = CharSet.Ansi)]
+        public static extern IntPtr ber_free_OSX([In] IntPtr berelement, int option);
     }
 }
