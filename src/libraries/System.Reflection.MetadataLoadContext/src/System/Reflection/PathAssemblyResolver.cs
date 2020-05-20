2d6633281564c4215c2dc6cd51d29594d342b507
DevDrake <mikolaj.raciborski@gmail.com>
2020-05-20 15:00:43 +0200

    Fixed trailing whitespaces
    Fixed problem with trailing whitespaces

--- src/libraries/System.Reflection.MetadataLoadContext/src/System/Reflection/PathAssemblyResolver.cs
+++ src/libraries/System.Reflection.MetadataLoadContext/src/System/Reflection/PathAssemblyResolver.cs
@@ -60,7 +60,7 @@ namespace System.Reflection
             {
                 return null;
             }
-            
+
             Assembly? candidateWithSamePkt = null;
             Assembly? candidateIgnoringPkt = null;
             ReadOnlySpan<byte> pktFromName = assemblyName.GetPublicKeyToken();
