0ce3182bd3edf9e10408ac002db3a4ef7686a65c
Egor Chesakov <Egor.Chesakov@microsoft.com>
2020-05-19 18:50:45 -0700

    Add ShiftLeftLogicalSaturateUnsigned in AdvSimd.cs AdvSimd.PlatformNotSupported.cs

--- src/libraries/System.Private.CoreLib/src/System/Runtime/Intrinsics/Arm/AdvSimd.PlatformNotSupported.cs
+++ src/libraries/System.Private.CoreLib/src/System/Runtime/Intrinsics/Arm/AdvSimd.PlatformNotSupported.cs
@@ -7634,6 +7634,55 @@ namespace System.Runtime.Intrinsics.Arm
         public static Vector64<ulong> ShiftLeftLogicalSaturateScalar(Vector64<ulong> value, byte count) { throw new PlatformNotSupportedException(); }
 
         /// <summary>
+        /// uint16x4_t vqshlu_n_s16 (int16x4_t a, const int n)
+        ///   A32: VQSHLU.S16 Dd, Dm, #n
+        ///   A64: SQSHLU Vd.4H, Vn.4H, #n
+        /// </summary>
+        public static Vector64<ushort> ShiftLeftLogicalSaturateUnsigned(Vector64<short> value, byte count) { throw new PlatformNotSupportedException(); }
+
+        /// <summary>
+        /// uint32x2_t vqshlu_n_s32 (int32x2_t a, const int n)
+        ///   A32: VQSHLU.S32 Dd, Dm, #n
+        ///   A64: SQSHLU Vd.2S, Vn.2S, #n
+        /// </summary>
+        public static Vector64<uint> ShiftLeftLogicalSaturateUnsigned(Vector64<int> value, byte count) { throw new PlatformNotSupportedException(); }
+
+        /// <summary>
+        /// uint8x8_t vqshlu_n_s8 (int8x8_t a, const int n)
+        ///   A32: VQSHLU.S8 Dd, Dm, #n
+        ///   A64: SQSHLU Vd.8B, Vn.8B, #n
+        /// </summary>
+        public static Vector64<byte> ShiftLeftLogicalSaturateUnsigned(Vector64<sbyte> value, byte count) { throw new PlatformNotSupportedException(); }
+
+        /// <summary>
+        /// uint16x8_t vqshluq_n_s16 (int16x8_t a, const int n)
+        ///   A32: VQSHLU.S16 Qd, Qm, #n
+        ///   A64: SQSHLU Vd.8H, Vn.8H, #n
+        /// </summary>
+        public static Vector128<ushort> ShiftLeftLogicalSaturateUnsigned(Vector128<short> value, byte count) { throw new PlatformNotSupportedException(); }
+
+        /// <summary>
+        /// uint32x4_t vqshluq_n_s32 (int32x4_t a, const int n)
+        ///   A32: VQSHLU.S32 Qd, Qm, #n
+        ///   A64: SQSHLU Vd.4S, Vn.4S, #n
+        /// </summary>
+        public static Vector128<uint> ShiftLeftLogicalSaturateUnsigned(Vector128<int> value, byte count) { throw new PlatformNotSupportedException(); }
+
+        /// <summary>
+        /// uint64x2_t vqshluq_n_s64 (int64x2_t a, const int n)
+        ///   A32: VQSHLU.S64 Qd, Qm, #n
+        ///   A64: SQSHLU Vd.2D, Vn.2D, #n
+        /// </summary>
+        public static Vector128<ulong> ShiftLeftLogicalSaturateUnsigned(Vector128<long> value, byte count) { throw new PlatformNotSupportedException(); }
+
+        /// <summary>
+        /// uint8x16_t vqshluq_n_s8 (int8x16_t a, const int n)
+        ///   A32: VQSHLU.S8 Qd, Qm, #n
+        ///   A64: SQSHLU Vd.16B, Vn.16B, #n
+        /// </summary>
+        public static Vector128<byte> ShiftLeftLogicalSaturateUnsigned(Vector128<sbyte> value, byte count) { throw new PlatformNotSupportedException(); }
+
+        /// <summary>
         /// float64x1_t vsqrt_f64 (float64x1_t a)
         ///   A32: VSQRT.F64 Dd, Dm
         ///   A64: FSQRT Dd, Dn
