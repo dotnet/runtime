f95b2b2fa2d290a2a79e21677a1f326284845ecb
Zoltan Varga <vargaz@gmail.com>
2020-05-28 05:35:18 -0400

    [wasm] Test runner improvements. (#37097)
    * Fix the test runner script.
    
    * Add a ExtraFiles property to the wasm app builder.
    
    * Add helper make targets.
    
    * Bump sdk version to pick up wasm support.
    
    * Fix make targets.
    
    * Compile wasm runtime with --enable-minimal=threads.

--- eng/testing/WasmRunnerTemplate.sh
+++ eng/testing/WasmRunnerTemplate.sh
@@ -7,6 +7,6 @@ TARGET_ARCH=$2
 echo "Test: $1 Arch: $2"
 
 cd $EXECUTION_DIR
-v8 --expose_wasm runtime.js -- --enable-gc --run WasmTestRunner.dll $TEST_NAME
+v8 --expose_wasm runtime.js -- --enable-gc --run WasmTestRunner.dll $TEST_NAME.dll
 
 exit 0
