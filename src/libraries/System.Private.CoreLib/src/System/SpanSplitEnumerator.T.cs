549c41bdaec0f23b75c78519fb39edeb70cdb335
bbartels <benjamin@bartels.dev>
2020-05-28 09:20:10 +0100

    Renamed parameters/fields

--- src/libraries/System.Private.CoreLib/src/System/SpanSplitEnumerator.T.cs
+++ src/libraries/System.Private.CoreLib/src/System/SpanSplitEnumerator.T.cs
@@ -19,19 +19,19 @@ namespace System
 
         internal SpanSplitEnumerator(ReadOnlySpan<T> buffer, ReadOnlySpan<T> separator)
         {
-            _sequence = span;
-            _separators = separators;
+            _buffer = buffer;
+            _separatorSequence = separator;
             _separator = default!;
             _isSequence = true;
             (_index, _offset) = (0, 0);
-            _separatorLength = _separators.Length;
+            _separatorLength = _separatorSequence.Length;
         }
 
         internal SpanSplitEnumerator(ReadOnlySpan<T> buffer, T separator)
         {
-            _sequence = span;
+            _buffer = buffer;
             _separator = separator;
-            _separators = default;
+            _separatorSequence = default;
             _isSequence = false;
             (_index, _offset) = (0, 0);
             _separatorLength = 1;
@@ -40,10 +40,10 @@ namespace System
         public bool MoveNext()
         {
             _offset += _index;
-            if (_offset > _sequence.Length) { return false; }
-            var slice = _sequence.Slice(_offset);
+            if (_offset > _buffer.Length) { return false; }
+            var slice = _buffer.Slice(_offset);
 
-            var nextIdx = _isSequence ? slice.IndexOf(_separators) : slice.IndexOf(_separator);
+            var nextIdx = _isSequence ? slice.IndexOf(_separatorSequence) : slice.IndexOf(_separator);
             _index = (nextIdx != -1 ? nextIdx : slice.Length) + _separatorLength;
             return true;
         }
