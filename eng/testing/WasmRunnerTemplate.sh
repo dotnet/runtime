5f91e0395e4186cbe136230e818bffaf02c90c07
Alexander Köplinger <alex.koeplinger@outlook.com>
2020-05-29 22:16:54 +0200

    Bump to updated WebAssembly runner from xharness

--- eng/testing/WasmRunnerTemplate.sh
+++ eng/testing/WasmRunnerTemplate.sh
@@ -1,12 +1,10 @@
 set -ev
 
 EXECUTION_DIR=$(dirname $0)
-TEST_NAME=$1
-TARGET_ARCH=$2
 
-echo "Test: $1 Arch: $2"
+echo "Test: $1"
 
 cd $EXECUTION_DIR
-v8 --expose_wasm runtime.js -- --enable-gc --run WasmTestRunner.dll $TEST_NAME.dll
+v8 --expose_wasm runtime.js -- --enable-gc --run WasmTestRunner.dll $*
 
 exit 0
