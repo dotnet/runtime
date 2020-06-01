581dc193406082f76ae3c58511f724ab5d5c0d6d
Steve MacLean <Steve.MacLean@microsoft.com>
2020-06-01 16:06:25 -0400

    Libunwind 1.5rc2 again (#36988)
    * Add arm64 support for UNWIND_CONTEXT_IS_UCONTEXT_T==0
    
    * Reapply libunwind 1.5rc2
    
    * Fix Linux Alpine libunwind1.5rc2

--- src/coreclr/src/pal/src/libunwind/src/mips/Gget_proc_info.c
+++ src/coreclr/src/pal/src/libunwind/src/mips/Gget_proc_info.c
@@ -30,11 +30,14 @@ unw_get_proc_info (unw_cursor_t *cursor, unw_proc_info_t *pi)
   struct cursor *c = (struct cursor *) cursor;
   int ret;
 
-  /* We can only unwind using Dwarf into on MIPS: return failure code
-     if it's not present.  */
   ret = dwarf_make_proc_info (&c->dwarf);
-  if (ret < 0)
-    return ret;
+  if (ret < 0) {
+    /* Construct a dummy proc info if Dwarf failed */
+    memset (pi, 0, sizeof (*pi));
+    pi->start_ip = c->dwarf.ip;
+    pi->end_ip = c->dwarf.ip + 4;
+    return 0;
+  }
 
   *pi = c->dwarf.pi;
   return 0;
