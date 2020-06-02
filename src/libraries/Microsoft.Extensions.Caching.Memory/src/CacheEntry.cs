2e7505433ba49aa22a1e777723098a00e811ab48
lindexi <lindexi_gd@163.com>
2020-06-02 09:27:46 +0800

    Add logger to cache entry to catch exception

--- src/libraries/Microsoft.Extensions.Caching.Memory/src/CacheEntry.cs
+++ src/libraries/Microsoft.Extensions.Caching.Memory/src/CacheEntry.cs
@@ -18,6 +18,7 @@ namespace Microsoft.Extensions.Caching.Memory
         private IList<IDisposable> _expirationTokenRegistrations;
         private IList<PostEvictionCallbackRegistration> _postEvictionCallbacks;
         private bool _isExpired;
+        private readonly ILogger _logger;
 
         internal IList<IChangeToken> _expirationTokens;
         internal DateTimeOffset? _absoluteExpiration;
@@ -31,7 +32,8 @@ namespace Microsoft.Extensions.Caching.Memory
         internal CacheEntry(
             object key,
             Action<CacheEntry> notifyCacheEntryDisposed,
-            Action<CacheEntry> notifyCacheOfExpiration)
+            Action<CacheEntry> notifyCacheOfExpiration,
+            ILogger logger)
         {
             if (key == null)
             {
@@ -48,11 +50,17 @@ namespace Microsoft.Extensions.Caching.Memory
                 throw new ArgumentNullException(nameof(notifyCacheOfExpiration));
             }
 
+            if (logger == null)
+            {
+                throw new ArgumentNullException(nameof(logger));
+            }
+
             Key = key;
             _notifyCacheEntryDisposed = notifyCacheEntryDisposed;
             _notifyCacheOfExpiration = notifyCacheOfExpiration;
 
             _scope = CacheEntryHelper.EnterScope(this);
+            _logger = logger;
         }
 
         /// <summary>
@@ -317,10 +325,10 @@ namespace Microsoft.Extensions.Caching.Memory
                 {
                     registration.EvictionCallback?.Invoke(entry.Key, entry.Value, entry.EvictionReason, registration.State);
                 }
-                catch (Exception)
+                catch (Exception e)
                 {
                     // This will be invoked on a background thread, don't let it throw.
-                    // TODO: LOG
+                    entry._logger.LogInformation(e, "EvictionCallback invoked failed");
                 }
             }
         }
