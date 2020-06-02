85a8627c72fe11c6e98064df3a3fd20982aab718
Jan Jahoda <jajahoda@microsoft.com>
2020-06-02 14:55:17 +0200

    Extract SafeCredentialReference to new file

--- src/libraries/Common/src/Interop/Windows/SspiCli/SecuritySafeHandles.cs
+++ src/libraries/Common/src/Interop/Windows/SspiCli/SecuritySafeHandles.cs
@@ -321,60 +321,6 @@ namespace System.Net.Security
         }
     }
 
-    //
-    // This is a class holding a Credential handle reference, used for static handles cache
-    //
-    internal sealed class SafeCredentialReference : CriticalFinalizerObject, IDisposable
-    {
-        //
-        // Static cache will return the target handle if found the reference in the table.
-        //
-        internal SafeFreeCredentials Target;
-
-        internal static SafeCredentialReference? CreateReference(SafeFreeCredentials target)
-        {
-            if (target.IsInvalid || target.IsClosed)
-            {
-                return null;
-            }
-
-            SafeCredentialReference result = new SafeCredentialReference(target);
-
-            return result;
-        }
-        private SafeCredentialReference(SafeFreeCredentials target)
-        {
-            // Bumps up the refcount on Target to signify that target handle is statically cached so
-            // its dispose should be postponed
-            bool ignore = false;
-            target.DangerousAddRef(ref ignore);
-            Target = target;
-        }
-
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        public void Dispose(bool disposing)
-        {
-            SafeFreeCredentials target = Target;
-            target?.DangerousRelease();
-            Target = null!;
-        }
-
-        ~SafeCredentialReference()
-        {
-            Dispose(false);
-
-#if DEBUG
-            Debug.Fail("Finalizer should never be called. The SafeCredentialReference should be always disposed manually.");
-            throw NotImplemented.ByDesign;
-#endif
-        }
-    }
-
     internal sealed class SafeFreeCredential_SECURITY : SafeFreeCredentials
     {
         public SafeFreeCredential_SECURITY() : base() { }
