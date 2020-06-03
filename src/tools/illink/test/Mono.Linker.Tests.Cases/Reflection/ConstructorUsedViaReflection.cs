a176c49baeabef93cafb20e7a39945112021cdfa
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2020-06-03 21:46:44 +0200

    Scope down PublicConstructors to DefaultConstructor if possible (dotnet/linker#1236)
    Also recognize `Type.EmptyTypes` and `Array.Empty<Type>`.
    
    Commit migrated from https://github.com/dotnet/linker/commit/59fef202348052aa8122d4cff446c09860e1c600

--- src/tools/illink/test/Mono.Linker.Tests.Cases/Reflection/ConstructorUsedViaReflection.cs
+++ src/tools/illink/test/Mono.Linker.Tests.Cases/Reflection/ConstructorUsedViaReflection.cs
@@ -64,8 +64,8 @@ namespace Mono.Linker.Tests.Cases.Reflection
 		[UnrecognizedReflectionAccessPattern (
 			typeof (Type), nameof (Type.GetConstructor), new Type[] { typeof (Type[]) },
 			"The return value of method 'System.Type Mono.Linker.Tests.Cases.Reflection.ConstructorUsedViaReflection::FindType()' with dynamically accessed member kinds 'None' " +
-			"is passed into the implicit 'this' parameter of method 'System.Reflection.ConstructorInfo System.Type::GetConstructor(System.Type[])' which requires dynamically accessed member kinds 'PublicConstructors'. " +
-			"To fix this add DynamicallyAccessedMembersAttribute to it and specify at least these member kinds 'PublicConstructors'.")]
+			"is passed into the implicit 'this' parameter of method 'System.Reflection.ConstructorInfo System.Type::GetConstructor(System.Type[])' which requires dynamically accessed member kinds 'DefaultConstructor'. " +
+			"To fix this add DynamicallyAccessedMembersAttribute to it and specify at least these member kinds 'DefaultConstructor'.")]
 		[Kept]
 		static void TestDataFlowType ()
 		{
