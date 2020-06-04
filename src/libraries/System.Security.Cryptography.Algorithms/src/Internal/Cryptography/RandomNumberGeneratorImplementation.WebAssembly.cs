ce76756433168b5b53b55dfe182d84a2a6160d9c
Zoltan Varga <vargaz@gmail.com>
2020-06-04 05:13:23 -0400

    Implement RandomNumberGeneratorImplementation for wasm.
    Simply read from /dev/random which is supported under WebAssembly.

---
+++ src/libraries/System.Security.Cryptography.Algorithms/src/Internal/Cryptography/RandomNumberGeneratorImplementation.WebAssembly.cs
@@ -0,0 +1,26 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.IO;
+using System.Diagnostics;
+
+namespace System.Security.Cryptography
+{
+    internal partial class RandomNumberGeneratorImplementation
+    {
+        private static FileStream s_randomStream = new FileStream("/dev/random", FileMode.Open, FileAccess.Read);
+
+        private static unsafe void GetBytes(byte* pbBuffer, int count)
+        {
+            Debug.Assert(count > 0);
+
+            var span = new Span<byte>(pbBuffer, count);
+            int res = s_randomStream.Read(span);
+            if (res != count)
+            {
+                throw Interop.Crypto.CreateOpenSslCryptographicException();
+            }
+        }
+    }
+}
