8f63cd0f8092d5a092468453381a8696c9d611f2
Maxim Lipnin <v-maxlip@microsoft.com>
2020-06-05 02:17:13 +0300

    Add an msbuild property to pass a JavaScript engine in WASM test build (#37420)

--- eng/testing/WasmRunnerTemplate.sh
+++ eng/testing/WasmRunnerTemplate.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
 EXECUTION_DIR=$(dirname $0)
-
-echo "Test: $1"
+JAVASCRIPT_ENGINE=$1
 
 cd $EXECUTION_DIR
 
 XHARNESS_OUT="$EXECUTION_DIR/xharness-output"
 
-dotnet xharness wasm test --engine=v8 \
+dotnet xharness wasm test --engine=$JAVASCRIPT_ENGINE \
     --js-file=runtime.js \
     --output-directory=$XHARNESS_OUT \
-    -- --enable-gc --run WasmTestRunner.dll $*
+    -- --enable-gc --run WasmTestRunner.dll ${@:2}
 
 _exitCode=$?
 
