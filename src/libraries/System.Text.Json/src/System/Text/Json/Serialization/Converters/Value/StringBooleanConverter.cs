a8c11b9df1681bdeffdcf4192f427ee7265ba9b6
Pratik Bhattacharya <patrickb404@outlook.com>
2020-06-06 03:30:41 +0530

    Added Json string to bool (vice-versa) converter

---
+++ src/libraries/System.Text.Json/src/System/Text/Json/Serialization/Converters/Value/StringBooleanConverter.cs
@@ -0,0 +1,34 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Text.Json.Serialization.Converters
+{
+    internal sealed class StringBooleanConverter : JsonConverter<bool>
+    {
+        private readonly bool _convertBooleanToString;
+
+        public StringBooleanConverter(bool convertBooleanToString)
+        {
+            _convertBooleanToString = convertBooleanToString;
+        }
+
+        public override bool Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var incomingValue = reader.GetString();
+            if (incomingValue.ToLowerInvariant() == bool.TrueString.ToLowerInvariant())
+                return true;
+            if (incomingValue.ToLowerInvariant() == bool.FalseString.ToLowerInvariant())
+                return false;
+            throw new JsonException("The string value could not be converted to System.Boolean");
+        }
+
+        public override void Write(Utf8JsonWriter writer, bool value, JsonSerializerOptions options)
+        {
+            if (_convertBooleanToString)
+                writer.WriteStringValue(value.ToString());
+            else
+                writer.WriteBooleanValue(value);
+        }
+    }
+}
---
+++ src/libraries/System.Text.Json/src/System/Text/Json/Serialization/JsonStringBooleanConverter.cs
@@ -0,0 +1,50 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Reflection;
+using System.Text.Json.Serialization.Converters;
+
+namespace System.Text.Json.System.Text.Json.Serialization
+{
+    /// <summary>
+    /// Converter to convert boolean to and from strings.
+    /// </summary>
+    public sealed class JsonStringBooleanConverter : JsonConverterFactory
+    {
+        private readonly bool _convertBooleanToString;
+
+        /// <summary>
+        /// Constructor. Creates the <see cref="JsonStringEnumConverter"/>
+        /// </summary>
+        /// <param name="convertBooleanToString">Flag to allow boolean to be written into string</param>
+        public JsonStringBooleanConverter(bool convertBooleanToString)
+        {
+            _convertBooleanToString = convertBooleanToString;
+        }
+
+        /// <summary>
+        /// Constructor. Creates the <see cref="JsonStringEnumConverter"/> allowing boolean to be written into string
+        /// </summary>
+        public JsonStringBooleanConverter() : this(true) { }
+
+        /// <inheritdoc />
+        public override bool CanConvert(Type typeToConvert)
+        {
+            return typeToConvert == typeof(bool);
+        }
+
+        /// <inheritdoc />
+        public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+        {
+            JsonConverter converter = (JsonConverter)Activator.CreateInstance(
+                typeof(StringBooleanConverter),
+                BindingFlags.Instance | BindingFlags.Public,
+                binder: null,
+                new object[] { _convertBooleanToString },
+                culture: null)!;
+
+            return converter;
+        }
+    }
+}
