0ea9a2f25a4a7c8c5305f4c790e810b80ffa2be4
Jan Jahoda <jajahoda@microsoft.com>
2020-06-05 18:27:33 +0200

    Fix nullable check

--- src/libraries/Common/src/System/Net/Security/SSPIHandleCache.cs
+++ src/libraries/Common/src/System/Net/Security/SSPIHandleCache.cs
@@ -14,7 +14,7 @@ namespace System.Net.Security
     internal static class SSPIHandleCache
     {
         private const int c_MaxCacheSize = 0x1F;  // must a (power of 2) - 1
-        private static readonly SafeCredentialReference[] s_cacheSlots = new SafeCredentialReference[c_MaxCacheSize + 1];
+        private static readonly SafeCredentialReference?[] s_cacheSlots = new SafeCredentialReference[c_MaxCacheSize + 1];
         private static int s_current = -1;
 
         internal static void CacheCredential(SafeFreeCredentials newHandle)
@@ -31,10 +31,10 @@ namespace System.Net.Security
                 unchecked
                 {
                     int index = Interlocked.Increment(ref s_current) & c_MaxCacheSize;
-                    newRef = Interlocked.Exchange<SafeCredentialReference>(ref s_cacheSlots[index], newRef);
+                    newRef = Interlocked.Exchange(ref s_cacheSlots[index], newRef);
                 }
 
-                newRef.Dispose();
+                newRef?.Dispose();
             }
             catch (Exception e)
             {
