efe9663636e977997f2a2c8be299d103d3954787
Tarek Mahmoud Sayed <tarekms@microsoft.com>
2020-06-08 19:21:01 -0700

    Support the Encoding enumeration in the Encoding Providers (#37528)
    * Support the Encoding enumeration in the Encoding Providers
    
    * Fix build
    
    * Fix netstandard build
    
    * Address the feedback
    
    * More Feedback
    
    * More Feedback

--- src/libraries/System.Private.CoreLib/src/System/Text/Encoding.cs
+++ src/libraries/System.Private.CoreLib/src/System/Text/Encoding.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Diagnostics;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Runtime.InteropServices;
@@ -290,8 +291,15 @@ namespace System.Text
                 GetEncoding(EncodingTable.GetCodePageFromName(name), encoderFallback, decoderFallback);
         }
 
-        // Return a list of all EncodingInfo objects describing all of our encodings
-        public static EncodingInfo[] GetEncodings() => EncodingTable.GetEncodings();
+        /// <summary>
+        /// Get the <see cref="EncodingInfo"/> list from the runtime and all registered encoding providers
+        /// </summary>
+        /// <returns>The list of the <see cref="EncodingProvider"/> objects</returns>
+        public static EncodingInfo[] GetEncodings()
+        {
+            Dictionary<int, EncodingInfo>? result = EncodingProvider.GetEncodingListFromProviders();
+            return result == null ? EncodingTable.GetEncodings() : EncodingTable.GetEncodings(result);
+        }
 
         public virtual byte[] GetPreamble() => Array.Empty<byte>();
 
