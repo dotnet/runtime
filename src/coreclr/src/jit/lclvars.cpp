ee17aa6e6c8b9b706f86645c2681b90a3c86c02e
Eugene Rozenfeld <erozen@microsoft.com>
2020-06-09 13:17:01 -0700

    Track promoted fields when struct local is no longer referenced. (#37280)
    This is a follow-up to #36918. It addresses one of the examples in #1007
    where we remove a struct zero initialization but fail to clean up a dead
    field assignment.
    
    The change is not to mark a dependently promoted field as untracked
    if we know that the struct local is no longer referenced.
    
    I also addressed a couple of late cosmetic review comments from #36918.
    
    No diffs in framework and benchmarks.

--- src/coreclr/src/jit/lclvars.cpp
+++ src/coreclr/src/jit/lclvars.cpp
@@ -3396,12 +3396,18 @@ void Compiler::lvaSortByRefCount()
                 lvaSetVarDoNotEnregister(lclNum DEBUGARG(DNER_IsStruct));
             }
         }
-        else if (varDsc->lvIsStructField && (lvaGetParentPromotionType(lclNum) != PROMOTION_TYPE_INDEPENDENT))
+        else if (varDsc->lvIsStructField && (lvaGetParentPromotionType(lclNum) != PROMOTION_TYPE_INDEPENDENT) &&
+                 (lvaGetDesc(varDsc->lvParentLcl)->lvRefCnt() > 1))
         {
             // SSA must exclude struct fields that are not independently promoted
             // as dependent fields could be assigned using a CopyBlock
             // resulting in a single node causing multiple SSA definitions
             // which isn't currently supported by SSA
+            //
+            // If the parent struct local ref count is less than 2, then either the struct is no longer
+            // referenced or the field is no longer referenced: we increment the struct local ref count in incRefCnts
+            // for each field use when the struct is dependently promoted. This can happen, e.g, if we've removed
+            // a block initialization for the struct. In that case we can still track the local field.
             //
             // TODO-CQ:  Consider using lvLclBlockOpAddr and only marking these LclVars
             // untracked when a blockOp is used to assign the struct.
