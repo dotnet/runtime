b8e1a49266199ccfaeb9cce8c2f5952048a4e3b0
John Kelly <johnkellyoxford@gmail.com>
2020-06-09 17:08:21 +0100

    Optimise MathF.CopySign and Math.CopySign using SSE intrinsics (#35456)
    * Optimise MathF.CopySign and Math.CopySign
    
    * Fix namespace error
    
    * Clear up constant
    
    * Remove x64 dependency for MathF method
    
    * Remove unnecessary constant and unsafe from Math
    
    * Remove unnecessary constant and unsafe from MathF
    
    * Add ARM32/64 paths, and SSE41 path. Change integral constant to FP for perf and add integral constant for fallback
    
    * Move conditional select to seperate helper methods, remove incorrect use of blendv
    
    * Inline mask constants
    
    * Correctly handle ISA not supported
    
    * Remove old comment
    
    * Fix syntax error
    
    * Var -> explicit type
    
    * Move helper method to sep class
    
    As discussed w tanner
    
    * move helper method
    
    * Remove locals
    
    * Fix namespace
    
    * namespace
    
    * Inline locals
    
    * Change helper method to only accept float/double
    
    * fix formatting
    
    * fix sybntax

--- src/libraries/System.Private.CoreLib/src/System/Numerics/VectorMath.cs
+++ src/libraries/System.Private.CoreLib/src/System/Numerics/VectorMath.cs
@@ -5,6 +5,8 @@
 using System.Diagnostics;
 using System.Runtime.Intrinsics;
 using System.Runtime.Intrinsics.X86;
+using System.Runtime.Intrinsics.Arm;
+using System.Runtime.CompilerServices;
 
 namespace System.Numerics
 {
@@ -26,6 +28,52 @@ namespace System.Numerics
         {
             Debug.Assert(Sse.IsSupported);
             return Sse.MoveMask(Sse.CompareNotEqual(vector1, vector2)) != 0;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector128<float> ConditionalSelectBitwise(Vector128<float> selector, Vector128<float> ifTrue, Vector128<float> ifFalse)
+        {
+            Debug.Assert(Sse.IsSupported || AdvSimd.IsSupported);
+
+            if (Sse.IsSupported)
+            {
+                return Sse.Or(
+                            Sse.And(ifTrue, selector),
+                            Sse.AndNot(selector, ifFalse)
+                        );
+            }
+            else if (AdvSimd.IsSupported)
+            {
+                return AdvSimd.BitwiseSelect(selector.AsByte(), ifTrue.AsByte(), ifFalse.AsByte()).As<byte, float>();
+            }
+
+            return default;
+        }
+
+        public static Vector128<double> ConditionalSelectBitwise(Vector128<double> selector, Vector128<double> ifTrue, Vector128<double> ifFalse)
+        {
+            Debug.Assert(Sse.IsSupported || AdvSimd.IsSupported);
+
+            if (Sse2.IsSupported)
+            {
+                return Sse2.Or(
+                            Sse2.And(ifTrue, selector),
+                            Sse2.AndNot(selector, ifFalse)
+                        );
+            }
+            else if (Sse.IsSupported)
+            {
+                return Sse.Or(
+                            Sse.And(ifTrue.AsSingle(), selector.AsSingle()),
+                            Sse.AndNot(selector.AsSingle(), ifFalse.AsSingle())
+                        ).As<float, double>();
+            }
+            else if (AdvSimd.IsSupported)
+            {
+                return AdvSimd.BitwiseSelect(selector.AsByte(), ifTrue.AsByte(), ifFalse.AsByte()).As<byte, double>();
+            }
+
+            return default;
         }
     }
 }
