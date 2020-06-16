51827185f6bedd5781af8a0d87cd83eb5460eb4c
Jose Perez Rodriguez <joperezr@microsoft.com>
2020-06-16 15:00:46 -0700

    Adding linker correctness tests to test library annotations for the linker (#37618)

---
+++ src/libraries/System.Runtime/tests/TrimmingTests/GenericArraySortHelperTest.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        var x = new[] { 5, 4, 3, 100, 2, 1 };
+        var y = new[] { "a", "b", "c", "h", "d", "e" };
+        // This will test that GenericArraySortHelper'2 called by reflection will be kept.
+        Array.Sort(x, y);
+        // This will test that GenericArraySortHelper'1 called by reflection will be kept.
+        Array.Sort(y);
+        return 100;
+    }
+}
