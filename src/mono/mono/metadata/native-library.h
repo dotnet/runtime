12dca6e59696e53fc8505cacdb1d33c93451086d
Thays Grazia <thaystg@gmail.com>
2020-06-17 09:11:04 -0300

    Update src/mono/mono/metadata/native-library.h
    Co-authored-by: Ryan Lucia <ryan@luciaonline.net>

--- src/mono/mono/metadata/native-library.h
+++ src/mono/mono/metadata/native-library.h
@@ -41,7 +41,7 @@ typedef struct MonoQCallDef
 typedef struct MonoQCallFunc {
     intptr_t        flags; //legal values (0x01 - end of array mareker, 0x08 - qcall)
     void*           implementation;
-    const char*     method_name;
+    const char     *method_name;
 } MonoQCallFunc;
 
 #endif
