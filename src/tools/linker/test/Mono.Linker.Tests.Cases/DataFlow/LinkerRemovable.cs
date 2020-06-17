ec5dbfb7629e46a0f6ece381c19946151dc80359
Tlakollo <tlcejava@microsoft.com>
2020-06-17 14:45:39 -0700

    - Initial commit for inserting LinkerRemovableAttribute into linker - Added simple test case with DynamicallyAccessedMembers
    Commit migrated from https://github.com/dotnet/linker/commit/76c9fc7a1a8da8a74149d66b7cd7cad1b8eb2585

---
+++ src/tools/linker/test/Mono.Linker.Tests.Cases/DataFlow/LinkerRemovable.cs
@@ -0,0 +1,28 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
+using Mono.Linker.Tests.Cases.Expectations.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Mono.Linker.Tests.Cases.DataFlow
+{
+	[SetupLinkAttributesFile ("LinkerRemovable.xml")]
+	[IgnoreLinkAttributes (false)]
+	[KeptMember(".ctor()")]
+	class LinkerRemovable
+	{
+		public static void Main ()
+		{
+			var instance = new LinkerRemovable ();
+			instance._typeWithDefaultConstructor = null;
+		}
+		[Kept]
+		[DynamicallyAccessedMembers (DynamicallyAccessedMemberTypes.DefaultConstructor)]
+		Type _typeWithDefaultConstructor;
+	}
+}
