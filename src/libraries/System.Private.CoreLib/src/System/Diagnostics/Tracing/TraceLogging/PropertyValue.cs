a7f8504f23a2b71dafd3553ee9c17d2b5f69fbc7
Eric Erhardt <eric.erhardt@microsoft.com>
2020-06-18 17:48:03 -0500

    Fix Diagnostics.Tracing ILLinkTrim entries
    - Fix PropertyValue/ReferenceTypeHelper with a DynamicDependency
    - RuntimeEventSource doesn't need to be rooted. This class is used by IL and doesn't get trimmed.
    - EventPipe* and NativeRuntimeEventSource only need to be rooted in CoreCLR, since they aren't used in Mono. So they are moved to just CoreCLR's ILLinkTrim file.
    
    Contributes to #35199

--- src/libraries/System.Private.CoreLib/src/System/Diagnostics/Tracing/TraceLogging/PropertyValue.cs
+++ src/libraries/System.Private.CoreLib/src/System/Diagnostics/Tracing/TraceLogging/PropertyValue.cs
@@ -6,6 +6,7 @@
 using System;
 using System.Diagnostics;
 #endif
+using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
@@ -196,6 +197,7 @@ namespace System.Diagnostics.Tracing
         /// </summary>
         /// <param name="property"></param>
         /// <returns></returns>
+        [DynamicDependency("#ctor", typeof(ReferenceTypeHelper<>))]
         private static Func<PropertyValue, PropertyValue> GetReferenceTypePropertyGetter(PropertyInfo property)
         {
             var helper = (TypeHelper)Activator.CreateInstance(typeof(ReferenceTypeHelper<>).MakeGenericType(property.DeclaringType!))!;
