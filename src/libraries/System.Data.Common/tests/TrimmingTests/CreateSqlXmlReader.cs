41038a501ec78b8803643259ef595aec65572e9f
Layomi Akinrinade <laakinri@microsoft.com>
2020-06-18 08:08:44 -0700

    Address review feedback

--- src/libraries/System.Data.Common/tests/TrimmingTests/CreateSqlXmlReader.cs
+++ src/libraries/System.Data.Common/tests/TrimmingTests/CreateSqlXmlReader.cs
@@ -1,3 +1,7 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 using System.Data.SqlTypes;
 using System.IO;
 
@@ -8,6 +12,6 @@ class Program
         MemoryStream ms = new MemoryStream();
         var sqlXml = new SqlXml(ms);
         var xmlReader = sqlXml.CreateReader();
-        return 100;
+        return xmlReader != null ? 100 : -1;
     }
 }
