3052acc56d8c83111ff4698529de8b2c8eadcf02
Simon Nattress <nattress@gmail.com>
2020-06-22 20:34:44 -0700

    Port type system tests from CoreRT repo (#38128)
    Port the managed type system unit tests and wire them up to build.cmd -test so they run if the clr.tools subset is included in compilation.
    
    Port enough USG layout and `Array<T>` support so we can keep their respective tests running in this repo.

---
+++ src/coreclr/src/tools/aot/ILCompiler.TypeSystem.ReadyToRun.Tests/CoreTestAssembly/SyntheticVirtualOverride.cs
@@ -0,0 +1,33 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+
+namespace SyntheticVirtualOverride
+{
+    struct StructWithNoEqualsAndGetHashCode
+    {
+    }
+
+    class ClassWithInjectedEqualsAndGetHashCode
+    {
+    }
+
+    class ClassOverridingEqualsAndGetHashCode : ClassWithInjectedEqualsAndGetHashCode
+    {
+        public override bool Equals(object other)
+        {
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return 0;
+        }
+    }
+
+    class ClassNotOverridingEqualsAndGetHashCode : ClassWithInjectedEqualsAndGetHashCode
+    {
+    }
+}
