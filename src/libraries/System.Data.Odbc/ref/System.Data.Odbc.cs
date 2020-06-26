d190c6bd905179408c46709d1583c5e5f1e17464
Anipik <anirudhagnihotry098@gmail.com>
2020-06-26 16:32:40 -0700

    fix odbc and facades

--- src/libraries/System.Data.Odbc/ref/System.Data.Odbc.cs
+++ src/libraries/System.Data.Odbc/ref/System.Data.Odbc.cs
@@ -7,6 +7,19 @@
 
 namespace System.Data.Odbc
 {
+    [System.Runtime.CompilerServices.TypeForwardedFrom("System.Data, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089")]
+    public static class ODBC32
+    {
+        [System.Runtime.CompilerServices.TypeForwardedFrom("System.Data, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089")]
+        public enum RETCODE : int
+        {
+            SUCCESS = 0,
+            SUCCESS_WITH_INFO = 1,
+            ERROR = -1,
+            INVALID_HANDLE = -2,
+            NO_DATA = 100,
+        }
+    }
     public sealed partial class OdbcCommand : System.Data.Common.DbCommand, System.ICloneable
     {
         public OdbcCommand() { }
