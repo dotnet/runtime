b148675f39b9bafaaf318b78a32b1489e5f34463
Angelo Breuer <46497296+angelobreuer@users.noreply.github.com>
2020-06-29 01:12:49 +0200

    Fix inconsistency of the 'CanRead' property after disposing an HTTP content stream

---
+++ src/libraries/System.Net.Http/tests/FunctionalTests/RawConnectionStreamTest.cs
@@ -0,0 +1,42 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.IO;
+using System.Net.Test.Common;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace System.Net.Http.Functional.Tests
+{
+    public class RawConnectionStreamTest 
+    {
+        [Fact]
+        public async Task RawConnectionStream_Dispose()
+        {
+            await LoopbackServer.CreateClientAndServerAsync(async url =>
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
+                    HttpResponseMessage response = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+                    Stream responseStream = await response.Content.ReadAsStreamAsync();
+
+                    Assert.True(responseStream.CanRead);
+                    Assert.True(responseStream.CanWrite);
+
+                    responseStream.Dispose();
+
+                    Assert.False(responseStream.CanRead);
+                    Assert.False(responseStream.CanWrite);
+                }
+            },
+            async server =>
+            {
+                // --> used to return System.Net.Http.HttpConnection+RawConnectionStream
+                await server.HandleRequestAsync(HttpStatusCode.SwitchingProtocols); 
+            });
+        }
+    }
+}
