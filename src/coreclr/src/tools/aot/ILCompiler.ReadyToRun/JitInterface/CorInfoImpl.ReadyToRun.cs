534276c59f09f431a59ca88894b703d2a153280f
David Wrighton <davidwr@microsoft.com>
2020-06-29 13:41:53 -0700

    Add type layout stress mode to crossgen2/runtime - New command line parameter --verify-type-and-field-layout   - Intended for use diagnosing bugs in crossgen2 only, not for general   production use - New r2r precode fixups Verify_TypeLayout and Verify_FieldOffset   - These allows the compiler to specify that a particular type/field   layout must match with the runtime calculation.   - If there is a mismatch, the runtime will failfast, even on release   builds - Add use of new switch to crossgen2smoke test - Fix encoding bug in FieldFixupSignature. Check_FieldOffset and Verify_FieldOffset use unsigned offsets, not signed offsets

--- src/coreclr/src/tools/aot/ILCompiler.ReadyToRun/JitInterface/CorInfoImpl.ReadyToRun.cs
+++ src/coreclr/src/tools/aot/ILCompiler.ReadyToRun/JitInterface/CorInfoImpl.ReadyToRun.cs
@@ -994,6 +994,12 @@ namespace Internal.JitInterface
                             CorInfoHelpFunc.CORINFO_HELP_GETGENERICS_GCSTATIC_BASE :
                             CorInfoHelpFunc.CORINFO_HELP_GETGENERICS_NONGCSTATIC_BASE);
                     }
+
+                    if (_compilation.SymbolNodeFactory.VerifyTypeAndFieldLayout)
+                    {
+                        // ENCODE_CHECK_FIELD_OFFSET
+                        _methodCodeNode.Fixups.Add(_compilation.SymbolNodeFactory.CheckFieldOffset(field));
+                    }
                 }
                 else
                 {
@@ -1062,6 +1068,13 @@ namespace Internal.JitInterface
                     else
                     if (helperId != ReadyToRunHelperId.Invalid)
                     {
+                        if (_compilation.SymbolNodeFactory.VerifyTypeAndFieldLayout)
+                        if (_compilation.SymbolNodeFactory.VerifyTypeAndFieldLayout)
+                        {
+                            // ENCODE_CHECK_FIELD_OFFSET
+                            _methodCodeNode.Fixups.Add(_compilation.SymbolNodeFactory.CheckFieldOffset(field));
+                        }
+
                         pResult->fieldLookup = CreateConstLookupToSymbol(
                             _compilation.SymbolNodeFactory.CreateReadyToRunHelper(helperId, field.OwningType)
                             );
@@ -1916,7 +1929,7 @@ namespace Internal.JitInterface
             if (!type.IsValueType)
                 return false;
 
-            return !_compilation.IsLayoutFixedInCurrentVersionBubble(type);
+            return !_compilation.IsLayoutFixedInCurrentVersionBubble(type) || _compilation.SymbolNodeFactory.VerifyTypeAndFieldLayout;
         }
 
         private bool HasLayoutMetadata(TypeDesc type)
@@ -1972,10 +1985,20 @@ namespace Internal.JitInterface
             }
             else if (pMT.IsValueType)
             {
+                if (_compilation.SymbolNodeFactory.VerifyTypeAndFieldLayout)
+                {
+                    // ENCODE_CHECK_FIELD_OFFSET
+                    _methodCodeNode.Fixups.Add(_compilation.SymbolNodeFactory.CheckFieldOffset(field));
+                }
                 // ENCODE_NONE
             }
             else if (_compilation.IsInheritanceChainLayoutFixedInCurrentVersionBubble(pMT.BaseType))
             {
+                if (_compilation.SymbolNodeFactory.VerifyTypeAndFieldLayout)
+                {
+                    // ENCODE_CHECK_FIELD_OFFSET
+                    _methodCodeNode.Fixups.Add(_compilation.SymbolNodeFactory.CheckFieldOffset(field));
+                }
                 // ENCODE_NONE
             }
             else if (HasLayoutMetadata(pMT))
@@ -1992,6 +2015,12 @@ namespace Internal.JitInterface
             else
             {
                 PreventRecursiveFieldInlinesOutsideVersionBubble(field, callerMethod);
+
+                if (_compilation.SymbolNodeFactory.VerifyTypeAndFieldLayout)
+                {
+                    // ENCODE_CHECK_FIELD_OFFSET
+                    _methodCodeNode.Fixups.Add(_compilation.SymbolNodeFactory.CheckFieldOffset(field));
+                }
 
                 // ENCODE_FIELD_BASE_OFFSET
                 Debug.Assert(pResult->offset >= (uint)pMT.BaseType.InstanceByteCount.AsInt);
