03bd1ed830fadafd0864af078ac967e33d3bda19
Levi Broderick <levib@microsoft.com>
2020-06-30 10:00:00 -0700

    Disallow unrestricted polymorphic deserialization in DataSet
    Fixes CVE-2020-1147
    https://portal.msrc.microsoft.com/en-us/security-guidance/advisory/CVE-2020-1147
    See also https://go.microsoft.com/fwlink/?linkid=2132227.

--- src/libraries/System.Data.Common/src/System/Data/DataSet.cs
+++ src/libraries/System.Data.Common/src/System/Data/DataSet.cs
@@ -1960,9 +1960,11 @@ namespace System.Data
 
         internal XmlReadMode ReadXml(XmlReader reader, bool denyResolving)
         {
+            IDisposable restrictedScope = null;
             long logScopeId = DataCommonEventSource.Log.EnterScope("<ds.DataSet.ReadXml|INFO> {0}, denyResolving={1}", ObjectID, denyResolving);
             try
             {
+                restrictedScope = TypeLimiter.EnterRestrictedScope(this);
                 DataTable.DSRowDiffIdUsageSection rowDiffIdUsage = default;
                 try
                 {
@@ -2230,6 +2232,7 @@ namespace System.Data
             }
             finally
             {
+                restrictedScope?.Dispose();
                 DataCommonEventSource.Log.ExitScope(logScopeId);
             }
         }
@@ -2466,9 +2469,11 @@ namespace System.Data
 
         internal XmlReadMode ReadXml(XmlReader? reader, XmlReadMode mode, bool denyResolving)
         {
+            IDisposable restictedScope = null;
             long logScopeId = DataCommonEventSource.Log.EnterScope("<ds.DataSet.ReadXml|INFO> {0}, mode={1}, denyResolving={2}", ObjectID, mode, denyResolving);
             try
             {
+                restictedScope = TypeLimiter.EnterRestrictedScope(this);
                 XmlReadMode ret = mode;
 
                 if (reader == null)
@@ -2710,6 +2715,7 @@ namespace System.Data
             }
             finally
             {
+                restictedScope?.Dispose();
                 DataCommonEventSource.Log.ExitScope(logScopeId);
             }
         }
