a0b0d1acf55a67ee759e8586fc6770e07346cd8c
Maryam Ariyan <maryam.ariyan@microsoft.com>
2020-06-30 11:03:07 -0700

    avoid unnecessary allocation when message not needed

--- src/libraries/Microsoft.Extensions.Logging.EventSource/src/EventSourceLogger.cs
+++ src/libraries/Microsoft.Extensions.Logging.EventSource/src/EventSourceLogger.cs
@@ -57,11 +57,12 @@ namespace Microsoft.Extensions.Logging.EventSource
             {
                 return;
             }
-            string message = formatter(state, exception);
+            string message = null;
 
             // See if they want the formatted message
             if (_eventSource.IsEnabled(EventLevel.Critical, LoggingEventSource.Keywords.FormattedMessage))
             {
+                message = formatter(state, exception);
                 _eventSource.FormattedMessage(
                     logLevel,
                     _factoryID,
@@ -104,6 +105,7 @@ namespace Microsoft.Extensions.Logging.EventSource
                     exceptionJson = ToJson(exceptionInfoData);
                 }
                 IReadOnlyList<KeyValuePair<string, string>> arguments = GetProperties(state);
+                message ??= formatter(state, exception);
                 _eventSource.MessageJson(
                     logLevel,
                     _factoryID,
