953dca100b9200c49f66e8078cf9c5b730042fc1
Kunal Pathak <Kunal.Pathak@microsoft.com>
2020-07-01 14:10:27 -0700

    fix build break

--- src/coreclr/src/jit/codegenarmarch.cpp
+++ src/coreclr/src/jit/codegenarmarch.cpp
@@ -588,8 +588,8 @@ void CodeGen::genSetGSSecurityCookie(regNumber initReg, bool* pInitRegModified)
     else
     {
         instGen_Set_Reg_To_Imm(EA_PTR_DSP_RELOC, initReg, (ssize_t)compiler->gsGlobalSecurityCookieAddr,
-                               INS_FLAGS_DONT_CARE,
-                               (CORINFO_METHOD_HANDLE)GenTreeIntCon::MethodHandleType::SetGSCookie);
+                               INS_FLAGS_DONT_CARE DEBUGARG(
+                                   (CORINFO_METHOD_HANDLE)GenTreeIntCon::MethodHandleType::SetGSCookie));
         GetEmitter()->emitIns_R_R_I(INS_ldr, EA_PTRSIZE, initReg, initReg, 0);
         regSet.verifyRegUsed(initReg);
         GetEmitter()->emitIns_S_R(INS_str, EA_PTRSIZE, initReg, compiler->lvaGSSecurityCookie, 0);
