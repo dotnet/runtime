f0f6ff66f2aaa03099d6f6e6bd080a06143edcc9
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2020-07-03 20:14:38 +0100

    implement MoveMask over AdvSimd

--- src/libraries/System.Text.Encodings.Web/src/System/Text/Encodings/Web/AdvSimdHelper.cs
+++ src/libraries/System.Text.Encodings.Web/src/System/Text/Encodings/Web/AdvSimdHelper.cs
@@ -2,11 +2,11 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 #if NETCOREAPP
+using System.Runtime.Intrinsics;
 using System.Runtime.Intrinsics.Arm;
-#endif
-#if NETCOREAPP5_0
 using System.Runtime.Intrinsics.X86;
 #endif
 
@@ -14,6 +14,25 @@ namespace System.Text.Encodings.Web
 {
     internal static class AdvSimdHelper
     {
+#if NETCOREAPP
+        private static readonly Vector128<byte> s_bitMask128 = BitConverter.IsLittleEndian ?
+                                                Vector128.Create(0x80402010_08040201).AsByte() :
+                                                Vector128.Create(0x01020408_10204080).AsByte();
 
+        public static ushort MoveMask(Vector128<byte> value)
+        {
+            Debug.Assert(AdvSimd.Arm64.IsSupported);
+
+            Vector128<byte> mostSignficantBitMask = Vector128.Create((byte)0x80);
+            Vector128<byte> mostSignificantBitIsSet = AdvSimd.CompareEqual(AdvSimd.And(value, mostSignficantBitMask), mostSignficantBitMask);
+
+            // pairwise-add until all flags have moved to the first two bytes of the vector
+            Vector128<byte> extractedBits = AdvSimd.And(mostSignificantBitIsSet, s_bitMask128);
+            extractedBits = AdvSimd.Arm64.AddPairwise(extractedBits, extractedBits);
+            extractedBits = AdvSimd.Arm64.AddPairwise(extractedBits, extractedBits);
+            extractedBits = AdvSimd.Arm64.AddPairwise(extractedBits, extractedBits);
+            return extractedBits.AsUInt16().ToScalar();
+        }
+#endif
     }
 }
