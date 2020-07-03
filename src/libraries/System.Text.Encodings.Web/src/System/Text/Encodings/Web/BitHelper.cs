67c3eef1e9156d4879ddaf7ffeef956aaa17ff30
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2020-07-03 15:18:08 +0100

    shim intrinsics apis for netstandard & netcoreapp targets

--- src/libraries/System.Text.Encodings.Web/src/System/Text/Encodings/Web/BitHelper.cs
+++ src/libraries/System.Text.Encodings.Web/src/System/Text/Encodings/Web/BitHelper.cs
@@ -12,6 +12,10 @@ namespace System.Text.Encodings.Web
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int GetIndexOfFirstNeedToEscape(int index)
         {
+#if NETSTANDARD2_0 || NETSTANDARD2_1
+            Debug.Fail("Should not be called in this platform.");
+            throw new PlatformNotSupportedException();
+#else
             // Found at least one byte that needs to be escaped, figure out the index of
             // the first one found that needed to be escaped within the 16 bytes.
             Debug.Assert(index > 0 && index <= 65_535);
@@ -19,6 +23,7 @@ namespace System.Text.Encodings.Web
             Debug.Assert(tzc >= 0 && tzc < 16);
 
             return tzc;
+#endif
         }
     }
 }
