9de7b731ba1176520e4c33e65468cc008f8e3479
Anipik <anirudhagnihotry098@gmail.com>
2020-07-04 10:50:08 -0700

    addressing minor feedback and disabling tests

--- src/libraries/System.Data.Odbc/ref/System.Data.Odbc.ODBC32.cs
+++ src/libraries/System.Data.Odbc/ref/System.Data.Odbc.ODBC32.cs
@@ -4,7 +4,7 @@
 
 namespace System.Data.Odbc
 {
-    // We need this class in the implementation broswer assembly for building the system.data facade.
+    // We need this class in the implementation browser assembly for building the system.data facade.
     [System.Runtime.CompilerServices.TypeForwardedFrom("System.Data, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089")]
     public static class ODBC32
     {
