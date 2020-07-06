e091d78f607d96240ad4697bfb93258412815594
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2020-07-06 14:47:32 +0200

    Postpone marking fields on types with layout (dotnet/linker#1309)
    We can postpone marking fields on types with layout until we're really sure they're needed. They're needed if they're visible to reflection, or considered instantiated (valuetypes are implicitly considered instantiated).
    
    I'm also allowing stripping fields on Auto layout structs. This is a rare sight, but there's no reason that I know of to force them to have all their fields kept.
    
    Commit migrated from https://github.com/dotnet/linker/commit/da2cc0fcd6c3a8e8e5d1b5d4a655f3653baa8980

--- src/tools/linker/test/Mono.Linker.Tests.Cases/Attributes.StructLayout/ExplicitClass.cs
+++ src/tools/linker/test/Mono.Linker.Tests.Cases/Attributes.StructLayout/ExplicitClass.cs
@@ -19,12 +19,36 @@ namespace Mono.Linker.Tests.Cases.Attributes.StructLayout
 		public int never_ever_used;
 	}
 
+	[StructLayout (LayoutKind.Explicit)]
+	[Kept]
+	class UnallocatedExplicitClassData
+	{
+		[FieldOffset (0)]
+		public int never_used;
+	}
+
+	[StructLayout (LayoutKind.Explicit)]
+	[Kept]
+	class UnallocatedButReferencedWithReflectionExplicitClassData
+	{
+		[Kept]
+		[FieldOffset (0)]
+		public int never_used;
+	}
+
 	public class ExplicitClass
 	{
+		[Kept]
+		static UnallocatedExplicitClassData _myField;
+
 		public static void Main ()
 		{
 			var c = new ExplicitClassData ();
 			c.used = 1;
+
+			_myField = null;
+
+			typeof (UnallocatedButReferencedWithReflectionExplicitClassData).ToString ();
 		}
 	}
 }
