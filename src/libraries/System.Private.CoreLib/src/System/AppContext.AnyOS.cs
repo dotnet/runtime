f6ad87e6bd9504740a274ebb42d948753782ad92
Steve Pfister <steve.pfister@microsoft.com>
2020-07-06 16:02:56 -0400

    Feedback

--- src/libraries/System.Private.CoreLib/src/System/AppContext.AnyOS.cs
+++ src/libraries/System.Private.CoreLib/src/System/AppContext.AnyOS.cs
@@ -13,9 +13,14 @@ namespace System
         {
             // Fallback path for hosts that do not set APP_CONTEXT_BASE_DIRECTORY explicitly
             string? directory = Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location);
-            if (directory != null && !Path.EndsInDirectorySeparator(directory))
+
+            if (directory == null)
+                return string.Empty;
+            
+            if (!Path.EndsInDirectorySeparator(directory))
                 directory += PathInternal.DirectorySeparatorCharAsString;
-            return directory ?? string.Empty;
+
+            return directory;
         }
     }
 }
