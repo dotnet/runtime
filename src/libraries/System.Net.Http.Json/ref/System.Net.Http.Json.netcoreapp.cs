e7c9345fa695a9964ea53fa71ab93aabd4c0d1b5
Stephen Toub <stoub@microsoft.com>
2020-07-08 15:21:30 -0400

    Add missing license headers under libraries

--- src/libraries/System.Net.Http.Json/ref/System.Net.Http.Json.netcoreapp.cs
+++ src/libraries/System.Net.Http.Json/ref/System.Net.Http.Json.netcoreapp.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// ------------------------------------------------------------------------------
+// Changes to this file must follow the https://aka.ms/api-review process.
+// ------------------------------------------------------------------------------
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
--- src/tests/JIT/Directed/StructPromote/Unsafe/ReadStructAsAnotherType.cs
+++ src/tests/JIT/Directed/StructPromote/Unsafe/ReadStructAsAnotherType.cs
@@ -1,4 +1,4 @@
-// Licensed to the.NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 // The test came from https://github.com/dotnet/corefx/issues/20085.
