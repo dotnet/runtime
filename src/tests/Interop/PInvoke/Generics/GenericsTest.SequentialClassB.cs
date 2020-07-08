a7dcd1db9a849d4c21a08c246f516011b49bf6c4
Stephen Toub <stoub@microsoft.com>
2020-07-08 15:09:28 -0400

    Fix Doundation to Foundation in some license headers

--- src/tests/Interop/PInvoke/Generics/GenericsTest.SequentialClassB.cs
+++ src/tests/Interop/PInvoke/Generics/GenericsTest.SequentialClassB.cs
@@ -1,5 +1,5 @@
-// Licensed to the .NET Doundation under one or more agreements.
-// The .NET Doundation licenses this file to you under the MIT license.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
 using System.Runtime.InteropServices;
