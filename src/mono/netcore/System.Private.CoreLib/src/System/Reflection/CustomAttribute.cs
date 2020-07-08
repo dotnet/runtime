9bf00d32036105137a3e96309b1e235379ffb7d5
Fan Yang <52458914+fanyang-mono@users.noreply.github.com>
2020-07-08 11:26:36 -0400

    Fix inconsistent copyright for C# files in Mono's corelib (#38664)
    * Fix inconsistent copyright for C# files in Mono's corelib
    
    * Update src/mono/netcore/System.Private.CoreLib/src/Mono/RuntimeMarshal.cs
    
    Co-authored-by: Stephen Toub <stoub@microsoft.com>
    
    * Use 2-line version of copyright
    
    Co-authored-by: Stephen Toub <stoub@microsoft.com>

--- src/mono/netcore/System.Private.CoreLib/src/System/Reflection/CustomAttribute.cs
+++ src/mono/netcore/System.Private.CoreLib/src/System/Reflection/CustomAttribute.cs
@@ -1,3 +1,5 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
 //
 // (c) 2002,2003 Ximian, Inc. (http://www.ximian.com)
 // Copyright (C) 2004 Novell, Inc (http://www.novell.com)
--- src/mono/netcore/System.Private.CoreLib/src/System/TypeIdentifier.cs
+++ src/mono/netcore/System.Private.CoreLib/src/System/TypeIdentifier.cs
@@ -1,3 +1,5 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
 //
 // Copyright (C) 2015 Xamarin, Inc (http://www.xamarin.com)
 //
