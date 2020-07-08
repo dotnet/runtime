15f1909f117759eae3ce364c7326fac14ff40e6c
Stephen Toub <stoub@microsoft.com>
2020-07-08 12:21:39 -0400

    Fix license header in .py files

--- src/coreclr/src/scripts/genEventing.py
+++ src/coreclr/src/scripts/genEventing.py
@@ -1,7 +1,6 @@
 #
 ## Licensed to the .NET Foundation under one or more agreements.
 ## The .NET Foundation licenses this file to you under the MIT license.
-## See the LICENSE file in the project root for more information.
 #
 #
 #USAGE:
@@ -19,7 +18,6 @@ from utilities import open_for_update
 stdprolog="""
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
 
 /******************************************************************
 
