79d940053a5ed8e6529c510d93d2361f453888bf
Jan Kotas <jkotas@microsoft.com>
2020-07-09 07:33:57 -0700

    Skeleton of wasm test system.

---
+++ src/common/wasm-test-driver/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace wasm_test_driver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	    string[] benchmarkAssemblyNames = new string [] { "runningmono" };
+
+	    string runtimeJs = "";
+            string wasmTestRunnerAssembly = "";
+
+
+            ProcessStartInfo processStartInfo = new ProcessStartInfo("sh");
+	    processStartInfo.WorkingDirectory = "/Users/naricc/workspace/runtime-webassembly-ci/src/common/wasm-test-runner/bin/Release/publish/";
+	    processStartInfo.UseShellExecute = true;
+
+	    foreach (string assemblyName in benchmarkAssemblyNames)
+	    {
+		processStartInfo.Arguments = $"run-v8.sh {assemblyName}";
+		Process.Start(processStartInfo);
+	    }
+        }
+    }
+}
---
+++ src/common/wasm-test-runner/Program.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Reflection;
+using System.IO;
+
+public static class Program
+{
+    public static int Main(string[] args)
+    {
+	string assemblyName = args[0];
+
+	var assembly = Assembly.Load(assemblyName);
+	var mainMethod = assembly.EntryPoint;
+	mainMethod.Invoke (null, new object [] { args } );
+
+	return 0;
+    }
+}
