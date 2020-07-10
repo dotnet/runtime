d892de5c980b316132c365cb3c706a2ef042c52b
Alexander Köplinger <alex.koeplinger@outlook.com>
2020-07-10 18:25:48 +0200

    Disable a test that shouldn't be running on Mono interpreter

--- src/libraries/System.Text.Json/tests/Serialization/Stream.Collections.cs
+++ src/libraries/System.Text.Json/tests/Serialization/Stream.Collections.cs
@@ -18,6 +18,7 @@ namespace System.Text.Json.Serialization.Tests
     {
         [Fact]
         [ActiveIssue("https://github.com/dotnet/runtime/issues/35927", typeof(PlatformDetection), nameof(PlatformDetection.IsMonoInterpreter))]
+        [ActiveIssue("https://github.com/dotnet/runtime/issues/35927", TestPlatforms.Browser)]
         public static async Task HandleCollectionsAsync()
         {
             await RunTestAsync<string>();
