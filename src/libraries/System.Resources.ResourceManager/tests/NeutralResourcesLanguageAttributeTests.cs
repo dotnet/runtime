b0d9238b6c76321ba5e87d9bb3a30bedb6044de6
Tarek Mahmoud Sayed <tarekms@microsoft.com>
2020-07-10 18:32:23 -0700

    Add test

--- src/libraries/System.Resources.ResourceManager/tests/NeutralResourcesLanguageAttributeTests.cs
+++ src/libraries/System.Resources.ResourceManager/tests/NeutralResourcesLanguageAttributeTests.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Xunit;
+using System.Linq;
+using System.Reflection;
 
 namespace System.Resources.Tests
 {
@@ -22,6 +24,26 @@ namespace System.Resources.Tests
         public static void ConstructorArgumentNull()
         {
             Assert.Throws<ArgumentNullException>(() => new NeutralResourcesLanguageAttribute(null));
+        }
+
+        [Theory]
+        [InlineData(typeof(object))] // System.Private.CoreLib,
+        [InlineData(typeof(System.Collections.Stack))] // System.Collections.NonGeneric
+        [InlineData(typeof(System.Collections.Specialized.ListDictionary))] // System.Collections.Specialized
+        [InlineData(typeof(System.Collections.Immutable.IImmutableList<string>))] // System.Collections.Immutable
+        [InlineData(typeof(System.Collections.Concurrent.ConcurrentBag<string>))] // System.Collections.Concurrent
+        [InlineData(typeof(System.Console))] // System.Console
+        [InlineData(typeof(System.IO.Directory))] // System.IO.FileSystem
+        [InlineData(typeof(System.Linq.Enumerable))] // System.Linq
+        [InlineData(typeof(System.Net.Http.HttpClient))] // System.Net.Http
+        [InlineData(typeof(System.Text.RegularExpressions.Regex))] // System.Text.RegularExpressions
+        [InlineData(typeof(System.Threading.Barrier))] // System.Threading
+        public static void TestAttributeExistence(Type type)
+        {
+            CustomAttributeData attribute = type.Assembly.CustomAttributes.FirstOrDefault(a => a.AttributeType.Name == "NeutralResourcesLanguageAttribute");
+            Assert.NotNull(attribute);
+            Assert.True(attribute.ConstructorArguments.Count() > 0);
+            Assert.Equal("en-US", attribute.ConstructorArguments[0].Value);
         }
     }
 }
