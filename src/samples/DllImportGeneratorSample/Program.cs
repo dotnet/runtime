c2a28f680bafe9fe7782446a3e0bee4d4ac5cd52
Aaron Robinson <arobins@microsoft.com>
2020-07-12 22:09:29 -0700

    Add example that runs on Windows to the demo project.
    It is interesting to step into the generated method and observe the UX.
    
    
    Commit migrated from https://github.com/dotnet/runtimelab/commit/35c9adc2b099a51aff24992e15380a1f26a60d39

--- src/samples/DllImportGeneratorSample/Program.cs
+++ src/samples/DllImportGeneratorSample/Program.cs
@@ -5,10 +5,19 @@ using System.Runtime.InteropServices;
 
 namespace Demo
 {
+    partial class Kernel32
+    {
+        [GeneratedDllImport(nameof(Kernel32), EntryPoint = "QueryPerformanceCounter")]
+        public static partial int Method(ref long t);
+    }
+
     unsafe class Program
     {
         static void Main(string[] args)
         {
+            var ts = (long)0;
+            int suc = Kernel32.Method(ref ts);
+            Console.WriteLine($"{suc}: 0x{ts:x}");
         }
     }
 }
