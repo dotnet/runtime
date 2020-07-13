d0c6e85af7b9ccad41310a0b090ef133774bc54f
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2020-07-13 13:29:42 +0200

    Update SyntheticVirtualOverrideTests.cs

--- src/coreclr/src/tools/aot/ILCompiler.TypeSystem.ReadyToRun.Tests/SyntheticVirtualOverrideTests.cs
+++ src/coreclr/src/tools/aot/ILCompiler.TypeSystem.ReadyToRun.Tests/SyntheticVirtualOverrideTests.cs
@@ -12,7 +12,7 @@ using Xunit;
 
 namespace TypeSystemTests
 {
-    public class SyntheticVirtualOverrideTests
+    public partial class SyntheticVirtualOverrideTests
     {
         TestTypeSystemContext _context;
         ModuleDesc _testModule;
@@ -155,7 +155,7 @@ namespace TypeSystemTests
             }
         }
 
-        private class SyntheticMethod : MethodDesc
+        private partial class SyntheticMethod : MethodDesc
         {
             private TypeDesc _owningType;
             private MethodSignature _signature;
