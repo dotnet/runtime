d5bdc9ae98e367a136ed0d5bc1dd357ecdc21f8d
Maryam Ariyan <maryam.ariyan@microsoft.com>
2020-07-13 15:21:47 -0700

    header licensing fixup

--- src/libraries/Microsoft.Extensions.Logging.Console/src/SimpleConsoleFormatter.cs
+++ src/libraries/Microsoft.Extensions.Logging.Console/src/SimpleConsoleFormatter.cs
@@ -1,6 +1,5 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
 
 using System;
 using System.IO;
@@ -21,7 +20,7 @@ namespace Microsoft.Extensions.Logging.Console
         private IDisposable _optionsReloadToken;
 
         public SimpleConsoleFormatter(IOptionsMonitor<SimpleConsoleFormatterOptions> options)
-        : base (ConsoleFormatterNames.Simple)
+            : base (ConsoleFormatterNames.Simple)
         {
             ReloadLoggerOptions(options.CurrentValue);
             _optionsReloadToken = options.OnChange(ReloadLoggerOptions);
