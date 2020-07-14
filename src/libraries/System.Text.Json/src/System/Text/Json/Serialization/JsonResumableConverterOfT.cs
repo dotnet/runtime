c9cab1c022ac0f3fe0cdcfcc4c80289e502cbd9f
David Cantu <dacantu@microsoft.com>
2020-07-14 15:43:11 -0700

    Create and add instance reference on ResolveMetadata

--- src/libraries/System.Text.Json/src/System/Text/Json/Serialization/JsonResumableConverterOfT.cs
+++ src/libraries/System.Text.Json/src/System/Text/Json/Serialization/JsonResumableConverterOfT.cs
@@ -41,5 +41,7 @@ namespace System.Text.Json.Serialization
         }
 
         public sealed override bool HandleNull => false;
+
+        public abstract void CreateInstance(ref Utf8JsonReader reader, ref ReadStack state, JsonSerializerOptions options);
     }
 }
