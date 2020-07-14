0a30f09c7ef42ea66960deed7364e2bfe93a0425
Tarek Mahmoud Sayed <tarekms@microsoft.com>
2020-07-13 22:09:24 -0700

    Add NeutralResourcesLanguageAttribute to corefx libraries (#39135)

--- src/libraries/System.Resources.ResourceManager/tests/NeutralResourcesLanguageAttributeTests.cs
+++ src/libraries/System.Resources.ResourceManager/tests/NeutralResourcesLanguageAttributeTests.cs
@@ -2,6 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Xunit;
+using System.Linq;
+using System.Reflection;
+using System.Globalization;
 
 namespace System.Resources.Tests
 {
@@ -22,6 +25,25 @@ namespace System.Resources.Tests
         public static void ConstructorArgumentNull()
         {
             Assert.Throws<ArgumentNullException>(() => new NeutralResourcesLanguageAttribute(null));
+        }
+
+        [Theory]
+        [InlineData(typeof(object))] // System.Private.CoreLib,
+        [InlineData(typeof(System.Collections.Stack))] // System.Collections.NonGeneric
+        [InlineData(typeof(System.Collections.Specialized.ListDictionary))] // System.Collections.Specialized
+        [InlineData(typeof(System.Collections.Immutable.IImmutableList<string>))] // System.Collections.Immutable
+        [InlineData(typeof(System.Collections.Concurrent.ConcurrentBag<string>))] // System.Collections.Concurrent
+        [InlineData(typeof(System.Console))] // System.Console
+        [InlineData(typeof(System.IO.Directory))] // System.IO.FileSystem
+        [InlineData(typeof(System.Linq.Enumerable))] // System.Linq
+        [InlineData(typeof(System.Net.Http.HttpClient))] // System.Net.Http
+        [InlineData(typeof(System.Text.RegularExpressions.Regex))] // System.Text.RegularExpressions
+        [InlineData(typeof(System.Threading.Barrier))] // System.Threading
+        public static void TestAttributeExistence(Type type) => Assert.Equal("en-US", ChildResourceManager.GetNeutralResourcesCulture(type.Assembly).Name);
+
+        public class ChildResourceManager : ResourceManager
+        {
+            public static CultureInfo GetNeutralResourcesCulture(Assembly a) => ResourceManager.GetNeutralResourcesLanguage(a);
         }
     }
 }
