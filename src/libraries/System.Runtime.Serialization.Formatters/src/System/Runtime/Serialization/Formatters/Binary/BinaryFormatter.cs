6b599b2731dab2342c5cc2937637bc85fa7d5c53
Levi Broderick <levib@microsoft.com>
2020-07-14 22:27:49 -0700

    Split implementation

--- src/libraries/System.Runtime.Serialization.Formatters/src/System/Runtime/Serialization/Formatters/Binary/BinaryFormatter.cs
+++ src/libraries/System.Runtime.Serialization.Formatters/src/System/Runtime/Serialization/Formatters/Binary/BinaryFormatter.cs
@@ -2,11 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Collections.Concurrent;
-using System.IO;
 
 namespace System.Runtime.Serialization.Formatters.Binary
 {
-    public sealed class BinaryFormatter : IFormatter
+    public sealed partial class BinaryFormatter : IFormatter
     {
         private static readonly ConcurrentDictionary<Type, TypeInformation> s_typeNameCache = new ConcurrentDictionary<Type, TypeInformation>();
 
@@ -35,95 +34,11 @@ namespace System.Runtime.Serialization.Formatters.Binary
             _context = context;
         }
 
-        public object Deserialize(Stream serializationStream)
-        {
-            // don't refactor the 'throw' into a helper method; linker will have difficulty trimming
-            if (!SerializationInfo.BinaryFormatterEnabled)
-            {
-                throw CreateBinaryFormatterDisallowedException();
-            }
-
-            if (serializationStream == null)
-            {
-                throw new ArgumentNullException(nameof(serializationStream));
-            }
-            if (serializationStream.CanSeek && (serializationStream.Length == 0))
-            {
-                throw new SerializationException(SR.Serialization_Stream);
-            }
-
-            var formatterEnums = new InternalFE()
-            {
-                _typeFormat = _typeFormat,
-                _serializerTypeEnum = InternalSerializerTypeE.Binary,
-                _assemblyFormat = _assemblyFormat,
-                _securityLevel = _securityLevel,
-            };
-
-            var reader = new ObjectReader(serializationStream, _surrogates, _context, formatterEnums, _binder)
-            {
-                _crossAppDomainArray = _crossAppDomainArray
-            };
-            try
-            {
-                var parser = new BinaryParser(serializationStream, reader);
-                return reader.Deserialize(parser);
-            }
-            catch (SerializationException)
-            {
-                throw;
-            }
-            catch (Exception e)
-            {
-                throw new SerializationException(SR.Serialization_CorruptedStream, e);
-            }
-        }
-
-        public void Serialize(Stream serializationStream, object graph)
-        {
-            // don't refactor the 'throw' into a helper method; linker will have difficulty trimming
-            if (!SerializationInfo.BinaryFormatterEnabled)
-            {
-                throw CreateBinaryFormatterDisallowedException();
-            }
-
-            if (serializationStream == null)
-            {
-                throw new ArgumentNullException(nameof(serializationStream));
-            }
-
-            var formatterEnums = new InternalFE()
-            {
-                _typeFormat = _typeFormat,
-                _serializerTypeEnum = InternalSerializerTypeE.Binary,
-                _assemblyFormat = _assemblyFormat,
-            };
-
-            var sow = new ObjectWriter(_surrogates, _context, formatterEnums, _binder);
-            BinaryFormatterWriter binaryWriter = new BinaryFormatterWriter(serializationStream, sow, _typeFormat);
-            sow.Serialize(graph, binaryWriter);
-            _crossAppDomainArray = sow._crossAppDomainArray;
-        }
-
         internal static TypeInformation GetTypeInformation(Type type) =>
             s_typeNameCache.GetOrAdd(type, t =>
             {
                 string assemblyName = FormatterServices.GetClrAssemblyName(t, out bool hasTypeForwardedFrom);
                 return new TypeInformation(FormatterServices.GetClrTypeFullName(t), assemblyName, hasTypeForwardedFrom);
             });
-
-        private static Exception CreateBinaryFormatterDisallowedException()
-        {
-            // If we're in an environment where BinaryFormatter never has a
-            // chance of succeeding, use PNSE. Otherwise use regular NSE.
-
-            string message = SR.BinaryFormatter_SerializationDisallowed;
-
-#if BINARYFORMATTER_PNSE
-            throw new PlatformNotSupportedException(message);
-#else
-            throw new NotSupportedException(message);
-#endif
-        }
     }
 }
