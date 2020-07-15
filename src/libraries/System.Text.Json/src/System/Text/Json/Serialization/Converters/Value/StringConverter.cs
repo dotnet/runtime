246b7296b68367a9818c48d5be78bddee176f1b3
Steve Harter <steveharter@users.noreply.github.com>
2020-07-15 17:27:54 -0500

    Micro optimizations for serialization (#39323)

--- src/libraries/System.Text.Json/src/System/Text/Json/Serialization/Converters/Value/StringConverter.cs
+++ src/libraries/System.Text.Json/src/System/Text/Json/Serialization/Converters/Value/StringConverter.cs
@@ -14,7 +14,15 @@ namespace System.Text.Json.Serialization.Converters
 
         public override void Write(Utf8JsonWriter writer, string? value, JsonSerializerOptions options)
         {
-            writer.WriteStringValue(value);
+            // For performance, lift up the writer implementation.
+            if (value == null)
+            {
+                writer.WriteNullValue();
+            }
+            else
+            {
+                writer.WriteStringValue(value.AsSpan());
+            }
         }
 
         internal override string ReadWithQuotes(ref Utf8JsonReader reader)
