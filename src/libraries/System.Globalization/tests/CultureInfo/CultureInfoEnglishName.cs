c06c63b78e439b3d9c7e34596e11da288021a123
EgorBo <egorbo@gmail.com>
2020-07-15 23:18:23 +0300

    EnglishName_TestData is back to normal for ICU

--- src/libraries/System.Globalization/tests/CultureInfo/CultureInfoEnglishName.cs
+++ src/libraries/System.Globalization/tests/CultureInfo/CultureInfoEnglishName.cs
@@ -12,15 +12,7 @@ namespace System.Globalization.Tests
         {
             yield return new object[] { CultureInfo.CurrentCulture.Name, CultureInfo.CurrentCulture.EnglishName };
             yield return new object[] { "en-US", "English (United States)" };
-            if (PlatformDetection.IsBrowser)
-            {
-                // Browser's ICU returns "French (FR)" for "fr-FR"
-                yield return new object[] { "fr-FR", "French (FR)" };
-            }
-            else
-            {
-                yield return new object[] { "fr-FR", "French (France)" };
-            }
+            yield return new object[] { "fr-FR", "French (France)" };
         }
 
         [Theory]
