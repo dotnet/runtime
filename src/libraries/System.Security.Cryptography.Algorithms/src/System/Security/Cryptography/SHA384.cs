7f53623ea6af0a773d54c94b7983f6a380130d44
Kevin Jones <kevin@vcsjones.com>
2020-07-15 10:01:24 -0400

    Add static one-shot hashing methods
    With this change:
    
    * Building up hashes, piecemeal: IncrementalHash
    * One-shot hashes, polymorphically: HashAlgorithm.ComputeHash
    * One-shot hashes, statically: [Algorithm].HashData

--- src/libraries/System.Security.Cryptography.Algorithms/src/System/Security/Cryptography/SHA384.cs
+++ src/libraries/System.Security.Cryptography.Algorithms/src/System/Security/Cryptography/SHA384.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Internal.Cryptography;
+using System.Diagnostics;
 
 namespace System.Security.Cryptography
 {
@@ -13,14 +14,92 @@ namespace System.Security.Cryptography
 
     public abstract class SHA384 : HashAlgorithm
     {
+        private const int HashSizeBits = 384;
+        private const int HashSizeBytes = HashSizeBits / 8;
+
         protected SHA384()
         {
-            HashSizeValue = 384;
+            HashSizeValue = HashSizeBits;
         }
 
         public static new SHA384 Create() => new Implementation();
 
         public static new SHA384? Create(string hashName) => (SHA384?)CryptoConfig.CreateFromName(hashName);
+
+        /// <summary>
+        /// Computes the hash of data using the SHA384 algorithm.
+        /// </summary>
+        /// <param name="source">The data to hash.</param>
+        /// <returns>The hash of the data.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="source" /> is <see langword="null" />.
+        /// </exception>
+        public static byte[] HashData(byte[] source)
+        {
+            if (source is null)
+                throw new ArgumentNullException(nameof(source));
+
+            return HashData(new ReadOnlySpan<byte>(source));
+        }
+
+        /// <summary>
+        /// Computes the hash of data using the SHA384 algorithm.
+        /// </summary>
+        /// <param name="source">The data to hash.</param>
+        /// <returns>The hash of the data.</returns>
+        public static byte[] HashData(ReadOnlySpan<byte> source)
+        {
+            byte[] buffer = GC.AllocateUninitializedArray<byte>(HashSizeBytes);
+
+            int written = HashData(source, buffer.AsSpan());
+            Debug.Assert(written == buffer.Length);
+
+            return buffer;
+        }
+
+        /// <summary>
+        /// Computes the hash of data using the SHA384 algorithm.
+        /// </summary>
+        /// <param name="source">The data to hash.</param>
+        /// <param name="destination">The buffer to receive the hash value.</param>
+        /// <returns>The total number of bytes written to <paramref name="destination" />.</returns>
+        /// <exception cref="ArgumentException">
+        /// The buffer in <paramref name="destination"/> is too small to hold the calculated hash
+        /// size. The SHA384 algorithm always produces a 384-bit hash, or 48 bytes.
+        /// </exception>
+        public static int HashData(ReadOnlySpan<byte> source, Span<byte> destination)
+        {
+            if (!TryHashData(source, destination, out int bytesWritten))
+                throw new ArgumentException(SR.Argument_DestinationTooShort, nameof(destination));
+
+            return bytesWritten;
+        }
+
+        /// <summary>
+        /// Attempts to compute the hash of data using the SHA384 algorithm.
+        /// </summary>
+        /// <param name="source">The data to hash.</param>
+        /// <param name="destination">The buffer to receive the hash value.</param>
+        /// <param name="bytesWritten">
+        /// When this method returns, the total number of bytes written into <paramref name="destination"/>.
+        /// </param>
+        /// <returns>
+        /// <see langword="false"/> if <paramref name="destination"/> is too small to hold the
+        /// calculated hash, <see langword="true"/> otherwise.
+        /// </returns>
+        public static bool TryHashData(ReadOnlySpan<byte> source, Span<byte> destination, out int bytesWritten)
+        {
+            if (destination.Length < HashSizeBytes)
+            {
+                bytesWritten = 0;
+                return false;
+            }
+
+            bytesWritten = HashProviderDispenser.OneShotHashProvider.HashData(HashAlgorithmNames.SHA384, source, destination);
+            Debug.Assert(bytesWritten == HashSizeBytes);
+
+            return true;
+        }
 
         private sealed class Implementation : SHA384
         {
