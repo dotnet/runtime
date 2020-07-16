84c1f62cc29ced0ea7e6b0f6a7d87aaf3b88679a
Stephen Toub <stoub@microsoft.com>
2020-07-16 10:57:47 -0400

    Sprinkle SuppressGCTransition across Unix shim P/Invokes (#39206)
    * Sprinkle SuppressGCTransition across Unix shim P/Invokes
    
    Did a quick audit and added it to all the trivial functions that just return a constant or return some static or map one value to another or such things.
    
    * Address PR feedback
    
    * Update src/libraries/Native/Unix/System.Native/pal_io.c
    
    Co-authored-by: Aaron Robinson <arobins@microsoft.com>
    
    Co-authored-by: Aaron Robinson <arobins@microsoft.com>

--- src/libraries/System.Net.Sockets/src/System/Net/Sockets/SocketPal.Unix.cs
+++ src/libraries/System.Net.Sockets/src/System/Net/Sockets/SocketPal.Unix.cs
@@ -22,10 +22,8 @@ namespace System.Net.Sockets
         // that get stackalloced in the Linux kernel.
         private const int IovStackThreshold = 8;
 
-        private static bool GetPlatformSupportsDualModeIPv4PacketInfo()
-        {
-            return Interop.Sys.PlatformSupportsDualModeIPv4PacketInfo();
-        }
+        private static bool GetPlatformSupportsDualModeIPv4PacketInfo() =>
+            Interop.Sys.PlatformSupportsDualModeIPv4PacketInfo() != 0;
 
         public static void Initialize()
         {
@@ -422,8 +420,8 @@ namespace System.Net.Sockets
         {
             Debug.Assert(socketAddress != null, "Expected non-null socketAddress");
 
-            int cmsgBufferLen = Interop.Sys.GetControlMessageBufferSize(isIPv4, isIPv6);
-            var cmsgBuffer = stackalloc byte[cmsgBufferLen];
+            int cmsgBufferLen = Interop.Sys.GetControlMessageBufferSize(Convert.ToInt32(isIPv4), Convert.ToInt32(isIPv6));
+            byte* cmsgBuffer = stackalloc byte[cmsgBufferLen];
 
             int sockAddrLen = socketAddressLen;
 
@@ -498,8 +496,8 @@ namespace System.Net.Sockets
                 fixed (byte* sockAddr = socketAddress)
                 fixed (Interop.Sys.IOVector* iov = iovecs)
                 {
-                    int cmsgBufferLen = Interop.Sys.GetControlMessageBufferSize(isIPv4, isIPv6);
-                    var cmsgBuffer = stackalloc byte[cmsgBufferLen];
+                    int cmsgBufferLen = Interop.Sys.GetControlMessageBufferSize(Convert.ToInt32(isIPv4), Convert.ToInt32(isIPv6));
+                    byte* cmsgBuffer = stackalloc byte[cmsgBufferLen];
 
                     var messageHeader = new Interop.Sys.MessageHeader
                     {
