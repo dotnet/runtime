c533866bd386856ae561b5c5a4f29fefc4a88b38
Maryam Ariyan <maryam.ariyan@microsoft.com>
2020-07-15 17:34:19 -0700

    Remove and sort usings

--- src/libraries/Microsoft.Extensions.Logging.Console/src/ConsoleLogger.cs
+++ src/libraries/Microsoft.Extensions.Logging.Console/src/ConsoleLogger.cs
@@ -2,9 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
-using System.Diagnostics;
 using System.IO;
-using System.Text;
 using Microsoft.Extensions.Logging.Abstractions;
 
 namespace Microsoft.Extensions.Logging.Console
--- src/libraries/Microsoft.Extensions.Logging.Console/src/JsonConsoleFormatter.cs
+++ src/libraries/Microsoft.Extensions.Logging.Console/src/JsonConsoleFormatter.cs
@@ -2,21 +2,12 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
-using System.Buffers;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
 using System.Text;
 using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Text.RegularExpressions;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Options;
 
