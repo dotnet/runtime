1088d7c25e28e62e1347eda09d4ff2ea3d121896
Maryam Ariyan <maryam.ariyan@microsoft.com>
2020-07-16 16:26:09 -0700

    Revert "Adding support for constrained open generics to DI (#34393)"
    This reverts commit 211bc884dbadb8765699554257699f5c82d21f6d.

--- src/libraries/Microsoft.Extensions.DependencyInjection/tests/DI.Tests/Fakes/AbstractClass.cs
+++ src/libraries/Microsoft.Extensions.DependencyInjection/tests/DI.Tests/Fakes/AbstractClass.cs
@@ -1,10 +1,12 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-namespace Microsoft.Extensions.DependencyInjection.Specification.Fakes
+namespace Microsoft.Extensions.DependencyInjection.Tests.Fakes
 {
-    public interface IFakeOpenGenericService<TValue>
+    public abstract class AbstractClass
     {
-        TValue Value { get; }
+        public AbstractClass()
+        {
+        }
     }
 }
