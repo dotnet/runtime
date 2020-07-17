f8f69ef8f9fddab35a859c5395219e7e653a59fb
lindexi <lindexi_gd@163.com>
2020-07-17 08:54:01 +0800

    Fix compile

--- src/libraries/Microsoft.Extensions.Caching.Memory/src/CacheEntry.cs
+++ src/libraries/Microsoft.Extensions.Caching.Memory/src/CacheEntry.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Primitives;
 
 namespace Microsoft.Extensions.Caching.Memory
