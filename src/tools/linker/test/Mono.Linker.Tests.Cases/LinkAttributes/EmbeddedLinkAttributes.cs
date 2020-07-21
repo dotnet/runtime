4907f1e62d869eec0ae3680b07c11c2487809b3d
vitek-karas <vitek.karas@microsoft.com>
2020-07-21 07:48:17 -0700

    Don't generate warning 2045 due to the attribute type members referencing the type itself
    If an attribute is marked for removal of instances, but the linker keeps the attribute type for other reasons (for example because it's used from a copy assembly) then there will be several other references to the attribute type from the code within the attribute type itself. These warnings are useless and should not be generated.
    
    Other changes:
    * Moved link attribute tests into their own separate folder - they don't belong to DataFlow tests
    * The log verification logic now removes messages which where matched by LogContains attributes, so it's possible to validate that there are not other messages of the same kind by adding LogDoesntContain at the end
    
    
    Commit migrated from https://github.com/dotnet/linker/commit/0f99d1cd4eb3018512fe79ac10fc6811e96f7d36

--- src/tools/linker/test/Mono.Linker.Tests.Cases/LinkAttributes/EmbeddedLinkAttributes.cs
+++ src/tools/linker/test/Mono.Linker.Tests.Cases/LinkAttributes/EmbeddedLinkAttributes.cs
@@ -1,11 +1,15 @@
-﻿using System;
-using Mono.Linker.Tests.Cases.Expectations.Assertions;
-using Mono.Linker.Tests.Cases.Expectations.Metadata;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Text;
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
+using Mono.Linker.Tests.Cases.Expectations.Metadata;
 
-namespace Mono.Linker.Tests.Cases.DataFlow
+namespace Mono.Linker.Tests.Cases.LinkAttributes
 {
 	[SkipKeptItemsValidation]
 	[SetupCompileResource ("EmbeddedLinkAttributes.xml", "ILLink.LinkAttributes.xml")]
