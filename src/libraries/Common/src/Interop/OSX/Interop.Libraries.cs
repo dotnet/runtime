b99bfc64542361f9d594146491eed556e6bdbfe7
Jan Kotas <jkotas@microsoft.com>
2020-07-21 17:09:18 -0700

    Revert .dylib on frameworks

--- src/libraries/Common/src/Interop/OSX/Interop.Libraries.cs
+++ src/libraries/Common/src/Interop/OSX/Interop.Libraries.cs
@@ -5,16 +5,16 @@ internal static partial class Interop
 {
     internal static partial class Libraries
     {
-        internal const string CoreFoundationLibrary = "/System/Library/Frameworks/CoreFoundation.framework/CoreFoundation.dylib";
-        internal const string CoreServicesLibrary = "/System/Library/Frameworks/CoreServices.framework/CoreServices.dylib";
-        internal const string CFNetworkLibrary = "/System/Library/Frameworks/CFNetwork.framework/CFNetwork.dylib";
+        internal const string CoreFoundationLibrary = "/System/Library/Frameworks/CoreFoundation.framework/CoreFoundation";
+        internal const string CoreServicesLibrary = "/System/Library/Frameworks/CoreServices.framework/CoreServices";
+        internal const string CFNetworkLibrary = "/System/Library/Frameworks/CFNetwork.framework/CFNetwork";
         internal const string libobjc = "/usr/lib/libobjc.dylib";
         internal const string libproc = "/usr/lib/libproc.dylib";
         internal const string LibSystemCommonCrypto = "/usr/lib/system/libcommonCrypto.dylib";
         internal const string LibSystemKernel = "/usr/lib/system/libsystem_kernel.dylib";
         internal const string Odbc32 = "libodbc.2.dylib";
         internal const string OpenLdap = "libldap.dylib";
-        internal const string SystemConfigurationLibrary = "/System/Library/Frameworks/SystemConfiguration.framework/SystemConfiguration.dylib";
+        internal const string SystemConfigurationLibrary = "/System/Library/Frameworks/SystemConfiguration.framework/SystemConfiguration";
         internal const string AppleCryptoNative = "libSystem.Security.Cryptography.Native.Apple.dylib";
         internal const string MsQuic = "libmsquic.dylib";
     }
