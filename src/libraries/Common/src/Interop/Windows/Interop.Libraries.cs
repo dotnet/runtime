c2c0ad821109a62f1581c3e52cd9ac79359af9ad
Aaron Robinson <arobins@microsoft.com>
2020-07-23 10:39:19 -0700

    Remove RoGetBufferMarshaler P/Invoke

--- src/libraries/Common/src/Interop/Windows/Interop.Libraries.cs
+++ src/libraries/Common/src/Interop/Windows/Interop.Libraries.cs
@@ -22,7 +22,6 @@ internal static partial class Interop
         internal const string Ole32 = "ole32.dll";
         internal const string OleAut32 = "oleaut32.dll";
         internal const string PerfCounter = "perfcounter.dll";
-        internal const string RoBuffer = "api-ms-win-core-winrt-robuffer-l1-1-0.dll";
         internal const string Secur32 = "secur32.dll";
         internal const string Shell32 = "shell32.dll";
         internal const string SspiCli = "sspicli.dll";
