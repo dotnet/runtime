b8c080bab06a03f8f5a234097a1ae1726939a367
Kenneth Pouncey <kjpou@pt.lu>
2020-07-27 11:14:46 +0200

    Remove unused variable from tests

--- src/libraries/System.IO.FileSystem/tests/FileSystemTest.Browser.cs
+++ src/libraries/System.IO.FileSystem/tests/FileSystemTest.Browser.cs
@@ -8,11 +8,6 @@ namespace System.IO.Tests
 {
     public abstract partial class FileSystemTest
     {
-        protected static int geteuid()
-        {
-            throw new PlatformNotSupportedException();
-        }
-
         protected static int mkfifo(string path, int mode)
         {
             throw new PlatformNotSupportedException();
