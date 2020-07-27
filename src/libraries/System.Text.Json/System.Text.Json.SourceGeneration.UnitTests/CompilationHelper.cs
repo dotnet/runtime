af317817a7aec7e8c554c6300f01b0da684eb7db
Kyungtak Woo <kevinwkt1997@gmail.com>
2020-07-26 21:19:41 -0500

    Change syntax receiver to do semantic model check

---
+++ src/libraries/System.Text.Json/System.Text.Json.SourceGeneration.UnitTests/CompilationHelper.cs
@@ -0,0 +1,54 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Reflection;
+using System.Text.Json.Serialization;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Xunit;
+
+namespace System.Text.Json.SourceGeneration.UnitTests
+{
+    public class CompilationHelper
+    {
+        public static Compilation CreateCompilation(string source)
+        {
+            // Bypass System.Runtime error.
+            Assembly systemRuntimeAssembly = Assembly.Load("System.Runtime, Version=5.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a");
+            string systemRuntimeAssemblyPath = systemRuntimeAssembly.Location;
+
+            MetadataReference[] references = new MetadataReference[] {
+                MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(Attribute).Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(JsonSerializableAttribute).Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(JsonSerializerOptions).Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(Type).Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(KeyValuePair).Assembly.Location),
+                MetadataReference.CreateFromFile(systemRuntimeAssemblyPath),
+            };
+
+            return CSharpCompilation.Create(
+                "TestAssembly",
+                syntaxTrees: new[] { CSharpSyntaxTree.ParseText(source) },
+                references: references,
+                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)
+            );
+        }
+
+        private static GeneratorDriver CreateDriver(Compilation compilation, params ISourceGenerator[] generators)
+            => new CSharpGeneratorDriver(
+                new CSharpParseOptions(kind: SourceCodeKind.Regular, documentationMode: DocumentationMode.Parse),
+                ImmutableArray.Create(generators),
+                ImmutableArray<AdditionalText>.Empty);
+
+        public static Compilation RunGenerators(Compilation compilation, out ImmutableArray<Diagnostic> diagnostics, params ISourceGenerator[] generators)
+        {
+            CreateDriver(compilation, generators).RunFullGeneration(compilation, out Compilation outCompilation, out diagnostics);
+            return outCompilation;
+        }
+    }
+}
--- src/libraries/System.Text.Json/System.Text.Json.SourceGeneration/JsonSerializableSyntaxReceiver.cs
+++ src/libraries/System.Text.Json/System.Text.Json.SourceGeneration/JsonSerializableSyntaxReceiver.cs
@@ -5,6 +5,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
@@ -12,42 +13,14 @@ namespace System.Text.Json.SourceGeneration
 {
         public class JsonSerializableSyntaxReceiver : ISyntaxReceiver
         {
-            public List<KeyValuePair<string, IdentifierNameSyntax>> ExternalClassTypes = new List<KeyValuePair<string, IdentifierNameSyntax>>();
-            public List<KeyValuePair<string, TypeDeclarationSyntax>> InternalClassTypes = new List<KeyValuePair<string, TypeDeclarationSyntax>>();
+            public List<TypeDeclarationSyntax> TypesWithAttributes = new List<TypeDeclarationSyntax>();
 
             public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
             {
-                // Look for classes or structs for JsonSerializable Attribute.
-                if (syntaxNode is ClassDeclarationSyntax || syntaxNode is StructDeclarationSyntax)
+                if(syntaxNode is TypeDeclarationSyntax typeDeclarationNode
+                   && typeDeclarationNode.AttributeLists.Count > 0)
                 {
-                    // Find JsonSerializable Attributes.
-                    IEnumerable<AttributeSyntax>? serializableAttributes = null;
-                    AttributeListSyntax attributeList = ((TypeDeclarationSyntax)syntaxNode).AttributeLists.SingleOrDefault();
-                    if (attributeList != null)
-                    {
-                        serializableAttributes = attributeList.Attributes.Where(node => (node is AttributeSyntax attr && attr.Name.ToString() == "JsonSerializable")).Cast<AttributeSyntax>();
-                    }
-
-                    if (serializableAttributes?.Any() == true)
-                    {
-                        // JsonSerializableAttribute has AllowMultiple as False, should only have 1 attribute.
-                        Debug.Assert(serializableAttributes.Count() == 1);
-                        AttributeSyntax attributeNode = serializableAttributes.First();
-
-                        // Check if the attribute is being passed a type.
-                        if (attributeNode.DescendantNodes().Where(node => node is TypeOfExpressionSyntax).Any())
-                        {
-                            // Get JsonSerializable attribute arguments.
-                            AttributeArgumentSyntax attributeArgumentNode = (AttributeArgumentSyntax)attributeNode.DescendantNodes().Where(node => node is AttributeArgumentSyntax).SingleOrDefault();
-                            // Get external class token from arguments.
-                            IdentifierNameSyntax externalTypeNode = (IdentifierNameSyntax)attributeArgumentNode?.DescendantNodes().Where(node => node is IdentifierNameSyntax).SingleOrDefault();
-                            ExternalClassTypes.Add(new KeyValuePair<string, IdentifierNameSyntax>(((TypeDeclarationSyntax)syntaxNode).Identifier.Text, externalTypeNode));
-                        }
-                        else
-                        {
-                            InternalClassTypes.Add(new KeyValuePair<string, TypeDeclarationSyntax>(((TypeDeclarationSyntax)syntaxNode).Identifier.Text, (TypeDeclarationSyntax)syntaxNode));
-                        }
-                    }
+                    TypesWithAttributes.Add(typeDeclarationNode);
                 }
             }
         }
