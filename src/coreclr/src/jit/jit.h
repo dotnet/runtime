b76824f91872a8256291ebef7612f0875500a200
Bruce Forstall <brucefo@microsoft.com>
2020-07-26 22:34:18 -0700

    Revert DUMP_GC_TABLES change

--- src/coreclr/src/jit/jit.h
+++ src/coreclr/src/jit/jit.h
@@ -422,7 +422,7 @@ typedef ptrdiff_t ssize_t;
 
 /*****************************************************************************/
 
-#define DUMP_GC_TABLES 1
+#define DUMP_GC_TABLES DEBUG
 #define VERIFY_GC_TABLES 0
 #define REARRANGE_ADDS 1
 
