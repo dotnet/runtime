6a36de9203cb7d01e3123d632d9f2af048a62300
John <johnkellyoxford@gmail.com>
2020-07-30 23:43:37 +0100

    Hide from stack trace

--- src/libraries/System.Private.CoreLib/src/System/Runtime/InteropServices/Marshal.cs
+++ src/libraries/System.Private.CoreLib/src/System/Runtime/InteropServices/Marshal.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics;
 using System.Security;
 using System.Reflection;
 using System.Runtime.CompilerServices;
@@ -611,6 +612,7 @@ namespace System.Runtime.InteropServices
             }
         }
 
+        [StackTraceHidden]
         private static void InternalThrowExceptionForHr(int errorCode, IntPtr errorInfo)
         {
             throw GetExceptionForHR(errorCode, errorInfo)!;
