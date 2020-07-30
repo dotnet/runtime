c265c60a1bc5a8738b2f9770ead848f27ba341c5
Andy Ayers <andya@microsoft.com>
2020-07-30 10:15:15 -0700

    Port JIT fix to Preview 8: ensure fgFirstBB has appropriate flags (#40086)
    Port of #40038 to Preview 8.
    
    Fix #39023
    
    Release jits might sometimes generate bad GC info.
    
    Mysterious intermittent crashes. Without this fix jit GC
    info generation for some methods is non-deterministically bad.
    
    Yes, problem does not occur in 3.1.
    
    Very low.

--- src/coreclr/src/jit/flowgraph.cpp
+++ src/coreclr/src/jit/flowgraph.cpp
@@ -175,6 +175,8 @@ void Compiler::fgInit()
 #ifdef FEATURE_SIMD
     fgPreviousCandidateSIMDFieldAsgStmt = nullptr;
 #endif
+
+    fgHasSwitch = false;
 }
 
 bool Compiler::fgHaveProfileData()
@@ -476,7 +478,8 @@ void Compiler::fgEnsureFirstBBisScratch()
 
     noway_assert(fgLastBB != nullptr);
 
-    block->bbFlags |= (BBF_INTERNAL | BBF_IMPORTED);
+    // Set the expected flags
+    block->bbFlags |= (BBF_INTERNAL | BBF_IMPORTED | BBF_JMP_TARGET | BBF_HAS_LABEL);
 
     // This new first BB has an implicit ref, and no others.
     block->bbRefs = 1;
