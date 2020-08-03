b0027792e027a7cd59620d4e43bf40ff3536a17e
Kenneth Pouncey <kjpou@pt.lu>
2020-08-03 09:57:06 +0200

    Fix enumeration test errors failing from `HiddenFilesAreReturned`.
    - WebAssembly (BROWSER) has dirent d_type but is not identifying correctly but by returning UNKNOWN the managed code properly stats the file to detect if entry is directory or not.

--- src/libraries/System.IO.FileSystem/tests/Directory/GetFileSystemEntries_str.cs
+++ src/libraries/System.IO.FileSystem/tests/Directory/GetFileSystemEntries_str.cs
@@ -175,7 +175,6 @@ namespace System.IO.Tests
         }
 
         [Fact]
-        [PlatformSpecific(~TestPlatforms.Browser)]
         public void HiddenFilesAreReturned()
         {
             // Note that APIs that take EnumerationOptions do NOT find hidden files by default
