0a571014b9716fba7d68aec7427e1a73cb4b88fd
Mateo Torres-Ruiz <mateoatr@users.noreply.github.com>
2020-08-03 10:50:26 -0700

    Add suppressions before checking for ca removal instances (dotnet/linker#1395)
    Commit migrated from https://github.com/dotnet/linker/commit/edaf18961fe80083245822bd2dec205db5df64f4

---
+++ src/tools/linker/test/Mono.Linker.Tests.Cases/Warnings/WarningSuppression/AddSuppressionsBeforeAttributeRemoval.cs
@@ -0,0 +1,27 @@
+﻿using Mono.Linker.Tests.Cases.Expectations.Assertions;
+using Mono.Linker.Tests.Cases.Expectations.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Mono.Linker.Tests.Cases.Warnings.WarningSuppression
+{
+	[SkipKeptItemsValidation]
+	[SetupLinkAttributesFile ("AddSuppressionsBeforeAttributeRemoval.xml")]
+	[LogDoesNotContain ("IL2006: Mono.Linker.Tests.Cases.Warnings.WarningSuppression.AddSuppressionsBeforeAttributeRemoval.Main()")]
+	public class AddSuppressionsBeforeAttributeRemoval
+	{
+		public static Type TriggerUnrecognizedPattern ()
+		{
+			return typeof (AddedPseudoAttributeAttribute);
+		}
+
+		[UnconditionalSuppressMessage ("ILLinker", "IL2006")]
+		public static void Main ()
+		{
+			Expression.Call (TriggerUnrecognizedPattern (), "", Type.EmptyTypes);
+		}
+	}
+}
