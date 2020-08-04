ab35f1be23beee61b89eecdd4dc65e656cf8eefd
Carol Eidt <carol.eidt@microsoft.com>
2020-08-03 18:50:37 -0700

    Transform dead late arg store to NOP
    When deleting a dead store, preserve the GTF_LATE_ARG and transform it to NOP.
    
    Fix #39742

--- src/coreclr/src/jit/lower.cpp
+++ src/coreclr/src/jit/lower.cpp
@@ -3181,7 +3181,9 @@ void Lowering::LowerStoreLocCommon(GenTreeLclVarCommon* lclStore)
             // Create the assignment node.
             lclStore->ChangeOper(GT_STORE_OBJ);
             GenTreeBlk* objStore = lclStore->AsObj();
-            objStore->gtFlags    = GTF_ASG | GTF_IND_NONFAULTING | GTF_IND_TGT_NOT_HEAP;
+            // Only the GTF_LATE_ARG flag (if present) is preserved.
+            objStore->gtFlags &= GTF_LATE_ARG;
+            objStore->gtFlags |= GTF_ASG | GTF_IND_NONFAULTING | GTF_IND_TGT_NOT_HEAP;
 #ifndef JIT32_GCENCODER
             objStore->gtBlkOpGcUnsafe = false;
 #endif
