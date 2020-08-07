b60dbbff9c35a20b0723350c9a222507f0fcb882
Adam Sitnik <adam.sitnik@gmail.com>
2020-08-07 12:18:34 +0200

    Remove new platform members from OSPlatform (#40373)
    * Remove new platform members from OSPlatform
    
    * remove IsOSPlatformEarlierThan and IsOSPlatformOrLater methods
    
    * remove unused resource and dependency
    
    * delegate RuntimeInformation.IsOSPlatform to call OperatingSystem.IsOSPlatform. it won't compile now as the new guards have not been merged yet (#40457)
    
    * remove OSPlatform.Browser and it's usage

--- src/libraries/System.Runtime.Extensions/tests/System/EnvironmentTests.cs
+++ src/libraries/System.Runtime.Extensions/tests/System/EnvironmentTests.cs
@@ -127,7 +127,7 @@ namespace System.Tests
         public void OSVersion_MatchesPlatform()
         {
             PlatformID id = Environment.OSVersion.Platform;
-            PlatformID expected = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? PlatformID.Win32NT : RuntimeInformation.IsOSPlatform(OSPlatform.Browser) ? PlatformID.Other : PlatformID.Unix;
+            PlatformID expected = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? PlatformID.Win32NT : OperatingSystem.IsBrowser() ? PlatformID.Other : PlatformID.Unix;
             Assert.Equal(expected, id);
         }
 
@@ -142,7 +142,7 @@ namespace System.Tests
 
             Assert.Contains(version.ToString(2), versionString);
 
-            string expectedOS = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "Windows " : RuntimeInformation.IsOSPlatform(OSPlatform.Browser) ? "Other " : "Unix ";
+            string expectedOS = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "Windows " : OperatingSystem.IsBrowser() ? "Other " : "Unix ";
             Assert.Contains(expectedOS, versionString);
         }
 
