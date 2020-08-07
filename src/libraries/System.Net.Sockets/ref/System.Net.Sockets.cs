1cc6271b696c3985123ef152db31711fa5416023
Anton Firszov <antonfir@gmail.com>
2020-08-07 04:04:06 +0200

    Use output from GenerateReferenceSource

--- src/libraries/System.Net.Sockets/ref/System.Net.Sockets.cs
+++ src/libraries/System.Net.Sockets/ref/System.Net.Sockets.cs
@@ -6,25 +6,16 @@
 
 namespace System.Net.Connections
 {
-    public class SocketsConnectionFactory : ConnectionFactory
+    public partial class SocketsConnectionFactory : System.Net.Connections.ConnectionFactory
     {
-        // dual-mode IPv6 socket. See Socket(SocketType socketType, ProtocolType protocolType)
-        public SocketsConnectionFactory(Sockets.SocketType socketType, Sockets.ProtocolType protocolType) { }
-
-        // See Socket(AddressFamily addressFamily, SocketType socketType, ProtocolType protocolType)
-        public SocketsConnectionFactory(Sockets.AddressFamily addressFamily, Sockets.SocketType socketType, Sockets.ProtocolType protocolType) { }
-
-        // This must be thread-safe!
-        public override Threading.Tasks.ValueTask<Connection> ConnectAsync(EndPoint? endPoint, IConnectionProperties? options = null, Threading.CancellationToken cancellationToken = default) { throw null; }
-
-        // These exist to provide an easy way to shim the default behavior.
-        // Note: Connect must call this to create its socket.
-        protected virtual Sockets.Socket CreateSocket(Sockets.AddressFamily addressFamily, Sockets.SocketType socketType, Sockets.ProtocolType protocolType, EndPoint? endPoint, IConnectionProperties? options) { throw null; }
-        protected virtual IO.Stream CreateStream(Sockets.Socket socket, IConnectionProperties? options) { throw null; }
-        protected virtual IO.Pipelines.IDuplexPipe CreatePipe(Sockets.Socket socket, IConnectionProperties? options) { throw null; }
+        public SocketsConnectionFactory(System.Net.Sockets.AddressFamily addressFamily, System.Net.Sockets.SocketType socketType, System.Net.Sockets.ProtocolType protocolType) { }
+        public SocketsConnectionFactory(System.Net.Sockets.SocketType socketType, System.Net.Sockets.ProtocolType protocolType) { }
+        public override System.Threading.Tasks.ValueTask<System.Net.Connections.Connection> ConnectAsync(System.Net.EndPoint? endPoint, System.Net.Connections.IConnectionProperties? options = null, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
+        protected virtual System.IO.Pipelines.IDuplexPipe CreatePipe(System.Net.Sockets.Socket socket, System.Net.Connections.IConnectionProperties? options) { throw null; }
+        protected virtual System.Net.Sockets.Socket CreateSocket(System.Net.Sockets.AddressFamily addressFamily, System.Net.Sockets.SocketType socketType, System.Net.Sockets.ProtocolType protocolType, System.Net.EndPoint? endPoint, System.Net.Connections.IConnectionProperties? options) { throw null; }
+        protected virtual System.IO.Stream CreateStream(System.Net.Sockets.Socket socket, System.Net.Connections.IConnectionProperties? options) { throw null; }
     }
 }
-
 namespace System.Net.Sockets
 {
     public enum IOControlCode : long
