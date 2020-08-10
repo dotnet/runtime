df41c2f492968091b276fbd58365483fdfe29042
Adam Sitnik <adam.sitnik@gmail.com>
2020-08-10 09:56:59 +0200

    use `||` instead of `|`

--- src/libraries/System.IO.FileSystem/tests/PortedCommon/IOInputs.cs
+++ src/libraries/System.IO.FileSystem/tests/PortedCommon/IOInputs.cs
@@ -9,7 +9,7 @@ using System.Linq;
 internal static class IOInputs
 {
     public static bool SupportsSettingCreationTime => OperatingSystem.IsWindows();
-    public static bool SupportsGettingCreationTime => OperatingSystem.IsWindows() | OperatingSystem.IsMacOS();
+    public static bool SupportsGettingCreationTime => OperatingSystem.IsWindows() || OperatingSystem.IsMacOS();
 
     // Max path length (minus trailing \0). Unix values vary system to system; just using really long values here likely to be more than on the average system.
     public static readonly int MaxPath = OperatingSystem.IsWindows() ? 259 : 10000;
