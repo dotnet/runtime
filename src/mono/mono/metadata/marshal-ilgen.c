a84ee9f4d20facb2a6e3159fe8ca5c408e0bec9d
Zoltan Varga <vargaz@gmail.com>
2020-08-10 14:50:16 -0400

    Fix test failures.

--- src/mono/mono/metadata/marshal-ilgen.c
+++ src/mono/mono/metadata/marshal-ilgen.c
@@ -2055,7 +2055,7 @@ emit_native_wrapper_ilgen (MonoImage *image, MonoMethodBuilder *mb, MonoMethodSi
 	if (need_gc_safe)
 		gc_safe_transition_builder_add_locals (&gc_safe_transition_builder);
 
-	if (!func && !aot) {
+	if (!func && !aot && !func_param && !MONO_CLASS_IS_IMPORT (mb->method->klass)) {
 		/*
 		 * On netcore, its possible to register pinvoke resolvers at runtime, so
 		 * a pinvoke lookup can fail, and then succeed later. So if the
