1064ded105904bfacf9d522f94d9606ec8534229
Vita Tauer <t@uer.cz>
2020-08-11 09:52:39 +0200

    Delayed failure for missing BCrypt mode in DES to actual use

--- src/libraries/Common/src/Interop/Windows/BCrypt/DESBCryptModes.cs
+++ src/libraries/Common/src/Interop/Windows/BCrypt/DESBCryptModes.cs
@@ -9,32 +9,37 @@ namespace Internal.Cryptography
 {
     internal static class DesBCryptModes
     {
-        private static readonly SafeAlgorithmHandle s_hAlgCbc = OpenDesAlgorithm(Cng.BCRYPT_CHAIN_MODE_CBC);
-        private static readonly SafeAlgorithmHandle s_hAlgEcb = OpenDesAlgorithm(Cng.BCRYPT_CHAIN_MODE_ECB);
-        private static readonly SafeAlgorithmHandle s_hAlgCfb8 = OpenDesAlgorithm(Cng.BCRYPT_CHAIN_MODE_CFB, 1);
+        private static readonly Lazy<SafeAlgorithmHandle> s_hAlgCbc = OpenDesAlgorithm(Cng.BCRYPT_CHAIN_MODE_CBC);
+        private static readonly Lazy<SafeAlgorithmHandle> s_hAlgEcb = OpenDesAlgorithm(Cng.BCRYPT_CHAIN_MODE_ECB);
+        private static readonly Lazy<SafeAlgorithmHandle> s_hAlgCfb8 = OpenDesAlgorithm(Cng.BCRYPT_CHAIN_MODE_CFB, 1);
 
         internal static SafeAlgorithmHandle GetSharedHandle(CipherMode cipherMode, int feedback) =>
             // Windows 8 added support to set the CipherMode value on a key,
             // but Windows 7 requires that it be set on the algorithm before key creation.
             (cipherMode, feedback) switch
             {
-                (CipherMode.CBC, _) => s_hAlgCbc,
-                (CipherMode.ECB, _) => s_hAlgEcb,
-                (CipherMode.CFB, 1) => s_hAlgCfb8,
+                (CipherMode.CBC, _) => s_hAlgCbc.Value,
+                (CipherMode.ECB, _) => s_hAlgEcb.Value,
+                (CipherMode.CFB, 1) => s_hAlgCfb8.Value,
                 _ => throw new NotSupportedException(),
             };
 
-        private static SafeAlgorithmHandle OpenDesAlgorithm(string cipherMode, int feedback = 0)
+        private static Lazy<SafeAlgorithmHandle> OpenDesAlgorithm(string cipherMode, int feedback = 0)
         {
-            SafeAlgorithmHandle hAlg = Cng.BCryptOpenAlgorithmProvider(Cng.BCRYPT_DES_ALGORITHM, null, Cng.OpenAlgorithmProviderFlags.NONE);
-            hAlg.SetCipherMode(cipherMode);
-
-            if (feedback > 0)
+            return new Lazy<SafeAlgorithmHandle>(() =>
             {
-                hAlg.SetFeedbackSize(feedback);
-            }
+                SafeAlgorithmHandle hAlg = Cng.BCryptOpenAlgorithmProvider(Cng.BCRYPT_DES_ALGORITHM,
+                    null,
+                    Cng.OpenAlgorithmProviderFlags.NONE);
+                hAlg.SetCipherMode(cipherMode);
 
-            return hAlg;
+                if (feedback > 0)
+                {
+                    hAlg.SetFeedbackSize(feedback);
+                }
+
+                return hAlg;
+            });
         }
     }
 }
