dc4197c255931f215dfe37cf4d28a5255dcc8a8f
Koundinya Veluri <kouvel@microsoft.com>
2020-08-17 14:37:06 -0700

    Add tests

---
+++ src/tests/baseservices/TieredCompilation/RunBasicTestWithMcj.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+CLRTestExpectedExitCode=100
+
+echo Collect profile without R2R, use profile without R2R
+rm -f profile.mcj
+"$CORE_ROOT/corerun" BasicTestWithMcj.dll
+CLRTestExitCode=$?
+if [ $CLRTestExitCode -ne $CLRTestExpectedExitCode ]; then
+  exit $CLRTestExitCode
+fi
+"$CORE_ROOT/corerun" BasicTestWithMcj.dll
+CLRTestExitCode=$?
+if [ $CLRTestExitCode -ne $CLRTestExpectedExitCode ]; then
+  exit $CLRTestExitCode
+fi
+
+echo Collect profile with R2R, use profile with R2R
+rm -f profile.mcj
+"$CORE_ROOT/corerun" r2r/BasicTestWithMcj.dll
+CLRTestExitCode=$?
+if [ $CLRTestExitCode -ne $CLRTestExpectedExitCode ]; then
+  exit $CLRTestExitCode
+fi
+"$CORE_ROOT/corerun" r2r/BasicTestWithMcj.dll
+CLRTestExitCode=$?
+if [ $CLRTestExitCode -ne $CLRTestExpectedExitCode ]; then
+  exit $CLRTestExitCode
+fi
+
+echo Collect profile without R2R, use profile with R2R
+rm -f profile.mcj
+"$CORE_ROOT/corerun" BasicTestWithMcj.dll
+CLRTestExitCode=$?
+if [ $CLRTestExitCode -ne $CLRTestExpectedExitCode ]; then
+  exit $CLRTestExitCode
+fi
+"$CORE_ROOT/corerun" r2r/BasicTestWithMcj.dll
+CLRTestExitCode=$?
+if [ $CLRTestExitCode -ne $CLRTestExpectedExitCode ]; then
+  exit $CLRTestExitCode
+fi
+
+echo Collect profile with R2R disabled, use profile with R2R enabled
+rm -f profile.mcj
+export COMPlus_ReadyToRun=0
+"$CORE_ROOT/corerun" r2r/BasicTestWithMcj.dll
+CLRTestExitCode=$?
+unset COMPlus_ReadyToRun
+if [ $CLRTestExitCode -ne $CLRTestExpectedExitCode ]; then
+  exit $CLRTestExitCode
+fi
+"$CORE_ROOT/corerun" r2r/BasicTestWithMcj.dll
+CLRTestExitCode=$?
+if [ $CLRTestExitCode -ne $CLRTestExpectedExitCode ]; then
+  exit $CLRTestExitCode
+fi
+
+rm -f profile.mcj
+exit $CLRTestExpectedExitCode
