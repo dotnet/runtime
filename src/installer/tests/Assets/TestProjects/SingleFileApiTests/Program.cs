3faf471702b92ca249e2c557322024e9c363c7b3
Andy Gocke <andy@commentout.net>
2020-08-18 01:07:28 -0700

    Throw from CodeBase if assembly is in the bundle

---
+++ src/installer/tests/Assets/TestProjects/SingleFileApiTests/Program.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace SingleFileApiTests
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            switch (args[0])
+            {
+                case "cmdlineargs":
+                    Console.WriteLine(Environment.GetCommandLineArgs()[0]);
+                    return;
+
+                case "codebase":
+                    try
+                    {
+                        #pragma warning disable SYSLIB0012
+                        _ = typeof(Program).Assembly.CodeBase;
+                        #pragma warning restore SYSLIB0012
+                    }
+                    catch (NotSupportedException)
+                    {
+                        Console.WriteLine("CodeBase NotSupported");
+                        return;
+                    }
+                    break;
+            }
+
+            Console.WriteLine("test failure");
+        }
+    }
+}
