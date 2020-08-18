2709cb7fb40550ad3299e4b85eac23bae3e1dfcf
Mitchell Hwang <mitchell.hwang@microsoft.com>
2020-08-18 16:15:51 -0400

    Provide context for skipping Assert in Copy.cs

--- src/libraries/System.IO.FileSystem/tests/File/Copy.cs
+++ src/libraries/System.IO.FileSystem/tests/File/Copy.cs
@@ -140,7 +140,12 @@ namespace System.IO.Tests
             }
 
             // Ensure last write/access time on the new file is appropriate
-            if (PlatformDetection.IsNotBrowser) // There is only one write time on browser vfs
+            //
+            // For browser, there is technically only 1 time.  It's the max
+            // of LastWrite and LastAccess.  On browser, File.SetLastWriteTime
+            // overwrites LastWrite and LastAccess, and File.Copy
+            // overwrites LastWrite , so this check doesn't apply.
+            if (PlatformDetection.IsNotBrowser)
             {
                 Assert.InRange(File.GetLastWriteTimeUtc(testFileDest), lastWriteTime.AddSeconds(-1), lastWriteTime.AddSeconds(1));
             }
