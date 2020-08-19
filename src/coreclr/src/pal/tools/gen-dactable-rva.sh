5d81c378745c93a3bdfc78a0f20f38c6d49c2af8
Jan Vorlicek <janvorli@microsoft.com>
2020-08-19 16:35:04 -0700

    Fix FreeBSD

--- src/coreclr/src/pal/tools/gen-dactable-rva.sh
+++ src/coreclr/src/pal/tools/gen-dactable-rva.sh
@@ -1,1 +1,1 @@
-${NM:-nm} -P -t x $1 | awk -F ' ' '/g_dacTable/ { print "#define DAC_TABLE_RVA 0x" substr("0000000000000000" $3, length($3) + 1); exit }' > $2
+${NM:-nm} -P -D -t x $1 | awk -F ' ' '/g_dacTable/ { print "#define DAC_TABLE_RVA 0x" substr("0000000000000000" $3, length($3) + 1); exit }' > $2
