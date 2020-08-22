d880c9954c809df4afa3ae2738dc6bcc3324ced3
EgorBo <egorbo@gmail.com>
2020-08-22 23:17:10 +0300

    bump jit-vm guid

--- src/coreclr/src/inc/corinfo.h
+++ src/coreclr/src/inc/corinfo.h
@@ -208,11 +208,11 @@ TODO: Talk about initializing strutures before use
 //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-constexpr GUID JITEEVersionIdentifier = { /* a5eec3a4-4176-43a7-8c2b-a05b551d4f49 */
-    0xa5eec3a4,
-    0x4176,
-    0x43a7,
-    {0x8c, 0x2b, 0xa0, 0x5b, 0x55, 0x1d, 0x4f, 0x49}
+constexpr GUID JITEEVersionIdentifier = { /* a0184d06-a562-43f6-94ad-44627db87310 */
+    0xa0184d06,
+    0xa562,
+    0x43f6,
+    {0x94, 0xad, 0x44, 0x62, 0x7d, 0xb8, 0x73, 0x10}
 };
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
