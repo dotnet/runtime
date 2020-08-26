3aba51ce2aab2f78c2181cb507e1f746a7be73c1
Andrew Au <andrewau@microsoft.com>
2020-08-25 20:02:28 -0700

    Restrict generational aware analysis to processes that matches the command line configuration (#41052)

--- src/coreclr/src/vm/processdiagnosticsprotocolhelper.cpp
+++ src/coreclr/src/vm/processdiagnosticsprotocolhelper.cpp
@@ -278,14 +278,7 @@ void ProcessDiagnosticsProtocolHelper::GetProcessInfo(DiagnosticsIpc::IpcMessage
     struct ProcessInfoPayload payload = {};
 
     // Get cmdline
-    payload.CommandLine = GetManagedCommandLine();
-
-    // Checkout https://github.com/dotnet/coreclr/pull/24433 for more information about this fall back.
-    if (payload.CommandLine == nullptr)
-    {
-        // Use the result from GetCommandLineW() instead
-        payload.CommandLine = GetCommandLineW();
-    }
+    payload.CommandLine = GetCommandLineForDiagnostics();
 
     // get OS + Arch info
     payload.OS = EventPipeEventSource::s_pOSInformation;
