6219594e0bb68891272d8e0c8e1e1f6a1e091781
Tammy Qiu <tammy.qiu@yahoo.com>
2020-08-26 12:06:14 -0400

    [wasm][browser] Fix System.IO.FileSystem failing tests requiring nanosecond/millisecond granularity (#41288)
    * Emscripten lacks ns/ms granularity which I included a switch for and added a browser platform check
    * Emscripten takes maximum of utime and atime so I set utime and atime together for Browser

--- src/libraries/System.IO.FileSystem/tests/FileInfo/GetSetTimes.cs
+++ src/libraries/System.IO.FileSystem/tests/FileInfo/GetSetTimes.cs
@@ -103,8 +103,7 @@ namespace System.IO.Tests
                 DateTimeKind.Utc);
         }
 
-        [ConditionalFact(nameof(isNotHFS))]
-        [ActiveIssue("https://github.com/dotnet/runtime/issues/40530", TestPlatforms.Browser)]
+        [ConditionalFact(nameof(HighTemporalResolution))]
         public void CopyToMillisecondPresent()
         {
             FileInfo input = GetNonZeroMilliseconds();
@@ -118,8 +117,7 @@ namespace System.IO.Tests
             Assert.NotEqual(0, output.LastWriteTime.Millisecond);
         }
 
-        [ConditionalFact(nameof(isNotHFS))]
-        [ActiveIssue("https://github.com/dotnet/runtime/issues/40530", TestPlatforms.Browser)]
+        [ConditionalFact(nameof(HighTemporalResolution))]
         public void CopyToNanosecondsPresent()
         {
             FileInfo input = GetNonZeroNanoseconds();
@@ -135,8 +133,8 @@ namespace System.IO.Tests
             Assert.True(HasNonZeroNanoseconds(output.LastWriteTime));
         }
 
-        [ConditionalFact(nameof(isHFS))]
-        public void CopyToNanosecondsPresent_HFS()
+        [ConditionalFact(nameof(LowTemporalResolution))]
+        public void CopyToNanosecondsPresent_LowTempRes()
         {
             FileInfo input = new FileInfo(GetTestFilePath());
             input.Create().Dispose();
@@ -149,8 +147,8 @@ namespace System.IO.Tests
             Assert.False(HasNonZeroNanoseconds(output.LastWriteTime));
         }
 
-        [ConditionalFact(nameof(isHFS))]
-        public void MoveToMillisecondPresent_HFS()
+        [ConditionalFact(nameof(LowTemporalResolution))]
+        public void MoveToMillisecondPresent_LowTempRes()
         {
             FileInfo input = new FileInfo(GetTestFilePath());
             input.Create().Dispose();
@@ -161,8 +159,7 @@ namespace System.IO.Tests
             Assert.Equal(0, output.LastWriteTime.Millisecond);
         }
 
-        [ConditionalFact(nameof(isNotHFS))]
-        [ActiveIssue("https://github.com/dotnet/runtime/issues/40530", TestPlatforms.Browser)]
+        [ConditionalFact(nameof(HighTemporalResolution))]
         public void MoveToMillisecondPresent()
         {
             FileInfo input = GetNonZeroMilliseconds();
@@ -173,8 +170,8 @@ namespace System.IO.Tests
             Assert.NotEqual(0, output.LastWriteTime.Millisecond);
         }
 
-        [ConditionalFact(nameof(isHFS))]
-        public void CopyToMillisecondPresent_HFS()
+        [ConditionalFact(nameof(LowTemporalResolution))]
+        public void CopyToMillisecondPresent_LowTempRes()
         {
             FileInfo input = new FileInfo(GetTestFilePath());
             input.Create().Dispose();
