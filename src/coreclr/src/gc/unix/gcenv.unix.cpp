d9da30eae0bca0c816a5b29c8e3ab25399a1ba5c
Manish Godse <61718172+mangod9@users.noreply.github.com>
2020-08-28 16:54:36 -0700

    fixing copy-paste error

--- src/coreclr/src/gc/unix/gcenv.unix.cpp
+++ src/coreclr/src/gc/unix/gcenv.unix.cpp
@@ -834,7 +834,7 @@ static size_t GetLogicalProcessorCacheSizeFromOS()
     cacheSize = std::max(cacheSize, ( size_t) sysconf(_SC_LEVEL4_CACHE_SIZE));
 #endif
 
-#if defined(TARGET_LINUX)
+#if defined(TARGET_LINUX) && !defined(HOST_ARM)
     if (cacheSize == 0)
     {
         //
@@ -857,7 +857,7 @@ static size_t GetLogicalProcessorCacheSizeFromOS()
     }
 #endif
 
-#if defined(TARGET_LINUX) && !defined(HOST_ARM)
+#if defined(HOST_ARM64)
     if (cacheSize == 0)
     {
         // It is currently expected to be missing cache size info
