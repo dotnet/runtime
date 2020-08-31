0004ca60b74703b6331fd3ec1a10b3a5dce31a9e
Jan Jahoda <aik.jahoda@post.cz>
2020-08-31 13:02:11 +0200

    Update src/libraries/Common/src/System/Obsoletions.cs
    Co-authored-by: Adeel Mujahid <adeelbm@outlook.com>

--- src/libraries/Common/src/System/Obsoletions.cs
+++ src/libraries/Common/src/System/Obsoletions.cs
@@ -43,7 +43,7 @@ namespace System
         internal const string CodeBaseMessage = "Assembly.CodeBase and Assembly.EscapedCodeBase are only included for .NET Framework compatibility. Use Assembly.Location instead.";
         internal const string CodeBaseDiagId = "SYSLIB0012";
 
-        internal const string EscapeUriStringMessage = "Uri.EscapeUriString can corrupt the Uri string in some cases. Consider using Uri.EscapeDataString instead.";
+        internal const string EscapeUriStringMessage = "Uri.EscapeUriString can corrupt the Uri string in some cases. Consider using Uri.EscapeDataString only for the query string component instead.";
         internal const string EscapeUriStringDiagId = "SYSLIB0013";
 
         internal const string WebRequestMessage = "Use HttpClient instead.";
