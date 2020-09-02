4ac552ac73cc77131a49ba960c74de8e9208f973
Jon Fortescue <jofortes@microsoft.com>
2020-09-02 10:16:44 -0700

    remove some imports

--- eng/pipelines/download-artifacts.ps1
+++ eng/pipelines/download-artifacts.ps1
@@ -3,5 +3,7 @@ Set-PSDebug -Trace 1
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $uri = "https://runtimesigningtest.blob.core.windows.net/runtimesigningtest/IntermediateUnsignedArtifacts_minimal.zip";
 $zipLocation = "$env:AGENT_TEMPDIRECTORY\IntermediateUnsignedArtifacts.zip";
+Write-Host "ZipLocation: $zipLocation"
 Invoke-WebRequest -Uri $uri -OutFile $zipLocation;
+Write-Host "DestinationPath: $env:BUILD_SOURCESDIRECTORY\artifacts\PackageDownload"
 Expand-Archive -Path $zipLocation -DestinationPath "$env:BUILD_SOURCESDIRECTORY\artifacts\PackageDownload";
