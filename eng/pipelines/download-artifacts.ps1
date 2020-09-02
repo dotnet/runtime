caa84c6732f492928cd3d0cc174118e87b0fc843
Jon Fortescue <jofortes@microsoft.com>
2020-09-01 19:28:11 -0700

    a smaller package

--- eng/pipelines/download-artifacts.ps1
+++ eng/pipelines/download-artifacts.ps1
@@ -1,5 +1,7 @@
+Set-PSDebug -Trace 1
+
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$uri = "https://runtimesigningtest.blob.core.windows.net/runtimesigningtest/IntermediateUnsignedArtifacts.zip";
+$uri = "https://runtimesigningtest.blob.core.windows.net/runtimesigningtest/IntermediateUnsignedArtifacts_minimal.zip";
 $zipLocation = "$env:AGENT_TEMPDIRECTORY\IntermediateUnsignedArtifacts.zip";
 Invoke-WebRequest -Uri $uri -OutFile $zipLocation;
 Expand-Archive -Path $zipLocation -DestinationPath "$env:BUILD_SOURCESDIRECTORY\artifacts\PackageDownload";
