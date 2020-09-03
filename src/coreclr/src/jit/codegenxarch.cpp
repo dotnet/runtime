ddb338220257dae240d7c9fa1166842695b52288
Egor Bogatov <egorbo@gmail.com>
2020-09-03 19:05:45 +0300

    Update codegenxarch.cpp

--- src/coreclr/src/jit/codegenxarch.cpp
+++ src/coreclr/src/jit/codegenxarch.cpp
@@ -4085,7 +4085,7 @@ void CodeGen::genCodeForShift(GenTree* tree)
         {
             int typeWidth    = genTypeSize(genActualType(targetType)) * 8;
             int shiftByValue = (int)shiftBy->AsIntConCommon()->IconValue();
-            if (compiler->compExactlyDependsOn(InstructionSet_BMI2) && tree->OperIs(GT_ROL) && (shiftByValue > 0) &&
+            if (compiler->compOpportunisticallyDependsOn(InstructionSet_BMI2) && tree->OperIs(GT_ROL) && (shiftByValue > 0) &&
                 (shiftByValue < typeWidth))
             {
                 assert((typeWidth == 32) || (typeWidth == 64));
