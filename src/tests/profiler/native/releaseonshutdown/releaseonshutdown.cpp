d7b86998c9edfdc91e793a0c44993906ee4dd1e0
David Mason <davmason@microsoft.com>
2020-09-04 14:32:54 -0700

    fix casing for non-windows

--- src/tests/profiler/native/releaseonshutdown/releaseonshutdown.cpp
+++ src/tests/profiler/native/releaseonshutdown/releaseonshutdown.cpp
@@ -1,7 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#include "ReleaseOnShutdown.h"
+#include "releaseonshutdown.h"
 
 #ifdef WIN32
 #include <Windows.h>
