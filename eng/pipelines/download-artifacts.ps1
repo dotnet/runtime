406604e1d71351f2f96e5b37e287f34c947faf65
Jon Fortescue <jofortes@microsoft.com>
2020-09-03 17:33:38 -0700

    full build

--- eng/pipelines/download-artifacts.ps1
+++
@@ -1,9 +0,0 @@
-Set-PSDebug -Trace 1
-
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$uri = "https://runtimesigningtest.blob.core.windows.net/runtimesigningtest/IntermediateUnsignedArtifacts_infintesimal.zip";
-$zipLocation = "$env:AGENT_TEMPDIRECTORY\IntermediateUnsignedArtifacts.zip";
-Write-Host "ZipLocation: $zipLocation"
-Invoke-WebRequest -Uri $uri -OutFile $zipLocation;
-Write-Host "DestinationPath: $env:BUILD_SOURCESDIRECTORY\artifacts\PackageDownload"
-Expand-Archive -Path $zipLocation -DestinationPath "$env:BUILD_SOURCESDIRECTORY\artifacts\PackageDownload";
