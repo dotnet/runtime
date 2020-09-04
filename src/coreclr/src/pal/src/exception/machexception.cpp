03af208fa4e3c29260b0ea9671036d021c91494f
Steve MacLean <Steve.MacLean@Microsoft.com>
2020-09-03 18:06:07 -0700

    Only activate for ESR.EC == SVC

--- src/coreclr/src/pal/src/exception/machexception.cpp
+++ src/coreclr/src/pal/src/exception/machexception.cpp
@@ -1492,7 +1492,13 @@ InjectActivationInternal(CPalThread* pThread)
         static const int MaxHardwareExceptionVector = 31;
         if (ExceptionState.__trapno > MaxHardwareExceptionVector)
 #elif defined(HOST_ARM64)
-        // TBD // Inject the activation only if the thread doesn't have a pending hardware exception
+        NONPAL_TRACE("InjectActivationInternal IsHardwareException? far %016llx esr %08x exception %08x\n",
+            ExceptionState.__far,
+            ExceptionState.__esr,
+            ExceptionState.__exception);
+        // Inject the activation only if the last ESR was an SVC and therefore the thread doesn't have
+        // a pending hardware exception
+        if ((ExceptionState.__esr >> 26) == 0x15)
 #else
 #error Unexpected architecture
 #endif
