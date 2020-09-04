edbaa230572bd51070ce630a935a2d45e4dd39f8
Ryan Lucia <rylucia@microsoft.com>
2020-09-04 11:01:01 -0400

    Take correct lock around mp

--- src/mono/mono/metadata/appdomain.c
+++ src/mono/mono/metadata/appdomain.c
@@ -1052,10 +1052,11 @@ gboolean
 mono_domain_owns_vtable_slot (MonoDomain *domain, gpointer vtable_slot)
 {
 	gboolean res;
+	MonoMemoryManager *memory_manager = mono_domain_ambient_memory_manager (domain);
 
-	mono_domain_lock (domain);
-	res = mono_mempool_contains_addr (mono_domain_ambient_memory_manager (domain)->mp, vtable_slot);
-	mono_domain_unlock (domain);
+	mono_memory_manager_lock (memory_manager);
+	res = mono_mempool_contains_addr (memory_manager->mp, vtable_slot);
+	mono_memory_manager_unlock (memory_manager);
 	return res;
 }
 
