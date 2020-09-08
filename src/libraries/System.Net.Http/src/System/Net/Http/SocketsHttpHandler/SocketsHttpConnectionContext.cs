715208625855d296d279fa62ab2e955b347e97cc
Geoffrey Kizer <geoffrek@windows.microsoft.com>
2020-09-08 11:42:59 -0700

    add ConnectionContext

---
+++ src/libraries/System.Net.Http/src/System/Net/Http/SocketsHttpHandler/SocketsHttpConnectionContext.cs
@@ -0,0 +1,21 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Net.Http
+{
+    public sealed class SocketsHttpConnectionContext
+    {
+        private readonly DnsEndPoint _dnsEndPoint;
+        private readonly HttpRequestMessage _requestMessage;
+
+        internal SocketsHttpConnectionContext(DnsEndPoint dnsEndPoint, HttpRequestMessage requestMessage)
+        {
+            _dnsEndPoint = dnsEndPoint;
+            _requestMessage = requestMessage;
+        }
+
+        public DnsEndPoint DnsEndPoint => _dnsEndPoint;
+
+        public HttpRequestMessage HttpRequestMessage => _requestMessage;
+    }
+}
