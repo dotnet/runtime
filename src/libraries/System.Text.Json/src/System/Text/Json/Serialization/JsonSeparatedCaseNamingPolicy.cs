869fa14c4df8806317b388b1b3a98232029bfc2d
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2020-09-09 17:24:12 +0800

    Copy snake case implementation from Json.NET.

---
+++ src/libraries/System.Text.Json/src/System/Text/Json/Serialization/JsonSeparatedCaseNamingPolicy.cs
@@ -0,0 +1,82 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Text.Json
+{
+    internal abstract class JsonSeparatedCaseNamingPolicy : JsonNamingPolicy
+    {
+        private enum SeparatedCaseState
+        {
+            Start,
+            NewWord,
+            Upper,
+            Lower
+        }
+
+        protected static string ToSeparatedCase(string s, char separator)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return s;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            SeparatedCaseState state = SeparatedCaseState.Start;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == ' ')
+                {
+                    if (state != SeparatedCaseState.Start)
+                    {
+                        state = SeparatedCaseState.NewWord;
+                    }
+                }
+                else if (char.IsUpper(s[i]))
+                {
+                    switch (state)
+                    {
+                        case SeparatedCaseState.Upper:
+                            bool hasNext = (i + 1 < s.Length);
+                            if (i > 0 && hasNext)
+                            {
+                                char nextChar = s[i + 1];
+                                if (!char.IsUpper(nextChar) && nextChar != separator)
+                                {
+                                    sb.Append(separator);
+                                }
+                            }
+                            break;
+                        case SeparatedCaseState.Lower:
+                        case SeparatedCaseState.NewWord:
+                            sb.Append(separator);
+                            break;
+                    }
+
+                    char c;
+                    c = char.ToLowerInvariant(s[i]);
+                    sb.Append(c);
+
+                    state = SeparatedCaseState.Upper;
+                }
+                else if (s[i] == separator)
+                {
+                    sb.Append(separator);
+                    state = SeparatedCaseState.Start;
+                }
+                else
+                {
+                    if (state == SeparatedCaseState.NewWord)
+                    {
+                        sb.Append(separator);
+                    }
+
+                    sb.Append(s[i]);
+                    state = SeparatedCaseState.Lower;
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
