dotnet/runtime

commit cfd60300ced9acd1bfa417d89cfd9f0136ec07e2

Delete multi-byte related functions from PAL (#41611)
* Delete multi-byte related functions from PAL

In PAL, delete:
* `IsDBCSLeadByte`; unimplemented.
* `IsDBCSLeadByteEx`; no-op.
* `GetCPInfo`; has hardcoded length of 4, and the only usage checks for
  `length == 1`.
* `_mbsinc`; has one usage in ilasm, which is simplified for Unix
  (after the removal of above).
* `_mbsninc`; unused.
* `_mbsdec`; usage is inlined as `p--` because `IsDBCSLeadByteEx` is a
  no-op.
* `GetCaseInsensitiveValueA`, handle ANSI cases as Unicode.
* `IsTextUnicode`, single usage in ilasm is inlined.

* Delete _MBCS, _ismbblead and simplifications

* Delete trailing whitespaces from PR file changed

src/coreclr/src/ilasm/grammar_after.cpp
@@ -76,11 +76,6 @@ static Keywords keywords[] = {
 /********************************************************************************/
 /* File encoding-dependent functions */
 /*--------------------------------------------------------------------------*/
-char* nextcharA(__in __nullterminated char* pos)
-{
-    return (*pos > 0) ? ++pos : (char *)_mbsinc((const unsigned char *)pos);
-}
-
 char* nextcharU(__in __nullterminated char* pos)
 {
     return ++pos;
@@ -929,14 +924,10 @@ Its_An_Id:
                                     tok = parse_literal(curSym, curPos, FALSE);
                                     if(tok == QSTRING)
                                     {
-                                        // if not ANSI, then string is in UTF-8,
-                                        // insert prefix
-                                        if(nextchar != nextcharA)
-                                        {
-                                            yylval.binstr->insertInt8(0xEF);
-                                            yylval.binstr->insertInt8(0xBB);
-                                            yylval.binstr->insertInt8(0xBF);
-                                        }
+                                        // insert UTF-8 BOM prefix
+                                        yylval.binstr->insertInt8(0xEF);
+                                        yylval.binstr->insertInt8(0xBB);
+                                        yylval.binstr->insertInt8(0xBF);
                                         yylval.binstr->appendInt8(' ');
                                         DefineVar(newstr, yylval.binstr);
                                     }
@@ -1488,12 +1479,6 @@ AsmParse::~AsmParse()
 }
 
 /**************************************************************************/
-DWORD AsmParse::IsItUnicode(CONST LPVOID pBuff, int cb, LPINT lpi)
-{
-    return IsTextUnicode(pBuff,cb,lpi);
-}
-
-/**************************************************************************/
 void AsmParse::CreateEnvironment(ReadStream* stream)
 {
     penv = new PARSING_ENVIRONMENT;
@@ -1520,19 +1505,21 @@ void AsmParse::ParseFile(ReadStream* stream)
 char* AsmParse::fillBuff(__in_opt __nullterminated char* pos)
 {
     int iPutToBuffer;
-    int iOptions = IS_TEXT_UNICODE_UNICODE_MASK;
-    g_uCodePage = CP_ACP;
+    g_uCodePage = CP_UTF8;
     iPutToBuffer = (int)penv->in->getAll(&(penv->curPos));
 
     penv->endPos = penv->curPos + iPutToBuffer;
     if(iPutToBuffer > 128) iPutToBuffer = 128;
-    if(IsItUnicode(penv->curPos,iPutToBuffer,&iOptions))
+    if(iPutToBuffer >= 4 && (penv->curPos[0] & 0xFF) == 0xFF && (penv->curPos[1] & 0xFF) == 0xFE)
     {
-        g_uCodePage = CP_UTF8;
-        if(iOptions & IS_TEXT_UNICODE_SIGNATURE)
+        // U+FFFE followed by U+0000 is UTF-32 LE, any other value than 0 is a true UTF-16 LE
+        if((penv->curPos[2] & 0xFF) == 0x00 && (penv->curPos[3] & 0xFF) == 0x00)
         {
-            penv->curPos += 2;
+            error("UTF-32 LE is not supported\n\n");
+            return NULL;
         }
+
+        penv->curPos += 2; // skip signature
         if(assem->m_fReportProgress) printf("Source file is UNICODE\n\n");
         penv->pfn_Sym = SymW;
         penv->pfn_nextchar = nextcharW;
@@ -1542,18 +1529,13 @@ char* AsmParse::fillBuff(__in_opt __nullterminated char* pos)
     }
     else
     {
-        if(((penv->curPos[0]&0xFF)==0xEF)&&((penv->curPos[1]&0xFF)==0xBB)&&((penv->curPos[2]&0xFF)==0xBF))
+        if((penv->curPos[0] & 0xFF) == 0xEF && (penv->curPos[1] & 0xFF) == 0xBB && (penv->curPos[2] & 0xFF) == 0xBF)
         {
-            g_uCodePage = CP_UTF8;
             penv->curPos += 3;
-            if(assem->m_fReportProgress) printf("Source file is UTF-8\n\n");
-            penv->pfn_nextchar = nextcharU;
         }
-        else
-        {
-            if(assem->m_fReportProgress) printf("Source file is ANSI\n\n");
-            penv->pfn_nextchar = nextcharA;
-        }
+
+        if(assem->m_fReportProgress) printf("Source file is UTF-8\n\n");
+        penv->pfn_nextchar = nextcharU;
         penv->pfn_Sym = SymAU;
         penv->pfn_NewStrFromToken = NewStrFromTokenAU;
         penv->pfn_NewStaticStrFromToken = NewStaticStrFromTokenAU;

src/coreclr/src/dlls/mscordac/mscordac_unixexports.src
@@ -111,7 +111,6 @@ nativeStringResourceTable_mscorrc
 #FreeLibrary
 #FileTimeToSystemTime
 #GetACP
-#GetCPInfo
 #GetCurrentDirectoryW
 #GetCurrentProcess
 #GetCurrentProcessId
@@ -134,7 +133,6 @@ nativeStringResourceTable_mscorrc
 #GetTempPathA
 #GetTempPathW
 #InitializeCriticalSection
-#IsDBCSLeadByte
 #LeaveCriticalSection
 #LoadLibraryA
 #LoadLibraryW

src/coreclr/src/ilasm/asmparse.h
@@ -214,7 +214,6 @@ typedef LIFO<ARG_NAME_LIST> ARG_NAME_LIST_STACK;
 /*--------------------------------------------------------------------------*/
 typedef char*(*PFN_NEXTCHAR)(char*);
 
-char* nextcharA(__in __nullterminated char* pos);
 char* nextcharU(__in __nullterminated char* pos);
 char* nextcharW(__in __nullterminated char* pos);
 
@@ -292,7 +291,7 @@ public:
     ARG_NAME_LIST_STACK  m_ANSFirst;
     ARG_NAME_LIST_STACK  m_ANSLast;
     PARSING_ENVIRONMENT *penv;
-    PARSING_ENVIRONMENT_STACK   PEStack;
+    PARSING_ENVIRONMENT_STACK PEStack;
 
 private:
     BinStr* MakeSig(unsigned callConv, BinStr* retType, BinStr* args, int ntyargs = 0);
@@ -300,9 +299,8 @@ private:
     BinStr* MakeTypeArray(CorElementType kind, BinStr* elemType, BinStr* bounds);
 
     char* fillBuff(__in_opt __nullterminated char* curPos);   // refill the input buffer
-    DWORD IsItUnicode(CONST LPVOID pBuff, int cb, LPINT lpi);
-	HANDLE	hstdout;
-	HANDLE	hstderr;
+    HANDLE hstdout;
+    HANDLE hstderr;
 
 private:
     friend void yyerror(__in __nullterminated const char* str);

src/coreclr/src/ilasm/ilasmpch.h
@@ -18,7 +18,6 @@
 
 #include "specstrings.h"
 #include <string.h>             // for strcmp
-#include <mbstring.h>           // for _mbsinc
 #include <ctype.h>                      // for isspace
 #include "openum.h"             // for CEE_*
 #include <stdarg.h>         // for vararg macros

src/coreclr/src/pal/inc/pal.h
@@ -2812,26 +2812,6 @@ typedef struct _cpinfo {
     BYTE LeadByte[MAX_LEADBYTES];
 } CPINFO, *LPCPINFO;
 
-PALIMPORT
-BOOL
-PALAPI
-GetCPInfo(
-      IN UINT CodePage,
-      OUT LPCPINFO lpCPInfo);
-
-PALIMPORT
-BOOL
-PALAPI
-IsDBCSLeadByteEx(
-         IN UINT CodePage,
-         IN BYTE TestChar);
-
-PALIMPORT
-BOOL
-PALAPI
-IsDBCSLeadByte(
-        IN BYTE TestChar);
-
 #define MB_PRECOMPOSED            0x00000001
 #define MB_ERR_INVALID_CHARS      0x00000008
 
@@ -4021,9 +4001,6 @@ PALIMPORT DLLEXPORT int __cdecl _stricmp(const char *, const char *);
 PALIMPORT DLLEXPORT int __cdecl vsprintf_s(char *, size_t, const char *, va_list);
 PALIMPORT char * __cdecl _gcvt_s(char *, int, double, int);
 PALIMPORT int __cdecl __iscsym(int);
-PALIMPORT unsigned char * __cdecl _mbsinc(const unsigned char *);
-PALIMPORT unsigned char * __cdecl _mbsninc(const unsigned char *, size_t);
-PALIMPORT unsigned char * __cdecl _mbsdec(const unsigned char *, const unsigned char *);
 PALIMPORT DLLEXPORT int __cdecl _wcsicmp(const WCHAR *, const WCHAR*);
 PALIMPORT int __cdecl _wcsnicmp(const WCHAR *, const WCHAR *, size_t);
 PALIMPORT int __cdecl _vsnprintf(char *, size_t, const char *, va_list);

src/coreclr/src/pal/inc/rt/palrt.h
@@ -708,10 +708,6 @@ STDAPI_(LPWSTR) StrCatBuffW(LPWSTR pszDest, LPCWSTR pszSrc, int cchDestBuffSize)
 #define _SAFECRT_SET_ERRNO 0
 #define _SAFECRT_DEFINE_MBS_FUNCTIONS 0
 #define _SAFECRT_DEFINE_TCS_MACROS 1
-/*
-#define _SAFECRT__ISMBBLEAD(_Character) 0
-#define _SAFECRT__MBSDEC(_String, _Current) (_Current - 1)
-*/
 #include "safecrt.h"
 #include "specstrings.h"
 
@@ -875,8 +871,6 @@ typedef HANDLE HWND;
 
 #define IS_TEXT_UNICODE_SIGNATURE             0x0008
 #define IS_TEXT_UNICODE_UNICODE_MASK          0x000F
-
-BOOL IsTextUnicode(CONST VOID* lpv, int iSize, LPINT lpiResult);
 
 typedef struct _LIST_ENTRY {
    struct _LIST_ENTRY *Flink;

src/coreclr/src/pal/src/CMakeLists.txt
@@ -123,7 +123,6 @@ set(SOURCES
   cruntime/filecrt.cpp
   cruntime/malloc.cpp
   cruntime/math.cpp
-  cruntime/mbstring.cpp
   cruntime/misc.cpp
   cruntime/path.cpp
   cruntime/printf.cpp
@@ -246,12 +245,12 @@ if(CLR_CMAKE_TARGET_OSX)
     ${LIBUNWIND_DAC_OBJECTS}
   )
 
-  target_compile_definitions(coreclrpal_dac PUBLIC 
+  target_compile_definitions(coreclrpal_dac PUBLIC
     "-D_XOPEN_SOURCE"
     "-DUNW_REMOTE_ONLY"
   )
 
-  target_include_directories(coreclrpal_dac PUBLIC 
+  target_include_directories(coreclrpal_dac PUBLIC
     ${CMAKE_CURRENT_SOURCE_DIR}/libunwind/include
     ${CMAKE_CURRENT_SOURCE_DIR}/libunwind/include/tdep
     ${CMAKE_CURRENT_BINARY_DIR}

commit b18c93fce20796c6acac9a369a294436d46324a2

Fix ilasm parsing of defines
Regression introduced by #41611

Fixes #42001

src/coreclr/src/ilasm/grammar_after.cpp
@@ -924,10 +924,6 @@ Its_An_Id:
                                     tok = parse_literal(curSym, curPos, FALSE);
                                     if(tok == QSTRING)
                                     {
-                                        // insert UTF-8 BOM prefix
-                                        yylval.binstr->insertInt8(0xEF);
-                                        yylval.binstr->insertInt8(0xBB);
-                                        yylval.binstr->insertInt8(0xBF);
                                         yylval.binstr->appendInt8(' ');
                                         DefineVar(newstr, yylval.binstr);
                                     }