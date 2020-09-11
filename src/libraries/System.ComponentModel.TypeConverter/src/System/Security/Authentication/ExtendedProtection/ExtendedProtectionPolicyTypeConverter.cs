320a99eb3e8b5c09f34a52ade3a3e24f264a352b
Mitchell Hwang <mitchhwang1418@gmail.com>
2020-09-11 14:46:12 -0400

    [release/5.0-rc2] Backport Browser Unsupported Attribute Changes Properly (#42071)
    Co-authored-by: Mitchell Hwang <mitchell.hwang@microsoft.com>
    Co-authored-by: Maxim Lipnin <v-maxlip@microsoft.com>

--- src/libraries/System.ComponentModel.TypeConverter/src/System/Security/Authentication/ExtendedProtection/ExtendedProtectionPolicyTypeConverter.cs
+++ src/libraries/System.ComponentModel.TypeConverter/src/System/Security/Authentication/ExtendedProtection/ExtendedProtectionPolicyTypeConverter.cs
@@ -6,6 +6,7 @@ using System.ComponentModel;
 using System.ComponentModel.Design.Serialization;
 using System.Globalization;
 using System.Reflection;
+using System.Runtime.Versioning;
 
 namespace System.Security.Authentication.ExtendedProtection
 {
@@ -16,6 +17,7 @@ namespace System.Security.Authentication.ExtendedProtection
             return destinationType == typeof(InstanceDescriptor) || base.CanConvertTo(context, destinationType);
         }
 
+        [UnsupportedOSPlatform("browser")]
         public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
         {
             if (destinationType == typeof(InstanceDescriptor))
