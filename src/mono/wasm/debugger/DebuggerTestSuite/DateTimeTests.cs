19ccbde8625fdf66faf4af2f2920966e22423cd3
Thays Grazia <thaystg@gmail.com>
2020-09-11 16:39:06 -0300

    Changing what @radical suggested.

--- src/mono/wasm/debugger/DebuggerTestSuite/DateTimeTests.cs
+++ src/mono/wasm/debugger/DebuggerTestSuite/DateTimeTests.cs
@@ -13,9 +13,9 @@ namespace DebuggerTests
 
         [Theory]
         [InlineData("en-US", "dddd, MMMM d, yyyy h:mm:ss tt", "dddd, MMMM d, yyyy", "h:mm:ss tt", "M/d/yyyy", "h:mm tt")]
-        [InlineData ("ja-JP", "yyyy年M月d日dddd H:mm:ss", "yyyy年M月d日dddd", "H:mm:ss", "yyyy/MM/dd", "H:mm")]
-        [InlineData ("es-ES", "dddd, d 'de' MMMM 'de' yyyy H:mm:ss", "dddd, d 'de' MMMM 'de' yyyy", "H:mm:ss", "d/M/yyyy", "H:mm")]
-        [InlineData ("de-DE", "dddd, d. MMMM yyyy HH:mm:ss", "dddd, d. MMMM yyyy", "HH:mm:ss", "dd.MM.yyyy", "HH:mm")]
+        [InlineData("ja-JP", "yyyy年M月d日dddd H:mm:ss", "yyyy年M月d日dddd", "H:mm:ss", "yyyy/MM/dd", "H:mm")]
+        [InlineData("es-ES", "dddd, d 'de' MMMM 'de' yyyy H:mm:ss", "dddd, d 'de' MMMM 'de' yyyy", "H:mm:ss", "d/M/yyyy", "H:mm")]
+        [InlineData("de-DE", "dddd, d. MMMM yyyy HH:mm:ss", "dddd, d. MMMM yyyy", "HH:mm:ss", "dd.MM.yyyy", "HH:mm")]
         public async Task CheckDateTimeLocale(string locale, string fdtp, string ldp, string ltp, string sdp, string stp)
         {
             var insp = new Inspector();
@@ -34,18 +34,20 @@ namespace DebuggerTests
                     $"'{locale}'); }}, 1);",
                     debugger_test_loc, 25, 12, "LocaleTest",
                     locals_fn: async (locals) =>
-                   {
-                       DateTimeFormatInfo dtfi = CultureInfo.GetCultureInfo(locale).DateTimeFormat;
-                       CultureInfo.CurrentCulture = new CultureInfo(locale, false);
-                       DateTime dt = new DateTime(2020, 1, 2, 3, 4, 5);
+                    {
+                        DateTimeFormatInfo dtfi = CultureInfo.GetCultureInfo(locale).DateTimeFormat;
+                        CultureInfo.CurrentCulture = new CultureInfo(locale, false);
 
-                       CheckString(locals, "fdtp", fdtp);
-                       CheckString(locals, "ldp", ldp);
-                       CheckString(locals, "ltp", ltp);
-                       CheckString(locals, "sdp", sdp);
-                       CheckString(locals, "stp", stp);
-                       await CheckDateTime(locals, "dt", dt);
-                   }
+                        await CheckProps(locals, new
+                        {
+                            fdtp = TString(fdtp),
+                            ldp = TString(ldp),
+                            ltp = TString(ltp),
+                            sdp = TString(sdp),
+                            stp = TString(stp),
+                            dt = TDateTime(new DateTime(2020, 1, 2, 3, 4, 5))
+                        }, "locals", num_fields: 8);
+                    }
                 );
 
             });
