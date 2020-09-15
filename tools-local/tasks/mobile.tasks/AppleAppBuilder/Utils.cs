b5c6eed0dbd9dd980294f072bca6c458d3c33106
Stephen Toub <stoub@microsoft.com>
2020-09-15 15:37:11 -0400

    Fix analyzer warnings in additional projects

--- tools-local/tasks/mobile.tasks/AppleAppBuilder/Utils.cs
+++ tools-local/tasks/mobile.tasks/AppleAppBuilder/Utils.cs
@@ -72,7 +72,7 @@ internal class Utils
             throw new Exception("Error: " + errorBuilder);
         }
 
-        return outputBuilder.ToString().Trim('\r','\n');
+        return outputBuilder.ToString().Trim('\r', '\n');
     }
 
     public static TaskLoggingHelper? Logger { get; set; }
