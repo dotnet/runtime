1d99b5036655a129d1017d463146fc9313ce6eb3
David Wrighton <davidwr@microsoft.com>
2020-09-14 17:34:49 -0700

    Attempt to fix both permissions and launch issues for the helix command

--- src/coreclr/src/pal/tests/palsuite/runpaltestshelix.sh
+++ src/coreclr/src/pal/tests/palsuite/runpaltestshelix.sh
@@ -5,7 +5,7 @@
 cd $HELIX_WORKITEM_ROOT
 
 TEST_OUTPUT_DIR_HELIX=$HELIX_WORKITEM_ROOT/testoutput
-./runpaltests.sh $HELIX_WORKITEM_ROOT $HELIX_WORKITEM_ROOT/testoutput $HELIX_WORKITEM_ROOT/testoutputtmp
+$HELIX_WORKITEM_ROOT/runpaltests.sh $HELIX_WORKITEM_ROOT $HELIX_WORKITEM_ROOT/testoutput $HELIX_WORKITEM_ROOT/testoutputtmp
 exit_code_paltests=$?
-cp $TEST_OUTPUT_DIR_HELIX/pal_tests.xml testResults.xml
+cp $TEST_OUTPUT_DIR_HELIX/pal_tests.xml $HELIX_WORKITEM_ROOT/testResults.xml
 exit $exit_code_paltests
