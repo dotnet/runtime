329bc8313d17e1a48f67684b2f58620bfd20aca0
devsko <devsko@outlook.com>
2020-09-16 17:27:36 +0200

    Test with original repro data from #42070

--- src/libraries/System.Text.Json/tests/Serialization/StreamTests.cs
+++ src/libraries/System.Text.Json/tests/Serialization/StreamTests.cs
@@ -364,5 +364,132 @@ namespace System.Text.Json.Serialization.Tests
             public string A { get; set; }
             public int B { get; set; }
         }
+
+        // From https://github.com/dotnet/runtime/issues/42070
+        [Theory]
+        [InlineData("Issue42070_0004.json")]
+        [InlineData("Issue42070_0228.json")]
+        public static async Task ContinuationAtNullToken(string resourceName)
+        {
+            using (Stream stream = typeof(StreamTests).Assembly.GetManifestResourceStream("System.Text.Json.Tests.Resources." + resourceName))
+            {
+                CustomerCollectionResponse response = await JsonSerializer.DeserializeAsync<CustomerCollectionResponse>(stream, new JsonSerializerOptions { IgnoreNullValues = true });
+                Assert.Equal(50, response.Customers.Count);
+            }
+        }
+
+        private class CustomerCollectionResponse
+        {
+            [JsonPropertyName("customers")]
+            public List<Customer> Customers { get; set; }
+        }
+
+        private class CustomerAddress
+        {
+            [JsonPropertyName("first_name")]
+            public string FirstName { get; set; }
+
+            [JsonPropertyName("address1")]
+            public string Address1 { get; set; }
+
+            [JsonPropertyName("phone")]
+            public string Phone { get; set; }
+
+            [JsonPropertyName("city")]
+            public string City { get; set; }
+
+            [JsonPropertyName("zip")]
+            public string Zip { get; set; }
+
+            [JsonPropertyName("province")]
+            public string Province { get; set; }
+
+            [JsonPropertyName("country")]
+            public string Country { get; set; }
+
+            [JsonPropertyName("last_name")]
+            public string LastName { get; set; }
+
+            [JsonPropertyName("address2")]
+            public string Address2 { get; set; }
+
+            [JsonPropertyName("company")]
+            public string Company { get; set; }
+
+            [JsonPropertyName("latitude")]
+            public float? Latitude { get; set; }
+
+            [JsonPropertyName("longitude")]
+            public float? Longitude { get; set; }
+
+            [JsonPropertyName("name")]
+            public string Name { get; set; }
+
+            [JsonPropertyName("country_code")]
+            public string CountryCode { get; set; }
+
+            [JsonPropertyName("province_code")]
+            public string ProvinceCode { get; set; }
+        }
+
+        private class Customer
+        {
+            [JsonPropertyName("id")]
+            public long Id { get; set; }
+
+            [JsonPropertyName("email")]
+            public string Email { get; set; }
+
+            [JsonPropertyName("accepts_marketing")]
+            public bool AcceptsMarketing { get; set; }
+
+            [JsonPropertyName("created_at")]
+            public DateTimeOffset? CreatedAt { get; set; }
+
+            [JsonPropertyName("updated_at")]
+            public DateTimeOffset? UpdatedAt { get; set; }
+
+            [JsonPropertyName("first_name")]
+            public string FirstName { get; set; }
+
+            [JsonPropertyName("last_name")]
+            public string LastName { get; set; }
+
+            [JsonPropertyName("orders_count")]
+            public int OrdersCount { get; set; }
+
+            [JsonPropertyName("state")]
+            public string State { get; set; }
+
+            [JsonPropertyName("total_spent")]
+            public string TotalSpent { get; set; }
+
+            [JsonPropertyName("last_order_id")]
+            public long? LastOrderId { get; set; }
+
+            [JsonPropertyName("note")]
+            public string Note { get; set; }
+
+            [JsonPropertyName("verified_email")]
+            public bool VerifiedEmail { get; set; }
+
+            [JsonPropertyName("multipass_identifier")]
+            public string MultipassIdentifier { get; set; }
+
+            [JsonPropertyName("tax_exempt")]
+            public bool TaxExempt { get; set; }
+
+            [JsonPropertyName("tags")]
+            public string Tags { get; set; }
+
+            [JsonPropertyName("last_order_name")]
+            public string LastOrderName { get; set; }
+
+            [JsonPropertyName("default_address")]
+            public CustomerAddress DefaultAddress { get; set; }
+
+            [JsonPropertyName("addresses")]
+            public IList<CustomerAddress> Addresses { get; set; }
+        }
     }
 }
