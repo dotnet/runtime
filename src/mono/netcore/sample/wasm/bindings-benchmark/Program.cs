3c29e856f145994740914335bca6a1590c36a38e
Katelyn Gadd <kg@luminance.org>
2020-09-16 18:24:06 -0700

    Revert sample and add separate proj for benchmark

--- src/mono/netcore/sample/wasm/bindings-benchmark/Program.cs
+++ src/mono/netcore/sample/wasm/bindings-benchmark/Program.cs
@@ -1,3 +1,4 @@
+// -*- indent-tabs-mode: nil -*-
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
@@ -8,7 +9,7 @@ namespace Sample
 {
     public class Test
     {
-        public static void Main(string[] args)
+        public static void Main(String[] args)
         {
             Console.WriteLine ("Hello, World!");
         }
@@ -17,6 +18,10 @@ namespace Sample
         public static int TestMeaning()
         {
             return 42;
+        }
+
+        public static float BenchmarkMethod (int i, float f, double d) {
+            return (float)(i + f + d);
         }
     }
 }
