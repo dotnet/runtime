e17335da42e2a3d638e12da3dd6c553650aecd10
Kunal Pathak <Kunal.Pathak@microsoft.com>
2020-09-16 22:31:08 -0700

    Remove commented code

--- eng/common/superpmi/superpmi-setup.ps1
+++ eng/common/superpmi/superpmi-setup.ps1
@@ -37,65 +37,6 @@ robocopy $CoreRootDirectory $PmiAssembliesDirectory\Core_Root /E
 
 New-Item -Path $WorkItemDirectory -Name "placeholder.txt" -ItemType "file" -Value "Placeholder file." -Force
 
-# # TODO: Implement a better logic to determine if Framework is .NET Core or >= .NET 5.
-# if ($Framework.StartsWith("netcoreapp") -or ($Framework -eq "net5.0")) {
-#     $Queue = "Windows.10.Amd64.ClientRS5.Open"
-# }
-
-# if ($Compare) {
-#     $Queue = "Windows.10.Amd64.19H1.Tiger.Perf.Open"
-#     $PerfLabArguments = ""
-#     $ExtraBenchmarkDotNetArguments = ""
-# }
-
-# if ($Internal) {
-#     $Queue = "Windows.10.Amd64.19H1.Tiger.Perf"
-#     $PerfLabArguments = "--upload-to-perflab-container"
-#     $ExtraBenchmarkDotNetArguments = ""
-#     $Creator = ""
-#     $HelixSourcePrefix = "official"
-# }
-
-# if($MonoInterpreter)
-# {
-#     $ExtraBenchmarkDotNetArguments = "--category-exclusion-filter NoInterpreter"
-# }
-
-# if($MonoDotnet -ne "")
-# {
-#     $Configurations += " LLVM=$LLVM MonoInterpreter=$MonoInterpreter MonoAOT=$MonoAOT"
-#     if($ExtraBenchmarkDotNetArguments -eq "")
-#     {
-#         #FIX ME: We need to block these tests as they don't run on mono for now
-#         $ExtraBenchmarkDotNetArguments = "--exclusion-filter *Perf_Image* *Perf_NamedPipeStream*"
-#     }
-#     else
-#     {
-#         #FIX ME: We need to block these tests as they don't run on mono for now
-#         $ExtraBenchmarkDotNetArguments += " --exclusion-filter *Perf_Image* *Perf_NamedPipeStream*"
-#     }
-# }
-
-# # FIX ME: This is a workaround until we get this from the actual pipeline
-# $CommonSetupArguments="--channel master --queue $Queue --build-number $BuildNumber --build-configs $Configurations --architecture $Architecture"
-# $SetupArguments = "--repository https://github.com/$Repository --branch $Branch --get-perf-hash --commit-sha $CommitSha $CommonSetupArguments"
-
-
-# #This grabs the LKG version number of dotnet and passes it to our scripts
-# $VersionJSON = Get-Content global.json | ConvertFrom-Json
-# $DotNetVersion = $VersionJSON.tools.dotnet
-# $SetupArguments = "--dotnet-versions $DotNetVersion $SetupArguments"
-
-
-# if ($RunFromPerformanceRepo) {
-#     $SetupArguments = "--perf-hash $CommitSha $CommonSetupArguments"
-    
-#     robocopy $SourceDirectory $PerformanceDirectory /E /XD $PayloadDirectory $SourceDirectory\artifacts $SourceDirectory\.git
-# }
-# else {
-#
-# }
-
 Write-Host "Cloning into JitUtilsDirectory"
 
 git clone --branch master --depth 1 --quiet https://github.com/dotnet/jitutils $JitUtilsDirectory
@@ -103,25 +44,6 @@ pushd $JitUtilsDirectory
 $env:PATH = "$SourceDirectory\.dotnet;$env:PATH"
 Write-Host "dotnet PATH: $env:PATH"
 .\bootstrap.cmd
-
-# if($MonoDotnet -ne "")
-# {
-#     $UsingMono = "true"
-#     $MonoDotnetPath = (Join-Path $PayloadDirectory "dotnet-mono")
-#     Move-Item -Path $MonoDotnet -Destination $MonoDotnetPath
-# }
-
-# if ($UseCoreRun) {
-#     $NewCoreRoot = (Join-Path $PayloadDirectory "Core_Root")
-#     Move-Item -Path $CoreRootDirectory -Destination $NewCoreRoot
-# }
-# if ($UseBaselineCoreRun) {
-#     $NewBaselineCoreRoot = (Join-Path $PayloadDirectory "Baseline_Core_Root")
-#     Move-Item -Path $BaselineCoreRootDirectory -Destination $NewBaselineCoreRoot
-# }
-
-# $DocsDir = (Join-Path $PerformanceDirectory "docs")
-# robocopy $DocsDir $WorkItemDirectory
 
 # Set variables that we will need to have in future steps
 $ci = $true
