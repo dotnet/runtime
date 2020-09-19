55c815a4752d687f57c14b80cc16a2abcc39d200
Adeel <adeelbm@outlook.com>
2020-09-19 15:54:16 +0300

    Remove workarounds from ParameterDefaultValue class

--- src/libraries/Common/src/Extensions/ParameterDefaultValue/ParameterDefaultValue.cs
+++ src/libraries/Common/src/Extensions/ParameterDefaultValue/ParameterDefaultValue.cs
@@ -4,66 +4,42 @@
 #nullable enable
 
 using System;
-using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 
 namespace Microsoft.Extensions.Internal
 {
     internal class ParameterDefaultValue
     {
-        private static readonly Type _nullable = typeof(Nullable<>);
-
         public static bool TryGetDefaultValue(ParameterInfo parameter, out object? defaultValue)
         {
-            bool hasDefaultValue;
-            bool tryToGetDefaultValue = true;
             defaultValue = null;
 
-            try
+            if (!parameter.HasDefaultValue)
             {
-                hasDefaultValue = parameter.HasDefaultValue;
-            }
-            catch (FormatException) when (parameter.ParameterType == typeof(DateTime))
-            {
-                // Workaround for https://github.com/dotnet/runtime/issues/18844
-                // If HasDefaultValue throws FormatException for DateTime
-                // we expect it to have default value
-                hasDefaultValue = true;
-                tryToGetDefaultValue = false;
+                return false;
             }
 
-            if (hasDefaultValue)
+            defaultValue = parameter.DefaultValue;
+
+            // Workaround for https://github.com/dotnet/runtime/issues/18599
+            if (defaultValue == null && parameter.ParameterType.IsValueType)
             {
-                if (tryToGetDefaultValue)
-                {
-                    defaultValue = parameter.DefaultValue;
-                }
+                defaultValue = Activator.CreateInstance(parameter.ParameterType);
+            }
 
-                // Workaround for https://github.com/dotnet/runtime/issues/18599
-                if (defaultValue == null && parameter.ParameterType.IsValueType)
+            // Handle nullable enums
+            if (defaultValue != null &&
+                parameter.ParameterType.IsGenericType &&
+                parameter.ParameterType.GetGenericTypeDefinition() == typeof(Nullable<>))
+            {
+                Type? underlyingType = Nullable.GetUnderlyingType(parameter.ParameterType);
+                if (underlyingType != null && underlyingType.IsEnum)
                 {
-                    defaultValue = CreateValueType(parameter.ParameterType);
-                }
-
-                [UnconditionalSuppressMessage("ReflectionAnalysis", "IL2067:UnrecognizedReflectionPattern",
-                    Justification = "CreateInstance is only called on a ValueType, which will always have a default constructor.")]
-                object? CreateValueType(Type t) => Activator.CreateInstance(t);
-
-                // Handle nullable enums
-                if (defaultValue != null &&
-                    parameter.ParameterType.IsGenericType &&
-                    parameter.ParameterType.GetGenericTypeDefinition() == _nullable
-                    )
-                {
-                    Type? underlyingType = Nullable.GetUnderlyingType(parameter.ParameterType);
-                    if (underlyingType != null && underlyingType.IsEnum)
-                    {
-                        defaultValue = Enum.ToObject(underlyingType, defaultValue);
-                    }
+                    defaultValue = Enum.ToObject(underlyingType, defaultValue);
                 }
             }
 
-            return hasDefaultValue;
+            return true;
         }
     }
 }
