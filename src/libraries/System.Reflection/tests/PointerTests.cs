82167b42d98369c38e5ff0da7199b8f8003eb032
Dimension4 <Dimension4@users.noreply.github.com>
2020-09-21 19:27:52 +0200

    Equals and GetHashCode for Reflection.Pointer
    If a pointer is accessed via reflection it is boxed into a
    `System.Reflection.Pointer`. This type didn't override `Equals` and
    `GetHashCode`, meaning instances are equal by reference.
    
    On the other hand, pointers are compared by their value. Since
    value types are equal by either memcmp or memberwise equality
    (via reflection), the outcome of an equality test of a pointer itself
    depends on the type that holds that pointer.
    
    Fix #42457

---
+++ src/libraries/System.Reflection/tests/PointerTests.cs
@@ -0,0 +1,60 @@
+﻿using Xunit;
+
+namespace System.Reflection.Tests
+{
+    public class PointerTests
+    {
+        public unsafe struct BitwiseComparable
+        {
+            public int* PublicInt;
+        }
+
+        public unsafe struct MemberwiseComparable
+        {
+            public string ReferenceType;
+            public int* PublicInt;
+        }
+
+        [Fact]
+        public unsafe void BitwiseEquality_AreEqual()
+        {
+            int someNumber = 1;
+            var a = new BitwiseComparable();
+            a.PublicInt = &someNumber;
+            var b = a;
+
+            Assert.True(a.Equals(b));
+        }
+
+        [Fact]
+        public unsafe void BitwiseEquality_EqualWithSelf()
+        {
+            int someNumber = 1;
+            var a = new BitwiseComparable();
+            a.PublicInt = &someNumber;
+
+            Assert.True(a.Equals(a));
+        }
+
+        [Fact]
+        public unsafe void MemberwiseEquality_AreEqual()
+        {
+            int someNumber = 1;
+            var a = new MemberwiseComparable();
+            a.PublicInt = &someNumber;
+            var b = a;
+
+            Assert.True(a.Equals(b));
+        }
+
+        [Fact]
+        public unsafe void MemberwiseEquality_EqualWithSelf()
+        {
+            int someNumber = 1;
+            var a = new MemberwiseComparable();
+            a.PublicInt = &someNumber;
+
+            Assert.True(a.Equals(a));
+        }
+    }
+}
