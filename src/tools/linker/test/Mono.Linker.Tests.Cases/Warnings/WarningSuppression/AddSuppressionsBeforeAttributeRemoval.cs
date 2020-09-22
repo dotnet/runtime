096a6ffda0211e1657bc21afc509873f6b96f586
brian-taylor-unity <52750961+brian-taylor-unity@users.noreply.github.com>
2020-09-22 11:32:55 -0400

    Remove remaining unused using directives from test cases for compatibility with UnityLinker (dotnet/linker#1502)
    Commit migrated from https://github.com/dotnet/linker/commit/fe430b87123dd2c0f864c35a2150cfc9f1cb8b8c

--- src/tools/linker/test/Mono.Linker.Tests.Cases/Warnings/WarningSuppression/AddSuppressionsBeforeAttributeRemoval.cs
+++ src/tools/linker/test/Mono.Linker.Tests.Cases/Warnings/WarningSuppression/AddSuppressionsBeforeAttributeRemoval.cs
@@ -1,13 +1,14 @@
 ﻿using Mono.Linker.Tests.Cases.Expectations.Assertions;
 using Mono.Linker.Tests.Cases.Expectations.Metadata;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq.Expressions;
-using System.Text;
 
 namespace Mono.Linker.Tests.Cases.Warnings.WarningSuppression
 {
+#if !ILLINK
+	[Reference ("System.Core.dll")]
+#endif
 	[SkipKeptItemsValidation]
 	[SetupLinkAttributesFile ("AddSuppressionsBeforeAttributeRemoval.xml")]
 	[LogDoesNotContain ("IL2067: Mono.Linker.Tests.Cases.Warnings.WarningSuppression.AddSuppressionsBeforeAttributeRemoval.Main()")]
