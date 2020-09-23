dotnet/runtime

commit ddc83ad2b6f0127f81486301185c57f8e9aac055

Fix JS<->WASM string marshaling crash (#42486)
Fix JS strings being truncated at the first null when passed to mono
Fix crashes when moving large strings across the JS<->WASM boundary

src/mono/wasm/runtime/binding_support.js
@@ -52,7 +52,7 @@ var BindingSupportLib = {
 			this.find_method = Module.cwrap ('mono_wasm_assembly_find_method', 'number', ['number', 'string', 'number']);
 			this.invoke_method = Module.cwrap ('mono_wasm_invoke_method', 'number', ['number', 'number', 'number', 'number']);
 			this.mono_string_get_utf8 = Module.cwrap ('mono_wasm_string_get_utf8', 'number', ['number']);
-			this.js_string_to_mono_string = Module.cwrap ('mono_wasm_string_from_js', 'number', ['string']);
+			this.mono_wasm_string_from_utf16 = Module.cwrap ('mono_wasm_string_from_utf16', 'number', ['number', 'number']);
 			this.mono_get_obj_type = Module.cwrap ('mono_wasm_get_obj_type', 'number', ['number']);
 			this.mono_unbox_int = Module.cwrap ('mono_unbox_int', 'number', ['number']);
 			this.mono_unbox_float = Module.cwrap ('mono_wasm_unbox_float', 'number', ['number']);
@@ -141,6 +141,17 @@ var BindingSupportLib = {
 			return this.call_method (this.is_simple_array, null, "mi", [ ele ]);
 		},
 
+		js_string_to_mono_string: function (string) {
+			var buffer = Module._malloc ((string.length + 1) * 2);
+			var buffer16 = (buffer / 2) | 0;
+			for (var i = 0; i < string.length; i++)
+				Module.HEAP16[buffer16 + i] = string.charCodeAt (i);
+			Module.HEAP16[buffer16 + string.length] = 0;
+			var result = this.mono_wasm_string_from_utf16 (buffer, string.length);
+			Module._free (buffer);
+			return result;
+		},
+		
 		mono_array_to_js_array: function (mono_array) {
 			if (mono_array === 0)
 				return null;

src/mono/wasm/runtime/driver.c
@@ -645,6 +645,17 @@ mono_wasm_string_from_js (const char *str)
 		return NULL;
 }
 
+EMSCRIPTEN_KEEPALIVE MonoString *
+mono_wasm_string_from_utf16 (const mono_unichar2 * chars, int length)
+{
+	assert (length >= 0);
+
+	if (chars)
+		return mono_string_new_utf16 (root_domain, chars, length);
+	else
+		return NULL;
+}
+
 static int
 class_is_task (MonoClass *klass)
 {

commit b4a7724d5b5c8227aa95e66b64dc5494276c34b5

Fix an api regression introduced in #42486

src/mono/wasm/runtime/binding_support.js
@@ -142,6 +142,9 @@ var BindingSupportLib = {
 		},
 
 		js_string_to_mono_string: function (string) {
+			if (string === null || typeof string === "undefined")
+				return null;
+
 			var buffer = Module._malloc ((string.length + 1) * 2);
 			var buffer16 = (buffer / 2) | 0;
 			for (var i = 0; i < string.length; i++)