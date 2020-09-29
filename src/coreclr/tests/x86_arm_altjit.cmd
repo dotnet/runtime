855e2961ba70d3ed8b094d230aaf655adac2b76e
Kunal Pathak <Kunal.Pathak@microsoft.com>
2020-09-28 23:49:06 -0700

    Rename references of protononjit to clrjit_ (#42133)
    * wip: rename protononjit
    
    * Rename protononjit to clrjit_
    
    * revert changes in run-pmi-diffs.py
    
    * revert superpmi.py changes
    
    * fix variable name

--- src/coreclr/tests/x86_arm_altjit.cmd
+++ src/coreclr/tests/x86_arm_altjit.cmd
@@ -1,6 +1,6 @@
 set COMPlus_AltJit=*
 set COMPlus_AltJitNgen=*
-set COMPlus_AltJitName=protononjit.dll
+set COMPlus_AltJitName=clrjit_win_arm_x86.dll
 set COMPlus_NoGuiOnAssert=1
 set COMPlus_ContinueOnAssert=0
 set COMPlus_AltJitAssertOnNYI=1
