828baf7329bb3f9b304c43286e8dd7af13c03294
Tomas <trylek@microsoft.com>
2020-09-29 08:56:45 +0200

    Address Bruce's PR feedback
    I have deleted the obsolete scripts protononjit_testenv,
    x64_arm64_altjit and x86_arm_altjit, and I have updated references
    to runtest.py / runtest.proj in the renamed run.py script. For
    making run.cmd / run.sh thinner wrappers, I'll tackle that in a
    follow-up change, I plan to do a similar thing for build.cmd / sh.
    
    Thanks
    
    Tomas

--- src/tests/Common/scripts/x64_arm64_altjit.cmd
+++
@@ -1,7 +0,0 @@
-set COMPlus_AltJit=*
-set COMPlus_AltJitNgen=*
-set COMPlus_AltJitName=clrjit_win_arm64_x64.dll
-set COMPlus_NoGuiOnAssert=1
-set COMPlus_ContinueOnAssert=0
-set COMPlus_AltJitAssertOnNYI=1
-set COMPlus_SIMD16ByteOnly=1
