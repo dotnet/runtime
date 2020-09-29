db84dd4e95fc65f37cf8523f65c21fc54a8c94bd
Ankit Jain <radical@gmail.com>
2020-09-29 16:58:58 -0400

    [wasm][tests][browser] Http - FunctionalTests - mark failing tests
    - Most of these are failing due to LoopbackServer
    - some others are due to monitors not being supported
    
    This brings the result to zero failures:
    
    `Tests run: 439, Errors: 0, Failures: 0, Skipped: 18. Time: 2.128495s`

--- src/libraries/System.Net.Http/tests/FunctionalTests/HttpClientHandlerTest.Http1.cs
+++ src/libraries/System.Net.Http/tests/FunctionalTests/HttpClientHandlerTest.Http1.cs
@@ -12,6 +12,7 @@ using Xunit.Abstractions;
 namespace System.Net.Http.Functional.Tests
 {
     // This class is dedicated to SocketHttpHandler tests specific to HTTP/1.x.
+    [ConditionalClass(typeof(PlatformDetection), nameof(PlatformDetection.IsNotBrowser))]
     public class HttpClientHandlerTest_Http1 : HttpClientHandlerTestBase
     {
         public HttpClientHandlerTest_Http1(ITestOutputHelper output) : base(output) { }
