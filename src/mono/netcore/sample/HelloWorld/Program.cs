bc40b7dec8dae92987c162b20e3f82ed718234ba
Egor Bogatov <egorbo@gmail.com>
2020-09-29 21:32:54 +0300

    Update Program.cs

--- src/mono/netcore/sample/HelloWorld/Program.cs
+++ src/mono/netcore/sample/HelloWorld/Program.cs
@@ -10,8 +10,9 @@ namespace HelloWorld
         private static void Main(string[] args)
         {
             bool isMono = typeof(object).Assembly.GetType("Mono.RuntimeStructs") != null;
-            Console.WriteLine(typeof(object).Assembly.FullName);
             Console.WriteLine($"Hello World {(isMono ? "from Mono!" : "from CoreCLR!")}");
+            Console.WriteLine(typeof(object).Assembly.FullName);
+            Console.WriteLine(System.Reflection.Assembly.GetEntryAssembly ());
             Console.WriteLine(System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription);
         }
     }
