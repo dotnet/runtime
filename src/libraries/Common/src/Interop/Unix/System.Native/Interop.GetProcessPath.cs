5da357f2d914d0a5519c1eec5f7dffd269d90d11
Jan Kotas <jkotas@microsoft.com>
2020-09-30 09:03:38 -0700

    Update src/libraries/Common/src/Interop/Unix/System.Native/Interop.GetProcessPath.cs
    Co-authored-by: Stephen Toub <stoub@microsoft.com>

--- src/libraries/Common/src/Interop/Unix/System.Native/Interop.GetProcessPath.cs
+++ src/libraries/Common/src/Interop/Unix/System.Native/Interop.GetProcessPath.cs
@@ -8,7 +8,7 @@ internal static partial class Interop
     internal static partial class Sys
     {
         /// <summary>
-        /// Returns the full path to the executable for the current process resolving symbolic links.
+        /// Returns the full path to the executable for the current process, resolving symbolic links.
         /// </summary>
         [DllImport(Libraries.SystemNative, EntryPoint = "SystemNative_GetProcessPath", SetLastError = true)]
         internal static extern string? GetProcessPath();
