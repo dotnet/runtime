6b1a4aae5a7158a6c8cea513f91c97921ab35cde
HuyLuong <huy.luong@orientsoftware.com>
2020-10-03 22:32:23 +0700

    Add new function to scan the open generic's constructors when the type is generic

--- src/libraries/System.Composition.TypedParts/src/System/Composition/TypedParts/Discovery/DiscoveredPart.cs
+++ src/libraries/System.Composition.TypedParts/src/System/Composition/TypedParts/Discovery/DiscoveredPart.cs
@@ -98,6 +98,11 @@ namespace System.Composition.TypedParts.Discovery
                     }
                 }
 
+                if (_constructor == null && _partType.IsGenericType)
+                {
+                    _constructor = GetConstructorInfoFromGenericType(_partType);
+                }
+
                 if (_constructor == null)
                     _constructor = _partType.DeclaredConstructors
                         .FirstOrDefault(ci => ci.IsPublic && !(ci.IsStatic || ci.GetParameters().Any()));
@@ -128,6 +133,33 @@ namespace System.Composition.TypedParts.Discovery
                         yield return optional;
                 }
             }
+        }
+
+        private ConstructorInfo GetConstructorInfoFromGenericType(TypeInfo type)
+        {
+            var genericPartTypeInfo = type.GetGenericTypeDefinition().GetTypeInfo();
+            var genericPartType = type.GetGenericTypeDefinition();
+            int indexConstructorFound = -1;
+            ConstructorInfo constructor = null;
+
+            foreach (var c in genericPartTypeInfo.DeclaredConstructors.Where(ci => ci.IsPublic && !(ci.IsStatic)))
+            {
+                if (_attributeContext.GetDeclaredAttribute<ImportingConstructorAttribute>(genericPartType, c) != null)
+                {
+                    if (constructor != null)
+                    {
+                        string message = SR.Format(SR.DiscoveredPart_MultipleImportingConstructorsFound, type);
+                        throw new CompositionFailedException(message);
+                    }
+
+                    ++indexConstructorFound;
+                    constructor = c;
+                }
+            }
+            if (constructor != null)
+                constructor = type.DeclaredConstructors.Where(ci => ci.IsPublic && !(ci.IsStatic)).ElementAt(indexConstructorFound);
+
+            return constructor;
         }
 
         public CompositeActivator GetActivator(DependencyAccessor definitionAccessor, IEnumerable<CompositionDependency> dependencies)
