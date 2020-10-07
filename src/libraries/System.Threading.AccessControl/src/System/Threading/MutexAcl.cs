1f2541a8cd2cceca4ef4594cf0271f024f1975bc
Carlos Sanchez Lopez <carlossanlop@users.noreply.github.com>
2020-10-07 00:25:37 -0700

    Implement MutexAcl methods.

--- src/libraries/System.Threading.AccessControl/src/System/Threading/MutexAcl.cs
+++ src/libraries/System.Threading.AccessControl/src/System/Threading/MutexAcl.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Security.AccessControl;
@@ -42,49 +43,97 @@ namespace System.Threading
                     (uint)MutexRights.FullControl // Equivalent to MUTEX_ALL_ACCESS
                 );
 
-                ValidateMutexHandle(handle, name, out createdNew);
+                int errorCode = Marshal.GetLastWin32Error();
 
-                Mutex mutex = new Mutex(initiallyOwned);
-                SafeWaitHandle old = mutex.SafeWaitHandle;
-                mutex.SafeWaitHandle = handle;
-                old.Dispose();
+                if (handle.IsInvalid)
+                {
+                    handle.SetHandleAsInvalid();
 
-                return mutex;
+                    if (errorCode == Interop.Errors.ERROR_FILENAME_EXCED_RANGE)
+                    {
+                        throw new ArgumentException(SR.Argument_WaitHandleNameTooLong, nameof(name));
+                    }
+
+                    if (errorCode == Interop.Errors.ERROR_INVALID_HANDLE)
+                    {
+                        throw new WaitHandleCannotBeOpenedException(SR.Format(SR.Threading_WaitHandleCannotBeOpenedException_InvalidHandle, name));
+                    }
+
+                    throw Win32Marshal.GetExceptionForWin32Error(errorCode, name);
+                }
+
+                createdNew = (errorCode != Interop.Errors.ERROR_ALREADY_EXISTS);
+
+                return CreateAndReplaceHandle(handle);
             }
         }
 
         public static Mutex OpenExisting(string name, MutexRights rights)
         {
-            throw null;
-        }
-
-        public static bool TryOpenExisting(string name, MutexRights rights, out Mutex result)
-        {
-            throw null;
-        }
-
-        private static void ValidateMutexHandle(SafeWaitHandle mutexHandle, string? name, out bool createdNew)
-        {
-            int errorCode = Marshal.GetLastWin32Error();
-
-            if (mutexHandle.IsInvalid)
+            switch (OpenExistingWorker(name, rights, out Mutex? result))
             {
-                mutexHandle.SetHandleAsInvalid();
+                case OpenExistingResult.NameNotFound:
+                    throw new WaitHandleCannotBeOpenedException();
 
-                if (errorCode == Interop.Errors.ERROR_FILENAME_EXCED_RANGE)
-                {
-                    throw new ArgumentException(SR.Argument_WaitHandleNameTooLong, nameof(name));
-                }
-
-                if (errorCode == Interop.Errors.ERROR_INVALID_HANDLE)
-                {
+                case OpenExistingResult.NameInvalid:
                     throw new WaitHandleCannotBeOpenedException(SR.Format(SR.Threading_WaitHandleCannotBeOpenedException_InvalidHandle, name));
-                }
 
-                throw Win32Marshal.GetExceptionForWin32Error(errorCode, name);
+                case OpenExistingResult.PathNotFound:
+                    throw new DirectoryNotFoundException(SR.Format(SR.IO_PathNotFound_Path, name));
+
+                case OpenExistingResult.Success:
+                default:
+                    Debug.Assert(result != null, "result should be non-null on success");
+                    return result;
+            }
+        }
+
+        public static bool TryOpenExisting(string name, MutexRights rights, out Mutex result) =>
+            OpenExistingWorker(name, rights, out result!) == OpenExistingResult.Success;
+
+        private static OpenExistingResult OpenExistingWorker(string name, MutexRights rights, out Mutex? result)
+        {
+            if (name == null)
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+            if (name.Length == 0)
+            {
+                throw new ArgumentException(SR.Argument_EmptyName, nameof(name));
             }
 
-            createdNew = (errorCode != Interop.Errors.ERROR_ALREADY_EXISTS);
+            result = null;
+            SafeWaitHandle existingHandle = Interop.Kernel32.OpenMutex((uint)rights, false, name);
+
+            int errorCode = Marshal.GetLastWin32Error();
+            if (existingHandle.IsInvalid)
+            {
+                return errorCode switch
+                {
+                    Interop.Errors.ERROR_FILE_NOT_FOUND or Interop.Errors.ERROR_INVALID_NAME => OpenExistingResult.NameNotFound,
+                    Interop.Errors.ERROR_PATH_NOT_FOUND => OpenExistingResult.PathNotFound,
+                    Interop.Errors.ERROR_INVALID_HANDLE => OpenExistingResult.NameInvalid,
+                    _ => throw Win32Marshal.GetExceptionForWin32Error(errorCode, name)
+                };
+            }
+
+            result = CreateAndReplaceHandle(existingHandle);
+
+            return OpenExistingResult.Success;
+        }
+
+        private static Mutex CreateAndReplaceHandle(SafeWaitHandle replacementHandle)
+        {
+            // The value of initiallyOwned should not matter since we are replacing the
+            // handle with one from an existing Mutex, and disposing the old one
+            // We should only make sure that it is a valid value
+            Mutex mutex = new Mutex(initiallyOwned: default);
+
+            SafeWaitHandle old = mutex.SafeWaitHandle;
+            mutex.SafeWaitHandle = replacementHandle;
+            old.Dispose();
+
+            return mutex;
         }
     }
 }
