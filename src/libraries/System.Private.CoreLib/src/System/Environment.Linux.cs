a0e3b493ee89cb4ae0f3c103d2ba4a974e0372de
root <3840695+am11@users.noreply.github.com>
2020-10-13 17:12:04 +0300

    Replace ReusableTextReader with shared ArrayPool

--- src/libraries/System.Private.CoreLib/src/System/Environment.Linux.cs
+++ src/libraries/System.Private.CoreLib/src/System/Environment.Linux.cs
@@ -2,13 +2,11 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Runtime.InteropServices;
-using System.Text;
 
 namespace System
 {
     public static partial class Environment
     {
-        private static ReusableTextReader reusableReader = new ReusableTextReader();
-        public static long WorkingSet => (long)(Interop.procfs.TryReadStatusFile(ProcessId, out Interop.procfs.ParsedStatus status, reusableReader) ? status.VmRSS : 0);
+        public static long WorkingSet => (long)(Interop.procfs.TryReadStatusFile(ProcessId, out Interop.procfs.ParsedStatus status) ? status.VmRSS : 0);
     }
 }
