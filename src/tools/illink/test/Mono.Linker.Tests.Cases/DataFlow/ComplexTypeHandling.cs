e57405292bcf39ef5839e9fb6e9f530ca714ff93
Vitek Karas <vitek.karas@microsoft.com>
2020-10-13 05:37:35 -0700

    Test handling of arrays in data flow (dotnet/linker#1564)
    The test is currently failing due to https://github.com/mono/linker/issues/1559
    
    
    Commit migrated from https://github.com/dotnet/linker/commit/2bcc466760c8c5fd08f79eb35ca1e5beb888ae77

---
+++ src/tools/illink/test/Mono.Linker.Tests.Cases/DataFlow/ComplexTypeHandling.cs
@@ -0,0 +1,100 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
+
+namespace Mono.Linker.Tests.Cases.DataFlow
+{
+	[IgnoreTestCase ("Active issue https://github.com/mono/linker/issues/1559")]
+	public class ComplexTypeHandling
+	{
+		public static void Main ()
+		{
+			TestArray ();
+			TestArrayOnGeneric ();
+			TestGenericArray ();
+			TestGenericArrayOnGeneric ();
+		}
+
+		[Kept]
+		class ArrayElementType
+		{
+			public ArrayElementType () { }
+
+			[Kept]
+			public void PublicMethod () { }
+
+			private int _privateField;
+		}
+
+		[Kept]
+		static void TestArray ()
+		{
+			RequirePublicMethods (typeof (ArrayElementType[]));
+		}
+
+		[Kept]
+		static void TestGenericArray ()
+		{
+			RequirePublicMethodsOnArrayOfGeneric<ArrayElementType> ();
+		}
+
+		[Kept]
+		static void RequirePublicMethodsOnArrayOfGeneric<T> ()
+		{
+			RequirePublicMethods (typeof (T[]));
+		}
+
+		[Kept]
+		private static void RequirePublicMethods (
+			[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicMethods)]
+			[KeptAttributeAttribute(typeof(DynamicallyAccessedMembersAttribute))]
+			Type type)
+		{
+		}
+
+		[Kept]
+		class ArrayElementInGenericType
+		{
+			public ArrayElementInGenericType () { }
+
+			[Kept]
+			public void PublicMethod () { }
+
+			private int _privateField;
+		}
+
+		[Kept]
+		[KeptMember (".ctor()")]
+		class RequirePublicMethodsGeneric<
+			[DynamicallyAccessedMembers (DynamicallyAccessedMemberTypes.PublicMethods)]
+		[KeptAttributeAttribute (typeof (DynamicallyAccessedMembersAttribute))]
+		T>
+		{
+		}
+
+		[Kept]
+		static void TestArrayOnGeneric ()
+		{
+			_ = new RequirePublicMethodsGeneric<ArrayElementInGenericType[]> ();
+		}
+
+		[Kept]
+		static void TestGenericArrayOnGeneric ()
+		{
+			RequirePublicMethodsOnArrayOfGenericParameter<ArrayElementInGenericType> ();
+		}
+
+		static void RequirePublicMethodsOnArrayOfGenericParameter<T> ()
+		{
+			_ = new RequirePublicMethodsGeneric<T[]> ();
+		}
+	}
+}
