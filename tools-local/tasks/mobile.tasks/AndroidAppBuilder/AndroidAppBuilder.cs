0d2a1cdf92e7242b0c4e2d4a3b406744d84907f1
Steve Pfister <steve.pfister@microsoft.com>
2020-10-13 08:18:03 -0400

    Address feedback items

--- tools-local/tasks/mobile.tasks/AndroidAppBuilder/AndroidAppBuilder.cs
+++ tools-local/tasks/mobile.tasks/AndroidAppBuilder/AndroidAppBuilder.cs
@@ -21,11 +21,8 @@ public class AndroidAppBuilderTask : Task
     [Required]
     public string MainLibraryFileName { get; set; } = ""!;
 
-    /// <summary>
-    /// Target arch, can be 'x86', 'x86_64', 'armeabi-v7a' or 'arm64-v8a'
-    /// </summary>
     [Required]
-    public string Abi { get; set; } = ""!;
+    public string RuntimeIdentifier { get; set; } = ""!;
 
     public string? ProjectName { get; set; }
 
@@ -53,6 +50,8 @@ public class AndroidAppBuilderTask : Task
     {
         Utils.Logger = Log;
 
+        string abi = DetermineAbi();
+
         var apkBuilder = new ApkBuilder();
         apkBuilder.ProjectName = ProjectName;
         apkBuilder.OutputDir = OutputDir;
@@ -62,8 +61,25 @@ public class AndroidAppBuilderTask : Task
         apkBuilder.BuildApiLevel = BuildApiLevel;
         apkBuilder.BuildToolsVersion = BuildToolsVersion;
         apkBuilder.StripDebugSymbols = StripDebugSymbols;
-        (ApkBundlePath, ApkPackageId) = apkBuilder.BuildApk(SourceDir, Abi, MainLibraryFileName, MonoRuntimeHeaders);
+        (ApkBundlePath, ApkPackageId) = apkBuilder.BuildApk(SourceDir, abi, MainLibraryFileName, MonoRuntimeHeaders);
 
         return true;
+    }
+
+    private string DetermineAbi()
+    {
+        switch (RuntimeIdentifier)
+        {
+            case "android-x86":
+                return "x86";
+            case "android-x64":
+                return "x86_64";
+            case "android-arm":
+                return "armeabi-v7a";
+            case "android-arm64":
+                return "arm64-v8a";
+            default:
+                throw new ArgumentException(RuntimeIdentifier + " is not supported for Android");
+        }
     }
 }
