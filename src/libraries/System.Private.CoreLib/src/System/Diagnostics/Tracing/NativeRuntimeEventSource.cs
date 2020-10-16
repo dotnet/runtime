78caa08ea7119e29748294f0f756baa2a4bdba5d
vitek-karas <vitek.karas@microsoft.com>
2020-10-16 05:32:34 -0700

    Cleanup

--- src/libraries/System.Private.CoreLib/src/System/Diagnostics/Tracing/NativeRuntimeEventSource.cs
+++ src/libraries/System.Private.CoreLib/src/System/Diagnostics/Tracing/NativeRuntimeEventSource.cs
@@ -28,6 +28,11 @@ namespace System.Diagnostics.Tracing
         [NonEvent]
         internal unsafe void ProcessEvent(uint eventID, uint osThreadID, DateTime timeStamp, Guid activityId, Guid childActivityId, ReadOnlySpan<byte> payload)
         {
+            if (!IsSupported)
+            {
+                return;
+            }
+
             // Make sure the eventID is valid.
             if (eventID >= m_eventData!.Length)
             {
