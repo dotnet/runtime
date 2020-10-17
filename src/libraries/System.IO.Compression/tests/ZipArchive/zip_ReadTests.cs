b4e3b1f7008517e7dae2defaaace67f797dae7c9
Sergey Mikolaytis <blasterloremaster@gmail.com>
2020-10-17 18:16:37 +0300

    fix test after azure pipeline checks with errors

--- src/libraries/System.IO.Compression/tests/ZipArchive/zip_ReadTests.cs
+++ src/libraries/System.IO.Compression/tests/ZipArchive/zip_ReadTests.cs
@@ -185,11 +185,11 @@ namespace System.IO.Compression.Tests
         }
 
         [Fact]
-        public static async Task TestEmptyLastModifiedEntryValueNotThrowingInternalException()
+        public static void TestEmptyLastModifiedEntryValueNotThrowingInternalException()
         {
             var emptyDateIndicator = new DateTime(1980, 1, 1, 1, 0, 0);
             var buffer = new byte[100];//empty archive we will make will have exact this size
-            await using var memoryStream = new MemoryStream(buffer);
+            using var memoryStream = new MemoryStream(buffer);
 
             using (var singleEntryArchive = new ZipArchive(memoryStream, ZipArchiveMode.Create, true))
             {
@@ -205,7 +205,7 @@ namespace System.IO.Compression.Tests
             memoryStream.Seek(0, SeekOrigin.Begin);
 
             using var archive = new ZipArchive(memoryStream, ZipArchiveMode.Read, true);
-            Assert.Equal(archive.Entries[0].LastWriteTime, emptyZipDateIndicator);
+            Assert.Equal(archive.Entries[0].LastWriteTime, new DateTimeOffset(emptyDateIndicator));
         }
     }
 }
