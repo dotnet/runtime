0d830ed3bb064d310326d2dceaddcf5e4c79d7dc
Kenneth Pouncey <kjpou@pt.lu>
2020-10-21 11:32:05 +0200

    Replace PAL interop interface implementation in favor of using the JavaScript bindings
    - reference `System.Private.Runtime.InteropServices.JavaScript`

--- src/libraries/System.Security.Cryptography.Algorithms/src/Internal/Cryptography/HashProviderDispenser.Browser.cs
+++ src/libraries/System.Security.Cryptography.Algorithms/src/Internal/Cryptography/HashProviderDispenser.Browser.cs
@@ -5,287 +5,160 @@ using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
-using System.Security.Cryptography.Browser;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices.JavaScript;
 
 namespace Internal.Cryptography
 {
     internal static partial class HashProviderDispenser
     {
+        private static readonly JSObject? s_crypto = (JSObject)System.Runtime.InteropServices.JavaScript.Runtime.GetGlobalObject("crypto");
+        private static readonly JSObject? s_subtle = (JSObject?)s_crypto?.GetObjectProperty("subtle");
+
         public static HashProvider CreateHashProvider(string hashAlgorithmId)
         {
             //System.Diagnostics.Debug.WriteLine($"HashProviderDispenser::CreateHashProvider {hashAlgorithmId}");
-            Interop.SubtleCrypto.PAL_HashAlgorithm algorithm = HashAlgorithmToPal(hashAlgorithmId);
-            return new BrowserAsyncDigestProvider(algorithm);
+            if (s_subtle == null)
+            {
+                Debug.Fail($"WebCrypto can not be found");
+                throw new CryptographicException();
+            }
+
+            return new BrowserAsyncDigestProvider(hashAlgorithmId);
         }
 
-        public static unsafe HashProvider CreateMacProvider(string hashAlgorithmId, ReadOnlySpan<byte> key)
-        {
-            throw new PlatformNotSupportedException(SR.SystemSecurityCryptographyAlgorithms_PlatformNotSupported);
-        }
-        private static Interop.SubtleCrypto.PAL_HashAlgorithm HashAlgorithmToPal(string hashAlgorithmId) => hashAlgorithmId switch {
-            HashAlgorithmNames.MD5 => Interop.SubtleCrypto.PAL_HashAlgorithm.Md5,
-            HashAlgorithmNames.SHA1 => Interop.SubtleCrypto.PAL_HashAlgorithm.Sha1,
-            HashAlgorithmNames.SHA256 => Interop.SubtleCrypto.PAL_HashAlgorithm.Sha256,
-            HashAlgorithmNames.SHA384 => Interop.SubtleCrypto.PAL_HashAlgorithm.Sha384,
-            HashAlgorithmNames.SHA512 => Interop.SubtleCrypto.PAL_HashAlgorithm.Sha512,
+        public static unsafe HashProvider CreateMacProvider(string hashAlgorithmId, ReadOnlySpan<byte> key) => throw new PlatformNotSupportedException(SR.SystemSecurityCryptographyAlgorithms_PlatformNotSupported);
+        private static string HashAlgorithmToPal(string hashAlgorithmId) => hashAlgorithmId switch {
+            HashAlgorithmNames.MD5 => "MD5",
+            HashAlgorithmNames.SHA1 => "SHA-1",
+            HashAlgorithmNames.SHA256 => "SHA-256",
+            HashAlgorithmNames.SHA384 => "SHA-384",
+            HashAlgorithmNames.SHA512 => "SHA-512",
             _ => throw new CryptographicException(SR.Format(SR.Cryptography_UnknownHashAlgorithm, hashAlgorithmId))
         };
 
         internal static class OneShotHashProvider
         {
-            public static unsafe int HashData(string hashAlgorithmId, ReadOnlySpan<byte> source, Span<byte> destination)
+            public static unsafe int HashData(string hashAlgorithmId, ReadOnlySpan<byte> source, Span<byte> destination) => throw new PlatformNotSupportedException(SR.SystemSecurityCryptographyAlgorithms_PlatformNotSupported);
+
+            public static async Task<int> HashDataAsync(string hashAlgorithmId, byte[] source, byte[] destination, CancellationToken cancellationToken)
             {
-                // System.Diagnostics.Debug.WriteLine($"HashProviderDispenser::OneShotHashProvider {hashAlgorithmId} ");
-                // Interop.SubtleCrypto.PAL_HashAlgorithm algorithm = HashAlgorithmToPal(hashAlgorithmId);
+                string algorithm = HashAlgorithmToPal(hashAlgorithmId);
 
-                // fixed (byte* pSource = source)
-                // fixed (byte* pDestination = destination)
-                // {
-                //     System.Diagnostics.Debug.WriteLine($"HashProviderDispenser::OneShotHashProvider->Interop.SubtleCrypto.DigestOneShot {hashAlgorithmId} / {source.Length} // {destination.Length}");
-
-                //     int ret = Interop.SubtleCrypto.DigestOneShot(
-                //         algorithm,
-                //         pSource,
-                //         source.Length,
-                //         pDestination,
-                //         destination.Length,
-                //         out int digestSize);
-
-                //     if (ret != 1)
-                //     {
-                //         Debug.Fail($"HashData return value {ret} was not 1");
-                //         throw new CryptographicException();
-                //     }
-
-                //     Debug.Assert(digestSize <= destination.Length);
-
-                    return -1;
-                //}
-            }
-
-            public static unsafe Task<int> HashDataAsync(string hashAlgorithmId, byte[] source, byte[] destination, CancellationToken cancellationToken)
-            {
-                //System.Diagnostics.Debug.WriteLine($"HashProviderDispenser::OneShotHashProvider {hashAlgorithmId} ");
-                Interop.SubtleCrypto.PAL_HashAlgorithm algorithm = HashAlgorithmToPal(hashAlgorithmId);
-
-                fixed (byte* pSource = source)
-                fixed (byte* pDestination = destination)
+                int written = 0;
+                if (s_subtle?.GetObjectProperty("digest") is Function digest)
                 {
-                    //System.Diagnostics.Debug.WriteLine($"HashProviderDispenser::OneShotHashProvider->Interop.SubtleCrypto.DigestOneShot {hashAlgorithmId} / {source.Length} // {destination.Length}");
-
-                    TaskCompletionSource<int> finalizetcs = new TaskCompletionSource<int>();
-                    int ret = Interop.SubtleCrypto.DigestOneShot(
-                        algorithm,
-                        pSource,
-                        source.Length,
-                        pDestination,
-                        destination.Length,
-                        out int digestSize,
-                        finalizetcs);
-
-                    if (ret != 1)
-                    {
-                        Debug.Fail($"HashData return value {ret} was not 1");
-                        throw new CryptographicException();
-                    }
-
-                    Debug.Assert(digestSize <= destination.Length);
-
-                    return finalizetcs.Task;
+                    using Uint8Array taSource = Uint8Array.From(source);
+                    using (digest)
+                        if (digest!.Call(s_subtle, algorithm, taSource) is Task<object> hic)
+                        {
+                            using ArrayBuffer? digestValue = await hic.ConfigureAwait(false) as ArrayBuffer;
+                            using Uint8Array hashArray = new Uint8Array(digestValue!);
+                            written = hashArray.Length;
+                            Debug.Assert(written == destination.Length);
+                            System.Array.Copy(hashArray.ToArray(), destination, destination.Length);
+                        }
                 }
+                else
+                {
+                    Debug.Fail($"WebCrypto API can not be found");
+                    throw new CryptographicException();
+                }
+                return written;
             }
         }
 
         private sealed class BrowserAsyncDigestProvider : HashProvider
         {
-            private readonly SafeDigestCtxHandle _ctx;
+            private Uint8Array? _dataToHash;
+            private readonly string _hashAlgorithm;
 
             public override int HashSizeInBytes { get; }
 
-            internal BrowserAsyncDigestProvider(Interop.SubtleCrypto.PAL_HashAlgorithm algorithm)
+            internal BrowserAsyncDigestProvider(string algorithm)
             {
                 int hashSizeInBytes;
-                _ctx = Interop.SubtleCrypto.DigestCreate(algorithm, out hashSizeInBytes);
-                //System.Diagnostics.Debug.WriteLine($"BrowserDigestProvider::Interop.SubtleCrypto.DigestCreate {algorithm} / {hashSizeInBytes}");
-                if (hashSizeInBytes < 0)
+                switch (algorithm)
                 {
-                    _ctx.Dispose();
-                    throw new PlatformNotSupportedException(
-                        SR.Format(
-                            SR.Cryptography_UnknownHashAlgorithm,
-                            Enum.GetName(typeof(Interop.SubtleCrypto.PAL_HashAlgorithm), algorithm)));
+                    case HashAlgorithmNames.SHA1:
+                        hashSizeInBytes = 20;
+                        break;
+                    case HashAlgorithmNames.SHA256:
+                        hashSizeInBytes = 32;
+                        break;
+                    case HashAlgorithmNames.SHA384:
+                        hashSizeInBytes = 48;
+                        break;
+                    case HashAlgorithmNames.SHA512:
+                        hashSizeInBytes = 64;
+                        break;
+                    default:
+                        throw new CryptographicException(SR.Format(SR.Cryptography_UnknownHashAlgorithm, algorithm));
                 }
 
-                if (_ctx.IsInvalid)
-                {
-                    _ctx.Dispose();
-                    throw new CryptographicException();
-                }
-
+                _hashAlgorithm = HashAlgorithmToPal(algorithm);
                 HashSizeInBytes = hashSizeInBytes;
             }
 
-            public override void AppendHashData(ReadOnlySpan<byte> data)
-            {
-                //System.Diagnostics.Debug.WriteLine($"BrowserDigestProvider::Interop.SubtleCrypto.DigestUpdate synchronous data length: {data.Length}");
-                int ret = Interop.SubtleCrypto.DigestUpdate(_ctx, data);
-
-                if (ret != 1)
-                {
-                    Debug.Assert(ret == 0, $"DigestUpdate return value {ret} was not 0 or 1");
-                    throw new CryptographicException();
-                }
-            }
+            public override void AppendHashData(ReadOnlySpan<byte> data) => throw new PlatformNotSupportedException();
 
             public override Task AppendHashDataAsync(byte[] array, int ibStart, int cbSize, CancellationToken cancellationToken)
             {
                 //System.Diagnostics.Debug.WriteLine($"BrowserDigestProvider:AppendHashDataAsync::Interop.SubtleCrypto.DigestUpdate data length: {array.Length}");
                 // At this time the DigestUpdate does not do anything async
-                int ret = Interop.SubtleCrypto.DigestUpdate(_ctx, array, ibStart, cbSize);
-
-                if (ret != 1)
+                if (_dataToHash == null)
                 {
-                    Debug.Assert(ret == 0, $"DigestUpdate return value {ret} was not 0 or 1");
-                    throw new CryptographicException();
+                    _dataToHash = Uint8Array.From(array.AsSpan<byte>(ibStart, cbSize));
+                }
+                else
+                {
+                    // TypedArrays do not support concatenation
+                    // So we have to resort to creating multiple arrays.
+                    long hashLength = _dataToHash.Length;
+                    byte[] appendage = new byte[_dataToHash.Length + cbSize];
+                    System.Array.Copy(_dataToHash.ToArray(), appendage, hashLength);
+                    System.Array.Copy(array, ibStart, appendage, hashLength, cbSize);
+                    _dataToHash = Uint8Array.From(appendage.AsSpan<byte>());
                 }
                 return Task.CompletedTask;
             }
-            public override int FinalizeHashAndReset(Span<byte> destination)
+            public override int FinalizeHashAndReset(Span<byte> destination) => throw new PlatformNotSupportedException();
+
+            public override async Task<int> FinalizeHashAndResetAsync(byte[] destination, CancellationToken cancellationToken)
             {
-                Debug.Assert(destination.Length >= HashSizeInBytes);
-                //System.Diagnostics.Debug.WriteLine($"BrowserDigestProvider::Interop.SubtleCrypto.DigestFinal destination length: {destination.Length}");
-
-                TaskCompletionSource<int> finalizetcs = new TaskCompletionSource<int>();
-                int ret = Interop.SubtleCrypto.DigestFinal(_ctx, destination, finalizetcs);
-                //return await mytcs.Task;
-                //int ret = Interop.SubtleCrypto.DigestFinal(_ctx, destination);
-
-                if (ret != 1)
+                int written = 0;
+                if (s_subtle?.GetObjectProperty("digest") is Function digest)
                 {
-                    Debug.Assert(ret == 0, $"DigestFinal return value {ret} was not 0 or 1");
+                    using (digest)
+                    using (_dataToHash)
+                        if (digest!.Call(s_subtle, _hashAlgorithm, _dataToHash) is Task<object> hic)
+                        {
+                            using ArrayBuffer? digestValue = await hic.ConfigureAwait(false) as ArrayBuffer;
+                            using Uint8Array hashArray = new Uint8Array(digestValue!);
+                            written = hashArray.Length;
+                            Debug.Assert(written == destination.Length);
+                            System.Array.Copy(hashArray.ToArray(), destination, destination.Length);
+                        }
+                    _dataToHash = null;
+                }
+                else
+                {
+                    Debug.Fail($"WebCrypto API can not be found");
                     throw new CryptographicException();
                 }
-
-                return HashSizeInBytes;
+                return written;
             }
 
-            public override Task<int> FinalizeHashAndResetAsync(byte[] destination, CancellationToken cancellationToken)
-            {
-                TaskCompletionSource<int> finalizetcs = new TaskCompletionSource<int>();
-                int ret = Interop.SubtleCrypto.DigestFinal(_ctx, destination, finalizetcs);
-                return finalizetcs.Task;
-            }
-
-            public override int GetCurrentHash(Span<byte> destination)
-            {
-                Debug.Assert(destination.Length >= HashSizeInBytes);
-                System.Diagnostics.Debug.WriteLine($"BrowserDigestProvider::Interop.SubtleCrypto.GetCurrentHash destination length: {destination.Length}");
-                int ret = Interop.SubtleCrypto.DigestCurrent(_ctx, destination);
-
-                if (ret != 1)
-                {
-                    Debug.Assert(ret == 0, $"DigestCurrent return value {ret} was not 0 or 1");
-                    throw new CryptographicException();
-                }
-
-                return HashSizeInBytes;
-            }
+            public override int GetCurrentHash(Span<byte> destination) => throw new PlatformNotSupportedException();
 
             public override void Dispose(bool disposing)
             {
                 if (disposing)
                 {
-                    _ctx?.Dispose();
-                }
-            }
-        }
-
-        private sealed class BrowserDigestProvider : HashProvider
-        {
-            private readonly SafeDigestCtxHandle _ctx;
-
-            public override int HashSizeInBytes { get; }
-
-            internal BrowserDigestProvider(Interop.SubtleCrypto.PAL_HashAlgorithm algorithm)
-            {
-                int hashSizeInBytes;
-                _ctx = Interop.SubtleCrypto.DigestCreate(algorithm, out hashSizeInBytes);
-                //System.Diagnostics.Debug.WriteLine($"BrowserDigestProvider::Interop.SubtleCrypto.DigestCreate {algorithm} / {hashSizeInBytes}");
-                if (hashSizeInBytes < 0)
-                {
-                    _ctx.Dispose();
-                    throw new PlatformNotSupportedException(
-                        SR.Format(
-                            SR.Cryptography_UnknownHashAlgorithm,
-                            Enum.GetName(typeof(Interop.SubtleCrypto.PAL_HashAlgorithm), algorithm)));
-                }
-
-                if (_ctx.IsInvalid)
-                {
-                    _ctx.Dispose();
-                    throw new CryptographicException();
-                }
-
-                HashSizeInBytes = hashSizeInBytes;
-            }
-
-            public override void AppendHashData(ReadOnlySpan<byte> data)
-            {
-                //System.Diagnostics.Debug.WriteLine($"BrowserDigestProvider::Interop.SubtleCrypto.DigestUpdate data length: {data.Length}");
-                int ret = Interop.SubtleCrypto.DigestUpdate(_ctx, data);
-
-                if (ret != 1)
-                {
-                    Debug.Assert(ret == 0, $"DigestUpdate return value {ret} was not 0 or 1");
-                    throw new CryptographicException();
-                }
-            }
-
-            public override Task AppendHashDataAsync(byte[] array, int ibStart, int cbSize, CancellationToken cancellationToken) => throw new NotImplementedException();
-
-            public override int FinalizeHashAndReset(Span<byte> destination)
-            {
-                Debug.Assert(destination.Length >= HashSizeInBytes);
-                //System.Diagnostics.Debug.WriteLine($"BrowserDigestProvider::Interop.SubtleCrypto.DigestFinal destination length: {destination.Length}");
-
-                int ret = Interop.SubtleCrypto.DigestFinal(_ctx, destination);
-
-                if (ret != 1)
-                {
-                    Debug.Assert(ret == 0, $"DigestFinal return value {ret} was not 0 or 1");
-                    throw new CryptographicException();
-                }
-
-                return HashSizeInBytes;
-            }
-
-            public override Task<int> FinalizeHashAndResetAsync(byte[] destination, CancellationToken cancellationToken) => throw new NotImplementedException();
-
-            public override int GetCurrentHash(Span<byte> destination)
-            {
-                Debug.Assert(destination.Length >= HashSizeInBytes);
-                System.Diagnostics.Debug.WriteLine($"BrowserDigestProvider::Interop.SubtleCrypto.GetCurrentHash destination length: {destination.Length}");
-                int ret = Interop.SubtleCrypto.DigestCurrent(_ctx, destination);
-
-                if (ret != 1)
-                {
-                    Debug.Assert(ret == 0, $"DigestCurrent return value {ret} was not 0 or 1");
-                    throw new CryptographicException();
-                }
-
-                return HashSizeInBytes;
-            }
-
-            public override void Dispose(bool disposing)
-            {
-                if (disposing)
-                {
-                    _ctx?.Dispose();
+                    _dataToHash?.Dispose();
                 }
             }
         }
     }
-
 }
