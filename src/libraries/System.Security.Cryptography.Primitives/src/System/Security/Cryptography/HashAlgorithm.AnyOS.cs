43b2e5b62a39730cc595507be5f8cf2a0fc98435
Kenneth Pouncey <kjpou@pt.lu>
2020-10-22 08:47:28 +0200

    Use async calls for stream hashing when used on browser.

---
+++ src/libraries/System.Security.Cryptography.Primitives/src/System/Security/Cryptography/HashAlgorithm.AnyOS.cs
@@ -0,0 +1,38 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Buffers;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace System.Security.Cryptography
+{
+    public abstract partial class HashAlgorithm : IDisposable, ICryptoTransform
+    {
+        private async Task<byte[]> ComputeHashAsyncCore(
+            Stream inputStream,
+            CancellationToken cancellationToken)
+        {
+            // Use ArrayPool.Shared instead of CryptoPool because the array is passed out.
+            byte[] rented = ArrayPool<byte>.Shared.Rent(4096);
+            Memory<byte> buffer = rented;
+            int clearLimit = 0;
+            int bytesRead;
+
+            while ((bytesRead = await inputStream.ReadAsync(buffer, cancellationToken).ConfigureAwait(false)) > 0)
+            {
+                if (bytesRead > clearLimit)
+                {
+                    clearLimit = bytesRead;
+                }
+
+                await HashCoreAsync(rented, 0, bytesRead, cancellationToken).ConfigureAwait(false);
+            }
+
+            CryptographicOperations.ZeroMemory(rented.AsSpan(0, clearLimit));
+            ArrayPool<byte>.Shared.Return(rented, clearArray: false);
+            return await CaptureHashCodeAndReinitializeAsync(cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
--- src/libraries/System.Security.Cryptography.Primitives/src/System/Security/Cryptography/HashAlgorithm.cs
+++ src/libraries/System.Security.Cryptography.Primitives/src/System/Security/Cryptography/HashAlgorithm.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace System.Security.Cryptography
 {
-    public abstract class HashAlgorithm : IDisposable, ICryptoTransform
+    public abstract partial class HashAlgorithm : IDisposable, ICryptoTransform
     {
         private bool _disposed;
         protected int HashSizeValue;
@@ -148,32 +148,6 @@ namespace System.Security.Cryptography
                 throw new ObjectDisposedException(null);
 
             return ComputeHashAsyncCore(inputStream, cancellationToken);
-        }
-
-
-        private async Task<byte[]> ComputeHashAsyncCore(
-            Stream inputStream,
-            CancellationToken cancellationToken)
-        {
-            // Use ArrayPool.Shared instead of CryptoPool because the array is passed out.
-            byte[] rented = ArrayPool<byte>.Shared.Rent(4096);
-            Memory<byte> buffer = rented;
-            int clearLimit = 0;
-            int bytesRead;
-
-            while ((bytesRead = await inputStream.ReadAsync(buffer, cancellationToken).ConfigureAwait(false)) > 0)
-            {
-                if (bytesRead > clearLimit)
-                {
-                    clearLimit = bytesRead;
-                }
-
-                HashCore(rented, 0, bytesRead);
-            }
-
-            CryptographicOperations.ZeroMemory(rented.AsSpan(0, clearLimit));
-            ArrayPool<byte>.Shared.Return(rented, clearArray: false);
-            return CaptureHashCodeAndReinitialize();
         }
 
         private byte[] CaptureHashCodeAndReinitialize()
