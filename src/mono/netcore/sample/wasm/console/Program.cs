e377057411920406d0f245654316cdb380d2097b
Fan Yang <yangfan@microsoft.com>
2020-10-25 15:13:42 -0400

    Hacks to help with development

--- src/mono/netcore/sample/wasm/console/Program.cs
+++ src/mono/netcore/sample/wasm/console/Program.cs
@@ -2,11 +2,16 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
+using System.Threading;
 
 public class Test
 {
-    public static void Main (string[] args)
-    {
-        Console.WriteLine ("Hello, World!");
+    public static void Main (String[] args) {
+        double _location = 0;
+        double _newValue = 1;
+        //Volatile.Read(ref _location);
+        Console.WriteLine("The original result is: " + _location);
+        Volatile.Write(ref _location, _newValue);
+        Console.WriteLine("The result is: " + _location);
     }
 }
