dotnet/runtime

commit 2b6a3f9af1ab81dd1a39e04dad64c1c148ba9dc7

[jit] Fix gsharedvt constrained calls to Object.GetType () under netcore. (#43729)
* [jit] Fix gsharedvt constrained calls to Object.GetType () under netcore.

Fixes https://github.com/dotnet/runtime/issues/35674.

* Update src/mono/mono/mini/jit-icalls.c

Co-authored-by: Aleksey Kliger (λgeek) <akliger@gmail.com>

Co-authored-by: Aleksey Kliger (λgeek) <akliger@gmail.com>

src/mono/mono/mini/jit-icalls.c
@@ -1420,6 +1420,21 @@ mono_gsharedvt_constrained_call (gpointer mp, MonoMethod *cmethod, MonoClass *kl
 	gpointer this_arg;
 	gpointer new_args [16];
 
+#ifdef ENABLE_NETCORE
+	if (!mono_class_is_ginst (cmethod->klass) && !cmethod->is_inflated) {
+		MonoVTable *vt;
+
+		/* Object.GetType () is an intrinsic under netcore */
+		g_assertf (!strcmp (cmethod->name, "GetType"), "%s.%s", m_class_get_name (cmethod->klass), cmethod->name);
+		vt = mono_class_vtable_checked (mono_domain_get (), klass, error);
+		if (!is_ok (error)) {
+			mono_error_set_pending_exception (error);
+			return NULL;
+		}
+		return vt->type;
+	}
+#endif
+
 	m = constrained_gsharedvt_call_setup (mp, cmethod, klass, &this_arg, error);
 	if (!is_ok (error)) {
 		mono_error_set_pending_exception (error);

commit d61a389bbfde93fe7988a2c02a6f9fe583555b8e

Fix gsharedvt constrained calls to Object.GetHashCode () which was broken by #43729.
Fixes https://github.com/dotnet/runtime/issues/44000.

src/mono/mono/mini/jit-icalls.c
@@ -1421,11 +1421,10 @@ mono_gsharedvt_constrained_call (gpointer mp, MonoMethod *cmethod, MonoClass *kl
 	gpointer new_args [16];
 
 #ifdef ENABLE_NETCORE
-	if (!mono_class_is_ginst (cmethod->klass) && !cmethod->is_inflated) {
+	/* Object.GetType () is an intrinsic under netcore */
+	if (!mono_class_is_ginst (cmethod->klass) && !cmethod->is_inflated && !strcmp (cmethod->name, "GetType")) {
 		MonoVTable *vt;
 
-		/* Object.GetType () is an intrinsic under netcore */
-		g_assertf (!strcmp (cmethod->name, "GetType"), "%s.%s", m_class_get_name (cmethod->klass), cmethod->name);
 		vt = mono_class_vtable_checked (mono_domain_get (), klass, error);
 		if (!is_ok (error)) {
 			mono_error_set_pending_exception (error);