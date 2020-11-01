587f9abaf5a7c0ca4018ef055466f950b6acb398
Egor Bogatov <egorbo@gmail.com>
2020-11-02 00:13:28 +0300

    Update Convert.ToHexString.cs

--- src/libraries/System.Runtime.Extensions/tests/System/Convert.ToHexString.cs
+++ src/libraries/System.Runtime.Extensions/tests/System/Convert.ToHexString.cs
@@ -1,5 +1,10 @@
-﻿using System.Text;
+  
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 using Xunit;
+using System.Text;
+using System.Collections.Generic;
 
 namespace System.Tests
 { 
