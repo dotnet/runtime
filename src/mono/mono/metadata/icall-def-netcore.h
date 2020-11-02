d1dc08cfe96869c8305c38f6e5cd29c2096967b4
Aleksey Kliger <alklig@microsoft.com>
2020-11-02 16:01:18 -0500

    fix icall-def-netcore typo

--- src/mono/mono/metadata/icall-def-netcore.h
+++ src/mono/mono/metadata/icall-def-netcore.h
@@ -327,7 +327,7 @@ HANDLES(MPROP_4, "get_property_info", ves_icall_RuntimePropertyInfo_get_property
 HANDLES(MPROP_5, "internal_from_handle_type", ves_icall_System_Reflection_RuntimePropertyInfo_internal_from_handle_type, MonoReflectionProperty, 2, (MonoProperty_ptr, MonoType_ptr))
 
 ICALL_TYPE(RUNF, "System.Runtime.CompilerServices.RuntimeFeature", RUNF_1)
-HANDLES(RUNT_1, "LoadMetadataUpdate_internal", ves_icall_Mono_Runtime_LoadMetadataUpdate, void, 5, (MonoReflectionAssembly, gconstpointer, gint32, gconstpointer, gint32))
+HANDLES(RUNF_1, "LoadMetadataUpdate_internal", ves_icall_Mono_Runtime_LoadMetadataUpdate, void, 5, (MonoReflectionAssembly, gconstpointer, gint32, gconstpointer, gint32))
 
 ICALL_TYPE(RUNH, "System.Runtime.CompilerServices.RuntimeHelpers", RUNH_1)
 HANDLES(RUNH_1, "GetObjectValue", ves_icall_System_Runtime_CompilerServices_RuntimeHelpers_GetObjectValue, MonoObject, 1, (MonoObject))
