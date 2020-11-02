4483fa750532ad9d4bde36099b682debbbb779db
EgorBo <egorbo@gmail.com>
2020-11-02 18:50:20 +0300

    Fix build

--- src/libraries/Native/Unix/System.Security.Cryptography.Native.Android/android_security.c
+++ src/libraries/Native/Unix/System.Security.Cryptography.Native.Android/android_security.c
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#include "pal_err.h"
 #include "pal_types.h"
 #include "pal_utilities.h"
 #include "pal_safecrt.h"
