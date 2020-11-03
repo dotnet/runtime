e5e1002680d58cb48e0ae487d765f0cfb90afc3b
Rattenkrieg <shallowstack@gmail.com>
2020-11-03 14:06:41 +0300

    tests cleanup

--- src/tests/Loader/classloader/DefaultInterfaceMethods/staticshared/staticshared.cs
+++ src/tests/Loader/classloader/DefaultInterfaceMethods/staticshared/staticshared.cs
@@ -17,18 +17,3 @@ public static class Program
         return 100;
     }
 }
-
-/*
-impDevirtualizeCall: Trying to devirtualize interface call:
-    class for 'this' is C [exact] (attrib 20000000)
-    base method is I`1[__Canon]::DefaultTypeOf
---- base class is interface
-    devirt to I`1[[System.String, System.Private.CoreLib, Version=5.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e]][System.String]::DefaultTypeOf -- exact
-               [000010] --C-G-------              *  CALLV stub ref    I`1[__Canon][System.__Canon].DefaultTypeOf
-               [000009] ------------ this in rcx  \--*  LCL_VAR   ref    V00 loc0
-    exact; can devirtualize
-... after devirt...
-               [000010] --C-G-------              *  CALL nullcheck ref    I`1[[System.String, System.Private.CoreLib, Version=5.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e]][System.String].DefaultTypeOf
-               [000009] ------------ this in rcx  \--*  LCL_VAR   ref    V00 loc0
-Devirtualized interface call to I`1[__Canon]:DefaultTypeOf; now direct call to I`1[[System.String, System.Private.CoreLib, Version=5.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e]][System.String]:DefaultTypeOf [exact]
-*/
