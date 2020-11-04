02d0b7e058ca59f5d4ed2762906aa989cf078ef0
Adeel <3840695+am11@users.noreply.github.com>
2020-11-04 13:15:00 +0000

    Implement Enviornment.WorkingSet for SunOS Difference between Linux and SunOS procfs is that files in latter contain binary data, so we need `read(2)` and cast into corresponding struct. Redeclaring system structs in managed code is not ideal, as they do change across the major versions of OS, which inevitably requires recompilation of binaries and replicating them in C# as is means additional/unnecessary maintenance of code.

--- src/libraries/System.Private.CoreLib/src/System/Environment.SunOS.cs
+++ src/libraries/System.Private.CoreLib/src/System/Environment.SunOS.cs
@@ -1,12 +1,13 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
+using System.Runtime.InteropServices;
 
 namespace System
 {
     public static partial class Environment
     {
-        public static long WorkingSet => throw new PlatformNotSupportedException();
+        public static long WorkingSet =>
+            (long)(Interop.procfs.TryReadProcessStatusInfo(ProcessId, out Interop.procfs.ProcessStatusInfo status) ? status.ResidentSetSize : 0);
     }
 }
