ed2b8444e33df90e7b723737796a1fa60622880f
Buyaa Namnan <bunamnan@microsoft.com>
2020-11-13 11:15:01 -0800

    Revering unwanted changes

--- src/libraries/System.Net.Http/src/System/Net/Http/BrowserHttpHandler/HttpTelemetry.Browser.cs
+++ src/libraries/System.Net.Http/src/System/Net/Http/BrowserHttpHandler/HttpTelemetry.Browser.cs
@@ -1,15 +1,24 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics.Tracing;
 
 namespace System.Net.Http
 {
     internal sealed partial class HttpTelemetry
     {
+        [NonEvent]
+        public void Http11RequestLeftQueue(double timeOnQueueMilliseconds)
+        {
+        }
+
+        [NonEvent]
+        public void Http20RequestLeftQueue(double timeOnQueueMilliseconds)
+        {
+        }
+
+        protected override void OnEventCommand(EventCommandEventArgs command)
+        {
+        }
     }
 }
