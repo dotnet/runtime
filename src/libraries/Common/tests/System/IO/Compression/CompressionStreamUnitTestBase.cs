d3adc388937df74c91bd385c15f749d5212a1d4b
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2020-11-13 20:51:49 +0800

    Re-add size in order test.

--- src/libraries/Common/tests/System/IO/Compression/CompressionStreamUnitTestBase.cs
+++ src/libraries/Common/tests/System/IO/Compression/CompressionStreamUnitTestBase.cs
@@ -441,6 +441,31 @@ namespace System.IO.Compression
             else
                 baseStream.Read(bytes, 0, size);
         }
+
+        [Theory]
+        [MemberData(nameof(UncompressedTestFiles))]
+        public async Task CompressionLevel_SizeInOrder(string testFile)
+        {
+            using var uncompressedStream = await LocalMemoryStream.readAppFileAsync(testFile);
+
+            async Task<long> GetLengthAsync(CompressionLevel compressionLevel)
+            {
+                using var mms = new MemoryStream();
+                using var compressor = CreateStream(mms, compressionLevel);
+                await uncompressedStream.CopyToAsync(compressor);
+                compressor.Flush();
+                return mms.Length;
+            }
+
+            long noCompressionLength = await GetLengthAsync(CompressionLevel.NoCompression);
+            long fastestLength = await GetLengthAsync(CompressionLevel.Fastest);
+            long optimalLength = await GetLengthAsync(CompressionLevel.Optimal);
+            long smallestLength = await GetLengthAsync(CompressionLevel.SmallestSize);
+
+            Assert.True(noCompressionLength >= fastestLength);
+            Assert.True(fastestLength >= optimalLength);
+            Assert.True(optimalLength >= smallestLength);
+        }
     }
 
     internal sealed class BadWrappedStream : MemoryStream
