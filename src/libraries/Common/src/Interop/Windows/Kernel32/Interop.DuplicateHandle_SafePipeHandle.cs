ef2a1878793e7e3fc3060396d3d2655ac53b1316
Stephen Toub <stoub@microsoft.com>
2020-11-17 17:48:19 -0500

    Remove some overhead from Process.Start (#44691)
    - Avoid StringBuilder marshaling
    - Let CreateProcess{WithLogonW} determine the current working directory
    - Avoid creating SafeHandles for GetCurrentProcess()
    - Avoid forcing ProcessStartInfo.ArgumentList into existence just to check if it contains anything
    - Prefer using ProcessStartInfo.Arguments in Start(string, IEnumerable<string>) if there's only one string in the enumerable
    - Use ValueStringBuilder instead of StringBuilder to build up arguments, so as to use stack space / pooled char[]s and avoid actually needing to produce strings.
    - Avoid unnecessary SafeHandle for PROCESS_INFORMATION.hThread

--- src/libraries/Common/src/Interop/Windows/Kernel32/Interop.DuplicateHandle_SafePipeHandle.cs
+++ src/libraries/Common/src/Interop/Windows/Kernel32/Interop.DuplicateHandle_SafePipeHandle.cs
@@ -9,12 +9,12 @@ internal static partial class Interop
 {
     internal static partial class Kernel32
     {
-        [DllImport(Interop.Libraries.Kernel32, SetLastError = true)]
+        [DllImport(Libraries.Kernel32, SetLastError = true)]
         internal static extern bool DuplicateHandle(
             IntPtr hSourceProcessHandle,
-            IntPtr hSourceHandle,
+            SafeHandle hSourceHandle,
             IntPtr hTargetProcessHandle,
-            ref SafeAccessTokenHandle lpTargetHandle,
+            out SafePipeHandle lpTargetHandle,
             uint dwDesiredAccess,
             bool bInheritHandle,
             uint dwOptions);
