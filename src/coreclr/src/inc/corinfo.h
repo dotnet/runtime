c155d321499c1b4e33d6376c1cffdd933a11b27f
David Wrighton <davidwr@microsoft.com>
2020-11-20 12:24:25 -0800

    Change jitinterace code to include corjit.h - Removes duplicate copy of jit ee api version guid - Sets up for making jit interface wrapper actually implement the jit interface
    Update generater to match previous manual modification
    
    Use ICorJitInfo interface for jit thunk directly instead of relying on parallel definition to match up
    - Found a bug in handling of TryResolveToken
    
    Generate api names
    
    Generate jit api wrapper from thunk generator
    
    Generate icorjitinfoimpl.h
    
    spmi counter shim icorjitinfo
    
    superpmi-shim-simple
    
    Update linux script
    
    Fix utf8 char out marshalling
    
    Builds on Linux but doesn't work
    
    Compile with PAL
    
    Respond to code review feedback

--- src/coreclr/src/inc/corinfo.h
+++ src/coreclr/src/inc/corinfo.h
@@ -184,37 +184,9 @@ TODO: Talk about initializing strutures before use
 #ifndef _COR_INFO_H_
 #define _COR_INFO_H_
 
-#include <corhdr.h>
+#include "corhdr.h"
 #include <specstrings.h>
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-// NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE
-//
-// #JITEEVersionIdentifier
-//
-// This GUID represents the version of the JIT/EE interface. Any time the interface between the JIT and
-// the EE changes (by adding or removing methods to any interface shared between them), this GUID should
-// be changed. This is the identifier verified by ICorJitCompiler::getVersionIdentifier().
-//
-// You can use "uuidgen.exe -s" to generate this value.
-//
-// **** NOTE TO INTEGRATORS:
-//
-// If there is a merge conflict here, because the version changed in two different places, you must
-// create a **NEW** GUID, not simply choose one or the other!
-//
-// NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE NOTE
-//
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-constexpr GUID JITEEVersionIdentifier = { /* 0d235fe4-65a1-487a-8553-c845496da901 */
-    0x0d235fe4,
-    0x65a1,
-    0x487a,
-    {0x85, 0x53, 0xc8, 0x45, 0x49, 0x6d, 0xa9, 0x01}
-};
-
+#include "jiteeversionguid.h"
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
 // END JITEEVersionIdentifier
@@ -2758,8 +2730,9 @@ public:
     // to interface functions that may throw exceptions without needing to be aware of
     // the EH ABI, exception types, etc. Returns true if the given function completed
     // successfully and false otherwise.
+    typedef void (*errorTrapFunction)(void*);
     virtual bool runWithErrorTrap(
-        void (*function)(void*), // The function to run
+        errorTrapFunction function, // The function to run
         void* parameter          // The context parameter that will be passed to the function and the handler
         ) = 0;
 
