fb01382e3a7c49c4917b94e1087302f3a0af82bb
Stephen Toub <stoub@microsoft.com>
2020-11-25 16:16:49 -0500

    Update src/libraries/Microsoft.Extensions.Options/src/OptionsCache.cs
    Co-authored-by: David Fowler <davidfowl@gmail.com>

--- src/libraries/Microsoft.Extensions.Options/src/OptionsCache.cs
+++ src/libraries/Microsoft.Extensions.Options/src/OptionsCache.cs
@@ -15,7 +15,7 @@ namespace Microsoft.Extensions.Options
         IOptionsMonitorCache<TOptions>
         where TOptions : class
     {
-        private readonly ConcurrentDictionary<string, Lazy<TOptions>> _cache = new ConcurrentDictionary<string, Lazy<TOptions>>(concurrencyLevel: 1, capacity: 31, StringComparer.Ordinal);
+        private readonly ConcurrentDictionary<string, Lazy<TOptions>> _cache = new ConcurrentDictionary<string, Lazy<TOptions>>(concurrencyLevel: 1, capacity: 31, StringComparer.Ordinal); // 31 == default capacity
 
         /// <summary>
         /// Clears all options instances from the cache.
