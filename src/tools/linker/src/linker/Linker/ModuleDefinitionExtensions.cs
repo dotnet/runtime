2075431ac586a9771a86997427cf9498a6f529fa
Mateo Torres-Ruiz <mateoatr@users.noreply.github.com>
2020-11-24 23:31:19 -0800

    Try to resolve type from exported types (dotnet/linker#1587)
    * Check exported types
    
    * Add method extension
    
    * Add generic type test
    
    * Add nested type test
    
    * Add comment
    
    Commit migrated from https://github.com/dotnet/linker/commit/a878c6fbdc08b556b350632c8f3dc4428f0eba74

--- src/tools/linker/src/linker/Linker/ModuleDefinitionExtensions.cs
+++ src/tools/linker/src/linker/Linker/ModuleDefinitionExtensions.cs
@@ -10,5 +10,26 @@ namespace Mono.Linker
 			return (module.Attributes & ModuleAttributes.ILOnly) == 0 &&
 				(module.Attributes & ModuleAttributes.ILLibrary) != 0;
 		}
+
+		public static TypeDefinition ResolveType (this ModuleDefinition module, string typeFullName)
+		{
+			if (typeFullName == null)
+				return null;
+
+			var type = module.GetType (typeFullName);
+			if (type != null)
+				return type;
+
+			if (!module.HasExportedTypes)
+				return null;
+
+			// When resolving a forwarded type from a string, typeFullName should be a simple type name.
+			int idx = typeFullName.LastIndexOf ('.');
+			(string typeNamespace, string typeName) = idx > 0 ? (typeFullName.Substring (0, idx), typeFullName.Substring (idx + 1)) :
+				(string.Empty, typeFullName);
+
+			TypeReference typeReference = new TypeReference (typeNamespace, typeName, module, module);
+			return typeReference.Resolve ();
+		}
 	}
 }
---
+++ src/tools/linker/test/Mono.Linker.Tests.Cases/DynamicDependencies/DynamicDependencyOnForwardedType.cs
@@ -0,0 +1,30 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
+using Mono.Linker.Tests.Cases.Expectations.Metadata;
+
+namespace Mono.Linker.Tests.Cases.DynamicDependencies
+{
+	[SetupCSharpCompilerToUse ("csc")]
+	[SetupCompileBefore ("FacadeAssembly.dll", new[] { "Dependencies/ReferenceImplementationLibrary.cs" })]
+	[SetupCompileAfter ("ImplementationLibrary.dll", new[] { "Dependencies/ImplementationLibrary.cs" })]
+	[SetupCompileAfter ("FacadeAssembly.dll", new[] { "Dependencies/FacadeAssembly.cs" }, new[] { "ImplementationLibrary.dll" })]
+	[LogDoesNotContain ("IL2036")]
+	public class DynamicDependencyOnForwardedType
+	{
+		[DynamicDependency (".ctor", "Mono.Linker.Tests.Cases.DynamicDependencies.Dependencies.ImplementationLibrary", "FacadeAssembly")]
+		[DynamicDependency (".ctor", "Mono.Linker.Tests.Cases.DynamicDependencies.Dependencies.ImplementationLibraryGenericType`2", "FacadeAssembly")]
+		[DynamicDependency (".ctor", "Mono.Linker.Tests.Cases.DynamicDependencies.Dependencies.ImplementationLibrary.NestedType", "FacadeAssembly")]
+		static void Main ()
+		{
+		}
+	}
+}
