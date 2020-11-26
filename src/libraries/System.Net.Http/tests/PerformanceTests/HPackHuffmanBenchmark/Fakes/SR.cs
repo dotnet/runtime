3bcd31d5a1567fd022f41c375bb3953db94cc5dd
Roman Konecny <rokonecn@microsoft.com>
2020-11-26 09:11:18 +0100

    Added benchmark

---
+++ src/libraries/System.Net.Http/tests/PerformanceTests/HPackHuffmanBenchmark/Fakes/SR.cs
@@ -0,0 +1,10 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System
+{
+    internal class SR
+    {
+        internal static string @net_http_hpack_huffman_decode_failed => "Huffman-coded literal string failed to decode.";
+    }
+}
