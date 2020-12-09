b62af1a9e8597537570a1b66e13d0a648ebd2615
Alexander Köplinger <alex.koeplinger@outlook.com>
2020-12-09 13:57:08 +0100

    Remove autotools dependencies from the build requirements (#45825)
    Now that Mono has switched to CMake we no longer need the autoconf, automake and libtool build dependencies.

--- eng/install-native-dependencies.sh
+++ eng/install-native-dependencies.sh
@@ -24,7 +24,7 @@ if [ "$1" = "Linux" ]; then
     if [ "$?" != "0" ]; then
        exit 1;
     fi
-    sudo apt install cmake llvm-3.9 clang-3.9 lldb-3.9 liblldb-3.9-dev libunwind8 libunwind8-dev gettext libicu-dev liblttng-ust-dev libcurl4-openssl-dev libssl-dev libkrb5-dev libnuma-dev autoconf automake libtool build-essential
+    sudo apt install cmake llvm-3.9 clang-3.9 lldb-3.9 liblldb-3.9-dev libunwind8 libunwind8-dev gettext libicu-dev liblttng-ust-dev libcurl4-openssl-dev libssl-dev libkrb5-dev libnuma-dev build-essential
     if [ "$?" != "0" ]; then
         exit 1;
     fi
