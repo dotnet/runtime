02ec1e92c871d92b1322a4ed2a1ad337164659ca
Santiago Fernandez Madero <safern@microsoft.com>
2020-12-08 17:45:52 -0800

    Add workarounds for brew on old OSX images (#45802)
    * Add workarounds for brew on old OSX images
    
    * Don't remove openssl directories

--- eng/install-native-dependencies.sh
+++ eng/install-native-dependencies.sh
@@ -30,6 +30,13 @@ if [ "$1" = "Linux" ]; then
     fi
 elif [ "$1" = "OSX" ] || [ "$1" = "tvOS" ] || [ "$1" = "iOS" ]; then
     engdir=$(dirname "${BASH_SOURCE[0]}")
+
+    if [ "$3" = "azDO" ]; then
+        # workaround for old osx images on hosted agents
+        # piped in case we get an agent without these values installed
+        brew uninstall openssl@1.0.2t 2>&1 | true
+    fi
+
     brew update --preinstall
     brew bundle --no-upgrade --no-lock --file "${engdir}/Brewfile"
     if [ "$?" != "0" ]; then
