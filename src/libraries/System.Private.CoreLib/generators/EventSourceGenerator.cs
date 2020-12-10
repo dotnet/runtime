c99cc08cc80386d82ed42ab22bc7b2dd32b440d6
Ben Adams <thundercat@illyriad.co.uk>
2020-12-10 04:31:17 +0000

    Revert "Change to record"
    This reverts commit 5c8092a876d348315c1ea8a81780f46d5f6afb3e.

--- src/libraries/System.Private.CoreLib/generators/EventSourceGenerator.cs
+++ src/libraries/System.Private.CoreLib/generators/EventSourceGenerator.cs
@@ -97,13 +97,12 @@ namespace Generators
             }
         }
 
-        // Can change to terse record syntax as isn't supported by netstandard 2.0
-        private record EventSourceClass
+        private class EventSourceClass
         {
-            public string Namespace { get; set; } = string.Empty;
-            public string ClassName { get; set; } = string.Empty;
-            public string SourceName { get; set; } = string.Empty;
-            public Guid Guid { get; set; } = Guid.Empty;
+            public string Namespace = string.Empty;
+            public string ClassName = string.Empty;
+            public string SourceName = string.Empty;
+            public Guid Guid = Guid.Empty;
         }
     }
 }
