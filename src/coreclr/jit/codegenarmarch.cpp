e6f399a3c5758ac81cb407866465bad5e56c31aa
Sergey Andreenko <seandree@microsoft.com>
2020-12-10 14:04:11 -0800

    Update src/coreclr/jit/codegenarmarch.cpp

--- src/coreclr/jit/codegenarmarch.cpp
+++ src/coreclr/jit/codegenarmarch.cpp
@@ -918,7 +918,7 @@ void CodeGen::genPutArgStk(GenTreePutArgStk* treeNode)
                 // We can generate a smaller code if store size is a multiple of TARGET_POINTER_SIZE.
                 // The dst size can be rounded up to PUTARG_STK size.
                 // The src size can be rounded up if it reads a local variable slot because the local
-                // variable stack allocation size is rounded up to be a multiple of the slot size.
+                // variable stack allocation size is rounded up to be a multiple of the TARGET_POINTER_SIZE.
                 // The exception  is arm64 apple arguments because they can be packed without padding.
                 if (varNode != nullptr)
                 {
