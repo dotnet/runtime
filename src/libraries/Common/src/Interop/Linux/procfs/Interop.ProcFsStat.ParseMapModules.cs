ec9785af35863ebc0a10f0693cf4ac21d9ff3674
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2020-12-10 11:23:02 +0200

    Decouple line parsing from ProcessModule parsing

--- src/libraries/Common/src/Interop/Linux/procfs/Interop.ProcFsStat.ParseMapModules.cs
+++ src/libraries/Common/src/Interop/Linux/procfs/Interop.ProcFsStat.ParseMapModules.cs
@@ -15,39 +15,8 @@ internal static partial class Interop
     {
         private const string MapsFileName = "/maps";
 
-        private static string GetMapsFilePathForProcess(int pid)
-        {
-            return RootPath + pid.ToString(CultureInfo.InvariantCulture) + MapsFileName;
-        }
-
-        private static (long Start, int Size) TryParseAddressRange(string s, ref int start, ref int end)
-        {
-            int pos = s.IndexOf('-', start, end - start);
-            if (pos > 0)
-            {
-                if (long.TryParse(s.AsSpan(start, pos), NumberStyles.HexNumber, CultureInfo.InvariantCulture, out long startingAddress) &&
-                    long.TryParse(s.AsSpan(pos + 1, end - (pos + 1)), NumberStyles.HexNumber, CultureInfo.InvariantCulture, out long endingAddress))
-                {
-                    return (startingAddress, (int)(endingAddress - startingAddress));
-                }
-            }
-
-            return default;
-        }
-
-        private static bool HasReadAndExecFlags(string s, ref int start, ref int end)
-        {
-            bool sawRead = false, sawExec = false;
-            for (int i = start; i < end; i++)
-            {
-                if (s[i] == 'r')
-                    sawRead = true;
-                else if (s[i] == 'x')
-                    sawExec = true;
-            }
-
-            return sawRead & sawExec;
-        }
+        private static string GetMapsFilePathForProcess(int pid) =>
+            RootPath + pid.ToString(CultureInfo.InvariantCulture) + MapsFileName;
 
         internal static ProcessModuleCollection? ParseMapsModules(int pid)
         {
@@ -71,68 +40,52 @@ internal static partial class Interop
 
             foreach (string line in lines)
             {
-                // Use a StringParser to avoid string.Split costs
-                var parser = new StringParser(line, separator: ' ', skipEmpty: true);
-
-                // Parse the address start and size
-                (long Start, int Size) addressRange = parser.ParseRaw(TryParseAddressRange);
-
-                if (addressRange == default)
+                if (!TryParseMapsEntry(line, out (long StartAddress, int Size, bool HasReadAndExecFlags, string Path) parsedLine))
                 {
-                    Commit();
+                    // Invalid entry for the purposes of ProcessModule parsing,
+                    // discard flushing the current module if it exists.
+                    CommitCurrentModule();
                     continue;
                 }
 
-                // Parse the permissions
-                bool lineHasReadAndExecFlags = parser.ParseRaw(HasReadAndExecFlags);
-
-                // Skip past the offset, dev, and inode fields
-                parser.MoveNext();
-                parser.MoveNext();
-                parser.MoveNext();
-
-                // we only care about the named modules
-                if (!parser.MoveNext())
+                // Check if entry is a continuation of the current module.
+                if (module is not null &&
+                    module.FileName == parsedLine.Path &&
+                    (long)module.BaseAddress + module.ModuleMemorySize == parsedLine.StartAddress)
                 {
-                    Commit();
+                    // Is continuation, update the current module.
+                    module.ModuleMemorySize += parsedLine.Size;
+                    moduleHasReadAndExecFlags |= parsedLine.HasReadAndExecFlags;
                     continue;
                 }
 
-                // Parse the pathname
-                string pathname = parser.ExtractCurrentToEnd();
-                bool isContinuation = module?.FileName == pathname && (long)module.BaseAddress + module.ModuleMemorySize == addressRange.Start;
+                // Not a continuation, commit any current modules and create a new one.
+                CommitCurrentModule();
 
-                if (isContinuation)
-                {
-                    module!.ModuleMemorySize += addressRange.Size;
-                    moduleHasReadAndExecFlags |= lineHasReadAndExecFlags;
-                    continue;
-                }
-
-                Commit();
-
-                moduleHasReadAndExecFlags = lineHasReadAndExecFlags;
                 module = new ProcessModule
                 {
-                    FileName = pathname,
-                    ModuleName = Path.GetFileName(pathname),
-                    ModuleMemorySize = addressRange.Size,
+                    FileName = parsedLine.Path,
+                    ModuleName = Path.GetFileName(parsedLine.Path),
+                    ModuleMemorySize = parsedLine.Size,
                     EntryPointAddress = IntPtr.Zero // unknown
                 };
 
-                // on 32-bit platforms, it throws System.OverflowException without the void* cast.
+                // on 32-bit platforms, it throws System.OverflowException with IntPtr.ctor(Int64),
+                // so we use IntPtr.ctor(void*) to skip the overflow checking.
                 unsafe
                 {
-                    module.BaseAddress = new IntPtr(unchecked((void*)addressRange.Start));
+                    module.BaseAddress = new IntPtr((void*)parsedLine.StartAddress);
                 }
+
+                moduleHasReadAndExecFlags = parsedLine.HasReadAndExecFlags;
             }
 
-            // Commit last line.
-            Commit();
+            // Commit any pending modules.
+            CommitCurrentModule();
 
             return modules;
 
-            void Commit()
+            void CommitCurrentModule()
             {
                 // we only add module to collection, if at least one row had 'r' and 'x' set.
                 if (moduleHasReadAndExecFlags && module is not null)
@@ -140,6 +93,70 @@ internal static partial class Interop
                     modules.Add(module);
                     module = null;
                 }
+            }
+        }
+
+        private static bool TryParseMapsEntry(string line, out (long StartAddress, int Size, bool HasReadAndExecFlags, string Path) parsedLine)
+        {
+            // Use a StringParser to avoid string.Split costs
+            var parser = new StringParser(line, separator: ' ', skipEmpty: true);
+
+            // Parse the address start and size
+            (long start, int size) = parser.ParseRaw(TryParseAddressRange);
+
+            if (size < 0)
+            {
+                parsedLine = default;
+                return false;
+            }
+
+            // Parse the permissions
+            bool lineHasReadAndExecFlags = parser.ParseRaw(HasReadAndExecFlags);
+
+            // Skip past the offset, dev, and inode fields
+            parser.MoveNext();
+            parser.MoveNext();
+            parser.MoveNext();
+
+            // we only care about the named modules
+            if (!parser.MoveNext())
+            {
+                parsedLine = default;
+                return false;
+            }
+
+            // Parse the pathname
+            string pathname = parser.ExtractCurrentToEnd();
+            parsedLine = (start, size, lineHasReadAndExecFlags, pathname);
+            return true;
+
+            static (long Start, int Size) TryParseAddressRange(string s, ref int start, ref int end)
+            {
+                int pos = s.IndexOf('-', start, end - start);
+                if (pos > 0)
+                {
+                    if (long.TryParse(s.AsSpan(start, pos), NumberStyles.HexNumber, CultureInfo.InvariantCulture, out long startingAddress) &&
+                        long.TryParse(s.AsSpan(pos + 1, end - (pos + 1)), NumberStyles.HexNumber, CultureInfo.InvariantCulture, out long endingAddress))
+                    {
+                        return (startingAddress, (int)(endingAddress - startingAddress));
+                    }
+                }
+
+                return (0, -1);
+            }
+
+            static bool HasReadAndExecFlags(string s, ref int start, ref int end)
+            {
+                bool sawRead = false, sawExec = false;
+                for (int i = start; i < end; i++)
+                {
+                    if (s[i] == 'r')
+                        sawRead = true;
+                    else if (s[i] == 'x')
+                        sawExec = true;
+                }
+
+                return sawRead & sawExec;
             }
         }
     }
