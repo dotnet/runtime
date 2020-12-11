8559bff303b03526b4f5c0e4d618478a58c2bd9a
Adam Sitnik <adam.sitnik@gmail.com>
2020-12-11 15:43:56 +0100

    revert ordering change to address PR feedback

--- src/libraries/Microsoft.Extensions.Caching.Memory/src/CacheEntry.cs
+++ src/libraries/Microsoft.Extensions.Caching.Memory/src/CacheEntry.cs
@@ -145,6 +145,8 @@ namespace Microsoft.Extensions.Caching.Memory
             {
                 IsDisposed = true;
 
+                CacheEntryHelper.ExitScope(this, _previous);
+
                 // Don't commit or propagate options if the CacheEntry Value was never set.
                 // We assume an exception occurred causing the caller to not set the Value successfully,
                 // so don't use this entry.
@@ -158,7 +160,6 @@ namespace Microsoft.Extensions.Caching.Memory
                     }
                 }
 
-                CacheEntryHelper.ExitScope(this, _previous);
                 _previous = null; // we don't want to root unnecessary objects
             }
         }
