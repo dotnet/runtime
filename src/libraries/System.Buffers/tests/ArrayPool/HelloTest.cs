3436a5f28fd7bdadc9d5742475e326c74f347c14
Mitchell Hwang <mitchell.hwang@microsoft.com>
2020-12-15 14:36:23 -0500

    Add SystemBuffers HelloTest and only test that

---
+++ src/libraries/System.Buffers/tests/ArrayPool/HelloTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace System.Buffers.ArrayPool.Tests
+{
+    public class ArrayPoolHelloTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
