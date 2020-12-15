e2562779aa35b25b6f6ccf65e83059c64b252bf7
Marek Safar <marek.safar@gmail.com>
2020-12-15 13:59:42 +0100

    Remove static mutable state from the output step. It's not threading safe (dotnet/linker#1699)
    for parallel test runs
    
    Commit migrated from https://github.com/dotnet/linker/commit/83f69b3f9899a1f0b0d83ad9359d4506b2b0884f

--- src/tools/linker/src/linker/Linker.Steps/OutputStep.cs
+++ src/tools/linker/src/linker/Linker.Steps/OutputStep.cs
@@ -39,7 +39,7 @@ namespace Mono.Linker.Steps
 
 	public class OutputStep : BaseStep
 	{
-		private static Dictionary<UInt16, TargetArchitecture> architectureMap;
+		private Dictionary<UInt16, TargetArchitecture> architectureMap;
 
 		private enum NativeOSOverride
 		{
@@ -57,7 +57,7 @@ namespace Mono.Linker.Steps
 			assembliesWritten = new List<string> ();
 		}
 
-		static TargetArchitecture CalculateArchitecture (TargetArchitecture readyToRunArch)
+		TargetArchitecture CalculateArchitecture (TargetArchitecture readyToRunArch)
 		{
 			if (architectureMap == null) {
 				architectureMap = new Dictionary<UInt16, TargetArchitecture> ();
