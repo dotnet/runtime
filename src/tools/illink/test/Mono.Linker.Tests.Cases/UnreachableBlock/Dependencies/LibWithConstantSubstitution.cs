4751f94769308febe24f993406e047506353d6ae
Vitek Karas <vitek.karas@microsoft.com>
2020-12-16 09:06:18 -0800

    Add some more tests for constant propagation (dotnet/linker#1689)
    Fixes a problem with unsigned int comparisons when determining constant conditions
    
    Co-authored-by: Marek Safar <marek.safar@gmail.com>
    
    Commit migrated from https://github.com/dotnet/linker/commit/61b68d8b899b318f3ee09ed7f174131696e8ece1

---
+++ src/tools/illink/test/Mono.Linker.Tests.Cases/UnreachableBlock/Dependencies/LibWithConstantSubstitution.cs
@@ -0,0 +1,16 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Mono.Linker.Tests.Cases.UnreachableBlock.Dependencies
+{
+	public class LibWithConstantSubstitution
+	{
+		static bool _value;
+
+		public static bool ReturnFalse ()
+		{
+			return _value;
+		}
+	}
+}
