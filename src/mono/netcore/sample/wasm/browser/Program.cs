482855f7acec104dd6d80292cd1b3811bd528f30
Steve Pfister <steve.pfister@microsoft.com>
2020-12-16 10:21:29 -0500

    Tweak browser sample to be able to run aot

--- src/mono/netcore/sample/wasm/browser/Program.cs
+++ src/mono/netcore/sample/wasm/browser/Program.cs
@@ -2,12 +2,14 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 
 namespace Sample
 {
     public class Test
     {
+        [DynamicDependency(DynamicallyAccessedMemberTypes.All, "System.Runtime.InteropServices.JavaScript.Runtime", "System.Private.Runtime.InteropServices.JavaScript")]
         public static void Main(string[] args)
         {
             Console.WriteLine ("Hello, World!");
