acd4855de9be7b51c853c4e5c719aa456a4865c9
Egor Bogatov <egorbo@gmail.com>
2020-12-17 04:42:02 +0300

    RyuJIT: Don't emit cast helpers for (T)array.Clone() (#45311)
    Note that Array.Clone()'s return type is the same as the type of its first operand.
    
    This allows optimization of casts on the result of calls to Clone().

--- src/coreclr/jit/gentree.cpp
+++ src/coreclr/jit/gentree.cpp
@@ -17757,6 +17757,14 @@ CORINFO_CLASS_HANDLE Compiler::gtGetClassHandle(GenTree* tree, bool* pIsExact, b
         case GT_CALL:
         {
             GenTreeCall* call = tree->AsCall();
+            if (call->gtFlags & CORINFO_FLG_JIT_INTRINSIC)
+            {
+                if (lookupNamedIntrinsic(call->gtCallMethHnd) == NI_System_Array_Clone)
+                {
+                    objClass = gtGetClassHandle(call->gtCallThisArg->GetNode(), pIsExact, pIsNonNull);
+                    break;
+                }
+            }
             if (call->IsInlineCandidate())
             {
                 // For inline candidates, we've already cached the return
