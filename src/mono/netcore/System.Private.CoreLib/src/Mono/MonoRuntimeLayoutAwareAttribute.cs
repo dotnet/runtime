6224db02da59e7c3f663f03936753798a62a5944
Aleksey Kliger <alklig@microsoft.com>
2020-12-21 14:27:41 -0500

    [corelib] Add Mono.MonoRuntimeLayoutAwareAttribute

---
+++ src/mono/netcore/System.Private.CoreLib/src/Mono/MonoRuntimeLayoutAwareAttribute.cs
@@ -0,0 +1,16 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+
+namespace Mono
+{
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Field)]
+    public class MonoRuntimeLayoutAwareAttribute : Attribute
+    {
+        public MonoRuntimeLayoutAwareAttribute () {}
+
+        public MonoRuntimeLayoutAwareAttribute (string keepInSyncWith) {}
+    }
+}
