360b6744a2b4e3d1248fbf1b55299f4dc7d2ea62
Adam Sitnik <adam.sitnik@microsoft.com>
2020-12-21 22:14:40 +0100

    according to https://man7.org/linux/man-pages/man2/getgroups.2.html it's not specified if this group is included in the list returned by getgroups

--- src/libraries/System.Diagnostics.Process/tests/ProcessTests.Unix.cs
+++ src/libraries/System.Diagnostics.Process/tests/ProcessTests.Unix.cs
@@ -923,7 +923,11 @@ namespace System.Diagnostics.Tests
                 }
 
                 // Return this as a HashSet to filter out duplicates.
-                return new HashSet<uint>(groups.Slice(0, rv).ToArray());
+                var result = new HashSet<uint>(groups.Slice(0, rv).ToArray());
+                // according to https://man7.org/linux/man-pages/man2/getgroups.2.html it's not specified
+                // if this group is included in the list returned by getgroups
+                result.Add(getegid());
+                return result;
             }
         }
 
