9584b15b1cfd4fb7c36e29f183ecbbfadb12a6cc
xiang17 <xili9@microsoft.com>
2021-01-02 22:53:32 -0800

    Revert "Use HexConverter.FromLowerChar"
    This reverts commit 46101a8a5a4d0bd8a5c1123055cdf6763913159b.

--- src/libraries/System.Diagnostics.DiagnosticSource/src/System/Diagnostics/Activity.cs
+++ src/libraries/System.Diagnostics.DiagnosticSource/src/System/Diagnostics/Activity.cs
@@ -832,7 +832,9 @@ namespace System.Diagnostics
             //  = 55 chars (see https://w3c.github.io/trace-context)
             // The version (00-fe) is used to indicate that this is a WC3 ID.
             return id.Length == 55 &&
-                   ((HexConverter.FromLowerChar(id[0]) << 4) + HexConverter.FromLowerChar(id[1]) < 255);
+                   ('0' <= id[0] && id[0] <= '9' || 'a' <= id[0] && id[0] <= 'f') &&
+                   ('0' <= id[1] && id[1] <= '9' || 'a' <= id[1] && id[1] <= 'f') &&
+                   (id[0] != 'f' || id[1] != 'f');
         }
 
 #if ALLOW_PARTIALLY_TRUSTED_CALLERS
