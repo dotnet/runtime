b75bb4826a47c8d0aabef3eaf5b7206d1cc420d4
Justin Kotalik <jukotali@microsoft.com>
2021-01-04 14:17:58 -0800

    Set only on connection

--- src/libraries/System.Net.Quic/src/System/Net/Quic/Implementations/MsQuic/MsQuicListener.cs
+++ src/libraries/System.Net.Quic/src/System/Net/Quic/Implementations/MsQuic/MsQuicListener.cs
@@ -51,8 +51,6 @@ namespace System.Net.Quic.Implementations.MsQuic
             _listenEndPoint = options.ListenEndPoint!;
 
             _ptr = _session.ListenerOpen(options);
-
-            SetIdleTimeout(options.IdleTimeout);
         }
 
         internal override IPEndPoint ListenEndPoint
@@ -141,11 +139,6 @@ namespace System.Net.Quic.Implementations.MsQuic
             SetListenPort();
         }
 
-        private void SetIdleTimeout(TimeSpan timeout)
-        {
-            MsQuicParameterHelpers.SetULongParam(MsQuicApi.Api, _ptr, (uint)QUIC_PARAM_LEVEL.LISTENER, (uint)QUIC_PARAM_CONN.IDLE_TIMEOUT, (ulong)timeout.TotalMilliseconds);
-        }
-
         internal override void Close()
         {
             ThrowIfDisposed();
@@ -173,7 +166,7 @@ namespace System.Net.Quic.Implementations.MsQuic
                             IPEndPoint localEndPoint = MsQuicAddressHelpers.INetToIPEndPoint(ref *(SOCKADDR_INET*)connectionInfo.LocalAddress);
                             IPEndPoint remoteEndPoint = MsQuicAddressHelpers.INetToIPEndPoint(ref *(SOCKADDR_INET*)connectionInfo.RemoteAddress);
 
-                            MsQuicConnection msQuicConnection = new MsQuicConnection(localEndPoint, remoteEndPoint, evt.Data.NewConnection.Connection);
+                            MsQuicConnection msQuicConnection = new MsQuicConnection(localEndPoint, remoteEndPoint, evt.Data.NewConnection.Connection, _options.IdleTimeout);
                             msQuicConnection.SetNegotiatedAlpn(connectionInfo.NegotiatedAlpn, connectionInfo.NegotiatedAlpnLength);
 
                             _acceptConnectionQueue.Writer.TryWrite(msQuicConnection);
