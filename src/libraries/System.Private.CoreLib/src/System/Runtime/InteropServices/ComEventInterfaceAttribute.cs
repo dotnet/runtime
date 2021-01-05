052df28eb3541e48eb305b20a25e26eed0ebaf30
Eric Erhardt <eric.erhardt@microsoft.com>
2021-01-05 15:31:43 -0600

    Resolve ILLink warnings in System.Runtime.InteropServices
    Contributes to #45623

--- src/libraries/System.Private.CoreLib/src/System/Runtime/InteropServices/ComEventInterfaceAttribute.cs
+++ src/libraries/System.Private.CoreLib/src/System/Runtime/InteropServices/ComEventInterfaceAttribute.cs
@@ -1,17 +1,20 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics.CodeAnalysis;
+
 namespace System.Runtime.InteropServices
 {
     [AttributeUsage(AttributeTargets.Interface, Inherited = false)]
     public sealed class ComEventInterfaceAttribute : Attribute
     {
-        public ComEventInterfaceAttribute(Type SourceInterface, Type EventProvider)
+        public ComEventInterfaceAttribute([DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicMethods)] Type SourceInterface, Type EventProvider)
         {
             this.SourceInterface = SourceInterface;
             this.EventProvider = EventProvider;
         }
 
+        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicMethods)]
         public Type SourceInterface { get; }
         public Type EventProvider { get; }
     }
