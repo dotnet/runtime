1b1ff800dae81574d9ecea0d106df9c71a6733da
Dan Moseley <danmose@microsoft.com>
2021-01-07 19:04:13 -0800

    Port System.Speech to .NET Core (#45941)
    * Initial sources, with banners
    
    * Run the code formatter
    
    * Fix hang in XUnit due to failing to complete all AsyncOperation-s
    
    * Remove reflection over RegistryKey
    
    * Add ref and packaging
    
    * Add tests
    
    * Add sln
    
    * Fix CS1584
    
    * Fix CA1823
    
    * Fix CA1834
    
    * Unnecessary suppressions
    
    * Fix SA1028
    
    * Fix CA1507
    
    * Fix CA1810
    
    * Fix CA1825
    
    * Fix CA1825
    
    * Unnecessary suppressions
    
    * Fix CA1805
    
    * Fix IDE0004
    
    * Fix IDE0090
    
    * Remove CAS
    
    * Remove tabs and dead code
    
    * Unnecessary suppressions
    
    * Fix SA1212
    
    * Fix SA1121
    
    * Disable SA1129
    
    * Fix SA1206
    
    * Fix SA1518
    
    * Fix SA1617
    
    * Fix SA1001
    
    * Fix CS0618
    
    * Remove unnecessary comments
    
    * Remove unnecessary whitespace
    
    * Remove low value xml doc comments
    
    * Unused usings
    
    * dead files
    
    * Remove CAS
    
    * More junk
    
    * Fix obvious original bug
    
    * Remove/insert newlines
    
    * Remove reference to old design document
    
    * Fix spacing
    
    * Fix typo name
    
    * Fix file casing
    
    * Remove dead code
    
    * Add to compat pack
    
    * Remove AppDomain etc
    
    * Fix casing of .NET
    
    * Remove low value XML docs
    
    * Remove code that relies on compiling assemblies
    
    * Fix inadvertently removed padding
    
    * Use EDI to preserve stack when rethrowing
    
    * Fix misaligned resource ID's to match sperror.h
    
    * Skip SpeechRecognitionEngine tests if no installed recognizers
    
    * Fix misformatted string bug
    
    * Logging for CI error
    
    * Fix NRE trying to map phonemes for voice for culture for which we do not have phoneme map
    
    * Fix 153 spelling errors in comments using `Visual Studio Spell Checker`
    
    * Remove extraneous file
    
    * Fix spacing
    
    * Fix project reference
    
    * Reorder properties in csproj
    
    * Change from netcoreapp2.0 to netcoreapp2.1
    
    * Update src/libraries/System.Speech/pkg/System.Speech.pkgproj
    
    Co-authored-by: Jose Perez Rodriguez <joperezr@microsoft.com>
    
    * Add build error for targeting netcoreapp2.0
    
    * Suppress new error during packaging testing
    
    * Update System.Speech.targets
    
    * Remove ref comments
    
    * Update pkgproj
    
    * Remove placeholder
    
    Co-authored-by: Jose Perez Rodriguez <joperezr@microsoft.com>
    Co-authored-by: Viktor Hofer <viktor.hofer@microsoft.com>

---
+++ src/libraries/System.Speech/tests/GrammarTests.cs
@@ -0,0 +1,175 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.IO;
+using System.Speech.Recognition;
+using System.Speech.Recognition.SrgsGrammar;
+using System.Text;
+using System.Xml;
+using Xunit;
+
+namespace SampleSynthesisTests
+{
+    public class GrammarTests : FileCleanupTestBase
+    {
+        [Fact]
+        public void WriteStronglyTypedGrammarToXml()
+        {
+            SrgsDocument srgsDoc = CreateSrgsDocument();
+
+            var builder = new StringBuilder();
+            using (XmlWriter writer = XmlWriter.Create(builder))
+            {
+                srgsDoc.WriteSrgs(writer);
+            }
+
+            Assert.Contains("someRule", builder.ToString());
+        }
+
+        [Fact]
+        public void CompileStronglyTypedGrammarToCfg()
+        {
+            SrgsDocument srgsDoc = CreateSrgsDocument();
+
+            using var ms = new MemoryStream();
+            SrgsGrammarCompiler.Compile(srgsDoc, ms);
+
+            Assert.True(ms.Position > 0);
+        }
+
+        [Fact]
+        public void CompileStronglyTypedGrammarFromFileToCfg()
+        {
+            SrgsDocument srgsDoc = CreateSrgsDocument();
+
+            string temp = GetTestFilePath();
+
+            using (XmlWriter writer = XmlWriter.Create(temp))
+            {
+                srgsDoc.WriteSrgs(writer);
+            }
+
+            using var ms = new MemoryStream();
+
+            SrgsGrammarCompiler.Compile(temp, ms);
+
+            Assert.True(ms.Position > 0);
+        }
+
+        [Fact]
+        public void CompileStronglyTypedGrammarToDllFromPath()
+        {
+            SrgsDocument srgsDoc = CreateSrgsDocument();
+
+            string temp = GetTestFilePath();
+
+            // Cannot compile to assemblies on .NET Core
+            Assert.Throws<PlatformNotSupportedException>(() => SrgsGrammarCompiler.CompileClassLibrary(srgsDoc, temp, new string[0], keyFile: null));
+        }
+
+        [Fact]
+        public void ParseGrammarXml()
+        {
+            string xml = @"<grammar version=""1.0"" xml:lang=""en-US"" root=""playCommands"" xmlns=""http://www.w3.org/2001/06/grammar"">
+                             <rule id=""playCommands"">
+                               <ruleref uri=""#playAction"" />
+                               <item> the </item>
+                               <ruleref uri=""#fileWords"" />
+                             </rule>
+                             <rule id=""playAction"">
+                               <one-of>
+                                 <item> play </item>
+                                 <item> start </item>
+                                 <item> begin </item>
+                               </one-of>
+                             </rule>
+                             <rule id=""fileWords"">
+                               <one-of>
+                                 <item> song </item>
+                                 <item> tune </item>
+                                 <item> track </item>
+                                 <item> item </item>
+                               </one-of>
+                             </rule>
+
+                           </grammar>";
+            var grammar = new Grammar(new MemoryStream(Encoding.Unicode.GetBytes(xml)));
+
+            grammar.Name = "test";
+        }
+
+        [ConditionalFact(typeof(SynthesizeRecognizeTests), nameof(SynthesizeRecognizeTests.HasInstalledRecognizers))]
+        public void GrammarBuilder()
+        {
+            Choices colorChoice = new Choices(new string[] { "red", "green", "blue" });
+            GrammarBuilder colorElement = new GrammarBuilder(colorChoice);
+
+            GrammarBuilder makePhrase = new GrammarBuilder("Make background");
+            makePhrase.Append(colorElement);
+            GrammarBuilder setPhrase = new GrammarBuilder("Set background to");
+            setPhrase.Append(colorElement);
+
+            Choices bothChoices = new Choices(new GrammarBuilder[] { makePhrase, setPhrase });
+            Grammar grammar = new Grammar((GrammarBuilder)bothChoices);
+            grammar.Name = "backgroundColor";
+
+            using (var rec = new SpeechRecognitionEngine())
+            {
+                rec.LoadGrammar(grammar);
+            }
+        }
+
+        [Fact]
+        public void CreateMoreElaborateGrammar()
+        {
+            Choices fruit = new Choices("oranges", "apples", "bananas");
+            Choices vegetable = new Choices("cabbage", "carrots", "spinach");
+            Choices misc = new Choices("chocolate bars", "coke bottles", "cookies");
+
+            string[] quantities = new string[9];
+            for (int i = 2; i < 11; ++i)
+            {
+                quantities[i - 2] = i.ToString();
+            }
+
+            Choices quantity = new Choices(quantities);
+
+            GrammarBuilder gb = new GrammarBuilder("I would like");
+            gb.Append(new SemanticResultKey("Quantity", quantity));
+            gb.Append(new SemanticResultKey("Fruit", fruit));
+
+            GrammarBuilder gb2 = new GrammarBuilder("and");
+            gb2.Append(new SemanticResultKey("Quantity", quantity));
+            gb2.Append(new SemanticResultKey("Misc", misc));
+
+            gb.Append(new SemanticResultKey("Misc", gb2), 0, 1);
+            gb.Append("and some");
+            gb.Append(new SemanticResultKey("Vegetable", vegetable));
+
+            SrgsDocument srgsDoc = new SrgsDocument(gb);
+
+            var builder = new StringBuilder();
+            using (XmlWriter writer = XmlWriter.Create(builder))
+            {
+                srgsDoc.WriteSrgs(writer);
+            }
+
+            Assert.Contains("oranges", builder.ToString());
+        }
+
+        private SrgsDocument CreateSrgsDocument()
+        {
+            SrgsDocument srgsDoc = new SrgsDocument();
+            SrgsRule rule = new SrgsRule("someRule");
+            SrgsItem item = new SrgsItem("someItem");
+            item.Add(new SrgsSemanticInterpretationTag("out = \"foo\";"));
+            SrgsOneOf oneOf = new SrgsOneOf(item);
+            rule.Add(oneOf);
+
+            srgsDoc.Rules.Add(rule);
+            srgsDoc.Root = rule;
+            return srgsDoc;
+        }
+    }
+}
---
+++ src/libraries/System.Speech/tests/SynthesizeRecognizeTests.cs
@@ -0,0 +1,191 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Speech.AudioFormat;
+using System.Speech.Recognition;
+using System.Speech.Recognition.SrgsGrammar;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading;
+using System.Xml;
+using Xunit;
+
+namespace SampleSynthesisTests
+{
+    [ConditionalClass(typeof(PlatformDetection), nameof(PlatformDetection.IsNotWindowsNanoServer))] // No SAPI on Nano
+    public class SynthesizeRecognizeTests : FileCleanupTestBase
+    {
+        // Our Windows 7 and Windows 8.1 queues seem to have no recognizers installed
+        public static bool HasInstalledRecognizers => PlatformDetection.IsNotWindowsNanoServer && SpeechRecognitionEngine.InstalledRecognizers().Count > 0;
+
+        [ConditionalFact(nameof(HasInstalledRecognizers))]
+        public void SpeechSynthesizerToSpeechRecognitionEngine()
+        {
+            if (Thread.CurrentThread.CurrentCulture.ToString() != "en-US")
+                return;
+
+            using var ms = new MemoryStream();
+
+            using (var synth = new SpeechSynthesizer())
+            {
+                synth.SetOutputToWaveStream(ms);
+                var prompt = new Prompt("synthesizer");
+                synth.Speak(prompt);
+            }
+
+            ms.Position = 0;
+
+            using (var rec = new SpeechRecognitionEngine())
+            {
+                rec.LoadGrammar(new DictationGrammar());
+                rec.SetInputToWaveStream(ms);
+                RecognitionResult result = rec.Recognize();
+
+                Assert.True(result.Confidence > 0.1);
+                // handles "synthesizer", "synthesizes", etc.
+                Assert.StartsWith("synthe", result.Text, StringComparison.OrdinalIgnoreCase);
+            }
+        }
+
+        [ConditionalFact(nameof(HasInstalledRecognizers))]
+        public void SpeechRecognizerInvalidInput()
+        {
+            using var ms = new MemoryStream();
+            ms.WriteByte(1);
+
+            using (var rec = new SpeechRecognitionEngine())
+            {
+                Assert.Throws<FormatException>(() => rec.SetInputToWaveStream(ms));
+            }
+        }
+
+        [ConditionalFact(nameof(HasInstalledRecognizers))]
+        public void SpeechRecognizerProperties()
+        {
+            using (var rec = new SpeechRecognitionEngine())
+            {
+                rec.SetInputToNull();
+                rec.InitialSilenceTimeout = new TimeSpan();
+                rec.BabbleTimeout = new TimeSpan();
+                rec.EndSilenceTimeout = new TimeSpan();
+                rec.EndSilenceTimeoutAmbiguous = new TimeSpan();
+                rec.MaxAlternates = 1;
+
+                Assert.Throws<KeyNotFoundException>(() => rec.QueryRecognizerSetting("foo"));
+                Assert.Throws<KeyNotFoundException>(() => rec.UpdateRecognizerSetting("foo", "bar"));
+                Assert.Throws<KeyNotFoundException>(() => rec.UpdateRecognizerSetting("foo", 1));
+            }
+        }
+
+        [Fact]
+        public void SpeechSynthesizerToWavAndRepeat()
+        {
+            string wav = GetTestFilePath() + ".wav";
+
+            using (var synth = new SpeechSynthesizer())
+            {
+                synth.SetOutputToWaveFile(wav);
+                synth.Speak("hello");
+            }
+
+            Assert.True(new FileInfo(wav).Length > 0);
+
+            using var ms = new MemoryStream();
+            using (var synth = new SpeechSynthesizer())
+            {
+                synth.SetOutputToWaveStream(ms);
+
+                var builder = new PromptBuilder();
+                builder.AppendAudio(wav);
+                synth.Speak(builder);
+
+                Assert.True(ms.Position > 0);
+            }
+        }
+
+        [Fact]
+        public void SpeechSynthSsmlInvalidPhoneme()
+        {
+            using (var synth = new SpeechSynthesizer())
+            {
+                synth.SetOutputToNull();
+
+                string ssml = @"
+<speak version='1.0' xml:lang='en-US' xmlns='https://www.w3.org/2001/10/synthesis'>
+	<s>His name is Mike <phoneme alphabet='ups' ph='@#$#@$'>Zhou </phoneme></s>
+</speak>";
+                Assert.Throws<FormatException>(() => synth.SpeakSsml(ssml));
+                ssml = @"
+<speak version='1.0' xml:lang='en-US' xmlns='https://www.w3.org/2001/10/synthesis'>
+	<s>His name is Mike <phoneme alphabet='@#$@#$' ph='JH'>Zhou </phoneme></s>
+</speak>";
+                Assert.Throws<FormatException>(() => synth.SpeakSsml(ssml));
+            }
+        }
+
+        [Fact]
+        public void SpeechSynthesizerEventsAndProperties()
+        {
+            using (var synth = new SpeechSynthesizer())
+            {
+                using var ms = new MemoryStream();
+
+                synth.SetOutputToNull();
+                synth.SetOutputToAudioStream(ms, new SpeechAudioFormatInfo(16000, AudioBitsPerSample.Sixteen, AudioChannel.Stereo));
+                synth.SelectVoiceByHints(VoiceGender.Male, VoiceAge.Adult);
+                Assert.True(synth.Volume > 0);
+                Assert.NotNull(synth.Voice);
+                Assert.NotEmpty(synth.GetInstalledVoices());
+                Assert.Null(synth.GetCurrentlySpokenPrompt());
+
+                var builder = new PromptBuilder();
+                builder.AppendText("synthesizer");
+
+                int events = 0;
+                synth.BookmarkReached += (object o, BookmarkReachedEventArgs e) => events++;
+                synth.PhonemeReached += (object o, PhonemeReachedEventArgs e) => events++;
+                synth.SpeakProgress += (object o, SpeakProgressEventArgs e) => events++;
+                synth.SpeakStarted += (object o, SpeakStartedEventArgs e) => events++;
+                synth.VisemeReached += (object o, VisemeReachedEventArgs e) => events++;
+                synth.VoiceChange += (object o, VoiceChangeEventArgs e) => events++;
+                synth.StateChanged += (object o, System.Speech.Synthesis.StateChangedEventArgs e) => events++;
+                synth.SpeakCompleted += (object o, SpeakCompletedEventArgs e) =>
+                {
+                    events++;
+                    Assert.Equal(34, events++);
+                };
+
+                Assert.Equal(SynthesizerState.Ready, synth.State);
+                synth.SpeakSsml(builder.ToXml());
+                Assert.Equal(SynthesizerState.Ready, synth.State);
+                synth.Pause();
+                Assert.Equal(SynthesizerState.Paused, synth.State);
+                synth.Resume();
+                Assert.Equal(SynthesizerState.Ready, synth.State);
+            }
+        }
+
+        [Fact]
+        public void AddLexicon()
+        {
+            string temp = GetTestFilePath();
+            string content = @"
+<lexicon alphabet='x-microsoft-ups' version='1.0' xml:lang='en-US' xmlns='http://www.w3.org/2005/01/pronunciation-lexicon'>
+	<lexeme>
+		<grapheme>blue </grapheme>
+		<phoneme>B L I P </phoneme>
+	</lexeme>";
+            File.WriteAllText(temp, content);
+
+            using (var synth = new SpeechSynthesizer())
+            {
+                synth.AddLexicon(new Uri(temp), "application/pls+xml");
+            }
+        }
+    }
+}
