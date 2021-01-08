5965268398ccb248bfce7ae29a83a1a090c3fac9
Adeel <3840695+am11@users.noreply.github.com>
2021-01-08 22:01:27 +0200

    Add tests for MarshalUtils.IsBillitableType

---
+++ src/coreclr/tools/aot/ILCompiler.TypeSystem.ReadyToRun.Tests/CoreTestAssembly/Marshalling.cs
@@ -0,0 +1,146 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Runtime.InteropServices;
+
+namespace Marshalling
+{
+    [StructLayout(LayoutKind.Explicit)]
+    public class ExplicitEmptyBase
+    {
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    public class ClassWithExplicitEmptyBase : ExplicitEmptyBase
+    {
+        [FieldOffset(0)]
+        public int i;
+    }
+
+    [StructLayout(LayoutKind.Explicit, Size = 0)]
+    public class ExplicitEmptySizeZeroBase
+    {
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    public class ClassWithExplicitSizeZeroEmptyBase : ExplicitEmptySizeZeroBase
+    {
+        [FieldOffset(0)]
+        public int i;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    public class ExplicitByteBase
+    {
+        [FieldOffset(0)]
+        public byte x;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    public class ClassWithExplicitByteBase : ExplicitByteBase
+    {
+        [FieldOffset(1)]
+        public int i;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    public class ExplicitInt16Base
+    {
+        [FieldOffset(0)]
+        public short x;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    public class ClassWithExplicitInt16Base : ExplicitInt16Base
+    {
+        [FieldOffset(1)]
+        public int i;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    public class ExplicitInt32Base
+    {
+        [FieldOffset(0)]
+        public int x;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    public class ClassWithExplicitInt32Base : ExplicitInt32Base
+    {
+        [FieldOffset(1)]
+        public int i;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    public class ExplicitInt64Base
+    {
+        [FieldOffset(0)]
+        public long x;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    public class ClassWithExplicitInt64Base : ExplicitInt64Base
+    {
+        [FieldOffset(1)]
+        public int i;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public class SequentialEmptyBase
+    {
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public class ClassWithSequentialEmptyBase : SequentialEmptyBase
+    {
+        public int i;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public class SequentialByteBase
+    {
+        public byte x;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public class ClassWithSequentialByteBase : SequentialByteBase
+    {
+        public int i;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public class SequentialInt16Base
+    {
+        public short x;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public class ClassWithSequentialInt16Base : SequentialInt16Base
+    {
+        public int i;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public class SequentialInt32Base
+    {
+        public int x;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public class ClassWithSequentialInt32Base : SequentialInt32Base
+    {
+        public int i;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public class SequentialInt64Base
+    {
+        public long x;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public class ClassWithSequentialInt64Base : SequentialInt64Base
+    {
+        public int i;
+    }
+}
