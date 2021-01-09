d40eb92721b766b91163b8af99514e92904c32af
Aleksey Kliger <alklig@microsoft.com>
2021-01-08 20:14:27 -0500

    [mbr] Extend console sample
    Add a busy thread to demonstrate that interp frames since the last managed
    frame are visible to the metadata update mechanism and the active method bodies are
    copied before being invalidated.

--- src/mono/netcore/sample/mbr/console/Program.cs
+++ src/mono/netcore/sample/mbr/console/Program.cs
@@ -14,11 +14,24 @@ namespace HelloWorld
 	class State {
 	    public readonly ManualResetEventSlim mreIn;
 	    public readonly ManualResetEventSlim mreOut;
+	    public readonly ManualResetEventSlim mreBusy;
 	    public string res;
+	    private volatile bool busyChanged;
+
 	    public State() {
 		mreIn = new ManualResetEventSlim ();
 		mreOut = new ManualResetEventSlim ();
+		mreBusy = new ManualResetEventSlim ();
 		res = "";
+		busyChanged = false;
+	    }
+
+
+	    public bool BusyChanged {get => busyChanged ; set { busyChanged = value; mreBusy.Set ();} }
+
+	    public void WaitForBusy () {
+		mreBusy.Wait ();
+		mreBusy.Reset ();
 	    }
 
 	    public string ConsumerStep () {
@@ -50,6 +63,8 @@ namespace HelloWorld
 	    var st = new State ();
 	    var t = new Thread (MutatorThread);
 	    t.Start (st);
+	    var t2 = new Thread (BusyThread) { IsBackground = true };
+	    t2.Start (st);
 
 	    string res = st.ConsumerStep ();
 	    if (res != "OLD STRING")
@@ -61,6 +76,9 @@ namespace HelloWorld
 	    if (res != "NEW STRING")
 		return 2;
 
+	    st.WaitForBusy ();
+	    Console.WriteLine ("BusyChanged: {0}", st.BusyChanged);
+
 	    return 0;
 	}
 
@@ -70,6 +88,28 @@ namespace HelloWorld
 	    static string Step () => TestClass.TargetMethod ();
 	    st.ProducerStep (Step);
 	    st.ProducerStep (Step);
+        }
+
+	// This method is not affected by the update, but it calls the target
+	// method which is.  Still we expect to see "BusyThread" and its
+	// callees show up in the trace output when it safepoints during an
+	// update.
+        private static void BusyThread (object o)
+        {
+	    State st = (State)o;
+	    string prev = TestClass.TargetMethod ();
+            while (true) {
+		Thread.Sleep (0);
+		for (int i = 0; i < 5000; ++i) {
+		    if (i % 1000 == 0) {
+			string cur = TestClass.TargetMethod ();
+			if (cur != prev) {
+			    st.BusyChanged = true;
+			    prev = cur;
+			}
+		    }
+		}
+	    }
 	}
 
     }
