f0fcfa411038ca3a8e99d0619ae69bb97577cccf
Tammy Qiu <tammy.qiu@yahoo.com>
2021-01-12 21:08:37 -0500

    Update with new changes in wasm samples

--- src/mono/netcore/sample/wasm/browser-profile/runtime.js
+++ src/mono/netcore/sample/wasm/browser-profile/runtime.js
@@ -3,7 +3,12 @@
 var Module = { 
     onRuntimeInitialized: function () {
         config.loaded_cb = function () {
-            App.init ();
+            try {
+                App.init ();
+            } catch (error) {
+                test_exit(1);
+                throw (error);
+            }
         };
         config.fetch_file_cb = function (asset) {
             return fetch (asset, { credentials: 'same-origin' });
@@ -16,6 +21,13 @@ var Module = {
                 send_to: "System.Runtime.InteropServices.JavaScript.Runtime::DumpAotProfileData"
             }
         }
-        MONO.mono_load_runtime_and_bcl_args (config);
+        
+        try
+        {
+            MONO.mono_load_runtime_and_bcl_args (config);
+        } catch (error) {
+            test_exit(1);
+            throw(error);
+        }
     }
 };
