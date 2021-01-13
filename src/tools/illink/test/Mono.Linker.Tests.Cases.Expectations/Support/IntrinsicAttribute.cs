64deadc65cd3a3382d83a67ed98fc46f039a4fc4
Vitek Karas <vitek.karas@microsoft.com>
2021-01-13 11:12:40 -0800

    Fix constant prop around intrinsic (dotnet/linker#1749)
    In dotnet/linker#1734 a change was made that substitutions are only considered for methods which are not intrinsic or no-inline. That is not how it should work, even intrinsic or no-inline methods should still allow substitutions and constant propagation from those.
    
    What intrinsic and no-inline methods should not do is participate in constant propagation as intermediate methods (so methods which become constant because they're calling other constant methods).
    
    In short substitutions effectively override the intrinsic/no-inline.
    
    Added test cases.
    
    Fixes dotnet/linker#1747
    
    Commit migrated from https://github.com/dotnet/linker/commit/fc8abeff62398d46ac3408d20f50879e6f2e713f

---
+++ src/tools/illink/test/Mono.Linker.Tests.Cases.Expectations/Support/IntrinsicAttribute.cs
@@ -0,0 +1,15 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+
+namespace System.Runtime.CompilerServices
+{
+	// This attribute is normally implemented in CoreLib as internal, but in order to test
+	// linker behavior around it, we need to be able to use it in the tests.
+	[AttributeUsage (AttributeTargets.Method)]
+	public sealed class IntrinsicAttribute : Attribute
+	{
+	}
+}
