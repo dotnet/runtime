b6e77a75f2d585b9ad1675cd9b484e2962392263
Jeremy Koritzinsky <jekoritz@microsoft.com>
2021-01-20 11:07:39 -0800

    Remove trailing whitespace.

--- src/libraries/System.Private.CoreLib/src/System/Runtime/InteropServices/Marshal.cs
+++ src/libraries/System.Private.CoreLib/src/System/Runtime/InteropServices/Marshal.cs
@@ -1002,7 +1002,7 @@ namespace System.Runtime.InteropServices
         /// </summary>
         /// <param name="safeHandle"><see cref="SafeHandle"/> instance to update</param>
         /// <param name="handle">Pre-existing handle</param>
-        public static void InitHandle(SafeHandle safeHandle, IntPtr handle) 
+        public static void InitHandle(SafeHandle safeHandle, IntPtr handle)
         {
             // To help maximize performance of P/Invokes, don't check if safeHandle is null.
             safeHandle.SetHandle(handle);
