ae21ee398a985420bf33a7173faf951722ed7f42
Sven Boemer <sbomer@gmail.com>
2021-01-22 15:27:26 -0800

    Ensure that CopyUsed entry point gets marked (dotnet/linker#1773)
    * Ensure that CopyUsed entry point gets marked
    
    * Fix formatting
    
    Commit migrated from https://github.com/dotnet/linker/commit/f3cb254461d6077f1b9c18c4a6a76c64c7869413

--- src/tools/linker/test/Mono.Linker.Tests.Cases/Libraries/CopyUsedAssemblyWithMainEntryRoot.cs
+++ src/tools/linker/test/Mono.Linker.Tests.Cases/Libraries/CopyUsedAssemblyWithMainEntryRoot.cs
@@ -1,5 +1,6 @@
 using Mono.Linker.Tests.Cases.Expectations.Assertions;
 using Mono.Linker.Tests.Cases.Expectations.Metadata;
+using Mono.Linker.Tests.Cases.Libraries.Dependencies;
 
 namespace Mono.Linker.Tests.Cases.Libraries
 {
@@ -9,11 +10,16 @@ namespace Mono.Linker.Tests.Cases.Libraries
 	[Kept]
 	[KeptMember (".ctor()")]
 	[SetupLinkerAction ("copyused", "test")]
+	[SetupCompileBefore ("lib.dll", new[] { "Dependencies/CopyUsedAssemblyWithMainEntryRoot_Lib.cs" })]
+	[KeptMemberInAssembly ("lib.dll", typeof (CopyUsedAssemblyWithMainEntryRoot_Lib), "Used()")]
+	// Marked CopyUsed assemblies are not fully marked like Copy assemblies, so the Unused dependency is not kept.
+	[RemovedMemberInAssembly ("lib.dll", typeof (CopyUsedAssemblyWithMainEntryRoot_Lib), "Unused()")]
 	public class CopyUsedAssemblyWithMainEntryRoot
 	{
 		[Kept]
 		public static void Main ()
 		{
+			CopyUsedAssemblyWithMainEntryRoot_Lib.Used ();
 		}
 
 		[Kept]
@@ -24,6 +30,7 @@ namespace Mono.Linker.Tests.Cases.Libraries
 		[Kept]
 		private void UnusedPrivateMethod ()
 		{
+			CopyUsedAssemblyWithMainEntryRoot_Lib.Unused ();
 		}
 	}
 }
