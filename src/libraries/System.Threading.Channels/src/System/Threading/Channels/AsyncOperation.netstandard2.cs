665c65a8929a866b79409b832f0fa6af14cbf230
Kornei Dontsov <KorneiDontsov@gmail.com>
2021-01-22 03:02:04 +0200

    Reduce allocations in System.Threading.Channels for netstandard2.* and net461 (#46458)
    * Reduce allocations in System.Threading.Channels for netstandard2.* and net461
    
    System.Threading.Channels.AsyncOperation<TResult> is well optimized for .NET Core, but there are no optimizations for .NET Standard 2.0 and .NET Framework. They use the same code as .NET Standard 1.3.
    
    * Remove execution flow suppression as redundant
    
    * Rollback QueueUserWorkItem

---
+++ src/libraries/System.Threading.Channels/src/System/Threading/Channels/AsyncOperation.netstandard2.cs
@@ -0,0 +1,20 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Threading.Tasks;
+
+namespace System.Threading.Channels
+{
+    internal partial class AsyncOperation<TResult>
+    {
+        private void UnsafeQueueSetCompletionAndInvokeContinuation() =>
+            ThreadPool.UnsafeQueueUserWorkItem(static s => ((AsyncOperation<TResult>)s).SetCompletionAndInvokeContinuation(), this);
+
+        private static void QueueUserWorkItem(Action<object?> action, object? state) =>
+            Task.Factory.StartNew(action, state,
+                CancellationToken.None, TaskCreationOptions.DenyChildAttach, TaskScheduler.Default);
+
+        private static CancellationTokenRegistration UnsafeRegister(CancellationToken cancellationToken, Action<object?> action, object? state) =>
+            cancellationToken.Register(action, state);
+    }
+}
