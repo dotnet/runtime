d838d1f7d23803509e14f740b43886fc9258c7b5
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2021-01-25 14:04:57 -0800

    Stub out managed ObjectiveC API and provide PNSE implementation.

---
+++ src/libraries/System.Private.CoreLib/src/System/Runtime/InteropServices/ObjectiveC/ObjectiveCBase.cs
@@ -0,0 +1,40 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections;
+using System.Runtime.Versioning;
+
+namespace System.Runtime.InteropServices.ObjectiveC
+{
+    /// <summary>
+    /// Base type for all types participating in Objective-C interop.
+    /// </summary>
+    [SupportedOSPlatform("macos")]
+    public abstract class ObjectiveCBase : IDisposable
+    {
+        public static readonly IntPtr InvalidInstanceValue = (IntPtr)(-1);
+
+        /// <summary>
+        /// Create a <see cref="ObjectiveCBase"/> instance.
+        /// </summary>
+        protected ObjectiveCBase()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Method called during Dispose().
+        /// </summary>
+        /// <param name="disposing">If called from <see cref="Dispose"/></param>
+        protected virtual void Dispose(bool disposing)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
