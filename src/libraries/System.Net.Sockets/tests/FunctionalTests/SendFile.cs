7ecc497cf1ad9608f8b7f4f286ef765c222eb907
Anton Firszov <antonfir@gmail.com>
2021-01-26 18:27:48 +0100

    IncludeFile_Success

--- src/libraries/System.Net.Sockets/tests/FunctionalTests/SendFile.cs
+++ src/libraries/System.Net.Sockets/tests/FunctionalTests/SendFile.cs
@@ -19,29 +19,6 @@ namespace System.Net.Sockets.Tests
         {
         }
 
-        public static IEnumerable<object[]> SendFile_MemberData()
-        {
-            foreach (IPAddress listenAt in new[] { IPAddress.Loopback, IPAddress.IPv6Loopback })
-            {
-                foreach (bool sendPreAndPostBuffers in new[] { true, false })
-                {
-                    foreach (int bytesToSend in new[] { 512, 1024, 12345678 })
-                    {
-                        yield return new object[] { listenAt, sendPreAndPostBuffers, bytesToSend };
-                    }
-                }
-            }
-        }
-
-        public static IEnumerable<object[]> SendFileSync_MemberData()
-        {
-            foreach (object[] memberData in SendFile_MemberData())
-            {
-                yield return memberData.Concat(new object[] { true }).ToArray();
-                yield return memberData.Concat(new object[] { false }).ToArray();
-            }
-        }
-
         [Fact]
         public async Task Disposed_ThrowsException()
         {
@@ -90,6 +67,79 @@ namespace System.Net.Sockets.Tests
             }
         }
 
+        public static IEnumerable<object[]> SendFile_MemberData()
+        {
+            foreach (IPAddress listenAt in new[] { IPAddress.Loopback, IPAddress.IPv6Loopback })
+            {
+                foreach (bool sendPreAndPostBuffers in new[] { true, false })
+                {
+                    foreach (int bytesToSend in new[] { 512, 1024, 12345678 })
+                    {
+                        yield return new object[] { listenAt, sendPreAndPostBuffers, bytesToSend };
+                    }
+                }
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(SendFile_MemberData))]
+        public async Task IncludeFile_Success(IPAddress listenAt, bool sendPreAndPostBuffers, int bytesToSend)
+        {
+            const int ListenBacklog = 1;
+            const int TestTimeout = 30000;
+
+            // Create file to send
+            byte[] preBuffer;
+            byte[] postBuffer;
+            Fletcher32 sentChecksum;
+            using TempFile tempFile = CreateFileToSend(bytesToSend, sendPreAndPostBuffers, out preBuffer, out postBuffer, out sentChecksum);
+
+            // Start server
+            var server = new Socket(listenAt.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            server.BindToAnonymousPort(listenAt);
+            Listen(server, ListenBacklog); // Configures NonBlocking behavior
+
+            int bytesReceived = 0;
+            var receivedChecksum = new Fletcher32();
+            var serverTask = Task.Run(() =>
+            {
+                using (server)
+                {
+                    Socket remote = server.Accept();
+                    Assert.NotNull(remote);
+
+                    using (remote)
+                    {
+                        var recvBuffer = new byte[256];
+                        while (true)
+                        {
+                            int received = remote.Receive(recvBuffer, 0, recvBuffer.Length, SocketFlags.None);
+                            if (received == 0)
+                            {
+                                break;
+                            }
+
+                            bytesReceived += received;
+                            receivedChecksum.Add(recvBuffer, 0, received);
+                        }
+                    }
+                }
+            });
+
+            // Run client
+            EndPoint serverEndpoint = server.LocalEndPoint;
+            using (var client = new Socket(serverEndpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp))
+            {
+                await ConnectAsync(client, serverEndpoint); // Configures NonBlocking behavior
+                await SendFileAsync(client, tempFile.Path, preBuffer, postBuffer, TransmitFileOptions.UseDefaultWorkerThread);
+                client.Shutdown(SocketShutdown.Send);
+            }
+
+            await serverTask.TimeoutAfter(TestTimeout);
+            Assert.Equal(bytesToSend, bytesReceived);
+            Assert.Equal(sentChecksum.Sum, receivedChecksum.Sum);
+        }
+
         [Theory]
         [InlineData(false, false)]
         [InlineData(false, true)]
@@ -121,6 +171,45 @@ namespace System.Net.Sockets.Tests
             }
 
             Assert.Equal(0, client.Available);
+        }
+
+        [ActiveIssue("https://github.com/dotnet/runtime/issues/42534", TestPlatforms.Windows)]
+        [OuterLoop("Creates and sends a file several gigabytes long")]
+        [Fact]
+        public async Task GreaterThan2GBFile_SendsAllBytes()
+        {
+            const long FileLength = 100L + int.MaxValue;
+
+            using var tmpFile = TempFile.Create();
+            using (FileStream fs = File.Create(tmpFile.Path))
+            {
+                fs.SetLength(FileLength);
+            }
+
+            using var client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            using var listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listener.BindToAnonymousPort(IPAddress.Loopback);
+            listener.Listen(1);
+
+            client.Connect(listener.LocalEndPoint);
+            using Socket server = listener.Accept();
+
+            await new Task[]
+            {
+                SendFileAsync(server, tmpFile.Path),
+                Task.Run(() =>
+                {
+                    byte[] buffer = new byte[100_000];
+                    long count = 0;
+                    while (count < FileLength)
+                    {
+                        int received = client.Receive(buffer);
+                        Assert.NotEqual(0, received);
+                        count += received;
+                    }
+                    Assert.Equal(0, client.Available);
+                })
+            }.WhenAllOrAnyFailed();
         }
 
         private TempFile CreateFileToSend(int size, bool sendPreAndPostBuffers, out byte[] preBuffer, out byte[] postBuffer, out Fletcher32 checksum)
@@ -217,129 +306,6 @@ namespace System.Net.Sockets.Tests
             return tempFile;
         }
 
-        [ActiveIssue("https://github.com/dotnet/runtime/issues/42534", TestPlatforms.Windows)]
-        [OuterLoop("Creates and sends a file several gigabytes long")]
-        [Theory]
-        [InlineData(false)]
-        [InlineData(true)]
-        public async Task SendFile_GreaterThan2GBFile_SendsAllBytes(bool useAsync)
-        {
-            const long FileLength = 100L + int.MaxValue;
-
-            using var tmpFile = TempFile.Create();
-            using (FileStream fs = File.Create(tmpFile.Path))
-            {
-                fs.SetLength(FileLength);
-            }
-
-            using var client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            using var listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            listener.BindToAnonymousPort(IPAddress.Loopback);
-            listener.Listen(1);
-
-            client.Connect(listener.LocalEndPoint);
-            using Socket server = listener.Accept();
-
-            await new Task[]
-            {
-                Task.Run(async () =>
-                {
-                    if (useAsync)
-                    {
-                        await Task.Factory.FromAsync(server.BeginSendFile, server.EndSendFile, tmpFile.Path, null);
-                    }
-                    else
-                    {
-                        server.SendFile(tmpFile.Path);
-                    }
-                }),
-                Task.Run(() =>
-                {
-                    byte[] buffer = new byte[100_000];
-                    long count = 0;
-                    while (count < FileLength)
-                    {
-                        int received = client.Receive(buffer);
-                        Assert.NotEqual(0, received);
-                        count += received;
-                    }
-                    Assert.Equal(0, client.Available);
-                })
-            }.WhenAllOrAnyFailed();
-        }
-
-        [OuterLoop]
-        [Theory]
-        [MemberData(nameof(SendFileSync_MemberData))]
-        public void SendFile_Synchronous(IPAddress listenAt, bool sendPreAndPostBuffers, int bytesToSend, bool forceNonBlocking)
-        {
-            const int ListenBacklog = 1;
-            const int TestTimeout = 30000;
-
-            // Create file to send
-            byte[] preBuffer;
-            byte[] postBuffer;
-            Fletcher32 sentChecksum;
-            using TempFile tempFile = CreateFileToSend(bytesToSend, sendPreAndPostBuffers, out preBuffer, out postBuffer, out sentChecksum);
-
-            // Start server
-            var server = new Socket(listenAt.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-            server.BindToAnonymousPort(listenAt);
-
-            server.Listen(ListenBacklog);
-
-            server.ForceNonBlocking(forceNonBlocking);
-
-            int bytesReceived = 0;
-            var receivedChecksum = new Fletcher32();
-            var serverThread = new Thread(() =>
-            {
-                using (server)
-                {
-                    Socket remote = server.Accept();
-                    Assert.NotNull(remote);
-
-                    remote.ForceNonBlocking(forceNonBlocking);
-
-                    using (remote)
-                    {
-                        var recvBuffer = new byte[256];
-                        while (true)
-                        {
-                            int received = remote.Receive(recvBuffer, 0, recvBuffer.Length, SocketFlags.None);
-                            if (received == 0)
-                            {
-                                break;
-                            }
-
-                            bytesReceived += received;
-                            receivedChecksum.Add(recvBuffer, 0, received);
-                        }
-                    }
-                }
-            });
-            serverThread.Start();
-
-            // Run client
-            EndPoint clientEndpoint = server.LocalEndPoint;
-            var client = new Socket(clientEndpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-
-            client.ForceNonBlocking(forceNonBlocking);
-
-            client.Connect(clientEndpoint);
-
-            using (client)
-            {
-                client.SendFile(tempFile.Path, preBuffer, postBuffer, TransmitFileOptions.UseDefaultWorkerThread);
-                client.Shutdown(SocketShutdown.Send);
-            }
-
-            Assert.True(serverThread.Join(TestTimeout), "Completed within allowed time");
-
-            Assert.Equal(bytesToSend, bytesReceived);
-            Assert.Equal(sentChecksum.Sum, receivedChecksum.Sum);
-        }
-
         [Fact]
         public async Task SyncSendFileGetsCanceledByDispose()
         {
@@ -411,62 +377,6 @@ namespace System.Net.Sockets.Tests
                     }
                 }
             }, maxAttempts: 10, retryWhen: e => e is XunitException);
-        }
-
-        [OuterLoop]
-        [Theory]
-        [MemberData(nameof(SendFile_MemberData))]
-        public async Task SendFile_APM(IPAddress listenAt, bool sendPreAndPostBuffers, int bytesToSend)
-        {
-            const int ListenBacklog = 1, TestTimeout = 30000;
-
-            // Create file to send
-            byte[] preBuffer, postBuffer;
-            Fletcher32 sentChecksum;
-            using TempFile tempFile = CreateFileToSend(bytesToSend, sendPreAndPostBuffers, out preBuffer, out postBuffer, out sentChecksum);
-
-            // Start server
-            using (var listener = new Socket(listenAt.AddressFamily, SocketType.Stream, ProtocolType.Tcp))
-            {
-                listener.BindToAnonymousPort(listenAt);
-                listener.Listen(ListenBacklog);
-
-                int bytesReceived = 0;
-                var receivedChecksum = new Fletcher32();
-
-                Task serverTask = Task.Run(async () =>
-                {
-                    using (var serverStream = new NetworkStream(await listener.AcceptAsync(), ownsSocket: true))
-                    {
-                        var buffer = new byte[256];
-                        int bytesRead;
-                        while ((bytesRead = await serverStream.ReadAsync(buffer, 0, buffer.Length)) != 0)
-                        {
-                            bytesReceived += bytesRead;
-                            receivedChecksum.Add(buffer, 0, bytesRead);
-                        }
-                    }
-                });
-                Task clientTask = Task.Run(async () =>
-                {
-                    using (var client = new Socket(listener.LocalEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp))
-                    {
-                        await client.ConnectAsync(listener.LocalEndPoint);
-                        await Task.Factory.FromAsync(
-                            (callback, state) => client.BeginSendFile(tempFile.Path, preBuffer, postBuffer, TransmitFileOptions.UseDefaultWorkerThread, callback, state),
-                            iar => client.EndSendFile(iar),
-                            null);
-                        client.Shutdown(SocketShutdown.Send);
-                    }
-                });
-
-                // Wait for the tasks to complete
-                await (new[] { serverTask, clientTask }).WhenAllOrAnyFailed(TestTimeout);
-
-                // Validate the results
-                Assert.Equal(bytesToSend, bytesReceived);
-                Assert.Equal(sentChecksum.Sum, receivedChecksum.Sum);
-            }
         }
     }
 
