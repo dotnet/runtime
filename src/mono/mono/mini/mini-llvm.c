72355202de76455c9152e63f793c172b5d0d166e
Joseph Shields <directhex@apebox.org>
2021-01-27 11:17:31 -0500

    Make mini-llvm build w/ wasm

--- src/mono/mono/mini/mini-llvm.c
+++ src/mono/mono/mini/mini-llvm.c
@@ -11888,6 +11888,9 @@ MonoCPUFeatures mono_llvm_get_cpu_features (void)
 #if defined(TARGET_ARM64)
 		{ "crc",	MONO_CPU_ARM64_CRC },
 #endif
+#if defined(TARGET_WASM)
+		{ "simd",	MONO_CPU_WASM_SIMD },
+#endif
 	};
 	if (!cpu_features)
 		cpu_features = MONO_CPU_INITED | (MonoCPUFeatures)mono_llvm_check_cpu_features (flags_map, G_N_ELEMENTS (flags_map));
