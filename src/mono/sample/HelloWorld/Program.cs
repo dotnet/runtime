f23795b05a0e0cde4a3ca949e151ef20aee34709
Fan Yang <yangfan@microsoft.com>
2021-01-28 20:16:10 -0800

    Revert the change of HelloWorld sample

--- src/mono/sample/HelloWorld/Program.cs
+++ src/mono/sample/HelloWorld/Program.cs
@@ -2,28 +2,18 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
-using System.Runtime.Intrinsics.Arm;
-using System.Runtime.Intrinsics;
-using System.Runtime.CompilerServices;
 
 namespace HelloWorld
 {
     internal class Program
     {
-
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        public static Vector64<uint> test_create(float v) {
-            //var v1 = Vector128.Create((uint) v);
-            var v2 = Vector64.Create((uint) v);
-
-            //var result = Sha256.ScheduleUpdate1(v1, v1, v1);
-
-            return v2;
-        }
-
-        public static void Main() {
-            var result = test_create(15.5f);
-            System.Console.WriteLine("hello" + result);
+        private static void Main(string[] args)
+        {
+            bool isMono = typeof(object).Assembly.GetType("Mono.RuntimeStructs") != null;
+            Console.WriteLine($"Hello World {(isMono ? "from Mono!" : "from CoreCLR!")}");
+            Console.WriteLine(typeof(object).Assembly.FullName);
+            Console.WriteLine(System.Reflection.Assembly.GetEntryAssembly ());
+            Console.WriteLine(System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription);
         }
     }
 }
