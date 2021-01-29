dcef2897eb61012d6e8c2ccc44e72eb7c173e06b
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2021-01-29 14:51:03 +0000

    Revert "Use socketpair to implement Process.Start redirection" (#47461)
    * Revert #34861
    
    * reinstate removed test
    
    * Update src/libraries/System.Diagnostics.Process/tests/ProcessStreamReadTests.cs
    
    Co-authored-by: Stephen Toub <stoub@microsoft.com>
    
    Co-authored-by: Stephen Toub <stoub@microsoft.com>

--- src/libraries/System.Diagnostics.Process/tests/ProcessStreamReadTests.cs
+++ src/libraries/System.Diagnostics.Process/tests/ProcessStreamReadTests.cs
@@ -344,7 +344,8 @@ namespace System.Diagnostics.Tests
             }
         }
 
-        [PlatformSpecific(~TestPlatforms.Windows)] // currently on Windows these operations async-over-sync on Windows
+        [ActiveIssue("https://github.com/dotnet/runtime/issues/44329")]
+        [PlatformSpecific(~TestPlatforms.Windows)] // currently on Windows these operations async-over-sync on Windows	
         [ConditionalFact(typeof(RemoteExecutor), nameof(RemoteExecutor.IsSupported))]
         public async Task ReadAsync_OutputStreams_Cancel_RespondsQuickly()
         {
