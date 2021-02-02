c4ab7e7e8fb1065f3374ac292b6e37212d359b37
Jeremy Koritzinsky <jekoritz@microsoft.com>
2021-02-02 12:05:35 -0800

    Disable -Wmissing-noreturn on Clang and GCC.

--- src/libraries/Native/Unix/System.Native/pal_autoreleasepool.c
+++ src/libraries/Native/Unix/System.Native/pal_autoreleasepool.c
@@ -5,6 +5,11 @@
 #include <stdlib.h>
 #include "pal_utilities.h"
 
+#ifndef _MSC_VER
+// Don't warning about not declaring a function with [[noreturn]] since it's only true in Debug mode.
+#pragma GCC diagnostic ignored "-Wmissing-noreturn"
+#endif
+
 // These functions should not be used, but they need to be defined
 // to satisfy the tooling we used to enable redirecting P/Invokes
 // for the single file scenario.
