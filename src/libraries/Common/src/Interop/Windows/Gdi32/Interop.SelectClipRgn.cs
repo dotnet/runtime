4ec64c8bed0a4dea07e759b61e194b1766d75565
Santiago Fernandez Madero <safern@microsoft.com>
2021-02-03 19:09:14 -0800

    Fix GDI handle leak in Icon.DrawImage

--- src/libraries/Common/src/Interop/Windows/Gdi32/Interop.SelectClipRgn.cs
+++ src/libraries/Common/src/Interop/Windows/Gdi32/Interop.SelectClipRgn.cs
@@ -10,20 +10,5 @@ internal static partial class Interop
     {
         [DllImport(Libraries.Gdi32, SetLastError = true, ExactSpelling = true)]
         public static extern RegionType SelectClipRgn(IntPtr hdc, IntPtr hrgn);
-
-        public static RegionType SelectClipRgn(HandleRef hdc, IntPtr hrgn)
-        {
-            RegionType result = SelectClipRgn(hdc.Handle, hrgn);
-            GC.KeepAlive(hdc.Wrapper);
-            return result;
-        }
-
-        public static RegionType SelectClipRgn(HandleRef hdc, HandleRef hrgn)
-        {
-            RegionType result = SelectClipRgn(hdc.Handle, hrgn.Handle);
-            GC.KeepAlive(hdc.Wrapper);
-            GC.KeepAlive(hrgn.Wrapper);
-            return result;
-        }
     }
 }
