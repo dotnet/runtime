1192773df6a912466615c9bc0fe2ec1cc5b5f625
Joseph Shields <directhex@apebox.org>
2021-02-05 10:58:48 -0500

    More fixes from Alex

--- src/tasks/AppleAppBuilder/Templates/runtime.m
+++ src/tasks/AppleAppBuilder/Templates/runtime.m
@@ -201,7 +201,7 @@ register_dllmap (void)
 //%DllMap%
 }
 
-#if FORCE_INTERPRETER || FORCE_AOT
+#if FORCE_INTERPRETER || FORCE_AOT || (!TARGET_OS_SIMULATOR && !TARGET_OS_MACCATALYST)
 void mono_jit_set_aot_mode (MonoAotMode mode);
 void register_aot_modules (void);
 #endif
@@ -236,7 +236,7 @@ mono_ios_runtime_init (void)
 #if FORCE_INTERPRETER
     os_log_info (OS_LOG_DEFAULT, "INTERP Enabled");
     mono_jit_set_aot_mode (MONO_AOT_MODE_INTERP_ONLY);
-#elif FORCE_AOT
+#elif (!TARGET_OS_SIMULATOR && !TARGET_OS_MACCATALYST) || FORCE_AOT
     register_dllmap ();
     // register modules
     register_aot_modules ();
