744f1ffe5557442c802f75161e5bb05642913a9d
Jose Perez Rodriguez <joperezr@microsoft.com>
2021-02-08 12:44:48 -0800

    Resolve ILLink warnings for System.Resources.ResourceManager (#47778)
    * Making System.Resources.ResourceManager trim safe
    
    * Addressing PR Feedback
    
    * Rename objects => types
    
    * Also rename the strings.resx
    
    * More PR Feedback
    
    * Add UnconditionalSuppressMessage to Extensions
    
    * Rename feature switch and reword error messages
    
    * Removing new warning that is already addressed
    
    * Rename AppContext switch and add feature switch to docs
    
    * Addressing Feedback
    
    * Revert changes to findtype plus some other feedback
    
    * Revert FindType back
    
    * move feature check up

---
+++ src/libraries/System.Resources.ResourceManager/tests/TrimCompatibilityTests.cs
@@ -0,0 +1,48 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using Microsoft.DotNet.RemoteExecutor;
+using Xunit;
+
+namespace System.Resources.Tests
+{
+    public static class TrimCompatibilityTests
+    {
+        /// <summary>
+        /// Verifies that ResourceReader.CreateUntypedDelegate doesn't have any DynamicallyAccessedMembers attributes,
+        /// so we can safely call MakeGenericMethod on its methods.
+        /// </summary>
+        [Fact]
+        public static void VerifyMethodsCalledWithMakeGenericMethod()
+        {
+            Type type = typeof(ResourceReader);
+            MethodInfo mi = type.GetMethod("CreateUntypedDelegate", BindingFlags.NonPublic | BindingFlags.Static);
+
+            Type[] genericTypes = mi.GetGenericArguments();
+            if (genericTypes != null)
+            {
+                foreach(Type genericType in genericTypes)
+                {
+                    Assert.Null(genericType.GetCustomAttribute<DynamicallyAccessedMembersAttribute>());
+                }
+            }
+        }
+
+        [Fact]
+        public static void VerifyFeatureSwitchGeneratesTheRightException()
+        {
+            var remoteInvokeOptions = new RemoteInvokeOptions();
+            remoteInvokeOptions.RuntimeConfigurationOptions.Add("System.Resources.ResourceManager.AllowCustomResourceTypes", false);
+
+            using var handle = RemoteExecutor.Invoke(() =>
+            {
+                ResourceManager rm = new ResourceManager("System.Resources.Tests.Resources.CustomReader", typeof(TrimCompatibilityTests).Assembly);
+                Assert.Throws<NotSupportedException>(() => rm.GetObject("myGuid"));
+            }, remoteInvokeOptions);
+        }
+    }
+}
