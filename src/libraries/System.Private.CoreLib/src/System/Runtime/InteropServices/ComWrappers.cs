874f5bd205e2d33a1749c1e20fa57727263117f6
Jan Kotas <jkotas@microsoft.com>
2021-02-07 21:44:58 -0800

    Update src/libraries/System.Private.CoreLib/src/System/Runtime/InteropServices/ComWrappers.cs

--- src/libraries/System.Private.CoreLib/src/System/Runtime/InteropServices/ComWrappers.cs
+++ src/libraries/System.Private.CoreLib/src/System/Runtime/InteropServices/ComWrappers.cs
@@ -1,12 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
 using System.Collections;
-using System.Threading;
-using System.Runtime.CompilerServices;
 using System.Runtime.Versioning;
-using Internal.Runtime.CompilerServices;
 
 namespace System.Runtime.InteropServices
 {
