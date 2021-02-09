4396f0948e66b0d78ec106209bdd61d0e3ecaf85
Tanner Gooding <tagoo@outlook.com>
2021-02-09 09:17:18 -0800

    Fixing impMathIntrinsic to not insert unnecessary casts (#47923)
    * Add the Math benchmarks as tests
    
    * Fixing impMathIntrinsic to not insert unnecessary casts

---
+++ src/tests/JIT/Math/Functions/Double/CosDouble.cs
@@ -0,0 +1,36 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+
+namespace System.MathBenchmarks
+{
+    public partial class Double
+    {
+        // Tests Math.Cos(double) over 5000 iterations for the domain 0, PI
+
+        private const double cosDoubleDelta = 0.0006283185307180;
+        private const double cosDoubleExpectedResult = -1.0000000005924159;
+
+        public void Cos() => CosDoubleTest();
+
+        public static void CosDoubleTest()
+        {
+            double result = 0.0, value = 0.0;
+
+            for (int iteration = 0; iteration < MathTests.Iterations; iteration++)
+            {
+                value += cosDoubleDelta;
+                result += Math.Cos(value);
+            }
+
+            double diff = Math.Abs(cosDoubleExpectedResult - result);
+
+            if (diff > MathTests.DoubleEpsilon)
+            {
+                throw new Exception($"Expected Result {cosDoubleExpectedResult,20:g17}; Actual Result {result,20:g17}");
+            }
+        }
+    }
+}
