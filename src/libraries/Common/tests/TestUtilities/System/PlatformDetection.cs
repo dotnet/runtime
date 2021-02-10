c344d28bfe09391bf5c88af7c5726494d9a656d4
Zoltan Varga <vargaz@gmail.com>
2021-02-10 15:55:51 -0500

    Add GetICUVersion to .LibraryBuild.xml so its retained during the class libs build.

--- src/libraries/Common/tests/TestUtilities/System/PlatformDetection.cs
+++ src/libraries/Common/tests/TestUtilities/System/PlatformDetection.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Security;
 using System.Security.Authentication;
