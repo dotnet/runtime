af25fe652ec08fda63c23ae8f61470a48e65660e
Egor Bogatov <egorbo@gmail.com>
2021-02-10 21:03:35 +0300

    [RyuJIT] Implement Interlocked.And and Interlocked.Or for arm64-v8.1 (#46253)

--- src/coreclr/jit/optimizer.cpp
+++ src/coreclr/jit/optimizer.cpp
@@ -7155,7 +7155,7 @@ void Compiler::optHoistLoopBlocks(unsigned loopNum, ArrayStack<BasicBlock*>* blo
                         m_beforeSideEffect = false;
                     }
                 }
-                else if (tree->OperIs(GT_XADD, GT_XCHG, GT_LOCKADD, GT_CMPXCHG, GT_MEMORYBARRIER))
+                else if (tree->OperIs(GT_XADD, GT_XORR, GT_XAND, GT_XCHG, GT_LOCKADD, GT_CMPXCHG, GT_MEMORYBARRIER))
                 {
                     // If this node is a MEMORYBARRIER or an Atomic operation
                     // then don't hoist and stop any further hoisting after this node
@@ -7969,6 +7969,8 @@ bool Compiler::optComputeLoopSideEffectsOfBlock(BasicBlock* blk)
                         break;
 
                     case GT_LOCKADD:
+                    case GT_XORR:
+                    case GT_XAND:
                     case GT_XADD:
                     case GT_XCHG:
                     case GT_CMPXCHG:
