265157bee092a85935dabdd738ff7d696b853320
Mateo Torres Ruiz <matorre@microsoft.com>
2021-02-12 16:40:56 -0800

    Fixup path after appending rel path Remove COREHOST_TRACE

--- src/installer/tests/Microsoft.NET.HostModel.Tests/Helpers/BundleHelper.cs
+++ src/installer/tests/Microsoft.NET.HostModel.Tests/Helpers/BundleHelper.cs
@@ -83,7 +83,8 @@ namespace BundleTests.Helpers
         public static string[] GetFilesNeverExtracted(TestProjectFixture fixture)
         {
             string appBaseName = GetAppBaseName(fixture);
-            return new string[] { $"{appBaseName}.exe",
+            return new string[] { $"{appBaseName}",
+                                  $"{appBaseName}.exe",
                                   $"{appBaseName}.pdb",
                                   $"{appBaseName}.runtimeconfig.dev.json",
                                   Path.GetFileName(fixture.TestProject.HostFxrDll),
