c7c949f2bf7a7fd0a8df431d41f3113c22e8ca0c
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2021-02-15 21:39:58 +0100

    Add generic type constraint to Parse<T> and TryParse<T>
    Fixes issue #48310

--- src/libraries/System.Private.CoreLib/src/System/Enum.cs
+++ src/libraries/System.Private.CoreLib/src/System/Enum.cs
@@ -378,10 +378,10 @@ namespace System
             return result!;
         }
 
-        public static TEnum Parse<TEnum>(string value) where TEnum : struct =>
+        public static TEnum Parse<TEnum>(string value) where TEnum : struct, Enum =>
             Parse<TEnum>(value, ignoreCase: false);
 
-        public static TEnum Parse<TEnum>(string value, bool ignoreCase) where TEnum : struct
+        public static TEnum Parse<TEnum>(string value, bool ignoreCase) where TEnum : struct, Enum
         {
             bool success = TryParse<TEnum>(value, ignoreCase, throwOnFailure: true, out TEnum result);
             Debug.Assert(success);
@@ -464,13 +464,13 @@ namespace System
             }
         }
 
-        public static bool TryParse<TEnum>([NotNullWhen(true)] string? value, out TEnum result) where TEnum : struct =>
+        public static bool TryParse<TEnum>([NotNullWhen(true)] string? value, out TEnum result) where TEnum : struct, Enum =>
             TryParse<TEnum>(value, ignoreCase: false, out result);
 
-        public static bool TryParse<TEnum>([NotNullWhen(true)] string? value, bool ignoreCase, out TEnum result) where TEnum : struct =>
+        public static bool TryParse<TEnum>([NotNullWhen(true)] string? value, bool ignoreCase, out TEnum result) where TEnum : struct, Enum =>
             TryParse<TEnum>(value, ignoreCase, throwOnFailure: false, out result);
 
-        private static bool TryParse<TEnum>(string? value, bool ignoreCase, bool throwOnFailure, out TEnum result) where TEnum : struct
+        private static bool TryParse<TEnum>(string? value, bool ignoreCase, bool throwOnFailure, out TEnum result) where TEnum : struct, Enum
         {
             // Validation on the enum type itself.  Failures here are considered non-parsing failures
             // and thus always throw rather than returning false.
