a16f9ac4cbff0ee47bb2c053ec8381bd798767c2
Bruce Forstall <brucefo@microsoft.com>
2021-02-17 11:55:33 -0800

    Update SuperPMI license headers

--- src/coreclr/ToolBox/superpmi/superpmi-shared/spmidumphelper.h
+++ src/coreclr/ToolBox/superpmi/superpmi-shared/spmidumphelper.h
@@ -1,7 +1,5 @@
-//
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-//
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
 
 //----------------------------------------------------------
 // SpmiDumpHelper.h - a helper to dump structs that are used in JitEEInterface calls and spmi collections.
