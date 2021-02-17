3c1b7be20edf6750d11a8b315a32fddc082c6800
Prashanth Govindarajan <prgovi@microsoft.com>
2021-02-17 15:55:12 -0800

    sq

--- src/libraries/System.ComponentModel.TypeConverter/tests/Design/DesignTimeLicenseContextSerializerTests.cs
+++ src/libraries/System.ComponentModel.TypeConverter/tests/Design/DesignTimeLicenseContextSerializerTests.cs
@@ -10,9 +10,15 @@ namespace System.ComponentModel.Design.Tests
 {
     public class DesigntimeLicenseContextSerializerTests
     {
-        [Fact]
-        public void SerializeAndDeserialize()
+        [Theory]
+        [InlineData(false)]
+        [InlineData(true)]
+        public void SerializeAndDeserialize(bool useBinaryFormatter)
         {
+            if (useBinaryFormatter)
+            {
+                AppContext.SetSwitch("System.Runtime.Serialization.EnableUnsafeBinaryFormatterSerialization", true);
+            }
             var context = new DesigntimeLicenseContext();
             context.SetSavedLicenseKey(typeof(int), "key");
             var assembly = typeof(DesigntimeLicenseContextSerializer).Assembly;
@@ -36,6 +42,36 @@ namespace System.ComponentModel.Design.Tests
                 var value = savedLicenseKeys[typeof(int).AssemblyQualifiedName];
                 Assert.True(value is string);
                 Assert.Equal("key", value);
+            }
+        }
+
+        [Fact]
+        public void SerializeWithBinaryFormatter_DeserializeWithBinaryWriter()
+        {
+            AppContext.SetSwitch("TestSwitch.LocalAppContext.DisableCaching", true);
+            AppContext.SetSwitch("System.Runtime.Serialization.EnableUnsafeBinaryFormatterSerialization", true);
+            var context = new DesigntimeLicenseContext();
+            context.SetSavedLicenseKey(typeof(int), "key");
+            var assembly = typeof(DesigntimeLicenseContextSerializer).Assembly;
+            Type runtimeLicenseContextType = assembly.GetType("System.ComponentModel.Design.RuntimeLicenseContext");
+            Assert.NotNull(runtimeLicenseContextType);
+            object runtimeLicenseContext = Activator.CreateInstance(runtimeLicenseContextType);
+            Assert.NotNull(runtimeLicenseContext);
+
+            Type designtimeLicenseContextSerializer = assembly.GetType("System.ComponentModel.Design.DesigntimeLicenseContextSerializer");
+            Assert.NotNull(designtimeLicenseContextSerializer);
+            Reflection.MethodInfo deserializeMethod = designtimeLicenseContextSerializer.GetMethod("Deserialize", Reflection.BindingFlags.NonPublic | Reflection.BindingFlags.Static);
+
+            using (MemoryStream stream = new MemoryStream())
+            {
+                long position = stream.Position;
+                System.ComponentModel.Design.DesigntimeLicenseContextSerializer.Serialize(stream, "crypto", context);
+                AppContext.SetSwitch("System.Runtime.Serialization.EnableUnsafeBinaryFormatterSerialization", false);
+                // Cannot deserialize anymore
+                stream.Seek(position, SeekOrigin.Begin);
+                deserializeMethod.Invoke(null, new object[] { stream, "crypto", runtimeLicenseContext });
+                Hashtable savedLicenseKeys = runtimeLicenseContext.GetType().GetField("_savedLicenseKeys").GetValue(runtimeLicenseContext) as Hashtable;
+                Assert.Null(savedLicenseKeys);
             }
         }
     }
