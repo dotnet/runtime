542a2586af3f8ab05e08a4de9de5b12b3109e169
Ankit Jain <radical@gmail.com>
2021-02-17 12:39:36 -0500

    [wasm] Add new RunWithEmSdkEnv task

---
+++ src/tasks/WasmAppBuilder/RunWithEmSdkEnv.cs
@@ -0,0 +1,51 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Tasks;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Microsoft.WebAssembly.Build.Tasks
+{
+    public class RunWithEmSdkEnv : Exec
+    {
+        [NotNull]
+        [Required]
+        public string? EmSdkPath { get; set; }
+
+        public override bool Execute()
+        {
+            IgnoreStandardErrorWarningFormat = true;
+            StandardOutputImportance = "Low";
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                string envScriptPath = Path.Combine(EmSdkPath, "emsdk_env.bat");
+                if (!CheckEnvScript(envScriptPath))
+                    return false;
+
+                Command = $"{envScriptPath} > nul 2>&1 && {Command}";
+            }
+            else
+            {
+                string envScriptPath = Path.Combine(EmSdkPath, "emsdk_env.sh");
+                if (!CheckEnvScript(envScriptPath))
+                    return false;
+
+                Command = $"bash -c 'source {envScriptPath} > /dev/null 2>&1 && {Command}'";
+            }
+
+            return base.Execute();
+
+            bool CheckEnvScript(string envScriptPath)
+            {
+                if (!File.Exists(envScriptPath))
+                    Log.LogError($"Could not find '{envScriptPath}' required to run command: {Command}");
+
+                return !Log.HasLoggedErrors;
+            }
+        }
+    }
+}
