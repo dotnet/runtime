a25fc47698a33f34965f76a0b5553db3914857ad
Bruce Forstall <brucefo@microsoft.com>
2021-02-17 14:38:26 -1000

    Update SuperPMI license headers (#48417)

--- src/coreclr/ToolBox/superpmi/superpmi-shared/methodcontext.h
+++ src/coreclr/ToolBox/superpmi/superpmi-shared/methodcontext.h
@@ -1,7 +1,5 @@
-//
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-//
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
 
 //----------------------------------------------------------
 // MethodContext.h - Primary structure to store all the EE-JIT details required to replay creation of a method
