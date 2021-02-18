91ec839803a7730c91d5a24acd3578dbbb929193
Kenneth Pouncey <kjpou@pt.lu>
2021-02-18 09:40:30 +0100

    Emscripten now builds a complete sysroot inside the EM_CACHE directory. - This includes the system headers which get copied into place there rather than adding a sequence of extra include directories.

--- src/mono/mono/tools/offsets-tool/offsets-tool.py
+++ src/mono/mono/tools/offsets-tool/offsets-tool.py
@@ -83,9 +83,9 @@ class OffsetsTool:
 
 		if "wasm" in args.abi:
 			require_emscipten_path (args)
-			self.sys_includes = [args.emscripten_path + "/system/include", args.emscripten_path + "/system/include/libc", args.emscripten_path + "/system/lib/libc/musl/arch/emscripten"]
 			self.target = Target ("TARGET_WASM", None, [])
 			self.target_args += ["-target", args.abi]
+			self.target_args += ["-I", args.emscripten_path + "/cache/sysroot/include"]
 
 		# Linux
 		elif "arm-linux-gnueabihf" == args.abi:
