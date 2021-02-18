ca7d47053f3ffd4a001e579b95fa06d115918bd8
Sung Yoon Whang <suwhang@microsoft.com>
2021-02-18 15:05:19 -0800

    Add a test on EventListener consuming ThreadPool events

---
+++ src/tests/tracing/eventlistener/EventListenerThreadPool.cs
@@ -0,0 +1,83 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.IO;
+using System.Diagnostics.Tracing;
+using System.Threading;
+using System.Threading.Tasks;
+using Tracing.Tests.Common;
+
+namespace Tracing.Tests
+{
+    internal sealed class RuntimeEventListener : EventListener
+    {
+        private readonly string _targetSourceName;
+        private readonly EventLevel _level;
+        
+        public int TPWorkerThreadStartCount = 0;
+        public int TPWorkerThreadStopCount = 0;
+        public int TPWorkerThreadWaitCount = 0;
+        
+        protected override void OnEventSourceCreated(EventSource source)
+        {
+            if (source.Name.Equals("Microsoft-Windows-DotNETRuntime"))
+            {
+                EnableEvents(source, EventLevel.Informational, (EventKeywords)0x10000);
+            }
+        }
+
+        protected override void OnEventWritten(EventWrittenEventArgs eventData)
+        {
+            if (eventData.EventName.Equals("ThreadPoolWorkerThreadStart"))
+            {
+                TPWorkerThreadStartCount += 1;
+            }
+            else if (eventData.EventName.Equals("ThreadPoolWorkerThreadStop"))
+            {
+                TPWorkerThreadStopCount += 1;
+            }
+            else if (eventData.EventName.Equals("ThreadPoolWorkerThreadWait"))
+            {
+                TPWorkerThreadWaitCount += 1;
+            }
+        }
+    }
+
+    class EventPipeSmoke
+    {
+        private static int messageIterations = 100;
+
+        static int Main(string[] args)
+        {
+            bool pass = false;
+            using (RuntimeEventListener listener = new RuntimeEventListener())
+            {
+                int someNumber = 0;
+                Task[] tasks = new Task[100];
+                for (int i = 0; i < tasks.Length; i++) 
+                {
+                    tasks[i] = Task.Run(() => { someNumber += 1; });
+                }
+                Task.WaitAll(tasks);
+                Thread.Sleep(1000);
+
+                if (listener.TPWorkerThreadStartCount > 10 ||
+                    listener.TPWorkerThreadStopCount > 10 ||
+                    listener.TPWorkerThreadWaitCount > 10)
+                {
+                    Console.WriteLine("Test Passed.");
+                    return 100;
+                }
+                else
+                {
+                    Console.WriteLine("Test Failed: Did not see any of the expected events.");
+                    Console.WriteLine($"ThreadPoolWorkerThreadStartCount: {listener.TPWorkerThreadStartCount}");
+                    Console.WriteLine($"ThreadPoolWorkerThreadStopCount: {listener.TPWorkerThreadStopCount}");
+                    Console.WriteLine($"ThreadPoolWorkerThreadWaitCount: {listener.TPWorkerThreadWaitCount}");
+                    return -1;
+                }
+            }
+        }
+    }
+}
