9ed81b76ac9cc1a90eaa8fd4916bfe375bffe713
vsadov <vsadov@microsoft.com>
2021-02-17 18:40:26 -0800

    move the check after/if SetThreadContext has failed

--- src/coreclr/vm/threadsuspend.cpp
+++ src/coreclr/vm/threadsuspend.cpp
@@ -2933,18 +2933,6 @@ BOOL Thread::RedirectThreadAtHandledJITCase(PFN_REDIRECTTARGET pTgt)
     if (!IsContextSafeToRedirect(pCtx))
         return (FALSE);
 
-    // In some rare cases the stack pointer may be outside the stack limits.
-    // SetThreadContext would fail assuming that we are trying to bypass CFG.
-    // 
-    // NB: the check here is slightly more strict than what OS requires,
-    //     but it is simple and uses only documented parts of TEB
-    auto pTeb = this->GetTEB();
-    void* stackPointer = (void*)GetSP(pCtx);
-    if ((stackPointer < pTeb->StackLimit) || (stackPointer > pTeb->StackBase))
-    {
-        return (FALSE);
-    }
-
     ////////////////////////////////////////////////////
     // Now redirect the thread to the helper function
 
@@ -2964,7 +2952,22 @@ BOOL Thread::RedirectThreadAtHandledJITCase(PFN_REDIRECTTARGET pTgt)
         this, this->GetThreadId(), dwOrigEip, pTgt);
 
     bRes = EESetThreadContext(this, pCtx);
-    _ASSERTE(bRes && "Failed to SetThreadContext in RedirectThreadAtHandledJITCase - aborting redirect.");
+    if (!bRes)
+    {
+        // In some rare cases the stack pointer may be outside the stack limits.
+        // SetThreadContext would fail assuming that we are trying to bypass CFG.
+        // 
+        // NB: the check here is slightly more strict than what OS requires,
+        //     but it is simple and uses only documented parts of TEB
+        auto pTeb = this->GetTEB();
+        void* stackPointer = (void*)GetSP(pCtx);
+        if ((stackPointer < pTeb->StackLimit) || (stackPointer > pTeb->StackBase))
+        {
+            return (FALSE);
+        }
+
+        _ASSERTE(!"Failed to SetThreadContext in RedirectThreadAtHandledJITCase - aborting redirect.");
+    }
 
     // Restore original IP
     SetIP(pCtx, dwOrigEip);
