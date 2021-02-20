4d310fbbc24f3f730ee4cfaa57b6642bb140fe0f
Adeel Mujahid <3840695+am11@users.noreply.github.com>
2021-02-20 18:34:53 +0200

    Fix JIT rolling build (#48552)

--- src/coreclr/clrdefinitions.cmake
+++ src/coreclr/clrdefinitions.cmake
@@ -104,7 +104,6 @@ add_definitions(-DFEATURE_DEFAULT_INTERFACES)
 if(FEATURE_EVENT_TRACE)
     add_compile_definitions($<$<NOT:$<BOOL:$<TARGET_PROPERTY:CROSSGEN_COMPONENT>>>:FEATURE_EVENT_TRACE>)
     add_definitions(-DFEATURE_PERFTRACING)
-    add_definitions(-DFEATURE_PERFTRACING_C_LIB)
 else(FEATURE_EVENT_TRACE)
     add_custom_target(eventing_headers) # add a dummy target to avoid checking for FEATURE_EVENT_TRACE in multiple places
 endif(FEATURE_EVENT_TRACE)
