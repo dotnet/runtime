89b7e09414c02310a06c81de031759b0219ac49b
Ivan Zlatanov <ivan@zlatanov.net>
2021-02-20 17:15:24 +0200

    Added links to RFC of each of the websocket deflate properties. Also added explanation why we don't support 8 bits althouh the RFC allows it.

--- src/libraries/System.Net.WebSockets/src/System/Net/WebSockets/WebSocketDeflateOptions.cs
+++ src/libraries/System.Net.WebSockets/src/System/Net/WebSockets/WebSocketDeflateOptions.cs
@@ -6,6 +6,10 @@ namespace System.Net.WebSockets
     /// <summary>
     /// Options to enable per-message deflate compression for <seealso cref="WebSocket" />.
     /// </summary>
+    /// <remarks>
+    /// Although the WebSocket spec allows window bits from 8 to 15, the current implementation doesn't support 8 bits.
+    /// For more information refer to the zlib manual https://zlib.net/manual.html.
+    /// </remarks>
     public sealed class WebSocketDeflateOptions
     {
         private int _clientMaxWindowBits = 15;
@@ -15,6 +19,7 @@ namespace System.Net.WebSockets
         /// This parameter indicates the base-2 logarithm of the LZ77 sliding window size of the client context.
         /// Must be a value between 9 and 15. The default is 15.
         /// </summary>
+        /// <remarks>https://tools.ietf.org/html/rfc7692#section-7.1.2.2</remarks>
         public int ClientMaxWindowBits
         {
             get => _clientMaxWindowBits;
@@ -32,12 +37,14 @@ namespace System.Net.WebSockets
         /// When true the client-side of the connection indicates that it will persist the deflate context accross messages.
         /// The default is true.
         /// </summary>
+        /// <remarks>https://tools.ietf.org/html/rfc7692#section-7.1.1.2</remarks>
         public bool ClientContextTakeover { get; set; } = true;
 
         /// <summary>
         /// This parameter indicates the base-2 logarithm of the LZ77 sliding window size of the server context.
         /// Must be a value between 9 and 15. The default is 15.
         /// </summary>
+        /// <remarks>https://tools.ietf.org/html/rfc7692#section-7.1.2.1</remarks>
         public int ServerMaxWindowBits
         {
             get => _serverMaxWindowBits;
@@ -55,6 +62,7 @@ namespace System.Net.WebSockets
         /// When true the server-side of the connection indicates that it will persist the deflate context accross messages.
         /// The default is true.
         /// </summary>
+        /// <remarks>https://tools.ietf.org/html/rfc7692#section-7.1.1.1</remarks>
         public bool ServerContextTakeover { get; set; } = true;
     }
 }
