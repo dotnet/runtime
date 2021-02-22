d19278ea6f26525d6e598d42b4608f593414e4bc
carlossanlop <carlossanlop@users.noreply.github.com>
2021-02-22 10:46:24 -0800

    Remove GlobalSuppresions

--- src/libraries/System.Diagnostics.Process/src/GlobalSuppressions.cs
+++
@@ -1,7 +0,0 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Documentation", "CA1200:Avoid using cref tags with a prefix", Justification = "https://github.com/dotnet/csharplang/issues/320", Scope = "type", Target = "~T:System.Diagnostics.Process")]
-[assembly: SuppressMessage("Documentation", "CA1200:Avoid using cref tags with a prefix", Justification = "https://github.com/dotnet/csharplang/issues/320", Scope = "member", Target = "~T:System.Diagnostics.ProcessStartInfo")]
