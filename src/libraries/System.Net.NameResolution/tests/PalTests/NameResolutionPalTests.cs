098e3c99983215f69c409ad785c585954558eb64
Günther Foidl <gue@korporal.at>
2021-02-23 22:52:12 +0100

    Fixed unreachable code bug
    SupportsGetAddrInfoAsync is a constant on *nix. Tests check if it's supported and return if not, so unreachable code was created on *nix.

--- src/libraries/System.Net.NameResolution/tests/PalTests/NameResolutionPalTests.cs
+++ src/libraries/System.Net.NameResolution/tests/PalTests/NameResolutionPalTests.cs
@@ -243,6 +243,8 @@ namespace System.Net.NameResolution.PalTests
             Assert.NotNull(NameResolutionPal.GetHostName());
         }
 
+#pragma warning disable CS0162 // Unreachable code detected -- SupportsGetAddrInfoAsync is a constant on *nix.
+
         [Theory]
         [InlineData(false)]
         [InlineData(true)]
