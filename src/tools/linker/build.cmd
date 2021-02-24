50a500f251c1886081e0d216e048a48ce2fee1ca
Omair Majid <omajid@redhat.com>
2021-02-24 06:19:32 -0500

    Enable source-build through arcade (dotnet/linker#1823)
    Co-authored-by: Marek Safar <marek.safar@gmail.com>
    
    Commit migrated from https://github.com/dotnet/linker/commit/6b3a3050c70577bd1b3fd7611eef56679e22a4f1

--- src/tools/linker/build.cmd
+++ src/tools/linker/build.cmd
@@ -1,2 +1,2 @@
 @echo off
-powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -projects """%~dp0illink.sln""" -restore -build %*"
+powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -restore -build %*"
