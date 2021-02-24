0914a7578f35f71a9711839a3ded71b694e0a5e9
Kenneth Pouncey <kjpou@pt.lu>
2021-02-24 09:32:40 +0100

    Add batched implementation to fix error with quota exceeded limit

--- src/libraries/Native/Unix/System.Native/pal_random.js
+++ src/libraries/Native/Unix/System.Native/pal_random.js
@@ -3,14 +3,28 @@
 
 var DotNetEntropyLib = {
     $DOTNETENTROPY: {
+        // batchedQuotaMax is the max number of bytes as specified by the api spec.
+        // If the byteLength of array is greater than 65536, throw a QuotaExceededError and terminate the algorithm.
+        // https://www.w3.org/TR/WebCryptoAPI/#Crypto-method-getRandomValues
+        batchedQuotaMax: 65536,
+        getBatchedRandomValues: function (buffer, bufferLength) {
+            // for modern web browsers
+            // map the work array to the memory buffer passed with the length
+            var wrkArray = new Uint8Array(Module.HEAPU8.buffer, buffer, bufferLength);
+            while (bufferLength > 0) {
+                var sliceEm = bufferLength % this.batchedQuotaMax;
+                sliceEm = sliceEm > 0 ? sliceEm : this.batchedQuotaMax;
+                var diceEmSegment = new Uint8Array(sliceEm);
+                crypto.getRandomValues(diceEmSegment);
+                bufferLength -= sliceEm;
+                wrkArray.set(diceEmSegment, bufferLength);
+            }
+        }
     },
     dotnet_browser_entropy : function (buffer, bufferLength) {
         // check that we have crypto available
         if (typeof crypto === 'object' && typeof crypto['getRandomValues'] === 'function') {
-            // for modern web browsers
-            // map the work array to the memory buffer passed with the length
-            var wrkArray = new Uint8Array(Module.HEAPU8.buffer, buffer, bufferLength);
-            crypto.getRandomValues(wrkArray);
+            DOTNETENTROPY.getBatchedRandomValues(buffer, bufferLength)
             return 0;
         } else {
             // we couldn't find a proper implementation, as Math.random() is not suitable
