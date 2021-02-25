c3db3e6aa77aa448888e44333212a635045eb050
Adam Sitnik <adam.sitnik@gmail.com>
2021-02-25 14:45:49 +0100

    add few extra asserts to the test to see why it fails in CI

--- src/libraries/System.Diagnostics.Process/tests/ProcessModuleTests.cs
+++ src/libraries/System.Diagnostics.Process/tests/ProcessModuleTests.cs
@@ -96,25 +96,28 @@ namespace System.Diagnostics.Tests
         [PlatformSpecific(TestPlatforms.Windows)]
         public void LongModuleFileNamesAreSupported()
         {
-            // To be able to test Long Path support for ProcessModule.FileName we need a .dll that has a path >= 260 chars.
+            // To be able to test Long Path support for ProcessModule.FileName we need a .dll that has a path > 260 chars.
             // Since Long Paths support can be disabled (see the ConditionalFact attribute usage above),
             // we just copy "LongName.dll" from bin to a temp directory with a long name and load it from there.
             // Loading from new path is possible because the type exposed by the assembly is not referenced in any explicit way.
             const string libraryName = "LongPath.dll";
+            const int minPathLength = 261;
 
             string testBinPath = Path.GetDirectoryName(typeof(ProcessModuleTests).Assembly.Location);
             string libraryToCopy = Path.Combine(testBinPath, libraryName);
             Assert.True(File.Exists(libraryToCopy), $"{libraryName} was not present in bin folder '{testBinPath}'");
 
-            string directoryWithLongName = Path.Combine(TestDirectory, new string('a', Math.Max(1, 261 - TestDirectory.Length)));
+            string directoryWithLongName = Path.Combine(TestDirectory, new string('a', Math.Max(1, minPathLength - TestDirectory.Length)));
             Directory.CreateDirectory(directoryWithLongName);
 
             string longNamePath = Path.Combine(directoryWithLongName, libraryName);
-            Assert.True(longNamePath.Length > 260);
+            Assert.True(longNamePath.Length > minPathLength);
 
             File.Copy(libraryToCopy, longNamePath);
+            Assert.True(File.Exists(longNamePath));
 
             Assembly loaded = Assembly.LoadFile(longNamePath);
+            Assert.Equal(longNamePath, loaded.Location);
 
             Assert.Contains(Process.GetCurrentProcess().Modules.Cast<ProcessModule>(), module => module.FileName == longNamePath);
         }
