098356a6946652b5bf40fc84b0a0f110ce921f9f
Anton Firszov <antonfir@gmail.com>
2021-02-25 15:06:36 +0100

    simplify GetTrailersSupported

--- src/libraries/System.Net.Http.WinHttpHandler/src/System/Net/Http/WinHttpTrailersHelper.cs
+++ src/libraries/System.Net.Http.WinHttpHandler/src/System/Net/Http/WinHttpTrailersHelper.cs
@@ -37,36 +37,27 @@ namespace System.Net.Http
         // Both features were introduced in Manganese and are planned to be backported to older Windows versions together.
         private static bool GetTrailersSupported()
         {
-            SafeWinHttpHandle sessionHandle = null;
-
-            try
-            {
-                sessionHandle = Interop.WinHttp.WinHttpOpen(
+            using SafeWinHttpHandle sessionHandle = Interop.WinHttp.WinHttpOpen(
                     IntPtr.Zero,
                     Interop.WinHttp.WINHTTP_ACCESS_TYPE_DEFAULT_PROXY,
                     Interop.WinHttp.WINHTTP_NO_PROXY_NAME,
                     Interop.WinHttp.WINHTTP_NO_PROXY_BYPASS,
                     (int)Interop.WinHttp.WINHTTP_FLAG_ASYNC);
 
-                if (sessionHandle.IsInvalid) return false;
-                uint buffer = 0;
-                uint bufferSize = sizeof(uint);
-                if (Interop.WinHttp.WinHttpQueryOption(sessionHandle, Interop.WinHttp.WINHTTP_OPTION_STREAM_ERROR_CODE, ref buffer, ref bufferSize))
-                {
-                    Debug.Fail("Querying WINHTTP_OPTION_STREAM_ERROR_CODE on a session handle should never succeed.");
-                    return false;
-                }
-
-                int lastError = Marshal.GetLastWin32Error();
-
-                // New Windows builds are expected to fail with ERROR_WINHTTP_INCORRECT_HANDLE_TYPE,
-                // when querying WINHTTP_OPTION_STREAM_ERROR_CODE on a session handle.
-                return lastError != Interop.WinHttp.ERROR_INVALID_PARAMETER;
-            }
-            finally
+            if (sessionHandle.IsInvalid) return false;
+            uint buffer = 0;
+            uint bufferSize = sizeof(uint);
+            if (Interop.WinHttp.WinHttpQueryOption(sessionHandle, Interop.WinHttp.WINHTTP_OPTION_STREAM_ERROR_CODE, ref buffer, ref bufferSize))
             {
-                sessionHandle.Dispose();
+                Debug.Fail("Querying WINHTTP_OPTION_STREAM_ERROR_CODE on a session handle should never succeed.");
+                return false;
             }
+
+            int lastError = Marshal.GetLastWin32Error();
+
+            // New Windows builds are expected to fail with ERROR_WINHTTP_INCORRECT_HANDLE_TYPE,
+            // when querying WINHTTP_OPTION_STREAM_ERROR_CODE on a session handle.
+            return lastError != Interop.WinHttp.ERROR_INVALID_PARAMETER;
         }
     }
 }
