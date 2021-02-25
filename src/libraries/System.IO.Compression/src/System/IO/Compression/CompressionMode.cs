d4bae09fc24d44652c523253fb39a5ac2f35409a
carlossanlop <carlossanlop@users.noreply.github.com>
2021-02-25 15:48:43 -0800

    CompressionMode

--- src/libraries/System.IO.Compression/src/System/IO/Compression/CompressionMode.cs
+++ src/libraries/System.IO.Compression/src/System/IO/Compression/CompressionMode.cs
@@ -3,9 +3,18 @@
 
 namespace System.IO.Compression
 {
+    /// <summary>Specifies whether to compress or decompress the underlying stream.</summary>
+    /// <remarks>This enumeration is used with the <see cref="System.IO.Compression.GZipStream" /> and <see cref="System.IO.Compression.DeflateStream" /> classes.</remarks>
+    /// <example>The following code example uses the <see cref="System.IO.Compression.CompressionMode" /> enumeration with the <see cref="System.IO.Compression.GZipStream" /> class to compress and decompress a file.
+    /// <format type="text/markdown"><![CDATA[
+    /// [!code-csharp[IO.Compression.GZip1#1](~/samples/snippets/csharp/VS_Snippets_CLR/IO.Compression.GZip1/CS/gziptest.cs#1)]
+    /// [!code-vb[IO.Compression.GZip1#1](~/samples/snippets/visualbasic/VS_Snippets_CLR/IO.Compression.GZip1/VB/gziptest.vb#1)]
+    /// ]]></format></example>
     public enum CompressionMode
     {
+        /// <summary>Decompresses the underlying stream.</summary>
         Decompress = 0,
+        /// <summary>Compresses the underlying stream.</summary>
         Compress = 1
     }
 }
