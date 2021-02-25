1f0243e5f83f96c771b8444d246a4e1f2639ad57
carlossanlop <carlossanlop@users.noreply.github.com>
2021-02-25 12:38:37 -0800

    ProcessAfinity lengthy remarks

--- src/libraries/System.Diagnostics.Process/src/System/Diagnostics/Process.cs
+++ src/libraries/System.Diagnostics.Process/src/System/Diagnostics/Process.cs
@@ -770,17 +770,9 @@ namespace System.Diagnostics
 
         /// <summary>Gets or sets the processors on which the threads in this process can be scheduled to run.</summary>
         /// <value>A bitmask representing the processors that the threads in the associated process can run on. The default depends on the number of processors on the computer. The default value is 2 <sup>n</sup> -1, where n is the number of processors.</value>
-        /// <remarks>The value returned by this property represents the most recently refreshed affinity of the process. To get the most up to date affinity, you need to call <see cref="System.Diagnostics.Process.Refresh" /> method first.
-        /// In Windows 2000 and later, a thread in a process can migrate from processor to processor, with each migration reloading the processor cache. Under heavy system loads, specifying which processor should run a specific thread can improve performance by reducing the number of times the processor cache is reloaded. The association between a processor and a thread is called the processor affinity.
-        /// Each processor is represented as a bit. Bit 0 is processor one, bit 1 is processor two, and so forth. If you set a bit to the value 1, the corresponding processor is selected for thread assignment. When you set the <see cref="System.Diagnostics.Process.ProcessorAffinity" /> value to zero, the operating system's scheduling algorithms set the thread's affinity. When the <see cref="System.Diagnostics.Process.ProcessorAffinity" /> value is set to any nonzero value, the value is interpreted as a bitmask that specifies those processors eligible for selection.
-        /// The following table shows a selection of <see cref="System.Diagnostics.Process.ProcessorAffinity" /> values for an eight-processor system.
-        /// |Bitmask|Binary value|Eligible processors|
-        /// |-------------|------------------|-------------------------|
-        /// |0x0001|00000000 00000001|1|
-        /// |0x0003|00000000 00000011|1 and 2|
-        /// |0x0007|00000000 00000111|1, 2 and 3|
-        /// |0x0009|00000000 00001001|1 and 4|
-        /// |0x007F|00000000 01111111|1, 2, 3, 4, 5, 6 and 7|</remarks>
+        /// <format type="text/markdown"><![CDATA[
+        /// [!INCLUDE[remarks] (~/includes/remarks/System.Diagnostics/Process/ProcessAfinity.md)]
+        /// ]]></format>
         /// <exception cref="System.ComponentModel.Win32Exception"><see cref="System.Diagnostics.Process.ProcessorAffinity" /> information could not be set or retrieved from the associated process resource.
         /// -or-
         /// The process identifier or process handle is zero. (The process has not been started.)</exception>
