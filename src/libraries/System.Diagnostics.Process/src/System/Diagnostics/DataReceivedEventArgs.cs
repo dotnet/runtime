8ca94a14dca9862dc2ecd97c91df58f4dade0157
carlossanlop <carlossanlop@users.noreply.github.com>
2021-02-25 12:26:21 -0800

    DataReceivedEventArgs lengthy remarks

--- src/libraries/System.Diagnostics.Process/src/System/Diagnostics/DataReceivedEventArgs.cs
+++ src/libraries/System.Diagnostics.Process/src/System/Diagnostics/DataReceivedEventArgs.cs
@@ -6,15 +6,9 @@ namespace System.Diagnostics
     /// <summary>Represents the method that will handle the <see cref="System.Diagnostics.Process.OutputDataReceived" /> event or <see cref="System.Diagnostics.Process.ErrorDataReceived" /> event of a <see cref="System.Diagnostics.Process" />.</summary>
     /// <param name="sender">The source of the event.</param>
     /// <param name="e">A <see cref="System.Diagnostics.DataReceivedEventArgs" /> that contains the event data.</param>
-    /// <remarks>When you create a <see cref="System.Diagnostics.DataReceivedEventHandler" /> delegate, you identify the method that will handle the event. To associate the event with your event handler, add an instance of the delegate to the event. The event handler is called whenever the event occurs, unless you remove the delegate. For more information about event-handler delegates, see [Handling and Raising Events](/dotnet/standard/events/).
-    /// To asynchronously collect the redirected <see cref="System.Diagnostics.Process.StandardOutput" /> or <see cref="System.Diagnostics.Process.StandardError" /> stream output of a process, add your event handler to the <see cref="System.Diagnostics.Process.OutputDataReceived" /> or <see cref="System.Diagnostics.Process.ErrorDataReceived" /> event. These events are raised each time the process writes a line to the corresponding redirected stream. When the redirected stream is closed, a null line is sent to the event handler. Ensure that your event handler checks for this condition before accessing the <see cref="System.Diagnostics.DataReceivedEventArgs.Data" /> property. For example, you can use the <see langword="static" /> method <see cref="string.IsNullOrEmpty" /> to validate the <see cref="System.Diagnostics.DataReceivedEventArgs.Data" /> property in your event handler.</remarks>
-    /// <example>The following code example illustrates how to perform asynchronous read operations on the redirected <see cref="System.Diagnostics.Process.StandardOutput" /> stream of the **sort** command. The **sort** command is a console application that reads and sorts text input.
-    /// The example creates a <see cref="System.Diagnostics.DataReceivedEventHandler" /> delegate for the `SortOutputHandler` event handler and associates the delegate with the <see cref="System.Diagnostics.Process.OutputDataReceived" /> event. The event handler receives text lines from the redirected <see cref="System.Diagnostics.Process.StandardOutput" /> stream, formats the text, and writes the text to the screen.
     /// <format type="text/markdown"><![CDATA[
-    /// [!code-cpp[Process_AsyncStreams#1](~/samples/snippets/cpp/VS_Snippets_CLR/process_asyncstreams/CPP/sort_async.cpp#1)]
-    /// [!code-csharp[Process_AsyncStreams#1](~/samples/snippets/csharp/VS_Snippets_CLR/process_asyncstreams/CS/sort_async.cs#1)]
-    /// [!code-vb[Process_AsyncStreams#1](~/samples/snippets/visualbasic/VS_Snippets_CLR/process_asyncstreams/VB/sort_async.vb#1)]
-    /// ]]></format></example>
+    /// [!INCLUDE[remarks] (~/includes/remarks/System.Diagnostics/DataReceivedEventArgs/DataReceivedEventHandler.md)]
+    /// ]]></format>
     /// <altmember cref="System.Diagnostics.DataReceivedEventArgs"/>
     /// <altmember cref="System.Diagnostics.Process.OutputDataReceived"/>
     /// <altmember cref="System.Diagnostics.Process.ErrorDataReceived"/>
@@ -45,15 +39,9 @@ namespace System.Diagnostics
 
         /// <summary>Gets the line of characters that was written to a redirected <see cref="System.Diagnostics.Process" /> output stream.</summary>
         /// <value>The line that was written by an associated <see cref="System.Diagnostics.Process" /> to its redirected <see cref="System.Diagnostics.Process.StandardOutput" /> or <see cref="System.Diagnostics.Process.StandardError" /> stream.</value>
-        /// <remarks>When you redirect the <see cref="System.Diagnostics.Process.StandardOutput" /> or <see cref="System.Diagnostics.Process.StandardError" /> stream of a <see cref="System.Diagnostics.Process" /> to your event handler, an event is raised each time the process writes a line to the redirected stream. The <see cref="System.Diagnostics.DataReceivedEventArgs.Data" /> property is the line that the <see cref="System.Diagnostics.Process" /> wrote to the redirected output stream. Your event handler can use the <see cref="System.Diagnostics.DataReceivedEventArgs.Data" /> property to filter process output or write output to an alternate location. For example, you might create an event handler that stores all error output lines into a designated error log file.
-        /// A line is defined as a sequence of characters followed by a line feed ("\n") or a carriage return immediately followed by a line feed ("\r\n"). The line characters are encoded using the default system ANSI code page. The <see cref="System.Diagnostics.DataReceivedEventArgs.Data" /> property does not include the terminating carriage return or line feed.
-        /// When the redirected stream is closed, a null line is sent to the event handler. Ensure your event handler checks the <see cref="System.Diagnostics.DataReceivedEventArgs.Data" /> property appropriately before accessing it. For example, you can use the static method <see cref="string.IsNullOrEmpty" /> to validate the <see cref="System.Diagnostics.DataReceivedEventArgs.Data" /> property in your event handler.</remarks>
-        /// <example>The following code example illustrates a simple event handler associated with the <see cref="System.Diagnostics.Process.OutputDataReceived" /> event. The event handler receives text lines from the redirected <see cref="System.Diagnostics.Process.StandardOutput" /> stream, formats the text, and writes the text to the screen.
         /// <format type="text/markdown"><![CDATA[
-        /// [!code-cpp[Process_AsyncStreams#4](~/samples/snippets/cpp/VS_Snippets_CLR/process_asyncstreams/CPP/datareceivedevent.cpp#4)]
-        /// [!code-csharp[Process_AsyncStreams#4](~/samples/snippets/csharp/VS_Snippets_CLR/process_asyncstreams/CS/datareceivedevent.cs#4)]
-        /// [!code-vb[Process_AsyncStreams#4](~/samples/snippets/visualbasic/VS_Snippets_CLR/process_asyncstreams/VB/datareceivedevent.vb#4)]
-        /// ]]></format></example>
+        /// [!INCLUDE[remarks] (~/includes/remarks/System.Diagnostics/DataReceivedEventArgs/Data.md)]
+        /// ]]></format>
         public string? Data
         {
             get { return _data; }
