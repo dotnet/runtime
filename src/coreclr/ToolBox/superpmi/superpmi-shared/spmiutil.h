f805314ab83c4b740bfe533b69bc89d4dd1f8ce5
Egor Chesakov <Egor.Chesakov@microsoft.com>
2021-02-25 20:09:04 -0800

    Add IsSpmiTarget64Bit() and SpmiTargetPointerSize() helpers in spmiutil.h

--- src/coreclr/ToolBox/superpmi/superpmi-shared/spmiutil.h
+++ src/coreclr/ToolBox/superpmi/superpmi-shared/spmiutil.h
@@ -56,6 +56,16 @@ enum SPMI_TARGET_ARCHITECTURE
 SPMI_TARGET_ARCHITECTURE GetSpmiTargetArchitecture();
 void SetSpmiTargetArchitecture(SPMI_TARGET_ARCHITECTURE spmiTargetArchitecture);
 
+inline bool IsSpmiTarget64Bit()
+{
+    return (GetSpmiTargetArchitecture() == SPMI_TARGET_ARCHITECTURE_AMD64) || (GetSpmiTargetArchitecture() == SPMI_TARGET_ARCHITECTURE_ARM64);
+}
+
+inline size_t SpmiTargetPointerSize()
+{
+    return IsSpmiTarget64Bit() ? sizeof(INT64) : sizeof(INT32);
+}
+
 void PutArm64Rel28(UINT32* pCode, INT32 imm28);
 void PutArm64Rel21(UINT32* pCode, INT32 imm21);
 void PutArm64Rel12(UINT32* pCode, INT32 imm12);
