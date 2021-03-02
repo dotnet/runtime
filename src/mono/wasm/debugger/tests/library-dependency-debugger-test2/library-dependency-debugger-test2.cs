ff63ca2d9cfbe23a7a981df5d1aca9468945f13c
Thays <thaystg@gmail.com>
2021-03-02 18:09:10 -0300

    Removing unused includes, change test name.

--- src/mono/wasm/debugger/tests/library-dependency-debugger-test2/library-dependency-debugger-test2.cs
+++ src/mono/wasm/debugger/tests/library-dependency-debugger-test2/library-dependency-debugger-test2.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
 
 public enum SomeEnum
 {
