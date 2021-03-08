217b24253133651410dff60acc64d285cc0b3739
Prashanth Govindarajan <prgovi@microsoft.com>
2021-03-08 15:25:39 -0800

    Prohibit BF unless the app opts in

--- src/libraries/System.ComponentModel.TypeConverter/src/System/ComponentModel/Design/DesigntimeLicenseContextSerializer.cs
+++ src/libraries/System.ComponentModel.TypeConverter/src/System/ComponentModel/Design/DesigntimeLicenseContextSerializer.cs
@@ -5,7 +5,7 @@ using System.Runtime.Serialization.Formatters.Binary;
 using System.Runtime.Serialization;
 using System.Collections;
 using System.IO;
-using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics;
 
 namespace System.ComponentModel.Design
 {
@@ -14,6 +14,19 @@ namespace System.ComponentModel.Design
     /// </summary>
     public class DesigntimeLicenseContextSerializer
     {
+        internal const string s_enableUnsafeBinaryFormatterInDesigntimeLicenseContextSerialization = "System.ComponentModel.TypeConverter.EnableUnsafeBinaryFormatterInDesigntimeLicenseContextSerialization";
+        private const string s_enableBinaryFormatter = "System.Runtime.Serialization.EnableUnsafeBinaryFormatterSerialization";
+
+        private static bool EnableUnsafeBinaryFormatterInDesigntimeLicenseContextSerialization
+        {
+            get => AppContext.TryGetSwitch(s_enableUnsafeBinaryFormatterInDesigntimeLicenseContextSerialization, out bool isEnabled) ? isEnabled : false;
+        }
+
+        private static bool BinaryFormatterSerializationEnabled
+        {
+            get => AppContext.TryGetSwitch(s_enableBinaryFormatter, out bool isEnabled) ? isEnabled : false;
+        }
+
         // Not creatable.
         private DesigntimeLicenseContextSerializer()
         {
@@ -25,25 +38,175 @@ namespace System.ComponentModel.Design
         /// </summary>
         public static void Serialize(Stream o, string cryptoKey, DesigntimeLicenseContext context)
         {
-            IFormatter formatter = new BinaryFormatter();
-#pragma warning disable SYSLIB0011 // Issue https://github.com/dotnet/runtime/issues/39293 tracks finding an alternative to BinaryFormatter
-            formatter.Serialize(o, new object[] { cryptoKey, context._savedLicenseKeys });
+            if (EnableUnsafeBinaryFormatterInDesigntimeLicenseContextSerialization)
+            {
+                SerializeWithBinaryFormatter(o, cryptoKey, context);
+            }
+            else
+            {
+                using (BinaryWriter writer = new BinaryWriter(o, encoding: Text.Encoding.UTF8, leaveOpen: true))
+                {
+                    writer.Write((sbyte)-1); // flag to identify BinaryWriter
+                    writer.Write(cryptoKey);
+                    writer.Write(context._savedLicenseKeys.Count);
+                    foreach (DictionaryEntry keyAndValue in context._savedLicenseKeys)
+                    {
+                        writer.Write(keyAndValue.Key.ToString());
+                        writer.Write(keyAndValue.Value.ToString());
+                    }
+                }
+            }
+        }
+
+        private static void SerializeWithBinaryFormatter(Stream o, string cryptoKey, DesigntimeLicenseContext context)
+        {
+            try
+            {
+                IFormatter formatter = new BinaryFormatter();
+#pragma warning disable SYSLIB0011
+                formatter.Serialize(o, new object[] { cryptoKey, context._savedLicenseKeys });
 #pragma warning restore SYSLIB0011
+            }
+            catch (NotSupportedException exception)
+            {
+                if (!BinaryFormatterSerializationEnabled)
+                {
+                    throw new NotSupportedException(exception.Message + " Turn on the EnableUnsafeBinaryFormatterSerialization flag to continue using BinaryFormatter");
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        internal class StreamWrapper : Stream
+        {
+            private Stream _stream;
+            internal bool readFirstByte;
+            internal byte firstByte;
+            public StreamWrapper(Stream stream)
+            {
+                _stream = stream;
+                readFirstByte = false;
+                firstByte = 0;
+            }
+
+            public override bool CanRead => _stream.CanRead;
+
+            public override bool CanSeek => _stream.CanSeek;
+
+            public override bool CanWrite => _stream.CanWrite;
+
+            public override long Length => _stream.Length;
+
+            public override long Position { get => _stream.Position; set => _stream.Position = value; }
+
+            public override void Flush() => _stream.Flush();
+
+            public override int Read(byte[] buffer, int offset, int count)
+            {
+                Debug.Assert(_stream.Position != 0, "Expected the first byte to be read first");
+                if (_stream.Position == 1)
+                {
+                    Debug.Assert(readFirstByte == true);
+                    // Add the first byte read by ReadByte into buffer here
+                    buffer[0] = firstByte;
+                    return _stream.Read(buffer, offset + 1, count - 1) + 1;
+                }
+                return _stream.Read(buffer, offset, count);
+            }
+
+            public override long Seek(long offset, SeekOrigin origin) => _stream.Seek(offset, origin);
+
+            public override void SetLength(long value) => _stream.SetLength(value);
+
+            public override void Write(byte[] buffer, int offset, int count) => _stream.Write(buffer, offset, count);
+
+            public override int ReadByte()
+            {
+                byte read = (byte)_stream.ReadByte();
+                firstByte = read;
+                readFirstByte = true;
+                return read;
+            }
+        }
+
+        /// <summary>
+        /// During deserialization, the stream passed in may be binary formatted or may have used binary writer. This is quick test to discern between them.
+        /// </summary>
+        internal static bool StreamIsBinaryFormatted(StreamWrapper stream)
+        {
+            // For binary formatter, the first byte is the SerializationHeaderRecord and has a value 0
+            int firstByte = stream.ReadByte();
+            if (firstByte != 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static void DeserializeUsingBinaryFormatter(StreamWrapper wrappedStream, string cryptoKey, RuntimeLicenseContext context)
+        {
+            if (EnableUnsafeBinaryFormatterInDesigntimeLicenseContextSerialization)
+            {
+                try
+                {
+#pragma warning disable SYSLIB0011
+                    IFormatter formatter = new BinaryFormatter();
+
+                    object obj = formatter.Deserialize(wrappedStream);
+#pragma warning restore SYSLIB0011
+
+                    if (obj is object[] value)
+                    {
+                        if (value[0] is string && (string)value[0] == cryptoKey)
+                        {
+                            context._savedLicenseKeys = (Hashtable)value[1];
+                        }
+                    }
+                }
+                catch (NotSupportedException exception)
+                {
+                    if (!BinaryFormatterSerializationEnabled)
+                    {
+                        throw new NotSupportedException(exception.Message + " Turn on the EnableUnsafeBinaryFormatterSerialization flag to continue using BinaryFormatter");
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+            else
+            {
+                throw new NotSupportedException(SR.Format(SR.EnableUnsafeBinaryFormatterInDesigntimeLicenseContextSerialization, s_enableUnsafeBinaryFormatterInDesigntimeLicenseContextSerialization));
+            }
         }
 
         internal static void Deserialize(Stream o, string cryptoKey, RuntimeLicenseContext context)
         {
-#pragma warning disable SYSLIB0011 // Issue https://github.com/dotnet/runtime/issues/39293 tracks finding an alternative to BinaryFormatter
-            IFormatter formatter = new BinaryFormatter();
-
-            object obj = formatter.Deserialize(o);
-#pragma warning restore SYSLIB0011
-
-            if (obj is object[] value)
+            StreamWrapper wrappedStream = new StreamWrapper(o);
+            if (StreamIsBinaryFormatted(wrappedStream))
             {
-                if (value[0] is string && (string)value[0] == cryptoKey)
+                DeserializeUsingBinaryFormatter(wrappedStream, cryptoKey, context);
+            }
+            else
+            {
+                using (BinaryReader reader = new BinaryReader(wrappedStream, encoding: Text.Encoding.UTF8, leaveOpen: true))
                 {
-                    context._savedLicenseKeys = (Hashtable)value[1];
+                    sbyte binaryWriterIdentifer = (sbyte)wrappedStream.firstByte;
+                    Debug.Assert(binaryWriterIdentifer == -1, "Expected the first sbyte to be -1");
+                    string streamCryptoKey = reader.ReadString();
+                    int numEntries = reader.ReadInt32();
+                    if (streamCryptoKey == cryptoKey)
+                    {
+                        for (int i = 0; i < numEntries; i++)
+                        {
+                            context._savedLicenseKeys.Add(reader.ReadString(), reader.ReadString());
+                        }
+                    }
                 }
             }
         }
---
+++ src/libraries/System.ComponentModel.TypeConverter/tests/Design/DesigntimeLicenseContextSerializerTests.cs
@@ -0,0 +1,115 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections;
+using System.IO;
+using System.Reflection;
+using Microsoft.DotNet.RemoteExecutor;
+using Xunit;
+
+namespace System.ComponentModel.Design.Tests
+{
+    public static class DesigntimeLicenseContextSerializerTests
+    {
+        private static string enableBinaryFormatterInTypeConverter = "System.ComponentModel.TypeConverter.EnableUnsafeBinaryFormatterInDesigntimeLicenseContextSerialization";
+        private static string enableBinaryFormatter = "System.Runtime.Serialization.EnableUnsafeBinaryFormatterSerialization";
+
+        public static bool AreBinaryFormatterAndRemoteExecutorSupportedOnThisPlatform => PlatformDetection.IsBinaryFormatterSupported && RemoteExecutor.IsSupported;
+
+        [ConditionalFact(nameof(AreBinaryFormatterAndRemoteExecutorSupportedOnThisPlatform))]
+        public static void SerializeAndDeserialize()
+        {
+            RemoteExecutor.Invoke(() =>
+            {
+                foreach (var (useBinaryFormatter, key) in new System.Collections.Generic.List<Tuple<bool, string>>() {
+                    new Tuple<bool, string>(false, "key" ),
+                    new Tuple<bool, string>(true, "key" ),
+                    new Tuple<bool, string>(false, "" ),
+                    new Tuple<bool, string>(true, "" ),
+            })
+                {
+                    {
+                        AppContext.SetSwitch("TestSwitch.LocalAppContext.DisableCaching", true);
+                        if (!useBinaryFormatter)
+                        {
+                            AppContext.SetSwitch(enableBinaryFormatterInTypeConverter, false);
+                        }
+                        else
+                        {
+                            AppContext.SetSwitch(enableBinaryFormatterInTypeConverter, true);
+                        }
+                        var context = new DesigntimeLicenseContext();
+                        context.SetSavedLicenseKey(typeof(int), key);
+                        var assembly = typeof(DesigntimeLicenseContextSerializer).Assembly;
+                        Type runtimeLicenseContextType = assembly.GetType("System.ComponentModel.Design.RuntimeLicenseContext");
+                        Assert.NotNull(runtimeLicenseContextType);
+                        object runtimeLicenseContext = Activator.CreateInstance(runtimeLicenseContextType);
+                        FieldInfo _savedLicenseKeys = runtimeLicenseContextType.GetField("_savedLicenseKeys", BindingFlags.NonPublic | BindingFlags.Instance);
+                        Assert.NotNull(_savedLicenseKeys);
+                        _savedLicenseKeys.SetValue(runtimeLicenseContext, new Hashtable());
+                        Assert.NotNull(runtimeLicenseContext);
+
+                        Type designtimeLicenseContextSerializer = assembly.GetType("System.ComponentModel.Design.DesigntimeLicenseContextSerializer");
+                        Assert.NotNull(designtimeLicenseContextSerializer);
+                        Reflection.MethodInfo deserializeMethod = designtimeLicenseContextSerializer.GetMethod("Deserialize", Reflection.BindingFlags.NonPublic | Reflection.BindingFlags.Static);
+
+                        using (MemoryStream stream = new MemoryStream())
+                        {
+                            long position = stream.Position;
+                            System.ComponentModel.Design.DesigntimeLicenseContextSerializer.Serialize(stream, key, context);
+                            stream.Seek(position, SeekOrigin.Begin);
+                            deserializeMethod.Invoke(null, new object[] { stream, key, runtimeLicenseContext });
+                            Hashtable savedLicenseKeys = runtimeLicenseContext.GetType().GetField("_savedLicenseKeys", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(runtimeLicenseContext) as Hashtable;
+                            Assert.NotNull(savedLicenseKeys);
+                            var value = savedLicenseKeys[typeof(int).AssemblyQualifiedName];
+                            Assert.True(value is string);
+                            Assert.Equal(key, value);
+                        }
+                    }
+                }
+            }).Dispose();
+        }
+
+        [ConditionalFact(nameof(AreBinaryFormatterAndRemoteExecutorSupportedOnThisPlatform))]
+        public static void SerializeWithBinaryFormatter_DeserializeWithBinaryWriter()
+        {
+            RemoteExecutor.Invoke(() =>
+            {
+                foreach (var key in new System.Collections.Generic.List<string>() { "key", "" })
+                {
+                    AppContext.SetSwitch("TestSwitch.LocalAppContext.DisableCaching", true);
+                    AppContext.SetSwitch(enableBinaryFormatterInTypeConverter, true);
+                    AppContext.SetSwitch(enableBinaryFormatter, true);
+                    var context = new DesigntimeLicenseContext();
+                    context.SetSavedLicenseKey(typeof(int), key);
+                    var assembly = typeof(DesigntimeLicenseContextSerializer).Assembly;
+                    Type runtimeLicenseContextType = assembly.GetType("System.ComponentModel.Design.RuntimeLicenseContext");
+                    Assert.NotNull(runtimeLicenseContextType);
+                    object runtimeLicenseContext = Activator.CreateInstance(runtimeLicenseContextType);
+                    Assert.NotNull(runtimeLicenseContext);
+
+                    Type designtimeLicenseContextSerializer = assembly.GetType("System.ComponentModel.Design.DesigntimeLicenseContextSerializer");
+                    Assert.NotNull(designtimeLicenseContextSerializer);
+
+                    using (MemoryStream stream = new MemoryStream())
+                    {
+                        long position = stream.Position;
+                        System.ComponentModel.Design.DesigntimeLicenseContextSerializer.Serialize(stream, key, context);
+                        AppContext.SetSwitch(enableBinaryFormatter, false);
+                        Reflection.MethodInfo deserializeMethod = designtimeLicenseContextSerializer.GetMethod("Deserialize", Reflection.BindingFlags.NonPublic | Reflection.BindingFlags.Static);
+                        stream.Seek(position, SeekOrigin.Begin);
+                        try
+                        {
+                            deserializeMethod.Invoke(null, new object[] { stream, key, runtimeLicenseContext });
+                        }
+                        catch (System.Reflection.TargetInvocationException exception)
+                        {
+                            var baseException = exception.GetBaseException();
+                            Assert.IsType<NotSupportedException>(baseException);
+                        }
+                    }
+                }
+            }).Dispose();
+        }
+    }
+}
