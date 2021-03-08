6a2b4faf337994cbcb7726e4ef5f054910c1965f
Stephen Toub <stoub@microsoft.com>
2021-03-08 09:52:18 -0500

    Reduce startup allocation from event pipe metadata
    A "hello world" console app results in EventPipeMetadataGenerate.GenerateEventMetadata making almost 500 calls to factory methods on ScalarTypeInfo, each of which allocates a new ScalarTypeInfo object and a Func delegate, but they're all immutable objects that could be cached on first use and reused for all subsequent use.  Eliminating the need for the funcs entirely and caching the ScalarTypeInfo objects saves almost 1000 object allocations and 66K of allocation at startup.  It does mean we'll end up holding on to a few objects (9 objects == ~575 bytes) for the duration of the app that we would otherwise have dropped (plus the static fields).

--- src/libraries/System.Private.CoreLib/src/System/Diagnostics/Tracing/TraceLogging/Statics.cs
+++ src/libraries/System.Private.CoreLib/src/System/Diagnostics/Tracing/TraceLogging/Statics.cs
@@ -313,6 +313,16 @@ namespace System.Diagnostics.Tracing
             };
         }
 
+        public static TraceLoggingDataType FormatScalar(EventFieldFormat format, TraceLoggingDataType nativeFormat) =>
+            nativeFormat switch
+            {
+                TraceLoggingDataType.Boolean8 or TraceLoggingDataType.Int8 or TraceLoggingDataType.UInt8 => Format8(format, nativeFormat),
+                TraceLoggingDataType.Char16 or TraceLoggingDataType.Int16 or TraceLoggingDataType.UInt16 => Format16(format, nativeFormat),
+                TraceLoggingDataType.Int32 or TraceLoggingDataType.UInt32 or TraceLoggingDataType.Float => Format32(format, nativeFormat),
+                TraceLoggingDataType.Int64 or TraceLoggingDataType.UInt64 or TraceLoggingDataType.Double => Format64(format, nativeFormat),
+                _ => MakeDataType(nativeFormat, format),
+            };
+
         #endregion
 
         #region Reflection helpers
@@ -477,7 +487,7 @@ namespace System.Diagnostics.Tracing
 
                 if (dataType == typeof(string))
                 {
-                    result = new StringTypeInfo();
+                    result = StringTypeInfo.Instance();
                 }
                 else if (dataType == typeof(bool))
                 {
@@ -529,11 +539,11 @@ namespace System.Diagnostics.Tracing
                 }
                 else if (dataType == typeof(DateTime))
                 {
-                    result = new DateTimeTypeInfo();
+                    result = DateTimeTypeInfo.Instance();
                 }
                 else if (dataType == typeof(decimal))
                 {
-                    result = new DecimalTypeInfo();
+                    result = DecimalTypeInfo.Instance();
                 }
                 else if (dataType == typeof(IntPtr))
                 {
@@ -549,15 +559,15 @@ namespace System.Diagnostics.Tracing
                 }
                 else if (dataType == typeof(TimeSpan))
                 {
-                    result = new TimeSpanTypeInfo();
+                    result = TimeSpanTypeInfo.Instance();
                 }
                 else if (dataType == typeof(DateTimeOffset))
                 {
-                    result = new DateTimeOffsetTypeInfo();
+                    result = DateTimeOffsetTypeInfo.Instance();
                 }
                 else if (dataType == typeof(EmptyStruct))
                 {
-                    result = new NullTypeInfo();
+                    result = NullTypeInfo.Instance();
                 }
                 else if (IsGenericMatch(dataType, typeof(Nullable<>)))
                 {
