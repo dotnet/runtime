f1e28b30525cdf96672d1b24c25ff924b3d9454e
Michael Klement <mklement0@gmail.com>
2021-03-09 17:51:05 -0500

    addressed @krwq's feedback

--- src/libraries/System.Private.Xml/tests/XmlDocument/XmlDocumentTests/SaveTests.cs
+++ src/libraries/System.Private.Xml/tests/XmlDocument/XmlDocumentTests/SaveTests.cs
@@ -7,61 +7,53 @@ using Xunit;
 
 namespace System.Xml.Tests
 {
-  public class SaveTests
-  {
-    public static readonly byte[] utf8BomBytes = new byte[] { 0xef, 0xbb, 0xbf };
-
-    [Fact]
-    public void SaveDocumentToFilePath()
+    public class SaveTests
     {
-      var xmlDocument = new XmlDocument();
-      xmlDocument.AppendChild(xmlDocument.CreateXmlDeclaration("1.0", "UTF-8", null));
-      xmlDocument.AppendChild(xmlDocument.CreateElement("Foo"));
+        public static readonly byte[] utf8BomBytes = Encoding.UTF8.GetPreamble();
 
-      var tmpFile = Path.GetTempFileName();
-      try
-      {
-        // Let .Save() infer the encoding from the declaration.
-        xmlDocument.Save(tmpFile);
-
-        var bytes = File.ReadAllBytes(tmpFile);
-        Assert.False(bytes[0] == utf8BomBytes[0] && bytes[1] == utf8BomBytes[1] && bytes[2] == utf8BomBytes[2], ".Save() should create BOM-less UTF-8 files by default.");
-      }
-      finally
-      {
-        File.Delete(tmpFile);
-      }
-    }
-
-    [Fact]
-    public void SaveDocumentViaWriter()
-    {
-      var xmlDocument = new XmlDocument();
-      xmlDocument.AppendChild(xmlDocument.CreateXmlDeclaration("1.0", null, null));
-      xmlDocument.AppendChild(xmlDocument.CreateElement("Foo"));
-
-      XmlWriterSettings settings = new XmlWriterSettings();
-      // Set the encoding explicitly, to UTF-8 with BOM.
-      settings.Encoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier: true);
-
-      var tmpFile = Path.GetTempFileName();
-      try
-      {
-        using (XmlWriter writer = XmlWriter.Create(tmpFile, settings))
+        [Fact]
+        public void SaveDocumentToFilePath()
         {
-          xmlDocument.Save(writer);
+            var xmlDocument = new XmlDocument();
+            xmlDocument.AppendChild(xmlDocument.CreateXmlDeclaration("1.0", "UTF-8", null));
+            xmlDocument.AppendChild(xmlDocument.CreateElement("Foo"));
+
+            var tmpFile = Path.GetTempFileName();
+            try
+            {
+                // Let .Save() infer the encoding from the declaration.
+                xmlDocument.Save(tmpFile);
+
+                var bytes = File.ReadAllBytes(tmpFile);
+                Assert.False(bytes[0] == utf8BomBytes[0] && bytes[1] == utf8BomBytes[1] && bytes[2] == utf8BomBytes[2], ".Save() should create BOM-less UTF-8 files by default.");
+            }
+            finally
+            {
+                File.Delete(tmpFile);
+            }
         }
 
-        var bytes = File.ReadAllBytes(tmpFile);
-        Assert.True(bytes[0] == utf8BomBytes[0] && bytes[1] == utf8BomBytes[1] && bytes[2] == utf8BomBytes[2], ".Save() should respect an XmlWriter's encoding.");
+        [Fact]
+        public void SaveDocumentViaWriter()
+        {
+            var xmlDocument = new XmlDocument();
+            xmlDocument.AppendChild(xmlDocument.CreateXmlDeclaration("1.0", null, null));
+            xmlDocument.AppendChild(xmlDocument.CreateElement("Foo"));
 
-      }
-      finally
-      {
-        File.Delete(tmpFile);
-      }
+            XmlWriterSettings settings = new XmlWriterSettings();
+            // Set the encoding explicitly, to UTF-8 with BOM.
+            settings.Encoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier: true);
 
+            using (var ms = new MemoryStream())
+            {
+                using (XmlWriter writer = XmlWriter.Create(ms, settings))
+                {
+                    xmlDocument.Save(writer);
+                }
+
+                var bytes = ms.GetBuffer();
+                Assert.True(bytes[0] == utf8BomBytes[0] && bytes[1] == utf8BomBytes[1] && bytes[2] == utf8BomBytes[2], ".Save() should respect an XmlWriter's encoding.");
+            }
+        }
     }
-
-  }
 }
