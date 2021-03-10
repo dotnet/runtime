1f8bf56aaa1efbd62b6d4df16421608a8bd4c543
Matthew Hawkeye Williamson <Hawkeye@76thvfw.org>
2021-03-10 09:10:09 -0600

    Added class ValueChangedEventArgs.

---
+++ src/libraries/System.Private.CoreLib/src/System/ValueChangedEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+[Serializeable]
+public class ValueChangedEventArgs<T> : EventArgs
+{
+    public T OldValue { get; }
+
+    public T NewValue { get; }
+
+    public ValueChangedEventArgs(T oldValue, T newValue)
+    {
+        OldValue = oldValue;
+        NewValue = newValue;
+    }
+}
+
+public delegate void OnValueChanged(object sender, ValueChangedEventArgs e);
