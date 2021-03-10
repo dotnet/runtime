fed0c92f0b5cf7511e1ee1d6269b6a34fa34234c
Steve MacLean <Steve.MacLean@Microsoft.com>
2021-03-10 18:15:37 -0500

    Disable failing Apple Silicon Libraries tests
    -  Use tests.proj to disable catastrophically failing libraries tests
    -  Add ActiveIssues where XUnit didn't crash

--- src/libraries/System.Diagnostics.Process/tests/ProcessStreamReadTests.cs
+++ src/libraries/System.Diagnostics.Process/tests/ProcessStreamReadTests.cs
@@ -14,6 +14,7 @@ using Xunit;
 
 namespace System.Diagnostics.Tests
 {
+    [ActiveIssue("https://github.com/dotnet/runtime/issues/49568", typeof(PlatformDetection), nameof(PlatformDetection.IsMacOsAppleSilicon))]
     public class ProcessStreamReadTests : ProcessTestBase
     {
         [ConditionalFact(typeof(RemoteExecutor), nameof(RemoteExecutor.IsSupported))]
