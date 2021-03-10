89f4d8fb0d476063551f19667c6e63d990f7df4c
Egor Bogatov <egorbo@gmail.com>
2021-03-10 19:58:03 +0300

    [release/5.0] Fix logic to test bits in a constant vector (Vector256) (#48613)
    * [5.0] Fix logic to test bits in a constant vector (Vector256)
    Backport of https://github.com/dotnet/runtime/pull/47385
    
    * rename folder
    
    * rename test files

--- src/coreclr/src/jit/lower.h
+++ src/coreclr/src/jit/lower.h
@@ -349,6 +349,28 @@ private:
     };
 
     //----------------------------------------------------------------------------------------------
+    // VectorConstantIsBroadcastedI64: Check N i64 elements in a constant vector for equality
+    //
+    //  Arguments:
+    //     vecCns  - Constant vector
+    //     count   - Amount of i64 components to compare
+    //
+    //  Returns:
+    //     true if N i64 elements of the given vector are equal
+    static bool VectorConstantIsBroadcastedI64(VectorConstant& vecCns, int count)
+    {
+        assert(count >= 1 && count <= 4);
+        for (int i = 1; i < count; i++)
+        {
+            if (vecCns.i64[i] != vecCns.i64[0])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //----------------------------------------------------------------------------------------------
     // ProcessArgForHWIntrinsicCreate: Processes an argument for the Lowering::LowerHWIntrinsicCreate method
     //
     //  Arguments:
