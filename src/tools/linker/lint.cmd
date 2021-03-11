e9ab3442854f5c0d74aaf742a347c56b500dd208
Vitek Karas <vitek.karas@microsoft.com>
2021-03-11 04:37:47 -0800

    Fix lint to avoid generating binlog file (dotnet/linker#1879)
    Latest lint writes formatDiagnosticLog.binlong next to the solution file when running with --verbosity diagnostic. There's no way to redirect or disable this - https://github.com/dotnet/format/issues/1041.
    
    So this change disables --verbosity diagnostic for local runs of lint and adds it only to the CI (where the machine will clean the enlistment anyway).
    
    Commit migrated from https://github.com/dotnet/linker/commit/db52b51e8fbdefb8ddfa076dbb40edc55af47c42

--- src/tools/linker/lint.cmd
+++ src/tools/linker/lint.cmd
@@ -1,3 +1,3 @@
 @echo off
 powershell -ExecutionPolicy ByPass -NoProfile -command "Set-Location %~dp0; & """%~dp0eng\dotnet.ps1""" ""tool restore"""
-powershell -ExecutionPolicy ByPass -NoProfile -command "Set-Location %~dp0; & """%~dp0eng\dotnet.ps1""" ""tool run dotnet-format -- illink.sln --verbosity diagnostic --fix-whitespace --exclude src/analyzer src/tuner external %*"""
+powershell -ExecutionPolicy ByPass -NoProfile -command "Set-Location %~dp0; & """%~dp0eng\dotnet.ps1""" ""tool run dotnet-format -- illink.sln --fix-whitespace --exclude src/analyzer src/tuner external %*"""
