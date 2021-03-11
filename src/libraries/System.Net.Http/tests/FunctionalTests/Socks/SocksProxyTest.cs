488218118a5ae2867ec47a81bc6b230e15f1f071
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-03-11 20:58:00 +0800

    Expand test matrix.

--- src/libraries/System.Net.Http/tests/FunctionalTests/Socks/SocksProxyTest.cs
+++ src/libraries/System.Net.Http/tests/FunctionalTests/Socks/SocksProxyTest.cs
@@ -2,14 +2,14 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Net.Test.Common;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace System.Net.Http.Functional.Tests.Socks
 {
-    [ConditionalClass(typeof(PlatformDetection), nameof(PlatformDetection.IsNotBrowser))]
-    public sealed class SocksProxyTest : HttpClientHandlerTestBase
+    public abstract class SocksProxyTest : HttpClientHandlerTestBase
     {
         public SocksProxyTest(ITestOutputHelper helper) : base(helper)
         {
@@ -19,7 +19,7 @@ namespace System.Net.Http.Functional.Tests.Socks
         [InlineData("socks4")]
         [InlineData("socks4a")]
         [InlineData("socks5")]
-        public async Task TestLoopbackHttp1Async(string schema)
+        public async Task TestLoopbackAsync(string schema)
         {
             await LoopbackServerFactory.CreateClientAndServerAsync(async url =>
             {
@@ -28,7 +28,9 @@ namespace System.Net.Http.Functional.Tests.Socks
                     using (var handler = CreateHttpClientHandler())
                     using (var client = CreateHttpClient())
                     {
+                        client.DefaultRequestVersion = UseVersion;
                         handler.Proxy = new WebProxy($"{schema}://localhost:{proxy.Port}");
+
                         string response = await client.GetStringAsync(url);
                         Assert.Equal("Echo", response);
                     }
@@ -36,5 +38,59 @@ namespace System.Net.Http.Functional.Tests.Socks
             },
             async server => await server.HandleRequestAsync(content: "Echo"));
         }
+
+        [Theory]
+        [InlineData("socks4")]
+        [InlineData("socks4a")]
+        [InlineData("socks5")]
+        public async Task TestLoopbackHttpsAsync(string schema)
+        {
+            using var cert = TestHelper.CreateServerSelfSignedCertificate();
+
+            await LoopbackServerFactory.CreateClientAndServerAsync(async url =>
+            {
+                using (var proxy = LoopbackSocksServer.Create())
+                {
+                    using (var handler = CreateHttpClientHandler())
+                    using (var client = CreateHttpClient())
+                    {
+                        client.DefaultRequestVersion = UseVersion;
+                        handler.Proxy = new WebProxy($"{schema}://localhost:{proxy.Port}");
+                        handler.ServerCertificateCustomValidationCallback = (_1, _2, _3, _4) => true;
+
+                        string response = await client.GetStringAsync($"https://{cert.GetNameInfo(X509NameType.SimpleName, false)}:{url.Port}/");
+                        Assert.Equal("Echo", response);
+                    }
+                }
+            },
+            async server => await server.HandleRequestAsync(content: "Echo"),
+            options: new GenericLoopbackOptions
+            {
+                UseSsl = true,
+                Certificate = cert
+            });
+        }
+    }
+
+
+    [ConditionalClass(typeof(PlatformDetection), nameof(PlatformDetection.IsNotBrowser))]
+    public sealed class SocksProxyTest_Http1 : SocksProxyTest
+    {
+        public SocksProxyTest_Http1(ITestOutputHelper helper) : base(helper)
+        {
+        }
+
+        protected override Version UseVersion => HttpVersion.Version11;
+    }
+
+
+    [ConditionalClass(typeof(PlatformDetection), nameof(PlatformDetection.IsNotBrowser))]
+    public sealed class SocksProxyTest_Http2 : SocksProxyTest
+    {
+        public SocksProxyTest_Http2(ITestOutputHelper helper) : base(helper)
+        {
+        }
+
+        protected override Version UseVersion => HttpVersion.Version20;
     }
 }
