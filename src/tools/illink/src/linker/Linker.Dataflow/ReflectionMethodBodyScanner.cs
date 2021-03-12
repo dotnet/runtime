3e2831ac177adb0f6d39781f3c4c9b7a308f1307
Vitek Karas <vitek.karas@microsoft.com>
2021-03-12 05:53:07 -0800

    Relax validtion of MakeGeneric* on struct and unmanaged constraint (dotnet/linker#1888)
    `struct` and `unmanaged` constraint also imply `new()` constraint, but any type allowable for `struct` or `unmanaged` constraints always has public parameterless constructor. Or rather the type can be constructed without parameters (the .ctor itself is actually not accessible through reflection).
    
    Since MakeGenericType and MakeGenericMethod both enforce that only types compatible with `struct` or `unmanaged` constraints can be passed as type arguments to such generic parameters, there's no need for linker to validate this as well. At runtime it will always work - and on top of that, there's nothing for linker to mark - struct and unmanaged don't have a parameterless .ctor in IL - it's just that at runtime they behave as if they do.
    
    So enforcing the `new()` constraint in `MakeGenericMethod` or `MakeGenericType` makes sense only when it's just `new()` constraint, not if it's implied from `struct` or `unmanaged` constraints.
    
    * Change the behavior to not relax but instead single out Nullable<> as a special case.
    
    Nullable<> doesn't make use of the implied new() constraint, so there's no reason for linker to enforce it. It's by far the most common case where this is problematic - that's why the special handling.
    
    Commit migrated from https://github.com/dotnet/linker/commit/47c00c1bcf90e9086edf2b28f8fcf22672beb250

--- src/tools/illink/src/linker/Linker.Dataflow/ReflectionMethodBodyScanner.cs
+++ src/tools/illink/src/linker/Linker.Dataflow/ReflectionMethodBodyScanner.cs
@@ -681,9 +681,14 @@ namespace Mono.Linker.Dataflow
 							if (value is SystemTypeValue typeValue) {
 								foreach (var genericParameter in typeValue.TypeRepresented.GenericParameters) {
 									if (_context.Annotations.FlowAnnotations.GetGenericParameterAnnotation (genericParameter) != DynamicallyAccessedMemberTypes.None ||
-										genericParameter.HasDefaultConstructorConstraint) {
+										(genericParameter.HasDefaultConstructorConstraint && !typeValue.TypeRepresented.IsTypeOf ("System", "Nullable`1"))) {
 										// There is a generic parameter which has some requirements on the input types.
 										// For now we don't support tracking actual array elements, so we can't validate that the requirements are fulfilled.
+
+										// Special case: Nullable<T> where T : struct
+										//  The struct constraint in C# implies new() constraints, but Nullable doesn't make a use of that part.
+										//  There are several places even in the framework where typeof(Nullable<>).MakeGenericType would warn
+										//  without any good reason to do so.
 										reflectionContext.RecordUnrecognizedPattern (
 											2055,
 											$"Call to '{calledMethodDefinition.GetDisplayName ()}' can not be statically analyzed. " +
