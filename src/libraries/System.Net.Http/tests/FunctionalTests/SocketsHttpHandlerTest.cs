94df2acdad31a2f22d8d4a88062db2fd0967e58b
Stephen Toub <stoub@microsoft.com>
2021-03-16 07:19:44 -0400

    Add another test

--- src/libraries/System.Net.Http/tests/FunctionalTests/SocketsHttpHandlerTest.cs
+++ src/libraries/System.Net.Http/tests/FunctionalTests/SocketsHttpHandlerTest.cs
@@ -121,6 +121,7 @@ namespace System.Net.Http.Functional.Tests
                 using (HttpClient client = CreateHttpClient())
                 {
                     client.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Language", "en-US,en;q=0.5"); // validation would add spaces
+                    client.DefaultRequestHeaders.TryAddWithoutValidation("From", "invalidemail"); // would fail to parse if validated
 
                     var m = new HttpRequestMessage(HttpMethod.Get, uri) { Version = UseVersion };
                     (await client.SendAsync(TestAsync, m)).Dispose();
@@ -128,7 +129,8 @@ namespace System.Net.Http.Functional.Tests
             }, async server =>
             {
                 List<string> headers = await server.AcceptConnectionSendResponseAndCloseAsync();
-                Assert.Contains(headers, header => header.Contains("en-US,en;q=0.5"));
+                Assert.Contains(headers, header => header.Contains("Accept-Language: en-US,en;q=0.5"));
+                Assert.Contains(headers, header => header.Contains("From: invalidemail"));
             });
         }
     }
