d0d3d2f65fde976eb1067da58e9fee33de0e9f0c
Levi Broderick <levib@microsoft.com>
2021-03-17 10:53:09 -0700

    Rename files JavaScriptStringEncoder -> JavaScriptEncoder

--- src/libraries/System.Text.Encodings.Web/tests/JavaScriptEncoderTests.Relaxed.cs
+++ src/libraries/System.Text.Encodings.Web/tests/JavaScriptEncoderTests.Relaxed.cs
@@ -8,7 +8,7 @@ using Xunit;
 
 namespace System.Text.Encodings.Web.Tests
 {
-    public partial class JavaScriptStringEncoderTests
+    public partial class JavaScriptEncoderTests
     {
         [Fact]
         public void TestSurrogate_Relaxed()
--- src/libraries/System.Text.Encodings.Web/tests/JavaScriptEncoderTests.cs
+++ src/libraries/System.Text.Encodings.Web/tests/JavaScriptEncoderTests.cs
@@ -11,7 +11,7 @@ using Xunit;
 
 namespace System.Text.Encodings.Web.Tests
 {
-    public partial class JavaScriptStringEncoderTests
+    public partial class JavaScriptEncoderTests
     {
         [Fact]
         public void Factory_NullArgs_Throws()
