11cd9bcc493ea340437e8545ba11e8fe53274e0d
Radek Doulik <radekdoulik@gmail.com>
2021-03-17 11:17:59 +0100

    Do not set $os, it is already set above

--- eng/build.ps1
+++ eng/build.ps1
@@ -229,7 +229,7 @@ foreach ($argument in $PSBoundParameters.Keys)
     "runtimeFlavor"          { $arguments += " /p:RuntimeFlavor=$($PSBoundParameters[$argument].ToLowerInvariant())" }
     "librariesConfiguration" { $arguments += " /p:LibrariesConfiguration=$((Get-Culture).TextInfo.ToTitleCase($($PSBoundParameters[$argument])))" }
     "framework"              { $arguments += " /p:BuildTargetFramework=$($PSBoundParameters[$argument].ToLowerInvariant())" }
-    "os"                     { $os = $($PSBoundParameters[$argument]); $arguments += " /p:TargetOS=$os" }
+    "os"                     { $arguments += " /p:TargetOS=$($PSBoundParameters[$argument])" }
     "allconfigurations"      { $arguments += " /p:BuildAllConfigurations=true" }
     "properties"             { $arguments += " " + $properties }
     "verbosity"              { $arguments += " -$argument " + $($PSBoundParameters[$argument]) }
