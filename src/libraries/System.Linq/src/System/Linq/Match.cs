1a054cd2f9bcf347ab7ae65b6b113029e7eef74c
Alexey Sosnin <sosnin.alexey@outlook.com>
2021-03-18 14:49:14 +0300

    Add System.Linq.Enumerable.Match implementation

--- src/libraries/System.Linq/src/System/Linq/Match.cs
+++ src/libraries/System.Linq/src/System/Linq/Match.cs
@@ -9,7 +9,32 @@ namespace System.Linq
     {
         public static (List<TSource> Matched, List<TSource> Unmatched) Match<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
         {
-            throw new NotImplementedException();
+            if (source == null)
+            {
+                ThrowHelper.ThrowArgumentNullException(ExceptionArgument.source);
+            }
+
+            if (predicate == null)
+            {
+                ThrowHelper.ThrowArgumentNullException(ExceptionArgument.predicate);
+            }
+
+            List<TSource> matched = new List<TSource>();
+            List<TSource> unmatched = new List<TSource>();
+
+            foreach (var element in source)
+            {
+                if (predicate(element))
+                {
+                    matched.Add(element);
+                }
+                else
+                {
+                    unmatched.Add(element);
+                }
+            }
+
+            return (matched, unmatched);
         }
     }
 }
