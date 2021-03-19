c514abcaa4c8c7c39e89b42c6a23130fc4a50452
Eric Erhardt <eric.erhardt@microsoft.com>
2021-03-18 19:02:24 -0500

    Add back a couple suppressions that are still necessary.

--- src/libraries/System.Linq.Expressions/src/System/Runtime/CompilerServices/CallSite.cs
+++ src/libraries/System.Linq.Expressions/src/System/Runtime/CompilerServices/CallSite.cs
@@ -279,6 +279,10 @@ namespace System.Runtime.CompilerServices
             }
         }
 
+#if FEATURE_COMPILE
+        [UnconditionalSuppressMessage("ReflectionAnalysis", "IL2060:MakeGenericMethod",
+            Justification = "UpdateDelegates methods don't have ILLink annotations.")]
+#endif
         internal T MakeUpdateDelegate()
         {
 #if !FEATURE_COMPILE
@@ -352,6 +356,8 @@ namespace System.Runtime.CompilerServices
         }
 #endif
 
+        [UnconditionalSuppressMessage("ReflectionAnalysis", "IL2060:MakeGenericMethod",
+            Justification = "CallSiteOps methods don't have trimming annotations.")]
         private T CreateCustomUpdateDelegate(MethodInfo invoke)
         {
             Type returnType = invoke.GetReturnType();
