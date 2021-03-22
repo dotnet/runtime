143103b07ccdbcd2e1637ccd9bff9ad7a4d4a993
Bruce Forstall <brucefo@microsoft.com>
2021-03-22 10:47:07 -0700

    Remove duplicate method name string initialization (#49963)
    Both `compInit` and `compCompile` were initializing the method/class/full
    name strings. The call to `eeGetMethodFullName` is not cheap, and shows
    up high on a SuperPMI replay trace.
    
    Remove the duplicate initialization in `compCompile`.
    
    Speeds up SuperPMI replay and asm diffs.

--- src/coreclr/jit/compiler.cpp
+++ src/coreclr/jit/compiler.cpp
@@ -5613,18 +5613,6 @@ int Compiler::compCompile(CORINFO_MODULE_HANDLE classPtr,
 
     info.compProfilerCallback = false; // Assume false until we are told to hook this method.
 
-#if defined(DEBUG) || defined(LATE_DISASM)
-    const char* classNamePtr;
-
-    info.compMethodName = eeGetMethodName(info.compMethodHnd, &classNamePtr);
-    unsigned len        = (unsigned)roundUp(strlen(classNamePtr) + 1);
-    info.compClassName  = getAllocator(CMK_DebugOnly).allocate<char>(len);
-    strcpy_s((char*)info.compClassName, len, classNamePtr);
-
-    info.compFullName  = eeGetMethodFullName(info.compMethodHnd);
-    info.compPerfScore = 0.0;
-#endif // defined(DEBUG) || defined(LATE_DISASM)
-
 #ifdef DEBUG
     if (!compIsForInlining())
     {
