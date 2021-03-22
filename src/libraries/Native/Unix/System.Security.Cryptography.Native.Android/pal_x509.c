61889c8e8926aed2a22000c312baa0a7969cc927
Jeremy Koritzinsky <jekoritz@microsoft.com>
2021-03-22 13:16:47 -0700

    Skip explanatory text at the start of cert byte blobs if we find a PEM header. (#49920)
    * Skip whitespace bytes at the start of cert byte blobs.
    
    This enables support for PEM-formatted certs prepended with whitespace.
    
    * Handle non-whitespace explanatory text.
    
    * PR feedback.
    
    * Bail early if we don't have enough space to find a header.
    
    Run formatting.

--- src/libraries/Native/Unix/System.Security.Cryptography.Native.Android/pal_x509.c
+++ src/libraries/Native/Unix/System.Security.Cryptography.Native.Android/pal_x509.c
@@ -4,30 +4,37 @@
 #include "pal_x509.h"
 
 #include "pal_eckey.h"
-#include "pal_rsa.h"
 #include "pal_misc.h"
+#include "pal_rsa.h"
 
 #include <assert.h>
+#include <ctype.h>
 #include <stdbool.h>
 #include <string.h>
 
-#define INIT_LOCALS(name, ...) \
-    enum { __VA_ARGS__, count_##name }; \
-    jobject name[count_##name] = { 0 }; \
+#define INIT_LOCALS(name, ...)                                                                                         \
+    enum                                                                                                               \
+    {                                                                                                                  \
+        __VA_ARGS__,                                                                                                   \
+        count_##name                                                                                                   \
+    };                                                                                                                 \
+    jobject name[count_##name] = {0};
 
-#define RELEASE_LOCALS(name, env) \
-{ \
-    for (int i_##name = 0; i_##name < count_##name; ++i_##name) \
-    { \
-        jobject local = name[i_##name]; \
-        if (local != NULL) \
-            (*env)->DeleteLocalRef(env, local); \
-    } \
-} \
+#define RELEASE_LOCALS(name, env)                                                                                      \
+    {                                                                                                                  \
+        for (int i_##name = 0; i_##name < count_##name; ++i_##name)                                                    \
+        {                                                                                                              \
+            jobject local = name[i_##name];                                                                            \
+            if (local != NULL)                                                                                         \
+                (*env)->DeleteLocalRef(env, local);                                                                    \
+        }                                                                                                              \
+    }
 
 #define INSUFFICIENT_BUFFER -1
 
 static int32_t PopulateByteArray(JNIEnv* env, jbyteArray source, uint8_t* dest, int32_t* len);
+
+static void FindCertStart(const uint8_t** buffer, int32_t* len);
 
 // Handles both DER and PEM formats
 jobject /*X509Certificate*/ AndroidCryptoNative_X509Decode(const uint8_t* buf, int32_t len)
@@ -37,6 +44,8 @@ jobject /*X509Certificate*/ AndroidCryptoNative_X509Decode(const uint8_t* buf, i
 
     jobject ret = NULL;
     INIT_LOCALS(loc, bytes, stream, certType, certFactory)
+
+    FindCertStart(&buf, &len);
 
     // byte[] bytes = new byte[] { ... }
     // InputStream stream = new ByteArrayInputStream(bytes);
@@ -184,7 +193,8 @@ int32_t AndroidCryptoNative_X509ExportPkcs7(jobject* /*X509Certificate[]*/ certs
 
     // CertPath certPath = certFactory.generateCertPath(certList);
     // byte[] encoded = certPath.getEncoded("PKCS7");
-    loc[certPath] = (*env)->CallObjectMethod(env, loc[certFactory], g_CertFactoryGenerateCertPathFromList, loc[certList]);
+    loc[certPath] =
+        (*env)->CallObjectMethod(env, loc[certFactory], g_CertFactoryGenerateCertPathFromList, loc[certList]);
     ON_EXCEPTION_PRINT_AND_GOTO(cleanup);
     loc[pkcs7Type] = JSTRING("PKCS7");
     loc[encoded] = (*env)->CallObjectMethod(env, loc[certPath], g_CertPathGetEncoded, loc[pkcs7Type]);
@@ -211,6 +221,8 @@ PAL_X509ContentType AndroidCryptoNative_X509GetContentType(const uint8_t* buf, i
     // The generateCertificate method used for the X509 DER/PEM check will succeed for some
     // PKCS7 blobs, so it is done after the PKCS7 check.
 
+    FindCertStart(&buf, &len);
+
     // byte[] bytes = new byte[] { ... }
     // InputStream stream = new ByteArrayInputStream(bytes);
     loc[bytes] = (*env)->NewByteArray(env, len);
@@ -225,7 +237,8 @@ PAL_X509ContentType AndroidCryptoNative_X509GetContentType(const uint8_t* buf, i
 
     // CertPath certPath = certFactory.generateCertPath(stream, "PKCS7");
     loc[pkcs7Type] = JSTRING("PKCS7");
-    loc[certPath] = (*env)->CallObjectMethod(env, loc[certFactory], g_CertFactoryGenerateCertPathFromStream, loc[stream], loc[pkcs7Type]);
+    loc[certPath] = (*env)->CallObjectMethod(
+        env, loc[certFactory], g_CertFactoryGenerateCertPathFromStream, loc[stream], loc[pkcs7Type]);
     if (!TryClearJNIExceptions(env))
     {
         ret = PAL_Pkcs7;
@@ -286,4 +299,57 @@ static int32_t PopulateByteArray(JNIEnv* env, jbyteArray source, uint8_t* dest, 
 
     (*env)->GetByteArrayRegion(env, source, 0, bytesLen, (jbyte*)dest);
     return CheckJNIExceptions(env) ? FAIL : SUCCESS;
+}
+
+static void FindCertStart(const uint8_t** buffer, int32_t* len)
+{
+    assert(buffer != NULL && *buffer != NULL);
+    assert(*len >= 0);
+
+    if (iscntrl(**buffer) && !isspace(**buffer))
+    {
+        // If the character is a control character that isn't whitespace, then we're probably using a DER encoding
+        // and not using a PEM encoding in ASCII.
+        return;
+    }
+
+    const uint8_t* bufferLocal = *buffer;
+    int32_t lengthLocal = *len;
+
+    while (lengthLocal > 0)
+    {
+        const char pemHeader[] = "-----BEGIN ";
+        int32_t pemHeaderLength = (int32_t)(sizeof(pemHeader) - 1); // Exclude the null-terminator
+        // Skip until we see the - that could start a PEM block.
+        while (lengthLocal >= pemHeaderLength && (!iscntrl(*bufferLocal) || isspace(*bufferLocal)) &&
+               *bufferLocal != pemHeader[0])
+        {
+            bufferLocal += 1;
+            lengthLocal -= 1;
+        }
+
+        if (lengthLocal < pemHeaderLength || (iscntrl(*bufferLocal) && !isspace(*bufferLocal)))
+        {
+            // Either the buffer doesn't have enough space to contain a PEM header
+            // or we encountered a control character that isn't whitespace.
+            // In the insufficient size case, we didn't find the PEM header, so we can't skip to it.
+            // In the control character case, we know that this isn't explanatory info since that needs to
+            // all be printable or whitespace characters, not non-whitespace control characters.
+            return;
+        }
+
+        if (memcmp(bufferLocal, pemHeader, (size_t)pemHeaderLength) == 0)
+        {
+            // We found the PEM header.
+            *buffer = bufferLocal;
+            *len = lengthLocal;
+            return;
+        }
+        else
+        {
+            // This PEM header is invalid. Skip it.
+            bufferLocal += 1;
+            lengthLocal -= 1;
+        }
+    }
 }
