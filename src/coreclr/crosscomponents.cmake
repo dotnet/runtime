7acef3206be24d34f861cf206c38cc0a631849ca
Jeremy Koritzinsky <jekoritz@microsoft.com>
2021-03-22 15:16:02 -0700

    Fix destinations.

--- src/coreclr/crosscomponents.cmake
+++ src/coreclr/crosscomponents.cmake
@@ -4,12 +4,14 @@ if (CLR_CMAKE_HOST_OS STREQUAL CLR_CMAKE_TARGET_OS)
     install_clr (TARGETS
         clrjit
         jitinterface_${ARCH_HOST_NAME}
+        DESTINATIONS .
         COMPONENT crosscomponents
     )
 
     if(CLR_CMAKE_HOST_LINUX OR NOT FEATURE_CROSSBITNESS)
         install_clr (TARGETS
             crossgen
+            DESTINATIONS .
             COMPONENT crosscomponents
         )
     endif()
@@ -17,6 +19,7 @@ if (CLR_CMAKE_HOST_OS STREQUAL CLR_CMAKE_TARGET_OS)
     if (CLR_CMAKE_TARGET_UNIX)
         install_clr (TARGETS
             clrjit_unix_${ARCH_TARGET_NAME}_${ARCH_HOST_NAME}
+            DESTINATIONS .
             COMPONENT crosscomponents
         )
     endif(CLR_CMAKE_TARGET_UNIX)
@@ -26,6 +29,7 @@ if(NOT CLR_CMAKE_HOST_LINUX AND NOT CLR_CMAKE_HOST_OSX AND NOT FEATURE_CROSSBITN
     install_clr (TARGETS
         mscordaccore
         mscordbi
+        DESTINATIONS .
         COMPONENT crosscomponents
     )
 endif()
