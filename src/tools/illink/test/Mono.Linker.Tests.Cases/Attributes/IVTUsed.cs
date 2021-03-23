548e9d436d65de04883251bb507dfe55d88a0182
Marek Safar <marek.safar@gmail.com>
2021-03-23 18:15:56 +0100

    Run primary queue processing after IVT attributes are checked. (dotnet/linker#1914)
    Commit migrated from https://github.com/dotnet/linker/commit/6646911ccb8113135d2bc90e8a957f45d451c0f4

--- src/tools/illink/test/Mono.Linker.Tests.Cases/Attributes/IVTUsed.cs
+++ src/tools/illink/test/Mono.Linker.Tests.Cases/Attributes/IVTUsed.cs
@@ -1,6 +1,7 @@
 using System.Runtime.CompilerServices;
 using Mono.Linker.Tests.Cases.Attributes.Dependencies;
 using Mono.Linker.Tests.Cases.Expectations.Assertions;
+using Mono.Linker.Tests.Cases.Expectations.Helpers;
 using Mono.Linker.Tests.Cases.Expectations.Metadata;
 
 namespace Mono.Linker.Tests.Cases.Attributes
@@ -9,6 +10,11 @@ namespace Mono.Linker.Tests.Cases.Attributes
 	[KeptAssembly ("lib.dll")]
 	[KeptMemberInAssembly ("lib.dll", typeof (External), "InternalMethod()")]
 	[KeptAttributeInAssembly ("lib.dll", typeof (InternalsVisibleToAttribute))]
+
+	// This is a bit fragile but it's used to test that ITV attribute is marked correctly
+	[SetupLinkerTrimMode ("link")]
+	[SetupLinkerArgument ("--skip-unresolved", "true")]
+	[KeptTypeInAssembly (PlatformAssemblies.CoreLib, typeof (InternalsVisibleToAttribute))]
 	class IVTUsed
 	{
 		static void Main ()
