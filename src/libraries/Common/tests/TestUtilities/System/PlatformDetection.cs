631dbd3341a29fca6aa822e99f1ff51cc36763da
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2021-03-24 11:57:49 +0100

    Update PlatformDetection.cs

--- src/libraries/Common/tests/TestUtilities/System/PlatformDetection.cs
+++ src/libraries/Common/tests/TestUtilities/System/PlatformDetection.cs
@@ -76,6 +76,8 @@ namespace System
 
             }
         }
+        
+        public static bool IsLineNumbersSupported => !IsBrowser;
 
         public static bool IsInContainer => GetIsInContainer();
         public static bool SupportsComInterop => IsWindows && IsNotMonoRuntime; // matches definitions in clr.featuredefines.props
