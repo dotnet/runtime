1fc73ef68802991deedc9ee3ee948bad739da6fa
David Wrighton <davidwr@microsoft.com>
2021-03-25 11:18:02 -0700

    Refactor so that C# code is expressed as C# not as hand copied around IL

---
+++ src/tests/Loader/classloader/StaticVirtualMethods/GenericContext/GenericContextCommonCs.cs
@@ -0,0 +1,60 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+
+static class Statics
+{
+    public static string String;
+    public static int Failures;
+    public static volatile IntPtr FtnHolder;
+    public static volatile Action ActionHolder;
+
+    public static void CheckForFailure(string scenario, string expectedResult)
+    {
+        if (String != expectedResult)
+        {
+            Console.WriteLine($"Scenario {scenario} failed");
+            Failures++;
+        }
+    }
+    public static string MakeName(RuntimeTypeHandle t)
+    {
+        return MakeName(Type.GetTypeFromHandle(t));
+    }
+    public static string MakeName(Type t)
+    {
+        StringBuilder sb = new StringBuilder();
+        if (t == typeof(int))
+            return "int32";
+        if (t == typeof(string))
+            return "string";
+        if (t == typeof(object))
+            return "object";
+        if (t.IsValueType)
+            sb.Append("valuetype ");
+        else
+            sb.Append("class ");
+
+        sb.Append(t.Name);
+        if (t.GetGenericArguments().Length > 0)
+        {
+            sb.Append('<');
+            bool first = true;
+            foreach (var inst in t.GetGenericArguments())
+            {
+                if (!first)
+                {
+                    sb.Append(',');
+                }
+                else
+                {
+                    first = false;
+                }
+                sb.Append(MakeName(inst));
+            }
+            sb.Append('>');
+        }
+        return sb.ToString();
+    }
+}
