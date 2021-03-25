701ffe9f47f98cf28255b35ea3c15074fb6aea96
Neale Ferguson <neale@sinenomine.net>
2021-03-25 06:25:06 +0000

    Add explicit conversion for s390x

--- src/mono/mono/mini/method-to-ir.c
+++ src/mono/mono/mini/method-to-ir.c
@@ -9844,6 +9844,7 @@ field_access_end:
 
 			context_used = mini_class_check_context_used (cfg, klass);
 
+#ifndef TARGET_S390X
 			if (sp [0]->type == STACK_I8 && TARGET_SIZEOF_VOID_P == 4) {
 				MONO_INST_NEW (cfg, ins, OP_LCONV_TO_OVF_U4);
 				ins->sreg1 = sp [0]->dreg;
@@ -9852,6 +9853,9 @@ field_access_end:
 				MONO_ADD_INS (cfg->cbb, ins);
 				*sp = mono_decompose_opcode (cfg, ins);
 			}
+#else
+			/* The array allocator expects a 64-bit input, and we cannot rely
+			   on the high bits of a 32-bit result, so we have to extend.  */
 			if (sp [0]->type == STACK_I4 && TARGET_SIZEOF_VOID_P == 8) {
 				MONO_INST_NEW (cfg, ins, OP_ICONV_TO_I8);
 				ins->sreg1 = sp [0]->dreg;
@@ -9860,6 +9864,7 @@ field_access_end:
 				MONO_ADD_INS (cfg->cbb, ins);
 				*sp = mono_decompose_opcode (cfg, ins);
 			}
+#endif
 
 			if (context_used) {
 				MonoInst *args [3];
