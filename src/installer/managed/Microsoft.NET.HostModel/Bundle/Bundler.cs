19b3bbc1f7cf24edfc6accac692596657096543c
vsadov <vsadov@microsoft.com>
2021-03-29 08:26:35 -0700

    sorted usings

--- src/installer/managed/Microsoft.NET.HostModel/Bundle/Bundler.cs
+++ src/installer/managed/Microsoft.NET.HostModel/Bundle/Bundler.cs
@@ -1,15 +1,15 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Microsoft.NET.HostModel.AppHost;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.IO;
+using System.IO.Compression;
+using System.Linq;
 using System.Reflection.PortableExecutable;
 using System.Runtime.InteropServices;
-using System.IO.Compression;
+using Microsoft.NET.HostModel.AppHost;
 
 namespace Microsoft.NET.HostModel.Bundle
 {
