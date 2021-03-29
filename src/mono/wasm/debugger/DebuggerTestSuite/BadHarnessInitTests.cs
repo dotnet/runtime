541ea2c15114f2dfdad575d040f2bb62d7b79f45
Ankit Jain <radical@gmail.com>
2021-03-29 02:06:25 -0400

    InvalidInitCmds hanging, disable for now

--- src/mono/wasm/debugger/DebuggerTestSuite/BadHarnessInitTests.cs
+++ src/mono/wasm/debugger/DebuggerTestSuite/BadHarnessInitTests.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#if false
 using System;
 using System.Collections.Generic;
 using System.Threading;
@@ -35,3 +36,4 @@ namespace DebuggerTests
         }
     }
 }
+#endif
