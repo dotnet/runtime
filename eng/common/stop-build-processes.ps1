77a0872701ba887348d3b930674a05493cbfd2cf
Radek Doulik <radekdoulik@gmail.com>
2021-03-31 00:09:57 +0200

    Rebuild

--- eng/common/stop-build-processes.ps1
+++ eng/common/stop-build-processes.ps1
@@ -1,4 +1,4 @@
-Write-Host 'Killing running build processes...'
+Write-Host 'Killing running build processes ...'
 foreach ($processName in @('msbuild', 'dotnet', 'vbcscompiler')) {
   Get-Process -Name $processName -ErrorAction SilentlyContinue | Stop-Process
 }
