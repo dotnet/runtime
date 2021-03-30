d2206193a1988295b64e9f3b0dfbbe5a5cba17bc
Radek Doulik <radekdoulik@gmail.com>
2021-03-30 23:12:38 +0200

    Try stop build processes to see if it helps
    with:
    
        error MSB3231: Unable to remove directory "D:\workspace\_work\1\s\artifacts\bin\WebAssembly.Browser.AOT.Test\net6.0-Release\browser-wasm\publish\". The process cannot access the file 'Microsoft.DotNet.XHarness.TestRunners.Xunit.dll' because it is being used by another process.

---
+++ eng/common/stop-build-processes.ps1
@@ -0,0 +1,4 @@
+Write-Host 'Killing running build processes...'
+foreach ($processName in @('msbuild', 'dotnet', 'vbcscompiler')) {
+  Get-Process -Name $processName -ErrorAction SilentlyContinue | Stop-Process
+}
