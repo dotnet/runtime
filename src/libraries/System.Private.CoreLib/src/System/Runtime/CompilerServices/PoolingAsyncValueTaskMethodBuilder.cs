2784aadb026463eb5b1189b912369f359a5008e3
Stephen Toub <stoub@microsoft.com>
2021-03-31 09:30:24 -0400

    Add AsyncMethodBuilderOverride and PoolingAsyncValueTaskMethodBuilders (#50116)
    * Add AsyncMethodBuilderOverride and PoolingAsyncValueTaskMethodBuilders
    
    * Revise based on C# LDM changes to model
    
    * Fix API compat errors

--- src/libraries/System.Private.CoreLib/src/System/Runtime/CompilerServices/PoolingAsyncValueTaskMethodBuilder.cs
+++ src/libraries/System.Private.CoreLib/src/System/Runtime/CompilerServices/PoolingAsyncValueTaskMethodBuilder.cs
@@ -1,38 +1,43 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Globalization;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
+using StateMachineBox = System.Runtime.CompilerServices.PoolingAsyncValueTaskMethodBuilder<System.Threading.Tasks.VoidTaskResult>.StateMachineBox;
+
 namespace System.Runtime.CompilerServices
 {
-    /// <summary>Represents a builder for asynchronous methods that returns a <see cref="ValueTask{TResult}"/>.</summary>
-    /// <typeparam name="TResult">The type of the result.</typeparam>
+    /// <summary>Represents a builder for asynchronous methods that return a <see cref="ValueTask"/>.</summary>
     [StructLayout(LayoutKind.Auto)]
-    public struct AsyncValueTaskMethodBuilder<TResult>
+    public struct PoolingAsyncValueTaskMethodBuilder
     {
+        /// <summary>Maximum number of boxes that are allowed to be cached per state machine type.</summary>
+        internal static readonly int s_valueTaskPoolingCacheSize =
+            int.TryParse(Environment.GetEnvironmentVariable("DOTNET_SYSTEM_THREADING_POOLINGASYNCVALUETASKSCACHESIZE"), NumberStyles.Integer, CultureInfo.InvariantCulture, out int result) && result > 0 ?
+                result :
+                Environment.ProcessorCount * 4; // arbitrary default value
+
         /// <summary>Sentinel object used to indicate that the builder completed synchronously and successfully.</summary>
+        private static readonly StateMachineBox s_syncSuccessSentinel = PoolingAsyncValueTaskMethodBuilder<VoidTaskResult>.s_syncSuccessSentinel;
+
+        /// <summary>The wrapped state machine box.</summary>
         /// <remarks>
-        /// To avoid memory safety issues even in the face of invalid race conditions, we ensure that the type of this object
-        /// is valid for the mode in which we're operating.  As such, it's cached on the generic builder per TResult
-        /// rather than having one sentinel instance for all types.
+        /// If the operation completed synchronously and successfully, this will be <see cref="s_syncSuccessSentinel"/>.
         /// </remarks>
-        internal static readonly Task<TResult> s_syncSuccessSentinel = new Task<TResult>(default(TResult)!);
+        private StateMachineBox? m_task; // Debugger depends on the exact name of this field.
 
-        /// <summary>The wrapped task.  If the operation completed synchronously and successfully, this will be a sentinel object compared by reference identity.</summary>
-        private Task<TResult>? m_task; // Debugger depends on the exact name of this field.
-        /// <summary>The result for this builder if it's completed synchronously, in which case <see cref="m_task"/> will be <see cref="s_syncSuccessSentinel"/>.</summary>
-        private TResult _result;
-
-        /// <summary>Creates an instance of the <see cref="AsyncValueTaskMethodBuilder{TResult}"/> struct.</summary>
+        /// <summary>Creates an instance of the <see cref="PoolingAsyncValueTaskMethodBuilder"/> struct.</summary>
         /// <returns>The initialized instance.</returns>
-        public static AsyncValueTaskMethodBuilder<TResult> Create() => default;
+        public static PoolingAsyncValueTaskMethodBuilder Create() => default;
 
         /// <summary>Begins running the builder with the associated state machine.</summary>
         /// <typeparam name="TStateMachine">The type of the state machine.</typeparam>
         /// <param name="stateMachine">The state machine instance, passed by reference.</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public void Start<TStateMachine>(ref TStateMachine stateMachine) where TStateMachine : IAsyncStateMachine =>
+        public void Start<TStateMachine>(ref TStateMachine stateMachine)
+            where TStateMachine : IAsyncStateMachine =>
             AsyncMethodBuilderCore.Start(ref stateMachine);
 
         /// <summary>Associates the builder with the specified state machine.</summary>
@@ -40,34 +45,32 @@ namespace System.Runtime.CompilerServices
         public void SetStateMachine(IAsyncStateMachine stateMachine) =>
             AsyncMethodBuilderCore.SetStateMachine(stateMachine, task: null);
 
-        /// <summary>Marks the value task as successfully completed.</summary>
-        /// <param name="result">The result to use to complete the value task.</param>
-        public void SetResult(TResult result)
+        /// <summary>Marks the task as successfully completed.</summary>
+        public void SetResult()
         {
             if (m_task is null)
             {
-                _result = result;
                 m_task = s_syncSuccessSentinel;
             }
             else
             {
-                AsyncTaskMethodBuilder<TResult>.SetExistingTaskResult(m_task, result);
+                m_task.SetResult(default);
             }
         }
 
-        /// <summary>Marks the value task as failed and binds the specified exception to the value task.</summary>
-        /// <param name="exception">The exception to bind to the value task.</param>
+        /// <summary>Marks the task as failed and binds the specified exception to the task.</summary>
+        /// <param name="exception">The exception to bind to the task.</param>
         public void SetException(Exception exception) =>
-            AsyncTaskMethodBuilder<TResult>.SetException(exception, ref m_task);
+            PoolingAsyncValueTaskMethodBuilder<VoidTaskResult>.SetException(exception, ref m_task);
 
-        /// <summary>Gets the value task for this builder.</summary>
-        public ValueTask<TResult> Task
+        /// <summary>Gets the task for this builder.</summary>
+        public ValueTask Task
         {
             get
             {
                 if (m_task == s_syncSuccessSentinel)
                 {
-                    return new ValueTask<TResult>(_result);
+                    return default;
                 }
 
                 // With normal access paterns, m_task should always be non-null here: the async method should have
@@ -75,34 +78,34 @@ namespace System.Runtime.CompilerServices
                 // or it should be completing asynchronously, in which case AwaitUnsafeOnCompleted would have similarly
                 // initialized m_task to a state machine object.  However, if the type is used manually (not via
                 // compiler-generated code) and accesses Task directly, we force it to be initialized.  Things will then
-                // "work" but in a degraded mode, as we don't know the TStateMachine type here, and thus we use a
-                // normal task object instead.
+                // "work" but in a degraded mode, as we don't know the TStateMachine type here, and thus we use a box around
+                // the interface instead.
 
-                Task<TResult>? task = m_task ??= new Task<TResult>(); // base task used rather than box to minimize size when used as manual promise
-                return new ValueTask<TResult>(task);
+                StateMachineBox? box = m_task ??= PoolingAsyncValueTaskMethodBuilder<VoidTaskResult>.CreateWeaklyTypedStateMachineBox();
+                return new ValueTask(box, box.Version);
             }
         }
 
         /// <summary>Schedules the state machine to proceed to the next action when the specified awaiter completes.</summary>
         /// <typeparam name="TAwaiter">The type of the awaiter.</typeparam>
         /// <typeparam name="TStateMachine">The type of the state machine.</typeparam>
-        /// <param name="awaiter">the awaiter</param>
+        /// <param name="awaiter">The awaiter.</param>
         /// <param name="stateMachine">The state machine.</param>
         public void AwaitOnCompleted<TAwaiter, TStateMachine>(ref TAwaiter awaiter, ref TStateMachine stateMachine)
             where TAwaiter : INotifyCompletion
             where TStateMachine : IAsyncStateMachine =>
-            AsyncTaskMethodBuilder<TResult>.AwaitOnCompleted(ref awaiter, ref stateMachine, ref m_task);
+            PoolingAsyncValueTaskMethodBuilder<VoidTaskResult>.AwaitOnCompleted(ref awaiter, ref stateMachine, ref m_task);
 
         /// <summary>Schedules the state machine to proceed to the next action when the specified awaiter completes.</summary>
         /// <typeparam name="TAwaiter">The type of the awaiter.</typeparam>
         /// <typeparam name="TStateMachine">The type of the state machine.</typeparam>
-        /// <param name="awaiter">the awaiter</param>
+        /// <param name="awaiter">The awaiter.</param>
         /// <param name="stateMachine">The state machine.</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void AwaitUnsafeOnCompleted<TAwaiter, TStateMachine>(ref TAwaiter awaiter, ref TStateMachine stateMachine)
             where TAwaiter : ICriticalNotifyCompletion
             where TStateMachine : IAsyncStateMachine =>
-            AsyncTaskMethodBuilder<TResult>.AwaitUnsafeOnCompleted(ref awaiter, ref stateMachine, ref m_task);
+            PoolingAsyncValueTaskMethodBuilder<VoidTaskResult>.AwaitUnsafeOnCompleted(ref awaiter, ref stateMachine, ref m_task);
 
         /// <summary>
         /// Gets an object that may be used to uniquely identify this builder to the debugger.
@@ -112,6 +115,7 @@ namespace System.Runtime.CompilerServices
         /// It must only be used by the debugger and tracing purposes, and only in a single-threaded manner
         /// when no other threads are in the middle of accessing this or other members that lazily initialize the box.
         /// </remarks>
-        internal object ObjectIdForDebugger => m_task ??= AsyncTaskMethodBuilder<TResult>.CreateWeaklyTypedStateMachineBox();
+        internal object ObjectIdForDebugger =>
+            m_task ??= PoolingAsyncValueTaskMethodBuilder<VoidTaskResult>.CreateWeaklyTypedStateMachineBox();
     }
 }
