a5f24ff0f40b8e7e0bfa7253b1aae2f52f0e93a6
Tlakaelel Axayakatl Ceja <tlakaelel.ceja@microsoft.com>
2021-03-31 20:38:10 -0700

    Merge SingleFileAnalyzer into RequiresAssemblyFilesAnalyzer and add capability to suppress IL3000/IL3001 with RequiresAssemblyFilesAnalyzer (dotnet/linker#1921)
    Merge SingleFileAnalyzer into RequiresAssemblyFilesAnalyzer
    Suppress warnings on dangerous patterns if RequiresAssemblyFilesAttribute is used on the parent call
    Fix case in which we have a dangerous method inside a property
    Add tests
    
    Commit migrated from https://github.com/dotnet/linker/commit/f4147aa53a425420f725c9cb5dbd36552df99372

--- src/tools/linker/src/ILLink.RoslynAnalyzer/RequiresAssemblyFilesAnalyzer.cs
+++ src/tools/linker/src/ILLink.RoslynAnalyzer/RequiresAssemblyFilesAnalyzer.cs
@@ -14,9 +14,34 @@ namespace ILLink.RoslynAnalyzer
 	[DiagnosticAnalyzer (LanguageNames.CSharp)]
 	public sealed class RequiresAssemblyFilesAnalyzer : DiagnosticAnalyzer
 	{
+		public const string IL3000 = nameof (IL3000);
+		public const string IL3001 = nameof (IL3001);
 		public const string IL3002 = nameof (IL3002);
+
 		internal const string RequiresAssemblyFilesAttribute = nameof (RequiresAssemblyFilesAttribute);
 		internal const string FullyQualifiedRequiresAssemblyFilesAttribute = "System.Diagnostics.CodeAnalysis." + RequiresAssemblyFilesAttribute;
+
+		static readonly DiagnosticDescriptor s_locationRule = new DiagnosticDescriptor (
+			IL3000,
+			new LocalizableResourceString (nameof (Resources.AvoidAssemblyLocationInSingleFileTitle),
+				Resources.ResourceManager, typeof (Resources)),
+			new LocalizableResourceString (nameof (Resources.AvoidAssemblyLocationInSingleFileMessage),
+				Resources.ResourceManager, typeof (Resources)),
+			DiagnosticCategory.SingleFile,
+			DiagnosticSeverity.Warning,
+			isEnabledByDefault: true,
+			helpLinkUri: "https://docs.microsoft.com/dotnet/fundamentals/code-analysis/quality-rules/il3000");
+
+		static readonly DiagnosticDescriptor s_getFilesRule = new DiagnosticDescriptor (
+			IL3001,
+			new LocalizableResourceString (nameof (Resources.AvoidAssemblyGetFilesInSingleFileTitle),
+				Resources.ResourceManager, typeof (Resources)),
+			new LocalizableResourceString (nameof (Resources.AvoidAssemblyGetFilesInSingleFileMessage),
+				Resources.ResourceManager, typeof (Resources)),
+			DiagnosticCategory.SingleFile,
+			DiagnosticSeverity.Warning,
+			isEnabledByDefault: true,
+			helpLinkUri: "https://docs.microsoft.com/dotnet/fundamentals/code-analysis/quality-rules/il3001");
 
 		static readonly DiagnosticDescriptor s_requiresAssemblyFilesRule = new DiagnosticDescriptor (
 			IL3002,
@@ -28,7 +53,7 @@ namespace ILLink.RoslynAnalyzer
 			DiagnosticSeverity.Warning,
 			isEnabledByDefault: true);
 
-		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create (s_requiresAssemblyFilesRule);
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create (s_locationRule, s_getFilesRule, s_requiresAssemblyFilesRule);
 
 		public override void Initialize (AnalysisContext context)
 		{
@@ -46,14 +71,33 @@ namespace ILLink.RoslynAnalyzer
 				if (string.Equals (includesAllContent?.Trim (), "true", StringComparison.OrdinalIgnoreCase))
 					return;
 
+				var dangerousPatternsBuilder = ImmutableArray.CreateBuilder<ISymbol> ();
+
+				var assemblyType = compilation.GetTypeByMetadataName ("System.Reflection.Assembly");
+				if (assemblyType != null) {
+					// properties
+					ImmutableArrayOperations.AddIfNotNull (dangerousPatternsBuilder, ImmutableArrayOperations.TryGetSingleSymbol<IPropertySymbol> (assemblyType.GetMembers ("Location")));
+
+					// methods
+					dangerousPatternsBuilder.AddRange (assemblyType.GetMembers ("GetFile").OfType<IMethodSymbol> ());
+					dangerousPatternsBuilder.AddRange (assemblyType.GetMembers ("GetFiles").OfType<IMethodSymbol> ());
+				}
+
+				var assemblyNameType = compilation.GetTypeByMetadataName ("System.Reflection.AssemblyName");
+				if (assemblyNameType != null) {
+					ImmutableArrayOperations.AddIfNotNull (dangerousPatternsBuilder, ImmutableArrayOperations.TryGetSingleSymbol<IPropertySymbol> (assemblyNameType.GetMembers ("CodeBase")));
+					ImmutableArrayOperations.AddIfNotNull (dangerousPatternsBuilder, ImmutableArrayOperations.TryGetSingleSymbol<IPropertySymbol> (assemblyNameType.GetMembers ("EscapedCodeBase")));
+				}
+				var dangerousPatterns = dangerousPatternsBuilder.ToImmutable ();
+
 				context.RegisterOperationAction (operationContext => {
 					var methodInvocation = (IInvocationOperation) operationContext.Operation;
-					CheckCalledMember (operationContext, methodInvocation.TargetMethod);
+					CheckCalledMember (operationContext, methodInvocation.TargetMethod, dangerousPatterns);
 				}, OperationKind.Invocation);
 
 				context.RegisterOperationAction (operationContext => {
 					var objectCreation = (IObjectCreationOperation) operationContext.Operation;
-					CheckCalledMember (operationContext, objectCreation.Constructor);
+					CheckCalledMember (operationContext, objectCreation.Constructor, dangerousPatterns);
 				}, OperationKind.ObjectCreation);
 
 				context.RegisterOperationAction (operationContext => {
@@ -61,26 +105,40 @@ namespace ILLink.RoslynAnalyzer
 					var prop = propAccess.Property;
 					var usageInfo = propAccess.GetValueUsageInfo (prop);
 					if (usageInfo.HasFlag (ValueUsageInfo.Read) && prop.GetMethod != null)
-						CheckCalledMember (operationContext, prop.GetMethod);
+						CheckCalledMember (operationContext, prop.GetMethod, dangerousPatterns);
 
 					if (usageInfo.HasFlag (ValueUsageInfo.Write) && prop.SetMethod != null)
-						CheckCalledMember (operationContext, prop.SetMethod);
+						CheckCalledMember (operationContext, prop.SetMethod, dangerousPatterns);
 
-					CheckCalledMember (operationContext, prop);
+					CheckCalledMember (operationContext, prop, dangerousPatterns);
 				}, OperationKind.PropertyReference);
 
 				context.RegisterOperationAction (operationContext => {
 					var eventRef = (IEventReferenceOperation) operationContext.Operation;
-					CheckCalledMember (operationContext, eventRef.Member);
+					CheckCalledMember (operationContext, eventRef.Member, dangerousPatterns);
 				}, OperationKind.EventReference);
 
 				static void CheckCalledMember (
 					OperationAnalysisContext operationContext,
-					ISymbol member)
+					ISymbol member,
+					ImmutableArray<ISymbol> dangerousPatterns)
 				{
 					// Do not emit any diagnostic if caller is annotated with the attribute too.
 					if (operationContext.ContainingSymbol.HasAttribute (RequiresAssemblyFilesAttribute))
 						return;
+					// In case ContainingSymbol is a property accesor check also for RequiresAssemblyFilesAttribute in the associated property
+					if (operationContext.ContainingSymbol is IMethodSymbol containingSymbol && (containingSymbol.MethodKind == MethodKind.PropertyGet || containingSymbol.MethodKind == MethodKind.PropertySet)
+						&& containingSymbol.AssociatedSymbol!.HasAttribute (RequiresAssemblyFilesAttribute)) {
+						return;
+					}
+
+					if (member is IMethodSymbol && ImmutableArrayOperations.Contains (dangerousPatterns, member, SymbolEqualityComparer.Default)) {
+						operationContext.ReportDiagnostic (Diagnostic.Create (s_getFilesRule, operationContext.Operation.Syntax.GetLocation (), member));
+						return;
+					} else if (member is IPropertySymbol && ImmutableArrayOperations.Contains (dangerousPatterns, member, SymbolEqualityComparer.Default)) {
+						operationContext.ReportDiagnostic (Diagnostic.Create (s_locationRule, operationContext.Operation.Syntax.GetLocation (), member));
+						return;
+					}
 
 					if (member.TryGetRequiresAssemblyFileAttribute (out AttributeData? requiresAssemblyFilesAttribute)) {
 						var message = requiresAssemblyFilesAttribute?.NamedArguments.FirstOrDefault (na => na.Key == "Message").Value.Value?.ToString ();
