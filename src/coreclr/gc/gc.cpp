bbfb25ae486b85f56ba6712a136122ad15988440
Andrew Au <andrewau@microsoft.com>
2021-04-02 15:17:18 -0700

    Make sure separated_poh_p and use_large_pages_p works together

--- src/coreclr/gc/gc.cpp
+++ src/coreclr/gc/gc.cpp
@@ -4964,6 +4964,25 @@ BOOL gc_heap::reserve_initial_memory (size_t normal_size, size_t large_size, siz
         }
     }
 
+    if (reserve_success && separated_poh_p)
+    {
+        for (int heap_no = 0; reserve_success && heap_no < num_heaps; heap_no++)
+        {
+            if (!GCToOSInterface::VirtualCommit(memory_details.initial_pinned_heap[heap_no].memory_base, pinned_size))
+            {
+                reserve_success = FALSE;
+            }
+        }
+        if (!reserve_success)
+        {
+            for (int heap_no = 0; reserve_success && heap_no < num_heaps; heap_no++)
+            {
+                virtual_free(memory_details.initial_normal_heap[heap_no].memory_base, normal_size);
+                virtual_free(memory_details.initial_large_heap[heap_no].memory_base, large_size);
+                virtual_free(memory_details.initial_pinned_heap[heap_no].memory_base, pinned_size);
+            }
+        }
+    }
 
     return reserve_success;
 }
@@ -6581,7 +6600,6 @@ bool gc_heap::virtual_decommit (void* address, size_t size, gc_oh_num oh, int h_
 
 void gc_heap::virtual_free (void* add, size_t allocated_size, heap_segment* sg)
 {
-    assert(!heap_hard_limit);
     bool release_succeeded_p = GCToOSInterface::VirtualRelease (add, allocated_size);
     if (release_succeeded_p)
     {
@@ -11732,13 +11750,17 @@ HRESULT gc_heap::initialize_gc (size_t soh_segment_size,
         return E_FAIL;
     }
 #else //USE_REGIONS
-    bool separated_poh_p = use_large_pages_p && 
-                           heap_hard_limit_oh[soh] && 
-                           (GCConfig::GetGCHeapHardLimitPOH() == 0) && 
-                           (GCConfig::GetGCHeapHardLimitPOHPercent() == 0);
+    bool empty_poh_p = heap_hard_limit_oh[soh] && 
+                       (GCConfig::GetGCHeapHardLimitPOH() == 0) && 
+                       (GCConfig::GetGCHeapHardLimitPOHPercent() == 0);
     if (!reserve_initial_memory (soh_segment_size, loh_segment_size, poh_segment_size, number_of_heaps, 
-                                 use_large_pages_p, separated_poh_p, heap_no_to_numa_node))
+                                 use_large_pages_p, empty_poh_p && use_large_pages_p, heap_no_to_numa_node))
         return E_OUTOFMEMORY;
+    if (empty_poh_p)
+    {
+        heap_hard_limit_oh[poh] = min_segment_size_hard_limit * number_of_heaps;
+        heap_hard_limit += heap_hard_limit_oh[poh];
+    }
 #endif //USE_REGIONS
 
 #ifdef CARD_BUNDLE
