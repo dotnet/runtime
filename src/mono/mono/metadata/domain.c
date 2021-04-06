52ea821b02fc8e3154bcb1d31f5d95336eb97e02
Ryan Lucia <rylucia@microsoft.com>
2021-04-06 09:16:55 -0400

    Clean up rebase

--- src/mono/mono/metadata/domain.c
+++ src/mono/mono/metadata/domain.c
@@ -547,8 +547,8 @@ mono_domain_unset (void)
 	SET_APPDOMAIN (NULL);
 }
 
-gboolean
-mono_domain_set_fast (MonoDomain *domain, gboolean force)
+void
+mono_domain_set_fast (MonoDomain *domain)
 {
 	MONO_REQ_GC_UNSAFE_MODE;
 
