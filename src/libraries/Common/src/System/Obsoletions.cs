4be8f9511aee4000023dfeb9d4477ba24fa8c6fc
Levi Broderick <levib@microsoft.com>
2021-04-07 13:51:53 -0700

    Obsolete SuppressIldasmAttribute

--- src/libraries/Common/src/System/Obsoletions.cs
+++ src/libraries/Common/src/System/Obsoletions.cs
@@ -51,5 +51,8 @@ namespace System
 
         internal const string DisablePrivateReflectionAttributeMessage = "DisablePrivateReflectionAttribute has no effect in .NET 6.0+ applications.";
         internal const string DisablePrivateReflectionAttributeDiagId = "SYSLIB0015";
+
+        internal const string SuppressIldasmAttributeMessage = "SuppressIldasmAttribute has no effect in .NET 6.0+ applications.";
+        internal const string SuppressIldasmAttributeDiagId = "SYSLIB0016";
     }
 }
