df6030485c7760485debc86fe7071484de54cd8f
Maxim Lipnin <v-maxlip@microsoft.com>
2021-04-08 12:28:27 +0300

    Mark System.Net.NetworkInformation methods throwing PNSE as unsupported on Android

--- src/libraries/System.Net.NetworkInformation/src/System/Net/NetworkInformation/LinuxIPGlobalProperties.cs
+++ src/libraries/System.Net.NetworkInformation/src/System/Net/NetworkInformation/LinuxIPGlobalProperties.cs
@@ -1,15 +1,19 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Runtime.Versioning;
+
 namespace System.Net.NetworkInformation
 {
     internal sealed class LinuxIPGlobalProperties : UnixIPGlobalProperties
     {
+        [UnsupportedOSPlatform("android")]
         public override TcpConnectionInformation[] GetActiveTcpConnections()
         {
             return StringParsingHelpers.ParseActiveTcpConnectionsFromFiles(NetworkFiles.Tcp4ConnectionsFile, NetworkFiles.Tcp6ConnectionsFile);
         }
 
+        [UnsupportedOSPlatform("android")]
         public override IPEndPoint[] GetActiveTcpListeners()
         {
             return StringParsingHelpers.ParseActiveTcpListenersFromFiles(NetworkFiles.Tcp4ConnectionsFile, NetworkFiles.Tcp6ConnectionsFile);
