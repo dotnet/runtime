57061f0f6176b13721b57240af96ea7880c0eb0e
Radek Doulik <radekdoulik@gmail.com>
2021-04-09 21:12:05 +0200

    Use spaces for indentation

--- eng/testing/WasmRunnerTemplate.cmd
+++ eng/testing/WasmRunnerTemplate.cmd
@@ -5,25 +5,25 @@ set EXECUTION_DIR=%~dp0
 set SCENARIO=%3
 
 if [%HELIX_WORKITEM_UPLOAD_ROOT%] == [] (
-	set XHARNESS_OUT="%EXECUTION_DIR%xharness-output"
+    set XHARNESS_OUT="%EXECUTION_DIR%xharness-output"
 ) else (
-	set XHARNESS_OUT="%HELIX_WORKITEM_UPLOAD_ROOT%xharness-output"
+    set XHARNESS_OUT="%HELIX_WORKITEM_UPLOAD_ROOT%xharness-output"
 )
 
 if [%XHARNESS_CLI_PATH%] NEQ [] (
-	:: When running in CI, we only have the .NET runtime available
-	:: We need to call the XHarness CLI DLL directly via dotnet exec
-	set HARNESS_RUNNER=dotnet.exe exec "%XHARNESS_CLI_PATH%"
+    :: When running in CI, we only have the .NET runtime available
+    :: We need to call the XHarness CLI DLL directly via dotnet exec
+    set HARNESS_RUNNER=dotnet.exe exec "%XHARNESS_CLI_PATH%"
 ) else (
-	set HARNESS_RUNNER=dotnet.exe xharness
+    set HARNESS_RUNNER=dotnet.exe xharness
 )
 
 if [%SCENARIO%]==[WasmTestOnBrowser] (
-	set XHARNESS_COMMAND=test-browser
+    set XHARNESS_COMMAND=test-browser
 ) else (
-	if [%XHARNESS_COMMAND%] == [] (
-	set XHARNESS_COMMAND=test
-	)
+    if [%XHARNESS_COMMAND%] == [] (
+    set XHARNESS_COMMAND=test
+    )
 )
 
 :: ========================= BEGIN Test Execution ============================= 
