c9d275b1893a16368752c93d5ed766960d04a101
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2021-04-09 16:33:03 +0100

    Update src/libraries/System.Linq/src/System/Linq/Distinct.cs
    Co-authored-by: Stephen Toub <stoub@microsoft.com>

--- src/libraries/System.Linq/src/System/Linq/Distinct.cs
+++ src/libraries/System.Linq/src/System/Linq/Distinct.cs
@@ -50,7 +50,6 @@ namespace System.Linq
                     {
                         yield return element;
                     }
-
                 }
                 while (enumerator.MoveNext());
             }
