8c1ed616b9dca720d22bb84a45207aed2f79a6eb
Levi Broderick <levib@microsoft.com>
2021-04-08 19:26:17 -0700

    Add extended async tests to TranscodingStream

--- src/libraries/System.Text.Encoding/tests/Encoding/TranscodingStreamTests.cs
+++ src/libraries/System.Text.Encoding/tests/Encoding/TranscodingStreamTests.cs
@@ -1,9 +1,11 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.IO.Pipelines;
 using System.IO.Tests;
 using System.Threading;
 using System.Threading.Tasks;
@@ -466,6 +468,56 @@ namespace System.Text.Tests
                 sink.Write(buffer.AsSpan(0..numBytesRead));
                 return numBytesRead;
             });
+        }
+
+        [Fact]
+        public async Task ReadAsync_LoopsWhenPartialDataReceived()
+        {
+            // Validates that the TranscodingStream will loop instead of returning 0
+            // if the inner stream read partial data and GetBytes cannot make forward progress.
+
+            using AsyncComms comms = new AsyncComms();
+            Stream transcodingStream = Encoding.CreateTranscodingStream(comms.ReadStream, Encoding.UTF8, Encoding.UTF8);
+
+            // First, ensure that writing [ C0 ] (always invalid UTF-8) to the stream
+            // causes the reader to return immediately with fallback behavior.
+
+            byte[] readBuffer = new byte[1024];
+            comms.WriteBytes(new byte[] { 0xC0 });
+
+            int numBytesRead = await transcodingStream.ReadAsync(readBuffer.AsMemory());
+            Assert.Equal(new byte[] { 0xEF, 0xBF, 0xBD }, readBuffer[0..numBytesRead]); // fallback substitution
+
+            // Next, ensure that writing [ C2 ] (partial UTF-8, needs more data) to the stream
+            // causes the reader to asynchronously loop, returning "not yet complete".
+
+            readBuffer = new byte[1024];
+            comms.WriteBytes(new byte[] { 0xC2 });
+
+            ValueTask<int> task = transcodingStream.ReadAsync(readBuffer.AsMemory());
+            Assert.False(task.IsCompleted);
+            comms.WriteBytes(new byte[] { 0x80 }); // [ C2 80 ] is valid UTF-8
+
+            numBytesRead = await task; // should complete successfully
+            Assert.Equal(new byte[] { 0xC2, 0x80 }, readBuffer[0..numBytesRead]);
+
+            // Finally, ensure that writing [ C2 ] (partial UTF-8, needs more data) to the stream
+            // followed by EOF causes the reader to perform substitution before returning EOF.
+
+            readBuffer = new byte[1024];
+            comms.WriteBytes(new byte[] { 0xC2 });
+
+            task = transcodingStream.ReadAsync(readBuffer.AsMemory());
+            Assert.False(task.IsCompleted);
+            comms.WriteEof();
+
+            numBytesRead = await task; // should complete successfully
+            Assert.Equal(new byte[] { 0xEF, 0xBF, 0xBD }, readBuffer[0..numBytesRead]); // fallback substitution
+
+            // Next call really should return "EOF reached"
+
+            readBuffer = new byte[1024];
+            Assert.Equal(0, await transcodingStream.ReadAsync(readBuffer.AsMemory()));
         }
 
         [Fact]
@@ -973,6 +1025,50 @@ namespace System.Text.Tests
             public override int GetMaxCharCount(int byteCount) => byteCount;
 
             public override byte[] GetPreamble() => Array.Empty<byte>();
+        }
+
+        // A helper type that allows synchronously writing to a stream while asynchronously
+        // reading from it.
+        private sealed class AsyncComms : IDisposable
+        {
+            private readonly BlockingCollection<byte[]> _blockingCollection;
+            private readonly PipeWriter _writer;
+
+            public AsyncComms()
+            {
+                _blockingCollection = new BlockingCollection<byte[]>();
+                var pipe = new Pipe();
+                ReadStream = pipe.Reader.AsStream();
+                _writer = pipe.Writer;
+                Task.Run(_DrainWorker);
+            }
+
+            public Stream ReadStream { get; }
+
+            public void Dispose()
+            {
+                _blockingCollection.Dispose();
+            }
+
+            public void WriteBytes(ReadOnlySpan<byte> bytes)
+            {
+                _blockingCollection.Add(bytes.ToArray());
+            }
+
+            public void WriteEof()
+            {
+                _blockingCollection.Add(null);
+            }
+
+            private async Task _DrainWorker()
+            {
+                byte[] buffer;
+                while ((buffer = _blockingCollection.Take()) is not null)
+                {
+                    await _writer.WriteAsync(buffer);
+                }
+                _writer.Complete();
+            }
         }
     }
 }
