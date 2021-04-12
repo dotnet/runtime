911d385a0096631a44f0ac8e9ba0b6d7ac72022c
Filip Navara <navara@emclient.com>
2021-04-12 21:12:22 +0200

    Ensure NSThread is in multi-threaded mode before using autorelease pool

--- src/libraries/Native/Unix/System.Native/pal_iossupportversion.m
+++ src/libraries/Native/Unix/System.Native/pal_iossupportversion.m
@@ -2,10 +2,13 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 #include "pal_iossupportversion.h"
+#include "pal_autoreleasepool.h"
 #import <Foundation/Foundation.h>
 
 const char* SystemNative_iOSSupportVersion()
 {
+    EnsureNSThreadIsMultiThreaded();
+
     @autoreleasepool
     {
         NSDictionary *plist = [NSDictionary dictionaryWithContentsOfFile:@"/System/Library/CoreServices/SystemVersion.plist"];
