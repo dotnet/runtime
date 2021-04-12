81e867ea5dcf698471d664e797a83d03e467842a
Alexander Köplinger <alex.koeplinger@outlook.com>
2021-04-12 20:13:39 +0200

    Fix minimum iOS/tvOS version and add iOS 14 to RID graph (#50167)
    As specified in https://github.com/dotnet/runtime/issues/44654 the minimum iOS/tvOS version we're targeting for .NET 6 is iOS 10.
    Bump the versions used in our build scripts and RID graph to that and also add iOS 14.

--- src/mono/mono/utils/mono-logger.c
+++ src/mono/mono/utils/mono-logger.c
@@ -137,10 +137,10 @@ mono_trace_set_logdest_string (const char *dest)
 	logger.writer = mono_log_write_logcat;
 	logger.closer = mono_log_close_logcat;
 	logger.dest   = (char*) dest;
-#elif defined (HOST_IOS)
-	logger.opener = mono_log_open_asl;
-	logger.writer = mono_log_write_asl;
-	logger.closer = mono_log_close_asl;
+#elif defined(HOST_IOS) || defined(HOST_TVOS) || defined(HOST_WATCHOS) || defined(HOST_MACCAT)
+	logger.opener = mono_log_open_os_log;
+	logger.writer = mono_log_write_os_log;
+	logger.closer = mono_log_close_os_log;
 	logger.dest   = (char*) dest;
 #else
 	if (dest && !strcmp("flight-recorder", dest)) {
