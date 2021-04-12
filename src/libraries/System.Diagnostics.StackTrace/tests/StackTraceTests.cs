8c8532686238292637ccc0ddefce79cecb91a748
Woongsuk Cho <ws77.cho@samsung.com>
2021-04-12 14:29:48 +0900

    Skipt test in mono interpreter mode.
    Related issue : https://github.com/dotnet/runtime/issues/51096

--- src/libraries/System.Diagnostics.StackTrace/tests/StackTraceTests.cs
+++ src/libraries/System.Diagnostics.StackTrace/tests/StackTraceTests.cs
@@ -305,6 +305,7 @@ namespace System.Diagnostics.Tests
         }
 
         [ConditionalFact(typeof(RemoteExecutor), nameof(RemoteExecutor.IsSupported))]
+        [ActiveIssue("https://github.com/dotnet/runtime/issues/51096", typeof(PlatformDetection), nameof(PlatformDetection.IsMonoInterpreter))]
         public void ToString_ShowILOffset()
         {
             string AssemblyName = "ExceptionTestAssembly.dll";
