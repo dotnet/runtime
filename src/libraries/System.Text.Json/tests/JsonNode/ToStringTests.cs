f661e1bfd4fd771f7bf49375b727fbb7c191d51b
Steve Harter <steveharter@users.noreply.github.com>
2021-04-12 14:29:28 -0500

    Fix test failure on non-windows

--- src/libraries/System.Text.Json/tests/JsonNode/ToStringTests.cs
+++ src/libraries/System.Text.Json/tests/JsonNode/ToStringTests.cs
@@ -7,14 +7,13 @@ namespace System.Text.Json.Node.Tests
 {
     public static class ToStringTests
     {
-        internal const string JsonWithWhitespace = "{\r\n  \"MyString\": \"Hello!\",\r\n  \"MyNull\": null,\r\n  \"MyBoolean\": false,\r\n  \"MyArray\": [\r\n    2,\r\n    3,\r\n    42\r\n  ],\r\n  \"MyInt\": 43,\r\n  \"MyDateTime\": \"2020-07-08T00:00:00\",\r\n  \"MyGuid\": \"ed957609-cdfe-412f-88c1-02daca1b4f51\",\r\n  \"MyObject\": {\r\n    \"MyString\": \"Hello!!\"\r\n  },\r\n  \"Child\": {\r\n    \"ChildProp\": 1\r\n  }\r\n}";
-
         [Fact]
         public static void NodeToString()
         {
+            string Expected = $"{{{Environment.NewLine}  \"MyString\": \"Hello!\",{Environment.NewLine}  \"MyNull\": null,{Environment.NewLine}  \"MyBoolean\": false,{Environment.NewLine}  \"MyArray\": [{Environment.NewLine}    2,{Environment.NewLine}    3,{Environment.NewLine}    42{Environment.NewLine}  ],{Environment.NewLine}  \"MyInt\": 43,{Environment.NewLine}  \"MyDateTime\": \"2020-07-08T00:00:00\",{Environment.NewLine}  \"MyGuid\": \"ed957609-cdfe-412f-88c1-02daca1b4f51\",{Environment.NewLine}  \"MyObject\": {{{Environment.NewLine}    \"MyString\": \"Hello!!\"{Environment.NewLine}  }},{Environment.NewLine}  \"Child\": {{{Environment.NewLine}    \"ChildProp\": 1{Environment.NewLine}  }}{Environment.NewLine}}}";
             JsonNode node = JsonNode.Parse(JsonNodeTests.ExpectedDomJson);
             string json = node.ToString();
-            Assert.Equal(JsonWithWhitespace, json);
+            Assert.Equal(Expected, json);
         }
 
         [Fact]
