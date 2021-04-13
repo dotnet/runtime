2d28900c34c6a0ed582fdf7cc0b285222ce3e9d4
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2021-04-13 20:33:38 +0200

    resolve https://github.com/dotnet/runtime/pull/46740#discussion_r608241398

--- src/libraries/Microsoft.Extensions.Options.ConfigurationExtensions/tests/FakeConfigurationProvider.cs
+++ src/libraries/Microsoft.Extensions.Options.ConfigurationExtensions/tests/FakeConfigurationProvider.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.Memory;
 
 namespace Microsoft.Extensions.Options.ConfigurationExtensions.Tests
