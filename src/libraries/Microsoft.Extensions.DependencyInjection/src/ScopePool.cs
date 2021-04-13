99d57f341925bb98ecaed81a78ffd302de50e7d1
David Fowler <davidfowl@gmail.com>
2021-04-13 10:08:01 -0700

    Made some adjustments based on PR feedback - Cache 100 scoped entries maximum - Keep track of the maximum capacity up to a limit

--- src/libraries/Microsoft.Extensions.DependencyInjection/src/ScopePool.cs
+++ src/libraries/Microsoft.Extensions.DependencyInjection/src/ScopePool.cs
@@ -14,27 +14,33 @@ namespace Microsoft.Extensions.DependencyInjection
         // Stash the scope state on a thread static
         [ThreadStatic]
         private static State t_tlsScopeState;
+        private volatile int _maxScopeSize;
 
-        // More than 1000 scopes services seems like too many to cache (~36KB in size)
-        private const int MaxResolvedServicesCount = 1000;
+        // Roughly ~3.6KB
+        private const int MaxResolvedServicesCount = 100;
+
+        // Below default ~85K LOH threshold, avoid large temporary garbage
+        private const int MaxCapacity = 2000;
 
         public State Rent()
         {
-            State state = null;
-
             // Try to get a scope from TLS
-            if (t_tlsScopeState != null)
+            State state = t_tlsScopeState;
+
+            if (state != null)
             {
-                state = t_tlsScopeState;
+                // Clear the state
                 t_tlsScopeState = null;
             }
 
-            return state ?? new State(this);
+            return state ?? new State(this, _maxScopeSize);
         }
 
         public bool Return(State state)
         {
-            if (state.Size <= MaxResolvedServicesCount)
+            var size = state.Size;
+
+            if (size <= MaxResolvedServicesCount)
             {
                 // Stash the state back in TLS
                 state.Clear();
@@ -42,6 +48,9 @@ namespace Microsoft.Extensions.DependencyInjection
 
                 return true;
             }
+
+            // Clamp the size between the min and max
+            _maxScopeSize = Math.Min(MaxCapacity, Math.Max(size, _maxScopeSize));
 
             return false;
         }
@@ -55,12 +64,12 @@ namespace Microsoft.Extensions.DependencyInjection
 
             public int Size => ((Dictionary<ServiceCacheKey, object>)ResolvedServices).Count;
 
-            public State(ScopePool pool = null)
+            public State(ScopePool pool = null, int initialCapacity = 0)
             {
                 _pool = pool;
                 // When pool is null, we're in the global scope which doesn't need pooling.
                 // To reduce lock contention for singletons upon resolve we use a concurrent dictionary.
-                ResolvedServices = pool == null ? new ConcurrentDictionary<ServiceCacheKey, object>() : new Dictionary<ServiceCacheKey, object>();
+                ResolvedServices = pool == null ? new ConcurrentDictionary<ServiceCacheKey, object>() : new Dictionary<ServiceCacheKey, object>(initialCapacity);
             }
 
             internal void Clear()
