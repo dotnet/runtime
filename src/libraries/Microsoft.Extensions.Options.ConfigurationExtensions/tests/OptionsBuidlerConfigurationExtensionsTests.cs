2f9c03901d41e1b94aef983af6d6c9c52525337a
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2021-04-13 20:53:10 +0200

    Add updating test for non-empty configuration section
    resolves https://github.com/dotnet/runtime/pull/46740#discussion_r608245088

--- src/libraries/Microsoft.Extensions.Options.ConfigurationExtensions/tests/OptionsBuidlerConfigurationExtensionsTests.cs
+++ src/libraries/Microsoft.Extensions.Options.ConfigurationExtensions/tests/OptionsBuidlerConfigurationExtensionsTests.cs
@@ -107,7 +107,7 @@ namespace Microsoft.Extensions.Options.ConfigurationExtensions.Tests
         }
 
         [Fact]
-        public static void BindConfiguration_UpdatesOptionOnConfigurationUpdate()
+        public static void BindConfiguration_UpdatesOptionOnConfigurationUpdateWithEmptySectionName()
         {
             const string messageValue1 = "This is a test";
             const string messageValue2 = "This is the message after update";
@@ -136,6 +136,43 @@ namespace Microsoft.Extensions.Options.ConfigurationExtensions.Tests
             FakeOptions optionsValue1 = optionsMonitor.CurrentValue;
             Assert.Equal(messageValue1, optionsValue1.Message);
             configSource.Provider.Set(nameof(FakeOptions.Message), messageValue2);
+            FakeOptions optionsValue2 = optionsMonitor.CurrentValue;
+            Assert.True(updateHasRun);
+            Assert.Equal(messageValue2, optionsValue2.Message);
+        }
+
+        [Fact]
+        public static void BindConfiguration_UpdatesOptionOnConfigurationUpdate()
+        {
+            const string configSectionName = "Test";
+            string messageConfigKey = ConfigurationPath.Combine(configSectionName, nameof(FakeOptions.Message));
+            const string messageValue1 = "This is a test";
+            const string messageValue2 = "This is the message after update";
+
+            FakeConfigurationSource configSource = new()
+            {
+                InitialData = new Dictionary<string, string>
+                {
+                    [messageConfigKey] = messageValue1
+                }
+            };
+
+            var services = new ServiceCollection();
+            services.AddSingleton<IConfiguration>(new ConfigurationBuilder()
+                    .Add(configSource)
+                    .Build());
+            OptionsBuilder<FakeOptions> optionsBuilder = services.AddOptions<FakeOptions>();
+            _ = optionsBuilder.BindConfiguration(configSectionName);
+            using ServiceProvider serviceProvider = services.BuildServiceProvider();
+            var optionsMonitor = serviceProvider.GetRequiredService<IOptionsMonitor<FakeOptions>>();
+            bool updateHasRun = false;
+            optionsMonitor.OnChange((opts, name) =>
+            {
+                updateHasRun = true;
+            });
+            FakeOptions optionsValue1 = optionsMonitor.CurrentValue;
+            Assert.Equal(messageValue1, optionsValue1.Message);
+            configSource.Provider.Set(messageConfigKey, messageValue2);
             FakeOptions optionsValue2 = optionsMonitor.CurrentValue;
             Assert.True(updateHasRun);
             Assert.Equal(messageValue2, optionsValue2.Message);
