2d41c6dcf603ea977971d9f26868471314576f48
Maryam Ariyan <maryam.ariyan@microsoft.com>
2021-04-14 10:26:20 -0700

    - Fix new line issue when comparing baselines

--- src/libraries/Microsoft.Extensions.Logging/tests/Microsoft.Extensions.Logging.Generators.Tests/LoggerMessageGeneratorEmitterTests.cs
+++ src/libraries/Microsoft.Extensions.Logging/tests/Microsoft.Extensions.Logging.Generators.Tests/LoggerMessageGeneratorEmitterTests.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
 using System.IO;
 using System.Reflection;
 using System.Threading.Tasks;
@@ -45,16 +46,7 @@ namespace Microsoft.Extensions.Logging.Generators.Tests.TestClasses
         public static partial void M0(ILogger logger, int a1);
     }
 }";
-            string expectedResult = await File.ReadAllTextAsync(Path.Combine("Baselines", "TestWithOneParam.generated.txt")).ConfigureAwait(false);
-
-            var (d, r) = await RoslynTestUtils.RunGenerator(
-                new LoggerMessageGenerator(),
-                new[] { typeof(ILogger).Assembly, typeof(LoggerMessageAttribute).Assembly },
-                new[] { testSourceCode }).ConfigureAwait(false);
-
-            Assert.Empty(d);
-            Assert.Single(r);
-            Assert.Equal(expectedResult, r[0].SourceText.ToString());
+            await VerifyAgainstBaselineUsingFile("TestWithOneParam.generated.txt", testSourceCode);
         }
 
         [Fact]
@@ -70,16 +62,7 @@ namespace Microsoft.Extensions.Logging.Generators.Tests.TestClasses
         public static partial void Method9(ILogger logger, int p1, int p2, int p3, int p4, int p5, int p6, int p7);
     }
 }";
-            string expectedResult = await File.ReadAllTextAsync(Path.Combine("Baselines", "TestWithMoreThan6Params.generated.txt")).ConfigureAwait(false);
-
-            var (d, r) = await RoslynTestUtils.RunGenerator(
-                new LoggerMessageGenerator(),
-                new[] { typeof(ILogger).Assembly, typeof(LoggerMessageAttribute).Assembly },
-                new[] { testSourceCode }).ConfigureAwait(false);
-
-            Assert.Empty(d);
-            Assert.Single(r);
-            Assert.Equal(expectedResult, r[0].SourceText.ToString());
+            await VerifyAgainstBaselineUsingFile("TestWithMoreThan6Params.generated.txt", testSourceCode);
         }
 
         [Fact]
@@ -94,7 +77,12 @@ namespace Microsoft.Extensions.Logging.Generators.Tests.TestClasses
         public static partial void M9(LogLevel level, ILogger logger);
     }
 }";
-            string expectedResult = await File.ReadAllTextAsync(Path.Combine("Baselines", "TestWithDynamicLogLevel.generated.txt")).ConfigureAwait(false);
+            await VerifyAgainstBaselineUsingFile("TestWithDynamicLogLevel.generated.txt", testSourceCode);
+        }
+
+        private async Task VerifyAgainstBaselineUsingFile(string filename, string testSourceCode)
+        {
+            string[] expectedLines = await File.ReadAllLinesAsync(Path.Combine("Baselines", filename)).ConfigureAwait(false);
 
             var (d, r) = await RoslynTestUtils.RunGenerator(
                 new LoggerMessageGenerator(),
@@ -103,7 +91,33 @@ namespace Microsoft.Extensions.Logging.Generators.Tests.TestClasses
 
             Assert.Empty(d);
             Assert.Single(r);
-            Assert.Equal(expectedResult, r[0].SourceText.ToString());
+
+            Assert.True(CompareLines(expectedLines, r[0].SourceText,
+                out string errorMessage), errorMessage);
+        }
+
+        private bool CompareLines(string[] expectedLines, SourceText sourceText, out string message)
+        {
+            if (expectedLines.Length != sourceText.Lines.Count)
+            {
+                message = string.Format("Line numbers do not match. Expected: {0} lines, but generated {1}",
+                    expectedLines.Length, sourceText.Lines.Count);
+                return false;
+            }
+            int index = 0;
+            foreach (TextLine textLine in sourceText.Lines)
+            {
+                string expectedLine = expectedLines[index];
+                if (!expectedLine.Equals(textLine.ToString(), StringComparison.Ordinal))
+                {
+                    message = string.Format("Line {0} does not match.{1}Expected Line:{1}{2}{1}Actual Line:{1}{3}",
+                        textLine.LineNumber, Environment.NewLine, expectedLine, textLine);
+                    return false;
+                }
+                index++;
+            }
+            message = string.Empty;
+            return true;
         }
     }
 }
