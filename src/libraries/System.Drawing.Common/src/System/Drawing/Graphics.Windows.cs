ddbf284c14dd44bf304543cfe675d42ff417b00f
Jeremy Kuhne <jkuhne@microsoft.com>
2021-04-15 14:04:19 -0700

    Conditionalize the obsoletion in the src

--- src/libraries/System.Drawing.Common/src/System/Drawing/Graphics.Windows.cs
+++ src/libraries/System.Drawing.Common/src/System/Drawing/Graphics.Windows.cs
@@ -684,7 +684,9 @@ namespace System.Drawing
         /// WARNING: This method is for internal FX support only.
         /// </summary>
         [EditorBrowsable(EditorBrowsableState.Never)]
+#if NET50_OBSOLETIONS
         [Obsolete(Obsoletions.GetContextInfoMessage, DiagnosticId = Obsoletions.GetContextInfoDiagId, UrlFormat = Obsoletions.SharedUrlFormat)]
+#endif
         [SupportedOSPlatform("windows")]
         public object GetContextInfo()
         {
