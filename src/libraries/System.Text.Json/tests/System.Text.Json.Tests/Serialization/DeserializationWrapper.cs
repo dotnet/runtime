cd35514b600466314940c29b02d59bbc446e4341
Layomi Akinrinade <layomia@gmail.com>
2021-04-18 22:24:37 -0700

    Add new methods to JsonSerializer and System.Net.Http.Json APIs that take type metadata

--- src/libraries/System.Text.Json/tests/System.Text.Json.Tests/Serialization/DeserializationWrapper.cs
+++ src/libraries/System.Text.Json/tests/System.Text.Json.Tests/Serialization/DeserializationWrapper.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.IO;
+using System.Text.Json.Serialization.Metadata;
 using System.Threading.Tasks;
 
 namespace System.Text.Json.Serialization.Tests
@@ -15,11 +16,17 @@ namespace System.Text.Json.Serialization.Tests
 
         public static DeserializationWrapper StringDeserializer => new StringDeserializerWrapper();
         public static DeserializationWrapper StreamDeserializer => new StreamDeserializerWrapper();
-        public static DeserializationWrapper SpanDeserializer => new SpanDesearializationWrapper();
+        public static DeserializationWrapper SpanDeserializer => new SpanDeserializerWrapper();
+
+        public static DeserializationWrapper ReaderDeserializer => new ReaderDeserializerWrapper();
 
         protected internal abstract Task<T> DeserializeWrapper<T>(string json, JsonSerializerOptions options = null);
 
         protected internal abstract Task<object> DeserializeWrapper(string json, Type type, JsonSerializerOptions options = null);
+
+        protected internal abstract Task<T> DeserializeWrapper<T>(string json, JsonTypeInfo<T> jsonTypeInfo);
+
+        protected internal abstract Task<object> DeserializeWrapper(string json, Type type, JsonSerializerContext context);
 
         private class StringDeserializerWrapper : DeserializationWrapper
         {
@@ -32,38 +39,54 @@ namespace System.Text.Json.Serialization.Tests
             {
                 return Task.FromResult(JsonSerializer.Deserialize(json, type, options));
             }
+
+            protected internal override Task<T> DeserializeWrapper<T>(string json, JsonTypeInfo<T> jsonTypeInfo)
+            {
+                return Task.FromResult(JsonSerializer.Deserialize(json, jsonTypeInfo));
+            }
+
+            protected internal override Task<object> DeserializeWrapper(string json, Type type, JsonSerializerContext context)
+            {
+                return Task.FromResult(JsonSerializer.Deserialize(json, type, context));
+            }
         }
 
         private class StreamDeserializerWrapper : DeserializationWrapper
         {
             protected internal override async Task<T> DeserializeWrapper<T>(string json, JsonSerializerOptions options = null)
             {
-                if (options == null)
-                {
-                    options = _optionsWithSmallBuffer;
-                }
-
                 using (MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(json)))
                 {
-                    return await JsonSerializer.DeserializeAsync<T>(stream, options);
+                    return await JsonSerializer.DeserializeAsync<T>(stream, options ?? _optionsWithSmallBuffer);
                 }
             }
 
             protected internal override async Task<object> DeserializeWrapper(string json, Type type, JsonSerializerOptions options = null)
             {
-                if (options == null)
-                {
-                    options = _optionsWithSmallBuffer;
-                }
-
                 using (MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(json)))
                 {
-                    return await JsonSerializer.DeserializeAsync(stream, type, options);
+                    return await JsonSerializer.DeserializeAsync(stream, type, options ?? _optionsWithSmallBuffer);
+                }
+            }
+
+            protected internal override async Task<T> DeserializeWrapper<T>(string json, JsonTypeInfo<T> jsonTypeInfo)
+            {
+                using (MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(json)))
+                {
+                    return await JsonSerializer.DeserializeAsync(stream, jsonTypeInfo);
+                }
+            }
+
+            protected internal override async Task<object> DeserializeWrapper(string json, Type type, JsonSerializerContext context)
+            {
+                using (MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(json)))
+                {
+                    return await JsonSerializer.DeserializeAsync(stream, type, context);
                 }
             }
         }
 
-        private class SpanDesearializationWrapper : DeserializationWrapper
+        private class SpanDeserializerWrapper : DeserializationWrapper
         {
             protected internal override Task<T> DeserializeWrapper<T>(string json, JsonSerializerOptions options = null)
             {
@@ -73,6 +96,43 @@ namespace System.Text.Json.Serialization.Tests
             protected internal override Task<object> DeserializeWrapper(string json, Type type, JsonSerializerOptions options = null)
             {
                 return Task.FromResult(JsonSerializer.Deserialize(json.AsSpan(), type, options));
+            }
+
+            protected internal override Task<T> DeserializeWrapper<T>(string json, JsonTypeInfo<T> jsonTypeInfo)
+            {
+                return Task.FromResult(JsonSerializer.Deserialize(json.AsSpan(), jsonTypeInfo));
+            }
+
+            protected internal override Task<object> DeserializeWrapper(string json, Type type, JsonSerializerContext context)
+            {
+                return Task.FromResult(JsonSerializer.Deserialize(json.AsSpan(), type, context));
+            }
+        }
+
+        private class ReaderDeserializerWrapper : DeserializationWrapper
+        {
+            protected internal override Task<T> DeserializeWrapper<T>(string json, JsonSerializerOptions options = null)
+            {
+                Utf8JsonReader reader = new(Encoding.UTF8.GetBytes(json));
+                return Task.FromResult(JsonSerializer.Deserialize<T>(ref reader, options));
+            }
+
+            protected internal override Task<object> DeserializeWrapper(string json, Type type, JsonSerializerOptions options = null)
+            {
+                Utf8JsonReader reader = new(Encoding.UTF8.GetBytes(json));
+                return Task.FromResult(JsonSerializer.Deserialize(ref reader, type, options));
+            }
+
+            protected internal override Task<T> DeserializeWrapper<T>(string json, JsonTypeInfo<T> jsonTypeInfo)
+            {
+                Utf8JsonReader reader = new(Encoding.UTF8.GetBytes(json));
+                return Task.FromResult(JsonSerializer.Deserialize(ref reader, jsonTypeInfo));
+            }
+
+            protected internal override Task<object> DeserializeWrapper(string json, Type type, JsonSerializerContext context)
+            {
+                Utf8JsonReader reader = new(Encoding.UTF8.GetBytes(json));
+                return Task.FromResult(JsonSerializer.Deserialize(ref reader, type, context));
             }
         }
     }
