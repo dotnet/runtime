49f18b60e8cf8531b62a1e1528826eab56a06d02
Stephen Toub <stoub@microsoft.com>
2021-04-21 14:04:26 -0400

    Fix thread-safety of TypeDescriptor.Refresh enumerating hashtable (#51621)

--- src/libraries/System.ComponentModel.TypeConverter/src/System/ComponentModel/ReflectTypeDescriptionProvider.cs
+++ src/libraries/System.ComponentModel.TypeConverter/src/System/ComponentModel/ReflectTypeDescriptionProvider.cs
@@ -838,17 +838,22 @@ namespace System.ComponentModel
         {
             List<Type> typeList = new List<Type>();
 
-            // Manual use of IDictionaryEnumerator instead of foreach to avoid DictionaryEntry box allocations.
-            IDictionaryEnumerator e = _typeData.GetEnumerator();
-            while (e.MoveNext())
+            lock (s_internalSyncObject)
             {
-                DictionaryEntry de = e.Entry;
-                Type type = (Type)de.Key;
-                ReflectedTypeData typeData = (ReflectedTypeData)de.Value;
-
-                if (type.Module == module && typeData.IsPopulated)
+                Hashtable typeData = _typeData;
+                if (typeData != null)
                 {
-                    typeList.Add(type);
+                    // Manual use of IDictionaryEnumerator instead of foreach to avoid DictionaryEntry box allocations.
+                    IDictionaryEnumerator e = typeData.GetEnumerator();
+                    while (e.MoveNext())
+                    {
+                        DictionaryEntry de = e.Entry;
+                        Type type = (Type)de.Key;
+                        if (type.Module == module && ((ReflectedTypeData)de.Value).IsPopulated)
+                        {
+                            typeList.Add(type);
+                        }
+                    }
                 }
             }
 
