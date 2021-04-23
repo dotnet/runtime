da0ac72bdeb5ef8d1914c369c2d4a011714ced95
Coderrob <Rob@Coderrob.com>
2021-04-23 17:22:56 -0500

    Remove ordinal check to avoid system namespace use

--- src/libraries/Microsoft.Extensions.Options.DataAnnotations/src/DataAnnotationValidateOptions.cs
+++ src/libraries/Microsoft.Extensions.Options.DataAnnotations/src/DataAnnotationValidateOptions.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
@@ -36,7 +35,7 @@ namespace Microsoft.Extensions.Options
         public ValidateOptionsResult Validate(string name, TOptions options)
         {
             // Null name is used to configure all named options.
-            if (Name != null && !string.Equals(name, Name, StringComparison.OrdinalIgnoreCase))
+            if (Name != null && !string.Equals(name, Name))
             {
                 // Ignored if not validating this instance.
                 return ValidateOptionsResult.Skip;
