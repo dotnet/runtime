a379bdc071833bdb49f9cc081002b147cc434eb3
lateralusX <lateralusx.github@gmail.com>
2021-04-23 13:58:14 +0200

    Specify that last start is a timestamp and not a time.

--- src/mono/mono/sgen/sgen-gc.c
+++ src/mono/mono/sgen/sgen-gc.c
@@ -302,7 +302,7 @@ static guint64 time_major_fragment_creation = 0;
 static guint64 time_max = 0;
 static guint64 time_last = 0;
 static guint64 time_since_last = 0;
-static guint64 last_start = 0;
+static guint64 timestamp_last_start = 0;
 
 static int sgen_max_pause_time = SGEN_DEFAULT_MAX_PAUSE_TIME;
 static float sgen_max_pause_margin = SGEN_DEFAULT_MAX_PAUSE_MARGIN;
@@ -2706,12 +2706,12 @@ sgen_perform_collection_inner (size_t requested_size, int generation_to_collect,
 
 	// Long running timers start/stop on different threads might not be correct an all platforms
 	// using mono_100ns_ticks, use mono_100ns_datetime for long running timers.
-	gint64 last_end = mono_100ns_datetime ();
+	gint64 timestamp_last_end = mono_100ns_datetime ();
 
 	// Elapsed time since ending last GC.
-	time_since_last = TV_ELAPSED (last_start, last_end);
+	time_since_last = TV_ELAPSED (timestamp_last_start, timestamp_last_end);
 
-	last_start = last_end;
+	timestamp_last_start = timestamp_last_end;
 
 	time_max = MAX (time_max, time_last);
 
