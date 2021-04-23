6ceadf8d0c756007264b31697d52ba3fccfeff45
Katelyn Gadd <kg@luminance.org>
2021-04-22 17:48:02 -0700

    Fix conditional error

--- src/mono/mono/metadata/monitor.c
+++ src/mono/mono/metadata/monitor.c
@@ -1436,10 +1436,8 @@ mono_set_string_interned_internal (MonoObject* obj)
 {
 	LockWord tmp;
 	tmp.sync = obj->synchronisation;
-	if (lock_word_is_inflated (tmp))
-		return;
-	
-	mono_monitor_inflate (obj);
+	if (!lock_word_is_inflated (tmp))
+		mono_monitor_inflate (obj);
 	obj->synchronisation->is_interned = 1;
 }
 
