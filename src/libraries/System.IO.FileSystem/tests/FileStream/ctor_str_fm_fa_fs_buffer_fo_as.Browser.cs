6875a9fc1619a82ecef4442454ed7731a27ed93d
Adam Sitnik <adam.sitnik@gmail.com>
2021-04-23 13:40:59 +0200

    try to enable the new tests for Browser build

---
+++ src/libraries/System.IO.FileSystem/tests/FileStream/ctor_str_fm_fa_fs_buffer_fo_as.Browser.cs
@@ -0,0 +1,16 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.IO.Tests
+{
+    public partial class FileStream_ctor_str_fm_fa_fs_buffer_fo_as : FileStream_ctor_str_fm_fa_fs_buffer_fo_as_base
+    {
+        protected override long AllocationSize => 10;
+
+        protected override long InitialLength => 10;
+
+        private long GetExpectedFileLength(long allocationSize) => allocationSize;
+
+        private long GetActualAllocationSize(FileStream fileStream) => fileStream.Length;
+    }
+}
