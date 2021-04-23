ed5808fbb13329510b12ff96290f5d3e5bc03dad
Adam Sitnik <adam.sitnik@gmail.com>
2021-04-23 12:07:44 +0200

    add a test for "file was too large" exception

--- src/libraries/System.IO.FileSystem/tests/FileStream/ctor_str_fm_fa_fs_buffer_fo_as.Windows.cs
+++ src/libraries/System.IO.FileSystem/tests/FileStream/ctor_str_fm_fa_fs_buffer_fo_as.Windows.cs
@@ -1,6 +1,8 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Runtime.InteropServices;
+using System.Text;
 using Xunit;
 
 namespace System.IO.Tests
@@ -21,5 +23,63 @@ namespace System.IO.Tests
 
             return info.AllocationSize;
         }
+
+        [PlatformSpecific(TestPlatforms.Windows)]
+        [ConditionalTheory(nameof(IsFat32))]
+        [InlineData(FileMode.Create)]
+        [InlineData(FileMode.CreateNew)]
+        [InlineData(FileMode.OpenOrCreate)]
+        public void WhenFileIsTooLargeTheErrorMessageContainsAllDetails(FileMode mode)
+        {
+            const long tooMuch = uint.MaxValue + 1L; // more than FAT32 max size
+
+            string filePath = GetPathToNonExistingFile();
+            Assert.StartsWith(Path.GetTempPath(), filePath); // this is what IsFat32 method relies on
+
+            IOException ex = Assert.Throws<IOException>(() => new FileStream(filePath, mode, FileAccess.Write, FileShare.None, c_DefaultBufferSize, FileOptions.None, tooMuch));
+            Assert.Contains("file was too large", ex.Message);
+            Assert.Contains(filePath, ex.Message);
+            Assert.Contains(tooMuch.ToString(), ex.Message);
+
+            Assert.False(File.Exists(filePath)); // ensure it was NOT created
+        }
+
+        public static bool IsFat32
+        {
+            get
+            {
+                string testDirectory = Path.GetTempPath(); // logic taken from FileCleanupTestBase, can't call the property here as it's not static
+
+                var volumeNameBufffer = new StringBuilder(250);
+                var fileSystemNameBuffer = new StringBuilder(250);
+
+                if (GetVolumeInformation(
+                    Path.GetPathRoot(testDirectory),
+                    volumeNameBufffer,
+                    volumeNameBufffer.Capacity,
+                    out uint _,
+                    out uint _,
+                    out uint _,
+                    fileSystemNameBuffer,
+                    fileSystemNameBuffer.Capacity
+                    ))
+                {
+                    return fileSystemNameBuffer.ToString().Equals("FAT32", StringComparison.OrdinalIgnoreCase);
+                }
+
+                return false;
+            }
+        }
+
+        [DllImport(Interop.Libraries.Kernel32, CharSet = CharSet.Auto, SetLastError = true)]
+        public extern static bool GetVolumeInformation(
+           string rootPathName,
+           StringBuilder volumeNameBuffer,
+           int volumeNameSize,
+           out uint volumeSerialNumber,
+           out uint maximumComponentLength,
+           out uint fileSystemFlags,
+           StringBuilder fileSystemNameBuffer,
+           int fileSystemNameSize);
     }
 }
