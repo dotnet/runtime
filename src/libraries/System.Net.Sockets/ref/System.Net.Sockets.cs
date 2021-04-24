ee5ce8bc642fbd46dcd3f94f44b86490934ed681
Vlad <hrrrrustic@gmail.com>
2021-04-25 01:15:39 +0300

    fix default argument value

--- src/libraries/System.Net.Sockets/ref/System.Net.Sockets.cs
+++ src/libraries/System.Net.Sockets/ref/System.Net.Sockets.cs
@@ -151,7 +151,7 @@ namespace System.Net.Sockets
         public override void Write(byte[] buffer, int offset, int count) { }
         public override void Write(System.ReadOnlySpan<byte> buffer) { }
         public override System.Threading.Tasks.Task WriteAsync(byte[] buffer, int offset, int count, System.Threading.CancellationToken cancellationToken) { throw null; }
-        public override System.Threading.Tasks.ValueTask WriteAsync(System.ReadOnlyMemory<byte> buffer, System.Threading.CancellationToken cancellationToken) { throw null; }
+        public override System.Threading.Tasks.ValueTask WriteAsync(System.ReadOnlyMemory<byte> buffer, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
         public override void WriteByte(byte value) { }
     }
     public enum ProtocolFamily
