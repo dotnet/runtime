2f557096dfb807962703f0ca1150e73e88c8367c
Coderrob <Rob@Coderrob.com>
2021-04-24 16:09:21 -0500

    Update type names in downstream unit test.

--- src/libraries/Microsoft.Extensions.Options.DataAnnotations/src/DataAnnotationValidateOptions.cs
+++ src/libraries/Microsoft.Extensions.Options.DataAnnotations/src/DataAnnotationValidateOptions.cs
@@ -54,7 +54,7 @@ namespace Microsoft.Extensions.Options
             var errors = new List<string>();
             foreach (var result in validationResults)
             {
-                errors.Add($"DataAnnotation validation failed for {typeName}'s members: '{string.Join(",", result.MemberNames)}' with the error: '{result.ErrorMessage}'.");
+                errors.Add($"DataAnnotation validation failed for {typeName} members: '{string.Join(",", result.MemberNames)}' with the error: '{result.ErrorMessage}'.");
             }
 
             return ValidateOptionsResult.Fail(errors);
