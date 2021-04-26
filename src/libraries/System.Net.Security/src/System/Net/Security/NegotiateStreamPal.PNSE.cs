457c937753fadaffbd7ea4c4ae585be9758bd4c3
Maxim Lipnin <v-maxlip@microsoft.com>
2021-04-26 16:57:43 +0300

    Add PNSE version of NegotiateStreamPal class

---
+++ src/libraries/System.Net.Security/src/System/Net/Security/NegotiateStreamPal.PNSE.cs
@@ -0,0 +1,31 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.ComponentModel;
+using System.Security.Principal;
+
+namespace System.Net.Security
+{
+    //
+    // The class maintains the state of the authentication process and the security context.
+    // It encapsulates security context and does the real work in authentication and
+    // user data encryption with NEGO SSPI package.
+    //
+    internal static partial class NegotiateStreamPal
+    {
+        internal static IIdentity GetIdentity(NTAuthentication context)
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        internal static string QueryContextAssociatedName(SafeDeleteContext? securityContext)
+        {
+            throw new PlatformNotSupportedException(SR.net_nego_server_not_supported);
+        }
+
+        internal static void ValidateImpersonationLevel(TokenImpersonationLevel impersonationLevel)
+        {
+            throw new PlatformNotSupportedException();
+        }
+    }
+}
