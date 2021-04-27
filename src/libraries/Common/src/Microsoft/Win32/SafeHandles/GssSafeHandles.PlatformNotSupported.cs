60d99f7430a69cfc21434243f8e53cfeaa9bb6e6
Maxim Lipnin <v-maxlip@microsoft.com>
2021-04-27 15:00:28 +0300

    Remove the interop files from the build

---
+++ src/libraries/Common/src/Microsoft/Win32/SafeHandles/GssSafeHandles.PlatformNotSupported.cs
@@ -0,0 +1,61 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Microsoft.Win32.SafeHandles
+{
+    internal sealed class SafeGssNameHandle : SafeHandle
+    {
+        public static SafeGssNameHandle CreateUser(string name) => throw new PlatformNotSupportedException();
+        public static SafeGssNameHandle CreateTarget(string name) => throw new PlatformNotSupportedException();
+        public override bool IsInvalid
+        {
+            get {  throw new PlatformNotSupportedException(); }
+        }
+
+        protected override bool ReleaseHandle() => throw new PlatformNotSupportedException();
+        public SafeGssNameHandle()
+            : base(IntPtr.Zero, true)
+        {
+            throw new PlatformNotSupportedException();
+        }
+    }
+
+    internal sealed class SafeGssCredHandle : SafeHandle
+    {
+        public static SafeGssCredHandle CreateAcceptor() => throw new PlatformNotSupportedException();
+        public static SafeGssCredHandle Create(string username, string password, bool isNtlmOnly) => throw new PlatformNotSupportedException();
+        public SafeGssCredHandle()
+            : base(IntPtr.Zero, true)
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        public override bool IsInvalid
+        {
+            get { throw new PlatformNotSupportedException(); }
+        }
+
+        protected override bool ReleaseHandle() => throw new PlatformNotSupportedException();
+    }
+
+    internal sealed class SafeGssContextHandle : SafeHandle
+    {
+        public SafeGssContextHandle()
+            : base(IntPtr.Zero, true)
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        public override bool IsInvalid
+        {
+            get {  throw new PlatformNotSupportedException(); }
+        }
+
+        protected override bool ReleaseHandle() => throw new PlatformNotSupportedException();
+    }
+}
---
+++ src/libraries/Common/src/System/Net/ContextFlagsAdapterPal.PlatformNotSupported.cs
@@ -0,0 +1,20 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+
+namespace System.Net
+{
+    internal static class ContextFlagsAdapterPal
+    {
+        internal static ContextFlagsPal GetContextFlagsPalFromInterop(Interop.NetSecurityNative.GssFlags gssFlags, bool isServer)
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        internal static Interop.NetSecurityNative.GssFlags GetInteropFromContextFlagsPal(ContextFlagsPal flags, bool isServer)
+        {
+            throw new PlatformNotSupportedException();
+        }
+    }
+}
