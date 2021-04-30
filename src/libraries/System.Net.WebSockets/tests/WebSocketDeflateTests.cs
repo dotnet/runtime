721384062e9d812f2e12639319efb0d5c424da63
Dan Moseley <danmose@microsoft.com>
2021-04-30 01:12:25 -0700

    increase timeout for websocket compression tests (#52086)
    * increase timeout for websocket compression tests
    * disable for 14 and 15 on ARM

--- src/libraries/System.Net.WebSockets/tests/WebSocketDeflateTests.cs
+++ src/libraries/System.Net.WebSockets/tests/WebSocketDeflateTests.cs
@@ -20,7 +20,7 @@ namespace System.Net.WebSockets.Tests
         {
             if (!Debugger.IsAttached)
             {
-                _cancellation = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+                _cancellation = new CancellationTokenSource(TimeSpan.FromSeconds(30));
             }
         }
 
@@ -421,6 +421,12 @@ namespace System.Net.WebSockets.Tests
         [MemberData(nameof(SupportedWindowBits))]
         public async Task PayloadShouldHaveSimilarSizeWhenSplitIntoSegments(int windowBits)
         {
+            if (PlatformDetection.IsArmOrArm64Process && (windowBits == 14 || windowBits == 15))
+            {
+                // https://github.com/dotnet/runtime/issues/52031
+                return;
+            }
+
             MemoryStream stream = new();
             using WebSocket client = WebSocket.CreateFromStream(stream, new WebSocketCreationOptions
             {
