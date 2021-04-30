092c3620335db826d43f7b11c42c6d169ecb85cc
Dan Moseley <danmose@microsoft.com>
2021-04-29 20:42:27 -0700

    Update WebSocketDeflateTests.cs

--- src/libraries/System.Net.WebSockets/tests/WebSocketDeflateTests.cs
+++ src/libraries/System.Net.WebSockets/tests/WebSocketDeflateTests.cs
@@ -20,7 +20,7 @@ namespace System.Net.WebSockets.Tests
         {
             if (!Debugger.IsAttached)
             {
-                _cancellation = new CancellationTokenSource(30);
+                _cancellation = new CancellationTokenSource(TimeSpan.FromSeconds(30));
             }
         }
 
