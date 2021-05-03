92c5ac673f39d7eedadc2f87236e2abdf00a335a
Filip Navara <navara@emclient.com>
2021-05-03 17:18:24 +0200

    Remove debugging code

--- src/libraries/Native/Unix/System.Security.Cryptography.Native.Apple/extra_libs.cmake
+++ src/libraries/Native/Unix/System.Security.Cryptography.Native.Apple/extra_libs.cmake
@@ -2,7 +2,6 @@
 macro(append_extra_cryptography_apple_libs NativeLibsExtra)
     find_library(COREFOUNDATION_LIBRARY CoreFoundation)
     find_library(SECURITY_LIBRARY Security)
-    find_library(FOUNDATION_LIBRARY Foundation)
 
-    list(APPEND ${NativeLibsExtra} ${COREFOUNDATION_LIBRARY} ${SECURITY_LIBRARY} ${FOUNDATION_LIBRARY})
+    list(APPEND ${NativeLibsExtra} ${COREFOUNDATION_LIBRARY} ${SECURITY_LIBRARY})
 endmacro()
