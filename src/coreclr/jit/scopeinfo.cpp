e8258b7e3e72de487ad5d2f526d2f8d04256e2c6
Sergey <seandree@microsoft.com>
2021-05-03 13:55:34 -0700

    improve siVarLoc logic.

--- src/coreclr/jit/scopeinfo.cpp
+++ src/coreclr/jit/scopeinfo.cpp
@@ -452,24 +452,15 @@ void CodeGenInterface::siVarLoc::siFillRegisterVarLoc(
 //    Called for every psiScope in "psiScopeList" codegen.h
 CodeGenInterface::siVarLoc::siVarLoc(const LclVarDsc* varDsc, regNumber baseReg, int offset, bool isFramePointerUsed)
 {
-    var_types regType = varDsc->GetRegisterType();
-    var_types type;
-    if (regType == TYP_UNDEF)
-    {
-        type = genActualType(varDsc->TypeGet());
-    }
-    else
-    {
-        type = genActualType(regType);
-    }
-
     if (varDsc->lvIsInReg())
     {
-        siFillRegisterVarLoc(varDsc, type, baseReg, offset, isFramePointerUsed);
+        var_types regType = varDsc->GetActualRegisterType();
+        siFillRegisterVarLoc(varDsc, regType, baseReg, offset, isFramePointerUsed);
     }
     else
     {
-        siFillStackVarLoc(varDsc, type, baseReg, offset, isFramePointerUsed);
+        var_types stackType = genActualType(varDsc->TypeGet());
+        siFillStackVarLoc(varDsc, stackType, baseReg, offset, isFramePointerUsed);
     }
 }
 
