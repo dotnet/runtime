5f65e1842fb947c25c710ca248afd51b3acdcb5b
Mitchell Hwang <mitchell.hwang@microsoft.com>
2021-05-05 16:45:26 -0400

    [iOS][tvOS] Reenable System.Diagnostics.Process.Tests ActiveIssue and fix typo

--- src/libraries/System.Diagnostics.Process/tests/ProcessStartInfoTests.cs
+++ src/libraries/System.Diagnostics.Process/tests/ProcessStartInfoTests.cs
@@ -31,7 +31,7 @@ namespace System.Diagnostics.Tests
             => PlatformDetection.IsWindowsAndElevated && PlatformDetection.IsNotWindowsNanoServer && RemoteExecutor.IsSupported;
 
         [Fact]
-        [ActiveIssue("https://github.com/dotnet/runtime/issues/51384", TestPlatforms.iOS | TestPlatforms.tvOS)]
+        [ActiveIssue("https://github.com/dotnet/runtime/issues/51386", TestPlatforms.iOS | TestPlatforms.tvOS)]
         public void TestEnvironmentProperty()
         {
             Assert.NotEqual(0, new Process().StartInfo.Environment.Count);
@@ -773,7 +773,7 @@ namespace System.Diagnostics.Tests
 
         [PlatformSpecific(TestPlatforms.AnyUnix)]  // Test case is specific to Unix
         [Fact]
-        // [ActiveIssue("https://github.com/dotnet/runtime/issues/51386", TestPlatforms.iOS | TestPlatforms.tvOS)]
+        [ActiveIssue("https://github.com/dotnet/runtime/issues/51386", TestPlatforms.iOS | TestPlatforms.tvOS)]
         public void TestEnvironmentVariablesPropertyUnix()
         {
             ProcessStartInfo psi = new ProcessStartInfo();
