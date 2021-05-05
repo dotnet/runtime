4bd84ac5de965eeca746ebdfa7f400eae10f217c
Pavel Savara <pavelsavara@microsoft.com>
2021-05-05 14:29:45 +0200

    - fix null reference in describe_value() + add test for it - fix null reference in PDB load - improved handling of targetCrashed in Inspector - improve Makefile to allow passing test arguments together with filters - add windows chrome to probe path - include debugging payload into test failure exception for easier debugging - make it possible to compile with WasmBuildNative=true - make it possible to compile with Debug runtime - enable Mono logging in debug mode

---
+++ src/mono/wasm/debugger/DebuggerTestSuite/AssignmentTests.cs
@@ -0,0 +1,235 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace DebuggerTests
+{
+    public class AssignmentTests : DebuggerTestBase
+    {
+        [Fact]
+        async Task InspectObjectAssignmentsDuringSteppingIn()
+        {
+            await InspectAssignmentDuringSteppingIn("MONO_TYPE_OBJECT",
+                (locals) => CheckObject(locals, "r", "object", is_null: true),
+                (locals) => CheckObject(locals, "r", "object")
+                );
+        }
+
+        [Fact]
+        async Task InspectClassAssignmentsDuringSteppingIn()
+        {
+            await InspectAssignmentDuringSteppingIn("MONO_TYPE_CLASS",
+                (locals) => CheckObject(locals, "r", "DebuggerTests.MONO_TYPE_CLASS", is_null: true),
+                (locals) => CheckObject(locals, "r", "DebuggerTests.MONO_TYPE_CLASS")
+                );
+        }
+
+        [Fact]
+        async Task InspectBoolAssignmentsDuringSteppingIn()
+        {
+            await InspectAssignmentDuringSteppingIn("MONO_TYPE_BOOLEAN",
+                (locals) => CheckBool(locals, "r", default),
+                (locals) => CheckBool(locals, "r", true)
+                );
+        }
+
+        [Fact]
+        async Task InspectCharAssignmentsDuringSteppingIn()
+        {
+            await InspectAssignmentDuringSteppingIn("MONO_TYPE_CHAR",
+                (locals) => CheckSymbol(locals, "r", "0 '\u0000'"),
+                (locals) => CheckSymbol(locals, "r", "97 'a'")
+                );
+        }
+
+        [Fact]
+        async Task InspectSByteAssignmentsDuringSteppingIn()
+        {
+            await InspectAssignmentDuringSteppingIn("MONO_TYPE_I1",
+                (locals) => CheckNumber<sbyte>(locals, "r", default),
+                (locals) => CheckNumber<sbyte>(locals, "r", -1)
+                );
+        }
+
+        [Fact]
+        async Task InspectShortAssignmentsDuringSteppingIn()
+        {
+            await InspectAssignmentDuringSteppingIn("MONO_TYPE_I2",
+                (locals) => CheckNumber<short>(locals, "r", default),
+                (locals) => CheckNumber<short>(locals, "r", -1)
+                );
+        }
+
+        [Fact]
+        async Task InspectIntAssignmentsDuringSteppingIn()
+        {
+            await InspectAssignmentDuringSteppingIn("MONO_TYPE_I4",
+                (locals) => CheckNumber<int>(locals, "r", default),
+                (locals) => CheckNumber<int>(locals, "r", -1)
+                );
+        }
+
+        [Fact]
+        async Task InspectLongAssignmentsDuringSteppingIn()
+        {
+            await InspectAssignmentDuringSteppingIn("MONO_TYPE_I8",
+                (locals) => CheckNumber<long>(locals, "r", default),
+                (locals) => CheckNumber<long>(locals, "r", -1)
+                );
+        }
+
+        [Fact]
+        async Task InspectByteAssignmentsDuringSteppingIn()
+        {
+            await InspectAssignmentDuringSteppingIn("MONO_TYPE_U1",
+                (locals) => CheckNumber<byte>(locals, "r", default),
+                (locals) => CheckNumber<byte>(locals, "r", 1)
+                );
+        }
+
+        [Fact]
+        async Task InspectUShortAssignmentsDuringSteppingIn()
+        {
+            await InspectAssignmentDuringSteppingIn("MONO_TYPE_U2",
+                (locals) => CheckNumber<ushort>(locals, "r", default),
+                (locals) => CheckNumber<ushort>(locals, "r", 1)
+                );
+        }
+
+        [Fact]
+        async Task InspectUIntAssignmentsDuringSteppingIn()
+        {
+            await InspectAssignmentDuringSteppingIn("MONO_TYPE_U4",
+                (locals) => CheckNumber<uint>(locals, "r", default),
+                (locals) => CheckNumber<uint>(locals, "r", 1)
+                );
+        }
+
+        [Fact]
+        async Task InspectULongAssignmentsDuringSteppingIn()
+        {
+            await InspectAssignmentDuringSteppingIn("MONO_TYPE_U8",
+                (locals) => CheckNumber<ulong>(locals, "r", default),
+                (locals) => CheckNumber<ulong>(locals, "r", 1)
+                );
+        }
+
+        [Fact]
+        async Task InspectFloatAssignmentsDuringSteppingIn()
+        {
+            await InspectAssignmentDuringSteppingIn("MONO_TYPE_R4",
+                (locals) => CheckNumber<float>(locals, "r", default),
+                (locals) => CheckNumber<float>(locals, "r", 3.1415F)
+                );
+        }
+
+        [Fact]
+        async Task InspectDoubleAssignmentsDuringSteppingIn()
+        {
+            await InspectAssignmentDuringSteppingIn("MONO_TYPE_R8",
+                (locals) => CheckNumber<double>(locals, "r", default),
+                (locals) => CheckNumber<double>(locals, "r", 3.1415D)
+                );
+        }
+
+        [Fact]
+        async Task InspectStringAssignmentsDuringSteppingIn()
+        {
+            await InspectAssignmentDuringSteppingIn("MONO_TYPE_STRING",
+                (locals) => CheckString(locals, "r", default),
+                (locals) => CheckString(locals, "r", "hello")
+                );
+        }
+
+        [Fact]
+        async Task InspectEnumAssignmentsDuringSteppingIn()
+        {
+            await InspectAssignmentDuringSteppingIn("MONO_TYPE_ENUM",
+                (locals) => CheckEnum(locals, "r", "DebuggerTests.RGB", "Red"),
+                (locals) => CheckEnum(locals, "r", "DebuggerTests.RGB", "Blue")
+                );
+        }
+
+        [Fact]
+        async Task InspectArrayAssignmentsDuringSteppingIn()
+        {
+            await InspectAssignmentDuringSteppingIn("MONO_TYPE_ARRAY",
+                (locals) => CheckObject(locals, "r", "byte[]", is_null: true),
+                (locals) => CheckArray(locals, "r", "byte[]", 2)
+                );
+        }
+
+        [Fact]
+        async Task InspectStructAssignmentsDuringSteppingIn()
+        {
+            await InspectAssignmentDuringSteppingIn("MONO_TYPE_VALUETYPE",
+                (locals) => CheckValueType(locals, "r", "DebuggerTests.Point"),
+                (locals) => CheckValueType(locals, "r", "DebuggerTests.Point")
+                );
+        }
+
+        [Fact]
+        async Task InspectDecimalAssignmentsDuringSteppingIn()
+        {
+            await InspectAssignmentDuringSteppingIn("MONO_TYPE_VALUETYPE2",
+                (locals) => CheckValueType(locals, "r", "System.Decimal", description: "0"),
+                (locals) => CheckValueType(locals, "r", "System.Decimal", description: "1.1")
+                );
+        }
+
+        [Fact]
+        async Task InspectFunctionAssignmentsDuringSteppingIn()
+        {
+            await InspectAssignmentDuringSteppingIn("MONO_TYPE_GENERICINST",
+                (locals) => CheckObject(locals, "r", "System.Func<int>", is_null: true),
+                (locals) => CheckObject(locals, "r", "System.Func<int>", description: "int OuterMethod ()")
+                );
+        }
+
+        [Fact]
+        async Task InspectFunctionPtrAssignmentsDuringSteppingIn()
+        {
+            await InspectAssignmentDuringSteppingIn("MONO_TYPE_FNPTR",
+                (locals) => CheckSymbol(locals, "r", "(*()) 0"),
+                (locals) => { } // CheckObject(locals, "r", "(*()) 0x22dd230") // FixMe: Could we do better ?
+                );
+        }
+
+        [Fact]
+        async Task InspectIntPtrAssignmentsDuringSteppingIn()
+        {
+            await InspectAssignmentDuringSteppingIn("MONO_TYPE_PTR",
+                (locals) => CheckSymbol(locals, "r", "(int*) 0"),
+                (locals) => { } //CheckObject(locals, "r", "(int*) 0x211c058") // FixMe: Could we do better ?
+                );
+        }
+
+        private async Task InspectAssignmentDuringSteppingIn(string clazz, Action<JToken> checkDefault, Action<JToken> checkValue)
+        {
+            await SetBreakpointInMethod("debugger-test", "DebuggerTests." + clazz, "OuterMethod", 2);
+            await EvaluateAndCheck("window.setTimeout(function() { invoke_static_method('[debugger-test] DebuggerTests." + clazz + ":OuterMethod'); })", null, -1, -1, "OuterMethod");
+
+            await StepAndCheck(StepKind.Into, "dotnet://debugger-test.dll/debugger-stepping-test.cs", -1, -1, "InnerMethod",
+                locals_fn: (locals) =>
+                {
+                    Console.WriteLine(locals);
+                    Assert.Equal(2, locals.Count());
+                    checkDefault(locals);
+                }
+            );
+            await StepAndCheck(StepKind.Over, "dotnet://debugger-test.dll/debugger-stepping-test.cs", -1, -1, "InnerMethod", times: 3,
+                locals_fn: (locals) =>
+                {
+                    Console.WriteLine(locals);
+                    Assert.Equal(2, locals.Count());
+                    checkValue(locals);
+                }
+            );
+        }
+    }
+}
---
+++ src/mono/wasm/debugger/tests/debugger-test/debugger-stepping-test.cs
@@ -0,0 +1,232 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DebuggerTests
+{
+    public struct StepInTest<T>
+    {
+        public static int InnerMethod(T value)
+        {
+            T r;
+            r = value;
+            return 0;
+        }
+    }
+
+    public class MONO_TYPE_OBJECT
+    {
+        public static int OuterMethod()
+        {
+            var value = new object();
+            return StepInTest<object>.InnerMethod(value);
+        }
+    }
+
+    public class MONO_TYPE_CLASS
+    {
+        public static int OuterMethod()
+        {
+            var value = new MONO_TYPE_CLASS();
+            return StepInTest<MONO_TYPE_CLASS>.InnerMethod(value);
+        }
+    }
+
+    public class MONO_TYPE_BOOLEAN
+    {
+        public static int OuterMethod()
+        {
+            var value = true;
+            return StepInTest<bool>.InnerMethod(value);
+        }
+    }
+
+    public class MONO_TYPE_CHAR
+    {
+        public static int OuterMethod()
+        {
+            var value = 'a';
+            return StepInTest<char>.InnerMethod(value);
+        }
+    }
+
+    public class MONO_TYPE_I1
+    {
+        public static int OuterMethod()
+        {
+            sbyte value = -1;
+            return StepInTest<sbyte>.InnerMethod(value);
+        }
+    }
+
+    public class MONO_TYPE_I2
+    {
+        public static int OuterMethod()
+        {
+            short value = -1;
+            return StepInTest<short>.InnerMethod(value);
+        }
+    }
+
+    public class MONO_TYPE_I4
+    {
+        public static int OuterMethod()
+        {
+            int value = -1;
+            return StepInTest<int>.InnerMethod(value);
+        }
+    }
+
+    public class MONO_TYPE_I8
+    {
+        public static int OuterMethod()
+        {
+            long value = -1;
+            return StepInTest<long>.InnerMethod(value);
+        }
+    }
+
+    public class MONO_TYPE_U1
+    {
+        public static int OuterMethod()
+        {
+            byte value = 1;
+            return StepInTest<byte>.InnerMethod(value);
+        }
+    }
+
+    public class MONO_TYPE_U2
+    {
+        public static int OuterMethod()
+        {
+            ushort value = 1;
+            return StepInTest<ushort>.InnerMethod(value);
+        }
+    }
+
+    public class MONO_TYPE_U4
+    {
+        public static int OuterMethod()
+        {
+            uint value = 1;
+            return StepInTest<uint>.InnerMethod(value);
+        }
+    }
+
+    public class MONO_TYPE_U8
+    {
+        public static int OuterMethod()
+        {
+            ulong value = 1;
+            return StepInTest<ulong>.InnerMethod(value);
+        }
+    }
+
+    public class MONO_TYPE_R4
+    {
+        public static int OuterMethod()
+        {
+            float value = 3.1415F;
+            return StepInTest<float>.InnerMethod(value);
+        }
+    }
+
+    public class MONO_TYPE_R8
+    {
+        public static int OuterMethod()
+        {
+            double value = 3.1415D;
+            return StepInTest<double>.InnerMethod(value);
+        }
+    }
+
+    public class MONO_TYPE_STRING
+    {
+        public static int OuterMethod()
+        {
+            string value = "hello";
+            return StepInTest<string>.InnerMethod(value);
+        }
+    }
+
+    public class MONO_TYPE_ENUM
+    {
+        public static int OuterMethod()
+        {
+            RGB value = RGB.Blue;
+            return StepInTest<RGB>.InnerMethod(value);
+        }
+    }
+
+    public class MONO_TYPE_ARRAY
+    {
+        public static int OuterMethod()
+        {
+            byte[] value = new byte[2] { 1, 2 };
+            return StepInTest<byte[]>.InnerMethod(value);
+        }
+    }
+
+    public class MONO_TYPE_VALUETYPE
+    {
+        public static int OuterMethod()
+        {
+            Point value = new Point();
+            return StepInTest<Point>.InnerMethod(value);
+        }
+    }
+
+    public class MONO_TYPE_VALUETYPE2
+    {
+        public static int OuterMethod()
+        {
+            Decimal value = 1.1m;
+            return StepInTest<Decimal>.InnerMethod(value);
+        }
+    }
+
+    public class MONO_TYPE_GENERICINST
+    {
+        public static int OuterMethod()
+        {
+            Func<int> value = MONO_TYPE_GENERICINST.OuterMethod;
+            return StepInTest<Func<int>>.InnerMethod(value);
+        }
+    }
+
+    public class MONO_TYPE_FNPTR
+    {
+        public unsafe static int OuterMethod()
+        {
+            delegate*<int> value = &MONO_TYPE_FNPTR.OuterMethod;
+            return InnerMethod(value);
+        }
+
+        public unsafe static int InnerMethod(delegate*<int> value)
+        {
+            delegate*<int> r;
+            r = value;
+            return 0;
+        }
+    }
+
+    public class MONO_TYPE_PTR
+    {
+        public unsafe static int OuterMethod()
+        {
+            int a = 1; int* value = &a;
+            return InnerMethod(value);
+        }
+
+        public unsafe static int InnerMethod(int* value)
+        {
+            int* r;
+            r = value;
+            return 0;
+        }
+    }
+}
