f782919125afe80f382e4622a644ae3e5f064901
Maxim <43318993+DarkBullNull@users.noreply.github.com>
2021-05-05 16:26:30 +0300

    Update codegencommon.cpp

--- src/coreclr/jit/codegencommon.cpp
+++ src/coreclr/jit/codegencommon.cpp
@@ -2377,12 +2377,6 @@ void CodeGen::genEmitMachineCode()
 
     compiler->unwindReserve();
 
-#if DISPLAY_SIZES
-
-    dataSize = GetEmitter()->emitDataSize();
-
-#endif // DISPLAY_SIZES
-
     bool trackedStackPtrsContig; // are tracked stk-ptrs contiguous ?
 
 #if defined(TARGET_AMD64) || defined(TARGET_ARM64)
@@ -2601,7 +2595,8 @@ void CodeGen::genEmitUnwindDebugGCandEH()
     regSet.tmpDone();
 
 #if DISPLAY_SIZES
-
+    
+    size_t dataSize = GetEmitter()->emitDataSize();
     grossVMsize += compiler->info.compILCodeSize;
     totalNCsize += codeSize + dataSize + compiler->compInfoBlkSize;
     grossNCsize += codeSize + dataSize;
