32ff0f5c0f24c4a5ca73755a47827b0baec97069
Pavel Savara <pavelsavara@microsoft.com>
2021-05-06 13:38:32 +0200

    moved to start of the constructor

--- src/mono/wasm/debugger/DebuggerTestSuite/DebuggerTestBase.cs
+++ src/mono/wasm/debugger/DebuggerTestSuite/DebuggerTestBase.cs
@@ -76,15 +76,15 @@ namespace DebuggerTests
 
         public DebuggerTestBase(string driver = "debugger-driver.html")
         {
+            // the debugger is working in locale of the debugged application. For example Datetime.ToString()
+            // we want the test to mach it. We are also starting chrome with --lang=en-US
+            System.Globalization.CultureInfo.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+
             insp = new Inspector();
             cli = insp.Client;
             scripts = SubscribeToScripts(insp);
 
             startTask = TestHarnessProxy.Start(FindChromePath(), DebuggerTestAppPath, driver);
-
-            // the debugger is working in locale of the debugged application. For example Datetime.ToString()
-            // we want the test to mach it. We are also starting chrome with --lang=en-US
-            System.Globalization.CultureInfo.CurrentCulture = new System.Globalization.CultureInfo("en-US");
         }
 
         public virtual async Task InitializeAsync()
