31a950b3427deb48b4fd87f852419378d1e3959a
Tammy Qiu <tammy.qiu@yahoo.com>
2021-05-06 15:06:53 -0400

    revert naming changes for non-android specific cases

--- src/libraries/Common/tests/TestUtilities/Interop/Interop.Libraries.cs
+++ src/libraries/Common/tests/TestUtilities/Interop/Interop.Libraries.cs
@@ -6,7 +6,7 @@ internal static partial class Interop
     internal static partial class Libraries
     {
         // Shims
-        public const string CryptoNative = "libSystem.Security.Cryptography.Native.Android";
+        public const string CryptoNative = "libSystem.Security.Cryptography.Native.OpenSsl";
         public const string SystemNative = "libSystem.Native";
     }
 }
