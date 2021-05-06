7a8f4d8393a27c55830d789c45b229ba0feaae37
hrrrrustic <35951936+hrrrrustic@users.noreply.github.com>
2021-05-06 19:08:58 +0300

    System.Data.* missed Equals nullable annotations  (#52165)

--- src/libraries/System.Data.Common/src/System/Data/UniqueConstraint.cs
+++ src/libraries/System.Data.Common/src/System/Data/UniqueConstraint.cs
@@ -3,6 +3,7 @@
 
 using System.Diagnostics;
 using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 
 namespace System.Data
 {
@@ -388,7 +389,7 @@ namespace System.Data
         /// <summary>
         /// Compares this constraint to a second to determine if both are identical.
         /// </summary>
-        public override bool Equals(object? key2)
+        public override bool Equals([NotNullWhen(true)] object? key2)
         {
             if (!(key2 is UniqueConstraint))
                 return false;
