447f8485a08f831f34d00597a30800ac36f034ef
Jeff Handley <jeff.handley@microsoft.com>
2021-05-06 18:58:31 -0700

    Use RandomNumberGenerator.GetBytes() instead of RandomNumberGenerator.Create().GetBytes()

--- src/libraries/System.Security.Principal.Windows/tests/WindowsIdentityImpersonatedTests.netcoreapp.cs
+++ src/libraries/System.Security.Principal.Windows/tests/WindowsIdentityImpersonatedTests.netcoreapp.cs
@@ -127,62 +127,57 @@ public sealed class WindowsTestAccount : IDisposable
 
     private void CreateUser()
     {
-        string testAccountPassword;
-        using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+        byte[] randomBytes = RandomNumberGenerator.GetBytes(33);
+
+        // Add special chars to ensure it satisfies password requirements.
+        string testAccountPassword = Convert.ToBase64String(randomBytes) + "_-As@!%*(1)4#2";
+
+        USER_INFO_1 userInfo = new USER_INFO_1
         {
-            byte[] randomBytes = new byte[33];
-            rng.GetBytes(randomBytes);
+            usri1_name = _userName,
+            usri1_password = testAccountPassword,
+            usri1_priv = 1
+        };
 
-            // Add special chars to ensure it satisfies password requirements.
-            testAccountPassword = Convert.ToBase64String(randomBytes) + "_-As@!%*(1)4#2";
+        // Create user and remove/create if already exists
+        uint result = NetUserAdd(null, 1, ref userInfo, out uint param_err);
 
-            USER_INFO_1 userInfo = new USER_INFO_1
+        // error codes https://docs.microsoft.com/en-us/windows/desktop/netmgmt/network-management-error-codes
+        // 0 == NERR_Success
+        if (result == 2224) // NERR_UserExists
+        {
+            result = NetUserDel(null, userInfo.usri1_name);
+            if (result != 0)
             {
-                usri1_name = _userName,
-                usri1_password = testAccountPassword,
-                usri1_priv = 1
-            };
-
-            // Create user and remove/create if already exists
-            uint result = NetUserAdd(null, 1, ref userInfo, out uint param_err);
-
-            // error codes https://docs.microsoft.com/en-us/windows/desktop/netmgmt/network-management-error-codes
-            // 0 == NERR_Success
-            if (result == 2224) // NERR_UserExists
-            {
-                result = NetUserDel(null, userInfo.usri1_name);
-                if (result != 0)
-                {
-                    throw new Win32Exception((int)result);
-                }
-                result = NetUserAdd(null, 1, ref userInfo, out param_err);
-                if (result != 0)
-                {
-                    throw new Win32Exception((int)result);
-                }
+                throw new Win32Exception((int)result);
             }
-
-            const int LOGON32_PROVIDER_DEFAULT = 0;
-            const int LOGON32_LOGON_INTERACTIVE = 2;
-
-            if (!LogonUser(_userName, ".", testAccountPassword, LOGON32_LOGON_INTERACTIVE, LOGON32_PROVIDER_DEFAULT, out _accountTokenHandle))
+            result = NetUserAdd(null, 1, ref userInfo, out param_err);
+            if (result != 0)
             {
-                _accountTokenHandle = null;
-                throw new Exception($"Failed to get SafeAccessTokenHandle for test account {_userName}", new Win32Exception());
+                throw new Win32Exception((int)result);
             }
+        }
 
-            bool gotRef = false;
-            try
-            {
-                _accountTokenHandle.DangerousAddRef(ref gotRef);
-                IntPtr logonToken = _accountTokenHandle.DangerousGetHandle();
-                AccountName = new WindowsIdentity(logonToken).Name;
-            }
-            finally
-            {
-                if (gotRef)
-                    _accountTokenHandle.DangerousRelease();
-            }
+        const int LOGON32_PROVIDER_DEFAULT = 0;
+        const int LOGON32_LOGON_INTERACTIVE = 2;
+
+        if (!LogonUser(_userName, ".", testAccountPassword, LOGON32_LOGON_INTERACTIVE, LOGON32_PROVIDER_DEFAULT, out _accountTokenHandle))
+        {
+            _accountTokenHandle = null;
+            throw new Exception($"Failed to get SafeAccessTokenHandle for test account {_userName}", new Win32Exception());
+        }
+
+        bool gotRef = false;
+        try
+        {
+            _accountTokenHandle.DangerousAddRef(ref gotRef);
+            IntPtr logonToken = _accountTokenHandle.DangerousGetHandle();
+            AccountName = new WindowsIdentity(logonToken).Name;
+        }
+        finally
+        {
+            if (gotRef)
+                _accountTokenHandle.DangerousRelease();
         }
     }
 
