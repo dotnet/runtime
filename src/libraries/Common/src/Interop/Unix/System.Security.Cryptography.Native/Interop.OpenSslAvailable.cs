7e3ae659ebac75561d8be72b55692ba0f58b61ab
Kevin Jones <kevin@vcsjones.com>
2021-05-12 13:17:09 -0400

    Code review feedback

--- src/libraries/Common/src/Interop/Unix/System.Security.Cryptography.Native/Interop.OpenSslAvailable.cs
+++ src/libraries/Common/src/Interop/Unix/System.Security.Cryptography.Native/Interop.OpenSslAvailable.cs
@@ -11,9 +11,9 @@ internal static partial class Interop
         [DllImport(Libraries.CryptoNative, EntryPoint = "CryptoNative_OpenSslAvailable")]
         private static extern int OpenSslAvailable();
 
-        private static readonly Lazy<bool> s_LazyOpenSslAvailable =
+        private static readonly Lazy<bool> s_openSslAvailable =
             new Lazy<bool>(() => OpenSslAvailable() != 0);
 
-        internal static bool OpenSslIsAvailable => s_LazyOpenSslAvailable.Value;
+        internal static bool OpenSslIsAvailable => s_openSslAvailable.Value;
     }
 }
