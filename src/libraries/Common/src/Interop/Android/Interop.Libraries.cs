d01f1f04a441f5b5d3b27b63bdc2ff1e0efdb305
Tammy Qiu <tammy.qiu@yahoo.com>
2021-05-13 18:05:27 -0400

    [Android] Split System.Security.Cryptography.Native.Android files to use Interop.AndroidCryptoNative headers instead

--- src/libraries/Common/src/Interop/Android/Interop.Libraries.cs
+++ src/libraries/Common/src/Interop/Android/Interop.Libraries.cs
@@ -6,6 +6,6 @@ internal static partial class Interop
     internal static partial class Libraries
     {
         internal const string Liblog = "liblog";
-        internal const string CryptoNative = "libSystem.Security.Cryptography.Native.Android";
+        internal const string AndroidCryptoNative = "libSystem.Security.Cryptography.Native.Android";
     }
 }
