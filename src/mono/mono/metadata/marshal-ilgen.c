b59d32c53da2934f5142989ab8a839a40cf7ddd0
Zoltan Varga <vargaz@gmail.com>
2021-05-14 16:19:57 -0400

    [mono][aot] Fix an assertion. (#52762)
    Fixes https://github.com/dotnet/runtime/issues/52736.

--- src/mono/mono/metadata/marshal-ilgen.c
+++ src/mono/mono/metadata/marshal-ilgen.c
@@ -1448,6 +1448,7 @@ mono_mb_emit_restore_result (MonoMethodBuilder *mb, MonoType *return_type)
 		g_assert_not_reached ();
 		break;
 	case MONO_TYPE_PTR:
+	case MONO_TYPE_FNPTR:
 	case MONO_TYPE_STRING:
 	case MONO_TYPE_CLASS: 
 	case MONO_TYPE_OBJECT: 
