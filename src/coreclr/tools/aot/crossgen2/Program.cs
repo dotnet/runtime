c538a9b39c5171e9f4ff877daf565a7061abe54a
David Wrighton <davidwr@microsoft.com>
2021-05-13 17:39:04 -0700

    Move copy of Crossgen2 tasks/targets to runtime repo and build composite image for runtime repo in parallel path (#52579)
    * Move all crossgen2 tasks/targets used in Runtime repo to be local to the repo
    
    * Move ResolveReadyToRunCompilers task into the Crossgen2Tasks dll
    
    * Create composite bundle and package
    - Note that it doesn't build properly, but hopefully that's a crossgen2 task bug
    
    * Composite image production
    
    * Refactor how composite image creation works
    - Move it to allow mixed composite and non-composite r2r in the same build, controlled by the PublishReadyToRunCompositeExclusions list
    - Enhance the accuracy of how msbuild tracks which files are input vs reference for composite images
    - Implement the R2R exclusion list for composite images
    - Remove the always on usage of --inputbubble when compiling a composite image
    
    * - Add a scheme to perform public signing with a PublicKeyToken to a composite of the composite image if required
      - Required adding a new parameter to crossgen2

--- src/coreclr/tools/aot/crossgen2/Program.cs
+++ src/coreclr/tools/aot/crossgen2/Program.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.IO;
 using System.Reflection.Metadata;
 using System.Reflection.PortableExecutable;
@@ -634,6 +635,19 @@ namespace ILCompiler
                         optimizationMode = ((EcmaAssembly)inputModules[0].Assembly).HasOptimizationsDisabled() ? OptimizationMode.None : OptimizationMode.Blended;
                     }
 
+                    CompositeImageSettings compositeImageSettings = new CompositeImageSettings();
+
+                    if (_commandLineOptions.CompositeKeyFile != null)
+                    {
+                        ImmutableArray<byte> compositeStrongNameKey = File.ReadAllBytes(_commandLineOptions.CompositeKeyFile).ToImmutableArray();
+                        if (!IsValidPublicKey(compositeStrongNameKey))
+                        {
+                            throw new Exception(string.Format(SR.ErrorCompositeKeyFileNotPublicKey));
+                        }
+
+                        compositeImageSettings.PublicKey = compositeStrongNameKey;
+                    }
+
                     //
                     // Compile
                     //
@@ -659,6 +673,7 @@ namespace ILCompiler
                         .UseParallelism(_commandLineOptions.Parallelism)
                         .UseProfileData(profileDataManager)
                         .FileLayoutAlgorithms(_methodLayout, _fileLayout)
+                        .UseCompositeImageSettings(compositeImageSettings)
                         .UseJitPath(_commandLineOptions.JitPath)
                         .UseInstructionSetSupport(instructionSetSupport)
                         .UseCustomPESectionAlignment(_commandLineOptions.CustomPESectionAlignment)
@@ -826,6 +841,128 @@ namespace ILCompiler
             Console.WriteLine(CreateReproArgumentString(failingMethod));
             return false;
         }
+
+        private enum AlgorithmClass
+        {
+            Signature = 1,
+            Hash = 4,
+        }
+
+        private enum AlgorithmSubId
+        {
+            Sha1Hash = 4,
+            MacHash = 5,
+            RipeMdHash = 6,
+            RipeMd160Hash = 7,
+            Ssl3ShaMD5Hash = 8,
+            HmacHash = 9,
+            Tls1PrfHash = 10,
+            HashReplacOwfHash = 11,
+            Sha256Hash = 12,
+            Sha384Hash = 13,
+            Sha512Hash = 14,
+        }
+
+        private struct AlgorithmId
+        {
+            // From wincrypt.h
+            private const int AlgorithmClassOffset = 13;
+            private const int AlgorithmClassMask = 0x7;
+            private const int AlgorithmSubIdOffset = 0;
+            private const int AlgorithmSubIdMask = 0x1ff;
+
+            private readonly uint _flags;
+
+            public const int RsaSign = 0x00002400;
+            public const int Sha = 0x00008004;
+
+            public bool IsSet
+            {
+                get { return _flags != 0; }
+            }
+
+            public AlgorithmClass Class
+            {
+                get { return (AlgorithmClass)((_flags >> AlgorithmClassOffset) & AlgorithmClassMask); }
+            }
+
+            public AlgorithmSubId SubId
+            {
+                get { return (AlgorithmSubId)((_flags >> AlgorithmSubIdOffset) & AlgorithmSubIdMask); }
+            }
+
+            public AlgorithmId(uint flags)
+            {
+                _flags = flags;
+            }
+        }
+
+        private static readonly ImmutableArray<byte> s_ecmaKey = ImmutableArray.Create(new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0 });
+
+        private const int SnPublicKeyBlobSize = 13;
+
+        // From wincrypt.h
+        private const byte PublicKeyBlobId = 0x06;
+        private const byte PrivateKeyBlobId = 0x07;
+
+        // internal for testing
+        internal const int s_publicKeyHeaderSize = SnPublicKeyBlobSize - 1;
+
+        // From StrongNameInternal.cpp
+        // Checks to see if a public key is a valid instance of a PublicKeyBlob as
+        // defined in StongName.h
+        internal static bool IsValidPublicKey(ImmutableArray<byte> blob)
+        {
+            // The number of public key bytes must be at least large enough for the header and one byte of data.
+            if (blob.IsDefault || blob.Length < s_publicKeyHeaderSize + 1)
+            {
+                return false;
+            }
+
+            var blobReader = new BinaryReader(new MemoryStream(blob.ToArray()));
+
+            // Signature algorithm ID
+            var sigAlgId = blobReader.ReadUInt32();
+            // Hash algorithm ID
+            var hashAlgId = blobReader.ReadUInt32();
+            // Size of public key data in bytes, not including the header
+            var publicKeySize = blobReader.ReadUInt32();
+            // publicKeySize bytes of public key data
+            var publicKey = blobReader.ReadByte();
+
+            // The number of public key bytes must be the same as the size of the header plus the size of the public key data.
+            if (blob.Length != s_publicKeyHeaderSize + publicKeySize)
+            {
+                return false;
+            }
+
+            // Check for the ECMA key, which does not obey the invariants checked below.
+            if (System.Linq.Enumerable.SequenceEqual(blob, s_ecmaKey))
+            {
+                return true;
+            }
+
+            // The public key must be in the wincrypto PUBLICKEYBLOB format
+            if (publicKey != PublicKeyBlobId)
+            {
+                return false;
+            }
+
+            var signatureAlgorithmId = new AlgorithmId(sigAlgId);
+            if (signatureAlgorithmId.IsSet && signatureAlgorithmId.Class != AlgorithmClass.Signature)
+            {
+                return false;
+            }
+
+            var hashAlgorithmId = new AlgorithmId(hashAlgId);
+            if (hashAlgorithmId.IsSet && (hashAlgorithmId.Class != AlgorithmClass.Hash || hashAlgorithmId.SubId < AlgorithmSubId.Sha1Hash))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
 
         private static int Main(string[] args)
         {
