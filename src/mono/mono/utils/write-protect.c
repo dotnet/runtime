79c040d0ec6ef8b5533ce0a063f5da681b9308b8
Aleksey Kliger <alklig@microsoft.com>
2021-05-17 10:49:29 -0400

    use HOST_ARM64 macros instead of compiler __aarch64__ macro

--- src/mono/mono/utils/write-protect.c
+++ src/mono/mono/utils/write-protect.c
@@ -7,7 +7,7 @@
 #include "mono-compiler.h"
 #include "write-protect.h"
 
-#if (defined(HOST_IOS) || defined(HOST_TVOS)) && defined (HOST_DARWIN_SIMULATOR) && defined (__aarch64__)
+#if (defined(HOST_IOS) || defined(HOST_TVOS)) && defined (HOST_DARWIN_SIMULATOR) && defined (HOST_ARM64)
 
 /* our own declaration of pthread_jit_write_protect_np so that we don't see the __API_UNAVAILABLE__ header */
 void
@@ -20,7 +20,7 @@ mono_jit_write_protect (int enabled)
         pthread_jit_write_protect_np (enabled);
 }
 
-#else
+#else /* (defined(HOST_IOS) || defined(HOST_TVOS)) && defined (HOST_DARWIN_SIMULATOR) && defined (HOST_ARM64) */
 
 MONO_EMPTY_SOURCE_FILE (write_protect);
 
--- src/mono/mono/utils/write-protect.h
+++ src/mono/mono/utils/write-protect.h
@@ -7,12 +7,10 @@
 
 #include <mono/utils/mono-publib.h>
 
-#if (defined(HOST_IOS) || defined(HOST_TVOS)) && defined (HOST_DARWIN_SIMULATOR) && defined (__aarch64__)
-
-void
+#if (defined(HOST_IOS) || defined(HOST_TVOS)) && defined (HOST_DARWIN_SIMULATOR) && defined (HOST_ARM64)
 mono_jit_write_protect (int enabled);
 
-#endif /* defined (HOST_DARWIN_SIMULATOR) && defined (__aarch64__) */
+#endif /* (defined(HOST_IOS) || defined(HOST_TVOS)) && defined (HOST_DARWIN_SIMULATOR) && defined (HOST_ARM64) */
 
 #endif /* __MONO_WRITE_PROTECT_H__ */
 
