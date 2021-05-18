7be77b8fa0ab07b67aaf958173817ec90873c09e
hrrrrustic <35951936+hrrrrustic@users.noreply.github.com>
2021-05-18 22:16:09 +0300

    Obsolete Socket.UseOnlyOverlappedIO  (#52475)
    close #47163

--- src/libraries/System.Net.Sockets/tests/FunctionalTests/CreateSocketTests.cs
+++ src/libraries/System.Net.Sockets/tests/FunctionalTests/CreateSocketTests.cs
@@ -379,7 +379,9 @@ namespace System.Net.Sockets.Tests
             Assert.Equal(orig.ReceiveTimeout, copy.ReceiveTimeout);
             Assert.Equal(orig.SendBufferSize, copy.SendBufferSize);
             Assert.Equal(orig.SendTimeout, copy.SendTimeout);
+#pragma warning disable 0618
             Assert.Equal(orig.UseOnlyOverlappedIO, copy.UseOnlyOverlappedIO);
+#pragma warning restore 0618
         }
 
         [Theory]
@@ -424,7 +426,6 @@ namespace System.Net.Sockets.Tests
             Assert.Equal(orig.SendBufferSize, client.SendBufferSize);
             Assert.Equal(orig.SendTimeout, client.SendTimeout);
             Assert.Equal(orig.Ttl, client.Ttl);
-            Assert.Equal(orig.UseOnlyOverlappedIO, client.UseOnlyOverlappedIO);
 
             // Validate setting various properties on the new instance and seeing them roundtrip back to the original.
             client.ReceiveTimeout = 42;
