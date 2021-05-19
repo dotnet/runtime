6847e2f81969c35950722b0e7360de2b14ba29e1
Stefano Moioli <smxdev4@gmail.com>
2021-05-19 11:08:51 +0200

    HAVE_SIGACTION_ULONG_FLAGS: updated flags

--- src/libraries/Native/Unix/configure.cmake
+++ src/libraries/Native/Unix/configure.cmake
@@ -499,7 +499,8 @@ check_c_source_compiles(
      "
      HAVE_GETNAMEINFO_SIGNED_FLAGS)
 
-check_cxx_source_compiles(
+set(CMAKE_REQUIRED_FLAGS "-Werror=incompatible-pointer-types")
+check_c_source_compiles(
     "
     #include <signal.h>
     int main(void)
