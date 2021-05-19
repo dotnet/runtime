be990106be3529093d5dc081c0362c8f0dbd5d35
Steve Harter <steveharter@users.noreply.github.com>
2021-05-19 18:09:18 -0500

    Remove ILLink.Suppressions.xml

--- src/libraries/System.Text.Json/src/System/Text/Json/Nodes/JsonValueNotTrimmable.cs
+++ src/libraries/System.Text.Json/src/System/Text/Json/Nodes/JsonValueNotTrimmable.cs
@@ -1,6 +1,8 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics.CodeAnalysis;
+
 namespace System.Text.Json.Nodes
 {
     /// <summary>
@@ -8,10 +10,12 @@ namespace System.Text.Json.Nodes
     /// </summary>
     internal sealed partial class JsonValueNotTrimmable<TValue> : JsonValue<TValue>
     {
-        public JsonValueNotTrimmable(TValue value, JsonNodeOptions? options = null) : base(value, options)
-        {
-        }
+        public JsonValueNotTrimmable(TValue value, JsonNodeOptions? options = null) : base(value, options) { }
 
+        [UnconditionalSuppressMessage("ReflectionAnalysis", "IL2026:RequiresUnreferencedCode",
+            Justification = "The methods used to create this JsonValue are marked RequiresUnreferencedCode.")]
+        [UnconditionalSuppressMessage("ReflectionAnalysis", "IL2091:UnrecognizedReflectionPattern",
+            Justification = "The methods used to create this JsonValue are marked RequiresUnreferencedCode.")]
         public override void WriteTo(Utf8JsonWriter writer, JsonSerializerOptions? options = null)
         {
             if (writer == null)
