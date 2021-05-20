806376da9e790b9bc2dec94ee07550358a5862ba
Filip Navara <navara@emclient.com>
2021-05-20 08:10:01 +0200

    Add UnsupportedOSPlatform attributes

--- src/libraries/System.Security.Cryptography.Algorithms/src/System/Security/Cryptography/DSA.Create.NotSupported.cs
+++ src/libraries/System.Security.Cryptography.Algorithms/src/System/Security/Cryptography/DSA.Create.NotSupported.cs
@@ -1,10 +1,15 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Runtime.Versioning;
+
 namespace System.Security.Cryptography
 {
     public partial class DSA : AsymmetricAlgorithm
     {
+        [UnsupportedOSPlatform("ios")]
+        [UnsupportedOSPlatform("tvos")]
+        [UnsupportedOSPlatform("maccatalyst")]
         public static new DSA Create()
         {
             throw new PlatformNotSupportedException();
