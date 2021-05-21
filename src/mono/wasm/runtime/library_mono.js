90be5a766ea02336e9da9124b94a5d4c63057b97
Katelyn Gadd <kg@luminance.org>
2021-05-20 17:24:05 -0700

    Checkpoint

--- src/mono/wasm/runtime/library_mono.js
+++ src/mono/wasm/runtime/library_mono.js
@@ -1580,6 +1580,7 @@ var MonoSupportLib = {
 			cmt["System.Uri"] = "System.Runtime.InteropServices.JavaScript.UriMarshaler";
 			cmt["System.DateTime"] = "System.Runtime.InteropServices.JavaScript.DateTimeMarshaler";
 			cmt["System.DateTimeOffset"] = "System.Runtime.InteropServices.JavaScript.DateTimeOffsetMarshaler";
+			cmt["Sample.CustomClass"] = "Sample.CustomClassMarshaler";
 		},
 
 		_get_fetch_file_cb_from_args: function (args) {
