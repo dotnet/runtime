227847cb6b1ea703d7f4ac27d30422245022df7a
Steve Pfister <steve@Steves-M1.fios-router.home>
2021-05-20 20:51:09 -0400

    Incorporate @lambdageek's changes and identify native libraries to skip during System.Diagnostics.FileVersionInfo test run

--- src/mono/mono/utils/mono-mmap.c
+++ src/mono/mono/utils/mono-mmap.c
@@ -305,7 +305,7 @@ mono_valloc (void *addr, size_t length, int flags, MonoMemAccountType type)
 		}
 		if ((flags & MONO_MMAP_JIT) && (use_mmap_jit || is_hardened_runtime == 1))
 			mflags |= MAP_JIT;
-#if defined(HOST_ARM64)
+#if defined(HOST_ARM64) && !defined(HOST_MACCAT)
 		/* Patching code on apple silicon seems to cause random crashes without this flag */
 		/* No __builtin_available in old versions of Xcode that could be building Mono on x86 or amd64 */
 		if (__builtin_available (macOS 11, *))
