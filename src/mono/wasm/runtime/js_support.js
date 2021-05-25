5b24b9711a0733a49c7a991ce1a50ce947cfd097
Daniel-Genkin <genkind@gmail.com>
2021-05-25 12:43:15 -0400

    Fixed tests as config is loaded async now

--- src/mono/wasm/runtime/js_support.js
+++ src/mono/wasm/runtime/js_support.js
@@ -1,32 +1,25 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+// THIS FILE IS COPIED DIRECTLY INTO THE DOTNET.JS FILE WITHOUT BEING RUN OR COMPILED/OPTIMZIED
+// IT I MEANT AS A SET OF JS TOOLS TO SUPPORT THE SDK
+
 var JSSupportLib = {
-    // $JS__postset: 'JS.export_functions (Module);',
-    // $JS: {
-        load_config: function (callback) {
-            if (ENVIRONMENT_IS_NODE){
-                const config = require('./mono-config.json');
+    // Loads the config file located in the root of the project
+    load_config: function (callback) {
+        if (ENVIRONMENT_IS_NODE){
+            const config = require('./mono-config.json');
+            callback(config);
+        }
+        var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+        xobj.open('GET', './mono-config.json', true);
+        xobj.onreadystatechange = function () {
+            if (xobj.readyState == 4 && xobj.status == "200") {
+                const config = JSON.parse(xobj.responseText);
                 callback(config);
             }
-            var xobj = new XMLHttpRequest();
-            xobj.overrideMimeType("application/json");
-            xobj.open('GET', './mono-config.json', true);
-            xobj.onreadystatechange = function () {
-                if (xobj.readyState == 4 && xobj.status == "200") {
-                    const config = JSON.parse(xobj.responseText);
-                    callback(config);
-                }
-            };
-            xobj.send();  
-        },
-
-    //     export_functions: function (module) {
-	// 		module ["load_config"] = JS.load_config.bind(JS);
-            
-    //     }
-    // },
+        };
+        xobj.send();  
+    },
 }
-
-// autoAddDeps(JSSupportLib, '$JS')
-// mergeInto(LibraryManager.library, JSSupportLib) // TODO FOR SOME REASON THE LOAD_CONFIG FUNCTION IS NOT BEING ADDED TO DOTNET.JS. THIS IS NEEDED SO THAT I CAN CALL LOAD_CONFIG() IN THE SAMPLES
