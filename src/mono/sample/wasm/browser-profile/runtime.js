c3d996dbe233934a293ca291ac5a26e49a409650
Daniel-Genkin <genkind@gmail.com>
2021-05-25 12:43:15 -0400

    Fixed tests as config is loaded async now

--- src/mono/sample/wasm/browser-profile/runtime.js
+++ src/mono/sample/wasm/browser-profile/runtime.js
@@ -1,16 +1,18 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 var Module = { 
+    is_testing: false,
+
     onRuntimeInitialized: function () {
-        JSSupportLib.load_config(this.setupConfig);
+        JSSupportLib.load_config(Module.setupConfig);
     },
 
     setupConfig: function (config) {
         config.loaded_cb = function () {
             try {
-                App.init ();
+                Module.init();
             } catch (error) {
-                test_exit(1);
+                Module.test_exit(1);
                 throw (error);
             }
         };
@@ -30,8 +32,61 @@ var Module = {
         {
             MONO.mono_load_runtime_and_bcl_args (config);
         } catch (error) {
-            test_exit(1);
+            Module.test_exit(1);
             throw(error);
         }
     },
+
+    init: function () {
+        console.log("not ready yet")
+        var ret = BINDING.call_static_method("[Wasm.BrowserProfile.Sample] Sample.Test:TestMeaning", []);
+        document.getElementById("out").innerHTML = ret;
+        console.log ("ready");
+
+        if (Module.is_testing)
+        {
+          console.debug(`ret: ${ret}`);
+          let exit_code = ret == 42 ? 0 : 1;
+          Module.test_exit(exit_code);
+        }
+
+        if (config.enable_profiler) {
+          BINDING.call_static_method("[Wasm.BrowserProfile.Sample] Sample.Test:StopProfile", []);
+          Module.saveProfile();
+        }
+    },
+
+    onLoad: function() {
+        var url = new URL(decodeURI(window.location));
+        let args = url.searchParams.getAll('arg');
+        is_testing = args !== undefined && (args.find(arg => arg == '--testing') !== undefined);
+    },
+  
+    test_exit: function(exit_code) {
+        if (!Module.is_testing) {
+            console.log(`test_exit: ${exit_code}`);
+            return;
+        }
+
+        /* Set result in a tests_done element, to be read by xharness */
+        var tests_done_elem = document.createElement("label");
+        tests_done_elem.id = "tests_done";
+        tests_done_elem.innerHTML = exit_code.toString();
+        document.body.appendChild(tests_done_elem);
+
+        console.log(`WASM EXIT ${exit_code}`);
+    },
+  
+    saveProfile: function () {
+        var a = document.createElement('a');
+        var blob = new Blob([Module.aot_profile_data]);
+        a.href = URL.createObjectURL(blob);
+        a.download = "data.aotprofile";
+        // Append anchor to body.
+        document.body.appendChild(a);
+        a.click();
+
+        // Remove anchor from body
+        document.body.removeChild(a);
+    }
 };
