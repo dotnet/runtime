9e8af0130fa33fa717ceeffc86050722a28075dd
Daniel-Genkin <genkind@gmail.com>
2021-05-26 18:20:13 -0400

    changed post-js to pre-js

--- src/mono/sample/wasm/browser/runtime.js
+++ src/mono/sample/wasm/browser/runtime.js
@@ -2,20 +2,28 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 var Module = { 
-    // Called when the runtime is initialized and wasm is ready
-    onRuntimeInitialized: function () {
-        JSSupportLib.load_config(this.onConfigLoaded);
-    },
 
-    // Called once the config file is loaded. THe contents of the config file
+    config: null,
+
+    // Called once the config file is loaded. The contents of the config file
     // are passed as a JS object within the config parameter
     onConfigLoaded: function (config) {
         if (!config || config.error){
-            alert("An error occured while loading the config file");
+            console.log("An error occured while loading the config file");
             return;
         }
 
-        config.loaded_cb = function () {
+        Module.config = config;
+    },
+
+    // Called when the runtime is initialized and wasm is ready
+    onRuntimeInitialized: function () {
+        if (!Module.config || Module.config.error){
+            alert("No config found");
+            return;
+        }
+
+        Module.config.loaded_cb = function () {
             try {
                 App.init ();
             } catch (error) {
@@ -23,13 +31,13 @@ var Module = {
                 throw (error);
             }
         };
-        config.fetch_file_cb = function (asset) {
+        Module.config.fetch_file_cb = function (asset) {
             return fetch (asset, { credentials: 'same-origin' });
         }
 
         try
         {
-            MONO.mono_load_runtime_and_bcl_args (config);
+            MONO.mono_load_runtime_and_bcl_args (Module.config);
         } catch (error) {
             test_exit(1);
             throw(error);
--- src/mono/wasm/runtime/js_support.js
+++ src/mono/wasm/runtime/js_support.js
@@ -4,44 +4,46 @@
 // THIS FILE IS COPIED DIRECTLY INTO THE DOTNET.JS FILE WITHOUT BEING RUN OR COMPILED/OPTIMIZED
 // IT I MEANT AS A SET OF JS TOOLS TO SUPPORT THE SDK
 
-var JSSupportLib = {
-    // Loads the config file located in the root of the project
-    load_config: function (callback) {
-        if (ENVIRONMENT_IS_NODE){
-            const config = require('./mono-config.json');
-            if (callback){
-                callback(config);
-            }
-        } else {
-            var xobj = new XMLHttpRequest();
-            xobj.overrideMimeType("application/json");
-            xobj.open('GET', './mono-config.json', true);
-            xobj.onreadystatechange = function() {
-                if (callback && xobj.readyState == XMLHttpRequest.DONE) {
-                    if (xobj.status === 0 || (xobj.status >= 200 && xobj.status < 400)) {
-                        const config = JSON.parse(xobj.responseText);
-                        callback(config);
-                    } else {
-                        // error if the request to load the file was successful but loading failed
-                        callback({error: "Error loading mono-config.json file from current directory"});
-                    }
-                }
-            };
-            xobj.onerror = function() {
-                // error if the request failed
-                if (callback){ 
-                    callback({error: "Error loading mono-config.json file from current directory"});
-                }
-            }
+// Loads the config file located in the root of the project
+// Not meant to be used outside of this class (TODO make private to this file when project converted to TS)
+function load_config() {
+    callback = Module['onConfigLoaded']
 
-            try {
-                xobj.send()
-            } catch(e) {
-                // other kinds of errors
-                if (callback){ 
+    if (ENVIRONMENT_IS_NODE){
+        const config = require('./mono-config.json');
+        if (callback){
+            callback(config);
+        }
+    } else {
+        var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+        xobj.open('GET', './mono-config.json', true);
+        xobj.onreadystatechange = function() {
+            if (callback && xobj.readyState == XMLHttpRequest.DONE) {
+                if (xobj.status === 0 || (xobj.status >= 200 && xobj.status < 400)) {
+                    const config = JSON.parse(xobj.responseText);
+                    callback(config);
+                } else {
+                    // error if the request to load the file was successful but loading failed
                     callback({error: "Error loading mono-config.json file from current directory"});
                 }
+            }
+        };
+        xobj.onerror = function() {
+            // error if the request failed
+            if (callback){ 
+                callback({error: "Error loading mono-config.json file from current directory"});
             }
         }
-    },
+
+        try {
+            xobj.send()
+        } catch(e) {
+            // other kinds of errors
+            if (callback){ 
+                callback({error: "Error loading mono-config.json file from current directory"});
+            }
+        }
+    }
 }
+load_config()
