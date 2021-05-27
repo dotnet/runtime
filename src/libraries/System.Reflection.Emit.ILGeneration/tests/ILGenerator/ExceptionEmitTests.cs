b3ceed5e7b6580aed5ae0218c41fbbf839c4058c
Steve Pfister <steve.pfister@microsoft.com>
2021-05-27 16:05:53 -0400

    Move ConditionalClass up to the right level

--- src/libraries/System.Reflection.Emit.ILGeneration/tests/ILGenerator/ExceptionEmitTests.cs
+++ src/libraries/System.Reflection.Emit.ILGeneration/tests/ILGenerator/ExceptionEmitTests.cs
@@ -5,9 +5,9 @@ using Xunit;
 
 namespace System.Reflection.Emit.Tests
 {
+    [ConditionalClass(typeof(PlatformDetection), nameof(PlatformDetection.IsNotDeviceAOT))]
     public class ExceptionEmitTests
     {
-        [ConditionalClass(typeof(PlatformDetection), nameof(PlatformDetection.IsNotDeviceAOT))]
         public void TestExceptionEmitCalls()
         {
             TypeBuilder type = Helpers.DynamicType(TypeAttributes.NotPublic);
