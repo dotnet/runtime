9c93d7a377d1273f67e425ce48aa0fcc81028f69
Daniel-Genkin <genkind@gmail.com>
2021-05-27 11:39:34 -0400

    Fixed console sample

--- src/mono/wasm/runtime/js_support.js
+++ src/mono/wasm/runtime/js_support.js
@@ -7,21 +7,29 @@
 // Loads the config file located in the root of the project
 // Not meant to be used outside of this class (TODO make private to this file when project converted to TS)
 function load_config() {
-    callback = Module['onConfigLoaded']
+    // since this file loads before emsdk we don't have environment vars yet, so we define them locally
+    var ENVIRONMENT_IS_NODE = typeof process === 'object';
+    var ENVIRONMENT_IS_WEB = typeof window === 'object';
+    // var ENVIRONMENT_IS_WORKER = typeof importScripts === 'function';
+    // var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIRONMENT_IS_WORKER;
+
+    callback = Module['onConfigLoaded'];
 
     if (ENVIRONMENT_IS_NODE){
-        const config = require('./mono-config.json');
-        if (callback){
+        try {
+            var config = JSON.parse(require('./mono-config.json'));
             callback(config);
+        } catch(e) {
+            callback({error: "Error loading mono-config.json file from current directory"});
         }
-    } else {
+    } else if (ENVIRONMENT_IS_WEB){
         var xobj = new XMLHttpRequest();
         xobj.overrideMimeType("application/json");
         xobj.open('GET', './mono-config.json', true);
         xobj.onreadystatechange = function() {
-            if (callback && xobj.readyState == XMLHttpRequest.DONE) {
+            if (xobj.readyState == XMLHttpRequest.DONE) {
                 if (xobj.status === 0 || (xobj.status >= 200 && xobj.status < 400)) {
-                    const config = JSON.parse(xobj.responseText);
+                    var config = JSON.parse(xobj.responseText);
                     callback(config);
                 } else {
                     // error if the request to load the file was successful but loading failed
@@ -31,19 +39,22 @@ function load_config() {
         };
         xobj.onerror = function() {
             // error if the request failed
-            if (callback){ 
-                callback({error: "Error loading mono-config.json file from current directory"});
-            }
+            callback({error: "Error loading mono-config.json file from current directory"});
         }
 
         try {
-            xobj.send()
+            xobj.send();
         } catch(e) {
             // other kinds of errors
-            if (callback){ 
-                callback({error: "Error loading mono-config.json file from current directory"});
-            }
+            callback({error: "Error loading mono-config.json file from current directory"});
+        }
+    } else {
+        try {
+            var config = JSON.parse(load("./mono-config.json"));
+            callback(config);
+        } catch(e) {
+            callback({error: "Error loading mono-config.json file from current directory"});
         }
     }
 }
-load_config()
+load_config();
