5a5a3e6fe15480300faff69ae9080a92efb16491
EgorBo <egorbo@gmail.com>
2021-06-03 17:59:47 +0300

    Fix FinalizeXml

--- src/coreclr/jit/compiler.cpp
+++ src/coreclr/jit/compiler.cpp
@@ -1466,7 +1466,16 @@ void Compiler::compShutdown()
 
 #if defined(DEBUG) || defined(INLINE_DATA)
     // Finish reading and/or writing inline xml
-    InlineStrategy::FinalizeXml();
+    FILE* file = _wfopen(JitConfig.JitInlineDumpXmlFile(), W("a"));
+    if (file != nullptr)
+    {
+        InlineStrategy::FinalizeXml(file);
+        fflush(file);
+    }
+    else
+    {
+        InlineStrategy::FinalizeXml();
+    }
 #endif // defined(DEBUG) || defined(INLINE_DATA)
 
 #if defined(DEBUG) || MEASURE_NODE_SIZE || MEASURE_BLOCK_SIZE || DISPLAY_SIZES || CALL_ARG_STATS
