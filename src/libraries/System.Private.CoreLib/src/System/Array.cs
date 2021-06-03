fece10f5aecceb0c956b816a371d09eb5cda3d79
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-06-03 20:52:30 +0800

    Mention SZArray in MaxLength docs.

--- src/libraries/System.Private.CoreLib/src/System/Array.cs
+++ src/libraries/System.Private.CoreLib/src/System/Array.cs
@@ -1936,9 +1936,11 @@ namespace System
         /// <summary>Gets the maximum number of elements that may be contained in an array.</summary>
         /// <returns>The maximum count of elements allowed in any array.</returns>
         /// <remarks>
-        /// This property represents a runtime limitation, the maximum number of elements (not bytes)
+        /// <para>This property represents a runtime limitation, the maximum number of elements (not bytes)
         /// the runtime will allow in an array. There is no guarantee that an allocation under this length
-        /// will succeed, but all attempts to allocate a larger array will fail.
+        /// will succeed, but all attempts to allocate a larger array will fail.</para>
+        /// <para>This property only applies to single-dimension, zero-bound (SZ) arrays.
+        /// <see cref="Length"/> property may return larger value than this property for multi-dimensional arrays.</para>
         /// </remarks>
         public static int MaxLength =>
             // Keep in sync with `inline SIZE_T MaxArrayLength()` from gchelpers and HashHelpers.MaxPrimeArrayLength.
