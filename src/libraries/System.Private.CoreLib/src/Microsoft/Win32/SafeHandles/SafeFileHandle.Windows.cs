b1d300626f9caa446d7a257f0b7fa5096a61b257
Adam Sitnik <adam.sitnik@gmail.com>
2021-06-07 17:13:45 +0200

    move the call to Dispose to the catch block

--- src/libraries/System.Private.CoreLib/src/Microsoft/Win32/SafeHandles/SafeFileHandle.Windows.cs
+++ src/libraries/System.Private.CoreLib/src/Microsoft/Win32/SafeHandles/SafeFileHandle.Windows.cs
@@ -117,15 +117,10 @@ namespace Microsoft.Win32.SafeHandles
                 }
                 catch (ArgumentException ex)
                 {
+                    // We should close the handle so that the handle is not open until SafeFileHandle GC
+                    Dispose();
+
                     throw new IOException(SR.IO_BindHandleFailed, ex);
-                }
-                finally
-                {
-                    if (ThreadPoolBinding == null)
-                    {
-                        // We should close the handle so that the handle is not open until SafeFileHandle GC
-                        Dispose();
-                    }
                 }
             }
         }
