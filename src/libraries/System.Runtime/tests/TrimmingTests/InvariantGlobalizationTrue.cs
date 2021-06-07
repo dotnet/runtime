c0b146a7186bf50dc0978df5c27fcdd7ba24d3d0
Eric Erhardt <eric.erhardt@microsoft.com>
2021-06-07 18:48:02 -0500

    Respond to PR feedback.
    - Split the substitutions of GlobalizationMode.Invariant between true and false
    - Add a trimming test that ensures Invariant=true trims everything it is supposed to

--- src/libraries/System.Runtime/tests/TrimmingTests/InvariantGlobalizationTrue.cs
+++ src/libraries/System.Runtime/tests/TrimmingTests/InvariantGlobalizationTrue.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Globalization;
+using System.Reflection;
 using System.Threading;
 
 /// <summary>
@@ -21,6 +22,34 @@ class Program
             return -1;
         }
 
+        // Ensure the internal GlobalizationMode class is trimmed correctly
+        Type globalizationMode = GetCoreLibType("System.Globalization.GlobalizationMode");
+        const BindingFlags allStatics = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static;
+        foreach (MemberInfo member in globalizationMode.GetMembers(allStatics))
+        {
+            // properties and their backing getter methods are OK
+            if (member is PropertyInfo || member.Name.StartsWith("get_"))
+            {
+                continue;
+            }
+
+            if (OperatingSystem.IsWindows())
+            {
+                // Windows still contains a static cctor and a backing field for UseNls
+                if (member is ConstructorInfo || (member is FieldInfo field && field.Name.Contains("UseNls")))
+                {
+                    continue;
+                }
+            }
+
+            // Some unexpected member was left on GlobalizationMode, fail
+            Console.WriteLine($"Member '{member.Name}' was not trimmed from GlobalizationMode, but should have been.");
+            return -2;
+        }
+
         return 100;
     }
+
+    private static Type GetCoreLibType(string name) =>
+        typeof(object).Assembly.GetType(name, throwOnError: true);
 }
