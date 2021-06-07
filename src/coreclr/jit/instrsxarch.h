d373963317eb0b4c8bc0d6a211b89f064ec446fd
Kunal Pathak <Kunal.Pathak@microsoft.com>
2021-06-07 03:02:19 -0700

    minor fixup

--- src/coreclr/jit/instrsxarch.h
+++ src/coreclr/jit/instrsxarch.h
@@ -115,7 +115,7 @@ INST3(cmovle,           "cmovle",           IUM_WR, BAD_CODE,     BAD_CODE,     
 INST3(cmovg,            "cmovg",            IUM_WR, BAD_CODE,     BAD_CODE,     0x0F004F,                                Reads_OF       | Reads_SF      | Reads_ZF                                                      )
 
 INST3(xchg,             "xchg",             IUM_RW, 0x000086,     BAD_CODE,     0x000086,                                INS_FLAGS_None )
-INST3(imul,             "imul",             IUM_RW, 0x0F00AC,     BAD_CODE,     0x0F00AF,                                Writes_OF                                                                      | Writes_CF     )
+INST3(imul,             "imul",             IUM_RW, 0x0F00AC,     BAD_CODE,     0x0F00AF,                                Writes_OF      | Undefined_SF  | Undefined_ZF  | Undefined_AF  | Undefined_PF  | Writes_CF     )
 
 //    id                nm                  um      mr            mi            rm                                       flags
 
