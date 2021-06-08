3c90adc8b144a4e0703ced87a72c6fdc3321e504
Elinor Fung <elfung@microsoft.com>
2021-06-08 11:08:27 -0700

    Use GeneratedDllImport in System.Security.Principal.Windows

--- src/libraries/Common/src/Interop/Windows/Advapi32/Interop.CreateWellKnownSid.cs
+++ src/libraries/Common/src/Interop/Windows/Advapi32/Interop.CreateWellKnownSid.cs
@@ -8,7 +8,16 @@ internal static partial class Interop
 {
     internal static partial class Advapi32
     {
+#if DLLIMPORTGENERATOR_ENABLED
+        [GeneratedDllImport(Interop.Libraries.Advapi32, EntryPoint = "CreateWellKnownSid", SetLastError = true, CharSet = CharSet.Unicode)]
+        internal static partial int CreateWellKnownSid(
+#else
         [DllImport(Interop.Libraries.Advapi32, EntryPoint = "CreateWellKnownSid", SetLastError = true, CharSet = CharSet.Unicode)]
-        internal static extern int CreateWellKnownSid(int sidType, byte[]? domainSid, byte[] resultSid, ref uint resultSidLength);
+        internal static extern int CreateWellKnownSid(
+#endif
+            int sidType,
+            byte[]? domainSid,
+            byte[] resultSid,
+            ref uint resultSidLength);
     }
 }
--- src/libraries/Common/src/Interop/Windows/Advapi32/Interop.RevertToSelf.cs
+++ src/libraries/Common/src/Interop/Windows/Advapi32/Interop.RevertToSelf.cs
@@ -8,7 +8,12 @@ internal static partial class Interop
 {
     internal static partial class Advapi32
     {
+#if DLLIMPORTGENERATOR_ENABLED
+        [GeneratedDllImport(Interop.Libraries.Advapi32, SetLastError = true, ExactSpelling = true, CharSet = CharSet.Unicode)]
+        internal static partial bool RevertToSelf();
+#else
         [DllImport(Interop.Libraries.Advapi32, SetLastError = true, ExactSpelling = true, CharSet = CharSet.Unicode)]
         internal static extern bool RevertToSelf();
+#endif
     }
 }
