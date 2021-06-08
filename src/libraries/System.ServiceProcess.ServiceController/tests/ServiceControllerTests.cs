14e0f9622a18c0dc3bb5dee54d67408f807ef600
Fs00 <francescosaltori@gmail.com>
2021-06-08 09:28:21 +0200

    Run tests for the new Stop(bool) overload only on .NETCoreApp

--- src/libraries/System.ServiceProcess.ServiceController/tests/ServiceControllerTests.cs
+++ src/libraries/System.ServiceProcess.ServiceController/tests/ServiceControllerTests.cs
@@ -7,7 +7,7 @@ namespace System.ServiceProcess.Tests
 {
     [OuterLoop(/* Modifies machine state */)]
     [SkipOnTargetFramework(TargetFrameworkMonikers.NetFramework, "Persistent issues starting test service on NETFX")]
-    public class ServiceControllerTests : IDisposable
+    public partial class ServiceControllerTests : IDisposable
     {
         private const int connectionTimeout = 30000;
         private readonly TestServiceProvider _testService;
@@ -76,27 +76,6 @@ namespace System.ServiceProcess.Tests
         {
             var controller = new ServiceController(_testService.TestServiceName);
             Assert.Throws<ArgumentNullException>(() => controller.Start(new string[] { null } ));
-        }
-
-        [ConditionalFact(nameof(IsProcessElevated))]
-        public void Stop_FalseArg_WithDependentServices_ThrowsInvalidOperationException()
-        {
-            var controller = new ServiceController(_testService.TestServiceName);
-            controller.WaitForStatus(ServiceControllerStatus.Running, _testService.ControlTimeout);
-            Assert.Throws<InvalidOperationException>(() => controller.Stop(stopDependentServices: false));
-        }
-
-        [ConditionalFact(nameof(IsProcessElevated))]
-        public void Stop_TrueArg_WithDependentServices_StopsTheServiceAndItsDependents()
-        {
-            var controller = new ServiceController(_testService.TestServiceName);
-            controller.WaitForStatus(ServiceControllerStatus.Running, _testService.ControlTimeout);
-
-            controller.Stop(stopDependentServices: true);
-            controller.WaitForStatus(ServiceControllerStatus.Stopped, _testService.ControlTimeout);
-
-            Assert.Equal(ServiceControllerStatus.Stopped, controller.Status);
-            Assert.All(controller.DependentServices, service => Assert.Equal(ServiceControllerStatus.Stopped, service.Status));
         }
 
         [ConditionalFact(nameof(IsProcessElevated))]
