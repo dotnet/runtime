f8e0bcf058c03aedbc166a7e6daec94b3d8849d5
Adam Sitnik <adam.sitnik@gmail.com>
2021-06-08 10:59:24 +0200

    add missing docs

--- src/libraries/System.Private.CoreLib/src/System/IO/File.netcoreapp.cs
+++ src/libraries/System.Private.CoreLib/src/System/IO/File.netcoreapp.cs
@@ -5,6 +5,10 @@ namespace System.IO
 {
     public static partial class File
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="System.IO.FileStream" /> class with the specified path, creation mode, read/write and sharing permission, the access other FileStreams can have to the same file, the buffer size, additional file options and the allocation size.
+        /// </summary>
+        /// <remarks><see cref="System.IO.FileStream(string,System.IO.FileStreamOptions)"/> for information about exceptions.</remarks>
         public static FileStream Open(string path, FileStreamOptions options) => new FileStream(path, options);
     }
 }
