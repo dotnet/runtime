d4c538329ffe1db94e2af570e4559bb34741e438
Fs00 <francescosaltori@gmail.com>
2021-06-09 09:35:22 +0200

    Add a test for manually stopping a service and its dependents with Stop(false)

--- src/libraries/System.ServiceProcess.ServiceController/tests/ServiceControllerTests.netcoreapp.cs
+++ src/libraries/System.ServiceProcess.ServiceController/tests/ServiceControllerTests.netcoreapp.cs
@@ -28,5 +28,23 @@ namespace System.ServiceProcess.Tests
             Assert.Equal(ServiceControllerStatus.Stopped, controller.Status);
             Assert.All(controller.DependentServices, service => Assert.Equal(ServiceControllerStatus.Stopped, service.Status));
         }
+
+        [ConditionalFact(nameof(IsProcessElevated))]
+        public void StopTheServiceAndItsDependentsManually()
+        {
+            var controller = new ServiceController(_testService.TestServiceName);
+            controller.WaitForStatus(ServiceControllerStatus.Running, _testService.ControlTimeout);
+
+            foreach (var dependentService in controller.DependentServices)
+            {
+                dependentService.Stop(stopDependentServices: false);
+                dependentService.WaitForStatus(ServiceControllerStatus.Stopped, _testService.ControlTimeout);
+            }
+            controller.Stop(stopDependentServices: false);
+            controller.WaitForStatus(ServiceControllerStatus.Stopped, _testService.ControlTimeout);
+
+            Assert.Equal(ServiceControllerStatus.Stopped, controller.Status);
+            Assert.All(controller.DependentServices, service => Assert.Equal(ServiceControllerStatus.Stopped, service.Status));
+        }
     }
 }
