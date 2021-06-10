2993789afa3d22e5c85c07e8b05f746f03cc8708
Alexander Köplinger <alex.koeplinger@outlook.com>
2021-06-10 14:39:22 +0200

    Return false for SocketsHttpHandler.IsSupported on Browser (#53926)
    We were returning true even though sockets don't work on Browser.
    
    Co-authored-by: Marie Píchová <11718369+ManickaP@users.noreply.github.com>

--- src/libraries/System.Net.Http/src/System/Net/Http/SocketsHttpHandler/SocketsHttpHandler.cs
+++ src/libraries/System.Net.Http/src/System/Net/Http/SocketsHttpHandler/SocketsHttpHandler.cs
@@ -43,7 +43,7 @@ namespace System.Net.Http
         /// Gets a value that indicates whether the handler is supported on the current platform.
         /// </summary>
         [UnsupportedOSPlatformGuard("browser")]
-        public static bool IsSupported => true;
+        public static bool IsSupported => !OperatingSystem.IsBrowser();
 
         public bool UseCookies
         {
