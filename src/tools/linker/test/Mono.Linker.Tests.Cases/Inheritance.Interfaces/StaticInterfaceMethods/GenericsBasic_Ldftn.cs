3569b99a637135223b6f824ff92bdc407715547b
Lakshan Fernando <lakshanf@microsoft.com>
2021-06-10 06:03:27 -0700

    Initial work to support virtual static interface methods (dotnet/linker#2072)
    * Initial work to support static interface methods
    
    * commit to allow Tlakaelel help me with the il test
    
    * support for the runtime il test
    
    * Incorporating feedback
    
    * reverting a file that was checked in by mistake
    
    * test updates
    
    * generics tests restructuring
    
    * tests
    
    * fixed tests
    
    * added a comment to clarify
    
    * FB
    
    Commit migrated from https://github.com/dotnet/linker/commit/3fc88d44d239fa0f6dfc772ee2474c8a94304da5

---
+++ src/tools/linker/test/Mono.Linker.Tests.Cases/Inheritance.Interfaces/StaticInterfaceMethods/GenericsBasic_Ldftn.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
+using Mono.Linker.Tests.Cases.Expectations.Metadata;
+
+namespace Mono.Linker.Tests.Cases.Inheritance.Interfaces.StaticInterfaceMethods
+{
+	[SetupLinkerArgument ("--skip-unresolved", "true")]
+#if !NETCOREAPP
+	[IgnoreTestCase("Only for .NET Core for some reason")]
+#endif
+	[Define ("IL_ASSEMBLY_AVAILABLE")]
+	[SetupCompileBefore ("library.dll", new[] { "Dependencies/GenericsBasic_Ldftn.il" })]
+	[KeptTypeInAssembly ("library.dll", "IFaceNonGeneric")]
+	[KeptMemberInAssembly ("library.dll", "NonGenericClass", "IFaceNonGeneric.NormalMethod()", "IFaceNonGeneric.GenericMethod<#1>()")]
+
+	class GenericsBasic_Ldftn
+	{
+		static void Main ()
+		{
+#if IL_ASSEMBLY_AVAILABLE
+			TestEntrypoint.Test();
+#endif
+		}
+	}
+}
