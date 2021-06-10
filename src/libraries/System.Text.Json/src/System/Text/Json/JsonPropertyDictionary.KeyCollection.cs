43b1f2c165257faae4d6a4aa886b701b63d2d5c0
Steve Harter <steveharter@users.noreply.github.com>
2021-06-10 11:23:17 -0500

    Add JsonPropertyDictionary and associated refactoring (#53206)

--- src/libraries/System.Text.Json/src/System/Text/Json/JsonPropertyDictionary.KeyCollection.cs
+++ src/libraries/System.Text.Json/src/System/Text/Json/JsonPropertyDictionary.KeyCollection.cs
@@ -4,34 +4,33 @@
 using System.Collections;
 using System.Collections.Generic;
 
-namespace System.Text.Json.Nodes
+namespace System.Text.Json
 {
-    public partial class JsonObject
+    internal partial class JsonPropertyDictionary<T>
     {
         private KeyCollection? _keyCollection;
 
-        private KeyCollection GetKeyCollection(JsonObject jsonObject)
+        public ICollection<string> GetKeyCollection()
         {
-            CreateList();
-            return _keyCollection ??= new KeyCollection(jsonObject);
+            return _keyCollection ??= new KeyCollection(this);
         }
 
         private sealed class KeyCollection : ICollection<string>
         {
-            private readonly JsonObject _jObject;
+            private readonly JsonPropertyDictionary<T> _parent;
 
-            public KeyCollection(JsonObject jsonObject)
+            public KeyCollection(JsonPropertyDictionary<T> jsonObject)
             {
-                _jObject = jsonObject;
+                _parent = jsonObject;
             }
 
-            public int Count => _jObject.Count;
+            public int Count => _parent.Count;
 
             public bool IsReadOnly => true;
 
             IEnumerator IEnumerable.GetEnumerator()
             {
-                foreach (KeyValuePair<string, JsonNode?> item in _jObject)
+                foreach (KeyValuePair<string, T?> item in _parent)
                 {
                     yield return item.Key;
                 }
@@ -39,12 +38,9 @@ namespace System.Text.Json.Nodes
 
             public void Add(string propertyName) => throw ThrowHelper.NotSupportedException_NodeCollectionIsReadOnly();
 
-
             public void Clear() => throw ThrowHelper.NotSupportedException_NodeCollectionIsReadOnly();
 
-
-            public bool Contains(string propertyName) => _jObject.ContainsNode(propertyName);
-
+            public bool Contains(string propertyName) => _parent.ContainsProperty(propertyName);
 
             public void CopyTo(string[] propertyNameArray, int index)
             {
@@ -53,7 +49,7 @@ namespace System.Text.Json.Nodes
                     ThrowHelper.ThrowArgumentOutOfRangeException_NodeArrayIndexNegative(nameof(index));
                 }
 
-                foreach (KeyValuePair<string, JsonNode?> item in _jObject)
+                foreach (KeyValuePair<string, T?> item in _parent)
                 {
                     if (index >= propertyNameArray.Length)
                     {
@@ -66,7 +62,7 @@ namespace System.Text.Json.Nodes
 
             public IEnumerator<string> GetEnumerator()
             {
-                foreach (KeyValuePair<string, JsonNode?> item in _jObject)
+                foreach (KeyValuePair<string, T?> item in _parent)
                 {
                     yield return item.Key;
                 }
