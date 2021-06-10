5eb052935d663df3ab61e9b6ff66d80d925cca2a
Elinor Fung <elfung@microsoft.com>
2021-06-09 20:22:05 -0700

    [Unix] System.IO.MemoryMappedFiles

--- src/libraries/Common/src/Interop/Unix/System.Native/Interop.MSync.cs
+++ src/libraries/Common/src/Interop/Unix/System.Native/Interop.MSync.cs
@@ -16,7 +16,7 @@ internal static partial class Interop
             MS_INVALIDATE = 0x10,
         }
 
-        [DllImport(Libraries.SystemNative, EntryPoint = "SystemNative_MSync", SetLastError = true)]
-        internal static extern int MSync(IntPtr addr, ulong len, MemoryMappedSyncFlags flags);
+        [GeneratedDllImport(Libraries.SystemNative, EntryPoint = "SystemNative_MSync", SetLastError = true)]
+        internal static partial int MSync(IntPtr addr, ulong len, MemoryMappedSyncFlags flags);
     }
 }
