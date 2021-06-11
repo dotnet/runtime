c20edbff0ed88d3e61d306410c6cee0da4584b58
Thays <thaystg@gmail.com>
2021-06-10 22:29:55 -0300

    Fix assertion on debugger-engine Sync to mono/mono #21100

--- src/mono/mono/mini/mini.h
+++ src/mono/mono/mini/mini.h
@@ -2935,6 +2935,9 @@ mono_arch_load_function (MonoJitICallId jit_icall_id);
 MonoGenericContext
 mono_get_generic_context_from_stack_frame (MonoJitInfo *ji, gpointer generic_info);
 
+gpointer
+mono_get_generic_info_from_stack_frame (MonoJitInfo *ji, MonoContext *ctx);
+
 MonoMemoryManager* mini_get_default_mem_manager (void);
 
 #endif /* __MONO_MINI_H__ */
