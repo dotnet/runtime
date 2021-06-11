b556a0e29d959d28f893af3d4a0480c2bd4e0ae2
David Fowler <davidfowl@gmail.com>
2021-06-11 07:47:26 -0700

    Update src/libraries/Microsoft.Extensions.DependencyInjection.Abstractions/src/IServiceProviderIsService.cs
    Co-authored-by: Travis Illig <tillig@paraesthesia.com>

--- src/libraries/Microsoft.Extensions.DependencyInjection.Abstractions/src/IServiceProviderIsService.cs
+++ src/libraries/Microsoft.Extensions.DependencyInjection.Abstractions/src/IServiceProviderIsService.cs
@@ -14,7 +14,7 @@ namespace Microsoft.Extensions.DependencyInjection
         /// Determines if the specified service type is available from the <see cref="IServiceProvider"/>.
         /// </summary>
         /// <param name="serviceType">An object that specifies the type of service object to test.</param>
-        /// <returns>true if the specified service is a resolvable, false if it is not.</returns>
+        /// <returns>true if the specified service is a available, false if it is not.</returns>
         bool IsService(Type serviceType);
     }
 }
