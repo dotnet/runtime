a046f3ec598bddb46a997c2fdb0c9ccf2f8e9bbe
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-06-12 17:18:28 +0800

    No need to special case for invariant culture

--- src/libraries/System.Private.Xml/src/System/Xml/Xsl/XPathConvert.cs
+++ src/libraries/System.Private.Xml/src/System/Xml/Xsl/XPathConvert.cs
@@ -12,27 +12,11 @@ namespace System.Xml.Xsl
     {
         public static string DoubleToString(double dbl)
         {
-            if (double.IsPositiveInfinity(dbl))
-            {
-                return "Infinity";
-            }
-            else if (double.IsNegativeInfinity(dbl))
-            {
-                return "-Infinity";
-            }
-            // NaN is "NaN" in InvariantCulture
-
             return dbl.ToString(CultureInfo.InvariantCulture);
         }
 
         public static double StringToDouble(string s)
         {
-            if (s.Contains("∞"))
-            {
-                // Previous unsafe implementation doesn't accept inf symbol
-                return double.NaN;
-            }
-
             if (double.TryParse(s, NumberStyles.Float, CultureInfo.InvariantCulture, out double result))
             {
                 return result;
