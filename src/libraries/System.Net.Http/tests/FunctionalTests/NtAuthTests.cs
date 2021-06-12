ea4dc615824c207845628143addc7d8c774ce702
Filip Navara <navara@emclient.com>
2021-06-12 12:59:01 +0200

    Use full domain name in test credentials

--- src/libraries/System.Net.Http/tests/FunctionalTests/NtAuthTests.cs
+++ src/libraries/System.Net.Http/tests/FunctionalTests/NtAuthTests.cs
@@ -167,7 +167,7 @@ namespace System.Net.Http.Functional.Tests
         public async Task AuthTest(string authType)
         {
             var testUri = new Uri("http://emclientntlm.westus.cloudapp.azure.com/");
-            var networkCredential = new NetworkCredential("user1", "PLACEHOLDERcorrect20", "emclientntlm");
+            var networkCredential = new NetworkCredential("user1", "PLACEHOLDERcorrect20", "emclientntlm.westus.cloudapp.azure.com");
 
             using var socketsHandler = new SocketsHttpHandler();
             var credentialCache = new CredentialCache();
