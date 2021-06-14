4c47d2adabb1c2d039ba1572a051900c6a41b8b2
Elinor Fung <elfung@microsoft.com>
2021-06-14 14:06:53 -0700

    Use GeneratedDllImport in System.IO.FileSystem.DriveInfo

--- src/libraries/Common/src/Interop/Windows/Kernel32/Interop.GetDiskFreeSpaceEx.cs
+++ src/libraries/Common/src/Interop/Windows/Kernel32/Interop.GetDiskFreeSpaceEx.cs
@@ -9,7 +9,7 @@ internal static partial class Interop
     {
         // NOTE: The out parameters are PULARGE_INTEGERs and may require
         // some byte munging magic.
-        [DllImport(Libraries.Kernel32, EntryPoint = "GetDiskFreeSpaceExW", CharSet = CharSet.Unicode, SetLastError = true, BestFitMapping = false)]
-        internal static extern bool GetDiskFreeSpaceEx(string drive, out long freeBytesForUser, out long totalBytes, out long freeBytes);
+        [GeneratedDllImport(Libraries.Kernel32, EntryPoint = "GetDiskFreeSpaceExW", CharSet = CharSet.Unicode, SetLastError = true)]
+        internal static partial bool GetDiskFreeSpaceEx(string drive, out long freeBytesForUser, out long totalBytes, out long freeBytes);
     }
 }
