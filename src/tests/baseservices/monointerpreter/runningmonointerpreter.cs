4a59ba10233dfcf7a998b7daf2d558140b8907a8
Nathan Ricci <naricc@microsoft.com>
2021-06-14 13:59:14 -0400

    Added interpreter test.

---
+++ src/tests/baseservices/monointerpreter/runningmonointerpreter.cs
@@ -0,0 +1,28 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+using System;
+using System.Runtime.CompilerServices;
+
+namespace TestRunningMono
+{
+    class Program
+    {
+        public static int Main(string[] args)
+        {
+             const int Pass = 100, Fail = 1;
+             bool isMono = typeof(object).Assembly.GetType("Mono.RuntimeStructs") != null;
+
+	         bool isInterpreter = RuntimeFeature.IsDynamicCodeSupported == true && RuntimeFeature.IsDynamicCodeCompiled == false;
+
+             if(isMono && isInterpreter)
+             {
+                 return Pass;
+             }
+             else
+             {
+                 return Fail;
+             }
+        }
+   }
+}
+
