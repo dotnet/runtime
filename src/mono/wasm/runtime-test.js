8476110f516bf75a2ec363f8f4bd6005975e1fb9
Daniel-Genkin <genkind@gmail.com>
2021-06-14 14:02:36 -0400

    Fixed trimming tests

--- src/mono/wasm/runtime-test.js
+++ src/mono/wasm/runtime-test.js
@@ -211,7 +211,7 @@ var Module = {
 	printErr,
 
     preInit: async function() {
-        await MONO.mono_wasm_load_config("./mono-config.json");
+        Module.config = await MONO.mono_wasm_load_config("./mono-config.json");
     },
 
 	onAbort: function(x) {
