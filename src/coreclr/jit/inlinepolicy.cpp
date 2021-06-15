d03d9baf6a559389ec629f14d4a7f5d50bc7876d
Egor Bogatov <egorbo@gmail.com>
2021-06-16 00:55:55 +0300

    Update src/coreclr/jit/inlinepolicy.cpp
    Co-authored-by: Andy Ayers <andya@microsoft.com>

--- src/coreclr/jit/inlinepolicy.cpp
+++ src/coreclr/jit/inlinepolicy.cpp
@@ -96,7 +96,7 @@ InlinePolicy* InlinePolicy::GetPolicy(Compiler* compiler, bool isPrejitRoot)
 
     const bool useExtendedDefaultPolicy = JitConfig.JitExtendedDefaultPolicyModel() != 0;
 
-    if (!useExtendedDefaultPolicy || (!isPrejitRoot && compiler->opts.IsReadyToRun()))
+    if (!useExtendedDefaultPolicy || (compiler->opts.jitFlags->IsSet(JitFlags::JIT_FLAG_PREJIT) && !isPrejitRoot))
     {
         // DefaultPolicy is better for AOT in terms of code-size.
         return new (compiler, CMK_Inlining) DefaultPolicy(compiler, isPrejitRoot);
