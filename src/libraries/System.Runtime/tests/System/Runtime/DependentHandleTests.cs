748d88e6378e6c7fab173bd6b95d9cfd471f15cc
Sergio Pedri <sergio0694@live.com>
2021-06-16 02:01:07 +0200

    Add more unit tests for new public DependentHandle APIs

---
+++ src/libraries/System.Runtime/tests/System/Runtime/DependentHandleTests.cs
@@ -0,0 +1,73 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Xunit;
+
+namespace System.Runtime.Tests
+{
+    // NOTE: DependentHandle is already heavily tested indirectly through ConditionalWeakTable<,>.
+    // This class contains some specific tests for APIs that are only relevant when used directly.
+    public class DependentHandleTests
+    {
+        [Fact]
+        public void GetTarget_ThrowsInvalidOperationException()
+        {
+            Assert.Throws<InvalidOperationException>(() => default(DependentHandle).Target);
+        }
+
+        [Fact]
+        public void SetTarget_ThrowsInvalidOperationException()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                DependentHandle handle = default;
+                handle.Target = new();
+            });
+        }
+
+        [Fact]
+        public void GetDependent_ThrowsInvalidOperationException()
+        {
+            Assert.Throws<InvalidOperationException>(() => default(DependentHandle).Dependent);
+        }
+
+        [Fact]
+        public void SetDependent_ThrowsInvalidOperationException()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                DependentHandle handle = default;
+                handle.Dependent = new();
+            });
+        }
+
+        [Fact]
+        public void GetTargetAndDependent_ThrowsInvalidOperationException()
+        {
+            Assert.Throws<InvalidOperationException>(() => default(DependentHandle).GetTargetAndDependent());
+        }
+
+        [Fact]
+        public void Dispose_RepeatedCallsAreFine()
+        {
+            object key = new(), value = new();
+            DependentHandle handle = new(key, value);
+
+            Assert.True(handle.IsAllocated);
+
+            handle.Dispose();
+
+            Assert.False(handle.IsAllocated);
+
+            handle.Dispose();
+
+            Assert.False(handle.IsAllocated);
+
+            handle.Dispose();
+            handle.Dispose();
+            handle.Dispose();
+
+            Assert.False(handle.IsAllocated);
+        }
+    }
+}
