5a780c8a03159dfb8f236a68f368033255eb9049
Daniel-Genkin <genkind@gmail.com>
2021-06-22 21:59:07 -0400

    cleanup accidental spaces and tabs cleanup

--- src/mono/wasm/debugger/tests/debugger-test/runtime-debugger.js
+++ src/mono/wasm/debugger/tests/debugger-test/runtime-debugger.js
@@ -10,23 +10,23 @@ var Module = {
 
     // Called when the runtime is initialized and wasm is ready
     onRuntimeInitialized: function () {
-        if (!Module.config || Module.config.error) {
+		if (!Module.config || Module.config.error) {
             console.log("An error occured while loading the config file");
             return;
         }
 
-        Module.config.loaded_cb = function () {
-            App.init ();
-        };
-        // For custom logging patch the functions below
-        /*
-        MONO.logging = {
-            trace: function (domain, log_level, message, isFatal, dataPtr) {},
-            debugger: function (level, message) {}
-        };
-        MONO.mono_wasm_setenv ("MONO_LOG_LEVEL", "debug");
-        MONO.mono_wasm_setenv ("MONO_LOG_MASK", "all");
-        */
-        MONO.mono_load_runtime_and_bcl_args (Module.config)
-    },
+		Module.config.loaded_cb = function () {
+			App.init ();
+		};
+		// For custom logging patch the functions below
+		/*
+		MONO.logging = {
+			trace: function (domain, log_level, message, isFatal, dataPtr) {},
+			debugger: function (level, message) {}
+		};
+		MONO.mono_wasm_setenv ("MONO_LOG_LEVEL", "debug");
+		MONO.mono_wasm_setenv ("MONO_LOG_MASK", "all");
+		*/
+		MONO.mono_load_runtime_and_bcl_args (Module.config)
+	},
 };
