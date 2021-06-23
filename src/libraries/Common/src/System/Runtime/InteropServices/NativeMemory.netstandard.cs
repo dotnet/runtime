d52bb61f27a93ecb0eca74a9e9ddf05e9a1041a4
reflectronic <john-tur@outlook.com>
2021-06-22 20:05:28 -0400

    fix project files

---
+++ src/libraries/Common/src/System/Runtime/InteropServices/NativeMemory.netstandard.cs
@@ -0,0 +1,29 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace System
+{
+    // Reduced version of NativeMemory for targets older than .NET 6.
+    internal static unsafe class NativeMemory
+    {
+        public static void* Alloc(nuint byteCount)
+        {
+            return (void*)(nint)Marshal.AllocHGlobal((nint)byteCount);
+        }
+
+        public static void* Realloc(void* ptr, nuint byteCount)
+        {
+            return (void*)(nint)Marshal.ReAllocHGlobal((void*)(nint)ptr, (nint)byteCount);
+        }
+
+        public static void Free(void* ptr)
+        {
+            Marshal.FreeHGlobal((nint)ptr);
+        }
+    }
+}
