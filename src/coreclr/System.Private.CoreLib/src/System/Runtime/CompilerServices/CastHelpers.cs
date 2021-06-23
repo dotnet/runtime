0f0e2493cfebd45e81b4e8fe1f90edfd931dadc3
David Wrighton <davidwr@microsoft.com>
2021-06-22 18:39:24 -0700

    Adjust casting for new NonTrivial cases

--- src/coreclr/System.Private.CoreLib/src/System/Runtime/CompilerServices/CastHelpers.cs
+++ src/coreclr/System.Private.CoreLib/src/System/Runtime/CompilerServices/CastHelpers.cs
@@ -257,12 +257,12 @@ namespace System.Runtime.CompilerServices
                 }
 
             extra:
-                if (mt->NonTrivialInterfaceCast)
+//                if (mt->NonTrivialInterfaceCast) Remove the NonTrivialInterfaceCast check for now as the uninstantiated interface being used for the curiously recurring pattern scenario makes more cases NonTrivial
                 {
                     goto slowPath;
                 }
 
-                obj = null;
+//                obj = null;
             }
 
         done:
