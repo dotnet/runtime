15fc697d7e89a51dee188a37b483ec7a0b2351aa
Aleksey Kliger <alklig@microsoft.com>
2021-06-23 16:55:49 -0400

    Change to older version of the runtime.js code

--- src/tests/FunctionalTests/WebAssembly/Browser/HotReload/runtime.js
+++ src/tests/FunctionalTests/WebAssembly/Browser/HotReload/runtime.js
@@ -2,21 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 var Module = { 
-
-    config: null,
-
-    preInit: async function() {
-        Module.config = await MONO.mono_wasm_load_config("./mono-config.json");
-    },
-
     onRuntimeInitialized: function () {
-        if (!Module.config || Module.config.error) {
-            console.log("No config found");
-            test_exit(1);
-            throw(Module.config.error);
-        }
-        
-        Module.config.loaded_cb = function () {
+        config.loaded_cb = function () {
             try {
                 App.init ();
             } catch (error) {
@@ -24,21 +11,21 @@ var Module = {
                 throw (error);
             }
         };
-        Module.config.fetch_file_cb = function (asset) {
+        config.fetch_file_cb = function (asset) {
             return fetch (asset, { credentials: 'same-origin' });
         }
 
-        if (Module.config.environment_variables !== undefined) {
-            console.log ("expected environment variables to be undefined, but they're: ", Module.config.environment_variables);
+        if (config.environment_variables !== undefined) {
+            console.log ("expected environment variables to be undefined, but they're: ", config.environment_variables);
             test_exit(1);
         }
-        Module.config.environment_variables = {
+        config.environment_variables = {
             "DOTNET_MODIFIABLE_ASSEMBLIES": "debug"
         };
 
         try
         {
-            MONO.mono_load_runtime_and_bcl_args (Module.config);
+            MONO.mono_load_runtime_and_bcl_args (config);
         } catch (error) {
             test_exit(1);
             throw(error);
