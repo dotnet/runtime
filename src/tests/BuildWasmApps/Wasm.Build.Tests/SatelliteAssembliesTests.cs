902f8aac6096bdde98fa39de24bef74b5148da32
Daniel Genkin <85504900+Daniel-Genkin-MS@users.noreply.github.com>
2021-06-25 18:42:07 +0000

    work in progress

--- src/tests/BuildWasmApps/Wasm.Build.Tests/SatelliteAssembliesTests.cs
+++ src/tests/BuildWasmApps/Wasm.Build.Tests/SatelliteAssembliesTests.cs
@@ -135,7 +135,7 @@ namespace Wasm.Build.Tests
               <PropertyGroup>
                 <TargetFramework>{s_targetFramework}</TargetFramework>
                 <OutputType>Exe</OutputType>
-                <WasmGenerateRunV8Script>true</WasmGenerateRunV8Script>
+                <WasmGenerateRunScript>true</WasmGenerateRunScript>
                 <WasmMainJSPath>runtime-test.js</WasmMainJSPath>
                 ##EXTRA_PROPERTIES##
               </PropertyGroup>
