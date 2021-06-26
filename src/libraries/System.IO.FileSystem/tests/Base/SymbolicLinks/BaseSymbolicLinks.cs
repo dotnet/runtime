cbcf7af23f79ab8f11d696760da220e15be88765
carlossanlop <carlossanlop@users.noreply.github.com>
2021-06-25 18:28:11 -0700

    Fix CI bug in Windows Nano where %TEMP% points to "C:\Windows\TEMP" but the actual folder name is "C:\Windows\Temp", which prevents us from using Assert.Equal for path comparison.

--- src/libraries/System.IO.FileSystem/tests/Base/SymbolicLinks/BaseSymbolicLinks.cs
+++ src/libraries/System.IO.FileSystem/tests/Base/SymbolicLinks/BaseSymbolicLinks.cs
@@ -1,14 +1,17 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Buffers;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
 using Xunit;
 
 namespace System.IO.Tests
 {
     [ConditionalClass(typeof(BaseSymbolicLinks), nameof(CanCreateSymbolicLinks))]
     // Contains helper methods that are shared by all symbolic link test classes.
-    public abstract class BaseSymbolicLinks : FileSystemTest
+    public abstract partial class BaseSymbolicLinks : FileSystemTest
     {
         protected DirectoryInfo CreateDirectoryContainingSelfReferencingSymbolicLink()
         {
@@ -22,9 +25,10 @@ namespace System.IO.Tests
         protected string GetRandomLinkName() => GetTestFileName() + ".link";
         protected string GetRandomDirName()  => GetTestFileName() + "_dir";
 
-        protected string GetRandomFilePath() => Path.Join(TestDirectory, GetRandomFileName());
-        protected string GetRandomLinkPath() => Path.Join(TestDirectory, GetRandomLinkName());
-        protected string GetRandomDirPath()  => Path.Join(TestDirectory, GetRandomDirName());
+        protected string GetRandomFilePath() => Path.Join(ActualTestDirectory.Value, GetRandomFileName());
+        protected string GetRandomLinkPath() => Path.Join(ActualTestDirectory.Value, GetRandomLinkName());
+        protected string GetRandomDirPath()  => Path.Join(ActualTestDirectory.Value, GetRandomDirName());
 
+        private Lazy<string> ActualTestDirectory => new Lazy<string>(() => GetTestDirectoryActualCasing());
     }
 }
