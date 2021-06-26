767c36de3ec6db2d88d4d974bb25534dd12f4c86
SingleAccretion <AccretionMail@yandex.ru>
2021-06-26 19:10:04 +0300

    Don't use GT_ARR_ELEM as a location
    It represents an address.
    
    No diffs.

--- src/coreclr/jit/assertionprop.cpp
+++ src/coreclr/jit/assertionprop.cpp
@@ -1197,13 +1197,6 @@ AssertionIndex Compiler::optCreateAssertion(GenTree*         op1,
                     toType = op2->gtType;
                     goto SUBRANGE_COMMON;
 
-                case GT_ARR_ELEM:
-
-                    /* Assigning the result of an indirection into a LCL_VAR, see if we can add a subrange assertion */
-
-                    toType = op2->gtType;
-                    goto SUBRANGE_COMMON;
-
                 case GT_LCL_FLD:
 
                     /* Assigning the result of an indirection into a LCL_VAR, see if we can add a subrange assertion */
