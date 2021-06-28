ee51c67cb7d063d0b93a2e22880421b4cd5d26ce
Mitchell Hwang <mitchell.hwang@microsoft.com>
2021-06-28 14:15:38 -0400

    Clean up mono style to match runtime style

--- src/libraries/System.Private.CoreLib/src/System/TimeZoneInfo.Android.cs
+++ src/libraries/System.Private.CoreLib/src/System/TimeZoneInfo.Android.cs
@@ -11,31 +11,31 @@ namespace System
     {
         private const string TimeZoneFileName = "tzdata";
 
-        private static string GetApexTimeDataRoot ()
+        private static string GetApexTimeDataRoot()
         {
-            var ret = Environment.GetEnvironmentVariable ("ANDROID_TZDATA_ROOT");
-            if (!string.IsNullOrEmpty(ret)) {
+            var ret = Environment.GetEnvironmentVariable("ANDROID_TZDATA_ROOT");
+            if (!string.IsNullOrEmpty(ret))
+            {
                 return ret;
             }
 
             return "/apex/com.android.tzdata";
         }
 
-        private static string GetApexRuntimeRoot ()
+        private static string GetApexRuntimeRoot()
         {
-            var ret = Environment.GetEnvironmentVariable ("ANDROID_RUNTIME_ROOT");
-            if (!string.IsNullOrEmpty (ret)) {
+            var ret = Environment.GetEnvironmentVariable("ANDROID_RUNTIME_ROOT");
+            if (!string.IsNullOrEmpty(ret))
+            {
                 return ret;
             }
 
             return "/apex/com.android.runtime";
         }
 
-        internal static readonly string[] Paths = new string[]{
-            GetApexTimeDataRoot () + "/etc/tz/", // Android 10+, TimeData module where the updates land
-            GetApexRuntimeRoot () + "/etc/tz/",  // Android 10+, Fallback location if the above isn't found or corrupted
-            Environment.GetEnvironmentVariable ("ANDROID_DATA") + "/misc/zoneinfo/",
-        };
+        internal static readonly string[] Paths = new string[] { GetApexTimeDataRoot() + "/etc/tz/", // Android 10+, TimeData module where the updates land
+                                                                 GetApexRuntimeRoot() + "/etc/tz/",  // Android 10+, Fallback location if the above isn't found or corrupted
+                                                                 Environment.GetEnvironmentVariable("ANDROID_DATA") + "/misc/zoneinfo/",};
 
         private static string GetTimeZoneDirectory()
         {
@@ -47,7 +47,7 @@ namespace System
                 }
             }
 
-            return Environment.GetEnvironmentVariable ("ANDROID_ROOT") + DefaultTimeZoneDirectory;
+            return Environment.GetEnvironmentVariable("ANDROID_ROOT") + DefaultTimeZoneDirectory;
         }
     }
 }
