eb829378ad6a2606f69e9eadcfe453c6a2c8f037
Isaiah Inuwa <isaiah.inuwa@moody.edu>
2021-06-28 07:14:19 -0500

    fixup! Allow disabling LDAP referral chasing on Linux.

--- src/libraries/System.DirectoryServices.Protocols/src/System/DirectoryServices/Protocols/ldap/LdapSessionOptions.Linux.cs
+++ src/libraries/System.DirectoryServices.Protocols/src/System/DirectoryServices/Protocols/ldap/LdapSessionOptions.Linux.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.ComponentModel;
-using System.Runtime.InteropServices;
-using System.Runtime.Versioning;
 
 namespace System.DirectoryServices.Protocols
 {
