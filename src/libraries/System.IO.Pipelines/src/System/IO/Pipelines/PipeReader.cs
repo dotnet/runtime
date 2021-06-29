8ca456b9f748f9abb41434cc3ee11252a1dce733
Will Fuqua <wafuqua@gmail.com>
2021-06-29 20:38:43 +0700

    remove doc reference to internal StreamPipeReader
    The StreamPipeReader <see .. /> reference is an internal type, so it should not be mentioned in the public documentation.

--- src/libraries/System.IO.Pipelines/src/System/IO/Pipelines/PipeReader.cs
+++ src/libraries/System.IO.Pipelines/src/System/IO/Pipelines/PipeReader.cs
@@ -15,9 +15,15 @@ namespace System.IO.Pipelines
         /// <summary>Attempts to synchronously read data from the <see cref="System.IO.Pipelines.PipeReader" />.</summary>
         /// <param name="result">When this method returns <see langword="true" />, this value is set to a <see cref="System.IO.Pipelines.ReadResult" /> instance that represents the result of the read call; otherwise, this value is set to <see langword="default" />.</param>
         /// <returns><see langword="true" /> if data was available, or if the call was canceled or the writer was completed; otherwise, <see langword="false" />.</returns>
-        /// <remarks>If the pipe returns <see langword="false" />, there is no need to call <see cref="System.IO.Pipelines.PipeReader.AdvanceTo(System.SequencePosition,System.SequencePosition)" />.
-        /// If the current <see cref="System.IO.Pipelines.PipeReader" /> implementation is a <see cref="System.IO.Pipelines.StreamPipeReader" />,
-        /// <see cref="System.IO.Pipelines.PipeReader.ReadAsync(System.Threading.CancellationToken)" /> must be called first in order to asynchronously retrieve data from the <see cref="System.IO.Pipelines.PipeReader" />.</remarks>
+        /// <remarks>
+        /// If the pipe returns <see langword="false" />, there is no need to call <see cref="System.IO.Pipelines.PipeReader.AdvanceTo(System.SequencePosition,System.SequencePosition)" />.
+        /// <para>
+        /// Note: the <see cref="System.IO.Pipelines.PipeReader" /> implementation returned by <see cref="System.IO.Pipelines.PipeReader.Create(System.IO.Stream, System.IO.Pipelines.StreamPipeReaderOptions?)"/>
+        /// will not read new data from the backing <see cref="System.IO.Stream"/> when <see cref="System.IO.Pipelines.PipeReader.TryRead(out System.IO.Pipelines.ReadResult)"/> is called.
+        /// <see cref="System.IO.Pipelines.PipeReader.ReadAsync(System.Threading.CancellationToken)" /> must be called to read new data from the backing <see cref="System.IO.Stream"/>.
+        /// Any unconsumed data from a previous asynchronous read will be available to <see cref="System.IO.Pipelines.PipeReader.TryRead(out System.IO.Pipelines.ReadResult)"/>.
+        /// </para>
+        /// </remarks>
         public abstract bool TryRead(out ReadResult result);
 
         /// <summary>Asynchronously reads a sequence of bytes from the current <see cref="System.IO.Pipelines.PipeReader" />.</summary>
