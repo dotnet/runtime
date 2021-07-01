c1847f4233a93fa5370c7352be9104615f4fb8d1
Sergey <seandree@microsoft.com>
2021-07-01 11:08:04 -0700

    try to get zero diffs on other platforms.

--- src/coreclr/jit/lclvars.cpp
+++ src/coreclr/jit/lclvars.cpp
@@ -3482,6 +3482,10 @@ void Compiler::lvaSortByRefCount()
             {
                 varDsc->lvTracked = 0;
             }
+            else if ((varDsc->lvType == TYP_STRUCT) && !varDsc->lvRegStruct && !compEnregStructLocals())
+            {
+                lvaSetVarDoNotEnregister(lclNum DEBUGARG(DNER_IsStruct));
+            }
         }
         if (varDsc->lvIsStructField && (lvaGetParentPromotionType(lclNum) != PROMOTION_TYPE_INDEPENDENT))
         {
