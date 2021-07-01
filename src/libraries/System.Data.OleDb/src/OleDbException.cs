7401706aa8c9273d8b93d5d4cb5f38c136b4430d
Andrii Kurdiumov <kant2002@gmail.com>
2021-07-01 09:09:41 +0600

    Consolidate code

--- src/libraries/System.Data.OleDb/src/OleDbException.cs
+++ src/libraries/System.Data.OleDb/src/OleDbException.cs
@@ -64,6 +64,45 @@ namespace System.Data.OleDb
             }
         }
 
+        internal static OleDbException CreateException(UnsafeNativeMethods.IErrorInfo errorInfo, OleDbHResult errorCode, Exception? inner)
+        {
+            OleDbErrorCollection errors = new OleDbErrorCollection(errorInfo);
+            string? message = null;
+            string? source = null;
+            OleDbHResult hr = 0;
+            hr = errorInfo.GetDescription(out message);
+            hr = errorInfo.GetSource(out source);
+
+            int count = errors.Count;
+            if (0 < errors.Count)
+            {
+                StringBuilder builder = new StringBuilder();
+
+                if ((null != message) && (message != errors[0].Message))
+                {
+                    builder.Append(message.TrimEnd(ODB.ErrorTrimCharacters));
+                    if (1 < count)
+                    {
+                        builder.Append(Environment.NewLine);
+                    }
+                }
+                for (int i = 0; i < count; ++i)
+                {
+                    if (0 < i)
+                    {
+                        builder.Append(Environment.NewLine);
+                    }
+                    builder.Append(errors[i].Message.TrimEnd(ODB.ErrorTrimCharacters));
+                }
+                message = builder.ToString();
+            }
+            if (ADP.IsEmpty(message))
+            {
+                message = ODB.NoErrorMessage(errorCode);
+            }
+            return new OleDbException(message, inner, source, errorCode, errors);
+        }
+
         internal static OleDbException CombineExceptions(List<OleDbException> exceptions)
         {
             Debug.Assert(0 < exceptions.Count, "missing exceptions");
