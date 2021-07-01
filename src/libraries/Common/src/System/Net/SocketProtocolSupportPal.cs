e6fc806631afdb3c900f1194f8ea200b0363d4b3
Anton Firszov <antonfir@gmail.com>
2021-07-01 15:28:26 +0200

    switch to disable IPv6

---
+++ src/libraries/Common/src/System/Net/SocketProtocolSupportPal.cs
@@ -0,0 +1,42 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Net.Sockets;
+
+namespace System.Net
+{
+    internal static partial class SocketProtocolSupportPal
+    {
+        private const string DisableIPv6AppCtxSwitch = "System.Net.DisableIPv6";
+        private const string DisableIPv6EnvironmentVariable = "DOTNET_SYSTEM_NET_DISABLEIPV6";
+
+        public static bool OSSupportsIPv6 { get; } = IsSupported(AddressFamily.InterNetworkV6) && !IsIPv6Disabled();
+        public static bool OSSupportsIPv4 { get; } = IsSupported(AddressFamily.InterNetwork);
+        public static bool OSSupportsUnixDomainSockets { get; } = IsSupported(AddressFamily.Unix);
+
+        private static bool IsIPv6Disabled()
+        {
+            bool value;
+
+            // First check for the AppContext switch, giving it priority over the environment variable.
+            if (AppContext.TryGetSwitch(DisableIPv6AppCtxSwitch, out value))
+            {
+                return value;
+            }
+
+            // AppContext switch wasn't used. Check the environment variable.
+            string? envVar = Environment.GetEnvironmentVariable(DisableIPv6EnvironmentVariable);
+
+            if (bool.TryParse(envVar, out value))
+            {
+                return value;
+            }
+            else if (uint.TryParse(envVar, out uint intVal))
+            {
+                return intVal != 0;
+            }
+
+            return false;
+        }
+    }
+}
