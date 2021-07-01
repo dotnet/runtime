c726d521080f05cbcfee7315b001c415fe2be712
Kevin Jones <kevin@vcsjones.com>
2021-07-01 15:53:38 +0000

    Stubs for Windows and macOS

--- src/libraries/System.Security.Cryptography.Algorithms/src/Internal/Cryptography/AesImplementation.OSX.cs
+++ src/libraries/System.Security.Cryptography.Algorithms/src/Internal/Cryptography/AesImplementation.OSX.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
 using System.Security.Cryptography;
 
 namespace Internal.Cryptography
@@ -28,6 +29,18 @@ namespace Internal.Cryptography
                 paddingSize);
 
             return UniversalCryptoTransform.Create(paddingMode, cipher, encrypting);
+        }
+
+        private static ILiteSymmetricCipher CreateLiteCipher(
+            CipherMode cipherMode,
+            ReadOnlySpan<byte> key,
+            ReadOnlySpan<byte> iv,
+            int blockSize,
+            int paddingSize,
+            int feedback,
+            bool encrypting)
+        {
+            throw new NotImplementedException();
         }
     }
 }
