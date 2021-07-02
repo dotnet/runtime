638d17026bcdeafc070b154990ea5137f43ca89d
Daniel Martinez <59089259+dmrayt@users.noreply.github.com>
2021-07-02 17:40:10 +0200

    Update src/libraries/Common/src/Interop/Unix/System.IO.Ports.Native/Interop.Termios.cs
    Add marshaling from bool to int32_t

--- src/libraries/Common/src/Interop/Unix/System.IO.Ports.Native/Interop.Termios.cs
+++ src/libraries/Common/src/Interop/Unix/System.IO.Ports.Native/Interop.Termios.cs
@@ -48,7 +48,7 @@ internal static partial class Interop
         internal static extern int TermiosGetSpeed(SafeSerialDeviceHandle handle);
 
         [DllImport(Libraries.IOPortsNative, EntryPoint = "SystemIoPortsNative_TermiosAvailableBytes", SetLastError = true)]
-        internal static extern int TermiosGetAvailableBytes(SafeSerialDeviceHandle handle, bool fromReadBuffer);
+        internal static extern int TermiosGetAvailableBytes(SafeSerialDeviceHandle handle, [MarshalAs(UnmanagedType.I4)]bool fromReadBuffer);
 
         [DllImport(Libraries.IOPortsNative, EntryPoint = "SystemIoPortsNative_TermiosDiscard", SetLastError = true)]
         internal static extern int TermiosDiscard(SafeSerialDeviceHandle handle, Queue input);
