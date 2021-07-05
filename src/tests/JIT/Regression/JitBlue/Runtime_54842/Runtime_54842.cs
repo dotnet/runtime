f9102db34ac0cb436a9161e7338054fee234b014
SingleAccretion <AccretionMail@yandex.ru>
2021-07-05 23:06:20 +0300

    Add a test

---
+++ src/tests/JIT/Regression/JitBlue/Runtime_54842/Runtime_54842.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Runtime.CompilerServices;
+
+public class Runtime_54842
+{
+    public static int Main()
+    {
+        try
+        {
+            DoubleCheckedConvert(uint.MaxValue);
+        }
+        catch (OverflowException)
+        {
+            return 100;
+        }
+
+        return 101;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private static uint DoubleCheckedConvert(ulong a)
+    {
+        var b = (int)checked((uint)a);
+
+        // Make sure the importer spills "b" to a local.
+        Use(b);
+
+        return checked((uint)b);
+    }
+
+    private static void Use(int value) { }
+}
