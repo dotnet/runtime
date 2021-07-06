8ab5d168e5f56c31b89e57929ce0080b5324fac8
Tom Deseyn <tom.deseyn@gmail.com>
2021-07-06 16:42:15 +0200

    FileSystem.Exists.Unix: eliminate a stat call when checking a non-existing file/directory.
    When checking for a directory, we can use `stat`, there is no need to make an `lstat` call.
    
    When checking for a file, use `lstat` which will give us what we need in most cases.
    Only when it is a link, we need to call `stat` to check if the final target is not a directory.

--- src/libraries/System.Private.CoreLib/src/Microsoft/Win32/SafeHandles/SafeFileHandle.Unix.cs
+++ src/libraries/System.Private.CoreLib/src/Microsoft/Win32/SafeHandles/SafeFileHandle.Unix.cs
@@ -96,11 +96,7 @@ namespace Microsoft.Win32.SafeHandles
         {
             Interop.Sys.FileStatus fileinfo;
 
-            // First use stat, as we want to follow symlinks.  If that fails, it could be because the symlink
-            // is broken, we don't have permissions, etc., in which case fall back to using LStat to evaluate
-            // based on the symlink itself.
-            if (Interop.Sys.Stat(fullPath, out fileinfo) < 0 &&
-                Interop.Sys.LStat(fullPath, out fileinfo) < 0)
+            if (Interop.Sys.Stat(fullPath, out fileinfo) < 0)
             {
                 return false;
             }
