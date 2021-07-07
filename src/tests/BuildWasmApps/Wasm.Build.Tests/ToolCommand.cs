0ef56df4a7c0753296a736b071946d6bc833fdc2
Ankit Jain <radical@gmail.com>
2021-07-07 19:32:43 -0400

    [wasm] Update Wasm.Build.Tests to build with net6.0 (#54936)
    Co-authored-by: Larry Ewing <lewing@microsoft.com>

---
+++ src/tests/BuildWasmApps/Wasm.Build.Tests/ToolCommand.cs
@@ -0,0 +1,184 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+#nullable enable
+
+namespace Wasm.Build.Tests
+{
+    public class ToolCommand
+    {
+        private string _label;
+
+        protected string _command;
+
+        public Process? CurrentProcess { get; private set; }
+
+        public Dictionary<string, string> Environment { get; } = new Dictionary<string, string>();
+
+        public event DataReceivedEventHandler? ErrorDataReceived;
+
+        public event DataReceivedEventHandler? OutputDataReceived;
+
+        public string? WorkingDirectory { get; set; }
+
+        public ToolCommand(string command, string label="")
+        {
+            _command = command;
+            _label = label;
+        }
+
+        public ToolCommand WithWorkingDirectory(string dir)
+        {
+            WorkingDirectory = dir;
+            return this;
+        }
+
+        public ToolCommand WithEnvironmentVariable(string key, string value)
+        {
+            Environment[key] = value;
+            return this;
+        }
+
+        public ToolCommand WithEnvironmentVariables(IDictionary<string, string>? extraEnvVars)
+        {
+            if (extraEnvVars != null)
+            {
+                foreach ((string key, string value) in extraEnvVars)
+                    Environment[key] = value;
+            }
+
+            return this;
+        }
+
+        public virtual CommandResult Execute(params string[] args)
+        {
+            return Task.Run(async () => await ExecuteAsync(args)).Result;
+        }
+
+        public async virtual Task<CommandResult> ExecuteAsync(params string[] args)
+        {
+            var resolvedCommand = _command;
+            string fullArgs = GetFullArgs(args);
+            Console.WriteLine($"[{_label}] Executing - {resolvedCommand} {fullArgs} - {WorkingDirectoryInfo()}");
+            return await ExecuteAsyncInternal(resolvedCommand, fullArgs);
+        }
+
+        public virtual CommandResult ExecuteWithCapturedOutput(params string[] args)
+        {
+            var resolvedCommand = _command;
+            string fullArgs = GetFullArgs(args);
+            Console.WriteLine($"[{_label}] Executing (Captured Output) - {resolvedCommand} {fullArgs} - {WorkingDirectoryInfo()}");
+            return Task.Run(async () => await ExecuteAsyncInternal(resolvedCommand, fullArgs)).Result;
+        }
+
+        protected virtual string GetFullArgs(params string[] args) => string.Join(" ", args);
+
+        private async Task<CommandResult> ExecuteAsyncInternal(string executable, string args)
+        {
+            var output = new List<string>();
+            CurrentProcess = CreateProcess(executable, args);
+            CurrentProcess.ErrorDataReceived += (s, e) =>
+            {
+                if (e.Data == null)
+                    return;
+
+                output.Add($"[{_label}] {e.Data}");
+                ErrorDataReceived?.Invoke(s, e);
+            };
+
+            CurrentProcess.OutputDataReceived += (s, e) =>
+            {
+                if (e.Data == null)
+                    return;
+
+                output.Add($"[{_label}] {e.Data}");
+                OutputDataReceived?.Invoke(s, e);
+            };
+
+            var completionTask = CurrentProcess.StartAndWaitForExitAsync();
+            CurrentProcess.BeginOutputReadLine();
+            CurrentProcess.BeginErrorReadLine();
+            await completionTask;
+
+            CurrentProcess.WaitForExit();
+            RemoveNullTerminator(output);
+
+            return new CommandResult(
+                CurrentProcess.StartInfo,
+                CurrentProcess.ExitCode,
+                string.Join(System.Environment.NewLine, output));
+        }
+
+        private Process CreateProcess(string executable, string args)
+        {
+            var psi = new ProcessStartInfo
+            {
+                FileName = executable,
+                Arguments = args,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                RedirectStandardInput = true,
+                UseShellExecute = false
+            };
+
+            psi.Environment["DOTNET_MULTILEVEL_LOOKUP"] = "0";
+            psi.Environment["DOTNET_SKIP_FIRST_TIME_EXPERIENCE"] = "1";
+
+            AddEnvironmentVariablesTo(psi);
+            AddWorkingDirectoryTo(psi);
+            var process = new Process
+            {
+                StartInfo = psi
+            };
+
+            process.EnableRaisingEvents = true;
+            return process;
+        }
+
+        private string WorkingDirectoryInfo()
+        {
+            if (WorkingDirectory == null)
+            {
+                return "";
+            }
+
+            return $" in pwd {WorkingDirectory}";
+        }
+
+        private void RemoveNullTerminator(List<string> strings)
+        {
+            var count = strings.Count;
+
+            if (count < 1)
+            {
+                return;
+            }
+
+            if (strings[count - 1] == null)
+            {
+                strings.RemoveAt(count - 1);
+            }
+        }
+
+        private void AddEnvironmentVariablesTo(ProcessStartInfo psi)
+        {
+            foreach (var item in Environment)
+            {
+                psi.Environment[item.Key] = item.Value;
+            }
+        }
+
+        private void AddWorkingDirectoryTo(ProcessStartInfo psi)
+        {
+            if (!string.IsNullOrWhiteSpace(WorkingDirectory))
+            {
+                psi.WorkingDirectory = WorkingDirectory;
+            }
+        }
+    }
+}
