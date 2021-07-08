04eced6414f39399ac8783aaa2d9ff2725efa4b7
Adam Sitnik <adam.sitnik@microsoft.com>
2021-07-08 10:22:02 +0200

    handle SMB2_SUPER_MAGIC as well (#53182)

--- src/libraries/System.Private.CoreLib/src/Microsoft/Win32/SafeHandles/SafeFileHandle.Unix.cs
+++ src/libraries/System.Private.CoreLib/src/Microsoft/Win32/SafeHandles/SafeFileHandle.Unix.cs
@@ -337,6 +337,7 @@ namespace Microsoft.Win32.SafeHandles
                 case 0x6969: // NFS_SUPER_MAGIC
                 case 0xFF534D42: // CIFS_MAGIC_NUMBER
                 case 0x517B: // SMB_SUPER_MAGIC
+                case 0xFE534D42: // SMB2_SUPER_MAGIC (#53182)
                     return false; // LOCK_SH is not OK when writing to NFS, CIFS or SMB
                 case -1: // error
                     return false; // assume we should not acquire the lock if we don't know the File System
