519e6de6c0db48cda4cfcc844a3ae4a845fc5cf2
Mateo Torres-Ruiz <mateoatr@users.noreply.github.com>
2021-07-08 13:51:38 -0700

    Check target of module/assembly level suppressions (dotnet/linker#2129)
    * Check that global level suppressions do not specify any target
    
    * Run lint
    
    * Update test/Mono.Linker.Tests.Cases/Warnings/WarningSuppression/SuppressWarningsInModule.cs
    
    Co-authored-by: Sven Boemer <sbomer@gmail.com>
    
    * Add code snippet with an example of 2108.
    
    * PR feedback
    Add tests
    
    * Lint
    
    * Don't silently suppress warnings on module if scope is missing and a target was used.
    
    Co-authored-by: Sven Boemer <sbomer@gmail.com>
    
    Commit migrated from https://github.com/dotnet/linker/commit/b9501922637806f4135df09a9922d5540e203858

--- src/tools/linker/test/Mono.Linker.Tests.Cases/Warnings/WarningSuppression/SuppressWarningsInModule.cs
+++ src/tools/linker/test/Mono.Linker.Tests.Cases/Warnings/WarningSuppression/SuppressWarningsInModule.cs
@@ -5,6 +5,8 @@ using Mono.Linker.Tests.Cases.Expectations.Assertions;
 using Mono.Linker.Tests.Cases.Expectations.Metadata;
 
 [module: UnconditionalSuppressMessage ("Test", "IL2072:Suppress unrecognized reflection pattern warnings in this module")]
+[module: UnconditionalSuppressMessage ("Test", "IL2026:Test that specifying an invalid scope will result in a global suppression being ignored.",
+	Scope = "invalidScope", Target = "Non-existent target")]
 
 namespace Mono.Linker.Tests.Cases.Warnings.WarningSuppression
 {
@@ -13,13 +15,16 @@ namespace Mono.Linker.Tests.Cases.Warnings.WarningSuppression
 #endif
 	[SkipKeptItemsValidation]
 	[LogDoesNotContain ("TriggerUnrecognizedPattern()")]
+	[LogContains ("warning IL2108: Invalid scope 'invalidScope' used in 'UnconditionalSuppressMessageAttribute'")]
 	public class SuppressWarningsInModule
 	{
+		[ExpectedWarning ("IL2026", "TriggerUnrecognizedPattern()")]
 		public static void Main ()
 		{
 			Expression.Call (TriggerUnrecognizedPattern (), "", Type.EmptyTypes);
 		}
 
+		[RequiresUnreferencedCode ("Test that the global unconditional suppression will not work when using an invalid scope.")]
 		public static Type TriggerUnrecognizedPattern ()
 		{
 			return typeof (SuppressWarningsInModule);
