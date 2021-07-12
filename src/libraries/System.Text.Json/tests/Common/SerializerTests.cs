layomia/dotnet_runtime

Enable property visibility tests in source-gen mode

src/libraries/System.Text.Json/tests/Common/SerializerTests.cs
@@ -1,0 +1,12 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Text.Json.Serialization.Tests
+{
+    public abstract class SerializerTests
+    {
+        protected JsonSerializerWrapper JsonSerializerWrapper { get; }
+
+        protected SerializerTests(JsonSerializerWrapper serializerWrapper) => JsonSerializerWrapper = serializerWrapper;
+    }
+}

dotnet/runtime

src/libraries/System.Text.Json/tests/Common/SerializerTests.cs
@@ -1,0 +1,12 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Text.Json.Serialization.Tests
+{
+    public abstract class SerializerTests
+    {
+        protected JsonSerializerWrapperForString JsonSerializerWrapperForString { get; }
+
+        protected SerializerTests(JsonSerializerWrapperForString serializerWrapper) => JsonSerializerWrapperForString = serializerWrapper;
+    }
+}