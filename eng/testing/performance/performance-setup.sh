782ede5b70465add20e994f0994e2c4a2916de07
Alicia Li <alicial@microsoft.com>
2021-07-12 13:30:43 -0700

    fixed $wasm_dotnet_path/artifacts/BrowserWasm/artifacts path

--- eng/testing/performance/performance-setup.sh
+++ eng/testing/performance/performance-setup.sh
@@ -273,7 +273,7 @@ if [[ "$wasm_runtime_loc" != "" ]]; then
         # wasm aot needs some source code from dotnet\runtime repo
         rsync -aq --progress $source_directory/* $wasm_dotnet_path --exclude payload --exclude docs --exclude src/coreclr --exclude src/tests --exclude artifacts/obj
         # copy wasm build drop to the location that aot build expects
-        rsync -aq --progress $wasm_dotnet_path/BrowserWasm/artifacts/* $wasm_dotnet_path/artifacts
+        rsync -a --progress $wasm_dotnet_path/artifacts/BrowserWasm/artifacts/* $wasm_dotnet_path/artifacts
         extra_benchmark_dotnet_arguments="$extra_benchmark_dotnet_arguments --wasmEngine /home/helixbot/.jsvu/$javascript_engine --aotcompilermode wasm --runtimeSrcDir \$HELIX_CORRELATION_PAYLOAD/dotnet-wasm --buildTimeout 3600 --keepfiles" 
     else
         extra_benchmark_dotnet_arguments="$extra_benchmark_dotnet_arguments --wasmMainJS \$HELIX_CORRELATION_PAYLOAD/dotnet-wasm/runtime-test.js --wasmEngine /home/helixbot/.jsvu/$javascript_engine --customRuntimePack \$HELIX_CORRELATION_PAYLOAD/dotnet-wasmS --keepfiles"
