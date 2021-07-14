d1e30b17b65a3d5e7fcf652e9fd2a4e6699d83f5
fenix <fenix@DESKTOP-VDKQGU5>
2021-07-14 13:58:03 +0300

    build fixes (#19838)

--- src/libraries/System.Configuration.ConfigurationManager/src/System/Configuration/FipsAwareEncryptedXml.cs
+++ src/libraries/System.Configuration.ConfigurationManager/src/System/Configuration/FipsAwareEncryptedXml.cs
@@ -1,17 +1,18 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Collections;
+using System;
 using System.Security.Cryptography;
 using System.Security.Cryptography.Xml;
 using System.Xml;
+using System.Linq;
 
 namespace System.Configuration
 {
     //
     // Extends EncryptedXml to use FIPS-certified symmetric algorithm
     //
-    class FipsAwareEncryptedXml : EncryptedXml
+    internal sealed class FipsAwareEncryptedXml : EncryptedXml
     {
         public FipsAwareEncryptedXml(XmlDocument doc)
             : base(doc)
