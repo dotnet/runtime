21e36e872556c157d4b65a1c997d92dfb31f18c5
Egor Bogatov <egorbo@gmail.com>
2021-07-14 14:01:14 +0300

    Inliner: Extend IL limit for profiled call-sites, allow inlining for switches. (#55478)

--- src/coreclr/jit/compiler.h
+++ src/coreclr/jit/compiler.h
@@ -5814,6 +5814,7 @@ public:
     void WalkSpanningTree(SpanningTreeVisitor* visitor);
     void fgSetProfileWeight(BasicBlock* block, BasicBlock::weight_t weight);
     void fgApplyProfileScale();
+    bool fgHaveSufficientProfileData();
 
     // fgIsUsingProfileWeights - returns true if we have real profile data for this method
     //                           or if we have some fake profile data for the stress mode
