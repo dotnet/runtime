9717cf578bc38180866461cba7907e4ad6e9dd0f
Stephen Toub <stoub@microsoft.com>
2021-07-14 22:41:10 -0400

    Simplify File.WriteAllTextAsync (#55644)
    Avoid the unnecessary ArrayPool usage and extra layer of buffering.

--- src/libraries/System.Private.CoreLib/src/System/IO/File.cs
+++ src/libraries/System.Private.CoreLib/src/System/IO/File.cs
@@ -921,6 +921,7 @@ namespace System.IO
 
         private static async Task InternalWriteAllTextAsync(StreamWriter sw, string contents, CancellationToken cancellationToken)
         {
+#if MS_IO_REDIST
             char[]? buffer = null;
             try
             {
@@ -931,11 +932,7 @@ namespace System.IO
                 {
                     int batchSize = Math.Min(DefaultBufferSize, count - index);
                     contents.CopyTo(index, buffer, 0, batchSize);
-#if MS_IO_REDIST
                     await sw.WriteAsync(buffer, 0, batchSize).ConfigureAwait(false);
-#else
-                    await sw.WriteAsync(new ReadOnlyMemory<char>(buffer, 0, batchSize), cancellationToken).ConfigureAwait(false);
-#endif
                     index += batchSize;
                 }
 
@@ -950,6 +947,13 @@ namespace System.IO
                     ArrayPool<char>.Shared.Return(buffer);
                 }
             }
+#else
+            using (sw)
+            {
+                await sw.WriteAsync(contents.AsMemory(), cancellationToken).ConfigureAwait(false);
+                await sw.FlushAsync().ConfigureAwait(false);
+            }
+#endif
         }
 
         public static Task AppendAllTextAsync(string path, string? contents, CancellationToken cancellationToken = default(CancellationToken))
