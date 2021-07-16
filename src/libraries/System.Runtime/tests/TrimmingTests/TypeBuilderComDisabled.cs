801d9c84a0ffd0aa3150b77d1ca5fd24198b8ba6
Elinor Fung <elfung@microsoft.com>
2021-07-16 14:14:31 -0700

    Fix trimming test

--- src/libraries/System.Runtime/tests/TrimmingTests/TypeBuilderComDisabled.cs
+++ src/libraries/System.Runtime/tests/TrimmingTests/TypeBuilderComDisabled.cs
@@ -4,9 +4,25 @@ using System.Reflection.Emit;
 
 AssemblyBuilder assembly = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName("GeneratedAssembly"), AssemblyBuilderAccess.Run);
 ModuleBuilder module = assembly.DefineDynamicModule("GeneratedModule");
-TypeBuilder genericType = module.DefineType("GeneratedType");
+
+string typeName = "GeneratedType";
+TypeBuilder genericType = module.DefineType(typeName);
 genericType.DefineField("_int", typeof(int), FieldAttributes.Private);
 genericType.DefineProperty("Prop", PropertyAttributes.None, typeof(string), null);
 
 Type generatedType = genericType.CreateType();
+if (generatedType.Name != typeName)
+{
+    Console.WriteLine($"Unexpected name for generated type. Expected: {typeName}, Actual: {generatedType.Name}");
+    return -1;
+}
+
+object obj = Activator.CreateInstance(generatedType);
+string objAsString = obj.ToString();
+if (objAsString != typeName)
+{
+    Console.WriteLine($"Unexpected result of ToString() for instance of generated type. Expected: {typeName}, Actual: {objAsString}");
+    return -2;
+}
+
 return 100;
