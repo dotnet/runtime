5c21fe7f88d639fb28eb0711a71f32595a0c6d57
Maxim Lipnin <v-maxlip@microsoft.com>
2021-07-16 14:16:30 +0300

    Take into account possible null terminator

--- src/libraries/System.IO.FileSystem/tests/FileSystemTest.cs
+++ src/libraries/System.IO.FileSystem/tests/FileSystemTest.cs
@@ -70,7 +70,7 @@ namespace System.IO.Tests
             const int MinUdsPathLength = 104; // required min is 92, but every platform we currently target is at least 104
             const int MinAvailableForSufficientRandomness = 5; // we want enough randomness in the name to avoid conflicts between concurrent tests
             string prefix = Path.Combine(Path.GetTempPath(), "CoreFxPipe_");
-            int availableLength = MinUdsPathLength  - prefix.Length;
+            int availableLength = MinUdsPathLength - prefix.Length - 1; // 1 - for possible null terminator
             Assert.True(availableLength >= MinAvailableForSufficientRandomness, $"UDS prefix {prefix} length {prefix.Length} is too long");
             
             return string.Create(availableLength, 0, (span, _) =>
