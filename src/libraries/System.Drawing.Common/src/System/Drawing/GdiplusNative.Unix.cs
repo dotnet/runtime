841b88ab1a4c963346c43cff1080398d1fe114d3
Santiago Fernandez Madero <safern@microsoft.com>
2021-07-16 19:23:27 -0700

    Make System.Drawing.Common throw on Unix unless a config switch is set

--- src/libraries/System.Drawing.Common/src/System/Drawing/GdiplusNative.Unix.cs
+++ src/libraries/System.Drawing.Common/src/System/Drawing/GdiplusNative.Unix.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
 using System.Collections.Generic;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
@@ -62,6 +63,9 @@ namespace System.Drawing
 
             private static void PlatformInitialize()
             {
+                if (!LocalAppContextSwitches.EnableUnixSupport)
+                    throw new PlatformNotSupportedException(SR.PlatformNotSupported_Unix);
+
                 LibraryResolver.EnsureRegistered();
             }
 
