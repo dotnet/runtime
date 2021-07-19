c01ba9aa0afec8d07f84f8a0e32fa383bb5496cc
Vitek Karas <vitek.karas@microsoft.com>
2021-07-19 18:53:06 +0200

    Fixes a null reference when trying to get RUC on a type (dotnet/linker#2152)
    If the type in question has unresolvable base type, we null ref.
    
    Commit migrated from https://github.com/dotnet/linker/commit/8d49e169d872d6225bcf73ae14ae50b002f39319

--- src/tools/illink/src/linker/Linker/Annotations.cs
+++ src/tools/illink/src/linker/Linker/Annotations.cs
@@ -561,13 +561,14 @@ namespace Mono.Linker
 		/// <returns>Returns true along with the RequiresUnreferencedCodeAttribute if found, otherwise returns false</returns>
 		public bool TryGetEffectiveRequiresUnreferencedCodeAttributeOnType (TypeDefinition type, out RequiresUnreferencedCodeAttribute attribute)
 		{
-			do {
+			while (type != null) {
 				if (TryGetLinkerAttribute (type, out RequiresUnreferencedCodeAttribute declaringTypeAttribute)) {
 					attribute = declaringTypeAttribute;
 					return true;
 				}
 				type = type.DeclaringType;
-			} while (type != null);
+			}
+
 			attribute = null;
 			return false;
 		}
