1530b998257130c71d1f8320e81f2b127e79f643
Karel Zikmund <karelz@microsoft.com>
2021-07-20 17:38:08 +0200

    Disable test Connect_DualMode_MultiAddressFamilyConnect_RetrievedEndPoints_Success on Linux
    Disabled test tracked by #55053

--- src/libraries/System.Net.Sockets/tests/FunctionalTests/Connect.cs
+++ src/libraries/System.Net.Sockets/tests/FunctionalTests/Connect.cs
@@ -84,6 +84,7 @@ namespace System.Net.Sockets.Tests
         }
 
         [Fact]
+        [ActiveIssue("https://github.com/dotnet/runtime/issues/55053", TestPlatforms.AnyUnix)]
         public async Task Connect_DualMode_MultiAddressFamilyConnect_RetrievedEndPoints_Success()
         {
             if (!SupportsMultiConnect)
