94a843027c5fe3550ec9ba8568b8872aff47779d
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2021-07-21 11:29:36 +0200

    Disallow widening for explicit tailcalls (#55989)
    It is a runtime detail that the managed calling convention widens return
    values, so only allow this behavior for opportunistic tailcalls.
    
    Fix #55253

--- src/coreclr/jit/morph.cpp
+++ src/coreclr/jit/morph.cpp
@@ -6872,7 +6872,7 @@ bool Compiler::fgCanFastTailCall(GenTreeCall* callee, const char** failReason)
     if (callee->IsTailPrefixedCall())
     {
         var_types retType = info.compRetType;
-        assert(impTailCallRetTypeCompatible(retType, info.compMethodInfo->args.retTypeClass, info.compCallConv,
+        assert(impTailCallRetTypeCompatible(false, retType, info.compMethodInfo->args.retTypeClass, info.compCallConv,
                                             (var_types)callee->gtReturnType, callee->gtRetClsHnd,
                                             callee->GetUnmanagedCallConv()));
     }
