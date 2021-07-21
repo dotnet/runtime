8ece868ab4771c2fb5c09ad836f095723705b379
Geoff Kizer <geoffrek@microsoft.com>
2021-07-21 15:37:20 -0700

    Remove QuicStream.ShutdownWriteCompleted method (#55981)
    * remove QuicStream.ShutdownWriteCompleted API and associated implementation logic
    
    Co-authored-by: Geoffrey Kizer <geoffrek@windows.microsoft.com>

--- src/libraries/System.Net.Quic/src/System/Net/Quic/QuicStream.cs
+++ src/libraries/System.Net.Quic/src/System/Net/Quic/QuicStream.cs
@@ -99,8 +99,6 @@ namespace System.Net.Quic
 
         public ValueTask WriteAsync(ReadOnlyMemory<ReadOnlyMemory<byte>> buffers, bool endStream, CancellationToken cancellationToken = default) => _provider.WriteAsync(buffers, endStream, cancellationToken);
 
-        public ValueTask ShutdownWriteCompleted(CancellationToken cancellationToken = default) => _provider.ShutdownWriteCompleted(cancellationToken);
-
         public ValueTask ShutdownCompleted(CancellationToken cancellationToken = default) => _provider.ShutdownCompleted(cancellationToken);
 
         public void Shutdown() => _provider.Shutdown();
