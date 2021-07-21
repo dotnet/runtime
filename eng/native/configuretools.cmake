5f0ff6d051fc6d2c396ce24f725aca340f91c918
Jan Vorlicek <janvorli@microsoft.com>
2021-07-21 10:41:13 -0700

    Fix cmake 3.6 build issue

--- eng/native/configuretools.cmake
+++ eng/native/configuretools.cmake
@@ -66,7 +66,7 @@ endif()
 if (NOT CLR_CMAKE_HOST_WIN32)
 
   # detect linker
-  separate_arguments(ldVersion NATIVE_COMMAND "${CMAKE_C_COMPILER} ${CMAKE_SHARED_LINKER_FLAGS} -Wl,--version")
+  separate_arguments(ldVersion UNIX_COMMAND "${CMAKE_C_COMPILER} ${CMAKE_SHARED_LINKER_FLAGS} -Wl,--version")
   execute_process(COMMAND ${ldVersion}
     ERROR_QUIET
     OUTPUT_VARIABLE ldVersionOutput)
