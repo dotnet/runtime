b6127430d315780ca2252e957ec9c8b757dc6155
Karel Zikmund <karelz@microsoft.com>
2021-07-21 12:02:07 +0200

    Disable [QUIC] WriteCanceled_NextWriteThrows on Mock
    Disabled test tracked by #55995

--- src/libraries/System.Net.Quic/tests/FunctionalTests/QuicStreamTests.cs
+++ src/libraries/System.Net.Quic/tests/FunctionalTests/QuicStreamTests.cs
@@ -617,6 +617,12 @@ namespace System.Net.Quic.Tests
         [Fact]
         public async Task WriteCanceled_NextWriteThrows()
         {
+            // [ActiveIssue("https://github.com/dotnet/runtime/issues/55995")]
+            if (this.UseQuicImplementationProvider == QuicImplementationProviders.Mock)
+            {
+                return;
+            }
+            
             const long expectedErrorCode = 1234;
 
             await RunClientServer(
