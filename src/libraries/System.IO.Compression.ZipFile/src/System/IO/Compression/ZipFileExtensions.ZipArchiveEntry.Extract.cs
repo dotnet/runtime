5f36d3f343fbf0dc80b323ab35bad5eecb5e60fd
Adam Sitnik <adam.sitnik@gmail.com>
2021-07-27 17:57:12 +0200

    address code review suggestion: remove exception filter

--- src/libraries/System.IO.Compression.ZipFile/src/System/IO/Compression/ZipFileExtensions.ZipArchiveEntry.Extract.cs
+++ src/libraries/System.IO.Compression.ZipFile/src/System/IO/Compression/ZipFileExtensions.ZipArchiveEntry.Extract.cs
@@ -85,8 +85,9 @@ namespace System.IO.Compression
             {
                 File.SetLastWriteTime(destinationFileName, source.LastWriteTime.DateTime);
             }
-            catch (UnauthorizedAccessException) when (OperatingSystem.IsAndroid()) // not every Android supports that (#35374)
+            catch (UnauthorizedAccessException)
             {
+                // some OSes like Android (#35374) might not support setting the last write time, the extraction should not fail because of that
             }
         }
 
