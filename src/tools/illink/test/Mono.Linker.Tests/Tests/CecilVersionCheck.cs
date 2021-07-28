84a2897cf41a3c0dd37cfc0af6bf137357e9c394
Sven Boemer <sbomer@gmail.com>
2021-07-28 08:18:03 -0700

    Bump cecil package version (dotnet/linker#2174)
    * Bump cecil package version
    
    The submodule update in https://github.com/mono/linker/pull/2156
    incremented the cecil version, but we were still using 0.11.3 as the version
    in our package graph. See https://github.com/mono/linker/pull/1515 for more
    context on how this is set up.
    
    Fixes https://github.com/mono/linker/issues/2173
    
    * Remove unnecessary cast
    
    Commit migrated from https://github.com/dotnet/linker/commit/0cb9250a903cfc90cbac602ed79c0cbc588d8d3f

---
+++ src/tools/illink/test/Mono.Linker.Tests/Tests/CecilVersionCheck.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Linq;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace Mono.Linker.Tests
+{
+	[TestFixture]
+	public class CecilVersionCheck
+	{
+		[TestCase]
+		public void CecilPackageVersionMatchesAssemblyVersion ()
+		{
+			var thisAssembly = Assembly.GetExecutingAssembly ();
+			var cecilPackageVersion = thisAssembly
+				.GetCustomAttributes<AssemblyMetadataAttribute> ()
+				.Where (ca => ca.Key == "CecilPackageVersion")
+				.Single ().Value;
+			// Assume that the test assembly builds against the same cecil as the linker.
+			var cecilAssemblyVersion = thisAssembly
+				.GetReferencedAssemblies ()
+				.Where (an => an.Name == "Mono.Cecil")
+				.Single ().Version;
+			Assert.AreEqual (cecilPackageVersion, cecilAssemblyVersion.ToString (3));
+		}
+	}
+}
