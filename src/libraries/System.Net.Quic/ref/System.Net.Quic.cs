53ec1acf0483f38ee4073ce910a20521a49eb513
Tomas Weinfurt <tweinfurt@yahoo.com>
2021-07-29 13:53:40 -0700

    improve quic client cert validation options on server (#56491)

--- src/libraries/System.Net.Quic/ref/System.Net.Quic.cs
+++ src/libraries/System.Net.Quic/ref/System.Net.Quic.cs
@@ -31,6 +31,7 @@ namespace System.Net.Quic
         public int GetRemoteAvailableUnidirectionalStreamCount() { throw null; }
         public System.Net.Quic.QuicStream OpenBidirectionalStream() { throw null; }
         public System.Net.Quic.QuicStream OpenUnidirectionalStream() { throw null; }
+        public System.Security.Cryptography.X509Certificates.X509Certificate? RemoteCertificate { get { throw null; } }
         public System.Threading.Tasks.ValueTask WaitForAvailableBidirectionalStreamsAsync(System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
         public System.Threading.Tasks.ValueTask WaitForAvailableUnidirectionalStreamsAsync(System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken)) { throw null; }
     }
