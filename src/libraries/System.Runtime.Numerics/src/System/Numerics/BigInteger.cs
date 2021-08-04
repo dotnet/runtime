e5153713497301dbc1f7d11873a9fed3831ac043
Takym (たかやま) <15681312+Takym@users.noreply.github.com>
2021-08-04 14:55:59 +0900

    `BigInteger`: made properties simple format.

--- src/libraries/System.Runtime.Numerics/src/System/Numerics/BigInteger.cs
+++ src/libraries/System.Runtime.Numerics/src/System/Numerics/BigInteger.cs
@@ -627,11 +627,11 @@ namespace System.Numerics
             return;
         }
 
-        public static BigInteger Zero { get { return s_bnZeroInt; } }
+        public static BigInteger Zero => s_bnZeroInt;
 
-        public static BigInteger One { get { return s_bnOneInt; } }
+        public static BigInteger One => s_bnOneInt;
 
-        public static BigInteger MinusOne { get { return s_bnMinusOneInt; } }
+        public static BigInteger MinusOne => s_bnMinusOneInt;
 
         public bool IsPowerOfTwo
         {
