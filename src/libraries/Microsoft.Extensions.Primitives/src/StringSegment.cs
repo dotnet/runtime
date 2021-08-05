193f26541ca2ca6737a574d8a825f4b3f1c7bb78
Dan Moseley <danmose@microsoft.com>
2021-08-04 22:12:22 -0600

    GHC

--- src/libraries/Microsoft.Extensions.Primitives/src/StringSegment.cs
+++ src/libraries/Microsoft.Extensions.Primitives/src/StringSegment.cs
@@ -270,7 +270,12 @@ namespace Microsoft.Extensions.Primitives
             return AsSpan().Equals(text.AsSpan(), comparisonType);
         }
 
-        /// <inheritdoc />
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table.
+        /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public override int GetHashCode()
         {
