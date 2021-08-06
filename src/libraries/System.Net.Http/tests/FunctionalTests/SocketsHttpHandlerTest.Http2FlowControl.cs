a8baca13ea08916e32b57413488a6cd0a20c6d13
Anton Firszov <Anton.Firszov@microsoft.com>
2021-08-06 14:41:04 +0200

    Redisign HTTP/2 KeepAlive PING tests (#56736)
    Completely redesign tests for HTTP/2 KeepAlive PING, so they:
    - Work well with RTT pings introduced in Implement dynamic HTTP/2 window scaling #54755
    - Run sequentially, reducing the chance of failing because of timing issues caused by parallel workloads
    - Are better organized: multiple test cases for different scenarios, instead of one theory with complex branches on parameters
    
    Instead of reading / reacting to frames inline, there is a separate Task for processing incoming frames, responding to PING immediately and pushing other frames to a Channel<Frame>.
    
    Fixes #41929

--- src/libraries/System.Net.Http/tests/FunctionalTests/SocketsHttpHandlerTest.Http2FlowControl.cs
+++ src/libraries/System.Net.Http/tests/FunctionalTests/SocketsHttpHandlerTest.Http2FlowControl.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Diagnostics;
 using System.Linq;
 using System.Net.Test.Common;
 using System.Threading;
@@ -236,7 +235,6 @@ namespace System.Net.Http.Functional.Tests
 
                 int nextRemainingBytes = remainingBytes - bytesToSend;
                 bool endStream = nextRemainingBytes == 0;
-
                 await writeSemaphore.WaitAsync();
                 Interlocked.Add(ref credit, -bytesToSend);
                 await connection.SendResponseDataAsync(streamId, responseData, endStream);
