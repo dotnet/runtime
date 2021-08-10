a4bc1ebf92e7c83b32550b380408d7e64153afef
Ankit Jain <radical@gmail.com>
2021-08-10 16:24:54 -0400

    fix build

--- src/tests/BuildWasmApps/Wasm.Build.Tests/RebuildTests.cs
+++ src/tests/BuildWasmApps/Wasm.Build.Tests/RebuildTests.cs
@@ -2,7 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Xunit;
 using Xunit.Abstractions;
 using Xunit.Sdk;
