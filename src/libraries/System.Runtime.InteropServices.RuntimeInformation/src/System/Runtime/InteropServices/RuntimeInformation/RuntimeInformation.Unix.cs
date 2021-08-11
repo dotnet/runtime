26e8af26c03141559da61b32b8643bc0a190906f
Adeel <3840695+am11@users.noreply.github.com>
2021-08-11 03:48:44 +0300

    Split ProcessArchitecture into partials

--- src/libraries/System.Runtime.InteropServices.RuntimeInformation/src/System/Runtime/InteropServices/RuntimeInformation/RuntimeInformation.Unix.cs
+++ src/libraries/System.Runtime.InteropServices.RuntimeInformation/src/System/Runtime/InteropServices/RuntimeInformation/RuntimeInformation.Unix.cs
@@ -11,23 +11,6 @@ namespace System.Runtime.InteropServices
 
         public static string OSDescription => s_osDescription ??= Interop.Sys.GetUnixVersion();
 
-        public static Architecture ProcessArchitecture { get; } =
-#if TARGET_AMD64
-            Architecture.X64;
-#elif TARGET_ARM
-            Architecture.Arm;
-#elif TARGET_ARM64
-            Architecture.Arm64;
-#elif TARGET_S390X
-            Architecture.S390x;
-#elif TARGET_WASM
-            Architecture.Wasm;
-#elif TARGET_X86
-            Architecture.X86;
-#else
-#error Unidentified Architecture
-#endif
-
         public static Architecture OSArchitecture { get; } = ProcessArchitecture;
     }
 }
