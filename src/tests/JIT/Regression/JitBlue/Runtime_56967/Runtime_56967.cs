adbedf0037e63158bf4ff6205b2ba35e79ceff37
Kunal Pathak <Kunal.Pathak@microsoft.com>
2021-08-12 07:36:59 -0700

    Update test case to add Avx2.IsSupported

--- src/tests/JIT/Regression/JitBlue/Runtime_56967/Runtime_56967.cs
+++ src/tests/JIT/Regression/JitBlue/Runtime_56967/Runtime_56967.cs
@@ -9,18 +9,22 @@ public class Program
     // src as 'delayFree'.
     static unsafe int Main()
     {
-        int* values = stackalloc int[256];
-        var vmsk = Vector256.Create(-1, -1, -1, 0, -1, -1, -1, 0);
-        var vlu1 = Vector256.Create(0, 1, 2, 3, 4, 5, 6, 7);
-        var vlu2 = Vector256.Create(7, 6, 5, 4, 3, 2, 1, 0);
-
-        vlu1 = Avx2.GatherMaskVector256(vlu1, values, vlu1, vmsk, sizeof(int));
-        vlu2 = Avx2.GatherMaskVector256(vlu2, values, vlu2, vmsk, sizeof(int));
-
-        if (vlu1.GetElement(3) == 3 && vlu2.GetElement(3) == 4)
+        if (Avx2.IsSupported)
         {
-            return 100;
+            int* values = stackalloc int[256];
+            var vmsk = Vector256.Create(-1, -1, -1, 0, -1, -1, -1, 0);
+            var vlu1 = Vector256.Create(0, 1, 2, 3, 4, 5, 6, 7);
+            var vlu2 = Vector256.Create(7, 6, 5, 4, 3, 2, 1, 0);
+
+            vlu1 = Avx2.GatherMaskVector256(vlu1, values, vlu1, vmsk, sizeof(int));
+            vlu2 = Avx2.GatherMaskVector256(vlu2, values, vlu2, vmsk, sizeof(int));
+
+            if (vlu1.GetElement(3) != 3 || vlu2.GetElement(3) != 4)
+            {
+                return 1;
+            }
         }
-        return 1;
+
+        return 100;
     }
 }
