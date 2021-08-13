d4b0720907da027fa6b4dc8c85959cd0cec24995
Andy Gocke <angocke@microsoft.com>
2021-08-13 11:55:10 -0700

    Add preliminary tests

--- src/tests/tracing/eventcounter/runtimecounters.cs
+++ src/tests/tracing/eventcounter/runtimecounters.cs
@@ -16,37 +16,36 @@ namespace RuntimeEventCounterTests
 {
     public class RuntimeCounterListener : EventListener
     {
-        public RuntimeCounterListener()
-        {
-            observedRuntimeCounters = new Dictionary<string, bool>() {
-                { "cpu-usage" , false },
-                { "working-set", false },
-                { "gc-heap-size", false },
-                { "gen-0-gc-count", false },
-                { "gen-1-gc-count", false },
-                { "gen-2-gc-count", false },
-                { "threadpool-thread-count", false },
-                { "monitor-lock-contention-count", false },
-                { "threadpool-queue-length", false },
-                { "threadpool-completed-items-count", false },
-                { "alloc-rate", false },
-                { "active-timer-count", false },
-                { "gc-fragmentation", false },
-                { "gc-committed", false },
-                { "exception-count", false },
-                { "time-in-gc", false },
-                { "gen-0-size", false },
-                { "gen-1-size", false },
-                { "gen-2-size", false },
-                { "loh-size", false },
-                { "poh-size", false },
-                { "assembly-count", false },
-                { "il-bytes-jitted", false },
-                { "methods-jitted-count", false },
-                { "time-in-jit", false }
-            };
-        }
-        private Dictionary<string, bool> observedRuntimeCounters;
+        private Dictionary<string, bool> observedEvents = new Dictionary<string, bool>() {
+            { "cpu-usage" , false },
+            { "working-set", false },
+            { "gc-heap-size", false },
+            { "gen-0-gc-count", false },
+            { "gen-1-gc-count", false },
+            { "gen-2-gc-count", false },
+            { "threadpool-thread-count", false },
+            { "monitor-lock-contention-count", false },
+            { "threadpool-queue-length", false },
+            { "threadpool-completed-items-count", false },
+            { "alloc-rate", false },
+            { "active-timer-count", false },
+            { "gc-fragmentation", false },
+            { "gc-committed", false },
+            { "exception-count", false },
+            { "time-in-gc", false },
+            { "gen-0-size", false },
+            { "gen-1-size", false },
+            { "gen-2-size", false },
+            { "loh-size", false },
+            { "poh-size", false },
+            { "assembly-count", false },
+            { "il-bytes-jitted", false },
+            { "methods-jitted-count", false },
+            { "time-in-jit", false },
+            // These are AppContext switches, not counters
+            { "appContextSwitch", false },
+            { "appContextData", false }
+        };
 
         protected override void OnEventSourceCreated(EventSource source)
         {
@@ -60,7 +59,17 @@ namespace RuntimeEventCounterTests
 
         protected override void OnEventWritten(EventWrittenEventArgs eventData)
         {
+            // Check AppContext switches
+            if (eventData is { EventName: "LogAppContextSwitch",
+                               Payload: { Count: 2 } } &&
+                eventData.Payload[0] is string switchName &&
+                observedEvents.ContainsKey(switchName))
+            {
+                observedEvents[switchName] = true;
+                return;
+            }
 
+            // Check counters
             for (int i = 0; i < eventData.Payload.Count; i++)
             {
                 IDictionary<string, object> eventPayload = eventData.Payload[i] as IDictionary<string, object>;
@@ -69,7 +78,7 @@ namespace RuntimeEventCounterTests
                     foreach (KeyValuePair<string, object> payload in eventPayload)
                     {
                         if (payload.Key.Equals("Name"))
-                            observedRuntimeCounters[payload.Value.ToString()] = true;
+                            observedEvents[payload.Value.ToString()] = true;
                     }
                 }
             }
@@ -77,11 +86,10 @@ namespace RuntimeEventCounterTests
 
         public bool Verify()
         {
-            foreach (string counterName in observedRuntimeCounters.Keys)
+            foreach (string counterName in observedEvents.Keys)
             {
-                if (!observedRuntimeCounters[counterName])
+                if (!observedEvents[counterName])
                 {
-                    Console.WriteLine($"Did not see {counterName}");
                     return false;
                 }
                 else
@@ -97,10 +105,13 @@ namespace RuntimeEventCounterTests
     {
         public static int Main(string[] args)
         {
+            AppContext.SetSwitch("appContextSwitch", true);
+            AppDomain.CurrentDomain.SetData("appContextData", true);
+
             // Create an EventListener.
             using (RuntimeCounterListener myListener = new RuntimeCounterListener())
             {
-                Thread.Sleep(3000); 
+                Thread.Sleep(3000);
                 if (myListener.Verify())
                 {
                     Console.WriteLine("Test passed");
