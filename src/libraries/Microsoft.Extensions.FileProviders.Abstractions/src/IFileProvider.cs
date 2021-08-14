ef86a4d7992a3731071d0321f800329792ceaa6d
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2021-08-14 17:01:18 +0300

    Annotate src

--- src/libraries/Microsoft.Extensions.FileProviders.Abstractions/src/IFileProvider.cs
+++ src/libraries/Microsoft.Extensions.FileProviders.Abstractions/src/IFileProvider.cs
@@ -15,20 +15,20 @@ namespace Microsoft.Extensions.FileProviders
         /// </summary>
         /// <param name="subpath">Relative path that identifies the file.</param>
         /// <returns>The file information. Caller must check Exists property.</returns>
-        IFileInfo GetFileInfo(string subpath);
+        IFileInfo GetFileInfo(string? subpath);
 
         /// <summary>
         /// Enumerate a directory at the given path, if any.
         /// </summary>
         /// <param name="subpath">Relative path that identifies the directory.</param>
         /// <returns>Returns the contents of the directory.</returns>
-        IDirectoryContents GetDirectoryContents(string subpath);
+        IDirectoryContents GetDirectoryContents(string? subpath);
 
         /// <summary>
         /// Creates a <see cref="IChangeToken"/> for the specified <paramref name="filter"/>.
         /// </summary>
         /// <param name="filter">Filter string used to determine what files or folders to monitor. Example: **/*.cs, *.*, subFolder/**/*.cshtml.</param>
         /// <returns>An <see cref="IChangeToken"/> that is notified when a file matching <paramref name="filter"/> is added, modified or deleted.</returns>
-        IChangeToken Watch(string filter);
+        IChangeToken Watch(string? filter);
     }
 }
--- src/libraries/Microsoft.Extensions.FileProviders.Abstractions/src/NullFileProvider.cs
+++ src/libraries/Microsoft.Extensions.FileProviders.Abstractions/src/NullFileProvider.cs
@@ -15,20 +15,20 @@ namespace Microsoft.Extensions.FileProviders
         /// </summary>
         /// <param name="subpath">A path under the root directory. This parameter is ignored.</param>
         /// <returns>A <see cref="IDirectoryContents"/> that does not exist and does not contain any contents.</returns>
-        public IDirectoryContents GetDirectoryContents(string subpath) => NotFoundDirectoryContents.Singleton;
+        public IDirectoryContents GetDirectoryContents(string? subpath) => NotFoundDirectoryContents.Singleton;
 
         /// <summary>
         /// Locate a non-existent file.
         /// </summary>
         /// <param name="subpath">A path under the root directory.</param>
         /// <returns>A <see cref="IFileInfo"/> representing a non-existent file at the given path.</returns>
-        public IFileInfo GetFileInfo(string subpath) => new NotFoundFileInfo(subpath);
+        public IFileInfo GetFileInfo(string? subpath) => new NotFoundFileInfo(subpath);
 
         /// <summary>
         /// Returns a <see cref="IChangeToken"/> that monitors nothing.
         /// </summary>
         /// <param name="filter">Filter string used to determine what files or folders to monitor. This parameter is ignored.</param>
         /// <returns>A <see cref="IChangeToken"/> that does not register callbacks.</returns>
-        public IChangeToken Watch(string filter) => NullChangeToken.Singleton;
+        public IChangeToken Watch(string? filter) => NullChangeToken.Singleton;
     }
 }
