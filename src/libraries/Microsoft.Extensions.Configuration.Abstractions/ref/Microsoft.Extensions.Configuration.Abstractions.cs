8a85d256a9bfc7b3e5114b3af8909b5a8e480dd3
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2021-08-14 15:08:34 +0300

    Annotate ref

--- src/libraries/Microsoft.Extensions.Configuration.Abstractions/ref/Microsoft.Extensions.Configuration.Abstractions.cs
+++ src/libraries/Microsoft.Extensions.Configuration.Abstractions/ref/Microsoft.Extensions.Configuration.Abstractions.cs
@@ -4,15 +4,17 @@
 // Changes to this file must follow the https://aka.ms/api-review process.
 // ------------------------------------------------------------------------------
 
+using System.Diagnostics.CodeAnalysis;
+
 namespace Microsoft.Extensions.Configuration
 {
     public static partial class ConfigurationExtensions
     {
-        public static Microsoft.Extensions.Configuration.IConfigurationBuilder Add<TSource>(this Microsoft.Extensions.Configuration.IConfigurationBuilder builder, System.Action<TSource> configureSource) where TSource : Microsoft.Extensions.Configuration.IConfigurationSource, new() { throw null; }
-        public static System.Collections.Generic.IEnumerable<System.Collections.Generic.KeyValuePair<string, string>> AsEnumerable(this Microsoft.Extensions.Configuration.IConfiguration configuration) { throw null; }
-        public static System.Collections.Generic.IEnumerable<System.Collections.Generic.KeyValuePair<string, string>> AsEnumerable(this Microsoft.Extensions.Configuration.IConfiguration configuration, bool makePathsRelative) { throw null; }
-        public static bool Exists(this Microsoft.Extensions.Configuration.IConfigurationSection section) { throw null; }
-        public static string GetConnectionString(this Microsoft.Extensions.Configuration.IConfiguration configuration, string name) { throw null; }
+        public static Microsoft.Extensions.Configuration.IConfigurationBuilder Add<TSource>(this Microsoft.Extensions.Configuration.IConfigurationBuilder builder, System.Action<TSource>? configureSource) where TSource : Microsoft.Extensions.Configuration.IConfigurationSource, new() { throw null; }
+        public static System.Collections.Generic.IEnumerable<System.Collections.Generic.KeyValuePair<string, string?>> AsEnumerable(this Microsoft.Extensions.Configuration.IConfiguration configuration) { throw null; }
+        public static System.Collections.Generic.IEnumerable<System.Collections.Generic.KeyValuePair<string, string?>> AsEnumerable(this Microsoft.Extensions.Configuration.IConfiguration configuration, bool makePathsRelative) { throw null; }
+        public static bool Exists([NotNullWhen(true)] this Microsoft.Extensions.Configuration.IConfigurationSection? section) { throw null; }
+        public static string? GetConnectionString(this Microsoft.Extensions.Configuration.IConfiguration configuration, string name) { throw null; }
         public static Microsoft.Extensions.Configuration.IConfigurationSection GetRequiredSection(this Microsoft.Extensions.Configuration.IConfiguration configuration, string key) { throw null; }
     }
     [System.AttributeUsageAttribute(System.AttributeTargets.Property)]
@@ -26,8 +28,9 @@ namespace Microsoft.Extensions.Configuration
         public static readonly string KeyDelimiter;
         public static string Combine(System.Collections.Generic.IEnumerable<string> pathSegments) { throw null; }
         public static string Combine(params string[] pathSegments) { throw null; }
-        public static string GetParentPath(string path) { throw null; }
-        public static string GetSectionKey(string path) { throw null; }
+        public static string? GetParentPath(string? path) { throw null; }
+        [return: NotNullIfNotNull("path")]
+        public static string? GetSectionKey(string? path) { throw null; }
     }
     public static partial class ConfigurationRootExtensions
     {
@@ -35,7 +38,7 @@ namespace Microsoft.Extensions.Configuration
     }
     public partial interface IConfiguration
     {
-        string this[string key] { get; set; }
+        string? this[string key] { get; set; }
         System.Collections.Generic.IEnumerable<Microsoft.Extensions.Configuration.IConfigurationSection> GetChildren();
         Microsoft.Extensions.Primitives.IChangeToken GetReloadToken();
         Microsoft.Extensions.Configuration.IConfigurationSection GetSection(string key);
@@ -49,11 +52,11 @@ namespace Microsoft.Extensions.Configuration
     }
     public partial interface IConfigurationProvider
     {
-        System.Collections.Generic.IEnumerable<string> GetChildKeys(System.Collections.Generic.IEnumerable<string> earlierKeys, string parentPath);
+        System.Collections.Generic.IEnumerable<string> GetChildKeys(System.Collections.Generic.IEnumerable<string> earlierKeys, string? parentPath);
         Microsoft.Extensions.Primitives.IChangeToken GetReloadToken();
         void Load();
-        void Set(string key, string value);
-        bool TryGet(string key, out string value);
+        void Set(string key, string? value);
+        bool TryGet(string key, [MaybeNullWhen(false)] out string value);
     }
     public partial interface IConfigurationRoot : Microsoft.Extensions.Configuration.IConfiguration
     {
@@ -64,7 +67,7 @@ namespace Microsoft.Extensions.Configuration
     {
         string Key { get; }
         string Path { get; }
-        string Value { get; set; }
+        string? Value { get; set; }
     }
     public partial interface IConfigurationSource
     {
