3cccc99fa99a644c49db0bd3c788e2f582bdef1e
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2021-08-14 17:05:39 +0300

    Annotate ref

--- src/libraries/Microsoft.Extensions.FileProviders.Abstractions/ref/Microsoft.Extensions.FileProviders.Abstractions.cs
+++ src/libraries/Microsoft.Extensions.FileProviders.Abstractions/ref/Microsoft.Extensions.FileProviders.Abstractions.cs
@@ -17,14 +17,14 @@ namespace Microsoft.Extensions.FileProviders
         System.DateTimeOffset LastModified { get; }
         long Length { get; }
         string Name { get; }
-        string PhysicalPath { get; }
+        string? PhysicalPath { get; }
         System.IO.Stream CreateReadStream();
     }
     public partial interface IFileProvider
     {
-        Microsoft.Extensions.FileProviders.IDirectoryContents GetDirectoryContents(string subpath);
-        Microsoft.Extensions.FileProviders.IFileInfo GetFileInfo(string subpath);
-        Microsoft.Extensions.Primitives.IChangeToken Watch(string filter);
+        Microsoft.Extensions.FileProviders.IDirectoryContents GetDirectoryContents(string? subpath);
+        Microsoft.Extensions.FileProviders.IFileInfo GetFileInfo(string? subpath);
+        Microsoft.Extensions.Primitives.IChangeToken Watch(string? filter);
     }
     public partial class NotFoundDirectoryContents : Microsoft.Extensions.FileProviders.IDirectoryContents, System.Collections.Generic.IEnumerable<Microsoft.Extensions.FileProviders.IFileInfo>, System.Collections.IEnumerable
     {
@@ -42,7 +42,7 @@ namespace Microsoft.Extensions.FileProviders
         public System.DateTimeOffset LastModified { get { throw null; } }
         public long Length { get { throw null; } }
         public string Name { get { throw null; } }
-        public string PhysicalPath { get { throw null; } }
+        public string? PhysicalPath { get { throw null; } }
         public System.IO.Stream CreateReadStream() { throw null; }
     }
     public partial class NullChangeToken : Microsoft.Extensions.Primitives.IChangeToken
@@ -51,13 +51,13 @@ namespace Microsoft.Extensions.FileProviders
         public bool ActiveChangeCallbacks { get { throw null; } }
         public bool HasChanged { get { throw null; } }
         public static Microsoft.Extensions.FileProviders.NullChangeToken Singleton { get { throw null; } }
-        public System.IDisposable RegisterChangeCallback(System.Action<object> callback, object state) { throw null; }
+        public System.IDisposable RegisterChangeCallback(System.Action<object?> callback, object? state) { throw null; }
     }
     public partial class NullFileProvider : Microsoft.Extensions.FileProviders.IFileProvider
     {
         public NullFileProvider() { }
-        public Microsoft.Extensions.FileProviders.IDirectoryContents GetDirectoryContents(string subpath) { throw null; }
-        public Microsoft.Extensions.FileProviders.IFileInfo GetFileInfo(string subpath) { throw null; }
-        public Microsoft.Extensions.Primitives.IChangeToken Watch(string filter) { throw null; }
+        public Microsoft.Extensions.FileProviders.IDirectoryContents GetDirectoryContents(string? subpath) { throw null; }
+        public Microsoft.Extensions.FileProviders.IFileInfo GetFileInfo(string? subpath) { throw null; }
+        public Microsoft.Extensions.Primitives.IChangeToken Watch(string? filter) { throw null; }
     }
 }
