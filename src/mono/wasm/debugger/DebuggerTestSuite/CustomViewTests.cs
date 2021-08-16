91673def34f04568fa016d21efea9d793cb17975
Thays Grazia <thaystg@gmail.com>
2021-08-16 18:52:06 -0300

    [debugger][wasm] Support DebuggerProxyAttribute (#56872)
    * Implementing debugger proxy
    
    * fix compilation
    
    * Implement debuggerproxy attribute.
    
    * Reusing code for DebuggerProxy method and DebuggerDisplay method.
    
    * Fix unit tests.
    
    * Fixing unit tests that uses List<T>.
    
    * Fix unit tests that uses List.
    
    * Addressing @radical comments.
    
    * Using flags enum as suggested by @lewing.
    
    * Fixing merge.
    
    * Addressing @radical comments.
    
    * Addressing @radical comments.
    
    Co-authored-by: Larry Ewing <lewing@microsoft.com>

--- src/mono/wasm/debugger/DebuggerTestSuite/CustomViewTests.cs
+++ src/mono/wasm/debugger/DebuggerTestSuite/CustomViewTests.cs
@@ -15,9 +15,9 @@ namespace DebuggerTests
     public class CustomViewTests : DebuggerTestBase
     {
         [Fact]
-        public async Task CustomView()
+        public async Task UsingDebuggerDisplay()
         {
-            var bp = await SetBreakpointInMethod("debugger-test.dll", "DebuggerTests.DebuggerCustomViewTest", "run", 5);
+            var bp = await SetBreakpointInMethod("debugger-test.dll", "DebuggerTests.DebuggerCustomViewTest", "run", 6);
             var pause_location = await EvaluateAndCheck(
                 "window.setTimeout(function() { invoke_static_method ('[debugger-test] DebuggerTests.DebuggerCustomViewTest:run'); }, 1);",
                 "dotnet://debugger-test.dll/debugger-custom-view-test.cs",
@@ -27,7 +27,35 @@ namespace DebuggerTests
 
             var locals = await GetProperties(pause_location["callFrames"][0]["callFrameId"].Value<string>());
             CheckObject(locals, "a", "DebuggerTests.WithDisplayString", description:"Some one Value 2 End");
-			  CheckObject(locals, "c", "DebuggerTests.DebuggerDisplayMethodTest", description: "First Int:32 Second Int:43");
+            CheckObject(locals, "c", "DebuggerTests.DebuggerDisplayMethodTest", description: "First Int:32 Second Int:43");
+            CheckObject(locals, "myList", "System.Collections.Generic.List<int>", description: "Count = 4");
+        }
+
+        [Fact]
+        public async Task UsingDebuggerTypeProxy()
+        {
+            var bp = await SetBreakpointInMethod("debugger-test.dll", "DebuggerTests.DebuggerCustomViewTest", "run", 6);
+            var pause_location = await EvaluateAndCheck(
+                "window.setTimeout(function() { invoke_static_method ('[debugger-test] DebuggerTests.DebuggerCustomViewTest:run'); }, 1);",
+                "dotnet://debugger-test.dll/debugger-custom-view-test.cs",
+                bp.Value["locations"][0]["lineNumber"].Value<int>(),
+                bp.Value["locations"][0]["columnNumber"].Value<int>(),
+                "run");
+
+            var frame = pause_location["callFrames"][0];
+            var locals = await GetProperties(frame["callFrameId"].Value<string>());
+            CheckObject(locals, "myList", "System.Collections.Generic.List<int>", description: "Count = 4");
+            var props = await GetObjectOnFrame(frame, "myList");
+            Assert.Equal(1, props.Count());
+
+            CheckArray(props, "Items", "int[]", 4);
+
+            CheckObject(locals, "b", "DebuggerTests.WithProxy", description:"DebuggerTests.WithProxy");
+            props = await GetObjectOnFrame(frame, "b");
+            CheckString(props, "Val2", "one");
+
+            await EvaluateOnCallFrameAndCheck(frame["callFrameId"].Value<string>(),
+                ("listToTestToList.ToList()", TObject("System.Collections.Generic.List<int>", description: "Count = 11")));
         }
     }
 }
