f98dfa752c3547b2f9f5d66aab002e73694a2b3d
Mitchell Hwang <mitchell.hwang@microsoft.com>
2021-08-16 15:52:17 -0400

    Address crashes and PNSE failures on iOS MacCatalyst

--- src/libraries/System.Runtime.Extensions/tests/System/EnvironmentTests.cs
+++ src/libraries/System.Runtime.Extensions/tests/System/EnvironmentTests.cs
@@ -347,7 +347,7 @@ namespace System.Tests
                 Assert.Equal(Environment.GetEnvironmentVariable("HOME"), Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
             }
             // tvOS effectively doesn't have a HOME
-            if (!PlatformDetection.IstvOS)
+            if (!PlatformDetection.IsiOS && !Platform.Detection.IstvOS)
             {
                 Assert.Equal(Environment.GetEnvironmentVariable("HOME"), Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
             }
