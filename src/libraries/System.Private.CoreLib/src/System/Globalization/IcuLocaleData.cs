e62d744eb8df1d0c500fe24cb162383f95c85547
Tarek Mahmoud Sayed <tarekms@microsoft.com>
2021-08-18 19:39:48 -0700

    Fix Culture creation regression when using invalid names on Windows

--- src/libraries/System.Private.CoreLib/src/System/Globalization/IcuLocaleData.cs
+++ src/libraries/System.Private.CoreLib/src/System/Globalization/IcuLocaleData.cs
@@ -3816,10 +3816,9 @@ namespace System.Globalization
             for (int i = 0; i < name.Length; ++i)
             {
                 char ch = name[i];
-                if (ch > 'z')
-                    return -1;
 
-                lower_case[i] = (byte)(ch | 0x20);
+                Debug.Assert(ch <= 'z');
+                lower_case[i] = ((uint)(ch - 'A') <= 'Z' - 'A') ? (byte)(ch | 0x20) : (byte)ch;
             }
 
             ReadOnlySpan<byte> lname = lower_case;
