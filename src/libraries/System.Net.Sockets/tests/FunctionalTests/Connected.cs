bebda1c39a9ba4178a183255be246d0621654805
Tom Deseyn <tom.deseyn@gmail.com>
2021-08-19 20:14:06 +0200

    Socket: check connect result when non-blocking connect completes.
    In addition to checking whether the socket is writable to know the
    connect completed, also read SO_ERROR to know if the connect was
    succesful before setting the Socket state to connected.

---
+++ src/libraries/System.Net.Sockets/tests/FunctionalTests/Connected.cs
@@ -0,0 +1,23 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Xunit;
+
+namespace System.Net.Sockets.Tests
+{
+    public class Connected
+    {
+        [Fact]
+        public void NonBlockingFailedConnect_ConnectedReturnsFalse()
+        {
+            using Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            socket.Blocking = false;
+
+            SocketException se = Assert.ThrowsAny<SocketException>(() => socket.Connect(IPAddress.Loopback, 0));
+
+            Assert.Equal(SocketError.WouldBlock, se.SocketErrorCode);
+            Assert.False(socket.Connected);
+        }
+    }
+}
