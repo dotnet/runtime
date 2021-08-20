239f510bb24a263304e00d2e02c092fbae88e758
Filip Toth <filip.toth@accelley.com>
2021-08-20 14:36:40 -0400

    Fix trailing whitespaces in XML comments

--- src/libraries/System.Net.Http/src/System/Net/Http/HttpContent.cs
+++ src/libraries/System.Net.Http/src/System/Net/Http/HttpContent.cs
@@ -554,8 +554,8 @@ namespace System.Net.Http
         ///<summary>
         /// Creates a <see cref="Stream"/> to read the content from.
         ///</summary>
-        ///<param name="cancellationToken"><see cref="CancellationToken"> to register in case the stream needs to be disposed.</param>
-        ///<returns>The output <see cref="Stream"/>.</returns> 
+        ///<param name="cancellationToken"><see cref="CancellationToken"/> to register in case the stream needs to be disposed.</param>
+        ///<returns>The output <see cref="Stream"/>.</returns>
         protected virtual Stream CreateContentReadStream(CancellationToken cancellationToken)
         {
             LoadIntoBuffer(MaxBufferSize, cancellationToken);
