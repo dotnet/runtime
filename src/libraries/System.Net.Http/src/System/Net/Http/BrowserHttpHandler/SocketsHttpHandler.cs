1c747a49d048432a0624502b0cbdc346aeb95ebf
Filip Toth <filip.toth@accelley.com>
2021-08-22 09:55:54 -0400

    Add documentation to `\BrowserHttpHandler\SocketsHttpHandler.cs`

--- src/libraries/System.Net.Http/src/System/Net/Http/BrowserHttpHandler/SocketsHttpHandler.cs
+++ src/libraries/System.Net.Http/src/System/Net/Http/BrowserHttpHandler/SocketsHttpHandler.cs
@@ -15,6 +15,9 @@ namespace System.Net.Http
     [UnsupportedOSPlatform("browser")]
     public sealed class SocketsHttpHandler : HttpMessageHandler
     {
+        ///<summary>
+        ///Gets a value that indicates whether the this Type is supported.
+        ///<summary/>
         [UnsupportedOSPlatformGuard("browser")]
         public static bool IsSupported => false;
 
@@ -46,72 +49,108 @@ namespace System.Net.Http
             set => throw new PlatformNotSupportedException();
         }
 
+        ///<summary>
+        ///Throws a <see cref="PlatformNotSupportedException"/> exception in all cases.
+        ///</summary>
         public bool UseProxy
         {
             get => throw new PlatformNotSupportedException();
             set => throw new PlatformNotSupportedException();
         }
 
+        ///<summary>
+        ///Throws a <see cref="PlatformNotSupportedException"/> exception in all cases.
+        ///</summary>
         public IWebProxy? Proxy
         {
             get => throw new PlatformNotSupportedException();
             set => throw new PlatformNotSupportedException();
         }
 
+        ///<summary>
+        ///Throws a <see cref="PlatformNotSupportedException"/> exception in all cases.
+        ///</summary>
         public ICredentials? DefaultProxyCredentials
         {
             get => throw new PlatformNotSupportedException();
             set => throw new PlatformNotSupportedException();
         }
 
+        ///<summary>
+        ///Throws a <see cref="PlatformNotSupportedException"/> exception in all cases.
+        ///</summary>
         public bool PreAuthenticate
         {
             get => throw new PlatformNotSupportedException();
             set => throw new PlatformNotSupportedException();
         }
 
+        ///<summary>
+        ///Throws a <see cref="PlatformNotSupportedException"/> exception in all cases.
+        ///</summary>
         public ICredentials? Credentials
         {
             get => throw new PlatformNotSupportedException();
             set => throw new PlatformNotSupportedException();
         }
 
+        ///<summary>
+        ///Throws a <see cref="PlatformNotSupportedException"/> exception in all cases.
+        ///</summary>
         public bool AllowAutoRedirect
         {
             get => throw new PlatformNotSupportedException();
             set => throw new PlatformNotSupportedException();
         }
 
+        ///<summary>
+        ///Throws a <see cref="PlatformNotSupportedException"/> exception in all cases.
+        ///</summary>
         public int MaxAutomaticRedirections
         {
             get => throw new PlatformNotSupportedException();
             set => throw new PlatformNotSupportedException();
         }
 
+        ///<summary>
+        ///Throws a <see cref="PlatformNotSupportedException"/> exception in all cases.
+        ///</summary>
         public int MaxConnectionsPerServer
         {
             get => throw new PlatformNotSupportedException();
             set => throw new PlatformNotSupportedException();
         }
 
+        ///<summary>
+        ///Throws a <see cref="PlatformNotSupportedException"/> exception in all cases.
+        ///</summary>
         public int MaxResponseDrainSize
         {
             get => throw new PlatformNotSupportedException();
             set => throw new PlatformNotSupportedException();
         }
 
+        ///<summary>
+        ///Throws a <see cref="PlatformNotSupportedException"/> exception in all cases.
+        ///</summary>
         public TimeSpan ResponseDrainTimeout
         {
             get => throw new PlatformNotSupportedException();
             set => throw new PlatformNotSupportedException();
         }
 
+        ///<summary>
+        ///Throws a <see cref="PlatformNotSupportedException"/> exception in all cases.
+        ///</summary>
         public int MaxResponseHeadersLength
         {
             get => throw new PlatformNotSupportedException();
             set => throw new PlatformNotSupportedException();
         }
 
+        ///<summary>
+        ///Throws a <see cref="PlatformNotSupportedException"/> exception in all cases.
+        ///</summary>
         [AllowNull]
         public SslClientAuthenticationOptions SslOptions
         {
@@ -119,68 +158,104 @@ namespace System.Net.Http
             set => throw new PlatformNotSupportedException();
         }
 
+        ///<summary>
+        ///Throws a <see cref="PlatformNotSupportedException"/> exception in all cases.
+        ///</summary>
         public TimeSpan PooledConnectionLifetime
         {
             get => throw new PlatformNotSupportedException();
             set => throw new PlatformNotSupportedException();
         }
 
+        ///<summary>
+        ///Throws a <see cref="PlatformNotSupportedException"/> exception in all cases.
+        ///</summary>
         public TimeSpan PooledConnectionIdleTimeout
         {
             get => throw new PlatformNotSupportedException();
             set => throw new PlatformNotSupportedException();
         }
 
+        ///<summary>
+        ///Throws a <see cref="PlatformNotSupportedException"/> exception in all cases.
+        ///</summary>
         public TimeSpan ConnectTimeout
         {
             get => throw new PlatformNotSupportedException();
             set => throw new PlatformNotSupportedException();
         }
 
+        ///<summary>
+        ///Throws a <see cref="PlatformNotSupportedException"/> exception in all cases.
+        ///</summary>
         public TimeSpan Expect100ContinueTimeout
         {
             get => throw new PlatformNotSupportedException();
             set => throw new PlatformNotSupportedException();
         }
 
+        ///<summary>
+        ///Throws a <see cref="PlatformNotSupportedException"/> exception in all cases.
+        ///</summary>
         public int InitialHttp2StreamWindowSize
         {
             get => throw new PlatformNotSupportedException();
             set => throw new PlatformNotSupportedException();
         }
 
+        ///<summary>
+        ///Throws a <see cref="PlatformNotSupportedException"/> exception in all cases.
+        ///</summary>
         public TimeSpan KeepAlivePingDelay
         {
             get => throw new PlatformNotSupportedException();
             set => throw new PlatformNotSupportedException();
         }
 
+        ///<summary>
+        ///Throws a <see cref="PlatformNotSupportedException"/> exception in all cases.
+        ///</summary>
         public TimeSpan KeepAlivePingTimeout
         {
             get => throw new PlatformNotSupportedException();
             set => throw new PlatformNotSupportedException();
         }
 
+        ///<summary>
+        ///Throws a <see cref="PlatformNotSupportedException"/> exception in all cases.
+        ///</summary>
         public HttpKeepAlivePingPolicy KeepAlivePingPolicy
         {
             get => throw new PlatformNotSupportedException();
             set => throw new PlatformNotSupportedException();
         }
 
+        ///<summary>
+        ///Throws a <see cref="PlatformNotSupportedException"/> exception in all cases.
+        ///</summary>
         public IDictionary<string, object?> Properties => throw new PlatformNotSupportedException();
 
+        ///<summary>
+        ///Throws a <see cref="PlatformNotSupportedException"/> exception in all cases.
+        ///</summary>
         public HeaderEncodingSelector<HttpRequestMessage>? RequestHeaderEncodingSelector
         {
             get => throw new PlatformNotSupportedException();
             set => throw new PlatformNotSupportedException();
         }
 
+        ///<summary>
+        ///Throws a <see cref="PlatformNotSupportedException"/> exception in all cases.
+        ///</summary>
         public HeaderEncodingSelector<HttpRequestMessage>? ResponseHeaderEncodingSelector
         {
             get => throw new PlatformNotSupportedException();
             set => throw new PlatformNotSupportedException();
         }
 
+        ///<summary>
+        ///Throws a <see cref="PlatformNotSupportedException"/> exception in all cases.
+        ///</summary>
         [CLSCompliant(false)]
         public DistributedContextPropagator? ActivityHeadersPropagator
         {
@@ -188,21 +263,33 @@ namespace System.Net.Http
             set => throw new PlatformNotSupportedException();
         }
 
+        ///<summary>
+        ///Throws a <see cref="PlatformNotSupportedException"/> exception in all cases.
+        ///</summary>
         protected internal override Task<HttpResponseMessage> SendAsync(
             HttpRequestMessage request, CancellationToken cancellationToken) => throw new PlatformNotSupportedException();
 
+        ///<summary>
+        ///Throws a <see cref="PlatformNotSupportedException"/> exception in all cases.
+        ///</summary>
         public bool EnableMultipleHttp2Connections
         {
             get => throw new PlatformNotSupportedException();
             set => throw new PlatformNotSupportedException();
         }
 
+        ///<summary>
+        ///Throws a <see cref="PlatformNotSupportedException"/> exception in all cases.
+        ///</summary>
         public Func<SocketsHttpConnectionContext, CancellationToken, ValueTask<Stream>>? ConnectCallback
         {
             get => throw new PlatformNotSupportedException();
             set => throw new PlatformNotSupportedException();
         }
 
+        ///<summary>
+        ///Throws a <see cref="PlatformNotSupportedException"/> exception in all cases.
+        ///</summary>
         public Func<SocketsHttpPlaintextStreamFilterContext, CancellationToken, ValueTask<Stream>>? PlaintextStreamFilter
         {
             get => throw new PlatformNotSupportedException();
