52898fd0203b873cc384f5b2ae09d1ff4940c128
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2021-08-22 18:38:05 +0800

    Annotate remaining types

--- src/libraries/Common/src/Interop/Windows/Crypt32/Interop.CertOpenStore.cs
+++ src/libraries/Common/src/Interop/Windows/Crypt32/Interop.CertOpenStore.cs
@@ -14,6 +14,6 @@ internal static partial class Interop
         internal const uint CERT_STORE_PROV_MEMORY = 2;
 
         [DllImport(Interop.Libraries.Crypt32, CharSet = CharSet.Unicode, SetLastError = true)]
-        internal static extern SafeCertStoreHandle CertOpenStore(IntPtr lpszStoreProvider, uint dwMsgAndCertEncodingType, IntPtr hCryptProv, uint dwFlags, string pvPara);
+        internal static extern SafeCertStoreHandle CertOpenStore(IntPtr lpszStoreProvider, uint dwMsgAndCertEncodingType, IntPtr hCryptProv, uint dwFlags, string? pvPara);
     }
 }
