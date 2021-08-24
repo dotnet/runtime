82d1a43536eb90385d5d9b762b2ad6d7593f146e
Steve Pfister <steve.pfister@microsoft.com>
2021-08-24 16:50:18 -0400

    Feedback

--- src/libraries/Native/Unix/System.Security.Cryptography.Native.Apple/pal_ssl.c
+++ src/libraries/Native/Unix/System.Security.Cryptography.Native.Apple/pal_ssl.c
@@ -596,7 +596,7 @@ int32_t AppleCryptoNative_SslSetEnabledCipherSuites(SSLContextRef sslContext, co
     // Max numCipherSuites is 2^16 (all possible cipher suites)
     assert(numCipherSuites < (1 << 16));
 
-#if !defined(TARGET_IOS) && !defined(TARGET_TVOS)
+#if !defined(TARGET_ARM64) && !defined(TARGET_IOS) && !defined(TARGET_TVOS)
     if (sizeof(SSLCipherSuite) == sizeof(uint32_t))
     {
 #pragma clang diagnostic push
