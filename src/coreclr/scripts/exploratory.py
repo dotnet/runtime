716a1a96e7733e71b35d2d94f04f462825d7f4f9
Kunal Pathak <Kunal.Pathak@microsoft.com>
2021-08-24 12:31:07 -0700

    Exploratory tool pipeline
    Add pipeline to run exploratory tools and return back the result

---
+++ src/coreclr/scripts/exploratory.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python3
+#
+## Licensed to the .NET Foundation under one or more agreements.
+## The .NET Foundation licenses this file to you under the MIT license.
+#
+##
+# Title: exploratory.py
+#
+# Notes:
+#
+# Script to execute exploratory tool on a platform and return back the repro
+# issues they found.
+#
+################################################################################
+################################################################################
+
+import shutil
+import argparse
+from os import linesep, listdir, path, walk
+from os.path import isfile, join, getsize
+import os
+from os import listdir
+from coreclr_arguments import *
+from superpmi_setup import run_command, copy_directory, set_pipeline_variable
+from superpmi import TempDir
+
+parser = argparse.ArgumentParser(description="description")
+
+parser.add_argument("-run_configuration", help="RunConfiguration")
+parser.add_argument("-exploratory_directory", help="Path to exploratory tool")
+parser.add_argument("-output_directory", help="Path to output directory")
+parser.add_argument("-partition", help="Partition name")
+parser.add_argument("-core_root", help="path to CORE_ROOT directory")
+parser.add_argument("-tool_name", help="tool name")
+is_windows = platform.system() == "Windows"
+
+
+def setup_args(args):
+    """ Setup the args for SuperPMI to use.
+
+    Args:
+        args (ArgParse): args parsed by arg parser
+
+    Returns:
+        args (CoreclrArguments)
+
+    """
+    coreclr_args = CoreclrArguments(args, require_built_core_root=False, require_built_product_dir=False,
+                                    require_built_test_dir=False, default_build_type="Checked")
+
+    coreclr_args.verify(args,
+                        "run_configuration",
+                        lambda unused: True,
+                        "Unable to set run_configuration")
+
+    coreclr_args.verify(args,
+                        "exploratory_directory",
+                        lambda exploratory_directory: os.path.isdir(exploratory_directory),
+                        "exploratory_directory doesn't exist")
+
+    coreclr_args.verify(args,
+                        "output_directory",
+                        lambda unused: True,
+                        "output_directory doesn't exist")
+
+    coreclr_args.verify(args,
+                        "partition",
+                        lambda unused: True,
+                        "Unable to set partition")
+
+    coreclr_args.verify(args,
+                        "tool_name",
+                        lambda unused: True,
+                        "Unable to set tool_name")
+
+    coreclr_args.verify(args,
+                        "core_root",
+                        lambda core_root: os.path.isdir(core_root),
+                        "core_root doesn't exist")
+
+    return coreclr_args
+
+
+def copy_issues(issues_directory, upload_directory, tag_name):
+    """Copies issue files (only top 5 smallest files from each folder) into the upload_directory
+
+    Args:
+        issues_directory (string): Issues directory
+        upload_directory (string): Upload directory
+        tag_name (string): Tag name for zip file
+
+    Returns:
+        [type]: [description]
+    """
+    # Create upload_directory
+    if not os.path.isdir(upload_directory):
+        os.makedirs(upload_directory)
+
+    # Create temp directory to copy all issues to upload. We don't want to create a sub-folder
+    # inside issues_directory because that will also get included twice.
+    with TempDir() as prep_directory:
+
+        def sorter_by_size(pair):
+            """ Sorts the pair (file_name, file_size) tuple in ascending order of file_size
+
+            Args:
+                pair ([(string, int)]): List of tuple of file_name, file_size
+            """
+            pair.sort(key=lambda x: x[1], reverse=False)
+            return pair
+
+        summary_of_summary = []
+        for file_path, dirs, files in walk(issues_directory, topdown=True):
+            filename_with_size = []
+            # Credit: https://stackoverflow.com/a/19859907
+            dirs[:] = [d for d in dirs]
+            for name in files:
+                if not name.lower().endswith(".g.cs"):
+                    continue
+
+                curr_file_path = path.join(file_path, name)
+                size = getsize(curr_file_path)
+                filename_with_size.append((curr_file_path, size))
+
+            if len(filename_with_size) == 0:
+                continue
+            summary_file = path.join(file_path, "summary.txt")
+            summary_of_summary.append("**** " + file_path)
+            with open(summary_file, 'r') as sf:
+                summary_of_summary.append(sf.read())
+            filename_with_size.append((summary_file, 0))  # always copy summary.txt
+
+            # Copy atmost 5 files from each bucket
+            sorted_files = [f[0] for f in sorter_by_size(filename_with_size)[:6]]  # sorter_by_size(filename_with_size)[:6]
+            print('### Copying below files from {0} to {1}:'.format(issues_directory, prep_directory))
+            print('')
+            print(os.linesep.join(sorted_files))
+            for src_file in sorted_files:
+                dst_file = src_file.replace(issues_directory, prep_directory)
+                dst_directory = path.dirname(dst_file)
+                if not os.path.exists(dst_directory):
+                    os.makedirs(dst_directory)
+                try:
+                    shutil.copy2(src_file, dst_file)
+                except PermissionError as pe_error:
+                    print('Ignoring PermissionError: {0}'.format(pe_error))
+
+        issues_summary_file_name = "issues-summary-{}.txt".format(tag_name)
+        print("Creating {} in {}".format(issues_summary_file_name, upload_directory))
+        issues_summary_file = os.path.join(upload_directory, issues_summary_file_name)
+        with open(issues_summary_file, 'w') as sf:
+            sf.write(os.linesep.join(summary_of_summary))
+
+        # Also copy the issues-summary inside zip folder
+        dst_issue_summary_file = os.path.join(prep_directory, issues_summary_file_name)
+        try:
+            shutil.copy2(issues_summary_file, dst_issue_summary_file)
+        except PermissionError as pe_error:
+            print('Ignoring PermissionError: {0}'.format(pe_error))
+
+        # Zip compress the files we will upload
+        zip_path = os.path.join(prep_directory, "AllIssues-" + tag_name)
+        print("Creating archive: " + zip_path)
+        shutil.make_archive(zip_path, 'zip', prep_directory)
+
+        zip_path += ".zip"
+        dst_zip_path = os.path.join(upload_directory, "AllIssues-" + tag_name + ".zip")
+        print("Copying {} to {}".format(zip_path, dst_zip_path))
+        try:
+            shutil.copy2(zip_path, dst_zip_path)
+        except PermissionError as pe_error:
+            print('Ignoring PermissionError: {0}'.format(pe_error))
+
+
+
+def main(main_args):
+    """Main entrypoint
+
+    Args:
+        main_args ([type]): Arguments to the script
+    """
+
+    coreclr_args = setup_args(main_args)
+
+    exploratory_directory = coreclr_args.exploratory_directory
+    core_root = coreclr_args.core_root
+    tag_name = "{}-{}".format(coreclr_args.run_configuration, coreclr_args.partition)
+    output_directory = coreclr_args.output_directory
+    tool_name = coreclr_args.tool_name
+
+    path_to_corerun = os.path.join(core_root, "corerun.exe" if is_windows else "corerun")
+    path_to_tool = os.path.join(exploratory_directory, "{}.exe".format(tool_name) if is_windows else tool_name)
+
+    # Run tool such that issues are placed in a temp folder
+    with TempDir() as temp_location:
+        run_command([path_to_tool, path_to_corerun, temp_location])
+
+        # Copy issues for upload
+        print("Copying issues to " + output_directory)
+        copy_issues(temp_location, output_directory, tag_name)
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    sys.exit(main(args))
