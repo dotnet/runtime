bc7eee7334ff630dcd08558d32b18eaaf6d69cdc
Pent Ploompuu <kaalikas@gmail.com>
2021-08-24 17:46:20 +0300

    Fix build

--- src/libraries/System.Private.CoreLib/src/System/Globalization/GregorianCalendarHelper.cs
+++ src/libraries/System.Private.CoreLib/src/System/Globalization/GregorianCalendarHelper.cs
@@ -53,12 +53,12 @@ namespace System.Globalization
         //
         internal int MaxYear => m_maxYear;
 
-        private static readonly int[] DaysToMonth365 =
+        internal static readonly int[] DaysToMonth365 =
         {
             0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365
         };
 
-        private static readonly int[] DaysToMonth366 =
+        internal static readonly int[] DaysToMonth366 =
         {
             0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366
         };
