e478b6674c8c9b0bd0997afa81523eaf46bf3622
Alexander Köplinger <alex.koeplinger@outlook.com>
2021-08-24 17:00:44 +0200

    Simplify mobile HttpClientHandler source files (#57766)
    They're 99% identical so we can just use one .cs file with ifdefs instead. This reduces the chance of missing one of the files when a new method is added.

--- src/libraries/System.Net.Http/src/System/Net/Http/HttpClientHandler.AnyMobile.InvokeNativeHandler.cs
+++ src/libraries/System.Net.Http/src/System/Net/Http/HttpClientHandler.AnyMobile.InvokeNativeHandler.cs
@@ -15,8 +15,25 @@ namespace System.Net.Http
     {
         private static MethodInfo? _nativeHandlerMethod;
 
+#if TARGET_ANDROID
+        private const string NativeHandlerType = "Xamarin.Android.Net.AndroidMessageHandler";
+        private const string AssemblyName = "Mono.Android";
+        private const string GetHttpMessageHandlerType = "Android.Runtime.AndroidEnvironment, Mono.Android";
+#elif TARGET_IOS
+        private const string NativeHandlerType = "System.Net.Http.NSUrlSessionHandler";
+        private const string AssemblyName = "Xamarin.iOS";
+        private const string GetHttpMessageHandlerType = "ObjCRuntime.RuntimeOptions, Xamarin.iOS";
+#elif TARGET_MACCATALYST
+        private const string NativeHandlerType = "System.Net.Http.NSUrlSessionHandler";
+        private const string AssemblyName = "Xamarin.MacCatalyst";
+        private const string GetHttpMessageHandlerType = "ObjCRuntime.RuntimeOptions, Xamarin.MacCatalyst";
+#elif TARGET_TVOS
         private const string NativeHandlerType = "System.Net.Http.NSUrlSessionHandler";
         private const string AssemblyName = "Xamarin.TVOS";
+        private const string GetHttpMessageHandlerType = "ObjCRuntime.RuntimeOptions, Xamarin.TVOS";
+#else
+#error Unknown target
+#endif
 
         [DynamicDependency("get_DefaultProxyCredentials", NativeHandlerType, AssemblyName)]
         private ICredentials? GetDefaultProxyCredentials() => (ICredentials?)InvokeNativeHandlerMethod("get_DefaultProxyCredentials");
@@ -44,6 +61,12 @@ namespace System.Net.Http
 
         [DynamicDependency("get_ClientCertificates", NativeHandlerType, AssemblyName)]
         private X509CertificateCollection GetClientCertificates() => (X509CertificateCollection)InvokeNativeHandlerMethod("get_ClientCertificates");
+
+        [DynamicDependency("get_ServerCertificateCustomValidationCallback", NativeHandlerType, AssemblyName)]
+        private Func<HttpRequestMessage, X509Certificate2?, X509Chain?, SslPolicyErrors, bool> GetServerCertificateCustomValidationCallback() => (Func<HttpRequestMessage, X509Certificate2?, X509Chain?, SslPolicyErrors, bool>)InvokeNativeHandlerMethod("get_ServerCertificateCustomValidationCallback");
+
+        [DynamicDependency("set_ServerCertificateCustomValidationCallback", NativeHandlerType, AssemblyName)]
+        private void SetServerCertificateCustomValidationCallback(Func<HttpRequestMessage, X509Certificate2?, X509Chain?, SslPolicyErrors, bool>? value) => InvokeNativeHandlerMethod("set_ServerCertificateCustomValidationCallback", value);
 
         [DynamicDependency("get_CheckCertificateRevocationList", NativeHandlerType, AssemblyName)]
         private bool GetCheckCertificateRevocationList() => (bool)InvokeNativeHandlerMethod("get_CheckCertificateRevocationList");
@@ -127,7 +150,7 @@ namespace System.Net.Http
         {
             if (_nativeHandlerMethod == null)
             {
-                Type? runtimeOptions = Type.GetType("ObjCRuntime.RuntimeOptions, Xamarin.TVOS");
+                Type? runtimeOptions = Type.GetType(GetHttpMessageHandlerType);
                 _nativeHandlerMethod = runtimeOptions!.GetMethod("GetHttpMessageHandler", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
             }
 
