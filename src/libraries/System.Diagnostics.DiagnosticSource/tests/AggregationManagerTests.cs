d177ddd81bcacb37c3658b667c006cd97857f182
Larry Ewing <lewing@microsoft.com>
2021-08-25 13:12:48 -0500

    Exclude GetMultiRank4Start too

--- src/libraries/System.Diagnostics.DiagnosticSource/tests/AggregationManagerTests.cs
+++ src/libraries/System.Diagnostics.DiagnosticSource/tests/AggregationManagerTests.cs
@@ -620,6 +620,7 @@ namespace System.Diagnostics.Metrics.Tests
         }
 
         [Fact]
+        [ActiveIssue("https://github.com/dotnet/runtime/issues/57003", typeof(PlatformDetection), nameof(PlatformDetection.IsBrowser), nameof(PlatformDetection.IsMonoAOT))]
         public void GetMultiRank4Start()
         {
             AggregatorStore<LastValue> store = new AggregatorStore<LastValue>(() => new LastValue());
