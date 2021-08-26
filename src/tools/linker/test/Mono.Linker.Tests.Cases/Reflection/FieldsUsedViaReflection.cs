77255e1a9cca70843f6f67ee5f3990ecfba98c9a
Mike Voorhees <mrvoorhe@users.noreply.github.com>
2021-08-26 06:07:27 -0400

    Remove unused usings (dotnet/linker#2229)
    Commit migrated from https://github.com/dotnet/linker/commit/e92ede15d5c2e33ed640375b1eac02d6af166238

--- src/tools/linker/test/Mono.Linker.Tests.Cases/Reflection/FieldsUsedViaReflection.cs
+++ src/tools/linker/test/Mono.Linker.Tests.Cases/Reflection/FieldsUsedViaReflection.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
-using System.Security.Policy;
 using Mono.Linker.Tests.Cases.Expectations.Assertions;
 using Mono.Linker.Tests.Cases.Expectations.Metadata;
 
