22ee205ed60f93fd511c968d34b12496ce28e31e
carlossanlop <carlossanlop@users.noreply.github.com>
2021-08-26 17:34:26 -0700

    Add dwReserved0 check for mount points in GetFinalLinkTarget.

--- src/libraries/System.Private.CoreLib/src/System/IO/FileSystem.Windows.cs
+++ src/libraries/System.Private.CoreLib/src/System/IO/FileSystem.Windows.cs
@@ -555,8 +555,9 @@ namespace System.IO
 
             // The file or directory is not a reparse point.
             if ((data.dwFileAttributes & (uint)FileAttributes.ReparsePoint) == 0 ||
-                // Only symbolic links are supported at the moment.
-                (data.dwReserved0 & Interop.Kernel32.IOReparseOptions.IO_REPARSE_TAG_SYMLINK) == 0)
+                // Only symbolic links and mount points are supported at the moment.
+                ((data.dwReserved0 & Interop.Kernel32.IOReparseOptions.IO_REPARSE_TAG_SYMLINK) == 0 &&
+                 (data.dwReserved0 & Interop.Kernel32.IOReparseOptions.IO_REPARSE_TAG_MOUNT_POINT) == 0))
             {
                 return null;
             }
