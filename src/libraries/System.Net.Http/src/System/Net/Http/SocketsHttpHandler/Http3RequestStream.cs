e534ebb216225b5d1fad5f4463a5d9f3aa19c348
Natalia Kondratyeva <knatalia@microsoft.com>
2021-08-30 23:21:28 +0200

    Fix hang if full data frame is not available

--- src/libraries/System.Net.Http/src/System/Net/Http/SocketsHttpHandler/Http3RequestStream.cs
+++ src/libraries/System.Net.Http/src/System/Net/Http/SocketsHttpHandler/Http3RequestStream.cs
@@ -1076,12 +1076,9 @@ namespace System.Net.Http
                         _responseDataPayloadRemaining -= bytesRead;
                         buffer = buffer.Slice(bytesRead);
 
-                        // Stop, if we've reached the end of a data frame. Waiting for the next data frame may cause a hang, e.g. in echo scenario
+                        // Stop, even if we are in the middle of a data frame. Waiting for the next data may cause a hang
                         // TODO: this is inefficient if data is already available in transport
-                        if (_responseDataPayloadRemaining == 0)
-                        {
-                            break;
-                        }
+                        break;
                     }
                 }
 
@@ -1148,12 +1145,9 @@ namespace System.Net.Http
                         _responseDataPayloadRemaining -= bytesRead;
                         buffer = buffer.Slice(bytesRead);
 
-                        // Stop, if we've reached the end of a data frame. Waiting for the next data frame may cause a hang e.g. in echo scenario
+                        // Stop, even if we are in the middle of a data frame. Waiting for the next data may cause a hang
                         // TODO: this is inefficient if data is already available in transport
-                        if (_responseDataPayloadRemaining == 0)
-                        {
-                            break;
-                        }
+                        break;
                     }
                 }
 
