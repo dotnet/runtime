eb66e0a55aaf2b5241362afeb17635e29bc5754f
Radek Doulik <radekdoulik@gmail.com>
2021-08-31 10:52:00 +0200

    [wa-info] Read instructions

--- src/mono/wasm/tools/wa-info/WasmReader.cs
+++ src/mono/wasm/tools/wa-info/WasmReader.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace WebAssemblyInfo
 {
@@ -111,6 +109,7 @@ namespace WebAssemblyInfo
         struct Code
         {
             public LocalsBlock[] Locals;
+            public Instruction[] Instructions;
         }
 
         Code[]? funcsCode;
@@ -132,47 +131,108 @@ namespace WebAssemblyInfo
                 var pos = reader.BaseStream.Position;
 
                 if (Program.Verbose2)
-                    Console.WriteLine($"  code: {size} bytes");
+                    Console.WriteLine($"  code[{i}]: {size} bytes");
 
                 var vecSize = ReadU32();
                 funcsCode[i].Locals = new LocalsBlock[vecSize];
 
                 if (Program.Verbose2)
-                    Console.WriteLine($"  locals blocks count {vecSize}");
+                    Console.WriteLine($"    locals blocks count {vecSize}");
 
                 for (var j = 0; j < vecSize; j++)
                 {
                     funcsCode[i].Locals[j].Count = ReadU32();
                     ReadValueType(ref funcsCode[i].Locals[j].Type);
 
-                    if (Program.Verbose2)
-                        Console.WriteLine($"    locals count: {funcsCode[i].Locals[j].Count} type: {funcsCode[i].Locals[j].Type}");
+                    //Console.WriteLine($"    locals count: {funcsCode[i].Locals[j].Count} type: {funcsCode[i].Locals[j].Type}");
                 }
 
                 // read expr
-                List<Instruction> instructions = new();
-                do
-                {
-                    var b = reader.ReadByte();
-                    if (b == 0xb)
-                        break;
-
-                    Console.WriteLine($"    opcode: {(Opcode)b}");
-                    instructions.Add(ReadInstruction(b));
-
-                    while (reader.ReadByte() != 0xb) ;
-
-                    break;
-
-                } while (true);
+                (funcsCode[i].Instructions, _) = ReadBlock();
 
                 reader.BaseStream.Seek(pos + size, SeekOrigin.Begin);
             }
         }
 
+        enum BlockTypeKind
+        {
+            Empty,
+            ValueType,
+            TypeIdx
+        }
+
+        struct BlockType
+        {
+            public BlockTypeKind Kind;
+            public ValueType ValueType;
+            public UInt32 TypeIdx;
+        }
+
+        BlockType ReadBlockType()
+        {
+            BlockType blockType = new();
+            byte b = reader.ReadByte();
+
+            switch (b)
+            {
+                case 0x40:
+                    blockType.Kind = BlockTypeKind.Empty;
+                    break;
+                case (byte)NumberType.f32:
+                case (byte)NumberType.i32:
+                case (byte)NumberType.f64:
+                case (byte)NumberType.i64:
+                case (byte)ReferenceType.ExternRef:
+                case (byte)ReferenceType.FuncRef:
+                    blockType.Kind = BlockTypeKind.ValueType;
+                    reader.BaseStream.Seek(-1, SeekOrigin.Current);
+                    ReadValueType(ref blockType.ValueType);
+                    break;
+                default:
+                    blockType.Kind = BlockTypeKind.TypeIdx;
+                    reader.BaseStream.Seek(-1, SeekOrigin.Current);
+                    blockType.TypeIdx = (UInt32)ReadI64();
+                    break;
+            }
+
+            return blockType;
+        }
+
         struct Instruction
         {
-            public byte Opcode;
+            public Opcode Opcode;
+
+            public BlockType BlockType;
+
+            public Instruction[] Block;
+            public Instruction[] Block2;
+
+            public UInt32 Idx;
+            public UInt32 Idx2;
+            public Int32 I32;
+            public Int64 I64;
+            public Single F32;
+            public Double F64;
+
+            public MemArg MemArg;
+
+            public UInt32[] IdxArray;
+        }
+
+        struct MemArg
+        {
+            public UInt32 Align;
+            public UInt32 Offset;
+        }
+
+        MemArg ReadMemArg()
+        {
+            MemArg ma = new();
+
+            ma.Align = ReadU32();
+            ma.Offset = ReadU32();
+
+            return ma;
         }
 
         enum Opcode : byte
@@ -184,6 +244,7 @@ namespace WebAssemblyInfo
             Loop = 0x03,
             If = 0x04,
             Else = 0x05,
+            End = 0x0b,
             Br = 0x0c,
             Br_If = 0x0d,
             Br_Table = 0x0e,
@@ -370,12 +431,259 @@ namespace WebAssemblyInfo
             Prefix = 0xfc,
         }
 
-        Instruction ReadInstruction(byte opcode)
+        void DumpBytes(int count)
         {
-            Instruction instruction;
+            Console.WriteLine("bytes");
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write($" {reader.ReadByte():x}");
+            }
+
+            Console.WriteLine();
+        }
+
+        Instruction ReadInstruction(Opcode opcode)
+        {
+            Instruction instruction = new();
             instruction.Opcode = opcode;
+            Opcode op;
+
+            switch (opcode)
+            {
+                case Opcode.Block:
+                case Opcode.Loop:
+                case Opcode.If:
+                    //DumpBytes(16);
+                    instruction.BlockType = ReadBlockType();
+
+                    //Console.WriteLine($"if blocktype: {instruction.BlockType.Kind}");
+
+                    (instruction.Block, op) = ReadBlock(opcode == Opcode.If ? Opcode.Else : Opcode.End);
+                    if (op == Opcode.Else)
+                        (instruction.Block2, op) = ReadBlock();
+                    break;
+                case Opcode.Memory_Size:
+                case Opcode.Memory_Grow:
+                    op = (Opcode)reader.ReadByte();
+                    if (op != Opcode.Unreachable)
+                        throw new Exception($"0x00 expected after opcode: {opcode}, got {op} instead");
+                    break;
+                case Opcode.Call:
+                case Opcode.Local_Get:
+                case Opcode.Local_Set:
+                case Opcode.Local_Tee:
+                case Opcode.Global_Get:
+                case Opcode.Global_Set:
+                case Opcode.Br:
+                case Opcode.Br_If:
+                    instruction.Idx = ReadU32();
+                    break;
+                case Opcode.Br_Table:
+                    var count = ReadU32();
+
+                    instruction.IdxArray = new UInt32[count];
+
+                    for (var i = 0; i < count; i++)
+                        instruction.IdxArray[i] = ReadU32();
+
+                    instruction.Idx = ReadU32();
+
+                    break;
+                case Opcode.Call_Indirect:
+                    instruction.Idx = ReadU32();
+                    instruction.Idx2 = ReadU32();
+                    break;
+                case Opcode.I32_Const:
+                    instruction.I32 = ReadI32();
+                    break;
+                case Opcode.F32_Const:
+                    instruction.F32 = reader.ReadSingle();
+                    break;
+                case Opcode.I64_Const:
+                    instruction.I64 = ReadI64();
+                    break;
+                case Opcode.F64_Const:
+                    instruction.F64 = reader.ReadDouble();
+                    break;
+                case Opcode.I32_Load:
+                case Opcode.I64_Load:
+                case Opcode.F32_Load:
+                case Opcode.F64_Load:
+                case Opcode.I32_Load8_S:
+                case Opcode.I32_Load8_U:
+                case Opcode.I32_Load16_S:
+                case Opcode.I32_Load16_U:
+                case Opcode.I64_Load8_S:
+                case Opcode.I64_Load8_U:
+                case Opcode.I64_Load16_S:
+                case Opcode.I64_Load16_U:
+                case Opcode.I64_Load32_S:
+                case Opcode.I64_Load32_U:
+                case Opcode.I32_Store:
+                case Opcode.I64_Store:
+                case Opcode.F32_Store:
+                case Opcode.F64_Store:
+                case Opcode.I32_Store8:
+                case Opcode.I32_Store16:
+                case Opcode.I64_Store8:
+                case Opcode.I64_Store16:
+                case Opcode.I64_Store32:
+                    instruction.MemArg = ReadMemArg();
+                    break;
+                case Opcode.Unreachable:
+                case Opcode.Nop:
+                case Opcode.Return:
+                case Opcode.Drop:
+                case Opcode.Select:
+                case Opcode.I32_Eqz:
+                case Opcode.I32_Eq:
+                case Opcode.I32_Ne:
+                case Opcode.I32_Lt_S:
+                case Opcode.I32_Lt_U:
+                case Opcode.I32_Gt_S:
+                case Opcode.I32_Gt_U:
+                case Opcode.I32_Le_S:
+                case Opcode.I32_Le_U:
+                case Opcode.I32_Ge_S:
+                case Opcode.I32_Ge_U:
+                case Opcode.I64_Eqz:
+                case Opcode.I64_Eq:
+                case Opcode.I64_Ne:
+                case Opcode.I64_Lt_S:
+                case Opcode.I64_Lt_U:
+                case Opcode.I64_Gt_S:
+                case Opcode.I64_Gt_U:
+                case Opcode.I64_Le_S:
+                case Opcode.I64_Le_U:
+                case Opcode.I64_Ge_S:
+                case Opcode.I64_Ge_U:
+                case Opcode.F32_Eq:
+                case Opcode.F32_Ne:
+                case Opcode.F32_Lt:
+                case Opcode.F32_Gt:
+                case Opcode.F32_Le:
+                case Opcode.F32_Ge:
+                case Opcode.F64_Eq:
+                case Opcode.F64_Ne:
+                case Opcode.F64_Lt:
+                case Opcode.F64_Gt:
+                case Opcode.F64_Le:
+                case Opcode.F64_Ge:
+                case Opcode.I32_Clz:
+                case Opcode.I32_Ctz:
+                case Opcode.I32_Popcnt:
+                case Opcode.I32_Add:
+                case Opcode.I32_Sub:
+                case Opcode.I32_Mul:
+                case Opcode.I32_Div_S:
+                case Opcode.I32_Div_U:
+                case Opcode.I32_Rem_S:
+                case Opcode.I32_Rem_U:
+                case Opcode.I32_And:
+                case Opcode.I32_Or:
+                case Opcode.I32_Xor:
+                case Opcode.I32_Shl:
+                case Opcode.I32_Shr_S:
+                case Opcode.I32_Shr_U:
+                case Opcode.I32_Rotl:
+                case Opcode.I32_Rotr:
+                case Opcode.I64_Clz:
+                case Opcode.I64_Ctz:
+                case Opcode.I64_Popcnt:
+                case Opcode.I64_Add:
+                case Opcode.I64_Sub:
+                case Opcode.I64_Mul:
+                case Opcode.I64_Div_S:
+                case Opcode.I64_Div_U:
+                case Opcode.I64_Rem_S:
+                case Opcode.I64_Rem_U:
+                case Opcode.I64_And:
+                case Opcode.I64_Or:
+                case Opcode.I64_Xor:
+                case Opcode.I64_Shl:
+                case Opcode.I64_Shr_S:
+                case Opcode.I64_Shr_U:
+                case Opcode.I64_Rotl:
+                case Opcode.I64_Rotr:
+                case Opcode.F32_Abs:
+                case Opcode.F32_Neg:
+                case Opcode.F32_Ceil:
+                case Opcode.F32_Floor:
+                case Opcode.F32_Trunc:
+                case Opcode.F32_Nearest:
+                case Opcode.F32_Sqrt:
+                case Opcode.F32_Add:
+                case Opcode.F32_Sub:
+                case Opcode.F32_Mul:
+                case Opcode.F32_Div:
+                case Opcode.F32_Min:
+                case Opcode.F32_Max:
+                case Opcode.F32_Copysign:
+                case Opcode.F64_Abs:
+                case Opcode.F64_Neg:
+                case Opcode.F64_Ceil:
+                case Opcode.F64_Floor:
+                case Opcode.F64_Trunc:
+                case Opcode.F64_Nearest:
+                case Opcode.F64_Sqrt:
+                case Opcode.F64_Add:
+                case Opcode.F64_Sub:
+                case Opcode.F64_Mul:
+                case Opcode.F64_Div:
+                case Opcode.F64_Min:
+                case Opcode.F64_Max:
+                case Opcode.F64_Copysign:
+                case Opcode.I32_Wrap_I64:
+                case Opcode.I32_Trunc_F32_S:
+                case Opcode.I32_Trunc_F32_U:
+                case Opcode.I32_Trunc_F64_S:
+                case Opcode.I32_Trunc_F64_U:
+                case Opcode.I64_Extend_I32_S:
+                case Opcode.I64_Extend_I32_U:
+                case Opcode.I64_Trunc_F32_S:
+                case Opcode.I64_Trunc_F32_U:
+                case Opcode.I64_Trunc_F64_S:
+                case Opcode.I64_Trunc_F64_U:
+                case Opcode.F32_Convert_I32_S:
+                case Opcode.F32_Convert_I32_U:
+                case Opcode.F32_Convert_I64_S:
+                case Opcode.F32_Convert_I64_U:
+                case Opcode.F32_Demote_F64:
+                case Opcode.F64_Convert_I32_S:
+                case Opcode.F64_Convert_I32_U:
+                case Opcode.F64_Convert_I64_S:
+                case Opcode.F64_Convert_I64_U:
+                case Opcode.F64_Promote_F32:
+                case Opcode.I32_Reinterpret_F32:
+                case Opcode.I64_Reinterpret_F64:
+                case Opcode.F32_Reinterpret_I32:
+                case Opcode.F64_Reinterpret_I64:
+                    break;
+
+                default:
+                    throw new FileLoadException($"Unknown opcode: {opcode} ({opcode:x})");
+            }
 
             return instruction;
+        }
+
+        (Instruction[], Opcode) ReadBlock(Opcode end = Opcode.End)
+        {
+            List<Instruction> instructions = new();
+            Opcode b;
+            do
+            {
+                b = (Opcode)reader.ReadByte();
+                if (b == Opcode.End || b == end)
+                    break;
+
+                //Console.WriteLine($"    opcode: {b}");
+
+                instructions.Add(ReadInstruction(b));
+            } while (true);
+
+            return (instructions.ToArray(), b);
         }
 
         void ReadCustomSection()
@@ -628,6 +936,52 @@ namespace WebAssemblyInfo
 
                 offset += 7;
             } while (true);
+
+            return value;
+        }
+
+        Int32 ReadI32()
+        {
+            Int32 value = 0;
+            var offset = 0;
+            byte b;
+
+            do
+            {
+                b = reader.ReadByte();
+                value |= (Int32)(b & 0x7f) << offset;
+
+                if ((b & 0x80) == 0)
+                    break;
+
+                offset += 7;
+            } while (true);
+
+            if (offset < 32 && (b & 0x40) == 0x40)
+                value |= (~(Int32)0 << offset);
+
+            return value;
+        }
+
+        Int64 ReadI64()
+        {
+            Int64 value = 0;
+            var offset = 0;
+            byte b;
+
+            do
+            {
+                b = reader.ReadByte();
+                value |= (Int64)(b & 0x7f) << offset;
+
+                if ((b & 0x80) == 0)
+                    break;
+
+                offset += 7;
+            } while (true);
+
+            if (offset < 64 && (b & 0x40) == 0x40)
+                value |= (~(Int64)0 << offset);
 
             return value;
         }
