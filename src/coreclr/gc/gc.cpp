30d596a0a948d5b3fb5ba372d859fed979175744
Peter Sollich <petersol@microsoft.com>
2021-09-01 11:22:01 +0200

    Make high_memory_load_p a member function of gc_heap and rename to dt_high_memory_load_p.

--- src/coreclr/gc/gc.cpp
+++ src/coreclr/gc/gc.cpp
@@ -3264,6 +3264,12 @@ gc_heap::dt_low_card_table_efficiency_p (gc_tuning_point tp)
 }
 
 inline BOOL
+gc_heap::dt_high_memory_load_p()
+{
+    return ((settings.entry_memory_load >= high_memory_load_th) || g_low_memory_status);
+}
+
+inline BOOL
 in_range_for_segment(uint8_t* add, heap_segment* seg)
 {
     return ((add >= heap_segment_mem (seg)) && (add < heap_segment_reserved (seg)));
@@ -11027,12 +11033,6 @@ int gc_heap::object_gennum_plan (uint8_t* o)
 #pragma optimize("", on)        // Go back to command line default optimizations
 #endif //_MSC_VER && TARGET_X86
 
-inline
-bool high_memory_load_p()
-{
-    return ((gc_heap::settings.entry_memory_load >= gc_heap::high_memory_load_th) || gc_heap::g_low_memory_status);
-}
-
 #ifdef USE_REGIONS
 void get_initial_region(int gen, int hn, uint8_t** region_start, uint8_t** region_end)
 {
@@ -11121,7 +11121,7 @@ void gc_heap::clear_region_info (heap_segment* region)
                         settings.gc_index, current_bgc_state,
                         seg_deleted);
 
-    if (high_memory_load_p())
+    if (dt_high_memory_load_p())
     {
         decommit_mark_array_by_seg (region);
     }
@@ -11580,7 +11580,7 @@ size_t gc_heap::decommit_heap_segment_pages_worker (heap_segment* seg,
                                                     uint8_t* new_committed)
 {
 #ifdef USE_REGIONS
-    if (!high_memory_load_p())
+    if (!dt_high_memory_load_p())
     {
         return 0;
     }
@@ -11615,7 +11615,7 @@ size_t gc_heap::decommit_heap_segment_pages_worker (heap_segment* seg,
 void gc_heap::decommit_heap_segment (heap_segment* seg)
 {
 #ifdef USE_REGIONS
-    if (!high_memory_load_p())
+    if (!dt_high_memory_load_p())
     {
         return;
     }
@@ -39545,7 +39545,7 @@ void reset_memory (uint8_t* o, size_t sizeo)
         size_t size = align_lower_page ((size_t)o + sizeo - size_to_skip - plug_skew) - page_start;
         // Note we need to compensate for an OS bug here. This bug would cause the MEM_RESET to fail
         // on write watched memory.
-        if (reset_mm_p && high_memory_load_p())
+        if (reset_mm_p && gc_heap::dt_high_memory_load_p())
         {
 #ifdef MULTIPLE_HEAPS
             bool unlock_p = true;
