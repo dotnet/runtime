5fbd69ebedbf2817b3c0fdd2d59e17b46f4225e8
Jeremy Koritzinsky <jekoritz@microsoft.com>
2021-09-08 10:31:51 -0700

    Output version files in the artifacts/obj dir so they can be easily shared.

--- eng/native/configurepaths.cmake
+++ eng/native/configurepaths.cmake
@@ -2,3 +2,4 @@ get_filename_component(CLR_REPO_ROOT_DIR ${CMAKE_CURRENT_LIST_DIR}/../.. ABSOLUT
 set(CLR_ENG_NATIVE_DIR ${CMAKE_CURRENT_LIST_DIR})
 get_filename_component(CLR_SRC_NATIVE_DIR ${CMAKE_CURRENT_LIST_DIR}/../../src/native ABSOLUTE)
 get_filename_component(CLR_SRC_LIBS_NATIVE_DIR ${CMAKE_CURRENT_LIST_DIR}/../../src/libraries/Native ABSOLUTE)
+set (CLR_ARTIFACTS_OBJ_DIR ${CLR_REPO_ROOT_DIR}/artifacts/obj)
