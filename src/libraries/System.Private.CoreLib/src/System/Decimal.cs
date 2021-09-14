fdf0c67b24c3232067ac2ae5343a927bcddb6143
Prashanth Govindarajan <prgovi@microsoft.com>
2021-09-14 09:39:23 -0700

    sq

--- src/libraries/System.Private.CoreLib/src/System/Decimal.cs
+++ src/libraries/System.Private.CoreLib/src/System/Decimal.cs
@@ -60,7 +60,7 @@ namespace System
     [System.Runtime.CompilerServices.TypeForwardedFrom("mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089")]
     public readonly partial struct Decimal : ISpanFormattable, IComparable, IConvertible, IComparable<decimal>, IEquatable<decimal>, ISerializable, IDeserializationCallback
 #if FEATURE_GENERIC_MATH
-#pragma warning disable SA1001, CA2252
+#pragma warning disable SA1001, CA2252 // SA1001: Comma positioning; CA2252: Preview Features
         , IMinMaxValue<decimal>,
           ISignedNumber<decimal>
 #pragma warning restore SA1001, CA2252
--- src/libraries/System.Private.CoreLib/src/System/Single.cs
+++ src/libraries/System.Private.CoreLib/src/System/Single.cs
@@ -25,7 +25,7 @@ namespace System
     [TypeForwardedFrom("mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089")]
     public readonly struct Single : IComparable, IConvertible, ISpanFormattable, IComparable<float>, IEquatable<float>
 #if FEATURE_GENERIC_MATH
-#pragma warning disable SA1001, CA2252
+#pragma warning disable SA1001, CA2252 // SA1001: Comma positioning; CA2252: Preview Features
         , IBinaryFloatingPoint<float>,
           IMinMaxValue<float>
 #pragma warning restore SA1001, CA2252
