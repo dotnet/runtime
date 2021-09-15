ca51a8c65c69128294c7d8a10d3dd5a95d44b8f7
Anton Firszov <antonfir@gmail.com>
2021-09-15 17:07:37 +0200

    add HttpStress.ReportAnalyzer skeleton

---
+++ src/libraries/System.Net.Http/tests/StressTests/HttpStress.ReportAnalyzer/Program.cs
@@ -0,0 +1,8 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+
+
---
+++ src/libraries/System.Net.Http/tests/StressTests/HttpStress.ReportAnalyzer/ReportAggregator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace HttpStress.ReportAnalyzer
+{
+    internal class FailureType
+    {
+        public string Fingerprint { get; }
+
+        public string ErrorText { get; }
+
+        public int Count { get; set; }
+
+        public FailureType(string fingerprint, string errorText)
+        {
+            Fingerprint = fingerprint;
+            ErrorText = errorText;
+        }
+    }
+
+    internal class ReportAggregator
+    {
+        private Dictionary<string, FailureType> _failures = new();
+        public ReportAggregator()
+        {
+
+        }
+
+        public void AppendReportXml(XDocument doc)
+        {
+            foreach (XElement failureElement in doc.Descendants("Failure"))
+            {
+                string fingerprint = failureElement.Attribute("FailureTypeFingerprint")!.Value;
+                if (!_failures.TryGetValue(fingerprint, out FailureType? failureType))
+                {
+                    XCData failureTextNode = (XCData)failureElement.Element("FailureText")!.FirstNode!;
+                    string errorText = failureTextNode.Value;
+                    failureType = new FailureType(fingerprint, errorText);
+                }
+
+                failureType.Count++;
+            }
+        }
+    }
+}
