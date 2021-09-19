fb2d0961c3df1f7371caa21d329ed2b6b4741dc0
Egor Bogatov <egorbo@gmail.com>
2021-09-19 13:30:51 +0300

    Update Program.cs
    Don't ignore ilOffset in GetLikelyClass

--- src/coreclr/tools/dotnet-pgo/Program.cs
+++ src/coreclr/tools/dotnet-pgo/Program.cs
@@ -741,6 +741,9 @@ namespace Microsoft.Diagnostics.Tools.Pgo
             for (int i = 0; i < schema.Length; i++)
             {
                 var elem = schema[i];
+                if (elem.ILOffset != ilOffset)
+                    continue;
+                
                 if (elem.InstrumentationKind == PgoInstrumentationKind.GetLikelyClass)
                 {
                     Trace.Assert(elem.Count == 1);
