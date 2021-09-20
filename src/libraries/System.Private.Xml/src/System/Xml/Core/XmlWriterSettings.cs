e5143f4baa80421597ad85e62621b83877f98fda
kronic <kronic@softland.ru>
2021-09-20 16:09:27 +0300

    -

--- src/libraries/System.Private.Xml/src/System/Xml/Core/XmlWriterSettings.cs
+++ src/libraries/System.Private.Xml/src/System/Xml/Core/XmlWriterSettings.cs
@@ -16,9 +16,9 @@ namespace System.Xml
 {
     public enum XmlOutputMethod
     {
-        Xml = 0,    // Use Xml 1.0 rules to serialize
-        Html = 1,    // Use Html rules specified by Xslt specification to serialize
-        Text = 2,    // Only serialize text blocks
+        Xml = 0,       // Use Xml 1.0 rules to serialize
+        Html = 1,      // Use Html rules specified by Xslt specification to serialize
+        Text = 2,      // Only serialize text blocks
         AutoDetect = 3 // Choose between Xml and Html output methods at runtime (using Xslt rules to do so)
     }
 
