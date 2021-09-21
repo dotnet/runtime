f9aa7bedf3040e24c3531b29668a3b8efeb62edc
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2021-09-21 08:48:26 -0700

    Update eng/native/version/copy_version_files.sh
    Co-authored-by: Adeel Mujahid <3840695+am11@users.noreply.github.com>

--- eng/native/version/copy_version_files.sh
+++ eng/native/version/copy_version_files.sh
@@ -1,5 +1,9 @@
 __VersionFolder="$(cd "$(dirname "$0")"; pwd -P)"
 __RepoRoot="$(cd "$(dirname "$__VersionFolder")/../../"; pwd -P)"
 
-# Use yes n and interactive cp instead of -n since -n is not supported on macOS.
-yes n | cp -i "${__VersionFolder}/"*{.h,.c} "$__RepoRoot/artifacts/obj/"
+for path in "${__VersionFolder}/"*{.h,.c}; do
+    # -s <filename> checks if file has nonzero size (we don't need any more guarantee than that)
+    if [ ! -s "$__RepoRoot/artifacts/obj/$(basename "$path")" ]; then
+        cp "$path" "$__RepoRoot/artifacts/obj/"
+    fi
+done
