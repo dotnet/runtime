400147da1e5679fca4a47f4abedf25619c6b70d8
Stephen Toub <stoub@microsoft.com>
2021-09-21 13:38:05 -0400

    Update resources per PR feedback

--- src/libraries/System.Text.RegularExpressions/gen/DiagnosticDescriptors.cs
+++ src/libraries/System.Text.RegularExpressions/gen/DiagnosticDescriptors.cs
@@ -10,7 +10,7 @@ namespace System.Text.RegularExpressions.Generator
     {
         public static DiagnosticDescriptor InvalidRegexGeneratorAttribute { get; } = new DiagnosticDescriptor(
             id: "SYSLIB1040",
-            title: new LocalizableResourceString(nameof(SR.InvalidRegexGeneratorAttributeMessage), SR.ResourceManager, typeof(FxResources.System.Text.RegularExpressions.Generator.SR)),
+            title: new LocalizableResourceString(nameof(SR.InvalidRegexGeneratorAttributeTitle), SR.ResourceManager, typeof(FxResources.System.Text.RegularExpressions.Generator.SR)),
             messageFormat: new LocalizableResourceString(nameof(SR.InvalidRegexGeneratorAttributeMessage), SR.ResourceManager, typeof(FxResources.System.Text.RegularExpressions.Generator.SR)),
             category: "RegexGenerator",
             DiagnosticSeverity.Error,
@@ -19,7 +19,7 @@ namespace System.Text.RegularExpressions.Generator
 
         public static DiagnosticDescriptor MultipleRegexGeneratorAttributes { get; } = new DiagnosticDescriptor(
             id: "SYSLIB1041",
-            title: new LocalizableResourceString(nameof(SR.MultipleRegexGeneratorAttributesMessage), SR.ResourceManager, typeof(FxResources.System.Text.RegularExpressions.Generator.SR)),
+            title: new LocalizableResourceString(nameof(SR.InvalidRegexGeneratorAttributeTitle), SR.ResourceManager, typeof(FxResources.System.Text.RegularExpressions.Generator.SR)),
             messageFormat: new LocalizableResourceString(nameof(SR.MultipleRegexGeneratorAttributesMessage), SR.ResourceManager, typeof(FxResources.System.Text.RegularExpressions.Generator.SR)),
             category: "RegexGenerator",
             DiagnosticSeverity.Error,
@@ -28,7 +28,7 @@ namespace System.Text.RegularExpressions.Generator
 
         public static DiagnosticDescriptor InvalidRegexArguments { get; } = new DiagnosticDescriptor(
             id: "SYSLIB1042",
-            title: new LocalizableResourceString(nameof(SR.InvalidRegexArgumentsMessage), SR.ResourceManager, typeof(FxResources.System.Text.RegularExpressions.Generator.SR)),
+            title: new LocalizableResourceString(nameof(SR.InvalidRegexGeneratorAttributeTitle), SR.ResourceManager, typeof(FxResources.System.Text.RegularExpressions.Generator.SR)),
             messageFormat: new LocalizableResourceString(nameof(SR.InvalidRegexArgumentsMessage), SR.ResourceManager, typeof(FxResources.System.Text.RegularExpressions.Generator.SR)),
             category: "RegexGenerator",
             DiagnosticSeverity.Error,
@@ -37,7 +37,7 @@ namespace System.Text.RegularExpressions.Generator
 
         public static DiagnosticDescriptor RegexMethodMustHaveValidSignature { get; } = new DiagnosticDescriptor(
             id: "SYSLIB1043",
-            title: new LocalizableResourceString(nameof(SR.RegexMethodMustHaveValidSignatureMessage), SR.ResourceManager, typeof(FxResources.System.Text.RegularExpressions.Generator.SR)),
+            title: new LocalizableResourceString(nameof(SR.InvalidRegexGeneratorAttributeTitle), SR.ResourceManager, typeof(FxResources.System.Text.RegularExpressions.Generator.SR)),
             messageFormat: new LocalizableResourceString(nameof(SR.RegexMethodMustHaveValidSignatureMessage), SR.ResourceManager, typeof(FxResources.System.Text.RegularExpressions.Generator.SR)),
             category: "RegexGenerator",
             DiagnosticSeverity.Error,
@@ -46,7 +46,7 @@ namespace System.Text.RegularExpressions.Generator
 
         public static DiagnosticDescriptor InvalidLangVersion { get; } = new DiagnosticDescriptor(
             id: "SYSLIB1044",
-            title: new LocalizableResourceString(nameof(SR.InvalidLangVersionMessage), SR.ResourceManager, typeof(FxResources.System.Text.RegularExpressions.Generator.SR)),
+            title: new LocalizableResourceString(nameof(SR.InvalidRegexGeneratorAttributeTitle), SR.ResourceManager, typeof(FxResources.System.Text.RegularExpressions.Generator.SR)),
             messageFormat: new LocalizableResourceString(nameof(SR.InvalidLangVersionMessage), SR.ResourceManager, typeof(FxResources.System.Text.RegularExpressions.Generator.SR)),
             category: "RegexGenerator",
             DiagnosticSeverity.Error,
