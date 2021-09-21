74236258fc948299a2162478058533d6c9273367
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2021-09-21 11:52:46 -0700

    Apply suggestions from code review
    Co-authored-by: Adeel Mujahid <3840695+am11@users.noreply.github.com>

--- eng/native/version/copy_version_files.sh
+++ eng/native/version/copy_version_files.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 __VersionFolder="$(cd "$(dirname "$0")"; pwd -P)"
 __RepoRoot="$(cd "$(dirname "$__VersionFolder")/../../"; pwd -P)"
 
@@ -10,10 +12,10 @@ for path in "${__VersionFolder}/"*{.h,.c}; do
         commit="$(git rev-parse HEAD 2>/dev/null)"
         commit="${commit:-N/A}"
         substitute="$(printf 'static char sccsid[] __attribute__((used)) = "@(#)Version N/A @Commit: %s";\n' "$commit")"
-        version_file_contents=$(cat $path | sed "s|^static.*|$substitute|")
-        version_file_destination=$__RepoRoot/artifacts/obj/_version.c
+        version_file_contents="$(cat "$path" | sed "s|^static.*|$substitute|")"
+        version_file_destination="$__RepoRoot/artifacts/obj/_version.c"
         if [[ ! -e "$version_file_destination" || "$version_file_contents" != "$(<"$__RepoRoot/artifacts/obj/_version.c")" ]]; then
-            echo $version_file_contents > "$version_file_destination"
+            echo "$version_file_contents" > "$version_file_destination"
         fi
     elif [[ ! -e "$__RepoRoot/artifacts/obj/$(basename "$path")" ]]; then
         cp "$path" "$__RepoRoot/artifacts/obj/"
