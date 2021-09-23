ba43e08205ff11ab05200918646399780caa7e75
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2021-09-23 17:17:45 +0200

    Fix underestimation of temps size (#58969)
    It was possible for us to underestimate the size of temps required. This
    happened because we used the state of the frame layout to check if the
    size of temps was computed and that check was wrong. This could lead us
    to make different decisions about which registers needed to be saved in
    the prolog and epilog on ARM32. In particular, if the frame size was
    around the size where a stack probe is necessary, this could be
    possible.
    
    There are also other comments that suggest that this could result in
    failure while encoding instructions that reference the stack locals.
    
    Fix #58293

--- src/coreclr/jit/lclvars.cpp
+++ src/coreclr/jit/lclvars.cpp
@@ -4689,7 +4689,7 @@ unsigned Compiler::lvaGetMaxSpillTempSize()
 {
     unsigned result = 0;
 
-    if (lvaDoneFrameLayout >= REGALLOC_FRAME_LAYOUT)
+    if (codeGen->regSet.hasComputedTmpSize())
     {
         result = codeGen->regSet.tmpGetTotalSize();
     }
