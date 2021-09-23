3c2a226440e765b9167c5d3905d58bbd4eaa4b16
Juan Sebastian Hoyos Ayala <juhoyosa@microsoft.com>
2021-09-23 16:13:40 -0700

    Fix behavior ObjectCollection for single item contains

--- src/libraries/System.Net.Http/tests/UnitTests/Headers/ObjectCollectionTest.cs
+++ src/libraries/System.Net.Http/tests/UnitTests/Headers/ObjectCollectionTest.cs
@@ -36,9 +36,29 @@ namespace System.Net.Http.Tests
             c.Add("value1");
 
             Assert.Throws<InvalidOperationException>(() => { c.Add(null); });
+        }
+
+        [Fact]
+        public void ContainsAndRemove_UsesEqualitySemantics()
+        {
+            // Use default validator
+            ObjectCollection<string> c = new ObjectCollection<string>();
+
+            Assert.Equal(0, c.Count);
+            Assert.False(c.Contains("value" + 1));
+
+            c.Add("value" + 1);
 
             Assert.Equal(1, c.Count);
-            Assert.True(c.Contains("value1"));
+            // Force the reference to be different to ensure we are checking for semantic equality
+            // and not reference equality.
+            Assert.True(c.Contains("value" + 1));
+            c.Add("value" + 2);
+
+            Assert.True(c.Remove("value" + 1));
+            Assert.Equal(1, c.Count);
+
+            Assert.True(c.Contains("value" + 2));
         }
     }
 }
