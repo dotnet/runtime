8220116f784b50428d3a58ad2331ba28d5683489
Sven Boemer <sbomer@gmail.com>
2021-09-23 16:09:01 -0700

    Share logic for discovering typereferences (dotnet/linker#2287)
    * Share logic for discovering typereferences
    
    * PR feedback
    
    - Add non-nullable property over "visited" hashset
    - Hide construction/process pattern in static helpers
    
    Commit migrated from https://github.com/dotnet/linker/commit/8097e395086a58853c94f557840afd094b071905

--- src/tools/illink/src/linker/Linker.Steps/MarkStep.cs
+++ src/tools/illink/src/linker/Linker.Steps/MarkStep.cs
@@ -1380,18 +1380,47 @@ namespace Mono.Linker.Steps
 			foreach (TypeDefinition type in module.Types)
 				MarkEntireType (type, new DependencyInfo (DependencyKind.TypeInAssembly, assembly));
 
-			foreach (ExportedType exportedType in module.ExportedTypes) {
-				MarkingHelpers.MarkExportedType (exportedType, module, new DependencyInfo (DependencyKind.ExportedType, assembly));
-				MarkingHelpers.MarkForwardedScope (new TypeReference (exportedType.Namespace, exportedType.Name, module, exportedType.Scope));
+			// Mark scopes of type references and exported types.
+			TypeReferenceMarker.MarkTypeReferences (assembly, MarkingHelpers);
+		}
+
+		class TypeReferenceMarker : TypeReferenceWalker
+		{
+
+			readonly MarkingHelpers markingHelpers;
+
+			TypeReferenceMarker (AssemblyDefinition assembly, MarkingHelpers markingHelpers)
+				: base (assembly)
+			{
+				this.markingHelpers = markingHelpers;
 			}
 
-			// Mark scopes of type references by traversing the assembly.
-			new TypeReferenceMarker (assembly, MarkingHelpers).Process ();
+			public static void MarkTypeReferences (AssemblyDefinition assembly, MarkingHelpers markingHelpers)
+			{
+				new TypeReferenceMarker (assembly, markingHelpers).Process ();
+			}
 
-			// Also mark the scopes of metadata typeref rows to cover any not discovered by the traversal.
-			// This can happen when the compiler emits typerefs into IL which aren't strictly necessary per ECMA 335.
-			foreach (TypeReference typeReference in module.GetTypeReferences ())
-				MarkingHelpers.MarkForwardedScope (typeReference);
+			protected override void ProcessTypeReference (TypeReference type)
+			{
+				markingHelpers.MarkForwardedScope (type);
+			}
+
+			protected override void ProcessExportedType (ExportedType exportedType)
+			{
+				markingHelpers.MarkExportedType (exportedType, assembly.MainModule, new DependencyInfo (DependencyKind.ExportedType, assembly));
+				markingHelpers.MarkForwardedScope (new TypeReference (exportedType.Namespace, exportedType.Name, assembly.MainModule, exportedType.Scope));
+			}
+
+			protected override void ProcessExtra ()
+			{
+				// Also mark the scopes of metadata typeref rows to cover any not discovered by the traversal.
+				// This can happen when the compiler emits typerefs into IL which aren't strictly necessary per ECMA 335.
+				foreach (TypeReference typeReference in assembly.MainModule.GetTypeReferences ()) {
+					if (!Visited!.Add (typeReference))
+						continue;
+					markingHelpers.MarkForwardedScope (typeReference);
+				}
+			}
 		}
 
 		void ProcessModuleType (AssemblyDefinition assembly)
