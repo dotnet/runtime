7fad42ac6a475fb31a510b00d41ba46d19ec78db
Anton Firszov <antonfir@gmail.com>
2021-09-24 14:40:54 +0200

    yeah

--- src/libraries/System.Net.Http/tests/StressTests/HttpStress/Program.cs
+++ src/libraries/System.Net.Http/tests/StressTests/HttpStress/Program.cs
@@ -35,10 +35,7 @@ namespace HttpStress
                 return (int)ExitCode.CliError;
             }
 
-            File.WriteAllText(config.ReportFile, "<Lol/>");
-
-            return (int)await Task.FromResult(ExitCode.StressError);
-            //return (int)await Run(config);
+            return (int)await Run(config);
         }
 
         private static bool TryParseCli(string[] args, [NotNullWhen(true)] out Configuration? config)
