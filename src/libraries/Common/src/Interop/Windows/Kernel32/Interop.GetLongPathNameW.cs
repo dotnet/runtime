a24525ef863ccb106f3d2cdb55cdcfaea5658221
Jeremy Koritzinsky <jekoritz@microsoft.com>
2021-09-27 16:01:32 -0700

    Undo GeneratedDllImport on GetLongPathNameW for the same reason as GetFullPathNameW.

--- src/libraries/Common/src/Interop/Windows/Kernel32/Interop.GetLongPathNameW.cs
+++ src/libraries/Common/src/Interop/Windows/Kernel32/Interop.GetLongPathNameW.cs
@@ -7,16 +7,13 @@ internal static partial class Interop
 {
     internal static partial class Kernel32
     {
+#pragma warning disable DLLIMPORTGENANALYZER015 // Use 'GeneratedDllImportAttribute' instead of 'DllImportAttribute' to generate P/Invoke marshalling code at compile time
+                                                // Disabled since GetFullPathNameW takes 'ref's into buffers (the ref parameters don't represent single characters).
         /// <summary>
         /// WARNING: This method does not implicitly handle long paths. Use GetFullPath/PathHelper.
         /// </summary>
-#if DLLIMPORTGENERATOR_ENABLED
-        [GeneratedDllImport(Libraries.Kernel32, SetLastError = true, CharSet = CharSet.Unicode, ExactSpelling = true)]
-        internal static partial uint GetLongPathNameW(
-#else
         [DllImport(Libraries.Kernel32, SetLastError = true, CharSet = CharSet.Unicode, BestFitMapping = false, ExactSpelling = true)]
         internal static extern uint GetLongPathNameW(
-#endif
             ref char lpszShortPath,
             ref char lpszLongPath,
             uint cchBuffer);
