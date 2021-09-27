6f4053131a3f6f0ddb92ef1dc32de7cc167e5041
Jeremy Koritzinsky <jekoritz@microsoft.com>
2021-09-27 10:10:49 -0700

    Keep some of the P/Invokes using GeneratedDllImport for various reasons (marked in source with analyzer suppressions).

--- src/libraries/Common/src/Interop/Windows/Kernel32/Interop.GetFullPathNameW.cs
+++ src/libraries/Common/src/Interop/Windows/Kernel32/Interop.GetFullPathNameW.cs
@@ -8,16 +8,13 @@ internal static partial class Interop
 {
     internal static partial class Kernel32
     {
+#pragma warning disable DLLIMPORTGENANALYZER015 // Use 'GeneratedDllImportAttribute' instead of 'DllImportAttribute' to generate P/Invoke marshalling code at compile time
+                                                // Disabled since GetFullPathNameW takes 'ref's into buffers (the ref parameters don't represent single characters).
         /// <summary>
         /// WARNING: This method does not implicitly handle long paths. Use GetFullPathName or PathHelper.
         /// </summary>
-#if DLLIMPORTGENERATOR_ENABLED
-        [GeneratedDllImport(Libraries.Kernel32, SetLastError = true, CharSet = CharSet.Unicode, ExactSpelling = true)]
-        internal static partial uint GetFullPathNameW(
-#else
         [DllImport(Libraries.Kernel32, SetLastError = true, CharSet = CharSet.Unicode, BestFitMapping = false, ExactSpelling = true)]
         internal static extern uint GetFullPathNameW(
-#endif
             ref char lpFileName,
             uint nBufferLength,
             ref char lpBuffer,
