5585626369aa01e07756bef3cdae14e530423676
Elinor Fung <elfung@microsoft.com>
2021-09-28 00:29:47 -0700

    Add test

--- src/libraries/System.Runtime.InteropServices/tests/TestAssets/NativeExports/Characters.cs
+++ src/libraries/System.Runtime.InteropServices/tests/TestAssets/NativeExports/Characters.cs
@@ -1,6 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
 using System.Runtime.InteropServices;
 
 namespace NativeExports
@@ -23,6 +24,13 @@ namespace NativeExports
         public static void ReturnUIntAsRefUInt(uint input, uint* res)
         {
             *res = input;
+        }
+
+        [UnmanagedCallersOnly(EntryPoint = "char_reverse_buffer_ref")]
+        public static void ReverseBuffer(ushort *buffer, int len)
+        {
+            var span = new Span<ushort>(buffer, len);
+            span.Reverse();
         }
     }
 }
