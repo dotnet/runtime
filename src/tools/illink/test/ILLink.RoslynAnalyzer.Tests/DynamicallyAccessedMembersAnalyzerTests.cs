7b88bc23da2d0a329a07381ae211300eeb717be9
Sven Boemer <sbomer@gmail.com>
2021-09-29 12:33:32 -0700

    Simplify DAM analyzer and add conversion test (dotnet/linker#2297)
    * Simplify DAM analyzer and add conversion test
    
    * PR feedback
    - Take Location in helper instead of originOperation
    - Avoid optional parameters
    - Add test with DAM on conversion return type
    
    * Add success testcase
    
    Commit migrated from https://github.com/dotnet/linker/commit/8e7b310cf7d7579418dbf78b5565c74431f5fdcb

--- src/tools/illink/test/ILLink.RoslynAnalyzer.Tests/DynamicallyAccessedMembersAnalyzerTests.cs
+++ src/tools/illink/test/ILLink.RoslynAnalyzer.Tests/DynamicallyAccessedMembersAnalyzerTests.cs
@@ -679,6 +679,120 @@ namespace System
 		}
 
 		[Fact]
+		public Task ConversionOperation ()
+		{
+			var ConversionOperation = @"
+namespace System
+{
+    class ConvertsToType
+    {
+        public static implicit operator Type(ConvertsToType value) => typeof (ConvertsToType);
+    }
+
+    class C : TestSystemTypeBase
+    {
+        public static void Main()
+        {
+            new C().M1();
+        }
+
+        private void M1()
+        {
+            M2(new ConvertsToType());
+        }
+
+        private static void M2(
+            [System.Diagnostics.CodeAnalysis.DynamicallyAccessedMembers(
+				System.Diagnostics.CodeAnalysis.DynamicallyAccessedMemberTypes.PublicMethods)] Type type)
+        {
+        }
+    }
+}";
+
+			return VerifyDynamicallyAccessedMembersAnalyzer (string.Concat (GetSystemTypeBase (), ConversionOperation),
+				VerifyCS.Diagnostic (DiagnosticId.DynamicallyAccessedMembersMismatchMethodReturnTypeTargetsParameter)
+				.WithSpan (183, 16, 183, 36)
+				.WithArguments ("type", "System.C.M2(System.Type)", "System.ConvertsToType.implicit operator System.Type(System.ConvertsToType)", "'DynamicallyAccessedMemberTypes.PublicMethods'"));
+		}
+
+
+		[Fact]
+		public Task ConversionOperationAnnotationDoesNotMatch ()
+		{
+			var AnnotatedConversionOperation = @"
+namespace System
+{
+    class ConvertsToType
+    {
+        [return: System.Diagnostics.CodeAnalysis.DynamicallyAccessedMembers(
+            System.Diagnostics.CodeAnalysis.DynamicallyAccessedMemberTypes.PublicFields)]
+        public static implicit operator Type(ConvertsToType value) => null;
+    }
+
+    class C : TestSystemTypeBase
+    {
+        public static void Main()
+        {
+            new C().M1();
+        }
+
+        private void M1()
+        {
+            M2(new ConvertsToType());
+        }
+
+        private static void M2(
+            [System.Diagnostics.CodeAnalysis.DynamicallyAccessedMembers(
+				System.Diagnostics.CodeAnalysis.DynamicallyAccessedMemberTypes.PublicMethods)] Type type)
+        {
+        }
+    }
+}";
+
+			return VerifyDynamicallyAccessedMembersAnalyzer (string.Concat (GetSystemTypeBase (), AnnotatedConversionOperation),
+				VerifyCS.Diagnostic (DiagnosticId.DynamicallyAccessedMembersMismatchMethodReturnTypeTargetsParameter)
+				.WithSpan (185, 16, 185, 36)
+				.WithArguments ("type", "System.C.M2(System.Type)", "System.ConvertsToType.implicit operator System.Type(System.ConvertsToType)", "'DynamicallyAccessedMemberTypes.PublicMethods'"));
+		}
+
+		[Fact]
+		public Task ConversionOperationAnnotationMatches ()
+		{
+			var AnnotatedConversionOperation = @"
+namespace System
+{
+    class ConvertsToType
+    {
+        [return: System.Diagnostics.CodeAnalysis.DynamicallyAccessedMembers(
+            System.Diagnostics.CodeAnalysis.DynamicallyAccessedMemberTypes.PublicMethods)]
+        public static implicit operator Type(ConvertsToType value) => null;
+    }
+
+    class C : TestSystemTypeBase
+    {
+        public static void Main()
+        {
+            new C().M1();
+        }
+
+        private void M1()
+        {
+            M2(new ConvertsToType());
+        }
+
+        private static void M2(
+            [System.Diagnostics.CodeAnalysis.DynamicallyAccessedMembers(
+				System.Diagnostics.CodeAnalysis.DynamicallyAccessedMemberTypes.PublicMethods)] Type type)
+        {
+        }
+    }
+}";
+
+			return VerifyDynamicallyAccessedMembersAnalyzer (string.Concat (GetSystemTypeBase (), AnnotatedConversionOperation));
+		}
+
+
+		[Fact]
 		public Task SourceMethodDoesNotMatchTargetMethodReturnTypeAnnotations ()
 		{
 			var TargetMethodReturnTypeWithAnnotations = @"
