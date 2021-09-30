cd689b5338ad26979f5ac6a899d45d2180565658
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2021-09-30 15:47:28 +0200

    Use Retry function from tools.ps1

--- eng/formatting/download-tools.ps1
+++ eng/formatting/download-tools.ps1
@@ -1,3 +1,4 @@
+. "$PSScriptRoot/../common/tools.ps1" # for Retry function
 
 function DownloadClangTool {
     param (
@@ -10,29 +11,13 @@ function DownloadClangTool {
     $baseUri = "https://clrjit.blob.core.windows.net/clang-tools/windows"
 
     if (-not $(ls $downloadOutputPath | Where-Object { $_.Name -eq "$toolName.exe" })) {
-        $baseBackoffSeconds = 2;
 
-        $success = $false
-        for ($i = 0; $i -lt 5; $i++) {
-            Write-Output "Attempting download of '$baseUri/$toolName.exe'"
-            try {
-                # Pass -PassThru as otherwise Invoke-WebRequest leaves a corrupted file if the download fails. With -PassThru the download is buffered first.
-                # -UseBasicParsing is necessary for older PowerShells when Internet Explorer might not be installed/configured
-                $null = Invoke-WebRequest -Uri "$baseUri/$toolName.exe" -OutFile $(Join-Path $downloadOutputPath -ChildPath "$toolName.exe") -PassThru -UseBasicParsing
-                $success = $true
-                break
-            }
-            catch {
-                Write-Output $_
-            }
-			
-            Write-Output "Download attempt $($i+1) failed. Trying again in $($baseBackoffSeconds + $baseBackoffSeconds * $i) seconds"
-            Start-Sleep -Seconds $($baseBackoffSeconds + $baseBackoffSeconds * $i)
-        }
-        if (-not $success) {
-            Write-Output "Failed to download $toolName"
-            throw [System.IO.IOException] "Could not download $toolName"
-        }
+        Retry({
+            Write-Output "Downloading '$baseUri/$toolName.exe'"
+            # Pass -PassThru as otherwise Invoke-WebRequest leaves a corrupted file if the download fails. With -PassThru the download is buffered first.
+            # -UseBasicParsing is necessary for older PowerShells when Internet Explorer might not be installed/configured
+            $null = Invoke-WebRequest -Uri "$baseUri/$toolName.exe" -OutFile $(Join-Path $downloadOutputPath -ChildPath "$toolName.exe") -PassThru -UseBasicParsing
+        })
     }
 }
 
