70ff1d322bf80f3b33fc5c7980bbe97ca2f7aab4
kronic <kronic@softland.ru>
2021-10-01 12:32:53 +0300

    remove unused methods

--- src/libraries/System.Private.Xml/src/System/Xml/Xslt/XslCompiledTransform.cs
+++ src/libraries/System.Private.Xml/src/System/Xml/Xslt/XslCompiledTransform.cs
@@ -390,23 +390,5 @@ namespace System.Xml.Xsl
 
             return XmlNullResolver.Singleton;
         }
-
-        private QilExpression TestCompile(object stylesheet, XsltSettings settings, XmlResolver stylesheetResolver)
-        {
-            Reset();
-            CompileXsltToQil(stylesheet, settings, stylesheetResolver);
-            return _qil!;
-        }
-
-        private void TestGenerate(XsltSettings settings)
-        {
-            Debug.Assert(_qil != null, "You must compile to Qil first");
-            CompileQilToMsil(settings);
-        }
-
-        private void Transform(string inputUri, XsltArgumentList? arguments, XmlWriter results, XmlResolver documentResolver)
-        {
-            _command!.Execute(inputUri, documentResolver, arguments, results);
-        }
     }
 }
