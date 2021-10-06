d9e8a17c2f2e00410fce810e092b731b285435e9
Nikola Milosavljevic <nikolam@microsoft.com>
2021-10-06 14:17:08 -0700

    Infra for regeneration of third-party-notices file

---
+++ src/tasks/installer.tasks/StaticFileRegeneration/EnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.DotNet.Build.Tasks
+{
+    internal static class EnumerableExtensions
+    {
+        public static IEnumerable<T> NullAsEmpty<T>(this IEnumerable<T> source)
+        {
+            return source ?? Enumerable.Empty<T>();
+        }
+    }
+}
---
+++ src/tasks/installer.tasks/StaticFileRegeneration/TpnSectionHeaderFormat.cs
@@ -0,0 +1,32 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace Microsoft.DotNet.Build.Tasks
+{
+    public enum TpnSectionHeaderFormat
+    {
+        /// <summary>
+        /// {blank line}
+        /// {3+ section separator chars}
+        /// {blank line}
+        /// {name}
+        /// </summary>
+        Separated,
+
+        /// <summary>
+        /// {blank line}
+        /// {name (multiline)}
+        /// {3+ section separator chars}
+        /// {blank line}
+        /// </summary>
+        Underlined,
+
+        /// <summary>
+        /// {blank line}
+        /// {number}.{tab}{name}
+        /// {blank line}
+        /// </summary>
+        Numbered
+    }
+}
