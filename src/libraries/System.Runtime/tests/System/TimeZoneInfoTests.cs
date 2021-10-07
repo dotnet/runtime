43e93e4dd333760c554d2a46b6e802cbca682343
Tarek Mahmoud Sayed <tarekms@microsoft.com>
2021-10-07 13:30:34 -0700

    Fix TimeZones Test failure

--- src/libraries/System.Runtime/tests/System/TimeZoneInfoTests.cs
+++ src/libraries/System.Runtime/tests/System/TimeZoneInfoTests.cs
@@ -3101,6 +3101,14 @@ namespace System.Tests
                 // native string is null terminated
                 var cultureName = new string(lpUiLanguageString);
 
+                string tzResourceFilePath = Path.Join(Environment.SystemDirectory, cultureName, "tzres.dll.mui");
+                if (!File.Exists(tzResourceFilePath))
+                {
+                    // If Windows installed a UI language and not including the time zone resources DLL for that language,
+                    // then skip this language as .NET will not be able to get the localized resources for that language.
+                    return 1;
+                }
+
                 try
                 {
                     var handle = GCHandle.FromIntPtr(lParam);
