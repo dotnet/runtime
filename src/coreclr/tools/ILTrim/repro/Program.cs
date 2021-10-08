d0d77f40c512a4af569f60aab7de3d10a856d253
Andy Gocke <angocke@microsoft.com>
2021-10-08 12:07:45 -0700

    Add back repro project

---
+++ src/coreclr/tools/ILTrim/repro/Program.cs
@@ -0,0 +1,24 @@
+
+#pragma warning disable 649
+
+interface IFirstType
+{
+    static void Method() { }
+}
+
+interface IProgram
+{
+    static int Field;
+
+    static int FirstMethod() => 300;
+    static int Main()
+    {
+        return 42;
+    }
+    static void LastMethod(int someParameter) { }
+}
+
+interface IAnortherType
+{
+    static void Method() { }
+}
