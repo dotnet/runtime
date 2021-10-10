4d45c2b82e8f74f131598041b1f3859546ce9838
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2021-10-10 13:25:51 +0200

    Unify set-reg-to-imm functions and optimize with LEA at runtime on x64
    This change
    * Unifies the genSetRegToImm and instGen_Set_Reg_To_Imm functions,
      removing the former. genSetRegToImm seems to be a variant that did not
      support relocations/handles, while the latter supported both. We now
      exclusively use the emitAttr to determine this. This was only a change
      in xarch since on ARM architectures the former function just forwarded
      to the latter.
    * Generates lea more often for handle GT_CNS_INT nodes on x64.
      Previously the logic generated only lea when a relocation was
      *required*, which is only the case for prejit. In particular, all VSD
      calls load an indirection cell that can use lea instead of the longer
      mov encoding during runtime JIT, which results in a nice code size
      decrease.
    * Changes ARM64 and ARM to handle byref-typed GT_CNS_INT nodes. This was
      fixed for xarch a few years back in
      52a8890da45ce320ce84fdc3975d887ff814c678 but that change was not made
      to ARM architectures.

--- src/coreclr/jit/codegen.h
+++ src/coreclr/jit/codegen.h
@@ -1145,10 +1145,6 @@ protected:
     void genConsumeHWIntrinsicOperands(GenTreeHWIntrinsic* tree);
 #endif // FEATURE_HW_INTRINSICS
     void genEmitGSCookieCheck(bool pushReg);
-    void genSetRegToIcon(regNumber reg,
-                         ssize_t   val,
-                         var_types type = TYP_INT,
-                         insFlags flags = INS_FLAGS_DONT_CARE DEBUGARG(GenTreeFlags gtFlags = GTF_EMPTY));
     void genCodeForShift(GenTree* tree);
 
 #if defined(TARGET_X86) || defined(TARGET_ARM)
