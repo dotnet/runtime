04b225948d76f7ac4eb0c3b90b112cee6538b16f
sakno <roman.sakno@gmail.com>
2021-10-12 17:20:17 +0300

    Quick fix of #60182

--- src/libraries/System.Private.CoreLib/src/System/Threading/CancellationTokenSource.cs
+++ src/libraries/System.Private.CoreLib/src/System/Threading/CancellationTokenSource.cs
@@ -396,9 +396,21 @@ namespace System.Threading
                 // to reset it to be infinite so that it won't fire, and then recognize that it could have already
                 // fired by the time we successfully changed it, and so check to see whether that's possibly the case.
                 // If we successfully reset it and it never fired, then we can be sure it won't trigger cancellation.
-                bool reset =
-                    _timer is not TimerQueueTimer timer ||
-                    (timer.Change(Timeout.UnsignedInfinite, Timeout.UnsignedInfinite) && !timer._everQueued);
+                bool reset = false;
+                if (_timer is TimerQueueTimer timer)
+                {
+                    try
+                    {
+                        reset = timer.Change(Timeout.UnsignedInfinite, Timeout.UnsignedInfinite) && !timer._everQueued;
+                    }
+                    catch (ObjectDisposedException)
+                    {
+                        // Just eat the exception. There is no other way to tell that 
+                        // the timer has been disposed, and even if there were, there 
+                        // would not be a good way to deal with the observe/dispose 
+                        // race condition. 
+                    }
+                }
 
                 if (reset)
                 {
