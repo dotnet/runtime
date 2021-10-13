76d9194dd8a3e52947a729544375287213c46afb
Aleksey Kliger <aleksey@lambdageek.org>
2021-10-12 22:22:11 -0400

    revert unintended change

--- src/mono/mono/metadata/sre-encode.c
+++ src/mono/mono/metadata/sre-encode.c
@@ -165,7 +165,7 @@ encode_type (MonoDynamicImage *assembly, MonoType *type, SigBuffer *buf)
 	if (m_type_is_byref (type))
 		sigbuffer_add_value (buf, MONO_TYPE_BYREF);
 
-	switch (m_type_is_byref (type)) {
+	switch (type->type){
 	case MONO_TYPE_VOID:
 	case MONO_TYPE_BOOLEAN:
 	case MONO_TYPE_CHAR:
