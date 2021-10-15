313eca38094d4ded6f816f602f7e5a60e5facd98
Adam Sitnik <adam.sitnik@gmail.com>
2021-10-15 09:47:33 +0200

    add a failing test

--- src/libraries/System.IO.FileSystem/tests/FileStream/Read.cs
+++ src/libraries/System.IO.FileSystem/tests/FileStream/Read.cs
@@ -14,5 +14,39 @@ namespace System.IO.Tests
             Assert.Throws<UnauthorizedAccessException>(() =>
                 new FileStream(Path.GetPathRoot(Directory.GetCurrentDirectory()), FileMode.Open, FileAccess.Read));
         }
+
+        [Fact]
+        public void NoInt32OverflowInTheBufferingLogic()
+        {
+            const long positon1 = 10;
+            const long positon2 = (1L << 32) + positon1;
+
+            string filePath = GetTestFilePath();
+            byte[] data1 = new byte[] { 1, 2, 3, 4, 5 };
+            byte[] data2 = new byte[] { 6, 7, 8, 9, 10 };
+            byte[] buffer = new byte[5];
+
+            using (var stream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+            {
+                stream.Seek(positon1, SeekOrigin.Begin);
+                stream.Write(data1, 0, data1.Length);
+
+                stream.Seek(positon2, SeekOrigin.Begin);
+                stream.Write(data2, 0, data2.Length);
+            }
+
+            using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                stream.Seek(positon1, SeekOrigin.Begin);
+                Assert.Equal(buffer.Length, stream.Read(buffer));
+                Assert.Equal(data1, buffer);
+
+                Array.Clear(buffer);
+
+                stream.Seek(positon2, SeekOrigin.Begin);
+                Assert.Equal(buffer.Length, stream.Read(buffer));
+                Assert.Equal(data2, buffer);
+            }
+        }
     }
 }
