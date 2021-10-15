c2e08aa27aca98739712c1c034a3f9cedcdacbf2
Santiago Fernandez Madero <safern@microsoft.com>
2021-10-15 09:48:15 -0700

    Move 2 Drawing APIs that are not implemented in netfx to netcoreapp3.1 or later (#60371)

--- src/libraries/System.Drawing.Common/ref/System.Drawing.Common.cs
+++ src/libraries/System.Drawing.Common/ref/System.Drawing.Common.cs
@@ -596,12 +596,16 @@ namespace System.Drawing
         public static System.Drawing.Graphics FromImage(System.Drawing.Image image) { throw null; }
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Runtime.Versioning.SupportedOSPlatformAttribute("windows")]
+#if NETCOREAPP3_1_OR_GREATER
         [System.ObsoleteAttribute("Use the Graphics.GetContextInfo overloads that accept arguments for better performance and fewer allocations.", DiagnosticId = "SYSLIB0016", UrlFormat = "https://aka.ms/dotnet-warnings/{0}")]
+#endif
         public object GetContextInfo() { throw null; }
+#if NETCOREAPP3_1_OR_GREATER
         [System.Runtime.Versioning.SupportedOSPlatformAttribute("windows")]
         public void GetContextInfo(out PointF offset) { throw null; }
         [System.Runtime.Versioning.SupportedOSPlatformAttribute("windows")]
         public void GetContextInfo(out PointF offset, out Region? clip) { throw null; }
+#endif
         public static System.IntPtr GetHalftonePalette() { throw null; }
         public System.IntPtr GetHdc() { throw null; }
         public System.Drawing.Color GetNearestColor(System.Drawing.Color color) { throw null; }
