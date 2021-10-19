8676d97374a76dc21bcbbc58a452fa0c5f2275ee
Stephen Toub <stoub@microsoft.com>
2021-10-19 09:54:15 -0400

    Add RegexOptions.NonBacktracking (#60607)
    The simple API addition (a new RegexOptions.NonBacktracking enum member) is the tip of the iceberg for a new engine in System.Text.RegularExpressions.  Setting this option now opts the regex processing into a mode that can guarantee linear-time matching in the length of the input (doing so disables certain features of Regex and regex patterns).
    
    This commit is the initial implementation ported over from dotnet/runtimelab, which was itself seeded from the MSR Symbolic Regex Matcher project.
    
    Co-authored-by: Olli Saarikivi <olsaarik@microsoft.com>
    Co-authored-by: Stephen Toub <stoub@microsoft.com>
    Co-authored-by: Dan Moseley <danmose@microsoft.com>
    
    Co-authored-by: Margus Veanes <veanes@users.noreply.github.com>
    Co-authored-by: Olli Saarikivi <olsaarik@microsoft.com>
    Co-authored-by: Dan Moseley <danmose@microsoft.com>

---
+++ src/libraries/System.Text.RegularExpressions/src/System/Text/RegularExpressions/Symbolic/TransitionRegexKind.cs
@@ -0,0 +1,14 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Text.RegularExpressions.Symbolic
+{
+    /// <summary>Kinds of transition regexes. Transition regexes maintain a DNF form that pushes all intersections and complements to the leaves.</summary>
+    internal enum TransitionRegexKind
+    {
+        Leaf,
+        Conditional,
+        Union,
+        Lookaround
+    }
+}
---
+++ src/libraries/System.Text.RegularExpressions/src/System/Text/RegularExpressions/Symbolic/Unicode/IgnoreCaseRelationGenerator.cs
@@ -0,0 +1,112 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+
+namespace System.Text.RegularExpressions.Symbolic.Unicode
+{
+#if DEBUG
+    internal static class IgnoreCaseRelationGenerator
+    {
+        private const string DefaultCultureName = "en-US";
+
+        public static void Generate(string namespacename, string classname, string path)
+        {
+            Debug.Assert(namespacename != null);
+            Debug.Assert(classname != null);
+            Debug.Assert(path != null);
+
+            using StreamWriter sw = new StreamWriter($"{Path.Combine(path, classname)}.cs");
+            sw.WriteLine(
+$@"// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+// This is a programmatically generated file from Regex.GenerateUnicodeTables.
+// It provides serialized BDD Unicode category definitions for System.Environment.Version = {Environment.Version}
+
+namespace {namespacename}
+{{
+    internal static class {classname}
+    {{");
+            WriteIgnoreCaseBDD(sw);
+            sw.WriteLine($@"    }}
+}}");
+        }
+
+        private static void WriteIgnoreCaseBDD(StreamWriter sw)
+        {
+            sw.WriteLine("        /// <summary>Serialized BDD for mapping characters to their case-ignoring equivalence classes in the default (en-US) culture.</summary>");
+
+            var solver = new CharSetSolver();
+            Dictionary<char, BDD> ignoreCase = ComputeIgnoreCaseDictionary(solver, new CultureInfo(DefaultCultureName));
+            BDD ignorecase = solver.False;
+            foreach (KeyValuePair<char, BDD> kv in ignoreCase)
+            {
+                BDD a = solver.CreateCharSetFromRange(kv.Key, kv.Key);
+                BDD b = kv.Value;
+                ignorecase = solver.Or(ignorecase, solver.And(solver.ShiftLeft(a, 16), b));
+            }
+
+            sw.Write("        public static readonly long[] IgnoreCaseEnUsSerializedBDD = ");
+            GeneratorHelper.WriteInt64ArrayInitSyntax(sw, ignorecase.Serialize());
+            sw.WriteLine(";");
+        }
+
+        private static Dictionary<char, BDD> ComputeIgnoreCaseDictionary(CharSetSolver solver, CultureInfo culture)
+        {
+            CultureInfo originalCulture = CultureInfo.CurrentCulture;
+            try
+            {
+                CultureInfo.CurrentCulture = culture;
+
+                var ignoreCase = new Dictionary<char, BDD>();
+
+                for (uint i = 0; i <= 0xFFFF; i++)
+                {
+                    char c = (char)i;
+                    char cUpper = char.ToUpper(c);
+                    char cLower = char.ToLower(c);
+
+                    if (cUpper == cLower)
+                    {
+                        continue;
+                    }
+
+                    // c may be different from both cUpper as well as cLower.
+                    // Make sure that the regex engine considers c as being equivalent to cUpper and cLower, else ignore c.
+                    // In some cases c != cU but the regex engine does not consider the chacarters equivalent wrt the ignore-case option.
+                    if (Regex.IsMatch($"{cUpper}{cLower}", $"^(?i:\\u{i:X4}\\u{i:X4})$"))
+                    {
+                        BDD equiv = solver.False;
+
+                        if (ignoreCase.ContainsKey(c))
+                            equiv = solver.Or(equiv, ignoreCase[c]);
+
+                        if (ignoreCase.ContainsKey(cUpper))
+                            equiv = solver.Or(equiv, ignoreCase[cUpper]);
+
+                        if (ignoreCase.ContainsKey(cLower))
+                            equiv = solver.Or(equiv, ignoreCase[cLower]);
+
+                        // Make sure all characters are included initially or when some is still missing
+                        equiv = solver.Or(equiv, solver.Or(solver.CreateCharSetFromRange(c, c), solver.Or(solver.CreateCharSetFromRange(cUpper, cUpper), solver.CreateCharSetFromRange(cLower, cLower))));
+
+                        // Update all the members with their case-invariance equivalence classes
+                        foreach (char d in solver.GenerateAllCharacters(equiv))
+                            ignoreCase[d] = equiv;
+                    }
+                }
+
+                return ignoreCase;
+            }
+            finally
+            {
+                CultureInfo.CurrentCulture = originalCulture;
+            }
+        }
+    };
+#endif
+}
