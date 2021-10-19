b93305be7d085d45cea32d9c71fb4f5e54e2aba1
carlossanlop <1175054+carlossanlop@users.noreply.github.com>
2021-10-19 14:30:13 -0500

    Adjust tests

--- src/libraries/System.IO.Compression/tests/ZipArchive/zip_UpdateTests.cs
+++ src/libraries/System.IO.Compression/tests/ZipArchive/zip_UpdateTests.cs
@@ -438,6 +438,64 @@ namespace System.IO.Compression.Tests
             }
         }
 
+        [Theory]
+        // General purpose bit flag must get the appropriate bit set if a file comment or an entry name is unicode
+        [InlineData("ascii", "ascii!!!", "utf-8", "UÄäÖöÕõÜü")]
+        [InlineData("utf-8", "UÄäÖöÕõÜü", "ascii", "ascii!!!")]
+        [InlineData("ascii", "ascii!!!", "latin1", "LÄäÖöÕõÜü")]
+        [InlineData("latin1", "LÄäÖöÕõÜü", "ascii", "ascii!!!")]
+        [InlineData("utf-8", "UÄäÖöÕõÜü", "latin1", "LÄäÖöÕõÜü")]
+        [InlineData("latin1", "LÄäÖöÕõÜü", "utf-8", "UÄäÖöÕõÜü")]
+        public static void Update_ZipArchiveEntry_DifferentEncodingsFullNameAndComment(string en1, string s1, string en2, string s2)
+        {
+            Encoding e1 = Encoding.GetEncoding(en1);
+            Encoding e2 = Encoding.GetEncoding(en2);
+            string entryName = e1.GetString(e1.GetBytes(s1));
+            string comment = e2.GetString(e2.GetBytes(s2));
+
+            var stream = new MemoryStream();
+            var testStream = new WrappedStream(stream, true, true, true, null);
+
+            // Create with no encoding to autoselect it if one of the two strings is unicode
+            using (var zip = new ZipArchive(testStream, ZipArchiveMode.Create, leaveOpen: true))
+            {
+                ZipArchiveEntry entry = zip.CreateEntry(entryName, CompressionLevel.NoCompression);
+                entry.Comment = comment;
+
+                Assert.Equal(entryName, entry.FullName);
+                Assert.Equal(comment, entry.Comment);
+            }
+
+            // Open with no encoding to verify
+            using (var zip = new ZipArchive(testStream, ZipArchiveMode.Read, leaveOpen: true))
+            {
+                foreach (var entry in zip.Entries)
+                {
+                    Assert.Equal(entryName, entry.FullName);
+                    Assert.Equal(comment, entry.Comment);
+                }
+            }
+
+            // Open with no encoding to update
+            using (var zip = new ZipArchive(testStream, ZipArchiveMode.Read, leaveOpen: true))
+            {
+                foreach (var entry in zip.Entries)
+                {
+                    entry.Comment = entryName; // Change it to the other string
+                }
+            }
+
+            // Open with no encoding to verify
+            using (var zip = new ZipArchive(testStream, ZipArchiveMode.Read, leaveOpen: false))
+            {
+                foreach (var entry in zip.Entries)
+                {
+                    Assert.Equal(entryName, entry.FullName);
+                    Assert.Equal(comment, entry.Comment);
+                }
+            }
+        }
+
         private static Encoding[] s_encodings = new[] { Encoding.ASCII, Encoding.Latin1, Encoding.UTF8 };
 
         // entryName, encoding
