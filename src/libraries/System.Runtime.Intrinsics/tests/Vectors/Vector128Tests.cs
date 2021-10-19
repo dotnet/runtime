d90eaaaf447108d675efa331878d21991a003d45
Robin Lindner <robin.lindner1@t-online.de>
2021-10-19 21:29:49 +0200

    Create Test project for System.Runtime.Intrinsics and move vector tests from System.Runtime to System.Runtime.Intrinsics

---
+++ src/libraries/System.Runtime.Intrinsics/tests/Vectors/Vector128Tests.cs
@@ -0,0 +1,41 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Linq;
+using Xunit;
+
+namespace System.Runtime.Intrinsics.Tests.Vectors
+{
+    public sealed class Vector128Tests
+    {
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(0, 1, 2, 3, 4, 5, 6, 7, 8)]
+        [InlineData(50, 430, int.MaxValue, int.MinValue)]
+        public void Vector128Int32IndexerTest(params int[] values)
+        {
+            var vector = Vector128.Create(values);
+
+            Assert.All(
+                values.Select((value, index) => (index, value)),
+                tuple => Assert.Equal(tuple.value, vector[tuple.index])
+            );
+        }
+
+        [Theory]
+        [InlineData(0L)]
+        [InlineData(1L)]
+        [InlineData(0L, 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L)]
+        [InlineData(50L, 430L, long.MaxValue, long.MinValue)]
+        public void Vector128Int64IndexerTest(params long[] values)
+        {
+            var vector = Vector128.Create(values);
+
+            Assert.All(
+                values.Select((value, index) => (index, value)),
+                tuple => Assert.Equal(tuple.value, vector[tuple.index])
+            );
+        }
+    }
+}
---
+++ src/libraries/System.Runtime.Intrinsics/tests/Vectors/Vector256Tests.cs
@@ -0,0 +1,42 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Linq;
+using Xunit;
+
+namespace System.Runtime.Intrinsics.Tests.Vectors
+{
+    public sealed class Vector256Tests
+    {
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(-1)]
+        [InlineData(0, 1, 2, 3, 4, 5, 6, 7, 8)]
+        [InlineData(50, 430, int.MaxValue, int.MinValue)]
+        public void Vector256Int32IndexerTest(params int[] values)
+        {
+            var vector = Vector256.Create(values);
+
+            Assert.All(
+                values.Select((value, index) => (index, value)),
+                tuple => Assert.Equal(tuple.value, vector[tuple.index])
+            );
+        }
+
+        [Theory]
+        [InlineData(0L)]
+        [InlineData(1L)]
+        [InlineData(0L, 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L)]
+        [InlineData(50L, 430L, long.MaxValue, long.MinValue)]
+        public void Vector256Int64IndexerTest(params long[] values)
+        {
+            var vector = Vector256.Create(values);
+
+            Assert.All(
+                values.Select((value, index) => (index, value)),
+                tuple => Assert.Equal(tuple.value, vector[tuple.index])
+            );
+        }
+    }
+}
