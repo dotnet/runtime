ba0d909c8f9988448a084d5972325871b4a167d5
Robin Lindner <robin.lindner1@t-online.de>
2021-10-20 21:41:49 +0200

    Fix test cases

--- src/libraries/System.Runtime.Intrinsics/tests/Vectors/Vector128Tests.cs
+++ src/libraries/System.Runtime.Intrinsics/tests/Vectors/Vector128Tests.cs
@@ -9,8 +9,8 @@ namespace System.Runtime.Intrinsics.Tests.Vectors
     public sealed class Vector128Tests
     {
         [Theory]
-        [InlineData(0)]
-        [InlineData(1)]
+        [InlineData(0, 0, 0, 0)]
+        [InlineData(1, 1, 1, 1)]
         [InlineData(0, 1, 2, 3, 4, 5, 6, 7, 8)]
         [InlineData(50, 430, int.MaxValue, int.MinValue)]
         public void Vector128Int32IndexerTest(params int[] values)
@@ -24,8 +24,8 @@ namespace System.Runtime.Intrinsics.Tests.Vectors
         }
 
         [Theory]
-        [InlineData(0L)]
-        [InlineData(1L)]
+        [InlineData(0L, 0L, 0L, 0L)]
+        [InlineData(1L, 1L, 1L, 1L)]
         [InlineData(0L, 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L)]
         [InlineData(50L, 430L, long.MaxValue, long.MinValue)]
         public void Vector128Int64IndexerTest(params long[] values)
--- src/libraries/System.Runtime.Intrinsics/tests/Vectors/Vector256Tests.cs
+++ src/libraries/System.Runtime.Intrinsics/tests/Vectors/Vector256Tests.cs
@@ -9,11 +9,11 @@ namespace System.Runtime.Intrinsics.Tests.Vectors
     public sealed class Vector256Tests
     {
         [Theory]
-        [InlineData(0)]
-        [InlineData(1)]
-        [InlineData(-1)]
+        [InlineData(0, 0, 0, 0, 0, 0, 0, 0)]
+        [InlineData(1, 1, 1, 1, 1, 1, 1, 1)]
+        [InlineData(-1, -1, -1, -1, -1, -1, -1, -1)]
         [InlineData(0, 1, 2, 3, 4, 5, 6, 7, 8)]
-        [InlineData(50, 430, int.MaxValue, int.MinValue)]
+        [InlineData(0, 0, 50, 430, -64, 0, int.MaxValue, int.MinValue)]
         public void Vector256Int32IndexerTest(params int[] values)
         {
             var vector = Vector256.Create(values);
@@ -25,10 +25,10 @@ namespace System.Runtime.Intrinsics.Tests.Vectors
         }
 
         [Theory]
-        [InlineData(0L)]
-        [InlineData(1L)]
+        [InlineData(0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L)]
+        [InlineData(1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L)]
         [InlineData(0L, 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L)]
-        [InlineData(50L, 430L, long.MaxValue, long.MinValue)]
+        [InlineData(0L, 0L, 50L, 430L, -64L, 0L, long.MaxValue, long.MinValue)]
         public void Vector256Int64IndexerTest(params long[] values)
         {
             var vector = Vector256.Create(values);
