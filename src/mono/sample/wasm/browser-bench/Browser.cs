8901a0b2092b951b5048ac556f2909d3b7f04024
Radek Doulik <radekdoulik@gmail.com>
2021-10-21 21:52:22 +0200

    [wasm] Add console version of browser-bench sample

---
+++ src/mono/sample/wasm/browser-bench/Browser.cs
@@ -0,0 +1,13 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Sample
+{
+    public partial class Test
+    {
+        public static int Main(string[] args)
+        {
+            return 0;
+        }
+    }
+}
