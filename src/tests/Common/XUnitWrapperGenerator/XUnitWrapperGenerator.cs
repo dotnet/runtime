6d602f79f79a42b558cbc0a5e9c493d108ae5850
Jeremy Koritzinsky <jekoritz@microsoft.com>
2021-10-22 14:42:35 -0700

    Implement support for [Fact] methods that are instance methods on IDisposable types.

--- src/tests/Common/XUnitWrapperGenerator/XUnitWrapperGenerator.cs
+++ src/tests/Common/XUnitWrapperGenerator/XUnitWrapperGenerator.cs
@@ -1,39 +1,57 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
-namespace XUnitWrapperGenerator
+namespace XUnitWrapperGenerator;
+
+[Generator]
+public sealed class XUnitWrapperGenerator : IIncrementalGenerator
 {
-    [Generator]
-    public sealed class XUnitWrapperGenerator : IIncrementalGenerator
+    public void Initialize(IncrementalGeneratorInitializationContext context)
     {
-        public void Initialize(IncrementalGeneratorInitializationContext context)
+        context.RegisterImplementationSourceOutput(
+            context.SyntaxProvider.CreateSyntaxProvider(
+                static (node, ct) =>
+                    node.IsKind(SyntaxKind.MethodDeclaration)
+                        && node is MethodDeclarationSyntax method
+                        && method.AttributeLists.Count > 0,
+                static (context, ct) => (IMethodSymbol)context.SemanticModel.GetDeclaredSymbol(context.Node)!)
+                .SelectMany((method, ct) => ImmutableArray.CreateRange(GetTestMethodInfosForMethod(method)))
+                .Collect(),
+            static (context, methods) =>
+            {
+                // For simplicity, we'll use top-level statements for the generated Main method.
+                StringBuilder builder = new();
+                builder.AppendLine("try {");
+                builder.Append(string.Join("\n", methods.Select(m => m.ExecutionStatement)));
+                builder.AppendLine("} catch(System.Exception) { return 101; }");
+                builder.AppendLine("return 100;");
+                context.AddSource("Main.g.cs", builder.ToString());
+            });
+    }
+
+    private static IEnumerable<ITestMethodInfo> GetTestMethodInfosForMethod(IMethodSymbol method)
+    {
+        bool factAttribute = false;
+        foreach (var attr in method.GetAttributes())
         {
-            context.RegisterImplementationSourceOutput(
-                context.SyntaxProvider.CreateSyntaxProvider(
-                    static (node, ct) =>
-                        node.IsKind(SyntaxKind.MethodDeclaration)
-                            && node is MethodDeclarationSyntax method
-                            && method.AttributeLists.Count > 0,
-                    static (context, ct) => (IMethodSymbol)context.SemanticModel.GetDeclaredSymbol(context.Node)!)
-                    .Where(method => method.IsStatic && method.Parameters.IsEmpty && method.GetAttributes().Any(attr => attr.AttributeClass!.ToDisplayString() == "Xunit.FactAttribute"))
-                    .Select((method, ct) => $"{method.ContainingType.ToDisplayString()}.{method.Name}")
-                    .Collect(),
-                static (context, methods) =>
-                {
-                    // For simplicity, we'll use top-level statements for the generated Main method.
-                    StringBuilder builder = new();
-                    builder.AppendLine("try {");
-                    builder.Append(string.Join("\n", methods.Select(m => $"{m}();")));
-                    builder.AppendLine("} catch(System.Exception) { return 101; }");
-                    builder.AppendLine("return 100;");
-                    context.AddSource("Main.g.cs", builder.ToString());
-                });
+            if (attr.AttributeClass!.ToDisplayString() == "Xunit.FactAttribute")
+            {
+                factAttribute = true;
+            }
         }
+        if (factAttribute && method.Parameters.IsEmpty)
+        {
+            return new[] { method.IsStatic ? (ITestMethodInfo)new StaticFactMethod(method) : new InstanceFactMethod(method) };
+        }
+
+        return Enumerable.Empty<ITestMethodInfo>();
     }
 }
