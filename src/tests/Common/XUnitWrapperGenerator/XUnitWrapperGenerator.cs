1bbfc9c237b4d4f1be9844695b5bc099a8094343
Jeremy Koritzinsky <jekoritz@microsoft.com>
2021-10-21 17:02:14 -0700

    Add a basic xunit runner generator that generates a top-level-statement Main method that runs all static parameterless [Fact] attributes in sequence, wrapped in a simple try-catch.

---
+++ src/tests/Common/XUnitWrapperGenerator/XUnitWrapperGenerator.cs
@@ -0,0 +1,39 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Linq;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace XUnitWrapperGenerator
+{
+    [Generator]
+    public sealed class XUnitWrapperGenerator : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            context.RegisterImplementationSourceOutput(
+                context.SyntaxProvider.CreateSyntaxProvider(
+                    static (node, ct) =>
+                        node.IsKind(SyntaxKind.MethodDeclaration)
+                            && node is MethodDeclarationSyntax method
+                            && method.AttributeLists.Count > 0,
+                    static (context, ct) => (IMethodSymbol)context.SemanticModel.GetDeclaredSymbol(context.Node)!)
+                    .Where(method => method.IsStatic && method.Parameters.IsEmpty && method.GetAttributes().Any(attr => attr.AttributeClass!.ToDisplayString() == "Xunit.FactAttribute"))
+                    .Select((method, ct) => method.ToDisplayString())
+                    .Collect(),
+                static (context, methods) =>
+                {
+                    // For simplicity, we'll use top-level statements for the generated Main method.
+                    StringBuilder builder = new();
+                    builder.AppendLine("try {");
+                    builder.Append(string.Join("\n", methods.Select(m => $"{m}();")));
+                    builder.AppendLine("} catch(Exception) { return 101; }");
+                    builder.AppendLine("return 100;");
+                    context.AddSource("Main.g.cs", builder.ToString());
+                });
+        }
+    }
+}
