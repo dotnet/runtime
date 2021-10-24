85fd91d7d26ccfe388ab04b4a844f02a37416b39
Jan Kotas <jkotas@microsoft.com>
2021-10-24 10:57:56 -0700

    Update src/libraries/System.Private.CoreLib/src/System/Diagnostics/Tracing/TraceLogging/PropertyValue.cs

--- src/libraries/System.Private.CoreLib/src/System/Diagnostics/Tracing/TraceLogging/PropertyValue.cs
+++ src/libraries/System.Private.CoreLib/src/System/Diagnostics/Tracing/TraceLogging/PropertyValue.cs
@@ -214,7 +214,11 @@ namespace System.Diagnostics.Tracing
         private sealed class ReferenceTypeHelper<TContainer> : TypeHelper where TContainer : class
         {
             private static Func<TContainer, TProperty> GetGetMethod<TProperty>(PropertyInfo property) where TProperty : struct =>
+#if ES_BUILD_STANDALONE
+                (Func<TContainer, TProperty>)GetMethod!.CreateDelegate(typeof(Func<TContainer, TProperty>));
+#else
                 property.GetMethod!.CreateDelegate<Func<TContainer, TProperty>>();
+#endif
 
             public override Func<PropertyValue, PropertyValue> GetPropertyGetter(PropertyInfo property)
             {
