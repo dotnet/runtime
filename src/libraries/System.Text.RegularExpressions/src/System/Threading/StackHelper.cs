9ba1b85c4867391730faa5b2661e81e1637dbcc1
Olli Saarikivi <olsaarik@microsoft.com>
2021-10-24 09:04:01 -0700

    More stack overflow avoidance in regex NonBacktracking (#60786)
    * Add stackoverflow avoidance to SymbolicRegexNode
    
    Remove unused Restrict methods instead of adding avoidance.
    
    Co-authored-by: Stephen Toub <stoub@microsoft.com>
    
    * Add more stack overflow avoidance in regex
    
    This follows a full audit of the NonBacktracking code.
    
    * Apply suggestions from code review
    
    Co-authored-by: Stephen Toub <stoub@microsoft.com>

--- src/libraries/System.Text.RegularExpressions/src/System/Threading/StackHelper.cs
+++ src/libraries/System.Text.RegularExpressions/src/System/Threading/StackHelper.cs
@@ -32,11 +32,29 @@ namespace System.Threading
         // also plays nicely with the thread pool's sync-over-async aggressive thread injection policies.
 
         /// <summary>Calls the provided action on the stack of a different thread pool thread.</summary>
+        /// <param name="action">The action to invoke.</param>
+        public static void CallOnEmptyStack(Action action) =>
+            Task.Run(() => action())
+                .ContinueWith(t => t.GetAwaiter().GetResult(), CancellationToken.None, TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default)
+                .GetAwaiter().GetResult();
+
+        /// <summary>Calls the provided action on the stack of a different thread pool thread.</summary>
         /// <typeparam name="TArg1">The type of the first argument to pass to the function.</typeparam>
         /// <param name="action">The action to invoke.</param>
         /// <param name="arg1">The first argument to pass to the action.</param>
         public static void CallOnEmptyStack<TArg1>(Action<TArg1> action, TArg1 arg1) =>
             Task.Run(() => action(arg1))
+                .ContinueWith(t => t.GetAwaiter().GetResult(), CancellationToken.None, TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default)
+                .GetAwaiter().GetResult();
+
+        /// <summary>Calls the provided action on the stack of a different thread pool thread.</summary>
+        /// <typeparam name="TArg1">The type of the first argument to pass to the function.</typeparam>
+        /// <typeparam name="TArg2">The type of the second argument to pass to the function.</typeparam>
+        /// <param name="action">The action to invoke.</param>
+        /// <param name="arg1">The first argument to pass to the action.</param>
+        /// <param name="arg2">The second argument to pass to the action.</param>
+        public static void CallOnEmptyStack<TArg1, TArg2>(Action<TArg1, TArg2> action, TArg1 arg1, TArg2 arg2) =>
+            Task.Run(() => action(arg1, arg2))
                 .ContinueWith(t => t.GetAwaiter().GetResult(), CancellationToken.None, TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default)
                 .GetAwaiter().GetResult();
 
@@ -50,6 +68,24 @@ namespace System.Threading
         /// <param name="arg3">The second argument to pass to the action.</param>
         public static void CallOnEmptyStack<TArg1, TArg2, TArg3>(Action<TArg1, TArg2, TArg3> action, TArg1 arg1, TArg2 arg2, TArg3 arg3) =>
             Task.Run(() => action(arg1, arg2, arg3))
+                .ContinueWith(t => t.GetAwaiter().GetResult(), CancellationToken.None, TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default)
+                .GetAwaiter().GetResult();
+
+        /// <summary>Calls the provided function on the stack of a different thread pool thread.</summary>
+        /// <typeparam name="TResult">The return type of the function.</typeparam>
+        /// <param name="func">The function to invoke.</param>
+        public static TResult CallOnEmptyStack<TResult>(Func<TResult> func) =>
+            Task.Run(() => func())
+                .ContinueWith(t => t.GetAwaiter().GetResult(), CancellationToken.None, TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default)
+                .GetAwaiter().GetResult();
+
+        /// <summary>Calls the provided function on the stack of a different thread pool thread.</summary>
+        /// <typeparam name="TArg1">The type of the first argument to pass to the function.</typeparam>
+        /// <typeparam name="TResult">The return type of the function.</typeparam>
+        /// <param name="func">The function to invoke.</param>
+        /// <param name="arg1">The first argument to pass to the function.</param>
+        public static TResult CallOnEmptyStack<TArg1, TResult>(Func<TArg1, TResult> func, TArg1 arg1) =>
+            Task.Run(() => func(arg1))
                 .ContinueWith(t => t.GetAwaiter().GetResult(), CancellationToken.None, TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default)
                 .GetAwaiter().GetResult();
 
