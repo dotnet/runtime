1cd5a999e034e61c58554ac19173b149fa6d5547
Andy Ayers <andya@microsoft.com>
2021-10-26 11:21:06 -0700

    JIT: limited version of forward substitution for some relops
    Add a new optimization to redundant branch opts that looks at prior statements
    in the same block for redundant relop trees. If one is found, we check to see
    if it can be forward-substituted down to the terminal jump tree. And if it can
    we duplicate the computation down at the jump.
    
    This removes many of the cases we see in our generated code where we materialize
    a boolean value in a register and then immedately test that register to see if
    it is true/false, and then use that second test to drive branching -- instead we
    now use the initial test logic to drive the jump and so the boolean value only
    exists in the flags.

--- src/coreclr/jit/compiler.h
+++ src/coreclr/jit/compiler.h
@@ -7468,6 +7468,7 @@ public:
     // Redundant branch opts
     //
     PhaseStatus optRedundantBranches();
+    bool optRedundantRelop(BasicBlock* const block);
     bool optRedundantBranch(BasicBlock* const block);
     bool optJumpThread(BasicBlock* const block, BasicBlock* const domBlock, bool domIsSameRelop);
     bool optReachable(BasicBlock* const fromBlock, BasicBlock* const toBlock, BasicBlock* const excludedBlock);
