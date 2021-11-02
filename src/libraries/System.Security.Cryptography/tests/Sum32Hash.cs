1bbf6df7ec87f7081a6c5dd810082c572b6ce59c
Jeremy Barton <jbarton@microsoft.com>
2021-11-02 10:09:45 -0700

    Fix namespaces of tests from Primitives

--- src/libraries/System.Security.Cryptography/tests/Sum32Hash.cs
+++ src/libraries/System.Security.Cryptography/tests/Sum32Hash.cs
@@ -1,7 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-namespace System.Security.Cryptography.Hashing.Tests
+namespace System.Security.Cryptography.Tests
 {
     internal class Sum32Hash : HashAlgorithm
     {
