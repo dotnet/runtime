6527f540e4b50bc84eb72705f80d3f2bdd57473b
Aaron Robinson <arobins@microsoft.com>
2021-11-02 21:27:34 -0700

    Add TFM and version support query to marshaller interface. (#61064)
    * Add TFM and version support query to marshaller interface.
    
    * If any marshaller fails to support the current tfm version, fallback
    to using the built-in DllImport attribute.
    
    * Add tests for fallback generation on downlevel or generation if marshallers
      are supported on the target framework.
    
    * Convert CreateCompilationWithReferenceAssemblies to CreateCompilation overload.
    
    Co-authored-by: Elinor Fung <elfung@microsoft.com>

--- src/libraries/System.Runtime.InteropServices/gen/DllImportGenerator/PInvokeStubCodeGenerator.cs
+++ src/libraries/System.Runtime.InteropServices/gen/DllImportGenerator/PInvokeStubCodeGenerator.cs
@@ -34,6 +34,10 @@ namespace Microsoft.Interop
 
         public override bool AdditionalTemporaryStateLivesAcrossStages => true;
 
+        public bool SupportsTargetFramework { get; init; }
+
+        public bool StubIsBasicForwarder { get; init; }
+
         /// <summary>
         /// Identifier for managed return value
         /// </summary>
@@ -58,9 +62,8 @@ namespace Microsoft.Interop
         private readonly List<BoundGenerator> _sortedMarshallers;
         private readonly bool _stubReturnsVoid;
 
-        public bool StubIsBasicForwarder { get; }
-
         public PInvokeStubCodeGenerator(
+            StubEnvironment environment,
             IEnumerable<TypePositionInfo> argTypes,
             bool setLastError,
             Action<TypePositionInfo, MarshallingNotSupportedException> marshallingNotSupportedCallback,
@@ -68,6 +71,19 @@ namespace Microsoft.Interop
         {
             _setLastError = setLastError;
 
+            // Support for SetLastError logic requires .NET 6+. Initialize the
+            // supports target framework value with this value.
+            if (_setLastError)
+            {
+                SupportsTargetFramework = environment.TargetFramework == TargetFramework.Net
+                    && environment.TargetFrameworkVersion.Major >= 6;
+            }
+            else
+            {
+                SupportsTargetFramework = true;
+            }
+
+            bool noMarshallingNeeded = true;
             List<BoundGenerator> allMarshallers = new();
             List<BoundGenerator> paramMarshallers = new();
             bool foundNativeRetMarshaller = false;
@@ -78,6 +94,14 @@ namespace Microsoft.Interop
             foreach (TypePositionInfo argType in argTypes)
             {
                 BoundGenerator generator = CreateGenerator(argType);
+
+                // Check each marshaler if the current target framework is supported or not.
+                SupportsTargetFramework &= generator.Generator.IsSupported(environment.TargetFramework, environment.TargetFrameworkVersion);
+
+                // Check if generator is either blittable or just a forwarder.
+                noMarshallingNeeded &= generator is { Generator: BlittableMarshaller, TypeInfo: { IsByRef: false } }
+                        or { Generator: Forwarder };
+
                 allMarshallers.Add(generator);
                 if (argType.IsManagedReturnPosition)
                 {
@@ -139,9 +163,7 @@ namespace Microsoft.Interop
 
             StubIsBasicForwarder = !setLastError
                 && managedRetMarshaller.TypeInfo.IsNativeReturnPosition // If the managed return has native return position, then it's the return for both.
-                && _sortedMarshallers.All(
-                    m => m is { Generator: BlittableMarshaller, TypeInfo: { IsByRef: false } }
-                        or { Generator: Forwarder });
+                && noMarshallingNeeded;
 
             if (managedRetMarshaller.Generator.UsesNativeIdentifier(managedRetMarshaller.TypeInfo, this))
             {
--- src/libraries/System.Runtime.InteropServices/tests/DllImportGenerator.UnitTests/Compiles.cs
+++ src/libraries/System.Runtime.InteropServices/tests/DllImportGenerator.UnitTests/Compiles.cs
@@ -310,27 +310,38 @@ namespace DllImportGenerator.UnitTests
             Assert.Empty(newCompDiags);
         }
 
-        public static IEnumerable<object[]> CodeSnippetsToCompileWithForwarder()
+        public static IEnumerable<object[]> CodeSnippetsToValidateFallbackForwarder()
         {
-            yield return new[] { CodeSnippets.UserDefinedEntryPoint };
-            yield return new[] { CodeSnippets.AllSupportedDllImportNamedArguments };
+            yield return new object[] { CodeSnippets.UserDefinedEntryPoint, TestTargetFramework.Net, true };
 
-            // Parameter / return types (supported in DllImportGenerator)
-            yield return new[] { CodeSnippets.BasicParametersAndModifiers<byte>() };
-            // Parameter / return types (not supported in DllImportGenerator)
-            yield return new[] { CodeSnippets.BasicParametersAndModifiers<string>() };
+            // Confirm that all unsupported target frameworks can be generated.
+            {
+                string code = CodeSnippets.BasicParametersAndModifiers<byte>(CodeSnippets.GeneratedDllImportAttributeDeclaration);
+                yield return new object[] { code, TestTargetFramework.Net5, false };
+                yield return new object[] { code, TestTargetFramework.Core, false };
+                yield return new object[] { code, TestTargetFramework.Standard, false };
+                yield return new object[] { code, TestTargetFramework.Framework, false };
+            }
+
+            // Confirm that all unsupported target frameworks fallback to a forwarder.
+            {
+                string code = CodeSnippets.BasicParametersAndModifiersWithCharSet<string>(CharSet.Unicode, CodeSnippets.GeneratedDllImportAttributeDeclaration);
+                yield return new object[] { code, TestTargetFramework.Net5, true };
+                yield return new object[] { code, TestTargetFramework.Core, true };
+                yield return new object[] { code, TestTargetFramework.Standard, true };
+                yield return new object[] { code, TestTargetFramework.Framework, true };
+            }
         }
 
         [ConditionalTheory]
-        [MemberData(nameof(CodeSnippetsToCompileWithForwarder))]
-        public async Task ValidateSnippetsWithForwarder(string source)
+        [MemberData(nameof(CodeSnippetsToValidateFallbackForwarder))]
+        public async Task ValidateSnippetsFallbackForwarder(string source, TestTargetFramework targetFramework, bool expectFallbackForwarder)
         {
-            Compilation comp = await TestUtils.CreateCompilation(source);
+            Compilation comp = await TestUtils.CreateCompilation(source, targetFramework);
             TestUtils.AssertPreSourceGeneratorCompilation(comp);
 
             var newComp = TestUtils.RunGenerators(
                 comp,
-                new DllImportGeneratorOptionsProvider(useMarshalType: false, generateForwarders: true),
                 out var generatorDiags,
                 new Microsoft.Interop.DllImportGenerator());
 
@@ -349,7 +360,8 @@ namespace DllImportGenerator.UnitTests
 
             IMethodSymbol method = model.GetDeclaredSymbol(generatedMethod)!;
 
-            Assert.NotNull(method.GetDllImportData());
+            // If we expect fallback forwarder, then the DllImportData will not be null.
+            Assert.Equal(expectFallbackForwarder, method.GetDllImportData() is not null);
         }
 
         public static IEnumerable<object[]> FullyBlittableSnippetsToCompile()
