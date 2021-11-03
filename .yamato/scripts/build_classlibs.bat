c0a413eff7708db61f6e4ec6b5e277b8d6983f59
Alex Thibodeau <alexthibodeau@unity3d.com>
2021-11-03 12:29:34 -0400

    PR feedback: Use a function for echoing system calls

--- .yamato/scripts/build_classlibs.bat
+++ .yamato/scripts/build_classlibs.bat
@@ -3,18 +3,20 @@
 if not exist "incomingbuilds" mkdir "incomingbuilds"
 
 for %%x IN ("windows", "OSX", "Linux") do (
-    ECHO build.cmd libs -os %%x -c release
-    build.cmd libs -os %%x -c release
+    CALL :EchoAndExecute build.cmd libs -os %%x -c release
     if NOT %errorlevel% == 0 (
         echo "build failed"
         EXIT /B %errorlevel%
     )
     if not exist "incomingbuilds/coreclrjit-%%x" mkdir "incomingbuilds/coreclrjit-%%x"
-    ECHO xcopy /s /e /h /y "artifacts/bin/runtime/net7.0-%%x-Release-x64" "incomingbuilds/coreclrjit-%%x"
-    xcopy /s /e /h /y "artifacts/bin/runtime/net7.0-%%x-Release-x64" "incomingbuilds/coreclrjit-%%x"
-    ECHO taskkill /IM "dotnet.exe" /F
-    taskkill /IM "dotnet.exe" /F
-    echo build.cmd -clean
-    build.cmd -clean
+
+    CALL :EchoAndExecute xcopy /s /e /h /y "artifacts/bin/runtime/net7.0-%%x-Release-x64" "incomingbuilds/coreclrjit-%%x"
+    CALL :EchoAndExecute taskkill /IM "dotnet.exe" /F
+    CALL :EchoAndExecute build.cmd -clean
 )
 EXIT /B %ERRORLEVEL%
+
+:EchoAndExecute
+ECHO %*
+CALL %*
+GOTO :EOF
