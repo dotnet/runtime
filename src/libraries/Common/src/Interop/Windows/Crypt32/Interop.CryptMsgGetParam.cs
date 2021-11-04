5104c5824bbfa0678fe0ce872b798e39d5046f49
Aaron Robinson <arobins@microsoft.com>
2021-11-04 16:40:22 -0700

    Update Crypto P/Invokes.

--- src/libraries/Common/src/Interop/Windows/Crypt32/Interop.CryptMsgGetParam.cs
+++ src/libraries/Common/src/Interop/Windows/Crypt32/Interop.CryptMsgGetParam.cs
@@ -11,28 +11,29 @@ internal static partial class Interop
     {
 #if DLLIMPORTGENERATOR_ENABLED
         [GeneratedDllImport(Libraries.Crypt32, CharSet = CharSet.Unicode, SetLastError = true)]
+        internal static partial bool CryptMsgGetParam(
+#else
+        [DllImport(Libraries.Crypt32, CharSet = CharSet.Unicode, SetLastError = true)]
+        internal static extern bool CryptMsgGetParam(
+#endif
+            SafeCryptMsgHandle hCryptMsg,
+            CryptMsgParamType dwParamType,
+            int dwIndex,
+            out int pvData,
+            ref int pcbData);
+
+#if DLLIMPORTGENERATOR_ENABLED
+        [GeneratedDllImport(Libraries.Crypt32, CharSet = CharSet.Unicode, SetLastError = true)]
         internal static unsafe partial bool CryptMsgGetParam(
+#else
+        [DllImport(Libraries.Crypt32, CharSet = CharSet.Unicode, SetLastError = true)]
+        internal static unsafe extern bool CryptMsgGetParam(
+#endif
             SafeCryptMsgHandle hCryptMsg,
             CryptMsgParamType dwParamType,
             int dwIndex,
             byte* pvData,
             ref int pcbData);
-
-        [GeneratedDllImport(Libraries.Crypt32, CharSet = CharSet.Unicode, SetLastError = true)]
-        internal static partial bool CryptMsgGetParam(SafeCryptMsgHandle hCryptMsg,
-            CryptMsgParamType dwParamType,
-            int dwIndex,
-            out int pvData,
-            ref int pcbData);
-#else
-        [DllImport(Libraries.Crypt32, CharSet = CharSet.Unicode, SetLastError = true)]
-        internal static extern bool CryptMsgGetParam(
-            SafeCryptMsgHandle hCryptMsg,
-            CryptMsgParamType dwParamType,
-            int dwIndex,
-            out int pvData,
-            [In, Out] ref int pcbData);
-#endif
 
         [DllImport(Libraries.Crypt32, CharSet = CharSet.Unicode, SetLastError = true)]
         internal static extern bool CryptMsgGetParam(
@@ -40,15 +41,7 @@ internal static partial class Interop
             CryptMsgParamType dwParamType,
             int dwIndex,
             out CryptMsgType pvData,
-            [In, Out] ref int pcbData);
-
-        [DllImport(Libraries.Crypt32, CharSet = CharSet.Unicode, SetLastError = true)]
-        internal static extern bool CryptMsgGetParam(
-            SafeCryptMsgHandle hCryptMsg,
-            CryptMsgParamType dwParamType,
-            int dwIndex,
-            [Out] byte[]? pvData,
-            [In, Out] ref int pcbData);
+            ref int pcbData);
 
         [DllImport(Libraries.Crypt32, CharSet = CharSet.Unicode, SetLastError = true)]
         internal static extern bool CryptMsgGetParam(
@@ -56,6 +49,6 @@ internal static partial class Interop
             CryptMsgParamType dwParamType,
             int dwIndex,
             IntPtr pvData,
-            [In, Out] ref int pcbData);
+            ref int pcbData);
     }
 }
