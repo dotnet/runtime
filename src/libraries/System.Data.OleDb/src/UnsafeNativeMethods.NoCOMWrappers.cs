a77797f4aaf10811fc4d8301cc6d6b9137ed07b1
Aaron Robinson <arobins@microsoft.com>
2021-11-04 12:55:02 -0700

    Remove unused DllImports.

--- src/libraries/System.Data.OleDb/src/UnsafeNativeMethods.NoCOMWrappers.cs
+++ src/libraries/System.Data.OleDb/src/UnsafeNativeMethods.NoCOMWrappers.cs
@@ -11,8 +11,8 @@ namespace System.Data.Common
         // Oleaut32
         //
 
-        [GeneratedDllImport(Interop.Libraries.OleAut32, CharSet = CharSet.Unicode, PreserveSig = true)]
-        internal static partial System.Data.OleDb.OleDbHResult GetErrorInfo(
+        [DllImport(Interop.Libraries.OleAut32, CharSet = CharSet.Unicode, PreserveSig = true)]
+        internal static extern System.Data.OleDb.OleDbHResult GetErrorInfo(
             int dwReserved,
             [MarshalAs(UnmanagedType.Interface)] out UnsafeNativeMethods.IErrorInfo? ppIErrorInfo);
 
