5ff27f0acb8b429bf0a4e14030a5abac9da06b58
Ilona Tomkowicz <itomkowicz@microsoft.com>
2021-11-05 11:57:05 +0100

    Using current namespace as the default place to serach for the resolved class.

--- src/mono/wasm/debugger/BrowserDebugProxy/MemberReferenceResolver.cs
+++ src/mono/wasm/debugger/BrowserDebugProxy/MemberReferenceResolver.cs
@@ -216,6 +216,11 @@ namespace Microsoft.WebAssembly.Diagnostics
                         }
                     }
                 }
+                else if (rootObject == null)
+                {
+                    rootObject = await TryToRunOnLoadedClasses(varName, token);
+                    return rootObject;
+                }
             }
             if (rootObject == null)
                 rootObject = await TryToRunOnLoadedClasses(varName, token);
