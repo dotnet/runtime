8a7e822e448a1f8d70e7ac9a1db048be38fd318e
Bruce Forstall <brucefo@microsoft.com>
2021-11-10 10:13:31 -0800

    Assume Windows; don't use osGroup or `-platform` option to summarize script
    There will be more changes required if we ever run on non-Windows platforms,
    so don't keep these partial measures.

--- src/coreclr/scripts/superpmi_asmdiffs_summarize.py
+++ src/coreclr/scripts/superpmi_asmdiffs_summarize.py
@@ -22,7 +22,6 @@ parser = argparse.ArgumentParser(description="description")
 
 parser.add_argument("-diff_summary_dir", help="Path to diff summary directory")
 parser.add_argument("-arch", help="Architecture")
-parser.add_argument("-platform", help="OS platform")
 
 def setup_args(args):
     """ Setup the args.
@@ -47,21 +46,15 @@ def setup_args(args):
                         lambda unused: True,
                         "Unable to set arch")
 
-    coreclr_args.verify(args,
-                        "platform",
-                        lambda unused: True,
-                        "Unable to set platform")
-
     return coreclr_args
 
 
-def append_diff_file(f, platform, arch, file_name, full_file_path):
+def append_diff_file(f, arch, file_name, full_file_path):
     """ Append a single summary file to the consolidated diff file.
 
     Args:
         f : File we are appending to
-        platform (string): OS platform we ran on
-        arch (string): OS architecture we ran on
+        arch (string): architecture we ran on
         file_name (string): base file name of file to append (not including path components)
         full_file_path (string): full path to file to append
 
@@ -75,11 +68,11 @@ def append_diff_file(f, platform, arch, file_name, full_file_path):
     # What platform is this file summarizing? We parse the filename itself, which is of the form:
     #   superpmi_diff_summary_<platform>_<arch>.md
 
-    diff_platform = "unknown"
+    diff_os = "unknown"
     diff_arch = "unknown"
     match_obj = re.search(r'^superpmi_diff_summary_(.*)_(.*).md', file_name)
     if match_obj is not None:
-        diff_platform = match_obj.group(1)
+        diff_os = match_obj.group(1)
         diff_arch = match_obj.group(2)
 
     with open(full_file_path, "r") as current_superpmi_md:
@@ -106,12 +99,12 @@ def append_diff_file(f, platform, arch, file_name, full_file_path):
 
 Summary file: `{2}`
 
-To reproduce these diffs on {3} {4}:
+To reproduce these diffs on Windows {3}:
 ```
-superpmi.py asmdiffs -target_os {0} -target_arch {1} -arch {4}
+superpmi.py asmdiffs -target_os {0} -target_arch {1} -arch {3}
 ```
 
-""".format(diff_platform, diff_arch, file_name, platform, arch))
+""".format(diff_os, diff_arch, file_name, arch))
 
             # Now write the contents
             f.write(contents)
@@ -137,9 +130,8 @@ def main(main_args):
 
     diff_summary_dir = coreclr_args.diff_summary_dir
     arch = coreclr_args.arch
-    platform = coreclr_args.platform
 
-    # Consolidate all superpmi_diff_summary_*.md in overall_diff_summary_<platform>_<architecture>.md
+    # Consolidate all superpmi_diff_summary_*.md in overall_diff_summary_<os>_<architecture>.md
     # (Don't name it "superpmi_xxx.md" or we might consolidate it into itself.)
     # If there are no summary files found, add a "No diffs found" text to be explicit about that.
     #
@@ -150,19 +142,19 @@ def main(main_args):
 
     any_diffs_found = False
 
-    final_md_path = os.path.join(diff_summary_dir, "overall_diff_summary_{}_{}.md".format(platform, arch))
+    final_md_path = os.path.join(diff_summary_dir, "overall_diff_summary_windows_{}.md".format(arch))
     print("Consolidating final {}".format(final_md_path))
     with open(final_md_path, "a") as f:
 
         f.write("""\
-# ASM diffs generated on {} {}
-""".format(platform, arch))
+# ASM diffs generated on Windows {}
+""".format(arch))
 
         for dirpath, _, files in os.walk(diff_summary_dir):
             for file_name in files:
                 if file_name.startswith("superpmi_") and file_name.endswith(".md"):
                     full_file_path = os.path.join(dirpath, file_name)
-                    if append_diff_file(f, platform, arch, file_name, full_file_path):
+                    if append_diff_file(f, arch, file_name, full_file_path):
                         any_diffs_found = True
 
         if not any_diffs_found:
