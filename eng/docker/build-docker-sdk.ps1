b274ef6711d08aa0af371f72bd09d53ec19f6347
Anton Firszov <antonfir@gmail.com>
2021-11-10 15:37:27 +0000

    actually build on Windows

--- eng/docker/build-docker-sdk.ps1
+++ eng/docker/build-docker-sdk.ps1
@@ -26,12 +26,12 @@ if ($buildWindowsContainers)
   # Due to size concerns, we don't currently do docker builds on windows.
   # Build on the host machine, then simply copy artifacts to the target docker image.
   # This should result in significantly lower build times, for now.
-  # & "$REPO_ROOT_DIR/build.cmd" clr+libs -ci -rc release -c $configuration
+  & "$REPO_ROOT_DIR/build.cmd" clr+libs -ci -rc release -c $configuration
 
-  # if (!$?)
-  # {
-  #   exit $LASTEXITCODE
-  # }
+  if (!$?)
+  {
+    exit $LASTEXITCODE
+  }
 
   $dockerFile="$dockerFilePrefix-daily.windows.Dockerfile"
   
