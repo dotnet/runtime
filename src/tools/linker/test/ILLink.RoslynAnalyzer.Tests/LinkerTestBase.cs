eda6fc1ce4686b8e8e23a4e9f2df2f6918ecf725
Andy Gocke <angocke@microsoft.com>
2021-11-09 18:49:52 -0800

    Move analyzer tests to one test per file
    Moves away from generating Theory cases for each method in the file and
    uses one Fact per test file.
    
    
    Commit migrated from https://github.com/dotnet/linker/commit/9e1cdb623ba2411dc17fb86be415e0dcd3f78b8e

---
+++ src/tools/linker/test/ILLink.RoslynAnalyzer.Tests/LinkerTestBase.cs
@@ -0,0 +1,19 @@
+
+using System.Threading.Tasks;
+
+namespace ILLink.RoslynAnalyzer.Tests
+{
+    public abstract class LinkerTestBase : TestCaseUtils
+    {
+        protected abstract string TestSuiteName { get; }
+
+        private static readonly (string, string)[] MSBuildProperties = UseMSBuildProperties (
+            MSBuildPropertyOptionNames.EnableTrimAnalyzer,
+            MSBuildPropertyOptionNames.EnableSingleFileAnalyzer);
+
+        protected Task RunTest(string testName)
+        {
+            return RunTestFile (TestSuiteName, testName, MSBuildProperties);
+        }
+    }
+}
---
+++ src/tools/linker/test/ILLink.RoslynAnalyzer.Tests/RequiresCapabilityTests.cs
@@ -0,0 +1,41 @@
+
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ILLink.RoslynAnalyzer.Tests
+{
+    public sealed class RequiresCapabilityTests : LinkerTestBase
+    {
+        protected override string TestSuiteName => "RequiresCapability";
+
+        [Fact]
+        public Task RequiresCapability()
+        {
+            return RunTest(nameof(RequiresCapability));
+        }
+
+        [Fact]
+        public Task RequiresCapabilityFromCopiedAssembly ()
+        {
+            return RunTest (nameof(RequiresCapabilityFromCopiedAssembly));
+        }
+
+        [Fact]
+        public Task RequiresCapabilityReflectionAnalysisEnabled ()
+        {
+            return RunTest (nameof(RequiresCapabilityReflectionAnalysisEnabled));
+        }
+
+        [Fact]
+        public Task RequiresInCompilerGeneratedCode ()
+        {
+            return RunTest(nameof(RequiresInCompilerGeneratedCode));
+        }
+
+        [Fact]
+        public Task RequiresOnAttributeCtor ()
+        {
+            return RunTest (nameof(RequiresOnAttributeCtor));
+        }
+    }
+}
