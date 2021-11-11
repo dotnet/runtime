5dde7adb3b8be1e9eab32305d14b7c4f58d018b5
Andy Gocke <angocke@microsoft.com>
2021-11-11 12:14:31 -0800

    Lint
    Commit migrated from https://github.com/dotnet/linker/commit/959a30acdd4622c1e57f48389e97a9ef01aa5963

--- src/tools/illink/test/ILLink.RoslynAnalyzer.Tests/RequiresCapabilityTests.cs
+++ src/tools/illink/test/ILLink.RoslynAnalyzer.Tests/RequiresCapabilityTests.cs
@@ -4,38 +4,38 @@ using Xunit;
 
 namespace ILLink.RoslynAnalyzer.Tests
 {
-    public sealed class RequiresCapabilityTests : LinkerTestBase
-    {
-        protected override string TestSuiteName => "RequiresCapability";
+	public sealed class RequiresCapabilityTests : LinkerTestBase
+	{
+		protected override string TestSuiteName => "RequiresCapability";
 
-        [Fact]
-        public Task RequiresCapability()
-        {
-            return RunTest(nameof(RequiresCapability));
-        }
+		[Fact]
+		public Task RequiresCapability ()
+		{
+			return RunTest (nameof (RequiresCapability));
+		}
 
-        [Fact]
-        public Task RequiresCapabilityFromCopiedAssembly ()
-        {
-            return RunTest (nameof(RequiresCapabilityFromCopiedAssembly));
-        }
+		[Fact]
+		public Task RequiresCapabilityFromCopiedAssembly ()
+		{
+			return RunTest (nameof (RequiresCapabilityFromCopiedAssembly));
+		}
 
-        [Fact]
-        public Task RequiresCapabilityReflectionAnalysisEnabled ()
-        {
-            return RunTest (nameof(RequiresCapabilityReflectionAnalysisEnabled));
-        }
+		[Fact]
+		public Task RequiresCapabilityReflectionAnalysisEnabled ()
+		{
+			return RunTest (nameof (RequiresCapabilityReflectionAnalysisEnabled));
+		}
 
-        [Fact]
-        public Task RequiresInCompilerGeneratedCode ()
-        {
-            return RunTest(nameof(RequiresInCompilerGeneratedCode));
-        }
+		[Fact]
+		public Task RequiresInCompilerGeneratedCode ()
+		{
+			return RunTest (nameof (RequiresInCompilerGeneratedCode));
+		}
 
-        [Fact]
-        public Task RequiresOnAttributeCtor ()
-        {
-            return RunTest (nameof(RequiresOnAttributeCtor));
-        }
-    }
+		[Fact]
+		public Task RequiresOnAttributeCtor ()
+		{
+			return RunTest (nameof (RequiresOnAttributeCtor));
+		}
+	}
 }
