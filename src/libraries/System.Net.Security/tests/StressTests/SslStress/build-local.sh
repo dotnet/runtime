e974c687896be7fefeaf7ce44d315bdac4228bdd
Anton Firszov <antonfir@gmail.com>
2021-11-11 11:14:24 +0000

    build-local.sh for SslStress

---
+++ src/libraries/System.Net.Security/tests/StressTests/SslStress/build-local.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+## This is a helper script for non-containerized local build and test execution.
+## Usage:
+## ./build-local.sh [Configuration]
+
+version=7.0
+repo_root=$(git rev-parse --show-toplevel)
+
+configuration="Release"
+if [ "$1" != "" ]
+then
+    configuration=${1,,}            # Lowercase all characters in $1
+    configuration=${configuration^} # Uppercase first character
+fi
+
+echo "Building solution."
+dotnet build -c $configuration
+
+testhost_root=$repo_root/artifacts/bin/testhost/net$version-Linux-$configuration-x64
+
+runscript=./run-stress-${configuration,,}.sh
+if [ ! -f $runscript ]
+then
+    echo "Generating runscript."
+    echo "$testhost_root/dotnet exec ./bin/$configuration/net$version/SslStress.dll \$@" > $runscript
+    chmod +x $runscript
+fi
+
+echo "To run tests type:"
+echo "$runscript [stress test args]"
