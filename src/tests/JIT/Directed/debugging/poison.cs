9c4ca5fea506e9529941573e919dce4c3697a2af
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2021-11-15 16:35:35 +0100

    Add regression test

--- src/tests/JIT/Directed/debugging/poison.cs
+++ src/tests/JIT/Directed/debugging/poison.cs
@@ -19,6 +19,22 @@ public class Program
         WithoutGCRef poisoned2;
         Unsafe.SkipInit(out poisoned2);
         result &= VerifyPoison(&poisoned2, sizeof(WithoutGCRef));
+        
+        Massive poisoned3;
+        Unsafe.SkipInit(out poisoned3);
+        result &= VerifyPoison(&poisoned3, sizeof(Massive));
+
+        WithoutGCRef poisoned4;
+        Unsafe.SkipInit(out poisoned4);
+        result &= VerifyPoison(&poisoned4, sizeof(WithoutGCRef));
+
+        Massive poisoned5;
+        Unsafe.SkipInit(out poisoned5);
+        result &= VerifyPoison(&poisoned5, sizeof(Massive));
+
+        GCRef zeroed2;
+        Unsafe.SkipInit(out zeroed2);
+        result &= VerifyZero(Unsafe.AsPointer(ref zeroed2), Unsafe.SizeOf<GCRef>());
 
         return result ? 100 : 101;
     }
@@ -52,5 +68,10 @@ public class Program
         public double ADouble;
         public int ANumber;
         public float AFloat;
+    }
+    
+    private unsafe struct Massive
+    {
+        public fixed byte Bytes[0x10008];
     }
 }
