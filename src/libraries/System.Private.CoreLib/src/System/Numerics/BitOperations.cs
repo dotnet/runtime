4b91869527299dd5df3aacfdfbf5f095f52e19c3
Robin Lindner <robin.lindner1@t-online.de>
2021-11-15 21:43:12 +0100

    Usage of WriteUnaligned instead of As
    Co-authored-by: Günther Foidl <gue@korporal.at>

--- src/libraries/System.Private.CoreLib/src/System/Numerics/BitOperations.cs
+++ src/libraries/System.Private.CoreLib/src/System/Numerics/BitOperations.cs
@@ -884,7 +884,7 @@ namespace System.Numerics
             // Software fallback
 
             Span<byte> bytes = stackalloc byte[sizeof(ulong)];
-            Unsafe.As<byte, ulong>(ref bytes[0]) = data;
+            Unsafe.WriteUnaligned(ref MemoryMarshal.GetReference(bytes), data);
 
             foreach (byte b in bytes)
             {
