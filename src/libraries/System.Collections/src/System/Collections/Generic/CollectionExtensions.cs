a72b3f9615c425da5173c9fb08c078b906c36b7f
mrxx99 <mrxx99@googlemail.com>
2021-11-15 17:24:30 +0100

    Added XML-Doc comments

--- src/libraries/System.Collections/src/System/Collections/Generic/CollectionExtensions.cs
+++ src/libraries/System.Collections/src/System/Collections/Generic/CollectionExtensions.cs
@@ -57,11 +57,28 @@ namespace System.Collections.Generic
             return false;
         }
 
+        /// <summary>
+        /// Returns a read-only <see cref="ReadOnlyCollection{T}"/> wrapper
+        /// for the specified list.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the collection.</typeparam>
+        /// <param name="list">The list to wrap.</param>
+        /// <returns>An object that acts as a read-only wrapper around the current <see cref="IList{T}"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="list"/> is null.</exception>
         public static ReadOnlyCollection<T> AsReadOnly<T>(this IList<T> list)
         {
             return new ReadOnlyCollection<T>(list);
         }
 
+        /// <summary>
+        /// Returns a read-only <see cref="ReadOnlyDictionary{TKey, TValue}"/> wrapper
+        /// for the current dictionary.
+        /// </summary>
+        /// <typeparam name="TKey">The type of keys in the dictionary.</typeparam>
+        /// <typeparam name="TValue">The type of values in the dictionary.</typeparam>
+        /// <param name="dictionary">The dictionary to wrap.</param>
+        /// <returns>An object that acts as a read-only wrapper around the current <see cref="IDictionary{TKey, TValue}"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="dictionary"/> is null.</exception>
         public static ReadOnlyDictionary<TKey, TValue> AsReadOnly<TKey, TValue>(this IDictionary<TKey, TValue> dictionary) where TKey : notnull
         {
             return new ReadOnlyDictionary<TKey, TValue>(dictionary);
