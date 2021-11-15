7d49b8291b1ca6fb1b3630ae16b694fae47d3c19
Simon Rozsival <simon@rozsival.com>
2021-11-15 12:53:33 +0100

    Modify test

--- src/tests/Loader/classloader/explicitlayout/misc/nested.cs
+++ src/tests/Loader/classloader/explicitlayout/misc/nested.cs
@@ -4,8 +4,8 @@
 using System;
 using System.Runtime.InteropServices;
 
-[StructLayout(LayoutKind.Explicit, Size = 24)]
-public struct OuterStruct
+[StructLayout(LayoutKind.Explicit, Size = 16)]
+public struct ComplexStruct
 {
     [FieldOffset(0)]
     public object? Object;
@@ -15,12 +15,6 @@ public struct OuterStruct
 
     [FieldOffset(8)]
     public double Double;
-
-    [FieldOffset(8)]
-    public ulong High;
-
-    [FieldOffset(16)]
-    public ulong Low;
 }
 
 [StructLayout(LayoutKind.Explicit, Size = 16)]
@@ -37,11 +31,21 @@ public struct InnerStruct
 }
 
 public class Test_NestedStructsWithExplicitLayout {
+    private ComplexStruct currentCount = default;
+
+    private void IncrementCount()
+    {
+        var x = new ComplexStruct();
+        x.Inner.High = currentCount.Inner.High + 1;
+        currentCount = x;
+    }
+
     public static int Main ()
     {
         try
         {
-            var x = new OuterStruct();
+            var instance = new Test_NestedStructsWithExplicitLayout();
+            instance.IncrementCount();
         }
         catch (TypeLoadException e)
         {
