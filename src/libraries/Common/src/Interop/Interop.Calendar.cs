2c85f41b3d65623b022b5cd3202181ab5286b300
Aaron Robinson <arobins@microsoft.com>
2021-11-16 13:07:54 -0800

    Fix WASM build.

--- src/libraries/Common/src/Interop/Interop.Calendar.cs
+++ src/libraries/Common/src/Interop/Interop.Calendar.cs
@@ -15,8 +15,15 @@ internal static partial class Interop
         [GeneratedDllImport(Libraries.GlobalizationNative, EntryPoint = "GlobalizationNative_GetCalendarInfo", CharSet = CharSet.Unicode)]
         internal static unsafe partial ResultCode GetCalendarInfo(string localeName, CalendarId calendarId, CalendarDataType calendarDataType, char* result, int resultCapacity);
 
+        internal static unsafe bool EnumCalendarInfo(delegate* unmanaged<char*, IntPtr, void> callback, string localeName, CalendarId calendarId, CalendarDataType calendarDataType, IntPtr context)
+        {
+            return EnumCalendarInfo((IntPtr)callback, localeName, calendarId, calendarDataType, context);
+        }
+
         [GeneratedDllImport(Libraries.GlobalizationNative, EntryPoint = "GlobalizationNative_EnumCalendarInfo", CharSet = CharSet.Unicode)]
-        internal static unsafe partial bool EnumCalendarInfo(delegate* unmanaged<char*, IntPtr, void> callback, string localeName, CalendarId calendarId, CalendarDataType calendarDataType, IntPtr context);
+        // We skip the following DllImport because of 'Parsing function pointer types in signatures is not supported.' for some targeted
+        // platforms (for example, WASM build).
+        private static unsafe partial bool EnumCalendarInfo(IntPtr callback, string localeName, CalendarId calendarId, CalendarDataType calendarDataType, IntPtr context);
 
         [GeneratedDllImport(Libraries.GlobalizationNative, EntryPoint = "GlobalizationNative_GetLatestJapaneseEra")]
         internal static partial int GetLatestJapaneseEra();
