e1d6b2bcb564b190e0801fc6986b909f7086ea4a
kronic <kronic@softland.ru>
2021-11-17 10:33:36 +0300

    Use IndentedWriter.Write(char c)

--- src/libraries/System.Private.Xml/src/System/Xml/Serialization/Compilation.cs
+++ src/libraries/System.Private.Xml/src/System/Xml/Serialization/Compilation.cs
@@ -369,7 +369,7 @@ namespace System.Xml.Serialization
 
                     writer.Write("[assembly:");
                     writer.Write(typeof(XmlSerializerVersionAttribute).FullName);
-                    writer.Write("(");
+                    writer.Write('(');
                     writer.Write("ParentAssemblyId=");
                     ReflectionAwareCodeGen.WriteQuotedCSharpString(writer, GenerateAssemblyId(types[0]!));
                     writer.Write(", Version=");
