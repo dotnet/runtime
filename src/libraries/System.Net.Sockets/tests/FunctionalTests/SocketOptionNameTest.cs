0e9ce96f846e579e457e0b2d6ddf7bb1b2991415
Simon Rozsival <simon@rozsival.com>
2021-11-22 18:48:14 +0100

    Revert "[Mono] Skip flaky android tests (#61460)" (#61771)
    This reverts commit 8ad84e88d86fb7845187ae0aa17be48ed1a46c97.

--- src/libraries/System.Net.Sockets/tests/FunctionalTests/SocketOptionNameTest.cs
+++ src/libraries/System.Net.Sockets/tests/FunctionalTests/SocketOptionNameTest.cs
@@ -51,7 +51,6 @@ namespace System.Net.Sockets.Tests
         }
 
         [Fact]
-        [ActiveIssue("https://github.com/dotnet/runtime/issues/61343", TestPlatforms.Android)]
         public void MulticastOption_CreateSocketSetGetOption_GroupAndInterfaceIndex_SetSucceeds_GetThrows()
         {
             int interfaceIndex = 0;
@@ -66,7 +65,6 @@ namespace System.Net.Sockets.Tests
         }
 
         [ConditionalFact(typeof(PlatformDetection), nameof(PlatformDetection.IsNotWindowsNanoNorServerCore))] // Skip on Nano: https://github.com/dotnet/runtime/issues/26286
-        [ActiveIssue("https://github.com/dotnet/runtime/issues/61343", TestPlatforms.Android)]
         public async Task MulticastInterface_Set_AnyInterface_Succeeds()
         {
             // On all platforms, index 0 means "any interface"
