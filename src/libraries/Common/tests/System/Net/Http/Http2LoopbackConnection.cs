fefa416942c18625af4a9f4ed20eb0470aa14a5f
Anudeep Gunukula <55506841+AnudeepGunukula@users.noreply.github.com>
2021-11-23 16:23:46 +0530

    Update src/libraries/Common/tests/System/Net/Http/Http2LoopbackConnection.cs
    Co-authored-by: Karel Zikmund <karelz@microsoft.com>

--- src/libraries/Common/tests/System/Net/Http/Http2LoopbackConnection.cs
+++ src/libraries/Common/tests/System/Net/Http/Http2LoopbackConnection.cs
@@ -921,7 +921,7 @@ namespace System.Net.Test.Common
 
         public override Task SendResponseHeadersAsync(HttpStatusCode statusCode = HttpStatusCode.OK, IList<HttpHeaderData> headers = null)
         {
-            int streamId =  _lastStreamId;
+            int streamId = _lastStreamId;
             return SendResponseHeadersAsync(streamId, endStream: false, statusCode, isTrailingHeader: false, endHeaders: true, headers);
         }
 
