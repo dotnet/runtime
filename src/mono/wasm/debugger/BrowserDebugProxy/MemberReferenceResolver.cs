6a1b7ad0f28e5d3e02637d87bcdeac8fc6ddca35
Ilona Tomkowicz <itomkowicz@microsoft.com>
2021-11-24 09:27:21 +0100

    Undo nested static changes that will be sumbitted in another PR.

--- src/mono/wasm/debugger/BrowserDebugProxy/MemberReferenceResolver.cs
+++ src/mono/wasm/debugger/BrowserDebugProxy/MemberReferenceResolver.cs
@@ -103,8 +103,7 @@ namespace Microsoft.WebAssembly.Diagnostics
                     if (memberObject != null && i < parts.Length - 1)
                         remaining = string.Join('.', parts[(i + 1)..]);
 
-                    if (memberObject != null || remaining != null)
-                        return (memberObject, remaining);
+                    return (memberObject, remaining);
                 }
 
                 if (!string.IsNullOrEmpty(methodInfo.TypeInfo.Namespace))
