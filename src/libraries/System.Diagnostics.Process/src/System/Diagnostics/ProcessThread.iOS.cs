8997e8600fa2bceb0f122f7a5d2d90a1268cfcc1
Maxim Lipnin <v-maxlip@microsoft.com>
2021-11-24 14:36:32 +0300

    Use separate partials for iOS&tvOS instead of UnknowUnix in System.Diagnostics.Process (#61871)

--- src/libraries/System.Diagnostics.Process/src/System/Diagnostics/ProcessThread.iOS.cs
+++ src/libraries/System.Diagnostics.Process/src/System/Diagnostics/ProcessThread.iOS.cs
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Runtime.Versioning;
+
 namespace System.Diagnostics
 {
     public partial class ProcessThread
@@ -12,38 +14,43 @@ namespace System.Diagnostics
         /// </summary>
         private ThreadPriorityLevel PriorityLevelCore
         {
-            get { throw new PlatformNotSupportedException(); }  // Not available on POSIX
-            set { throw new PlatformNotSupportedException(); }  // Not available on POSIX
+            get { throw new PlatformNotSupportedException(); }
+            set { throw new PlatformNotSupportedException(); }
         }
 
         /// <summary>
         /// Returns the amount of time the thread has spent running code inside the operating
         /// system core.
         /// </summary>
+        [UnsupportedOSPlatform("ios")]
+        [UnsupportedOSPlatform("tvos")]
         public TimeSpan PrivilegedProcessorTime
         {
-            get { throw new PlatformNotSupportedException(); }  // Not available on POSIX
+            get { throw new PlatformNotSupportedException(); }
         }
 
-        private DateTime GetStartTime() => throw new PlatformNotSupportedException(); // Not available on POSIX
-
+        private DateTime GetStartTime() => throw new PlatformNotSupportedException();
         /// <summary>
         /// Returns the amount of time the associated thread has spent utilizing the CPU.
         /// It is the sum of the System.Diagnostics.ProcessThread.UserProcessorTime and
         /// System.Diagnostics.ProcessThread.PrivilegedProcessorTime.
         /// </summary>
+        [UnsupportedOSPlatform("ios")]
+        [UnsupportedOSPlatform("tvos")]
         public TimeSpan TotalProcessorTime
         {
-            get { throw new PlatformNotSupportedException(); }  // Not available on POSIX
+            get { throw new PlatformNotSupportedException(); }
         }
 
         /// <summary>
         /// Returns the amount of time the associated thread has spent running code
         /// inside the application (not the operating system core).
         /// </summary>
+        [UnsupportedOSPlatform("ios")]
+        [UnsupportedOSPlatform("tvos")]
         public TimeSpan UserProcessorTime
         {
-            get { throw new PlatformNotSupportedException(); }  // Not available on POSIX
+            get { throw new PlatformNotSupportedException(); }
         }
     }
 }
