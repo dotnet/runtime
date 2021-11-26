42634daa5af7f22816466917f19576afbd8b2e39
pavelsavara <pavel.savara@gmail.com>
2021-11-26 11:43:35 +0100

    simplify TS Sample

--- src/mono/sample/wasm/console-ts/main.ts
+++ src/mono/sample/wasm/console-ts/main.ts
@@ -2,8 +2,9 @@ import { createRequire } from 'module';
 import createDotnetRuntime from 'dotnet'
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import { exit } from 'process';
 
-const { MONO, BINDING, INTERNAL, Module, RuntimeBuildInfo } = await createDotnetRuntime(() => ({
+const { BINDING } = await createDotnetRuntime(() => ({
     imports: {
         require: createRequire(import.meta.url)
     },
@@ -16,15 +17,9 @@ const { MONO, BINDING, INTERNAL, Module, RuntimeBuildInfo } = await createDotnet
 }));
 
 try {
-
-    const main_assembly_name = "Wasm.Console.TypeScript.Sample.dll";
-    const app_args = process.argv.slice(4);
-    INTERNAL.mono_wasm_set_main_args(main_assembly_name, app_args);
-
-    // Automatic signature isn't working correctly
-    const result = await BINDING.call_assembly_entry_point(main_assembly_name, [app_args], "m");
-
-    console.log("WASM EXIT " + result);
+    const app_args = process.argv.slice(2);
+    const result = await BINDING.call_assembly_entry_point("Wasm.Console.TypeScript.Sample.dll", [app_args], "m");
+    exit(result)
 } catch (error) {
     console.log("WASM ERROR " + error);
 }
