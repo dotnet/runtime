d3b1688ec2a06c8dbeaa58f2d05ed166b636c23b
pavelsavara <pavel.savara@gmail.com>
2021-11-26 13:46:59 +0100

    flat sample

--- src/mono/sample/wasm/browser-es6/main.js
+++ src/mono/sample/wasm/browser-es6/main.js
@@ -8,20 +8,14 @@ const { MONO, BINDING, Module, RuntimeBuildInfo } = await createDotnetRuntime((a
     },
 }));
 
-const App = {
-    init: function () {
-        const testMeaning = BINDING.bind_static_method("[Wasm.Browser.ES6.Sample] Sample.Test:TestMeaning");
-        const ret = testMeaning();
-        document.getElementById("out").innerHTML = `${ret} as computed on dotnet ver ${RuntimeBuildInfo.ProductVersion}`;
-
-        console.debug(`ret: ${ret}`);
-        let exit_code = ret == 42 ? 0 : 1;
-        wasm_exit(exit_code);
-    },
-};
-
-App.init();
-
 function wasm_exit(exit_code) {
     console.log(`WASM EXIT ${exit_code}`);
 }
+
+const testMeaning = BINDING.bind_static_method("[Wasm.Browser.ES6.Sample] Sample.Test:TestMeaning");
+const ret = testMeaning();
+document.getElementById("out").innerHTML = `${ret} as computed on dotnet ver ${RuntimeBuildInfo.ProductVersion}`;
+
+console.debug(`ret: ${ret}`);
+let exit_code = ret == 42 ? 0 : 1;
+wasm_exit(exit_code);
