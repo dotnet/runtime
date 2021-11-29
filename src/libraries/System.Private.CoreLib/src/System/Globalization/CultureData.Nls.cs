8fb965d522f786ddb2319fe6c7c3a00585eb6c7b
Tarek Mahmoud Sayed <tarekms@microsoft.com>
2021-11-29 14:26:56 -0800

    Update src/libraries/System.Private.CoreLib/src/System/Globalization/CultureData.Nls.cs
    Co-authored-by: Adeel Mujahid <3840695+am11@users.noreply.github.com>

--- src/libraries/System.Private.CoreLib/src/System/Globalization/CultureData.Nls.cs
+++ src/libraries/System.Private.CoreLib/src/System/Globalization/CultureData.Nls.cs
@@ -74,7 +74,7 @@ namespace System.Globalization
 
             // Ask OS for data, note that we presume it returns success, so we have to know that
             // sWindowsName is valid before calling.
-            Debug.Assert(_sRealName != null, "[CultureData.DoGetLocaleInfoInt] Expected _sRealName to be populated by already");
+            Debug.Assert(_sRealName != null, "[CultureData.DoGetLocaleInfoInt] Expected _sRealName to be populated already");
             return GetLocaleInfoExInt(_sRealName, lctype);
         }
 
