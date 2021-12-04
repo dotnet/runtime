93a4caa74498d3163f02dcbdea9e65a409df1ba4
Carl Verret <carlverret76@gmail.com>
2021-12-04 16:29:59 -0500

    Update src/libraries/System.Private.CoreLib/src/System/Number.NumberToFloatingPointBits.cs
    Co-authored-by: Günther Foidl <gue@korporal.at>

--- src/libraries/System.Private.CoreLib/src/System/Number.NumberToFloatingPointBits.cs
+++ src/libraries/System.Private.CoreLib/src/System/Number.NumberToFloatingPointBits.cs
@@ -1007,7 +1007,7 @@ namespace System
             uint res = 0;
 
             // parse batches of 8 digits with SWAR
-            while (end - p >= 8)
+            while (p <= end - 8)
             {
                 res = (res * 100000000) + ParseEightDigitsUnrolled(p);
                 p += 8;
