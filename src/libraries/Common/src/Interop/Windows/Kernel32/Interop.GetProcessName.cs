67732e46f12eadad2368a3bc17ebbae0ac604398
Adam Sitnik <adam.sitnik@gmail.com>
2021-12-07 13:27:55 +0100

    handle ERROR_INSUFFICIENT_BUFFER

--- src/libraries/Common/src/Interop/Windows/Kernel32/Interop.GetProcessName.cs
+++ src/libraries/Common/src/Interop/Windows/Kernel32/Interop.GetProcessName.cs
@@ -2,40 +2,72 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
+using System.Buffers;
 using System.Runtime.InteropServices;
-using System.Text;
 using Microsoft.Win32.SafeHandles;
 
 internal static partial class Interop
 {
     internal static partial class Kernel32
     {
-        [DllImport(Libraries.Kernel32, CharSet = CharSet.Unicode, EntryPoint = "QueryFullProcessImageNameW", ExactSpelling = true)]
-        private static extern bool QueryFullProcessImageName(
+        [DllImport(Libraries.Kernel32, CharSet = CharSet.Unicode, EntryPoint = "QueryFullProcessImageNameW", ExactSpelling = true, SetLastError = true)]
+        private static unsafe extern bool QueryFullProcessImageName(
             SafeHandle hProcess,
             uint dwFlags,
-            ref char lpBuffer,
+            char* lpBuffer,
             ref uint lpdwSize);
 
-        private const int MAX_PROCESSNAME_LENGTH = 1024;
-
-        internal static string? GetProcessName(uint processId)
+        internal static unsafe string? GetProcessName(uint processId)
         {
-            using (SafeProcessHandle h = Interop.Kernel32.OpenProcess(Interop.Advapi32.ProcessOptions.PROCESS_QUERY_LIMITED_INFORMATION, false, (int)processId))
+            using (SafeProcessHandle handle = OpenProcess(Advapi32.ProcessOptions.PROCESS_QUERY_LIMITED_INFORMATION, false, (int)processId))
             {
-                if (h.IsInvalid)
+                if (handle.IsInvalid) // OpenProcess can fail
                 {
                     return null;
                 }
-                uint length = (uint)buffer.Length;
 
-                bool queried = QueryFullProcessImageName(
-                    h,
-                    0,
-                    ref MemoryMarshal.GetReference(buffer),
-                    ref length);
+                const int StartLength =
+#if DEBUG
+                    1; // in debug, validate ArrayPool growth
+#else
+                    Interop.Kernel32.MAX_PATH;
+#endif
 
-                return queried ? buffer.Slice(0, (int)length).ToString() : null;
+                Span<char> buffer = stackalloc char[StartLength + 1];
+                char[]? rentedArray = null;
+
+                try
+                {
+                    while (true)
+                    {
+                        uint length = (uint)buffer.Length;
+                        fixed (char* pinnedBuffer = &MemoryMarshal.GetReference(buffer))
+                        {
+                            if (QueryFullProcessImageName(handle, 0, pinnedBuffer, ref length))
+                            {
+                                return buffer.Slice(0, (int)length).ToString();
+                            }
+                            else if (Marshal.GetLastWin32Error() != Errors.ERROR_INSUFFICIENT_BUFFER)
+                            {
+                                return null;
+                            }
+                        }
+
+                        char[]? toReturn = rentedArray;
+                        buffer = rentedArray = ArrayPool<char>.Shared.Rent(buffer.Length * 2);
+                        if (toReturn is not null)
+                        {
+                            ArrayPool<char>.Shared.Return(toReturn);
+                        }
+                    }
+                }
+                finally
+                {
+                    if (rentedArray is not null)
+                    {
+                        ArrayPool<char>.Shared.Return(rentedArray);
+                    }
+                }
             }
         }
     }
