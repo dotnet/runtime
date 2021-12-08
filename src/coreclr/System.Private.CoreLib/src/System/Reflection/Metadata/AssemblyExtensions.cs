66ecea131fe818ef1a823a9a3a7788acb5de749b
Buyaa Namnan <buyankhishig.namnan@microsoft.com>
2021-12-07 17:58:08 -0800

    Apply suggestions from code review
    Co-authored-by: Carlos Sanchez <1175054+carlossanlop@users.noreply.github.com>

--- src/coreclr/System.Private.CoreLib/src/System/Reflection/Metadata/AssemblyExtensions.cs
+++ src/coreclr/System.Private.CoreLib/src/System/Reflection/Metadata/AssemblyExtensions.cs
@@ -15,17 +15,17 @@ namespace System.Reflection.Metadata
         /// <summary>
         /// Retrieves the metadata section of the assembly, for use with <see cref="T:System.Reflection.Metadata.MetadataReader" />.
         /// </summary>
-        /// <param name="assembly">The assembly for which retrieve metadata.</param>
+        /// <param name="assembly">The assembly from which to retrieve the metadata.</param>
         /// <param name="blob">When this method returns, contains the pointer to the metadata section blob.</param>
         /// <param name="length">When this method returns, contains the length of the metadata section blob.</param>
         /// <returns>
-        /// <see langword="true" /> if the metadata retrieved successfully; <see langword="false" /> upon failure.
-        /// Metadata might not be available for some assemblies, such as <see cref="System.Reflection.Emit.AssemblyBuilder" />, AOT images, etc.
+        /// <see langword="true" /> if the metadata is retrieved successfully; <see langword="false" /> upon failure.
+        /// The metadata might not be available for some assemblies, such as <see cref="System.Reflection.Emit.AssemblyBuilder" />, AOT images, etc.
         /// </returns>
         /// <remarks>
         /// <para>Callers should not write to the metadata blob.</para>
-        /// <para>The metadata blob pointer will remain valid as long as the assembly is alive.
-        ///   The caller is responsible for keeping the assembly object alive while accessing the metadata blob.</para>
+        /// <para>The metadata blob pointer will remain valid as long as the assembly is alive.</para>
+        /// <para>The caller is responsible for keeping the assembly object alive while accessing the metadata blob.</para>
         /// </remarks>
         [CLSCompliant(false)] // out byte* blob
         public static unsafe bool TryGetRawMetadata(this Assembly assembly, out byte* blob, out int length)
