f693c9a17284c144f650029396bcd1d05aa9963f
pavelsavara <pavel.savara@gmail.com>
2021-12-08 12:36:06 +0100

    cleanup

--- src/libraries/System.Private.Runtime.InteropServices.JavaScript/tests/simple.js
+++ src/libraries/System.Private.Runtime.InteropServices.JavaScript/tests/simple.js
@@ -1,34 +1,7 @@
 const { exit } = require("process");
 const createDotnetRuntime = require("./dotnet.js");
 
-async function main() {
-    try {
-        const { BINDING, RuntimeBuildInfo } = await createDotnetRuntime(() => ({
-            imports: {
-                require
-            },
-            disableDotnet6Compatibility: true,
-            configSrc: "./mono-config.json",
-            onAbort: (err) => {
-                console.log(`WASM ERROR ${err}`);
-                exit(-1)
-            },
-        }));
-
-        const exit_code = await BINDING.call_assembly_entry_point("System.Private.Runtime.InteropServices.JavaScript.Tests.dll", [""], "m");
-        console.log("WASM EXIT " + exit_code);
-        exit(exit_code);
-    } catch (error) {
-        console.log("WASM ERROR " + error);
-        console.log(error.stack);
-        throw error;
-    }
-};
-
-main();
-
 class TimersHelper {
-
     install() {
         const measuredCallbackName = "mono_wasm_set_timeout_exec";
         globalThis.registerCounter = 0;
@@ -70,4 +43,31 @@ class TimersHelper {
         globalThis.setTimeout = globalThis.originalSetTimeout;
     }
 }
+
 globalThis.timersHelper = new TimersHelper();
+
+async function main() {
+    try {
+        const { BINDING } = await createDotnetRuntime(() => ({
+            imports: {
+                require
+            },
+            disableDotnet6Compatibility: true,
+            configSrc: "./mono-config.json",
+            onAbort: (err) => {
+                console.log(`WASM ERROR ${err}`);
+                exit(-1)
+            },
+        }));
+
+        const exit_code = await BINDING.call_assembly_entry_point("System.Private.Runtime.InteropServices.JavaScript.Tests.dll", [""], "m");
+        console.log("WASM EXIT " + exit_code);
+        exit(exit_code);
+    } catch (error) {
+        console.log("WASM ERROR " + error);
+        console.log(error.stack);
+        throw error;
+    }
+};
+
+main();
