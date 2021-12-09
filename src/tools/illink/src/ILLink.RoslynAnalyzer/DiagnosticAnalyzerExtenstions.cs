79a3dd3f13ed12045d46907986ebc3a4dd843db0
Jackson Schuster <jschuster@microsoft.com>
2021-12-09 12:16:29 -0600

    Move IsMemberInRequiresScope logic to extension method for DiagnosticAnalyzer
    Commit migrated from https://github.com/dotnet/linker/commit/35e9b3b3aad4d5c252b653bb864ef3ecb8653c73

---
+++ src/tools/illink/src/ILLink.RoslynAnalyzer/DiagnosticAnalyzerExtenstions.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace ILLink.RoslynAnalyzer
+{
+	static class RoslynDiagnosticAnalyzerExtenstions 
+	{
+		public static bool IsMemberInRequiresScope(this DiagnosticAnalyzer _, ISymbol member, string requiresAttribute)
+		{
+			if (member is ISymbol containingSymbol) {
+				if (containingSymbol.HasAttribute (requiresAttribute)
+					|| (containingSymbol is not ITypeSymbol &&
+						 containingSymbol.ContainingType.HasAttribute (requiresAttribute))) {
+					return true;
+				}
+			}
+			if (member is IMethodSymbol { AssociatedSymbol: { } associated } && associated.HasAttribute (requiresAttribute))
+				return true;
+
+			return false;
+		}
+	}
+	
+}
