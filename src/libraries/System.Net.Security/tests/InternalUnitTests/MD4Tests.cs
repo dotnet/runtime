187f80afa4c93e235f4825259a3605a04968fd48
Maxim Lipnin <v-maxlip@microsoft.com>
2021-12-10 18:03:24 +0300

    Extract the MD4 tests to a separate test suite to not depend on System.Net.Security test suites disabled for CI due to timeout issue

--- src/libraries/System.Net.Security/tests/InternalUnitTests/MD4Tests.cs
+++ src/libraries/System.Net.Security/tests/InternalUnitTests/MD4Tests.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.IO;
-using System.IO.Tests;
 using System.Net.Security;
 using System.Text;
 using System.Threading.Tasks;
