7389f9b84ff04139898759e0761fa02376ad4ad3
ntr <ntr@ihfood.dk>
2021-12-14 13:02:01 +0100

    paren nit

--- src/libraries/System.Private.CoreLib/src/System/String.Manipulation.cs
+++ src/libraries/System.Private.CoreLib/src/System/String.Manipulation.cs
@@ -1929,7 +1929,7 @@ namespace System
             Buffer.Memmove(
                 elementCount: (uint)result.Length, // derefing Length now allows JIT to prove 'result' not null below
                 destination: ref result._firstChar,
-                source: ref Unsafe.Add(ref _firstChar, (nint)(uint)startIndex) /* force zero-extension */);
+                source: ref Unsafe.Add(ref _firstChar, (nint)(uint)startIndex /* force zero-extension */));
 
             return result;
         }
