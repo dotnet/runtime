8ccd6e037875d597ceb1576e5d994a86f2789eb2
Andrii Kurdiumov <kant2002@gmail.com>
2021-12-14 16:38:05 +0600

    Being too smart for my own good.

--- src/libraries/System.Drawing.Common/src/Interop/Windows/Interop.Comdlg32.cs
+++ src/libraries/System.Drawing.Common/src/Interop/Windows/Interop.Comdlg32.cs
@@ -10,10 +10,10 @@ internal static partial class Interop
     {
 #pragma warning disable DLLIMPORTGENANALYZER015 // Use 'GeneratedDllImportAttribute' instead of 'DllImportAttribute' to generate P/Invoke marshalling code at compile time
         // TODO: [DllImportGenerator] Switch to use GeneratedDllImport once we support non-blittable structs.
-        [DllImport(Libraries.Comdlg32, SetLastError = true)]
+        [DllImport(Libraries.Comdlg32, CharSet = CharSet.Auto, SetLastError = true)]
         internal static extern bool PrintDlg(ref PRINTDLG lppd);
 
-        [DllImport(Libraries.Comdlg32, SetLastError = true)]
+        [DllImport(Libraries.Comdlg32, CharSet = CharSet.Auto, SetLastError = true)]
         internal static extern bool PrintDlg(ref PRINTDLGX86 lppd);
 #pragma warning restore DLLIMPORTGENANALYZER015
 
