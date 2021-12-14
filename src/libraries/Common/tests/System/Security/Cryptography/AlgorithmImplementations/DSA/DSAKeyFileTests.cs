36632c13e21f38486e379e2ef6eeb48db1731719
Maxim Lipnin <v-maxlip@microsoft.com>
2021-12-14 13:16:29 +0300

    Disable more tests

--- src/libraries/Common/tests/System/Security/Cryptography/AlgorithmImplementations/DSA/DSAKeyFileTests.cs
+++ src/libraries/Common/tests/System/Security/Cryptography/AlgorithmImplementations/DSA/DSAKeyFileTests.cs
@@ -9,6 +9,7 @@ using Xunit;
 namespace System.Security.Cryptography.Dsa.Tests
 {
     [SkipOnPlatform(TestPlatforms.Browser | TestPlatforms.iOS | TestPlatforms.tvOS | TestPlatforms.MacCatalyst, "Not supported on Browser/iOS/tvOS/MacCatalyst")]
+    [ActiveIssue("TODO - create GH issue, x86", TestPlatforms.Android)]
     public static class DSAKeyFileTests
     {
         public static bool SupportsFips186_3 => DSAFactory.SupportsFips186_3;
