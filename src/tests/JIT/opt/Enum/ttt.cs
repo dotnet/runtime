453114b1ee15670e0deedb8893a8c401af48909a
EgorBo <egorbo@gmail.com>
2021-12-16 14:46:33 +0300

    Add test

---
+++ src/tests/JIT/opt/Enum/ttt.cs
@@ -0,0 +1,44 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+struct MyStruct
+{
+    int x;
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    int IncrementAndReturn()
+    {
+        Volatile.Write(ref x, Volatile.Read(ref x) + 1);
+        Volatile.Write(ref x, Volatile.Read(ref x) + 1);
+        Volatile.Write(ref x, Volatile.Read(ref x) + 1);
+        Volatile.Write(ref x, Volatile.Read(ref x) + 1);
+        Volatile.Write(ref x, Volatile.Read(ref x) + 1);
+        return x;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static void Test()
+    {
+        for (int i = 0; i < 100_000_000; i++)
+        {
+            if (new MyStruct().IncrementAndReturn() != 5)
+                throw new InvalidOperationException("oops");
+        }
+    }
+}
+
+class Program
+{
+    static int Main()
+    {
+        Console.WriteLine("Running...");
+        Parallel.For(0, 500, _ => MyStruct.Test());
+        Console.WriteLine("Done");
+        return 100;
+    }
+}
