93d3ce16167c03131cdeff93b25804f9e2b2d723
pavelsavara <pavel.savara@gmail.com>
2021-12-17 13:49:09 +0100

    fix

--- eng/testing/WasmRunnerTemplate.cmd
+++ eng/testing/WasmRunnerTemplate.cmd
@@ -35,17 +35,17 @@ if [%XHARNESS_COMMAND%] == [] (
 if [%XHARNESS_COMMAND%] == [test] (
     if [%JS_ENGINE%] == [] (
         if [%SCENARIO%] == [WasmTestOnNodeJs] (
-            set "JS_ENGINE=--engine=NodeJS"
+            set "JS_ENGINE=--engine^=NodeJS"
         ) else (
-            set "JS_ENGINE=--engine=V8"
+            set "JS_ENGINE=--engine^=V8"
         )
 
         if [%MAIN_JS%] == [] (
-            set "MAIN_JS=--js-file=test-main.js"
+            set "MAIN_JS=--js-file^=test-main.js"
         )
 
         if [%JS_ENGINE_ARGS%] == [] (
-            set "JS_ENGINE_ARGS=--engine-arg=--stack-trace-limit=1000"
+            set "JS_ENGINE_ARGS=--engine-arg^=--stack-trace-limit=1000"
         )
     )
 )
