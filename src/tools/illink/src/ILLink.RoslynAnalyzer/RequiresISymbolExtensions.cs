fb8832e06286bab09ab69a113aa10d73077895e7
Tlakaelel Axayakatl Ceja <tlakaelel.ceja@microsoft.com>
2021-12-16 16:44:09 -0800

    Cherry pick changes from main to feature/DamAnalyzer (dotnet/linker#2457)
    * Add TargetHasRequiresAttribute to RequiresISymbolExtensions file (dotnet/linker#2445)
    
    * Add TargetHasRequiresAttribute to RequiresISymbolExtensions file
    * Add a RequiresUnreferencedCodeUtils file so other analyzers dont have to pass the RUC name everytime they use the RequiresISymbolExtensions
    * Refactor the RequiresAnalyzerBase to not have to verify the arguments while getting the attribute since the specific details on how to validate the attribute arguments are specific of the attribute implementation
    * Change COMAnalyzer to use the RequiresUnreferencedCodeUtils instead of the RequiresISymbolExtensions
    
    * After separating the argument verification from getting the attribute the method TryGetRequiresAttribute is just TryGetAttribute, so deleting the method to simplify stuff (dotnet/linker#2448)
    
    * Remove COM ExpectedWarnings
    
    Commit migrated from https://github.com/dotnet/linker/commit/4f19b0431b814f9fd8121fed8a397e774b2744c0

---
+++ src/tools/illink/src/ILLink.RoslynAnalyzer/RequiresISymbolExtensions.cs
@@ -0,0 +1,68 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.CodeAnalysis;
+
+namespace ILLink.RoslynAnalyzer
+{
+	public static class RequiresISymbolExtensions
+	{
+		// TODO: Consider sharing with linker DoesMethodRequireUnreferencedCode method
+		/// <summary>
+		/// True if the target of a call is considered to be annotated with the Requires... attribute
+		/// </summary>
+		public static bool TargetHasRequiresAttribute (this ISymbol member, string requiresAttribute, [NotNullWhen (returnValue: true)] out AttributeData? requiresAttributeData)
+		{
+			requiresAttributeData = null;
+			if (member.IsStaticConstructor ())
+				return false;
+
+			if (member.TryGetAttribute (requiresAttribute, out requiresAttributeData))
+				return true;
+
+			// Also check the containing type
+			if (member.IsStatic || member.IsConstructor ())
+				return member.ContainingType.TryGetAttribute (requiresAttribute, out requiresAttributeData);
+
+			return false;
+		}
+
+		// TODO: Consider sharing with linker IsMethodInRequiresUnreferencedCodeScope method
+		/// <summary>
+		/// True if the source of a call is considered to be annotated with the Requires... attribute
+		/// </summary>
+		public static bool IsInRequiresScope (this ISymbol member, string requiresAttribute)
+		{
+			return member.IsInRequiresScope (requiresAttribute, true);
+		}
+
+		/// <summary>
+		/// True if member of a call is considered to be annotated with the Requires... attribute.
+		/// Doesn't check the associated symbol for overrides and virtual methods because the analyzer should warn on mismatched between the property AND the accessors
+		/// </summary>
+		/// <param name="containingSymbol">
+		///	Symbol that is either an overriding member or an overriden/virtual member
+		/// </param>
+		public static bool IsOverrideInRequiresScope (this ISymbol member, string requiresAttribute)
+		{
+			return member.IsInRequiresScope (requiresAttribute, false);
+		}
+
+		private static bool IsInRequiresScope (this ISymbol member, string requiresAttribute, bool checkAssociatedSymbol)
+		{
+			if (member is ISymbol containingSymbol) {
+				if (containingSymbol.HasAttribute (requiresAttribute)
+					|| (containingSymbol is not ITypeSymbol &&
+						 containingSymbol.ContainingType.HasAttribute (requiresAttribute))) {
+					return true;
+				}
+			}
+			// Only check associated symbol if not override or virtual method
+			if (checkAssociatedSymbol && member is IMethodSymbol { AssociatedSymbol: { } associated } && associated.HasAttribute (requiresAttribute))
+				return true;
+
+			return false;
+		}
+	}
+}
