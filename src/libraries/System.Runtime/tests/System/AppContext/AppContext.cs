4ccaeca55bcd347c80db7f14fd21cd1bafde1a84
Robin Lindner <robin.lindner1@t-online.de>
2021-12-19 21:39:57 +0100

    Implement test for Set/GetData

---
+++ src/libraries/System.Runtime/tests/System/AppContext/AppContext.cs
@@ -0,0 +1,29 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Xunit;
+
+namespace System.Tests
+{
+    public partial class AppContextTests
+    {
+        [Theory]
+        [InlineData("AppContext_Case1", 123)]
+        [InlineData("AppContext_Case2", "")]
+        [InlineData("AppContext_Case3", null)]
+        public void AppContext_GetSetDataTest(string dataKey, object value)
+        {
+            // Set data
+            AppContext.SetData(dataKey, value);
+
+            // Get previously set data
+            object actual = AppContext.GetData(dataKey);
+
+            // Validate data equality
+            Assert.Equal(value, actual);
+
+            // Validate instance equality
+            Assert.Same(value, actual);
+        }
+    }
+}
