ceb498aaca77ce28a095040e71ee180ee712e85b
Robin Lindner <robin.lindner1@t-online.de>
2021-12-19 19:32:27 +0100

    Expose AppContext.SetData in ref assembly

--- src/libraries/System.Runtime/ref/System.Runtime.cs
+++ src/libraries/System.Runtime/ref/System.Runtime.cs
@@ -112,6 +112,7 @@ namespace System
         public static string BaseDirectory { get { throw null; } }
         public static string? TargetFrameworkName { get { throw null; } }
         public static object? GetData(string name) { throw null; }
+        public static void SetData(string name, object? data) { }
         public static void SetSwitch(string switchName, bool isEnabled) { }
         public static bool TryGetSwitch(string switchName, out bool isEnabled) { throw null; }
     }
