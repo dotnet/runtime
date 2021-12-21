e4a3f5734dc096a1702fd572539d796f6e69a7bd
Aleksey Kliger <alklig@microsoft.com>
2021-12-21 16:37:51 -0500

    XXX no merge - change the sample to add a static lambda

--- src/mono/sample/mbr/console/TestClass_v1.cs
+++ src/mono/sample/mbr/console/TestClass_v1.cs
@@ -4,11 +4,18 @@ using System.Runtime.CompilerServices;
 public class TestClass {
 	[MethodImpl(MethodImplOptions.NoInlining)]
 	public static string TargetMethod () {
+#if false
 		var o = new Inner();
 		return o.GetIt();
+#endif
+		return NewFunc (() => "NEW STRING");
         }
 
+	public static string NewFunc(Func<string> f) => f ();
+
+#if false
 	public class Inner {
 		public string GetIt() => "NEW STRING";
 	}
+#endif
 }
