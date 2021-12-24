dotnet/runtime

commit 9ddd58a58d14a7bec5ed6eb777c6703c48aca15d

Various WASM AOT changes (#62997)
* [mono] Add a mono_dummy_jit_icall_val () icall.

This can be used to trace values from AOT code during runtime
debugging.

* [mono] Avoid adding a try clause to vector alloc wrappers.

Previously, a try-catch clause was added to the vector Alloc
wrappers to convert the OverflowException thrown by the .ovf
opcodes to an OutOfMemoryException. This could cause EH problems on wasm
since on wasm no AOTed code should contain clauses, but this
wrapper was still AOTed for performance reasons.
Instead, add a new CIL opcode to make the .ovf opcodes throw the
OutOfMemoryException directly.

* Disable inline rgctx fetch in mono_decompose_vtype_opts () as well.

It can't handle the new bblocks added by the inline rgctx fetch code.

* [mono] Factor out the slow path of Array.Copy () into a separate method.

The slowpath contained a try-catch clause, preventing Array.Copy () from
being AOTed on WASM.

* [mono] Fix assigning a name to LLVM Alloca instructions.

* Add a V=1 makefile option to wasm/Makefile.

* Add a WasmAotProfilePath build property.

src/mono/mono/metadata/jit-icall-reg.h
@@ -181,6 +181,7 @@ MONO_JIT_ICALL (mono_delegate_end_invoke) \
 MONO_JIT_ICALL (mono_delegate_to_ftnptr) \
 MONO_JIT_ICALL (mono_domain_get) \
 MONO_JIT_ICALL (mono_dummy_jit_icall) \
+MONO_JIT_ICALL (mono_dummy_jit_icall_val) \
 MONO_JIT_ICALL (mono_exception_from_token) \
 MONO_JIT_ICALL (mono_fill_class_rgctx) \
 MONO_JIT_ICALL (mono_fill_method_rgctx) \

src/mono/mono/mini/jit-icalls.c
@@ -1622,3 +1622,8 @@ void
 mono_dummy_jit_icall (void)
 {
 }
+
+void
+mono_dummy_jit_icall_val (gpointer val)
+{
+}

src/mono/mono/mini/jit-icalls.h
@@ -229,4 +229,6 @@ ICALL_EXPORT void mono_throw_invalid_program (const char *msg);
 
 ICALL_EXPORT void mono_dummy_jit_icall (void);
 
+ICALL_EXPORT void mono_dummy_jit_icall_val (gpointer ptr);
+
 #endif /* __MONO_JIT_ICALLS_H__ */

src/mono/mono/mini/method-to-ir.c
@@ -524,6 +524,7 @@ add_widen_op (MonoCompile *cfg, MonoInst *ins, MonoInst **arg1_ref, MonoInst **a
 		ins->sreg2 = sp [1]->dreg;	\
 		type_from_op (cfg, ins, sp [0], sp [1]);	\
 		CHECK_TYPE (ins);	\
+		if (ovf_exc) ins->inst_exc_name = ovf_exc; else ins->inst_exc_name = "OverflowException"; ovf_exc = NULL; \
 		/* Have to insert a widening op */		 \
         add_widen_op (cfg, ins, &sp [0], &sp [1]);		 \
         ins->dreg = alloc_dreg ((cfg), (MonoStackType)(ins)->type); \
@@ -6149,6 +6150,7 @@ mono_method_to_ir (MonoCompile *cfg, MonoMethod *method, MonoBasicBlock *start_b
 	MonoDebugMethodInfo *minfo;
 	MonoBitSet *seq_point_locs = NULL;
 	MonoBitSet *seq_point_set_locs = NULL;
+	const char *ovf_exc = NULL;
 	gboolean emitted_funccall_seq_point = FALSE;
 	gboolean detached_before_ret = FALSE;
 	gboolean ins_has_side_effect;
@@ -11084,6 +11086,11 @@ mono_ldptr:
 			*sp++ = ins;
 			break;
 		}
+		case MONO_CEE_MONO_REMAP_OVF_EXC:
+			/* Remap the exception thrown by the next _OVF opcode */
+			g_assert (method->wrapper_type != MONO_WRAPPER_NONE);
+			ovf_exc = (const char*)mono_method_get_wrapper_data (method, token);
+			break;
 
 		case MONO_CEE_ARGLIST: {
 			/* somewhat similar to LDTOKEN */

src/mono/mono/mini/mini-runtime.c
@@ -4919,6 +4919,7 @@ register_icalls (void)
 	register_icall (mono_throw_platform_not_supported, mono_icall_sig_void, FALSE);
 	register_icall (mono_throw_invalid_program, mono_icall_sig_void_ptr, FALSE);
 	register_icall_no_wrapper (mono_dummy_jit_icall, mono_icall_sig_void);
+	//register_icall_no_wrapper (mono_dummy_jit_icall_val, mono_icall_sig_void_ptr);
 
 	register_icall_with_wrapper (mono_monitor_enter_internal, mono_icall_sig_int32_obj);
 	register_icall_with_wrapper (mono_monitor_enter_v4_internal, mono_icall_sig_void_obj_ptr);

src/mono/System.Private.CoreLib/src/System/Array.Mono.cs
@@ -144,6 +144,11 @@ namespace System
             if (FastCopy(sourceArray, sourceIndex, destinationArray, destinationIndex, length))
                 return;
 
+            CopySlow(sourceArray, sourceIndex, destinationArray, destinationIndex, length, reliable);
+        }
+
+        private static void CopySlow(Array sourceArray, int sourceIndex, Array destinationArray, int destinationIndex, int length, bool reliable)
+        {
             int source_pos = sourceIndex - sourceArray.GetLowerBound(0);
             int dest_pos = destinationIndex - destinationArray.GetLowerBound(0);
 

src/mono/mono/cil/cil-opcodes.xml
@@ -327,4 +327,5 @@
 <opcode name="mono_get_sp" input="Pop0" output="PushI" args="InlineNone" o1="0xF0" o2="0x20" flow="next" />
 <opcode name="mono_methodconst" input="Pop0" output="PushI" args="InlineI" o1="0xF0" o2="0x21" flow="next" />
 <opcode name="mono_pinvoke_addr_cache" input="Pop0" output="PushI" args="InlineI" o1="0xF0" o2="0x22" flow="next" />
+<opcode name="mono_remap_ovf_exc" input="Pop0" output="Push0" args="InlineI" o1="0xF0" o2="0x23" flow="next" />
 </opdesc>

src/mono/mono/cil/opcode.def
@@ -327,6 +327,7 @@ OPDEF(CEE_MONO_RETHROW, "mono_rethrow", PopRef, Push0, InlineNone, 0, 2, 0xF0, 0
 OPDEF(CEE_MONO_GET_SP, "mono_get_sp", Pop0, PushI, InlineNone, 0, 2, 0xF0, 0x20, NEXT)
 OPDEF(CEE_MONO_METHODCONST, "mono_methodconst", Pop0, PushI, InlineI, 0, 2, 0xF0, 0x21, NEXT)
 OPDEF(CEE_MONO_PINVOKE_ADDR_CACHE, "mono_pinvoke_addr_cache", Pop0, PushI, InlineI, 0, 2, 0xF0, 0x22, NEXT)
+OPDEF(CEE_MONO_REMAP_OVF_EXC, "mono_remap_ovf_exc", Pop0, Push0, InlineI, 0, 2, 0xF0, 0x23, NEXT)
 #ifndef OPALIAS
 #define _MONO_CIL_OPALIAS_DEFINED_
 #define OPALIAS(a,s,r)

commit edbe39f62d8cc3afe7d5aa1d54f15f9aee4cb030

[mono] Fix a regression caused by 9ddd58a58d14a7bec5ed6eb777c6703c48aca15d.
Fixes https://github.com/dotnet/runtime/issues/63079.

src/mono/mono/metadata/jit-icall-reg.h
@@ -98,6 +98,7 @@ MONO_JIT_ICALL (__emul_ldiv_un) \
 MONO_JIT_ICALL (__emul_lmul) \
 MONO_JIT_ICALL (__emul_lmul_ovf) \
 MONO_JIT_ICALL (__emul_lmul_ovf_un) \
+MONO_JIT_ICALL (__emul_lmul_ovf_un_oom) \
 MONO_JIT_ICALL (__emul_lrem) \
 MONO_JIT_ICALL (__emul_lrem_un) \
 MONO_JIT_ICALL (__emul_lshl) \
@@ -108,6 +109,7 @@ MONO_JIT_ICALL (__emul_op_idiv_un) \
 MONO_JIT_ICALL (__emul_op_imul) \
 MONO_JIT_ICALL (__emul_op_imul_ovf) \
 MONO_JIT_ICALL (__emul_op_imul_ovf_un) \
+MONO_JIT_ICALL (__emul_op_imul_ovf_un_oom) \
 MONO_JIT_ICALL (__emul_op_irem) \
 MONO_JIT_ICALL (__emul_op_irem_un) \
 MONO_JIT_ICALL (__emul_rconv_to_i8) \

src/mono/mono/mini/jit-icalls.c
@@ -196,6 +196,40 @@ mono_llmult_ovf_un (guint64 a, guint64 b)
 }
 
 guint64
+mono_llmult_ovf_un_oom (guint64 a, guint64 b)
+{
+	guint32 al = a;
+	guint32 ah = a >> 32;
+	guint32 bl = b;
+	guint32 bh = b >> 32;
+	guint64 res, t1;
+
+	// fixme: this is incredible slow
+
+	if (ah && bh)
+		goto raise_exception;
+
+	res = (guint64)al * (guint64)bl;
+
+	t1 = (guint64)ah * (guint64)bl + (guint64)al * (guint64)bh;
+
+	if (t1 > 0xffffffff)
+		goto raise_exception;
+
+	res += ((guint64)t1) << 32;
+
+	return res;
+
+ raise_exception:
+	{
+		ERROR_DECL (error);
+		mono_error_set_out_of_memory (error, "");
+		mono_error_set_pending_exception (error);
+	}
+	return 0;
+}
+
+guint64
 mono_llmult_ovf (gint64 a, gint64 b)
 {
 	guint32 al = a;
@@ -515,6 +549,21 @@ mono_imul_ovf_un (guint32 a, guint32 b)
 	if (res >> 32) {
 		ERROR_DECL (error);
 		mono_error_set_overflow (error);
+		mono_error_set_pending_exception (error);
+		return 0;
+	}
+
+	return res;
+}
+
+gint32
+mono_imul_ovf_un_oom (guint32 a, guint32 b)
+{
+	const guint64 res = (guint64)a * (guint64)b;
+
+	if (res >> 32) {
+		ERROR_DECL (error);
+		mono_error_set_out_of_memory (error, "");
 		mono_error_set_pending_exception (error);
 		return 0;
 	}

src/mono/mono/mini/jit-icalls.h
@@ -21,6 +21,8 @@ ICALL_EXPORT gint64 mono_llmult (gint64 a, gint64 b);
 
 ICALL_EXPORT guint64 mono_llmult_ovf_un (guint64 a, guint64 b);
 
+ICALL_EXPORT guint64 mono_llmult_ovf_un_oom (guint64 a, guint64 b);
+
 ICALL_EXPORT guint64 mono_llmult_ovf (gint64 a, gint64 b);
 
 ICALL_EXPORT gint32 mono_idiv (gint32 a, gint32 b);
@@ -36,6 +38,8 @@ ICALL_EXPORT gint32 mono_imul (gint32 a, gint32 b);
 ICALL_EXPORT gint32 mono_imul_ovf (gint32 a, gint32 b);
 
 ICALL_EXPORT gint32 mono_imul_ovf_un (guint32 a, guint32 b);
+
+ICALL_EXPORT gint32 mono_imul_ovf_un_oom (guint32 a, guint32 b);
 
 ICALL_EXPORT double mono_fdiv (double a, double b);
 

src/mono/mono/mini/method-to-ir.c
@@ -517,21 +517,6 @@ add_widen_op (MonoCompile *cfg, MonoInst *ins, MonoInst **arg1_ref, MonoInst **a
 #endif
 }
 
-#define ADD_BINOP(op) do {	\
-		MONO_INST_NEW (cfg, ins, (op));	\
-		sp -= 2;	\
-		ins->sreg1 = sp [0]->dreg;	\
-		ins->sreg2 = sp [1]->dreg;	\
-		type_from_op (cfg, ins, sp [0], sp [1]);	\
-		CHECK_TYPE (ins);	\
-		if (ovf_exc) ins->inst_exc_name = ovf_exc; else ins->inst_exc_name = "OverflowException"; ovf_exc = NULL; \
-		/* Have to insert a widening op */		 \
-        add_widen_op (cfg, ins, &sp [0], &sp [1]);		 \
-        ins->dreg = alloc_dreg ((cfg), (MonoStackType)(ins)->type); \
-        MONO_ADD_INS ((cfg)->cbb, (ins)); \
-        *sp++ = mono_decompose_opcode ((cfg), (ins));	\
-	} while (0)
-
 #define ADD_UNOP(op) do {	\
 		MONO_INST_NEW (cfg, ins, (op));	\
 		sp--;	\
@@ -8674,7 +8659,37 @@ calli_end:
 		case MONO_CEE_MUL_OVF_UN:
 		case MONO_CEE_SUB_OVF:
 		case MONO_CEE_SUB_OVF_UN:
-			ADD_BINOP (il_op);
+			MONO_INST_NEW (cfg, ins, il_op);
+			sp -= 2;
+			ins->sreg1 = sp [0]->dreg;
+			ins->sreg2 = sp [1]->dreg;
+			type_from_op (cfg, ins, sp [0], sp [1]);
+			CHECK_TYPE (ins);
+			if (ovf_exc)
+				ins->inst_exc_name = ovf_exc;
+			else
+				ins->inst_exc_name = "OverflowException";
+			/* Have to insert a widening op */
+			add_widen_op (cfg, ins, &sp [0], &sp [1]);
+			ins->dreg = alloc_dreg (cfg, (MonoStackType)(ins)->type);
+			MONO_ADD_INS ((cfg)->cbb, ins);
+			/* The opcode might be emulated, so need to special case this */
+			if (ovf_exc && mono_find_jit_opcode_emulation (ins->opcode)) {
+				switch (ins->opcode) {
+				case OP_IMUL_OVF_UN:
+					/* This opcode is just a placeholder, it will be emulated also */
+					ins->opcode = OP_IMUL_OVF_UN_OOM;
+					break;
+				case OP_LMUL_OVF_UN:
+					/* This opcode is just a placeholder, it will be emulated also */
+					ins->opcode = OP_LMUL_OVF_UN_OOM;
+					break;
+				default:
+					g_assert_not_reached ();
+				}
+			}
+			ovf_exc = NULL;
+			*sp++ = mono_decompose_opcode (cfg, ins);
 			break;
 		case MONO_CEE_CPOBJ:
 			GSHAREDVT_FAILURE (il_op);

src/mono/mono/mini/mini-ops.h
@@ -639,6 +639,11 @@ MINI_OP(OP_FGETHIGH32, "float_gethigh32", IREG, FREG, NONE)
 
 MINI_OP(OP_JUMP_TABLE, "jump_table", IREG, NONE, NONE)
 
+/* Same as OP_IMUL_OVF_UN, but throws an OutOfMemoryException */
+/* Emulated */
+MINI_OP(OP_IMUL_OVF_UN_OOM,   "int_mul_ovf_un_oom", IREG, IREG, IREG)
+MINI_OP(OP_LMUL_OVF_UN_OOM,   "long_mul_ovf_un_oom", LREG, LREG, LREG)
+
 /* aot compiler */
 MINI_OP(OP_AOTCONST, "aotconst", IREG, NONE, NONE)
 MINI_OP(OP_PATCH_INFO, "patch_info", NONE, NONE, NONE)

src/mono/mono/mini/mini-runtime.c
@@ -4729,6 +4729,7 @@ register_icalls (void)
 #if !defined(MONO_ARCH_NO_EMULATE_LONG_MUL_OPTS) || defined(MONO_ARCH_EMULATE_LONG_MUL_OVF_OPTS)
 	register_opcode_emulation (OP_LMUL_OVF_UN, __emul_lmul_ovf_un, mono_icall_sig_long_long_long, mono_llmult_ovf_un, FALSE);
 	register_opcode_emulation (OP_LMUL_OVF, __emul_lmul_ovf, mono_icall_sig_long_long_long, mono_llmult_ovf, FALSE);
+	register_opcode_emulation (OP_LMUL_OVF_UN_OOM, __emul_lmul_ovf_un_oom, mono_icall_sig_long_long_long, mono_llmult_ovf_un_oom, FALSE);
 #endif
 
 #ifndef MONO_ARCH_NO_EMULATE_LONG_SHIFT_OPS
@@ -4751,6 +4752,7 @@ register_icalls (void)
 #if defined(MONO_ARCH_EMULATE_MUL_DIV) || defined(MONO_ARCH_EMULATE_MUL_OVF)
 	register_opcode_emulation (OP_IMUL_OVF, __emul_op_imul_ovf, mono_icall_sig_int32_int32_int32, mono_imul_ovf, FALSE);
 	register_opcode_emulation (OP_IMUL_OVF_UN, __emul_op_imul_ovf_un, mono_icall_sig_int32_int32_int32, mono_imul_ovf_un, FALSE);
+	register_opcode_emulation (OP_IMUL_OVF_UN_OOM, __emul_op_imul_ovf_un_oom, mono_icall_sig_int32_int32_int32, mono_imul_ovf_un_oom, FALSE);
 #endif
 
 #if defined(MONO_ARCH_EMULATE_MUL_DIV) || defined(MONO_ARCH_SOFT_FLOAT_FALLBACK)

src/mono/mono/mini/mini-x86.c
@@ -2992,7 +2992,7 @@ mono_arch_output_basic_block (MonoCompile *cfg, MonoBasicBlock *bb)
 			break;
 		case OP_IMUL_OVF:
 			x86_imul_reg_reg (code, ins->sreg1, ins->sreg2);
-			EMIT_COND_SYSTEM_EXCEPTION (X86_CC_O, FALSE, "OverflowException");
+			EMIT_COND_SYSTEM_EXCEPTION (X86_CC_O, FALSE, ins->inst_exc_name);
 			break;
 		case OP_IMUL_OVF_UN: {
 			/* the mul operation and the exception check should most likely be split */
@@ -3028,7 +3028,7 @@ mono_arch_output_basic_block (MonoCompile *cfg, MonoBasicBlock *bb)
 				x86_pop_reg (code, X86_EDX);
 			if (saved_eax)
 				x86_pop_reg (code, X86_EAX);
-			EMIT_COND_SYSTEM_EXCEPTION (X86_CC_O, FALSE, "OverflowException");
+			EMIT_COND_SYSTEM_EXCEPTION (X86_CC_O, FALSE, ins->inst_exc_name);
 			break;
 		}
 		case OP_ICONST: