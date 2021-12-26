a95cc4d3d8bb4636193897c957e5e11c33641d4d
Adeel Mujahid <3840695+am11@users.noreply.github.com>
2021-12-26 06:30:45 +0200

    Parse compiler version in init-compiler.sh (#63126)
    When user passes versioned native compiler argument such as
    `-clang10.1` or `-gcc9.2` to the top-level `:/build.sh`,
    `:/eng/build.sh` transforms that to msbuild property called
    `<Compiler>` with raw/unprocessed value.
    
    In coreclr, libs, corehost and tests partitions, we end up calling
    `:/eng/native/build-commons.sh`, which slices the compiler name, major
    and minor versions, then calls `:/eng/native/gen-buildsys.sh` with
    individual components. That is just a pass-thru script for the final
    `:/eng/common/native/init-compiler.sh`, where those arguments are
    actually used.
    
    In `mono.proj` and `NativeExports.csproj`, we do not use
    `build-commons` script, but instead, invoke `init-compiler.sh` directly
    with the raw `<Compiler>` property. That causes an error when versioned
    native compiler is specified on the command line.
    
    This PR fixes this issue by pushing version parsing code into
    `init-compiler.sh` so all its consumers get the same behavior.

--- eng/native/build-commons.sh
+++ eng/native/build-commons.sh
@@ -117,7 +117,7 @@ build_native()
             scan_build=scan-build
         fi
 
-        nextCommand="\"$__RepoRootDir/eng/native/gen-buildsys.sh\" \"$cmakeDir\" \"$intermediatesDir\" $platformArch $__Compiler \"$__CompilerMajorVersion\" \"$__CompilerMinorVersion\" $__BuildType \"$generator\" $scan_build $cmakeArgs"
+        nextCommand="\"$__RepoRootDir/eng/native/gen-buildsys.sh\" \"$cmakeDir\" \"$intermediatesDir\" $platformArch $__Compiler $__BuildType \"$generator\" $scan_build $cmakeArgs"
         echo "Invoking $nextCommand"
         eval $nextCommand
 
@@ -304,15 +304,7 @@ while :; do
             ;;
 
         clang*|-clang*)
-                __Compiler=clang
-                # clangx.y or clang-x.y
-                version="$(echo "$lowerI" | tr -d '[:alpha:]-=')"
-                parts=(${version//./ })
-                __CompilerMajorVersion="${parts[0]}"
-                __CompilerMinorVersion="${parts[1]}"
-                if [[ -z "$__CompilerMinorVersion" && "$__CompilerMajorVersion" -le 6 ]]; then
-                    __CompilerMinorVersion=0;
-                fi
+            __Compiler="$lowerI"
             ;;
 
         cmakeargs|-cmakeargs)
@@ -340,12 +332,7 @@ while :; do
             ;;
 
         gcc*|-gcc*)
-                __Compiler=gcc
-                # gccx.y or gcc-x.y
-                version="$(echo "$lowerI" | tr -d '[:alpha:]-=')"
-                parts=(${version//./ })
-                __CompilerMajorVersion="${parts[0]}"
-                __CompilerMinorVersion="${parts[1]}"
+            __Compiler="$lowerI"
             ;;
 
         keepnativesymbols|-keepnativesymbols)
