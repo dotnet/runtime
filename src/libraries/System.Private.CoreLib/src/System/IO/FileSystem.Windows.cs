41ee8fef233b2cd86c9814d0c637688c5d49c9ce
Dan Moseley <danmose@microsoft.com>
2021-12-30 14:27:27 -0700

    fix build

--- src/libraries/System.Private.CoreLib/src/System/IO/FileSystem.Windows.cs
+++ src/libraries/System.Private.CoreLib/src/System/IO/FileSystem.Windows.cs
@@ -147,7 +147,7 @@ namespace System.IO
 
         private static SafeFileHandle OpenHandleToWriteAttributes(string fullPath, bool asDirectory)
         {
-            if (fullPath.Length == PathInternal.GetRootLength(fullPath) && fullPath[1] == Path.VolumeSeparatorChar)
+            if (fullPath.Length == PathInternal.GetRootLength(fullPath.AsSpan()) && fullPath[1] == Path.VolumeSeparatorChar)
             {
                 // intentionally not fullpath, most upstack public APIs expose this as path.
                 throw new ArgumentException(SR.Arg_PathIsVolume, "path");
