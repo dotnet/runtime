20f559fcd4f83b620a19a40472569430d6ba64c3
Christopher Yeleighton <ne01026@shark.2a.pl>
2022-01-05 18:49:07 +0100

    see in remarks

--- src/libraries/Microsoft.Extensions.Hosting.Abstractions/src/BackgroundService.cs
+++ src/libraries/Microsoft.Extensions.Hosting.Abstractions/src/BackgroundService.cs
@@ -28,8 +28,9 @@ namespace Microsoft.Extensions.Hosting
         /// the lifetime of the long running operation(s) being performed.
         /// </summary>
         /// <param name="stoppingToken">Triggered when <see cref="IHostedService.StopAsync(CancellationToken)"/> is called.</param>
-        /// See <see href="https://docs.microsoft.com/dotnet/core/extensions/workers">Worker Services in .NET</see> for implementation guidelines.
         /// <returns>A <see cref="Task"/> that represents the long running operations.</returns>
+        /// <remarks>See <see href="https://docs.microsoft.com/dotnet/core/extensions/workers">Worker Services in .NET</see> for implementation guidelines.</remarks>
+
         protected abstract Task ExecuteAsync(CancellationToken stoppingToken);
 
         /// <summary>
