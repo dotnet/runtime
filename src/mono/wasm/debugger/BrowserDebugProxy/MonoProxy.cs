58eae50f23f44faeb680d4f0b3a9213d7e18a1ca
DESKTOP-GEPIA6N\Thays <thaystg@gmail.com>
2022-01-05 15:25:53 -0300

    Passing null to name.

--- src/mono/wasm/debugger/BrowserDebugProxy/MonoProxy.cs
+++ src/mono/wasm/debugger/BrowserDebugProxy/MonoProxy.cs
@@ -697,7 +697,7 @@ namespace Microsoft.WebAssembly.Diagnostics
                     }
                     case "methodId":
                     {
-                        var resMethod = await context.SdbAgent.InvokeMethodInObject(objectId.Value, objectId.SubValue, "", token);
+                        var resMethod = await context.SdbAgent.InvokeMethodInObject(objectId.Value, objectId.SubValue, null, token);
                         return sortByAccessLevel ? JObject.FromObject(new { result = new JArray(resMethod) }) : new JArray(resMethod);
                     }
                     case "object":
