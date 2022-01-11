b84b38b659baad0efdf978bc09de916575313935
Aleksey Kliger <alklig@microsoft.com>
2022-01-11 14:50:18 -0500

    Revert "WIP: adding static fields" changes to sample

--- src/mono/sample/mbr/console/TestClass_v1.cs
+++ src/mono/sample/mbr/console/TestClass_v1.cs
@@ -4,34 +4,16 @@ using System.Runtime.CompilerServices;
 public class TestClass {
 	[MethodImpl(MethodImplOptions.NoInlining)]
 	public static string TargetMethod () {
-#if ADD_INNER_INST
+#if false
 		var o = new Inner();
 		return o.GetIt();
 #endif
-#if true
 		return NewFunc (() => "NEW STRING");
-#endif
-#if ADD_MUTUALS
-		return ClassOne.F1.s;
-#endif
         }
 
-#if true
 	public static string NewFunc(Func<string> f) => f ();
-#endif
 
-#if ADD_MUTUALS
-	public class ClassOne {
-		public static ClassTwo F1;
-	}
-
-	public class ClassTwo {
-		public static ClassOne F2;
-		public string s;
-	}
-#endif
-
-#if ADD_INNER_INST
+#if false
 	public class Inner {
 		public string GetIt() => "NEW STRING";
 	}
