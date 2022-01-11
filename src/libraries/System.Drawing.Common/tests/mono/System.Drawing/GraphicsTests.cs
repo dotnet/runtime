6352e26d43e1cb1e8a756391049f0e6058a6735d
Josip Medved <jmedved@jmedved.com>
2022-01-11 07:44:12 -0800

    Removing RectangleF overload from mono tests
    DrawRectangle with RectangleF overload is new and not compiled into
    mono.

--- src/libraries/System.Drawing.Common/tests/mono/System.Drawing/GraphicsTests.cs
+++ src/libraries/System.Drawing.Common/tests/mono/System.Drawing/GraphicsTests.cs
@@ -921,7 +921,6 @@ namespace MonoTests.System.Drawing
                 g.DrawRectangle(pen, 5, 5, -10, -10);
                 g.DrawRectangle(pen, 0.0f, 0.0f, 5.0f, -10.0f);
                 g.DrawRectangle(pen, new Rectangle(15, 0, -10, 5));
-                g.DrawRectangle(pen, new RectangleF(0.0f, 5.0f, -10.0f, -10.0f));
                 CheckForEmptyBitmap(bitmap);
                 pen.Dispose();
                 g.Dispose();
@@ -965,7 +964,6 @@ namespace MonoTests.System.Drawing
                 g.FillRectangle(brush, 5, 5, -10, -10);
                 g.FillRectangle(brush, 0.0f, 0.0f, 5.0f, -10.0f);
                 g.FillRectangle(brush, new Rectangle(15, 0, -10, 5));
-                g.FillRectangle(brush, new RectangleF(15.0f, 0.0f, -10.0f, 5.0f));
                 CheckForEmptyBitmap(bitmap);
                 brush.Dispose();
                 g.Dispose();
