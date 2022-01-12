77e4c8831f6fdecd352cbd09d8a7d726cfbd8752
Simon Rozsival <simon@rozsival.com>
2022-01-12 21:23:40 +0100

    Refactoring

--- src/libraries/System.Net.NetworkInformation/src/System/Net/NetworkInformation/NetworkInterfacePal.Android.cs
+++ src/libraries/System.Net.NetworkInformation/src/System/Net/NetworkInformation/NetworkInterfacePal.Android.cs
@@ -9,44 +9,14 @@ namespace System.Net.NetworkInformation
     {
         /// Returns objects that describe the network interfaces on the local computer.
         public static NetworkInterface[] GetAllNetworkInterfaces() => GetAndroidNetworkInterfaces();
-
-        public static bool GetIsNetworkAvailable()
-        {
-            foreach (var ni in GetAndroidNetworkInterfaces())
-            {
-                if (ni.NetworkInterfaceType == NetworkInterfaceType.Loopback
-                    || ni.NetworkInterfaceType == NetworkInterfaceType.Tunnel)
-                {
-                    continue;
-                }
-                if (ni.OperationalStatus == OperationalStatus.Up)
-                {
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
+        public static bool GetIsNetworkAvailable() => TransformNetworkInterfacess(IsSomeNetworkUp);
         public static int IPv6LoopbackInterfaceIndex => LoopbackInterfaceIndex;
-
-        public static int LoopbackInterfaceIndex
-        {
-            get
-            {
-                foreach (var networkInterface in GetAndroidNetworkInterfaces())
-                {
-                    if (networkInterface.NetworkInterfaceType == NetworkInterfaceType.Loopback)
-                    {
-                        return networkInterface.Index;
-                    }
-                }
-
-                throw new NetworkInformationException(SR.net_NoLoopback);
-            }
-        }
+        public static int LoopbackInterfaceIndex => TransformNetworkInterfacess(FindLoopbackInterfaceIndex);
 
         internal static unsafe AndroidNetworkInterface[] GetAndroidNetworkInterfaces()
+            => TransformNetworkInterfacess(ToAndroidNetworkInterfaceArray);
+
+        private static unsafe T TransformNetworkInterfacess<T>(Func<int, IntPtr, int, IntPtr, T> transform)
         {
             int interfaceCount = 0;
             int addressCount = 0;
@@ -66,34 +36,75 @@ namespace System.Net.NetworkInformation
 
             try
             {
-                var networkInterfaces = new AndroidNetworkInterface[interfaceCount];
-
-                for (int i = 0; i < interfaceCount; i++, networkInterfaceInfo++)
-                {
-                    var name = Marshal.PtrToStringAnsi((IntPtr)networkInterfaceInfo->Name);
-                    networkInterfaces[i] = new AndroidNetworkInterface(name!, networkInterfaceInfo);
-                }
-
-                for (int i = 0; i < addressCount; i++, addressInfo++)
-                {
-                    // there is usually just a handful of few network interfaces on Android devices
-                    // and this linear search does not have any impact on performance
-                    foreach (var networkInterface in networkInterfaces)
-                    {
-                        if (networkInterface.Index == addressInfo->InterfaceIndex)
-                        {
-                            networkInterface.AddAddress(addressInfo);
-                            break;
-                        }
-                    }
-                }
-
-                return networkInterfaces;
+                return transform(interfaceCount, (IntPtr)networkInterfaceInfo, addressCount, (IntPtr)addressInfo);
             }
             finally
             {
                 Marshal.FreeHGlobal(globalMemory);
             }
+        }
+
+        private static unsafe AndroidNetworkInterface[] ToAndroidNetworkInterfaceArray(int interfaceCount, IntPtr networkInterfacesPtr, int addressCount, IntPtr addressPtr)
+        {
+            var networkInterfaces = new AndroidNetworkInterface[interfaceCount];
+
+            var networkInterfaceInfo = (Interop.Sys.NetworkInterfaceInfo*)networkInterfacesPtr;
+            for (int i = 0; i < interfaceCount; i++, networkInterfaceInfo++)
+            {
+                var name = Marshal.PtrToStringAnsi((IntPtr)networkInterfaceInfo->Name);
+                networkInterfaces[i] = new AndroidNetworkInterface(name!, networkInterfaceInfo);
+            }
+
+            var addressInfo = (Interop.Sys.IpAddressInfo*)addressPtr;
+            for (int i = 0; i < addressCount; i++, addressInfo++)
+            {
+                // there is usually just a handful of few network interfaces on Android devices
+                // and this linear search does not have any impact on performance
+                foreach (var networkInterface in networkInterfaces)
+                {
+                    if (networkInterface.Index == addressInfo->InterfaceIndex)
+                    {
+                        networkInterface.AddAddress(addressInfo);
+                        break;
+                    }
+                }
+            }
+
+            return networkInterfaces;
+        }
+
+        private static unsafe int FindLoopbackInterfaceIndex(int interfaceCount, IntPtr networkInterfacesPtr, int addressCount, IntPtr addressPtr)
+        {
+            var networkInterfaceInfo = (Interop.Sys.NetworkInterfaceInfo*)networkInterfacesPtr;
+            for (int i = 0; i < interfaceCount; i++, networkInterfaceInfo++)
+            {
+                if (networkInterfaceInfo->HardwareType == (int)NetworkInterfaceType.Loopback)
+                {
+                    return networkInterfaceInfo->InterfaceIndex;
+                }
+            }
+
+            throw new NetworkInformationException(SR.net_NoLoopback);
+        }
+
+        private static unsafe bool IsSomeNetworkUp(int interfaceCount, IntPtr networkInterfacesPtr, int addressCount, IntPtr addressPtr)
+        {
+            var networkInterfaceInfo = (Interop.Sys.NetworkInterfaceInfo*)networkInterfacesPtr;
+            for (int i = 0; i < interfaceCount; i++, networkInterfaceInfo++)
+            {
+                if (networkInterfaceInfo->HardwareType == (int)NetworkInterfaceType.Loopback
+                    || networkInterfaceInfo->HardwareType == (int)NetworkInterfaceType.Tunnel)
+                {
+                    continue;
+                }
+
+                if (networkInterfaceInfo->OperationalState == (int)OperationalStatus.Up)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
