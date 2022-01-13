be87205123b673f544e7822a9687f1f6d631f0bd
Wraith2 <wraith2@gmail.com>
2022-01-13 09:01:12 +0000

    correct missing IsOper(GT_HWINTRINSIC) check

--- src/coreclr/jit/lower.cpp
+++ src/coreclr/jit/lower.cpp
@@ -2710,8 +2710,9 @@ GenTree* Lowering::OptimizeConstCompare(GenTree* cmp)
 #ifdef TARGET_XARCH
             (op1->OperIs(GT_AND, GT_OR, GT_XOR, GT_ADD, GT_SUB, GT_NEG)
 #ifdef FEATURE_HW_INTRINSICS
-             || emitter::DoesWriteZeroFlag(
-                    HWIntrinsicInfo::lookupIns(op1->AsHWIntrinsic()->GetHWIntrinsicId(), op1->TypeGet()))
+             || (op1->OperIs(GT_HWINTRINSIC) &&
+                 emitter::DoesWriteZeroFlag(
+                    HWIntrinsicInfo::lookupIns(op1->AsHWIntrinsic()->GetHWIntrinsicId(), op1->TypeGet())))
 #endif // FEATURE_HW_INTRINSICS
                  )
 #else // TARGET_ARM64
