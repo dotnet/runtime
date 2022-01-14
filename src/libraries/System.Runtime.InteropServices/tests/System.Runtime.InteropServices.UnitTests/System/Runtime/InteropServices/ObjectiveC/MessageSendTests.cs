c483c14de73739c2afb0d23a55fea0a049c5187e
Elinor Fung <elfung@microsoft.com>
2022-01-14 07:47:06 -0800

    Add TODO to revert once dotnet/arcade#5865 is addressed

--- src/libraries/System.Runtime.InteropServices/tests/System.Runtime.InteropServices.UnitTests/System/Runtime/InteropServices/ObjectiveC/MessageSendTests.cs
+++ src/libraries/System.Runtime.InteropServices/tests/System.Runtime.InteropServices.UnitTests/System/Runtime/InteropServices/ObjectiveC/MessageSendTests.cs
@@ -72,7 +72,8 @@ namespace System.Runtime.InteropServices.Tests
                 ObjectiveCMarshal.SetMessageSendCallback(msgSend, func);
                 Assert.Throws<InvalidOperationException>(() => ObjectiveCMarshal.SetMessageSendCallback(msgSend, func));
 
-                // RemoteExecutor only checks the expected exit code if the invoked function returns an int.
+                // TODO: Remove once https://github.com/dotnet/arcade/issues/5865 is resolved
+                // RemoteExecutor currently only checks the expected exit code if the invoked function returns an int.
                 // Check the exit code to ensure the test will fail if there was a crash that could not be caught by the executor.
                 return RemoteExecutor.SuccessExitCode;
             }).Dispose();
@@ -102,7 +103,8 @@ namespace System.Runtime.InteropServices.Tests
 
                 SetMessageSendCallbackImpl(msgSendArray);
 
-                // RemoteExecutor only checks the expected exit code if the invoked function returns an int.
+                // TODO: Remove once https://github.com/dotnet/arcade/issues/5865 is resolved
+                // RemoteExecutor currently only checks the expected exit code if the invoked function returns an int.
                 // Check the exit code to ensure the test will fail if there was a crash that could not be caught by the executor.
                 return RemoteExecutor.SuccessExitCode;
             }, string.Join(';', funcsToOverride)).Dispose();
