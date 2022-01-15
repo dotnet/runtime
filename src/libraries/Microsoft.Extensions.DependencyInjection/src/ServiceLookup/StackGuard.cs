cd95ddb746ce5c8066b8ca8df36d358ca2c76fe0
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2022-01-16 00:25:22 +0200

    Update RunOnEmptyStackCore

--- src/libraries/Microsoft.Extensions.DependencyInjection/src/ServiceLookup/StackGuard.cs
+++ src/libraries/Microsoft.Extensions.DependencyInjection/src/ServiceLookup/StackGuard.cs
@@ -45,27 +45,27 @@ namespace Microsoft.Extensions.DependencyInjection.ServiceLookup
 #if NETFRAMEWORK || NETSTANDARD2_0
             return RunOnEmptyStackCore(static s =>
             {
-                var t = (Tuple<Func<T1, T2, TR>, T1, T2>)s!;
+                var t = (Tuple<Func<T1, T2, TR>, T1, T2>)s;
                 return t.Item1(t.Item2, t.Item3);
             }, Tuple.Create(action, arg1, arg2));
 #else
             // Prefer ValueTuple when available to reduce dependencies on Tuple
             return RunOnEmptyStackCore(static s =>
             {
-                var t = ((Func<T1, T2, TR>, T1, T2))s!;
+                var t = ((Func<T1, T2, TR>, T1, T2))s;
                 return t.Item1(t.Item2, t.Item3);
             }, (action, arg1, arg2));
 #endif
         }
 
-        private R RunOnEmptyStackCore<R>(Func<object?, R> action, object? state)
+        private R RunOnEmptyStackCore<R>(Func<object, R> action, object state)
         {
             _executionStackCount++;
 
             try
             {
                 // Using default scheduler rather than picking up the current scheduler.
-                Task<R> task = Task.Factory.StartNew(action, state, CancellationToken.None, TaskCreationOptions.DenyChildAttach, TaskScheduler.Default);
+                Task<R> task = Task.Factory.StartNew((Func<object?, R>)action, state, CancellationToken.None, TaskCreationOptions.DenyChildAttach, TaskScheduler.Default);
 
                 // Avoid AsyncWaitHandle lazy allocation of ManualResetEvent in the rare case we finish quickly.
                 if (!task.IsCompleted)
