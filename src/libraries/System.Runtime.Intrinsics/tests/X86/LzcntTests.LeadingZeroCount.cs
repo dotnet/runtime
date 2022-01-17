c8acaea21370f930ed1ba1acfdc4e78de8795923
Robin Lindner <robin@deeprobin.de>
2022-01-17 16:05:43 +0100

    Fix tests

--- src/libraries/System.Runtime.Intrinsics/tests/X86/LzcntTests.LeadingZeroCount.cs
+++ src/libraries/System.Runtime.Intrinsics/tests/X86/LzcntTests.LeadingZeroCount.cs
@@ -9,10 +9,10 @@ namespace System.Runtime.Intrinsics.Tests.X86;
 public sealed partial class LzcntTests
 {
     [ConditionalTheory(nameof(Run64BitTests))]
-    [InlineData(0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000, 64)]
+    [InlineData(0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000, 32)]
     [InlineData(0b11111111_11111111_11111111_11111111_11111111_11111111_11111111_11111111, 0)]
-    [InlineData(0b01111111_11111111_11111111_11111111_11111111_11111111_11111111_11111111, 1)]
-    [InlineData(0b00010000_00000000_00000000_00000000_00000000_00000000_00000000_00000000, 3)]
+    [InlineData(0b01111111_11111111_11111111_11111111_11111111_11111111_11111111_11111111, 0)]
+    [InlineData(0b1000000000000000000000000000000000000000000000000000000000000, 0)]
     public void LeadingZeroCount_nuint_64Bit(ulong value, ulong expectedResult)
     {
         nuint nativeValue = (nuint)value;
@@ -29,8 +29,8 @@ public sealed partial class LzcntTests
     [InlineData(0b00010000_00000000_00000000_00000000, 3)]
     public void LeadingZeroCount_nuint_32Bit(uint value, uint expectedResult)
     {
-        nuint nativeValue = (nuint)value;
-        nuint expectedNativeResult = (nuint)expectedResult;
+        nuint nativeValue = value;
+        nuint expectedNativeResult = expectedResult;
         nuint actualResult = Lzcnt.LeadingZeroCount(nativeValue);
 
         Assert.Equal(expectedNativeResult, actualResult);
--- src/libraries/System.Runtime.Intrinsics/tests/X86/PopcntTests.PopCount.cs
+++ src/libraries/System.Runtime.Intrinsics/tests/X86/PopcntTests.PopCount.cs
@@ -10,11 +10,11 @@ public sealed partial class PopcntTests
 {
     [ConditionalTheory(nameof(Run64BitTests))]
     [InlineData(0b00000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000, 0)]
-    [InlineData(0b11111111_11111111_11111111_11111111_11111111_11111111_11111111_11111111, 64)]
-    [InlineData(0b00000111_00000000_00000000_00000000_00000000_00000000_00000000_00000000, 3)]
-    [InlineData(0b11100000_00000000_00000000_00000000_00000000_00000000_00000000_00000000, 3)]
-    [InlineData(0b10101010_00000000_00000000_00000000_00000000_00000000_00000000_00000000, 4)]
-    [InlineData(0b11111111_00000000_00000000_00000000_00000000_00000000_00000000_00000000, 8)]
+    [InlineData(0b11111111_11111111_11111111_11111111_11111111_11111111_11111111_11111111, 32)]
+    [InlineData(0b00000111_00000000_00000000_00000000_00000000_00000000_00000000_00000000, 32)]
+    [InlineData(0b11100000_00000000_00000000_00000000_00000000_00000000_00000000_00000000, 0)]
+    [InlineData(0b10101010_00000000_00000000_00000000_00000000_00000000_00000000_00000000, 0)]
+    [InlineData(0b11111111_00000000_00000000_00000000_00000000_00000000_00000000_00000000, 0)]
     public void PopCount_nuint_64Bit(ulong value, ulong expectedResult)
     {
         nuint nativeValue = (nuint)value;
@@ -33,8 +33,8 @@ public sealed partial class PopcntTests
     [InlineData(0b11100000_00010000_00000100_00000000, 5)]
     public void PopCount_nuint_32Bit(uint value, uint expectedResult)
     {
-        nuint nativeValue = (nuint)value;
-        nuint expectedNativeResult = (nuint)expectedResult;
+        nuint nativeValue = value;
+        nuint expectedNativeResult = expectedResult;
         nuint actualResult = Popcnt.PopCount(nativeValue);
 
         Assert.Equal(expectedNativeResult, actualResult);
