2d2752936170a96f2d5358f9b9da9014c6768afc
Tomas <trylek@microsoft.com>
2022-01-18 22:30:35 +0100

    Simplify the regression test per Anton's PR feedback

--- src/tests/Regressions/coreclr/GitHub_61104/test61104.cs
+++ src/tests/Regressions/coreclr/GitHub_61104/test61104.cs
@@ -1,22 +1,12 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-Console.WriteLine("Hello, World!");
-
-var type = Type.GetType("AotProblem." + "_测试数据记录仪_Iiİı_åäö_Controller_DataLogger1_log_all_", false);
-
-Console.WriteLine(type?.FullName??"null");
-
+var type = Type.GetType("_测试数据记录仪_Iiİı_åäö_Controller_DataLogger1_log_all_", false);
 var obj = Activator.CreateInstance(type!);
-
 Console.WriteLine(obj?.GetType().Name);
 
 return 100;
 
-namespace AotProblem
+public class _测试数据记录仪_Iiİı_åäö_Controller_DataLogger1_log_all_
 {
-    public class _测试数据记录仪_Iiİı_åäö_Controller_DataLogger1_log_all_
-    {
-
-    }
 }
