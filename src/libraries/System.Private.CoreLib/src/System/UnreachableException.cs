3c43140b114fe7090cd78e4f9240953990eeb2c1
Bar Arnon <i3arnon@gmail.com>
2022-01-18 14:37:11 +0200

    Add XML docs

--- src/libraries/System.Private.CoreLib/src/System/UnreachableException.cs
+++ src/libraries/System.Private.CoreLib/src/System/UnreachableException.cs
@@ -3,18 +3,36 @@
 
 namespace System
 {
+    /// <summary>
+    /// Exception thrown when the program executes an instruction that was thought to be unreachable.
+    /// </summary>
     public sealed class UnreachableException : SystemException
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="System.UnreachableException"/> class with the default error message.
+        /// </summary>
         public UnreachableException()
             : base(SR.Arg_UnreachableException)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="System.UnreachableException"/>
+        /// class with a specified error message.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
         public UnreachableException(string? message)
             : base(message)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="System.UnreachableException"/>
+        /// class with a specified error message and a reference to the inner exception that is the cause of
+        /// this exception.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception.</param>
         public UnreachableException(string? message, Exception? innerException)
             : base(message, innerException)
         {
