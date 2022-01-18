faf6297ce20ce40991fc311fa697d40f5789aac3
Tomas <trylek@microsoft.com>
2022-01-18 21:53:30 +0100

    Fix issue #61104 and add regression test
    The issue tracks the runtime regression failure where
    Crossgen2-compiled app is unable to locate a type with non-ASCII
    characters in its name. The failure was caused by the fact that
    Crossgen2 was incorrectly zero-extended the individual characters
    when calculating the hash whereas runtime is sign-extending them.
    
    Thanks
    
    Tomas

---
+++ src/tests/Regressions/coreclr/GitHub_61104/test61104.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+Console.WriteLine("Hello, World!");
+
+var type = Type.GetType("AotProblem." + "_测试数据记录仪_Iiİı_åäö_Controller_DataLogger1_log_all_", false);
+
+Console.WriteLine(type?.FullName??"null");
+
+var obj = Activator.CreateInstance(type!);
+
+Console.WriteLine(obj?.GetType().Name);
+
+return 100;
+
+namespace AotProblem
+{
+    public class _测试数据记录仪_Iiİı_åäö_Controller_DataLogger1_log_all_
+    {
+
+    }
+}
