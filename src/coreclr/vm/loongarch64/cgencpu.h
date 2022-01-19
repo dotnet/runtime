5798329219f4996cb3b3c08a8c4695dfac032847
qiaopengcheng <qiaopengcheng-hf@loongson.cn>
2022-01-19 09:45:57 +0800

    [LoongArch64] update the version of the `LICENSE description`.

--- src/coreclr/vm/loongarch64/cgencpu.h
+++ src/coreclr/vm/loongarch64/cgencpu.h
@@ -1,11 +1,5 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
-
-// Copyright (c) Loongson Technology. All rights reserved.
-
-//
-
 
 #ifndef TARGET_LOONGARCH64
 #error Should only include "cgencpu.h" for LOONGARCH64 builds
