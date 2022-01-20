6fbbc90bec6ee5723e92c3eff4b0e0d16d17270f
vdjuric <vladimir@LEGION21.localdomain>
2022-01-20 22:48:27 +0100

    Adding socket receive timeout to native method + functional test

--- src/libraries/System.Net.NetworkInformation/tests/FunctionalTests/NetworkAddressChangedTests.cs
+++ src/libraries/System.Net.NetworkInformation/tests/FunctionalTests/NetworkAddressChangedTests.cs
@@ -2,6 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Xunit;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace System.Net.NetworkInformation.Tests
 {
@@ -22,6 +25,57 @@ namespace System.Net.NetworkInformation.Tests
         public void NetworkAddressChanged_JustRemove_Success()
         {
             NetworkChange.NetworkAddressChanged -= _addressHandler;
+        }
+
+        [PlatformSpecific(TestPlatforms.Linux)]
+        [Fact]
+        public async void NetworkAddressChanged_AddRemoveMultipleTimes_CheckForLeakingThreads()
+        {
+            static async Task<int> GetNumberOfNetworkAddressChangeThreadsAsync()
+            {
+                int pid = Process.GetCurrentProcess().Id;
+                ProcessStartInfo psi = new ProcessStartInfo("ps", $"-T -p {pid}");
+                psi.RedirectStandardOutput = true;
+
+                using Process process = Process.Start(psi);
+                if (process == null)
+                {
+                    throw new Exception("Could not create process 'ps'");
+                }
+
+                int threadCounter = 0;
+                string output = await process.StandardOutput.ReadToEndAsync();
+                using StringReader sr = new StringReader(output);
+                while (true)
+                {
+                    string? line = await sr.ReadLineAsync();
+                    if (line == null)
+                    {
+                        break;
+                    }
+
+                    if (line.IndexOf(".NET Network Ad") > 0)
+                    {
+                        //We are searching for threads containing ".NET Network Ad"
+                        //because ps command trims actual thread name ".NET Network Address Change".
+                        //This thread is created in:
+                        //  src/libraries/System.Net.NetworkInformation/src/System/Net/NetworkInformation/NetworkAddressChange.Unix.cs
+                        threadCounter++;
+                    }
+                }
+
+                return threadCounter;
+            }
+
+            for (int i = 1; i <= 10; i++)
+            {
+                NetworkChange.NetworkAddressChanged += _addressHandler;
+                NetworkChange.NetworkAddressChanged -= _addressHandler;
+            }
+
+            await Task.Delay(2000); //allow some time for threads to exit
+            int numberOfNetworkAddressChangeThreads = await GetNumberOfNetworkAddressChangeThreadsAsync();
+            Assert.Equal(0, numberOfNetworkAddressChangeThreads); //there should be no threads because there are no event subscribers
         }
     }
 }
