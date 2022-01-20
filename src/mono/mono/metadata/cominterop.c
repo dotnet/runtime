fce5bb7d3d697150b8b0129c5100389b1d40eb9d
lateralusX <lateralusx.github@gmail.com>
2022-01-20 17:38:13 +0100

    Fix native custom marshaler logic, passing ICustomMarshaler runtime test.

--- src/mono/mono/metadata/cominterop.c
+++ src/mono/mono/metadata/cominterop.c
@@ -2104,7 +2104,7 @@ cominterop_get_ccw_method (MonoClass *iface, MonoMethod *method, MonoError *erro
 	cominterop_setup_marshal_context (&m, adjust_method);
 	m.mb = mb;
 	m.runtime_marshalling_enabled = TRUE;
-	mono_marshal_emit_managed_wrapper (mb, sig_adjusted, mspecs, &m, adjust_method, 0);
+	mono_marshal_emit_managed_wrapper (mb, sig_adjusted, mspecs, &m, adjust_method, 0, NULL);
 	mono_cominterop_lock ();
 	wrapper_method = mono_mb_create_method (mb, m.csig, m.csig->param_count + 16);
 	mono_cominterop_unlock ();
