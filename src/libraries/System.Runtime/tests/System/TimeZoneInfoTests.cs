ca04d45ed6522d0cbd64eb27032831cef8f5354a
mdh1418 <mitchhwang1418@gmail.com>
2022-01-20 20:02:27 -0500

    Temporarily show timezone displayNames in NoBackwardTimeZones

--- src/libraries/System.Runtime/tests/System/TimeZoneInfoTests.cs
+++ src/libraries/System.Runtime/tests/System/TimeZoneInfoTests.cs
@@ -2881,7 +2881,12 @@ namespace System.Tests
 
             foreach (TimeZoneInfo timezone in tzCollection)
             {
+                Console.WriteLine($"tzCollection: {timezone.DisplayName}");
                 tzDisplayNames.Add(timezone.DisplayName);
+            }
+            foreach (string timezone in tzDisplayNames)
+            {
+                Console.WriteLine($"tzDisplayNames: {timezone}");
             }
             Assert.Equal(tzCollection.Count, tzDisplayNames.Count);
         }
