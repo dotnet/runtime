b75b558e12176750b386c263986ec3f76f938ca6
Tanner Gooding <tagoo@outlook.com>
2022-01-20 17:23:24 -0800

    Allow CreateScalarUnsafe to be directly contained by hwintrinsics that support scalar loads (#62407)
    * Ensure that floating-point constants can be contained by hardware intrinsics
    
    * Allow CreateScalarUnsafe to be directly contained by hwintrinsics that support scalar loads
    
    * Rename IsContainableHWIntrinsicOp to TryGetContainableHWIntrinsicOp and improve handling
    
    * Ensure that NI_AVX2_BroadcastScalarToVector128/256 are properly tracked as MaybeMemoryLoad
    
    * Applying formatting patch
    
    * Ensure a few other "maybe memory" and special memory operand size cases are handled
    
    * Applying formatting patch

--- src/coreclr/jit/gentree.cpp
+++ src/coreclr/jit/gentree.cpp
@@ -21414,7 +21414,9 @@ GenTreeHWIntrinsic* Compiler::gtNewScalarHWIntrinsicNode(
 bool GenTreeHWIntrinsic::OperIsMemoryLoad() const
 {
 #if defined(TARGET_XARCH) || defined(TARGET_ARM64)
-    HWIntrinsicCategory category = HWIntrinsicInfo::lookupCategory(GetHWIntrinsicId());
+    NamedIntrinsic      intrinsicId = GetHWIntrinsicId();
+    HWIntrinsicCategory category    = HWIntrinsicInfo::lookupCategory(intrinsicId);
+
     if (category == HW_Category_MemoryLoad)
     {
         return true;
@@ -21423,17 +21425,42 @@ bool GenTreeHWIntrinsic::OperIsMemoryLoad() const
     else if (HWIntrinsicInfo::MaybeMemoryLoad(GetHWIntrinsicId()))
     {
         // Some intrinsics (without HW_Category_MemoryLoad) also have MemoryLoad semantics
+        // This is generally because they have both vector and pointer overloads, e.g.,
+        // * Vector128<byte> BroadcastScalarToVector128(Vector128<byte> value)
+        // * Vector128<byte> BroadcastScalarToVector128(byte* source)
+        // So, we need to check the argument's type is memory-reference or Vector128
 
-        if (category == HW_Category_SIMDScalar)
+        if ((category == HW_Category_SimpleSIMD) || (category == HW_Category_SIMDScalar))
         {
-            // Avx2.BroadcastScalarToVector128/256 have vector and pointer overloads both, e.g.,
-            // Vector128<byte> BroadcastScalarToVector128(Vector128<byte> value)
-            // Vector128<byte> BroadcastScalarToVector128(byte* source)
-            // So, we need to check the argument's type is memory-reference or Vector128
             assert(GetOperandCount() == 1);
-            return (GetHWIntrinsicId() == NI_AVX2_BroadcastScalarToVector128 ||
-                    GetHWIntrinsicId() == NI_AVX2_BroadcastScalarToVector256) &&
-                   !Op(1)->TypeIs(TYP_SIMD16);
+
+            switch (intrinsicId)
+            {
+                case NI_SSE41_ConvertToVector128Int16:
+                case NI_SSE41_ConvertToVector128Int32:
+                case NI_SSE41_ConvertToVector128Int64:
+                case NI_AVX2_BroadcastScalarToVector128:
+                case NI_AVX2_BroadcastScalarToVector256:
+                case NI_AVX2_ConvertToVector256Int16:
+                case NI_AVX2_ConvertToVector256Int32:
+                case NI_AVX2_ConvertToVector256Int64:
+                {
+                    CorInfoType auxiliaryType = GetAuxiliaryJitType();
+
+                    if (auxiliaryType == CORINFO_TYPE_PTR)
+                    {
+                        return true;
+                    }
+
+                    assert(auxiliaryType == CORINFO_TYPE_UNDEF);
+                    return false;
+                }
+
+                default:
+                {
+                    unreached();
+                }
+            }
         }
         else if (category == HW_Category_IMM)
         {
