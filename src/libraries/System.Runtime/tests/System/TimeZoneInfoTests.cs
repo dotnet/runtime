09e71d20c99f2acd3dfda22becc359f9691d5f83
mdh1418 <mitchhwang1418@gmail.com>
2022-01-21 18:59:32 -0500

    Skip test with TestPlatforms instead

--- src/libraries/System.Runtime/tests/System/TimeZoneInfoTests.cs
+++ src/libraries/System.Runtime/tests/System/TimeZoneInfoTests.cs
@@ -2922,7 +2922,7 @@ namespace System.Tests
         }
 
         [Fact]
-        [ActiveIssue("https://github.com/dotnet/runtime/issues/64111", typeof(PlatformDetection), nameof(PlatformDetection.IsLinux), nameof(PlatformDetection.IsArm64Process))]
+        [ActiveIssue("https://github.com/dotnet/runtime/issues/64111", TestPlatforms.Linux)]
         public static void NoBackwardTimeZones()
         {
             ReadOnlyCollection<TimeZoneInfo> tzCollection = TimeZoneInfo.GetSystemTimeZones();
