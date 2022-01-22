d6e7c0fda845bec01de47aa868b8ae1115be166c
Aaron Robinson <arobins@microsoft.com>
2022-01-21 18:47:10 -0800

    Instead of dropping the byref, compute a token from a typespec.

--- src/coreclr/System.Private.CoreLib/src/System/Reflection/Emit/ModuleBuilder.cs
+++ src/coreclr/System.Private.CoreLib/src/System/Reflection/Emit/ModuleBuilder.cs
@@ -222,8 +222,6 @@ namespace System.Reflection.Emit
 
         private int GetTypeRefNested(Type type, Module? refedModule, string? strRefedModuleFileName)
         {
-            type = type.IsByRef ? type.GetElementType()! : type;
-
             // This function will generate correct TypeRef token for top level type and nested type.
             Type? enclosingType = type.DeclaringType;
             int tkResolution = 0;
@@ -1082,14 +1080,16 @@ namespace System.Reflection.Emit
             // instructions.  Tokens are always relative to the Module.  For example,
             // the token value for System.String is likely to be different from
             // Module to Module.  Calling GetTypeToken will cause a reference to be
-            // added to the Module.  This reference becomes a perminate part of the Module,
-            // multiple calles to this method with the same class have no additional side affects.
-            // This function is optimized to use the TypeDef token if Type is within the same module.
-            // We should also be aware of multiple dynamic modules and multiple implementation of Type!!!
-            if ((type.IsGenericType && (!type.IsGenericTypeDefinition || !getGenericDefinition)) ||
-                type.IsGenericParameter ||
-                type.IsArray ||
-                type.IsPointer)
+            // added to the Module.  This reference becomes a permanent part of the Module,
+            // multiple calls to this method with the same class have no additional side-effects.
+            // This function is optimized to use the TypeDef token if the Type is within the
+            // same module. We should also be aware of multiple dynamic modules and multiple
+            // implementations of a Type.
+            if ((type.IsGenericType && (!type.IsGenericTypeDefinition || !getGenericDefinition))
+                || type.IsGenericParameter
+                || type.IsArray
+                || type.IsPointer
+                || type.IsByRef)
             {
                 byte[] sig = SignatureHelper.GetTypeSigToken(this, type).InternalGetSignature(out int length);
                 return GetTokenFromTypeSpec(sig, length);
