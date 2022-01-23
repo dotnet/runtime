1f2116cd60ec0f3e39abbf60d7feedc537e5dfd3
Kevin Jones <kevin@vcsjones.com>
2022-01-23 18:23:24 -0500

    Implement hash and HMAC stream one shots
    This implements hashing and HMAC statics for streams. Additionally,
    "LiteHmac" and "LiteHash" were introduced. The existing HMAC and hash
    provider functionality do some bookkeeping we don't need for resetting.
    Since we do not need to use these hash handles after the digest has
    been finalized, resetting is unnecessary work. For HMAC, that also means
    keeping a copy of the key around for some implementations which we don't
    need to do.
    
    The LiteHash and LiteHmac types are implemented as structs with a common
    interface. To avoid boxing, generics are used and constrained to the interface
    where possible.
    
    The Browser implementation just defers to the existing HashDispenser rather
    than do anything novel.
    
    The HashProviderCng is somewhat specialized in its ability to reset. It did
    up-front check to determine if the platform supported reusable hash providers,
    and further had a single implementation for HMAC and Digests. The current
    Lite hash design requires that they remain separate types.

--- src/libraries/Common/src/Interop/OSX/System.Security.Cryptography.Native.Apple/Interop.Hmac.cs
+++ src/libraries/Common/src/Interop/OSX/System.Security.Cryptography.Native.Apple/Interop.Hmac.cs
@@ -16,7 +16,15 @@ internal static partial class Interop
         internal static partial SafeHmacHandle HmacCreate(PAL_HashAlgorithm algorithm, ref int cbDigest);
 
         [GeneratedDllImport(Libraries.AppleCryptoNative, EntryPoint = "AppleCryptoNative_HmacInit")]
-        internal static partial int HmacInit(SafeHmacHandle ctx, byte[] pbKey, int cbKey);
+        private static unsafe partial int HmacInit(SafeHmacHandle ctx, byte* pbKey, int cbKey);
+
+        internal static unsafe int HmacInit(SafeHmacHandle ctx, ReadOnlySpan<byte> key)
+        {
+            fixed (byte* pKey = &MemoryMarshal.GetReference(key))
+            {
+                return HmacInit(ctx, pKey, key.Length);
+            }
+        }
 
         internal static int HmacUpdate(SafeHmacHandle ctx, ReadOnlySpan<byte> data) =>
             HmacUpdate(ctx, ref MemoryMarshal.GetReference(data), data.Length);
