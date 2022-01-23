afd9caaaf7a696521a97a4d545835fdeadcf9c59
Vladimir Djurić <vladimir@djuric.si>
2022-01-23 11:11:28 +0100

    Update src/native/libs/System.Native/pal_networkchange.c
    ManickaP replaced tabs with spaces
    
    Co-authored-by: Marie Píchová <11718369+ManickaP@users.noreply.github.com>

--- src/native/libs/System.Native/pal_networkchange.c
+++ src/native/libs/System.Native/pal_networkchange.c
@@ -35,14 +35,14 @@ Error SystemNative_CreateNetworkChangeListenerSocket(int32_t* retSocket)
     sa.nl_family = AF_NETLINK;
     sa.nl_groups = RTMGRP_LINK | RTMGRP_IPV4_IFADDR | RTMGRP_IPV4_ROUTE | RTMGRP_IPV6_ROUTE;
     int32_t sock = socket(AF_NETLINK, SOCK_RAW, NETLINK_ROUTE);
-	
-	// Added receive timeout to prevent recvmsg method in SystemNative_ReadEvents to block thread indefinitely.
-	// This allows method SystemNative_ReadEvents to periodically exit and allows .NET thread to exit if needed.
-	struct timeval tv;
-	tv.tv_sec = 1; //timeout in seconds
-	tv.tv_usec = 0;
-	setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof tv);
-	
+
+    // Added receive timeout to prevent recvmsg method in SystemNative_ReadEvents to block thread indefinitely.
+    // This allows method SystemNative_ReadEvents to periodically exit and allows .NET thread to exit if needed.
+    struct timeval tv;
+    tv.tv_sec = 1; //timeout in seconds
+    tv.tv_usec = 0;
+    setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof tv);
+
 #elif HAVE_RT_MSGHDR
     int32_t sock = socket(PF_ROUTE, SOCK_RAW, 0);
 #endif
