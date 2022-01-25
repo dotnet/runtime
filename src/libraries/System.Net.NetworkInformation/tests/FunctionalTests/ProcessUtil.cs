69f7106e734816b335cc45b0565dcc65061b7879
vdjuric <vladimir@djuric.si>
2022-01-25 10:50:48 +0100

    Test method update due to some failing tests

--- src/libraries/System.Net.NetworkInformation/tests/FunctionalTests/ProcessUtil.cs
+++ src/libraries/System.Net.NetworkInformation/tests/FunctionalTests/ProcessUtil.cs
@@ -9,34 +9,63 @@ namespace System.Net.NetworkInformation.Tests
 {
     internal static class ProcessUtil
     {
-        public static IEnumerable<string> GetProcessThreadsWithPsCommand(int pid)
+        public static IEnumerable<string> GetProcessThreadsWithPsCommand(int pid, int? timeoutInMilliseconds = null)
         {
             ProcessStartInfo psi = new ProcessStartInfo("ps", $"-T --no-headers -p {pid}");
             psi.RedirectStandardOutput = true;
 
-            using Process process = Process.Start(psi);
+            Process? process;
+
+            try
+            {
+                process = Process.Start(psi);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Exception while trying to run 'ps' command for user '{Environment.UserName}'", ex);
+            }
+
             if (process == null)
             {
                 throw new Exception("Could not create process 'ps'");
             }
 
-            string output = process.StandardOutput.ReadToEnd();
-            if (process.ExitCode != 0)
+            try
             {
-                throw new Exception($"Process 'ps' returned exit code {process.ExitCode}");
-            }
-
-            using StringReader sr = new StringReader(output);
-
-            while (true)
-            {
-                string? line = sr.ReadLine();
-                if (line == null)
+                if (timeoutInMilliseconds.HasValue)
                 {
-                    break;
+                    if (!process.WaitForExit(timeoutInMilliseconds.Value))
+                    {
+                        throw new Exception($"Process 'ps' did not exit after {timeoutInMilliseconds} milliseconds");
+                    }
+                }
+                else
+                {
+                    process.WaitForExit();
                 }
 
-                yield return line;
+                string output = process.StandardOutput.ReadToEnd();
+                if (process.ExitCode != 0)
+                {
+                    throw new Exception($"Process 'ps' returned exit code {process.ExitCode}");
+                }
+
+                using StringReader sr = new StringReader(output);
+
+                while (true)
+                {
+                    string? line = sr.ReadLine();
+                    if (line == null)
+                    {
+                        break;
+                    }
+
+                    yield return line;
+                }
+            }
+            finally
+            {
+                process.Dispose();
             }
         }
     }
