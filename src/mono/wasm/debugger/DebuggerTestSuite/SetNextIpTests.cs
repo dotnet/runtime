a4c06c01837e8f0e50a9b1bd2b0693b50bd25037
DESKTOP-GEPIA6N\Thays <thaystg@gmail.com>
2022-01-31 13:33:36 -0300

    Addressing @radical comments.

---
+++ src/mono/wasm/debugger/DebuggerTestSuite/SetNextIpTests.cs
@@ -0,0 +1,210 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.WebAssembly.Diagnostics;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using Xunit;
+using System.Threading;
+
+namespace DebuggerTests;
+
+
+public class SetNextIpTests : DebuggerTestBase
+{
+    [Fact]
+    public async Task SetNextIP()
+    {
+        var bp = await SetBreakpoint("dotnet://debugger-test.dll/debugger-test.cs", 9, 8);
+
+        var pause_location = await EvaluateAndCheck(
+            "window.setTimeout(function() { invoke_add(); invoke_add(); }, 1);",
+            "dotnet://debugger-test.dll/debugger-test.cs", 9, 8,
+            "IntAdd",
+            wait_for_event_fn: async (pause_location) =>
+            {
+                Assert.Equal("other", pause_location["reason"]?.Value<string>());
+                Assert.Equal(bp.Value["breakpointId"]?.ToString(), pause_location["hitBreakpoints"]?[0]?.Value<string>());
+
+                var top_frame = pause_location["callFrames"][0];
+                Assert.Equal("IntAdd", top_frame["functionName"].Value<string>());
+                Assert.Contains("debugger-test.cs", top_frame["url"].Value<string>());
+
+                CheckLocation("dotnet://debugger-test.dll/debugger-test.cs", 8, 4, scripts, top_frame["functionLocation"]);
+                await Task.CompletedTask;
+            }
+        );
+        var top_frame = pause_location["callFrames"][0]["functionLocation"];
+        await SetNextIPAndCheck(top_frame["scriptId"].Value<string>(), "dotnet://debugger-test.dll/debugger-test.cs", 12, 8, "IntAdd",
+        locals_fn: async (locals) =>
+            {
+                CheckNumber(locals, "c", 0);
+                CheckNumber(locals, "d", 0);
+                CheckNumber(locals, "e", 0);
+                await CheckBool(locals, "f", false);
+            });
+        await StepAndCheck(StepKind.Over, "dotnet://debugger-test.dll/debugger-test.cs", 13, 8, "IntAdd",
+        locals_fn: async (locals) =>
+            {
+                CheckNumber(locals, "c", 0);
+                CheckNumber(locals, "d", 0);
+                CheckNumber(locals, "e", 0);
+                await CheckBool(locals, "f", true);
+            });
+        await SetNextIPAndCheck(top_frame["scriptId"].Value<string>(), "dotnet://debugger-test.dll/debugger-test.cs", 9, 8, "IntAdd",
+        locals_fn: async (locals) =>
+            {
+                CheckNumber(locals, "c", 0);
+                CheckNumber(locals, "d", 0);
+                CheckNumber(locals, "e", 0);
+                await CheckBool(locals, "f", true);
+            });
+        await StepAndCheck(StepKind.Over, "dotnet://debugger-test.dll/debugger-test.cs", 10, 8, "IntAdd",
+        locals_fn: async (locals) =>
+            {
+                CheckNumber(locals, "c", 30);
+                CheckNumber(locals, "d", 0);
+                CheckNumber(locals, "e", 0);
+                await CheckBool(locals, "f", true);
+            });
+        await SetNextIPAndCheck(top_frame["scriptId"].Value<string>(), "dotnet://debugger-test.dll/debugger-test.cs", 11, 8, "IntAdd",
+        locals_fn: async (locals) =>
+            {
+                CheckNumber(locals, "c", 30);
+                CheckNumber(locals, "d", 0);
+                CheckNumber(locals, "e", 0);
+                await CheckBool(locals, "f", true);
+            });
+        await StepAndCheck(StepKind.Over, "dotnet://debugger-test.dll/debugger-test.cs", 12, 8, "IntAdd",
+        locals_fn: async (locals) =>
+            {
+                CheckNumber(locals, "c", 30);
+                CheckNumber(locals, "d", 0);
+                CheckNumber(locals, "e", 10);
+                await CheckBool(locals, "f", true);
+            });
+
+        //to check that after moving the execution pointer to the same line that there is already
+        //a breakpoint, the breakpoint continue working
+        pause_location = await StepAndCheck(StepKind.Resume,
+            "dotnet://debugger-test.dll/debugger-test.cs", 9, 8,
+            "IntAdd",
+            wait_for_event_fn: async (pause_location) =>
+            {
+                Assert.Equal("other", pause_location["reason"]?.Value<string>());
+                Assert.Equal(bp.Value["breakpointId"]?.ToString(), pause_location["hitBreakpoints"]?[0]?.Value<string>());
+
+                var top_frame = pause_location["callFrames"][0];
+                Assert.Equal("IntAdd", top_frame["functionName"].Value<string>());
+                Assert.Contains("debugger-test.cs", top_frame["url"].Value<string>());
+
+                CheckLocation("dotnet://debugger-test.dll/debugger-test.cs", 8, 4, scripts, top_frame["functionLocation"]);
+                await Task.CompletedTask;
+            }
+        );
+    }
+
+    [Fact]
+    public async Task SetNextIPOutsideTheCurrentMethod()
+    {
+        var bp = await SetBreakpoint("dotnet://debugger-test.dll/debugger-test.cs", 9, 8);
+
+        var pause_location = await EvaluateAndCheck(
+            "window.setTimeout(function() { invoke_add(); }, 1);",
+            "dotnet://debugger-test.dll/debugger-test.cs", 9, 8,
+            "IntAdd",
+            wait_for_event_fn: async (pause_location) =>
+            {
+                Assert.Equal("other", pause_location["reason"]?.Value<string>());
+                Assert.Equal(bp.Value["breakpointId"]?.ToString(), pause_location["hitBreakpoints"]?[0]?.Value<string>());
+
+                var top_frame = pause_location["callFrames"][0];
+                Assert.Equal("IntAdd", top_frame["functionName"].Value<string>());
+                Assert.Contains("debugger-test.cs", top_frame["url"].Value<string>());
+
+                CheckLocation("dotnet://debugger-test.dll/debugger-test.cs", 8, 4, scripts, top_frame["functionLocation"]);
+                await Task.CompletedTask;
+            }
+        );
+        var top_frame = pause_location["callFrames"][0]["functionLocation"];
+        await SetNextIPAndCheck(top_frame["scriptId"].Value<string>(), "dotnet://debugger-test.dll/debugger-test.cs", 20, 8, "IntAdd",
+        expected_error: true);
+        await StepAndCheck(StepKind.Over, "dotnet://debugger-test.dll/debugger-test.cs", 10, 8, "IntAdd",
+        locals_fn: async (locals) =>
+            {
+                CheckNumber(locals, "c", 30);
+                CheckNumber(locals, "d", 0);
+                CheckNumber(locals, "e", 0);
+                await CheckBool(locals, "f", false);
+            });
+    }
+
+    [Fact]
+    public async Task SetNextIP_AsyncMethod()
+    {
+        var debugger_test_loc = "dotnet://debugger-test.dll/debugger-test.cs";
+
+        await SetBreakpoint(debugger_test_loc, 140, 12);
+
+        var pause_location = await EvaluateAndCheck(
+            "window.setTimeout(function() { invoke_async_method_with_await(); }, 1);",
+            debugger_test_loc, 140, 12, "MoveNext",
+            locals_fn: async (locals) =>
+            {
+                CheckNumber(locals, "li", 0);
+                CheckNumber(locals, "i", 42);
+                await CheckString(locals, "ls", null);
+            }
+        );
+        var top_frame = pause_location["callFrames"][0]["functionLocation"];
+        await SetNextIPAndCheck(top_frame["scriptId"].Value<string>(), "dotnet://debugger-test.dll/debugger-test.cs", 141, 12, "MoveNext",
+        locals_fn: async (locals) =>
+            {
+                CheckNumber(locals, "li", 0);
+                CheckNumber(locals, "i", 42);
+                await CheckString(locals, "ls", null);
+            });
+        await StepAndCheck(StepKind.Over, "dotnet://debugger-test.dll/debugger-test.cs", 142, 12, "MoveNext",
+        locals_fn: async (locals) =>
+            {
+                CheckNumber(locals, "li", 0);
+                CheckNumber(locals, "i", 42);
+                await CheckString(locals, "ls", "string from jstest");
+            });
+    }
+
+    [Fact]
+        public async Task SetNextIP_Lambda()
+        {
+        var debugger_test_loc = "dotnet://debugger-test.dll/debugger-async-test.cs";
+
+        await SetBreakpoint(debugger_test_loc, 77, 12);
+        var pause_location = await EvaluateAndCheck(
+        "window.setTimeout(function() { invoke_static_method('[debugger-test] DebuggerTests.AsyncTests.ContinueWithTests:RunAsync'); })",
+        debugger_test_loc, 77, 12, "MoveNext",
+        locals_fn: async (locals) =>
+        {
+            await CheckString(locals, "str", "foobar");
+            await CheckValueType(locals, "code", "System.Threading.Tasks.TaskStatus", description: "Created");
+            await CheckValueType(locals, "dt0", "System.DateTime", description: "1/1/0001 12:00:00 AM");
+        });
+        var top_frame = pause_location["callFrames"][0]["functionLocation"];
+        await SetNextIPAndCheck(top_frame["scriptId"].Value<string>(), "dotnet://debugger-test.dll/debugger-async-test.cs", 79, 16, "MoveNext",
+        locals_fn: async (locals) =>
+            {
+                await CheckString(locals, "str", "foobar");
+                await CheckValueType(locals, "code", "System.Threading.Tasks.TaskStatus", description: "Created");
+                await CheckValueType(locals, "dt0", "System.DateTime", description: "1/1/0001 12:00:00 AM");
+            });
+        await StepAndCheck(StepKind.Over, "dotnet://debugger-test.dll/debugger-async-test.cs", 80, 16, "MoveNext",
+        locals_fn: async (locals) =>
+            {
+                await CheckString(locals, "str", "foobar");
+                await CheckValueType(locals, "code", "System.Threading.Tasks.TaskStatus", description: "Created");
+                await CheckDateTime(locals, "dt0", new DateTime(3412, 4, 6, 8, 0, 2));
+            });
+        }
+}
