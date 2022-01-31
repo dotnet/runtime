73669bcd85c40d0f833d481af5df35e4ca49ce0b
Sven Boemer <sbomer@gmail.com>
2022-01-31 23:39:13 +0000

    Fix GetDisplayName for cctors
    Commit migrated from https://github.com/dotnet/linker/commit/47850891f7baa5468046d446ef4770e3d36feef7

--- src/tools/illink/src/ILLink.RoslynAnalyzer/ISymbolExtensions.cs
+++ src/tools/illink/src/ILLink.RoslynAnalyzer/ISymbolExtensions.cs
@@ -101,6 +101,11 @@ namespace ILLink.RoslynAnalyzer
 				sb.Append (parameterSymbol.Name);
 				break;
 
+			case IMethodSymbol methodSymbol when methodSymbol.IsStaticConstructor ():
+				sb.Append (symbol.ContainingType.ToDisplayString (ILLinkTypeDisplayFormat));
+				sb.Append ("..cctor()");
+				break;
+
 			case IMethodSymbol methodSymbol:
 				// Format the declaring type with namespace and containing types.
 				if (methodSymbol.ContainingSymbol.Kind == SymbolKind.NamedType) {
@@ -115,11 +120,7 @@ namespace ILLink.RoslynAnalyzer
 				break;
 
 			default:
-				if (symbol.IsStaticConstructor ()) {
-					sb.Append (symbol.ContainingType.ToDisplayString ());
-					sb.Append ("..cctor()");
-				} else
-					sb.Append (symbol.ToDisplayString ());
+				sb.Append (symbol.ToDisplayString ());
 				break;
 			}
 
