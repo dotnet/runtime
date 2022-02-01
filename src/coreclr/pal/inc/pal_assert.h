863e46dfddfded9b4c7477eb46e4243616616fe9
Andrii Kurdiumov <kant2002@gmail.com>
2022-02-01 23:11:36 +0600

    Fix pal_assert.h for PAL_STDCPP_COMPAT Improves https://github.com/dotnet/runtime/issues/37310

--- src/coreclr/pal/inc/pal_assert.h
+++ src/coreclr/pal/inc/pal_assert.h
@@ -34,7 +34,7 @@ extern "C" {
 #if defined(_DEBUG)
 #define _ASSERTE(e) do {                                        \
         if (!(e)) {                                             \
-            fprintf (stderr,                                    \
+            PAL_fprintf (PAL_get_stderr(PAL_get_caller),        \
                      "ASSERT FAILED\n"                          \
                      "\tExpression: %s\n"                       \
                      "\tLocation:   line %d in %s\n"            \
