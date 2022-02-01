178153b0e94d04882dab99fa48546df079878d90
Sven Boemer <sbomer@gmail.com>
2022-02-01 19:18:46 +0000

    Skip failing test with array type generic argument
    Commit migrated from https://github.com/dotnet/linker/commit/40591d33720d2a8efac2cad32b3cf7d4bf8a2acf

--- src/tools/illink/test/ILLink.RoslynAnalyzer.Tests/DataFlowTests.cs
+++ src/tools/illink/test/ILLink.RoslynAnalyzer.Tests/DataFlowTests.cs
@@ -49,7 +49,7 @@ namespace ILLink.RoslynAnalyzer.Tests
 			return RunTest (nameof (ByRefDataflow));
 		}
 
-		[Fact]
+		[Fact (Skip = "https://github.com/dotnet/linker/issues/2574")]
 		public Task ComplexTypeHandling ()
 		{
 			return RunTest (nameof (ComplexTypeHandling));
