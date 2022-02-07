dotnet/runtime

commit 4a6d169f5391532340074b0be4cab737c6ccdfc8

Add runtime support for `ref` fields (#63985)
* Add mono and coreclr runtime support for ref fields

* Update Reflection.Emit tests to validate ref fields.

Add test for TypedReference as a ref field.

src/mono/System.Private.CoreLib/src/System/Reflection/Emit/TypeBuilder.Mono.cs
@@ -74,6 +74,7 @@ namespace System.Reflection.Emit
 
         [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)]
         private TypeInfo? created;
+        private bool is_byreflike_set;
 
         private int state;
 #endregion
@@ -273,8 +274,7 @@ namespace System.Reflection.Emit
         {
             if (interfaceType == null)
                 throw new ArgumentNullException(nameof(interfaceType));
-            if (interfaceType.IsByRef)
-                throw new ArgumentException(SR.Argument_CannotGetTypeTokenForByRef);
+
             check_not_created();
 
             if (interfaces != null)
@@ -838,7 +838,7 @@ namespace System.Reflection.Emit
             if (parent != null)
             {
                 if (parent.IsByRef)
-                    throw new ArgumentException();
+                    throw new NotSupportedException();
                 if (IsInterface)
                     throw new TypeLoadException();
             }
@@ -874,17 +874,6 @@ namespace System.Reflection.Emit
                         throw new TypeLoadException();
                     if (iface is TypeBuilder builder && !builder.is_created)
                         throw new TypeLoadException();
-                }
-            }
-
-            if (fields != null)
-            {
-                foreach (FieldBuilder fb in fields)
-                {
-                    if (fb == null)
-                        continue;
-                    if (fb.FieldType.IsByRef)
-                        throw new COMException();
                 }
             }
 
@@ -1544,6 +1533,10 @@ namespace System.Reflection.Emit
             {
                 attrs |= TypeAttributes.HasSecurity;
             }
+            else if (attrname == "System.Runtime.CompilerServices.IsByRefLikeAttribute")
+            {
+                is_byreflike_set = true;
+            }
 
             if (cattrs != null)
             {
@@ -1571,8 +1564,7 @@ namespace System.Reflection.Emit
             if (eventtype == null)
                 throw new ArgumentNullException(nameof(eventtype));
             check_not_created();
-            if (eventtype.IsByRef)
-                throw new ArgumentException(SR.Argument_CannotGetTypeTokenForByRef);
+
             EventBuilder res = new EventBuilder(this, name, attributes, eventtype);
             if (events != null)
             {

src/mono/mono/metadata/object-internals.h
@@ -1222,6 +1222,7 @@ struct _MonoReflectionTypeBuilder {
 	MonoGenericContainer *generic_container;
 	MonoArray *generic_params;
 	MonoReflectionType *created;
+	gboolean is_byreflike_set;
 	gint32 state;
 };
 

src/mono/mono/metadata/sre.c
@@ -3556,6 +3556,11 @@ typebuilder_setup_one_field (MonoDynamicImage *dynamic_image, MonoClass *klass, 
 			}
 		}
 
+		if (m_type_is_byref (field->type) && !m_class_is_byreflike (klass)) {
+			mono_error_set_type_load_name (error, NULL, NULL, "Field '%s' is a byref in a non-byref-like type", field->name);
+			goto leave;
+		}
+
 		if ((fb->attrs & FIELD_ATTRIBUTE_HAS_FIELD_RVA) && (rva_data = fb->rva_data)) {
 			char *base = mono_array_addr_internal (rva_data, char, 0);
 			size_t size = mono_array_length_internal (rva_data);
@@ -3856,6 +3861,12 @@ ves_icall_TypeBuilder_create_runtime_class (MonoReflectionTypeBuilderHandle ref_
 	klass->supertypes = NULL;
 	mono_class_setup_supertypes (klass);
 	mono_class_setup_mono_type (klass);
+
+	/* Check if the type is marked as byreflike.
+	 * The IsByRefLike attribute only applies to value types and enums. This matches CoreCLR behavior.
+	 */
+	if (klass->enumtype || klass->valuetype)
+		klass->is_byreflike = MONO_HANDLE_GETVAL (MONO_HANDLE_CAST (MonoReflectionTypeBuilder, ref_tb), is_byreflike_set);
 
 	/* enums are done right away */
 	if (!klass->enumtype)

src/coreclr/System.Private.CoreLib/src/System/Reflection/Emit/ModuleBuilder.cs
@@ -233,7 +233,7 @@ namespace System.Reflection.Emit
                 typeName = UnmangleTypeName(typeName);
             }
 
-            Debug.Assert(!type.IsByRef, "Must not be ByRef.");
+            Debug.Assert(!type.IsByRef, "Must not be ByRef. Get token from TypeSpec.");
             Debug.Assert(!type.IsGenericType || type.IsGenericTypeDefinition, "Must not have generic arguments.");
 
             ModuleBuilder thisModule = this;
@@ -1081,19 +1081,16 @@ namespace System.Reflection.Emit
             // instructions.  Tokens are always relative to the Module.  For example,
             // the token value for System.String is likely to be different from
             // Module to Module.  Calling GetTypeToken will cause a reference to be
-            // added to the Module.  This reference becomes a perminate part of the Module,
-            // multiple calles to this method with the same class have no additional side affects.
-            // This function is optimized to use the TypeDef token if Type is within the same module.
-            // We should also be aware of multiple dynamic modules and multiple implementation of Type!!!
-            if (type.IsByRef)
-            {
-                throw new ArgumentException(SR.Argument_CannotGetTypeTokenForByRef);
-            }
-
-            if ((type.IsGenericType && (!type.IsGenericTypeDefinition || !getGenericDefinition)) ||
-                type.IsGenericParameter ||
-                type.IsArray ||
-                type.IsPointer)
+            // added to the Module.  This reference becomes a permanent part of the Module,
+            // multiple calls to this method with the same class have no additional side-effects.
+            // This function is optimized to use the TypeDef token if the Type is within the
+            // same module. We should also be aware of multiple dynamic modules and multiple
+            // implementations of a Type.
+            if ((type.IsGenericType && (!type.IsGenericTypeDefinition || !getGenericDefinition))
+                || type.IsGenericParameter
+                || type.IsArray
+                || type.IsPointer
+                || type.IsByRef)
             {
                 byte[] sig = SignatureHelper.GetTypeSigToken(this, type).InternalGetSignature(out int length);
                 return GetTokenFromTypeSpec(sig, length);

src/coreclr/System.Private.CoreLib/src/System/Reflection/Emit/TypeBuilder.cs
@@ -1922,7 +1922,9 @@ namespace System.Reflection.Emit
 
             int tkParent = 0;
             if (m_typeParent != null)
+            {
                 tkParent = m_module.GetTypeTokenInternal(m_typeParent);
+            }
 
             ModuleBuilder module = m_module;
 

src/coreclr/utilcode/util.cpp
@@ -1806,9 +1806,7 @@ HRESULT validateOneArg(
                 // Validate the referenced type.
                 if(FAILED(hr = validateOneArg(tk, pSig, pulNSentinels, pImport, FALSE))) IfFailGo(hr);
                 break;
-            case ELEMENT_TYPE_BYREF:  //fallthru
-                if(TypeFromToken(tk)==mdtFieldDef) IfFailGo(VLDTR_E_SIG_BYREFINFIELD);
-                FALLTHROUGH;
+            case ELEMENT_TYPE_BYREF:
             case ELEMENT_TYPE_PINNED:
             case ELEMENT_TYPE_SZARRAY:
                 // Validate the referenced type.

src/coreclr/vm/field.cpp
@@ -61,6 +61,8 @@ VOID FieldDesc::Init(mdFieldDef mb, CorElementType FieldType, DWORD dwMemberAttr
         FieldType == ELEMENT_TYPE_R8 ||
         FieldType == ELEMENT_TYPE_CLASS ||
         FieldType == ELEMENT_TYPE_VALUETYPE ||
+        FieldType == ELEMENT_TYPE_BYREF ||
+        FieldType == ELEMENT_TYPE_TYPEDBYREF ||
         FieldType == ELEMENT_TYPE_PTR ||
         FieldType == ELEMENT_TYPE_FNPTR
         );
@@ -70,7 +72,8 @@ VOID FieldDesc::Init(mdFieldDef mb, CorElementType FieldType, DWORD dwMemberAttr
     m_requiresFullMbValue = 0;
     SetMemberDef(mb);
 
-    m_type = FieldType;
+    // A TypedByRef should be treated like a regular value type.
+    m_type = FieldType != ELEMENT_TYPE_TYPEDBYREF ? FieldType : ELEMENT_TYPE_VALUETYPE;
     m_prot = fdFieldAccessMask & dwMemberAttrs;
     m_isStatic = fIsStatic != 0;
     m_isRVA = fIsRVA != 0;
@@ -81,7 +84,7 @@ VOID FieldDesc::Init(mdFieldDef mb, CorElementType FieldType, DWORD dwMemberAttr
 #endif
 
     _ASSERTE(GetMemberDef() == mb);                 // no truncation
-    _ASSERTE(GetFieldType() == FieldType);
+    _ASSERTE(GetFieldType() == FieldType || (FieldType == ELEMENT_TYPE_TYPEDBYREF && m_type == ELEMENT_TYPE_VALUETYPE));
     _ASSERTE(GetFieldProtection() == (fdFieldAccessMask & dwMemberAttrs));
     _ASSERTE((BOOL) IsStatic() == (fIsStatic != 0));
 }
@@ -152,6 +155,7 @@ TypeHandle FieldDesc::LookupFieldTypeHandle(ClassLoadLevel level, BOOL dropGener
     _ASSERTE(type == ELEMENT_TYPE_CLASS ||
              type == ELEMENT_TYPE_VALUETYPE ||
              type == ELEMENT_TYPE_STRING ||
+             type == ELEMENT_TYPE_TYPEDBYREF ||
              type == ELEMENT_TYPE_SZARRAY ||
              type == ELEMENT_TYPE_VAR
              );

src/coreclr/vm/jitinterface.cpp
@@ -9002,9 +9002,6 @@ CorInfoType CEEInfo::getFieldTypeInternal (CORINFO_FIELD_HANDLE fieldHnd,
     FieldDesc* field = (FieldDesc*) fieldHnd;
     CorElementType type   = field->GetFieldType();
 
-    // <REVISIT_TODO>TODO should not burn the time to do this for anything but Value Classes</REVISIT_TODO>
-    _ASSERTE(type != ELEMENT_TYPE_BYREF);
-
     if (type == ELEMENT_TYPE_I)
     {
         PTR_MethodTable enclosingMethodTable = field->GetApproxEnclosingMethodTable();

commit 8894928df636fbe38379a46339ffd04d0d6c351b

[mono] Fix is_byreflike_set on big-endian platforms
* Use int / gint32 types to avoid C# vs. native type mismatches

* Fixes s390x regression after https://github.com/dotnet/runtime/pull/63985

src/mono/System.Private.CoreLib/src/System/Reflection/Emit/TypeBuilder.Mono.cs
@@ -74,7 +74,7 @@ namespace System.Reflection.Emit
 
         [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)]
         private TypeInfo? created;
-        private bool is_byreflike_set;
+        private int is_byreflike_set;
 
         private int state;
 #endregion
@@ -1534,7 +1534,7 @@ namespace System.Reflection.Emit
             }
             else if (attrname == "System.Runtime.CompilerServices.IsByRefLikeAttribute")
             {
-                is_byreflike_set = true;
+                is_byreflike_set = 1;
             }
 
             if (cattrs != null)

src/mono/mono/metadata/object-internals.h
@@ -1222,7 +1222,7 @@ struct _MonoReflectionTypeBuilder {
 	MonoGenericContainer *generic_container;
 	MonoArray *generic_params;
 	MonoReflectionType *created;
-	gboolean is_byreflike_set;
+	gint32 is_byreflike_set;
 	gint32 state;
 };
 

src/mono/mono/metadata/sre.c
@@ -3866,7 +3866,7 @@ ves_icall_TypeBuilder_create_runtime_class (MonoReflectionTypeBuilderHandle ref_
 	 * The IsByRefLike attribute only applies to value types and enums. This matches CoreCLR behavior.
 	 */
 	if (klass->enumtype || klass->valuetype)
-		klass->is_byreflike = MONO_HANDLE_GETVAL (MONO_HANDLE_CAST (MonoReflectionTypeBuilder, ref_tb), is_byreflike_set);
+		klass->is_byreflike = MONO_HANDLE_GETVAL (MONO_HANDLE_CAST (MonoReflectionTypeBuilder, ref_tb), is_byreflike_set) != 0;
 
 	/* enums are done right away */
 	if (!klass->enumtype)