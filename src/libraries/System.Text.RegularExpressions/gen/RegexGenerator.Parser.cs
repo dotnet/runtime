033879812f64696293698dfc12c72ef388150b39
Theodore Tsirpanis <teo@tsirpanis.gr>
2022-02-08 19:46:37 +0200

    Use the common downlevel IsExternalInit file in the regex source generator.
    And remove a now-unused file.

--- src/libraries/System.Text.RegularExpressions/gen/RegexGenerator.Parser.cs
+++ src/libraries/System.Text.RegularExpressions/gen/RegexGenerator.Parser.cs
@@ -4,7 +4,6 @@
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.DotnetRuntime.Extensions;
 using System;
 using System.Collections;
 using System.Collections.Generic;
