ac10ce2f9c419b678f76e8c831f952475f97d721
Theodore Tsirpanis <teo@tsirpanis.gr>
2022-02-08 19:51:46 +0200

    Remove a couple of unused files from the project.

--- src/libraries/System.Private.CoreLib/gen/EventSourceGenerator.Parser.cs
+++ src/libraries/System.Private.CoreLib/gen/EventSourceGenerator.Parser.cs
@@ -11,7 +11,6 @@ using System.Threading;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.DotnetRuntime.Extensions;
 
 namespace Generators
 {
