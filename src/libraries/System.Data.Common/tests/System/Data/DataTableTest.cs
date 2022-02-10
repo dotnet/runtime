b73147fe196018de73b3b28baecd0ff9f19a81ad
Shay Rojansky <roji@roji.org>
2022-02-10 15:28:10 +0100

    Obsolete and put SerializationFormat.Binary behind an appcontext switch
    Part of #39289

--- src/libraries/System.Data.Common/tests/System/Data/DataTableTest.cs
+++ src/libraries/System.Data.Common/tests/System/Data/DataTableTest.cs
@@ -28,6 +28,7 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Data.SqlTypes;
 using System.Diagnostics;
 using System.Globalization;
@@ -376,23 +377,18 @@ namespace System.Data.Tests
 
         }
 
-        [ConditionalFact(typeof(PlatformDetection), nameof(PlatformDetection.IsBinaryFormatterSupported), nameof(PlatformDetection.IsNotInvariantGlobalization))]
-        public void DataColumnTypeSerialization()
+        [Fact]
+        public void SerializationFormat_Binary_does_not_work_by_default_on_DataTable()
         {
             DataTable dt = new DataTable("MyTable");
-            DataColumn dc = new DataColumn("dc", typeof(int));
-            dt.Columns.Add(dc);
-            dt.RemotingFormat = SerializationFormat.Binary;
+            Assert.Throws<InvalidEnumArgumentException>(() => dt.RemotingFormat = SerializationFormat.Binary);
+        }
 
-            DataTable dtDeserialized;
-            using (MemoryStream ms = new MemoryStream())
-            {
-                BinaryFormatter bf = new BinaryFormatter();
-                bf.Serialize(ms, dt);
-                ms.Seek(0, SeekOrigin.Begin);
-                dtDeserialized = (DataTable)bf.Deserialize(ms);
-            }
-            Assert.Equal(dc.DataType, dtDeserialized.Columns[0].DataType);
+        [Fact]
+        public void SerializationFormat_Binary_does_not_work_by_default_on_DataSet()
+        {
+            DataSet ds = new DataSet();
+            Assert.Throws<InvalidEnumArgumentException>(() => ds.RemotingFormat = SerializationFormat.Binary);
         }
 
         [Fact]
