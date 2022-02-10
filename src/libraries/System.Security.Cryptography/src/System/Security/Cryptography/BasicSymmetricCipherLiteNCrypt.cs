b8b657457e4bf63d186bfa3892be14998098f5d4
Kevin Jones <kevin@vcsjones.com>
2022-02-10 19:09:13 +0000

    Fixup namespace

--- src/libraries/System.Security.Cryptography/src/System/Security/Cryptography/BasicSymmetricCipherLiteNCrypt.cs
+++ src/libraries/System.Security.Cryptography/src/System/Security/Cryptography/BasicSymmetricCipherLiteNCrypt.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
-using System.Security.Cryptography;
 using System.Text;
 using Internal.Cryptography;
 using Internal.NativeCrypto;
