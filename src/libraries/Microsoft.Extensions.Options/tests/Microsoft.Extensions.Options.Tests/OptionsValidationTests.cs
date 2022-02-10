ac70ad5b60e250ebd79149be47c0f11c6dccd9a5
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2022-02-10 18:59:08 +0200

    Update OptionsValidationTests.cs

--- src/libraries/Microsoft.Extensions.Options/tests/Microsoft.Extensions.Options.Tests/OptionsValidationTests.cs
+++ src/libraries/Microsoft.Extensions.Options/tests/Microsoft.Extensions.Options.Tests/OptionsValidationTests.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
 using System.Collections.Generic;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
@@ -74,5 +75,18 @@ namespace Microsoft.Extensions.Options.Tests
             }
         }
 
+
+        [Fact]
+        public void ValidationCannotBeNull()
+        {
+            string validName = "Name";
+            string validFailureMessage = "Something's wrong";
+
+            Assert.Throws<ArgumentNullException>(() => new ValidateOptions<object>(validName, null, validFailureMessage));
+            Assert.Throws<ArgumentNullException>(() => new ValidateOptions<object, object>(validName, null, validFailureMessage));
+            Assert.Throws<ArgumentNullException>(() => new ValidateOptions<object, object, object>(validName, null, validFailureMessage));
+            Assert.Throws<ArgumentNullException>(() => new ValidateOptions<object, object, object, object>(validName, null, validFailureMessage));
+            Assert.Throws<ArgumentNullException>(() => new ValidateOptions<object, object, object, object, object, object>(validName, null, validFailureMessage));
+        }
     }
 }
