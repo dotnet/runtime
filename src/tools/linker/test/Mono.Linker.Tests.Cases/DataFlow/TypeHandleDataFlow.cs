a06e54ed0458232dfbfe7d715e04cde281b3c923
Sven Boemer <sbomer@gmail.com>
2022-02-15 09:33:54 -0700

    [DAM analyzer] Track null values in analyzer (dotnet/linker#2604)
    This tracks null values and adjusts the handling of null values in a
    few intrinsics which either take structs that can never be null, or
    don't return when the input is null.
    
    MethodBodyScanner needed a small adjustment to correctly deal with the
    case where an intrinsic returns TopValue.
    
    Commit migrated from https://github.com/dotnet/linker/commit/bd2cc03c2bb8457727ee487a9152790c04837d8c

--- src/tools/linker/test/Mono.Linker.Tests.Cases/DataFlow/TypeHandleDataFlow.cs
+++ src/tools/linker/test/Mono.Linker.Tests.Cases/DataFlow/TypeHandleDataFlow.cs
@@ -64,8 +64,6 @@ namespace Mono.Linker.Tests.Cases.DataFlow
 			Type.GetTypeFromHandle (typeWithMethods.TypeHandle).RequiresPublicMethods ();
 		}
 
-		// https://github.com/dotnet/linker/issues/2528
-		[ExpectedWarning ("IL2072", nameof (Type.GetTypeFromHandle), nameof (DataFlowTypeExtensions.RequiresPublicMethods), ProducedBy = ProducedBy.Analyzer)]
 		static void TestNull ()
 		{
 			Type type = null;
