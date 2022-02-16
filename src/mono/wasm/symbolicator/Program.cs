41b78d6bdf9767dfdc4d0e9d932c0b466ec9baac
Ankit Jain <radical@gmail.com>
2022-02-16 12:52:27 -0500

    wip

---
+++ src/mono/wasm/symbolicator/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.DotNet.XHarness.CLI.Commands.Wasm;
+using Microsoft.Extensions.Logging;
+
+var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder.AddSimpleConsole(options =>
+        {
+            options.SingleLine = true;
+            options.TimestampFormat = "[HH:mm:ss] ";
+        })
+        .AddFilter(null, LogLevel.Trace);
+});
+var logger = loggerFactory.CreateLogger("symbolicator");
+logger.LogInformation("hello");
+
+var sym = new Symbolicator(args[0], args[1], true, logger);
+
+foreach (var line in File.ReadAllLines(args[2]))
+{
+    var newLine = sym.Symbolicate(line);
+    Console.WriteLine (newLine);
+}
