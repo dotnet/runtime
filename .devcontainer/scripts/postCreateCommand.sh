05e40fbe6b0662214b2e51781f2b83ed0e7dba29
Ilona Tomkowicz <itomkowicz@microsoft.com>
2022-02-18 10:31:14 +0100

    Applied @eerhardt suggestion.

--- .devcontainer/scripts/postCreateCommand.sh
+++ .devcontainer/scripts/postCreateCommand.sh
@@ -4,7 +4,3 @@ set -e
 
 # reset the repo to the commit hash that was used to build the prebuilt Codespace
 git reset --hard $(cat ./artifacts/prebuild.sha)
-
-# add harness --no-sandbox argument if running in container; reload changes
-echo 'export NO_SANDBOX_IN_CONTAINER="--no-sandbox"' >> ~/.bashrc
-exec bash
