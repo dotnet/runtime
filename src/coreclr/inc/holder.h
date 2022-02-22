9e5211ac0382042e8a6f7493556d8625bf35b3a6
Jan Vorlicek <janvorli@microsoft.com>
2022-02-22 23:05:04 +0100

    Fix build with executable allocator logging disabled

--- src/coreclr/inc/holder.h
+++ src/coreclr/inc/holder.h
@@ -945,12 +945,13 @@ FORCEINLINE void StubRelease(TYPE* value)
 {
     if (value)
     {
+#ifdef LOG_EXECUTABLE_ALLOCATOR_STATISTICS
 #ifdef TARGET_UNIX
         LOGGER::LogUsage(__FILE__, __LINE__, __PRETTY_FUNCTION__);
 #else
         LOGGER::LogUsage(__FILE__, __LINE__, __FUNCTION__);
 #endif
-
+#endif // LOG_EXECUTABLE_ALLOCATOR_STATISTICS
         ExecutableWriterHolderNoLog<TYPE> stubWriterHolder(value, sizeof(TYPE));
         stubWriterHolder.GetRW()->DecRef();
     }
