3faf7ae1a58e4041afbfc8fbd830cef928ea7613
vitek-karas <10670590+vitek-karas@users.noreply.github.com>
2022-02-22 10:50:06 -0800

    Fixes

--- src/installer/tests/HostActivation.Tests/DependencyResolution/DependencyResolutionCommandResultExtensions.cs
+++ src/installer/tests/HostActivation.Tests/DependencyResolution/DependencyResolutionCommandResultExtensions.cs
@@ -6,7 +6,6 @@ using FluentAssertions.Execution;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using TestUtils.Assertions;
 
 namespace Microsoft.DotNet.CoreSetup.Test.HostActivation.DependencyResolution
 {
