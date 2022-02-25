d2518c2b44370cf688544ed1aff60e46c7ff9d95
EgorBo <egorbo@gmail.com>
2022-02-25 12:12:52 +0300

    use bashtoconst

--- src/coreclr/jit/morph.cpp
+++ src/coreclr/jit/morph.cpp
@@ -14105,7 +14105,7 @@ GenTree* Compiler::fgOptimizeRelationalComparisonWithCasts(GenTreeOp* cmp)
             knownPositiveOp->ChangeType(TYP_INT);
 #ifndef TARGET_64BIT
             assert(knownPositiveOp->OperIs(GT_CNS_LNG));
-            knownPositiveOp->BashToConst(static_cast<int>(knownPositiveOp->AsIntCommon()->IntegralValue()));
+            knownPositiveOp->BashToConst(static_cast<int>(knownPositiveOp->AsIntConCommon()->IntegralValue()));
 #endif
             fgUpdateConstTreeValueNumber(knownPositiveOp);
         }
