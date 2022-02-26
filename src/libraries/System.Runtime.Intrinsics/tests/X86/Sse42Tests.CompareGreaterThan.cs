eaf57f68a2bb763e31f83aaa63dda71060f0fe42
Robin Lindner <robin@deeprobin.de>
2022-02-26 20:38:32 +0100

    Add SSE42 tests

---
+++ src/libraries/System.Runtime.Intrinsics/tests/X86/Sse42Tests.CompareGreaterThan.cs
@@ -0,0 +1,48 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Runtime.Intrinsics.X86;
+using Xunit;
+
+namespace System.Runtime.Intrinsics.Tests.X86;
+
+public sealed partial class Sse42Tests
+{
+    [ConditionalTheory(nameof(Run64BitTests))]
+    [InlineData(0, 0, 0, 0, 0, 0)]
+    [InlineData(1, 0, 0, 0, -1, 0)]
+    [InlineData(0, 1, 0, 0, 0, -1)]
+    [InlineData(1, 1, 1, 1, 0, 0)]
+    [InlineData(123, 0, 0, 0, -1, 0)]
+    [InlineData(0, 321, 0, 0, 0, -1)]
+    [InlineData(123, 123, 123, 123, 0, 0)]
+    public void CompareGreaterThan_nuint_64Bit(long lowerLeft, long upperLeft, long lowerRight, long upperRight, long expectedLower, long expectedUpper)
+    {
+        Vector128<nint> expectedResult = Vector128.Create(expectedLower, expectedUpper).AsNInt();
+
+        Vector128<nint> left = Vector128.Create(lowerLeft, upperLeft).AsNInt();
+        Vector128<nint> right = Vector128.Create(lowerRight, upperRight).AsNInt();
+        Vector128<nint> actualResult = Sse42.CompareGreaterThan(left, right);
+
+        Assert.Equal(expectedResult, actualResult);
+    }
+
+    [ConditionalTheory(nameof(Run32BitTests))]
+    [InlineData(0, 0, 0, 0, 0, 0)]
+    [InlineData(1, 0, 0, 0, -1, 0)]
+    [InlineData(0, 1, 0, 0, 0, -1)]
+    [InlineData(1, 1, 1, 1, 0, 0)]
+    [InlineData(123, 0, 0, 0, -1, 0)]
+    [InlineData(0, 321, 0, 0, 0, -1)]
+    [InlineData(123, 123, 123, 123, 0, 0)]
+    public void CompareGreaterThan_nuint_32Bit(int lowerLeft, int upperLeft, int lowerRight, int upperRight, int expectedLower, int expectedUpper)
+    {
+        Vector128<nint> expectedResult = Vector128.Create(expectedLower, expectedUpper).AsNInt();
+
+        Vector128<nint> left = Vector128.Create(lowerLeft, upperLeft).AsNInt();
+        Vector128<nint> right = Vector128.Create(lowerRight, upperRight).AsNInt();
+        Vector128<nint> actualResult = Sse42.CompareGreaterThan(left, right);
+
+        Assert.Equal(expectedResult, actualResult);
+    }
+}
---
+++ src/libraries/System.Runtime.Intrinsics/tests/X86/Sse42Tests.Crc32.cs
@@ -0,0 +1,42 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Runtime.Intrinsics.X86;
+using Xunit;
+
+namespace System.Runtime.Intrinsics.Tests.X86;
+
+public sealed partial class Sse42Tests
+{
+    [ConditionalTheory(nameof(Run64BitTests))]
+    [InlineData(0, 0, 0)]
+    [InlineData(1234, 1234, 0)]
+    [InlineData(1234, 4321, 1804194867)]
+    [InlineData(ulong.MinValue, ulong.MaxValue, 3293575501)]
+    [InlineData(ulong.MaxValue, ulong.MinValue, 1943489909)]
+    public void Crc32_nuint_64Bit(ulong crc, ulong data, ulong expectedResult)
+    {
+        nuint nativeCrc = (nuint)crc;
+        nuint nativeData = (nuint)data;
+        nuint nativeExpectedResult = (nuint)expectedResult;
+
+        nuint actualResult = Sse42.Crc32(nativeCrc, nativeData);
+        Assert.Equal(nativeExpectedResult, actualResult);
+    }
+
+    [ConditionalTheory(nameof(Run32BitTests))]
+    [InlineData(0, 0, 0)]
+    [InlineData(1234, 1234, 0)]
+    [InlineData(1234, 4321, 2988438815)]
+    [InlineData(uint.MinValue, uint.MaxValue, 3080238136)]
+    [InlineData(uint.MaxValue, uint.MinValue, 3080238136)]
+    public void Crc32_nuint_32Bit(uint crc, uint data, uint expectedResult)
+    {
+        nuint nativeCrc = crc;
+        nuint nativeData = data;
+        nuint nativeExpectedResult = expectedResult;
+
+        nuint actualResult = Sse42.Crc32(nativeCrc, nativeData);
+        Assert.Equal(nativeExpectedResult, actualResult);
+    }
+}
