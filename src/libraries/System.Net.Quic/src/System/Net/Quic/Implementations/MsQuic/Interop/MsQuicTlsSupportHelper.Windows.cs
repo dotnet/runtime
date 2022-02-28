7472ca5eedcb0192e17dccf0606958d06f163d6f
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2022-02-28 21:34:41 +0000

    Scaffold TLS Support Helper
    Scaffolds an interoperable TLS support helper.
    Needs to be multi-file so that the Microsoft.Win32 dependency can be safely used in `MsQuicTlsSupportHelper.Windows.cs`

---
+++ src/libraries/System.Net.Quic/src/System/Net/Quic/Implementations/MsQuic/Interop/MsQuicTlsSupportHelper.Windows.cs
@@ -0,0 +1,14 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Net.Quic.Implementations.MsQuic.Internal
+{
+    internal static partial class MsQuicTlsSupportHelper
+    {
+        public static bool IsTls1_3Enabled()
+        {
+
+            return false;
+        }
+    }
+}
