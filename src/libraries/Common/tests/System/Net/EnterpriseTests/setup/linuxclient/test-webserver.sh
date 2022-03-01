639485d7620f333c362e310f9af358287f12a433
wfurt <tweinfurt@yahoo.com>
2022-02-28 17:08:00 -0800

    attemp to fix up EnterpriseTests

--- src/libraries/Common/tests/System/Net/EnterpriseTests/setup/linuxclient/test-webserver.sh
+++ src/libraries/Common/tests/System/Net/EnterpriseTests/setup/linuxclient/test-webserver.sh
@@ -4,3 +4,10 @@ kdestroy
 echo password | kinit user1
 curl --verbose --negotiate -u: http://apacheweb.linux.contoso.com
 kdestroy
+
+nslookup github.com
+if [ $? -ne 0 ]; then
+  # try to fix-up DNS adding public server
+  echo nameserver 8.8.8.8 >> /etc/resolv.conf
+fi
+nslookup github.com
