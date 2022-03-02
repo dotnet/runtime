0736ff1e9ccca27eec23ffd466c9c04062324699
Aleksey Kliger <aleksey@lambdageek.org>
2022-03-02 08:49:46 -0500

    add MIT banner

--- src/tests/Interop/MonoAPI/Native/mono-embedding-api-test/api-functions.h
+++ src/tests/Interop/MonoAPI/Native/mono-embedding-api-test/api-functions.h
@@ -1,3 +1,6 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+//
 // This file intentionally doesn't have header guards
 #include <mono/metadata/details/appdomain-functions.h>
 #include <mono/metadata/details/assembly-functions.h>
