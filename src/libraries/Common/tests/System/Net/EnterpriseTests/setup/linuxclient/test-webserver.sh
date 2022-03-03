8b1fbfe978b199e4bb17858528330dabdc72c250
wfurt <tweinfurt@yahoo.com>
2022-03-03 00:13:51 -0800

    experiment

--- src/libraries/Common/tests/System/Net/EnterpriseTests/setup/linuxclient/test-webserver.sh
+++ src/libraries/Common/tests/System/Net/EnterpriseTests/setup/linuxclient/test-webserver.sh
@@ -6,12 +6,4 @@ curl --verbose --negotiate -u: http://apacheweb.linux.contoso.com
 kdestroy
 
 nslookup github.com
-if [ $? -ne 0 ]; then
-  cp /etc/resolv.conf /etc/resolv.conf.ORI
-  # try to fix-up DNS by adding public server
-  echo nameserver 8.8.8.8 >> /etc/resolv.conf
-
-  nslookup github.com
-  curl --verbose http://apacheweb.linux.contoso.com
-fi
 
