6e9f52e4935d3bf4b8da615fba7e3137ae49480d
Alexander Köplinger <alex.koeplinger@outlook.com>
2022-03-04 21:44:13 +0100

    Remove ANDROID_UNIFIED_HEADERS and HAVE_ANDROID_NDK_VERSION_H
    We're requiring a recent Android NDK now that only supports unified headers.

--- src/mono/cmake/configure.cmake
+++ src/mono/cmake/configure.cmake
@@ -70,7 +70,7 @@ ac_check_headers (
   sys/ioctl.h sys/errno.h sys/sendfile.h sys/statvfs.h sys/statfs.h sys/mman.h sys/mount.h sys/time.h sys/random.h
   strings.h stdint.h unistd.h signal.h setjmp.h syslog.h netdb.h utime.h semaphore.h libproc.h alloca.h ucontext.h pwd.h elf.h
   gnu/lib-names.h netinet/tcp.h netinet/in.h link.h arpa/inet.h unwind.h poll.h wchar.h linux/magic.h
-  android/legacy_signal_inlines.h android/ndk-version.h execinfo.h pthread.h pthread_np.h net/if.h dirent.h
+  android/legacy_signal_inlines.h execinfo.h pthread.h pthread_np.h net/if.h dirent.h
   CommonCrypto/CommonDigest.h dlfcn.h getopt.h pwd.h alloca.h
   /usr/include/malloc.h)
 
