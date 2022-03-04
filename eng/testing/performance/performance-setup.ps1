7d04249eb07e55f0500f1d2fe3b8d2b61da5df2b
Parker Bibus <parkerbibus@microsoft.com>
2022-03-04 10:40:42 -0800

    Setup pipeline for testing.

--- eng/testing/performance/performance-setup.ps1
+++ eng/testing/performance/performance-setup.ps1
@@ -125,7 +125,7 @@ if ($RunFromPerformanceRepo) {
     robocopy $SourceDirectory $PerformanceDirectory /E /XD $PayloadDirectory $SourceDirectory\artifacts $SourceDirectory\.git
 }
 else {
-    git clone --branch main --depth 1 --quiet https://github.com/dotnet/performance $PerformanceDirectory
+    git clone --branch EnableiOSIPASizeMeasurement --depth 1 --quiet https://github.com/LoopedBard3/performance $PerformanceDirectory
 }
 
 if($MonoDotnet -ne "")
