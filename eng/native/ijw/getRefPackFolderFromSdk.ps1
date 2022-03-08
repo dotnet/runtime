627851323fe8f15539c163aabd7d7c644766c549
Elinor Fung <elfung@microsoft.com>
2022-03-08 09:14:43 -0800

    Add tests for `ijwhost` (#66308)

--- eng/native/ijw/getRefPackFolderFromSdk.ps1
+++ eng/native/ijw/getRefPackFolderFromSdk.ps1
@@ -1,10 +1,9 @@
 # Licensed to the .NET Foundation under one or more agreements.
 # The .NET Foundation licenses this file to you under the MIT license.
-$interopFolder = Split-Path $PSScriptRoot -Parent
-$testsFolder = Split-Path $interopFolder -Parent
-$srcFolder = Split-Path $testsFolder -Parent
-$repoRoot = Split-Path $srcFolder -Parent
 
+$engNativeFolder = Split-Path $PSScriptRoot -Parent
+$engFolder = Split-Path $engNativeFolder -Parent
+$repoRoot = Split-Path $engFolder -Parent
 
 . "$repoRoot/eng/common/tools.ps1"
 
@@ -22,7 +21,7 @@ $refPackPath = "$dotnetRoot/packs/Microsoft.NETCore.App.Ref/$refPackVersion/ref/
 if (-not (Test-Path $refPackPath))
 {
     Write-Error "Reference assemblies not found in the SDK folder. Did the SDK layout change? Did the SDK change how it describes the bundled runtime version?"
-    return 1
+    exit 1
 }
 
 Write-Output "refPackPath=$refPackPath"
