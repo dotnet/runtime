cbcc616cf386b88e49f97f74182ffff241528179
Simon Rozsival <simon@rozsival.com>
2022-03-09 15:24:14 +0100

    [Mono] Add SIMD intrinsics for Vector64/128 "All" and "Any" variants of GT/GE/LT/LE (#65889)
    * Add All and Any variants of GT/GE/LT/LE
    
    * Fix indentation
    
    * Fix indentation
    
    * Add xones to llvm
    
    * Fix comparisons for floating point numbers
    
    * Add missing unsigned type
    
    * Reduce code duplication
    
    * Fix build
    
    * Reorganize code a bit
    
    * Remove redundant assertion
    
    * Fix bitwise operators intrinsics
    
    * Simplify checking float args
    
    * Fix typo

--- src/mono/mono/mini/simd-intrinsics.c
+++ src/mono/mono/mini/simd-intrinsics.c
@@ -248,15 +248,52 @@ get_underlying_type (MonoType* type)
 		return type->type;
 }
 
+static gboolean type_enum_is_unsigned (MonoTypeEnum type);
+static gboolean type_enum_is_float (MonoTypeEnum type);
+
 static MonoInst*
 emit_xcompare (MonoCompile *cfg, MonoClass *klass, MonoTypeEnum etype, MonoInst *arg1, MonoInst *arg2)
 {
 	MonoInst *ins;
-	gboolean is_fp = etype == MONO_TYPE_R4 || etype == MONO_TYPE_R8;
+	int opcode = type_enum_is_float (etype) ? OP_XCOMPARE_FP : OP_XCOMPARE;
 
-	ins = emit_simd_ins (cfg, klass, is_fp ? OP_XCOMPARE_FP : OP_XCOMPARE, arg1->dreg, arg2->dreg);
+	ins = emit_simd_ins (cfg, klass, opcode, arg1->dreg, arg2->dreg);
 	ins->inst_c0 = CMP_EQ;
 	ins->inst_c1 = etype;
+	return ins;
+}
+
+static MonoInst*
+emit_xcompare_for_intrinsic (MonoCompile *cfg, MonoClass *klass, int intrinsic_id, MonoTypeEnum etype, MonoInst *arg1, MonoInst *arg2)
+{
+	MonoInst *ins = emit_xcompare (cfg, klass, etype, arg1, arg2);
+	gboolean is_unsigned = type_enum_is_unsigned (etype);
+
+	switch (intrinsic_id) {
+	case SN_GreaterThan:
+	case SN_GreaterThanAll:
+	case SN_GreaterThanAny:
+		ins->inst_c0 = is_unsigned ? CMP_GT_UN : CMP_GT;
+		break;
+	case SN_GreaterThanOrEqual:
+	case SN_GreaterThanOrEqualAll:
+	case SN_GreaterThanOrEqualAny:
+		ins->inst_c0 = is_unsigned ? CMP_GE_UN : CMP_GE;
+		break;
+	case SN_LessThan:
+	case SN_LessThanAll:
+	case SN_LessThanAny:
+		ins->inst_c0 = is_unsigned ? CMP_LT_UN : CMP_LT;
+		break;
+	case SN_LessThanOrEqual:
+	case SN_LessThanOrEqualAll:
+	case SN_LessThanOrEqualAny:
+		ins->inst_c0 = is_unsigned ? CMP_LE_UN : CMP_LE;
+		break;
+	default:
+		g_assert_not_reached ();
+	}
+
 	return ins;
 }
 
@@ -269,7 +306,7 @@ emit_xequal (MonoCompile *cfg, MonoClass *klass, MonoInst *arg1, MonoInst *arg2)
 static MonoInst*
 emit_not_xequal (MonoCompile *cfg, MonoClass *klass, MonoInst *arg1, MonoInst *arg2)
 {
-	MonoInst *ins = emit_simd_ins (cfg, klass, OP_XEQUAL, arg1->dreg, arg2->dreg);
+	MonoInst *ins = emit_xequal (cfg, klass, arg1, arg2);
 	int sreg = ins->dreg;
 	int dreg = alloc_ireg (cfg);
 	MONO_EMIT_NEW_BIALU_IMM (cfg, OP_COMPARE_IMM, -1, sreg, 0);
@@ -281,6 +318,12 @@ static MonoInst*
 emit_xzero (MonoCompile *cfg, MonoClass *klass)
 {
 	return emit_simd_ins (cfg, klass, OP_XZERO, -1, -1);
+}
+
+static MonoInst*
+emit_xones (MonoCompile *cfg, MonoClass *klass)
+{
+	return emit_simd_ins (cfg, klass, OP_XONES, -1, -1);
 }
 
 static gboolean
@@ -313,7 +356,13 @@ static gboolean
 type_is_unsigned (MonoType *type) {
 	MonoClass *klass = mono_class_from_mono_type_internal (type);
 	MonoType *etype = mono_class_get_context (klass)->class_inst->type_argv [0];
-	switch (etype->type) {
+	return type_enum_is_unsigned (etype->type);
+}
+
+static gboolean
+type_enum_is_unsigned (MonoTypeEnum type)
+{
+	switch (type) {
 	case MONO_TYPE_U1:
 	case MONO_TYPE_U2:
 	case MONO_TYPE_U4:
@@ -328,12 +377,13 @@ static gboolean
 type_is_float (MonoType *type) {
 	MonoClass *klass = mono_class_from_mono_type_internal (type);
 	MonoType *etype = mono_class_get_context (klass)->class_inst->type_argv [0];
-	switch (etype->type) {
-	case MONO_TYPE_R4:
-	case MONO_TYPE_R8:
-		return TRUE;
-	}
-	return FALSE;
+	return type_enum_is_float (etype->type);
+}
+
+static gboolean
+type_enum_is_float (MonoTypeEnum type)
+{
+	return type == MONO_TYPE_R4 || type == MONO_TYPE_R8;
 }
 
 static int
@@ -624,9 +674,17 @@ static guint16 sri_vector_methods [] = {
 	SN_GetLower,
 	SN_GetUpper,
 	SN_GreaterThan,
+	SN_GreaterThanAll,
+	SN_GreaterThanAny,
 	SN_GreaterThanOrEqual,
+	SN_GreaterThanOrEqualAll,
+	SN_GreaterThanOrEqualAny,
 	SN_LessThan,
+	SN_LessThanAll,
+	SN_LessThanAny,
 	SN_LessThanOrEqual,
+	SN_LessThanOrEqualAll,
+	SN_LessThanOrEqualAny,
 	SN_Max,
 	SN_Min,
 	SN_Multiply,
@@ -697,16 +755,10 @@ emit_sri_vector (MonoCompile *cfg, MonoMethod *cmethod, MonoMethodSignature *fsi
 		if (!is_element_type_primitive (fsig->params [0]))
 			return NULL;
 #ifdef TARGET_ARM64
-		switch (arg0_type) {
-			case MONO_TYPE_U1:
-			case MONO_TYPE_U2:
-			case MONO_TYPE_U4:
-			case MONO_TYPE_U8:
-			case MONO_TYPE_U:
+		if (type_enum_is_unsigned (arg0_type))
 			return NULL;
-			}
-		gboolean is_float = arg0_type == MONO_TYPE_R4 || arg0_type == MONO_TYPE_R8;
-		int iid = is_float ? INTRINS_AARCH64_ADV_SIMD_FABS : INTRINS_AARCH64_ADV_SIMD_ABS;
+
+		int iid = type_enum_is_float (arg0_type) ? INTRINS_AARCH64_ADV_SIMD_FABS : INTRINS_AARCH64_ADV_SIMD_ABS;
 		return emit_simd_ins_for_sig (cfg, klass, OP_XOP_OVR_X_X, iid, arg0_type, fsig, args);
 #else
 		return NULL;
@@ -721,7 +773,7 @@ emit_sri_vector (MonoCompile *cfg, MonoMethod *cmethod, MonoMethodSignature *fsi
 		if (!is_element_type_primitive (fsig->params [0]))
 			return NULL;
 		int instc0 = -1;
-		if (arg0_type == MONO_TYPE_R4 || arg0_type == MONO_TYPE_R8) {
+		if (type_enum_is_float (arg0_type)) {
 			switch (id) {
 			case SN_Add:
 				instc0 = OP_FADD;
@@ -816,7 +868,7 @@ emit_sri_vector (MonoCompile *cfg, MonoMethod *cmethod, MonoMethodSignature *fsi
 	case SN_Ceiling:
 	case SN_Floor: {
 #ifdef TARGET_ARM64
-		if ((arg0_type != MONO_TYPE_R4) && (arg0_type != MONO_TYPE_R8))
+		if (!type_enum_is_float (arg0_type))
 			return NULL;
 		int ceil_or_floor = id == SN_Ceiling ? INTRINS_AARCH64_ADV_SIMD_FRINTP : INTRINS_AARCH64_ADV_SIMD_FRINTM;
 		return emit_simd_ins_for_sig (cfg, klass, OP_XOP_OVR_X_X, ceil_or_floor, arg0_type, fsig, args);
@@ -919,25 +971,53 @@ emit_sri_vector (MonoCompile *cfg, MonoMethod *cmethod, MonoMethodSignature *fsi
 	case SN_LessThanOrEqual: {
 		if (!is_element_type_primitive (fsig->params [0]))
 			return NULL;
-		gboolean is_unsigned = type_is_unsigned (fsig->params [0]);
-		MonoInst *ins = emit_xcompare (cfg, klass, arg0_type, args [0], args [1]);
+
+		return emit_xcompare_for_intrinsic (cfg, klass, id, arg0_type, args [0], args [1]);
+	}
+	case SN_GreaterThanAll:
+	case SN_GreaterThanAny:
+	case SN_GreaterThanOrEqualAll:
+	case SN_GreaterThanOrEqualAny:
+	case SN_LessThanAll:
+	case SN_LessThanAny:
+	case SN_LessThanOrEqualAll:
+	case SN_LessThanOrEqualAny: {
+		if (!is_element_type_primitive (fsig->params [0]))
+			return NULL;
+
+		g_assert (fsig->param_count == 2 &&
+			fsig->ret->type == MONO_TYPE_BOOLEAN &&
+			mono_metadata_type_equal (fsig->params [0], fsig->params [1]));
+
+		MonoInst *cmp = emit_xcompare_for_intrinsic (cfg, klass, id, arg0_type, args [0], args [1]);
+		MonoClass *arg_class = mono_class_from_mono_type_internal (fsig->params [0]);
+
 		switch (id) {
-		case SN_GreaterThan:
-			ins->inst_c0 = is_unsigned ? CMP_GT_UN : CMP_GT;
-			break;
-		case SN_GreaterThanOrEqual:
-			ins->inst_c0 = is_unsigned ? CMP_GE_UN : CMP_GE;
-			break;
-		case SN_LessThan:
-			ins->inst_c0 = is_unsigned ? CMP_LT_UN : CMP_LT;
-			break;
-		case SN_LessThanOrEqual:
-			ins->inst_c0 = is_unsigned ? CMP_LE_UN : CMP_LE;
-			break;
+		case SN_GreaterThanAll:
+		case SN_GreaterThanOrEqualAll:
+		case SN_LessThanAll:
+		case SN_LessThanOrEqualAll: {
+			// for floating point numbers all ones is NaN and so
+			// they must be treated differently than integer types
+			if (type_enum_is_float (arg0_type)) {
+				MonoInst *zero = emit_xzero (cfg, arg_class);
+				MonoInst *inverted_cmp = emit_xcompare (cfg, klass, arg0_type, cmp, zero);
+				return emit_xequal (cfg, klass, inverted_cmp, zero);
+			}
+
+			MonoInst *ones = emit_xones (cfg, arg_class);
+			return emit_xequal (cfg, klass, cmp, ones);
+		}
+		case SN_GreaterThanAny:
+		case SN_GreaterThanOrEqualAny:
+		case SN_LessThanAny:
+		case SN_LessThanOrEqualAny: {
+			MonoInst *zero = emit_xzero (cfg, arg_class);
+			return emit_not_xequal (cfg, klass, cmp, zero);
+		}
 		default:
 			g_assert_not_reached ();
 		}
-		return ins;
 	}
 	case SN_Negate:
 	case SN_OnesComplement: {
@@ -954,7 +1034,7 @@ emit_sri_vector (MonoCompile *cfg, MonoMethod *cmethod, MonoMethodSignature *fsi
 		if (!is_element_type_primitive (fsig->params [0]))
 			return NULL;
 #ifdef TARGET_ARM64
-		if ((arg0_type != MONO_TYPE_R4) && (arg0_type != MONO_TYPE_R8))
+		if (!type_enum_is_float (arg0_type))
 			return NULL;
 		return emit_simd_ins_for_sig (cfg, klass, OP_XOP_OVR_X_X, INTRINS_AARCH64_ADV_SIMD_FSQRT, arg0_type, fsig, args);
 #else
@@ -1066,8 +1146,7 @@ emit_vector64_vector128_t (MonoCompile *cfg, MonoMethod *cmethod, MonoMethodSign
 		return emit_xzero (cfg, klass);
 	}
 	case SN_get_AllBitsSet: {
-		MonoInst *ins = emit_xzero (cfg, klass);
-		return emit_xcompare (cfg, klass, etype->type, ins, ins);
+		return emit_xones (cfg, klass);
 	}
 	case SN_Equals: {
 		if (fsig->param_count == 1 && fsig->ret->type == MONO_TYPE_BOOLEAN && mono_metadata_type_equal (fsig->params [0], type)) {
@@ -1271,9 +1350,7 @@ emit_sys_numerics_vector_t (MonoCompile *cfg, MonoMethod *cmethod, MonoMethodSig
 		return emit_simd_ins (cfg, klass, expand_opcode, one->dreg, -1);
 	}
 	case SN_get_AllBitsSet: {
-		/* Compare a zero vector with itself */
-		ins = emit_xzero (cfg, klass);
-		return emit_xcompare (cfg, klass, etype->type, ins, ins);
+		return emit_xones (cfg, klass);
 	}
 	case SN_get_Item: {
 		if (!COMPILE_LLVM (cfg))
@@ -1424,7 +1501,7 @@ emit_sys_numerics_vector_t (MonoCompile *cfg, MonoMethod *cmethod, MonoMethodSig
 		ins = emit_simd_ins (cfg, klass, OP_XBINOP, args [0]->dreg, args [1]->dreg);
 		ins->inst_c1 = etype->type;
 
-		if (etype->type == MONO_TYPE_R4 || etype->type == MONO_TYPE_R8) {
+		if (type_enum_is_float (etype->type)) {
 			switch (id) {
 			case SN_op_Addition:
 				ins->inst_c0 = OP_FADD;
