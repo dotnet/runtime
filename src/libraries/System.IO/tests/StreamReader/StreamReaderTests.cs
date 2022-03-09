8f2ab2d48911f49ba11e66bf3045d74266a8009e
Adam Sitnik <adam.sitnik@gmail.com>
2022-03-09 16:14:52 +0100

    skip the ReadToEndAsync_WithCancellation test if there is not enough disk space to create 1GB test file

--- src/libraries/System.IO/tests/StreamReader/StreamReaderTests.cs
+++ src/libraries/System.IO/tests/StreamReader/StreamReaderTests.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.DotNet.XUnitExtensions;
 using Xunit;
 
 namespace System.IO.Tests
@@ -136,12 +137,10 @@ namespace System.IO.Tests
         public async Task ReadToEndAsync_WithCancellation()
         {
             string path = GetTestFilePath();
-            
-            // create large (~1GB) file
-            File.WriteAllLines(path, Enumerable.Repeat("A very large file used for testing StreamReader cancellation. 0123456789012345678901234567890123456789.", 10_000_000));
+            CreateLargeFile(path);
 
             using StreamReader reader = File.OpenText(path);
-            using CancellationTokenSource cts = new ();
+            using CancellationTokenSource cts = new();
             var token = cts.Token;
 
             var ex = await Assert.ThrowsAnyAsync<OperationCanceledException>(async () =>
@@ -160,6 +159,31 @@ namespace System.IO.Tests
                 await readToEndTask;
             });
             Assert.Equal(token, ex.CancellationToken);
+        }
+
+        private void CreateLargeFile(string path)
+        {
+            const string sentence = "A very large file used for testing StreamReader cancellation. 0123456789012345678901234567890123456789.";
+            const int repeatCount = 10_000_000;
+            Encoding encoding = Encoding.UTF8;
+
+            using FileStream fs = File.OpenWrite(path);
+            long fileSize = encoding.GetByteCount(sentence) * repeatCount;
+
+            try
+            {
+                fs.SetLength(fileSize);
+            }
+            catch (IOException)
+            {
+                throw new SkipTestException($"Unable to run {ReadToEndAsync_WithCancellation} due to lack of available disk space");
+            }
+
+            using StreamWriter streamWriter = new StreamWriter(fs, encoding);
+            for (int i = 0; i < repeatCount; i++)
+            {
+                streamWriter.WriteLine(sentence);
+            }
         }
 
         [Fact]
