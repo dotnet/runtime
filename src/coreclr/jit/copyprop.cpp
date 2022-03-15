84de1282388f5c2506413326f2d49b6abec677df
SingleAccretion <62474226+SingleAccretion@users.noreply.github.com>
2022-03-15 19:13:46 +0300

    Some small copy propagation changes (#66582)
    * Add asserts
    
    * Remove the quirk comment, replace with a TODO-CQ

--- src/coreclr/jit/copyprop.cpp
+++ src/coreclr/jit/copyprop.cpp
@@ -312,7 +312,7 @@ void Compiler::optCopyPropPushDef(GenTreeOp*           asg,
     {
         assert((lclNode->gtFlags & GTF_VAR_DEF) != 0);
 
-        // Quirk: do not collect defs from PHIs. Preserves previous behavior.
+        // TODO-CQ: design better heuristics for propagation and remove this condition.
         if (!asg->IsPhiDefn())
         {
             ssaDefNum = GetSsaNumForLocalVarDef(lclNode);
@@ -480,10 +480,27 @@ void Compiler::optVnCopyProp()
             // TODO-Cleanup: Move this function from Compiler to this class.
             m_compiler->optBlockCopyPropPopStacks(block, &m_curSsaName);
         }
+
+        void PropagateCopies()
+        {
+            WalkTree();
+
+#ifdef DEBUG
+            // Verify the definitions remaining are only those we pushed for parameters.
+            for (LclNumToLiveDefsMap::KeyIterator iter = m_curSsaName.Begin(); !iter.Equal(m_curSsaName.End()); ++iter)
+            {
+                unsigned lclNum = iter.Get();
+                assert(m_compiler->lvaGetDesc(lclNum)->lvIsParam || (lclNum == m_compiler->info.compThisArg));
+
+                CopyPropSsaDefStack* defStack = iter.GetValue();
+                assert(defStack->Height() == 1);
+            }
+#endif // DEBUG
+        }
     };
 
     CopyPropDomTreeVisitor visitor(this);
-    visitor.WalkTree();
+    visitor.PropagateCopies();
 
     // Tracked variable count increases after CopyProp, so don't keep a shorter array around.
     // Destroy (release) the varset.
