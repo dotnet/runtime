2c56d90d3fefb052f1126eef86742a7b598744d7
Aleksey Kliger <alklig@microsoft.com>
2022-03-17 15:25:25 -0400

    make the Browser EAT lanes happy
    Add some fields to the baseline versions of some test assemblies to keep some
    types that are used in the deltas from getting trimmed away

--- src/libraries/System.Runtime.Loader/tests/ApplyUpdate/System.Reflection.Metadata.ApplyUpdate.Test.ReflectionAddNewType/ReflectionAddNewType_v1.cs
+++ src/libraries/System.Runtime.Loader/tests/ApplyUpdate/System.Reflection.Metadata.ApplyUpdate.Test.ReflectionAddNewType/ReflectionAddNewType_v1.cs
@@ -9,9 +9,22 @@ public interface IExistingInterface {
     public string ItfMethod(int i);
 }
 
+public struct QExistingStruct
+{
+}
+
+public enum FExistingEnum {
+    One, Two
+}
+
 public class ZExistingClass
 {
-    public class PreviousNestedClass { }
+    public class PreviousNestedClass {
+        public static DateTime Now; // make the linker happy
+        public static ICloneable C;
+        public event EventHandler<string> E;
+        public void R() { E(this,"123"); }
+    }
     public class NewNestedClass {};
 
 
