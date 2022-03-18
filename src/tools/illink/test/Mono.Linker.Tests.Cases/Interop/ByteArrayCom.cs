56212d693b57d3d170ee332803b0ac657b07eb9d
Jackson Schuster <36744439+jtschuster@users.noreply.github.com>
2022-03-18 16:02:30 -0700

    Add tests for analyzer null references in byte array COM interop (dotnet/linker#2688)
    Commit migrated from https://github.com/dotnet/linker/commit/9f2b304f6eb43c32bd7fb6f7cf7b39d5aa677378

---
+++ src/tools/illink/test/Mono.Linker.Tests.Cases/Interop/ByteArrayCom.cs
@@ -0,0 +1,26 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
+
+namespace Mono.Linker.Tests.Cases.Interop
+{
+	[ExpectedNoWarnings]
+	[SkipKeptItemsValidation]
+	class ByteArrayCom
+	{
+		public static void Main ()
+		{
+			TakesByteArray (new byte[] { });
+		}
+
+		[DllImport ("SampleText.dll")]
+		static extern void TakesByteArray (byte[] x);
+	}
+}
