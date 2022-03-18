faacddfe2393e15abf6d93debb0e261b2df19658
Zoltan Varga <vargaz@gmail.com>
2022-03-18 03:44:57 -0400

    [mono] Remove unused MONO_DEBUG=dont-free-domains option.

--- src/mono/mono/metadata/domain-internals.h
+++ src/mono/mono/metadata/domain-internals.h
@@ -24,14 +24,6 @@
 
 G_BEGIN_DECLS
 
-/*
- * If this is set, the memory belonging to appdomains is not freed when a domain is
- * unloaded, and assemblies loaded by the appdomain are not unloaded either. This
- * allows us to use typed gc in non-default appdomains too, leading to increased
- * performance.
- */
-extern gboolean mono_dont_free_domains;
-
 struct _MonoAppContext {
 	MonoObject obj;
 	gint32 domain_id;
