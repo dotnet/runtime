c992dc2a3142360b0d52eb62a908a6af13da0e28
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2022-03-21 18:19:15 +0100

    Revert "[crossgen2] Promote single byref aot. (#65682)" (#66946)
    This reverts commit c6ca9dc012bfd1a1fca7b6a2139d20368a258deb.

--- src/coreclr/jit/compiler.h
+++ src/coreclr/jit/compiler.h
@@ -4172,8 +4172,6 @@ public:
         void PromoteStructVar(unsigned lclNum);
         void SortStructFields();
 
-        bool CanConstructAndPromoteField(lvaStructPromotionInfo* structPromotionInfo);
-
         lvaStructFieldInfo GetFieldInfo(CORINFO_FIELD_HANDLE fieldHnd, BYTE ordinal);
         bool TryPromoteStructField(lvaStructFieldInfo& outerFieldInfo);
 
