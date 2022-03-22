701dc5cd9caaf5f1df2582c66b3ca9219fad775e
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2022-03-22 13:48:52 +0900

    Use UTF-8 for file path in ObjWriter
    LLVM already assumes the `char*` it receives is UTF-8. Fixes #66913.

--- src/coreclr/tools/aot/ILCompiler.Compiler/Compiler/DependencyAnalysis/ObjectWriter.cs
+++ src/coreclr/tools/aot/ILCompiler.Compiler/Compiler/DependencyAnalysis/ObjectWriter.cs
@@ -80,7 +80,7 @@ namespace ILCompiler.DependencyAnalysis
 #endif
 
         [DllImport(NativeObjectWriterFileName)]
-        private static extern IntPtr InitObjWriter(string objectFilePath, string triple = null);
+        private static extern IntPtr InitObjWriter([MarshalAs(UnmanagedType.LPUTF8Str)] string objectFilePath, string triple = null);
 
         [DllImport(NativeObjectWriterFileName)]
         private static extern void FinishObjWriter(IntPtr objWriter);
