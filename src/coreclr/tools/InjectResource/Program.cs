474462db1f73b2c5a0e8253593c8e801aff1323a
Jeremy Koritzinsky <jekoritz@microsoft.com>
2022-03-22 17:09:20 -0700

    Move InjectResource to managed to enable us to decouple the cross-component build (tools that run on the build machine for the target architecture) from the cross build (tools that run on the target architecture)

---
+++ src/coreclr/tools/InjectResource/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.CommandLine;
+using System.IO;
+
+var binOption = new Option<FileInfo?>(
+    name: "--bin",
+    description: "Binary data to attach to the image");
+var imageOption = new Option<FileInfo?>(
+    name: "--image",
+    description: "PE image to add the binary resource into");
+var nameOption = new Option<string>(
+    name: "--name",
+    description: "Resource name");
+var rootCommand = new RootCommand("Inject native resources into a Portable Executable image");
+rootCommand.AddOption(binOption);
+rootCommand.AddOption(imageOption);
+rootCommand.AddOption(nameOption);
+
+rootCommand.SetHandler(
+    async (FileInfo binaryData, FileInfo peImage, string name) => 
+        {
+            using ResourceUpdater updater = new(peImage);
+            updater.AddBinaryResource(name, await File.ReadAllBytesAsync(binaryData.FullName));
+        },
+    binOption,
+    imageOption,
+    nameOption);
+
+return await rootCommand.InvokeAsync(args);
