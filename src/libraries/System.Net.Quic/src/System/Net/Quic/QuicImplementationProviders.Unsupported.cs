bdd26a84c131a59e47e0a5a20bf4ed608ddd5f94
Radek Zikmund <radekzikmund@microsoft.com>
2022-03-24 16:49:13 +0100

    Add custom QuicImplementationProviders impl for unsupported platforms

---
+++ src/libraries/System.Net.Quic/src/System/Net/Quic/QuicImplementationProviders.Unsupported.cs
@@ -0,0 +1,19 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Net.Quic.Implementations;
+
+namespace System.Net.Quic
+{
+    public static partial class QuicImplementationProviders
+    {
+        public static Implementations.QuicImplementationProvider Mock => Default;
+        public static Implementations.QuicImplementationProvider MsQuic => Default;
+        public static Implementations.QuicImplementationProvider Default { get; } = new UnsupportedQuicImplementationProvider();
+
+        private class UnsupportedQuicImplementationProvider : QuicImplementationProvider
+        {
+            public override bool IsSupported => false;
+        }
+    }
+}
