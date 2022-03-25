50ae42492c497b2a913013e35c8ade36f1bbdf90
Ilona Tomkowicz <itomkowicz@microsoft.com>
2022-03-25 15:18:21 +0100

    Overlooked whitespaces - undo.

--- src/mono/wasm/debugger/BrowserDebugProxy/MonoSDBHelper.cs
+++ src/mono/wasm/debugger/BrowserDebugProxy/MonoSDBHelper.cs
@@ -209,7 +209,7 @@ namespace Microsoft.WebAssembly.Diagnostics
         ApplyChanges = 2,
     }
 
-    internal enum CmdPointer {
+    internal enum CmdPointer{
         GetValue = 1
     }
 
@@ -291,45 +291,45 @@ namespace Microsoft.WebAssembly.Diagnostics
     }
 
     internal enum ElementType {
-        End = 0x00,
-        Void = 0x01,
-        Boolean = 0x02,
-        Char = 0x03,
-        I1 = 0x04,
-        U1 = 0x05,
-        I2 = 0x06,
-        U2 = 0x07,
-        I4 = 0x08,
-        U4 = 0x09,
-        I8 = 0x0a,
-        U8 = 0x0b,
-        R4 = 0x0c,
-        R8 = 0x0d,
-        String = 0x0e,
-        Ptr = 0x0f,
-        ByRef = 0x10,
-        ValueType = 0x11,
-        Class = 0x12,
-        Var = 0x13,
-        Array = 0x14,
-        GenericInst = 0x15,
-        TypedByRef = 0x16,
-        I = 0x18,
-        U = 0x19,
-        FnPtr = 0x1b,
-        Object = 0x1c,
-        SzArray = 0x1d,
-        MVar = 0x1e,
-        CModReqD = 0x1f,
-        CModOpt = 0x20,
-        Internal = 0x21,
-        Modifier = 0x40,
-        Sentinel = 0x41,
-        Pinned = 0x45,
+        End             = 0x00,
+        Void            = 0x01,
+        Boolean         = 0x02,
+        Char            = 0x03,
+        I1              = 0x04,
+        U1              = 0x05,
+        I2              = 0x06,
+        U2              = 0x07,
+        I4              = 0x08,
+        U4              = 0x09,
+        I8              = 0x0a,
+        U8              = 0x0b,
+        R4              = 0x0c,
+        R8              = 0x0d,
+        String          = 0x0e,
+        Ptr             = 0x0f,
+        ByRef           = 0x10,
+        ValueType       = 0x11,
+        Class           = 0x12,
+        Var             = 0x13,
+        Array           = 0x14,
+        GenericInst     = 0x15,
+        TypedByRef      = 0x16,
+        I               = 0x18,
+        U               = 0x19,
+        FnPtr           = 0x1b,
+        Object          = 0x1c,
+        SzArray         = 0x1d,
+        MVar            = 0x1e,
+        CModReqD        = 0x1f,
+        CModOpt         = 0x20,
+        Internal        = 0x21,
+        Modifier        = 0x40,
+        Sentinel        = 0x41,
+        Pinned          = 0x45,
 
-        Type = 0x50,
-        Boxed = 0x51,
-        Enum = 0x55
+        Type            = 0x50,
+        Boxed           = 0x51,
+        Enum            = 0x55
     }
 
     internal enum ValueTypeId {
@@ -338,7 +338,7 @@ namespace Microsoft.WebAssembly.Diagnostics
         VType = 0xf2,
         FixedArray = 0xf3
     }
-    internal enum MonoTypeNameFormat {
+    internal enum MonoTypeNameFormat{
         FormatIL,
         FormatReflection,
         FullName,
@@ -364,7 +364,7 @@ namespace Microsoft.WebAssembly.Diagnostics
         internal int Rank { get; }
         internal int [] Bounds { get; }
         internal int TotalLength { get; }
-        public ArrayDimensions(int[] rank)
+        public ArrayDimensions(int [] rank)
         {
             Rank = rank.Length;
             Bounds = rank;
@@ -431,7 +431,7 @@ namespace Microsoft.WebAssembly.Diagnostics
             HasError = hasError;
         }
 
-        public MonoBinaryReader(byte [] data) : base(new MemoryStream(data)) { }
+        public MonoBinaryReader(byte [] data) : base(new MemoryStream(data)) {}
 
         public static MonoBinaryReader From(Result result)
         {
@@ -578,8 +578,7 @@ namespace Microsoft.WebAssembly.Diagnostics
             {
                 case SyntaxKind.NumericLiteralExpression:
                 {
-                    switch (constValue.Token.Value)
-                    {
+                    switch (constValue.Token.Value) {
                         case double d:
                             Write(ElementType.R8, d);
                             break;
