fb941067f7547aaf3618fe190c519ba653cb6d7e
james <judilsteve>
2022-03-29 19:42:22 +0800

    Fix trailing whitespace.
    Self-review action for pull request 67288.
    
    Signed-off-by: james <judilsteve>

--- src/libraries/Microsoft.Extensions.Hosting.Systemd/src/SystemdHelpers.cs
+++ src/libraries/Microsoft.Extensions.Hosting.Systemd/src/SystemdHelpers.cs
@@ -49,7 +49,7 @@ namespace Microsoft.Extensions.Hosting.Systemd
                 int parentPid = Interop.libc.GetParentPid();
                 string ppidString = parentPid.ToString(NumberFormatInfo.InvariantInfo);
                 byte[] comm = File.ReadAllBytes("/proc/" + ppidString + "/comm");
-                return comm.AsSpan().SequenceEqual(Encoding.ASCII.GetBytes("systemd\n")); 
+                return comm.AsSpan().SequenceEqual(Encoding.ASCII.GetBytes("systemd\n"));
             }
             catch
             {
