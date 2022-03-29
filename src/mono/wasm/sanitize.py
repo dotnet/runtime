5843dbfb8e3d34ebc08f397971c02f8436b456c0
Larry Ewing <lewing@microsoft.com>
2022-03-28 19:36:36 -0500

    [wasm] Attempt to fix audit errors in sanitize (#67255)
    * Run audit fix by default
    
    * Run update before audit
    
    * review suggestions

--- src/mono/wasm/sanitize.py
+++ src/mono/wasm/sanitize.py
@@ -64,9 +64,19 @@ def prune():
         print(error)
 
 
-os.chdir(emscripten_path)
-rewrite_package_json("package.json")
+if upgrade:
+    for path in node_paths:
+        update_npm(path)
 
+
+os.chdir(emscripten_path)
+try:
+    os.system("npm audit fix")
+except OSError as error:
+    print("npm audit fix failed")
+    print(error)
+
+rewrite_package_json("package.json")
 
 remove(
     "tests",
@@ -84,8 +94,6 @@ remove(
 
 prune()
 
-for path in node_paths:
-    if upgrade:
-        update_npm(path)
-    else:
+if not upgrade:
+    for path in node_paths:
         remove_npm(path)
