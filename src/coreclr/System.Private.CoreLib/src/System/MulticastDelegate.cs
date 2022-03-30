364b7add7506846f8691a57927780bfd70553677
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2022-03-31 00:56:26 +0800

    Remove outdated comment

--- src/coreclr/System.Private.CoreLib/src/System/MulticastDelegate.cs
+++ src/coreclr/System.Private.CoreLib/src/System/MulticastDelegate.cs
@@ -450,7 +450,6 @@ namespace System
             // so it can become a simple test
             if (d2 is null)
             {
-                // return true/false not the test result https://github.com/dotnet/runtime/issues/4207
                 return d1 is not null;
             }
 
