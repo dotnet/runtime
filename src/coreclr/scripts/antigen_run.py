aa0f143224af497a890429279b3ebe8af5155934
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2022-03-30 15:24:45 +0200

    Disable coredumps for fuzzers

--- src/coreclr/scripts/antigen_run.py
+++ src/coreclr/scripts/antigen_run.py
@@ -202,6 +202,16 @@ def main(main_args):
         path_to_corerun += ".exe"
         path_to_tool += ".exe"
 
+    if not is_windows:
+        # Disable core dumps. The fuzzers have their own graceful handling for
+        # runtime crashes. Especially on macOS we can quickly fill up the drive
+        # with dumps if we find lots of crashes since dumps there are very big.
+        try:
+            import resource
+            resource.setrlimit(resource.RLIMIT_CORE, 0)
+        except:
+            pass
+
     try:
         # Run tool such that issues are placed in a temp folder
         with TempDir() as temp_location:
