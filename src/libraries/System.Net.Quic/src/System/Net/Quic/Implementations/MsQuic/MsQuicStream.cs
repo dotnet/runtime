4fd9623bfdf5a2f80bdb812d947a863d05da9d16
Radek Zikmund <radekzikmund@microsoft.com>
2022-03-30 16:14:15 +0200

    Fix Wrong exception for Open*Stream after connection is closed

--- src/libraries/System.Net.Quic/src/System/Net/Quic/Implementations/MsQuic/MsQuicStream.cs
+++ src/libraries/System.Net.Quic/src/System/Net/Quic/Implementations/MsQuic/MsQuicStream.cs
@@ -173,6 +173,12 @@ namespace System.Net.Quic.Implementations.MsQuic
                     GCHandle.ToIntPtr(_state.StateGCHandle),
                     out _state.Handle);
 
+                if (status == MsQuicStatusCodes.Aborted || status == MsQuicStatusCodes.InvalidState)
+                {
+                    // connection already aborted (from either direction), throw relevant exception
+                    throw ThrowHelper.GetConnectionAbortedException(connectionState.AbortErrorCode);
+                }
+
                 QuicExceptionHelpers.ThrowIfFailed(status, "Failed to open stream to peer.");
 
                 Debug.Assert(!Monitor.IsEntered(_state), "!Monitor.IsEntered(_state)");
