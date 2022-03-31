b12574209b0c650a71656f2af1b80c36c54f86c8
Jeremy Koritzinsky <jekoritz@microsoft.com>
2022-03-31 15:50:07 -0700

    Enable passing the issues.targets exclusion list to XHarness-based test runners.

---
+++ src/tasks/Base64Tasks/EncodeUnicodeBase64.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+namespace Base64Tasks;
+
+public class EncodeUnicodeBase64 : Task
+{
+    [Required]
+    public string? Input { get; set; }
+
+    [Output]
+    public string? Base64 { get; set; }
+
+    public override bool Execute()
+    {
+        Base64 = Convert.ToBase64String(Encoding.Unicode.GetBytes(Input!));
+        return true;
+    }
+}
