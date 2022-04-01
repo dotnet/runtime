dotnet/runtime

commit 8a498cca21b26d431105676b6e13fc2c25f63ccb

Fix InvokeMember corner case (#62891)

src/coreclr/tools/aot/ILCompiler.Compiler/Compiler/MetadataManager.cs
@@ -332,6 +332,8 @@ namespace ILCompiler
                 {
                     // We're going to generate a mapping table entry for this. Collect dependencies.
                     ReflectionInvokeMapNode.AddDependenciesDueToReflectability(ref dependencies, factory, method);
+
+                    ReflectionInvokeSupportDependencyAlgorithm.GetDependenciesFromParamsArray(ref dependencies, factory, method);
                 }
             }
         }

src/coreclr/tools/aot/ILCompiler.Compiler/Compiler/DependencyAnalysis/ReflectionInvokeSupportDependencyAlgorithm.cs
@@ -1,0 +1,59 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Reflection.Metadata;
+
+using Internal.TypeSystem;
+using Internal.TypeSystem.Ecma;
+
+using DependencyList = ILCompiler.DependencyAnalysisFramework.DependencyNodeCore<ILCompiler.DependencyAnalysis.NodeFactory>.DependencyList;
+
+namespace ILCompiler.DependencyAnalysis
+{
+    internal static class ReflectionInvokeSupportDependencyAlgorithm
+    {
+        // Inserts dependencies to make the following corner case work (we need MethodTable for `MyStruct[]`):
+        //
+        // struct MyStruct
+        // {
+        //     public static int Count(params MyStruct[] myStructs)
+        //     {
+        //         return myStructs.Length;
+        //     }
+        //
+        //     public static void Main()
+        //     {
+        //         typeof(MyStruct).InvokeMember(nameof(Count), BindingFlags.InvokeMethod | BindingFlags.Public | BindingFlags.Static, null, null, new object[] { default(MyStruct) });
+        //     }
+        // }
+        public static void GetDependenciesFromParamsArray(ref DependencyList dependencies, NodeFactory factory, MethodDesc method)
+        {
+            MethodSignature sig = method.Signature;
+            if (sig.Length < 1 || !sig[sig.Length - 1].IsArray)
+                return;
+
+            if (method.GetTypicalMethodDefinition() is not EcmaMethod ecmaMethod)
+                return;
+
+            MetadataReader reader = ecmaMethod.MetadataReader;
+            MethodDefinition methodDef = reader.GetMethodDefinition(ecmaMethod.Handle);
+
+            foreach (ParameterHandle paramHandle in methodDef.GetParameters())
+            {
+                Parameter param = reader.GetParameter(paramHandle);
+                if (param.SequenceNumber == sig.Length /* SequenceNumber is 1-based */)
+                {
+                    if (!reader.GetCustomAttributeHandle(param.GetCustomAttributes(), "System", "ParamArrayAttribute").IsNil)
+                    {
+                        dependencies ??= new DependencyList();
+                        dependencies.Add(
+                            factory.ConstructedTypeSymbol(sig[sig.Length - 1].NormalizeInstantiation()),
+                            "Reflection invoke");
+                    }
+
+                    break;
+                }
+            }
+        }
+    }
+}

src/coreclr/tools/aot/ILCompiler.Compiler/ILCompiler.Compiler.csproj
@@ -204,6 +204,7 @@
     <Compile Include="Compiler\DependencyAnalysis\MethodExceptionHandlingInfoNode.cs" />
     <Compile Include="Compiler\DependencyAnalysis\ModuleInitializerListNode.cs" />
     <Compile Include="Compiler\DependencyAnalysis\ObjectGetTypeFlowDependenciesNode.cs" />
+    <Compile Include="Compiler\DependencyAnalysis\ReflectionInvokeSupportDependencyAlgorithm.cs" />
     <Compile Include="Compiler\DependencyAnalysis\ReflectionMethodBodyScanner.cs" />
     <Compile Include="Compiler\DependencyAnalysis\StructMarshallingDataNode.cs" />
     <Compile Include="Compiler\DependencyAnalysis\Target_ARM64\ARM64TentativeMethodNode.cs" />

src/tests/nativeaot/SmokeTests/Reflection/Reflection.cs
@@ -59,6 +59,7 @@ internal class ReflectionTest
         TestGetUninitializedObject.Run();
         TestInstanceFields.Run();
         TestReflectionInvoke.Run();
+        TestInvokeMemberParamsCornerCase.Run();
         TestDefaultInterfaceInvoke.Run();
         TestCovariantReturnInvoke.Run();
 #if !CODEGEN_CPP
@@ -261,6 +262,26 @@ internal class ReflectionTest
                     throw new Exception();
             }
 #endif
+        }
+    }
+
+    class TestInvokeMemberParamsCornerCase
+    {
+        public struct MyStruct { }
+
+        public static int Count(params MyStruct[] myStructs)
+        {
+            return myStructs.Length;
+        }
+
+        public static void Run()
+        {
+            Console.WriteLine(nameof(TestInvokeMemberParamsCornerCase));
+
+            // Needs MethodTable for MyStruct[] and the compiler should have created it.
+            typeof(TestInvokeMemberParamsCornerCase).InvokeMember(nameof(Count),
+                BindingFlags.InvokeMethod | BindingFlags.Public | BindingFlags.Static,
+                null, null, new object[] { default(MyStruct) });
         }
     }
 

commit 0cd0390f7244c14ead1bcf0d98ad3fd96490bed5

Don't consider ShadowMethods reflectable
Fixes #65612. This regression was exposed in #62891 - we had a mismatch between what methods are considered reflectable during scanning vs during optimized compilation due to extra array being injected for reflectable `params` methods on instantiated generic types.

There are two potential fixes - either expand the set of methods considered reflectable during scanning, or limit the number of methods considered reflectable in optimized compilation. It doesn't look like we need the expanded set, so going with a restriction instead. The CI may prove me otherwise, so keeping an open mind about this.

src/coreclr/tools/aot/ILCompiler.Compiler/Compiler/MetadataManager.cs
@@ -184,16 +184,18 @@ namespace ILCompiler
             }
 
             IMethodNode methodNode = methodBodyNode;
+            if (methodNode != null)
+            {
+                if (AllMethodsCanBeReflectable)
+                    _reflectableMethods.Add(methodNode.Method);
+            }
+
             if (methodNode == null)
                 methodNode = obj as ShadowConcreteMethodNode;
 
             if (methodNode != null)
             {
                 _methodsGenerated.Add(methodNode.Method);
-
-                if (AllMethodsCanBeReflectable)
-                    _reflectableMethods.Add(methodNode.Method);
-
                 return;
             }
 

src/tests/nativeaot/SmokeTests/UnitTests/Generics.cs
@@ -45,6 +45,7 @@ class Generics
         TestRecursionInGenericVirtualMethods.Run();
         TestRecursionThroughGenericLookups.Run();
         TestGvmLookupDependency.Run();
+        TestInvokeMemberCornerCaseInGenerics.Run();
 #if !CODEGEN_CPP
         TestNullableCasting.Run();
         TestVariantCasting.Run();
@@ -3166,6 +3167,28 @@ class Generics
         {
             CatConcepts<Technique, int>();
             CatConcepts<Technique, object>();
+        }
+    }
+
+    class TestInvokeMemberCornerCaseInGenerics
+    {
+        class Generic<T>
+        {
+            public void Method(params T[] ts) { }
+        }
+
+        class Atom { }
+
+        static Generic<Atom> s_instance = new Generic<Atom>();
+        static Type s_atomType = typeof(Atom);
+
+        public static void Run()
+        {
+            s_instance.Method(null);
+
+            // Regression test for https://github.com/dotnet/runtime/issues/65612
+            // This requires MethodTable for "Atom[]" - just make sure the compiler didn't crash and we can invoke
+            typeof(Generic<>).MakeGenericType(s_atomType).InvokeMember("Method", BindingFlags.Public | BindingFlags.InvokeMethod | BindingFlags.Instance, null, s_instance, new object[] { new Atom() });
         }
     }
 }