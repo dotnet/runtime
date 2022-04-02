dc7a5fa706c1befe6afa8d6e1c18a699f8fa0dde
Danyy427 <ozgur.guzeldereli@gmail.com>
2022-04-02 20:51:52 +0300

    Added sanitization as a method
    The tests added pass with this build

---
+++ src/libraries/System.IO.Compression.ZipFile/src/System/IO/Compression/ZipFileValidFullName_Unix.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Text;
+
+namespace System.IO.Compression
+{
+    public static partial class ZipFileExtensions
+    {
+        internal static string SanitizeZipFilePath(string zipPath)
+        {
+            StringBuilder builder = new StringBuilder(zipPath);
+            for (int i = 0; i < zipPath.Length; i++)
+            {
+                if ((int)builder[i] == 0)
+                {
+                    builder[i] = '_';
+                }
+            }
+            return builder.ToString();
+        }
+    }
+}
