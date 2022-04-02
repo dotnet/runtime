dfc6f33ad1f5e902ef80a70326e585568bdb71d5
Ankit Jain <radical@gmail.com>
2022-04-02 06:37:29 -0400

    tmp: performance-setup.sh: use a specific perf branch

--- eng/testing/performance/performance-setup.sh
+++ eng/testing/performance/performance-setup.sh
@@ -274,7 +274,9 @@ if [[ "$run_from_perf_repo" == true ]]; then
     performance_directory=$workitem_directory
     setup_arguments="--perf-hash $commit_sha $common_setup_arguments"
 else
-    git clone --branch main --depth 1 --quiet https://github.com/dotnet/performance.git $performance_directory
+    #git clone --branch main --depth 1 --quiet https://github.com/dotnet/performance.git $performance_directory
+    git clone --branch aj/wasm-testing --depth 1 --quiet https://github.com/radical/performance.git $performance_directory
+    #test -z "$perf_sha" || git reset --hard $perf_sha
     # uncomment to use BenchmarkDotNet sources instead of nuget packages
     # git clone https://github.com/dotnet/BenchmarkDotNet.git $benchmark_directory
 
