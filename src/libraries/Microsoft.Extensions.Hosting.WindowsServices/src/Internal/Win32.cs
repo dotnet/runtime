a93720000b76ee0bf21ea853bca4ce6ee947cd8c
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2022-04-03 23:52:11 +0300

    Annotate

--- src/libraries/Microsoft.Extensions.Hosting.WindowsServices/src/Internal/Win32.cs
+++ src/libraries/Microsoft.Extensions.Hosting.WindowsServices/src/Internal/Win32.cs
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#nullable enable
+
 using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
@@ -11,7 +13,7 @@ namespace Microsoft.Extensions.Hosting.WindowsServices.Internal
     [SupportedOSPlatform("windows")]
     internal static class Win32
     {
-        internal static Process GetParentProcess()
+        internal static Process? GetParentProcess()
         {
             var snapshotHandle = IntPtr.Zero;
             try
