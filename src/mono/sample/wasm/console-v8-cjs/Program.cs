3d290a16046c4595d38a21d20903c6c368f1f4ec
Radek Doulik <radekdoulik@gmail.com>
2022-04-04 15:34:28 +0200

    Remove test code

--- src/mono/sample/wasm/console-v8-cjs/Program.cs
+++ src/mono/sample/wasm/console-v8-cjs/Program.cs
@@ -2,20 +2,12 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
-using System.Runtime.Intrinsics;
-using System.Runtime.Intrinsics.Wasm;
 using System.Threading.Tasks;
 
 public class Test
 {
-    static Vector128<byte> TestSIMD()
-    {
-        return WasmBase.Constant(0xff11ff22ff33ff44, 0xff55ff66ff77ff88);
-    }
-
     public static async Task<int> Main(string[] args)
     {
-        var v = TestSIMD();
         await Task.Delay(1);
         Console.WriteLine("Hello World!");
         for (int i = 0; i < args.Length; i++) {
