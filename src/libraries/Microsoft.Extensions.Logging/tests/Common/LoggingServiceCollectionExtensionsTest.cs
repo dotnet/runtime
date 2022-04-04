de6fa0557b4c22d1d16ca3972a1a3836a51774fc
Heath Baron-Morgan <heathbm@outlook.com>
2022-04-03 21:23:47 -0700

    Allow custom IExternalScopeProvider in LoggerFactory via DI

--- src/libraries/Microsoft.Extensions.Logging/tests/Common/LoggingServiceCollectionExtensionsTest.cs
+++ src/libraries/Microsoft.Extensions.Logging/tests/Common/LoggingServiceCollectionExtensionsTest.cs
@@ -1,9 +1,11 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
 using System.Linq;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
+using Moq;
 
 namespace Microsoft.Extensions.Logging.Test
 {
@@ -20,6 +22,43 @@ namespace Microsoft.Extensions.Logging.Test
                 callbackCalled = true;
                 Assert.Same(services, builder.Services);
             });
+            Assert.True(callbackCalled);
+        }
+
+        [Fact]
+        public void AddLogging_InjectScopeProvider()
+        {
+            var services = new ServiceCollection();
+            services.AddSingleton(typeof(IExternalScopeProvider), Mock.Of<IExternalScopeProvider>());
+
+            var callbackCalled = false;
+            var loggerBuilder = services.AddLogging(builder =>
+            {
+                callbackCalled = true;
+                Assert.Same(services, builder.Services);
+            });
+
+            Assert.True(callbackCalled);
+        }
+
+        [Fact]
+        public void AddLogging_TestInjectedScopeProvider()
+        {
+            var testScope = "test scope";
+            var services = new ServiceCollection();
+            var externalScopeProvider = new Mock<IExternalScopeProvider>();
+            var callbackCalled = false;
+            externalScopeProvider
+                .Setup(e => e.Push(It.IsAny<object?>()))
+                .Callback(() => callbackCalled = true);
+
+            services.AddSingleton(externalScopeProvider.Object);
+            var loggerBuilder = services.AddLogging();
+            var serviceProvider = services.BuildServiceProvider();
+            var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+            var logger = loggerFactory.CreateLogger<LoggingServiceCollectionExtensionsTest>();
+            logger.BeginScope(testScope);
+
             Assert.True(callbackCalled);
         }
 
