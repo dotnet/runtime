901ef1f6043a5111e48faf9cff03b24f2aa3cd07
Jeremy Koritzinsky <jekoritz@microsoft.com>
2022-04-05 11:57:45 -0700

    Inject the exclusion list as a file in the mobile test apps to avoid command-line length limitations.

---
+++ src/tasks/TestExclusionListTasks/PatchExclusionListInApks.cs
@@ -0,0 +1,37 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+namespace TestExclusionListTasks;
+
+public class PatchExclusionListInApks : Task
+{
+    [Required]
+    public ITaskItem[]? ApkPaths { get; set; }
+
+    [Required]
+    public ITaskItem[]? ExcludedTests { get; set; }
+
+    public override bool Execute()
+    {
+        string testExclusionList = string.Join(
+            '\n',
+            (ExcludedTests ?? Enumerable.Empty<ITaskItem>()).Select(t => t.ItemSpec));
+        foreach (ITaskItem apk in ApkPaths ?? Enumerable.Empty<ITaskItem>())
+        {
+            using ZipArchive apkArchive = ZipFile.Open(apk.GetMetadata("FullPath"), ZipArchiveMode.Update);
+            ZipArchiveEntry assetsZipEntry = apkArchive.GetEntry("assets/assets.zip")!;
+            using ZipArchive assetsArchive = new ZipArchive(assetsZipEntry.Open(), ZipArchiveMode.Update);
+            ZipArchiveEntry testExclusionListEntry = assetsArchive.GetEntry("TestExclusionList.txt")!;
+            using StreamWriter textExclusionListWriter = new StreamWriter(testExclusionListEntry.Open());
+            textExclusionListWriter.WriteLine(testExclusionList);
+        }
+        return true;
+    }
+}
