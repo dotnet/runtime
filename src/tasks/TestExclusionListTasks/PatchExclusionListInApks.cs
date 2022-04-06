0fd5fef04ee697fd0347c1cda12b70a0e07047e9
Jeremy Koritzinsky <jekoritz@microsoft.com>
2022-04-05 18:03:04 -0700

    Use Android SDK tools to fix up APK after patching.

--- src/tasks/TestExclusionListTasks/PatchExclusionListInApks.cs
+++ src/tasks/TestExclusionListTasks/PatchExclusionListInApks.cs
@@ -18,19 +18,38 @@ public class PatchExclusionListInApks : Task
     [Required]
     public ITaskItem[]? ExcludedTests { get; set; }
 
+    public string? AndroidSdk { get; set; }
+
+    public string? MinApiLevel { get; set; }
+
+    public string? BuildToolsVersion { get; set; }
+
+    public string? KeyStorePath { get; set; }
+
     public override bool Execute()
     {
+        var apkBuilder = new ApkBuilder(Log);
+        apkBuilder.AndroidSdk = AndroidSdk;
+        apkBuilder.MinApiLevel = MinApiLevel;
+        apkBuilder.BuildToolsVersion = BuildToolsVersion;
+        apkBuilder.KeyStorePath = KeyStorePath;
+
         string testExclusionList = string.Join(
             '\n',
             (ExcludedTests ?? Enumerable.Empty<ITaskItem>()).Select(t => t.ItemSpec));
         foreach (ITaskItem apk in ApkPaths ?? Enumerable.Empty<ITaskItem>())
         {
-            using ZipArchive apkArchive = ZipFile.Open(apk.GetMetadata("FullPath"), ZipArchiveMode.Update);
-            ZipArchiveEntry assetsZipEntry = apkArchive.GetEntry("assets/assets.zip")!;
-            using ZipArchive assetsArchive = new ZipArchive(assetsZipEntry.Open(), ZipArchiveMode.Update);
-            ZipArchiveEntry testExclusionListEntry = assetsArchive.GetEntry("TestExclusionList.txt")!;
-            using StreamWriter textExclusionListWriter = new StreamWriter(testExclusionListEntry.Open());
-            textExclusionListWriter.WriteLine(testExclusionList);
+            string apkPath = apk.GetMetadata("FullPath")!;
+            apkBuilder.OutputDir = Path.GetDirectoryName(apkPath)!;
+            using (ZipArchive apkArchive = ZipFile.Open(apkPath, ZipArchiveMode.Update))
+            {
+                ZipArchiveEntry assetsZipEntry = apkArchive.GetEntry("assets/assets.zip")!;
+                using ZipArchive assetsArchive = new ZipArchive(assetsZipEntry.Open(), ZipArchiveMode.Update);
+                ZipArchiveEntry testExclusionListEntry = assetsArchive.GetEntry("TestExclusionList.txt")!;
+                using StreamWriter textExclusionListWriter = new StreamWriter(testExclusionListEntry.Open());
+                textExclusionListWriter.WriteLine(testExclusionList);
+            }
+            apkBuilder.ZipAndSignApk(apkPath);
         }
         return true;
     }
