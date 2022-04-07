8e868c1b8e23308d0995787bc48de96023600e83
Radek Zikmund <radekzikmund@microsoft.com>
2022-04-07 16:13:21 +0200

    Code review feedback

--- src/libraries/System.Net.Quic/src/System/Net/Quic/Implementations/MsQuic/MsQuicStream.cs
+++ src/libraries/System.Net.Quic/src/System/Net/Quic/Implementations/MsQuic/MsQuicStream.cs
@@ -1264,9 +1264,9 @@ namespace System.Net.Quic.Implementations.MsQuic
         private static uint HandleEventSendComplete(State state, ref StreamEvent evt)
         {
             StreamEventDataSendComplete sendCompleteEvent = evt.Data.SendComplete;
+            bool canceled = sendCompleteEvent.Canceled != 0;
 
             bool complete = false;
-            Exception? abortException = null;
 
             lock (state)
             {
@@ -1276,18 +1276,9 @@ namespace System.Net.Quic.Implementations.MsQuic
                     complete = true;
                 }
 
-                if (sendCompleteEvent.Canceled != 0)
+                if (canceled)
                 {
                     state.SendState = SendState.Aborted;
-
-                    //
-                    // There are multiple reasons the send could have been cancelled:
-                    //   - Connection was aborted (either by transport or peer) => error-code already provided on the connection-level event
-                    //   - Stream's receive side was aborted by peer => already handled by HandleEventPeerRecvAborted
-                    //     and we will not set the exception due to complete == false
-                    //   - Stream's send side was aborted locally => no connection-level abort code and we return QuicOperationAbortException
-                    //
-                    abortException = ThrowHelper.GetConnectionAbortedException(state.ConnectionState.AbortErrorCode);
                 }
             }
 
@@ -1295,14 +1286,22 @@ namespace System.Net.Quic.Implementations.MsQuic
             {
                 CleanupSendState(state);
 
-                if (abortException == null)
+                if (!canceled)
                 {
                     state.SendResettableCompletionSource.Complete(MsQuicStatusCodes.Success);
                 }
                 else
                 {
+                    //
+                    // There are multiple reasons the send could have been cancelled:
+                    //   - Connection was aborted (either by transport or peer) => error-code already provided on the connection-level event
+                    //   - Stream's receive side was aborted by peer => already handled by HandleEventPeerRecvAborted
+                    //     and we will not set the exception due to complete == false
+                    //   - Stream's send side was aborted locally => no connection-level abort code and we return QuicOperationAbortException
+                    //
                     state.SendResettableCompletionSource.CompleteException(
-                        ExceptionDispatchInfo.SetCurrentStackTrace(abortException));
+                        ExceptionDispatchInfo.SetCurrentStackTrace(
+                            ThrowHelper.GetConnectionAbortedException(state.ConnectionState.AbortErrorCode)));
                 }
             }
 
