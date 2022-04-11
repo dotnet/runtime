897fdb959bbf0790e951915066cde664a960c568
Tanner Gooding <tagoo@outlook.com>
2022-04-11 08:59:59 -0700

    Adding barebones Int128 and UInt128 structs

---
+++ src/libraries/System.Private.CoreLib/src/System/Int128.cs
@@ -0,0 +1,30 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Runtime.InteropServices;
+
+namespace System
+{
+    /// <summary>Represents a 128-bit signed integer.</summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public readonly struct Int128
+    {
+#if BIGENDIAN
+        private readonly ulong _upper;
+        private readonly ulong _lower;
+#else
+        private readonly ulong _lower;
+        private readonly ulong _upper;
+#endif
+
+        /// <summary>Initializes a new instance of the <see cref="Int128" /> struct.</summary>
+        /// <param name="upper">The upper 64-bits of the 128-bit value.</param>
+        /// <param name="lower">The lower 64-bits of the 128-bit value.</param>
+        [CLSCompliant(false)]
+        public Int128(ulong upper, ulong lower)
+        {
+            _lower = lower;
+            _upper = upper;
+        }
+    }
+}
