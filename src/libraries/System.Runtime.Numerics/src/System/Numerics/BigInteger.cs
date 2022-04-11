8f684fc248f293cc644e897f3d98a9b73cff5fce
Drew Kersnar <drewkersnar@microsoft.com>
2022-04-11 13:20:14 -0500

    Fix #65633, multiplication overflow causing incorrect right shift results

--- src/libraries/System.Runtime.Numerics/src/System/Numerics/BigInteger.cs
+++ src/libraries/System.Runtime.Numerics/src/System/Numerics/BigInteger.cs
@@ -2204,7 +2204,7 @@ namespace System.Numerics
 
             if (negx)
             {
-                if (shift >= (kcbitUint * xd.Length))
+                if (shift >= ((long) kcbitUint * xd.Length))
                 {
                     result = MinusOne;
                     goto exit;
