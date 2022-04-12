6f932bb783cdf52800122bad12deb5bb6f405329
Mikel Blanchard <mblanchard@macrosssoftware.com>
2022-04-12 15:54:35 -0700

    Verify foreach in tests.

--- src/libraries/System.Diagnostics.DiagnosticSource/tests/ActivityTests.cs
+++ src/libraries/System.Diagnostics.DiagnosticSource/tests/ActivityTests.cs
@@ -172,7 +172,8 @@ namespace System.Diagnostics.Tests
         [ConditionalFact(typeof(RemoteExecutor), nameof(RemoteExecutor.IsSupported))]
         public void TestBaggageWithChainedActivities()
         {
-            RemoteExecutor.Invoke(() => {
+            RemoteExecutor.Invoke(() =>
+            {
                 Activity a1 = new Activity("a1");
                 a1.Start();
 
@@ -763,7 +764,7 @@ namespace System.Diagnostics.Tests
             {
                 Activity activity = new Activity("activity15");
                 activity.Start();
-                 Assert.Equal(ActivityIdFormat.Hierarchical, activity.IdFormat);
+                Assert.Equal(ActivityIdFormat.Hierarchical, activity.IdFormat);
             }, new RemoteInvokeOptions() { StartInfo = psi }).Dispose();
         }
 
@@ -1611,7 +1612,7 @@ namespace System.Diagnostics.Tests
                 Assert.Equal(tags[i].Value, tagObjects[i].Value);
             }
 
-            activity.AddTag("s4", (object) null);
+            activity.AddTag("s4", (object)null);
             Assert.Equal(4, activity.Tags.Count());
             Assert.Equal(4, activity.TagObjects.Count());
             tags = activity.Tags.ToArray();
@@ -1732,9 +1733,9 @@ namespace System.Diagnostics.Tests
 
 
         [Theory]
-        [InlineData("key1", null, true,  1)]
+        [InlineData("key1", null, true, 1)]
         [InlineData("key2", null, false, 0)]
-        [InlineData("key3", "v1", true,  1)]
+        [InlineData("key3", "v1", true, 1)]
         [InlineData("key4", "v2", false, 1)]
         public void TestInsertingFirstTag(string key, object value, bool add, int resultCount)
         {
@@ -1829,8 +1830,8 @@ namespace System.Diagnostics.Tests
             Assert.Equal(ActivityStatusCode.Error, a.Status);
             Assert.Equal("Another Error Code Description", a.StatusDescription);
 
-            a.SetStatus((ActivityStatusCode) 100, "Another Error Code Description");
-            Assert.Equal((ActivityStatusCode) 100, a.Status);
+            a.SetStatus((ActivityStatusCode)100, "Another Error Code Description");
+            Assert.Equal((ActivityStatusCode)100, a.Status);
             Assert.Null(a.StatusDescription);
         }
 
@@ -1891,7 +1892,7 @@ namespace System.Diagnostics.Tests
             RemoteExecutor.Invoke(() =>
             {
                 Random random = new Random();
-                byte [] traceIdBytes = new byte[16];
+                byte[] traceIdBytes = new byte[16];
 
                 Activity.TraceIdGenerator = () =>
                 {
@@ -1939,6 +1940,12 @@ namespace System.Diagnostics.Tests
             Assert.False(enumerator.MoveNext());
 
             Assert.Equal(a.TagObjects, values);
+
+            foreach (ref readonly KeyValuePair<string, object?> tag in a.EnumerateTagObjects())
+            {
+                Assert.Equal(values[0], tag);
+                values.RemoveAt(0);
+            }
         }
 
         [Fact]
@@ -1967,6 +1974,12 @@ namespace System.Diagnostics.Tests
             Assert.False(enumerator.MoveNext());
 
             Assert.Equal(a.Events, values);
+
+            foreach (ref readonly ActivityEvent activityEvent in a.EnumerateEvents())
+            {
+                Assert.Equal(values[0], activityEvent);
+                values.RemoveAt(0);
+            }
         }
 
         [Fact]
@@ -2015,6 +2028,12 @@ namespace System.Diagnostics.Tests
             Assert.False(enumerator.MoveNext());
 
             Assert.Equal(a.Links, values);
+
+            foreach (ref readonly ActivityLink activityLink in a.EnumerateLinks())
+            {
+                Assert.Equal(values[0], activityLink);
+                values.RemoveAt(0);
+            }
         }
 
         public void Dispose()
