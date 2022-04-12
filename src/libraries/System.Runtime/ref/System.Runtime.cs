59e16e4a89248893b11cbf79c950488b70114bde
Fred Silberberg <frsilb@microsoft.com>
2022-04-12 18:33:39 +0000

    Add CompilerFeatureRequiredAttribute
    Closes https://github.com/dotnet/runtime/issues/66167.

--- src/libraries/System.Runtime/ref/System.Runtime.cs
+++ src/libraries/System.Runtime/ref/System.Runtime.cs
@@ -11275,6 +11275,15 @@ namespace System.Runtime.CompilerServices
         public CompilationRelaxationsAttribute(System.Runtime.CompilerServices.CompilationRelaxations relaxations) { }
         public int CompilationRelaxations { get { throw null; } }
     }
+    [System.AttributeUsage(System.AttributeTargets.All, AllowMultiple = true, Inherited = false)]
+    public sealed class CompilerFeatureRequiredAttribute : System.Attribute
+    {
+        public CompilerFeatureRequiredAttribute(string featureName) { }
+        public string FeatureName { get { throw null; } }
+        public bool IsOptional { get { throw null; } init { throw null; } }
+        public const string RefStructs = "RefStructs";
+        public const string RequiredMembers = "RequiredMembers";
+    }
     [System.AttributeUsageAttribute(System.AttributeTargets.All, Inherited=true)]
     public sealed partial class CompilerGeneratedAttribute : System.Attribute
     {
