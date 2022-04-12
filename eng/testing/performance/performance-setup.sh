c094bc8a4e201367f93ddcb455585bdd2b9f26ce
Parker Bibus <parkerbibus@microsoft.com>
2022-04-12 14:33:14 -0700

    Reset Testing repo to main.

--- eng/testing/performance/performance-setup.sh
+++ eng/testing/performance/performance-setup.sh
@@ -299,7 +299,7 @@ if [[ "$run_from_perf_repo" == true ]]; then
     performance_directory=$workitem_directory
     setup_arguments="--perf-hash $commit_sha $common_setup_arguments"
 else
-    git clone --branch iosstartup --depth 1 https://github.com/akoeplinger/performance.git $performance_directory
+    git clone --branch main --depth 1 --quiet https://github.com/dotnet/performance.git $performance_directory
     # uncomment to use BenchmarkDotNet sources instead of nuget packages
     # git clone https://github.com/dotnet/BenchmarkDotNet.git $benchmark_directory
 
