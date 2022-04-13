e3aad71a1c53ef18054a8c709edbec9c2b945bb3
Aaron Robinson <arobins@microsoft.com>
2022-04-12 17:01:18 -0700

    Box can throw expcetions other than OOM.

--- src/coreclr/jit/utils.cpp
+++ src/coreclr/jit/utils.cpp
@@ -1285,7 +1285,6 @@ void HelperCallProperties::init()
             case CORINFO_HELP_NEWSFAST_FINALIZE:
             case CORINFO_HELP_NEWSFAST_ALIGN8_FINALIZE:
             case CORINFO_HELP_READYTORUN_NEW:
-            case CORINFO_HELP_BOX:
 
                 isAllocator   = true;
                 nonNullReturn = true;
@@ -1300,6 +1299,7 @@ void HelperCallProperties::init()
             case CORINFO_HELP_NEWARR_1_DIRECT:
             case CORINFO_HELP_NEWARR_1_OBJ:
             case CORINFO_HELP_READYTORUN_NEWARR_1:
+            case CORINFO_HELP_BOX:
 
                 isAllocator   = true;
                 nonNullReturn = true;
