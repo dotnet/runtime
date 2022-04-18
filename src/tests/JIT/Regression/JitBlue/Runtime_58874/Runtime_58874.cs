d8b518403b931bdb248d366ab60bfd0f473da41f
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2022-04-18 15:25:25 +0200

    Do not leak in case of merged tests

--- src/tests/JIT/Regression/JitBlue/Runtime_58874/Runtime_58874.cs
+++ src/tests/JIT/Regression/JitBlue/Runtime_58874/Runtime_58874.cs
@@ -8,46 +8,81 @@ unsafe class Runtime_58874
 {
     private static int Main(string[] args)
     {
-        void* mem;
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        using EndOfPage endOfPage = EndOfPage.Create();
+        if (endOfPage == null)
         {
-            const int MEM_COMMIT = 0x1000;
-            const int MEM_RESERVE = 0x2000;
-            const int PAGE_READWRITE = 0x04;
-
-            // Reserve 2 pages
-            void* pages = VirtualAlloc(null, 0x2000, MEM_RESERVE, PAGE_READWRITE);
-            if (pages == null)
-            {
-                return 100;
-            }
-            // Commit first page
-            mem = VirtualAlloc(pages, 0x1000, MEM_COMMIT, PAGE_READWRITE);
-            if (mem != pages)
-            {
-                return 100;
-            }
-        }
-        else
-        {
-            mem = NativeMemory.Alloc(0x1000);
+            return 100;
         }
 
-        ref Test validRef = ref *(Test*)((byte*)mem + 0x1000 - sizeof(Test));
-        validRef = default;
-        Foo(ref validRef);
+        Foo(endOfPage.Pointer);
         return 100;
     }
 
     [MethodImpl(MethodImplOptions.NoInlining)]
-    private static Test Foo(ref Test t)
+    private static Test Foo(Test* t)
     {
         // This read was too wide.
-        return t;
+        return *t;
     }
 
-    [DllImport("kernel32")]
-    private static extern void* VirtualAlloc(void* lpAddress, nuint dwSize, uint flAllocationType, uint flProtect);
+    private class EndOfPage : IDisposable
+    {
+        private void* _addr;
+        private EndOfPage()
+        {
+        }
+
+        public Test* Pointer => (Test*)((byte*)_addr + 0x1000 - sizeof(Test));
+        public void Dispose()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                const int MEM_RELEASE = 0x8000;
+                VirtualFree(_addr, 0, MEM_RELEASE);
+            }
+            else
+            {
+                NativeMemory.Free(_addr);
+            }
+        }
+
+        public static EndOfPage Create()
+        {
+            void* mem;
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                const int MEM_COMMIT = 0x1000;
+                const int MEM_RESERVE = 0x2000;
+                const int PAGE_READWRITE = 0x04;
+
+                // Reserve 2 pages
+                void* pages = VirtualAlloc(null, 0x2000, MEM_RESERVE, PAGE_READWRITE);
+                if (pages == null)
+                {
+                    return null;
+                }
+                // Commit first page
+                mem = VirtualAlloc(pages, 0x1000, MEM_COMMIT, PAGE_READWRITE);
+                if (mem != pages)
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                mem = NativeMemory.Alloc(0x1000);
+            }
+
+            return new EndOfPage { _addr = mem };
+        }
+
+        [DllImport("kernel32")]
+        private static extern void* VirtualAlloc(void* lpAddress, nuint dwSize, uint flAllocationType, uint flProtect);
+
+        [DllImport("kernel32")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool VirtualFree(void* lpAddress, nuint dwSize, uint dwFreeType);
+    }
 }
 
 struct Test
