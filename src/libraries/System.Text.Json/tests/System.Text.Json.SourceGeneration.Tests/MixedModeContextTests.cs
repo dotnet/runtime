802a4a1df303be9cbe134629d14b1fb65e09ebdb
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2022-04-20 09:23:28 +0100

    Enable System.Text.Json tests on netfx (#63803) (#67007)
    * Enable System.Text.Json tests on netfx (#63803)
    
    * Enable System.Text.Json tests on netfx
    
    * use NETFRAMEWORK define
    
    * disable another two test cases after rebase
    
    * disable last test case which repros only on CI for me
    
    * add p2p only on netfx
    
    * Remove hardcoded assembly qualified names
    
    * Address feedback
    
    Co-authored-by: Krzysztof Wicher <kwicher@microsoft.com>

--- src/libraries/System.Text.Json/tests/System.Text.Json.SourceGeneration.Tests/MixedModeContextTests.cs
+++ src/libraries/System.Text.Json/tests/System.Text.Json.SourceGeneration.Tests/MixedModeContextTests.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Text.Json.Serialization;
+using System.Reflection;
 using Xunit;
 
 namespace System.Text.Json.SourceGeneration.Tests
@@ -48,6 +49,7 @@ namespace System.Text.Json.SourceGeneration.Tests
     internal partial class MixedModeContext : JsonSerializerContext, ITestContext
     {
         public JsonSourceGenerationMode JsonSourceGenerationMode => JsonSourceGenerationMode.Metadata | JsonSourceGenerationMode.Serialization;
+        public bool IsIncludeFieldsEnabled => GetType().GetCustomAttribute<JsonSourceGenerationOptionsAttribute>()?.IncludeFields ?? false;
     }
 
     public sealed class MixedModeContextTests : RealWorldContextTests
