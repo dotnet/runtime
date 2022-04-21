22d0a27f59ab0b66c42d923a35a5b6f33bc5418a
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2022-04-21 18:27:01 +0200

    Add comment

--- src/coreclr/jit/buildstring.cpp
+++ src/coreclr/jit/buildstring.cpp
@@ -39,5 +39,7 @@
 #define DLLEXPORT
 #endif
 
+// This string is used by SuperPMI when measuring throughput impact of changes
+// to validate that the baseline and release JITs are comparable.
 extern "C" DLLEXPORT const char jitBuildString[] =
     "RyuJIT built by " BUILD_COMPILER " targeting " TARGET_OS_STRING "-" TARGET_ARCH_STRING;
