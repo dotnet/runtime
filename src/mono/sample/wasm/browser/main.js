35985591b93cfe94fb60f7cbc7c58195f6a3c0aa
DESKTOP-GEPIA6N\Thays <thaystg@gmail.com>
2022-04-22 18:52:37 -0300

    fix debugging wasm sample

--- src/mono/sample/wasm/browser/main.js
+++ src/mono/sample/wasm/browser/main.js
@@ -1,3 +1,5 @@
+var BINDING_GLOBAL;
+document.getElementById("demo").onclick = function() {click()};
 function wasm_exit(exit_code) {
     /* Set result in a tests_done element, to be read by xharness in runonly CI test */
     const tests_done_elem = document.createElement("label");
@@ -22,6 +24,9 @@ async function main() {
             return {
                 disableDotnet6Compatibility: true,
                 configSrc: "./mono-config.json",
+                locateFile: (path, prefix) => {
+                    return window.location.protocol + "//" + location.host + "/" + prefix + path;
+                },
                 preInit: () => { console.log('user code Module.preInit') },
                 preRun: () => { console.log('user code Module.preRun') },
                 onRuntimeInitialized: () => { console.log('user code Module.onRuntimeInitialized') },
@@ -37,10 +42,16 @@ async function main() {
         console.debug(`ret: ${ret}`);
         let exit_code = ret == 42 ? 0 : 1;
         wasm_exit(exit_code);
+        BINDING_GLOBAL = BINDING;
     } catch (err) {
         console.log(`WASM ERROR ${err}`);
         wasm_exit(2)
     }
 }
 
+function click() {
+    const testMeaning = BINDING_GLOBAL.bind_static_method("[Wasm.Browser.CJS.Sample] Sample.Test:TestMeaning");
+    const ret = testMeaning();
+    document.getElementById("out2").innerHTML = `${ret}`;
+}
 main();
