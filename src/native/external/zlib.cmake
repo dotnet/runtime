f5f0fa3272522bb1a268bde85471198da8d4e935
Aleksey Kliger <alklig@microsoft.com>
2022-04-22 11:03:38 -0400

    [zlib] Turn off warnings as errors for -Wstrict-prototypes
    until upstream https://github.com/madler/zlib/issues/633 is resolved

--- src/native/external/zlib.cmake
+++ src/native/external/zlib.cmake
@@ -24,3 +24,8 @@ set(ZLIB_SOURCES_BASE
 )
 
 addprefix(ZLIB_SOURCES "${CMAKE_CURRENT_LIST_DIR}/zlib"  "${ZLIB_SOURCES_BASE}")
+
+if (CMAKE_C_COMPILER_ID MATCHES "Clang")
+  # Turn off warn-as-error for strict prototype checking until https://github.com/madler/zlib/issues/633 is fixed upstream
+  set_source_files_properties(${ZLIB_SOURCES} PROPERTIES COMPILE_OPTIONS "-Wno-error=strict-prototypes")
+endif()
