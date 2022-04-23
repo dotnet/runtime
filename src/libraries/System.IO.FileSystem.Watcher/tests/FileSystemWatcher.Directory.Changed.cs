b373e7fa68330412647406799bdf96f1aa19f532
Danyy427 <56359485+Danyy427@users.noreply.github.com>
2022-04-23 22:19:08 +0300

    Reverted changes

--- src/libraries/System.IO.FileSystem.Watcher/tests/FileSystemWatcher.Directory.Changed.cs
+++ src/libraries/System.IO.FileSystem.Watcher/tests/FileSystemWatcher.Directory.Changed.cs
@@ -65,8 +65,9 @@ namespace System.IO.Tests
         public void FileSystemWatcher_Directory_Changed_SymLink()
         {
             string dir = Path.Combine(TestDirectory, "dir");
-            Directory.CreateDirectory(Path.Combine(dir, "tempDir"));
-            string file = Path.Combine(dir, "tempDir", "test");
+            string tempDir = Path.Combine(dir, "tempDir");
+            string file = Path.Combine(tempDir, "test");
+            Directory.CreateDirectory(tempDir);
             
             File.Create(file).Dispose();
             using (var watcher = new FileSystemWatcher(dir, "*"))
