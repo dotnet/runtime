a046b08661dfb7b480a029e57171f18be3412c4e
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2022-04-24 13:47:07 +0200

    Fix stale comment

--- src/coreclr/jit/lir.cpp
+++ src/coreclr/jit/lir.cpp
@@ -1557,8 +1557,6 @@ bool LIR::Range::CheckLIR(Compiler* compiler, bool checkUnusedValues) const
                 // Stack arguments do not produce a value, but they are considered children of the call.
                 // It may be useful to remove these from being call operands, but that may also impact
                 // other code that relies on being able to reach all the operands from a call node.
-                // The GT_NOP case is because sometimes we eliminate stack argument stores as dead, but
-                // instead of removing them we replace with a NOP.
                 // The argument of a JTRUE doesn't produce a value (just sets a flag).
                 assert(((node->OperGet() == GT_CALL) && def->OperIs(GT_PUTARG_STK)) ||
                        ((node->OperGet() == GT_JTRUE) && (def->TypeGet() == TYP_VOID) &&
