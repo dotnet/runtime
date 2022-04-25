63147d539c02a6548557a5fbcdfaf75a2f244331
Tanner Gooding <tagoo@outlook.com>
2022-04-25 07:57:31 -0700

    Ensure that System.Guid can build successfully

--- src/libraries/System.Private.CoreLib/src/System/Guid.cs
+++ src/libraries/System.Private.CoreLib/src/System/Guid.cs
@@ -5,6 +5,7 @@ using System.Buffers.Binary;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
+using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Runtime.Intrinsics;
