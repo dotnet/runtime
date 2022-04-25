85910e923d905b1c4117d5715d85dea2de73b524
madelson <1269046+madelson@users.noreply.github.com>
2022-04-25 10:25:39 -0400

    Vectorize Guid equality. (#66889)
    * Vectorize Guid equality.
    
    * Update src/libraries/System.Private.CoreLib/src/System/Guid.cs
    
    Feedback from https://github.com/dotnet/runtime/pull/66889#discussion_r830625854
    
    Co-authored-by: Jan Kotas <jkotas@microsoft.com>
    
    Co-authored-by: Jan Kotas <jkotas@microsoft.com>

--- src/libraries/System.Private.CoreLib/src/System/Guid.cs
+++ src/libraries/System.Private.CoreLib/src/System/Guid.cs
@@ -5,9 +5,9 @@ using System.Buffers.Binary;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
-using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics;
 using System.Runtime.Versioning;
 
 namespace System
@@ -870,6 +870,11 @@ namespace System
 
         private static bool EqualsCore(in Guid left, in Guid right)
         {
+            if (Vector128.IsHardwareAccelerated)
+            {
+                return Vector128.LoadUnsafe(ref Unsafe.As<Guid, byte>(ref Unsafe.AsRef(in left))) == Vector128.LoadUnsafe(ref Unsafe.As<Guid, byte>(ref Unsafe.AsRef(in right)));
+            }
+
             ref int rA = ref Unsafe.AsRef(in left._a);
             ref int rB = ref Unsafe.AsRef(in right._a);
 
