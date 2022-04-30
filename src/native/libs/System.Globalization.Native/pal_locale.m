8db8113b2cdff24d61d657229fa4c373cc9bef5b
Steve Pfister <steve.pfister@microsoft.com>
2022-04-30 00:35:52 -0400

    Check LANG env variable first

--- src/native/libs/System.Globalization.Native/pal_locale.m
+++ src/native/libs/System.Globalization.Native/pal_locale.m
@@ -1,19 +1,31 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#include <stdlib.h>
 #include "pal_locale_internal.h"
+
 #import <Foundation/Foundation.h>
 
 const char* DetectDefaultAppleLocaleName()
 {
     NSLocale *currentLocale = [NSLocale currentLocale];
     NSString *localeName = @"";
+    const char* envLocaleName;
+    
+    // Match the ICU behavior where the default locale can be overriden by 
+    // and env variable.
+    envLocaleName = getenv("LANG");
+
+    if (envLocaleName != NULL)
+    {
+        return envLocaleName;
+    }
     
     if (!currentLocale)
     {
         return strdup([localeName UTF8String]);
     }
-    
+
     if ([currentLocale.languageCode length] > 0 && [currentLocale.countryCode length] > 0)
     {
         localeName = [NSString stringWithFormat:@"%@-%@", currentLocale.languageCode, currentLocale.countryCode];
@@ -25,3 +37,4 @@ const char* DetectDefaultAppleLocaleName()
     
     return strdup([localeName UTF8String]);
 }
+
