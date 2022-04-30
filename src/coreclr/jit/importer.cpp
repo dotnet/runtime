dae04e8a01ec4ad7dc1d069fc21f7375ae064934
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2022-04-30 23:58:28 +0200

    Inline local var

--- src/coreclr/jit/importer.cpp
+++ src/coreclr/jit/importer.cpp
@@ -842,8 +842,7 @@ void Compiler::impPopCallArgs(CORINFO_SIG_INFO* sig, GenTreeCall* call)
     // actual order so that we can walk the signature at the same time.
     for (unsigned stackIndex = sig->numArgs; stackIndex > 0; stackIndex--)
     {
-        unsigned          fromTopIndex = stackIndex - 1;
-        const StackEntry& se           = impStackTop(fromTopIndex);
+        const StackEntry& se           = impStackTop(stackIndex - 1);
 
         typeInfo ti      = se.seTypeInfo;
         GenTree* argNode = se.val;
