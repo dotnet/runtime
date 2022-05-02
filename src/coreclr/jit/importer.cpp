7166b2e0dcc6ed267e15a0e474c6ce3f0c54c3b7
Tanner Gooding <tagoo@outlook.com>
2022-05-02 08:16:35 -0700

    Implement NI_SRCS_UNSAFE_SubtractByteOffset

--- src/coreclr/jit/importer.cpp
+++ src/coreclr/jit/importer.cpp
@@ -5158,7 +5158,12 @@ GenTree* Compiler::impSRCSUnsafeIntrinsic(NamedIntrinsic        intrinsic,
             // sub
             // ret
 
-            return nullptr;
+            GenTree* op2 = impPopStack().val;
+            GenTree* op1 = impPopStack().val;
+            impBashVarAddrsToI(op1, op2);
+
+            var_types type = impGetByRefResultType(GT_SUB, /* uns */ false, &op1, &op2);
+            return gtNewOperNode(GT_SUB, type, op1, op2);
         }
 
         case NI_SRCS_UNSAFE_Unbox:
