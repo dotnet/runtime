39a10d3fb544457a75bfb272c06c9f865772e381
WeiZheng <13881045+wzchua@users.noreply.github.com>
2022-05-08 19:24:12 +0800

    Fix NRE in EmitCall for DynamicMethod

---
+++ src/libraries/System.Reflection.Emit.ILGeneration/tests/ILGenerator/Emit5Tests.cs
@@ -0,0 +1,61 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Xunit;
+
+namespace System.Reflection.Emit.Tests
+{
+    public sealed class ILGeneratorEmit5
+    {
+        public interface IHelper
+        {
+            int PassThrough(in int value);
+        }
+
+        public sealed class Helper : IHelper
+        {
+            public int PassThrough(in int value) => value;
+        }
+
+        [Fact]
+        public void TestEmitCallFunctionWithInArgumentFromDynamicMethod()
+        {
+            var methodToCall = typeof(IHelper).GetMethod("PassThrough");
+
+            var dynamicMethod = new DynamicMethod("CallingPassThrough",
+                MethodAttributes.Public | MethodAttributes.Static,
+                CallingConventions.Standard,
+                typeof(int),
+                new[]
+                {
+                  typeof(IHelper),
+                  typeof(int),
+                },
+                typeof(ILGeneratorEmit5),
+                true);
+
+            var il = dynamicMethod.GetILGenerator();
+
+            var copy = il.DeclareLocal(typeof(int), false);
+
+            il.Emit(OpCodes.Ldarg_1);
+            il.Emit(OpCodes.Stloc, (ushort)copy.LocalIndex);
+
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ldloca, (ushort)copy.LocalIndex);
+
+            il.EmitCall(OpCodes.Callvirt, methodToCall, null);
+
+            il.Emit(OpCodes.Ret);
+
+            var func = (Func<IHelper, int, int>)dynamicMethod.CreateDelegate(typeof(Func<IHelper, int, int>));
+
+            var helperInstance = new Helper();
+
+            var sum = func(helperInstance, 888);
+
+            Assert.Equal(888, sum);
+        }
+    }
+
+}
