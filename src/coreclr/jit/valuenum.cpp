5ae19b79998e436efe84475280c678a1ebde858f
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2022-05-09 15:55:27 +0200

    Revert valuenum changes

--- src/coreclr/jit/valuenum.cpp
+++ src/coreclr/jit/valuenum.cpp
@@ -10,7 +10,6 @@ XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 */
 
-//
 #include "jitpch.h"
 #ifdef _MSC_VER
 #pragma hdrstop
@@ -579,7 +578,10 @@ T ValueNumStore::EvalOpSpecialized(VNFunc vnf, T v0)
                 {
                     UINT64 v0_unsigned = UINT64(v0);
 
-                    v0_unsigned = ((v0_unsigned >> 56) & 0xFF) | ((v0_unsigned >> 40) & 0xFF00) | ((v0_unsigned >> 24) & 0xFF0000) | ((v0_unsigned >> 8) & 0xFF000000) | ((v0_unsigned << 8) & 0xFF00000000) | ((v0_unsigned << 24) & 0xFF0000000000) | ((v0_unsigned << 40) & 0xFF000000000000) | ((v0_unsigned << 56) & 0xFF00000000000000);
+                    v0_unsigned = ((v0_unsigned >> 56) & 0xFF) | ((v0_unsigned >> 40) & 0xFF00) |
+                                  ((v0_unsigned >> 24) & 0xFF0000) | ((v0_unsigned >> 8) & 0xFF000000) |
+                                  ((v0_unsigned << 8) & 0xFF00000000) | ((v0_unsigned << 24) & 0xFF0000000000) |
+                                  ((v0_unsigned << 40) & 0xFF000000000000) | ((v0_unsigned << 56) & 0xFF00000000000000);
                     return T(v0_unsigned);
                 }
                 else
