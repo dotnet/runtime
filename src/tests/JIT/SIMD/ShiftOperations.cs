f629bfd0d2582017c14d872bab96f99cf2e9d898
Julie Lee <63486087+JulieLeeMSFT@users.noreply.github.com>
2022-05-13 12:11:12 -0700

    [Emit shlx, sarx, shrx] addressed feedback on unit test

--- src/tests/JIT/SIMD/ShiftOperations.cs
+++ src/tests/JIT/SIMD/ShiftOperations.cs
@@ -4,11 +4,12 @@ using System;
 using System.Runtime.CompilerServices;
 using System.Numerics;
 using System.Runtime.InteropServices;
+using System.Collections.Generic;
 
 public class Test
 {
     [MethodImpl(MethodImplOptions.NoInlining)]
-    private static uint Shlx32bit(uint x, int y) => x<< y;
+    private static uint Shlx32bit(uint x, int y) => x << y;
 
     [MethodImpl(MethodImplOptions.NoInlining)]
     private static int Sarx32bit(int x, int y) => x >> y;
@@ -29,29 +30,21 @@ public class Test
     private static ulong Shrx64bit(ulong x, int y) => x >> y;
 
     [MethodImpl(MethodImplOptions.NoInlining)]
-    private static unsafe ulong ShrxRef64bit(ulong *x, int y) => *x >> y;
+    private static unsafe ulong ShrxRef64bit(ulong* x, int y) => *x >> y;
 
     [MethodImpl(MethodImplOptions.NoInlining)]
     private static ulong Rorx(ulong x) => BitOperations.RotateRight(x, 2);
 
-    public int Validate(actual, expected)
-    {
-        if (expected != actual)
-        {
-            Console.WriteLine("Fail");
-            return 101;
-        }
-        return 100;
-    }
-
     public static unsafe int Main()
     {
-        int returnCode = 0;
+        const int PASS = 100;
+        const int FAIL = 101;
+        int returnCode = PASS;
 
         try
         {
-            uint  valUInt = 0;
-            int   valInt = 0;
+            uint valUInt = 0;
+            int valInt = 0;
             ulong valULong = 0;
             long valLong = 0;
             int shiftBy = 0;
@@ -70,7 +63,7 @@ public class Test
             // Shlx32bit tests
             //
 
-            Console.Write("### UnitTest: Shlx32bit ###############\n");
+            Console.WriteLine("### UnitTest: Shlx32bit ###############");
             uint[] valShlx32bit = new uint[] { 0, 8, 1, 1 };
             int[] shiftByShlx32bit = new int[] { 1, 1, 31, 33 };
             for (int idx = 0; idx < valShlx32bit.Length; idx++)
@@ -79,7 +72,10 @@ public class Test
                 shiftBy = shiftByShlx32bit[idx];
                 resUInt = Shlx32bit(valUInt, shiftBy);
                 expectedUInt = (uint)(valUInt * Math.Pow(2, (shiftBy % MOD32)));
-                returnCode = Validate<uint>(valUInt, shiftBy, resUInt, expectedUInt);
+                if (!Validate<uint>(valUInt, shiftBy, resUInt, expectedUInt))
+                {
+                    returnCode = FAIL;
+                }
             }
 
             // Test only on x64 and x86. There is a known undefined behavior for Arm64 and Arm.
@@ -89,14 +85,17 @@ public class Test
                 shiftBy = 1;
                 resUInt = Shlx32bit(valUInt, shiftBy);
                 expectedUInt = (uint)(valUInt * Math.Pow(2, (shiftBy % MOD32)));
-                returnCode = Validate<uint>(valUInt, shiftBy, resUInt, expectedUInt);
+                if (!Validate<uint>(valUInt, shiftBy, resUInt, expectedUInt))
+                {
+                    returnCode = FAIL;
+                }
             }
 
             //
             // Sarx32bit tests
             //
 
-            Console.Write("### UnitTest: Sarx32bit ###############\n");
+            Console.WriteLine("### UnitTest: Sarx32bit ###############");
             int[] valSarx32bit = new int[] { 0, -8, 1, 0x7FFFFFFF, 0x7FFFFFFF };
             int[] shiftBySarx32bit = new int[] { 1, 1, 33, 33, 30 };
             for (int idx = 0; idx < valSarx32bit.Length; idx++)
@@ -105,14 +104,17 @@ public class Test
                 shiftBy = shiftBySarx32bit[idx];
                 resInt = Sarx32bit(valInt, shiftBy);
                 expectedInt = (int)(valInt / Math.Pow(2, (shiftBy % MOD32)));
-                returnCode = Validate<int>(valInt, shiftBy, resInt, expectedInt);
+                if (!Validate<int>(valInt, shiftBy, resInt, expectedInt))
+                {
+                    returnCode = FAIL;
+                }
             }
 
             //
             // Shrx32bit tests
             //
 
-            Console.Write("### UnitTest: Shrx32bit ###############\n");
+            Console.WriteLine("### UnitTest: Shrx32bit ###############");
             uint[] valShrx32bit = new uint[] { 1, 8, 1, 0xFFFFFFFF, 0xFFFFFFFF };
             int[] shiftByShrx32bit = new int[] { 1, 2, 33, 31, 33 };
             for (int idx = 0; idx < valShrx32bit.Length; idx++)
@@ -121,25 +123,31 @@ public class Test
                 shiftBy = shiftByShrx32bit[idx];
                 resUInt = Shrx32bit(valUInt, shiftBy);
                 expectedUInt = (uint)(valUInt / Math.Pow(2, (shiftBy % MOD32)));
-                returnCode = Validate<uint>(valUInt, shiftBy, resUInt, expectedUInt);
+                if (!Validate<uint>(valUInt, shiftBy, resUInt, expectedUInt))
+                {
+                    returnCode = FAIL;
+                }
             }
 
             //
             // Ror tests
             //
 
-            Console.Write("### UnitTest: Ror ###############\n");
+            Console.WriteLine("### UnitTest: Ror ###############");
             valUInt = 0xFF;
             shiftBy = 2;
             resUInt = Ror(valUInt);
             expectedUInt = 0xC000003F;
-            returnCode = Validate<uint>(valUInt, shiftBy, resUInt, expectedUInt);
+            if (!Validate<uint>(valUInt, shiftBy, resUInt, expectedUInt))
+            {
+                returnCode = FAIL;
+            }
 
             //
             // Shlx64bit tests
             //
 
-            Console.Write("### UnitTest: Shlx64bit ###############\n");
+            Console.WriteLine("### UnitTest: Shlx64bit ###############");
             ulong[] valShlx64bit = new ulong[] { 0, 8, 1, 1, 0xFFFFFFFFFFFFFFFF };
             int[] shiftByShlx64bit = new int[] { 1, 1, 63, 65, 1 };
             for (int idx = 0; idx < valShlx64bit.Length; idx++)
@@ -151,14 +159,17 @@ public class Test
                     expectedULong = (ulong)(valULong ^ 1);
                 else
                     expectedULong = (ulong)(valULong * Math.Pow(2, (shiftBy % MOD64)));
-                returnCode = Validate<ulong>(valULong, shiftBy, resULong, expectedULong);
+                if (!Validate<ulong>(valULong, shiftBy, resULong, expectedULong))
+                {
+                    returnCode = FAIL;
+                }
             }
 
             //
             // Sarx64bit tests
             //
 
-            Console.Write("### UnitTest: Sarx64bit ###############\n");
+            Console.WriteLine("### UnitTest: Sarx64bit ###############");
             long[] valSarx64bit = new long[] { 1, -8, -8, 0x7FFFFFFFFFFFFFFF };
             int[] shiftBySarx64bit = new int[] { 1, 1, 65, 63 };
             for (int idx = 0; idx < valSarx64bit.Length; idx++)
@@ -168,18 +179,19 @@ public class Test
                 resLong = Sarx64bit(valLong, shiftBy);
                 if (idx == 3)
                     expectedLong = 0;
-                else if (idx == 4)
-                    expectedLong = 0x1FFFFFFFFFFFFFFF;
                 else
                     expectedLong = (long)(valLong / Math.Pow(2, (shiftBy % MOD64)));
-                returnCode = Validate<long>(valLong, shiftBy, resLong, expectedLong);
+                if (!Validate<long>(valLong, shiftBy, resLong, expectedLong))
+                {
+                    returnCode = FAIL;
+                }
             }
 
             //
             // Shrx64bit tests
             //
 
-            Console.Write("### UnitTest: Shrx64bit ###############\n");
+            Console.WriteLine("### UnitTest: Shrx64bit ###############");
             ulong[] valShrx64bit = new ulong[] { 1, 8, 8, 0xFFFFFFFFFFFFFFFF, 0x7FFFFFFFFFFFFFFF };
             int[] shiftByShrx64bit = new int[] { 1, 2, 65, 63, 65 };
             for (int idx = 0; idx < valShrx64bit.Length; idx++)
@@ -193,61 +205,69 @@ public class Test
                     expectedULong = 0x3FFFFFFFFFFFFFFF;
                 else
                     expectedULong = (ulong)(valULong / Math.Pow(2, (shiftBy % MOD64)));
-                returnCode = Validate<ulong>(valULong, shiftBy, resULong, expectedULong);
+                if (!Validate<ulong>(valULong, shiftBy, resULong, expectedULong))
+                {
+                    returnCode = FAIL;
+                }
             }
 
             //
             // ShrxRef64bit
             //
 
-            Console.Write("### UnitTest: ShrxRef64bit ###############\n");
+            Console.WriteLine("### UnitTest: ShrxRef64bit ###############");
             valULong = 8;
             shiftBy = 1;
             resULong = ShrxRef64bit(&valULong, shiftBy);
-            expectedULong = (ulong) (valULong / Math.Pow(2, (shiftBy % MOD64)));
-            returnCode = Validate<ulong>(valULong, shiftBy, resULong, expectedULong);
+            expectedULong = (ulong)(valULong / Math.Pow(2, (shiftBy % MOD64)));
+            if (!Validate<ulong>(valULong, shiftBy, resULong, expectedULong))
+            {
+                returnCode = FAIL;
+            }
 
             //
             // Rorx tests
             //
 
-            Console.Write("### UnitTest: Rorx ###############\n");
+            Console.WriteLine("### UnitTest: Rorx ###############");
             valULong = 0xFF;
             shiftBy = 2;
             resULong = Rorx(valULong);
             expectedULong = 0xC00000000000003F;
-            returnCode = Validate<ulong>(valULong, shiftBy, resULong, expectedULong);
+            if (!Validate<ulong>(valULong, shiftBy, resULong, expectedULong))
+            {
+                returnCode = FAIL;
+            }
         }
         catch (Exception e)
         {
             Console.WriteLine(e.Message);
-            return 101;
+            return FAIL;
         }
 
-        if (returnCode == 101)
-        {
-            Console.WriteLine("FAILED.");
-        }
-        else if (returnCode == 100)
+        if (returnCode == PASS)
         {
             Console.WriteLine("PASSED.");
         }
-
+        else
+        {
+            Console.WriteLine("FAILED.");
+        }
         return returnCode;
     }
 
-    private static int Validate<T>(T value, int shiftBy, T actual, T expected)
+    private static bool Validate<T>(T value, int shiftBy, T actual, T expected)
     {
         Console.Write("(value, shiftBy) ({0},{1}): {2}", value, shiftBy, actual);
         if (EqualityComparer<T>.Default.Equals(actual, expected))
         {
-            Console.Write(" == {0}   ==> Passed.\n", expected);
-            return 100;
+            Console.WriteLine(" == {0}   ==> Passed.", expected);
+            return true;
         }
         else
         {
-            Console.Write(" != {0}    ==> Failed.\n", expected);
-            return 101;
+            Console.WriteLine(" != {0}    ==> Failed.", expected);
+            return false;
         }
     }
 }
