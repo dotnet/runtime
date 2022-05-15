cb7fd0979ffc54ac97f91714ac202b2e08810183
Nathan Ricci <naricc@microsoft.com>
2022-05-15 10:14:44 -0400

    Fixed ifdefs around mono_marshal_noilgen_init_heavyweight/lightweight.

--- src/mono/mono/metadata/marshal-ilgen.h
+++ src/mono/mono/metadata/marshal-ilgen.h
@@ -27,13 +27,13 @@ mono_install_marshal_callbacks_ilgen (MonoMarshalIlgenCallbacks *cb);
 #ifdef ENABLE_ILGEN
 void
 mono_marshal_ilgen_init (void);
-#else
+#endif 
+
 void
 mono_marshal_noilgen_init_heavyweight (void);
 
 void
 mono_marshal_noilgen_init_lightweight (void);
-#endif 
 
 int
 mono_emit_marshal_ilgen (EmitMarshalContext *m, int argnum, MonoType *t,
