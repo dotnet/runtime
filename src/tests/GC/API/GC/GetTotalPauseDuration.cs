4079a8e0bec710c54dc080ca802c0530e4fbfe7e
Andrew Au <andrewau@microsoft.com>
2022-05-16 12:38:10 -0700

    Improve test

--- src/tests/GC/API/GC/GetTotalPauseDuration.cs
+++ src/tests/GC/API/GC/GetTotalPauseDuration.cs
@@ -7,20 +7,29 @@ using System.Diagnostics;
 
 public class Test_Collect
 {
-	public static int Main()
-	{
-		Stopwatch sw = Stopwatch.StartNew();
-		GC.Collect();
-		sw.Stop();
-		TimeSpan elapsed = sw.Elapsed;
-		TimeSpan totalPauseDuration = GC.GetTotalPauseDuration();
-		if (TimeSpan.Zero < totalPauseDuration && totalPauseDuration <= elapsed)
-		{
-			return 100;
-		}
-		else
-		{
-			return 101;
-		}		
-	}
+    public static int Main()
+    {
+        Stopwatch sw = Stopwatch.StartNew();
+        GC.Collect();
+        sw.Stop();
+        TimeSpan elapsed = sw.Elapsed;
+        TimeSpan totalPauseDuration = GC.GetTotalPauseDuration();
+        GCMemoryInfo memoryInfo = GC.GetGCMemoryInfo();
+        TimeSpan lastGcDuration = memoryInfo.PauseDurations[0];
+
+        // These conditions assume the only GC in the process 
+        // is the one we just triggered. This makes the test incompatible 
+        // with any changes that might introduce extra GCs.
+
+        if (TimeSpan.Zero < totalPauseDuration &&
+            totalPauseDuration <= elapsed &&
+            lastGcDuration == totalPauseDuration)
+        {
+            return 100;
+        }
+        else
+        {
+            return 101;
+        }
+    }
 }
