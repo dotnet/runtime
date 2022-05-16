566a0e05c083c585931faf317f10ca004b8d8b29
Robin Lindner <robin@deeprobin.de>
2022-05-16 21:02:20 +0200

    Remove unnecessary `#nullable` directives

--- src/libraries/Microsoft.Extensions.Hosting/src/HostAbortedException.cs
+++ src/libraries/Microsoft.Extensions.Hosting/src/HostAbortedException.cs
@@ -1,6 +1,5 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-#nullable enable
 using System;
 using System.Runtime.Serialization;
 
@@ -55,4 +54,3 @@ namespace Microsoft.Extensions.Hosting
         public HostAbortedException(string? message, Exception? innerException) : base(message, innerException) { }
     }
 }
-#nullable restore
