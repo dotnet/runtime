4ff3740b81b6620cb07a07386d93b5289c7f243f
Adeel <3840695+am11@users.noreply.github.com>
2022-05-17 02:24:24 +0300

    Delete the comment

--- src/coreclr/inc/winwrap.h
+++ src/coreclr/inc/winwrap.h
@@ -1,5 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
+
 //*****************************************************************************
 // WinWrap.h
 //
@@ -213,11 +214,6 @@
 #define WszGetEnvironmentVariable GetEnvironmentVariableWrapper
 #define WszSearchPath          SearchPathWrapper
 #define WszGetModuleFileName   GetModuleFileNameWrapper
-
-//NOTE: IF the following API's are enabled ensure that they can work with LongFile Names
-//See the usage and implementation of above API's
-//
-//#define WszGetBinaryType       GetBinaryTypeWrapper     //Coresys does not seem to have this API
 
 //*****************************************************************************
 // Prototypes for API's.
