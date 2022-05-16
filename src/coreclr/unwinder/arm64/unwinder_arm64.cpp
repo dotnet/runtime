c213c5bcb921c131e05e2ef3ff30de98e7ac9a71
Juan Hoyos <juan.hoyos@microsoft.com>
2022-05-16 15:09:18 -0700

    [release/6.0] Restore FP pairs when unwinding in ARM64 (#69359)
    * Add regression test
    * Restore FP pairs when unwinding in ARM64
    * Disable windows arm64 version of the test

--- src/coreclr/unwinder/arm64/unwinder_arm64.cpp
+++ src/coreclr/unwinder/arm64/unwinder_arm64.cpp
@@ -1038,7 +1038,7 @@ ExecuteCodes:
                         ContextRecord,
                         8 * (NextCode & 0x3f),
                         8 + ((CurCode & 1) << 2) + (NextCode >> 6),
-                        2 + AccumulatedSaveNexts,
+                        2 + 2 * AccumulatedSaveNexts,
                         UnwindParams);
             AccumulatedSaveNexts = 0;
         }
@@ -1054,7 +1054,7 @@ ExecuteCodes:
                         ContextRecord,
                         -8 * ((NextCode & 0x3f) + 1),
                         8 + ((CurCode & 1) << 2) + (NextCode >> 6),
-                        2 + AccumulatedSaveNexts,
+                        2 + 2 * AccumulatedSaveNexts,
                         UnwindParams);
             AccumulatedSaveNexts = 0;
         }
