6126396ae86c810574a387ecda197723a7942f7c
CyrusNajmabadi <cyrus.najmabadi@gmail.com>
2022-05-16 17:31:16 -0700

    Update src/libraries/System.Text.Json/gen/JsonSourceGenerator.Roslyn3.11.cs
    Co-authored-by: Sam Harwell <sam@tunnelvisionlabs.com>

--- src/libraries/System.Text.Json/gen/JsonSourceGenerator.Roslyn3.11.cs
+++ src/libraries/System.Text.Json/gen/JsonSourceGenerator.Roslyn3.11.cs
@@ -32,7 +32,7 @@ namespace System.Text.Json.SourceGeneration
             // (the one in GeneratorInitializationContext is not safe to capture). 
             // In practice this should still be ok as the generator driver itself will
             // cancel after every file it processes. 
-            context.RegisterForSyntaxNotifications(() => new SyntaxContextReceiver(CancellationToken.None));
+            context.RegisterForSyntaxNotifications(static () => new SyntaxContextReceiver(CancellationToken.None));
         }
 
         /// <summary>
