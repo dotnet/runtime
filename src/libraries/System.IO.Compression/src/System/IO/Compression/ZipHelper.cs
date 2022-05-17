49e5c7404a16eb713f9356df5f33548bc3558d53
Eric Erhardt <eric.erhardt@microsoft.com>
2022-05-17 10:33:32 -0500

    Reuse the throw away buffer in ZipHelper
    ZipHelper.AdvanceToPosition was allocating a new temporary buffer every time through the loop while reading from the stream. Changing the code to reuse the same buffer through the loop.

--- src/libraries/System.IO.Compression/src/System/IO/Compression/ZipHelper.cs
+++ src/libraries/System.IO.Compression/src/System/IO/Compression/ZipHelper.cs
@@ -163,14 +163,18 @@ namespace System.IO.Compression
         {
             long numBytesLeft = position - stream.Position;
             Debug.Assert(numBytesLeft >= 0);
-            while (numBytesLeft != 0)
+            if (numBytesLeft > 0)
             {
                 const int throwAwayBufferSize = 64;
-                int numBytesToSkip = (numBytesLeft > throwAwayBufferSize) ? throwAwayBufferSize : (int)numBytesLeft;
-                int numBytesActuallySkipped = stream.Read(new byte[throwAwayBufferSize], 0, numBytesToSkip);
-                if (numBytesActuallySkipped == 0)
-                    throw new IOException(SR.UnexpectedEndOfStream);
-                numBytesLeft -= numBytesActuallySkipped;
+                byte[] throwAwayBuffer = new byte[throwAwayBufferSize];
+                do
+                {
+                    int numBytesToSkip = (numBytesLeft > throwAwayBufferSize) ? throwAwayBufferSize : (int)numBytesLeft;
+                    int numBytesActuallySkipped = stream.Read(throwAwayBuffer, 0, numBytesToSkip);
+                    if (numBytesActuallySkipped == 0)
+                        throw new IOException(SR.UnexpectedEndOfStream);
+                    numBytesLeft -= numBytesActuallySkipped;
+                } while (numBytesLeft > 0);
             }
         }
 
