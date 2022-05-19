3de611dedd37f08dd2df9dcb1609e470b2d439fd
Drew Kersnar <18474647+dakersnar@users.noreply.github.com>
2022-05-19 12:43:33 -0500

    Ran jit-format

--- src/coreclr/jit/lowerxarch.cpp
+++ src/coreclr/jit/lowerxarch.cpp
@@ -1482,16 +1482,17 @@ void Lowering::LowerHWIntrinsicCndSel(GenTreeHWIntrinsic* node)
 
     // If the condition vector comes from a hardware intrinsic that
     // returns a per-element mask (marked with HW_Flag_ReturnsPerElementMask),
-    // we can optimize the entire conditional select to 
+    // we can optimize the entire conditional select to
     // a single BlendVariable instruction (if supported by the architecture)
 
     // First, determine if the condition is a per-element mask
     if (op1->OperIsHWIntrinsic() && HWIntrinsicInfo::ReturnsPerElementMask(op1->AsHWIntrinsic()->GetHWIntrinsicId()))
-    {   
+    {
         // Next, determine if the target architecture supports BlendVariable
         NamedIntrinsic blendVariableId = NI_Illegal;
 
-        // For Vector256 (simdSize == 32), BlendVariable for floats/doubles is available on AVX, whereas other types require AVX2
+        // For Vector256 (simdSize == 32), BlendVariable for floats/doubles is available on AVX, whereas other types
+        // require AVX2
         if (simdSize == 32)
         {
             if (varTypeIsFloating(simdBaseType))
@@ -1503,7 +1504,7 @@ void Lowering::LowerHWIntrinsicCndSel(GenTreeHWIntrinsic* node)
             else if (comp->compOpportunisticallyDependsOn(InstructionSet_AVX2))
             {
                 blendVariableId = NI_AVX2_BlendVariable;
-            }      
+            }
         }
         // For Vector128, BlendVariable is available on SSE41
         else if (comp->compOpportunisticallyDependsOn(InstructionSet_SSE41))
