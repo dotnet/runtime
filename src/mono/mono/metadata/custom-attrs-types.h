66db12d4063624e59ae5a9ac3e669d21394a2514
Ivan Povazan <ivan.povazan@gmail.com>
2022-05-20 11:16:49 +0200

    Removing type information from MonoCustomAttrValue - the type is available in CattrNameArg

--- src/mono/mono/metadata/custom-attrs-types.h
+++ src/mono/mono/metadata/custom-attrs-types.h
@@ -12,7 +12,6 @@ typedef struct _MonoCustomAttrValue {
 		gpointer primitive; /* int/enum/MonoType/string */
 		MonoCustomAttrValueArray *array;
 	} value;
-	MonoTypeEnum type : 8;
 } MonoCustomAttrValue;
 
 struct _MonoCustomAttrValueArray {
