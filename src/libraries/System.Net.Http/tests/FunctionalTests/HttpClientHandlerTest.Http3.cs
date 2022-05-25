5f8358d0e779b78b3e530255d8f29bea6e4b5772
Marie Píchová <11718369+ManickaP@users.noreply.github.com>
2022-05-25 14:28:02 +0200

    [HTTP/3] Added another H/3 server (#69786)
    * Added another H/3 server
    
    * Removed unused endpoint

--- src/libraries/System.Net.Http/tests/FunctionalTests/HttpClientHandlerTest.Http3.cs
+++ src/libraries/System.Net.Http/tests/FunctionalTests/HttpClientHandlerTest.Http3.cs
@@ -1430,9 +1430,7 @@ namespace System.Net.Http.Functional.Tests
             new TheoryData<string>
             {
                 { "https://cloudflare-quic.com/" }, // Cloudflare with content
-                // This endpoint is consistently failing.
-                // [ActiveIssue("https://github.com/dotnet/runtime/issues/63009")]
-                //{ "https://pgjones.dev/" }, // aioquic with content
+                { "https://quic.nginx.org/" }, // Nginx with content
             };
     }
 
