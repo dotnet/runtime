7b6e205a9b1e1ec7afa4226b9d616e1ed1e2284d
Jackson Schuster <jschuster@microsoft.com>
2022-05-26 09:30:51 -0500

    Remove fiji issue and change ilc bug

--- src/libraries/System.Runtime/tests/System/TimeZoneInfoTests.cs
+++ src/libraries/System.Runtime/tests/System/TimeZoneInfoTests.cs
@@ -2870,7 +2870,6 @@ namespace System.Tests
         }
 
         [Fact]
-        [ActiveIssue("https://github.com/dotnet/runtime/issues/69801", typeof(PlatformDetection), nameof(PlatformDetection.IsNativeAot))]
         public static void FijiTimeZoneTest()
         {
             TimeZoneInfo fijiTZ = TimeZoneInfo.FindSystemTimeZoneById(s_strFiji); // "Fiji Standard Time" - "Pacific/Fiji"
