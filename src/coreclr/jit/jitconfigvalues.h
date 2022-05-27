e727fba91f40096313200b52d0fb20e9bdebfc47
Aman Khalid <t-amankhalid@microsoft.com>
2022-05-27 14:11:31 -0700

    Fix unwind info when fake-splitting
    Fake-splitting currently breaks stack walks by not generating unwind
    info for cold code. This commit implements unwind info on x86/64 when
    fake-splitting by generating unwind info for the combined hot/cold
    section just once, rather than generating unwind info for the separate
    sections.
    
    For reasons to be investigated, this implementation does not work when
    the code sections are separated by an arbitrary buffer (such as the 4KB
    buffer previously used). Thus, the buffer has been removed from the
    fake-splitting implementation: Now, the hot and cold sections are
    placed contiguously in memory, but the JIT continues to behave as if
    they are arbitrarily far away (for example, by using long branches
    between sections).
    
    Following this fix, fake-splitting no longer requires the GC to be
    suppressed by setting `COMPlus_GCgen0size=1000000`. A test job
    has been added to `runtime-jit-experimental` to ensure
    fake/stress-splitting does not regress.

--- src/coreclr/jit/jitconfigvalues.h
+++ src/coreclr/jit/jitconfigvalues.h
@@ -196,11 +196,6 @@ CONFIG_INTEGER(JitDumpInlinePhases, W("JitDumpInlinePhases"), 1) // Dump inline 
 CONFIG_METHODSET(JitEHDump, W("JitEHDump")) // Dump the EH table for the method, as reported to the VM
 CONFIG_METHODSET(JitExclude, W("JitExclude"))
 CONFIG_INTEGER(JitFakeProcedureSplitting, W("JitFakeProcedureSplitting"), 0) // Do code splitting independent of VM.
-                                                                             // For now, this disables unwind info for
-                                                                             // cold sections, breaking stack walks.
-                                                                             // Set COMPlus_GCgen0size=1000000 to avoid
-                                                                             // running the GC, which requires
-                                                                             // stack-walking.
 CONFIG_METHODSET(JitForceProcedureSplitting, W("JitForceProcedureSplitting"))
 CONFIG_METHODSET(JitGCDump, W("JitGCDump"))
 CONFIG_METHODSET(JitDebugDump, W("JitDebugDump"))
