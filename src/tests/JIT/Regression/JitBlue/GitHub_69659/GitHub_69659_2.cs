37955fe58e1fa066472c397038a330ef1aaa3a0f
Kunal Pathak <Kunal.Pathak@microsoft.com>
2022-05-27 07:55:28 -0700

    Add test cases

---
+++ src/tests/JIT/Regression/JitBlue/GitHub_69659/GitHub_69659_2.cs
@@ -0,0 +1,42 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+public class _65659_2
+{
+    public static bool[][,] s_2;
+    public static short[,][] s_8;
+    public static bool[] s_10;
+    public static ushort[][] s_29;
+    public static int Main()
+    {
+        bool vr1 = M47();
+        return 100;
+    }
+
+    public static bool M47()
+    {
+        bool var0 = default(bool);
+        try
+        {
+            if (var0)
+            {
+                try
+                {
+                    if (s_10[0])
+                    {
+                        return s_2[0][0, 0];
+                    }
+                }
+                finally
+                {
+                    s_8[0, 0][0] = 0;
+                }
+            }
+        }
+        finally
+        {
+            s_29 = s_29;
+        }
+
+        return true;
+    }
+}
