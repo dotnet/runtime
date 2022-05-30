ec51212ea81b4825789d26449aee8b4f56220ab6
Tom Deseyn <tom.deseyn@gmail.com>
2022-05-30 13:36:40 +0200

    Implement SupportsSignature in test suite.

--- src/libraries/Common/tests/System/Security/Cryptography/SignatureSupport.cs
+++ src/libraries/Common/tests/System/Security/Cryptography/SignatureSupport.cs
@@ -1,69 +1,51 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-namespace System.Security.Cryptography
+namespace System.Security.Cryptography.Tests
 {
     internal static class SignatureSupport
     {
-        private static int _supportsRsaSha1Signatures = 0;
-
         internal static bool CanProduceSha1Signature(AsymmetricAlgorithm algorithm)
         {
-#if NETFRAMEWORK
-    return true;
-#else
-            try
+            // We expect all non-Linux platforms to support SHA1 signatures, currently.
+            if (!OperatingSystem.IsLinux())
             {
-                // We expect all non-Linux platforms to support SHA1 signatures, currently.
-                if (!OperatingSystem.IsLinux())
-                {
-                    return true;
-                }
+                return true;
+            }
 
-                switch (algorithm)
-                {
-                    case ECDsa ecdsa:
-                        try
-                        {
-                            ecdsa.SignData(Array.Empty<byte>(), HashAlgorithmName.SHA1);
-                            return true;
-                        }
-                        catch (CryptographicException)
-                        {
-                            return false;
-                        }
-                    case RSA rsa:
-                        try
-                        {
-                            rsa.SignData(Array.Empty<byte>(), HashAlgorithmName.SHA1, RSASignaturePadding.Pkcs1);
-                            return true;
-                        }
-                        catch (CryptographicException)
-                        {
-                            return false;
-                        }
-                    default:
-                        throw new NotSupportedException($"Algorithm type {algorithm.GetType()} is not supported.");
-                }
-            }
-            finally
+            switch (algorithm)
             {
-                algorithm.Dispose();
+                case ECDsa ecdsa:
+                    try
+                    {
+                        ecdsa.SignData(Array.Empty<byte>(), HashAlgorithmName.SHA1);
+                        return true;
+                    }
+                    catch (CryptographicException)
+                    {
+                        return false;
+                    }
+                    finally
+                    {
+                        algorithm.Dispose();
+                    }
+                case RSA rsa:
+                    try
+                    {
+                        rsa.SignData(Array.Empty<byte>(), HashAlgorithmName.SHA1, RSASignaturePadding.Pkcs1);
+                        return true;
+                    }
+                    catch (CryptographicException)
+                    {
+                        return false;
+                    }
+                    finally
+                    {
+                        algorithm.Dispose();
+                    }
+                default:
+                    throw new NotSupportedException($"Algorithm type {algorithm.GetType()} is not supported.");
             }
-#endif
         }
-
-        public static bool SupportsRsaSha1Signatures
-        {
-            get
-            {
-                if (_supportsRsaSha1Signatures == 0)
-                {
-                    bool supported = CanProduceSha1Signature(new RSACryptoServiceProvider());
-                    _supportsRsaSha1Signatures = supported ? 1 : -1;
-                }
-                return _supportsRsaSha1Signatures == 1;
-            }
-        }  
     }
 }
