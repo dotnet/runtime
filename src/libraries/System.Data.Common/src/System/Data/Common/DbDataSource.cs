a29d9e08594cdf1d14922996b21e8405164c0331
Shay Rojansky <roji@roji.org>
2022-05-31 11:51:05 +0200

    Implement DbDataSource
    Closes #64812

---
+++ src/libraries/System.Data.Common/src/System/Data/Common/DbDataSource.cs
@@ -0,0 +1,497 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace System.Data.Common
+{
+    public abstract class DbDataSource : IDisposable, IAsyncDisposable
+    {
+        public abstract string ConnectionString { get; }
+
+        protected abstract DbConnection CreateDbConnection();
+
+        protected virtual DbConnection OpenDbConnection()
+        {
+            var connection = CreateDbConnection();
+            connection.Open();
+
+            return connection;
+        }
+
+        protected virtual async ValueTask<DbConnection> OpenDbConnectionAsync(CancellationToken cancellationToken = default)
+        {
+            var connection = CreateDbConnection();
+            await connection.OpenAsync(cancellationToken).ConfigureAwait(false);
+
+            return connection;
+        }
+
+        protected virtual DbCommand CreateDbCommand(string? commandText = null)
+        {
+            var command = CreateDbConnection().CreateCommand();
+            command.CommandText = commandText;
+
+            return new DbCommandWrapper(command);
+        }
+
+        protected virtual DbBatch CreateDbBatch()
+            => new DbBatchWrapper(CreateDbConnection().CreateBatch());
+
+        public DbConnection CreateConnection() => CreateDbConnection();
+        public DbConnection OpenConnection() => OpenDbConnection();
+        public ValueTask<DbConnection> OpenConnectionAsync() => OpenDbConnectionAsync();
+        public DbCommand CreateCommand(string? commandText = null) => CreateDbCommand();
+        public DbBatch CreateBatch() => CreateDbBatch();
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await DisposeAsyncCore().ConfigureAwait(false);
+
+            Dispose(disposing: false);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+        }
+
+        protected virtual ValueTask DisposeAsyncCore()
+            => default;
+
+        private sealed class DbCommandWrapper : DbCommand
+        {
+            private readonly DbCommand _wrappedCommand;
+            private readonly DbConnection _connection;
+
+            internal DbCommandWrapper(DbCommand wrappedCommand)
+            {
+                Debug.Assert(wrappedCommand.Connection is not null);
+
+                _wrappedCommand = wrappedCommand;
+                _connection = wrappedCommand.Connection;
+            }
+
+            public override int ExecuteNonQuery()
+            {
+                _connection.Open();
+
+                try
+                {
+                    return _wrappedCommand.ExecuteNonQuery();
+                }
+                finally
+                {
+                    try
+                    {
+                        _connection.Close();
+                    }
+                    catch
+                    {
+                        // Swallow to allow the original exception to bubble up
+                    }
+                }
+            }
+
+            public override async Task<int> ExecuteNonQueryAsync(CancellationToken cancellationToken)
+            {
+                await _connection.OpenAsync(cancellationToken).ConfigureAwait(false);
+
+                try
+                {
+                    return await _wrappedCommand.ExecuteNonQueryAsync(cancellationToken)
+                        .ConfigureAwait(false);
+                }
+                finally
+                {
+                    try
+                    {
+                        await _connection.CloseAsync().ConfigureAwait(false);
+                    }
+                    catch
+                    {
+                        // Swallow to allow the original exception to bubble up
+                    }
+                }
+            }
+
+            public override object? ExecuteScalar()
+            {
+                _connection.Open();
+
+                try
+                {
+                    return _wrappedCommand.ExecuteScalar();
+                }
+                finally
+                {
+                    try
+                    {
+                        _connection.Close();
+                    }
+                    catch
+                    {
+                        // Swallow to allow the original exception to bubble up
+                    }
+                }
+            }
+
+            public override async Task<object?> ExecuteScalarAsync(CancellationToken cancellationToken)
+            {
+                await _connection.OpenAsync(cancellationToken).ConfigureAwait(false);
+
+                try
+                {
+                    return await _wrappedCommand.ExecuteScalarAsync(cancellationToken)
+                        .ConfigureAwait(false);
+                }
+                finally
+                {
+                    try
+                    {
+                        await _connection.CloseAsync().ConfigureAwait(false);
+                    }
+                    catch
+                    {
+                        // Swallow to allow the original exception to bubble up
+                    }
+                }
+            }
+
+            protected override DbDataReader ExecuteDbDataReader(CommandBehavior behavior)
+            {
+                _connection.Open();
+
+                try
+                {
+                    return _wrappedCommand.ExecuteReader(behavior | CommandBehavior.CloseConnection);
+                }
+                catch
+                {
+                    try
+                    {
+                        _connection.Close();
+                    }
+                    catch
+                    {
+                        // Swallow to allow the original exception to bubble up
+                    }
+
+                    throw;
+                }
+            }
+
+            protected override async Task<DbDataReader> ExecuteDbDataReaderAsync(
+                CommandBehavior behavior,
+                CancellationToken cancellationToken)
+            {
+                await _connection.OpenAsync(cancellationToken).ConfigureAwait(false);
+
+                try
+                {
+                    return await _wrappedCommand.ExecuteReaderAsync(
+                            behavior | CommandBehavior.CloseConnection,
+                            cancellationToken)
+                        .ConfigureAwait(false);
+                }
+                catch
+                {
+                    try
+                    {
+                        await _connection.CloseAsync().ConfigureAwait(false);
+                    }
+                    catch
+                    {
+                        // Swallow to allow the original exception to bubble up
+                    }
+
+                    throw;
+                }
+            }
+
+            protected override DbParameter CreateDbParameter()
+                => _wrappedCommand.CreateParameter();
+
+            public override void Cancel()
+                => _wrappedCommand.Cancel();
+
+            public override void Prepare()
+                => _wrappedCommand.Prepare();
+
+            public override Task PrepareAsync(CancellationToken cancellationToken = default)
+                => _wrappedCommand.PrepareAsync(cancellationToken);
+
+            [AllowNull]
+            public override string CommandText
+            {
+                get => _wrappedCommand.CommandText;
+                set => _wrappedCommand.CommandText = value;
+            }
+
+            public override int CommandTimeout
+            {
+                get => _wrappedCommand.CommandTimeout;
+                set => _wrappedCommand.CommandTimeout = value;
+            }
+
+            public override CommandType CommandType
+            {
+                get => _wrappedCommand.CommandType;
+                set => _wrappedCommand.CommandType = value;
+            }
+
+            protected override DbParameterCollection DbParameterCollection
+                => _wrappedCommand.Parameters;
+
+            public override bool DesignTimeVisible
+            {
+                get => _wrappedCommand.DesignTimeVisible;
+                set => _wrappedCommand.DesignTimeVisible = value;
+            }
+
+            public override UpdateRowSource UpdatedRowSource
+            {
+                get => _wrappedCommand.UpdatedRowSource;
+                set => _wrappedCommand.UpdatedRowSource = value;
+            }
+
+            protected override void Dispose(bool disposing)
+            {
+                var connection = _wrappedCommand.Connection;
+
+                _wrappedCommand.Dispose();
+                connection!.Dispose();
+            }
+
+            public override async ValueTask DisposeAsync()
+            {
+                var connection = _wrappedCommand.Connection;
+
+                await _wrappedCommand.DisposeAsync().ConfigureAwait(false);
+                await connection!.DisposeAsync().ConfigureAwait(false);
+            }
+
+            // The below are incompatible with commands executed directly against DbDataSource, since no DbConnection
+            // is involved at the user API level and the DbCommandWrapper owns the DbConnection.
+            protected override DbConnection? DbConnection
+            {
+                get => throw ExceptionBuilder.NotSupportedOnDataSourceCommand();
+                set => throw ExceptionBuilder.NotSupportedOnDataSourceCommand();
+            }
+
+            protected override DbTransaction? DbTransaction
+            {
+                get => throw ExceptionBuilder.NotSupportedOnDataSourceCommand();
+                set => throw ExceptionBuilder.NotSupportedOnDataSourceCommand();
+            }
+        }
+
+        private sealed class DbBatchWrapper : DbBatch
+        {
+            private readonly DbBatch _wrappedBatch;
+            private readonly DbConnection _connection;
+
+            internal DbBatchWrapper(DbBatch wrappedBatch)
+            {
+                Debug.Assert(wrappedBatch.Connection is not null);
+
+                _wrappedBatch = wrappedBatch;
+                _connection = wrappedBatch.Connection;
+            }
+
+            public override int ExecuteNonQuery()
+            {
+                _connection.Open();
+
+                try
+                {
+                    return _wrappedBatch.ExecuteNonQuery();
+                }
+                finally
+                {
+                    try
+                    {
+                        _connection.Close();
+                    }
+                    catch
+                    {
+                        // Swallow to allow the original exception to bubble up
+                    }
+                }
+            }
+
+            public override async Task<int> ExecuteNonQueryAsync(CancellationToken cancellationToken)
+            {
+                await _connection.OpenAsync(cancellationToken).ConfigureAwait(false);
+
+                try
+                {
+                    return await _wrappedBatch.ExecuteNonQueryAsync(cancellationToken)
+                        .ConfigureAwait(false);
+                }
+                finally
+                {
+                    try
+                    {
+                        await _connection.CloseAsync().ConfigureAwait(false);
+                    }
+                    catch
+                    {
+                        // Swallow to allow the original exception to bubble up
+                    }
+                }
+            }
+
+            public override object? ExecuteScalar()
+            {
+                _connection.Open();
+
+                try
+                {
+                    return _wrappedBatch.ExecuteScalar();
+                }
+                finally
+                {
+                    try
+                    {
+                        _connection.Close();
+                    }
+                    catch
+                    {
+                        // Swallow to allow the original exception to bubble up
+                    }
+                }
+            }
+
+            public override async Task<object?> ExecuteScalarAsync(CancellationToken cancellationToken)
+            {
+                await _connection.OpenAsync(cancellationToken).ConfigureAwait(false);
+
+                try
+                {
+                    return await _wrappedBatch.ExecuteScalarAsync(cancellationToken)
+                        .ConfigureAwait(false);
+                }
+                finally
+                {
+                    try
+                    {
+                        await _connection.CloseAsync().ConfigureAwait(false);
+                    }
+                    catch
+                    {
+                        // Swallow to allow the original exception to bubble up
+                    }
+                }
+            }
+
+            protected override DbDataReader ExecuteDbDataReader(CommandBehavior behavior)
+            {
+                _connection.Open();
+
+                try
+                {
+                    return _wrappedBatch.ExecuteReader(behavior | CommandBehavior.CloseConnection);
+                }
+                catch
+                {
+                    try
+                    {
+                        _connection.Close();
+                    }
+                    catch
+                    {
+                        // Swallow to allow the original exception to bubble up
+                    }
+
+                    throw;
+                }
+            }
+
+            protected override async Task<DbDataReader> ExecuteDbDataReaderAsync(
+                CommandBehavior behavior,
+                CancellationToken cancellationToken)
+            {
+                await _connection.OpenAsync(cancellationToken).ConfigureAwait(false);
+
+                try
+                {
+                    return await _wrappedBatch.ExecuteReaderAsync(
+                            behavior | CommandBehavior.CloseConnection,
+                            cancellationToken)
+                        .ConfigureAwait(false);
+                }
+                catch
+                {
+                    try
+                    {
+                        await _connection.CloseAsync().ConfigureAwait(false);
+                    }
+                    catch
+                    {
+                        // Swallow to allow the original exception to bubble up
+                    }
+
+                    throw;
+                }
+            }
+
+            protected override DbBatchCommand CreateDbBatchCommand() => throw new NotImplementedException();
+
+            public override void Cancel()
+                => _wrappedBatch.Cancel();
+
+            public override void Prepare()
+                => _wrappedBatch.Prepare();
+
+            public override Task PrepareAsync(CancellationToken cancellationToken = default) => throw new NotImplementedException();
+
+            protected override DbBatchCommandCollection DbBatchCommands => _wrappedBatch.BatchCommands;
+
+            public override int Timeout
+            {
+                get => _wrappedBatch.Timeout;
+                set => _wrappedBatch.Timeout = value;
+            }
+
+            public override void Dispose()
+            {
+                var connection = _wrappedBatch.Connection;
+
+                _wrappedBatch.Dispose();
+                connection!.Dispose();
+            }
+
+            public override async ValueTask DisposeAsync()
+            {
+                var connection = _wrappedBatch.Connection;
+
+                await _wrappedBatch.DisposeAsync().ConfigureAwait(false);
+                await connection!.DisposeAsync().ConfigureAwait(false);
+            }
+
+            // The below are incompatible with batches executed directly against DbDataSource, since no DbConnection
+            // is involved at the user API level and the DbBatchWrapper owns the DbConnection.
+            protected override DbConnection? DbConnection
+            {
+                get => throw ExceptionBuilder.NotSupportedOnDataSourceBatch();
+                set => throw ExceptionBuilder.NotSupportedOnDataSourceBatch();
+            }
+
+            protected override DbTransaction? DbTransaction
+            {
+                get => throw ExceptionBuilder.NotSupportedOnDataSourceBatch();
+                set => throw ExceptionBuilder.NotSupportedOnDataSourceBatch();
+            }
+        }
+    }
+}
