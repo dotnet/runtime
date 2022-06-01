b41062da28d156cfaff88dcf211f9662443c9921
Eduardo Manuel Velarde Polar <evelardepola@microsoft.com>
2022-05-31 21:51:49 -0700

    Erase Update Opcode

--- src/libraries/System.Private.CoreLib/src/System/Threading/NativeRuntimeEventSource.PortableThreadPool.NativeSinks.cs
+++ src/libraries/System.Private.CoreLib/src/System/Threading/NativeRuntimeEventSource.PortableThreadPool.NativeSinks.cs
@@ -51,7 +51,6 @@ namespace System.Diagnostics.Tracing
             public const EventOpcode IODequeue = (EventOpcode)14;
             public const EventOpcode IOPack = (EventOpcode)15;
             public const EventOpcode Wait = (EventOpcode)90;
-            public const EventOpcode Update = (EventOpcode)91;
             public const EventOpcode Sample = (EventOpcode)100;
             public const EventOpcode Adjustment = (EventOpcode)101;
             public const EventOpcode Stats = (EventOpcode)102;
