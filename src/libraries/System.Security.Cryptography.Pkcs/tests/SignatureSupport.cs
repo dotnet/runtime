c1fe4ba406ad3e2a02005ff8ed1d33f2059901e0
Tom Deseyn <tom.deseyn@gmail.com>
2022-06-01 10:22:05 +0200

    Refactor SignatureSupport.

--- src/libraries/System.Security.Cryptography.Pkcs/tests/SignatureSupport.cs
+++ src/libraries/System.Security.Cryptography.Pkcs/tests/SignatureSupport.cs
@@ -8,30 +8,7 @@ namespace System.Security.Cryptography.Pkcs.Tests
 {
     public class SignatureSupport
     {
-        private static int _supportsRsaSha1Signatures = 0;
-
-        public static bool SupportsRsaSha1Signatures
-        {
-            get
-            {
-                if (_supportsRsaSha1Signatures == 0)
-                {
-                    bool supported;
-                    try
-                    {
-                        using var rsa = RSA.Create();
-                        rsa.SignData(Array.Empty<byte>(), HashAlgorithmName.SHA1, RSASignaturePadding.Pkcs1);
-                        supported = true;
-                    }
-                    catch (CryptographicException)
-                    {
-                        supported = false;
-                    }
-
-                    _supportsRsaSha1Signatures = supported ? 1 : -1;
-                }
-                return _supportsRsaSha1Signatures == 1;
-            }
-        }
+        public static bool SupportsRsaSha1Signatures { get; } =
+            System.Security.Cryptography.Tests.SignatureSupport.CanProduceSha1Signature(RSA.Create());
     }
 }
