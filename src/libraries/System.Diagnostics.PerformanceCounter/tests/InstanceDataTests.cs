89d85ab04daa8f4a89d4d30f5994e41eb7ea1bd3
smasher164 <akhilindurti@microsoft.com>
2022-06-02 09:58:38 -0700

    throw xUnitException instead of making a custom one

--- src/libraries/System.Diagnostics.PerformanceCounter/tests/InstanceDataTests.cs
+++ src/libraries/System.Diagnostics.PerformanceCounter/tests/InstanceDataTests.cs
@@ -130,7 +130,7 @@ namespace System.Diagnostics.Tests
                 var idcc = pcc.ReadCategory();
                 if (idcc.Values.Count == 0)
                 {
-                    throw new Exception();
+                    Assert.InRange(idcc.Values.Count, 1, int.MaxValue);
                 }
                 return idcc;
             });
