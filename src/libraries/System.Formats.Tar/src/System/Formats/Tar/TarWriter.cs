2c2748d76db2a992bd29cf172baedd7f1f9a970d
carlossanlop <1175054+carlossanlop@users.noreply.github.com>
2022-06-02 14:42:39 -0700

    src: Do not readposition when stream is unseekable.

--- src/libraries/System.Formats.Tar/src/System/Formats/Tar/TarWriter.cs
+++ src/libraries/System.Formats.Tar/src/System/Formats/Tar/TarWriter.cs
@@ -311,7 +311,10 @@ namespace System.Formats.Tar
             byte[] emptyRecord = new byte[TarHelpers.RecordSize];
             _archiveStream.Write(emptyRecord);
             _archiveStream.Write(emptyRecord);
-            _archiveStream.SetLength(_archiveStream.Position);
+            if (_archiveStream.CanSeek)
+            {
+                _archiveStream.SetLength(_archiveStream.Position);
+            }
         }
 
         // Partial method for reading an entry from disk and writing it into the archive stream.
