e2c26ef1011f4bb565393dbd5d95ef1c57d33525
carlossanlop <1175054+carlossanlop@users.noreply.github.com>
2022-06-02 16:04:04 -0700

    Address Dan's suggestions

--- src/libraries/System.Formats.Tar/tests/TarWriter/TarWriter.Tests.cs
+++ src/libraries/System.Formats.Tar/tests/TarWriter/TarWriter.Tests.cs
@@ -82,8 +82,8 @@ namespace System.Formats.Tar.Tests
         [Fact]
         public void Write_To_UnseekableStream()
         {
-            MemoryStream stream = new MemoryStream();
-            using WrappedStream wrapped = new WrappedStream(stream, canRead: true, canWrite: true, canSeek: false);
+            using MemoryStream inner = new MemoryStream();
+            using WrappedStream wrapped = new WrappedStream(inner, canRead: true, canWrite: true, canSeek: false);
 
             using (TarWriter writer = new TarWriter(wrapped, leaveOpen: true))
             {
@@ -91,7 +91,7 @@ namespace System.Formats.Tar.Tests
                 writer.WriteEntry(paxEntry);
             } // The final records should get written, and the length should not be set because position cannot be read
 
-            stream.Seek(0, SeekOrigin.Begin); // Rewind the base stream (wrapped cannot be rewound)
+            inner.Seek(0, SeekOrigin.Begin); // Rewind the base stream (wrapped cannot be rewound)
 
             using (TarReader reader = new TarReader(wrapped))
             {
