8f7697e399774f221e21bec563cfc232c2540eed
Tomas <trylek@microsoft.com>
2022-06-03 00:21:30 +0200

    Mop-up changes per Jakob's PR suggestions
    1) Rename CORINFO_HELP_STATIC_VIRTUAL_AMBIGUOUS_RESOLUTION to
    CORINFO_HELP_THROW_AMBIGUOUS_RESOLUTION_EXCEPTION and put it next
    to the other CORINFO_HELP_THROW methods;
    
    2) Add the new helper to CorInfoHelpFunc.cs;
    
    3) Remove the jitinterface member doesFieldBelongToClass;
    
    4) Update the JIT EE GUID.
    
    Thanks
    
    Tomas
    
    Fixes: #69900

--- src/coreclr/inc/corinfo.h
+++ src/coreclr/inc/corinfo.h
@@ -625,6 +625,7 @@ enum CorInfoHelpFunc
     CORINFO_HELP_THROW_NOT_IMPLEMENTED,             // throw NotImplementedException
     CORINFO_HELP_THROW_PLATFORM_NOT_SUPPORTED,      // throw PlatformNotSupportedException
     CORINFO_HELP_THROW_TYPE_NOT_SUPPORTED,          // throw TypeNotSupportedException
+    CORINFO_HELP_THROW_AMBIGUOUS_RESOLUTION_EXCEPTION, // throw AmbiguousResolutionException for failed static virtual method resolution
 
     CORINFO_HELP_JIT_PINVOKE_BEGIN, // Transition to preemptive mode before a P/Invoke, frame is the first argument
     CORINFO_HELP_JIT_PINVOKE_END,   // Transition to cooperative mode after a P/Invoke, frame is the first argument
@@ -645,8 +646,6 @@ enum CorInfoHelpFunc
 
     CORINFO_HELP_VALIDATE_INDIRECT_CALL,    // CFG: Validate function pointer
     CORINFO_HELP_DISPATCH_INDIRECT_CALL,    // CFG: Validate and dispatch to pointer
-
-    CORINFO_HELP_STATIC_VIRTUAL_AMBIGUOUS_RESOLUTION, // Throw AmbiguousResolutionException for failed static virtual method resolution
 
     CORINFO_HELP_COUNT,
 };
