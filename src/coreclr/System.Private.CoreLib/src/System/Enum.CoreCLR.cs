d0ce9a3ddffe11cfc17face9c8fea9dcea728d77
Sergio Pedri <sergio0694@live.com>
2022-06-03 16:01:26 +0200

    Add assert for default size case in Enum.BoxValue
    Co-Authored-By: Stephen Toub <stoub@microsoft.com>

--- src/coreclr/System.Private.CoreLib/src/System/Enum.CoreCLR.cs
+++ src/coreclr/System.Private.CoreLib/src/System/Enum.CoreCLR.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -30,6 +31,7 @@ namespace System
                     Unsafe.As<byte, uint>(ref dataRef) = (uint)value;
                     break;
                 default:
+                    Debug.Assert(pMethodTable->GetNumInstanceFieldBytes() == 8);
                     Unsafe.As<byte, ulong>(ref dataRef) = (ulong)value;
                     break;
             }
