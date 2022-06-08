c970d52a3af0b112e524086b37913403c0ac66b9
Jose Perez Rodriguez <joperezr@microsoft.com>
2022-06-08 13:07:51 -0700

    Don't use the live ref pack references infrastructure

--- src/libraries/System.Text.RegularExpressions/tests/FunctionalTests/CSharpCodeFixVerifier`2.cs
+++ src/libraries/System.Text.RegularExpressions/tests/FunctionalTests/CSharpCodeFixVerifier`2.cs
@@ -85,7 +85,7 @@ namespace System.Text.RegularExpressions.Tests
                     // Clear out the default reference assemblies. We explicitly add references from the live ref pack,
                     // so we don't want the Roslyn test infrastructure to resolve/add any default reference assemblies
                     ReferenceAssemblies = new ReferenceAssemblies(string.Empty);
-                    TestState.AdditionalReferences.AddRange(SourceGenerators.Tests.LiveReferencePack.GetMetadataReferences());
+                    TestState.AdditionalReferences.AddRange(RegexGeneratorHelper.References);
                 }
 
                 NumberOfFixAllIterations = numberOfIterations;
