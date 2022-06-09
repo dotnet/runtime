368ce7b5d103b140440f755091126f8e0261c277
tlakollo <tlakaelel_axayakatl@outlook.com>
2022-06-08 21:22:31 -0700

    PR feedback

--- src/coreclr/tools/aot/Mono.Linker.Tests/TestCasesRunner/PathUtilities.cs
+++ src/coreclr/tools/aot/Mono.Linker.Tests/TestCasesRunner/PathUtilities.cs
@@ -1,9 +1,7 @@
 // Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-#define DEBUG
 
 using System;
-using System.Diagnostics;
 using System.IO;
 using System.Runtime.CompilerServices;
 
