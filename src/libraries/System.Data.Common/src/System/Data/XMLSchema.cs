9dfbc5bd7c8c9f0cb8a5973b7bdbe5d67070feac
Stephen Toub <stoub@microsoft.com>
2022-06-10 06:07:54 -0400

    Remove unnecessary CA1052 suppressions (#70512)
    The analyzer bug was fixed.

--- src/libraries/System.Data.Common/src/System/Data/XMLSchema.cs
+++ src/libraries/System.Data.Common/src/System/Data/XMLSchema.cs
@@ -13,9 +13,7 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace System.Data
 {
-#pragma warning disable CA1052 // TODO: https://github.com/dotnet/roslyn-analyzers/issues/4968
-    internal class XMLSchema
-#pragma warning restore CA1052
+    internal abstract class XMLSchema
     {
         [RequiresUnreferencedCode("Generic TypeConverters may require the generic types to be annotated. For example, NullableConverter requires the underlying type to be DynamicallyAccessedMembers All.")]
         internal static TypeConverter GetConverter(Type type)
