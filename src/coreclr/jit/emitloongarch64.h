3d2702f6044509c46bf2ade81f3afcb2218b9185
Qiao Pengcheng <qiaopengcheng@loongson.cn>
2022-06-10 21:52:53 +0800

    [LoongArch64] Fixed the compiling error for #69041. (#70544)

--- src/coreclr/jit/emitloongarch64.h
+++ src/coreclr/jit/emitloongarch64.h
@@ -134,6 +134,12 @@ inline static bool isFloatReg(regNumber reg)
 }
 
 /************************************************************************/
+/*                   Output target-independent instructions             */
+/************************************************************************/
+
+void emitIns_J(instruction ins, BasicBlock* dst, int instrCount = 0);
+
+/************************************************************************/
 /*           The public entry points to output instructions             */
 /************************************************************************/
 
