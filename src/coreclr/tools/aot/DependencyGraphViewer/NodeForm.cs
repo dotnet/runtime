e197016a1fc332a33f76963f91c3dc50fd1f6e71
Isabel Serrato <56236284+iserrato@users.noreply.github.com>
2022-06-14 15:29:26 -0700

    Update src/coreclr/tools/aot/DependencyGraphViewer/NodeForm.cs
    Co-authored-by: Aaron Robinson <arobins@microsoft.com>

--- src/coreclr/tools/aot/DependencyGraphViewer/NodeForm.cs
+++ src/coreclr/tools/aot/DependencyGraphViewer/NodeForm.cs
@@ -26,7 +26,7 @@ namespace DependencyLogViewer
             InitializeComponent();
 
             this.Text = "Graph Pid:" + _graph.PID + " Id:" + _graph.ID + " Node:" + _node.ToString();
-            this.nodeTitle.Text = "Current Node: " + _node.ToString();
+            this.nodeTitle.Text = $"Current Node: {_node}";
 
             lock (GraphCollection.Singleton)
             {
