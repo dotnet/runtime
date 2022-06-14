81cb851b2f45e5f6bf622edaa91d31a143a97140
Aleksey Kliger (λgeek) <alklig@microsoft.com>
2022-06-14 09:19:45 -0400

    add header

--- src/mono/wasm/runtime/guarded-promise.ts
+++ src/mono/wasm/runtime/guarded-promise.ts
@@ -1,3 +1,6 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 /// A Promise<T> that guards against multiple-resolve, multiple-reject, reject-after-accept and accept-after-reject.
 class GuardedPromise<T> extends Promise<T> {
     constructor(executor: (resolve: (value: T | PromiseLike<T>) => void, reject: (reason?: any) => void) => void) {
