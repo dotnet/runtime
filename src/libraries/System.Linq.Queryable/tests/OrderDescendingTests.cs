fbb3a74dbceac78ca3c6f4646b7d3891f264e6aa
Robin Lindner <robin@deeprobin.de>
2022-06-14 16:26:10 +0200

    Add more Queryable test cases

--- src/libraries/System.Linq.Queryable/tests/OrderDescendingTests.cs
+++ src/libraries/System.Linq.Queryable/tests/OrderDescendingTests.cs
@@ -2,13 +2,44 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Collections.Generic;
-using System.Linq;
 using Xunit;
 
 namespace System.Linq.Tests
 {
     public sealed class OrderDescendingTests : EnumerableBasedTests
     {
+        [Fact]
+        public void FirstAndLastAreDuplicatesCustomComparer()
+        {
+            string[] source = { "Prakash", "Alpha", "DAN", "dan", "Prakash" };
+            string[] expected = { "Prakash", "Prakash", "DAN", "dan", "Alpha" };
+
+            Assert.Equal(expected, source.AsQueryable().OrderDescending(StringComparer.OrdinalIgnoreCase));
+        }
+
+        [Fact]
+        public void FirstAndLastAreDuplicatesNullPassedAsComparer()
+        {
+            int[] source = { 5, 1, 3, 2, 5 };
+            int[] expected = { 5, 5, 3, 2, 1 };
+
+            Assert.Equal(expected, source.AsQueryable().OrderDescending(null));
+        }
+
+        [Fact]
+        public void NullSource()
+        {
+            IQueryable<int> source = null;
+            AssertExtensions.Throws<ArgumentNullException>("source", () => source.OrderDescending());
+        }
+
+        [Fact]
+        public void NullSourceComparer()
+        {
+            IQueryable<int> source = null;
+            AssertExtensions.Throws<ArgumentNullException>("source", () => source.OrderDescending(Comparer<int>.Default));
+        }
+
         [Fact]
         public void OrderDescending1()
         {
--- src/libraries/System.Linq.Queryable/tests/OrderTests.cs
+++ src/libraries/System.Linq.Queryable/tests/OrderTests.cs
@@ -2,13 +2,44 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Collections.Generic;
-using System.Linq;
 using Xunit;
 
 namespace System.Linq.Tests
 {
     public sealed class OrderTests : EnumerableBasedTests
     {
+        [Fact]
+        public void FirstAndLastAreDuplicatesCustomComparer()
+        {
+            string[] source = { "Prakash", "Alpha", "dan", "DAN", "Prakash" };
+            string[] expected = { "Alpha", "dan", "DAN", "Prakash", "Prakash" };
+
+            Assert.Equal(expected, source.AsQueryable().Order(StringComparer.OrdinalIgnoreCase));
+        }
+
+        [Fact]
+        public void FirstAndLastAreDuplicatesNullPassedAsComparer()
+        {
+            int[] source = { 5, 1, 3, 2, 5 };
+            int[] expected = { 1, 2, 3, 5, 5 };
+
+            Assert.Equal(expected, source.AsQueryable().Order(null));
+        }
+
+        [Fact]
+        public void NullSource()
+        {
+            IQueryable<int> source = null;
+            AssertExtensions.Throws<ArgumentNullException>("source", () => source.Order());
+        }
+
+        [Fact]
+        public void NullSourceComparer()
+        {
+            IQueryable<int> source = null;
+            AssertExtensions.Throws<ArgumentNullException>("source", () => source.Order(Comparer<int>.Default));
+        }
+
         [Fact]
         public void Order1()
         {
