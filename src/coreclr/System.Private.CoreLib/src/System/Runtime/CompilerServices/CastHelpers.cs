e645a9874ff1ce8174926079d0ff397a765cca56
Egor Bogatov <egorbo@gmail.com>
2022-06-14 21:34:13 +0200

    Update CastHelpers.cs

--- src/coreclr/System.Private.CoreLib/src/System/Runtime/CompilerServices/CastHelpers.cs
+++ src/coreclr/System.Private.CoreLib/src/System/Runtime/CompilerServices/CastHelpers.cs
@@ -478,7 +478,7 @@ namespace System.Runtime.CompilerServices
         {
             MethodTable* mt = RuntimeHelpers.GetMethodTable(obj);
 
-            // Normally, this case is expected to be handled by JIT inline. 
+            // Normally, this case is expected to be handled by JIT inline.
             // However, with PGO data JIT might decide to check a different type instead
             // so this one has to be always checked here
             if (toTypeHnd == mt)
