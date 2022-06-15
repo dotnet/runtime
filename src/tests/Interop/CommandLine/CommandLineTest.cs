aaa5defdf0655b41e4b61472c58b7c2fe5f4c718
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2022-06-15 17:01:37 +0800

    Add test using private reflection

---
+++ src/tests/Interop/CommandLine/CommandLineTest.cs
@@ -0,0 +1,34 @@
+using System;
+using System.IO;
+using System.Reflection;
+using TestLibrary;
+using Xunit;
+
+namespace CommandLineTest
+{
+    class Program
+    {
+        int Main()
+        {
+            // Currently native command line is only implemented on CoreCLR Windows
+            if (!TestLibrary.Utilities.IsMonoRuntime && !TestLibrary.Utilities.IsNativeAot)
+            {
+                // Clear the command line args set for managed entry point
+                var field = typeof(Environment).GetField("s_commandLineArgs", BindingFlags.Static | BindingFlags.NonPublic);
+                Assert.NotNull(field);
+                field.SetValue(null, null);
+
+                string[] args = Environment.GetCommandLineArgs();
+                if (OperatingSystem.IsWindows())
+                {
+                    // The command line should be "corerun assemblyname.dll" for coreclr test
+                    Assert.Equal(2, args.Length);
+                    Assert.Equal("corerun", Path.GetFileNameWithoutExtension(args[0]));
+                    Assert.Equal(typeof(Program).Assembly.GetName().Name, Path.GetFileNameWithoutExtension(args[1]));
+                }
+            }
+
+            return 100;
+        }
+    }
+}
