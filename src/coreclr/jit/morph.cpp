53e52f28cdc9b800a4b04fee2f200e4087fcfd58
Tanner Gooding <tagoo@outlook.com>
2022-06-15 06:30:25 -0700

    Updating HWIntrinsicInfo::lookupId to not accelerate Vector256 APIs when AVX2 is unsupported (#70686)
    * Updating HWIntrinsicINfo::lookupId to not accelerate Vector256 APIs when AVX2 is unsupported
    
    * Fixing a check in lookupId to properly negate the condition
    
    * Ensure the special-cased EA_32BYTE constants only happen when AVX/AVX2 are supported
    
    * Fixing a bad assert in morph

--- src/coreclr/jit/morph.cpp
+++ src/coreclr/jit/morph.cpp
@@ -9934,7 +9934,7 @@ GenTree* Compiler::fgMorphFieldToSimdGetElement(GenTree* tree)
         var_types simdBaseType = JitType2PreciseVarType(simdBaseJitType);
         GenTree*  op2          = gtNewIconNode(index, TYP_INT);
 
-        assert(simdSize <= 16);
+        assert(simdSize <= 32);
         assert(simdSize >= ((index + 1) * genTypeSize(simdBaseType)));
 
 #if defined(TARGET_XARCH)
@@ -10009,7 +10009,7 @@ GenTree* Compiler::fgMorphFieldAssignToSimdSetElement(GenTree* tree)
         var_types simdType     = simdStructNode->gtType;
         var_types simdBaseType = JitType2PreciseVarType(simdBaseJitType);
 
-        assert(simdSize <= 16);
+        assert(simdSize <= 32);
         assert(simdSize >= ((index + 1) * genTypeSize(simdBaseType)));
 
         GenTree*       op2         = gtNewIconNode(index, TYP_INT);
