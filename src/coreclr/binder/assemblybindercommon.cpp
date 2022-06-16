dotnet/runtime

commit eb9dd67ce9fc64c468cf5c795fda7057310c22fc

Delete AssemblySpec::m_wszCodeBase (#68186)
* Delete AssemblySpec::m_wszCodeBase

This was only used for assembly loading using hosting APIs that can be short-circuited to load directly without going through AssemblySpec

* Delete unnecessary ownedFlags argument

src/coreclr/binder/assemblybindercommon.cpp
@@ -107,53 +107,6 @@ namespace BINDER_SPACE
             return true;
         }
 
-        const WCHAR* s_httpURLPrefix = W("http://");
-        HRESULT URLToFullPath(PathString &assemblyPath)
-        {
-            HRESULT hr = S_OK;
-
-            SString::Iterator pos = assemblyPath.Begin();
-            if (assemblyPath.MatchCaseInsensitive(pos, s_httpURLPrefix))
-            {
-                // HTTP downloads are unsupported
-                hr = FUSION_E_CODE_DOWNLOAD_DISABLED;
-            }
-            else
-            {
-                SString fullAssemblyPath;
-                WCHAR *pwzFullAssemblyPath = fullAssemblyPath.OpenUnicodeBuffer(MAX_LONGPATH);
-                DWORD dwCCFullAssemblyPath = MAX_LONGPATH + 1; // SString allocates extra byte for null.
-
-                MutateUrlToPath(assemblyPath);
-
-                dwCCFullAssemblyPath = WszGetFullPathName(assemblyPath.GetUnicode(),
-                                                          dwCCFullAssemblyPath,
-                                                          pwzFullAssemblyPath,
-                                                          NULL);
-                if (dwCCFullAssemblyPath > MAX_LONGPATH)
-                {
-                    fullAssemblyPath.CloseBuffer();
-                    pwzFullAssemblyPath = fullAssemblyPath.OpenUnicodeBuffer(dwCCFullAssemblyPath - 1);
-                    dwCCFullAssemblyPath = WszGetFullPathName(assemblyPath.GetUnicode(),
-                                                              dwCCFullAssemblyPath,
-                                                              pwzFullAssemblyPath,
-                                                              NULL);
-                }
-                fullAssemblyPath.CloseBuffer(dwCCFullAssemblyPath);
-
-                if (dwCCFullAssemblyPath == 0)
-                {
-                    hr = HRESULT_FROM_GetLastError();
-                }
-                else
-                {
-                    assemblyPath.Set(fullAssemblyPath);
-                }
-            }
-
-            return hr;
-        }
-
         HRESULT CreateImageAssembly(PEImage                 *pPEImage,
                                     BindResult              *pBindResult)
         {
@@ -238,7 +191,6 @@ namespace BINDER_SPACE
 
     HRESULT AssemblyBinderCommon::BindAssembly(/* in */  AssemblyBinder      *pBinder,
                                                /* in */  AssemblyName        *pAssemblyName,
-                                               /* in */  LPCWSTR              szCodeBase,
                                                /* in */  bool                 excludeAppPaths,
                                                /* out */ Assembly           **ppAssembly)
     {
@@ -255,28 +207,13 @@ namespace BINDER_SPACE
             // Lock the binding application context
             CRITSEC_Holder contextLock(pApplicationContext->GetCriticalSectionCookie());
 
-            if (szCodeBase == NULL)
-            {
-                _ASSERTE(pAssemblyName != NULL);
-                IF_FAIL_GO(BindByName(pApplicationContext,
-                                      pAssemblyName,
-                                      false, // skipFailureCaching
-                                      false, // skipVersionCompatibilityCheck
-                                      excludeAppPaths,
-                                      &bindResult));
-            }
-            else
-            {
-                PathString assemblyPath(szCodeBase);
-
-                // Convert URL to full path and block HTTP downloads
-                IF_FAIL_GO(URLToFullPath(assemblyPath));
-
-                IF_FAIL_GO(BindWhereRef(pApplicationContext,
-                                        assemblyPath,
-                                        excludeAppPaths,
-                                        &bindResult));
-            }
+            _ASSERTE(pAssemblyName != NULL);
+            IF_FAIL_GO(BindByName(pApplicationContext,
+                                    pAssemblyName,
+                                    false, // skipFailureCaching
+                                    false, // skipVersionCompatibilityCheck
+                                    excludeAppPaths,
+                                    &bindResult));
 
             // Remember the post-bind version
             kContextVersion = pApplicationContext->GetVersion();
@@ -520,61 +457,6 @@ namespace BINDER_SPACE
             }
 
             hr = pApplicationContext->AddToFailureCache(assemblyDisplayName, hr);
-        }
-
-    LogExit:
-        return hr;
-    }
-
-    /* static */
-    HRESULT AssemblyBinderCommon::BindWhereRef(ApplicationContext *pApplicationContext,
-                                         PathString         &assemblyPath,
-                                         bool                excludeAppPaths,
-                                         BindResult         *pBindResult)
-    {
-        HRESULT hr = S_OK;
-
-        ReleaseHolder<Assembly> pAssembly;
-        BindResult lockedBindResult;
-
-        // Look for already cached binding failure
-        hr = pApplicationContext->GetFailureCache()->Lookup(assemblyPath);
-        if (FAILED(hr))
-        {
-            goto LogExit;
-        }
-
-        // If we return this assembly, then it is guaranteed to be not in GAC
-        // Design decision. For now, keep the V2 model of Fusion being oblivious of the strong name.
-        // Security team did not see any security concern with interpreting the version information.
-        IF_FAIL_GO(GetAssembly(assemblyPath,
-                               FALSE /* fIsInTPA */,
-                               &pAssembly,
-                               Bundle::ProbeAppBundle(assemblyPath)));
-
-        AssemblyName *pAssemblyName;
-        pAssemblyName = pAssembly->GetAssemblyName();
-
-        IF_FAIL_GO(BindLocked(pApplicationContext,
-                                pAssemblyName,
-                                false, // skipVersionCompatibilityCheck
-                                excludeAppPaths,
-                                &lockedBindResult));
-        if (lockedBindResult.HaveResult())
-        {
-            pBindResult->SetResult(&lockedBindResult);
-            GO_WITH_HRESULT(S_OK);
-        }
-
-        hr = S_OK;
-        pBindResult->SetResult(pAssembly);
-
-    Exit:
-
-        if (FAILED(hr))
-        {
-            // Always cache binding failures
-            hr = pApplicationContext->AddToFailureCache(assemblyPath, hr);
         }
 
     LogExit:

src/coreclr/binder/customassemblybinder.cpp
@@ -27,7 +27,6 @@ HRESULT CustomAssemblyBinder::BindAssemblyByNameWorker(BINDER_SPACE::AssemblyNam
     // Do we have the assembly already loaded in the context of the current binder?
     hr = AssemblyBinderCommon::BindAssembly(this,
                                             pAssemblyName,
-                                            NULL,  // szCodeBase
                                             false, //excludeAppPaths,
                                             ppCoreCLRFoundAssembly);
     if (!FAILED(hr))

src/coreclr/binder/defaultassemblybinder.cpp
@@ -25,7 +25,6 @@ HRESULT DefaultAssemblyBinder::BindAssemblyByNameWorker(BINDER_SPACE::AssemblyNa
 
     hr = AssemblyBinderCommon::BindAssembly(this,
                                             pAssemblyName,
-                                            NULL, // szCodeBase
                                             excludeAppPaths,
                                             ppCoreCLRFoundAssembly);
     if (!FAILED(hr))
@@ -184,32 +183,6 @@ HRESULT DefaultAssemblyBinder::SetupBindingPaths(SString  &sTrustedPlatformAssem
         hr = GetAppContext()->SetupBindingPaths(sTrustedPlatformAssemblies, sPlatformResourceRoots, sAppPaths, TRUE /* fAcquireLock */);
     }
     EX_CATCH_HRESULT(hr);
-    return hr;
-}
-
-HRESULT DefaultAssemblyBinder::Bind(LPCWSTR                  wszCodeBase,
-                                    BINDER_SPACE::Assembly **ppAssembly)
-{
-    HRESULT hr = S_OK;
-    VALIDATE_ARG_RET(wszCodeBase != NULL && ppAssembly != NULL);
-
-    EX_TRY
-    {
-        ReleaseHolder<BINDER_SPACE::Assembly> pAsm;
-        hr = AssemblyBinderCommon::BindAssembly(this,
-                                                NULL, // pAssemblyName
-                                                wszCodeBase,
-                                                false, // excludeAppPaths
-                                                &pAsm);
-        if(SUCCEEDED(hr))
-        {
-            _ASSERTE(pAsm != NULL);
-            pAsm->SetBinder(this);
-            *ppAssembly = pAsm.Extract();
-        }
-    }
-    EX_CATCH_HRESULT(hr);
-
     return hr;
 }
 

src/coreclr/binder/inc/assemblybindercommon.hpp
@@ -32,7 +32,6 @@ namespace BINDER_SPACE
     public:
         static HRESULT BindAssembly(/* in */  AssemblyBinder      *pBinder, 
                                     /* in */  AssemblyName        *pAssemblyName,
-                                    /* in */  LPCWSTR              szCodeBase,
                                     /* in */  bool                 excludeAppPaths,
                                     /* out */ Assembly           **ppAssembly);
 
@@ -74,11 +73,6 @@ namespace BINDER_SPACE
                                   /* in */  bool                skipVersionCompatibilityCheck,
                                   /* in */  bool                excludeAppPaths,
                                   /* out */ BindResult         *pBindResult);
-
-        static HRESULT BindWhereRef(/* in */  ApplicationContext *pApplicationContext,
-                                    /* in */  PathString         &assemblyPath,
-                                    /* in */  bool                excludeAppPaths,
-                                    /* out */ BindResult         *pBindResult);
 
         static HRESULT BindLocked(/* in */  ApplicationContext *pApplicationContext,
                                   /* in */  AssemblyName       *pAssemblyName,

src/coreclr/binder/inc/defaultassemblybinder.h
@@ -38,8 +38,6 @@ public:
                               SString  &sPlatformResourceRoots,
                               SString  &sAppPaths);
 
-    HRESULT Bind(LPCWSTR wszCodeBase, BINDER_SPACE::Assembly **ppAssembly);
-
     HRESULT BindToSystem(BINDER_SPACE::Assembly **ppSystemAssembly);
 
 private:

src/coreclr/vm/assemblynative.cpp
@@ -89,8 +89,6 @@ extern "C" void QCALLTYPE AssemblyNative_InternalLoad(QCall::ObjectHandleOnStack
 
     GCPROTECT_END();
 
-    spec.SetCodeBase(NULL);
-
     if (pParentAssembly != NULL)
         spec.SetParentAssembly(pParentAssembly);
 
@@ -163,7 +161,7 @@ Assembly* AssemblyNative::LoadFromPEImage(AssemblyBinder* pBinder, PEImage *pIma
         }
 
         StackSString name;
-        spec.GetFileOrDisplayName(0, name);
+        spec.GetDisplayName(0, name);
         COMPlusThrowHR(COR_E_FILELOAD, dwMessageID, name);
     }
 
@@ -999,7 +997,7 @@ FCIMPL1(Object*, AssemblyNative::GetReferencedAssemblies, AssemblyBaseObject * p
         spec.InitializeSpec(mdAssemblyRef, pImport);
 
         gc.pObj = (ASSEMBLYNAMEREF) AllocateObject(pAsmNameClass);
-        spec.AssemblyNameInit(&gc.pObj,NULL);
+        spec.AssemblyNameInit(&gc.pObj);
 
         gc.ItemArray->SetAt(i, (OBJECTREF) gc.pObj);
     }

src/coreclr/vm/assemblyspec.cpp
@@ -22,6 +22,7 @@
 #include "strongnameinternal.h"
 #include "strongnameholders.h"
 #include "eventtrace.h"
+#include "assemblynative.hpp"
 
 #include "../binder/inc/bindertracing.h"
 
@@ -144,38 +145,6 @@ AssemblySpecHash::~AssemblySpecHash()
 
         ++i;
     }
-}
-
-// Check assembly name for invalid characters
-// Return value:
-//      TRUE: If no invalid characters were found, or if the assembly name isn't set
-//      FALSE: If invalid characters were found
-// This is needed to prevent security loopholes with ':', '/' and '\' in the assembly name
-BOOL AssemblySpec::IsValidAssemblyName()
-{
-    CONTRACTL
-    {
-        THROWS;
-        GC_TRIGGERS;
-    }
-    CONTRACTL_END;
-
-    if (GetName())
-    {
-        SString ssAssemblyName(SString::Utf8, GetName());
-        for (SString::Iterator i = ssAssemblyName.Begin(); i[0] != W('\0'); i++) {
-            switch (i[0]) {
-                case W(':'):
-                case W('\\'):
-                case W('/'):
-                    return FALSE;
-
-                default:
-                    break;
-            }
-        }
-    }
-    return TRUE;
 }
 
 HRESULT AssemblySpec::InitializeSpecInternal(mdToken kAssemblyToken,
@@ -371,7 +340,7 @@ void AssemblySpec::InitializeSpec(StackingAllocator* alloc, ASSEMBLYNAMEREF* pNa
     CloneFieldsToStackingAllocator(alloc);
 }
 
-void AssemblySpec::AssemblyNameInit(ASSEMBLYNAMEREF* pAsmName, PEImage* pImageInfo)
+void AssemblySpec::AssemblyNameInit(ASSEMBLYNAMEREF* pAsmName)
 {
     CONTRACTL
     {
@@ -496,20 +465,7 @@ void AssemblySpec::AssemblyNameInit(ASSEMBLYNAMEREF* pAsmName, PEImage* pImageIn
     if(GetName())
         gc.Name = StringObject::NewString(GetName());
 
-    if (GetCodeBase())
-        gc.CodeBase = StringObject::NewString(GetCodeBase());
-
     BOOL fPublicKey = m_dwFlags & afPublicKey;
-
-    ULONG hashAlgId=0;
-    if (pImageInfo != NULL)
-    {
-        if(!pImageInfo->GetMDImport()->IsValidToken(TokenFromRid(1, mdtAssembly)))
-        {
-            ThrowHR(COR_E_BADIMAGEFORMAT);
-        }
-        IfFailThrow(pImageInfo->GetMDImport()->GetAssemblyProps(TokenFromRid(1, mdtAssembly), NULL, NULL, &hashAlgId, NULL, NULL, NULL));
-    }
 
     MethodDescCallSite init(METHOD__ASSEMBLY_NAME__CTOR);
 
@@ -523,32 +479,10 @@ void AssemblySpec::AssemblyNameInit(ASSEMBLYNAMEREF* pAsmName, PEImage* pImageIn
         ObjToArgSlot(gc.PublicKeyOrToken), // public key token
         ObjToArgSlot(gc.Version),
         ObjToArgSlot(gc.CultureInfo),
-        (ARG_SLOT) hashAlgId,
-        (ARG_SLOT) 1, // AssemblyVersionCompatibility.SameMachine
-        ObjToArgSlot(gc.CodeBase),
         (ARG_SLOT) m_dwFlags,
     };
 
     init.Call(MethodArgs);
-
-    // Only set the processor architecture if we're looking at a newer binary that has
-    // that information in the PE, and we're not looking at a reference assembly.
-    if(pImageInfo && !pImageInfo->HasV1Metadata() && !pImageInfo->IsReferenceAssembly())
-    {
-        DWORD dwMachine, dwKind;
-
-        pImageInfo->GetPEKindAndMachine(&dwMachine,&dwKind);
-
-        MethodDescCallSite setPA(METHOD__ASSEMBLY_NAME__SET_PROC_ARCH_INDEX);
-
-        ARG_SLOT PAMethodArgs[] = {
-            ObjToArgSlot(*pAsmName),
-            (ARG_SLOT)dwMachine,
-            (ARG_SLOT)dwKind
-        };
-
-        setPA.Call(PAMethodArgs);
-    }
 
     GCPROTECT_END();
 }
@@ -579,7 +513,7 @@ void AssemblySpec::InitializeAssemblyNameRef(_In_ BINDER_SPACE::AssemblyName* as
         spec.SetCulture(culture);
     }
 
-    spec.AssemblyNameInit(assemblyNameRef, NULL);
+    spec.AssemblyNameInit(assemblyNameRef);
 }
 
 
@@ -786,9 +720,19 @@ Assembly *AssemblySpec::LoadAssembly(LPCWSTR pFilePath)
     }
     CONTRACT_END;
 
-    AssemblySpec spec;
-    spec.SetCodeBase(pFilePath);
-    RETURN spec.LoadAssembly(FILE_LOADED);
+    GCX_PREEMP();
+
+    PEImageHolder pILImage;
+
+    pILImage = PEImage::OpenImage(pFilePath,
+        MDInternalImport_Default,
+        Bundle::ProbeAppBundle(pFilePath));
+
+    // Need to verify that this is a valid CLR assembly.
+    if (!pILImage->CheckILFormat())
+        THROW_BAD_FORMAT(BFA_BAD_IL, pILImage.GetValue());
+
+    RETURN AssemblyNative::LoadFromPEImage(AppDomain::GetCurrentDomain()->GetDefaultBinder(), pILImage);
 }
 
 HRESULT AssemblySpec::CheckFriendAssemblyName()

src/coreclr/System.Private.CoreLib/src/System/Reflection/AssemblyName.CoreCLR.cs
@@ -16,19 +16,14 @@ namespace System.Reflection
             byte[]? publicKeyToken,
             Version? version,
             CultureInfo? cultureInfo,
-            AssemblyHashAlgorithm hashAlgorithm,
-            AssemblyVersionCompatibility versionCompatibility,
-            string? codeBase,
             AssemblyNameFlags flags)
+            : this()
         {
             _name = name;
             _publicKey = publicKey;
             _publicKeyToken = publicKeyToken;
             _version = version;
             _cultureInfo = cultureInfo;
-            _hashAlgorithm = hashAlgorithm;
-            _versionCompatibility = versionCompatibility;
-            _codeBase = codeBase;
             _flags = flags;
         }
 

commit b8dd2b731770a2f10b42ae60dbf2f1bda7d28986

Fix corerun for assemblies with mismatched filename
corerun gracefully handled filename not matching the assembly name. Handling of this case regressed in #68186. This change is fixing the regression.

Fixes #68455

src/coreclr/binder/assemblybindercommon.cpp
@@ -1180,6 +1180,7 @@ HRESULT AssemblyBinderCommon::BindUsingHostAssemblyResolver(/* in */ INT_PTR pMa
 HRESULT AssemblyBinderCommon::BindUsingPEImage(/* in */  AssemblyBinder* pBinder,
                                                /* in */  BINDER_SPACE::AssemblyName *pAssemblyName,
                                                /* in */  PEImage            *pPEImage,
+                                               /* in */  bool               excludeAppPaths,
                                                /* [retval] [out] */  Assembly **ppAssembly)
 {
     HRESULT hr = E_FAIL;
@@ -1208,7 +1209,7 @@ Retry:
                         pAssemblyName,
                         true,  // skipFailureCaching
                         true,  // skipVersionCompatibilityCheck
-                        false, // excludeAppPaths
+                        excludeAppPaths, // excludeAppPaths
                         &bindResult);
 
         if (hr == HRESULT_FROM_WIN32(ERROR_FILE_NOT_FOUND))

src/coreclr/binder/customassemblybinder.cpp
@@ -102,6 +102,7 @@ Exit:;
 }
 
 HRESULT CustomAssemblyBinder::BindUsingPEImage( /* in */ PEImage *pPEImage,
+                                                /* in */ bool excludeAppPaths,
                                                 /* [retval][out] */ BINDER_SPACE::Assembly **ppAssembly)
 {
     HRESULT hr = S_OK;
@@ -128,7 +129,7 @@ HRESULT CustomAssemblyBinder::BindUsingPEImage( /* in */ PEImage *pPEImage,
             IF_FAIL_GO(HRESULT_FROM_WIN32(ERROR_FILE_NOT_FOUND));
         }
 
-        hr = AssemblyBinderCommon::BindUsingPEImage(this, pAssemblyName, pPEImage, &pCoreCLRFoundAssembly);
+        hr = AssemblyBinderCommon::BindUsingPEImage(this, pAssemblyName, pPEImage, excludeAppPaths, &pCoreCLRFoundAssembly);
         if (hr == S_OK)
         {
             _ASSERTE(pCoreCLRFoundAssembly != NULL);

src/coreclr/binder/defaultassemblybinder.cpp
@@ -111,6 +111,7 @@ Exit:;
 
 #if !defined(DACCESS_COMPILE)
 HRESULT DefaultAssemblyBinder::BindUsingPEImage( /* in */ PEImage *pPEImage,
+                                                 /* in */ bool excludeAppPaths,
                                                  /* [retval][out] */ BINDER_SPACE::Assembly **ppAssembly)
 {
     HRESULT hr = S_OK;
@@ -157,7 +158,7 @@ HRESULT DefaultAssemblyBinder::BindUsingPEImage( /* in */ PEImage *pPEImage,
             }
         }
 
-        hr = AssemblyBinderCommon::BindUsingPEImage(this, pAssemblyName, pPEImage, &pCoreCLRFoundAssembly);
+        hr = AssemblyBinderCommon::BindUsingPEImage(this, pAssemblyName, pPEImage, excludeAppPaths, &pCoreCLRFoundAssembly);
         if (hr == S_OK)
         {
             _ASSERTE(pCoreCLRFoundAssembly != NULL);

src/coreclr/binder/inc/assemblybindercommon.hpp
@@ -56,6 +56,7 @@ namespace BINDER_SPACE
         static HRESULT BindUsingPEImage(/* in */  AssemblyBinder     *pBinder,
                                         /* in */  BINDER_SPACE::AssemblyName *pAssemblyName,
                                         /* in */  PEImage            *pPEImage,
+                                        /* in */  bool              excludeAppPaths,
                                         /* [retval] [out] */  Assembly **ppAssembly);
 #endif // !defined(DACCESS_COMPILE)
 

src/coreclr/binder/inc/customassemblybinder.h
@@ -18,6 +18,7 @@ class CustomAssemblyBinder final : public AssemblyBinder
 public:
 
     HRESULT BindUsingPEImage(PEImage* pPEImage,
+        bool excludeAppPaths,
         BINDER_SPACE::Assembly** ppAssembly) override;
 
     HRESULT BindUsingAssemblyName(BINDER_SPACE::AssemblyName* pAssemblyName,

src/coreclr/binder/inc/defaultassemblybinder.h
@@ -16,6 +16,7 @@ class DefaultAssemblyBinder final : public AssemblyBinder
 public:
 
     HRESULT BindUsingPEImage(PEImage* pPEImage,
+        bool excludeAppPaths,
         BINDER_SPACE::Assembly** ppAssembly) override;
 
     HRESULT BindUsingAssemblyName(BINDER_SPACE::AssemblyName* pAssemblyName,

src/coreclr/vm/assemblybinder.h
@@ -19,7 +19,7 @@ class AssemblyBinder
 public:
 
     HRESULT BindAssemblyByName(AssemblyNameData* pAssemblyNameData, BINDER_SPACE::Assembly** ppAssembly);
-    virtual HRESULT BindUsingPEImage(PEImage* pPEImage, BINDER_SPACE::Assembly** ppAssembly) = 0;
+    virtual HRESULT BindUsingPEImage(PEImage* pPEImage, bool excludeAppPaths, BINDER_SPACE::Assembly** ppAssembly) = 0;
     virtual HRESULT BindUsingAssemblyName(BINDER_SPACE::AssemblyName* pAssemblyName, BINDER_SPACE::Assembly** ppAssembly) = 0;
 
     /// <summary>

src/coreclr/vm/assemblynative.cpp
@@ -124,7 +124,7 @@ extern "C" void QCALLTYPE AssemblyNative_InternalLoad(NativeAssemblyNameParts* p
 }
 
 /* static */
-Assembly* AssemblyNative::LoadFromPEImage(AssemblyBinder* pBinder, PEImage *pImage)
+Assembly* AssemblyNative::LoadFromPEImage(AssemblyBinder* pBinder, PEImage *pImage, bool excludeAppPaths)
 {
     CONTRACT(Assembly*)
     {
@@ -152,7 +152,7 @@ Assembly* AssemblyNative::LoadFromPEImage(AssemblyBinder* pBinder, PEImage *pIma
 
     HRESULT hr = S_OK;
     PTR_AppDomain pCurDomain = GetAppDomain();
-    hr = pBinder->BindUsingPEImage(pImage, &pAssembly);
+    hr = pBinder->BindUsingPEImage(pImage, excludeAppPaths, &pAssembly);
 
     if (hr != S_OK)
     {

src/coreclr/vm/assemblynative.hpp
@@ -25,7 +25,7 @@ class AssemblyNative
 
 public:
 
-    static Assembly* LoadFromPEImage(AssemblyBinder* pBinder, PEImage *pImage);
+    static Assembly* LoadFromPEImage(AssemblyBinder* pBinder, PEImage *pImage, bool excludeAppPaths = false);
 
     // static FCALLs
     static FCDECL0(FC_BOOL_RET, IsTracingEnabled);

src/coreclr/vm/assemblyspec.cpp
@@ -503,7 +503,7 @@ Assembly *AssemblySpec::LoadAssembly(LPCWSTR pFilePath)
     if (!pILImage->CheckILFormat())
         THROW_BAD_FORMAT(BFA_BAD_IL, pILImage.GetValue());
 
-    RETURN AssemblyNative::LoadFromPEImage(AppDomain::GetCurrentDomain()->GetDefaultBinder(), pILImage);
+    RETURN AssemblyNative::LoadFromPEImage(AppDomain::GetCurrentDomain()->GetDefaultBinder(), pILImage, true /* excludeAppPaths */);
 }
 
 HRESULT AssemblySpec::CheckFriendAssemblyName()