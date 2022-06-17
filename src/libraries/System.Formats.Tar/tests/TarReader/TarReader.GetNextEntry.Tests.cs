52799457be9ac00274cf1ce3952c90c0db15186b
carlossanlop <carlossanlop@users.noreply.github.com>
2022-06-16 21:27:10 -0700

    tests: Verify Global Extended Attributes API changes

--- src/libraries/System.Formats.Tar/tests/TarReader/TarReader.GetNextEntry.Tests.cs
+++ src/libraries/System.Formats.Tar/tests/TarReader/TarReader.GetNextEntry.Tests.cs
@@ -1,7 +1,9 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Collections.Generic;
 using System.IO;
+using System.Reflection.PortableExecutable;
 using Xunit;
 
 namespace System.Formats.Tar.Tests
