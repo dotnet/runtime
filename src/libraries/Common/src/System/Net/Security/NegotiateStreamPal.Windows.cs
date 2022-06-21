79f6709eded51a0cfd8bcfcdf501dd42e4358113
Filip Navara <navara@emclient.com>
2022-06-21 13:38:18 +0200

    Implement NegotiateAuthentication API (#70720)
    * WIP: Add implementation of NegotiateAuthentication
    
    Switch System.Net.Http to use NegotiateAuthentication
    Fix IsCompleted in managed NTLM implementation
    
    * WIP: Update error code mapping
    
    * Spanify input of GetOutgoingBlob
    
    * Update comments
    
    * Move NegotiateStreamPal.Encrypt/Decrypt to shared sources. Unix implementation already had them and they get trimmed anyway.
    
    * Revert accidental change
    
    * Build fixes.
    
    * Fix error handling condition
    
    * Update error mapping based on HttpListener usage.
    
    * WIP: HttpListener test
    
    * Move workaround from HttpListener to low-level SSPI code
    
    * Fix build
    
    * Clean up
    
    * Revert "WIP: HttpListener test"
    
    This reverts commit 18d7d93f04c93e048efcaca0f3c55c3f1f73516a.
    
    * Convert System.Net.Http.FunctionalTests to use NegotiateAuthentication instead of NTAuthentication
    
    * Dispose the identity along NegotiateAuthentication
    
    * Modify unit tests to use the new API
    
    * Add exceptions for invalid inputs/states
    
    * Remove tvOS unsupported marker, managed NTLM is used on tvOS
    
    * Apply suggestions from code review
    
    Co-authored-by: Stephen Toub <stoub@microsoft.com>
    
    * Fix typo
    
    * Remove reference equality checks from IsNTLM/IsKerberos
    
    * Remove NTAuthentication.AssociatedName to make it more obvious which exceptions are thrown
    
    * Add comment
    
    * Add more tests, handle unsupported protocols
    
    * Handle NotSupportedException from NTAuthentication constructor
    
    * Add workaround for linker issue
    
    * Apply suggestions from code review
    
    Co-authored-by: Stephen Toub <stoub@microsoft.com>

--- src/libraries/Common/src/System/Net/Security/NegotiateStreamPal.Windows.cs
+++ src/libraries/Common/src/System/Net/Security/NegotiateStreamPal.Windows.cs
@@ -55,6 +55,11 @@ namespace System.Net.Security
             }
         }
 
+        internal static string? QueryContextAssociatedName(SafeDeleteContext securityContext)
+        {
+            return SSPIWrapper.QueryStringContextAttributes(GlobalSSPI.SSPIAuth, securityContext, Interop.SspiCli.ContextAttribute.SECPKG_ATTR_NAMES);
+        }
+
         internal static string? QueryContextClientSpecifiedSpn(SafeDeleteContext securityContext)
         {
             return SSPIWrapper.QueryStringContextAttributes(GlobalSSPI.SSPIAuth, securityContext, Interop.SspiCli.ContextAttribute.SECPKG_ATTR_CLIENT_SPECIFIED_TARGET);
@@ -75,14 +80,14 @@ namespace System.Net.Security
             ref SafeDeleteContext? securityContext,
             string? spn,
             ContextFlagsPal requestedContextFlags,
-            byte[]? incomingBlob,
+            ReadOnlySpan<byte> incomingBlob,
             ChannelBinding? channelBinding,
             ref byte[]? resultBlob,
             ref ContextFlagsPal contextFlags)
         {
 
             InputSecurityBuffers inputBuffers = default;
-            if (incomingBlob != null)
+            if (!incomingBlob.IsEmpty)
             {
                 inputBuffers.SetNextBuffer(new InputSecurityBuffer(incomingBlob, SecurityBufferType.SECBUFFER_TOKEN));
             }
@@ -132,13 +137,13 @@ namespace System.Net.Security
             SafeFreeCredentials? credentialsHandle,
             ref SafeDeleteContext? securityContext,
             ContextFlagsPal requestedContextFlags,
-            byte[]? incomingBlob,
+            ReadOnlySpan<byte> incomingBlob,
             ChannelBinding? channelBinding,
             ref byte[]? resultBlob,
             ref ContextFlagsPal contextFlags)
         {
             InputSecurityBuffers inputBuffers = default;
-            if (incomingBlob != null)
+            if (!incomingBlob.IsEmpty)
             {
                 inputBuffers.SetNextBuffer(new InputSecurityBuffer(incomingBlob, SecurityBufferType.SECBUFFER_TOKEN));
             }
@@ -162,6 +167,14 @@ namespace System.Net.Security
                 inputBuffers,
                 ref outSecurityBuffer,
                 ref outContextFlags);
+
+            // SSPI Workaround
+            // If a client sends up a blob on the initial request, Negotiate returns SEC_E_INVALID_HANDLE
+            // when it should return SEC_E_INVALID_TOKEN.
+            if (winStatus == Interop.SECURITY_STATUS.InvalidHandle && securityContext == null && !incomingBlob.IsEmpty)
+            {
+                winStatus = Interop.SECURITY_STATUS.InvalidToken;
+            }
 
             resultBlob = outSecurityBuffer.token;
             securityContext = sslContext;
@@ -262,6 +275,196 @@ namespace System.Net.Security
 
             // return signed size
             return securityBuffer[0].size + securityBuffer[1].size;
+        }
+
+        internal static int Encrypt(
+            SafeDeleteContext securityContext,
+            ReadOnlySpan<byte> buffer,
+            bool isConfidential,
+            bool isNtlm,
+            [NotNull] ref byte[]? output,
+            uint sequenceNumber)
+        {
+            SecPkgContext_Sizes sizes = default;
+            bool success = SSPIWrapper.QueryBlittableContextAttributes(GlobalSSPI.SSPIAuth, securityContext, Interop.SspiCli.ContextAttribute.SECPKG_ATTR_SIZES, ref sizes);
+            Debug.Assert(success);
+
+            int maxCount = checked(int.MaxValue - 4 - sizes.cbBlockSize - sizes.cbSecurityTrailer);
+            if (buffer.Length > maxCount)
+            {
+                throw new ArgumentOutOfRangeException(nameof(buffer.Length), SR.Format(SR.net_io_out_range, maxCount));
+            }
+
+            int resultSize = buffer.Length + sizes.cbSecurityTrailer + sizes.cbBlockSize;
+            if (output == null || output.Length < resultSize + 4)
+            {
+                output = new byte[resultSize + 4];
+            }
+
+            // Make a copy of user data for in-place encryption.
+            buffer.CopyTo(output.AsSpan(4 + sizes.cbSecurityTrailer));
+
+            // Prepare buffers TOKEN(signature), DATA and Padding.
+            ThreeSecurityBuffers buffers = default;
+            var securityBuffer = MemoryMarshal.CreateSpan(ref buffers._item0, 3);
+            securityBuffer[0] = new SecurityBuffer(output, 4, sizes.cbSecurityTrailer, SecurityBufferType.SECBUFFER_TOKEN);
+            securityBuffer[1] = new SecurityBuffer(output, 4 + sizes.cbSecurityTrailer, buffer.Length, SecurityBufferType.SECBUFFER_DATA);
+            securityBuffer[2] = new SecurityBuffer(output, 4 + sizes.cbSecurityTrailer + buffer.Length, sizes.cbBlockSize, SecurityBufferType.SECBUFFER_PADDING);
+
+            int errorCode;
+            if (isConfidential)
+            {
+                errorCode = SSPIWrapper.EncryptMessage(GlobalSSPI.SSPIAuth, securityContext, securityBuffer, sequenceNumber);
+            }
+            else
+            {
+                if (isNtlm)
+                {
+                    securityBuffer[1].type |= SecurityBufferType.SECBUFFER_READONLY;
+                }
+
+                errorCode = SSPIWrapper.MakeSignature(GlobalSSPI.SSPIAuth, securityContext, securityBuffer, 0);
+            }
+
+            if (errorCode != 0)
+            {
+                Exception e = new Win32Exception(errorCode);
+                if (NetEventSource.Log.IsEnabled()) NetEventSource.Error(null, e);
+                throw e;
+            }
+
+            // Compacting the result.
+            resultSize = securityBuffer[0].size;
+            bool forceCopy = false;
+            if (resultSize != sizes.cbSecurityTrailer)
+            {
+                forceCopy = true;
+                Buffer.BlockCopy(output, securityBuffer[1].offset, output, 4 + resultSize, securityBuffer[1].size);
+            }
+
+            resultSize += securityBuffer[1].size;
+            if (securityBuffer[2].size != 0 && (forceCopy || resultSize != (buffer.Length + sizes.cbSecurityTrailer)))
+            {
+                Buffer.BlockCopy(output, securityBuffer[2].offset, output, 4 + resultSize, securityBuffer[2].size);
+            }
+
+            resultSize += securityBuffer[2].size;
+            unchecked
+            {
+                output[0] = (byte)((resultSize) & 0xFF);
+                output[1] = (byte)(((resultSize) >> 8) & 0xFF);
+                output[2] = (byte)(((resultSize) >> 16) & 0xFF);
+                output[3] = (byte)(((resultSize) >> 24) & 0xFF);
+            }
+
+            return resultSize + 4;
+        }
+
+        internal static int Decrypt(
+            SafeDeleteContext securityContext,
+            byte[]? buffer,
+            int offset,
+            int count,
+            bool isConfidential,
+            bool isNtlm,
+            out int newOffset,
+            uint sequenceNumber)
+        {
+            if (offset < 0 || offset > (buffer == null ? 0 : buffer.Length))
+            {
+                Debug.Fail("Argument 'offset' out of range.");
+                throw new ArgumentOutOfRangeException(nameof(offset));
+            }
+
+            if (count < 0 || count > (buffer == null ? 0 : buffer.Length - offset))
+            {
+                Debug.Fail("Argument 'count' out of range.");
+                throw new ArgumentOutOfRangeException(nameof(count));
+            }
+
+            if (isNtlm)
+            {
+                return DecryptNtlm(securityContext, buffer, offset, count, isConfidential, out newOffset, sequenceNumber);
+            }
+
+            //
+            // Kerberos and up
+            //
+            TwoSecurityBuffers buffers = default;
+            var securityBuffer = MemoryMarshal.CreateSpan(ref buffers._item0, 2);
+            securityBuffer[0] = new SecurityBuffer(buffer, offset, count, SecurityBufferType.SECBUFFER_STREAM);
+            securityBuffer[1] = new SecurityBuffer(0, SecurityBufferType.SECBUFFER_DATA);
+
+            int errorCode = isConfidential ?
+                SSPIWrapper.DecryptMessage(GlobalSSPI.SSPIAuth, securityContext, securityBuffer, sequenceNumber) :
+                SSPIWrapper.VerifySignature(GlobalSSPI.SSPIAuth, securityContext, securityBuffer, sequenceNumber);
+
+            if (errorCode != 0)
+            {
+                Exception e = new Win32Exception(errorCode);
+                if (NetEventSource.Log.IsEnabled()) NetEventSource.Error(null, e);
+                throw e;
+            }
+
+            if (securityBuffer[1].type != SecurityBufferType.SECBUFFER_DATA)
+            {
+                throw new InternalException(securityBuffer[1].type);
+            }
+
+            newOffset = securityBuffer[1].offset;
+            return securityBuffer[1].size;
+        }
+
+        private static int DecryptNtlm(
+            SafeDeleteContext securityContext,
+            byte[]? buffer,
+            int offset,
+            int count,
+            bool isConfidential,
+            out int newOffset,
+            uint sequenceNumber)
+        {
+            const int ntlmSignatureLength = 16;
+            // For the most part the arguments are verified in Decrypt().
+            if (count < ntlmSignatureLength)
+            {
+                Debug.Fail("Argument 'count' out of range.");
+                throw new ArgumentOutOfRangeException(nameof(count));
+            }
+
+            TwoSecurityBuffers buffers = default;
+            var securityBuffer = MemoryMarshal.CreateSpan(ref buffers._item0, 2);
+            securityBuffer[0] = new SecurityBuffer(buffer, offset, ntlmSignatureLength, SecurityBufferType.SECBUFFER_TOKEN);
+            securityBuffer[1] = new SecurityBuffer(buffer, offset + ntlmSignatureLength, count - ntlmSignatureLength, SecurityBufferType.SECBUFFER_DATA);
+
+            int errorCode;
+            SecurityBufferType realDataType = SecurityBufferType.SECBUFFER_DATA;
+
+            if (isConfidential)
+            {
+                errorCode = SSPIWrapper.DecryptMessage(GlobalSSPI.SSPIAuth, securityContext, securityBuffer, sequenceNumber);
+            }
+            else
+            {
+                realDataType |= SecurityBufferType.SECBUFFER_READONLY;
+                securityBuffer[1].type = realDataType;
+                errorCode = SSPIWrapper.VerifySignature(GlobalSSPI.SSPIAuth, securityContext, securityBuffer, sequenceNumber);
+            }
+
+            if (errorCode != 0)
+            {
+                Exception e = new Win32Exception(errorCode);
+                if (NetEventSource.Log.IsEnabled()) NetEventSource.Error(null, e);
+                throw new Win32Exception(errorCode);
+            }
+
+            if (securityBuffer[1].type != realDataType)
+            {
+                throw new InternalException(securityBuffer[1].type);
+            }
+
+            newOffset = securityBuffer[1].offset;
+            return securityBuffer[1].size;
         }
     }
 }
--- src/libraries/System.Net.Security/tests/UnitTests/NTAuthenticationTests.cs
+++ src/libraries/System.Net.Security/tests/UnitTests/NTAuthenticationTests.cs
@@ -17,95 +17,7 @@ namespace System.Net.Security.Tests
         private static bool IsNtlmInstalled => Capability.IsNtlmInstalled();
 
         private static NetworkCredential s_testCredentialRight = new NetworkCredential("rightusername", "rightpassword");
-        private static NetworkCredential s_testCredentialWrong = new NetworkCredential("rightusername", "wrongpassword");
         private static readonly byte[] s_Hello = "Hello"u8.ToArray();
-
-        [Fact]
-        public void NtlmProtocolExampleTest()
-        {
-            // Mirrors the NTLMv2 example in the NTLM specification:
-            NetworkCredential credential = new NetworkCredential("User", "Password", "Domain");
-            FakeNtlmServer fakeNtlmServer = new FakeNtlmServer(credential);
-            fakeNtlmServer.SendTimestamp = false;
-            fakeNtlmServer.TargetIsServer = true;
-            fakeNtlmServer.PreferUnicode = false;
-
-            // NEGOTIATE_MESSAGE
-            // Flags:
-            //   NTLMSSP_NEGOTIATE_KEY_EXCH
-            //   NTLMSSP_NEGOTIATE_56
-            //   NTLMSSP_NEGOTIATE_128
-            //   NTLMSSP_NEGOTIATE_VERSION
-            //   NTLMSSP_NEGOTIATE_TARGET_INFO
-            //   NTLMSSP_NEGOTIATE_EXTENDED_SESSIONSECURITY
-            //   NTLMSSP_TARGET_TYPE_SERVER
-            //   NTLMSSP_NEGOTIATE_ALWAYS_SIGN
-            //   NTLMSSP_NEGOTIATE_NTLM
-            //   NTLMSSP_NEGOTIATE_SEAL
-            //   NTLMSSP_NEGOTIATE_SIGN
-            //   NTLMSSP_NEGOTIATE_OEM
-            //   NTLMSSP_NEGOTIATE_UNICODE
-            // Domain: (empty) (should be "Domain" but the fake server doesn't check)
-            // Workstation: (empty) (should be "COMPUTER" but the fake server doesn't check)
-            // Version: 6.1.7600 / 15
-            byte[] negotiateBlob = Convert.FromHexString("4e544c4d535350000100000033828ae2000000000000000000000000000000000601b01d0000000f");
-            byte[]? challengeBlob = fakeNtlmServer.GetOutgoingBlob(negotiateBlob);
-
-            // CHALLENGE_MESSAGE from 4.2.4.3 Messages
-            byte[] expectedChallengeBlob = Convert.FromHexString(
-                "4e544c4d53535000020000000c000c003800000033828ae20123456789abcdef" +
-                "00000000000000002400240044000000060070170000000f5300650072007600" +
-                "6500720002000c0044006f006d00610069006e0001000c005300650072007600" +
-                "6500720000000000");
-            Assert.Equal(expectedChallengeBlob, challengeBlob);
-
-            // AUTHENTICATE_MESSAGE from 4.2.4.3 Messages
-            byte[] authenticateBlob = Convert.FromHexString(
-                "4e544c4d5353500003000000180018006c00000054005400840000000c000c00" +
-                "480000000800080054000000100010005c00000010001000d8000000358288e2" +
-                "0501280a0000000f44006f006d00610069006e00550073006500720043004f00" +
-                "4d005000550054004500520086c35097ac9cec102554764a57cccc19aaaaaaaa" +
-                "aaaaaaaa68cd0ab851e51c96aabc927bebef6a1c010100000000000000000000" +
-                "00000000aaaaaaaaaaaaaaaa0000000002000c0044006f006d00610069006e00" +
-                "01000c005300650072007600650072000000000000000000c5dad2544fc97990" +
-                "94ce1ce90bc9d03e");
-            byte[]? empty = fakeNtlmServer.GetOutgoingBlob(authenticateBlob);
-            Assert.Null(empty);
-            Assert.True(fakeNtlmServer.IsAuthenticated);
-            Assert.False(fakeNtlmServer.IsMICPresent);
-        }
-
-        [ConditionalFact(nameof(IsNtlmInstalled))]
-        public void NtlmCorrectExchangeTest()
-        {
-            FakeNtlmServer fakeNtlmServer = new FakeNtlmServer(s_testCredentialRight);
-            NTAuthentication ntAuth = new NTAuthentication(
-                isServer: false, "NTLM", s_testCredentialRight, "HTTP/foo",
-                ContextFlagsPal.Connection | ContextFlagsPal.InitIntegrity, null);
-
-            DoNtlmExchange(fakeNtlmServer, ntAuth);
-
-            Assert.True(fakeNtlmServer.IsAuthenticated);
-            // NTLMSSP on Linux doesn't send the MIC and sends incorrect SPN (drops the service prefix)
-            if (!OperatingSystem.IsLinux())
-            {
-                Assert.True(fakeNtlmServer.IsMICPresent);
-                Assert.Equal("HTTP/foo", fakeNtlmServer.ClientSpecifiedSpn);
-            }
-        }
-
-        [ConditionalFact(nameof(IsNtlmInstalled))]
-        public void NtlmIncorrectExchangeTest()
-        {
-            FakeNtlmServer fakeNtlmServer = new FakeNtlmServer(s_testCredentialRight);
-            NTAuthentication ntAuth = new NTAuthentication(
-                isServer: false, "NTLM", s_testCredentialWrong, "HTTP/foo",
-                ContextFlagsPal.Connection | ContextFlagsPal.InitIntegrity, null);
-
-            DoNtlmExchange(fakeNtlmServer, ntAuth);
-
-            Assert.False(fakeNtlmServer.IsAuthenticated);
-        }
 
         [ConditionalFact(nameof(IsNtlmInstalled))]
         [ActiveIssue("https://github.com/dotnet/runtime/issues/65678", TestPlatforms.OSX | TestPlatforms.iOS | TestPlatforms.MacCatalyst)]
@@ -152,51 +64,6 @@ namespace System.Net.Security.Tests
             Assert.NotNull(authenticateBlob);
             byte[]? empty = fakeNtlmServer.GetOutgoingBlob(authenticateBlob);
             Assert.Null(empty);
-        }
-
-        [ConditionalTheory(nameof(IsNtlmInstalled))]
-        [InlineData(true, true)]
-        [InlineData(true, false)]
-        [InlineData(false, false)]
-        public void NegotiateCorrectExchangeTest(bool requestMIC, bool requestConfidentiality)
-        {
-            // Older versions of gss-ntlmssp on Linux generate MIC at incorrect offset unless ForceNegotiateVersion is specified
-            FakeNtlmServer fakeNtlmServer = new FakeNtlmServer(s_testCredentialRight) { ForceNegotiateVersion = true };
-            FakeNegotiateServer fakeNegotiateServer = new FakeNegotiateServer(fakeNtlmServer) { RequestMIC = requestMIC };
-            NTAuthentication ntAuth = new NTAuthentication(
-                isServer: false, "Negotiate", s_testCredentialRight, "HTTP/foo",
-                ContextFlagsPal.Connection | ContextFlagsPal.InitIntegrity |
-                (requestConfidentiality ? ContextFlagsPal.Confidentiality : 0), null);
-
-            byte[]? clientBlob = null;
-            byte[]? serverBlob = null;
-            do
-            {
-                clientBlob = ntAuth.GetOutgoingBlob(serverBlob, throwOnError: false, out SecurityStatusPal status);
-                if (clientBlob != null)
-                {
-                    Assert.False(fakeNegotiateServer.IsAuthenticated);
-                    // Send the client blob to the fake server
-                    serverBlob = fakeNegotiateServer.GetOutgoingBlob(clientBlob);
-                }
-
-                if (status.ErrorCode == SecurityStatusPalErrorCode.OK)
-                {
-                    Assert.True(ntAuth.IsCompleted);
-                    Assert.True(fakeNegotiateServer.IsAuthenticated);
-                    Assert.True(fakeNtlmServer.IsAuthenticated);
-                }
-                else if (status.ErrorCode == SecurityStatusPalErrorCode.ContinueNeeded)
-                {
-                    Assert.NotNull(clientBlob);
-                    Assert.NotNull(serverBlob);
-                }
-                else
-                {
-                    Assert.Fail(status.ErrorCode.ToString());
-                }
-            }
-            while (!ntAuth.IsCompleted);
         }
     }
 }
