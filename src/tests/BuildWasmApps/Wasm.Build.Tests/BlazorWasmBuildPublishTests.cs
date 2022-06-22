7ed35e9bf36b61102ce267db0f045270dc0df811
Ankit Jain <radical@gmail.com>
2022-06-22 16:02:33 +0000

    trying with dotnet-sdk-7.0.100-preview.6.22315.7-linux-x64.tar.gz

--- src/tests/BuildWasmApps/Wasm.Build.Tests/BlazorWasmBuildPublishTests.cs
+++ src/tests/BuildWasmApps/Wasm.Build.Tests/BlazorWasmBuildPublishTests.cs
@@ -82,7 +82,7 @@ namespace Wasm.Build.Tests
         [InlineData("Debug", false)]
         [InlineData("Release", true)]
         [InlineData("Release", false)]
-        [ActiveIssue("https://github.com/dotnet/runtime/issues/70985", TestPlatforms.Linux)]
+        //[ActiveIssue("https://github.com/dotnet/runtime/issues/70985", TestPlatforms.Linux)]
         public void NativeBuild_WithDeployOnBuild_UsedByVS(string config, bool nativeRelink)
         {
             string id = $"blz_deploy_on_build_{config}_{nativeRelink}";
@@ -144,7 +144,7 @@ namespace Wasm.Build.Tests
         [Theory]
         [InlineData("Debug")]
         [InlineData("Release")]
-        [ActiveIssue("https://github.com/dotnet/runtime/issues/70985", TestPlatforms.Linux)]
+        //[ActiveIssue("https://github.com/dotnet/runtime/issues/70985", TestPlatforms.Linux)]
         public void WithNativeReference_AOTInProjectFile(string config)
         {
             string id = $"blz_nativeref_aot_pf_{config}";
@@ -162,7 +162,7 @@ namespace Wasm.Build.Tests
         [Theory]
         [InlineData("Debug")]
         [InlineData("Release")]
-        [ActiveIssue("https://github.com/dotnet/runtime/issues/70985", TestPlatforms.Linux)]
+        //[ActiveIssue("https://github.com/dotnet/runtime/issues/70985", TestPlatforms.Linux)]
         public void WithNativeReference_AOTOnCommandLine(string config)
         {
             string id = $"blz_nativeref_aot_cl_{config}";
