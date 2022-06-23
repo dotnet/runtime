2906e1e886499aeb08683e4dc29970b3a4e5c772
Aleksey Kliger <alklig@microsoft.com>
2022-06-23 15:39:39 -0400

    [diagnostic_server] wasm-specific fn_table
    We won't be using the native C version

--- src/mono/mono/component/diagnostics_server.c
+++ src/mono/mono/component/diagnostics_server.c
@@ -7,10 +7,14 @@
 #include <mono/utils/mono-publib.h>
 #include <mono/utils/mono-compiler.h>
 #include <eventpipe/ds-server.h>
+#ifdef HOST_BROWSER
+#include <emscripten/emscripten.h>
+#endif
 
 static bool
 diagnostics_server_available (void);
 
+#ifndef HOST_BROWSER
 static MonoComponentDiagnosticsServer fn_table = {
 	{ MONO_COMPONENT_ITF_VERSION, &diagnostics_server_available },
 	&ds_server_init,
@@ -18,15 +22,81 @@ static MonoComponentDiagnosticsServer fn_table = {
 	&ds_server_pause_for_diagnostics_monitor,
 	&ds_server_disable
 };
+#else
+static bool
+ds_server_wasm_init (void);
+
+static bool
+ds_server_wasm_shutdown (void);
+
+static void
+ds_server_wasm_pause_for_diagnostics_monitor (void);
+
+static void
+ds_server_wasm_disable (void);
+
+static MonoComponentDiagnosticsServer fn_table = {
+	{ MONO_COMPONENT_ITF_VERSION, &diagnostics_server_available },
+	&ds_server_wasm_init,
+	&ds_server_wasm_shutdown,
+	&ds_server_wasm_pause_for_diagnostics_monitor,
+	&ds_server_wasm_disable,
+};
+#endif
 
 static bool
 diagnostics_server_available (void)
 {
+	EM_ASM({
+			console.log ("diagnostic server available");
+		});
 	return true;
 }
 
 MonoComponentDiagnosticsServer *
 mono_component_diagnostics_server_init (void)
 {
+	EM_ASM({
+			console.log ("diagnostic server component init");
+		});
 	return &fn_table;
 }
+
+#ifdef HOST_BROWSER
+
+static bool
+ds_server_wasm_init (void)
+{
+	EM_ASM({
+			console.log ("ds_server_wasm_init");
+		});
+	return true;
+}
+
+
+static bool
+ds_server_wasm_shutdown (void)
+{
+	EM_ASM({
+			console.log ("ds_server_wasm_shutdown");
+		});
+	return true;
+}
+
+static void
+ds_server_wasm_pause_for_diagnostics_monitor (void)
+{
+	EM_ASM({
+			console.log ("ds_server_wasm_pause_for_diagnostics_monitor");
+		});
+}
+
+static void
+ds_server_wasm_disable (void)
+{
+	EM_ASM({
+			console.log ("ds_server_wasm_disable");
+		});
+}
+
+#endif;
