8966644f12d73c5db635fd43b5ae9449db767d62
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2022-06-27 14:14:09 +0100

    Revert "Move IsCalendarDateOnly field to the end of the struct."
    This reverts commit 217740ae352c14c3245f6cb01c67e9c78aa2fbd5.

--- src/libraries/System.Text.Json/src/System/Text/Json/JsonHelpers.Date.cs
+++ src/libraries/System.Text.Json/src/System/Text/Json/JsonHelpers.Date.cs
@@ -13,6 +13,7 @@ namespace System.Text.Json
             public int Year;
             public int Month;
             public int Day;
+            public bool IsCalendarDateOnly;
             public int Hour;
             public int Minute;
             public int Second;
@@ -20,7 +21,6 @@ namespace System.Text.Json
             public int OffsetHours;
             public int OffsetMinutes;
             public bool OffsetNegative => OffsetToken == JsonConstants.Hyphen;
-            public bool IsCalendarDateOnly;
             public byte OffsetToken;
         }
 
