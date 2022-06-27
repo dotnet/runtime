632b85867b9994c6ac98d1caa7ad8f6ca09aa21f
Denys Zhuravel <derwasp@users.noreply.github.com>
2022-06-27 20:44:03 +0000

    Add an assert of te SetExternalAttributes

--- src/libraries/System.IO.Compression.ZipFile/src/System/IO/Compression/ZipFileExtensions.ZipArchive.Create.Unix.cs
+++ src/libraries/System.IO.Compression.ZipFile/src/System/IO/Compression/ZipFileExtensions.ZipArchive.Create.Unix.cs
@@ -1,12 +1,16 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics;
+
 namespace System.IO.Compression
 {
     public static partial class ZipFileExtensions
     {
         static partial void SetExternalAttributes(FileStream fs, ZipArchiveEntry entry)
         {
+            Debug.Assert(!OperatingSystem.IsWindows());
+
             Interop.Sys.FileStatus status;
             Interop.CheckIo(Interop.Sys.FStat(fs.SafeFileHandle, out status), fs.Name);
 
