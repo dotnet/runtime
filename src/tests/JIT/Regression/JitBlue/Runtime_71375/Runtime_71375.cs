b533e49e6ca2a41e8c4caee14dab363f57622b9c
SingleAccretion <AccretionMail@yandex.ru>
2022-06-28 19:19:52 +0300

    Work around vectors in varargs being broken

--- src/tests/JIT/Regression/JitBlue/Runtime_71375/Runtime_71375.cs
+++ src/tests/JIT/Regression/JitBlue/Runtime_71375/Runtime_71375.cs
@@ -8,14 +8,19 @@ public class Runtime_71375
 {
     public static int Main()
     {
-        return Problem() ? 101 : 100;
+        // At the time of writing this test, the calling convention for incoming vector parameters on
+        // Windows ARM64 was broken, so only the fact that "Problem" compiled without asserts was
+        // checked. If/once the above is fixed, this test should be changed to actually call "Problem".
+        RuntimeHelpers.PrepareMethod(typeof(Runtime_71375).GetMethod("Problem").MethodHandle);
+
+        return 100;
     }
 
     [MethodImpl(MethodImplOptions.NoInlining)]
     static int VarArgs(int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, Vector128<int> splitArg, __arglist) => splitArg.GetElement(0);
 
     [MethodImpl(MethodImplOptions.NoInlining)]
-    private static bool Problem()
+    public static bool Problem()
     {
         return VarArgs(0, 0, 0, 0, 0, 0, Vector128<int>.AllBitsSet, __arglist()) != -1;
     }
