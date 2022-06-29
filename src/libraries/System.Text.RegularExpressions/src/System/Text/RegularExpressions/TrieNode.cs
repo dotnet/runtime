71fe365d16bc7a6278e367fdc8c25554d55109fe
Theodore Tsirpanis <teo@tsirpanis.gr>
2022-06-29 18:41:01 +0300

    Do not allow the root node to be a match.

--- src/libraries/System.Text.RegularExpressions/src/System/Text/RegularExpressions/TrieNode.cs
+++ src/libraries/System.Text.RegularExpressions/src/System/Text/RegularExpressions/TrieNode.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Collections.Generic;
@@ -24,7 +24,19 @@ namespace System.Text.RegularExpressions
 
         public Dictionary<char, int> Children = new();
 
-        public bool IsMatch;
+        private bool _isMatch;
+
+        public bool IsMatch
+        {
+            get => _isMatch;
+            set
+            {
+                if (Parent != -1)
+                {
+                    _isMatch = value;
+                }
+            }
+        }
 
         public int Parent { get; init; }
 
