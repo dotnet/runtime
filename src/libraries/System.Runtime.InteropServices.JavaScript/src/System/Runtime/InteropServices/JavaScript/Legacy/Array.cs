4823df3deeb236b5506f49739fafe787f66d8c2b
Pavel Savara <pavel.savara@gmail.com>
2022-06-30 16:59:47 +0200

    [wasm] Preparation for public API in System.Runtime.InteropServices.JavaScript (#71332)
    *    new ref assembly System.Runtime.InteropServices.JavaScript - empty
    *    new src assembly System.Runtime.InteropServices.JavaScript
            moved all implementation from System.Private.Runtime.InteropServices.JavaScript into it
    *    added IMPORTS, EXPORTS to js API
    *    refactored setup_managed_proxy and teardown_managed_proxy in JS
    *    added more range assert for working with wasm memory in JS

--- src/libraries/System.Runtime.InteropServices.JavaScript/src/System/Runtime/InteropServices/JavaScript/Legacy/Array.cs
+++ src/libraries/System.Runtime.InteropServices.JavaScript/src/System/Runtime/InteropServices/JavaScript/Legacy/Array.cs
@@ -6,14 +6,18 @@ namespace System.Runtime.InteropServices.JavaScript
     /// <summary>
     /// Initializes a new instance of JavaScript Core Array class.
     /// </summary>
+    [Obsolete]
     public class Array : JSObject
     {
         /// <summary>
         /// Initializes a new instance of the Array class.
         /// </summary>
         /// <param name="_params">Parameters.</param>
-        public Array(params object[] _params) : base(nameof(Array), _params)
-        { }
+        public Array(params object[] _params)
+            : base(JavaScriptImports.CreateCSOwnedObject(nameof(Array), _params))
+        {
+            JSHostImplementation.RegisterCSOwnedObject(this);
+        }
 
         /// <summary>
         /// Initializes a new instance of the Array/> class.
@@ -27,26 +31,26 @@ namespace System.Runtime.InteropServices.JavaScript
         /// </summary>
         /// <returns>The new length of the Array push was called on</returns>
         /// <param name="elements">Elements.</param>
-        public int Push(params object[] elements) => (int)Invoke("push", elements);
+        public int Push(params object[] elements) => (int)this.Invoke("push", elements);
 
         /// <summary>
         /// Pop this instance.
         /// </summary>
         /// <returns>The element removed from the array or null if the array was empty</returns>
-        public object Pop() => (object)Invoke("pop");
+        public object Pop() => (object)this.Invoke("pop");
 
         /// <summary>
         /// Remove the first element of the Array and return that element
         /// </summary>
         /// <returns>The removed element</returns>
-        public object Shift() => Invoke("shift");
+        public object Shift() => this.Invoke("shift");
 
         /// <summary>
         /// Add <paramref name="elements"/> to the array starting at index <c>0</c>
         /// </summary>
         /// <returns>The length after shift.</returns>
         /// <param name="elements">Elements.</param>
-        public int UnShift(params object[] elements) => (int)Invoke("unshift", elements);
+        public int UnShift(params object[] elements) => (int)this.Invoke("unshift", elements);
 
         /// <summary>
         /// Index of the search element.
@@ -54,14 +58,14 @@ namespace System.Runtime.InteropServices.JavaScript
         /// <returns>The index of first occurrence of searchElement in the Array or -1 if not Found</returns>
         /// <param name="searchElement">Search element.</param>
         /// <param name="fromIndex">The index to start the search from</param>
-        public int IndexOf(object searchElement, int fromIndex = 0) => (int)Invoke("indexOf", searchElement, fromIndex);
+        public int IndexOf(object searchElement, int fromIndex = 0) => (int)this.Invoke("indexOf", searchElement, fromIndex);
 
         /// <summary>
         /// Finds the index of the last occurrence of<paramref name="searchElement" />
         /// </summary>
         /// <returns>The index of the last occurrence</returns>
         /// <param name="searchElement">Search element.</param>
-        public int LastIndexOf(object searchElement) => (int)Invoke("lastIndexOf", searchElement);
+        public int LastIndexOf(object searchElement) => (int)this.Invoke("lastIndexOf", searchElement);
 
         /// <summary>
         /// Finds the index of the last occurrence of<paramref name="searchElement" /> between 0 and <paramref name="endIndex" />.
@@ -69,7 +73,7 @@ namespace System.Runtime.InteropServices.JavaScript
         /// <returns>The index of the last occurrence.</returns>
         /// <param name="searchElement">Search element.</param>
         /// <param name="endIndex">End index.</param>
-        public int LastIndexOf(object searchElement, int endIndex) => (int)Invoke("lastIndexOf", searchElement, endIndex);
+        public int LastIndexOf(object searchElement, int endIndex) => (int)this.Invoke("lastIndexOf", searchElement, endIndex);
 
         /// <summary>
         /// Gets or sets the Array with the index specified by <paramref name="i" />.
@@ -79,18 +83,18 @@ namespace System.Runtime.InteropServices.JavaScript
         {
             get
             {
-                AssertNotDisposed();
+                this.AssertNotDisposed();
 
                 Interop.Runtime.GetByIndexRef(JSHandle, i, out int exception, out object indexValue);
 
                 if (exception != 0)
                     throw new JSException((string)indexValue);
-                Interop.Runtime.ReleaseInFlight(indexValue);
+                JSHostImplementation.ReleaseInFlight(indexValue);
                 return indexValue;
             }
             set
             {
-                AssertNotDisposed();
+                this.AssertNotDisposed();
 
                 Interop.Runtime.SetByIndexRef(JSHandle, i, value, out int exception, out object res);
 
@@ -106,8 +110,8 @@ namespace System.Runtime.InteropServices.JavaScript
         /// <value>The length.</value>
         public int Length
         {
-            get => Convert.ToInt32(GetObjectProperty("length"));
-            set => SetObjectProperty("length", value, false);
+            get => Convert.ToInt32(this.GetObjectProperty("length"));
+            set => this.SetObjectProperty("length", value, false);
         }
     }
 }
