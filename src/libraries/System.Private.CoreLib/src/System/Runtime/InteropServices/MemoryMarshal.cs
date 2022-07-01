e785b70e823250fa4f2734d27a458e6a5d0c356d
Aaron Robinson <arobins@microsoft.com>
2022-07-01 13:39:44 -0700

    Remove unnecessary Unsafe.AsRef<T> usage.

--- src/libraries/System.Private.CoreLib/src/System/Runtime/InteropServices/MemoryMarshal.cs
+++ src/libraries/System.Private.CoreLib/src/System/Runtime/InteropServices/MemoryMarshal.cs
@@ -77,13 +77,13 @@ namespace System.Runtime.InteropServices
         /// Returns a reference to the 0th element of the Span. If the Span is empty, returns a reference to the location where the 0th element
         /// would have been stored. Such a reference may or may not be null. It can be used for pinning but must never be dereferenced.
         /// </summary>
-        public static ref T GetReference<T>(Span<T> span) => ref Unsafe.AsRef<T>(in span._reference);
+        public static ref T GetReference<T>(Span<T> span) => ref span._reference;
 
         /// <summary>
         /// Returns a reference to the 0th element of the ReadOnlySpan. If the ReadOnlySpan is empty, returns a reference to the location where the 0th element
         /// would have been stored. Such a reference may or may not be null. It can be used for pinning but must never be dereferenced.
         /// </summary>
-        public static ref T GetReference<T>(ReadOnlySpan<T> span) => ref Unsafe.AsRef<T>(in span._reference);
+        public static ref T GetReference<T>(ReadOnlySpan<T> span) => ref span._reference;
 
         /// <summary>
         /// Returns a reference to the 0th element of the Span. If the Span is empty, returns a reference to fake non-null pointer. Such a reference can be used
