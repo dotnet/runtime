19be28617b0f54cfc116d18b19d17b9843b8008c
Pavel Savara <pavelsavara@microsoft.com>
2022-07-01 10:14:01 +0200

    Update src/libraries/System.Runtime.InteropServices.JavaScript/src/System/Runtime/InteropServices/JavaScript/JSObject.cs
    Co-authored-by: Marek Fišera <mara@neptuo.com>

--- src/libraries/System.Runtime.InteropServices.JavaScript/src/System/Runtime/InteropServices/JavaScript/JSObject.cs
+++ src/libraries/System.Runtime.InteropServices.JavaScript/src/System/Runtime/InteropServices/JavaScript/JSObject.cs
@@ -46,7 +46,7 @@ namespace System.Runtime.InteropServices.JavaScript
         }
 
         /// <summary>
-        /// Returns value of the property as <see cref="string"/> if the property exists, otherwise 0.
+        /// Returns value of the property as <see cref="int"/> if the property exists, otherwise 0.
         /// </summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public int GetPropertyAsInt32(string propertyName)
