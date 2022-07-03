7744ffdf3973e080729c2d97eca9dc435857c438
SingleAccretion <AccretionMail@yandex.ru>
2022-07-04 01:03:57 +0300

    Delete invalid assert

--- src/coreclr/jit/codegenarmarch.cpp
+++ src/coreclr/jit/codegenarmarch.cpp
@@ -1224,9 +1224,6 @@ void CodeGen::genPutArgSplit(GenTreePutArgSplit* treeNode)
             assert(baseReg != addrReg);
         }
 
-        // We don't split HFAs.
-        assert(!compiler->IsHfa(layout->GetClassHandle()));
-
         // Put on stack first
         unsigned structOffset  = treeNode->gtNumRegs * TARGET_POINTER_SIZE;
         unsigned remainingSize = layout->GetSize() - structOffset;
