858019ca1a5fb458f790789437dfa7029a1169b0
SingleAccretion <AccretionMail@yandex.ru>
2022-07-04 18:24:39 +0300

    Add a test for the "src" case

---
+++ src/tests/JIT/Regression/JitBlue/Runtime_71601/Runtime_71601.cs
@@ -0,0 +1,41 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Numerics;
+using System.Runtime.CompilerServices;
+
+public class Runtime_71601
+{
+    public static int Main()
+    {
+        if (ProblemWithPrimitiveSrc())
+        {
+            return 101;
+        }
+
+        return 100;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private static bool ProblemWithPrimitiveSrc()
+    {
+        WrapTuple p = new WrapTuple { FieldOne = { Value = 1 }, FieldTwo = { Value = 2 } };
+        Wrap a = p.FieldTwo;
+        Wrap b = WrapTuple.GetFieldTwo(ref p);
+
+        return a.Value != b.Value;
+    }
+
+    struct WrapTuple
+    {
+        public Wrap FieldOne;
+        public Wrap FieldTwo;
+
+        public static ref Wrap GetFieldTwo(ref WrapTuple t) => ref Unsafe.Add(ref t.FieldOne, 1);
+    }
+
+    struct Wrap
+    {
+        public int Value;
+    }
+}
