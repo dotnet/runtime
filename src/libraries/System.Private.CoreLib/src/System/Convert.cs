80dd0fcdc3554f95da762c26e34997408b7c6d83
EgorBo <egorbo@gmail.com>
2022-07-08 02:54:29 +0200

    Clean up

--- src/libraries/System.Private.CoreLib/src/System/Convert.cs
+++ src/libraries/System.Private.CoreLib/src/System/Convert.cs
@@ -2342,9 +2342,16 @@ namespace System
 
             if (!insertLineBreaks && bytes.Length >= 64)
             {
-                Span<byte> utf8Result = outputLength <= 256 ? stackalloc byte[256] : new byte[outputLength];
-                Base64.EncodeToUtf8(bytes, utf8Result, out int _, out int _);
-                return Encoding.Latin1.GetString(utf8Result);
+                // For large inputs it's faster to allocate a temp buffer and call UTF8 version
+                // which is then extended to UTF8 via Latin1.GetString (base64 is always ASCI)
+                [MethodImpl(MethodImplOptions.NoInlining)]
+                static string ToBase64StringLargeInputs(ReadOnlySpan<byte> data, int outputLen)
+                {
+                    Span<byte> utf8buffer = outputLen <= 256 ? stackalloc byte[256] : new byte[outputLen];
+                    Base64.EncodeToUtf8(data, utf8buffer, out int _, out int _);
+                    return Encoding.Latin1.GetString(utf8buffer);
+                }
+                return ToBase64StringLargeInputs(bytes, outputLength);
             }
 
             string result = string.FastAllocateString(outputLength);
