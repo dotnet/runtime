efecfa47f7544f7de27c3ab61e02cb9eab21107f
Jeremy Koritzinsky <jekoritz@microsoft.com>
2022-07-08 11:16:58 -0700

    Remove explicit suppressions in source. Add a prototype diagnostic suppressor for suppressing any diagnostics that guide users to not follow the marshaller shapes.

---
+++ src/libraries/System.Runtime.InteropServices/gen/LibraryImportGenerator/Analyzers/ShapeBreakingDiagnosticSuppressor.cs
@@ -0,0 +1,59 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Microsoft.Interop.Analyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public class ShapeBreakingDiagnosticSuppressor : DiagnosticSuppressor
+    {
+        public static readonly SuppressionDescriptor MarkMethodsAsStaticSuppression = new SuppressionDescriptor("SYSLIBSUPPRESS1001", "CA1822", "Do not offer to make methods static when they need to be instance methods.");
+
+        public override ImmutableArray<SuppressionDescriptor> SupportedSuppressions =>
+            ImmutableArray.Create(MarkMethodsAsStaticSuppression);
+
+        public override void ReportSuppressions(SuppressionAnalysisContext context)
+        {
+            foreach (var diagnostic in context.ReportedDiagnostics)
+            {
+                if (diagnostic.Id == MarkMethodsAsStaticSuppression.SuppressedDiagnosticId)
+                {
+                    SuppressMarkMethodsAsStaticDiagnosticIfNeeded(context, diagnostic);
+                }
+            }
+        }
+
+        private static void SuppressMarkMethodsAsStaticDiagnosticIfNeeded(SuppressionAnalysisContext context, Diagnostic diagnostic)
+        {
+            SemanticModel model = context.GetSemanticModel(diagnostic.Location.SourceTree);
+            ISymbol symbol = model.GetDeclaredSymbol(diagnostic.Location.SourceTree.GetRoot(context.CancellationToken).FindNode(diagnostic.Location.SourceSpan), context.CancellationToken);
+            if (symbol.Name == "Free" && symbol.Kind == SymbolKind.Method) // TODO: Extend to all names recognized in the shape
+            {
+                if (symbol.ContainingType is { TypeKind: TypeKind.Struct })
+                {
+                    bool isCustomTypeMarshaller = GetAllContainingTypes(symbol).Any(type => type.GetAttributes().Any(
+                        attr => attr.AttributeClass?.ToDisplayString() == TypeNames.CustomMarshallerAttribute
+                            && attr.AttributeConstructor is not null
+                            && attr.ConstructorArguments[2].Value is INamedTypeSymbol marshallerType
+                            && SymbolEqualityComparer.Default.Equals(marshallerType, symbol.ContainingType)));
+                    context.ReportSuppression(Suppression.Create(MarkMethodsAsStaticSuppression, diagnostic));
+                }
+            }
+        }
+
+        private static IEnumerable<ITypeSymbol> GetAllContainingTypes(ISymbol symbol)
+        {
+            for (INamedTypeSymbol containingType = symbol.ContainingType; containingType is not null; containingType = containingType.ContainingType)
+            {
+                yield return containingType;
+            }
+        }
+    }
+}
---
+++ src/libraries/System.Runtime.InteropServices/tests/LibraryImportGenerator.UnitTests/ShapeBreakingDiagnosticSuppressorTests.cs
@@ -0,0 +1,93 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using LibraryImportGenerator.UnitTests.Verifiers;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Testing;
+using Microsoft.Interop.Analyzers;
+using Xunit;
+
+namespace LibraryImportGenerator.Unit.Tests
+{
+    public class ShapeBreakingDiagnosticSuppressorTests
+    {
+        [Fact]
+        public async Task EmptyParameterlessFreeMethodHasSuppressedDiagnostic()
+        {
+            await VerifySuppressorAsync("""
+                using System.Runtime.InteropServices.Marshalling;
+
+                struct S
+                {
+                    public bool b;
+                };
+
+                [CustomMarshaller(typeof(S), MarshalMode.ManagedToUnmanagedIn, typeof(ManagedToUnmanagedIn))]
+                static class Marshaller
+                {
+                    public struct ManagedToUnmanagedIn
+                    {
+                        private int i;
+                        public void FromManaged(S s) { i = s.b ? 1 : 0; }
+                        public ManagedToUnmanagedIn ToUnmanaged() => this;
+
+                        public void {|#0:Free|}() {}
+                    }
+                }
+                """,
+                new DiagnosticResult(ShapeBreakingDiagnosticSuppressor.MarkMethodsAsStaticSuppression.SuppressedDiagnosticId, DiagnosticSeverity.Info).WithLocation(0).WithIsSuppressed(true));
+        }
+
+        private static async Task VerifySuppressorAsync(string source, params DiagnosticResult[] expected)
+        {
+            var test = new Test
+            {
+                TestCode = source,
+            };
+
+            test.ExpectedDiagnostics.AddRange(expected);
+            await test.RunAsync(CancellationToken.None);
+        }
+
+        class Test : CSharpCodeFixVerifier<EmptyDiagnosticAnalyzer, EmptyCodeFixProvider>.Test
+        {
+            public Test()
+            {
+                // Don't verify that we don't emit any diagnostics when they're disabled with #pragma warning disable
+                // This check doesn't work when we set up the CompilationWithAnalyzers object to report suppressed diagnostics
+                TestBehaviors |= TestBehaviors.SkipSuppressionCheck;
+            }
+            protected override IEnumerable<DiagnosticAnalyzer> GetDiagnosticAnalyzers()
+            {
+                return new DiagnosticAnalyzer[]
+                {
+                    // Our diagnostic suppressor
+                    new ShapeBreakingDiagnosticSuppressor(),
+                    // Each of the analyzers the suppressor supports suppressing diagnostics from
+                    new Microsoft.CodeQuality.Analyzers.QualityGuidelines.MarkMembersAsStaticAnalyzer()
+                };
+            }
+
+            protected override CompilationWithAnalyzers CreateCompilationWithAnalyzers(Compilation compilation, ImmutableArray<DiagnosticAnalyzer> analyzers, AnalyzerOptions options, CancellationToken cancellationToken)
+            {
+                return new CompilationWithAnalyzers(
+                    compilation,
+                    analyzers,
+                    new CompilationWithAnalyzersOptions(
+                        options,
+                        onAnalyzerException: null,
+                        concurrentAnalysis: true,
+                        logAnalyzerExecutionTime: true,
+                        reportSuppressedDiagnostics: true)); // We're specifically testing a DiagnosticSuppressor here, so we want to test that we find suppressed diagnostics.
+            }
+        }
+    }
+}
