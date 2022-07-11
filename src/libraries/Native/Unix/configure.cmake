e680ba8070c6c04f65c98f780c758ff15b4118e9
Simon Rozsival <simon@rozsival.com>
2022-07-11 15:35:20 +0200

    Port getifaddrs implementation from monodroid

--- src/libraries/Native/Unix/configure.cmake
+++ src/libraries/Native/Unix/configure.cmake
@@ -142,18 +142,6 @@ check_c_source_compiles(
     "
     HAVE_FLOCK64)
 
-check_c_source_compiles(
-    "
-    #include <sys/types.h>
-    #include <ifaddrs.h>
-    int main(void)
-    {
-        struct ifaddrs ia;
-        return 0;
-    }
-    "
-    HAVE_IFADDRS)
-
 check_symbol_exists(
     O_CLOEXEC
     fcntl.h
