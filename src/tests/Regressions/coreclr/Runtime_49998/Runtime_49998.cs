2d3398ef1c0774ea38e61c6b59696471862d3827
Will Smith <lol.tihan@gmail.com>
2022-07-11 14:16:18 -0700

    ILASM - Allow EntryPoint methods to be on interfaces

---
+++ src/tests/Regressions/coreclr/Runtime_49998/Runtime_49998.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace InterfaceMain
+{
+    interface Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
