91e94761d2a55b87b32ffd19269f3f950dc1b72c
antonfirsov <antonfir@gmail.com>
2022-07-11 15:43:48 +0200

    fix HttpListener after merge

--- src/libraries/System.Net.HttpListener/src/System/Net/Managed/HttpConnection.cs
+++ src/libraries/System.Net.HttpListener/src/System/Net/Managed/HttpConnection.cs
@@ -101,7 +101,9 @@ namespace System.Net
             }
 
             _timer = new Timer(OnTimeout, null, Timeout.Infinite, Timeout.Infinite);
+#pragma warning disable SYSLIB0014 // ServicePointManager is obsolete
             _sslStream?.AuthenticateAsServer(_cert, true, (SslProtocols)ServicePointManager.SecurityProtocol, false);
+#pragma warning restore SYSLIB0014
             Init();
         }
 
