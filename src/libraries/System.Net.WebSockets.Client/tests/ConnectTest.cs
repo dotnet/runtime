adea0cc8f74cc80f6379e21253aae170aa65d5c1
Katya Sokolova <esokolov@microsoft.com>
2022-07-11 16:23:46 +0200

    Update src/libraries/System.Net.WebSockets.Client/tests/ConnectTest.cs
    Co-authored-by: Natalia Kondratyeva <knatalia@microsoft.com>

--- src/libraries/System.Net.WebSockets.Client/tests/ConnectTest.cs
+++ src/libraries/System.Net.WebSockets.Client/tests/ConnectTest.cs
@@ -319,7 +319,7 @@ namespace System.Net.WebSockets.Client.Tests
         [ConditionalFact(nameof(WebSocketsSupported))]
         [ActiveIssue("https://github.com/dotnet/runtime/issues/34690", TestPlatforms.Windows, TargetFrameworkMonikers.Netcoreapp, TestRuntimes.Mono)]
         [SkipOnPlatform(TestPlatforms.Browser, "CollectHttpResponseDetails not supported on Browser")]
-        public async Task ConnectAsync_Failed()
+        public async Task ConnectAsync_HttpResponseDetailsCollectedOnFailure()
         {
             await LoopbackServer.CreateClientAndServerAsync(async uri =>
             {
