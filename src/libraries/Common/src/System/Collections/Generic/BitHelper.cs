186fb580680f8a8fb10aa07bed81b3c42916f4a1
Günther Foidl <gue@korporal.at>
2022-07-11 20:13:14 +0200

    Added Debug.Asserts to BitHelper's method that take bitPosition

--- src/libraries/Common/src/System/Collections/Generic/BitHelper.cs
+++ src/libraries/Common/src/System/Collections/Generic/BitHelper.cs
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics;
+
 namespace System.Collections.Generic
 {
     internal ref struct BitHelper
@@ -19,6 +21,8 @@ namespace System.Collections.Generic
 
         internal void MarkBit(int bitPosition)
         {
+            Debug.Assert(bitPosition >= 0);
+
             uint bitArrayIndex = (uint)bitPosition / IntSize;
             Span<int> span = _span;
             if (bitArrayIndex < (uint)span.Length)
@@ -29,6 +33,8 @@ namespace System.Collections.Generic
 
         internal bool IsMarked(int bitPosition)
         {
+            Debug.Assert(bitPosition >= 0);
+
             uint bitArrayIndex = (uint)bitPosition / IntSize;
             Span<int> span = _span;
             return
