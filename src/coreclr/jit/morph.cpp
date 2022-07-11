52d9d0caf8202053694ba4c23b7ac710cc62cbf7
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2022-07-11 19:30:19 +0200

    JIT: Fix reordering with 'backfilled' arguments on x86 (#71940)
    In a previous change I made the assumption that all register args come
    before stack args on x86. This assumption is not true.
    
    Fix #71939

--- src/coreclr/jit/morph.cpp
+++ src/coreclr/jit/morph.cpp
@@ -820,9 +820,8 @@ void CallArgs::ArgsComplete(Compiler* comp, GenTreeCall* call)
                 if (prevArg.AbiInfo.GetRegNum() == REG_STK)
                 {
                     // All stack args are already evaluated and placed in order
-                    // in this case; we only need to check this for register
-                    // args.
-                    break;
+                    // in this case.
+                    continue;
                 }
 #endif
 
@@ -902,7 +901,7 @@ void CallArgs::ArgsComplete(Compiler* comp, GenTreeCall* call)
                 {
                     // All stack args are already evaluated and placed in order
                     // in this case.
-                    break;
+                    continue;
                 }
 #endif
 
@@ -965,7 +964,7 @@ void CallArgs::ArgsComplete(Compiler* comp, GenTreeCall* call)
                         {
                             // All stack args are already evaluated and placed in order
                             // in this case.
-                            break;
+                            continue;
                         }
 #endif
                         // Invariant here is that all nodes that were not
