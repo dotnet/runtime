4965387ced48f228563dcd7ce709f983e1d016f8
Vitek Karas <10670590+vitek-karas@users.noreply.github.com>
2022-07-12 01:13:06 -0700

    Add a test for unsupported branch instruction crash (dotnet/linker#2887)
    * Add a test for unsupported branch instruction crash
    
    * Make the test pass for now
    
    Commit migrated from https://github.com/dotnet/linker/commit/33a76b856466b96dd3d179e599a7fc0118389641

---
+++ src/tools/illink/test/Mono.Linker.Tests.Cases/UnreachableBlock/ComplexConditionsOptimized.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Reflection.Emit;
+using System.Runtime.Serialization;
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
+using Mono.Linker.Tests.Cases.Expectations.Metadata;
+
+namespace Mono.Linker.Tests.Cases.UnreachableBlock
+{
+	[Reference ("System.Reflection.Emit.dll")]
+	[SetupCompileArgument ("/optimize+")]
+	[SetupLinkerArgument ("--enable-opt", "ipconstprop")]
+	public class ComplexConditionsOptimized
+	{
+		public static void Main ()
+		{
+			TestSwitch.Test ();
+		}
+
+		[Kept]
+		class TestSwitch
+		{
+			static int KnownInteger {
+				get => 2;
+			}
+
+			[Kept]
+			[ExpectBodyModified]
+			public static void Test ()
+			{
+				// https://github.com/dotnet/linker/issues/2872
+				// Uncomment the call to make the test crash the linker
+				//Helper ();
+				switch (KnownInteger) {
+				case 0:
+					Unreached ();
+					break;
+				case 1:
+					Unreached ();
+					break;
+				case 2:
+					Reached ();
+					break;
+				default: throw new ApplicationException ();
+				}
+			}
+
+			// https://github.com/dotnet/linker/issues/2872
+			// Should be removed
+			[Kept]
+			static void Unreached () { }
+
+			[Kept]
+			static void Reached () { }
+
+			// https://github.com/dotnet/linker/issues/2872
+			//[Kept]
+			static void Helper () { }
+		}
+	}
+}
