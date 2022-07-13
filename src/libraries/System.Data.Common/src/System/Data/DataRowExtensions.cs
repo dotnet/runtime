ce55f2b5e70fbcc19d5e8c28eb854b5f26c91287
Lakshan Fernando <lakshanf@microsoft.com>
2022-07-13 09:31:03 -0700

    Annotating librries with small number of warnings to be AOT safe

--- src/libraries/System.Data.Common/src/System/Data/DataRowExtensions.cs
+++ src/libraries/System.Data.Common/src/System/Data/DataRowExtensions.cs
@@ -143,6 +143,8 @@ namespace System.Data
         {
             internal static readonly Converter<object, T?> s_unbox = Create();
 
+            [UnconditionalSuppressMessage("AotAnalysis", "IL3050:RequiresDynamicCode",
+                        Justification = "The value type field would be available")]
             private static Converter<object, T?> Create()
             {
                 if (typeof(T).IsValueType)
