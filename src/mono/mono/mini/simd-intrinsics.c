e4a3294cd444f626173fda75e37df3c966a7c8fe
Egor Bogatov <egorbo@gmail.com>
2022-07-14 00:43:34 +0200

    Update simd-intrinsics.c

--- src/mono/mono/mini/simd-intrinsics.c
+++ src/mono/mono/mini/simd-intrinsics.c
@@ -1129,7 +1129,7 @@ emit_sri_vector (MonoCompile *cfg, MonoMethod *cmethod, MonoMethodSignature *fsi
 	}
 	case SN_Create: {
 		MonoType *etype = get_vector_t_elem_type (fsig->ret);
-		if (!is_element_type_primitive (fsig->params [0]))
+		if (!MONO_TYPE_IS_VECTOR_PRIMITIVE (etype))
 			return NULL;
 		if (fsig->param_count == 1 && mono_metadata_type_equal (fsig->params [0], etype))
 			return emit_simd_ins (cfg, klass, type_to_expand_op (etype), args [0]->dreg, -1);
