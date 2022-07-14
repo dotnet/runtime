0fef99aa08b51acc1b4d97f0c900828c089674fc
Radek Doulik <radekdoulik@gmail.com>
2022-07-14 13:17:07 +0200

    [wasm] Add interop code to the browser template

--- src/mono/wasm/templates/templates/browser/main.js
+++ src/mono/wasm/templates/templates/browser/main.js
@@ -1,5 +1,15 @@
 import { App } from './app-support.js'
 
 App.main = async function (applicationArguments) {
+    App.IMPORTS.window = {
+        location: {
+            href: () => globalThis.window.location.href
+        }
+    };
+
+    const exports = await App.MONO.mono_wasm_get_assembly_exports("browser.0.dll");
+    const text = exports[""].MyClass.Greeting();
+    document.getElementById("out").innerHTML = `${text}`;
+
     await App.MONO.mono_run_main("browser.0.dll", applicationArguments);
 }
