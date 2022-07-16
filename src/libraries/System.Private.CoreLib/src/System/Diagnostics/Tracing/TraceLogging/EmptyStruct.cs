b6932fd190e1c1159cee7fa6c48a1dded66e0fd7
Noah Falk <noahfalk@users.noreply.github.com>
2022-07-15 22:53:14 -0700

    Remove Microsoft.Diagnostics.Tracing.EventSource.Redist (#71573)
    This package was used historically to distribute EventSource fixes out-of-band but
    is no longer needed. This change removed the project and the ES_BUILD_STANDALONE
    ifdef which was only defined when the project was building.
    
    Fixes #71231

--- src/libraries/System.Private.CoreLib/src/System/Diagnostics/Tracing/TraceLogging/EmptyStruct.cs
+++ src/libraries/System.Private.CoreLib/src/System/Diagnostics/Tracing/TraceLogging/EmptyStruct.cs
@@ -1,11 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#if ES_BUILD_STANDALONE
-namespace Microsoft.Diagnostics.Tracing
-#else
 namespace System.Diagnostics.Tracing
-#endif
 {
     /// <summary>
     /// TraceLogging: Empty struct indicating no payload data.
