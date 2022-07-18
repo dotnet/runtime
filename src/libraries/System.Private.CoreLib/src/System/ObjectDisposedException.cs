ea252fd8b5942c22faeaecbb69d00ebbe9039b51
Stephen Toub <stoub@microsoft.com>
2022-07-18 17:56:49 -0400

    Add ObjectDisposedException.ThrowIf XML comments

--- src/libraries/System.Private.CoreLib/src/System/ObjectDisposedException.cs
+++ src/libraries/System.Private.CoreLib/src/System/ObjectDisposedException.cs
@@ -46,6 +46,10 @@ namespace System
             _objectName = info.GetString("ObjectName");
         }
 
+        /// <summary>Throws an <see cref="ObjectDisposedException"/> if the specified <paramref name="condition"/> is <see langword="true"/>.</summary>
+        /// <param name="condition">The condition to evaluate.</param>
+        /// <param name="instance">The object whose type's full name should be included in any resulting <see cref="ObjectDisposedException"/>.</param>
+        /// <exception cref="ObjectDisposedException">The <paramref name="condition"/> is true.</exception>
         [StackTraceHidden]
         public static void ThrowIf([DoesNotReturnIf(true)] bool condition, object instance)
         {
@@ -55,6 +59,10 @@ namespace System
             }
         }
 
+        /// <summary>Throws an <see cref="ObjectDisposedException"/> if the specified <paramref name="condition"/> is <see langword="true"/>.</summary>
+        /// <param name="condition">The condition to evaluate.</param>
+        /// <param name="type">The type whose full name should be included in any resulting <see cref="ObjectDisposedException"/>.</param>
+        /// <exception cref="ObjectDisposedException">The <paramref name="condition"/> is true.</exception>
         [StackTraceHidden]
         public static void ThrowIf([DoesNotReturnIf(true)] bool condition, Type type)
         {
