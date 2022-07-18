43c1881e457ed18464b7f5b8e5cbe5be657b2dd4
ManickaP <mapichov@microsoft.com>
2022-07-18 11:42:50 +0200

    Reverted CanRead/CanWrite; comments; tests for QuicStream task properties; fixed behavior of ResettableValueTaskSource

--- src/libraries/System.Net.Quic/src/System/Net/Quic/QuicStream.cs
+++ src/libraries/System.Net.Quic/src/System/Net/Quic/QuicStream.cs
@@ -327,6 +327,8 @@ public sealed partial class QuicStream
 
 
     /// <inheritdoc cref="WriteAsync(System.ReadOnlyMemory{byte},System.Threading.CancellationToken)"/>
+    /// <param name="buffer">The region of memory to write data from.</param>
+    /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is <see cref="CancellationToken.None"/>.</param>
     /// <param name="completeWrites">Notifies the peer about gracefully closing the write side, i.e.: sends FIN flag with the data.</param>
     public ValueTask WriteAsync(ReadOnlyMemory<byte> buffer, bool completeWrites, CancellationToken cancellationToken = default)
     {
