b90151521cbcc2265a1e6a4e7c15277325234819
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2022-07-18 17:28:47 +0200

    JIT: Avoid using type info from GDV guesses during import
    We can only use this type information after they have been expanded and
    if we are on the happy path.
    
    Fix #72363

--- src/coreclr/jit/gentree.cpp
+++ src/coreclr/jit/gentree.cpp
@@ -17496,10 +17496,12 @@ CORINFO_CLASS_HANDLE Compiler::gtGetClassHandle(GenTree* tree, bool* pIsExact, b
                     break;
                 }
             }
-            if (call->IsInlineCandidate())
+            if (call->IsInlineCandidate() && !call->IsGuardedDevirtualizationCandidate())
             {
                 // For inline candidates, we've already cached the return
-                // type class handle in the inline info.
+                // type class handle in the inline info (for GDV candidates,
+                // this data is valid only for a correct guess, so we cannot
+                // use it).
                 InlineCandidateInfo* inlInfo = call->gtInlineCandidateInfo;
                 assert(inlInfo != nullptr);
 
