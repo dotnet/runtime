42075040cb6a096fd8ba5416fd19878da586a8e9
Jeremy Barton <jbarton@microsoft.com>
2022-07-19 09:08:03 -0700

    Small test enhancements

--- src/libraries/System.Security.Cryptography.X509Certificates/tests/ExtensionsTests/SubjectAlternativeNameTests.cs
+++ src/libraries/System.Security.Cryptography.X509Certificates/tests/ExtensionsTests/SubjectAlternativeNameTests.cs
@@ -16,6 +16,7 @@ namespace System.Security.Cryptography.X509Certificates.Tests.ExtensionsTests
             Assert.Equal("2.5.29.17", ext.Oid.Value);
             Assert.Empty(ext.EnumerateDnsNames());
             Assert.Empty(ext.EnumerateIPAddresses());
+            Assert.False(ext.Critical, "ext.Critical");
         }
 
         [Fact]
@@ -106,6 +107,57 @@ namespace System.Security.Cryptography.X509Certificates.Tests.ExtensionsTests
         [InlineData(LoadMode.CopyFrom)]
         [InlineData(LoadMode.Array)]
         [InlineData(LoadMode.Span)]
+        public static void CopyFromAfterLoaded(LoadMode originalLoadMode)
+        {
+            SubjectAlternativeNameBuilder builder = new SubjectAlternativeNameBuilder();
+            builder.AddDnsName("foo");
+            builder.AddIpAddress(IPAddress.Loopback);
+            builder.AddUserPrincipalName("user@some.domain");
+            builder.AddIpAddress(IPAddress.IPv6Loopback);
+            builder.AddDnsName("*.foo");
+            X509Extension built = builder.Build(true);
+
+            X509SubjectAlternativeNameExtension ext;
+
+            switch (originalLoadMode)
+            {
+                case LoadMode.CopyFrom:
+                    ext = new X509SubjectAlternativeNameExtension();
+                    ext.CopyFrom(built);
+                    break;
+                case LoadMode.Array:
+                    ext = new X509SubjectAlternativeNameExtension(built.RawData, critical: true);
+                    break;
+                case LoadMode.Span:
+                    byte[] tmp = new byte[built.RawData.Length + 2];
+                    built.RawData.AsSpan().CopyTo(tmp.AsSpan(1));
+                    ext = new X509SubjectAlternativeNameExtension(tmp.AsSpan()[1..^1], critical: true);
+                    tmp.AsSpan().Clear();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(originalLoadMode), originalLoadMode, "Unexpected mode");
+            }
+
+            Assert.True(ext.Critical, "ext.Critical");
+            Assert.Equal(new[] { "foo", "*.foo" }, ext.EnumerateDnsNames());
+            Assert.Equal(new[] { IPAddress.Loopback, IPAddress.IPv6Loopback }, ext.EnumerateIPAddresses());
+
+            builder = new SubjectAlternativeNameBuilder();
+            builder.AddDnsName("a");
+            builder.AddDnsName("b");
+            builder.AddDnsName("c");
+            builder.AddIpAddress(IPAddress.IPv6Loopback);
+            ext.CopyFrom(builder.Build());
+
+            Assert.False(ext.Critical, "ext.Critical");
+            Assert.Equal(new[] { "a", "b", "c" }, ext.EnumerateDnsNames());
+            Assert.Equal(new[] { IPAddress.IPv6Loopback }, ext.EnumerateIPAddresses());
+        }
+
+        [Theory]
+        [InlineData(LoadMode.CopyFrom)]
+        [InlineData(LoadMode.Array)]
+        [InlineData(LoadMode.Span)]
         public static void VerifyDecodeFailureBehavior(LoadMode loadMode)
         {
             byte[] invalidEncoding = { 0x05, 0x00 };
@@ -121,7 +173,7 @@ namespace System.Security.Cryptography.X509Certificates.Tests.ExtensionsTests
         {
             // dNSName: foo
             // iPAddress: 127.0.0.1
-            // iPAddress: 7F 00 00 01 00 (127.0.0.1 with a trailing 0
+            // iPAddress: 7F 00 00 01 00 (127.0.0.1 with a trailing 0)
             // UPN: user@some.domain
             // iPAddress: ::1
             // dNSName: *.foo
@@ -149,7 +201,7 @@ namespace System.Security.Cryptography.X509Certificates.Tests.ExtensionsTests
             // dNSName: 86 6F 6F ("foo" with the f changed from 66 to 86)
             // iPAddress: ::1
             // dNSName: *.foo
-            byte[] invalidEncoding = new byte[]
+            byte[] invalidEncoding =
             {
                 0x30, 0x4B, 0x82, 0x03, 0x66, 0x6F, 0x6F, 0x87, 0x04, 0x7F, 0x00, 0x00, 0x01, 0xA0, 0x20, 0x06,
                 0x0A, 0x2B, 0x06, 0x01, 0x04, 0x01, 0x82, 0x37, 0x14, 0x02, 0x03, 0xA0, 0x12, 0x0C, 0x10, 0x75,
