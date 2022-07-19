348c2ebdefc93a8e0cc8f16e17e075694c000c7e
Stephen Toub <stoub@microsoft.com>
2022-07-18 20:37:01 -0400

    Apply suggestions from code review
    Co-authored-by: Carlos Sanchez <1175054+carlossanlop@users.noreply.github.com>

--- src/libraries/System.Private.CoreLib/src/System/ObjectDisposedException.cs
+++ src/libraries/System.Private.CoreLib/src/System/ObjectDisposedException.cs
@@ -49,7 +49,7 @@ namespace System
         /// <summary>Throws an <see cref="ObjectDisposedException"/> if the specified <paramref name="condition"/> is <see langword="true"/>.</summary>
         /// <param name="condition">The condition to evaluate.</param>
         /// <param name="instance">The object whose type's full name should be included in any resulting <see cref="ObjectDisposedException"/>.</param>
-        /// <exception cref="ObjectDisposedException">The <paramref name="condition"/> is true.</exception>
+        /// <exception cref="ObjectDisposedException">The <paramref name="condition"/> is <see langword="true"/>.</exception>
         [StackTraceHidden]
         public static void ThrowIf([DoesNotReturnIf(true)] bool condition, object instance)
         {
@@ -62,7 +62,7 @@ namespace System
         /// <summary>Throws an <see cref="ObjectDisposedException"/> if the specified <paramref name="condition"/> is <see langword="true"/>.</summary>
         /// <param name="condition">The condition to evaluate.</param>
         /// <param name="type">The type whose full name should be included in any resulting <see cref="ObjectDisposedException"/>.</param>
-        /// <exception cref="ObjectDisposedException">The <paramref name="condition"/> is true.</exception>
+        /// <exception cref="ObjectDisposedException">The <paramref name="condition"/> is <see langword="true"/>.</exception>
         [StackTraceHidden]
         public static void ThrowIf([DoesNotReturnIf(true)] bool condition, Type type)
         {
