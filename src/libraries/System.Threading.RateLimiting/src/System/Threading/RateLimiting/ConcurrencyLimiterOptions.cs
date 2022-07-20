faa57854df1c9dfd8cab03bc3b22f5f62576b05d
William Godbe <wigodbe@microsoft.com>
2022-07-20 11:27:21 -0700

    Update ConcurrencyLimiterOptions.cs

--- src/libraries/System.Threading.RateLimiting/src/System/Threading/RateLimiting/ConcurrencyLimiterOptions.cs
+++ src/libraries/System.Threading.RateLimiting/src/System/Threading/RateLimiting/ConcurrencyLimiterOptions.cs
@@ -10,7 +10,7 @@ namespace System.Threading.RateLimiting
     {
         /// <summary>
         /// Maximum number of permits that can be leased concurrently.
-        /// Must be set to a value >= 0 by the team these options are passed to the constructor of <see cref="ConcurrencyLimiter"/>.
+        /// Must be set to a value > 0 by the time these options are passed to the constructor of <see cref="ConcurrencyLimiter"/>.
         /// </summary>
         public int PermitLimit { get; set; }
 
@@ -24,7 +24,7 @@ namespace System.Threading.RateLimiting
 
         /// <summary>
         /// Maximum number of permits that can be queued concurrently.
-        /// Must be set to a value >= 0 by the team these options are passed to the constructor of <see cref="ConcurrencyLimiter"/>.
+        /// Must be set to a value > 0 by the time these options are passed to the constructor of <see cref="ConcurrencyLimiter"/>.
         /// </summary>
         public int QueueLimit { get; set; }
     }
