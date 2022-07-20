9502828b8c58054957b33db12b9dd3242d08e47f
Tomas Rylek <trylek@microsoft.com>
2022-07-20 13:50:25 +0200

    Fix native component build outside of Windows

--- src/tests/baseservices/CET/CETCheck.cpp
+++ src/tests/baseservices/CET/CETCheck.cpp
@@ -1,9 +1,9 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#if defined(TARGET_AMD64) && defined(TARGET_WINDOWS)
 #include <intrin.h>
 
-#if defined(TARGET_AMD64) && defined(TARGET_WINDOWS)
 extern "C" __declspec(dllexport) __int64 ReadShadowStackPointer()
 {
     return _rdsspq();
