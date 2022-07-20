af59dd8927ec4c0410bbab912a50624d92e1b634
William Godbe <wigodbe@microsoft.com>
2022-07-20 11:27:50 -0700

    Update FixedWindowRateLimiterOptions.cs

--- src/libraries/System.Threading.RateLimiting/src/System/Threading/RateLimiting/FixedWindowRateLimiterOptions.cs
+++ src/libraries/System.Threading.RateLimiting/src/System/Threading/RateLimiting/FixedWindowRateLimiterOptions.cs
@@ -10,7 +10,7 @@ namespace System.Threading.RateLimiting
     {
         /// <summary>
         /// Specifies the time window that takes in the requests.
-        /// Must be set to a nonzero value by the team these options are passed to the constructor of <see cref="FixedWindowRateLimiter"/>.
+        /// Must be set to a nonzero value by the time these options are passed to the constructor of <see cref="FixedWindowRateLimiter"/>.
         /// </summary>
         public TimeSpan Window { get; set; } = TimeSpan.Zero;
 
@@ -25,7 +25,7 @@ namespace System.Threading.RateLimiting
 
         /// <summary>
         /// Maximum number of permit counters that can be allowed in a window.
-        /// Must be set to a value >= 0 by the team these options are passed to the constructor of <see cref="FixedWindowRateLimiter"/>.
+        /// Must be set to a value > 0 by the time these options are passed to the constructor of <see cref="FixedWindowRateLimiter"/>.
         /// </summary>
         public int PermitLimit { get; set; }
 
@@ -39,7 +39,7 @@ namespace System.Threading.RateLimiting
 
         /// <summary>
         /// Maximum cumulative permit count of queued acquisition requests.
-        /// Must be set to a value >= 0 by the team these options are passed to the constructor of <see cref="FixedWindowRateLimiter"/>.
+        /// Must be set to a value > 0 by the time these options are passed to the constructor of <see cref="FixedWindowRateLimiter"/>.
         /// </summary>
         public int QueueLimit { get; set; }
     }
