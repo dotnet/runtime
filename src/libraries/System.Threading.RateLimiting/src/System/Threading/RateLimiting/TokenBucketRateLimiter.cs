76380eaa826c43d9515a76db88ba5cf230ec4c3e
William Godbe <wigodbe@microsoft.com>
2022-07-20 11:26:48 -0700

    Update TokenBucketRateLimiter.cs

--- src/libraries/System.Threading.RateLimiting/src/System/Threading/RateLimiting/TokenBucketRateLimiter.cs
+++ src/libraries/System.Threading.RateLimiting/src/System/Threading/RateLimiting/TokenBucketRateLimiter.cs
@@ -48,7 +48,7 @@ namespace System.Threading.RateLimiting
             _options = options ?? throw new ArgumentNullException(nameof(options));
             if (options.TokenLimit <= 0 || options.QueueLimit <= 0 || options.TokensPerPeriod <= 0 || options.ReplenishmentPeriod.Equals(TimeSpan.Zero))
             {
-                throw new ArgumentException($"{nameof(options.TokenLimit)}, {nameof(options.QueueLimit)}, {nameof(options.TokensPerPeriod)}, and {nameof(options.ReplenishmentPeriod)} must all be set to values >= 0.");
+                throw new ArgumentException($"{nameof(options.TokenLimit)}, {nameof(options.QueueLimit)}, {nameof(options.TokensPerPeriod)}, and {nameof(options.ReplenishmentPeriod)} must all be set to values greater than 0.");
             }
             _tokenCount = options.TokenLimit;
 
