73c2ce3a8698dc21f70c8f1bbdafe499566b9b0c
Stephen Toub <stoub@microsoft.com>
2022-07-20 11:18:48 -0400

    Enable cancellation for anonymous pipes and non-async named pipes on Windows (#72503)
    * Enable cancellation for anonymous pipes and non-async named pipes on Windows
    
    Although ReadAsync, WriteAsync, and WaitForConnectionAsync on pipes all accept a CancellationToken, that token is only usable on Windows for canceling an in-flight operation when the pipe is using overlapped I/O.  If the pipe was created for non-overlapped I/O, as is the case for anonymous pipes and can be the case for named pipes, the token stops being useful for anything other than an up-front cancellation check.
    
    This change fixes that by using CancelSynchronousIo to cancel the synchronous I/O performed as part of these async operations, which are implemented as async-over-sync (queueing to the thread pool a work item that performs the synchronous I/O).
    
    (The Unix implementation already supports cancellation in these situations.)
    
    * Address PR feedback (tweak comments)

--- src/libraries/Common/src/Interop/Windows/Kernel32/Interop.CancelSynchronousIo.cs
+++ src/libraries/Common/src/Interop/Windows/Kernel32/Interop.CancelSynchronousIo.cs
@@ -1,16 +1,17 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Microsoft.Win32.SafeHandles;
+using System;
 using System.Runtime.InteropServices;
+using System.Threading;
+using Microsoft.Win32.SafeHandles;
 
 internal static partial class Interop
 {
     internal static partial class Kernel32
     {
-        internal const int THREAD_TERMINATE = 0x0001;
-
         [LibraryImport(Libraries.Kernel32, SetLastError = true)]
-        internal static partial SafeThreadHandle OpenThread(int dwDesiredAccess, [MarshalAs(UnmanagedType.Bool)] bool bInheritHandle, int dwThreadId);
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static unsafe partial bool CancelSynchronousIo(SafeThreadHandle hThread);
     }
 }
