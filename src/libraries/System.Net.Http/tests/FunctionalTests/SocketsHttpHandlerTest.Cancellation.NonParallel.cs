1a20776c3cfa9025f89bc98ea53e4001711a871f
antonfirsov <antonfir@gmail.com>
2022-07-21 21:53:16 +0200

    delete sandbox logging code

--- src/libraries/System.Net.Http/tests/FunctionalTests/SocketsHttpHandlerTest.Cancellation.NonParallel.cs
+++ src/libraries/System.Net.Http/tests/FunctionalTests/SocketsHttpHandlerTest.Cancellation.NonParallel.cs
@@ -1,14 +1,10 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Diagnostics;
-using System.Diagnostics.Tracing;
 using System.IO;
 using System.Net.Sockets;
 using System.Net.Test.Common;
-using System.Text;
 using System.Threading;
-using System.Threading.Channels;
 using System.Threading.Tasks;
 using Microsoft.DotNet.RemoteExecutor;
 using Xunit;
@@ -43,7 +39,6 @@ namespace System.Net.Http.Functional.Tests
 
         private static async Task CancelPendingRequest_DropsStalledConnectionAttempt_Impl(string versionString, string firstConnectionDelayMsString, string requestTimeoutMsString)
         {
-            using var log = new LogHttpEventListener();
             var version = Version.Parse(versionString);
             LoopbackServerFactory factory = GetFactoryForVersion(version);
 
@@ -65,14 +60,10 @@ namespace System.Net.Http.Functional.Tests
                     await client.GetAsync(uri, cts0.Token);
                 });
 
-                await log.WriteLineAsync("!Initial done");
-
                 for (int i = 0; i < AttemptCount; i++)
                 {
                     using var cts1 = new CancellationTokenSource(requestTimeoutMs);
-                    await log.WriteLineAsync($"!Sending R{i}");
                     using var response = await client.GetAsync(uri, cts1.Token);
-                    await log.WriteLineAsync($"!R{i} done");
                     Assert.Equal(HttpStatusCode.OK, response.StatusCode);
                 }
             }, async server =>
@@ -144,119 +135,6 @@ namespace System.Net.Http.Functional.Tests
 
                 Assert.True(connected);
             }, timeout.ToString()).Dispose();
-        }
-    }
-
-    public sealed class LogHttpEventListener : EventListener
-    {
-        public const string LogDirectory = @"c:\_dev\r7d\src\libraries\System.Net.Http\tests\_logs";
-
-        private int _lastLogNumber = 0;
-        private FileStream _log;
-        private Channel<string> _messagesChannel = Channel.CreateUnbounded<string>();
-        private Task _processMessages;
-        private StringBuilder? _cachedBuilder = null;
-
-        private FileStream CreateNextLogFileStream()
-        {
-            string fn = Path.Combine(LogDirectory, $"client_{++_lastLogNumber:000}.log");
-            if (File.Exists(fn))
-            {
-                File.Delete(fn);
-            }
-            return new FileStream(fn, FileMode.CreateNew, FileAccess.Write);
-        }
-
-        public LogHttpEventListener()
-        {
-            if (!Directory.Exists(LogDirectory))
-            {
-                Directory.CreateDirectory(LogDirectory);
-            }
-
-            foreach (string filename in Directory.GetFiles(LogDirectory, "client*.log"))
-            {
-                try
-                {
-                    File.Delete(filename);
-                }
-                catch { }
-            }
-            _log = CreateNextLogFileStream();
-            _messagesChannel = Channel.CreateUnbounded<string>();
-            _processMessages = ProcessMessagesAsync();
-        }
-
-        protected override void OnEventSourceCreated(EventSource eventSource)
-        {
-            if (eventSource.Name == "Private.InternalDiagnostics.System.Net.Http" ||
-                eventSource.Name == "Private.InternalDiagnostics.System.Net.Quic")
-            {
-                EnableEvents(eventSource, EventLevel.LogAlways);
-            }
-        }
-
-        private async Task ProcessMessagesAsync()
-        {
-            byte[] buffer = new byte[8192];
-            var encoding = Encoding.ASCII;
-
-            int i = 0;
-            await foreach (string message in _messagesChannel.Reader.ReadAllAsync())
-            {
-                if ((++i % 10_000) == 0)
-                {
-                    await RotateFiles();
-                }
-                int maxLen = encoding.GetMaxByteCount(message.Length);
-                if (maxLen > buffer.Length)
-                {
-                    buffer = new byte[maxLen];
-                }
-                int byteCount = encoding.GetBytes(message, buffer);
-
-                await _log.WriteAsync(buffer.AsMemory(0, byteCount));
-            }
-
-            async ValueTask RotateFiles()
-            {
-                await _log.FlushAsync();
-                // Rotate the log if it reaches 50 MB size.
-                if (_log.Length > (100 << 20))
-                {
-                    await _log.DisposeAsync();
-                    _log = CreateNextLogFileStream();
-                }
-            }
-        }
-
-        protected override async void OnEventWritten(EventWrittenEventArgs
-            eventData)
-        {
-            StringBuilder sb = Interlocked.Exchange(ref _cachedBuilder, null) ?? new StringBuilder();
-            sb.Append($"{eventData.TimeStamp:HH:mm:ss.fffffff}[{eventData.EventName}] ");
-            for (int i = 0; i < eventData.Payload?.Count; i++)
-            {
-                if (i > 0)
-                {
-                    sb.Append(", ");
-                }
-                sb.Append(eventData.PayloadNames?[i]).Append(": ").Append(eventData.Payload[i]);
-            }
-            sb.Append(Environment.NewLine);
-            await _messagesChannel.Writer.WriteAsync(sb.ToString());
-            Interlocked.Exchange(ref _cachedBuilder, sb);
-        }
-
-        public ValueTask WriteLineAsync(string str) => _messagesChannel.Writer.WriteAsync(str);
-
-        public override void Dispose()
-        {
-            base.Dispose();
-            _log.Flush();
-            _messagesChannel.Writer.Complete();
-            _processMessages.Wait();
-            _log.Dispose();
         }
     }
 }
