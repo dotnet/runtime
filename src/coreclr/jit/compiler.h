90680365a09479cb1ad18014914c9e96bec0b353
SingleAccretion <62474226+SingleAccretion@users.noreply.github.com>
2022-07-23 20:59:35 +0300

    Consolidate importer spilling code (#72291)
    * Add tests
    
    * Fix losing GLOB_REF on the LHS
    
    The comment states we don't need it, which is incorrect.
    
    Diffs are improvements because we block forward substitution of
    calls into "ASG(BLK(ADDR(LCL_VAR<field>, ...)))", which allows
    morph to leave the "can be replaced with its field" local alone.
    
    * Prospective fix
    
    Spill "glob refs" on stores to "aliased" locals.
    
    * Delete now-not-necessary code
    
    * Fix up asserts
    
    * Clean out '(unsigned)CHECK_SPILL_ALL/NONE' casts
    
    * Don't manually spill for 'st[s]fld'
    
    * Revert 'Clean out '(unsigned)CHECK_SPILL_ALL/NONE' casts'

--- src/coreclr/jit/compiler.h
+++ src/coreclr/jit/compiler.h
@@ -3797,11 +3797,8 @@ protected:
     Statement* impLastStmt; // The last statement for the current BB.
 
 public:
-    enum
-    {
-        CHECK_SPILL_ALL  = -1,
-        CHECK_SPILL_NONE = -2
-    };
+    static const unsigned CHECK_SPILL_ALL  = static_cast<unsigned>(-1);
+    static const unsigned CHECK_SPILL_NONE = static_cast<unsigned>(-2);
 
     void impBeginTreeList();
     void impEndTreeList(BasicBlock* block, Statement* firstStmt, Statement* lastStmt);
@@ -4001,7 +3998,7 @@ private:
     void impSpillSpecialSideEff();
     void impSpillSideEffect(bool spillGlobEffects, unsigned chkLevel DEBUGARG(const char* reason));
     void impSpillSideEffects(bool spillGlobEffects, unsigned chkLevel DEBUGARG(const char* reason));
-    void impSpillLclRefs(unsigned lclNum);
+    void impSpillLclRefs(unsigned lclNum, unsigned chkLevel);
 
     BasicBlock* impPushCatchArgOnStack(BasicBlock* hndBlk, CORINFO_CLASS_HANDLE clsHnd, bool isSingleBlockFilter);
 
