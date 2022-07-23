5f57a0d983f35663e6b86bc4b7d2c9e5c0e001ec
Adeel <3840695+am11@users.noreply.github.com>
2022-07-23 11:46:29 +0300

    Revert a CFSTR

--- src/native/libs/System.Security.Cryptography.Native.Apple/pal_x509chain.c
+++ src/native/libs/System.Security.Cryptography.Native.Apple/pal_x509chain.c
@@ -173,7 +173,7 @@ static void MergeStatusCodes(CFTypeRef key, CFTypeRef value, void* context)
         *pStatus |= PAL_X509ChainInvalidNameConstraints;
     else if (CFEqual(keyString, CFSTR("PolicyConstraints")))
         *pStatus |= PAL_X509ChainInvalidPolicyConstraints;
-    else if (CFEqual(keyString, CFSTR("UnparsableExtension")))
+    else if (CFEqual(keyString, CFSTR("UnparseableExtension")))
     {
         // 10.15 introduced new status code value which is not reported by Windows. Ignoring for now.
     }
