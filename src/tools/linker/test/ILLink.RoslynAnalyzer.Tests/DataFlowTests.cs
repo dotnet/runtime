5e073ae5cef065ded925471c5c1e5d8ea813c384
Sven Boemer <sbomer@gmail.com>
2022-07-25 17:15:44 -0700

    Map type parameters for static closures (dotnet/linker#2899)
    This discovers type parameter mappings between generic type
    parameters and static closures which reference them. It detects
    the closure environment by scanning for stsfld instructions which
    reference generic closure types. There may be multiple methods
    associated with the same static closure, so this case doesn't
    produce warnings.
    
    This also fixes an unrelated issue where in Release mode, the
    compiler can generate struct closure environments that will never
    get instantiated via a ctor call. A new testcase runs the
    CompilerGeneratedTypes tests in Release mode to cover this.
    
    
    Commit migrated from https://github.com/dotnet/linker/commit/b883ec2c6469c26eef1bc913a769c8714b995ec8

--- src/tools/linker/test/ILLink.RoslynAnalyzer.Tests/DataFlowTests.cs
+++ src/tools/linker/test/ILLink.RoslynAnalyzer.Tests/DataFlowTests.cs
@@ -71,6 +71,12 @@ namespace ILLink.RoslynAnalyzer.Tests
 		}
 
 		[Fact]
+		public Task CompilerGeneratedTypesRelease ()
+		{
+			return RunTest ();
+		}
+
+		[Fact]
 		public Task ComplexTypeHandling ()
 		{
 			return RunTest ();
