73374e8ee580b86204a8aafa95e4b04e948baa24
Pavel Savara <pavel.savara@gmail.com>
2022-07-26 13:52:15 +0200

    [wasm] improve startup (#72275)
    - streaming wasm instantiation
    - more parallel download of DLLs
    - new hook `downloadResource` and `config.assets[].resolvedUrl `
    - improved `locateFile` allows to run dotnet with another working directory
    - call `init_crypto` in blazor startup sequence
    
    Co-authored-by: Marek Fišera <mara@neptuo.com>

--- src/mono/wasm/runtime/es6/dotnet.es6.pre.js
+++ src/mono/wasm/runtime/es6/dotnet.es6.pre.js
@@ -1,5 +1,7 @@
 const MONO = {}, BINDING = {}, INTERNAL = {}, IMPORTS = {}, EXPORTS = {};
 let ENVIRONMENT_IS_GLOBAL = false;
+var require = require || undefined;
+var __dirname = __dirname || '';
 if (typeof createDotnetRuntime === "function") {
     Module = { ready: Module.ready };
     const extension = createDotnetRuntime({ MONO, BINDING, INTERNAL, IMPORTS, EXPORTS, Module })
@@ -8,14 +10,14 @@ if (typeof createDotnetRuntime === "function") {
     }
     Object.assign(Module, extension);
     createDotnetRuntime = Module;
+    if (!createDotnetRuntime.locateFile) createDotnetRuntime.locateFile = createDotnetRuntime.__locateFile = (path) => scriptDirectory + path;
 }
 else if (typeof createDotnetRuntime === "object") {
     Module = { ready: Module.ready, __undefinedConfig: Object.keys(createDotnetRuntime).length === 1 };
     Object.assign(Module, createDotnetRuntime);
     createDotnetRuntime = Module;
+    if (!createDotnetRuntime.locateFile) createDotnetRuntime.locateFile = createDotnetRuntime.__locateFile = (path) => scriptDirectory + path;
 }
 else {
     throw new Error("MONO_WASM: Can't use moduleFactory callback of createDotnetRuntime function.")
 }
-var require = require || undefined;
-var __dirname = __dirname || '';
--- src/mono/wasm/runtime/polyfills.ts
+++ src/mono/wasm/runtime/polyfills.ts
@@ -1,23 +1,20 @@
+import Configuration from "consts:configuration";
 import MonoWasmThreads from "consts:monoWasmThreads";
-import { ENVIRONMENT_IS_ESM, ENVIRONMENT_IS_NODE, Module, requirePromise } from "./imports";
+import { ENVIRONMENT_IS_ESM, ENVIRONMENT_IS_NODE, ENVIRONMENT_IS_SHELL, ENVIRONMENT_IS_WEB, ENVIRONMENT_IS_WORKER, INTERNAL, Module, runtimeHelpers } from "./imports";
+import { afterUpdateGlobalBufferAndViews } from "./memory";
+import { afterLoadWasmModuleToWorker } from "./pthreads/browser";
+import { afterThreadInitTLS } from "./pthreads/worker";
+import { DotnetModuleConfigImports, EarlyReplacements } from "./types";
 
 let node_fs: any | undefined = undefined;
 let node_url: any | undefined = undefined;
 
-export async function init_polyfills(): Promise<void> {
+export function init_polyfills(replacements: EarlyReplacements): void {
+    const anyModule = Module as any;
+
     // performance.now() is used by emscripten and doesn't work in JSC
     if (typeof globalThis.performance === "undefined") {
-        if (ENVIRONMENT_IS_NODE && ENVIRONMENT_IS_ESM) {
-            const node_require = await requirePromise;
-            const { performance } = node_require("perf_hooks");
-            globalThis.performance = performance;
-        } else {
-            globalThis.performance = {
-                now: function () {
-                    return Date.now();
-                }
-            } as any;
-        }
+        globalThis.performance = dummyPerformance as any;
     }
     if (typeof globalThis.URL === "undefined") {
         globalThis.URL = class URL {
@@ -121,13 +118,104 @@ export async function init_polyfills(): Promise<void> {
             }
         };
     }
+
+    // require replacement
+    const imports = anyModule.imports = Module.imports || <DotnetModuleConfigImports>{};
+    const requireWrapper = (wrappedRequire: Function) => (name: string) => {
+        const resolved = (<any>Module.imports)[name];
+        if (resolved) {
+            return resolved;
+        }
+        return wrappedRequire(name);
+    };
+    if (imports.require) {
+        runtimeHelpers.requirePromise = replacements.requirePromise = Promise.resolve(requireWrapper(imports.require));
+    }
+    else if (replacements.require) {
+        runtimeHelpers.requirePromise = replacements.requirePromise = Promise.resolve(requireWrapper(replacements.require));
+    } else if (replacements.requirePromise) {
+        runtimeHelpers.requirePromise = replacements.requirePromise.then(require => requireWrapper(require));
+    } else {
+        runtimeHelpers.requirePromise = replacements.requirePromise = Promise.resolve(requireWrapper((name: string) => {
+            throw new Error(`Please provide Module.imports.${name} or Module.imports.require`);
+        }));
+    }
+
+    // script location
+    runtimeHelpers.scriptDirectory = replacements.scriptDirectory = detectScriptDirectory(replacements);
+    anyModule.mainScriptUrlOrBlob = replacements.scriptUrl;// this is needed by worker threads
+    if (Configuration === "Debug") {
+        console.debug(`MONO_WASM: starting script ${replacements.scriptUrl}`);
+        console.debug(`MONO_WASM: starting in ${runtimeHelpers.scriptDirectory}`);
+    }
+    if (anyModule.__locateFile === anyModule.locateFile) {
+        // above it's our early version from dotnet.es6.pre.js, we could replace it with better
+        anyModule.locateFile = runtimeHelpers.locateFile = (path) => {
+            if (isPathAbsolute(path)) return path;
+            return runtimeHelpers.scriptDirectory + path;
+        };
+    } else {
+        // we use what was given to us
+        runtimeHelpers.locateFile = anyModule.locateFile;
+    }
+
+    // fetch poly
+    if (imports.fetch) {
+        replacements.fetch = runtimeHelpers.fetch_like = imports.fetch;
+    }
+    else {
+        replacements.fetch = runtimeHelpers.fetch_like = fetch_like;
+    }
+
+    // misc
+    replacements.noExitRuntime = ENVIRONMENT_IS_WEB;
+
+    // threads
+    if (MonoWasmThreads) {
+        if (replacements.pthreadReplacements) {
+            const originalLoadWasmModuleToWorker = replacements.pthreadReplacements.loadWasmModuleToWorker;
+            replacements.pthreadReplacements.loadWasmModuleToWorker = (worker: Worker, onFinishedLoading: Function): void => {
+                originalLoadWasmModuleToWorker(worker, onFinishedLoading);
+                afterLoadWasmModuleToWorker(worker);
+            };
+            const originalThreadInitTLS = replacements.pthreadReplacements.threadInitTLS;
+            replacements.pthreadReplacements.threadInitTLS = (): void => {
+                originalThreadInitTLS();
+                afterThreadInitTLS();
+            };
+        }
+    }
+
+    // memory
+    const originalUpdateGlobalBufferAndViews = replacements.updateGlobalBufferAndViews;
+    replacements.updateGlobalBufferAndViews = (buffer: ArrayBufferLike) => {
+        originalUpdateGlobalBufferAndViews(buffer);
+        afterUpdateGlobalBufferAndViews(buffer);
+    };
 }
 
-export async function fetch_like(url: string): Promise<Response> {
+export async function init_polyfills_async(): Promise<void> {
+    if (ENVIRONMENT_IS_NODE && ENVIRONMENT_IS_ESM) {
+        // wait for locateFile setup on NodeJs
+        INTERNAL.require = await runtimeHelpers.requirePromise;
+        if (globalThis.performance === dummyPerformance) {
+            const { performance } = INTERNAL.require("perf_hooks");
+            globalThis.performance = performance;
+        }
+    }
+}
+
+const dummyPerformance = {
+    now: function () {
+        return Date.now();
+    }
+};
+
+export async function fetch_like(url: string, init?: RequestInit): Promise<Response> {
     try {
         if (ENVIRONMENT_IS_NODE) {
             if (!node_fs) {
-                const node_require = await requirePromise;
+                const node_require = await runtimeHelpers.requirePromise;
                 node_url = node_require("url");
                 node_fs = node_require("fs");
             }
@@ -144,7 +232,7 @@ export async function fetch_like(url: string): Promise<Response> {
             };
         }
         else if (typeof (globalThis.fetch) === "function") {
-            return globalThis.fetch(url, { credentials: "same-origin" });
+            return globalThis.fetch(url, init || { credentials: "same-origin" });
         }
         else if (typeof (read) === "function") {
             // note that it can't open files with unicode names, like Stra<unicode char - Latin Small Letter Sharp S>e.xml
@@ -169,10 +257,67 @@ export async function fetch_like(url: string): Promise<Response> {
     throw new Error("No fetch implementation available");
 }
 
-export function readAsync_like(url: string, onload: Function, onerror: Function): void {
-    fetch_like(url).then((res: Response) => {
-        onload(res.arrayBuffer());
-    }).catch((err) => {
-        onerror(err);
-    });
+function normalizeFileUrl(filename: string) {
+    // unix vs windows
+    // remove query string
+    return filename.replace(/\\/g, "/").replace(/[?#].*/, "");
+}
+
+function normalizeDirectoryUrl(dir: string) {
+    return dir.slice(0, dir.lastIndexOf("/")) + "/";
+}
+
+export function detectScriptDirectory(replacements: EarlyReplacements): string {
+    if (ENVIRONMENT_IS_WORKER) {
+        // Check worker, not web, since window could be polyfilled
+        replacements.scriptUrl = self.location.href;
+    }
+    // when ENVIRONMENT_IS_ESM we have scriptUrl from import.meta.url from dotnet.es6.lib.js
+    if (!ENVIRONMENT_IS_ESM) {
+        if (ENVIRONMENT_IS_WEB) {
+            if (
+                (typeof (globalThis.document) === "object") &&
+                (typeof (globalThis.document.createElement) === "function")
+            ) {
+                // blazor injects a module preload link element for dotnet.[version].[sha].js
+                const blazorDotNetJS = Array.from(document.head.getElementsByTagName("link")).filter(elt => elt.rel !== undefined && elt.rel == "modulepreload" && elt.href !== undefined && elt.href.indexOf("dotnet") != -1 && elt.href.indexOf(".js") != -1);
+                if (blazorDotNetJS.length == 1) {
+                    replacements.scriptUrl = blazorDotNetJS[0].href;
+                } else {
+                    const temp = globalThis.document.createElement("a");
+                    temp.href = "dotnet.js";
+                    replacements.scriptUrl = temp.href;
+                }
+            }
+        }
+        if (ENVIRONMENT_IS_NODE) {
+            if (typeof __filename !== "undefined") {
+                // unix vs windows
+                replacements.scriptUrl = __filename;
+            }
+        }
+    }
+    if (!replacements.scriptUrl) {
+        // probably V8 shell in non ES6
+        replacements.scriptUrl = "./dotnet.js";
+    }
+    replacements.scriptUrl = normalizeFileUrl(replacements.scriptUrl);
+    return normalizeDirectoryUrl(replacements.scriptUrl);
+}
+
+const protocolRx = /^[a-zA-Z][a-zA-Z\d+\-.]*?:\/\//;
+const windowsAbsoluteRx = /[a-zA-Z]:[\\/]/;
+function isPathAbsolute(path: string): boolean {
+    if (ENVIRONMENT_IS_NODE || ENVIRONMENT_IS_SHELL) {
+        // unix /x.json
+        // windows \x.json
+        // windows C:\x.json
+        // windows C:/x.json
+        return path.startsWith("/") || path.startsWith("\\") || path.indexOf("///") !== -1 || windowsAbsoluteRx.test(path);
+    }
+
+    // anything with protocol is always absolute
+    // windows file:///C:/x.json
+    // windows http://C:/x.json
+    return protocolRx.test(path);
 }
