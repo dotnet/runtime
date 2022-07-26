8cc644bfb5dec222d132359623d561aa75d5aa87
Stephen Toub <stoub@microsoft.com>
2022-07-26 10:52:08 -0400

    Change RegexGenerator to produce limited implementation for C# < 11
    Today the RegexGenerator fails to produce any code for GeneratedRegex if the LangVersion is 10 or lower.  However, that restriction only exists because the code generated for a regex implementation contains use of language features only available in C# 11 and newer.  Rather than failing outright for 10 or lower, we can just fall back to the limited support implementation that just caches an instance of the regex.  That still provides some value and makes it easier to transition between language versions.

--- src/libraries/System.Text.RegularExpressions/gen/DiagnosticDescriptors.cs
+++ src/libraries/System.Text.RegularExpressions/gen/DiagnosticDescriptors.cs
@@ -46,17 +46,8 @@ namespace System.Text.RegularExpressions.Generator
             isEnabledByDefault: true,
             customTags: WellKnownDiagnosticTags.NotConfigurable);
 
-        public static DiagnosticDescriptor InvalidLangVersion { get; } = new DiagnosticDescriptor(
-            id: "SYSLIB1044",
-            title: new LocalizableResourceString(nameof(SR.InvalidRegexGeneratorAttributeTitle), SR.ResourceManager, typeof(FxResources.System.Text.RegularExpressions.Generator.SR)),
-            messageFormat: new LocalizableResourceString(nameof(SR.InvalidLangVersionMessage), SR.ResourceManager, typeof(FxResources.System.Text.RegularExpressions.Generator.SR)),
-            category: Category,
-            DiagnosticSeverity.Error,
-            isEnabledByDefault: true,
-            customTags: WellKnownDiagnosticTags.NotConfigurable);
-
         public static DiagnosticDescriptor LimitedSourceGeneration { get; } = new DiagnosticDescriptor(
-            id: "SYSLIB1045",
+            id: "SYSLIB1044",
             title: new LocalizableResourceString(nameof(SR.LimitedSourceGenerationTitle), SR.ResourceManager, typeof(FxResources.System.Text.RegularExpressions.Generator.SR)),
             messageFormat: new LocalizableResourceString(nameof(SR.LimitedSourceGenerationMessage), SR.ResourceManager, typeof(FxResources.System.Text.RegularExpressions.Generator.SR)),
             category: Category,
@@ -64,7 +55,7 @@ namespace System.Text.RegularExpressions.Generator
             isEnabledByDefault: true);
 
         public static DiagnosticDescriptor UseRegexSourceGeneration { get; } = new DiagnosticDescriptor(
-            id: "SYSLIB1046",
+            id: "SYSLIB1045",
             title: new LocalizableResourceString(nameof(SR.UseRegexSourceGeneratorTitle), SR.ResourceManager, typeof(FxResources.System.Text.RegularExpressions.Generator.SR)),
             messageFormat: new LocalizableResourceString(nameof(SR.UseRegexSourceGeneratorMessage), SR.ResourceManager, typeof(FxResources.System.Text.RegularExpressions.Generator.SR)),
             category: Category,
