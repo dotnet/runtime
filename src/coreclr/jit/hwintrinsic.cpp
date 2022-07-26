17feeffe24c6f7edb006c6573a3c5e5738bb9e4c
Kunal Pathak <Kunal.Pathak@microsoft.com>
2022-07-26 16:23:51 -0700

    do not embed result type for scalar hw intrinsics

--- src/coreclr/jit/hwintrinsic.cpp
+++ src/coreclr/jit/hwintrinsic.cpp
@@ -210,10 +210,17 @@ CORINFO_CLASS_HANDLE Compiler::gtGetStructHandleForHWSIMD(var_types simdType, Co
 //
 /* static */ bool Compiler::vnEncodesResultTypeForHWIntrinsic(NamedIntrinsic hwIntrinsicID)
 {
+    // No extra type information is needed for scalar HW Intrinsic.
+    //
+    if (HWIntrinsicInfo::lookupCategory(hwIntrinsicID) == HW_Category_Scalar)
+    {
+        return false;
+    }
+
     int numArgs = HWIntrinsicInfo::lookupNumArgs(hwIntrinsicID);
 
     // HW Intrinsic's with -1 for numArgs have a varying number of args, so we currently
-    // give themm a unique value number them, and don't add an extra argument.
+    // give them a unique value number, and don't add an extra argument.
     //
     if (numArgs == -1)
     {
