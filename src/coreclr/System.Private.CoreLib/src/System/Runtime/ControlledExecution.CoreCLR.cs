b4716dc7296c6aedb132b52069f8aebf0e55cdef
Anton Lapounov <antonl@microsoft.com>
2022-07-27 14:30:30 -0700

    Update comments

--- src/coreclr/System.Private.CoreLib/src/System/Runtime/ControlledExecution.CoreCLR.cs
+++ src/coreclr/System.Private.CoreLib/src/System/Runtime/ControlledExecution.CoreCLR.cs
@@ -21,15 +21,23 @@ namespace System.Runtime
         /// </summary>
         /// <param name="action">The delegate that represents the code to execute.</param>
         /// <param name="cancellationToken">The cancellation token that may be used to abort execution.</param>
+        /// <exception cref="System.PlatformNotSupportedException">The method is not supported on this platform.</exception>
         /// <exception cref="System.ArgumentNullException">The <paramref name="action"/> argument is null.</exception>
         /// <exception cref="System.InvalidOperationException">
         /// The current thread is already running the <see cref="ControlledExecution.Run"/> method.
         /// </exception>
         /// <exception cref="System.OperationCanceledException">The execution was aborted.</exception>
         /// <remarks>
-        /// <see cref="ControlledExecution"/> enables aborting arbitrary code in a non-cooperative manner.
-        /// Doing so may corrupt the process.  This method is not recommended for use in production code
-        /// in which reliability is important.
+        /// <para>This method enables aborting arbitrary managed code in a non-cooperative manner by throwing an exception
+        /// in the thread executing that code.  While the exception may be caught by the code, it is re-thrown at the end
+        /// of `catch` blocks until the execution flow returns to the `ControlledExecution.Run` method.</para>
+        /// <para>Execution of the code is not guaranteed to abort immediately, or at all.  This situation can occur, for
+        /// example, if a thread is stuck executing unmanaged code or the `catch` and `finally` blocks that are called as
+        /// part of the abort procedure, thereby indefinitely delaying the abort.  Furthermore, execution may not be
+        /// aborted immediately if the thread is currently executing a `catch` or `finally` block.</para>
+        /// <para>Aborting code at an unexpected location may corrupt the state of data structures in the process and lead
+        /// to unpredictable results.  For that reason, this method should not be used in production code and calling it
+        /// produces a compile-time warning.</para>
         /// </remarks>
         [Obsolete(Obsoletions.ControlledExecutionRunMessage, DiagnosticId = Obsoletions.ControlledExecutionRunDiagId, UrlFormat = Obsoletions.SharedUrlFormat)]
         public static void Run(Action action, CancellationToken cancellationToken)
