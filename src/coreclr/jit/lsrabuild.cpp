e08f373acb1c551607880ce5f8dc0a2bbd0c9b3b
SingleAccretion <AccretionMail@yandex.ru>
2022-07-27 18:39:10 +0300

    Avoid regressions from RA copies
    Lowering will now retype non-reg-candidate LCL_VAR nodes to LCL_FLD
    ones in more circumstances, and this pessimizes LSRA a bit.
    
    Apply the same fix as used by the LCL_VAR case. This turns out to
    be a good CQ improvement on its own as well.

--- src/coreclr/jit/lsrabuild.cpp
+++ src/coreclr/jit/lsrabuild.cpp
@@ -3644,13 +3644,7 @@ int LinearScan::BuildReturn(GenTree* tree)
 #ifdef TARGET_ARM64
         if (varTypeIsSIMD(tree) && !op1->IsMultiRegLclVar())
         {
-            useCandidates = allSIMDRegs();
-            if (op1->OperGet() == GT_LCL_VAR)
-            {
-                assert(op1->TypeGet() != TYP_SIMD32);
-                useCandidates = RBM_DOUBLERET;
-            }
-            BuildUse(op1, useCandidates);
+            BuildUse(op1, RBM_DOUBLERET);
             return 1;
         }
 #endif // TARGET_ARM64
