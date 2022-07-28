868993dfa8a19ff2e09761e3390779c3229fad3d
Kunal Pathak <Kunal.Pathak@microsoft.com>
2022-07-28 16:46:14 -0700

    Minor comment removal

--- src/coreclr/jit/assertionprop.cpp
+++ src/coreclr/jit/assertionprop.cpp
@@ -2009,8 +2009,7 @@ AssertionIndex Compiler::optAddAssertion(AssertionDsc* newAssertion)
 #ifdef DEBUG
             if (found)
             {
-                JITDUMP("HashCode= %u not found in map.\n",
-                        AssertionDscKeyFuncs /*<true>*/ ::GetHashCode(*newAssertion));
+                JITDUMP("HashCode= %u not found in map.\n", AssertionDscKeyFuncs::GetHashCode(*newAssertion));
                 assert(false);
             }
 #endif
@@ -2032,7 +2031,7 @@ AssertionIndex Compiler::optAddAssertion(AssertionDsc* newAssertion)
         if (found)
         {
             JITDUMP("HashCode=%u was not found in map and we added it.\n",
-                    AssertionDscKeyFuncs /*<true>*/ ::GetHashCode(*newAssertion));
+                    AssertionDscKeyFuncs::GetHashCode(*newAssertion));
             assert(false);
         }
     }
