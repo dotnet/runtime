b5927eeb458f0c9aa9689a158ed0917b59a482ec
Tlakollo <tlcejava@microsoft.com>
2022-08-01 15:30:16 -0700

    Fix conflict

--- src/coreclr/tools/Common/Compiler/EventPseudoDesc.cs
+++ src/coreclr/tools/Common/Compiler/EventPseudoDesc.cs
@@ -47,7 +47,6 @@ namespace ILCompiler
             }
         }
 
-<<<<<<< HEAD:src/coreclr/tools/Common/Compiler/EventPseudoDesc.cs
         public CustomAttributeHandleCollection GetCustomAttributes
         {
             get
@@ -56,8 +55,6 @@ namespace ILCompiler
             }
         }
 
-=======
->>>>>>> e6d5bba17d727b8a2d196186f2e181d9bf73578d:src/coreclr/tools/aot/ILCompiler.Compiler/Compiler/EventPseudoDesc.cs
         public MetadataType OwningType
         {
             get
