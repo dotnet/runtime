6b2492e424824c76673dae4858e28bb2ab3871a3
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2022-08-01 21:02:47 +0100

    address feedback

--- src/libraries/System.Text.Json/ref/System.Text.Json.cs
+++ src/libraries/System.Text.Json/ref/System.Text.Json.cs
@@ -978,7 +978,7 @@ namespace System.Text.Json.Serialization
         public System.Text.Json.Serialization.JsonKnownNamingPolicy PropertyNamingPolicy { get { throw null; } set { } }
         public bool WriteIndented { get { throw null; } set { } }
     }
-    [System.Diagnostics.CodeAnalysis.RequiresDynamicCodeAttribute("JsonStringEnumConverter cannot be statically analyzed and requires runtime code generation. There is no built-in solution compatible with native AOT applications. Consider authoring a custom converter that is not a factory to work around the issue. See https://github.com/dotnet/runtime/issues/73124.")]
+    [System.Diagnostics.CodeAnalysis.RequiresDynamicCodeAttribute("JsonStringEnumConverter cannot be statically analyzed and requires runtime code generation. Consider authoring a custom converter that is not a factory to work around the issue. See https://github.com/dotnet/runtime/issues/73124.")]
     public partial class JsonStringEnumConverter : System.Text.Json.Serialization.JsonConverterFactory
     {
         public JsonStringEnumConverter() { }
--- src/libraries/System.Text.Json/src/System/Text/Json/Serialization/JsonStringEnumConverter.cs
+++ src/libraries/System.Text.Json/src/System/Text/Json/Serialization/JsonStringEnumConverter.cs
@@ -14,9 +14,8 @@ namespace System.Text.Json.Serialization
     /// </remarks>
     [RequiresDynamicCode(
         "JsonStringEnumConverter cannot be statically analyzed and requires runtime code generation. " +
-        "There is no built-in solution compatible with native AOT applications. " +
         "Consider authoring a custom converter that is not a factory to work around the issue. " +
-        "See https://github.com/dotnet/runtime/issues/73124." )]
+        "See https://github.com/dotnet/runtime/issues/73124.")]
     public class JsonStringEnumConverter : JsonConverterFactory
     {
         private readonly JsonNamingPolicy? _namingPolicy;
