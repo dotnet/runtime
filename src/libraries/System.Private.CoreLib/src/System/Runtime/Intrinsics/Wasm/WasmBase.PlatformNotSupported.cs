b95f3ac5b4e75710363ab12d6a0dc2213f10c32e
Radek Doulik <radekdoulik@gmail.com>
2022-08-03 11:59:51 +0200

    Add more methods to WasmBase classes

--- src/libraries/System.Private.CoreLib/src/System/Runtime/Intrinsics/Wasm/WasmBase.PlatformNotSupported.cs
+++ src/libraries/System.Private.CoreLib/src/System/Runtime/Intrinsics/Wasm/WasmBase.PlatformNotSupported.cs
@@ -44,5 +44,54 @@ namespace System.Runtime.Intrinsics.Wasm
         public static Vector128<sbyte> Swizzle(Vector128<sbyte> a, Vector128<sbyte> s) { throw new PlatformNotSupportedException(); }
         public static Vector128<byte>  Swizzle(Vector128<byte>  a, Vector128<byte>  s) { throw new PlatformNotSupportedException(); }
 
+        public static Vector128<sbyte>  And(Vector128<sbyte> left, Vector128<sbyte> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<byte>   And(Vector128<byte> left, Vector128<byte> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<short>  And(Vector128<short> left, Vector128<short> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<ushort> And(Vector128<ushort> left, Vector128<ushort> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<int>    And(Vector128<int> left, Vector128<int> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<uint>   And(Vector128<uint> left, Vector128<uint> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<long>   And(Vector128<long> left, Vector128<long> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<ulong>  And(Vector128<ulong> left, Vector128<ulong> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<float>  And(Vector128<float> left, Vector128<float> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<double> And(Vector128<double> left, Vector128<double> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<nint>   And(Vector128<nint> left, Vector128<nint> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<nuint>  And(Vector128<nuint> left, Vector128<nuint> right) { throw new PlatformNotSupportedException(); }
+
+        public static int Bitmask(Vector128<sbyte> a) { throw new PlatformNotSupportedException(); }
+        public static int Bitmask(Vector128<byte> a) { throw new PlatformNotSupportedException(); }
+        public static int Bitmask(Vector128<short> a) { throw new PlatformNotSupportedException(); }
+        public static int Bitmask(Vector128<ushort> a) { throw new PlatformNotSupportedException(); }
+        public static int Bitmask(Vector128<int> a) { throw new PlatformNotSupportedException(); }
+        public static int Bitmask(Vector128<uint> a) { throw new PlatformNotSupportedException(); }
+        public static int Bitmask(Vector128<long> a) { throw new PlatformNotSupportedException(); }
+        public static int Bitmask(Vector128<ulong> a) { throw new PlatformNotSupportedException(); }
+        public static int Bitmask(Vector128<nint> a) { throw new PlatformNotSupportedException(); }
+        public static int Bitmask(Vector128<nuint> a) { throw new PlatformNotSupportedException(); }
+
+        public static Vector128<sbyte>  CompareEqual(Vector128<sbyte> left, Vector128<sbyte> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<byte>   CompareEqual(Vector128<byte> left, Vector128<byte> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<short>  CompareEqual(Vector128<short> left, Vector128<short> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<ushort> CompareEqual(Vector128<ushort> left, Vector128<ushort> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<int>    CompareEqual(Vector128<int> left, Vector128<int> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<uint>   CompareEqual(Vector128<uint> left, Vector128<uint> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<long>   CompareEqual(Vector128<long> left, Vector128<long> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<ulong>  CompareEqual(Vector128<ulong> left, Vector128<ulong> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<float>  CompareEqual(Vector128<float> left, Vector128<float> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<double> CompareEqual(Vector128<double> left, Vector128<double> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<nint>   CompareEqual(Vector128<nint> left, Vector128<nint> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<nuint>  CompareEqual(Vector128<nuint> left, Vector128<nuint> right) { throw new PlatformNotSupportedException(); }
+
+        public static Vector128<sbyte>  CompareNotEqual(Vector128<sbyte> left, Vector128<sbyte> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<byte>   CompareNotEqual(Vector128<byte> left, Vector128<byte> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<short>  CompareNotEqual(Vector128<short> left, Vector128<short> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<ushort> CompareNotEqual(Vector128<ushort> left, Vector128<ushort> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<int>    CompareNotEqual(Vector128<int> left, Vector128<int> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<uint>   CompareNotEqual(Vector128<uint> left, Vector128<uint> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<long>   CompareNotEqual(Vector128<long> left, Vector128<long> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<ulong>  CompareNotEqual(Vector128<ulong> left, Vector128<ulong> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<float>  CompareNotEqual(Vector128<float> left, Vector128<float> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<double> CompareNotEqual(Vector128<double> left, Vector128<double> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<nint>   CompareNotEqual(Vector128<nint> left, Vector128<nint> right) { throw new PlatformNotSupportedException(); }
+        public static Vector128<nuint>  CompareNotEqual(Vector128<nuint> left, Vector128<nuint> right) { throw new PlatformNotSupportedException(); }
     }
 }
--- src/libraries/System.Private.CoreLib/src/System/Runtime/Intrinsics/Wasm/WasmBase.cs
+++ src/libraries/System.Private.CoreLib/src/System/Runtime/Intrinsics/Wasm/WasmBase.cs
@@ -9,9 +9,7 @@ namespace System.Runtime.Intrinsics.Wasm
     [Intrinsic]
     internal abstract class WasmBase
     {
-        public static bool IsSupported { get => IsSupported; }
-
-        // Constructing SIMD Values
+        public static bool IsSupported { [Intrinsic] get { return false; } }
 
         [Intrinsic]
         public static Vector128<byte> Constant(Vector128<byte> imm) => Constant(imm);
@@ -102,28 +100,99 @@ namespace System.Runtime.Intrinsics.Wasm
         public static Vector128<byte>  Swizzle(Vector128<byte>  a, Vector128<byte>  s) => Swizzle(a, s);
 
         [Intrinsic]
-        public static Vector128<sbyte> And(Vector128<sbyte> left, Vector128<sbyte> right) => And(left, right);
+        public static Vector128<sbyte>  And(Vector128<sbyte> left, Vector128<sbyte> right) => And(left, right);
         [Intrinsic]
-        public static Vector128<byte> And(Vector128<byte> left, Vector128<byte> right) => And(left, right);
+        public static Vector128<byte>   And(Vector128<byte> left, Vector128<byte> right) => And(left, right);
         [Intrinsic]
-        public static Vector128<short> And(Vector128<short> left, Vector128<short> right) => And(left, right);
+        public static Vector128<short>  And(Vector128<short> left, Vector128<short> right) => And(left, right);
         [Intrinsic]
         public static Vector128<ushort> And(Vector128<ushort> left, Vector128<ushort> right) => And(left, right);
         [Intrinsic]
-        public static Vector128<int> And(Vector128<int> left, Vector128<int> right) => And(left, right);
+        public static Vector128<int>    And(Vector128<int> left, Vector128<int> right) => And(left, right);
         [Intrinsic]
-        public static Vector128<uint> And(Vector128<uint> left, Vector128<uint> right) => And(left, right);
+        public static Vector128<uint>   And(Vector128<uint> left, Vector128<uint> right) => And(left, right);
         [Intrinsic]
-        public static Vector128<long> And(Vector128<long> left, Vector128<long> right) => And(left, right);
+        public static Vector128<long>   And(Vector128<long> left, Vector128<long> right) => And(left, right);
         [Intrinsic]
-        public static Vector128<ulong> And(Vector128<ulong> left, Vector128<ulong> right) => And(left, right);
+        public static Vector128<ulong>  And(Vector128<ulong> left, Vector128<ulong> right) => And(left, right);
         [Intrinsic]
-        public static Vector128<float> And(Vector128<float> left, Vector128<float> right) => And(left, right);
+        public static Vector128<float>  And(Vector128<float> left, Vector128<float> right) => And(left, right);
         [Intrinsic]
         public static Vector128<double> And(Vector128<double> left, Vector128<double> right) => And(left, right);
         [Intrinsic]
-        public static Vector128<nint> And(Vector128<nint> left, Vector128<nint> right) => And(left, right);
+        public static Vector128<nint>   And(Vector128<nint> left, Vector128<nint> right) => And(left, right);
         [Intrinsic]
-        public static Vector128<nuint> And(Vector128<nuint> left, Vector128<nuint> right) => And(left, right);
+        public static Vector128<nuint>  And(Vector128<nuint> left, Vector128<nuint> right) => And(left, right);
+
+        [Intrinsic]
+        public static int Bitmask(Vector128<sbyte> a) => Bitmask(a);
+        [Intrinsic]
+        public static int Bitmask(Vector128<byte> a) => Bitmask(a);
+        [Intrinsic]
+        public static int Bitmask(Vector128<short> a) => Bitmask(a);
+        [Intrinsic]
+        public static int Bitmask(Vector128<ushort> a) => Bitmask(a);
+        [Intrinsic]
+        public static int Bitmask(Vector128<int> a) => Bitmask(a);
+        [Intrinsic]
+        public static int Bitmask(Vector128<uint> a) => Bitmask(a);
+        [Intrinsic]
+        public static int Bitmask(Vector128<long> a) => Bitmask(a);
+        [Intrinsic]
+        public static int Bitmask(Vector128<ulong> a) => Bitmask(a);
+        [Intrinsic]
+        public static int Bitmask(Vector128<nint> a) => Bitmask(a);
+        [Intrinsic]
+        public static int Bitmask(Vector128<nuint> a) => Bitmask(a);
+
+        [Intrinsic]
+        public static Vector128<sbyte>  CompareEqual(Vector128<sbyte> left, Vector128<sbyte> right) => CompareEqual(left, right);
+        [Intrinsic]
+        public static Vector128<byte>   CompareEqual(Vector128<byte> left, Vector128<byte> right) => CompareEqual(left, right);
+        [Intrinsic]
+        public static Vector128<short>  CompareEqual(Vector128<short> left, Vector128<short> right) => CompareEqual(left, right);
+        [Intrinsic]
+        public static Vector128<ushort> CompareEqual(Vector128<ushort> left, Vector128<ushort> right) => CompareEqual(left, right);
+        [Intrinsic]
+        public static Vector128<int>    CompareEqual(Vector128<int> left, Vector128<int> right) => CompareEqual(left, right);
+        [Intrinsic]
+        public static Vector128<uint>   CompareEqual(Vector128<uint> left, Vector128<uint> right) => CompareEqual(left, right);
+        [Intrinsic]
+        public static Vector128<long>   CompareEqual(Vector128<long> left, Vector128<long> right) => CompareEqual(left, right);
+        [Intrinsic]
+        public static Vector128<ulong>  CompareEqual(Vector128<ulong> left, Vector128<ulong> right) => CompareEqual(left, right);
+        [Intrinsic]
+        public static Vector128<float>  CompareEqual(Vector128<float> left, Vector128<float> right) => CompareEqual(left, right);
+        [Intrinsic]
+        public static Vector128<double> CompareEqual(Vector128<double> left, Vector128<double> right) => CompareEqual(left, right);
+        [Intrinsic]
+        public static Vector128<nint>   CompareEqual(Vector128<nint> left, Vector128<nint> right) => CompareEqual(left, right);
+        [Intrinsic]
+        public static Vector128<nuint>  CompareEqual(Vector128<nuint> left, Vector128<nuint> right) => CompareEqual(left, right);
+
+        [Intrinsic]
+        public static Vector128<sbyte>  CompareNotEqual(Vector128<sbyte> left, Vector128<sbyte> right) => CompareNotEqual(left, right);
+        [Intrinsic]
+        public static Vector128<byte>   CompareNotEqual(Vector128<byte> left, Vector128<byte> right) => CompareNotEqual(left, right);
+        [Intrinsic]
+        public static Vector128<short>  CompareNotEqual(Vector128<short> left, Vector128<short> right) => CompareNotEqual(left, right);
+        [Intrinsic]
+        public static Vector128<ushort> CompareNotEqual(Vector128<ushort> left, Vector128<ushort> right) => CompareNotEqual(left, right);
+        [Intrinsic]
+        public static Vector128<int>    CompareNotEqual(Vector128<int> left, Vector128<int> right) => CompareNotEqual(left, right);
+        [Intrinsic]
+        public static Vector128<uint>   CompareNotEqual(Vector128<uint> left, Vector128<uint> right) => CompareNotEqual(left, right);
+        [Intrinsic]
+        public static Vector128<long>   CompareNotEqual(Vector128<long> left, Vector128<long> right) => CompareNotEqual(left, right);
+        [Intrinsic]
+        public static Vector128<ulong>  CompareNotEqual(Vector128<ulong> left, Vector128<ulong> right) => CompareNotEqual(left, right);
+        [Intrinsic]
+        public static Vector128<float>  CompareNotEqual(Vector128<float> left, Vector128<float> right) => CompareNotEqual(left, right);
+        [Intrinsic]
+        public static Vector128<double> CompareNotEqual(Vector128<double> left, Vector128<double> right) => CompareNotEqual(left, right);
+        [Intrinsic]
+        public static Vector128<nint>   CompareNotEqual(Vector128<nint> left, Vector128<nint> right) => CompareNotEqual(left, right);
+        [Intrinsic]
+        public static Vector128<nuint>  CompareNotEqual(Vector128<nuint> left, Vector128<nuint> right) => CompareNotEqual(left, right);
     }
 }
