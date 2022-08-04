9f1dd1aa499a882453b34d4e8810626423fbfed8
Aaron Robinson <arobins@microsoft.com>
2022-08-04 15:12:41 -0700

    NativeAOT tests interopservices (#73200)
    * Update InteropServices tests for NativeAOT
    
    * Remove SetWin32ContextInIDispatchAttribute attribute and tests.
      - this was no longer in the ref assembly so just removing it.
    
    * Remove dependency on registered COM server. This enables more
      expansive testing on all Windows SKUs.

--- src/libraries/System.Runtime.InteropServices/tests/System.Runtime.InteropServices.UnitTests/System/Runtime/InteropServices/Marshal/DestroyStructureTests.cs
+++ src/libraries/System.Runtime.InteropServices/tests/System.Runtime.InteropServices.UnitTests/System/Runtime/InteropServices/Marshal/DestroyStructureTests.cs
@@ -84,10 +84,13 @@ namespace System.Runtime.InteropServices.Tests
 
             yield return new object[] { typeof(GenericClass<>).GetTypeInfo().GenericTypeParameters[0] };
 
-            AssemblyBuilder assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName("Assembly"), AssemblyBuilderAccess.Run);
-            ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule("Module");
-            TypeBuilder typeBuilder = moduleBuilder.DefineType("Type");
-            yield return new object[] { typeBuilder };
+            if (PlatformDetection.IsReflectionEmitSupported)
+            {
+                AssemblyBuilder assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName("Assembly"), AssemblyBuilderAccess.Run);
+                ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule("Module");
+                TypeBuilder typeBuilder = moduleBuilder.DefineType("Type");
+                yield return new object[] { typeBuilder };
+            }
         }
 
         [Theory]
