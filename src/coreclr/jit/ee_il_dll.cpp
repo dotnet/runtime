3fd454a18f091b9de40ae916139f95f236b84c56
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2022-08-05 17:27:18 +0200

    JIT: Ensure correct alignment of CILJit singleton (#73451)
    Fix #73409

--- src/coreclr/jit/ee_il_dll.cpp
+++ src/coreclr/jit/ee_il_dll.cpp
@@ -152,18 +152,6 @@ void jitShutdown(bool processIsTerminating)
 
 /*****************************************************************************/
 
-struct CILJitSingletonAllocator
-{
-    int x;
-};
-const CILJitSingletonAllocator CILJitSingleton = {0};
-
-void* __cdecl operator new(size_t, const CILJitSingletonAllocator&)
-{
-    static char CILJitBuff[sizeof(CILJit)];
-    return CILJitBuff;
-}
-
 DLLEXPORT ICorJitCompiler* getJit()
 {
     if (!g_jitInitialized)
@@ -173,7 +161,8 @@ DLLEXPORT ICorJitCompiler* getJit()
 
     if (ILJitter == nullptr)
     {
-        ILJitter = new (CILJitSingleton) CILJit();
+        alignas(alignof(CILJit)) static char CILJitBuff[sizeof(CILJit)];
+        ILJitter = new (CILJitBuff, jitstd::placement_t()) CILJit();
     }
     return (ILJitter);
 }
