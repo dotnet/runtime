e9bdfd8b02d236121b15764c9fe68008b1b163d6
Larry Ewing <lewing@microsoft.com>
2022-08-05 07:27:08 -0500

    [release/6.0][mono] add 400 band workloads (#72379)
    This is to work around the broken sdk workload band install logic that has since been fixed.
    
    Co-authored-by: Ankit Jain <radical@gmail.com>
    Co-authored-by: Steve Pfister <steve.pfister@microsoft.com>

--- src/tasks/WorkloadBuildTasks/InstallWorkloadFromArtifacts.cs
+++ src/tasks/WorkloadBuildTasks/InstallWorkloadFromArtifacts.cs
@@ -4,9 +4,9 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using Microsoft.Build.Framework;
@@ -94,11 +94,18 @@ namespace Microsoft.Workload.Build.Tasks
         {
             Log.LogMessage(MessageImportance.High, $"Installing workload manifest for {name}/{version} for sdk band {VersionBand}");
 
+            string sdkManifestsVersionBadDir = Path.Combine(SdkDir, "sdk-manifests", VersionBand);
+            if (!Directory.Exists(sdkManifestsVersionBadDir))
+            {
+                Log.LogMessage(MessageImportance.Low, $"Creating directory {sdkManifestsVersionBadDir} because it does not already exist.");
+                Directory.CreateDirectory(sdkManifestsVersionBadDir);
+            }
+
             // Find any existing directory with the manifest name, ignoring the case
             // Multiple directories for a manifest, differing only in case causes
             // workload install to fail due to duplicate manifests!
             // This is applicable only on case-sensitive filesystems
-            string outputDir = FindSubDirIgnoringCase(Path.Combine(SdkDir, "sdk-manifests", VersionBand), name);
+            string outputDir = FindOrCreateSubDirIgnoringCase(sdkManifestsVersionBadDir, name);
 
             PackageReference pkgRef = new(Name: $"{name}.Manifest-{VersionBand}",
                                           Version: version,
@@ -171,7 +178,7 @@ namespace Microsoft.Workload.Build.Tasks
             return false;
         }
 
-        private string FindSubDirIgnoringCase(string parentDir, string dirName)
+        private string FindOrCreateSubDirIgnoringCase(string parentDir, string dirName)
         {
             IEnumerable<string> matchingDirs = Directory.EnumerateDirectories(parentDir,
                                                             dirName,
@@ -184,7 +191,7 @@ namespace Microsoft.Workload.Build.Tasks
                                 + $"{Environment.NewLine}Using the first one: {first}");
             }
 
-            return first ?? Path.Combine(parentDir, dirName);
+            return first ?? Path.Combine(parentDir, dirName.ToLower(CultureInfo.InvariantCulture));
         }
 
         private record ManifestInformation(
