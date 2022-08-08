980ec2e0882cd09ba48d7a6ecc332cc49862cc2e
Josh Peterson <joshuap@unity3d.com>
2022-08-08 14:06:16 -0400

    Use batch file comments in Windows batch files

--- .yamato/scripts/build_windows_x64.cmd
+++ .yamato/scripts/build_windows_x64.cmd
@@ -1,10 +1,10 @@
-# build solution
+rem build solution
 dotnet build unity\managed.sln -c Release
 cd unity\unitygc
 cmake .
 cmake --build . --config Release
 cd ../../
-# build subset library and core clr
+rem build subset library and core clr
 build.cmd -subset clr+libs -a x64 -c release -ci
 copy unity\unitygc\Release\unitygc.dll artifacts\bin\microsoft.netcore.app.runtime.win-x64\Release\runtimes\win-x64\native
 copy artifacts\bin\unity-embed-host\Release\net6.0\unity-embed-host.dll artifacts\bin\microsoft.netcore.app.runtime.win-x64\Release\runtimes\win-x64\lib\net7.0 
