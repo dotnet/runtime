5ccf469adec38536389a3c4f517a14362826d61d
Marek Fišera <mara@neptuo.com>
2022-08-08 14:51:46 +0200

    Feedback.

--- src/mono/wasm/runtime/network-information.ts
+++ src/mono/wasm/runtime/network-information.ts
@@ -1,32 +1,35 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+import { mono_assert } from "./types";
+
 export function network_wasm_is_online(): boolean {
+    mono_assert(network_wasm_is_online_api_available(), "The network_wasm_is_online_api_available is not met");
     return globalThis.navigator.onLine;
 }
 
 let change_listener: ((onLine: boolean) => void) | null = null;
 
 export function network_wasm_set_change_listener(listener: (onLine: boolean) => void): void {
-    if (window) {
-        if (!change_listener) {
-            window.addEventListener("offline", network_wasm_available_changed);
-            window.addEventListener("online", network_wasm_available_changed);
-        }
-
-        change_listener = listener;
+    mono_assert(network_wasm_is_online_api_available(), "The network_wasm_is_online_api_available is not met");
+    
+    if (!change_listener) {
+        window.addEventListener("offline", network_wasm_available_changed);
+        window.addEventListener("online", network_wasm_available_changed);
     }
+
+    change_listener = listener;
 }
 
 export function network_wasm_remove_change_listener(): void {
-    if (window) {
-        if (change_listener) {
-            window.removeEventListener("offline", network_wasm_available_changed);
-            window.removeEventListener("online", network_wasm_available_changed);
-        }
-
-        change_listener = null;
+    mono_assert(network_wasm_is_online_api_available(), "The network_wasm_is_online_api_available is not met");
+    
+    if (change_listener) {
+        window.removeEventListener("offline", network_wasm_available_changed);
+        window.removeEventListener("online", network_wasm_available_changed);
     }
+
+    change_listener = null;
 }
 
 function network_wasm_available_changed() {
