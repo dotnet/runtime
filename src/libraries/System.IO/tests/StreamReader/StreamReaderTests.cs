9687016b1652735b143845bc4b6b35ec0b72a680
Jo Shields <directhex@apebox.org>
2022-08-09 17:33:37 -0400

    PlatformNotSupportedException for UNIX domain sockets on iOS/tvOS (#73374)
    * PNSE for domain sockets on iOS/tvOS
    * Re-enable some tests which SHOULD now throw PNSE

--- src/libraries/System.IO/tests/StreamReader/StreamReaderTests.cs
+++ src/libraries/System.IO/tests/StreamReader/StreamReaderTests.cs
@@ -555,8 +555,8 @@ namespace System.IO.Tests
         [InlineData(1, false)]
         [InlineData(1, true)]
         [SkipOnPlatform(TestPlatforms.Browser, "Not supported on Browser.")]
-        [SkipOnPlatform(TestPlatforms.LinuxBionic, "SElinux blocks UNIX sockets")]
-        [ActiveIssue("https://github.com/dotnet/runtime/issues/51390", TestPlatforms.iOS | TestPlatforms.tvOS | TestPlatforms.MacCatalyst)]
+        [SkipOnPlatform(TestPlatforms.LinuxBionic, "SElinux blocks UNIX sockets in our CI environment")]
+        [SkipOnPlatform(TestPlatforms.iOS | TestPlatforms.tvOS, "iOS/tvOS blocks binding to UNIX sockets")]
         public async Task ReadAsync_Canceled_ThrowsException(int method, bool precanceled)
         {
             Func<StreamReader, CancellationToken, Task<int>> func = method switch
