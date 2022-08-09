a571ef105bdd2ed4f02f14ed27454aecd5750e0c
Jeremi Kurdek <59935235+jkurdek@users.noreply.github.com>
2022-08-09 13:26:21 +0200

    Fixed IL2121 single warn issue (dotnet/linker#2953)
    Commit migrated from https://github.com/dotnet/linker/commit/a0a2dc8ff0367c1acabb4c373f71c9ef1a6b6121

--- src/tools/linker/src/ILLink.Shared/DiagnosticId.cs
+++ src/tools/linker/src/ILLink.Shared/DiagnosticId.cs
@@ -222,7 +222,7 @@ namespace ILLink.Shared
 				2103 => MessageSubCategory.TrimAnalysis,
 				2106 => MessageSubCategory.TrimAnalysis,
 				2107 => MessageSubCategory.TrimAnalysis,
-				>= 2109 and <= 2120 => MessageSubCategory.TrimAnalysis,
+				>= 2109 and <= 2121 => MessageSubCategory.TrimAnalysis,
 				>= 3050 and <= 3052 => MessageSubCategory.AotAnalysis,
 				>= 3054 and <= 3055 => MessageSubCategory.AotAnalysis,
 				_ => MessageSubCategory.None,
