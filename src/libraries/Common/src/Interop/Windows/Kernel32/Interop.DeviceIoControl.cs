cebd734764d4b57617bd074b306fc87f26af7461
Adam Sitnik <adam.sitnik@gmail.com>
2022-08-11 10:35:48 +0200

    Apply suggestions from code review
    Co-authored-by: Jan Kotas <jkotas@microsoft.com>

--- src/libraries/Common/src/Interop/Windows/Kernel32/Interop.DeviceIoControl.cs
+++ src/libraries/Common/src/Interop/Windows/Kernel32/Interop.DeviceIoControl.cs
@@ -13,7 +13,7 @@ internal static partial class Interop
         internal const int FSCTL_GET_REPARSE_POINT = 0x000900a8;
 
         // https://docs.microsoft.com/windows-hardware/drivers/ddi/ntddstor/ni-ntddstor-ioctl_storage_read_capacity
-        internal const int IOCTL_STORAGE_READ_CAPACITY = 2969920;
+        internal const int IOCTL_STORAGE_READ_CAPACITY = 0x002D5140;
 
         [LibraryImport(Libraries.Kernel32, EntryPoint = "DeviceIoControl", SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
