2b68bd3b56733322aabd418f3c7c542fdc3a5abf
Drew Kersnar <18474647+dakersnar@users.noreply.github.com>
2022-08-11 19:07:19 -0500

    Add unit test for DIM

---
+++ src/libraries/System.Runtime/tests/System/Numerics/IBinaryNumberTests.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace System.Numerics.Tests
+{
+
+    public class IBinaryNumberTests
+    {
+
+        [Fact]
+        public static void AllBitsSetTest()
+        {
+            Assert.Equal(unchecked((int)0xFFFF_FFFF), BinaryNumberHelper<BinaryNumberDimHelper>.AllBitsSet.value);
+            Assert.Equal(0, ~BinaryNumberHelper<BinaryNumberDimHelper>.AllBitsSet.value);
+        }
+    }
+}
