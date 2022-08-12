2158b73f9e8ff4887053a35cd6f426a27e0d96c4
Marek Fišera <mara@neptuo.com>
2022-08-12 23:24:29 +0200

    Fix exporting INTERNAL api.
    Move all legacy API object creation to dotnet.es6.post.js where module export is created.

--- src/mono/wasm/runtime/es6/dotnet.es6.post.js
+++ src/mono/wasm/runtime/es6/dotnet.es6.post.js
@@ -7,6 +7,6 @@ return createDotnetRuntime.ready
 __dotnet_runtime.__setEmscriptenEntrypoint(createDotnetRuntime);
 const dotnet = __dotnet_runtime.moduleExports.dotnet;
 const exit = __dotnet_runtime.moduleExports.exit;
-const INTERNAL = __dotnet_runtime.moduleExports.requirePromise;
+const MONO = {}, BINDING = {}, INTERNAL = {}, IMPORTS = {};
 export { dotnet, exit, INTERNAL };
 var ignoredIffe = (() => { return (function () {
