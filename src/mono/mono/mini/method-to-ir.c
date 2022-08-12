8e9d27a81321330f38cc24e1f32a71e81dc6d4dd
Jan Dupej <jandupej@microsoft.com>
2022-08-12 15:27:07 +0200

    [mono][jit] box+callvirt no longer optimized in AOT when generics are involved.

--- src/mono/mono/mini/method-to-ir.c
+++ src/mono/mono/mini/method-to-ir.c
@@ -9568,7 +9568,7 @@ calli_end:
 			//   call SomeStruct::Method()
 			guchar* callvirt_ip;
 			guint32 callvirt_proc_token;
-			if (!cfg->llvm_only && // virtual calls are handled differently with llvm aot
+			if (!((cfg->compile_aot || cfg->compile_llvm) && !mono_class_is_def(klass)) && // we cannot devirtualize in AOT when using generics
 				next_ip < end &&
 				(callvirt_ip = il_read_callvirt (next_ip, end, &callvirt_proc_token)) && 
 				ip_in_bb (cfg, cfg->cbb, callvirt_ip) ) {
