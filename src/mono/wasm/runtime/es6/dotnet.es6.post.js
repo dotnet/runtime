e6add0d4129318dea3f41f2ac3814ee0c7b142a5
pavelsavara <pavel.savara@gmail.com>
2022-08-13 07:31:26 +0200

    fix release build

--- src/mono/wasm/runtime/es6/dotnet.es6.post.js
+++ src/mono/wasm/runtime/es6/dotnet.es6.post.js
@@ -1,12 +1,1 @@
-createDotnetRuntime.ready = createDotnetRuntime.ready.then(() => {
-    return __dotnet_exportedAPI;
-});
-
-return createDotnetRuntime.ready
-});}) ();
-__dotnet_runtime.__setEmscriptenEntrypoint(createDotnetRuntime);
-const dotnet = __dotnet_runtime.moduleExports.dotnet;
-const exit = __dotnet_runtime.moduleExports.exit;
-const MONO = {}, BINDING = {}, INTERNAL = {}, IMPORTS = {};
-export { dotnet, exit, INTERNAL };
-var ignoredIffe = (() => { return (function () {
+createDotnetRuntime.ready = createDotnetRuntime.ready.then(() => { return __dotnet_exportedAPI; });
