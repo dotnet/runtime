dotnet/runtime

commit 802bbbdb3232912c6343db7a31e50ed63a59dc46

DCS alignment to 4.8 with schema support (#71752)
* Fix Private.Xml solution broken by https://github.com/dotnet/runtime/pull/70706.

* Align DCS with 4.8 implementation - minus schema import/export.

* External DCS Schema support groundwork. Before DC-tree work and other public APIs.

* Added public APIs, but not the DataContract tree yet.

* All APIs in.

* Cleanup.

* Addressed some nits and PR feedback on API.

* API nit, more PR feedback.

* Fix HasRoot hiding issue.

* Add basic schema tests. Fix DataMember bug.

* Add /// comments for new schema project.

* Fixing bad format in xml comment.

* Skip import test on Wasm.

* Obsolete old half-coded schema exporter.

* Add obsolete attribute in ref project as well.

* Removing obsolete for now, since it appears Syndication depends on old class.

* Fix the DCJS ref project.

* API cleanup.

* Project file cleanup

* Fix mono issue with reflection access to non-public private fields.

* Addressing feedback from API review.

* Port NetFx Export/Import test suites to Serializer test projects.

* Fix bugs found by newly ported tests.

* Account for different newline sizes on different platforms.

* Skip flaky test on wasm for now.

* Non-draft PR feedback.

* Change DC.Members API. Drop KVP shennanigans.

* More PR feedback.

Co-authored-by: Viktor Hofer <viktor.hofer@microsoft.com>

src/libraries/System.Private.DataContractSerialization/src/System/Xml/XmlBufferReader.cs
@@ -12,6 +12,7 @@ using System.Globalization;
 using System.Runtime.Serialization;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Buffers.Binary;
 
 namespace System.Xml
 {
@@ -210,13 +211,14 @@ namespace System.Xml
             if (_stream == null)
                 return false;
 
+            DiagnosticUtility.DebugAssert(_offset <= int.MaxValue - count, "");
+
             // The data could be coming from an untrusted source, so we use a standard
             // "multiply by 2" growth algorithm to avoid overly large memory utilization.
             // Constant value of 256 comes from MemoryStream implementation.
 
             do
             {
-                DiagnosticUtility.DebugAssert(_offset <= int.MaxValue - count, "");
                 int newOffsetMax = _offset + count;
                 if (newOffsetMax <= _offsetMax)
                     return true;
@@ -427,16 +429,19 @@ namespace System.Xml
             return value;
         }
 
-        public unsafe decimal ReadDecimal()
+        public decimal ReadDecimal()
         {
-            int offset;
-            byte[] buffer = GetBuffer(ValueHandleLength.Decimal, out offset);
-            decimal value;
-            byte* pb = (byte*)&value;
-            for (int i = 0; i < sizeof(decimal); i++)
-                pb[i] = buffer[offset + i];
-            Advance(ValueHandleLength.Decimal);
-            return value;
+            byte[] buffer = GetBuffer(ValueHandleLength.Decimal, out int offset);
+            ReadOnlySpan<byte> bytes = buffer.AsSpan(offset, sizeof(decimal));
+            ReadOnlySpan<int> span = stackalloc int[4]
+            {
+                BinaryPrimitives.ReadInt32LittleEndian(bytes),
+                BinaryPrimitives.ReadInt32LittleEndian(bytes.Slice(4)),
+                BinaryPrimitives.ReadInt32LittleEndian(bytes.Slice(8)),
+                BinaryPrimitives.ReadInt32LittleEndian(bytes.Slice(12))
+            };
+
+            return new decimal(span);
         }
 
         public UniqueId ReadUniqueId()
@@ -678,6 +683,13 @@ namespace System.Xml
             char[] chars = GetCharBuffer(length);
             int charCount = GetEscapedChars(offset, length, chars);
             return new string(chars, 0, charCount);
+        }
+
+        public string GetEscapedString(int offset, int length, XmlNameTable nameTable)
+        {
+            char[] chars = GetCharBuffer(length);
+            int charCount = GetEscapedChars(offset, length, chars);
+            return nameTable.Add(chars, 0, charCount);
         }
 
         private int GetLessThanCharEntity(int offset, int length)
@@ -1059,14 +1071,18 @@ namespace System.Xml
             return value;
         }
 
-        public unsafe decimal GetDecimal(int offset)
+        public decimal GetDecimal(int offset)
         {
-            byte[] buffer = _buffer;
-            decimal value;
-            byte* pb = (byte*)&value;
-            for (int i = 0; i < sizeof(decimal); i++)
-                pb[i] = buffer[offset + i];
-            return value;
+            ReadOnlySpan<byte> bytes = _buffer.AsSpan(offset, sizeof(decimal));
+            ReadOnlySpan<int> span = stackalloc int[4]
+            {
+                BinaryPrimitives.ReadInt32LittleEndian(bytes),
+                BinaryPrimitives.ReadInt32LittleEndian(bytes.Slice(4)),
+                BinaryPrimitives.ReadInt32LittleEndian(bytes.Slice(8)),
+                BinaryPrimitives.ReadInt32LittleEndian(bytes.Slice(12))
+            };
+
+            return new decimal(span);
         }
 
         public UniqueId GetUniqueId(int offset)
@@ -1132,9 +1148,11 @@ namespace System.Xml
             {
                 keyDictionary = _dictionary!;
             }
+
             XmlDictionaryString? s;
             if (!keyDictionary.TryLookup(key >> 1, out s))
                 XmlExceptionHelper.ThrowInvalidBinaryFormat(_reader);
+
             return s;
         }
 
@@ -1165,6 +1183,7 @@ namespace System.Xml
                     XmlExceptionHelper.ThrowXmlDictionaryStringIDUndefinedStatic(_reader, staticKey);
                 }
             }
+
             return key;
         }
 

commit 9a2f8bd7f645fdf913402f51d3df96429ba7208f

FIx bugs introduced in #71752 * Add missing Advance to prevent corruption of reader * Correctly read decimal values

src/libraries/System.Private.DataContractSerialization/src/System/Xml/XmlBufferReader.cs
@@ -390,11 +390,13 @@ namespace System.Xml
                 ReadOnlySpan<byte> bytes = buffer.AsSpan(offset, sizeof(decimal));
                 ReadOnlySpan<int> span = stackalloc int[4]
                 {
-                    BinaryPrimitives.ReadInt32LittleEndian(bytes),
-                    BinaryPrimitives.ReadInt32LittleEndian(bytes.Slice(4)),
-                    BinaryPrimitives.ReadInt32LittleEndian(bytes.Slice(8)),
-                    BinaryPrimitives.ReadInt32LittleEndian(bytes.Slice(12))
+                    BinaryPrimitives.ReadInt32LittleEndian(bytes.Slice(8, 4)),
+                    BinaryPrimitives.ReadInt32LittleEndian(bytes.Slice(12, 4)),
+                    BinaryPrimitives.ReadInt32LittleEndian(bytes.Slice(4, 4)),
+                    BinaryPrimitives.ReadInt32LittleEndian(bytes.Slice(0, 4))
                 };
+
+                Advance(ValueHandleLength.Decimal);
                 return new decimal(span);
             }
         }
@@ -978,10 +980,10 @@ namespace System.Xml
                 ReadOnlySpan<byte> bytes = _buffer.AsSpan(offset, sizeof(decimal));
                 ReadOnlySpan<int> span = stackalloc int[4]
                 {
-                    BinaryPrimitives.ReadInt32LittleEndian(bytes),
-                    BinaryPrimitives.ReadInt32LittleEndian(bytes.Slice(4)),
-                    BinaryPrimitives.ReadInt32LittleEndian(bytes.Slice(8)),
-                    BinaryPrimitives.ReadInt32LittleEndian(bytes.Slice(12))
+                    BinaryPrimitives.ReadInt32LittleEndian(bytes.Slice(8, 4)),
+                    BinaryPrimitives.ReadInt32LittleEndian(bytes.Slice(12, 4)),
+                    BinaryPrimitives.ReadInt32LittleEndian(bytes.Slice(4, 4)),
+                    BinaryPrimitives.ReadInt32LittleEndian(bytes.Slice(0, 4))
                 };
 
                 return new decimal(span);