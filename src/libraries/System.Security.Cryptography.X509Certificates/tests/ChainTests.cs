eb82f7f91dc8e9cbba72c548a39c0aa69f421abe
Tom Deseyn <tom.deseyn@gmail.com>
2022-08-16 01:37:47 +0200

    X509Certificates.Tests: make tests pass when sha1 cert signatures are not supported
    Co-authored-by: Jeremy Barton <jbarton@microsoft.com>

--- src/libraries/System.Security.Cryptography.X509Certificates/tests/ChainTests.cs
+++ src/libraries/System.Security.Cryptography.X509Certificates/tests/ChainTests.cs
@@ -168,7 +168,7 @@ namespace System.Security.Cryptography.X509Certificates.Tests
             Assert.Equal(IntPtr.Zero, chain.ChainContext);
         }
 
-        [Fact]
+        [ConditionalFact(typeof(SignatureSupport), nameof(SignatureSupport.SupportsX509Sha1Signatures))]
         public static void TestResetMethod()
         {
             using (var sampleCert = new X509Certificate2(TestData.DssCer))
@@ -312,7 +312,12 @@ namespace System.Security.Cryptography.X509Certificates.Tests
             if (!PlatformDetection.IsAndroid)
             {
                 // Android doesn't support an empty custom root
-                yield return new object[] { false, X509ChainStatusFlags.UntrustedRoot, BuildChainCustomTrustStoreTestArguments.TrustedIntermediateUntrustedRoot };
+                X509ChainStatusFlags flags = X509ChainStatusFlags.UntrustedRoot;
+                if (!SignatureSupport.SupportsX509Sha1Signatures)
+                {
+                    flags |= X509ChainStatusFlags.NotSignatureValid;
+                }
+                yield return new object[] { false, flags, BuildChainCustomTrustStoreTestArguments.TrustedIntermediateUntrustedRoot };
             }
 
             yield return new object[] { true, X509ChainStatusFlags.NoError, BuildChainCustomTrustStoreTestArguments.UntrustedIntermediateTrustedRoot };
@@ -557,7 +562,7 @@ namespace System.Security.Cryptography.X509Certificates.Tests
             }
         }
 
-        [Fact]
+        [ConditionalFact(typeof(SignatureSupport), nameof(SignatureSupport.SupportsX509Sha1Signatures))]
         public static void BuildChain_WithCertificatePolicy_Match()
         {
             using (var cert = new X509Certificate2(TestData.CertWithPolicies))
