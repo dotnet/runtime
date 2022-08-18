f1b54fff20cbdfbdb39434ad51f447a1e9e0744b
Katelyn Gadd <kg@luminance.org>
2022-08-18 11:33:12 -0700

    Omit the JS sync context if threads are not enabled

--- src/libraries/System.Runtime.InteropServices.JavaScript/src/System/Runtime/InteropServices/JavaScript/JSSynchronizationContext.cs
+++ src/libraries/System.Runtime.InteropServices.JavaScript/src/System/Runtime/InteropServices/JavaScript/JSSynchronizationContext.cs
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#if FEATURE_WASM_THREADS
+
 using System;
 using System.Threading;
 using System.Threading.Channels;
@@ -141,3 +143,5 @@ namespace System.Runtime.InteropServices.JavaScript {
         }
     }
 }
+
+#endif
