5cd2c38c57540544c772f0a7e4b1d493253aa813
Kunal Pathak <Kunal.Pathak@microsoft.com>
2022-08-17 23:01:34 -0700

    Revert "Revert "Ensure that GT_CNS_VEC is handled in LinearScan::isMatchingConstant (#70171)""
    This reverts commit 984120f0cde0d26f9768112296e68164cfe76c8e.

--- src/coreclr/jit/lsraxarch.cpp
+++ src/coreclr/jit/lsraxarch.cpp
@@ -147,6 +147,7 @@ int LinearScan::BuildNode(GenTree* tree)
         case GT_CNS_INT:
         case GT_CNS_LNG:
         case GT_CNS_DBL:
+        case GT_CNS_VEC:
         {
             srcCount = 0;
             assert(dstCount == 1);
