8792a67f1c7340558eea05701c8a92120c5ca87b
Kevin Jones <kevin@vcsjones.com>
2022-08-18 10:47:09 -0400

    Split SkipOnPlatform per code review feedback

--- src/libraries/System.Security.Cryptography/tests/X509Certificates/RevocationTests/AiaTests.cs
+++ src/libraries/System.Security.Cryptography/tests/X509Certificates/RevocationTests/AiaTests.cs
@@ -9,7 +9,8 @@ using Xunit;
 namespace System.Security.Cryptography.X509Certificates.Tests.RevocationTests
 {
     [ActiveIssue("https://github.com/dotnet/runtime/issues/57506", typeof(PlatformDetection), nameof(PlatformDetection.IsMonoRuntime), nameof(PlatformDetection.IsMariner))]
-    [SkipOnPlatform(TestPlatforms.Android | TestPlatforms.Browser, "Android nor Browser does not support AIA fetching")]
+    [SkipOnPlatform(TestPlatforms.Android, "Android does not support AIA fetching")]
+    [SkipOnPlatform(TestPlatforms.Browser, "Browser doesn't support X.509 certificates")]
     public static class AiaTests
     {
         [Fact]
