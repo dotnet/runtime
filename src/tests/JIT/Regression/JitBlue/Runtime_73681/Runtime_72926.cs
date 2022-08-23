3ba6b4e6dbc9ecb2044fc092bca6410bca084222
Mark Plesko <markples@microsoft.com>
2022-08-23 13:27:21 -0700

    Add test

---
+++ src/tests/JIT/Regression/JitBlue/Runtime_73681/Runtime_72926.cs
@@ -0,0 +1,29 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Runtime.CompilerServices;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(CallFoo(new C()));
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private static int CallFoo<T>(T val) where T : IFace
+    {
+        return val.Foo();
+    }
+}
+
+public interface IFace
+{
+    int Foo();
+}
+
+public class C : IFace
+{
+    public int Foo() => 0;
+}
