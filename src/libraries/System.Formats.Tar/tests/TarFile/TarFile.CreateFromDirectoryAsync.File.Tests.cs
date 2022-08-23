65484f361af3fb8467e8445b1363c7b86a3b168b
Dan Moseley <danmose@microsoft.com>
2022-08-23 04:21:02 -0600

    Set modified timestamps on files being extracted from tar archives (#74400)

--- src/libraries/System.Formats.Tar/tests/TarFile/TarFile.CreateFromDirectoryAsync.File.Tests.cs
+++ src/libraries/System.Formats.Tar/tests/TarFile/TarFile.CreateFromDirectoryAsync.File.Tests.cs
@@ -238,7 +238,7 @@ namespace System.Formats.Tar.Tests
             }
         }
 
-        [Fact]
+        [ConditionalFact(typeof(MountHelper), nameof(MountHelper.CanCreateSymbolicLinks))]
         public async Task SkipRecursionIntoDirectorySymlinksAsync()
         {
             using TempDirectory root = new TempDirectory();
@@ -269,7 +269,7 @@ namespace System.Formats.Tar.Tests
             Assert.Null(await reader.GetNextEntryAsync()); // file.txt should not be found
         }
 
-        [Fact]
+        [ConditionalFact(typeof(MountHelper), nameof(MountHelper.CanCreateSymbolicLinks))]
         public async Task SkipRecursionIntoBaseDirectorySymlinkAsync()
         {
             using TempDirectory root = new TempDirectory();
