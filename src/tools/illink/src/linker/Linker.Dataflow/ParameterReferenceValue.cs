647ae93099ef31184856eb510764eeada45fce4e
Jackson Schuster <36744439+jtschuster@users.noreply.github.com>
2022-08-23 09:51:19 -0500

    Check variable type in ValidateNoReferencesToReferences instead of dataflow-tracked value and only validate on changes to locals (dotnet/linker#2983)
    Commit migrated from https://github.com/dotnet/linker/commit/86951339aa858607be7aa8df2edfbefda862742d

--- src/tools/illink/src/linker/Linker.Dataflow/ParameterReferenceValue.cs
+++ src/tools/illink/src/linker/Linker.Dataflow/ParameterReferenceValue.cs
@@ -2,11 +2,13 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 using ILLink.Shared.DataFlow;
 using Mono.Cecil;
+using Mono.Linker;
 
 namespace ILLink.Shared.TrimAnalysis
 {
 	public partial record ParameterReferenceValue (MethodDefinition MethodDefinition, int ParameterIndex)
-: ReferenceValue
+		: ReferenceValue (MethodDefinition.HasImplicitThis () && ParameterIndex == 0 ? MethodDefinition.DeclaringType
+			: MethodDefinition.Parameters[MethodDefinition.HasImplicitThis () ? --ParameterIndex : ParameterIndex].ParameterType)
 	{
 		public override SingleValue DeepCopy ()
 		{
