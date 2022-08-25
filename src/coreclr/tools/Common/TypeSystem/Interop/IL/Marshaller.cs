442c137891821a567e9a05411f821dbf2aec5aa5
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2022-08-25 19:42:20 +0900

    Only stack allocate when marshalling Utf8 arguments (#74478)
    * Only stack allocate when marshalling Utf8 arguments
    
    Fixes #74394.
    
    * Review feedback

--- src/coreclr/tools/Common/TypeSystem/Interop/IL/Marshaller.cs
+++ src/coreclr/tools/Common/TypeSystem/Interop/IL/Marshaller.cs
@@ -1474,7 +1474,7 @@ namespace Internal.TypeSystem.Interop
             get
             {
                 return MarshalDirection == MarshalDirection.Forward
-                    && MarshallerType != MarshallerType.Field
+                    && MarshallerType == MarshallerType.Argument
                     && !IsManagedByRef
                     && In
                     && !Out;
@@ -1672,7 +1672,11 @@ namespace Internal.TypeSystem.Interop
         {
             ILEmitter emitter = _ilCodeStreams.Emitter;
 
-            if (In && !Out && !IsManagedByRef)
+            if (MarshalDirection == MarshalDirection.Forward
+                && MarshallerType == MarshallerType.Argument
+                && !IsManagedByRef
+                && In
+                && !Out)
             {
                 TypeDesc marshallerIn = MarshallerIn;
 
