74d79db3cd12b6ded64d45a281ce522b18364958
En3Tho- <razerael@gmail.com>
2022-08-26 18:10:12 +0300

    Update il range in optOptimizeBoolsUpdateTrees

--- src/coreclr/jit/optimizer.cpp
+++ src/coreclr/jit/optimizer.cpp
@@ -9416,6 +9416,9 @@ void OptBoolsDsc::optOptimizeBoolsUpdateTrees()
     {
         m_comp->fgUpdateLoopsAfterCompacting(m_b1, m_b3);
     }
+
+    // Update IL range of first block
+    m_b1->bbCodeOffsEnd = optReturnBlock ? m_b3->bbCodeOffsEnd : m_b2->bbCodeOffsEnd;
 }
 
 //-----------------------------------------------------------------------------
