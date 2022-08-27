06aceb7015f3bd2ff019ef5920d2354eb2ea2c92
Aleksey Kliger (λgeek) <alklig@microsoft.com>
2022-08-26 20:33:35 -0400

    Revert "[MONO] Move marshal-ilgen into a component (#71203)" (#74675)
    This reverts commit de32c4466489090fdd41574d1be2945fd03ab7f2.

--- src/mono/mono/metadata/marshal-ilgen.h
+++ src/mono/mono/metadata/marshal-ilgen.h
@@ -1,24 +1,9 @@
-/**
- * \file
- * Copyright 2022 Microsoft
- * Licensed under the MIT license. See LICENSE file in the project root for full license information.
- */
+
 #ifndef __MARSHAL_ILGEN_H__
 #define __MARSHAL_ILGEN_H__
 
 #include "metadata/marshal-lightweight.h"
 #include "metadata/marshal.h"
-#include "mono/component/component.h"
-
-typedef struct MonoComponentMarshalILgen {
-	MonoComponent component;
-	void (*ilgen_init) (void);
-	int (*emit_marshal_ilgen) (EmitMarshalContext *m, int argnum, MonoType *t,
-	      MonoMarshalSpec *spec, int conv_arg,
-	      MonoType **conv_arg_type, MarshalAction action,  MonoMarshalLightweightCallbacks* lightweigth_cb);
-	void (*install_callbacks_mono) (IlgenCallbacksToMono *callbacks);
-
-} MonoComponentMarshalILgen;
 
 typedef struct {
 	int version;
@@ -34,23 +19,24 @@ typedef struct {
 	int (*emit_marshal_custom) (EmitMarshalContext *m, int argnum, MonoType *t, MonoMarshalSpec *spec, int conv_arg, MonoType **conv_arg_type, MarshalAction action);
 	int (*emit_marshal_asany) (EmitMarshalContext *m, int argnum, MonoType *t, MonoMarshalSpec *spec, int conv_arg, MonoType **conv_arg_type, MarshalAction action);
 	int (*emit_marshal_handleref) (EmitMarshalContext *m, int argnum, MonoType *t, MonoMarshalSpec *spec, int conv_arg, MonoType **conv_arg_type, MarshalAction action);
-} MonoMarshalILgenCallbacks;
-
-MONO_COMPONENT_EXPORT_ENTRYPOINT
-MonoComponentMarshalILgen* mono_component_marshal_ilgen_init (void);
+} MonoMarshalIlgenCallbacks;
 
 void
-mono_install_marshal_callbacks_ilgen (MonoMarshalILgenCallbacks *cb);
+mono_install_marshal_callbacks_ilgen (MonoMarshalIlgenCallbacks *cb);
+
 
 MONO_API void
 mono_marshal_ilgen_init (void);
  
+void
+mono_marshal_noilgen_init_heavyweight (void);
+
+void
+mono_marshal_noilgen_init_lightweight (void);
+
 int
 mono_emit_marshal_ilgen (EmitMarshalContext *m, int argnum, MonoType *t,
 	      MonoMarshalSpec *spec, int conv_arg,
 	      MonoType **conv_arg_type, MarshalAction action,  MonoMarshalLightweightCallbacks* lightweigth_cb);
-
-void
-mono_marshal_ilgen_install_callbacks_mono (IlgenCallbacksToMono *callbacks);
 
 #endif // __MARSHAL_ILGEN_H__
