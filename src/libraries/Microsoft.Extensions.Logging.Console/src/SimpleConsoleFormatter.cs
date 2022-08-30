61758f4e050b5b513decb0af80b390af3191107f
Alexander Köplinger <alex.koeplinger@outlook.com>
2022-08-30 20:13:16 +0200

    PR feedback

--- src/libraries/Microsoft.Extensions.Logging.Console/src/SimpleConsoleFormatter.cs
+++ src/libraries/Microsoft.Extensions.Logging.Console/src/SimpleConsoleFormatter.cs
@@ -16,9 +16,9 @@ namespace Microsoft.Extensions.Logging.Console
         private static readonly string _messagePadding = new string(' ', GetLogLevelString(LogLevel.Information).Length + LoglevelPadding.Length);
         private static readonly string _newLineWithMessagePadding = Environment.NewLine + _messagePadding;
 #if NETCOREAPP
-        private static readonly bool _isAndroidOrAppleMobile = OperatingSystem.IsAndroid()
+        private static readonly bool _isAndroidOrAppleMobile => OperatingSystem.IsAndroid()
                                                             || OperatingSystem.IsTvOS()
-                                                            || OperatingSystem.IsIOS();
+                                                            || OperatingSystem.IsIOS(); // returns true on MacCatalyst
 #else
         private const bool _isAndroidOrAppleMobile = false;
 #endif
