eb4aebf5b7e8f18c21708c98904ccb0bc932c4da
carlossanlop <carlossanlop@users.noreply.github.com>
2022-08-31 13:46:02 -0700

    Address more suggestions

--- src/libraries/System.Formats.Tar/tests/TarReader/TarReader.Tests.cs
+++ src/libraries/System.Formats.Tar/tests/TarReader/TarReader.Tests.cs
@@ -1,11 +1,10 @@
-using System.Xml.Linq;
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Collections.Generic;
 using System.IO;
-using Xunit;
 using System.Linq;
+using Xunit;
 
 namespace System.Formats.Tar.Tests
 {
