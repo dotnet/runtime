1838cbc328f36ebdad11e8c94285fcecd3c143e5
Alexander Köplinger <alex.koeplinger@outlook.com>
2022-09-02 14:56:12 +0200

    System.Net.Requests: Pick correct RemoteExecutor overload in test
    The test was using `RemoteExecutor.Invoke(Action<string, string, string, string> method, ...)` since there is no overload that takes `Func<string, string, string, string, Task>` (only one with three strings) so we weren't awaiting the Task.
    
    Fix by removing one string parameter so it picks the correct overload.
    
    Fixes https://github.com/dotnet/runtime/issues/74667

--- src/libraries/System.Net.Requests/tests/AssemblyInfo.cs
+++ src/libraries/System.Net.Requests/tests/AssemblyInfo.cs
@@ -5,5 +5,4 @@ using System;
 using Xunit;
 
 [assembly: ActiveIssue("https://github.com/dotnet/runtime/issues/34690", TestPlatforms.Windows, TargetFrameworkMonikers.Netcoreapp, TestRuntimes.Mono)]
-[assembly: ActiveIssue("https://github.com/dotnet/runtime/issues/74667", typeof(PlatformDetection), nameof(PlatformDetection.IsMonoLinuxArm64))]
 [assembly: SkipOnPlatform(TestPlatforms.Browser, "System.Net.Requests is not supported on Browser.")]
