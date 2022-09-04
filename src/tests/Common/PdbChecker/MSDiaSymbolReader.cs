176296c6bdd8a067c2280aae99e9dfc87971657d
Tomas Rylek <trylek@microsoft.com>
2022-09-04 20:22:38 +0200

    Initial provisions for semantic Crossgen2 PDB validation
    This Quality Week work item is motivated by my findings from a few
    months back that Crossgen2 PDB generator had been producing bogus
    symbol files for almost half a year due to a trivial bug and no
    testing in place was able to catch that. This change adds initial
    provisions for semantic PDB validation.
    
    In this initial commit I'm adding a new managed app PdbChecker
    that uses the DIA library to read a given PDB and optionally check
    it for the presence of given symbols. In parallel I'm making test
    infra changes that enable selective PDB validation support in
    individual tests including checks for the presence of expected
    symbols.
    
    This change by itself introduces rudimentary PR / CI validation of
    PDB files produced by Crossgen2. As next step I plan to introduce
    additional provisions for running this logic for all tests to be
    added to one of the Crossgen2-specific pipelines, and validation
    of PDBs produced during compilation of the framework assemblies.
    
    Thanks
    
    Tomas

---
+++ src/tests/Common/PdbChecker/MSDiaSymbolReader.cs
@@ -0,0 +1,58 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Dia2Lib;
+
+class MSDiaSymbolReader
+{
+    private readonly IDiaDataSource _diaDataSource;
+    private readonly IDiaSession _diaSession;
+
+    private readonly List<string> _pdbSymbols;
+
+    public MSDiaSymbolReader(string pdbFile)
+    {
+        try
+        {
+            _diaDataSource = new DiaSourceClass();
+            _diaDataSource.loadDataFromPdb(pdbFile);
+            _diaDataSource.openSession(out _diaSession);
+
+            _pdbSymbols = new List<string>();
+
+            _diaSession.getSymbolsByAddr(out IDiaEnumSymbolsByAddr symbolEnum);
+            int symbolsTotal = 0;
+            for (IDiaSymbol symbol = symbolEnum.symbolByRVA(0); symbol != null; symbolEnum.Next(1, out symbol, out uint fetched))
+            {
+                symbolsTotal++;
+                if (symbol.symTag == (uint)SymTagEnum.SymTagFunction || symbol.symTag == (uint)SymTagEnum.SymTagPublicSymbol)
+                {
+                    _pdbSymbols.Add(symbol.name);
+                }
+            }
+
+            Console.WriteLine("PDB file:       {0}", pdbFile);
+            Console.WriteLine("Total symbols:  {0}", symbolsTotal);
+            Console.WriteLine("Public symbols: {0}", _pdbSymbols.Count);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"Error opening PDB file {pdbFile}", ex);
+        }
+    }
+
+    public void DumpSymbols()
+    {
+        Console.WriteLine("PDB public symbol list:");
+        foreach (string symbol in _pdbSymbols.OrderBy(s => s))
+        {
+            Console.WriteLine(symbol);
+        }
+        Console.WriteLine("End of PDB public symbol list");
+    }
+
+    public bool ContainsSymbol(string symbolName) => _pdbSymbols.Any(s => s.Contains(symbolName));
+}
