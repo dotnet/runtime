b90a734e1c7d7d1e0a7e1a25ca054a3b76dd2dfa
Radek Zikmund <32671551+rzikm@users.noreply.github.com>
2022-09-05 00:07:01 -0700

    Add missing documentation to System.Net.Quic and System.Net.WebSockets (#74945)
    * Fix the inheritdoc cref on Quic* classes
    
    * Add missing documentation
    
    * WIP
    
    * Add summary and exception annotation to Stream overriden members
    
    * Code review feedback

--- src/libraries/System.Net.Quic/src/System/Net/Quic/QuicStream.cs
+++ src/libraries/System.Net.Quic/src/System/Net/Quic/QuicStream.cs
@@ -126,7 +126,7 @@ public sealed partial class QuicStream
     /// </summary>
     public Task WritesClosed => _sendTcs.GetFinalTask();
 
-    /// <inheritdoc cref="ToString"/>
+    /// <inheritdoc />
     public override string ToString() => _handle.ToString();
 
     /// <summary>
@@ -233,7 +233,7 @@ public sealed partial class QuicStream
         return valueTask;
     }
 
-    /// <inheritdoc cref="ReadAsync(System.Memory{byte},System.Threading.CancellationToken)"/>
+    /// <inheritdoc />
     public override async ValueTask<int> ReadAsync(Memory<byte> buffer, CancellationToken cancellationToken = default)
     {
         ObjectDisposedException.ThrowIf(_disposed == 1, this);
@@ -307,12 +307,12 @@ public sealed partial class QuicStream
         return totalCopied;
     }
 
-    /// <inheritdoc cref="WriteAsync(System.ReadOnlyMemory{byte},System.Threading.CancellationToken)"/>
+    /// <inheritdoc />
     public override ValueTask WriteAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default)
         => WriteAsync(buffer, completeWrites: false, cancellationToken);
 
 
-    /// <inheritdoc cref="WriteAsync(System.ReadOnlyMemory{byte},System.Threading.CancellationToken)"/>
+    /// <inheritdoc cref="WriteAsync(ReadOnlyMemory{byte}, CancellationToken)"/>
     /// <param name="buffer">The region of memory to write data from.</param>
     /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is <see cref="CancellationToken.None"/>.</param>
     /// <param name="completeWrites">Notifies the peer about gracefully closing the write side, i.e.: sends FIN flag with the data.</param>
@@ -484,8 +484,8 @@ public sealed partial class QuicStream
     private unsafe int HandleEventReceive(ref RECEIVE data)
     {
         ulong totalCopied = (ulong)_receiveBuffers.CopyFrom(
-            new ReadOnlySpan<QUIC_BUFFER>(data.Buffers, (int) data.BufferCount),
-            (int) data.TotalBufferLength,
+            new ReadOnlySpan<QUIC_BUFFER>(data.Buffers, (int)data.BufferCount),
+            (int)data.TotalBufferLength,
             data.Flags.HasFlag(QUIC_RECEIVE_FLAGS.FIN));
 
         if (NetEventSource.Log.IsEnabled())
