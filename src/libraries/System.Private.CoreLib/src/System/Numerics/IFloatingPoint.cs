d12a9543d894db87c1eb652cc9ee7fec8d5f2aba
Blokyk <32983140+Blokyk@users.noreply.github.com>
2022-09-08 19:10:31 +0200

    Fix `Exp10M1(TSelf)` and `Round(TSelf, MidpointRounding)` (#75063)
    * Don't ignore mode in IFloatingPoint.Round(TSelf, MipointRounding) DIM
    
    * Fix recursion in IExponentialFunctions.Exp10M1's DIM
    
    * Add tests for IExponentialFunctions.Exp10M1's DIM
    
    * Add tests for IFloatingPoint.Round's DIM
    
    * fix nits from tannergooding

--- src/libraries/System.Private.CoreLib/src/System/Numerics/IFloatingPoint.cs
+++ src/libraries/System.Private.CoreLib/src/System/Numerics/IFloatingPoint.cs
@@ -36,7 +36,7 @@ namespace System.Numerics
         /// <param name="x">The value to round.</param>
         /// <param name="mode">The mode under which <paramref name="x" /> should be rounded.</param>
         /// <returns>The result of rounding <paramref name="x" /> to the nearest integer using <paramref name="mode" />.</returns>
-        static virtual TSelf Round(TSelf x, MidpointRounding mode) => TSelf.Round(x, digits: 0, MidpointRounding.ToEven);
+        static virtual TSelf Round(TSelf x, MidpointRounding mode) => TSelf.Round(x, digits: 0, mode);
 
         /// <summary>Rounds a value to a specified number of fractional-digits using the default rounding mode (<see cref="MidpointRounding.ToEven" />).</summary>
         /// <param name="x">The value to round.</param>
