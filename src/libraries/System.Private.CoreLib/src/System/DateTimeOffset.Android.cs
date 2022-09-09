e611215e6973c3addb4f90268e8a988d0b9dbad0
Steve Pfister <steve.pfister@microsoft.com>
2022-09-09 16:11:15 -0400

    Feedback

--- src/libraries/System.Private.CoreLib/src/System/DateTimeOffset.Android.cs
+++ src/libraries/System.Private.CoreLib/src/System/DateTimeOffset.Android.cs
@@ -8,7 +8,7 @@ namespace System
     public readonly partial struct DateTimeOffset
     {
         // 0 == in process of being loaded, 1 == loaded
-        private static int s_androidTZDataLoaded = -1;
+        private static volatile int s_androidTZDataLoaded = -1;
 
         // Now on Android does the following
         // 1) quickly returning a fast path result when first called if the right AppContext data element is set
@@ -26,21 +26,18 @@ namespace System
             get
             {
                 DateTime utcDateTime = DateTime.UtcNow;
-                object? localDateTimeOffset;
 
                 if (s_androidTZDataLoaded == 1) // The background thread finished, the cache is loaded.
                 {
                     return ToLocalTime(utcDateTime, true);
                 }
-                else
+
+                object? localDateTimeOffset = AppContext.GetData("System.TimeZoneInfo.LocalDateTimeOffset");
+                if (localDateTimeOffset == null) // If no offset property provided through monovm app context, default
                 {
-                    localDateTimeOffset = AppContext.GetData("System.TimeZoneInfo.LocalDateTimeOffset");
-                    if (localDateTimeOffset == null) // If no offset property provided through monovm app context, default
-                    {
-                        // no need to create the thread, load tzdata now
-                        s_androidTZDataLoaded = 1;
-                        return ToLocalTime(utcDateTime, true);
-                    }
+                    // no need to create the thread, load tzdata now
+                    s_androidTZDataLoaded = 1;
+                    return ToLocalTime(utcDateTime, true);
                 }
 
                 // The cache isn't loaded yet.
@@ -57,7 +54,7 @@ namespace System
                         }
                         finally
                         {
-                            Volatile.Write(ref s_androidTZDataLoaded, 1);
+                            s_androidTZDataLoaded = 1;
                         }
                     }) { IsBackground = true }.Start();
                 }
