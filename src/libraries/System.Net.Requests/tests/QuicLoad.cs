3186a891f945e6bbb09d3c448ead574c47f77ea9
Jan Kotas <jkotas@microsoft.com>
2022-09-12 07:09:15 -0700

    Update src/libraries/System.Net.Requests/tests/QuicLoad.cs

--- src/libraries/System.Net.Requests/tests/QuicLoad.cs
+++ src/libraries/System.Net.Requests/tests/QuicLoad.cs
@@ -1,9 +1,9 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
 using System.Net.Quic;
 using System.Runtime.CompilerServices;
-
 
 namespace System.Net.Test.Common
 {
