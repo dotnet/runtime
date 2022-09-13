a92e44e85546b5921dac55d5977c4215ce08ec49
Thays Grazia <thaystg@gmail.com>
2022-09-13 17:43:39 -0300

    fixing setting a breakpoint in an invalid IL offset

--- src/mono/wasm/debugger/BrowserDebugProxy/MonoProxy.cs
+++ src/mono/wasm/debugger/BrowserDebugProxy/MonoProxy.cs
@@ -1727,6 +1727,9 @@ namespace Microsoft.WebAssembly.Diagnostics
                 return false;
 
             var breakpointId = await context.SdbAgent.SetBreakpoint(scope.Method.DebugId, ilOffset.Offset, token);
+            if (breakpointId == -1)
+                return false;
+
             context.TempBreakpointForSetNextIP = breakpointId;
             await SendResume(sessionId, token);
             return true;
