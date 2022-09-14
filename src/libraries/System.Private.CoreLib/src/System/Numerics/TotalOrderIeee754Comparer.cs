57d13041852e628440d891327ae7250dce70e3b5
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2022-09-14 12:33:23 +0800

    Update xmldoc

--- src/libraries/System.Private.CoreLib/src/System/Numerics/TotalOrderIeee754Comparer.cs
+++ src/libraries/System.Private.CoreLib/src/System/Numerics/TotalOrderIeee754Comparer.cs
@@ -8,35 +8,42 @@ namespace System.Numerics
 {
     /// <summary>
     /// Represents a comparison operation that compares floating-point numbers
-    /// with IEEE754 totalOrder semantic.
+    /// with IEEE 754 totalOrder semantic.
     /// </summary>
-    /// <typeparam name="T">The type of the numbers to be compared, must be an IEEE754 floating-point type.</typeparam>
+    /// <typeparam name="T">The type of the numbers to be compared, must be an IEEE 754 floating-point type.</typeparam>
     public class TotalOrderIeee754Comparer<T> : IComparer<T>, IEqualityComparer<T>
         where T : IFloatingPointIeee754<T>?
     {
         /// <summary>
-        /// Compares two numbers with IEEE754 totalOrder semantic and returns
+        /// Compares two numbers with IEEE 754 totalOrder semantic and returns
         /// a value indicating whether one is less than, equal to, or greater than the other.
         /// </summary>
         /// <param name="x">The first number to compare.</param>
         /// <param name="y">The second number to compare.</param>
         /// <returns>
         /// A signed integer that indicates the relative
-        /// values of <paramref name="x"/> and <paramref name="y"/>.
-        /// <list type="bullet">
+        /// values of <paramref name="x"/> and <paramref name="y"/>, as shown in the following table.
+        /// <list type="table">
+        ///   <listheader>
+        ///     <term> Value</term>
+        ///     <description> Meaning</description>
+        ///   </listheader>
         ///   <item>
-        ///     <description>If less than 0, <paramref name="x" /> is less than <paramref name="y" />.</desciption>
+        ///     <term> Less than zero</term>
+        ///     <description><paramref name = "x" /> is less than <paramref name="y" /></description>
         ///   </item>
         ///   <item>
-        ///     <description>If 0, <paramref name="x" /> equals <paramref name="y" />.</desciption>
+        ///     <term> Zero</term>
+        ///     <description><paramref name = "x" /> equals <paramref name="y" /></description>
         ///   </item>
         ///   <item>
-        ///     <description>If greater than 0, <paramref name="x" /> is greater than <paramref name="y" />.</desciption>
+        ///     <term> Greater than zero</term>
+        ///     <description><paramref name = "x" /> is greater than <paramref name="y" /></description>
         ///   </item>
         /// </list>
         /// </returns>
         /// <remarks>
-        /// IEEE754 specification defines totalOrder as a &lt;= semantic.
+        /// IEEE 754 specification defines totalOrder as a &lt;= semantic.
         /// totalOrder(x,y) is true corresponds to the result of this method &lt;= 0.
         /// </remarks>
         public int Compare(T? x, T? y)
@@ -115,7 +122,7 @@ namespace System.Numerics
                 }
                 else
                 {
-                    // T does not correctly implement IEEE754 semantics
+                    // T does not correctly implement IEEE 754 semantics
                     // return 0 for this case
                     return 0;
                 }
@@ -129,7 +136,7 @@ namespace System.Numerics
         /// <param name="y">The second number of type <typeparamref name="T"/> to compare.</param>
         /// <returns><see langword="true"/> if the specified numbers are equal; otherwise, <see langword="false"/>.</returns>
         /// <remarks>
-        /// There is no corresponding equals semantic with totalOrder defined by IEEE754 specification.
+        /// There is no corresponding equals semantic with totalOrder defined by IEEE 754 specification.
         /// This method returns <see langword="true"/> when <see cref="Compare(T?, T?)"/> returns 0.
         /// </remarks>
         public bool Equals(T? x, T? y) => Compare(x, y) == 0;
