9b394f30f65173131cf6438076e89b138714e898
Will Smith <lol.tihan@gmail.com>
2022-09-14 16:41:48 -0700

    Cleanup

--- src/tests/run.sh
+++ src/tests/run.sh
@@ -31,7 +31,6 @@ function print_usage {
     echo '  --long-gc                        : Runs the long GC tests'
     echo '  --useServerGC                    : Enable server GC for this test run'
     echo '  --ilasmroundtrip                 : Runs ilasm round trip on the tests'
-    echo '  --disasmchecks                   : Runs checks on the disassembly output'
     echo '  --link <ILlink>                  : Runs the tests after linking via ILlink'
     echo '  --printLastResultsOnly           : Print the results of the last run'
     echo '  --runincontext                   : Run each tests in an unloadable AssemblyLoadContext'
@@ -64,7 +63,6 @@ limitedCoreDumps=
 # Handle arguments
 verbose=0
 ilasmroundtrip=
-disasmchecks=
 printLastResultsOnly=
 runSequential=0
 runincontext=0
@@ -132,9 +130,6 @@ do
             ;;
         --ilasmroundtrip)
             ((ilasmroundtrip = 1))
-            ;;
-        --disasmchecks)
-            ((disasmchecks = 1))
             ;;
         --testRootDir=*)
             testRootDir=${i#*=}
@@ -241,11 +236,6 @@ fi
 if [[ -n "$ilasmroundtrip" ]]; then
     echo "Running Ilasm round trip"
     runtestPyArguments+=("--ilasmroundtrip")
-fi
-
-if [[ -n "$disasmchecks" ]]; then
-    echo "Running disasm checks"
-    runtestPyArguments+=("--disasmchecks")
 fi
 
 if (($verbose!=0)); then
