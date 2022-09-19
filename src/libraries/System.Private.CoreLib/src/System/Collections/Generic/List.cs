09a0fc505adb0781a4e2989907b2246be8de2938
viveknuna <vivek.nuna@gmail.com>
2022-09-19 17:41:14 +0530

    Update List.cs (#75831)
    * Update List.cs
    
    The comment was misleading, It should be removing the item not the item from given index
    
    * Update List.cs
    
    Changing the comment based on the review comment

--- src/libraries/System.Private.CoreLib/src/System/Collections/Generic/List.cs
+++ src/libraries/System.Private.CoreLib/src/System/Collections/Generic/List.cs
@@ -866,7 +866,7 @@ namespace System.Collections.Generic
             return Array.LastIndexOf(_items, item, index, count);
         }
 
-        // Removes the element at the given index. The size of the list is
+        // Removes the first occurrence given element, if found. The size of the list is
         // decreased by one.
         public bool Remove(T item)
         {
