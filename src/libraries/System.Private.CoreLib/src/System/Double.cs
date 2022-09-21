f526724ba5d25ee82834c6df6f3375323deed713
Drew Kersnar <18474647+dakersnar@users.noreply.github.com>
2022-09-21 13:19:57 -0500

    Add missing inheritdocs references to IParsable.TryParse (#75856)
    * Add missing inheritdocs for TryParse
    
    * Fix documentation for UInt128/Int128.ToString
    
    * Revert "Fix documentation for UInt128/Int128.ToString"
    
    This reverts commit f18cfa7ee95cea427b72e8da15e6af91da8b0391.
    
    * Apply suggestions from dotnet-api-docs review

--- src/libraries/System.Private.CoreLib/src/System/Double.cs
+++ src/libraries/System.Private.CoreLib/src/System/Double.cs
@@ -1411,6 +1411,7 @@ namespace System
         // IParsable
         //
 
+        /// <inheritdoc cref="IParsable{TSelf}.TryParse(string?, IFormatProvider?, out TSelf)" />
         public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, out double result) => TryParse(s, NumberStyles.Float | NumberStyles.AllowThousands, provider, out result);
 
         //
