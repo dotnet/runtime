43bec4c7352f866ab10056ade22c41647de85cde
SilentCC <silentchenyoo@gmail.com>
2022-09-23 23:48:23 +0800

    add unit test

---
+++ src/libraries/System.Text.Json/tests/System.Text.Json.FSharp.Tests/EnumTests.fs
@@ -0,0 +1,18 @@
+﻿module System.Text.Json.Tests.FSharp.EnumTests
+
+open System
+open System.Text.Json
+open Xunit
+
+[<Flags>]
+type MyEnum =
+  | ``There's a comma, in my name`` = 1
+  | ``There's a comma, even here`` = 2
+
+let enum = (MyEnum.``There's a comma, in my name`` ||| MyEnum.``There's a comma, even here``).ToString()
+
+[<Fact>]
+let ``Throw Exception If Enum Contains Comma`` () =
+    Assert.Throws<JsonException>(fun () -> JsonSerializer.Deserialize<MyEnum>(enum) |> ignore)
+
+
