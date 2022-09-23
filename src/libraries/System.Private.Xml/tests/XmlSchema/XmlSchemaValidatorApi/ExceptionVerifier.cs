008df76932e3d31f82a26c615d3d2a3bed085015
Filip Navara <filip.navara@gmail.com>
2022-09-23 15:51:03 +0200

    Fix test failure on NativeAOT with non-English locale

--- src/libraries/System.Private.Xml/tests/XmlSchema/XmlSchemaValidatorApi/ExceptionVerifier.cs
+++ src/libraries/System.Private.Xml/tests/XmlSchema/XmlSchemaValidatorApi/ExceptionVerifier.cs
@@ -134,13 +134,18 @@ namespace System.Xml.XmlSchemaValidatorApiTests
                     {
                         _output.WriteLine(e2.ToString());
                     }
+                    catch (PlatformNotSupportedException e3)
+                    {
+                        // NativeAOT doesn't support assembly loading
+                        _output.WriteLine(e3.ToString());
+                    }
                 }
             }
             catch (Exception e)
             {
                 _output.WriteLine("Exception: " + e.Message);
                 _output.WriteLine("Stack: " + e.StackTrace);
-                throw new VerifyException("Error while loading assembly");
+                throw new VerifyException("Error while loading assembly", e);
             }
 
             string[] resArray;
@@ -394,6 +399,10 @@ namespace System.Xml.XmlSchemaValidatorApiTests
     {
         public VerifyException(string msg)
             : base(msg)
+        { }
+
+        public VerifyException(string msg, Exception innerException)
+            : base(msg, innerException)
         { }
     }
 }
