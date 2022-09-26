dc3f5f8de7142b9b046074bbf752fd60e8d3b33e
Marek Fišera <mara@neptuo.com>
2022-09-26 14:41:29 +0200

    Unify string quotes.

--- src/mono/wasm/templates/templates/browser/main.js
+++ src/mono/wasm/templates/templates/browser/main.js
@@ -3,15 +3,15 @@
 
 import { dotnet } from './dotnet.js'
 
-const is_browser = typeof window != "undefined";
-if (!is_browser) throw new Error(`Expected to be running in a browser`);
+const is_browser = typeof window != 'undefined';
+if (!is_browser) throw new Error('Expected to be running in a browser');
 
 const { setModuleImports, getAssemblyExports, getConfig, runMainAndExit } = await dotnet
     .withDiagnosticTracing(false)
     .withApplicationArgumentsFromQuery()
     .create();
 
-setModuleImports("main.js", {
+setModuleImports('main.js', {
     window: {
         location: {
             href: () => globalThis.window.location.href
@@ -24,5 +24,5 @@ const exports = await getAssemblyExports(config.mainAssemblyName);
 const text = exports.MyClass.Greeting();
 console.log(text);
 
-document.getElementById("out").innerHTML = `${text}`;
-await runMainAndExit(config.mainAssemblyName, ["dotnet", "is", "great!"]);
+document.getElementById('out').innerHTML = text;
+await runMainAndExit(config.mainAssemblyName, ['dotnet', 'is', 'great!']);
