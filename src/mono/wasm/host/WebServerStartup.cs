009a252946d028f7fa76e13f0ede0a2ea925e2f1
Ankit Jain <radical@gmail.com>
2022-09-27 10:12:20 +0000

    WasmAppHost: improve how we get the urls for the webserver
    This could fail like:
    ```
    WasmAppHost --runtime-config /tmp/cc/bin/Debug/net7.0/browser-wasm/AppBundle/cc.runtimeconfig.json --forward-console
    Unhandled exception. System.InvalidOperationException: Failed to determine web server's IP address or port
       at Microsoft.WebAssembly.AppHost.WebServer.StartAsync(WebServerOptions options, ILogger logger, CancellationToken token)
       at Microsoft.WebAssembly.AppHost.BrowserHost.StartWebServerAsync(String appPath, Boolean forwardConsole, String[] urls, CancellationToken token)
       at Microsoft.WebAssembly.AppHost.BrowserHost.RunAsync(ILoggerFactory loggerFactory, CancellationToken token)
       at Microsoft.WebAssembly.AppHost.BrowserHost.InvokeAsync(CommonConfiguration commonArgs, ILoggerFactory loggerFactory, ILogger logger, CancellationToken token)
       at Microsoft.WebAssembly.AppHost.WasmAppHost.Main(String[] args)
       at Microsoft.WebAssembly.AppHost.WasmAppHost.<Main>(String[] args)
    ```
    
    Instead read the addresses on application lifetime's ApplicationStarted
    event.

--- src/mono/wasm/host/WebServerStartup.cs
+++ src/mono/wasm/host/WebServerStartup.cs
@@ -1,12 +1,19 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Net.WebSockets;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 #nullable enable
@@ -16,11 +23,17 @@ namespace Microsoft.WebAssembly.AppHost;
 internal sealed class WebServerStartup
 {
     private readonly IWebHostEnvironment _hostingEnvironment;
+    private ILogger? _logger;
 
     public WebServerStartup(IWebHostEnvironment hostingEnvironment) => _hostingEnvironment = hostingEnvironment;
 
-    public void Configure(IApplicationBuilder app, IOptions<WebServerOptions> optionsContainer)
+    public void Configure(IApplicationBuilder app,
+                          IOptions<WebServerOptions> optionsContainer,
+                          TaskCompletionSource<ServerURLs> realUrlsAvailableTcs,
+                          ILogger logger,
+                          IHostApplicationLifetime applicationLifetime)
     {
+        _logger = logger;
         var provider = new FileExtensionContentTypeProvider();
         provider.Mappings[".wasm"] = "application/wasm";
         provider.Mappings[".cjs"] = "text/javascript";
@@ -72,6 +85,43 @@ internal sealed class WebServerStartup
                 });
             });
         }
+
+        applicationLifetime.ApplicationStarted.Register(() =>
+        {
+            TaskCompletionSource<ServerURLs> tcs = realUrlsAvailableTcs;
+            try
+            {
+                ICollection<string>? addresses = app.ServerFeatures
+                                                    .Get<IServerAddressesFeature>()
+                                                    ?.Addresses;
+
+                string? ipAddress = null;
+                string? ipAddressSecure = null;
+                if (addresses is not null)
+                {
+                    ipAddress = GetHttpServerAddress(addresses, secure: false);
+                    ipAddressSecure = GetHttpServerAddress(addresses, secure: true);
+                }
+
+                if (ipAddress == null)
+                    tcs.SetException(new InvalidOperationException("Failed to determine web server's IP address or port"));
+                else
+                    tcs.SetResult(new ServerURLs(ipAddress, ipAddressSecure));
+            }
+            catch (Exception ex)
+            {
+                _logger?.LogError($"Failed to get urls for the webserver: {ex}");
+                tcs.TrySetException(ex);
+                throw;
+            }
+
+            static string? GetHttpServerAddress(ICollection<string> addresses, bool secure)
+                => addresses?
+                        .Where(a => a.StartsWith(secure ? "https:" : "http:", StringComparison.InvariantCultureIgnoreCase))
+                        .Select(a => new Uri(a))
+                        .Select(uri => uri.ToString())
+                        .FirstOrDefault();
+        });
 
         // app.UseEndpoints(endpoints =>
         // {
