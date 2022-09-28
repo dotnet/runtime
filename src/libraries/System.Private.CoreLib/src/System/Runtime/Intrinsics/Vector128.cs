30c210532abefc65bccdf0776b6b667c5a26ef10
Tanner Gooding <tagoo@outlook.com>
2022-09-27 20:12:32 -0700

    Ensure nint/nuint are handled in the relevant fallback paths

--- src/libraries/System.Private.CoreLib/src/System/Runtime/Intrinsics/Vector128.cs
+++ src/libraries/System.Private.CoreLib/src/System/Runtime/Intrinsics/Vector128.cs
@@ -60,6 +60,10 @@ namespace System.Runtime.Intrinsics
             {
                 return vector;
             }
+            else if (typeof(T) == typeof(nuint))
+            {
+                return vector;
+            }
             else if (typeof(T) == typeof(ushort))
             {
                 return vector;
@@ -708,6 +712,14 @@ namespace System.Runtime.Intrinsics
             else if (typeof(T) == typeof(long))
             {
                 return Create((long)(object)value).As<long, T>();
+            }
+            else if (typeof(T) == typeof(nint))
+            {
+                return Create((nint)(object)value).As<nint, T>();
+            }
+            else if (typeof(T) == typeof(nuint))
+            {
+                return Create((nuint)(object)value).As<nuint, T>();
             }
             else if (typeof(T) == typeof(sbyte))
             {
