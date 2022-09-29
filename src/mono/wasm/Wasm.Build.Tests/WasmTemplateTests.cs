5dcce55dfab1844230872d6793cdb457b7785225
Ankit Jain <radical@gmail.com>
2022-09-29 13:40:21 +0000

    Fix Wasm.Build.Tests failures
    - Use latest sdk for testing
    - Add dotnet8 feed, needed by blazorwasm templates
    
    - Workaround a bug in 7.0 templates which will be fixed by
    https://github.com/dotnet/runtime/pull/76373 . And this can be removed
    once we have packages with that.

--- src/mono/wasm/Wasm.Build.Tests/WasmTemplateTests.cs
+++ src/mono/wasm/Wasm.Build.Tests/WasmTemplateTests.cs
@@ -222,9 +222,19 @@ namespace Wasm.Build.Tests
 
             (int exitCode, string output) = RunProcess(s_buildEnv.DotNet, _testOutput, args: $"run --no-build -c {config} x y z", workingDir: _projectDir);
             Assert.Equal(42, exitCode);
-            Assert.Contains("args[0] = x", output);
-            Assert.Contains("args[1] = y", output);
-            Assert.Contains("args[2] = z", output);
+            if (extraNewArgs.Contains("-f net7.0"))
+            {
+                // Workaround for https://github.com/dotnet/runtime/issues/76201
+                Assert.Contains("args[0] = dotnet", output);
+                Assert.Contains("args[1] = is", output);
+                Assert.Contains("args[2] = great!", output);
+            }
+            else
+            {
+                Assert.Contains("args[0] = x", output);
+                Assert.Contains("args[1] = y", output);
+                Assert.Contains("args[2] = z", output);
+            }
         }
 
         public static TheoryData<bool, bool, string> TestDataForAppBundleDir()
