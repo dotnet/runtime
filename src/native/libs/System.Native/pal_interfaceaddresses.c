9224b932a106f36bda641a4b5ad5e6a65adeab76
Simon Rozsival <simon@rozsival.com>
2022-09-29 15:58:09 +0200

    Indent using spaces instead of tabs

--- src/native/libs/System.Native/pal_interfaceaddresses.c
+++ src/native/libs/System.Native/pal_interfaceaddresses.c
@@ -106,18 +106,18 @@ static inline uint8_t mask2prefix(uint8_t* mask, int length)
 // might not be available (e.g., in bionic used in Android before API 24) we need to mirror it here
 // so that we can dynamically load the getifaddrs function and use it.
 struct ifaddrs
-{
-	struct ifaddrs *ifa_next;
-	char *ifa_name;
-	unsigned int ifa_flags;
-	struct sockaddr *ifa_addr;
-	struct sockaddr *ifa_netmask;
-	union
-	{
-		struct sockaddr *ifu_broadaddr;
-		struct sockaddr *ifu_dstaddr;
-	} ifa_ifu;
-	void *ifa_data;
+    {
+    struct ifaddrs *ifa_next;
+    char *ifa_name;
+    unsigned int ifa_flags;
+    struct sockaddr *ifa_addr;
+    struct sockaddr *ifa_netmask;
+    union
+    {
+        struct sockaddr *ifu_broadaddr;
+        struct sockaddr *ifu_dstaddr;
+    } ifa_ifu;
+    void *ifa_data;
 };
 #endif
 
