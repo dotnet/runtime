a1569394adc94c8959973205df6e20dbb8ea92e2
Ankit Jain <radical@gmail.com>
2022-09-29 15:55:43 +0000

    Revert "WBT: workaround for https://github.com/dotnet/runtime/issues/76201"
    This reverts commit 33e367920f106c5e01c3f488174ff2b50471ff6f.

--- src/mono/wasm/Wasm.Build.Tests/WasmTemplateTests.cs
+++ src/mono/wasm/Wasm.Build.Tests/WasmTemplateTests.cs
@@ -222,19 +222,9 @@ namespace Wasm.Build.Tests
 
             (int exitCode, string output) = RunProcess(s_buildEnv.DotNet, _testOutput, args: $"run --no-build -c {config} x y z", workingDir: _projectDir);
             Assert.Equal(42, exitCode);
-            if (extraNewArgs.Contains("-f net7.0"))
-            {
-                // Workaround for https://github.com/dotnet/runtime/issues/76201
-                Assert.Contains("args[0] = dotnet", output);
-                Assert.Contains("args[1] = is", output);
-                Assert.Contains("args[2] = great!", output);
-            }
-            else
-            {
-                Assert.Contains("args[0] = x", output);
-                Assert.Contains("args[1] = y", output);
-                Assert.Contains("args[2] = z", output);
-            }
+            Assert.Contains("args[0] = x", output);
+            Assert.Contains("args[1] = y", output);
+            Assert.Contains("args[2] = z", output);
         }
 
         public static TheoryData<bool, bool, string> TestDataForAppBundleDir()
