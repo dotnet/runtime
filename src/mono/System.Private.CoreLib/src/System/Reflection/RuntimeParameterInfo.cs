0ff89a8f4e5592a4b09261ab06aedecfb936a114
Ivan Povazan <ivan.povazan@gmail.com>
2022-09-29 10:19:36 +0200

    Aligning Mono implementation based on new default value resolution rules

--- src/mono/System.Private.CoreLib/src/System/Reflection/RuntimeParameterInfo.cs
+++ src/mono/System.Private.CoreLib/src/System/Reflection/RuntimeParameterInfo.cs
@@ -131,53 +131,123 @@ namespace System.Reflection
             AttrsImpl = ParameterAttributes.None;
         }
 
-        public override
-        object? DefaultValue
+        private object? GetDefaultValueFromCustomAttributeData()
         {
-            get
+            foreach (CustomAttributeData attributeData in RuntimeCustomAttributeData.GetCustomAttributes(this))
             {
-                // In case of multiple CustomAttributes the following rules apply:
-                // - if declared type is DateTime, then DateTimeConstantAttribute is favoured over others
-                // - else if there is at least one CustomConstantAttribute, then it is favoured over others
-                // - else use the first attribute providing the default value
-                if (ClassImpl == typeof(DateTime) || ClassImpl == typeof(DateTime?))
+                Type attributeType = attributeData.AttributeType;
+                if (attributeType == typeof(DecimalConstantAttribute))
                 {
-                    /* default values for DateTime are encoded using a custom attribute */
-                    DateTimeConstantAttribute[] attrs = (DateTimeConstantAttribute[])GetCustomAttributes(typeof(DateTimeConstantAttribute), false);
-                    if (attrs.Length > 0)
-                        return attrs[0].Value;
+                    return GetRawDecimalConstant(attributeData);
                 }
+                else if (attributeType.IsSubclassOf(typeof(CustomConstantAttribute)))
+                {
+                    if (attributeType == typeof(DateTimeConstantAttribute))
+                    {
+                        return GetRawDateTimeConstant(attributeData);
+                    }
+                    return GetRawConstant(attributeData);
+                }
+            }
+            return DBNull.Value;
+        }
 
-                object? defaultValue = DefaultValueImpl;
-                if (defaultValue != null && (defaultValue.GetType() == typeof(DBNull) || defaultValue.GetType() == typeof(Missing)))
-                {
-                    /* look for a default value encoded using a custom attribute */
-                    CustomConstantAttribute[] attrs = (CustomConstantAttribute[])GetCustomAttributes(typeof(CustomConstantAttribute), false);
-                    if (attrs.Length > 0)
-                    {
-                        return attrs[0].Value;
-                    }
-                    else
-                    {
-                        /* default values for decimals are encoded using a custom attribute */
-                        DecimalConstantAttribute[] dattrs = (DecimalConstantAttribute[])GetCustomAttributes(typeof(DecimalConstantAttribute), false);
-                        if (dattrs.Length > 0)
-                            return dattrs[0].Value;
-                    }
-                }
-                return defaultValue;
+        private static decimal GetRawDecimalConstant(CustomAttributeData attr)
+        {
+            ParameterInfo[] parameters = attr.Constructor.GetParameters();
+            System.Collections.Generic.IList<CustomAttributeTypedArgument> args = attr.ConstructorArguments;
+
+            if (parameters[2].ParameterType == typeof(uint))
+            {
+                // DecimalConstantAttribute(byte scale, byte sign, uint hi, uint mid, uint low)
+                int low = (int)(uint)args[4].Value!;
+                int mid = (int)(uint)args[3].Value!;
+                int hi = (int)(uint)args[2].Value!;
+                byte sign = (byte)args[1].Value!;
+                byte scale = (byte)args[0].Value!;
+
+                return new decimal(low, mid, hi, sign != 0, scale);
+            }
+            else
+            {
+                // DecimalConstantAttribute(byte scale, byte sign, int hi, int mid, int low)
+                int low = (int)args[4].Value!;
+                int mid = (int)args[3].Value!;
+                int hi = (int)args[2].Value!;
+                byte sign = (byte)args[1].Value!;
+                byte scale = (byte)args[0].Value!;
+
+                return new decimal(low, mid, hi, sign != 0, scale);
             }
         }
+
+        private static DateTime GetRawDateTimeConstant(CustomAttributeData attr)
+        {
+            return new DateTime((long)attr.ConstructorArguments[0].Value!);
+        }
+
+        // We are relying only on named arguments for historical reasons
+        private static object? GetRawConstant(CustomAttributeData attr)
+        {
+            foreach (CustomAttributeNamedArgument namedArgument in attr.NamedArguments)
+            {
+                if (namedArgument.MemberInfo.Name.Equals("Value"))
+                    return namedArgument.TypedValue.Value;
+            }
+            return DBNull.Value;
+        }
+
+        private object? GetDefaultValueFromCustomAttributes()
+        {
+            object[] customAttributes = GetCustomAttributes(typeof(CustomConstantAttribute), false);
+            if (customAttributes.Length != 0)
+                return ((CustomConstantAttribute)customAttributes[0]).Value;
+
+            customAttributes = GetCustomAttributes(typeof(DecimalConstantAttribute), false);
+            if (customAttributes.Length != 0)
+                return ((DecimalConstantAttribute)customAttributes[0]).Value;
+
+            return DBNull.Value;
+        }
+
+        private object? GetDefaultValue(bool raw)
+        {
+            // Prioritize metadata constant over custom attribute constant
+            object? defaultValue = DefaultValueImpl;
+            if (defaultValue != null && (defaultValue.GetType() == typeof(DBNull) || defaultValue.GetType() == typeof(Missing)))
+            {
+                // If default value is not specified in metadata, look for it in custom attributes
+                // The resolution of default value is done by following these rules:
+                // 1. For RawDefaultValue, we pick the first custom attribute holding the constant value
+                //  in the following order: DecimalConstantAttribute, DateTimeConstantAttribute, CustomConstantAttribute
+                // 2. For DefaultValue, we first look for CustomConstantAttribute and pick the first occurrence.
+                //  If none is found, then we repeat the same process searching for DecimalConstantAttribute.
+                // IMPORTANT: Please note that there is a subtle difference in order custom attributes are inspected for
+                //  RawDefaultValue and DefaultValue.
+                defaultValue = raw ? GetDefaultValueFromCustomAttributeData() : GetDefaultValueFromCustomAttributes();
+            }
+
+            if (defaultValue == DBNull.Value && IsOptional)
+            {
+                // If the argument is marked as optional then the default value is Missing.Value.
+                defaultValue = Type.Missing;
+            }
+
+            return defaultValue;
+        }
+
+        public override object? DefaultValue { get => GetDefaultValue(false); }
 
         public override
         object? RawDefaultValue
         {
             get
             {
-                if (DefaultValue != null && DefaultValue.GetType().IsEnum)
-                    return ((Enum)DefaultValue).GetValue();
-                /*FIXME right now DefaultValue doesn't throw for reflection-only assemblies. Change this once the former is fixed.*/
-                return DefaultValue;
+                object? defaultValue = GetDefaultValue(true);
+                if (defaultValue != null && defaultValue.GetType().IsEnum)
+                    return ((Enum)defaultValue).GetValue();
+                /*FIXME right now defaultValue doesn't throw for reflection-only assemblies. Change this once the former is fixed.*/
+                return defaultValue;
             }
         }
 
