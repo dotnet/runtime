b33a6a1f946ca0be70ec2ce414e811b505b5fe7d
Jan Kotas <jkotas@microsoft.com>
2022-09-30 08:33:55 -0700

    Fix conv_u as well

--- src/coreclr/tools/Common/TypeSystem/IL/ILImporter.cs
+++ src/coreclr/tools/Common/TypeSystem/IL/ILImporter.cs
@@ -820,7 +820,7 @@ namespace Internal.IL
                         ImportStoreIndirect(WellKnownType.IntPtr);
                         break;
                     case ILOpcode.conv_u:
-                        ImportConvert(WellKnownType.UIntPtr, false, false);
+                        ImportConvert(WellKnownType.UIntPtr, false, true);
                         break;
                     case ILOpcode.prefix1:
                         opCode = (ILOpcode)(0x100 + ReadILByte());
