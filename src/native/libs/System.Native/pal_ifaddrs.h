769aff93bdc93a5c5a02056188fe85677d575909
Simon Rozsival <simon@rozsival.com>
2022-10-03 17:09:13 +0200

    Update the header file

--- src/native/libs/System.Native/pal_ifaddrs.h
+++ src/native/libs/System.Native/pal_ifaddrs.h
@@ -8,21 +8,14 @@
 #endif
 
 #if __ANDROID_API__ >= 24
-#error The pal_ifaddrs.h shim is not necessary when minimum SDK version is 24 or higher
+#error The pal_ifaddrs.h shim is only necessary for Android API 21-23 and it should be removed now that the minimum supported API level is 24 or higher
 #endif
 
 // Android doesn't include the getifaddrs and freeifaddrs functions in older Bionic libc (pre API 24).
 // This shim is a port of Xamarin Android's implementation of getifaddrs using Netlink.
 // https://github.com/xamarin/xamarin-android/blob/681887ebdbd192ce7ce1cd02221d4939599ba762/src/monodroid/jni/xamarin_getifaddrs.h
 
-#include "pal_compiler.h"
-#include "pal_config.h"
-#include "pal_types.h"
-
 #include <ifaddrs.h>
-#include <sys/cdefs.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
 
 int _netlink_getifaddrs (struct ifaddrs **ifap);
 void _netlink_freeifaddrs (struct ifaddrs *ifap);
