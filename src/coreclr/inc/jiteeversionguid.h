e0737c03940d13c0558b9ba366da86a8ab168dc8
EgorBo <egorbo@gmail.com>
2022-10-05 02:24:35 +0200

    Regenerate generated files (JIT-EE, CRST)

--- src/coreclr/inc/jiteeversionguid.h
+++ src/coreclr/inc/jiteeversionguid.h
@@ -43,11 +43,11 @@ typedef const GUID *LPCGUID;
 #define GUID_DEFINED
 #endif // !GUID_DEFINED
 
-constexpr GUID JITEEVersionIdentifier = { /* 11b4ea58-c400-4c3d-995e-4e2f0676f6e8 */
-    0x11b4ea58,
-    0xc400,
-    0x4c3d,
-    {0x99, 0x5e, 0x4e, 0x2f, 0x6, 0x76, 0xf6, 0xe8}
+constexpr GUID JITEEVersionIdentifier = { /* 3f5e4630-b29a-4aeb-bab7-07bdff43a156 */
+    0x3f5e4630,
+    0xb29a,
+    0x4aeb,
+    {0xba, 0xb7, 0x7, 0xbd, 0xff, 0x43, 0xa1, 0x56}
   };
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
