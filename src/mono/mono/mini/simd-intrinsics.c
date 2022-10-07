9b7ac8dbf11cf3e09ea60edf154ce58beb0d4bf3
Radek Doulik <radekdoulik@gmail.com>
2022-10-07 17:14:26 +0200

    Fix after merge

--- src/mono/mono/mini/simd-intrinsics.c
+++ src/mono/mono/mini/simd-intrinsics.c
@@ -1301,9 +1301,9 @@ emit_sri_vector (MonoCompile *cfg, MonoMethod *cmethod, MonoMethodSignature *fsi
 	case SN_Dot: {
 		if (!is_element_type_primitive (fsig->params [0]))
 			return NULL;
+#if defined(TARGET_ARM64) || defined(TARGET_WASM)
 		int instc0 = type_enum_is_float (arg0_type) ? OP_FMUL : OP_IMUL;
 		MonoInst *pairwise_multiply = emit_simd_ins_for_sig (cfg, klass, OP_XBINOP, instc0, arg0_type, fsig, args);
-#if defined(TARGET_ARM64) || defined(TARGET_WASM)
 		return emit_sum_vector (cfg, fsig->params [0], arg0_type, pairwise_multiply);
 #elif defined(TARGET_AMD64)
 		MonoClass *arg_class = mono_class_from_mono_type_internal (fsig->params [0]);
