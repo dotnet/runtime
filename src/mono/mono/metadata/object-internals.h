0e5ddb47322b12be6a85e041266226bf06237870
Vlad Brezae <brezaevlad@gmail.com>
2022-10-07 19:41:30 +0300

    [mono] Remove old invoke code
    Which was still used only by mono_runtime_invoke_array

--- src/mono/mono/metadata/object-internals.h
+++ src/mono/mono/metadata/object-internals.h
@@ -1881,14 +1881,6 @@ mono_runtime_invoke_handle (MonoMethod *method, MonoObjectHandle obj, void **par
 void
 mono_runtime_invoke_handle_void (MonoMethod *method, MonoObjectHandle obj, void **params, MonoError* error);
 
-MonoObject*
-mono_runtime_try_invoke_array (MonoMethod *method, void *obj, MonoArray *params,
-			       MonoObject **exc, MonoError *error);
-
-MonoObject*
-mono_runtime_invoke_span_checked (MonoMethod *method, void *obj, MonoSpanOfObjects *params,
-				   MonoError *error);
-
 void*
 mono_compile_method_checked (MonoMethod *method, MonoError *error);
 
