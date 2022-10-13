107939071da867d0eb2b6447f22ea9e50f1ccfd1
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2022-10-13 19:28:56 +0200

    Address feedback

--- src/libraries/System.Collections.Concurrent/src/System/Collections/Concurrent/ConcurrentDictionary.cs
+++ src/libraries/System.Collections.Concurrent/src/System/Collections/Concurrent/ConcurrentDictionary.cs
@@ -555,7 +555,7 @@ namespace System.Collections.Concurrent
                         {
                             if (valueComparer.Equals(node._value, comparisonValue))
                             {
-                                if (!typeof(TValue).IsValueType || ConcurrentDictionaryTypeProps<TValue>.IsWriteAtomic)
+                                if (IsValueWriteAtomic)
                                 {
                                     node._value = newValue;
                                 }
@@ -894,7 +894,7 @@ namespace System.Collections.Concurrent
                             // be written atomically, since lock-free reads may be happening concurrently.
                             if (updateIfExists)
                             {
-                                if (!typeof(TValue).IsValueType || ConcurrentDictionaryTypeProps<TValue>.IsWriteAtomic)
+                                if (IsValueWriteAtomic)
                                 {
                                     node._value = value;
                                 }
@@ -2124,6 +2124,15 @@ namespace System.Collections.Concurrent
             {
                 ReleaseLocks(0, locksAcquired);
             }
+        }
+
+        /// <summary>Determine whether writing a value is atomic.</summary>
+        private static bool IsValueWriteAtomic
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            // Do the reference type check up front here since JIT is unable to
+            // inline across the different shared generic dictionary.
+            get => !typeof(TValue).IsValueType || ConcurrentDictionaryTypeProps<TValue>.IsWriteAtomic;
         }
 
         /// <summary>
