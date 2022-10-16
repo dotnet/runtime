1ca32fb6c3823f7ede608c4bbb891acc2ab5f081
Tarek Mahmoud Sayed <tarekms@microsoft.com>
2022-10-16 10:19:22 -0700

    Fix the test OS filtering

--- src/libraries/System.Globalization/tests/IcuAppLocal/IcuAppLocal.cs
+++ src/libraries/System.Globalization/tests/IcuAppLocal/IcuAppLocal.cs
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Microsoft.DotNet.RemoteExecutor;
+using System.Diagnostics;
 using System.Reflection;
 using Xunit;
 
@@ -13,23 +15,36 @@ namespace System.Globalization.Tests
                                                          PlatformDetection.IsNotOSX &&
                                                          PlatformDetection.IsNotMobile &&
                                                          !PlatformDetection.IsAlpine &&
-                                                         !PlatformDetection.IsLinuxBionic;
+                                                         !PlatformDetection.IsLinuxBionic &&
+                                                         RemoteExecutor.IsSupported;
 
         [ConditionalFact(nameof(SupportIcuPackageDownload))]
-        [SkipOnPlatform(TestPlatforms.OSX | TestPlatforms.Browser | TestPlatforms.iOS | TestPlatforms.MacCatalyst | TestPlatforms.tvOS, "ICU package doesn't support these platforms.")]
         public void TestIcuAppLocal()
         {
-            Type? interopGlobalization = Type.GetType("Interop+Globalization, System.Private.CoreLib");
-            Assert.NotNull(interopGlobalization);
+            // We define this switch dynamically during the runtime using RemoteExecutor.
+            // The reason is, if we enable ICU app-local here, this test will compile and run
+            // on all supported OSs even the ICU NuGet package not have native bits support such OSs.
+            // Note, it doesn't matter if we have test case conditioned to not run on such OSs, because
+            // the test has to start running first before filtering the test cases and the globalization
+            // code will run at that time and will fail fast at that time.
 
-            MethodInfo? methodInfo = interopGlobalization.GetMethod("GetICUVersion", BindingFlags.NonPublic | BindingFlags.Static);
-            Assert.NotNull(methodInfo);
+            ProcessStartInfo psi = new ProcessStartInfo() { UseShellExecute = false };
+            psi.Environment.Add("DOTNET_SYSTEM_GLOBALIZATION_APPLOCALICU", "68.2.0.9");
 
-            // Assert the ICU version 0x44020009 is 68.2.0.9
-            Assert.Equal(0x44020009, (int)methodInfo.Invoke(null, null));
+            RemoteExecutor.Invoke(() =>
+            {
+                Type? interopGlobalization = Type.GetType("Interop+Globalization, System.Private.CoreLib");
+                Assert.NotNull(interopGlobalization);
 
-            // Now call globalization API to ensure the binding working without any problem.
-            Assert.Equal(-1, CultureInfo.GetCultureInfo("en-US").CompareInfo.Compare("sample\u0000", "Sample\u0000", CompareOptions.IgnoreSymbols));
+                MethodInfo? methodInfo = interopGlobalization.GetMethod("GetICUVersion", BindingFlags.NonPublic | BindingFlags.Static);
+                Assert.NotNull(methodInfo);
+
+                // Assert the ICU version 0x44020009 is 68.2.0.9
+                Assert.Equal(0x44020009, (int)methodInfo.Invoke(null, null));
+
+                // Now call globalization API to ensure the binding working without any problem.
+                Assert.Equal(-1, CultureInfo.GetCultureInfo("en-US").CompareInfo.Compare("sample\u0000", "Sample\u0000", CompareOptions.IgnoreSymbols));
+            }, new RemoteInvokeOptions { StartInfo = psi }).Dispose();
         }
     }
 }
