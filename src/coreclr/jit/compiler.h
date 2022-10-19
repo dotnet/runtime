888ad2bab1fc7ae25b4e27cf31dbd817eeb306d5
Egor Bogatov <egorbo@gmail.com>
2022-10-20 00:09:43 +0200

    Fold 'static readonly single-field-struct' to constants (#77102)
    Co-authored-by: Andy Ayers <andya@microsoft.com>

--- src/coreclr/jit/compiler.h
+++ src/coreclr/jit/compiler.h
@@ -3632,7 +3632,8 @@ protected:
 
     GenTree* impInitClass(CORINFO_RESOLVED_TOKEN* pResolvedToken);
 
-    GenTree* impImportStaticReadOnlyField(uint8_t* buffer, int bufferSize, var_types valueType);
+    GenTree* impImportStaticReadOnlyField(CORINFO_FIELD_HANDLE field, CORINFO_CLASS_HANDLE ownerCls);
+    GenTree* impImportCnsTreeFromBuffer(uint8_t* buffer, var_types valueType);
 
     GenTree* impImportStaticFieldAccess(CORINFO_RESOLVED_TOKEN* pResolvedToken,
                                         CORINFO_ACCESS_FLAGS    access,
