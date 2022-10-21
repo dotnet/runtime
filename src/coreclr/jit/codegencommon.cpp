9755e9650745c032b927fd12ea6bc6fb8f67eb47
Brian Bohe <brianbohe@microsoft.com>
2022-10-21 14:52:08 -0700

    Extending variable live ranges in more cases
    When the emitter moved to the next group but has not
    emitted any instruction, and the variable died and
    becomes alive again, we would like to extend its range.

--- src/coreclr/jit/codegencommon.cpp
+++ src/coreclr/jit/codegencommon.cpp
@@ -8730,7 +8730,7 @@ void CodeGenInterface::VariableLiveKeeper::VariableLiveDescriptor::startLiveRang
 
     if (!m_VariableLiveRanges->empty() &&
         siVarLoc::Equals(&varLocation, &(m_VariableLiveRanges->back().m_VarLocation)) &&
-        m_VariableLiveRanges->back().m_EndEmitLocation.IsPreviousInsNum(emit))
+        m_VariableLiveRanges->back().m_EndEmitLocation.IsLessOneInsAway(emit))
     {
         JITDUMP("Extending debug range...\n");
 
