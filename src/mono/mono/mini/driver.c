58994cc1324d2a458d760b75267f5ce384b82c9f
Zoltan Varga <vargaz@gmail.com>
2022-10-22 18:19:26 -0400

    [mono] Avoid checking for exceptions after calls to Math icalls.
    Also add a way to turn off exception checking for arbitrary icalls.

--- src/mono/mono/mini/driver.c
+++ src/mono/mono/mini/driver.c
@@ -2021,6 +2021,7 @@ print_icall_table (void)
 
 	printf ("[\n{ \"klass\": \"\", \"icalls\": [");
 #define NOHANDLES(inner) inner
+#define NOHANDLES_FLAGS(inner,flags) inner
 #define HANDLES(id, name, func, ...)	printf ("\t,{ \"name\": \"%s\", \"func\": \"%s_raw\", \"handles\": true }\n", name, #func);
 #define HANDLES_REUSE_WRAPPER		HANDLES
 #define MONO_HANDLE_REGISTER_ICALL(...) /* nothing  */
