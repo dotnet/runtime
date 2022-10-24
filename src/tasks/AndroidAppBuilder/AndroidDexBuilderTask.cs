a4726a6fbf5a0a5b07cff255b42213429ef5b7a1
Simon Rozsival <simon@rozsival.com>
2022-10-24 12:09:55 +0200

    Implement AndroidDexBuilderTask

---
+++ src/tasks/AndroidAppBuilder/AndroidDexBuilderTask.cs
@@ -0,0 +1,59 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+public class AndroidDexBuilderTask : Task
+{
+    public ITaskItem[] JavaFiles { get; set; } = Array.Empty<ITaskItem>();
+
+    [Required]
+    public string OutputDir { get; set; } = ""!;
+
+    [Required]
+    public string DexFileName { get; set; } = ""!;
+
+    public string? AndroidSdk { get; set; }
+
+    public string? BuildApiLevel { get; set; }
+
+    public string? BuildToolsVersion { get; set; }
+
+    [Output]
+    public string DexFilePath { get; set; } = ""!;
+
+    public override bool Execute()
+    {
+        var androidSdk = new AndroidSdkHelper(
+            androidSdkPath: AndroidSdk,
+            buildApiLevel: BuildApiLevel,
+            buildToolsVersion: BuildToolsVersion);
+
+        var compiler = new JavaCompiler(Log, androidSdk, workingDir: OutputDir);
+        var dexBuilder = new DexBuilder(Log, androidSdk, workingDir: OutputDir);
+
+        var objDir = "obj";
+        var objPath = Path.Combine(OutputDir, objDir);
+        Directory.CreateDirectory(objPath);
+
+        try
+        {
+            foreach (var file in JavaFiles)
+            {
+                compiler.Compile(file.ItemSpec, outputDir: objDir);
+            }
+
+            dexBuilder.Build(inputDir: objDir, outputFileName: DexFileName);
+
+            DexFilePath = Path.Combine(OutputDir, DexFileName);
+            return true;
+        }
+        finally
+        {
+            Directory.Delete(objPath, recursive: true);
+        }
+    }
+}
---
+++ src/tasks/Common/JavaCompiler.cs
@@ -0,0 +1,29 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.IO;
+using Microsoft.Build.Utilities;
+
+internal sealed class JavaCompiler
+{
+    private readonly string _javaCompilerArgs;
+    private readonly string _workingDir;
+    private readonly TaskLoggingHelper _logger;
+
+    public JavaCompiler(
+        TaskLoggingHelper logger,
+        AndroidSdkHelper androidSdk,
+        string workingDir,
+        string javaVersion = "1.8")
+    {
+        _javaCompilerArgs = $"-classpath src -bootclasspath {androidSdk.AndroidJarPath} -source {javaVersion} -target {javaVersion}";
+        _workingDir = workingDir;
+        _logger = logger;
+    }
+
+    public void Compile(string javaSourceFile, string outputDir)
+    {
+        Utils.RunProcess(_logger, "javac", $"{_javaCompilerArgs} -d {outputDir} {javaSourceFile}", workingDir: _workingDir);
+    }
+}
