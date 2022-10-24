eb4482e4c8a2cf6f87fbe5c8a440935f0021030b
tlakollo <tlakaelel_axayakatl@outlook.com>
2022-10-24 16:20:53 -0700

    Format file headers as runtime

--- src/tools/linker/test/Mono.Linker.Tests/TestCases/Dependencies/WarningSuppressionExpectations2.cs
+++ src/tools/linker/test/Mono.Linker.Tests/TestCases/Dependencies/WarningSuppressionExpectations2.cs
@@ -1,4 +1,7 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics.CodeAnalysis;
 
 [assembly: UnconditionalSuppressMessage("ILLink", "IL2072", Scope = "member", Target = "M:Mono.Linker.Tests.Cases.Warnings.Dependencies.TriggerWarnings_Lib.get_Warning2")]
 [assembly: UnconditionalSuppressMessage("ILLink", "IL2072", Scope = "member", Target = "M:Mono.Linker.Tests.Cases.Warnings.Dependencies.TriggerWarnings_Lib.Main")]
