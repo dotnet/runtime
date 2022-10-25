16495dbcc31a63fbf07dd7294b058255230eed38
Manish Godse <61718172+mangod9@users.noreply.github.com>
2022-10-25 13:42:49 -0700

    statically linking GC PAL on linux (#76985)
    * statically linking GC PAL
    
    The GC PAL will be used for both coreclr and standalone GC on linux
    
    * fixing arm64 and nativeaot build breaks
    
    * macos build break and reducing renaming.
    
    * trying to remove numa support from PAL
    
    * one more rename to resolve MacOS break
    
    * delete pal numa code.
    
    * Adding missing madvise in GC PAL
    
    * added missing MADV_DONTDUMP calls.
    
    * CR feedback
    
    * undo (long long) cast in GetMemoryStatus
    
    * only invoke madvise on success.

--- src/coreclr/pal/src/include/pal/palinternal.h
+++ src/coreclr/pal/src/include/pal/palinternal.h
@@ -652,7 +652,7 @@ typedef enum _TimeConversionConstants
 }
 
 bool
-ReadMemoryValueFromFile(const char* filename, uint64_t* val);
+PAL_ReadMemoryValueFromFile(const char* filename, uint64_t* val);
 
 #ifdef __APPLE__
 bool
