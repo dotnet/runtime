fb09bfef48cefa3acfee092d1d27a461d1b9a6dc
Nathan Ricci <naricc@microsoft.com>
2022-10-26 14:47:20 -0400

    Updated name in .h file.

--- src/mono/mono/metadata/marshal-lightweight.h
+++ src/mono/mono/metadata/marshal-lightweight.h
@@ -10,6 +10,6 @@ MONO_API void
 mono_marshal_lightweight_init (void);
 
 gboolean
-mono_marshal_did_embedder_request_ilgen_callbacks (void);
+mono_marshal_is_ilgen_requested (void);
 
 #endif // __MONO_MARSHAL_LIGHTWEIGHT_H__
