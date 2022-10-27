0f79e9c4a22254cd537cbad181fc1f2d8c5e2ca6
tlakollo <tlakaelel_axayakatl@outlook.com>
2022-10-27 13:43:16 -0700

    Format file headers as runtime

--- src/tools/linker/test/ILLink.RoslynAnalyzer.Tests/TestCaseUtils.cs
+++ src/tools/linker/test/ILLink.RoslynAnalyzer.Tests/TestCaseUtils.cs
@@ -1,5 +1,5 @@
-// Copyright (c) .NET Foundation and contributors. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
 using System.Collections.Generic;
@@ -7,7 +7,6 @@ using System.Collections.Immutable;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
