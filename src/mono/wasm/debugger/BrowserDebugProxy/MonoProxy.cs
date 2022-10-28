cb3d9bba272ce9a9e14e1a7d4d4728e861beeac8
Thays Grazia <thaystg@gmail.com>
2022-10-28 17:57:00 -0300

    adressing @radical comments

--- src/mono/wasm/debugger/BrowserDebugProxy/MonoProxy.cs
+++ src/mono/wasm/debugger/BrowserDebugProxy/MonoProxy.cs
@@ -268,9 +268,7 @@ namespace Microsoft.WebAssembly.Diagnostics
                         _defaultPauseOnExceptions = pauseOnException;
                 }
                 // don't pass through DotnetDebugger.* messages to the browser
-                if (method.StartsWith("DotnetDebugger.", StringComparison.Ordinal))
-                    return true;
-                return false;
+                return method.StartsWith("DotnetDebugger.", StringComparison.OrdinalIgnoreCase);
             }
 
             switch (method)
@@ -570,9 +568,7 @@ namespace Microsoft.WebAssembly.Diagnostics
                     }
             }
             // don't pass through DotnetDebugger.* messages to the browser
-            if (method.StartsWith("DotnetDebugger.", StringComparison.Ordinal))
-                return true;
-            return false;
+            return method.StartsWith("DotnetDebugger.", StringComparison.OrdinalIgnoreCase);
         }
 
         private async Task<bool> ApplyUpdates(MessageId id, JObject args, CancellationToken token)
