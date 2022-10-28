79988a2b37147f7b9e73a392ac5cac37efaddaee
Thays Grazia <thaystg@gmail.com>
2022-10-28 17:47:36 -0300

    ignore messages from protocol extensions even for unknown context

--- src/mono/wasm/debugger/BrowserDebugProxy/MonoProxy.cs
+++ src/mono/wasm/debugger/BrowserDebugProxy/MonoProxy.cs
@@ -267,6 +267,9 @@ namespace Microsoft.WebAssembly.Diagnostics
                     if (pauseOnException != PauseOnExceptionsKind.Unset)
                         _defaultPauseOnExceptions = pauseOnException;
                 }
+                //ignore messages from protocol extensions even for unknown context
+                if (method.StartsWith("DotnetDebugger.", StringComparison.Ordinal))
+                    return true;
                 return false;
             }
 
