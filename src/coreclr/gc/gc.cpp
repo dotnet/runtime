b57ee3cb543100ef90a3affb9972eb101696b38d
Peter Sollich <petersol@microsoft.com>
2022-10-28 10:11:48 +0200

    Fix issue with spurious unproductive full GC which led to OOM. (#77478)
    Problem was that a BGC was in progress when the full GC was requested by setting the last_gc_before_oom flag, and at the end of the BGC we turned off the last_gc_before_oom flag.
    
    Fix is simply not to turn off the flag in the case of BGC.

--- src/coreclr/gc/gc.cpp
+++ src/coreclr/gc/gc.cpp
@@ -21711,7 +21711,10 @@ void gc_heap::gc1()
 #endif //BACKGROUND_GC
 #endif //MULTIPLE_HEAPS
 #ifdef USE_REGIONS
-    last_gc_before_oom = FALSE;
+    if (!(settings.concurrent))
+    {
+        last_gc_before_oom = FALSE;
+    }
 #endif //USE_REGIONS
 }
 
