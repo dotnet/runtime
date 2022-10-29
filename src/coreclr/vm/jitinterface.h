bfaf6ee831b12b1d394a4cdb47b67eaa1ccad050
Egor Bogatov <egorbo@gmail.com>
2022-10-29 17:29:45 +0200

    Update jitinterface.h

--- src/coreclr/vm/jitinterface.h
+++ src/coreclr/vm/jitinterface.h
@@ -574,7 +574,7 @@ protected:
     MethodDesc*             m_pMethodBeingCompiled;             // Top-level method being compiled
     Thread *                m_pThread;                          // Cached current thread for faster JIT-EE transitions
     CORJIT_FLAGS            m_jitFlags;
-    
+
     CORINFO_METHOD_HANDLE getMethodBeingCompiled()
     {
         LIMITED_METHOD_CONTRACT;
