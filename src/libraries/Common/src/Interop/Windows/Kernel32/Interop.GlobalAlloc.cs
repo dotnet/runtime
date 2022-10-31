22de912c032d169539f2c9cfc5886a7f78cb36ed
hangy <hangy@hangy.de>
2022-10-31 18:47:28 +0100

    Move Interop code from System.Drawings.Common to Common directory (#77565)
    * Move Library names to Common directory
    
    * Move ExtractAssociatedIcon P/Invoke to Interop.ExtractAssociatedIcon.cs
    
    * Move PrintDlg P/Invoke to Interop.PrintDlg.cs
    
    * Move Kernel32 P/Invoke to Interop.*.cs
    
    * Move Winspool P/Invoke to Interop.*.cs
    
    * Move User32 P/Invoke to Interop.*.cs
    
    * Move Gdi32 P/Invoke to Interop.*.cs

--- src/libraries/Common/src/Interop/Windows/Kernel32/Interop.GlobalAlloc.cs
+++ src/libraries/Common/src/Interop/Windows/Kernel32/Interop.GlobalAlloc.cs
@@ -3,17 +3,11 @@
 
 using System;
 using System.Runtime.InteropServices;
-#if NET7_0_OR_GREATER
-using System.Runtime.InteropServices.Marshalling;
-#endif
 
 internal static partial class Interop
 {
     internal static partial class Kernel32
     {
-        [LibraryImport(Libraries.Kernel32, SetLastError = true)]
-        public static partial int GetSystemDefaultLCID();
-
         [LibraryImport(Libraries.Kernel32, EntryPoint = "GlobalAlloc", SetLastError = true)]
         internal static partial IntPtr IntGlobalAlloc(int uFlags, UIntPtr dwBytes); // size should be 32/64bits compatible
 
@@ -21,16 +15,5 @@ internal static partial class Interop
         {
             return IntGlobalAlloc(uFlags, new UIntPtr(dwBytes));
         }
-
-        [LibraryImport(Libraries.Gdi32, SetLastError = true)]
-        internal static partial IntPtr SelectObject(
-#if NET7_0_OR_GREATER
-            [MarshalUsing(typeof(HandleRefMarshaller))]
-#endif
-            HandleRef hdc,
-#if NET7_0_OR_GREATER
-            [MarshalUsing(typeof(HandleRefMarshaller))]
-#endif
-            HandleRef obj);
     }
 }
