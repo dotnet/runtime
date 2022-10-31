5b34fb035480f55c9687bcee8a2f4698ecdce9bb
Parker Bibus <parkerbibus@microsoft.com>
2022-10-31 12:53:14 -0700

    Setup testing.

--- eng/testing/performance/performance-setup.ps1
+++ eng/testing/performance/performance-setup.ps1
@@ -126,7 +126,7 @@ if ($RunFromPerformanceRepo) {
     robocopy $SourceDirectory $PerformanceDirectory /E /XD $PayloadDirectory $SourceDirectory\artifacts $SourceDirectory\.git
 }
 else {
-    git clone --branch release/7.0 --depth 1 --quiet https://github.com/dotnet/performance $PerformanceDirectory
+    git clone --branch FixPerfRelease/7.0 --depth 1 --quiet https://github.com/LoopedBard3/performance $PerformanceDirectory
 }
 
 if($MonoDotnet -ne "")
