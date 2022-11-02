b16ce6f6616ee7832dcc68ce51eb72a6a1fda49c
TIHan <lol.tihan@gmail.com>
2022-11-01 17:59:55 -0700

    Update disasm tests

--- src/tests/JIT/opt/Remainder/IntRemainder.cs
+++ src/tests/JIT/opt/Remainder/IntRemainder.cs
@@ -8,15 +8,34 @@ namespace CodeGenTests
 {
     static class IntRemainder
     {
+        static int _fieldValue = 123;
+
         [MethodImpl(MethodImplOptions.NoInlining)]
-        static int Int32_RemainderByOne(int value)
+        static int Int32_RemainderByOne()
         {
+            // X64:      call CORINFO
+            // X64-NEXT: xor [[REG0:[a-z]+]], [[REG0]]
+
+            // ARM64:      bl CORINFO
+            // ARM64-NEXT: mov [[REG0:[a-z0-9]+]], wzr
+            return _fieldValue % 1;
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        static int Int32_RemainderByOneWithValue(int value)
+        {
+            // X64: xor [[REG0:[a-z]+]], [[REG0]]
+
+            // ARM64: mov [[REG0:[a-z0-9]+]], wzr
             return value % 1;
         }
 
         static int Main()
         {
-            if (Int32_RemainderByOne(-123) != 0)
+            if (Int32_RemainderByOne() != 0)
+                return 0;
+
+            if (Int32_RemainderByOneWithValue(-123) != 0)
                 return 0;
 
             return 100;
