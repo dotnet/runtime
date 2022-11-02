94c95d7defdfc59dcc808f64e9ebdcb11c16135e
Drew Kersnar <18474647+dakersnar@users.noreply.github.com>
2022-11-02 12:04:58 -0500

    Update comment

--- src/libraries/System.Runtime.Numerics/src/System/Numerics/BigInteger.cs
+++ src/libraries/System.Runtime.Numerics/src/System/Numerics/BigInteger.cs
@@ -3481,9 +3481,8 @@ namespace System.Numerics
 
             ulong result = 0;
 
-            // As explained in https://github.com/dotnet/runtime/issues/77720, both positive values and
-            // their two's-compliment negative representation will share the same TrailingZeroCount,
-            // so the stored sign of value does not matter
+            // Both positive values and their two's-compliment negative representation will share the same TrailingZeroCount,
+            // so the stored sign of value does not matter and both cases can be handled in the same way
 
             uint part = value._bits[0];
 
