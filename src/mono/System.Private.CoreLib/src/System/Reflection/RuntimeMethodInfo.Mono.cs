71daea4fe6903a5386a7e46306d0e27b12b9c291
Zoltan Varga <vargaz@gmail.com>
2022-11-05 05:10:55 -0400

    [corlib] Move some types from coreclr's MdImport.cs to the shared partition.

--- src/mono/System.Private.CoreLib/src/System/Reflection/RuntimeMethodInfo.Mono.cs
+++ src/mono/System.Private.CoreLib/src/System/Reflection/RuntimeMethodInfo.Mono.cs
@@ -39,40 +39,6 @@ using InteropServicesCallingConvention = System.Runtime.InteropServices.CallingC
 
 namespace System.Reflection
 {
-
-    [Flags]
-    internal enum PInvokeAttributes
-    {
-        NoMangle = 0x0001,
-
-        CharSetMask = 0x0006,
-        CharSetNotSpec = 0x0000,
-        CharSetAnsi = 0x0002,
-        CharSetUnicode = 0x0004,
-        CharSetAuto = 0x0006,
-
-        BestFitUseAssem = 0x0000,
-        BestFitEnabled = 0x0010,
-        BestFitDisabled = 0x0020,
-        BestFitMask = 0x0030,
-
-        ThrowOnUnmappableCharUseAssem = 0x0000,
-        ThrowOnUnmappableCharEnabled = 0x1000,
-        ThrowOnUnmappableCharDisabled = 0x2000,
-        ThrowOnUnmappableCharMask = 0x3000,
-
-        SupportsLastError = 0x0040,
-
-        CallConvMask = 0x0700,
-        CallConvWinapi = 0x0100,
-        CallConvCdecl = 0x0200,
-        CallConvStdcall = 0x0300,
-        CallConvThiscall = 0x0400,
-        CallConvFastcall = 0x0500,
-
-        MaxValue = 0xFFFF,
-    }
-
     internal struct MonoMethodInfo
     {
 #pragma warning disable 649
