054e203bbfc864329f79054492b8a8bbd4e30b3a
Zoltan Varga <vargaz@gmail.com>
2022-11-05 06:35:46 -0400

    Fix the nativeaot build.

--- src/libraries/System.Private.CoreLib/src/System/Reflection/Emit/SignatureHelper.cs
+++ src/libraries/System.Private.CoreLib/src/System/Reflection/Emit/SignatureHelper.cs
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#if !NATIVEAOT
+
 using System.Buffers.Binary;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
@@ -928,3 +930,5 @@ namespace System.Reflection.Emit
         #endregion
     }
 }
+
+#endif
