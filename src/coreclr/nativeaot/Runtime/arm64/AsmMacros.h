c2e4c86196a9fa901e4cfd556baea6cd8dbf9eaa
vsadov <8218165+VSadov@users.noreply.github.com>
2022-11-04 18:44:51 -0700

    import conditionally

--- src/coreclr/nativeaot/Runtime/arm64/AsmMacros.h
+++ src/coreclr/nativeaot/Runtime/arm64/AsmMacros.h
@@ -107,8 +107,14 @@ OFFSETOF__Thread__m_alloc_context__alloc_limit      equ OFFSETOF__Thread__m_rgbA
     EXTERN g_ephemeral_low
     EXTERN g_ephemeral_high
     EXTERN g_card_table
+
+#ifdef FEATURE_MANUALLY_MANAGED_CARD_BUNDLES
     EXTERN g_card_bundle_table
+#endif
+
+#ifdef FEATURE_USE_SOFTWARE_WRITE_WATCH_FOR_GC_HEAP
     EXTERN g_write_watch_table
+#endif
 
 
 ;; -----------------------------------------------------------------------------
