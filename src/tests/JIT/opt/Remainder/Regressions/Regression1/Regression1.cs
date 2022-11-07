9c1d6792baf03d91d3d0cd233665049403bdbc97
TIHan <lol.tihan@gmail.com>
2022-11-07 11:10:43 -0800

    Try catch the regression

--- src/tests/JIT/opt/Remainder/Regressions/Regression1/Regression1.cs
+++ src/tests/JIT/opt/Remainder/Regressions/Regression1/Regression1.cs
@@ -7,9 +7,12 @@ public class Program
     public static int Main()
     {
         // This should not assert.
-        
-        ushort vr10 = default(ushort);
-        bool vr11 = 0 < ((s_1[0, 0] * (uint)(0 / vr10)) % 1);
+        try
+        {
+            ushort vr10 = default(ushort);
+            bool vr11 = 0 < ((s_1[0, 0] * (uint)(0 / vr10)) % 1);
+        }
+        catch {}
 
         return 100;
     }
