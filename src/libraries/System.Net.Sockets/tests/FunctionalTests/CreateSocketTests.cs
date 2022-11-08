620b54d04891d7364f29fa0dd1d61dbed3a78654
Ilya <darpa@yandex.ru>
2022-11-08 21:42:10 +0500

    Remove using System

--- src/libraries/System.Net.Sockets/tests/FunctionalTests/CreateSocketTests.cs
+++ src/libraries/System.Net.Sockets/tests/FunctionalTests/CreateSocketTests.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
@@ -481,7 +480,7 @@ namespace System.Net.Sockets.Tests
                         }
                     }
                 }
-            }).WaitAsync(TestSettings.PassingTestTimeout); 
+            }).WaitAsync(TestSettings.PassingTestTimeout);
         }
 
         [DllImport("libc")]
