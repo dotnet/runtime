ae24cf68ae11b44318a0dc1f1cac35ee83a4b93d
Zoltan Varga <vargaz@gmail.com>
2022-11-08 02:33:02 -0500

    Exclude SignatureHelper.cs on nativeaot using a csproj conditional.

--- src/libraries/System.Private.CoreLib/src/System/Reflection/Emit/SignatureHelper.cs
+++ src/libraries/System.Private.CoreLib/src/System/Reflection/Emit/SignatureHelper.cs
@@ -1,8 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#if !NATIVEAOT
-
 using System.Buffers.Binary;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
@@ -930,5 +928,3 @@ namespace System.Reflection.Emit
         #endregion
     }
 }
-
-#endif
