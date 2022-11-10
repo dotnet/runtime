039594b6236b38350dd3e9fb2a09bc52f4af3d00
Parker Bibus <parkerbibus@microsoft.com>
2022-11-10 14:39:28 -0800

    Disabled Transitive Framework download when building tools and set the perf branch to be used to release/6.0.

--- eng/testing/performance/performance-setup.ps1
+++ eng/testing/performance/performance-setup.ps1
@@ -124,7 +124,7 @@ if ($RunFromPerformanceRepo) {
     robocopy $SourceDirectory $PerformanceDirectory /E /XD $PayloadDirectory $SourceDirectory\artifacts $SourceDirectory\.git
 }
 else {
-    git clone --branch main --depth 1 --quiet https://github.com/dotnet/performance $PerformanceDirectory
+    git clone --branch release/6.0 --depth 1 --quiet https://github.com/dotnet/performance $PerformanceDirectory
 }
 
 if($MonoDotnet -ne "")
