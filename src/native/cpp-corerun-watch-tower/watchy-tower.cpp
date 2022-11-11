e79ae6678d68dfbcb2dc5417912c2efaf7694e9d
Ivan Diaz <ivdiazsa@microsoft.com>
2022-11-10 16:47:54 -0800

    Parsing command-line arguments done.

--- src/native/cpp-corerun-watch-tower/watchy-tower.cpp
+++ src/native/cpp-corerun-watch-tower/watchy-tower.cpp
@@ -3,15 +3,30 @@
 // Will replace these includes with Microsoft .NET's ones if necessary afterwards :)
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
+
+struct configuration
+{
+    configuration() = default;
+
+    long int timeout;
+    const char *corerun_path;
+    const char *corerun_args;
+};
 
 static void display_usage();
-static bool parse_args(const int, const char *[]);
+static bool parse_args(const int, const char *[], configuration&);
 
 int main(const int argc, const char *argv[])
 {
-    if (!parse_args(argc, argv))
+    configuration config {};
+
+    if (!parse_args(argc, argv, config))
         return EXIT_FAILURE;
 
+    printf("Timeout Given: %ld\n", config.timeout);
+    printf("Corerun Path Given: %s\n", config.corerun_path);
+    printf("Corerun Arguments Given: %s\n", config.corerun_args);
     return EXIT_SUCCESS;
 }
 
@@ -21,7 +36,7 @@ static void display_usage()
     return ;
 }
 
-static bool parse_args(const int argc, const char *argv[])
+static bool parse_args(const int argc, const char *argv[], configuration& config)
 {
     if (argc < 2)
     {
@@ -31,7 +46,56 @@ static bool parse_args(const int argc, const char *argv[])
 
     for (int i = 1; i < argc; i++)
     {
-        printf("%s\n", argv[i]);
+        const char *arg = argv[i];
+        // printf("%s\n", argv[i]);
+
+        if (strcmp(arg, "-timeout") == 0 || strcmp(arg, "--timeout") == 0)
+        {
+            if (++i < argc)
+            {
+                config.timeout = strtol(argv[i], nullptr, 10);
+            }
+            else
+            {
+                fprintf(stderr, "Option '%s': Missing value in seconds.\n", arg);
+                return false;
+            }
+        }
+        else if (strcmp(arg, "-corerun") == 0 || strcmp(arg, "--corerun") == 0)
+        {
+            if (++i < argc)
+            {
+                config.corerun_path = argv[i];
+            }
+            else
+            {
+                fprintf(stderr, "Option '%s': Missing path to corerun executable.\n", arg);
+                return false;
+            }
+        }
+        else if (strcmp(arg, "-args") == 0 || strcmp(arg, "--args") == 0)
+        {
+            if (++i < argc)
+            {
+                config.corerun_args = argv[i];
+            }
+            else
+            {
+                fprintf(stderr, "Option '%s': Missing arguments for corerun.\n", arg);
+                return false;
+            }
+        }
+        else if (strcmp(arg, "-h") == 0 || strcmp(arg, "--h") == 0 || strcmp(arg, "-?") == 0)
+        {
+            display_usage();
+            return true;
+        }
+        else
+        {
+            fprintf(stderr, "Unknown option '%s'.\n", arg);
+            return false;
+        }
+
     }
 
     return true;
