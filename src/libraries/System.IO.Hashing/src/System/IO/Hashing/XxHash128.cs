91ce914a7f3ad958b3d1f1f74c0ec7a4bd7aa078
Alexandre Mutel <alexandre_mutel@live.com>
2022-11-12 08:46:55 +0100

    Update src/libraries/System.IO.Hashing/src/System/IO/Hashing/XxHash128.cs
    Co-authored-by: Stephen Toub <stoub@microsoft.com>

--- src/libraries/System.IO.Hashing/src/System/IO/Hashing/XxHash128.cs
+++ src/libraries/System.IO.Hashing/src/System/IO/Hashing/XxHash128.cs
@@ -1,7 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-// Based on the XXH3 implementation from https://github.com/Cyan4973/xxHash.
+// Based on the XXH128 implementation from https://github.com/Cyan4973/xxHash.
 
 using System.Buffers.Binary;
 using System.Diagnostics;
