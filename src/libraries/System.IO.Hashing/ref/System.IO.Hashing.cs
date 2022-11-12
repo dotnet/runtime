dbcd877039508cea1346b8329b07c031fead76d1
Alexandre Mutel <alexandre_mutel@live.com>
2022-11-12 14:18:50 +0100

    Add new methods to ref

--- src/libraries/System.IO.Hashing/ref/System.IO.Hashing.cs
+++ src/libraries/System.IO.Hashing/ref/System.IO.Hashing.cs
@@ -57,10 +57,12 @@ namespace System.IO.Hashing
         public XxHash128(long seed) : base (default(int)) { }
         public override void Append(System.ReadOnlySpan<byte> source) { }
         protected override void GetCurrentHashCore(System.Span<byte> destination) { }
+        public UInt128 GetCurrentHashAsUInt128() { throw null; }
         public static byte[] Hash(byte[] source) { throw null; }
         public static byte[] Hash(byte[] source, long seed) { throw null; }
         public static byte[] Hash(System.ReadOnlySpan<byte> source, long seed = (long)0) { throw null; }
         public static int Hash(System.ReadOnlySpan<byte> source, System.Span<byte> destination, long seed = (long)0) { throw null; }
+        public static UInt128 HashToUInt128(ReadOnlySpan<byte> source, long seed = 0) { throw null; }
         public override void Reset() { }
         public static bool TryHash(System.ReadOnlySpan<byte> source, System.Span<byte> destination, out int bytesWritten, long seed = (long)0) { throw null; }
     }
