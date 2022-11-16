ab796de99873b110f2b3e869f63e22a47ba45e47
Vladimir Sadov <vsadov@microsoft.com>
2022-11-16 10:48:20 -0800

    [NativeAOT] Address some COOP native helpers that could cause suspension pauses. (#78450)
    * Memmove<T> to shared source.
    
    * Delete RhpCopyObjectContents
    
    * RhCompareObjectContentsAndPadding is ok, but added an assert.
    
    * remove unused BulkMoveWithWriteBarrier in Augments

--- src/coreclr/nativeaot/System.Private.CoreLib/src/System/Object.NativeAot.cs
+++ src/coreclr/nativeaot/System.Private.CoreLib/src/System/Object.NativeAot.cs
@@ -26,9 +26,24 @@ namespace System
         }
 
         [Intrinsic]
-        protected object MemberwiseClone()
+        protected internal unsafe object MemberwiseClone()
         {
-            return RuntimeImports.RhMemberwiseClone(this);
+            object clone = this.GetMethodTable()->IsArray ?
+                RuntimeExports.RhNewArray(this.GetMethodTable(), Unsafe.As<Array>(this).Length) :
+                RuntimeExports.RhNewObject(this.GetMethodTable());
+
+            // copy contents of "this" to the clone
+
+            nuint byteCount = RuntimeHelpers.GetRawObjectDataSize(this);
+            ref byte src = ref this.GetRawData();
+            ref byte dst = ref clone.GetRawData();
+
+            if (this.GetMethodTable()->HasGCPointers)
+                Buffer.BulkMoveWithWriteBarrier(ref dst, ref src, byteCount);
+            else
+                Buffer.Memmove(ref dst, ref src, byteCount);
+
+            return clone;
         }
     }
 }
