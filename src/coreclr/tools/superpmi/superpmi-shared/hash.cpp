545f1cc6df96af0781b73d1ecbb74cdf196b1686
EgorBo <egorbo@gmail.com>
2022-11-18 13:10:47 +0100

    fix compilation issues

--- src/coreclr/tools/superpmi/superpmi-shared/hash.cpp
+++ src/coreclr/tools/superpmi/superpmi-shared/hash.cpp
@@ -71,24 +71,26 @@ void MurmurHash3_128(const void* key, const size_t len, const uint32_t seed, voi
 
     switch (len & 15)
     {
-        case 15: k2 ^= static_cast<uint64_t>(tail[14]) << 48;
-        case 14: k2 ^= static_cast<uint64_t>(tail[13]) << 40;
-        case 13: k2 ^= static_cast<uint64_t>(tail[12]) << 32;
-        case 12: k2 ^= static_cast<uint64_t>(tail[11]) << 24;
-        case 11: k2 ^= static_cast<uint64_t>(tail[10]) << 16;
-        case 10: k2 ^= static_cast<uint64_t>(tail[9]) << 8;
+        case 15: k2 ^= static_cast<uint64_t>(tail[14]) << 48; FALLTHROUGH;
+        case 14: k2 ^= static_cast<uint64_t>(tail[13]) << 40; FALLTHROUGH;
+        case 13: k2 ^= static_cast<uint64_t>(tail[12]) << 32; FALLTHROUGH;
+        case 12: k2 ^= static_cast<uint64_t>(tail[11]) << 24; FALLTHROUGH;
+        case 11: k2 ^= static_cast<uint64_t>(tail[10]) << 16; FALLTHROUGH;
+        case 10: k2 ^= static_cast<uint64_t>(tail[9]) << 8;   FALLTHROUGH;
         case 9:  k2 ^= static_cast<uint64_t>(tail[8]) << 0;
             k2 *= c2; k2 = ROTL64(k2, 33); k2 *= c1; h2 ^= k2;
+            FALLTHROUGH;
 
-        case 8: k1 ^= static_cast<uint64_t>(tail[7]) << 56;
-        case 7: k1 ^= static_cast<uint64_t>(tail[6]) << 48;
-        case 6: k1 ^= static_cast<uint64_t>(tail[5]) << 40;
-        case 5: k1 ^= static_cast<uint64_t>(tail[4]) << 32;
-        case 4: k1 ^= static_cast<uint64_t>(tail[3]) << 24;
-        case 3: k1 ^= static_cast<uint64_t>(tail[2]) << 16;
-        case 2: k1 ^= static_cast<uint64_t>(tail[1]) << 8;
+        case 8: k1 ^= static_cast<uint64_t>(tail[7]) << 56; FALLTHROUGH;
+        case 7: k1 ^= static_cast<uint64_t>(tail[6]) << 48; FALLTHROUGH;
+        case 6: k1 ^= static_cast<uint64_t>(tail[5]) << 40; FALLTHROUGH;
+        case 5: k1 ^= static_cast<uint64_t>(tail[4]) << 32; FALLTHROUGH;
+        case 4: k1 ^= static_cast<uint64_t>(tail[3]) << 24; FALLTHROUGH;
+        case 3: k1 ^= static_cast<uint64_t>(tail[2]) << 16; FALLTHROUGH;
+        case 2: k1 ^= static_cast<uint64_t>(tail[1]) << 8;  FALLTHROUGH;
         case 1: k1 ^= static_cast<uint64_t>(tail[0]) << 0;
             k1 *= c1; k1 = ROTL64(k1, 31); k1 *= c2; h1 ^= k1;
+            break;
     }
 
     // finalization
