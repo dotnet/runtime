fba9b9fe17e87d61124e52f2d4b40c78e7171c29
Kunal Pathak <Kunal.Pathak@microsoft.com>
2022-11-17 23:12:09 -0800

    Also check for isUsedFromSpillTemp()`

--- src/coreclr/jit/codegenxarch.cpp
+++ src/coreclr/jit/codegenxarch.cpp
@@ -6459,7 +6459,8 @@ void CodeGen::genCompareFloat(GenTree* treeNode)
     // Are we evaluating this into a register?
     if (targetReg != REG_NA)
     {
-        if ((condition.GetCode() == GenCondition::FNEU) && (op1->GetRegNum() == op2->GetRegNum()))
+        if ((condition.GetCode() == GenCondition::FNEU) && (op1->GetRegNum() == op2->GetRegNum()) &&
+            !op1->isUsedFromSpillTemp() && !op2->isUsedFromSpillTemp())
         {
             // For floating point, `x != x` is a common way of
             // checking for NaN. So, in the case where both
