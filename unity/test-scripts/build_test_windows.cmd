0236a64f4318cc0ace88ba0f17f4c4a21d5c1d75
Josh Peterson <joshuap@unity3d.com>
2022-11-22 14:48:12 -0500

    Add support for debug and release testing for Windows on CI
    It is often useful to use the CoreCLR runtime in debug in Unity. To
    ensure that it continues to work as expected in debug, we need to run
    tests for it on CI in debug.
    
    Modify the build and test scripts for Windows so that they accept
    arguments to indicate the architecture and configuration they should
    use.
    
    Adjust the Yamato configurations to build and run tests in both release
    (which was done in the past) and debug (which is new) configurations. In
    addition, centralize the Windows tests into one top-level Yamato
    configuration file.

---
+++ unity/test-scripts/build_test_windows.cmd
@@ -0,0 +1,23 @@
+rem Usage: build_test_windows.cmd <Architecture> <Configuration>
+rem
+rem   Architecture is one of x86 or x64 - x64 is the default if not specified
+rem   Configuration is one of Debug or Release - Release is the default is not specified
+rem
+rem To specify Configuration, Architecture must be specified as well.
+
+if "%~1"=="" goto :default_architecture
+set architecture=%1
+goto :set_configuration
+:default_architecture
+set architecture=x64
+
+:set_configuration
+if "%~2"=="" goto :default_configuration
+set configuration=%2
+goto :run_build_and_test
+:default_configuration
+set configuration=Release
+
+:run_build_and_test
+../../.yamato/scripts/build_windows.cmd %architecture% %configuration%
+../../.yamato/scripts/test_windows.cmd %architecture% %configuration%
