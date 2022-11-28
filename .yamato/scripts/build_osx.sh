c2797aaa2b2e28b26ccfe8eb6acb2b6959d0761c
Josh Peterson <joshuap@unity3d.com>
2022-11-28 08:49:38 -0500

    Use the host machine architecture by default on macOS

--- .yamato/scripts/build_osx.sh
+++ .yamato/scripts/build_osx.sh
@@ -3,13 +3,19 @@ set -e
 
 # Usage: build_osx.cmd <Architecture> <Configuration>
 #
-#   Architecture is one of arm64 or x64 - x64 is the default if not specified
+#   Architecture is one of arm64 or x64 - the local machine architecture is the default if not specified
 #   Configuration is one of Debug or Release - Release is the default is not specified
 #
 # To specify Configuration, Architecture must be specified as well.
 
 if [ "$1" = "" ]; then
-    architecture=x64
+    # Since the architecture was not specified, check the architecture
+    # of the host machine and use it.
+    if [ `uname -p` = "i386" ]; then
+        architecture=x64
+    else
+        architecture=arm64
+    fi
 else
     architecture=$1
 fi
