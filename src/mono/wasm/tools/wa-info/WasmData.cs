9d4b74a2a3a32d12e7e84591da64813cee9ed108
Radek Doulik <radekdoulik@gmail.com>
2022-11-28 17:29:11 +0100

    [wa-info] Add forgotten file

---
+++ src/mono/wasm/tools/wa-info/WasmData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAssemblyInfo
+{
+    enum DataMode
+    {
+        Active = 0,
+        Passive = 1,
+        ActiveMemory = 2,
+    }
+}
