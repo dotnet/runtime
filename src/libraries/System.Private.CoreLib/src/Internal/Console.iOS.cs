0a9159d04daf8b28f59f57d253ec3a4994525190
Milos Kotlar <kotlarmilos@gmail.com>
2022-11-29 18:34:45 +0100

    Internal.Console.Write uses unicode encoding for iOS

---
+++ src/libraries/System.Private.CoreLib/src/Internal/Console.iOS.cs
@@ -0,0 +1,31 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Text;
+
+namespace Internal
+{
+    public static partial class Console
+    {
+        public static unsafe void Write(string s)
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(s);
+            fixed (byte* pBytes = bytes)
+            {
+                Interop.Sys.Log(pBytes, bytes.Length);
+            }
+        }
+        public static partial class Error
+        {
+            public static unsafe void Write(string s)
+            {
+                byte[] bytes = Encoding.Unicode.GetBytes(s);
+                fixed (byte* pBytes = bytes)
+                {
+                    Interop.Sys.LogError(pBytes, bytes.Length);
+                }
+            }
+        }
+    }
+}
