4561c63e16d6b3505b916a6d57f7d72e9590c190
Aaron Robinson <arobins@microsoft.com>
2022-11-30 14:09:25 -0800

    Fix buffer overflow during encoding.

--- src/coreclr/vm/stringliteralmap.cpp
+++ src/coreclr/vm/stringliteralmap.cpp
@@ -441,12 +441,18 @@ static void LogStringLiteral(_In_z_ const char* action, EEStringData *pStringDat
     STATIC_CONTRACT_GC_NOTRIGGER;
     STATIC_CONTRACT_FORBID_FAULT;
 
+    ULONG length = pStringData->GetCharCount();
+    length = min(length, 128);
+    WCHAR *szString = (WCHAR *)_alloca((length + 1) * sizeof(WCHAR));
+    memcpyNoGCRefs((void*)szString, (void*)pStringData->GetStringBuffer(), length * sizeof(WCHAR));
+    szString[length] = '\0';
+
     LPCUTF8 strUtf8 = "<Failed to convert to UTF8>";
-    MAKE_UTF8PTR_FROMWIDE_NOTHROW(cvtStr, pStringData->GetStringBuffer());
+    MAKE_UTF8PTR_FROMWIDE_NOTHROW(cvtStr, szString);
     if (cvtStr != NULL)
         strUtf8 = cvtStr;
 
-    LOG((LF_APPDOMAIN, LL_INFO10000, "String literal \"%s\" %s to Global map, %u characters\n", strUtf8, action, pStringData->GetCharCount()));
+    LOG((LF_APPDOMAIN, LL_INFO10000, "String literal \"%s\" %s to Global map, %u characters\n", strUtf8, action, length));
 }
 #endif
 
