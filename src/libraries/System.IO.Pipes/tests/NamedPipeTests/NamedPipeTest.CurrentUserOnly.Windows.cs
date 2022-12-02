8e07dd415ec6b0fb258a95adb18287271b68f9e5
David Cantú <dacantu@microsoft.com>
2022-12-02 11:17:44 -0600

    Wait for serverTask before asserting IsCompletedSuccessfully (#79071)
    * Wait for serverTask before asserting IsCompletedSuccessfully
    
    * Apply suggestions from code review
    
    Co-authored-by: Adam Sitnik <adam.sitnik@gmail.com>

--- src/libraries/System.IO.Pipes/tests/NamedPipeTests/NamedPipeTest.CurrentUserOnly.Windows.cs
+++ src/libraries/System.IO.Pipes/tests/NamedPipeTests/NamedPipeTest.CurrentUserOnly.Windows.cs
@@ -197,6 +197,7 @@ namespace System.IO.Pipes.Tests
                 {
                     // When CurrentUserOnly is only on client side and asks for ReadOnly access, the connection is not rejected
                     // but we get the UnauthorizedAccessException on the client regardless.
+                    Assert.True(serverTask.Wait(TimeSpan.FromSeconds(10)));
                     Assert.True(serverTask.IsCompletedSuccessfully);
                 }
                 else
