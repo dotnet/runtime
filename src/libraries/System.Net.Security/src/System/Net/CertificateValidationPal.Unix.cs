fceba8327555ac37577731dd4e3eca207e13a5b4
Tomas Weinfurt <tweinfurt@yahoo.com>
2022-12-05 21:25:48 -0800

    Apply suggestions from code review
    Co-authored-by: Radek Zikmund <32671551+rzikm@users.noreply.github.com>
    Co-authored-by: Simon Rozsival <simon@rozsival.com>

--- src/libraries/System.Net.Security/src/System/Net/CertificateValidationPal.Unix.cs
+++ src/libraries/System.Net.Security/src/System/Net/CertificateValidationPal.Unix.cs
@@ -96,7 +96,7 @@ namespace System.Net
         }
 
         // This is only called when we selected local client certificate.
-        // Currently this is only when OpenSSL needs it becaseu peer asked.
+        // Currently this is only when OpenSSL needs it because peer asked.
         internal static bool IsLocalCertificateUsed(SafeDeleteContext? _) => true;
 
         //
