5e780fc3cbd8b921d2c2ed0dab0c3401cad33f91
vitek-karas <10670590+vitek-karas@users.noreply.github.com>
2022-12-08 06:07:28 -0800

    Fixes remaining issues with DynamicDependency attribute and adds tests
    Ports over all DynamicDependencyAttribute tests we have in linker. All tests are now passing, two had to be partially disabled due to:
    * copyused testing which NativeAOT doesn't really support
    * known missing support for XML attribute files in AOT
    
    Product changes:
    * DynamicDependencyAttribute will produce a warning if it can't resolve the target of the dependency.
    * Support for DynamicDependencyAttribute on fields
    
    Small improvements to assembly checker - mainly ability to handle delegate types.

--- src/coreclr/tools/aot/ILCompiler.Compiler/Compiler/DependencyAnalysis/DynamicDependencyAttributeAlgorithm.cs
+++ src/coreclr/tools/aot/ILCompiler.Compiler/Compiler/DependencyAnalysis/DynamicDependencyAttributeAlgorithm.cs
@@ -4,11 +4,15 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection.Metadata;
 
 using Internal.TypeSystem;
 using Internal.TypeSystem.Ecma;
 
 using ILCompiler.Logging;
+
+using ILLink.Shared;
 
 using static ILCompiler.Dataflow.DynamicallyAccessedMembersBinder;
 
@@ -26,135 +30,187 @@ namespace ILCompiler.DependencyAnalysis
         {
             foreach (var attribute in method.GetDecodedCustomAttributes("System.Diagnostics.CodeAnalysis", "DynamicDependencyAttribute"))
             {
-                IEnumerable<TypeSystemEntity> members;
+                AddDependenciesDueToDynamicDependencyAttribute(ref dependencies, factory, method, method.OwningType, attribute);
+            }
+        }
 
-                static MetadataType Linkerify(TypeDesc type)
+        public static void AddDependenciesDueToDynamicDependencyAttribute(ref DependencyList dependencies, NodeFactory factory, EcmaField field)
+        {
+            foreach (var attribute in field.GetDecodedCustomAttributes("System.Diagnostics.CodeAnalysis", "DynamicDependencyAttribute"))
+            {
+                AddDependenciesDueToDynamicDependencyAttribute(ref dependencies, factory, field, field.OwningType, attribute);
+            }
+        }
+
+        private static void AddDependenciesDueToDynamicDependencyAttribute(
+            ref DependencyList dependencies,
+            NodeFactory factory,
+            TypeSystemEntity entity,
+            TypeDesc owningType,
+            CustomAttributeValue<TypeDesc> attribute)
+        {
+            IEnumerable<TypeSystemEntity> members;
+
+            static MetadataType Linkerify(TypeDesc type)
+            {
+                // IL Linker compatibility: illink will call Resolve() that will strip parameter types and genericness
+                // and operate on the definition.
+                while (type.IsParameterizedType)
+                    type = ((ParameterizedType)type).ParameterType;
+                return (MetadataType)type.GetTypeDefinition();
+            }
+
+            // First figure out the list of members that this maps to.
+            // These are the ways to specify the members:
+            //
+            // * A string that contains a documentation signature
+            // * DynamicallyAccessedMembers enum
+
+            var fixedArgs = attribute.FixedArguments;
+            TypeDesc targetType;
+
+            if (fixedArgs.Length > 0 && fixedArgs[0].Value is string sigFromAttribute)
+            {
+                if (fixedArgs.Length == 1)
                 {
-                    // IL Linker compatibility: illink will call Resolve() that will strip parameter types and genericness
-                    // and operate on the definition.
-                    while (type.IsParameterizedType)
-                        type = ((ParameterizedType)type).ParameterType;
-                    return (MetadataType)type.GetTypeDefinition();
+                    // DynamicDependencyAttribute(String)
+                    targetType = owningType;
                 }
-
-                // First figure out the list of members that this maps to.
-                // These are the ways to specify the members:
-                //
-                // * A string that contains a documentation signature
-                // * DynamicallyAccessedMembers enum
-
-                var fixedArgs = attribute.FixedArguments;
-                TypeDesc targetType;
-
-                if (fixedArgs.Length > 0 && fixedArgs[0].Value is string sigFromAttribute)
+                else if (fixedArgs.Length == 2 && fixedArgs[1].Value is TypeDesc typeFromAttribute)
                 {
-                    if (fixedArgs.Length == 1)
-                    {
-                        // DynamicDependencyAttribute(String)
-                        targetType = method.OwningType;
-                    }
-                    else if (fixedArgs.Length == 2 && fixedArgs[1].Value is TypeDesc typeFromAttribute)
-                    {
-                        // DynamicDependencyAttribute(String, Type)
-                        targetType = typeFromAttribute;
-                    }
-                    else if (fixedArgs.Length == 3 && fixedArgs[1].Value is string typeStringFromAttribute
-                        && fixedArgs[2].Value is string assemblyStringFromAttribute)
-                    {
-                        // DynamicDependencyAttribute(String, String, String)
-                        ModuleDesc asm = factory.TypeSystemContext.ResolveAssembly(new System.Reflection.AssemblyName(assemblyStringFromAttribute), throwIfNotFound: false);
-                        if (asm == null)
-                        {
-                            // _context.LogWarning($"Unresolved assembly '{dynamicDependency.AssemblyName}' in 'DynamicDependencyAttribute'", 2035, context);
-                            continue;
-                        }
-
-                        targetType = DocumentationSignatureParser.GetTypeByDocumentationSignature((IAssemblyDesc)asm, typeStringFromAttribute);
-                        if (targetType == null)
-                        {
-                            // _context.LogWarning ($"Unresolved type '{typeName}' in DynamicDependencyAttribute", 2036, context);
-                            continue;
-                        }
-                    }
-                    else
-                    {
-                        Debug.Fail("Did we introduce a new overload?");
-                        continue;
-                    }
-
-                    members = DocumentationSignatureParser.GetMembersByDocumentationSignature(Linkerify(targetType), sigFromAttribute, acceptName: true);
+                    // DynamicDependencyAttribute(String, Type)
+                    targetType = typeFromAttribute;
                 }
-                else if (fixedArgs.Length > 0 && fixedArgs[0].Value is int memberTypesFromAttribute)
+                else if (fixedArgs.Length == 3 && fixedArgs[1].Value is string typeStringFromAttribute
+                    && fixedArgs[2].Value is string assemblyStringFromAttribute)
                 {
-                    if (fixedArgs.Length == 2 && fixedArgs[1].Value is TypeDesc typeFromAttribute)
+                    // DynamicDependencyAttribute(String, String, String)
+                    ModuleDesc asm = factory.TypeSystemContext.ResolveAssembly(new System.Reflection.AssemblyName(assemblyStringFromAttribute), throwIfNotFound: false);
+                    if (asm == null)
                     {
-                        // DynamicDependencyAttribute(DynamicallyAccessedMemberTypes, Type)
-                        targetType = typeFromAttribute;
-                    }
-                    else if (fixedArgs.Length == 3 && fixedArgs[1].Value is string typeStringFromAttribute
-                        && fixedArgs[2].Value is string assemblyStringFromAttribute)
-                    {
-                        // DynamicDependencyAttribute(DynamicallyAccessedMemberTypes, String, String)
-                        ModuleDesc asm = factory.TypeSystemContext.ResolveAssembly(new System.Reflection.AssemblyName(assemblyStringFromAttribute), throwIfNotFound: false);
-                        if (asm == null)
-                        {
-                            // _context.LogWarning($"Unresolved assembly '{dynamicDependency.AssemblyName}' in 'DynamicDependencyAttribute'", 2035, context);
-                            continue;
-                        }
-
-                        targetType = DocumentationSignatureParser.GetTypeByDocumentationSignature((IAssemblyDesc)asm, typeStringFromAttribute);
-                        if (targetType == null)
-                        {
-                            // _context.LogWarning ($"Unresolved type '{typeName}' in DynamicDependencyAttribute", 2036, context);
-                            continue;
-                        }
-                    }
-                    else
-                    {
-                        Debug.Fail("Did we introduce a new overload?");
-                        continue;
+                        (factory.MetadataManager as UsageBasedMetadataManager)?.Logger?.LogWarning(
+                            new MessageOrigin(entity),
+                            DiagnosticId.UnresolvedAssemblyInDynamicDependencyAttribute,
+                            assemblyStringFromAttribute);
+                        return;
                     }
 
-                    members = Linkerify(targetType).GetDynamicallyAccessedMembers((DynamicallyAccessedMemberTypes)memberTypesFromAttribute);
+                    targetType = DocumentationSignatureParser.GetTypeByDocumentationSignature((IAssemblyDesc)asm, typeStringFromAttribute);
+                    if (targetType == null)
+                    {
+                        (factory.MetadataManager as UsageBasedMetadataManager)?.Logger?.LogWarning(
+                            new MessageOrigin(entity),
+                            DiagnosticId.UnresolvedTypeInDynamicDependencyAttribute,
+                            typeStringFromAttribute);
+                        return;
+                    }
                 }
                 else
                 {
                     Debug.Fail("Did we introduce a new overload?");
-                    continue;
+                    return;
                 }
 
-                const string reason = "DynamicDependencyAttribute";
+                members = DocumentationSignatureParser.GetMembersByDocumentationSignature(Linkerify(targetType), sigFromAttribute, acceptName: true);
 
-                // Now root the discovered members
-                foreach (var member in members)
+                if (!members.Any())
                 {
-                    switch (member)
+                    (factory.MetadataManager as UsageBasedMetadataManager)?.Logger?.LogWarning(
+                        new MessageOrigin(entity),
+                        DiagnosticId.NoMembersResolvedForMemberSignatureOrType,
+                        sigFromAttribute,
+                        targetType.GetDisplayName());
+                    return;
+                }
+            }
+            else if (fixedArgs.Length > 0 && fixedArgs[0].Value is int memberTypesFromAttribute)
+            {
+                if (fixedArgs.Length == 2 && fixedArgs[1].Value is TypeDesc typeFromAttribute)
+                {
+                    // DynamicDependencyAttribute(DynamicallyAccessedMemberTypes, Type)
+                    targetType = typeFromAttribute;
+                }
+                else if (fixedArgs.Length == 3 && fixedArgs[1].Value is string typeStringFromAttribute
+                    && fixedArgs[2].Value is string assemblyStringFromAttribute)
+                {
+                    // DynamicDependencyAttribute(DynamicallyAccessedMemberTypes, String, String)
+                    ModuleDesc asm = factory.TypeSystemContext.ResolveAssembly(new System.Reflection.AssemblyName(assemblyStringFromAttribute), throwIfNotFound: false);
+                    if (asm == null)
                     {
-                        case MethodDesc m:
-                            RootingHelpers.TryGetDependenciesForReflectedMethod(ref dependencies, factory, m, reason);
-                            break;
-                        case FieldDesc field:
-                            RootingHelpers.TryGetDependenciesForReflectedField(ref dependencies, factory, field, reason);
-                            break;
-                        case MetadataType nestedType:
-                            RootingHelpers.TryGetDependenciesForReflectedType(ref dependencies, factory, nestedType, reason);
-                            break;
-                        case PropertyPseudoDesc property:
-                            if (property.GetMethod != null)
-                                RootingHelpers.TryGetDependenciesForReflectedMethod(ref dependencies, factory, property.GetMethod, reason);
-                            if (property.SetMethod != null)
-                                RootingHelpers.TryGetDependenciesForReflectedMethod(ref dependencies, factory, property.SetMethod, reason);
-                            break;
-                        case EventPseudoDesc @event:
-                            if (@event.AddMethod != null)
-                                RootingHelpers.TryGetDependenciesForReflectedMethod(ref dependencies, factory, @event.AddMethod, reason);
-                            if (@event.RemoveMethod != null)
-                                RootingHelpers.TryGetDependenciesForReflectedMethod(ref dependencies, factory, @event.RemoveMethod, reason);
-                            break;
-                        default:
-                            Debug.Fail(member.GetType().ToString());
-                            break;
+                        (factory.MetadataManager as UsageBasedMetadataManager)?.Logger?.LogWarning(
+                            new MessageOrigin(entity),
+                            DiagnosticId.UnresolvedAssemblyInDynamicDependencyAttribute,
+                            assemblyStringFromAttribute);
+                        return;
                     }
+
+                    targetType = DocumentationSignatureParser.GetTypeByDocumentationSignature((IAssemblyDesc)asm, typeStringFromAttribute);
+                    if (targetType == null)
+                    {
+                        (factory.MetadataManager as UsageBasedMetadataManager)?.Logger?.LogWarning(
+                            new MessageOrigin(entity),
+                            DiagnosticId.UnresolvedTypeInDynamicDependencyAttribute,
+                            typeStringFromAttribute);
+                        return;
+                    }
+                }
+                else
+                {
+                    Debug.Fail("Did we introduce a new overload?");
+                    return;
+                }
+
+                members = Linkerify(targetType).GetDynamicallyAccessedMembers((DynamicallyAccessedMemberTypes)memberTypesFromAttribute);
+
+                if (!members.Any())
+                {
+                    (factory.MetadataManager as UsageBasedMetadataManager)?.Logger?.LogWarning(
+                        new MessageOrigin(entity),
+                        DiagnosticId.NoMembersResolvedForMemberSignatureOrType,
+                        memberTypesFromAttribute.ToString(),
+                        targetType.GetDisplayName());
+                    return;
+                }
+            }
+            else
+            {
+                Debug.Fail("Did we introduce a new overload?");
+                return;
+            }
+
+            Debug.Assert(members.Any());
+
+            const string reason = "DynamicDependencyAttribute";
+
+            // Now root the discovered members
+            foreach (var member in members)
+            {
+                switch (member)
+                {
+                    case MethodDesc m:
+                        RootingHelpers.TryGetDependenciesForReflectedMethod(ref dependencies, factory, m, reason);
+                        break;
+                    case FieldDesc field:
+                        RootingHelpers.TryGetDependenciesForReflectedField(ref dependencies, factory, field, reason);
+                        break;
+                    case MetadataType nestedType:
+                        RootingHelpers.TryGetDependenciesForReflectedType(ref dependencies, factory, nestedType, reason);
+                        break;
+                    case PropertyPseudoDesc property:
+                        if (property.GetMethod != null)
+                            RootingHelpers.TryGetDependenciesForReflectedMethod(ref dependencies, factory, property.GetMethod, reason);
+                        if (property.SetMethod != null)
+                            RootingHelpers.TryGetDependenciesForReflectedMethod(ref dependencies, factory, property.SetMethod, reason);
+                        break;
+                    case EventPseudoDesc @event:
+                        if (@event.AddMethod != null)
+                            RootingHelpers.TryGetDependenciesForReflectedMethod(ref dependencies, factory, @event.AddMethod, reason);
+                        if (@event.RemoveMethod != null)
+                            RootingHelpers.TryGetDependenciesForReflectedMethod(ref dependencies, factory, @event.RemoveMethod, reason);
+                        break;
+                    default:
+                        Debug.Fail(member.GetType().ToString());
+                        break;
                 }
             }
         }
