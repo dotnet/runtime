cf248d9c1672209b8a14edb48eaeb9fe09ff1d4f
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2022-12-09 14:51:20 +0900

    Update eng/native/configurecompiler.cmake

--- eng/native/configurecompiler.cmake
+++ eng/native/configurecompiler.cmake
@@ -480,8 +480,7 @@ if (CLR_CMAKE_HOST_UNIX)
   # We mark the function which needs exporting with DLLEXPORT
   add_compile_options(-fvisibility=hidden)
   
-  # Separate functions so linker can remove them. But not on tvOS because
-  # -ffunction-sections is not supported with -fembed-bitcode.
+  # Separate functions so linker can remove them.
   add_compile_options(-ffunction-sections)
 
   # Specify the minimum supported version of macOS
