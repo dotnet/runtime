f9a0ae69820b3cdd9473015e563b90366965fa88
Ankit Jain <radical@gmail.com>
2022-12-12 20:34:09 +0000

    WBT: Remove unused AppRefDir arguments

--- src/mono/wasm/Wasm.Build.Tests/BuildEnvironment.cs
+++ src/mono/wasm/Wasm.Build.Tests/BuildEnvironment.cs
@@ -77,21 +77,10 @@ namespace Wasm.Build.Tests
             {
                 DirectoryBuildPropsContents = s_directoryBuildPropsForWorkloads;
                 DirectoryBuildTargetsContents = s_directoryBuildTargetsForWorkloads;
-
-                var appRefDir = EnvironmentVariables.AppRefDir;
-                if (string.IsNullOrEmpty(appRefDir))
-                    throw new Exception($"Cannot test with workloads without AppRefDir environment variable being set");
-
-                DefaultBuildArgs = $" /p:AppRefDir={appRefDir}";
                 IsWorkload = true;
             }
             else
             {
-                var appRefDir = EnvironmentVariables.AppRefDir;
-                if (string.IsNullOrEmpty(appRefDir))
-                    throw new Exception($"Cannot test with workloads without AppRefDir environment variable being set");
-
-                DefaultBuildArgs = $" /p:AppRefDir={appRefDir}";
                 DirectoryBuildPropsContents = s_directoryBuildPropsForLocal;
                 DirectoryBuildTargetsContents = s_directoryBuildTargetsForLocal;
             }
