2997a8ed5ca2284542f42a8808d52f6507aca0c7
Andrew Graham <andrew.graham@arm.com>
2022-12-14 10:26:55 +0000

    Revert "Diagnostic change only, to confirm whether a theory is correct or"
    This reverts commit 4b0e51e87af44cbef5b9e643f9c5bf7aff48b548.

--- src/coreclr/jit/emit.h
+++ src/coreclr/jit/emit.h
@@ -2181,7 +2181,7 @@ private:
     insGroup* emitSavIG(bool emitAdd = false);
     void emitNxtIG(bool extend = false);
 
-    bool emitRemoveLastInstruction();
+    void emitRemoveLastInstruction();
 
     bool emitCurIGnonEmpty()
     {
