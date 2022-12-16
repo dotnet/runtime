5d177129d54a7f422ae9a3551c9b5d362fe9ca51
Michał Petryka <35800402+MichalPetryka@users.noreply.github.com>
2022-12-16 19:27:25 +0100

    Update MemoryMarshalGetArrayDataReference.cs

--- src/tests/JIT/Intrinsics/MemoryMarshalGetArrayDataReference.cs
+++ src/tests/JIT/Intrinsics/MemoryMarshalGetArrayDataReference.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Runtime.Intrinsics;
