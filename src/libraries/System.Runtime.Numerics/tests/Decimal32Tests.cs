61611be023cd7f32187acb8ee7a89d6b02719ce9
Drew Kersnar <18474647+dakersnar@users.noreply.github.com>
2022-12-21 17:35:38 -0600

    add Decimal32Tests.cs

---
+++ src/libraries/System.Runtime.Numerics/tests/Decimal32Tests.cs
@@ -0,0 +1,246 @@
+﻿
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Generic;
+using System.Globalization;
+using Xunit;
+
+namespace System.Numerics.Tests
+{
+    public partial class Decimal32Tests
+    {
+        public static IEnumerable<object[]> Parse_Valid_TestData()
+        {
+            NumberStyles defaultStyle = NumberStyles.Float | NumberStyles.AllowThousands;
+
+            NumberFormatInfo emptyFormat = NumberFormatInfo.CurrentInfo;
+
+            var dollarSignCommaSeparatorFormat = new NumberFormatInfo()
+            {
+                CurrencySymbol = "$",
+                CurrencyGroupSeparator = ","
+            };
+
+            var decimalSeparatorFormat = new NumberFormatInfo()
+            {
+                NumberDecimalSeparator = "."
+            };
+
+            NumberFormatInfo invariantFormat = NumberFormatInfo.InvariantInfo;
+
+            yield return new object[] { "-123", defaultStyle, null, -123.0f };
+            yield return new object[] { "0", defaultStyle, null, 0.0f };
+            yield return new object[] { "123", defaultStyle, null, 123.0f };
+            yield return new object[] { "  123  ", defaultStyle, null, 123.0f };
+            yield return new object[] { (567.89f).ToString(), defaultStyle, null, 567.89f };
+            yield return new object[] { (-567.89f).ToString(), defaultStyle, null, -567.89f };
+            yield return new object[] { "1E23", defaultStyle, null, 1E23f };
+
+            yield return new object[] { emptyFormat.NumberDecimalSeparator + "234", defaultStyle, null, 0.234f };
+            yield return new object[] { "234" + emptyFormat.NumberDecimalSeparator, defaultStyle, null, 234.0f };
+            yield return new object[] { new string('0', 13) + "65504" + emptyFormat.NumberDecimalSeparator, defaultStyle, null, 65504f };
+            yield return new object[] { new string('0', 14) + "65504" + emptyFormat.NumberDecimalSeparator, defaultStyle, null, 65504f };
+
+            // 2^11 + 1. Not exactly representable
+            yield return new object[] { "2049.0", defaultStyle, invariantFormat, 2048.0f };
+            yield return new object[] { "2049.000000000000001", defaultStyle, invariantFormat, 2050.0f };
+            yield return new object[] { "2049.0000000000000001", defaultStyle, invariantFormat, 2050.0f };
+            yield return new object[] { "2049.00000000000000001", defaultStyle, invariantFormat, 2050.0f };
+            yield return new object[] { "5.000000000000000004", defaultStyle, invariantFormat, 5.0f };
+            yield return new object[] { "5.0000000000000000004", defaultStyle, invariantFormat, 5.0f };
+            yield return new object[] { "5.004", defaultStyle, invariantFormat, 5.004f };
+            yield return new object[] { "5.004000000000000000", defaultStyle, invariantFormat, 5.004f };
+            yield return new object[] { "5.0040000000000000000", defaultStyle, invariantFormat, 5.004f };
+            yield return new object[] { "5.040", defaultStyle, invariantFormat, 5.04f };
+
+            yield return new object[] { "5004.000000000000000", defaultStyle, invariantFormat, 5004.0f };
+            yield return new object[] { "50040.0", defaultStyle, invariantFormat, 50040.0f };
+            yield return new object[] { "5004", defaultStyle, invariantFormat, 5004.0f };
+            yield return new object[] { "050040", defaultStyle, invariantFormat, 50040.0f };
+            yield return new object[] { "0.000000000000000000", defaultStyle, invariantFormat, 0.0f };
+            yield return new object[] { "0.005", defaultStyle, invariantFormat, 0.005f };
+            yield return new object[] { "0.0400", defaultStyle, invariantFormat, 0.04f };
+            yield return new object[] { "1200e0", defaultStyle, invariantFormat, 1200.0f };
+            yield return new object[] { "120100e-4", defaultStyle, invariantFormat, 12.01f };
+            yield return new object[] { "12010.00e-4", defaultStyle, invariantFormat, 1.201f };
+            yield return new object[] { "12000e-4", defaultStyle, invariantFormat, 1.2f };
+            yield return new object[] { "1200", defaultStyle, invariantFormat, 1200.0f };
+
+            yield return new object[] { (123.1f).ToString(), NumberStyles.AllowDecimalPoint, null, 123.1f };
+            yield return new object[] { (1000.0f).ToString("N0"), NumberStyles.AllowThousands, null, 1000.0f };
+
+            yield return new object[] { "123", NumberStyles.Any, emptyFormat, 123.0f };
+            yield return new object[] { (123.567f).ToString(), NumberStyles.Any, emptyFormat, 123.567f };
+            yield return new object[] { "123", NumberStyles.Float, emptyFormat, 123.0f };
+            yield return new object[] { "$1,000", NumberStyles.Currency, dollarSignCommaSeparatorFormat, 1000.0f };
+            yield return new object[] { "$1000", NumberStyles.Currency, dollarSignCommaSeparatorFormat, 1000.0f };
+            yield return new object[] { "123.123", NumberStyles.Float, decimalSeparatorFormat, 123.123f };
+            yield return new object[] { "(123)", NumberStyles.AllowParentheses, decimalSeparatorFormat, -123.0f };
+
+            yield return new object[] { "NaN", NumberStyles.Any, invariantFormat, float.NaN };
+            yield return new object[] { "Infinity", NumberStyles.Any, invariantFormat, float.PositiveInfinity };
+            yield return new object[] { "-Infinity", NumberStyles.Any, invariantFormat, float.NegativeInfinity };
+        }
+
+        [Theory]
+        [MemberData(nameof(Parse_Valid_TestData))]
+        public static void Parse(string value, NumberStyles style, IFormatProvider provider, float expectedFloat)
+        {
+            bool isDefaultProvider = provider == null || provider == NumberFormatInfo.CurrentInfo;
+            Half result;
+            Half expected = (Half)expectedFloat;
+            if ((style & ~(NumberStyles.Float | NumberStyles.AllowThousands)) == 0 && style != NumberStyles.None)
+            {
+                // Use Parse(string) or Parse(string, IFormatProvider)
+                if (isDefaultProvider)
+                {
+                    Assert.True(Half.TryParse(value, out result));
+                    Assert.True(expected.Equals(result));
+
+                    Assert.Equal(expected, Half.Parse(value));
+                }
+
+                Assert.True(expected.Equals(Half.Parse(value, provider: provider)));
+            }
+
+            // Use Parse(string, NumberStyles, IFormatProvider)
+            Assert.True(Half.TryParse(value, style, provider, out result));
+            Assert.True(expected.Equals(result) || (Half.IsNaN(expected) && Half.IsNaN(result)));
+
+            Assert.True(expected.Equals(Half.Parse(value, style, provider)) || (Half.IsNaN(expected) && Half.IsNaN(result)));
+
+            if (isDefaultProvider)
+            {
+                // Use Parse(string, NumberStyles) or Parse(string, NumberStyles, IFormatProvider)
+                Assert.True(Half.TryParse(value, style, NumberFormatInfo.CurrentInfo, out result));
+                Assert.True(expected.Equals(result));
+
+                Assert.True(expected.Equals(Half.Parse(value, style)));
+                Assert.True(expected.Equals(Half.Parse(value, style, NumberFormatInfo.CurrentInfo)));
+            }
+        }
+
+        public static IEnumerable<object[]> Parse_Invalid_TestData()
+        {
+            NumberStyles defaultStyle = NumberStyles.Float;
+
+            var dollarSignDecimalSeparatorFormat = new NumberFormatInfo();
+            dollarSignDecimalSeparatorFormat.CurrencySymbol = "$";
+            dollarSignDecimalSeparatorFormat.NumberDecimalSeparator = ".";
+
+            yield return new object[] { null, defaultStyle, null, typeof(ArgumentNullException) };
+            yield return new object[] { "", defaultStyle, null, typeof(FormatException) };
+            yield return new object[] { " ", defaultStyle, null, typeof(FormatException) };
+            yield return new object[] { "Garbage", defaultStyle, null, typeof(FormatException) };
+
+            yield return new object[] { "ab", defaultStyle, null, typeof(FormatException) }; // Hex value
+            yield return new object[] { "(123)", defaultStyle, null, typeof(FormatException) }; // Parentheses
+            yield return new object[] { (100.0f).ToString("C0"), defaultStyle, null, typeof(FormatException) }; // Currency
+
+            yield return new object[] { (123.456f).ToString(), NumberStyles.Integer, null, typeof(FormatException) }; // Decimal
+            yield return new object[] { "  " + (123.456f).ToString(), NumberStyles.None, null, typeof(FormatException) }; // Leading space
+            yield return new object[] { (123.456f).ToString() + "   ", NumberStyles.None, null, typeof(FormatException) }; // Leading space
+            yield return new object[] { "1E23", NumberStyles.None, null, typeof(FormatException) }; // Exponent
+
+            yield return new object[] { "ab", NumberStyles.None, null, typeof(FormatException) }; // Negative hex value
+            yield return new object[] { "  123  ", NumberStyles.None, null, typeof(FormatException) }; // Trailing and leading whitespace
+        }
+
+        [Theory]
+        [MemberData(nameof(Parse_Invalid_TestData))]
+        public static void Parse_Invalid(string value, NumberStyles style, IFormatProvider provider, Type exceptionType)
+        {
+            bool isDefaultProvider = provider == null || provider == NumberFormatInfo.CurrentInfo;
+            Half result;
+            if ((style & ~(NumberStyles.Float | NumberStyles.AllowThousands)) == 0 && style != NumberStyles.None && (style & NumberStyles.AllowLeadingWhite) == (style & NumberStyles.AllowTrailingWhite))
+            {
+                // Use Parse(string) or Parse(string, IFormatProvider)
+                if (isDefaultProvider)
+                {
+                    Assert.False(Half.TryParse(value, out result));
+                    Assert.Equal(default(Half), result);
+
+                    Assert.Throws(exceptionType, () => Half.Parse(value));
+                }
+
+                Assert.Throws(exceptionType, () => Half.Parse(value, provider: provider));
+            }
+
+            // Use Parse(string, NumberStyles, IFormatProvider)
+            Assert.False(Half.TryParse(value, style, provider, out result));
+            Assert.Equal(default(Half), result);
+
+            Assert.Throws(exceptionType, () => Half.Parse(value, style, provider));
+
+            if (isDefaultProvider)
+            {
+                // Use Parse(string, NumberStyles) or Parse(string, NumberStyles, IFormatProvider)
+                Assert.False(Half.TryParse(value, style, NumberFormatInfo.CurrentInfo, out result));
+                Assert.Equal(default(Half), result);
+
+                Assert.Throws(exceptionType, () => Half.Parse(value, style));
+                Assert.Throws(exceptionType, () => Half.Parse(value, style, NumberFormatInfo.CurrentInfo));
+            }
+        }
+
+        public static IEnumerable<object[]> Parse_ValidWithOffsetCount_TestData()
+        {
+            foreach (object[] inputs in Parse_Valid_TestData())
+            {
+                yield return new object[] { inputs[0], 0, ((string)inputs[0]).Length, inputs[1], inputs[2], inputs[3] };
+            }
+
+            const NumberStyles DefaultStyle = NumberStyles.Float | NumberStyles.AllowThousands;
+
+            yield return new object[] { "-123", 1, 3, DefaultStyle, null, (float)123 };
+            yield return new object[] { "-123", 0, 3, DefaultStyle, null, (float)-12 };
+            yield return new object[] { "1E23", 0, 3, DefaultStyle, null, (float)1E2 };
+            yield return new object[] { "123", 0, 2, NumberStyles.Float, new NumberFormatInfo(), (float)12 };
+            yield return new object[] { "$1,000", 1, 3, NumberStyles.Currency, new NumberFormatInfo() { CurrencySymbol = "$", CurrencyGroupSeparator = "," }, (float)10 };
+            yield return new object[] { "(123)", 1, 3, NumberStyles.AllowParentheses, new NumberFormatInfo() { NumberDecimalSeparator = "." }, (float)123 };
+            yield return new object[] { "-Infinity", 1, 8, NumberStyles.Any, NumberFormatInfo.InvariantInfo, float.PositiveInfinity };
+        }
+
+        [Theory]
+        [MemberData(nameof(Parse_ValidWithOffsetCount_TestData))]
+        public static void Parse_Span_Valid(string value, int offset, int count, NumberStyles style, IFormatProvider provider, float expectedFloat)
+        {
+            bool isDefaultProvider = provider == null || provider == NumberFormatInfo.CurrentInfo;
+            Half result;
+            Half expected = (Half)expectedFloat;
+            if ((style & ~(NumberStyles.Float | NumberStyles.AllowThousands)) == 0 && style != NumberStyles.None)
+            {
+                // Use Parse(string) or Parse(string, IFormatProvider)
+                if (isDefaultProvider)
+                {
+                    Assert.True(Half.TryParse(value.AsSpan(offset, count), out result));
+                    Assert.Equal(expected, result);
+
+                    Assert.Equal(expected, Half.Parse(value.AsSpan(offset, count)));
+                }
+
+                Assert.Equal(expected, Half.Parse(value.AsSpan(offset, count), provider: provider));
+            }
+
+            Assert.True(expected.Equals(Half.Parse(value.AsSpan(offset, count), style, provider)) || (Half.IsNaN(expected) && Half.IsNaN(Half.Parse(value.AsSpan(offset, count), style, provider))));
+
+            Assert.True(Half.TryParse(value.AsSpan(offset, count), style, provider, out result));
+            Assert.True(expected.Equals(result) || (Half.IsNaN(expected) && Half.IsNaN(result)));
+        }
+
+        [Theory]
+        [MemberData(nameof(Parse_Invalid_TestData))]
+        public static void Parse_Span_Invalid(string value, NumberStyles style, IFormatProvider provider, Type exceptionType)
+        {
+            if (value != null)
+            {
+                Assert.Throws(exceptionType, () => float.Parse(value.AsSpan(), style, provider));
+
+                Assert.False(float.TryParse(value.AsSpan(), style, provider, out float result));
+                Assert.Equal(0, result);
+            }
+        }
+    }
+}
+
