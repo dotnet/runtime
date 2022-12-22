df136a387de74ac3670a0782d10a1a528deaf597
Buyaa Namnan <bunamnan@microsoft.com>
2022-12-21 17:22:50 -0800

    Fix findings found with new CA1853 analyzer

--- src/libraries/System.Reflection.Metadata/src/System/Reflection/PortableExecutable/DebugDirectory/DebugDirectoryBuilder.cs
+++ src/libraries/System.Reflection.Metadata/src/System/Reflection/PortableExecutable/DebugDirectory/DebugDirectoryBuilder.cs
@@ -114,7 +114,12 @@ namespace System.Reflection.PortableExecutable
             }
 
             // We allow NUL characters to allow for padding for backward compat purposes.
-            if (pdbPath.Length == 0 || pdbPath.IndexOf('\0') == 0)
+            if (pdbPath.Length == 0 ||
+#if NETCOREAPP
+                pdbPath.StartsWith('\0'))
+#else
+                pdbPath.Length > 0 && pdbPath[0] == '\0')
+#endif
             {
                 Throw.InvalidArgument(SR.ExpectedNonEmptyString, nameof(pdbPath));
             }
