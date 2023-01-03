2218a861e8b68c4c75236923e04fa1d9edc3278a
vsadov <8218165+VSadov@users.noreply.github.com>
2023-01-02 17:17:54 -0800

    fix for a stress issue.

--- src/coreclr/nativeaot/Runtime/windows/PalRedhawkMinWin.cpp
+++ src/coreclr/nativeaot/Runtime/windows/PalRedhawkMinWin.cpp
@@ -503,13 +503,12 @@ REDHAWK_PALEXPORT void REDHAWK_PALAPI PalHijack(HANDLE hThread, _In_opt_ void* p
         // An APC can be interrupted by another one, do not queue more if one is pending.
         if (!pThread->IsSuspensionApcPending())
         {
+            pThread->SetSuspensionApcPending(true);
             pfnQueueUserAPC2Proc(
                 &ActivationHandler,
                 hThread,
                 (ULONG_PTR)pThreadToHijack,
                 SpecialUserModeApcWithContextFlags);
-
-            pThread->SetSuspensionApcPending(true);
         }
 
         return;
