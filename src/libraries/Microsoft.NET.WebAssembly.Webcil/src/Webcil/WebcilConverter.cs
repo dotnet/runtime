c1d5169ff268063ade190825a732103b2ea4c557
Aleksey Kliger <alklig@microsoft.com>
2023-01-06 13:24:34 -0500

    Add Microsoft.NET.WebAssembly.Webcil assembly project
    Mark it as shipping, but not shipping a nuget package.
    
    The idea is that it will be shipped along with the WasmAppBuilder
    msbuild task, and with the BrowserDebugProxy tool.

---
+++ src/libraries/Microsoft.NET.WebAssembly.Webcil/src/Webcil/WebcilConverter.cs
@@ -0,0 +1,20 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.IO;
+using System.Collections.Immutable;
+using System.Reflection.PortableExecutable;
+using System.Runtime.InteropServices;
+
+using Microsoft.Build.Utilities;
+
+namespace Microsoft.NET.WebAssembly.Webcil;
+
+public class WebcilConverter
+{
+    public WebcilConverter()
+    {
+    }
+}
+
