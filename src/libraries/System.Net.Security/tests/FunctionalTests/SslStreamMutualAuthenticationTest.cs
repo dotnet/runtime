7c652aad094f967d1f2c53888ba61474b4e0e03b
Radek Zikmund <radekzikmund@microsoft.com>
2023-01-06 09:49:30 +0100

    Remove unnecessary section in mutual auth test

--- src/libraries/System.Net.Security/tests/FunctionalTests/SslStreamMutualAuthenticationTest.cs
+++ src/libraries/System.Net.Security/tests/FunctionalTests/SslStreamMutualAuthenticationTest.cs
@@ -44,7 +44,7 @@ namespace System.Net.Security.Tests
         {
             TheoryData<bool, ClientCertSource> data = new();
 
-            foreach(var source in Enum.GetValues<ClientCertSource>())
+            foreach (var source in Enum.GetValues<ClientCertSource>())
             {
                 data.Add(true, source);
                 data.Add(false, source);
@@ -88,28 +88,17 @@ namespace System.Net.Security.Tests
 
                 await TestConfiguration.WhenAllOrAnyFailedWithTimeout(t1, t2);
 
-                if (Capability.IsTrustedRootCertificateInstalled())
+                if (clientCertificateRequired)
                 {
-                    // https://technet.microsoft.com/en-us/library/hh831771.aspx#BKMK_Changes2012R2
-                    // Starting with Windows 8, the "Management of trusted issuers for client authentication" has changed:
-                    // The behavior to send the Trusted Issuers List by default is off.
-                    //
-                    // In Windows 7 the Trusted Issuers List is sent within the Server Hello TLS record. This list is built
-                    // by the server using certificates from the Trusted Root Authorities certificate store.
-                    // The client side will use the Trusted Issuers List, if not empty, to filter proposed certificates.
-
-                    if (clientCertificateRequired)
-                    {
-                        Assert.True(client.IsMutuallyAuthenticated, "client.IsMutuallyAuthenticated");
-                        Assert.True(server.IsMutuallyAuthenticated, "server.IsMutuallyAuthenticated");
-                    }
-                    else
-                    {
-                        // Even though the certificate was provided, it was not requested by the server and thus the client
-                        // was not authenticated.
-                        Assert.False(client.IsMutuallyAuthenticated, "client.IsMutuallyAuthenticated");
-                        Assert.False(server.IsMutuallyAuthenticated, "server.IsMutuallyAuthenticated");
-                    }
+                    Assert.True(client.IsMutuallyAuthenticated, "client.IsMutuallyAuthenticated");
+                    Assert.True(server.IsMutuallyAuthenticated, "server.IsMutuallyAuthenticated");
+                }
+                else
+                {
+                    // Even though the certificate was provided, it was not requested by the server and thus the client
+                    // was not authenticated.
+                    Assert.False(client.IsMutuallyAuthenticated, "client.IsMutuallyAuthenticated");
+                    Assert.False(server.IsMutuallyAuthenticated, "server.IsMutuallyAuthenticated");
                 }
             }
         }
