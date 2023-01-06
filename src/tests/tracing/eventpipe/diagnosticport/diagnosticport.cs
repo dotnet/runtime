b501c974714d1e75b5c8bcdaba7f5094d7dffff6
Elinor Fung <elfung@microsoft.com>
2023-01-06 09:49:34 -0800

    Add tests

--- src/tests/tracing/eventpipe/diagnosticport/diagnosticport.cs
+++ src/tests/tracing/eventpipe/diagnosticport/diagnosticport.cs
@@ -407,7 +407,10 @@ namespace Tracing.Tests.DiagnosticPortValidation
                     IpcMessage response = IpcClient.SendMessage(stream, processInfoMessage);
                     Logger.logger.Log($"Received: [{response.Payload.Select(b => b.ToString("X2") + " ").Aggregate(string.Concat)}]");
                     ProcessInfo2 processInfo2 = ProcessInfo2.TryParse(response.Payload);
-                    Utils.Assert(String.IsNullOrEmpty(processInfo2.ManagedEntrypointAssemblyName));
+
+                    string expectedName = Assembly.GetExecutingAssembly().GetName().Name;
+                    Utils.Assert(expectedName.Equals(processInfo2.ManagedEntrypointAssemblyName),
+                        $"ManagedEntrypointAssemblyName must match. Expected: {expectedName}, Received: {processInfo2.ManagedEntrypointAssemblyName}");
 
                     // send resume command on this connection
                     var message = new IpcMessage(0x04,0x01);
