10747d938f8e3a08b2cfde7c3b192d12f002ba33
Ivan Diaz <ivdiazsa@microsoft.com>
2023-01-06 17:01:55 -0800

    Added placeholder for the XUnit log checker and it now builds with src/tests/build.sh

---
+++ src/tests/Common/XUnitLogChecker/XUnitLogChecker.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+public class XUnitLogChecker
+{
+    private static class Patterns
+    {
+        public const string OpenTag = @"(\B<\w+)|(\B<!\[CDATA\[)";
+        public const string CloseTag = @"(\B</\w+>)|(\]\]>)";
+    }
+
+    const int SUCCESS = 0;
+    const int FAILURE = -1;
+
+    static int Main(string[] args)
+    {
+        if (TryLoadXml(args[0]))
+        {
+            Console.WriteLine($"The given XML '{args[0]}' is well formed! Exiting...");
+            return SUCCESS;
+        }
+
+        return FAILURE;
+    }
+
+    static bool TryLoadXml(string xFile)
+    {
+        try
+        {
+            using (var xReader = XmlReader.Create(new StreamReader(xFile)))
+            {
+                var xDocument = new XmlDocument();
+                xDocument.Load(xReader);
+            }
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine($"The given XML {xFile} was malformed. Fixing now...");
+            return false;
+        }
+
+        return true;
+    }
+}
+
