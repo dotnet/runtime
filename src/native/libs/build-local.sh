3973f1180cbb4d39417080b4c190ce5641eb005d
Andrii Kurdiumov <kant2002@gmail.com>
2023-01-10 02:56:15 +0600

    Restore formatting

--- src/native/libs/build-local.sh
+++ src/native/libs/build-local.sh
@@ -16,12 +16,21 @@ TARGET_LIBRARY=$3
 
 if [ -d "$SHIM_SOURCE_DIR" ]; then
     LOCAL_SHIM_DIR="$INTERMEDIATE_OUTPUT_PATH"/libs/$TARGET_LIBRARY/build
-    mkdir -p "$LOCAL_SHIM_DIR" && cd "$LOCAL_SHIM_DIR"
-    if [ $? -ne 0 ]; then echo "build-local.sh::ERROR: Cannot use local build directory"; exit 1; fi
-    cmake -S "$SHIM_SOURCE_DIR/libs/$TARGET_LIBRARY/" -DLOCAL_BUILD:STRING=1 -DCLR_CMAKE_TARGET_UNIX:STRING=1
-    if [ $? -ne 0 ]; then echo "build-local.sh::ERROR: cmake failed"; exit 1; fi
-    make -j
-    if [ $? -ne 0 ]; then echo "build-local.sh::ERROR: Build failed"; exit 1; fi
+
+    if ! { mkdir -p "$LOCAL_SHIM_DIR" && cd "$LOCAL_SHIM_DIR"; }; then
+        echo "local_build.sh::ERROR: Cannot use local build directory"
+        exit 1
+    fi
+
+    if ! cmake -S "$SHIM_SOURCE_DIR/libs/$TARGET_LIBRARY/" -DLOCAL_BUILD:STRING=1 -DCLR_CMAKE_TARGET_UNIX:STRING=1; then
+        echo "local_build.sh::ERROR: cmake failed"
+        exit 1
+    fi
+
+    if ! make -j; then
+        echo "local_build.sh::ERROR: Build failed"
+        exit 1
+    fi
 fi
 
 exit 0
