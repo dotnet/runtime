e6599909feeb3a9814049423cdd72536e2da3a3c
Eric Erhardt <eric.erhardt@microsoft.com>
2023-01-08 20:26:15 -0600

    Add Intrinsic attribute back for Mono

--- src/libraries/System.Private.CoreLib/src/System/Runtime/CompilerServices/RuntimeFeature.NonNativeAot.cs
+++ src/libraries/System.Private.CoreLib/src/System/Runtime/CompilerServices/RuntimeFeature.NonNativeAot.cs
@@ -5,9 +5,20 @@ namespace System.Runtime.CompilerServices
 {
     public static partial class RuntimeFeature
     {
-        public static bool IsDynamicCodeSupported { get; } =
-            AppContext.TryGetSwitch("System.Runtime.CompilerServices.RuntimeFeature.IsDynamicCodeSupported", out bool isDynamicCodeSupported) ? isDynamicCodeSupported : true;
+        public static bool IsDynamicCodeSupported
+        {
+#if MONO
+            [Intrinsic]  // the Mono AOT compiler will change this flag to false for FullAOT scenarios, otherwise this code is used
+#endif
+            get;
+        } = AppContext.TryGetSwitch("System.Runtime.CompilerServices.RuntimeFeature.IsDynamicCodeSupported", out bool isDynamicCodeSupported) ? isDynamicCodeSupported : true;
 
-        public static bool IsDynamicCodeCompiled => IsDynamicCodeSupported;
+        public static bool IsDynamicCodeCompiled
+        {
+#if MONO
+            [Intrinsic]  // the Mono AOT compiler and Interpreter will change this flag to false for FullAOT and interpreted scenarios, otherwise this code is used
+#endif
+            get => IsDynamicCodeSupported;
+        }
     }
 }
