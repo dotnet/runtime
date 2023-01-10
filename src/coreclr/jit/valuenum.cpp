b7c0918ff1e8edc5429944f922faea97ee086cc1
EgorBo <egorbo@gmail.com>
2023-01-10 14:55:14 +0100

    Add more checks

--- src/coreclr/jit/valuenum.cpp
+++ src/coreclr/jit/valuenum.cpp
@@ -5134,7 +5134,8 @@ void Compiler::fgValueNumberFieldLoad(GenTree* loadTree, GenTree* baseAddr, Fiel
     //
     //   string GetName() => MyStr.Name; // <- loadTree
     //
-    if ((baseAddr == nullptr) && loadTree->TypeIs(TYP_REF))
+    if ((baseAddr == nullptr) && loadTree->TypeIs(TYP_REF) &&
+        (fieldSeq->GetKind() == FieldSeq::FieldKind::SimpleStatic) && (fieldSeq->GetOffset() == TARGET_POINTER_SIZE))
     {
         uint8_t buffer[TARGET_POINTER_SIZE] = {0};
         if (((UINT)offset < INT_MAX) &&
