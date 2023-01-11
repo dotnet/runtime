8fe001524fc36df88e8e601389efb87f318737f1
Mark Plesko <markples@microsoft.com>
2023-01-11 00:59:52 -0800

    Add comment to test

--- src/tests/Loader/classloader/TypeInitialization/CctorsWithSideEffects/CctorForWrite.cs
+++ src/tests/Loader/classloader/TypeInitialization/CctorsWithSideEffects/CctorForWrite.cs
@@ -1,3 +1,10 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+// Showed a JIT importer bug where a tree would be constructed for
+// call / stsfld and then the type initialization call inserted before
+// the entire tree.  The call needs to happen before type initialization.
+
 using System;
 using System.Runtime.CompilerServices;
 
