f741789a5be3dcea770b37feca0972eb9c4fd9ea
Stephen Toub <stoub@microsoft.com>
2023-01-12 15:47:40 -0500

    Delete CharArrayHelpers.EqualsOrdinalAsciiIgnoreCase/Trim
    Just use span directly.

--- src/libraries/Common/src/System/CharArrayHelpers.cs
+++ src/libraries/Common/src/System/CharArrayHelpers.cs
@@ -7,36 +7,6 @@ namespace System
 {
     internal static class CharArrayHelpers
     {
-        internal static bool EqualsOrdinalAsciiIgnoreCase(string left, char[] right, int rightStartIndex, int rightLength)
-        {
-            Debug.Assert(left != null, "Expected non-null string");
-            DebugAssertArrayInputs(right, rightStartIndex, rightLength);
-
-            return
-                left.Length == rightLength &&
-                left.AsSpan().Equals(right.AsSpan(rightStartIndex, rightLength), StringComparison.OrdinalIgnoreCase);
-        }
-
-        internal static void Trim(char[] array, ref int startIndex, ref int length)
-        {
-            DebugAssertArrayInputs(array, startIndex, length);
-
-            int offset = 0;
-            while (offset < length && char.IsWhiteSpace(array[startIndex + offset]))
-            {
-                offset++;
-            }
-
-            int end = length - 1;
-            while (end >= offset && char.IsWhiteSpace(array[startIndex + end]))
-            {
-                end--;
-            }
-
-            startIndex += offset;
-            length = end - offset + 1;
-        }
-
         [Conditional("DEBUG")]
         internal static void DebugAssertArrayInputs(char[] array, int startIndex, int length)
         {
