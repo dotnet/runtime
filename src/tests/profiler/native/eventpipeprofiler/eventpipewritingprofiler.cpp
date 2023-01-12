16577ab51c4ddf203b9a9c236e8bfba54826d1a7
David Mason <davmason@microsoft.com>
2023-01-11 17:41:29 -0800

    No, like really fix it
    Update eventpipewritingprofiler.cpp

--- src/tests/profiler/native/eventpipeprofiler/eventpipewritingprofiler.cpp
+++ src/tests/profiler/native/eventpipeprofiler/eventpipewritingprofiler.cpp
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 #include "eventpipewritingprofiler.h"
+#include <inttypes.h>
 
 EventPipeWritingProfiler *EventPipeWritingProfiler::s_singleton = nullptr;
 
