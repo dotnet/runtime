c8a73afdf24cd1182b19c8b7ead209a798edf893
David Cantú <dacantu@microsoft.com>
2023-01-13 15:01:29 -0600

    [release/7.0] TarReader should dispose underlying stream if leaveOpen is false (#80598)
    * TarReader should dispose underlying stream if leaveOpen is false (#79899)
    
    * Dispose underlying stream in TarReader.DisposeAsync() as well (#79920)
    
    * Dispose underlying stream in TarReader.DisposeAsync() as well
    
    Same as https://github.com/dotnet/runtime/pull/79899
    
    * Consolidate duplicated WrappedStream test helpers to Common sources
    
    * Dispose stream passed to WrappedStream
    
    * Dispose archive stream after the list of DataStreams (#80572)
    
    * Dispose archive stream after the list of DataStreams
    
    * Add tests for TarReader.DisposeAsync properly disposing underlying stream
    
    Co-authored-by: Alexander Köplinger <alex.koeplinger@outlook.com>

--- src/libraries/Common/tests/System/IO/WrappedStream.cs
+++ src/libraries/Common/tests/System/IO/WrappedStream.cs
@@ -1,9 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.IO;
-
-namespace System.Formats.Tar
+namespace System.IO
 {
     public class WrappedStream : Stream
     {
