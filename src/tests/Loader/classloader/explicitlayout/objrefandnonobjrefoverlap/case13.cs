21bf6f53e2281c74be212e8bc6ebcac1acdd6c7e
Aaron R Robinson <arobins@microsoft.com>
2023-01-13 17:27:43 -0800

    Add tests

---
+++ src/tests/Loader/classloader/explicitlayout/objrefandnonobjrefoverlap/case13.cs
@@ -0,0 +1,95 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+using Xunit;
+
+public class Test
+{
+    public struct WithORefs
+    {
+        public object F;
+    }
+
+    public struct WithNoORefs
+    {
+        public int F;
+    }
+
+    public ref struct WithByORefs
+    {
+        public ref int F;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    public ref struct Explicit1
+    {
+        [FieldOffset(0)] public Inner1 Field1;
+        public ref struct Inner1
+        {
+            public WithORefs Field2;
+        }
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    public ref struct Explicit2
+    {
+        [FieldOffset(0)] public Inner2 Field1;
+        public ref struct Inner2
+        {
+            public WithNoORefs Field2;
+        }
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    public ref struct Explicit3
+    {
+        [FieldOffset(0)] public Inner3 Field1;
+        public ref struct Inner3
+        {
+            public WithByORefs Field2;
+        }
+    }
+
+    [Fact]
+    public static void Validate_Explicit1()
+    {
+        Load1();
+        // that's enough. if we didn't throw a TypeLoadException, the test case will fail.
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        static string Load1()
+        {
+            return typeof(Explicit1).ToString();
+        }
+    }
+
+    [Fact]
+    public static void Validate_Explicit2()
+    {
+        Load2();
+        // that's enough. if we didn't throw a TypeLoadException, the test case will fail.
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        static string Load2()
+        {
+            return typeof(Explicit2).ToString();
+        }
+    }
+
+    [Fact]
+    public static void Validate_Explicit3()
+    {
+        Load3();
+        // that's enough. if we didn't throw a TypeLoadException, the test case will fail.
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        static string Load3()
+        {
+            return typeof(Explicit3).ToString();
+        }
+    }
+}
