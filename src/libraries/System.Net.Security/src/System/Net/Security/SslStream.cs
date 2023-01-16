83ea9545e3671cf8fd90a3932098635eab071e60
wfurt <tweinfurt@yahoo.com>
2023-01-16 15:39:29 -0800

    using

--- src/libraries/System.Net.Security/src/System/Net/Security/SslStream.cs
+++ src/libraries/System.Net.Security/src/System/Net/Security/SslStream.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Runtime.CompilerServices;
 using System.Runtime.ExceptionServices;
+using System.Runtime.Versioning;
 using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading;
