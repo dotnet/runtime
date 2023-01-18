e6bad065d38367335f217116f58000021a0e2aa7
EgorBo <egorbo@gmail.com>
2023-01-18 23:45:47 +0100

    Don't check time budget for AOT + --Ot

--- src/coreclr/jit/inlinepolicy.cpp
+++ src/coreclr/jit/inlinepolicy.cpp
@@ -488,8 +488,13 @@ bool DefaultPolicy::BudgetCheck() const
     // The strategy tracks the amout of inlining done so far,
     // so it performs the actual check.
     //
-    InlineStrategy* strategy   = m_RootCompiler->m_inlineStrategy;
-    const bool      overBudget = strategy->BudgetCheck(EstimatedTotalILSize());
+    InlineStrategy* strategy = m_RootCompiler->m_inlineStrategy;
+
+    const bool preferSpeed = m_RootCompiler->opts.jitFlags->IsSet(JitFlags::JIT_FLAG_SPEED_OPT);
+    const bool isAot       = m_RootCompiler->opts.IsReadyToRun();
+
+    // Ignore time budget for AOT + PreferSpeed, otherwise check if we run out of it.
+    const bool overBudget = !(isAot && preferSpeed) && strategy->BudgetCheck(EstimatedTotalILSize());
 
     if (overBudget)
     {
