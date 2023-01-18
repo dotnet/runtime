f7b6f3f2c050e2126c4f1be09b62351638c31617
neon-sunset <20912188+neon-sunset@users.noreply.github.com>
2023-01-18 02:04:22 +0200

    Remove unused reference

--- src/libraries/System.Linq/src/System/Linq/Range.SpeedOpt.cs
+++ src/libraries/System.Linq/src/System/Linq/Range.SpeedOpt.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
