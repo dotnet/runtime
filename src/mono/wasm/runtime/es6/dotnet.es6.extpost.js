b4f07599aa87cd3f1a6699be41016a723cf7ffd2
pavelsavara <pavel.savara@gmail.com>
2023-01-19 15:51:20 +0100

    WasmMinifyJsLevel SIMPLE_OPTIMIZATIONS and NONE

--- src/mono/wasm/runtime/es6/dotnet.es6.extpost.js
+++ src/mono/wasm/runtime/es6/dotnet.es6.extpost.js
@@ -1,9 +1,7 @@
 const ENVIRONMENT_IS_WEB = typeof window == "object";
 const ENVIRONMENT_IS_NODE = typeof process == "object" && typeof process.versions == "object" && typeof process.versions.node == "string";
 const MONO = {}, BINDING = {}, INTERNAL = {}, IMPORTS = {};
-var fetch = fetch || undefined;
-var require = require || undefined;
-var __dirname = __dirname || '';
+var fetch = fetch || undefined; var require = require || undefined; var __dirname = __dirname || '';
 __dotnet_runtime.__setEmscriptenEntrypoint(createDotnetRuntime);
 const __initializeImportsAndExports = __dotnet_runtime.__initializeImportsAndExports;
 const __requirePromise = ENVIRONMENT_IS_NODE ? import(/* webpackIgnore: true */'module').then(mod => mod.createRequire(import.meta.url)) : undefined;
