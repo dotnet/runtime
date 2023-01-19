1c9547de2ba59a0268e623d32c8696676bc931ce
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2023-01-19 11:49:37 +0900

    Remove uses of `TypeInfo` from the reflection stack
    Not aware of a good reason to keep using it.

--- src/coreclr/nativeaot/System.Private.CoreLib/src/System/Reflection/Runtime/BindingFlagSupport/NestedTypePolicies.cs
+++ src/coreclr/nativeaot/System.Private.CoreLib/src/System/Reflection/Runtime/BindingFlagSupport/NestedTypePolicies.cs
@@ -29,9 +29,9 @@ namespace System.Reflection.Runtime.BindingFlagSupport
 
         [UnconditionalSuppressMessage("ReflectionAnalysis", "IL2070:UnrecognizedReflectionPattern",
             Justification = "Reflection implementation")]
-        public sealed override IEnumerable<Type> GetDeclaredMembers(TypeInfo typeInfo)
+        public sealed override IEnumerable<Type> GetDeclaredMembers(Type type)
         {
-            return typeInfo.DeclaredNestedTypes;
+            return type.GetNestedTypes(DeclaredOnlyLookup);
         }
 
         public sealed override IEnumerable<Type> CoreGetDeclaredMembers(RuntimeTypeInfo type, NameFilter? optionalNameFilter, RuntimeTypeInfo reflectedType)
--- src/coreclr/nativeaot/System.Private.CoreLib/src/System/Reflection/Runtime/BindingFlagSupport/PropertyPolicies.cs
+++ src/coreclr/nativeaot/System.Private.CoreLib/src/System/Reflection/Runtime/BindingFlagSupport/PropertyPolicies.cs
@@ -19,9 +19,9 @@ namespace System.Reflection.Runtime.BindingFlagSupport
 
         [UnconditionalSuppressMessage("ReflectionAnalysis", "IL2070:UnrecognizedReflectionPattern",
             Justification = "Reflection implementation")]
-        public sealed override IEnumerable<PropertyInfo> GetDeclaredMembers(TypeInfo typeInfo)
+        public sealed override IEnumerable<PropertyInfo> GetDeclaredMembers(Type type)
         {
-            return typeInfo.DeclaredProperties;
+            return type.GetProperties(DeclaredOnlyLookup);
         }
 
         public sealed override IEnumerable<PropertyInfo> CoreGetDeclaredMembers(RuntimeTypeInfo type, NameFilter? optionalNameFilter, RuntimeTypeInfo reflectedType)
