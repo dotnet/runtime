5b3037e33ef31862c4596529f78cbcfc448e69cd
Lakshan Fernando <lakshanf@hotmail.com>
2023-01-19 11:13:40 -0800

    trying another option to get arounf inttypes.h and PRIu64

--- src/native/eventpipe/ep-json-file.c
+++ src/native/eventpipe/ep-json-file.c
@@ -9,7 +9,15 @@
 #include "ep-event-instance.h"
 #include "ep-rt.h"
 
-#include <inttypes.h>
+// Some configurations don't seem to have access to inttypes.h
+#ifndef PRIu64
+#if defined(TARGET_UNIX)
+#define PRIu64 "lu"
+#else 
+#define PRIu64 "llu"
+#endif
+#endif
+
 
 #ifdef EP_CHECKED_BUILD
 
