a15b3ef5dcab31b0abe2bc914eee6299df7cf56f
Radek Doulik <radekdoulik@gmail.com>
2023-01-20 10:25:04 +0100

    Try to pass full path to v8
    Also remove --experimental-wasm-simd option, which shouldn't be needed
    for newer v8

--- eng/testing/WasmRunnerAOTTemplate.sh
+++ eng/testing/WasmRunnerAOTTemplate.sh
@@ -37,8 +37,7 @@ if [[ "$XHARNESS_COMMAND" == "test" ]]; then
 			JS_ENGINE="--engine=NodeJS"
 			JS_ENGINE_ARGS="$JS_ENGINE_ARGS --engine-arg=--experimental-wasm-simd"
 		else
-			JS_ENGINE="--engine=V8"
-			JS_ENGINE_ARGS="$JS_ENGINE_ARGS --engine-arg=--experimental-wasm-simd"
+			JS_ENGINE="--engine=/home/helixbot/.jsvu/bin/v8"
 		fi
 	fi
 
