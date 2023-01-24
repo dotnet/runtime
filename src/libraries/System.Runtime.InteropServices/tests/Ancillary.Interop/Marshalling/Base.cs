0f061fd47315d01a62280f371ef9530bbe91966f
Jackson Schuster <jschuster@microsoft.com>
2023-01-23 20:14:37 -0800

    PR feedback
    - Add license banner to new files
    - Change count to 0 for null VTables

--- src/libraries/System.Runtime.InteropServices/tests/Ancillary.Interop/Marshalling/Base.cs
+++ src/libraries/System.Runtime.InteropServices/tests/Ancillary.Interop/Marshalling/Base.cs
@@ -1,3 +1,6 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 // Types for the COM Source Generator that define the basic vtable interactions and that we would need in the COM source generator in one form or another.
 namespace System.Runtime.InteropServices.Marshalling;
 
