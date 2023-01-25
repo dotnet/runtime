0be94b8e94b9aca5d9fca8a3741af9c15faed340
Ankit Jain <radical@gmail.com>
2023-01-25 00:56:51 +0000

    fix up running sample

--- src/mono/sample/wasi/wasi.mk
+++ src/mono/sample/wasi/wasi.mk
@@ -8,19 +8,8 @@ endif
 
 CONFIG?=Release
 
-WASM_DEFAULT_BUILD_ARGS?=/p:TargetArchitecture=wasm /p:TargetOS=WASI /p:Configuration=$(CONFIG)
-
-# we set specific headers to enable SharedArrayBuffer support in browsers for threading: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/SharedArrayBuffer#security_requirements
-CORS_HEADERS?= -h "Cross-Origin-Opener-Policy:same-origin" -h "Cross-Origin-Embedder-Policy:require-corp"
-
-# if we're in a container, don't try to open the browser
-ifneq ("$(wildcard /.dockerenv)", "")
-  OPEN_BROWSER=
-  V8_PATH=v8
-else
-  OPEN_BROWSER=-o
-  V8_PATH=~/.jsvu/v8
-endif
+WASM_DEFAULT_BUILD_ARGS?=/p:TargetArchitecture=wasm /p:TargetOS=wasi /p:Configuration=$(CONFIG)
+WASMTIME_PROV_PATH=$(realpath $(TOP)/artifacts/obj/wasmtime/wasmtime)
 
 all: publish
 
@@ -33,19 +22,5 @@ publish:
 clean:
 	rm -rf bin $(TOP)/artifacts/obj/mono/$(PROJECT_NAME:%.csproj=%)
 
-run-browser:
-	if ! $(DOTNET) tool list --global | grep dotnet-serve && ! which dotnet-serve ; then \
-		echo "The tool dotnet-serve could not be found. Install with: $(DOTNET) tool install --global dotnet-serve"; \
-		exit 1; \
-	else  \
-		$(DOTNET) serve -d:bin/$(CONFIG)/AppBundle $(CORS_HEADERS) $(OPEN_BROWSER) -p:8000; \
-	fi
-
 run-console:
-	cd bin/$(CONFIG)/AppBundle && $(V8_PATH) --stack-trace-limit=1000 --single-threaded --expose_wasm $(MAIN_JS) -- $(ARGS)
-
-run-console-node:
-	cd bin/$(CONFIG)/AppBundle && node --stack-trace-limit=1000 --single-threaded --expose_wasm $(MAIN_JS) $(ARGS)
-
-debug-console-node:
-	cd bin/$(CONFIG)/AppBundle && node --inspect=9222 --stack-trace-limit=1000 --single-threaded --expose_wasm $(MAIN_JS) $(ARGS)
+	cd bin/wasi-wasm/AppBundle && $(WASMTIME_PROV_PATH) run $(PROJECT_NAME:.csproj=.wasm) $(ARGS)
