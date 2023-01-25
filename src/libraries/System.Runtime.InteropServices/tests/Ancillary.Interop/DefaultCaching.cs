046b9d1aa572903dd0e55b2e4fbe8282b6fc8ada
Jackson Schuster <jschuster@microsoft.com>
2023-01-24 21:36:23 -0800

    PR Feedback:
    - Move types to their own file and update comments
    - Re-add dereference to 'this' pointer for vtable

--- src/libraries/System.Runtime.InteropServices/tests/Ancillary.Interop/DefaultCaching.cs
+++ src/libraries/System.Runtime.InteropServices/tests/Ancillary.Interop/DefaultCaching.cs
@@ -1,51 +1,12 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 // Implementations of the COM strategy interfaces defined in Com.cs that we would want to ship (can be internal only if we don't want to allow users to provide their own implementations in v1).
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 
 namespace System.Runtime.InteropServices.Marshalling;
 
-
-public sealed class DefaultIUnknownInterfaceDetailsStrategy : IIUnknownInterfaceDetailsStrategy
-{
-    public static readonly IIUnknownInterfaceDetailsStrategy Instance = new DefaultIUnknownInterfaceDetailsStrategy();
-
-    public IUnknownDerivedDetails? GetIUnknownDerivedDetails(RuntimeTypeHandle type)
-    {
-        return IUnknownDerivedDetails.GetFromAttribute(type);
-    }
-}
-
-public sealed unsafe class FreeThreadedStrategy : IIUnknownStrategy
-{
-    public static readonly IIUnknownStrategy Instance = new FreeThreadedStrategy();
-
-    void* IIUnknownStrategy.CreateInstancePointer(void* unknown)
-    {
-        return unknown;
-    }
-
-    unsafe int IIUnknownStrategy.QueryInterface(void* thisPtr, in Guid handle, out void* ppObj)
-    {
-        int hr = Marshal.QueryInterface((nint)thisPtr, ref Unsafe.AsRef(in handle), out nint ppv);
-        if (hr < 0)
-        {
-            ppObj = null;
-        }
-        else
-        {
-            ppObj = (void*)ppv;
-        }
-        return hr;
-    }
-
-    unsafe int IIUnknownStrategy.Release(void* thisPtr)
-        => Marshal.Release((nint)thisPtr);
-}
-
-public sealed unsafe class DefaultCaching : IIUnknownCacheStrategy
+internal sealed unsafe class DefaultCaching : IIUnknownCacheStrategy
 {
     // [TODO] Implement some smart/thread-safe caching
     private readonly Dictionary<RuntimeTypeHandle, IIUnknownCacheStrategy.TableInfo> _cache = new();
