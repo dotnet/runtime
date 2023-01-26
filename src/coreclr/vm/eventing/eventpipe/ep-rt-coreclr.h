1656b1b36f2309efd2af91b64a407c2a7b157aac
Jeremy Koritzinsky <jekoritz@microsoft.com>
2023-01-26 13:21:20 -0800

    Use nullptr as the "did not yet look up assembly name" sentinel

--- src/coreclr/vm/eventing/eventpipe/ep-rt-coreclr.h
+++ src/coreclr/vm/eventing/eventpipe/ep-rt-coreclr.h
@@ -1139,29 +1139,32 @@ ep_rt_entrypoint_assembly_name_get_utf8 (void)
 	// suspended before an assembly is loaded.
 	// We'll cache the value in a static function global as the caller expect the lifetime of this value
 	// to outlast the calling function.
-	static const ep_char8_t* entrypoint_assembly_name = NULL;
-	static bool read_name_from_host = false;
-	SString assembly_name;
-	if (!read_name_from_host || HostInformation::GetProperty (HOST_PROPERTY_ENTRY_ASSEMBLY_NAME, assembly_name))
+	static const ep_char8_t* entrypoint_assembly_name = nullptr;
+	if (entrypoint_assembly_name == nullptr)
 	{
-		const ep_char8_t* entrypoint_assembly_name_local = reinterpret_cast<const ep_char8_t*>(assembly_name.GetCopyOfUTF8String ());
+		const ep_char8_t* entrypoint_assembly_name_local;
+		SString assembly_name;
+		if (!HostInformation::GetProperty (HOST_PROPERTY_ENTRY_ASSEMBLY_NAME, assembly_name))
+		{
+			entrypoint_assembly_name_local = reinterpret_cast<const ep_char8_t*>(assembly_name.GetCopyOfUTF8String ());
+		}
+		else
+		{
+			// fallback to the empty string
+			// Allocate a new empty string here so we consistently allocate with the same allocator no matter our code-path.
+			entrypoint_assembly_name_local = new ep_char8_t [1] { '\0' };
+		}
 		// Try setting this entrypoint name as the cached value.
 		// If someone else beat us to it, free the memory we allocated.
 		// We want to only leak the one global copy of the entrypoint name,
 		// not mutliple copies.
-		if (InterlockedCompareExchangeT(&entrypoint_assembly_name, entrypoint_assembly_name_local, NULL) != NULL)
+		if (InterlockedCompareExchangeT(&entrypoint_assembly_name, entrypoint_assembly_name_local, nullptr) != nullptr)
 		{
 			delete[] entrypoint_assembly_name_local;
 		}
-		read_name_from_host = true;
-		return entrypoint_assembly_name;
 	}
 
-	// Record that we tried to read the name from the host so we don't try again.
-	read_name_from_host = true;
-
-	// fallback to the empty string
-	return reinterpret_cast<const ep_char8_t*>("");
+	return entrypoint_assembly_name;
 }
 
 static
