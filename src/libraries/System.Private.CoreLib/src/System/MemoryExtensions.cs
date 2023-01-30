fafa98a7c6fe26a7c5157e43596e89061a4c3cde
Ondřej Kubíček <lordfanger@gmail.com>
2023-01-30 19:22:57 +0100

    doc update

--- src/libraries/System.Private.CoreLib/src/System/MemoryExtensions.cs
+++ src/libraries/System.Private.CoreLib/src/System/MemoryExtensions.cs
@@ -3353,8 +3353,7 @@ namespace System
             string.CheckStringSplitOptions(options);
 
             // If the separators list is empty, whitespace is used as separators.  In that case, we want to ignore TrimEntries if specified,
-            // since TrimEntries also impacts whitespace.
-            // The TrimEntries flag must be left intact if we are constrained by count because we need to process last substring.
+            // since TrimEntries also impacts whitespace.  The TrimEntries flag must be left intact if we are constrained by count because we need to process last substring.
             if (separators.IsEmpty && destination.Length > source.Length)
             {
                 options &= ~StringSplitOptions.TrimEntries;
@@ -3395,8 +3394,7 @@ namespace System
             string.CheckStringSplitOptions(options);
 
             // If the separators list is empty, whitespace is used as separators.  In that case, we want to ignore TrimEntries if specified,
-            // since TrimEntries also impacts whitespace.
-            // The TrimEntries flag must be left intact if we are constrained by count because we need to process last substring.
+            // since TrimEntries also impacts whitespace.  The TrimEntries flag must be left intact if we are constrained by count because we need to process last substring.
             if (separators.IsEmpty && destination.Length > source.Length)
             {
                 options &= ~StringSplitOptions.TrimEntries;
