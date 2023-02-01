e3a62da90f753e89c904340b72b1384f589d3422
vitek-karas <10670590+vitek-karas@users.noreply.github.com>
2023-02-01 03:47:35 -0800

    Revert "Revert "Fix generic parameter data flow validation in NativeAOT (#80956)" (#81259)"
    This reverts commit faa19f21bfe48aa2e2091a5b3010ac0016a34d5e.

--- src/coreclr/tools/aot/ILCompiler.Compiler/Compiler/Dataflow/TrimAnalysisReflectionAccessPattern.cs
+++ src/coreclr/tools/aot/ILCompiler.Compiler/Compiler/Dataflow/TrimAnalysisReflectionAccessPattern.cs
@@ -1,8 +1,11 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics;
 using ILCompiler.Logging;
 using Internal.TypeSystem;
+
+#nullable enable
 
 namespace ILCompiler.Dataflow
 {
@@ -17,12 +20,25 @@ namespace ILCompiler.Dataflow
             Origin = origin;
         }
 
-        // No Merge - there's nothing to merge since this pattern is unequily identified by both the origin and the entity
+        // No Merge - there's nothing to merge since this pattern is uniquely identified by both the origin and the entity
         // and there's only one way to "reflection access" an entity.
 
         public void MarkAndProduceDiagnostics(ReflectionMarker reflectionMarker, Logger logger)
         {
-            reflectionMarker.CheckAndWarnOnReflectionAccess(Origin, Entity);
+            switch (Entity)
+            {
+                case MethodDesc method:
+                    reflectionMarker.CheckAndWarnOnReflectionAccess(Origin, method);
+                    break;
+
+                case FieldDesc field:
+                    reflectionMarker.CheckAndWarnOnReflectionAccess(Origin, field);
+                    break;
+
+                default:
+                    Debug.Fail($"Unsupported entity for reflection access pattern: {Entity}");
+                    break;
+            }
         }
     }
 }
