ce07065236293f92f3179f7b1381a046a4a5424a
Levi Broderick <levib@microsoft.com>
2023-02-01 19:01:24 -0800

    Add guards to zlib memory allocator

--- src/native/external/zlib-intel.cmake
+++ src/native/external/zlib-intel.cmake
@@ -22,4 +22,13 @@ set(ZLIB_SOURCES_BASE
     zutil.c
 )
 
+# Configure our custom allocator
+if(MSVC)
+    set(ZLIB_SOURCES_BASE ${ZLIB_SOURCES_BASE} ../zlib/dotnet_allocator_win.c)
+    add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:/DMY_ZCALLOC>)
+else()
+    set(ZLIB_SOURCES_BASE ${ZLIB_SOURCES_BASE} ../zlib/dotnet_allocator_unix.c)
+    add_definitions(-DMY_ZCALLOC)
+endif()
+
 addprefix(ZLIB_SOURCES "${CMAKE_CURRENT_LIST_DIR}/zlib-intel"  "${ZLIB_SOURCES_BASE}")
