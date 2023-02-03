46f0abd86d6e4cd2e0f89042b9cbea7e912e9acd
Kunal Pathak <Kunal.Pathak@microsoft.com>
2023-02-02 21:48:05 -0800

    Fix test cases

--- src/tests/JIT/HardwareIntrinsics/Arm/AdvSimd/Program.AdvSimd.cs
+++ src/tests/JIT/HardwareIntrinsics/Arm/AdvSimd/Program.AdvSimd.cs
@@ -1,8 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-extern alias CoreLib;
-global using AdvSimd = CoreLib::System.Runtime.Intrinsics.Arm.AdvSimd;
 using System;
 using System.Collections.Generic;
 
