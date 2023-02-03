bd08a682e645504960e9be62fec2ca6b4d5d0910
Andy Gocke <andy@commentout.net>
2023-02-03 12:05:31 -0800

    Add Linux NativeAOT debug info regression test
    The test runs llvm-dwarfdump and verifies that the number of warnings
    and errors is expected. As we improve the debug information we can
    start tracking individual warnings and errors, hopefully bringing
    this number to zero.

---
+++ src/tests/nativeaot/SmokeTests/DwarfDump/Program.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Diagnostics;
+
+public class Program
+{
+    public static int Main(string[] args)
+    {
+        if (!OperatingSystem.IsLinux())
+        {
+            // Linux-only test
+            return 100;
+        }
+
+        var proc = Process.Start(new ProcessStartInfo
+        {
+            FileName = "/bin/sh",
+            Arguments = $"-c \"llvm-dwarfdump --verify {Environment.ProcessPath}",
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false
+        });
+
+        // Just count the number of warnings and errors. There are so many right now that it's not worth enumerating the list
+        const int ExpectedCount = 13396;
+        int count = 0;
+        string line;
+        while ((line = proc.StandardOutput.ReadLine()) != null)
+        {
+            if (line.Contains("warning:") || line.Contains("error:"))
+            {
+                count++;
+            }
+        }
+        proc.WaitForExit();
+        if (count != ExpectedCount)
+        {
+            Console.WriteLine($"Found {count} warnings and errors, expected {ExpectedCount}");
+            Console.WriteLine("This is likely a result of debug info changes. To see the new output, run the following command:");
+            Console.WriteLine("\tllvm-dwarfdump --verify " + Environment.ProcessPath);
+            return 1;
+        }
+        return 100;
+    }
+}
