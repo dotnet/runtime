f736fa53e3654b7dca1aca2543363c2265229962
Radek Doulik <radekdoulik@gmail.com>
2023-02-07 11:48:15 +0100

    Add dynamic dependency

--- src/libraries/System.Runtime.Intrinsics/tests/Vectors/Vector128Tests.cs
+++ src/libraries/System.Runtime.Intrinsics/tests/Vectors/Vector128Tests.cs
@@ -1,6 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics.CodeAnalysis;
 using System.Numerics;
 using System.Reflection;
 using System.Runtime.InteropServices;
@@ -11,6 +12,7 @@ namespace System.Runtime.Intrinsics.Tests.Vectors
     public sealed class Vector128Tests
     {
         [Fact]
+        [DynamicDependency(DynamicallyAccessedMemberTypes.PublicProperties, typeof(Vector128))]
         public unsafe void Vector128IsHardwareAcceleratedTest()
         {
             MethodInfo methodInfo = typeof(Vector128).GetMethod("get_IsHardwareAccelerated");
