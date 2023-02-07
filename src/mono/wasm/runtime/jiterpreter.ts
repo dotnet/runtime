59dddfdef247b9b85a69d663a1d1c1746206711e
Austin Wise <AustinWise@gmail.com>
2023-02-07 15:50:01 -0800

    [Jiterpreter] Add support for TryGetHashCode intrinsic (#81644)
    #80520 added support in the Jiterpreter for the intrinsic that underlies RuntimeHelpers.GetHashCode. This PR adds similar support for the intrinsic that underlies RuntimeHelpers.TryGetHashCode.
    
    For reference RuntimeHelpers.TryGetHashCode was added in #80059. It is currently only used in ConditionalWeakTable.TryGetValue.

--- src/mono/wasm/runtime/jiterpreter.ts
+++ src/mono/wasm/runtime/jiterpreter.ts
@@ -243,6 +243,7 @@ function getTraceImports () {
         ["relop_fp", "relop_fp", getRawCwrap("mono_jiterp_relop_fp")],
         ["safepoint", "safepoint", getRawCwrap("mono_jiterp_auto_safepoint")],
         ["hashcode", "hashcode", getRawCwrap("mono_jiterp_get_hashcode")],
+        ["try_hash", "try_hash", getRawCwrap("mono_jiterp_try_get_hashcode")],
         ["hascsize", "hascsize", getRawCwrap("mono_jiterp_object_has_component_size")],
         ["hasflag", "hasflag", getRawCwrap("mono_jiterp_enum_hasflag")],
         ["array_rank", "array_rank", getRawCwrap("mono_jiterp_get_array_rank")],
@@ -465,6 +466,11 @@ function initialize_builder (builder: WasmBuilder) {
     );
     builder.defineType(
         "hashcode", {
+            "ppObj": WasmValtype.i32,
+        }, WasmValtype.i32, true
+    );
+    builder.defineType(
+        "try_hash", {
             "ppObj": WasmValtype.i32,
         }, WasmValtype.i32, true
     );
