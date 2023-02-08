0304f1f5157a8280fa093bdfc7cfb8d9f62e016f
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2023-02-08 19:44:45 +0000

    Support use of fast-path serialization in combined JsonSerializerContexts. (#80741)
    * Support use of fast-path serialization in combined JsonSerializerContexts.
    
    * Strengthen assertions in the nested JsonTypeInfo accessors.

--- src/libraries/System.Text.Json/src/System/Text/Json/Serialization/Metadata/JsonPropertyInfo.cs
+++ src/libraries/System.Text.Json/src/System/Text/Json/Serialization/Metadata/JsonPropertyInfo.cs
@@ -18,7 +18,6 @@ namespace System.Text.Json.Serialization.Metadata
         internal static readonly JsonPropertyInfo s_missingProperty = GetPropertyPlaceholder();
 
         internal JsonTypeInfo? ParentTypeInfo { get; private set; }
-        private JsonTypeInfo? _jsonTypeInfo;
 
         /// <summary>
         /// Converter after applying CustomConverter (i.e. JsonConverterAttribute)
@@ -266,10 +265,7 @@ namespace System.Text.Json.Serialization.Metadata
 
         private protected void VerifyMutable()
         {
-            if (ParentTypeInfo?.IsReadOnly == true)
-            {
-                ThrowHelper.ThrowInvalidOperationException_PropertyInfoImmutable();
-            }
+            ParentTypeInfo?.VerifyMutable();
         }
 
         internal bool IsConfigured { get; private set; }
@@ -805,6 +801,15 @@ namespace System.Text.Json.Serialization.Metadata
                 _jsonTypeInfo = value;
             }
         }
+
+        private JsonTypeInfo? _jsonTypeInfo;
+
+        /// <summary>
+        /// Returns true if <see cref="JsonTypeInfo"/> has been configured.
+        /// This might be false even if <see cref="IsConfigured"/> is true
+        /// in cases of recursive types or <see cref="IsIgnored"/> is true.
+        /// </summary>
+        internal bool IsPropertyTypeInfoConfigured => _jsonTypeInfo?.IsConfigured == true;
 
         /// <summary>
         /// Property was marked JsonIgnoreCondition.Always and also hasn't been configured by the user.
