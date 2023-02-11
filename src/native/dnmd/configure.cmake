fdd42799c4149f07dd85f495f7b7e73014d02abc
Aaron R Robinson <arobins@microsoft.com>
2023-02-11 09:58:17 -0800

    Add rpath settings for macOS.

--- src/native/dnmd/configure.cmake
+++ src/native/dnmd/configure.cmake
@@ -8,6 +8,25 @@ set(CMAKE_CXX_STANDARD 14)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 
+#
+# Configure CMake for platforms
+#
+if(WIN32)
+  add_compile_definitions(BUILD_WINDOWS=1)
+elseif(APPLE)
+  add_compile_definitions(BUILD_MACOS=1)
+
+  set(CMAKE_BUILD_WITH_INSTALL_NAME_DIR ON)
+  set(CMAKE_INSTALL_NAME_DIR "@rpath")
+  set(CMAKE_BUILD_WITH_INSTALL_RPATH TRUE)
+  set(CMAKE_INSTALL_RPATH "@loader_path")
+else()
+  add_compile_definitions(BUILD_UNIX=1)
+endif()
+
+#
+# Configure compilers
+#
 if(MSVC)
   add_compile_options(/Zc:wchar_t-) # wchar_t is a built-in type.
   add_compile_options(/W4 /WX) # warning level 4 and warnings are errors.
@@ -21,13 +40,7 @@ else()
   add_compile_options(-Wno-pragma-pack) # cor.h controls pack pragmas via headers.
 endif()
 
-# Define values for platform detection
-if(WIN32)
-  add_compile_definitions(BUILD_WINDOWS=1)
-elseif(APPLE)
-  add_compile_definitions(BUILD_MACOS=1)
-else()
-  add_compile_definitions(BUILD_UNIX=1)
-endif()
-
+#
+# Agnostic compiler/platform settings
+#
 add_compile_definitions(__STDC_WANT_LIB_EXT1__=1) # https://en.cppreference.com/w/c/error#Bounds_checking
