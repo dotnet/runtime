d3a0de7dd4496e8e30c58c926d699c43b545718e
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2023-02-13 11:45:38 +0100

    Allow liveness to remove CMP/TEST/BT when GTF_SET_FLAGS is not set

--- src/coreclr/jit/liveness.cpp
+++ src/coreclr/jit/liveness.cpp
@@ -2122,11 +2122,6 @@ void Compiler::fgComputeLifeLIR(VARSET_TP& life, BasicBlock* block, VARSET_VALAR
             case GT_STORE_BLK:
             case GT_STORE_DYN_BLK:
             case GT_JCMP:
-            case GT_CMP:
-            case GT_TEST:
-#ifdef TARGET_XARCH
-            case GT_BT:
-#endif
             case GT_JCC:
             case GT_JTRUE:
             case GT_RETURN:
