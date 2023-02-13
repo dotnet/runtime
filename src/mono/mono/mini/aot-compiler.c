fbc0a92fc423eb7d05aff9be54f5e4b836d5d76f
Milos Kotlar <kotlarmilos@gmail.com>
2023-02-13 17:30:37 +0100

    Disable dedup for tvOS

--- src/mono/mono/mini/aot-compiler.c
+++ src/mono/mono/mini/aot-compiler.c
@@ -6589,7 +6589,7 @@ emit_and_reloc_code (MonoAotCompile *acfg, MonoMethod *method, guint8 *code, gui
 						direct_call_target = symbol;
 						patch_info->type = MONO_PATCH_INFO_NONE;
 					} else if ((m_class_get_image (patch_info->data.method->klass) == acfg->image) && !got_only && is_direct_callable (acfg, method, patch_info)) {
-#ifndef TARGET_IOS
+#if !defined(TARGET_IOS) || defined(TARGET_TVOS)
 						MonoCompile *callee_cfg = (MonoCompile *)g_hash_table_lookup (acfg->method_to_cfg, cmethod);
 
 						// Don't compile inflated methods if we're doing dedup
@@ -14886,7 +14886,7 @@ aot_assembly (MonoAssembly *ass, guint32 jit_opts, MonoAotOptions *aot_options)
 	TV_GETTIME (btv);
 
 	acfg->stats.jit_time = GINT64_TO_INT (TV_ELAPSED (atv, btv));
-#ifndef TARGET_IOS
+#if !defined(TARGET_IOS) || defined(TARGET_TVOS)
 	dedup_skip_methods (acfg);
 #endif
 	if (acfg->dedup_collect_only) {
