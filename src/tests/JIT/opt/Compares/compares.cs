2f9b7e789a5507a70524782ca0767912e2380c1c
Will Smith <lol.tihan@gmail.com>
2023-02-14 16:54:54 -0800

    Update compares.cs

--- src/tests/JIT/opt/Compares/compares.cs
+++ src/tests/JIT/opt/Compares/compares.cs
@@ -179,7 +179,7 @@ public class FullRangeComparisonTest
     public static void Lt_double_long_consume(double f1, double f2, long a1, long a2)
     {
         // ARM64-FULL-LINE:      fcmp {{d[0-9]+}}, {{d[0-9]+}}
-        // ARM64-FULL-LINE-NEXT: csel {{x[0-31]}}, {{x[0-31]}}, {{x[0-31]}}, {{lt|ge}}
+        // ARM64-FULL-LINE-NEXT: csel {{x[0-31]}}, {{x[0-31]}}, {{x[0-31]}}, {{le|gt}}
         //
         // X64-FULL-LINE:        cmov{{be|a}} {{[a-z0-9]+}}, {{.*}}
 
