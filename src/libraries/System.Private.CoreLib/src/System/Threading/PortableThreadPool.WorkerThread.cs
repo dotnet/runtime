0fedfbb54792139eb607864d9cad4e24ee3cf28c
Koundinya Veluri <kouvel@microsoft.com>
2023-02-16 10:11:47 -0800

    Refactor Windows-specific code

--- src/libraries/System.Private.CoreLib/src/System/Threading/PortableThreadPool.WorkerThread.cs
+++ src/libraries/System.Private.CoreLib/src/System/Threading/PortableThreadPool.WorkerThread.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Diagnostics;
 using System.Diagnostics.Tracing;
 
 namespace System.Threading
@@ -11,7 +10,7 @@ namespace System.Threading
         /// <summary>
         /// The worker thread infastructure for the CLR thread pool.
         /// </summary>
-        private static class WorkerThread
+        private static partial class WorkerThread
         {
             private const int SemaphoreSpinCountDefaultBaseline = 70;
 #if !TARGET_ARM64 && !TARGET_ARM && !TARGET_LOONGARCH64
@@ -117,12 +116,7 @@ namespace System.Threading
                     }
 
                     // The thread cannot exit if it has IO pending, otherwise the IO may be canceled
-                    bool success =
-                        Interop.Kernel32.GetThreadIOPendingFlag(
-                            Interop.Kernel32.GetCurrentThread(),
-                            out Interop.BOOL isIOPending);
-                    Debug.Assert(success);
-                    if (!success || isIOPending != Interop.BOOL.FALSE)
+                    if (IsIOPending)
                     {
                         continue;
                     }
