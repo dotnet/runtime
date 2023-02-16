621569ba6cc865ff38b8c5fbe7ffb988e11ff360
Tlakollo <tlcejava@microsoft.com>
2023-02-15 17:29:24 -0800

    lint

--- src/tools/illink/test/Mono.Linker.Tests/TestCasesRunner/PathUtilities.cs
+++ src/tools/illink/test/Mono.Linker.Tests/TestCasesRunner/PathUtilities.cs
@@ -25,14 +25,14 @@ namespace Mono.Linker.Tests.TestCasesRunner
 #error "Unknown TFM"
 #endif
 
-		public static string GetTestsSourceRootDirectory([CallerFilePath] string thisFile = null) =>
-			Path.GetFullPath((string)AppContext.GetData("Mono.Linker.Tests.LinkerTestDir")!);
+		public static string GetTestsSourceRootDirectory ([CallerFilePath] string thisFile = null) =>
+			Path.GetFullPath ((string) AppContext.GetData ("Mono.Linker.Tests.LinkerTestDir")!);
 
 		public static string GetTestAssemblyPath (string assemblyName)
 		{
-			var artifactsBinDirectory = (string)AppContext.GetData("Mono.Linker.Tests.ArtifactsBinDir")!;
-			var configuration = (string)AppContext.GetData("Mono.Linker.Tests.Configuration")!;
-			return Path.GetFullPath(Path.Combine(artifactsBinDirectory, assemblyName, configuration, TFMDirectoryName, $"{assemblyName}.dll"));
+			var artifactsBinDirectory = (string) AppContext.GetData ("Mono.Linker.Tests.ArtifactsBinDir")!;
+			var configuration = (string) AppContext.GetData ("Mono.Linker.Tests.Configuration")!;
+			return Path.GetFullPath (Path.Combine (artifactsBinDirectory, assemblyName, configuration, TFMDirectoryName, $"{assemblyName}.dll"));
 		}
 	}
 }
