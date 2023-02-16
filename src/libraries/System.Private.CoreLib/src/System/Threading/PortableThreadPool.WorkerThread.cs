e4d78fc5e4792a76e9f96683c3efd4c225610e92
Koundinya Veluri <kouvel@microsoft.com>
2023-02-15 22:28:14 -0800

    Check for pending IO in the portable thread pool's worker threads
    - When Resource Monitor is attached, some async IO operations are bound to the thread that issued it even though the IO handle is bound to an IOCP. If the thread exits, the async IO operation is aborted. This can lead to hangs or unexpected exceptions.
    - Added a check that was missing in the portable thread pool implementation to prevent exiting a worker thread when it has pending IO
    
    Fixes https://github.com/dotnet/runtime/issues/82207

--- src/libraries/System.Private.CoreLib/src/System/Threading/PortableThreadPool.WorkerThread.cs
+++ src/libraries/System.Private.CoreLib/src/System/Threading/PortableThreadPool.WorkerThread.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics;
 using System.Diagnostics.Tracing;
 
 namespace System.Threading
@@ -113,6 +114,17 @@ namespace System.Threading
                             // the number of working workers to reflect that we are done working for now
                             RemoveWorkingWorker(threadPoolInstance);
                         }
+                    }
+
+                    // The thread cannot exit if it has IO pending, otherwise the IO may be canceled
+                    bool success =
+                        Interop.Kernel32.GetThreadIOPendingFlag(
+                            Interop.Kernel32.GetCurrentThread(),
+                            out Interop.BOOL isIOPending);
+                    Debug.Assert(success);
+                    if (!success || isIOPending != Interop.BOOL.FALSE)
+                    {
+                        continue;
                     }
 
                     threadAdjustmentLock.Acquire();
