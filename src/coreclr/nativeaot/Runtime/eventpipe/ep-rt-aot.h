1d631b95a7225c37f7bedc3f8b13ab0618a6fef3
Lakshan Fernando <lakshanf@hotmail.com>
2023-02-16 05:55:04 -0800

    Add copyright info to eventpipe files

--- src/coreclr/nativeaot/Runtime/eventpipe/ep-rt-aot.h
+++ src/coreclr/nativeaot/Runtime/eventpipe/ep-rt-aot.h
@@ -1,4 +1,7 @@
-// Implementation of ep-rt.h targeting CoreCLR runtime.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+// Implementation of ep-rt.h targeting NativeAOT runtime.
 #ifndef __EVENTPIPE_RT_AOT_H__
 #define __EVENTPIPE_RT_AOT_H__
 
