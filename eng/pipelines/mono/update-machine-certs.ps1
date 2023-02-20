c302891a115ab2c3d229e54a71100d23277ad77e
Radek Doulik <radekdoulik@gmail.com>
2023-02-20 17:14:37 +0100

    Try to force reinstall of certifi

--- eng/pipelines/mono/update-machine-certs.ps1
+++ eng/pipelines/mono/update-machine-certs.ps1
@@ -1,6 +1,6 @@
 # This seems to update the machine cert store so that python can download the files as required by emscripten's install
 # Based on info at https://pypi.org/project/certifi/
-pip install certifi
+pip install --force-reinstall certifi
 
 $WebsiteURL="storage.googleapis.com"
 Try {
