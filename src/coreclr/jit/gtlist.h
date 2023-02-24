b3515471c7d95c51af528fe8142ab7e2f8d32976
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2023-02-24 16:28:47 +0100

    Fix outdated comment

--- src/coreclr/jit/gtlist.h
+++ src/coreclr/jit/gtlist.h
@@ -247,8 +247,7 @@ GTNODE(SELECTCC         , GenTreeOpCC        ,0,GTK_BINOP|DBK_NOTHIR)
 #ifdef TARGET_ARM64
 // The arm64 ccmp instruction. If the specified condition is true, compares two
 // operands and sets the condition flags according to the result. Otherwise
-// sets the condition flags such that they make a specified (assuming to be
-// upcoming) condition false.
+// sets the condition flags to the specified immediate value.
 GTNODE(CCMP             , GenTreeCCMP        ,0,GTK_BINOP|GTK_NOVALUE|DBK_NOTHIR)
 #endif
 
