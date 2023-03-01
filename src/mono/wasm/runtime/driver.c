340508f5c750d190090be93f0c731b1a8055a354
Pavel Savara <pavel.savara@gmail.com>
2023-03-01 16:09:21 +0100

    [wasi+browser] bundle timezones into .wasm (#82250)
    Co-authored-by: Ankit Jain <radical@gmail.com>

--- src/mono/wasm/runtime/driver.c
+++ src/mono/wasm/runtime/driver.c
@@ -68,6 +68,7 @@ void mono_free (void*);
 int32_t mini_parse_debug_option (const char *option);
 char *mono_method_get_full_name (MonoMethod *method);
 char *mono_method_full_name (MonoMethod *method, int signature);
+extern void mono_wasm_register_timezones_bundle();
 
 static void mono_wasm_init_finalizer_thread (void);
 
@@ -537,6 +538,7 @@ mono_wasm_load_runtime (const char *unused, int debug_level)
 
 	mini_parse_debug_option ("top-runtime-invoke-unhandled");
 
+	mono_wasm_register_timezones_bundle();
 	mono_dl_fallback_register (wasm_dl_load, wasm_dl_symbol, NULL, NULL);
 	mono_wasm_install_get_native_to_interp_tramp (get_native_to_interp);
 
