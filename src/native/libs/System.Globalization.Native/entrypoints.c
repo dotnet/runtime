09d599e8df305e99e6e0523e775e18041f1e6348
Meri Khamoyan <merikhamoyan@microsoft.com>
2023-03-02 14:42:35 +0100

    Native functions only for apple

--- src/native/libs/System.Globalization.Native/entrypoints.c
+++ src/native/libs/System.Globalization.Native/entrypoints.c
@@ -58,8 +58,10 @@ static const Entry s_globalizationNative[] =
     DllImportEntry(GlobalizationNative_ToAscii)
     DllImportEntry(GlobalizationNative_ToUnicode)
     DllImportEntry(GlobalizationNative_WindowsIdToIanaId)
+#if defined(TARGET_MACCATALYST) || defined(TARGET_IOS) || defined(TARGET_TVOS)
     DllImportEntry(NativeGetLocaleName)
     DllImportEntry(NativeGetLocaleInfoString)
+#endif
 };
 
 EXTERN_C const void* GlobalizationResolveDllImport(const char* name);
