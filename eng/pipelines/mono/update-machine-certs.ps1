4f5b21613fd6f958e50af0e582143941714b3c31
Radek Doulik <radekdoulik@gmail.com>
2023-03-02 19:35:46 +0100

    Try to use pip-system-certs package
    Co-authored-by: Ankit Jain <radical@gmail.com>

--- eng/pipelines/mono/update-machine-certs.ps1
+++ eng/pipelines/mono/update-machine-certs.ps1
@@ -1,6 +1,7 @@
 # This seems to update the machine cert store so that python can download the files as required by emscripten's install
 # Based on info at https://pypi.org/project/certifi/
-pip install --force-reinstall certifi
+pip install certifi
+pip install pip-system-certs
 
 $WebsiteURL="storage.googleapis.com"
 Try {
