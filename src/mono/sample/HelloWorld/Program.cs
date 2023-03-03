6cdd45d66c104c6123b75c98a626690ae8e40712
Aleksey Kliger <alklig@microsoft.com>
2023-03-03 15:35:02 -0500

    WIP: update HelloWorld sample to run generic hotreload samples

--- src/mono/sample/HelloWorld/Program.cs
+++ src/mono/sample/HelloWorld/Program.cs
@@ -2,55 +2,74 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
+using System.IO;
+using System.Reflection;
 
-interface IDefault
+if (args.Length != 1)
 {
-    public void Method1()
-    {
-        Console.WriteLine("Interface Method1");
-    }
-
-    public object Method2()
-    {
-        Console.WriteLine("Interface Method2");
-        return null;
-    }
+    Console.WriteLine("Usage: generic-runner <directory>");
+    return 1;
 }
 
-abstract class ClassA : IDefault
+string directory = args[0];
+
+if (!Directory.Exists(directory))
 {
-    virtual public void Method1()
-    {
-        Console.WriteLine("ClassA Method 1");
-    }
+    Console.WriteLine("Directory does not exist: {0}", directory);
+    return 1;
 }
 
-class ClassB : ClassA
-{
-    public virtual object Method2()
-    {
-        Console.WriteLine("ClassB Method2");
-        return null;
-    }
-}
 
-namespace HelloWorld
+foreach (var subdir in Directory.GetDirectories(directory))
 {
-    internal class Program
+    // FIXME Mono: updates don't apply in custom ALCs
+    //var alc = new TestALC(subdir);
+    var alc = System.Runtime.Loader.AssemblyLoadContext.Default;
+
+    var assemblies = Directory.GetFiles(subdir, "*.dll");
+    foreach (var assembly in assemblies)
     {
-        private static void Main(string[] args)
+        var asm = alc.LoadFromAssemblyPath(assembly);
+        var type = asm.GetType("HotReloadTest", throwOnError: false);
+        if (type == null)
+            continue;
+        var mi = type.GetMethod("Run", BindingFlags.Static | BindingFlags.Public);
+        if (mi == null)
+            continue;
+        Console.WriteLine($"Running {asm.GetName().Name}");
+        var passed = false;
+        try
         {
-            bool isMono = typeof(object).Assembly.GetType("Mono.RuntimeStructs") != null;
-            Console.WriteLine($"Hello World {(isMono ? "from Mono!" : "from CoreCLR!")}");
-            Console.WriteLine(typeof(object).Assembly.FullName);
-            Console.WriteLine(System.Reflection.Assembly.GetEntryAssembly ());
-            Console.WriteLine(System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription);
-
-            IDefault c = new ClassB();
-
-            c.Method1();
-            c.Method2();
-            
+            mi.Invoke(null, null);
+            passed = true;
         }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+        if (passed)
+            Console.WriteLine($"Passed {asm.GetName().Name}");
+    }
+}
+
+return 0;
+
+class TestALC : System.Runtime.Loader.AssemblyLoadContext
+{
+    System.Runtime.Loader.AssemblyDependencyResolver _resolver;
+    public TestALC(string basedir) : base(false)
+    {
+        _resolver = new System.Runtime.Loader.AssemblyDependencyResolver(basedir);
+
+    }
+    protected override Assembly? Load(AssemblyName assemblyName)
+    {
+        string? assemblyPath = _resolver.ResolveAssemblyToPath(assemblyName);
+        if (assemblyPath != null)
+        {
+            return LoadFromAssemblyPath(assemblyPath);
+        }
+
+        return null;
     }
 }
