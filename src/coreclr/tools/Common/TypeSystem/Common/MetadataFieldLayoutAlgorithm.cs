295caf1aed637efaaf1fc47594cf58cd09cb6e75
vsadov <8218165+VSadov@users.noreply.github.com>
2023-03-03 16:25:56 -0800

    limit the max size of array instance to 1MiB

--- src/coreclr/tools/Common/TypeSystem/Common/MetadataFieldLayoutAlgorithm.cs
+++ src/coreclr/tools/Common/TypeSystem/Common/MetadataFieldLayoutAlgorithm.cs
@@ -468,9 +468,8 @@ namespace Internal.TypeSystem
                 long size = instanceByteSizeAndAlignment.Size.AsInt;
                 size *= repeat;
 
-                // matching the max size validation in MethodTableBuilder
-                // see: FIELD_OFFSET_LAST_REAL_OFFSET
-                const int maxSize = ((1 << 27) - 1) - 6;
+                // limit the max size of array instance to 1MiB
+                const int maxSize = 1024 * 1024;
                 if (size > maxSize)
                 {
                     ThrowHelper.ThrowTypeLoadException(ExceptionStringID.ClassLoadValueClassTooLarge, type);
