18a002aed91de2c79d38b498b9b114adbfb852dd
Zoltan Varga <vargaz@gmail.com>
2023-03-04 07:23:52 -0500

    [mono] Fix Hello World sample.

--- src/mono/sample/HelloWorld/Program.cs
+++ src/mono/sample/HelloWorld/Program.cs
@@ -3,37 +3,6 @@
 
 using System;
 
-interface IDefault
-{
-    public void Method1()
-    {
-        Console.WriteLine("Interface Method1");
-    }
-
-    public object Method2()
-    {
-        Console.WriteLine("Interface Method2");
-        return null;
-    }
-}
-
-abstract class ClassA : IDefault
-{
-    virtual public void Method1()
-    {
-        Console.WriteLine("ClassA Method 1");
-    }
-}
-
-class ClassB : ClassA
-{
-    public virtual object Method2()
-    {
-        Console.WriteLine("ClassB Method2");
-        return null;
-    }
-}
-
 namespace HelloWorld
 {
     internal class Program
@@ -45,12 +14,6 @@ namespace HelloWorld
             Console.WriteLine(typeof(object).Assembly.FullName);
             Console.WriteLine(System.Reflection.Assembly.GetEntryAssembly ());
             Console.WriteLine(System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription);
-
-            IDefault c = new ClassB();
-
-            c.Method1();
-            c.Method2();
-            
         }
     }
 }
