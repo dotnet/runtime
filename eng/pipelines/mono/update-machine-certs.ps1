1e02d7c49adfc1a5668ced44569d36b938b0d32b
Radek Doulik <radekdoulik@gmail.com>
2023-03-06 16:16:43 +0100

    Try to update machine certs for www.sqlite.org

--- eng/pipelines/mono/update-machine-certs.ps1
+++ eng/pipelines/mono/update-machine-certs.ps1
@@ -27,3 +27,28 @@ Try {
             Write-Host "Status:" $_.exception.innerexception.message -ForegroundColor Yellow
             Write-Host ""
 }
+
+$WebsiteURL="www.sqlite.org"
+Try {
+    $Conn = New-Object System.Net.Sockets.TcpClient($WebsiteURL,443)
+
+    Try {
+        $Stream = New-Object System.Net.Security.SslStream($Conn.GetStream())
+        $Stream.AuthenticateAsClient($WebsiteURL)
+
+        $Cert = $Stream.Get_RemoteCertificate()
+
+        $ValidTo = [datetime]::Parse($Cert.GetExpirationDatestring())
+
+        Write-Host "`nConnection Successful" -ForegroundColor DarkGreen
+        Write-Host "Website: $WebsiteURL"
+    }
+    Catch { Throw $_ }
+    Finally { $Conn.close() }
+    }
+    Catch {
+            Write-Host "`nError occurred connecting to $($WebsiteURL)" -ForegroundColor Yellow
+            Write-Host "Website: $WebsiteURL"
+            Write-Host "Status:" $_.exception.innerexception.message -ForegroundColor Yellow
+            Write-Host ""
+}
