a53d1b1cd1eef1323e57c0321a5dd047d66121a3
Mike McLaughlin <mikem@microsoft.com>
2023-03-06 17:07:06 -0800

    Add NT_SIGINFO NOTE to ELF dumps
    Linux Watson needs this to better triage ELF dumps.
    
    Add CreateDumpOptions helper struct to pass all the command options around. Add the
    "--code", "--errno", "--address" command line options used to fill the NT_SIGINFO
    NOTE. The runtime passes to createdump on a crash.
    
    Added "ExceptionType" field to "Parameters" section of the Linux crash report json.

--- src/coreclr/debug/createdump/crashinfo.h
+++ src/coreclr/debug/createdump/crashinfo.h
@@ -56,6 +56,7 @@ private:
 #ifdef __APPLE__
     vm_map_t m_task;                                // the mach task for the process
 #else
+    siginfo_t m_siginfo;                            // signal info (if any)
     bool m_canUseProcVmReadSyscall;
     int m_fd;                                       // /proc/<pid>/mem handle
 #endif
@@ -79,7 +80,7 @@ private:
     void operator=(const CrashInfo&) = delete;
 
 public:
-    CrashInfo(pid_t pid, bool gatherFrames, pid_t crashThread, uint32_t signal);
+    CrashInfo(const CreateDumpOptions& options);
     virtual ~CrashInfo();
 
     bool Initialize();
@@ -116,6 +117,7 @@ public:
 #ifndef __APPLE__
     inline const std::vector<elf_aux_entry>& AuxvEntries() const { return m_auxvEntries; }
     inline size_t GetAuxvSize() const { return m_auxvEntries.size() * sizeof(elf_aux_entry); }
+    inline const siginfo_t* SigInfo() const { return &m_siginfo; }
 #endif
 
     // IUnknown
