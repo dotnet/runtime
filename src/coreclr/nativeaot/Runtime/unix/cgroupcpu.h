1dd525fa51a75b7cddda93516ac8ad0a0ad551bb
Adeel <3840695+am11@users.noreply.github.com>
2023-03-07 10:32:44 +0200

    Rename cgroup.cpp to cgroupcpu.cpp

--- src/coreclr/nativeaot/Runtime/unix/cgroupcpu.h
+++ src/coreclr/nativeaot/Runtime/unix/cgroupcpu.h
@@ -1,12 +1,10 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-
 #ifndef __CGROUPCPU_H__
 #define __CGROUPCPU_H__
 
 void InitializeCpuCGroup();
-void CleanupCpuCGroup();
+bool GetCpuLimit(uint32_t* val);
 
 #endif // __CGROUPCPU_H__
-
