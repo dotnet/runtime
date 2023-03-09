4c458010c46474ff50053b586c74d4865c5c7765
Radek Doulik <radekdoulik@gmail.com>
2023-03-09 13:39:09 +0100

    Disable 3 filesystem related tests

--- src/libraries/System.IO.FileSystem/tests/File/GetSetTimes.cs
+++ src/libraries/System.IO.FileSystem/tests/File/GetSetTimes.cs
@@ -138,6 +138,7 @@ namespace System.IO.Tests
         }
 
         [Fact]
+        [ActiveIssue("https://github.com/dotnet/runtime/issues/83197", TestPlatforms.Browser)]
         public void SetLastWriteTimeTicks()
         {
             string firstFile = GetTestFilePath();
