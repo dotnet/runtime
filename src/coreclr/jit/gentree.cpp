03894d3c7070a031e57158e78a2120c52329e979
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2023-03-14 10:37:31 +0100

    JIT: Add missing 'MadeChanges = true' in gtSplitTree (#83360)
    Fix #83332

--- src/coreclr/jit/gentree.cpp
+++ src/coreclr/jit/gentree.cpp
@@ -16386,7 +16386,9 @@ bool Compiler::gtSplitTree(
                     // directly instead of the comma so that we get the proper
                     // location treatment. The edge will then be the User ---
                     // op2 edge.
-                    *use = (*use)->gtGetOp2();
+                    *use        = (*use)->gtGetOp2();
+                    MadeChanges = true;
+
                     UseInfo use2{use, user};
 
                     // Locations are never returned.
