3b0d6cb5c9807c7adaa006775859af492295b52e
Vlad - Alexandru Ionescu <vlad-alexandruionescu@Vlads-MacBook-Pro-4.local>
2023-03-16 19:35:06 +0100

    Check return value in the test

--- src/tests/JIT/Delegate/Tests.cs
+++ src/tests/JIT/Delegate/Tests.cs
@@ -4,15 +4,17 @@ using System.Runtime.InteropServices;
 public class Tests
 {
     public static int Main () {
+        int retVal = 100;
         try {
             var del = (Func<string, string>)Delegate.CreateDelegate (typeof (Func<string, string>), null, typeof (object).GetMethod ("ToString"));
-            Console.WriteLine (del ("FOO"));
+            if (del ("FOO") != "FOO")
+                retVal = 1;
         } catch(Exception e) {
             Console.WriteLine(e);
-            return 0;
+            retVal = 1;
         }
         
-        return 100;
+        return retVal;
         
     }
 }
