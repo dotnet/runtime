b156cdb7bd113fb86b8569477e61004123de110a
vsadov <8218165+VSadov@users.noreply.github.com>
2023-03-17 16:44:18 -0700

    all-pointer maps in AOT

--- src/coreclr/tools/Common/Internal/Runtime/GCDescEncoder.cs
+++ src/coreclr/tools/Common/Internal/Runtime/GCDescEncoder.cs
@@ -23,7 +23,7 @@ namespace Internal.Runtime
                 TypeDesc elementType = ((ArrayType)type).ElementType;
                 if (elementType.IsGCPointer)
                 {
-                    // For efficiency this is special cased and encoded as one serie.
+                    // For efficiency this is special cased and encoded as one serie
                     return 3 * type.Context.Target.PointerSize;
                 }
                 else if (elementType.IsDefType)
@@ -34,7 +34,7 @@ namespace Internal.Runtime
                         GCPointerMap pointerMap = GCPointerMap.FromInstanceLayout(defType);
                         if (pointerMap.IsAllGCPointers)
                         {
-                            // For efficiency this is special cased and encoded as one serie.
+                            // For efficiency this is special cased and encoded as one serie
                             return 3 * type.Context.Target.PointerSize;
                         }
                         else
@@ -51,9 +51,18 @@ namespace Internal.Runtime
                 var defType = (MetadataType)type;
                 if (defType.ContainsGCPointers)
                 {
-                    int numSeries = GCPointerMap.FromInstanceLayout(defType).NumSeries;
-                    Debug.Assert(numSeries > 0);
-                    return (numSeries * 2 + 1) * type.Context.Target.PointerSize;
+                    GCPointerMap pointerMap = GCPointerMap.FromInstanceLayout(defType);
+                    if (pointerMap.IsAllGCPointers)
+                    {
+                        // For efficiency this is special cased and encoded as one serie
+                        return 3 * type.Context.Target.PointerSize;
+                    }
+                    else
+                    {
+                        int numSeries = pointerMap.NumSeries;
+                        Debug.Assert(numSeries > 0);
+                        return (numSeries * 2 + 1) * type.Context.Target.PointerSize;
+                    }
                 }
             }
 
@@ -69,18 +78,18 @@ namespace Internal.Runtime
             {
                 TypeDesc elementType = ((ArrayType)type).ElementType;
 
-                // 2 means m_pEEType and _numComponents. Syncblock is sort of appended at the end of the object layout in this case.
-                int baseSize = 2 * builder.TargetPointerSize;
+                // 2 means m_pEEType and _numComponents.
+                int offsetToData = 2 * builder.TargetPointerSize;
 
                 if (type.IsMdArray)
                 {
                     // Multi-dim arrays include upper and lower bounds for each rank
-                    baseSize += 2 * sizeof(int) * ((ArrayType)type).Rank;
+                    offsetToData += 2 * sizeof(int) * ((ArrayType)type).Rank;
                 }
 
                 if (elementType.IsGCPointer)
                 {
-                    EncodeAllGCPointersArrayGCDesc(ref builder, baseSize);
+                    EncodeAllGCPointersGCDesc(ref builder, offsetToData);
                 }
                 else if (elementType.IsDefType)
                 {
@@ -90,11 +99,11 @@ namespace Internal.Runtime
                         GCPointerMap pointerMap = GCPointerMap.FromInstanceLayout(elementDefType);
                         if (pointerMap.IsAllGCPointers)
                         {
-                            EncodeAllGCPointersArrayGCDesc(ref builder, baseSize);
+                            EncodeAllGCPointersGCDesc(ref builder, offsetToData);
                         }
                         else
                         {
-                            EncodeArrayGCDesc(ref builder, pointerMap, baseSize);
+                            EncodeArrayGCDesc(ref builder, pointerMap, offsetToData);
                         }
                     }
                 }
@@ -104,13 +113,22 @@ namespace Internal.Runtime
                 var defType = (MetadataType)type;
                 if (defType.ContainsGCPointers)
                 {
-                    // Computing the layout for the boxed version if this is a value type.
-                    int offs = defType.IsValueType ? builder.TargetPointerSize : 0;
+                    GCPointerMap pointerMap = GCPointerMap.FromInstanceLayout(defType);
+                    if (pointerMap.IsAllGCPointers)
+                    {
+                        int offsetToData = builder.TargetPointerSize;
+                        EncodeAllGCPointersGCDesc(ref builder, offsetToData);
+                    }
+                    else
+                    {
+                        // Pointer map for value types is for the unboxed version, so need to add an offset
+                        int offs = defType.IsValueType ? builder.TargetPointerSize : 0;
 
-                    // Include syncblock
-                    int objectSize = defType.InstanceByteCount.AsInt + offs + builder.TargetPointerSize;
+                        // Include syncblock
+                        int objectSize = defType.InstanceByteCount.AsInt + offs + builder.TargetPointerSize;
 
-                    EncodeStandardGCDesc(ref builder, GCPointerMap.FromInstanceLayout(defType), objectSize, offs);
+                        EncodeStandardGCDesc(ref builder, GCPointerMap.FromInstanceLayout(defType), objectSize, offs);
+                    }
                 }
             }
 
@@ -150,22 +168,22 @@ namespace Internal.Runtime
         }
 
         // Arrays of all GC references are encoded as special kind of GC desc for efficiency
-        private static void EncodeAllGCPointersArrayGCDesc<T>(ref T builder, int baseSize)
+        private static void EncodeAllGCPointersGCDesc<T>(ref T builder, int offsetToData)
             where T : struct, ITargetBinaryWriter
         {
-            // Construct the gc info as if this array contains exactly one pointer
+            // Construct the gc info as if this instance contains exactly one pointer
             // - the encoding trick where the size of the series is measured as a difference from
-            // total object size will make this work for arbitrary array lengths
+            // total object size will make this work for arbitrary instance lengths
 
             // Series size
-            builder.EmitNaturalInt(-(baseSize + builder.TargetPointerSize));
+            builder.EmitNaturalInt(-(offsetToData + builder.TargetPointerSize));
             // Series offset
-            builder.EmitNaturalInt(baseSize);
+            builder.EmitNaturalInt(offsetToData);
             // NumSeries
             builder.EmitNaturalInt(1);
         }
 
-        private static void EncodeArrayGCDesc<T>(ref T builder, GCPointerMap map, int baseSize)
+        private static void EncodeArrayGCDesc<T>(ref T builder, GCPointerMap map, int ofsetToData)
             where T : struct, ITargetBinaryWriter
         {
             // NOTE: This format cannot properly represent element types with sizes >= 64k bytes.
@@ -212,7 +230,7 @@ namespace Internal.Runtime
             }
 
             Debug.Assert(numSeries > 0);
-            builder.EmitNaturalInt(baseSize + leadingNonPointerCount * pointerSize);
+            builder.EmitNaturalInt(ofsetToData + leadingNonPointerCount * pointerSize);
             builder.EmitNaturalInt(-numSeries);
         }
     }
