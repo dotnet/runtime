1c79441cd09e822a7e93903c5250c4da097e95c7
Fan Yang <yangfan@microsoft.com>
2023-03-17 06:42:26 -0700

    uncomment

--- src/mono/mono/mini/simd-intrinsics.c
+++ src/mono/mono/mini/simd-intrinsics.c
@@ -1190,10 +1190,10 @@ emit_sri_vector (MonoCompile *cfg, MonoMethod *cmethod, MonoMethodSignature *fsi
 		return NULL;
 #endif
 // FIXME: This limitation could be removed once everything here are supported by mini JIT on arm64
-// #ifdef TARGET_ARM64
-// 	if (!(cfg->compile_aot && cfg->full_aot && !cfg->interp))
-// 		return NULL;
-// #endif
+#ifdef TARGET_ARM64
+	if (!(cfg->compile_aot && cfg->full_aot && !cfg->interp))
+		return NULL;
+#endif
 
 	int id = lookup_intrins (sri_vector_methods, sizeof (sri_vector_methods), cmethod);
 	if (id == -1) {
