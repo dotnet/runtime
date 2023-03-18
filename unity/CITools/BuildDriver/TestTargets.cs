c8297e856627fe2049da71a81de0d9dcfa9ad737
Michael Voorhees <michaelv@unity3d.com>
2023-03-17 21:23:28 -0400

    New command line test abilities
    * Support `--test=embedding` or `--test=coreclr`
    
    * Add `--build` which can be used with `--test` to cause the test to build and test

---
+++ unity/CITools/BuildDriver/TestTargets.cs
@@ -0,0 +1,13 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace BuildDriver;
+
+[Flags]
+public enum TestTargets
+{
+    None = 1 << 0,
+    Embedding = 1 << 1,
+    CoreClr = 1 << 2,
+    All = ~0
+}
