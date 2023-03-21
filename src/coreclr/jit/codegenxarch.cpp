0fbc82c8b6320aac048cc2e5db5b0d062bbe8601
Egor Bogatov <egorbo@gmail.com>
2023-03-21 12:33:33 +0100

    Update codegenxarch.cpp

--- src/coreclr/jit/codegenxarch.cpp
+++ src/coreclr/jit/codegenxarch.cpp
@@ -2593,7 +2593,7 @@ void CodeGen::genCodeForMemmove(GenTreeBlk* tree)
     {
         // Number of SIMD regs needed to save the whole src to regs.
         unsigned numberOfSimdRegs = tree->AvailableTempRegCount(RBM_ALLFLOAT);
-        
+
         // Lowering takes care to only introduce this node such that we will always have enough
         // temporary SIMD registers to fully load the source and avoid any potential issues with overlap.
         assert(numberOfSimdRegs * simdSize >= size);
