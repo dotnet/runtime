3d08aa12dbff74b93d090d06b40114368433fe59
JBraunsma <jonbraunsma@gmail.com>
2023-03-24 02:21:43 -0400

    [83871] Added Any overload for arrays

--- src/libraries/System.Linq/src/System/Linq/AnyAll.cs
+++ src/libraries/System.Linq/src/System/Linq/AnyAll.cs
@@ -43,6 +43,29 @@ namespace System.Linq
             return false;
         }
 
+        public static bool Any<TSource>(this TSource[] source, Func<TSource, bool> predicate)
+        {
+            if (source == null)
+            {
+                ThrowHelper.ThrowArgumentNullException(ExceptionArgument.source);
+            }
+
+            if (predicate == null)
+            {
+                ThrowHelper.ThrowArgumentNullException(ExceptionArgument.predicate);
+            }
+
+            foreach (TSource element in source)
+            {
+                if (predicate(element))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
         public static bool All<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
         {
             if (source == null)
