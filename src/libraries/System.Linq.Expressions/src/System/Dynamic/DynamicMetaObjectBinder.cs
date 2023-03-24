490ed463e2e4bb859a5590cf8c2188c3fac423c5
Stephen Toub <stoub@microsoft.com>
2023-03-23 22:14:09 -0400

    Add ArgumentOutOfRangeException.ThrowIf{Not}Equal

--- src/libraries/System.Linq.Expressions/src/System/Dynamic/DynamicMetaObjectBinder.cs
+++ src/libraries/System.Linq.Expressions/src/System/Dynamic/DynamicMetaObjectBinder.cs
@@ -50,18 +50,9 @@ namespace System.Dynamic
             ArgumentNullException.ThrowIfNull(args);
             ArgumentNullException.ThrowIfNull(parameters);
             ArgumentNullException.ThrowIfNull(returnLabel);
-            if (args.Length == 0)
-            {
-                throw System.Linq.Expressions.Error.OutOfRange("args.Length", 1);
-            }
-            if (parameters.Count == 0)
-            {
-                throw System.Linq.Expressions.Error.OutOfRange("parameters.Count", 1);
-            }
-            if (args.Length != parameters.Count)
-            {
-                throw new ArgumentOutOfRangeException(nameof(args));
-            }
+            ArgumentOutOfRangeException.ThrowIfEqual(args.Length, 0);
+            ArgumentOutOfRangeException.ThrowIfEqual(parameters.Count, 0);
+            ArgumentOutOfRangeException.ThrowIfNotEqual(args.Length, parameters.Count);
 
             // Ensure that the binder's ReturnType matches CallSite's return
             // type. We do this so meta objects and language binders can
