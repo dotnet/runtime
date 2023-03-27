db18003a0154968247d0fafda0a3a9a38dddf1f1
Michael Voorhees <michaelv@unity3d.com>
2023-03-27 12:11:17 -0400

    Recipe engine & break up test jobs
    * Switch to Recipe Engine.  This makes it easier to adjust & reconfigure jobs as desired.
    
    * Change job organization.  There's really no need to have a .yml file per job like we currently do.
    
    * Break the different test categories out into different jobs.  Advantages being
    
       Reduces mental overhead of instabilities in a suite unrelated to changes in your PR.
    
       Gives the ability to run what you need.  If you change only the embedding api, you could run just the embedding tests and get quicker turn around.

---
+++ unity/CITools/Unity.Cookbook/Architecture.cs
@@ -0,0 +1,11 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Unity.Cookbook;
+
+public enum Architecture
+{
+    x64,
+    x86,
+    arm64
+}
---
+++ unity/CITools/Unity.Cookbook/Recipes/BaseTestRecipe.cs
@@ -0,0 +1,58 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using RecipeEngine.Api.Attributes;
+using RecipeEngine.Api.Dependencies;
+using RecipeEngine.Api.Extensions;
+using RecipeEngine.Api.Jobs;
+using RecipeEngine.Api.Recipes;
+using RecipeEngine.Platforms;
+using Unity.Cookbook.Modules;
+using Unity.Cookbook.Platforms;
+
+namespace Unity.Cookbook.Recipes;
+
+public abstract class BaseTestRecipe : RecipeBase
+{
+    private readonly DesktopPlatformSet _platformSet;
+
+    public BaseTestRecipe(DesktopPlatformSet platformSet)
+    {
+        _platformSet = platformSet;
+    }
+
+    protected abstract string DisplayName { get; }
+
+    protected abstract string BuildScriptTestArgumentValue { get; }
+
+    protected override ISet<Job> LoadJobs()
+    {
+        var jobs = Data.StandardJobConfigurations
+            .Select(CreateTestJob)
+            .ToArray();
+
+        return jobs
+            .Append(this.AllJob($"All {DisplayName} Tests", jobs))
+            .ToHashSet();
+    }
+
+    Job CreateTestJob(JobConfiguration configuration)
+        => CreateTestJob(_platformSet.Platforms[configuration.SystemType], configuration.Architecture, configuration.Configuration);
+
+    Job CreateTestJob(Platform platform, Architecture architecture, Configuration configuration)
+    {
+        return JobBuilder.Create($"Test - {DisplayName} - {platform.System.JobDisplayName()} {architecture} {configuration}")
+            .WithAgent(platform.Agent)
+            .WithCommands(
+                $"{Utils.BuildScriptForPlatform(platform)} --arch={architecture} --config={configuration} --test={BuildScriptTestArgumentValue}")
+            .WithTags(platform.System.ToString())
+            // The arm64 tests are currently broken and should not be included by the top level test jobs
+            .WithConditional(
+                architecture != Architecture.arm64,
+                b => b
+                    .WithTags(GroupingTag.TestJob),
+                b => b)
+            .WithDependencies(new Dependency(nameof(Build), platform.BuildJobName(architecture, configuration)))
+            .Build();
+    }
+}
