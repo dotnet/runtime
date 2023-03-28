ce4857970dff1e987a3991238c8b32e8eae3cab7
Jeremy Koritzinsky <jekoritz@microsoft.com>
2023-03-28 11:01:18 -0700

    Provide support for exposing .NET classes to COM through source generation (#83755)

--- src/libraries/System.Runtime.InteropServices/tests/Ancillary.Interop/GeneratedComClassAttribute.cs
+++ src/libraries/System.Runtime.InteropServices/tests/Ancillary.Interop/GeneratedComClassAttribute.cs
@@ -1,10 +1,12 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
+
 namespace System.Runtime.InteropServices.Marshalling
 {
-    [AttributeUsage(AttributeTargets.Interface)]
-    public class GeneratedComInterfaceAttribute : Attribute
+    [AttributeUsage(AttributeTargets.Class)]
+    public sealed class GeneratedComClassAttribute : Attribute
     {
     }
 }
