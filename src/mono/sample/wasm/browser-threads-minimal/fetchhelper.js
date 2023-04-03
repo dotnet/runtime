c8afabadae354c5a0697cca4d696e1b3e0db3364
Aleksey Kliger <alklig@microsoft.com>
2023-04-03 15:57:53 -0400

    change minimal sample's fetch helper to artificially delay
    the delay is longer that the threadpool worker's semaphore timeout, in
    order to validate that the worker stays alive while there are
    unsettled promises

--- src/mono/sample/wasm/browser-threads-minimal/fetchhelper.js
+++ src/mono/sample/wasm/browser-threads-minimal/fetchhelper.js
@@ -1,4 +1,11 @@
 
-export function responseText(response) /* Promise<string> */ {
-  return response.text();
+function delay(timeoutMs) {
+    return new Promise(resolve => setTimeout(resolve, timeoutMs));
+}
+
+export async function responseText(response) /* Promise<string> */ {
+  console.log("artificially waiting for response for 25 seconds");
+  await delay(25000);
+  console.log("artificial waiting done");
+  return await response.text();
 }
