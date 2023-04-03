86ccf9867abb6a8e6268138207382de7fa062169
Egor Bogatov <egorbo@gmail.com>
2023-04-03 18:04:01 +0200

    Update src/coreclr/tools/aot/ILCompiler.RyuJit/JitInterface/CorInfoImpl.RyuJit.cs
    Co-authored-by: Jan Kotas <jkotas@microsoft.com>

--- src/coreclr/tools/aot/ILCompiler.RyuJit/JitInterface/CorInfoImpl.RyuJit.cs
+++ src/coreclr/tools/aot/ILCompiler.RyuJit/JitInterface/CorInfoImpl.RyuJit.cs
@@ -2359,7 +2359,7 @@ namespace Internal.JitInterface
             MetadataType type = (MetadataType)HandleToObject(cls);
             addr.addr = (void*)ObjectToHandle(_compilation.NodeFactory.TypeNonGCStaticsSymbol(type));
             addr.accessType = InfoAccessType.IAT_VALUE;
-            offset = _compilation.NodeFactory.Target.PointerSize -
+            offset = -
                 NonGCStaticsNode.GetClassConstructorContextSize(_compilation.NodeFactory.Target);
             return true;
         }
