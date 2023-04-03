fd66b123c469c630e827fd4c172e26552bb87f40
xtqqczze <45661989+xtqqczze@users.noreply.github.com>
2023-04-03 15:01:20 +0100

    Use `UnmanagedCallConv`

--- src/libraries/Common/src/Interop/Windows/Kernel32/Interop.GetSystemTimePreciseAsFileTime.cs
+++ src/libraries/Common/src/Interop/Windows/Kernel32/Interop.GetSystemTimePreciseAsFileTime.cs
@@ -1,17 +1,21 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
 using System.Runtime.InteropServices;
 
 internal static partial class Interop
 {
     internal static partial class Kernel32
     {
+#pragma warning disable CS3016 // Arrays as attribute arguments is not CLS-compliant
         // https://learn.microsoft.com/windows/win32/api/sysinfoapi/nf-sysinfoapi-getsystemtimepreciseasfiletime
         [LibraryImport(Libraries.Kernel32)]
         [SuppressGCTransition]
+        [UnmanagedCallConv(CallConvs = new Type[] { typeof(System.Runtime.CompilerServices.CallConvCdecl) })]
         internal static unsafe partial void GetSystemTimePreciseAsFileTime(
             // [NativeTypeName("LPFILETIME")]
             ulong* lpSystemTimeAsFileTime);
     }
+#pragma warning restore CS3016 // Arrays as attribute arguments is not CLS-compliant
 }
