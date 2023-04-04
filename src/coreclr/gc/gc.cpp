864211fe0c96590f23b9c69c2fe7d0648646a491
mrsharm <musharm@microsoft.com>
2023-04-04 12:34:56 -0700

    Added the SetGCRegionRange for the regions range

--- src/coreclr/gc/gc.cpp
+++ src/coreclr/gc/gc.cpp
@@ -45356,7 +45356,8 @@ HRESULT GCHeap::Initialize()
         }
         gc_heap::regions_range = align_on_page(gc_heap::regions_range);
     }
-    // TODO: Set config after config API is merged.
+
+    GCConfig::SetGCRegionRange(gc_heap::regions_range);
 #endif //USE_REGIONS
 
 #endif //HOST_64BIT
