06180cc8103d7c4818ebbab88480592f3d14b00f
Eric StJohn <ericstj@microsoft.com>
2023-04-05 21:42:17 -0700

    Make WindowsServiceLifetime gracefully stop (#83892)
    * Make WindowsServiceLifetime gracefully stop
    
    WindowsServiceLifetime was not waiting for ServiceBase to stop the service.  As a result
    we would sometimes end the process before notifying service control manager that the service
    had stopped -- resulting in an error in the eventlog and sometimes a service restart.
    
    We also were permitting multiple calls to Stop to occur - through SCM callbacks, and through
    public API.  We must not call SetServiceStatus again once the service is marked as stopped.
    
    * Alternate approach to ensuring we only ever set STATE_STOPPED once.
    
    * Avoid calling ServiceBase.Stop on stopped service
    
    I fixed double-calling STATE_STOPPED in ServiceBase, but this fix will
    not be present on .NETFramework.  Workaround that by avoiding calling
    ServiceBase.Stop when the service has already been stopped by SCM.
    
    * Add tests for WindowsServiceLifetime
    
    These tests leverage RemoteExecutor to avoid creating a separate service
    assembly.
    
    * Respond to feedback and add more tests.
    
    This better integrates with the RemoteExecutor component as well,
    by hooking up the service process and fetching its handle.
    
    This gives us the correct logging and exitcode handling from
    RemoteExecutor.
    
    * Honor Cancellation in StopAsync
    
    * Fix bindingRedirects in RemoteExecutor
    
    * Use Async lambdas for service testing
    
    * Fix issue on Win7 where duplicate service descriptions are disallowed
    
    * Respond to feedback
    
    * Fix comment and add timeout

--- src/libraries/Microsoft.Extensions.Hosting.WindowsServices/test/UseWindowsServiceTests.cs
+++ src/libraries/Microsoft.Extensions.Hosting.WindowsServices/test/UseWindowsServiceTests.cs
@@ -1,14 +1,23 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
+using System.Reflection;
+using System.ServiceProcess;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting.Internal;
+using Microsoft.Extensions.Hosting.WindowsServices;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.EventLog;
+using Microsoft.Extensions.Options;
 using Xunit;
 
 namespace Microsoft.Extensions.Hosting
 {
     public class UseWindowsServiceTests
     {
+        private static MethodInfo? _addWindowsServiceLifetimeMethod = null;
+
         [Fact]
         public void DefaultsToOffOutsideOfService()
         {
@@ -21,6 +30,26 @@ namespace Microsoft.Extensions.Hosting
                 var lifetime = host.Services.GetRequiredService<IHostLifetime>();
                 Assert.IsType<ConsoleLifetime>(lifetime);
             }
+        }
+
+        [ConditionalFact(typeof(PlatformDetection), nameof(PlatformDetection.IsPrivilegedProcess))]
+        public void CanCreateService()
+        {
+            using var serviceTester = WindowsServiceTester.Create(() =>
+            {
+                using IHost host = new HostBuilder()
+                    .UseWindowsService()
+                    .Build();
+                host.Run();
+            });
+
+            serviceTester.Start();
+            serviceTester.WaitForStatus(ServiceControllerStatus.Running);
+            serviceTester.Stop();
+            serviceTester.WaitForStatus(ServiceControllerStatus.Stopped);
+
+            var status = serviceTester.QueryServiceStatus();
+            Assert.Equal(0, status.win32ExitCode);
         }
     }
 }
