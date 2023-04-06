71fb5c28c8b8ae3100c20682bafafd2c6c058e7f
Aleksey Kliger <aleksey@lambdageek.org>
2023-04-05 22:15:55 -0400

    add placeholder dn_rt functions for coreclr and nativeaot

--- src/native/containers/dn-rt.h
+++ src/native/containers/dn-rt.h
@@ -14,10 +14,43 @@ dn_rt_failfast_msgv(const char* fmt, va_list ap);
 DN_NORETURN static void
 dn_rt_failfast_nomsg(const char* file, int line);
 
-#if defined(FEATUERE_CORECLR)
+#if defined(FEATURE_CORECLR)
+#include "pal.h"
 // TODO: add CoreCLR runtime impl
+DN_NORETURN static inline void
+dn_rt_failfast_msgv(const char* fmt, va_list ap)
+{
+	RaiseFailFastException(nullptr, nullptr, 0);
+}
+
+DN_NORETURN static inline void
+dn_rt_failfast_nomsg(const char* file, int line)
+{
+	RaiseFailFastException(nullptr, nullptr, 0);
+}
+
 #elif defined(FEATURE_NATIVEAOT)
+#include "common.h"
+#include "gcenv.h"
+#include "CommonTypes.h"
+#include "CommonMacros.h"
+#include "PalRedhawkCommon.h"
+#include "PalRedhawk.h"
+#include "rhassert.h"
 // TODO: add NativeAOT runtime impl
+DN_NORETURN static inline void
+dn_rt_failfast_msgv(const char* fmt, va_list ap)
+{
+	RhFailFast();
+	UNREACHABLE();
+}
+
+DN_NORETURN static inline void
+dn_rt_failfast_nomsg(const char* file, int line)
+{
+	RhFailFast();
+	UNREACHABLE();
+}
 #else
 // Mono
 #include "dn-rt-mono.h"
