739aad93b97d196570c68528a42940500b995905
pedrobsaila <badrebsaila@outlook.com>
2023-04-09 00:12:09 +0200

    delete empty instruction

--- src/installer/tests/HostActivation.Tests/PortableAppActivation.cs
+++ src/installer/tests/HostActivation.Tests/PortableAppActivation.cs
@@ -205,7 +205,7 @@ namespace Microsoft.DotNet.CoreSetup.Test.HostActivation
                     .Should().Pass()
                     .And.HaveStdOutContaining("Hello World")
                     .And.HaveStdOutContaining(sharedTestState.RepoDirectories.MicrosoftNETCoreAppVersion)
-                    .And.NotHaveStdErrContaining("Failed to read environment variable"); ;
+                    .And.NotHaveStdErrContaining("Failed to read environment variable");
 
                 // Verify running from within the working directory
                 Command.Create(appExe)
@@ -222,7 +222,7 @@ namespace Microsoft.DotNet.CoreSetup.Test.HostActivation
                     .Should().Pass()
                     .And.HaveStdOutContaining("Hello World")
                     .And.HaveStdOutContaining(sharedTestState.RepoDirectories.MicrosoftNETCoreAppVersion)
-                    .And.NotHaveStdErrContaining("Failed to read environment variable"); ;
+                    .And.NotHaveStdErrContaining("Failed to read environment variable");
             }
         }
 
