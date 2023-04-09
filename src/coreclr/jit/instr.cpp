f1049dfa5ce6c444c9e98856f64521bfbb59e6be
EgorBo <egorbo@gmail.com>
2023-04-09 20:09:37 +0200

    fix minor jitdisasm issue

--- src/coreclr/jit/instr.cpp
+++ src/coreclr/jit/instr.cpp
@@ -238,8 +238,8 @@ const char* CodeGen::genSizeStr(emitAttr attr)
         "dword ptr ",
         "qword ptr ",
         "xmmword ptr ",
-        "ymmword ptr",
-        "zmmword ptr"
+        "ymmword ptr ",
+        "zmmword ptr "
     };
     // clang-format on
 
