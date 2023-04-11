df1f49bf37a832ad59d2a0e4e95b7417d2c309f4
petersolMS <petersol@microsoft.com>
2023-04-11 14:08:33 +0200

    Compile coreclr.dll with regions, enable memory mapped stresslog

--- src/coreclr/gc/gcpriv.h
+++ src/coreclr/gc/gcpriv.h
@@ -138,7 +138,7 @@ inline void FATAL_GC_ERROR()
 // This means any empty regions can be freely used for any generation. For
 // Server GC we will balance regions between heaps.
 // For now disable regions for StandAlone GC, NativeAOT and MacOS builds
-#if defined (HOST_64BIT) && defined (BUILD_AS_STANDALONE) && !defined(__APPLE__)
+#if defined (HOST_64BIT) && !defined (BUILD_AS_STANDALONE) && !defined(__APPLE__)
 #define USE_REGIONS
 #endif //HOST_64BIT && BUILD_AS_STANDALONE
 
@@ -229,7 +229,7 @@ inline void FATAL_GC_ERROR()
 #endif // MAX_LONGPATH
 
 #define TRACE_GC
-#define SIMPLE_DPRINTF
+//#define SIMPLE_DPRINTF
 
 //#define JOIN_STATS         //amount of time spent in the join
 
