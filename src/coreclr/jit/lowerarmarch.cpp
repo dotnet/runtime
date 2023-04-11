860c8dec1edd8397902b897615777d48d06066a2
TIHan <lol.tihan@gmail.com>
2023-04-10 20:12:52 -0700

    Remove commented code

--- src/coreclr/jit/lowerarmarch.cpp
+++ src/coreclr/jit/lowerarmarch.cpp
@@ -2288,43 +2288,6 @@ void Lowering::ContainCheckCompare(GenTreeOp* cmp)
         }
     }
 #endif
-
-//#ifdef TARGET_ARM64
-//    if (comp->opts.OptimizationEnabled() && cmp->gtGetOp2()->OperIs(GT_LSH, GT_RSH, GT_RSZ))
-//    {
-//        auto isValidImmForShift = [](ssize_t imm, var_types type)
-//        { return emitter::isValidImmShift(imm, EA_ATTR(genTypeSize(type))); };
-//
-//        GenTree* op2 = cmp->gtGetOp2();
-//
-//        if (op2->gtGetOp2()->IsCnsIntOrI() &&
-//            isValidImmForShift(op2->gtGetOp2()->AsIntConCommon()->IntegralValue(), cmp->TypeGet()) &&
-//            IsContainableBinaryOp(cmp, cmp->gtGetOp2()))
-//        {
-//            op2->ClearContained();
-//            op2->gtGetOp1()->ClearContained();
-//
-//            MakeSrcContained(op2, op2->gtGetOp2());
-//            MakeSrcContained(cmp, op2);
-//        }
-//        else if (cmp->OperIsCommutative() && IsContainableBinaryOp(cmp, cmp->gtGetOp1()))
-//        {
-//        }
-//
-//        //        if (node->OperIsCommutative() && IsContainableBinaryOp(node, op1))
-//        //{
-//        //    if (op1->OperIs(GT_CAST))
-//        //    {
-//        //        // We want to prefer the combined op here over containment of the cast op
-//        //        op1->AsCast()->CastOp()->ClearContained();
-//        //    }
-//        //    MakeSrcContained(node, op1);
-//
-//        //    std::swap(node->gtOp1, node->gtOp2);
-//        //    return;
-//        //}
-//    }
-//#endif // TARGET_ARM64
 }
 
 #ifdef TARGET_ARM64
