94af6fa6137f7ac9e41cbb819ea6cb27de25cffc
Levi Broderick <levib@microsoft.com>
2023-04-12 16:02:20 -0700

    Fix whitespace

--- src/native/external/zlib/dotnet_allocator_unix.c
+++ src/native/external/zlib/dotnet_allocator_unix.c
@@ -79,7 +79,7 @@ static void WriteAllocCookieUnaligned(void* pDest, DOTNET_ALLOC_COOKIE vCookie)
 const size_t DOTNET_ALLOC_HEADER_COOKIE_SIZE_WITH_PADDING = (sizeof(DOTNET_ALLOC_COOKIE) + MEMORY_ALLOCATION_ALIGNMENT - 1) & ~((size_t)MEMORY_ALLOCATION_ALIGNMENT  - 1);
 const size_t DOTNET_ALLOC_TRAILER_COOKIE_SIZE = sizeof(DOTNET_ALLOC_COOKIE);
 
-voidpf ZLIB_INTERNAL zcalloc (opaque, items, size)
+voidpf ZLIB_INTERNAL zcalloc(opaque, items, size)
     voidpf opaque;
     unsigned items;
     unsigned size;
@@ -128,7 +128,7 @@ static void zcfree_trash_cookie(void* pCookie)
     memset(pCookie, 0, sizeof(DOTNET_ALLOC_COOKIE));
 }
 
-void ZLIB_INTERNAL zcfree (opaque, ptr)
+void ZLIB_INTERNAL zcfree(opaque, ptr)
     voidpf opaque;
     voidpf ptr;
 {
