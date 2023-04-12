3f59f4d16e5e0bf263ce478755269dfdaae05b25
WuXinlong <821408745@qq.com>
2023-04-12 18:51:05 +0800

    update test case

--- src/mono/sample/TestRun/TestRun.cs
+++ src/mono/sample/TestRun/TestRun.cs
@@ -3,14 +3,14 @@ namespace HelloWorld
 {
   class Program
   {
-    static int test(){
-      throw new System.Exception();
+    static int test(int c){
+      return 1 / c;
     }
     static int Main()
     {
       Console.WriteLine($"IsMono: {Type.GetType("Mono.RuntimeStructs") != null}");
       Console.WriteLine("Hello RISC-V");
-      test();
+      // test(0);
       return 0;
     }
   }
