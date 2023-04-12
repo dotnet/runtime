3687f38f40a039547c6f0bf8ce5d8be7e99c77a7
Levi Broderick <levib@microsoft.com>
2023-04-12 16:42:01 -0700

    Remove dead code from cmake file

--- src/native/external/zlib-intel.cmake
+++ src/native/external/zlib-intel.cmake
@@ -1,7 +1,11 @@
 if(MSVC)
+    add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:/DMY_ZCALLOC>) # custom zlib allocator
     add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:/wd4244>) # conversion from 'type1' to 'type2', possible loss of data
-else(CMAKE_C_COMPILER_ID MATCHES "Clang")
-    add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:-Wno-implicit-int-conversion>)
+else()
+    add_definitions(-DMY_ZCALLOC) # custom zlib allocator
+    if(CMAKE_C_COMPILER_ID MATCHES "Clang")
+        add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:-Wno-implicit-int-conversion>)
+    endif()
 endif()
 
 set(ZLIB_SOURCES_BASE
@@ -20,15 +24,7 @@ set(ZLIB_SOURCES_BASE
     trees.c
     x86.c
     zutil.c
+    ../zlib/dotnet_allocator_win.c
 )
-
-# Configure our custom allocator
-if(MSVC)
-    set(ZLIB_SOURCES_BASE ${ZLIB_SOURCES_BASE} ../zlib/dotnet_allocator_win.c)
-    add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:/DMY_ZCALLOC>)
-else()
-    set(ZLIB_SOURCES_BASE ${ZLIB_SOURCES_BASE} ../zlib/dotnet_allocator_unix.c)
-    add_definitions(-DMY_ZCALLOC)
-endif()
 
 addprefix(ZLIB_SOURCES "${CMAKE_CURRENT_LIST_DIR}/zlib-intel"  "${ZLIB_SOURCES_BASE}")
