cfa8deded109d28e411a2fc212fd02f802cc8af7
Jan Dupej <jandupej@microsoft.com>
2023-04-13 10:27:37 +0200

    PInvoke

--- src/tasks/MarshalingPInvokeScanner/MarshalingPInvokeScanner.cs
+++ src/tasks/MarshalingPInvokeScanner/MarshalingPInvokeScanner.cs
@@ -5,12 +5,87 @@ using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Diagnostics.CodeAnalysis;
+using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
+using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Reflection;
+using System.Reflection.PortableExecutable;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
+
+
+internal sealed class MinimalMarshalingTypeCompatibilityProvider : ISignatureTypeProvider<bool, object>
+{
+
+    //Dictionary<string, 
+
+    public bool GetArrayType(bool elementType, ArrayShape shape) => false;
+    public bool GetByReferenceType(bool elementType) => true;
+    public bool GetFunctionPointerType(MethodSignature<bool> signature) => true;
+    public bool GetGenericInstantiation(bool gennricType, ImmutableArray<bool> typeArguments) => false;
+    public bool GetGenericMethodParameter(object genericContext, int index) => false;
+    public bool GetGenericTypeParameter(object genericContext, int index) => false;
+    public bool GetModifiedType(bool modifier, bool unmodifiedType, bool isRequired) => false;
+    public bool GetPinnedType(bool elementType) => true; // TODO: really?
+    public bool GetPointerType(bool elementType) => true;
+    public bool GetPrimitiveType(PrimitiveTypeCode typeCode)
+    {
+        return typeCode switch
+        {
+           PrimitiveTypeCode.Object => false,
+           PrimitiveTypeCode.String => false,
+           PrimitiveTypeCode.TypedReference => false, // TODO: really?
+           _ => true
+        };
+    }
+
+    public bool GetSZArrayType(bool elementType) => false;
+
+    public bool GetTypeFromDefinition(MetadataReader reader, TypeDefinitionHandle handle, byte rawTypeKind)
+    {
+        TypeDefinition typeDef = reader.GetTypeDefinition(handle);
+
+        EntityHandle baseTypeHandle = typeDef.BaseType;
+        if(baseTypeHandle.Kind == HandleKind.TypeReference)
+        {
+            TypeReference baseType = reader.GetTypeReference((TypeReferenceHandle)baseTypeHandle);
+            if (reader.GetString(baseType.Name) == "Enum")
+                return true;
+        }
+        else
+        {
+            throw new NotImplementedException();
+        }
+
+        return false;
+    }
+
+    public bool GetTypeFromReference(MetadataReader reader, TypeReferenceHandle handle, byte rawTypeKind)
+    {
+        if (rawTypeKind == 0x11 /*ELEMENT_TYPE_VALUETYPE*/)
+        {
+            TypeReference typeRef = reader.GetTypeReference(handle);
+            EntityHandle scope = typeRef.ResolutionScope;
+
+            if (scope.Kind == HandleKind.AssemblyReference)
+            {
+                AssemblyReferenceHandle assyRefHandle = (AssemblyReferenceHandle)typeRef.ResolutionScope;
+                AssemblyReference assyRef = reader.GetAssemblyReference(assyRefHandle);
+
+            }
+        }
+    }
+
+    public bool GetTypeFromSpecification(MetadataReader reader, object genericContext, TypeSpecificationHandle handle, byte rawTypeKind) =>
+        false; // TODO: Really?
+}
+
+
+
+
 
 public class MarshalingPInvokeScanner : Task
 {
@@ -51,7 +126,7 @@ public class MarshalingPInvokeScanner : Task
             IncompatibleAssemblies = ScanAssemblies(Assemblies);
     }
 
-    private string[] ScanAssemblies(string[] assemblies)
+    private static string[] ScanAssemblies(string[] assemblies)
     {
         List<string> incompatible = new List<string>();
 
@@ -59,21 +134,84 @@ public class MarshalingPInvokeScanner : Task
         using MetadataLoadContext mlc = new MetadataLoadContext(resolver, "System.Private.CoreLib");
         foreach (string aname in assemblies)
         {
-            Assembly assy = mlc.LoadFromAssemblyPath(aname);
-            List<PInvoke> pinvokes = new List<PInvoke>();
-            List<string> signatures = new List<string>();
-            List<PInvokeCallback> callbacks = new List<PInvokeCallback>();
-            PInvokeCollector pinvokeCollector = new PInvokeCollector(Log);
-
-            foreach (Type type in assy.GetTypes())
-                pinvokeCollector.CollectPInvokes(pinvokes, callbacks, signatures, type);
-
-            if (IsAssemblyIncompatible(assy, pinvokes, signatures, callbacks))
+            if (IsAssemblyIncompatible(aname))
                 incompatible.Add(aname);
         }
 
         return incompatible.ToArray();
     }
+
+    private static string GetMethodName(MetadataReader mr, MethodDefinition md)
+    {
+        return mr.GetString(md.Name);
+    }
+
+    private static bool IsAssemblyIncompatible(string path)
+    {
+        using FileStream file = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+        using PEReader peReader = new PEReader(file);
+        MetadataReader mdtReader = peReader.GetMetadataReader();
+
+        Console.WriteLine(path);
+
+        foreach(CustomAttributeHandle attrHandle in mdtReader.CustomAttributes)
+        {
+            CustomAttribute attr = mdtReader.GetCustomAttribute(attrHandle);
+
+            try
+            {
+                MethodDefinitionHandle mdh = (MethodDefinitionHandle)attr.Constructor;
+                MethodDefinition md = mdtReader.GetMethodDefinition(mdh);
+                TypeDefinitionHandle tdh = md.GetDeclaringType();
+                TypeDefinition td = mdtReader.GetTypeDefinition(tdh);
+
+                if(mdtReader.GetString(td.Name) == "DisableRuntimeMarshallingAttribute")
+                {
+                    Console.WriteLine(string.Format("Assembly {0} is annotated with DisableRuntimeMarshallingAttribute.", path));
+                    return false;
+                }
+            }
+            catch(InvalidCastException)
+            {
+            }
+        }
+
+        MinimalMarshalingTypeCompatibilityProvider mmtcp =  new MinimalMarshalingTypeCompatibilityProvider();
+        foreach (TypeDefinitionHandle typeDefHandle in mdtReader.TypeDefinitions)
+        {
+            TypeDefinition typeDef = mdtReader.GetTypeDefinition(typeDefHandle);
+            string ns = mdtReader.GetString(typeDef.Namespace);
+            string name = mdtReader.GetString(typeDef.Name);
+
+            foreach(MethodDefinitionHandle mthDefHandle in typeDef.GetMethods())
+            {
+                MethodDefinition mthDef = mdtReader.GetMethodDefinition(mthDefHandle);
+                if(!mthDef.Attributes.HasFlag(MethodAttributes.PinvokeImpl))
+                    continue;
+
+                BlobReader sgnBlobReader = mdtReader.GetBlobReader(mthDef.Signature);
+                SignatureDecoder<bool, object> decoder = new SignatureDecoder<bool, object>(
+                    mmtcp, mdtReader, null!);
+
+                MethodSignature<bool> sgn = decoder.DecodeMethodSignature(ref sgnBlobReader);
+                if(!sgn.ReturnType)
+                {
+                    Console.WriteLine("   " + GetMethodName(mdtReader, mthDef) + " - incompatible return type.");
+                    return true;
+                }
+
+                if(!sgn.ParameterTypes.All(p => p)) // if not all are true
+                {
+                    Console.WriteLine("   " + GetMethodName(mdtReader, mthDef) + " - incompatible parameter type.");
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+
 
     #pragma warning disable IDE0060
     private bool IsAssemblyIncompatible(Assembly assy, List<PInvoke> pivs, List<string> strs, List<PInvokeCallback> cbks)
