061a65536031eea9114777da030fe53020de9811
Levi Broderick <levib@microsoft.com>
2023-04-12 17:17:42 -0700

    Normalize cmake files

--- src/native/external/zlib-intel.cmake
+++ src/native/external/zlib-intel.cmake
@@ -1,11 +1,7 @@
 if(MSVC)
-    add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:/DMY_ZCALLOC>) # custom zlib allocator
     add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:/wd4244>) # conversion from 'type1' to 'type2', possible loss of data
-else()
-    add_definitions(-DMY_ZCALLOC) # custom zlib allocator
-    if(CMAKE_C_COMPILER_ID MATCHES "Clang")
-        add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:-Wno-implicit-int-conversion>)
-    endif()
+else(CMAKE_C_COMPILER_ID MATCHES "Clang")
+    add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:-Wno-implicit-int-conversion>)
 endif()
 
 set(ZLIB_SOURCES_BASE
@@ -26,5 +22,8 @@ set(ZLIB_SOURCES_BASE
     zutil.c
     ../zlib/dotnet_allocator_win.c
 )
+
+# enable custom zlib allocator
+add_definitions(-DMY_ZCALLOC)
 
 addprefix(ZLIB_SOURCES "${CMAKE_CURRENT_LIST_DIR}/zlib-intel"  "${ZLIB_SOURCES_BASE}")
--- src/native/external/zlib.cmake
+++ src/native/external/zlib.cmake
@@ -29,13 +29,12 @@ set(ZLIB_SOURCES_BASE
     zutil.h
 )
 
-# Configure our custom allocator
-if(MSVC)
+# enable custom zlib allocator
+add_definitions(-DMY_ZCALLOC)
+if(CLR_CMAKE_HOST_WIN32)
     set(ZLIB_SOURCES_BASE ${ZLIB_SOURCES_BASE} dotnet_allocator_win.c)
-    add_compile_options($<$<COMPILE_LANGUAGE:C,CXX>:/DMY_ZCALLOC>)
 else()
     set(ZLIB_SOURCES_BASE ${ZLIB_SOURCES_BASE} dotnet_allocator_unix.c)
-    add_definitions(-DMY_ZCALLOC)
 endif()
 
 addprefix(ZLIB_SOURCES "${CMAKE_CURRENT_LIST_DIR}/zlib"  "${ZLIB_SOURCES_BASE}")
