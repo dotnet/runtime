8f296766e0fca4a468937c75087302b5a2dacc69
Jeremy Koritzinsky <jekoritz@microsoft.com>
2023-04-14 16:53:01 -0700

    The memory allocated by AllocateTypeAssociatedMemory is expected to be zeroed (CoreCLR does this). The interop source generators depend on this behavior for lazy initialization.

--- src/coreclr/nativeaot/System.Private.CoreLib/src/System/Runtime/CompilerServices/RuntimeHelpers.NativeAot.cs
+++ src/coreclr/nativeaot/System.Private.CoreLib/src/System/Runtime/CompilerServices/RuntimeHelpers.NativeAot.cs
@@ -274,7 +274,10 @@ namespace System.Runtime.CompilerServices
             ArgumentOutOfRangeException.ThrowIfNegative(size);
 
             // We don't support unloading; the memory will never be freed.
-            return (IntPtr)NativeMemory.Alloc((uint)size);
+            void* mem = NativeMemory.Alloc((uint)size);
+            NativeMemory.Clear(mem, (uint)size);
+
+            return (IntPtr)mem;
         }
 
         public static void PrepareDelegate(Delegate d)
