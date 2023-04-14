dotnet/runtime

commit 5117e1f78d2f4924181a9ed6d1fd525f27a9fc91

Annotate XslCompiledTransform as RequiresDynamicCode. (#84426)
* Annotate XslCompiledTransform as RequiresDynamicCode.

* Annotate System.Security.Cryptography.Xml as RequiresDynamicCode

* Annotate Microsoft.Extensions.Configuration.Xml as RequiresDynamicCode

src/libraries/System.Security.Cryptography.Xml/src/Resources/Strings.resx
@@ -396,4 +396,7 @@
   <data name="Cryptography_Xml_EntityResolutionNotSupported" xml:space="preserve">
     <value>External entity resolution is not supported.</value>
   </data>
+  <data name="Cryptography_Xml_XsltRequiresDynamicCode" xml:space="preserve">
+    <value>Xslts are only supported when dynamic code is supported.</value>
+  </data>
 </root>

src/libraries/System.Security.Cryptography.Xml/src/System/Security/Cryptography/Xml/Reference.cs
@@ -202,6 +202,7 @@ namespace System.Security.Cryptography.Xml
             return referenceElement;
         }
 
+        [RequiresDynamicCode(CryptoHelpers.XsltRequiresDynamicCodeMessage)]
         public void LoadXml(XmlElement value)
         {
             if (value is null)
@@ -343,7 +344,7 @@ namespace System.Security.Cryptography.Xml
         {
             // refList is a list of elements that might be targets of references
             // Now's the time to create our hashing algorithm
-            _hashAlgorithm = CryptoHelpers.CreateFromName<HashAlgorithm>(_digestMethod);
+            _hashAlgorithm = CryptoHelpers.CreateNonTransformFromName<HashAlgorithm>(_digestMethod);
             if (_hashAlgorithm == null)
                 throw new CryptographicException(SR.Cryptography_Xml_CreateHashAlgorithmFailed);
 

src/libraries/System.Security.Cryptography.Xml/src/System/Security/Cryptography/Xml/TransformChain.cs
@@ -14,6 +14,7 @@
 // stream. (We only bother implementing that much now since every use of transform chains in XmlDsig ultimately yields something to hash).
 
 using System.Collections;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Xml;
 
@@ -166,6 +167,7 @@ namespace System.Security.Cryptography.Xml
             return transformsElement;
         }
 
+        [RequiresDynamicCode(CryptoHelpers.XsltRequiresDynamicCodeMessage)]
         internal void LoadXml(XmlElement value)
         {
             if (value is null)

src/libraries/Microsoft.Extensions.Configuration.Xml/ref/Microsoft.Extensions.Configuration.Xml.cs
@@ -8,21 +8,29 @@ namespace Microsoft.Extensions.Configuration
 {
     public static partial class XmlConfigurationExtensions
     {
+        [System.Diagnostics.CodeAnalysis.RequiresDynamicCodeAttribute("Microsoft.Extensions.Configuration.Xml can use EncryptedXml which may contain XSLTs in the xml. XSLTs require dynamic code.")]
         public static Microsoft.Extensions.Configuration.IConfigurationBuilder AddXmlFile(this Microsoft.Extensions.Configuration.IConfigurationBuilder builder, Microsoft.Extensions.FileProviders.IFileProvider? provider, string path, bool optional, bool reloadOnChange) { throw null; }
+        [System.Diagnostics.CodeAnalysis.RequiresDynamicCodeAttribute("Microsoft.Extensions.Configuration.Xml can use EncryptedXml which may contain XSLTs in the xml. XSLTs require dynamic code.")]
         public static Microsoft.Extensions.Configuration.IConfigurationBuilder AddXmlFile(this Microsoft.Extensions.Configuration.IConfigurationBuilder builder, System.Action<Microsoft.Extensions.Configuration.Xml.XmlConfigurationSource>? configureSource) { throw null; }
+        [System.Diagnostics.CodeAnalysis.RequiresDynamicCodeAttribute("Microsoft.Extensions.Configuration.Xml can use EncryptedXml which may contain XSLTs in the xml. XSLTs require dynamic code.")]
         public static Microsoft.Extensions.Configuration.IConfigurationBuilder AddXmlFile(this Microsoft.Extensions.Configuration.IConfigurationBuilder builder, string path) { throw null; }
+        [System.Diagnostics.CodeAnalysis.RequiresDynamicCodeAttribute("Microsoft.Extensions.Configuration.Xml can use EncryptedXml which may contain XSLTs in the xml. XSLTs require dynamic code.")]
         public static Microsoft.Extensions.Configuration.IConfigurationBuilder AddXmlFile(this Microsoft.Extensions.Configuration.IConfigurationBuilder builder, string path, bool optional) { throw null; }
+        [System.Diagnostics.CodeAnalysis.RequiresDynamicCodeAttribute("Microsoft.Extensions.Configuration.Xml can use EncryptedXml which may contain XSLTs in the xml. XSLTs require dynamic code.")]
         public static Microsoft.Extensions.Configuration.IConfigurationBuilder AddXmlFile(this Microsoft.Extensions.Configuration.IConfigurationBuilder builder, string path, bool optional, bool reloadOnChange) { throw null; }
+        [System.Diagnostics.CodeAnalysis.RequiresDynamicCodeAttribute("Microsoft.Extensions.Configuration.Xml can use EncryptedXml which may contain XSLTs in the xml. XSLTs require dynamic code.")]
         public static Microsoft.Extensions.Configuration.IConfigurationBuilder AddXmlStream(this Microsoft.Extensions.Configuration.IConfigurationBuilder builder, System.IO.Stream stream) { throw null; }
     }
 }
 namespace Microsoft.Extensions.Configuration.Xml
 {
+    [System.Diagnostics.CodeAnalysis.RequiresDynamicCodeAttribute("Microsoft.Extensions.Configuration.Xml can use EncryptedXml which may contain XSLTs in the xml. XSLTs require dynamic code.")]
     public partial class XmlConfigurationProvider : Microsoft.Extensions.Configuration.FileConfigurationProvider
     {
         public XmlConfigurationProvider(Microsoft.Extensions.Configuration.Xml.XmlConfigurationSource source) : base (default(Microsoft.Extensions.Configuration.FileConfigurationSource)) { }
         public override void Load(System.IO.Stream stream) { }
     }
+    [System.Diagnostics.CodeAnalysis.RequiresDynamicCodeAttribute("Microsoft.Extensions.Configuration.Xml can use EncryptedXml which may contain XSLTs in the xml. XSLTs require dynamic code.")]
     public partial class XmlConfigurationSource : Microsoft.Extensions.Configuration.FileConfigurationSource
     {
         public XmlConfigurationSource() { }
@@ -32,16 +40,20 @@ namespace Microsoft.Extensions.Configuration.Xml
     {
         public static readonly Microsoft.Extensions.Configuration.Xml.XmlDocumentDecryptor Instance;
         protected XmlDocumentDecryptor() { }
+        [System.Diagnostics.CodeAnalysis.RequiresDynamicCodeAttribute("Microsoft.Extensions.Configuration.Xml can use EncryptedXml which may contain XSLTs in the xml. XSLTs require dynamic code.")]
         public System.Xml.XmlReader CreateDecryptingXmlReader(System.IO.Stream input, System.Xml.XmlReaderSettings? settings) { throw null; }
-        [System.Runtime.Versioning.UnsupportedOSPlatform("browser")]
+        [System.Diagnostics.CodeAnalysis.RequiresDynamicCodeAttribute("Microsoft.Extensions.Configuration.Xml can use EncryptedXml which may contain XSLTs in the xml. XSLTs require dynamic code.")]
+        [System.Runtime.Versioning.UnsupportedOSPlatformAttribute("browser")]
         protected virtual System.Xml.XmlReader DecryptDocumentAndCreateXmlReader(System.Xml.XmlDocument document) { throw null; }
     }
+    [System.Diagnostics.CodeAnalysis.RequiresDynamicCodeAttribute("Microsoft.Extensions.Configuration.Xml can use EncryptedXml which may contain XSLTs in the xml. XSLTs require dynamic code.")]
     public partial class XmlStreamConfigurationProvider : Microsoft.Extensions.Configuration.StreamConfigurationProvider
     {
         public XmlStreamConfigurationProvider(Microsoft.Extensions.Configuration.Xml.XmlStreamConfigurationSource source) : base (default(Microsoft.Extensions.Configuration.StreamConfigurationSource)) { }
         public override void Load(System.IO.Stream stream) { }
         public static System.Collections.Generic.IDictionary<string, string?> Read(System.IO.Stream stream, Microsoft.Extensions.Configuration.Xml.XmlDocumentDecryptor decryptor) { throw null; }
     }
+    [System.Diagnostics.CodeAnalysis.RequiresDynamicCodeAttribute("Microsoft.Extensions.Configuration.Xml can use EncryptedXml which may contain XSLTs in the xml. XSLTs require dynamic code.")]
     public partial class XmlStreamConfigurationSource : Microsoft.Extensions.Configuration.StreamConfigurationSource
     {
         public XmlStreamConfigurationSource() { }

src/libraries/Microsoft.Extensions.Configuration.Xml/ref/Microsoft.Extensions.Configuration.Xml.csproj
@@ -7,6 +7,10 @@
     <Compile Include="Microsoft.Extensions.Configuration.Xml.cs" />
   </ItemGroup>
 
+  <ItemGroup Condition="!$([MSBuild]::IsTargetFrameworkCompatible('$(TargetFramework)', 'net7.0'))">
+    <Compile Include="$(CoreLibSharedDir)System\Diagnostics\CodeAnalysis\RequiresDynamicCodeAttribute.cs" />
+  </ItemGroup>
+
   <ItemGroup>
     <ProjectReference Include="$(LibrariesProjectRoot)Microsoft.Extensions.Configuration\ref\Microsoft.Extensions.Configuration.csproj" />
     <ProjectReference Include="$(LibrariesProjectRoot)Microsoft.Extensions.Configuration.Abstractions\ref\Microsoft.Extensions.Configuration.Abstractions.csproj" />

src/libraries/Microsoft.Extensions.Configuration.Xml/src/Microsoft.Extensions.Configuration.Xml.csproj
@@ -17,6 +17,10 @@
              Link="Common\System\ThrowHelper.cs" />
   </ItemGroup>
 
+  <ItemGroup Condition="!$([MSBuild]::IsTargetFrameworkCompatible('$(TargetFramework)', 'net7.0'))">
+    <Compile Include="$(CoreLibSharedDir)System\Diagnostics\CodeAnalysis\RequiresDynamicCodeAttribute.cs" />
+  </ItemGroup>
+  
   <ItemGroup Condition="'$(TargetFrameworkIdentifier)' == '.NETFramework'">
     <Reference Include="System.Security" />
   </ItemGroup>

src/libraries/Microsoft.Extensions.Configuration.Xml/src/XmlConfigurationExtensions.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using Microsoft.Extensions.Configuration.Xml;
 using Microsoft.Extensions.FileProviders;
@@ -20,6 +21,7 @@ namespace Microsoft.Extensions.Configuration
         /// <param name="path">Path relative to the base path stored in
         /// <see cref="IConfigurationBuilder.Properties"/> of <paramref name="builder"/>.</param>
         /// <returns>The <see cref="IConfigurationBuilder"/>.</returns>
+        [RequiresDynamicCode(XmlDocumentDecryptor.RequiresDynamicCodeMessage)]
         public static IConfigurationBuilder AddXmlFile(this IConfigurationBuilder builder, string path)
         {
             return AddXmlFile(builder, provider: null, path: path, optional: false, reloadOnChange: false);
@@ -33,6 +35,7 @@ namespace Microsoft.Extensions.Configuration
         /// <see cref="IConfigurationBuilder.Properties"/> of <paramref name="builder"/>.</param>
         /// <param name="optional">Whether the file is optional.</param>
         /// <returns>The <see cref="IConfigurationBuilder"/>.</returns>
+        [RequiresDynamicCode(XmlDocumentDecryptor.RequiresDynamicCodeMessage)]
         public static IConfigurationBuilder AddXmlFile(this IConfigurationBuilder builder, string path, bool optional)
         {
             return AddXmlFile(builder, provider: null, path: path, optional: optional, reloadOnChange: false);
@@ -47,6 +50,7 @@ namespace Microsoft.Extensions.Configuration
         /// <param name="optional">Whether the file is optional.</param>
         /// <param name="reloadOnChange">Whether the configuration should be reloaded if the file changes.</param>
         /// <returns>The <see cref="IConfigurationBuilder"/>.</returns>
+        [RequiresDynamicCode(XmlDocumentDecryptor.RequiresDynamicCodeMessage)]
         public static IConfigurationBuilder AddXmlFile(this IConfigurationBuilder builder, string path, bool optional, bool reloadOnChange)
         {
             return AddXmlFile(builder, provider: null, path: path, optional: optional, reloadOnChange: reloadOnChange);
@@ -62,6 +66,7 @@ namespace Microsoft.Extensions.Configuration
         /// <param name="optional">Whether the file is optional.</param>
         /// <param name="reloadOnChange">Whether the configuration should be reloaded if the file changes.</param>
         /// <returns>The <see cref="IConfigurationBuilder"/>.</returns>
+        [RequiresDynamicCode(XmlDocumentDecryptor.RequiresDynamicCodeMessage)]
         public static IConfigurationBuilder AddXmlFile(this IConfigurationBuilder builder, IFileProvider? provider, string path, bool optional, bool reloadOnChange)
         {
             ThrowHelper.ThrowIfNull(builder);
@@ -87,6 +92,7 @@ namespace Microsoft.Extensions.Configuration
         /// <param name="builder">The <see cref="IConfigurationBuilder"/> to add to.</param>
         /// <param name="configureSource">Configures the source.</param>
         /// <returns>The <see cref="IConfigurationBuilder"/>.</returns>
+        [RequiresDynamicCode(XmlDocumentDecryptor.RequiresDynamicCodeMessage)]
         public static IConfigurationBuilder AddXmlFile(this IConfigurationBuilder builder, Action<XmlConfigurationSource>? configureSource)
             => builder.Add(configureSource);
 
@@ -96,6 +102,7 @@ namespace Microsoft.Extensions.Configuration
         /// <param name="builder">The <see cref="IConfigurationBuilder"/> to add to.</param>
         /// <param name="stream">The <see cref="Stream"/> to read the XML configuration data from.</param>
         /// <returns>The <see cref="IConfigurationBuilder"/>.</returns>
+        [RequiresDynamicCode(XmlDocumentDecryptor.RequiresDynamicCodeMessage)]
         public static IConfigurationBuilder AddXmlStream(this IConfigurationBuilder builder, Stream stream)
         {
             ThrowHelper.ThrowIfNull(builder);

src/libraries/Microsoft.Extensions.Configuration.Xml/src/XmlConfigurationProvider.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 
 namespace Microsoft.Extensions.Configuration.Xml
@@ -8,6 +9,7 @@ namespace Microsoft.Extensions.Configuration.Xml
     /// <summary>
     /// Represents an XML file as an <see cref="IConfigurationSource"/>.
     /// </summary>
+    [RequiresDynamicCode(XmlDocumentDecryptor.RequiresDynamicCodeMessage)]
     public class XmlConfigurationProvider : FileConfigurationProvider
     {
         /// <summary>

commit 2d21cb1d0c5ff254d89069c68cf2ca06c7df3390

Fix LoadXsltTransforms and LoadAllTransforms tests in S.S.Cryptography.Xml on iOS/tvOS
They started failing after https://github.com/dotnet/runtime/pull/84426 because we now throw an exception when xslt is used.
That exception gets hidden in CryptoHelpers.CreateFromName() though since you only see "Unknown transform has been encountered" so it is hard to figure out what happened. Added a more specific error message that mentions the algorithm name.

src/libraries/System.Security.Cryptography.Xml/src/Resources/Strings.resx
@@ -231,6 +231,9 @@
   <data name="Cryptography_Xml_UnknownTransform" xml:space="preserve">
     <value>Unknown transform has been encountered.</value>
   </data>
+  <data name="Cryptography_Xml_UnknownTransform_AlgorithmName" xml:space="preserve">
+    <value>Unknown transform has been encountered, couldn't create algorithm with name "{0}". Note that Xslts are only supported when dynamic code is supported.</value>
+  </data>
   <data name="Cryptography_Xml_UriNotResolved" xml:space="preserve">
     <value>Unable to resolve Uri {0}.</value>
   </data>

src/libraries/System.Security.Cryptography.Xml/src/System/Security/Cryptography/Xml/Reference.cs
@@ -258,7 +258,7 @@ namespace System.Security.Cryptography.Xml
                         Transform? transform = CryptoHelpers.CreateFromName<Transform>(algorithm);
                         if (transform == null)
                         {
-                            throw new CryptographicException(SR.Cryptography_Xml_UnknownTransform);
+                            throw new CryptographicException(SR.Format(SR.Cryptography_Xml_UnknownTransform_AlgorithmName, algorithm));
                         }
                         AddTransform(transform);
                         // let the transform read the children of the transformElement for data

src/libraries/System.Security.Cryptography.Xml/src/System/Security/Cryptography/Xml/TransformChain.cs
@@ -190,7 +190,7 @@ namespace System.Security.Cryptography.Xml
                 string? algorithm = Utils.GetAttribute(transformElement, "Algorithm", SignedXml.XmlDsigNamespaceUrl);
                 Transform? transform = CryptoHelpers.CreateFromName<Transform>(algorithm);
                 if (transform == null)
-                    throw new CryptographicException(SR.Cryptography_Xml_UnknownTransform);
+                    throw new CryptographicException(SR.Format(SR.Cryptography_Xml_UnknownTransform_AlgorithmName, algorithm));
                 // let the transform read the children of the transformElement for data
                 transform.LoadInnerXml(transformElement.ChildNodes);
                 _transforms.Add(transform);

src/libraries/System.Security.Cryptography.Xml/tests/ReferenceTest.cs
@@ -10,9 +10,11 @@
 // (C) 2004 Novell (http://www.novell.com)
 
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.IO;
 using System.Text;
 using System.Xml;
+using Microsoft.DotNet.XUnitExtensions;
 using Xunit;
 
 namespace System.Security.Cryptography.Xml.Tests
@@ -126,9 +128,15 @@ namespace System.Security.Cryptography.Xml.Tests
             Assert.Equal(1, reference.TransformChain.Count);
         }
 
-        [Fact]
+        [ConditionalFact]
         public void LoadXsltTransforms()
         {
+#if NETCOREAPP
+            if (!RuntimeFeature.IsDynamicCodeSupported)
+            {
+                throw new SkipTestException("XSLTs are only supported when dynamic code is supported. See https://github.com/dotnet/runtime/issues/84389");
+            }
+#endif
             string test = "<Reference xmlns=\"http://www.w3.org/2000/09/xmldsig#\"><Transforms>";
             test += "<Transform Algorithm=\"http://www.w3.org/TR/1999/REC-xslt-19991116\">";
             test += "<xsl:stylesheet xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\" xmlns=\"http://www.w3.org/TR/xhtml1/strict\" exclude-result-prefixes=\"foo\" version=\"1.0\">";
@@ -151,13 +159,23 @@ namespace System.Security.Cryptography.Xml.Tests
         public void LoadAllTransforms()
         {
             string test = "<Reference xmlns=\"http://www.w3.org/2000/09/xmldsig#\"><Transforms><Transform Algorithm=\"http://www.w3.org/2000/09/xmldsig#base64\" /><Transform Algorithm=\"http://www.w3.org/TR/2001/REC-xml-c14n-20010315\" /><Transform Algorithm=\"http://www.w3.org/TR/2001/REC-xml-c14n-20010315#WithComments\" /><Transform Algorithm=\"http://www.w3.org/2000/09/xmldsig#enveloped-signature\" /><Transform Algorithm=\"http://www.w3.org/TR/1999/REC-xpath-19991116\"><XPath /></Transform>";
-            test += "<Transform Algorithm=\"http://www.w3.org/TR/1999/REC-xslt-19991116\">";
-            test += "<xsl:stylesheet xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\" xmlns=\"http://www.w3.org/TR/xhtml1/strict\" exclude-result-prefixes=\"foo\" version=\"1.0\">";
-            test += "<xsl:output encoding=\"UTF-8\" indent=\"no\" method=\"xml\" />";
-            test += "<xsl:template match=\"/\"><html><head><title>Notaries</title>";
-            test += "</head><body><table><xsl:for-each select=\"Notaries/Notary\">";
-            test += "<tr><th><xsl:value-of select=\"@name\" /></th></tr></xsl:for-each>";
-            test += "</table></body></html></xsl:template></xsl:stylesheet></Transform>";
+            string xsltTransform = "<Transform Algorithm=\"http://www.w3.org/TR/1999/REC-xslt-19991116\">";
+            xsltTransform += "<xsl:stylesheet xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\" xmlns=\"http://www.w3.org/TR/xhtml1/strict\" exclude-result-prefixes=\"foo\" version=\"1.0\">";
+            xsltTransform += "<xsl:output encoding=\"UTF-8\" indent=\"no\" method=\"xml\" />";
+            xsltTransform += "<xsl:template match=\"/\"><html><head><title>Notaries</title>";
+            xsltTransform += "</head><body><table><xsl:for-each select=\"Notaries/Notary\">";
+            xsltTransform += "<tr><th><xsl:value-of select=\"@name\" /></th></tr></xsl:for-each>";
+            xsltTransform += "</table></body></html></xsl:template></xsl:stylesheet></Transform>";
+            int expectedTransformsCount = 6;
+#if NETCOREAPP
+            if (!RuntimeFeature.IsDynamicCodeSupported)
+            {
+                // XSLTs are only supported when dynamic code is supported. See https://github.com/dotnet/runtime/issues/84389
+                xsltTransform = "";
+                expectedTransformsCount = 5;
+            }
+#endif
+            test += xsltTransform;
             test += "</Transforms><DigestMethod Algorithm=\"http://www.w3.org/2000/09/xmldsig#sha1\" /><DigestValue>AAAAAAAAAAAAAAAAAAAAAAAAAAA=</DigestValue></Reference>";
             Reference reference = new Reference();
             XmlDocument doc = new XmlDocument();
@@ -165,7 +183,7 @@ namespace System.Security.Cryptography.Xml.Tests
             reference.LoadXml(doc.DocumentElement);
             string result = reference.GetXml().OuterXml;
             Assert.Equal(test, result);
-            Assert.Equal(6, reference.TransformChain.Count);
+            Assert.Equal(expectedTransformsCount, reference.TransformChain.Count);
         }
 
         [Fact]