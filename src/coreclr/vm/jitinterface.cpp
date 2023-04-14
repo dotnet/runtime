f66c469be184f39420291c456c3422136c09ebdf
Dong-Heon Jung <clamp03@gmail.com>
2023-04-14 23:32:25 +0900

    [RISC-V] coreclr-vm and other directories in coreclr (#82381)
    * [RISC-V] coreclr-vm and other directories in coreclr
    
    - Successfully cross-build for RISC-V.
    - Run A simple application "helloworld"
    - Fail a test in clr.paltest
    
    * Remove commented codes
    
    * Fix mistakes
    
    * Update by reviews
    
    * [VM] Fix test
    
    * [VM] Update
    
    Updated by review in #82380
    
    * [VM] Update assert and todo comments
    
    * [VM] Updated
    
    * [VM] Fix a bug
    
    * [VM] Add getRISCV64PassStructInRegisterFlags
    
    * Revert "[VM] Add getRISCV64PassStructInRegisterFlags"
    
    This reverts commit cd1ea45de36458a5e3ec66a325bf3e0c4812680d.
    
    * [VM] Restore getLoongArch64PassStructInRegisterFlags
    
    In coreclr-jit patch, it makes getRISCV64PassStructInRegisterFlags for
    RISCV64. So restore getLoongArch64PassStructInRegisterFlags
    
    * [VM] FIX TEST ERRORS
    
    * [VM] Update Stubs
    
    * Fix exceptionhandling, stack smashing detected
    
    * [VM] Fix vm/riscv64
    
    ---------
    
    Co-authored-by: Timur Mustafin <t.mustafin@partner.samsung.com>

--- src/coreclr/vm/jitinterface.cpp
+++ src/coreclr/vm/jitinterface.cpp
@@ -11241,6 +11241,12 @@ void reservePersonalityRoutineSpace(uint32_t &unwindSize)
 
     // Add space for personality routine, it must be 4-byte aligned.
     unwindSize += sizeof(ULONG);
+#elif defined(TARGET_RISCV64)
+    // The JIT passes in a 4-byte aligned block of unwind data.
+    _ASSERTE(IS_ALIGNED(unwindSize, sizeof(ULONG)));
+
+    // Add space for personality routine, it must be 4-byte aligned.
+    unwindSize += sizeof(ULONG);
 #else
     PORTABILITY_ASSERT("reservePersonalityRoutineSpace");
 #endif // !defined(TARGET_AMD64)
@@ -11458,6 +11464,12 @@ void CEEJitInfo::allocUnwindInfo (
 
 #elif defined(TARGET_LOONGARCH64)
 
+    *(LONG *)pUnwindInfoRW |= (1 << 20); // X bit
+
+    ULONG * pPersonalityRoutineRW = (ULONG*)((BYTE *)pUnwindInfoRW + ALIGN_UP(unwindSize, sizeof(ULONG)));
+    *pPersonalityRoutineRW = ExecutionManager::GetCLRPersonalityRoutineValue();
+
+#elif defined(TARGET_RISCV64)
     *(LONG *)pUnwindInfoRW |= (1 << 20); // X bit
 
     ULONG * pPersonalityRoutineRW = (ULONG*)((BYTE *)pUnwindInfoRW + ALIGN_UP(unwindSize, sizeof(ULONG)));
--- src/coreclr/vm/riscv64/excepcpu.h
+++ src/coreclr/vm/riscv64/excepcpu.h
@@ -1,7 +1,50 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
+//
+//
+
 
 #ifndef __excepcpu_h__
 #define __excepcpu_h__
 
-#error "TODO-RISCV64: missing implementation"
+
+#define THROW_CONTROL_FOR_THREAD_FUNCTION  RedirectForThreadAbort
+EXTERN_C void RedirectForThreadAbort();
+
+
+#define STATUS_CLR_GCCOVER_CODE         STATUS_ILLEGAL_INSTRUCTION
+
+class Thread;
+class FaultingExceptionFrame;
+
+#define INSTALL_EXCEPTION_HANDLING_RECORD(record)
+#define UNINSTALL_EXCEPTION_HANDLING_RECORD(record)
+//
+// On ARM, the COMPlusFrameHandler's work is done by our personality routine.
+//
+#define DECLARE_CPFH_EH_RECORD(pCurThread)
+
+//
+// Retrieves the redirected CONTEXT* from the stack frame of one of the
+// RedirectedHandledJITCaseForXXX_Stub's.
+//
+PTR_CONTEXT GetCONTEXTFromRedirectedStubStackFrame(T_DISPATCHER_CONTEXT * pDispatcherContext);
+PTR_CONTEXT GetCONTEXTFromRedirectedStubStackFrame(T_CONTEXT * pContext);
+
+//
+// Retrieves the FaultingExceptionFrame* from the stack frame of
+// RedirectForThrowControl.
+//
+FaultingExceptionFrame *GetFrameFromRedirectedStubStackFrame (T_DISPATCHER_CONTEXT *pDispatcherContext);
+
+inline
+PCODE GetAdjustedCallAddress(PCODE returnAddress)
+{
+    LIMITED_METHOD_CONTRACT;
+
+    return returnAddress - 4;
+}
+
+BOOL AdjustContextForVirtualStub(EXCEPTION_RECORD *pExceptionRecord, T_CONTEXT *pContext);
+
+#endif // __excepcpu_h__
