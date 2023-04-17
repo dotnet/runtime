add51b82baf268c3678bdd124480a46b89d72ce4
Lee Culver <leculver@microsoft.com>
2023-04-17 14:09:56 -0700

    Memory Diagnostics: GC Bookkeeping (#84454)
    * Add ISOSMemoryEnum and HandleTable enum
    
    * Update comment
    
    * Add GC Bookkeeping data to the dac
    
    - bookkeeping_covered_start is now compiled into all versions of the GC, instead of just with USE_REGIONS.  This allows us to find the base address of the allocated memory for GC Bookkeeping.
    - Added dac enumeration of GC Bookkeeping.
    
    * Add assert
    
    * Add support for free gc regions
    
    * Don't require card_table_element_layout
    
    * Fix issue with naming in request
    
    * Remote DEFINE_MISSING_FIELD from dac
    
    We should never have missing fields in the dac.  This is a leftover from
    previous code.
    
    * Whitespace fix
    
    * Break if we loop
    
    * Fix static_assert
    
    * Segment fixes
    
    When using segments, update bookkeeping_covered_start and card_table_info.size when we update the card table.
    
    * Defensive coding
    
    Check how many times we loop through memory to guard against heap corruption.
    
    * free_region fixes
    
    * Remove hardcoding of region list
    
    * Fix gcc warnings
    
    * Remove unneeded #define
    
    * Add freeable_soh_segment/freeable_uoh_segment
    
    * Add more card table checks
    
    * Fix field definition issue
    
    * Fix pointer issue
    
    * Fix compile issue
    
    * Rename bookkeeping_covered_start to bookkeeping_start
    
    * Update src/coreclr/debug/daccess/daccess.cpp
    
    Co-authored-by: Andrew Au <cshung@gmail.com>
    
    * Code review feedback
    
    * Remove unused parameter
    
    * Add heap number
    
    ---------
    
    Co-authored-by: Andrew Au <andrewau@microsoft.com>
    Co-authored-by: Andrew Au <cshung@gmail.com>

--- src/coreclr/gc/handletablepriv.h
+++ src/coreclr/gc/handletablepriv.h
@@ -14,113 +14,7 @@
 
 #include "handletable.h"
 
-
- // Build support for async pinned handles into standalone GC to make it usable with older runtimes
-#if defined(BUILD_AS_STANDALONE) && !defined(FEATURE_NATIVEAOT)
-#define FEATURE_ASYNC_PINNED_HANDLES
-#endif
-
-
-/*--------------------------------------------------------------------------*/
-
-//<TODO>@TODO: find a home for this in a project-level header file</TODO>
-#define BITS_PER_BYTE               (8)
-/*--------------------------------------------------------------------------*/
-
-
-
-/****************************************************************************
- *
- * MAJOR TABLE DEFINITIONS THAT CHANGE DEPENDING ON THE WEATHER
- *
- ****************************************************************************/
-
-// 64k reserved per segment with 4k as header.
-#define HANDLE_SEGMENT_SIZE     (0x10000)   // MUST be a power of 2 (and currently must be 64K due to VirtualAlloc semantics)
-#define HANDLE_HEADER_SIZE      (0x1000)    // SHOULD be <= OS page size
-
-#define HANDLE_SEGMENT_ALIGNMENT     HANDLE_SEGMENT_SIZE
-
-
-#if !BIGENDIAN
-
-    // little-endian write barrier mask manipulation
-    #define GEN_CLUMP_0_MASK        (0x000000FF)
-    #define NEXT_CLUMP_IN_MASK(dw)  ((dw) >> BITS_PER_BYTE)
-
-#else
-
-    // big-endian write barrier mask manipulation
-    #define GEN_CLUMP_0_MASK        (0xFF000000)
-    #define NEXT_CLUMP_IN_MASK(dw)  ((dw) << BITS_PER_BYTE)
-
-#endif
-
-
-// if the above numbers change than these will likely change as well
-#define HANDLE_HANDLES_PER_CLUMP    (16)        // segment write-barrier granularity
-#define HANDLE_HANDLES_PER_BLOCK    (64)        // segment suballocation granularity
-#define HANDLE_OPTIMIZE_FOR_64_HANDLE_BLOCKS    // flag for certain optimizations
-
-// number of types allowed for public callers
-#define HANDLE_MAX_PUBLIC_TYPES     (HANDLE_MAX_INTERNAL_TYPES - 1) // reserve one internal type
-
-// internal block types
-#define HNDTYPE_INTERNAL_DATABLOCK  (HANDLE_MAX_INTERNAL_TYPES - 1) // reserve last type for data blocks
-
-// max number of generations to support statistics on
-#define MAXSTATGEN                  (5)
-
-/*--------------------------------------------------------------------------*/
-
-
-
-/****************************************************************************
- *
- * MORE DEFINITIONS
- *
- ****************************************************************************/
-
-// fast handle-to-segment mapping
-#define HANDLE_SEGMENT_CONTENT_MASK     (HANDLE_SEGMENT_SIZE - 1)
-#define HANDLE_SEGMENT_ALIGN_MASK       (~HANDLE_SEGMENT_CONTENT_MASK)
-
-// table layout metrics
-#define HANDLE_SIZE                     sizeof(_UNCHECKED_OBJECTREF)
-#define HANDLE_HANDLES_PER_SEGMENT      ((HANDLE_SEGMENT_SIZE - HANDLE_HEADER_SIZE) / HANDLE_SIZE)
-#define HANDLE_BLOCKS_PER_SEGMENT       (HANDLE_HANDLES_PER_SEGMENT / HANDLE_HANDLES_PER_BLOCK)
-#define HANDLE_CLUMPS_PER_SEGMENT       (HANDLE_HANDLES_PER_SEGMENT / HANDLE_HANDLES_PER_CLUMP)
-#define HANDLE_CLUMPS_PER_BLOCK         (HANDLE_HANDLES_PER_BLOCK / HANDLE_HANDLES_PER_CLUMP)
-#define HANDLE_BYTES_PER_BLOCK          (HANDLE_HANDLES_PER_BLOCK * HANDLE_SIZE)
-#define HANDLE_HANDLES_PER_MASK         (sizeof(uint32_t) * BITS_PER_BYTE)
-#define HANDLE_MASKS_PER_SEGMENT        (HANDLE_HANDLES_PER_SEGMENT / HANDLE_HANDLES_PER_MASK)
-#define HANDLE_MASKS_PER_BLOCK          (HANDLE_HANDLES_PER_BLOCK / HANDLE_HANDLES_PER_MASK)
-#define HANDLE_CLUMPS_PER_MASK          (HANDLE_HANDLES_PER_MASK / HANDLE_HANDLES_PER_CLUMP)
-
-// We use this relation to check for free mask per block.
-C_ASSERT (HANDLE_HANDLES_PER_MASK * 2 == HANDLE_HANDLES_PER_BLOCK);
-
-
-// cache layout metrics
-#define HANDLE_CACHE_TYPE_SIZE          128 // 128 == 63 handles per bank
-#define HANDLES_PER_CACHE_BANK          ((HANDLE_CACHE_TYPE_SIZE / 2) - 1)
-
-// cache policy defines
-#define REBALANCE_TOLERANCE             (HANDLES_PER_CACHE_BANK / 3)
-#define REBALANCE_LOWATER_MARK          (HANDLES_PER_CACHE_BANK - REBALANCE_TOLERANCE)
-#define REBALANCE_HIWATER_MARK          (HANDLES_PER_CACHE_BANK + REBALANCE_TOLERANCE)
-
-// bulk alloc policy defines
-#define SMALL_ALLOC_COUNT               (HANDLES_PER_CACHE_BANK / 10)
-
-// misc constants
-#define MASK_FULL                       (0)
-#define MASK_EMPTY                      (0xFFFFFFFF)
-#define MASK_LOBYTE                     (0x000000FF)
-#define TYPE_INVALID                    ((uint8_t)0xFF)
-#define BLOCK_INVALID                   ((uint8_t)0xFF)
-
-/*--------------------------------------------------------------------------*/
+#include "handletableconstants.h"
 
 
 
@@ -484,6 +378,11 @@ struct HandleTable
     uint32_t rgTypeFlags[HANDLE_MAX_INTERNAL_TYPES];
 
     /*
+     * head of segment list for this table
+     */
+    PTR_TableSegment pSegmentList;
+
+    /*
      * lock for this table
      */
     CrstStatic Lock;
@@ -498,11 +397,6 @@ struct HandleTable
      * (this includes the handles residing in rgMainCache and rgQuickCache)
      */
     uint32_t dwCount;
-
-    /*
-     * head of segment list for this table
-     */
-    PTR_TableSegment pSegmentList;
 
     /*
      * information on current async scan (if any)
