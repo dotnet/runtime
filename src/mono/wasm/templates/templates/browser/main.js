89a0acf8d492db9dba11ac8febe61ba798494f03
Marek Fišera <mara@neptuo.com>
2023-04-18 11:11:14 +0200

    Revert "Update browser template"
    This reverts commit 3279ffac5165f61ada00c01ae14769e3b69aa7a0.

--- src/mono/wasm/templates/templates/browser/main.js
+++ src/mono/wasm/templates/templates/browser/main.js
@@ -1,10 +1,9 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-import { dotnet } from './_framework/dotnet.js'
+import { dotnet } from './dotnet.js'
 
 const { setModuleImports, getAssemblyExports, getConfig } = await dotnet
-    .withStartupOptions({})
     .withDiagnosticTracing(false)
     .withApplicationArgumentsFromQuery()
     .create();
