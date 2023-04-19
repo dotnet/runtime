b5b300449751b14126e0d776cca2fdae24e1196e
Jackson Schuster <jschuster@microsoft.com>
2023-04-19 09:49:37 -0700

    wip

---
+++ src/libraries/System.Runtime.InteropServices/gen/Microsoft.Interop.SourceGeneration/HashCode.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Interop
+{
+    public static class HashCode
+    {
+        public static int Combine(params object[] values)
+        {
+            int hash = 31;
+            foreach (object value in values)
+            {
+                hash = hash * 29 + value.GetHashCode();
+            }
+            return hash;
+        }
+    }
+}
---
+++ src/libraries/System.Runtime.InteropServices/gen/Microsoft.Interop.SourceGeneration/ValueEqualityImmutableDictionary.cs
@@ -0,0 +1,58 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+
+namespace Microsoft.Interop
+{
+    internal record struct ValueEqualityImmutableDictionary<T, U>(ImmutableDictionary<T, U> Map) : IDictionary<T, U>
+    {
+        public bool Equals(ValueEqualityImmutableDictionary<T, U> other)
+        {
+            if (Count != other.Count)
+            {
+                return false;
+            }
+
+            foreach(var kvp in this)
+            {
+                if (!other.TryGetValue(kvp.Key, out var value) || !kvp.Value.Equals(value))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Map.Values);
+        }
+
+        public U this[T key] { get => ((IDictionary<T, U>)Map)[key]; set => ((IDictionary<T, U>)Map)[key] = value; }
+
+        public ICollection<T> Keys => ((IDictionary<T, U>)Map).Keys;
+
+        public ICollection<U> Values => ((IDictionary<T, U>)Map).Values;
+
+        public int Count => Map.Count;
+
+        public bool IsReadOnly => ((ICollection<KeyValuePair<T, U>>)Map).IsReadOnly;
+
+        public void Add(T key, U value) => ((IDictionary<T, U>)Map).Add(key, value);
+        public void Add(KeyValuePair<T, U> item) => ((ICollection<KeyValuePair<T, U>>)Map).Add(item);
+        public void Clear() => ((ICollection<KeyValuePair<T, U>>)Map).Clear();
+        public bool Contains(KeyValuePair<T, U> item) => Map.Contains(item);
+        public bool ContainsKey(T key) => Map.ContainsKey(key);
+        public void CopyTo(KeyValuePair<T, U>[] array, int arrayIndex) => ((ICollection<KeyValuePair<T, U>>)Map).CopyTo(array, arrayIndex);
+        public IEnumerator<KeyValuePair<T, U>> GetEnumerator() => ((IEnumerable<KeyValuePair<T, U>>)Map).GetEnumerator();
+        public bool Remove(T key) => ((IDictionary<T, U>)Map).Remove(key);
+        public bool Remove(KeyValuePair<T, U> item) => ((ICollection<KeyValuePair<T, U>>)Map).Remove(item);
+        public bool TryGetValue(T key, out U value) => Map.TryGetValue(key, out value);
+        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)Map).GetEnumerator();
+    }
+}
