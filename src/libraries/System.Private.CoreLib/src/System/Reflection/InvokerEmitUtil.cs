e318706f77f589a00d37d917f938d8c1570beee3
Steve Harter <steveharter@users.noreply.github.com>
2023-04-21 12:43:00 -0500

    Change interpreter detection

--- src/libraries/System.Private.CoreLib/src/System/Reflection/InvokerEmitUtil.cs
+++ src/libraries/System.Private.CoreLib/src/System/Reflection/InvokerEmitUtil.cs
@@ -4,6 +4,7 @@
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
 
 namespace System.Reflection
 {
@@ -64,17 +65,19 @@ namespace System.Reflection
                 }
             }
 
-            // Invoke the method.
             // For CallStack reasons, don't inline target method.
-#if TARGET_WASM
-            // Mono interpreter does not support\need this intrinsic.
-#elif MONO
-             il.Emit(OpCodes.Call, Methods.DisableInline());
+            // Mono interpreter does not support\need this.
+            if (RuntimeFeature.IsDynamicCodeCompiled)
+            {
+#if MONO
+                il.Emit(OpCodes.Call, Methods.DisableInline());
 #else
-             il.Emit(OpCodes.Call, Methods.NextCallReturnAddress());
-             il.Emit(OpCodes.Pop);
+                il.Emit(OpCodes.Call, Methods.NextCallReturnAddress());
+                il.Emit(OpCodes.Pop);
 #endif
+            }
 
+            // Invoke the method.
             if (emitNew)
             {
                 il.Emit(OpCodes.Newobj, (ConstructorInfo)method);
