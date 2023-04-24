d6a0beab0856865bf3a545def64b94a17b25b89e
EgorBo <egorbo@gmail.com>
2023-04-24 18:46:08 +0200

    Add LeftShift

--- src/coreclr/jit/hwintrinsicarm64.cpp
+++ src/coreclr/jit/hwintrinsicarm64.cpp
@@ -1519,6 +1519,8 @@ GenTree* Compiler::impSpecialIntrinsic(NamedIntrinsic        intrinsic,
 
         case NI_Vector64_ShiftLeft:
         case NI_Vector128_ShiftLeft:
+        case NI_Vector64_op_LeftShift:
+        case NI_Vector128_op_LeftShift:
         {
             assert(sig->numArgs == 2);
 
