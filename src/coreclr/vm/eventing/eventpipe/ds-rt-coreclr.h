ea8fc677d7537d257903248d56d472d8de7465c1
Nickolas McDonald <43690021+n77y@users.noreply.github.com>
2023-04-27 18:28:14 -0400

    Update ds-rt-coreclr.h
    Added “DOTNET_” to line 316

--- src/coreclr/vm/eventing/eventpipe/ds-rt-coreclr.h
+++ src/coreclr/vm/eventing/eventpipe/ds-rt-coreclr.h
@@ -313,7 +313,7 @@ ds_rt_server_log_pause_message (void)
 	uint32_t port_suspended = ds_rt_config_value_get_default_port_suspend();
 
 	printf("The runtime has been configured to pause during startup and is awaiting a Diagnostics IPC ResumeStartup command from a Diagnostic Port.\n");
-	printf("%s=\"%s\"\n", diagPortsName, ports == nullptr ? "" : ports);
+	printf("DOTNET_%s=\"%s\"\n", diagPortsName, ports == nullptr ? "" : ports);
 	printf("DOTNET_DefaultDiagnosticPortSuspend=%u\n", port_suspended);
 	fflush(stdout);
 }
