b9fa699e62989f7d15d84789a7ddeb0b1f0c7a0c
Michał Petryka <35800402+MichalPetryka@users.noreply.github.com>
2023-05-01 23:17:11 +0200

    Update IndirectNative.cpp

--- src/tests/JIT/opt/Devirtualization/IndirectNative.cpp
+++ src/tests/JIT/opt/Devirtualization/IndirectNative.cpp
@@ -1,7 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#include <stint.h>
+#include <stdint.h>
 #include <xplatform.h>
 #include <platformdefines.h>
 
