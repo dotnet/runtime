ce759c9f663f551ec87272aa8eb85a6e1acaf6c0
Michał Petryka <35800402+MichalPetryka@users.noreply.github.com>
2023-05-01 23:54:21 +0200

    Update IndirectNative.cpp

--- src/tests/JIT/opt/Devirtualization/IndirectNative.cpp
+++ src/tests/JIT/opt/Devirtualization/IndirectNative.cpp
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 #include <stdint.h>
-#include <xplatform.h>
 #include <platformdefines.h>
 
 extern "C" DLL_EXPORT int32_t STDMETHODCALLTYPE E()
