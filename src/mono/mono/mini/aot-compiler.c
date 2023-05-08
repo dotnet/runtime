d7130c8a9b6a316e61fc010bb0e16e077a00211c
Milos Kotlar <kotlarmilos@gmail.com>
2023-05-08 12:29:13 +0200

    Deduplicate runtime invoke wrappers on iOS

--- src/mono/mono/mini/aot-compiler.c
+++ src/mono/mono/mini/aot-compiler.c
@@ -4331,6 +4331,12 @@ collect_dedup_method (MonoAotCompile *acfg, MonoMethod *method)
 static int
 add_method_full (MonoAotCompile *acfg, MonoMethod *method, gboolean extra, int depth)
 {
+	if (collect_dedup_method (acfg, method))
+		return -1;
+
+	if (extra && acfg->aot_opts.log_generics)
+		aot_printf (acfg, "%*sAdding method %s.\n", depth, "", mono_method_get_full_name (method));
+
 	int index;
 
 	index = GPOINTER_TO_UINT (g_hash_table_lookup (acfg->method_indexes, method));
@@ -4416,12 +4422,6 @@ add_extra_method_full (MonoAotCompile *acfg, MonoMethod *method, gboolean prefer
 		method = mini_get_shared_method_full (method, SHARE_MODE_GSHAREDVT, error);
 		mono_error_assert_ok (error);
 	}
-
-	if (collect_dedup_method (acfg, method))
-		return;
-
-	if (acfg->aot_opts.log_generics)
-		aot_printf (acfg, "%*sAdding method %s.\n", depth, "", mono_method_get_full_name (method));
 
 	add_method_full (acfg, method, TRUE, depth);
 }
