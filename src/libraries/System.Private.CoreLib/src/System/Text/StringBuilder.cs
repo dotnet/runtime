7de305c6896de9856e01e3b1fded8d3e81076fc4
EgorBo <egorbo@gmail.com>
2023-05-08 17:15:54 +0200

    JIT: Boost inlining if we can unroll memmove/memcmp

--- src/libraries/System.Private.CoreLib/src/System/Text/StringBuilder.cs
+++ src/libraries/System.Private.CoreLib/src/System/Text/StringBuilder.cs
@@ -728,7 +728,7 @@ namespace System.Text
         /// <param name="value">The string to append.</param>
         public StringBuilder Append(string? value)
         {
-            if (value is not null)
+            if (value != null)
             {
                 Append(ref value.GetRawStringData(), value.Length);
             }
