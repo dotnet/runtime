76d578253027c2fdb94b5e20c943e5c1aaed92df
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2023-05-08 13:44:24 +0200

    Undo some changes

--- src/coreclr/jit/promotion.h
+++ src/coreclr/jit/promotion.h
@@ -54,7 +54,6 @@ class Promotion
     void InsertInitStatement(Statement** prevStmt, GenTree* tree);
     static GenTree* CreateWriteBack(Compiler* compiler, unsigned structLclNum, const Replacement& replacement);
     static GenTree* CreateReadBack(Compiler* compiler, unsigned structLclNum, const Replacement& replacement);
-    static void IncrementRefCount(Compiler* compiler, unsigned lclNum);
 
     //------------------------------------------------------------------------
     // BinarySearch:
