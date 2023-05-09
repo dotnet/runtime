f8f5bb878e2d659287b3a3277a94f29bd03f6cab
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2023-05-09 22:21:10 +0800

    Cleanup unused parsing code

--- src/libraries/System.Runtime.Numerics/src/System/Globalization/FormatProvider.BigInteger.cs
+++ src/libraries/System.Runtime.Numerics/src/System/Globalization/FormatProvider.BigInteger.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Security;
 using System.Text;
 
 namespace System.Globalization
@@ -30,44 +29,6 @@ namespace System.Globalization
                         Number.NumberToStringFormat(ref sb, ref numberBuffer, format, numberFormatInfo);
                     }
                 }
-            }
-        }
-
-        internal static bool TryStringToBigInteger(
-            ReadOnlySpan<char> s,
-            NumberStyles styles,
-            NumberFormatInfo numberFormatInfo,
-            StringBuilder receiver,  // Receives the decimal digits
-            out int precision,
-            out int scale,
-            out bool sign
-            )
-        {
-            FormatProvider.Number.NumberBuffer numberBuffer = default;
-
-            unsafe
-            {
-                // Note: because we passed a non-null StringBuilder (receiver) to TryStringToNumber, it streams the digits into
-                // that instead of numberBuffer.digits. This is quite important since numberBuffer.digits is a fixed-buffer size
-                // and BigNumbers can have an arbitrary number of digits.
-                //
-                // Just in case a bug is ever introduced into TryStringToNumber that violates this, set the pointer that numberBuffer.digits returns
-                // to something that will AV.
-                numberBuffer.overrideDigits = (char*)0x1;
-            }
-            if (!Number.TryStringToNumber(s, styles, ref numberBuffer, receiver, numberFormatInfo, parseDecimal: false))
-            {
-                precision = default(int);
-                scale = default(int);
-                sign = default(bool);
-                return false;
-            }
-            else
-            {
-                precision = numberBuffer.precision;
-                scale = numberBuffer.scale;
-                sign = numberBuffer.sign;
-                return true;
             }
         }
     }
