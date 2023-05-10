f50408b6b9f6bef3b64b2f54922ca377f44bb1ab
Khushal Modi <khushal.chandresh.modi@intel.com>
2023-05-10 03:20:29 -0700

    Making changes to the library test case expected output based on the architecture. This is because we have changed the JITDbl2Ulng helper function to mimic the new IEEE compliant AVX512 instruction vcvtsd2usi. In the process, we needed to update the library test case because the default Floating Point Error (FPE) value for the new instruction is different from the default MSVC FPE value i.e. 0.

--- src/libraries/System.Runtime/tests/System/UIntPtrTests.GenericMath.cs
+++ src/libraries/System.Runtime/tests/System/UIntPtrTests.GenericMath.cs
@@ -12,6 +12,7 @@ namespace System.Tests
         //
         // IAdditionOperators
         //
+        public static Architecture arch = RuntimeInformation.ProcessArchitecture;
 
         [Fact]
         public static void op_AdditionTest()
@@ -2223,7 +2224,7 @@ namespace System.Tests
             Assert.Equal(nuint.MaxValue, NumberBaseHelper<nuint>.CreateSaturating<double>(double.MaxValue));
             Assert.Equal(nuint.MinValue, NumberBaseHelper<nuint>.CreateSaturating<double>(double.MinValue));
 
-            Assert.Equal(nuint.MinValue, NumberBaseHelper<nuint>.CreateSaturating<double>(double.NaN));
+            Assert.Equal((arch == Architecture.X86 || arch == Architecture.X64)?nuint.MaxValue:nuint.MinValue, NumberBaseHelper<nuint>.CreateSaturating<double>(double.NaN));
         }
 
         [Fact]
@@ -2244,7 +2245,7 @@ namespace System.Tests
             Assert.Equal(nuint.MinValue, NumberBaseHelper<nuint>.CreateSaturating<Half>(Half.NegativeInfinity));
 
             Assert.Equal(nuint.MinValue, NumberBaseHelper<nuint>.CreateSaturating<Half>(Half.MinValue));
-            Assert.Equal(nuint.MinValue, NumberBaseHelper<nuint>.CreateSaturating<Half>(Half.NaN));
+            Assert.Equal((arch == Architecture.X86 || arch == Architecture.X64)?nuint.MaxValue:nuint.MinValue, NumberBaseHelper<nuint>.CreateSaturating<Half>(Half.NaN));
         }
 
         [Fact]
@@ -2351,7 +2352,7 @@ namespace System.Tests
             Assert.Equal(nuint.MaxValue, NumberBaseHelper<nuint>.CreateSaturating<NFloat>(NFloat.MaxValue));
             Assert.Equal(nuint.MinValue, NumberBaseHelper<nuint>.CreateSaturating<NFloat>(NFloat.MinValue));
 
-            Assert.Equal(nuint.MinValue, NumberBaseHelper<nuint>.CreateSaturating<NFloat>(NFloat.NaN));
+            Assert.Equal((arch == Architecture.X86 || arch == Architecture.X64)?nuint.MaxValue:nuint.MinValue, NumberBaseHelper<nuint>.CreateSaturating<NFloat>(NFloat.NaN));
         }
 
         [Fact]
@@ -2396,7 +2397,7 @@ namespace System.Tests
             Assert.Equal(nuint.MaxValue, NumberBaseHelper<nuint>.CreateSaturating<float>(float.MaxValue));
             Assert.Equal(nuint.MinValue, NumberBaseHelper<nuint>.CreateSaturating<float>(float.MinValue));
 
-            Assert.Equal(nuint.MinValue, NumberBaseHelper<nuint>.CreateSaturating<float>(float.NaN));
+            Assert.Equal((arch == Architecture.X86 || arch == Architecture.X64)?nuint.MaxValue:nuint.MinValue, NumberBaseHelper<nuint>.CreateSaturating<float>(float.NaN));
         }
 
         [Fact]
@@ -2535,7 +2536,7 @@ namespace System.Tests
             Assert.Equal(nuint.MaxValue, NumberBaseHelper<nuint>.CreateTruncating<double>(double.MaxValue));
             Assert.Equal(nuint.MinValue, NumberBaseHelper<nuint>.CreateTruncating<double>(double.MinValue));
 
-            Assert.Equal(nuint.MinValue, NumberBaseHelper<nuint>.CreateTruncating<double>(double.NaN));
+            Assert.Equal((arch == Architecture.X86 || arch == Architecture.X64)?nuint.MaxValue:nuint.MinValue, NumberBaseHelper<nuint>.CreateTruncating<double>(double.NaN));
         }
 
         [Fact]
@@ -2556,7 +2557,7 @@ namespace System.Tests
             Assert.Equal(nuint.MinValue, NumberBaseHelper<nuint>.CreateTruncating<Half>(Half.NegativeInfinity));
 
             Assert.Equal(nuint.MinValue, NumberBaseHelper<nuint>.CreateTruncating<Half>(Half.MinValue));
-            Assert.Equal(nuint.MinValue, NumberBaseHelper<nuint>.CreateTruncating<Half>(Half.NaN));
+            Assert.Equal((arch == Architecture.X86 || arch == Architecture.X64)?nuint.MaxValue:nuint.MinValue, NumberBaseHelper<nuint>.CreateTruncating<Half>(Half.NaN));
         }
 
         [Fact]
@@ -2685,7 +2686,7 @@ namespace System.Tests
             Assert.Equal(nuint.MaxValue, NumberBaseHelper<nuint>.CreateTruncating<NFloat>(NFloat.MaxValue));
             Assert.Equal(nuint.MinValue, NumberBaseHelper<nuint>.CreateTruncating<NFloat>(NFloat.MinValue));
 
-            Assert.Equal(nuint.MinValue, NumberBaseHelper<nuint>.CreateTruncating<NFloat>(NFloat.NaN));
+            Assert.Equal((arch == Architecture.X86 || arch == Architecture.X64)?nuint.MaxValue:nuint.MinValue, NumberBaseHelper<nuint>.CreateTruncating<NFloat>(NFloat.NaN));
         }
 
         [Fact]
@@ -2741,7 +2742,7 @@ namespace System.Tests
             Assert.Equal(nuint.MaxValue, NumberBaseHelper<nuint>.CreateTruncating<float>(float.MaxValue));
             Assert.Equal(nuint.MinValue, NumberBaseHelper<nuint>.CreateTruncating<float>(float.MinValue));
 
-            Assert.Equal(nuint.MinValue, NumberBaseHelper<nuint>.CreateTruncating<float>(float.NaN));
+            Assert.Equal((arch == Architecture.X86 || arch == Architecture.X64)?nuint.MaxValue:nuint.MinValue, NumberBaseHelper<nuint>.CreateTruncating<float>(float.NaN));
         }
 
         [Fact]
