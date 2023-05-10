a55d46e3eee79d7f6261cb4c4efa8a355d5cdd89
Tanner Gooding <tagoo@outlook.com>
2023-05-10 12:27:20 -0700

    Add explicit tests covering the other two DecomposeRotate paths

--- src/tests/JIT/Regression/JitBlue/Runtime_86027/Runtime_86207.cs
+++ src/tests/JIT/Regression/JitBlue/Runtime_86027/Runtime_86207.cs
@@ -8,7 +8,7 @@ using Xunit;
 public class Runtime_86027
 {
     [Fact]
-    public static int TestRotateLeft()
+    public static int TestRotateLeft1()
     {
         long[] firstOp = new long[] { 7822136809956075968, 1 };
 
@@ -21,11 +21,63 @@ public class Runtime_86027
     }
 
     [Fact]
-    public static int TestRotateRight()
+    public static int TestRotateLeft32()
+    {
+        long[] firstOp = new long[] { 7822136809956075968, 1 };
+
+        if (long.RotateLeft(firstOp[0], 32) != 3886722753596608508)
+        {
+            return 0;
+        }
+
+        return 100;
+    }
+
+    [Fact]
+    public static int TestRotateLeft33()
+    {
+        long[] firstOp = new long[] { 7822136809956075968, 1 };
+
+        if (long.RotateLeft(firstOp[0], 33) != 7773445507193217016)
+        {
+            return 0;
+        }
+
+        return 100;
+    }
+
+    [Fact]
+    public static int TestRotateRight1()
     {
         long[] firstOp = new long[] { 7822136809956075968, 1 };
 
         if (long.RotateRight(firstOp[0], 1) != 3911068404978037984)
+        {
+            return 0;
+        }
+
+        return 100;
+    }
+
+    [Fact]
+    public static int TestRotateRight32()
+    {
+        long[] firstOp = new long[] { 7822136809956075968, 1 };
+
+        if (long.RotateRight(firstOp[0], 32) != 3886722753596608508)
+        {
+            return 0;
+        }
+
+        return 100;
+    }
+
+    [Fact]
+    public static int TestRotateRight33()
+    {
+        long[] firstOp = new long[] { 7822136809956075968, 1 };
+
+        if (long.RotateRight(firstOp[0], 33) != 1943361376798304254)
         {
             return 0;
         }
