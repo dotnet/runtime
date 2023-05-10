2c8544f26453e4f3417c2db81b5f911dc54df52f
Jan Kotas <jkotas@microsoft.com>
2023-05-10 08:28:18 -0700

    Apply suggestions from code review

--- src/coreclr/nativeaot/System.Private.CoreLib/src/Internal/Reflection/Core/Execution/MethodInvoker.cs
+++ src/coreclr/nativeaot/System.Private.CoreLib/src/Internal/Reflection/Core/Execution/MethodInvoker.cs
@@ -40,10 +40,8 @@ namespace Internal.Reflection.Core.Execution
             if (thisObject == null)
                 throw new TargetException(SR.RFLCT_Targ_StatMethReqTarg);
 
-            if (RuntimeAugments.IsAssignable(thisObject, declaringTypeHandle))
-                return;
-
-            throw new TargetException(SR.RFLCT_Targ_ITargMismatch);
+            if (!RuntimeAugments.IsAssignable(thisObject, declaringTypeHandle))
+                throw new TargetException(SR.RFLCT_Targ_ITargMismatch);
         }
     }
 }
