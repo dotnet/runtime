9a5109f646ff4564eecaebb579f3b4365ee83eb4
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2023-05-11 13:36:20 +0900

    Handle cctor dependency on RVA static access
    Contributes to #68278.

--- src/coreclr/tools/aot/ILCompiler.Compiler/IL/ILImporter.Scanner.cs
+++ src/coreclr/tools/aot/ILCompiler.Compiler/IL/ILImporter.Scanner.cs
@@ -1045,7 +1045,9 @@ namespace Internal.IL
                     // magic fields the compiler synthetized, the data blob might bring more dependencies
                     // and we need to scan those.
                     _dependencies.Add(_compilation.GetFieldRvaData(field), reason);
-                    // TODO: lazy cctor dependency
+                    Debug.Assert(!field.OwningType.HasInstantiation);
+                    if (_compilation.HasLazyStaticConstructor(field.OwningType))
+                        _dependencies.Add(_factory.TypeNonGCStaticsSymbol((MetadataType)field.OwningType), "Cctor context");
                     return;
                 }
 
