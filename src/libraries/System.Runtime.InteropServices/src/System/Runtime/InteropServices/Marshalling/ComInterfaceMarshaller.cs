e18c8e9df8fd2c467f81c66dc5d253b2a2f09891
Jeremy Koritzinsky <jekoritz@microsoft.com>
2023-05-11 14:24:47 -0700

    Implement new marshaller types

---
+++ src/libraries/System.Runtime.InteropServices/src/System/Runtime/InteropServices/Marshalling/ComInterfaceMarshaller.cs
@@ -0,0 +1,45 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Runtime.Versioning;
+
+namespace System.Runtime.InteropServices.Marshalling
+{
+    /// <summary>
+    /// COM interface marshaller using a StrategyBasedComWrappers instance
+    /// </summary>
+    /// <remarks>
+    /// This marshaller will always pass the <see cref="CreateObjectFlags.Unwrap"/> flag
+    /// to <see cref="ComWrappers.GetOrCreateObjectForComInstance(IntPtr, CreateObjectFlags)"/>.
+    /// </remarks>
+    [UnsupportedOSPlatform("android")]
+    [UnsupportedOSPlatform("browser")]
+    [UnsupportedOSPlatform("ios")]
+    [UnsupportedOSPlatform("tvos")]
+    [CLSCompliant(false)]
+    [CustomMarshaller(typeof(CustomMarshallerAttribute.GenericPlaceholder), MarshalMode.Default, typeof(ComInterfaceMarshaller<>))]
+    public static unsafe class ComInterfaceMarshaller<T>
+    {
+        public static void* ConvertToUnmanaged(T? managed)
+        {
+            if (managed == null)
+            {
+                return null;
+            }
+            if (ComWrappers.TryGetComInstance(managed, out nint unknown))
+            {
+                return (void*)unknown;
+            }
+            return (void*)StrategyBasedComWrappers.DefaultMarshallingInstance.GetOrCreateComInterfaceForObject(managed, CreateComInterfaceFlags.None);
+        }
+
+        public static T? ConvertToManaged(void* unmanaged)
+        {
+            if (unmanaged == null)
+            {
+                return default;
+            }
+            return (T)StrategyBasedComWrappers.DefaultMarshallingInstance.GetOrCreateObjectForComInstance((nint)unmanaged, CreateObjectFlags.Unwrap);
+        }
+    }
+}
