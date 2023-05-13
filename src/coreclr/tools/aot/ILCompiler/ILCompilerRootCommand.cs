1f0950e3407a35ec61ecf673f11083ae61929ea8
Adeel <3840695+am11@users.noreply.github.com>
2023-05-13 15:38:37 +0300

    Update ilc description

--- src/coreclr/tools/aot/ILCompiler/ILCompilerRootCommand.cs
+++ src/coreclr/tools/aot/ILCompiler/ILCompilerRootCommand.cs
@@ -164,7 +164,7 @@ namespace ILCompiler
         public OptimizationMode OptimizationMode { get; private set; }
         public ParseResult Result;
 
-        public ILCompilerRootCommand(string[] args) : base(".NET Native IL Compiler")
+        public ILCompilerRootCommand(string[] args) : base(".NET AOT Compiler")
         {
             AddArgument(InputFilePaths);
             AddOption(ReferenceFiles);
