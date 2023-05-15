50037e540018b48fca9c5b5b6613e08d092c315b
Thays Grazia <thaystg@gmail.com>
2023-05-15 15:52:42 -0300

    Remove generation of dbgshim. Address @lambdageek comment.

--- src/mono/mono/component/debugger-agent.c
+++ src/mono/mono/component/debugger-agent.c
@@ -8846,10 +8846,8 @@ set_value:
 	}
 	case MDBGPROT_CMD_TYPE_ELEMENT_TYPE: 
 	{
-		if (m_class_get_byval_arg (klass)->type == MONO_TYPE_GENERICINST && MONO_TYPE_ISSTRUCT (m_class_get_byval_arg (klass)))
-			buffer_add_int (buf, MONO_TYPE_VALUETYPE);
-		else    
-			buffer_add_int (buf, m_class_get_byval_arg (klass)->type);
+		buffer_add_int (buf, m_class_get_byval_arg (klass)->type);
+		buffer_add_byte (buf, MONO_TYPE_ISSTRUCT (m_class_get_byval_arg (klass)));
 		break;
 	}
 	case MDBGPROT_CMD_TYPE_RANK: 
