da206a68e4811893ceffa6904c612b37a00e71e6
wfurt <tweinfurt@yahoo.com>
2023-05-15 09:41:08 -0700

    fixes & feedback

--- src/libraries/System.Net.Quic/src/System/Net/Quic/Internal/MsQuicTlsSecret.cs
+++ src/libraries/System.Net.Quic/src/System/Net/Quic/Internal/MsQuicTlsSecret.cs
@@ -15,104 +15,90 @@ namespace System.Net.Quic;
 internal sealed class MsQuicTlsSecret : IDisposable
 {
     private static readonly string? s_keyLogFile = Environment.GetEnvironmentVariable("SSLKEYLOGFILE");
+    private static readonly FileStream? s_fileStream = s_keyLogFile != null ? File.Open(s_keyLogFile, FileMode.Append, FileAccess.Write) : null;
 
-    private static readonly int s_clientRandomOffset = (int)Marshal.OffsetOf(typeof(QUIC_TLS_SECRETS), "ClientRandom");
-    private static readonly int s_clientEarlyTrafficSecretOffset = (int)Marshal.OffsetOf(typeof(QUIC_TLS_SECRETS), "ClientEarlyTrafficSecret");
-    private static readonly int s_clientHandshakeTrafficSecretOffset = (int)Marshal.OffsetOf(typeof(QUIC_TLS_SECRETS), "ClientHandshakeTrafficSecret");
-    private static readonly int s_serverHandshakeTrafficSecretOffset = (int)Marshal.OffsetOf(typeof(QUIC_TLS_SECRETS), "ServerHandshakeTrafficSecret");
-    private static readonly int s_clientTrafficSecretOffset = (int)Marshal.OffsetOf(typeof(QUIC_TLS_SECRETS), "ClientTrafficSecret0");
-    private static readonly int s_serverTrafficSecretOffset = (int)Marshal.OffsetOf(typeof(QUIC_TLS_SECRETS), "ServerTrafficSecret0");
+    private unsafe QUIC_TLS_SECRETS* _tlsSecrets;
 
-    private IntPtr tlsSecretsPtr = IntPtr.Zero;
-
-    public static MsQuicTlsSecret? Create(MsQuicContextSafeHandle handle)
+    public static unsafe MsQuicTlsSecret? Create(MsQuicContextSafeHandle handle)
     {
-        MsQuicTlsSecret? secret = null;
-        if (s_keyLogFile != null)
+        if (s_fileStream != null)
         {
             try {
-                secret = new MsQuicTlsSecret(handle);
+                QUIC_TLS_SECRETS* ptr = handle.GetSecretsBuffer();
+                if (ptr != null)
+                {
+                    int status = MsQuicApi.Api.SetParam(handle, QUIC_PARAM_CONN_TLS_SECRETS, (uint)sizeof(QUIC_TLS_SECRETS), ptr );
+
+                    if (status == QUIC_STATUS_SUCCESS)
+                    {
+                        return new MsQuicTlsSecret(ptr);
+                    }
+                    else
+                    {
+                        NetEventSource.Error(handle, "Failed to set native memory for TLS secret.");
+                    }
+                }
             }
             catch { };
         }
 
-        return secret;
+        return null;
     }
 
-    private unsafe MsQuicTlsSecret(MsQuicContextSafeHandle handle)
+    private unsafe MsQuicTlsSecret(QUIC_TLS_SECRETS* memory)
     {
-        void * ptr = NativeMemory.Alloc((nuint)sizeof(QUIC_TLS_SECRETS));
-        if (ptr != null)
-        {
-            MsQuicApi.Api.SetParam(handle, QUIC_PARAM_CONN_TLS_SECRETS, (uint)sizeof(QUIC_TLS_SECRETS), ptr);
-            tlsSecretsPtr = (IntPtr)ptr;
-        }
+        _tlsSecrets = memory;
     }
 
-    public void WriteSecret()
+    public void WriteSecret() => WriteSecret(s_fileStream);
+    public unsafe void WriteSecret(FileStream? stream)
     {
-        if (s_keyLogFile != null)
+        if (stream != null && _tlsSecrets != null)
         {
-            WriteSecret(s_keyLogFile);
-        }
-    }
-
-    public void WriteSecret(string fileName)
-    {
-        using (FileStream stream = File.Open(fileName, FileMode.Append, FileAccess.Write))
-        {
-            WriteSecret(stream);
-        }
-    }
-
-    public unsafe void WriteSecret(FileStream stream)
-    {
-        lock (stream)
-        {
-            string clientRandom = string.Empty;
-            ReadOnlySpan<byte> buffer = new ReadOnlySpan<byte>((void*)tlsSecretsPtr, sizeof(QUIC_TLS_SECRETS));
-            ReadOnlySpan<QUIC_TLS_SECRETS> secrets = MemoryMarshal.Cast<byte, QUIC_TLS_SECRETS>(buffer);
-
-            if (secrets[0].IsSet.ClientRandom != 0)
+            lock (stream)
             {
-                clientRandom =  HexConverter.ToString(buffer.Slice(s_clientRandomOffset, 32));
-            }
+                string clientRandom = string.Empty;
 
-            if (secrets[0].IsSet.ClientHandshakeTrafficSecret != 0)
-            {
-                stream.Write(Encoding.ASCII.GetBytes($"CLIENT_HANDSHAKE_TRAFFIC_SECRET {clientRandom} {HexConverter.ToString(buffer.Slice(s_clientHandshakeTrafficSecretOffset, secrets[0].SecretLength))}\n"));
-            }
+                if (_tlsSecrets->IsSet.ClientRandom != 0)
+                {
+                    clientRandom = HexConverter.ToString(new ReadOnlySpan<byte>(_tlsSecrets->ClientRandom, 32));
+                }
 
-            if (secrets[0].IsSet.ServerHandshakeTrafficSecret != 0)
-            {
-                stream.Write(Encoding.ASCII.GetBytes($"SERVER_HANDSHAKE_TRAFFIC_SECRET {clientRandom} {HexConverter.ToString(buffer.Slice(s_serverHandshakeTrafficSecretOffset, secrets[0].SecretLength))}\n"));
-            }
+                if (_tlsSecrets->IsSet.ClientHandshakeTrafficSecret != 0)
+                {
+                    stream.Write(Encoding.ASCII.GetBytes($"CLIENT_HANDSHAKE_TRAFFIC_SECRET {clientRandom} {HexConverter.ToString(new ReadOnlySpan<byte>(_tlsSecrets->ClientHandshakeTrafficSecret, _tlsSecrets->SecretLength))}\n"));
+                }
 
-            if (secrets[0].IsSet.ClientTrafficSecret0 != 0)
-            {
-                stream.Write(Encoding.ASCII.GetBytes($"CLIENT_TRAFFIC_SECRET_0 {clientRandom} {HexConverter.ToString(buffer.Slice(s_clientTrafficSecretOffset, secrets[0].SecretLength))}\n"));
-            }
+                if (_tlsSecrets->IsSet.ServerHandshakeTrafficSecret != 0)
+                {
+                    stream.Write(Encoding.ASCII.GetBytes($"SERVER_HANDSHAKE_TRAFFIC_SECRET {clientRandom} {HexConverter.ToString(new ReadOnlySpan<byte>(_tlsSecrets->ServerHandshakeTrafficSecret, _tlsSecrets->SecretLength))}\n"));
+                }
 
-            if (secrets[0].IsSet.ServerTrafficSecret0 != 0)
-            {
-                stream.Write(Encoding.ASCII.GetBytes($"SERVER_TRAFFIC_SECRET_0 {clientRandom} {HexConverter.ToString(buffer.Slice(s_serverTrafficSecretOffset, secrets[0].SecretLength))}\n"));
-            }
+                if (_tlsSecrets->IsSet.ClientTrafficSecret0 != 0)
+                {
+                    stream.Write(Encoding.ASCII.GetBytes($"CLIENT_TRAFFIC_SECRET_0 {clientRandom} {HexConverter.ToString(new ReadOnlySpan<byte>(_tlsSecrets->ClientTrafficSecret0, _tlsSecrets->SecretLength))}\n"));
+                }
 
-            if (secrets[0].IsSet.ClientEarlyTrafficSecret != 0)
-            {
-                stream.Write(Encoding.ASCII.GetBytes($"CLIENT_EARLY_TRAFFIC_SECRET {clientRandom} {HexConverter.ToString(buffer.Slice(s_clientEarlyTrafficSecretOffset, secrets[0].SecretLength))}\n"));
+                if (_tlsSecrets->IsSet.ServerTrafficSecret0 != 0)
+                {
+                    stream.Write(Encoding.ASCII.GetBytes($"SERVER_TRAFFIC_SECRET_0 {clientRandom} {HexConverter.ToString(new ReadOnlySpan<byte>(_tlsSecrets->ServerTrafficSecret0, _tlsSecrets->SecretLength))}\n"));
+                }
+
+                if (_tlsSecrets->IsSet.ClientEarlyTrafficSecret != 0)
+                {
+                    stream.Write(Encoding.ASCII.GetBytes($"CLIENT_EARLY_TRAFFIC_SECRET {clientRandom} {HexConverter.ToString(new ReadOnlySpan<byte>(_tlsSecrets->ClientEarlyTrafficSecret, _tlsSecrets->SecretLength))}\n"));
+                }
+
+                stream.Flush();
             }
         }
     }
 
     public unsafe void Dispose()
     {
-        IntPtr ptr = Interlocked.Exchange(ref tlsSecretsPtr, IntPtr.Zero);
-        if (ptr != IntPtr.Zero)
+        if (_tlsSecrets != null)
         {
-            Span<byte> secret = new Span<byte>((void*)ptr, sizeof(QUIC_TLS_SECRETS));
-            secret.Clear();
-            NativeMemory.Free((void*)ptr);
+            NativeMemory.Clear(_tlsSecrets, (nuint)sizeof(QUIC_TLS_SECRETS));
         }
     }
 }
