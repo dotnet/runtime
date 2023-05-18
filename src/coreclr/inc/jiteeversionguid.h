0f8afd209a401836a8fe116936ff9428992d15ef
Tanner Gooding <tagoo@outlook.com>
2023-05-18 14:25:00 -0700

    Add an undocumented switch to allow controlling the preferred vector width emitted implicitly by the JIT (#86457)
    * Add an undocumented switch to allow controlling the preferred vector width emitted implicitly by the JIT
    
    * Resolving issues and responding to PR feedback
    
    * Simplifying the xarch cpu info check

--- src/coreclr/inc/jiteeversionguid.h
+++ src/coreclr/inc/jiteeversionguid.h
@@ -43,11 +43,11 @@ typedef const GUID *LPCGUID;
 #define GUID_DEFINED
 #endif // !GUID_DEFINED
 
-constexpr GUID JITEEVersionIdentifier = { /* f63c2964-bae9-448f-baaf-9c9f2d4292f2  */
-    0xf63c2964,
-    0xbae9,
-    0x448f,
-    {0xba, 0xaf, 0x9c, 0x9f, 0x2d, 0x42, 0x92, 0xf2}
+constexpr GUID JITEEVersionIdentifier = { /* c540b287-0d17-4fc0-bac8-abd055acccb8 */
+    0xc540b287,
+    0x0d17,
+    0x4fc0,
+    {0xba, 0xc8, 0xab, 0xd0, 0x55, 0xac, 0xcc, 0xb8}
   };
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
