6022b3e0c340fbe1abb124152ad05fc68ccd6030
Ilona Tomkowicz <32700855+ilonatommy@users.noreply.github.com>
2023-05-18 14:39:34 +0200

    [browser][non-icu] `HybridGlobalization` indexing (#85254)
    * A bit faster version of indexing. WIP
    
    * Tiny speedup.
    
    * Fixed IndexOf, ToDo: LastIndexOf.
    
    * All tests pass.
    
    * Updated docs.
    
    * Update docs.
    
    * Slicing + saving previous absolute index instead of pushing the iterator to the start position.
    
    * Refactored.
    
    * Fixed tests on browser.
    
    * Str1 and str2 was confusing.
    
    * Fix CI- correctly trimming Hybrid properties.
    
    * Previous commit should target only Browser.
    
    * Applied @mkhamoyan's suggestion to avoid code duplication.
    
    * Applied @pavelsavara's review.
    
    * Get rid of build errors.
    
    * Revert.

--- src/mono/wasm/runtime/corebindings.c
+++ src/mono/wasm/runtime/corebindings.c
@@ -48,6 +48,7 @@ extern void mono_wasm_change_case(MonoString **exceptionMessage, MonoString **cu
 extern int mono_wasm_compare_string(MonoString **exceptionMessage, MonoString **culture, const uint16_t* str1, int32_t str1Length, const uint16_t* str2, int32_t str2Length, int32_t options);
 extern mono_bool mono_wasm_starts_with(MonoString **exceptionMessage, MonoString **culture, const uint16_t* str1, int32_t str1Length, const uint16_t* str2, int32_t str2Length, int32_t options);
 extern mono_bool mono_wasm_ends_with(MonoString **exceptionMessage, MonoString **culture, const uint16_t* str1, int32_t str1Length, const uint16_t* str2, int32_t str2Length, int32_t options);
+extern int mono_wasm_index_of(MonoString **exceptionMessage, MonoString **culture, const uint16_t* str1, int32_t str1Length, const uint16_t* str2, int32_t str2Length, int32_t options, mono_bool fromBeginning);
 
 void bindings_initialize_internals (void)
 {
@@ -81,4 +82,5 @@ void bindings_initialize_internals (void)
 	mono_add_internal_call ("Interop/JsGlobalization::CompareString", mono_wasm_compare_string);
 	mono_add_internal_call ("Interop/JsGlobalization::StartsWith", mono_wasm_starts_with);
 	mono_add_internal_call ("Interop/JsGlobalization::EndsWith", mono_wasm_ends_with);
+	mono_add_internal_call ("Interop/JsGlobalization::IndexOf", mono_wasm_index_of);
 }
