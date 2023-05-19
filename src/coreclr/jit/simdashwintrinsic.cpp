7981feec29bda917b71e5290b08cb374d54909c7
Tanner Gooding <tagoo@outlook.com>
2023-05-18 17:45:00 -0700

    Ensure Vector2/3/4, Quaternion, and Plane don't have a false dependency on Vector<T>

--- src/coreclr/jit/simdashwintrinsic.cpp
+++ src/coreclr/jit/simdashwintrinsic.cpp
@@ -44,10 +44,11 @@ const SimdAsHWIntrinsicInfo& SimdAsHWIntrinsicInfo::lookup(NamedIntrinsic id)
 // lookupId: Gets the NamedIntrinsic for a given method name and InstructionSet
 //
 // Arguments:
+//    comp               -- The compiler
+//    sig                -- The signature of the intrinsic
 //    className          -- The name of the class associated with the SimdIntrinsic to lookup
 //    methodName         -- The name of the method associated with the SimdIntrinsic to lookup
 //    enclosingClassName -- The name of the enclosing class
-//    sizeOfVectorT      -- The size of Vector<T> in bytes
 //
 // Return Value:
 //    The NamedIntrinsic associated with methodName and classId
@@ -55,10 +56,9 @@ NamedIntrinsic SimdAsHWIntrinsicInfo::lookupId(Compiler*         comp,
                                                CORINFO_SIG_INFO* sig,
                                                const char*       className,
                                                const char*       methodName,
-                                               const char*       enclosingClassName,
-                                               int               sizeOfVectorT)
+                                               const char*       enclosingClassName)
 {
-    SimdAsHWIntrinsicClassId classId = lookupClassId(className, enclosingClassName, sizeOfVectorT);
+    SimdAsHWIntrinsicClassId classId = lookupClassId(comp, className, enclosingClassName);
 
     if (classId == SimdAsHWIntrinsicClassId::Unknown)
     {
@@ -74,10 +74,41 @@ NamedIntrinsic SimdAsHWIntrinsicInfo::lookupId(Compiler*         comp,
         isInstanceMethod = true;
     }
 
-    if (strcmp(methodName, "get_IsHardwareAccelerated") == 0)
+    if (classId == SimdAsHWIntrinsicClassId::Vector)
     {
-        return comp->IsBaselineSimdIsaSupported() ? NI_IsSupported_True : NI_IsSupported_False;
+        // We want to avoid doing anything that would unnecessarily trigger a recorded dependency against Vector<T>
+        // so we duplicate a few checks here to ensure this works smoothly for the static Vector class.
+
+        assert(!isInstanceMethod);
+
+        if (strcmp(methodName, "get_IsHardwareAccelerated") == 0)
+        {
+            return comp->IsBaselineSimdIsaSupported() ? NI_IsSupported_True : NI_IsSupported_False;
+        }
+
+        var_types            retType         = JITtype2varType(sig->retType);
+        CorInfoType          simdBaseJitType = CORINFO_TYPE_UNDEF;
+        CORINFO_CLASS_HANDLE argClass        = NO_CLASS_HANDLE;
+
+        if (retType == TYP_STRUCT)
+        {
+            argClass = sig->retTypeSigClass;
+        }
+        else if (numArgs != 0)
+        {
+            argClass = comp->info.compCompHnd->getArgClass(sig, sig->args);
+        }
+
+        const char* argNamespaceName;
+        const char* argClassName = comp->getClassNameFromMetadata(argClass, &argNamespaceName);
+
+        classId = lookupClassId(comp, argClassName, nullptr);
+
+        assert(classId != SimdAsHWIntrinsicClassId::Unknown);
+        assert(classId != SimdAsHWIntrinsicClassId::Vector);
     }
+
+    assert(strcmp(methodName, "get_IsHardwareAccelerated") != 0);
 
     for (int i = 0; i < (NI_SIMD_AS_HWINTRINSIC_END - NI_SIMD_AS_HWINTRINSIC_START - 1); i++)
     {
@@ -113,15 +144,15 @@ NamedIntrinsic SimdAsHWIntrinsicInfo::lookupId(Compiler*         comp,
 // lookupClassId: Gets the SimdAsHWIntrinsicClassId for a given class name and enclsoing class name
 //
 // Arguments:
+//    comp               -- The compiler
 //    className          -- The name of the class associated with the SimdAsHWIntrinsicClassId to lookup
 //    enclosingClassName -- The name of the enclosing class
-//    sizeOfVectorT      -- The size of Vector<T> in bytes
 //
 // Return Value:
 //    The SimdAsHWIntrinsicClassId associated with className and enclosingClassName
-SimdAsHWIntrinsicClassId SimdAsHWIntrinsicInfo::lookupClassId(const char* className,
-                                                              const char* enclosingClassName,
-                                                              int         sizeOfVectorT)
+SimdAsHWIntrinsicClassId SimdAsHWIntrinsicInfo::lookupClassId(Compiler*   comp,
+                                                              const char* className,
+                                                              const char* enclosingClassName)
 {
     assert(className != nullptr);
 
@@ -159,7 +190,11 @@ SimdAsHWIntrinsicClassId SimdAsHWIntrinsicInfo::lookupClassId(const char* classN
 
             className += 6;
 
-            if (strcmp(className, "2") == 0)
+            if (className[0] == '\0')
+            {
+                return SimdAsHWIntrinsicClassId::Vector;
+            }
+            else if (strcmp(className, "2") == 0)
             {
                 return SimdAsHWIntrinsicClassId::Vector2;
             }
@@ -171,17 +206,25 @@ SimdAsHWIntrinsicClassId SimdAsHWIntrinsicInfo::lookupClassId(const char* classN
             {
                 return SimdAsHWIntrinsicClassId::Vector4;
             }
-            else if ((className[0] == '\0') || (strcmp(className, "`1") == 0))
+            else if (strcmp(className, "`1") == 0)
             {
+                uint32_t vectorTByteLength = comp->getVectorTByteLength();
+
 #if defined(TARGET_XARCH)
-                if (sizeOfVectorT == 32)
+                if (vectorTByteLength == 32)
                 {
                     return SimdAsHWIntrinsicClassId::VectorT256;
                 }
 #endif // TARGET_XARCH
 
-                assert(sizeOfVectorT == 16);
-                return SimdAsHWIntrinsicClassId::VectorT128;
+                if (vectorTByteLength == 16)
+                {
+                    return SimdAsHWIntrinsicClassId::VectorT128;
+                }
+                else
+                {
+                    return SimdAsHWIntrinsicClassId::Unknown;
+                }
             }
             break;
         }
@@ -655,6 +698,10 @@ GenTree* Compiler::impSimdAsHWIntrinsicSpecial(NamedIntrinsic       intrinsic,
             break;
         }
 
+        case NI_Quaternion_WithElement:
+        case NI_Vector2_WithElement:
+        case NI_Vector3_WithElement:
+        case NI_Vector4_WithElement:
         case NI_VectorT128_WithElement:
         case NI_VectorT256_WithElement:
         {
@@ -736,6 +783,10 @@ GenTree* Compiler::impSimdAsHWIntrinsicSpecial(NamedIntrinsic       intrinsic,
             break;
         }
 
+        case NI_Quaternion_WithElement:
+        case NI_Vector2_WithElement:
+        case NI_Vector3_WithElement:
+        case NI_Vector4_WithElement:
         case NI_VectorT128_WithElement:
         {
             assert(numArgs == 3);
@@ -1484,9 +1535,13 @@ GenTree* Compiler::impSimdAsHWIntrinsicSpecial(NamedIntrinsic       intrinsic,
                 }
 
                 case NI_Quaternion_get_Item:
+                case NI_Quaternion_GetElement:
                 case NI_Vector2_get_Item:
+                case NI_Vector2_GetElement:
                 case NI_Vector3_get_Item:
+                case NI_Vector3_GetElement:
                 case NI_Vector4_get_Item:
+                case NI_Vector4_GetElement:
                 case NI_VectorT128_get_Item:
                 case NI_VectorT128_GetElement:
 #if defined(TARGET_XARCH)
@@ -1986,6 +2041,10 @@ GenTree* Compiler::impSimdAsHWIntrinsicSpecial(NamedIntrinsic       intrinsic,
                     break;
                 }
 
+                case NI_Quaternion_WithElement:
+                case NI_Vector2_WithElement:
+                case NI_Vector3_WithElement:
+                case NI_Vector4_WithElement:
                 case NI_VectorT128_WithElement:
 #if defined(TARGET_XARCH)
                 case NI_VectorT256_WithElement:
