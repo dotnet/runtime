b70e514846879625c8e659687243df8f4bce92ed
James Newton-King <james@newtonking.com>
2023-05-22 11:09:50 +0800

    WIP enrichment

---
+++ src/libraries/Microsoft.Extensions.Logging/tests/Common/EnrichmentTests.cs
@@ -0,0 +1,163 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ConsoleApp31.Prototype;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging.Testing;
+using Xunit;
+
+namespace Microsoft.Extensions.Logging.Test
+{
+    public class EnrichmentTests
+    {
+        [Fact]
+        public void LogInformation_InvokesProcessor()
+        {
+            // Arrange
+            var sink = new TestSink();
+            var provider = new TestLoggerProvider(sink, isEnabled: true);
+
+            List<string> logMessages = new List<string>();
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddLogging(builder =>
+            {
+                builder.SetMinimumLevel(LogLevel.Trace);
+                builder.AddProvider(provider);
+                builder.Enrich<string>("prop1", () => "Value!");
+                builder.AddProcessor((serviceProvider, processor) => new TestLogEntryProcessor(processor, m => logMessages.Add(m)));
+            });
+            var loggerFactory = serviceCollection.BuildServiceProvider().GetRequiredService<ILoggerFactory>();
+            var logger = loggerFactory.CreateLogger("Test");
+
+            // Act
+            logger.LogInformation("Hello {Name}", "John Doe");
+
+            // Assert
+            Assert.Collection(logMessages, m => Assert.Equal("Hello John Doe", m));
+
+            Assert.Equal(1, sink.Writes.Count());
+            Assert.True(sink.Writes.TryTake(out var write));
+            Assert.Equal(LogLevel.Information, write.LogLevel);
+            Assert.Equal("Hello John Doe", write.State.ToString());
+            Assert.Equal(0, write.EventId);
+            Assert.Null(write.Exception);
+        }
+
+        [Fact]
+        public void DefinedLog_InvokesProcessor()
+        {
+            // Arrange
+            var sink = new TestSink();
+            var provider = new TestLoggerProvider(sink, isEnabled: true);
+
+            List<string> logMessages = new List<string>();
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddLogging(builder =>
+            {
+                builder.SetMinimumLevel(LogLevel.Trace);
+                builder.AddProvider(provider);
+                builder.AddProcessor((serviceProvider, processor) => new TestLogEntryProcessor(processor, m => logMessages.Add(m)));
+            });
+            var loggerFactory = serviceCollection.BuildServiceProvider().GetRequiredService<ILoggerFactory>();
+            var logger = loggerFactory.CreateLogger("Test");
+
+            var definedLog = LoggerMessage.Define<string, int>(
+                LogLevel.Information,
+                new EventId(1, "Test"),
+                "Hello {Name}. You are {Age} years old.");
+
+            // Act
+            definedLog(logger, "John Doe", 10, null);
+
+            // Assert
+            Assert.Collection(logMessages, m => Assert.Equal("Hello John Doe. You are 10 years old.", m));
+
+            Assert.Equal(1, sink.Writes.Count());
+            Assert.True(sink.Writes.TryTake(out var write));
+            Assert.Equal(LogLevel.Information, write.LogLevel);
+            Assert.Equal("Hello John Doe. You are 10 years old.", write.State.ToString());
+            Assert.Equal(1, write.EventId);
+            Assert.Null(write.Exception);
+        }
+
+        private sealed class TestLogEntryProcessor : ILogEntryProcessor
+        {
+            private readonly ILogEntryProcessor _nextProcessor;
+            private readonly Action<string> _handleLogEntryCallback;
+
+            public TestLogEntryProcessor(ILogEntryProcessor nextProcessor, Action<string> handleLogEntryCallback)
+            {
+                _nextProcessor = nextProcessor;
+                _handleLogEntryCallback = handleLogEntryCallback;
+            }
+
+            public LogEntryHandler<TState, TEnrichmentProperties> GetLogEntryHandler<TState, TEnrichmentProperties>(ILogMetadata<TState>? metadata, out bool enabled, out bool dynamicEnabledCheckRequired)
+            {
+                var nextHandler = _nextProcessor.GetLogEntryHandler<TState, TEnrichmentProperties>(metadata, out enabled, out dynamicEnabledCheckRequired);
+                return new TestLogEntryHandler<TState, TEnrichmentProperties>(nextHandler, _handleLogEntryCallback);
+            }
+
+            public ScopeHandler<TState> GetScopeHandler<TState>(ILogMetadata<TState>? metadata, out bool enabled, out bool dynamicEnabledCheckRequired) where TState : notnull
+            {
+                var nextHandler = _nextProcessor.GetScopeHandler<TState>(metadata, out enabled, out dynamicEnabledCheckRequired);
+                return new TestScopeHandler<TState>(nextHandler, _handleLogEntryCallback);
+            }
+
+            public bool IsEnabled(LogLevel logLevel) => _nextProcessor.IsEnabled(logLevel);
+        }
+
+        private sealed class TestLogEntryHandler<TState, TEnrichmentProperties> : LogEntryHandler<TState, TEnrichmentProperties>
+        {
+            private readonly LogEntryHandler<TState, TEnrichmentProperties> _nextHandler;
+            private readonly Action<string> _handleLogEntryCallback;
+
+            public TestLogEntryHandler(LogEntryHandler<TState, TEnrichmentProperties> nextHandler, Action<string> handleLogEntryCallback)
+            {
+                _nextHandler = nextHandler;
+                _handleLogEntryCallback = handleLogEntryCallback;
+            }
+
+            public override void HandleLogEntry(ref LogEntry<TState, TEnrichmentProperties> logEntry)
+            {
+                var message = logEntry.Formatter(logEntry.State, logEntry.Exception);
+                _handleLogEntryCallback(message);
+
+                _nextHandler.HandleLogEntry(ref logEntry);
+            }
+
+            public override bool IsEnabled(LogLevel level)
+            {
+                return _nextHandler.IsEnabled(level);
+            }
+        }
+
+        private sealed class TestScopeHandler<TState> : ScopeHandler<TState>
+        {
+            private readonly ScopeHandler<TState> _nextHandler;
+            private readonly Action<string> _handleLogEntryCallback;
+
+            public TestScopeHandler(ScopeHandler<TState> nextHandler, Action<string> handleLogEntryCallback)
+            {
+                _nextHandler = nextHandler;
+                _handleLogEntryCallback = handleLogEntryCallback;
+            }
+
+            public override IDisposable? HandleBeginScope(ref TState state)
+            {
+                _handleLogEntryCallback(state.ToString());
+
+                return _nextHandler.HandleBeginScope(ref state);
+            }
+
+            public override bool IsEnabled(LogLevel level)
+            {
+                return _nextHandler.IsEnabled(level);
+            }
+        }
+    }
+}
