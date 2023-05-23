aa76f240230bdc56e4791993557b32e8370a3f85
Hamish Arblaster <hamarb123@gmail.com>
2023-05-23 16:46:32 +1000

    Try clonefile immediately as per feedback

--- src/libraries/System.Private.CoreLib/src/System/IO/FileSystem.TryCloneFile.OSX.cs
+++ src/libraries/System.Private.CoreLib/src/System/IO/FileSystem.TryCloneFile.OSX.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Microsoft.Win32.SafeHandles;
+using System.Diagnostics;
 
 namespace System.IO
 {
@@ -9,78 +10,81 @@ namespace System.IO
     {
         private static partial bool TryCloneFile(string sourceFullPath, in Interop.Sys.FileStatus srcStat, string destFullPath, bool overwrite)
         {
-            // get the file permissions for source file
-            UnixFileMode filePermissions = (UnixFileMode)srcStat.Mode & SafeFileHandle.PermissionMask;
-
-            // Read FileStatus of destination file to determine how to continue
-            int destError = Interop.Sys.Stat(destFullPath, out Interop.Sys.FileStatus destStat);
-            if (destError != 0)
+            // Try to clone the file immediately, this will only succeed if the destination doesn't exist,
+            // so we don't worry about locking for this one.
+            if (Interop.@libc.clonefile(sourceFullPath, destFullPath, Interop.@libc.CLONE_ACL) == 0)
             {
-                // stat failed. If the destination doesn't exist (which is the expected
-                // cause), try clonefile; otherwise, fall back to a normal copy.
-                if (Interop.Sys.GetLastError() == Interop.Error.ENOENT)
+                // Success
+                return true;
+            }
+            Interop.Error error = Interop.Sys.GetLastError();
+
+            // Try to delete the destination file if we're overwriting.
+            if (error == Interop.Error.EEXIST && overwrite)
+            {
+                // Get the file permissions for source file
+                UnixFileMode filePermissions = srcStat.Mode & SafeFileHandle.PermissionMask;
+
+                // Read FileStatus of destination file to determine how to continue (we need to check that
+                // destination doesn't point to the same file as the source file so we can fail appropriately)
+                int destError = Interop.Sys.Stat(destFullPath, out Interop.Sys.FileStatus destStat);
+                if (destError != 0)
                 {
-                    goto tryCloneFile;
+                    // stat failed. If the destination doesn't exist anymore,
+                    // try clonefile; otherwise, fall back to a normal copy.
+                    if (Interop.Sys.GetLastError() == Interop.Error.ENOENT)
+                    {
+                        goto tryCloneFile;
+                    }
+                    else
+                    {
+                        return false;
+                    }
                 }
                 else
                 {
-                    return false;
-                }
-            }
-            else
-            {
-                // Destination exists:
-                if (!overwrite)
-                {
-                    // Throw an error if we're not overriding
-                    throw Interop.GetExceptionForIoErrno(new Interop.ErrorInfo(Interop.Error.EEXIST));
-                }
-                if (srcStat.Dev != destStat.Dev)
-                {
-                    // On different device
-                    return false;
-                }
-                if (srcStat.Ino == destStat.Ino)
-                {
-                    // Copying onto itself
-                    throw new IOException(SR.Format(SR.IO_SharingViolation_File, destFullPath));
-                }
-            }
-
-            // Try deleting destination:
-            {
-                // Delete the destination. This should fail on directories.
-                // Get a lock to the dest file to ensure we don't copy onto it when it's locked by something else, and then delete it.
-                try
-                {
-                    using SafeFileHandle? dstHandle = SafeFileHandle.Open(destFullPath, FileMode.Open, FileAccess.ReadWrite,
-                        FileShare.None, FileOptions.None, preallocationSize: 0, filePermissions);
-                    File.Delete(destFullPath);
-                }
-                catch (FileNotFoundException)
-                {
-                    // We don't want to throw if it's just the file not existing, since we're trying to delete it.
-                }
-            }
-
-            // Try clonefile:
-            tryCloneFile:
-            {
-                if (Interop.@libc.clonefile(sourceFullPath, destFullPath, Interop.@libc.CLONE_ACL) == 0)
-                {
-                    // Success
-                    return true;
+                    // Destination exists:
+                    if (srcStat.Dev != destStat.Dev)
+                    {
+                        // On different device, so fall back to normal copy
+                        return false;
+                    }
+                    if (srcStat.Ino == destStat.Ino)
+                    {
+                        // Copying onto itself
+                        throw new IOException(SR.Format(SR.IO_SharingViolation_File, destFullPath));
+                    }
                 }
 
-                // Clonefile fails if the destination exists (which we try to avoid), so throw if error is
-                // the destination exists and we're not overwriting, otherwise we will go to fallback path.
-                if (Interop.Sys.GetLastError() == Interop.Error.EEXIST && !overwrite)
+                // Try deleting destination:
                 {
-                    throw Interop.GetExceptionForIoErrno(new Interop.ErrorInfo(Interop.Error.EEXIST));
+                    // Delete the destination. This should fail on directories.
+                    // Get a lock to the dest file to ensure we don't copy onto it when it's locked by something else, and then delete it.
+                    try
+                    {
+                        using SafeFileHandle? dstHandle = SafeFileHandle.Open(destFullPath, FileMode.Open, FileAccess.ReadWrite,
+                            FileShare.None, FileOptions.None, preallocationSize: 0, filePermissions);
+                        File.Delete(destFullPath);
+                    }
+                    catch (FileNotFoundException)
+                    {
+                        // We don't want to throw if it's just the file not existing, since we're trying to delete it.
+                    }
+                }
+
+                // Try clonefile:
+                tryCloneFile:
+                {
+                    if (Interop.@libc.clonefile(sourceFullPath, destFullPath, Interop.@libc.CLONE_ACL) == 0)
+                    {
+                        // Success
+                        return true;
+                    }
+                    error = Interop.Sys.GetLastError();
                 }
             }
 
-            // Otherwise we want to go the the fallback
+            // Fall back to regular copy
             return false;
         }
     }
