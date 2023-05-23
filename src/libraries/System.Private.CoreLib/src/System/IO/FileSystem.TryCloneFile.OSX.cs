323cf672f8c4b6a3b26113a78525eb51bfcf0be2
Hamish Arblaster <hamarb123@gmail.com>
2023-05-24 08:58:51 +1000

    Add some code to deal with EINVAL
    • We can get EINVAL resulting from trying to copy ACLs, so we retry with with the flag to copy ACLs disabled (up to 1 time)

--- src/libraries/System.Private.CoreLib/src/System/IO/FileSystem.TryCloneFile.OSX.cs
+++ src/libraries/System.Private.CoreLib/src/System/IO/FileSystem.TryCloneFile.OSX.cs
@@ -12,12 +12,22 @@ namespace System.IO
         {
             // Try to clone the file immediately, this will only succeed if the
             // destination doesn't exist, so we don't worry about locking for this one.
-            if (Interop.@libc.clonefile(sourceFullPath, destFullPath, Interop.@libc.CLONE_ACL) == 0)
+            int flags = Interop.@libc.CLONE_ACL;
+            retry1:
+            if (Interop.@libc.clonefile(sourceFullPath, destFullPath, flags) == 0)
             {
                 // Success.
                 return true;
             }
             Interop.Error error = Interop.Sys.GetLastError();
+
+            // Some filesystems don't support ACLs, so may fail due to trying to copy ACLs.
+            // This will disable them and allow trying again (a maximum of 1 time).
+            if (flags != 0 && error == Interop.Error.EINVAL)
+            {
+                flags = 0;
+                goto retry1;
+            }
 
             // Try to delete the destination file if we're overwriting.
             if (error == Interop.Error.EEXIST && overwrite)
@@ -36,16 +46,26 @@ namespace System.IO
                 }
 
                 // Try clonefile now we've deleted the destination file.
-                if (Interop.@libc.clonefile(sourceFullPath, destFullPath, Interop.@libc.CLONE_ACL) == 0)
+                retry2:
+                if (Interop.@libc.clonefile(sourceFullPath, destFullPath, flags) == 0)
                 {
                     // Success.
                     return true;
                 }
                 error = Interop.Sys.GetLastError();
+
+                // Some filesystems don't support ACLs, so may fail due to trying to copy ACLs.
+                // This will disable them and allow trying again (a maximum of 1 time).
+                if (flags != 0 && error == Interop.Error.EINVAL)
+                {
+                    flags = 0;
+                    goto retry2;
+                }
             }
 
-            // Check if it's not supported, if they're on different filesystems, or the destination file still exists.
-            if (error == Interop.Error.ENOTSUP || error == Interop.Error.EXDEV || error == Interop.Error.EEXIST)
+            // Check if it's not supported, if they're on different filesystems, the destination file still exists, or if we have unsupported flags still.
+            // EINVAL is checked again here since clonefile is implemented by filesystem-specific drivers, therefore it could potentially fail with flags=0.
+            if (error == Interop.Error.ENOTSUP || error == Interop.Error.EXDEV || error == Interop.Error.EEXIST || error == Interop.Error.EINVAL)
             {
                 // Fall back to normal copy.
                 return false;
