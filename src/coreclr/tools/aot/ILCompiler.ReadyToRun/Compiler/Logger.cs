09ce974d4ea63e268512314ffce8d11d40effcb8
Tomas Rylek <trylek@microsoft.com>
2023-05-24 14:00:38 +0200

    Delete no longer needed logging changes

--- src/coreclr/tools/aot/ILCompiler.ReadyToRun/Compiler/Logger.cs
+++ src/coreclr/tools/aot/ILCompiler.ReadyToRun/Compiler/Logger.cs
@@ -2,9 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.IO;
-using ILCompiler.Logging;
-using ILLink.Shared;
-using Internal.TypeSystem;
 
 namespace ILCompiler
 {
@@ -26,20 +23,5 @@ namespace ILCompiler
         {
             Writer.WriteLine(message);
         }
-
-        public void LogWarning(MessageOrigin origin, DiagnosticId id, params string[] args)
-        {
-            MessageContainer? warning = MessageContainer.CreateWarningMessage(this, origin, id, args);
-            if (warning.HasValue)
-                Writer.WriteLine(warning.Value);
-        }
-
-        public void LogWarning(TypeSystemEntity origin, DiagnosticId id, params string[] args) =>
-            LogWarning(new MessageOrigin(origin), id, args);
-
-        internal bool IsWarningSuppressed(int code, MessageOrigin origin) => false; // TODO - unify with ILCompiler.Compiler/Compiler/Logger.cs
-        internal bool IsWarningSubcategorySuppressed(string category) => false; // dtto
-        internal bool IsSingleWarn(ModuleDesc owningModule, string messageSubcategory) => false; // dtto
-        internal static bool IsWarningAsError(int _/*code*/) => false; // dtto
     }
 }
--- src/coreclr/tools/aot/ILCompiler.ReadyToRun/Compiler/ReadyToRunCompilerContext.cs
+++ src/coreclr/tools/aot/ILCompiler.ReadyToRun/Compiler/ReadyToRunCompilerContext.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using Internal.TypeSystem;
 
 using Debug = System.Diagnostics.Debug;
