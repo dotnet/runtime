7a119156cbe958c5c34098434e84010526fac5f1
Ilona Tomkowicz <32700855+ilonatommy@users.noreply.github.com>
2023-05-24 09:12:11 +0200

    [browser][non-icu] `HybridGlobalization` normalization. (#85510)
    * Normalization.
    
    * Missing measurement.
    
    * Trying to fix trimming error.
    
    * Applied @pavelsavara's idea.
    
    * Code review improvement.
    
    * Feedback: standardize the exceptions.
    
    * Revert changes from #85516 to clean the CI.
    
    * feedback

--- src/libraries/Common/src/Interop/Browser/Interop.Normalization.cs
+++ src/libraries/Common/src/Interop/Browser/Interop.Normalization.cs
@@ -2,14 +2,16 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Runtime.CompilerServices;
+using System.Text;
 
 internal static partial class Interop
 {
     internal static unsafe partial class JsGlobalization
     {
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        internal static extern unsafe void ChangeCaseInvariant(char* src, int srcLen, char* dstBuffer, int dstBufferCapacity, bool bToUpper, out int exceptionalResult, out object result);
+        internal static extern unsafe int IsNormalized(NormalizationForm normalizationForm, in string source, out int exceptionalResult, out object result);
+
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        internal static extern unsafe void ChangeCase(in string culture, char* src, int srcLen, char* dstBuffer, int dstBufferCapacity, bool bToUpper, out int exceptionalResult, out object result);
+        internal static extern unsafe int NormalizeString(NormalizationForm normalizationForm, in string source, char* dstBuffer, int dstBufferCapacity, out int exceptionalResult, out object result);
     }
 }
--- src/mono/wasm/runtime/exports-linker.ts
+++ src/mono/wasm/runtime/exports-linker.ts
@@ -27,7 +27,9 @@ import {
     mono_wasm_invoke_js_blazor, mono_wasm_invoke_js_with_args_ref, mono_wasm_get_object_property_ref, mono_wasm_set_object_property_ref,
     mono_wasm_get_by_index_ref, mono_wasm_set_by_index_ref, mono_wasm_get_global_object_ref
 } from "./net6-legacy/method-calls";
-import { mono_wasm_change_case, mono_wasm_change_case_invariant, mono_wasm_compare_string, mono_wasm_ends_with, mono_wasm_index_of, mono_wasm_starts_with } from "./hybrid-globalization";
+import { mono_wasm_change_case, mono_wasm_change_case_invariant } from "./hybrid-globalization/change-case";
+import { mono_wasm_compare_string, mono_wasm_ends_with, mono_wasm_starts_with, mono_wasm_index_of } from "./hybrid-globalization/collations";
+import { mono_wasm_is_normalized, mono_wasm_normalize_string } from "./hybrid-globalization/normalization";
 
 // the methods would be visible to EMCC linker
 // --- keep in sync with dotnet.cjs.lib.js ---
@@ -103,6 +105,8 @@ export function export_linker(): any {
         mono_wasm_starts_with,
         mono_wasm_ends_with,
         mono_wasm_index_of,
+        mono_wasm_is_normalized,
+        mono_wasm_normalize_string,
 
         // threading exports, if threading is enabled
         ...mono_wasm_threads_exports,
