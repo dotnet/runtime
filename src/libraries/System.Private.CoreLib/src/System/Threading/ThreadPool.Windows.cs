883d96672a571417bd9a8f07f8018c587cea85ca
Eduardo Velarde <32459232+eduardo-vp@users.noreply.github.com>
2023-05-26 14:17:35 -0700

    Update src/libraries/System.Private.CoreLib/src/System/Threading/ThreadPool.Windows.cs
    Co-authored-by: Koundinya Veluri <kouvel@users.noreply.github.com>

--- src/libraries/System.Private.CoreLib/src/System/Threading/ThreadPool.Windows.cs
+++ src/libraries/System.Private.CoreLib/src/System/Threading/ThreadPool.Windows.cs
@@ -21,16 +21,14 @@ namespace System.Threading
 #endif
 
 
-#if CORECLR
+#if !(TARGET_BROWSER && FEATURE_WASM_THREADS)
         // Indicates whether the thread pool should yield the thread from the dispatch loop to the runtime periodically so that
         // the runtime may use the thread for processing other work.
         //
         // Windows thread pool threads need to yield back to the thread pool periodically, otherwise those threads may be
         // considered to be doing long-running work and change thread pool heuristics, such as slowing or halting thread
         // injection.
-        internal static bool YieldFromDispatchLoop => UseWindowsThreadPool ? true : false;
-#elif !(TARGET_BROWSER && FEATURE_WASM_THREADS)
-        internal static bool YieldFromDispatchLoop => false;
+        internal static bool YieldFromDispatchLoop => UseWindowsThreadPool;
 #endif
 
         [CLSCompliant(false)]
