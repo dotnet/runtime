5b8437d9f9e410bf672bb62d5371fdeb8931e5f1
Isabelle Santin <isabelle@chosenfewsoftware.com>
2023-05-28 14:57:15 -0500

    [browser][mt] Update memory views after growth, refactor string processing, fix SharedArrayBuffer detection (#86664)
    Co-authored-by: pavelsavara <pavel.savara@gmail.com>

--- src/mono/wasm/runtime/hybrid-globalization/change-case.ts
+++ src/mono/wasm/runtime/hybrid-globalization/change-case.ts
@@ -1,26 +1,22 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-import { Module } from "../globals";
-import { setU16 } from "../memory";
 import { mono_wasm_new_external_root } from "../roots";
-import { conv_string_root } from "../strings";
+import { monoStringToString, utf16ToStringLoop, stringToUTF16 } from "../strings";
 import { MonoObject, MonoObjectRef, MonoString, MonoStringRef } from "../types/internal";
 import { Int32Ptr } from "../types/emscripten";
 import { wrap_error_root, wrap_no_error_root } from "../invoke-js";
 
-export function mono_wasm_change_case_invariant(src: number, srcLength: number, dst: number, dstLength: number, toUpper: number, is_exception: Int32Ptr, ex_address: MonoObjectRef) : void{
+export function mono_wasm_change_case_invariant(src: number, srcLength: number, dst: number, dstLength: number, toUpper: number, is_exception: Int32Ptr, ex_address: MonoObjectRef): void {
     const exceptionRoot = mono_wasm_new_external_root<MonoObject>(ex_address);
-    try{
-        const input = get_utf16_string(src, srcLength);
+    try {
+        const input = utf16ToStringLoop(src, src + 2 * srcLength);
         let result = toUpper ? input.toUpperCase() : input.toLowerCase();
         // Unicode defines some codepoints which expand into multiple codepoints,
         // originally we do not support this expansion
         if (result.length > dstLength)
             result = input;
-
-        for (let i = 0; i < result.length; i++)
-            setU16(dst + i*2, result.charCodeAt(i));
+        stringToUTF16(dst, dst + 2 * dstLength, result);
         wrap_no_error_root(is_exception, exceptionRoot);
     }
     catch (ex: any) {
@@ -31,20 +27,19 @@ export function mono_wasm_change_case_invariant(src: number, srcLength: number, 
     }
 }
 
-export function mono_wasm_change_case(culture: MonoStringRef, src: number, srcLength: number, dst: number, destLength: number, toUpper: number, is_exception: Int32Ptr, ex_address: MonoObjectRef) : void{
+export function mono_wasm_change_case(culture: MonoStringRef, src: number, srcLength: number, dst: number, dstLength: number, toUpper: number, is_exception: Int32Ptr, ex_address: MonoObjectRef): void {
     const cultureRoot = mono_wasm_new_external_root<MonoString>(culture),
         exceptionRoot = mono_wasm_new_external_root<MonoObject>(ex_address);
-    try{
-        const cultureName = conv_string_root(cultureRoot);
+    try {
+        const cultureName = monoStringToString(cultureRoot);
         if (!cultureName)
             throw new Error("Cannot change case, the culture name is null.");
-        const input = get_utf16_string(src, srcLength);
+        const input = utf16ToStringLoop(src, src + 2 * srcLength);
         let result = toUpper ? input.toLocaleUpperCase(cultureName) : input.toLocaleLowerCase(cultureName);
-        if (result.length > destLength)
+        if (result.length > dstLength)
             result = input;
 
-        for (let i = 0; i < destLength; i++)
-            setU16(dst + i*2, result.charCodeAt(i));
+        stringToUTF16(dst, dst + 2 * dstLength, result);
         wrap_no_error_root(is_exception, exceptionRoot);
     }
     catch (ex: any) {
@@ -54,12 +49,4 @@ export function mono_wasm_change_case(culture: MonoStringRef, src: number, srcLe
         cultureRoot.release();
         exceptionRoot.release();
     }
-}
-
-function get_utf16_string(ptr: number, length: number): string{
-    const view = new Uint16Array(Module.HEAPU16.buffer, ptr, length);
-    let string = "";
-    for (let i = 0; i < length; i++)
-        string += String.fromCharCode(view[i]);
-    return string;
 }
--- src/mono/wasm/runtime/jiterpreter-support.ts
+++ src/mono/wasm/runtime/jiterpreter-support.ts
@@ -7,6 +7,8 @@ import { WasmOpcode, WasmSimdOpcode } from "./jiterpreter-opcodes";
 import { MintOpcode } from "./mintops";
 import cwraps from "./cwraps";
 import { mono_log_error, mono_log_info } from "./logging";
+import { localHeapViewU8 } from "./memory";
+import { utf8ToString } from "./strings";
 
 export const maxFailures = 2,
     maxMemsetSize = 64,
@@ -903,7 +905,7 @@ export class BlobBuilder {
     constructor() {
         this.capacity = 16 * 1024;
         this.buffer = <any>Module._malloc(this.capacity);
-        Module.HEAPU8.fill(0, this.buffer, this.buffer + this.capacity);
+        localHeapViewU8().fill(0, this.buffer, this.buffer + this.capacity);
         this.size = 0;
         this.clear();
         if (typeof (TextEncoder) === "function")
@@ -919,7 +921,7 @@ export class BlobBuilder {
             throw new Error("Buffer full");
 
         const result = this.size;
-        Module.HEAPU8[this.buffer + (this.size++)] = value;
+        localHeapViewU8()[this.buffer + (this.size++)] = value;
         return result;
     }
 
@@ -1008,16 +1010,17 @@ export class BlobBuilder {
         if (typeof (count) !== "number")
             count = this.size;
 
-        Module.HEAPU8.copyWithin(destination.buffer + destination.size, this.buffer, this.buffer + count);
+        localHeapViewU8().copyWithin(destination.buffer + destination.size, this.buffer, this.buffer + count);
         destination.size += count;
     }
 
     appendBytes(bytes: Uint8Array, count?: number) {
         const result = this.size;
-        if (bytes.buffer === Module.HEAPU8.buffer) {
+        const heapU8 = localHeapViewU8();
+        if (bytes.buffer === heapU8.buffer) {
             if (typeof (count) !== "number")
                 count = bytes.length;
-            Module.HEAPU8.copyWithin(this.buffer + result, bytes.byteOffset, bytes.byteOffset + count);
+            heapU8.copyWithin(this.buffer + result, bytes.byteOffset, bytes.byteOffset + count);
             this.size += count;
         } else {
             if (typeof (count) === "number")
@@ -1067,7 +1070,7 @@ export class BlobBuilder {
     }
 
     getArrayView(fullCapacity?: boolean) {
-        return new Uint8Array(Module.HEAPU8.buffer, this.buffer, fullCapacity ? this.capacity : this.size);
+        return new Uint8Array(localHeapViewU8().buffer, this.buffer, fullCapacity ? this.capacity : this.size);
     }
 }
 
@@ -1483,7 +1486,7 @@ export function copyIntoScratchBuffer(src: NativePointer, size: number): NativeP
     if (size > 64)
         throw new Error("Scratch buffer size is 64");
 
-    Module.HEAPU8.copyWithin(<any>scratchBuffer, <any>src, <any>src + size);
+    localHeapViewU8().copyWithin(<any>scratchBuffer, <any>src, <any>src + size);
     return scratchBuffer;
 }
 
@@ -1882,7 +1885,7 @@ export function getOptions() {
 
 function updateOptions() {
     const pJson = cwraps.mono_jiterp_get_options_as_json();
-    const json = Module.UTF8ToString(<any>pJson);
+    const json = utf8ToString(<any>pJson);
     Module._free(<any>pJson);
     const blob = JSON.parse(json);
 
