15f5f79950841a42d136692b7a25d1b42feb810a
Andrii Kurdiumov <kant2002@gmail.com>
2023-05-30 09:44:37 +0600

    Read public GC options if available (#86068)

--- src/coreclr/tools/aot/ILCompiler/ILCompilerRootCommand.cs
+++ src/coreclr/tools/aot/ILCompiler/ILCompilerRootCommand.cs
@@ -94,6 +94,8 @@ namespace ILCompiler
             new(new[] { "--feature" }, Array.Empty<string>, "Feature switches to apply (format: 'Namespace.Name=[true|false]'");
         public Option<string[]> RuntimeOptions { get; } =
             new(new[] { "--runtimeopt" }, Array.Empty<string>, "Runtime options to set");
+        public Option<string[]> RuntimeKnobs { get; } =
+            new(new[] { "--runtimeknob" }, Array.Empty<string>, "Runtime knobs to set");
         public Option<int> Parallelism { get; } =
             new(new[] { "--parallelism" }, result =>
             {
@@ -208,6 +210,7 @@ namespace ILCompiler
             AddOption(AppContextSwitches);
             AddOption(FeatureSwitches);
             AddOption(RuntimeOptions);
+            AddOption(RuntimeKnobs);
             AddOption(Parallelism);
             AddOption(InstructionSet);
             AddOption(Guard);
