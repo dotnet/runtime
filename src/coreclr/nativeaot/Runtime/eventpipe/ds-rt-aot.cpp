af22e8326568231652f89a699891dc85646f8a42
Lakshan Fernando <lakshanf@hotmail.com>
2023-05-31 10:58:15 -0700

    Fix FreeBSD build break

--- src/coreclr/nativeaot/Runtime/eventpipe/ds-rt-aot.cpp
+++ src/coreclr/nativeaot/Runtime/eventpipe/ds-rt-aot.cpp
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#include <sys/types.h>
+
 #ifdef __APPLE__
 #include <sys/sysctl.h>
 #endif
