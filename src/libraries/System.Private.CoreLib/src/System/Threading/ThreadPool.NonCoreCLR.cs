37c096a3aa07aabf1ac75e9485abfa50abf2ad2a
Eduardo Velarde <32459232+eduardo-vp@users.noreply.github.com>
2023-05-31 16:36:45 -0700

    Update src/libraries/System.Private.CoreLib/src/System/Threading/ThreadPool.NonCoreCLR.cs
    Co-authored-by: Jan Kotas <jkotas@microsoft.com>

--- src/libraries/System.Private.CoreLib/src/System/Threading/ThreadPool.NonCoreCLR.cs
+++ src/libraries/System.Private.CoreLib/src/System/Threading/ThreadPool.NonCoreCLR.cs
@@ -13,15 +13,5 @@ namespace System.Threading
     public static partial class ThreadPool
     {
         private static bool EnsureConfigInitializedCore() => true;
-
-#pragma warning disable IDE0060
-        internal static bool CanSetMinIOCompletionThreads(int ioCompletionThreads) => false;
-        internal static bool CanSetMaxIOCompletionThreads(int ioCompletionThreads) => false;
-#pragma warning restore IDE0060
-
-        [Conditional("unnecessary")]
-        internal static void SetMinIOCompletionThreads(int ioCompletionThreads) { }
-        [Conditional("unnecessary")]
-        internal static void SetMaxIOCompletionThreads(int ioCompletionThreads) { }
     }
 }
