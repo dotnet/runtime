a54d68076c49421b058fdc117165b890d7250030
Jan Dupej <jandupej@microsoft.com>
2023-06-01 16:17:14 +0200

    Restoring minimal functionality to marshal-ilgen-stub.

--- src/mono/mono/component/marshal-ilgen-stub.c
+++ src/mono/mono/component/marshal-ilgen-stub.c
@@ -14,6 +14,37 @@ stub_emit_marshal_ilgen (EmitMarshalContext *m, int argnum, MonoType *t,
 		MonoMarshalSpec *spec, int conv_arg,	
 		MonoType **conv_arg_type, MarshalAction action, MonoMarshalLightweightCallbacks* lightweight_cb)
 {
+	if (spec && spec->native == MONO_NATIVE_CUSTOM)
+		return conv_arg;
+
+	if (spec && spec->native == MONO_NATIVE_ASANY)
+		return conv_arg;
+
+	switch (t->type) {
+	case MONO_TYPE_BOOLEAN:
+		return lightweight_cb->emit_marshal_scalar (m, argnum, t, spec, conv_arg, conv_arg_type, action);
+	case MONO_TYPE_PTR:
+		return lightweight_cb->emit_marshal_scalar (m, argnum, t, spec, conv_arg, conv_arg_type, action);
+	case MONO_TYPE_CHAR:
+		return lightweight_cb->emit_marshal_scalar (m, argnum, t, spec, conv_arg, conv_arg_type, action);
+	case MONO_TYPE_I1:
+	case MONO_TYPE_U1:
+	case MONO_TYPE_I2:
+	case MONO_TYPE_U2:
+	case MONO_TYPE_I4:
+	case MONO_TYPE_U4:
+	case MONO_TYPE_I:
+	case MONO_TYPE_U:
+	case MONO_TYPE_R4:
+	case MONO_TYPE_R8:
+	case MONO_TYPE_I8:
+	case MONO_TYPE_U8:
+	case MONO_TYPE_FNPTR:
+		return lightweight_cb->emit_marshal_scalar (m, argnum, t, spec, conv_arg, conv_arg_type, action);
+	default:
+		return conv_arg;
+	}
+
 	return 0;
 }
 
