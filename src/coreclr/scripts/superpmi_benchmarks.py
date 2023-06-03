a7b019a6e9b5c296fb84e46f09e006f405d5360e
Kunal Pathak <Kunal.Pathak@microsoft.com>
2023-06-03 08:41:10 -0700

    fix physics engine benchmarks:

--- src/coreclr/scripts/superpmi_benchmarks.py
+++ src/coreclr/scripts/superpmi_benchmarks.py
@@ -212,6 +212,14 @@ def build_and_run(coreclr_args, output_mch_name):
                             f"--partition-index {partition_index} --envVars DOTNET_JitName:{shim_name} " \
                             " DOTNET_ZapDisable:1  DOTNET_ReadyToRun:0 " \
                             "--iterationCount 1 --warmupCount 0 --invocationCount 1 --unrollFactor 1 --strategy ColdStart --logBuildOutput"
+    elif benchmark_binary.lower().startswith("demobenchmarks"):
+        # Disable ReadyToRun so we always JIT R2R methods and collect them
+        collection_command = f"{dotnet_exe} {benchmarks_dll}  -f *CollisionBatcherTaskBenchmarks.* *ConvexCollisionTesterBenchmarks.* *GatherScatterBenchmarks.* " \
+                            " *OneBodyConstraintBenchmarks.* *OneBodyConstraintBenchmarksDeep.* *TwoBodyConstraintBenchmarks.* *TwoBodyConstraintBenchmarksDeep.* " \
+                            " *ThreeBodyConstraintBenchmarks.* *FourBodyConstraintBenchmarks.* *SweepBenchmarks.* *SweepBenchmarksDeep.* *ShapeRayBenchmarksDeep.* " \
+                            f" *ShapePileBenchmark.* *RagdollTubeBenchmark.* --corerun {os.path.join(core_root, corerun_exe)} --envVars DOTNET_JitName:{shim_name} " \
+                            " DOTNET_ZapDisable:1  DOTNET_ReadyToRun:0 " \
+                            "--iterationCount 1 --warmupCount 0 --invocationCount 1 --unrollFactor 1 --strategy ColdStart --logBuildOutput"
     else:
         # Disable ReadyToRun so we always JIT R2R methods and collect them
         collection_command = f"{dotnet_exe} {benchmarks_dll}  --filter \"*\" --corerun {os.path.join(core_root, corerun_exe)} --envVars DOTNET_JitName:{shim_name} " \
