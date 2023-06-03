47328d3c1fb7203af6af8a9ea2742d4a6f50d7e7
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2023-06-03 19:46:36 +0100

    Add ShutdownDelay to refs

--- src/libraries/Microsoft.Extensions.Hosting/ref/Microsoft.Extensions.Hosting.cs
+++ src/libraries/Microsoft.Extensions.Hosting/ref/Microsoft.Extensions.Hosting.cs
@@ -109,6 +109,7 @@ namespace Microsoft.Extensions.Hosting
         public bool ServicesStopConcurrently { get { throw null; } set { } }
         public System.TimeSpan ShutdownTimeout { get { throw null; } set { } }
         public System.TimeSpan StartupTimeout { get { throw null; } set { } }
+        public System.TimeSpan? ShutdownDelay { get { throw null; } set { } }
     }
 }
 namespace Microsoft.Extensions.Hosting.Internal
