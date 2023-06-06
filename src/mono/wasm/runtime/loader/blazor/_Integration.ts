0f4539542aa1ae31a4b9d746f016929446862e21
Marek Fišera <mara@neptuo.com>
2023-06-06 11:53:28 +0200

    Fix culture on v8/node

--- src/mono/wasm/runtime/loader/blazor/_Integration.ts
+++ src/mono/wasm/runtime/loader/blazor/_Integration.ts
@@ -149,7 +149,7 @@ export function mapBootConfigToMonoConfig(moduleConfig: MonoConfigInternal, appl
             assets.push(asset);
         }
     }
-    const applicationCulture = resourceLoader.startOptions.applicationCulture || (globalThis.navigator && navigator.languages && navigator.languages[0]);
+    const applicationCulture = resourceLoader.startOptions.applicationCulture || ENVIRONMENT_IS_WEB ? (navigator.languages && navigator.languages[0]) : Intl.DateTimeFormat().resolvedOptions().locale;
     const icuDataResourceName = getICUResourceName(resourceLoader.bootConfig, applicationCulture);
     let hasIcuData = false;
     for (const name in resources.runtime) {
