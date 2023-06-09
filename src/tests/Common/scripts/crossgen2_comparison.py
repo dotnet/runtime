edaeb52cf1506c486559016dcbb838162e1f3ebf
David Wrighton <davidwr@microsoft.com>
2023-06-09 09:25:14 -0700

    Update crossgen2_comparison.py
    10 was too big a number for the first runs through this.

--- src/tests/Common/scripts/crossgen2_comparison.py
+++ src/tests/Common/scripts/crossgen2_comparison.py
@@ -529,7 +529,7 @@ class CrossGenRunner:
         args.append('"' + platform_assemblies_paths + self.platform_directory_sep + '*.dll"' )
         args.append('-O')
         args.append('--determinism-stress')
-        args.append('10')
+        args.append('3')
         args.append('--out')
         args.append(ni_filename)
         args.append('--targetos ')
