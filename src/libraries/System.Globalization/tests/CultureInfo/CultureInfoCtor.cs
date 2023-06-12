c8a5f9ffa99c07a75ae35359e1183b414f995223
Tarek Mahmoud Sayed <tarekms@microsoft.com>
2023-06-12 09:39:36 -0700

    Fix neutral cultures created with the underscore

--- src/libraries/System.Globalization/tests/CultureInfo/CultureInfoCtor.cs
+++ src/libraries/System.Globalization/tests/CultureInfo/CultureInfoCtor.cs
@@ -463,6 +463,8 @@ namespace System.Globalization.Tests
         [InlineData("xx-u-XX-u-yy", "xx-u-xx-u-yy")]
         [InlineData("xx-t-ja-JP", "xx-t-ja-jp")]
         [InlineData("qps-plocm", "qps-PLOCM")] // ICU normalize this name to "qps--plocm" which we normalize it back to "qps-plocm"
+        [InlineData("zh_CN", "zh_cn")]
+        [InlineData("km_KH", "km_kh")]
         [ConditionalTheory(typeof(PlatformDetection), nameof(PlatformDetection.IsIcuGlobalization))]
         public void TestCreationWithICUNormalizedNames(string cultureName, string expectedCultureName)
         {
