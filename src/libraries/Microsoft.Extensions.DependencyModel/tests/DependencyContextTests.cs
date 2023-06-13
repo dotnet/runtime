dotnet/runtime

commit 5e67657e20665c32c2bd5c4ac1c8b1af78c9677e

Fix warnings found with CA1861 Avoid constant arrays as arguments (#86229)
* Fix warnings found with CA1861

* Update src/coreclr/tools/aot/ILCompiler/Program.cs

Co-authored-by: Stephen Toub <stoub@microsoft.com>

* Remove supression from static field

* Revert using string.Split(char)

* Update src/libraries/System.Private.Xml/src/System/Xml/Serialization/XmlSerializationILGen.cs

---------

Co-authored-by: Stephen Toub <stoub@microsoft.com>
Co-authored-by: Dan Moseley <danmose@microsoft.com>

src/libraries/Microsoft.Extensions.DependencyModel/src/DependencyContextPaths.cs
@@ -20,6 +20,8 @@ namespace Microsoft.Extensions.DependencyModel
 
         public IEnumerable<string> NonApplicationPaths { get; }
 
+        private static readonly char[] s_semicolon = new[] { ';' };
+
         public DependencyContextPaths(
             string? application,
             string? sharedRuntime,
@@ -40,7 +42,7 @@ namespace Microsoft.Extensions.DependencyModel
 
         internal static DependencyContextPaths Create(string? depsFiles, string? sharedRuntime)
         {
-            string[]? files = depsFiles?.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+            string[]? files = depsFiles?.Split(s_semicolon, StringSplitOptions.RemoveEmptyEntries);
             string? application = files != null && files.Length > 0 ? files[0] : null;
 
             string[]? nonApplicationPaths = files?

eng/CodeAnalysis.src.globalconfig
@@ -471,6 +471,9 @@ dotnet_diagnostic.CA1859.severity = warning
 # CA1860: Avoid using 'Enumerable.Any()' extension method
 dotnet_diagnostic.CA1860.severity = warning
 
+# CA1861: Avoid constant arrays as arguments
+dotnet_diagnostic.CA1861.severity = warning
+
 # CA2000: Dispose objects before losing scope
 dotnet_diagnostic.CA2000.severity = none
 

eng/CodeAnalysis.test.globalconfig
@@ -468,6 +468,9 @@ dotnet_diagnostic.CA1859.severity = none
 # CA1860: Avoid using 'Enumerable.Any()' extension method
 dotnet_diagnostic.CA1860.severity = none
 
+# CA1861: Avoid constant arrays as arguments
+dotnet_diagnostic.CA1861.severity = none
+
 # CA2000: Dispose objects before losing scope
 dotnet_diagnostic.CA2000.severity = none
 

src/coreclr/tools/aot/ILCompiler/ILCompilerRootCommand.cs
@@ -267,9 +267,11 @@ namespace ILCompiler
                         // + the original command line arguments
                         // + a rsp file that should work to directly run out of the zip file
 
+#pragma warning disable CA1861 // Avoid constant arrays as arguments. Only executed once during the execution of the program.
                         Helpers.MakeReproPackage(makeReproPath, context.ParseResult.GetValue(OutputFilePath), args, context.ParseResult,
                             inputOptions : new[] { "r", "reference", "m", "mibc", "rdxml", "directpinvokelist", "descriptor" },
                             outputOptions : new[] { "o", "out", "exportsfile" });
+#pragma warning restore CA1861 // Avoid constant arrays as arguments
                     }
 
                     context.ExitCode = new Program(this).Run();

src/coreclr/tools/aot/ILCompiler/Program.cs
@@ -29,6 +29,7 @@ namespace ILCompiler
     internal sealed class Program
     {
         private readonly ILCompilerRootCommand _command;
+        private static readonly char[] s_separator = new char[] { ',', ';', ' ' };
 
         public Program(ILCompilerRootCommand command)
         {
@@ -682,7 +683,7 @@ namespace ILCompiler
         {
             foreach (string value in warningCodes)
             {
-                string[] values = value.Split(new char[] { ',', ';', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                string[] values = value.Split(s_separator, StringSplitOptions.RemoveEmptyEntries);
                 foreach (string id in values)
                 {
                     if (!id.StartsWith("IL", StringComparison.Ordinal) || !ushort.TryParse(id.AsSpan(2), out ushort code))

src/libraries/Microsoft.CSharp/src/Microsoft/CSharp/RuntimeBinder/ComInterop/IDispatchMetaObject.cs
@@ -11,6 +11,7 @@ namespace Microsoft.CSharp.RuntimeBinder.ComInterop
     internal sealed class IDispatchMetaObject : ComFallbackMetaObject
     {
         private readonly IDispatchComObject _self;
+        private static readonly bool[] s_false = new bool[] { false };
 
         [RequiresUnreferencedCode(Binder.TrimmerWarning)]
         internal IDispatchMetaObject(Expression expression, IDispatchComObject self)
@@ -218,7 +219,7 @@ namespace Microsoft.CSharp.RuntimeBinder.ComInterop
                 DynamicMetaObject result = new ComInvokeBinder(
                     new CallInfo(1),
                     new[] { value },
-                    new bool[] { false },
+                    s_false,
                     restrictions,
                     Expression.Constant(method),
                     dispatch,

src/libraries/Microsoft.CSharp/src/Microsoft/CSharp/RuntimeBinder/ComInterop/VariantArray.cs
@@ -49,6 +49,7 @@ namespace Microsoft.CSharp.RuntimeBinder.ComInterop
         // Don't need a dictionary for this, it will have very few elements
         // (guaranteed less than 28, in practice 0-2)
         private static readonly List<Type> s_generatedTypes = new List<Type>(0);
+        private static readonly string[] s_genericTName = new string[] { "T" };
 
         [DynamicDependency(DynamicallyAccessedMemberTypes.PublicFields, typeof(VariantArray1))]
         [DynamicDependency(DynamicallyAccessedMemberTypes.PublicFields, typeof(VariantArray2))]
@@ -100,7 +101,7 @@ namespace Microsoft.CSharp.RuntimeBinder.ComInterop
         {
             TypeAttributes attrs = TypeAttributes.NotPublic | TypeAttributes.SequentialLayout;
             TypeBuilder type = UnsafeMethods.DynamicModule.DefineType("VariantArray" + size, attrs, typeof(ValueType));
-            GenericTypeParameterBuilder T = type.DefineGenericParameters(new string[] { "T" })[0];
+            GenericTypeParameterBuilder T = type.DefineGenericParameters(s_genericTName)[0];
             for (int i = 0; i < size; i++)
             {
                 type.DefineField("Element" + i, T, FieldAttributes.Public);

src/libraries/Microsoft.CSharp/src/Microsoft/CSharp/RuntimeBinder/Semantics/ExpressionBinder.cs
@@ -1151,7 +1151,7 @@ namespace Microsoft.CSharp.RuntimeBinder.Semantics
                             CType arrayType = (ArrayType)TypeManager.SubstType(mp.Params[mp.Params.Count - 1], type, pTypeArgs);
 
                             // Use an EK_ARRINIT even in the empty case so empty param arrays in attributes work.
-                            ExprArrayInit arrayInit = ExprFactory.CreateArrayInit(arrayType, null, null, new[] { 0 });
+                            ExprArrayInit arrayInit = ExprFactory.CreateArrayInit(arrayType, null, null, s_zero);
                             arrayInit.GeneratedForParamArray = true;
                             arrayInit.OptionalArguments = named.Value;
 
@@ -1229,7 +1229,7 @@ namespace Microsoft.CSharp.RuntimeBinder.Semantics
             CType elementType = subArr.ElementType;
 
             // Use an EK_ARRINIT even in the empty case so empty param arrays in attributes work.
-            ExprArrayInit exprArrayInit = ExprFactory.CreateArrayInit(substitutedArrayType, null, null, new[] { 0 });
+            ExprArrayInit exprArrayInit = ExprFactory.CreateArrayInit(substitutedArrayType, null, null, s_zero);
             exprArrayInit.GeneratedForParamArray = true;
 
             if (it.AtEnd())
@@ -1617,6 +1617,8 @@ namespace Microsoft.CSharp.RuntimeBinder.Semantics
         }
 
         private AggregateSymbol ContextForMemberLookup => Context.ContextForMemberLookup;
+
+        private static readonly int[] s_zero = new[] { 0 };
 
         private static ExprWrap WrapShortLivedExpression(Expr expr) => ExprFactory.CreateWrap(expr);
 

commit 476cfea78571fe99356d429c2661c0481808db5f

Fix DependencyContext splitting on semi-colon
https://github.com/dotnet/runtime/commit/5e67657e20665c32c2bd5c4ac1c8b1af78c9677e introduced a bug in DependencyContextPaths where the static array is not initialized before it is being used in the Create static method.

This fix reorders the fields so that s_semicolon is initialized before it is first used.

src/libraries/Microsoft.Extensions.DependencyModel/src/DependencyContextPaths.cs
@@ -12,6 +12,8 @@ namespace Microsoft.Extensions.DependencyModel
         private const string DepsFilesProperty = "APP_CONTEXT_DEPS_FILES";
         private const string FxDepsFileProperty = "FX_DEPS_FILE";
 
+        private static readonly char[] s_semicolon = new[] { ';' };
+
         public static DependencyContextPaths Current { get; } = GetCurrent();
 
         public string? Application { get; }
@@ -19,8 +21,6 @@ namespace Microsoft.Extensions.DependencyModel
         public string? SharedRuntime { get; }
 
         public IEnumerable<string> NonApplicationPaths { get; }
-
-        private static readonly char[] s_semicolon = new[] { ';' };
 
         public DependencyContextPaths(
             string? application,

src/libraries/Microsoft.Extensions.DependencyModel/tests/DependencyContextTests.cs
@@ -276,6 +276,18 @@ namespace Microsoft.Extensions.DependencyModel.Tests
                 Subject.Fallbacks.Should().BeEquivalentTo("win7-x64", "win7-x86");
         }
 
+        [Fact]
+        public void DefaultWorksCorrectly()
+        {
+            // only need to assert the context contains non-null properties.
+
+            var context = DependencyContext.Default;
+            Assert.NotNull(context);
+            Assert.NotNull(context.RuntimeGraph);
+            Assert.NotNull(context.RuntimeLibraries);
+            Assert.NotNull(context.Target);
+        }
+
         private TargetInfo CreateTargetInfo()
         {
             return new TargetInfo(