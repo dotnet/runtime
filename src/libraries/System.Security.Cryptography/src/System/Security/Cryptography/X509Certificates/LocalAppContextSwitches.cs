cb4c902c01972e706e87280de7ea958e1fd18183
David Cantu <dacantu@microsoft.com>
2023-06-13 14:08:22 -0500

    Specify InvariantCulture on Convert.ToInt64

--- src/libraries/System.Security.Cryptography/src/System/Security/Cryptography/X509Certificates/LocalAppContextSwitches.cs
+++ src/libraries/System.Security.Cryptography/src/System/Security/Cryptography/X509Certificates/LocalAppContextSwitches.cs
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Globalization;
+
 namespace System
 {
     internal static partial class LocalAppContextSwitches
@@ -20,7 +22,7 @@ namespace System
 
             try
             {
-                return Convert.ToInt64(data);
+                return Convert.ToInt64(data, CultureInfo.InvariantCulture);
             }
             catch
             {
