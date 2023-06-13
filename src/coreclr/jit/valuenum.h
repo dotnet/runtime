a052348f9fd28d2066636444d6a93f1df6adf8d9
Jasper <jasper-d@users.noreply.github.com>
2023-06-13 13:53:59 +0200

    Fold const WithElement to CNS_VEC (#86212)
    Co-authored-by: Egor Bogatov <egorbo@gmail.com>

--- src/coreclr/jit/valuenum.h
+++ src/coreclr/jit/valuenum.h
@@ -1184,6 +1184,16 @@ public:
                                       bool           encodeResultType,
                                       ValueNum       resultTypeVN);
 
+    ValueNum EvalHWIntrinsicFunTernary(var_types      type,
+                                       var_types      baseType,
+                                       NamedIntrinsic ni,
+                                       VNFunc         func,
+                                       ValueNum       arg0VN,
+                                       ValueNum       arg1VN,
+                                       ValueNum       arg2VN,
+                                       bool           encodeResultType,
+                                       ValueNum       resultTypeVN);
+
     // Returns "true" iff "vn" represents a function application.
     bool IsVNFunc(ValueNum vn);
 
