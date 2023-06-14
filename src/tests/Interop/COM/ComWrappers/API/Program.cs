c5e98856649b5798af2039fd6a93c5458c15cc23
Austin Wise <AustinWise@gmail.com>
2023-06-14 09:40:51 -0700

    Also apply the feedback to the correct unit test.
    This code was copy-pasted.

--- src/tests/Interop/COM/ComWrappers/API/Program.cs
+++ src/tests/Interop/COM/ComWrappers/API/Program.cs
@@ -375,12 +375,12 @@ namespace ComWrappersTests
                 Assert.NotEqual(IntPtr.Zero, nativeInstance);
 
                 var iid = typeof(ITest).GUID;
-                IntPtr itestPtr;
+                nint itestPtr;
                 Assert.Equal(0, Marshal.QueryInterface(nativeInstance, ref iid, out itestPtr));
 
-                var inst = Marshal.PtrToStructure<VtblPtr>(itestPtr);
-                var vtbl = Marshal.PtrToStructure<ITestVtbl>(inst.Vtbl);
-                var setValue = (delegate* unmanaged<IntPtr, int, int>)vtbl.SetValue;
+                var inst = (ComWrappers.ComInterfaceDispatch*)itestPtr;
+                var vtbl = (ITestVtbl*)(inst->Vtable);
+                var setValue = (delegate* unmanaged<nint, int, int>)vtbl->SetValue;
 
                 Assert.Equal(0, setValue(itestPtr, 42));
                 Assert.Equal(42, Test.Resurrected.GetValue());
@@ -388,7 +388,7 @@ namespace ComWrappersTests
                 // release for QueryInterface
                 Assert.Equal(1, Marshal.Release(itestPtr));
                 // release for GetOrCreateComInterfaceForObject
-                Assert.Equal(0, Marshal.Release(itestPtr));
+                Assert.Equal(0, Marshal.Release(nativeInstance));
             }
         }
 
