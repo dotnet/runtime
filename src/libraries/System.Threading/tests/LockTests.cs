35dbebc70df996f93da837e01d5c73fa766636c0
Koundinya Veluri <kouvel@microsoft.com>
2023-06-16 10:48:46 -0700

    Fix tests

--- src/libraries/System.Threading/tests/LockTests.cs
+++ src/libraries/System.Threading/tests/LockTests.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Collections.Generic;
-using System.Runtime.Serialization.Formatters.Tests;
 using System.Threading.Tasks;
 using Xunit;
 
