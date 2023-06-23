e2310ca2709291cb2266efe023e2f937db115025
Lakshan Fernando <lakshanf@hotmail.com>
2023-06-23 09:59:53 -0700

    Fix typo in OSX environ def

--- src/coreclr/nativeaot/Runtime/eventpipe/ep-rt-aot.cpp
+++ src/coreclr/nativeaot/Runtime/eventpipe/ep-rt-aot.cpp
@@ -673,7 +673,7 @@ bool ep_rt_aot_spin_lock_release (ep_rt_spin_lock_handle_t *spin_lock)
 #if defined(__APPLE__)
 #if defined (HOST_OSX)
 extern "C" {
-gchar ***_NSGetEnviron(void);
+char ***_NSGetEnviron(void);
 }
 #define environ (*_NSGetEnviron())
 #else
