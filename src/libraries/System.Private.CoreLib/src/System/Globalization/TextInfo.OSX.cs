8c2efb4754ee91816b3e6a4f58c87752273445b6
Meri Khamoyan <merikhamoyan@microsoft.com>
2023-06-23 10:30:06 +0200

    Order alphabetically function declarations

--- src/libraries/System.Private.CoreLib/src/System/Globalization/TextInfo.OSX.cs
+++ src/libraries/System.Private.CoreLib/src/System/Globalization/TextInfo.OSX.cs
@@ -1,14 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Buffers;
 using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Runtime.Serialization;
-using System.Text;
-using System.Text.Unicode;
 
 namespace System.Globalization
 {
