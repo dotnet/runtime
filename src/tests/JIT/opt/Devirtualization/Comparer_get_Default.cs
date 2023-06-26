f8249c781c2bb0315f13eea8878233c68930b639
Michał Petryka <35800402+MichalPetryka@users.noreply.github.com>
2023-06-26 19:38:19 +0200

    Update Comparer_get_Default.cs

--- src/tests/JIT/opt/Devirtualization/Comparer_get_Default.cs
+++ src/tests/JIT/opt/Devirtualization/Comparer_get_Default.cs
@@ -121,16 +121,16 @@ public class Program
         AssertThrows<ArgumentException>(() => Comparer<Struct2>.Default.Compare(a, b));
 
     private static void Compare_Struct1_Nullable(Struct1? a, Struct1? b) =>
-        AssertEquals(((IComparable)a).CompareTo(b), Comparer<Struct1?>.Default.Compare(a, b));
+        AssertEquals(((IComparable)a)?.CompareTo(b) ?? (b.IsNull ? 0 : -1), Comparer<Struct1?>.Default.Compare(a, b));
 
     private static void Compare_Struct2_Nullable(Struct2? a, Struct2? b)
     {
         if (a.HasValue && b.HasValue)
             AssertEquals(0, Comparer<Struct2?>.Default.Compare(a, b));
         else if (a.HasValue)
-            AssertEquals(-1, Comparer<Struct2?>.Default.Compare(a, b));
-        else if (b.HasValue)
             AssertEquals(1, Comparer<Struct2?>.Default.Compare(a, b));
+        else if (b.HasValue)
+            AssertEquals(-1, Comparer<Struct2?>.Default.Compare(a, b));
         else
             AssertThrows<ArgumentException>(() => Comparer<Struct2?>.Default.Compare(a, b));
     }
