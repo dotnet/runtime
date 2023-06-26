71795cb924f00a435a7482380229058c64e673ba
EgorBo <egorbo@gmail.com>
2023-06-26 19:00:28 +0200

    Address feedback

--- src/coreclr/tools/Common/JitInterface/ThunkGenerator/newguid.bat
+++
@@ -1,3 +0,0 @@
-pushd %~dp0
-call ..\..\..\..\..\..\dotnet.cmd run -- NewJITEEVersion ..\..\..\..\inc\jiteeversionguid.h
-popd
--- src/coreclr/tools/Common/JitInterface/ThunkGenerator/newguid.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-cd "$(dirname ${BASH_SOURCE[0]})"
-../../../../../../dotnet.sh run -- NewJITEEVersion ../../../../inc/jiteeversionguid.h
