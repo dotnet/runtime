b7e148439e6682c7b4b9ad1415bed54eefcff0ec
vitek-karas <10670590+vitek-karas@users.noreply.github.com>
2023-06-26 02:39:04 -0700

    Revert to method group marking
    Parameter overload resolution is very problematic in Cecil - would have to reimplement it effectively.

--- src/tools/illink/src/linker/Linker.Steps/UnsafeAccessorMarker.cs
+++ src/tools/illink/src/linker/Linker.Steps/UnsafeAccessorMarker.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Runtime.CompilerServices;
-using ILLink.Shared.TypeSystemProxy;
 using Mono.Cecil;
 
 namespace Mono.Linker.Steps
@@ -11,23 +10,28 @@ namespace Mono.Linker.Steps
 	// so there's no problem with forgetting the implicit "this".
 #pragma warning disable RS0030 // MethodReference.Parameters is banned
 
-	internal struct UnsafeAccessorMarker (LinkContext context, MarkStep markStep)
+	readonly struct UnsafeAccessorMarker (LinkContext context, MarkStep markStep)
 	{
 		readonly LinkContext _context = context;
 		readonly MarkStep _markStep = markStep;
+
+		// We don't perform method overload resolution based on list of parameters (or return type) for now
+		// Mono.Cecil's method resolution is problematic and has bugs. It's also not extensible
+		//   and we would need that to correctly implement the desired behavior around custom modifiers. So for now we decided to not
+		//   duplicate the logic to tweak it and will just mark entire method groups.
 
 		public void ProcessUnsafeAccessorMethod (MethodDefinition method)
 		{
 			if (!method.IsStatic || !method.HasCustomAttributes)
 				return;
 
-			foreach (var customAttribute in method.CustomAttributes) {
+			foreach (CustomAttribute customAttribute in method.CustomAttributes) {
 				if (customAttribute.Constructor.DeclaringType.FullName == "System.Runtime.CompilerServices.UnsafeAccessorAttribute") {
 					if (customAttribute.HasConstructorArguments && customAttribute.ConstructorArguments[0].Value is int kindValue) {
 						UnsafeAccessorKind kind = (UnsafeAccessorKind) kindValue;
 						string? name = null;
 						if (customAttribute.HasProperties) {
-							foreach (var prop in customAttribute.Properties) {
+							foreach (CustomAttributeNamedArgument prop in customAttribute.Properties) {
 								if (prop.Name == "Name") {
 									name = prop.Argument.Value as string;
 									break;
@@ -61,23 +65,16 @@ namespace Mono.Linker.Steps
 		{
 			// A return type is required for a constructor, otherwise
 			// we don't know the type to construct.
-			// Types should not be parameterized (that is, byref).
+			// Types should not be parameterized (that is, by-ref).
 			// The name is defined by the runtime and should be empty.
 			if (method.ReturnsVoid () || method.ReturnType.IsByRefOrPointer () || !string.IsNullOrEmpty (name))
 				return;
 
-			// Construct a method reference with the .ctor signature
-			MethodReference targetMethodReference = new MethodReference (".ctor", BCL.FindPredefinedType (WellKnownType.System_Void, _context), method.ReturnType) {
-				CallingConvention = method.CallingConvention
-			};
+			if (_context.TryResolve (method.ReturnType) is not TypeDefinition targetType)
+				return;
 
-			foreach (ParameterDefinition? parameter in method.Parameters) {
-				targetMethodReference.Parameters.Add (new ParameterDefinition (parameter.Name, parameter.Attributes, parameter.ParameterType));
-			}
-
-			MethodDefinition? targetMethodDefinition = ResolveMethodReference (targetMethodReference, isStatic: false);
-			if (targetMethodDefinition != null)
-				_markStep.MarkMethodVisibleToReflection (targetMethodDefinition, new DependencyInfo (DependencyKind.UnsafeAccessorTarget, method), new MessageOrigin (method));
+			foreach (MethodDefinition ctor in targetType.GetConstructorsOnType (null, bindingFlags: null))
+				_markStep.MarkMethodVisibleToReflection (ctor, new DependencyInfo (DependencyKind.UnsafeAccessorTarget, method), new MessageOrigin (method));
 		}
 
 		void ProcessStaticMethodAccessor (MethodDefinition method, string? name)
@@ -90,26 +87,14 @@ namespace Mono.Linker.Steps
 				name = method.Name;
 
 			// TODO - struct values should be by-ref, we probably need to unwrap/resolve it here
-			TypeReference targetType = method.Parameters[0].ParameterType;
+			if (_context.TryResolve (method.Parameters[0].ParameterType) is not TypeDefinition targetType)
+				return;
 
-			MethodReference targetMethodReference = new MethodReference (name, method.ReturnType, targetType) {
-				CallingConvention = method.CallingConvention
-			};
+			foreach (MethodDefinition targetMethod in targetType.GetMethodsOnTypeHierarchy (_context, MethodFilter, bindingFlags: null))
+				_markStep.MarkMethodVisibleToReflection (targetMethod, new DependencyInfo (DependencyKind.UnsafeAccessorTarget, method), new MessageOrigin (method));
 
-			bool first = true;
-			foreach (ParameterDefinition? parameter in method.Parameters) {
-				// Skip the first parameter which is the target type
-				if (first) {
-					first = false;
-					continue;
-				}
-
-				targetMethodReference.Parameters.Add (new ParameterDefinition (parameter.Name, parameter.Attributes, parameter.ParameterType));
-			}
-
-			MethodDefinition? targetMethodDefinition = ResolveMethodReference (targetMethodReference, isStatic: true);
-			if (targetMethodDefinition != null)
-				_markStep.MarkMethodVisibleToReflection (targetMethodDefinition, new DependencyInfo (DependencyKind.UnsafeAccessorTarget, method), new MessageOrigin (method));
+			bool MethodFilter (MethodDefinition m)
+				=> m.Name == name && m.IsStatic;
 		}
 
 		void ProcessInstanceMethodAccessor (MethodDefinition method, string? name)
@@ -122,42 +107,14 @@ namespace Mono.Linker.Steps
 				name = method.Name;
 
 			// TODO - struct values should be by-ref, we probably need to unwrap/resolve it here
-			TypeReference targetType = method.Parameters[0].ParameterType;
+			if (_context.TryResolve (method.Parameters[0].ParameterType) is not TypeDefinition targetType)
+				return;
 
-			MethodReference targetMethodReference = new MethodReference (name, method.ReturnType, targetType) {
-				CallingConvention = method.CallingConvention
-			};
+			foreach (MethodDefinition targetMethod in targetType.GetMethodsOnTypeHierarchy (_context, MethodFilter, bindingFlags: null))
+				_markStep.MarkMethodVisibleToReflection (targetMethod, new DependencyInfo (DependencyKind.UnsafeAccessorTarget, method), new MessageOrigin (method));
 
-			bool first = true;
-			foreach (ParameterDefinition? parameter in method.Parameters) {
-				// Skip the first parameter which is the target type
-				if (first) {
-					first = false;
-					continue;
-				}
-
-				targetMethodReference.Parameters.Add (new ParameterDefinition (parameter.Name, parameter.Attributes, parameter.ParameterType));
-			}
-
-			MethodDefinition? targetMethodDefinition = ResolveMethodReference (targetMethodReference, isStatic: false);
-			if (targetMethodDefinition != null)
-				_markStep.MarkMethodVisibleToReflection (targetMethodDefinition, new DependencyInfo (DependencyKind.UnsafeAccessorTarget, method), new MessageOrigin (method));
-		}
-
-		static MethodDefinition? ResolveMethodReference(MethodReference reference, bool isStatic)
-		{
-			// TODO: Compare calling conventions
-			// TODO: Generics
-			// TODO: Custom modifiers
-			MethodDefinition resolvedMethod = reference.Module.MetadataResolver.Resolve (reference);
-
-			if (resolvedMethod == null)
-				return null;
-
-			if (resolvedMethod.IsStatic != isStatic)
-				return null;
-
-			return resolvedMethod;
+			bool MethodFilter (MethodDefinition m)
+				=> m.Name == name && !m.IsStatic;
 		}
 	}
 }
