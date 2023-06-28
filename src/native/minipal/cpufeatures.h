8a613e52787238583f28f62bc12e34ef3d7d39ec
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2023-06-28 16:18:03 +0900

    Arm64/non-MSVC

--- src/native/minipal/cpufeatures.h
+++ src/native/minipal/cpufeatures.h
@@ -58,6 +58,10 @@ enum ARM64IntrinsicConstants
     ARM64IntrinsicConstants_VectorT128 = 0x0200,
 };
 
+#if !defined(__cplusplus) && !defined(static_assert)
+#define static_assert _Static_assert
+#endif
+
 // Bit position for the ARM64IntrinsicConstants_Atomics flags, to be used with tbz / tbnz instructions
 #define ARM64_ATOMICS_FEATURE_FLAG_BIT 7
 static_assert((1 << ARM64_ATOMICS_FEATURE_FLAG_BIT) == ARM64IntrinsicConstants_Atomics, "ARM64_ATOMICS_FEATURE_FLAG_BIT must match with ARM64IntrinsicConstants_Atomics");
