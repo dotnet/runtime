670b5138c6179091b68d8152ca9c2e86278db056
pavelsavara <pavel.savara@gmail.com>
2023-06-28 13:39:15 +0200

    feedback

--- src/mono/wasm/Wasm.Build.Tests/InvariantTimezoneTests.cs
+++ src/mono/wasm/Wasm.Build.Tests/InvariantTimezoneTests.cs
@@ -52,34 +52,10 @@ namespace Wasm.Build.Tests
             if (dotnetWasmFromRuntimePack == null)
                 dotnetWasmFromRuntimePack = !(buildArgs.AOT || buildArgs.Config == "Release");
 
-            string programText = @"
-                using System;
-
-                // https://github.com/dotnet/runtime/blob/main/docs/design/features/timezone-invariant-mode.md
-
-                var timezonesCount = TimeZoneInfo.GetSystemTimeZones().Count;
-                Console.WriteLine($""Found {timezonesCount} timezones in the TZ database"");
-
-                TimeZoneInfo utc = TimeZoneInfo.FindSystemTimeZoneById(""UTC"");
-                Console.WriteLine($""{utc.DisplayName} BaseUtcOffset is {utc.BaseUtcOffset}"");
-
-                try
-                {
-                    TimeZoneInfo tst = TimeZoneInfo.FindSystemTimeZoneById(""Asia/Tokyo"");
-                    Console.WriteLine($""{tst.DisplayName} BaseUtcOffset is {tst.BaseUtcOffset}"");
-                }
-                catch (TimeZoneNotFoundException tznfe)
-                {
-                    Console.WriteLine($""Could not find Asia/Tokyo: {tznfe.Message}"");
-                }
-
-                return 42;
-            ";
-
             BuildProject(buildArgs,
                             id: id,
                             new BuildProjectOptions(
-                                InitProject: () => File.WriteAllText(Path.Combine(_projectDir!, "Program.cs"), programText),
+                                InitProject: () => File.Copy(Path.Combine(BuildEnvironment.TestAssetsPath, "Wasm.Buid.Tests.Programs", "InvariantTimezone.cs"), Path.Combine(_projectDir!, "Program.cs")),
                                 DotnetWasmFromRuntimePack: dotnetWasmFromRuntimePack));
 
             string output = RunAndTestWasmApp(buildArgs, expectedExitCode: 42, host: host, id: id);
