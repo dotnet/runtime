2ed3fb244d536a05a2100de449b163a00859625e
Miha Zupan <mihazupan.zupan1@gmail.com>
2023-06-30 01:49:48 +0200

    More comments

--- src/libraries/System.Private.CoreLib/src/System/SearchValues/Strings/Helpers/TeddyHelper.cs
+++ src/libraries/System.Private.CoreLib/src/System/SearchValues/Strings/Helpers/TeddyHelper.cs
@@ -262,7 +262,6 @@ namespace System.Buffers
             }
             else
             {
-                // TODO: Can we do better?
                 Vector128<byte> leftShifted = Vector128.Shuffle(left, Vector128.Create(15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0).AsByte());
                 return AdvSimd.Arm64.VectorTableLookupExtension(leftShifted, right, Vector128.Create(0xFF, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14));
             }
@@ -285,7 +284,6 @@ namespace System.Buffers
             }
             else
             {
-                // TODO: Can we do better?
                 Vector128<byte> leftShifted = Vector128.Shuffle(left, Vector128.Create(14, 15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0).AsByte());
                 return AdvSimd.Arm64.VectorTableLookupExtension(leftShifted, right, Vector128.Create(0xFF, 0xFF, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13));
             }
@@ -295,6 +293,25 @@ namespace System.Buffers
         [CompExactlyDependsOn(typeof(Avx2))]
         private static Vector256<byte> RightShift1(Vector256<byte> left, Vector256<byte> right)
         {
+            // Given input vectors like
+            // left:      0,  1,  2,  3,  4,  5, ... , 26, 27, 28, 29, 30, [31]
+            // right:    32, 33, 34, 35, 36, 37, ... , 58, 59, 60, 61, 62, 63
+            // We want to shift the last element of left (31) to be the first element of the result
+            // result: [31], 32, 33, 34, 35, 36, ... , 57, 58, 59, 60, 61, 62
+            //
+            // Avx2.AlignRight acts like two separate Ssse3.AlignRight calls on the lower and upper halves of the source operands.
+            // Result of Avx2.AlignRight(right, left, 15) is
+            // lower: [15], 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46,
+            // upper: [31], 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62
+            // note how elements at indexes 0 and 16 are off by 16 places.
+            // We want to read 31 instead of 15 and 47 instead of 31.
+            //
+            // To achieve that we create a temporary value where we combine the second half of the first operand and the first half of the second operand (Permute2x128).
+            // left:      0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, [ 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 ] control: (1 << 0)
+            // right:  [ 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47 ], 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63   control: (2 << 4)
+            // result:   16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, [31], 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, [47]
+            // This effectively shifts the 0th and 16th element by 16 places (note values 31 and 47).
+
             Vector256<byte> leftShifted = Avx2.Permute2x128(left, right, (1 << 0) + (2 << 4));
             return Avx2.AlignRight(right, leftShifted, 15);
         }
@@ -303,6 +320,7 @@ namespace System.Buffers
         [CompExactlyDependsOn(typeof(Avx2))]
         private static Vector256<byte> RightShift2(Vector256<byte> left, Vector256<byte> right)
         {
+            // See comments in 'RightShift1(Vector256<byte> left, Vector256<byte> right)' above.
             Vector256<byte> leftShifted = Avx2.Permute2x128(left, right, (1 << 0) + (2 << 4));
             return Avx2.AlignRight(right, leftShifted, 14);
         }
@@ -311,6 +329,22 @@ namespace System.Buffers
         [CompExactlyDependsOn(typeof(Avx512BW))]
         private static Vector512<byte> RightShift1(Vector512<byte> left, Vector512<byte> right)
         {
+            // Given input vectors like
+            // left:     0,   1,   2,   3,   4,   5, ... ,  58,  59,  60,  61,  62, [63]
+            // right:   64,  65,  66,  67,  68,  69, ... , 122, 123, 124, 125, 126, 127
+            // We want to shift the last element of left (63) to be the first element of the result
+            // result: [63], 64,  65,  66,  67,  68, ... , 121, 122, 123, 124, 125, 126
+            //
+            // Avx512BW.AlignRight acts like four separate Ssse3.AlignRight calls on each 128-bit pair of the of the source operands.
+            // Result of Avx512BW.AlignRight(right, left, 15) is
+            // lower: [15],  64,  65,  66,  67,  68,  69,  70,  71,  72,  73,  74,  75,  76,  77,  78, [31],  80,  81,  82,  83,  84,  85,  86,  87,  88,  89,  90,  91,  92,  93,  94,
+            // upper: [47],  96,  97,  98,  99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, [63], 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126
+            // note how elements at indexes 0, 16, 32 and 48 are off by 48 places.
+            // We want to read 63 instead of 15, 79 instead of 31, 95 instead of 47, and 111 instead of 63.
+            //
+            // Similar to Avx2 above, we create a temporary value where we shift these positions by 48 places - shift 8-byte values by 6 places (PermuteVar8x64x2).
+            // The indices vector below could be [6, 7, 8, 9, 10, 11, 12, 13], but we only care about the last byte in each 128-bit block (positions with value 0 don't affect the result).
+
             Vector512<byte> leftShifted = Avx512F.PermuteVar8x64x2(left.AsInt64(), Vector512.Create(0, 7, 0, 9, 0, 11, 0, 13), right.AsInt64()).AsByte();
             return Avx512BW.AlignRight(right, leftShifted, 15);
         }
@@ -319,6 +353,7 @@ namespace System.Buffers
         [CompExactlyDependsOn(typeof(Avx512BW))]
         private static Vector512<byte> RightShift2(Vector512<byte> left, Vector512<byte> right)
         {
+            // See comments in 'RightShift1(Vector512<byte> left, Vector512<byte> right)' above.
             Vector512<byte> leftShifted = Avx512F.PermuteVar8x64x2(left.AsInt64(), Vector512.Create(0, 7, 0, 9, 0, 11, 0, 13), right.AsInt64()).AsByte();
             return Avx512BW.AlignRight(right, leftShifted, 14);
         }
