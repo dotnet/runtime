7659053ab1738054fda41a518c0799dc0896097d
Michał Petryka <35800402+MichalPetryka@users.noreply.github.com>
2023-06-29 18:49:12 +0200

    Update transform.c

--- src/mono/mono/mini/interp/transform.c
+++ src/mono/mono/mini/interp/transform.c
@@ -2536,7 +2536,7 @@ interp_handle_intrinsics (TransformData *td, MonoMethod *target_method, MonoClas
 		MonoType *t = ctx->method_inst->type_argv [0];
 		t = mini_get_underlying_type (t);
 
-		if (t->type == MONO_TYPE_BOOLEAN || t->type == MONO_TYPE_R4 || t->type == MONO_TYPE_R8)
+		if (t->type == MONO_TYPE_R4 || t->type == MONO_TYPE_R8)
 			return FALSE;
 
 		gboolean is_i8 = (t->type == MONO_TYPE_I8 || t->type == MONO_TYPE_U8 || (TARGET_SIZEOF_VOID_P == 8 && (t->type == MONO_TYPE_I || t->type == MONO_TYPE_U)));
