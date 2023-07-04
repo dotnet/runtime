7fa5c32e9f949a55630d3893ed1335e3fa718bfe
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2023-07-04 17:25:18 +0200

    Run jit-format

--- src/coreclr/jit/ifconversion.cpp
+++ src/coreclr/jit/ifconversion.cpp
@@ -659,7 +659,7 @@ bool OptIfConversionDsc::optIfConvert()
         // Don't optimise the block if it is inside a loop. Loop-carried
         // dependencies can cause significant stalls if if-converted.
         // Detect via the block weight as that will be high when inside a loop.
-        
+
         if (m_startBlock->getBBWeight(m_comp) > BB_UNITY_WEIGHT * 1.05)
         {
             JITDUMP("Skipping if-conversion inside loop (via weight)\n");
