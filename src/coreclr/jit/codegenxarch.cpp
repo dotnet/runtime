19c474a8bd00663f34fb43d3aa76c24dbdfb0ea1
EgorBo <egorbo@gmail.com>
2023-07-04 23:22:14 +0200

    Fix asserts

--- src/coreclr/jit/codegenxarch.cpp
+++ src/coreclr/jit/codegenxarch.cpp
@@ -3272,7 +3272,7 @@ void CodeGen::genCodeForInitBlkUnroll(GenTreeBlk* node)
     // Handle the non-SIMD remainder by overlapping with previously processed data if needed
     if (size > 0)
     {
-        assert(size < REGSIZE_BYTES);
+        assert(size <= REGSIZE_BYTES);
 
         // Round down to the closest power of two (1, 2 or 4)
         regSize = 1 << BitOperations::Log2(size);
@@ -3555,7 +3555,7 @@ void CodeGen::genCodeForCpBlkUnroll(GenTreeBlk* node)
         // Handle the non-SIMD remainder by overlapping with previously processed data if needed
         if (size > 0)
         {
-            assert(size < REGSIZE_BYTES);
+            assert(size <= REGSIZE_BYTES);
 
             // Round down to the closest power of two (1, 2 or 4)
             regSize = 1 << BitOperations::Log2(size);
