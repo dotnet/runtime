443b3bb3c77df23fffe8321283f50ab323784b99
Egor Bogatov <egorbo@gmail.com>
2023-07-05 14:26:24 +0200

    Improve non-SIMD block copy/init codegen (#88393)

--- src/coreclr/jit/compiler.h
+++ src/coreclr/jit/compiler.h
@@ -8932,6 +8932,20 @@ private:
 #endif // FEATURE_SIMD
 
 public:
+    // Similar to roundUpSIMDSize, but for General Purpose Registers (GPR)
+    unsigned int roundUpGPRSize(unsigned size)
+    {
+        if (size > 4 && (REGSIZE_BYTES == 8))
+        {
+            return 8;
+        }
+        else if (size > 2)
+        {
+            return 4;
+        }
+        return size; // 2, 1, 0
+    }
+
     enum UnrollKind
     {
         Memset,
