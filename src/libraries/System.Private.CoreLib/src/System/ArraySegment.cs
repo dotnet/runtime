4e2062fd09bdd5476be3f50dc923711778b89863
Dan Moseley <danmose@microsoft.com>
2023-07-05 18:45:42 -0500

    more

--- src/libraries/System.Private.CoreLib/src/System/ArraySegment.cs
+++ src/libraries/System.Private.CoreLib/src/System/ArraySegment.cs
@@ -1,17 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-/*============================================================
-**
-**
-**
-** Purpose: Convenient wrapper for an array, an offset, and
-**          a count.  Ideally used in streams & collections.
-**          Net Classes will consume an array of these.
-**
-**
-===========================================================*/
-
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -20,11 +9,9 @@ using System.Runtime.CompilerServices;
 
 namespace System
 {
-    // Note: users should make sure they copy the fields out of an ArraySegment onto their stack
-    // then validate that the fields describe valid bounds within the array.  This must be done
-    // because assignments to value types are not atomic, and also because one thread reading
-    // three fields from an ArraySegment may not see the same ArraySegment from one call to another
-    // (ie, users could assign a new value to the old location).
+    /// <summary>
+    /// Delimits a section of a one-dimensional array.
+    /// </summary>
     [Serializable]
     [TypeForwardedFrom("mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089")]
 #pragma warning disable CA1066 // adding IEquatable<T> implementation could change semantics of code like that in xunit that queries for IEquatable vs enumerating contents
