b39151f9a2dd6f600c6b57355ca097fcac4def16
Jean-Claude Grenier <jeanclaude.grenier@unity3d.com>
2023-07-06 13:39:28 -0400

    implemented C# version of mono_class_is_valuetype

--- unity/UnityEmbedHost.Tests/BaseEmbeddingApiTests.cs
+++ unity/UnityEmbedHost.Tests/BaseEmbeddingApiTests.cs
@@ -616,6 +616,16 @@ public abstract class BaseEmbeddingApiTests
         Assert.That(actualFlat, Is.EquivalentTo(expectedFlat));
     }
 
+
+    [TestCase(typeof(Animal),        false)]
+    [TestCase(typeof(ValueMammal),    true)]
+    [TestCase(typeof(Classification), true)]
+    public void ClassIsValuetypeReturnsProperValue(Type klass, bool expectedResult)
+    {
+        bool isValueType = ClrHost.class_is_valuetype(klass);
+        Assert.That(isValueType, Is.EqualTo(expectedResult));
+    }
+
     static List<object?> FlattenedArray(Array arr)
     {
         var result = new List<object?>();
