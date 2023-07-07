3a3ab7a83c332b4d30e37817c5e75d8e4b716aa6
Marek Fišera <mara@neptuo.com>
2023-07-07 09:29:28 +0200

    Licence headers

--- src/mono/wasm/testassets/WasmBasicTestApp/wwwroot/WasmBasicTestApp.lib.module.js
+++ src/mono/wasm/testassets/WasmBasicTestApp/wwwroot/WasmBasicTestApp.lib.module.js
@@ -1,3 +1,6 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 export function onRuntimeConfigLoaded(config) {
     config.environmentVariables["LIBRARY_INITIALIZER_TEST"] = "1";
 }
