2d80415ef88c5fdccaf1582dec55cf34d162c32c
Steve Harter <steveharter@users.noreply.github.com>
2023-07-08 09:22:21 -0500

    Move startup validation to the options assembly

---
+++ src/libraries/Microsoft.Extensions.Options/src/IStartupValidator.cs
@@ -0,0 +1,14 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.Extensions.Options
+{
+    /// <summary>
+    /// Interface used by hosts to validate options during startup.
+    /// </summary>
+    /// <seealso cref="DependencyInjection.OptionsBuilderExtensions.ValidateOnStart{TOptions}(OptionsBuilder{TOptions})"/>
+    public interface IStartupValidator
+    {
+        void Validate();
+    }
+}
