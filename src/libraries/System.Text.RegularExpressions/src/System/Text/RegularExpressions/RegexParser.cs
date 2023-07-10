70bdb4f919cda4813884949b1d52b4bd54c62359
Dan Moseley <danmose@microsoft.com>
2023-07-09 22:23:03 -0500

    revert starstwith

--- src/libraries/System.Text.RegularExpressions/src/System/Text/RegularExpressions/RegexParser.cs
+++ src/libraries/System.Text.RegularExpressions/src/System/Text/RegularExpressions/RegexParser.cs
@@ -662,11 +662,20 @@ namespace System.Text.RegularExpressions
                             break; // this break will only break out of the switch
                     }
                 }
-                else if (ch == '[' && _pos < _pattern.Length && _pattern[_pos] == ':' && !inRange && !_pattern.AsSpan(_pos).StartsWith(":]".AsSpan()))
+                else if (ch == '[')
                 {
                     // This is code for Posix style properties - [:Ll:] or [:IsTibetan:].
                     // It currently doesn't do anything other than skip the whole thing!
-                    _pos++;
+                    if (_pos < _pattern.Length && _pattern[_pos] == ':' && !inRange)
+                    {
+                        int savePos = _pos;
+
+                        _pos++;
+                        if (_pos + 1 >= _pattern.Length || _pattern[_pos++] != ':' || _pattern[_pos++] != ']')
+                        {
+                            _pos = savePos;
+                        }
+                    }
                 }
 
                 if (inRange)
