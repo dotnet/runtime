0c8f4da0983bb70dddd4c1b7bc3a907f1edce4e7
Aleksey Kliger <alklig@microsoft.com>
2023-07-10 13:12:02 -0400

    [mini] compile wrapper

--- src/mono/mono/mini/mini-runtime.c
+++ src/mono/mono/mini/mini-runtime.c
@@ -2655,8 +2655,15 @@ compile_special (MonoMethod *method, MonoError *error)
 		int accessor_kind = -1;
 		if (mono_method_get_unsafe_accessor_attr_data (method, &accessor_kind, &member_name, error)) {
 			g_warning ("Method %s is an unsafe accessor with kind %d and target %s", method->name, accessor_kind, member_name);
-			g_assert_not_reached ();
-    	}
+			MonoMethod *wrapper = mono_marshal_get_unsafe_accessor_wrapper (method, (MonoUnsafeAccessorKind)accessor_kind, member_name);
+			gpointer compiled_wrapper = mono_jit_compile_method_jit_only (wrapper, error);
+			return_val_if_nok (error, NULL);
+			code = mono_get_addr_from_ftnptr (compiled_wrapper);
+			jinfo = mini_jit_info_table_find (code);
+			if (jinfo)
+				MONO_PROFILER_RAISE (jit_done, (method, jinfo));
+			return code;
+		}
 	}
 
 	return NULL;
