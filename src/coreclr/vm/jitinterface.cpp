2d957d32c1d33b8a456af470b5c95c9f0af299be
Dong-Heon Jung <clamp03@gmail.com>
2023-07-10 11:10:43 +0900

    [RISC-V] Disable Inline TLS field access
    - Disable https://github.com/dotnet/runtime/pull/87082 for RISC-V

--- src/coreclr/vm/jitinterface.cpp
+++ src/coreclr/vm/jitinterface.cpp
@@ -1587,6 +1587,8 @@ void CEEInfo::getFieldInfo (CORINFO_RESOLVED_TOKEN * pResolvedToken,
                 // Optimization is disabled for linux/x86
 #elif defined(TARGET_LINUX_MUSL) && defined(TARGET_ARM64)
                 // Optimization is disabled for linux musl arm64
+#elif defined(TARGET_RISCV64)
+                // Optimization is disabled for riscv64
 #else
                 bool optimizeThreadStaticAccess = true;
 #if !defined(TARGET_OSX) && defined(TARGET_UNIX) && defined(TARGET_AMD64)
