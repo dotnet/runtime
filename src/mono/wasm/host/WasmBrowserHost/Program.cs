54d2152c82f3a3152d709587d378edc2ac17a886
Marek Fišera <mara@neptuo.com>
2023-07-11 15:05:20 +0200

    Copy dev server from blazor

---
+++ src/mono/wasm/host/WasmBrowserHost/Program.cs
@@ -0,0 +1,46 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.ObjectPool;
+
+internal static class WasmBrowserHost
+{
+    public static void Run(string[] args)
+    {
+        var host = Host.CreateDefaultBuilder(args)
+            .ConfigureHostConfiguration(config =>
+            {
+                var applicationPath = args.SkipWhile(a => a != "--applicationpath").Skip(1).First();
+                var applicationDirectory = Path.GetDirectoryName(applicationPath)!;
+                var name = Path.ChangeExtension(applicationPath, ".staticwebassets.runtime.json");
+                name = !File.Exists(name) ? Path.ChangeExtension(applicationPath, ".StaticWebAssets.xml") : name;
+
+                var inMemoryConfiguration = new Dictionary<string, string?>
+                {
+                    [WebHostDefaults.EnvironmentKey] = "Development",
+                    ["Logging:LogLevel:Microsoft"] = "Warning",
+                    ["Logging:LogLevel:Microsoft.Hosting.Lifetime"] = "Information",
+                    [WebHostDefaults.StaticWebAssetsKey] = name,
+                    ["ApplyCopHeaders"] = args.Contains("--apply-cop-headers").ToString()
+                };
+
+                config.AddInMemoryCollection(inMemoryConfiguration);
+                config.AddJsonFile(Path.Combine(applicationDirectory, "blazor-devserversettings.json"), optional: true, reloadOnChange: true);
+            })
+            .ConfigureWebHostDefaults(app =>
+            {
+                app.UseStaticWebAssets();
+                app.UseStartup<Startup>();
+            })
+            .Build();
+
+
+        host.Run();
+    }
+}
