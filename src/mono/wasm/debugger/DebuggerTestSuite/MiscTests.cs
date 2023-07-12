a4525bacd67995ec2e591ec01232512a5cdebb1f
Ilona Tomkowicz <32700855+ilonatommy@users.noreply.github.com>
2023-07-12 15:35:43 +0000

    Unblock.

--- src/mono/wasm/debugger/DebuggerTestSuite/MiscTests.cs
+++ src/mono/wasm/debugger/DebuggerTestSuite/MiscTests.cs
@@ -1042,7 +1042,7 @@ namespace DebuggerTests
             );
         }
 
-        [ConditionalTheory(nameof(RunningOnChrome))]
+        [ConditionalTheory]
         [InlineData("ClassInheritsFromClassWithoutDebugSymbols", 1287, true)]
         [InlineData("ClassInheritsFromClassWithoutDebugSymbols", 1287, false)]
         [InlineData("ClassInheritsFromNonUserCodeClass", 1335, true)]
