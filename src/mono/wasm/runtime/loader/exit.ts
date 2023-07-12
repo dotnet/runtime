1994535baa74e50508f13ed88bb54d9098ee5989
pavelsavara <pavel.savara@gmail.com>
2023-07-12 12:29:52 +0200

    fix

--- src/mono/wasm/runtime/loader/exit.ts
+++ src/mono/wasm/runtime/loader/exit.ts
@@ -54,7 +54,7 @@ export function abort_startup(reason: any, should_exit: boolean, should_throw?: 
 // this will also call mono_wasm_exit if available, which will call exitJS -> _proc_exit -> terminateAllThreads
 export function mono_exit(exit_code: number, reason?: any): void {
     // make sure we have both reason and exit_code
-    exit_code = reason?.status || exit_code;
+    exit_code = (reason && typeof reason.status === "number") ? reason.status : exit_code;
     reason = reason || runtimeHelpers.ExitStatus
         ? new runtimeHelpers.ExitStatus(exit_code)
         : new Error("Exit with code " + exit_code);
