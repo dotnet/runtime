bf748809aa860e25b6957f860e5d2ff8ecf82e35
Miha Zupan <mihazupan.zupan1@gmail.com>
2023-07-15 01:14:20 +0200

    Add SocketsHttpHandler connection metrics (#88893)
    * Add SocketsHttpHandler connection metrics
    
    * PR feedback
    
    * Improve assert around _originAuthority
    
    * Typo

---
+++ src/libraries/System.Net.Http/src/System/Net/Http/SocketsHttpHandler/Metrics/SocketsHttpHandlerMetrics.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics.Metrics;
+
+namespace System.Net.Http.Metrics
+{
+    internal sealed class SocketsHttpHandlerMetrics(Meter meter)
+    {
+        public readonly UpDownCounter<long> CurrentConnections = meter.CreateUpDownCounter<long>(
+            name: "http-client-current-connections",
+            description: "Number of outbound HTTP connections that are currently active on the client.");
+
+        public readonly UpDownCounter<long> IdleConnections = meter.CreateUpDownCounter<long>(
+            name: "http-client-current-idle-connections",
+            description: "Number of outbound HTTP connections that are currently idle on the client.");
+
+        public readonly Histogram<double> ConnectionDuration = meter.CreateHistogram<double>(
+            name: "http-client-connection-duration",
+            unit: "s",
+            description: "The duration of outbound HTTP connections.");
+    }
+}
