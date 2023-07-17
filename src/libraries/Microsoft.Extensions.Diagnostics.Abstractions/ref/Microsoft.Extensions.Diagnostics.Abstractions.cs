919033d8c5c3cdfbd9e598a63f69d57c1b013042
Chris R <Tratcher@outlook.com>
2023-07-17 11:47:24 -0700

    Packaging

---
+++ src/libraries/Microsoft.Extensions.Diagnostics.Abstractions/ref/Microsoft.Extensions.Diagnostics.Abstractions.cs
@@ -0,0 +1,25 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// ------------------------------------------------------------------------------
+// Changes to this file must follow the https://aka.ms/api-review process.
+// ------------------------------------------------------------------------------
+
+namespace Microsoft.Extensions.Diagnostics
+{
+    /* Format example:
+    public readonly partial struct EventId : System.IEquatable<Microsoft.Extensions.Logging.EventId>
+    {
+        private readonly object _dummy;
+        private readonly int _dummyPrimitive;
+        public EventId(int id, string? name = null) { throw null; }
+        public int Id { get { throw null; } }
+        public string? Name { get { throw null; } }
+        public bool Equals(Microsoft.Extensions.Logging.EventId other) { throw null; }
+        public override bool Equals([System.Diagnostics.CodeAnalysis.NotNullWhenAttribute(true)] object? obj) { throw null; }
+        public override int GetHashCode() { throw null; }
+        public static bool operator ==(Microsoft.Extensions.Logging.EventId left, Microsoft.Extensions.Logging.EventId right) { throw null; }
+        public static implicit operator Microsoft.Extensions.Logging.EventId (int i) { throw null; }
+        public static bool operator !=(Microsoft.Extensions.Logging.EventId left, Microsoft.Extensions.Logging.EventId right) { throw null; }
+        public override string ToString() { throw null; }
+    }*/
+}
