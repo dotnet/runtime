478308ce0646e39fd51a3a5ad8e768a0920d7312
Layomi Akinrinade <laakinri@microsoft.com>
2023-07-17 20:04:10 -0700

    Share SourceWriter between JSON & config binding generators

--- src/libraries/Common/tests/TestUtilities/System/LineEndingsHelper.cs
+++ src/libraries/Common/tests/TestUtilities/System/LineEndingsHelper.cs
@@ -5,14 +5,9 @@ namespace System
 {
     public static class LineEndingsHelper
     {
-        public const string CompiledNewline = @"
+        private const string CompiledNewline = @"
 ";
-        public static readonly bool s_consistentNewlines = StringComparer.Ordinal.Equals(CompiledNewline, Environment.NewLine);
-
-        static public bool IsNewLineConsistent
-        {
-            get { return s_consistentNewlines; }
-        }
+        private static readonly bool s_consistentNewlines = StringComparer.Ordinal.Equals(CompiledNewline, Environment.NewLine);
 
         public static string Normalize(string expected)
         {
