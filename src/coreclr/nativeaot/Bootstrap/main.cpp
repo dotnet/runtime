dotnet/runtime

commit 2811e7c7551c5ec56f1afbfd1749e1f42d264dbf

Address Sanitizer support in CoreCLR (#74623)
* Enable AddressSanitizer in CoreCLR, Libs, and Host and update runtime-sanitized to run tests with ASAN on Linux and Mac

* Unify on HAS_ADDRESS_SANITIZER define name

Centralize setting up sanitizer flags.

Remove last usage of CLR_CMAKE_ENABLE_ASAN outside of eng/native

Remove unnecessary diffs

Use the cpuid intrinsic and enable ASAN on InitJitHelpers1.

Add comments for the places where we have ASAN disabled

Undo changes to src/coreclr/jit/CMakeLists.txt

Add docs and fix the docs in the build script.

Add docs for the SkipVCEnvInit hook

* Remove CRT runtime changes. We won't need this when we finally onboard to Windows with some changes they have coming down the pipeline, so remove them for now to reduce the diff of this PR.

* Remove suppression now that we've fixed the underlying issue.

* Remove some Windows-only sanitizer CMake that we won't need when ASAN is ready for us to consume on Windows.

* Set schedule for runtime-sanitized pipeline

* Remove workaround in JIT memory set/copy helpers now that the JIT bug has been fixed.

* Add missing helix queues setup template.

* Fix missing command to build nativeaot runtime tests as nativeaot.

* Add a scenario name to trigger the extended timeout in the libraries test helix configuration.

* Remove extraneous whitespace.

* Fix using the cross-targetting ILC when sanitizers are enabled. Also pass through our TargetOS and TargetArchitecture variables to the publish command for our native sanitizers targets to correctly do their extra logic.

* Disable LSAN on the CustomMain test.

* Disable some tests on sanitized runtimes.

* Copy the sanitizer runtime for OSX NativeAOT runtime tests.

* A little cleanup to try to get the build right now that we are always doing cross-builds on Linux.

* Fix crossgen-corelib.proj syntax.

* Fix cross-os dac builds to not include the host architecture in the output path.

src/coreclr/nativeaot/Bootstrap/main.cpp
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 #include <stdint.h>
+#include "minipal/utils.h"
 
 //
 // This is the mechanism whereby multiple linked modules contribute their global data for initialization at
@@ -217,6 +218,12 @@ int main(int argc, char* argv[])
 
     return __managed__Main(argc, argv);
 }
+
+// We need to build the bootstrapper as a single object file, to ensure
+// the linker can detect that we have ASAN components early enough in the build.
+// Include our asan support sources for executable projects here to ensure they
+// are compiled into the bootstrapper object.
+#include "minipal/asansupport.cpp"
 #endif // !NATIVEAOT_DLL
 
 #ifdef NATIVEAOT_DLL

commit b326b6e19b4590a54841c9f62ceb00cef5e1ddbc

Fix building the reproNative.vcxproj project
This got broken in #74623.

src/coreclr/nativeaot/Bootstrap/main.cpp
@@ -2,7 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 #include <stdint.h>
+
+#ifdef HAS_ADDRESS_SANITIZER
 #include "minipal/utils.h"
+#endif
 
 //
 // This is the mechanism whereby multiple linked modules contribute their global data for initialization at
@@ -219,11 +222,14 @@ int main(int argc, char* argv[])
     return __managed__Main(argc, argv);
 }
 
+#ifdef HAS_ADDRESS_SANITIZER
 // We need to build the bootstrapper as a single object file, to ensure
 // the linker can detect that we have ASAN components early enough in the build.
 // Include our asan support sources for executable projects here to ensure they
 // are compiled into the bootstrapper object.
 #include "minipal/asansupport.cpp"
+#endif // HAS_ADDRESS_SANITIZER
+
 #endif // !NATIVEAOT_DLL
 
 #ifdef NATIVEAOT_DLL