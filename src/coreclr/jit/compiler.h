42322f5a9df7273509b74057457914da57a88265
Will Smith <lol.tihan@gmail.com>
2023-07-20 09:22:37 -0700

    [JIT] Fix - Do not remove `CAST` nodes on store if the `LCL_VAR` is a parameter or struct field (#85734)
    * Do not remove CAST nodes on assignment if the LCL_VAR is a parameter.
    
    * Added NormalizeOnLoad rules from SingleAccretion. Added description of why we cannot remove CAST nodes from parameters.
    
    * Remove morph optimization for NormalizeOnLoad in fgMorphLocalVar. Update test.
    
    * Do not OptimizeCastOnStore for params and struct fields
    
    * Update src/coreclr/jit/morph.cpp
    
    Co-authored-by: Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
    
    * Formatting
    
    ---------
    
    Co-authored-by: Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>

--- src/coreclr/jit/compiler.h
+++ src/coreclr/jit/compiler.h
@@ -1096,6 +1096,14 @@ public:
         assert(lvIsStructField);
         return ((lvFldOffset % TARGET_POINTER_SIZE) == 0);
     }
+
+    // NormalizeOnLoad Rules:
+    //     1. All small locals are actually TYP_INT locals.
+    //     2. NOL locals are such that not all definitions can be controlled by the compiler and so the upper bits can
+    //        be undefined.For parameters this is the case because of ABI.For struct fields - because of padding.For
+    //        address - exposed locals - because not all stores are direct.
+    //     3. Hence, all NOL uses(unless proven otherwise) are assumed in morph to have undefined upper bits and
+    //        explicit casts have be inserted to "normalize" them back to conform to IL semantics.
     bool lvNormalizeOnLoad() const
     {
         return varTypeIsSmall(TypeGet()) &&
