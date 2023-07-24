dda2156dac99b789b144054ed76f3f09668104d5
Hamish Arblaster <hamarb123@gmail.com>
2023-07-25 09:07:04 +1000

    Implement fix for #89347 and add a test

--- src/libraries/System.Reflection.Emit.ILGeneration/tests/ILGenerator/DeclareLocalTests.cs
+++ src/libraries/System.Reflection.Emit.ILGeneration/tests/ILGenerator/DeclareLocalTests.cs
@@ -119,6 +119,16 @@ namespace System.Reflection.Emit.Tests
             Assert.Throws<InvalidOperationException>(() => ilGenerator.DeclareLocal(typeof(int)));
         }
 
+        public void DeclareLocal_Pinned()
+        {
+            TypeBuilder type = Helpers.DynamicType(TypeAttributes.NotPublic);
+            MethodBuilder method = type.DefineMethod("TestMethod_Pinning", MethodAttributes.Public | MethodAttributes.Static);
+            ILGenerator generator = method.GetILGenerator();
+            Assert.True(generator.DeclareLocal(typeof(int).MakeByRefType(), true).IsPinned);
+            Assert.True(generator.DeclareLocal(typeof(object), true).IsPinned);
+            Assert.True(generator.DeclareLocal(typeof(byte), true).IsPinned);
+        }
+
         private void VerifyDeclareLocal(ILGenerator generator)
         {
             for (int i = 0; i < TestData.Length; i++)
