276c4fb35039c5ab8250b9295594c647a0f859cf
Maksim Golev <mgolev@htc-cs.ru>
2023-07-27 10:35:37 +0400

    feat(86031): Adding additional tests for inheritance with required modifiers.

--- src/libraries/System.Text.Json/tests/System.Text.Json.SourceGeneration.Tests/Serialization/RequiredKeywordTests.cs
+++ src/libraries/System.Text.Json/tests/System.Text.Json.SourceGeneration.Tests/Serialization/RequiredKeywordTests.cs
@@ -19,6 +19,8 @@ namespace System.Text.Json.SourceGeneration.Tests
         }
 
         [JsonSerializable(typeof(InheritedPersonWithRequiredMembers))]
+        [JsonSerializable(typeof(InheritedPersonWithRequiredMembersWithAdditionalRequiredMembers))]
+        [JsonSerializable(typeof(InheritedPersonWithRequiredMembersSetsRequiredMembers))]
         [JsonSerializable(typeof(PersonWithRequiredMembers))]
         [JsonSerializable(typeof(PersonWithRequiredMembersAndSmallParametrizedCtor))]
         [JsonSerializable(typeof(PersonWithRequiredMembersAndLargeParametrizedCtor))]
