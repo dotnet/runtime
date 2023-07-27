5e6f581ac603b7ba64afd2baf7346172aec500e3
anatawa12 <anatawa12@icloud.com>
2023-07-27 15:28:22 +0900

    add more unit tests

--- src/installer/tests/Microsoft.NET.HostModel.Tests/AppHost.Bundle.Tests/ResourceUpdaterTests.cs
+++ src/installer/tests/Microsoft.NET.HostModel.Tests/AppHost.Bundle.Tests/ResourceUpdaterTests.cs
@@ -6,7 +6,6 @@ using System.Reflection.Metadata;
 using System.Reflection.Metadata.Ecma335;
 using System.Reflection.PortableExecutable;
 using ILCompiler.Win32Resources;
-using Microsoft.DotNet.CoreSetup.Test;
 using Microsoft.NET.HostModel;
 using Xunit;
 
@@ -14,10 +13,7 @@ namespace AppHost.Bundle.Tests;
 
 public class ResourceUpdaterTests
 {
-    RepoDirectoriesProvider RepoDirectories = new RepoDirectoriesProvider();
-
-    [Fact]
-    void ResourceUpdaterAddResource()
+    private MemoryStream CreateTestPEFile(bool withResources)
     {
         var peBuilder = new ManagedPEBuilder(
             PEHeaderBuilder.CreateExecutableHeader(),
@@ -25,9 +21,27 @@ public class ResourceUpdaterTests
             ilStream: new BlobBuilder());
         var peImageBuilder = new BlobBuilder();
         peBuilder.Serialize(peImageBuilder);
-        using var memoryStream = new MemoryStream();
+        var memoryStream = new MemoryStream();
         memoryStream.Write(peImageBuilder.ToArray());
         memoryStream.Seek(0, SeekOrigin.Begin);
+
+        if (withResources)
+        {
+            using var updater = new ResourceUpdater(memoryStream, true);
+
+            updater.AddResource("Test Resource"u8.ToArray(), "testType", 0);
+            updater.Update();
+        }
+
+        memoryStream.Seek(0, SeekOrigin.Begin);
+
+        return memoryStream;
+    }
+
+    [Fact]
+    void ResourceUpdaterAddResource()
+    {
+        using var memoryStream = CreateTestPEFile(false);
 
         using (var updater = new ResourceUpdater(memoryStream, true))
         {
@@ -42,6 +56,27 @@ public class ResourceUpdaterTests
             var resourceReader = new ResourceData(reader);
             byte[]? testType = resourceReader.FindResource(0, "testType", 0);
             Assert.Equal("Test Resource"u8.ToArray(), testType);
+        }
+    }
+
+    [Fact]
+    void ResourceUpdaterAddResourceToExistingRsrc()
+    {
+        using var memoryStream = CreateTestPEFile(true);
+
+        using (var updater = new ResourceUpdater(memoryStream, true))
+        {
+            updater.AddResource("OtherResource"u8.ToArray(), "testType2", 0);
+            updater.Update();
+        }
+
+        memoryStream.Seek(0, SeekOrigin.Begin);
+
+        using (var reader = new PEReader(memoryStream, PEStreamOptions.LeaveOpen))
+        {
+            var resourceReader = new ResourceData(reader);
+            byte[]? testType = resourceReader.FindResource(0, "testType2", 0);
+            Assert.Equal("OtherResource"u8.ToArray(), testType);
         }
     }
 }
