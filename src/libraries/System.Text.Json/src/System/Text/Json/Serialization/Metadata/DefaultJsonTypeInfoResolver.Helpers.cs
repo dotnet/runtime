8d23440e014cf55563693a662e989837a5870ecf
Maksim Golev <mgolev@htc-cs.ru>
2023-07-27 16:13:57 +0400

    fix(86031): Fixes of formatting.

--- src/libraries/System.Text.Json/src/System/Text/Json/Serialization/Metadata/DefaultJsonTypeInfoResolver.Helpers.cs
+++ src/libraries/System.Text.Json/src/System/Text/Json/Serialization/Metadata/DefaultJsonTypeInfoResolver.Helpers.cs
@@ -76,8 +76,9 @@ namespace System.Text.Json.Serialization.Metadata
             Debug.Assert(!typeInfo.IsReadOnly);
             Debug.Assert(typeInfo.Kind is JsonTypeInfoKind.Object);
 
-            bool constructorHasSetsRequiredMembersAttribute
-                = typeInfo.Converter.ConstructorInfo?.HasSetsRequiredMembersAttribute() ?? false;
+            // SetsRequiredMembersAttribute means that all required members are assigned by constructor and therefore there is no enforcement
+            bool constructorHasSetsRequiredMembersAttribute =
+                typeInfo.Converter.ConstructorInfo?.HasSetsRequiredMembersAttribute() ?? false;
 
             JsonTypeInfo.PropertyHierarchyResolutionState state = new();
 
@@ -90,8 +91,9 @@ namespace System.Text.Json.Serialization.Metadata
                     break;
                 }
 
-                bool shouldCheckMembersForRequiredMemberAttribute
-                    = !constructorHasSetsRequiredMembersAttribute && currentType.HasRequiredMemberAttribute();
+                // Compiler adds RequiredMemberAttribute to type if any of the members is marked with 'required' keyword.
+                bool shouldCheckMembersForRequiredMemberAttribute =
+                    !constructorHasSetsRequiredMembersAttribute && currentType.HasRequiredMemberAttribute();
 
                 AddMembersDeclaredBySuperType(
                     typeInfo,
