cc14d20c407892def189808c5d426e27f05f3221
Lakshan Fernando <lakshanf@hotmail.com>
2023-07-28 15:41:11 -0700

    Tag events with thread specific information (#89516)
    * Tagging events with thread specific information
    
    * FB
    
    * Update src/coreclr/nativeaot/Runtime/eventpipe/ep-rt-aot.cpp
    
    Co-authored-by: Elinor Fung <elfung@microsoft.com>
    
    * Remove stale comments
    
    ---------
    
    Co-authored-by: Elinor Fung <elfung@microsoft.com>

--- src/coreclr/nativeaot/Runtime/eventpipe/ep-rt-aot.cpp
+++ src/coreclr/nativeaot/Runtime/eventpipe/ep-rt-aot.cpp
@@ -20,6 +20,9 @@
 #include <minipal/random.h>
 
 #include "gcenv.h"
+#include "thread.h"
+#include "threadstore.h"
+#include "threadstore.inl"
 
 #ifndef DIRECTORY_SEPARATOR_CHAR
 #ifdef TARGET_UNIX
@@ -56,7 +59,7 @@ ep_rt_aot_walk_managed_stack_for_thread (
     ep_rt_thread_handle_t thread,
     EventPipeStackContents *stack_contents)
 {
-    PalDebugBreak();
+    // NativeAOT does not support getting the call stack
     return false;
 }
 
@@ -826,6 +829,15 @@ void ep_rt_aot_create_activity_id (uint8_t *activity_id, uint32_t activity_id_le
 #endif
 }
 
+ep_rt_thread_handle_t ep_rt_aot_thread_get_handle (void)
+{
+    return ThreadStore::GetCurrentThreadIfAvailable();
+}
+
+ep_rt_thread_id_t ep_rt_aot_thread_get_id (ep_rt_thread_handle_t thread_handle)
+{
+    return thread_handle->GetPalThreadIdForLogging();
+}
 
 #ifdef EP_CHECKED_BUILD
 
