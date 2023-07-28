fd2c7d3932c7db798ba6dc0ac8aba5500524ef48
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2023-07-28 23:03:49 +0200

    SPMI: Gracefully handle recorded EE-side exceptions during replay
    Various JIT-EE APIs are expected to be able to throw exceptions, which
    SPMI will record and rethrow during replay. This change rethrows these
    exceptions using a known exception code so that we can gracefully handle
    these exceptions during replay; this makes us no longer consider these
    as replay failures.
    
    Also do a small amount of cleanup for the EH in SPMI.

--- src/coreclr/tools/superpmi/superpmi-shared/errorhandling.h
+++ src/coreclr/tools/superpmi/superpmi-shared/errorhandling.h
@@ -19,11 +19,12 @@
 #define EXCEPTIONCODE_LWM 0xe0423000
 #define EXCEPTIONCODE_CALLUTILS 0xe0426000
 #define EXCEPTIONCODE_TYPEUTILS 0xe0427000
+#define EXCEPTIONCODE_RECORDED_EXCEPTION 0xe0428000
 #define EXCEPTIONCODE_ASSERT 0xe0440000
 
 // RaiseException wrappers
-void MSC_ONLY(__declspec(noreturn)) ThrowException(DWORD exceptionCode);
-void MSC_ONLY(__declspec(noreturn)) ThrowException(DWORD exceptionCode, const char* message, ...);
+void MSC_ONLY(__declspec(noreturn)) ThrowSpmiException(DWORD exceptionCode, const char* message, ...);
+void MSC_ONLY(__declspec(noreturn)) ThrowRecordedException(DWORD innerExceptionCode);
 
 // Assert stuff
 #define AssertCodeMsg(expr, exCode, msg, ...)                                                                          \
