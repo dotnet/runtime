e554aa5f1cfe3b112c9450f1f50fee65d90ee183
Steven He <hez2010@outlook.com>
2023-07-30 23:35:19 +0900

    Fix an assert

--- src/coreclr/vm/jitinterface.cpp
+++ src/coreclr/vm/jitinterface.cpp
@@ -416,6 +416,8 @@ CorInfoType CEEInfo::asCorInfoType(CorElementType eeType,
         CORINFO_TYPE_UNDEF,          // CMOD_REQD
         CORINFO_TYPE_UNDEF,          // CMOD_OPT
         CORINFO_TYPE_UNDEF,          // INTERNAL
+        
+        CORINFO_TYPE_UNDEF,          // CTARG
         };
 
     _ASSERTE(sizeof(map) == ELEMENT_TYPE_MAX);
