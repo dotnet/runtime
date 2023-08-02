eefc98745e6db04815ac5bed47b2d41c777420da
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2023-08-02 13:10:49 +0900

    Rebaseline failing pri0 tests
    * We don't throw PNSE for unguarded HW intrinsic use. We could translate illegal instruction to PNSE but it feels too big of a hammer. We could do something more sophisticated such as disassemble the instruction and compare with "known problematic" ones. But it doesn't feel worth the effort.
    * Non-zero lower bounds are not supported.
    * Corner case weird enums like `enum Foo : double { }` are not supported.

--- src/tests/Common/CoreCLRTestLibrary/PlatformDetection.cs
+++ src/tests/Common/CoreCLRTestLibrary/PlatformDetection.cs
@@ -22,6 +22,28 @@ namespace TestLibrary
                                                 ? isEnabled
                                                 : true);
 
+        private static volatile Tuple<bool> s_lazyNonZeroLowerBoundArraySupported;
+        public static bool IsNonZeroLowerBoundArraySupported
+        {
+            get
+            {
+                if (s_lazyNonZeroLowerBoundArraySupported == null)
+                {
+                    bool nonZeroLowerBoundArraysSupported = false;
+                    try
+                    {
+                        Array.CreateInstance(typeof(int), new int[] { 5 }, new int[] { 5 });
+                        nonZeroLowerBoundArraysSupported = true;
+                    }
+                    catch (PlatformNotSupportedException)
+                    {
+                    }
+                    s_lazyNonZeroLowerBoundArraySupported = Tuple.Create<bool>(nonZeroLowerBoundArraysSupported);
+                }
+                return s_lazyNonZeroLowerBoundArraySupported.Item1;
+            }
+        }
+
         static string _variant = Environment.GetEnvironmentVariable("DOTNET_RUNTIME_VARIANT");
 
         public static bool IsMonoLLVMAOT => _variant == "llvmaot";
