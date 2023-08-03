9c394385b4992a5c9cfedf0acb83e23defc0b719
antonfirsov <antonfir@gmail.com>
2023-08-03 21:17:00 +0200

    Harmonize version tags

--- src/libraries/System.Net.Http/src/System/Net/Http/SocketsHttpHandler/HttpConnectionBase.cs
+++ src/libraries/System.Net.Http/src/System/Net/Http/SocketsHttpHandler/HttpConnectionBase.cs
@@ -47,8 +47,8 @@ namespace System.Net.Http
                 // While requests may report HTTP/1.0 as the protocol, we treat all HTTP/1.X connections as HTTP/1.1.
                 string protocol =
                     this is HttpConnection ? "1.1" :
-                    this is Http2Connection ? "2.0" :
-                    "3.0";
+                    this is Http2Connection ? "2" :
+                    "3";
 
                 _connectionMetrics = new ConnectionMetrics(
                     metrics,
