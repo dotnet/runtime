452e3867732ac3e63364687184564ddfd90ab125
Egor Bogatov <egorbo@gmail.com>
2023-08-03 15:54:56 +0200

    Update cordebug.h

--- src/coreclr/pal/prebuilt/inc/cordebug.h
+++ src/coreclr/pal/prebuilt/inc/cordebug.h
@@ -6539,7 +6539,7 @@ enum CorDebugGenerationTypes
         CorDebug_Gen2   = 2,
         CorDebug_LOH    = 3,
         CorDebug_POH    = 4,
-        CorDebug_NonGC  = INT_MAX
+        CorDebug_NonGC  = 0x7FFFFFFF
     }   CorDebugGenerationTypes;
 
 typedef struct _COR_SEGMENT
