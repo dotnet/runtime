c5c559346fd5ce668cde475daeb79383fca16051
Gan Keyu <gankeyu@hotmail.com>
2023-08-06 17:46:47 +0800

    Further avoid alloc by inlining GetUnescapedSpan
    Allocations are further avoided when the property name is shorter than
    JsonConstants.StackallocByteThreshold, by inlining
    JsonReaderHelper.GetUnescapedSpan.

--- src/libraries/System.Text.Json/src/System/Text/Json/Document/JsonProperty.cs
+++ src/libraries/System.Text.Json/src/System/Text/Json/Document/JsonProperty.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
+using System.Buffers;
 using System.Diagnostics;
 
 namespace System.Text.Json
@@ -120,16 +121,37 @@ namespace System.Text.Json
             if (_name is null)
             {
                 ReadOnlySpan<byte> rawName = Value.GetRawPropertyNameAsUtf8Span();
-#if NET462_OR_GREATER || NETSTANDARD2_0
-                if (rawName.IndexOf(JsonConstants.BackSlash) >= 0)
-#else
-                if (rawName.Contains(JsonConstants.BackSlash))
-#endif
+                int firstBackSlashIndex = rawName.IndexOf(JsonConstants.BackSlash);
+                if (firstBackSlashIndex >= 0)
                 {
-                    rawName = JsonReaderHelper.GetUnescapedSpan(rawName);
-                }
+                    // Code is adapted from JsonReaderHelper.GetUnescapedSpan to avoid allocations further.
+                    // writer.WritePropertyName(JsonReaderHelper.GetUnescapedSpan(rawName));
 
-                writer.WritePropertyName(rawName);
+                    int length = rawName.Length;
+                    byte[]? pooledName = null;
+
+                    Span<byte> utf8Unescaped = length <= JsonConstants.StackallocByteThreshold ?
+                        stackalloc byte[JsonConstants.StackallocByteThreshold] :
+                        (pooledName = ArrayPool<byte>.Shared.Rent(length));
+
+                    JsonReaderHelper.Unescape(rawName, utf8Unescaped, firstBackSlashIndex, out int written);
+                    Debug.Assert(written > 0);
+
+                    ReadOnlySpan<byte> propertyName = utf8Unescaped.Slice(0, written);
+                    Debug.Assert(!propertyName.IsEmpty);
+
+                    writer.WritePropertyName(propertyName);
+
+                    if (pooledName != null)
+                    {
+                        new Span<byte>(pooledName, 0, written).Clear();
+                        ArrayPool<byte>.Shared.Return(pooledName);
+                    }
+                }
+                else
+                {
+                    writer.WritePropertyName(rawName);
+                }
             }
             else
             {
