928cd02ef91b589c4b9e7829adbb24f440a10a3a
TIHan <lol.tihan@gmail.com>
2023-08-06 19:12:38 -0700

    Formatting

--- src/coreclr/jit/hwintrinsiccodegenarm64.cpp
+++ src/coreclr/jit/hwintrinsiccodegenarm64.cpp
@@ -335,8 +335,7 @@ void CodeGen::genHWIntrinsic(GenTreeHWIntrinsic* node)
         {
             assert(hasImmediateOperand);
 
-            auto emitShift = [&](GenTree* op, regNumber reg)
-            {
+            auto emitShift = [&](GenTree* op, regNumber reg) {
                 HWIntrinsicImmOpHelper helper(this, op, node);
 
                 for (helper.EmitBegin(); !helper.Done(); helper.EmitCaseEnd())
