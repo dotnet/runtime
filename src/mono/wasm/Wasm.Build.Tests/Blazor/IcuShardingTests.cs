fee57ddccafc268858dee06176d68e18f7234110
Ilona Tomkowicz <32700855+ilonatommy@users.noreply.github.com>
2023-08-09 15:34:56 +0000

    Issue 2 solved: files from runtime pack work when abs path is used.

--- src/mono/wasm/Wasm.Build.Tests/Blazor/IcuShardingTests.cs
+++ src/mono/wasm/Wasm.Build.Tests/Blazor/IcuShardingTests.cs
@@ -7,6 +7,7 @@ using Xunit;
 using Xunit.Abstractions;
 using Xunit.Sdk;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 #nullable enable
 
@@ -19,13 +20,49 @@ public class IcuShardingTests : BlazorWasmTestBase
         : base(output, buildContext) {}
 
     [Theory]
+    [InlineData("Debug", "icudt.dat")]
+    [InlineData("Release", "icudt.dat")]
+    [InlineData("Debug", "icudt_EFIGS.dat")]
+    [InlineData("Release", "icudt_EFIGS.dat")]
+    [InlineData("Debug", "icudt_no_CJK.dat")]
+    [InlineData("Release", "icudt_no_CJK.dat")]
+    [InlineData("Debug", "icudt_CJK.dat")]
+    [InlineData("Release", "icudt_CJK.dat")]
+    public async Task CustomIcuFileFromRuntimePack(string config, string fileName)
+    {
+        string id = $"blz_customFromRuntimePack_{config}_{GetRandomId()}";
+        string projectFile = CreateBlazorWasmTemplateProject(id);
+        var buildOptions = new BlazorBuildOptions(
+                id,
+                config,
+                WarnAsError: true,
+                GlobalizationMode: GlobalizationMode.PredefinedIcu,
+                PredefinedIcudt: fileName
+            );
+        string icuFilePath = GetAbsolutePathToIcuFromRuntimePack(fileName, buildOptions);
+        AddItemsPropertiesToProject(
+            projectFile,
+            extraProperties: 
+                $"<BlazorIcuDataFileName>{icuFilePath}</BlazorIcuDataFileName>");
+
+        (CommandResult res, string logPath) = BlazorBuild(buildOptions);
+        await BlazorRunForBuildWithDotnetRun(new BlazorRunOptions() { Config = config });
+    }
+
+    private string GetAbsolutePathToIcuFromRuntimePack(string icuFileName, BlazorBuildOptions options)
+    {
+        string runtimePackPath = ProjectProviderBase.GetExpectedRuntimePackDir(options.TargetFramework ?? DefaultTargetFramework);
+        return Path.Combine(runtimePackPath, "runtimes", "browser-wasm", "native", icuFileName);
+    }
+
+    [Theory]
     [InlineData("Debug")]
     [InlineData("Release")]
-    public void NonExistingCustomFileAssertError(string config)
+    public async Task NonExistingCustomFileAssertError(string config)
     {
         string id = $"blz_invalidCustomIcu_{config}_{GetRandomId()}";
         string fileName = "nonexisting.dat";
-        string projectFile = CreateProjectWithNativeReference(id);
+        string projectFile = CreateBlazorWasmTemplateProject(id);
         AddItemsPropertiesToProject(
             projectFile,
             extraProperties: 
@@ -50,15 +87,16 @@ public class IcuShardingTests : BlazorWasmTestBase
         {
             throw new Exception("Unexpected exception in test scenario.");
         }
+        await BlazorRunForBuildWithDotnetRun(new BlazorRunOptions() { Config = config });
     }
 
     [Theory]
     [InlineData("Debug")]
     [InlineData("Release")]
-    public void CustomFileNotFromRuntimePackAbsolutePath(string config)
+    public async Task CustomFileNotFromRuntimePackAbsolutePath(string config)
     {
         string id = $"blz_invalidCustomIcu_{config}_{GetRandomId()}";
-        string projectFile = CreateProjectWithNativeReference(id);
+        string projectFile = CreateBlazorWasmTemplateProject(id);
         AddItemsPropertiesToProject(
             projectFile,
             extraProperties: 
@@ -70,8 +108,8 @@ public class IcuShardingTests : BlazorWasmTestBase
                 config,
                 WarnAsError: false,
                 GlobalizationMode: GlobalizationMode.PredefinedIcu,
-                PredefinedIcudt: IcuTestsBase.CustomIcuPath,
-                ExpectedFileType: NativeFilesType.Relinked
+                PredefinedIcudt: IcuTestsBase.CustomIcuPath
             ));
+        await BlazorRunForBuildWithDotnetRun(new BlazorRunOptions() { Config = config });
     }
 }
