928e07bfca016a3f9f4e8f444640e1e615efbc12
EgorBo <egorbo@gmail.com>
2023-08-10 02:49:12 +0200

    fix-improve-impStringEqualsOrStartsWith

--- src/coreclr/jit/importervectorization.cpp
+++ src/coreclr/jit/importervectorization.cpp
@@ -616,11 +616,19 @@ GenTree* Compiler::impStringEqualsOrStartsWith(bool startsWith, CORINFO_SIG_INFO
         op2 = impStackTop(0).val;
     }
 
-    if (!(op1->OperIs(GT_CNS_STR) ^ op2->OperIs(GT_CNS_STR)))
+    if (!op1->OperIs(GT_CNS_STR) && !op2->OperIs(GT_CNS_STR))
     {
-        // either op1 or op2 has to be CNS_STR, but not both - that case is optimized
-        // just fine as is.
         return nullptr;
+    }
+
+    if (op1->OperIs(GT_CNS_STR) && op2->OperIs(GT_CNS_STR) && GenTree::Compare(op1, op2))
+    {
+        // Both are literals and represent the same string - return true
+        for (int i = 0; i < argsCount; i++)
+        {
+            impPopStack();
+        }
+        return gtNewTrue();
     }
 
     GenTree*       varStr;
