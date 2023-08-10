79a8359fad723bc4cd45165d84734530b8dcf0e3
Pavel Savara <pavel.savara@gmail.com>
2023-08-10 16:40:17 +0200

    [browser] make cachedResourcesPurgeDelay configurable (#90264)

--- src/mono/wasm/runtime/dotnet.d.ts
+++ src/mono/wasm/runtime/dotnet.d.ts
@@ -130,6 +130,10 @@ type MonoConfig = {
      */
     cacheBootResources?: boolean;
     /**
+     * Delay of the purge of the cached resources in milliseconds. Default is 10000 (10 seconds).
+     */
+    cachedResourcesPurgeDelay?: number;
+    /**
      * Configures use of the `integrity` directive for fetching assets
      */
     disableIntegrityCheck?: boolean;
