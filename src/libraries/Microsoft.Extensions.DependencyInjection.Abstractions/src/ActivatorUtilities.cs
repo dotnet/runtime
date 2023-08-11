32709f7ed3ef2640bd4ada88c6304de476f3eb71
Benjamin Petit <bpetit@microsoft.com>
2023-08-11 18:50:20 +0200

    Improvement

--- src/libraries/Microsoft.Extensions.DependencyInjection.Abstractions/src/ActivatorUtilities.cs
+++ src/libraries/Microsoft.Extensions.DependencyInjection.Abstractions/src/ActivatorUtilities.cs
@@ -508,13 +508,10 @@ namespace Microsoft.Extensions.DependencyInjection
 
         private static bool TryGetServiceKey(ParameterInfo parameterInfo, out object? key)
         {
-            foreach (var attribute in parameterInfo.GetCustomAttributes(true))
+            foreach (var attribute in parameterInfo.GetCustomAttributes<FromKeyedServicesAttribute>(false))
             {
-                if (attribute is FromKeyedServicesAttribute keyed)
-                {
-                    key = keyed.Key;
-                    return true;
-                }
+                key = attribute.Key;
+                return true;
             }
             key = null;
             return false;
