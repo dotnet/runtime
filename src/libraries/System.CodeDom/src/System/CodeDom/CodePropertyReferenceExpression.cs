8636a193a48d0117ba23bf6b29fbd2410910b179
halgab <24685886+halgab@users.noreply.github.com>
2023-08-11 09:59:17 +0200

    Fix annotations of CodePropertyReferenceExpression

--- src/libraries/System.CodeDom/src/System/CodeDom/CodePropertyReferenceExpression.cs
+++ src/libraries/System.CodeDom/src/System/CodeDom/CodePropertyReferenceExpression.cs
@@ -1,22 +1,25 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics.CodeAnalysis;
+
 namespace System.CodeDom
 {
     public class CodePropertyReferenceExpression : CodeExpression
     {
-        private string _propertyName;
+        private string? _propertyName;
 
         public CodePropertyReferenceExpression() { }
 
-        public CodePropertyReferenceExpression(CodeExpression targetObject, string propertyName)
+        public CodePropertyReferenceExpression(CodeExpression? targetObject, string? propertyName)
         {
             TargetObject = targetObject;
             PropertyName = propertyName;
         }
 
-        public CodeExpression TargetObject { get; set; }
+        public CodeExpression? TargetObject { get; set; }
 
+        [AllowNull]
         public string PropertyName
         {
             get => _propertyName ?? string.Empty;
