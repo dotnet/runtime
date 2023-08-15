92f3388909a7b988d3886ad07215002e02b67754
Mike Voorhees <mrvoorhe@users.noreply.github.com>
2023-08-15 18:39:13 -0400

    Add tests for missing references (#90261)

---
+++ src/tools/illink/test/Mono.Linker.Tests.Cases/References/MissingReferenceInUsedCodePath.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
+using Mono.Linker.Tests.Cases.Expectations.Metadata;
+using Mono.Linker.Tests.Cases.References.Dependencies;
+
+namespace Mono.Linker.Tests.Cases.References;
+
+[ExpectNonZeroExitCode (1)]
+[NoLinkedOutput]
+[SetupCompileBefore ("missing.dll", new[] { "Dependencies/MissingAssembly.cs" })]
+[DeleteBefore ("missing.dll")]
+public class MissingReferenceInUsedCodePath
+{
+	public static void Main ()
+	{
+		typeof (MissingAssembly).ToString ();
+	}
+}
