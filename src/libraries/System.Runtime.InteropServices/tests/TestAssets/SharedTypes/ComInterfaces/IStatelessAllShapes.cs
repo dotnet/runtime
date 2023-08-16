99b6765d1694923c8935fe58a8746369b6e5b046
Jackson Schuster <36744439+jtschuster@users.noreply.github.com>
2023-08-16 11:59:38 -0700

    Make native type convertible to void*

--- src/libraries/System.Runtime.InteropServices/tests/TestAssets/SharedTypes/ComInterfaces/IStatelessAllShapes.cs
+++ src/libraries/System.Runtime.InteropServices/tests/TestAssets/SharedTypes/ComInterfaces/IStatelessAllShapes.cs
@@ -1,46 +1,42 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
-using System.Runtime.InteropServices;
-using System.Runtime.InteropServices.Marshalling;
-
 namespace SharedTypes.ComInterfaces
 {
-    /// <summary>
-    /// This type uses a stateless marshaller with all methods from every marshaller shape present
-    /// </summary>
-    [GeneratedComInterface]
-    [Guid("4732FA5D-C105-4A23-87A7-58DCEDD4A9B3")]
-    internal partial interface IStatelessAllShapes
-    {
-        void Method(StatelessAllShapesType param);
-        void MethodIn(in StatelessAllShapesType param);
-        void MethodOut(out StatelessAllShapesType param);
-        void MethodRef(ref StatelessAllShapesType param);
-        StatelessAllShapesType Return();
-        [PreserveSig]
-        StatelessAllShapesType ReturnPreserveSig();
-    }
+	/// <summary>
+	/// This type uses a stateless marshaller with all methods from every marshaller shape present
+	/// </summary>
+	[GeneratedComInterface]
+	[Guid("4732FA5D-C105-4A23-87A7-58DCEDD4A9B3")]
+	internal partial interface IStatelessAllShapes
+	{
+		void Method(StatelessAllShapesType param);
+		void MethodIn(in StatelessAllShapesType param);
+		void MethodOut(out StatelessAllShapesType param);
+		void MethodRef(ref StatelessAllShapesType param);
+		StatelessAllShapesType Return();
+		[PreserveSig]
+		StatelessAllShapesType ReturnPreserveSig();
+	}
 
-    [NativeMarshalling(typeof(AllStatelessMarshallerShapes))]
-    internal class StatelessAllShapesType
-    {
-    }
+	[NativeMarshalling(typeof(AllStatelessMarshallerShapes))]
+	internal class StatelessAllShapesType
+	{
+	}
 
-    internal struct StatelessAllShapesNative
-    {
-    }
+	internal struct StatelessAllShapesNative
+	{
+	}
 
-    [CustomMarshaller(typeof(StatelessAllShapesType), MarshalMode.Default, typeof(AllStatelessMarshallerShapes))]
-    internal static class AllStatelessMarshallerShapes
-    {
-        public static ref nint GetPinnableReference(StatelessAllShapesType managed) => throw new NotImplementedException();
-        public static int BufferSize => 32;
-        public static StatelessAllShapesNative ConvertToUnmanaged(StatelessAllShapesType managed, Span<byte> buffer) => throw new NotImplementedException();
-        public static StatelessAllShapesNative ConvertToUnmanaged(StatelessAllShapesType managed) => throw new NotImplementedException();
-        public static StatelessAllShapesType ConvertToManaged(StatelessAllShapesNative unmanaged) => throw new NotImplementedException();
-        public static StatelessAllShapesType ConvertToManagedFinally(StatelessAllShapesNative unmanaged) => throw new NotImplementedException();
-        public static void Free(StatelessAllShapesNative unmanaged) { }
-    }
+	[CustomMarshaller(typeof(StatelessAllShapesType), MarshalMode.Default, typeof(AllStatelessMarshallerShapes))]
+	internal static class AllStatelessMarshallerShapes
+	{
+		public static ref nint GetPinnableReference(StatelessAllShapesType managed) => throw new NotImplementedException();
+		public static int BufferSize => 32;
+		public static StatelessAllShapesNative* ConvertToUnmanaged(StatelessAllShapesType managed, Span<byte> buffer) => throw new NotImplementedException();
+		public static StatelessAllShapesNative* ConvertToUnmanaged(StatelessAllShapesType managed) => throw new NotImplementedException();
+		public static StatelessAllShapesType ConvertToManaged(StatelessAllShapesNative* unmanaged) => throw new NotImplementedException();
+		public static StatelessAllShapesType ConvertToManagedFinally(StatelessAllShapesNative* unmanaged) => throw new NotImplementedException();
+		public static void Free(StatelessAllShapesNative* unmanaged) { }
+	}
 }
