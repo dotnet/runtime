a9795f79fde93c51f097f323f60707dfe62e9c5c
Steve <hez2010@outlook.com>
2023-08-17 19:11:18 +0900

    Fix format

--- src/coreclr/jit/objectalloc.h
+++ src/coreclr/jit/objectalloc.h
@@ -21,7 +21,7 @@ XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 class ObjectAllocator final : public Phase
 {
-    typedef SmallHashTable<unsigned int, unsigned int, 8U> LocalToLocalMap;
+    typedef SmallHashTable<unsigned int, unsigned int, 8U>       LocalToLocalMap;
     typedef SmallHashTable<unsigned int, unsigned long long, 8U> LocalToLclOffMap;
 
     //===============================================================================
