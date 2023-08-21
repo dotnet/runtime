45c205d40344d570e0e160524ea211d3615a6307
David Cantu <dacantu@microsoft.com>
2023-08-21 18:21:24 -0500

    Fix ocurrences in src/tasks projects

--- src/tasks/Common/FileCache.cs
+++ src/tasks/Common/FileCache.cs
@@ -16,6 +16,7 @@ internal sealed class FileCache
 {
     private CompilerCache? _newCache;
     private CompilerCache? _oldCache;
+    private static readonly JsonSerializerOptions s_jsonOptions = new JsonSerializerOptions { WriteIndented = true };
 
     public bool Enabled { get; }
     public TaskLoggingHelper Log { get; }
@@ -34,7 +35,7 @@ internal sealed class FileCache
         {
             _oldCache = (CompilerCache?)JsonSerializer.Deserialize(File.ReadAllText(cacheFilePath),
                                                                     typeof(CompilerCache),
-                                                                    new JsonSerializerOptions());
+                                                                    s_jsonOptions);
         }
 
         _oldCache ??= new();
@@ -84,7 +85,7 @@ internal sealed class FileCache
         if (!Enabled || string.IsNullOrEmpty(cacheFilePath))
             return false;
 
-        var json = JsonSerializer.Serialize (_newCache, new JsonSerializerOptions { WriteIndented = true });
+        var json = JsonSerializer.Serialize (_newCache, s_jsonOptions);
         File.WriteAllText(cacheFilePath!, json);
         return true;
     }
