658bdd0376326f43cc8a6f316b2e01cacdcc688d
Rich Lander <rich@lander.ca>
2023-08-24 10:20:53 -0700

    Upgrade all VMs to AMD64 Ubuntu 22.04 (#86194)
    * Upgrade all VMs to Ubuntu 22.04
    
    * Update display names
    
    * Revert wasm and mono back to 18.04
    
    * Update eng/pipelines/coreclr/templates/helix-queues-setup.yml
    
    Co-authored-by: Jeremy Koritzinsky <jkoritzinsky@gmail.com>
    
    * Fix merge conflicts
    
    * Update Android queue
    
    * Removed an Ubuntu.1804.Tiger queue I accidentally enabled when merging with main earlier.
    
    * Restored the Ubuntu 18.04 OS for the crossgen tiger perf queue due to a requirement currently unavailable on 22.04
    
    * Restored Ubuntu 18.04 in source-build.yml because those changes come from the arcade repo.
    
    * Fix bad merge
    
    ---------
    
    Co-authored-by: Jeremy Koritzinsky <jkoritzinsky@gmail.com>
    Co-authored-by: Alexander Köplinger <alex.koeplinger@outlook.com>
    Co-authored-by: Ivan Diaz Sanchez <ivdiazsa@microsoft.com>

--- eng/testing/performance/performance-setup.sh
+++ eng/testing/performance/performance-setup.sh
@@ -270,7 +270,7 @@ benchmark_directory=$payload_directory/BenchmarkDotNet
 workitem_directory=$source_directory/workitem
 extra_benchmark_dotnet_arguments="--iterationCount 1 --warmupCount 0 --invocationCount 1 --unrollFactor 1 --strategy ColdStart --stopOnFirstError true"
 perflab_arguments=
-queue=Ubuntu.1804.Amd64.Open
+queue=Ubuntu.2204.Amd64.Open
 creator=$BUILD_DEFINITIONNAME
 helix_source_prefix="pr"
 
@@ -285,12 +285,12 @@ if [[ "$internal" == true ]]; then
     elif [[ "$logical_machine" == "perfampere" ]]; then
         queue=Ubuntu.2004.Arm64.Perf
     elif [[ "$logical_machine" == "cloudvm" ]]; then
-        queue=Ubuntu.1804.Amd64
+        queue=Ubuntu.2204.Amd64
     elif [[ "$architecture" == "arm64" ]]; then
         queue=Ubuntu.1804.Arm64.Perf
     else
         if [[ "$logical_machine" == "perfowl" ]]; then
-            queue=Ubuntu.1804.Amd64.Owl.Perf
+            queue=Ubuntu.2204.Amd64.Owl.Perf
         elif [[ "$logical_machine" == "perftiger_crossgen" ]]; then
             queue=Ubuntu.1804.Amd64.Tiger.Perf
         else
@@ -305,7 +305,7 @@ else
     if [[ "$architecture" == "arm64" ]]; then
         queue=ubuntu.1804.armarch.open
     else
-        queue=Ubuntu.1804.Amd64.Open
+        queue=Ubuntu.2204.Amd64.Open
     fi
 
     if [[ "$alpine" == "true" ]]; then
