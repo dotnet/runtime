7b310900b67b8dcb8a5e806b19d2d48bf213bc4d
Ivan Diaz <ivdiazsa@microsoft.com>
2023-08-28 13:46:28 -0700

    New beginning for the Jitted Methods Counting Test.

---
+++ src/tests/readytorun/JittedMethodsCountingTest/HelloWorld/HelloWorld.cs
@@ -0,0 +1,13 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+
+internal class HelloWorld
+{
+    static int Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        return 0;
+    }
+}
