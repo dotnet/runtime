dcd6a5fa85337fdf8daa9e70657947b2966d34db
Radek Doulik <radek.doulik@gmail.com>
2023-08-28 11:49:00 +0200

    [wasm] Add blazor startup measurement to browser-bench
    New blazor-frame project is slightly modified blazor template.
    
    The browser bench measures the starup time of the blazor-frame app
    inside the iframe. It tries to load it from the `blazor-frame/index.html`
    relative url. So to enable the blazor measurements, build the blazor-frame
    app separately and add a link to (or copy) the published wwwroot
    to the browser-bench app bundle. This will be usually handled by our
    measurements infrastructure.
    
    Also fixed responses for missing files in the simple server.

---
+++ src/mono/sample/wasm/blazor-frame/Program.cs
@@ -0,0 +1,14 @@
+﻿#pragma warning disable IDE0073
+
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using blazor;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync().ConfigureAwait(true);
---
+++ src/mono/sample/wasm/blazor-frame/wwwroot/frame.js
@@ -0,0 +1,45 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+"use strict";
+
+import { dotnet, exit } from './_framework/dotnet.js'
+
+class FrameApp {
+    async init({ getAssemblyExports }) {
+        const exports = await getAssemblyExports("blazor.dll");
+        exports.BrowserBench.FrameApp.ReachedManaged();
+    }
+
+    reachedCallback() {
+        if (window.parent != window) {
+            window.parent.resolveAppStartEvent("reached");
+        }
+    }
+}
+
+let mute = false;
+try {
+    globalThis.frameApp = new FrameApp();
+    globalThis.frameApp.ReachedCallback = globalThis.frameApp.reachedCallback.bind(globalThis.frameApp);
+    if (window.parent != window) {
+        window.addEventListener("pageshow", event => { window.parent.resolveAppStartEvent("pageshow"); })
+    }
+
+    window.muteErrors = () => {
+        mute = true;
+    }
+
+    await Blazor.start();
+
+    if (window.parent != window) {
+        window.parent.resolveAppStartEvent("onDotnetReady");
+    }
+    await frameApp.init(Blazor.runtime);
+}
+catch (err) {
+    if (!mute) {
+        console.error(`WASM ERROR ${err}`);
+    }
+    exit(1, err);
+}
