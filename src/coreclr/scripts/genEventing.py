b8fcdfa4fc2408361f6c93e48373e1ef5a252640
Lakshan Fernando <lakshanf@hotmail.com>
2023-08-28 04:03:39 -0700

    removing enum import

--- src/coreclr/scripts/genEventing.py
+++ src/coreclr/scripts/genEventing.py
@@ -14,7 +14,6 @@ from __future__ import print_function
 import os
 import xml.dom.minidom as DOM
 from utilities import open_for_update, parseInclusionList
-from enum import Enum
 
 class RuntimeFlavor:
     def __init__(self, runtime):
