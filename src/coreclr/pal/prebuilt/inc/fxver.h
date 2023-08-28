60ff9dbe76d307e5135546355dc7c435cf492cc0
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2023-08-28 11:23:59 +0800

    Try add back #include <verrsrc.h>

--- src/coreclr/pal/prebuilt/inc/fxver.h
+++ src/coreclr/pal/prebuilt/inc/fxver.h
@@ -1,6 +1,10 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#ifdef HOST_WINDOWS
+#include <verrsrc.h>
+#endif
+
 #define QUOTE_MACRO_HELPER(x)       #x
 #define QUOTE_MACRO(x)              QUOTE_MACRO_HELPER(x)
 
