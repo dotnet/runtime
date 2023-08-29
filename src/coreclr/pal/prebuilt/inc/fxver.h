e42b22084950f1e6a2dd0327e5076679b16f1bbc
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2023-08-29 14:39:52 +0800

    Use HOST_UNIX instead

--- src/coreclr/pal/prebuilt/inc/fxver.h
+++ src/coreclr/pal/prebuilt/inc/fxver.h
@@ -1,7 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#ifndef TARGET_UNIX
+#ifndef HOST_UNIX
 #include <verrsrc.h>
 #endif
 
