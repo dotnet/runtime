21f008e44dbf6326ffd73c4ee4bd95948b9a7ed3
Marek Fišera <mara@neptuo.com>
2023-08-30 12:35:45 +0200

    wip

--- src/libraries/Common/tests/WasmTestRunner/WasmTestRunner.cs
+++ src/libraries/Common/tests/WasmTestRunner/WasmTestRunner.cs
@@ -3,9 +3,13 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
-
+using Microsoft.DotNet.XHarness.TestRunners.Common;
 using Microsoft.DotNet.XHarness.TestRunners.Xunit;
+using Xunit.Sdk;
 
 public class SimpleWasmTestRunner : WasmApplicationEntryPoint
 {
@@ -13,7 +17,7 @@ public class SimpleWasmTestRunner : WasmApplicationEntryPoint
     {
         if (args.Length == 0)
         {
-            Console.WriteLine ($"No args given");
+            Console.WriteLine($"No args given");
             return -1;
         }
 
@@ -30,40 +34,104 @@ public class SimpleWasmTestRunner : WasmApplicationEntryPoint
             switch (option)
             {
                 case "-notrait":
-                    excludedTraits.Add (args[i + 1]);
+                    excludedTraits.Add(args[i + 1]);
                     i++;
                     break;
                 case "-trait":
-                    includedTraits.Add (args[i + 1]);
+                    includedTraits.Add(args[i + 1]);
                     i++;
                     break;
                 case "-namespace":
-                    includedNamespaces.Add (args[i + 1]);
+                    includedNamespaces.Add(args[i + 1]);
                     i++;
                     break;
                 case "-class":
-                    includedClasses.Add (args[i + 1]);
+                    includedClasses.Add(args[i + 1]);
                     i++;
                     break;
                 case "-method":
-                    includedMethods.Add (args[i + 1]);
+                    includedMethods.Add(args[i + 1]);
                     i++;
+                    break;
+                case "-mt":
                     break;
                 default:
                     throw new ArgumentException($"Invalid argument '{option}'.");
             }
         }
 
-        var runner = new SimpleWasmTestRunner()
+        WasmApplicationEntryPointBase? runner = null;
+        if (args.Contains("-mt"))
         {
-            TestAssembly = testAssembly,
-            ExcludedTraits = excludedTraits,
-            IncludedTraits = includedTraits,
-            IncludedNamespaces = includedNamespaces,
-            IncludedClasses = includedClasses,
-            IncludedMethods = includedMethods
-        };
+            Console.WriteLine("Using MultiThreadedTestRunner");
+            runner = new MultiThreadedTestRunner()
+            {
+                TestAssembly = testAssembly,
+                ExcludedTraits = excludedTraits,
+                IncludedTraits = includedTraits,
+                IncludedNamespaces = includedNamespaces,
+                IncludedClasses = includedClasses,
+                IncludedMethods = includedMethods
+            };
+        }
+        else
+        {
+            Console.WriteLine("Using SimpleWasmTestRunner");
+            runner = new SimpleWasmTestRunner()
+            {
+                TestAssembly = testAssembly,
+                ExcludedTraits = excludedTraits,
+                IncludedTraits = includedTraits,
+                IncludedNamespaces = includedNamespaces,
+                IncludedClasses = includedClasses,
+                IncludedMethods = includedMethods
+            };
+        }
 
-        return await runner.Run();
+        await runner.RunAsync();
+        return runner.LastRunHadFailedTests ? 1 : 0;
     }
+}
+
+class MultiThreadedTestRunner : WasmApplicationEntryPointBase
+{
+    public virtual string TestAssembly { get; set; } = "";
+    public virtual IEnumerable<string> ExcludedTraits { get; set; } = Array.Empty<string>();
+    public virtual IEnumerable<string> IncludedTraits { get; set; } = Array.Empty<string>();
+    public virtual IEnumerable<string> IncludedClasses { get; set; } = Array.Empty<string>();
+    public virtual IEnumerable<string> IncludedMethods { get; set; } = Array.Empty<string>();
+    public virtual IEnumerable<string> IncludedNamespaces { get; set; } = Array.Empty<string>();
+
+    protected override bool IsXunit => true;
+
+    protected override TestRunner GetTestRunner(LogWriter logWriter)
+    {
+        var runner = new MyXUnitTestRunner(logWriter);
+        //var xUnitTestRunnerType = typeof(XunitTestRunnerBase).Assembly.GetType("Microsoft.DotNet.XHarness.TestRunners.Xunit.XUnitTestRunner");
+        //var runner = (XunitTestRunnerBase)xUnitTestRunnerType!.GetConstructors().First().Invoke(new[] { logWriter });
+
+        //ConfigureRunnerFilters(runner, ApplicationOptions.Current);
+        var configureRunnerFiltersMethod = typeof(ApplicationEntryPoint).GetMethod("ConfigureRunnerFilters", BindingFlags.Static | BindingFlags.NonPublic);
+        configureRunnerFiltersMethod!.Invoke(null, new object[] { runner, ApplicationOptions.Current });
+
+        runner.SkipCategories(ExcludedTraits);
+        runner.SkipCategories(IncludedTraits, isExcluded: false);
+        foreach (var cls in IncludedClasses)
+        {
+            runner.SkipClass(cls, false);
+        }
+        foreach (var method in IncludedMethods)
+        {
+            runner.SkipMethod(method, false);
+        }
+        foreach (var ns in IncludedNamespaces)
+        {
+            runner.SkipNamespace(ns, isExcluded: false);
+        }
+
+        return runner;
+    }
+
+    protected override IEnumerable<TestAssemblyInfo> GetTestAssemblies()
+        => new[] { new TestAssemblyInfo(Assembly.LoadFrom(TestAssembly), TestAssembly) };
 }
