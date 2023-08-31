87212daa15114b5712314e5b2a0aa19c7d3ed283
Ivan Diaz <ivdiazsa@microsoft.com>
2023-08-31 11:34:22 -0700

    Changed the test's methodology to use the JitInfo API, instead of logging all the jitted methods into a file.

--- src/tests/readytorun/JittedMethodsCountingTest/HelloWorld/HelloWorld.cs
+++ src/tests/readytorun/JittedMethodsCountingTest/HelloWorld/HelloWorld.cs
@@ -8,6 +8,10 @@ internal class HelloWorld
     static int Main(string[] args)
     {
         Console.WriteLine("Hello World!");
-        return 0;
+        // We're fine converting to int here because if we got that many jitted
+        // methods that would require a long, then that means something is
+        // extremely wrong. Actually, I'm not even sure it's possible.
+        int jits = (int) System.Runtime.JitInfo.GetCompiledMethodCount(true);
+        return jits > 0 ? jits : -1;
     }
 }
--- src/tests/readytorun/JittedMethodsCountingTest/JittedMethodsCountingTest.cs
+++ src/tests/readytorun/JittedMethodsCountingTest/JittedMethodsCountingTest.cs
@@ -11,7 +11,7 @@ using Reflection = System.Reflection;
 
 public class JittedMethodsCountingTest
 {
-    private const int MAX_JITTED_METHODS_ACCEPTED = 120;
+    private const int MAX_JITTED_METHODS_ACCEPTED = 70;
 
     [Fact]
     public static int TestEntryPoint()
@@ -28,33 +28,27 @@ public class JittedMethodsCountingTest
                                                       .Assembly
                                                       .GetExecutingAssembly()
                                                       .Location);
-
         string appName = Path.Combine(testAppLocation, "HelloWorld.dll");
-        string jitOutputFile = Path.Combine(testAppLocation, "jits.txt");
 
-        // DOTNET_JitStdOutFile appends to the file in question if it exists.
-        // This can potentially cause issues, so we have to make sure it is
-        // created exclusively for this test's purpose.
-        if (File.Exists(jitOutputFile))
-        {
-            File.Delete(jitOutputFile);
-        }
+        // For adding any new apps for this test, make sure they return a negative
+        // number when failing.
+        int appResult = RunHelloWorldApp(appName);
 
-        // For adding any new apps for this test, make sure their success return
-        // code is 0, so we can universally handle when they fail.
-        int appResult = RunHelloWorldApp(appName, jitOutputFile);
-
-        if (appResult != 0)
+        if (appResult < 0)
         {
             Console.WriteLine("App failed somewhere and so we can't proceed with"
-                              + " the Jitted Methods analysis. Exiting...");
+                              + " the Jitted Methods analysis.");
+
+            Console.WriteLine("App Exit Code: {0}", appResult);
+            Console.WriteLine("Exiting...");
+
             return 101;
         }
 
-        // App finished successfully. We can now take a look at the methods that
+        // App finished successfully. We can now take a look at how many methods
         // got jitted at runtime.
-        int jits = GetNumberOfJittedMethods(jitOutputFile);
-        return jits > 0 && jits <= MAX_JITTED_METHODS_ACCEPTED ? 100 : 101;
+        Console.WriteLine("Number of Jitted Methods: {0}\n", appResult);
+        return appResult > 0 && appResult <= MAX_JITTED_METHODS_ACCEPTED ? 100 : 101;
     }
 
     private static bool IsReadyToRunEnvSet()
@@ -63,8 +57,9 @@ public class JittedMethodsCountingTest
         return (string.IsNullOrEmpty(dotnetR2R) || dotnetR2R == "1") ? true : false;
     }
 
-    private static int RunHelloWorldApp(string appName, string jitOutputFile)
+    private static int RunHelloWorldApp(string appName)
     {
+        // The app's exit code is the number of jitted methods it got.
         int appExitCode = -1;
 
         // Set up our CoreRun call.
@@ -87,8 +82,8 @@ public class JittedMethodsCountingTest
             // the runtime to print them out with DOTNET_JitDisasmSummary, and
             // write them out to a file we can parse and investigate later
             // with DOTNET_JitStdOutFile.
-            startInfo.EnvironmentVariables.Add("DOTNET_JitDisasmSummary", "1");
-            startInfo.EnvironmentVariables.Add("DOTNET_JitStdOutFile", jitOutputFile);
+            // startInfo.EnvironmentVariables.Add("DOTNET_JitDisasmSummary", "1");
+            // startInfo.EnvironmentVariables.Add("DOTNET_JitStdOutFile", jitOutputFile);
 
             Console.WriteLine("\nLaunching Test App: {0} {1}", startInfo.FileName,
                                                                startInfo.Arguments);
@@ -98,38 +93,38 @@ public class JittedMethodsCountingTest
             app.WaitForExit();
 
             appExitCode = app.ExitCode;
-            Console.WriteLine("App Exit Code: {0}", appExitCode);
-            Console.WriteLine("Jitted Methods Generated File: {0}", jitOutputFile);
+            // Console.WriteLine("App Exit Code: {0}", appExitCode);
+            // Console.WriteLine("Jitted Methods Generated File: {0}", jitOutputFile);
         }
 
         return appExitCode;
     }
 
-    private static int GetNumberOfJittedMethods(string jitOutputFile)
-    {
-        string[] lines = File.ReadAllLines(jitOutputFile);
+//     private static int GetNumberOfJittedMethods(string jitOutputFile)
+//     {
+//         string[] lines = File.ReadAllLines(jitOutputFile);
 
-        // Print out the jitted methods from the app run previously. This is
-        // mostly done as additional logging to simplify potential bug investigations
-        // in the future.
+//         // Print out the jitted methods from the app run previously. This is
+//         // mostly done as additional logging to simplify potential bug investigations
+//         // in the future.
 
-        Console.WriteLine("\n========== App Jitted Methods Start ==========");
-        foreach (string line in lines)
-        {
-            Console.WriteLine(line);
-        }
-        Console.WriteLine("========== App Jitted Methods End ==========\n");
+//         Console.WriteLine("\n========== App Jitted Methods Start ==========");
+//         foreach (string line in lines)
+//         {
+//             Console.WriteLine(line);
+//         }
+//         Console.WriteLine("========== App Jitted Methods End ==========\n");
 
-        // The jitted methods are printed in the following format:
-        //
-        //    method number: method description
-        //
-        // This is why we split by ':' and parse the left side as a number.
+//         // The jitted methods are printed in the following format:
+//         //
+//         //    method number: method description
+//         //
+//         // This is why we split by ':' and parse the left side as a number.
 
-        string[] tokens = lines.Last().Split(":");
-        int numJittedMethods = Int32.Parse(tokens[0]);
-        Console.WriteLine("Total Jitted Methods: {0}\n", numJittedMethods);
+//         string[] tokens = lines.Last().Split(":");
+//         int numJittedMethods = Int32.Parse(tokens[0]);
+//         Console.WriteLine("Total Jitted Methods: {0}\n", numJittedMethods);
 
-        return numJittedMethods;
-    }
+//         return numJittedMethods;
+//     }
 }
