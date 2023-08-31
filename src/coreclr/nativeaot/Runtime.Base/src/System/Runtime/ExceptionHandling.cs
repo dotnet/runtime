0947cc226fc32a0ab65fcfa8e245ca11fcf61546
Jan Vorlicek <janvorli@microsoft.com>
2023-08-31 21:22:22 +0200

    Fix NativeAOT unhandled exception stack trace
    The recent change to add a new exception handling mechanism to coreclr
    has broken stack traces on unhandled exceptions in nativeaot.
    
    This change fixes it by reverting the part of the change that caused the
    problem and that turned out to not to be needed for the new exception
    handling either.
    
    Close #91298

--- src/coreclr/nativeaot/Runtime.Base/src/System/Runtime/ExceptionHandling.cs
+++ src/coreclr/nativeaot/Runtime.Base/src/System/Runtime/ExceptionHandling.cs
@@ -708,13 +708,13 @@ namespace System.Runtime
             uint startIdx = MaxTryRegionIdx;
             for (; isValid; isValid = frameIter.Next(&startIdx, &unwoundReversePInvoke))
             {
-                prevControlPC = frameIter.ControlPC;
-                prevOriginalPC = frameIter.OriginalControlPC;
-
                 // For GC stackwalking, we'll happily walk across native code blocks, but for EH dispatch, we
                 // disallow dispatching exceptions across native code.
                 if (unwoundReversePInvoke)
                     break;
+
+                prevControlPC = frameIter.ControlPC;
+                prevOriginalPC = frameIter.OriginalControlPC;
 
                 DebugScanCallFrame(exInfo._passNumber, frameIter.ControlPC, frameIter.SP);
 
