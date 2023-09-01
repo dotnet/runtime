8b7b8e58c0b5cb6dbc476dd05a43408071af5084
Tomas Rylek <trylek@microsoft.com>
2023-09-01 23:00:40 +0200

    Convert baseservices/exceptions to merged mode

--- src/tests/baseservices/exceptions/regressions/whidbeybeta2/349379/349379.cs
+++ src/tests/baseservices/exceptions/regressions/whidbeybeta2/349379/349379.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 using System;
+using Xunit;
 
 namespace TestCS
 {
@@ -10,7 +11,8 @@ namespace TestCS
         static string expectedExceptionString;
         static string expectedOuterExceptionString = "Foobar";
                 
-        static public int Main()
+        [Fact]
+        static public int TestEntryPoint()
         {
             Object foo = null;
              try
--- src/tests/baseservices/exceptions/stackoverflow/stackoverflow3.cs
+++ src/tests/baseservices/exceptions/stackoverflow/stackoverflow3.cs
@@ -1,15 +1,17 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 using System;
+using Xunit;
 
 namespace TestStackOverflow3
 {
-    class Program
+    public class Program
     {
         private const int MAX_RECURSIVE_CALLS = 1000000;
         static int ctr = 0;
 
-        public static void Main()
+        [Fact]
+        public static void TestEntryPoint()
         {
             Program ex = new Program();
             ex.Execute();
