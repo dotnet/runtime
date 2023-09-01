f4c145506918415aad5dfe5a6561a1a52c0da531
Aaron R Robinson <arobins@microsoft.com>
2023-09-01 08:14:51 -0700

    Use correct flag.

--- src/coreclr/debug/di/process.cpp
+++ src/coreclr/debug/di/process.cpp
@@ -181,7 +181,7 @@ STDAPI DLLEXPORT OpenVirtualProcessImpl2(
     CLR_DEBUGGING_PROCESS_FLAGS* pFlagsOut)
 {
 #ifdef TARGET_WINDOWS
-    HMODULE hDac = LoadLibraryExW(pDacModulePath, NULL, LOAD_LIBRARY_SEARCH_SYSTEM32);
+    HMODULE hDac = LoadLibraryExW(pDacModulePath, NULL, LOAD_WITH_ALTERED_SEARCH_PATH);
 #else
     HMODULE hDac = LoadLibraryW(pDacModulePath);
 #endif // !TARGET_WINDOWS
