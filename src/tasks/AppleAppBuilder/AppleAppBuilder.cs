f90f749d41f20563bb563bba29b27b98e0ceb8bc
Filip Navara <navara@emclient.com>
2023-09-02 22:11:41 +0200

    Cleanup

--- src/tasks/AppleAppBuilder/AppleAppBuilder.cs
+++ src/tasks/AppleAppBuilder/AppleAppBuilder.cs
@@ -204,7 +204,7 @@ public class AppleAppBuilderTask : Task
             if (!string.IsNullOrEmpty(MonoRuntimeHeaders))
                 throw new ArgumentException($"Property \"{nameof(MonoRuntimeHeaders)}\" is not supported with {Runtime} runtime and will be ignored.");
 
-            if (!string.IsNullOrEmpty(MainLibraryFileName) && Runtime == "NativeAOT")
+            if (!string.IsNullOrEmpty(MainLibraryFileName) && targetRuntime == TargetRuntime.NativeAOT)
                 throw new ArgumentException($"Property \"{nameof(MainLibraryFileName)}\" is not supported with {Runtime} runtime and will be ignored.");
 
             if (ForceInterpreter)
@@ -213,7 +213,7 @@ public class AppleAppBuilderTask : Task
             if (ForceAOT)
                 throw new ArgumentException($"Property \"{nameof(ForceAOT)}\" is not supported with {Runtime} runtime and will be ignored.");
 
-            if (!string.IsNullOrEmpty(RuntimeComponents) && Runtime == "NativeAOT")
+            if (!string.IsNullOrEmpty(RuntimeComponents) && targetRuntime == TargetRuntime.NativeAOT)
                 throw new ArgumentException($"Property \"{nameof(RuntimeComponents)}\" is not supported with {Runtime} runtime and will be ignored.");
 
             if (!string.IsNullOrEmpty(DiagnosticPorts))
