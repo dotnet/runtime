15210ce71f4f28df043f6eba78f343178d43dcf6
Kunal Pathak <Kunal.Pathak@microsoft.com>
2023-09-07 03:52:25 -0700

    Pass the right size to check if immediate will fit or not

--- src/coreclr/jit/codegenarm64.cpp
+++ src/coreclr/jit/codegenarm64.cpp
@@ -335,10 +335,14 @@ bool CodeGen::genInstrWithConstant(instruction ins,
             break;
 
         case INS_ldrsb:
-        case INS_ldrsh:
-        case INS_ldrsw:
         case INS_ldrb:
+            immFitsInIns = emitter::emitIns_valid_imm_for_ldst_offset(imm, EA_1BYTE);
+            break;
+        case INS_ldrsh:
         case INS_ldrh:
+            immFitsInIns = emitter::emitIns_valid_imm_for_ldst_offset(imm, EA_2BYTE);
+            break;
+        case INS_ldrsw:
         case INS_ldr:
             immFitsInIns = emitter::emitIns_valid_imm_for_ldst_offset(imm, size);
             break;
