2bd5ea4b4a7126b7cd020522738aa59245a79b86
Juan Sebastian Hoyos Ayala <juan.hoyos@microsoft.com>
2023-09-06 17:38:27 -0700

    Attempt to collect dumps of update process

--- eng/collect_vsinfo.ps1
+++ eng/collect_vsinfo.ps1
@@ -13,6 +13,10 @@ Param (
 
 . $PSScriptRoot/common/tools.ps1
 
+$ProgressPreference = "SilentlyContinue"
+$LogDir = Join-Path $LogDir $ArchiveRunName
+mkdir $LogDir
+
 $vscollect_uri="http://aka.ms/vscollect.exe"
 $vscollect="$env:TEMP\vscollect.exe"
 
@@ -29,9 +33,6 @@ if (-not (Test-Path $vscollect)) {
 }
 
 &"$vscollect"
-
-$LogDir = Join-Path $LogDir $ArchiveRunName
-mkdir $LogDir
 Move-Item $env:TEMP\vslogs.zip "$LogDir"
 
 $vswhere = "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe"
@@ -41,4 +42,24 @@ if (-not (Test-Path -Path "$vswhere" -PathType Leaf))
     exit 1
 }
 
- &"$vswhere" -all -prerelease -products * |  Tee-Object -FilePath "$LogDir\vs_where.log"
+&"$vswhere" -all -prerelease -products * |  Tee-Object -FilePath "$LogDir\vs_where.log"
+
+$vsdir = &"$vswhere" -latest -prerelease -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath
+
+if (-not (Test-Path $vsdir))
+{
+    $procDumpDir = Join-Path $ToolsDir "procdump"
+    $procDumpToolPath = Join-Path $procDumpDir "procdump.exe"
+    $procdump_uri = "https://download.sysinternals.com/files/Procdump.zip"
+
+    if (-not (Test-Path $procDumpToolPath)) {
+        Retry({
+            Write-Host "GET $procdump_uri"
+            Invoke-WebRequest $procdump_uri -OutFile "$TempDir\Procdump.zip" -UseBasicParsing
+        })
+
+        Expand-Archive -Path "$TempDir\Procdump.zip" $procDumpDir
+    }
+
+    &"$procDumpToolPath" -ma -accepteula VSIXAutoUpdate.exe "$LogDir"
+}
