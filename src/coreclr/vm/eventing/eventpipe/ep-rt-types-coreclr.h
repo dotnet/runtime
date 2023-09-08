0ea13251208e94ed9f6719c351dbbe07ee012a4a
David Mason <davmason@microsoft.com>
2023-09-08 14:42:28 -0700

    Revert previous changes

--- src/coreclr/vm/eventing/eventpipe/ep-rt-types-coreclr.h
+++ src/coreclr/vm/eventing/eventpipe/ep-rt-types-coreclr.h
@@ -20,13 +20,6 @@
 #define EP_ASSERT(expr)
 #endif
 
-#define EP_FAILFAST(msg) \
-	do \
-	{ \
-        SString converter(SString::Literal, msg); \
-		EEPOLICY_HANDLE_FATAL_ERROR_WITH_MESSAGE(COR_E_FAILFAST, converter.GetUnicode()); \
-	} while(FALSE)
-
 #undef EP_UNREACHABLE
 #define EP_UNREACHABLE(msg) do { UNREACHABLE_MSG(msg); } while (0)
 
