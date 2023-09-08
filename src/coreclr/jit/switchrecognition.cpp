56b680d5eab8bd2fffafcca794aac5ab433d156c
EgorBo <egorbo@gmail.com>
2023-09-08 20:24:55 +0200

    code formatting

--- src/coreclr/jit/switchrecognition.cpp
+++ src/coreclr/jit/switchrecognition.cpp
@@ -121,13 +121,13 @@ bool Compiler::optSwitchConvert(BasicBlock* firstBlock, int testsCount, ssize_t*
         }
 
 #ifndef TARGET_XARCH
-		if ((newMaxValue - newMinValue) > 16)
-		{
-			// Currently, we rely on TryLowerAndOpToExtractLowestSetBit to lower the switch
-			// and it only supports XARCH targets. For non-XARCH targets, we only support
-			// low deltas to avoid emitting huge jump tables.
-		    break;
-		}
+        if ((newMaxValue - newMinValue) > 16)
+        {
+            // Currently, we rely on TryLowerAndOpToExtractLowestSetBit to lower the switch
+            // and it only supports XARCH targets. For non-XARCH targets, we only support
+            // low deltas to avoid emitting huge jump tables.
+            break;
+        }
 #endif
 
         // TODO: currently we mainly focus on creating the shape that is then expanded into a bit test
