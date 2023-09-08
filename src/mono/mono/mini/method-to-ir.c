4633336efad3a3540494720b5a786c2e904b4350
Jan Dupej <jandupej@microsoft.com>
2023-09-08 14:01:27 +0200

    Fixing ro_type.

--- src/mono/mono/mini/method-to-ir.c
+++ src/mono/mono/mini/method-to-ir.c
@@ -9894,6 +9894,8 @@ calli_end:
 			MonoType *ftype;
 			MonoInst *store_val = NULL;
 			MonoInst *thread_ins;
+			int ro_type;
+			gboolean is_typeload_failure = FALSE;
 
 			is_instance = (il_op == MONO_CEE_LDFLD || il_op == MONO_CEE_LDFLDA || il_op == MONO_CEE_STFLD);
 			if (is_instance) {
@@ -9923,6 +9925,7 @@ calli_end:
 				field = mono_field_from_token_checked (image, token, &klass, generic_context, cfg->error);
 				if (!field || CLASS_HAS_FAILURE (klass)) {
 						HANDLE_TYPELOAD_ERROR (cfg, klass);
+						is_typeload_failure = TRUE;
 
 						// Reached only in AOT. Cannot turn a token into a class. We silence the compilation error
 						// and generate a runtime exception.
@@ -10333,7 +10336,7 @@ calli_end:
 			} else {
 				gboolean is_const = FALSE;
 				MonoVTable *vtable = NULL;
-
+				
 				addr = NULL;
 				if (!context_used) {
 					vtable = mono_class_vtable_checked (klass, cfg->error);
@@ -10342,14 +10345,18 @@ calli_end:
 				}
 				if ((ftype->attrs & FIELD_ATTRIBUTE_INIT_ONLY) && (((addr = mono_aot_readonly_field_override (field)) != NULL) ||
 						(!context_used && !cfg->compile_aot && vtable->initialized))) {
-					int ro_type = ftype->type;
 					if (!addr)
 						addr = mono_static_field_get_addr (vtable, field);
 
 fld_emit_stage:
+					ro_type = ftype->type;
 					if (ro_type == MONO_TYPE_VALUETYPE && m_class_is_enumtype (ftype->data.klass)) {
 						ro_type = mono_class_enum_basetype_internal (ftype->data.klass)->type;
 					}
+
+					gint64 dummy_zero = 0;
+					if (is_typeload_failure && !addr)
+						addr = &dummy_zero;
 
 					GSHAREDVT_FAILURE (il_op);
 
