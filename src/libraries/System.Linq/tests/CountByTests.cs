256f96a8097a431b54c79636a962276066517805
Emmanuel ANDRE <2341261+manandre@users.noreply.github.com>
2023-09-13 21:47:58 +0200

    Make test results more explicit

--- src/libraries/System.Linq/tests/CountByTests.cs
+++ src/libraries/System.Linq/tests/CountByTests.cs
@@ -107,13 +107,23 @@ namespace System.Linq.Tests
                 source: new string[] { "Bob", "bob", "tim", "Bob", "Tim" },
                 keySelector: x => x,
                 null,
-                expected: new string[] { "Bob", "bob", "tim", "Tim" }.ToDictionary(x => x, x => x == "Bob" ? 2 : 1));
+                expected: new Dictionary<string, int>()
+                {
+                    { "Bob", 2 },
+                    { "bob", 1 },
+                    { "tim", 1 },
+                    { "Tim", 1 }
+                });
 
             yield return WrapArgs(
                 source: new string[] { "Bob", "bob", "tim", "Bob", "Tim" },
                 keySelector: x => x,
                 StringComparer.OrdinalIgnoreCase,
-                expected: new string[] { "Bob", "tim" }.ToDictionary(x => x, x => x == "Bob" ? 3 : 2));
+                expected: new Dictionary<string, int>()
+                {
+                    { "Bob", 3 },
+                    { "tim", 2 }
+                });
 
             yield return WrapArgs(
                 source: new (string Name, int Age)[] { ("Tom", 20), ("Dick", 30), ("Harry", 40) },
@@ -125,7 +135,11 @@ namespace System.Linq.Tests
                 source: new (string Name, int Age)[] { ("Tom", 20), ("Dick", 20), ("Harry", 40) },
                 keySelector: x => x.Age,
                 comparer: null,
-                expected: new int[] { 20, 40 }.ToDictionary(x => x, x => x == 20 ? 2 : 1));
+                expected: new Dictionary<int, int>()
+                {
+                    { 20, 2 },
+                    { 40, 1 }
+                });
 
             yield return WrapArgs(
                 source: new (string Name, int Age)[] { ("Bob", 20), ("bob", 30), ("Harry", 40) },
@@ -137,7 +151,11 @@ namespace System.Linq.Tests
                 source: new (string Name, int Age)[] { ("Bob", 20), ("bob", 30), ("Harry", 40) },
                 keySelector: x => x.Name,
                 comparer: StringComparer.OrdinalIgnoreCase,
-                expected: new string[] { "Bob", "Harry" }.ToDictionary(x => x, x => x == "Bob" ? 2 : 1));
+                expected: new Dictionary<string, int>()
+                {
+                    { "Bob", 2 },
+                    { "Harry", 1 }
+                });
 
             object[] WrapArgs<TSource, TKey>(IEnumerable<TSource> source, Func<TSource, TKey> keySelector, IEqualityComparer<TKey>? comparer, IEnumerable<KeyValuePair<TKey, int>> expected)
                 => new object[] { source, keySelector, comparer, expected };
