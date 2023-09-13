c4140dff9d8e2393e62df9d1afa36a1f458a8a36
Tarek Mahmoud Sayed <tarekms@microsoft.com>
2023-09-13 14:17:25 -0700

    Add missed empty lines

--- src/libraries/System.Diagnostics.DiagnosticSource/tests/TestNotSupported.cs
+++ src/libraries/System.Diagnostics.DiagnosticSource/tests/TestNotSupported.cs
@@ -24,11 +24,13 @@ namespace System.Diagnostics.Metrics.Tests
             RemoteExecutor.Invoke((val) =>
             {
                 bool isSupported = bool.Parse(val);
+
                 Meter meter = new Meter("IsSupportedTest");
                 Counter<long> counter = meter.CreateCounter<long>("counter");
                 bool instrumentsPublished = false;
                 bool instrumentCompleted = false;
                 long counterValue = 100;
+
                 using (MeterListener listener = new MeterListener
                 {
                     InstrumentPublished = (instruments, theListener) => instrumentsPublished = true,
@@ -38,10 +40,13 @@ namespace System.Diagnostics.Metrics.Tests
                     listener.EnableMeasurementEvents(counter, null);
                     listener.SetMeasurementEventCallback<long>((inst, measurement, tags, state) => counterValue = measurement);
                     listener.Start();
+
                     Assert.Equal(isSupported, counter.Enabled);
+
                     counter.Add(20);
                 }
                 meter.Dispose();
+
                 Assert.Equal(isSupported, instrumentsPublished);
                 Assert.Equal(isSupported, instrumentCompleted);
                 Assert.Equal(isSupported ? 20 : 100, counterValue);
