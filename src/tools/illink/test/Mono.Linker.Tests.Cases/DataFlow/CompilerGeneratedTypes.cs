aa77980d526ee94231f4c05f3e1385e6e5b639ac
Sven Boemer <sbomer@gmail.com>
2023-09-14 21:49:51 +0000

    Fix test for analyzer

--- src/tools/illink/test/Mono.Linker.Tests.Cases/DataFlow/CompilerGeneratedTypes.cs
+++ src/tools/illink/test/Mono.Linker.Tests.Cases/DataFlow/CompilerGeneratedTypes.cs
@@ -361,7 +361,9 @@ namespace Mono.Linker.Tests.Cases.DataFlow
 					(Func<T, T> x) => v => x(v);
 			}
 
-			[ExpectedWarning ("IL2091", "T", nameof (Container<T>), nameof (DynamicallyAccessedMemberTypes.PublicMethods))]
+			[ExpectedWarning ("IL2091", "T", nameof (Container<T>), nameof (DynamicallyAccessedMemberTypes.PublicMethods),
+				// https://github.com/dotnet/runtime/issues/84918
+				ProducedBy = Tool.Trimmer | Tool.NativeAot)]
 			public static void Test<T> ()
 			{
 				Container<T>.NestedLambda ((T t) => t) (default (T));
