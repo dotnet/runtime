162f08aa58dd9898003387e1a49e4d025ffd9a34
Zoltan Varga <vargaz@gmail.com>
2023-09-19 00:26:23 -0400

    Fix mingw package path.

--- src/mono/wasi/provision.ps1
+++ src/mono/wasi/provision.ps1
@@ -13,7 +13,7 @@ Set-StrictMode -version 2.0
 $ErrorActionPreference='Stop'
 
 New-Item -Path $WasiSdkPath -ItemType "directory"
-Invoke-WebRequest -Uri $WasiSdkUrl -OutFile ./wasi-sdk-$WasiSdkVersion.0.m-mingw.tar.gz
-tar --strip-components=1 -xzf ./wasi-sdk-$WasiSdkVersion.0.m-mingw.tar.gz -C $WasiSdkPath
+Invoke-WebRequest -Uri $WasiSdkUrl -OutFile ./wasi-sdk-$WasiSdkVersion.0-mingw.tar.gz
+tar --strip-components=1 -xzf ./wasi-sdk-$WasiSdkVersion.0-mingw.tar.gz -C $WasiSdkPath
 Copy-Item $WasiLocalPath/wasi-sdk-version.txt $WasiSdkPath/wasi-sdk-version.txt
 Remove-Item ./wasi-sdk-$WasiSdkVersion.0-mingw.tar.gz -fo
