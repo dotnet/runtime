99fe5af3e8643f26cd0705d26a1c740c2feb4999
TIHan <lol.tihan@gmail.com>
2023-09-20 13:33:23 -0700

    Remove comment

--- src/tests/JIT/Regression/JitBlue/Runtime_92201/Runtime_92201.cs
+++ src/tests/JIT/Regression/JitBlue/Runtime_92201/Runtime_92201.cs
@@ -1,13 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-/******************************************************************************
- * This file is auto-generated from a template file by the GenerateTests.csx  *
- * script in tests\src\JIT\HardwareIntrinsics\X86\Shared. In order to make    *
- * changes, please update the corresponding template and run according to the *
- * directions listed in the file.                                             *
- ******************************************************************************/
-
 using System;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
