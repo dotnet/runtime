fa7e30e8b92ae304863bdc9926a15cdeefc7d86c
Eric StJohn <ericstj@microsoft.com>
2023-09-20 18:30:28 -0700

    Address feedback

--- src/libraries/Microsoft.Extensions.Configuration.Binder/tests/Common/ConfigurationBinderTests.TestClasses.cs
+++ src/libraries/Microsoft.Extensions.Configuration.Binder/tests/Common/ConfigurationBinderTests.TestClasses.cs
@@ -888,5 +888,11 @@ namespace Microsoft.Extensions
             }
         }
 
+        public class SimplePoco
+        {
+            public string A { get; set; }
+            public string B { get; set; }
+        }
+
     }
 }
