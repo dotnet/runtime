f1584dd11572a5576c99d75d08e6cd7aa63e111e
Tanner Gooding <tagoo@outlook.com>
2023-09-21 08:26:26 -0700

    Ensure that embedded broadcast checks the base type of the parent node

---
+++ src/tests/JIT/Regression/JitBlue/Runtime_92357/Runtime_92357.cs
@@ -0,0 +1,29 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
+using Xunit;
+
+public static class Runtime_92357
+{
+    [Fact]
+    [MethodImpl(MethodImplOptions.AggressiveOptimization)]
+    public static void Problem()
+    {
+        int y = 5;
+
+        Vector256<short> actual = Test(Vector256.Create((short)1), ref y);
+        Vector256<short> expected = Vector256.Create(10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0, 10, 0);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.AggressiveOptimization)]
+    public static Vector256<short> Test(Vector256<short> x, ref int y)
+    {
+        return Avx2.MultiplyLow(x + x, Vector256.Create(y).AsInt16());
+    }
+}
