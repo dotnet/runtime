714ea8a8edf3fc6bb4858c5d46f759fb0a3f96d7
Will Smith <lol.tihan@gmail.com>
2023-09-21 15:18:48 -0700

    Update Runtime_92349.cs

--- src/tests/JIT/Regression/JitBlue/Runtime_92349/Runtime_92349.cs
+++ src/tests/JIT/Regression/JitBlue/Runtime_92349/Runtime_92349.cs
@@ -9,13 +9,13 @@ using Xunit;
 public static class Runtime_92349
 {
     [MethodImpl(MethodImplOptions.AggressiveOptimization)]
-    unsafe static void Test(byte* pValue)
+    public unsafe static void Test(byte* pValue)
     {
         *pValue = (byte)Sse2.ConvertToInt32(Vector128.Create(-10, 0, 0, 0));
     }
 
     [Fact]
-    unsafe static void EntryPoint()
+    public unsafe static void EntryPoint()
     {
         ulong value = 0;
         Test((byte*)Unsafe.AsPointer(ref value));
