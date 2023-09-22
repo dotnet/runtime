6039b239fbcd1aa09d1a4c049aaf00ffcd3d3ddb
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2023-09-22 09:34:00 +0200

    Set stack size on MUSL with PT_GNU_STACK header
    Fixes #72920.

--- src/coreclr/nativeaot/Bootstrap/main.cpp
+++ src/coreclr/nativeaot/Bootstrap/main.cpp
@@ -197,26 +197,12 @@ static int InitializeRuntime()
 
 #ifndef NATIVEAOT_DLL
 
-#ifdef ENSURE_PRIMARY_STACK_SIZE
-__attribute__((noinline, optnone))
-static void EnsureStackSize(int stackSize)
-{
-    volatile char* s = (char*)_alloca(stackSize);
-    *s = 0;
-}
-#endif // ENSURE_PRIMARY_STACK_SIZE
-
 #if defined(_WIN32)
 int __cdecl wmain(int argc, wchar_t* argv[])
 #else
 int main(int argc, char* argv[])
 #endif
 {
-#ifdef ENSURE_PRIMARY_STACK_SIZE
-    // TODO: https://github.com/dotnet/runtimelab/issues/791
-    EnsureStackSize(1536 * 1024);
-#endif
-
     int initval = InitializeRuntime();
     if (initval != 0)
         return initval;
