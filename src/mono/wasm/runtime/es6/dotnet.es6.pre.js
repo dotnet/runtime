68e954d4635d039857fcd1a6f20e978320dea7dd
Ankit Jain <radical@gmail.com>
2023-09-27 16:37:24 -0400

    address review feedback

--- src/mono/wasm/runtime/es6/dotnet.es6.pre.js
+++ src/mono/wasm/runtime/es6/dotnet.es6.pre.js
@@ -5,4 +5,3 @@ Module["getMemory"] = function () { return wasmMemory; }
 Module["getWasmIndirectFunctionTable"] = function () { return wasmTable; }
 createDotnetRuntime = Module = createDotnetRuntime(Module);
 Module["getMemory"] = function () { return wasmMemory; }
-Module["getWasmIndirectFunctionTable"] = function () { return wasmTable; }
