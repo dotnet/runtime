273f9f4578e7e449eeef53cd806db19d84ff64bf
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2023-09-27 23:50:44 +0200

    Stupid analyzer is stupid

--- src/coreclr/tools/aot/ILCompiler.Compiler/Compiler/TypePreinit.cs
+++ src/coreclr/tools/aot/ILCompiler.Compiler/Compiler/TypePreinit.cs
@@ -3126,6 +3126,7 @@ namespace ILCompiler
         }
     }
 
+#pragma warning disable SA1400 // Element 'Extensions' should declare an access modifier
     file static class Extensions
     {
         public static StackValueKind WithNormalizedNativeInt(this StackValueKind kind, TypeSystemContext context)
