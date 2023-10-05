dotnet/runtime

commit 3b0cd57dc2b01dbb5c8a0d740a33a4b0aed470bf

SPMI: Simplify and improve reporting of context information (#92824)
Currently we have multiple separate mechanisms to report information
back from superpmi.exe:
1. -baseMetricsSummary/metricsSummary, which outputs a .csv file with
   aggregated statics for all contexts from the perspective of the base
   JIT
2. -diffMetricsSummary, which is the corresponding for the diff JIT
   when diffing
3. -diffsInfo, which during diffing will output a .csv with individual
   rows for every context that had diffs in it

This PR replaces these three mechanisms with a -details argument. When
passed, superpmi.exe will write a .csv file to the specified path that
contains a row for every context.

The arg is supported in both replay and diff mode but creates .csv files
with slightly different formats for these. For replays the header output
is:
```
Context,Context size,Result,MinOpts,Size,Instructions
```

For diffs the output is:
```
Context,Context size,Base result,Diff result,MinOpts,Has diff,Base size,Diff size,Base instructions,Diff instructions
```

superpmi.py is changed to utilize this new output instead, which
involves computing some of the same details we were getting from the
metrics summaries before.

Prerequisite for #85755

src/coreclr/tools/superpmi/superpmi/superpmi.cpp
@@ -18,7 +18,6 @@
 #include "mclist.h"
 #include "methodstatsemitter.h"
 #include "spmiutil.h"
-#include "metricssummary.h"
 #include "fileio.h"
 
 extern int doParallelSuperPMI(CommandLine::Options& o);
@@ -122,19 +121,57 @@ static NearDifferResult InvokeNearDiffer(NearDiffer*           nearDiffer,
     return param.result;
 }
 
+static const char* ResultToString(ReplayResult result)
+{
+    switch (result)
+    {
+    case ReplayResult::Success:
+        return "Success";
+    case ReplayResult::Error:
+        return "Error";
+    case ReplayResult::Miss:
+        return "Miss";
+    default:
+        return "Unknown";
+    }
+}
+
 static bool PrintDiffsCsvHeader(FileWriter& fw)
 {
-    return fw.Printf("Context,Context size,Base size,Diff size,Base instructions,Diff instructions\n");
+    return fw.Printf("Context,Context size,Base result,Diff result,MinOpts,Has diff,Base size,Diff size,Base instructions,Diff instructions\n");
 }
 
 static bool PrintDiffsCsvRow(
     FileWriter& fw,
-    int context,
-    uint32_t contextSize,
-    long long baseSize, long long diffSize,
-    long long baseInstructions, long long diffInstructions)
+    int context, uint32_t contextSize,
+    const ReplayResults& baseRes,
+    const ReplayResults& diffRes,
+    bool hasDiff)
 {
-    return fw.Printf("%d,%u,%lld,%lld,%lld,%lld\n", context, contextSize, baseSize, diffSize, baseInstructions, diffInstructions);
+    return fw.Printf("%d,%u,%s,%s,%s,%s,%u,%u,%lld,%lld\n",
+        context, contextSize,
+        ResultToString(baseRes.Result), ResultToString(diffRes.Result),
+        baseRes.IsMinOpts ? "True" : "False",
+        hasDiff ? "True" : "False",
+        baseRes.NumCodeBytes, diffRes.NumCodeBytes,
+        baseRes.NumExecutedInstructions, diffRes.NumExecutedInstructions);
+}
+
+static bool PrintReplayCsvHeader(FileWriter& fw)
+{
+    return fw.Printf("Context,Context size,Result,MinOpts,Size,Instructions\n");
+}
+
+static bool PrintReplayCsvRow(
+    FileWriter& fw,
+    int context, uint32_t contextSize,
+    const ReplayResults& res)
+{
+    return fw.Printf("%d,%u,%s,%s,%u,%lld\n",
+        context, contextSize,
+        ResultToString(res.Result),
+        res.IsMinOpts ? "True" : "False",
+        res.NumCodeBytes, res.NumExecutedInstructions);
 }
 
 // Run superpmi. The return value is as follows:
@@ -161,10 +198,8 @@ int __cdecl main(int argc, char* argv[])
     SimpleTimer st3;
     SimpleTimer st4;
     st2.Start();
-    JitInstance::Result res, res2 = JitInstance::RESULT_ERROR;
-    HRESULT             hr  = E_FAIL;
-    MethodContext*      mc  = nullptr;
-    JitInstance *       jit = nullptr, *jit2 = nullptr;
+    JitInstance* jit = nullptr;
+    JitInstance* jit2 = nullptr;
     MethodStatsEmitter* methodStatsEmitter = nullptr;
 
 #ifdef SuperPMI_ChewMemory
@@ -182,7 +217,7 @@ int __cdecl main(int argc, char* argv[])
 
     bool   collectThroughput = false;
     MCList failingToReplayMCL;
-    FileWriter diffCsv;
+    FileWriter detailsCsv;
 
     CommandLine::Options o;
     if (!CommandLine::Parse(argc, argv, &o))
@@ -239,11 +274,11 @@ int __cdecl main(int argc, char* argv[])
     {
         failingToReplayMCL.InitializeMCL(o.mclFilename);
     }
-    if (o.diffsInfo != nullptr)
+    if (o.details != nullptr)
     {
-        if (!FileWriter::CreateNew(o.diffsInfo, &diffCsv))
+        if (!FileWriter::CreateNew(o.details, &detailsCsv))
         {
-            LogError("Could not create file %s", o.diffsInfo);
+            LogError("Could not create file %s", o.details);
             return (int)SpmiResult::GeneralFailure;
         }
     }
@@ -264,6 +299,7 @@ int __cdecl main(int argc, char* argv[])
     int failToReplayCount = 0;
     int errorCount        = 0;
     int errorCount2       = 0;
+    int diffsCount        = 0;
     int missingCount      = 0;
     int index             = 0;
     int excludedCount     = 0;
@@ -279,13 +315,17 @@ int __cdecl main(int argc, char* argv[])
         }
     }
 
-    if (o.diffsInfo != nullptr)
+    if (o.details != nullptr)
     {
-        PrintDiffsCsvHeader(diffCsv);
+        if (o.applyDiff)
+        {
+            PrintDiffsCsvHeader(detailsCsv);
+        }
+        else
+        {
+            PrintReplayCsvHeader(detailsCsv);
+        }
     }
-
-    MetricsSummaries totalBaseMetrics;
-    MetricsSummaries totalDiffMetrics;
 
     while (true)
     {
@@ -305,7 +345,7 @@ int __cdecl main(int argc, char* argv[])
             if (o.applyDiff)
             {
                 LogVerbose(" %2.1f%% - Loaded %d  Jitted %d  Diffs %d  FailedCompile %d at %d per second",
-                           reader->PercentComplete(), loadedCount, jittedCount, totalBaseMetrics.Overall.NumContextsWithDiffs,
+                           reader->PercentComplete(), loadedCount, jittedCount, diffsCount,
                            failToReplayCount, (int)((double)500 / st1.GetSeconds()));
             }
             else
@@ -321,6 +361,7 @@ int __cdecl main(int argc, char* argv[])
 
         loadedCount++;
         const int mcIndex = reader->GetMethodContextIndex();
+        MethodContext* mc = nullptr;
         if (!MethodContext::Initialize(mcIndex, mcb.buff, mcb.size, &mc))
         {
             return (int)SpmiResult::GeneralFailure;
@@ -386,30 +427,21 @@ int __cdecl main(int argc, char* argv[])
             }
         }
 
-        MetricsSummary baseMetrics;
-        bool isMinOpts;
         jittedCount++;
         st3.Start();
-        res = jit->CompileMethod(mc, reader->GetMethodContextIndex(), collectThroughput, &baseMetrics, &isMinOpts);
+        ReplayResults res = jit->CompileMethod(mc, reader->GetMethodContextIndex(), collectThroughput);
         st3.Stop();
         LogDebug("Method %d compiled%s in %fms, result %d",
             reader->GetMethodContextIndex(), (o.nameOfJit2 == nullptr) ? "" : " by JIT1", st3.GetMilliseconds(), res);
 
-        MetricsSummary& totalBaseMetricsOpts = isMinOpts ? totalBaseMetrics.MinOpts : totalBaseMetrics.FullOpts;
-        MetricsSummary& totalDiffMetricsOpts = isMinOpts ? totalDiffMetrics.MinOpts : totalDiffMetrics.FullOpts;
-
-        totalBaseMetrics.Overall.AggregateFrom(baseMetrics);
-
-        if (res == JitInstance::RESULT_SUCCESS)
+        if (res.Result == ReplayResult::Success)
         {
-            totalBaseMetricsOpts.AggregateFrom(baseMetrics);
-
             if (Logger::IsLogLevelEnabled(LOGLEVEL_DEBUG))
             {
                 mc->cr->dumpToConsole(); // Dump the compile results if doing debug logging
             }
         }
-        else if (res == JitInstance::RESULT_ERROR)
+        else if (res.Result == ReplayResult::Success)
         {
             errorCount++;
             LogError("Method %d of size %d failed to load and compile correctly%s (%s).",
@@ -448,9 +480,8 @@ int __cdecl main(int argc, char* argv[])
             }
         }
 
-        MetricsSummary diffMetrics;
-
-        res2 = JitInstance::RESULT_SUCCESS;
+        ReplayResults res2;
+        res2.Result = ReplayResult::Success;
         if (o.nameOfJit2 != nullptr)
         {
             // Lets get the results for the 2nd JIT
@@ -459,25 +490,20 @@ int __cdecl main(int argc, char* argv[])
             crl    = mc->cr;
             mc->cr = new CompileResult();
 
-            bool isMinOptsDiff;
             st4.Start();
-            res2 = jit2->CompileMethod(mc, reader->GetMethodContextIndex(), collectThroughput, &diffMetrics, &isMinOptsDiff);
+            res2 = jit2->CompileMethod(mc, reader->GetMethodContextIndex(), collectThroughput);
             st4.Stop();
             LogDebug("Method %d compiled by JIT2 in %fms, result %d", reader->GetMethodContextIndex(),
                      st4.GetMilliseconds(), res2);
 
-            totalDiffMetrics.Overall.AggregateFrom(diffMetrics);
-
-            if (res2 == JitInstance::RESULT_SUCCESS)
+            if (res2.Result == ReplayResult::Success)
             {
-                totalDiffMetricsOpts.AggregateFrom(diffMetrics);
-
                 if (Logger::IsLogLevelEnabled(LOGLEVEL_DEBUG))
                 {
                     mc->cr->dumpToConsole(); // Dump the compile results if doing debug logging
                 }
             }
-            else if (res2 == JitInstance::RESULT_ERROR)
+            else if (res2.Result == ReplayResult::Error)
             {
                 errorCount2++;
                 LogError("Method %d of size %d failed to load and compile correctly by JIT2 (%s).",
@@ -490,11 +516,11 @@ int __cdecl main(int argc, char* argv[])
             }
         }
 
-        if ((res == JitInstance::RESULT_SUCCESS) && (res2 == JitInstance::RESULT_SUCCESS))
+        if ((res.Result == ReplayResult::Success) && (res2.Result == ReplayResult::Success))
         {
             if (collectThroughput)
             {
-                if (o.nameOfJit2 != nullptr && res2 == JitInstance::RESULT_SUCCESS)
+                if ((o.nameOfJit2 != nullptr) && (res2.Result == ReplayResult::Success))
                 {
                     // TODO-Bug?: bug in getting the lowest cycle time??
                     ULONGLONG dif1, dif2, dif3, dif4;
@@ -589,6 +615,7 @@ int __cdecl main(int argc, char* argv[])
 
             if (o.applyDiff)
             {
+                NearDifferResult diffResult = NearDifferResult::Failure;
                 // We need at least two compile results to diff: they can either both come from JIT
                 // invocations, or one can be loaded from the method context file.
 
@@ -600,30 +627,16 @@ int __cdecl main(int argc, char* argv[])
                 }
                 else
                 {
-                    NearDifferResult result = InvokeNearDiffer(&nearDiffer, &mc, &crl, &reader);
+                    diffResult = InvokeNearDiffer(&nearDiffer, &mc, &crl, &reader);
 
-                    switch (result)
+                    switch (diffResult)
                     {
                         case NearDifferResult::SuccessWithDiff:
-                            totalBaseMetrics.Overall.NumContextsWithDiffs++;
-                            totalDiffMetrics.Overall.NumContextsWithDiffs++;
-
-                            totalBaseMetricsOpts.NumContextsWithDiffs++;
-                            totalDiffMetricsOpts.NumContextsWithDiffs++;
-
+                            diffsCount++;
                             // This is a difference in ASM outputs from Jit1 & Jit2 and not a playback failure
-                            // We will add this MC to the diffs info if there is one.
-                            // Otherwise this will end up in failingMCList
-                            if (o.diffsInfo != nullptr)
-                            {
-                                PrintDiffsCsvRow(
-                                    diffCsv,
-                                    reader->GetMethodContextIndex(),
-                                    mcb.size,
-                                    baseMetrics.NumCodeBytes, diffMetrics.NumCodeBytes,
-                                    baseMetrics.NumExecutedInstructions, diffMetrics.NumExecutedInstructions);
-                            }
-                            else if (o.mclFilename != nullptr)
+                            // We will add this MC to the details if there is one below.
+                            // Otherwise add it in the failingMCList here.
+                            if (o.details == nullptr)
                             {
                                 failingToReplayMCL.AddMethodToMCL(reader->GetMethodContextIndex());
                             }
@@ -637,18 +650,27 @@ int __cdecl main(int argc, char* argv[])
 
                             break;
                     }
+                }
 
-                    totalBaseMetrics.Overall.NumDiffedCodeBytes += baseMetrics.NumCodeBytes;
-                    totalDiffMetrics.Overall.NumDiffedCodeBytes += diffMetrics.NumCodeBytes;
-
-                    totalBaseMetricsOpts.NumDiffedCodeBytes += baseMetrics.NumCodeBytes;
-                    totalDiffMetricsOpts.NumDiffedCodeBytes += diffMetrics.NumCodeBytes;
-
-                    totalBaseMetrics.Overall.NumDiffExecutedInstructions += baseMetrics.NumExecutedInstructions;
-                    totalDiffMetrics.Overall.NumDiffExecutedInstructions += diffMetrics.NumExecutedInstructions;
-
-                    totalBaseMetricsOpts.NumDiffExecutedInstructions += baseMetrics.NumExecutedInstructions;
-                    totalDiffMetricsOpts.NumDiffExecutedInstructions += diffMetrics.NumExecutedInstructions;
+                if (o.details != nullptr)
+                {
+                    PrintDiffsCsvRow(
+                        detailsCsv,
+                        reader->GetMethodContextIndex(),
+                        mcb.size,
+                        res, res2,
+                        /* hasDiff */ diffResult != NearDifferResult::SuccessWithoutDiff);
+                }
+            }
+            else
+            {
+                if (o.details != nullptr)
+                {
+                    PrintReplayCsvRow(
+                        detailsCsv,
+                        reader->GetMethodContextIndex(),
+                        mcb.size,
+                        res);
                 }
             }
         }
@@ -663,9 +685,25 @@ int __cdecl main(int argc, char* argv[])
                 failingToReplayMCL.AddMethodToMCL(reader->GetMethodContextIndex());
             }
 
-            if ((res == JitInstance::RESULT_MISSING) || (res2 == JitInstance::RESULT_MISSING))
+            if ((res.Result == ReplayResult::Miss) || (res2.Result == ReplayResult::Miss))
             {
                 missingCount++;
+            }
+
+            if (o.details != nullptr)
+            {
+                if (o.applyDiff)
+                {
+                    PrintDiffsCsvRow(
+                        detailsCsv,
+                        reader->GetMethodContextIndex(), mcb.size,
+                        res, res2,
+                        /* hasDiff */ false);
+                }
+                else
+                {
+                    PrintReplayCsvRow(detailsCsv, reader->GetMethodContextIndex(), mcb.size, res);
+                }
             }
         }
 
@@ -678,7 +716,7 @@ int __cdecl main(int argc, char* argv[])
     if (o.applyDiff)
     {
         LogInfo(g_AsmDiffsSummaryFormatString, loadedCount, jittedCount, failToReplayCount, excludedCount,
-                missingCount, totalDiffMetrics.Overall.NumContextsWithDiffs);
+                missingCount, diffsCount);
     }
     else
     {
@@ -687,16 +725,6 @@ int __cdecl main(int argc, char* argv[])
 
     st2.Stop();
     LogVerbose("Total time: %fms", st2.GetMilliseconds());
-
-    if (o.baseMetricsSummaryFile != nullptr)
-    {
-        totalBaseMetrics.SaveToFile(o.baseMetricsSummaryFile);
-    }
-
-    if (o.diffMetricsSummaryFile != nullptr)
-    {
-        totalDiffMetrics.SaveToFile(o.diffMetricsSummaryFile);
-    }
 
     if (methodStatsEmitter != nullptr)
     {
@@ -715,7 +743,7 @@ int __cdecl main(int argc, char* argv[])
     {
         result = SpmiResult::Error;
     }
-    else if (o.applyDiff && (totalDiffMetrics.Overall.NumContextsWithDiffs > 0))
+    else if (o.applyDiff && (diffsCount > 0))
     {
         result = SpmiResult::Diffs;
     }

src/coreclr/scripts/superpmi.py
@@ -482,29 +482,7 @@ def create_artifacts_base_name(coreclr_args, mch_file):
         artifacts_base_name = "{}.{}".format(coreclr_args.tag, artifacts_base_name)
     return artifacts_base_name
 
-
-def read_csv_metrics(path):
-    """ Read a metrics summary file produced by superpmi and return the rows as a dictionary of dictionaries.
-
-    Args:
-        path (str) : path to .csv file
-
-    Returns:
-        A dictionary of dictionaries. For example, dict["Overall"]["Successful
-        compiles"] will access the total number of successful compiles and
-        dict["MinOpts"]["Successful compiles"] will access the number of
-        minopts compilations.
-    """
-
-    dict = {}
-    with open(path) as csv_file:
-        reader = csv.DictReader(csv_file)
-        for row in reader:
-            dict[row["Name"]] = row
-
-    return dict
-
-def read_csv_diffs(path):
+def read_csv(path):
     with open(path) as csv_file:
         reader = csv.DictReader(csv_file)
         return list(reader)
@@ -1620,19 +1598,19 @@ class SuperPMIReplay:
                 flags = common_flags.copy()
 
                 fail_mcl_file = os.path.join(temp_location, os.path.basename(mch_file) + "_fail.mcl")
-                metrics_summary_file = os.path.join(temp_location, os.path.basename(mch_file) + "_metrics.csv")
+                details_info_file = os.path.join(temp_location, os.path.basename(mch_file) + "_details.csv")
 
                 flags += [
                     "-f", fail_mcl_file,  # Failing mc List
-                    "-metricsSummary", metrics_summary_file
+                    "-details", details_info_file
                 ]
 
                 command = [self.superpmi_path] + flags + [self.jit_path, mch_file]
                 return_code = run_and_log(command)
 
-                metrics = read_csv_metrics(metrics_summary_file)
+                details = read_csv(details_info_file)
+                print_superpmi_result(return_code, self.coreclr_args, self.aggregate_replay_metrics(details), None)
 
-                print_superpmi_result(return_code, self.coreclr_args, metrics, None)
                 if return_code != 0:
                     # Don't report as replay failure missing data (return code 3).
                     # Anything else, such as compilation failure (return code 1, typically a JIT assert) will be
@@ -1666,6 +1644,29 @@ class SuperPMIReplay:
                 logging.info("    %s", file)
 
         return result
+
+    def aggregate_replay_metrics(self, details):
+        """ Given the CSV details file output by SPMI for a replay aggregate the
+        successes, misses and failures
+
+        Returns:
+            A dictionary of metrics
+        """
+
+        num_successes = 0
+        num_misses = 0
+        num_failures = 0
+        for row in details:
+            result = row["Result"]
+            if result == "Success":
+                num_successes += 1
+            elif result == "Miss":
+                num_misses += 1
+            else:
+                assert(result == "Failure")
+                num_failures += 1
+
+        return {"Overall": {"Successful compiles": num_successes, "Missing compiles": num_misses, "Failing compiles": num_failures}}
 
 ################################################################################
 # SuperPMI Replay/AsmDiffs
@@ -1752,6 +1753,71 @@ class DetailsSection:
 
     def __exit__(self, *args):
         self.write_fh.write("\n\n</div></details>\n")
+
+def aggregate_diff_metrics(details):
+    """ Given the CSV details file output by SPMI for a diff aggregate the metrics.
+    """
+
+    base_minopts = {"Successful compiles": 0, "Missing compiles": 0, "Failing compiles": 0,
+                    "Contexts with diffs": 0, "Diffed code bytes": 0, "Diff executed instructions": 0}
+    base_fullopts = base_minopts.copy()
+
+    diff_minopts = base_minopts.copy()
+    diff_fullopts = base_minopts.copy()
+
+    for row in details:
+        base_result = row["Base result"]
+
+        if row["MinOpts"] == "True":
+            base_dict = base_minopts
+            diff_dict = diff_minopts
+        else:
+            base_dict = base_fullopts
+            diff_dict = diff_fullopts
+
+        if base_result == "Success":
+            base_dict["Successful compiles"] += 1
+        elif base_result == "Miss":
+            base_dict["Missing compiles"] += 1
+        else:
+            assert(base_result == "Failure")
+            base_dict["Failing compiles"] += 1
+
+        diff_result = row["Diff result"]
+        if diff_result == "Success":
+            diff_dict["Successful compiles"] += 1
+        elif diff_result == "Miss":
+            diff_dict["Missing compiles"] += 1
+        else:
+            assert(diff_result == "Failure")
+            diff_dict["Failing compiles"] += 1
+
+        if base_result == "Success" and diff_result == "Success":
+            base_size = int(row["Base size"])
+            diff_size = int(row["Diff size"])
+            base_dict["Diffed code bytes"] += base_size
+            diff_dict["Diffed code bytes"] += diff_size
+
+            base_insts = int(row["Base instructions"])
+            diff_insts = int(row["Diff instructions"])
+            base_dict["Diff executed instructions"] += base_insts
+            diff_dict["Diff executed instructions"] += diff_insts
+
+            if row["Has diff"] == "True":
+                base_dict["Contexts with diffs"] += 1
+                diff_dict["Contexts with diffs"] += 1
+
+    base_overall = base_minopts.copy()
+    for k in base_overall.keys():
+        base_overall[k] += base_fullopts[k]
+
+    diff_overall = diff_minopts.copy()
+    for k in diff_overall.keys():
+        diff_overall[k] += diff_fullopts[k]
+
+    return ({"Overall": base_overall, "MinOpts": base_minopts, "FullOpts": base_fullopts},
+            {"Overall": diff_overall, "MinOpts": diff_minopts, "FullOpts": diff_fullopts})
+
 
 class SuperPMIReplayAsmDiffs:
     """ SuperPMI Replay AsmDiffs class
@@ -1905,19 +1971,14 @@ class SuperPMIReplayAsmDiffs:
                 logging.info("Running asm diffs of %s", mch_file)
 
                 fail_mcl_file = os.path.join(temp_location, os.path.basename(mch_file) + "_fail.mcl")
-                diffs_info = os.path.join(temp_location, os.path.basename(mch_file) + "_diffs.csv")
-
-                base_metrics_summary_file = os.path.join(temp_location, os.path.basename(mch_file) + "_base_metrics.csv")
-                diff_metrics_summary_file = os.path.join(temp_location, os.path.basename(mch_file) + "_diff_metrics.csv")
+                detailed_info_file = os.path.join(temp_location, os.path.basename(mch_file) + "_details.csv")
 
                 flags = [
                     "-a",  # Asm diffs
                     "-v", "ewi",  # display errors, warnings, missing, jit info
                     "-f", fail_mcl_file,  # Failing mc List
-                    "-diffsInfo", diffs_info,  # Information about diffs
+                    "-details", detailed_info_file,  # Detailed information about each context
                     "-r", os.path.join(temp_location, "repro"),  # Repro name prefix, create .mc repro files
-                    "-baseMetricsSummary", base_metrics_summary_file, # Create summary of metrics we can use to get total code size impact
-                    "-diffMetricsSummary", diff_metrics_summary_file,
                 ]
                 flags += altjit_asm_diffs_flags
                 flags += base_option_flags
@@ -1955,8 +2016,8 @@ class SuperPMIReplayAsmDiffs:
                     command = [self.superpmi_path] + flags + [self.base_jit_path, self.diff_jit_path, mch_file]
                     return_code = run_and_log(command)
 
-                base_metrics = read_csv_metrics(base_metrics_summary_file)
-                diff_metrics = read_csv_metrics(diff_metrics_summary_file)
+                details = read_csv(detailed_info_file)
+                (base_metrics, diff_metrics) = aggregate_diff_metrics(details)
 
                 print_superpmi_result(return_code, self.coreclr_args, base_metrics, diff_metrics)
                 artifacts_base_name = create_artifacts_base_name(self.coreclr_args, mch_file)
@@ -1976,8 +2037,7 @@ class SuperPMIReplayAsmDiffs:
                             repro_base_command_line = "{} {} {}".format(self.superpmi_path, " ".join(altjit_asm_diffs_flags), self.diff_jit_path)
                             save_repro_mc_files(temp_location, self.coreclr_args, artifacts_base_name, repro_base_command_line)
 
-                diffs = read_csv_diffs(diffs_info)
-
+                diffs = [r for r in details if r["Has diff"] == "True"]
                 if any(diffs):
                     files_with_asm_diffs.append(mch_file)
 
@@ -2212,13 +2272,9 @@ class SuperPMIReplayAsmDiffs:
                         os.remove(fail_mcl_file)
                         fail_mcl_file = None
 
-                    if os.path.isfile(base_metrics_summary_file):
-                        os.remove(base_metrics_summary_file)
-                        base_metrics_summary_file = None
-
-                    if os.path.isfile(diff_metrics_summary_file):
-                        os.remove(diff_metrics_summary_file)
-                        diff_metrics_summary_file = None
+                    if os.path.isfile(detailed_info_file):
+                        os.remove(detailed_info_file)
+                        detailed_info_file = None
 
             ################################################################################################ end of for mch_file in self.mch_files
 
@@ -2647,8 +2703,7 @@ class SuperPMIReplayThroughputDiff:
 
                 logging.info("Running throughput diff of %s", mch_file)
 
-                base_metrics_summary_file = os.path.join(temp_location, os.path.basename(mch_file) + "_base_metrics.csv")
-                diff_metrics_summary_file = os.path.join(temp_location, os.path.basename(mch_file) + "_diff_metrics.csv")
+                detailed_info_file = os.path.join(temp_location, os.path.basename(mch_file) + "_details.csv")
 
                 pin_options = [
                     "-follow_execv", # attach to child processes
@@ -2656,8 +2711,7 @@ class SuperPMIReplayThroughputDiff:
                 ]
                 flags = [
                     "-applyDiff",
-                    "-baseMetricsSummary", base_metrics_summary_file, # Instruction counts are stored in these
-                    "-diffMetricsSummary", diff_metrics_summary_file,
+                    "-details", detailed_info_file,
                 ]
                 flags += target_flags
                 flags += base_option_flags
@@ -2692,8 +2746,8 @@ class SuperPMIReplayThroughputDiff:
                     command_string = " ".join(command)
                     logging.debug("'%s': Error return code: %s", command_string, return_code)
 
-                base_metrics = read_csv_metrics(base_metrics_summary_file)
-                diff_metrics = read_csv_metrics(diff_metrics_summary_file)
+                details = read_csv(detailed_info_file)
+                (base_metrics, diff_metrics) = aggregate_diff_metrics(details)
 
                 if base_metrics is not None and diff_metrics is not None:
                     base_instructions = int(base_metrics["Overall"]["Diff executed instructions"])
@@ -2711,13 +2765,9 @@ class SuperPMIReplayThroughputDiff:
                     logging.warning("No metric files present?")
 
                 if not self.coreclr_args.skip_cleanup:
-                    if os.path.isfile(base_metrics_summary_file):
-                        os.remove(base_metrics_summary_file)
-                        base_metrics_summary_file = None
-
-                    if os.path.isfile(diff_metrics_summary_file):
-                        os.remove(diff_metrics_summary_file)
-                        diff_metrics_summary_file = None
+                    if os.path.isfile(detailed_info_file):
+                        os.remove(detailed_info_file)
+                        detailed_info_file = None
 
             ################################################################################################ end of for mch_file in self.mch_files
 

src/coreclr/tools/superpmi/superpmi/CMakeLists.txt
@@ -31,7 +31,6 @@ set(SUPERPMI_SOURCES
     jitdebugger.cpp
     jitinstance.cpp
     methodstatsemitter.cpp
-    metricssummary.cpp
     neardiffer.cpp
     parallelsuperpmi.cpp
     superpmi.cpp

commit 8ad972e4b4e4cd95855bfb78ff19d6b462ab0839

SPMI: Fix wrong SPMI replay result check
Fix a bug introduced in #92824 that would SPMI to not show replay
failures.

src/coreclr/tools/superpmi/superpmi/superpmi.cpp
@@ -441,7 +441,7 @@ int __cdecl main(int argc, char* argv[])
                 mc->cr->dumpToConsole(); // Dump the compile results if doing debug logging
             }
         }
-        else if (res.Result == ReplayResult::Success)
+        else if (res.Result == ReplayResult::Error)
         {
             errorCount++;
             LogError("Method %d of size %d failed to load and compile correctly%s (%s).",