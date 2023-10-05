78e9803e8b8e7885a21af2f6a8a8a7fe57fef032
Andrew Au <andrewau@microsoft.com>
2023-10-05 10:53:25 -0700

    Avoid duplicate a definition

--- src/coreclr/nativeaot/Runtime/clrgc.disabled.cpp
+++ src/coreclr/nativeaot/Runtime/clrgc.disabled.cpp
@@ -1,13 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#ifdef _WIN32
-// this must exactly match the typedef used by windows.h
-typedef long HRESULT;
-#else
-#include <stdint.h>
-typedef int32_t HRESULT;
-#endif
+#include "CommonTypes.h"
+#include "CommonMacros.h"
 
 HRESULT InitializeDefaultGC();
 
