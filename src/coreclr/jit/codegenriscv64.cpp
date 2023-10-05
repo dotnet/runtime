c03b7c9b022400d1adae414c56d5550663d611cf
Aman Khalid <amankhalid@microsoft.com>
2023-10-05 19:25:25 -0400

    Missed one

--- src/coreclr/jit/codegenriscv64.cpp
+++ src/coreclr/jit/codegenriscv64.cpp
@@ -1182,7 +1182,7 @@ BasicBlock* CodeGen::genCallFinally(BasicBlock* block)
         BasicBlock* const jumpDest = nextBlock->bbJumpDest;
 
         // Now go to where the finally funclet needs to return to.
-        if ((jumpDest == nextBlock->Next()) && !compiler->fgInDifferentRegions(nextBlock, jumpDest))
+        if (nextBlock->NextIs(jumpDest) && !compiler->fgInDifferentRegions(nextBlock, jumpDest))
         {
             // Fall-through.
             // TODO-RISCV64-CQ: Can we get rid of this instruction, and just have the call return directly
