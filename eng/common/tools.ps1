0c2796c70c658706684bf00b65cfdd975cf8d33a
Larry Ewing <lewing@microsoft.com>
2023-10-06 14:02:37 -0500

    [release/8.0] Manually update release/8.0 arcade to latest (#92778)
    * Manually update release/8.0 arcade to latest
    
    * Fix ILCompiler build
    
    * Fix analyzer warning
    
    * work around rid issues
    
    * Move to Directory.Build.targets
    
    And use the same condition as in main.
    
    * Set RunAnalyzers to false for projects with no sources
    
    * Fix Microsoft.NETCore.App.deps.json to include RID graph
    
    ---------
    
    Co-authored-by: Sven Boemer <sbomer@gmail.com>
    Co-authored-by: Marek Safar <marek.safar@gmail.com>
    Co-authored-by: Elinor Fung <elfung@microsoft.com>

--- eng/common/tools.ps1
+++ eng/common/tools.ps1
@@ -384,8 +384,8 @@ function InitializeVisualStudioMSBuild([bool]$install, [object]$vsRequirements =
 
   # If the version of msbuild is going to be xcopied,
   # use this version. Version matches a package here:
-  # https://dev.azure.com/dnceng/public/_artifacts/feed/dotnet-eng/NuGet/RoslynTools.MSBuild/versions/17.6.0-2
-  $defaultXCopyMSBuildVersion = '17.6.0-2'
+  # https://dev.azure.com/dnceng/public/_artifacts/feed/dotnet-eng/NuGet/RoslynTools.MSBuild/versions/17.7.2
+  $defaultXCopyMSBuildVersion = '17.7.2'
 
   if (!$vsRequirements) {
     if (Get-Member -InputObject $GlobalJson.tools -Name 'vs') {
