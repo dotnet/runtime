06be81da2da780eb96bc7999653bfed01ecf8225
Buyaa Namnan <buyankhishig.namnan@microsoft.com>
2023-10-09 13:43:48 -0700

    Update src/libraries/System.Reflection.Emit/src/System/Reflection/Emit/ILGeneratorImpl.cs
    Co-authored-by: Aaron Robinson <arobins@microsoft.com>

--- src/libraries/System.Reflection.Emit/src/System/Reflection/Emit/ILGeneratorImpl.cs
+++ src/libraries/System.Reflection.Emit/src/System/Reflection/Emit/ILGeneratorImpl.cs
@@ -44,34 +44,34 @@ namespace System.Reflection.Emit
         private static int GetStackChangeFor(StackBehaviour stackBehaviour) =>
             stackBehaviour switch
             {
-                StackBehaviour.Pop0 or
-                StackBehaviour.Push0 => 0,
+                StackBehaviour.Pop0
+                or StackBehaviour.Push0 => 0,
                 StackBehaviour.Pop1
                 or StackBehaviour.Popi
                 or StackBehaviour.Popref
                 or StackBehaviour.Varpop => -1,
-                StackBehaviour.Pop1_pop1 or
-                StackBehaviour.Popi_pop1 or
-                StackBehaviour.Popi_popi or
-                StackBehaviour.Popi_popi8 or
-                StackBehaviour.Popi_popr4 or
-                StackBehaviour.Popi_popr8 or
-                StackBehaviour.Popref_pop1 or
-                StackBehaviour.Popref_popi => -2,
-                StackBehaviour.Popi_popi_popi or
-                StackBehaviour.Popref_popi_pop1 or
-                StackBehaviour.Popref_popi_popi or
-                StackBehaviour.Popref_popi_popi8 or
-                StackBehaviour.Popref_popi_popr4 or
-                StackBehaviour.Popref_popi_popr8 or
-                StackBehaviour.Popref_popi_popref => -3,
-                StackBehaviour.Push1 or
-                StackBehaviour.Pushi or
-                StackBehaviour.Pushi8 or
-                StackBehaviour.Pushr4 or
-                StackBehaviour.Pushr8 or
-                StackBehaviour.Pushref or
-                StackBehaviour.Varpush => 1,
+                StackBehaviour.Pop1_pop1
+                or StackBehaviour.Popi_pop1
+                or StackBehaviour.Popi_popi
+                or StackBehaviour.Popi_popi8
+                or StackBehaviour.Popi_popr4
+                or StackBehaviour.Popi_popr8
+                or StackBehaviour.Popref_pop1
+                or StackBehaviour.Popref_popi => -2,
+                StackBehaviour.Popi_popi_popi
+                or StackBehaviour.Popref_popi_pop1
+                or StackBehaviour.Popref_popi_popi
+                or StackBehaviour.Popref_popi_popi8
+                or StackBehaviour.Popref_popi_popr4
+                or StackBehaviour.Popref_popi_popr8
+                or StackBehaviour.Popref_popi_popref => -3,
+                StackBehaviour.Push1
+                or StackBehaviour.Pushi
+                or StackBehaviour.Pushi8
+                or StackBehaviour.Pushr4
+                or StackBehaviour.Pushr8
+                or StackBehaviour.Pushref
+                or StackBehaviour.Varpush => 1,
                 StackBehaviour.Push1_push1 => 2,
                 _ => throw new InvalidOperationException()
             };
