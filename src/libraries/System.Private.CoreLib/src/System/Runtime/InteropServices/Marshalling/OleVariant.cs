40d27a8af15d942e62924446f9e3c6a8f5509107
Jeremy Koritzinsky <jekoritz@microsoft.com>
2023-10-11 17:24:07 -0700

    Add initial implementation of OleVariant struct.

---
+++ src/libraries/System.Private.CoreLib/src/System/Runtime/InteropServices/Marshalling/OleVariant.cs
@@ -0,0 +1,361 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace System.Runtime.InteropServices.Marshalling
+{
+    [StructLayout(LayoutKind.Explicit)]
+    public struct OleVariant : IDisposable
+    {
+#if DEBUG
+        unsafe static OleVariant()
+        {
+            // Variant size is the size of 4 pointers (16 bytes) on a 32-bit processor,
+            // and 3 pointers (24 bytes) on a 64-bit processor.
+            int variantSize = sizeof(OleVariant);
+            if (IntPtr.Size == 4)
+            {
+                Debug.Assert(variantSize == (4 * IntPtr.Size));
+            }
+            else
+            {
+                Debug.Assert(IntPtr.Size == 8);
+                Debug.Assert(variantSize == (3 * IntPtr.Size));
+            }
+        }
+#endif
+
+        // Most of the data types in the Variant are carried in _typeUnion
+        [FieldOffset(0)] private TypeUnion _typeUnion;
+
+        // Decimal is the largest data type and it needs to use the space that is normally unused in TypeUnion._wReserved1, etc.
+        // Hence, it is declared to completely overlap with TypeUnion. A Decimal does not use the first two bytes, and so
+        // TypeUnion._vt can still be used to encode the type.
+        [FieldOffset(0)] private decimal _decimal;
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct TypeUnion
+        {
+            public ushort _vt;
+            public ushort _wReserved1;
+            public ushort _wReserved2;
+            public ushort _wReserved3;
+
+            public UnionTypes _unionTypes;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct Record
+        {
+            public IntPtr _record;
+            public IntPtr _recordInfo;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        private struct UnionTypes
+        {
+            [FieldOffset(0)] public sbyte _i1;
+            [FieldOffset(0)] public short _i2;
+            [FieldOffset(0)] public int _i4;
+            [FieldOffset(0)] public long _i8;
+            [FieldOffset(0)] public byte _ui1;
+            [FieldOffset(0)] public ushort _ui2;
+            [FieldOffset(0)] public uint _ui4;
+            [FieldOffset(0)] public ulong _ui8;
+            [FieldOffset(0)] public int _int;
+            [FieldOffset(0)] public uint _uint;
+            [FieldOffset(0)] public short _bool;
+            [FieldOffset(0)] public int _error;
+            [FieldOffset(0)] public float _r4;
+            [FieldOffset(0)] public double _r8;
+            [FieldOffset(0)] public Currency _cy;
+            [FieldOffset(0)] public double _date;
+            [FieldOffset(0)] public IntPtr _bstr;
+            [FieldOffset(0)] public IntPtr _unknown;
+            [FieldOffset(0)] public IntPtr _dispatch;
+            [FieldOffset(0)] public IntPtr _pvarVal;
+            [FieldOffset(0)] public IntPtr _byref;
+            [FieldOffset(0)] public Record _record;
+        }
+
+        public unsafe void Dispose()
+        {
+#if TARGET_WINDOWS
+            fixed (void* pThis = &this)
+            {
+                Interop.Ole32.PropVariantClear((nint)pThis);
+            }
+#else
+            // TODO: Replicate the behavior of PropVariantClear on non-Windows platforms.
+            throw new NotImplementedException();
+#endif
+        }
+
+#pragma warning disable CS0618 // We support the obsolete *Wrapper types
+        public static OleVariant Create<T>([DisallowNull] T value)
+        {
+            Unsafe.SkipInit(out OleVariant variant);
+            if (typeof(T) == typeof(DBNull))
+            {
+                variant = Null;
+            }
+            else if (typeof(T) == typeof(short))
+            {
+                variant.VarType = VarEnum.VT_I2;
+                variant._typeUnion._unionTypes._i2 = (short)(object)value;
+            }
+            else if (typeof(T) == typeof(int))
+            {
+                variant.VarType = VarEnum.VT_I4;
+                variant._typeUnion._unionTypes._i4 = (int)(object)value;
+            }
+            else if (typeof(T) == typeof(float))
+            {
+                variant.VarType = VarEnum.VT_R4;
+                variant._typeUnion._unionTypes._r4 = (float)(object)value;
+            }
+            else if (typeof(T) == typeof(double))
+            {
+                variant.VarType = VarEnum.VT_R8;
+                variant._typeUnion._unionTypes._r8 = (double)(object)value;
+            }
+            else if (typeof(T) == typeof(CurrencyWrapper))
+            {
+                variant.VarType = VarEnum.VT_CY;
+                variant._typeUnion._unionTypes._cy = new Currency(((CurrencyWrapper)(object)value).WrappedObject);
+            }
+            else if (typeof(T) == typeof(DateTime))
+            {
+                variant.VarType = VarEnum.VT_DATE;
+                variant._typeUnion._unionTypes._date = ((DateTime)(object)value).ToOADate();
+            }
+            else if (typeof(T) == typeof(BStrWrapper))
+            {
+                variant.VarType = VarEnum.VT_BSTR;
+                variant._typeUnion._unionTypes._bstr = Marshal.StringToBSTR(((BStrWrapper)(object)value).WrappedObject);
+            }
+            else if (typeof(T) == typeof(ErrorWrapper))
+            {
+                variant.VarType = VarEnum.VT_ERROR;
+                variant._typeUnion._unionTypes._error = ((ErrorWrapper)(object)value).ErrorCode;
+            }
+            else if (typeof(T) == typeof(bool))
+            {
+                // bool values in OLE VARIANTs are VARIANT_BOOL values.
+                variant.VarType = VarEnum.VT_BOOL;
+                variant._typeUnion._unionTypes._bool = ((bool)(object)value) ? (short)-1 : (short)0;
+            }
+            else if (typeof(T) == typeof(decimal))
+            {
+                // Set the value first and then the type as the decimal storage
+                // overlaps with the type descriminator.
+                variant._decimal = (decimal)(object)value;
+                variant.VarType = VarEnum.VT_DECIMAL;
+            }
+            else if (typeof(T) == typeof(sbyte))
+            {
+                variant.VarType = VarEnum.VT_I1;
+                variant._typeUnion._unionTypes._i1 = (sbyte)(object)value;
+            }
+            else if (typeof(T) == typeof(byte))
+            {
+                variant.VarType = VarEnum.VT_UI1;
+                variant._typeUnion._unionTypes._ui1 = (byte)(object)value;
+            }
+            else if (typeof(T) == typeof(ushort))
+            {
+                variant.VarType = VarEnum.VT_UI2;
+                variant._typeUnion._unionTypes._ui2 = (ushort)(object)value;
+            }
+            else if (typeof(T) == typeof(uint))
+            {
+                variant.VarType = VarEnum.VT_UI4;
+                variant._typeUnion._unionTypes._ui4 = (uint)(object)value;
+            }
+            else if (typeof(T) == typeof(long))
+            {
+                variant.VarType = VarEnum.VT_I8;
+                variant._typeUnion._unionTypes._i8 = (long)(object)value;
+            }
+            else if (typeof(T) == typeof(ulong))
+            {
+                variant.VarType = VarEnum.VT_UI8;
+                variant._typeUnion._unionTypes._ui8 = (ulong)(object)value;
+            }
+            else
+            {
+                throw new ArgumentException("Unsupported type", nameof(T));
+            }
+            // We do not support mapping nint or nuint to VT_INT and VT_UINT respectively
+            // as this does not match the MS-OAUT spec.
+            // We do not map string to any VT_* type directly as there are multiple equally valid options.
+            // We do not map VT_BYREF automatically, nor do we map any of the array types.
+            return variant;
+        }
+#pragma warning restore CS0618
+
+        public static unsafe OleVariant CreateRaw<T>(VarEnum vt, T rawValue)
+            where T : unmanaged
+        {
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(Unsafe.SizeOf<T>(), sizeof(UnionTypes), nameof(T));
+            if (vt == VarEnum.VT_DECIMAL)
+            {
+                throw new ArgumentException("VT_DECIMAL is not supported in CreateRaw. Use the Create method.", nameof(vt));
+            }
+            if (vt == VarEnum.VT_VARIANT)
+            {
+                throw new ArgumentException("VT_VARIANT is not supported in variants.", nameof(vt));
+            }
+
+            Unsafe.SkipInit(out OleVariant value);
+            value.VarType = vt;
+            value.GetRawDataRef<T>() = (vt, sizeof(T)) switch
+            {
+                (VarEnum.VT_I1 or VarEnum.VT_UI1, 1) => rawValue,
+                (VarEnum.VT_I2 or VarEnum.VT_UI2 or VarEnum.VT_BOOL, 2) => rawValue,
+                (VarEnum.VT_ERROR or VarEnum.VT_HRESULT or VarEnum.VT_I4 or VarEnum.VT_UI4 or VarEnum.VT_R4, 4) => rawValue,
+                (VarEnum.VT_I8 or VarEnum.VT_UI8 or VarEnum.VT_R8 or VarEnum.VT_DATE, 8) => rawValue,
+                (VarEnum.VT_INT or VarEnum.VT_UINT or VarEnum.VT_UNKNOWN or VarEnum.VT_DISPATCH or VarEnum.VT_LPSTR or VarEnum.VT_BSTR or VarEnum.VT_LPWSTR or VarEnum.VT_SAFEARRAY or VarEnum.VT_CARRAY, _) when sizeof(T) == nint.Size => rawValue,
+                (VarEnum.VT_CY or VarEnum.VT_FILETIME, 8) => rawValue,
+                (VarEnum.VT_RECORD, _) when sizeof(T) == sizeof(Record) => rawValue,
+                _ when vt.HasFlag(VarEnum.VT_BYREF) && sizeof(T) == nint.Size => rawValue,
+                _ when vt.HasFlag(VarEnum.VT_VECTOR) && sizeof(T) == nint.Size => rawValue,
+                _ when vt.HasFlag(VarEnum.VT_ARRAY) && sizeof(T) == nint.Size => rawValue,
+                (VarEnum.VT_CLSID, _) when sizeof(T) == sizeof(Guid) => rawValue,
+                _ => throw new ArgumentException("Size of T should be the same size as the value specified by vt")
+            };
+
+            return value;
+        }
+
+        public static OleVariant Null { get; } = new() { VarType = VarEnum.VT_NULL };
+
+        private readonly void ThrowIfNotVarType(params VarEnum[] requiredType)
+        {
+            if (Array.IndexOf(requiredType, VarType) != -1)
+            {
+                throw new InvalidOperationException($"Variant type {VarType} cannot be cast to {requiredType}");
+            }
+        }
+
+#pragma warning disable CS0618 // Type or member is obsolete
+        // This method will match the exact same semantics as Create<T> with the same set of supported types.
+        public readonly T As<T>()
+        {
+            if (VarType == VarEnum.VT_EMPTY)
+            {
+                return default!;
+            }
+            if (typeof(T) == typeof(DBNull))
+            {
+                ThrowIfNotVarType(VarEnum.VT_NULL);
+                return (T)(object)DBNull.Value;
+            }
+            else if (typeof(T) == typeof(short))
+            {
+                ThrowIfNotVarType(VarEnum.VT_I2);
+                return (T)(object)_typeUnion._unionTypes._i2;
+            }
+            else if (typeof(T) == typeof(int))
+            {
+                ThrowIfNotVarType(VarEnum.VT_I4);
+                return (T)(object)_typeUnion._unionTypes._i4;
+            }
+            else if (typeof(T) == typeof(float))
+            {
+                ThrowIfNotVarType(VarEnum.VT_R4);
+                return (T)(object)_typeUnion._unionTypes._r4;
+            }
+            else if (typeof(T) == typeof(double))
+            {
+                ThrowIfNotVarType(VarEnum.VT_R8);
+                return (T)(object)_typeUnion._unionTypes._r8;
+            }
+            else if (typeof(T) == typeof(CurrencyWrapper))
+            {
+                ThrowIfNotVarType(VarEnum.VT_CY);
+                return (T)(object)new CurrencyWrapper(new decimal(_typeUnion._unionTypes._cy));
+            }
+            else if (typeof(T) == typeof(DateTime))
+            {
+                ThrowIfNotVarType(VarEnum.VT_DATE);
+                return (T)(object)DateTime.FromOADate(_typeUnion._unionTypes._date);
+            }
+            else if (typeof(T) == typeof(BStrWrapper))
+            {
+                ThrowIfNotVarType(VarEnum.VT_BSTR);
+                return (T)(object)new BStrWrapper(Marshal.PtrToStringBSTR(_typeUnion._unionTypes._bstr));
+            }
+            else if (typeof(T) == typeof(ErrorWrapper))
+            {
+                ThrowIfNotVarType(VarEnum.VT_ERROR);
+                return (T)(object)new ErrorWrapper(_typeUnion._unionTypes._error);
+            }
+            else if (typeof(T) == typeof(bool))
+            {
+                // bool values in OLE VARIANTs are VARIANT_BOOL values.
+                ThrowIfNotVarType(VarEnum.VT_BOOL);
+                return (T)(object)(_typeUnion._unionTypes._bool != 0);
+            }
+            else if (typeof(T) == typeof(decimal))
+            {
+                // Set the value first and then the type as the decimal storage
+                // overlaps with the type descriminator.
+                ThrowIfNotVarType(VarEnum.VT_DECIMAL);
+                // Create a second variant copy with the VarType set to VT_EMPTY.
+                // This will ensure that we don't leak the VT_DECMIAL flag into the decimal value itself.
+                OleVariant variantWithoutVarType = this;
+                variantWithoutVarType.VarType = VarEnum.VT_EMPTY;
+                return (T)(object)variantWithoutVarType._decimal;
+            }
+            else if (typeof(T) == typeof(sbyte))
+            {
+                ThrowIfNotVarType(VarEnum.VT_I1);
+                return (T)(object)_typeUnion._unionTypes._i1;
+            }
+            else if (typeof(T) == typeof(byte))
+            {
+                ThrowIfNotVarType(VarEnum.VT_UI1);
+                return (T)(object)_typeUnion._unionTypes._ui1;
+            }
+            else if (typeof(T) == typeof(ushort))
+            {
+                ThrowIfNotVarType(VarEnum.VT_UI2);
+                return (T)(object)_typeUnion._unionTypes._ui2;
+            }
+            else if (typeof(T) == typeof(uint))
+            {
+                ThrowIfNotVarType(VarEnum.VT_UI4);
+                return (T)(object)_typeUnion._unionTypes._ui4;
+            }
+            else if (typeof(T) == typeof(long))
+            {
+                ThrowIfNotVarType(VarEnum.VT_I8);
+                return (T)(object)_typeUnion._unionTypes._i8;
+            }
+            else if (typeof(T) == typeof(ulong))
+            {
+                ThrowIfNotVarType(VarEnum.VT_UI8);
+                return (T)(object)_typeUnion._unionTypes._ui8;
+            }
+            throw new ArgumentException("Unsupported type", nameof(T));
+        }
+#pragma warning restore CS0618 // Type or member is obsolete
+
+        public VarEnum VarType
+        {
+            readonly get => (VarEnum)_typeUnion._vt;
+            private set => _typeUnion._vt = (ushort)value;
+        }
+
+        [UnscopedRef]
+        public unsafe ref T GetRawDataRef<T>()
+            where T : unmanaged
+        {
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(Unsafe.SizeOf<T>(), sizeof(UnionTypes), nameof(T));
+            return ref Unsafe.As<UnionTypes, T>(ref _typeUnion._unionTypes);
+        }
+    }
+}
