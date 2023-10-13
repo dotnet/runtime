1886a46b2f41d5e1beb8c35767915c8aaa7f67eb
Michael Sharp <misharp@microsoft.com>
2023-10-13 06:34:28 -0600

    minor code cleanup

--- src/libraries/System.Numerics.Tensors/tests/TensorPrimitivesTests.cs
+++ src/libraries/System.Numerics.Tensors/tests/TensorPrimitivesTests.cs
@@ -990,8 +990,6 @@ namespace System.Numerics.Tensors.Tests
         [Fact]
         public static void IndexOfMax_Negative0LesserThanPositive0()
         {
-            System.Diagnostics.Debugger.Launch();
-
             Assert.Equal(1, TensorPrimitives.IndexOfMax([-0f, +0f]));
             Assert.Equal(0, TensorPrimitives.IndexOfMax([-0f, -0f, -0f, -0f]));
             Assert.Equal(4, TensorPrimitives.IndexOfMax([-0f, -0f, -0f, -0f, +0f, +0f, +0f]));
@@ -1099,24 +1097,6 @@ namespace System.Numerics.Tensors.Tests
         [MemberData(nameof(TensorLengths))]
         public static void IndexOfMinMagnitude(int tensorLength)
         {
-            foreach (int expected in new[] { 0, tensorLength / 2, tensorLength - 1 })
-            {
-                using BoundedMemory<float> x = CreateTensor(tensorLength);
-                for (int i = 0; i < x.Length; i++)
-                {
-                    x[i] = i % 2 == 0 ? 42 : -42;
-                }
-
-                x[expected] = -41;
-
-                Assert.Equal(expected, TensorPrimitives.IndexOfMinMagnitude(x));
-            }
-        }
-
-        [Fact]
-        public static void IndexOfMinMagnitude_Temp()
-        {
-            int tensorLength = 5;
             foreach (int expected in new[] { 0, tensorLength / 2, tensorLength - 1 })
             {
                 using BoundedMemory<float> x = CreateTensor(tensorLength);
@@ -1332,21 +1312,6 @@ namespace System.Numerics.Tensors.Tests
                 Assert.Equal(max, TensorPrimitives.Max(x));
                 Assert.Equal(SingleToUInt32(x[TensorPrimitives.IndexOfMax(x)]), SingleToUInt32(TensorPrimitives.Max(x)));
             }, x);
-        }
-
-        [Fact]
-        public static void Max_Tensor_Temp()
-        {
-            using BoundedMemory<float> x = CreateAndFillTensor(128);
-
-            Assert.Equal(Enumerable.Max(MemoryMarshal.ToEnumerable<float>(x.Memory)), TensorPrimitives.Max(x));
-
-            float max = float.NegativeInfinity;
-            foreach (float f in x.Span)
-            {
-                max = Math.Max(max, f);
-            }
-            Assert.Equal(max, TensorPrimitives.Max(x));
         }
 
         [Theory]
