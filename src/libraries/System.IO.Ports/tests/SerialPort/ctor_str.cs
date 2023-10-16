87da810f7dae2a356b346aa294122d23d0ba6d35
Viktor Hofer <viktor.hofer@microsoft.com>
2023-10-16 11:47:47 +0000

    System.IO.Ports failing tests

--- src/libraries/System.IO.Ports/tests/SerialPort/ctor_str.cs
+++ src/libraries/System.IO.Ports/tests/SerialPort/ctor_str.cs
@@ -128,7 +128,7 @@ namespace System.IO.Ports.Tests
                     serPortProp.VerifyPropertiesAndPrint(com);
                 }
             }
-            catch (TrueException)
+            catch (FailException)
             {
                 // This is an inner failure
                 throw;
