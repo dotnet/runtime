e4e9673690eb27574e5348300223d0b36ff41484
Jan Dupej <jandupej@microsoft.com>
2023-10-18 13:25:31 +0200

    Fixed 32-bit MSVC build.

--- src/mono/mono/utils/mono-bitutils.c
+++ src/mono/mono/utils/mono-bitutils.c
@@ -8,6 +8,9 @@
 #include <intrin.h>
 #endif
 
+#define LOW32(x) ((guint32)(x & 0xffffffff))
+#define HIGH32(x) ((guint32)((x >> 32) & 0xffffffff))
+
 int mono_lzcnt32 (guint32 x)
 {
 #ifdef _MSC_VER
@@ -23,12 +26,20 @@ int mono_lzcnt32 (guint32 x)
 
 int mono_lzcnt64 (guint64 x)
 {
-#ifdef _MSC_VER
+#if defined(_MSC_VER) && (defined(_M_ARM64) || defined(_M_ARM64EC) || defined (_M_X64))
 	unsigned long index = 0;
 	if ( _BitScanReverse64( &index, x ) )
 		return (int)index;
 	else
 		return 64;
+#elif defined(_MSC_VER)
+  guint32 high = HIGH32 (x);
+  if (x == 0)
+    return 64;
+  else if (high == 0)
+    return mono_lzcnt32 (LOW32 (x)) + 32;
+  else
+    return mono_lzcnt32 (high);
 #else
 	return __builtin_clzll(x);
 #endif
@@ -49,12 +60,20 @@ int mono_tzcnt32 (guint32 x)
 
 int mono_tzcnt64 (guint64 x)
 {
-#ifdef _MSC_VER
+#if defined(_MSC_VER) && (defined(_M_ARM64) || defined(_M_ARM64EC) || defined (_M_X64))
 	unsigned long index = 0;
 	if ( _BitScanForward64( &index, x ) )
 		return (int)index;
 	else
 		return 64;
+#elif defined(_MSC_VER)
+  guint32 low = LOW32 (x);
+  if (x == 0)
+    return 64;
+  else if (low == 0)
+    return mono_tzcnt32 (HIGH32 (x)) + 32;
+  else
+    return mono_tzcnt32 (low);
 #else
 	return __builtin_ctzll(x);
 #endif
