b2c85b975041a6fdafd37219dd9613fc884e77c9
TIHan <lol.tihan@gmail.com>
2023-10-18 12:46:21 -0700

    Fixing debug check

--- src/coreclr/tools/AssemblyChecker/Program.cs
+++ src/coreclr/tools/AssemblyChecker/Program.cs
@@ -9,6 +9,7 @@
 
 using System.Text;
 using System.Diagnostics;
+using System.Reflection;
 using System.Reflection.Metadata;
 using System.Reflection.PortableExecutable;
 
@@ -33,52 +34,28 @@ namespace AssemblyChecker
             return reader.IsAssembly;
         }
 
-        static bool HasDebuggableAttribute(MetadataReader reader, EntityHandle entity)
-        {
-            if (entity.IsNil)
-                return false;
-
-            switch (entity.Kind)
-            {
-                case HandleKind.MemberReference:
-                    {
-                        var memRef = reader.GetMemberReference((MemberReferenceHandle)entity);
-                        switch (memRef.Parent.Kind)
-                        {
-                            case HandleKind.TypeReference:
-                                {
-                                    var tyRef = reader.GetTypeReference((TypeReferenceHandle)memRef.Parent);
-                                    return reader.GetString(tyRef.Name) == "DebuggableAttribute";
-                                }
-
-                            default:
-                                return false;
-                        }
-                }
-
-                default:
-                    return false;
-            }
-
-        }
-
         static bool IsDebug(string path)
         {
-            using var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            var asm = Assembly.LoadFrom(path);
+            object[] attribs = asm.GetCustomAttributes(typeof(DebuggableAttribute), false);
 
-            // Try to read CLI metadata from the PE file.
-            using var peReader = new PEReader(fs);
-
-            if (!peReader.HasMetadata)
+            if (attribs.Length > 0)
             {
-                return false; // File does not have CLI metadata.
+                return
+                    attribs.Any(x =>
+                    {
+                        DebuggableAttribute? debuggableAttribute = attribs[0] as DebuggableAttribute;
+                        if (debuggableAttribute != null)
+                        {
+                            return debuggableAttribute.IsJITOptimizerDisabled;
+                        }
+                        return false;
+                    });
             }
-
-            // Check that file has an assembly manifest.
-            MetadataReader reader = peReader.GetMetadataReader();
-
-            var attrs = reader.GetAssemblyDefinition().GetCustomAttributes();
-            return attrs.Any(x => HasDebuggableAttribute(reader, reader.GetCustomAttribute(x).Constructor));
+            else
+            {
+                return false;
+            }
         }
 
         static bool IsExe(string path)
