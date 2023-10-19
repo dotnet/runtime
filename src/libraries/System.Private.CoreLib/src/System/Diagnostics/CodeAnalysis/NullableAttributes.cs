5bbd7d3d4b1c442a89a4bd4770286804ccd0a434
Rikki Gibson <rigibson@microsoft.com>
2023-10-19 11:22:10 -0700

    Update NotNullIfNotNull documentation

--- src/libraries/System.Private.CoreLib/src/System/Diagnostics/CodeAnalysis/NullableAttributes.cs
+++ src/libraries/System.Private.CoreLib/src/System/Diagnostics/CodeAnalysis/NullableAttributes.cs
@@ -89,7 +89,7 @@ namespace System.Diagnostics.CodeAnalysis
     {
         /// <summary>Initializes the attribute with the associated parameter name.</summary>
         /// <param name="parameterName">
-        /// The associated parameter name.  The output will be non-null if the argument to the parameter specified is non-null.
+        /// The associated parameter name.  The value will be non-null if the argument to the parameter specified is non-null.
         /// </param>
         public NotNullIfNotNullAttribute(string parameterName) => ParameterName = parameterName;
 
