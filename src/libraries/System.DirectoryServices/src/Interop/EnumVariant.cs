b100b983705c38ec584398f0c29424ff7e92eb46
Chris Moore <chrisdcmoore@live.com>
2023-10-21 10:45:14 +0100

    remove unused consts

--- src/libraries/System.DirectoryServices/src/Interop/EnumVariant.cs
+++ src/libraries/System.DirectoryServices/src/Interop/EnumVariant.cs
@@ -8,9 +8,6 @@ namespace System.DirectoryServices
     internal static partial class SafeNativeMethods
     {
         public const int
-            FORMAT_MESSAGE_IGNORE_INSERTS = 0x00000200,
-            FORMAT_MESSAGE_FROM_SYSTEM = 0x00001000,
-            FORMAT_MESSAGE_ARGUMENT_ARRAY = 0x00002000,
             ERROR_MORE_DATA = 234,
             ERROR_SUCCESS = 0;
 
