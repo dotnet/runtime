6d1091bd172cc1354cefbd3e217c1b3c152a34f8
David Wrighton <davidwr@microsoft.com>
2023-10-23 16:15:54 -0700

    Add test case with new logic which doesn't actually yield, but has all the various yield points in there

---
+++ src/tests/Loader/async/async2fibonacci-without-yields.cs
@@ -0,0 +1,63 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Xunit;
+
+public class Async2FibonacceWithoutYields
+{
+    const uint Threshold = 1_000;
+    static bool doYields = false;
+
+    [Fact]
+    public static void Test()
+    {
+        AsyncEntry().Wait();
+    }
+
+    public static async Task AsyncEntry()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            uint result = await A(100_000_000);
+            Console.WriteLine($"{sw.ElapsedMilliseconds} ms result={result}");
+        }
+    }
+
+    static async2 uint A(uint n)
+    {
+        uint result = n;
+        for (uint i = 0; i < n; i++)
+            result = await B(result);
+        return result;
+    }
+
+    static async2 uint B(uint n)
+    {
+        uint result = n;
+
+        result = result * 1_999_999_981;
+        if ((result < Threshold) && doYields)
+        {
+            await Task.Yield();
+        }
+
+        result = result * 1_999_999_981;
+        if ((result < Threshold) && doYields)
+        {
+            await Task.Yield();
+        }
+
+        result = result * 1_999_999_981;
+        if ((result < Threshold) && doYields)
+        {
+            await Task.Yield();
+        }
+
+        return result;
+    }
+}
