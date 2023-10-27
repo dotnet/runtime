1f1f0b9a4d9dde3ae0505cc9c18235ca0eee7502
artl <artl@microsoft.com>
2023-10-27 13:21:29 -0700

    Updated styles

--- src/libraries/System.Diagnostics.Tracing/tests/BasicEventSourceTest/TestsWrite.cs
+++ src/libraries/System.Diagnostics.Tracing/tests/BasicEventSourceTest/TestsWrite.cs
@@ -13,6 +13,7 @@ using System.Diagnostics;
 
 namespace BasicEventSourceTests
 {
+#pragma warning disable SA1136 // Enum values should be on separate lines
     internal enum Color { Red, Blue, Green };
     internal enum ColorUInt32 : uint { Red, Blue, Green };
     internal enum ColorByte : byte { Red, Blue, Green };
@@ -21,6 +22,7 @@ namespace BasicEventSourceTests
     internal enum ColorUInt16 : ushort { Red, Blue, Green };
     internal enum ColorInt64 : long { Red, Blue, Green };
     internal enum ColorUInt64 : ulong { Red, Blue, Green };
+#pragma warning restore
 
     public partial class TestsWrite
     {
