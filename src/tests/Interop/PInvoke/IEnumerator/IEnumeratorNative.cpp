d6c8d6b6e08cc2897503582ab77bea2ac82e3545
Jeremy Koritzinsky <jekoritz@microsoft.com>
2023-10-26 17:38:08 -0700

    Fix a refleak in the IEnumerator marshalling tests.

--- src/tests/Interop/PInvoke/IEnumerator/IEnumeratorNative.cpp
+++ src/tests/Interop/PInvoke/IEnumerator/IEnumeratorNative.cpp
@@ -98,6 +98,13 @@ extern "C" DLL_EXPORT HRESULT STDMETHODCALLTYPE VerifyIntegerEnumeration(IDispat
         return hr;
     }
 
+    hr = VariantClear(&result);
+    
+    if (FAILED(hr))
+    {
+        return hr;
+    }
+
     hr = VerifyIntegerEnumerator(pEnum, start, count);
 
     pEnum->Release();
