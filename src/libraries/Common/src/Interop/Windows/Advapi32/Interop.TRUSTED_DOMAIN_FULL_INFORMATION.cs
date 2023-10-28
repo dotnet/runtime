ef237828a51d7efc0d09ea42dda53f1b8579da26
Chris Moore <chrisdcmoore@live.com>
2023-10-28 11:19:41 +0100

    make these not nullable
    they appear not to be nullable on the unmanaged side
    see https://learn.microsoft.com/en-us/windows/win32/api/ntsecapi/ns-ntsecapi-trusted_domain_full_information
    
    suggested by review comment https://github.com/dotnet/runtime/pull/93722#discussion_r1374909293

--- src/libraries/Common/src/Interop/Windows/Advapi32/Interop.TRUSTED_DOMAIN_FULL_INFORMATION.cs
+++ src/libraries/Common/src/Interop/Windows/Advapi32/Interop.TRUSTED_DOMAIN_FULL_INFORMATION.cs
@@ -11,8 +11,8 @@ internal static partial class Interop
         internal struct TRUSTED_DOMAIN_FULL_INFORMATION
         {
             public TRUSTED_DOMAIN_INFORMATION_EX Information;
-            internal TRUSTED_POSIX_OFFSET_INFO? PosixOffset;
-            public TRUSTED_DOMAIN_AUTH_INFORMATION? AuthInformation;
+            internal TRUSTED_POSIX_OFFSET_INFO PosixOffset;
+            public TRUSTED_DOMAIN_AUTH_INFORMATION AuthInformation;
         }
     }
 }
