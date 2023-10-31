65d4b754cd02ae2dc9fbbb8023f3f67438e37b0d
Steve Pfister <steve.pfister@microsoft.com>
2023-10-30 22:06:13 -0400

    make sure icdt73 symbol is found

--- src/mono/wasm/runtime/es6/dotnet.es6.lib.js
+++ src/mono/wasm/runtime/es6/dotnet.es6.lib.js
@@ -68,7 +68,7 @@ function setup(linkerSetup) {
 
 const DotnetSupportLib = {
     $DOTNET: { setup },
-    icudt68_dat: function () { throw new Error('dummy link symbol') },
+    icudt73_dat: function () { throw new Error('dummy link symbol') },
 };
 
 function createWasmImportStubsFrom(collection) {
