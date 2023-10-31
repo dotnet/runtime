9a921db9f585f3784bb7126184cdab03b2b0068e
Christopher Moore <chrisdcmoore@users.noreply.github.com>
2023-10-31 08:36:35 +0000

    Remove redundant EntryPoint property
    Co-authored-by: Aaron Robinson <arobins@microsoft.com>

--- src/libraries/Common/src/Interop/Windows/Kernel32/Interop.OpenThread_IntPtr.cs
+++ src/libraries/Common/src/Interop/Windows/Kernel32/Interop.OpenThread_IntPtr.cs
@@ -12,7 +12,7 @@ internal static partial class Interop
         private const uint SYNCHRONIZE = 0x00100000;
         internal const uint THREAD_ALL_ACCESS = STANDARD_RIGHTS_REQUIRED | SYNCHRONIZE | 0x3FF;
 
-        [LibraryImport(Libraries.Kernel32, EntryPoint = "OpenThread", SetLastError = true)]
+        [LibraryImport(Libraries.Kernel32, SetLastError = true)]
         internal static partial IntPtr OpenThread(uint desiredAccess, [MarshalAs(UnmanagedType.Bool)] bool inherited, int threadID);
     }
 }
