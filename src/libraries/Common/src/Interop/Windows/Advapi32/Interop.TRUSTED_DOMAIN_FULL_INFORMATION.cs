f3258383edef350902bc32e096668ac5e8c534d6
Christopher Moore <chrisdcmoore@users.noreply.github.com>
2023-10-31 08:35:09 +0000

    Use consistent visibility modifiers for TRUSTED_DOMAIN_FULL_INFORMATION struct fields
    Co-authored-by: Aaron Robinson <arobins@microsoft.com>

--- src/libraries/Common/src/Interop/Windows/Advapi32/Interop.TRUSTED_DOMAIN_FULL_INFORMATION.cs
+++ src/libraries/Common/src/Interop/Windows/Advapi32/Interop.TRUSTED_DOMAIN_FULL_INFORMATION.cs
@@ -10,9 +10,9 @@ internal static partial class Interop
         [StructLayout(LayoutKind.Sequential)]
         internal struct TRUSTED_DOMAIN_FULL_INFORMATION
         {
-            public TRUSTED_DOMAIN_INFORMATION_EX Information;
+            internal TRUSTED_DOMAIN_INFORMATION_EX Information;
             internal TRUSTED_POSIX_OFFSET_INFO PosixOffset;
-            public TRUSTED_DOMAIN_AUTH_INFORMATION AuthInformation;
+            internal TRUSTED_DOMAIN_AUTH_INFORMATION AuthInformation;
         }
     }
 }
