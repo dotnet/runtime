42ea1ec1070764f760a9dfeb10ffbcc1a8f31409
Sergio Pedri <sergio0694@live.com>
2023-11-02 18:28:02 +0100

    Add XML comments

--- src/libraries/System.Private.CoreLib/src/System/Runtime/CompilerServices/InlineArrayAttribute.cs
+++ src/libraries/System.Private.CoreLib/src/System/Runtime/CompilerServices/InlineArrayAttribute.cs
@@ -6,14 +6,37 @@ namespace System.Runtime.CompilerServices
     /// <summary>
     /// Indicates that the instance's storage is sequentially replicated "length" times.
     /// </summary>
+    /// <remarks>
+    /// <para>
+    /// This attribute can be used to annotate a <see langword="struct"/> type with a single field.
+    /// The runtime will replicate that field as many times as specified in the actual type layout.
+    /// </para>
+    /// <para>
+    /// Here's an example of how an inline array type with 8 <see cref="float"/> values can be declared:
+    /// <code lang="csharp">
+    /// [InlineArray(8)]
+    /// struct Float8InlineArray
+    /// {
+    ///     private float _value;
+    /// }
+    /// </code>
+    /// </para>
+    /// <para>
+    /// The compiler will also automatically generate an indexer property, and support casting the type
+    /// to <see cref="Span{T}"/> and <see cref="ReadOnlySpan{T}"/> for safe enumeration of all elements.
+    /// </para>
+    /// </remarks>
     [AttributeUsage(AttributeTargets.Struct, AllowMultiple = false)]
     public sealed class InlineArrayAttribute : Attribute
     {
+        /// <summary>Creates a new <see cref="InlineArrayAttribute"/> instance with the specified parameters.</summary>
+        /// <param name="length">The number of sequential fields to replicate in the inline array type.</param>
         public InlineArrayAttribute(int length)
         {
             Length = length;
         }
 
+        /// <summary>Gets the number of sequential fields to replicate in the inline array type.</summary>
         public int Length { get; }
     }
 }
