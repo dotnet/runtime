a702f0d17c93d3b83418d53faf6d837664877d40
Milos Kotlar <kotlarmilos@gmail.com>
2023-11-02 22:32:24 +0100

    [mono] Update Swift interop smoke test

--- src/tests/Interop/Swift/ErrorHandling.swift
+++ src/tests/Interop/Swift/ErrorHandling.swift
@@ -1,11 +1,11 @@
 import Foundation
 
 public enum MyError: Error {
-    case runtimeError(message: String, info: String)
+    case runtimeError(message: String)
 }
 
 public func someFuncThatMightThrow (willThrow: Bool, dummy: UnsafeRawPointer) throws -> Int {
-    if willThrow { throw MyError.runtimeError (message: "Catch me if you can!", info: "abcd abcd"); }
+    if willThrow { throw MyError.runtimeError (message: "Catch me if you can!"); }
     else { return 42; }
 }
 
@@ -17,7 +17,7 @@ public func handleError(from pointer: UnsafePointer<MyError>) {
     if let offsetErrorPointer = offsetPointer?.assumingMemoryBound(to: MyError.self) {
         let errorInstance = offsetErrorPointer.pointee
         switch errorInstance {
-        case .runtimeError(let message, _):
+        case .runtimeError(let message):
             print(message)
         }
     } else {
