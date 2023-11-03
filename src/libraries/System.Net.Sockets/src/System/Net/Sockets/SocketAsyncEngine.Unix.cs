46b8077ce90e35a40d2f9ce569c53a64b919babf
Eduardo Velarde <eduardo.velarde@pucp.pe>
2023-11-03 15:15:27 -0700

    Revert "Remove Memory Barrier"
    This reverts commit 52d488a9dd7a555deddf4b51c1043f53b1400a62.

--- src/libraries/System.Net.Sockets/src/System/Net/Sockets/SocketAsyncEngine.Unix.cs
+++ src/libraries/System.Net.Sockets/src/System/Net/Sockets/SocketAsyncEngine.Unix.cs
@@ -238,6 +238,7 @@ namespace System.Net.Sockets
             {
                 Debug.Assert(_eventQueueProcessingStage == (int)EventQueueProcessingStage.Scheduled);
                 _eventQueueProcessingStage = (int)EventQueueProcessingStage.Determining;
+                Interlocked.MemoryBarrier();
 
                 if (eventQueue.TryDequeue(out ev))
                 {
