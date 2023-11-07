29399966a9fc8a693e8492a180b69df049ffe855
Milos Kotlar <kotlarmilos@gmail.com>
2023-11-07 16:38:11 +0100

    Remove helper script

--- src/tests/Interop/Swift/demangle_symbols.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Get symbols using nm
-nm $1 > symbols.txt
-
-# Strip _$ and demangle
-cat symbols.txt | awk '{print $3}' | sed 's/_$//g' | xargs -I {} xcrun swift-demangle {} > demangled_symbols.txt
-
-echo "Demangled symbols are exported to demangled_symbols.txt."
