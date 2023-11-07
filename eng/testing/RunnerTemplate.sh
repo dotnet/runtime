a57ced6f38571498e76bc3ef9614e3006266e03e
Carlos Sánchez López <1175054+carlossanlop@users.noreply.github.com>
2023-11-07 09:04:37 -0800

    RunnerTemplate.sh: Invoke XUnitLogChecker

--- eng/testing/RunnerTemplate.sh
+++ eng/testing/RunnerTemplate.sh
@@ -54,76 +54,24 @@ exitcode_list[131]="SIGQUIT Ctrl-\ occurred. Core dumped."
 exitcode_list[132]="SIGILL  Illegal Instruction. Core dumped. Likely codegen issue."
 exitcode_list[133]="SIGTRAP Breakpoint hit. Core dumped."
 exitcode_list[134]="SIGABRT Abort. Managed or native assert, or runtime check such as heap corruption, caused call to abort(). Core dumped."
-exitcode_list[135]="IGBUS  Unaligned memory access. Core dumped."
+exitcode_list[135]="IGBUS   Unaligned memory access. Core dumped."
 exitcode_list[136]="SIGFPE  Bad floating point arguments. Core dumped."
 exitcode_list[137]="SIGKILL Killed eg by kill"
 exitcode_list[139]="SIGSEGV Illegal memory access. Deref invalid pointer, overrunning buffer, stack overflow etc. Core dumped."
 exitcode_list[143]="SIGTERM Terminated. Usually before SIGKILL."
 exitcode_list[159]="SIGSYS  Bad System Call."
 
-function print_info_from_core_file_using_lldb {
-  local core_file_name=$1
-  local executable_name=$2
-  local plugin_path_name="$RUNTIME_PATH/shared/Microsoft.NETCore.App/9.9.9/libsosplugin.so"
-
-  # check for existence of lldb on the path
-  hash lldb 2>/dev/null || { echo >&2 "lldb was not found. Unable to print core file."; return; }
-
-  # pe, clrstack, and dumpasync are defined in libsosplugin.so
-  if [ ! -f $plugin_path_name ]; then
-    echo $plugin_path_name cannot be found.
-    return
-  fi
-
-  echo ----- start ===============  lldb Output =====================================================
-  echo Printing managed exceptions, managed call stacks, and async state machines.
-  lldb -O "settings set target.exec-search-paths $RUNTIME_PATH" -o "plugin load $plugin_path_name" -o "clrthreads -managedexception" -o "pe -nested" -o "clrstack -all -a -f" -o "dumpasync -fields -stacks -roots" -o "quit"  --core $core_file_name $executable_name
-  echo ----- end ===============  lldb Output =======================================================
-}
-
-function print_info_from_core_file_using_gdb {
-  local core_file_name=$1
-  local executable_name=$2
-
-  # Check for the existence of GDB on the path
-  hash gdb 2>/dev/null || { echo >&2 "GDB was not found. Unable to print core file."; return; }
-
-  echo ----- start ===============  GDB Output =====================================================
-  # Open the dump in GDB and print the stack from each thread. We can add more
-  # commands here if desired.
-  echo printing native stack.
-  gdb --batch -ex "thread apply all bt full" -ex "quit" $executable_name $core_file_name
-  echo ----- end ===============  GDB Output =======================================================
-}
-
-function print_info_from_core_file {
-  local core_file_name=$1
-  local executable_name=$RUNTIME_PATH/$2
-
-  if ! [ -e $executable_name ]; then
-    echo "Unable to find executable $executable_name"
-    return
-  elif ! [ -e $core_file_name ]; then
-    echo "Unable to find core file $core_file_name"
-    return
-  fi
-  echo "Printing info from core file $core_file_name"
-  print_info_from_core_file_using_gdb $core_file_name $executable_name
-  print_info_from_core_file_using_lldb $core_file_name $executable_name
-}
-
-function copy_core_file_to_temp_location {
+function move_core_file_to_temp_location {
   local core_file_name=$1
 
-  local storage_location="/tmp/coredumps"
+  # Append the dmp extension to ensure XUnitLogChecker finds it
+  local new_location=$HELIX_DUMP_FOLDER/core.$RANDOM.dmp
 
-  # Create the directory (this shouldn't fail even if it already exists).
-  mkdir -p $storage_location
-
-  local new_location=$storage_location/core.$RANDOM
-
-  echo "Copying core file $core_file_name to $new_location in case you need it."
+  echo "Copying dump file '$core_file_name' to '$new_location'"
   cp $core_file_name $new_location
+
+  # Delete the old one
+  rm $core_file_name
 }
 
 # ========================= BEGIN Core File Setup ============================
@@ -155,6 +103,10 @@ elif [[ "$(uname -s)" == "Linux" ]]; then
 
   ulimit -c unlimited
 fi
+
+export DOTNET_DbgEnableMiniDump=1
+export DOTNET_EnableCrashReport=1
+export DOTNET_DbgMiniDumpName=$HELIX_DUMP_FOLDER/coredump.%d.dmp
 # ========================= END Core File Setup ==============================
 
 # ========================= BEGIN support for SuperPMI collection ==============================
@@ -230,7 +182,7 @@ if [[ "$(uname -s)" == "Linux" && $test_exitcode -ne 0 ]]; then
 
     have_sleep=$(which sleep)
     if [ -x "$have_sleep" ]; then
-      echo Waiting a few seconds for any dump to be written..
+      echo Waiting a few seconds for any dump to be written...
       sleep 10s
     fi
   fi
@@ -239,7 +191,7 @@ if [[ "$(uname -s)" == "Linux" && $test_exitcode -ne 0 ]]; then
   echo cat /proc/sys/kernel/core_uses_pid: $(cat /proc/sys/kernel/core_uses_pid)
   echo cat /proc/sys/kernel/coredump_filter: $(cat /proc/sys/kernel/coredump_filter)
 
-  echo Looking around for any Linux dump..
+  echo Looking around for any Linux dumps...
 
   # Depending on distro/configuration, the core files may either be named "core"
   # or "core.<PID>" by default. We read /proc/sys/kernel/core_uses_pid to
@@ -252,18 +204,47 @@ if [[ "$(uname -s)" == "Linux" && $test_exitcode -ne 0 ]]; then
   if [[ "$core_name_uses_pid" == "1" ]]; then
     # We don't know what the PID of the process was, so let's look at all core
     # files whose name matches core.NUMBER
-    echo Looking for files matching core.* ...
-    for f in core.*; do
-      [[ $f =~ core.[0-9]+ ]] && print_info_from_core_file "$f" "dotnet" && copy_core_file_to_temp_location "$f" && rm "$f"
+    echo "Looking for files matching core.*"
+    for f in $(find . -name "core.*"); do
+      [[ $f =~ core.[0-9]+ ]] && move_core_file_to_temp_location "$f"
     done
-  elif [ -f core ]; then
-    echo found a dump named core in $EXECUTION_DIR !
-    print_info_from_core_file "core" "dotnet"
-    copy_core_file_to_temp_location "core"
-    rm "core"
-  else
-    echo ... found no dump in $PWD
   fi
+
+  if [ -f core ]; then
+    move_core_file_to_temp_location "core"
+  fi
+
+  total_dumps=$(find $HELIX_DUMP_FOLDER -name "*.dmp" | wc -l)
+  
+  echo ----- start ===============  XUnitLogChecker Output =====================================================
+  xunitlogchecker_exit_code=0
+  if [ $total_dumps -gt 0 ]; then
+    
+    echo "Total dumps found in $HELIX_DUMP_FOLDER: $total_dumps"
+    xunitlogchecker_file_name="$HELIX_CORRELATION_PAYLOAD/XUnitLogChecker.dll"
+    dotnet_file_name="$RUNTIME_PATH/dotnet"
+
+    if [ ! -f $dotnet_file_name ]; then
+      echo "'$dotnet_file_name' was not found. Unable to run XUnitLogChecker."
+      xunitlogchecker_exit_code=1
+    elif [ ! -f $xunitlogchecker_file_name ]; then 
+      echo "'$xunitlogchecker_file_name' was not found. Unable to print dump file contents."
+      xunitlogchecker_exit_code=2
+    else
+      cmd="$dotnet_file_name --roll-forward Major $xunitlogchecker_file_name --dumps-path $HELIX_DUMP_FOLDER"
+      echo "$cmd"
+      $cmd
+      xunitlogchecker_exit_code=$?
+    fi
+  else
+    echo "No dumps found."
+  fi
+
+  if [ $xunitlogchecker_exit_code -ne 0 ]; then
+    test_exitcode = $xunitlogchecker_exit_code
+  fi
+  echo ----- end ===============  XUnitLogChecker Output - exit code $xunitlogchecker_exit_code ===========================
+
 fi
 popd >/dev/null
 # ======================== END Core File Inspection ==========================
