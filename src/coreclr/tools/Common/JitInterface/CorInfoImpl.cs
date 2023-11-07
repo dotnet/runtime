66653b97938134af4905251078cb1989b9bc6512
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2023-11-07 08:55:51 +0100

    Disallow inlining Main
    When we compile managed code, `Main` is not the actual spot where execution of managed code starts. Instead it's the `StartupCodeMain` method that the compiler generates. This method is responsible for initializing the managed environment, calling `Main` and tearing down the environment. If `Main` is short enough, sometimes it gets inlined into `StartupCodeMain` this has bad impact on diagnostics (don't see `Main` in stack traces, can't set breakpoints). Pretend it was marked `NoInlining`.

--- src/coreclr/tools/Common/JitInterface/CorInfoImpl.cs
+++ src/coreclr/tools/Common/JitInterface/CorInfoImpl.cs
@@ -1129,6 +1129,15 @@ namespace Internal.JitInterface
             {
                 result |= CorInfoFlag.CORINFO_FLG_FORCEINLINE;
             }
+#if !READYTORUN
+            else if (method.OwningType is EcmaType ecmaOwningType
+                && ecmaOwningType.EcmaModule.EntryPoint == method)
+            {
+                // Mark entrypoint as NoInlining for better debugging. We don't want it to inline
+                // into startup code because breakpoints won't hit and stack traces won't have Main.
+                result |= CorInfoFlag.CORINFO_FLG_DONT_INLINE;
+            }
+#endif
 
             if (method.OwningType.IsDelegate && method.Name == "Invoke")
             {
