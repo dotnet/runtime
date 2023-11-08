14405762feb57605d8a63037e4a6110634e09635
andreygolubkow <andreygolubkow@gmail.com>
2023-11-08 15:51:34 +0400

    Removed unnecessary using
    In Program.cs of ilverify the using was removed, because it unused
    
    Fix #93361

--- src/coreclr/tools/ILVerify/Program.cs
+++ src/coreclr/tools/ILVerify/Program.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.CommandLine;
-using System.CommandLine.Parsing;
 using System.IO;
 using System.Reflection;
 using System.Reflection.Metadata;
