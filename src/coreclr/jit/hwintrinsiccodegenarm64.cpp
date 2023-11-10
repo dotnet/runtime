2ac0117eb73866e22adaf057e0d7d47972152cd7
Swapnil Gaikwad <swapnil.gaikwad@arm.com>
2023-11-10 13:26:58 +0000

    Implement StoreVectorAndZipNxM for Arm64 N=(64,128) & M=(3, 4)

--- src/coreclr/jit/hwintrinsiccodegenarm64.cpp
+++ src/coreclr/jit/hwintrinsiccodegenarm64.cpp
@@ -796,6 +796,12 @@ void CodeGen::genHWIntrinsic(GenTreeHWIntrinsic* node)
                 GetEmitter()->emitIns_R_R_R(ins, emitTypeSize(intrin.baseType), op2Reg, op3Reg, op1Reg);
                 break;
 
+            case NI_AdvSimd_StoreVector64x2AndZip:
+            case NI_AdvSimd_StoreVector64x3AndZip:
+            case NI_AdvSimd_StoreVector64x4AndZip:
+            case NI_AdvSimd_Arm64_StoreVector128x2AndZip:
+            case NI_AdvSimd_Arm64_StoreVector128x3AndZip:
+            case NI_AdvSimd_Arm64_StoreVector128x4AndZip:
             case NI_AdvSimd_StoreVector64x2:
             case NI_AdvSimd_StoreVector64x3:
             case NI_AdvSimd_StoreVector64x4:
