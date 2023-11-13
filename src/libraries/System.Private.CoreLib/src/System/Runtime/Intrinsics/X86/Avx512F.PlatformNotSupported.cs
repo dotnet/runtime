4726a6ccfa06dd880cfb0636cc5372bc96f4660a
Ruihan-Yin <ruihan.yin@intel.com>
2023-11-13 12:39:39 -0800

    Remove unexpected comment-out

--- src/libraries/System.Private.CoreLib/src/System/Runtime/Intrinsics/X86/Avx512F.PlatformNotSupported.cs
+++ src/libraries/System.Private.CoreLib/src/System/Runtime/Intrinsics/X86/Avx512F.PlatformNotSupported.cs
@@ -1332,11 +1332,11 @@ namespace System.Runtime.Intrinsics.X86
         ///   VPADDQ zmm1 {k1}{z}, zmm2, zmm3/m512/m64bcst
         /// </summary>
         public static Vector512<ulong> Add(Vector512<ulong> left, Vector512<ulong> right) { throw new PlatformNotSupportedException(); }
-        // /// <summary>
-        // /// __m512d _mm512_add_pd (__m512d a, __m512d b)
-        // ///   VADDPD zmm1 {k1}{z}, zmm2, zmm3/m512/m64bcst{er}
-        // /// </summary>
-        // public static Vector512<double> Add(Vector512<double> left, Vector512<double> right) { throw new PlatformNotSupportedException(); }
+        /// <summary>
+        /// __m512d _mm512_add_pd (__m512d a, __m512d b)
+        ///   VADDPD zmm1 {k1}{z}, zmm2, zmm3/m512/m64bcst{er}
+        /// </summary>
+        public static Vector512<double> Add(Vector512<double> left, Vector512<double> right) { throw new PlatformNotSupportedException(); }
         /// <summary>
         /// __m512d _mm512_add_pd (__m512d a, __m512d b)
         ///   VADDPD zmm1 {k1}{z}, zmm2, zmm3/m512/m64bcst{er}
