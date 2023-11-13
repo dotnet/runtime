af9e24526fcd3392227a734509598cf59f6eceb6
Andrew Au <andrewau@microsoft.com>
2023-11-13 08:29:32 -0800

    Fixes the build when IlcStandaloneGCSupport is specified (#94624)

--- src/coreclr/nativeaot/Runtime/gcrhenv.cpp
+++ src/coreclr/nativeaot/Runtime/gcrhenv.cpp
@@ -1125,6 +1125,11 @@ void GCToEEInterface::UpdateGCEventStatus(int currentPublicLevel, int currentPub
     // TODO: Linux LTTng
 }
 
+void GCToEEInterface::LogStressMsg(unsigned level, unsigned facility, const StressLogMsg& msg)
+{
+    // TODO: Implementation
+}
+
 uint32_t GCToEEInterface::GetCurrentProcessCpuCount()
 {
     return PalGetProcessCpuCount();
