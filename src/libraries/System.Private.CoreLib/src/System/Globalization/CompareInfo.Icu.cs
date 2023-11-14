e7111bd9d92d0213aceb4cf8a18ea4b39a04b0bd
Meri Khamoyan <merikhamoyan@microsoft.com>
2023-11-14 16:25:00 +0100

    Try without checking hybrid mode

--- src/libraries/System.Private.CoreLib/src/System/Globalization/CompareInfo.Icu.cs
+++ src/libraries/System.Private.CoreLib/src/System/Globalization/CompareInfo.Icu.cs
@@ -24,7 +24,10 @@ namespace System.Globalization
             _isAsciiEqualityOrdinal = GetIsAsciiEqualityOrdinal(interopCultureName);
             if (!GlobalizationMode.Invariant)
             {
-#if TARGET_MACCATALYST || TARGET_IOS || TARGET_TVOS || TARGET_BROWSER
+#if TARGET_MACCATALYST || TARGET_IOS || TARGET_TVOS
+                    return;
+#endif
+#if TARGET_BROWSER
                 if (GlobalizationMode.Hybrid)
                     return;
 #endif
