e58f897a1466bde099cef6d59d1890315f478a42
Carlos Sánchez López <1175054+carlossanlop@users.noreply.github.com>
2023-11-15 13:08:41 -0800

    Revert "Add XUnitLogChecker to log libraries dumps (#93906)" (#94807)
    This reverts commit d561f390b8085ffed309c49cf30ec205a47868c0.

--- src/tests/Common/XUnitLogChecker/XUnitLogChecker.cs
+++ src/tests/Common/XUnitLogChecker/XUnitLogChecker.cs
@@ -489,14 +489,15 @@ public class XUnitLogChecker
             );
 
             dumpsFound = Directory
-                         .GetFiles(s_configuration.DumpsPath, "*.dmp")
+                         .GetFiles(s_configuration.DumpsPath, "*coredump*.dmp")
                          .Where(dmp =>
                                     DateTime.Compare(File.GetCreationTime(dmp),
                                                      testRunDateTime) >= 0);
         }
         else
         {
-            dumpsFound = Directory.GetFiles(s_configuration.DumpsPath, "*.dmp");
+            dumpsFound = Directory.GetFiles(s_configuration.DumpsPath,
+                                            "*coredump*.dmp");
         }
 
         if (dumpsFound.Count() == 0)
@@ -509,7 +510,7 @@ public class XUnitLogChecker
         {
             if (OperatingSystem.IsWindows())
             {
-                WriteLineTimestamp($"Reading crash dump '{dumpPath}'...");
+                WriteLineTimestamp("Reading crash dump '{dumpPath}'...");
                 WriteLineTimestamp("Stack Trace Found:\n");
 
                 CoreclrTestWrapperLib.TryPrintStackTraceFromDmp(dumpPath,
@@ -526,7 +527,7 @@ public class XUnitLogChecker
                     continue;
                 }
 
-                WriteLineTimestamp($"Reading crash report '{crashReportPath}'...");
+                WriteLineTimestamp("Reading crash report '{crashReportPath}'...");
                 WriteLineTimestamp("Stack Trace Found:\n");
 
                 CoreclrTestWrapperLib.TryPrintStackTraceFromCrashReport(crashReportPath,
