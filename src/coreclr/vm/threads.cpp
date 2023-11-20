f96f636a1e39ab2e5d73500cf16df0d377c2ec92
Grégoire Verdier <g.verdier@criteo.com>
2023-11-20 16:00:07 -0500

    Fix uninitialized variable

--- src/coreclr/vm/threads.cpp
+++ src/coreclr/vm/threads.cpp
@@ -3179,7 +3179,7 @@ DWORD Thread::DoAppropriateWait(int countHandles, HANDLE *handles, BOOL waitAll,
     param.mode = mode;
     param.dwRet = (DWORD) -1;
 
-    LARGE_INTEGER startTicks;
+    LARGE_INTEGER startTicks = { { 0 } };
     bool isWaitHandleKeywordEnabled = ETW_TRACING_CATEGORY_ENABLED(MICROSOFT_WINDOWS_DOTNETRUNTIME_PROVIDER_DOTNET_Context, TRACE_LEVEL_VERBOSE, CLR_WAITHANDLE_KEYWORD);
     if (isWaitHandleKeywordEnabled)
     {
