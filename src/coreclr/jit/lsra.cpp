6d8545db7d0a2d2f060b2431ab659797a7a246ee
Bruce Forstall <brucefo@microsoft.com>
2023-11-21 14:23:39 -0800

    Updated comment to be more descriptive.

--- src/coreclr/jit/lsra.cpp
+++ src/coreclr/jit/lsra.cpp
@@ -12458,7 +12458,14 @@ regMaskTP LinearScan::RegisterSelection::select(Interval*    currentInterval,
         candidates &= ~busyRegs;
 
 #ifdef TARGET_ARM
-        // For TYP_DOUBLE on ARM, we can only use register for which the odd half is also available.
+        // For TYP_DOUBLE on ARM, we can only use an even floating-point register for which the odd half
+        // is also available. Thus, for every busyReg that is an odd floating-point register, we need to
+        // remove from candidates the corresponding even floating-point register. For example, if busyRegs
+        // contains `f3`, we need to remove `f2` from the candidates for a double register interval. The
+        // `~(busyRegs >> 1)` clause below creates a mask to do this. Note one oddity: if busyRegs contained
+        // f0, we would mask off some general-purpose register. If it contains any general-purpose registers,
+        // the mask will also contain general-purpose registers. That's ok because candidates for a double
+        // type starts off as only the even floating-point registers, so masking off anything else is a no-op.
         if (currentInterval->registerType == TYP_DOUBLE)
         {
             candidates &= ~(busyRegs >> 1);
