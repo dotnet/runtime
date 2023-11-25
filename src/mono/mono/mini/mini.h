2f8ef582a8e2e7e7996267694cd7ac73f9e8f34b
Sanjam Panda <36253777+saitama951@users.noreply.github.com>
2023-11-25 12:43:59 +0530

    enable div optimization (#95201)

--- src/mono/mono/mini/mini.h
+++ src/mono/mono/mini/mini.h
@@ -1257,7 +1257,6 @@ typedef struct {
 	guint            no_unaligned_access : 1;
 	guint            disable_div_with_mul : 1;
 	guint            explicit_null_checks : 1;
-	guint            optimized_div : 1;
 	int              monitor_enter_adjustment;
 	int              dyn_call_param_area;
 } MonoBackend;
