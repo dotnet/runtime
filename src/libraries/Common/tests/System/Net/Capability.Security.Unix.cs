1ede5678375f23cd32a6f78861a145a0798f6085
Radek Zikmund <32671551+rzikm@users.noreply.github.com>
2023-11-29 14:03:24 +0100

    Update src/libraries/Common/tests/System/Net/Capability.Security.Unix.cs
    Co-authored-by: Adeel Mujahid <3840695+am11@users.noreply.github.com>

--- src/libraries/Common/tests/System/Net/Capability.Security.Unix.cs
+++ src/libraries/Common/tests/System/Net/Capability.Security.Unix.cs
@@ -11,7 +11,7 @@ namespace System.Net.Test.Common
         {
             return
                 // Linux bionic uses managed NTLM implementation
-                OperatingSystem.IsLinux() && RuntimeInformation.RuntimeIdentifier.StartsWith("linux-bionic-", StringComparison.OrdinalIgnoreCase) ||
+                (OperatingSystem.IsLinux() && RuntimeInformation.RuntimeIdentifier.StartsWith("linux-bionic-", StringComparison.Ordinal)) ||
                 // GSS on Linux does not work with OpenSSL 3.0. Fix was submitted to gss-ntlm but it will take a while to make to
                 // all supported distributions. The second part of the check should be removed when it does.
                 Interop.NetSecurityNative.IsNtlmInstalled() && (!PlatformDetection.IsOpenSslSupported || PlatformDetection.OpenSslVersion.Major < 3);
