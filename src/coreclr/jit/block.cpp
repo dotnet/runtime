c64c8516f5f18e354894cdb326b6c975af4e046f
Aman Khalid (from Dev Box) <amankhalid@microsoft.com>
2023-11-29 20:55:18 -0500

    Style

--- src/coreclr/jit/block.cpp
+++ src/coreclr/jit/block.cpp
@@ -297,7 +297,7 @@ bool BasicBlock::IsFirstColdBlock(Compiler* compiler) const
 bool BasicBlock::CanRemoveJumpToNext(Compiler* compiler)
 {
     assert(KindIs(BBJ_ALWAYS));
-    return (JumpsToNext() && !hasAlign() && !compiler->fgInDifferentRegions(this, bbJumpDest));
+    return JumpsToNext() && !hasAlign() && !compiler->fgInDifferentRegions(this, bbJumpDest);
 }
 
 //------------------------------------------------------------------------
