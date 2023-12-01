f315946d4d9d7b4b6f1eae9ed856a3dc3f65a60f
Elinor Fung <elfung@microsoft.com>
2023-11-30 19:49:21 -0800

    Switch HostApiInvokerApp to pre-built test asset

--- src/installer/tests/HostActivation.Tests/NativeHostApis.cs
+++ src/installer/tests/HostActivation.Tests/NativeHostApis.cs
@@ -54,14 +54,14 @@ namespace Microsoft.DotNet.CoreSetup.Test.HostActivation
 
         private class SdkResolutionFixture
         {
-            private readonly TestProjectFixture _fixture;
+            private readonly TestApp _app;
 
             public DotNetCli Dotnet { get; }
-            public string AppDll => _fixture.TestProject.AppDll;
-            public string ExeDir => Path.Combine(_fixture.TestProject.ProjectDirectory, "ed");
+            public string AppDll => _app.AppDll;
+            public string ExeDir => Path.Combine(_app.Location, "ed");
             public string ProgramFiles => Path.Combine(ExeDir, "pf");
             public string SelfRegistered => Path.Combine(ExeDir, "sr");
-            public string WorkingDir => Path.Combine(_fixture.TestProject.ProjectDirectory, "wd");
+            public string WorkingDir => Path.Combine(_app.Location, "wd");
             public string ProgramFilesGlobalSdkDir => Path.Combine(ProgramFiles, "dotnet", "sdk");
             public string ProgramFilesGlobalFrameworksDir => Path.Combine(ProgramFiles, "dotnet", "shared");
             public string SelfRegisteredGlobalSdkDir => Path.Combine(SelfRegistered, "sdk");
@@ -88,7 +88,7 @@ namespace Microsoft.DotNet.CoreSetup.Test.HostActivation
             {
                 Dotnet = TestContext.BuiltDotNet;
 
-                _fixture = state.HostApiInvokerAppFixture.Copy();
+                _app = state.HostApiInvokerApp.Copy();
 
                 Directory.CreateDirectory(WorkingDir);
 
@@ -266,9 +266,7 @@ namespace Microsoft.DotNet.CoreSetup.Test.HostActivation
         [Fact]
         public void Hostfxr_corehost_set_error_writer_test()
         {
-            var fixture = sharedTestState.HostApiInvokerAppFixture.Copy();
-
-            fixture.BuiltDotnet.Exec(fixture.TestProject.AppDll, "Test_hostfxr_set_error_writer")
+            TestContext.BuiltDotNet.Exec(sharedTestState.HostApiInvokerApp.AppDll, "Test_hostfxr_set_error_writer")
                 .CaptureStdOut()
                 .CaptureStdErr()
                 .Execute()
@@ -482,9 +480,7 @@ namespace Microsoft.DotNet.CoreSetup.Test.HostActivation
         [Fact]
         public void Hostpolicy_corehost_set_error_writer_test()
         {
-            var fixture = sharedTestState.HostApiInvokerAppFixture.Copy();
-
-            fixture.BuiltDotnet.Exec(fixture.TestProject.AppDll, "Test_corehost_set_error_writer")
+            TestContext.BuiltDotNet.Exec(sharedTestState.HostApiInvokerApp.AppDll, "Test_corehost_set_error_writer")
                 .CaptureStdOut()
                 .CaptureStdErr()
                 .Execute()
@@ -494,22 +490,33 @@ namespace Microsoft.DotNet.CoreSetup.Test.HostActivation
         [Fact]
         public void HostRuntimeContract_get_runtime_property()
         {
-            var fixture = sharedTestState.HostApiInvokerAppFixture;
-
-            fixture.BuiltDotnet.Exec(fixture.TestProject.AppDll, "host_runtime_contract.get_runtime_property", "APP_CONTEXT_BASE_DIRECTORY", "RUNTIME_IDENTIFIER", "DOES_NOT_EXIST", "ENTRY_ASSEMBLY_NAME")
+            TestApp app = sharedTestState.HostApiInvokerApp;
+            TestContext.BuiltDotNet.Exec(app.AppDll, "host_runtime_contract.get_runtime_property", "APP_CONTEXT_BASE_DIRECTORY", "RUNTIME_IDENTIFIER", "DOES_NOT_EXIST", "ENTRY_ASSEMBLY_NAME")
                 .CaptureStdOut()
                 .CaptureStdErr()
                 .Execute()
                 .Should().Pass()
-                .And.HaveStdOutContaining($"APP_CONTEXT_BASE_DIRECTORY = {Path.GetDirectoryName(fixture.TestProject.AppDll)}")
+                .And.HaveStdOutContaining($"APP_CONTEXT_BASE_DIRECTORY = {Path.GetDirectoryName(app.AppDll)}")
                 .And.HaveStdOutContaining($"RUNTIME_IDENTIFIER = {TestContext.BuildRID}")
                 .And.HaveStdOutContaining($"DOES_NOT_EXIST = <none>")
-                .And.HaveStdOutContaining($"ENTRY_ASSEMBLY_NAME = {fixture.TestProject.AssemblyName}");
+                .And.HaveStdOutContaining($"ENTRY_ASSEMBLY_NAME = {app.AssemblyName}");
+        }
+
+        [Fact]
+        public void HostRuntimeContract_bundle_probe()
+        {
+            TestContext.BuiltDotNet.Exec(sharedTestState.HostApiInvokerApp.AppDll, "host_runtime_contract.bundle_probe", "APP_CONTEXT_BASE_DIRECTORY", "RUNTIME_IDENTIFIER", "DOES_NOT_EXIST", "ENTRY_ASSEMBLY_NAME")
+                .CaptureStdOut()
+                .CaptureStdErr()
+                .Execute()
+                .Should().Pass()
+                .And.HaveStdOutContaining("host_runtime_contract.bundle_probe is not set");
         }
 
         public class SharedTestState : IDisposable
         {
-            public TestProjectFixture HostApiInvokerAppFixture { get; }
+            public DotNetCli BuiltDotNet { get; }
+            public TestApp HostApiInvokerApp { get; }
             public TestProjectFixture PortableAppFixture { get; }
             public RepoDirectoriesProvider RepoDirectories { get; }
 
@@ -517,11 +524,7 @@ namespace Microsoft.DotNet.CoreSetup.Test.HostActivation
 
             public SharedTestState()
             {
-                RepoDirectories = new RepoDirectoriesProvider();
-
-                HostApiInvokerAppFixture = new TestProjectFixture("HostApiInvokerApp", RepoDirectories)
-                    .EnsureRestored()
-                    .BuildProject();
+                HostApiInvokerApp = TestApp.CreateFromBuiltAssets("HostApiInvokerApp");
 
                 PortableAppFixture = new TestProjectFixture("PortableApp", RepoDirectories)
                     .EnsureRestored()
@@ -538,16 +541,13 @@ namespace Microsoft.DotNet.CoreSetup.Test.HostActivation
                     Directory.CreateDirectory(BreadcrumbLocation);
 
                     // On non-Windows, we can't just P/Invoke to already loaded hostfxr, so copy it next to the app dll.
-                    var fixture = HostApiInvokerAppFixture;
-                    FileUtils.CopyIntoDirectory(
-                        fixture.BuiltDotnet.GreatestVersionHostFxrFilePath,
-                        Path.GetDirectoryName(fixture.TestProject.AppDll));
+                    File.Copy(Binaries.HostFxr.FilePath, Path.Combine(HostApiInvokerApp.Location, Binaries.HostFxr.FileName));
                 }
             }
 
             public void Dispose()
             {
-                HostApiInvokerAppFixture.Dispose();
+                HostApiInvokerApp?.Dispose();
                 PortableAppFixture.Dispose();
             }
         }
