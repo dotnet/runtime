063cd10668127b82d1827a3e46b87b1045bb877b
EgorBo <egorbo@gmail.com>
2023-12-03 02:41:48 +0100

    Add another test case

--- src/coreclr/jit/gentree.h
+++ src/coreclr/jit/gentree.h
@@ -7323,7 +7323,9 @@ public:
 #ifdef TARGET_XARCH
         BlkOpKindCpObjRepInstr,
 #endif
+#ifndef TARGET_X86
         BlkOpKindHelper,
+#endif
 #ifdef TARGET_XARCH
         BlkOpKindRepInstr,
 #endif
