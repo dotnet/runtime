152fa6e70cd345307e3972fc38895c9ddee67f59
Milos Kotlar <kotlarmilos@gmail.com>
2023-12-04 16:33:16 +0100

    Use NativeLibrary.Free for Swift interop scenarios

--- src/tests/Interop/Swift/SwiftErrorHandling/SwiftErrorHandling.cs
+++ src/tests/Interop/Swift/SwiftErrorHandling/SwiftErrorHandling.cs
@@ -64,7 +64,7 @@ public class ErrorHandlingTests
         IntPtr pointer = GetErrorMessage(error.Value);
         byte[] byteArray = new byte[length];
         Marshal.Copy(pointer, byteArray, 0, length);
-        Marshal.FreeCoTaskMem(pointer);
+        NativeLibrary.Free(pointer);
         return Encoding.UTF8.GetString(byteArray);
     }
 }
