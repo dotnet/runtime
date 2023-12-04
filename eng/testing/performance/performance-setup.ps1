a7201afba0442a469c50024977cf3d3d511a21e7
Parker Bibus <parkerbibus@microsoft.com>
2023-12-04 13:36:07 -0800

    Update Perf Android jobs to use the Windows 11 Pixel Queue.

--- eng/testing/performance/performance-setup.ps1
+++ eng/testing/performance/performance-setup.ps1
@@ -50,7 +50,7 @@ if ($Internal) {
         "perftiger_crossgen" { $Queue = "Windows.10.Amd64.19H1.Tiger.Perf" }
         "perfowl" { $Queue = "Windows.10.Amd64.20H2.Owl.Perf" }
         "perfsurf" { $Queue = "Windows.10.Arm64.Perf.Surf" }
-        "perfpixel4a" { $Queue = "Windows.10.Amd64.Pixel.Perf" }
+        "perfpixel4a" { $Queue = "Windows.11.Amd64.Pixel.Perf" }
         "perfampere" { $Queue = "Windows.Server.Arm64.Perf" }
         Default { $Queue = "Windows.10.Amd64.19H1.Tiger.Perf" }
     }
