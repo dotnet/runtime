e9f806af9b18197562699354adb20df452f56f6a
Milos Kotlar <kotlarmilos@gmail.com>
2023-12-06 15:05:39 +0100

    Remove additional deref register

--- src/mono/mono/mini/jit-icalls.c
+++ src/mono/mono/mini/jit-icalls.c
@@ -1483,9 +1483,8 @@ mono_gsharedvt_constrained_call (gpointer mp, MonoMethod *cmethod, MonoClass *kl
 		g_assert (fsig->param_count < 16);
 		memcpy (new_args, args, fsig->param_count * sizeof (gpointer));
 		for (int i = 0; i < fsig->param_count; ++i) {
-			// If the argument is a ref or nullable non-vtype, deref it
-			MonoClass *pklass = mono_class_from_mono_type_internal (fsig->params [i]);
-			if (deref_args [i] && !m_class_is_valuetype (pklass))
+			// If the argument is not a vtype or nullable, deref it
+			if (deref_args [i] != MONO_GSHAREDVT_BOX_TYPE_VTYPE && deref_args [i] != MONO_GSHAREDVT_BOX_TYPE_NULLABLE)
 				new_args [i] = *(gpointer*)new_args [i];
 		}
 		args = new_args;
