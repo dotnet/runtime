0aaac7867f366465a6dd7613433a535301edd7f3
Khushal Modi <kcmodi@asu.edu>
2023-12-06 14:41:24 -0800

    removing conflicts from gentree.h flags merging with main doubel to uint conversion

--- src/coreclr/jit/instr.cpp
+++ src/coreclr/jit/instr.cpp
@@ -2387,11 +2387,8 @@ instruction CodeGen::ins_FloatConv(var_types to, var_types from, emitAttr attr)
                     return INS_cvtss2sd;
                 case TYP_ULONG:
                     return INS_vcvttss2usi64;
-<<<<<<< HEAD
                 case TYP_UINT:
                     return INS_vcvttss2usi32;
-=======
->>>>>>> 0ec0bd6bb4b (Initial changes for scalar conversion double -> ulong)
                 default:
                     unreached();
             }
@@ -2410,11 +2407,8 @@ instruction CodeGen::ins_FloatConv(var_types to, var_types from, emitAttr attr)
                     return ins_Move_Extend(TYP_DOUBLE, false);
                 case TYP_ULONG:
                     return INS_vcvttsd2usi64;
-<<<<<<< HEAD
                 case TYP_UINT:
                     return INS_vcvttsd2usi32;
-=======
->>>>>>> 0ec0bd6bb4b (Initial changes for scalar conversion double -> ulong)
                 default:
                     unreached();
             }
