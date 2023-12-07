37230be6d33c289a061759df6151b8089d0f3d45
Matous Kozak <matouskozak@seznam.cz>
2023-12-07 18:10:44 +0100

    null-terminate error message string

--- src/tests/Interop/Swift/SwiftErrorHandling/SwiftErrorHandling.cs
+++ src/tests/Interop/Swift/SwiftErrorHandling/SwiftErrorHandling.cs
@@ -55,7 +55,7 @@ public class ErrorHandlingTests
     
     private static void SetErrorMessageForSwift(string message)
     {
-        var messageBytes = Encoding.UTF8.GetBytes(message);
+        var messageBytes = Encoding.UTF8.GetBytes(message + char.MinValue); // Add null terminator
         SetErrorMessage(messageBytes, messageBytes.Length);
     }
 
