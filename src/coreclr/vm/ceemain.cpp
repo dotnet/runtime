4344375c9cffe2fe2cfbe950bf62a8696cfd4163
Andrew Au <andrewau@microsoft.com>
2023-12-08 11:19:39 -0800

    Delete unwanted change

--- src/coreclr/vm/ceemain.cpp
+++ src/coreclr/vm/ceemain.cpp
@@ -871,7 +871,6 @@ void EEStartupHelper()
         // requires write barriers to have been set up on x86, which happens as part
         // of InitJITHelpers1.
         hr = g_pGCHeap->Initialize();
-
         if (FAILED(hr))
         {
             LogErrorToHost("GC heap initialization failed with error 0x%08X", hr);
