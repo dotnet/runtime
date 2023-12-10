a931fb031e2354da4023e6f087621c207fcf41db
Zoltan Varga <vargaz@gmail.com>
2023-12-10 08:17:06 -0500

    Use the shared files in wasi.

--- src/mono/wasi/runtime/driver.c
+++ src/mono/wasi/runtime/driver.c
@@ -36,7 +36,7 @@
 #endif
 #include "gc-common.h"
 #include "driver.h"
-
+#include "runtime.h"
 
 #if !defined(ENABLE_AOT) || defined(EE_MODE_LLVMONLY_INTERP)
 #define NEED_INTERP 1
@@ -83,22 +83,8 @@ extern const char* dotnet_wasi_getentrypointassemblyname();
 int32_t mono_wasi_load_icu_data(const void* pData);
 void load_icu_data (void);
 
-int mono_wasm_enable_gc = 1;
-
-/* Not part of public headers */
-#define MONO_ICALL_TABLE_CALLBACKS_VERSION 3
-
-typedef struct {
-	int version;
-	void* (*lookup) (MonoMethod *method, char *classname, char *methodname, char *sigstart, int32_t *flags);
-	const char* (*lookup_icall_symbol) (void* func);
-} MonoIcallTableCallbacks;
-
 int
 mono_string_instance_is_interned (MonoString *str_raw);
-
-void
-mono_install_icall_table_callbacks (const MonoIcallTableCallbacks *cb);
 
 void mono_trace_init (void);
 
@@ -239,117 +225,7 @@ wasm_dl_symbol (void *handle, const char *name, char **err, void *user_data)
 
 #if !defined(ENABLE_AOT) || defined(EE_MODE_LLVMONLY_INTERP)
 #define NEED_INTERP 1
-#ifndef LINK_ICALLS
-// FIXME: llvm+interp mode needs this to call icalls
-#define NEED_NORMAL_ICALL_TABLES 1
 #endif
-#endif
-
-#ifdef LINK_ICALLS
-
-#include "icall-table.h"
-
-static int
-compare_int (const void *k1, const void *k2)
-{
-	return *(int*)k1 - *(int*)k2;
-}
-
-static void*
-icall_table_lookup (MonoMethod *method, char *classname, char *methodname, char *sigstart, int32_t *out_flags)
-{
-	uint32_t token = mono_method_get_token (method);
-	assert (token);
-	assert ((token & MONO_TOKEN_METHOD_DEF) == MONO_TOKEN_METHOD_DEF);
-	uint32_t token_idx = token - MONO_TOKEN_METHOD_DEF;
-
-	int *indexes = NULL;
-	int indexes_size = 0;
-	uint8_t *flags = NULL;
-	void **funcs = NULL;
-
-	*out_flags = 0;
-
-	const char *image_name = mono_image_get_name (mono_class_get_image (mono_method_get_class (method)));
-
-#if defined(ICALL_TABLE_corlib)
-	if (!strcmp (image_name, "System.Private.CoreLib")) {
-		indexes = corlib_icall_indexes;
-		indexes_size = sizeof (corlib_icall_indexes) / 4;
-		flags = corlib_icall_flags;
-		funcs = corlib_icall_funcs;
-		assert (sizeof (corlib_icall_indexes [0]) == 4);
-	}
-#endif
-#ifdef ICALL_TABLE_System
-	if (!strcmp (image_name, "System")) {
-		indexes = System_icall_indexes;
-		indexes_size = sizeof (System_icall_indexes) / 4;
-		flags = System_icall_flags;
-		funcs = System_icall_funcs;
-	}
-#endif
-	assert (indexes);
-
-	void *p = bsearch (&token_idx, indexes, indexes_size, 4, compare_int);
-	if (!p) {
-		return NULL;
-		printf ("wasm: Unable to lookup icall: %s\n", mono_method_get_name (method));
-		exit (1);
-	}
-
-	uint32_t idx = (int*)p - indexes;
-	*out_flags = flags [idx];
-
-	//printf ("ICALL: %s %x %d %d\n", methodname, token, idx, (int)(funcs [idx]));
-
-	return funcs [idx];
-}
-
-static const char*
-icall_table_lookup_symbol (void *func)
-{
-	assert (0);
-	return NULL;
-}
-
-#endif
-
-/*
- * get_native_to_interp:
- *
- *   Return a pointer to a wasm function which can be used to enter the interpreter to
- * execute METHOD from native code.
- * EXTRA_ARG is the argument passed to the interp entry functions in the runtime.
- */
-void*
-get_native_to_interp (MonoMethod *method, void *extra_arg)
-{
-	void *addr;
-
-	MONO_ENTER_GC_UNSAFE;
-	MonoClass *klass = mono_method_get_class (method);
-	MonoImage *image = mono_class_get_image (klass);
-	MonoAssembly *assembly = mono_image_get_assembly (image);
-	MonoAssemblyName *aname = mono_assembly_get_name (assembly);
-	const char *name = mono_assembly_name_get_name (aname);
-	const char *class_name = mono_class_get_name (klass);
-	const char *method_name = mono_method_get_name (method);
-	char key [128];
-	int len;
-
-	assert (strlen (name) < 100);
-	snprintf (key, sizeof(key), "%s_%s_%s", name, class_name, method_name);
-	len = strlen (key);
-	for (int i = 0; i < len; ++i) {
-		if (key [i] == '.')
-			key [i] = '_';
-	}
-
-	addr = wasm_dl_get_native_to_interp (key, extra_arg);
-	MONO_EXIT_GC_UNSAFE;
-	return addr;
-}
 
 #ifndef INVARIANT_GLOBALIZATION
 void load_icu_data (void)
@@ -483,7 +359,7 @@ mono_wasm_load_runtime (const char *unused, int debug_level)
 	mono_register_assemblies_bundle ();
 #endif
 	mono_dl_fallback_register (wasm_dl_load, wasm_dl_symbol, NULL, NULL);
-	mono_wasm_install_get_native_to_interp_tramp (get_native_to_interp);
+	mono_wasm_install_get_native_to_interp_tramp (mono_wasm_get_native_to_interp);
 
 #ifdef GEN_PINVOKE
 	mono_wasm_install_interp_to_native_callback (mono_wasm_interp_to_native_callback);
@@ -517,20 +393,8 @@ mono_wasm_load_runtime (const char *unused, int debug_level)
 
 #endif
 
-#ifdef LINK_ICALLS
-	/* Link in our own linked icall table */
-	static const MonoIcallTableCallbacks mono_icall_table_callbacks =
-	{
-		MONO_ICALL_TABLE_CALLBACKS_VERSION,
-		icall_table_lookup,
-		icall_table_lookup_symbol
-	};
-	mono_install_icall_table_callbacks (&mono_icall_table_callbacks);
-#endif
+	mono_wasm_init_icall_table ();
 
-#ifdef NEED_NORMAL_ICALL_TABLES
-	mono_icall_table_init ();
-#endif
 	mono_ee_interp_init (interp_opts);
 	mono_marshal_ilgen_init ();
 	mono_method_builder_ilgen_init ();
