715c990e43138328f8af21144c68d6960de1ec82
Ankit Jain <radical@gmail.com>
2023-12-13 07:42:54 +0000

    [wasm] Disable DebuggerTests.CallFunctionOnTests.CheckVSCodeTestFunction1
    Issue: https://github.com/dotnet/runtime/issues/95950

--- src/mono/wasm/debugger/DebuggerTestSuite/CallFunctionOnTests.cs
+++ src/mono/wasm/debugger/DebuggerTestSuite/CallFunctionOnTests.cs
@@ -20,8 +20,8 @@ namespace DebuggerTests
         // This tests `callFunctionOn` with a function that the vscode-js-debug extension uses
         // Using this here as a non-trivial test case
         [ConditionalTheory(nameof(RunningOnChrome))]
-        [InlineData("big_array_js_test (10);", "/other.js", 10, 1, 10, false)]
-        [InlineData("big_array_js_test (0);", "/other.js", 10, 1, 0, true)]
+        // [InlineData("big_array_js_test (10);", "/other.js", 10, 1, 10, false)] - ActiveIssue https://github.com/dotnet/runtime/issues/95950
+        // [InlineData("big_array_js_test (0);", "/other.js", 10, 1, 0, true)] - ActiveIssue https://github.com/dotnet/runtime/issues/95950
         [InlineData("invoke_static_method ('[debugger-test] DebuggerTests.CallFunctionOnTest:LocalsTest', 10);", "dotnet://debugger-test.dll/debugger-cfo-test.cs", 23, 12, 10, false)]
         [InlineData("invoke_static_method ('[debugger-test] DebuggerTests.CallFunctionOnTest:LocalsTest', 0);", "dotnet://debugger-test.dll/debugger-cfo-test.cs", 23, 12, 0, true)]
         public async Task CheckVSCodeTestFunction1(string eval_fn, string bp_loc, int line, int col, int len, bool roundtrip)
