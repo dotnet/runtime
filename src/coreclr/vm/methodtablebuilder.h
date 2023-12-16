2a462ffd01c974d7c96e2f3e751ebc4f730fc078
David Wrighton <davidwr@microsoft.com>
2023-12-15 18:08:29 -0800

    PlaceInterfaceMethod is now more optimized - It naturally has an O(N*M) algorithm, we reduce it to O(N*(Msmaller)), and make the constant factor faster - We take a local copy of the declared methods and name hashes, and the scan is now a straight linear scan of 32 bit memory DWORDS, and naturally skips nonpublic and non virtual methods.

--- src/coreclr/vm/methodtablebuilder.h
+++ src/coreclr/vm/methodtablebuilder.h
@@ -808,6 +808,11 @@ private:
         operator PCCOR_SIGNATURE() const
             { return GetSignature(); }
 
+        //-----------------------------------------------------------------------------------------
+        // Get a hash of the Name that can be compared with hashes from other MethodSignatures
+        UINT32
+        GetNameHash() const;
+
     protected:
         //-----------------------------------------------------------------------------------------
         Module *                m_pModule;
@@ -834,10 +839,6 @@ private:
         //-----------------------------------------------------------------------------------------
         void
         GetMethodAttributes() const;
-
-        //-----------------------------------------------------------------------------------------
-        UINT32
-        GetNameHash() const;
 
     private:
         //-----------------------------------------------------------------------------------
