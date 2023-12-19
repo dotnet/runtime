df1bfaadd86ea59ae24b9d2c5a3c3bcd7234d795
Milos Kotlar <kotlarmilos@gmail.com>
2023-12-19 17:46:24 +0100

    [tests] Trim library tests on devices

--- src/libraries/System.Runtime.Numerics/tests/BigInteger/DebuggerDisplayTests.cs
+++ src/libraries/System.Runtime.Numerics/tests/BigInteger/DebuggerDisplayTests.cs
@@ -10,8 +10,7 @@ namespace System.Numerics.Tests
 {
     public class DebuggerDisplayTests
     {
-        [ConditionalTheory(typeof(PlatformDetection), nameof(PlatformDetection.IsDebuggerTypeProxyAttributeSupported))]
-        [ConditionalTheory(typeof(PlatformDetection), nameof(PlatformDetection.IsBuiltWithAggressiveTrimming))]
+        [ConditionalTheory(typeof(PlatformDetection), nameof(PlatformDetection.IsDebuggerTypeProxyAttributeSupported), nameof(PlatformDetection.IsNotBuiltWithAggressiveTrimming))]
         [InlineData(new uint[] { 0, 0, 1 }, "18446744073709551616")]
         [InlineData(new uint[] { 0, 0, 0, 0, 1 }, "3.40282367e+38")]
         [InlineData(new uint[] { 0, 0x12345678, 0, 0xCC00CC00, 0x80808080 }, "7.33616508e+47")]
