f6a4490ffb076b86a51164c80e0cea56d60d7566
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2023-12-20 11:48:49 -0800

    Apply suggestions from code review
    removing tests

--- src/libraries/System.Text.Json/tests/System.Text.Json.SourceGeneration.Unit.Tests/JsonSourceGeneratorTests.cs
+++ src/libraries/System.Text.Json/tests/System.Text.Json.SourceGeneration.Unit.Tests/JsonSourceGeneratorTests.cs
@@ -768,37 +768,5 @@ namespace System.Text.Json.SourceGeneration.UnitTests
             Compilation compilation = CompilationHelper.CreateCompilation(source);
             CompilationHelper.RunJsonSourceGenerator(compilation);
         }
-
-        [Fact]
-        public static void NoCrashWithNonNameUsingAlias()
-        {
-            string source = $$"""
-                using System.Collections.Generic;
-                using System.Text.Json;
-                using System.Text.Json.Serialization;
-                using Point = (int x, int y);
-                #nullable enable
-
-                namespace HelloWorld
-                {
-                    public static class MyClass
-                    {
-                        public static string Test()
-                        {
-                            Dictionary<int, string?> values = new();
-                            return JsonSerializer.Serialize(values, JsonContext.Default.DictionaryInt32String);
-                        }
-                    }
-
-                    [JsonSerializable(typeof(Dictionary<int, string>))]
-                    internal partial class JsonContext : JsonSerializerContext
-                    {
-                    }
-                }
-                """;
-
-            Compilation compilation = CompilationHelper.CreateCompilation(source, parseOptions: CompilationHelper.CreateParseOptions(LanguageVersion.CSharp12));
-            CompilationHelper.RunJsonSourceGenerator(compilation);
-        }
     }
 }
