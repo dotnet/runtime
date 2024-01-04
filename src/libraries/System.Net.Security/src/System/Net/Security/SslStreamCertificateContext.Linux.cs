aa4f9ed43006670d408893b672dab213f4e2f220
Radek Zikmund <r.zikmund.rz@gmail.com>
2024-01-04 12:37:45 +0100

    Add 5s back-off after failed OCSP querry

--- src/libraries/System.Net.Security/src/System/Net/Security/SslStreamCertificateContext.Linux.cs
+++ src/libraries/System.Net.Security/src/System/Net/Security/SslStreamCertificateContext.Linux.cs
@@ -257,6 +257,14 @@ namespace System.Net.Security
                 GC.KeepAlive(caCert);
 
                 _pendingDownload = null;
+                if (ret == null)
+                {
+                    // all download attempts failed, don't try again for 5 seconds.
+                    // Note that if server does not send OCSP staples, clients may still
+                    // contact OCSP responders directly.
+                    _nextDownload = DateTimeOffset.UtcNow.AddSeconds(5);
+                    _ocspExpiration = _nextDownload;
+                }
                 return ret;
             }
         }
