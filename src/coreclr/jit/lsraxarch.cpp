bcffc5d4e9a3b6f40d26497ec3136f0baf13409c
EgorBo <egorbo@gmail.com>
2024-01-05 00:15:59 +0100

    forgot to call buildInternalRegisterUses

--- src/coreclr/jit/lsraxarch.cpp
+++ src/coreclr/jit/lsraxarch.cpp
@@ -439,10 +439,11 @@ int LinearScan::BuildNode(GenTree* tree)
             if (!tree->IsUnusedValue())
             {
                 // if value is unused, we'll emit a cmpxchg-loop idiom (requires RAX)
+                buildInternalIntRegisterDefForNode(tree, availableIntRegs & ~RBM_RAX);
                 BuildUse(tree->gtGetOp1(), availableIntRegs & ~RBM_RAX);
                 BuildUse(tree->gtGetOp2(), availableIntRegs & ~RBM_RAX);
-                buildInternalIntRegisterDefForNode(tree, availableIntRegs & ~RBM_RAX);
                 BuildDef(tree, RBM_RAX);
+                buildInternalRegisterUses();
                 srcCount = 2;
                 assert(dstCount == 1);
                 break;
