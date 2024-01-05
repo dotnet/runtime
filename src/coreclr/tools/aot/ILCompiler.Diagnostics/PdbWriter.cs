e2d322a2ff17b5b816ba1ea0ee3e9e1ded9e281b
Tomáš Rylek <trylek@microsoft.com>
2024-01-05 02:45:08 +0100

    Fix Crossgen2 PDB generation (again)

--- src/coreclr/tools/aot/ILCompiler.Diagnostics/PdbWriter.cs
+++ src/coreclr/tools/aot/ILCompiler.Diagnostics/PdbWriter.cs
@@ -90,6 +90,7 @@ namespace ILCompiler.Diagnostics
         Dictionary<SymDocument,int> _documentToChecksumOffsetMapping;
 
         UIntPtr _pdbMod;
+        IntPtr _pdbWriterInst;
         ISymNGenWriter2 _ngenWriter;
 
         static PdbWriter()
@@ -143,9 +144,15 @@ namespace ILCompiler.Diagnostics
                 }
                 finally
                 {
-                    if ((_ngenWriter != null) && (_pdbMod != UIntPtr.Zero))
+                    if (_ngenWriter != null)
                     {
-                        _ngenWriter.CloseMod(_pdbMod);
+                        if (_pdbMod != UIntPtr.Zero)
+                        {
+                            _ngenWriter.CloseMod(_pdbMod);
+                        }
+                        ComObject ngenWriterComObject = (ComObject)(object)_ngenWriter;
+                        ngenWriterComObject.FinalRelease();
+                        Marshal.Release(_pdbWriterInst);
                     }
                 }
 
@@ -209,8 +216,8 @@ namespace ILCompiler.Diagnostics
             File.Delete(_pdbFilePath);
 
             var comWrapper = new StrategyBasedComWrappers();
-            CreateNGenPdbWriter(dllPath, _pdbFilePath, out var pdbWriterInst);
-            _ngenWriter = (ISymNGenWriter2)comWrapper.GetOrCreateObjectForComInstance(pdbWriterInst, CreateObjectFlags.UniqueInstance);
+            CreateNGenPdbWriter(dllPath, _pdbFilePath, out _pdbWriterInst);
+            _ngenWriter = (ISymNGenWriter2)comWrapper.GetOrCreateObjectForComInstance(_pdbWriterInst, CreateObjectFlags.UniqueInstance);
 
             {
                 // PDB file is now created. Get its path and update _pdbFilePath so the PDB file
