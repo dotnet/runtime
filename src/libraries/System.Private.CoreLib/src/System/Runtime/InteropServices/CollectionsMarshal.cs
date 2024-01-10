2bfc643ca943ab08acd1f02064e3cc38a424afe0
Stephen Toub <stoub@microsoft.com>
2024-01-10 13:29:52 -0500

    Update src/libraries/System.Private.CoreLib/src/System/Runtime/InteropServices/CollectionsMarshal.cs
    Co-authored-by: Jan Kotas <jkotas@microsoft.com>

--- src/libraries/System.Private.CoreLib/src/System/Runtime/InteropServices/CollectionsMarshal.cs
+++ src/libraries/System.Private.CoreLib/src/System/Runtime/InteropServices/CollectionsMarshal.cs
@@ -35,7 +35,7 @@ namespace System.Runtime.InteropServices
                 }
 
                 Debug.Assert(typeof(T[]) == list._items.GetType(), "Implementation depends on List<T> always using a T[] and not U[] where U : T.");
-                span = new(ref MemoryMarshal.GetArrayDataReference(items), size);
+                span = new Span<T>(ref MemoryMarshal.GetArrayDataReference(items), size);
             }
 
             return span;
