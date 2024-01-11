a49d8b030b268fc017c8d0b68561cf2b31a5cf3f
mdh1418 <mitchhwang1418@gmail.com>
2024-01-11 11:33:39 -0500

    Guard AdvSimdEncode logic for not Mono

--- src/libraries/System.Private.CoreLib/src/System/Buffers/Text/Base64Encoder.cs
+++ src/libraries/System.Private.CoreLib/src/System/Buffers/Text/Base64Encoder.cs
@@ -493,9 +493,7 @@ namespace System.Buffers.Text
         [CompExactlyDependsOn(typeof(AdvSimd.Arm64))]
         private static unsafe void AdvSimdEncode(ref byte* srcBytes, ref byte* destBytes, byte* srcEnd, int sourceLength, int destLength, byte* srcStart, byte* destStart)
         {
-#if MONO // https://github.com/dotnet/runtime/issues/96828
-            return;
-#endif
+#if !MONO // https://github.com/dotnet/runtime/issues/96828
             // C# implementatino of https://github.com/aklomp/base64/blob/3a5add8652076612a8407627a42c768736a4263f/lib/arch/neon64/enc_loop.c
             Vector128<byte> str1;
             Vector128<byte> str2;
@@ -548,6 +546,7 @@ namespace System.Buffers.Text
 
             srcBytes = src;
             destBytes = dest;
+#endif // !MONO
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
