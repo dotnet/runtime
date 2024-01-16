5ba146039cd6e9a9d35453a1a804b3a777bbb35a
Marek Fišera <mara@neptuo.com>
2024-01-16 11:20:34 +0100

    Fix order of commands (change project dir after app is copied)

--- src/mono/wasm/Wasm.Build.Tests/TestAppScenarios/AppTestBase.cs
+++ src/mono/wasm/Wasm.Build.Tests/TestAppScenarios/AppTestBase.cs
@@ -28,12 +28,12 @@ public abstract class AppTestBase : BlazorWasmTestBase
         Id = $"{generatedProjectNamePrefix ?? assetName}_{GetRandomId()}";
         InitBlazorWasmProjectDir(Id);
 
+        LogPath = Path.Combine(s_buildEnv.LogRootPath, Id);
+        Utils.DirectoryCopy(Path.Combine(BuildEnvironment.TestAssetsPath, assetName), Path.Combine(_projectDir!));
+
         // WasmBasicTestApp consists of App + Library projects
         if (assetName == "WasmBasicTestApp")
             _projectDir = Path.Combine(_projectDir!, "App");
-
-        LogPath = Path.Combine(s_buildEnv.LogRootPath, Id);
-        Utils.DirectoryCopy(Path.Combine(BuildEnvironment.TestAssetsPath, assetName), Path.Combine(_projectDir!));
     }
 
     protected void BuildProject(string configuration)
