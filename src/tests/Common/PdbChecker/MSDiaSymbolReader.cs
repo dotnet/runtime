c8448af353553f61f7e0851593597f6a494a133e
Tomáš Rylek <trylek@microsoft.com>
2024-01-17 12:33:29 +0100

    Fix PdbChecker to not require msdia140 registration

--- src/tests/Common/PdbChecker/MSDiaSymbolReader.cs
+++ src/tests/Common/PdbChecker/MSDiaSymbolReader.cs
@@ -4,10 +4,28 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using Dia2Lib;
 
 class MSDiaSymbolReader
 {
+    [return: MarshalAs(UnmanagedType.Interface)]
+    [DllImport("msdia140.dll", CharSet = CharSet.Unicode, ExactSpelling = true, PreserveSig = false)]
+    private static extern object DllGetClassObject(
+        [In] in Guid rclsid,
+        [In] in Guid riid);
+
+    [ComImport, ComVisible(false), Guid("00000001-0000-0000-C000-000000000046"),
+        InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    private interface IClassFactory
+    {
+        void CreateInstance([MarshalAs(UnmanagedType.Interface)] object? aggregator,
+                            [In] in Guid refiid,
+                            [MarshalAs(UnmanagedType.Interface)] out object createdObject);
+        void LockServer(bool incrementRefCount);
+    }
+
     private readonly IDiaDataSource _diaDataSource;
     private readonly IDiaSession _diaSession;
 
@@ -17,7 +35,11 @@ class MSDiaSymbolReader
     {
         try
         {
-            _diaDataSource = new DiaSourceClass();
+            var dia140SourceClassGuid = new Guid("{e6756135-1e65-4d17-8576-610761398c3c}");
+            IClassFactory diaClassFactory = (IClassFactory)DllGetClassObject(dia140SourceClassGuid, typeof(IClassFactory).GetTypeInfo().GUID);
+            diaClassFactory.CreateInstance(null, typeof(IDiaDataSource).GetTypeInfo().GUID, out object comObject);
+            
+            _diaDataSource = (IDiaDataSource)comObject;
             _diaDataSource.loadDataFromPdb(pdbFile);
             _diaDataSource.openSession(out _diaSession);
 
