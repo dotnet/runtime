ed1a1604572f796038a138594e4180e6a8d69ec9
Jeremy Koritzinsky <jekoritz@microsoft.com>
2024-01-17 10:59:43 -0800

    Add using

--- src/libraries/System.Runtime.InteropServices/src/System/Runtime/InteropServices/Marshalling/ComObject.cs
+++ src/libraries/System.Runtime.InteropServices/src/System/Runtime/InteropServices/Marshalling/ComObject.cs
@@ -6,6 +6,7 @@
 
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Threading;
 using System.Runtime.Versioning;
 
 namespace System.Runtime.InteropServices.Marshalling
