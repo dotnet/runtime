4fa945723f43297b5421725106f4d44ed83846e6
kzrnm <gengesa@gmail.com>
2024-01-18 09:57:10 +0900

    Set threshold to 1233

--- src/libraries/System.Runtime.Numerics/src/System/Number.BigInteger.cs
+++ src/libraries/System.Runtime.Numerics/src/System/Number.BigInteger.cs
@@ -668,7 +668,7 @@ namespace System
 #else
         internal const
 #endif
-        int s_naiveThreshold = 3200;
+        int s_naiveThreshold = 1233;
         private static ParsingStatus NumberToBigInteger(ref NumberBuffer number, out BigInteger result)
         {
             int currentBufferSize = 0;
