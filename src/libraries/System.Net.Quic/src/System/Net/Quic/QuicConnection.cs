3a4e39336592329eca3b79d6da7ee0700ed93ebf
ManickaP <mapichov@microsoft.com>
2024-01-19 12:17:39 +0100

    Revert "Lower probability of creating extra TCS instance."
    This reverts commit 636037cf9e85bc1f275d6cb7346866d4b86c5938.

--- src/libraries/System.Net.Quic/src/System/Net/Quic/QuicConnection.cs
+++ src/libraries/System.Net.Quic/src/System/Net/Quic/QuicConnection.cs
@@ -109,21 +109,7 @@ public sealed partial class QuicConnection : IAsyncDisposable
     private int _disposed;
 
     private readonly ValueTaskSource _connectedTcs = new ValueTaskSource();
-
     private TaskCompletionSource? _shutdownTcs;
-
-    private bool InitializeShutdownTcs(out TaskCompletionSource shutdownTcs)
-    {
-        if (_shutdownTcs is not null)
-        {
-            shutdownTcs = _shutdownTcs;
-            return false;
-        }
-
-        TaskCompletionSource? original = Interlocked.CompareExchange(ref _shutdownTcs, new TaskCompletionSource(TaskCreationOptions.RunContinuationsAsynchronously), null);
-        shutdownTcs = _shutdownTcs;
-        return original is null;
-    }
 
     private readonly CancellationTokenSource _shutdownTokenSource = new CancellationTokenSource();
 
@@ -481,7 +467,7 @@ public sealed partial class QuicConnection : IAsyncDisposable
     {
         ObjectDisposedException.ThrowIf(_disposed == 1, this);
 
-        if (InitializeShutdownTcs(out TaskCompletionSource shutdownTcs))
+        if (Interlocked.CompareExchange(ref _shutdownTcs, new TaskCompletionSource(TaskCreationOptions.RunContinuationsAsynchronously), null) == null)
         {
             unsafe
             {
@@ -492,7 +478,7 @@ public sealed partial class QuicConnection : IAsyncDisposable
             }
         }
 
-        return new ValueTask(shutdownTcs.Task.WaitAsync(cancellationToken));
+        return new ValueTask(_shutdownTcs.Task.WaitAsync(cancellationToken));
     }
 
     private unsafe int HandleEventConnected(ref CONNECTED_DATA data)
@@ -534,8 +520,8 @@ public sealed partial class QuicConnection : IAsyncDisposable
         _acceptQueue.Writer.TryComplete(exception);
         _connectedTcs.TrySetException(exception);
         _shutdownTokenSource.Cancel();
-        InitializeShutdownTcs(out TaskCompletionSource shutdownTcs);
-        shutdownTcs.TrySetResult();
+        Interlocked.CompareExchange(ref _shutdownTcs, new TaskCompletionSource(TaskCreationOptions.RunContinuationsAsynchronously), null);
+        _shutdownTcs.TrySetResult();
         return QUIC_STATUS_SUCCESS;
     }
     private unsafe int HandleEventLocalAddressChanged(ref LOCAL_ADDRESS_CHANGED_DATA data)
@@ -641,7 +627,7 @@ public sealed partial class QuicConnection : IAsyncDisposable
         }
 
         // Check if the connection has been shut down and if not, shut it down.
-        if (InitializeShutdownTcs(out TaskCompletionSource shutdownTcs))
+        if (Interlocked.CompareExchange(ref _shutdownTcs, new TaskCompletionSource(TaskCreationOptions.RunContinuationsAsynchronously), null) == null)
         {
             unsafe
             {
@@ -651,7 +637,7 @@ public sealed partial class QuicConnection : IAsyncDisposable
                     (ulong)_defaultCloseErrorCode);
             }
         }
-        else if (!shutdownTcs.Task.IsCompletedSuccessfully)
+        else if (!_shutdownTcs.Task.IsCompletedSuccessfully)
         {
             unsafe
             {
@@ -663,7 +649,7 @@ public sealed partial class QuicConnection : IAsyncDisposable
         }
 
         // Wait for SHUTDOWN_COMPLETE, the last event, so that all resources can be safely released.
-        await shutdownTcs.Task.ConfigureAwait(false);
+        await _shutdownTcs.Task.ConfigureAwait(false);
         Debug.Assert(_connectedTcs.IsCompleted);
         _handle.Dispose();
         _shutdownTokenSource.Dispose();
