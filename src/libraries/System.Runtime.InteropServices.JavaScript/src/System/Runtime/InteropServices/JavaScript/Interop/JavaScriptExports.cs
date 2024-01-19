dd82372263932f4d860ba811ae3f41d773ca4914
Radek Doulik <radek.doulik@gmail.com>
2024-01-19 16:51:02 +0100

    Changes from unsaved file

--- src/libraries/System.Runtime.InteropServices.JavaScript/src/System/Runtime/InteropServices/JavaScript/Interop/JavaScriptExports.cs
+++ src/libraries/System.Runtime.InteropServices.JavaScript/src/System/Runtime/InteropServices/JavaScript/Interop/JavaScriptExports.cs
@@ -220,17 +220,17 @@ namespace System.Runtime.InteropServices.JavaScript
                 {
                     if (holder.Callback == null)
                     {
-                        var threadFlag = Monitor.ThrowOnBlockingWaitOnJSInteropThread;
-                        Monitor.ThrowOnBlockingWaitOnJSInteropThread = false;
                         holder.CallbackReady = new ManualResetEventSlim(false);
-                        Monitor.ThrowOnBlockingWaitOnJSInteropThread = threadFlag;
                     }
                 }
                 if (holder.CallbackReady != null)
                 {
+                    var threadFlag = Monitor.ThrowOnBlockingWaitOnJSInteropThread;
+                    Monitor.ThrowOnBlockingWaitOnJSInteropThread = false;
 #pragma warning disable CA1416 // Validate platform compatibility
                     holder.CallbackReady?.Wait();
 #pragma warning restore CA1416 // Validate platform compatibility
+                    Monitor.ThrowOnBlockingWaitOnJSInteropThread = threadFlag;
                 }
 #endif
                 var callback = holder.Callback!;
