5ad4073b56a5ae25e93ba2ed800e4a41abf690be
Aaron Robinson <arobins@microsoft.com>
2024-01-20 17:09:39 -0800

    Update TestsManifestGeneration.Etw.cs
    Disable tests on x86

--- src/libraries/System.Diagnostics.Tracing/tests/BasicEventSourceTest/TestsManifestGeneration.Etw.cs
+++ src/libraries/System.Diagnostics.Tracing/tests/BasicEventSourceTest/TestsManifestGeneration.Etw.cs
@@ -31,6 +31,7 @@ namespace BasicEventSourceTests
         /// ETW only works with elevated process
         [ConditionalFact(nameof(IsProcessElevatedAndNotWindowsNanoServerAndRemoteExecutorSupported))]
         [SkipOnCoreClr("Test should only be run in non-stress modes", ~RuntimeTestModes.RegularRun)]
+        [ActiveIssue("https://github.com/dotnet/runtime/issues/97255", typeof(TestLibrary.PlatformDetection), nameof(TestLibrary.PlatformDetection.Is32BitProcess))]
         public void Test_EventSource_EtwManifestGeneration()
         {
             var pid = Process.GetCurrentProcess().Id;
@@ -71,6 +72,7 @@ namespace BasicEventSourceTests
 
         [ConditionalFact(nameof(IsProcessElevatedAndNotWindowsNanoServerAndRemoteExecutorSupported))]
         [SkipOnCoreClr("Test should only be run in non-stress modes", ~RuntimeTestModes.RegularRun)]
+        [ActiveIssue("https://github.com/dotnet/runtime/issues/97255", typeof(TestLibrary.PlatformDetection), nameof(TestLibrary.PlatformDetection.Is32BitProcess))]
         public void Test_EventSource_EtwManifestGenerationRollover()
         {
             var pid = Process.GetCurrentProcess().Id;
