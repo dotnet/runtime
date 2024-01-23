3838c8de716426d9b611568a22d65018b2504ab8
EgorBo <egorbo@gmail.com>
2024-01-23 20:55:18 +0100

    Add more tests

--- src/coreclr/jit/importer.cpp
+++ src/coreclr/jit/importer.cpp
@@ -5460,9 +5460,6 @@ GenTree* Compiler::impCastClassOrIsInstToTree(
         shouldExpandInline = isCastClass && isClassExact;
     }
 
-    // CI test, expand all casts in the late phase
-    shouldExpandInline = false;
-
     // Pessimistically assume the jit cannot expand this as an inline test
     bool                  canExpandInline = false;
     bool                  partialExpand   = false;
