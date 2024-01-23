2f63c1aaecd10f50977dfc1f8c9903c61f788805
Ilona Tomkowicz <itomkowicz@microsoft.com>
2024-01-23 18:17:33 +0100

    Fix net462 by reverting conversion to record.

--- src/libraries/Microsoft.Extensions.Logging.Console/tests/Microsoft.Extensions.Logging.Console.Tests/ConsoleTestsBase.cs
+++ src/libraries/Microsoft.Extensions.Logging.Console/tests/Microsoft.Extensions.Logging.Console.Tests/ConsoleTestsBase.cs
@@ -8,14 +8,32 @@ namespace Microsoft.Extensions.Logging.Console.Test
 {
     public class ConsoleTestsBase
     {
-        internal sealed record SetupDisposeHelper(
-            ConsoleLogger Logger,
-            ConsoleSink Sink,
-            ConsoleSink ErrorSink,
-            Func<LogLevel, string> GetLevelPrefix,
-            int WritesPerMsg,
-            TestLoggerProcessor LoggerProcessor) : IDisposable
+        // converting to record fails on net462
+        internal sealed class SetupDisposeHelper : IDisposable
         {
+            public ConsoleLogger Logger;
+            public ConsoleSink Sink;
+            public ConsoleSink ErrorSink;
+            public Func<LogLevel, string> GetLevelPrefix;
+            public int WritesPerMsg;
+            public TestLoggerProcessor LoggerProcessor;
+
+            public SetupDisposeHelper(
+                ConsoleLogger logger,
+                ConsoleSink sink,
+                ConsoleSink errorSink,
+                Func<LogLevel, string> getLevelPrefix,
+                int writesPerMsg,
+                TestLoggerProcessor loggerProcessor)
+            {
+                Logger = logger;
+                Sink = sink;
+                ErrorSink = errorSink;
+                GetLevelPrefix = getLevelPrefix;
+                WritesPerMsg = writesPerMsg;
+                LoggerProcessor = loggerProcessor;
+            }
+
             public void Dispose() => LoggerProcessor.Dispose();
         }
     }
