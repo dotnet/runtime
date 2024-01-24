af93cc635565db12390573fd06d702c6c6dc340c
Aman Khalid (from Dev Box) <amankhalid@microsoft.com>
2024-01-24 11:39:08 -0500

    Merge fgRemoveConditionalJump and fgOptimizeBranchToNext

--- src/coreclr/jit/compiler.h
+++ src/coreclr/jit/compiler.h
@@ -5966,8 +5966,6 @@ public:
 
     bool fgOptimizeSwitchBranches(BasicBlock* block);
 
-    bool fgOptimizeBranchToNext(BasicBlock* block, BasicBlock* bNext, BasicBlock* bPrev);
-
     bool fgOptimizeSwitchJumps();
 #ifdef DEBUG
     void fgPrintEdgeWeights();
