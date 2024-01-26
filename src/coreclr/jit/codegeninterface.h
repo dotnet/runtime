da25bc86da88d4f0b27029650b36c301cfc69fdb
Andy Ayers <andya@microsoft.com>
2024-01-25 18:08:07 -0800

    JIT: capture all write barrier helper addresses for SPMI
    When varying CSEs we can sometimes alter the write barrier that is
    needed. In particular if we CSE a heap address computation we may
    lose track of the fact that an indir is writing to the heap, and so
    change which write barrier needs to be used.
    
    See #97534.
    
    Even if that's fixed it seems like we still might change our minds
    for various reasons, so when running under SPMI, just collect all
    the possible write barrier helper addresses.

--- src/coreclr/jit/codegeninterface.h
+++ src/coreclr/jit/codegeninterface.h
@@ -173,6 +173,11 @@ public:
     bool genUseOptimizedWriteBarriers(GenTreeStoreInd* store);
     CorInfoHelpFunc genWriteBarrierHelperForWriteBarrierForm(GCInfo::WriteBarrierForm wbf);
 
+#ifdef DEBUG
+    bool genWriteBarrierUsed;
+    void WriteBarrierExtraSuperPmiQueries();
+#endif
+
     // The following property indicates whether the current method sets up
     // an explicit stack frame or not.
 private:
