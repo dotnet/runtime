dotnet/runtime

commit 7373fd0a15880f51ac09be6e0a3b02b14882b3c9

JIT: capture all write barrier helper addresses for SPMI (#97535)
When varying CSEs we can sometimes alter the write barrier that is
needed. In particular if we CSE a heap address computation we may
lose track of the fact that an indir is writing to the heap, and so
change which write barrier needs to be used.

See #97534.

Even if that's fixed it seems like we still might change our minds
for various reasons, so when running under SPMI, just collect all
the possible write barrier helper addresses.

src/coreclr/jit/codegencommon.cpp
@@ -1688,6 +1688,8 @@ void CodeGen::genGenerateCode(void** codePtr, uint32_t* nativeSizeOfCode)
         printf("*************** In genGenerateCode()\n");
         compiler->fgDispBasicBlocks(compiler->verboseTrees);
     }
+
+    genWriteBarrierUsed = false;
 #endif
 
     this->codePtr          = codePtr;
@@ -1696,6 +1698,17 @@ void CodeGen::genGenerateCode(void** codePtr, uint32_t* nativeSizeOfCode)
     DoPhase(this, PHASE_GENERATE_CODE, &CodeGen::genGenerateMachineCode);
     DoPhase(this, PHASE_EMIT_CODE, &CodeGen::genEmitMachineCode);
     DoPhase(this, PHASE_EMIT_GCEH, &CodeGen::genEmitUnwindDebugGCandEH);
+
+#ifdef DEBUG
+    if (genWriteBarrierUsed && JitConfig.EnableExtraSuperPmiQueries())
+    {
+        void* ignored;
+        for (int i = CORINFO_HELP_ASSIGN_REF; i <= CORINFO_HELP_ASSIGN_STRUCT; i++)
+        {
+            compiler->compGetHelperFtn((CorInfoHelpFunc)i, &ignored);
+        }
+    }
+#endif
 }
 
 //----------------------------------------------------------------------
@@ -2693,6 +2706,8 @@ bool CodeGenInterface::genUseOptimizedWriteBarriers(GenTreeStoreInd* store)
 //
 CorInfoHelpFunc CodeGenInterface::genWriteBarrierHelperForWriteBarrierForm(GCInfo::WriteBarrierForm wbf)
 {
+    INDEBUG(genWriteBarrierUsed = true);
+
     switch (wbf)
     {
         case GCInfo::WBF_BarrierChecked:

src/coreclr/jit/codegeninterface.h
@@ -173,6 +173,10 @@ public:
     bool genUseOptimizedWriteBarriers(GenTreeStoreInd* store);
     CorInfoHelpFunc genWriteBarrierHelperForWriteBarrierForm(GCInfo::WriteBarrierForm wbf);
 
+#ifdef DEBUG
+    bool genWriteBarrierUsed;
+#endif
+
     // The following property indicates whether the current method sets up
     // an explicit stack frame or not.
 private:

src/coreclr/jit/gcinfo.cpp
@@ -240,7 +240,7 @@ GCInfo::WriteBarrierForm GCInfo::gcIsWriteBarrierCandidate(GenTreeStoreInd* stor
     }
 
     // Ignore any assignments of NULL.
-    GenTree* data = store->Data()->gtSkipReloadOrCopy();
+    GenTree* const data = store->Data()->gtSkipReloadOrCopy();
     if ((data->GetVN(VNK_Liberal) == ValueNumStore::VNForNull()) || data->IsIntegralConst(0))
     {
         return WBF_NoBarrier;
@@ -248,7 +248,7 @@ GCInfo::WriteBarrierForm GCInfo::gcIsWriteBarrierCandidate(GenTreeStoreInd* stor
 
     if ((store->gtFlags & GTF_IND_TGT_NOT_HEAP) != 0)
     {
-        // This indirection is not from to the heap.
+        // This indirection is not storing to the heap.
         // This case occurs for stack-allocated objects.
         return WBF_NoBarrier;
     }

commit 4db68dffcef4e897771ba0e3a094656e7d20a269

JIT: don't ask for all write barrer helpers for AOT
Fixes an issue introduced by #97535. AOT hosts don't support all the
possible write barriers and blow things up if the JIT asks for ones
they don't support.

src/coreclr/jit/codegencommon.cpp
@@ -1701,7 +1701,9 @@ void CodeGen::genGenerateCode(void** codePtr, uint32_t* nativeSizeOfCode)
     DoPhase(this, PHASE_EMIT_GCEH, &CodeGen::genEmitUnwindDebugGCandEH);
 
 #ifdef DEBUG
-    if (genWriteBarrierUsed && JitConfig.EnableExtraSuperPmiQueries())
+    // For R2R/NAOT not all these helpers are implmented. So don't ask for them.
+    //
+    if (genWriteBarrierUsed && JitConfig.EnableExtraSuperPmiQueries() && !compiler->opts.IsReadyToRun())
     {
         void* ignored;
         for (int i = CORINFO_HELP_ASSIGN_REF; i <= CORINFO_HELP_ASSIGN_STRUCT; i++)