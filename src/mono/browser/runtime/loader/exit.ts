2fc30ac4d4a03d7230c01efd79a65038c9678d32
Pavel Savara <pavelsavara@microsoft.com>
2024-01-30 11:57:58 +0100

    Update src/mono/browser/runtime/loader/exit.ts
    Co-authored-by: Marek Fišera <mara@neptuo.com>

--- src/mono/browser/runtime/loader/exit.ts
+++ src/mono/browser/runtime/loader/exit.ts
@@ -13,7 +13,7 @@ export function is_runtime_running() {
 }
 
 export function assert_runtime_running() {
-    mono_assert(runtimeHelpers.runtimeReady, "dotnet runtime didn't start yet. Please call dotnet.create() first.");
+    mono_assert(runtimeHelpers.runtimeReady, ".NET runtime didn't start yet. Please call dotnet.create() first.");
     mono_assert(!loaderHelpers.assertAfterExit || !is_exited(), () => `dotnet runtime already exited with ${loaderHelpers.exitCode} ${loaderHelpers.exitReason}. You can pass dotnet.run(false) or use runtime.runMain() instead.`);
 }
 
