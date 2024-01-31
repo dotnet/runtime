dd7e478c92c996d11e36b92068c4fefaf7567761
Andrew Au <andrewau@microsoft.com>
2024-01-30 20:00:12 -0800

    Test GCC

--- src/native/external/libunwind/src/dwarf/Gget_proc_info_in_range.c
+++ src/native/external/libunwind/src/dwarf/Gget_proc_info_in_range.c
@@ -24,17 +24,12 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  */
 #include "dwarf_i.h"
 
 int
-unw_get_proc_info_in_range (unw_word_t        start_ip,
-							unw_word_t        end_ip,
-                            unw_word_t        eh_frame_table,
-                            unw_word_t        eh_frame_table_len UNUSED,
-                            unw_word_t        exidx_frame_table UNUSED,
-                            unw_word_t        exidx_frame_table_len UNUSED,
-                            unw_addr_space_t  as,
-                            unw_word_t        ip,
-                            unw_proc_info_t  *pi,
-                            int               need_unwind_info,
-                            void             *arg)
+unw_get_proc_info_in_range (unw_word_t start_ip, unw_word_t end_ip,
+                            unw_word_t eh_frame_table, unw_word_t eh_frame_table_len,
+                            unw_word_t exidx_frame_table, unw_word_t exidx_frame_table_len,
+                            unw_addr_space_t as, unw_word_t ip,
+                            unw_proc_info_t *pi, int need_unwind_info,
+                            void *arg)
 {
     int ret = 0;
 
@@ -58,13 +53,14 @@ unw_get_proc_info_in_range (unw_word_t        start_ip,
     if (eh_frame_table != 0) {
         unw_accessors_t *a = unw_get_accessors_int (as);
 
-        struct dwarf_eh_frame_hdr exhdr;
-        if ((*a->access_mem)(as, eh_frame_table, (unw_word_t*)&exhdr, 0, arg) < 0) {
+        unw_word_t hdr[2];
+        if ((*a->access_mem)(as, eh_frame_table, hdr, 0, arg) < 0) {
             return -UNW_EINVAL;
         }
+        struct dwarf_eh_frame_hdr* exhdr = (struct dwarf_eh_frame_hdr*)hdr;
 
-        if (exhdr.version != DW_EH_VERSION) {
-            Debug (1, "Unexpected version %d\n", exhdr.version);
+        if (exhdr->version != DW_EH_VERSION) {
+            Debug (1, "Unexpected version %d\n", exhdr->version);
             return -UNW_EBADVERSION;
         }
         unw_word_t addr = eh_frame_table + offsetof(struct dwarf_eh_frame_hdr, eh_frame);
@@ -72,12 +68,12 @@ unw_get_proc_info_in_range (unw_word_t        start_ip,
         unw_word_t fde_count;
 
         /* read eh_frame_ptr */
-        if ((ret = dwarf_read_encoded_pointer(as, a, &addr, exhdr.eh_frame_ptr_enc, pi, &eh_frame_start, arg)) < 0) {
+        if ((ret = dwarf_read_encoded_pointer(as, a, &addr, exhdr->eh_frame_ptr_enc, pi, &eh_frame_start, arg)) < 0) {
             return ret;
         }
 
         /* read fde_count */
-        if ((ret = dwarf_read_encoded_pointer(as, a, &addr, exhdr.fde_count_enc, pi, &fde_count, arg)) < 0) {
+        if ((ret = dwarf_read_encoded_pointer(as, a, &addr, exhdr->fde_count_enc, pi, &fde_count, arg)) < 0) {
             return ret;
         }
 
@@ -87,8 +83,8 @@ unw_get_proc_info_in_range (unw_word_t        start_ip,
             return -UNW_ENOINFO;
         }
 
-        if (exhdr.table_enc != (DW_EH_PE_datarel | DW_EH_PE_sdata4)) {
-            Debug (1, "Table encoding not supported %x\n", exhdr.table_enc);
+        if (exhdr->table_enc != (DW_EH_PE_datarel | DW_EH_PE_sdata4)) {
+            Debug (1, "Table encoding not supported %x\n", exhdr->table_enc);
             return -UNW_EINVAL;
         }
 
