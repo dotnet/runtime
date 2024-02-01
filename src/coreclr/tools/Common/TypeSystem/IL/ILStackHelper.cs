f40fcacfb2acdcaa880ad4988fc12c0f0ae21429
PaulusParssinen <paulus.parssinen@gmail.com>
2024-02-01 16:50:03 +0200

    Use short to represent the stack height

--- src/coreclr/tools/Common/TypeSystem/IL/ILStackHelper.cs
+++ src/coreclr/tools/Common/TypeSystem/IL/ILStackHelper.cs
@@ -24,15 +24,15 @@ namespace Internal.IL
         /// </summary>
         public static int ComputeMaxStack(this MethodIL methodIL)
         {
-            const int StackHeightNotSet = int.MinValue;
-            const int StackAllocThreshold = 256 / sizeof(int);
+            const short StackHeightNotSet = short.MinValue;
+            const int StackallocThreshold = 256 / sizeof(short);
 
             var ilReader = new ILReader(methodIL.GetILBytes());
-            int stackHeight = 0;
-            int maxStack = 0;
+            short stackHeight = 0;
+            short maxStack = 0;
 
-            Span<int> stackHeights = ilReader.Size <= StackAllocThreshold ?
-                stackalloc int[StackAllocThreshold].Slice(0, ilReader.Size) : new int[ilReader.Size];
+            Span<short> stackHeights = ilReader.Size <= StackallocThreshold ?
+                stackalloc short[StackallocThreshold].Slice(0, ilReader.Size) : new short[ilReader.Size];
 
             stackHeights.Fill(StackHeightNotSet);
 
@@ -209,7 +209,7 @@ namespace Internal.IL
                             int target = ilReader.ReadBranchDestination(opcode);
                             hasReadOperand = true;
 
-                            int adjustment;
+                            short adjustment;
                             bool isConditional;
                             if (opcode is ILOpcode.br or ILOpcode.br_s or ILOpcode.leave or ILOpcode.leave_s)
                             {
@@ -253,7 +253,7 @@ namespace Internal.IL
 
                             MethodSignature sig = obj is MethodSignature methodSignature ?
                                 methodSignature : ((MethodDesc)obj).Signature;
-                            int adjustment = sig.Length;
+                            short adjustment = (short)sig.Length;
                             if (opcode == ILOpcode.newobj)
                             {
                                 adjustment--;
