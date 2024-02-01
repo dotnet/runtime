8a9f1f7ea120cfc825eb17bbc089dc03c15d5ad9
Patrick Sycz <patrick.sycz@unity3d.com>
2024-02-01 16:59:25 -0500

    Removed new builddriver-nuget and replaced with MS's NuGet config, updated scripts

--- .yamato/scripts/build_yamato.cmd
+++ .yamato/scripts/build_yamato.cmd
@@ -1,4 +1,4 @@
 @echo off
 
-call %~dp0../../dotnet.cmd restore %~dp0../../unity/CITools/BuildDriver/BuildDriver.csproj --configfile %~dp0../../unity/CITools/builddriver-nuget.config --
+call %~dp0../../dotnet.cmd restore %~dp0../../unity/CITools/BuildDriver/BuildDriver.csproj --configfile %~dp0../../NuGet.config
 call %~dp0../../dotnet.cmd run --project %~dp0../../unity/CITools/BuildDriver/BuildDriver.csproj --no-restore -- %*
--- .yamato/scripts/build_yamato.sh
+++ .yamato/scripts/build_yamato.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 BASEDIR=$(dirname $0)
-$BASEDIR/../../dotnet.sh restore $BASEDIR/../../unity/CITools/BuildDriver/BuildDriver.csproj --configfile $BASEDIR/../../unity/CITools/builddriver-nuget.config
+$BASEDIR/../../dotnet.sh restore $BASEDIR/../../unity/CITools/BuildDriver/BuildDriver.csproj --configfile $BASEDIR/../../NuGet.config
 $BASEDIR/../../dotnet.sh run --project $BASEDIR/../../unity/CITools/BuildDriver/BuildDriver.csproj --no-restore -- "$@"
