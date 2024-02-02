139846ca0214303d119c9cfe5ab70f0a7ac6f50c
Ilona Tomkowicz <32700855+ilonatommy@users.noreply.github.com>
2024-02-02 11:18:22 +0000

    Use only one env var for debugger.

--- src/mono/browser/debugger/DebuggerTestSuite/EnvironmentVariables.cs
+++ src/mono/browser/debugger/DebuggerTestSuite/EnvironmentVariables.cs
@@ -12,6 +12,7 @@ internal static class EnvironmentVariables
     public static readonly string? DebuggerTestPath = Environment.GetEnvironmentVariable("DEBUGGER_TEST_PATH");
     public static readonly string? TestLogPath      = Environment.GetEnvironmentVariable("TEST_LOG_PATH");
     public static readonly bool    SkipCleanup      = Environment.GetEnvironmentVariable("SKIP_CLEANUP") == "1" ||
-                                                       Environment.GetEnvironmentVariable("SKIP_CLEANUP") == "true";
-    public static readonly string? WasmTestsUsingVariant = Environment.GetEnvironmentVariable("WASM_TESTS_USING_VARIANT");
+                                                        Environment.GetEnvironmentVariable("SKIP_CLEANUP") == "true";
+    public static readonly bool WasmEnableThreads = Environment.GetEnvironmentVariable("WasmEnableThreads") == "1" ||
+                                                    Environment.GetEnvironmentVariable("WasmEnableThreads") == "true";
 }
