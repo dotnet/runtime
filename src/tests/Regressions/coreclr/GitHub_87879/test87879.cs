2dbbfc9a5e64ca0c7c59a1c7c503a7acaedfe7f3
Hamish Arblaster <hamarb123@gmail.com>
2024-02-06 12:05:57 +1100

    Fix test

--- src/tests/Regressions/coreclr/GitHub_87879/test87879.cs
+++ src/tests/Regressions/coreclr/GitHub_87879/test87879.cs
@@ -4,11 +4,14 @@
 using System;
 using System.Threading;
 using System.Runtime.CompilerServices;
+using Xunit;
 
-class Program
+namespace test87879;
+
+class test87879
 {
-    [SkipLocalsInit]
-    static int Main()
+    [Fact, SkipLocalsInit]
+    public static int TestEntryPoint()
     {
         //determine the expected available stack size (4MB or 1MB), minus a little bit (384kB) for overhead.
         var expectedSize = (IntPtr.Size == 8 ? 0x4_00000 : 0x1_00000) - 0x60000;
