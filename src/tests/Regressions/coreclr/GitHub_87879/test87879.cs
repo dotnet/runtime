a473b2abd2e5de17f775b62a4db0bc7f43fc284b
Hamish Arblaster <hamarb123@gmail.com>
2024-02-06 15:11:15 +1100

    Change tests and fix for NAOT
    - Fix test definition
    - Fix NAOT apps stack size
    - Add `<UseAppHost>false</UseAppHost>` version of non-NAOT tests

--- src/tests/Regressions/coreclr/GitHub_87879/test87879.cs
+++ src/tests/Regressions/coreclr/GitHub_87879/test87879.cs
@@ -8,10 +8,10 @@ using Xunit;
 
 namespace test87879;
 
-class test87879
+public class test87879
 {
     [Fact, SkipLocalsInit]
-    public static int TestEntryPoint()
+    public static void TestEntryPoint()
     {
         //determine the expected available stack size (4MB or 1MB), minus a little bit (384kB) for overhead.
         var expectedSize = (IntPtr.Size == 8 ? 0x4_00000 : 0x1_00000) - 0x60000;
@@ -30,9 +30,6 @@ class test87879
         t.Start();
         t.Join();
         Console.WriteLine("Secondary thread succeeded.");
-
-        //success
-        return 100;
     }
 
     [MethodImpl(MethodImplOptions.NoInlining)]
