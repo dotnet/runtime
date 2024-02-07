c4befe9e0b38c03662510260dfc9005054f36471
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2024-02-07 09:21:38 +0100

    Revert "Revert "[RISC-V] Added designated output instruction emitters (#96741)""
    This reverts commit ecc044d4e6082c0f727bd7e36d53878416cf4207.

--- src/coreclr/jit/emit.h
+++ src/coreclr/jit/emit.h
@@ -418,8 +418,9 @@ struct emitLclVarAddr
     // Constructor
     void initLclVarAddr(int varNum, unsigned offset);
 
-    int lvaVarNum(); // Returns the variable to access. Note that it returns a negative number for compiler spill temps.
-    unsigned lvaOffset(); // returns the offset into the variable to access
+    int lvaVarNum() const; // Returns the variable to access. Note that it returns a negative number for compiler spill
+                           // temps.
+    unsigned lvaOffset() const; // returns the offset into the variable to access
 
     // This struct should be 32 bits in size for the release build.
     // We have this constraint because this type is used in a union
@@ -2225,7 +2226,7 @@ protected:
     static const IS_INFO emitGetSchedInfo(insFormat f);
 #endif // TARGET_XARCH
 
-    cnsval_ssize_t emitGetInsSC(instrDesc* id);
+    cnsval_ssize_t emitGetInsSC(const instrDesc* id) const;
     unsigned emitInsCount;
 
     /************************************************************************/
