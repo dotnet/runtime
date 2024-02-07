9f2446c1b164c879cae0dc548a6ada961c4e8cfa
Tanner Gooding <tagoo@outlook.com>
2024-02-07 14:16:51 -0800

    Add a missing using statement to the test

--- src/tests/JIT/Regression/JitBlue/Runtime_98068/Runtime_98068.cs
+++ src/tests/JIT/Regression/JitBlue/Runtime_98068/Runtime_98068.cs
@@ -2,8 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Runtime.CompilerServices;
 using Xunit;
 
 public static class Runtime_98068
