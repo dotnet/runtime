9514316f98628d4b2bb8a8b6a39df58344f73264
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2024-02-08 09:38:27 +0000

    Improve API documentation for Enumerable.Index

--- src/libraries/System.Linq/src/System/Linq/Index.cs
+++ src/libraries/System.Linq/src/System/Linq/Index.cs
@@ -7,9 +7,9 @@ namespace System.Linq
 {
     public static partial class Enumerable
     {
-        /// <summary>Return index and the associated item.</summary>
+        /// <summary>Returns an enumerable that incorporates the element's index in a tuple.</summary>
         /// <typeparam name="TSource">The type of the elements of <paramref name="source" />.</typeparam>
-        /// <param name="source">An <see cref="IEnumerable{T}" /> to return an element from.</param>
+        /// <param name="source">The source enumerable providing the elements.</param>
         /// <exception cref="ArgumentNullException"><paramref name="source" /> is <see langword="null" />.</exception>
         public static IEnumerable<(int Index, TSource Item)> Index<TSource>(this IEnumerable<TSource> source)
         {
