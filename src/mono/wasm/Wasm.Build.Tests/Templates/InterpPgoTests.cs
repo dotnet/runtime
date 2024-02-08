2dcf7139e45449faff7a0cb83b0a4c19467ffe8e
Marek Fišera <mara@neptuo.com>
2024-02-08 18:22:17 +0100

    Explicitly set debug level to 0 to trigger tiering

--- src/mono/wasm/Wasm.Build.Tests/Templates/InterpPgoTests.cs
+++ src/mono/wasm/Wasm.Build.Tests/Templates/InterpPgoTests.cs
@@ -74,7 +74,7 @@ public class InterpPgoTests : WasmTemplateTestBase
         using var runCommand = new RunCommand(s_buildEnv, _testOutput)
                                     .WithWorkingDirectory(_projectDir!);
         await using var runner = new BrowserRunner(_testOutput);
-        var url = await runner.StartServerAndGetUrlAsync(runCommand, $"run --no-silent -c {config} --no-build --project \"{projectFile}\" --forward-console");
+        var url = await runner.StartServerAndGetUrlAsync(runCommand, $"run --no-silent -c {config} -p:WasmDebugLevel=0 --no-build --project \"{projectFile}\" --forward-console");
         IBrowser browser = await runner.SpawnBrowserAsync(url);
         IBrowserContext context = await browser.NewContextAsync();
 
