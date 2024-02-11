e243bdc67d60ecf8db9773900a85bb8f5992062b
Theodore Tsirpanis <teo@tsirpanis.gr>
2024-02-11 19:59:45 +0200

    Add a test for `AssemblyExtensions.TryGetRawMetadata`.

---
+++ src/libraries/System.Runtime.Loader/tests/AssemblyExtensionsTest.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Xunit;
+using System.Reflection.Metadata;
+using System.Reflection;
+
+namespace System.Runtime.Loader.Tests
+{
+    public unsafe class AssemblyExtensionsTest
+    {
+        [Fact]
+        public void TryGetRawMetadata()
+        {
+            bool supportsRawMetadata = PlatformDetection.IsNotMonoRuntime && PlatformDetection.IsNotNativeAot;
+
+            Assembly assembly = typeof(AssemblyExtensionsTest).Assembly;
+            bool hasMetadata = assembly.TryGetRawMetadata(out byte* blob, out int length);
+
+            Assert.Equal(supportsRawMetadata, hasMetadata);
+            Assert.Equal(supportsRawMetadata, blob != null);
+            Assert.Equal(supportsRawMetadata, length > 0);
+        }
+    }
+}
