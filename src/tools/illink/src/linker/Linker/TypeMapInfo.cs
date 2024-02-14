a73cc1f0a2ac77ea416db290da30adab7ca3aaf3
Jackson Schuster <36744439+jtschuster@users.noreply.github.com>
2024-02-14 10:18:36 -0800

    Make TypeMapInfo internal for API Compat

--- src/tools/illink/src/linker/Linker/TypeMapInfo.cs
+++ src/tools/illink/src/linker/Linker/TypeMapInfo.cs
@@ -38,7 +38,7 @@ using Mono.Cecil;
 namespace Mono.Linker
 {
 
-	public class TypeMapInfo
+	internal class TypeMapInfo
 	{
 		readonly HashSet<AssemblyDefinition> assemblies = new HashSet<AssemblyDefinition> ();
 		readonly LinkContext context;
