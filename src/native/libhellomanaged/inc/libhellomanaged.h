44ce17661760ec1c8da18e1ac86ea3fe9450a26a
Aleksey Kliger <alklig@microsoft.com>
2024-02-15 16:16:45 -0500

    add an include dir for libhellomanaged

---
+++ src/native/libhellomanaged/inc/libhellomanaged.h
@@ -0,0 +1,19 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#ifndef _LIBHELLOMANAGED_H
+#define _LIBHELLOMANAGED_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+void
+hellomanaged_Hello(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
