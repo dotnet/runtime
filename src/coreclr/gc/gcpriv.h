98876bf4bbf59bbaace57cb6ef34ebf4976a8161
Manish Godse <61718172+mangod9@users.noreply.github.com>
2024-02-15 12:44:03 -0800

    enable regions for MacOS

--- src/coreclr/gc/gcpriv.h
+++ src/coreclr/gc/gcpriv.h
@@ -141,7 +141,7 @@ inline void FATAL_GC_ERROR()
 // This means any empty regions can be freely used for any generation. For
 // Server GC we will balance regions between heaps.
 // For now disable regions for StandAlone GC, NativeAOT and MacOS builds
-#if defined (HOST_64BIT) && !defined (BUILD_AS_STANDALONE) && !defined(__APPLE__)
+#if defined (HOST_64BIT) && !defined (BUILD_AS_STANDALONE)
 #define USE_REGIONS
 #endif //HOST_64BIT && BUILD_AS_STANDALONE
 
