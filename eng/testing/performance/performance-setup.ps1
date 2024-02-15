0c772b8614f010508ff201fa0884c0b3629c2911
Parker Bibus <parkerbibus@microsoft.com>
2024-02-14 16:33:17 -0800

    Test custom android instrumentation flow.

--- eng/testing/performance/performance-setup.ps1
+++ eng/testing/performance/performance-setup.ps1
@@ -154,7 +154,7 @@ if ($RunFromPerformanceRepo) {
     robocopy $SourceDirectory $PerformanceDirectory /E /XD $PayloadDirectory $SourceDirectory\artifacts $SourceDirectory\.git
 }
 else {
-    git clone --branch main --depth 1 --quiet https://github.com/dotnet/performance $PerformanceDirectory
+    git clone --branch AddDeviceRestartForAndroidInstrumentation --depth 1 --quiet https://github.com/LoopedBard3/performance $PerformanceDirectory
 }
 
 if ($MonoDotnet -ne "") {
