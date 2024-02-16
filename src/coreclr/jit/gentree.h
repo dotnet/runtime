49bf6399e8258b68ea079568f52416fc30d937b8
Aman Khalid (from Dev Box) <amankhalid@microsoft.com>
2024-02-16 17:46:31 -0500

    Remove space

--- src/coreclr/jit/gentree.h
+++ src/coreclr/jit/gentree.h
@@ -4114,7 +4114,6 @@ enum GenTreeCallFlags : unsigned int
     GTF_CALL_M_HAS_LATE_DEVIRT_INFO    = 0x01000000, // this call has late devirtualzation info
     GTF_CALL_M_LDVIRTFTN_INTERFACE     = 0x02000000, // ldvirtftn on an interface type
     GTF_CALL_M_CAST_CAN_BE_EXPANDED    = 0x04000000, // this cast (helper call) can be expanded if it's profitable. To be removed.
-
     GTF_CALL_M_CAST_OBJ_NONNULL        = 0x08000000, // if we expand this specific cast we don't need to check the input object for null
                                                      // NOTE: if needed, this flag can be removed, and we can introduce new _NONNUL cast helpers
 
