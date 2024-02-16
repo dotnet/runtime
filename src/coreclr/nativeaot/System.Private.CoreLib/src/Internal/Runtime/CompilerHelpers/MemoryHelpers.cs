6d2da0be724e71c6da4f05a07d80755a77f5bd9f
Filip Navara <filip.navara@gmail.com>
2024-02-16 10:33:55 +0100

    Add null checks into memcpy/memset helpers

---
+++ src/coreclr/nativeaot/System.Private.CoreLib/src/Internal/Runtime/CompilerHelpers/MemoryHelpers.cs
@@ -0,0 +1,36 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Runtime;
+using System.Runtime.CompilerServices;
+
+namespace Internal.Runtime.CompilerHelpers
+{
+    /// <summary>
+    /// These methods are used to implement memcpy and memset intrinsics with null checks.
+    /// </summary>
+    internal static class MemoryHelpers
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static unsafe void MemSet(byte* dest, int value, nuint size)
+        {
+            if (size > 0)
+            {
+                _ = *dest;
+                RuntimeImports.memset(dest, value, size);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static unsafe void MemCopy(byte* dest, byte* src, nuint size)
+        {
+            if (size > 0)
+            {
+                _ = *dest;
+                _ = *src;
+                RuntimeImports.memcpy(dest, src, size);
+            }
+        }
+    }
+}
