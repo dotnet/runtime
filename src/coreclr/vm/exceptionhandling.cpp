918ff7cc8e894893fe25698731bc7dc32536b2fa
Jan Vorlicek <janvorli@microsoft.com>
2024-02-18 20:22:04 +0100

    Reflect PR - switch the config name
    From the EnableNewExceptionHandling to LegacyExceptionHandling

--- src/coreclr/vm/exceptionhandling.cpp
+++ src/coreclr/vm/exceptionhandling.cpp
@@ -237,7 +237,7 @@ void InitializeExceptionHandling()
     // Initialize the lock used for synchronizing access to the stacktrace in the exception object
     g_StackTraceArrayLock.Init(LOCK_TYPE_DEFAULT, TRUE);
 
-    g_isNewExceptionHandlingEnabled = Configuration::GetKnobBooleanValue(W("System.Runtime.EnableNewExceptionHandling"), CLRConfig::EXTERNAL_EnableNewExceptionHandling) != 0;
+    g_isNewExceptionHandlingEnabled = Configuration::GetKnobBooleanValue(W("System.Runtime.LegacyExceptionHandling"), CLRConfig::EXTERNAL_LegacyExceptionHandling ) == 0;
 
 #ifdef TARGET_UNIX
     // Register handler of hardware exceptions like null reference in PAL
