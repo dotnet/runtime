4bc7578b2304b434446fc213f6e00dd6ca073dd6
EgorBo <egorbo@gmail.com>
2024-02-18 23:46:05 +0100

    Fix regressions

--- src/coreclr/jit/codegenarm64.cpp
+++ src/coreclr/jit/codegenarm64.cpp
@@ -5108,7 +5108,18 @@ void CodeGen::genEmitHelperCall(unsigned helper, int argSize, emitAttr retSize, 
 
         callTarget = callTargetReg;
 
-        instGen_Set_Reg_To_Imm(EA_PTRSIZE, callTarget, (ssize_t)pAddr);
+        if (compiler->opts.compReloc)
+        {
+            // adrp + add with relocations will be emitted
+            GetEmitter()->emitIns_R_AI(INS_adrp, EA_PTR_DSP_RELOC, callTarget,
+                (ssize_t)pAddr DEBUGARG((size_t)compiler->eeFindHelper(helper))
+                DEBUGARG(GTF_ICON_METHOD_HDL));
+        }
+        else
+        {
+            instGen_Set_Reg_To_Imm(EA_PTRSIZE, callTarget, (ssize_t)pAddr);
+        }
+
         GetEmitter()->emitIns_R_R(INS_ldr, EA_PTRSIZE, callTarget, callTarget);
         callType = emitter::EC_INDIR_R;
     }
