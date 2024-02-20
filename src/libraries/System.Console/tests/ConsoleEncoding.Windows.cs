afcf0a95bec8d1fff6ca90215dbbc731d02a376b
Nick Beer <nick.beer@ni.com>
2024-02-20 13:38:10 -0600

    Add tests validating new behavior

--- src/libraries/System.Console/tests/ConsoleEncoding.Windows.cs
+++ src/libraries/System.Console/tests/ConsoleEncoding.Windows.cs
@@ -2,8 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
-using System.Text;
 using System.Runtime.InteropServices;
+using System.Text;
 using Microsoft.DotNet.RemoteExecutor;
 using Xunit;
 
@@ -40,6 +40,27 @@ public partial class ConsoleEncoding
 
     [ConditionalFact(typeof(RemoteExecutor), nameof(RemoteExecutor.IsSupported))]
     [PlatformSpecific(TestPlatforms.Windows)]
+    public void InputEncoding_SetEncodingWhenDetached_ErrorIsSilentlyIgnored()
+    {
+        RemoteExecutor.Invoke(() =>
+        {
+            Encoding encoding = Encoding.GetEncoding(0);
+            Assert.NotEqual(encoding.CodePage, Console.InputEncoding.CodePage);
+
+            // use FreeConsole to detach the current console - simulating a process started with the "DETACHED_PROCESS" flag
+            FreeConsole();
+
+            // Setting the input encoding should not throw an exception
+            Console.InputEncoding = encoding;
+            // The internal state of Console should have updated, despite the failure to change the console's input encoding
+            Assert.Equal(encoding, Console.InputEncoding);
+            // Operations on the console are no longer valid - GetConsoleCP fails.
+            Assert.Equal(0u, GetConsoleCP());
+        }).Dispose();
+    }
+
+    [ConditionalFact(typeof(RemoteExecutor), nameof(RemoteExecutor.IsSupported))]
+    [PlatformSpecific(TestPlatforms.Windows)]
     public void OutputEncoding_SetDefaultEncoding_Success()
     {
         RemoteExecutor.Invoke(() =>
@@ -67,9 +88,33 @@ public partial class ConsoleEncoding
         }).Dispose();
     }
 
+    [ConditionalFact(typeof(RemoteExecutor), nameof(RemoteExecutor.IsSupported))]
+    [PlatformSpecific(TestPlatforms.Windows)]
+    public void OutputEncoding_SetEncodingWhenDetached_ErrorIsSilentlyIgnored()
+    {
+        RemoteExecutor.Invoke(() =>
+        {
+            Encoding encoding = Encoding.GetEncoding(0);
+            Assert.NotEqual(encoding.CodePage, Console.OutputEncoding.CodePage);
+
+            // use FreeConsole to detach the current console - simulating a process started with the "DETACHED_PROCESS" flag
+            FreeConsole();
+
+            // Setting the output encoding should not throw an exception
+            Console.OutputEncoding = encoding;
+            // The internal state of Console should have updated, despite the failure to change the console's output encoding
+            Assert.Equal(encoding, Console.OutputEncoding);
+            // Operations on the console are no longer valid - GetConsoleOutputCP fails.
+            Assert.Equal(0u, GetConsoleOutputCP());
+        }).Dispose();
+    }
+
     [LibraryImport("kernel32.dll")]
     public static partial uint GetConsoleCP();
 
     [LibraryImport("kernel32.dll")]
     public static partial uint GetConsoleOutputCP();
+
+    [LibraryImport("kernel32.dll")]
+    public static partial int FreeConsole();
 }
