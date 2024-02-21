25e46722fbb32d85b2d20300a815eccb6352a20c
Filip Navara <filip.navara@gmail.com>
2024-02-21 10:27:10 +0100

    Add PInvoke helpers

--- src/coreclr/nativeaot/Runtime/i386/PInvoke.asm
+++ src/coreclr/nativeaot/Runtime/i386/PInvoke.asm
@@ -10,14 +10,32 @@ include AsmMacros.inc
 
 FASTCALL_FUNC RhpPInvoke, 4
 ALTERNATE_HELPER_ENTRY RhpPInvoke
-        ;; TODO
-        int 3
+        INLINE_GETTHREAD eax, edx
+
+        mov         edx, [esp]                  ; edx <- return address
+        mov         dword ptr [ecx + OFFSETOF__PInvokeTransitionFrame__m_pThread], eax
+        mov         dword ptr [ecx + OFFSETOF__PInvokeTransitionFrame__m_FramePointer], ebp
+        mov         dword ptr [ecx + OFFSETOF__PInvokeTransitionFrame__m_RIP], edx
+
+        lea         edx, [esp + 4]              ; edx <- caller SP
+        mov         dword ptr [ecx + OFFSETOF__PInvokeTransitionFrame__m_Flags], PTFF_SAVE_RSP
+        mov         dword ptr [ecx + OFFSETOF__PInvokeTransitionFrame__m_PreservedRegs], edx
+
+        mov         dword ptr [eax + OFFSETOF__Thread__m_pTransitionFrame], ecx
+
+        ret
 FASTCALL_ENDFUNC
 
 FASTCALL_FUNC RhpPInvokeReturn, 4
 ALTERNATE_HELPER_ENTRY RhpPInvokeReturn
-        ;; TODO
-        int 3
+        mov         edx, [ecx + OFFSETOF__PInvokeTransitionFrame__m_pThread]
+        mov         dword ptr [edx + OFFSETOF__Thread__m_pTransitionFrame], 0
+        cmp         [RhpTrapThreads], TrapThreadsFlags_None
+        jne         @F                  ; forward branch - predicted not taken
+        ret
+@@:
+        ; passing transition frame pointer in rcx
+        jmp         RhpWaitForGC2
 FASTCALL_ENDFUNC
 
         end
