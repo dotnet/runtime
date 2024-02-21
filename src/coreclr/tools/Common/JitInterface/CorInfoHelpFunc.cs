86c25d784ec8c438bc4a9b48a1fdcb5cce2135d8
EgorBo <egorbo@gmail.com>
2024-02-22 00:41:17 +0100

    Remove whitespace

--- src/coreclr/tools/Common/JitInterface/CorInfoHelpFunc.cs
+++ src/coreclr/tools/Common/JitInterface/CorInfoHelpFunc.cs
@@ -215,7 +215,7 @@ namespace Internal.JitInterface
 
         CORINFO_HELP_MEMSET,                // Init block of memory
         CORINFO_HELP_MEMCPY,                // Copy block of memory
-        CORINFO_HELP_NATIVE_MEMSET,         // Init block of memory using native memset (not safe for pDst being null, 
+        CORINFO_HELP_NATIVE_MEMSET,         // Init block of memory using native memset (not safe for pDst being null,
                                             // not safe for unbounded size, does not trigger GC)
 
         CORINFO_HELP_RUNTIMEHANDLE_METHOD,  // determine a type/field/method handle at run-time
