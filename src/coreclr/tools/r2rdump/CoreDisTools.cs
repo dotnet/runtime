3aace6ab4e5d3d0f91170c0dc1d0196981d5a22c
Tomek Sowiński <tomeksowi@gmail.com>
2024-02-22 10:34:31 +0100

    Remove unused dll import DumpCodeBlock

--- src/coreclr/tools/r2rdump/CoreDisTools.cs
+++ src/coreclr/tools/r2rdump/CoreDisTools.cs
@@ -30,9 +30,6 @@ namespace R2RDump
         public static extern IntPtr InitBufferedDisasm(TargetArch Target);
 
         [DllImport(_dll, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void DumpCodeBlock(IntPtr Disasm, IntPtr Address, IntPtr Bytes, IntPtr Size);
-
-        [DllImport(_dll, CallingConvention = CallingConvention.Cdecl)]
         public static extern int DumpInstruction(IntPtr Disasm, IntPtr Address, IntPtr Bytes, IntPtr Size);
 
         [DllImport(_dll, CallingConvention = CallingConvention.Cdecl)]
