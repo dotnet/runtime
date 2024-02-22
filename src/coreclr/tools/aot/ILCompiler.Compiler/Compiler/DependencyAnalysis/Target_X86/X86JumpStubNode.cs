ed6153032385a13d8fa4d49613dbf88835d9ae0f
Filip Navara <filip.navara@gmail.com>
2024-02-22 22:24:02 +0100

    Revert unnecessary change

--- src/coreclr/tools/aot/ILCompiler.Compiler/Compiler/DependencyAnalysis/Target_X86/X86JumpStubNode.cs
+++ src/coreclr/tools/aot/ILCompiler.Compiler/Compiler/DependencyAnalysis/Target_X86/X86JumpStubNode.cs
@@ -9,15 +9,7 @@ namespace ILCompiler.DependencyAnalysis
     {
         protected override void EmitCode(NodeFactory factory, ref X86Emitter encoder, bool relocsOnly)
         {
-            if (!_target.RepresentsIndirectionCell)
-            {
-                encoder.EmitJMP(_target);
-            }
-            else
-            {
-                encoder.EmitMOV(encoder.TargetRegister.Result, _target);
-                encoder.EmitJMP(_target);
-            }
+            encoder.EmitJMP(_target);
         }
     }
 }
