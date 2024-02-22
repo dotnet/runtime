3f64252742953b39156c3fe12aa9410ffb852be1
vsadov <8218165+VSadov@users.noreply.github.com>
2024-02-21 22:30:36 -0800

    make gcc happy

--- src/coreclr/jit/codegenlinear.cpp
+++ src/coreclr/jit/codegenlinear.cpp
@@ -712,8 +712,8 @@ void CodeGen::genCodeForBBlist()
                     if ((call != nullptr) && (call->gtOper == GT_CALL))
                     {
                         if ((call->AsCall()->gtCallMoreFlags & GTF_CALL_M_DOES_NOT_RETURN) != 0 ||
-                            (call->AsCall()->gtCallType == CT_HELPER) &&
-                                Compiler::s_helperCallProperties.AlwaysThrow(call->AsCall()->GetHelperNum()))
+                            ((call->AsCall()->gtCallType == CT_HELPER) &&
+                                Compiler::s_helperCallProperties.AlwaysThrow(call->AsCall()->GetHelperNum())))
                         {
                             instGen(INS_BREAKPOINT); // This should never get executed
                         }
