3f748cf7eb1f6ccb4b2f34035633202072ec120b
Aman Khalid (from Dev Box) <amankhalid@microsoft.com>
2024-02-21 21:43:11 -0500

    Invalidate map after profile checks

--- src/coreclr/jit/fgprofile.cpp
+++ src/coreclr/jit/fgprofile.cpp
@@ -5285,7 +5285,8 @@ bool Compiler::fgProfileWeightsConsistent(weight_t weight1, weight_t weight2)
 //
 void Compiler::fgDebugCheckProfileWeights()
 {
-    const bool configEnabled = (JitConfig.JitProfileChecks() >= 0) && fgHaveProfileWeights() && fgPredsComputed;
+    const bool configEnabled  = (JitConfig.JitProfileChecks() >= 0) && fgHaveProfileWeights() && fgPredsComputed;
+    const bool resetSwitchMap = (m_switchDescMap == nullptr);
 
     if (configEnabled)
     {
@@ -5295,6 +5296,14 @@ void Compiler::fgDebugCheckProfileWeights()
     {
         ProfileChecks checks = ProfileChecks::CHECK_HASLIKELIHOOD | ProfileChecks::RAISE_ASSERT;
         fgDebugCheckProfileWeights(checks);
+    }
+
+    if (resetSwitchMap)
+    {
+        // m_switchDescMap did not exist beforehand, but we created it during profile checks
+        // (likely while iterating successor edges for a switch block).
+        // We don't want Debug behavior to diverge from Release behavior, so reset the map.
+        InvalidateUniqueSwitchSuccMap();
     }
 }
 
