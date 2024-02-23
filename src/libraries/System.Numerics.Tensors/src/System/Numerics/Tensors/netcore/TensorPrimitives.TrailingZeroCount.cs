cf59f13325ac51b3fd28f927b010a07f6a8bdf1d
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2024-02-23 22:36:25 +0000

    Vectorize LeadingZeroCount & TrailingZeroCount.

--- src/libraries/System.Numerics.Tensors/src/System/Numerics/Tensors/netcore/TensorPrimitives.TrailingZeroCount.cs
+++ src/libraries/System.Numerics.Tensors/src/System/Numerics/Tensors/netcore/TensorPrimitives.TrailingZeroCount.cs
@@ -1,7 +1,11 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Runtime.CompilerServices;
 using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.Arm;
+
+#pragma warning disable CS8500 // This takes the address of, gets the size of, or declares a pointer to a managed type
 
 namespace System.Numerics.Tensors
 {
@@ -22,13 +26,52 @@ namespace System.Numerics.Tensors
             InvokeSpanIntoSpan<T, TrailingZeroCountOperator<T>>(x, destination);
 
         /// <summary>T.TrailingZeroCount(x)</summary>
-        private readonly struct TrailingZeroCountOperator<T> : IUnaryOperator<T, T> where T : IBinaryInteger<T>
+        private readonly unsafe struct TrailingZeroCountOperator<T> : IUnaryOperator<T, T> where T : IBinaryInteger<T>
         {
-            public static bool Vectorizable => false; // TODO: Vectorize
+            public static bool Vectorizable =>
+                (AdvSimd.IsSupported && AdvSimd.Arm64.IsSupported && sizeof(T) == 1) ||
+                PopCountOperator<T>.Vectorizable; // http://0x80.pl/notesen/2023-01-31-avx512-bsf.html#trailing-zeros-simplified
+
             public static T Invoke(T x) => T.TrailingZeroCount(x);
-            public static Vector128<T> Invoke(Vector128<T> x) => throw new NotSupportedException();
-            public static Vector256<T> Invoke(Vector256<T> x) => throw new NotSupportedException();
-            public static Vector512<T> Invoke(Vector512<T> x) => throw new NotSupportedException();
+
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static Vector128<T> Invoke(Vector128<T> x)
+            {
+                if (AdvSimd.IsSupported && sizeof(T) == 1)
+                {
+                    return AdvSimd.LeadingZeroCount(AdvSimd.Arm64.ReverseElementBits(x.AsByte())).As<byte, T>();
+                }
+
+                if (PopCountOperator<T>.Vectorizable)
+                {
+                    return PopCountOperator<T>.Invoke(~x & (x - Vector128<T>.One));
+                }
+
+                throw new NotSupportedException();
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static Vector256<T> Invoke(Vector256<T> x)
+            {
+                if (PopCountOperator<T>.Vectorizable)
+                {
+                    PopCountOperator<T>.Invoke(~x & (x - Vector256<T>.One));
+                }
+
+                return Vector256.Create(Invoke(x.GetLower()), Invoke(x.GetUpper()));
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static Vector512<T> Invoke(Vector512<T> x)
+            {
+                if (PopCountOperator<T>.Vectorizable)
+                {
+                    PopCountOperator<T>.Invoke(~x & (x - Vector512<T>.One));
+                }
+
+                return Vector512.Create(Invoke(x.GetLower()), Invoke(x.GetUpper()));
+            }
         }
     }
 }
