6e145f55789dcf6ec3f561ab0c7aff6348ff6170
Jan Vorlicek <janvorli@microsoft.com>
2024-02-23 17:22:21 +0100

    Make the fix x64 specific
    The debugger also special-cases this for x64

--- src/coreclr/vm/debugdebugger.cpp
+++ src/coreclr/vm/debugdebugger.cpp
@@ -1153,14 +1153,14 @@ void DebugStackTrace::GetStackFramesFromException(OBJECTREF * e,
                 DWORD dwNativeOffset;
 
                 UINT_PTR ip = cur.ip;
-#if DACCESS_COMPILE
+#if defined(DACCESS_COMPILE) && defined(TARGET_AMD64)
                 // Compensate for a bug in the old EH that for a frame that faulted
                 // has the ip pointing to an address before the faulting instruction
                 if (g_isNewExceptionHandlingEnabled && (i == 0) && ((cur.flags & STEF_IP_ADJUSTED) == 0))
                 {
                     ip -= 1;
                 }
-#endif // DACCESS_COMPILE
+#endif // DACCESS_COMPILE && TARGET_AMD64
                 if (ip)
                 {
                     EECodeInfo codeInfo(ip);
