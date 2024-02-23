37502b11b6b80c03bf7920a23105ef81c3726e69
Steven He <hez2010@outlook.com>
2024-02-23 20:49:50 +0900

    Fix tests

--- src/libraries/System.Diagnostics.StackTrace/tests/StackTraceTests.cs
+++ src/libraries/System.Diagnostics.StackTrace/tests/StackTraceTests.cs
@@ -285,13 +285,8 @@ namespace System.Diagnostics.Tests
             yield return new object[] { NoParameters(), "System.Diagnostics.Tests.StackTraceTests.NoParameters()" };
             yield return new object[] { OneParameter(1), "System.Diagnostics.Tests.StackTraceTests.OneParameter(Int32 x)" };
             yield return new object[] { TwoParameters(1, null), "System.Diagnostics.Tests.StackTraceTests.TwoParameters(Int32 x, String y)" };
-#if DEBUG
             yield return new object[] { Generic<int>(), "System.Diagnostics.Tests.StackTraceTests.Generic[System.Int32]()" };
             yield return new object[] { Generic<int, string>(), "System.Diagnostics.Tests.StackTraceTests.Generic[System.Int32,System.String]()" };
-#else
-            yield return new object[] { Generic<int>(), "System.Diagnostics.Tests.StackTraceTests.Generic[System.Int32]()" };
-            yield return new object[] { Generic<int, string>(), "System.Diagnostics.Tests.StackTraceTests.Generic[System.Int32,System.__Canon]()" };
-#endif
             yield return new object[] { new ClassWithConstructor().StackTrace, "System.Diagnostics.Tests.StackTraceTests.ClassWithConstructor..ctor()" };
 
             // Methods belonging to the System.Diagnostics namespace are ignored.
