6dfe8c0368ab735d2676a13c9aa357d193c3c4f3
EgorBo <egorbo@gmail.com>
2024-02-23 20:05:09 +0100

    Fix unix build

--- src/libraries/System.Private.CoreLib/src/System/Buffer.Unix.cs
+++ src/libraries/System.Private.CoreLib/src/System/Buffer.Unix.cs
@@ -9,11 +9,11 @@ namespace System
         // Managed code is currently faster than glibc unoptimized memmove
         // TODO-ARM64-UNIX-OPT revisit when glibc optimized memmove is in Linux distros
         // https://github.com/dotnet/runtime/issues/8897
-        private static nuint MemmoveNativeThreshold => nuint.MaxValue;
+        internal static nuint MemmoveNativeThreshold => nuint.MaxValue;
 #elif TARGET_ARM
-        private const nuint MemmoveNativeThreshold = 512;
+        internal const nuint MemmoveNativeThreshold = 512;
 #else
-        private const nuint MemmoveNativeThreshold = 2048;
+        internal const nuint MemmoveNativeThreshold = 2048;
 #endif
         // TODO: Determine optimal value
         internal const nuint ZeroMemoryNativeThreshold = 1024;
