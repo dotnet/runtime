1225ccb592f1e1b433a3c5b309ee7bbb73cb8efd
Katelyn Gadd <kg@luminance.org>
2024-02-26 18:18:53 -0800

    Apply suggestions from code review
    Co-authored-by: Jan Kotas <jkotas@microsoft.com>

--- src/coreclr/System.Private.CoreLib/src/System/RuntimeType.CoreCLR.cs
+++ src/coreclr/System.Private.CoreLib/src/System/RuntimeType.CoreCLR.cs
@@ -2757,14 +2757,12 @@ namespace System
                 MethodBase? rtTypeMethodBase = GetMethodBase(reflectedType, classRtMethodHandle);
                 // a class may not implement all the methods of an interface (abstract class) so null is a valid value
                 Debug.Assert(rtTypeMethodBase is null || rtTypeMethodBase is RuntimeMethodInfo);
-                RuntimeMethodInfo? targetMethod = rtTypeMethodBase as RuntimeMethodInfo;
+                RuntimeMethodInfo? targetMethod = (RuntimeMethodInfo)rtTypeMethodBase;
                 // the TargetMethod provided to us by runtime internals may be a generic method instance,
                 //  potentially with invalid arguments. TargetMethods in the InterfaceMap should never be
                 //  instances, only definitions.
-                if ((targetMethod?.IsGenericMethod == true) && !targetMethod.IsGenericMethodDefinition)
-                    targetMethod = (RuntimeMethodInfo)targetMethod.GetGenericMethodDefinition();
-
-                im.TargetMethods[i] = targetMethod!;
+                im.TargetMethods[i] = (targetMethod is { IsGenericMethod: true, IsGenericMethodDefinition: false })
+                    ? targetMethod.GetGenericMethodDefinition() : targetMethod!;
             }
 
             return im;
