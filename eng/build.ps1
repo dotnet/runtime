d65fae82e616e8a972e1247ce949bf0183377f7a
Alexander Köplinger <alex.koeplinger@outlook.com>
2024-02-28 12:05:02 +0100

    Disable terminal logger when running from root build script
    Since the 9.0-preview1 bump the msbuild terminal logger was used again since the option from Directory.Build.rsp was ignored.
    The reason is that we're using Build.proj from an arcade nuget package so the file isn't in the directory tree above, it looks like it was only working due to an msbuild bug before.
    
    Add the -tl:false option into the build scripts directly. Individual invocations of `dotnet build` for repo projects still work because there the Directory.Build.rsp is in the directory tree above.

--- eng/build.ps1
+++ eng/build.ps1
@@ -325,6 +325,9 @@ if ($env:TreatWarningsAsErrors -eq 'false') {
   $arguments += " -warnAsError 0"
 }
 
+# disable terminal logger for now: https://github.com/dotnet/runtime/issues/97211
+$arguments += " -tl:false"
+
 # Disable targeting pack caching as we reference a partially constructed targeting pack and update it later.
 # The later changes are ignored when using the cache.
 $env:DOTNETSDK_ALLOW_TARGETING_PACK_CACHING=0
