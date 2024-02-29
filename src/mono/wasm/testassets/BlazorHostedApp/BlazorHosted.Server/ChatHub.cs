05d8fd41c0c584a229545e264f5d4f829f152124
Ilona Tomkowicz <itomkowicz@microsoft.com>
2024-02-29 12:55:29 +0100

    Feedback - these logs don't need to be "TestOutput" type.

--- src/mono/wasm/testassets/BlazorHostedApp/BlazorHosted.Server/ChatHub.cs
+++ src/mono/wasm/testassets/BlazorHostedApp/BlazorHosted.Server/ChatHub.cs
@@ -8,19 +8,14 @@ public class ChatHub : Hub
 {
     public async Task SendMessage(string message, int sendingThreadId)
     {
-        TestOutputWriteLine($"Server: receives Message=[{message}] sent by treadID = {sendingThreadId} and sends it back.");
+        Console.WriteLine($"Server: receives Message=[{message}] sent by treadID = {sendingThreadId} and sends it back.");
         string changedMessage = $"{message}-pong";
         await Clients.All.SendAsync("ReceiveMessage", changedMessage).ConfigureAwait(false);
     }
 
     public void Exit(int code)
     {
-        TestOutputWriteLine($"Received exit code {code} from client.");
+        Console.WriteLine($"Received exit code {code} from client.");
         Environment.Exit(code);
-    }
-
-    public static void TestOutputWriteLine(string message)
-    {
-        Console.WriteLine("TestOutput -> " + message);
     }
 }
