049da221a7f0e38eb92ef72fe0611b9b72e513da
xtqqczze <45661989+xtqqczze@users.noreply.github.com>
2024-03-01 15:16:02 +0000

    Remove IDE0060 suppressions (#99113)
    Issue resolved in `Microsoft.CodeAnalysis.CSharp.CodeStyle` 4.5.0-3.22613.20

--- src/libraries/System.Private.CoreLib/src/System/SearchValues/ProbabilisticMap.cs
+++ src/libraries/System.Private.CoreLib/src/System/SearchValues/ProbabilisticMap.cs
@@ -10,7 +10,6 @@ using System.Runtime.Intrinsics.Arm;
 using System.Runtime.Intrinsics.Wasm;
 using System.Runtime.Intrinsics.X86;
 
-#pragma warning disable IDE0060 // https://github.com/dotnet/roslyn-analyzers/issues/6228
 
 namespace System.Buffers
 {
