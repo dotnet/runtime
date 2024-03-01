ada9f29f8d1d52bbb32bd4d3dd71ca62aafecc9d
pedrobsaila <badrebsaila@outlook.com>
2024-03-01 01:34:25 +0100

    add tests

--- PimpMyApp/Program.cs
+++ PimpMyApp/Program.cs
@@ -14,6 +14,12 @@ public class Program
         int result = Bambala1(6, 23);
         result += Bambala2(6, 23, 45, 66, 2);
         long result2 = Bambala3(6, 23, 45, 66, 2, 2);
+
+        if (Or10Or5(14, 23) != Or15(14, 23) || Or10Or5(78, 11) != Or15(78, 11))
+        {
+            Console.WriteLine("Oups");
+        }
+
         return result + (int)result2;
     }
 
@@ -25,4 +31,16 @@ public class Program
 
     [MethodImpl(MethodImplOptions.NoInlining)]
     private static long Bambala3(long m, int p, int u, int x, int y, int z) => m * System.Math.BigMul(((u | 2) | (x | 5) | (y | 3) | (z | 6)), p);
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private static int Or10Or5(int x, int y)
+    {
+        return (x | 10) | (y | 5);
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private static int Or15(int x, int y)
+    {
+        return (x | y) | 15;
+    }
 }
