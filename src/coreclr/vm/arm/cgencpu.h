f805eda12e91658fbb1f35e0b7a2723752b4d7d7
David Wrighton <davidwr@microsoft.com>
2024-03-05 14:50:02 -0800

    Enable new model for Arm32 by removing all special cases for Arm32

--- src/coreclr/vm/arm/cgencpu.h
+++ src/coreclr/vm/arm/cgencpu.h
@@ -1005,11 +1005,6 @@ inline BOOL ClrFlushInstructionCache(LPCVOID pCodeAddr, size_t sizeOfCode, bool 
 //
 // Create alias for optimized implementations of helpers provided on this platform
 //
-#define JIT_GetSharedGCStaticBase           JIT_GetSharedGCStaticBase_SingleAppDomain
-#define JIT_GetSharedNonGCStaticBase        JIT_GetSharedNonGCStaticBase_SingleAppDomain
-#define JIT_GetSharedGCStaticBaseNoCtor     JIT_GetSharedGCStaticBaseNoCtor_SingleAppDomain
-#define JIT_GetSharedNonGCStaticBaseNoCtor  JIT_GetSharedNonGCStaticBaseNoCtor_SingleAppDomain
-
 
 //------------------------------------------------------------------------
 //
