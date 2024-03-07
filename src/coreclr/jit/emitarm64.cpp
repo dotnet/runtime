1b5aa83d6c0b820c9699d6d5b1fa024db95777e5
Sebastian Nickolls <sebastian.nickolls@arm.com>
2024-03-07 13:31:57 +0000

    Remove unused tables

--- src/coreclr/jit/emitarm64.cpp
+++ src/coreclr/jit/emitarm64.cpp
@@ -3286,28 +3286,6 @@ static const char * const  qRegNames[] =
     "q30", "q31"
 };
 
-static const char * const  dRegNames[] =
-{
-    "d0",  "d1",  "d2",  "d3",  "d4",
-    "d5",  "d6",  "d7",  "d8",  "d9",
-    "d10", "d11", "d12", "d13", "d14",
-    "d15", "d16", "d17", "d18", "d19",
-    "d20", "d21", "d22", "d23", "d24",
-    "d25", "d26", "d27", "d28", "d29",
-    "d30", "d31"
-};
-
-static const char * const  sRegNames[] =
-{
-    "s0",  "s1",  "s2",  "s3",  "s4",
-    "s5",  "s6",  "s7",  "s8",  "s9",
-    "s10", "s11", "s12", "s13", "s14",
-    "s15", "s16", "s17", "s18", "s19",
-    "s20", "s21", "s22", "s23", "s24",
-    "s25", "s26", "s27", "s28", "s29",
-    "s30", "s31"
-};
-
 static const char * const  hRegNames[] =
 {
     "h0",  "h1",  "h2",  "h3",  "h4",
