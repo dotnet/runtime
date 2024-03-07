aa13ab53b3d5b632ccec7e2d034054a9fde37401
Jackson Schuster <36744439+jtschuster@users.noreply.github.com>
2024-03-07 15:40:44 -0800

    Add Dictionary<T, List<T>>.AddToList

---
+++ src/tools/illink/src/linker/Linker/DictionaryExtensions.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+
+namespace Mono.Linker
+{
+	public static class DictionaryExtensions
+	{
+		public static void AddToList<TKey, TList, TValueElement> (this Dictionary<TKey, TList> me, TKey key, TValueElement value) where TKey : notnull where TList : ICollection<TValueElement>, new()
+		{
+			if (!me.TryGetValue (key, out TList? methods)) {
+				methods = new TList ();
+				me[key] = methods;
+			}
+			methods.Add (value);
+		}
+	}
+}
