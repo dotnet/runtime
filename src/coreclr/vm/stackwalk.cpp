379e08ce0f8571bea5d775f61afb640383a84386
Jan Vorlicek <janvorli@microsoft.com>
2024-03-07 16:58:29 +0100

    The real fix

--- src/coreclr/vm/stackwalk.cpp
+++ src/coreclr/vm/stackwalk.cpp
@@ -2145,7 +2145,15 @@ ProcessFuncletsForGCReporting:
                                                 "STACKWALK: Reached parent of funclet which didn't report GC roots is not a funclet, resetting m_fDidFuncletReportGCReferences to true\n");
                                         }
 
-                                        if (!g_isNewExceptionHandlingEnabled)
+                                        if (g_isNewExceptionHandlingEnabled)
+                                        {
+                                            _ASSERTE(!ExceptionTracker::HasFrameBeenUnwoundByAnyActiveException(&m_crawl));
+                                            if (m_fFuncletNotSeen && m_crawl.IsFunclet())
+                                            {
+                                                m_fFuncletNotSeen = false;
+                                            }
+                                        }
+                                        else
                                         {
                                             STRESS_LOG4(LF_GCROOTS, LL_INFO100,
                                             "Funclet didn't report references: handling frame: %p, m_sfFuncletParent = %p, is funclet: %d, skip reporting %d\n",
