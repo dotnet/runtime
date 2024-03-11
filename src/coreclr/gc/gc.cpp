397d04883ccec635d21d6891c4beea3cd26150bd
Alexey Zakharov <alexeyzakharov@unity3d.com>
2024-03-11 09:42:17 +0100

    Skip ro segments which are not in range when walking heap for GC diagnostics

--- src/coreclr/gc/gc.cpp
+++ src/coreclr/gc/gc.cpp
@@ -52095,7 +52095,7 @@ void CFinalize::CheckFinalizerObjects()
 void gc_heap::walk_heap_per_heap (walk_fn fn, void* context, int gen_number, BOOL walk_large_object_heap_p)
 {
     generation* gen = gc_heap::generation_of (gen_number);
-    heap_segment*    seg = generation_start_segment (gen);
+    heap_segment*    seg = heap_segment_in_range (generation_start_segment (gen));
     uint8_t* x = ((gen_number == max_generation) ? heap_segment_mem (seg) : get_soh_start_object (seg, gen));
     uint8_t*       end = heap_segment_allocated (seg);
     int align_const = get_alignment_constant (TRUE);
@@ -52105,7 +52105,7 @@ void gc_heap::walk_heap_per_heap (walk_fn fn, void* context, int gen_number, BOO
     {
         if (x >= end)
         {
-            if ((seg = heap_segment_next (seg)) != 0)
+            if ((seg = heap_segment_next_in_range (seg)) != 0)
             {
                 x = heap_segment_mem (seg);
                 end = heap_segment_allocated (seg);
@@ -52117,7 +52117,7 @@ void gc_heap::walk_heap_per_heap (walk_fn fn, void* context, int gen_number, BOO
                 // advance to next lower generation
                 gen_number--;
                 gen = gc_heap::generation_of (gen_number);
-                seg = generation_start_segment (gen);
+                seg = heap_segment_in_range (generation_start_segment (gen));
 
                 x = heap_segment_mem (seg);
                 end = heap_segment_allocated (seg);
@@ -52129,12 +52129,12 @@ void gc_heap::walk_heap_per_heap (walk_fn fn, void* context, int gen_number, BOO
                 if (walk_large_object_heap_p)
                 {
                     walk_large_object_heap_p = FALSE;
-                    seg = generation_start_segment (large_object_generation);
+                    seg = heap_segment_in_range (generation_start_segment (large_object_generation));
                 }
                 else if (walk_pinned_object_heap)
                 {
                     walk_pinned_object_heap = FALSE;
-                    seg = generation_start_segment (pinned_object_generation);
+                    seg = heap_segment_in_range (generation_start_segment (pinned_object_generation));
                 }
                 else
                 {
