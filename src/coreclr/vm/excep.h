1400c1e7a888ea1e710e5c08d55c800e0b04bf8a
Jan Kotas <jkotas@microsoft.com>
2024-03-11 15:02:31 -0700

    Cleanup ifdefs (#99511)
    - Delete unused defines
    - Fix logic around funclet related ifdefs in a few places

--- src/coreclr/vm/excep.h
+++ src/coreclr/vm/excep.h
@@ -520,9 +520,9 @@ EXCEPTION_HANDLER_DECL(COMPlusFrameHandlerRevCom);
 // Pop off any SEH handlers we have registered below pTargetSP
 VOID __cdecl PopSEHRecords(LPVOID pTargetSP);
 
-#if defined(TARGET_X86) && defined(DEBUGGING_SUPPORTED)
+#ifdef DEBUGGING_SUPPORTED
 VOID UnwindExceptionTrackerAndResumeInInterceptionFrame(ExInfo* pExInfo, EHContext* context);
-#endif // TARGET_X86 && DEBUGGING_SUPPORTED
+#endif // DEBUGGING_SUPPORTED
 
 BOOL PopNestedExceptionRecords(LPVOID pTargetSP, BOOL bCheckForUnknownHandlers = FALSE);
 VOID PopNestedExceptionRecords(LPVOID pTargetSP, T_CONTEXT *pCtx, void *pSEH);
