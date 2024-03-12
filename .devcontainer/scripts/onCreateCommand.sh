cc29f99e5f28d280487024bae63bad810e3da069
Ilona Tomkowicz <32700855+ilonatommy@users.noreply.github.com>
2024-03-12 16:20:36 +0000

    Add MT option.

--- .devcontainer/scripts/onCreateCommand.sh
+++ .devcontainer/scripts/onCreateCommand.sh
@@ -20,15 +20,33 @@ case "$opt" in
     ;;
 
     wasm)
-        # prebuild for WASM, so it is ready for wasm development
-        make -C src/mono/browser provision-wasm
-        export EMSDK_PATH=$PWD/src/mono/browser/emsdk
-        ./build.sh mono+libs -os browser -c Release
+        wasm_common $opt
+    ;;
 
-        # install dotnet-serve for running wasm samples
-        ./dotnet.sh tool install dotnet-serve --version 1.10.172 --tool-path ./.dotnet-tools-global
+    wasm-multithread)
+        wasm_common $opt
     ;;
 esac
+
+function wasm_common() {
+    # prebuild for WASM, so it is ready for wasm development
+    make -C src/mono/browser provision-wasm
+    export EMSDK_PATH=$PWD/src/mono/browser/emsdk
+    case "$1" in
+    wasm)
+        # Put your common commands for wasm here
+        ./build.sh mono+libs -os browser -c Release
+        ;;
+    wasm-multithread)
+        # Put your common commands for wasm-multithread here
+        ./build.sh mono+libs -os browser -c Release /p:WasmEnableThreads=true
+        ;;
+    *)
+        # install dotnet-serve for running wasm samples
+    ./dotnet.sh tool install dotnet-serve --version 1.10.172 --tool-path ./.dotnet-tools-global
+    ;;
+    esac
+}
 
 # save the commit hash of the currently built assemblies, so developers know which version was built
 git rev-parse HEAD > ./artifacts/prebuild.sha
