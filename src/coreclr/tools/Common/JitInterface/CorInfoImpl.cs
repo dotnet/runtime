60f5d23813ee54ee79b4d695e065d3f86e3ecffd
Sergio Pedri <sergio0694@live.com>
2024-03-13 09:43:26 -0700

    Update __Canon check in managed impl too
    Co-authored-by: Michal Strehovský <MichalStrehovsky@users.noreply.github.com>

--- src/coreclr/tools/Common/JitInterface/CorInfoImpl.cs
+++ src/coreclr/tools/Common/JitInterface/CorInfoImpl.cs
@@ -2945,7 +2945,7 @@ namespace Internal.JitInterface
         {
             TypeDesc type = HandleToObject(cls);
 
-            if (type.IsCanonicalSubtype(CanonicalFormKind.Any))
+            if (type.IsCanonicalDefinitionType(CanonicalFormKind.Any))
             {
                 return TypeCompareState.May;
             }
