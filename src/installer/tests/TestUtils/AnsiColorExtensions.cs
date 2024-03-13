e5724b3e23b6d855f6cd58458e79d02d6107206f
Stephen Toub <stoub@microsoft.com>
2024-03-13 13:17:47 -0400

    Use \e instead of \u001B or \x1B

--- src/installer/tests/TestUtils/AnsiColorExtensions.cs
+++ src/installer/tests/TestUtils/AnsiColorExtensions.cs
@@ -7,46 +7,46 @@ namespace Microsoft.DotNet.Cli.Build.Framework
     {
         public static string Black(this string text)
         {
-            return "\x1B[30m" + text + "\x1B[39m";
+            return "\e[30m" + text + "\e[39m";
         }
 
         public static string Red(this string text)
         {
-            return "\x1B[31m" + text + "\x1B[39m";
+            return "\e[31m" + text + "\e[39m";
         }
         public static string Green(this string text)
         {
-            return "\x1B[32m" + text + "\x1B[39m";
+            return "\e[32m" + text + "\e[39m";
         }
 
         public static string Yellow(this string text)
         {
-            return "\x1B[33m" + text + "\x1B[39m";
+            return "\e[33m" + text + "\e[39m";
         }
 
         public static string Blue(this string text)
         {
-            return "\x1B[34m" + text + "\x1B[39m";
+            return "\e[34m" + text + "\e[39m";
         }
 
         public static string Magenta(this string text)
         {
-            return "\x1B[35m" + text + "\x1B[39m";
+            return "\e[35m" + text + "\e[39m";
         }
 
         public static string Cyan(this string text)
         {
-            return "\x1B[36m" + text + "\x1B[39m";
+            return "\e[36m" + text + "\e[39m";
         }
 
         public static string White(this string text)
         {
-            return "\x1B[37m" + text + "\x1B[39m";
+            return "\e[37m" + text + "\e[39m";
         }
 
         public static string Bold(this string text)
         {
-            return "\x1B[1m" + text + "\x1B[22m";
+            return "\e[1m" + text + "\e[22m";
         }
     }
 }
