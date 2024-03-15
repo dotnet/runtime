777fc0d86263e1e75434a127584b694775d26185
Tanner Gooding <tagoo@outlook.com>
2024-03-15 09:52:42 -0700

    Fixing the shift-left handling to correctly account for overshifting

--- src/libraries/System.Private.CoreLib/src/System/Number.BigInteger.cs
+++ src/libraries/System.Private.CoreLib/src/System/Number.BigInteger.cs
@@ -31,7 +31,9 @@ namespace System
             private const int MaxBits = BitsForLongestBinaryMantissa + BitsForLongestDigitSequence + BitsPerBlock;
 
             private const int BitsPerBlock = sizeof(int) * 8;
-            private const int MaxBlockCount = (MaxBits + (BitsPerBlock - 1)) / BitsPerBlock;
+
+            // We need one extra block to make our shift left algorithm significantly simpler
+            private const int MaxBlockCount = ((MaxBits + (BitsPerBlock - 1)) / BitsPerBlock) + 1;
 
             private static readonly uint[] s_Pow10UInt32Table = new uint[]
             {
@@ -302,7 +304,8 @@ namespace System
                 0xD9D61A05,
                 0x00000325,
 
-                // 9 Trailing blocks to ensure MaxBlockCount
+                // 10 Trailing blocks to ensure MaxBlockCount
+                0x00000000,
                 0x00000000,
                 0x00000000,
                 0x00000000,
@@ -737,8 +740,6 @@ namespace System
                 int index = 0;
                 uint carry = 0;
 
-                int resultLength = lhsLength;
-
                 while (index < lhsLength)
                 {
                     ulong product = ((ulong)(lhs._blocks[index]) * value) + carry;
@@ -747,6 +748,8 @@ namespace System
 
                     index++;
                 }
+
+                int resultLength = lhsLength;
 
                 if (carry != 0)
                 {
@@ -1217,19 +1220,21 @@ namespace System
                 int readIndex = (length - 1);
                 int writeIndex = readIndex + (int)(blocksToShift);
 
-                uint remainingBitsInLastBlock = (uint)BitOperations.LeadingZeroCount(_blocks[readIndex]);
-
-                if (remainingBitsToShift > remainingBitsInLastBlock)
-                {
-                    // We need an extra block for the partial shift
-                    writeIndex++;
-                }
-
-                Debug.Assert(unchecked((uint)(writeIndex)) < MaxBlockCount);
-
                 // Check if the shift is block aligned
                 if (remainingBitsToShift == 0)
                 {
+                    Debug.Assert(unchecked((uint)(length)) < MaxBlockCount);
+
+                    if (unchecked((uint)(length)) >= MaxBlockCount)
+                    {
+                        // We shouldn't reach here, and the above assert will help flag this
+                        // during testing, but we'll ensure that we return a safe value of
+                        // zero in the case we end up overflowing in any way.
+
+                        SetZero(out this);
+                        return;
+                    }
+
                     while (readIndex >= 0)
                     {
                         _blocks[writeIndex] = _blocks[readIndex];
@@ -1244,6 +1249,21 @@ namespace System
                 }
                 else
                 {
+                    // We need an extra block for the partial shift
+
+                    writeIndex++;
+                    Debug.Assert(unchecked((uint)(length)) < MaxBlockCount);
+
+                    if (unchecked((uint)(length)) >= MaxBlockCount)
+                    {
+                        // We shouldn't reach here, and the above assert will help flag this
+                        // during testing, but we'll ensure that we return a safe value of
+                        // zero in the case we end up overflowing in any way.
+
+                        SetZero(out this);
+                        return;
+                    }
+
                     // Set the length to hold the shifted blocks
                     _length = writeIndex + 1;
 
