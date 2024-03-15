b3b7ee0e76fd25171d423f09464657d8e0a76bcd
fanyang-mono <yangfan@microsoft.com>
2024-03-15 12:46:24 -0400

    Split the testcase out which works on Mono

--- src/tests/Loader/classloader/generics/ByRefLike/ValidateNegative.cs
+++ src/tests/Loader/classloader/generics/ByRefLike/ValidateNegative.cs
@@ -12,13 +12,21 @@ using Xunit;
 public class ValidateNegative
 {
     [Fact]
+    [SkipOnMono("https://github.com/dotnet/runtime/issues/99820")]
     public static void AllowByRefLike_Substituted_For_NonByRefLike_Invalid()
     {
         Console.WriteLine($"{nameof(AllowByRefLike_Substituted_For_NonByRefLike_Invalid)}...");
 
         Assert.Throws<TypeLoadException>(() => { Exec.TypeSubstitutionInterfaceImplementationAllowByRefLikeIntoNonByRefLike(); });
-        // Assert.Throws<TypeLoadException>(() => { Exec.TypeSubstitutionInheritanceAllowByRefLikeIntoNonByRefLike(); });
-        // Assert.Throws<TypeLoadException>(() => { Exec.TypeSubstitutionFieldAllowByRefLikeIntoNonByRefLike(); });
-        // Assert.Throws<TypeLoadException>(() => { Exec.OverrideMethodNotByRefLike(); });
+        Assert.Throws<TypeLoadException>(() => { Exec.TypeSubstitutionFieldAllowByRefLikeIntoNonByRefLike(); });
+        Assert.Throws<TypeLoadException>(() => { Exec.OverrideMethodNotByRefLike(); });
+    }
+
+    [Fact]
+    public static void AllowByRefLike_Substituted_For_NonByRefLike_Invalid_Class()
+    {
+        Console.WriteLine($"{nameof(AllowByRefLike_Substituted_For_NonByRefLike_Invalid_Class)}...");
+
+        Assert.Throws<TypeLoadException>(() => { Exec.TypeSubstitutionInheritanceAllowByRefLikeIntoNonByRefLike(); });
     }
 }
