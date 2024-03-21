3be291127cebc01cce6a2cc0ff3cc77fc6bc3ffb
Jeremy Koritzinsky <jekoritz@microsoft.com>
2024-03-21 13:58:50 -0700

    ifdef out the types in corelib.h for non-x86

--- src/coreclr/vm/metasig.h
+++ src/coreclr/vm/metasig.h
@@ -590,7 +590,9 @@ DEFINE_METASIG_T(SM(RefCleanupWorkListElement_Obj_RetVoid, r(C(CLEANUP_WORK_LIST
 
 DEFINE_METASIG(SM(PtrVoid_RetPtrVoid, P(v), P(v)))
 DEFINE_METASIG(IM(PtrVoid_RetVoid, P(v), v))
+#ifdef TARGET_X86
 DEFINE_METASIG_T(IM(PtrCopyConstructorCookie_RetVoid, P(g(COPY_CONSTRUCTOR_COOKIE)), v))
+#endif // TARGET_X86
 
 
 #ifdef FEATURE_ICASTABLE
