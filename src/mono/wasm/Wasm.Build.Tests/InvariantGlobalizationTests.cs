2f9b9ecc60ef6e34711194cc8f2083ef4ff3a4d1
Ilona Tomkowicz <32700855+ilonatommy@users.noreply.github.com>
2024-03-22 16:34:58 +0000

    `IsWorkloadWithMultiThreadingForDefaultFramework` was true even for ST, change to `FEATURE_WASM_MANAGED_THREADS`.

--- src/mono/wasm/Wasm.Build.Tests/InvariantGlobalizationTests.cs
+++ src/mono/wasm/Wasm.Build.Tests/InvariantGlobalizationTests.cs
@@ -69,10 +69,13 @@ namespace Wasm.Build.Tests
             }
             else
             {
-                string nativeName = IsWorkloadWithMultiThreadingForDefaultFramework ? "de-DE" : "Deutsch (Deutschland)"; // MT does not use JS to get the full name
                 string output = RunAndTestWasmApp(buildArgs, expectedExitCode: 42, host: host, id: id);
                 Assert.Contains("de-DE: Is Invariant LCID: False", output);
-                Assert.Contains($"NativeName: {nativeName}", output);
+#if FEATURE_WASM_MANAGED_THREADS
+                Assert.Contains("NativeName: de-DE", output); // MT does not use JS to get the full name
+#else
+                Assert.Contains("NativeName: Deutsch (Deutschland)", output);
+#endif
 
                 // ignoring the last line of the output which prints the current culture
             }
