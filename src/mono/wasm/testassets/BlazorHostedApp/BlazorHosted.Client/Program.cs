8b550be1df066309296f17b0c979531953e28ceb
Ilona Tomkowicz <32700855+ilonatommy@users.noreply.github.com>
2024-03-22 11:00:03 +0000

    Even more logging.

--- src/mono/wasm/testassets/BlazorHostedApp/BlazorHosted.Client/Program.cs
+++ src/mono/wasm/testassets/BlazorHostedApp/BlazorHosted.Client/Program.cs
@@ -5,9 +5,13 @@ using BlazorHosted.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+Console.WriteLine($"Program.cs start");
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Logging.SetMinimumLevel(LogLevel.Debug);
+
+Console.WriteLine($"Program.cs builder created");
 
 await builder.Build().RunAsync().ConfigureAwait(false);
