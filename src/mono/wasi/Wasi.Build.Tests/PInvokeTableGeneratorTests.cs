2d8780fae747073ab68e1ef8cbc3f876ef5615a4
Meri Khamoyan <merikhamoyan@microsoft.com>
2024-03-28 00:01:11 +0400

    Fix PInvokeTableGeneratorTests

--- src/mono/wasi/Wasi.Build.Tests/PInvokeTableGeneratorTests.cs
+++ src/mono/wasi/Wasi.Build.Tests/PInvokeTableGeneratorTests.cs
@@ -17,33 +17,60 @@ public class PInvokeTableGeneratorTests : BuildTestBase
     {
     }
 
-    [Theory]
+    [Fact]
     public void InteropSupportForUnmanagedEntryPointWithoutDelegate()
     {
         string config = "Release";
         string id = $"{config}_{GetRandomId()}";
-        string projectFile = CreateWasmTemplateProject(id, "wasinative");
-        string projectName = Path.GetFileNameWithoutExtension(projectFile);
+        string projectFile = CreateWasmTemplateProject(id, "wasiconsole");
+        string code =
+                """
+                using System;
+                using System.Runtime.InteropServices;
+                public unsafe class Test
+                {
+                    [UnmanagedCallersOnly(EntryPoint = "ManagedFunc")]
+                    public static int MyExport(int number)
+                    {
+                        // called from MyImport aka UnmanagedFunc
+                        Console.WriteLine($"MyExport({number}) -> 42");
+                        return 42;
+                    }
 
-        File.Delete(Path.Combine(_projectDir!, "runtimeconfig.template.json"));
+                    [DllImport("*", EntryPoint = "UnmanagedFunc")]
+                    public static extern void MyImport(); // calls ManagedFunc aka MyExport
+
+                    public unsafe static int Main(string[] args)
+                    {
+                        Console.WriteLine($"main: {args.Length}");
+                        MyImport();
+                        return 0;
+                    }
+                }
+                """;
+
+        string projectName = Path.GetFileNameWithoutExtension(projectFile);
 
         var buildArgs = new BuildArgs(projectName, config, AOT: true, ProjectFileContents: id, ExtraBuildArgs: null);
         buildArgs = ExpandBuildArgs(buildArgs);
-        AddItemsPropertiesToProject(projectFile, "<NativeFileReference Include=\"local.c\" />");
+        AddItemsPropertiesToProject("<NativeFileReference Include=\"local.c\" />");
+        AddItemsPropertiesToProject(projectFile, "<WasmSingleFileBundle>true</WasmSingleFileBundle>");
 
         BuildProject(buildArgs,
                     id: id,
                     new BuildProjectOptions(
-                        DotnetWasmFromRuntimePack: true,
-                        CreateProject: false,
+                        InitProject: () =>
+                        {
+                            File.WriteAllText(Path.Combine(_projectDir!, "Program.cs"), code);
+                        },
+                        DotnetWasmFromRuntimePack: false,
                         Publish: true,
-                        TargetFramework: BuildTestBase.DefaultTargetFramework,
-                        UseCache: false));
+                        TargetFramework: BuildTestBase.DefaultTargetFramework));
 
         CommandResult res = new RunCommand(s_buildEnv, _testOutput)
                                     .WithWorkingDirectory(_projectDir!)
-                                    .ExecuteWithCapturedOutput($"run --no-silent --no-build -c {config} x y z")
+                                    .ExecuteWithCapturedOutput($"run --no-silent --no-build -c {config}")
                                     .EnsureSuccessful();
-        Assert.Contains("MyExport(123) -> 42", res.output);
+        Assert.Contains("MyExport(123) -> 42", res.Output);
     }
 }
