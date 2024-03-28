a3d86227414bd804cab689060005c9d75ff16546
Katelyn Gadd <kg@luminance.org>
2024-03-28 10:59:07 -0700

    Checkpoint

--- src/native/containers/dn-simdhash-specialization.h
+++ src/native/containers/dn-simdhash-specialization.h
@@ -68,17 +68,19 @@ static_assert(sizeof(DN_SIMDHASH_VALUE_T) == sizeof(void *), "You said your valu
 #define DEREF_VALUE(P) *P
 #endif
 
+// Gluing macro expansions together requires nested macro invocation :/
+#define _GLUE(a,b) a ## b
+
 // We generate unique names for each specialization so that they will be easy to distinguish
 //  when debugging, profiling, or disassembling. Otherwise they would have linker-assigned names
-#define DN_SIMDHASH_BUCKET_T DN_SIMDHASH_T ## _bucket
-#define DN_SIMDHASH_T_VTABLE DN_SIMDHASH_T ## _vtable
-#define DN_SIMDHASH_T_META DN_SIMDHASH_T ## _meta
-#define DN_SIMDHASH_SCAN_BUCKET_INTERNAL DN_SIMDHASH_T ## _scan_bucket_internal
-#define DN_SIMDHASH_FIND_VALUE_INTERNAL DN_SIMDHASH_T ## _find_value_internal
-#define DN_SIMDHASH_TRY_INSERT_INTERNAL DN_SIMDHASH_T ## _try_insert_internal
-#define DN_SIMDHASH_REHASH_INTERNAL DN_SIMDHASH_T ## _rehash_internal
-#define DN_SIMDHASH_COMPUTE_HASH_INTERNAL DN_SIMDHASH_T ## _compute_hash_internal
-#define DN_SIMDHASH_NEW DN_SIMDHASH_T ## _new
+#define DN_SIMDHASH_T_VTABLE(t) _GLUE(t,_vtable)
+#define DN_SIMDHASH_T_META(t) _GLUE(t,_meta)
+#define DN_SIMDHASH_SCAN_BUCKET_INTERNAL(t) _GLUE(t,_scan_bucket_internal)
+#define DN_SIMDHASH_FIND_VALUE_INTERNAL(t) _GLUE(t,_find_value_internal)
+#define DN_SIMDHASH_TRY_INSERT_INTERNAL(t) _GLUE(t,_try_insert_internal)
+#define DN_SIMDHASH_REHASH_INTERNAL(t) _GLUE(t,_rehash_internal)
+#define DN_SIMDHASH_COMPUTE_HASH_INTERNAL(t) _GLUE(t,_compute_hash_internal)
+#define DN_SIMDHASH_NEW(t) _GLUE(t,_new)
 
 static_assert (DN_SIMDHASH_BUCKET_CAPACITY < DN_SIMDHASH_MAX_BUCKET_CAPACITY, "Maximum bucket capacity exceeded");
 static_assert (DN_SIMDHASH_BUCKET_CAPACITY > 1, "Bucket capacity too low");
@@ -86,15 +88,15 @@ static_assert (DN_SIMDHASH_BUCKET_CAPACITY > 1, "Bucket capacity too low");
 // We set bucket_size_bytes to sizeof() this struct so that we can let the compiler
 //  generate the most optimal code possible when we're manipulating pointers to it -
 //  that is, it can do mul-by-constant instead of mul-by-(hash->meta.etc)
-typedef struct DN_SIMDHASH_BUCKET_T {
+typedef struct bucket {
     dn_simdhash_suffixes suffixes;
     DN_SIMDHASH_KEY_T keys[DN_SIMDHASH_BUCKET_CAPACITY];
-} DN_SIMDHASH_BUCKET_T;
+} bucket;
 
-static DN_FORCEINLINE(DN_SIMDHASH_BUCKET_T *)
+static DN_FORCEINLINE(bucket *)
 address_of_bucket (dn_simdhash_buffers_t buffers, uint32_t bucket_index)
 {
-    return &((DN_SIMDHASH_BUCKET_T *)buffers.buckets)[bucket_index];
+    return &((bucket *)buffers.buckets)[bucket_index];
 }
 
 static DN_FORCEINLINE(DN_SIMDHASH_VALUE_T *)
@@ -104,14 +106,14 @@ address_of_value (dn_simdhash_buffers_t buffers, uint32_t value_slot_index)
 }
 
 static uint32_t
-DN_SIMDHASH_COMPUTE_HASH_INTERNAL (KEY_REF key_ptr)
+DN_SIMDHASH_COMPUTE_HASH_INTERNAL(DN_SIMDHASH_T) (KEY_REF key_ptr)
 {
     return DN_SIMDHASH_KEY_HASHER(DEREF_KEY(key_ptr));
 }
 
 // This is split out into a helper so we can eventually reuse it for more efficient add/remove
 static DN_FORCEINLINE(int)
-DN_SIMDHASH_SCAN_BUCKET_INTERNAL (DN_SIMDHASH_BUCKET_T *bucket, DN_SIMDHASH_KEY_T needle, dn_simdhash_suffixes search_vector)
+DN_SIMDHASH_SCAN_BUCKET_INTERNAL(DN_SIMDHASH_T) (bucket *bucket, DN_SIMDHASH_KEY_T needle, dn_simdhash_suffixes search_vector)
 {
     dn_simdhash_suffixes suffixes = bucket->suffixes;
     int index = dn_simdhash_find_first_matching_suffix (search_vector, suffixes);
@@ -126,17 +128,17 @@ DN_SIMDHASH_SCAN_BUCKET_INTERNAL (DN_SIMDHASH_BUCKET_T *bucket, DN_SIMDHASH_KEY_
 }
 
 static DN_SIMDHASH_VALUE_T *
-DN_SIMDHASH_FIND_VALUE_INTERNAL (dn_simdhash_t *hash, KEY_REF key_ptr, uint32_t key_hash)
+DN_SIMDHASH_FIND_VALUE_INTERNAL(DN_SIMDHASH_T) (dn_simdhash_t *hash, KEY_REF key_ptr, uint32_t key_hash)
 {
     dn_simdhash_buffers_t buffers = hash->buffers;
     uint8_t suffix = dn_simdhash_select_suffix(key_hash);
     uint32_t bucket_index = dn_simdhash_select_bucket_index(buffers, key_hash);
-    DN_SIMDHASH_BUCKET_T *bucket_address = address_of_bucket(buffers, bucket_index);
+    bucket *bucket_address = address_of_bucket(buffers, bucket_index);
     DN_SIMDHASH_KEY_T key = DEREF_KEY(key_ptr);
     dn_simdhash_suffixes search_vector = dn_simdhash_build_search_vector(suffix);
 
     for (uint32_t c = buffers.buckets_length; bucket_index < c; bucket_index++, bucket_address++) {
-        int index_in_bucket = DN_SIMDHASH_SCAN_BUCKET_INTERNAL(bucket_address, key, search_vector);
+        int index_in_bucket = DN_SIMDHASH_SCAN_BUCKET_INTERNAL(DN_SIMDHASH_T)(bucket_address, key, search_vector);
         if (index_in_bucket >= 0) {
             uint32_t value_slot_index = (bucket_index * DN_SIMDHASH_BUCKET_CAPACITY) + index_in_bucket;
             return address_of_value(buffers, value_slot_index);
@@ -150,7 +152,7 @@ DN_SIMDHASH_FIND_VALUE_INTERNAL (dn_simdhash_t *hash, KEY_REF key_ptr, uint32_t 
 }
 
 static dn_simdhash_insert_result
-DN_SIMDHASH_TRY_INSERT_INTERNAL (dn_simdhash_t *hash, KEY_REF key_ptr, VALUE_REF value_ptr, uint32_t key_hash, uint8_t ensure_not_present)
+DN_SIMDHASH_TRY_INSERT_INTERNAL(DN_SIMDHASH_T) (dn_simdhash_t *hash, KEY_REF key_ptr, VALUE_REF value_ptr, uint32_t key_hash, uint8_t ensure_not_present)
 {
     // HACK: Early out. Better to grow without scanning here.
     if (hash->count >= hash->buffers.values_length)
@@ -159,12 +161,12 @@ DN_SIMDHASH_TRY_INSERT_INTERNAL (dn_simdhash_t *hash, KEY_REF key_ptr, VALUE_REF
     // TODO: Optimize this to do a single scan that either locates an existing item or chooses
     //  a slot for the new item
     if (ensure_not_present)
-        if (DN_SIMDHASH_FIND_VALUE_INTERNAL(hash, key_ptr, key_hash))
+        if (DN_SIMDHASH_FIND_VALUE_INTERNAL(DN_SIMDHASH_T)(hash, key_ptr, key_hash))
             return DN_SIMDHASH_INSERT_KEY_ALREADY_PRESENT;
 
     uint8_t suffix = dn_simdhash_select_suffix(key_hash);
     uint32_t bucket_index = dn_simdhash_select_bucket_index(hash->buffers, key_hash);
-    DN_SIMDHASH_BUCKET_T *bucket_address = address_of_bucket(hash->buffers, bucket_index);
+    bucket *bucket_address = address_of_bucket(hash->buffers, bucket_index);
 
     for (uint32_t c = hash->buffers.buckets_length; bucket_index < c; bucket_index++, bucket_address++) {
         uint32_t new_index = dn_simdhash_bucket_count (bucket_address->suffixes);
@@ -190,9 +192,9 @@ DN_SIMDHASH_TRY_INSERT_INTERNAL (dn_simdhash_t *hash, KEY_REF key_ptr, VALUE_REF
 }
 
 static void
-DN_SIMDHASH_REHASH_INTERNAL (dn_simdhash_t *hash, dn_simdhash_buffers_t old_buffers)
+DN_SIMDHASH_REHASH_INTERNAL(DN_SIMDHASH_T) (dn_simdhash_t *hash, dn_simdhash_buffers_t old_buffers)
 {
-    DN_SIMDHASH_BUCKET_T *bucket_address = address_of_bucket(old_buffers, 0);
+    bucket *bucket_address = address_of_bucket(old_buffers, 0);
     for (
         uint32_t i = 0, bc = old_buffers.buckets_length, value_slot_base = 0;
         i < bc; i++, bucket_address++, value_slot_base += DN_SIMDHASH_BUCKET_CAPACITY
@@ -205,7 +207,7 @@ DN_SIMDHASH_REHASH_INTERNAL (dn_simdhash_t *hash, dn_simdhash_buffers_t old_buff
             // But I'm not sure it's possible in practice, since we just grew the table -
             //  we should have double the previous number of buckets and the items should
             //  be spread out better
-            dn_simdhash_insert_result ok = DN_SIMDHASH_TRY_INSERT_INTERNAL(
+            dn_simdhash_insert_result ok = DN_SIMDHASH_TRY_INSERT_INTERNAL(DN_SIMDHASH_T)(
                 hash, key,
                 REF_VALUE(
                     ((DN_SIMDHASH_VALUE_T *)old_buffers.values)[value_slot_base + j]
@@ -225,21 +227,21 @@ DN_SIMDHASH_REHASH_INTERNAL (dn_simdhash_t *hash, dn_simdhash_buffers_t old_buff
 //  them directly for some reason
 
 // TODO: Store this by-reference instead of inline in the hash?
-dn_simdhash_vtable_t DN_SIMDHASH_T_VTABLE = {
+dn_simdhash_vtable_t DN_SIMDHASH_T_VTABLE(DN_SIMDHASH_T) = {
     // HACK: Cast these fn pointers to (void *), because their signatures
     //  aren't exact matches, but they're compatible.
-    (void *)DN_SIMDHASH_FIND_VALUE_INTERNAL,
-    (void *)DN_SIMDHASH_TRY_INSERT_INTERNAL,
-    (void *)DN_SIMDHASH_REHASH_INTERNAL,
-    (void *)DN_SIMDHASH_COMPUTE_HASH_INTERNAL,
+    (void *)DN_SIMDHASH_FIND_VALUE_INTERNAL(DN_SIMDHASH_T),
+    (void *)DN_SIMDHASH_TRY_INSERT_INTERNAL(DN_SIMDHASH_T),
+    (void *)DN_SIMDHASH_REHASH_INTERNAL(DN_SIMDHASH_T),
+    (void *)DN_SIMDHASH_COMPUTE_HASH_INTERNAL(DN_SIMDHASH_T),
 };
 
 // While we've inlined these constants into the specialized code generated above,
 //  the generic code in dn-simdhash.c needs them, so we put them in this meta header
 //  that lives inside every hash instance. (TODO: Store it by-reference?)
-dn_simdhash_meta_t DN_SIMDHASH_T_META = {
+dn_simdhash_meta_t DN_SIMDHASH_T_META(DN_SIMDHASH_T) = {
     DN_SIMDHASH_BUCKET_CAPACITY,
-    sizeof(DN_SIMDHASH_BUCKET_T),
+    sizeof(bucket),
     sizeof(DN_SIMDHASH_KEY_T),
     sizeof(DN_SIMDHASH_VALUE_T),
     DN_SIMDHASH_KEY_IS_POINTER,
@@ -248,10 +250,10 @@ dn_simdhash_meta_t DN_SIMDHASH_T_META = {
 
 // HACK: We don't expect the caller to pre-declare this in the specialization file
 dn_simdhash_t *
-DN_SIMDHASH_NEW (uint32_t capacity, dn_allocator_t *allocator);
+DN_SIMDHASH_NEW(DN_SIMDHASH_T) (uint32_t capacity, dn_allocator_t *allocator);
 
 dn_simdhash_t *
-DN_SIMDHASH_NEW (uint32_t capacity, dn_allocator_t *allocator)
+DN_SIMDHASH_NEW(DN_SIMDHASH_T) (uint32_t capacity, dn_allocator_t *allocator)
 {
-    return dn_simdhash_new_internal(DN_SIMDHASH_T_META, DN_SIMDHASH_T_VTABLE, capacity, allocator);
+    return dn_simdhash_new_internal(DN_SIMDHASH_T_META(DN_SIMDHASH_T), DN_SIMDHASH_T_VTABLE(DN_SIMDHASH_T), capacity, allocator);
 }
---
+++ src/native/containers/dn-simdhash-test.c
@@ -0,0 +1,96 @@
+#include <assert.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "dn-vector.h"
+#include "dn-simdhash.h"
+
+#define DN_SIMDHASH_T dn_simdhash_size_t_size_t
+#define DN_SIMDHASH_KEY_T size_t
+#define DN_SIMDHASH_VALUE_T size_t
+#define DN_SIMDHASH_KEY_HASHER(key) key
+#define DN_SIMDHASH_KEY_COMPARER(lhs, rhs) (lhs != rhs)
+#define DN_SIMDHASH_KEY_IS_POINTER 1
+#define DN_SIMDHASH_VALUE_IS_POINTER 1
+
+#include "dn-simdhash-specialization.h"
+
+uint8_t tassert (int b, const char *msg) {
+    if (b)
+        return b;
+    printf("%s\n", msg);
+    return 0;
+}
+
+uint8_t tassert1 (int b, size_t v, const char *msg) {
+    if (b)
+        return b;
+    printf("%s (%zd)\n", msg, v);
+    return 0;
+}
+
+uint8_t tasserteq (size_t actual, size_t expected, const char *msg) {
+    if (actual == expected)
+        return 1;
+    printf("%s: expected %zd, got %zd\n", msg, actual, expected);
+    return 0;
+}
+
+int main () {
+    const int c = 4096;
+    dn_simdhash_t *test = dn_simdhash_size_t_size_t_new(0, NULL);
+    dn_vector_t *keys = dn_vector_alloc(sizeof(DN_SIMDHASH_KEY_T)),
+        *values = dn_vector_alloc(sizeof(DN_SIMDHASH_VALUE_T));
+    for (int i = 0; i < c; i++) {
+        DN_SIMDHASH_KEY_T key = rand();
+        dn_vector_push_back(keys, key);
+        DN_SIMDHASH_VALUE_T value = (i * 2) + 1;
+        dn_vector_push_back(values, value);
+    }
+
+    for (int i = 0; i < c; i++) {
+        DN_SIMDHASH_KEY_T key = *dn_vector_at_t(keys, DN_SIMDHASH_KEY_T, i);
+        DN_SIMDHASH_VALUE_T value, expected_value = *dn_vector_at_t(values, DN_SIMDHASH_VALUE_T, i);
+        uint8_t ok = dn_simdhash_try_get_value(test, (void *)key, &value, sizeof(value));
+        if (tassert1(ok, key, "did not find key"))
+            tasserteq(value, expected_value, "value did not match");
+    }
+
+    return 0;
+    /*
+    var test = new SimdDictionary<long, long>();
+    var rng = new Random(1234);
+    int c = 4096, d = 4096 * 5;
+    var keys = new List<long>();
+    for (int i = 0; i < c; i++)
+        keys.Add(rng.NextInt64());
+    for (int i = 0; i < c; i++)
+        test.Add(keys[i], i * 2 + 1);
+
+    for (int j = 0; j < d; j++)
+        for (int i = 0; i < c; i++)
+            if (!test.TryGetValue(keys[i], out _))
+                throw new Exception();
+
+    var keyList = test.Keys.ToArray();
+    var valueList = test.Values.ToArray();
+
+    var copy = new SimdDictionary<long, long>(test);
+    for (int i = 0; i < c; i++)
+        if (!copy.TryGetValue(keys[i], out _))
+            throw new Exception();
+
+    for (int i = 0; i < c; i++)
+        if (!test.Remove(keys[i]))
+            throw new Exception();
+
+    for (int i = 0; i < c; i++)
+        if (test.TryGetValue(keys[i], out _))
+            throw new Exception();
+
+    if (test.Count != 0)
+        throw new Exception();
+    */
+}
