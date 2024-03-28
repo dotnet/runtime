4949c534e700926ea091af3e84d567614cf4d617
Katelyn Gadd <kg@luminance.org>
2024-03-28 15:46:28 -0700

    Prohibit declaring multiple specializations in one .c file since it would cause duplicate includes

--- src/native/containers/dn-simdhash-specialization.h
+++ src/native/containers/dn-simdhash-specialization.h
@@ -1,6 +1,12 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#ifdef __DN_SIMDHASH_SPECIALIZATION_H__
+#error Specialization header already included
+#else
+#define __DN_SIMDHASH_SPECIALIZATION_H__
+#endif
+
 #ifndef __DN_SIMDHASH_H__
 #error Include dn-simdhash.h first
 // HACK: for better language server parsing
