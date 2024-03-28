65862c1309b05802b1757f0b727861086d8fa31b
Katelyn Gadd <kg@luminance.org>
2024-03-28 14:04:23 -0700

    Fix linker problem by defining string comparison and hasher inline

--- src/native/containers/dn-simdhash-string-ptr.c
+++ src/native/containers/dn-simdhash-string-ptr.c
@@ -3,14 +3,31 @@
 
 #include "dn-simdhash.h"
 
-unsigned int g_str_hash (const void * v1);
-int32_t g_str_equal (const void * v1, const void * v2);
+// FIXME: for some reason g_str_hash and g_str_equal don't link properly if we use them here...
+static int32_t
+dn_simdhash_str_equal (const char * v1, const char * v2)
+{
+	return v1 == v2 || strcmp (v1, v2) == 0;
+}
+
+// FIXME: Use a hash function that's fast and avalanches
+static uint32_t
+dn_simdhash_str_hash (const char * v1)
+{
+	uint32_t hash = 0;
+	unsigned char *p = (unsigned char *) v1;
+
+	while (*p++)
+		hash = (hash << 5) - (hash + *p);
+
+	return hash;
+}
 
 #define DN_SIMDHASH_T dn_simdhash_string_ptr
 #define DN_SIMDHASH_KEY_T const char *
 #define DN_SIMDHASH_VALUE_T void *
-#define DN_SIMDHASH_KEY_HASHER g_str_hash
-#define DN_SIMDHASH_KEY_COMPARER g_str_equal
+#define DN_SIMDHASH_KEY_HASHER dn_simdhash_str_hash
+#define DN_SIMDHASH_KEY_COMPARER dn_simdhash_str_equal
 #define DN_SIMDHASH_KEY_IS_POINTER 1
 #define DN_SIMDHASH_VALUE_IS_POINTER 1
 
