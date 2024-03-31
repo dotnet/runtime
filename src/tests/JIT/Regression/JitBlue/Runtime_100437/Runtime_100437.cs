e82d780905ec6ff5fa94b72dfac6a7bc1fcd2376
Jan Kotas <jkotas@microsoft.com>
2024-03-31 10:32:10 -0700

    Better test

--- src/tests/JIT/Regression/JitBlue/Runtime_100437/Runtime_100437.cs
+++ src/tests/JIT/Regression/JitBlue/Runtime_100437/Runtime_100437.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.Loader;
 using Xunit;
@@ -9,79 +10,93 @@ using Xunit;
 public class Runtime_100437
 {
     [Fact]
-    public static int TestCollectibleEmptyArrayNotInFrozenHeap()
-    {
-        string assemblyPath = typeof(Runtime_100437).Assembly.Location;
-
-        // Skip this test for single file
-        if (string.IsNullOrEmpty(assemblyPath))
-            return 100;
-
-        WeakReference[] wrs = new WeakReference[10];
-        for (int i = 0; i < wrs.Length; i++)
-        {
-            var alc = new MyAssemblyLoadContext();
-            var a = alc.LoadFromAssemblyPath(assemblyPath);
-            wrs[i] = (WeakReference)a.GetType(nameof(Runtime_100437)).GetMethod(nameof(Work)).Invoke(null, null);
-            GC.Collect();
-        }
-
-        int result = 0;
-        foreach (var wr in wrs)
-        {
-            // This is testing that the empty array from Work(), if collected, should not have been allocated on the Frozen heap
-            // otherwise it will result in a random crash.
-            result += wr.Target?.ToString()?.GetHashCode() ?? 0;
-        }
-        return (result & 100) | 100;
-    }
+    public static void TestNonCollectibleType() => TestCollectibleReadOnlyStatics(nameof(NonCollectibleType));
 
     [Fact]
-    public static int TestCollectibleReadOnlyStaticWithGeneric()
+    public static void TestNonCollectibleTypeInSharedGenericCode() => TestCollectibleReadOnlyStatics(nameof(NonCollectibleTypeInSharedGenericCode));
+
+    [Fact]
+    public static void TestNonCollectibleArrayTypeInSharedGenericCode() => TestCollectibleReadOnlyStatics(nameof(NonCollectibleArrayTypeInSharedGenericCode));
+
+    [Fact]
+    public static void TestCollectibleEmptyArray() => TestCollectibleReadOnlyStatics(nameof(CollectibleEmptyArray));
+
+    private static void TestCollectibleReadOnlyStatics(string methodName)
     {
         string assemblyPath = typeof(Runtime_100437).Assembly.Location;
 
         // Skip this test for single file
         if (string.IsNullOrEmpty(assemblyPath))
-            return 100;
+            return;
 
-        WeakReference[] wrs = new WeakReference[10];
-        for (int i = 0; i < wrs.Length; i++)
+        WeakReference wr = CreateReadOnlyStaticWeakReference();
+
+        for (int i = 0; i < 10; i++)
         {
-            var alc = new MyAssemblyLoadContext();
-            var a = alc.LoadFromAssemblyPath(assemblyPath);
-            wrs[i] = (WeakReference)a.GetType(nameof(Runtime_100437)).GetMethod(nameof(WorkForGenericCase)).Invoke(null, null);
             GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            if (wr.Target == null)
+                return;
         }
 
-        int result = 0;
-        foreach (var wr in wrs)
+        throw new Exception("Test failed - readonly static has not been collected.");
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        WeakReference CreateReadOnlyStaticWeakReference()
         {
-            result += wr.Target?.ToString()?.GetHashCode() ?? 0;
+            AssemblyLoadContext alc = new CollectibleAssemblyLoadContext();
+            Assembly a = alc.LoadFromAssemblyPath(assemblyPath);
+            return (WeakReference)a.GetType(nameof(Runtime_100437)).GetMethod(methodName).Invoke(null, new object[] { typeof(Runtime_100437).Assembly });
         }
-        return (result & 100) | 100;
     }
 
-    public static WeakReference Work()
+    public static WeakReference NonCollectibleType(Assembly assemblyInDefaultContext)
     {
-        return new WeakReference(Array.Empty<Runtime_100437>());
+        return new WeakReference(Holder.Singleton, trackResurrection: true);
     }
 
-    public static WeakReference WorkForGenericCase()
+    public static WeakReference NonCollectibleTypeInSharedGenericCode(Assembly assemblyInDefaultContext)
     {
-        return new WeakReference(GenericHolder<Runtime_100437>.Singleton);
+        // Create instance of a non-collectible generic type definition over a collectible type
+        var type = assemblyInDefaultContext.GetType("Runtime_100437+GenericHolder`1", throwOnError: true).MakeGenericType(typeof(Runtime_100437));
+        var field = type.GetField("Singleton", BindingFlags.Static | BindingFlags.Public);
+        return new WeakReference(field.GetValue(null), trackResurrection: true);
     }
 
-    private class MyAssemblyLoadContext : AssemblyLoadContext
+    public static WeakReference NonCollectibleArrayTypeInSharedGenericCode(Assembly assemblyInDefaultContext)
     {
-        public MyAssemblyLoadContext()
+        // Create instance of a non-collectible generic type definition over a collectible type
+        var type = assemblyInDefaultContext.GetType("Runtime_100437+GenericArrayHolder`1", throwOnError: true).MakeGenericType(typeof(Runtime_100437));
+        var field = type.GetField("Singleton", BindingFlags.Static | BindingFlags.Public);
+        return new WeakReference(field.GetValue(null), trackResurrection: true);
+    }
+
+    public static WeakReference CollectibleEmptyArray(Assembly assemblyInDefaultContext)
+    {
+        return new WeakReference(Array.Empty<Runtime_100437>(), trackResurrection: true);
+    }
+
+    private class CollectibleAssemblyLoadContext : AssemblyLoadContext
+    {
+        public CollectibleAssemblyLoadContext()
             : base(isCollectible: true)
         {
         }
     }
 
+    private class Holder
+    {
+        public static readonly object Singleton = new object();
+    }
+
     private class GenericHolder<T>
     {
         public static readonly object Singleton = new object();
+    }
+
+    private class GenericArrayHolder<T>
+    {
+        public static readonly int[] Singleton = new int[0];
     }
 }
