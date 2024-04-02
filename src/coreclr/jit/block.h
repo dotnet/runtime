941c575d525972b17753e60a5ae5b29af250da5e
SingleAccretion <AccretionMail@yandex.ru>
2024-04-02 22:05:11 +0300

    Delete bbFPinVars

--- src/coreclr/jit/block.h
+++ src/coreclr/jit/block.h
@@ -1535,10 +1535,7 @@ public:
 #define handlerGetsXcptnObj(hndTyp) ((hndTyp) != BBCT_NONE && (hndTyp) != BBCT_FAULT && (hndTyp) != BBCT_FINALLY)
 
     // TODO-Cleanup: Get rid of bbStkDepth and use bbStackDepthOnEntry() instead
-    union {
-        unsigned short bbStkDepth; // stack depth on entry
-        unsigned short bbFPinVars; // number of inner enregistered FP vars
-    };
+    unsigned short bbStkDepth; // stack depth on entry
 
     // Basic block predecessor lists. Predecessor lists are created by fgLinkBasicBlocks(), stored
     // in 'bbPreds', and then maintained throughout compilation. 'fgPredsComputed' will be 'true' after the
