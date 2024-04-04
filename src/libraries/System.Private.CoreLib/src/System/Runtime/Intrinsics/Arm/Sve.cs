20defbdb6d53c28cb51e5a15206f6925dbc8f03b
Kunal Pathak <Kunal.Pathak@microsoft.com>
2024-04-04 01:28:57 -0700

    Add ConditionalSelect() APIs

--- src/libraries/System.Private.CoreLib/src/System/Runtime/Intrinsics/Arm/Sve.cs
+++ src/libraries/System.Private.CoreLib/src/System/Runtime/Intrinsics/Arm/Sve.cs
@@ -118,7 +118,93 @@ namespace System.Runtime.Intrinsics.Arm
         /// </summary>
         public static unsafe Vector<ulong> CreateTrueMaskUInt64([ConstantExpected] SveMaskPattern pattern = SveMaskPattern.All) => CreateTrueMaskUInt64(pattern);
 
+        ///  ConditionalSelect : Conditionally select elements
 
+        /// <summary>
+        /// svint8_t svsel[_s8](svbool_t pg, svint8_t op1, svint8_t op2)
+        ///   SEL Zresult.B, Pg, Zop1.B, Zop2.B
+        /// svbool_t svsel[_b](svbool_t pg, svbool_t op1, svbool_t op2)
+        ///   SEL Presult.B, Pg, Pop1.B, Pop2.B
+        ///
+        /// </summary>
+        public static unsafe Vector<sbyte> ConditionalSelect(Vector<sbyte> mask, Vector<sbyte> left, Vector<sbyte> right) => ConditionalSelect(mask, left, right);
+
+        /// <summary>
+        /// svint16_t svsel[_s16](svbool_t pg, svint16_t op1, svint16_t op2)
+        ///   SEL Zresult.H, Pg, Zop1.H, Zop2.H
+        /// svbool_t svsel[_b](svbool_t pg, svbool_t op1, svbool_t op2)
+        ///   SEL Presult.B, Pg, Pop1.B, Pop2.B
+        ///
+        /// </summary>
+        public static unsafe Vector<short> ConditionalSelect(Vector<short> mask, Vector<short> left, Vector<short> right) => ConditionalSelect(mask, left, right);
+
+        /// <summary>
+        /// svint32_t svsel[_s32](svbool_t pg, svint32_t op1, svint32_t op2)
+        ///   SEL Zresult.S, Pg, Zop1.S, Zop2.S
+        /// svbool_t svsel[_b](svbool_t pg, svbool_t op1, svbool_t op2)
+        ///   SEL Presult.B, Pg, Pop1.B, Pop2.B
+        ///
+        /// </summary>
+        public static unsafe Vector<int> ConditionalSelect(Vector<int> mask, Vector<int> left, Vector<int> right) => ConditionalSelect(mask, left, right);
+
+        /// <summary>
+        /// svint64_t svsel[_s64](svbool_t pg, svint64_t op1, svint64_t op2)
+        ///   SEL Zresult.D, Pg, Zop1.D, Zop2.D
+        /// svbool_t svsel[_b](svbool_t pg, svbool_t op1, svbool_t op2)
+        ///   SEL Presult.B, Pg, Pop1.B, Pop2.B
+        ///
+        /// </summary>
+        public static unsafe Vector<long> ConditionalSelect(Vector<long> mask, Vector<long> left, Vector<long> right) => ConditionalSelect(mask, left, right);
+
+        /// <summary>
+        /// svuint8_t svsel[_u8](svbool_t pg, svuint8_t op1, svuint8_t op2)
+        ///   SEL Zresult.B, Pg, Zop1.B, Zop2.B
+        /// svbool_t svsel[_b](svbool_t pg, svbool_t op1, svbool_t op2)
+        ///   SEL Presult.B, Pg, Pop1.B, Pop2.B
+        ///
+        /// </summary>
+        public static unsafe Vector<byte> ConditionalSelect(Vector<byte> mask, Vector<byte> left, Vector<byte> right) => ConditionalSelect(mask, left, right);
+
+        /// <summary>
+        /// svuint16_t svsel[_u16](svbool_t pg, svuint16_t op1, svuint16_t op2)
+        ///   SEL Zresult.H, Pg, Zop1.H, Zop2.H
+        /// svbool_t svsel[_b](svbool_t pg, svbool_t op1, svbool_t op2)
+        ///   SEL Presult.B, Pg, Pop1.B, Pop2.B
+        ///
+        /// </summary>
+        public static unsafe Vector<ushort> ConditionalSelect(Vector<ushort> mask, Vector<ushort> left, Vector<ushort> right) => ConditionalSelect(mask, left, right);
+
+        /// <summary>
+        /// svuint32_t svsel[_u32](svbool_t pg, svuint32_t op1, svuint32_t op2)
+        ///   SEL Zresult.S, Pg, Zop1.S, Zop2.S
+        /// svbool_t svsel[_b](svbool_t pg, svbool_t op1, svbool_t op2)
+        ///   SEL Presult.B, Pg, Pop1.B, Pop2.B
+        ///
+        /// </summary>
+        public static unsafe Vector<uint> ConditionalSelect(Vector<uint> mask, Vector<uint> left, Vector<uint> right) => ConditionalSelect(mask, left, right);
+
+        /// <summary>
+        /// svuint64_t svsel[_u64](svbool_t pg, svuint64_t op1, svuint64_t op2)
+        ///   SEL Zresult.D, Pg, Zop1.D, Zop2.D
+        /// svbool_t svsel[_b](svbool_t pg, svbool_t op1, svbool_t op2)
+        ///   SEL Presult.B, Pg, Pop1.B, Pop2.B
+        ///
+        /// </summary>
+        public static unsafe Vector<ulong> ConditionalSelect(Vector<ulong> mask, Vector<ulong> left, Vector<ulong> right) => ConditionalSelect(mask, left, right);
+
+        /// <summary>
+        /// svfloat32_t svsel[_f32](svbool_t pg, svfloat32_t op1, svfloat32_t op2)
+        ///   SEL Zresult.S, Pg, Zop1.S, Zop2.S
+        ///
+        /// </summary>
+        public static unsafe Vector<float> ConditionalSelect(Vector<float> mask, Vector<float> left, Vector<float> right) => ConditionalSelect(mask, left, right);
+
+        /// <summary>
+        /// svfloat64_t svsel[_f64](svbool_t pg, svfloat64_t op1, svfloat64_t op2)
+        ///   SEL Zresult.D, Pg, Zop1.D, Zop2.D
+        ///
+        /// </summary>
+        public static unsafe Vector<double> ConditionalSelect(Vector<double> mask, Vector<double> left, Vector<double> right) => ConditionalSelect(mask, left, right);
 
         ///  LoadVector : Unextended load
 
