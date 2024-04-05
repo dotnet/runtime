008681891f2fd9327afe0d12820800cc9a3fe626
Omair Majid <omajid@redhat.com>
2024-04-05 09:41:25 -0400

    Do not merge: Remove hardcoded list of compiler versions in init-compiler.sh
    This commit is just to exercise CI for https://github.com/dotnet/arcade/pull/14696

--- eng/common/native/init-compiler.sh
+++ eng/common/native/init-compiler.sh
@@ -62,51 +62,34 @@ if [ -z "$CLR_CC" ]; then
 
     # Set default versions
     if [ -z "$majorVersion" ]; then
-        # note: gcc (all versions) and clang versions higher than 6 do not have minor version in file name, if it is zero.
-        if [ "$compiler" = "clang" ]; then versions="18 17 16 15 14 13 12 11 10 9 8 7 6.0 5.0 4.0 3.9 3.8 3.7 3.6 3.5"
-        elif [ "$compiler" = "gcc" ]; then versions="13 12 11 10 9 8 7 6 5 4.9"; fi
+        if ! command -v "$compiler" > /dev/null; then
+            echo "No usable version of $compiler found."
+            exit 1
+        fi
 
-        for version in $versions; do
-            _major="${version%%.*}"
-            [ -z "${version##*.*}" ] && _minor="${version#*.}"
-            desired_version="$(check_version_exists "$_major" "$_minor")"
-            if [ "$desired_version" != "-1" ]; then majorVersion="$_major"; break; fi
-        done
+        CC="$(command -v "$compiler")"
+        CXX="$(command -v "$cxxCompiler")"
 
-        if [ -z "$majorVersion" ]; then
-            if command -v "$compiler" > /dev/null; then
-                if [ "$(uname)" != "Darwin" ]; then
-                    echo "Warning: Specific version of $compiler not found, falling back to use the one in PATH."
-                fi
-                CC="$(command -v "$compiler")"
-                CXX="$(command -v "$cxxCompiler")"
-            else
-                echo "No usable version of $compiler found."
+        version="$("$CC" -dumpversion)"
+        # gcc and clang often display 3 part versions. However, gcc can show only 1 part in some environments.
+        IFS=. read -r majorVersion minorVersion patchVersion <<EOF
+$version
+EOF
+        unset patchVersion
+
+        if [ "$compiler" = "clang" ] && [ "$majorVersion" -lt 5 ]; then
+            if [ "$build_arch" = "arm" ] || [ "$build_arch" = "armel" ]; then
+                echo "Found clang version $majorVersion which is not supported on arm/armel architectures."
                 exit 1
             fi
-        else
-            if [ "$compiler" = "clang" ] && [ "$majorVersion" -lt 5 ]; then
-                if [ "$build_arch" = "arm" ] || [ "$build_arch" = "armel" ]; then
-                    if command -v "$compiler" > /dev/null; then
-                        echo "Warning: Found clang version $majorVersion which is not supported on arm/armel architectures, falling back to use clang from PATH."
-                        CC="$(command -v "$compiler")"
-                        CXX="$(command -v "$cxxCompiler")"
-                    else
-                        echo "Found clang version $majorVersion which is not supported on arm/armel architectures, and there is no clang in PATH."
-                        exit 1
-                    fi
-                fi
-            fi
         fi
+
     else
         desired_version="$(check_version_exists "$majorVersion" "$minorVersion")"
         if [ "$desired_version" = "-1" ]; then
             echo "Could not find specific version of $compiler: $majorVersion $minorVersion."
             exit 1
         fi
-    fi
-
-    if [ -z "$CC" ]; then
         CC="$(command -v "$compiler$desired_version")"
         CXX="$(command -v "$cxxCompiler$desired_version")"
         if [ -z "$CXX" ]; then CXX="$(command -v "$cxxCompiler")"; fi
