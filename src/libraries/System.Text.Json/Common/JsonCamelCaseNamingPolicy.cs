749d0ee4eb998b5888c4073c83703f80dee00e9e
Yoh Deadfall <yoh.deadfall@hotmail.com>
2024-04-05 18:49:31 +0300

    Marked FixCasing to be always inlined

--- src/libraries/System.Text.Json/Common/JsonCamelCaseNamingPolicy.cs
+++ src/libraries/System.Text.Json/Common/JsonCamelCaseNamingPolicy.cs
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Runtime.CompilerServices;
+
 namespace System.Text.Json
 {
     internal sealed class JsonCamelCaseNamingPolicy : JsonNamingPolicy
@@ -25,6 +27,7 @@ namespace System.Text.Json
 #endif
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static void FixCasing(Span<char> chars)
         {
             for (int i = 0; i < chars.Length; i++)
