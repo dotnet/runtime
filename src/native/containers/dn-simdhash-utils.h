31eb0ba255f23eee591cfa0064438d636da06c2d
Katelyn Gadd <kg@luminance.org>
2024-04-08 14:43:59 -0700

    Update utils header from successor branch

--- src/native/containers/dn-simdhash-utils.h
+++ src/native/containers/dn-simdhash-utils.h
@@ -1,20 +1,25 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#ifndef __DN_SIMDHASH_UTILS_H__
+#define __DN_SIMDHASH_UTILS_H__
+
 #include <stdint.h>
 
 // MurmurHash3 was written by Austin Appleby, and is placed in the public
 // domain. The author hereby disclaims copyright to this source code.
 
+static const uint32_t murmur3_c1 = 0xcc9e2d51, murmur3_c2 = 0x1b873593;
+
 inline static uint32_t
-ROTL32 (uint32_t x, int8_t r)
+murmur3_rotl32 (uint32_t x, int8_t r)
 {
 	return (x << r) | (x >> (32 - r));
 }
 
 // Finalization mix - force all bits of a hash block to avalanche
 inline static uint32_t
-fmix32 (uint32_t h)
+murmur3_fmix32 (uint32_t h)
 {
 	h ^= h >> 16;
 	h *= 0x85ebca6b;
@@ -25,35 +30,52 @@ fmix32 (uint32_t h)
 	return h;
 }
 
-#define BLOCK_COUNT ((sizeof (void *)) / 4)
+inline static uint64_t
+murmur3_fmix64(uint64_t k)
+{
+	k ^= k >> 33;
+	k *= 0xff51afd7ed558ccdLLU;
+	k ^= k >> 33;
+	k *= 0xc4ceb9fe1a85ec53LLU;
+	k ^= k >> 33;
+	return k;
+}
 
-// Hash a void * (number of 4-byte blocks determined by sizeof (void *))
-static uint32_t
+// Convenience macro so you can define your own fixed-size MurmurHashes
+#define MURMUR3_HASH_BLOCK(block) \
+	{ \
+		uint32_t k1 = block; \
+		k1 *= murmur3_c1; \
+		k1 = murmur3_rotl32(k1, 15); \
+		k1 *= murmur3_c2; \
+		h1 ^= k1; \
+		h1 = murmur3_rotl32(h1, 13); \
+		h1 = h1 * 5 + 0xe6546b64; \
+	}
+
+// Hash a void * (either 4 or 8 bytes)
+static inline uint32_t
 MurmurHash3_32_ptr (const void *ptr, uint32_t seed)
 {
-	uint32_t h1 = seed;
-	const uint32_t c1 = 0xcc9e2d51, c2 = 0x1b873593;
-
+	// mono_aligned_addr_hash shifts all incoming pointers by 3 bits to account
+	//  for a presumed 8-byte alignment of addresses (the dmalloc default). However,
+	// I'm not doing this right now because I'm not sure it is necessary w/the
+	//  finalization mixer in place - if anything, it can discard data.
+	const uint32_t alignment_shift = 0;
 	union {
-		uint32_t u32[BLOCK_COUNT];
+		uint32_t u32;
+		uint64_t u64;
 		const void *ptr;
 	} u;
 	u.ptr = ptr;
 
-	for (uint32_t i = 0; i < BLOCK_COUNT; i++) {
-		uint32_t k1 = u.u32[i];
-		k1 *= c1;
-		k1 = ROTL32(k1, 15);
-		k1 *= c2;
-		h1 ^= k1;
-		h1 = ROTL32(h1, 13);
-		h1 = h1 * 5 + 0xe6546b64;
+	// Apply murmurhash3's 32 or 64 bit finalization bit mixer to the pointer,
+	//  then return the result truncated to 32 bits.
+	if (sizeof (void*) == 8) {
+		return (uint32_t)(murmur3_fmix64(u.u64 >> alignment_shift) & 0xFFFFFFFFu);
+	} else {
+		return murmur3_fmix32(u.u32 >> alignment_shift);
 	}
-
-	// finalize
-	h1 ^= BLOCK_COUNT;
-	h1 = fmix32(h1);
-	return h1;
 }
 
 // end of murmurhash
@@ -73,20 +95,20 @@ MurmurHash3_32_ptr (const void *ptr, uint32_t seed)
 // The output of this will probably be different from regular MurmurHash3. I don't
 //  see that as a problem, since you shouldn't rely on the exact bit patterns of
 //  a non-cryptographic hash anyway.
-typedef struct scan_result_t {
+typedef struct murmur3_scan_result_t {
 	union {
 		uint32_t u32;
 		uint8_t bytes[4];
 	} result;
 	const uint8_t *next;
-} scan_result_t;
+} murmur3_scan_result_t;
 
-static inline scan_result_t
-scan_forward (const uint8_t *ptr)
+static inline murmur3_scan_result_t
+murmur3_scan_forward (const uint8_t *ptr)
 {
 	// TODO: On wasm we could do a single u32 load then scan the bytes,
 	//  as long as we're sure ptr isn't up against the end of memory
-	scan_result_t result = { 0, };
+	murmur3_scan_result_t result = { 0, };
 
 	// I tried to get a loop to auto-unroll, but GCC only unrolls at O3 and MSVC never does.
 #define SCAN_1(i) \
@@ -109,26 +131,19 @@ static inline uint32_t
 MurmurHash3_32_streaming (const uint8_t *key, uint32_t seed)
 {
 	uint32_t h1 = seed, block_count = 0;
-	const uint32_t c1 = 0xcc9e2d51, c2 = 0x1b873593;
 
 	// Scan forward through the buffer collecting up to 4 bytes at a time, then hash
-	scan_result_t block = scan_forward(key);
+	murmur3_scan_result_t block = murmur3_scan_forward(key);
 	// As long as the scan found at least one nonzero byte, u32 will be != 0
 	while (block.result.u32) {
 		block_count += 1;
 
-		uint32_t k1 = block.result.u32;
-		k1 *= c1;
-		k1 = ROTL32(k1, 15);
-		k1 *= c2;
-		h1 ^= k1;
-		h1 = ROTL32(h1, 13);
-		h1 = h1 * 5 + 0xe6546b64;
+		MURMUR3_HASH_BLOCK(block.result.u32);
 
 		// If the scan found a null byte next will be 0, so we stop scanning
 		if (!block.next)
 			break;
-		block = scan_forward(block.next);
+		block = murmur3_scan_forward(block.next);
 	}
 
 	// finalize. we don't have an exact byte length but we have a block count
@@ -136,8 +151,13 @@ MurmurHash3_32_streaming (const uint8_t *key, uint32_t seed)
 	//  since then we can compute the length and hash in one go and use memcmp later,
 	// since emscripten/musl strcmp isn't optimized at all
 	h1 ^= block_count;
-	h1 = fmix32(h1);
+	h1 = murmur3_fmix32(h1);
 	return h1;
 }
 
 // end of reformulated murmur3-32
+
+#undef unlikely
+#undef likely
+
+#endif // __DN_SIMDHASH_UTILS_H__
