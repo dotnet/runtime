84eaa3ba33811b079fd690af0b806ba6db960938
Aleksey Kliger <alklig@microsoft.com>
2024-04-08 17:20:18 -0400

    remove fixme; document

--- src/coreclr/debug/runtimeinfo/data-descriptor.h
+++ src/coreclr/debug/runtimeinfo/data-descriptor.h
@@ -1,3 +1,68 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+//
+// No include guards. This file is included multiple times.
+
+// The format is:
+// CDAC_BASELINE("string") baseline data contract that the runtime should follow. "empty" is reasonable
+// CDAC_TYPES_BEGIN()
+// ... <types> ...
+// CDAC_TYPES_END()
+// CDAC_GLOBALS_BEGIN()
+// ... <globals> ...
+// CDAC_GLOBALS_END()
+//
+// In <types> the format is:
+// CDAC_TYPE_BEGIN(cdacTypeIdentifier) // defined a new data descriptor names cdacIdentifier
+//
+// CDAC_TYPE_SIZE(k) -or- CDAC_TYPE_INDETERMINATE(cdacTypeIdentifier) specifies that the type has
+//   size k (bytes - usually sizeof(SomeNativeType)) or specify that the type's size is not provided
+//   It is important that CDAC_TYPE_SIZE or CDAC_TYPE_INDETERMINATE immediately follows
+//   CDAC_TYPE_BEGIN
+//
+// CDAC_TYPE_FIELD(cdacTypeIdentifier, cdacFieldTypeIdentifier, cdacFieldName, k) specifies the
+//   field of "cdacTypeIdentifier" that has name cdacFieldName and has the type
+//   "cdacFieldtypeIdentifier" located at offset k in the type layout.  k is usually
+//   offsetof(SomeClass, m_FieldName) if the field is public
+//
+//     if the field is private, the convention is that SomeClass declares a friend struct
+//     cdac_offsets<T> and provides a specialization of cdac_offsets<T> with a public constexpr
+//     size_t member that holds the offset:
+//
+//     class MyClass {
+//       private:
+//         void* m_myField;
+//         friend template<typename T> cdac_offsets<T>;
+//     };
+//     template<> struct cdac_offsets<MyClass> {
+//       static constexpr size_t MyField = offsetof(MyClass, m_myField);
+//     };
+//
+//     then the field layout can be specified as
+//     CDAC_TYPE_FIELD(MyClassLayout, pointer, MyField, cdac_offsets<MyClass>::MyField)
+//  There can be zero or more CDAC_TYPE_FIELD entries per type layout
+//
+// CDAC_TYPE_END(cdacTypeIdentifier)  specifies the end of the type layout for cdacTypeIdentifier
+//
+// In <globals> the format is:
+//
+// CDAC_GLOBAL(cdacGlobalName, cdacTypeIdentifier, value)
+// or
+// CDAC_GLOBAL_POINTER(cdacGlobalName, cdacTypeIdentifier, address)
+//
+// Zero or more globals can be defined
+//
+// if a global is given with CDAC_GLOBAL(), `value` should be a constexpr uint64_t (or convertible
+// to uint64_t) for example, it can be a literal constant or a preprocessor definition
+//
+// if a global is a CDAC_GLOBAL_POINTER(), address should be a constexpr pointer or a constexpr
+// uintptr_t
+//
+//
+//
+// This file is compiled using the target architecture.  Preprocessor defines for the target
+// platform will be available.  It is ok to use `#ifdef`.
+
 CDAC_BASELINE("empty")
 CDAC_TYPES_BEGIN()
 
@@ -20,5 +85,5 @@ CDAC_GLOBAL(FeatureEHFunclets, uint8, 1)
 #else
 CDAC_GLOBAL(FeatureEHFunclets, uint8, 0)
 #endif
-CDAC_GLOBAL(SomeMagicNumber, uint32, 42)
+CDAC_GLOBAL(SOSBreakingChangeVersion, uint8, SOS_BREAKING_CHANGE_VERSION)
 CDAC_GLOBALS_END()
--- src/coreclr/vm/cdac.h
+++ src/coreclr/vm/cdac.h
@@ -2,6 +2,18 @@
 #ifndef CDAC_H__
 #define CDAC_H__
 
+// See data-descriptor.h
+//
+// If the offset of some field F in class C must be provided to cDAC, but the field is private, the
+// class C should declare cdac_offsets<T> as a friend:
+// 
+//     friend template<typename T> struct cdac_offsets;
+//
+// and provide a specialization cdac_offsets<C> with a constexpr size_t member providing the offset:
+//
+//     template<> struct cdac_offsets<C> {
+//         static constexpr size_t F_Offset = offsetof(C, F);
+//     };
 template<typename T>
 struct cdac_offsets
 {
