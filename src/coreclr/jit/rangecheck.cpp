262c9c3ea3d1788ee4b9f4528dd304654541f6b9
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2024-04-09 16:48:25 +0200

    JIT: Disable range check overflow check
    Just a CI test to see if I can gain some insights into why this check is
    necessary... Seemingly the range computation already accounts for
    overflow.

--- src/coreclr/jit/rangecheck.cpp
+++ src/coreclr/jit/rangecheck.cpp
@@ -353,11 +353,11 @@ void RangeCheck::OptimizeRangeCheck(BasicBlock* block, Statement* stmt, GenTree*
         return;
     }
 
-    if (DoesOverflow(block, treeIndex))
-    {
-        JITDUMP("Method determined to overflow.\n");
-        return;
-    }
+    //if (DoesOverflow(block, treeIndex))
+    //{
+    //    JITDUMP("Method determined to overflow.\n");
+    //    return;
+    //}
 
     JITDUMP("Range value %s\n", range.ToString(m_pCompiler));
     m_pSearchPath->RemoveAll();
