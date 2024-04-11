60ef7ec81b235e12f2e238d92969ad31ac56b548
Andy Ayers <andya@microsoft.com>
2024-04-11 07:29:24 -0700

    JIT: revise how synthesis treats handler regions (#100899)
    Instead of giving hander regions a fraction of the entry weight, give them a small
    fixed weight. This is intended to combat the lack of profile propagation out of
    handler regions, where there are currently sometimes weight discontinuities large
    enough to cause profile check asserts.
    
    Contributes to #93020.

--- src/coreclr/jit/jitconfigvalues.h
+++ src/coreclr/jit/jitconfigvalues.h
@@ -719,9 +719,6 @@ CONFIG_INTEGER(JitRandomlyCollect64BitCounts, W("JitRandomlyCollect64BitCounts")
 // 3: profile synthesis for root methods, blend with existing PGO data
 CONFIG_INTEGER(JitSynthesizeCounts, W("JitSynthesizeCounts"), 0)
 
-// Check if synthesis left consistent counts
-CONFIG_INTEGER(JitCheckSynthesizedCounts, W("JitCheckSynthesizedCounts"), 0)
-
 // If instrumenting the method, run synthesis and save the synthesis results
 // as edge or block profile data. Do not actually instrument.
 CONFIG_INTEGER(JitPropagateSynthesizedCountsToProfileData, W("JitPropagateSynthesizedCountsToProfileData"), 0)
@@ -729,8 +726,8 @@ CONFIG_INTEGER(JitPropagateSynthesizedCountsToProfileData, W("JitPropagateSynthe
 // Use general (gauss-seidel) solver
 CONFIG_INTEGER(JitSynthesisUseSolver, W("JitSynthesisUseSolver"), 1)
 
-// Relative likelihood of exceptions for synthesis
-CONFIG_STRING(JitSynthesisExceptionScale, W("JitSynthesisExceptionScale"))
+// Weight for exception regions for synthesis
+CONFIG_STRING(JitSynthesisExceptionWeight, W("JitSynthesisExceptionWeight"))
 
 // Devirtualize virtual calls with getExactClasses (NativeAOT only for now)
 RELEASE_CONFIG_INTEGER(JitEnableExactDevirtualization, W("JitEnableExactDevirtualization"), 1)
