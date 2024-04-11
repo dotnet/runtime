1edfcdaa7601426ca4fa1c73e4da61c9c36128f2
Jackson Schuster <36744439+jtschuster@users.noreply.github.com>
2024-04-11 14:54:52 -0700

    Make method internal for API compat

--- src/tools/illink/src/linker/Linker/Annotations.cs
+++ src/tools/illink/src/linker/Linker/Annotations.cs
@@ -451,7 +451,7 @@ namespace Mono.Linker
 		/// Returns a list of all known methods that override <paramref name="method"/>.
 		/// The list may be incomplete if other overrides exist in assemblies that haven't been processed by TypeMapInfo yet
 		/// </summary>
-		public List<OverrideInformation>? GetOverrides (MethodDefinition method)
+		internal List<OverrideInformation>? GetOverrides (MethodDefinition method)
 		{
 			return TypeMapInfo.GetOverrides (method);
 		}
