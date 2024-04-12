1ba48a2cf193a00bdc29778f58e4bd54a99700d0
Elinor Fung <elfung@microsoft.com>
2024-04-11 21:00:34 -0700

    Add reading pointer from target

--- src/native/managed/cdacreader/src/Target.cs
+++ src/native/managed/cdacreader/src/Target.cs
@@ -1,7 +1,18 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
+using System.Buffers.Binary;
+
 namespace Microsoft.Diagnostics.DataContractReader;
+
+public struct TargetPointer
+{
+    public static TargetPointer Null = new(0);
+
+    public ulong Value;
+    public TargetPointer(ulong value) => Value = value;
+}
 
 internal sealed unsafe class Target
 {
@@ -20,4 +31,34 @@ internal sealed unsafe class Target
         _isLittleEndian = BitConverter.IsLittleEndian;
         _pointerSize = IntPtr.Size;
     }
+
+    public bool TryReadPointer(ulong address, out TargetPointer pointer)
+    {
+        pointer = TargetPointer.Null;
+
+        byte* buffer = stackalloc byte[_pointerSize];
+        ReadOnlySpan<byte> span = new ReadOnlySpan<byte>(buffer, _pointerSize);
+        if (ReadFromTarget(address, buffer, (uint)_pointerSize) < 0)
+            return false;
+
+        if (_pointerSize == sizeof(uint))
+        {
+            pointer = new TargetPointer(
+                _isLittleEndian
+                    ? BinaryPrimitives.ReadUInt32LittleEndian(span)
+                    : BinaryPrimitives.ReadUInt32BigEndian(span));
+        }
+        else if (_pointerSize == sizeof(ulong))
+        {
+            pointer = new TargetPointer(
+                _isLittleEndian
+                    ? BinaryPrimitives.ReadUInt64LittleEndian(span)
+                    : BinaryPrimitives.ReadUInt64BigEndian(span));
+        }
+
+        return true;
+    }
+
+    private int ReadFromTarget(ulong address, byte* buffer, uint bytesToRead)
+        => _readFromTarget(address, buffer, bytesToRead, _readContext);
 }
