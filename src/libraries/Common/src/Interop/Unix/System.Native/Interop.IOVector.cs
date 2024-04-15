f58107bd4b141bedd350da475e8eeb1bebe05dce
wfurt <tweinfurt@yahoo.com>
2024-04-15 11:22:19 -0700

    feedback

--- src/libraries/Common/src/Interop/Unix/System.Native/Interop.IOVector.cs
+++ src/libraries/Common/src/Interop/Unix/System.Native/Interop.IOVector.cs
@@ -2,17 +2,17 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
+using System.Runtime.InteropServices;
 
 internal static partial class Interop
 {
     internal static partial class Sys
     {
+        [StructLayout(LayoutKind.Sequential)]
         internal unsafe struct IOVector
         {
-#pragma warning disable CS0649
             public byte* Base;
             public UIntPtr Count;
-#pragma warning restore CS0649
         }
     }
 }
