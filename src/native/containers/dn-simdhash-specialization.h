1f25dfa33129d326686f6bd90a4b786347a45deb
Katelyn Gadd <kg@luminance.org>
2024-04-15 12:30:14 -0700

    Remove unnecessary assert

--- src/native/containers/dn-simdhash-specialization.h
+++ src/native/containers/dn-simdhash-specialization.h
@@ -385,14 +385,6 @@ dn_simdhash_vtable_t DN_SIMDHASH_T_VTABLE = {
 DN_SIMDHASH_T_PTR
 DN_SIMDHASH_NEW (uint32_t capacity, dn_allocator_t *allocator)
 {
-	// If this isn't satisfied, the generic code will allocate incorrectly sized buffers
-	// HACK: Use static_assert because for some reason assert produces unused variable warnings only on CI
-	struct silence_nuisance_msvc_warning { bucket_t a, b; };
-	static_assert(
-		sizeof(struct silence_nuisance_msvc_warning) == (sizeof(bucket_t) * 2),
-		"Inconsistent spacing/sizing for bucket_t"
-	);
-
 	return dn_simdhash_new_internal(&DN_SIMDHASH_T_META, DN_SIMDHASH_T_VTABLE, capacity, allocator);
 }
 #endif
