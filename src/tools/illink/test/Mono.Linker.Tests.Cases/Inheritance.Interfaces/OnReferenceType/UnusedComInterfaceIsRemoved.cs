826202067a38eea5c1fee492df5a9814fe1a080f
Sven Boemer <sbomer@gmail.com>
2024-04-16 13:34:24 -0700

    Add option to keep/remove COM interfaces (#101087)
    Adds an illink option that can be used to remove COM
    interfaces (the existing behavior is to preserve COM interfaces
    on marked types). If a type implements a COM interface that is
    also marked elsewhere, it will still be kept even with
    `--keep-com-interfaces false`. This includes windows runtime
    interfaces under the same option name.
    
    This eliminates some trim warnings in winforms due to
    trim-incompatible code in Control's implementations of COM
    interfaces like IPersistPropertyBag. Experimentally, this still
    produces a working app for the scenario we've been testing
    (https://github.com/dotnet/winforms/tree/main/src/System.Windows.Forms/tests/IntegrationTests/TrimTest).
    For now, this just introduces the option without setting it by
    default anywhere (or exposing it in MSBuild) so that we can opt
    in from the winforms scenario. We might consider setting it
    wherever `BuiltInComInteropSupport` is false.

--- src/tools/illink/test/Mono.Linker.Tests.Cases/Inheritance.Interfaces/OnReferenceType/UnusedComInterfaceIsRemoved.cs
+++ src/tools/illink/test/Mono.Linker.Tests.Cases/Inheritance.Interfaces/OnReferenceType/UnusedComInterfaceIsRemoved.cs
@@ -1,13 +1,14 @@
 using System.Runtime.InteropServices;
 using Mono.Linker.Tests.Cases.Expectations.Assertions;
+using Mono.Linker.Tests.Cases.Expectations.Metadata;
 
 namespace Mono.Linker.Tests.Cases.Inheritance.Interfaces.OnReferenceType
 {
 	/// <summary>
-	/// It's much harder to know if a com interface will be needed since so much can be on the native side.
-	/// As a precaution we will not apply the unused interface rules to com interfaces
+	/// With --keep-com-interfaces false, we apply the unused interface  rules also to com interfaces.
 	/// </summary>
-	public class UnusedComInterfaceIsKept
+	[SetupLinkerArgument ("--keep-com-interfaces", "false")]
+	public class UnusedComInterfaceIsRemoved
 	{
 		public static void Main ()
 		{
@@ -15,21 +16,15 @@ namespace Mono.Linker.Tests.Cases.Inheritance.Interfaces.OnReferenceType
 			i.Foo ();
 		}
 
-		[Kept]
-		[KeptAttributeAttribute (typeof (GuidAttribute))]
 		[ComImport]
 		[Guid ("D7BB1889-3AB7-4681-A115-60CA9158FECA")]
 		interface IBar
 		{
-			// Trimming may remove members from COM interfaces
-			// even when keeping the COM-related attributes.
-			// https://github.com/dotnet/runtime/issues/101128
 			void Bar ();
 		}
 
 		[Kept]
 		[KeptMember (".ctor()")]
-		[KeptInterface (typeof (IBar))]
 		class A : IBar
 		{
 			[Kept]
