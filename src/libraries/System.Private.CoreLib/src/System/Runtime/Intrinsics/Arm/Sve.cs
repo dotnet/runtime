6d84dd066f0bfcfcc6d6c468f20c4f6752173aa9
Kunal Pathak <Kunal.Pathak@microsoft.com>
2024-04-25 16:13:57 -0700

    Remove *DIVR and *SUBR from comments

--- src/libraries/System.Private.CoreLib/src/System/Runtime/Intrinsics/Arm/Sve.cs
+++ src/libraries/System.Private.CoreLib/src/System/Runtime/Intrinsics/Arm/Sve.cs
@@ -580,11 +580,9 @@ namespace System.Runtime.Intrinsics.Arm
         ///   MOVPRFX Zresult, Zop1; FDIV Zresult.S, Pg/M, Zresult.S, Zop2.S
         /// svfloat32_t svdiv[_f32]_x(svbool_t pg, svfloat32_t op1, svfloat32_t op2)
         ///   FDIV Ztied1.S, Pg/M, Ztied1.S, Zop2.S
-        ///   FDIVR Ztied2.S, Pg/M, Ztied2.S, Zop1.S
         ///   MOVPRFX Zresult, Zop1; FDIV Zresult.S, Pg/M, Zresult.S, Zop2.S
         /// svfloat32_t svdiv[_f32]_z(svbool_t pg, svfloat32_t op1, svfloat32_t op2)
         ///   MOVPRFX Zresult.S, Pg/Z, Zop1.S; FDIV Zresult.S, Pg/M, Zresult.S, Zop2.S
-        ///   MOVPRFX Zresult.S, Pg/Z, Zop2.S; FDIVR Zresult.S, Pg/M, Zresult.S, Zop1.S
         /// </summary>
         public static unsafe Vector<float> Divide(Vector<float> left, Vector<float> right) => Divide(left, right);
 
@@ -594,11 +592,9 @@ namespace System.Runtime.Intrinsics.Arm
         ///   MOVPRFX Zresult, Zop1; FDIV Zresult.D, Pg/M, Zresult.D, Zop2.D
         /// svfloat64_t svdiv[_f64]_x(svbool_t pg, svfloat64_t op1, svfloat64_t op2)
         ///   FDIV Ztied1.D, Pg/M, Ztied1.D, Zop2.D
-        ///   FDIVR Ztied2.D, Pg/M, Ztied2.D, Zop1.D
         ///   MOVPRFX Zresult, Zop1; FDIV Zresult.D, Pg/M, Zresult.D, Zop2.D
         /// svfloat64_t svdiv[_f64]_z(svbool_t pg, svfloat64_t op1, svfloat64_t op2)
         ///   MOVPRFX Zresult.D, Pg/Z, Zop1.D; FDIV Zresult.D, Pg/M, Zresult.D, Zop2.D
-        ///   MOVPRFX Zresult.D, Pg/Z, Zop2.D; FDIVR Zresult.D, Pg/M, Zresult.D, Zop1.D
         /// </summary>
         public static unsafe Vector<double> Divide(Vector<double> left, Vector<double> right) => Divide(left, right);
 
@@ -1041,11 +1037,9 @@ namespace System.Runtime.Intrinsics.Arm
         ///   MOVPRFX Zresult, Zop1; SUB Zresult.B, Pg/M, Zresult.B, Zop2.B
         /// svint8_t svsub[_s8]_x(svbool_t pg, svint8_t op1, svint8_t op2)
         ///   SUB Ztied1.B, Pg/M, Ztied1.B, Zop2.B
-        ///   SUBR Ztied2.B, Pg/M, Ztied2.B, Zop1.B
         ///   SUB Zresult.B, Zop1.B, Zop2.B
         /// svint8_t svsub[_s8]_z(svbool_t pg, svint8_t op1, svint8_t op2)
         ///   MOVPRFX Zresult.B, Pg/Z, Zop1.B; SUB Zresult.B, Pg/M, Zresult.B, Zop2.B
-        ///   MOVPRFX Zresult.B, Pg/Z, Zop2.B; SUBR Zresult.B, Pg/M, Zresult.B, Zop1.B
         /// </summary>
         public static unsafe Vector<sbyte> Subtract(Vector<sbyte> left, Vector<sbyte> right) => Subtract(left, right);
 
@@ -1055,11 +1049,9 @@ namespace System.Runtime.Intrinsics.Arm
         ///   MOVPRFX Zresult, Zop1; SUB Zresult.H, Pg/M, Zresult.H, Zop2.H
         /// svint16_t svsub[_s16]_x(svbool_t pg, svint16_t op1, svint16_t op2)
         ///   SUB Ztied1.H, Pg/M, Ztied1.H, Zop2.H
-        ///   SUBR Ztied2.H, Pg/M, Ztied2.H, Zop1.H
         ///   SUB Zresult.H, Zop1.H, Zop2.H
         /// svint16_t svsub[_s16]_z(svbool_t pg, svint16_t op1, svint16_t op2)
         ///   MOVPRFX Zresult.H, Pg/Z, Zop1.H; SUB Zresult.H, Pg/M, Zresult.H, Zop2.H
-        ///   MOVPRFX Zresult.H, Pg/Z, Zop2.H; SUBR Zresult.H, Pg/M, Zresult.H, Zop1.H
         /// </summary>
         public static unsafe Vector<short> Subtract(Vector<short> left, Vector<short> right) => Subtract(left, right);
 
@@ -1069,11 +1061,9 @@ namespace System.Runtime.Intrinsics.Arm
         ///   MOVPRFX Zresult, Zop1; SUB Zresult.S, Pg/M, Zresult.S, Zop2.S
         /// svint32_t svsub[_s32]_x(svbool_t pg, svint32_t op1, svint32_t op2)
         ///   SUB Ztied1.S, Pg/M, Ztied1.S, Zop2.S
-        ///   SUBR Ztied2.S, Pg/M, Ztied2.S, Zop1.S
         ///   SUB Zresult.S, Zop1.S, Zop2.S
         /// svint32_t svsub[_s32]_z(svbool_t pg, svint32_t op1, svint32_t op2)
         ///   MOVPRFX Zresult.S, Pg/Z, Zop1.S; SUB Zresult.S, Pg/M, Zresult.S, Zop2.S
-        ///   MOVPRFX Zresult.S, Pg/Z, Zop2.S; SUBR Zresult.S, Pg/M, Zresult.S, Zop1.S
         /// </summary>
         public static unsafe Vector<int> Subtract(Vector<int> left, Vector<int> right) => Subtract(left, right);
 
@@ -1083,11 +1073,9 @@ namespace System.Runtime.Intrinsics.Arm
         ///   MOVPRFX Zresult, Zop1; SUB Zresult.D, Pg/M, Zresult.D, Zop2.D
         /// svint64_t svsub[_s64]_x(svbool_t pg, svint64_t op1, svint64_t op2)
         ///   SUB Ztied1.D, Pg/M, Ztied1.D, Zop2.D
-        ///   SUBR Ztied2.D, Pg/M, Ztied2.D, Zop1.D
         ///   SUB Zresult.D, Zop1.D, Zop2.D
         /// svint64_t svsub[_s64]_z(svbool_t pg, svint64_t op1, svint64_t op2)
         ///   MOVPRFX Zresult.D, Pg/Z, Zop1.D; SUB Zresult.D, Pg/M, Zresult.D, Zop2.D
-        ///   MOVPRFX Zresult.D, Pg/Z, Zop2.D; SUBR Zresult.D, Pg/M, Zresult.D, Zop1.D
         /// </summary>
         public static unsafe Vector<long> Subtract(Vector<long> left, Vector<long> right) => Subtract(left, right);
 
@@ -1097,11 +1085,9 @@ namespace System.Runtime.Intrinsics.Arm
         ///   MOVPRFX Zresult, Zop1; SUB Zresult.B, Pg/M, Zresult.B, Zop2.B
         /// svuint8_t svsub[_u8]_x(svbool_t pg, svuint8_t op1, svuint8_t op2)
         ///   SUB Ztied1.B, Pg/M, Ztied1.B, Zop2.B
-        ///   SUBR Ztied2.B, Pg/M, Ztied2.B, Zop1.B
         ///   SUB Zresult.B, Zop1.B, Zop2.B
         /// svuint8_t svsub[_u8]_z(svbool_t pg, svuint8_t op1, svuint8_t op2)
         ///   MOVPRFX Zresult.B, Pg/Z, Zop1.B; SUB Zresult.B, Pg/M, Zresult.B, Zop2.B
-        ///   MOVPRFX Zresult.B, Pg/Z, Zop2.B; SUBR Zresult.B, Pg/M, Zresult.B, Zop1.B
         /// </summary>
         public static unsafe Vector<byte> Subtract(Vector<byte> left, Vector<byte> right) => Subtract(left, right);
 
@@ -1111,11 +1097,9 @@ namespace System.Runtime.Intrinsics.Arm
         ///   MOVPRFX Zresult, Zop1; SUB Zresult.H, Pg/M, Zresult.H, Zop2.H
         /// svuint16_t svsub[_u16]_x(svbool_t pg, svuint16_t op1, svuint16_t op2)
         ///   SUB Ztied1.H, Pg/M, Ztied1.H, Zop2.H
-        ///   SUBR Ztied2.H, Pg/M, Ztied2.H, Zop1.H
         ///   SUB Zresult.H, Zop1.H, Zop2.H
         /// svuint16_t svsub[_u16]_z(svbool_t pg, svuint16_t op1, svuint16_t op2)
         ///   MOVPRFX Zresult.H, Pg/Z, Zop1.H; SUB Zresult.H, Pg/M, Zresult.H, Zop2.H
-        ///   MOVPRFX Zresult.H, Pg/Z, Zop2.H; SUBR Zresult.H, Pg/M, Zresult.H, Zop1.H
         /// </summary>
         public static unsafe Vector<ushort> Subtract(Vector<ushort> left, Vector<ushort> right) => Subtract(left, right);
 
@@ -1125,11 +1109,9 @@ namespace System.Runtime.Intrinsics.Arm
         ///   MOVPRFX Zresult, Zop1; SUB Zresult.S, Pg/M, Zresult.S, Zop2.S
         /// svuint32_t svsub[_u32]_x(svbool_t pg, svuint32_t op1, svuint32_t op2)
         ///   SUB Ztied1.S, Pg/M, Ztied1.S, Zop2.S
-        ///   SUBR Ztied2.S, Pg/M, Ztied2.S, Zop1.S
         ///   SUB Zresult.S, Zop1.S, Zop2.S
         /// svuint32_t svsub[_u32]_z(svbool_t pg, svuint32_t op1, svuint32_t op2)
         ///   MOVPRFX Zresult.S, Pg/Z, Zop1.S; SUB Zresult.S, Pg/M, Zresult.S, Zop2.S
-        ///   MOVPRFX Zresult.S, Pg/Z, Zop2.S; SUBR Zresult.S, Pg/M, Zresult.S, Zop1.S
         /// </summary>
         public static unsafe Vector<uint> Subtract(Vector<uint> left, Vector<uint> right) => Subtract(left, right);
 
@@ -1139,11 +1121,9 @@ namespace System.Runtime.Intrinsics.Arm
         ///   MOVPRFX Zresult, Zop1; SUB Zresult.D, Pg/M, Zresult.D, Zop2.D
         /// svuint64_t svsub[_u64]_x(svbool_t pg, svuint64_t op1, svuint64_t op2)
         ///   SUB Ztied1.D, Pg/M, Ztied1.D, Zop2.D
-        ///   SUBR Ztied2.D, Pg/M, Ztied2.D, Zop1.D
         ///   SUB Zresult.D, Zop1.D, Zop2.D
         /// svuint64_t svsub[_u64]_z(svbool_t pg, svuint64_t op1, svuint64_t op2)
         ///   MOVPRFX Zresult.D, Pg/Z, Zop1.D; SUB Zresult.D, Pg/M, Zresult.D, Zop2.D
-        ///   MOVPRFX Zresult.D, Pg/Z, Zop2.D; SUBR Zresult.D, Pg/M, Zresult.D, Zop1.D
         /// </summary>
         public static unsafe Vector<ulong> Subtract(Vector<ulong> left, Vector<ulong> right) => Subtract(left, right);
 
@@ -1153,12 +1133,10 @@ namespace System.Runtime.Intrinsics.Arm
         ///   MOVPRFX Zresult, Zop1; FSUB Zresult.S, Pg/M, Zresult.S, Zop2.S
         /// svfloat32_t svsub[_f32]_x(svbool_t pg, svfloat32_t op1, svfloat32_t op2)
         ///   FSUB Ztied1.S, Pg/M, Ztied1.S, Zop2.S
-        ///   FSUBR Ztied2.S, Pg/M, Ztied2.S, Zop1.S
         ///   FSUB Zresult.S, Zop1.S, Zop2.S
         ///   MOVPRFX Zresult, Zop1; FSUB Zresult.S, Pg/M, Zresult.S, Zop2.S
         /// svfloat32_t svsub[_f32]_z(svbool_t pg, svfloat32_t op1, svfloat32_t op2)
         ///   MOVPRFX Zresult.S, Pg/Z, Zop1.S; FSUB Zresult.S, Pg/M, Zresult.S, Zop2.S
-        ///   MOVPRFX Zresult.S, Pg/Z, Zop2.S; FSUBR Zresult.S, Pg/M, Zresult.S, Zop1.S
         /// </summary>
         public static unsafe Vector<float> Subtract(Vector<float> left, Vector<float> right) => Subtract(left, right);
 
@@ -1168,12 +1146,10 @@ namespace System.Runtime.Intrinsics.Arm
         ///   MOVPRFX Zresult, Zop1; FSUB Zresult.D, Pg/M, Zresult.D, Zop2.D
         /// svfloat64_t svsub[_f64]_x(svbool_t pg, svfloat64_t op1, svfloat64_t op2)
         ///   FSUB Ztied1.D, Pg/M, Ztied1.D, Zop2.D
-        ///   FSUBR Ztied2.D, Pg/M, Ztied2.D, Zop1.D
         ///   FSUB Zresult.D, Zop1.D, Zop2.D
         ///   MOVPRFX Zresult, Zop1; FSUB Zresult.D, Pg/M, Zresult.D, Zop2.D
         /// svfloat64_t svsub[_f64]_z(svbool_t pg, svfloat64_t op1, svfloat64_t op2)
         ///   MOVPRFX Zresult.D, Pg/Z, Zop1.D; FSUB Zresult.D, Pg/M, Zresult.D, Zop2.D
-        ///   MOVPRFX Zresult.D, Pg/Z, Zop2.D; FSUBR Zresult.D, Pg/M, Zresult.D, Zop1.D
         /// </summary>
         public static unsafe Vector<double> Subtract(Vector<double> left, Vector<double> right) => Subtract(left, right);
 
