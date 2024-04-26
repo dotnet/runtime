2b21f22b38b2afe5d62dd30d507fd0ec5c279735
Radek Zikmund <r.zikmund.rz@gmail.com>
2024-04-26 20:46:51 +0200

    Add runtime collection to Linux docker runs

---
+++ src/libraries/System.Net.Http/tests/StressTests/HttpStress/run.sh
@@ -0,0 +1,11 @@
+/live-runtime-artifacts/testhost/net$VERSION-linux-$CONFIGURATION-x64/dotnet exec --roll-forward Major ./bin/$CONFIGURATION/net$VERSION/HttpStress.dll $HTTPSTRESS_ARGS
+
+if [ $? -ne 0 ]; then
+    echo "HttpStress failed"
+    # copy runtime artifacts and msquic
+    cp -r /live-runtime-artifacts/testhost/net$VERSION-linux-$CONFIGURATION-x64/ $DUMPS_SHARE_MOUNT_ROOT
+    mkdir -p $DUMPS_SHARE_MOUNT_ROOT/msquic
+    cp -r /msquic/msquic/bin/Debug $DUMPS_SHARE_MOUNT_ROOT/msquic
+
+    exit 1
+fi
