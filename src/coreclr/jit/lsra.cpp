77468e0198d24058c94220186f56459026da0f58
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2024-04-27 16:57:55 +0200

    Remove histogram

--- src/coreclr/jit/lsra.cpp
+++ src/coreclr/jit/lsra.cpp
@@ -1428,18 +1428,6 @@ PhaseStatus LinearScan::doLinearScan()
     }
     compiler->EndPhase(PHASE_LINEAR_SCAN_RESOLVE);
 
-    // for (BasicBlock* block : compiler->Blocks())
-    //{
-    //     for (GenTree* node : LIR::AsRange(block))
-    //     {
-    //         static const unsigned s_buckets[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 0 };
-    //         static Histogram s_numInternalTemps(s_buckets);
-    //         static DumpOnShutdown s_d("Num internal temps", &s_numInternalTemps);
-
-    //        s_numInternalTemps.record(genCountBits(compiler->codeGen->internalRegisters.GetAll(node)));
-    //    }
-    //}
-
     assert(blockSequencingDone); // Should do at least one traversal.
     assert(blockEpoch == compiler->GetCurBasicBlockEpoch());
 
