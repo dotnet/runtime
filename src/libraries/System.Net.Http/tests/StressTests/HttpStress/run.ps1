455cdbb9167db774318730ae1adacf523b48be1f
Radek Zikmund <r.zikmund.rz@gmail.com>
2024-04-29 17:54:12 +0200

    Fix script

--- src/libraries/System.Net.Http/tests/StressTests/HttpStress/run.ps1
+++ src/libraries/System.Net.Http/tests/StressTests/HttpStress/run.ps1
@@ -1,5 +1,5 @@
-& "C:/live-runtime-artifacts/testhost/net$env:VERSION-windows-$env:CONFIGURATION-x64/dotnet.exe" exec --roll-forward Major ./bin/$env:CONFIGURATION/net$env:VERSION/HttpStress.dll $env:HTTPSTRESS_ARGS.Split(' ',[System.StringSplitOptions]::RemoveEmptyEntries)
+& "C:/live-runtime-artifacts/testhost/net$env:VERSION-windows-$env:CONFIGURATION-x64/dotnet.exe" exec --roll-forward Major ./bin/$env:CONFIGURATION/net$env:VERSION/HttpStress.dll $env:HTTPSTRESS_ARGS.Split(' ', [System.StringSplitOptions]::RemoveEmptyEntries)
 
 if ($LASTEXITCODE -ne 0) {
-    cp "C:/live-runtime-artifacts/testhost/net$env:VERSION-windows-$env:CONFIGURATION-x64/ $env:DUMPS_SHARE_MOUNT_ROOT
+    cp "C:/live-runtime-artifacts/testhost/net$env:VERSION-windows-$env:CONFIGURATION-x64/" $env:DUMPS_SHARE_MOUNT_ROOT
 }
