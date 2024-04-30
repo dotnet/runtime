a810556b8f13c688b38d49c764d04786cc3895a9
wfurt <tweinfurt@yahoo.com>
2024-04-30 02:19:59 +0000

    unsafe

--- src/libraries/Common/src/Interop/Android/System.Security.Cryptography.Native.Android/Interop.Initialization.cs
+++ src/libraries/Common/src/Interop/Android/System.Security.Cryptography.Native.Android/Interop.Initialization.cs
@@ -49,6 +49,6 @@ internal static partial class Interop
         }
 
         [LibraryImport(Libraries.AndroidCryptoNative, EntryPoint = "CryptoNative_EnsureOpenSslInitialized")]
-        private static partial int EnsureOpenSslInitialized(void* mallocFunction, void* reallocFunction, void* freeFunction);
+        private static unsafe partial int EnsureOpenSslInitialized(void* mallocFunction, void* reallocFunction, void* freeFunction);
     }
 }
