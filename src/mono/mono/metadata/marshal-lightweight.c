6fcefc69f5920d8e82514d7a74fc30837b5810ef
Aleksey Kliger <alklig@microsoft.com>
2024-05-06 14:58:59 -0400

    [metadata] add ability to inflate wrapper data
    When we create generic wrappers (or wrappers in a generic class),
    if the wrapper data needs to refer to a field, method, or parameter
    type of the definition, that data might need to be inflated if the
    containing class is inflated (or the generic wrapper method is
    inflated).
    
    Add a new function to opt into inflation:
    
    ```c
        get_marshal_cb ()->mb_inflate_wrapper_data (mb);
    ```
    
    Add a new function to be called after mono_mb_emit_op (or any other
    call that calls mono_mb_add_data):
    
    ```c
    	mono_mb_emit_op (mb, CEE_LDFLDA, field);
            mono_mb_set_wrapper_data_kind (mb, MONO_MB_ILGEN_WRAPPER_DATA_FIELD);
    ```
    
    Note: mono_mb_set_wrapper_data_kind asserts if you haven't called mb_inflate_wrapper_data.
    
    TODO: add more wrapper data kinds for MonoMethod* and MonoClass* etc

--- src/mono/mono/metadata/marshal-lightweight.c
+++ src/mono/mono/metadata/marshal-lightweight.c
@@ -2137,6 +2137,16 @@ mb_skip_visibility_ilgen (MonoMethodBuilder *mb)
 }
 
 static void
+mb_inflate_wrapper_data_ilgen (MonoMethodBuilder *mb)
+{
+	g_assert (!mb->dynamic); // dynamic methods with inflated data not implemented yet - needs at least mono_free_method changes, probably more
+	mb->inflate_wrapper_data = TRUE;
+	int idx = mono_mb_add_data (mb, NULL);
+	// note: match index used in create_method_ilgen
+	g_assertf (idx == MONO_MB_ILGEN_INFLATE_WRAPPER_INFO_IDX, "mb_inflate_wrapper_data called after data already added");
+}
+
+static void
 emit_synchronized_wrapper_ilgen (MonoMethodBuilder *mb, MonoMethod *method, MonoGenericContext *ctx, MonoGenericContainer *container, MonoMethod *enter_method, MonoMethod *exit_method, MonoMethod *gettypefromhandle_method)
 {
 	int i, pos, pos2, this_local, taken_local, ret_local = 0;
@@ -3467,6 +3477,7 @@ mono_marshal_lightweight_init (void)
 	cb.emit_return = emit_return_ilgen;
 	cb.emit_vtfixup_ftnptr = emit_vtfixup_ftnptr_ilgen;
 	cb.mb_skip_visibility = mb_skip_visibility_ilgen;
+	cb.mb_inflate_wrapper_data = mb_inflate_wrapper_data_ilgen;
 	cb.mb_emit_exception = mb_emit_exception_ilgen;
 	cb.mb_emit_exception_for_error = mb_emit_exception_for_error_ilgen;
 	cb.mb_emit_byte = mb_emit_byte_ilgen;
