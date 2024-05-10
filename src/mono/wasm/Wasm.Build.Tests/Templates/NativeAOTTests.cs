a13f2a0cff30487afb597b570bb772d7e82fc70a
Marek Fišera <mara@neptuo.com>
2024-05-10 13:56:07 +0200

    Linux is not supported

--- src/mono/wasm/Wasm.Build.Tests/Templates/NativeAOTTests.cs
+++ src/mono/wasm/Wasm.Build.Tests/Templates/NativeAOTTests.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using Wasm.Build.Tests.Blazor;
@@ -44,17 +45,24 @@ public class NativeAOTTests : BlazorWasmTestBase
             )
         );
 
-        string outputDir = Path.Combine(_projectDir!, "bin", config, DefaultTargetFramework, BuildEnvironment.DefaultRuntimeIdentifier, "native");
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            string outputDir = Path.Combine(_projectDir!, "bin", config, DefaultTargetFramework, BuildEnvironment.DefaultRuntimeIdentifier, "native");
 
-        List<string> consoleOutput = new();
-        BlazorRunOptions blazorRunOptions = new(
-            CheckCounter: false,
-            Config: config,
-            OnConsoleMessage: (_, message) => consoleOutput.Add(message.Text),
-            Host: BlazorRunHost.WebServer
-        );
-        await BlazorRunTest($"{s_xharnessRunnerCommand} wasm webserver --app=. --web-server-use-default-files", outputDir, blazorRunOptions);
+            List<string> consoleOutput = new();
+            BlazorRunOptions blazorRunOptions = new(
+                CheckCounter: false,
+                Config: config,
+                OnConsoleMessage: (_, message) => consoleOutput.Add(message.Text),
+                Host: BlazorRunHost.WebServer
+            );
+            await BlazorRunTest($"{s_xharnessRunnerCommand} wasm webserver --app=. --web-server-use-default-files", outputDir, blazorRunOptions);
 
-        Assert.True(consoleOutput.Contains("Hello, NativeAOT!"), $"Expected 'Hello, NativeAOT!' wasn't emitted by the test app. Output was: {String.Join(", ", consoleOutput)}");
+            Assert.True(consoleOutput.Contains("Hello, NativeAOT!"), $"Expected 'Hello, NativeAOT!' wasn't emitted by the test app. Output was: {String.Join(", ", consoleOutput)}");
+        }
+        else
+        {
+            Assert.Contains("NETSDK1204", buildOutput); // Ahead-of-time compilation is not supported on the current platform 'linux-x64'
+        }
     }
 }
