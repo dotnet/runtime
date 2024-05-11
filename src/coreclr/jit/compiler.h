7ce5d0fdf728bb2ae213541b17c2e489d2b06d09
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2024-05-11 18:57:31 +0200

    JIT: Expand optRelopImpliesRelop for constants as op1 (#102024)
    VN does not guarantee that constants in relops are first, so swap them
    if this isn't the case. This allows the logic to prove the value of more relops.

--- src/coreclr/jit/compiler.h
+++ src/coreclr/jit/compiler.h
@@ -7502,6 +7502,9 @@ public:
     BitVecTraits* optReachableBitVecTraits;
     BitVec        optReachableBitVec;
     void          optRelopImpliesRelop(RelopImplicationInfo* rii);
+    bool          optRelopTryInferWithOneEqualOperand(const VNFuncApp&      domApp,
+                                                      const VNFuncApp&      treeApp,
+                                                      RelopImplicationInfo* rii);
 
     /**************************************************************************
      *               Value/Assertion propagation
