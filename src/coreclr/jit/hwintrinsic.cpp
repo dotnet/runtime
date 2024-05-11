1b16a09e751186ad2eac1fa9d160a61b35909b29
Tanner Gooding <tagoo@outlook.com>
2024-05-11 10:31:23 -0700

    Apply formatting patch

--- src/coreclr/jit/hwintrinsic.cpp
+++ src/coreclr/jit/hwintrinsic.cpp
@@ -1612,7 +1612,8 @@ GenTree* Compiler::impHWIntrinsic(NamedIntrinsic        intrinsic,
     }
     else
     {
-        retNode = impSpecialIntrinsic(intrinsic, clsHnd, method, sig, simdBaseJitType, nodeRetType, simdSize, mustExpand);
+        retNode =
+            impSpecialIntrinsic(intrinsic, clsHnd, method, sig, simdBaseJitType, nodeRetType, simdSize, mustExpand);
     }
 
 #if defined(TARGET_ARM64)
