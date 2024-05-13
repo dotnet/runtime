a6a36b6e3d43bf47e9c2b10a085b76924b56dc8a
Ilona Tomkowicz <32700855+ilonatommy@users.noreply.github.com>
2024-05-13 10:54:11 +0000

    Fix previous commit.

--- src/mono/browser/runtime/exports-binding.ts
+++ src/mono/browser/runtime/exports-binding.ts
@@ -101,47 +101,80 @@ export const mono_wasm_imports = [
 ];
 
 function mono_wasm_change_case_invariant (src: number, srcLength: number, dst: number, dstLength: number, toUpper: number) : VoidPtr {
-    return runtimeHelpers?.mono_wasm_change_case_invariant(src, srcLength, dst, dstLength, toUpper) ?? VoidPtrNull;
+    if (typeof runtimeHelpers.mono_wasm_change_case_invariant === "function") {
+        return runtimeHelpers.mono_wasm_change_case_invariant(src, srcLength, dst, dstLength, toUpper);
+    }
+    return VoidPtrNull;
 }
 
 function mono_wasm_change_case (culture: number, cultureLength: number, src: number, srcLength: number, dst: number, dstLength: number, toUpper: number) : VoidPtr {
-    return runtimeHelpers?.mono_wasm_change_case(culture, cultureLength, src, srcLength, dst, dstLength, toUpper) ?? VoidPtrNull;
+    if (typeof runtimeHelpers.mono_wasm_change_case === "function") {
+        return runtimeHelpers.mono_wasm_change_case(culture, cultureLength, src, srcLength, dst, dstLength, toUpper);
+    }
+    return VoidPtrNull;
 }
 
 function mono_wasm_compare_string (culture: number, cultureLength: number, str1: number, str1Length: number, str2: number, str2Length: number, options: number, resultPtr: Int32Ptr) : VoidPtr {
-    return runtimeHelpers.mono_wasm_compare_string(culture, cultureLength, str1, str1Length, str2, str2Length, options, resultPtr) ?? VoidPtrNull;
+    if (typeof runtimeHelpers.mono_wasm_compare_string === "function") {
+        return runtimeHelpers.mono_wasm_compare_string(culture, cultureLength, str1, str1Length, str2, str2Length, options, resultPtr);
+    }
+    return VoidPtrNull;
 }
 
 function mono_wasm_starts_with (culture: number, cultureLength: number, str1: number, str1Length: number, str2: number, str2Length: number, options: number, resultPtr: Int32Ptr): VoidPtr {
-    return runtimeHelpers?.mono_wasm_starts_with(culture, cultureLength, str1, str1Length, str2, str2Length, options, resultPtr) ?? VoidPtrNull;
+    if (typeof runtimeHelpers.mono_wasm_starts_with === "function") {
+        return runtimeHelpers.mono_wasm_starts_with(culture, cultureLength, str1, str1Length, str2, str2Length, options, resultPtr);
+    }
+    return VoidPtrNull;
 }
 
 function mono_wasm_ends_with (culture: number, cultureLength: number, str1: number, str1Length: number, str2: number, str2Length: number, options: number, resultPtr: Int32Ptr): VoidPtr {
-    return runtimeHelpers?.mono_wasm_ends_with(culture, cultureLength, str1, str1Length, str2, str2Length, options, resultPtr) ?? VoidPtrNull;
+    if (typeof runtimeHelpers.mono_wasm_ends_with === "function") {
+        return runtimeHelpers.mono_wasm_ends_with(culture, cultureLength, str1, str1Length, str2, str2Length, options, resultPtr);
+    }
+    return VoidPtrNull;
 }
 
 function mono_wasm_index_of (culture: number, cultureLength: number, needlePtr: number, needleLength: number, srcPtr: number, srcLength: number, options: number, fromBeginning: number, resultPtr: Int32Ptr): VoidPtr {
-    return runtimeHelpers?.mono_wasm_index_of(culture, cultureLength, needlePtr, needleLength, srcPtr, srcLength, options, fromBeginning, resultPtr) ?? VoidPtrNull;
+    if (typeof runtimeHelpers.mono_wasm_index_of === "function") {
+        return runtimeHelpers.mono_wasm_index_of(culture, cultureLength, needlePtr, needleLength, srcPtr, srcLength, options, fromBeginning, resultPtr);
+    }
+    return VoidPtrNull;
 }
 
 function mono_wasm_get_calendar_info (culture: number, cultureLength: number, calendarId: number, dst: number, dstMaxLength: number, dstLength: Int32Ptr): VoidPtr {
-    return runtimeHelpers?.mono_wasm_get_calendar_info(culture, cultureLength, calendarId, dst, dstMaxLength, dstLength) ?? VoidPtrNull;
+    if (typeof runtimeHelpers.mono_wasm_get_calendar_info === "function") {
+        return runtimeHelpers.mono_wasm_get_calendar_info(culture, cultureLength, calendarId, dst, dstMaxLength, dstLength);
+    }
+    return VoidPtrNull;
 }
 
 function mono_wasm_get_culture_info (culture: number, cultureLength: number, dst: number, dstMaxLength: number, dstLength: Int32Ptr): VoidPtr {
-    return runtimeHelpers?.mono_wasm_get_culture_info(culture, cultureLength, dst, dstMaxLength, dstLength) ?? VoidPtrNull;
+    if (typeof runtimeHelpers.mono_wasm_get_culture_info === "function") {
+        return runtimeHelpers.mono_wasm_get_culture_info(culture, cultureLength, dst, dstMaxLength, dstLength);
+    }
+    return VoidPtrNull;
 }
 
 function mono_wasm_get_locale_info (culture: number, cultureLength: number, locale: number, localeLength: number, dst: number, dstMaxLength: number, dstLength: Int32Ptr): VoidPtr {
-    return runtimeHelpers?.mono_wasm_get_locale_info(culture, cultureLength, locale, localeLength, dst, dstMaxLength, dstLength) ?? VoidPtrNull;
+    if (typeof runtimeHelpers.mono_wasm_get_locale_info === "function") {
+        return runtimeHelpers.mono_wasm_get_locale_info(culture, cultureLength, locale, localeLength, dst, dstMaxLength, dstLength);
+    }
+    return VoidPtrNull;
 }
 
 function mono_wasm_get_first_day_of_week (culture: number, cultureLength: number, resultPtr: Int32Ptr): VoidPtr {
-    return runtimeHelpers?.mono_wasm_get_first_day_of_week(culture, cultureLength, resultPtr) ?? VoidPtrNull;
+    if (typeof runtimeHelpers.mono_wasm_get_first_day_of_week === "function") {
+        return runtimeHelpers.mono_wasm_get_first_day_of_week(culture, cultureLength, resultPtr);
+    }
+    return VoidPtrNull;
 }
 
 function mono_wasm_get_first_week_of_year (culture: number, cultureLength: number, resultPtr: Int32Ptr): VoidPtr {
-    return runtimeHelpers?.mono_wasm_get_first_week_of_year(culture, cultureLength, resultPtr) ?? VoidPtrNull;
+    if (typeof runtimeHelpers.mono_wasm_get_first_week_of_year === "function") {
+        return runtimeHelpers.mono_wasm_get_first_week_of_year(culture, cultureLength, resultPtr);
+    }
+    return VoidPtrNull;
 }
 
 export const mono_wasm_hybrid_globalization_imports = [
