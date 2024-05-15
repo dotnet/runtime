fcd2cff70a6aaa9e5a7aa6136e441e3ebe4d6281
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2024-05-16 02:36:26 +0800

    Cleanup Variant struct and FCall

--- src/coreclr/System.Private.CoreLib/src/System/Variant.cs
+++ src/coreclr/System.Private.CoreLib/src/System/Variant.cs
@@ -12,6 +12,7 @@
 
 using System.Diagnostics;
 using System.Globalization;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.Marshalling;
@@ -21,59 +22,8 @@ using Microsoft.Win32;
 
 namespace System
 {
-    internal partial struct Variant
+    internal static partial class Variant
     {
-        // Do Not change the order of these fields.
-        // They are mapped to the native VariantData * data structure.
-        private object? _objref;
-        private long _data;
-        private int _flags;
-
-        // The following bits have been taken up as follows
-        // bits 0-15    - Type code
-        // bit  16      - Array
-        // bits 19-23   - Enums
-        // bits 24-31   - Optional VT code (for roundtrip VT preservation)
-
-        // What are the consequences of making this an enum?
-        ///////////////////////////////////////////////////////////////////////
-        // If you update this, update the corresponding stuff in OAVariantLib.cs,
-        // OAVariant.cpp (2 tables, forwards and reverse), and perhaps OleVariant.h
-        ///////////////////////////////////////////////////////////////////////
-        internal const int CV_EMPTY = 0x0;
-        internal const int CV_VOID = 0x1;
-        internal const int CV_BOOLEAN = 0x2;
-        internal const int CV_CHAR = 0x3;
-        internal const int CV_I1 = 0x4;
-        internal const int CV_U1 = 0x5;
-        internal const int CV_I2 = 0x6;
-        internal const int CV_U2 = 0x7;
-        internal const int CV_I4 = 0x8;
-        internal const int CV_U4 = 0x9;
-        internal const int CV_I8 = 0xa;
-        internal const int CV_U8 = 0xb;
-        internal const int CV_R4 = 0xc;
-        internal const int CV_R8 = 0xd;
-        internal const int CV_STRING = 0xe;
-        internal const int CV_PTR = 0xf;
-        internal const int CV_DATETIME = 0x10;
-        internal const int CV_TIMESPAN = 0x11;
-        internal const int CV_OBJECT = 0x12;
-        internal const int CV_DECIMAL = 0x13;
-        internal const int CV_ENUM = 0x15;
-        internal const int CV_MISSING = 0x16;
-        internal const int CV_NULL = 0x17;
-        internal const int CV_LAST = 0x18;
-
-        internal const int TypeCodeBitMask = 0xffff;
-        internal const int VTBitMask = unchecked((int)0xff000000);
-        internal const int VTBitShift = 24;
-        internal const int ArrayBitMask = 0x10000;
-
-        internal static Variant Empty => default;
-        internal static Variant Missing => new Variant(CV_MISSING, Type.Missing, 0);
-        internal static Variant DBNull => new Variant(CV_NULL, System.DBNull.Value, 0);
-
         internal static bool IsSystemDrawingColor(Type type) => type.FullName == "System.Drawing.Color"; // Matches the behavior of IsTypeRefOrDef
 
         [LibraryImport(RuntimeHelpers.QCall, EntryPoint = "Variant_ConvertSystemColorToOleColor")]
@@ -81,243 +31,6 @@ namespace System
 
         [LibraryImport(RuntimeHelpers.QCall, EntryPoint = "Variant_ConvertOleColorToSystemColor")]
         internal static partial void ConvertOleColorToSystemColor(ObjectHandleOnStack objret, uint value, IntPtr pMT);
-
-        //
-        // Native Methods
-        //
-        [MethodImpl(MethodImplOptions.InternalCall)]
-        internal extern void SetFieldsObject(object val);
-
-        //
-        // Constructors
-        //
-
-        internal Variant(int flags, object or, long data)
-        {
-            _flags = flags;
-            _objref = or;
-            _data = data;
-        }
-
-        public Variant(bool val)
-        {
-            _objref = null;
-            _flags = CV_BOOLEAN;
-            _data = (val) ? bool.True : bool.False;
-        }
-
-        public Variant(sbyte val)
-        {
-            _objref = null;
-            _flags = CV_I1;
-            _data = val;
-        }
-
-        public Variant(byte val)
-        {
-            _objref = null;
-            _flags = CV_U1;
-            _data = val;
-        }
-
-        public Variant(short val)
-        {
-            _objref = null;
-            _flags = CV_I2;
-            _data = val;
-        }
-
-        public Variant(ushort val)
-        {
-            _objref = null;
-            _flags = CV_U2;
-            _data = val;
-        }
-
-        public Variant(char val)
-        {
-            _objref = null;
-            _flags = CV_CHAR;
-            _data = val;
-        }
-
-        public Variant(int val)
-        {
-            _objref = null;
-            _flags = CV_I4;
-            _data = val;
-        }
-
-        public Variant(uint val)
-        {
-            _objref = null;
-            _flags = CV_U4;
-            _data = val;
-        }
-
-        public Variant(long val)
-        {
-            _objref = null;
-            _flags = CV_I8;
-            _data = val;
-        }
-
-        public Variant(ulong val)
-        {
-            _objref = null;
-            _flags = CV_U8;
-            _data = (long)val;
-        }
-
-        public Variant(float val)
-        {
-            _objref = null;
-            _flags = CV_R4;
-            _data = BitConverter.SingleToUInt32Bits(val);
-        }
-
-        public Variant(double val)
-        {
-            _objref = null;
-            _flags = CV_R8;
-            _data = BitConverter.DoubleToInt64Bits(val);
-        }
-
-        public Variant(DateTime val)
-        {
-            _objref = null;
-            _flags = CV_DATETIME;
-            _data = val.Ticks;
-        }
-
-        public Variant(decimal val)
-        {
-            _objref = (object)val;
-            _flags = CV_DECIMAL;
-            _data = 0;
-        }
-
-        public Variant(object? obj)
-        {
-            _data = 0;
-
-            VarEnum vt = VarEnum.VT_EMPTY;
-
-            if (obj is DateTime)
-            {
-                _objref = null;
-                _flags = CV_DATETIME;
-                _data = ((DateTime)obj).Ticks;
-                return;
-            }
-
-            if (obj is string)
-            {
-                _flags = CV_STRING;
-                _objref = obj;
-                return;
-            }
-
-            if (obj == null)
-            {
-                this = Empty;
-                return;
-            }
-            if (obj == System.DBNull.Value)
-            {
-                this = DBNull;
-                return;
-            }
-            if (obj == Type.Missing)
-            {
-                this = Missing;
-                return;
-            }
-
-            if (obj is Array)
-            {
-                _flags = CV_OBJECT | ArrayBitMask;
-                _objref = obj;
-                return;
-            }
-
-            // Compiler appeasement
-            _flags = CV_EMPTY;
-            _objref = null;
-
-            // Check to see if the object passed in is a wrapper object.
-            if (obj is UnknownWrapper)
-            {
-                vt = VarEnum.VT_UNKNOWN;
-                obj = ((UnknownWrapper)obj).WrappedObject;
-            }
-            else if (obj is DispatchWrapper)
-            {
-                vt = VarEnum.VT_DISPATCH;
-                Debug.Assert(OperatingSystem.IsWindows());
-                obj = ((DispatchWrapper)obj).WrappedObject;
-            }
-            else if (obj is ErrorWrapper)
-            {
-                vt = VarEnum.VT_ERROR;
-                obj = (object)(((ErrorWrapper)obj).ErrorCode);
-                Debug.Assert(obj != null, "obj != null");
-            }
-#pragma warning disable 0618 // CurrencyWrapper is obsolete
-            else if (obj is CurrencyWrapper)
-            {
-                vt = VarEnum.VT_CY;
-                obj = (object)(((CurrencyWrapper)obj).WrappedObject);
-                Debug.Assert(obj != null, "obj != null");
-            }
-#pragma warning restore 0618
-            else if (obj is BStrWrapper)
-            {
-                vt = VarEnum.VT_BSTR;
-                obj = (object?)(((BStrWrapper)obj).WrappedObject);
-            }
-
-            if (obj != null)
-            {
-                SetFieldsObject(obj);
-            }
-
-            // If the object passed in is one of the wrappers then set the VARIANT type.
-            if (vt != VarEnum.VT_EMPTY)
-                _flags |= ((int)vt << VTBitShift);
-        }
-
-        // This is a family-only accessor for the CVType.
-        // This is never to be exposed externally.
-        internal int CVType => _flags & TypeCodeBitMask;
-
-        public object? ToObject() =>
-            CVType switch
-            {
-                CV_EMPTY => null,
-                CV_BOOLEAN => (int)_data != 0,
-                CV_I1 => (sbyte)_data,
-                CV_U1 => (byte)_data,
-                CV_CHAR => (char)_data,
-                CV_I2 => (short)_data,
-                CV_U2 => (ushort)_data,
-                CV_I4 => (int)_data,
-                CV_U4 => (uint)_data,
-                CV_I8 => _data,
-                CV_U8 => (ulong)_data,
-                CV_R4 => BitConverter.UInt32BitsToSingle((uint)_data),
-                CV_R8 => BitConverter.Int64BitsToDouble(_data),
-                CV_DATETIME => new DateTime(_data),
-                CV_TIMESPAN => new TimeSpan(_data),
-                CV_ENUM => BoxEnum(),
-                CV_MISSING => Type.Missing,
-                CV_NULL => System.DBNull.Value,
-                _ => _objref, // CV_DECIMAL, CV_STRING, CV_OBJECT
-            };
-
-        // This routine will return an boxed enum.
-        [MethodImpl(MethodImplOptions.InternalCall)]
-        private extern object BoxEnum();
 
         // Helper code for marshaling managed objects to VARIANT's
         internal static void MarshalHelperConvertObjectToVariant(object? o, out ComVariant pOle)
@@ -336,7 +49,7 @@ namespace System
                         pOle = ic.GetTypeCode() switch
                         {
                             TypeCode.Empty => default,
-                            TypeCode.DBNull => ComVariant.Create(System.DBNull.Value),
+                            TypeCode.DBNull => ComVariant.Create(DBNull.Value),
                             TypeCode.Boolean => ComVariant.Create(ic.ToBoolean(provider)),
                             TypeCode.Char => ComVariant.Create((ushort)ic.ToChar(provider)),
                             TypeCode.SByte => ComVariant.Create(ic.ToSByte(provider)),
@@ -357,7 +70,7 @@ namespace System
                         break;
                     }
 
-                case Reflection.Missing:
+                case Missing:
                     pOle = ComVariant.CreateRaw(VarEnum.VT_ERROR, HResults.DISP_E_PARAMNOTFOUND);
                     break;
 
@@ -384,11 +97,11 @@ namespace System
                     pOle = ComVariant.Create(wrapper);
                     break;
 
-                case System.Empty:
+                case Empty:
                     pOle = default;
                     break;
-                case System.DBNull:
-                    pOle = ComVariant.Create(System.DBNull.Value);
+                case DBNull:
+                    pOle = ComVariant.Create(DBNull.Value);
                     break;
 
                 case { } when IsSystemDrawingColor(o.GetType()):
@@ -459,7 +172,7 @@ namespace System
 
                 case VarEnum.VT_NULL:
                 case VarEnum.VT_BYREF | VarEnum.VT_NULL:
-                    return System.DBNull.Value;
+                    return DBNull.Value;
 
                 case VarEnum.VT_DATE:
                     return pOle.As<DateTime>();
@@ -489,10 +202,10 @@ namespace System
 
                 case VarEnum.VT_ERROR:
                     int error = pOle.GetRawDataRef<int>();
-                    return error == HResults.DISP_E_PARAMNOTFOUND ? Reflection.Missing.Value : error;
+                    return error == HResults.DISP_E_PARAMNOTFOUND ? Missing.Value : error;
                 case VarEnum.VT_BYREF | VarEnum.VT_ERROR:
                     int refError = *(int*)pOle.GetRawDataRef<IntPtr>();
-                    return refError == HResults.DISP_E_PARAMNOTFOUND ? Reflection.Missing.Value : refError;
+                    return refError == HResults.DISP_E_PARAMNOTFOUND ? Missing.Value : refError;
 
                 case VarEnum.VT_VOID:
                 case VarEnum.VT_BYREF | VarEnum.VT_VOID:
@@ -547,7 +260,7 @@ namespace System
                 v = (VarEnum)vt switch
                 {
                     VarEnum.VT_EMPTY => default,
-                    VarEnum.VT_NULL => ComVariant.Create(System.DBNull.Value),
+                    VarEnum.VT_NULL => ComVariant.Create(DBNull.Value),
                     VarEnum.VT_I2 => ComVariant.Create(iv.ToInt16(provider)),
                     VarEnum.VT_I4 => ComVariant.Create(iv.ToInt32(provider)),
                     VarEnum.VT_R4 => ComVariant.Create(iv.ToSingle(provider)),
