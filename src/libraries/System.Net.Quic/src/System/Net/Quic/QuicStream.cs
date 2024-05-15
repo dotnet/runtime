de8c3b919297e1da2bd3c3130319181894adb37d
ManickaP <mapichov@microsoft.com>
2024-05-15 17:26:56 +0200

    Validate Close/Abort errorCode parameter, shuffle around validation methods for better reuse

--- src/libraries/System.Net.Quic/src/System/Net/Quic/QuicStream.cs
+++ src/libraries/System.Net.Quic/src/System/Net/Quic/QuicStream.cs
@@ -447,6 +447,8 @@ public sealed partial class QuicStream
             return;
         }
 
+        ThrowHelper.ValidateErrorCode(nameof(Abort), errorCode);
+
         QUIC_STREAM_SHUTDOWN_FLAGS flags = QUIC_STREAM_SHUTDOWN_FLAGS.NONE;
         if (abortDirection.HasFlag(QuicAbortDirection.Read) && !_receiveTcs.IsCompleted)
         {
