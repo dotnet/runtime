8595aaad044dca9bb8bc1f21bf30cc05d7f461cd
Xu Liangyu <xuliangyu@loongson.cn>
2024-05-15 09:22:26 +0800

    Update src/coreclr/jit/lsraloongarch64.cpp
    Co-authored-by: Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>

--- src/coreclr/jit/lsraloongarch64.cpp
+++ src/coreclr/jit/lsraloongarch64.cpp
@@ -723,7 +723,7 @@ int LinearScan::BuildCall(GenTreeCall* call)
         {
             // Fast tail call - make sure that call target is always computed in volatile registers
             // that will not be overridden by epilog sequence.
-            ctrlExprCandidates = (allRegs(TYP_INT) & RBM_INT_CALLEE_TRASH) ^ RBM_GSCOOKIE_TMP;
+            ctrlExprCandidates = (allRegs(TYP_INT) & (RBM_INT_CALLEE_TRASH & ~RBM_GSCOOKIE_TMP));
             assert(ctrlExprCandidates != RBM_NONE);
         }
     }
