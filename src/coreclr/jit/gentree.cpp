d01738b73c0037aab12a3ba2c095d3b8b006132a
TIHan <lol.tihan@gmail.com>
2024-05-14 18:13:14 -0700

    Remove layout

--- src/coreclr/jit/gentree.cpp
+++ src/coreclr/jit/gentree.cpp
@@ -27025,7 +27025,6 @@ ClassLayout* GenTreeHWIntrinsic::GetLayout(Compiler* compiler) const
         case NI_AdvSimd_LoadVector64x2:
         case NI_AdvSimd_LoadAndInsertScalarVector64x2:
         case NI_AdvSimd_LoadAndReplicateToVector64x2:
-        case NI_Sve_LoadVectorx2:
             return compiler->typGetBlkLayout(16);
 
         case NI_AdvSimd_Arm64_LoadPairVector128:
@@ -27038,14 +27037,12 @@ ClassLayout* GenTreeHWIntrinsic::GetLayout(Compiler* compiler) const
         case NI_AdvSimd_Arm64_LoadAndReplicateToVector128x2:
         case NI_AdvSimd_Arm64_LoadAndInsertScalarVector128x2:
         case NI_AdvSimd_LoadAndInsertScalarVector64x4:
-        case NI_Sve_LoadVectorx4:
             return compiler->typGetBlkLayout(32);
 
         case NI_AdvSimd_LoadVector64x3AndUnzip:
         case NI_AdvSimd_LoadVector64x3:
         case NI_AdvSimd_LoadAndInsertScalarVector64x3:
         case NI_AdvSimd_LoadAndReplicateToVector64x3:
-        case NI_Sve_LoadVectorx3:
             return compiler->typGetBlkLayout(24);
 
         case NI_AdvSimd_Arm64_LoadVector128x3AndUnzip:
