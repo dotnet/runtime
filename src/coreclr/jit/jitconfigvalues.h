00b4b6d2b09465b52376d3e3b163c90951a865cf
Aman Khalid (from Dev Box) <amankhalid@microsoft.com>
2024-05-16 14:08:54 -0400

    Enable RPO-based block layout by default

--- src/coreclr/jit/jitconfigvalues.h
+++ src/coreclr/jit/jitconfigvalues.h
@@ -771,7 +771,7 @@ RELEASE_CONFIG_INTEGER(JitEnablePhysicalPromotion, W("JitEnablePhysicalPromotion
 RELEASE_CONFIG_INTEGER(JitEnableCrossBlockLocalAssertionProp, W("JitEnableCrossBlockLocalAssertionProp"), 1)
 
 // Do greedy RPO-based layout in Compiler::fgReorderBlocks.
-RELEASE_CONFIG_INTEGER(JitDoReversePostOrderLayout, W("JitDoReversePostOrderLayout"), 0);
+RELEASE_CONFIG_INTEGER(JitDoReversePostOrderLayout, W("JitDoReversePostOrderLayout"), 1);
 
 // JitFunctionFile: Name of a file that contains a list of functions. If the currently compiled function is in the
 // file, certain other JIT config variables will be active. If the currently compiled function is not in the file,
