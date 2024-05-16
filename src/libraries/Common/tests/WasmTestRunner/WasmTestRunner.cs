7189a0e9c201adcb2566d82ef111dcf932b66d7c
pavelsavara <pavel.savara@gmail.com>
2024-05-16 11:20:00 +0200

    more

--- src/libraries/Common/tests/WasmTestRunner/WasmTestRunner.cs
+++ src/libraries/Common/tests/WasmTestRunner/WasmTestRunner.cs
@@ -29,7 +29,6 @@ public class WasmTestRunner : WasmApplicationEntryPoint
         var includedNamespaces = new List<string>();
         var includedClasses = new List<string>();
         var includedMethods = new List<string>();
-        var backgroundExec = false;
         var untilFailed = false;
 
         for (int i = 1; i < args.Length; i++)
@@ -91,14 +90,7 @@ public class WasmTestRunner : WasmApplicationEntryPoint
         var res = 0;
         do
         {
-            if (backgroundExec)
-            {
-                res = await Task.Run(() => runner.Run());
-            }
-            else
-            {
-                res = await runner.Run();
-            }
+            res = await runner.Run();
         }
         while(res == 0 && untilFailed);
 
