b15e35138eaa43e242d5eaa8680f9cb3367b5ef6
Michael Sharp <51342856+michaelgsharp@users.noreply.github.com>
2024-05-15 22:57:29 -0600

    Tensor wave 1 API's. (#101196)
    * Native Index/Range and ref.
    
    * SpanND without slice
    
    * index/range testing and implicit conversions
    
    * SpanND Tests
    
    * tensor working, still need more statics
    
    * more tensors updates
    
    * ref files updated
    
    * final ref update
    
    * span updates
    
    * all but broadcast and some TensorPrimitives
    
    * broadcast in
    
    * organizational changes
    
    * ref and implicit broadcast
    
    * build failures
    
    * updates from PR comments
    
    * error text moved to strings.resx
    
    * exception strings moved to strings.resc
    
    * comments from PR
    
    * more fixes from PR and API review
    
    * rebase on main. XML comments. API updates
    
    * NIndex,NRange,RO/TensorSpan API updates
    
    * IROTensor,ITensor,Tensor API updates
    
    * changes from pr comments
    
    * changes from PR comments
    
    * fixed test failure

--- src/libraries/System.Numerics.Tensors/tests/Helpers.cs
+++ src/libraries/System.Numerics.Tensors/tests/Helpers.cs
@@ -4,10 +4,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Numerics.Tensors;
+using Xunit;
 
 namespace System.Numerics.Tensors.Tests
 {
-    internal static class Helpers
+    public static class Helpers
     {
         public static IEnumerable<int> TensorLengthsIncluding0 => Enumerable.Range(0, 257);
 
@@ -87,5 +89,33 @@ namespace System.Numerics.Tensors.Tests
 #endif
             return null;
         }
+
+#if NETCOREAPP
+        public delegate void AssertThrowsAction<T>(TensorSpan<T> span);
+
+        // Cannot use standard Assert.Throws() when testing Span - Span and closures don't get along.
+        public static void AssertThrows<E, T>(TensorSpan<T> span, AssertThrowsAction<T> action) where E : Exception
+        {
+            try
+            {
+                action(span);
+                Assert.Fail($"Expected exception: {typeof(E)}");
+            }
+            catch (Exception ex)
+            {
+                Assert.True(ex is E, $"Wrong exception thrown. Expected: {typeof(E)} Actual: {ex.GetType()}");
+            }
+        }
+
+        public static void AdjustIndices(int curIndex, nint addend, ref nint[] curIndices, ReadOnlySpan<nint> lengths)
+        {
+            if (addend == 0 || curIndex < 0)
+                return;
+            curIndices[curIndex] += addend;
+            AdjustIndices(curIndex - 1, curIndices[curIndex] / lengths[curIndex], ref curIndices, lengths);
+            curIndices[curIndex] = curIndices[curIndex] % lengths[curIndex];
+        }
+
+#endif
     }
 }
