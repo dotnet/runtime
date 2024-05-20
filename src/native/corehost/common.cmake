224fe0bd964c0130ba073faefe0ce1cd8db7e994
Jeremy Koritzinsky <jekoritz@microsoft.com>
2024-05-20 14:54:40 -0700

    Move static hostfxr away from lib_static

--- src/native/corehost/common.cmake
+++ src/native/corehost/common.cmake
@@ -22,7 +22,6 @@ if(CLR_CMAKE_TARGET_WIN32)
 endif()
 
 function(set_common_libs TargetType)
-
     # Libraries used for exe projects
     if (${TargetType} STREQUAL "exe")
         if((CLR_CMAKE_TARGET_LINUX OR CLR_CMAKE_TARGET_FREEBSD) AND NOT CLR_CMAKE_TARGET_ANDROID)
@@ -30,16 +29,14 @@ function(set_common_libs TargetType)
         endif()
     endif()
 
-    if (NOT ${TargetType} STREQUAL "lib-static")
-        # Specify the import library to link against for Arm32 build since the default set is minimal
-        if (CLR_CMAKE_TARGET_ARCH_ARM)
-            if (CLR_CMAKE_TARGET_WIN32)
-                target_link_libraries(${DOTNET_PROJECT_NAME} PRIVATE shell32.lib advapi32.lib)
-            else()
-                target_link_libraries(${DOTNET_PROJECT_NAME} PRIVATE atomic.a)
-            endif()
+    # Specify the import library to link against for Arm32 build since the default set is minimal
+    if (CLR_CMAKE_TARGET_ARCH_ARM)
+        if (CLR_CMAKE_TARGET_WIN32)
+            target_link_libraries(${DOTNET_PROJECT_NAME} PRIVATE shell32.lib advapi32.lib)
+        else()
+            target_link_libraries(${DOTNET_PROJECT_NAME} PRIVATE atomic.a)
         endif()
-
-        target_link_libraries (${DOTNET_PROJECT_NAME} PRIVATE ${CMAKE_DL_LIBS})
     endif()
+
+    target_link_libraries (${DOTNET_PROJECT_NAME} PRIVATE ${CMAKE_DL_LIBS})
 endfunction()
