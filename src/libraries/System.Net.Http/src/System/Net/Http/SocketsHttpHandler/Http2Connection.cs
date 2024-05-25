73d8e2e5c15e3764c4cbd516ced42601c3cc6acb
pedrobsaila <badrebsaila@outlook.com>
2024-05-25 23:54:28 +0200

    fix remarks

--- src/libraries/System.Net.Http/src/System/Net/Http/SocketsHttpHandler/Http2Connection.cs
+++ src/libraries/System.Net.Http/src/System/Net/Http/SocketsHttpHandler/Http2Connection.cs
@@ -2136,7 +2136,7 @@ namespace System.Net.Http
                     break;
                 case KeepAliveState.PingSent:
                     if (now > _keepAlivePingTimeoutTimestamp)
-                        ThrowProtocolError(Http2ProtocolErrorCode.ProtocolError, SR.Format(SR.net_ping_request_timed_out, _keepAlivePingTimeoutTimestamp));
+                        ThrowProtocolError(Http2ProtocolErrorCode.ProtocolError, SR.net_ping_request_timed_out);
                     break;
                 default:
                     Debug.Fail($"Unexpected keep alive state ({_keepAliveState})");
