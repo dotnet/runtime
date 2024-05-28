da9369dd8c53497aba5e6c5d3a6c424ba8511e92
ieviev <ieviev@users.noreply.github.com>
2024-05-28 16:12:20 +0300

    low memory variant

--- src/libraries/System.Text.RegularExpressions/src/System/Text/RegularExpressions/Symbolic/MintermClassifier.cs
+++ src/libraries/System.Text.RegularExpressions/src/System/Text/RegularExpressions/Symbolic/MintermClassifier.cs
@@ -20,8 +20,10 @@ namespace System.Text.RegularExpressions.Symbolic
     /// </remarks>
     internal sealed class MintermClassifier
     {
+        private static readonly int[] s_emptyLookup = new int[ushort.MaxValue + 1];
         /// <summary>An array used to map characters to minterms</summary>
         private readonly int[] _lookup;
+        private readonly bool _isAsciiOnly;
 
         /// <summary>Create a classifier that maps a character to the ID of its associated minterm.</summary>
         /// <param name="minterms">A BDD for classifying all characters (ASCII and non-ASCII) to their corresponding minterm IDs.</param>
@@ -29,16 +31,30 @@ namespace System.Text.RegularExpressions.Symbolic
         {
             Debug.Assert(minterms.Length > 0, "Requires at least");
 
-            int[] lookup = new int[ushort.MaxValue + 1];
+
             if (minterms.Length == 1)
             {
                 // With only a single minterm, the mapping is trivial: everything maps to it (ID 0).
-                _lookup = lookup;
+                _lookup = s_emptyLookup;
                 return;
             }
 
+            // low memory variant could create an ascii-only array
+            // cheaper to iterate twice than allocate an array and potentially not use it
+            _isAsciiOnly = true;
+            for (int mintermId = 1; mintermId < minterms.Length; mintermId++)
+            {
+                (uint, uint)[] mintermRanges = BDDRangeConverter.ToRanges(minterms[mintermId]);
+                if (mintermRanges[^1].Item2 >= 128)
+                {
+                    _isAsciiOnly = false;
+                }
+            }
+
+
             // assign minterm category for every char
             // unused characters in minterm 0 get mapped to zero
+            int[] lookup = new int[_isAsciiOnly ? 128 : 65536];
             for (int mintermId = 1; mintermId < minterms.Length; mintermId++)
             {
                 // precompute all assigned minterm categories
@@ -57,8 +73,16 @@ namespace System.Text.RegularExpressions.Symbolic
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public int GetMintermID(int c)
         {
+            if (_isAsciiOnly && (c >= 128))
+            {
+                return 0;
+            }
+            // high performance variant would use a span directly
+            // but this is not possible in low memory constraints
             return _lookup[c];
         }
-        public int[] Lookup => _lookup;
+
+        // [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        // public Span<int> LookupSpan() => _lookup.AsSpan();
     }
 }
