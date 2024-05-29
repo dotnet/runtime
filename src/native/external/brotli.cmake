c46e058394b61340d39955429e1b118bce447f19
Jeremy Koritzinsky <jekoritz@microsoft.com>
2024-05-29 11:49:38 -0700

    Integrate FetchContent-based discovery with system-brotli discovery via CMake's PkgConfig model

--- src/native/external/brotli.cmake
+++ src/native/external/brotli.cmake
@@ -1,37 +1,17 @@
-# IMPORTANT: do not use add_compile_options(), add_definitions() or similar functions here since it will leak to the including projects
+include(FetchContent)
 
-include_directories(BEFORE "${CMAKE_CURRENT_LIST_DIR}/brotli/include")
-
-set (BROTLI_SOURCES_BASE
-    common/constants.c
-    common/context.c
-    common/dictionary.c
-    common/platform.c
-    common/transform.c
-    dec/bit_reader.c
-    dec/decode.c
-    dec/huffman.c
-    dec/state.c
-    enc/backward_references.c
-    enc/backward_references_hq.c
-    enc/bit_cost.c
-    enc/block_splitter.c
-    enc/brotli_bit_stream.c
-    enc/cluster.c
-    enc/command.c
-    enc/compress_fragment.c
-    enc/compress_fragment_two_pass.c
-    enc/dictionary_hash.c
-    enc/encode.c
-    enc/encoder_dict.c
-    enc/entropy_encode.c
-    enc/fast_log.c
-    enc/histogram.c
-    enc/literal_cost.c
-    enc/memory.c
-    enc/metablock.c
-    enc/static_dict.c
-    enc/utf8_util.c
+FetchContent_Declare(
+    brotli
+    SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/brotli
 )
 
-addprefix(BROTLI_SOURCES "${CMAKE_CURRENT_LIST_DIR}/brotli" "${BROTLI_SOURCES_BASE}")
+set(BROTLI_DISABLE_TESTS ON)
+set(__CURRENT_BUILD_SHARED_LIBS ${BUILD_SHARED_LIBS})
+set(BUILD_SHARED_LIBS OFF)
+FetchContent_MakeAvailable(brotli)
+set(BUILD_SHARED_LIBS ${__CURRENT_BUILD_SHARED_LIBS})
+target_compile_options(brotlicommon PRIVATE $<$<COMPILE_LANG_AND_ID:C,MSVC>:/guard:cf>)
+target_compile_options(brotlienc PRIVATE $<$<COMPILE_LANG_AND_ID:C,MSVC>:/guard:cf>)
+target_compile_options(brotlidec PRIVATE $<$<COMPILE_LANG_AND_ID:C,MSVC>:/guard:cf>)
+target_compile_options(brotlidec PRIVATE $<$<COMPILE_LANG_AND_ID:C,MSVC>:/wd4334>) # result of 32-bit shift implicitly converted to 64 bits (was 64-bit shift intended?)
+target_compile_options(brotlienc PRIVATE $<$<COMPILE_LANG_AND_ID:C,MSVC>:/wd4334>) # result of 32-bit shift implicitly converted to 64 bits (was 64-bit shift intended?)
