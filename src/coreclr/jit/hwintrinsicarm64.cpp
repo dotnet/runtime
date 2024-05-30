d3bc07452c787b2d33ba4c292c7200344f25649f
Tanner Gooding <tagoo@outlook.com>
2024-05-30 07:09:23 -0700

    Ensure Arm64 uses ||

--- src/coreclr/jit/hwintrinsicarm64.cpp
+++ src/coreclr/jit/hwintrinsicarm64.cpp
@@ -1842,7 +1842,7 @@ GenTree* Compiler::impSpecialIntrinsic(NamedIntrinsic        intrinsic,
 
             GenTree* indices = impStackTop(0).val;
 
-            if (!indices->IsVectorConst() && !IsValidForShuffle(indices->AsVecCon(), simdSize, simdBaseType))
+            if (!indices->IsVectorConst() || !IsValidForShuffle(indices->AsVecCon(), simdSize, simdBaseType))
             {
                 assert(sig->numArgs == 2);
 
