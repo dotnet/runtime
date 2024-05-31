d4464d7ae6d99d75ff89309fb56fd2a5a8f0c845
Tarek Mahmoud Sayed <tarekms@microsoft.com>
2024-05-31 10:51:37 -0700

    Remove un-needed text from the comment

--- src/libraries/System.Diagnostics.DiagnosticSource/src/System/Diagnostics/Activity.cs
+++ src/libraries/System.Diagnostics.DiagnosticSource/src/System/Diagnostics/Activity.cs
@@ -530,7 +530,7 @@ namespace System.Diagnostics
         /// <para>- Any registered <see cref="ActivityListener"/> with the <see cref="ActivityListener.ExceptionRecorder"/> callback will be notified about this exception addition
         /// before the <see cref="ActivityEvent" /> object is added to the <see cref="Events" /> list.</para>
         /// <para>- Any registered <see cref="ActivityListener"/> with the <see cref="ActivityListener.ExceptionRecorder"/> callback that adds "exception.message", "exception.stacktrace", or "exception.type" tags
-        /// will not have these tags overwritten, except by any subsequent <see cref="ActivityListener"/> that explicitly overwrites them.</para>        public Activity AddException(Exception exception, TagList tags = default, DateTimeOffset timestamp = default)
+        /// will not have these tags overwritten, except by any subsequent <see cref="ActivityListener"/> that explicitly overwrites them.</para>
         /// </remarks>
         public Activity AddException(Exception exception, TagList tags = default, DateTimeOffset timestamp = default)
         {
