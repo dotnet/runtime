e9b25eba724e800cb8c1b8c1a401c9e861e135a9
pedrobsaila <badrebsaila@outlook.com>
2024-06-01 21:58:59 +0200

    HttpIOException when establishing connection, unobserved task exception

--- src/libraries/System.Net.Http/src/System/Net/Http/SocketsHttpHandler/Http2Connection.cs
+++ src/libraries/System.Net.Http/src/System/Net/Http/SocketsHttpHandler/Http2Connection.cs
@@ -226,6 +226,7 @@ namespace System.Net.Http
                 // Processing the incoming frames before sending the client preface and SETTINGS is necessary when using a NamedPipe as a transport.
                 // If the preface and SETTINGS coming from the server are not read first the below WriteAsync and the ProcessIncomingFramesAsync fall into a deadlock.
                 _ = ProcessIncomingFramesAsync();
+                LogExceptions(InitialSettingsReceived.Task);
                 await _stream.WriteAsync(_outgoingBuffer.ActiveMemory, cancellationToken).ConfigureAwait(false);
                 _rttEstimator.OnInitialSettingsSent();
                 _outgoingBuffer.ClearAndReturnBuffer();
