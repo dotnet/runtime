657401d7361654e7b4c05ee02bcb83f80cbffc3c
xuliangyu <xuliangyu@loongson.cn>
2024-06-04 20:44:05 +0800

    [LoongArch64] Fix some failed R2R testcases.

--- src/coreclr/jit/gentree.h
+++ src/coreclr/jit/gentree.h
@@ -5610,7 +5610,7 @@ struct GenTreeCall final : public GenTree
             return WellKnownArg::VirtualStubCell;
         }
 
-#if defined(TARGET_ARMARCH) || defined(TARGET_RISCV64)
+#if defined(TARGET_ARMARCH) || defined(TARGET_RISCV64) || defined(TARGET_LOONGARCH64)
         // For ARM architectures, we always use an indirection cell for R2R calls.
         if (IsR2RRelativeIndir() && !IsDelegateInvoke())
         {
