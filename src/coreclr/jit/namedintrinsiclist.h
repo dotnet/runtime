1e0c24330c4074de89d13bca21b3b670f051f6eb
Aaron Robinson <arobins@microsoft.com>
2024-06-05 11:00:25 -0700

    Convert `RuntimeHelpers.IsReferenceOrContainsReferences` to JIT intrinsic (#103010)
    * Convert IsReferenceOrContainsReferences to JIT intrinsic
    
    Collapse all to a single definition across the runtimes.

--- src/coreclr/jit/namedintrinsiclist.h
+++ src/coreclr/jit/namedintrinsiclist.h
@@ -109,6 +109,7 @@ enum NamedIntrinsic : unsigned short
     NI_System_Runtime_CompilerServices_RuntimeHelpers_CreateSpan,
     NI_System_Runtime_CompilerServices_RuntimeHelpers_InitializeArray,
     NI_System_Runtime_CompilerServices_RuntimeHelpers_IsKnownConstant,
+    NI_System_Runtime_CompilerServices_RuntimeHelpers_IsReferenceOrContainsReferences,
 
     NI_System_Runtime_InteropService_MemoryMarshal_GetArrayDataReference,
 
