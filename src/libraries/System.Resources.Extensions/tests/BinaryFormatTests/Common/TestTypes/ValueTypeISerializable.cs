82aba8203f84c0a162f78dd3929b16fde3d392a4
Adam Sitnik <adam.sitnik@gmail.com>
2024-06-10 13:07:59 +0200

    Use PayloadReader in System.Resources.Extensions (#102379)
    ---------
    
    Co-authored-by: Jan Kotas <jkotas@microsoft.com>
    Co-authored-by: Jeremy Barton <jbarton@microsoft.com>

---
+++ src/libraries/System.Resources.Extensions/tests/BinaryFormatTests/Common/TestTypes/ValueTypeISerializable.cs
@@ -0,0 +1,31 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Runtime.Serialization;
+
+namespace System.Resources.Extensions.Tests.Common.TestTypes;
+
+[Serializable]
+public struct ValueTypeISerializable : ISerializable, ValueTypeBase
+{
+    public string Name { get; set; } = string.Empty;
+
+    public BinaryTreeNodeWithEventsBase? Reference { get; set; }
+
+    private ValueTypeISerializable(SerializationInfo serializationInfo, StreamingContext streamingContext)
+    {
+        Name = serializationInfo.GetString(nameof(Name))!;
+        Reference = (BinaryTreeNodeWithEventsISerializable?)serializationInfo.GetValue(nameof(Reference), typeof(BinaryTreeNodeWithEventsISerializable));
+        BinaryTreeNodeWithEventsTracker.DeserializationOrder.Add($"{Name}s");
+    }
+
+    public readonly void GetObjectData(SerializationInfo info, StreamingContext context)
+    {
+        info.AddValue(nameof(Name), Name);
+        info.AddValue(nameof(Reference), Reference);
+    }
+
+    [OnDeserialized]
+    private readonly void OnDeserialized(StreamingContext context) => BinaryTreeNodeWithEventsTracker.DeserializationOrder.Add($"{Name}p");
+    public readonly void OnDeserialization(object? sender) => BinaryTreeNodeWithEventsTracker.DeserializationOrder.Add($"{Name}i");
+}
