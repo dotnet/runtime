c4acef88bbac884c23ed54965960e0fe103f8abf
Michael Sharp <misharp@microsoft.com>
2024-06-10 13:51:02 -0600

    fixed TensorShape issue

--- src/libraries/System.Numerics.Tensors/src/System/Numerics/Tensors/netcore/TensorShape.cs
+++ src/libraries/System.Numerics.Tensors/src/System/Numerics/Tensors/netcore/TensorShape.cs
@@ -16,24 +16,31 @@ using System.Xml.Linq;
 
 namespace System.Numerics.Tensors
 {
-    internal struct TensorShape
+    public readonly struct TensorShape
     {
-        internal nint[]? _metadata;      // 8 bytes
+        internal readonly nint[]? _metadata;      // 8 bytes
 
-#pragma warning disable CA1823 // Avoid unused private fields
-        internal nint _memoryLength;   // 8 bytes
-#pragma warning restore CA1823 // Avoid unused private fields
-        internal int _rank;              // 4 bytes
+        internal readonly nint _memoryLength;   // 8 bytes
+        internal readonly int _rank;              // 4 bytes
 
-        private LengthsBuffer _lengths;
-        private StridesBuffer _strides;
+        private readonly LengthsBuffer _lengths;
+        private readonly StridesBuffer _strides;
 
         internal TensorShape(nint memoryLength, ReadOnlySpan<nint> lengths, ReadOnlySpan<nint> strides)
         {
             _memoryLength = memoryLength;
             _rank = Lengths.Length;
-            lengths.CopyTo(_lengths);
-            strides.CopyTo(_strides);
+            if (lengths.Length > 5)
+            {
+                _metadata = new nint[lengths.Length + strides.Length];
+                lengths.CopyTo(MemoryMarshal.CreateSpan(ref _metadata[0], lengths.Length));
+                strides.CopyTo(MemoryMarshal.CreateSpan(ref _metadata[lengths.Length], strides.Length));
+            }
+            else
+            {
+                lengths.CopyTo(_lengths);
+                strides.CopyTo(_strides);
+            }
         }
 
         [InlineArray(5)] // 5x8 bytes (40)
@@ -49,10 +56,16 @@ namespace System.Numerics.Tensors
         }
 
         [UnscopedRef]
-        public ReadOnlySpan<nint> Lengths => (_metadata is null) ? ((ReadOnlySpan<nint>)_lengths).Slice(0, _rank) : MemoryMarshal.CreateReadOnlySpan(in _metadata[0], _rank);
+#pragma warning disable CS9192 // Argument should be passed with 'ref' or 'in' keyword
+        public ReadOnlySpan<nint> Lengths => (_metadata is null)
+                                           ? ((ReadOnlySpan<nint>)_lengths).Slice(0, _rank)
+                                           : MemoryMarshal.CreateReadOnlySpan(MemoryMarshal.GetArrayDataReference(_metadata), _rank);
 
         [UnscopedRef]
-        public ReadOnlySpan<nint> Strides => (_metadata is null) ? ((ReadOnlySpan<nint>)_strides).Slice(0, _rank) : MemoryMarshal.CreateReadOnlySpan(in _metadata[_metadata.Length / 2], _rank);
+        public ReadOnlySpan<nint> Strides => (_metadata is null)
+                                           ? ((ReadOnlySpan<nint>)_strides).Slice(0, _rank)
+                                           : MemoryMarshal.CreateReadOnlySpan(MemoryMarshal.GetArrayDataReference(_metadata), _rank).Slice(_rank / 2);
+#pragma warning restore CS9192 // Argument should be passed with 'ref' or 'in' keyword
 
         public nint FlattenedLength => TensorSpanHelpers.CalculateTotalLength(Lengths);
     }
