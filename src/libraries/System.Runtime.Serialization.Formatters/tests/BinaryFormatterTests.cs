7ab201156d03240d660c2ff291274f7f995f5743
Adam Sitnik <adsitnik@microsoft.com>
2024-06-11 16:37:25 +0000

    Bypassing Serialization Binders with BinaryFormatter Mutation

--- src/libraries/System.Runtime.Serialization.Formatters/tests/BinaryFormatterTests.cs
+++ src/libraries/System.Runtime.Serialization.Formatters/tests/BinaryFormatterTests.cs
@@ -536,6 +536,25 @@ namespace System.Runtime.Serialization.Formatters.Tests
             BinaryFormatterHelpers.Clone(Array.CreateInstance(typeof(uint[]), new[] { 5 }, new[] { 1 }));
         }
 
+        [Fact]
+        [SkipOnTargetFramework(TargetFrameworkMonikers.NetFramework, ".NET Framework has not been patched yet.")]
+        public void BypassingSerializationBinders()
+        {
+            Tuple<IComparable, object> tuple = new Tuple<IComparable, object>(42, new byte[] { 1, 2, 3, 4 });
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            using (MemoryStream stream = new MemoryStream())
+            {
+                formatter.Serialize(stream, tuple);
+
+                stream.Position = 0;
+
+                Tuple<IComparable, object> deserialized = (Tuple<IComparable, object>)formatter.Deserialize(stream);
+                Assert.Equal(tuple.Item1, deserialized.Item1);
+                Assert.Equal(tuple.Item2, deserialized.Item2);
+            }
+        }
+
         private static void ValidateEqualityComparer(object obj)
         {
             Type objType = obj.GetType();
