d69488f882d53884e49585d5f6e585fb1c1349c5
Jeremy Barton <jbarton@microsoft.com>
2024-06-12 17:00:35 -0700

    Teach PlatformDetection.IsBinaryFormatterSupported about asmver 8.1

--- src/libraries/Common/tests/TestUtilities/System/PlatformDetection.cs
+++ src/libraries/Common/tests/TestUtilities/System/PlatformDetection.cs
@@ -135,7 +135,9 @@ namespace System
         public static bool IsThreadingSupported => (!IsWasi && !IsBrowser) || IsWasmThreadingSupported;
         public static bool IsWasmThreadingSupported => IsBrowser && IsEnvironmentVariableTrue("IsBrowserThreadingSupported");
         public static bool IsNotWasmThreadingSupported => !IsWasmThreadingSupported;
-        public static bool IsBinaryFormatterSupported => IsNotMobile && !IsNativeAot;
+
+        private static readonly Lazy<bool> s_isBinaryFormatterSupported = new Lazy<bool>(DetermineBinaryFormatterSupport);
+        public static bool IsBinaryFormatterSupported => s_isBinaryFormatterSupported.Value;
 
         public static bool IsStartingProcessesSupported => !IsiOS && !IstvOS;
 
@@ -720,6 +722,38 @@ namespace System
             }
 
             return false;
+        }
+
+        private static bool DetermineBinaryFormatterSupport()
+        {
+            Assembly assembly = typeof(System.Runtime.Serialization.Formatters.Binary.BinaryFormatter).Assembly;
+            AssemblyName name = assembly.GetName();
+            Version assemblyVersion = name.Version;
+
+            if (IsNetFramework)
+            {
+                return true;
+            }
+
+            if (IsNativeAot)
+            {
+                Console.WriteLine("BinaryFormatter is disabled in NativeAOT");
+                return false;
+            }
+
+            bool isSupported = true;
+
+            // Version 8.1 is the version in the shared runtime (.NET 9+) that has the type disabled with no config.
+            // Assembly versions beyond 8.1 are the fully functional version from NuGet.
+            // Assembly versions before 8.1 probably won't be encountered, since that's the past.
+
+            if (assemblyVersion.Major == 8 && assemblyVersion.Minor == 1)
+            {
+                isSupported = false;
+            }
+
+            Console.WriteLine($"BinaryFormatter is from assembly version {assemblyVersion}, supported={isSupported}");
+            return isSupported;
         }
     }
 }
--- src/libraries/System.Runtime.Serialization.Formatters/tests/TestConfiguration.cs
+++ src/libraries/System.Runtime.Serialization.Formatters/tests/TestConfiguration.cs
@@ -1,9 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Reflection;
-using System.Runtime.Serialization.Formatters.Binary;
-
 namespace System.Runtime.Serialization.Formatters.Tests
 {
     public static class TestConfiguration
@@ -16,44 +13,28 @@ namespace System.Runtime.Serialization.Formatters.Tests
 
         static TestConfiguration()
         {
-            Assembly assembly = typeof(BinaryFormatter).Assembly;
-            AssemblyName name = assembly.GetName();
-            Version assemblyVersion = name.Version;
-
             if (!PlatformDetection.IsBinaryFormatterSupported)
             {
-                Console.WriteLine("BinaryFormatter is disabled by the platform");
                 return;
             }
 
+            IsBinaryFormatterSupported = true;
+            IsBinaryFormatterEnabled = true;
+
             if (PlatformDetection.IsNetFramework)
             {
-                IsBinaryFormatterSupported = IsBinaryFormatterEnabled = true;
                 Console.WriteLine("BinaryFormatter is always enabled by the platform (netfx).");
                 return;
             }
 
-            // Version 8.1 is the version in the shared runtime (.NET 9+) that has the type disabled with no config.
-            // Assembly versions beyond 8.1 are the fully functional version from NuGet.
-            // Assembly versions before 8.1 probably won't be encountered by this test library.
+            IsFeatureSwitchRespected = true;
 
-            if (assemblyVersion.Major == 8 && assemblyVersion.Minor == 1)
+            if (!AppContext.TryGetSwitch(EnableBinaryFormatterSwitchName, out IsBinaryFormatterEnabled))
             {
                 IsBinaryFormatterEnabled = false;
             }
-            else
-            {
-                IsBinaryFormatterSupported = IsBinaryFormatterEnabled = true;
 
-                IsFeatureSwitchRespected = true;
-
-                if (!AppContext.TryGetSwitch(EnableBinaryFormatterSwitchName, out IsBinaryFormatterEnabled))
-                {
-                    IsBinaryFormatterEnabled = false;
-                }
-            }
-
-            Console.WriteLine($"BinaryFormatter is from assembly version {assemblyVersion}, enabled={IsBinaryFormatterEnabled} supported={IsBinaryFormatterSupported}");
+            Console.WriteLine($"BinaryFormatter is enabled in AppConfig: {IsBinaryFormatterEnabled}");
         }
     }
 }
