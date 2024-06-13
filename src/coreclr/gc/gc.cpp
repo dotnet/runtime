af4ef87b2fb714e2dc1285afe60491469d51c797
Mark Plesko <markples@microsoft.com>
2024-06-13 11:40:12 -0700

    draft redist and age

--- src/coreclr/gc/gc.cpp
+++ src/coreclr/gc/gc.cpp
@@ -38195,6 +38195,27 @@ void gc_heap::background_mark_phase ()
 
     bgc_alloc_lock->check();
 
+    distribute_free_regions ();
+
+#ifdef MULTIPLE_HEAPS
+    for (int i = 0; i < gc_heap::n_heaps; i++)
+    {
+        gc_heap* hp = gc_heap::g_heaps[i];
+#else //MULTIPLE_HEAPS
+    {
+        gc_heap* hp = pGenGCHeap;
+        const int i = 0;
+#endif //MULTIPLE_HEAPS
+
+        hp->descr_generations ("BGC"); //string value?
+#ifdef USE_REGIONS
+        assert (settings.condemned_generation == max_generation); // not needed - 10 lines above
+        // age and print all kinds of free regions //! large and huge only?
+        region_free_list::age_free_regions (hp->free_regions);
+        region_free_list::print (hp->free_regions, i, "BGC");
+#endif //USE_REGIONS
+    }
+
     current_bgc_state = bgc_final_marking;
 
     //concurrent_print_time_delta ("concurrent marking ended");
