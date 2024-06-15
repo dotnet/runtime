a5c528e95797684ce5092bc477cc0e8cd8be0457
Aman Khalid (from Dev Box) <amankhalid@microsoft.com>
2024-06-14 22:46:38 -0400

    Fix build

--- src/coreclr/jit/fgopt.cpp
+++ src/coreclr/jit/fgopt.cpp
@@ -4582,7 +4582,7 @@ void Compiler::fgMoveHotJumps()
 
         // Don't bother trying to move cold blocks
         //
-        if (block->isBBWeightCold())
+        if (block->isBBWeightCold(this))
         {
             continue;
         }
@@ -4659,7 +4659,7 @@ void Compiler::fgMoveHotJumps()
             continue;
         }
 
-        if (target->isBBWeightCold())
+        if (target->isBBWeightCold(this))
         {
             // If target is block's most-likely successor, and block is not rarely-run,
             // perhaps the profile data is misleading, and we need to run profile repair?
