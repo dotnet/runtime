07b417f075a41e355bd6f2fe474f3f0597c4600c
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2024-06-17 12:16:17 +0200

    Add test for SwiftIndirectResult

---
+++ src/tests/Interop/Swift/SwiftIndirectResult/SwiftIndirectResult.cs
@@ -0,0 +1,52 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Swift;
+using Xunit;
+
+public class SwiftIndirectResultTests
+{
+	private struct NonFrozenStruct
+	{
+		public int A;
+		public int B;
+		public int C;
+	}
+	
+    private const string SwiftLib = "libSwiftIndirectResult.dylib";
+
+    [UnmanagedCallConv(CallConvs = new Type[] { typeof(CallConvSwift) })]
+    [DllImport(SwiftLib, EntryPoint = "$s6output21ReturnNonFrozenStruct1a1b1cAA0cdE0Vs5Int32V_A2ItF")]
+    public static extern void ReturnNonFrozenStruct(SwiftIndirectResult result, int a, int b, int c);
+
+	[UnmanagedCallConv(CallConvs = new Type[] { typeof(CallConvSwift) })]
+    [DllImport(SwiftLib, EntryPoint = "$s6output26SumReturnedNonFrozenStruct1fs5Int32VAA0deF0VyXE_tF")]
+    public unsafe static extern int SumReturnedNonFrozenStruct(delegate*[Swift]<SwiftIndirectResult, SwiftSelf, void> , void* funcContext);
+
+    [Fact]
+    public unsafe static void TestReturnNonFrozenStruct()
+    {
+		// In normal circumstances this instance would have unknown/dynamically determined size.
+		NonFrozenStruct instance;
+		ReturnNonFrozenStruct(&instance, 10, 20, 30);
+		Assert.Equal(10, instance.A);
+		Assert.Equal(20, instance.B);
+		Assert.Equal(30, instance.C);
+    }
+
+    [UnmanagedCallersOnly(CallConvs = new Type[] { typeof(CallConvSwift) })]
+    private static void ReversePInvokeReturnNonFrozenStruct(SwiftIndirectResult result, SwiftSelf self)
+	{
+		*(NonFrozenStruct*)result.Value = new NonFrozenStruct { A = 10, B = 20, C = 30 };
+	}
+
+    [Fact]
+    public unsafe static void TestSumReturnedNonFrozenStruct()
+    {
+		int result = SumReturnedNonFrozenStruct(&ReversePInvokeReturnNonFrozenStruct, null);
+		Assert.Equal(60, result);
+    }
+}
