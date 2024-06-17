1eb2bad5c2483017c71a420f8716423144e2b07f
Jan Kotas <jkotas@microsoft.com>
2024-06-17 10:57:51 -0700

    Improve test

--- src/libraries/System.Runtime/tests/System.Runtime.Extensions.Tests/System/Environment.SetEnvironmentVariable.cs
+++ src/libraries/System.Runtime/tests/System.Runtime.Extensions.Tests/System/Environment.SetEnvironmentVariable.cs
@@ -284,6 +284,7 @@ namespace System.Tests
                 Environment.SetEnvironmentVariable(varName, value);
                 Environment.SetEnvironmentVariable(varName, null);
                 Assert.Null(Environment.GetEnvironmentVariable(varName));
+                Assert.False(Environment.GetEnvironmentVariables().ContainsKey(varName));
             });
         }
 
