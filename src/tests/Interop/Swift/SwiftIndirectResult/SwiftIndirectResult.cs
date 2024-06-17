889a2e5d2bdece9ce822bdbefd2a9e1870e4e148
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2024-06-17 12:18:52 +0200

    Fix test

--- src/tests/Interop/Swift/SwiftIndirectResult/SwiftIndirectResult.cs
+++ src/tests/Interop/Swift/SwiftIndirectResult/SwiftIndirectResult.cs
@@ -9,44 +9,45 @@ using Xunit;
 
 public class SwiftIndirectResultTests
 {
-	private struct NonFrozenStruct
-	{
-		public int A;
-		public int B;
-		public int C;
-	}
-	
+    private struct NonFrozenStruct
+    {
+        public int A;
+        public int B;
+        public int C;
+    }
+
     private const string SwiftLib = "libSwiftIndirectResult.dylib";
 
     [UnmanagedCallConv(CallConvs = new Type[] { typeof(CallConvSwift) })]
     [DllImport(SwiftLib, EntryPoint = "$s6output21ReturnNonFrozenStruct1a1b1cAA0cdE0Vs5Int32V_A2ItF")]
     public static extern void ReturnNonFrozenStruct(SwiftIndirectResult result, int a, int b, int c);
 
-	[UnmanagedCallConv(CallConvs = new Type[] { typeof(CallConvSwift) })]
+    [UnmanagedCallConv(CallConvs = new Type[] { typeof(CallConvSwift) })]
     [DllImport(SwiftLib, EntryPoint = "$s6output26SumReturnedNonFrozenStruct1fs5Int32VAA0deF0VyXE_tF")]
-    public unsafe static extern int SumReturnedNonFrozenStruct(delegate*[Swift]<SwiftIndirectResult, SwiftSelf, void> , void* funcContext);
+    public unsafe static extern int SumReturnedNonFrozenStruct(delegate*[Swift]<SwiftIndirectResult, SwiftSelf, void> func, void* funcContext);
 
     [Fact]
     public unsafe static void TestReturnNonFrozenStruct()
     {
-		// In normal circumstances this instance would have unknown/dynamically determined size.
-		NonFrozenStruct instance;
-		ReturnNonFrozenStruct(&instance, 10, 20, 30);
-		Assert.Equal(10, instance.A);
-		Assert.Equal(20, instance.B);
-		Assert.Equal(30, instance.C);
+        // In normal circumstances this instance would have unknown/dynamically determined size.
+        NonFrozenStruct instance;
+        ReturnNonFrozenStruct(&instance, 10, 20, 30);
+        Assert.Equal(10, instance.A);
+        Assert.Equal(20, instance.B);
+        Assert.Equal(30, instance.C);
     }
 
     [UnmanagedCallersOnly(CallConvs = new Type[] { typeof(CallConvSwift) })]
     private static void ReversePInvokeReturnNonFrozenStruct(SwiftIndirectResult result, SwiftSelf self)
-	{
-		*(NonFrozenStruct*)result.Value = new NonFrozenStruct { A = 10, B = 20, C = 30 };
-	}
+    {
+        // In normal circumstances this would require using dynamically sized memcpy and members to create the struct.
+        *(NonFrozenStruct*)result.Value = new NonFrozenStruct { A = 10, B = 20, C = 30 };
+    }
 
     [Fact]
     public unsafe static void TestSumReturnedNonFrozenStruct()
     {
-		int result = SumReturnedNonFrozenStruct(&ReversePInvokeReturnNonFrozenStruct, null);
-		Assert.Equal(60, result);
+        int result = SumReturnedNonFrozenStruct(&ReversePInvokeReturnNonFrozenStruct, null);
+        Assert.Equal(60, result);
     }
 }
