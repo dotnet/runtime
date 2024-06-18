2fb5476a4126c00edc98d5ba9b9f4265dfe7affe
Aaron R Robinson <arobins@microsoft.com>
2024-06-18 08:45:20 -0700

    Review feedback.
    Add test for types where their layout is the same
    whether InlineArray is provided or not.

--- src/tests/Loader/classloader/InlineArray/InlineArrayValid.cs
+++ src/tests/Loader/classloader/InlineArray/InlineArrayValid.cs
@@ -41,7 +41,7 @@ public unsafe class Validate
 {
     // ====================== SizeOf ==============================================================
     [InlineArray(42)]
-    struct FourtyTwoBytes
+    struct FortyTwoBytes
     {
         byte b;
     }
@@ -50,11 +50,20 @@ public unsafe class Validate
     public static void Sizeof()
     {
         Console.WriteLine($"{nameof(Sizeof)}...");
-        Assert.Equal(42, sizeof(FourtyTwoBytes));
+        Assert.Equal(42, sizeof(FortyTwoBytes));
         Assert.Equal(84, sizeof(MyArray<char>));
     }
 
     // ====================== OneElement ==========================================================
+    // These types are interesting since their layouts are
+    // identical with or without the InlineArrayAttribute.
+
+    [InlineArray(1)]
+    struct OneInt
+    {
+        public int i;
+    }
+
     [InlineArray(1)]
     struct OneObj
     {
@@ -65,6 +74,7 @@ public unsafe class Validate
     public static void OneElement()
     {
         Console.WriteLine($"{nameof(OneElement)}...");
+        Assert.Equal(sizeof(int), sizeof(OneInt));
         Assert.Equal(sizeof(nint), sizeof(OneObj));
     }
 
@@ -394,9 +404,14 @@ public unsafe class Validate
         }
     }
 
-    struct StructHasInlineArrayField
+    struct StructHasFortyTwoBytesField
     {
-        FourtyTwoBytes _field;
+        FortyTwoBytes _field;
+    }
+
+    struct StructHasOneIntField
+    {
+        OneInt _field;
     }
 
     [Fact]
@@ -406,17 +421,32 @@ public unsafe class Validate
 
         Assert.Throws<NotSupportedException>(() =>
         {
-            new FourtyTwoBytes().Equals(new FourtyTwoBytes());
+            new OneInt().Equals(new OneInt());
         });
 
         Assert.Throws<NotSupportedException>(() =>
         {
-            new StructHasInlineArrayField().Equals(new StructHasInlineArrayField());
+            new StructHasOneIntField().Equals(new StructHasOneIntField());
         });
 
         Assert.Throws<NotSupportedException>(() =>
         {
-            new FourtyTwoBytes().GetHashCode();
+            new FortyTwoBytes().Equals(new FortyTwoBytes());
+        });
+
+        Assert.Throws<NotSupportedException>(() =>
+        {
+            new StructHasFortyTwoBytesField().Equals(new StructHasFortyTwoBytesField());
+        });
+
+        Assert.Throws<NotSupportedException>(() =>
+        {
+            new OneInt().GetHashCode();
+        });
+
+        Assert.Throws<NotSupportedException>(() =>
+        {
+            new FortyTwoBytes().GetHashCode();
         });
     }
 }
