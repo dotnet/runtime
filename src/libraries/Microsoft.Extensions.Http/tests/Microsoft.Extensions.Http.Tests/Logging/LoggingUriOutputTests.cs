af01fc6aefaf1db0a7e5663e7885d444349ef520
antonfirsov <antonfir@gmail.com>
2024-06-20 17:19:33 +0200

    shuffle tests

--- src/libraries/Microsoft.Extensions.Http/tests/Microsoft.Extensions.Http.Tests/Logging/LoggingUriOutputTests.cs
+++ src/libraries/Microsoft.Extensions.Http/tests/Microsoft.Extensions.Http.Tests/Logging/LoggingUriOutputTests.cs
@@ -4,8 +4,8 @@
 using System;
 using System.Linq;
 using System.Net.Http;
-using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.DotNet.RemoteExecutor;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Http.Logging;
 using Microsoft.Extensions.Logging;
@@ -16,156 +16,74 @@ namespace Microsoft.Extensions.Http.Tests.Logging
 {
     public class LoggingUriOutputTests
     {
-        [Fact]
-        public async Task LoggingHttpMessageHandler_LogsAbsoluteUri()
-        {
-            // Arrange
-            var sink = new TestSink();
-
-            var serviceCollection = new ServiceCollection();
-            serviceCollection.AddLogging();
-            serviceCollection.AddSingleton<ILoggerFactory>(new TestLoggerFactory(sink, enabled: true));
-
-            serviceCollection
-            .AddHttpClient("test")
-            .ConfigurePrimaryHttpMessageHandler(() => new TestMessageHandler());
-
-            var services = serviceCollection.BuildServiceProvider();
-
-            var client = services.GetRequiredService<IHttpClientFactory>().CreateClient("test");
-
-
-            // Act
-            var request = new HttpRequestMessage(HttpMethod.Get, "http://api.example.com/search?term=Western%20Australia");
-
-            await client.SendAsync(request);
-
-            // Assert
-            var messages = sink.Writes.ToArray();
-
-            var message = Assert.Single(messages.Where(m =>
-            {
-                return
-                    m.EventId == LoggingHttpMessageHandler.Log.EventIds.RequestStart &&
-                    m.LoggerName == "System.Net.Http.HttpClient.test.ClientHandler";
-            }));
-
-            Assert.Equal("Sending HTTP request GET http://api.example.com/search?term=Western%20Australia", message.Message);
-        }
-
-        [Fact]
-        public async Task LoggingScopeHttpMessageHandler_LogsAbsoluteUri()
-        {
-            // Arrange
-            var sink = new TestSink();
-
-            var serviceCollection = new ServiceCollection();
-            serviceCollection.AddLogging();
-            serviceCollection.AddSingleton<ILoggerFactory>(new TestLoggerFactory(sink, enabled: true));
-
-            serviceCollection
-            .AddHttpClient("test")
-            .ConfigurePrimaryHttpMessageHandler(() => new TestMessageHandler());
-
-            var services = serviceCollection.BuildServiceProvider();
-
-            var client = services.GetRequiredService<IHttpClientFactory>().CreateClient("test");
-
-
-            // Act
-            var request = new HttpRequestMessage(HttpMethod.Get, "http://api.example.com/search?term=Western%20Australia");
-
-            await client.SendAsync(request);
-
-            // Assert
-            var messages = sink.Writes.ToArray();
-
-            var message = Assert.Single(messages.Where(m =>
-            {
-                return
-                    m.EventId == LoggingScopeHttpMessageHandler.Log.EventIds.PipelineStart &&
-                    m.LoggerName == "System.Net.Http.HttpClient.test.LogicalHandler";
-            }));
-
-            Assert.Equal("Start processing HTTP request GET http://api.example.com/search?term=Western%20Australia", message.Message);
-            Assert.Equal("HTTP GET http://api.example.com/search?term=Western%20Australia", message.Scope.ToString());
-        }
-
+        [ConditionalTheory(typeof(RemoteExecutor), nameof(RemoteExecutor.IsSupported))]
+        [InlineData(false, false)]
+        [InlineData(false, true)]
 #if NET
-        [ConditionalFact(typeof(PlatformDetection), nameof(PlatformDetection.IsNetCore))]
-        public void LoggingHttpMessageHandler_LogsAbsoluteUri_Sync()
-        {
-            // Arrange
-            var sink = new TestSink();
-
-            var serviceCollection = new ServiceCollection();
-            serviceCollection.AddLogging();
-            serviceCollection.AddSingleton<ILoggerFactory>(new TestLoggerFactory(sink, enabled: true));
-
-            serviceCollection
-                .AddHttpClient("test")
-                .ConfigurePrimaryHttpMessageHandler(() => new TestMessageHandler());
-
-            var services = serviceCollection.BuildServiceProvider();
-
-            var client = services.GetRequiredService<IHttpClientFactory>().CreateClient("test");
-
-
-            // Act
-            var request = new HttpRequestMessage(HttpMethod.Get, "http://api.example.com/search?term=Western%20Australia");
-
-            client.Send(request);
-
-            // Assert
-            var messages = sink.Writes.ToArray();
-
-            var message = Assert.Single(messages.Where(m =>
-            {
-                return
-                    m.EventId == LoggingHttpMessageHandler.Log.EventIds.RequestStart &&
-                    m.LoggerName == "System.Net.Http.HttpClient.test.ClientHandler";
-            }));
-
-            Assert.Equal("Sending HTTP request GET http://api.example.com/search?term=Western%20Australia", message.Message);
-        }
-
-        [ConditionalFact(typeof(PlatformDetection), nameof(PlatformDetection.IsNetCore))]
-        public void LoggingScopeHttpMessageHandler_LogsAbsoluteUri_Sync()
-        {
-            // Arrange
-            var sink = new TestSink();
-
-            var serviceCollection = new ServiceCollection();
-            serviceCollection.AddLogging();
-            serviceCollection.AddSingleton<ILoggerFactory>(new TestLoggerFactory(sink, enabled: true));
-
-            serviceCollection
-                .AddHttpClient("test")
-                .ConfigurePrimaryHttpMessageHandler(() => new TestMessageHandler());
-
-            var services = serviceCollection.BuildServiceProvider();
-
-            var client = services.GetRequiredService<IHttpClientFactory>().CreateClient("test");
-
-
-            // Act
-            var request = new HttpRequestMessage(HttpMethod.Get, "http://api.example.com/search?term=Western%20Australia");
-
-            client.Send(request);
-
-            // Assert
-            var messages = sink.Writes.ToArray();
-
-            var message = Assert.Single(messages.Where(m =>
-            {
-                return
-                    m.EventId == LoggingScopeHttpMessageHandler.Log.EventIds.PipelineStart &&
-                    m.LoggerName == "System.Net.Http.HttpClient.test.LogicalHandler";
-            }));
-
-            Assert.Equal("Start processing HTTP request GET http://api.example.com/search?term=Western%20Australia", message.Message);
-            Assert.Equal("HTTP GET http://api.example.com/search?term=Western%20Australia", message.Scope.ToString());
-        }
+        [InlineData(true, false)]
+        [InlineData(true, true)]
 #endif
+        public async Task LogsAbsoluteUrl(bool syncApi, bool logQueryString)
+        {
+            const string BaseUri = "http://api.example.com/search";
+            const string QueryString = "term=Western%20Australia";
+            const string FullUri = $"{BaseUri}?{QueryString}";
+
+            await RemoteExecutor.Invoke(static async (syncApiStr, enableQueryStringLoggingStr) =>
+            {
+                bool syncApi = bool.Parse(syncApiStr);
+                bool enableQueryStringLogging = bool.Parse(enableQueryStringLoggingStr);
+
+                if (enableQueryStringLogging)
+                {
+                    AppContext.SetSwitch("Microsoft.Extensions.Http.LogQueryString", true);
+                }
+
+                var sink = new TestSink();
+                var serviceCollection = new ServiceCollection();
+                serviceCollection.AddLogging();
+                serviceCollection.AddSingleton<ILoggerFactory>(new TestLoggerFactory(sink, enabled: true));
+                serviceCollection.AddTransient<TestMessageHandler>();
+                serviceCollection.AddHttpClient("test").ConfigurePrimaryHttpMessageHandler<TestMessageHandler>();
+
+                var services = serviceCollection.BuildServiceProvider();
+                var factory = services.GetRequiredService<IHttpClientFactory>();
+                var client = factory.CreateClient("test");
+
+                using var request = new HttpRequestMessage(HttpMethod.Get, FullUri);
+#if NET
+                if (syncApi)
+                {
+                    client.Send(request);
+                    await Task.Yield();
+                }
+                else
+#endif
+                {
+                    _ = await client.SendAsync(request);
+                }
+
+                var pipelineStartMessage = Assert.Single(sink.Writes.Where(m =>
+                {
+                    return
+                        m.EventId == LoggingScopeHttpMessageHandler.Log.EventIds.PipelineStart &&
+                        m.LoggerName == "System.Net.Http.HttpClient.test.LogicalHandler";
+                }));
+
+                var requestStartMessage = Assert.Single(sink.Writes.Where(m =>
+                {
+                    return
+                        m.EventId == LoggingHttpMessageHandler.Log.EventIds.RequestStart &&
+                        m.LoggerName == "System.Net.Http.HttpClient.test.ClientHandler";
+                }));
+
+                string expectedUri = enableQueryStringLogging ? FullUri : BaseUri;
+
+                Assert.Equal($"HTTP GET {expectedUri}", pipelineStartMessage.Scope.ToString());
+                Assert.Equal($"Start processing HTTP request GET {expectedUri}", pipelineStartMessage.Message);
+                Assert.Equal($"Sending HTTP request GET {expectedUri}", requestStartMessage.Message);
+
+            }, syncApi.ToString(), logQueryString.ToString()).DisposeAsync();
+        }
     }
 }
