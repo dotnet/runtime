a21aee0d2e3797d0f6a85a1d26b55bb611789cc8
Alan Hayward <alan.hayward@arm.com>
2024-06-25 19:33:38 +0100

    Fix AsmOffsets.cs for windows

--- src/coreclr/System.Private.CoreLib/src/System/Runtime/ExceptionServices/AsmOffsets.cs
+++ src/coreclr/System.Private.CoreLib/src/System/Runtime/ExceptionServices/AsmOffsets.cs
@@ -30,9 +30,9 @@ class AsmOffsets
     public const int OFFSETOF__REGDISPLAY__SP = 0x958;
     public const int OFFSETOF__REGDISPLAY__ControlPC = 0x960;
 #else // TARGET_UNIX
-    public const int SIZEOF__REGDISPLAY = 0x930;
-    public const int OFFSETOF__REGDISPLAY__SP = 0x890;
-    public const int OFFSETOF__REGDISPLAY__ControlPC = 0x898;
+    public const int SIZEOF__REGDISPLAY = 0x940;
+    public const int OFFSETOF__REGDISPLAY__SP = 0x898;
+    public const int OFFSETOF__REGDISPLAY__ControlPC = 0x8a0;
 #endif // TARGET_UNIX
 #elif TARGET_ARM
     public const int SIZEOF__REGDISPLAY = 0x410;
@@ -77,9 +77,15 @@ class AsmOffsets
     public const int OFFSETOF__REGDISPLAY__ControlPC = 0xbd8;
 #endif // TARGET_UNIX
 #elif TARGET_ARM64
+#if TARGET_UNIX
     public const int SIZEOF__REGDISPLAY = 0x9f0;
     public const int OFFSETOF__REGDISPLAY__SP = 0x950;
     public const int OFFSETOF__REGDISPLAY__ControlPC = 0x958;
+#else // TARGET_UNIX
+    public const int SIZEOF__REGDISPLAY = 0x930;
+    public const int OFFSETOF__REGDISPLAY__SP = 0x890;
+    public const int OFFSETOF__REGDISPLAY__ControlPC = 0x898;
+#endif // TARGET_UNIX
 #elif TARGET_ARM
     public const int SIZEOF__REGDISPLAY = 0x408;
     public const int OFFSETOF__REGDISPLAY__SP = 0x3e8;
