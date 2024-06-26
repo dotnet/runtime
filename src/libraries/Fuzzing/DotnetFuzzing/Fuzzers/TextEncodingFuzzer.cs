6ee823ea8b8b87a06b7aab1c42295b7f218bbcce
Steve Harter <steveharter@users.noreply.github.com>
2024-06-26 10:17:27 -0500

    Remove unused #define

--- src/libraries/Fuzzing/DotnetFuzzing/Fuzzers/TextEncodingFuzzer.cs
+++ src/libraries/Fuzzing/DotnetFuzzing/Fuzzers/TextEncodingFuzzer.cs
@@ -1,8 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#define FORWARD_TO_NETFRAMEWORK
-
 using System.Buffers;
 using System.Diagnostics;
 using System.Text;
