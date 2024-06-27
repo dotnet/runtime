5399b6f39cf4bc272f892c421e88daa48e2001b6
carlossanlop <1175054+carlossanlop@users.noreply.github.com>
2024-06-27 14:42:05 -0700

    Remove FUNCTIONEXPORT

--- src/native/libs/System.IO.Compression.Native/zlib_allocator.h
+++ src/native/libs/System.IO.Compression.Native/zlib_allocator.h
@@ -4,16 +4,14 @@
 #ifdef _WIN32
     #include <stdint.h>
     #include <windows.h>
-    #define FUNCTIONEXPORT
     #define FUNCTIONCALLINGCONVENTION WINAPI
 #else
     #include "pal_types.h"
     #include "pal_compiler.h"
-    #define FUNCTIONEXPORT PALEXPORT
     #define FUNCTIONCALLINGCONVENTION
 #endif
 #include <zconf.h> // voidpf
 
-FUNCTIONEXPORT voidpf FUNCTIONCALLINGCONVENTION z_custom_calloc(voidpf opaque, unsigned items, unsigned size);
+voidpf FUNCTIONCALLINGCONVENTION z_custom_calloc(voidpf opaque, unsigned items, unsigned size);
 
-FUNCTIONEXPORT void FUNCTIONCALLINGCONVENTION z_custom_cfree(voidpf opaque, voidpf ptr);
+void FUNCTIONCALLINGCONVENTION z_custom_cfree(voidpf opaque, voidpf ptr);
