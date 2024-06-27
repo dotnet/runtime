13aeb0fde0aa5169066267e53c799d968ff653e8
Carlos Sánchez López <1175054+carlossanlop@users.noreply.github.com>
2024-06-27 15:40:42 -0700

    Simplify zlib_allocator.h

--- src/native/libs/System.IO.Compression.Native/zlib_allocator.h
+++ src/native/libs/System.IO.Compression.Native/zlib_allocator.h
@@ -1,17 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#ifdef _WIN32
-    #include <stdint.h>
-    #include <windows.h>
-    #define FUNCTIONCALLINGCONVENTION WINAPI
-#else
-    #include "pal_types.h"
-    #include "pal_compiler.h"
-    #define FUNCTIONCALLINGCONVENTION
-#endif
 #include <zconf.h> // voidpf
 
-voidpf FUNCTIONCALLINGCONVENTION z_custom_calloc(voidpf opaque, unsigned items, unsigned size);
+voidpf z_custom_calloc(voidpf opaque, unsigned items, unsigned size);
 
-void FUNCTIONCALLINGCONVENTION z_custom_cfree(voidpf opaque, voidpf ptr);
+void z_custom_cfree(voidpf opaque, voidpf ptr);
