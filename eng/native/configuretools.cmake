ae2b323bbec0a53b3baeae993378c52a59c9234c
Adeel Mujahid <3840695+am11@users.noreply.github.com>
2024-06-28 09:20:45 +0300

    .

--- eng/native/configuretools.cmake
+++ eng/native/configuretools.cmake
@@ -79,7 +79,8 @@ if (NOT CLR_CMAKE_HOST_WIN32)
   # detect linker
   execute_process(COMMAND sh -c "${CMAKE_C_COMPILER} ${CMAKE_SHARED_LINKER_FLAGS} -Wl,--version | head -1"
     ERROR_QUIET
-    OUTPUT_VARIABLE ldVersionOutput)
+    OUTPUT_VARIABLE ldVersionOutput
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
 
   if("${ldVersionOutput}" MATCHES "LLD")
     set(LD_LLVM 1)
