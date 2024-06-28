611754689ce1bbf6268bb91b56aec011f6b59685
Kunal Pathak <Kunal.Pathak@microsoft.com>
2024-06-28 12:47:40 -0700

    Update src/coreclr/vm/codeman.cpp
    Co-authored-by: Jan Kotas <jkotas@microsoft.com>

--- src/coreclr/vm/codeman.cpp
+++ src/coreclr/vm/codeman.cpp
@@ -1514,8 +1514,10 @@ void EEJitManager::SetCpuInfo()
         uint32_t maxVectorTLength = (maxVectorTBitWidth / 8);
         uint64_t sveLengthFromOS = GetSveLengthFromOS();
 
-        // Do not enable SVE when the system vector length is not 128-bits
+        // For now, enable only SVE when the system vector length is 128-bits
+        // TODO: https://github.com/dotnet/runtime/issues/101477
         if (sveLengthFromOS == 128)
+        // if ((maxVectorTLength >= sveLengthFromOS) || (maxVectorTBitWidth == 0))
         {
             CPUCompileFlags.Set(InstructionSet_Sve);
         }
