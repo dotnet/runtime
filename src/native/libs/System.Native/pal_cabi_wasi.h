d13c7859c76baa6fa3a800acf99d4ce15e5b20fe
pavelsavara <pavel.savara@gmail.com>
2024-07-02 18:00:58 +0200

    WASIp2 build and SDK provisioning

---
+++ src/native/libs/System.Native/pal_cabi_wasi.h
@@ -0,0 +1,6 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#include <stdlib.h>
+
+void *cabi_realloc(void *ptr, size_t old_size, size_t align, size_t new_size);
