1af7c80b3c5a56b8a417abeb122d4314f95d5bf0
Aleksey Kliger (λgeek) <alklig@microsoft.com>
2024-07-02 15:27:31 -0400

    Update src/coreclr/gc/env/gcenv.object.h
    Co-authored-by: Elinor Fung <elfung@microsoft.com>

--- src/coreclr/gc/env/gcenv.object.h
+++ src/coreclr/gc/env/gcenv.object.h
@@ -49,7 +49,7 @@ static_assert(sizeof(ObjHeader) == sizeof(uintptr_t), "this assumption is made b
 #define MTFlag_RequiresAlign8           0x00001000 // enum_flag_RequiresAlign8
 #define MTFlag_Category_ValueType       0x00040000 // enum_flag_Category_ValueType
 #define MTFlag_Category_ValueType_Mask  0x000C0000 // enum_flag_Category_ValueType_Mask
-#define MTFlag_ContainsGCPointers         0x01000000 // enum_flag_ContainsGCPointers
+#define MTFlag_ContainsGCPointers       0x01000000 // enum_flag_ContainsGCPointers
 #define MTFlag_HasCriticalFinalizer     0x00000002 // enum_flag_HasCriticalFinalizer
 #define MTFlag_HasFinalizer             0x00100000 // enum_flag_HasFinalizer
 #define MTFlag_IsArray                  0x00080000 // enum_flag_Category_Array
