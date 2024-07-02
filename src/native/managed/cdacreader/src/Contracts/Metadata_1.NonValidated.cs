76859d19fabd6deb79254c1c30880048212b0a3c
Aleksey Kliger <alklig@microsoft.com>
2024-07-02 16:03:25 -0400

    move non-validated MethodTable handling to a separate class

---
+++ src/native/managed/cdacreader/src/Contracts/Metadata_1.NonValidated.cs
@@ -0,0 +1,198 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+
+namespace Microsoft.Diagnostics.DataContractReader.Contracts;
+
+internal partial struct Metadata_1 : IMetadata
+{
+    // GC Heap corruption may create situations where a pointer value may point to garbage or even
+    // to an unmapped memory region.
+    // All types here have not been validated as actually representing a MethodTable, EEClass, etc.
+    // All checks are unsafe and may throw if we access an invalid address in target memory.
+    internal static class NonValidated
+    {
+
+        // This doesn't need as many properties as MethodTable because we don't want to be operating on
+        // a NonValidatedMethodTable for too long
+        internal struct MethodTable
+        {
+            private readonly Target _target;
+            private readonly Target.TypeInfo _type;
+            internal TargetPointer Address { get; init; }
+
+            private MethodTableFlags? _methodTableFlags;
+
+            internal MethodTable(Target target, TargetPointer methodTablePointer)
+            {
+                _target = target;
+                _type = target.GetTypeInfo(DataType.MethodTable);
+                Address = methodTablePointer;
+                _methodTableFlags = null;
+            }
+
+            private MethodTableFlags GetOrCreateFlags()
+            {
+                if (_methodTableFlags == null)
+                {
+                    // note: may throw if the method table Address is corrupted
+                    MethodTableFlags flags = new MethodTableFlags
+                    {
+                        MTFlags = _target.Read<uint>(Address + (ulong)_type.Fields[nameof(MethodTableFlags.MTFlags)].Offset),
+                        MTFlags2 = _target.Read<uint>(Address + (ulong)_type.Fields[nameof(MethodTableFlags.MTFlags2)].Offset),
+                        BaseSize = _target.Read<uint>(Address + (ulong)_type.Fields[nameof(MethodTableFlags.BaseSize)].Offset),
+                    };
+                    _methodTableFlags = flags;
+                }
+                return _methodTableFlags.Value;
+            }
+
+            internal MethodTableFlags Flags => GetOrCreateFlags();
+
+            internal TargetPointer EEClassOrCanonMT => _target.ReadPointer(Address + (ulong)_type.Fields[nameof(EEClassOrCanonMT)].Offset);
+            internal TargetPointer EEClass => GetEEClassOrCanonMTBits(EEClassOrCanonMT) == EEClassOrCanonMTBits.EEClass ? EEClassOrCanonMT : throw new InvalidOperationException("not an EEClass");
+            internal TargetPointer CanonMT
+            {
+                get
+                {
+                    if (GetEEClassOrCanonMTBits(EEClassOrCanonMT) == EEClassOrCanonMTBits.CanonMT)
+                    {
+                        return new TargetPointer((ulong)EEClassOrCanonMT & ~(ulong)EEClassOrCanonMTBits.Mask);
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException("not a canonical method table");
+                    }
+                }
+            }
+        }
+
+        internal struct EEClass
+        {
+            public readonly Target _target;
+            private readonly Target.TypeInfo _type;
+
+            internal TargetPointer Address { get; init; }
+
+            internal EEClass(Target target, TargetPointer eeClassPointer)
+            {
+                _target = target;
+                Address = eeClassPointer;
+                _type = target.GetTypeInfo(DataType.EEClass);
+            }
+
+            internal TargetPointer MethodTable => _target.ReadPointer(Address + (ulong)_type.Fields[nameof(MethodTable)].Offset);
+        }
+
+        internal static MethodTable GetMethodTableData(Target target, TargetPointer methodTablePointer)
+        {
+            return new MethodTable(target, methodTablePointer);
+        }
+
+        internal static EEClass GetEEClassData(Target target, TargetPointer eeClassPointer)
+        {
+            return new EEClass(target, eeClassPointer);
+        }
+
+    }
+
+    /// <summary>
+    /// Validates that the given address is a valid MethodTable.
+    /// </summary>
+    ///  <remarks>
+    ///  If the target process has memory corruption, we may see pointers that are not valid method tables.
+    ///  We validate by looking at the MethodTable -> EEClass -> MethodTable relationship (which may throw if we access invalid memory).
+    ///  And then we do some ad-hoc checks on the method table flags.
+    private bool ValidateMethodTablePointer(NonValidated.MethodTable umt)
+    {
+        try
+        {
+            if (!ValidateThrowing(umt))
+            {
+                return false;
+            }
+            if (!ValidateMethodTableAdHoc(umt))
+            {
+                return false;
+            }
+        }
+        catch (System.Exception)
+        {
+            // TODO(cdac): maybe don't swallow all exceptions? We could consider a richer contract that
+            // helps to track down what sort of memory corruption caused the validation to fail.
+            // TODO(cdac): we could also consider a more fine-grained exception type so we don't mask
+            // programmer mistakes in cdacreader.
+            return false;
+        }
+        return true;
+    }
+
+    // This portion of validation may throw if we are trying to read an invalid address in the target process
+    private bool ValidateThrowing(NonValidated.MethodTable methodTable)
+    {
+        // For non-generic classes, we can rely on comparing
+        //    object->methodtable->class->methodtable
+        // to
+        //    object->methodtable
+        //
+        //  However, for generic instantiation this does not work. There we must
+        //  compare
+        //
+        //    object->methodtable->class->methodtable->class
+        // to
+        //    object->methodtable->class
+        TargetPointer eeClassPtr = GetClassThrowing(methodTable);
+        if (eeClassPtr != TargetPointer.Null)
+        {
+            NonValidated.EEClass eeClass = NonValidated.GetEEClassData(_target, eeClassPtr);
+            TargetPointer methodTablePtrFromClass = eeClass.MethodTable;
+            if (methodTable.Address == methodTablePtrFromClass)
+            {
+                return true;
+            }
+            if (methodTable.Flags.HasInstantiation || methodTable.Flags.IsArray)
+            {
+                NonValidated.MethodTable methodTableFromClass = NonValidated.GetMethodTableData(_target, methodTablePtrFromClass);
+                TargetPointer classFromMethodTable = GetClassThrowing(methodTableFromClass);
+                return classFromMethodTable == eeClassPtr;
+            }
+        }
+        return false;
+    }
+
+    private bool ValidateMethodTableAdHoc(NonValidated.MethodTable methodTable)
+    {
+        // ad-hoc checks; add more here as needed
+        if (!methodTable.Flags.IsInterface && !methodTable.Flags.IsString)
+        {
+            if (methodTable.Flags.BaseSize == 0 || !_target.IsAlignedToPointerSize(methodTable.Flags.BaseSize))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    internal static EEClassOrCanonMTBits GetEEClassOrCanonMTBits(TargetPointer eeClassOrCanonMTPtr)
+    {
+        return (EEClassOrCanonMTBits)(eeClassOrCanonMTPtr & (ulong)EEClassOrCanonMTBits.Mask);
+    }
+    private TargetPointer GetClassThrowing(NonValidated.MethodTable methodTable)
+    {
+        TargetPointer eeClassOrCanonMT = methodTable.EEClassOrCanonMT;
+
+        if (GetEEClassOrCanonMTBits(eeClassOrCanonMT) == EEClassOrCanonMTBits.EEClass)
+        {
+            return methodTable.EEClass;
+        }
+        else
+        {
+            TargetPointer canonicalMethodTablePtr = methodTable.CanonMT;
+            NonValidated.MethodTable umt = NonValidated.GetMethodTableData(_target, canonicalMethodTablePtr);
+            return umt.EEClass;
+        }
+    }
+
+
+}
--- src/native/managed/cdacreader/src/Contracts/Metadata_1.cs
+++ src/native/managed/cdacreader/src/Contracts/Metadata_1.cs
@@ -4,109 +4,10 @@
 using System;
 using System.Collections.Generic;
 using System.Reflection.Metadata.Ecma335;
-using Microsoft.Diagnostics.DataContractReader.Data;
 
 namespace Microsoft.Diagnostics.DataContractReader.Contracts;
 
-// GC Heap corruption may create situations where a putative pointer to a MethodTable
-// may point to garbage. So this struct represents a MethodTable that we don't necessarily
-// trust to be valid.
-// see Metadata_1.ValidateMethodTablePointer
-// This doesn't need as many properties as MethodTable because we don't want to be operating on
-// a NonValidatedMethodTable for too long
-internal struct NonValidatedMethodTable_1
-{
-    private readonly Target _target;
-    private readonly Target.TypeInfo _type;
-    internal TargetPointer Address { get; init; }
 
-    private Metadata_1.MethodTableFlags? _methodTableFlags;
-
-    internal NonValidatedMethodTable_1(Target target, TargetPointer methodTablePointer)
-    {
-        _target = target;
-        _type = target.GetTypeInfo(DataType.MethodTable);
-        Address = methodTablePointer;
-        _methodTableFlags = null;
-    }
-
-    private Metadata_1.MethodTableFlags GetOrCreateFlags()
-    {
-        if (_methodTableFlags == null)
-        {
-            // note: may throw if the method table Address is corrupted
-            Metadata_1.MethodTableFlags flags = new Metadata_1.MethodTableFlags
-            {
-                MTFlags = _target.Read<uint>(Address + (ulong)_type.Fields[nameof(Metadata_1.MethodTableFlags.MTFlags)].Offset),
-                MTFlags2 = _target.Read<uint>(Address + (ulong)_type.Fields[nameof(Metadata_1.MethodTableFlags.MTFlags2)].Offset),
-                BaseSize = _target.Read<uint>(Address + (ulong)_type.Fields[nameof(Metadata_1.MethodTableFlags.BaseSize)].Offset),
-            };
-            _methodTableFlags = flags;
-        }
-        return _methodTableFlags.Value;
-    }
-
-    internal Metadata_1.MethodTableFlags Flags => GetOrCreateFlags();
-
-    internal TargetPointer EEClassOrCanonMT => _target.ReadPointer(Address + (ulong)_type.Fields[nameof(EEClassOrCanonMT)].Offset);
-    internal TargetPointer EEClass => Metadata_1.GetEEClassOrCanonMTBits(EEClassOrCanonMT) == Metadata_1.EEClassOrCanonMTBits.EEClass ? EEClassOrCanonMT : throw new InvalidOperationException("not an EEClass");
-    internal TargetPointer CanonMT
-    {
-        get
-        {
-            if (Metadata_1.GetEEClassOrCanonMTBits(EEClassOrCanonMT) == Metadata_1.EEClassOrCanonMTBits.CanonMT)
-            {
-                return new TargetPointer((ulong)EEClassOrCanonMT & ~(ulong)Metadata_1.EEClassOrCanonMTBits.Mask);
-            }
-            else
-            {
-                throw new InvalidOperationException("not a canonical method table");
-            }
-        }
-    }
-}
-
-internal struct NonValidatedEEClass_1
-{
-    public readonly Target _target;
-    private readonly Target.TypeInfo _type;
-
-    internal TargetPointer Address { get; init; }
-
-    internal NonValidatedEEClass_1(Target target, TargetPointer eeClassPointer)
-    {
-        _target = target;
-        Address = eeClassPointer;
-        _type = target.GetTypeInfo(DataType.EEClass);
-    }
-
-    internal TargetPointer MethodTable => _target.ReadPointer(Address + (ulong)_type.Fields[nameof(MethodTable)].Offset);
-}
-
-
-internal struct MethodTable_1
-{
-    internal Metadata_1.MethodTableFlags Flags { get; }
-    internal ushort NumInterfaces { get; }
-    internal ushort NumVirtuals { get; }
-    internal TargetPointer ParentMethodTable { get; }
-    internal TargetPointer Module { get; }
-    internal TargetPointer EEClassOrCanonMT { get; }
-    internal MethodTable_1(Data.MethodTable data)
-    {
-        Flags = new Metadata_1.MethodTableFlags
-        {
-            MTFlags = data.MTFlags,
-            MTFlags2 = data.MTFlags2,
-            BaseSize = data.BaseSize,
-        };
-        NumInterfaces = data.NumInterfaces;
-        NumVirtuals = data.NumVirtuals;
-        EEClassOrCanonMT = data.EEClassOrCanonMT;
-        Module = data.Module;
-        ParentMethodTable = data.ParentMethodTable;
-    }
-}
 
 internal partial struct Metadata_1 : IMetadata
 {
@@ -115,7 +16,32 @@ internal partial struct Metadata_1 : IMetadata
 
     // TODO(cdac): we mutate this dictionary - copies of the Metadata_1 struct share this instance.
     // If we need to invalidate our view of memory, we shoudl clear this dictionary.
-    private readonly Dictionary<TargetPointer, MethodTable_1> _methodTables = new();
+    private readonly Dictionary<TargetPointer, MethodTable> _methodTables = new();
+
+
+    internal struct MethodTable
+    {
+        internal MethodTableFlags Flags { get; }
+        internal ushort NumInterfaces { get; }
+        internal ushort NumVirtuals { get; }
+        internal TargetPointer ParentMethodTable { get; }
+        internal TargetPointer Module { get; }
+        internal TargetPointer EEClassOrCanonMT { get; }
+        internal MethodTable(Data.MethodTable data)
+        {
+            Flags = new MethodTableFlags
+            {
+                MTFlags = data.MTFlags,
+                MTFlags2 = data.MTFlags2,
+                BaseSize = data.BaseSize,
+            };
+            NumInterfaces = data.NumInterfaces;
+            NumVirtuals = data.NumVirtuals;
+            EEClassOrCanonMT = data.EEClassOrCanonMT;
+            Module = data.Module;
+            ParentMethodTable = data.ParentMethodTable;
+        }
+    }
 
     // Low order bit of EEClassOrCanonMT.
     // See MethodTable::LowBits UNION_EECLASS / UNION_METHODABLE
@@ -135,15 +61,6 @@ internal partial struct Metadata_1 : IMetadata
 
     internal TargetPointer FreeObjectMethodTablePointer => _freeObjectMethodTablePointer;
 
-    private NonValidatedMethodTable_1 GetNonValidatedMethodTableData(TargetPointer methodTablePointer)
-    {
-        return new NonValidatedMethodTable_1(_target, methodTablePointer);
-    }
-
-    private NonValidatedEEClass_1 GetUntrustedEEClassData(TargetPointer eeClassPointer)
-    {
-        return new NonValidatedEEClass_1(_target, eeClassPointer);
-    }
 
     public MethodTableHandle GetMethodTableHandle(TargetPointer methodTablePointer)
     {
@@ -156,7 +73,7 @@ internal partial struct Metadata_1 : IMetadata
         if (_target.ProcessedData.TryGet(methodTablePointer, out Data.MethodTable? methodTableData))
         {
             // we already cached the data, we must have validated the address, create the representation struct for our use
-            MethodTable_1 trustedMethodTable = new MethodTable_1(methodTableData);
+            MethodTable trustedMethodTable = new MethodTable(methodTableData);
             _ = _methodTables.TryAdd(methodTablePointer, trustedMethodTable);
             return new MethodTableHandle(methodTablePointer);
         }
@@ -165,13 +82,13 @@ internal partial struct Metadata_1 : IMetadata
         if (methodTablePointer == FreeObjectMethodTablePointer)
         {
             Data.MethodTable freeObjectMethodTableData = _target.ProcessedData.GetOrAdd<Data.MethodTable>(methodTablePointer);
-            MethodTable_1 trustedMethodTable = new MethodTable_1(freeObjectMethodTableData);
+            MethodTable trustedMethodTable = new MethodTable(freeObjectMethodTableData);
             _ = _methodTables.TryAdd(methodTablePointer, trustedMethodTable);
             return new MethodTableHandle(methodTablePointer);
         }
 
         // Otherwse, get ready to validate
-        NonValidatedMethodTable_1 nonvalidatedMethodTable = GetNonValidatedMethodTableData(methodTablePointer);
+        NonValidated.MethodTable nonvalidatedMethodTable = NonValidated.GetMethodTableData(_target, methodTablePointer);
 
         if (!ValidateMethodTablePointer(nonvalidatedMethodTable))
         {
@@ -179,106 +96,15 @@ internal partial struct Metadata_1 : IMetadata
         }
         // ok, we validated it, cache the data and add the MethodTable_1 struct to the dictionary
         Data.MethodTable trustedMethodTableData = _target.ProcessedData.GetOrAdd<Data.MethodTable>(methodTablePointer);
-        MethodTable_1 trustedMethodTableF = new MethodTable_1(trustedMethodTableData);
+        MethodTable trustedMethodTableF = new MethodTable(trustedMethodTableData);
         _ = _methodTables.TryAdd(methodTablePointer, trustedMethodTableF);
         return new MethodTableHandle(methodTablePointer);
     }
 
-    private bool ValidateMethodTablePointer(NonValidatedMethodTable_1 umt)
-    {
-        try
-        {
-            if (!ValidateThrowing(umt))
-            {
-                return false;
-            }
-            if (!ValidateMethodTable(umt))
-            {
-                return false;
-            }
-        }
-        catch (System.Exception)
-        {
-            // TODO(cdac): maybe don't swallow all exceptions? We could consider a richer contract that
-            // helps to track down what sort of memory corruption caused the validation to fail.
-            // TODO(cdac): we could also consider a more fine-grained exception type so we don't mask
-            // programmer mistakes in cdacreader.
-            return false;
-        }
-        return true;
-    }
-
-    // This portion of validation may throw if we are trying to read an invalid address in the target process
-    private bool ValidateThrowing(NonValidatedMethodTable_1 methodTable)
-    {
-        // For non-generic classes, we can rely on comparing
-        //    object->methodtable->class->methodtable
-        // to
-        //    object->methodtable
-        //
-        //  However, for generic instantiation this does not work. There we must
-        //  compare
-        //
-        //    object->methodtable->class->methodtable->class
-        // to
-        //    object->methodtable->class
-        TargetPointer eeClassPtr = GetClassWithPossibleAV(methodTable);
-        if (eeClassPtr != TargetPointer.Null)
-        {
-            NonValidatedEEClass_1 eeClass = GetUntrustedEEClassData(eeClassPtr);
-            TargetPointer methodTablePtrFromClass = GetMethodTableWithPossibleAV(eeClass);
-            if (methodTable.Address == methodTablePtrFromClass)
-            {
-                return true;
-            }
-            if (methodTable.Flags.HasInstantiation || methodTable.Flags.IsArray)
-            {
-                NonValidatedMethodTable_1 methodTableFromClass = GetNonValidatedMethodTableData(methodTablePtrFromClass);
-                TargetPointer classFromMethodTable = GetClassWithPossibleAV(methodTableFromClass);
-                return classFromMethodTable == eeClassPtr;
-            }
-        }
-        return false;
-    }
-
-    private bool ValidateMethodTable(NonValidatedMethodTable_1 methodTable)
-    {
-        // ad-hoc checks; add more here as needed
-        if (!methodTable.Flags.IsInterface && !methodTable.Flags.IsString)
-        {
-            if (methodTable.Flags.BaseSize == 0 || !_target.IsAlignedToPointerSize(methodTable.Flags.BaseSize))
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    internal static EEClassOrCanonMTBits GetEEClassOrCanonMTBits(TargetPointer eeClassOrCanonMTPtr)
-    {
-        return (EEClassOrCanonMTBits)(eeClassOrCanonMTPtr & (ulong)EEClassOrCanonMTBits.Mask);
-    }
-    private TargetPointer GetClassWithPossibleAV(NonValidatedMethodTable_1 methodTable)
-    {
-        TargetPointer eeClassOrCanonMT = methodTable.EEClassOrCanonMT;
-
-        if (GetEEClassOrCanonMTBits(eeClassOrCanonMT) == EEClassOrCanonMTBits.EEClass)
-        {
-            return methodTable.EEClass;
-        }
-        else
-        {
-            TargetPointer canonicalMethodTablePtr = methodTable.CanonMT;
-            NonValidatedMethodTable_1 umt = GetNonValidatedMethodTableData(canonicalMethodTablePtr);
-            return umt.EEClass;
-        }
-    }
-
-    private static TargetPointer GetMethodTableWithPossibleAV(NonValidatedEEClass_1 eeClass) => eeClass.MethodTable;
 
     public uint GetBaseSize(MethodTableHandle methodTableHandle) => _methodTables[methodTableHandle.Address].Flags.BaseSize;
 
-    private static uint GetComponentSize(MethodTable_1 methodTable)
+    private static uint GetComponentSize(MethodTable methodTable)
     {
         return methodTable.Flags.HasComponentSize ? methodTable.Flags.RawGetComponentSize() : 0u;
     }
@@ -286,7 +112,7 @@ internal partial struct Metadata_1 : IMetadata
 
     private TargetPointer GetClassPointer(MethodTableHandle methodTableHandle)
     {
-        MethodTable_1 methodTable = _methodTables[methodTableHandle.Address];
+        MethodTable methodTable = _methodTables[methodTableHandle.Address];
         switch (GetEEClassOrCanonMTBits(methodTable.EEClassOrCanonMT))
         {
             case EEClassOrCanonMTBits.EEClass:
@@ -294,7 +120,7 @@ internal partial struct Metadata_1 : IMetadata
             case EEClassOrCanonMTBits.CanonMT:
                 TargetPointer canonMTPtr = new TargetPointer((ulong)methodTable.EEClassOrCanonMT & ~(ulong)Metadata_1.EEClassOrCanonMTBits.Mask);
                 MethodTableHandle canonMTHandle = GetMethodTableHandle(canonMTPtr);
-                MethodTable_1 canonMT = _methodTables[canonMTHandle.Address];
+                MethodTable canonMT = _methodTables[canonMTHandle.Address];
                 return canonMT.EEClassOrCanonMT; // canonical method table EEClassOrCanonMT is always EEClass
             default:
                 throw new InvalidOperationException();
@@ -320,7 +146,7 @@ internal partial struct Metadata_1 : IMetadata
 
     public uint GetTypeDefToken(MethodTableHandle methodTableHandle)
     {
-        MethodTable_1 methodTable = _methodTables[methodTableHandle.Address];
+        MethodTable methodTable = _methodTables[methodTableHandle.Address];
         return (uint)(methodTable.Flags.GetTypeDefRid() | ((int)TableIndex.TypeDef << 24));
     }
 
