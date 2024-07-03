7e0e125ebaf52e5b7263a5bf24923e3f1a65c001
Buyaa Namnan <buyankhishig.namnan@microsoft.com>
2024-07-03 10:59:01 -0700

    Update src/libraries/System.Reflection.Emit/src/System/Reflection/Emit/SignatureHelper.cs

--- src/libraries/System.Reflection.Emit/src/System/Reflection/Emit/SignatureHelper.cs
+++ src/libraries/System.Reflection.Emit/src/System/Reflection/Emit/SignatureHelper.cs
@@ -287,7 +287,7 @@ namespace System.Reflection.Emit
                 case CoreTypeId.TypedReference:
                     signature.TypedReference();
                     break;
-                default:    // handles null and all other types 
+                default:    // handles null and all other types
                     EntityHandle typeHandle = module.GetTypeHandle(type);
                     signature.Type(typeHandle, type.IsValueType);
                     break;
