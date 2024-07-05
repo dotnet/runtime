78dd9273d3937f20e39c8f49e7e11c4b9a27a1ba
Carlos Sánchez López <1175054+carlossanlop@users.noreply.github.com>
2024-07-05 13:43:50 -0700

    Apply jkotas comment suggestion in configureplatform.cmake

--- eng/native/configureplatform.cmake
+++ eng/native/configureplatform.cmake
@@ -502,7 +502,9 @@ if(LOWERCASE_CMAKE_BUILD_TYPE STREQUAL debug)
 endif()
 
 if (CLR_CMAKE_TARGET_ANDROID OR CLR_CMAKE_TARGET_MACCATALYST OR CLR_CMAKE_TARGET_IOS OR CLR_CMAKE_TARGET_TVOS OR CLR_CMAKE_HOST_ARCH_WASM OR CLR_CMAKE_HOST_ARCH_ARMV6)
-    # Our zlib-ng copy should only be used in non-mobile and non-browser platforms only.
-    # In mobile platforms we should use the zlib package provided by the system.
+    # Some platforms are opted-out from using the in-tree zlib-ng by default:
+    # - Android and iOS-like platforms: concerns about extra binary size
+    # - Wasm: zlib-ng breaks the build
+    # - Armv6: zlib-ng has build breaks
     set(CLR_CMAKE_USE_SYSTEM_ZLIB 1)
 endif()
