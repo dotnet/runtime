5e0e9717e4b9b83529c681a0d09782a9ff304019
antonfirsov <antonfir@gmail.com>
2024-07-06 02:49:41 +0200

    Socket Activity tags

--- src/libraries/Common/tests/System/Net/ActivityRecorder.cs
+++ src/libraries/Common/tests/System/Net/ActivityRecorder.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -74,14 +75,18 @@ namespace System.Net.Test.Common
     {
         public static void HasTag<T>(Activity activity, string name, T expectedValue)
         {
-            T? value = (T?)activity.TagObjects.Single(t => t.Key == name).Value;
-            Assert.False(value is null, $"The Activity tags should contain {name}.");
-            Assert.Equal(expectedValue, value);
+            KeyValuePair<string, object> tag = activity.TagObjects.SingleOrDefault(t => t.Key == name);
+            if (tag.Key is null)
+            {
+                Assert.Fail($"The Activity tags should contain {name}.");
+            }
+
+            Assert.Equal(expectedValue, (T)tag.Value);
         }
 
         public static void HasTag<T>(Activity activity, string name, Func<T, bool> verifyValue)
         {
-            T? value = (T?)activity.TagObjects.Single(t => t.Key == name).Value;
+            T? value = (T?)activity.TagObjects.SingleOrDefault(t => t.Key == name).Value;
             Assert.False(value is null, $"The Activity tags should contain {name}.");
             Assert.True(verifyValue(value));
         }
@@ -100,11 +105,11 @@ namespace System.Net.Test.Common
             for (int i = 1; i < camel.Length; i++)
             {
                 char c = camel[i];
-                bld.Append(char.ToLower(c));
                 if (char.IsUpper(c))
                 {
                     bld.Append('_');
                 }
+                bld.Append(char.ToLower(c));
             }
             return bld.ToString();
         }
