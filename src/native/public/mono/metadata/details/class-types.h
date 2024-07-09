e5fdca99ad5f87cd0b7d969509fcd4ab16f97e0a
Katelyn Gadd <kg@luminance.org>
2024-07-09 12:06:25 -0700

    Cleanup

--- src/native/public/mono/metadata/details/class-types.h
+++ src/native/public/mono/metadata/details/class-types.h
@@ -17,11 +17,6 @@ typedef struct _MonoClassField MonoClassField;
 typedef struct _MonoProperty MonoProperty;
 typedef struct _MonoEvent MonoEvent;
 
-typedef struct MonoVarianceSearchEntry {
-	MonoClass *klass;
-	int interface_offset;
-} MonoVarianceSearchEntry;
-
 typedef enum {
 	MONO_TYPE_NAME_FORMAT_IL,
 	MONO_TYPE_NAME_FORMAT_REFLECTION,
