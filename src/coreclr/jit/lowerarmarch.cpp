32b6e137d8e92cd4c3543ebc14eb7983c4079015
Tanner Gooding <tagoo@outlook.com>
2024-07-09 11:03:07 -0700

    Ensure that we create a valid GT_IND node

--- src/coreclr/jit/lowerarmarch.cpp
+++ src/coreclr/jit/lowerarmarch.cpp
@@ -1264,9 +1264,10 @@ GenTree* Lowering::LowerHWIntrinsic(GenTreeHWIntrinsic* node)
 
             if (isContainableMemory || !op2->OperIsConst())
             {
-                unsigned  simdSize     = node->GetSimdSize();
-                var_types simdBaseType = node->GetSimdBaseType();
-                var_types simdType     = Compiler::getSIMDTypeForSize(simdSize);
+                unsigned    simdSize        = node->GetSimdSize();
+                CorInfoType simdBaseJitType = node->GetSimdBaseJitType();
+                var_types   simdBaseType    = node->GetSimdBaseType();
+                var_types   simdType        = Compiler::getSIMDTypeForSize(simdSize);
 
                 // We're either already loading from memory or we need to since
                 // we don't know what actual index is going to be retrieved.
@@ -1355,7 +1356,7 @@ GenTree* Lowering::LowerHWIntrinsic(GenTreeHWIntrinsic* node)
                 }
 
                 // Finally we can indirect the memory address to get the actual value
-                GenTreeIndir* indir = comp->gtNewIndir(simdBaseType, addr);
+                GenTreeIndir* indir = comp->gtNewIndir(JITtype2varType(simdBaseJitType), addr);
                 BlockRange().InsertBefore(node, indir);
 
                 LIR::Use use;
