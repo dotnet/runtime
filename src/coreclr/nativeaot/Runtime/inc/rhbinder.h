62f446240a2b22db09ac73def5e6ae8a5503fee8
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2024-07-10 10:16:01 +0200

    Update src/coreclr/nativeaot/Runtime/inc/rhbinder.h

--- src/coreclr/nativeaot/Runtime/inc/rhbinder.h
+++ src/coreclr/nativeaot/Runtime/inc/rhbinder.h
@@ -462,7 +462,7 @@ struct PInvokeTransitionFrame
 struct PInvokeTransitionFrame
 {
 #if defined(TARGET_ARM64) || defined(TARGET_ARM) || defined(TARGET_LOONGARCH64)
-    // On arm32/arm64, the FP and LR registers are pushed in that order when setting up frames
+    // The FP and LR registers are pushed in different order when setting up frames
     TgtPTR_Void     m_FramePointer;
     TgtPTR_Void     m_RIP;
 #else
