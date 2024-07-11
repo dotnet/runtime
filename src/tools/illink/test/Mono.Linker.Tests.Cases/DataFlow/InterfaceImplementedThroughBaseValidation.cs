0b66cb115142612e00fef94049899db64e62052e
Sven Boemer <sbomer@gmail.com>
2024-07-11 18:12:08 +0000

    Add some tests for ILC behavior

--- src/tools/illink/test/Mono.Linker.Tests.Cases/DataFlow/InterfaceImplementedThroughBaseValidation.cs
+++ src/tools/illink/test/Mono.Linker.Tests.Cases/DataFlow/InterfaceImplementedThroughBaseValidation.cs
@@ -24,6 +24,12 @@ namespace Mono.Linker.Tests.Cases.DataFlow
 			RUCOnDIM.Test ();
 			DAMOnInterfaceWithDIM.Test ();
 			DAMOnDIM.Test ();
+
+			GenericVirtualMethod.Test ();
+			GenericVirtualMethodWithDIM.Test ();
+
+			CalledThroughConstraint.Test ();
+			CalledThroughConstraintWithDIM.Test ();
 		}
 
 		class RUCOnInterfaceMethod
@@ -201,6 +207,115 @@ namespace Mono.Linker.Tests.Cases.DataFlow
 				Interface i = new C ();
 				i = new D ();
 				i.Method (typeof (int));
+			}
+		}
+
+		class CalledThroughConstraint
+		{
+			interface Interface {
+				[RequiresUnreferencedCode (nameof (Method))]
+				void Method ();
+			}
+
+			struct Struct : Interface {
+				[ExpectedWarning ("IL2046", Tool.TrimmerAnalyzerAndNativeAot, "https://github.com/dotnet/runtime/issues/97676")]
+				// NativeAot produces one warning for each call to NoteOverridingMethod.
+				// This is done on each callsite while compiling generic code with constraints.
+				[ExpectedWarning ("IL2046", Tool.TrimmerAnalyzerAndNativeAot, "https://github.com/dotnet/runtime/issues/97676")]
+				[ExpectedWarning ("IL2046", Tool.TrimmerAnalyzerAndNativeAot, "https://github.com/dotnet/runtime/issues/97676")]
+				public void Method () {}
+			}
+
+			[ExpectedWarning ("IL2026")]
+			[ExpectedWarning ("IL2026")]
+			[ExpectedWarning ("IL2026")]
+			static void Call<T> (T t) where T : Interface {
+				t.Method ();
+				t.Method ();
+				t.Method ();
+			}
+
+			public static void Test () {
+				Call(new Struct ());
+			}
+		}
+
+		class CalledThroughConstraintWithDIM
+		{
+			// Instance DIMs require boxing the struct.
+			// let's try a static DIM.
+			interface Interface {
+				[RequiresUnreferencedCode (nameof (Method))]
+				static abstract void Method ();
+			}
+
+			interface InterfaceImpl : Interface {
+				[ExpectedWarning ("IL2046")]
+				[ExpectedWarning ("IL2046", Tool.NativeAot, "https://github.com/dotnet/runtime/issues/104702")]
+				static void Interface.Method() {}
+			}
+
+			struct Struct : InterfaceImpl {}
+
+			[ExpectedWarning ("IL2026")]
+			[ExpectedWarning ("IL2026")]
+			static void Call<T> () where T : Interface {
+				T.Method ();
+				T.Method ();
+			}
+
+			public static void Test () {
+				Call<Struct> ();
+			}
+		}
+		
+		class GenericVirtualMethod
+		{
+			interface Interface {
+				[RequiresUnreferencedCode (nameof (Method))]
+				void Method<T> ();
+			}
+
+			class Base {
+				public void Method<T> () {}
+			}
+
+			[ExpectedWarning ("IL2046")]
+			class Derived : Base, Interface {}
+
+			[ExpectedWarning ("IL2026")]
+			[ExpectedWarning ("IL2026")]
+			public static void Test () {
+				Interface i = new Derived ();
+				i.Method<int> ();
+				i.Method<int> ();
+			}
+		} 
+
+		class GenericVirtualMethodWithDIM
+		{
+			interface Interface {
+				[RequiresUnreferencedCode (nameof (Method))]
+				static abstract void Method<T> ();
+			}
+
+			interface Impl : Interface {
+				[ExpectedWarning ("IL2046")]
+				[ExpectedWarning ("IL2046", Tool.NativeAot, "https://github.com/dotnet/runtime/issues/104702")]
+				static void Interface.Method<T> () {}
+			}
+
+			class Class : Impl {}
+
+			[ExpectedWarning ("IL2026")]
+			[ExpectedWarning ("IL2026")]
+			static void Call<T> () where T : Interface {
+				T.Method<int> ();
+				T.Method<int> ();
+			}
+
+			public static void Test () {
+				Call<Class> ();
 			}
 		}
 	}
