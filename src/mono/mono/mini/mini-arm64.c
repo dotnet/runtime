5756236c00cc8d899b20e3bb86c1de6c3a09df65
Milos Kotlar <kotlarmilos@gmail.com>
2024-07-12 15:57:34 +0200

    Update src/mono/mono/mini/mini-arm64.c
    Co-authored-by: Aleksey Kliger (λgeek) <akliger@gmail.com>

--- src/mono/mono/mini/mini-arm64.c
+++ src/mono/mono/mini/mini-arm64.c
@@ -1869,7 +1869,7 @@ get_call_info (MonoMemPool *mp, MonoMethodSignature *sig)
 				guint32 align;
 				MonoType *ptype = mini_get_underlying_type (sig->params [pindex]);
 				int size = mini_type_stack_size_full (ptype, &align, cinfo->pinvoke);
-				g_assert (size == SIZEOF_VOID_P);
+				g_assert (size == TARGET_SIZEOF_VOID_P);
 				ainfo->storage = ArgVtypeInIRegs;
 				ainfo->reg = (klass == swift_self) ? ARMREG_R20 : ARMREG_R8;
 				ainfo->nregs = 1;
