1eeadb38d28a4f484382ae345e57684f8c56da6e
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2024-07-12 15:51:20 +0100

    Update eng/native/init-vs-env.cmd
    Co-authored-by: Jan Kotas <jkotas@microsoft.com>

--- eng/native/init-vs-env.cmd
+++ eng/native/init-vs-env.cmd
@@ -9,10 +9,9 @@ if /i "%PROCESSOR_ARCHITECTURE%" == "ARM64" (
     if /i "%~1" == "x64"   ( set __VCBuildArch=arm64_amd64 )
     if /i "%~1" == "x86"   ( set __VCBuildArch=arm64_x86 )
 ) else (
-    if /i "%~1" == "x64"   ( set __VCBuildArch=amd64 )
+    set __VCBuildArch=amd64
     if /i "%~1" == "x86"   ( set __VCBuildArch=amd64_x86 )
     if /i "%~1" == "arm64" ( set __VCBuildArch=amd64_arm64 )
-    if /i "%~1" == "wasm"  ( set __VCBuildArch=amd64 )
 )
 
 :: Default to highest Visual Studio version available that has Visual C++ tools.
