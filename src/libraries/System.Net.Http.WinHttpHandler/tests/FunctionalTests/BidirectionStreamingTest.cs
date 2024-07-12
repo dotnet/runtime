2d3389c08c42238e352bb36bc17c93fbb3584ced
Ahmet Ibrahim Aksoy <aaksoy@microsoft.com>
2024-07-12 16:49:36 +0200

    Use helper property for WS2022

--- src/libraries/System.Net.Http.WinHttpHandler/tests/FunctionalTests/BidirectionStreamingTest.cs
+++ src/libraries/System.Net.Http.WinHttpHandler/tests/FunctionalTests/BidirectionStreamingTest.cs
@@ -23,7 +23,7 @@ namespace System.Net.Http.WinHttpHandlerFunctional.Tests
         // Build number suggested by the WinHttp team.
         // It can be reduced if bidirectional streaming is backported.
         public static bool OsSupportsWinHttpBidirectionalStreaming => Environment.OSVersion.Version >= new Version(10, 0, 22357, 0)
-            || Environment.OSVersion.Version == new Version(10, 0, 20348, 0); // This is required for WS2022
+            || PlatformDetection.IsWindowsServer2022; // This is required
                                                                               // because WINHTTP_FLAG_AUTOMATIC_CHUNKING is backported to WS2022.
 
         public static bool TestsEnabled => OsSupportsWinHttpBidirectionalStreaming && PlatformDetection.SupportsAlpn;
