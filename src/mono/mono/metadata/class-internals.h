0ea0e3287ed2c0a2dfe898d88f9cbbeec341053d
Katelyn Gadd <kg@luminance.org>
2024-07-12 16:27:13 -0700

    [mono] Skip exact interface matches when an interface has variance (#103757)
    * When searching for a variant interface, use a special 'variant search table' that contains only variant interfaces in the correct search order, and perform the search by doing two passes per class
    * Add missing interfaces to set of array special interfaces in mono
    * Don't dereference random memory if get_virtual_method fails in interp
    * Add regression test
    * Update reason why variance test is disabled

--- src/mono/mono/metadata/class-internals.h
+++ src/mono/mono/metadata/class-internals.h
@@ -1465,6 +1465,9 @@ mono_class_has_default_constructor (MonoClass *klass, gboolean public_only);
 gboolean
 mono_method_has_unmanaged_callers_only_attribute (MonoMethod *method);
 
+void
+mono_class_get_variance_search_table (MonoClass *klass, MonoClass ***table, int *table_size);
+
 // There are many ways to do on-demand initialization.
 //   Some allow multiple concurrent initializations. Some do not.
 //   Some allow multiple concurrent writes to the global. Some do not.
