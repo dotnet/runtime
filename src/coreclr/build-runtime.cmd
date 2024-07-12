e763e4a1c6958d53e9a1add1b57cedbed5356aa0
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2024-07-12 11:49:01 +0100

    Revert to using __HostArch when determining toolset.

--- src/coreclr/build-runtime.cmd
+++ src/coreclr/build-runtime.cmd
@@ -360,12 +360,12 @@ if %__BuildNative% EQU 1 (
     REM Set the environment for the native build
     if /i "%PROCESSOR_ARCHITECTURE%" == "ARM64" (
         set __VCTargetArch=arm64
-        if /i "%__TargetArch%" == "x64" ( set __VCTargetArch=arm64_amd64 )
-        if /i "%__TargetArch%" == "x86" ( set __VCTargetArch=arm64_x86 )
-    ) else
+        if /i "%__HostArch%" == "x64" ( set __VCTargetArch=arm64_amd64 )
+        if /i "%__HostArch%" == "x86" ( set __VCTargetArch=arm64_x86 )
+    ) else (
         set __VCTargetArch=amd64
-        if /i "%__TargetArch%" == "x86" ( set __VCTargetArch=amd64_x86 )
-        if /i "%__TargetArch%" == "arm64" ( set __VCTargetArch=amd64_arm64 )
+        if /i "%__HostArch%" == "x86" ( set __VCTargetArch=amd64_x86 )
+        if /i "%__HostArch%" == "arm64" ( set __VCTargetArch=amd64_arm64 )
     )
 
     if NOT DEFINED SkipVCEnvInit (
