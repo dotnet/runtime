4f9ff466f8d2d62e2dcdc0dec91b51442fc79e93
Egor Bogatov <egorbo@gmail.com>
2024-07-12 15:47:06 +0200

    Update ObjectStackAllocationTests.cs

--- src/tests/JIT/opt/ObjectStackAllocation/ObjectStackAllocationTests.cs
+++ src/tests/JIT/opt/ObjectStackAllocation/ObjectStackAllocationTests.cs
@@ -179,6 +179,8 @@ namespace ObjectStackAllocation
             Assembly objectAssembly = Assembly.GetAssembly(typeof(object));
             object[] attribs = objectAssembly.GetCustomAttributes(typeof(DebuggableAttribute),
                                                         false);
+            if (attribs.Length == 0)
+                return false;
             DebuggableAttribute debuggableAttribute = attribs[0] as DebuggableAttribute;
             return ((debuggableAttribute == null) || !debuggableAttribute.IsJITOptimizerDisabled);
         }
