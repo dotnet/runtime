d52bc9ba31c43cb111b13bd6cb30ec3ce4e839f2
Tanner Gooding <tagoo@outlook.com>
2024-07-16 07:50:23 -0700

    Don't accelerate SinCosPi for the time being

--- src/libraries/System.Numerics.Tensors/src/System/Numerics/Tensors/netcore/TensorPrimitives.SinCosPi.cs
+++ src/libraries/System.Numerics.Tensors/src/System/Numerics/Tensors/netcore/TensorPrimitives.SinCosPi.cs
@@ -1,7 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Diagnostics;
 using System.Runtime.Intrinsics;
 
 namespace System.Numerics.Tensors
@@ -30,91 +29,12 @@ namespace System.Numerics.Tensors
         /// <summary>T.SinCosPi(x)</summary>
         private readonly struct SinCosPiOperator<T> : IUnaryInputBinaryOutput<T> where T : ITrigonometricFunctions<T>
         {
-            public static bool Vectorizable => (typeof(T) == typeof(float))
-                                            || (typeof(T) == typeof(double));
+            public static bool Vectorizable => false; // TODO: vectorize
 
             public static (T, T) Invoke(T x) => T.SinCosPi(x);
-
-            public static (Vector128<T> First, Vector128<T> Second) Invoke(Vector128<T> x)
-            {
-                Vector128<T> xpi = x * Vector128.Create(T.Pi);
-
-                if (typeof(T) == typeof(float))
-                {
-                    if (Vector128.GreaterThanAny(xpi.AsUInt32() & Vector128.Create(SinOperatorSingle.SignMask), Vector128.Create(SinOperatorSingle.MaxVectorizedValue)) ||
-                        Vector128.GreaterThanAny(xpi.AsUInt32() & Vector128.Create(CosOperatorSingle.SignMask), Vector128.Create(CosOperatorSingle.MaxVectorizedValue)))
-                    {
-                        (Vector128<float> sin, Vector128<float> cos) = Apply2xScalar<SinCosPiOperator<float>>(x.AsSingle());
-                        return (sin.As<float, T>(), cos.As<float, T>());
-                    }
-                }
-                else
-                {
-                    Debug.Assert(typeof(T) == typeof(double));
-                    if (Vector128.GreaterThanAny(xpi.AsUInt64() & Vector128.Create(SinOperatorDouble.SignMask), Vector128.Create(SinOperatorDouble.MaxVectorizedValue)) ||
-                        Vector128.GreaterThanAny(xpi.AsUInt64() & Vector128.Create(CosOperatorDouble.SignMask), Vector128.Create(CosOperatorDouble.MaxVectorizedValue)))
-                    {
-                        (Vector128<double> sin, Vector128<double> cos) = Apply2xScalar<SinCosPiOperator<double>>(x.AsDouble());
-                        return (sin.As<double, T>(), cos.As<double, T>());
-                    }
-                }
-
-                return SinCosOperator<T>.Invoke(xpi);
-            }
-
-            public static (Vector256<T> First, Vector256<T> Second) Invoke(Vector256<T> x)
-            {
-                Vector256<T> xpi = x * Vector256.Create(T.Pi);
-
-                if (typeof(T) == typeof(float))
-                {
-                    if (Vector256.GreaterThanAny(xpi.AsUInt32() & Vector256.Create(SinOperatorSingle.SignMask), Vector256.Create(SinOperatorSingle.MaxVectorizedValue)) ||
-                        Vector256.GreaterThanAny(xpi.AsUInt32() & Vector256.Create(CosOperatorSingle.SignMask), Vector256.Create(CosOperatorSingle.MaxVectorizedValue)))
-                    {
-                        (Vector256<float> sin, Vector256<float> cos) = Apply2xScalar<SinCosPiOperator<float>>(x.AsSingle());
-                        return (sin.As<float, T>(), cos.As<float, T>());
-                    }
-                }
-                else
-                {
-                    Debug.Assert(typeof(T) == typeof(double));
-                    if (Vector256.GreaterThanAny(xpi.AsUInt64() & Vector256.Create(SinOperatorDouble.SignMask), Vector256.Create(SinOperatorDouble.MaxVectorizedValue)) ||
-                        Vector256.GreaterThanAny(xpi.AsUInt64() & Vector256.Create(CosOperatorDouble.SignMask), Vector256.Create(CosOperatorDouble.MaxVectorizedValue)))
-                    {
-                        (Vector256<double> sin, Vector256<double> cos) = Apply2xScalar<SinCosPiOperator<double>>(x.AsDouble());
-                        return (sin.As<double, T>(), cos.As<double, T>());
-                    }
-                }
-
-                return SinCosOperator<T>.Invoke(xpi);
-            }
-
-            public static (Vector512<T> First, Vector512<T> Second) Invoke(Vector512<T> x)
-            {
-                Vector512<T> xpi = x * Vector512.Create(T.Pi);
-
-                if (typeof(T) == typeof(float))
-                {
-                    if (Vector512.GreaterThanAny(xpi.AsUInt32() & Vector512.Create(SinOperatorSingle.SignMask), Vector512.Create(SinOperatorSingle.MaxVectorizedValue)) ||
-                        Vector512.GreaterThanAny(xpi.AsUInt32() & Vector512.Create(CosOperatorSingle.SignMask), Vector512.Create(CosOperatorSingle.MaxVectorizedValue)))
-                    {
-                        (Vector512<float> sin, Vector512<float> cos) = Apply2xScalar<SinCosPiOperator<float>>(x.AsSingle());
-                        return (sin.As<float, T>(), cos.As<float, T>());
-                    }
-                }
-                else
-                {
-                    Debug.Assert(typeof(T) == typeof(double));
-                    if (Vector512.GreaterThanAny(xpi.AsUInt64() & Vector512.Create(SinOperatorDouble.SignMask), Vector512.Create(SinOperatorDouble.MaxVectorizedValue)) ||
-                        Vector512.GreaterThanAny(xpi.AsUInt64() & Vector512.Create(CosOperatorDouble.SignMask), Vector512.Create(CosOperatorDouble.MaxVectorizedValue)))
-                    {
-                        (Vector512<double> sin, Vector512<double> cos) = Apply2xScalar<SinCosPiOperator<double>>(x.AsDouble());
-                        return (sin.As<double, T>(), cos.As<double, T>());
-                    }
-                }
-
-                return SinCosOperator<T>.Invoke(xpi);
-            }
+            public static (Vector128<T> First, Vector128<T> Second) Invoke(Vector128<T> x) => throw new NotSupportedException();
+            public static (Vector256<T> First, Vector256<T> Second) Invoke(Vector256<T> x) => throw new NotSupportedException();
+            public static (Vector512<T> First, Vector512<T> Second) Invoke(Vector512<T> x) => throw new NotSupportedException();
         }
     }
 }
