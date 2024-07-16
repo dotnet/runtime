81e10a570a19d1dbd36b43289e6932c13f720be5
Marc Gravell <marc.gravell@gmail.com>
2024-07-16 16:29:39 +0100

    prefer throw null

--- src/libraries/Microsoft.Extensions.Caching.Abstractions/ref/Microsoft.Extensions.Caching.Abstractions.cs
+++ src/libraries/Microsoft.Extensions.Caching.Abstractions/ref/Microsoft.Extensions.Caching.Abstractions.cs
@@ -201,7 +201,7 @@ namespace Microsoft.Extensions.Caching.Hybrid
         [System.Diagnostics.CodeAnalysis.SuppressMessage("ApiDesign", "RS0026:Do not add multiple public overloads with optional parameters", Justification = "Delegate differences make this unambiguous")]
         public System.Threading.Tasks.ValueTask<T> GetOrCreateAsync<T>(string key, System.Func<System.Threading.CancellationToken, System.Threading.Tasks.ValueTask<T>> factory,
             HybridCacheEntryOptions? options = null, System.Collections.Generic.IReadOnlyCollection<string>? tags = null, System.Threading.CancellationToken cancellationToken = default)
-            => throw new System.NotImplementedException();
+            => throw null;
 
         public abstract System.Threading.Tasks.ValueTask SetAsync<T>(string key, T value, HybridCacheEntryOptions? options = null, System.Collections.Generic.IReadOnlyCollection<string>? tags = null, System.Threading.CancellationToken cancellationToken = default);
 
@@ -210,11 +210,11 @@ namespace Microsoft.Extensions.Caching.Hybrid
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("ApiDesign", "RS0026:Do not add multiple public overloads with optional parameters", Justification = "Not ambiguous in context")]
         public virtual System.Threading.Tasks.ValueTask RemoveAsync(System.Collections.Generic.IEnumerable<string> keys, System.Threading.CancellationToken cancellationToken = default)
-            => throw new System.NotImplementedException();
+            => throw null;
 
        [System.Diagnostics.CodeAnalysis.SuppressMessage("ApiDesign", "RS0026:Do not add multiple public overloads with optional parameters", Justification = "Not ambiguous in context")]
         public virtual System.Threading.Tasks.ValueTask RemoveByTagAsync(System.Collections.Generic.IEnumerable<string> tags, System.Threading.CancellationToken cancellationToken = default)
-            => throw new System.NotImplementedException();
+            => throw null;
         public abstract System.Threading.Tasks.ValueTask RemoveByTagAsync(string tag, System.Threading.CancellationToken cancellationToken = default);
     }
 
