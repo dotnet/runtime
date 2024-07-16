afbd98e24b22c593363ce5a85cd4b6244f85de21
Noah Falk <noahfalk@users.noreply.github.com>
2024-07-16 00:00:46 -0700

    Update src/coreclr/nativeaot/Runtime/gcenv.ee.cpp
    Co-authored-by: Jan Kotas <jkotas@microsoft.com>

--- src/coreclr/nativeaot/Runtime/gcenv.ee.cpp
+++ src/coreclr/nativeaot/Runtime/gcenv.ee.cpp
@@ -143,7 +143,7 @@ void GCToEEInterface::GcEnumAllocContexts(enum_alloc_context_func* fn, void* par
         // combined_limit up-to-date. Note that the GC has multiple threads running this enumeration concurrently
         // with no synchronization. If you need to change this code think carefully about how that concurrency
         // may affect the results.
-        if(ac->alloc_limit == 0)
+        if (ac->alloc_limit == 0)
         {
             palloc_context->combined_limit = 0;
         }
