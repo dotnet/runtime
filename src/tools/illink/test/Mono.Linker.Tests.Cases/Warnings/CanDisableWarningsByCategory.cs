5d6c0e47c984e8d306bea67c7bf3152ee0601aeb
Sven Boemer <sbomer@gmail.com>
2024-07-17 21:58:32 +0000

    Add copyright headers

--- src/tools/illink/test/Mono.Linker.Tests.Cases/Warnings/CanDisableWarningsByCategory.cs
+++ src/tools/illink/test/Mono.Linker.Tests.Cases/Warnings/CanDisableWarningsByCategory.cs
@@ -1,3 +1,6 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using System.Diagnostics.CodeAnalysis;
 using Mono.Linker.Tests.Cases.Expectations.Assertions;
 using Mono.Linker.Tests.Cases.Expectations.Metadata;
