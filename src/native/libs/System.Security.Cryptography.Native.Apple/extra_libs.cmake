2c70e36356e8dfb50e6b32c8b7c9ce1a8e9f1331
Alexander Köplinger <alex.koeplinger@outlook.com>
2024-07-22 23:11:09 +0200

    Add support for AES-GCM and ChaCha20Poly1305 on iOS/tvOS/MacCatalyst via CryptoKit (#104383)
    Now that we support minimum OS versions that ship with Swift we can enable CryptoKit and pal_swiftbindings.swift.
    
    Fixes https://github.com/dotnet/runtime/issues/91523

--- src/native/libs/System.Security.Cryptography.Native.Apple/extra_libs.cmake
+++ src/native/libs/System.Security.Cryptography.Native.Apple/extra_libs.cmake
@@ -2,12 +2,7 @@
 macro(append_extra_cryptography_apple_libs NativeLibsExtra)
     find_library(COREFOUNDATION_LIBRARY CoreFoundation)
     find_library(SECURITY_LIBRARY Security)
+    find_library(CRYPTOKIT_LIBRARY CryptoKit)
 
-    list(APPEND ${NativeLibsExtra} ${COREFOUNDATION_LIBRARY} ${SECURITY_LIBRARY})
-
-    if (CLR_CMAKE_TARGET_OSX)
-        find_library(CRYPTOKIT_LIBRARY CryptoKit)
-
-        list(APPEND ${NativeLibsExtra} ${CRYPTOKIT_LIBRARY} -L/usr/lib/swift -lobjc -lswiftCore -lswiftFoundation)
-    endif()
+    list(APPEND ${NativeLibsExtra} ${COREFOUNDATION_LIBRARY} ${SECURITY_LIBRARY} ${CRYPTOKIT_LIBRARY} -L/usr/lib/swift -lobjc -lswiftCore -lswiftFoundation)
 endmacro()
