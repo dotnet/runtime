95f117e0941b458165aa410beec3b66d68a1c480
Alexander Köplinger <alex.koeplinger@outlook.com>
2024-07-22 18:12:53 +0200

    Don't rely on auto-linking to find CryptoKit/Swift

--- src/native/libs/System.Security.Cryptography.Native.Apple/extra_libs.cmake
+++ src/native/libs/System.Security.Cryptography.Native.Apple/extra_libs.cmake
@@ -2,14 +2,7 @@
 macro(append_extra_cryptography_apple_libs NativeLibsExtra)
     find_library(COREFOUNDATION_LIBRARY CoreFoundation)
     find_library(SECURITY_LIBRARY Security)
+    find_library(CRYPTOKIT_LIBRARY CryptoKit)
 
-    list(APPEND ${NativeLibsExtra} ${COREFOUNDATION_LIBRARY} ${SECURITY_LIBRARY})
-
-    if (CLR_CMAKE_TARGET_OSX OR CLR_CMAKE_TARGET_MACCATALYST OR CLR_CMAKE_TARGET_IOS OR CLR_CMAKE_TARGET_TVOS)
-        find_library(CRYPTOKIT_LIBRARY CryptoKit)
-
-        if (CLR_CMAKE_TARGET_OSX)
-            list(APPEND ${NativeLibsExtra} ${CRYPTOKIT_LIBRARY} -L/usr/lib/swift -lobjc -lswiftCore -lswiftFoundation)
-        endif()
-    endif()
+    list(APPEND ${NativeLibsExtra} ${COREFOUNDATION_LIBRARY} ${SECURITY_LIBRARY} ${CRYPTOKIT_LIBRARY} -L/usr/lib/swift -lobjc -lswiftCore -lswiftFoundation)
 endmacro()
