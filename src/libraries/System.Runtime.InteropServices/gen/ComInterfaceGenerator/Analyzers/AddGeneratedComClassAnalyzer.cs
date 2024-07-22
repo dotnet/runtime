baec33275b7f7c5a53495fdf96f42ddf31421e95
Jeremy Koritzinsky <jekoritz@microsoft.com>
2024-07-22 13:43:38 -0700

    Check for null

--- src/libraries/System.Runtime.InteropServices/gen/ComInterfaceGenerator/Analyzers/AddGeneratedComClassAnalyzer.cs
+++ src/libraries/System.Runtime.InteropServices/gen/ComInterfaceGenerator/Analyzers/AddGeneratedComClassAnalyzer.cs
@@ -53,7 +53,8 @@ namespace Microsoft.Interop.Analyzers
                         }
                     }
 
-                    if (type.BaseType.GetAttributes().Any(attr => generatedComClassAttributeType.Equals(attr.AttributeClass, SymbolEqualityComparer.Default)))
+                    if (type.BaseType is not null
+                        && type.BaseType.GetAttributes().Any(attr => generatedComClassAttributeType.Equals(attr.AttributeClass, SymbolEqualityComparer.Default)))
                     {
                         context.ReportDiagnostic(type.CreateDiagnostic(AddGeneratedComClassAttribute, type.Name));
                     }
