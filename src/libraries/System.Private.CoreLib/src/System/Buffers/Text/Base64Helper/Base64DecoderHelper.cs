8feb8f634e89918815b00f6ba13dc715f8537e42
Buyaa Namnan <bunamnan@microsoft.com>
2024-07-22 21:24:01 -0700

    Update comments

--- src/libraries/System.Private.CoreLib/src/System/Buffers/Text/Base64Helper/Base64DecoderHelper.cs
+++ src/libraries/System.Private.CoreLib/src/System/Buffers/Text/Base64Helper/Base64DecoderHelper.cs
@@ -200,7 +200,7 @@ namespace System.Buffers.Text
 
                     i0 |= i2;
 
-                    if ((i0 & 0x800000c0) != 0) // also check 2 unused lower bits are set to 0
+                    if ((i0 & 0x800000c0) != 0) // if negative or 4 unused bits are not 0.
                     {
                         goto InvalidDataExit;
                     }
@@ -216,7 +216,7 @@ namespace System.Buffers.Text
                 }
                 else
                 {
-                    if ((i0 & 0x8000F000) != 0) // also check 4 unused lower bits are set to 0
+                    if ((i0 & 0x8000F000) != 0) // if negative or 4 unused bits are not 0.
                     {
                         goto InvalidDataExit;
                     }
@@ -415,7 +415,7 @@ namespace System.Buffers.Text
 
                     i0 |= i2;
 
-                    if ((i0 & 0x800000c0) != 0)  // also check 2 unused lower bits are set to 0
+                    if ((i0 & 0x800000c0) != 0) // if negative or 4 unused bits are not 0.
                     {
                         goto InvalidExit;
                     }
