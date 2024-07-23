0c8b68869d8e878536211352deebb60f4e965fff
Kunal Pathak <Kunal.Pathak@microsoft.com>
2024-07-23 14:41:39 -0700

    unspill for LoadVectorFirstFaulting as well

--- src/coreclr/jit/hwintrinsiccodegenarm64.cpp
+++ src/coreclr/jit/hwintrinsiccodegenarm64.cpp
@@ -320,7 +320,6 @@ void CodeGen::genHWIntrinsic(GenTreeHWIntrinsic* node)
         emitSize = EA_SCALABLE;
         opt      = emitter::optGetSveInsOpt(emitTypeSize(intrin.baseType));
 
-        bool readsFfr = false;
         switch (intrin.id)
         {
             case NI_Sve_GetFfrByte:
@@ -331,16 +330,27 @@ void CodeGen::genHWIntrinsic(GenTreeHWIntrinsic* node)
             case NI_Sve_GetFfrUInt16:
             case NI_Sve_GetFfrUInt32:
             case NI_Sve_GetFfrUInt64:
-            case NI_Sve_LoadVectorFirstFaulting:
-                readsFfr = true;
+            {
+                if ((intrin.op1 != nullptr) && ((intrin.op1->gtFlags & GTF_SPILLED) != 0))
+                {
+                    // If there was a op1 for this intrinsic, it means FFR is consumed here
+                    // and we need to unspill.
+                    unspilledFfr = true;
+                }
                 break;
+            }
+            case NI_Sve_LoadVectorFirstFaulting:
+            {
+                if ((intrin.op3 != nullptr) && ((intrin.op3->gtFlags & GTF_SPILLED) != 0))
+                {
+                    // If there was a op3 for this intrinsic, it means FFR is consumed here
+                    // and we need to unspill.
+                    unspilledFfr = true;
+                }
+                break;
+            }
             default:
                 break;
-        }
-
-        if (readsFfr && (intrin.op1 != nullptr) && ((intrin.op1->gtFlags & GTF_SPILLED) != 0))
-        {
-            unspilledFfr = true;
         }
     }
     else if (intrin.category == HW_Category_Special)
@@ -2392,6 +2402,12 @@ void CodeGen::genHWIntrinsic(GenTreeHWIntrinsic* node)
 
             case NI_Sve_LoadVectorFirstFaulting:
             {
+                if (unspilledFfr)
+                {
+                    // We have unspilled the FFR in op1Reg. Restore it back in FFR register.
+                    GetEmitter()->emitIns_R(INS_sve_wrffr, emitSize, op1Reg, opt);
+                }
+
                 insScalableOpts sopt = (opt == INS_OPTS_SCALABLE_B) ? INS_SCALABLE_OPTS_NONE : INS_SCALABLE_OPTS_LSL_N;
                 GetEmitter()->emitIns_R_R_R_R(ins, emitSize, targetReg, op1Reg, op2Reg, REG_ZR, opt, sopt);
                 break;
