dc5a4a16c05dc5ab78c5c128766727eb6bce717b
Marek Fišera <mara@neptuo.com>
2024-07-23 07:25:10 +0200

    [browser] Extension agnostic lazy assembly loading (#104793)

--- src/mono/wasm/Wasm.Build.Tests/TestAppScenarios/LazyLoadingTests.cs
+++ src/mono/wasm/Wasm.Build.Tests/TestAppScenarios/LazyLoadingTests.cs
@@ -20,16 +20,32 @@ public class LazyLoadingTests : AppTestBase
     {
     }
 
-    [Fact, TestCategory("no-fingerprinting")]
-    public async Task LoadLazyAssemblyBeforeItIsNeeded()
+    public static IEnumerable<object?[]> LoadLazyAssemblyBeforeItIsNeededData()
+    {
+        string[] data = ["wasm", "dll", "NoExtension"];
+        return data.Select(d => new object[] { d, data });
+    }
+
+    [Theory, TestCategory("no-fingerprinting")]
+    [MemberData(nameof(LoadLazyAssemblyBeforeItIsNeededData))]
+    public async Task LoadLazyAssemblyBeforeItIsNeeded(string lazyLoadingTestExtension, string[] allLazyLoadingTestExtensions)
     {
         CopyTestAsset("WasmBasicTestApp", "LazyLoadingTests", "App");
-        BuildProject("Debug");
+        BuildProject("Debug", extraArgs: $"-p:LazyLoadingTestExtension={lazyLoadingTestExtension}");
 
-        var result = await RunSdkStyleAppForBuild(new(Configuration: "Debug", TestScenario: "LazyLoadingTest"));
+        // We are running the app and passing all possible lazy extensions to test matrix of all possibilities.
+        // We don't need to rebuild the application to test how client is trying to load the assembly.
+        foreach (var clientLazyLoadingTestExtension in allLazyLoadingTestExtensions)
+        {
+            var result = await RunSdkStyleAppForBuild(new(
+                Configuration: "Debug", 
+                TestScenario: "LazyLoadingTest", 
+                BrowserQueryString: new Dictionary<string, string> { ["lazyLoadingTestExtension"] = clientLazyLoadingTestExtension }
+            ));
 
-        Assert.True(result.TestOutput.Any(m => m.Contains("FirstName")), "The lazy loading test didn't emit expected message with JSON");
-        Assert.True(result.ConsoleOutput.Any(m => m.Contains("Attempting to download") && m.Contains("_framework/Json.") && m.Contains(".pdb")), "The lazy loading test didn't load PDB");
+            Assert.True(result.TestOutput.Any(m => m.Contains("FirstName")), "The lazy loading test didn't emit expected message with JSON");
+            Assert.True(result.ConsoleOutput.Any(m => m.Contains("Attempting to download") && m.Contains("_framework/Json.") && m.Contains(".pdb")), "The lazy loading test didn't load PDB");
+        }
     }
 
     [Fact]
