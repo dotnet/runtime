4bddb8755aa0608fbf79f0ca93c15ad0decf9dda
Ilona Tomkowicz <32700855+ilonatommy@users.noreply.github.com>
2024-07-24 12:33:02 +0000

    Feedback: cleanup.

--- src/mono/wasm/Wasm.Build.Tests/TestAppScenarios/MemoryTests.cs
+++ src/mono/wasm/Wasm.Build.Tests/TestAppScenarios/MemoryTests.cs
@@ -29,7 +29,7 @@ public class MemoryTests : AppTestBase
     {
         string config = "Release";
         CopyTestAsset("WasmBasicTestApp", "MemoryTests", "App");
-        string extraArgs = BuildTestBase.IsUsingWorkloads ? "-p:EmccMaximumHeapSize=4294901760" : "-p:EmccMaximumHeapSize=4294901760 -p:LazyLoad=false";
+        string extraArgs = BuildTestBase.IsUsingWorkloads ? "-p:EmccMaximumHeapSize=4294901760" : "-p:EmccMaximumHeapSize=4294901760";
         BuildProject(config, assertAppBundle: false, extraArgs: extraArgs, expectSuccess: BuildTestBase.IsUsingWorkloads);
 
         if (BuildTestBase.IsUsingWorkloads)
