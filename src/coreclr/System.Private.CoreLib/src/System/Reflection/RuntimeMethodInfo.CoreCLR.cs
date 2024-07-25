94c356d55a9f9d902f61708ee01e99d480597427
Stephen Toub <stoub@microsoft.com>
2024-07-25 11:13:50 -0400

    Some more automated C# modernization in corelib (#105151)
    * Fix IDE0056 on corelib (indexing can be simplified)
    
    * Fix IDE (null check can be simplified)
    
    * Fix IDE0078 (use pattern matching)
    
    * Fix IDE0019 (use pattern matching)
    
    * Fix IDE0066 (use switch expression)
    
    * Fix IDE0250 (struct can be made readonly)
    
    * Fix nullability warning and address PR feedback
    
    * Address PR feedback and revert a downlevel change
    
    * Wrap any `?? throw new`s that go beyond 120 characters

--- src/coreclr/System.Private.CoreLib/src/System/Reflection/RuntimeMethodInfo.CoreCLR.cs
+++ src/coreclr/System.Private.CoreLib/src/System/Reflection/RuntimeMethodInfo.CoreCLR.cs
@@ -380,20 +380,14 @@ namespace System.Reflection
         {
             ArgumentNullException.ThrowIfNull(delegateType);
 
-            RuntimeType? rtType = delegateType as RuntimeType;
-            if (rtType == null)
+            RuntimeType rtType = delegateType as RuntimeType ??
                 throw new ArgumentException(SR.Argument_MustBeRuntimeType, nameof(delegateType));
 
             if (!rtType.IsDelegate())
                 throw new ArgumentException(SR.Arg_MustBeDelegate, nameof(delegateType));
 
-            Delegate? d = Delegate.CreateDelegateInternal(rtType, this, firstArgument, bindingFlags);
-            if (d == null)
-            {
+            return Delegate.CreateDelegateInternal(rtType, this, firstArgument, bindingFlags) ??
                 throw new ArgumentException(SR.Arg_DlgtTargMeth);
-            }
-
-            return d;
         }
 
         #endregion
