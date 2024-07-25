b68854be29e88d2cc66d09cd97dc196c7a2b8f19
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2024-07-25 10:12:34 -0700

    Update CreateRuntimeRootDescriptorFileTests.cs

--- src/tools/illink/test/ILLink.Tasks.Tests/CreateRuntimeRootDescriptorFileTests.cs
+++ src/tools/illink/test/ILLink.Tasks.Tests/CreateRuntimeRootDescriptorFileTests.cs
@@ -93,7 +93,7 @@ namespace ILLink.Tasks.Tests
 						new XElement ("method", new XAttribute ("name", "TestMethodIfNotBothForILLink")),
 						new XElement ("method", new XAttribute ("name", "TestMethodForILLink"))),
 					new XElement ("type", new XAttribute("fullname", "TestNS.TestClass/Nested"),
-						new XElement ("method", new XAttribute("name", "TestMethod")))
+						new XElement ("method", new XAttribute("name", "TestMethod"))),
 					new XElement ("type", new XAttribute ("fullname", "TestNS.TestAlwaysException"),
 						new XElement ("method", new XAttribute ("name", ".ctor"))),
 					new XElement ("type", new XAttribute ("fullname", "TestNS.TestFeatureOnException"),
