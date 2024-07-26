18f8f52c0015278a5e12890550aabe40cd1c7daa
Kunal Pathak <Kunal.Pathak@microsoft.com>
2024-07-26 09:26:24 -0700

    reuse some of the code

--- src/coreclr/jit/hwintrinsiccodegenarm64.cpp
+++ src/coreclr/jit/hwintrinsiccodegenarm64.cpp
@@ -2075,42 +2075,7 @@ void CodeGen::genHWIntrinsic(GenTreeHWIntrinsic* node)
                     }
                 }
 
-                if (!varTypeIsSIMD(intrin.op2->gtType))
-                {
-                    // GatherVector...(Vector<T> mask, T* address, Vector<T2> indices)
-
-                    assert(intrin.numOperands == 3);
-                    emitAttr        baseSize = emitActualTypeSize(intrin.baseType);
-                    insScalableOpts sopt     = INS_SCALABLE_OPTS_NONE;
-
-                    if (baseSize == EA_8BYTE)
-                    {
-                        // Index is multiplied.
-                        sopt = (ins == INS_sve_ldff1b || ins == INS_sve_ldff1sb) ? INS_SCALABLE_OPTS_NONE
-                                                                                 : INS_SCALABLE_OPTS_LSL_N;
-                    }
-                    else
-                    {
-                        // Index is sign or zero extended to 64bits, then multiplied.
-                        assert(baseSize == EA_4BYTE);
-                        opt = varTypeIsUnsigned(node->GetAuxiliaryType()) ? INS_OPTS_SCALABLE_S_UXTW
-                                                                          : INS_OPTS_SCALABLE_S_SXTW;
-
-                        sopt = (ins == INS_sve_ldff1b || ins == INS_sve_ldff1sb) ? INS_SCALABLE_OPTS_NONE
-                                                                                 : INS_SCALABLE_OPTS_MOD_N;
-                    }
-
-                    GetEmitter()->emitIns_R_R_R_R(ins, emitSize, targetReg, op1Reg, op2Reg, op3Reg, opt, sopt);
-                }
-                else
-                {
-                    // GatherVector...(Vector<T> mask, Vector<T2> addresses)
-
-                    assert(intrin.numOperands == 2);
-                    GetEmitter()->emitIns_R_R_R_I(ins, emitSize, targetReg, op1Reg, op2Reg, 0, opt);
-                }
-
-                break;
+                FALLTHROUGH;
             }
             case NI_Sve_GatherVector:
             case NI_Sve_GatherVectorByteZeroExtend:
@@ -2128,25 +2093,24 @@ void CodeGen::genHWIntrinsic(GenTreeHWIntrinsic* node)
                 {
                     // GatherVector...(Vector<T> mask, T* address, Vector<T2> indices)
 
-                    assert(intrin.numOperands == 3);
-                    emitAttr        baseSize = emitActualTypeSize(intrin.baseType);
-                    insScalableOpts sopt     = INS_SCALABLE_OPTS_NONE;
+                    emitAttr baseSize   = emitActualTypeSize(intrin.baseType);
+                    bool isLoadingBytes = ((ins == INS_sve_ld1b) || (ins == INS_sve_ld1sb) || (ins == INS_sve_ldff1b) ||
+                                           (ins == INS_sve_ldff1sb));
+                    insScalableOpts sopt = INS_SCALABLE_OPTS_NONE;
 
-                    if (baseSize == EA_8BYTE)
-                    {
-                        // Index is multiplied.
-                        sopt = (ins == INS_sve_ld1b || ins == INS_sve_ld1sb) ? INS_SCALABLE_OPTS_NONE
-                                                                             : INS_SCALABLE_OPTS_LSL_N;
-                    }
-                    else
+                    if (baseSize == EA_4BYTE)
                     {
                         // Index is sign or zero extended to 64bits, then multiplied.
-                        assert(baseSize == EA_4BYTE);
                         opt = varTypeIsUnsigned(node->GetAuxiliaryType()) ? INS_OPTS_SCALABLE_S_UXTW
                                                                           : INS_OPTS_SCALABLE_S_SXTW;
 
-                        sopt = (ins == INS_sve_ld1b || ins == INS_sve_ld1sb) ? INS_SCALABLE_OPTS_NONE
-                                                                             : INS_SCALABLE_OPTS_MOD_N;
+                        sopt = isLoadingBytes ? INS_SCALABLE_OPTS_NONE : INS_SCALABLE_OPTS_MOD_N;
+                    }
+                    else
+                    {
+                        // Index is multiplied.
+                        assert(baseSize == EA_8BYTE);
+                        sopt = isLoadingBytes ? INS_SCALABLE_OPTS_NONE : INS_SCALABLE_OPTS_LSL_N;
                     }
 
                     GetEmitter()->emitIns_R_R_R_R(ins, emitSize, targetReg, op1Reg, op2Reg, op3Reg, opt, sopt);
@@ -2155,7 +2119,6 @@ void CodeGen::genHWIntrinsic(GenTreeHWIntrinsic* node)
                 {
                     // GatherVector...(Vector<T> mask, Vector<T2> addresses)
 
-                    assert(intrin.numOperands == 2);
                     GetEmitter()->emitIns_R_R_R_I(ins, emitSize, targetReg, op1Reg, op2Reg, 0, opt);
                 }
 
