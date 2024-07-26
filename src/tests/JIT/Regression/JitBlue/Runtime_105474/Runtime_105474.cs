20477eb3c4ad37082c9546d908764196f5a025ba
Kunal Pathak <Kunal.Pathak@microsoft.com>
2024-07-25 22:23:22 -0700

    Add test case

---
+++ src/tests/JIT/Regression/JitBlue/Runtime_105474/Runtime_105474.cs
@@ -0,0 +1,23 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Numerics;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.Arm;
+
+public class Program
+{
+    public static Vector<double> s_3;
+
+    [Fact]
+    public static void TestMethod()
+    {
+        if (Sve.IsSupported)
+        {
+            var vr1 = Vector128.CreateScalar(10).AsVector();
+            s_3 = Sve.FusedMultiplyAdd(vr1, s_3, s_3);
+        }
+    }
+}
