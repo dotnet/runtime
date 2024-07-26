24c6d187ef8003db448159197b5d9be20b7e7539
TIHan <lol.tihan@gmail.com>
2024-07-25 21:33:21 -0700

    Update test to include xarch

--- src/tests/JIT/Regression/JitBlue/Runtime_105474/Runtime_105474.cs
+++ src/tests/JIT/Regression/JitBlue/Runtime_105474/Runtime_105474.cs
@@ -5,6 +5,7 @@ using System;
 using System.Runtime.CompilerServices;
 using System.Runtime.Intrinsics;
 using System.Runtime.Intrinsics.Arm;
+using System.Runtime.Intrinsics.X86;
 using Xunit;
 
 #nullable disable
@@ -17,12 +18,27 @@ public class Runtime_105474_A
         AdvSimd.ShiftLeftLogicalSaturate(vr0, 229);
     }
 
+    private void Method1()
+    {
+        Vector128<float> vr1 = default;
+        Avx.Compare(vr1, vr1, (FloatComparisonMode)255);
+    }
+
     [Fact]
-    public static void TestEntryPoint()
+    public static void TestEntryPointArm()
     {
         if (AdvSimd.IsSupported)
         {
-            Assert.Throws<ArgumentOutOfRangeException>(() => new Runtime_105474_A().Method0());`
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Runtime_105474_A().Method0());
+        }
+    }
+
+    [Fact]
+    public static void TestEntryPoint()
+    {
+        if (Avx.IsSupported)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Runtime_105474_A().Method1());
         }
     }
 }
