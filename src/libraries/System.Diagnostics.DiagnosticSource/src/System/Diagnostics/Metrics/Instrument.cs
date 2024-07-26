dotnet/runtime

commit 75a48e92d8a04e14a31817974c40237538f16dba

Fix Deadlock Inside Metrics Code (#105259)

src/libraries/System.Diagnostics.DiagnosticSource/src/System/Diagnostics/Metrics/Instrument.cs
@@ -3,6 +3,7 @@
 
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace System.Diagnostics.Metrics
 {
@@ -87,6 +88,17 @@ namespace System.Diagnostics.Metrics
             {
                 return;
             }
+
+            // MeterListener has a static constructor that creates runtime metrics instruments.
+            // We need to ensure this static constructor is called before starting to publish the instrument.
+            // This is necessary because creating runtime metrics instruments will cause re-entry to the Publish method,
+            // potentially resulting in a deadlock due to the SyncObject lock.
+            // Sequence of the deadlock:
+            //   1. An application creates an early instrument (e.g., Counter) before the MeterListener static constructor is executed.
+            //   2. Instrument.Publish is called and enters the SyncObject lock.
+            //   3. Within the lock block, MeterListener is called, triggering its static constructor.
+            //   4. The static constructor creates runtime metrics instruments, causing re-entry to Instrument.Publish and leading to a deadlock.
+            RuntimeHelpers.RunClassConstructor(typeof(MeterListener).TypeHandle);
 
             List<MeterListener>? allListeners = null;
             lock (Instrument.SyncObject)

src/libraries/System.Diagnostics.DiagnosticSource/src/System/Diagnostics/Metrics/MeterListener.cs
@@ -293,7 +293,7 @@ namespace System.Diagnostics.Metrics
             }
         }
 
-        // Publish is called from Instrument.Publish
+        // GetAllListeners is called from Instrument.Publish inside Instrument.SyncObject lock.
         internal static List<MeterListener>? GetAllListeners() => s_allStartedListeners.Count == 0 ? null : new List<MeterListener>(s_allStartedListeners);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]

commit d8c545cf3eee2decde5902649331b42af1e13c0f

Simplify initialization of RuntimeMetrics
- Trigger the RuntimeMetrics initialization only when actually needed in the MeterListener constructor.
- Delete the lock-ordering workaround and wrong comment introduced in #105259. Trigger the RuntimeMetrics initialization only when actually needed should make the lock-ordering workarond unnecessary.

src/libraries/System.Diagnostics.DiagnosticSource/src/System/Diagnostics/Metrics/Instrument.cs
@@ -89,17 +89,6 @@ namespace System.Diagnostics.Metrics
                 return;
             }
 
-            // MeterListener has a static constructor that creates runtime metrics instruments.
-            // We need to ensure this static constructor is called before starting to publish the instrument.
-            // This is necessary because creating runtime metrics instruments will cause re-entry to the Publish method,
-            // potentially resulting in a deadlock due to the SyncObject lock.
-            // Sequence of the deadlock:
-            //   1. An application creates an early instrument (e.g., Counter) before the MeterListener static constructor is executed.
-            //   2. Instrument.Publish is called and enters the SyncObject lock.
-            //   3. Within the lock block, MeterListener is called, triggering its static constructor.
-            //   4. The static constructor creates runtime metrics instruments, causing re-entry to Instrument.Publish and leading to a deadlock.
-            RuntimeHelpers.RunClassConstructor(typeof(MeterListener).TypeHandle);
-
             List<MeterListener>? allListeners = null;
             lock (Instrument.SyncObject)
             {

src/libraries/System.Diagnostics.DiagnosticSource/src/System/Diagnostics/Metrics/MeterListener.cs
@@ -33,18 +33,16 @@ namespace System.Diagnostics.Metrics
         private MeasurementCallback<double>  _doubleMeasurementCallback  = (instrument, measurement, tags, state) => { /* no-op */ };
         private MeasurementCallback<decimal> _decimalMeasurementCallback = (instrument, measurement, tags, state) => { /* no-op */ };
 
-        static MeterListener()
-        {
-#if NET9_0_OR_GREATER
-            // This ensures that the static Meter gets created before any listeners exist.
-            _ = RuntimeMetrics.IsEnabled();
-#endif
-        }
-
         /// <summary>
         /// Creates a MeterListener object.
         /// </summary>
-        public MeterListener() { }
+        public MeterListener()
+        {
+#if NET9_0_OR_GREATER
+            // This ensures that the static Meter gets created before any listeners exist.
+            RuntimeMetrics.EnsureInitialized();
+#endif
+        }
 
         /// <summary>
         /// Callbacks to get notification when an instrument is published.

src/libraries/System.Diagnostics.DiagnosticSource/src/System/Diagnostics/Metrics/RuntimeMetrics.cs
@@ -20,6 +20,11 @@ namespace System.Diagnostics.Metrics
 
         private static readonly int s_maxGenerations = Math.Min(GC.GetGCMemoryInfo().GenerationInfo.Length, s_genNames.Length);
 
+        public static void EnsureInitialized()
+        {
+            // Dummy method to ensure that the static constructor have run and created the meters
+        }
+
         static RuntimeMetrics()
         {
             AppDomain.CurrentDomain.FirstChanceException += (source, e) =>
@@ -32,6 +37,8 @@ namespace System.Diagnostics.Metrics
                 t_handlingFirstChanceException = false;
             };
         }
+
+#pragma warning disable CA1823 // suppress unused fields warning, as the fields are used to keep the meters alive
 
         private static readonly ObservableCounter<long> s_gcCollections = s_meter.CreateObservableCounter(
             "dotnet.gc.collections",
@@ -156,28 +163,7 @@ namespace System.Diagnostics.Metrics
                                         unit: "s",
                                         description: "CPU time used by the process.");
 
-        public static bool IsEnabled()
-        {
-            return s_gcCollections.Enabled
-                || s_processWorkingSet.Enabled
-                || s_gcHeapTotalAllocated.Enabled
-                || s_gcLastCollectionMemoryCommitted.Enabled
-                || s_gcLastCollectionHeapSize.Enabled
-                || s_gcLastCollectionFragmentationSize.Enabled
-                || s_gcPauseTime.Enabled
-                || s_jitCompiledSize.Enabled
-                || s_jitCompiledMethodCount.Enabled
-                || s_jitCompilationTime.Enabled
-                || s_monitorLockContention.Enabled
-                || s_timerCount.Enabled
-                || s_threadPoolThreadCount.Enabled
-                || s_threadPoolCompletedWorkItems.Enabled
-                || s_threadPoolQueueLength.Enabled
-                || s_assembliesCount.Enabled
-                || s_exceptions.Enabled
-                || s_processCpuCount.Enabled
-                || s_processCpuTime?.Enabled is true;
-        }
+#pragma warning restore CA1823
 
         private static IEnumerable<Measurement<long>> GetGarbageCollectionCounts()
         {