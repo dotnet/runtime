0e97389af69fdc282b37caa6ddc6e301d19c6c9f
Buyaa Namnan <bunamnan@microsoft.com>
2024-07-29 16:43:33 -0700

    Revert RefEmitLoadContextTest in Runtime.Loader

--- src/libraries/System.Runtime.Loader/tests/RefEmitLoadContext/RefEmitLoadContextTest.cs
+++ src/libraries/System.Runtime.Loader/tests/RefEmitLoadContext/RefEmitLoadContextTest.cs
@@ -65,6 +65,7 @@ namespace System.Runtime.Loader.Tests
         }
 
         [Fact]
+        [ActiveIssue("https://github.com/dotnet/runtime/issues/31804", TestRuntimes.Mono)]
         public static void LoadRefEmitAssembly()
         {
             Init();
