b5304ad221a0683c6147a814ce47f2cee915a625
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2024-08-01 22:56:39 +0800

    Use public reflection instead.

--- src/tests/CoreMangLib/system/delegate/miscellaneous/SecondLevelIndirectionWithNullThis.cs
+++ src/tests/CoreMangLib/system/delegate/miscellaneous/SecondLevelIndirectionWithNullThis.cs
@@ -6,32 +6,20 @@ using Xunit;
 
 public class Program
 {
-    public int value = 100;
-
-    public void Update(int num)
-    {
-        value += num;
-    }
-
     [Fact]
     public static int TestEntryPoint()
     {
-        Program prog = new Program();
-
-        Action<int> action = prog.Update;
-        Action<int> secondLevel = action.Invoke;
-
-        FieldInfo targetField = typeof(Delegate).GetField("_target", BindingFlags.Instance | BindingFlags.NonPublic);
-        Assert.NotNull(targetField);
-        targetField.SetValue(secondLevel, null);
+        MethodInfo invokeMethod = typeof(Action).GetMethod("Invoke");
+        Action<Action> wrapperDelegate = invokeMethod.CreateDelegate<Action<Action>>();
+        Assert.NotNull(wrapperDelegate);
 
         try
         {
-            secondLevel(23);
+            wrapperDelegate(null);
         }
         catch (NullReferenceException)
         {
-            return prog.value;
+            return 100;
         }
 
         return 101;
