2f6c7f117f786786acdaf478f71ac21726da4555
Jeremy Koritzinsky <jekoritz@microsoft.com>
2024-08-01 11:56:00 -0700

    Re-enable tests in minopts

--- src/tests/Interop/IJW/CopyConstructorMarshaler/CopyConstructorMarshaler.cs
+++ src/tests/Interop/IJW/CopyConstructorMarshaler/CopyConstructorMarshaler.cs
@@ -62,7 +62,7 @@ namespace CopyConstructorMarshaler
         }
 
         [Fact]
-        [SkipOnCoreClr("JitStress and MinOpts can introduce extra copies that won't happen in production scenarios.", RuntimeTestModes.JitStress | RuntimeTestModes.JitMinOpts)]
+        [SkipOnCoreClr("JitStress can introduce extra copies that won't happen in production scenarios.", RuntimeTestModes.JitStress)]
         public static void CopyConstructorsInArgumentStackSlots()
         {
             Assembly ijwNativeDll = Assembly.Load("IjwCopyConstructorMarshaler");
@@ -74,7 +74,7 @@ namespace CopyConstructorMarshaler
         }
 
         [Fact]
-        [SkipOnCoreClr("JitStress and MinOpts can introduce extra copies that won't happen in production scenarios.", RuntimeTestModes.JitStress | RuntimeTestModes.JitMinOpts)]
+        [SkipOnCoreClr("JitStress can introduce extra copies that won't happen in production scenarios.", RuntimeTestModes.JitStress)]
         public static void CopyConstructorsInArgumentStackSlotsWithUnsafeValueType()
         {
             Assembly ijwNativeDll = Assembly.Load("IjwCopyConstructorMarshaler");
