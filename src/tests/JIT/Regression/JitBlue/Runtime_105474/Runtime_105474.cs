239940ca8f49e01796d97616ec2ecc10b9b08503
Aman Khalid (from Dev Box) <amankhalid@microsoft.com>
2024-08-05 17:50:14 -0400

    Use disasm checker in tests

--- src/tests/JIT/Regression/JitBlue/Runtime_105474/Runtime_105474.cs
+++ src/tests/JIT/Regression/JitBlue/Runtime_105474/Runtime_105474.cs
@@ -8,25 +8,16 @@ using System.Runtime.Intrinsics;
 using System.Runtime.Intrinsics.Arm;
 using Xunit;
 
-public class Program
+public class Runtime_105474
 {
-    public static Vector<double> s_3;
+    private static Vector<double> s_3;
 
     [Fact]
-    public static void TestMethod1()
+    public static void TestEntryPoint()
     {
         if (Sve.IsSupported)
         {
-            var vr1 = Vector128.CreateScalar((double)10).AsVector();
-            s_3 = Sve.FusedMultiplyAdd(vr1, s_3, s_3);
-        }
-    }
-
-    [Fact]
-    public static void MoreTestMethods()
-    {
-        if (Sve.IsSupported)
-        {
+            TestMethod1();
             TestMethod2(Vector<double>.Zero);
             TestMethod3(Vector<double>.Zero);
             TestMethod4(Vector<double>.Zero);
@@ -36,8 +27,17 @@ public class Program
     }
 
     [method: MethodImpl(MethodImplOptions.NoInlining)]
+    private static void TestMethod1()
+    {
+        // ARM64-FULL-LINE: fmad z17.d, p0/m, z17.d, z16.d
+        var vr1 = Vector128.CreateScalar((double)10).AsVector();
+        s_3 = Sve.FusedMultiplyAdd(vr1, s_3, s_3);
+    }
+
+    [method: MethodImpl(MethodImplOptions.NoInlining)]
     private static void TestMethod2(Vector<double> mask)
     {
+        // ARM64-FULL-LINE: fmla z16.d, p0/m, z17.d, z17.d
         var vr1 = Vector128.CreateScalar((double)10).AsVector();
         s_3 = Sve.ConditionalSelect(mask, Sve.FusedMultiplyAdd(vr1, s_3, s_3), s_3);
     }
@@ -45,12 +45,14 @@ public class Program
     [method: MethodImpl(MethodImplOptions.NoInlining)]
     private static void TestMethod3(Vector<double> mask)
     {
+        // ARM64-FULL-LINE: fmad z16.d, p0/m, z16.d, z16.d
         s_3 = Sve.ConditionalSelect(mask, Sve.FusedMultiplyAdd(s_3, s_3, s_3), s_3);
     }
 
     [method: MethodImpl(MethodImplOptions.NoInlining)]
     private static void TestMethod4(Vector<double> mask)
     {
+        // ARM64-FULL-LINE: fmad z16.d, p0/m, z17.d, z16.d
         var vr1 = Vector128.CreateScalar((double)10).AsVector();
         s_3 = Sve.ConditionalSelect(mask, Sve.FusedMultiplyAdd(s_3, vr1, s_3), s_3);
     }
@@ -58,6 +60,7 @@ public class Program
     [method: MethodImpl(MethodImplOptions.NoInlining)]
     private static void TestMethod5(Vector<double> mask)
     {
+        // ARM64-FULL-LINE: fmad z16.d, p0/m, z16.d, z17.d
         var vr1 = Vector128.CreateScalar((double)10).AsVector();
         s_3 = Sve.ConditionalSelect(mask, Sve.FusedMultiplyAdd(s_3, vr1, vr1), s_3);
     }
@@ -65,6 +68,7 @@ public class Program
     [method: MethodImpl(MethodImplOptions.NoInlining)]
     private static void TestMethod6(Vector<double> mask)
     {
+        // ARM64-FULL-LINE: fmad z16.d, p0/m, z16.d, z16.d
         var vr1 = Vector128.CreateScalar((double)10).AsVector();
         s_3 = Sve.ConditionalSelect(mask, Sve.FusedMultiplyAdd(vr1, vr1, vr1), s_3);
     }
