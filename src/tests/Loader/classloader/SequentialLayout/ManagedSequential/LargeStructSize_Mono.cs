33145a2af230cb5a3b5bf41e5fca24d632766b83
fanyang-mono <yangfan@microsoft.com>
2024-08-05 16:18:00 -0400

    Fix test

--- src/tests/Loader/classloader/SequentialLayout/ManagedSequential/LargeStructSize_Mono.cs
+++ src/tests/Loader/classloader/SequentialLayout/ManagedSequential/LargeStructSize_Mono.cs
@@ -6,37 +6,68 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Xunit;
 
-[SkipOnCoreClr("This test suite tests Mono-specific layout rules.")]
 public unsafe class LargeStructSize
 {
-    struct X
+    struct X_64
     {
         byte x;
-        BigArray1 a;
+        BigArray_64_1 a;
     }
 
-    struct Y
+    struct Y_64
     {
-        BigArray1 a;
+        BigArray_64_1 a;
+        byte y;
+    }
+
+    struct X_32
+    {
+        byte x;
+        BigArray_32_1 a;
+    }
+
+    struct Y_32
+    {
+        BigArray_32_1 a;
         byte y;
     }
 
     [StructLayout(LayoutKind.Sequential, Size = int.MaxValue - 16)]
-    struct BigArray1
+    struct BigArray_64_1
     {
     }
 
-    [StructLayout(LayoutKind.Sequential, Size = int.MaxValue - 15)]
-    struct BigArray2
+    [StructLayout(LayoutKind.Sequential, Size = int.MaxValue - 16 - 1)]
+    struct BigArray_64_2
+    {
+    }
+
+    [StructLayout(LayoutKind.Sequential, Size = int.MaxValue - 8)]
+    struct BigArray_32_1
+    {
+    }
+
+    [StructLayout(LayoutKind.Sequential, Size = int.MaxValue - 8 - 1)]
+    struct BigArray_32_2
     {
     }
 
     [Fact]
     public static void TestLargeStructSize()
     {
-        Assert.Equal(int.MaxValue - 16, sizeof(BigArray1));
-        Assert.Throws<TypeLoadException>(() => sizeof(BigArray2));
-        Assert.Throws<TypeLoadException>(() => sizeof(X));
-        Assert.Throws<TypeLoadException>(() => sizeof(Y));
+        if (Environment.Is64BitProcess)
+        {
+            Assert.Equal(int.MaxValue - (IntPtr.Size * 2), sizeof(BigArray_64_1));
+            Assert.Throws<TypeLoadException>(() => sizeof(BigArray_64_2));
+            Assert.Throws<TypeLoadException>(() => sizeof(X_64));
+            Assert.Throws<TypeLoadException>(() => sizeof(Y_64));
+        }
+        else
+        {
+            Assert.Equal(int.MaxValue - (IntPtr.Size * 2), sizeof(BigArray_32_1));
+            Assert.Throws<TypeLoadException>(() => sizeof(BigArray_32_2));
+            Assert.Throws<TypeLoadException>(() => sizeof(X_32));
+            Assert.Throws<TypeLoadException>(() => sizeof(Y_32));
+        }
     }
 }
