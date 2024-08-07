9685e6cb021f8e2bdd1a660b59dd8b286812fc7e
Aaron R Robinson <arobins@microsoft.com>
2024-08-07 16:03:00 -0700

    Add DebuggerHidden to helpers

--- src/coreclr/System.Private.CoreLib/src/System/ThrowHelper.cs
+++ src/coreclr/System.Private.CoreLib/src/System/ThrowHelper.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -13,6 +14,7 @@ namespace System
         [LibraryImport(RuntimeHelpers.QCall, EntryPoint = "ExceptionNative_ThrowAmbiguousResolutionException")]
         private static partial void ThrowAmbiguousResolutionException(MethodTable* targetType, MethodTable* interfaceType, void* methodDesc);
 
+        [DebuggerHidden]
         [DoesNotReturn]
         internal static void ThrowAmbiguousResolutionException(
             void* method,           // MethodDesc*
@@ -26,6 +28,7 @@ namespace System
         [LibraryImport(RuntimeHelpers.QCall, EntryPoint = "ExceptionNative_ThrowEntryPointNotFoundException")]
         private static partial void ThrowEntryPointNotFoundException(MethodTable* targetType, MethodTable* interfaceType, void* methodDesc);
 
+        [DebuggerHidden]
         [DoesNotReturn]
         internal static void ThrowEntryPointNotFoundException(
             void* method,           // MethodDesc*
