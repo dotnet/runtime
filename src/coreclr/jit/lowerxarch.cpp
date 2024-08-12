4c63c1507a4dede721a4c4a5a2d16f041f15b500
Ruihan-Yin <ruihan.yin@intel.com>
2024-08-12 10:55:00 -0700

    reset the base type of some intrinsics when the actual base type mismatches with the data type implied by the intrinsics.

--- src/coreclr/jit/lowerxarch.cpp
+++ src/coreclr/jit/lowerxarch.cpp
@@ -1398,6 +1398,27 @@ GenTree* Lowering::LowerHWIntrinsic(GenTreeHWIntrinsic* node)
 
     NamedIntrinsic intrinsicId = node->GetHWIntrinsicId();
 
+    if (intrinsicId == NI_AVX512F_And || intrinsicId == NI_AVX512F_AndNot || intrinsicId == NI_AVX512F_Or ||
+        intrinsicId == NI_AVX512F_Xor || intrinsicId == NI_AVX512F_ExtractVector256 ||
+        intrinsicId == NI_AVX512F_InsertVector256)
+    {
+        // Those nodes are using uniform underlying instructions for all the base bytes smaller than long/ulong.
+        // we need to fix the base type to int/uint.
+        var_types simdBaseType = node->GetSimdBaseType();
+
+        if (genTypeSize(simdBaseType) < 4)
+        {
+            if (varTypeIsUnsigned(simdBaseType))
+            {
+                node->SetSimdBaseJitType(CORINFO_TYPE_UINT);
+            }
+            else
+            {
+                node->SetSimdBaseJitType(CORINFO_TYPE_INT);
+            }
+        }
+    }
+
     if (node->OperIsEmbRoundingEnabled())
     {
         size_t   numArgs = node->GetOperandCount();
@@ -11123,14 +11144,6 @@ void Lowering::ContainCheckHWIntrinsic(GenTreeHWIntrinsic* node)
                                 {
                                     uint32_t maskSize = genTypeSize(simdBaseType);
                                     uint32_t operSize = genTypeSize(op2->AsHWIntrinsic()->GetSimdBaseType());
-
-                                    // Avx512F.Insert/ExtractVector256 accepts inputs with base type smaller that 64
-                                    // bits. it makes operSize wrong in those case.
-                                    if (op2->AsHWIntrinsic()->GetHWIntrinsicId() == NI_AVX512F_InsertVector256 ||
-                                        op2->AsHWIntrinsic()->GetHWIntrinsicId() == NI_AVX512F_ExtractVector256)
-                                    {
-                                        operSize = 8;
-                                    }
 
                                     if ((maskSize == operSize) && IsInvariantInRange(op2, node))
                                     {
