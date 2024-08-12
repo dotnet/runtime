77f2b61b9faea8310cfd490bfa690d0dbd181d99
Aaron R Robinson <arobins@microsoft.com>
2024-08-12 10:28:58 -0700

    Move direct helpers in native AOT to shared location.

--- src/coreclr/nativeaot/System.Private.CoreLib/src/Internal/Runtime/CompilerHelpers/ThrowHelpers.cs
+++ src/coreclr/nativeaot/System.Private.CoreLib/src/Internal/Runtime/CompilerHelpers/ThrowHelpers.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 
 using Internal.TypeSystem;
 
