6c2ac77b1186ef3a52ec0bd6c66b95daf5a0aa70
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2024-08-13 14:17:06 +0200

    Switch test to IL

--- src/tests/JIT/Regression/JitBlue/Runtime_101046/Runtime_101046.cs
+++ src/tests/JIT/Regression/JitBlue/Runtime_101046/Runtime_101046.cs
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+// Reference source for the .il file
+
 using System;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -18,7 +20,7 @@ public unsafe class Runtime_101046
     [MethodImpl(MethodImplOptions.NoInlining)]
     private static void Test(ref ushort p)
     {
-        int result = Runtime101046Native.ReturnExtendedShort((short)p);
+        int result = Runtime101046Native.ReturnExtendedShort((short)p); // this (short) cast is removed in the .il version
         Assert.Equal(-123, result);
     }
 }
