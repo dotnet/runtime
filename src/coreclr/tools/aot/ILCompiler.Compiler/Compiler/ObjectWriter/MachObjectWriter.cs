f856f4f04d8bdd4d162227064ad09fbb306a5f91
Filip Navara <filip.navara@gmail.com>
2024-08-19 17:39:22 +0200

    [ObjWriter/Mach-O] Mark non-global symbols as .private_extern to match ELF (#106446)
    Extracted from #106224
    
    `.private_extern` is the logical equivalent of `.hidden`+`.global` in ELF. We already emit those flags in ELF, so do it in Mach-O too.
    
    Documentation for `.private_extern`:
    > It's used to mark symbols with limited visibility. When the file is fed to the static linker, it clears the N_EXT bit for each symbol with the N_PEXT bit set. The ld option -keep_private_externs turns off this behavior.

--- src/coreclr/tools/aot/ILCompiler.Compiler/Compiler/ObjectWriter/MachObjectWriter.cs
+++ src/coreclr/tools/aot/ILCompiler.Compiler/Compiler/ObjectWriter/MachObjectWriter.cs
@@ -484,7 +484,7 @@ namespace ILCompiler.ObjectWriter
                     Section = section,
                     Value = section.VirtualAddress + (ulong)definition.Value,
                     Descriptor = N_NO_DEAD_STRIP,
-                    Type = N_SECT | N_EXT,
+                    Type = (byte)(N_SECT | N_EXT | (definition.Global ? 0 : N_PEXT)),
                 });
             }
             sortedDefinedSymbols.Sort((symA, symB) => string.CompareOrdinal(symA.Name, symB.Name));
