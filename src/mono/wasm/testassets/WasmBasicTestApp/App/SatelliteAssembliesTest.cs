285fca3c4ac94ec2a795285c65c3c98807ec71d8
Marek Fišera <mara@neptuo.com>
2024-08-20 15:22:08 +0200

    WBT

--- src/mono/wasm/testassets/WasmBasicTestApp/App/SatelliteAssembliesTest.cs
+++ src/mono/wasm/testassets/WasmBasicTestApp/App/SatelliteAssembliesTest.cs
@@ -2,25 +2,19 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
-using System.Globalization;
-using System.Threading.Tasks;
-using System.Resources;
 using System.Runtime.InteropServices.JavaScript;
+using System.Threading.Tasks;
 
 public partial class SatelliteAssembliesTest
 {
     [JSExport]
     public static async Task Run()
     {
-        var rm = new ResourceManager("WasmBasicTestApp.words", typeof(Program).Assembly);
-        TestOutput.WriteLine("default: " + rm.GetString("hello", CultureInfo.CurrentCulture));
-        TestOutput.WriteLine("es-ES without satellite: " + rm.GetString("hello", new CultureInfo("es-ES")));
+        ResourceLibrary.ResourceAccessor.Read(TestOutput.WriteLine);
 
         await LoadSatelliteAssemblies(new[] { "es-ES" });
 
-        rm = new ResourceManager("WasmBasicTestApp.words", typeof(Program).Assembly);
-        TestOutput.WriteLine("default: " + rm.GetString("hello", CultureInfo.CurrentCulture));
-        TestOutput.WriteLine("es-ES with satellite: " + rm.GetString("hello", new CultureInfo("es-ES")));
+        ResourceLibrary.ResourceAccessor.Read(TestOutput.WriteLine);
     }
 
     [JSImport("INTERNAL.loadSatelliteAssemblies")]
