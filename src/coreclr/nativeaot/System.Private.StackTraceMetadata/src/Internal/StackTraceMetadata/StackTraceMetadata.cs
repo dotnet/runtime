33695c71e503e540cb2882cd229c67f9bf1545b6
Sven Boemer <sbomer@gmail.com>
2024-08-20 11:56:20 -0700

    Cleanup

--- src/coreclr/nativeaot/System.Private.StackTraceMetadata/src/Internal/StackTraceMetadata/StackTraceMetadata.cs
+++ src/coreclr/nativeaot/System.Private.StackTraceMetadata/src/Internal/StackTraceMetadata/StackTraceMetadata.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 using System.Reflection.Runtime.General;
 
 using Internal.Metadata.NativeFormat;
@@ -187,6 +186,7 @@ namespace Internal.StackTraceMetadata
             return $"{reader.GetString(scopeRef.Name)}, Version={scopeRef.MajorVersion}.{scopeRef.MinorVersion}.{scopeRef.BuildNumber}.{scopeRef.RevisionNumber}";
         }
 
+        // Can be rewritten to false through a feature switch.
         private static bool IsReflectionExecutionAvailable() => true;
 
         /// <summary>
