32b598ead9461249f32096108a1a8677fd93372a
Aleksey Kliger <alklig@microsoft.com>
2024-08-20 16:32:21 -0400

    [mono] must expand CompareExchange(i16/u16)

--- src/libraries/System.Private.CoreLib/src/System/Threading/Interlocked.cs
+++ src/libraries/System.Private.CoreLib/src/System/Threading/Interlocked.cs
@@ -365,7 +365,7 @@ namespace System.Threading
         [CLSCompliant(false)]
         public static unsafe ushort CompareExchange(ref ushort location1, ushort value, ushort comparand)
         {
-#if !MONO && (TARGET_X86 || TARGET_AMD64 || TARGET_ARM64)
+#if (MONO && (TARGET_ARM64 || TARGET_AMD64 || TARGET_WASM)) || (!MONO && (TARGET_X86 || TARGET_AMD64 || TARGET_ARM64))
             return CompareExchange(ref location1, value, comparand); // Must expand intrinsic
 #else
             // this relies on GC keeping 4B alignment for refs and on subtracting to such alignment being in the same object
