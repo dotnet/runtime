c82465dca336268cd5a6c632957a192b29752884
Marek Fišera <mara@neptuo.com>
2024-08-21 09:35:22 +0200

    Feedback

--- src/mono/wasm/testassets/WasmBasicTestApp/ResourceLibrary/ResourceAccessor.cs
+++ src/mono/wasm/testassets/WasmBasicTestApp/ResourceLibrary/ResourceAccessor.cs
@@ -1,3 +1,6 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 using System;
 using System.Globalization;
 using System.Threading.Tasks;
@@ -7,10 +10,10 @@ namespace ResourceLibrary;
 
 public static class ResourceAccessor
 {
-    public static void Read(Action<string> testOuput)
+    public static void Read(Action<string> testOuput, bool hasSatellites)
     {
         var rm = new ResourceManager("ResourceLibrary.words", typeof(ResourceAccessor).Assembly);
-        testOuput("default: " + rm.GetString("hello", CultureInfo.CurrentCulture));
-        testOuput("es-ES without satellite: " + rm.GetString("hello", new CultureInfo("es-ES")));
+        testOuput($"default: {rm.GetString("hello", CultureInfo.CurrentCulture)}");
+        testOuput($"es-ES {(hasSatellites ? "with" : "without")} satellite: {rm.GetString("hello", new CultureInfo("es-ES"))}");
     }
 }
