01b53831cf94eef10702a53feb65474749d8bde1
Aleksey Kliger <aleksey@lambdageek.org>
2024-08-21 19:17:35 -0400

    [interp] remove if guarding a switch

--- src/mono/mono/mini/interp/transform.c
+++ src/mono/mono/mini/interp/transform.c
@@ -2461,34 +2461,28 @@ interp_handle_intrinsics (TransformData *td, MonoMethod *target_method, MonoClas
 			*op = MINT_MONO_EXCHANGE_I8;
 		else if (!strcmp (tm, "CompareExchange") && csignature->param_count == 3) {
 			MonoType *t = mini_get_underlying_type (csignature->params[1]);
-			if (t->type == MONO_TYPE_I4 ||
-			    t->type == MONO_TYPE_I8 ||
-			    t->type == MONO_TYPE_U1 ||
-			    t->type == MONO_TYPE_I1 ||
-			    t->type == MONO_TYPE_U2 ||
-			    t->type == MONO_TYPE_I2) {
-				switch (t->type) {
-				case MONO_TYPE_U1:
-					*op = MINT_MONO_CMPXCHG_U1;
-					break;
-				case MONO_TYPE_I1:
-					*op = MINT_MONO_CMPXCHG_I1;
-					break;
-				case MONO_TYPE_U2:
-					*op = MINT_MONO_CMPXCHG_U2;
-					break;
-				case MONO_TYPE_I2:
-					*op = MINT_MONO_CMPXCHG_I2;
-					break;
-				case MONO_TYPE_I4:
-					*op = MINT_MONO_CMPXCHG_I4;
-					break;
-				case MONO_TYPE_I8:
-					*op = MINT_MONO_CMPXCHG_I8;
-					break;
-				default:
-					g_assert_not_reached ();
-				}
+			switch (t->type) {
+			case MONO_TYPE_U1:
+				*op = MINT_MONO_CMPXCHG_U1;
+				break;
+			case MONO_TYPE_I1:
+				*op = MINT_MONO_CMPXCHG_I1;
+				break;
+			case MONO_TYPE_U2:
+				*op = MINT_MONO_CMPXCHG_U2;
+				break;
+			case MONO_TYPE_I2:
+				*op = MINT_MONO_CMPXCHG_I2;
+				break;
+			case MONO_TYPE_I4:
+				*op = MINT_MONO_CMPXCHG_I4;
+				break;
+			case MONO_TYPE_I8:
+				*op = MINT_MONO_CMPXCHG_I8;
+				break;
+			default:
+				/* no intrinsic */
+				break;
 			}
 		}
 	} else if (in_corlib && !strcmp (klass_name_space, "System.Threading") && !strcmp (klass_name, "Thread")) {
