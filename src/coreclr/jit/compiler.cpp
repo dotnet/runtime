2065108b76851aba02c44fccf5f777cd78a10b93
Adeel Mujahid <3840695+am11@users.noreply.github.com>
2024-08-22 22:10:28 +0300

    dummy change in jit

--- src/coreclr/jit/compiler.cpp
+++ src/coreclr/jit/compiler.cpp
@@ -31,6 +31,7 @@ unsigned Compiler::jitTotalMethodCompiled = 0;
 LONG Compiler::jitNestingLevel = 0;
 #endif // defined(DEBUG)
 
+// dummy change to trigger SPMI CI legs
 // static
 bool                Compiler::s_pAltJitExcludeAssembliesListInitialized = false;
 AssemblyNamesList2* Compiler::s_pAltJitExcludeAssembliesList            = nullptr;
