f857f3631877d616aa75b233a1a512458fbe73c9
pavelsavara <pavel.savara@gmail.com>
2024-08-27 11:59:45 +0200

    feedback

--- src/libraries/System.Private.CoreLib/src/System/Threading/Wasi/generate-wasi-poll-bindings.sh
+++ src/libraries/System.Private.CoreLib/src/System/Threading/Wasi/generate-wasi-poll-bindings.sh
@@ -14,11 +14,6 @@ set -ex
 cargo install --locked --no-default-features --features csharp --version 0.30.0 wit-bindgen-cli
 curl -OL https://github.com/WebAssembly/wasi-http/archive/refs/tags/v0.2.1.tar.gz
 tar xzf v0.2.1.tar.gz
-cat >wasi-http-0.2.1/wit/world.wit <<EOF
-world wasi-poll {
-  import wasi:io/poll@0.2.1;
-  import wasi:clocks/monotonic-clock@0.2.1;
-}
-EOF
+cp world.wit wasi-http-0.2.1/wit/world.wit
 wit-bindgen c-sharp -w wasi-poll -r native-aot --internal --skip-support-files wasi-http-0.2.1/wit
 rm -r wasi-http-0.2.1 v0.2.1.tar.gz
