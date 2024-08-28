0ca659804490074b374d95d8b31974bf0cce0242
pavelsavara <pavel.savara@gmail.com>
2024-08-28 19:24:45 +0200

    - fix finalizer
    - rewrite HTTP to respect child resources

---
+++ src/libraries/System.Net.Http/src/System/Net/Http/WasiHttpHandler/WasiInputStream.cs
@@ -0,0 +1,199 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using WasiHttpWorld;
+using WasiHttpWorld.wit.imports.wasi.http.v0_2_1;
+using WasiHttpWorld.wit.imports.wasi.io.v0_2_1;
+using static WasiHttpWorld.wit.imports.wasi.http.v0_2_1.ITypes;
+using static WasiHttpWorld.wit.imports.wasi.io.v0_2_1.IStreams;
+
+namespace System.Net.Http
+{
+    // on top of https://github.com/WebAssembly/wasi-io/blob/main/wit/streams.wit
+    internal sealed class WasiInputStream : Stream
+    {
+        private WasiRequestWrapper wrapper; // owned by this instance
+        private IncomingBody body; // owned by this instance
+        private InputStream stream; // owned by this instance
+
+        private int offset;
+        private byte[]? buffer;
+        private bool otherSideClosed;
+        internal bool isClosed;
+
+        public override bool CanRead => true;
+        public override bool CanWrite => false;
+        public override bool CanSeek => false;
+
+        public WasiInputStream(WasiRequestWrapper wrapper)
+        {
+            this.wrapper = wrapper;
+            this.body = wrapper.incomingResponse!.Consume();
+            this.stream = body.Stream();
+        }
+
+        ~WasiInputStream()
+        {
+            Dispose(false);
+        }
+
+        public override void Close()
+        {
+            Console.WriteLine("WasiInputStream.Close " + isClosed);
+            if (!isClosed)
+            {
+                isClosed = true;
+                stream.Dispose();
+                var futureTrailers = IncomingBody.Finish(body); // we just passed body ownership to Finish
+                futureTrailers.Dispose();
+                if (wrapper.requestBodyComplete != null && wrapper.requestBodyComplete.IsCompleted)
+                {
+                    Console.WriteLine("WasiInputStream.Close WRAPPER");
+                    wrapper.Dispose();
+                }
+            }
+            base.Close();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            Console.WriteLine("WasiInputStream.Dispose" + isClosed);
+            if (!isClosed)
+            {
+                isClosed = true;
+                stream.Dispose();
+                body.Dispose();
+            }
+            wrapper.Dispose();
+
+            base.Dispose(disposing);
+        }
+
+        public override async Task<int> ReadAsync(
+            byte[] bytes,
+            int offset,
+            int length,
+            CancellationToken cancellationToken
+        )
+        {
+            ObjectDisposedException.ThrowIf(isClosed, this);
+            cancellationToken.ThrowIfCancellationRequested();
+            while (true)
+            {
+                if (otherSideClosed)
+                {
+                    return 0;
+                }
+                else if (this.buffer == null)
+                {
+                    try
+                    {
+                        // TODO: should we add a special case to the bindings generator
+                        // to allow passing a buffer to InputStream.Read and
+                        // avoid the extra copy?
+                        var result = stream.Read(16 * 1024);
+                        var buffer = result;
+                        if (buffer.Length == 0)
+                        {
+                            cancellationToken.ThrowIfCancellationRequested();
+                            await WasiHttpInterop.RegisterWasiPollable(stream.Subscribe(), cancellationToken).ConfigureAwait(false);
+                            ObjectDisposedException.ThrowIf(isClosed, this);
+                        }
+                        else
+                        {
+                            this.buffer = buffer;
+                            this.offset = 0;
+                        }
+                    }
+                    catch (WitException e)
+                    {
+                        if (((StreamError)e.Value).Tag == StreamError.CLOSED)
+                        {
+                            otherSideClosed = true;
+                            return 0;
+                        }
+                        else
+                        {
+                            // TODO translate error ?
+                            throw;
+                        }
+                    }
+                }
+                else
+                {
+                    var min = Math.Min(this.buffer.Length - this.offset, length);
+                    Array.Copy(this.buffer, this.offset, bytes, offset, min);
+                    if (min < buffer.Length - this.offset)
+                    {
+                        this.offset += min;
+                    }
+                    else
+                    {
+                        this.buffer = null;
+                    }
+                    return min;
+                }
+            }
+        }
+
+        public override async ValueTask<int> ReadAsync(
+            Memory<byte> buffer,
+            CancellationToken cancellationToken = default
+        )
+        {
+            // TODO: avoid copy when possible and use ArrayPool when not
+            var dst = new byte[buffer.Length];
+            // We disable "CA1835: Prefer the memory-based overloads of
+            // ReadAsync/WriteAsync methods in stream-based classes" for
+            // now, since `ReadyAsync(byte[], int, int, CancellationToken)`
+            // is where the implementation currently resides, but we should
+            // revisit this if/when `wit-bindgen` learns to generate
+            // memory-based bindings.
+#pragma warning disable CA1835
+            var result = await ReadAsync(dst, 0, buffer.Length, cancellationToken)
+                .ConfigureAwait(false);
+#pragma warning restore CA1835
+            new ReadOnlySpan<byte>(dst, 0, result).CopyTo(buffer.Span);
+            return result;
+        }
+
+        #region PlatformNotSupported
+
+        public override void Flush()
+        {
+            // ignore
+        }
+
+        public override void SetLength(long length)
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        public override int Read(byte[] buffer, int offset, int length)
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int length)
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        public override long Length => throw new PlatformNotSupportedException();
+        public override long Position
+        {
+            get => throw new PlatformNotSupportedException();
+            set => throw new PlatformNotSupportedException();
+        }
+
+        #endregion
+    }
+}
---
+++ src/libraries/System.Net.Http/src/System/Net/Http/WasiHttpHandler/WasiOutputStream.cs
@@ -0,0 +1,173 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using WasiHttpWorld;
+using WasiHttpWorld.wit.imports.wasi.http.v0_2_1;
+using WasiHttpWorld.wit.imports.wasi.io.v0_2_1;
+using static WasiHttpWorld.wit.imports.wasi.http.v0_2_1.ITypes;
+using static WasiHttpWorld.wit.imports.wasi.io.v0_2_1.IStreams;
+
+namespace System.Net.Http
+{
+    // on top of https://github.com/WebAssembly/wasi-io/blob/main/wit/streams.wit
+    internal sealed class WasiOutputStream : Stream
+    {
+        private OutputStream stream; // owned by this instance
+        private OutgoingBody body; // owned by this instance
+        internal bool isClosed;
+
+        public override bool CanRead => false;
+        public override bool CanWrite => true;
+        public override bool CanSeek => false;
+
+        public WasiOutputStream(OutgoingBody body)
+        {
+            this.body = body;
+            this.stream = body.Write();
+        }
+
+        ~WasiOutputStream()
+        {
+            Dispose(false);
+        }
+
+        public override void Close()
+        {
+            Console.WriteLine("WasiOutputStream.Close " + isClosed);
+            if (!isClosed)
+            {
+                Console.WriteLine("WasiOutputStream.Close A");
+                isClosed = true;
+                Console.WriteLine("WasiOutputStream.Close B");
+                stream.Dispose();
+                Console.WriteLine("WasiOutputStream.Close C");
+                OutgoingBody.Finish(body, null);
+                Console.WriteLine("WasiOutputStream.Close D");
+            }
+            base.Close();
+            Console.WriteLine("WasiOutputStream.Close E");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            Console.WriteLine("WasiOutputStream.Dispose" + isClosed);
+            if (!isClosed)
+            {
+                Console.WriteLine("WasiOutputStream.Dispose A");
+                isClosed = true;
+                stream.Dispose();
+                Console.WriteLine("WasiOutputStream.Dispose B");
+                body.Dispose();
+                Console.WriteLine("WasiOutputStream.Dispose C");
+            }
+            base.Dispose(disposing);
+            Console.WriteLine("WasiOutputStream.Dispose E");
+        }
+
+        public override async Task WriteAsync(
+            byte[] bytes,
+            int offset,
+            int length,
+            CancellationToken cancellationToken
+        )
+        {
+            ObjectDisposedException.ThrowIf(isClosed, this);
+            var limit = offset + length;
+            var flushing = false;
+            while (true)
+            {
+                var count = (int)stream.CheckWrite();
+                if (count == 0)
+                {
+                    await WasiHttpInterop.RegisterWasiPollable(stream.Subscribe(), cancellationToken).ConfigureAwait(false);
+                    ObjectDisposedException.ThrowIf(isClosed, this);
+                }
+                else if (offset == limit)
+                {
+                    if (flushing)
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        stream.Flush();
+                        flushing = true;
+                    }
+                }
+                else
+                {
+                    var min = Math.Min(count, limit - offset);
+                    if (offset == 0 && min == bytes.Length)
+                    {
+                        stream.Write(bytes);
+                    }
+                    else
+                    {
+                        // TODO: is there a more efficient option than copying here?
+                        // Do we need to change the binding generator to accept
+                        // e.g. `Span`s?
+                        var copy = new byte[min];
+                        Array.Copy(bytes, offset, copy, 0, min);
+                        stream.Write(copy);
+                    }
+                    offset += min;
+                }
+            }
+        }
+
+        public override ValueTask WriteAsync(
+            ReadOnlyMemory<byte> buffer,
+            CancellationToken cancellationToken = default
+        )
+        {
+            // TODO: avoid copy when possible and use ArrayPool when not
+            var copy = new byte[buffer.Length];
+            buffer.Span.CopyTo(copy);
+            return new ValueTask(WriteAsync(copy, 0, buffer.Length, cancellationToken));
+        }
+
+        #region PlatformNotSupported
+
+        public override void Flush()
+        {
+            // ignore
+            //
+            // Note that flushing a `wasi:io/streams/output-stream` is an
+            // asynchronous operation, so it's not clear how we would
+            // implement it here instead of taking care of it as part of
+            // `WriteAsync`.
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        public override void SetLength(long length)
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        public override int Read(byte[] buffer, int offset, int length)
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int length)
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        public override long Length => throw new PlatformNotSupportedException();
+        public override long Position
+        {
+            get => throw new PlatformNotSupportedException();
+            set => throw new PlatformNotSupportedException();
+        }
+
+        #endregion
+    }
+}
