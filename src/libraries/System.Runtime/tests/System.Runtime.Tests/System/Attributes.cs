43b4a693ef84a7d6bab227427b5d8164cd468ed1
Buyaa Namnan <buyankhishig.namnan@microsoft.com>
2024-08-28 09:47:11 -0700

    Update src/libraries/System.Runtime/tests/System.Runtime.Tests/System/Attributes.cs

--- src/libraries/System.Runtime/tests/System.Runtime.Tests/System/Attributes.cs
+++ src/libraries/System.Runtime/tests/System.Runtime.Tests/System/Attributes.cs
@@ -290,7 +290,7 @@ namespace System.Tests
         }
 
         [Fact]
-        [ActiveIssue("https://github.com/dotnet/runtime/issues/56887", TestRuntimes.Mono)]
+        [ActiveIssue("https://github.com/dotnet/runtime/issues/107095", TestRuntimes.Mono)]
         public static void GetCustomAttributesOnOpenGenericBaseTypeRetrievesDerivedAttributes()
         {
             Attribute[] attributes = Attribute.GetCustomAttributes(typeof(HasGenericAttribute), typeof(GenericAttribute<>));
