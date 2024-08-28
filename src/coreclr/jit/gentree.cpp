605ec9838a8fe6ce1b2bbf1b1f6295e889e45151
EgorBo <egorbo@gmail.com>
2024-08-29 00:43:07 +0200

    Skip more covariance checks

--- src/coreclr/jit/gentree.cpp
+++ src/coreclr/jit/gentree.cpp
@@ -19174,6 +19174,24 @@ CORINFO_CLASS_HANDLE Compiler::gtGetClassHandle(GenTree* tree, bool* pIsExact, b
                         if (fieldType == TYP_REF)
                         {
                             objClass = fieldClass;
+                            if (!info.compCompHnd->isFieldStatic(fieldHnd))
+                            {
+                                // See if we can make objClass more specific, e.g. objClass is _Canon[]
+                                // but we can extract the real field type from field's owner:
+                                bool                 objIsExact, objIsNonNull;
+                                CORINFO_CLASS_HANDLE fieldOwnerObj = gtGetClassHandle(op1, &objIsExact, &objIsNonNull);
+                                if (fieldOwnerObj != NO_CLASS_HANDLE)
+                                {
+                                    CORINFO_CLASS_HANDLE fieldExactCls = NO_CLASS_HANDLE;
+                                    info.compCompHnd->getFieldType(fieldHnd, &fieldExactCls, fieldOwnerObj);
+
+                                    if ((fieldExactCls != NO_CLASS_HANDLE) && ((objClass == NO_CLASS_HANDLE) ||
+                                        info.compCompHnd->isMoreSpecificType(objClass, fieldExactCls)))
+                                    {
+                                        objClass = fieldExactCls;
+                                    }
+                                }
+                            }
                         }
                     }
                 }
