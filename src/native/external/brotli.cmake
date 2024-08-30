d717e9dce94c7697d4a846a4484b3b0bf30d5899
Jeremy Koritzinsky <jekoritz@microsoft.com>
2024-08-30 17:59:48 +0000

    Set the flags for brotli to allow exporting the brotli symbols from Compression.Native.

--- src/native/external/brotli.cmake
+++ src/native/external/brotli.cmake
@@ -10,6 +10,15 @@ set(__CURRENT_BUILD_SHARED_LIBS ${BUILD_SHARED_LIBS})
 set(BUILD_SHARED_LIBS OFF)
 FetchContent_MakeAvailable(brotli)
 set(BUILD_SHARED_LIBS ${__CURRENT_BUILD_SHARED_LIBS})
+
 target_compile_options(brotlicommon PRIVATE $<$<COMPILE_LANG_AND_ID:C,MSVC>:/guard:cf>)
 target_compile_options(brotlienc PRIVATE $<$<COMPILE_LANG_AND_ID:C,MSVC>:/guard:cf>)
 target_compile_options(brotlidec PRIVATE $<$<COMPILE_LANG_AND_ID:C,MSVC>:/guard:cf>)
+target_compile_options(brotlienc PRIVATE $<$<COMPILE_LANG_AND_ID:C,Clang,AppleClang,GNU>:-Wno-implicit-fallthrough>)
+target_compile_options(brotlidec PRIVATE $<$<COMPILE_LANG_AND_ID:C,Clang,AppleClang,GNU>:-Wno-implicit-fallthrough>)
+
+# Even though we aren't building brotli as shared libraries, we still need to be able to export the symbols
+# from the brotli libraries so that they can be used by System.IO.Compression.
+target_compile_definitions(brotlicommon PRIVATE BROTLI_SHARED_COMPILATION)
+target_compile_definitions(brotlienc PRIVATE BROTLI_SHARED_COMPILATION BROTLIENC_SHARED_COMPILATION)
+target_compile_definitions(brotlidec PRIVATE BROTLI_SHARED_COMPILATION BROTLIDEC_SHARED_COMPILATION)
