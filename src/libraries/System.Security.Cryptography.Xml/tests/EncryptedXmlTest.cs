5541c6cca3263f8ed6b3272e7480f418fb40b86d
Hootan Hemmati <hootanhemmati@outlook.com>
2024-09-02 07:20:11 +0330

    Update EncryptedXmlTest.cs

--- src/libraries/System.Security.Cryptography.Xml/tests/EncryptedXmlTest.cs
+++ src/libraries/System.Security.Cryptography.Xml/tests/EncryptedXmlTest.cs
@@ -1,4 +1,13 @@
-using System.Collections;
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+// EncryptedXmlTest.cs
+//
+// Author:
+//  Atsushi Enomoto  <atsushi@ximian.com>
+//
+// Copyright (C) 2006 Novell, Inc (http://www.novell.com)using System.Collections;
+
 using System.Collections.Generic;
 using System.IO;
 using System.Security.Cryptography.X509Certificates;
