d27ebe24ef914bbe15e5546a6380014938272616
Alan Hayward <alan.hayward@arm.com>
2024-09-04 15:19:54 +0100

    fix formatting

--- src/coreclr/jit/lowerarmarch.cpp
+++ src/coreclr/jit/lowerarmarch.cpp
@@ -4056,8 +4056,8 @@ GenTree* Lowering::LowerHWIntrinsicCndSel(GenTreeHWIntrinsic* cndSelNode)
             NamedIntrinsic nestedOp2Id = nestedOp2->AsHWIntrinsic()->GetHWIntrinsicId();
 
             // If the nested op uses Pg/Z, then inactive lanes will result in zeros, so can only transform if
-            // op3 is all zeros. Such a Csel operation is absorbed into the instruction when emitted. Skip this optimisation
-            // when the nestedOp is a reduce operation.
+            // op3 is all zeros. Such a Csel operation is absorbed into the instruction when emitted. Skip this
+            // optimisation when the nestedOp is a reduce operation.
 
             if (nestedOp1->IsMaskAllBitsSet() && !HWIntrinsicInfo::IsReduceOperation(nestedOp2Id) &&
                 (!HWIntrinsicInfo::IsZeroingMaskedOperation(nestedOp2Id) || op3->IsVectorZero()))
