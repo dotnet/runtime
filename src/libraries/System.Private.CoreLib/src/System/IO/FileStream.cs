3c4a4a3c7d34554c795f029cc3eb3bb76f38fd03
David Cantú <dacantu@microsoft.com>
2024-09-04 16:50:45 -0500

    Add missing documentation for new 9.0 APIs in System.IO

--- src/libraries/System.Private.CoreLib/src/System/IO/FileStream.cs
+++ src/libraries/System.Private.CoreLib/src/System/IO/FileStream.cs
@@ -536,6 +536,7 @@ namespace System.IO
             _strategy.CopyTo(destination, bufferSize);
         }
 
+        /// <inheritdoc />
         public override Task CopyToAsync(Stream destination, int bufferSize, CancellationToken cancellationToken)
         {
             ValidateCopyToArguments(destination, bufferSize);
