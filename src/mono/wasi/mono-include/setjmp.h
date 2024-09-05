ac9fd93ca75f920947a7a3609ccaa4e3d2a8939a
pavelsavara <pavel.savara@gmail.com>
2024-09-05 11:10:39 +0200

    fix WasmTestRunnerDir

--- src/mono/wasi/mono-include/setjmp.h
+++ src/mono/wasi/mono-include/setjmp.h
@@ -1,2 +1,5 @@
-// setjmp.h isn't provided by WASI SDK, but many Mono source files want to import it, so we need this here
+// setjmp.h is provided by WASI SDK, complains that:
+//    Setjmp/longjmp support requires Exception handling support, which is [not yet standardized](https://github.com/WebAssembly/proposals?tab=readme-ov-file#phase-3---implementation-phase-cg--wg).
+//    To enable it, compile with `-mllvm -wasm-enable-sjlj` and use an engine that implements the Exception handling proposal.
+// many Mono source files want to import it, so we need this here
 // We don't call any of its symbols at runtime so it can be left empty
