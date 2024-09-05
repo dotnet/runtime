fc7cce9d58a0919522a4182c7780d22631e5bb82
Aleksey Kliger <alklig@microsoft.com>
2024-09-05 11:47:30 -0400

    cleanup PipeChannel

--- src/coreclr/tools/superpmi/superpmi-shim-collector/pipechannel.cpp
+++ src/coreclr/tools/superpmi/superpmi-shim-collector/pipechannel.cpp
@@ -2,15 +2,93 @@
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/errno.h>
+#include <unistd.h>
 
 #include "pipechannel.hpp"
 
 PipeChannel PipeChannel::Create()
 {
     int fds[2] = {0,0};
-    if (pipe (fds) < 0) {
+    if (pipe (fds) < 0)
+    {
         perror ("pipe failed");
         abort();
     }
     return PipeChannel{fds[0], fds[1]};
 }
+
+int PipeChannel::Reader::ReadSome(char *dest, int destSize) const
+{
+    int toRead = destSize;
+    int haveRead = 0;
+    do
+    {
+        int res = read (m_readFd, dest, toRead);
+        if (res < 0)
+        {
+            if (errno == EINTR || errno == EAGAIN)
+            {
+                continue;
+            }
+            else
+            {
+                return res;
+            }
+        }
+        else if (res == 0)
+        {
+            break;
+        }
+        toRead -= res;
+        dest += res;
+        haveRead += res;
+    }
+    while (toRead > 0);
+    return haveRead;
+}
+
+int PipeChannel::Reader::ReadAll(char *dest, int destSize) const
+{
+    int res = ReadSome(dest, destSize);
+    if (res < 0)
+    {
+        return res;
+    }
+    else if (res != destSize)
+    {
+        return 0; // partial reads are not ok
+    }
+    else
+    {
+        return res;
+    }
+}
+
+int PipeChannel::Writer::SendAll(const char *buf, int bufSize) const
+{
+    const char *cur = buf;
+    int toWrite = bufSize;
+    int written = 0;
+    do
+    {
+        int res = write(m_writeFd, cur, toWrite);
+        if (res < 0)
+        {
+            if (errno == EINTR || errno == EAGAIN)
+            {
+                continue;
+            }
+            else
+            {
+                return res;
+            }
+        }
+        written += res;
+        cur += res;
+        toWrite -= res;
+    }
+    while (toWrite > 0);
+    return written;
+}
+
--- src/coreclr/tools/superpmi/superpmi-shim-collector/pipechannel.hpp
+++ src/coreclr/tools/superpmi/superpmi-shim-collector/pipechannel.hpp
@@ -3,54 +3,13 @@
 
 #include <utility>
 
-#include <string.h>
-#include <sys/errno.h>
-#include <unistd.h>
-
 class PipeChannel final
 {
-private:
-    int m_readFd;
-    int m_writeFd;
-
-    PipeChannel (int readFd, int writeFd) : m_readFd{readFd}, m_writeFd{writeFd} {}
 public:
-    class Reader;
-    class Writer;
-
     static PipeChannel Create();
-    ~PipeChannel()
-    {
-        if (m_writeFd >= 0)
-            close(m_writeFd);
-    }
+    ~PipeChannel() = default;
     PipeChannel (const PipeChannel&) = delete;
     PipeChannel& operator=(PipeChannel& other) = delete;
-
-    PipeChannel (PipeChannel &&other) : m_readFd{other.m_readFd}, m_writeFd{other.m_writeFd}
-    {
-        other.m_readFd = -1;
-        other.m_writeFd = -1;
-    }
-    PipeChannel& operator=(PipeChannel &&other) {
-        if (&other == this) {
-            return *this;
-        }
-        std::swap (m_readFd, other.m_readFd);
-        std::swap (m_writeFd, other.m_writeFd);
-        return *this;
-    }
-
-    Reader ExtractReader() {
-        int readFd = m_readFd;
-        m_readFd = -1;
-        return Reader(readFd);
-    }
-    Writer ExtractWriter() {
-        int writeFd = m_writeFd;
-        m_writeFd = -1;
-        return Writer(writeFd);
-    }
 
     class Reader final {
     private:
@@ -73,44 +32,19 @@ public:
             return *this;
         }
 
+        friend void swap(Reader& first, Reader& second) {
+            using std::swap;
+            swap(first.m_readFd, second.m_readFd);
+        }
+
         int ReadOne(char &dest) const
         {
-            return ReadFull(&dest, 1);
+            return ReadAll(&dest, 1);
         }
 
-        int ReadSome(char *dest, int destSize) const
-        {
-            int toRead = destSize;
-            int haveRead = 0;
-            do {
-                int res = read (m_readFd, dest, toRead);
-                if (res < 0) {
-                    if (errno == EINTR || errno == EAGAIN) {
-                        continue;
-                    } else {
-                        return res;
-                    }
-                } else if (res == 0) {
-                    break;
-                }
-                toRead -= res;
-                dest += res;
-                haveRead += res;
-            } while (toRead > 0);
-            return haveRead;
-        }
-
-        int ReadFull(char *dest, int destSize) const
-        {
-            int res = ReadSome(dest, destSize);
-            if (res < 0) {
-                return res;
-            } else if (res != destSize) {
-                return 0; // partial reads are not ok
-            } else {
-                return res;
-            }
-        }
+    private:
+        int ReadSome(char *dest, int destSize) const;
+        int ReadAll(char *dest, int destSize) const;
     };
 
     class Writer final {
@@ -134,32 +68,48 @@ public:
             return *this;
         }
 
-        int SendOne(char c) const
-        {
-            return SendFull(&c, sizeof(c));
+        friend void swap (Writer& first, Writer& second) {
+            using std::swap;
+            swap(first.m_writeFd, second.m_writeFd);
         }
 
-        int SendFull(const char *buf, int bufSize) const
+        int SendOne(char c) const
         {
-            const char *cur = buf;
-            int toWrite = bufSize;
-            int written = 0;
-            do {
-                int res = write(m_writeFd, cur, toWrite);
-                if (res < 0) {
-                    if (errno == EINTR || errno == EAGAIN) {
-                        continue;
-                    } else {
-                        return res;
-                    }
-                }
-                written += res;
-                cur += res;
-                toWrite -= res;
-            } while (toWrite > 0);
-            return written;
+            return SendAll(&c, sizeof(c));
         }
+
+    private:
+        int SendAll(const char *buf, int bufSize) const;
     };
+
+private:
+    Reader m_reader;
+    Writer m_writer;
+
+    PipeChannel (int readFd, int writeFd) : m_reader{readFd}, m_writer{writeFd} {}
+
+public:
+    PipeChannel (PipeChannel &&other) : m_reader{std::move(other.m_reader)}, m_writer{std::move(other.m_writer)}
+    {
+    }
+
+    PipeChannel& operator=(PipeChannel &&other) {
+        if (&other == this) {
+            return *this;
+        }
+        using std::swap;
+        swap (m_reader, other.m_reader);
+        swap (m_writer, other.m_writer);
+        return *this;
+    }
+
+    Reader ExtractReader() {
+        return std::move(m_reader);
+    }
+    Writer ExtractWriter() {
+        return std::move(m_writer);
+    }
+
 };
 
 #endif /*_PIPECHANNEL_HPP*/
