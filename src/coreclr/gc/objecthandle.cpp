dotnet/runtime

commit 815509cb9dba9fe72adc11119c2336e916a92966

Put handling of sized ref handles under FEATURE_SIZED_REF_HANDLES (#107326)

src/coreclr/gc/objecthandle.cpp
@@ -471,18 +471,20 @@ void CALLBACK ScanPointerForProfilerAndETW(_UNCHECKED_OBJECTREF *pObjRef, uintpt
     case    HNDTYPE_WEAK_INTERIOR_POINTER:
 #ifdef FEATURE_WEAK_NATIVE_COM_HANDLES
     case    HNDTYPE_WEAK_NATIVE_COM:
-#endif // FEATURE_WEAK_NATIVE_COM_HANDLES
+#endif
         rootFlags |= kEtwGCRootFlagsWeakRef;
         break;
 
     case    HNDTYPE_STRONG:
+#ifdef FEATURE_SIZED_REF_HANDLES
     case    HNDTYPE_SIZEDREF:
+#endif
         break;
 
     case    HNDTYPE_PINNED:
 #ifdef FEATURE_ASYNC_PINNED_HANDLES
     case    HNDTYPE_ASYNCPINNED:
-#endif // FEATURE_ASYNC_PINNED_HANDLES
+#endif
         rootFlags |= kEtwGCRootFlagsPinning;
         break;
 
@@ -1103,7 +1105,12 @@ void Ref_TraceNormalRoots(uint32_t condemned, uint32_t maxgen, ScanContext* sc, 
 
     // promote objects pointed to by strong handles
     // during ephemeral GCs we also want to promote the ones pointed to by sizedref handles.
-    uint32_t types[2] = {HNDTYPE_STRONG, HNDTYPE_SIZEDREF};
+    uint32_t types[2] = {
+        HNDTYPE_STRONG,
+#ifdef FEATURE_SIZED_REF_HANDLES
+        HNDTYPE_SIZEDREF
+#endif
+    };
     uint32_t uTypeCount = (((condemned >= maxgen) && !g_theGCHeap->IsConcurrentGCInProgress()) ? 1 : ARRAY_SIZE(types));
     uint32_t flags = (sc->concurrent) ? HNDGCF_ASYNC : HNDGCF_NORMAL;
 
@@ -1480,6 +1487,7 @@ void TraceDependentHandlesBySingleThread(HANDLESCANPROC pfnTrace, uintptr_t lp1,
     }
 }
 
+#ifdef FEATURE_SIZED_REF_HANDLES
 void ScanSizedRefByCPU(uint32_t maxgen, HANDLESCANPROC scanProc, ScanContext* sc, Ref_promote_func* fn, uint32_t flags)
 {
     HandleTableMap *walk = &g_HandleTableMap;
@@ -1519,6 +1527,7 @@ void Ref_ScanSizedRefHandles(uint32_t condemned, uint32_t maxgen, ScanContext* s
 
     ScanSizedRefByCPU(maxgen, CalculateSizedRefSize, sc, fn, flags);
 }
+#endif // FEATURE_SIZED_REF_HANDLES
 
 void Ref_CheckAlive(uint32_t condemned, uint32_t maxgen, ScanContext *sc)
 {
@@ -1602,7 +1611,9 @@ void Ref_UpdatePointers(uint32_t condemned, uint32_t maxgen, ScanContext* sc, Re
 #ifdef FEATURE_WEAK_NATIVE_COM_HANDLES
         HNDTYPE_WEAK_NATIVE_COM,
 #endif
+#ifdef FEATURE_SIZED_REF_HANDLES
         HNDTYPE_SIZEDREF,
+#endif
     };
 
     // perform a multi-type scan that updates pointers
@@ -1665,7 +1676,9 @@ void Ref_ScanHandlesForProfilerAndETW(uint32_t maxgen, uintptr_t lp1, handle_sca
 #ifdef FEATURE_ASYNC_PINNED_HANDLES
         HNDTYPE_ASYNCPINNED,
 #endif
+#ifdef FEATURE_SIZED_REF_HANDLES
         HNDTYPE_SIZEDREF,
+#endif
         HNDTYPE_WEAK_INTERIOR_POINTER
     };
 
@@ -1788,7 +1801,9 @@ void Ref_AgeHandles(uint32_t condemned, uint32_t maxgen, ScanContext* sc)
 #ifdef FEATURE_ASYNC_PINNED_HANDLES
         HNDTYPE_ASYNCPINNED,
 #endif
+#ifdef FEATURE_SIZED_REF_HANDLES
         HNDTYPE_SIZEDREF,
+#endif
         HNDTYPE_WEAK_INTERIOR_POINTER
     };
 
@@ -1843,7 +1858,9 @@ void Ref_RejuvenateHandles(uint32_t condemned, uint32_t maxgen, ScanContext* sc)
 #ifdef FEATURE_ASYNC_PINNED_HANDLES
         HNDTYPE_ASYNCPINNED,
 #endif
+#ifdef FEATURE_SIZED_REF_HANDLES
         HNDTYPE_SIZEDREF,
+#endif
         HNDTYPE_WEAK_INTERIOR_POINTER
     };
 
@@ -1896,7 +1913,9 @@ void Ref_VerifyHandleTable(uint32_t condemned, uint32_t maxgen, ScanContext* sc)
 #ifdef FEATURE_ASYNC_PINNED_HANDLES
         HNDTYPE_ASYNCPINNED,
 #endif
+#ifdef FEATURE_SIZED_REF_HANDLES
         HNDTYPE_SIZEDREF,
+#endif
         HNDTYPE_DEPENDENT,
         HNDTYPE_WEAK_INTERIOR_POINTER
     };

src/coreclr/debug/daccess/dacdbiimpl.cpp
@@ -7587,9 +7587,6 @@ UINT32 DacRefWalker::GetHandleWalkerMask()
     if (mHandleMask & CorHandleStrongDependent)
         result |= (1 << HNDTYPE_DEPENDENT);
 
-    if (mHandleMask & CorHandleStrongSizedByref)
-        result |= (1 << HNDTYPE_SIZEDREF);
-
     return result;
 }
 
@@ -7724,10 +7721,6 @@ HRESULT DacHandleWalker::Next(ULONG count, DacGcReference roots[], ULONG *pFetch
             case HNDTYPE_DEPENDENT:
                 roots[i].dwType = (DWORD)CorHandleStrongDependent;
                 roots[i].i64ExtraData = GetDependentHandleSecondary(CLRDATA_ADDRESS_TO_TADDR(handle.Handle)).GetAddr();
-                break;
-
-            case HNDTYPE_SIZEDREF:
-                roots[i].dwType = (DWORD)CorHandleStrongSizedByref;
                 break;
         }
     }

src/coreclr/debug/daccess/dacimpl.h
@@ -2171,7 +2171,7 @@ private:
     void WalkHandles();
     static inline bool IsAlwaysStrongReference(unsigned int type)
     {
-        return type == HNDTYPE_STRONG || type == HNDTYPE_PINNED || type == HNDTYPE_SIZEDREF;
+        return type == HNDTYPE_STRONG || type == HNDTYPE_PINNED;
     }
 
 private:

src/coreclr/debug/daccess/request.cpp
@@ -3800,7 +3800,7 @@ ClrDataAccess::GetThreadLocalModuleData(CLRDATA_ADDRESS thread, unsigned int ind
 HRESULT ClrDataAccess::GetHandleEnum(ISOSHandleEnum **ppHandleEnum)
 {
     unsigned int types[] = {HNDTYPE_WEAK_SHORT, HNDTYPE_WEAK_LONG, HNDTYPE_STRONG, HNDTYPE_PINNED, HNDTYPE_DEPENDENT,
-                            HNDTYPE_SIZEDREF, HNDTYPE_WEAK_INTERIOR_POINTER,
+                            HNDTYPE_WEAK_INTERIOR_POINTER,
 #if defined(FEATURE_COMINTEROP) || defined(FEATURE_COMWRAPPERS) || defined(FEATURE_OBJCMARSHAL)
                             HNDTYPE_REFCOUNTED,
 #endif // FEATURE_COMINTEROP || FEATURE_COMWRAPPERS || FEATURE_OBJCMARSHAL
@@ -3838,7 +3838,6 @@ HRESULT ClrDataAccess::GetHandleEnumForGC(unsigned int gen, ISOSHandleEnum **ppH
     SOSDacEnter();
 
     unsigned int types[] = {HNDTYPE_WEAK_SHORT, HNDTYPE_WEAK_LONG, HNDTYPE_STRONG, HNDTYPE_PINNED, HNDTYPE_DEPENDENT,
-                            HNDTYPE_SIZEDREF,
 #if defined(FEATURE_COMINTEROP) || defined(FEATURE_COMWRAPPERS) || defined(FEATURE_OBJCMARSHAL)
                             HNDTYPE_REFCOUNTED,
 #endif // FEATURE_COMINTEROP || FEATURE_COMWRAPPERS || FEATURE_OBJCMARSHAL

src/coreclr/gc/gc.cpp
@@ -21461,7 +21461,7 @@ bool gc_heap::init_table_for_region (int gen_number, heap_segment* region)
 // The following 2 methods Use integer division to prevent potential floating point exception.
 // FPE may occur if we use floating point division because of speculative execution.
 //
-// Return the percentage of efficiency (between 0 and 100) of the allocator. 
+// Return the percentage of efficiency (between 0 and 100) of the allocator.
 inline
 size_t gc_heap::generation_allocator_efficiency_percent (generation* inst)
 {
@@ -22947,7 +22947,7 @@ void gc_heap::gc1()
                 {
                     trigger_bgc_for_rethreading_p = false;
                 }
-            
+
                 process_datas_sample();
             }
 #endif //DYNAMIC_HEAP_COUNT
@@ -29751,7 +29751,10 @@ void gc_heap::mark_phase (int condemned_gen_number)
 #endif //USE_REGIONS
 
         GCToEEInterface::BeforeGcScanRoots(condemned_gen_number, /* is_bgc */ false, /* is_concurrent */ false);
+
+#ifdef FEATURE_SIZED_REF_HANDLES
         num_sizedrefs = GCToEEInterface::GetTotalNumSizedRefHandles();
+#endif // FEATURE_SIZED_REF_HANDLES
 
 #ifdef FEATURE_EVENT_TRACE
         informational_event_enabled_p = EVENT_ENABLED (GCMarkWithType);
@@ -29838,6 +29841,7 @@ void gc_heap::mark_phase (int condemned_gen_number)
         slow  = MAX_PTR;
 #endif //MULTIPLE_HEAPS
 
+#ifdef FEATURE_SIZED_REF_HANDLES
         if ((condemned_gen_number == max_generation) && (num_sizedrefs > 0))
         {
             GCScan::GcScanSizedRefs(GCHeap::Promote, condemned_gen_number, max_generation, &sc);
@@ -29859,6 +29863,7 @@ void gc_heap::mark_phase (int condemned_gen_number)
 #endif //MULTIPLE_HEAPS
             }
         }
+#endif // FEATURE_SIZED_REF_HANDLES
 
 #if defined(FEATURE_BASICFREEZE) && !defined(USE_REGIONS)
         if (ro_segments_in_range)
@@ -38384,7 +38389,9 @@ void gc_heap::background_mark_phase ()
         concurrent_print_time_delta ("CRWW");
 #endif // FEATURE_USE_SOFTWARE_WRITE_WATCH_FOR_GC_HEAP
 
+#ifdef FEATURE_SIZED_REF_HANDLES
         num_sizedrefs = GCToEEInterface::GetTotalNumSizedRefHandles();
+#endif // FEATURE_SIZED_REF_HANDLES
 
         // this c_write is not really necessary because restart_vm
         // has an instruction that will flush the cpu cache (interlocked
@@ -38463,6 +38470,7 @@ void gc_heap::background_mark_phase ()
 
     disable_preemptive (true);
 
+#ifdef FEATURE_SIZED_REF_HANDLES
     if (num_sizedrefs > 0)
     {
         GCScan::GcScanSizedRefs(background_promote, max_generation, max_generation, &sc);
@@ -38480,6 +38488,7 @@ void gc_heap::background_mark_phase ()
 
         disable_preemptive (true);
     }
+#endif // FEATURE_SIZED_REF_HANDLES
 
     dprintf (3,("BGC: handle table marking"));
     GCScan::GcScanHandles(background_promote,

src/coreclr/gc/gcinterface.h
@@ -483,6 +483,8 @@ typedef enum
      * are scanned as strong roots during each GC but only during full GCs would the size
      * be calculated.
      *
+     * NOTE: HNDTYPE_SIZEDREF is no longer used in the VM starting .NET 9
+     *       but we are keeping it here for backward compatibility purposes"
      */
     HNDTYPE_SIZEDREF     = 8,
 

src/coreclr/gc/gcscan.cpp
@@ -132,10 +132,12 @@ void GCScan::GcWeakPtrScanBySingleThread( int condemned, int max_gen, ScanContex
     GCToEEInterface::SyncBlockCacheWeakPtrScan(&CheckPromoted, (uintptr_t)sc, 0);
 }
 
+#ifdef FEATURE_SIZED_REF_HANDLES
 void GCScan::GcScanSizedRefs(promote_func* fn, int condemned, int max_gen, ScanContext* sc)
 {
     Ref_ScanSizedRefHandles(condemned, max_gen, sc, fn);
 }
+#endif // FEATURE_SIZED_REF_HANDLES
 
 void GCScan::GcShortWeakPtrScan(int condemned, int max_gen, ScanContext* sc)
 {

src/coreclr/gc/gcscan.h
@@ -33,8 +33,9 @@ struct DhContext
 class GCScan
 {
   public:
-
+#ifdef FEATURE_SIZED_REF_HANDLES
     static void GcScanSizedRefs(promote_func* fn, int condemned, int max_gen, ScanContext* sc);
+#endif // FEATURE_SIZED_REF_HANDLES
 
     // Regular stack Roots
     static void GcScanRoots (promote_func* fn, int condemned, int max_gen, ScanContext* sc);

commit ee1a9ee9da654aa2c832cd721ba10061d9dff4f1

Fix incorrect handle type count when FEATURE_SIZED_REF_HANDLES is not defined (#107433)
Regression from https://github.com/dotnet/runtime/pull/107326 - without `FEATURE_SIZED_REF_HANDLES` defined, this was incorrectly passing an array of two handle types - HNDTYPE_STRONG and HNDTYPE_WEAK_SHORT (0) - when promoting objects pointed to by strong handles.

src/coreclr/gc/objecthandle.cpp
@@ -1105,7 +1105,7 @@ void Ref_TraceNormalRoots(uint32_t condemned, uint32_t maxgen, ScanContext* sc, 
 
     // promote objects pointed to by strong handles
     // during ephemeral GCs we also want to promote the ones pointed to by sizedref handles.
-    uint32_t types[2] = {
+    uint32_t types[] = {
         HNDTYPE_STRONG,
 #ifdef FEATURE_SIZED_REF_HANDLES
         HNDTYPE_SIZEDREF