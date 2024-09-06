48d6e623d8896b4f46f6925a4bc3ba1dc52c9351
Alexander Köplinger <alex.koeplinger@outlook.com>
2024-09-06 21:44:43 +0200

    Update regex in legacy BinaryFormatterTests.cs too

--- src/libraries/System.Resources.Extensions/tests/BinaryFormatTests/Legacy/BinaryFormatterTests.cs
+++ src/libraries/System.Resources.Extensions/tests/BinaryFormatTests/Legacy/BinaryFormatterTests.cs
@@ -280,7 +280,7 @@ public partial class BinaryFormatterTests
         base64Blob = Encoding.UTF8.GetString(data);
 
 #pragma warning disable SYSLIB1045 // Convert to 'GeneratedRegexAttribute'.
-        return Regex.Replace(base64Blob, @"Version=\d.\d.\d.\d.", "Version=0.0.0.0", RegexOptions.Multiline)
+        return Regex.Replace(base64Blob, @"Version=\d+.\d+.\d+.\d+.", "Version=0.0.0.0", RegexOptions.Multiline)
             // Ignore the old Test key and Open public keys.
             .Replace("PublicKeyToken=cc7b13ffcd2ddd51", "PublicKeyToken=null")
             .Replace("PublicKeyToken=9d77cc7ad39b68eb", "PublicKeyToken=null")
