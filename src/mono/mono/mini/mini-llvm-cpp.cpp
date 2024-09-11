9b5b4abffdf814d273db8cac220e923afc808ddd
Radek Doulik <radek.doulik@gmail.com>
2024-09-11 17:04:16 +0200

    Set synchronous uw table kind on arm64

--- src/mono/mono/mini/mini-llvm-cpp.cpp
+++ src/mono/mono/mini/mini-llvm-cpp.cpp
@@ -485,7 +485,11 @@ mono_llvm_add_func_attr (LLVMValueRef func, AttrKind kind)
 {
 	unwrap<Function> (func)->addFnAttr (convert_attr (kind));
 	if (kind == LLVM_ATTR_UW_TABLE)
+#ifdef TARGET_ARM64
+		unwrap<Function> (func)->setUWTableKind (UWTableKind::Sync);
+#else
 		unwrap<Function> (func)->setUWTableKind (UWTableKind::Async);
+#endif
 }
 
 void
