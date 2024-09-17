90931363d009f0c61405ac1e0b7ffcb84e9c27cf
Ilona Tomkowicz <itomkowicz@microsoft.com>
2024-09-17 10:08:20 +0200

    Missing namespace

--- src/mono/wasm/Wasm.Build.Tests/BrowserRunner.cs
+++ src/mono/wasm/Wasm.Build.Tests/BrowserRunner.cs
@@ -6,6 +6,7 @@
 using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.Playwright;
