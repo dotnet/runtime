6c7e29f25e4adf3ddca12950cebe0d5dbad8afea
Tomek Sowiński <tomeksowi@gmail.com>
2024-09-18 11:06:18 +0200

    Harden Test_ShufflingThunk_PackedEmptyUintEmptyFloat_PackedEmptyDouble to cover interleaving FP and int arguments

--- src/tests/JIT/Directed/StructABI/EmptyStructs.cs
+++ src/tests/JIT/Directed/StructABI/EmptyStructs.cs
@@ -1833,9 +1833,11 @@ public static class Program
 	[MethodImpl(MethodImplOptions.NoInlining)]
 	private static void ShufflingThunk_PackedEmptyUintEmptyFloat_PackedEmptyDouble(
 		int a1_to_a0, int a2_to_a1, int a3_to_a2, int a4_to_a3, int a5_to_a4, int a6_to_a5, int a7_to_a6,
-		float fa0, float fa1, float fa2, float fa3, float fa4, float fa5, float fa6,
-		PackedEmptyUintEmptyFloat stack0_stack1_to_a7_fa7,
+		float fa0, float fa1,
+		PackedEmptyUintEmptyFloat stack0_stack1_to_a7_fa2,
+		float fa2_to_fa3, float fa3_to_fa4, float fa4_to_fa5,
 		int stack2_to_stack0,
+		float fa5_to_fa6, float fa6_to_fa7,
 		PackedEmptyDouble fa7_to_stack1_stack2)
 	{
 		Assert.Equal(0, a1_to_a0);
@@ -1847,13 +1849,13 @@ public static class Program
 		Assert.Equal(6, a7_to_a6);
 		Assert.Equal(0f, fa0);
 		Assert.Equal(1f, fa1);
-		Assert.Equal(2f, fa2);
-		Assert.Equal(3f, fa3);
-		Assert.Equal(4f, fa4);
-		Assert.Equal(5f, fa5);
-		Assert.Equal(6f, fa6);
-		Assert.Equal(PackedEmptyUintEmptyFloat.Get(), stack0_stack1_to_a7_fa7);
+		Assert.Equal(PackedEmptyUintEmptyFloat.Get(), stack0_stack1_to_a7_fa2);
+		Assert.Equal(2f, fa2_to_fa3);
+		Assert.Equal(3f, fa3_to_fa4);
+		Assert.Equal(4f, fa4_to_fa5);
 		Assert.Equal(7, stack2_to_stack0);
+		Assert.Equal(5f, fa5_to_fa6);
+		Assert.Equal(6f, fa6_to_fa7);
 		Assert.Equal(PackedEmptyDouble.Get(), fa7_to_stack1_stack2);
 	}
 
@@ -1862,8 +1864,8 @@ public static class Program
 	{
 		var getDelegate = [MethodImpl(MethodImplOptions.NoOptimization)] ()
 			=> ShufflingThunk_PackedEmptyUintEmptyFloat_PackedEmptyDouble;
-		getDelegate()(0, 1, 2, 3, 4, 5, 6, 0f, 1f, 2f, 3f, 4f, 5f, 6f,
-			PackedEmptyUintEmptyFloat.Get(), 7, PackedEmptyDouble.Get());
+		getDelegate()(0, 1, 2, 3, 4, 5, 6, 0f, 1f,
+			PackedEmptyUintEmptyFloat.Get(), 2f, 3f, 4f, 7, 5f, 6f, PackedEmptyDouble.Get());
 	}
 
 
