709c196cbed047f7fc6b8f6360bb064a5716517f
Mohamad <80650043+MDshirzad@users.noreply.github.com>
2024-09-22 22:44:15 +0400

    Adding extension method for adding header by dictinary which will be used in Httpclient class

---
+++ src/libraries/System.Net.Http/src/System/Net/Http/HttpClientExtensions.cs
@@ -0,0 +1,31 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Net.Http.System.Net.Http
+{
+    public static class HttpClientExtensions
+    {
+
+        public static Task AddHeader(this HttpClient httpclinet, Dictionary<string, string> dictionary)
+        {
+            if (dictionary == null)
+                return;
+            foreach (var item in dictionary)
+            {
+                if (!client.DefaultRequestHeaders.Contains(header.Key))
+                {
+                    httpclinet.DefaultRequestHeaders.Add(item.Key, item.Value);
+                }
+            }
+            return Task.CompletedTask;
+
+
+        }
+    }
+}
