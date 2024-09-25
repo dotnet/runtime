bdcfb10eec930bbe1b577f05f3b5165ef8fd2e05
Marek Fišera <mara@neptuo.com>
2024-09-25 10:20:39 +0200

    [browser] Fix passing parameter to test added in #108190 (#108199)

--- src/mono/wasm/Wasm.Build.Tests/TestAppScenarios/SatelliteLoadingTests.cs
+++ src/mono/wasm/Wasm.Build.Tests/TestAppScenarios/SatelliteLoadingTests.cs
@@ -35,14 +35,24 @@ public class SatelliteLoadingTests : AppTestBase
         var result = await RunSdkStyleAppForBuild(new(
             Configuration: "Debug", 
             TestScenario: "SatelliteAssembliesTest",
-            BrowserQueryString: new Dictionary<string, string> { ["loadAllSatelliteResources"] = loadAllSatelliteResources.ToString() }
+            BrowserQueryString: new Dictionary<string, string> { ["loadAllSatelliteResources"] = loadAllSatelliteResources.ToString().ToLowerInvariant() }
         ));
+
+        var expectedOutput = new List<Action<string>>();
+        if (!loadAllSatelliteResources)
+        {
+            // If we are loading all satellite, we don't have a way to test resources without satellite assemblies being loaded.
+            // So there messages are should be present only when we are lazily loading satellites.
+            expectedOutput.Add(m => Assert.Equal("default: hello", m));
+            expectedOutput.Add(m => Assert.Equal("es-ES without satellite: hello", m));
+        }
+
+        expectedOutput.Add(m => Assert.Equal("default: hello", m));
+        expectedOutput.Add(m => Assert.Equal("es-ES with satellite: hola", m));
+
         Assert.Collection(
             result.TestOutput,
-            m => Assert.Equal("default: hello", m),
-            m => Assert.Equal("es-ES without satellite: hello", m),
-            m => Assert.Equal("default: hello", m),
-            m => Assert.Equal("es-ES with satellite: hola", m)
+            expectedOutput.ToArray()
         );
     }
 
