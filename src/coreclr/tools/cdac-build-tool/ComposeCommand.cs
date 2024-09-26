2012f4660cd0dd0948990716c608d6d07ddabef2
Aleksey Kliger <alklig@microsoft.com>
2024-09-26 14:00:16 -0400

    don't embed the template source file in the tool

--- src/coreclr/tools/cdac-build-tool/ComposeCommand.cs
+++ src/coreclr/tools/cdac-build-tool/ComposeCommand.cs
@@ -14,6 +14,7 @@ internal sealed class ComposeCommand : CliCommand
     private readonly CliOption<string> outputFile = new("-o") { Arity = ArgumentArity.ExactlyOne, HelpName = "OUTPUT", Required = true, Description = "Output file" };
     private readonly CliOption<string[]> contractFile = new("-c") { Arity = ArgumentArity.ZeroOrMore, HelpName = "CONTRACT", Description = "Contract file (may be specified multiple times)" };
     private readonly CliOption<string> baselinePath = new("-b", "--baseline") { Arity = ArgumentArity.ExactlyOne, HelpName = "BASELINEPATH", Description = "Directory containing the baseline contracts"};
+    private readonly CliOption<string> templateFile = new ("-i", "--input-template") { Arity = ArgumentArity.ExactlyOne, HelpName = "TEMPLATE", Description = "Contract descriptor template to be filled in" };
     private readonly CliOption<bool> _verboseOption;
     public ComposeCommand(CliOption<bool> verboseOption) : base("compose")
     {
@@ -22,6 +23,7 @@ internal sealed class ComposeCommand : CliCommand
         Add(outputFile);
         Add(contractFile);
         Add(baselinePath);
+        Add(templateFile);
         SetAction(Run);
     }
 
@@ -49,6 +51,18 @@ internal sealed class ComposeCommand : CliCommand
         if (!System.IO.Directory.Exists(baselinesDir))
         {
             Console.Error.WriteLine($"Baseline path {baselinesDir} does not exist");
+            return 1;
+        }
+        var templateFilePath = parse.GetValue(templateFile);
+        if (templateFilePath == null)
+        {
+            Console.Error.WriteLine("No template file specified");
+            return 1;
+        }
+        templateFilePath = System.IO.Path.GetFullPath(templateFilePath);
+        if (!System.IO.File.Exists(templateFilePath))
+        {
+            Console.Error.WriteLine($"Template file {templateFilePath} does not exist");
             return 1;
         }
         var contracts = parse.GetValue(contractFile);
@@ -84,7 +98,7 @@ internal sealed class ComposeCommand : CliCommand
         }
         EnsureDirectoryExists(output);
         using var writer = new System.IO.StreamWriter(output);
-        var emitter = new ContractDescriptorSourceFileEmitter();
+        var emitter = new ContractDescriptorSourceFileEmitter(templateFilePath);
         emitter.SetPlatformFlags(model.PlatformFlags);
         emitter.SetPointerDataCount(model.PointerDataCount);
         emitter.SetJsonDescriptor(model.ToJson());
