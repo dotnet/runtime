88f74b9f836f0d3a118d2e95b154d17393ba97a5
Ilona Tomkowicz <32700855+ilonatommy@users.noreply.github.com>
2024-09-26 08:08:27 +0000

    Whitespace

--- src/mono/wasm/Wasm.Build.Tests/Common/GlobalizationMode.cs
+++ src/mono/wasm/Wasm.Build.Tests/Common/GlobalizationMode.cs
@@ -10,5 +10,5 @@ public enum GlobalizationMode
     Invariant,       // no icu
     FullIcu,         // full icu data: icudt.dat is loaded
     Custom,          // user set WasmIcuDataFileName/BlazorIcuDataFileName value and we are loading that file
-    Hybrid           // reduced icu, missing data is provided by platform-native functions (web api for wasm)    
+    Hybrid           // reduced icu, missing data is provided by platform-native functions (web api for wasm)
 };
