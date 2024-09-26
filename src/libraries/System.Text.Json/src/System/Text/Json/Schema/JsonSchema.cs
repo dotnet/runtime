931ea604f99918f8020fba95e25f2a0b0c788822
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2024-09-26 19:00:11 +0100

    Fix handling of appending keywords to boolean schemas. (#108246)
    * Fix handling of appending keywords to boolean schemas.
    
    * Add a few comments documenting boolean schemas.

--- src/libraries/System.Text.Json/src/System/Text/Json/Schema/JsonSchema.cs
+++ src/libraries/System.Text.Json/src/System/Text/Json/Schema/JsonSchema.cs
@@ -34,6 +34,14 @@ namespace System.Text.Json.Schema
 
         public bool IsTrue => _trueOrFalse is true;
         public bool IsFalse => _trueOrFalse is false;
+
+        /// <summary>
+        /// Per the JSON schema core specification section 4.3
+        /// (https://json-schema.org/draft/2020-12/json-schema-core#name-json-schema-documents)
+        /// A JSON schema must either be an object or a boolean.
+        /// We represent false and true schemas using this flag.
+        /// It is not possible to specify keywords in boolean schemas.
+        /// </summary>
         private readonly bool? _trueOrFalse;
 
         public string? Ref { get => _ref; set { VerifyMutable(); _ref = value; } }
@@ -95,6 +103,7 @@ namespace System.Text.Json.Schema
             {
                 if (_trueOrFalse != null)
                 {
+                    // Boolean schemas admit no keywords
                     return 0;
                 }
 
@@ -129,6 +138,7 @@ namespace System.Text.Json.Schema
         {
             if (_trueOrFalse != null)
             {
+                // boolean schemas do not admit type keywords.
                 return;
             }
 
@@ -257,6 +267,23 @@ namespace System.Text.Json.Schema
                 }
 
                 return schema;
+            }
+        }
+
+        /// <summary>
+        /// If the schema is boolean, replaces it with a semantically
+        /// equivalent object schema that allows appending keywords.
+        /// </summary>
+        public static void EnsureMutable(ref JsonSchema schema)
+        {
+            switch (schema._trueOrFalse)
+            {
+                case false:
+                    schema = new JsonSchema { Not = True };
+                    break;
+                case true:
+                    schema = new JsonSchema();
+                    break;
             }
         }
 
