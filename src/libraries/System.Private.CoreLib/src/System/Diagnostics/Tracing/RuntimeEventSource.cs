dotnet/runtime

commit 679a284f54365145a3c178fe4b79914dc32f7380

EventSource startup initialization (#106014)
* EventSource startup initialization

Fixes https://github.com/dotnet/runtime/issues/105845

Previously MetricsEventSource wasn't being created for apps that didn't ever create a Meter. This caused a chicken-and-egg problem for RuntimeMetrics which weren't created until MetricsEventSource started a tracing session. This change ensures that MetricsEventSource will be created on demand if ETW, EventPipe, or EventListener based tooling starts a tracing session. I took some extra effort to create the EventSource in a deferred fashion to avoid eager loading System.Diagnostics.DiagnosticSource.dll when it might never be needed.

Aside from the fix there were some small improvements:
- Moved NativeRuntimeEventSource to initialize in the same place as other startup EventSources
- Removed a useless lock(EventListener.EventListenersLock) around EventPipe eventProvider registration

src/libraries/System.Private.CoreLib/src/System/Diagnostics/Tracing/RuntimeEventSource.cs
@@ -14,6 +14,7 @@ namespace System.Diagnostics.Tracing
     [EventSourceAutoGenerate]
     internal sealed partial class RuntimeEventSource : EventSource
     {
+        internal static readonly Guid EventSourceGuid = new Guid("49592C0F-5A05-516D-AA4B-A64E02026C89");
         internal const string EventSourceName = "System.Runtime";
 
         public static class Keywords
@@ -22,7 +23,7 @@ namespace System.Diagnostics.Tracing
             public const EventKeywords ProcessorCount = (EventKeywords)0x2;
         }
 
-        private static RuntimeEventSource? s_RuntimeEventSource;
+        internal static RuntimeEventSource? Log => new RuntimeEventSource();
         private PollingCounter? _gcHeapSizeCounter;
         private IncrementingPollingCounter? _gen0GCCounter;
         private IncrementingPollingCounter? _gen1GCCounter;
@@ -50,21 +51,6 @@ namespace System.Diagnostics.Tracing
         private PollingCounter? _ilBytesJittedCounter;
         private PollingCounter? _methodsJittedCounter;
         private IncrementingPollingCounter? _jitTimeCounter;
-
-#if NATIVEAOT
-        // If EventSource feature is enabled, RuntimeEventSource needs to be initialized for NativeAOT
-        // In CoreCLR, this is done via a call from the runtime as part of coreclr_initialize
-#pragma warning disable CA2255
-        [ModuleInitializer]
-#pragma warning restore CA2255
-#endif
-        public static void Initialize()
-        {
-            // initializing more than once may lead to missing events
-            Debug.Assert(s_RuntimeEventSource == null);
-            if (IsSupported)
-                s_RuntimeEventSource = new RuntimeEventSource();
-        }
 
         // Parameterized constructor to block initialization and ensure the EventSourceGenerator is creating the default constructor
         // as you can't make a constructor partial.

src/coreclr/vm/corelib.h
@@ -775,8 +775,8 @@ DEFINE_FIELD_U(rgiColumnNumber,            StackFrameHelper,   rgiColumnNumber)
 DEFINE_FIELD_U(rgiLastFrameFromForeignExceptionStackTrace,            StackFrameHelper,   rgiLastFrameFromForeignExceptionStackTrace)
 DEFINE_FIELD_U(iFrameCount,                StackFrameHelper,   iFrameCount)
 
-DEFINE_CLASS(RUNTIME_EVENT_SOURCE,  Tracing,                RuntimeEventSource)
-DEFINE_METHOD(RUNTIME_EVENT_SOURCE, INITIALIZE, Initialize, SM_RetVoid)
+DEFINE_CLASS(EVENT_SOURCE,           Tracing,            EventSource)
+DEFINE_METHOD(EVENT_SOURCE,          INITIALIZE_DEFAULT_EVENT_SOURCES, InitializeDefaultEventSources, SM_RetVoid)
 
 DEFINE_CLASS(STARTUP_HOOK_PROVIDER,  System,                StartupHookProvider)
 DEFINE_METHOD(STARTUP_HOOK_PROVIDER, MANAGED_STARTUP, ManagedStartup, SM_PtrChar_RetVoid)

src/coreclr/vm/corhost.cpp
@@ -665,11 +665,11 @@ HRESULT CorHost2::CreateAppDomainWithManager(
     m_fAppDomainCreated = TRUE;
 
 #ifdef FEATURE_PERFTRACING
-    // Initialize RuntimeEventSource
+    // Initialize default event sources
     {
         GCX_COOP();
-        MethodDescCallSite initRuntimeEventSource(METHOD__RUNTIME_EVENT_SOURCE__INITIALIZE);
-        initRuntimeEventSource.Call(NULL);
+        MethodDescCallSite initEventSources(METHOD__EVENT_SOURCE__INITIALIZE_DEFAULT_EVENT_SOURCES);
+        initEventSources.Call(NULL);
     }
 #endif // FEATURE_PERFTRACING
 

src/libraries/System.Diagnostics.DiagnosticSource/src/ILLink/ILLink.Descriptors.LibraryBuild.xml
@@ -1,0 +1,8 @@
+﻿<linker>
+  <assembly fullname="System.Diagnostics.DiagnosticSource">
+    <type fullname="System.Diagnostics.Metrics.MetricsEventSource">
+      <!-- Used by System.Private.CoreLib via reflection to init the EventSource -->
+      <method name="GetInstance" />
+    </type>
+  </assembly>
+</linker>

src/libraries/System.Diagnostics.DiagnosticSource/src/System.Diagnostics.DiagnosticSource.csproj
@@ -25,6 +25,7 @@ System.Diagnostics.DiagnosticSource</PackageDescription>
 
   <ItemGroup>
     <ILLinkSubstitutionsXmls Include="ILLink/ILLink.Substitutions.Shared.xml" />
+    <Content Include="ILLink\ILLink.Descriptors.LibraryBuild.xml" />
   </ItemGroup>
 
   <ItemGroup>

src/libraries/System.Diagnostics.DiagnosticSource/src/System/Diagnostics/Metrics/MetricsEventSource.cs
@@ -46,6 +46,11 @@ namespace System.Diagnostics.Metrics
     {
         public static readonly MetricsEventSource Log = new();
 
+        // Although this API isn't public, it is invoked via reflection from System.Private.CoreLib and needs the same back-compat
+        // consideration as a public API. See EventSource.InitializeDefaultEventSources() in System.Private.CoreLib source for more
+        // details. We have a unit test GetInstanceMethodIsReflectable that verifies this method isn't accidentally removed or renamed.
+        public static MetricsEventSource GetInstance() { return Log; }
+
         private const string SharedSessionId = "SHARED";
         private const string ClientIdKey = "ClientId";
         private const string MaxHistogramsKey = "MaxHistograms";

src/libraries/System.Diagnostics.DiagnosticSource/tests/MetricEventSourceTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Diagnostics.Tracing;
 using System.Globalization;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
@@ -24,6 +25,23 @@ namespace System.Diagnostics.Metrics.Tests
         public MetricEventSourceTests(ITestOutputHelper output)
         {
             _output = output;
+        }
+
+        [Fact]
+        public void GetInstanceMethodIsReflectable()
+        {
+            // The startup code in System.Private.CoreLib needs to be able to get the MetricsEventSource instance via reflection. See EventSource.InitializeDefaultEventSources() in
+            // the System.Private.CoreLib source.
+            // Even though the the type isn't public this test ensures the GetInstance() API isn't removed or renamed.
+            Type? metricsEventSourceType = Type.GetType("System.Diagnostics.Metrics.MetricsEventSource, System.Diagnostics.DiagnosticSource", throwOnError: false);
+            Assert.True(metricsEventSourceType != null, "Unable to get MetricsEventSource type via reflection");
+            
+            MethodInfo? getInstanceMethod = metricsEventSourceType.GetMethod("GetInstance", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static, null, Type.EmptyTypes, null);
+            Assert.True(getInstanceMethod != null, "Unable to get MetricsEventSource.GetInstance method via reflection");
+
+            object? o = getInstanceMethod.Invoke(null, null);
+            Assert.True(o != null, "Expected non-null result invoking MetricsEventSource.GetInstance() via reflection");
+            Assert.True(o is EventSource, "Expected object returned from MetricsEventSource.GetInstance() to be assignable to EventSource");
         }
 
         [ConditionalFact(typeof(PlatformDetection), nameof(PlatformDetection.IsNotBrowser))]

src/libraries/System.Diagnostics.Tracing/tests/BasicEventSourceTest/TestUtilities.cs
@@ -31,6 +31,7 @@ namespace BasicEventSourceTests
                     eventSource.Name != "System.Reflection.Runtime.Tracing" &&
                     eventSource.Name != "Microsoft-Windows-DotNETRuntime" &&
                     eventSource.Name != "System.Runtime" &&
+                    eventSource.Name != "System.Diagnostics.Metrics" &&
 
                     // event source from xunit runner
                     eventSource.Name != "xUnit.TestEventSource" &&

commit 085a604257f85924304ff149979de238363cdd23

Root the System.Runtime EventSource
The System.Runtime EventSource (RuntimeEventSource), was unintentionally being garbage collected because it wasn't rooted. This caused runtime EventCounters to no longer be available after GC occurred.

This was a regression from a recent change (https://github.com/dotnet/runtime/pull/106014). That change accidentally converted the static field that was intended to the root the object into a property getter that returned a new instance each time it was called. This fix converts the property back into a statically initialized field.

This will fix #107919 once it is backported.

src/libraries/System.Private.CoreLib/src/System/Diagnostics/Tracing/RuntimeEventSource.cs
@@ -23,7 +23,7 @@ namespace System.Diagnostics.Tracing
             public const EventKeywords ProcessorCount = (EventKeywords)0x2;
         }
 
-        internal static RuntimeEventSource? Log => new RuntimeEventSource();
+        internal static RuntimeEventSource? Log = new RuntimeEventSource();
         private PollingCounter? _gcHeapSizeCounter;
         private IncrementingPollingCounter? _gen0GCCounter;
         private IncrementingPollingCounter? _gen1GCCounter;