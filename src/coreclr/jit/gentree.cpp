9085483bb17e0ad90bfaadb3ab47a9ea3de736eb
Alex Covington (Advanced Micro Devices <b-alexco@microsoft.com>
2024-09-27 13:50:59 -0700

    Remove conditional, use assert similar to GT_RSZ case

--- src/coreclr/jit/gentree.cpp
+++ src/coreclr/jit/gentree.cpp
@@ -21020,20 +21020,17 @@ GenTree* Compiler::gtNewSimdBinOpNode(
 
         case GT_LSH:
         {
-            if (varTypeIsByte(simdBaseType))
-            {
-                unsigned shiftCountMask = (genTypeSize(simdBaseType) * 8) - 1;
-                GenTree* andShiftVal =
-                    gtNewOperNode(GT_AND, op2ForLookup->TypeGet(), op2ForLookup, gtNewIconNode(shiftCountMask));
-                GenTree* op3 =
-                    gtNewSimdBinOpNode(GT_LSH, type, op1, andShiftVal,
-                                       varTypeIsUnsigned(simdBaseType) ? CORINFO_TYPE_USHORT : CORINFO_TYPE_SHORT,
-                                       simdSize);
-                GenTree* maskElement =
-                    gtNewOperNode(GT_LSH, andShiftVal->TypeGet(), gtNewIconNode(0xFF), gtCloneExpr(andShiftVal));
-                GenTree* mask = gtNewSimdCreateBroadcastNode(type, maskElement, simdBaseJitType, simdSize);
-                return gtNewSimdBinOpNode(GT_AND, type, op3, mask, simdBaseJitType, simdSize);
-            }
+            assert(varTypeIsByte(simdBaseType)) unsigned shiftCountMask = (genTypeSize(simdBaseType) * 8) - 1;
+            GenTree*                                     andShiftVal =
+                gtNewOperNode(GT_AND, op2ForLookup->TypeGet(), op2ForLookup, gtNewIconNode(shiftCountMask));
+            GenTree* op3 =
+                gtNewSimdBinOpNode(GT_LSH, type, op1, andShiftVal,
+                                   varTypeIsUnsigned(simdBaseType) ? CORINFO_TYPE_USHORT : CORINFO_TYPE_SHORT,
+                                   simdSize);
+            GenTree* maskElement =
+                gtNewOperNode(GT_LSH, andShiftVal->TypeGet(), gtNewIconNode(0xFF), gtCloneExpr(andShiftVal));
+            GenTree* mask = gtNewSimdCreateBroadcastNode(type, maskElement, simdBaseJitType, simdSize);
+            return gtNewSimdBinOpNode(GT_AND, type, op3, mask, simdBaseJitType, simdSize);
         }
 #endif // TARGET_XARCH
 
