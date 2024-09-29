bc01b1175a114bbad7919195dbc0817ca92c554b
Miha Zupan <mihazupan.zupan1@gmail.com>
2024-09-29 08:11:19 +0200

    Speed up single-value SearchValues<string> candidate verification

--- src/libraries/System.Private.CoreLib/src/System/String.Comparison.cs
+++ src/libraries/System.Private.CoreLib/src/System/String.Comparison.cs
@@ -27,9 +27,9 @@ namespace System
             Debug.Assert(strA.Length == strB.Length);
 
             return SpanHelpers.SequenceEqual(
-                    ref Unsafe.As<char, byte>(ref strA.GetRawStringData()),
-                    ref Unsafe.As<char, byte>(ref strB.GetRawStringData()),
-                    ((uint)strA.Length) * sizeof(char));
+                ref strA.GetRawStringDataAsUInt8(),
+                ref strB.GetRawStringDataAsUInt8(),
+                ((uint)strA.Length) * sizeof(char));
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -1138,8 +1138,8 @@ namespace System
                     return (value.Length == 1) ?
                             true :                 // First char is the same and thats all there is to compare
                             SpanHelpers.SequenceEqual(
-                                ref Unsafe.As<char, byte>(ref this.GetRawStringData()),
-                                ref Unsafe.As<char, byte>(ref value.GetRawStringData()),
+                                ref this.GetRawStringDataAsUInt8(),
+                                ref value.GetRawStringDataAsUInt8(),
                                 ((nuint)value.Length) * 2);
 
                 case StringComparison.OrdinalIgnoreCase:
