e12d70a9a1512612359aa3d8e7d5b378ecaf2a59
Marek Fišera <mara@neptuo.com>
2024-10-07 14:52:31 +0200

    More waits to stop the build

--- src/mono/wasm/Wasm.Build.Tests/BuildTestBase.cs
+++ src/mono/wasm/Wasm.Build.Tests/BuildTestBase.cs
@@ -178,7 +178,7 @@ namespace Wasm.Build.Tests
                 throw new XunitException($"Build should have failed, but it didn't. Process exited with exitCode : {res.ExitCode}");
 
             // Ensure we got all output.
-            if ((res.ExitCode == 0 && !res.Output.Contains("Build succeeded")) || (res.ExitCode != 0 && !res.Output.Contains("Build failed") && !res.Output.Contains("Restore failed")))
+            if ((res.ExitCode == 0 && !res.Output.Contains("Build succeeded")) || (res.ExitCode != 0 && !res.Output.Contains("Build failed") && !res.Output.Contains("Restore failed") && !res.Output.Contains("Stopping the build")))
             {
                 _testOutput.WriteLine("Replacing dotnet process output with messages from binlog");
 
