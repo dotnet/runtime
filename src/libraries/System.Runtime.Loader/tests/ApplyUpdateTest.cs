a97ac53d73628019ea88abd3c067a55a57404c2c
mikelle-rogers <mirogers@microsoft.com>
2024-10-09 11:49:52 -0700

    enable ApplyUpdateTest

--- src/libraries/System.Runtime.Loader/tests/ApplyUpdateTest.cs
+++ src/libraries/System.Runtime.Loader/tests/ApplyUpdateTest.cs
@@ -884,7 +884,6 @@ namespace System.Reflection.Metadata
             });
         }
 
-        [ActiveIssue("https://github.com/dotnet/runtime/issues/87574", TestRuntimes.CoreCLR)]
         [ConditionalFact(typeof(ApplyUpdateUtil), nameof(ApplyUpdateUtil.IsSupported))]
         public static void TestGenericAddInstanceField()
         {
