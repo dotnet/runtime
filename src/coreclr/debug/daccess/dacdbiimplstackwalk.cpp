d907479fe186126c6f68135d9b6978e9e3f7d8c5
Adeel <3840695+am11@users.noreply.github.com>
2024-10-10 02:01:27 +0300

    Align header style in clr debug

--- src/coreclr/debug/daccess/dacdbiimplstackwalk.cpp
+++ src/coreclr/debug/daccess/dacdbiimplstackwalk.cpp
@@ -1,12 +1,10 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-//
-// DacDbiImplStackWalk.cpp
-//
 
+//*****************************************************************************
+// File: DacDbiImplStackWalk.cpp
 //
 // This file contains the implementation of the stackwalking-related functions on the DacDbiInterface.
-//
 // ======================================================================================
 
 #include "stdafx.h"
--- src/coreclr/debug/daccess/fntableaccess.h
+++ src/coreclr/debug/daccess/fntableaccess.h
@@ -1,8 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-//
-
 #ifdef _MSC_VER
 #pragma once
 #endif
