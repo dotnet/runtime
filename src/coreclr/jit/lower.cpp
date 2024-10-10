dded5c94bbb2e65be24f858ad9123a3ff6f83b7b
EgorBo <egorbo@gmail.com>
2024-10-10 02:49:20 +0200

    test2

--- src/coreclr/jit/lower.cpp
+++ src/coreclr/jit/lower.cpp
@@ -5695,15 +5695,18 @@ GenTree* Lowering::LowerDelegateInvoke(GenTreeCall* call)
     BlockRange().InsertBefore(call, newThisAddr, newThis, thisArgNode);
 
 //#if !defined(TARGET_XARCH)
-    if (comp->GetInterruptible())
+    //if (comp->GetInterruptible())
     {
         // If the target's backend doesn't support indirect calls with immediate operands (contained)
         // and the method is marked as interruptible, we need to insert a GT_START_NONGC before the call.
         // to keep the delegate object alive while we're obtaining the function pointer.
-        //GenTree* startNonGCNode = new (comp, GT_START_NONGC) GenTree(GT_START_NONGC, TYP_VOID);
-        //GenTree* stopNonGCNode  = new (comp, GT_STOP_NONGC) GenTree(GT_STOP_NONGC, TYP_VOID);
-        //BlockRange().InsertAfter(base, startNonGCNode);
-        //BlockRange().InsertAfter(call, stopNonGCNode);
+        GenTree* startNonGCNode = new (comp, GT_START_NONGC) GenTree(GT_START_NONGC, TYP_VOID);
+        GenTree* stopNonGCNode  = new (comp, GT_STOP_NONGC) GenTree(GT_STOP_NONGC, TYP_VOID);
+        BlockRange().InsertAfter(thisArgNode, startNonGCNode);
+        if (!call->IsTailCall())
+        {
+            BlockRange().InsertAfter(call, stopNonGCNode);
+        }
     }
 //#endif
 
