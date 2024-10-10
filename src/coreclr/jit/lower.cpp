bef38f38fc293a69862691ce73054d990cbda71b
EgorBo <egorbo@gmail.com>
2024-10-10 22:15:15 +0200

    clean up

--- src/coreclr/jit/lower.cpp
+++ src/coreclr/jit/lower.cpp
@@ -2917,18 +2917,19 @@ GenTree* Lowering::LowerCall(GenTree* node)
 
         BlockRange().InsertBefore(call, std::move(controlExprRange));
 
-#ifndef JIT32_GCENCODER
-        if (call->IsDelegateInvoke())
+#ifndef TARGET_XARCH
+        if (call->IsDelegateInvoke() && comp->GetInterruptible())
         {
             // If the target's backend doesn't support indirect calls with immediate operands (contained)
             // and the method is marked as interruptible, we need to insert a GT_START_NONGC before the call.
             // to keep the delegate object alive while we're obtaining the function pointer.
             GenTree* startNonGCNode = new (comp, GT_START_NONGC) GenTree(GT_START_NONGC, TYP_VOID);
-            GenTree* stopNonGCNode = new (comp, GT_STOP_NONGC) GenTree(GT_STOP_NONGC, TYP_VOID);
             BlockRange().InsertBefore(controlExpr, startNonGCNode);
             if (!call->IsTailCall())
             {
-                BlockRange().InsertBefore(call, stopNonGCNode);
+                GenTree* stopNonGCNode = new (comp, GT_STOP_NONGC) GenTree(GT_STOP_NONGC, TYP_VOID);
+                BlockRange().InsertAfter(call, stopNonGCNode);
+                BlockRange().InsertAfter(stopNonGCNode, new (comp, GT_NO_OP) GenTree(GT_NO_OP, TYP_VOID));
             }
         }
 #endif
@@ -5710,7 +5711,6 @@ GenTree* Lowering::LowerDelegateInvoke(GenTreeCall* call)
     thisArgNode->AsOp()->gtOp1 = newThis;
     BlockRange().Remove(thisArgNode);
     BlockRange().InsertBefore(call, newThisAddr, newThis, thisArgNode);
-
 
     ContainCheckIndir(newThis->AsIndir());
 
