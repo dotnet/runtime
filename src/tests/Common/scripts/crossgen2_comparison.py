b212c5122904b89f783efda97ec75761d99d4573
Jan Kotas <jkotas@microsoft.com>
2024-10-14 00:14:06 -0700

    Fix python

--- src/tests/Common/scripts/crossgen2_comparison.py
+++ src/tests/Common/scripts/crossgen2_comparison.py
@@ -523,7 +523,8 @@ class CrossGenRunner:
 
     def _build_args_crossgen_il_file(self, il_filename, ni_filename, platform_assemblies_paths, target_os, target_arch):
         args = []
-        args.append(self.dotnet)
+        if self.dotnet:
+            args.append(self.dotnet)
         args.append(self.crossgen_executable_filename)
         args.append('-r')
         args.append('"' + platform_assemblies_paths + self.platform_directory_sep + '*.dll"' )
