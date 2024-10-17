aae4c867b25343c3adc07f12645d548aa2873945
Jeremy Koritzinsky <jekoritz@microsoft.com>
2024-10-17 15:47:59 -0700

    Make the co-task-mem-alloc PAL just call malloc

--- src/native/minipal/memory.c
+++ src/native/minipal/memory.c
@@ -16,21 +16,9 @@ void minipal_co_task_mem_free(void* pv)
     CoTaskMemFree(pv);
 }
 #else
-// CoTaskMemAlloc always aligns on an 8-byte boundary.
-#define ALIGN 8
-
 void* minipal_co_task_mem_alloc(size_t cb)
 {
-    // Ensure malloc always allocates.
-    if (cb == 0)
-        cb = ALIGN;
-
-    // Align the allocation size.
-    size_t cb_safe = (cb + (ALIGN - 1)) & ~(ALIGN - 1);
-    if (cb_safe < cb) // Overflow
-        return NULL;
-
-    return aligned_alloc(ALIGN, cb_safe);
+    return malloc(cb);
 }
 
 void minipal_co_task_mem_free(void* pv)
