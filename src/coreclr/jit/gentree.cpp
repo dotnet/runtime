7fa568a6c9f364ea75f41c2fa22b01e920deeaa8
Egor Bogatov <egorbo@gmail.com>
2024-10-17 09:27:02 +0200

    Update gentree.cpp

--- src/coreclr/jit/gentree.cpp
+++ src/coreclr/jit/gentree.cpp
@@ -17223,8 +17223,8 @@ GenTree* Compiler::gtWrapWithSideEffects(GenTree*     tree,
         // It should be possible to be smarter here and allow such cases by extracting the side effects
         // properly for this particular case. For now, caller is responsible for avoiding such cases.
 
-        GenTree* comma = gtNewOperNode(GT_COMMA, tree->TypeGet(), sideEffects, tree);
-        //comma->gtVNPair = tree->gtVNPair;
+        GenTree* comma  = gtNewOperNode(GT_COMMA, tree->TypeGet(), sideEffects, tree);
+        comma->gtVNPair = tree->gtVNPair;
         return comma;
     }
     return tree;
