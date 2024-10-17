5070e999649131aad70c272cdb2b62e4930019e1
pavelsavara <pavel.savara@gmail.com>
2024-10-17 08:30:56 +0200

    rebase

--- src/mono/sample/wasm/browser/Program.cs
+++ src/mono/sample/wasm/browser/Program.cs
@@ -4,18 +4,98 @@
 using System;
 using System.Runtime.InteropServices.JavaScript;
 using System.Runtime.InteropServices;
+using System.ComponentModel;
+using System.Collections.Generic;
 
-namespace Sample
+#pragma warning disable CS8632
+
+namespace Sample;
+
+public partial class TestClass
 {
-    public partial class Test
+    private static readonly ParentClass _parent = new();
+    private static readonly HashSet<ChildClass> _objects = [];
+
+    public static int Main(string[] args)
     {
-        public static int Main(string[] args)
+        //GC.AddMemoryPressure(1024*1024*512);
+        var tm = GetStats();
+        Console.WriteLine($"TotalMemory: {tm}");
+        return 0;
+    }
+
+    public static long GetStats()
+    {
+        var tm = GC.GetTotalMemory(forceFullCollection: false);
+        // Console.WriteLine($"TotalMemory: {tm}");
+
+        //var mi = GC.GetGCMemoryInfo();
+        //Console.WriteLine($"HighMemoryLoadThresholdBytes: {mi.HighMemoryLoadThresholdBytes}");
+        //Console.WriteLine($"TotalAvailableMemoryBytes: {mi.TotalAvailableMemoryBytes}");
+        return tm;
+    }
+
+    [JSExport]
+    [return: JSMarshalAs<JSType.Number>]
+    public static long AllocateObjects()
+    {
+        for (int i = 0; i < 10; i++)
         {
-            DisplayMeaning(42);
-            return 0;
+            var child = new ChildClass(_parent);
+            _objects.Add(child);
         }
 
-        [JSImport("Sample.Test.displayMeaning", "main.js")]
-        internal static partial void DisplayMeaning(int meaning);
+        return GetStats();
+    }
+
+    [JSExport]
+    public static void DisposeObjects()
+    {
+        foreach (var child in _objects)
+        {
+            child.Dispose();
+        }
+        _objects.Clear();
+    }
+
+    [JSExport]
+    [return: JSMarshalAs<JSType.Number>]
+    public static long ForceGC()
+    {
+        GC.Collect();
+        return GetStats();
+    }
+}
+
+public sealed class ChildClass : IDisposable
+{
+    private readonly ParentClass _parent;
+    private byte[] _junk = new byte[250_000];
+
+    public ChildClass(ParentClass parent)
+    {
+        _parent = parent;
+        _parent.PropertyChanged += OnPropertyChanged;
+    }
+
+    public void Dispose()
+    {
+        _parent.PropertyChanged -= OnPropertyChanged;
+        _junk=null;
+        GC.SuppressFinalize(this);
+    }
+
+    private void OnPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+    }
+}
+
+public sealed class ParentClass
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public void NotifyChilderen()
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Foo"));
     }
 }
