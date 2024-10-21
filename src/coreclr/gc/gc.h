ce7214c3b378821dd05ee1baa93570b416d468f7
Mark Plesko <markples@microsoft.com>
2024-10-21 16:44:29 -0700

    comments

--- src/coreclr/gc/gc.h
+++ src/coreclr/gc/gc.h
@@ -344,6 +344,17 @@ inline bool IsServerHeap()
 #define MAX_LONGPATH 1024
 #endif // MAX_LONGPATH
 
+// TRACE_GC has two sub-modes: the standard VM stress log mechanism and
+// SIMPLE_DPRINTF, which is text output.  By default, we enable TRACE_GC (not
+// SIMPLE_DPRINTF) for debug/checked builds so that we can catch build breaks.
+// HOST_64BIT is required because logging dprintf to the stress log us only
+// supported on 64 bit platforms.  We could consider enabling it in release
+// builds and for more logging sites (see below for details) but are being
+// conservative about performance impact.
+//
+// Normal development time changes are to enable SIMPLE_DPRINTF here (which
+// will automatically set TRACE_GC) or to only enable TRACE_GC.
+
 // #define SIMPLE_DPRINTF
 
 #if defined(SIMPLE_DPRINTF) || (defined(_DEBUG) && defined(HOST_64BIT))
@@ -384,29 +395,68 @@ void GCLog (const char *fmt, ... );
 
 #ifdef HOST_64BIT
 
-// This connects dprintf to the stress log mechanism.  There are some mismatches
-// between the two sides:
-// - dprintf(1) loosely corresponds to LL_INFO10 (4).
-// - dprintf has feature areas defined at 7 and higher.  Stress log uses a separate
-//   LogFacility for this.  There are more dprintf areas than available LogFacility
-//   bits.
-// - dprintf has some very low-level instrumentation where we don't want the dynamic
-//   "is logging on" check in normal builds.
-// - StressLogAnalyzer can filter GC events by type, but it requires the LogFacility
-//   to be set to (dprintfLevel<<16)|LF_GC.  This conflicts with other LogFacility
-//   values.
+// -------------------------------
+// Stress log / dprintf background
+// -------------------------------
 //
-// The following line works for normal builds (where STRESS_LOG is defined and
-// SIMPLE_DPRINTF is not).  All dprintfs sites are checked for compilation errors,
-// yet all but those with level 1 can be statically optimized away.  In the future
-// after more auditing, this could be expanded to more levels.
+// This code connects dprintf to the stress log mechanism.  These machanisms
+// and their usage has evolved a bit separately over time, so there are some
+// rough edges here.
 //
-// Note that zero is passed because STRESS_LOG_VA/LogMsg will add LF_GC.
+// The stress log mechanism has a LogFacility and a LogLevel.  Facilities can be
+// chosen through DOTNET_LogFacility, and the facility is recorded in the
+// stress log.  LogFacility is a bitmask.  The GC only has a few bits reserved
+// in the bitmask, and most GC logging uses a single value (LF_GC, which is 0x1).
+//
+// The stress log logging level can be chosen through DOTNET_LogLevel.  This
+// causes filtering at runtime, and the level is not recorded in the stress log.
+// The first argument to dprintf is similar, though it can record either a level
+// (values below 7) or a GC area (values starting with SEG_REUSE_LOG_0 above).
+// Developers often use StressLogAnalyzer to filter by this value at _analysis_
+// time, which doesn't match usual stress log usage.
+//
+// In practice, dprintf(1) and LL_INFO10 (which has the value 4) have been used
+// similarly on log messages.  A dprintf(1) is generally called about once per
+// GC, and LL_INFO10 is "10 logs per small but not trivial run".  Other values
+// have been audited.  We could consider moving the GC values to be in line with
+// the rest of the runtime (change 1 to 4 to make room for errors/warnings, etc.)
+// or (to avoid churn) convert them by adding 3.
+//
+// To allow StressLogAnalyzer to use the GC level values, we abuse the stress
+// log LogLevel by storing the GC value in the upper 16 bits of LogLevel and
+// also settings LF_GC (0x1).  This works because we don't enable other logging
+// when doing GC investigations.  However, we don't want to do this by default
+// because setting the upper bits will cause GC logging to masquerade as non-GC
+// logging.  For example, dprintf(3) would use (3 << 16) | LF_GC == 0x30001,
+// which is LF_ASSERT | LF_VERIFIER | LF_GC in other contexts.
+//
+// Lastly, we have GC logging for some very low level operations, so by default
+// we don't want to even have the check that logging is enabled for performance
+// reasons.  Right now we are very conservative and only allow dprintf(1) to go
+// to the stress log in default builds, but we could consider allowing more in
+// the future.
+
+// -----------------------------
+// Stress log / dprintf settings
+// -----------------------------
+//
+// (See above for details.)
+//
+// The following line works for normal debug/checked builds (where STRESS_LOG is
+// defined and SIMPLE_DPRINTF is not).  All dprintf sites are checked for
+// compilation errors, yet all but those with level 1 can be statically
+// optimized away.  In the future after more auditing, this could be expanded to
+// more levels.
+//
+// Note that zero is passed because STRESS_LOG_VA/LogMsg will add LF_GC and in
+// normal builds we don't want conflicts in the upper bits of LogFacility.
 #define dprintf(l,x) if (l == 1) {STRESS_LOG_VA(0,x);}
 
-// For private builds where LogFacility conflicts are ok (or more likely, non-GC
-// logging is disabled, more events can be allowed and the dprintf level can be
-// passed through.  Note that these in examples, 'l' ("ell", not "one") is passed
+// For private builds where it is ok (and useful) to have conflicts in the upper
+// bits of LogFacility, more events can be allowed and the dprintf level can be
+// passed through.  Usually this is going to be a GC investigation and the other
+// logging will be disabled, so the theoretical conflict won't happen in
+// practice.  Note that in these examples, 'l' ("ell", not "one") is passed
 // rather than '0'.
 //#define dprintf(l,x) STRESS_LOG_VA(l,x);
 //#define dprintf(l,x) {if ((l <= 2) || (l == 6666)) {STRESS_LOG_VA(l,x);}}
