246e42e2b4edd63db575ccd0dd0f348d36702060
Aman Khalid (from Dev Box) <amankhalid@microsoft.com>
2024-10-21 18:35:19 -0400

    Wip: windows/macos sha256 impl

--- src/coreclr/md/enc/sha256.cpp
+++ src/coreclr/md/enc/sha256.cpp
@@ -9,8 +9,93 @@
 //
 //*****************************************************************************
 
+#ifdef _WIN32
+HRESULT Sha256Hash(BYTE* pSrc, DWORD srcSize, BYTE* pDst, DWORD dstSize)
+{
+    NTSTATUS status;
+
+    BCRYPT_ALG_HANDLE   algHandle = NULL;
+    BCRYPT_HASH_HANDLE  hashHandle = NULL;
+
+    BYTE    hash[32]; // 256 bits
+    DWORD   hashLength = 0;
+    DWORD   resultLength = 0;
+    status = BCryptOpenAlgorithmProvider(&algHandle, BCRYPT_SHA256_ALGORITHM, NULL, BCRYPT_HASH_REUSABLE_FLAG);
+    if(!NT_SUCCESS(status))
+    {
+        goto cleanup;
+    }
+    status = BCryptGetProperty(algHandle, BCRYPT_HASH_LENGTH, (PBYTE)&hashLength, sizeof(hashLength), &resultLength, 0);
+    if(!NT_SUCCESS(status))
+    {
+        goto cleanup;
+    }
+    if (hashLength != 32)
+    {
+        status = STATUS_NO_MEMORY;
+        goto cleanup;
+    }
+    status = BCryptCreateHash(algHandle, &hashHandle, NULL, 0, NULL, 0, 0);
+    if(!NT_SUCCESS(status))
+    {
+        goto cleanup;
+    }
+
+    status = BCryptHashData(hashHandle, pSrc, srcSize, 0);
+    if(!NT_SUCCESS(status))
+    {
+        goto cleanup;
+    }
+
+    status = BCryptFinishHash(hashHandle, hash, hashLength, 0);
+    if(!NT_SUCCESS(status))
+    {
+        goto cleanup;
+    }
+
+    memcpy(pDst, hash, min(hashLength, dstSize));
+    status = S_OK;
+
+cleanup:
+    if (NULL != hashHandle)
+    {
+         BCryptDestroyHash(hashHandle);
+    }
+    if(NULL != algHandle)
+    {
+        BCryptCloseAlgorithmProvider(algHandle, 0);
+    }
+    return status;
+}
+#elif defined(__APPLE__)
+#include <CommonCrypto/CommonCrypto.h>
+#include <CommonCrypto/CommonDigest.h>
+
+HRESULT Sha256Hash(BYTE* pSrc, DWORD srcSize, BYTE* pDst, DWORD dstSize)
+{
+    if (dstSize < CC_SHA256_DIGEST_LENGTH)
+    {
+        return E_FAIL;
+    }
+
+    // Apple's documentation states these functions return 1 on success.
+    CC_SHA256_CTX ctx = {{ 0 }};
+    int ret = CC_SHA256_Init(&ctx);
+    assert(ret == 1);
+
+    ret = CC_SHA256_Update(&ctx, pSrc, srcSize);
+    assert(ret == 1);
+
+    ret = CC_SHA256_Final(pDst, &ctx);
+    assert(ret == 1);
+
+    return S_OK;
+}
+#else
+// Unsupported platform
 HRESULT Sha256Hash(BYTE* pSrc, DWORD srcSize, BYTE* pDst, DWORD dstSize)
 {
     memset(pDst, 0, dstSize);
     return S_OK;
 }
+#endif
