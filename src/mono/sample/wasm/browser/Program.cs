add6c8f71b593220a649116df2b454f3bc0bce43
pavelsavara <pavel.savara@gmail.com>
2024-10-21 17:03:31 +0200

    more

--- src/mono/sample/wasm/browser/Program.cs
+++ src/mono/sample/wasm/browser/Program.cs
@@ -4,98 +4,18 @@
 using System;
 using System.Runtime.InteropServices.JavaScript;
 using System.Runtime.InteropServices;
-using System.ComponentModel;
-using System.Collections.Generic;
 
-#pragma warning disable CS8632
-
-namespace Sample;
-
-public partial class TestClass
+namespace Sample
 {
-    private static readonly ParentClass _parent = new();
-    private static readonly HashSet<ChildClass> _objects = [];
-
-    public static int Main(string[] args)
+    public partial class Test
     {
-        //GC.AddMemoryPressure(1024*1024*512);
-        var tm = GetStats();
-        Console.WriteLine($"TotalMemory: {tm}");
-        return 0;
-    }
-
-    public static long GetStats()
-    {
-        var tm = GC.GetTotalMemory(forceFullCollection: false);
-        // Console.WriteLine($"TotalMemory: {tm}");
-
-        //var mi = GC.GetGCMemoryInfo();
-        //Console.WriteLine($"HighMemoryLoadThresholdBytes: {mi.HighMemoryLoadThresholdBytes}");
-        //Console.WriteLine($"TotalAvailableMemoryBytes: {mi.TotalAvailableMemoryBytes}");
-        return tm;
-    }
-
-    [JSExport]
-    [return: JSMarshalAs<JSType.Number>]
-    public static long AllocateObjects()
-    {
-        for (int i = 0; i < 10; i++)
+        public static int Main(string[] args)
         {
-            var child = new ChildClass(_parent);
-            _objects.Add(child);
+            DisplayMeaning(42);
+            return 0;
         }
 
-        return GetStats();
-    }
-
-    [JSExport]
-    public static void DisposeObjects()
-    {
-        foreach (var child in _objects)
-        {
-            child.Dispose();
-        }
-        _objects.Clear();
-    }
-
-    [JSExport]
-    [return: JSMarshalAs<JSType.Number>]
-    public static long ForceGC()
-    {
-        GC.Collect();
-        return GetStats();
-    }
-}
-
-public sealed class ChildClass : IDisposable
-{
-    private readonly ParentClass _parent;
-    private byte[] _junk = new byte[250_000];
-
-    public ChildClass(ParentClass parent)
-    {
-        _parent = parent;
-        _parent.PropertyChanged += OnPropertyChanged;
-    }
-
-    public void Dispose()
-    {
-        _parent.PropertyChanged -= OnPropertyChanged;
-        _junk=null;
-        GC.SuppressFinalize(this);
-    }
-
-    private void OnPropertyChanged(object? sender, PropertyChangedEventArgs e)
-    {
-    }
-}
-
-public sealed class ParentClass
-{
-    public event PropertyChangedEventHandler? PropertyChanged;
-
-    public void NotifyChilderen()
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Foo"));
+        [JSImport("Sample.Test.displayMeaning", "main.js")]
+        internal static partial void DisplayMeaning(int meaning);
     }
 }
--- src/mono/sample/wasm/browser/main.js
+++ src/mono/sample/wasm/browser/main.js
@@ -3,45 +3,25 @@
 
 import { dotnet, exit } from './_framework/dotnet.js'
 
+function displayMeaning(meaning) {
+    document.getElementById("out").innerHTML = `${meaning}`;
+}
+
 try {
-    const { getAssemblyExports, runMain, Module } = await dotnet
+    const { setModuleImports } = await dotnet
         .withElementOnExit()
         .withExitOnUnhandledError()
-        .withEnvironmentVariable("MONO_LOG_LEVEL", "debug")
-        .withEnvironmentVariable("MONO_LOG_MASK", "gc")
         .create();
 
-    await runMain("Wasm.Browser.Sample", []);
+    setModuleImports("main.js", {
+        Sample: {
+            Test: {
+                displayMeaning
+            }
+        }
+    });
 
-    let linear;
-    let tm;
-    const library = await getAssemblyExports("Wasm.Browser.Sample");
-    const testClass = library.Sample.TestClass;
-    console.log("Start allocating objects...");
-    for (var i = 0; i < 389; i++) {
-        tm = testClass?.AllocateObjects();
-        linear = Module.HEAP32.byteLength;
-        console.log(`${i}: managed ${tm} WASM ${linear} bytes. Ratio ${linear / tm}`);
-    }
-
-    console.log("Forcing GC ...");
-    tm = testClass.ForceGC();
-    linear = Module.HEAP32.byteLength;
-    console.log(`${i}: managed ${tm} WASM ${linear} bytes. Ratio ${linear / tm}`);
-
-    console.log("Disposing allocated objects...");
-    testClass.DisposeObjects();
-
-    tm = testClass.AllocateObjects();
-    linear = Module.HEAP32.byteLength;
-    console.log(`${i}: managed ${tm} WASM ${linear} bytes. Ratio ${linear / tm}`);
-
-    testClass.DisposeObjects();
-    console.log("Forcing GC ...");
-    tm = testClass.ForceGC();
-    console.log(`${i}: managed ${tm} WASM ${linear} bytes. Ratio ${linear / tm}`);
-    console.log("");
-    console.log("Tadaaaa!");
+    await dotnet.run();
 }
 catch (err) {
     exit(2, err);
