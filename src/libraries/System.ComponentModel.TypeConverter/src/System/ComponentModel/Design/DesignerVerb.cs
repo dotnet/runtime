08b6f71991b38174ca005d4e1185a20f01a04483
Stephen Toub <stoub@microsoft.com>
2024-10-22 10:27:18 -0400

    Use properties for some [GeneratedRegex]s

--- src/libraries/System.ComponentModel.TypeConverter/src/System/ComponentModel/Design/DesignerVerb.cs
+++ src/libraries/System.ComponentModel.TypeConverter/src/System/ComponentModel/Design/DesignerVerb.cs
@@ -22,11 +22,11 @@ namespace System.ComponentModel.Design
         /// </summary>
         public DesignerVerb(string text, EventHandler handler, CommandID startCommandID) : base(handler, startCommandID)
         {
-            Properties["Text"] = text == null ? null : GetParameterReplacementRegex().Replace(text, "");
+            Properties["Text"] = text == null ? null : GetParameterReplacementRegex.Replace(text, "");
         }
 
         [GeneratedRegex(@"\(\&.\)")]
-        private static partial Regex GetParameterReplacementRegex();
+        private static partial Regex GetParameterReplacementRegex { get; }
 
         /// <summary>
         /// Gets or sets the description of the menu item for the verb.
