6d578e0ecdde0a9f35a90295970b43411320a19a
Weihan Li <7604648+WeihanLi@users.noreply.github.com>
2024-10-26 11:28:27 +0800

    add missing using

--- src/libraries/System.Threading.RateLimiting/src/System/Threading/RateLimiting/RateLimiterHelper.cs
+++ src/libraries/System.Threading.RateLimiting/src/System/Threading/RateLimiting/RateLimiterHelper.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 namespace System.Threading.RateLimiting
 {
