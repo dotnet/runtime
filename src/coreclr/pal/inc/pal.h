cea356a72aec6e1918fdabe2f2b5e56365c193be
Adeel Mujahid <3840695+am11@users.noreply.github.com>
2024-10-29 17:02:29 +0200

    Define __assume on unix (#109247)

--- src/coreclr/pal/inc/pal.h
+++ src/coreclr/pal/inc/pal.h
@@ -140,9 +140,13 @@ extern bool g_arm64_atomics_present;
 
 #define EMPTY_BASES_DECL
 
-
 #if !defined(_MSC_VER) || defined(SOURCE_FORMATTING)
-#define __assume(x) (void)0
+#if __has_builtin(__builtin_assume)
+#define __assume(condition) do { bool assume_cond = (condition); __builtin_assume(assume_cond); } while (0)
+#else
+#define __assume(condition) do { if (!(condition)) __builtin_unreachable(); } while (0)
+#endif // __has_builtin(__builtin_assume)
+
 #define __annotation(x)
 #endif //!MSC_VER
 
