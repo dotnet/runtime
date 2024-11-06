e811f1add0f432e552da39af986143996652bc71
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2024-11-06 12:58:47 +0100

    Switch bbNum -> bbID

--- src/coreclr/jit/block.cpp
+++ src/coreclr/jit/block.cpp
@@ -1026,10 +1026,10 @@ unsigned JitPtrKeyFuncs<BasicBlock>::GetHashCode(const BasicBlock* ptr)
     unsigned hash = SsaStressHashHelper();
     if (hash != 0)
     {
-        return (hash ^ (ptr->bbNum << 16) ^ ptr->bbNum);
+        return (hash ^ (ptr->bbID << 16) ^ ptr->bbID);
     }
 #endif
-    return ptr->bbNum;
+    return ptr->bbID;
 }
 
 //------------------------------------------------------------------------
