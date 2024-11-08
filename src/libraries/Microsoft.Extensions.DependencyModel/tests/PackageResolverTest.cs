0559891aaf0c6823232ff9324b1b61bb58681439
kasperk81 <83082615+kasperk81@users.noreply.github.com>
2024-11-08 21:36:11 +0200

    Update PackageResolverTest.cs

--- src/libraries/Microsoft.Extensions.DependencyModel/tests/PackageResolverTest.cs
+++ src/libraries/Microsoft.Extensions.DependencyModel/tests/PackageResolverTest.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Runtime.InteropServices;
 using FluentAssertions;
 using Microsoft.DotNet.XUnitExtensions;
 using Microsoft.Extensions.DependencyModel.Resolution;
