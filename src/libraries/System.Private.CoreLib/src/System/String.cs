aa1a2eaa72b29e3977881cba4445d91431100eab
Meysam Bagheri <40636199+meysambagheri96@users.noreply.github.com>
2024-11-09 15:58:30 +0330

    Update String.cs and added HasValue extension method
    Add HasValue extension method. it makes code more readable and cleaner

--- src/libraries/System.Private.CoreLib/src/System/String.cs
+++ src/libraries/System.Private.CoreLib/src/System/String.cs
@@ -518,6 +518,18 @@ namespace System
             return true;
         }
 
+        public static bool HasValue(string? value)
+        {
+            if (value == null || value.Length == 0) return false;
+
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (!char.IsWhiteSpace(value[i])) return true;
+            }
+            
+            return false;
+        }
+        
         /// <summary>
         /// Returns a reference to the first element of the String. If the string is null, an access will throw a NullReferenceException.
         /// </summary>
