8b02937ac58252d479da3bfb3804146e9019d323
Aaron R Robinson <arobins@microsoft.com>
2024-11-11 08:02:01 -0800

    Update mono

--- src/mono/mono/metadata/class.c
+++ src/mono/mono/metadata/class.c
@@ -2580,7 +2580,7 @@ mono_class_get_field_from_name_full (MonoClass *klass, const char *name, MonoTyp
 				MonoClassField *gfield = mono_metadata_get_corresponding_field_from_generic_type_definition (field);
 				g_assert (gfield != NULL);
 				MonoType *field_type = gfield->type;
-				if (!mono_metadata_type_equal_full (type, field_type, TRUE))
+				if (!mono_metadata_type_equal_full (type, field_type, MONO_TYPE_EQ_FLAGS_SIG_ONLY))
 					continue;
 			}
 			return field;
