b694d3b170bb2cec84866b2cb9873e5b2535a86a
Michał Petryka <35800402+MichalPetryka@users.noreply.github.com>
2024-11-12 03:41:01 +0100

    Mark initied static readonly loads as non-faulting.

--- src/coreclr/jit/importer.cpp
+++ src/coreclr/jit/importer.cpp
@@ -4175,7 +4175,7 @@ GenTree* Compiler::impImportStaticFieldAddress(CORINFO_RESOLVED_TOKEN* pResolved
             }
             if (isStaticReadOnlyInitedRef)
             {
-                indirFlags |= (GTF_IND_INVARIANT | GTF_IND_NONNULL);
+                indirFlags |= (GTF_IND_NONFAULTING | GTF_IND_INVARIANT | GTF_IND_NONNULL);
             }
             break;
         }
