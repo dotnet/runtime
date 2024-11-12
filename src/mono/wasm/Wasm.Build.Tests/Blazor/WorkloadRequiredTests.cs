0af33559b27bf931ef3446eb2c50f9729522d25c
Ilona Tomkowicz <32700855+ilonatommy@users.noreply.github.com>
2024-11-12 20:50:35 +0000

    `Blazor.WorkloadRequiredTests`: done

--- src/mono/wasm/Wasm.Build.Tests/Blazor/WorkloadRequiredTests.cs
+++ src/mono/wasm/Wasm.Build.Tests/Blazor/WorkloadRequiredTests.cs
@@ -51,151 +51,128 @@ public class WorkloadRequiredTests : BlazorWasmTestBase
         return data;
     }
 
-    // [Theory, TestCategory("no-workload")]
-    // [MemberData(nameof(SettingDifferentFromValuesInRuntimePack))]
-    // public void WorkloadRequiredForBuild(string config, string extraProperties, bool workloadNeeded)
-    //     => CheckWorkloadRequired(config, extraProperties, workloadNeeded, publish: false);
+    [Theory, TestCategory("no-workload")]
+    [MemberData(nameof(SettingDifferentFromValuesInRuntimePack))]
+    public void WorkloadRequiredForBuild(string config, string extraProperties, bool workloadNeeded)
+        => CheckWorkloadRequired(config, extraProperties, workloadNeeded, publish: false);
 
-    // [Theory, TestCategory("no-workload")]
-    // [MemberData(nameof(SettingDifferentFromValuesInRuntimePack))]
-    // public void WorkloadRequiredForPublish(string config, string extraProperties, bool workloadNeeded)
-    //     => CheckWorkloadRequired(config, extraProperties, workloadNeeded, publish: true);
+    [Theory, TestCategory("no-workload")]
+    [MemberData(nameof(SettingDifferentFromValuesInRuntimePack))]
+    public void WorkloadRequiredForPublish(string config, string extraProperties, bool workloadNeeded)
+        => CheckWorkloadRequired(config, extraProperties, workloadNeeded, publish: true);
 
-    // public static TheoryData<string, bool, bool> InvariantGlobalizationTestData(bool publish)
-    // {
-    //     TheoryData<string, bool, bool> data = new();
-    //     foreach (string config in new[] { "Debug", "Release" })
-    //     {
-    //         data.Add(config, /*invariant*/ true, /*publish*/ publish);
-    //         data.Add(config, /*invariant*/ false, /*publish*/ publish);
-    //     }
-    //     return data;
-    // }
+    public static TheoryData<string, bool, bool> InvariantGlobalizationTestData(bool publish)
+    {
+        TheoryData<string, bool, bool> data = new();
+        foreach (string config in new[] { "Debug", "Release" })
+        {
+            data.Add(config, /*invariant*/ true, /*publish*/ publish);
+            data.Add(config, /*invariant*/ false, /*publish*/ publish);
+        }
+        return data;
+    }
 
-    // [Theory, TestCategory("no-workload")]
-    // [MemberData(nameof(InvariantGlobalizationTestData), parameters: /*publish*/ false)]
-    // [MemberData(nameof(InvariantGlobalizationTestData), parameters: /*publish*/ true)]
-    // public async Task WorkloadNotRequiredForInvariantGlobalization(string config, bool invariant, bool publish)
-    // {
-    //     string id = $"props_req_workload_{(publish ? "publish" : "build")}_{GetRandomId()}";
-    //     string projectFile = CreateWasmTemplateProject(id, "blazorwasm");
+    [Theory, TestCategory("no-workload")]
+    [MemberData(nameof(InvariantGlobalizationTestData), parameters: /*publish*/ false)]
+    [MemberData(nameof(InvariantGlobalizationTestData), parameters: /*publish*/ true)]
+    public async Task WorkloadNotRequiredForInvariantGlobalization(string config, bool invariant, bool publish)
+    {
+        string prefix = $"props_req_workload_{(publish ? "publish" : "build")}";
+        string extraProperties = invariant ? $"<InvariantGlobalization>true</InvariantGlobalization>" : "";
+        ProjectInfo info = CopyTestAsset(config, aot: false, "BlazorBasicTestApp", prefix, "App", extraProperties: extraProperties);
+        string ccText = "currentCount++;";
+        // UpdateFile throws if code that is to be replaced does not exist
+        UpdateFile(Path.Combine("Pages", "Counter.razor"), new Dictionary<string, string>
+        {
+            { ccText, $"{ccText}\nTestInvariantCulture();" },
+            { "private int currentCount = 0;", $"{s_invariantCultureMethodForBlazor}" }
+        });
+        string counterPath = Path.Combine(_projectDir!, "Pages", "Counter.razor");
+        string allText = File.ReadAllText(counterPath);
+        _testOutput.WriteLine($"Updated counter.razor: {allText}");
 
-    //     if (invariant)
-    //         AddItemsPropertiesToProject(projectFile, extraProperties: "<InvariantGlobalization>true</InvariantGlobalization>");
+        BuildTemplateProject(info,
+            new BuildProjectOptions(
+                info.Configuration,
+                info.ProjectName,
+                BinFrameworkDir: GetBlazorBinFrameworkDir(info.Configuration, publish),
+                ExpectedFileType: GetExpectedFileType(info, publish),
+                IsPublish: publish,
+                GlobalizationMode: invariant ? GlobalizationMode.Invariant : GlobalizationMode.Sharded
+        ));
 
-    //     string counterPath = Path.Combine(Path.GetDirectoryName(projectFile)!, "Pages", "Counter.razor");
-    //     string allText = File.ReadAllText(counterPath);
-    //     string ccText = "currentCount++;";
-    //     if (allText.IndexOf(ccText) < 0)
-    //         throw new Exception("Counter.razor does not have the expected content. Test needs to be updated.");
+        RunOptions runOptions = new(config);
+        RunResult result = publish ? await RunForPublishWithWebServer(runOptions) : await RunForBuildWithDotnetRun(runOptions);
 
-    //     allText = allText.Replace(ccText, $"{ccText}{Environment.NewLine}TestInvariantCulture();");
-    //     allText += s_invariantCultureMethodForBlazor;
-    //     File.WriteAllText(counterPath, allText);
-    //     _testOutput.WriteLine($"Updated counter.razor: {allText}");
+        if (invariant)
+        {
+            Assert.Contains(result.TestOutput, m => m.Contains("Could not create es-ES culture"));
+            // For invariant, we get:
+            //    Could not create es-ES culture: Argument_CultureNotSupportedInInvariantMode Arg_ParamName_Name, name
+            //    Argument_CultureInvalidIdentifier, es-ES
+            //  .. which is expected.
+            //
+            // Assert.Contains("es-ES is an invalid culture identifier.", output);
+            Assert.Contains(result.TestOutput, m => m.Contains("CurrentCulture.NativeName: Invariant Language (Invariant Country)"));
+            Assert.All(result.TestOutput, m => Assert.DoesNotContain("es-ES: Is-LCID-InvariantCulture", m));
+        }
+        else
+        {
+            Assert.All(result.TestOutput, m => Assert.DoesNotContain("Could not create es-ES culture", m));
+            Assert.All(result.TestOutput, m => Assert.DoesNotContain("invalid culture", m));
+            Assert.All(result.TestOutput, m => Assert.DoesNotContain("CurrentCulture.NativeName: Invariant Language (Invariant Country)", m));
+            Assert.Contains(result.TestOutput, m => m.Contains("es-ES: Is-LCID-InvariantCulture: False"));
+            Assert.Contains(result.TestOutput, m => m.Contains("NativeName: espa\u00F1ol (Espa\u00F1a)"));
+            // ignoring the last line of the output which prints the current culture
+        }
+    }
 
-    //     CommandResult result;
-    //     GlobalizationMode mode = invariant ? GlobalizationMode.Invariant : GlobalizationMode.Sharded;
-    //     if (publish)
-    //     {
-    //         (result, _) = BlazorPublish(
-    //                         new BuildProjectOptions(
-    //                             id,
-    //                             config,
-    //                             ExpectSuccess: true,
-    //                             GlobalizationMode: mode));
-    //     }
-    //     else
-    //     {
-    //         (result, _) = BlazorBuild(
-    //                         new BuildProjectOptions(
-    //                             id,
-    //                            config,
-    //                            ExpectSuccess: true,
-    //                            GlobalizationMode: mode));
-    //     }
+    private void CheckWorkloadRequired(string config, string extraProperties, bool workloadNeeded, bool publish)
+    {
+        string prefix = $"props_req_workload_{(publish ? "publish" : "build")}";
+        string insertAtEnd = @"<Target Name=""StopBuildBeforeCompile"" BeforeTargets=""Compile"">
+                    <Error Text=""Stopping the build"" />
+            </Target>";
+        ProjectInfo info = CopyTestAsset(config, aot: false, "BlazorBasicTestApp", prefix, "App", extraProperties: extraProperties, insertAtEnd: insertAtEnd);
+        (string _, string output) = BuildTemplateProject(info,
+            new BuildProjectOptions(
+                info.Configuration,
+                info.ProjectName,
+                BinFrameworkDir: GetBlazorBinFrameworkDir(info.Configuration, publish),
+                ExpectedFileType: GetExpectedFileType(info, publish),
+                IsPublish: publish,
+                ExpectSuccess: false
+        ));
 
-    //     StringBuilder sbOutput = new();
-    //     await RunBrowserTest(new RunOptions()
-    //     {
-    //         Configuration = config,
-    //         Host = publish ? RunHost.WebServer : RunHost.DotnetRun,
-    //         OnConsoleMessage = (_, msg) =>
-    //         {
-    //             sbOutput.AppendLine(msg.Text);
-    //         }
-    //     });
+        if (workloadNeeded)
+        {
+            Assert.Contains("following workloads must be installed: wasm-tools", output);
+            Assert.DoesNotContain("error : Stopping the build", output);
+        }
+        else
+        {
+            Assert.DoesNotContain("following workloads must be installed: wasm-tools", output);
+            Assert.Contains("error : Stopping the build", output);
+        }
+    }
 
-    //     string output = sbOutput.ToString();
-    //     if (invariant)
-    //     {
-    //         Assert.Contains("Could not create es-ES culture", output);
-    //         // For invariant, we get:
-    //         //    Could not create es-ES culture: Argument_CultureNotSupportedInInvariantMode Arg_ParamName_Name, name
-    //         //    Argument_CultureInvalidIdentifier, es-ES
-    //         //  .. which is expected.
-    //         //
-    //         // Assert.Contains("es-ES is an invalid culture identifier.", output);
-    //         Assert.Contains("CurrentCulture.NativeName: Invariant Language (Invariant Country)", output);
-    //         Assert.DoesNotContain($"es-ES: Is-LCID-InvariantCulture:", output);
-    //     }
-    //     else
-    //     {
-    //         Assert.DoesNotContain("Could not create es-ES culture", output);
-    //         Assert.DoesNotContain("invalid culture", output);
-    //         Assert.DoesNotContain("CurrentCulture.NativeName: Invariant Language (Invariant Country)", output);
-    //         Assert.Contains("es-ES: Is-LCID-InvariantCulture: False", output);
-    //         Assert.Contains("NativeName: espa\u00F1ol (Espa\u00F1a)", output);
+    private static string s_invariantCultureMethodForBlazor = """
+        private int currentCount = 0;
+        public int TestInvariantCulture()
+        {
+            // https://github.com/dotnet/runtime/blob/main/docs/design/features/globalization-invariant-mode.md#cultures-and-culture-data
+            try
+            {
+                System.Globalization.CultureInfo culture = new ("es-ES", false);
+                System.Console.WriteLine($"TestOutput -> es-ES: Is-LCID-InvariantCulture: {culture.LCID == System.Globalization.CultureInfo.InvariantCulture.LCID}, NativeName: {culture.NativeName}");
+            }
+            catch (System.Globalization.CultureNotFoundException cnfe)
+            {
+                System.Console.WriteLine($"TestOutput -> Could not create es-ES culture: {cnfe.Message}");
+            }
 
-    //         // ignoring the last line of the output which prints the current culture
-    //     }
-    // }
-
-    // private void CheckWorkloadRequired(string config, string extraProperties, bool workloadNeeded, bool publish)
-    // {
-    //     string id = $"props_req_workload_{(publish ? "publish" : "build")}_{GetRandomId()}";
-    //     string projectFile = CreateWasmTemplateProject(id, "blazorwasm");
-    //     AddItemsPropertiesToProject(projectFile, extraProperties,
-    //         insertAtEnd: @"<Target Name=""StopBuildBeforeCompile"" BeforeTargets=""Compile"">
-    //                 <Error Text=""Stopping the build"" />
-    //         </Target>");
-
-    //     CommandResult result;
-    //     if (publish)
-    //         (result, _) = BlazorPublish(new BuildProjectOptions(id, config, ExpectSuccess: false));
-    //     else
-    //         (result, _) = BlazorBuild(new BuildProjectOptions(id, config, ExpectSuccess: false));
-
-    //     if (workloadNeeded)
-    //     {
-    //         Assert.Contains("following workloads must be installed: wasm-tools", result.Output);
-    //         Assert.DoesNotContain("error : Stopping the build", result.Output);
-    //     }
-    //     else
-    //     {
-    //         Assert.DoesNotContain("following workloads must be installed: wasm-tools", result.Output);
-    //         Assert.Contains("error : Stopping the build", result.Output);
-    //     }
-    // }
-
-    // private static string s_invariantCultureMethodForBlazor = """
-    // @code {
-    //     public int TestInvariantCulture()
-    //     {
-    //         // https://github.com/dotnet/runtime/blob/main/docs/design/features/globalization-invariant-mode.md#cultures-and-culture-data
-    //         try
-    //         {
-    //             System.Globalization.CultureInfo culture = new ("es-ES", false);
-    //             System.Console.WriteLine($"es-ES: Is-LCID-InvariantCulture: {culture.LCID == System.Globalization.CultureInfo.InvariantCulture.LCID}, NativeName: {culture.NativeName}");
-    //         }
-    //         catch (System.Globalization.CultureNotFoundException cnfe)
-    //         {
-    //             System.Console.WriteLine($"Could not create es-ES culture: {cnfe.Message}");
-    //         }
-
-    //         System.Console.WriteLine($"CurrentCulture.NativeName: {System.Globalization.CultureInfo.CurrentCulture.NativeName}");
-    //         return 42;
-    //     }
-    // }
-    // """;
+            System.Console.WriteLine($"TestOutput -> CurrentCulture.NativeName: {System.Globalization.CultureInfo.CurrentCulture.NativeName}");
+            return 42;
+        }
+    """;
 }
