f40cf8a536df475ef171712243e405db14e1108c
Ilona Tomkowicz <32700855+ilonatommy@users.noreply.github.com>
2024-11-12 16:11:10 +0000

    `NoopNativeRebuildTest`: done

--- src/mono/wasm/Wasm.Build.Tests/Blazor/NoopNativeRebuildTest.cs
+++ src/mono/wasm/Wasm.Build.Tests/Blazor/NoopNativeRebuildTest.cs
@@ -18,59 +18,54 @@ namespace Wasm.Build.Tests.Blazor
         {
         }
 
-        // [Theory]
-        // [InlineData("Debug")]
-        // [InlineData("Release")]
-        // public void BlazorNoopRebuild(string config)
-        // {
-        //     string id = $"blz_rebuild_{config}_{GetRandomId()}";
-        //     string projectFile = CreateBlazorWasmTemplateProject(id);
-        //     AddItemsPropertiesToProject(projectFile, extraProperties: "<WasmBuildNative>true</WasmBuildNative>");
+        [Theory]
+        [InlineData("Debug")]
+        [InlineData("Release")]
+        public void BlazorNoopRebuild(string config)
+        {
+            string extraProperties = "<WasmBuildNative>true</WasmBuildNative>";
+            ProjectInfo info = CopyTestAsset(config, aot: false, "BlazorBasicTestApp", "blz_rebuild", "App", extraProperties: extraProperties);
+            BlazorBuild(info, isNativeBuild: true);
+            string projectDir = Path.GetFileName(Path.GetDirectoryName(Path.GetDirectoryName(info.ProjectFilePath)))!;
+            File.Move(Path.Combine(s_buildEnv.LogRootPath, projectDir, $"{info.ProjectName}-build.binlog"),
+                        Path.Combine(s_buildEnv.LogRootPath, projectDir, $"{info.ProjectName}-build-first.binlog"));
 
-        //     string objDir = Path.Combine(_projectDir!, "obj", config, DefaultTargetFrameworkForBlazor, "wasm");
+            string objDir = Path.Combine(_projectDir!, "obj", config, DefaultTargetFrameworkForBlazor, "wasm");
+            var pathsDict = _provider.GetFilesTable(true, objDir);
+            pathsDict.Remove("runtime-icall-table.h");
+            var originalStat = _provider.StatFiles(pathsDict);
 
-        //     BlazorBuild(new BuildProjectOptions(id, config, NativeFilesType.Relinked));
-        //     File.Move(Path.Combine(s_buildEnv.LogRootPath, id, $"{id}-build.binlog"),
-        //                 Path.Combine(s_buildEnv.LogRootPath, id, $"{id}-build-first.binlog"));
+            // build again
+            BlazorBuild(info, useCache: false, isNativeBuild: true);
+            var newStat = _provider.StatFiles(pathsDict);
 
-        //     var pathsDict = _provider.GetFilesTable(true, objDir);
-        //     pathsDict.Remove("runtime-icall-table.h");
-        //     var originalStat = _provider.StatFiles(pathsDict.Select(kvp => kvp.Value.fullPath));
-
-        //     // build again
-        //     BlazorBuild(new BuildProjectOptions(id, config, NativeFilesType.Relinked));
-        //     var newStat = _provider.StatFiles(pathsDict.Select(kvp => kvp.Value.fullPath));
-
-        //     _provider.CompareStat(originalStat, newStat, pathsDict.Values);
-        // }
+            _provider.CompareStat(originalStat, newStat, pathsDict);
+        }
 
 
-        // [Theory]
-        // [InlineData("Debug")]
-        // [InlineData("Release")]
-        // public void BlazorOnlyLinkRebuild(string config)
-        // {
-        //     string id = $"blz_relink_{config}_{GetRandomId()}";
-        //     string projectFile = CreateBlazorWasmTemplateProject(id);
-        //     AddItemsPropertiesToProject(projectFile, extraProperties: "<WasmBuildNative>true</WasmBuildNative>");
+        [Theory]
+        [InlineData("Debug")]
+        [InlineData("Release")]
+        public void BlazorOnlyLinkRebuild(string config)
+        {
+            string extraProperties = "<WasmBuildNative>true</WasmBuildNative>";
+            ProjectInfo info = CopyTestAsset(config, aot: false, "BlazorBasicTestApp", "blz_relink", "App", extraProperties: extraProperties);
+            BlazorBuild(info, isNativeBuild: true, extraArgs: "-p:EmccLinkOptimizationFlag=-O2");
+            string projectDir = Path.GetFileName(Path.GetDirectoryName(Path.GetDirectoryName(info.ProjectFilePath)))!;
+            File.Move(Path.Combine(s_buildEnv.LogRootPath, projectDir, $"{info.ProjectName}-build.binlog"),
+                        Path.Combine(s_buildEnv.LogRootPath, projectDir, $"{info.ProjectName}-build-first.binlog"));
 
-        //     string objDir = Path.Combine(_projectDir!, "obj", config, DefaultTargetFrameworkForBlazor, "wasm");
+            string objDir = Path.Combine(_projectDir!, "obj", config, DefaultTargetFrameworkForBlazor, "wasm");
+            var pathsDict = _provider.GetFilesTable(true, objDir);
+            pathsDict.Remove("runtime-icall-table.h");
+            pathsDict.UpdateTo(unchanged: false, "dotnet.native.wasm", "dotnet.native.js", "emcc-link.rsp");
+            var originalStat = _provider.StatFiles(pathsDict);
 
-        //     BlazorBuild(new BuildProjectOptions(id, config, NativeFilesType.Relinked), "-p:EmccLinkOptimizationFlag=-O2");
-        //     File.Move(Path.Combine(s_buildEnv.LogRootPath, id, $"{id}-build.binlog"),
-        //                 Path.Combine(s_buildEnv.LogRootPath, id, $"{id}-build-first.binlog"));
+            // build again
+            BlazorBuild(info, useCache: false, isNativeBuild: true, extraArgs: "-p:EmccLinkOptimizationFlag=-O1");
+            var newStat = _provider.StatFiles(pathsDict);
 
-        //     var pathsDict = _provider.GetFilesTable(true, objDir);
-        //     pathsDict.Remove("runtime-icall-table.h");
-        //     pathsDict.UpdateTo(unchanged: false, "dotnet.native.wasm", "dotnet.native.js", "emcc-link.rsp");
-
-        //     var originalStat = _provider.StatFiles(pathsDict.Select(kvp => kvp.Value.fullPath));
-
-        //     // build again
-        //     BlazorBuild(new BuildProjectOptions(id, config, NativeFilesType.Relinked), "-p:EmccLinkOptimizationFlag=-O1");
-        //     var newStat = _provider.StatFiles(pathsDict.Select(kvp => kvp.Value.fullPath));
-
-        //     _provider.CompareStat(originalStat, newStat, pathsDict.Values);
-        // }
+            _provider.CompareStat(originalStat, newStat, pathsDict);
+        }
     }
 }
