316ee5ec1210f1b4f21dce882fcd96c4b1d97854
Alan Hayward <alan.hayward@arm.com>
2024-11-13 10:29:03 +0000

    Hoist Sve check in testing

--- src/tests/JIT/opt/LocalMasks/ChangeMatchUse.cs
+++ src/tests/JIT/opt/LocalMasks/ChangeMatchUse.cs
@@ -23,51 +23,47 @@ public class ChangeMatchUse
     [MethodImpl(MethodImplOptions.NoInlining)]
     private static void ConsumrAddressExposed<T>(ref Vector<T> value) {}
 
-
-    // Create a mask. Use it as a mask.
-    // Conversion of mask1 will be removed.
     [Fact]
     public static void TestEntryPoint()
     {
-        for (int i = 0; i < 4; i++)
+        if (Sve.IsSupported)
         {
-            for (int j = 0; j < 200; j++)
+            for (int i = 0; i < 4; i++)
             {
-                UseMaskAsMask();
-                UseMaskAsVector();
-                UseMaskAsMaskAndVector();
-                UseMaskAsMaskAndVectorInsideLoop();
-                UseMaskAsVectorAndMaskInsideLoop();
-                CastMaskUseAsVector();
-                CastMaskUseAsMask();
-                UseMaskAsMaskAndRef();
+                for (int j = 0; j < 200; j++)
+                {
+                    UseMaskAsMask();
+                    UseMaskAsVector();
+                    UseMaskAsMaskAndVector();
+                    UseMaskAsMaskAndVectorInsideLoop();
+                    UseMaskAsVectorAndMaskInsideLoop();
+                    CastMaskUseAsVector();
+                    CastMaskUseAsMask();
+                    UseMaskAsMaskAndRef();
+                }
+
+                Thread.Sleep(100);
             }
-
-            Thread.Sleep(100);
+            UseMaskAsMask();
+            UseMaskAsVector();
+            UseMaskAsMaskAndVector();
+            UseMaskAsMaskAndVectorInsideLoop();
+            UseMaskAsVectorAndMaskInsideLoop();
+            CastMaskUseAsVector();
+            CastMaskUseAsMask();
+            UseMaskAsMaskAndRef();
         }
-        UseMaskAsMask();
-        UseMaskAsVector();
-        UseMaskAsMaskAndVector();
-        UseMaskAsMaskAndVectorInsideLoop();
-        UseMaskAsVectorAndMaskInsideLoop();
-        CastMaskUseAsVector();
-        CastMaskUseAsMask();
-        UseMaskAsMaskAndRef();
     }
-
 
     // Create a mask. Use it as a mask.
     // Conversion of mask1 will be removed.
     [MethodImpl(MethodImplOptions.NoInlining)]
     private static void UseMaskAsMask()
     {
-        if (Sve.IsSupported)
-        {
-            Vector<ulong> mask1 = Sve.CreateWhileLessThanMask64Bit(2, 9); // Create lcl mask
-            Vector<ulong> vec1  = Vector.Create<ulong>(5);
-            Vector<ulong> vec2  = Sve.Compact(mask1, vec1); // Use as mask
-            Consume(vec2);
-        }
+        Vector<ulong> mask1 = Sve.CreateWhileLessThanMask64Bit(2, 9); // Create lcl mask
+        Vector<ulong> vec1  = Vector.Create<ulong>(5);
+        Vector<ulong> vec2  = Sve.Compact(mask1, vec1); // Use as mask
+        Consume(vec2);
     }
 
     // Create a mask. Use it as a vector.
@@ -75,13 +71,10 @@ public class ChangeMatchUse
     [MethodImpl(MethodImplOptions.NoInlining)]
     private static void UseMaskAsVector()
     {
-        if (Sve.IsSupported)
-        {
-            Vector<short> mask1 = Sve.CreateFalseMaskInt16(); // Create lcl mask
-            Vector<short> vec1  = Vector.Create<short>(9);
-            Vector<short> vec2  = Sve.Add(vec1, mask1); // Use as vector
-            Consume(vec2);
-        }
+        Vector<short> mask1 = Sve.CreateFalseMaskInt16(); // Create lcl mask
+        Vector<short> vec1  = Vector.Create<short>(9);
+        Vector<short> vec2  = Sve.Add(vec1, mask1); // Use as vector
+        Consume(vec2);
     }
 
     // Create a mask. Use it as a mask, then use as a vector.
@@ -89,15 +82,12 @@ public class ChangeMatchUse
     [MethodImpl(MethodImplOptions.NoInlining)]
     private static void UseMaskAsMaskAndVector()
     {
-        if (Sve.IsSupported)
-        {
-            Vector<byte> mask1 = Sve.CreateWhileLessThanOrEqualMask8Bit(2, 9); // Create lcl mask
-            Vector<byte> vec1  = Vector.Create<byte>(3);
-            Vector<byte> vec2  = Vector.Create<byte>(4);
-            Vector<byte> vec3  = Sve.ConditionalExtractAfterLastActiveElement(mask1, vec1, vec2); // Use as mask
-            Vector<byte> vec4  = Sve.PopCount(mask1); // Use as vector
-            Consume(vec3, vec4);
-        }
+        Vector<byte> mask1 = Sve.CreateWhileLessThanOrEqualMask8Bit(2, 9); // Create lcl mask
+        Vector<byte> vec1  = Vector.Create<byte>(3);
+        Vector<byte> vec2  = Vector.Create<byte>(4);
+        Vector<byte> vec3  = Sve.ConditionalExtractAfterLastActiveElement(mask1, vec1, vec2); // Use as mask
+        Vector<byte> vec4  = Sve.PopCount(mask1); // Use as vector
+        Consume(vec3, vec4);
     }
 
     // Create a mask. Use it as a mask, then use as a vector inside a loop.
@@ -105,18 +95,15 @@ public class ChangeMatchUse
     [MethodImpl(MethodImplOptions.NoInlining)]
     private static void UseMaskAsMaskAndVectorInsideLoop()
     {
-        if (Sve.IsSupported)
-        {
-            Vector<short> mask1 = Sve.CreateFalseMaskInt16(); // Create lcl mask
-            Vector<short> vec1  = Vector.Create<short>(3);
-            Vector<short> vec2  = Vector.Create<short>(4);
-            Vector<short> vec3  = Sve.Splice(mask1, vec1, vec2); // Use as mask
+        Vector<short> mask1 = Sve.CreateFalseMaskInt16(); // Create lcl mask
+        Vector<short> vec1  = Vector.Create<short>(3);
+        Vector<short> vec2  = Vector.Create<short>(4);
+        Vector<short> vec3  = Sve.Splice(mask1, vec1, vec2); // Use as mask
 
-            for (int i = 0; i < 100; i++)
-            {
-                Vector<short> vec4 = Sve.ReverseElement8(mask1); // Use as vector
-                Consume(vec3, vec4);
-            }
+        for (int i = 0; i < 100; i++)
+        {
+            Vector<short> vec4 = Sve.ReverseElement8(mask1); // Use as vector
+            Consume(vec3, vec4);
         }
     }
 
@@ -125,17 +112,14 @@ public class ChangeMatchUse
     [MethodImpl(MethodImplOptions.NoInlining)]
     private static void UseMaskAsVectorAndMaskInsideLoop()
     {
-        if (Sve.IsSupported)
-        {
-            Vector<int> vec1 = Vector.Create<int>(7); // Create lcl vector
-            Vector<int> vec2 = Vector.Create<int>(3);
-            Vector<int> vec3  = Sve.Add(vec1, vec2); // Use as vector
+        Vector<int> vec1 = Vector.Create<int>(7); // Create lcl vector
+        Vector<int> vec2 = Vector.Create<int>(3);
+        Vector<int> vec3  = Sve.Add(vec1, vec2); // Use as vector
 
-            for (int i = 0; i < 100; i++)
-            {
-                Vector<int> vec4  = Sve.Compact(vec1, vec3); // Use as mask
-                Consume(vec3, vec4);
-            }
+        for (int i = 0; i < 100; i++)
+        {
+            Vector<int> vec4  = Sve.Compact(vec1, vec3); // Use as mask
+            Consume(vec3, vec4);
         }
     }
 
@@ -144,15 +128,12 @@ public class ChangeMatchUse
     [MethodImpl(MethodImplOptions.NoInlining)]
     private static void CastMaskUseAsVector()
     {
-        if (Sve.IsSupported)
-        {
-            Vector<int> mask1;
-            if (Environment.TickCount % 2 == 0)
-              mask1 = Sve.CreateTrueMaskInt32();
-            else
-              mask1 = Unsafe.BitCast<Vector<uint>, Vector<int>>(Sve.CreateTrueMaskUInt32());
-            Consume(mask1); // Use as vector
-        }
+        Vector<int> mask1;
+        if (Environment.TickCount % 2 == 0)
+          mask1 = Sve.CreateTrueMaskInt32();
+        else
+          mask1 = Unsafe.BitCast<Vector<uint>, Vector<int>>(Sve.CreateTrueMaskUInt32());
+        Consume(mask1); // Use as vector
     }
 
     // Create a mask, potentially bitcasting it. Use it as a mask.
@@ -160,18 +141,15 @@ public class ChangeMatchUse
     [MethodImpl(MethodImplOptions.NoInlining)]
     private static void CastMaskUseAsMask()
     {
-        if (Sve.IsSupported)
-        {
-            Vector<int> mask1;
-            if (Environment.TickCount % 2 == 0)
-              mask1 = Sve.CreateTrueMaskInt32();
-            else
-              mask1 = Unsafe.BitCast<Vector<uint>, Vector<int>>(Sve.CreateTrueMaskUInt32());
+        Vector<int> mask1;
+        if (Environment.TickCount % 2 == 0)
+          mask1 = Sve.CreateTrueMaskInt32();
+        else
+          mask1 = Unsafe.BitCast<Vector<uint>, Vector<int>>(Sve.CreateTrueMaskUInt32());
 
-            Vector<int> vec1  = Vector.Create<int>(25);
-            Vector<int> vec2  = Sve.Compact(mask1, vec1); // Use as mask
-            Consume(vec2);
-        }
+        Vector<int> vec1  = Vector.Create<int>(25);
+        Vector<int> vec2  = Sve.Compact(mask1, vec1); // Use as mask
+        Consume(vec2);
     }
 
     // Create a mask. Use it as a mask and a reference.
@@ -179,14 +157,11 @@ public class ChangeMatchUse
     [MethodImpl(MethodImplOptions.NoInlining)]
     private static void UseMaskAsMaskAndRef()
     {
-        if (Sve.IsSupported)
-        {
-            Vector<double> mask1 = Sve.CreateFalseMaskDouble(); // Create lcl mask
-            Vector<double> vec1  = Vector.Create<double>(1.3);
-            ConsumrAddressExposed(ref mask1); // Use as ref
-            Vector<double> vec2  = Sve.Compact(mask1, vec1); // Use as mask
-            Consume(vec2);
-        }
+        Vector<double> mask1 = Sve.CreateFalseMaskDouble(); // Create lcl mask
+        Vector<double> vec1  = Vector.Create<double>(1.3);
+        ConsumrAddressExposed(ref mask1); // Use as ref
+        Vector<double> vec2  = Sve.Compact(mask1, vec1); // Use as mask
+        Consume(vec2);
     }
 
 }
