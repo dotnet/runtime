097b9a1729744f550c32645c28a0c722c4acd5fe
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2024-11-14 12:06:25 -0800

    fix cref

--- src/libraries/Microsoft.Extensions.DependencyInjection/src/ServiceProviderOptions.cs
+++ src/libraries/Microsoft.Extensions.DependencyInjection/src/ServiceProviderOptions.cs
@@ -19,8 +19,11 @@ namespace Microsoft.Extensions.DependencyInjection
         public bool ValidateScopes { get; set; }
 
         /// <summary>
-        /// Gets or sets a value that indicates whether validation is performed to ensure all services can be created when <see cref="BuildServiceProvider(IServiceCollection,ServiceProviderOptions)" /> is called.
+        /// Gets or sets a value that indicates whether validation is performed to ensure all services can be created when <see cref="BuildServiceProvider(IServiceCollection, ServiceProviderOptions)" /> is called.
         /// </summary>
+        /// <value>
+        /// <see langword="true" /> if validation is performed to ensure all services can be created during a <see cref="BuildServiceProvider(IServiceCollection, ServiceProviderOptions)" /> call; otherwise, <see langword="false" />. The default is the value of <see cref="Microsoft.Extensions.Hosting.HostingEnvironmentExtensions.IsDevelopment(Microsoft.Extensions.Hosting.IHostingEnvironment)" />.
+        /// </value>
         /// <remarks>
         /// Open generics services aren't validated.
         /// </remarks>
