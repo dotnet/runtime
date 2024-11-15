a88718ed46980144860b7c9516ef7828d64ffc3a
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2024-11-15 11:47:10 -0800

    Update Interfaces.cs

--- src/tests/nativeaot/SmokeTests/UnitTests/Interfaces.cs
+++ src/tests/nativeaot/SmokeTests/UnitTests/Interfaces.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Text;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Diagnostics.CodeAnalysis;
