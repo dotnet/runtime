b35c650395bf400250c9b94b3ad78af52a6df18a
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2024-11-19 20:15:46 +0000

    Simplify PooledByteBufferWriter.

--- src/libraries/System.Net.ServerSentEvents/src/System/Net/ServerSentEvents/PooledByteBufferWriter.cs
+++ src/libraries/System.Net.ServerSentEvents/src/System/Net/ServerSentEvents/PooledByteBufferWriter.cs
@@ -8,142 +8,26 @@ namespace System.Net.ServerSentEvents
 {
     internal sealed class PooledByteBufferWriter : IBufferWriter<byte>, IDisposable
     {
-        private byte[] _rentedBuffer;
-        private int _index;
-        private const int MinimumBufferSize = 256;
+        private ArrayBuffer _buffer = new(initialSize: 256, usePool: true);
 
-        // Value copied from Array.MaxLength in System.Private.CoreLib/src/libraries/System.Private.CoreLib/src/System/Array.cs.
-        public const int MaximumBufferSize = 0X7FFFFFC7;
-
-        public PooledByteBufferWriter(int initialCapacity = MinimumBufferSize)
-        {
-            Debug.Assert(initialCapacity > 0);
-
-            _rentedBuffer = ArrayPool<byte>.Shared.Rent(initialCapacity);
-            _index = 0;
-        }
-
-        public ReadOnlyMemory<byte> WrittenMemory
-        {
-            get
-            {
-                Debug.Assert(_rentedBuffer != null);
-                Debug.Assert(_index <= _rentedBuffer.Length);
-                return _rentedBuffer.AsMemory(0, _index);
-            }
-        }
-
-        public int WrittenCount
-        {
-            get
-            {
-                Debug.Assert(_rentedBuffer != null);
-                return _index;
-            }
-        }
-
-        public int Capacity
-        {
-            get
-            {
-                Debug.Assert(_rentedBuffer != null);
-                return _rentedBuffer.Length;
-            }
-        }
-
-        public void Reset()
-        {
-            Debug.Assert(_rentedBuffer != null);
-            Debug.Assert(_index <= _rentedBuffer.Length);
-
-            _rentedBuffer.AsSpan(0, _index).Clear();
-            _index = 0;
-        }
-
-        // Returns the rented buffer back to the pool
-        public void Dispose()
-        {
-            if (_rentedBuffer == null)
-            {
-                return;
-            }
-
-            Reset();
-            byte[] toReturn = _rentedBuffer;
-            _rentedBuffer = null!;
-            ArrayPool<byte>.Shared.Return(toReturn);
-        }
-
-        public void Advance(int count)
-        {
-            Debug.Assert(_rentedBuffer != null);
-            Debug.Assert(count >= 0);
-            Debug.Assert(_index <= _rentedBuffer.Length - count);
-            _index += count;
-        }
+        public void Advance(int count) => _buffer.Commit(count);
 
         public Memory<byte> GetMemory(int sizeHint = 0)
         {
-            CheckAndResizeBuffer(sizeHint);
-            return _rentedBuffer.AsMemory(_index);
+            _buffer.EnsureAvailableSpace(sizeHint);
+            return _buffer.AvailableMemory;
         }
 
         public Span<byte> GetSpan(int sizeHint = 0)
         {
-            CheckAndResizeBuffer(sizeHint);
-            return _rentedBuffer.AsSpan(_index);
+            _buffer.EnsureAvailableSpace(sizeHint);
+            return _buffer.AvailableSpan;
         }
 
-        private void CheckAndResizeBuffer(int sizeHint)
-        {
-            Debug.Assert(_rentedBuffer != null);
-            Debug.Assert(sizeHint >= 0);
-
-            int currentLength = _rentedBuffer.Length;
-            int availableSpace = currentLength - _index;
-
-            if (sizeHint == 0)
-            {
-                sizeHint = MinimumBufferSize;
-            }
-
-            // If we've reached ~1GB written, grow to the maximum buffer
-            // length to avoid incessant minimal growths causing perf issues.
-            if (_index >= MaximumBufferSize / 2)
-            {
-                sizeHint = Math.Max(sizeHint, MaximumBufferSize - currentLength);
-            }
-
-            if (sizeHint > availableSpace)
-            {
-                int growBy = Math.Max(sizeHint, currentLength);
-
-                int newSize = currentLength + growBy;
-
-                if ((uint)newSize > MaximumBufferSize)
-                {
-                    newSize = currentLength + sizeHint;
-                    if ((uint)newSize > MaximumBufferSize)
-                    {
-                        throw new OutOfMemoryException();
-                    }
-                }
-
-                byte[] oldBuffer = _rentedBuffer;
-
-                _rentedBuffer = ArrayPool<byte>.Shared.Rent(newSize);
-
-                Debug.Assert(oldBuffer.Length >= _index);
-                Debug.Assert(_rentedBuffer.Length >= _index);
-
-                Span<byte> oldBufferAsSpan = oldBuffer.AsSpan(0, _index);
-                oldBufferAsSpan.CopyTo(_rentedBuffer);
-                oldBufferAsSpan.Clear();
-                ArrayPool<byte>.Shared.Return(oldBuffer);
-            }
-
-            Debug.Assert(_rentedBuffer.Length - _index > 0);
-            Debug.Assert(_rentedBuffer.Length - _index >= sizeHint);
-        }
+        public ReadOnlyMemory<byte> WrittenMemory => _buffer.ActiveMemory;
+        public int Capacity => _buffer.Capacity;
+        public int WrittenCount => _buffer.ActiveLength;
+        public void Reset() => _buffer.Discard(_buffer.ActiveLength);
+        public void Dispose() => _buffer.Dispose();
     }
 }
