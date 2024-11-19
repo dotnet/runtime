0aeebc3b2028a133f3e8385e0655940bb717463c
Elinor Fung <elfung@microsoft.com>
2024-11-19 15:20:01 -0800

    Make HotColdLookup helper return both hot and cold indexes

--- src/native/managed/cdacreader/Microsoft.Diagnostics.DataContractReader.Contracts/Contracts/ExecutionManager/Helpers/HotColdLookup.cs
+++ src/native/managed/cdacreader/Microsoft.Diagnostics.DataContractReader.Contracts/Contracts/ExecutionManager/Helpers/HotColdLookup.cs
@@ -19,13 +19,30 @@ internal sealed class HotColdLookup
 
     public uint GetHotFunctionIndex(uint numHotColdMap, TargetPointer hotColdMap, uint runtimeFunctionIndex)
     {
-        int lookupIndex = LookupHotColdMappingForMethod(numHotColdMap, hotColdMap, runtimeFunctionIndex);
+        if (!IsColdCode(numHotColdMap, hotColdMap, runtimeFunctionIndex))
+            return runtimeFunctionIndex;
+
+        uint hotIndex;
+        if (!TryLookupHotColdMappingForMethod(numHotColdMap, hotColdMap, runtimeFunctionIndex, out hotIndex, out _))
+            return runtimeFunctionIndex;
 
         // If runtime function is in the cold part, get the associated hot part
-        if (lookupIndex != -1 && (lookupIndex & 1) == 1)
-            runtimeFunctionIndex = _target.Read<uint>(hotColdMap + (ulong)lookupIndex * sizeof(uint));
+        Debug.Assert((hotIndex & 1) == 1);
+        return _target.Read<uint>(hotColdMap + (ulong)hotIndex * sizeof(uint));
+    }
 
-        return runtimeFunctionIndex;
+    public bool TryGetColdFunctionIndex(uint numHotColdMap, TargetPointer hotColdMap, uint runtimeFunctionIndex, out uint lookupIndex, out uint functionIndex)
+    {
+        lookupIndex = ~0u;
+        functionIndex = ~0u;
+
+        uint coldIndex;
+        if (!TryLookupHotColdMappingForMethod(numHotColdMap, hotColdMap, runtimeFunctionIndex, out uint _, out coldIndex))
+            return false;
+
+        lookupIndex = coldIndex;
+        functionIndex = _target.Read<uint>(hotColdMap + (ulong)lookupIndex * sizeof(uint));
+        return true;
     }
 
     private bool IsColdCode(uint numHotColdMap, TargetPointer hotColdMap, uint runtimeFunctionIndex)
@@ -39,34 +56,22 @@ internal sealed class HotColdLookup
         return runtimeFunctionIndex >= firstColdRuntimeFunctionIndex;
     }
 
-    public bool TryGetColdFunctionIndex(uint numHotColdMap, TargetPointer hotColdMap, uint runtimeFunctionIndex, out uint lookupIndex, out uint coldFunctionIndex)
+    // Look up a runtime function index in the hot/cold map. If the function is in the
+    // hot/cold map, return whether the function corresponds to cold code and the hot
+    // and cold lookup indexes for the function.
+    private bool TryLookupHotColdMappingForMethod(
+        uint numHotColdMap,
+        TargetPointer hotColdMap,
+        uint runtimeFunctionIndex,
+        out uint hotIndex,
+        out uint coldIndex)
     {
-        Debug.Assert(!IsColdCode(numHotColdMap, hotColdMap, runtimeFunctionIndex));
+        hotIndex = ~0u;
+        coldIndex = ~0u;
 
-        lookupIndex = ~0u;
-        coldFunctionIndex = ~0u;
-        int lookupIndexLocal = LookupHotColdMappingForMethod(numHotColdMap, hotColdMap, runtimeFunctionIndex);
-
-        // Runtime function has no cold part
-        if (lookupIndexLocal == -1)
-            return false;
-
-        Debug.Assert((lookupIndexLocal & 1) == 0);
-        lookupIndex = (uint)lookupIndexLocal;
-        coldFunctionIndex = _target.Read<uint>(hotColdMap + (ulong)lookupIndexLocal * sizeof(uint));
-        return true;
-    }
-
-    // Look up a runtime function index in the hot/cold map
-    // If the runtime function index is:
-    //  - cold and in the map, returns the index of the hot part in the hot/cold map
-    //  - hot and in the map, returns the index of the cold part in the hot/cold map
-    //  - not in the map, returns -1
-    private int LookupHotColdMappingForMethod(uint numHotColdMap, TargetPointer hotColdMap, uint runtimeFunctionIndex)
-    {
         // HotColdMappingLookupTable::LookupMappingForMethod
         if (numHotColdMap == 0)
-            return -1;
+            return false;
 
         // Each method is represented by a pair of unsigned 32-bit integers. First is the runtime
         // function index of the cold part, second is the runtime function index of the hot part.
@@ -103,26 +108,29 @@ internal sealed class HotColdLookup
             uint value = _target.Read<uint>(hotColdMap + (ulong)(index + indexCorrection) * sizeof(uint));
             if (value == runtimeFunctionIndex)
             {
-                return isColdCode
-                    ? (int)index + 1
-                    : (int)index;
+                hotIndex = index + 1;
+                coldIndex = index;
+                return true;
             }
-            else if (isColdCode && runtimeFunctionIndex > _target.Read<uint>(hotColdMap + (ulong)index * sizeof(uint)))
+
+            // If function index is a cold funclet from a cold block, the above check for equality will fail.
+            // To get its corresponding hot block, find the cold block containing the funclet,
+            // then use the lookup table.
+            // The cold funclet's function index will be greater than its cold block's function index,
+            // but less than the next cold block's function index in the lookup table.
+            if (isColdCode && runtimeFunctionIndex > _target.Read<uint>(hotColdMap + (ulong)index * sizeof(uint)))
             {
-                // If function index is a cold funclet from a cold block, the above check for equality will fail.
-                // To get its corresponding hot block, find the cold block containing the funclet,
-                // then use the lookup table.
-                // The cold funclet's function index will be greater than its cold block's function index,
-                // but less than the next cold block's function index in the lookup table.
                 bool isFuncletIndex = index + 2 == numHotColdMap
                     || runtimeFunctionIndex < _target.Read<uint>(hotColdMap + (ulong)(index + 2) * sizeof(uint));
                 if (isFuncletIndex)
                 {
-                    return (int)index + 1;
+                    hotIndex = index + 1;
+                    coldIndex = index;
+                    return true;
                 }
             }
         }
 
-        return -1;
+        return false;
     }
 }
