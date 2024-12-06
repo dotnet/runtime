52c12b3a45bef4520055da3cccbcb9b683b330de
Ilona Tomkowicz <itomkowicz@microsoft.com>
2024-12-06 16:36:54 +0100

    Move all no-workload blazor tests to one class.

--- src/mono/wasm/Wasm.Build.Tests/Blazor/WorkloadRequiredTests.cs
+++ src/mono/wasm/Wasm.Build.Tests/Blazor/WorkloadRequiredTests.cs
@@ -85,6 +85,39 @@ public class WorkloadRequiredTests : BlazorWasmTestBase
         await RunForPublishWithWebServer(new BlazorRunOptions(config));
     }
 
+    [Theory, TestCategory("no-workload")]
+    [InlineData(Configuration.Debug)]
+    [InlineData(Configuration.Release)]
+    public void NativeRef_EmitsWarningBecauseItRequiresWorkload(Configuration config)
+    {
+        CommandResult res = PublishForRequiresWorkloadTest(config, extraItems: "<NativeFileReference Include=\"native-lib.o\" />");
+        res.EnsureSuccessful();
+        Assert.Matches("warning : .*but the native references won't be linked in", res.Output);
+    }
+
+    [Theory, TestCategory("no-workload")]
+    [InlineData(Configuration.Debug)]
+    [InlineData(Configuration.Release)]
+    public void AOT_FailsBecauseItRequiresWorkload(Configuration config)
+    {
+        CommandResult res = PublishForRequiresWorkloadTest(config, extraProperties: "<RunAOTCompilation>true</RunAOTCompilation>");
+        Assert.NotEqual(0, res.ExitCode);
+        Assert.Contains("following workloads must be installed: wasm-tools", res.Output);
+    }
+
+    [Theory, TestCategory("no-workload")]
+    [InlineData(Configuration.Debug)]
+    [InlineData(Configuration.Release)]
+    public void AOT_And_NativeRef_FailBecauseTheyRequireWorkload(Configuration config)
+    {
+        CommandResult res = PublishForRequiresWorkloadTest(config,
+                                extraProperties: "<RunAOTCompilation>true</RunAOTCompilation>",
+                                extraItems: "<NativeFileReference Include=\"native-lib.o\" />");
+
+        Assert.NotEqual(0, res.ExitCode);
+        Assert.Contains("following workloads must be installed: wasm-tools", res.Output);
+    }
+
 
     [Theory, TestCategory("no-workload")]
     [MemberData(nameof(InvariantGlobalizationTestData), parameters: /*publish*/ false)]
@@ -134,6 +167,20 @@ public class WorkloadRequiredTests : BlazorWasmTestBase
             Assert.Contains(result.TestOutput, m => m.Contains("NativeName: espa\u00F1ol (Espa\u00F1a)"));
             // ignoring the last line of the output which prints the current culture
         }
+    }
+
+    private CommandResult PublishForRequiresWorkloadTest(Configuration config, string extraItems="", string extraProperties="")
+    {
+        ProjectInfo info = CopyTestAsset(
+            config, aot: false, TestAsset.BlazorBasicTestApp, "needs_workload", extraProperties: extraProperties, extraItems: extraItems);
+
+        string publishLogPath = Path.Combine(s_buildEnv.LogRootPath, info.ProjectName, $"{info.ProjectName}.binlog");
+        using DotNetCommand cmd = new DotNetCommand(s_buildEnv, _testOutput);
+        return cmd.WithWorkingDirectory(_projectDir)
+                    .WithEnvironmentVariable("NUGET_PACKAGES", _nugetPackagesDir)
+                    .ExecuteWithCapturedOutput("publish",
+                                                $"-bl:{publishLogPath}",
+                                                $"-p:Configuration={config}");
     }
 
     private void CheckWorkloadRequired(Configuration config, string extraProperties, bool workloadNeeded, bool publish)
