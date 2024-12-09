6efbb2849f22f3d32d1b809b5980e2e5935d9a17
Sergio Pedri <sergio0694@live.com>
2024-12-09 14:00:01 -0800

    Apply suggestions from PR review

--- src/coreclr/nativeaot/System.Private.CoreLib/src/System/Runtime/InteropServices/ComWrappers.NativeAot.cs
+++ src/coreclr/nativeaot/System.Private.CoreLib/src/System/Runtime/InteropServices/ComWrappers.NativeAot.cs
@@ -1420,9 +1420,9 @@ namespace System.Runtime.InteropServices
         {
             try
             {
-                // This could lead to deadlock if finalizer thread is trying to get back to this thread, because we are
-                // not pumping anymore. Disable this for now. See: https://github.com/dotnet/runtime/issues/109538.
-                // GC.WaitForPendingFinalizers();
+                // We'd like to call GC.WaitForPendingFinalizers() here, but this could lead to deadlock
+                // if the finalizer thread is trying to get back to this thread, because we are not pumping
+                // anymore. Disable this for now. See: https://github.com/dotnet/runtime/issues/109538.
                 return HResults.S_OK;
             }
             catch (Exception e)
