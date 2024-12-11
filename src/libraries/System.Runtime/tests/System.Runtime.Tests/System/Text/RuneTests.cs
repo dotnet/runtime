39111b0942ce8cd3a941c347afcf980158369c00
Ilona Tomkowicz <32700855+ilonatommy@users.noreply.github.com>
2024-12-11 16:02:35 +0100

    [browser] Remove WASM `HybridGlobalization` from library tests, WBT and docs (#110534)
    * Remove `HybridGlobalization` from library tests.
    
    * Remove `HybridGlobalization` from WBT.
    
    * Remove docs about HG on WASM.

--- src/libraries/System.Runtime/tests/System.Runtime.Tests/System/Text/RuneTests.cs
+++ src/libraries/System.Runtime/tests/System.Runtime.Tests/System/Text/RuneTests.cs
@@ -57,9 +57,8 @@ namespace System.Text.Tests
             Assert.Equal(new Rune(lower), Rune.ToLowerInvariant(rune));
         }
 
-        [ConditionalTheory(typeof(PlatformDetection), nameof(PlatformDetection.IsIcuGlobalizationAndNotHybridOnBrowser), nameof(PlatformDetection.IsNotHybridGlobalizationOnApplePlatform))]
+        [ConditionalTheory(typeof(PlatformDetection), nameof(PlatformDetection.IsIcuGlobalization), nameof(PlatformDetection.IsNotHybridGlobalizationOnApplePlatform))]
         // HybridGlobalization on Apple mobile platforms has issues with casing dotless I 
-        // HybridGlobalization on Browser uses Invariant HashCode and SortKey, so its effect does not match this of ICU
         [InlineData('0', '0', '0')]
         [InlineData('a', 'A', 'a')]
         [InlineData('i', 'I', 'i')]
