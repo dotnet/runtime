a219e17675e2068092c9d5a696f68138c7abe842
Jeremy Koritzinsky <jekoritz@microsoft.com>
2024-12-17 14:54:09 -0800

    Add os-only RID to portable RID graph in source-build to enable crossgen2 tasks in the SDK to discover the target OS from the source-build target RID

--- src/libraries/Microsoft.NETCore.Platforms/src/UpdateRuntimeIdentifierGraph.cs
+++ src/libraries/Microsoft.NETCore.Platforms/src/UpdateRuntimeIdentifierGraph.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
 using System.IO;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
@@ -40,7 +41,7 @@ namespace Microsoft.NETCore.Platforms
                     continue;
                 }
 
-                string[] importedRids = rid.GetMetadata("Imports").Split(';');
+                string[] importedRids = rid.GetMetadata("Imports").Split([';'], StringSplitOptions.RemoveEmptyEntries);
                 runtimes.Add(rid.ItemSpec, new JObject(new JProperty("#import", new JArray(importedRids))));
             }
 
