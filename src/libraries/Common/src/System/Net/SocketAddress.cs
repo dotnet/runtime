52bf6115e745f0f5fec63a4f34569f9371dd1c31
Egor Bogatov <egorbo@gmail.com>
2024-12-20 23:16:38 +0100

    Update src/libraries/Common/src/System/Net/SocketAddress.cs
    Co-authored-by: Miha Zupan <mihazupan.zupan1@gmail.com>

--- src/libraries/Common/src/System/Net/SocketAddress.cs
+++ src/libraries/Common/src/System/Net/SocketAddress.cs
@@ -164,8 +164,8 @@ namespace System.Net
                     10 + // Size (max length for a positive Int32)
                     2 + // :{
                     (Size - DataOffset) * 4 + // at most ','+3digits per byte
-                    1
-                ); // }
+                    1 // }
+                );
 
             Span<char> result = (uint)maxLength <= 256 ? // arbitrary limit that should be large enough for the vast majority of cases
                 stackalloc char[256] :
