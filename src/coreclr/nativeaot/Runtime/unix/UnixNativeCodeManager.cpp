280e3e270e1ed1602e7423f348c8ee968934c1bd
Adeel Mujahid <3840695+am11@users.noreply.github.com>
2025-01-08 14:09:08 +0200

    Update src/coreclr/nativeaot/Runtime/unix/UnixNativeCodeManager.cpp
    Co-authored-by: Jan Kotas <jkotas@microsoft.com>

--- src/coreclr/nativeaot/Runtime/unix/UnixNativeCodeManager.cpp
+++ src/coreclr/nativeaot/Runtime/unix/UnixNativeCodeManager.cpp
@@ -1240,9 +1240,9 @@ bool UnixNativeCodeManager::GetReturnAddressHijackInfo(MethodInfo *    pMethodIn
     {
         // This is the case when we are either:
         //
-        // 1) In a leaf method that does not push ReturnAddressRegisterLocation on stack, OR
-        // 2) In the prolog/epilog of a non-leaf method that has not yet pushed ReturnAddressRegisterLocation on stack
-        //    or has ReturnAddressRegisterLocation already popped off.
+        // 1) In a leaf method that does not push return address register on stack, OR
+        // 2) In the prolog/epilog of a non-leaf method that has not yet pushed return address register on stack
+        //    or has return address register already popped off.
         return false;
     }
 
