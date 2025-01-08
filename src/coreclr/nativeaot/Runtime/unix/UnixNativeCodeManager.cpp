8bb673a8e44b2b0fa4a42bd1b2e4c8fd36659699
Adeel <3840695+am11@users.noreply.github.com>
2025-01-08 06:42:44 +0200

    Address CR feedback

--- src/coreclr/nativeaot/Runtime/unix/UnixNativeCodeManager.cpp
+++ src/coreclr/nativeaot/Runtime/unix/UnixNativeCodeManager.cpp
@@ -1230,14 +1230,13 @@ bool UnixNativeCodeManager::GetReturnAddressHijackInfo(MethodInfo *    pMethodIn
         return false;
     }
 
-#ifndef TARGET_LOONGARCH64
-    PTR_uintptr_t pLR = pRegisterSet->pLR;
+    PTR_uintptr_t pLR = pRegisterSet->GetReturnAddressRegisterLocation();
     if (!VirtualUnwind(pMethodInfo, pRegisterSet))
     {
         return false;
     }
 
-    if (pRegisterSet->pLR == pLR)
+    if (pRegisterSet->GetReturnAddressRegisterLocation() == pLR)
     {
         // This is the case when we are either:
         //
@@ -1247,26 +1246,7 @@ bool UnixNativeCodeManager::GetReturnAddressHijackInfo(MethodInfo *    pMethodIn
         return false;
     }
 
-    *ppvRetAddrLocation = (PTR_PTR_VOID)pRegisterSet->pLR;
-#else
-    PTR_uintptr_t pRA = pRegisterSet->pRA;
-    if (!VirtualUnwind(pMethodInfo, pRegisterSet))
-    {
-        return false;
-    }
-
-    if (pRegisterSet->pRA == pRA)
-    {
-        // This is the case when we are either:
-        //
-        // 1) In a leaf method that does not push RA on stack, OR
-        // 2) In the prolog/epilog of a non-leaf method that has not yet pushed RA on stack
-        //    or has RA already popped off.
-        return false;
-    }
-
-    *ppvRetAddrLocation = (PTR_PTR_VOID)pRegisterSet->pRA;
-#endif     // TARGET_LOONGARCH64
+    *ppvRetAddrLocation = (PTR_PTR_VOID)pRegisterSet->GetReturnAddressRegisterLocation();
 
     return true;
 #else
