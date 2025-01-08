6261875d042066f925c764d69a51545a70db2685
Ilona Tomkowicz <32700855+ilonatommy@users.noreply.github.com>
2025-01-08 16:51:05 +0000

    Feedback: Make the asset more runnable.

---
+++ src/mono/wasm/testassets/LibraryMode/wwwroot/main.js
@@ -0,0 +1,19 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+import { dotnet } from './_framework/dotnet.js'
+
+const { setModuleImports, getAssemblyExports, getConfig } = await dotnet
+    .withApplicationArguments("start")
+    .create();
+
+setModuleImports('main.js', {
+    dom: {
+        setInnerText: (selector, time) => document.querySelector(selector).innerText = time
+    }
+});
+
+const config = getConfig();
+const exports = await getAssemblyExports(config.mainAssemblyName);
+var result = exports.LibraryMode.Test.MyExport();
+console.log(`MyExport: ${result}`);
