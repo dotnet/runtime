a930c23d8ff6292733e4dce815ea19a4d438cf9c
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2025-01-09 21:37:48 +0800

    GetCONTEXTFromRedirectedStubStackFrame

--- src/coreclr/vm/i386/unixstubs.cpp
+++ src/coreclr/vm/i386/unixstubs.cpp
@@ -2,9 +2,3 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 #include "common.h"
-
-PTR_CONTEXT GetCONTEXTFromRedirectedStubStackFrame(T_DISPATCHER_CONTEXT * pDispatcherContext)
-{
-    PORTABILITY_ASSERT("GetCONTEXTFromRedirectedStubStackFrame");
-    return NULL;
-}
