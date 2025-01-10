54e492deb12a4bd675c0f044a4cd521eeceb32b6
Marek Fišera <mara@neptuo.com>
2025-01-10 10:45:46 +0100

    Add using

--- src/mono/wasm/host/DevServer/DevServerStartup.cs
+++ src/mono/wasm/host/DevServer/DevServerStartup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.Net.Http.Headers;
 using Microsoft.WebAssembly.AppHost;
 
 namespace Microsoft.WebAssembly.AppHost.DevServer;
