1b0bf882698f82dc4420981109ba7eaa2d621ab1
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2025-01-14 01:10:58 +0800

    Cleanup leaked handle in R2R

--- src/coreclr/tools/aot/ILCompiler.ReadyToRun/TypeSystem/Mutable/MutableModule.cs
+++ src/coreclr/tools/aot/ILCompiler.ReadyToRun/TypeSystem/Mutable/MutableModule.cs
@@ -211,7 +211,6 @@ namespace Internal.TypeSystem.Ecma
 
                         byte[] metadataArrayTemp = _currentBinaryEmitter.EmitToMetadataBlob();
                         byte[] metadataArray = GC.AllocateArray<byte>(metadataArrayTemp.Length, pinned: true);
-                        System.Runtime.InteropServices.GCHandle.Alloc(metadataArray, System.Runtime.InteropServices.GCHandleType.Pinned);
                         Array.Copy(metadataArrayTemp, metadataArray, metadataArray.Length);
                         _readers.Add(metadataArray);
                         unsafe
