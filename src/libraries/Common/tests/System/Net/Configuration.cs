43d3e3d09bd032d95b4b6246236f69e2882287ff
Radek Zikmund <r.zikmund.rz@gmail.com>
2025-01-14 10:26:32 +0100

    Use staging server to test

--- src/libraries/Common/tests/System/Net/Configuration.cs
+++ src/libraries/Common/tests/System/Net/Configuration.cs
@@ -5,11 +5,11 @@ namespace System.Net.Test.Common
 {
     public static partial class Configuration
     {
-        #pragma warning disable 414
-        private static readonly string DefaultAzureServer = "corefx-net-http11.azurewebsites.net";
-        #pragma warning restore 414
+#pragma warning disable 414
+        private static readonly string DefaultAzureServer = "corefx-net-http11-staging.azurewebsites.net";
+#pragma warning restore 414
 
-        private static string GetValue(string envName, string defaultValue=null)
+        private static string GetValue(string envName, string defaultValue = null)
         {
             string envValue = Environment.GetEnvironmentVariable(envName);
 
@@ -32,15 +32,15 @@ namespace System.Net.Test.Common
             envValue = Environment.ExpandEnvironmentVariables(envValue);
 
             var split = envValue.Split(':');
-            if (split.Length<2)
+            if (split.Length < 2)
             {
                 return defaultValue;
             }
-            
+
             return int.Parse(split[1]);
         }
 
-        private static Uri GetUriValue(string envName, Uri defaultValue=null)
+        private static Uri GetUriValue(string envName, Uri defaultValue = null)
         {
             string envValue = GetValue(envName, null);
 
