05e991d1d3ceed62e1029d4091d5450a327fc1f0
Hamish Arblaster <hamarb123@gmail.com>
2025-01-22 10:01:22 +1100

    Update SearchValues.cs

--- src/libraries/System.Private.CoreLib/src/System/SearchValues/SearchValues.cs
+++ src/libraries/System.Private.CoreLib/src/System/SearchValues/SearchValues.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics;
 using System.Runtime.Intrinsics.Arm;
 using System.Runtime.Intrinsics.Wasm;
 using System.Runtime.Intrinsics.X86;
