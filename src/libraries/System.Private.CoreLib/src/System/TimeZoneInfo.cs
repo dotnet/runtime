1825361af438767a9fb54a542da47f162dc187de
Pavel Savara <pavelsavara@microsoft.com>
2025-01-22 12:37:02 +0100

    Update src/libraries/System.Private.CoreLib/src/System/TimeZoneInfo.cs
    Co-authored-by: Sven Boemer <sbomer@gmail.com>

--- src/libraries/System.Private.CoreLib/src/System/TimeZoneInfo.cs
+++ src/libraries/System.Private.CoreLib/src/System/TimeZoneInfo.cs
@@ -60,6 +60,7 @@ namespace System
         private static readonly TimeZoneInfo s_utcTimeZone = CreateUtcTimeZone();
         private static CachedData s_cachedData = new CachedData();
 
+        [FeatureSwitchDefinition("System.TimeZoneInfo.Invariant")]
         internal static bool Invariant { get; } = AppContextConfigHelper.GetBooleanConfig("System.TimeZoneInfo.Invariant", "DOTNET_SYSTEM_TIMEZONE_INVARIANT");
 
         //
