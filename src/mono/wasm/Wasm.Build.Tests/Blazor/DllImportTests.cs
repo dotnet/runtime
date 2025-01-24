d76497781e7bb2974f35e8d198ad4a6b9a6c21c3
Ilona Tomkowicz <itomkowicz@microsoft.com>
2025-01-24 13:29:59 +0100

    Revert investigative changes. Shorten the path by 4 chars.

--- src/mono/wasm/Wasm.Build.Tests/Blazor/DllImportTests.cs
+++ src/mono/wasm/Wasm.Build.Tests/Blazor/DllImportTests.cs
@@ -28,9 +28,9 @@ public class DllImportTests : BlazorWasmTestBase
     public static TheoryData<Configuration, bool, bool> DllImportTheoryData()
     {
         var data = new TheoryData<Configuration, bool, bool>();
-        // data.Add(Configuration.Debug, /*build*/true, /*publish*/false);
-        // data.Add(Configuration.Release, /*build*/true, /*publish*/false);
-        // data.Add(Configuration.Release, /*build*/false, /*publish*/true);
+        data.Add(Configuration.Debug, /*build*/true, /*publish*/false);
+        data.Add(Configuration.Release, /*build*/true, /*publish*/false);
+        data.Add(Configuration.Release, /*build*/false, /*publish*/true);
         data.Add(Configuration.Release, /*build*/true, /*publish*/true);
         return data;
     }
@@ -39,50 +39,43 @@ public class DllImportTests : BlazorWasmTestBase
     [MemberData(nameof(DllImportTheoryData))]
     public void WithDllImportInMainAssembly(Configuration config, bool build, bool publish)
     {
-        string output = $"isWindows?: {s_isWindows}, config={config}, build={build}, publish={publish}";
-        if (s_isWindows)
+        // Based on https://github.com/dotnet/runtime/issues/59255
+        string prefix = $"dllimp_{config}_{s_unicodeChars}";
+        if (build && publish)
+            prefix += "build_then_publish";
+        else if (build)
+            prefix += "build";
+        else
+            prefix += "publish";
+        string extraItems = @"<NativeFileReference Include=""mylib.cpp"" />";
+        ProjectInfo info = CopyTestAsset(config, aot: false, TestAsset.BlazorBasicTestApp, prefix, extraItems: extraItems);
+        File.Copy(Path.Combine(BuildEnvironment.TestAssetsPath, "EntryPoints", "MyDllImport.cs"), Path.Combine(_projectDir, "Pages", "MyDllImport.cs"));
+        File.Copy(Path.Combine(BuildEnvironment.TestAssetsPath, "native-libs", "mylib.cpp"), Path.Combine(_projectDir, "mylib.cpp"));
+        UpdateFile(Path.Combine("Pages", "MyDllImport.cs"), new Dictionary<string, string> { { "##NAMESPACE##", info.ProjectName } });
+
+        BlazorAddRazorButton("cpp_add", """
+            var result = MyDllImports.cpp_add(10, 12);
+            outputText = $"{result}";
+        """);
+
+        if (build)
+            BlazorBuild(info, config, isNativeBuild: true);
+
+        if (publish)
+            BlazorPublish(info, config, new PublishOptions(UseCache: false), isNativeBuild: true);
+
+        BlazorRunOptions runOptions = new(config, Test: TestDllImport);
+        if (publish)
+            await RunForPublishWithWebServer(runOptions);
+        else
+            await RunForBuildWithDotnetRun(runOptions);
+
+        async Task TestDllImport(IPage page)
         {
-            bool isLongPathEnabled = IsLongPathEnabled();
-            output += $"long path enabled -> {isLongPathEnabled}";
+            await page.Locator("text=\"cpp_add\"").ClickAsync();
+            var txt = await page.Locator("p[role='test']").InnerHTMLAsync();
+            Assert.Equal("Output: 22", txt);
         }
-        throw new Exception(output);
-        // // Based on https://github.com/dotnet/runtime/issues/59255
-        // string prefix = $"blz_dllimp_{config}_{s_unicodeChars}";
-        // if (build && publish)
-        //     prefix += "build_then_publish";
-        // else if (build)
-        //     prefix += "build";
-        // else
-        //     prefix += "publish";
-        // string extraItems = @"<NativeFileReference Include=""mylib.cpp"" />";
-        // ProjectInfo info = CopyTestAsset(config, aot: false, TestAsset.BlazorBasicTestApp, prefix, extraItems: extraItems);
-        // File.Copy(Path.Combine(BuildEnvironment.TestAssetsPath, "EntryPoints", "MyDllImport.cs"), Path.Combine(_projectDir, "Pages", "MyDllImport.cs"));
-        // File.Copy(Path.Combine(BuildEnvironment.TestAssetsPath, "native-libs", "mylib.cpp"), Path.Combine(_projectDir, "mylib.cpp"));
-        // UpdateFile(Path.Combine("Pages", "MyDllImport.cs"), new Dictionary<string, string> { { "##NAMESPACE##", info.ProjectName } });
-
-        // BlazorAddRazorButton("cpp_add", """
-        //     var result = MyDllImports.cpp_add(10, 12);
-        //     outputText = $"{result}";
-        // """);
-
-        // if (build)
-        //     BlazorBuild(info, config, isNativeBuild: true);
-
-        // if (publish)
-        //     BlazorPublish(info, config, new PublishOptions(UseCache: false), isNativeBuild: true);
-
-        // BlazorRunOptions runOptions = new(config, Test: TestDllImport);
-        // if (publish)
-        //     await RunForPublishWithWebServer(runOptions);
-        // else
-        //     await RunForBuildWithDotnetRun(runOptions);
-
-        // async Task TestDllImport(IPage page)
-        // {
-        //     await page.Locator("text=\"cpp_add\"").ClickAsync();
-        //     var txt = await page.Locator("p[role='test']").InnerHTMLAsync();
-        //     Assert.Equal("Output: 22", txt);
-        // }
     }
 
     public static bool IsLongPathEnabled()
