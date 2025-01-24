2ccc9368c81102b8528d5f971a578b037ae0decc
Ilona Tomkowicz <itomkowicz@microsoft.com>
2025-01-24 13:31:46 +0100

    Cleanup.

--- src/mono/wasm/Wasm.Build.Tests/Blazor/DllImportTests.cs
+++ src/mono/wasm/Wasm.Build.Tests/Blazor/DllImportTests.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -11,7 +10,6 @@ using Xunit;
 using Xunit.Abstractions;
 using Xunit.Sdk;
 using Microsoft.Playwright;
-using Microsoft.Win32;
 
 #nullable enable
 
