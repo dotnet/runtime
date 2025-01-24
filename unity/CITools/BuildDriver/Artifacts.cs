2ea343bb16278ab9ff1780a9a07d706ceca0411d
Alex Thibodeau <alexthibodeau@unity3d.com>
2025-01-24 10:49:09 -0500

    Rearrange artifacts in a way that makes a bit more sense for us on the Unity side.

--- unity/CITools/BuildDriver/Artifacts.cs
+++ unity/CITools/BuildDriver/Artifacts.cs
@@ -7,9 +7,22 @@ namespace BuildDriver;
 
 static class Artifacts
 {
+    // Clean up the artifacts dir and rearrange things into a structure that makes sense for us
     public static NPath ConsolidateArtifacts(GlobalConfig gConfig)
     {
         Paths.RepoRoot.Combine("LICENSE.TXT").Copy(Utils.RuntimeArtifactDirectory(gConfig).Combine("LICENSE.md"));
+
+        // This directory contains windows specific native libraries for reading/writing pdbs that we do not need.
+        Utils.RuntimeArtifactDirectory(gConfig).Combine("native", "runtimes").DeleteIfExists();
+
+        //Get rid of the tfm directory to save on code churn on our side when we upgrade. We won't be supporting more than one.
+        NPath lib = Utils.RuntimeArtifactDirectory(gConfig).Combine("lib");
+        NPath tfm = lib.Directories().First();
+        tfm.MoveFiles(lib, true);
+        tfm.Delete();
+
+        // Move System.Private.CoreLib from native to lib. This is where it lives when .Net is installed on a system
+        Utils.RuntimeArtifactDirectory(gConfig).Combine("native").MoveFiles(lib, false, path => path.FileName.StartsWith("System.Private.CoreLib"));
 
         return Utils.RuntimeArtifactDirectory(gConfig);
     }
