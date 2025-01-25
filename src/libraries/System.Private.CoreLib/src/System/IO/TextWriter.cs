d6f6a496d6c01e10ff8b10dfc7f04f2e2528e0db
Joyless <65855333+Joy-less@users.noreply.github.com>
2025-01-25 01:14:01 +0000

    Even more compiler error fixes

--- src/libraries/System.Private.CoreLib/src/System/IO/TextWriter.cs
+++ src/libraries/System.Private.CoreLib/src/System/IO/TextWriter.cs
@@ -557,8 +557,8 @@ namespace System.IO
             {
                 async Task WriteAsyncPair(char highSurrogate, char lowSurrogate)
                 {
-                    await WriteAsync(highSurrogate);
-                    await WriteAsync(lowSurrogate);
+                    await WriteAsync(highSurrogate).ConfigureAwait(false);
+                    await WriteAsync(lowSurrogate).ConfigureAwait(false);
                 }
                 return WriteAsyncPair(charsSlice[0], charsSlice[1]);
             }
