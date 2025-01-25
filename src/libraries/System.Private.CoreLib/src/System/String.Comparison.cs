bedf656eb21b15b0faa15e2cdd2c2f1ee171ab15
Joyless <65855333+Joy-less@users.noreply.github.com>
2025-01-25 00:57:31 +0000

    More compiler error fixes

--- src/libraries/System.Private.CoreLib/src/System/String.Comparison.cs
+++ src/libraries/System.Private.CoreLib/src/System/String.Comparison.cs
@@ -617,7 +617,7 @@ namespace System
 
         public bool EndsWith(Rune value, StringComparison comparisonType)
         {
-            if (Rune.DecodeLastFromUtf16(@this, out Rune result, out _) is OperationStatus.Done)
+            if (Rune.DecodeLastFromUtf16(this, out Rune result, out _) is OperationStatus.Done)
             {
                 return result.Equals(value, comparisonType);
             }
@@ -1213,7 +1213,7 @@ namespace System
 
         public bool StartsWith(Rune value, StringComparison comparisonType)
         {
-            if (Rune.DecodeFromUtf16(@this, out Rune result, out _) is OperationStatus.Done)
+            if (Rune.DecodeFromUtf16(this, out Rune result, out _) is OperationStatus.Done)
             {
                 return result.Equals(value, comparisonType);
             }
