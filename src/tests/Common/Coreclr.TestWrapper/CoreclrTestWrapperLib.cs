d6c58218bc58af86d0544e66951809562ac04e65
pavelsavara <pavel.savara@gmail.com>
2025-01-28 21:06:43 +0100

    chmod a+rw

--- src/tests/Common/Coreclr.TestWrapper/CoreclrTestWrapperLib.cs
+++ src/tests/Common/Coreclr.TestWrapper/CoreclrTestWrapperLib.cs
@@ -391,25 +391,21 @@ namespace CoreclrTestLib
                 }
 
                 Console.WriteLine("=========================================");
-                string? userName = Environment.GetEnvironmentVariable("USER");
-                if (!string.IsNullOrEmpty(userName))
+                if (!RunProcess("sudo", $"chmod a+rw {crashReportJsonFile}", Console.Out))
                 {
-                    if (!RunProcess("sudo", $"chown {userName} {crashReportJsonFile}", Console.Out))
-                    {
-                        return false;
-                    }
+                    return false;
+                }
 
-                    Console.WriteLine("=========================================");
-                    if (!RunProcess("sudo", $"ls -l {crashReportJsonFile}", Console.Out))
-                    {
-                        return false;
-                    }
+                Console.WriteLine("=========================================");
+                if (!RunProcess("sudo", $"ls -l {crashReportJsonFile}", Console.Out))
+                {
+                    return false;
+                }
 
-                    Console.WriteLine("=========================================");
-                    if (!RunProcess("ls", $"-l {crashReportJsonFile}", Console.Out))
-                    {
-                        return false;
-                    }
+                Console.WriteLine("=========================================");
+                if (!RunProcess("ls", $"-l {crashReportJsonFile}", Console.Out))
+                {
+                    return false;
                 }
             }
 
