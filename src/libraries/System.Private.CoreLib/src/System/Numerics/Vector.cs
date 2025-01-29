257a5e89e0f55d8bf70c10a3e90eabf98c0945b4
Hamish Arblaster <hamarb123@gmail.com>
2025-01-29 16:26:41 +1100

    Update Vector.cs

--- src/libraries/System.Private.CoreLib/src/System/Numerics/Vector.cs
+++ src/libraries/System.Private.CoreLib/src/System/Numerics/Vector.cs
@@ -311,7 +311,7 @@ namespace System.Numerics
         /// <param name="left">The vector that is selected when the corresponding bit in <paramref name="condition" /> is one.</param>
         /// <param name="right">The vector that is selected when the corresponding bit in <paramref name="condition" /> is zero.</param>
         /// <returns>A vector whose bits come from <paramref name="left" /> or <paramref name="right" /> based on the value of <paramref name="condition" />.</returns>
-        /// <remarks>This methods acts like a ternary on a bitwise basis; specifically the result bit is <paramref name="condition" /> <c>?</c> <paramref name="left" /> <c>:</c> <paramref name="right" />.</remarks>
+        /// <remarks>This is equivalent to <paramref name="condition" /> <c>?</c> <paramref name="left" /> <c>:</c> <paramref name="right" /> on a per-bit basis.</remarks>
         [Intrinsic]
         public static Vector<float> ConditionalSelect(Vector<int> condition, Vector<float> left, Vector<float> right) => ConditionalSelect(condition.As<int, float>(), left, right);
 
@@ -320,7 +320,7 @@ namespace System.Numerics
         /// <param name="left">The vector that is selected when the corresponding bit in <paramref name="condition" /> is one.</param>
         /// <param name="right">The vector that is selected when the corresponding bit in <paramref name="condition" /> is zero.</param>
         /// <returns>A vector whose bits come from <paramref name="left" /> or <paramref name="right" /> based on the value of <paramref name="condition" />.</returns>
-        /// <remarks>This methods acts like a ternary on a bitwise basis; specifically the result bit is <paramref name="condition" /> <c>?</c> <paramref name="left" /> <c>:</c> <paramref name="right" />.</remarks>
+        /// <remarks>This is equivalent to <paramref name="condition" /> <c>?</c> <paramref name="left" /> <c>:</c> <paramref name="right" /> on a per-bit basis.</remarks>
         [Intrinsic]
         public static Vector<double> ConditionalSelect(Vector<long> condition, Vector<double> left, Vector<double> right) => ConditionalSelect(condition.As<long, double>(), left, right);
 
