d58e1beb19a6d0ca4682e211d00e58280238fa2b
Hamish Arblaster <hamarb123@gmail.com>
2025-01-29 20:34:00 +1100

    Normalize indices for `vpshufb` in all cases of constant indices Shuffle
    - This is a nice-to-have, not a correctness issue - it matches what we're doing throughout the rest of the function, and keeps the asm more readable imo

--- src/coreclr/jit/gentree.cpp
+++ src/coreclr/jit/gentree.cpp
@@ -26411,6 +26411,12 @@ GenTree* Compiler::gtNewSimdShuffleNode(
                 }
             }
 
+            // Normalize vecCns
+            for (size_t i = 0; i < 64; i++)
+            {
+                if (vecCns.u8[i] < 64) vecCns.u8[i] &= 15;
+            }
+
             op2                        = gtNewVconNode(type);
             op2->AsVecCon()->gtSimdVal = vecCns;
 
