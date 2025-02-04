85550e7bc6f60adc7cb675de6f78c914a16bfe1a
pavelsavara <pavel.savara@gmail.com>
2025-02-04 14:06:54 +0100

    more

--- src/tasks/WasmAppBuilder/ForceMSBuildGC.cs
+++ src/tasks/WasmAppBuilder/ForceMSBuildGC.cs
@@ -16,32 +16,32 @@ namespace Microsoft.WebAssembly.Build.Tasks
             Process process = Process.GetCurrentProcess();
 
             // print current free memory
-            Log.LogMessage($"Before GC {process.Id} - PeakWorkingSet64: {process.PeakWorkingSet64} bytes");
-            Log.LogMessage($"Before GC {process.Id} - PrivateMemorySize64: {process.PrivateMemorySize64} bytes");
-            Log.LogMessage($"Before GC {process.Id} - WorkingSet64: {process.WorkingSet64} bytes");
+            Log.LogMessage(MessageImportance.High, $"Before GC {process.Id} - PeakWorkingSet64: {process.PeakWorkingSet64} bytes");
+            Log.LogMessage(MessageImportance.High, $"Before GC {process.Id} - PrivateMemorySize64: {process.PrivateMemorySize64} bytes");
+            Log.LogMessage(MessageImportance.High, $"Before GC {process.Id} - WorkingSet64: {process.WorkingSet64} bytes");
 #if NET
             var info = GC.GetGCMemoryInfo(GCKind.Any);
-            Log.LogMessage($"Before GC {process.Id} - FragmentedBytes: {info.FragmentedBytes} bytes");
-            Log.LogMessage($"Before GC {process.Id} - Compacted: {info.Compacted} bytes");
-            Log.LogMessage($"Before GC {process.Id} - HighMemoryLoadThresholdBytes: {info.HighMemoryLoadThresholdBytes} bytes");
-            Log.LogMessage($"Before GC {process.Id} - TotalAvailableMemoryBytes: {info.TotalAvailableMemoryBytes} bytes");
-            Log.LogMessage($"Before GC {process.Id} - TotalCommittedBytes: {info.TotalCommittedBytes} bytes");
+            Log.LogMessage(MessageImportance.High, $"Before GC {process.Id} - FragmentedBytes: {info.FragmentedBytes} bytes");
+            Log.LogMessage(MessageImportance.High, $"Before GC {process.Id} - Compacted: {info.Compacted} bytes");
+            Log.LogMessage(MessageImportance.High, $"Before GC {process.Id} - HighMemoryLoadThresholdBytes: {info.HighMemoryLoadThresholdBytes} bytes");
+            Log.LogMessage(MessageImportance.High, $"Before GC {process.Id} - TotalAvailableMemoryBytes: {info.TotalAvailableMemoryBytes} bytes");
+            Log.LogMessage(MessageImportance.High, $"Before GC {process.Id} - TotalCommittedBytes: {info.TotalCommittedBytes} bytes");
 #endif
 
             GCSettings.LargeObjectHeapCompactionMode = GCLargeObjectHeapCompactionMode.CompactOnce;
             GC.Collect();
             GC.WaitForPendingFinalizers();
 
-            Log.LogMessage($"After GC {process.Id} - PeakWorkingSet64: {process.PeakWorkingSet64} bytes");
-            Log.LogMessage($"After GC {process.Id} - PrivateMemorySize64: {process.PrivateMemorySize64} bytes");
-            Log.LogMessage($"After GC {process.Id} - WorkingSet64: {process.WorkingSet64} bytes");
+            Log.LogMessage(MessageImportance.High, $"After GC {process.Id} - PeakWorkingSet64: {process.PeakWorkingSet64} bytes");
+            Log.LogMessage(MessageImportance.High, $"After GC {process.Id} - PrivateMemorySize64: {process.PrivateMemorySize64} bytes");
+            Log.LogMessage(MessageImportance.High, $"After GC {process.Id} - WorkingSet64: {process.WorkingSet64} bytes");
 #if NET
             info = GC.GetGCMemoryInfo(GCKind.Any);
-            Log.LogMessage($"After GC {process.Id} - FragmentedBytes: {info.FragmentedBytes} bytes");
-            Log.LogMessage($"After GC {process.Id} - Compacted: {info.Compacted} bytes");
-            Log.LogMessage($"After GC {process.Id} - HighMemoryLoadThresholdBytes: {info.HighMemoryLoadThresholdBytes} bytes");
-            Log.LogMessage($"After GC {process.Id} - TotalAvailableMemoryBytes: {info.TotalAvailableMemoryBytes} bytes");
-            Log.LogMessage($"After GC {process.Id} - TotalCommittedBytes: {info.TotalCommittedBytes} bytes");
+            Log.LogMessage(MessageImportance.High, $"After GC {process.Id} - FragmentedBytes: {info.FragmentedBytes} bytes");
+            Log.LogMessage(MessageImportance.High, $"After GC {process.Id} - Compacted: {info.Compacted} bytes");
+            Log.LogMessage(MessageImportance.High, $"After GC {process.Id} - HighMemoryLoadThresholdBytes: {info.HighMemoryLoadThresholdBytes} bytes");
+            Log.LogMessage(MessageImportance.High, $"After GC {process.Id} - TotalAvailableMemoryBytes: {info.TotalAvailableMemoryBytes} bytes");
+            Log.LogMessage(MessageImportance.High, $"After GC {process.Id} - TotalCommittedBytes: {info.TotalCommittedBytes} bytes");
 #endif
             return true;
         }
