46a35d18b23ced6829b0b3e7fab1372e98d73ce1
ManickaP <mapichov@microsoft.com>
2025-02-04 13:43:56 +0100

    Revert using change

--- src/libraries/System.Net.Http/tests/FunctionalTests/HttpClientHandlerTest.Http3.cs
+++ src/libraries/System.Net.Http/tests/FunctionalTests/HttpClientHandlerTest.Http3.cs
@@ -4,7 +4,6 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.Tracing;
-using System.DirectoryServices.Protocols;
 using System.Globalization;
 using System.IO;
 using System.Linq;
