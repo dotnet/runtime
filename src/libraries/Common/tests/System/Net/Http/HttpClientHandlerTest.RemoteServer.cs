11c91e5d7fa0f030ee6b9b0c7784c3da8b267b6c
Roman Konecny <rokonecn@microsoft.com>
2025-02-06 15:15:08 +0100

    Test another way

--- src/libraries/Common/tests/System/Net/Http/HttpClientHandlerTest.RemoteServer.cs
+++ src/libraries/Common/tests/System/Net/Http/HttpClientHandlerTest.RemoteServer.cs
@@ -1384,6 +1384,9 @@ namespace System.Net.Http.Functional.Tests
 
             using (HttpClient client = CreateHttpClient())
             {
+                client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
+                                                                    "(KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36");
+
                 using (HttpResponseMessage response = await client.SendAsync(TestAsync, request))
                 {
                     Assert.Equal(HttpStatusCode.OK, response.StatusCode);
