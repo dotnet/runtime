a107cfd02ba27bc19d92ea82962920439f1e9010
Filip Navara <filip.navara@gmail.com>
2025-02-07 09:40:10 +0100

    Update comment

--- src/coreclr/jit/morph.cpp
+++ src/coreclr/jit/morph.cpp
@@ -13961,7 +13961,10 @@ void Compiler::fgSetOptions()
     // an EH clause that we delete as statically dead code before we
     // get here, leaving no EH clauses left, and thus no requirement
     // to use a frame pointer because of EH. But until all the code uses
-    // the same test, leave info.compXcptnsCount here.
+    // the same test, leave info.compXcptnsCount here. Also test for
+    // CORINFO_FLG_SYNCH methods which are converted into try-finally
+    // with Monitor helper calls and need to be treated as methods with
+    // EH.
     if (info.compXcptnsCount > 0 || (info.compFlags & CORINFO_FLG_SYNCH))
     {
         codeGen->setFramePointerRequiredEH(true);
