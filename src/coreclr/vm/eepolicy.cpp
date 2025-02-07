55d506ee821754c3f949058555b0596c6611180a
Jan Vorlicek <janvorli@microsoft.com>
2025-02-07 19:01:19 +0000

    Add another set of debugging prints for stack overflow issue
    This adds more debugging output to help figure out the mysterious failure
    of the stack overflow tests that occur regularly in the lab, but that
    I am unable to repro locally.

--- src/coreclr/vm/eepolicy.cpp
+++ src/coreclr/vm/eepolicy.cpp
@@ -671,6 +671,9 @@ void DECLSPEC_NORETURN EEPolicy::HandleFatalStackOverflow(EXCEPTION_POINTERS *pE
             // Wait for the stack trace logging completion
             DWORD res = WaitForSingleObject(stackDumpThreadHandle, INFINITE);
             _ASSERTE(res == WAIT_OBJECT_0);
+ #ifdef _DEBUG
+            PrintToStdErrA("Stack trace printing helper thread exited.\n");
+ #endif    
         }
 
         g_stackOverflowCallStackLogged = 2;
@@ -701,6 +704,9 @@ void DECLSPEC_NORETURN EEPolicy::HandleFatalStackOverflow(EXCEPTION_POINTERS *pE
         if (pThread)
         {
             GCX_COOP();
+#ifdef _DEBUG
+            PrintToStdErrA("Setting throwable.\n");
+#endif    
             // If we had a SO before preallocated exception objects are initialized, we will AV here. This can happen
             // during the initialization of SystemDomain during EEStartup. Thus, setup the SO throwable only if its not
             // NULL.
@@ -734,10 +740,15 @@ void DECLSPEC_NORETURN EEPolicy::HandleFatalStackOverflow(EXCEPTION_POINTERS *pE
         }
 #endif // !TARGET_UNIX
 
+#ifdef _DEBUG
+        PrintToStdErrA("Calling WatsonLastChance.\n");
+#endif    
         WatsonLastChance(pThread, pExceptionInfo,
             (fTreatAsNativeUnhandledException == FALSE)? TypeOfReportedError::UnhandledException: TypeOfReportedError::NativeThreadUnhandledException);
     }
-
+#ifdef _DEBUG
+    PrintToStdErrA("Terminating the process.\n");
+#endif    
     CrashDumpAndTerminateProcess(COR_E_STACKOVERFLOW);
     UNREACHABLE();
 }
