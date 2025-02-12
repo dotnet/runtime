10f5f798113a9dd5140d1de598a19bd55a3693a1
Andrew Au <andrewau@microsoft.com>
2025-02-12 08:51:44 -0800

    Code review feedback

--- src/coreclr/vm/finalizerthread.cpp
+++ src/coreclr/vm/finalizerthread.cpp
@@ -294,12 +294,12 @@ VOID FinalizerThread::FinalizerThreadWorker(void *args)
             gcGenAnalysisState = GcGenAnalysisState::Disabled;
             if (gcGenAnalysisTrace)
             {
-#ifdef FEATURE_EVENT_TRACE
+#ifdef FEATURE_PERFTRACING
                 EventPipeAdapter::Disable(gcGenAnalysisEventPipeSessionId);
 #ifdef GEN_ANALYSIS_STRESS
                 GenAnalysis::EnableGenerationalAwareSession();
 #endif //GEN_ANALYSIS_STRESS
-#endif //FEATURE_EVENT_TRACE
+#endif //FEATURE_PERFTRACING
             }
 
             // Writing an empty file to indicate completion
