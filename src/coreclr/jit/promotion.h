e0b448f5594cd001607acc777615c6e20cebb32a
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2025-02-12 22:03:18 +0100

    JIT: Read back parameter register replacements eagerly
    Have physical promotion read replacements that map to parameter
    registers back eagerly in the entry basic block. Reading these back
    initially makes it more likely that the backend will be able to
    substitute directly for registers.

--- src/coreclr/jit/promotion.h
+++ src/coreclr/jit/promotion.h
@@ -154,7 +154,7 @@ class Promotion
 
     static bool     IsCandidateForPhysicalPromotion(LclVarDsc* dsc);
     static GenTree* EffectiveUser(Compiler::GenTreeStack& ancestors);
-
+    static bool     MapsToParameterRegister(Compiler* comp, unsigned lclNum, unsigned offs, var_types accessType);
 public:
     explicit Promotion(Compiler* compiler)
         : m_compiler(compiler)
@@ -274,9 +274,9 @@ public:
         return m_mayHaveForwardSub;
     }
 
-    void StartBlock(BasicBlock* block);
-    void EndBlock();
-    void StartStatement(Statement* stmt);
+    Statement* StartBlock(BasicBlock* block);
+    void       EndBlock();
+    void       StartStatement(Statement* stmt);
 
     fgWalkResult PostOrderVisit(GenTree** use, GenTree* user);
 
