1048e2e94c03c85e0b16cb6a2569fe0516a7c13c
Adeel <3840695+am11@users.noreply.github.com>
2025-02-13 16:47:25 +0200

    Fix AOT exception handling on linux-riscv64

--- src/coreclr/nativeaot/Runtime/StackFrameIterator.cpp
+++ src/coreclr/nativeaot/Runtime/StackFrameIterator.cpp
@@ -1285,7 +1285,7 @@ void StackFrameIterator::UnwindFuncletInvokeThunk()
 #elif defined(TARGET_RISCV64)
     PTR_uint64_t f = (PTR_uint64_t)(m_RegDisplay.SP);
 
-    for (int i = 0; i < 32; i++)
+    for (int i = 0; i < 8; i++)
     {
         m_RegDisplay.F[i] = *f++;
     }
