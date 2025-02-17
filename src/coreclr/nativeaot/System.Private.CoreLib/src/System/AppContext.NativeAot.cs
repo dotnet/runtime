0bb7c80f91242cfe493146b1ac79267341c085bd
Jeremy Koritzinsky <jekoritz@microsoft.com>
2025-02-17 15:48:50 -0800

    Fix missing using

--- src/coreclr/nativeaot/System.Private.CoreLib/src/System/AppContext.NativeAot.cs
+++ src/coreclr/nativeaot/System.Private.CoreLib/src/System/AppContext.NativeAot.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Internal.Runtime.Augments;
 using System.Collections.Generic;
 using System.Runtime;
 using System.Runtime.ExceptionServices;
