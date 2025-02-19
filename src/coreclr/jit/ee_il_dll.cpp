617491fdb423c742405a3e89b3f3a03670cbe514
Elinor Fung <elfung@microsoft.com>
2025-02-18 18:32:41 -0800

    Make clrjit build with TARGET_ANDROID

--- src/coreclr/jit/ee_il_dll.cpp
+++ src/coreclr/jit/ee_il_dll.cpp
@@ -29,6 +29,10 @@ XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 #define DLLEXPORT
 #endif // !DLLEXPORT
 
+#if defined(TARGET_ANDROID)
+#include <android/log.h>
+#endif
+
 /*****************************************************************************/
 
 ICorJitHost* g_jitHost        = nullptr;
@@ -148,7 +152,11 @@ int jitprintf(const char* fmt, ...)
 {
     va_list vl;
     va_start(vl, fmt);
+#if defined(TARGET_ANDROID)
+    int status = __android_log_vprint(ANDROID_LOG_VERBOSE, MAIN_CLR_MODULE_NAME_A, fmt, vl);
+#else
     int status = vfprintf(jitstdout(), fmt, vl);
+#endif
     va_end(vl);
     return status;
 }
