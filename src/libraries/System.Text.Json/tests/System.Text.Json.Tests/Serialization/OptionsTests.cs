09ec94d5473d8eca40a06cf2baf800139760beac
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2025-02-20 13:05:36 +0000

    Fix stack overflow condition when appending modifiers to resolver chains.

--- src/libraries/System.Text.Json/tests/System.Text.Json.Tests/Serialization/OptionsTests.cs
+++ src/libraries/System.Text.Json/tests/System.Text.Json.Tests/Serialization/OptionsTests.cs
@@ -1413,12 +1413,19 @@ namespace System.Text.Json.Serialization.Tests
         }
 
         [Fact]
-        public static void TypeInfoResolverChain_AppendTypeInfoResolverToChain_ThrowsInvalidOperationException()
+        public static void TypeInfoResolverChain_AddsModifier_WorksAsExpected()
         {
+            // Regression test for https://github.com/dotnet/runtime/issues/112735
+            bool isModifierInvoked = false;
             var defaultResolver = new DefaultJsonTypeInfoResolver();
-            var options = new JsonSerializerOptions { TypeInfoResolver = defaultResolver };
+            var options = new JsonSerializerOptions { TypeInfoResolver = JsonContext.Default };
+            options.TypeInfoResolverChain.Add(new DefaultJsonTypeInfoResolver());
+            options.TypeInfoResolver = options.TypeInfoResolver!.WithAddedModifier(_ => isModifierInvoked = true);
+            options.MakeReadOnly();
 
-            Assert.Throws<InvalidOperationException>(() => options.TypeInfoResolverChain.Add(options.TypeInfoResolver));
+            options.GetTypeInfo(typeof(List<List<List<string>>>)); // A type not in JsonContext.Default
+
+            Assert.True(isModifierInvoked);
         }
 
         [Fact]
