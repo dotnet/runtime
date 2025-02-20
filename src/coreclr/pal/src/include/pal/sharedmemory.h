d0a6630a96f37c862c0adaf023c1daa6beb2de59
Eduardo Velarde <eduardo.velarde@pucp.pe>
2025-02-19 17:38:32 -0800

    Remove PAL dependencies in sharedmemory.h

--- src/coreclr/pal/src/include/pal/sharedmemory.h
+++ src/coreclr/pal/src/include/pal/sharedmemory.h
@@ -4,9 +4,6 @@
 #ifndef _PAL_SHARED_MEMORY_H_
 #define _PAL_SHARED_MEMORY_H_
 
-#include "corunix.hpp"
-#include <minipal/utils.h>
-
 #ifndef static_assert_no_msg
 #define static_assert_no_msg( cond ) static_assert( cond, #cond )
 #endif // !static_assert_no_msg
