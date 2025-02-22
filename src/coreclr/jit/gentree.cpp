3cedb210affc2a2464b8cd1feadaf5189b30f322
shkinosh <shkinosh@microsoft.com>
2025-02-22 14:52:45 +0900

    JIT: Add morphing to LE_UN/GT_UN(expr, uint.MaxValue) (dotnet#76525)
    * Add morphing for comparison between uint.MaxValue and a ulong value.
    * Remove assertions, which no longer is relevant.
    * Add a check to lsra to only use BMI2 if zero flag will not be set.
    * Add a check to optimize EQ/NE(RSZ, 0) only if RSZ has constant.

--- src/coreclr/jit/gentree.cpp
+++ src/coreclr/jit/gentree.cpp
@@ -19978,7 +19978,7 @@ bool GenTree::IsArrayAddr(GenTreeArrAddr** pArrAddr)
 bool GenTree::SupportsSettingZeroFlag()
 {
 #if defined(TARGET_XARCH)
-    if (OperIs(GT_AND, GT_OR, GT_XOR, GT_ADD, GT_SUB, GT_NEG))
+    if (OperIs(GT_AND, GT_OR, GT_XOR, GT_ADD, GT_SUB, GT_NEG, GT_RSH, GT_RSZ))
     {
         return true;
     }
