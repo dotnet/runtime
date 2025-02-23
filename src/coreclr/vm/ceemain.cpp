322dadc34e47ba57795fe14327ba1ae0e4742681
vsadov <8218165+VSadov@users.noreply.github.com>
2025-02-22 19:42:45 -0800

    better assert

--- src/coreclr/vm/ceemain.cpp
+++ src/coreclr/vm/ceemain.cpp
@@ -1751,20 +1751,27 @@ static void RuntimeThreadShutdown(void* thread)
 // Index for the fiber local storage of the attached thread pointer
 static uint32_t g_flsIndex = FLS_OUT_OF_INDEXES;
 
+#define FLS_STATE_CLEAR 0
+#define FLS_STATE_ARMED 1
+#define FLS_STATE_INVOKED 2
+
+static __declspec(thread) int flsState;
+
 // This is called when each *fiber* is destroyed. When the home fiber of a thread is destroyed,
 // it means that the thread itself is destroyed.
 // Since we receive that notification outside of the Loader Lock, it allows us to safely acquire
 // the ThreadStore lock in the RuntimeThreadShutdown.
 static void __stdcall FiberDetachCallback(void* lpFlsData)
 {
-    ASSERT(g_flsIndex != FLS_OUT_OF_INDEXES);
-    ASSERT(lpFlsData == FlsGetValue(g_flsIndex));
+    _ASSERTE(g_flsIndex != FLS_OUT_OF_INDEXES);
+    _ASSERTE(lpFlsData);
 
-    if (lpFlsData != NULL)
+    if (flsState == FLS_STATE_ARMED)
     {
-        // The current fiber is the home fiber of a thread, so the thread is shutting down
         RuntimeThreadShutdown(lpFlsData);
     }
+
+    flsState = FLS_STATE_INVOKED;
 }
 
 void InitFlsSlot()
@@ -1784,12 +1791,9 @@ void InitFlsSlot()
 //  thread        - thread to attach
 static void OsAttachThread(void* thread)
 {
-    void* threadFromCurrentFiber = FlsGetValue(g_flsIndex);
+    _ASSERTE_ALL_BUILDS((flsState != FLS_STATE_INVOKED) && "Initializing thread after termination callback has run.");
 
-    if (threadFromCurrentFiber != NULL)
-    {
-        _ASSERTE_ALL_BUILDS(!"Multiple threads encountered from a single fiber");
-    }
+    flsState = FLS_STATE_ARMED;
 
     // Associate the current fiber with the current thread.  This makes the current fiber the thread's "home"
     // fiber.  This fiber is the only fiber allowed to execute managed code on this thread.  When this fiber
@@ -1808,19 +1812,12 @@ void OsDetachThread(void* thread)
     ASSERT(g_flsIndex != FLS_OUT_OF_INDEXES);
     void* threadFromCurrentFiber = FlsGetValue(g_flsIndex);
 
-    if (threadFromCurrentFiber == NULL)
-    {
-        // we've seen this thread, but not this fiber.  It must be a "foreign" fiber that was
-        // borrowing this thread.
-        return;
-    }
-
     if (threadFromCurrentFiber != thread)
     {
         _ASSERTE_ALL_BUILDS(!"Detaching a thread from the wrong fiber");
     }
 
-    FlsSetValue(g_flsIndex, NULL);
+    flsState = FLS_STATE_CLEAR;
 }
 
 void EnsureTlsDestructionMonitor()
