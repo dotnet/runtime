a5a07d2cfb4d4b1d46411237a222f2e5880d5b68
Sanjam Panda <sanjam.panda@ibm.com>
2025-02-24 12:34:17 +0000

    blah

--- src/libraries/System.Runtime/tests/System.Buffers.Tests/ArrayPool/UnitTests.cs
+++ src/libraries/System.Runtime/tests/System.Buffers.Tests/ArrayPool/UnitTests.cs
@@ -446,7 +446,7 @@ namespace System.Buffers.ArrayPool.Tests
             RemoteInvokeWithTrimming(() =>
             {
                 var buffers = new List<byte[]>();
-                for (int i = 0; i < 1000; i++)
+                for (int i = 0; i < 2050; i++)
                 {
                     buffers.Add(ArrayPool<byte>.Shared.Rent(1));
                 }
