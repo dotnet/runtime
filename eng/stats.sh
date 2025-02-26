40d41182ff5ffb90d3527f7c0642712c84e24327
pavelsavara <pavel.savara@gmail.com>
2025-02-26 17:15:39 +0100

    more

--- eng/stats.sh
+++ eng/stats.sh
@@ -19,16 +19,30 @@ while true; do
   elapsed_time=$(($current_time - $start_time))
   echo "-------- elapsed_time $elapsed_time ---------"
 
-  if [ $elapsed_time -ge 180 ] && [ "$triggered" == "false" ]; then
+  if [ $elapsed_time -ge 600 ] && [ "$triggered" == "false" ]; then
     triggered="true"
     echo "--------installing---------"
     sudo /__w/1/s/.dotnet/dotnet tool install --global dotnet-trace
     export pid=$(ps aux | grep "noautoresponse" | sort -nrk 4 | awk 'NR==1{print $2}')
     echo "--------collecting $pid  ---------"
-    sudo mkdir -p /mnt/vss/_work/1/a/artifacts/log
-    sudo ls -la /mnt/vss/_work/1/a/artifacts/log/
-    sudo /root/.dotnet/tools/dotnet-trace collect --profile gc-collect -p "$pid" --duration 00:01:00 --output /mnt/vss/_work/1/a/artifacts/log/trace.$(date +%s).nettrace
-    sudo ls -la /mnt/vss/_work/1/a/artifacts/log/
+    sudo mkdir -p /__w/1/s/artifacts/log/
+    sudo mkdir -p /__w/1/a/artifacts/log/
+    echo /__w/1/s/artifacts/log/
+    sudo ls -la /__w/1/s/artifacts/log/
+    echo /__w/1/a/artifacts/log/
+    sudo ls -la /__w/1/a/artifacts/log/
+    timestamp=$(date +%s)
+    sudo /root/.dotnet/tools/dotnet-trace collect --profile gc-collect -p "$pid" --duration 00:10:00 --output /__w/1/s/artifacts/log/trace.$timestamp.nettrace
+    sudo chmod a+r /__w/1/s/artifacts/log/trace.$timestamp.nettrace
+    echo /__w/1/s/artifacts/log/
+    sudo ls -la /__w/1/s/artifacts/log/
+    echo /__w/1/a/artifacts/log/
+    sudo ls -la /__w/1/a/artifacts/log/
+    sudo cp /__w/1/s/artifacts/log/trace.$timestamp.nettrace /__w/1/a/artifacts/log/trace.$timestamp.nettrace
+    echo /__w/1/s/artifacts/log/
+    sudo ls -la /__w/1/s/artifacts/log/
+    echo /__w/1/a/artifacts/log/
+    sudo ls -la /__w/1/a/artifacts/log/
     echo "--------end collecting---------"
   fi
 done
