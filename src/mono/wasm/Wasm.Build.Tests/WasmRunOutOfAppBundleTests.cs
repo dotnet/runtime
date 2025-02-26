6c8540a73707c7c0dc007a0413e571c9f119a14e
Marek Fišera <mara@neptuo.com>
2025-02-26 21:52:43 +0100

    Fix missing head

--- src/mono/wasm/Wasm.Build.Tests/WasmRunOutOfAppBundleTests.cs
+++ src/mono/wasm/Wasm.Build.Tests/WasmRunOutOfAppBundleTests.cs
@@ -34,7 +34,7 @@ public class WasmRunOutOfAppBundleTests : WasmTemplateTestsBase
         string relativeMainJsPath = "./wwwroot/main.js";
         if (!File.Exists(indexHtmlPath))
         {
-            var html = $@"<!DOCTYPE html><html><body><script type=""module"" src=""{relativeMainJsPath}""></script></body></html>";
+            var html = $@"<!DOCTYPE html><html><head></head><body><script type=""module"" src=""{relativeMainJsPath}""></script></body></html>";
             File.WriteAllText(indexHtmlPath, html);
         }
 
