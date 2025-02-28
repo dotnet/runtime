d8714aadb3d9e2ea6676151a258c4cf883057f67
Jan Vorlicek <janvorli@microsoft.com>
2025-02-28 20:25:07 +0100

    Fix MUSL build break

--- src/coreclr/vm/codeman.h
+++ src/coreclr/vm/codeman.h
@@ -2770,14 +2770,14 @@ public:
     {
         // Not used for the interpreter
         _ASSERTE(FALSE);
-        return NULL;
+        return PTR_NULL;
     }
 
     virtual TADDR GetFuncletStartAddress(EECodeInfo * pCodeInfo)
     {
         // Not used for the interpreter
         _ASSERTE(FALSE);
-        return NULL;
+        return 0;
     }
 
     virtual DWORD GetFuncletStartOffsets(const METHODTOKEN& MethodToken, DWORD* pStartFuncletOffsets, DWORD dwLength)
