bb89b87023d149c9872a71d536ca3c7a38053ed8
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2025-03-01 14:39:22 +0100

    Update JIT-EE GUID after merge

--- src/coreclr/inc/jiteeversionguid.h
+++ src/coreclr/inc/jiteeversionguid.h
@@ -37,11 +37,11 @@
 
 #include <minipal/guid.h>
 
-constexpr GUID JITEEVersionIdentifier = { /* 4463d6ac-dfcb-4ab0-a941-c53b56089b7c */
-    0x4463d6ac,
-    0xdfcb,
-    0x4ab0,
-    {0xa9, 0x41, 0xc5, 0x3b, 0x56, 0x08, 0x9b, 0x7c}
+constexpr GUID JITEEVersionIdentifier = { /* 254e838d-821b-4600-9c4e-b5ea6ef20d38 */
+    0x254e838d,
+    0x821b,
+    0x4600,
+    {0x9c, 0x4e, 0xb5, 0xea, 0x6e, 0xf2, 0x0d, 0x38}
   };
 
 #endif // JIT_EE_VERSIONING_GUID_H
