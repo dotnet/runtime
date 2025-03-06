f34c2662179d9e31567656c95ea4f6d538b08bd1
Eduardo Velarde <eduardo.velarde@pucp.pe>
2025-03-05 16:07:19 -0800

    Disable spin waiting in Monitor

--- src/coreclr/vm/syncblk.cpp
+++ src/coreclr/vm/syncblk.cpp
@@ -1552,6 +1552,10 @@ BOOL ObjHeader::TryEnterObjMonitor(INT32 timeOut)
     return GetSyncBlock()->TryEnterMonitor(timeOut);
 }
 
+extern DWORD g_currentProcessCpuCount = 0;
+
+extern BOOL g_isCpuQuotaLimited = false;
+
 AwareLock::EnterHelperResult ObjHeader::EnterObjMonitorHelperSpin(Thread* pCurThread)
 {
     CONTRACTL{
@@ -1564,6 +1568,19 @@ AwareLock::EnterHelperResult ObjHeader::EnterObjMonitorHelperSpin(Thread* pCurTh
 
     if (g_SystemInfo.dwNumberOfProcessors == 1)
     {
+        return AwareLock::EnterHelperResult_Contention;
+    }
+
+    // Initialize g_currentProcessCpuCount and g_isCpuQuotaLimited if they haven't been initialized yet
+    if (g_currentProcessCpuCount == 0)
+    {
+        GetCurrentProcessCpuCount();
+    }
+
+    _ASSERTE(g_currentProcessCpuCount > 0);
+    if (g_isCpuQuotaLimited)
+    {
+        g_SpinConstants.dwMonitorSpinCount = 0;
         return AwareLock::EnterHelperResult_Contention;
     }
 
