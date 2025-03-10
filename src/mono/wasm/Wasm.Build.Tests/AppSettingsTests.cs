5b4aabc667bfcbda2545965eb7be871ed64e0b8f
Marek Fišera <mara@neptuo.com>
2025-03-10 12:22:07 +0100

    More test data

--- src/mono/wasm/Wasm.Build.Tests/AppSettingsTests.cs
+++ src/mono/wasm/Wasm.Build.Tests/AppSettingsTests.cs
@@ -21,32 +21,49 @@ public class AppSettingsTests : WasmTemplateTestsBase
     {
     }
 
+    public static IEnumerable<object?[]> LoadAppSettingsBasedOnApplicationEnvironmentData()
+    {
+        // Defaults
+        yield return new object?[] { false, null, null, "Development" };
+        yield return new object?[] { true, null, null, "Production" };
+
+        // Override defaults from MSBuild
+        yield return new object?[] { false, "Production", null, "Production" };
+        yield return new object?[] { true, "Development", null, "Development" };
+
+        // Override defaults from JavaScript
+        yield return new object?[] { false, null, "Production", "Production" };
+        yield return new object?[] { true, null, "Development", "Development" };
+
+        // Override MSBuild from JavaScript
+        yield return new object?[] { false, "FromMSBuild", "Production", "Production" };
+        yield return new object?[] { true, "FromMSBuild", "Development", "Development" };
+    }
+
     [Theory]
-    [InlineData("Development", null)]
-    [InlineData("Production", null)]
-    [InlineData(null, "Development")]
-    [InlineData(null, "Production")]
-    [InlineData("Production", "Development")]
-    [InlineData("Development", "Production")]
-    public async Task LoadAppSettingsBasedOnApplicationEnvironment(string msBuildApplicationEnvironment, string queryApplicationEnvironment)
+    [MemberData(nameof(LoadAppSettingsBasedOnApplicationEnvironmentData))]
+    public async Task LoadAppSettingsBasedOnApplicationEnvironment(bool publish, string msBuildApplicationEnvironment, string queryApplicationEnvironment, string expectedApplicationEnvironment)
     {
         Configuration config = Configuration.Debug;
         ProjectInfo info = CopyTestAsset(config, aot: false, TestAsset.WasmBasicTestApp, "AppSettingsTest");
-        PublishProject(
-            info, 
-            config,
-            new PublishOptions(ExtraMSBuildArgs: $"-p:WasmApplicationEnvironmentName={msBuildApplicationEnvironment}")
-        );
-        BrowserRunOptions options = new(
+        string extraMSBuildArgs = msBuildApplicationEnvironment != null ? $"-p:WasmApplicationEnvironmentName={msBuildApplicationEnvironment}" : string.Empty;
+
+        if (publish)
+            PublishProject(info, config, new PublishOptions(ExtraMSBuildArgs: extraMSBuildArgs));
+        else
+            BuildProject(info, config, new BuildOptions(ExtraMSBuildArgs: extraMSBuildArgs));
+        
+        BrowserRunOptions runOptions = new(
             config,
             TestScenario: "AppSettingsTest",
             BrowserQueryString: new NameValueCollection { { "applicationEnvironment", queryApplicationEnvironment } }
         );
-        RunResult result = await RunForPublishWithWebServer(options);
+        RunResult result = publish
+            ? await RunForPublishWithWebServer(runOptions)
+            : await RunForBuildWithDotnetRun(runOptions);
 
-        string effectiveApplicationEnvironment = queryApplicationEnvironment ?? msBuildApplicationEnvironment;
         Assert.Contains(result.TestOutput, m => m.Contains("'/appsettings.json' exists 'True'"));
-        Assert.Contains(result.TestOutput, m => m.Contains($"'/appsettings.Development.json' exists '{effectiveApplicationEnvironment == "Development"}'"));
-        Assert.Contains(result.TestOutput, m => m.Contains($"'/appsettings.Production.json' exists '{effectiveApplicationEnvironment == "Production"}'"));
+        Assert.Contains(result.TestOutput, m => m.Contains($"'/appsettings.Development.json' exists '{expectedApplicationEnvironment == "Development"}'"));
+        Assert.Contains(result.TestOutput, m => m.Contains($"'/appsettings.Production.json' exists '{expectedApplicationEnvironment == "Production"}'"));
     }
 }
