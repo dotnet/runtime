b792a762fa373fc6b59d40968b566646a9a49ffc
xin ding <beyond.mind@outlook.com>
2025-03-11 15:42:47 +0800

    Optimization

--- src/libraries/Microsoft.Extensions.Options/src/IInitializationOptions.cs
+++ src/libraries/Microsoft.Extensions.Options/src/IInitializationOptions.cs
@@ -3,8 +3,20 @@
 
 namespace Microsoft.Extensions.Options
 {
+    /// <summary>
+    /// Represents something that initialize the <typeparamref name="TOptions"/> type.
+    /// </summary>
+    /// <typeparam name="TOptions">Options type being initialized.</typeparam>
+    /// <remarks>
+    /// This is run before all <see cref="IConfigureOptions{TOptions}"/>.
+    /// </remarks>
     public interface IInitializationOptions<out TOptions> where TOptions : class
     {
+        /// <summary>
+        /// Creates a new instance of type <typeparamref name="TOptions"/>.
+        /// </summary>
+        /// <param name="name">The name of the <typeparamref name="TOptions"/> instance to create.</param>
+        /// <returns>The created <typeparamref name="TOptions"/> instance.</returns>
         TOptions Initialize(string name);
     }
 }
