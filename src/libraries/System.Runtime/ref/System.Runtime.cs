e2ef692b0eff69790ab2bd8d6e6ce140ab095a9e
Fred Silberberg <frsilb@microsoft.com>
2025-03-12 01:19:11 +0000

    PR feedback

--- src/libraries/System.Runtime/ref/System.Runtime.cs
+++ src/libraries/System.Runtime/ref/System.Runtime.cs
@@ -13490,72 +13490,72 @@ namespace System.Runtime.CompilerServices
     {
         public IndexerNameAttribute(string indexerName) { }
     }
-    [InlineArray(2)]
+    [System.Runtime.CompilerServices.InlineArray(2)]
     public struct InlineArray2<T>
     {
         private T t;
     }
-    [InlineArray(3)]
+    [System.Runtime.CompilerServices.InlineArray(3)]
     public struct InlineArray3<T>
     {
         private T t;
     }
-    [InlineArray(4)]
+    [System.Runtime.CompilerServices.InlineArray(4)]
     public struct InlineArray4<T>
     {
         private T t;
     }
-    [InlineArray(5)]
+    [System.Runtime.CompilerServices.InlineArray(5)]
     public struct InlineArray5<T>
     {
         private T t;
     }
-    [InlineArray(6)]
+    [System.Runtime.CompilerServices.InlineArray(6)]
     public struct InlineArray6<T>
     {
         private T t;
     }
-    [InlineArray(7)]
+    [System.Runtime.CompilerServices.InlineArray(7)]
     public struct InlineArray7<T>
     {
         private T t;
     }
-    [InlineArray(8)]
+    [System.Runtime.CompilerServices.InlineArray(8)]
     public struct InlineArray8<T>
     {
         private T t;
     }
-    [InlineArray(9)]
+    [System.Runtime.CompilerServices.InlineArray(9)]
     public struct InlineArray9<T>
     {
         private T t;
     }
-    [InlineArray(10)]
+    [System.Runtime.CompilerServices.InlineArray(10)]
     public struct InlineArray10<T>
     {
         private T t;
     }
-    [InlineArray(11)]
+    [System.Runtime.CompilerServices.InlineArray(11)]
     public struct InlineArray11<T>
     {
         private T t;
     }
-    [InlineArray(12)]
+    [System.Runtime.CompilerServices.InlineArray(12)]
     public struct InlineArray12<T>
     {
         private T t;
     }
-    [InlineArray(13)]
+    [System.Runtime.CompilerServices.InlineArray(13)]
     public struct InlineArray13<T>
     {
         private T t;
     }
-    [InlineArray(14)]
+    [System.Runtime.CompilerServices.InlineArray(14)]
     public struct InlineArray14<T>
     {
         private T t;
     }
-    [InlineArray(15)]
+    [System.Runtime.CompilerServices.InlineArray(15)]
     public struct InlineArray15<T>
     {
         private T t;
--- src/libraries/System.Runtime/tests/System.Runtime.Tests/System/Runtime/CompilerServices/InlineArrayTests.cs
+++ src/libraries/System.Runtime/tests/System.Runtime.Tests/System/Runtime/CompilerServices/InlineArrayTests.cs
@@ -8,194 +8,117 @@ namespace System.Runtime.CompilerServices.Tests
 {
     public static class InlineArrayTests
     {
+        private static void WriteRead(Span<int> span, int expectedLength)
+        {
+            Assert.Equal(expectedLength, span.Length);
+
+            for (int i = 0; i < span.Length; i++)
+            {
+                span[i] = i + 1;
+            }
+
+            for (int i = 0; i < span.Length; i++)
+            {
+                Assert.Equal(i + 1, span[i]);
+            }
+        }
+
         [Fact]
         public void InlineArray2Test()
         {
-            InlineArray2<int> inlineArray2 = new InlineArray2<int>();
-            inlineArray2[0] = 1;
-            inlineArray2[1] = 2;
-            Assert.Equal(1, inlineArray2[0]);
-            Assert.Equal(2, inlineArray2[1]);
+            InlineArray2<int> inlineArray2 = new();
+            WriteRead(inlineArray2, 2);
         }
 
         [Fact]
         public void InlineArray3Test()
         {
-            InlineArray3<int> inlineArray3 = new InlineArray3<int>();
-            inlineArray3[0] = 1;
-            inlineArray3[1] = 2;
-            inlineArray3[2] = 3;
-            Assert.Equal(1, inlineArray3[0]);
-            Assert.Equal(2, inlineArray3[1]);
-            Assert.Equal(3, inlineArray3[2]);
+            InlineArray3<int> inlineArray3 = new();
+            WriteRead(inlineArray3, 3);
         }
 
         [Fact]
         public void InlineArray4Test()
         {
-            InlineArray4<int> inlineArray4 = new InlineArray4<int>();
-            for (int i = 0; i < 4; i++)
-            {
-                inlineArray4[i] = i + 1;
-            }
-            for (int i = 0; i < 4; i++)
-            {
-                Assert.Equal(i + 1, inlineArray4[i]);
-            }
+            InlineArray4<int> inlineArray4 = new();
+            WriteRead(inlineArray4, 4);
         }
 
         [Fact]
         public void InlineArray5Test()
         {
-            InlineArray5<int> inlineArray5 = new InlineArray5<int>();
-            for (int i = 0; i < 5; i++)
-            {
-                inlineArray5[i] = i + 1;
-            }
-            for (int i = 0; i < 5; i++)
-            {
-                Assert.Equal(i + 1, inlineArray5[i]);
-            }
+            InlineArray5<int> inlineArray5 = new();
+            WriteRead(inlineArray5, 5);
         }
 
         [Fact]
         public void InlineArray6Test()
         {
-            InlineArray6<int> inlineArray6 = new InlineArray6<int>();
-            for (int i = 0; i < 6; i++)
-            {
-                inlineArray6[i] = i + 1;
-            }
-            for (int i = 0; i < 6; i++)
-            {
-                Assert.Equal(i + 1, inlineArray6[i]);
-            }
+            InlineArray6<int> inlineArray6 = new();
+            WriteRead(inlineArray6, 6);
         }
 
         [Fact]
         public void InlineArray7Test()
         {
-            InlineArray7<int> inlineArray7 = new InlineArray7<int>();
-            for (int i = 0; i < 7; i++)
-            {
-                inlineArray7[i] = i + 1;
-            }
-            for (int i = 0; i < 7; i++)
-            {
-                Assert.Equal(i + 1, inlineArray7[i]);
-            }
+            InlineArray7<int> inlineArray7 = new();
+            WriteRead(inlineArray7, 7);
         }
 
         [Fact]
         public void InlineArray8Test()
         {
-            InlineArray8<int> inlineArray8 = new InlineArray8<int>();
-            for (int i = 0; i < 8; i++)
-            {
-                inlineArray8[i] = i + 1;
-            }
-            for (int i = 0; i < 8; i++)
-            {
-                Assert.Equal(i + 1, inlineArray8[i]);
-            }
+            InlineArray8<int> inlineArray8 = new();
+            WriteRead(inlineArray8, 8);
         }
 
         [Fact]
         public void InlineArray9Test()
         {
-            InlineArray9<int> inlineArray9 = new InlineArray9<int>();
-            for (int i = 0; i < 9; i++)
-            {
-                inlineArray9[i] = i + 1;
-            }
-            for (int i = 0; i < 9; i++)
-            {
-                Assert.Equal(i + 1, inlineArray9[i]);
-            }
+            InlineArray9<int> inlineArray9 = new();
+            WriteRead(inlineArray9, 9);
         }
 
         [Fact]
         public void InlineArray10Test()
         {
-            InlineArray10<int> inlineArray10 = new InlineArray10<int>();
-            for (int i = 0; i < 10; i++)
-            {
-                inlineArray10[i] = i + 1;
-            }
-            for (int i = 0; i < 10; i++)
-            {
-                Assert.Equal(i + 1, inlineArray10[i]);
-            }
+            InlineArray10<int> inlineArray10 = new();
+            WriteRead(inlineArray10, 10);
         }
 
         [Fact]
         public void InlineArray11Test()
         {
-            InlineArray11<int> inlineArray11 = new InlineArray11<int>();
-            for (int i = 0; i < 11; i++)
-            {
-                inlineArray11[i] = i + 1;
-            }
-            for (int i = 0; i < 11; i++)
-            {
-                Assert.Equal(i + 1, inlineArray11[i]);
-            }
+            InlineArray11<int> inlineArray11 = new();
+            WriteRead(inlineArray11, 11);
         }
 
         [Fact]
         public void InlineArray12Test()
         {
-            InlineArray12<int> inlineArray12 = new InlineArray12<int>();
-            for (int i = 0; i < 12; i++)
-            {
-                inlineArray12[i] = i + 1;
-            }
-            for (int i = 0; i < 12; i++)
-            {
-                Assert.Equal(i + 1, inlineArray12[i]);
-            }
+            InlineArray12<int> inlineArray12 = new();
+            WriteRead(inlineArray12, 12);
         }
 
         [Fact]
         public void InlineArray13Test()
         {
-            InlineArray13<int> inlineArray13 = new InlineArray13<int>();
-            for (int i = 0; i < 13; i++)
-            {
-                inlineArray13[i] = i + 1;
-            }
-            for (int i = 0; i < 13; i++)
-            {
-                Assert.Equal(i + 1, inlineArray13[i]);
-            }
+            InlineArray13<int> inlineArray13 = new();
+            WriteRead(inlineArray13, 13);
         }
 
         [Fact]
         public void InlineArray14Test()
         {
-            InlineArray14<int> inlineArray14 = new InlineArray14<int>();
-            for (int i = 0; i < 14; i++)
-            {
-                inlineArray14[i] = i + 1;
-            }
-            for (int i = 0; i < 14; i++)
-            {
-                Assert.Equal(i + 1, inlineArray14[i]);
-            }
+            InlineArray14<int> inlineArray14 = new();
+            WriteRead(inlineArray14, 14);
         }
 
         [Fact]
         public void InlineArray15Test()
         {
-            InlineArray15<int> inlineArray15 = new InlineArray15<int>();
-            for (int i = 0; i < 15; i++)
-            {
-                inlineArray15[i] = i + 1;
-            }
-            for (int i = 0; i < 15; i++)
-            {
-                Assert.Equal(i + 1, inlineArray15[i]);
-            }
+            InlineArray15<int> inlineArray15 = new();
+            WriteRead(inlineArray15, 15);
         }
     }
 }
