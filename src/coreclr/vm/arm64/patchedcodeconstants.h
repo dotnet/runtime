6ea99c0ea088ad3a1611700f17bcc1ad751af181
Alan Hayward <alan.hayward@arm.com>
2025-03-14 17:39:02 +0000

    fix patched constants on windows

--- src/coreclr/vm/arm64/patchedcodeconstants.h
+++ src/coreclr/vm/arm64/patchedcodeconstants.h
@@ -13,7 +13,13 @@
 // the values are correct.
 
 #define JIT_WriteBarrier_Size					   0x3a0
+
+#ifdef TARGET_WINDOWS
+#define JIT_WriteBarrier_Table_Offset              (0x30 + JIT_WriteBarrier_Size)
+#else
 #define JIT_WriteBarrier_Table_Offset              (0x2c + JIT_WriteBarrier_Size)
+#endif
+
 #define JIT_WriteBarrier_Offset_CardTable          (0x0  + JIT_WriteBarrier_Table_Offset)
 #define JIT_WriteBarrier_Offset_CardBundleTable    (0x8  + JIT_WriteBarrier_Table_Offset)
 #define JIT_WriteBarrier_Offset_WriteWatchTable    (0x10 + JIT_WriteBarrier_Table_Offset)
