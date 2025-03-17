ec570a9b0d8cdcf43777d105cdadca3924bf8264
Jan Kotas <jkotas@microsoft.com>
2025-03-17 06:36:07 -0700

    Update src/coreclr/jit/codegenlinear.cpp

--- src/coreclr/jit/codegenlinear.cpp
+++ src/coreclr/jit/codegenlinear.cpp
@@ -2484,8 +2484,9 @@ CodeGen::GenIntCastDesc::GenIntCastDesc(GenTreeCast* cast)
         }
 
 #if defined(TARGET_LOONGARCH64) || defined(TARGET_RISCV64)
-        // For LoongArch64's ISA which is a RISC ISA which is different from any other existing ones,
-        // even the instructions of 32bits operation need the upper 32bits be sign-extended to 64 bits.
+        // LoongArch64 and Riscv64 ABIs require 32-bit values to be sign-extended to 64-bits.
+        // We apply the sign-extension unconditionally here to avoid corner case bugs, even
+        // though it may not be strictly necessary in all cases.
         m_extendKind = SIGN_EXTEND_INT;
 #else
         m_extendKind = COPY;
