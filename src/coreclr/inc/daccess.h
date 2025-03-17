6700a0513bc00a1a9131155a65e9402ed61caf96
Radek Doulik <radek.doulik@gmail.com>
2025-03-17 20:29:04 +0100

    Remove unwanted change

--- src/coreclr/inc/daccess.h
+++ src/coreclr/inc/daccess.h
@@ -1148,7 +1148,7 @@ public:
     {
         return DPtrType(m_addr - val * sizeof(type));
     }
-#if defined(HOST_64BIT)
+#ifdef HOST_64BIT
     DPtrType operator-(unsigned int val)
     {
         return DPtrType(m_addr - val * sizeof(type));
