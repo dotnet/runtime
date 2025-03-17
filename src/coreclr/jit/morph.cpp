c10ab09912ac83a38e63cd0de90134bfbf39d667
EgorBo <egorbo@gmail.com>
2025-03-17 15:47:27 +0100

    Forward implicit byrefs to the new promotion to handle

--- src/coreclr/jit/morph.cpp
+++ src/coreclr/jit/morph.cpp
@@ -14011,7 +14011,7 @@ PhaseStatus Compiler::fgPromoteStructs()
             }
             tooManyLocalsReported = true;
         }
-        else if (varTypeIsStruct(varDsc))
+        else if (varTypeIsStruct(varDsc) && !lvaIsImplicitByRefLocal(lclNum))
         {
             assert(structPromotionHelper != nullptr);
             promotedVar = structPromotionHelper->TryPromoteStructVar(lclNum);
