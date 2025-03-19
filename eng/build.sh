bed8c13352865e5711df8179c522ce21c9244d81
pavelsavara <pavel.savara@gmail.com>
2025-03-19 16:25:32 +0100

    give msbuild nodes some memory pressure for browser os target

--- eng/build.sh
+++ eng/build.sh
@@ -542,6 +542,9 @@ fi
 if [[ "$os" == "browser" ]]; then
     # override default arch for Browser, we only support wasm
     arch=wasm
+    # because on docker instance without swap file, we need to make some room for LLVM
+    export DOTNET_GCHighMemPercent="10"
+    export DOTNET_GCHeapHardLimitPercent="24"
 fi
 if [[ "$os" == "wasi" ]]; then
     # override default arch for wasi, we only support wasm
