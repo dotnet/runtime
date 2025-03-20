dc88476f102123edebd6b2d2efe5a56146f60094
Andy Ayers <andya@microsoft.com>
2025-03-20 12:52:01 -0700

    JIT: enable inlining methods with EH (#112998)
    Enable inlining of method with EH. Inlinee EH clauses are integrated
    into the root method EH table at the appropriate point (mid-table if
    the call site is in an EH region; at table end otherwise).
    
    Don't try an inline managed methods with unmanaged calling conventions.
    This mainly copes with x86 where unmanaged calling conventions use reversed
    arg order, but I've disabled it in general. No diffs as these methods seem
    to always include EH.
    
    Remove uses of `compXcptnsCount` as this goes stale whenever we clone or
    remove EH, or (eventually) inline methods with EH. Instead, rely on
    `compHndBBtabCount`.
    
    Defer allocating x86's shadow SP var and area until later in jitting,
    so this reflects any changes in EH table structure. In particular we
    often are able to eliminate EH in part or all together and this saves
    a low-offset allocation and so leads to some nice code size savings on
    x86.
    
    Also on x86 remove the runtime-dependent catch class case from the
    computation for keeping this alive, as we now transform such into
    runtime lookups in filters (that may well keep this alive).
    
    Once we can inline methods with EH, IL ranges are no longer a reliable indicator
    of a mutual-protect try regions. Instead, after importation, we can rely on
    mutual-protect trys having the same start and end blocks.
    
    Also update other case where we were using `info.compXcptnsCount` in morph
    to decide if we needed a frame pointer. This lets us simplify the logic around
    frame pointers and EH (though I still think we're making up our minds too early).
    
    Contributes to #108900.

--- src/coreclr/jit/importercalls.cpp
+++ src/coreclr/jit/importercalls.cpp
@@ -7765,6 +7765,25 @@ void Compiler::impMarkInlineCandidateHelper(GenTreeCall*           call,
         return;
     }
 
+    if (inlineCandidateInfo->methInfo.EHcount > 0)
+    {
+        // We cannot inline methods with EH into filter clauses, even if marked as aggressive inline
+        //
+        if (bbInFilterBBRange(compCurBB))
+        {
+            inlineResult->NoteFatal(InlineObservation::CALLSITE_IS_WITHIN_FILTER);
+            return;
+        }
+
+        // Do not inline pinvoke stubs with EH.
+        //
+        if ((methAttr & CORINFO_FLG_PINVOKE) != 0)
+        {
+            inlineResult->NoteFatal(InlineObservation::CALLEE_HAS_EH);
+            return;
+        }
+    }
+
     // The old value should be null OR this call should be a guarded devirtualization candidate.
     assert(call->IsGuardedDevirtualizationCandidate() || (call->GetSingleInlineCandidateInfo() == nullptr));
 
