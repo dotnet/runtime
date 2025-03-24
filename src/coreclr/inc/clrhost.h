aa8002c495064c6fa211a493cdaefc0be5ac6798
Jeremy Koritzinsky <jekoritz@microsoft.com>
2025-03-24 14:04:41 -0700

    Create a separate "preserve last error" holder type to allow removing a no-return scope

--- src/coreclr/inc/clrhost.h
+++ src/coreclr/inc/clrhost.h
@@ -29,16 +29,29 @@ using std::nothrow;
 #define _DEBUG_IMPL 1
 #endif
 
+struct PreserveLastErrorHolder
+{
+    PreserveLastErrorHolder()
+    {
+        m_dwLastError = ::GetLastError();
+    }
+
+    ~PreserveLastErrorHolder()
+    {
+        ::SetLastError(m_dwLastError);
+    }
+private:
+    DWORD m_dwLastError;
+};
+
+
 #define BEGIN_PRESERVE_LAST_ERROR \
     { \
-        DWORD __dwLastError = ::GetLastError(); \
-        DEBUG_ASSURE_NO_RETURN_BEGIN(PRESERVE_LAST_ERROR); \
-            {
+        PreserveLastErrorHolder __preserveLastErrorHolder; \
+        {
 
 #define END_PRESERVE_LAST_ERROR \
-            } \
-        DEBUG_ASSURE_NO_RETURN_END(PRESERVE_LAST_ERROR); \
-        ::SetLastError(__dwLastError); \
+        } \
     }
 
 //
