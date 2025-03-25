f139bcffeae3db13ab79baf20c5ece914c41a484
Kunal Pathak <Kunal.Pathak@microsoft.com>
2025-03-25 09:44:14 -0700

    Do not do sve_mov for scalar variant

--- src/coreclr/jit/hwintrinsiccodegenarm64.cpp
+++ src/coreclr/jit/hwintrinsiccodegenarm64.cpp
@@ -2544,7 +2544,8 @@ void CodeGen::genHWIntrinsic(GenTreeHWIntrinsic* node)
 
                     assert((targetReg == op2Reg) || (targetReg != op1Reg));
                     assert((targetReg == op2Reg) || (targetReg != op3Reg));
-                    GetEmitter()->emitIns_Mov(INS_sve_mov, EA_SCALABLE, targetReg, op2Reg, /* canSkip */ true, opt);
+                    GetEmitter()->emitIns_Mov(INS_mov, emitSize, targetReg, op2Reg,
+                        /* canSkip */ true);
                     GetEmitter()->emitInsSve_R_R_R(ins, emitSize, targetReg, op1Reg, op3Reg, opt,
                                                    INS_SCALABLE_OPTS_NONE);
                     break;
