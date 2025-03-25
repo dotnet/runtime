520ff7c4da66c45e588c4ac65cbcbc18d7708ec8
Katelyn Gadd <kg@luminance.org>
2025-03-25 15:17:27 -0700

    Repair merge damage

--- src/coreclr/vm/interpexec.cpp
+++ src/coreclr/vm/interpexec.cpp
@@ -1,12 +1,11 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#ifdef FEATURE_INTERPRETER
+
 #include "threads.h"
 #include "gcenv.h"
 #include <math.h>
-
-#ifdef FEATURE_INTERPRETER
-
 #include "interpexec.h"
 
 thread_local InterpThreadContext *t_pThreadContext = NULL;
