93b75b3a891af5713830b6007307c14ec075dd5b
Max Charlamb <maxcharlamb@microsoft.com>
2025-03-26 18:25:45 -0400

    add DAC like entrypoint

--- src/native/managed/cdacreader/src/Legacy/ICLRData.cs
+++ src/native/managed/cdacreader/src/Legacy/ICLRData.cs
@@ -17,3 +17,49 @@ internal unsafe partial interface ICLRDataEnumMemoryRegions
     [PreserveSig]
     int EnumMemoryRegions(/*ICLRDataEnumMemoryRegionsCallback*/ void* callback, uint miniDumpFlags, /*CLRDataEnumMemoryFlags*/ int clrFlags);
 }
+
+[GeneratedComInterface]
+[Guid("3e11ccee-d08b-43e5-af01-32717a64da03")]
+internal unsafe partial interface ICLRDataTarget
+{
+    [PreserveSig]
+    int GetMachineType(uint* machineType);
+
+    [PreserveSig]
+    int GetPointerSize(uint* pointerSize);
+
+    [PreserveSig]
+    int GetImageBase([MarshalAs(UnmanagedType.LPWStr)] string imagePath, ulong* baseAddress);
+
+    [PreserveSig]
+    int ReadVirtual(ulong address, byte* buffer, uint bytesRequested, uint* bytesRead);
+
+    [PreserveSig]
+    int WriteVirtual(ulong address, byte* buffer, uint bytesRequested, uint* bytesWritten);
+
+    [PreserveSig]
+    int GetTLSValue(uint threadID, uint index, ulong* value);
+
+    [PreserveSig]
+    int SetTLSValue(uint threadID, uint index, ulong value);
+
+    [PreserveSig]
+    int GetCurrentThreadID(uint* threadID);
+
+    [PreserveSig]
+    int GetThreadContext(uint threadID, uint contextFlags, uint contextSize, byte* context);
+
+    [PreserveSig]
+    int SetThreadContext(uint threadID, uint contextSize, byte* context);
+
+    [PreserveSig]
+    int Request(uint reqCode, uint inBufferSize, byte* inBuffer, uint outBufferSize, byte* outBuffer);
+}
+
+[GeneratedComInterface]
+[Guid("17d5b8c6-34a9-407f-af4f-a930201d4e02")]
+internal unsafe partial interface ICLRContractLocator
+{
+    [PreserveSig]
+    int GetContractDescriptor(ulong* contractAddress);
+}
