03afb2f04a43035bfe736e645c4ee83eaff5daa8
Maksim Golev <mixim33@yandex.ru>
2025-03-30 14:03:01 +0400

    refactor(#93189): Replacing magic values by nameof operator.

--- src/libraries/System.Private.Xml/src/System/Xml/Xsl/Runtime/XsltMethods.cs
+++ src/libraries/System.Private.Xml/src/System/Xml/Xsl/Runtime/XsltMethods.cs
@@ -12,60 +12,97 @@ namespace System.Xml.Xsl.Runtime
     internal static class XsltMethods
     {
         // Formatting error messages
-        public static readonly MethodInfo FormatMessage = typeof(XsltLibrary).GetMethod("FormatMessage")!;
+        public static readonly MethodInfo FormatMessage = typeof(XsltLibrary)
+            .GetMethod(nameof(XsltLibrary.FormatMessage))!;
 
         // Runtime type checks and casts
-        public static readonly MethodInfo EnsureNodeSet = typeof(XsltConvert).GetMethod("EnsureNodeSet", new[] { typeof(IList<XPathItem>) })!;
+        public static readonly MethodInfo EnsureNodeSet = typeof(XsltConvert)
+            .GetMethod(nameof(XsltConvert.EnsureNodeSet), new[] { typeof(IList<XPathItem>) })!;
 
         // Comparisons
-        public static readonly MethodInfo EqualityOperator = typeof(XsltLibrary).GetMethod("EqualityOperator")!;
-        public static readonly MethodInfo RelationalOperator = typeof(XsltLibrary).GetMethod("RelationalOperator")!;
+        public static readonly MethodInfo EqualityOperator = typeof(XsltLibrary)
+            .GetMethod(nameof(XsltLibrary.EqualityOperator))!;
+        public static readonly MethodInfo RelationalOperator = typeof(XsltLibrary)
+            .GetMethod(nameof(XsltLibrary.RelationalOperator))!;
 
         // XPath functions
-        public static readonly MethodInfo StartsWith = typeof(XsltFunctions).GetMethod("StartsWith")!;
-        public static readonly MethodInfo Contains = typeof(XsltFunctions).GetMethod("Contains")!;
-        public static readonly MethodInfo SubstringBefore = typeof(XsltFunctions).GetMethod("SubstringBefore")!;
-        public static readonly MethodInfo SubstringAfter = typeof(XsltFunctions).GetMethod("SubstringAfter")!;
-        public static readonly MethodInfo Substring2 = typeof(XsltFunctions).GetMethod("Substring", new[] { typeof(string), typeof(double) })!;
-        public static readonly MethodInfo Substring3 = typeof(XsltFunctions).GetMethod("Substring", new[] { typeof(string), typeof(double), typeof(double) })!;
-        public static readonly MethodInfo NormalizeSpace = typeof(XsltFunctions).GetMethod("NormalizeSpace")!;
-        public static readonly MethodInfo Translate = typeof(XsltFunctions).GetMethod("Translate")!;
-        public static readonly MethodInfo Lang = typeof(XsltFunctions).GetMethod("Lang")!;
-        public static readonly MethodInfo Floor = typeof(Math).GetMethod("Floor", new[] { typeof(double) })!;
-        public static readonly MethodInfo Ceiling = typeof(Math).GetMethod("Ceiling", new[] { typeof(double) })!;
-        public static readonly MethodInfo Round = typeof(XsltFunctions).GetMethod("Round")!;
+        public static readonly MethodInfo StartsWith = typeof(XsltFunctions)
+            .GetMethod(nameof(XsltFunctions.StartsWith))!;
+        public static readonly MethodInfo Contains = typeof(XsltFunctions)
+            .GetMethod(nameof(XsltFunctions.Contains))!;
+        public static readonly MethodInfo SubstringBefore = typeof(XsltFunctions)
+            .GetMethod(nameof(XsltFunctions.SubstringBefore))!;
+        public static readonly MethodInfo SubstringAfter = typeof(XsltFunctions)
+            .GetMethod(nameof(XsltFunctions.SubstringAfter))!;
+        public static readonly MethodInfo Substring2 = typeof(XsltFunctions)
+            .GetMethod(nameof(XsltFunctions.Substring), new[] { typeof(string), typeof(double) })!;
+        public static readonly MethodInfo Substring3 = typeof(XsltFunctions)
+            .GetMethod(nameof(XsltFunctions.Substring), new[] { typeof(string), typeof(double), typeof(double) })!;
+        public static readonly MethodInfo NormalizeSpace = typeof(XsltFunctions)
+            .GetMethod(nameof(XsltFunctions.NormalizeSpace))!;
+        public static readonly MethodInfo Translate = typeof(XsltFunctions)
+            .GetMethod(nameof(XsltFunctions.Translate))!;
+        public static readonly MethodInfo Lang = typeof(XsltFunctions)
+            .GetMethod(nameof(XsltFunctions.Lang))!;
+        public static readonly MethodInfo Floor = typeof(Math)
+            .GetMethod(nameof(Math.Floor), new[] { typeof(double) })!;
+        public static readonly MethodInfo Ceiling = typeof(Math)
+            .GetMethod(nameof(Math.Ceiling), new[] { typeof(double) })!;
+        public static readonly MethodInfo Round = typeof(XsltFunctions)
+            .GetMethod(nameof(XsltFunctions.Round))!;
 
         // XSLT functions and helper methods (static)
-        public static readonly MethodInfo SystemProperty = typeof(XsltFunctions).GetMethod("SystemProperty")!;
-        public static readonly MethodInfo BaseUri = typeof(XsltFunctions).GetMethod("BaseUri")!;
-        public static readonly MethodInfo OuterXml = typeof(XsltFunctions).GetMethod("OuterXml")!;
-        public static readonly MethodInfo OnCurrentNodeChanged = typeof(XmlQueryRuntime).GetMethod("OnCurrentNodeChanged")!;
+        public static readonly MethodInfo SystemProperty = typeof(XsltFunctions)
+            .GetMethod(nameof(XsltFunctions.SystemProperty))!;
+        public static readonly MethodInfo BaseUri = typeof(XsltFunctions)
+            .GetMethod(nameof(XsltFunctions.BaseUri))!;
+        public static readonly MethodInfo OuterXml = typeof(XsltFunctions)
+            .GetMethod(nameof(XsltFunctions.OuterXml))!;
+        public static readonly MethodInfo OnCurrentNodeChanged = typeof(XmlQueryRuntime)
+            .GetMethod(nameof(XmlQueryRuntime.OnCurrentNodeChanged))!;
 
         // MSXML extension functions
-        public static readonly MethodInfo MSFormatDateTime = typeof(XsltFunctions).GetMethod("MSFormatDateTime")!;
-        public static readonly MethodInfo MSStringCompare = typeof(XsltFunctions).GetMethod("MSStringCompare")!;
-        public static readonly MethodInfo MSUtc = typeof(XsltFunctions).GetMethod("MSUtc")!;
-        public static readonly MethodInfo MSNumber = typeof(XsltFunctions).GetMethod("MSNumber")!;
-        public static readonly MethodInfo MSLocalName = typeof(XsltFunctions).GetMethod("MSLocalName")!;
-        public static readonly MethodInfo MSNamespaceUri = typeof(XsltFunctions).GetMethod("MSNamespaceUri")!;
+        public static readonly MethodInfo MSFormatDateTime = typeof(XsltFunctions)
+            .GetMethod(nameof(XsltFunctions.MSFormatDateTime))!;
+        public static readonly MethodInfo MSStringCompare = typeof(XsltFunctions)
+            .GetMethod(nameof(XsltFunctions.MSStringCompare))!;
+        public static readonly MethodInfo MSUtc = typeof(XsltFunctions)
+            .GetMethod(nameof(XsltFunctions.MSUtc))!;
+        public static readonly MethodInfo MSNumber = typeof(XsltFunctions)
+            .GetMethod(nameof(XsltFunctions.MSNumber))!;
+        public static readonly MethodInfo MSLocalName = typeof(XsltFunctions)
+            .GetMethod(nameof(XsltFunctions.MSLocalName))!;
+        public static readonly MethodInfo MSNamespaceUri = typeof(XsltFunctions)
+            .GetMethod(nameof(XsltFunctions.MSNamespaceUri))!;
 
         // EXSLT functions
-        public static readonly MethodInfo EXslObjectType = typeof(XsltFunctions).GetMethod("EXslObjectType")!;
+        public static readonly MethodInfo EXslObjectType = typeof(XsltFunctions)
+            .GetMethod(nameof(XsltFunctions.EXslObjectType))!;
 
         // XSLT functions and helper methods (non-static)
-        public static readonly MethodInfo CheckScriptNamespace = typeof(XsltLibrary).GetMethod("CheckScriptNamespace")!;
+        public static readonly MethodInfo CheckScriptNamespace = typeof(XsltLibrary)
+            .GetMethod(nameof(XsltLibrary.CheckScriptNamespace))!;
         public static readonly MethodInfo FunctionAvailable = GetFunctionAvailableMethod();
         [UnconditionalSuppressMessage("ReflectionAnalysis", "IL2026:RequiresUnreferencedCode",
             Justification = "Suppressing warning about not having the RequiresUnreferencedCode attribute since this code path " +
             "will only be emitting IL that will later be called by Transform() method which is already annotated as RequiresUnreferencedCode")]
-        private static MethodInfo GetFunctionAvailableMethod() => typeof(XsltLibrary).GetMethod("FunctionAvailable")!;
-        public static readonly MethodInfo ElementAvailable = typeof(XsltLibrary).GetMethod("ElementAvailable")!;
-        public static readonly MethodInfo RegisterDecimalFormat = typeof(XsltLibrary).GetMethod("RegisterDecimalFormat")!;
-        public static readonly MethodInfo RegisterDecimalFormatter = typeof(XsltLibrary).GetMethod("RegisterDecimalFormatter")!;
-        public static readonly MethodInfo FormatNumberStatic = typeof(XsltLibrary).GetMethod("FormatNumberStatic")!;
-        public static readonly MethodInfo FormatNumberDynamic = typeof(XsltLibrary).GetMethod("FormatNumberDynamic")!;
-        public static readonly MethodInfo IsSameNodeSort = typeof(XsltLibrary).GetMethod("IsSameNodeSort")!;
-        public static readonly MethodInfo LangToLcid = typeof(XsltLibrary).GetMethod("LangToLcid")!;
-        public static readonly MethodInfo NumberFormat = typeof(XsltLibrary).GetMethod("NumberFormat")!;
+        private static MethodInfo GetFunctionAvailableMethod() => typeof(XsltLibrary)
+            .GetMethod(nameof(XsltLibrary.FunctionAvailable))!;
+        public static readonly MethodInfo ElementAvailable = typeof(XsltLibrary)
+            .GetMethod(nameof(XsltLibrary.ElementAvailable))!;
+        public static readonly MethodInfo RegisterDecimalFormat = typeof(XsltLibrary)
+            .GetMethod(nameof(XsltLibrary.RegisterDecimalFormat))!;
+        public static readonly MethodInfo RegisterDecimalFormatter = typeof(XsltLibrary)
+            .GetMethod(nameof(XsltLibrary.RegisterDecimalFormatter))!;
+        public static readonly MethodInfo FormatNumberStatic = typeof(XsltLibrary)
+            .GetMethod(nameof(XsltLibrary.FormatNumberStatic))!;
+        public static readonly MethodInfo FormatNumberDynamic = typeof(XsltLibrary)
+            .GetMethod(nameof(XsltLibrary.FormatNumberDynamic))!;
+        public static readonly MethodInfo IsSameNodeSort = typeof(XsltLibrary)
+            .GetMethod(nameof(XsltLibrary.IsSameNodeSort))!;
+        public static readonly MethodInfo LangToLcid = typeof(XsltLibrary)
+            .GetMethod(nameof(XsltLibrary.LangToLcid))!;
+        public static readonly MethodInfo NumberFormat = typeof(XsltLibrary)
+            .GetMethod(nameof(XsltLibrary.NumberFormat))!;
     }
 }
