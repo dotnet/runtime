ddea2aa721e97bc6548f0b0f7338eb305ff2f1d2
Daniel Draghici <daniel.draghici@stellenanzeigen.de>
2025-03-31 18:23:45 +0200

    Removed unused field in JsonProperty (#113643)
    Removed the unused field _name in JsonProperty which was never initialized anyway.

--- src/libraries/System.Text.Json/src/System/Text/Json/Document/JsonProperty.cs
+++ src/libraries/System.Text.Json/src/System/Text/Json/Document/JsonProperty.cs
@@ -16,7 +16,6 @@ namespace System.Text.Json
         ///   The value of this property.
         /// </summary>
         public JsonElement Value { get; }
-        private string? _name { get; }
 
         internal JsonProperty(JsonElement value)
         {
@@ -25,8 +24,9 @@ namespace System.Text.Json
 
         /// <summary>
         ///   The name of this property.
+        ///   This allocates a new string instance for each call.
         /// </summary>
-        public string Name => _name ?? Value.GetPropertyName();
+        public string Name => Value.GetPropertyName();
 
         /// <summary>
         ///   Compares <paramref name="text" /> to the name of this property.
@@ -119,15 +119,7 @@ namespace System.Text.Json
                 ThrowHelper.ThrowArgumentNullException(nameof(writer));
             }
 
-            if (_name is null)
-            {
-                Value.WritePropertyNameTo(writer);
-            }
-            else
-            {
-                writer.WritePropertyName(_name);
-            }
-
+            Value.WritePropertyNameTo(writer);
             Value.WriteTo(writer);
         }
 
