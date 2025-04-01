6b101b9b990f70ef01f87e143d233d66850477d6
Adeel Mujahid <3840695+am11@users.noreply.github.com>
2025-04-01 18:53:43 +0300

    Revert riscv (we need to do it differently for 32 bit)

--- src/native/external/llvm-libunwind/include/__libunwind_config.h
+++ src/native/external/llvm-libunwind/include/__libunwind_config.h
@@ -156,9 +156,9 @@
 #  endif
 #  define _LIBUNWIND_CONTEXT_SIZE (32 * (__riscv_xlen + RISCV_FLEN) / 64) + 32
 #  if __riscv_xlen == 32
-#   define _LIBUNWIND_CURSOR_SIZE (_LIBUNWIND_CONTEXT_SIZE + 7) + 32
+#   define _LIBUNWIND_CURSOR_SIZE (_LIBUNWIND_CONTEXT_SIZE + 7)
 #  elif __riscv_xlen == 64
-#   define _LIBUNWIND_CURSOR_SIZE (_LIBUNWIND_CONTEXT_SIZE + 12) + 32
+#   define _LIBUNWIND_CURSOR_SIZE (_LIBUNWIND_CONTEXT_SIZE + 12)
 #  else
 #   error "Unsupported RISC-V ABI"
 #  endif
