9e8a9e94ef298098d2a53a5335409dbb606663c6
anthonycanino <anthony.canino@intel.com>
2025-04-03 17:24:58 -0400

    [JIT] Enable conditional chaining for Intel APX (#111072)
    * Enable conditional compare chaining for AMD64.
    
    * Reduce duplication from `optSwitchDetectLikely`.
    
    * Update src/coreclr/jit/lsrabuild.cpp
    
    Co-authored-by: Bruce Forstall <brucefo@microsoft.com>
    
    * Update src/coreclr/jit/lowerxarch.cpp
    
    Co-authored-by: Bruce Forstall <brucefo@microsoft.com>
    
    * Update src/coreclr/jit/lowerxarch.cpp
    
    Co-authored-by: Bruce Forstall <brucefo@microsoft.com>
    
    * Widen the potential candidates for ccmp folding.
    
    Also lifts GenConditionDesc into CodeGenInterface to better check which
    flag lowerings will produce multiple instructions.
    
    * Refactor some common code into lower.cpp.
    
    Some code will conflict with latest changes. I've squashed so we
    can discuss how to merge in properly.
    
    * Refactored common code out.
    
    * Review edits.
    
    * Fix build errors.
    
    * Formatting.
    
    ---------
    
    Co-authored-by: Bruce Forstall <brucefo@microsoft.com>

--- src/coreclr/jit/lowerxarch.cpp
+++ src/coreclr/jit/lowerxarch.cpp
@@ -320,8 +320,64 @@ GenTree* Lowering::LowerBinaryArithmetic(GenTreeOp* binOp)
 
     ContainCheckBinary(binOp);
 
+#ifdef TARGET_AMD64
+    if (JitConfig.EnableApxConditionalChaining())
+    {
+        if (binOp->OperIs(GT_AND, GT_OR))
+        {
+            GenTree* next;
+            if (TryLowerAndOrToCCMP(binOp, &next))
+            {
+                return next;
+            }
+        }
+    }
+#endif // TARGET_AMD64
+
     return binOp->gtNext;
 }
+
+#ifdef TARGET_AMD64
+//------------------------------------------------------------------------
+// TruthifyingFlags: Get a flags immediate that will make a specified condition true.
+//
+// Arguments:
+//    condition - the condition.
+//
+// Returns:
+//    A flags immediate that, if those flags were set, would cause the specified condition to be true.
+//    (NOTE: This just has to make the condition be true, i.e., if the condition calls for (SF ^ OF), then
+//    returning one will suffice
+insCflags Lowering::TruthifyingFlags(GenCondition condition)
+{
+    switch (condition.GetCode())
+    {
+        case GenCondition::EQ:
+            return INS_FLAGS_ZF;
+        case GenCondition::NE:
+            return INS_FLAGS_NONE;
+        case GenCondition::SGE: // !(SF ^ OF)
+            return INS_FLAGS_NONE;
+        case GenCondition::SGT: // !(SF ^ OF) && !ZF
+            return INS_FLAGS_NONE;
+        case GenCondition::SLE: // !(SF ^ OF) || ZF
+            return INS_FLAGS_ZF;
+        case GenCondition::SLT: // (SF ^ OF)
+            return INS_FLAGS_SF;
+        case GenCondition::UGE: // !CF
+            return INS_FLAGS_NONE;
+        case GenCondition::UGT: // !CF && !ZF
+            return INS_FLAGS_NONE;
+        case GenCondition::ULE: // CF || ZF
+            return INS_FLAGS_ZF;
+        case GenCondition::ULT: // CF
+            return INS_FLAGS_CF;
+        default:
+            NO_WAY("unexpected condition type");
+            return INS_FLAGS_NONE;
+    }
+}
+#endif // TARGET_AMD64
 
 //------------------------------------------------------------------------
 // LowerBlockStore: Lower a block store node
