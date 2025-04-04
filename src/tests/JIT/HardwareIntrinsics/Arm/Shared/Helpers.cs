4324101f9e55dc28d0eb097e8c8153fb4a4387af
Kunal Pathak <Kunal.Pathak@microsoft.com>
2025-04-04 10:13:43 -0700

    Remove unrequired cast

--- src/tests/JIT/HardwareIntrinsics/Arm/Shared/Helpers.cs
+++ src/tests/JIT/HardwareIntrinsics/Arm/Shared/Helpers.cs
@@ -7704,52 +7704,52 @@ namespace JIT.HardwareIntrinsics.Arm
 
         public static byte getMaskByte()
         {
-            return (byte)(TestLibrary.Generator.GetByte()%(byte)2);
+            return (byte)(TestLibrary.Generator.GetByte() % 2);
         }
 
         public static sbyte getMaskSByte()
         {
-            return (sbyte)(TestLibrary.Generator.GetSByte()%(sbyte)2);
+            return (sbyte)(TestLibrary.Generator.GetSByte() % 2);
         }
 
         public static short getMaskInt16()
         {
-            return (short)(TestLibrary.Generator.GetInt16()%(short)2);
+            return (short)(TestLibrary.Generator.GetInt16() % 2);
         }
 
         public static ushort getMaskUInt16()
         {
-            return (ushort)(TestLibrary.Generator.GetUInt16()%(ushort)2);
+            return (ushort)(TestLibrary.Generator.GetUInt16() % 2);
         }
 
         public static int getMaskInt32()
         {
-            return (int)(TestLibrary.Generator.GetInt32()%(int)2);
+            return (int)(TestLibrary.Generator.GetInt32() % 2);
         }
 
         public static uint getMaskUInt32()
         {
-            return (uint)(TestLibrary.Generator.GetUInt32()%(uint)2);
+            return (uint)(TestLibrary.Generator.GetUInt32() % 2);
         }
 
         public static long getMaskInt64()
         {
-            return (long)(TestLibrary.Generator.GetInt64()%(long)2);
+            return (long)(TestLibrary.Generator.GetInt64() % 2);
         }
 
         public static ulong getMaskUInt64()
         {
-            return (ulong)(TestLibrary.Generator.GetUInt64()%(ulong)2);
+            return (ulong)(TestLibrary.Generator.GetUInt64() % 2);
         }
 
         public static float getMaskSingle()
         {
-            return (float)(TestLibrary.Generator.GetUInt32()%(int)2);
+            return (float)(TestLibrary.Generator.GetUInt32() % 2);
         }
 
         public static double getMaskDouble()
         {
-            return (double)(TestLibrary.Generator.GetUInt64()%(long)2);
+            return (double)(TestLibrary.Generator.GetUInt64() % 2);
         }
 
         public static int MaskNumberOfElementsVector(int elems, SveMaskPattern pattern)
