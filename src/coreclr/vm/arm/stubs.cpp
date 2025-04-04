6bb549287658ac5cf5cd372024f310b22776d954
vsadov <8218165+VSadov@users.noreply.github.com>
2025-04-03 18:18:42 -0700

    reference continuation return from volatileCurrContextPointers on ARM[64]

--- src/coreclr/vm/arm/stubs.cpp
+++ src/coreclr/vm/arm/stubs.cpp
@@ -1595,7 +1595,10 @@ void HijackFrame::UpdateRegDisplay_Impl(const PREGDISPLAY pRD, bool updateFloats
      pRD->pCurrentContext->Sp = PTR_TO_TADDR(m_Args) + sizeof(struct HijackArgs);
 
      pRD->pCurrentContext->R0 = m_Args->R0;
+     pRD->pCurrentContext->R2 = m_Args->R2;
+
      pRD->volatileCurrContextPointers.R0 = &m_Args->R0;
+     pRD->volatileCurrContextPointers.R2 = &m_Args->R2;
 
      pRD->pCurrentContext->R4 = m_Args->R4;
      pRD->pCurrentContext->R5 = m_Args->R5;
