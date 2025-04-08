453abdf908b73f97ea1b4aa5aff0ef1441ec3f0e
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2025-04-08 10:52:32 +0200

    Fix win-x86

--- src/coreclr/nativeaot/System.Private.CoreLib/src/System/Runtime/RuntimeImports.cs
+++ src/coreclr/nativeaot/System.Private.CoreLib/src/System/Runtime/RuntimeImports.cs
@@ -501,7 +501,13 @@ namespace System.Runtime
         internal static extern unsafe void RhUnregisterRefCountedHandleCallback(IntPtr pCalloutMethod, MethodTable* pTypeFilter);
 
         [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        [RuntimeImport(RuntimeLibrary, "RhIUnknown_AddRef")]
+        [RuntimeImport(RuntimeLibrary,
+#if TARGET_WINDOWS && TARGET_X86
+            "_RhIUnknown_AddRef@4"
+#else
+            "RhIUnknown_AddRef"
+#endif
+        )]
         internal static extern uint RhIUnknown_AddRef(nint pThis);
 
 #if FEATURE_OBJCMARSHAL
