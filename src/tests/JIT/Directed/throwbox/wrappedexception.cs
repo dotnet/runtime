5713469ef53c53f72c4916094366136bfa9ffc54
Andy Ayers <andya@microsoft.com>
2025-04-09 09:09:18 -0700

    add test case

---
+++ src/tests/JIT/Directed/throwbox/wrappedexception.cs
@@ -0,0 +1,15 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using JitTest;
+
+class WrappedException
+{
+    // C# wrapper method calling IL method
+    // that throws a non-exception based object
+    //
+    public static void Problem()
+    {
+        Test.Main();
+    }
+}
