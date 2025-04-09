f08ae8bfdd22eb428e18b51719203ee03bdd1d3a
Carlos Sánchez López <1175054+carlossanlop@users.noreply.github.com>
2025-04-08 19:48:49 -0700

    Implement ZipArchiveEntry async.

--- src/libraries/System.IO.Compression/ref/System.IO.Compression.cs
+++ src/libraries/System.IO.Compression/ref/System.IO.Compression.cs
@@ -126,6 +126,7 @@ namespace System.IO.Compression
         public string Name { get { throw null; } }
         public void Delete() { }
         public System.IO.Stream Open() { throw null; }
+        public System.Threading.Tasks.Task<System.IO.Stream> OpenAsync(System.Threading.CancellationToken cancellationToken = default) { throw null; }
         public override string ToString() { throw null; }
     }
     public enum ZipArchiveMode
