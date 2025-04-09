0dc278b25944e6f2581f3603a595979e14fceb3c
Marc Gravell <marc.gravell@gmail.com>
2025-04-09 10:41:49 +0100

    make ref API happy

--- src/libraries/Microsoft.Extensions.Caching.Abstractions/ref/Microsoft.Extensions.Caching.Abstractions.net10.cs
+++ src/libraries/Microsoft.Extensions.Caching.Abstractions/ref/Microsoft.Extensions.Caching.Abstractions.net10.cs
@@ -11,27 +11,27 @@ namespace Microsoft.Extensions.Caching.Distributed
         public System.Threading.Tasks.ValueTask<T> GetOrCreateAsync<T>(
             System.ReadOnlySpan<char> key,
             System.Func<System.Threading.CancellationToken, System.Threading.Tasks.ValueTask<T>> factory,
-            HybridCacheEntryOptions? options = null,
+            Microsoft.Extensions.Caching.Hybrid.HybridCacheEntryOptions? options = null,
             System.Collections.Generic.IEnumerable<string>? tags = null,
             System.Threading.CancellationToken cancellationToken = default) => throw null;
         public virtual System.Threading.Tasks.ValueTask<T> GetOrCreateAsync<TState, T>(
             System.ReadOnlySpan<char> key,
             TState state,
             System.Func<TState, System.Threading.CancellationToken, System.Threading.Tasks.ValueTask<T>> factory,
-            HybridCacheEntryOptions? options = null,
+            Microsoft.Extensions.Caching.Hybrid.HybridCacheEntryOptions? options = null,
             System.Collections.Generic.IEnumerable<string>? tags = null,
             System.Threading.CancellationToken cancellationToken = default) => throw null;
         public System.Threading.Tasks.ValueTask<T> GetOrCreateAsync<T>(
             ref System.Runtime.CompilerServices.DefaultInterpolatedStringHandler key,
             System.Func<System.Threading.CancellationToken, System.Threading.Tasks.ValueTask<T>> factory,
-            HybridCacheEntryOptions? options = null,
+            Microsoft.Extensions.Caching.Hybrid.HybridCacheEntryOptions? options = null,
             System.Collections.Generic.IEnumerable<string>? tags = null,
             System.Threading.CancellationToken cancellationToken = default) => throw null;
         public System.Threading.Tasks.ValueTask<T> GetOrCreateAsync<TState, T>(
             ref System.Runtime.CompilerServices.DefaultInterpolatedStringHandler key,
             TState state,
             System.Func<TState, System.Threading.CancellationToken, System.Threading.Tasks.ValueTask<T>> factory,
-            HybridCacheEntryOptions? options = null,
+            Microsoft.Extensions.Caching.Hybrid.HybridCacheEntryOptions? options = null,
             System.Collections.Generic.IEnumerable<string>? tags = null,
             System.Threading.CancellationToken cancellationToken = default) => throw null;
     }
