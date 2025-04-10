e9cb11da3bb3fe7070ed34610dc6b4b29b851266
Jeremy Barton <jbarton@microsoft.com>
2025-04-09 21:35:36 -0700

    Make CertificateRequest et al work with ML-DSA
    * Add ctors to CertificateRequest
    * Enlighten CertificateRequest that future signing algorithms might not require a HashAlgorithmName
    * Add support to CertificateRequestListBuilder
    * Add cert.GetMLDsaPublicKey/GetMLDsaPrivateKey/CopyWithPrivateKey to power the above.

--- src/libraries/Common/src/System/Security/Cryptography/MLDsaImplementation.cs
+++ src/libraries/Common/src/System/Security/Cryptography/MLDsaImplementation.cs
@@ -17,10 +17,32 @@ namespace System.Security.Cryptography
 
         internal static partial bool SupportsAny();
 
-        internal static partial MLDsa GenerateKeyImpl(MLDsaAlgorithm algorithm);
-        internal static partial MLDsa ImportPublicKey(MLDsaAlgorithm algorithm, ReadOnlySpan<byte> source);
-        internal static partial MLDsa ImportPkcs8PrivateKeyValue(MLDsaAlgorithm algorithm, ReadOnlySpan<byte> source);
-        internal static partial MLDsa ImportSecretKey(MLDsaAlgorithm algorithm, ReadOnlySpan<byte> source);
-        internal static partial MLDsa ImportSeed(MLDsaAlgorithm algorithm, ReadOnlySpan<byte> source);
+        internal static partial MLDsaImplementation GenerateKeyImpl(MLDsaAlgorithm algorithm);
+        internal static partial MLDsaImplementation ImportPublicKey(MLDsaAlgorithm algorithm, ReadOnlySpan<byte> source);
+        internal static partial MLDsaImplementation ImportPkcs8PrivateKeyValue(MLDsaAlgorithm algorithm, ReadOnlySpan<byte> source);
+        internal static partial MLDsaImplementation ImportSecretKey(MLDsaAlgorithm algorithm, ReadOnlySpan<byte> source);
+        internal static partial MLDsaImplementation ImportSeed(MLDsaAlgorithm algorithm, ReadOnlySpan<byte> source);
+
+        internal static MLDsaImplementation DuplicatePrivateKey(MLDsa key)
+        {
+            MLDsaAlgorithm alg = key.Algorithm;
+            byte[] rented = CryptoPool.Rent(alg.SecretKeySizeInBytes);
+            int written = 0;
+
+            try
+            {
+                written = key.ExportMLDsaPrivateSeed(rented);
+                return ImportSeed(alg, new ReadOnlySpan<byte>(rented, 0, written));
+            }
+            catch (CryptographicException)
+            {
+                written = key.ExportMLDsaSecretKey(rented);
+                return ImportSecretKey(alg, new ReadOnlySpan<byte>(rented, 0, written));
+            }
+            finally
+            {
+                CryptoPool.Return(rented, written);
+            }
+        }
     }
 }
