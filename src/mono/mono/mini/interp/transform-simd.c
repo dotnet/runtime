1a3725d7d1ca5ae4b495bf71f366d75c78de501f
Larry Ewing <lewing@microsoft.com>
2025-04-10 11:13:40 -0500

    Fix again

--- src/mono/mono/mini/interp/transform-simd.c
+++ src/mono/mono/mini/interp/transform-simd.c
@@ -1080,20 +1080,6 @@ emit_sri_packedsimd (TransformData *td, MonoMethod *cmethod, MonoMethodSignature
 	MonoClass *vector_klass;
 
 	bool is_packedsimd = strcmp (m_class_get_name (cmethod->klass), "PackedSimd") == 0;
-	// NOTE: Linker substitutions (used in AOT) will prevent this from running.
-	if ((id == SN_get_IsSupported) || (id == SN_get_IsHardwareAccelerated)) {
-		if (!is_packedsimd) {
-			// We don't want to emit the IsSupported or IsHardwareAccelerated methods for Vector* here
-			return FALSE;
-		}
-#if HOST_BROWSER
-		interp_add_ins (td, MINT_LDC_I4_1);
-#else
-		interp_add_ins (td, MINT_LDC_I4_0);
-#endif
-		goto opcode_added;
-	}
-
 	if (is_packedsimd) {
 		if (csignature->ret->type == MONO_TYPE_VOID && csignature->param_count > 1 && mono_type_is_pointer (csignature->params [0])) {
 			// The Store* methods have a more complicated signature
@@ -1113,6 +1099,21 @@ emit_sri_packedsimd (TransformData *td, MonoMethod *cmethod, MonoMethodSignature
 
 	MonoTypeEnum atype;
 	int vector_size = -1, arg_size, scalar_arg;
+
+	// NOTE: Linker substitutions (used in AOT) will prevent this from running.
+	if ((id == SN_get_IsSupported) || (id == SN_get_IsHardwareAccelerated)) {
+		if (!is_packedsimd) {
+			// We don't want to emit the IsSupported or IsHardwareAccelerated methods for Vector* here
+			return FALSE;
+		}
+#if HOST_BROWSER
+		interp_add_ins (td, MINT_LDC_I4_1);
+#else
+		interp_add_ins (td, MINT_LDC_I4_0);
+#endif
+		goto opcode_added;
+	}
+
 	if (!get_common_simd_info (vector_klass, csignature, &atype, &vector_size, &arg_size, &scalar_arg))
 		return FALSE;
 
