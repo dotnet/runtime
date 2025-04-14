0a2d6016a90e61595464dd092ffd34c1789bd817
Alexey Zakharov <alexeyzakharov@unity3d.com>
2025-04-14 10:59:14 +0200

    Added TypeDescriptor cache unloadability test

---
+++ src/libraries/System.ComponentModel.TypeConverter/tests/UnloadableTestTypes/UnloadableTestTypes.cs
@@ -0,0 +1,17 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+
+namespace UnloadableTestTypes
+{
+    [SimpleType]
+    public class SimpleType
+    {
+        public string P1 { get; set; }
+        public int P2 { get; set; }
+    }
+
+    [AttributeUsage(AttributeTargets.All)]
+    public sealed class SimpleTypeAttribute : Attribute { }
+}
