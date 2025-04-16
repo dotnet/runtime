248d6f422e777219e11762a330405234cbcee156
Clinton Ingram <clinton.ingram@outlook.com>
2025-04-16 15:55:00 -0700

    JIT: Fix containment check for decomposed long halves in CreateScalar (#114654)
    * fix containment check for decomposed long halves in CreateScalar
    
    * add IsSupported check to test

--- src/coreclr/jit/lowerxarch.cpp
+++ src/coreclr/jit/lowerxarch.cpp
@@ -9893,7 +9893,8 @@ void Lowering::ContainCheckHWIntrinsic(GenTreeHWIntrinsic* node)
 
                             for (GenTree* longOp : op1->Operands())
                             {
-                                if (IsContainableMemoryOp(longOp) && IsSafeToContainMem(node, longOp))
+                                if (!varTypeIsSmall(longOp) && IsContainableMemoryOp(longOp) &&
+                                    IsSafeToContainMem(node, longOp))
                                 {
                                     MakeSrcContained(node, longOp);
                                 }
