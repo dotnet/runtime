3579fde0210c190650b9c864550fedf0a32fe740
Adeel Mujahid <3840695+am11@users.noreply.github.com>
2025-04-17 14:32:47 +0300

    Strip trailing slash from source dir for cmake4

--- src/native/corehost/build.cmd
+++ src/native/corehost/build.cmd
@@ -5,7 +5,8 @@ setlocal
 :: Initialize the args that will be passed to cmake
 set "__sourceDir=%~dp0"
 :: remove trailing slash
-if %__sourceDir:~-1%==\ set "__ProjectDir=%__sourceDir:~0,-1%"
+if "%__sourceDir:~-1%"=="\" set "__sourceDir=%__sourceDir:~0,-1%"
+set "__ProjectDir=%__sourceDir%"
 
 set "__RepoRootDir=%__sourceDir%\..\..\.."
 :: normalize
