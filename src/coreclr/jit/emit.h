3bee83b4fef7f73407607f3c3208a8c3a5976483
Filip Navara <filip.navara@gmail.com>
2025-04-20 12:11:40 +0200

    Prevent peephole optimization in funclet prolog/epilog; it results in unwinding information not matching the code and error when unwinding inside the funclet epilog

--- src/coreclr/jit/emit.h
+++ src/coreclr/jit/emit.h
@@ -2890,6 +2890,7 @@ private:
         else
         {
             return (curInsIG->igFlags & IGF_EXTEND) &&
+                   ((curInsIG->igFlags & (IGF_FUNCLET_PROLOG | IGF_FUNCLET_EPILOG)) == 0) &&
                    ((prevInsIG->igFlags & IGF_NOGCINTERRUPT) == (curInsIG->igFlags & IGF_NOGCINTERRUPT));
         }
     }
