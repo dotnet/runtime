e72e0e3c6ea42d83e54d95c7a926a267a29636bb
Milos Kotlar <kotlarmilos@gmail.com>
2025-04-22 19:54:27 +0200

    Add preprocessor directive FEATURE_INTERPRETER

--- src/coreclr/vm/interpframeallocator.cpp
+++ src/coreclr/vm/interpframeallocator.cpp
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#ifdef FEATURE_INTERPRETER
+
 #include "interpexec.h"
 #include "interpframeallocator.h"
 
@@ -109,3 +111,5 @@ void FrameDataAllocator::PopInfo(InterpreterFrame *pFrame)
         pCurrent->pos = pInfo->pos;
     }
 }
+
+#endif // FEATURE_INTERPRETER
