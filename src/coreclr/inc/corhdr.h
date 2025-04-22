70c11274136f6ac0959d0619d4f112346515d224
Filip Navara <filip.navara@gmail.com>
2025-04-22 22:55:27 +0200

    Delete COR_ILEXCEPTION_CLAUSE_OFFSETLEN and COR_ILEXCEPTION_CLAUSE_DEPRECATED

--- src/coreclr/inc/corhdr.h
+++ src/coreclr/inc/corhdr.h
@@ -1136,8 +1136,6 @@ typedef struct IMAGE_COR_ILMETHOD_SECT_FAT
 typedef enum CorExceptionFlag                       // definitions for the Flags field below (for both big and small)
 {
     COR_ILEXCEPTION_CLAUSE_NONE,                    // This is a typed handler
-    COR_ILEXCEPTION_CLAUSE_OFFSETLEN = 0x0000,      // Deprecated
-    COR_ILEXCEPTION_CLAUSE_DEPRECATED = 0x0000,     // Deprecated
     COR_ILEXCEPTION_CLAUSE_FILTER  = 0x0001,        // If this bit is on, then this EH entry is for a filter
     COR_ILEXCEPTION_CLAUSE_FINALLY = 0x0002,        // This clause is a finally clause
     COR_ILEXCEPTION_CLAUSE_FAULT = 0x0004,          // Fault clause (finally that is called on exception only)
