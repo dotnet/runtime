b91da9052a7ed481c993c32bb45000df708ba72e
Milos Kotlar <kotlarmilos@gmail.com>
2025-04-22 20:49:14 +0200

    Disable ConfigSwitchIsHonored test

--- src/libraries/System.Runtime/tests/System.IO.FileSystem.Tests/DisabledFileLockingTests/DisabledFileLockingSwitchTests.cs
+++ src/libraries/System.Runtime/tests/System.IO.FileSystem.Tests/DisabledFileLockingTests/DisabledFileLockingSwitchTests.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
 using Xunit;
 
 namespace System.IO.Tests
@@ -8,9 +9,10 @@ namespace System.IO.Tests
     public class DisabledFileLockingSwitchTests
     {
         [Fact]
+        [ActiveIssue("TODO", typeof(PlatformDetection), nameof(PlatformDetection.IsSingleFile))]
         public static void ConfigSwitchIsHonored()
         {
-            Assert.Equal(OperatingSystem.IsWindows() || OperatingSystem.IsAndroid(), PlatformDetection.IsFileLockingEnabled);
+            Assert.Equal(OperatingSystem.IsWindows(), PlatformDetection.IsFileLockingEnabled);
         }
     }
 }
