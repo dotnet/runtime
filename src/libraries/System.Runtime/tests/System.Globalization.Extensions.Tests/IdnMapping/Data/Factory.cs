0748f1589adc620a6705afdfaa70b77eb6a35763
Tarek Mahmoud Sayed <10833894+tarekgh@users.noreply.github.com>
2025-04-24 17:25:57 -0700

    Minor fix

--- src/libraries/System.Runtime/tests/System.Globalization.Extensions.Tests/IdnMapping/Data/Factory.cs
+++ src/libraries/System.Runtime/tests/System.Globalization.Extensions.Tests/IdnMapping/Data/Factory.cs
@@ -67,7 +67,7 @@ namespace System.Globalization.Tests
         {
             if (PlatformDetection.ICUVersion >= new Version(76, 0))
                 return new Unicode_16_0_IdnaTest(line, lineCount);
-            else if (PlatformDetection.ICUVersion >= new Version(74, 0))
+            else if (PlatformDetection.ICUVersion >= new Version(72, 0))
                 return new Unicode_15_1_IdnaTest(line, lineCount);
             else if (PlatformDetection.ICUVersion >= new Version(66, 0) || PlatformDetection.IsHybridGlobalizationOnApplePlatform)
                 return new Unicode_13_0_IdnaTest(line, lineCount);
