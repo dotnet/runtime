6a51d2ec367912e0988aa1ccd9d94bdac4a5466b
Tarek Mahmoud Sayed <10833894+tarekgh@users.noreply.github.com>
2025-04-24 17:37:52 -0700

    Minor fix

--- src/libraries/System.Globalization.Extensions/tests/IdnMapping/Data/Factory.cs
+++ src/libraries/System.Globalization.Extensions/tests/IdnMapping/Data/Factory.cs
@@ -65,7 +65,11 @@ namespace System.Globalization.Tests
 
         private static IConformanceIdnaTest GetConformanceIdnaTest(string line, int lineCount)
         {
-            if (PlatformDetection.ICUVersion >= new Version(66, 0))
+            if (PlatformDetection.ICUVersion >= new Version(76, 0))
+                return new Unicode_16_0_IdnaTest(line, lineCount);
+            else if (PlatformDetection.ICUVersion >= new Version(72, 0))
+                return new Unicode_15_1_IdnaTest(line, lineCount);
+            else if (PlatformDetection.ICUVersion >= new Version(66, 0))
                 return new Unicode_13_0_IdnaTest(line, lineCount);
             else if (PlatformDetection.IsWindows7)
                 return new Unicode_Win7_IdnaTest(line, lineCount);
