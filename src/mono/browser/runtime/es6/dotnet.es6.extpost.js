109758183d2ba7508291d25e64209cde3678aa81
Mike Hudson <mikehudsonuk@icloud.com>
2025-04-30 12:35:53 +0100

    Revert "Declare _scriptDir"
    This reverts commit d032b31aaf88b633662acec8fcc07249fbbad3c6.

--- src/mono/browser/runtime/es6/dotnet.es6.extpost.js
+++ src/mono/browser/runtime/es6/dotnet.es6.extpost.js
@@ -1,2 +1,2 @@
 
-var _scriptDir = _scriptDir || ''; var fetch = fetch || undefined; var require = require || undefined; var __dirname = __dirname || ''; var _nativeModuleLoaded = false;
+var fetch = fetch || undefined; var require = require || undefined; var __dirname = __dirname || ''; var _nativeModuleLoaded = false;
