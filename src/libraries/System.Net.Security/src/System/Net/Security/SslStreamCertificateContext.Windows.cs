4509f552cef3cef4905773f7108a07ab237c428b
Radek Zikmund <r.zikmund.rz@gmail.com>
2025-04-30 14:06:09 +0200

    Swallow CryptographicExceptions when attempting to add certificates to store during SslStreamCertificateContext.Create

--- src/libraries/System.Net.Security/src/System/Net/Security/SslStreamCertificateContext.Windows.cs
+++ src/libraries/System.Net.Security/src/System/Net/Security/SslStreamCertificateContext.Windows.cs
@@ -78,7 +78,7 @@ namespace System.Net.Security
                                 // Add everything except the root
                                 for (int index = count; index < intermediates.Count - 1; index++)
                                 {
-                                    store.Add(intermediates[index]);
+                                    TryAddToStore(store, intermediates[index]);
                                 }
 
                                 osCanBuildChain = chain.Build(target);
@@ -94,8 +94,8 @@ namespace System.Net.Security
                                 if (!osCanBuildChain)
                                 {
                                     // Add also root to Intermediate CA store so OS can complete building chain.
-                                    // (This does not make it trusted.
-                                    store.Add(intermediates[intermediates.Count - 1]);
+                                    // (This does not make it trusted.)
+                                    TryAddToStore(store, intermediates[intermediates.Count - 1]);
                                 }
                             }
                         }
@@ -106,6 +106,22 @@ namespace System.Net.Security
             IntermediateCertificates = intermediates;
             TargetCertificate = target;
             Trust = trust;
+
+            static void TryAddToStore(X509Store store, X509Certificate2 certificate)
+            {
+                try
+                {
+                    store.Add(certificate);
+                }
+                catch (CryptographicException ex)
+                {
+                    // Continue even if we can't add certificates due to permission issues
+                    if (NetEventSource.Log.IsEnabled())
+                    {
+                        NetEventSource.Error(null, $"Failed to add certificate to store: {ex.Message}");
+                    }
+                }
+            }
         }
     }
 }
