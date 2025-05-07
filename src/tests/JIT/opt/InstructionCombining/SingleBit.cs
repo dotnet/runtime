90aa3eea9f0daa94f2f9971e22dc3e3d802e1b79
Tomek Sowiński <tomeksowi@gmail.com>
2025-05-07 10:49:42 +0200

    Fix test

--- src/tests/JIT/opt/InstructionCombining/SingleBit.cs
+++ src/tests/JIT/opt/InstructionCombining/SingleBit.cs
@@ -37,13 +37,14 @@ public static class SingleBit
     static bool ExtractShift(int a, int b) => ((a & (1 << b)) >> b) == 1;
 
     [MethodImpl(MethodImplOptions.NoInlining)]
-    static bool ExtractPow2Shift(int a, int b) => ((a & (0b100 << b)) >> b) == 1;
+    static bool ExtractPow2Shift(int a, int b) => ((a & (0b100 << b)) >> b) == 0b100;
 
     [MethodImpl(MethodImplOptions.NoInlining)]
     static bool Extract10Shift(int a) => ((a & (1 << 10)) >> 10) == 1;
 
     [MethodImpl(MethodImplOptions.NoInlining)]
     static bool Extract11Shift(int a) => ((a & (1 << 11)) >> 11) == 1;
+
 
     [MethodImpl(MethodImplOptions.NoInlining)]
     static bool ExtractCast(int a, int b) => (a & (1 << b)) != 0;
@@ -56,6 +57,7 @@ public static class SingleBit
 
     [MethodImpl(MethodImplOptions.NoInlining)]
     static bool Extract11Cast(int a) => (a & (1 << 11)) != 0;
+
 
     [MethodImpl(MethodImplOptions.NoInlining)]
     static bool ExtractEqual(int a, int b) => (a & (1 << b)) == (1 << b);
@@ -87,32 +89,48 @@ public static class SingleBit
     public static void Test()
     {
         Assert.Equal(0x12345478, Set(0x12345078, 10));
+        Assert.Equal(0x12345878, Set(0x12345078, 11));
         Assert.Equal(0x12345478, SetPow2(0x12345078, 8));
+        Assert.Equal(0x12345878, SetPow2(0x12345078, 9));
         Assert.Equal(0x12345478, Set10(0x12345078));
         Assert.Equal(0x12345878, Set11(0x12345078));
 
         Assert.Equal(0x12345078, Clear(0x12345478, 10));
+        Assert.Equal(0x12345078, Clear(0x12345878, 11));
         Assert.Equal(0x12345078, ClearPow2(0x12345478, 8));
+        Assert.Equal(0x12345078, ClearPow2(0x12345878, 9));
         Assert.Equal(0x12345078, Clear10(0x12345478));
         Assert.Equal(0x12345078, Clear11(0x12345878));
 
         Assert.False(ExtractShift(0x12345878, 10));
+        Assert.True (ExtractShift(0x12345878, 11));
         Assert.False(ExtractPow2Shift(0x12345878, 8));
+        Assert.True (ExtractPow2Shift(0x12345878, 9));
         Assert.False(Extract10Shift(0x12345878));
         Assert.True (Extract11Shift(0x12345878));
+
         Assert.False(ExtractCast(0x12345878, 10));
+        Assert.True (ExtractCast(0x12345878, 11));
         Assert.False(ExtractPow2Cast(0x12345878, 8));
+        Assert.True (ExtractPow2Cast(0x12345878, 9));
         Assert.False(Extract10Cast(0x12345878));
         Assert.True (Extract11Cast(0x12345878));
+
         Assert.False(ExtractEqual(0x12345878, 10));
+        Assert.True (ExtractEqual(0x12345878, 11));
         Assert.False(ExtractPow2Equal(0x12345878, 8));
+        Assert.True (ExtractPow2Equal(0x12345878, 9));
         Assert.False(Extract10Equal(0x12345878));
         Assert.True (Extract11Equal(0x12345878));
 
         Assert.Equal(0x12345478, Invert(0x12345078, 10));
         Assert.Equal(0x12345078, Invert(0x12345478, 10));
+        Assert.Equal(0x12345878, Invert(0x12345078, 11));
+        Assert.Equal(0x12345078, Invert(0x12345878, 11));
         Assert.Equal(0x12345478, InvertPow2(0x12345078, 8));
         Assert.Equal(0x12345078, InvertPow2(0x12345478, 8));
+        Assert.Equal(0x12345878, InvertPow2(0x12345078, 9));
+        Assert.Equal(0x12345078, InvertPow2(0x12345878, 9));
         Assert.Equal(0x12345478, Invert10(0x12345078));
         Assert.Equal(0x12345078, Invert10(0x12345478));
         Assert.Equal(0x12345878, Invert11(0x12345078));
