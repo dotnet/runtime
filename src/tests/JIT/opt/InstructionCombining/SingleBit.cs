7a4fd7863af5a3c916f31071adb11039bbb942ca
Tomek Sowiński <tomeksowi@gmail.com>
2025-05-07 13:39:03 +0200

    Simplify ExtractShift test

--- src/tests/JIT/opt/InstructionCombining/SingleBit.cs
+++ src/tests/JIT/opt/InstructionCombining/SingleBit.cs
@@ -34,16 +34,29 @@ public static class SingleBit
 
 
     [MethodImpl(MethodImplOptions.NoInlining)]
-    static bool ExtractShift(int a, int b) => ((a & (1 << b)) >> b) == 1;
+    static bool ExtractShift(int a, int b) => ((a >> b) & 1) == 1;
 
     [MethodImpl(MethodImplOptions.NoInlining)]
-    static bool ExtractPow2Shift(int a, int b) => ((a & (0b100 << b)) >> b) == 0b100;
+    static bool ExtractPow2Shift(int a, int b) => ((a >> b) & 0b100) == 0b100;
 
     [MethodImpl(MethodImplOptions.NoInlining)]
-    static bool Extract10Shift(int a) => ((a & (1 << 10)) >> 10) == 1;
+    static bool Extract10Shift(int a) => ((a >> 10) & 1) == 1;
 
     [MethodImpl(MethodImplOptions.NoInlining)]
-    static bool Extract11Shift(int a) => ((a & (1 << 11)) >> 11) == 1;
+    static bool Extract11Shift(int a) => ((a >> 11) & 1) == 1;
+
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    static bool ExtractDoubleShift(int a, int b) => ((a & (1 << b)) >> b) == 1;
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    static bool ExtractPow2DoubleShift(int a, int b) => ((a & (0b100 << b)) >> b) == 0b100;
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    static bool Extract10DoubleShift(int a) => ((a & (1 << 10)) >> 10) == 1;
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    static bool Extract11DoubleShift(int a) => ((a & (1 << 11)) >> 11) == 1;
 
 
     [MethodImpl(MethodImplOptions.NoInlining)]
@@ -108,6 +121,13 @@ public static class SingleBit
         Assert.True (ExtractPow2Shift(0x12345878, 9));
         Assert.False(Extract10Shift(0x12345878));
         Assert.True (Extract11Shift(0x12345878));
+
+        Assert.False(ExtractDoubleShift(0x12345878, 10));
+        Assert.True (ExtractDoubleShift(0x12345878, 11));
+        Assert.False(ExtractPow2DoubleShift(0x12345878, 8));
+        Assert.True (ExtractPow2DoubleShift(0x12345878, 9));
+        Assert.False(Extract10DoubleShift(0x12345878));
+        Assert.True (Extract11DoubleShift(0x12345878));
 
         Assert.False(ExtractCast(0x12345878, 10));
         Assert.True (ExtractCast(0x12345878, 11));
