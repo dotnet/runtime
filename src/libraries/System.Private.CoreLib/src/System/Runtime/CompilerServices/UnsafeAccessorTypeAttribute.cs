be088860effba34b21a5f441385f37baa96650f0
Aaron R Robinson <arobins@microsoft.com>
2025-05-13 14:03:22 -0700

    Feedback

--- src/libraries/System.Private.CoreLib/src/System/Runtime/CompilerServices/UnsafeAccessorTypeAttribute.cs
+++ src/libraries/System.Private.CoreLib/src/System/Runtime/CompilerServices/UnsafeAccessorTypeAttribute.cs
@@ -16,7 +16,7 @@ namespace System.Runtime.CompilerServices
         /// <remarks>
         /// <paramref name="typeName"/> is expected to follow the same rules as if it were being
         /// passed to <see name="Type.GetType(String)"/>. When unbound generics are involved they
-        /// should follow the IL syntax of referencing an ELEMENT_TYPE_VAR or ELEMENT_TYPE_MVAR using
+        /// should follow the IL syntax of referencing a type or method generic variables using
         /// the syntax of <c>!N</c> or <c>!!N</c> respectively, where N is the zero-based index of the
         /// generic parameters. The generic rules defined for <see cref="UnsafeAccessorAttribute"/>
         /// apply to this attribute as well, meaning the arity and type of generic parameter must match
@@ -29,11 +29,10 @@ namespace System.Runtime.CompilerServices
         ///
         /// <ul>
         ///   <li>References should be typed as <lang>object</lang>.</li>
-        ///   <li>Arrays should be typed as <lang>object</lang>.</li>
         ///   <li>Byref arguments should be typed with <lang>in</lang>, <lang>ref</lang>, or <lang>out</lang> to <lang>object</lang>.</li>
-        ///   <li>Pointers should be typed as <lang>void*</lang>.</li>
+        ///   <li>Unmanaged pointers should be typed as <lang>void*</lang>.</li>
         ///   <li>Byref arguments to reference types or arrays should be typed with <lang>in</lang>, <lang>ref</lang>, or <lang>out</lang> to <lang>object</lang>.</li>
-        ///   <li>Byref arguments to pointer types should be typed with <lang>in</lang>, <lang>ref</lang>, or <lang>out</lang> to <lang>void*</lang>.</li>
+        ///   <li>Byref arguments to unmanaged pointer types should be typed with <lang>in</lang>, <lang>ref</lang>, or <lang>out</lang> to <lang>void*</lang>.</li>
         /// </ul>
         ///
         /// Value types are not supported.
