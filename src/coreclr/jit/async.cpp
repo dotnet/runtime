275f2e02ac817b30bf56a3739a8f4d4669b33a9d
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2025-05-15 16:32:33 +0200

    JIT: Fix SIMD/struct type mismatches in async transformation

--- src/coreclr/jit/async.cpp
+++ src/coreclr/jit/async.cpp
@@ -324,6 +324,7 @@ PhaseStatus AsyncTransformation::Run()
 
         m_comp->lvaComputeRefCounts(true, false);
         m_comp->fgLocalVarLiveness();
+        INDEBUG(m_comp->mostRecentlyActivePhase = PHASE_ASYNC);
         VarSetOps::AssignNoCopy(m_comp, m_comp->compCurLife, VarSetOps::MakeEmpty(m_comp));
     }
 
@@ -1091,7 +1092,7 @@ void AsyncTransformation::FillInDataOnSuspension(const jitstd::vector<LiveLocalI
         if (dsc->IsImplicitByRef())
         {
             GenTree* baseAddr = m_comp->gtNewLclvNode(inf.LclNum, dsc->TypeGet());
-            value             = m_comp->gtNewBlkIndir(dsc->GetLayout(), baseAddr, GTF_IND_NONFAULTING);
+            value             = m_comp->gtNewLoadValueNode(dsc->GetLayout(), baseAddr, GTF_IND_NONFAULTING);
         }
         else
         {
@@ -1106,7 +1107,7 @@ void AsyncTransformation::FillInDataOnSuspension(const jitstd::vector<LiveLocalI
             // This is to heap, but all GC refs are nulled out already, so we can skip the write barrier.
             // TODO-CQ: Backend does not care about GTF_IND_TGT_NOT_HEAP for STORE_BLK.
             store =
-                m_comp->gtNewStoreBlkNode(dsc->GetLayout(), addr, value, GTF_IND_NONFAULTING | GTF_IND_TGT_NOT_HEAP);
+                m_comp->gtNewStoreValueNode(dsc->GetLayout(), addr, value, GTF_IND_NONFAULTING | GTF_IND_TGT_NOT_HEAP);
         }
         else
         {
@@ -1280,7 +1281,7 @@ void AsyncTransformation::RestoreFromDataOnResumption(unsigned                  
         GenTree* value;
         if ((dsc->TypeGet() == TYP_STRUCT) || dsc->IsImplicitByRef())
         {
-            value = m_comp->gtNewBlkIndir(dsc->GetLayout(), addr, GTF_IND_NONFAULTING);
+            value = m_comp->gtNewLoadValueNode(dsc->GetLayout(), addr, GTF_IND_NONFAULTING);
         }
         else
         {
@@ -1291,8 +1292,8 @@ void AsyncTransformation::RestoreFromDataOnResumption(unsigned                  
         if (dsc->IsImplicitByRef())
         {
             GenTree* baseAddr = m_comp->gtNewLclvNode(inf.LclNum, dsc->TypeGet());
-            store             = m_comp->gtNewStoreBlkNode(dsc->GetLayout(), baseAddr, value,
-                                                          GTF_IND_NONFAULTING | GTF_IND_TGT_NOT_HEAP);
+            store             = m_comp->gtNewStoreValueNode(dsc->GetLayout(), baseAddr, value,
+                                                            GTF_IND_NONFAULTING | GTF_IND_TGT_NOT_HEAP);
         }
         else
         {
@@ -1505,7 +1506,7 @@ void AsyncTransformation::CopyReturnValueOnResumption(GenTreeCall*              
 
     assert(callDefInfo.DefinitionNode != nullptr);
     LclVarDsc* resultLcl = m_comp->lvaGetDesc(callDefInfo.DefinitionNode);
-    assert((resultLcl->TypeGet() == TYP_STRUCT) == (call->gtReturnType == TYP_STRUCT));
+    assert(varTypeIsStruct(resultLcl) == varTypeIsStruct(call->gtReturnType));
 
     // TODO-TP: We can use liveness to avoid generating a lot of this IR.
     if (call->gtReturnType == TYP_STRUCT)
@@ -1514,7 +1515,7 @@ void AsyncTransformation::CopyReturnValueOnResumption(GenTreeCall*              
         {
             GenTree* resultOffsetNode = m_comp->gtNewIconNode((ssize_t)resultOffset, TYP_I_IMPL);
             GenTree* resultAddr       = m_comp->gtNewOperNode(GT_ADD, TYP_BYREF, resultBase, resultOffsetNode);
-            GenTree* resultData       = m_comp->gtNewBlkIndir(layout.ReturnStructLayout, resultAddr, resultIndirFlags);
+            GenTree* resultData       = m_comp->gtNewLoadValueNode(layout.ReturnStructLayout, resultAddr, resultIndirFlags);
             GenTree* storeResult;
             if ((callDefInfo.DefinitionNode->GetLclOffs() == 0) &&
                 ClassLayout::AreCompatible(resultLcl->GetLayout(), layout.ReturnStructLayout))
