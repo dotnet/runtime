86c0f77486c9ebcbd1c0d8ce16e772373199ec1e
Shun Kinoshita <shkinosh@microsoft.com>
2025-05-17 08:22:05 +0900

    Revert "Move !tree->gtSetFlags() to larger if condition"
    This reverts commit 00acd283fd713e626ea4fd42470ab1a444cf135e.

--- src/coreclr/jit/codegenxarch.cpp
+++ src/coreclr/jit/codegenxarch.cpp
@@ -4863,11 +4863,11 @@ void CodeGen::genCodeForShift(GenTree* tree)
     GenTree* shiftBy = tree->gtGetOp2();
     emitAttr size    = emitTypeSize(tree);
 
-    if (shiftBy->isContainedIntOrIImmed() && !tree->gtSetFlags())
+    if (shiftBy->isContainedIntOrIImmed())
     {
         assert(tree->OperIsRotate() || (operandReg != REG_NA));
 
-        bool mightOptimizeLsh = tree->OperIs(GT_LSH);
+        bool mightOptimizeLsh = tree->OperIs(GT_LSH) && !tree->gtSetFlags();
 
         // Optimize "X<<1" to "lea [reg+reg]" or "add reg, reg"
         if (mightOptimizeLsh && shiftBy->IsIntegralConst(1))
@@ -4897,7 +4897,7 @@ void CodeGen::genCodeForShift(GenTree* tree)
         {
             int shiftByValue = (int)shiftBy->AsIntConCommon()->IconValue();
 
-            if (tree->OperIsRotate() && compiler->compOpportunisticallyDependsOn(InstructionSet_BMI2))
+            if (tree->OperIsRotate() && compiler->compOpportunisticallyDependsOn(InstructionSet_BMI2) && !tree->gtSetFlags())
             {
                 // If we have a contained source operand, we must emit rorx.
                 // We may also use rorx for 64bit values when a mov would otherwise be required,
