a0ee7ecb4d269e9ee57f3ec580237e1a23e1193d
Manish Godse <61718172+mangod9@users.noreply.github.com>
2025-05-22 13:05:10 -0700

    update reservationSize on iOS
    on my local testing looks like reservation of 1/4th of physical memory seems to succeed.

--- src/coreclr/gc/gc.cpp
+++ src/coreclr/gc/gc.cpp
@@ -49322,8 +49322,13 @@ HRESULT GCHeap::Initialize()
             // If no hard_limit is configured the reservation size is min of 1/2 GetVirtualMemoryLimit() or max of 256Gb or 2x physical limit.
             gc_heap::regions_range = max((size_t)256 * 1024 * 1024 * 1024, (size_t)(2 * gc_heap::total_physical_mem));
 #else // MUTLIPLE_HEAPS
+#if defined(TARGET_IOS) && defined(TARGET_TVOS)
+            // On iOS and tvOS only try to reserve 1/4 of physical memory
+            gc_heap::regions_range = (size_t)(gc_heap::total_physical_mem / 4);
+#else  // TARGET_IOS && TARGET_TVOS
             // If no hard_limit is configured the reservation size is min of 1/2 GetVirtualMemoryLimit() or max of 256Gb or 2x physical limit.
             gc_heap::regions_range = (size_t)(2 * gc_heap::total_physical_mem);
+#endif // TARGET_IOS && TARGET_TVOS
 #endif // MUTLIPLE_HEAPS
         }
         size_t virtual_mem_limit = GCToOSInterface::GetVirtualMemoryLimit();
