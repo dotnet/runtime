27347c430ef75e5807f627612adba7177bbd5495
Deepak Rajendrakumaran <deepak.rajendrakumaran@intel.com>
2025-05-22 14:49:52 -0700

    Add APX knob

--- src/coreclr/jit/compiler.h
+++ src/coreclr/jit/compiler.h
@@ -9887,6 +9887,12 @@ public:
     //
     bool canUseApxEncoding() const
     {
+#ifdef DEBUG
+        if (JitConfig.JitBypassApxCheck())
+        {
+            return true;
+        }
+#endif
         return compOpportunisticallyDependsOn(InstructionSet_APX);
     }
 
