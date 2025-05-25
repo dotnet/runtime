03b9eeb92d96a4cb34ff5352c98bcdae2274be05
Filip Navara <filip.navara@gmail.com>
2025-05-25 22:05:02 +0200

    Cleanup usages of WantsReportOnlyLeaf

--- src/coreclr/vm/gcenv.ee.common.cpp
+++ src/coreclr/vm/gcenv.ee.common.cpp
@@ -335,37 +335,22 @@ StackWalkAction GcStackCrawlCallBack(CrawlFrame* pCF, VOID* pData)
 #if defined(FEATURE_EH_FUNCLETS)
             if (pCF->ShouldParentToFuncletUseUnwindTargetLocationForGCReporting())
             {
-                bool wantsReportOnlyLeaf = true;
+                // We're in a special case of unwinding from a funclet, and resuming execution in
+                // another catch funclet associated with same parent function. We need to report roots.
+                // Reporting at the original throw site gives incorrect liveness information. We choose to
+                // report the liveness information at the first interruptible instruction of the catch funclet
+                // that we are going to execute. We also only report stack slots, since no registers can be
+                // live at the first instruction of a handler, except the catch object, which the VM protects
+                // specially. If the catch funclet has not interruptible point, we fall back and just report
+                // what we used to: at the original throw instruction. This might lead to bad GC behavior
+                // if the liveness is not correct.
+                const EE_ILEXCEPTION_CLAUSE& ehClauseForCatch = pCF->GetEHClauseForCatch();
+                relOffsetOverride = FindFirstInterruptiblePoint(pCF, ehClauseForCatch.HandlerStartPC,
+                                                                ehClauseForCatch.HandlerEndPC);
+                _ASSERTE(relOffsetOverride != NO_OVERRIDE_OFFSET);
 
-#if defined(USE_GC_INFO_DECODER)
-                GCInfoToken gcInfoToken = pCF->GetGCInfoToken();
-                GcInfoDecoder _gcInfoDecoder(
-                                    gcInfoToken,
-                                    DECODE_CODE_LENGTH
-                                    );
-                wantsReportOnlyLeaf = _gcInfoDecoder.WantsReportOnlyLeaf();
-#endif // USE_GC_INFO_DECODER
-
-                if (wantsReportOnlyLeaf)
-                {
-                    // We're in a special case of unwinding from a funclet, and resuming execution in
-                    // another catch funclet associated with same parent function. We need to report roots.
-                    // Reporting at the original throw site gives incorrect liveness information. We choose to
-                    // report the liveness information at the first interruptible instruction of the catch funclet
-                    // that we are going to execute. We also only report stack slots, since no registers can be
-                    // live at the first instruction of a handler, except the catch object, which the VM protects
-                    // specially. If the catch funclet has not interruptible point, we fall back and just report
-                    // what we used to: at the original throw instruction. This might lead to bad GC behavior
-                    // if the liveness is not correct.
-                    const EE_ILEXCEPTION_CLAUSE& ehClauseForCatch = pCF->GetEHClauseForCatch();
-                    relOffsetOverride = FindFirstInterruptiblePoint(pCF, ehClauseForCatch.HandlerStartPC,
-                                                                    ehClauseForCatch.HandlerEndPC);
-                    _ASSERTE(relOffsetOverride != NO_OVERRIDE_OFFSET);
-
-                    STRESS_LOG3(LF_GCROOTS, LL_INFO1000, "Setting override offset = %u for method %pM ControlPC = %p\n",
-                        relOffsetOverride, pMD, GetControlPC(pCF->GetRegisterSet()));
-                }
-
+                STRESS_LOG3(LF_GCROOTS, LL_INFO1000, "Setting override offset = %u for method %pM ControlPC = %p\n",
+                    relOffsetOverride, pMD, GetControlPC(pCF->GetRegisterSet()));
             }
 #endif // FEATURE_EH_FUNCLETS
 
