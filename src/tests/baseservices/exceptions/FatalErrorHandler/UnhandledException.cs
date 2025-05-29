b967f3a05b994e22a53bd6832248241a1fab8fb3
vsadov <8218165+VSadov@users.noreply.github.com>
2025-05-29 13:06:50 -0700

    added simple test

---
+++ src/tests/baseservices/exceptions/FatalErrorHandler/UnhandledException.cs
@@ -0,0 +1,30 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+using System;
+using System.Runtime.InteropServices;
+using System.Runtime.ExceptionServices;
+using System.Threading;
+using System.Threading.Tasks;
+using TestLibrary;
+using Xunit;
+
+unsafe public class UnhandledException
+{
+    [UnmanagedCallersOnly]
+    static int Handler(int i, void* ptr)
+    {
+        Environment.Exit(100);
+
+        // unreachable
+        return 42;
+    }
+
+    public static int Main()
+    {
+        // set handler
+        ExceptionHandling.SetFatalErrorHandler((delegate* unmanaged<int, void*, int>)&Handler);
+
+        // throw
+        throw new Exception();
+    }
+}
