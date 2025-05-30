1c0ef359510f9eb873c4b1f95cf5a3d5a6bb95dd
Jan Kotas <jkotas@microsoft.com>
2025-05-29 22:49:36 -0700

    Update src/coreclr/nativeaot/Runtime/unix/PalRedhawkUnix.cpp

--- src/coreclr/nativeaot/Runtime/unix/PalRedhawkUnix.cpp
+++ src/coreclr/nativeaot/Runtime/unix/PalRedhawkUnix.cpp
@@ -1154,3 +1154,8 @@ void PalGetSystemTimeAsFileTime(FILETIME *lpSystemTimeAsFileTime)
     lpSystemTimeAsFileTime->dwLowDateTime = (uint32_t)result;
     lpSystemTimeAsFileTime->dwHighDateTime = (uint32_t)(result >> 32);
 }
+
+extern "C" uint64_t PalGetCurrentOSThreadId()
+{
+    return (uint64_t)minipal_get_current_thread_id();
+}
