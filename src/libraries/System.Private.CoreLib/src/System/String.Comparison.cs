683b87737cfe644e814dcb0a4ea8e8a724b81d90
Egor Bogatov <egorbo@gmail.com>
2025-05-30 02:52:40 +0200

    Update String.Comparison.cs

--- src/libraries/System.Private.CoreLib/src/System/String.Comparison.cs
+++ src/libraries/System.Private.CoreLib/src/System/String.Comparison.cs
@@ -829,7 +829,32 @@ namespace System
 
         internal int GetNonRandomizedHashCode()
         {
-            return GetNonRandomizedHashCode(new ReadOnlySpan<char>(ref _firstChar, _stringLength));
+            fixed (char* src = &_firstChar)
+            {
+                Debug.Assert(src[Length] == '\0', "src[Length] == '\\0'");
+                Debug.Assert(((int)src) % 4 == 0, "Managed string should start at 4 bytes boundary");
+
+                uint hash1 = (5381 << 16) + 5381;
+                uint hash2 = hash1;
+
+                uint* ptr = (uint*)src;
+                int length = Length;
+
+                while (length > 2)
+                {
+                    length -= 4;
+                    hash1 = (BitOperations.RotateLeft(hash1, 5) + hash1) ^ ptr[0];
+                    hash2 = (BitOperations.RotateLeft(hash2, 5) + hash2) ^ ptr[1];
+                    ptr += 2;
+                }
+
+                if (length > 0)
+                {
+                    hash2 = (BitOperations.RotateLeft(hash2, 5) + hash2) ^ ptr[0];
+                }
+
+                return (int)(hash1 + (hash2 * 1566083941));
+            }
         }
 
         internal static unsafe int GetNonRandomizedHashCode(ReadOnlySpan<char> span)
