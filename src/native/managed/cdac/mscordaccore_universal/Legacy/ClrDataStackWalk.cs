233615e8526d83733e2b388d8771fd5991f9485e
Max Charlamb <maxcharlamb@microsoft.com>
2025-06-03 13:48:45 -0400

    import nit

--- src/native/managed/cdac/mscordaccore_universal/Legacy/ClrDataStackWalk.cs
+++ src/native/managed/cdac/mscordaccore_universal/Legacy/ClrDataStackWalk.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.Marshalling;
 using Microsoft.Diagnostics.DataContractReader.Contracts;
