d4b08f96ee0368867be579c03181a2f58b9e01a4
Jeremy Koritzinsky <jekoritz@microsoft.com>
2025-06-04 11:21:49 -0700

    Fix no-argument ./build.sh invocation (#116315)

--- eng/build.sh
+++ eng/build.sh
@@ -153,7 +153,7 @@ showSubsetHelp()
   "$scriptroot/common/build.sh" "-restore" "-build" "/p:Subset=help" "/clp:nosummary /tl:false"
 }
 
-arguments=()
+arguments=("")
 cmakeargs=''
 extraargs=()
 crossBuild=0
