0ba41ca7c810d1570e973adac94d62c9a7f40342
kasperk81 <83082615+kasperk81@users.noreply.github.com>
2025-06-04 03:42:45 +0300

    Update HardwareExceptions.cpp

--- src/coreclr/nativeaot/Runtime/unix/HardwareExceptions.cpp
+++ src/coreclr/nativeaot/Runtime/unix/HardwareExceptions.cpp
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 #include "CommonTypes.h"
+#include "Pal.h"
 #include "PalLimitedContext.h"
 #include "CommonMacros.h"
 #include "config.h"
