c43a90263831a8f16cda08c876d5901697f4dfe1
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2025-06-05 21:49:19 +0200

    Put back GUID

--- src/coreclr/nativeaot/Runtime/Pal.h
+++ src/coreclr/nativeaot/Runtime/Pal.h
@@ -87,6 +87,13 @@ struct FILETIME
     uint32_t dwHighDateTime;
 };
 
+typedef struct _GUID {
+    uint32_t Data1;
+    uint16_t Data2;
+    uint16_t Data3;
+    uint8_t Data4[8];
+} GUID;
+
 typedef struct _CONTEXT CONTEXT, *PCONTEXT;
 
 typedef struct _EXCEPTION_RECORD EXCEPTION_RECORD, *PEXCEPTION_RECORD;
