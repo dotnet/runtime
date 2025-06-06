dotnet/runtime

commit 5a65f7c86f3b44975fa611b0569f256c7c58d961

Enable RhpNewObjectArrayFast optimization on NativeAOT (#116290)
* Enable RhpNewObjectArrayFast optimization on NativeAOT

* Rename CORINFO_HELP_NEWARR_1_OBJ -> CORINFO_HELP_NEWARR_1_PTR, RhpNewObjectArrayFast -> RhpNewPtrArrayFast, fix incorrect comments

* Use CORINFO_HELP_NEWARR_1_PTR for all SZARRAYs with pointer-sized elements

---------

Co-authored-by: Jan Kotas <jkotas@microsoft.com>

src/coreclr/runtime/amd64/AllocFast.S
@@ -114,7 +114,7 @@ LOCAL_LABEL(NewOutOfMemory):
 NESTED_END RhpNewObject, _TEXT
 
 
-// Shared code for RhNewString, RhpNewArrayFast and RhpNewObjectArrayFast
+// Shared code for RhNewString, RhpNewArrayFast and RhpNewPtrArrayFast
 //  RAX == string/array size
 //  RDI == MethodTable
 //  ESI == character/element count
@@ -228,11 +228,10 @@ LOCAL_LABEL(ArraySizeOverflow):
 LEAF_END RhpNewArrayFast, _TEXT
 
 
-#ifndef FEATURE_NATIVEAOT
-// Allocate one dimensional, zero based array (SZARRAY) of objects (pointer sized elements).
+// Allocate one dimensional, zero based array (SZARRAY) of pointer sized elements.
 //  RDI == MethodTable
 //  ESI == element count
-LEAF_ENTRY RhpNewObjectArrayFast, _TEXT
+LEAF_ENTRY RhpNewPtrArrayFast, _TEXT
 
         // Delegate overflow handling to the generic helper conservatively
 
@@ -250,8 +249,7 @@ LEAF_ENTRY RhpNewObjectArrayFast, _TEXT
 
         NEW_ARRAY_FAST
 
-LEAF_END RhpNewObjectArrayFast, _TEXT
-#endif
+LEAF_END RhpNewPtrArrayFast, _TEXT
 
 
 NESTED_ENTRY RhpNewVariableSizeObject, _TEXT, NoHandler

src/coreclr/runtime/amd64/AllocFast.asm
@@ -95,7 +95,7 @@ NewOutOfMemory:
 NESTED_END RhpNewObject, _TEXT
 
 
-;; Shared code for RhNewString, RhpNewArrayFast and RhpNewObjectArrayFast
+;; Shared code for RhNewString, RhpNewArrayFast and RhpNewPtrArrayFast
 ;;  RAX == string/array size
 ;;  RCX == MethodTable
 ;;  RDX == character/element count
@@ -182,11 +182,10 @@ ArraySizeOverflow:
 LEAF_END RhpNewArrayFast, _TEXT
 
 
-IFNDEF FEATURE_NATIVEAOT
-;; Allocate one dimensional, zero based array (SZARRAY) of objects (pointer sized elements).
+;; Allocate one dimensional, zero based array (SZARRAY) of pointer sized elements.
 ;;  RCX == MethodTable
 ;;  EDX == element count
-LEAF_ENTRY RhpNewObjectArrayFast, _TEXT
+LEAF_ENTRY RhpNewPtrArrayFast, _TEXT
 
         ; Delegate overflow handling to the generic helper conservatively
 
@@ -204,8 +203,7 @@ LEAF_ENTRY RhpNewObjectArrayFast, _TEXT
 
         NEW_ARRAY_FAST
 
-LEAF_END RhpNewObjectArrayFast, _TEXT
-ENDIF ; FEATURE_NATIVEAOT
+LEAF_END RhpNewPtrArrayFast, _TEXT
 
 
 NESTED_ENTRY RhpNewVariableSizeObject, _TEXT

src/coreclr/runtime/arm/AllocFast.S
@@ -334,32 +334,30 @@ LOCAL_LABEL(ArraySizeOverflow):
 LEAF_END RhpNewArrayFast, _TEXT
 
 
-#ifndef FEATURE_NATIVEAOT
-// Allocate one dimensional, zero based array (SZARRAY) of objects (pointer sized elements).
+// Allocate one dimensional, zero based array (SZARRAY) of pointer sized elements.
 //  r0 == MethodTable
 //  r1 == element count
-LEAF_ENTRY RhpNewObjectArrayFast, _TEXT
+LEAF_ENTRY RhpNewPtrArrayFast, _TEXT
         NEW_ARRAY_FAST_PROLOG
 
         // Delegate overflow handling to the generic helper conservatively
 
         mov         r2, #1 << 28 // 0x40000000 / sizeof(void*)
         cmp         r1, r2
-        bhs         LOCAL_LABEL(RhpNewObjectArrayFast_RarePath)
+        bhs         LOCAL_LABEL(RhpNewPtrArrayFast_RarePath)
 
         mov         r2, #SZARRAY_BASE_SIZE
         add         r2, r2, r1, lsl #2
 
         NEW_ARRAY_FAST
 
-LOCAL_LABEL(RhpNewObjectArrayFast_RarePath):
+LOCAL_LABEL(RhpNewPtrArrayFast_RarePath):
         NEW_ARRAY_FAST_TAIL_EPILOG
         b           C_FUNC(RhpNewVariableSizeObject)
-LEAF_END RhpNewObjectArrayFast, _TEXT
-#endif
+LEAF_END RhpNewPtrArrayFast, _TEXT
 
 
-// Allocate one dimensional, zero based array (SZARRAY) using the slow path that calls a runtime helper.
+// Allocate variable sized object (eg. array, string) using the slow path that calls a runtime helper.
 //  r0 == MethodTable
 //  r1 == element count
 NESTED_ENTRY RhpNewVariableSizeObject, _TEXT, NoHandler

src/coreclr/runtime/arm64/AllocFast.S
@@ -100,7 +100,7 @@ LOCAL_LABEL(NewOutOfMemory):
     NESTED_END RhpNewObject, _TEXT
 
 
-// Shared code for RhNewString, RhpNewArrayFast and RhpNewObjectArrayFast
+// Shared code for RhNewString, RhpNewArrayFast and RhpNewPtrArrayFast
 //  x0 == MethodTable
 //  x1 == character/element count
 //  x2 == string/array size
@@ -210,11 +210,10 @@ LOCAL_LABEL(ArraySizeOverflow):
     LEAF_END    RhpNewArrayFast, _TEXT
 
 
-#ifndef FEATURE_NATIVEAOT
-// Allocate one dimensional, zero based array (SZARRAY) of objects (pointer sized elements).
+// Allocate one dimensional, zero based array (SZARRAY) of pointer sized elements.
 //  x0 == MethodTable
 //  x1 == element count
-    LEAF_ENTRY RhpNewObjectArrayFast, _Text
+    LEAF_ENTRY RhpNewPtrArrayFast, _Text
 
         // Delegate overflow handling to the generic helper conservatively
 
@@ -243,11 +242,10 @@ LOCAL_LABEL(ArraySizeOverflow):
         b           C_FUNC(RhpNewVariableSizeObject)
 #endif
 
-    LEAF_END    RhpNewObjectArrayFast, _TEXT
-#endif
+    LEAF_END    RhpNewPtrArrayFast, _TEXT
 
 
-// Allocate one dimensional, zero based array (SZARRAY) using the slow path that calls a runtime helper.
+// Allocate variable sized object (eg. array, string) using the slow path that calls a runtime helper.
 //  x0 == MethodTable
 //  x1 == element count
     NESTED_ENTRY RhpNewVariableSizeObject, _TEXT, NoHandler

src/coreclr/runtime/arm64/AllocFast.asm
@@ -95,7 +95,7 @@ NewOutOfMemory
 
     NESTED_END RhpNewObject
 
-;; Shared code for RhNewString, RhpNewArrayFast and RhpNewObjectArrayFast
+;; Shared code for RhNewString, RhpNewArrayFast and RhpNewPtrArrayFast
 ;;  x0 == MethodTable
 ;;  x1 == character/element count
 ;;  x2 == string/array size
@@ -191,11 +191,10 @@ ArraySizeOverflow
         b           RhExceptionHandling_FailedAllocation
     LEAF_END    RhpNewArrayFast
 
-#ifndef FEATURE_NATIVEAOT
-;; Allocate one dimensional, zero based array (SZARRAY) of objects (pointer sized elements).
+;; Allocate one dimensional, zero based array (SZARRAY) of pointer sized elements.
 ;;  x0 == MethodTable
 ;;  x1 == element count
-    LEAF_ENTRY RhpNewObjectArrayFast
+    LEAF_ENTRY RhpNewPtrArrayFast
 
         ; Delegate overflow handling to the generic helper conservatively
 
@@ -215,10 +214,9 @@ ArraySizeOverflow
 
         NEW_ARRAY_FAST
 
-    LEAF_END    RhpNewObjectArrayFast
-#endif
+    LEAF_END    RhpNewPtrArrayFast
 
-;; Allocate one dimensional, zero based array (SZARRAY) using the slow path that calls a runtime helper.
+;; Allocate variable sized object (eg. array, string) using the slow path that calls a runtime helper.
 ;;  x0 == MethodTable
 ;;  x1 == element count
     NESTED_ENTRY RhpNewVariableSizeObject

src/coreclr/runtime/i386/AllocFast.S
@@ -75,7 +75,7 @@ LOCAL_LABEL(NewOutOfMemory):
         jmp         RhExceptionHandling_FailedAllocation
 LEAF_END RhpNewObject, _TEXT
 
-// Shared code for RhNewString, RhpNewArrayFast and RhpNewObjectArrayFast
+// Shared code for RhNewString, RhpNewArrayFast and RhpNewPtrArrayFast
 //  EAX == string/array size
 //  ECX == MethodTable
 //  EDX == character/element count
@@ -212,11 +212,10 @@ LOCAL_LABEL(RhpNewArrayFast_ArraySizeOverflow):
 LEAF_END RhpNewArrayFast, _TEXT
 
 
-#ifndef FEATURE_NATIVEAOT
-// Allocate one dimensional, zero based array (SZARRAY) of objects (pointer sized elements).
+// Allocate one dimensional, zero based array (SZARRAY) of pointer sized elements.
 //  ECX == MethodTable
 //  EDX == element count
-LEAF_ENTRY RhpNewObjectArrayFast, _TEXT
+LEAF_ENTRY RhpNewPtrArrayFast, _TEXT
         // Delegate overflow handling to the generic helper conservatively
 
         cmp         edx, (0x40000000 / 4) // sizeof(void*)
@@ -230,8 +229,7 @@ LEAF_ENTRY RhpNewObjectArrayFast, _TEXT
 
         NEW_ARRAY_FAST_PROLOG
         NEW_ARRAY_FAST
-LEAF_END RhpNewObjectArrayFast, _TEXT
-#endif
+LEAF_END RhpNewPtrArrayFast, _TEXT
 
 //
 // Object* RhpNewVariableSizeObject(MethodTable *pMT, INT_PTR size)

src/coreclr/runtime/i386/AllocFast.asm
@@ -77,7 +77,7 @@ NewOutOfMemory:
         jmp         RhExceptionHandling_FailedAllocation
 FASTCALL_ENDFUNC
 
-; Shared code for RhNewString, RhpNewArrayFast and RhpNewObjectArrayFast
+; Shared code for RhNewString, RhpNewArrayFast and RhpNewPtrArrayFast
 ;  EAX == string/array size
 ;  ECX == MethodTable
 ;  EDX == character/element count
@@ -204,11 +204,10 @@ ArraySizeOverflow:
         jmp         RhExceptionHandling_FailedAllocation
 FASTCALL_ENDFUNC
 
-IFNDEF FEATURE_NATIVEAOT
-; Allocate one dimensional, zero based array (SZARRAY) of objects (pointer sized elements).
+; Allocate one dimensional, zero based array (SZARRAY) of pointer sized elements.
 ;  ECX == MethodTable
 ;  EDX == element count
-FASTCALL_FUNC   RhpNewObjectArrayFast, 8
+FASTCALL_FUNC   RhpNewPtrArrayFast, 8
         ; Delegate overflow handling to the generic helper conservatively
 
         cmp         edx, (40000000h / 4) ; sizeof(void*)
@@ -223,7 +222,6 @@ FASTCALL_FUNC   RhpNewObjectArrayFast, 8
         NEW_ARRAY_FAST_PROLOG
         NEW_ARRAY_FAST
 FASTCALL_ENDFUNC
-ENDIF
 
 ;
 ; Object* RhpNewVariableSizeObject(MethodTable *pMT, INT_PTR size)

src/coreclr/runtime/loongarch64/AllocFast.S
@@ -95,7 +95,7 @@ LOCAL_LABEL(NewOutOfMemory):
 
     NESTED_END RhpNewObject, _TEXT
 
-// Shared code for RhNewString, RhpNewArrayFast and RhpNewObjectArrayFast
+// Shared code for RhNewString, RhpNewArrayFast and RhpNewPtrArrayFast
 //  a0 == MethodTable
 //  a1 == character/element count
 //  a2 == string/array size
@@ -191,11 +191,10 @@ LOCAL_LABEL(ArraySizeOverflow):
 
     LEAF_END    RhpNewArrayFast, _TEXT
 
-#ifndef FEATURE_NATIVEAOT
-// Allocate one-dimensional, zero-based array (SZARRAY) of objects (pointer sized elements).
+// Allocate one dimensional, zero based array (SZARRAY) of pointer sized elements.
 //  $a0 == MethodTable
 //  $a1 == element count
-    LEAF_ENTRY RhpNewObjectArrayFast, _Text
+    LEAF_ENTRY RhpNewPtrArrayFast, _Text
 
         // Delegate overflow handling to the generic helper conservatively
 
@@ -214,10 +213,9 @@ LOCAL_LABEL(ArraySizeOverflow):
 
         NEW_ARRAY_FAST
 
-    LEAF_END    RhpNewObjectArrayFast, _TEXT
-#endif
+    LEAF_END    RhpNewPtrArrayFast, _TEXT
 
-// Allocate one dimensional, zero based array (SZARRAY) using the slow path that calls a runtime helper.
+// Allocate variable sized object (eg. array, string) using the slow path that calls a runtime helper.
 //  a0 == MethodTable
 //  a1 == element count
     NESTED_ENTRY RhpNewVariableSizeObject, _TEXT, NoHandler

commit c35a99d1e71326f89b646c65e2fa59c0d982b3a9

Fix NativeAOT outer loop test failure
The optimized path introduced in #116290 skipped error handling that's required for native AOT.

src/coreclr/runtime/amd64/AllocFast.S
@@ -236,7 +236,7 @@ LEAF_ENTRY RhpNewPtrArrayFast, _TEXT
         // Delegate overflow handling to the generic helper conservatively
 
         cmp         rsi, (0x40000000 / 8) // sizeof(void*)
-        jae         C_FUNC(RhpNewVariableSizeObject)
+        jae         C_FUNC(RhpNewArrayFast)
 
         // In this case we know the element size is sizeof(void *), or 8 for x64
         // This helps us in two ways - we can shift instead of multiplying, and

src/coreclr/runtime/amd64/AllocFast.asm
@@ -190,7 +190,7 @@ LEAF_ENTRY RhpNewPtrArrayFast, _TEXT
         ; Delegate overflow handling to the generic helper conservatively
 
         cmp         rdx, (40000000h / 8) ; sizeof(void*)
-        jae         RhpNewVariableSizeObject
+        jae         RhpNewArrayFast
 
         ; In this case we know the element size is sizeof(void *), or 8 for x64
         ; This helps us in two ways - we can shift instead of multiplying, and

src/coreclr/runtime/arm/AllocFast.S
@@ -353,7 +353,7 @@ LEAF_ENTRY RhpNewPtrArrayFast, _TEXT
 
 LOCAL_LABEL(RhpNewPtrArrayFast_RarePath):
         NEW_ARRAY_FAST_TAIL_EPILOG
-        b           C_FUNC(RhpNewVariableSizeObject)
+        b           C_FUNC(RhpNewArrayFast)
 LEAF_END RhpNewPtrArrayFast, _TEXT
 
 

src/coreclr/runtime/arm64/AllocFast.S
@@ -222,7 +222,7 @@ LOCAL_LABEL(ArraySizeOverflow):
 #if defined(__APPLE__)
         bhs         1f
 #else
-        bhs         C_FUNC(RhpNewVariableSizeObject)
+        bhs         C_FUNC(RhpNewArrayFast)
 #endif
 
         // In this case we know the element size is sizeof(void *), or 8 for arm64

src/coreclr/runtime/arm64/AllocFast.asm
@@ -200,7 +200,7 @@ ArraySizeOverflow
 
         mov         x2, #(0x40000000 / 8) ; sizeof(void*)
         cmp         x1, x2
-        bhs         RhpNewVariableSizeObject
+        bhs         RhpNewArrayFast
 
         ; In this case we know the element size is sizeof(void *), or 8 for arm64
         ; This helps us in two ways - we can shift instead of multiplying, and

src/coreclr/runtime/i386/AllocFast.S
@@ -219,7 +219,7 @@ LEAF_ENTRY RhpNewPtrArrayFast, _TEXT
         // Delegate overflow handling to the generic helper conservatively
 
         cmp         edx, (0x40000000 / 4) // sizeof(void*)
-        jae         RhpNewVariableSizeObject
+        jae         RhpNewArrayFast
 
         // In this case we know the element size is sizeof(void *), or 4 for x86
         // This helps us in two ways - we can shift instead of multiplying, and

src/coreclr/runtime/i386/AllocFast.asm
@@ -211,7 +211,7 @@ FASTCALL_FUNC   RhpNewPtrArrayFast, 8
         ; Delegate overflow handling to the generic helper conservatively
 
         cmp         edx, (40000000h / 4) ; sizeof(void*)
-        jae         @RhpNewVariableSizeObject@8
+        jae         @RhpNewArrayFast@8
 
         ; In this case we know the element size is sizeof(void *), or 4 for x86
         ; This helps us in two ways - we can shift instead of multiplying, and

src/coreclr/runtime/loongarch64/AllocFast.S
@@ -199,7 +199,7 @@ LOCAL_LABEL(ArraySizeOverflow):
         // Delegate overflow handling to the generic helper conservatively
 
         li.w  $a2, (0x40000000 / 8) // sizeof(void*)
-        bgeu  $a1, $a2, C_FUNC(RhpNewVariableSizeObject)
+        bgeu  $a1, $a2, C_FUNC(RhpNewArrayFast)
 
         // In this case we know the element size is sizeof(void *), or 8 for arm64
         // This helps us in two ways - we can shift instead of multiplying, and

src/coreclr/runtime/riscv64/AllocFast.S
@@ -233,7 +233,7 @@ LOCAL_LABEL(ArraySizeOverflow):
         // Delegate overflow handling to the generic helper conservatively
 
         li          t0, (0x40000000 / 8) // sizeof(void*)
-        bgeu        a1, t0, C_FUNC(RhpNewVariableSizeObject)
+        bgeu        a1, t0, C_FUNC(RhpNewArrayFast)
 
         // In this case we know the element size is sizeof(void *), or 8 for arm64
         // This helps us in two ways - we can shift instead of multiplying, and