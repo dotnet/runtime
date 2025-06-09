46cd22b6a5eec45c0b426d9b0cd2fdaece8a6483
Jeremy Koritzinsky <jekoritz@microsoft.com>
2025-06-09 13:47:39 -0700

    Add support in the illink analyzer test host for no-body classes and add support in the analyzer for the type map types (it's pretty small and would have been 90% of the work to gracefully not handle it).

--- src/tools/illink/test/Mono.Linker.Tests.Cases/Reflection/TypeMap.cs
+++ src/tools/illink/test/Mono.Linker.Tests.Cases/Reflection/TypeMap.cs
@@ -22,7 +22,7 @@ using Mono.Linker.Tests.Cases.Reflection;
 namespace Mono.Linker.Tests.Cases.Reflection
 {
 	[Kept]
-	[IgnoreTestCase("Trimmer support is currently not implemented", IgnoredBy = Tool.Trimmer | Tool.Analyzer)]
+	[IgnoreTestCase("Trimmer support is currently not implemented", IgnoredBy = Tool.Trimmer)]
 	class TypeMap
 	{
 		[Kept]
