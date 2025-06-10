e45f726010101ec5017f9b344358978aa564b89b
Radek Doulik <radek.doulik@gmail.com>
2025-06-10 18:30:37 +0200

    Enable interpreter in all configuration on wasm
    Fix build

--- src/coreclr/System.Private.CoreLib/src/System/Runtime/ExceptionServices/AsmOffsets.cs
+++ src/coreclr/System.Private.CoreLib/src/System/Runtime/ExceptionServices/AsmOffsets.cs
@@ -144,9 +144,9 @@ class AsmOffsets
     public const int OFFSETOF__StackFrameIterator__m_AdjustedControlPC = 0x3c8;
 #else // TARGET_64BIT
     public const int OFFSETOF__REGDISPLAY__m_pCurrentContext = 0x4;
-    public const int SIZEOF__StackFrameIterator = 0xc4;
+    public const int SIZEOF__StackFrameIterator = 0xd4;
     public const int OFFSETOF__StackFrameIterator__m_isRuntimeWrappedExceptions = 0xb2;
-    public const int OFFSETOF__StackFrameIterator__m_AdjustedControlPC = 0xc0;
+    public const int OFFSETOF__StackFrameIterator__m_AdjustedControlPC = 0xd0;
 #endif // TARGET_64BIT
 
 #endif // DEBUG
