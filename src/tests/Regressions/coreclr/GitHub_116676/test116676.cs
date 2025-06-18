56e9b31c33c5f30f19ea29d54aee05fe9a7f195c
Jan Vorlicek <janvorli@microsoft.com>
2025-06-18 16:05:42 +0200

    Modify the regression test to not to print to console

--- src/tests/Regressions/coreclr/GitHub_116676/test116676.cs
+++ src/tests/Regressions/coreclr/GitHub_116676/test116676.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 using System;
+using System.Threading;
 using System.Runtime.CompilerServices;
 using Xunit;
 
@@ -19,9 +20,9 @@ public class Test116676
     [Fact]
     public static void TestEntryPoint()
     {
-        for (int i = 0; i < 1_000_000; i++)
+        for (int i = 0; i < 200; i++)
         {
-            Console.WriteLine(i);
+            Thread.Sleep(10);
             Work(false);
         }
     }
