143ea2eb97b3571ea18cdfa2ff98ffbe8f674d31
Jan Vorlicek <janvorli@microsoft.com>
2025-06-18 14:38:47 +0200

    Make the regression test actually work
    It needs to be compiled as optimized and the Console.WriteLine is needed to repro
    the issue.

--- src/tests/Regressions/coreclr/GitHub_116676/test116676.cs
+++ src/tests/Regressions/coreclr/GitHub_116676/test116676.cs
@@ -1,5 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
+using System;
 using System.Runtime.CompilerServices;
 using Xunit;
 
@@ -20,6 +21,7 @@ public class Test116676
     {
         for (int i = 0; i < 1_000_000; i++)
         {
+            Console.WriteLine(i);
             Work(false);
         }
     }
