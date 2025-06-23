f9c1c662cedd58b6577fb4bffe9d84c6a2839abd
Mike Hudson <mikehudsonuk@icloud.com>
2025-06-23 11:58:50 +0100

    use L for signature mapping of pointers
    conditional on WasmEnableMemory64

--- src/tasks/WasmAppBuilder/SignatureMapper.cs
+++ src/tasks/WasmAppBuilder/SignatureMapper.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
@@ -38,8 +38,13 @@ internal static class SignatureMapper
                 nameof(Single) => 'F',
                 nameof(Double) => 'D',
                 // FIXME: These will need to be L for wasm64
+#if SignatureMappingWasm64
+                nameof(IntPtr) => 'L',
+                nameof(UIntPtr) => 'L',
+#else
                 nameof(IntPtr) => 'I',
                 nameof(UIntPtr) => 'I',
+#endif
                 "Void" => 'V',
                 _ => null
             };
