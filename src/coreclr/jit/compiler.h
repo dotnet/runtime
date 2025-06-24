5b8b862ebc8148df05236814268002f9324ed062
DeepakRajendrakumaran <deepak.rajendrakumaran@intel.com>
2025-06-23 22:33:39 -0700

    Adding push2/pop2 (#116035)
    * Add APX knob
    
    * Adding push2/pop2 + ppx.
    
    * Update unwind logic
    
    * Clean up + Revert "Add APX knob"
    
    * Addressing review comments.
    
    * Formatting + Review changes

--- src/coreclr/jit/compiler.h
+++ src/coreclr/jit/compiler.h
@@ -8792,6 +8792,9 @@ public:
     //
 
     void unwindPush(regNumber reg);
+#if defined(TARGET_AMD64)
+    void unwindPush2(regNumber reg1, regNumber reg2);
+#endif // TARGET_AMD64
     void unwindAllocStack(unsigned size);
     void unwindSetFrameReg(regNumber reg, unsigned offset);
     void unwindSaveReg(regNumber reg, unsigned offset);
@@ -8864,6 +8867,7 @@ private:
 
     void unwindBegPrologWindows();
     void unwindPushWindows(regNumber reg);
+    void unwindPush2Windows(regNumber reg1, regNumber reg2);
     void unwindAllocStackWindows(unsigned size);
     void unwindSetFrameRegWindows(regNumber reg, unsigned offset);
     void unwindSaveRegWindows(regNumber reg, unsigned offset);
@@ -8882,6 +8886,7 @@ private:
     short mapRegNumToDwarfReg(regNumber reg);
     void  createCfiCode(FuncInfoDsc* func, UNATIVE_OFFSET codeOffset, UCHAR opcode, short dwarfReg, INT offset = 0);
     void  unwindPushPopCFI(regNumber reg);
+    void  unwindPush2Pop2CFI(regNumber reg1, regNumber reg2);
     void  unwindBegPrologCFI();
     void  unwindPushPopMaskCFI(regMaskTP regMask, bool isFloat);
     void  unwindAllocStackCFI(unsigned size);
