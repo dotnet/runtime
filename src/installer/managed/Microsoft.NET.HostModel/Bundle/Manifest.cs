443e84189fe8b23971f56927f2dc5a6dc702235f
Jackson Schuster <36744439+jtschuster@users.noreply.github.com>
2025-06-24 13:35:08 -0700

    Actually change the length of BundleID to 12

--- src/installer/managed/Microsoft.NET.HostModel/Bundle/Manifest.cs
+++ src/installer/managed/Microsoft.NET.HostModel/Bundle/Manifest.cs
@@ -68,7 +68,7 @@ namespace Microsoft.NET.HostModel.Bundle
         // with path-names so that the AppHost can use it in
         // extraction path.
         public string BundleID { get; private set; }
-        private const int BundleIdLength = 32;
+        private const int BundleIdLength = 12;
         private SHA256 bundleHash = SHA256.Create();
         public readonly uint BundleMajorVersion;
         // The Minor version is currently unused, and is always zero
