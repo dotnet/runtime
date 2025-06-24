12494546da528a240c338a776b620f7b5fe53118
Kunal Pathak <Kunal.Pathak@microsoft.com>
2025-06-24 15:47:02 -0700

    Use LSB of vector to convert to mask

--- src/coreclr/jit/lsraarm64.cpp
+++ src/coreclr/jit/lsraarm64.cpp
@@ -1400,6 +1400,12 @@ int LinearScan::BuildHWIntrinsic(GenTreeHWIntrinsic* intrinsicTree, int* pDstCou
         delayFreeOp = getDelayFreeOperand(embeddedOp, /* embedded */ true);
     }
 
+    if (intrin.id == NI_Sve_ConvertVectorToMask)
+    {
+        // Need an extra temp to test LSB of source vector
+        buildInternalFloatRegisterDefForNode(intrinsicTree);
+    }
+
     // Build any immediates
     BuildHWIntrinsicImmediate(intrinsicTree, intrin);
 
