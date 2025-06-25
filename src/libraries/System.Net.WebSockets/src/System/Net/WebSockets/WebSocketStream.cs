1fd4d7057686e68349bf41e59fa46fd82ad1e0cd
Natalia Kondratyeva <knatalia@microsoft.com>
2025-06-25 18:56:27 +0200

    Apply suggestions from code review

--- src/libraries/System.Net.WebSockets/src/System/Net/WebSockets/WebSocketStream.cs
+++ src/libraries/System.Net.WebSockets/src/System/Net/WebSockets/WebSocketStream.cs
@@ -372,6 +372,10 @@ namespace System.Net.WebSockets
             public override ValueTask DisposeAsync()
             {
                 _disposed = true;
+                if (!_eof && WebSocket.State < WebSocketState.CloseReceived)
+                {
+                    WebSocket.Abort();
+                }
                 return default;
             }
         }
