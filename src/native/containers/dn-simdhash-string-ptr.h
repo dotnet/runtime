2ccdbbec3dbd95e9f77504410b971adf3f06223d
David Wrighton <davidwr@microsoft.com>
2025-06-26 10:55:34 -0700

    Update src/native/containers/dn-simdhash-string-ptr.h
    Co-authored-by: Adeel Mujahid <3840695+am11@users.noreply.github.com>

--- src/native/containers/dn-simdhash-string-ptr.h
+++ src/native/containers/dn-simdhash-string-ptr.h
@@ -1,3 +1,6 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 #ifdef __cplusplus
 extern "C" {
 #endif // __cplusplus
