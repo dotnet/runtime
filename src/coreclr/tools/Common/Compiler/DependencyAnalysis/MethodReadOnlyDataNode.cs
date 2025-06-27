b6cc6625b3ef67ae31d09609a5ac9cde8c43b6b2
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2025-06-27 12:05:19 +0200

    Update MethodReadOnlyDataNode.cs

--- src/coreclr/tools/Common/Compiler/DependencyAnalysis/MethodReadOnlyDataNode.cs
+++ src/coreclr/tools/Common/Compiler/DependencyAnalysis/MethodReadOnlyDataNode.cs
@@ -18,11 +18,13 @@ namespace ILCompiler.DependencyAnalysis
             _owningMethod = owningMethod;
         }
 
+#if !READYTORUN
         public override bool ShouldSkipEmittingObjectNode(NodeFactory factory)
         {
             IMethodNode owningBody = factory.MethodEntrypoint(_owningMethod);
             return factory.ObjectInterner.GetDeduplicatedSymbol(factory, owningBody) != owningBody;
         }
+#endif
 
         public override ObjectNodeSection GetSection(NodeFactory factory) => ObjectNodeSection.ReadOnlyDataSection;
         public override bool StaticDependenciesAreComputed => _data != null;
