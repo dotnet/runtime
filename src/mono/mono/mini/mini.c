dotnet/runtime

commit 0473604548af392de993dc772a3938ae50436231

[browser] samplepoint instrumentation into Mono profiler (#112352)

src/mono/mono/mini/mini.c
@@ -2753,6 +2753,47 @@ insert_safepoint (MonoCompile *cfg, MonoBasicBlock *bblock)
 	}
 }
 
+static bool
+skip_insert_safepoint (MonoCompile *cfg)
+{
+	if (cfg->method->wrapper_type == MONO_WRAPPER_MANAGED_TO_NATIVE) {
+		WrapperInfo *info = mono_marshal_get_wrapper_info (cfg->method);
+		/* These wrappers are called from the wrapper for the polling function, leading to potential stack overflow */
+		if (info && info->subtype == WRAPPER_SUBTYPE_ICALL_WRAPPER &&
+				(info->d.icall.jit_icall_id == MONO_JIT_ICALL_mono_threads_state_poll ||
+				 info->d.icall.jit_icall_id == MONO_JIT_ICALL_mono_thread_interruption_checkpoint ||
+				 info->d.icall.jit_icall_id == MONO_JIT_ICALL_mono_threads_exit_gc_safe_region_unbalanced)) {
+			if (cfg->verbose_level > 1)
+				printf ("SKIPPING SAFEPOINTS for the polling function icall\n");
+			return TRUE;
+		}
+	}
+
+	if (cfg->method->wrapper_type == MONO_WRAPPER_NATIVE_TO_MANAGED) {
+		if (cfg->verbose_level > 1)
+			printf ("SKIPPING SAFEPOINTS for native-to-managed wrappers.\n");
+		return TRUE;
+	}
+
+	if (cfg->method->wrapper_type == MONO_WRAPPER_OTHER) {
+		WrapperInfo *info = mono_marshal_get_wrapper_info (cfg->method);
+
+		if (info && (info->subtype == WRAPPER_SUBTYPE_INTERP_IN || info->subtype == WRAPPER_SUBTYPE_INTERP_LMF)) {
+			/* These wrappers shouldn't do any icalls */
+			if (cfg->verbose_level > 1)
+				printf ("SKIPPING SAFEPOINTS for interp-in wrappers.\n");
+			return TRUE;
+		}
+	}
+
+	if (cfg->method->wrapper_type == MONO_WRAPPER_WRITE_BARRIER) {
+		if (cfg->verbose_level > 1)
+			printf ("SKIPPING SAFEPOINTS for write barrier wrappers.\n");
+		return TRUE;
+	}
+	return FALSE;
+}
+
 /*
 This code inserts safepoints into managed code at important code paths.
 Those are:
@@ -2778,39 +2819,7 @@ insert_safepoints (MonoCompile *cfg)
 		}
 	}
 
-	if (cfg->method->wrapper_type == MONO_WRAPPER_MANAGED_TO_NATIVE) {
-		WrapperInfo *info = mono_marshal_get_wrapper_info (cfg->method);
-		/* These wrappers are called from the wrapper for the polling function, leading to potential stack overflow */
-		if (info && info->subtype == WRAPPER_SUBTYPE_ICALL_WRAPPER &&
-				(info->d.icall.jit_icall_id == MONO_JIT_ICALL_mono_threads_state_poll ||
-				 info->d.icall.jit_icall_id == MONO_JIT_ICALL_mono_thread_interruption_checkpoint ||
-				 info->d.icall.jit_icall_id == MONO_JIT_ICALL_mono_threads_exit_gc_safe_region_unbalanced)) {
-			if (cfg->verbose_level > 1)
-				printf ("SKIPPING SAFEPOINTS for the polling function icall\n");
-			return;
-		}
-	}
-
-	if (cfg->method->wrapper_type == MONO_WRAPPER_NATIVE_TO_MANAGED) {
-		if (cfg->verbose_level > 1)
-			printf ("SKIPPING SAFEPOINTS for native-to-managed wrappers.\n");
-		return;
-	}
-
-	if (cfg->method->wrapper_type == MONO_WRAPPER_OTHER) {
-		WrapperInfo *info = mono_marshal_get_wrapper_info (cfg->method);
-
-		if (info && (info->subtype == WRAPPER_SUBTYPE_INTERP_IN || info->subtype == WRAPPER_SUBTYPE_INTERP_LMF)) {
-			/* These wrappers shouldn't do any icalls */
-			if (cfg->verbose_level > 1)
-				printf ("SKIPPING SAFEPOINTS for interp-in wrappers.\n");
-			return;
-		}
-	}
-
-	if (cfg->method->wrapper_type == MONO_WRAPPER_WRITE_BARRIER) {
-		if (cfg->verbose_level > 1)
-			printf ("SKIPPING SAFEPOINTS for write barrier wrappers.\n");
+	if (skip_insert_safepoint (cfg)) {
 		return;
 	}
 
@@ -2840,6 +2849,62 @@ insert_safepoints (MonoCompile *cfg)
 
 }
 
+static void
+insert_samplepoint (MonoCompile *cfg, MonoBasicBlock *bblock)
+{
+	if (cfg->verbose_level > 1)
+		printf ("ADDING SAMPLE POINT TO BB%d\n", bblock->block_num);
+
+	// store the previous instruction list and make the bb empty
+	MonoInst *begin = bblock->code;
+	MonoInst *end = bblock->last_ins;
+	bblock->code = bblock->last_ins = NULL;
+
+	// insert the samplepoint at the start of the bb
+	MonoBasicBlock *prev_cbb = cfg->cbb;
+	cfg->cbb = bblock;
+	mini_profiler_emit_samplepoint (cfg);
+	cfg->cbb = prev_cbb;
+	
+	// append the previous instruction list to the end of the bb
+	if (begin) {
+		if(bblock->code) {
+			begin->prev = bblock->last_ins;
+			bblock->last_ins->next = begin;
+			bblock->last_ins = end;
+		} else {
+			bblock->code = begin;
+			bblock->last_ins = end;
+		}
+	}
+}
+
+static void
+insert_samplepoints (MonoCompile *cfg)
+{
+	MonoBasicBlock *bb;
+
+	if (skip_insert_safepoint (cfg)) {
+		return;
+	}
+
+	if (cfg->verbose_level > 1)
+		printf ("INSERTING SAMPLEPOINTS\n");
+	if (cfg->verbose_level > 2)
+		mono_print_code (cfg, "BEFORE SAMPLEPOINTS");
+
+	for (bb = cfg->bb_entry->next_bb; bb; bb = bb->next_bb) {
+		if (bb->loop_body_start || (bb->flags & BB_EXCEPTION_HANDLER)) {
+			insert_samplepoint (cfg, bb);
+		}
+	}
+
+	// we don't need samplepoint event on method entry, there is already a method entry event
+
+	if (cfg->verbose_level > 2)
+		mono_print_code (cfg, "AFTER SAMPLEPOINTS");
+
+}
 
 static void
 mono_insert_branches_between_bblocks (MonoCompile *cfg)
@@ -3391,7 +3456,8 @@ mini_method_compile (MonoMethod *method, guint32 opts, JitFlags flags, int parts
 	if (trace)
 		cfg->prof_flags = (MonoProfilerCallInstrumentationFlags)(
 			MONO_PROFILER_CALL_INSTRUMENTATION_ENTER | MONO_PROFILER_CALL_INSTRUMENTATION_ENTER_CONTEXT |
-			MONO_PROFILER_CALL_INSTRUMENTATION_LEAVE | MONO_PROFILER_CALL_INSTRUMENTATION_LEAVE_CONTEXT);
+			MONO_PROFILER_CALL_INSTRUMENTATION_LEAVE | MONO_PROFILER_CALL_INSTRUMENTATION_LEAVE_CONTEXT | 
+			MONO_PROFILER_CALL_INSTRUMENTATION_SAMPLEPOINT | MONO_PROFILER_CALL_INSTRUMENTATION_SAMPLEPOINT_CONTEXT);
 
 	/* The debugger has no liveness information, so avoid sharing registers/stack slots */
 	if (mini_debug_options.mdb_optimizations || MONO_CFG_PROFILE_CALL_CONTEXT (cfg)) {
@@ -3682,6 +3748,11 @@ mini_method_compile (MonoMethod *method, guint32 opts, JitFlags flags, int parts
 	if (mono_threads_are_safepoints_enabled ()) {
 		MONO_TIME_TRACK (mono_jit_stats.jit_insert_safepoints, insert_safepoints (cfg));
 		mono_cfg_dump_ir (cfg, "insert_safepoints");
+	}
+
+	if (MONO_CFG_PROFILE (cfg, SAMPLEPOINT) || MONO_CFG_PROFILE (cfg, SAMPLEPOINT_CONTEXT)) {
+		MONO_TIME_TRACK (mono_jit_stats.jit_insert_samplepoints, insert_samplepoints (cfg));
+		mono_cfg_dump_ir (cfg, "insert_samplepoints");
 	}
 
 	/* after method_to_ir */
@@ -4379,6 +4450,7 @@ mini_jit_init (void)
 	mono_counters_register ("JIT/compile_dominator_info", MONO_COUNTER_JIT | MONO_COUNTER_LONG | MONO_COUNTER_TIME, &mono_jit_stats.jit_compile_dominator_info);
 	mono_counters_register ("JIT/compute_natural_loops", MONO_COUNTER_JIT | MONO_COUNTER_LONG | MONO_COUNTER_TIME, &mono_jit_stats.jit_compute_natural_loops);
 	mono_counters_register ("JIT/insert_safepoints", MONO_COUNTER_JIT | MONO_COUNTER_LONG | MONO_COUNTER_TIME, &mono_jit_stats.jit_insert_safepoints);
+	mono_counters_register ("JIT/insert_samplepoints", MONO_COUNTER_JIT | MONO_COUNTER_LONG | MONO_COUNTER_TIME, &mono_jit_stats.jit_insert_samplepoints);
 	mono_counters_register ("JIT/ssa_compute", MONO_COUNTER_JIT | MONO_COUNTER_LONG | MONO_COUNTER_TIME, &mono_jit_stats.jit_ssa_compute);
 	mono_counters_register ("JIT/ssa_cprop", MONO_COUNTER_JIT | MONO_COUNTER_LONG | MONO_COUNTER_TIME, &mono_jit_stats.jit_ssa_cprop);
 	mono_counters_register ("JIT/ssa_deadce", MONO_COUNTER_JIT | MONO_COUNTER_LONG | MONO_COUNTER_TIME, &mono_jit_stats.jit_ssa_deadce);

src/mono/browser/runtime/cwraps.ts
@@ -63,7 +63,6 @@ const fn_signatures: SigLine[] = [
     [() => !runtimeHelpers.emscriptenBuildOptions.enableAotProfiler, "mono_wasm_profiler_init_aot", "void", ["string"]],
     [() => !runtimeHelpers.emscriptenBuildOptions.enableBrowserProfiler, "mono_wasm_profiler_init_browser", "void", ["string"]],
     [() => !runtimeHelpers.emscriptenBuildOptions.enableLogProfiler, "mono_wasm_profiler_init_log", "void", ["string"]],
-    [true, "mono_wasm_profiler_init_browser", "void", ["number"]],
     [false, "mono_wasm_exec_regression", "number", ["number", "string"]],
     [false, "mono_wasm_invoke_jsexport", "void", ["number", "number"]],
     [true, "mono_wasm_write_managed_pointer_unsafe", "void", ["number", "number"]],
@@ -73,6 +72,7 @@ const fn_signatures: SigLine[] = [
     [true, "mono_wasm_f64_to_i52", "number", ["number", "number"]],
     [true, "mono_wasm_f64_to_u52", "number", ["number", "number"]],
     [true, "mono_wasm_method_get_name", "number", ["number"]],
+    [true, "mono_wasm_method_get_name_ex", "number", ["number"]],
     [true, "mono_wasm_method_get_full_name", "number", ["number"]],
     [true, "mono_wasm_gc_lock", "void", []],
     [true, "mono_wasm_gc_unlock", "void", []],
@@ -131,6 +131,9 @@ const fn_signatures: SigLine[] = [
     [true, "mono_jiterp_end_catch", "void", []],
     [true, "mono_interp_pgo_load_table", "number", ["number", "number"]],
     [true, "mono_interp_pgo_save_table", "number", ["number", "number"]],
+    [() => !runtimeHelpers.emscriptenBuildOptions.enablePerfTracing, "mono_jiterp_prof_enter", "void", ["number", "number"]],
+    [() => !runtimeHelpers.emscriptenBuildOptions.enablePerfTracing, "mono_jiterp_prof_samplepoint", "void", ["number", "number"]],
+    [() => !runtimeHelpers.emscriptenBuildOptions.enablePerfTracing, "mono_jiterp_prof_leave", "void", ["number", "number"]],
 
     ...threading_cwraps,
 ];
@@ -193,6 +196,7 @@ export interface t_Cwraps {
     mono_wasm_f64_to_i52(destination: VoidPtr, value: number): I52Error;
     mono_wasm_f64_to_u52(destination: VoidPtr, value: number): I52Error;
     mono_wasm_method_get_name(method: MonoMethod): CharPtr;
+    mono_wasm_method_get_name_ex(method: MonoMethod): CharPtr;
     mono_wasm_method_get_full_name(method: MonoMethod): CharPtr;
     mono_wasm_gc_lock(): void;
     mono_wasm_gc_unlock(): void;

src/mono/browser/runtime/driver.c
@@ -534,6 +534,22 @@ EMSCRIPTEN_KEEPALIVE const char * mono_wasm_method_get_name (MonoMethod *method)
 	return res;
 }
 
+EMSCRIPTEN_KEEPALIVE const char * mono_wasm_method_get_name_ex (MonoMethod *method) {
+	const char *res;
+	MONO_ENTER_GC_UNSAFE;
+	res = mono_method_get_name (method);
+	// starts with .ctor or .cctor
+	if (mono_method_get_flags (method, NULL) & 0x0800 /* METHOD_ATTRIBUTE_SPECIAL_NAME */ && strlen (res) < 7) {
+		char *res_ex = (char *) malloc (128);
+		snprintf (res_ex, 128,"%s.%s", mono_class_get_name (mono_method_get_class (method)), res);
+		res = res_ex;
+	} else {
+		res = strdup (res);
+	}
+	MONO_EXIT_GC_UNSAFE;
+	return res;
+}
+
 EMSCRIPTEN_KEEPALIVE float mono_wasm_get_f32_unaligned (const float *src) {
 	return *src;
 }

src/mono/browser/runtime/es6/dotnet.es6.lib.js
@@ -90,6 +90,7 @@ function injectDependencies() {
         `enablePerfTracing: ${FEATURE_PERFTRACING ? "true" : "false"}, ` +
         `enableAotProfiler: ${ENABLE_AOT_PROFILER ? "true" : "false"}, ` +
         `enableBrowserProfiler: ${ENABLE_BROWSER_PROFILER ? "true" : "false"}, ` +
+        `enablePerfTracing: ${ENABLE_BROWSER_PROFILER ? "true" : "false"}, ` +
         `enableLogProfiler: ${ENABLE_LOG_PROFILER ? "true" : "false"}, ` +
         `runAOTCompilation: ${RUN_AOT_COMPILATION ? "true" : "false"}, ` +
         `wasmEnableThreads: ${USE_PTHREADS ? "true" : "false"}, ` +

src/mono/browser/runtime/exports-binding.ts
@@ -6,14 +6,14 @@ import WasmEnableThreads from "consts:wasmEnableThreads";
 import { mono_wasm_debugger_log, mono_wasm_add_dbg_command_received, mono_wasm_set_entrypoint_breakpoint, mono_wasm_fire_debugger_agent_message_with_data, mono_wasm_fire_debugger_agent_message_with_data_to_pause } from "./debug";
 import { mono_wasm_release_cs_owned_object } from "./gc-handles";
 import { mono_wasm_bind_js_import_ST, mono_wasm_invoke_js_function, mono_wasm_invoke_jsimport_MT, mono_wasm_invoke_jsimport_ST } from "./invoke-js";
-import { mono_interp_tier_prepare_jiterpreter, mono_jiterp_free_method_data_js } from "./jiterpreter";
+import { mono_interp_tier_prepare_jiterpreter, mono_wasm_free_method_data } from "./jiterpreter";
 import { mono_interp_jit_wasm_entry_trampoline, mono_interp_record_interp_entry } from "./jiterpreter-interp-entry";
 import { mono_interp_jit_wasm_jit_call_trampoline, mono_interp_invoke_wasm_jit_call_trampoline, mono_interp_flush_jitcall_queue } from "./jiterpreter-jit-call";
 import { mono_wasm_resolve_or_reject_promise } from "./marshal-to-js";
 import { mono_wasm_schedule_timer, schedule_background_exec } from "./scheduling";
 import { mono_wasm_asm_loaded } from "./startup";
 import { mono_log_warn, mono_wasm_console_clear, mono_wasm_trace_logger } from "./logging";
-import { mono_wasm_profiler_leave, mono_wasm_profiler_enter, ds_rt_websocket_close, ds_rt_websocket_create, ds_rt_websocket_poll, ds_rt_websocket_recv, ds_rt_websocket_send } from "./profiler";
+import { mono_wasm_profiler_record, mono_wasm_profiler_now, ds_rt_websocket_close, ds_rt_websocket_create, ds_rt_websocket_poll, ds_rt_websocket_recv, ds_rt_websocket_send } from "./profiler";
 import { mono_wasm_browser_entropy } from "./crypto";
 import { mono_wasm_cancel_promise } from "./cancelable-promise";
 
@@ -68,10 +68,10 @@ export const mono_wasm_imports = [
     mono_interp_jit_wasm_jit_call_trampoline,
     mono_interp_invoke_wasm_jit_call_trampoline,
     mono_interp_flush_jitcall_queue,
-    mono_jiterp_free_method_data_js,
+    mono_wasm_free_method_data,
 
-    mono_wasm_profiler_enter,
-    mono_wasm_profiler_leave,
+    mono_wasm_profiler_now,
+    mono_wasm_profiler_record,
 
     // driver.c
     mono_wasm_trace_logger,

src/mono/browser/runtime/jiterpreter-support.ts
@@ -1600,6 +1600,27 @@ export const _now = (globalThis.performance && globalThis.performance.now)
 
 let scratchBuffer: NativePointer = <any>0;
 
+export function append_profiler_event (builder: WasmBuilder, ip: MintOpcodePtr, opcode: MintOpcode) {
+    let event_name:string;
+    switch (opcode) {
+        case MintOpcode.MINT_PROF_ENTER:
+            event_name = "prof_enter";
+            break;
+        case MintOpcode.MINT_PROF_SAMPLEPOINT:
+            event_name = "prof_samplepoint";
+            break;
+        case MintOpcode.MINT_PROF_EXIT:
+        case MintOpcode.MINT_PROF_EXIT_VOID:
+            event_name = "prof_leave";
+            break;
+        default:
+            throw new Error(`Unimplemented profiler event ${opcode}`);
+    }
+    builder.local("frame");
+    builder.i32_const(ip);
+    builder.callImport(event_name);
+}
+
 export function append_safepoint (builder: WasmBuilder, ip: MintOpcodePtr) {
     // safepoints are never triggered in a single-threaded build
     if (!WasmEnableThreads)

src/mono/browser/runtime/jiterpreter-trace-generator.ts
@@ -28,6 +28,7 @@ import {
     try_append_memmove_fast, getOpcodeTableValue,
     getMemberOffset, isZeroPageReserved, CfgBranchType,
     append_safepoint, modifyCounter, simdFallbackCounters,
+    append_profiler_event,
 } from "./jiterpreter-support";
 import {
     sizeOfDataItem, sizeOfV128, sizeOfStackval,
@@ -1415,9 +1416,15 @@ export function generateWasmBody (
             }
 
             case MintOpcode.MINT_RETHROW:
+                ip = abort;
+                break;
+
+            // call C
+            case MintOpcode.MINT_PROF_ENTER:
+            case MintOpcode.MINT_PROF_SAMPLEPOINT:
             case MintOpcode.MINT_PROF_EXIT:
             case MintOpcode.MINT_PROF_EXIT_VOID:
-                ip = abort;
+                append_profiler_event(builder, ip, opcode);
                 break;
 
             // Generating code for these is kind of complex due to the intersection of JS and int64,

src/mono/browser/runtime/jiterpreter.ts
@@ -26,6 +26,7 @@ import { mono_jiterp_free_method_data_interp_entry } from "./jiterpreter-interp-
 import { mono_jiterp_free_method_data_jit_call } from "./jiterpreter-jit-call";
 import { mono_log_error, mono_log_info, mono_log_warn } from "./logging";
 import { utf8ToString } from "./strings";
+import { mono_wasm_profiler_free_method } from "./profiler";
 
 // Controls miscellaneous diagnostic output.
 export const trace = 0;
@@ -297,6 +298,12 @@ function getTraceImports () {
 
     if (nullCheckValidation)
         traceImports.push(importDef("notnull", assert_not_null));
+
+    if (runtimeHelpers.emscriptenBuildOptions.enablePerfTracing) {
+        traceImports.push(importDef("prof_enter", getRawCwrap("mono_jiterp_prof_enter")));
+        traceImports.push(importDef("prof_samplepoint", getRawCwrap("mono_jiterp_prof_samplepoint")));
+        traceImports.push(importDef("prof_leave", getRawCwrap("mono_jiterp_prof_leave")));
+    }
 
     const pushMathOps = (list: string[], type: string) => {
         for (let i = 0; i < list.length; i++) {
@@ -573,6 +580,30 @@ function initialize_builder (builder: WasmBuilder) {
     );
     builder.defineType(
         "safepoint",
+        {
+            "frame": WasmValtype.i32,
+            "ip": WasmValtype.i32,
+        },
+        WasmValtype.void, true
+    );
+    builder.defineType(
+        "prof_enter",
+        {
+            "frame": WasmValtype.i32,
+            "ip": WasmValtype.i32,
+        },
+        WasmValtype.void, true
+    );
+    builder.defineType(
+        "prof_samplepoint",
+        {
+            "frame": WasmValtype.i32,
+            "ip": WasmValtype.i32,
+        },
+        WasmValtype.void, true
+    );
+    builder.defineType(
+        "prof_leave",
         {
             "frame": WasmValtype.i32,
             "ip": WasmValtype.i32,
@@ -1050,9 +1081,13 @@ export function mono_interp_tier_prepare_jiterpreter (
 
 // NOTE: This will potentially be called once for every trace entry point
 //  in a given method, not just once per method
-export function mono_jiterp_free_method_data_js (
+export function mono_wasm_free_method_data (
     method: MonoMethod, imethod: number, traceIndex: number
 ) {
+    if (runtimeHelpers.emscriptenBuildOptions.enablePerfTracing) {
+        mono_wasm_profiler_free_method(method);
+    }
+
     // TODO: Uninstall the trace function pointer from the function pointer table,
     //  so that the compiled trace module can be freed by the browser eventually
     // Release the trace info object, if present

commit 857aff0e8a039926ec91a85020d41c874012a019

regression caused by https://github.com/dotnet/runtime/pull/112352

src/mono/mono/mini/mini.c
@@ -3456,8 +3456,7 @@ mini_method_compile (MonoMethod *method, guint32 opts, JitFlags flags, int parts
 	if (trace)
 		cfg->prof_flags = (MonoProfilerCallInstrumentationFlags)(
 			MONO_PROFILER_CALL_INSTRUMENTATION_ENTER | MONO_PROFILER_CALL_INSTRUMENTATION_ENTER_CONTEXT |
-			MONO_PROFILER_CALL_INSTRUMENTATION_LEAVE | MONO_PROFILER_CALL_INSTRUMENTATION_LEAVE_CONTEXT | 
-			MONO_PROFILER_CALL_INSTRUMENTATION_SAMPLEPOINT | MONO_PROFILER_CALL_INSTRUMENTATION_SAMPLEPOINT_CONTEXT);
+			MONO_PROFILER_CALL_INSTRUMENTATION_LEAVE | MONO_PROFILER_CALL_INSTRUMENTATION_LEAVE_CONTEXT );
 
 	/* The debugger has no liveness information, so avoid sharing registers/stack slots */
 	if (mini_debug_options.mdb_optimizations || MONO_CFG_PROFILE_CALL_CONTEXT (cfg)) {