b364e9de4687b9b38bd43dd03e462ffee2a4a0d7
Natalia Kondratyeva <knatalia@microsoft.com>
2025-07-04 14:36:07 +0100

    Fix browser build

--- src/libraries/System.Net.WebSockets.Client/tests/LoopbackServer/LoopbackWebSocketServer.cs
+++ src/libraries/System.Net.WebSockets.Client/tests/LoopbackServer/LoopbackWebSocketServer.cs
@@ -1,10 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.IO;
 using System.Net.Http;
 using System.Net.Test.Common;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
