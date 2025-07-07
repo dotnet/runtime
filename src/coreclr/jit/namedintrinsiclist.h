278670bc677b28ebf17b593c0ba4873b1b154edc
Andy Ayers <andya@microsoft.com>
2025-07-07 09:20:36 -0700

    JIT: relax another check in conditional escape analysis (#117295)
    Proceed with CEA cloning even when the allocation site dominates the assignment
    to the enumerator var. The cloning will be a bit wasteful as the original code will
    become unreachable, but the code is set up to specialize the clone and so this is
    the less risky fix.
    
    Also restore recognition of `IEnumerable<T>.GetEnumerator` as this gives a useful
    inlining boost in some cases.
    
    Fixes #117204 cases that were not fixed by #117222.

--- src/coreclr/jit/namedintrinsiclist.h
+++ src/coreclr/jit/namedintrinsiclist.h
@@ -255,10 +255,11 @@ enum NamedIntrinsic : unsigned short
     NI_PRIMITIVE_END,
 
     //
-    // Array Intrinsics
+    // Enumeration Intrinsics
     //
     NI_System_SZArrayHelper_GetEnumerator,
     NI_System_Array_T_GetEnumerator,
+    NI_System_Collections_Generic_IEnumerable_GetEnumerator,
 };
 
 #endif // _NAMEDINTRINSICLIST_H_
