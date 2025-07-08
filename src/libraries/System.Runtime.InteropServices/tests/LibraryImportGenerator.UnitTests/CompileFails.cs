7f916d92ed04e16d5d57dcd5092828bd81ac53ba
Jackson Schuster <36744439+jtschuster@users.noreply.github.com>
2025-07-08 16:37:00 -0700

    Update src/libraries/System.Runtime.InteropServices/tests/LibraryImportGenerator.UnitTests/CompileFails.cs
    Co-authored-by: Aaron Robinson <arobins@microsoft.com>

--- src/libraries/System.Runtime.InteropServices/tests/LibraryImportGenerator.UnitTests/CompileFails.cs
+++ src/libraries/System.Runtime.InteropServices/tests/LibraryImportGenerator.UnitTests/CompileFails.cs
@@ -57,7 +57,7 @@ namespace LibraryImportGenerator.UnitTests
             // Not LibraryImportAttribute
             yield return new object[] { ID(), CodeSnippets.UserDefinedPrefixedAttributes, Array.Empty<DiagnosticResult>() };
 
-            // Bug: crashes
+            // Bug: https://github.com/dotnet/runtime/issues/117448
             // yield return new[] { ID(), CodeSnippets.ImproperCollectionWithMarshalUsingOnElements };
 
             // No explicit marshalling for char or string
