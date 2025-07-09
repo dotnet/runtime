9900226cb6682b361126faf808fa51c33118d596
Gordon Ross <gordon.w.ross@gmail.com>
2025-07-09 16:30:48 -0400

    Testing a null change -- please ignore

--- src/libraries/System.Private.CoreLib/src/System/Environment.SunOS.cs
+++ src/libraries/System.Private.CoreLib/src/System/Environment.SunOS.cs
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+// testing a comment-only (null) change
+
 using System.Runtime.InteropServices;
 
 namespace System
