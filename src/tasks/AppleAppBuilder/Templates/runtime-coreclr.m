b9bb96e48312540d57ebf72c0df9ef19bf9f9408
Milos Kotlar <kotlarmilos@gmail.com>
2025-07-11 14:33:06 +0200

    Update executable name placeholder in runtime-coreclr.m

--- src/tasks/AppleAppBuilder/Templates/runtime-coreclr.m
+++ src/tasks/AppleAppBuilder/Templates/runtime-coreclr.m
@@ -112,7 +112,7 @@ mono_ios_runtime_init (void)
     res = snprintf (icu_dat_path, sizeof (icu_dat_path) - 1, "%s/%s", bundle, "icudt.dat");
 #endif
     assert (res > 0);
-    
+
     char pinvoke_override_addr [16];
     sprintf (pinvoke_override_addr, "%p", &pinvoke_override);
 
@@ -136,7 +136,7 @@ mono_ios_runtime_init (void)
 #endif
     };
 
-    const char* executable = "Program.dll";
+    const char* executable = "%EntryPointLibName%";
     const char *executablePath = [[[[NSBundle mainBundle] executableURL] path] UTF8String];
     unsigned int coreclr_domainId = 0;
     void *coreclr_handle = NULL;
