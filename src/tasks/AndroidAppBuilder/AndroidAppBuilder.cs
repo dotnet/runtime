0e7dbf3aaeecb1c0fe73356a7aeb59a51da586ed
David Nguyen <87228593+davidnguyen-tech@users.noreply.github.com>
2025-07-11 10:28:40 +0000

    Create InvariantGlobalization property

--- src/tasks/AndroidAppBuilder/AndroidAppBuilder.cs
+++ src/tasks/AndroidAppBuilder/AndroidAppBuilder.cs
@@ -108,6 +108,11 @@ public class AndroidAppBuilderTask : Task
 
     public bool ForceInterpreter { get; set; }
 
+    /// <summary>
+    /// Indicates whether we want to use invariant globalization mode.
+    /// </summary>
+    public bool InvariantGlobalization { get; set; }
+
     [Output]
     public string ApkBundlePath { get; set; } = ""!;
 
