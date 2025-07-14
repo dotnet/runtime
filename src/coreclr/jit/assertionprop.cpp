d50f2f5bf928994f9a5b48dbb1bab0813b11744a
Egor Bogatov <egorbo@gmail.com>
2025-07-14 12:18:19 +0200

    Update assertionprop.cpp

--- src/coreclr/jit/assertionprop.cpp
+++ src/coreclr/jit/assertionprop.cpp
@@ -3045,7 +3045,7 @@ GenTree* Compiler::optVNBasedFoldConstExpr(BasicBlock* block, GenTree* parent, G
 
         // Were able to optimize.
         conValTree->gtVNPair = vnPair;
-        return gtWrapWithSideEffects(conValTree, tree, GTF_ALL_EFFECT);
+        return gtWrapWithSideEffects(conValTree, tree, GTF_SIDE_EFFECT);
     }
     else
     {
