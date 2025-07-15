6a78bfe32d10f7ece82de1810a0167b03c5c5cde
Jeremy Koritzinsky <jekoritz@microsoft.com>
2025-07-15 15:48:29 +0000

    Rename and split out the "unsupported" impl

---
+++ src/native/libs/System.Native/pal_mutex_unsupported.c
@@ -0,0 +1,68 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#include "pal_config.h"
+#include "pal_mutex.h"
+#include "pal_errno.h"
+
+#include <stdio.h>
+#include <limits.h>
+#include <sched.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+struct LowLevelCrossProcessMutex
+{
+    bool Dummy;
+};
+
+int32_t SystemNative_LowLevelCrossProcessMutex_Size(void)
+{
+    return (int32_t)sizeof(LowLevelCrossProcessMutex);
+}
+
+int32_t SystemNative_LowLevelCrossProcessMutex_Init(LowLevelCrossProcessMutex* mutex)
+{
+    assert(false);
+    return Error_EINVAL;
+}
+
+int32_t SystemNative_LowLevelCrossProcessMutex_Acquire(LowLevelCrossProcessMutex* mutex, int32_t timeoutMilliseconds)
+{
+    assert(false);
+    return Error_EINVAL;
+}
+
+int32_t SystemNative_LowLevelCrossProcessMutex_Release(LowLevelCrossProcessMutex* mutex)
+{
+    assert(false);
+    return Error_EINVAL;
+}
+
+int32_t SystemNative_LowLevelCrossProcessMutex_Destroy(LowLevelCrossProcessMutex* mutex)
+{
+    assert(false);
+    return Error_EINVAL;
+}
+
+void SystemNative_LowLevelCrossProcessMutex_GetOwnerProcessAndThreadId(LowLevelCrossProcessMutex* mutex, uint32_t* pOwnerProcessId, uint32_t* pOwnerThreadId)
+{
+    assert(false);
+}
+
+void SystemNative_LowLevelCrossProcessMutex_SetOwnerProcessAndThreadId(LowLevelCrossProcessMutex* mutex, uint32_t ownerProcessId, uint32_t ownerThreadId)
+{
+    assert(false);
+}
+
+uint8_t SystemNative_LowLevelCrossProcessMutex_IsAbandoned(LowLevelCrossProcessMutex* mutex)
+{
+    assert(false);
+    return Error_EINVAL;
+}
+
+void SystemNative_LowLevelCrossProcessMutex_SetAbandoned(LowLevelCrossProcessMutex* mutex, uint8_t isAbandoned)
+{
+    assert(false);
+}
--- src/native/libs/System.Native/pal_threading.h
+++ src/native/libs/System.Native/pal_threading.h
@@ -33,23 +33,3 @@ PALEXPORT __attribute__((noreturn)) void SystemNative_Abort(void);
 
 PALEXPORT uint64_t SystemNative_GetUInt64OSThreadId(void);
 PALEXPORT uint32_t SystemNative_TryGetUInt32OSThreadId(void);
-
-PALEXPORT int32_t SystemNative_PThreadMutex_Init(void* mutex);
-
-PALEXPORT int32_t SystemNative_PThreadMutex_Acquire(void* mutex, int32_t timeoutMilliseconds);
-
-PALEXPORT int32_t SystemNative_PThreadMutex_Release(void* mutex);
-
-PALEXPORT int32_t SystemNative_PThreadMutex_Destroy(void* mutex);
-
-PALEXPORT int32_t SystemNative_PThreadMutex_Size(void);
-
-PALEXPORT int32_t SystemNative_LowLevelCrossPlatformMutex_Size(void);
-PALEXPORT int32_t SystemNative_LowLevelCrossPlatformMutex_Init(LowLevelCrossProcessMutex* mutex);
-PALEXPORT int32_t SystemNative_LowLevelCrossPlatformMutex_Acquire(LowLevelCrossProcessMutex* mutex, int32_t timeoutMilliseconds);
-PALEXPORT int32_t SystemNative_LowLevelCrossPlatformMutex_Release(LowLevelCrossProcessMutex* mutex);
-PALEXPORT int32_t SystemNative_LowLevelCrossPlatformMutex_Destroy(LowLevelCrossProcessMutex* mutex);
-PALEXPORT void SystemNative_LowLevelCrossPlatformMutex_GetOwnerProcessAndThreadId(LowLevelCrossProcessMutex* mutex, uint32_t* pOwnerProcessId, uint32_t* pOwnerThreadId);
-PALEXPORT void SystemNative_LowLevelCrossPlatformMutex_SetOwnerProcessAndThreadId(LowLevelCrossProcessMutex* mutex, uint32_t ownerProcessId, uint32_t ownerThreadId);
-PALEXPORT uint8_t SystemNative_LowLevelCrossPlatformMutex_IsAbandoned(LowLevelCrossProcessMutex* mutex);
-PALEXPORT void SystemNative_LowLevelCrossPlatformMutex_SetAbandoned(LowLevelCrossProcessMutex* mutex, uint8_t isAbandoned);
