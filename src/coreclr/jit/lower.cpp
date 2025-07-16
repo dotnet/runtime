52e8aaae1ba74ab7c8cb376db63e69add870ff73
Khushal Modi <kcmodi@asu.edu>
2025-07-15 17:10:50 -0700

    run formatting

--- src/coreclr/jit/lower.cpp
+++ src/coreclr/jit/lower.cpp
@@ -11578,23 +11578,23 @@ bool Lowering::TryLowerAndOrToCCMP(GenTreeOp* tree, GenTree** next)
     bool         canConvertOp2ToCCMP = CanConvertOpToCCMP(op2, tree);
     bool         canConvertOp1ToCCMP = CanConvertOpToCCMP(op1, tree);
 
-    if (canConvertOp2ToCCMP && 
+    if (canConvertOp2ToCCMP &&
 #if defined(TARGET_AMD64)
         (!canConvertOp1ToCCMP || IsOpPreferredForCCMP(op2)) &&
 #elif defined(TARGET_ARM64) // TARGET_AMD64
         (op2->gtGetOp1()->IsIntegralConst() || !op2->gtGetOp1()->isContained()) &&
         (op2->gtGetOp2() == nullptr || op2->gtGetOp2()->IsIntegralConst() || !op2->gtGetOp2()->isContained()) &&
-#endif //TARGET_ARM64
+#endif                      // TARGET_ARM64
         TryLowerConditionToFlagsNode(tree, op1, &cond1, false))
     {
         // Fall through, converting op2 to the CCMP
     }
-    else if (canConvertOp1ToCCMP && 
+    else if (canConvertOp1ToCCMP &&
 #if defined(TARGET_ARM64)
-        (op1->gtGetOp1()->IsIntegralConst() || !op1->gtGetOp1()->isContained()) &&
+             (op1->gtGetOp1()->IsIntegralConst() || !op1->gtGetOp1()->isContained()) &&
              (op1->gtGetOp2() == nullptr || op1->gtGetOp2()->IsIntegralConst() || !op1->gtGetOp2()->isContained()) &&
 #endif // TARGET_ARM64
-        TryLowerConditionToFlagsNode(tree, op2, &cond1, false))
+             TryLowerConditionToFlagsNode(tree, op2, &cond1, false))
     {
         std::swap(op1, op2);
     }
