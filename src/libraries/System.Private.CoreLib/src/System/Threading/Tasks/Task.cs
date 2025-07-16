eb6d1d973cd0f94c1483ee77766017161fd25bf2
Stephen Toub <stoub@microsoft.com>
2025-07-16 09:38:46 -0400

    Apply suggestion from @stephentoub

--- src/libraries/System.Private.CoreLib/src/System/Threading/Tasks/Task.cs
+++ src/libraries/System.Private.CoreLib/src/System/Threading/Tasks/Task.cs
@@ -6034,7 +6034,7 @@ namespace System.Threading.Tasks
                 case 1:
                     if (tasks[0] is not Task t)
                     {
-                        ThrowHelper.ThrowArgumentNullException(ExceptionArgument.task);
+                        ThrowHelper.ThrowArgumentException(ExceptionResource.Task_MultiTaskContinuation_NullTask, ExceptionArgument.tasks)
                     }
                     return t;
             
