3fb34b20a51b78f4fb4a3dbf0f6396f461d105c8
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2025-07-17 17:09:14 +0200

    Fix a few runtime async stub issues (#117418)
    - Allow byref parameters when creating stubs
    - Find `MethodDesc`s in right `MethodTable` used for tokens when
      creating stubs. Particularly we were using
      `TaskAwaiter<__Canon>.GetResult` for some tokens where we should have
      used non-shared instantiations (like on `TaskAwaiter<string>`).

--- src/coreclr/vm/jitinterface.cpp
+++ src/coreclr/vm/jitinterface.cpp
@@ -14417,7 +14417,6 @@ static Signature BuildResumptionStubCalliSignature(MetaSig& msig, MethodTable* m
 
     auto appendTypeHandle = [](SigBuilder& sigBuilder, TypeHandle th)
     {
-        _ASSERTE(!th.IsByRef());
         CorElementType ty = th.GetSignatureCorElementType();
         if (CorTypeInfo::IsObjRef(ty))
         {
