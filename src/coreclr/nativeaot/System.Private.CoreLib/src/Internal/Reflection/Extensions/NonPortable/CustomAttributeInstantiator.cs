3e6ef1d067b346c3e8fbfe47079d35a456856a1b
pedrobsaila <badrebsaila@outlook.com>
2025-07-20 14:57:09 +0200

    normalize conditions and move and rename test

--- src/coreclr/nativeaot/System.Private.CoreLib/src/Internal/Reflection/Extensions/NonPortable/CustomAttributeInstantiator.cs
+++ src/coreclr/nativeaot/System.Private.CoreLib/src/Internal/Reflection/Extensions/NonPortable/CustomAttributeInstantiator.cs
@@ -104,16 +104,32 @@ namespace Internal.Reflection.Extensions.NonPortable
                 else
                 {
                     // Property
+                    MethodInfo? getMethod = null;
+                    MethodInfo? setMethod = null;
+
                     for (; ; )
                     {
                         PropertyInfo? propertyInfo = walk.GetProperty(name, BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static | BindingFlags.DeclaredOnly);
-                        if (propertyInfo?.SetMethod is not null)
+
+                        if (propertyInfo is not null)
                         {
-                            propertyInfo.SetValue(newAttribute, argumentValue);
-                            break;
+                            getMethod = propertyInfo.GetGetMethod(true);
+                            setMethod = propertyInfo.GetSetMethod(true);
+
+                            if (setMethod is not null)
+                            {
+                                propertyInfo.SetValue(newAttribute, argumentValue);
+                                break;
+                            }
                         }
+                        else
+                        {
+                            getMethod = null;
+                            setMethod = null;
+                        }
+
                         Type? baseType = walk.BaseType;
-                        if (baseType == null)
+                        if (baseType == null || (getMethod is not null && !getMethod.IsVirtual))
                             throw new CustomAttributeFormatException(SR.Format(SR.RFLCT_InvalidPropFail, name));
                         walk = baseType;
                     }
