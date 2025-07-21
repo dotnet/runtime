9d4373030de16f83f3efecac37d5931d365d4b8b
Jeremy Koritzinsky <jekoritz@microsoft.com>
2025-07-21 22:05:38 +0000

    Unify LowLevelLifoSemaphore implementations for Unix to use the wait subsystem

--- src/coreclr/vm/comwaithandle.h
+++ src/coreclr/vm/comwaithandle.h
@@ -18,8 +18,4 @@ extern "C" INT32 QCALLTYPE WaitHandle_WaitOneCore(HANDLE handle, INT32 timeout, 
 extern "C" INT32 QCALLTYPE WaitHandle_WaitMultipleIgnoringSyncContext(HANDLE *handleArray, INT32 numHandles, BOOL waitForAll, INT32 timeout);
 extern "C" INT32 QCALLTYPE WaitHandle_SignalAndWait(HANDLE waitHandleSignal, HANDLE waitHandleWait, INT32 timeout);
 
-#ifdef TARGET_UNIX
-extern "C" INT32 QCALLTYPE WaitHandle_WaitOnePrioritized(HANDLE handle, INT32 timeoutMs);
-#endif // TARGET_UNIX
-
 #endif // _COM_WAITABLE_HANDLE_H
