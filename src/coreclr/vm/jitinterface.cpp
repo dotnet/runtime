167d862993217db12098bb498ca34f2fc3620fc6
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2025-07-24 13:40:56 +0200

    Save and restore `Thread.CurrentThread._synchronizationContext` for synchronous runtime async calls (#117725)
    This implements the remaining handling: saving and restoring
    `Thread.CurrentThread._synchronizationContext` around runtime async calls that
    finish synchronously. Lots of trickiness in representing this in the JIT. We
    introduce a new "suspended indicator" local that is defined by async calls and
    that the front end uses when it expands IR that restores the field.
    
    Plenty of optimization opportunities remaining, including a simple jump
    threading optimization we should be able to do to make the suspension indicator
    variable disappear in common cases (all cases except when resuming with an
    exception).

--- src/coreclr/vm/jitinterface.cpp
+++ src/coreclr/vm/jitinterface.cpp
@@ -10268,6 +10268,8 @@ void CEEInfo::getAsyncInfo(CORINFO_ASYNC_INFO* pAsyncInfoOut)
     pAsyncInfoOut->captureExecutionContextMethHnd = CORINFO_METHOD_HANDLE(CoreLibBinder::GetMethod(METHOD__ASYNC_HELPERS__CAPTURE_EXECUTION_CONTEXT));
     pAsyncInfoOut->restoreExecutionContextMethHnd = CORINFO_METHOD_HANDLE(CoreLibBinder::GetMethod(METHOD__ASYNC_HELPERS__RESTORE_EXECUTION_CONTEXT));
     pAsyncInfoOut->captureContinuationContextMethHnd = CORINFO_METHOD_HANDLE(CoreLibBinder::GetMethod(METHOD__ASYNC_HELPERS__CAPTURE_CONTINUATION_CONTEXT));
+    pAsyncInfoOut->captureContextsMethHnd = CORINFO_METHOD_HANDLE(CoreLibBinder::GetMethod(METHOD__ASYNC_HELPERS__CAPTURE_CONTEXTS));
+    pAsyncInfoOut->restoreContextsMethHnd = CORINFO_METHOD_HANDLE(CoreLibBinder::GetMethod(METHOD__ASYNC_HELPERS__RESTORE_CONTEXTS));
 
     EE_TO_JIT_TRANSITION();
 }
