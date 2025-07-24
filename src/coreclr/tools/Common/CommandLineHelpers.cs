68b972a6f5c23d03d497ec69a6e69b12cc4f675d
Jackson Schuster <36744439+jtschuster@users.noreply.github.com>
2025-07-24 23:47:03 +0000

    Remove extra android->linux translations

--- src/coreclr/tools/Common/CommandLineHelpers.cs
+++ src/coreclr/tools/Common/CommandLineHelpers.cs
@@ -82,7 +82,6 @@ namespace System.CommandLine
                 "ios" => TargetOS.iOS,
                 "tvossimulator" => TargetOS.tvOSSimulator,
                 "tvos" => TargetOS.tvOS,
-                "android" => TargetOS.Linux,
                 _ => throw new CommandLineException($"Target OS '{token}' is not supported")
             };
         }
