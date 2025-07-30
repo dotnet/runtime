c8fd5da9c23f6e11137f9b1c545515ca3cc6d6e2
Jonathan Davies <jonathan.davies@arm.com>
2025-07-30 09:04:53 +0000

    Move Isa & LoadIsa to TestGroup

--- src/tests/Common/GenerateHWIntrinsicTests/Arm/TestGroup.cs
+++ src/tests/Common/GenerateHWIntrinsicTests/Arm/TestGroup.cs
@@ -7,18 +7,25 @@ using System.Linq;
 
 public struct TestGroup
 {
+    public string Isa { get; set; }
+    public string LoadIsa { get; set; }
     public (string TemplateConfig, Dictionary<string, string> KeyValuePairs)[] Tests { get; set; }
 
-    public TestGroup((string, Dictionary<string, string>)[] tests)
+    public TestGroup(string Isa, string LoadIsa, (string, Dictionary<string, string>)[] Tests)
     {
-        Tests = tests;
+        this.Isa = Isa;
+        this.LoadIsa = LoadIsa;
+        this.Tests = Tests;
     }
 
     public (string, Dictionary<string, string>)[] GetTests()
     {
+        var self = this;
         return Tests.Select(t =>
         {
             var data = new Dictionary<string, string>(t.KeyValuePairs);
+            if (!string.IsNullOrEmpty(self.Isa)) data["Isa"] = self.Isa;
+            if (!string.IsNullOrEmpty(self.LoadIsa)) data["LoadIsa"] = self.LoadIsa;
             return (t.TemplateConfig, data);
         }).ToArray();
     }
