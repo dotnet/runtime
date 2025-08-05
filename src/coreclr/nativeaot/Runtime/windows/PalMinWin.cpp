f1c4f4fe64fdf6431272f54f148690b0d0bc8c95
Radek Doulik <radek.doulik@gmail.com>
2025-08-05 16:14:29 +0200

    Feedback
    Remove windows wrapper too

--- src/coreclr/nativeaot/Runtime/windows/PalMinWin.cpp
+++ src/coreclr/nativeaot/Runtime/windows/PalMinWin.cpp
@@ -1039,11 +1039,6 @@ UInt32_BOOL PalCloseHandle(HANDLE arg1)
     return ::CloseHandle(arg1);
 }
 
-void minipal_memory_barrier_process_wide()
-{
-    ::FlushProcessWriteBuffers();
-}
-
 uint32_t PalGetCurrentProcessId()
 {
     return static_cast<uint32_t>(::GetCurrentProcessId());
