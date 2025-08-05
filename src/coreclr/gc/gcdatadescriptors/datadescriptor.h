c5f8064578211b7dbddd11f1a66ab5121dc1d860
Max Charlamb <44248479+max-charlamb@users.noreply.github.com>
2025-08-05 13:34:10 -0400

    working

--- src/coreclr/gc/gcdatadescriptors/datadescriptor.h
+++ src/coreclr/gc/gcdatadescriptors/datadescriptor.h
@@ -1,28 +1,10 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#ifdef IN_PROCESS_GC
-
-// #include "../gc/env/common.h"
-// #include "../gc/env/gcenv.h"
-#include "static_assert.h"
-
 #include <stdint.h>
 #include <stddef.h>
 
-// #include "../gc/env/common.h"
-// #include "../gc/env/gcenv.h"
-// #include "../gc/gc.h"
-// #include "../gc/gcscan.h"
-
-#else // IN_PROCESS_GC
-
-#include <stdint.h>
-#include <stddef.h>
-
-#include "../gc/env/common.h"
-#include "../gc/env/gcenv.h"
-// #include "gc.h"
-// #include "gcscan.h"
-
-#endif // IN_PROCESS_GC
+#include "common.h"
+#include "gcenv.h"
+#include "gc.h"
+#include "gcscan.h"
