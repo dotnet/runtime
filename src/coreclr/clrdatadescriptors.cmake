192a5b13597dbb4758b7fabc97490fe0b8015c61
Max Charlamb <44248479+max-charlamb@users.noreply.github.com>
2025-08-05 11:07:09 -0400

    update to use interface target

--- src/coreclr/clrdatadescriptors.cmake
+++ src/coreclr/clrdatadescriptors.cmake
@@ -1,9 +1,9 @@
 # cDAC contract descriptor
 
 function(generate_data_descriptors)
-  set(options "")
-  set(oneValueArgs LIBRARY_NAME CONTRACT_FILE HEADER_FILE INLINE_FILE POINTER_DATA_NAME CONTRACT_NAME)
-  set(multiValueArgs INCLUDE_DIRS DEPENDENCIES)
+  set(options DLLEXPORT)
+  set(oneValueArgs LIBRARY_NAME CONTRACT_FILE CONTRACT_NAME INTERFACE_TARGET)
+  set(multiValueArgs "")
   cmake_parse_arguments(DATA_DESCRIPTORS "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGV})
 
   # INTERMEDIARY_LIBRARY is used as part of the build and not linked into the final product.
@@ -14,8 +14,13 @@ function(generate_data_descriptors)
   set(GENERATED_CDAC_DESCRIPTOR_DIR "${CMAKE_CURRENT_BINARY_DIR}/cdac-${LIBRARY}")
 
   # configure contract export name
-  set(POINTER_DATA_NAME ${DATA_DESCRIPTORS_POINTER_DATA_NAME})
+  set(POINTER_DATA_NAME ${DATA_DESCRIPTORS_CONTRACT_NAME}PointerData)
   set(CONTRACT_NAME ${DATA_DESCRIPTORS_CONTRACT_NAME})
+  if (DATA_DESCRIPTORS_DLLEXPORT)
+    set(EXPORT_CONTRACT 1)
+  else()
+    set(EXPORT_CONTRACT 0)
+  endif()
   configure_file("${DATA_DESCRIPTOR_SHARED_SOURCE_DIR}/contractconfiguration.h.in" "${GENERATED_CDAC_DESCRIPTOR_DIR}/contractconfiguration.h")
 
   if (NOT CDAC_BUILD_TOOL_BINARY_PATH)
@@ -32,6 +37,10 @@ function(generate_data_descriptors)
     endif()
 
     add_library(${INTERMEDIARY_LIBRARY} OBJECT "${DATA_DESCRIPTOR_SHARED_SOURCE_DIR}/datadescriptor.cpp")
+    # get_target_property(intermediary_includes ${INTERMEDIARY_LIBRARY} INCLUDE_DIRECTORIES)
+    # get_target_property(interface_includes ${DATA_DESCRIPTORS_INTERFACE_TARGET} INTERFACE_INCLUDE_DIRECTORIES)
+    # message(STATUS "Default include directories for ${INTERMEDIARY_LIBRARY} are: ${intermediary_includes}")
+    # message(STATUS "Default include directories for ${DATA_DESCRIPTORS_INTERFACE_TARGET} are: ${interface_includes}")
 
     if(CLR_CMAKE_TARGET_WIN32)
       # turn off whole program optimization:
@@ -42,13 +51,8 @@ function(generate_data_descriptors)
           INTERPROCEDURAL_OPTIMIZATION_RELWITHDEBINFO OFF)
     endif()
 
-    # don't build the data descriptor before dependencies
-    if(DEFINED DATA_DESCRIPTORS_DEPENDENCIES)
-      add_dependencies(${INTERMEDIARY_LIBRARY} ${DATA_DESCRIPTORS_DEPENDENCIES})
-    endif()
-    if(DEFINED DATA_DESCRIPTORS_INCLUDE_DIRS)
-      target_include_directories(${INTERMEDIARY_LIBRARY} BEFORE PRIVATE ${DATA_DESCRIPTORS_INCLUDE_DIRS})
-    endif()
+    # inherit definitions, include directories, and dependencies from the INTERFACE target
+    add_interface_library(${INTERMEDIARY_LIBRARY} ${DATA_DESCRIPTORS_INTERFACE_TARGET})
 
     set(CONTRACT_BASELINE_DIR "${CLR_REPO_ROOT_DIR}/docs/design/datacontracts/data")
     set(CONTRACT_DESCRIPTOR_INPUT "${DATA_DESCRIPTOR_SHARED_SOURCE_DIR}/contract-descriptor.c.in")
@@ -74,13 +78,21 @@ function(generate_data_descriptors)
     )
 
     add_dependencies(${LIBRARY} ${INTERMEDIARY_LIBRARY})
-    if(DEFINED DATA_DESCRIPTORS_DEPENDENCIES)
-      add_dependencies(${LIBRARY} ${DATA_DESCRIPTORS_DEPENDENCIES})
-    endif()
-
+    # Remove default include directories to avoid conflicts with the include order from the interface target.
     target_include_directories(${LIBRARY} PRIVATE ${GENERATED_CDAC_DESCRIPTOR_DIR})
-    if(DEFINED DATA_DESCRIPTORS_INCLUDE_DIRS)
-      target_include_directories(${LIBRARY} BEFORE PRIVATE ${DATA_DESCRIPTORS_INCLUDE_DIRS})
-    endif()
+
+    # inherit definitions, include directories, and dependencies from the INTERFACE target
+    add_interface_library(${LIBRARY} ${DATA_DESCRIPTORS_INTERFACE_TARGET})
   endif()
 endfunction(generate_data_descriptors)
+
+# Links in an interface to a target with the interface include directories included
+# before the targets include directories.
+function(add_interface_library target_name interface_name)
+  get_target_property(target_includes ${target_name} INCLUDE_DIRECTORIES)
+  target_link_libraries(${target_name} PUBLIC ${interface_name})
+  set_target_properties(${target_name} PROPERTIES INCLUDE_DIRECTORIES "${target_includes}")
+
+  get_target_property(interface_includes ${interface_name} INTERFACE_INCLUDE_DIRECTORIES)
+  target_include_directories(${target_name} BEFORE PRIVATE ${interface_includes})
+endfunction(add_interface_library)
