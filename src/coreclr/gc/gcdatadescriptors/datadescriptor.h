ec7539fca500bdaf7663ae2b9eb04b8cf3c1bde7
Max Charlamb <44248479+max-charlamb@users.noreply.github.com>
2025-08-05 14:45:44 -0400

    fix building datadescriptor

--- src/coreclr/gc/gcdatadescriptors/datadescriptor.h
+++ src/coreclr/gc/gcdatadescriptors/datadescriptor.h
@@ -8,3 +8,22 @@
 #include "gcenv.h"
 #include "gc.h"
 #include "gcscan.h"
+#include "gchandletableimpl.h"
+#include "gceventstatus.h"
+
+#ifdef SERVER_GC
+#define GC_NAMESPACE SVR
+#else // SERVER_GC
+#define GC_NAMESPACE WKS
+#endif // SERVER_GC
+
+#ifdef SERVER_GC
+namespace SVR {
+#else // SERVER_GC
+namespace WKS {
+#endif // SERVER_GC
+
+#include "gcimpl.h"
+#include "gcpriv.h"
+
+}
