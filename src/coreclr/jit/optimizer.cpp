320539e1125deb4e17b19fc0da3c2b5e3a1b5b63
Aman Khalid (from Dev Box) <amankhalid@microsoft.com>
2025-08-07 11:54:05 -0400

    Revert "More precise side effect propagation"
    This reverts commit 217d6e74c6e3e2e23a15d502e192c343f6ed0da3.

--- src/coreclr/jit/optimizer.cpp
+++ src/coreclr/jit/optimizer.cpp
@@ -4116,12 +4116,12 @@ void Compiler::optHoistLoopBlocks(FlowGraphNaturalLoop* loop,
         };
 
         ArrayStack<Value>     m_valueStack;
+        bool                  m_beforeSideEffect;
         FlowGraphNaturalLoop* m_loop;
         LoopHoistContext*     m_hoistContext;
         BasicBlock*           m_currentBlock;
         BitVecTraits*         m_traits;
         BitVec                m_defExec;
-        BitVec                m_sideEffectBlocks;
 
         bool IsNodeHoistable(GenTree* node)
         {
@@ -4251,12 +4251,12 @@ void Compiler::optHoistLoopBlocks(FlowGraphNaturalLoop* loop,
                      BitVec                defExec)
             : GenTreeVisitor(compiler)
             , m_valueStack(compiler->getAllocator(CMK_LoopHoist))
+            , m_beforeSideEffect(true)
             , m_loop(loop)
             , m_hoistContext(hoistContext)
             , m_currentBlock(nullptr)
             , m_traits(traits)
             , m_defExec(defExec)
-            , m_sideEffectBlocks(BitVecOps::MakeEmpty(traits))
         {
         }
 
@@ -4356,7 +4356,6 @@ void Compiler::optHoistLoopBlocks(FlowGraphNaturalLoop* loop,
             bool treeIsInvariant          = true;
             bool treeHasHoistableChildren = false;
             int  childCount;
-            bool beforeSideEffect = !BitVecOps::IsMember(m_traits, m_sideEffectBlocks, m_currentBlock->bbPostorderNum);
 
 #ifdef DEBUG
             const char* failReason = "unknown";
@@ -4459,7 +4458,7 @@ void Compiler::optHoistLoopBlocks(FlowGraphNaturalLoop* loop,
 
                 if (treeIsHoistable)
                 {
-                    if (beforeSideEffect)
+                    if (!m_beforeSideEffect)
                     {
                         // For now, we give up on an expression that might raise an exception if it is after the
                         // first possible global side effect.
@@ -4485,11 +4484,11 @@ void Compiler::optHoistLoopBlocks(FlowGraphNaturalLoop* loop,
                 }
             }
 
-            // Next check if we need to set 'beforeSideEffect' to false.
+            // Next check if we need to set 'm_beforeSideEffect' to false.
             //
             // If we have already set it to false then we can skip these checks
             //
-            if (beforeSideEffect)
+            if (m_beforeSideEffect)
             {
                 // Is the value of the whole tree loop invariant?
                 if (!treeIsInvariant)
@@ -4497,11 +4496,12 @@ void Compiler::optHoistLoopBlocks(FlowGraphNaturalLoop* loop,
                     // We have a tree that is not loop invariant and we thus cannot hoist
                     assert(treeIsHoistable == false);
 
-                    // Check if we should mark this block as having side effects.
+                    // Check if we should clear m_beforeSideEffect.
+                    // If 'tree' can throw an exception then we need to set m_beforeSideEffect to false.
                     // Note that calls are handled below
                     if (tree->OperMayThrow(m_compiler) && !tree->IsCall())
                     {
-                        beforeSideEffect = false;
+                        m_beforeSideEffect = false;
                     }
                 }
 
@@ -4517,19 +4517,19 @@ void Compiler::optHoistLoopBlocks(FlowGraphNaturalLoop* loop,
                     GenTreeCall* call = tree->AsCall();
                     if (!call->IsHelperCall())
                     {
-                        beforeSideEffect = false;
+                        m_beforeSideEffect = false;
                     }
                     else
                     {
                         CorInfoHelpFunc helpFunc = eeGetHelperNum(call->gtCallMethHnd);
                         if (s_helperCallProperties.MutatesHeap(helpFunc))
                         {
-                            beforeSideEffect = false;
+                            m_beforeSideEffect = false;
                         }
                         else if (s_helperCallProperties.MayRunCctor(helpFunc) &&
                                  (call->gtFlags & GTF_CALL_HOISTABLE) == 0)
                         {
-                            beforeSideEffect = false;
+                            m_beforeSideEffect = false;
                         }
 
                         // Additional check for helper calls that throw exceptions
@@ -4541,7 +4541,7 @@ void Compiler::optHoistLoopBlocks(FlowGraphNaturalLoop* loop,
                             // Does this helper call throw?
                             if (!s_helperCallProperties.NoThrow(helpFunc))
                             {
-                                beforeSideEffect = false;
+                                m_beforeSideEffect = false;
                             }
                         }
                     }
@@ -4562,20 +4562,9 @@ void Compiler::optHoistLoopBlocks(FlowGraphNaturalLoop* loop,
                     if (isGloballyVisibleStore)
                     {
                         INDEBUG(failReason = "store to globally visible memory");
-                        treeIsHoistable  = false;
-                        beforeSideEffect = false;
+                        treeIsHoistable    = false;
+                        m_beforeSideEffect = false;
                     }
-                }
-            }
-
-            // If this block does not execute before the first side effect in the loop,
-            // mark it and its successors.
-            if (!beforeSideEffect)
-            {
-                BitVecOps::AddElemD(m_traits, m_sideEffectBlocks, m_currentBlock->bbPostorderNum);
-                for (BasicBlock* const succ : m_currentBlock->Succs())
-                {
-                    BitVecOps::AddElemD(m_traits, m_sideEffectBlocks, succ->bbPostorderNum);
                 }
             }
 
