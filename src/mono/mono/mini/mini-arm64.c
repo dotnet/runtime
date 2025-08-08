dotnet/runtime

commit 4da283c90108e35aed0af4f36cff32c581c658e7

[mono][debugger] Fix assertion when stepping (#118458)
This PR fixes assertion failures when stepping in the Mono debugger on ARM64 by aligning the breakpoint address calculation logic with the AMD64 implementation. The changes remove the division by 4 that was causing incorrect indexing into the breakpoint addresses array.

src/mono/mono/mini/mini-arm64.c
@@ -4203,8 +4203,7 @@ mono_arch_output_basic_block (MonoCompile *cfg, MonoBasicBlock *bb)
 				guint32 val;
 
 				arm_ldrx (code, ARMREG_IP1, info_var->inst_basereg, GTMREG_TO_INT (info_var->inst_offset));
-				/* Add the bp_tramp_offset */
-				val = ((bp_tramp_offset / 4) * sizeof (target_mgreg_t)) + MONO_STRUCT_OFFSET (SeqPointInfo, bp_addrs);
+				val = (bp_tramp_offset * sizeof (target_mgreg_t)) + MONO_STRUCT_OFFSET (SeqPointInfo, bp_addrs);
 				/* Load the info->bp_addrs [bp_tramp_offset], which is either 0 or the address of the bp trampoline */
 				code = emit_ldrx (code, ARMREG_IP1, ARMREG_IP1, val);
 				/* Skip the load if its 0 */
@@ -6876,9 +6875,7 @@ mono_arch_set_breakpoint (MonoJitInfo *ji, guint8 *ip)
 
 		if (enable_ptrauth)
 			NOT_IMPLEMENTED;
-		g_assert (native_offset % 4 == 0);
-		g_assert (info->bp_addrs [native_offset / 4] == 0);
-		info->bp_addrs [native_offset / 4] = (guint8*)mini_get_breakpoint_trampoline ();
+		info->bp_addrs [native_offset] = (guint8*)mini_get_breakpoint_trampoline ();
 	} else {
 		/* ip points to an ldrx */
 		code += 4;
@@ -6901,8 +6898,7 @@ mono_arch_clear_breakpoint (MonoJitInfo *ji, guint8 *ip)
 		if (enable_ptrauth)
 			NOT_IMPLEMENTED;
 
-		g_assert (native_offset % 4 == 0);
-		info->bp_addrs [native_offset / 4] = NULL;
+		info->bp_addrs [native_offset] = NULL;
 	} else {
 		/* ip points to an ldrx */
 		code += 4;

commit ee4be3ddc6ef4cb4514bd7981704da525e0eb8e9

[mono][arm64] Fix SeqPointInfo allocation (#118542)
`bp_addrs` now needs to contain a pointer slot for every single byte in the code.

Regressed after https://github.com/dotnet/runtime/pull/118458

Fixes https://github.com/dotnet/runtime/issues/118519

src/mono/mono/mini/mini-arm64.c
@@ -6966,7 +6966,7 @@ mono_arch_get_seq_point_info (guint8 *code)
 		ji = mini_jit_info_table_find (code);
 		g_assert (ji);
 
-		info = g_malloc0 (sizeof (SeqPointInfo) + (ji->code_size / 4) * sizeof(guint8*));
+		info = g_malloc0 (sizeof (SeqPointInfo) + ji->code_size * sizeof(guint8*));
 
 		info->ss_tramp_addr = &ss_trampoline;
 