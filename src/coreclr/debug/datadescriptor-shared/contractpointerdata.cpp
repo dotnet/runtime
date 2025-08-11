2814588a677402789a49a43a5b68ef44274c3228
Max Charlamb <44248479+max-charlamb@users.noreply.github.com>
2025-08-11 13:56:05 -0400

    remove use of /Zc:externConstexpr which caused problems with existing code

--- src/coreclr/debug/datadescriptor-shared/contractpointerdata.cpp
+++ src/coreclr/debug/datadescriptor-shared/contractpointerdata.cpp
@@ -4,14 +4,16 @@
 #include "datadescriptor.h"
 #include "contractconfiguration.h"
 
+constexpr const void* POINTER_DATA_NAME_INTERNAL[] = {
+    (void*)0, // placeholder
+#define CDAC_GLOBAL_POINTER(name,value) (void*)(value),
+#define CDAC_GLOBAL_SUB_DESCRIPTOR(name,value) (void*)(value),
+#include "wrappeddatadescriptor.inc"
+};
+
 extern "C"
 {
     // without an extern declaration, clang does not emit this global into the object file
-    extern constexpr const void* POINTER_DATA_NAME[] = {
-        (void*)0, // placeholder
-    #define CDAC_GLOBAL_POINTER(name,value) (void*)(value),
-    #define CDAC_GLOBAL_SUB_DESCRIPTOR(name,value) (void*)(value),
-    #include "wrappeddatadescriptor.inc"
-    };
+    extern const void* const* POINTER_DATA_NAME = &POINTER_DATA_NAME_INTERNAL[0];
 };
 
