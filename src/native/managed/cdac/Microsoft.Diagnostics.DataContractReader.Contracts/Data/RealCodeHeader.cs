fc3236c69a10e6355784cdaf9ba27179392407c0
maxcharlamb <maxcharlamb+github@microsoft.com>
2025-08-11 16:33:50 -0400

    remove unused import

--- src/native/managed/cdac/Microsoft.Diagnostics.DataContractReader.Contracts/Data/RealCodeHeader.cs
+++ src/native/managed/cdac/Microsoft.Diagnostics.DataContractReader.Contracts/Data/RealCodeHeader.cs
@@ -1,8 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Microsoft.Diagnostics.DataContractReader.Contracts;
-
 namespace Microsoft.Diagnostics.DataContractReader.Data;
 
 internal sealed class RealCodeHeader : IData<RealCodeHeader>
