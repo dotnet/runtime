664a4061e344d7c65c2af04e8410ca30e193857a
Max Charlamb <44248479+max-charlamb@users.noreply.github.com>
2025-08-13 17:12:53 -0400

    fix test issues

--- src/native/managed/cdac/mscordaccore_universal/Legacy/SOSDacImpl.IXCLRDataProcess.cs
+++ src/native/managed/cdac/mscordaccore_universal/Legacy/SOSDacImpl.IXCLRDataProcess.cs
@@ -329,7 +329,7 @@ internal sealed unsafe partial class SOSDacImpl : IXCLRDataProcess, IXCLRDataPro
         }
     }
 
-    int IXCLRDataProcess.StartEnumMethodInstancesByAddress(ulong address, /*IXCLRDataAppDomain*/ void* appDomain, ulong* handle)
+    int IXCLRDataProcess.StartEnumMethodInstancesByAddress(ClrDataAddress address, /*IXCLRDataAppDomain*/ void* appDomain, ulong* handle)
     {
         int hr = HResults.S_OK;
 
@@ -347,14 +347,16 @@ internal sealed unsafe partial class SOSDacImpl : IXCLRDataProcess, IXCLRDataPro
 
         try
         {
+            TargetCodePointer methodAddr = address.ToTargetCodePointer(_target);
+
             // ClrDataAccess::IsPossibleCodeAddress
             // Does a trivial check on the readability of the address
-            bool isTriviallyReadable = _target.TryRead(address, out byte _);
+            bool isTriviallyReadable = _target.TryRead(methodAddr, out byte _);
             if (!isTriviallyReadable)
                 throw new ArgumentException();
 
             IExecutionManager eman = _target.Contracts.ExecutionManager;
-            if (eman.GetCodeBlockHandle(address) is CodeBlockHandle cbh &&
+            if (eman.GetCodeBlockHandle(methodAddr) is CodeBlockHandle cbh &&
                 eman.GetMethodDesc(cbh) is TargetPointer methodDesc)
             {
                 EnumMethodInstances emi = new(_target, methodDesc, TargetPointer.Null);
@@ -364,6 +366,11 @@ internal sealed unsafe partial class SOSDacImpl : IXCLRDataProcess, IXCLRDataPro
                 *handle = (ulong)GCHandle.ToIntPtr(gcHandle).ToInt64();
                 hr = emi.Start();
             }
+        }
+        catch (InvalidOperationException)
+        {
+            // If the target read fails, expect HResult to be CORDBG_E_READVIRTUAL_FAILURE
+            hr = CorDbgHResults.CORDBG_E_READVIRTUAL_FAILURE;
         }
         catch (System.Exception ex)
         {
