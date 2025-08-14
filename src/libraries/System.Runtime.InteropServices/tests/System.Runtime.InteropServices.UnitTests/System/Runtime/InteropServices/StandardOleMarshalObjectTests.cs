1a77fec06e3d1d561ab1956e6e6bad9c7d06c936
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2025-08-14 18:18:45 +0000

    Add test for QIing IDispatch from IUnknown from the native side.

--- src/libraries/System.Runtime.InteropServices/tests/System.Runtime.InteropServices.UnitTests/System/Runtime/InteropServices/StandardOleMarshalObjectTests.cs
+++ src/libraries/System.Runtime.InteropServices/tests/System.Runtime.InteropServices.UnitTests/System/Runtime/InteropServices/StandardOleMarshalObjectTests.cs
@@ -8,12 +8,27 @@ namespace System.Runtime.InteropServices.Tests
     [ConditionalClass(typeof(PlatformDetection), nameof(PlatformDetection.IsBuiltInComEnabled))]
     public class StandardOleMarshalObjectTests
     {
+        private static readonly Guid IID_IDispatch = new Guid("00020400-0000-0000-C000-000000000046");
         [Fact]
         public void CanGetIDispatchOfDerivedObject()
         {
             IntPtr disp = Marshal.GetIDispatchForObject(new DerivedObject());
             Assert.NotEqual(IntPtr.Zero, disp);
             Marshal.Release(disp);
+        }
+
+        [Fact]
+        public void CanQueryInterfaceForIDispatchOfDerivedObject()
+        {
+            IntPtr unk = Marshal.GetIUnknownForObject(new DerivedObject());
+            Assert.NotEqual(IntPtr.Zero, unk);
+
+            int hr = Marshal.QueryInterface(unk, IID_IDispatch, out IntPtr disp);
+            Assert.Equal(0, hr);
+            Assert.NotEqual(IntPtr.Zero, disp);
+ 
+            Marshal.Release(disp);
+            Marshal.Release(unk);
         }
 
         [ComVisible(true)]
