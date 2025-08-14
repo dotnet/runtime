6f2cebd1aa28a342129ed6713a285a779fec82c8
vsadov <8218165+VSadov@users.noreply.github.com>
2025-08-13 17:09:30 -0700

    Keep the BIT_SBLK_FINALIZER_RUN check.

--- src/coreclr/nativeaot/Runtime/FinalizerHelpers.cpp
+++ src/coreclr/nativeaot/Runtime/FinalizerHelpers.cpp
@@ -241,10 +241,24 @@ EXTERN_C UInt32_BOOL QCALLTYPE RhpWaitForFinalizerRequest()
 // Fetch next object which needs finalization or return null if we've reached the end of the list.
 FCIMPL0(OBJECTREF, RhpGetNextFinalizableObject)
 {
-    // Get the next finalizable object. If we get back NULL we've reached the end of the list.
-    OBJECTREF refNext = GCHeapUtilities::GetGCHeap()->GetNextFinalizable();
-    ASSERT((refNext->GetHeader()->GetBits() & BIT_SBLK_FINALIZER_RUN) == 0);
+    while (true)
+    {
+        // Get the next finalizable object. If we get back NULL we've reached the end of the list.
+        OBJECTREF refNext = GCHeapUtilities::GetGCHeap()->GetNextFinalizable();
+        if (refNext == NULL)
+            return NULL;
 
-    RETURN refNext;
+        // The queue may contain objects which have been marked as finalized already (via GC.SuppressFinalize()
+        // for instance). Skip finalization for these but reset the flag so that the object can be put back on
+        // the list with RegisterForFinalization().
+        if (refNext->GetHeader()->GetBits() & BIT_SBLK_FINALIZER_RUN)
+        {
+            refNext->GetHeader()->ClrBit(BIT_SBLK_FINALIZER_RUN);
+            continue;
+        }
+
+        // We've found the first finalizable object, return it to the caller.
+        return refNext;
+    }
 }
 FCIMPLEND
