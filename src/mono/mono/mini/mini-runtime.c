6fde69f42fe122bd1f6a1f9e4381d7be63772e2d
vsadov <8218165+VSadov@users.noreply.github.com>
2025-08-14 14:34:42 -0700

    only check for GuardedFinalize in corlib

--- src/mono/mono/mini/mini-runtime.c
+++ src/mono/mono/mini/mini-runtime.c
@@ -5363,10 +5363,12 @@ mono_precompile_assembly (MonoAssembly *ass, void *user_data)
 			mono_error_cleanup (error); /* FIXME don't swallow the error */
 			continue;
 		}
-		if (strcmp (method->name, "GuardedFinalize") == 0) {
-			invoke = mono_marshal_get_runtime_invoke_full (method, FALSE, TRUE);
-			mono_compile_method_checked (invoke, error);
-			mono_error_assert_ok (error);
+		if (mono_is_corlib_image (image)) {
+			if (strcmp (method->name, "GuardedFinalize") == 0) {
+				invoke = mono_marshal_get_runtime_invoke_full (method, FALSE, TRUE);
+				mono_compile_method_checked (invoke, error);
+				mono_error_assert_ok (error);
+			}
 		}
 	}
 
