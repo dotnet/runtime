1209ddec456f1d7871847b07b46f00d44012d687
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2025-08-16 04:08:27 +0200

    Add better explanation to AnalysisCharacteristicAttribute (#118799)

--- src/coreclr/nativeaot/System.Private.CoreLib/src/System/Runtime/CompilerServices/AnalysisCharacteristicAttribute.cs
+++ src/coreclr/nativeaot/System.Private.CoreLib/src/System/Runtime/CompilerServices/AnalysisCharacteristicAttribute.cs
@@ -4,6 +4,14 @@
 namespace System.Runtime.CompilerServices
 {
     // When applied to an intrinsic method, the method will become a characteristic check.
+    //
+    // The method needs to return bool and have no parameters.
+    //
+    // The compiler will replace the body of the method with a constant true/false depending
+    // on whether the characteristic (tag) was added to the whole program view.
+    // The name of the characteristic is the name of the method.
+    // Compiler can add characteristics to the whole program view using the
+    // NodeFactory.AnalysisCharacteristic(string) method.
     [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
     internal class AnalysisCharacteristicAttribute : Attribute
     {
