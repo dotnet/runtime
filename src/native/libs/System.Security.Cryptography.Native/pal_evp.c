ab3836c3b6e90978a510945428615957e57caff5
Kevin Jones <kevin@vcsjones.com>
2025-08-18 11:08:21 -0400

    No

--- src/native/libs/System.Security.Cryptography.Native/pal_evp.c
+++ src/native/libs/System.Security.Cryptography.Native/pal_evp.c
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-// #include "ossl_disable_legacy.h"
 #include "openssl.h"
 #include "pal_evp.h"
 #include "pal_utilities.h"
