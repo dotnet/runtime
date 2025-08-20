da6467a7db422aa61141ab14d57454f7d9f6bac8
Katelyn Gadd <kg@luminance.org>
2025-08-20 11:51:19 -0700

    Update src/coreclr/vm/eeconfig.cpp
    Co-authored-by: Jan Kotas <jkotas@microsoft.com>

--- src/coreclr/vm/eeconfig.cpp
+++ src/coreclr/vm/eeconfig.cpp
@@ -639,6 +639,8 @@ HRESULT EEConfig::sync()
 #if defined(FEATURE_INTERPRETER)
     if (fTieredCompilation)
     {
+        // Disable tiered compilation for interpreter testing. Tiered compilation and interpreter 
+        // do not work well together currently.
         LPWSTR pwzInterpreterMaybe;
         IfFailThrow(CLRConfig::GetConfigValue(CLRConfig::EXTERNAL_Interpreter, &pwzInterpreterMaybe));
         if (pwzInterpreterMaybe && pwzInterpreterMaybe[0] != 0)
