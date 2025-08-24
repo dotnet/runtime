5439d1fb1e4fd75a69dbf38398a27c10054ba7c0
Egor Bogatov <egorbo@gmail.com>
2025-08-24 21:45:18 +0200

    Update importer.cpp

--- src/coreclr/jit/importer.cpp
+++ src/coreclr/jit/importer.cpp
@@ -10114,7 +10114,7 @@ void Compiler::impImportBlockCode(BasicBlock* block)
                                 ssize_t  profiledValue = 0;
                                 uint32_t likelihood    = 0;
                                 if (pickProfiledValue(opcodeOffs, &likelihood, &profiledValue) && (likelihood >= 50) &&
-                                    profiledValue >= 0 && profiledValue <= (ssize_t)getUnrollThreshold(UnrollKind::Memset))
+                                    ((uint32_t)profiledValue <= UINT_MAX))
                                 {
                                     assert(FitsIn<int>(profiledValue));
 
