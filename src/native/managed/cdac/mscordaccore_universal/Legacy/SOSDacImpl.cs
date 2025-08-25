67d8ca6fde2b9c144714c41fcb1f3c2e43008137
Rachel <rachel.jarvi@gmail.com>
2025-08-25 16:52:17 -0700

    Adding GetMethodDescPtrFromFrame cDAC API (#118935)
    * Adding GetMethodDescPtrFromFrame cDAC API
    
    Co-authored-by: Max Charlamb <44248479+max-charlamb@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Max Charlamb <44248479+max-charlamb@users.noreply.github.com>

--- src/native/managed/cdac/mscordaccore_universal/Legacy/SOSDacImpl.cs
+++ src/native/managed/cdac/mscordaccore_universal/Legacy/SOSDacImpl.cs
@@ -1311,7 +1311,40 @@ internal sealed unsafe partial class SOSDacImpl
     }
 
     int ISOSDacInterface.GetMethodDescPtrFromFrame(ClrDataAddress frameAddr, ClrDataAddress* ppMD)
-        => _legacyImpl is not null ? _legacyImpl.GetMethodDescPtrFromFrame(frameAddr, ppMD) : HResults.E_NOTIMPL;
+    {
+        int hr = HResults.S_OK;
+        try
+        {
+            if (frameAddr == 0 || ppMD == null)
+                throw new ArgumentException();
+
+            IStackWalk stackWalkContract = _target.Contracts.StackWalk;
+            TargetPointer methodDescPtr = stackWalkContract.GetMethodDescPtr(frameAddr.ToTargetPointer(_target));
+            if (methodDescPtr == TargetPointer.Null)
+                throw new ArgumentException();
+
+            _target.Contracts.RuntimeTypeSystem.GetMethodDescHandle(methodDescPtr); // validation
+            *ppMD = methodDescPtr.ToClrDataAddress(_target);
+        }
+        catch (System.Exception ex)
+        {
+            hr = ex.HResult;
+        }
+#if DEBUG
+        if (_legacyImpl is not null)
+        {
+            ClrDataAddress ppMDLocal;
+            int hrLocal = _legacyImpl.GetMethodDescPtrFromFrame(frameAddr, &ppMDLocal);
+
+            Debug.Assert(hrLocal == hr);
+            if (hr == HResults.S_OK)
+            {
+                Debug.Assert(*ppMD == ppMDLocal);
+            }
+        }
+#endif
+        return hr;
+    }
     int ISOSDacInterface.GetMethodDescPtrFromIP(ClrDataAddress ip, ClrDataAddress* ppMD)
     {
         if (ip == 0 || ppMD == null)
