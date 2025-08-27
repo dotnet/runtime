2c97835f2b10472de31c32ffec71fed0fc3d2d1b
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2025-08-27 16:26:26 +0200

    JIT: Disallow widening tailcalls in async functions
    The JIT allows tailcalls that depend on widening happening in the
    callee: e.g. an int32 returning function can tailcall an int16 returning
    function because the managed ABI specifies that the callee will widen
    anyway.
    
    However, this is not legal for async methods. For async methods the
    int32 and int16 returning functions come with two different resumption
    stubs that are responsible for propagating the returned result into the
    next continuation in asynchronous cases. Allowing an int32 -> int16
    tailcall means only a 16-bit value gets propagated into a caller that
    expects 32 bits to be written into its continuation.
    
    Fix #119060

---
+++ src/tests/async/sign-changing-tailcall/sign-changing-tailcall.cs
@@ -0,0 +1,29 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Xunit;
+
+public class Async2Small
+{
+    [Fact]
+    public static void TestEntryPoint()
+    {
+        uint vr0 = (uint)M29().GetAwaiter().GetResult();
+        Assert.Equal(uint.MaxValue, vr0);
+    }
+
+    private static async Task<short> M29()
+    {
+        return await M40();
+    }
+
+    private static sbyte s_38 = -1;
+    private static async Task<sbyte> M40()
+    {
+        await Task.Yield();
+        return s_38;
+    }
+}
