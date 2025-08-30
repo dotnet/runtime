50877c5a4693a34eb3cf2177a270861d03f261c5
Gleb Popov <6yearold@gmail.com>
2025-08-30 12:04:33 +0300

    Make the InvalidSysctl exception more descriptive
    The parameter being acted on by sysctl() call is determined by an arbitrarily
    sized array of integers called MIB. The current version of the code only
    prints the first element of the MIB, which isn't descriptive at all. Change it
    to print all elements of the MIB tuple.
    
    While there, make type signatures of low-level functions match their C versions.
    
    The problem was raised by a FreeBSD user:
    https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=289022

--- src/libraries/Common/src/Interop/BSD/System.Native/Interop.Sysctl.cs
+++ src/libraries/Common/src/Interop/BSD/System.Native/Interop.Sysctl.cs
@@ -6,8 +6,6 @@ using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Text;
 
-using size_t = System.IntPtr;
-
 // This implements shim for sysctl calls.
 // They are available on BSD systems - FreeBSD, OSX and others.
 // Linux has sysctl() but it is deprecated as well as it is missing sysctlbyname()
@@ -17,68 +15,69 @@ internal static partial class Interop
     internal static partial class Sys
     {
         [LibraryImport(Libraries.SystemNative, EntryPoint = "SystemNative_Sysctl", SetLastError = true)]
-        private static unsafe partial int Sysctl(int* name, int namelen, void* value, size_t* len);
+        private static unsafe partial int Sysctl(int* name, uint namelen, void* value, nuint* len);
 
-        // This is 'raw' sysctl call, only wrapped to allocate memory if needed
-        // caller always needs to free returned buffer using  Marshal.FreeHGlobal()
-
-        internal static unsafe void Sysctl(ReadOnlySpan<int> name, ref byte* value, ref int len)
+        private static unsafe void Sysctl(ReadOnlySpan<int> name, ref byte* value, ref uint len)
         {
-            fixed (int* ptr = &MemoryMarshal.GetReference(name))
+            fixed (int* name_ptr = &MemoryMarshal.GetReference(name))
             {
-                Sysctl(ptr, name.Length, ref value, ref len);
+                uint name_len = (uint)name.Length;
+                nuint bytesLength = len;
+                int ret;
+                bool autoSize = (value == null && len == 0);
+
+                if (autoSize)
+                {
+                    // do one try to see how much data we need
+                    ret = Sysctl(name, name_len, value, &bytesLength);
+                    if (ret != 0)
+                    {
+                        ThrowInvalidSysctlException(name, Marshal.GetLastPInvokeError());
+                    }
+                    value = (byte*)NativeMemory.Alloc(bytesLength);
+                }
+
+                ret = Sysctl(name, name_len, value, &bytesLength);
+                while (autoSize && ret != 0 && GetLastErrorInfo().Error == Error.ENOMEM)
+                {
+                    // Do not realloc here: we don't care about
+                    // previous contents, and proper checking of value returned
+                    // will make code more complex.
+                    NativeMemory.Free(value);
+                    if ((int)bytesLength == int.MaxValue)
+                    {
+                        throw new OutOfMemoryException();
+                    }
+                    if ((int)bytesLength >= int.MaxValue / 2)
+                    {
+                        bytesLength = int.MaxValue;
+                    }
+                    else
+                    {
+                        bytesLength *= 2;
+                    }
+                    value = (byte*)NativeMemory.Alloc(bytesLength);
+                    ret = Sysctl(name, name_len, value, &bytesLength);
+                }
+                if (ret != 0)
+                {
+                    nint errno = Marshal.GetLastPInvokeError();
+                    if (autoSize)
+                    {
+                        NativeMemory.Free(value);
+                    }
+                    ThrowInvalidSysctlException(name, errno);
+                }
+
+                len = (uint)bytesLength;
             }
         }
 
-        private static unsafe void Sysctl(int* name, int name_len, ref byte* value, ref int len)
+        static void ThrowInvalidSysctlException(ReadOnlySpan<int> name, nint errno)
         {
-            nint bytesLength = len;
-            int ret = -1;
-            bool autoSize = (value == null && len == 0);
-
-            if (autoSize)
-            {
-                // do one try to see how much data we need
-                ret = Sysctl(name, name_len, value, &bytesLength);
-                if (ret != 0)
-                {
-                    throw new InvalidOperationException(SR.Format(SR.InvalidSysctl, *name, Marshal.GetLastPInvokeError()));
-                }
-                value = (byte*)Marshal.AllocHGlobal((int)bytesLength);
-            }
-
-            ret = Sysctl(name, name_len, value, &bytesLength);
-            while (autoSize && ret != 0 && GetLastErrorInfo().Error == Error.ENOMEM)
-            {
-                // Do not use ReAllocHGlobal() here: we don't care about
-                // previous contents, and proper checking of value returned
-                // will make code more complex.
-                Marshal.FreeHGlobal((IntPtr)value);
-                if ((int)bytesLength == int.MaxValue)
-                {
-                    throw new OutOfMemoryException();
-                }
-                if ((int)bytesLength >= int.MaxValue / 2)
-                {
-                    bytesLength = int.MaxValue;
-                }
-                else
-                {
-                    bytesLength = (int)bytesLength * 2;
-                }
-                value = (byte*)Marshal.AllocHGlobal(bytesLength);
-                ret = Sysctl(name, name_len, value, &bytesLength);
-            }
-            if (ret != 0)
-            {
-                if (autoSize)
-                {
-                    Marshal.FreeHGlobal((IntPtr)value);
-                }
-                throw new InvalidOperationException(SR.Format(SR.InvalidSysctl, *name, Marshal.GetLastPInvokeError()));
-            }
-
-            len = (int)bytesLength;
+            throw new InvalidOperationException(SR.Format(SR.InvalidSysctl,
+                                                          string.Join(",", name.ToArray())
+                                                          errno));
         }
     }
 }
