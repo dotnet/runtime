c0bf4d8066e73fc17afec6872e975d2b9bdb4d21
SingleAccretion <AccretionMail@yandex.ru>
2025-09-02 02:28:11 +0300

    Acquire the WASI SDK for runtime build automatically and centrally

--- eng/download-wasi-sdk.ps1
+++ eng/download-wasi-sdk.ps1
@@ -4,17 +4,13 @@ param(
      [Parameter()]
      [string]$WasiSdkVersion,
      [Parameter()]
-     [string]$WasiSdkPath,
-     [Parameter()]
-     [string]$WasiLocalPath
+     [string]$WasiSdkPath
 )
 
 Set-StrictMode -version 2.0
 $ErrorActionPreference='Stop'
 $ProgressPreference = 'SilentlyContinue'
 
-New-Item -Path $WasiSdkPath -ItemType "directory"
 Invoke-WebRequest -Uri $WasiSdkUrl -OutFile ./wasi-sdk-$WasiSdkVersion-x86_64-windows.tar.gz
-tar --strip-components=1 -xzf ./wasi-sdk-$WasiSdkVersion-x86_64-windows.tar.gz -C $WasiSdkPath
-Copy-Item $WasiLocalPath/wasi-sdk-version.txt $WasiSdkPath/wasi-sdk-version.txt
+tar --strip-components=1 -xzmf ./wasi-sdk-$WasiSdkVersion-x86_64-windows.tar.gz -C $WasiSdkPath
 Remove-Item ./wasi-sdk-$WasiSdkVersion-x86_64-windows.tar.gz -fo
