9bfda231242f9ee064f01a799c65a8d475707799
Radek Zikmund <r.zikmund.rz@gmail.com>
2025-09-08 14:44:42 +0200

    Export native zstd symbols

--- src/native/external/zstd.cmake
+++ src/native/external/zstd.cmake
@@ -11,18 +11,17 @@ set(ZSTD_BUILD_PROGRAMS OFF)
 set(ZSTD_BUILD_TESTS OFF)
 set(ZSTD_BUILD_CONTRIB OFF)
 set(ZSTD_BUILD_STATIC ON)
+set(ZSTD_BUILD_SHARED OFF)
+
 set(__CURRENT_BUILD_SHARED_LIBS ${BUILD_SHARED_LIBS})
 set(BUILD_SHARED_LIBS OFF)
 FetchContent_MakeAvailable(zstd)
 set(BUILD_SHARED_LIBS ${__CURRENT_BUILD_SHARED_LIBS})
 
-# Set up the variables to match the brotli pattern
-set(ZSTD_INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}/zstd/lib")
-set(ZSTD_LIBRARIES_CORE libzstd_static)
-set(ZSTD_LIBRARIES ${ZSTD_LIBRARIES_CORE})
-mark_as_advanced(ZSTD_INCLUDE_DIRS)
-mark_as_advanced(ZSTD_LIBRARIES)
+if (MSVC)
+    # Make the static library export its symbols so that we can P/Invoke them from Managed code
+    set_property(TARGET libzstd_static APPEND PROPERTY COMPILE_DEFINITIONS "ZSTD_DLL_EXPORT=1")
+endif()
 
 # disable warnings about lossy conversions
-target_compile_options(libzstd_shared PRIVATE $<$<COMPILE_LANG_AND_ID:C,MSVC>:/wd4242>)
 target_compile_options(libzstd_static PRIVATE $<$<COMPILE_LANG_AND_ID:C,MSVC>:/wd4242>)
