e74bceae26b37aa9eee2448c8d5f51cb5450ce91
Sven Boemer <sbomer@gmail.com>
2025-09-08 21:37:11 +0000

    PR feedback

--- src/tools/illink/src/ILLink.RoslynAnalyzer/TrimAnalysis/GenericArgumentDataFlow.cs
+++ src/tools/illink/src/ILLink.RoslynAnalyzer/TrimAnalysis/GenericArgumentDataFlow.cs
@@ -169,8 +169,11 @@ namespace ILLink.RoslynAnalyzer.TrimAnalysis
         {
             foreach (var typeParameter in typeParameters)
             {
+                if (typeParameter.HasConstructorConstraint)
+                    return true;
+
                 var genericParameterValue = new GenericParameterValue(typeParameter);
-                if (genericParameterValue.DynamicallyAccessedMemberTypes != DynamicallyAccessedMemberTypes.None || typeParameter.HasConstructorConstraint)
+                if (genericParameterValue.DynamicallyAccessedMemberTypes != DynamicallyAccessedMemberTypes.None)
                     return true;
             }
 
