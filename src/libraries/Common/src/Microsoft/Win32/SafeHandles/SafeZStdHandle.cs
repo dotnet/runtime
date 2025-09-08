2eaa008c63587d8667faab2b6cd071467a4dedde
Radek Zikmund <r.zikmund.rz@gmail.com>
2025-09-08 19:32:19 +0200

    Dictionary lifetime management

--- src/libraries/Common/src/Microsoft/Win32/SafeHandles/SafeZStdHandle.cs
+++ src/libraries/Common/src/Microsoft/Win32/SafeHandles/SafeZStdHandle.cs
@@ -2,18 +2,38 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 
 namespace Microsoft.Win32.SafeHandles
 {
     internal sealed class SafeZStdCompressHandle : SafeHandle
     {
+        internal SafeZStdCDictHandle? _dictionary;
         public SafeZStdCompressHandle() : base(IntPtr.Zero, true) { }
 
         protected override bool ReleaseHandle()
         {
             Interop.Zstd.ZSTD_freeCCtx(handle);
+
+            // release the addref we took in SetDictionary
+            _dictionary?.DangerousRelease();
             return true;
+        }
+
+        public void SetDictionary(SafeZStdCDictHandle dictionary)
+        {
+            Debug.Assert(dictionary != null);
+
+            bool added = false;
+            dictionary.DangerousAddRef(ref added);
+            _dictionary = dictionary;
+
+            nuint result = Interop.Zstd.ZSTD_CCtx_refCDict(this, dictionary);
+            if (Interop.Zstd.ZSTD_isError(result) != 0)
+            {
+                throw new Interop.Zstd.ZstdNativeException(SR.ZStandardEncoder_DictionaryAttachFailed);
+            }
         }
 
         public override bool IsInvalid => handle == IntPtr.Zero;
@@ -21,12 +41,31 @@ namespace Microsoft.Win32.SafeHandles
 
     internal sealed class SafeZStdDecompressHandle : SafeHandle
     {
+        internal SafeZStdDDictHandle? _dictionary;
         public SafeZStdDecompressHandle() : base(IntPtr.Zero, true) { }
 
         protected override bool ReleaseHandle()
         {
             Interop.Zstd.ZSTD_freeDCtx(handle);
+
+            // release the addref we took in SetDictionary
+            _dictionary?.DangerousRelease();
             return true;
+        }
+
+        public void SetDictionary(SafeZStdDDictHandle dictionary)
+        {
+            Debug.Assert(dictionary != null);
+
+            bool added = false;
+            dictionary.DangerousAddRef(ref added);
+            _dictionary = dictionary;
+
+            nuint result = Interop.Zstd.ZSTD_DCtx_refDDict(this, dictionary);
+            if (Interop.Zstd.ZSTD_isError(result) != 0)
+            {
+                throw new Interop.Zstd.ZstdNativeException(SR.ZStandardDecoder_DictionaryAttachFailed);
+            }
         }
 
         public override bool IsInvalid => handle == IntPtr.Zero;
