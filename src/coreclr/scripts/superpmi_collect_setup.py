1f7a02ed33937ca92e46b398f50c813512c9328e
Steve Pfister <stpfiste@microsoft.com>
2025-09-17 12:59:20 -0400

    More bumps

--- src/coreclr/scripts/superpmi_collect_setup.py
+++ src/coreclr/scripts/superpmi_collect_setup.py
@@ -472,7 +472,7 @@ def main(main_args):
             if arch == "arm64": # public osx_arm64
                 helix_queue = "osx.13.arm64.open"
             else: # public osx_x64
-                helix_queue = "OSX.1200.Amd64.Open"
+                helix_queue = "OSX.13.Amd64.Open"
     else:
         if platform_name == "windows":
             if arch == "arm64": # internal windows_arm64
