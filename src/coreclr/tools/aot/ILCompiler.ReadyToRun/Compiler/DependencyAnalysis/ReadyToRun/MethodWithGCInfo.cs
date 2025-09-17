1620f1d7768989d4467e1064c9b0dcfd767eb9d0
Jeremy Koritzinsky <jekoritz@microsoft.com>
2025-09-17 16:35:07 -0700

    Fix sections for code to avoid AV issues

--- src/coreclr/tools/aot/ILCompiler.ReadyToRun/Compiler/DependencyAnalysis/ReadyToRun/MethodWithGCInfo.cs
+++ src/coreclr/tools/aot/ILCompiler.ReadyToRun/Compiler/DependencyAnalysis/ReadyToRun/MethodWithGCInfo.cs
@@ -302,7 +302,7 @@ namespace ILCompiler.DependencyAnalysis.ReadyToRun
 
         public override ObjectNodeSection GetSection(NodeFactory factory)
         {
-            return factory.Target.IsWindows ? ObjectNodeSection.ManagedCodeWindowsContentSection : ObjectNodeSection.ManagedCodeUnixContentSection;
+            return factory.Target.IsWindows ? ObjectNodeSection.TextSection;
         }
 
         public FrameInfo[] FrameInfos => _frameInfos;
