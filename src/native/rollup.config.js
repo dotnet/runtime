b8c628dbe9dfc087b869ca94aa9f6fd4b6bb00bf
pavelsavara <pavel.savara@gmail.com>
2025-09-20 18:02:14 +0200

    more cmake

--- src/native/rollup.config.js
+++ src/native/rollup.config.js
@@ -2,19 +2,28 @@
 //! The .NET Foundation licenses this file to you under the MIT license.
 
 import { promises as fs } from "fs";
+import path from "path";
 import { artifactsObjDir, configuration } from "./rollup.config.defines.js"
 
 /**
  * This would be replace by real rollup with TypeScript compilation in next iteration
  */
 
-await fs.copyFile("./corehost/browserhost/loader/dotnet.d.ts",
-    artifactsObjDir + `/coreclr/browser.wasm.${configuration}/corehost/dotnet.d.ts`);
-await fs.copyFile("./corehost/browserhost/loader/dotnet.js",
-    artifactsObjDir + `/coreclr/browser.wasm.${configuration}/corehost/dotnet.js`);
-await fs.copyFile("./libs/System.Native.Browser/libSystem.Native.Browser.js",
-    artifactsObjDir + `/native/browser-${configuration}-wasm/System.Native.Browser/libSystem.Native.Browser.js`);
-await fs.copyFile("./libs/System.Runtime.InteropServices.JavaScript.Native/dotnet.runtime.js",
-    artifactsObjDir + `/native/browser-${configuration}-wasm/System.Runtime.InteropServices.JavaScript.Native/dotnet.runtime.js`);
-await fs.copyFile("./libs/System.Runtime.InteropServices.JavaScript.Native/libSystem.Runtime.InteropServices.JavaScript.Native.js",
-    artifactsObjDir + `/native/browser-${configuration}-wasm/System.Runtime.InteropServices.JavaScript.Native/libSystem.Runtime.InteropServices.JavaScript.Native.js`);
+const copies = [
+    ["./corehost/browserhost/loader/dotnet.d.ts",
+        `${artifactsObjDir}/coreclr/browser.wasm.${configuration}/corehost/dotnet.d.ts`],
+    ["./corehost/browserhost/loader/dotnet.js",
+        `${artifactsObjDir}/coreclr/browser.wasm.${configuration}/corehost/dotnet.js`],
+    ["./libs/System.Native.Browser/libSystem.Native.Browser.js",
+        `${artifactsObjDir}/native/browser-${configuration}-wasm/System.Native.Browser/libSystem.Native.Browser.js`],
+    ["./libs/System.Runtime.InteropServices.JavaScript.Native/dotnet.runtime.js",
+        `${artifactsObjDir}/native/browser-${configuration}-wasm/System.Runtime.InteropServices.JavaScript.Native/dotnet.runtime.js`],
+    ["./libs/System.Runtime.InteropServices.JavaScript.Native/libSystem.Runtime.InteropServices.JavaScript.Native.js",
+        `${artifactsObjDir}/native/browser-${configuration}-wasm/System.Runtime.InteropServices.JavaScript.Native/libSystem.Runtime.InteropServices.JavaScript.Native.js`]
+];
+
+for (const [src, dest] of copies) {
+    const destDir = path.dirname(dest);
+    await fs.mkdir(destDir, { recursive: true });
+    await fs.copyFile(src, dest);
+}
