e2a178fef2cd7efd6fad814b5bfc2b30a72b5c7f
Steve Molloy <smolloy@microsoft.com>
2025-09-24 22:50:45 -0700

    Make two-digit year boundary test more resilient to changes in DateTimeFormat.Calendar.TwoDigitYearMax

--- src/libraries/System.ServiceModel.Syndication/tests/System/ServiceModel/Syndication/Rfc822DateParsingTests.cs
+++ src/libraries/System.ServiceModel.Syndication/tests/System/ServiceModel/Syndication/Rfc822DateParsingTests.cs
@@ -20,9 +20,6 @@ namespace System.ServiceModel.Syndication.Tests
         [InlineData("02 Jun 03 09:39 GMT", 2003, 6, 2, 9, 39, 0)]
         [InlineData("Mon, 2 Jun 03 09:39 GMT", 2003, 6, 2, 9, 39, 0)]
         [InlineData("Mon, 02 Jun 03 09:39 GMT", 2003, 6, 2, 9, 39, 0)]
-        // As of .Net 8.0, CultureInfo.CurrentCulture.DateTimeFormat.Calendar.TwoDigitYearMax is 2049 for invariant and en-US cultures
-        [InlineData("2 Jun 50 09:39 GMT", 1950, 6, 2, 9, 39, 0)]
-        [InlineData("2 Jun 49 09:39 GMT", 2049, 6, 2, 9, 39, 0)]
         public void Rss20ItemFormatter_Read_SingleDigitDay_And_TwoDigitYear(string pubDate, int year, int month, int day, int hour, int minute, int second)
         {
             string xml = $"<item><pubDate>{pubDate}</pubDate></item>";
@@ -36,16 +33,9 @@ namespace System.ServiceModel.Syndication.Tests
 
         [SkipOnTargetFramework(TargetFrameworkMonikers.NetFramework)]   // NetFx requires double digit dates and four digit years
         [Theory]
-        [InlineData("Mon, 2 Jun 2003 09:39:21 +0000", 2003, 6, 2, 9, 39, 21)]
-        [InlineData("Mon, 2 Jun 2003 09:39:21 -0000", 2003, 6, 2, 9, 39, 21)]
-        [InlineData("Mon, 2 Jun 2003 09:39:21 UT", 2003, 6, 2, 9, 39, 21)]
         [InlineData("Mon, 02 Jun 03 09:39:21 +0000", 2003, 6, 2, 9, 39, 21)]
         [InlineData("Mon, 02 Jun 03 09:39:21 -0000", 2003, 6, 2, 9, 39, 21)]
         [InlineData("Mon, 02 Jun 03 09:39:21 UT", 2003, 6, 2, 9, 39, 21)]
-        // As of .Net 8.0, CultureInfo.CurrentCulture.DateTimeFormat.Calendar.TwoDigitYearMax is 2049 for invariant and en-US cultures
-        [InlineData("02 Jun 50 09:39:21 +0000", 1950, 6, 2, 9, 39, 21)]
-        [InlineData("02 Jun 50 09:39:21 -0000", 1950, 6, 2, 9, 39, 21)]
-        [InlineData("02 Jun 49 09:39:21 UT", 2049, 6, 2, 9, 39, 21)]
         public void Rss20ItemFormatter_Read_SingleDigitDay_NormalizedTimeZones(string pubDate, int year, int month, int day, int hour, int minute, int second)
         {
             string xml = $"<item><pubDate>{pubDate}</pubDate></item>";
@@ -55,6 +45,38 @@ namespace System.ServiceModel.Syndication.Tests
             formatter.ReadFrom(reader);
             DateTimeOffset expectedUtc = new DateTimeOffset(year, month, day, hour, minute, second, TimeSpan.Zero);
             Assert.Equal(expectedUtc, formatter.Item.PublishDate);
+        }
+
+        [SkipOnTargetFramework(TargetFrameworkMonikers.NetFramework)]   // NetFx requires double digit dates and four digit years
+        [Theory]
+        [InlineData("2 Jun {year} 09:39 GMT", 6, 2, 9, 39, 0)]
+        [InlineData("02 Jun {year} 09:39:21 +0000", 6, 2, 9, 39, 21)]
+        [InlineData("02 Jun {year} 09:39:21 -0000", 6, 2, 9, 39, 21)]
+        [InlineData("02 Jun {year} 09:39:21 UT", 6, 2, 9, 39, 21)]
+        public void Rss20ItemFormatter_Read_SingleDigitDay_And_TwoDigitYear_Max(string pubDate, int month, int day, int hour, int minute, int second)
+        {
+            // As of .Net 8.0, CultureInfo.CurrentCulture.DateTimeFormat.Calendar.TwoDigitYearMax is 2049 for invariant and en-US cultures
+            var maxDate = CultureInfo.CurrentCulture.DateTimeFormat.Calendar.TwoDigitYearMax % 100;
+
+            // Test under/at the 2-digit year max threshold
+            var underDate = pubDate.Replace("{year}", maxDate.ToString());
+            string xml = $"<item><pubDate>{underDate}</pubDate></item>";
+            using var stringReader = new StringReader(xml);
+            using var reader = XmlReader.Create(stringReader);
+            var formatter = new Rss20ItemFormatter();
+            formatter.ReadFrom(reader);
+            DateTimeOffset expectedUnder = new DateTimeOffset(2000 + maxDate, month, day, hour, minute, second, TimeSpan.Zero);
+            Assert.Equal(expectedUnder, formatter.Item.PublishDate);
+
+            // Test over the 2-digit year max threshold
+            var overDate = pubDate.Replace("{year}", (maxDate + 1).ToString());
+            xml = $"<item><pubDate>{overDate}</pubDate></item>";
+            using var stringReaderOver = new StringReader(xml);
+            using var readerOver = XmlReader.Create(stringReaderOver);
+            formatter = new Rss20ItemFormatter();
+            formatter.ReadFrom(readerOver);
+            DateTimeOffset expectedOver = new DateTimeOffset(1900 + maxDate + 1, month, day, hour, minute, second, TimeSpan.Zero);
+            Assert.Equal(expectedOver, formatter.Item.PublishDate);
         }
     }
 }
