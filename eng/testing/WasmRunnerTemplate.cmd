b4bd20761966da2a08e19142a652cb9f52a070a3
Ilona Tomkowicz <32700855+ilonatommy@users.noreply.github.com>
2025-09-29 12:53:14 +0000

    Backport changes.

--- eng/testing/WasmRunnerTemplate.cmd
+++ eng/testing/WasmRunnerTemplate.cmd
@@ -34,11 +34,7 @@ if [%XHARNESS_COMMAND%] == [] (
 
 if /I [%XHARNESS_COMMAND%] == [test] (
     if [%JS_ENGINE%] == [] (
-        if /I [%SCENARIO%] == [WasmTestOnNodeJS] (
-            set "JS_ENGINE=--engine^=NodeJS"
-        ) else (
-            set "JS_ENGINE=--engine^=V8"
-        )
+        set "JS_ENGINE=--engine^=V8"
     )
     if [%MAIN_JS%] == [] (
         set "MAIN_JS=--js-file^=test-main.js"
@@ -46,12 +42,6 @@ if /I [%XHARNESS_COMMAND%] == [test] (
 
     if [%JS_ENGINE_ARGS%] == [] (
         set "JS_ENGINE_ARGS=--engine-arg^=--stack-trace-limit^=1000"
-        if /I NOT [%SCENARIO%] == [WasmTestOnNodeJS] (
-            set "JS_ENGINE_ARGS=%JS_ENGINE_ARGS% --engine-arg^=--module"
-        )
-        if /I [%SCENARIO%] == [WasmTestOnNodeJS] (
-            set "JS_ENGINE_ARGS=%JS_ENGINE_ARGS% --engine-arg^=--experimental-wasm-eh"
-        )
     )
 ) else (
     if [%BROWSER_PATH%] == [] if not [%HELIX_CORRELATION_PAYLOAD%] == [] (
