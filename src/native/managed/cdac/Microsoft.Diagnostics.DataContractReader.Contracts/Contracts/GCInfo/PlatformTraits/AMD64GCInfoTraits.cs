58944f484ffe59ea2b125568632ff2619736d7b2
Max Charlamb <44248479+max-charlamb@users.noreply.github.com>
2025-10-01 14:32:54 -0400

    fix AMD64 traits

--- src/native/managed/cdac/Microsoft.Diagnostics.DataContractReader.Contracts/Contracts/GCInfo/PlatformTraits/AMD64GCInfoTraits.cs
+++ src/native/managed/cdac/Microsoft.Diagnostics.DataContractReader.Contracts/Contracts/GCInfo/PlatformTraits/AMD64GCInfoTraits.cs
@@ -14,32 +14,26 @@ internal class AMD64GCInfoTraits : IGCInfoTraits
     public static uint NormalizeCodeOffset(uint offset) => offset;
     public static int DenormalizeStackSlot(int x) => x << 3;
 
-    public static int CODE_LENGTH_ENCBASE => 8;
-    public static int NORM_PROLOG_SIZE_ENCBASE => 5;
-    public static int NORM_EPILOG_SIZE_ENCBASE => 3;
+    public static int GENERICS_INST_CONTEXT_STACK_SLOT_ENCBASE => 6;
+
     public static int GS_COOKIE_STACK_SLOT_ENCBASE => 6;
-    public static int GENERICS_INST_CONTEXT_STACK_SLOT_ENCBASE => 5;
-    public static int STACK_BASE_REGISTER_ENCBASE => 1;
+    public static int CODE_LENGTH_ENCBASE => 8;
+
+    public static int STACK_BASE_REGISTER_ENCBASE => 3;
+
     public static int SIZE_OF_EDIT_AND_CONTINUE_PRESERVED_AREA_ENCBASE => 4;
     public static int REVERSE_PINVOKE_FRAME_ENCBASE => 6;
-    public static int NUM_SAFE_POINTS_ENCBASE => 3;
-    public static int NUM_INTERRUPTIBLE_RANGES_ENCBASE => 1;
-
-    public static int INTERRUPTIBLE_RANGE_DELTA1_ENCBACE => 6;
-
-    public static int INTERRUPTIBLE_RANGE_DELTA2_ENCBACE => 6;
-
     public static int NUM_REGISTERS_ENCBASE => 2;
-
     public static int NUM_STACK_SLOTS_ENCBASE => 2;
-
     public static int NUM_UNTRACKED_SLOTS_ENCBASE => 1;
-
+    public static int NORM_PROLOG_SIZE_ENCBASE => 5;
+    public static int NORM_EPILOG_SIZE_ENCBASE => 3;
+    public static int INTERRUPTIBLE_RANGE_DELTA1_ENCBACE => 6;
+    public static int INTERRUPTIBLE_RANGE_DELTA2_ENCBACE => 6;
     public static int REGISTER_ENCBASE => 3;
-
     public static int REGISTER_DELTA_ENCBASE => 2;
-
     public static int STACK_SLOT_ENCBASE => 6;
-
     public static int STACK_SLOT_DELTA_ENCBASE => 4;
+    public static int NUM_SAFE_POINTS_ENCBASE => 2;
+    public static int NUM_INTERRUPTIBLE_RANGES_ENCBASE => 1;
 }
