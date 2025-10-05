d30d7d6d1499344c7f2f86fc60673c6674676845
Egor Bogatov <egorbo@gmail.com>
2025-10-05 15:15:30 +0200

    Apply Miha's FB

--- src/libraries/System.Net.Http.WinHttpHandler/src/System/Net/Http/NetEventSource.WinHttpHandler.cs
+++ src/libraries/System.Net.Http.WinHttpHandler/src/System/Net/Http/NetEventSource.WinHttpHandler.cs
@@ -1,15 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Diagnostics.Tracing;
-
 namespace System.Net
 {
-    [EventSource(Name = NetEventSourceName)]
-    internal sealed partial class NetEventSource
-    {
-        private const string NetEventSourceName = "Private.InternalDiagnostics.System.Net.Http.WinHttpHandler";
-
-        public NetEventSource() : base(NetEventSourceName, EventSourceSettings.EtwManifestEventFormat) { }
-    }
+    internal sealed partial class NetEventSource { }
 }
