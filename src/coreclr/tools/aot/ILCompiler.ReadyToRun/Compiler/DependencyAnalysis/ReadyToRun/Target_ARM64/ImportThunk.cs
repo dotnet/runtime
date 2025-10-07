2a6187eee27c13ef57dff66222612db52101b9a7
Jeremy Koritzinsky <jekoritz@microsoft.com>
2025-10-06 17:00:48 -0700

    Change the ARM64 import thunks to only emit aligned relocs.

--- src/coreclr/tools/aot/ILCompiler.ReadyToRun/Compiler/DependencyAnalysis/ReadyToRun/Target_ARM64/ImportThunk.cs
+++ src/coreclr/tools/aot/ILCompiler.ReadyToRun/Compiler/DependencyAnalysis/ReadyToRun/Target_ARM64/ImportThunk.cs
@@ -2,7 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
-
+using System.Diagnostics;
 using ILCompiler.DependencyAnalysis.ARM64;
 
 namespace ILCompiler.DependencyAnalysis.ReadyToRun
@@ -11,19 +11,43 @@ namespace ILCompiler.DependencyAnalysis.ReadyToRun
     /// This node emits a thunk calling DelayLoad_Helper with a given instance signature
     /// to populate its indirection cell.
     /// </summary>
-    public partial class ImportThunk
+    public partial class ImportThunk : ISymbolNode
     {
+        int ISymbolNode.Offset
+        {
+            get
+            {
+                // We stuff the reloc to the module import pointer before the start of the thunk
+                // to ensure alignment.
+                // The thunk itself starts immediately after the reloc.
+                // We don't need this for an Eager thunk.
+                if (_thunkKind == Kind.Eager)
+                    return base.Offset;
+                return base.Offset + 8;
+            }
+        }
+
         protected override void EmitCode(NodeFactory factory, ref ARM64Emitter instructionEncoder, bool relocsOnly)
         {
+            if (_thunkKind == Kind.Eager)
+            {
+                instructionEncoder.EmitJMP(_helperCell);
+                return;
+            }
+
+            instructionEncoder.Builder.RequireInitialPointerAlignment();
+            Debug.Assert(instructionEncoder.Builder.CountBytes == 0);
 
             switch (_thunkKind)
             {
-                case Kind.Eager:
-                    break;
-
                 case Kind.DelayLoadHelper:
                 case Kind.DelayLoadHelperWithExistingIndirectionCell:
                 case Kind.VirtualStubDispatch:
+
+                    instructionEncoder.Builder.EmitReloc(factory.ModuleImport, RelocType.IMAGE_REL_BASED_DIR64);
+
+                    Debug.Assert(instructionEncoder.Builder.CountBytes == ((ISymbolNode)this).Offset);
+
                     // x11 contains indirection cell
                     // Do nothing x11 contains our first param
 
@@ -35,17 +59,21 @@ namespace ILCompiler.DependencyAnalysis.ReadyToRun
                     }
 
                     // Move Module* -> x10
-                    // ldr x10, [PC+0x1c]
-                    instructionEncoder.EmitLDR(Register.X10, 0x1c);
+                    // ldr x10, [PC-0xc]
+                    instructionEncoder.EmitLDR(Register.X10, -0xc);
 
                     // ldr x10, [x10]
                     instructionEncoder.EmitLDR(Register.X10, Register.X10);
                     break;
 
                 case Kind.Lazy:
+                    instructionEncoder.Builder.EmitReloc(factory.ModuleImport, RelocType.IMAGE_REL_BASED_DIR64);
+
+                    Debug.Assert(instructionEncoder.Builder.CountBytes == ((ISymbolNode)this).Offset);
+
                     // Move Module* -> x1
-                    // ldr x1, [PC+0x1c]
-                    instructionEncoder.EmitLDR(Register.X1, 0x1c);
+                    // ldr x1, [PC-0x8]
+                    instructionEncoder.EmitLDR(Register.X1, -0x8);
 
                     // ldr x1, [x1]
                     instructionEncoder.EmitLDR(Register.X1, Register.X1);
@@ -57,10 +85,6 @@ namespace ILCompiler.DependencyAnalysis.ReadyToRun
 
             // branch to helper
             instructionEncoder.EmitJMP(_helperCell);
-
-            // Emit relocation for the Module* load above
-            if (_thunkKind != Kind.Eager)
-                instructionEncoder.Builder.EmitReloc(factory.ModuleImport, RelocType.IMAGE_REL_BASED_DIR64);
         }
     }
 }
