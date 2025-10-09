c4169a68e4adf45ebded5cba9175e603a93232dc
Jeff Handley <jeffhandley@users.noreply.github.com>
2025-10-08 22:54:45 -0700

    Revert use of Range and Index for down-level support
    This reverts commit ed3bcc332336ee3c6a433abd2b0e495814f505fe.

--- src/libraries/System.Text.Json/tests/System.Text.Json.SourceGeneration.Unit.Tests/CompilationHelper.cs
+++ src/libraries/System.Text.Json/tests/System.Text.Json.SourceGeneration.Unit.Tests/CompilationHelper.cs
@@ -889,11 +889,11 @@ namespace System.Text.Json.SourceGeneration.UnitTests
 
                 if (value.EndsWith("\r\n", StringComparison.Ordinal))
                 {
-                    WriteLine(value[..^2]);
+                    WriteLine(value.Substring(0, value.Length - 2));
                 }
                 else if (value.EndsWith("\n", StringComparison.Ordinal))
                 {
-                    WriteLine(value[..^1]);
+                    WriteLine(value.Substring(0, value.Length - 1));
                 }
                 else
                 {
