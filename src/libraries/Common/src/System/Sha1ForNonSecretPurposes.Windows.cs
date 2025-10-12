b60ffd470cf47414856800b10a90a247671eec5c
Jan Kotas <jkotas@microsoft.com>
2025-10-12 16:40:12 -0700

    Update src/libraries/Common/src/System/Sha1ForNonSecretPurposes.Windows.cs

--- src/libraries/Common/src/System/Sha1ForNonSecretPurposes.Windows.cs
+++ src/libraries/Common/src/System/Sha1ForNonSecretPurposes.Windows.cs
@@ -47,7 +47,7 @@ namespace System
                         else
                         {
                             throw new InvalidOperationException();
-                        }                        
+                        }
                     }
                 }
             }
