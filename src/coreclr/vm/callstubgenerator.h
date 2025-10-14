a57bb3d4a2872f28714ff725f80eea237d8d8106
David Wrighton <davidwr@microsoft.com>
2025-10-14 13:50:10 -0700

    [clr-interp] Fix stack alignment of 16 byte or greater aligned structures in the interpreter - Arguments are up to 16 byte aligned - Handle this in the call stub generator, and call emission. (We already did this in the argument input processing in the interpreter
    We need to align these at 16byte boundaries as the various ABIs tend to have a hard requirement around 16 byte alignment, when they don't have such a requirement around 32 or 64 byte alignment.

--- src/coreclr/vm/callstubgenerator.h
+++ src/coreclr/vm/callstubgenerator.h
@@ -145,8 +145,8 @@ private:
         int numArgs = sig.NumFixedArgs() + (sig.HasThis() ? 1 : 0);
 
         // The size of the temporary storage is the size of the CallStubHeader plus the size of the routines array.
-        // The size of the routines array is twice the number of arguments plus one slot for the target method pointer.
-        return sizeof(CallStubHeader) + ((numArgs + 1) * 2 + 1) * sizeof(PCODE);
+        // The size of the routines array is three the number of arguments plus one slot for the target method pointer.
+        return sizeof(CallStubHeader) + ((numArgs + 1) * 3 + 1) * sizeof(PCODE);
     }
     void ComputeCallStub(MetaSig &sig, PCODE *pRoutines);
 };
