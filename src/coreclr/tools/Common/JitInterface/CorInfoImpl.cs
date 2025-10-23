d07760d6a932a5863264328dac5a98cf3a7aec44
Jackson Schuster <36744439+jtschuster@users.noreply.github.com>
2025-10-23 15:08:12 -0700

    Add conditional compilation for ilc

--- src/coreclr/tools/Common/JitInterface/CorInfoImpl.cs
+++ src/coreclr/tools/Common/JitInterface/CorInfoImpl.cs
@@ -1829,7 +1829,11 @@ namespace Internal.JitInterface
             {
                 if (method.IsAsync)
                 {
+#if READYTORUN
                     throw new RequiresRuntimeJitException("RuntimeAsync methods will be jitted at runtime");
+#else
+                    throw new NotImplementedException("RuntimeAsync methods are not supported by nativeaot yet");
+#endif
                 }
                 pResolvedToken.hMethod = ObjectToHandle(method);
 
