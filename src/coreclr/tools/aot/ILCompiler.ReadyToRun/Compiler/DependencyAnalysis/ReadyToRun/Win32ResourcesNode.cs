addf696b78a6c6e275ab0ae02e7b2feaf60d128c
Jeremy Koritzinsky <jekoritz@microsoft.com>
2025-10-28 11:17:20 -0700

    Use an ObjectWriter-based PE writer for crossgen2 (#120454)

--- src/coreclr/tools/aot/ILCompiler.ReadyToRun/Compiler/DependencyAnalysis/ReadyToRun/Win32ResourcesNode.cs
+++ src/coreclr/tools/aot/ILCompiler.ReadyToRun/Compiler/DependencyAnalysis/ReadyToRun/Win32ResourcesNode.cs
@@ -19,7 +19,10 @@ namespace ILCompiler.DependencyAnalysis.ReadyToRun
             _size = -1;
         }
 
-        public override ObjectNodeSection GetSection(NodeFactory factory) => ObjectNodeSection.TextSection;
+        public override ObjectNodeSection GetSection(NodeFactory factory)
+        {
+            return ObjectNodeSection.Win32ResourcesSection;
+        }
 
         public override bool IsShareable => false;
 
@@ -44,6 +47,7 @@ namespace ILCompiler.DependencyAnalysis.ReadyToRun
         private ObjectData GetDataInternal()
         {
             ObjectDataBuilder builder = new ObjectDataBuilder();
+            builder.RequireInitialAlignment(1);
             builder.AddSymbol(this);
             _resourceData.WriteResources(this, ref builder);
             _size = builder.CountBytes;
