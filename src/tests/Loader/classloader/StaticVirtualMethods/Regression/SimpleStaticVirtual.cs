e7f60d4cfb97db3d67b2c3998b4055ba81fb8001
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2025-10-29 10:27:45 +0100

    Test only

---
+++ src/tests/Loader/classloader/StaticVirtualMethods/Regression/SimpleStaticVirtual.cs
@@ -0,0 +1,30 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Xunit;
+
+namespace SimpleStaticVirtual
+{
+    public class Tests
+    {
+        [Fact]
+        public static void TestEntryPoint()
+        {
+            Assert.Equal(nameof(IGetter), Get<IGetter>());
+            Assert.Equal("Object", GetVariant<IGetter<object>, string>());
+        }
+
+        static string Get<T>() where T : IGetter => T.Get();
+        static string GetVariant<T, U>() where T : IGetter<U> => T.Get();
+    }
+
+    interface IGetter
+    {
+        static virtual string Get() => nameof(IGetter);
+    }
+
+    interface IGetter<in T>
+    {
+        static virtual string Get() => typeof(T).Name;
+    }
+}
