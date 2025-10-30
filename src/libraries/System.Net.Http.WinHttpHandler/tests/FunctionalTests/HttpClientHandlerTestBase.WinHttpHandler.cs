992039fa9d5c1498e40fe15df7bffcae80983ee6
ManickaP <mapichov@microsoft.com>
2025-10-30 13:51:21 +0100

    Fix loopback server for H/3

--- src/libraries/System.Net.Http.WinHttpHandler/tests/FunctionalTests/HttpClientHandlerTestBase.WinHttpHandler.cs
+++ src/libraries/System.Net.Http.WinHttpHandler/tests/FunctionalTests/HttpClientHandlerTestBase.WinHttpHandler.cs
@@ -41,8 +41,10 @@ namespace System.Net.Http.Functional.Tests
         {
             return useVersion.Major switch
             {
+                1 => Http11LoopbackServerFactory.Singleton,
                 2 => Http2LoopbackServerFactory.Singleton,
-                _ => Http11LoopbackServerFactory.Singleton
+                3 => Http3LoopbackServerFactory.Singleton,
+                _ => throw new InvalidOperationException($"Unexpected HTTP version: {useVersion}")
             };
         }
 
