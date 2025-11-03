cae9ae9e353a4ac2c9e32a6bac76353187a62468
Pavel Savara <pavelsavara@microsoft.com>
2025-11-03 18:21:46 +0100

    Update src/libraries/System.Private.CoreLib/src/System/Runtime/CompilerServices/AsyncHelpers.NonBrowser.cs
    Co-authored-by: Aaron R Robinson <arobins@microsoft.com>

--- src/libraries/System.Private.CoreLib/src/System/Runtime/CompilerServices/AsyncHelpers.NonBrowser.cs
+++ src/libraries/System.Private.CoreLib/src/System/Runtime/CompilerServices/AsyncHelpers.NonBrowser.cs
@@ -10,7 +10,7 @@ namespace System.Runtime.CompilerServices
         /// <summary>
         /// This method is intended to be used by Roslyn-generated async entry point.
         /// On multi-threaded environments, it would block and await the specified task and return when the task has completed.
-        /// On single-threaded environments (e.g., browser), it would register a continuation and yield to the browser event loop immediately.
+        /// On single-threaded environments (for example, browser), it would register a continuation and yield to the browser event loop immediately.
         /// </summary>
         /// <param name="task">The result from the main entrypoint to await.</param>
         public static void HandleAsyncEntryPoint(Task task)
