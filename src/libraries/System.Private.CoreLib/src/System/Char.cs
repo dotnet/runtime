563d8f1c925d19e931220d7600f49b06978745cd
xtqqczze <45661989+xtqqczze@users.noreply.github.com>
2025-11-03 18:06:26 +0000

    Micro-optimization in IsAsciiLetterOrDigit
    Saves an instruction on XARCH:
    `lea` vs `mov`, `add`

--- src/libraries/System.Private.CoreLib/src/System/Char.cs
+++ src/libraries/System.Private.CoreLib/src/System/Char.cs
@@ -326,7 +326,7 @@ namespace System
         /// This determines whether the character is in the range 'A' through 'Z', inclusive,
         /// 'a' through 'z', inclusive, or '0' through '9', inclusive.
         /// </remarks>
-        public static bool IsAsciiLetterOrDigit(char c) => IsAsciiLetter(c) | IsBetween(c, '0', '9');
+        public static bool IsAsciiLetterOrDigit(char c) => IsBetween(c, '0', '9') | IsAsciiLetter(c);
 
         /// <summary>Indicates whether a character is categorized as an ASCII hexadecimal digit.</summary>
         /// <param name="c">The character to evaluate.</param>
