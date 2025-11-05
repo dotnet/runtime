aa867b760dcd66de32556b53718c2203113c3d0e
Aaron R Robinson <arobins@microsoft.com>
2025-11-04 16:03:44 -0800

    Build break.

--- src/coreclr/vm/corhost.cpp
+++ src/coreclr/vm/corhost.cpp
@@ -694,10 +694,10 @@ HRESULT CorHost2::CreateDelegate(
     EMPTY_STRING_TO_NULL(wszClassName);
     EMPTY_STRING_TO_NULL(wszMethodName);
 
-    if (fnPtr == 0)
+    if (fnPtr == NULL)
        return E_POINTER;
 
-    *fnPtr = NULL;
+    *fnPtr = 0;
 
     if(wszAssemblyName == NULL)
         return E_INVALIDARG;
