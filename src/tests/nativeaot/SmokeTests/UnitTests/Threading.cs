61e4a374c35d87f7001d1952f463f9a68f0197be
Sven Boemer <sbomer@gmail.com>
2025-11-05 16:42:23 -0800

    Try to fix condition for SingleFileTestRunner, fix test failure

--- src/tests/nativeaot/SmokeTests/UnitTests/Threading.cs
+++ src/tests/nativeaot/SmokeTests/UnitTests/Threading.cs
@@ -51,10 +51,10 @@ internal static class Threading
             // Disabled on Android due to https://github.com/dotnet/runtime/issues/121216
             Console.WriteLine("    ThreadPoolTests.ThreadPoolCanPickUpOneJobWhenThreadIsAvailable");
             ThreadPoolTests.ThreadPoolCanPickUpOneJobWhenThreadIsAvailable();
-        }
 
-        Console.WriteLine("    ThreadPoolTests.ThreadPoolCanPickUpMultipleJobsWhenThreadsAreAvailable");
-        ThreadPoolTests.ThreadPoolCanPickUpMultipleJobsWhenThreadsAreAvailable();
+            Console.WriteLine("    ThreadPoolTests.ThreadPoolCanPickUpMultipleJobsWhenThreadsAreAvailable");
+            ThreadPoolTests.ThreadPoolCanPickUpMultipleJobsWhenThreadsAreAvailable();
+        }
 
         Console.WriteLine("    ThreadPoolTests.ThreadPoolCanProcessManyWorkItemsInParallelWithoutDeadlocking");
         ThreadPoolTests.ThreadPoolCanProcessManyWorkItemsInParallelWithoutDeadlocking();
