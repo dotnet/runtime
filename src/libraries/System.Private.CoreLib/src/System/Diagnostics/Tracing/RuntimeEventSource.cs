cbf98254cb0ace9adcf5aff5d0b6cf5fc2593254
Jan Kotas <jkotas@microsoft.com>
2025-11-09 11:16:16 -0800

    Update src/libraries/System.Private.CoreLib/src/System/Diagnostics/Tracing/RuntimeEventSource.cs

--- src/libraries/System.Private.CoreLib/src/System/Diagnostics/Tracing/RuntimeEventSource.cs
+++ src/libraries/System.Private.CoreLib/src/System/Diagnostics/Tracing/RuntimeEventSource.cs
@@ -13,7 +13,6 @@ namespace System.Diagnostics.Tracing
     [EventSource(Guid = "49592C0F-5A05-516D-AA4B-A64E02026C89", Name = "System.Runtime")]
     internal sealed partial class RuntimeEventSource : EventSource
     {
-
         public static class Keywords
         {
             public const EventKeywords AppContext = (EventKeywords)0x1;
