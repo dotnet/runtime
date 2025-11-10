c31708ecc4fd49e81e2dacd786392b51b2dd6f23
Jan Kotas <jkotas@microsoft.com>
2025-11-09 18:42:06 -0800

    Update src/libraries/System.Diagnostics.Tracing/ref/System.Diagnostics.Tracing.cs

--- src/libraries/System.Diagnostics.Tracing/ref/System.Diagnostics.Tracing.cs
+++ src/libraries/System.Diagnostics.Tracing/ref/System.Diagnostics.Tracing.cs
@@ -155,9 +155,8 @@ namespace System.Diagnostics.Tracing
         public EventSource(string eventSourceName) { }
         public EventSource(string eventSourceName, System.Diagnostics.Tracing.EventSourceSettings config) { }
         public EventSource(string eventSourceName, System.Diagnostics.Tracing.EventSourceSettings config, params string[]? traits) { }
-        public EventSource(string eventSourceName, Guid eventSourceGuid) { }
-        public EventSource(string eventSourceName, Guid eventSourceGuid, System.Diagnostics.Tracing.EventSourceSettings settings, params string[]? traits) { }
-
+        public EventSource(string eventSourceName, System.Guid eventSourceGuid) { }
+        public EventSource(string eventSourceName, System.Guid eventSourceGuid, System.Diagnostics.Tracing.EventSourceSettings settings, params string[]? traits) { }
         public System.Exception? ConstructionException { get { throw null; } }
         public static System.Guid CurrentThreadActivityId { get { throw null; } }
         public System.Guid Guid { get { throw null; } }
