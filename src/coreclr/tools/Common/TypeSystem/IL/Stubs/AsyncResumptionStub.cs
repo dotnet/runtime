83496bfa0f0aec33525588ff2b8aee115915cebc
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2025-11-11 12:50:43 +0100

    Fixes for generics and shared generics (#121494)
    A couple fixes for things I ran into as I work on generic virtual method
    support with runtime async.
    
    Cc @dotnet/ilc-contrib

--- src/coreclr/tools/Common/TypeSystem/IL/Stubs/AsyncResumptionStub.cs
+++ src/coreclr/tools/Common/TypeSystem/IL/Stubs/AsyncResumptionStub.cs
@@ -13,24 +13,25 @@ namespace ILCompiler
 {
     public partial class AsyncResumptionStub : ILStubMethod
     {
-        private readonly MethodDesc _owningMethod;
+        private readonly MethodDesc _targetMethod;
+        private readonly TypeDesc _owningType;
         private MethodSignature _signature;
 
-        public AsyncResumptionStub(MethodDesc owningMethod)
+        public AsyncResumptionStub(MethodDesc targetMethod, TypeDesc owningType)
         {
-            Debug.Assert(owningMethod.IsAsyncVariant()
-                || (owningMethod.IsAsync && !owningMethod.Signature.ReturnsTaskOrValueTask()));
-            _owningMethod = owningMethod;
+            Debug.Assert(targetMethod.IsAsyncCall());
+            _targetMethod = targetMethod;
+            _owningType = owningType;
         }
 
-        public override ReadOnlySpan<byte> Name => _owningMethod.Name;
-        public override string DiagnosticName => _owningMethod.DiagnosticName;
+        public override ReadOnlySpan<byte> Name => _targetMethod.Name;
+        public override string DiagnosticName => _targetMethod.DiagnosticName;
 
-        public override TypeDesc OwningType => _owningMethod.OwningType;
+        public override TypeDesc OwningType => _owningType;
 
         public override MethodSignature Signature => _signature ??= InitializeSignature();
 
-        public override TypeSystemContext Context => _owningMethod.Context;
+        public override TypeSystemContext Context => _targetMethod.Context;
 
         private MethodSignature InitializeSignature()
         {
