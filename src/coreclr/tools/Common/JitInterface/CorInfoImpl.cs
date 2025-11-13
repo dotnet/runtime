228a12c99c7819b45726a9a0e08f2e79c0091cae
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2025-11-13 05:28:04 +0100

    Report `CORINFO_CALLCONV_GENERIC` to JIT (#121561)
    Outerloops are broken.
    
    Apparently we got away without reporting this until the test in #121130
    was added. Not reporting the calling convention results in this not
    kicking in:
    
    
    https://github.com/dotnet/runtime/blob/5e97723bdd8eb9eff95c52b7120cec6bfb5a0a19/src/coreclr/jit/importer.cpp#L7291-L7297
    
    and failing the test.
    
    Cc @dotnet/ilc-contrib
    
    ---------
    
    Co-authored-by: Jan Kotas <jkotas@microsoft.com>

--- src/coreclr/tools/Common/JitInterface/CorInfoImpl.cs
+++ src/coreclr/tools/Common/JitInterface/CorInfoImpl.cs
@@ -882,6 +882,8 @@ namespace Internal.JitInterface
             if (!signature.IsStatic) sig->callConv |= CorInfoCallConv.CORINFO_CALLCONV_HASTHIS;
             if (signature.IsExplicitThis) sig->callConv |= CorInfoCallConv.CORINFO_CALLCONV_EXPLICITTHIS;
 
+            if (signature.GenericParameterCount != 0) sig->callConv |= CorInfoCallConv.CORINFO_CALLCONV_GENERIC;
+
             TypeDesc returnType = signature.ReturnType;
 
             CorInfoType corInfoRetType = asCorInfoType(signature.ReturnType, &sig->retTypeClass);
