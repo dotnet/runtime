7a6b6efdf8ed50ed8b533b973aa1c41a2cfcb9c4
Andy Ayers <andya@microsoft.com>
2025-11-20 07:16:49 -0800

    JIT: disable stack allocation for sites in CEA cloned regions (#121771)
    Our escape analysis assumes each stack allocation candidate node has a
    unique temp that has that allocation as its only assigned value.
    
    Conditional escape analysis may clone these nodes, creating a second
    allocation site assigning to the associated temp. This breaks the 1-1
    assumption noted above.
    
    If those allocations do not escape then the JIT will stack allocate
    them, creating two distinct stack locals. In subsequent IR rewriting the
    JIT will then use the address of just one of the locals for all
    appearances of the temp, which is incorrect, and leads to the generated
    code possibly reading uninitialized stack slots.
    
    To fix this we disallow stack allocation for sites in blocks that were
    cloned, or their clones. (We can actually handle this case with better
    bookkeeping, but that is more involved).
    
    Fixes #121736.

---
+++ src/tests/JIT/opt/ObjectStackAllocation/Runtime_121736.cs
@@ -0,0 +1,50 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+public class Runtime_121736
+{
+    [Fact]
+    public static void Test()
+    {
+        HashSet<Fake> pureSelectedObjects = [new Fake(TestEnum.One)];
+        try
+        {
+            for (var i = 0; i < 10_000_000; i++)
+            {
+                TestEnum txt = pureSelectedObjects.Select(x => (TestEnum)x.GetSourceItem()).First();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+
+        Console.WriteLine("Passed");
+    }
+}
+
+public enum TestEnum
+{
+    One
+}
+
+public class Fake
+{
+    private readonly TestEnum src;
+
+    public Fake(TestEnum t)
+    {
+        src = t;
+    }
+
+    public object GetSourceItem()
+    {
+        return src;
+    }
+}
