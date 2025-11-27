5c15b23693c3b2f12573710b57c5d15b8c5702ef
Steven He <hez2010@outlook.com>
2025-11-28 01:58:36 +0900

    Disable GVM devirt for AOT

--- src/coreclr/jit/gentree.cpp
+++ src/coreclr/jit/gentree.cpp
@@ -2374,7 +2374,8 @@ int GenTreeCall::GetNonStandardAddedArgCount(Compiler* compiler) const
 //
 bool GenTreeCall::IsDevirtualizationCandidate(Compiler* compiler) const
 {
-    return IsVirtual() || IsGenericVirtual(compiler);
+    // TODO: Support devirtualization for AOT generic virtual calls.
+    return IsVirtual() || (!compiler->IsAot() && IsGenericVirtual(compiler));
 }
 
 //-------------------------------------------------------------------------
