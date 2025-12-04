c67b9f77e15b46f8aa3cee4d92214db79a250dc5
Sven Boemer <sbomer@gmail.com>
2025-12-04 15:28:45 -0800

    Exclude test changes
    This branch will not be updated to support using C# 14 in tests.

--- src/tools/illink/test/ILLink.RoslynAnalyzer.Tests/DataFlowTests.cs
+++ src/tools/illink/test/ILLink.RoslynAnalyzer.Tests/DataFlowTests.cs
@@ -131,13 +131,6 @@ namespace ILLink.RoslynAnalyzer.Tests
 			return RunTest ();
 		}
 
-		// Disabled until we can use C# 14 in tests.
-		// [Fact]
-		// public Task ExtensionMembersDataFlow ()
-		// {
-		// 	return RunTest ();
-		// }
-
 		[Fact]
 		public Task ExtensionsDataFlow ()
 		{
