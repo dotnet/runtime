c93c88affe57af8429878c042a4881425aea2363
Tom McDonald <tommcdon@hotmail.com>
2025-12-06 21:06:18 -0500

    Fix perfmap output

--- src/coreclr/vm/perfmap.cpp
+++ src/coreclr/vm/perfmap.cpp
@@ -261,14 +261,16 @@ void PerfMap::WriteLine(SString& line)
     EX_TRY
     {
         // Write the line.
-
-        if (fprintf(m_fp, "%s", line.GetUTF8()) != 0)
+        int fd = fileno(m_fp);
+        const char* buf = line.GetUTF8();
+        size_t len = line.GetCount();
+        ssize_t written = write(fd, buf, len);
+        if (written < 0 || (size_t)written != len)
         {
             // This will cause us to stop writing to the file.
             // The file will still remain open until shutdown so that we don't have to take a lock at this level when we touch the file stream.
             m_ErrorEncountered = true;
         }
-
     }
     EX_CATCH{} EX_END_CATCH
 }
