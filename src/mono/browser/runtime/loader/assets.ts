4bf31a6ba0288cdcad51d9ca312d71eb5b950ee0
Marek Fišera <mara@neptuo.com>
2025-12-10 13:24:45 +0100

    Introduce a new flag on asset definition to control caching behavior. It directly maps to fetchOptions.cache

--- src/mono/browser/runtime/loader/assets.ts
+++ src/mono/browser/runtime/loader/assets.ts
@@ -400,7 +400,7 @@ export function prepareAssets () {
                     name: configUrl,
                     behavior: "vfs",
                     // TODO what should be the virtualPath ?
-                    noCache: true,
+                    cache: "no-cache",
                     useCredentials: true
                 });
             }
@@ -674,10 +674,10 @@ function fetchResource(asset: AssetEntryInternal): Promise<Response> {
 
     const fetchOptions: RequestInit = {};
 
-    if (isAssetFingerprinted(asset)) {
-        // Fingerprinted assets are by definition never stale (newer version would have a different name).
-        fetchOptions.cache = "force-cache";
-    } else if (asset.noCache === true || !loaderHelpers.config.disableNoCacheFetch) {
+    if (asset.cache) {
+        // Internal no-cache setting overrides other configuration.
+        fetchOptions.cache = asset.cache;
+    } else if (!loaderHelpers.config.disableNoCacheFetch) {
         // We use no-cache if specified internally.
         // For backwards compatibility other assets also get no-cache setting unless disabled by the user.
         // https://github.com/dotnet/runtime/issues/74815
@@ -696,24 +696,6 @@ function fetchResource(asset: AssetEntryInternal): Promise<Response> {
     }
 
     return loaderHelpers.fetch_like(url, fetchOptions);
-}
-
-function isAssetFingerprinted (asset: AssetEntryInternal): boolean {
-    if (asset.virtualPath != undefined) {
-        return asset.virtualPath !== asset.name;
-    } else {
-        // Special assets such as dotnet.native.wasm can be missing virtualPath
-        // even when they are fingerprinted.
-        // In such cases we look at some core assembly that we know will have its
-        // virtualPath set and different from its name when fingerprinting is enabled.
-        const coreAssembly = loaderHelpers.config.resources?.coreAssembly?.[0];
-
-        if (coreAssembly && coreAssembly.virtualPath != undefined) {
-            return coreAssembly.virtualPath !== coreAssembly.name;
-        }
-    }
-
-    return false;
 }
 
 const monoToBlazorAssetTypeMap: { [key: string]: WebAssemblyBootResourceType | undefined } = {
