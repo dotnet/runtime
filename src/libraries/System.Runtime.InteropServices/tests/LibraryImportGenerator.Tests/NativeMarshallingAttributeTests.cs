899daeb389ca6a05c73dfc5eab83da9aa6304333
Jackson Schuster <36744439+jtschuster@users.noreply.github.com>
2025-12-10 15:53:46 -0800

    Make test conditional on running on CoreCLR

--- src/libraries/System.Runtime.InteropServices/tests/LibraryImportGenerator.Tests/NativeMarshallingAttributeTests.cs
+++ src/libraries/System.Runtime.InteropServices/tests/LibraryImportGenerator.Tests/NativeMarshallingAttributeTests.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.Marshalling;
 using SharedTypes.ComInterfaces;
@@ -16,7 +17,7 @@ namespace LibraryImportGenerator.IntegrationTests
 
     public class NativeMarshallingAttributeTests
     {
-        [Fact]
+        [ConditionalFact(typeof(PlatformDetection), nameof(PlatformDetection.IsCoreCLR))]
         public void GetSameComInterfaceTwiceReturnsUniqueInstances()
         {
             // When using NativeMarshalling with UniqueComInterfaceMarshaller,
