7c2c90f5ed67c27c20452c2312faa6760d9cf955
David Wrighton <davidwr@microsoft.com>
2025-12-17 15:38:35 -0800

    Adjust spacing to match existing logic

--- src/coreclr/vm/codeversion.cpp
+++ src/coreclr/vm/codeversion.cpp
@@ -1980,11 +1980,11 @@ HRESULT CodeVersionManager::PublishNativeCodeVersion(MethodDesc* pMethod, Native
                 pMethod,
                 nativeCodeVersion.GetVersionId()));
 
-#ifdef FEATURE_INTERPRETER
+        #ifdef FEATURE_INTERPRETER
             // When we hit the Precode that should fixup any issues with an unset interpreter code pointer. This is notably most important in ReJIT scenarios
             pMethod->ClearInterpreterCodePointer();
-#endif        
-#ifdef FEATURE_TIERED_COMPILATION
+        #endif        
+        #ifdef FEATURE_TIERED_COMPILATION
             bool wasSet = CallCountingManager::SetCodeEntryPoint(nativeCodeVersion, pCode, false, nullptr);
             _ASSERTE(wasSet);
         #else
