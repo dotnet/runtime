de127defa705c4b2b8d172b8e3a91b6a17a88358
Eduardo Velarde <evelardepola@microsoft.com>
2025-12-18 19:29:20 -0800

    Update IPrefixMangledType.cs

--- src/coreclr/tools/aot/ILCompiler.Compiler/Compiler/CompilerTypeSystemContext.Mangling.cs
+++ src/coreclr/tools/aot/ILCompiler.Compiler/Compiler/CompilerTypeSystemContext.Mangling.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
 using Internal.TypeSystem;
 
 namespace ILCompiler
@@ -18,11 +19,11 @@ namespace ILCompiler
                 }
             }
 
-            string IPrefixMangledType.Prefix
+            ReadOnlySpan<byte> IPrefixMangledType.Prefix
             {
                 get
                 {
-                    return "Boxed";
+                    return "Boxed"u8;
                 }
             }
         }
