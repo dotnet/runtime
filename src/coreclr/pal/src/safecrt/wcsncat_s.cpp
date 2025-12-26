a274767835a4570569fdc90c8cf21f943874c6a1
Adeel Mujahid <3840695+am11@users.noreply.github.com>
2025-12-26 19:40:37 +0200

    Cleanup some unused code in coreclr pal (#122733)

--- src/coreclr/pal/src/safecrt/wcsncat_s.cpp
+++ src/coreclr/pal/src/safecrt/wcsncat_s.cpp
@@ -4,12 +4,9 @@
 /***
 *wcsncat_s.c - append n chars of string to new string
 *
-
-*
 *Purpose:
 *   defines wcsncat_s() - appends n characters of string onto
 *   end of other string
-*
 *******************************************************************************/
 
 #define _SECURECRT_FILL_BUFFER 1
@@ -22,7 +19,6 @@
 
 #include "mbusafecrt_internal.h"
 
-#define _FUNC_PROLOGUE
 #define _FUNC_NAME wcsncat_s
 #define _CHAR char16_t
 #define _DEST _Dst
@@ -31,4 +27,3 @@
 #define _COUNT _Count
 
 #include "tcsncat_s.inl"
-
