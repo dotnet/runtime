10b01cfca732b6462d2802d0ba6a7a9f99b42e29
pavelsavara <pavel.savara@gmail.com>
2025-12-31 14:58:38 +0100

    gitHash

--- src/native/corehost/browserhost/host/index.ts
+++ src/native/corehost/browserhost/host/index.ts
@@ -5,6 +5,8 @@ import type { InternalExchange, BrowserHostExports, RuntimeAPI, BrowserHostExpor
 import { InternalExchangeIndex } from "./types";
 import { } from "./cross-linked"; // ensure ambient symbols are declared
 
+import GitHash from "consts:gitHash";
+
 import { runMain, runMainAndExit, registerDllBytes, installVfsFile, loadIcuData, initializeCoreCLR } from "./host";
 
 export function dotnetInitializeModule(internals: InternalExchange): void {
@@ -16,6 +18,9 @@ export function dotnetInitializeModule(internals: InternalExchange): void {
     const runtimeApi = internals[InternalExchangeIndex.RuntimeAPI];
     if (typeof runtimeApi !== "object") throw new Error("Expected internals to have RuntimeAPI");
     Object.assign(runtimeApi, runtimeApiLocal);
+    if (runtimeApi.runtimeBuildInfo.gitHash && runtimeApi.runtimeBuildInfo.gitHash !== GitHash) {
+        throw new Error(`Mismatched git hashes between loader and runtime. Loader: ${runtimeApi.runtimeBuildInfo.gitHash}, BrowserHost: ${GitHash}`);
+    }
 
     internals[InternalExchangeIndex.BrowserHostExportsTable] = browserHostExportsToTable({
         registerDllBytes,
