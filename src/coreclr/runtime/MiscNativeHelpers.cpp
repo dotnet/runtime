60923ca81e85d09f7433abc5e25f1cb9e69ab7de
Jan Kotas <jkotas@microsoft.com>
2025-12-30 20:19:14 -0800

    Update src/coreclr/runtime/MiscNativeHelpers.cpp

--- src/coreclr/runtime/MiscNativeHelpers.cpp
+++ src/coreclr/runtime/MiscNativeHelpers.cpp
@@ -15,20 +15,8 @@
 #include <minipal/cpuid.h>
 
 #if defined(TARGET_X86) || defined(TARGET_AMD64)
-
 extern "C" void QCALLTYPE X86Base_CpuId(int cpuInfo[4], int functionId, int subFunctionId)
 {
-#ifndef NATIVEAOT
-    QCALL_CONTRACT;
-
-    BEGIN_QCALL;
-#endif
-
     __cpuidex(cpuInfo, functionId, subFunctionId);
-
-#ifndef NATIVEAOT
-    END_QCALL;
-#endif
 }
-
 #endif // defined(TARGET_X86) || defined(TARGET_AMD64)
