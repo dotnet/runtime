5730f52f989138eb87c0ba3d6cc0c43a47ea9fad
Pavel Savara <pavelsavara@microsoft.com>
2025-12-31 16:30:47 +0100

    fix

--- src/native/corehost/browserhost/host/index.ts
+++ src/native/corehost/browserhost/host/index.ts
@@ -22,7 +22,7 @@ export function dotnetInitializeModule(internals: InternalExchange): void {
     }
     Object.assign(runtimeApi, runtimeApiLocal);
 
-    internals[InternalExchangeIndex.BrowserHostExportsTable] = browserHostExportsTable = browserHostExportsToTable({
+    internals[InternalExchangeIndex.BrowserHostExportsTable] = browserHostExportsToTable({
         registerDllBytes,
         installVfsFile,
         loadIcuData,
