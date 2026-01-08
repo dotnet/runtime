737ac0f9967453945bc765dbcd1e577c901f70fd
rcj1 <rachel.jarvi@gmail.com>
2026-01-08 09:39:30 -0800

    revert change to asyncresumptionstub

--- src/coreclr/tools/Common/TypeSystem/IL/Stubs/AsyncResumptionStub.cs
+++ src/coreclr/tools/Common/TypeSystem/IL/Stubs/AsyncResumptionStub.cs
@@ -34,8 +34,6 @@ namespace ILCompiler
 
         public override TypeSystemContext Context => _targetMethod.Context;
 
-        public MethodDesc TargetMethod => _targetMethod;
-
         private MethodSignature InitializeSignature()
         {
             TypeDesc objectType = Context.GetWellKnownType(WellKnownType.Object);
