533e0fcc5060c71d7971c47e8b000d37e9269218
Radek Doulik <radek.doulik@gmail.com>
2026-01-12 17:36:19 +0100

    [wasm][coreclr] callhelpers generator, pinvoke part
    Generate src/coreclr/vm/wasm/callhelpers-pinvoke.cpp and use it
    instead of hardcoded native libs entrypoints.c

--- src/coreclr/hosts/corerun/wasm/pinvoke_override.cpp
+++ src/coreclr/hosts/corerun/wasm/pinvoke_override.cpp
@@ -7,23 +7,9 @@
 #define _In_
 #include "pinvokeoverride.h"
 
-extern "C" const void* SystemResolveDllImport(const char* name);
-
-// pinvoke_override:
-// Check if given function belongs to one of statically linked libraries and return a pointer if found.
-static const void* pinvoke_override(const char* library_name, const char* entry_point_name)
-{
-    // This function is only called with the library name specified for a p/invoke, not any variations.
-    // It must handle exact matches to the names specified. See Interop.Libraries.cs for each platform.
-    if (strcmp(library_name, "libSystem.Native") == 0)
-    {
-        return SystemResolveDllImport(entry_point_name);
-    }
-
-    return nullptr;
-}
+const void* callhelpers_pinvoke_override(const char* library_name, const char* entry_point_name);
 
 void add_pinvoke_override()
 {
-    PInvokeOverride::SetPInvokeOverride(pinvoke_override, PInvokeOverride::Source::RuntimeConfiguration);
+    PInvokeOverride::SetPInvokeOverride(callhelpers_pinvoke_override, PInvokeOverride::Source::RuntimeConfiguration);
 }
