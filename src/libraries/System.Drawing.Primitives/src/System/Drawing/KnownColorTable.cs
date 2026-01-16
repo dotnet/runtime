c9e6ee49b88b4e70129ba9df6c8eff98a8598eb1
Leaf Shi <132890443+LeafShi1@users.noreply.github.com>
2026-01-16 11:08:22 +0800

    Update color values in KnownColorTable.cs

--- src/libraries/System.Drawing.Primitives/src/System/Drawing/KnownColorTable.cs
+++ src/libraries/System.Drawing.Primitives/src/System/Drawing/KnownColorTable.cs
@@ -458,13 +458,13 @@ namespace System.Drawing
             0xFF50503C, // FFFFFFE1 - FF50503C: Info - Link Label
             0xFFBEBEBE, // FF000000 - FFBEBEBE: InfoText - Prompt Text Color
             0xFF373737, // FFF0F0F0 - FF373737: Menu - Normal Menu Background
-            0xFFF0F0F0, // FF000000 - FFF0F0F0: MenuText - Light gray
+            0xFFF0F0F0, // FF000000 - FFF0F0F0: MenuText - White
             0xFF505050, // FFC8C8C8 - FF505050: ScrollBar - Scrollbars and Scrollbar Arrows
             0xFF323232, // FFFFFFFF - FF323232: Window - Window Background
-            0xFF404040, // FF646464 - FF404040: WindowFrame - Light border for window edges
-            0xFFF0F0F0, // FF000000 - FFF0F0F0: WindowText - Light gray
+            0xFF282828, // FF646464 - FF282828: WindowFrame - White
+            0xFFF0F0F0, // FF000000 - FFF0F0F0: WindowText - White
             0xFF202020, // FFF0F0F0 - FF373737: ButtonFace - Same as Window Background
-            0xFF2A2A2A, // FFFFFFFF - FF2A2A2A: ButtonHighlight - Light gray highlight
+            0xFF101010, // FFFFFFFF - FF101010: ButtonHighlight - White
             0xFF464646, // FFA0A0A0 - FF464646: ButtonShadow - Same as Scrollbar Elements
             0XFF416482, // FFB9D1EA - FF416482: GradientActiveCaption - Same as Highlighted Text Background
             0xFF557396, // FFD7E4F2 - FF557396: GradientInactiveCaption - Same as Highlighted Panel in DarkMode
