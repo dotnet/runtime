a7495b656a49b8acf5f1e69f11e6f3e1e2b4d68e
Sven Boemer <sbomer@gmail.com>
2026-01-16 15:06:33 -0800

    Fix: Visit field instance in analyzer for field assignments
    Extends the fix for visiting field instances to also cover field
    assignments. Previously only field reads were handled; now field
    writes (simple assignments) also visit the instance expression to
    ensure method calls used as the instance are analyzed.
    
    This resolves the remaining cases from dotnet/linker#2832.

--- src/tools/illink/test/Mono.Linker.Tests.Cases/DataFlow/FieldDataFlow.cs
+++ src/tools/illink/test/Mono.Linker.Tests.Cases/DataFlow/FieldDataFlow.cs
@@ -232,21 +232,21 @@ namespace Mono.Linker.Tests.Cases.DataFlow
 
             static AccessReturnedInstanceField GetInstance([DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)] Type unused) => null;
 
-            [ExpectedWarning("IL2072", nameof(GetUnknownType), nameof(GetInstance), Tool.Trimmer | Tool.NativeAot, "https://github.com/dotnet/linker/issues/2832")]
+            [ExpectedWarning("IL2072", nameof(GetUnknownType), nameof(GetInstance))]
             [ExpectedWarning("IL2077", nameof(field), nameof(DataFlowTypeExtensions.RequiresAll))]
             static void TestRead()
             {
                 GetInstance(GetUnknownType()).field.RequiresAll();
             }
 
-            [ExpectedWarning("IL2072", nameof(GetUnknownType), nameof(GetInstance), Tool.Trimmer | Tool.NativeAot, "https://github.com/dotnet/linker/issues/2832")]
+            [ExpectedWarning("IL2072", nameof(GetUnknownType), nameof(GetInstance))]
             [ExpectedWarning("IL2074", nameof(GetUnknownType), nameof(field))]
             static void TestWrite()
             {
                 GetInstance(GetUnknownType()).field = GetUnknownType();
             }
 
-            [ExpectedWarning("IL2072", nameof(GetUnknownType), nameof(GetInstance), Tool.Trimmer | Tool.NativeAot, "https://github.com/dotnet/linker/issues/2832")]
+            [ExpectedWarning("IL2072", nameof(GetUnknownType), nameof(GetInstance))]
             [ExpectedWarning("IL2074", nameof(GetUnknownType), nameof(field))]
             static void TestNullCoalescingAssignment()
             {
