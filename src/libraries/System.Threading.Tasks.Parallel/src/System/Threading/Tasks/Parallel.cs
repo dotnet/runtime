0cf5b0d3f580923cfe865548bccd4143fea685d0
pavelsavara <pavel.savara@gmail.com>
2026-01-18 18:21:40 +0100

    more

--- src/libraries/System.Threading.Tasks.Parallel/src/System/Threading/Tasks/Parallel.cs
+++ src/libraries/System.Threading.Tasks.Parallel/src/System/Threading/Tasks/Parallel.cs
@@ -329,7 +329,7 @@ namespace System.Threading.Tasks
                 else  // This is more efficient for a small number of actions and no DOP support:
                 {
 #if FEATURE_SINGLE_THREADED
-                if (OperatingSystem.IsBrowser()) throw new PlatformNotSupportedException();
+                    if (OperatingSystem.IsBrowser()) throw new PlatformNotSupportedException();
 #endif
                     // Initialize our array of tasks, one per action.
                     Task[] tasks = new Task[actionsCopy.Length];
