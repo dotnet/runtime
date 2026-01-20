59a11cfaf88ff81407d3f69d4696cff4ae37ec97
Marek Fišera <mara@neptuo.com>
2026-01-20 12:29:31 +0100

    Return from the loop once satellite assembly is processed

--- src/tasks/Microsoft.NET.Sdk.WebAssembly.Pack.Tasks/ComputeWasmBuildAssets.cs
+++ src/tasks/Microsoft.NET.Sdk.WebAssembly.Pack.Tasks/ComputeWasmBuildAssets.cs
@@ -150,6 +150,17 @@ public class ComputeWasmBuildAssets : Task
                     candidate.SetMetadata("RelatedAsset", relatedAssetPath);
 
                     Log.LogMessage(MessageImportance.Low, "Found satellite assembly '{0}' asset for inferred candidate '{1}' with culture '{2}'", candidate.ItemSpec, relatedAssetPath, culture);
+                    
+                    assetCandidates.Add(candidate);
+                    continue;
+                }
+                
+                // Check for unique relative path before adding candidate.
+                var candidateFileName = Path.GetFileName(candidate.ItemSpec);
+                if (!uniqueFileNames.Add(candidateFileName))
+                {
+                    Log.LogMessage(MessageImportance.Low, "Skipping duplicate file name '{0}' for candidate '{1}'", candidateFileName, candidate.ItemSpec);
+                    continue;
                 }
 
                 assetCandidates.Add(candidate);
