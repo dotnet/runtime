dotnet/runtime

commit 4c212c5efea71c7f95889cb996e64488da8c3377

Clean up Guid.DecodeByte (#88267)

src/libraries/System.Private.CoreLib/src/System/Guid.cs
@@ -727,27 +727,17 @@ namespace System
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static byte DecodeByte(nuint ch1, nuint ch2, ref int invalidIfNegative)
+        private static byte DecodeByte(char ch1, char ch2, ref int invalidIfNegative)
         {
-            // TODO https://github.com/dotnet/runtime/issues/13464:
-            // Replace the Unsafe.Add with HexConverter.FromChar once the bounds checks are eliminated.
-
             ReadOnlySpan<byte> lookup = HexConverter.CharToHexLookup;
+            Debug.Assert(lookup.Length == 256);
 
-            int h1 = -1;
-            if (ch1 < (nuint)lookup.Length)
-            {
-                h1 = (sbyte)Unsafe.Add(ref MemoryMarshal.GetReference(lookup), (nint)ch1);
-            }
-            h1 <<= 4;
+            int upper = (sbyte)lookup[(byte)ch1];
+            int lower = (sbyte)lookup[(byte)ch2];
+            int result = (upper << 4) | lower;
 
-            int h2 = -1;
-            if (ch2 < (nuint)lookup.Length)
-            {
-                h2 = (sbyte)Unsafe.Add(ref MemoryMarshal.GetReference(lookup), (nint)ch2);
-            }
-
-            int result = h1 | h2;
+            // Result will be negative if ch1 or/and ch2 are greater than 0xFF
+            result = (ch1 | ch2) >> 8 == 0 ? result : -1;
             invalidIfNegative |= result;
             return (byte)result;
         }

commit 278f0b4190181ae626b5745e6b1b7ba857e54b05

Optimise `Guid.DecodeByte` (#116574)
Address regressions from https://github.com/dotnet/runtime/pull/88267:
* https://github.com/dotnet/perf-autofiling-issues/issues/19429
* https://github.com/dotnet/perf-autofiling-issues/issues/19506
* https://github.com/dotnet/perf-autofiling-issues/issues/19466
* https://github.com/dotnet/perf-autofiling-issues/issues/19565

Benchmarks:
| Method | Toolchain | Mean | Error | Ratio |
|------------------ |------------------------
|------------:|----------:|------:|
| ParseExactD       | Main |  18.1742 ns | 0.3769 ns |  1.00 |
| ParseExactD       | PR   |  15.5857 ns | 0.0523 ns |  0.86 |
| | | | | |
| ctor_str          | Main |  18.5513 ns | 0.0808 ns |  1.00 |
| ctor_str          | PR   |  15.4892 ns | 0.0945 ns |  0.83 |
| | | | | |
| Parse             | Main |  18.6132 ns | 0.0761 ns |  1.00 |
| Parse             | PR   |  15.4071 ns | 0.0745 ns |  0.83 |


Diffs:
* https://github.com/MihuBot/runtime-utils/issues/1128
* https://github.com/MihuBot/runtime-utils/issues/1129

---------

Co-authored-by: Tanner Gooding <tagoo@microsoft.com>

src/libraries/System.Private.CoreLib/src/System/Guid.cs
@@ -830,14 +830,13 @@ namespace System
         {
             ReadOnlySpan<byte> lookup = HexConverter.CharToHexLookup;
             Debug.Assert(lookup.Length == 256);
-            int upper = (sbyte)lookup[byte.CreateTruncating(ch1)];
-            int lower = (sbyte)lookup[byte.CreateTruncating(ch2)];
-            int result = (upper << 4) | lower;
 
-            uint c1 = TChar.CastToUInt32(ch1);
-            uint c2 = TChar.CastToUInt32(ch2);
-            // Result will be negative if ch1 or/and ch2 are greater than 0xFF
-            result = (c1 | c2) >> 8 == 0 ? result : -1;
+            uint c1 = typeof(TChar) == typeof(byte) ? TChar.CastToUInt32(ch1) : Math.Min(TChar.CastToUInt32(ch1), 0x7F);
+            uint c2 = typeof(TChar) == typeof(byte) ? TChar.CastToUInt32(ch2) : Math.Min(TChar.CastToUInt32(ch2), 0x7F);
+            int upper = (sbyte)lookup[(int)c1];
+            int lower = (sbyte)lookup[(int)c2];
+
+            int result = (upper << 4) | lower;
             invalidIfNegative |= result;
             return (byte)result;
         }