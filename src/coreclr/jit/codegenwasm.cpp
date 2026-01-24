862cbdf18720b266203fc9543dc52831ee815a9a
Andy Ayers <andya@microsoft.com>
2026-01-23 16:25:27 -0800

    [RyuJit Wasm] don't home locals if none are referenced
    In such cases we won't have set up a stack pointer.

--- src/coreclr/jit/codegenwasm.cpp
+++ src/coreclr/jit/codegenwasm.cpp
@@ -115,6 +115,16 @@ void CodeGen::genHomeRegisterParams(regNumber initReg, bool* initRegStillZeroed)
 {
     JITDUMP("*************** In genHomeRegisterParams()\n");
 
+    if (GetStackPointerReg() == REG_NA)
+    {
+        // We didn't see any local or argument references, so there's nothing to spill.
+        // TODO-WASM: debug codegen would likely spill all the user args anyways.
+        //
+        JITDUMP("  No local references -- skipping parameter homing\n");
+        assert(!isFramePointerUsed());
+        return;
+    }
+
     auto spillParam = [this](unsigned lclNum, unsigned offset, unsigned paramLclNum, const ABIPassingSegment& segment) {
         assert(segment.IsPassedInRegister());
 
