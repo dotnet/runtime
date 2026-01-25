19c9248add579fff799668fe0d0eafd5dc2c2a74
Rachel <rachel.jarvi@gmail.com>
2026-01-24 19:15:03 -0800

    e

--- src/coreclr/tools/aot/ILCompiler.Compiler/Compiler/StackTraceEmissionPolicy.cs
+++ src/coreclr/tools/aot/ILCompiler.Compiler/Compiler/StackTraceEmissionPolicy.cs
@@ -41,7 +41,7 @@ namespace ILCompiler
 
             if (method.HasCustomAttribute("System.Diagnostics", "StackTraceHiddenAttribute")
                 || (method.OwningType is MetadataType mdType && mdType.HasCustomAttribute("System.Diagnostics", "StackTraceHiddenAttribute"))
-                || (method is ILCompiler.ILStubMethod)
+                || (method is Internal.IL.Stubs.ILStubMethod)
                 || method.IsAsyncThunk()) // see MethodDesc::IsDiagnosticsHidden() in src/coreclr/vm/method.inl
             {
                 result |= MethodStackTraceVisibilityFlags.IsHidden;
