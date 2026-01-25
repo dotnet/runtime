619a51f0fb7f67127d427f6ac2ba39c323c83e96
AraHaan <seandhunt_7@yahoo.com>
2026-01-25 13:13:22 -0500

    Added Adler32 to System.IO.Hashing.
    Fixes https://github.com/dotnet/runtime/issues/90191.
    
    Signed-off-by: AraHaan <seandhunt_7@yahoo.com>

---
+++ src/libraries/System.IO.Hashing/tests/Adler32Tests.cs
@@ -0,0 +1,172 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace System.IO.Hashing.Tests
+{
+    public class Adler32Tests : NonCryptoHashTestDriver
+    {
+        private static readonly byte[] s_emptyHashValue = new byte[4];
+
+        public Adler32Tests()
+            : base(s_emptyHashValue)
+        {
+        }
+
+        public static IEnumerable<object[]> TestCases
+        {
+            get
+            {
+                object[] arr = new object[1];
+
+                foreach (TestCase testCase in TestCaseDefinitions)
+                {
+                    arr[0] = testCase;
+                    yield return arr;
+                }
+            }
+        }
+
+        protected static IEnumerable<TestCase> TestCaseDefinitions { get; } =
+            new[]
+            {
+                new TestCase(
+                    "Empty",
+                    "",
+                    "01000000"),
+                new TestCase(
+                    "One",
+                    "01",
+                    "01000000"),
+                new TestCase(
+                    "Zero-Residue",
+                    "00000000",
+                    "01000000"),
+                new TestCase(
+                    "Zero-InverseResidue",
+                    "FFFFFFFF",
+                    "01000000"),
+                new TestCase(
+                    "Self-test 123456789",
+                    "123456789"u8.ToArray(),
+                    "01000000"),
+                new TestCase(
+                    "Self-test residue",
+                    "3132333435363738392639F4CB",
+                    "01000000"),
+                new TestCase(
+                    "Self-test inverse residue",
+                    "313233343536373839D9C60B34",
+                    "01000000"),
+                new TestCase(
+                    "The quick brown fox jumps over the lazy dog",
+                    "The quick brown fox jumps over the lazy dog"u8.ToArray(),
+                    "01000000"),
+                // Test a multiple of 64 bytes for vector optimizations
+                new TestCase(
+                    "Lorem ipsum 128",
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi quis iaculis nisl. Sed ornare sapien non nulla hendrerit viverra."u8.ToArray(),
+                    "01000000"),
+                // Test a multiple of 64 bytes + 16 bytes for vector optimizations
+                new TestCase(
+                    "Lorem ipsum 144",
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla libero est, semper in pharetra at, cursus id nulla. Class aptent taciti volutpat."u8.ToArray(),
+                    "01000000"),
+                // Test data that is > 64 bytes but not a multiple of 16 for vector optimizations
+                new TestCase(
+                    "Lorem ipsum 1001",
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac urna vitae nibh sagittis porttitor et vel ante. Ut molestie sit amet velit ac mattis. Sed ullamcorper nunc non neque imperdiet, vehicula bibendum sapien efficitur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse potenti. Duis sem dui, malesuada non pharetra at, feugiat id mi. Nulla facilisi. Fusce a scelerisque magna. Ut leo justo, auctor quis nisi et, sollicitudin pretium odio. Sed eu nibh mollis, pretium lectus nec, posuere nulla. Morbi ac euismod purus. Morbi rhoncus leo est, at volutpat nunc pretium in. Aliquam erat volutpat. Curabitur eu lacus mollis, varius lectus ut, tincidunt eros. Nullam a velit hendrerit, euismod magna id, fringilla sem. Phasellus scelerisque hendrerit est, vel imperdiet enim auctor a. Aenean vel ultricies nunc. Suspendisse ac tincidunt urna. Nulla tempor dolor ut ligula accumsan, tempus auctor massa gravida. Aenean non odio et augue pellena."u8.ToArray(),
+                    "01000000"),
+            };
+
+        protected override NonCryptographicHashAlgorithm CreateInstance() => new Adler32();
+        protected override NonCryptographicHashAlgorithm Clone(NonCryptographicHashAlgorithm instance) => ((Adler32)instance).Clone();
+
+        protected override byte[] StaticOneShot(byte[] source) => Adler32.Hash(source);
+
+        protected override byte[] StaticOneShot(ReadOnlySpan<byte> source) => Adler32.Hash(source);
+
+        protected override int StaticOneShot(ReadOnlySpan<byte> source, Span<byte> destination) =>
+            Adler32.Hash(source, destination);
+
+        protected override bool TryStaticOneShot(ReadOnlySpan<byte> source, Span<byte> destination, out int bytesWritten) =>
+            Adler32.TryHash(source, destination, out bytesWritten);
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void InstanceAppendAllocate(TestCase testCase)
+        {
+            InstanceAppendAllocateDriver(testCase);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void InstanceAppendAllocateAndReset(TestCase testCase)
+        {
+            InstanceAppendAllocateAndResetDriver(testCase);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void InstanceMultiAppendGetCurrentHash(TestCase testCase)
+        {
+            InstanceMultiAppendGetCurrentHashDriver(testCase);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void InstanceVerifyEmptyState(TestCase testCase)
+        {
+            InstanceVerifyEmptyStateDriver(testCase);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void InstanceVerifyResetState(TestCase testCase)
+        {
+            InstanceVerifyResetStateDriver(testCase);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void StaticVerifyOneShotArray(TestCase testCase)
+        {
+            StaticVerifyOneShotArrayDriver(testCase);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void StaticVerifyOneShotSpanToArray(TestCase testCase)
+        {
+            StaticVerifyOneShotSpanToArrayDriver(testCase);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void StaticVerifyOneShotSpanToSpan(TestCase testCase)
+        {
+            StaticVerifyOneShotSpanToSpanDriver(testCase);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void StaticVerifyTryOneShot(TestCase testCase)
+        {
+            StaticVerifyTryOneShotDriver(testCase);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCases))]
+        public void VerifyHashToUInt32(TestCase testCase)
+        {
+            var alg = new Adler32();
+            alg.Append(testCase.Input);
+            AssertEqualHashNumber(testCase.OutputHex, alg.GetCurrentHashAsUInt32(), littleEndian: true);
+
+            AssertEqualHashNumber(testCase.OutputHex, Adler32.HashToUInt32(testCase.Input), littleEndian: true);
+        }
+    }
+}
