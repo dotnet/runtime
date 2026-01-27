7791aa4d525178a34973411f910fa97425232859
Alan Hayward <alan.hayward@arm.com>
2026-01-27 11:10:59 +0000

    Fix SVE long narrowing for shifts
    Fixes #123496

--- src/coreclr/jit/gentree.cpp
+++ src/coreclr/jit/gentree.cpp
@@ -32697,7 +32697,7 @@ GenTree* Compiler::gtFoldExprHWIntrinsic(GenTreeHWIntrinsic* tree)
                             assert(tree->TypeIs(TYP_SIMD16));
 
                             simd16_t result = {};
-                            NarrowSimdLong<simd16_t>(simdBaseType, &result, otherNode->AsVecCon()->gtSimd16Val);
+                            NarrowAndDuplicateSimdLong<simd16_t>(simdBaseType, &result, otherNode->AsVecCon()->gtSimd16Val);
                             otherNode->AsVecCon()->gtSimd16Val = result;
                         }
 #endif // TARGET_XARCH
