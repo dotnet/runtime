b5c5126300ccdad33572935aa0c090cb2e413e97
AraHaan <seandhunt_7@yahoo.com>
2026-01-28 22:09:39 -0500

    Commented out the optimization paths until they are properly implemented.

--- src/libraries/System.IO.Hashing/src/System/IO/Hashing/Adler32.Avx2Sse.cs
+++ src/libraries/System.IO.Hashing/src/System/IO/Hashing/Adler32.Avx2Sse.cs
@@ -8,18 +8,25 @@ namespace System.IO.Hashing
 {
     public sealed partial class Adler32 : NonCryptographicHashAlgorithm
     {
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        private static uint UpdateScalarAvx2(uint adler, ReadOnlySpan<byte> buf)
-        {
-            // Placeholder implementation until a Avx2 implementation is provided.
-            return adler;
-        }
+        // Commented out until an Avx2 implementation is available.
+        // [MethodImpl(MethodImplOptions.NoInlining)]
+        // private static uint UpdateScalarAvx2(uint adler, ReadOnlySpan<byte> buf)
+        // {
+        //     return adler;
+        // }
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        private static uint UpdateScalarSse(uint adler, ReadOnlySpan<byte> buf)
-        {
-            // Placeholder implementation until a Sse implementation is provided.
-            return adler;
-        }
+        // Commented out until an Ssse3 based implementation is available.
+        // [MethodImpl(MethodImplOptions.NoInlining)]
+        // private static uint UpdateScalarSse(uint adler, ReadOnlySpan<byte> buf)
+        // {
+        //     return adler;
+        // }
+
+        // Commented out until an Arm based implementation is available.
+        // [MethodImpl(MethodImplOptions.NoInlining)]
+        // private static uint UpdateScalarArm(uint adler, ReadOnlySpan<byte> buf)
+        // {
+        //     return adler;
+        // }
     }
 }
