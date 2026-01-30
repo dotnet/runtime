b5f250987d38bc070c2c596a49815ba732e3de7f
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2026-01-30 19:14:35 +0800

    NATIVE_LINK

--- src/coreclr/inc/cor.h
+++ src/coreclr/inc/cor.h
@@ -1577,39 +1577,9 @@ DECLARE_INTERFACE_(IMetaDataInfo, IUnknown)
 
 //**********************************************************************
 //
-// Predefined CustomAttribute and structures for these custom value
+// Predefined CustomAttribute
 //
 //**********************************************************************
-
-//
-// Native Link method custom value definitions. This is for N-direct support.
-//
-
-#include <pshpack1.h>
-typedef struct
-{
-    BYTE        m_linkType;       // see CorNativeLinkType below
-    BYTE        m_flags;          // see CorNativeLinkFlags below
-    mdMemberRef m_entryPoint;     // member ref token giving entry point, format is lib:entrypoint
-} COR_NATIVE_LINK;
-#include <poppack.h>
-
-typedef enum
-{
-    nltNone         = 1,    // none of the keywords are specified
-    nltAnsi         = 2,    // ansi keyword specified
-    nltUnicode      = 3,    // unicode keyword specified
-    nltAuto         = 4,    // auto keyword specified
-    nltMaxValue     = 7,    // used so we can assert how many bits are required for this enum
-} CorNativeLinkType;
-
-typedef enum
-{
-    nlfNone         = 0x00,     // no flags
-    nlfLastError    = 0x01,     // setLastError keyword specified
-    nlfNoMangle     = 0x02,     // nomangle keyword specified
-    nlfMaxValue     = 0x03,     // used so we can assert how many bits are required for this enum
-} CorNativeLinkFlags;
 
 //
 // Base class for security custom attributes.
