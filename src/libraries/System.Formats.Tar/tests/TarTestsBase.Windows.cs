7b5570002cd2ccd3e838acacf6a7c83699c2f96f
Tom Deseyn <tom.deseyn@gmail.com>
2026-02-02 08:53:16 +0100

    TarWriter: when writing entries for the same hard-linked file use HardLink entries.

---
+++ src/libraries/System.Formats.Tar/tests/TarTestsBase.Windows.cs
@@ -0,0 +1,34 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.IO;
+using Microsoft.Win32.SafeHandles;
+using Xunit;
+
+namespace System.Formats.Tar.Tests
+{
+    public abstract partial class TarTestsBase
+    {
+        protected void AssertPathsAreHardLinked(string path1, string path2)
+        {
+            Assert.Equal(GetFileId(path1), GetFileId(path2));
+
+            static (uint dwVolumeSerialNumber, uint nFileIndexHigh, uint nFileIndexLow) GetFileId(string path)
+            {
+                using SafeFileHandle handle = Interop.Kernel32.CreateFile(
+                    path,
+                    Interop.Kernel32.GenericOperations.GENERIC_READ,
+                    FileShare.ReadWrite | FileShare.Delete,
+                    FileMode.Open,
+                    Interop.Kernel32.FileOperations.FILE_FLAG_BACKUP_SEMANTICS | Interop.Kernel32.FileOperations.FILE_FLAG_OPEN_REPARSE_POINT);
+
+                if (!Interop.Kernel32.GetFileInformationByHandle(handle, out Interop.Kernel32.BY_HANDLE_FILE_INFORMATION fileInfo))
+                {
+                    throw new IOException($"Failed to get file information for {path}");
+                }
+
+                return (fileInfo.dwVolumeSerialNumber, fileInfo.nFileIndexHigh, fileInfo.nFileIndexLow);
+            }
+        }
+    }
+}
