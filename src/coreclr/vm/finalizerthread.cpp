042ec8e56abeca80174fd0a979722317ea95fc82
pavelsavara <pavel.savara@gmail.com>
2026-02-02 15:34:44 +0100

    unix only contract

--- src/coreclr/vm/finalizerthread.cpp
+++ src/coreclr/vm/finalizerthread.cpp
@@ -555,7 +555,11 @@ DWORD WINAPI FinalizerThread::FinalizerThreadStart(void *args)
 {
     CONTRACTL
     {
+#ifdef TARGET_UNIX // because UNINSTALL_UNHANDLED_MANAGED_EXCEPTION_TRAP is catching exceptions only on Unix. On Windows OS is handling that.
         NOTHROW;
+#else // TARGET_UNIX
+        THROWS;
+#endif // TARGET_UNIX
         GC_TRIGGERS;
         MODE_PREEMPTIVE;
     }
