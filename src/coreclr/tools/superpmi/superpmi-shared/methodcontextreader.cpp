dotnet/runtime

commit a87245f02b8b59fd08da8f79b9097206f5dfb83e

CleanUp GetFileAttribute/SetFileAttribute(Ex) in PAL (#116096)
* Use stat in superpmi
* Delete GetFileAttributes
* Delete SetFikeAttributes
* Delete tests

src/coreclr/tools/superpmi/superpmi-shared/methodcontextreader.cpp
@@ -13,6 +13,10 @@
 #include "logging.h"
 #include "runtimedetails.h"
 
+#if TARGET_UNIX
+#include <sys/stat.h>
+#endif // TARGET_UNIX
+
 // Just a helper...
 HANDLE MethodContextReader::OpenFile(const char* inputFile, DWORD flags)
 {
@@ -32,6 +36,20 @@ static std::string to_lower(const std::string& input)
     return res;
 }
 
+bool test_filename_available(const std::string& path)
+{
+#ifdef TARGET_WINDOWS
+    DWORD attribs = GetFileAttributesA(path.c_str());
+    return (attribs != INVALID_FILE_ATTRIBUTES) && !(attribs & FILE_ATTRIBUTE_DIRECTORY);
+#else // TARGET_WINDOWS
+    struct stat stat_data;
+    if (stat(path.c_str(), &stat_data) != 0)
+        return false;
+
+    return (stat_data.st_mode & S_IFMT) == S_IFREG;
+#endif // TARGET_WINDOWS
+}
+
 // Looks for a file named foo.origSuffix.newSuffix or foo.newSuffix
 // but only if foo.origSuffix exists.
 //
@@ -47,20 +65,17 @@ std::string MethodContextReader::CheckForPairedFile(const std::string& fileName,
     if (suffix_offset == std::string::npos || suffix_offset == 0 || (tmp != to_lower(fileName.substr(suffix_offset))))
         return std::string();
 
-    DWORD attribs = GetFileAttributesA(fileName.c_str());
-    if ((attribs == INVALID_FILE_ATTRIBUTES) || (attribs & FILE_ATTRIBUTE_DIRECTORY))
+    if (test_filename_available(fileName))
         return std::string();
 
     // next, check foo.orig.new from foo.orig
     tmp = fileName + newSuffix;
-    attribs = GetFileAttributesA(tmp.c_str());
-    if ((attribs != INVALID_FILE_ATTRIBUTES) && !(attribs & FILE_ATTRIBUTE_DIRECTORY))
+    if (test_filename_available(tmp))
         return tmp;
 
     // Finally, lets try foo.new from foo.orig
     tmp = fileName.substr(0, suffix_offset) + newSuffix;
-    attribs = GetFileAttributesA(tmp.c_str());
-    if ((attribs != INVALID_FILE_ATTRIBUTES) && !(attribs & FILE_ATTRIBUTE_DIRECTORY))
+    if (test_filename_available(tmp))
         return tmp;
 
     return std::string();

src/coreclr/dlls/mscordac/mscordac_unixexports.src
@@ -86,8 +86,6 @@ nativeStringResourceTable_mscorrc
 #GetEnvironmentStringsW
 #GetEnvironmentVariableA
 #GetEnvironmentVariableW
-#GetFileAttributesExW
-#GetFileAttributesW
 #GetFileSize
 #GetFullPathNameW
 #GetLastError

src/coreclr/inc/longfilepathwrappers.h
@@ -26,13 +26,6 @@ CreateFileWrapper(
     );
 
 BOOL
-GetFileAttributesExWrapper(
-    _In_ LPCWSTR lpFileName,
-    _In_ GET_FILEEX_INFO_LEVELS fInfoLevelId,
-    _Out_writes_bytes_(sizeof(WIN32_FILE_ATTRIBUTE_DATA)) LPVOID lpFileInformation
-    );
-
-BOOL
 CopyFileExWrapper(
     _In_        LPCWSTR lpExistingFileName,
     _In_        LPCWSTR lpNewFileName,

src/coreclr/inc/winwrap.h
@@ -42,11 +42,9 @@
 #ifdef HOST_WINDOWS
 #define WszLoadLibrary         LoadLibraryExWrapper
 #define WszCreateFile          CreateFileWrapper
-#define WszGetFileAttributesEx GetFileAttributesExWrapper
 #else // HOST_WINDOWS
 #define WszLoadLibrary         LoadLibraryExW
 #define WszCreateFile          CreateFileW
-#define WszGetFileAttributesEx GetFileAttributesExW
 #endif // HOST_WINDOWS
 
 //APIS which have a buffer as an out parameter

src/coreclr/pal/inc/pal.h
@@ -517,56 +517,6 @@ SearchPathW(
 
 #define SearchPath  SearchPathW
 
-PALIMPORT
-DWORD
-PALAPI
-GetFileAttributesW(
-           IN LPCWSTR lpFileName);
-
-#ifdef UNICODE
-#define GetFileAttributes GetFileAttributesW
-#else
-#define GetFileAttributes GetFileAttributesA
-#endif
-
-typedef enum _GET_FILEEX_INFO_LEVELS {
-  GetFileExInfoStandard
-} GET_FILEEX_INFO_LEVELS;
-
-typedef enum _FINDEX_INFO_LEVELS {
-    FindExInfoStandard,
-    FindExInfoBasic,
-    FindExInfoMaxInfoLevel
-} FINDEX_INFO_LEVELS;
-
-typedef enum _FINDEX_SEARCH_OPS {
-    FindExSearchNameMatch,
-    FindExSearchLimitToDirectories,
-    FindExSearchLimitToDevices,
-    FindExSearchMaxSearchOp
-} FINDEX_SEARCH_OPS;
-
-typedef struct _WIN32_FILE_ATTRIBUTE_DATA {
-    DWORD      dwFileAttributes;
-    FILETIME   ftCreationTime;
-    FILETIME   ftLastAccessTime;
-    FILETIME   ftLastWriteTime;
-    DWORD      nFileSizeHigh;
-    DWORD      nFileSizeLow;
-} WIN32_FILE_ATTRIBUTE_DATA, *LPWIN32_FILE_ATTRIBUTE_DATA;
-
-PALIMPORT
-BOOL
-PALAPI
-GetFileAttributesExW(
-             IN LPCWSTR lpFileName,
-             IN GET_FILEEX_INFO_LEVELS fInfoLevelId,
-             OUT LPVOID lpFileInformation);
-
-#ifdef UNICODE
-#define GetFileAttributesEx GetFileAttributesExW
-#endif
-
 typedef struct _OVERLAPPED {
     ULONG_PTR Internal;
     ULONG_PTR InternalHigh;

src/coreclr/pal/inc/palprivate.h
@@ -43,26 +43,6 @@ CreateDirectoryW(
 PALIMPORT
 DWORD
 PALAPI
-GetFileAttributesA(
-           IN LPCSTR lpFileName);
-
-PALIMPORT
-BOOL
-PALAPI
-SetFileAttributesA(
-           IN LPCSTR lpFileName,
-           IN DWORD dwFileAttributes);
-
-PALIMPORT
-BOOL
-PALAPI
-SetFileAttributesW(
-           IN LPCWSTR lpFileName,
-           IN DWORD dwFileAttributes);
-
-PALIMPORT
-DWORD
-PALAPI
 GetFullPathNameA(
          IN LPCSTR lpFileName,
          IN DWORD nBufferLength,

src/coreclr/pal/src/config.h.in
@@ -63,10 +63,6 @@
 #cmakedefine01 HAVE__FPX_SW_BYTES_WITH_XSTATE_BV
 #cmakedefine01 HAVE_PR_SET_PTRACER
 
-#cmakedefine01 HAVE_STAT_TIMESPEC
-#cmakedefine01 HAVE_STAT_TIM
-#cmakedefine01 HAVE_STAT_NSEC
-
 #cmakedefine01 HAVE_BSD_REGS_T
 #cmakedefine01 HAVE_PT_REGS
 #cmakedefine01 HAVE_GREGSET_T

src/coreclr/pal/src/configure.cmake
@@ -151,9 +151,6 @@ int main(int argc, char **argv) {
   return 0;
 }" HAVE_CPUSET_T)
 
-check_struct_has_member ("struct stat" st_atimespec "sys/types.h;sys/stat.h" HAVE_STAT_TIMESPEC)
-check_struct_has_member ("struct stat" st_atim "sys/types.h;sys/stat.h" HAVE_STAT_TIM)
-check_struct_has_member ("struct stat" st_atimensec "sys/types.h;sys/stat.h" HAVE_STAT_NSEC)
 check_struct_has_member ("ucontext_t" uc_mcontext.gregs[0] ucontext.h HAVE_GREGSET_T)
 check_struct_has_member ("ucontext_t" uc_mcontext.__gregs[0] ucontext.h HAVE___GREGSET_T)
 check_struct_has_member ("ucontext_t" uc_mcontext.fpregs->__glibc_reserved1[0] ucontext.h HAVE_FPSTATE_GLIBC_RESERVED1)

commit 0e38af0e1543e36c75eafadb4f9e4b12d65a8640

SPMI: Fix TOC logic broken after PAL cleanup (#123993)
This was broken in #116096. It means that SPMI has not been using TOC
files since then, resulting in significant superpmi-diffs slowdown
(creating asmdiffs for the examples became very slow, taking multiple
minutes).

Before:
```
Measure-Command { C:\dev\dotnet\runtime\artifacts\tests\coreclr\windows.x64.Debug\Tests\Core_Root\superpmi.exe C:\dev\dotnet\runtime\artifacts\tests\coreclr\windows.x64.Debug\Tests\Core_Root\clrjit.dll C:\dev\dotnet\spmi\mch\c10e2acf-7c94-4c61-b895-5178602e0e1e.windows.x64\libraries_tests.run.windows.x64.Release.mch -c 684683 }
...
TotalMilliseconds : 2554.6079
```

After:
```
Measure-Command { C:\dev\dotnet\runtime\artifacts\tests\coreclr\windows.x64.Debug\Tests\Core_Root\superpmi.exe C:\dev\dotnet\runtime\artifacts\tests\coreclr\windows.x64.Debug\Tests\Core_Root\clrjit.dll C:\dev\dotnet\spmi\mch\c10e2acf-7c94-4c61-b895-5178602e0e1e.windows.x64\libraries_tests.run.windows.x64.Release.mch -c 684683 }
...
TotalMilliseconds : 46.0829
```

src/coreclr/tools/superpmi/superpmi-shared/methodcontextreader.cpp
@@ -65,7 +65,7 @@ std::string MethodContextReader::CheckForPairedFile(const std::string& fileName,
     if (suffix_offset == std::string::npos || suffix_offset == 0 || (tmp != to_lower(fileName.substr(suffix_offset))))
         return std::string();
 
-    if (test_filename_available(fileName))
+    if (!test_filename_available(fileName))
         return std::string();
 
     // next, check foo.orig.new from foo.orig