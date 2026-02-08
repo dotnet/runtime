732a71ebb0c57302e6b9b3acd04dc435ac770c69
EgorBo <egorbo@gmail.com>
2026-02-08 02:31:30 +0100

    impl

--- src/coreclr/jit/rangecheck.cpp
+++ src/coreclr/jit/rangecheck.cpp
@@ -889,7 +889,7 @@ void RangeCheck::MergeEdgeAssertions(Compiler*        comp,
         return;
     }
 
-    if (normalLclVN == ValueNumStore::NoVN)
+    if (!comp->optAssertionHasAssertionsForVN(normalLclVN))
     {
         return;
     }
