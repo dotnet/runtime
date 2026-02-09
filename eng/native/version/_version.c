7839ab6f8b960eb586668e86e3da89273e08dfdb
Andy Gocke <andy@commentout.net>
2026-02-09 08:36:04 -0800

    Try to narrow down problem

--- eng/native/version/_version.c
+++ eng/native/version/_version.c
@@ -1,4 +1,4 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-static char sccsid[] __attribute__((used)) = "@(#)No version information produced";
+static char sccsid[] __attribute__((used)) = "@(#)Version 42.42.42.42424";
