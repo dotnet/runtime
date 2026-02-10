d04615f2cc03e20adfe54fc4da433aec4d7acccf
Katelyn Gadd <kg@luminance.org>
2026-02-10 12:06:04 -0800

    Implement AppendMangledName to generate emscripten/wabt-style signatures

--- src/coreclr/tools/Common/Compiler/DependencyAnalysis/Target_Wasm/WasmTypeNode.cs
+++ src/coreclr/tools/Common/Compiler/DependencyAnalysis/Target_Wasm/WasmTypeNode.cs
@@ -50,8 +50,19 @@ namespace ILCompiler.DependencyAnalysis.Wasm
             return result;
         }
 
-        public void AppendMangledName(NameMangler nameMangler, Internal.Text.Utf8StringBuilder sb) =>
-            sb.Append(GetName(null!));
+        public void AppendMangledName(NameMangler nameMangler, Internal.Text.Utf8StringBuilder sb)
+        {
+            foreach (var type in _types)
+                sb.Append(type switch {
+                    CorInfoWasmType.CORINFO_WASM_TYPE_VOID => 'v',
+                    CorInfoWasmType.CORINFO_WASM_TYPE_V128 => 'V',
+                    CorInfoWasmType.CORINFO_WASM_TYPE_F64  => 'd',
+                    CorInfoWasmType.CORINFO_WASM_TYPE_F32  => 'f',
+                    CorInfoWasmType.CORINFO_WASM_TYPE_I64  => 'j',
+                    CorInfoWasmType.CORINFO_WASM_TYPE_I32  => 'i',
+                    _ => throw new NotImplementedException($"Unknown CorInfoWasmType: {type}"),
+                });
+        }
 
         public int Offset => 0;
     }
