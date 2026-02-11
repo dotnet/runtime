0e08ad2605191af6b2bab08b849877175c5bda52
EgorBo <egorbo@gmail.com>
2026-02-12 00:47:12 +0100

    Fix failed ElidedBoundsChecks test

--- src/coreclr/jit/assertionprop.cpp
+++ src/coreclr/jit/assertionprop.cpp
@@ -1385,6 +1385,15 @@ AssertionIndex Compiler::optAddAssertion(const AssertionDsc& newAssertion)
         {
             mayHaveDuplicates |= optAssertionHasAssertionsForVN(newAssertion.GetOp2().GetCheckedBound(),
                                                                 /* addIfNotFound */ canAddNewAssertions);
+
+            // Additionally, check for the pattern of "VN + const == checkedBndVN" and register "VN" as well.
+            ValueNum addOpVN;
+            int      addOpCns;
+            if ((newAssertion.GetOp2().GetCheckedBoundConstant() == 0) &&
+                vnStore->IsVNBinFuncWithConst(newAssertion.GetOp1().GetVN(), VNF_ADD, &addOpVN, &addOpCns))
+            {
+                mayHaveDuplicates |= optAssertionHasAssertionsForVN(addOpVN, /* addIfNotFound */ canAddNewAssertions);
+            }
         }
 
         if (mayHaveDuplicates)
