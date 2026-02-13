dotnet/runtime

commit 17189337194c9e97a26017f6b9b5b3dba7881367

Replace ReadFile/WriteFile usages with stdio (#116203)
Replaces the majority of ReadFile/WriteFile instances with stdio, and
fstream for some superpmi cases.
Remaining instances are StgIO and superpmi/fileio, which uses the same
file handles for memory map.

src/coreclr/vm/multicorejit.cpp
@@ -18,7 +18,6 @@
 #include "stubgen.h"
 #include "eventtrace.h"
 #include "array.h"
-#include "fstream.h"
 #include "hash.h"
 #include "minipal/time.h"
 
@@ -28,6 +27,7 @@
 #include "eventtracebase.h"
 #include "multicorejit.h"
 #include "multicorejitimpl.h"
+#include <dn-stdio.h>
 
 void MulticoreJitFireEtw(const WCHAR * pAction, const WCHAR * pTarget, int p1, int p2, int p3)
 {
@@ -154,11 +154,12 @@ HRESULT MulticoreJitRecorder::WriteOutput()
 
     EX_TRY
     {
-        CFileStream fileStream;
+        FILE* fp;
 
-        if (SUCCEEDED(hr = fileStream.OpenForWrite(m_fullFileName.GetUnicode())))
+        if (fopen_lp(&fp, m_fullFileName.GetUnicode(), W("wb")) == 0)
         {
-            hr = WriteOutput(& fileStream);
+            hr = WriteOutput(fp);
+            fclose(fp);
         }
     }
     EX_CATCH
@@ -169,7 +170,7 @@ HRESULT MulticoreJitRecorder::WriteOutput()
 }
 
 
-HRESULT WriteData(IStream * pStream, const void * pData, unsigned len)
+HRESULT WriteData(FILE * fp, const void * pData, unsigned len)
 {
     CONTRACTL
     {
@@ -179,11 +180,11 @@ HRESULT WriteData(IStream * pStream, const void * pData, unsigned len)
     }
     CONTRACTL_END
 
-    ULONG cbWritten;
+    size_t cbWritten = fwrite(pData, 1, len, fp);
 
-    HRESULT hr = pStream->Write(pData, len, & cbWritten);
+    HRESULT hr = S_OK;
 
-    if (SUCCEEDED(hr) && (cbWritten != len))
+    if (cbWritten != len)
     {
         hr = E_FAIL;
     }
@@ -192,7 +193,7 @@ HRESULT WriteData(IStream * pStream, const void * pData, unsigned len)
 }
 
 // Write string, round to DWORD alignment
-HRESULT WriteString(const void * pString, unsigned len, IStream * pStream)
+HRESULT WriteString(const void * pString, unsigned len, FILE * fp)
 {
     CONTRACTL
     {
@@ -202,25 +203,23 @@ HRESULT WriteString(const void * pString, unsigned len, IStream * pStream)
     }
     CONTRACTL_END;
 
-    ULONG cbWritten = 0;
+    size_t cbWritten = fwrite(pString, 1, len, fp);
 
-    HRESULT hr;
-
-    hr = pStream->Write(pString, len, & cbWritten);
-
-    if (SUCCEEDED(hr))
+    if (cbWritten == (size_t)len)
     {
         len = RoundUp(len) - len;
 
         if (len != 0)
         {
-            cbWritten = 0;
+            uint32_t temp = 0;
+            cbWritten = fwrite(&temp, 1, len, fp);
 
-            hr = pStream->Write(& cbWritten, len, & cbWritten);
+            if (cbWritten == (size_t)len)
+                return S_OK;
         }
     }
 
-    return hr;
+    return E_FAIL;
 }
 
 
@@ -329,7 +328,7 @@ bool RecorderModuleInfo::SetModule(Module * pMod)
 //
 /////////////////////////////////////////////////////
 
-HRESULT MulticoreJitRecorder::WriteModuleRecord(IStream * pStream, const RecorderModuleInfo & module)
+HRESULT MulticoreJitRecorder::WriteModuleRecord(FILE * fp, const RecorderModuleInfo & module)
 {
     CONTRACTL
     {
@@ -355,15 +354,15 @@ HRESULT MulticoreJitRecorder::WriteModuleRecord(IStream * pStream, const Recorde
     mod.wLoadLevel     = (unsigned short) module.loadLevel;
     mod.flags          = module.flags;
 
-    hr = WriteData(pStream, & mod, sizeof(mod));
+    hr = WriteData(fp, & mod, sizeof(mod));
 
     if (SUCCEEDED(hr))
     {
-        hr = WriteString(pModuleName, lenModuleName, pStream);
+        hr = WriteString(pModuleName, lenModuleName, fp);
 
         if (SUCCEEDED(hr))
         {
-            hr = WriteString(pAssemblyName, lenAssemblyName, pStream);
+            hr = WriteString(pAssemblyName, lenAssemblyName, fp);
         }
     }
 
@@ -371,7 +370,7 @@ HRESULT MulticoreJitRecorder::WriteModuleRecord(IStream * pStream, const Recorde
 }
 
 
-HRESULT MulticoreJitRecorder::WriteOutput(IStream * pStream)
+HRESULT MulticoreJitRecorder::WriteOutput(FILE * fp)
 {
     CONTRACTL
     {
@@ -486,14 +485,14 @@ HRESULT MulticoreJitRecorder::WriteOutput(IStream * pStream)
 
         _ASSERTE((sizeof(header) % sizeof(unsigned)) == 0);
 
-        hr = WriteData(pStream, & header, sizeof(header));
+        hr = WriteData(fp, & header, sizeof(header));
     }
 
     DWORD dwData = 0;
 
     for (unsigned i = 0; SUCCEEDED(hr) && (i < m_ModuleCount); i ++)
     {
-        hr = WriteModuleRecord(pStream, m_ModuleList[i]);
+        hr = WriteModuleRecord(fp, m_ModuleList[i]);
     }
 
     for (LONG i = 0 ; i < m_JitInfoCount && SUCCEEDED(hr); i++)
@@ -504,7 +503,7 @@ HRESULT MulticoreJitRecorder::WriteOutput(IStream * pStream)
             _ASSERTE(m_JitInfoArray[i].IsFullyInitialized());
 
             DWORD data1 = m_JitInfoArray[i].GetRawModuleData();
-            hr = WriteData(pStream, &data1, sizeof(data1));
+            hr = WriteData(fp, &data1, sizeof(data1));
         }
         else if (m_JitInfoArray[i].IsGenericMethodInfo())
         {
@@ -522,19 +521,19 @@ HRESULT MulticoreJitRecorder::WriteOutput(IStream * pStream)
             DWORD sigSize = m_JitInfoArray[i].GetMethodSignatureSize();
             DWORD paddingSize = m_JitInfoArray[i].GetMethodRecordPaddingSize();
 
-            hr = WriteData(pStream, &data1, sizeof(data1));
+            hr = WriteData(fp, &data1, sizeof(data1));
             if (SUCCEEDED(hr))
             {
-                hr = WriteData(pStream, &data2, sizeof(data2));
+                hr = WriteData(fp, &data2, sizeof(data2));
             }
             if (SUCCEEDED(hr))
             {
-                hr = WriteData(pStream, pSignature, sigSize);
+                hr = WriteData(fp, pSignature, sigSize);
             }
             if (SUCCEEDED(hr) && paddingSize > 0)
             {
                 DWORD tmp = 0;
-                hr = WriteData(pStream, &tmp, paddingSize);
+                hr = WriteData(fp, &tmp, paddingSize);
             }
         }
         else
@@ -545,10 +544,10 @@ HRESULT MulticoreJitRecorder::WriteOutput(IStream * pStream)
             DWORD data1 = m_JitInfoArray[i].GetRawMethodData1();
             unsigned data2 = m_JitInfoArray[i].GetRawMethodData2NonGeneric();
 
-            hr = WriteData(pStream, &data1, sizeof(data1));
+            hr = WriteData(fp, &data1, sizeof(data1));
             if (SUCCEEDED(hr))
             {
-                hr = WriteData(pStream, &data2, sizeof(data2));
+                hr = WriteData(fp, &data2, sizeof(data2));
             }
         }
     }

src/coreclr/dlls/mscorpe/pewriter.cpp
@@ -4,6 +4,7 @@
 
 #include "blobfetcher.h"
 #include "pedecoder.h"
+#include <dn-stdio.h>
 
 #ifdef _DEBUG
 #define LOGGING
@@ -547,7 +548,7 @@ HRESULT PEWriter::Init(PESectionMan *pFrom, DWORD createFlags)
     headers = NULL;
     headersEnd = NULL;
 
-    m_file = INVALID_HANDLE_VALUE;
+    m_file = NULL;
 
     return S_OK;
 }
@@ -626,21 +627,15 @@ HRESULT PEWriter::setDirectoryEntry(PEWriterSection *section, ULONG entry, ULONG
     return S_OK;
 }
 
-//-----------------------------------------------------------------------------
-// These 2 write functions must be implemented here so that they're in the same
-// .obj file as whoever creates the FILE struct. We can't pass a FILE struct
-// across a dll boundary and use it.
-//-----------------------------------------------------------------------------
-
-HRESULT PEWriterSection::write(HANDLE file)
+HRESULT PEWriterSection::write(FILE* file)
 {
     return m_blobFetcher.Write(file);
 }
 
 //-----------------------------------------------------------------------------
-// Write out the section to the stream
+// Write out the section to the file
 //-----------------------------------------------------------------------------
-HRESULT CBlobFetcher::Write(HANDLE file)
+HRESULT CBlobFetcher::Write(FILE* file)
 {
 // Must write out each pillar (including idx = m_nIndexUsed), one after the other
     unsigned idx;
@@ -648,10 +643,10 @@ HRESULT CBlobFetcher::Write(HANDLE file)
         if (m_pIndex[idx].GetDataLen() > 0)
         {
             ULONG length = m_pIndex[idx].GetDataLen();
-            DWORD dwWritten = 0;
-            if (!WriteFile(file, m_pIndex[idx].GetRawDataStart(), length, &dwWritten, NULL))
+            size_t dwWritten = 0;
+            if ((dwWritten = fwrite(m_pIndex[idx].GetRawDataStart(), 1, length, file)) <= 0)
             {
-                return HRESULT_FROM_GetLastError();
+                return HRESULTFromErrno();
             }
             _ASSERTE(dwWritten == length);
         }
@@ -1336,37 +1331,32 @@ HRESULT PEWriter::fixup(CeeGenTokenMapper *pMapper)
 
 HRESULT PEWriter::Open(_In_ LPCWSTR fileName)
 {
-    _ASSERTE(m_file == INVALID_HANDLE_VALUE);
+    _ASSERTE(m_file == NULL);
     HRESULT hr = NOERROR;
 
-    m_file = WszCreateFile(fileName,
-                           GENERIC_WRITE,
-                           0, // No sharing.  Was: FILE_SHARE_READ | FILE_SHARE_WRITE,
-                           NULL,
-                           CREATE_ALWAYS,
-                           FILE_ATTRIBUTE_NORMAL,
-                           NULL );
-    if (m_file == INVALID_HANDLE_VALUE)
-        hr = HRESULT_FROM_GetLastErrorNA();
+    int err = fopen_lp(&m_file, fileName, W("wb"));
+
+    if (err != 0)
+        hr = HRESULTFromErrno();
 
     return hr;
 }
 
 HRESULT PEWriter::Seek(int offset)
 {
-    _ASSERTE(m_file != INVALID_HANDLE_VALUE);
-    if (SetFilePointer(m_file, offset, 0, FILE_BEGIN))
+    _ASSERTE(m_file != NULL);
+    if (fseek(m_file, offset, SEEK_SET) == 0)
         return S_OK;
     else
-        return HRESULT_FROM_GetLastError();
+        return HRESULTFromErrno();
 }
 
 HRESULT PEWriter::Write(const void *data, int size)
 {
-    _ASSERTE(m_file != INVALID_HANDLE_VALUE);
+    _ASSERTE(m_file != NULL);
 
     HRESULT hr = S_OK;
-    DWORD dwWritten = 0;
+    size_t dwWritten = 0;
     if (size)
     {
         CQuickBytes zero;
@@ -1379,13 +1369,13 @@ HRESULT PEWriter::Write(const void *data, int size)
                 data = zero.Ptr();
             }
         }
-
-        if (WriteFile(m_file, data, size, &dwWritten, NULL))
+        _ASSERTE(data != NULL);
+        if ((dwWritten = fwrite(data, 1, size, m_file)) >= 0)
         {
-            _ASSERTE(dwWritten == (DWORD)size);
+            _ASSERTE(dwWritten == (size_t)size);
         }
         else
-            hr = HRESULT_FROM_GetLastError();
+            hr = HRESULTFromErrno();
     }
 
     return hr;
@@ -1393,7 +1383,7 @@ HRESULT PEWriter::Write(const void *data, int size)
 
 HRESULT PEWriter::Pad(int align)
 {
-    DWORD offset = SetFilePointer(m_file, 0, NULL, FILE_CURRENT);
+    DWORD offset = (DWORD)ftell(m_file);
     int pad = padLen(offset, align);
     if (pad > 0)
         return Write(NULL, pad);
@@ -1403,16 +1393,17 @@ HRESULT PEWriter::Pad(int align)
 
 HRESULT PEWriter::Close()
 {
-    if (m_file == INVALID_HANDLE_VALUE)
+    if (m_file == NULL)
         return S_OK;
 
+    int err = fclose(m_file);
     HRESULT hr;
-    if (CloseHandle(m_file))
+    if (err == 0)
         hr = S_OK;
     else
-        hr = HRESULT_FROM_GetLastError();
+        hr = HRESULTFromErrno();
 
-    m_file = INVALID_HANDLE_VALUE;
+    m_file = NULL;
 
     return hr;
 }

src/coreclr/dlls/mscorpe/pewriter.h
@@ -111,7 +111,7 @@ private:
     ULONG  m_codeRvaBase;
     DWORD  m_peFileTimeStamp;
 
-    HANDLE   m_file;
+    FILE*   m_file;
 
     PEWriterSection **getSectStart() {
         return (PEWriterSection**)sectStart;
@@ -203,7 +203,7 @@ public:
                          DWORD               dataRvaBase,
                          DWORD               textRvaBase);
 
-    virtual HRESULT  write      (HANDLE file);
+    virtual HRESULT  write      (FILE* file);
     virtual unsigned writeMem   (void ** pMem);
 };
 

src/coreclr/ilasm/asmman.cpp
@@ -10,6 +10,7 @@
 #include "assembler.h"
 #include "strongnameinternal.h"
 #include <limits.h>
+#include <dn-stdio.h>
 
 extern WCHAR*   pwzInputFiles[];
 
@@ -404,28 +405,23 @@ void    AsmMan::EndAssembly()
                 else
                 {
                     // Read public key or key pair from file.
-                    HANDLE hFile = WszCreateFile(((Assembler*)m_pAssembler)->m_wzKeySourceName,
-                                                 GENERIC_READ,
-                                                 FILE_SHARE_READ,
-                                                 NULL,
-                                                 OPEN_EXISTING,
-                                                 FILE_ATTRIBUTE_NORMAL | FILE_FLAG_SEQUENTIAL_SCAN,
-                                                 NULL);
-                    if(hFile == INVALID_HANDLE_VALUE)
+                    FILE* fp;
+                    int err = fopen_lp(&fp, ((Assembler*)m_pAssembler)->m_wzKeySourceName, W("rb"));
+                    if (err != 0)
                     {
-                        hr = GetLastError();
                         MAKE_UTF8PTR_FROMWIDE(keySourceNameUtf8, ((Assembler*)m_pAssembler)->m_wzKeySourceName);
-                        report->error("Failed to open key file '%s': 0x%08X\n",keySourceNameUtf8,hr);
+                        report->error("Failed to open key file '%s': 0x%08X\n",keySourceNameUtf8,err);
                         m_pCurAsmRef = NULL;
                         return;
                     }
 
                     // Determine file size and allocate an appropriate buffer.
-                    m_sStrongName.m_cbPublicKey = SafeGetFileSize(hFile, NULL);
-                    if (m_sStrongName.m_cbPublicKey == 0xffffffff) {
+                    int64_t fSize = fgetsize(fp);
+                    m_sStrongName.m_cbPublicKey = (DWORD)fSize;
+                    if (fSize > UINT32_MAX) {
                         report->error("File size too large\n");
                         m_pCurAsmRef = NULL;
-                        CloseHandle(hFile);
+                        fclose(fp);
                         return;
                     }
 
@@ -433,23 +429,24 @@ void    AsmMan::EndAssembly()
                     if (m_sStrongName.m_pbPublicKey == NULL) {
                         report->error("Failed to allocate key buffer\n");
                         m_pCurAsmRef = NULL;
-                        CloseHandle(hFile);
+                        fclose(fp);
                         return;
                     }
                     m_sStrongName.m_dwPublicKeyAllocated = AsmManStrongName::AllocatedByNew;
 
                     // Read the file into the buffer.
-                    DWORD dwBytesRead;
-                    if (!ReadFile(hFile, m_sStrongName.m_pbPublicKey, m_sStrongName.m_cbPublicKey, &dwBytesRead, NULL)) {
-                        hr = GetLastError();
+                    size_t dwBytesRead;
+                    
+                    if ((dwBytesRead = fread(m_sStrongName.m_pbPublicKey, 1, m_sStrongName.m_cbPublicKey, fp)) <= m_sStrongName.m_cbPublicKey) {
+                        HRESULT hr = HRESULTFromErrno();
                         MAKE_UTF8PTR_FROMWIDE(keySourceNameUtf8, ((Assembler*)m_pAssembler)->m_wzKeySourceName);
-                        report->error("Failed to read key file '%s': 0x%08X\n",keySourceNameUtf8,hr);
+                        report->error("Failed to read key file '%s': 0x%d\n",keySourceNameUtf8,hr);
                         m_pCurAsmRef = NULL;
-                        CloseHandle(hFile);
+                        fclose(fp);
                         return;
                     }
 
-                    CloseHandle(hFile);
+                    fclose(fp);
 
                     // Guess whether we're full or delay signing based on
                     // whether the blob passed to us looks like a public
@@ -954,13 +951,13 @@ HRESULT AsmMan::EmitManifest()
             }
             else // embedded mgd.resource, go after the file
             {
-                HANDLE hFile = INVALID_HANDLE_VALUE;
+                FILE* fp = NULL;
                 int j;
                 WCHAR   wzFileName[2048];
                 WCHAR*  pwz;
 
                 pManRes->ulOffset = m_dwMResSizeTotal;
-                for(j=0; (hFile == INVALID_HANDLE_VALUE)&&(pwzInputFiles[j] != NULL); j++)
+                for(j=0; (fp == NULL)&&(pwzInputFiles[j] != NULL); j++)
                 {
                     wcscpy_s(wzFileName,2048,pwzInputFiles[j]);
                     pwz = (WCHAR*)u16_strrchr(wzFileName,DIRECTORY_SEPARATOR_CHAR_A);
@@ -970,10 +967,10 @@ HRESULT AsmMan::EmitManifest()
                     if(pwz == NULL) pwz = &wzFileName[0];
                     else pwz++;
                     wcscpy_s(pwz,2048-(pwz-wzFileName),wzUniBuf);
-                    hFile = WszCreateFile(wzFileName, GENERIC_READ, FILE_SHARE_READ,
-                             0, OPEN_EXISTING, 0, 0);
+                    if (fopen_lp(&fp, wzFileName, W("rb")) != 0)
+                        fp = NULL;
                 }
-                if (hFile == INVALID_HANDLE_VALUE)
+                if (fp == NULL)
                 {
                     report->error("Failed to open managed resource file '%s'\n",pManRes->szAlias);
                     fOK = FALSE;
@@ -987,14 +984,16 @@ HRESULT AsmMan::EmitManifest()
                     }
                     else
                     {
-                        m_dwMResSize[m_dwMResNum] = SafeGetFileSize(hFile,NULL);
-                        if(m_dwMResSize[m_dwMResNum] == 0xFFFFFFFF)
+                        uint64_t fSize = fgetsize(fp);
+                        if(fSize >= 0xFFFFFFFF)
                         {
+                            m_dwMResSize[m_dwMResNum] = 0xFFFFFFFF;
                             report->error("Failed to get size of managed resource file '%s'\n",pManRes->szAlias);
                             fOK = FALSE;
                         }
                         else
                         {
+                            m_dwMResSize[m_dwMResNum] = (DWORD)fSize;
                             m_dwMResSizeTotal += m_dwMResSize[m_dwMResNum]+sizeof(DWORD);
                             m_wzMResName[m_dwMResNum] = new WCHAR[u16_strlen(wzFileName)+1];
                             wcscpy_s(m_wzMResName[m_dwMResNum],u16_strlen(wzFileName)+1,wzFileName);
@@ -1003,7 +1002,7 @@ HRESULT AsmMan::EmitManifest()
                         }
                     }
 
-                    CloseHandle(hFile);
+                    fclose(fp);
                 }
             }
             if(fOK || ((Assembler*)m_pAssembler)->OnErrGo)

src/coreclr/ildasm/dman.cpp
@@ -716,8 +716,7 @@ static void DumpResourceFile(void *GUICookie, BYTE *pRes, DWORD dwOffset, LPCWST
         if (g_pFile != NULL) // embedded resource -- dump as .resources file
         {
             FILE *pF = NULL;
-            _wfopen_s(&pF, pParam->wzFileName, W("wb"));
-            if (pF)
+            if (fopen_lp(&pF, pParam->wzFileName, W("wb")) == 0)
             {
                 struct Param
                 {

src/coreclr/ildasm/dres.cpp
@@ -240,8 +240,7 @@ DWORD   DumpResourceToFile(_In_ __nullterminated WCHAR*   wzFileName)
                 ret = 1;
 #ifdef RES_FILE_DUMP_ENABLED
 
-                _wfopen_s(&pF,wzFileName,L"wb");
-                if(pF)
+                if(fopen_lp(&pF,wzFileName,L"wb") == 0)
                 {
                     // Dump them to pF
                     // Write dummy header

src/coreclr/ildasm/ildasmpch.h
@@ -13,6 +13,7 @@
 #include <string.h>
 #include <limits.h>
 #include <algorithm>
+#include "dn-stdio.h"
 
 using std::min;
 using std::max;

commit 7a9a56b33bbaaeabd82af368356759455f496387

Fix Optimization Profile writing (#124330)
In NET11 preview 1 optimization profile can't be saved if there is a
module with name length DWORD-aligned
This appears to be a regression introduced by PR
https://github.com/dotnet/runtime/pull/116203

src/coreclr/vm/multicorejit.cpp
@@ -209,14 +209,16 @@ HRESULT WriteString(const void * pString, unsigned len, FILE * fp)
     {
         len = RoundUp(len) - len;
 
-        if (len != 0)
+        if (len == 0)
         {
-            uint32_t temp = 0;
-            cbWritten = fwrite(&temp, 1, len, fp);
-
-            if (cbWritten == (size_t)len)
-                return S_OK;
+            return S_OK;
         }
+
+        uint32_t temp = 0;
+        cbWritten = fwrite(&temp, 1, len, fp);
+
+        if (cbWritten == (size_t)len)
+            return S_OK;
     }
 
     return E_FAIL;