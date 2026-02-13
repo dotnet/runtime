2e5f8171fdf9298029362edde81ca0a578371d32
Adam Sitnik <adam.sitnik@gmail.com>
2026-02-13 16:53:21 +0100

    fix the build

--- src/libraries/System.Diagnostics.Process/tests/SafeProcessHandleTests.Windows.cs
+++ src/libraries/System.Diagnostics.Process/tests/SafeProcessHandleTests.Windows.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Microsoft.Win32.SafeHandles;
 using Xunit;
