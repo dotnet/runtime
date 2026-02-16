214a18dd188239f6aeb4b171ae8a2e21b36ab70b
Jeff Handley <jeffhandley@users.noreply.github.com>
2026-02-15 23:44:05 -0800

    Use TrimEnd to force trimming the separator char off the root

--- src/libraries/System.Private.CoreLib/src/System/IO/DirectoryInfo.cs
+++ src/libraries/System.Private.CoreLib/src/System/IO/DirectoryInfo.cs
@@ -83,14 +83,10 @@ namespace System.IO
             string newPath = Path.GetFullPath(Path.Combine(FullPath, path));
 
             ReadOnlySpan<char> trimmedNewPath = Path.TrimEndingDirectorySeparator(newPath.AsSpan());
-            ReadOnlySpan<char> trimmedCurrentPath = Path.TrimEndingDirectorySeparator(FullPath.AsSpan());
 
-            // If trimmedCurrentPath still has a trailing separator (root directory case like "C:\" or "/"),
-            // remove it for proper boundary checking
-            if (trimmedCurrentPath.Length > 0 && PathInternal.IsDirectorySeparator(trimmedCurrentPath[trimmedCurrentPath.Length - 1]))
-            {
-                trimmedCurrentPath = trimmedCurrentPath.Slice(0, trimmedCurrentPath.Length - 1);
-            }
+            // Trim any trailing separator, including from a root path for proper boundary checking.
+            // TrimEndingDirectorySeparator does not trim the character from path roots.
+            ReadOnlySpan<char> trimmedCurrentPath = FullPath.TrimEnd(Path.DirectorySeparatorChar);
 
             // We want to make sure the requested directory is actually under the subdirectory.
             if (trimmedNewPath.StartsWith(trimmedCurrentPath, PathInternal.StringComparison)
