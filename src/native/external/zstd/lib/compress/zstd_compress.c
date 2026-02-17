1f11e1198c52663c0af99c190260c9fa74b8cd56
kzrnm <gengesa@gmail.com>
2026-02-17 09:51:34 +0900

    Avoid non-ascii charactor

--- src/native/external/zstd/lib/compress/zstd_compress.c
+++ src/native/external/zstd/lib/compress/zstd_compress.c
@@ -7404,7 +7404,7 @@ BlockSummary ZSTD_get1BlockSummary(const ZSTD_Sequence* seqs, size_t nbSeqs)
 
     /* Process 2 structs (32 bytes) at a time */
     for (i = 0; i + 2 <= nbSeqs; i += 2) {
-        /* Load two consecutive ZSTD_Sequence (8×4 = 32 bytes) */
+        /* Load two consecutive ZSTD_Sequence (8x4 = 32 bytes) */
         __m256i data     = _mm256_loadu_si256((const __m256i*)(const void*)&seqs[i]);
         /* check end of block signal */
         __m256i cmp      = _mm256_cmpeq_epi32(data, zeroVec);
