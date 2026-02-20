8904240552c0b0cdf579ed6d9fac3be41f438ea8
pavelsavara <pavel.savara@gmail.com>
2026-02-20 17:03:49 +0100

    rebase on top of boring PR

--- src/libraries/System.Private.CoreLib/src/System/IO/Stream.cs
+++ src/libraries/System.Private.CoreLib/src/System/IO/Stream.cs
@@ -219,13 +219,15 @@ namespace System.IO
 
             // The synchronous path is emulating legacy behavior.
             // Drop the emulation for !IsMultithreadingSupported to avoid throwing.
-            if (!Thread.IsMultithreadingSupported || serializeAsynchronously)
+            if (!RuntimeFeature.IsMultithreadingSupported || serializeAsynchronously)
             {
                 semaphoreTask = semaphore.WaitAsync();
             }
             else
             {
+#pragma warning disable CA1416 // guarded by IsMultithreadingSupported
                 semaphore.Wait();
+#pragma warning restore CA1416 // guarded by IsMultithreadingSupported
             }
 
             // Create the task to asynchronously do a Read.  This task serves both
@@ -494,13 +496,15 @@ namespace System.IO
 
             // The synchronous path is emulating legacy behavior.
             // Drop the emulation for !IsMultithreadingSupported to avoid throwing.
-            if (!Thread.IsMultithreadingSupported || serializeAsynchronously)
+            if (!RuntimeFeature.IsMultithreadingSupported || serializeAsynchronously)
             {
                 semaphoreTask = semaphore.WaitAsync(); // kick off the asynchronous wait, but don't block
             }
             else
             {
-                semaphore.Wait(); // synchronously wait here
+#pragma warning disable CA1416 // guarded by IsMultithreadingSupported
+                semaphore.Wait();
+#pragma warning restore CA1416 // guarded by IsMultithreadingSupported
             }
 
             // Create the task to asynchronously do a Write.  This task serves both
