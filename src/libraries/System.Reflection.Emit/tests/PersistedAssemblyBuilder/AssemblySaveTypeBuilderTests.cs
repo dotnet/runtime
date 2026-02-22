dotnet/runtime

commit 2ff39b5f21a304c04ee0726c3e2ae56e7b23c904

`PersistedAssemblyBuilder`: Fix encoding of custom modifiers. (#123925)
Fixes #123857.

---------

Co-authored-by: Jan Kotas <jkotas@microsoft.com>

src/libraries/System.Reflection.Emit/src/System/Reflection/Emit/SignatureHelper.cs
@@ -30,8 +30,7 @@ namespace System.Reflection.Emit
         {
             BlobBuilder fieldSignature = new();
             FieldTypeEncoder encoder = new BlobEncoder(fieldSignature).Field();
-            WriteReturnTypeCustomModifiers(encoder.CustomModifiers(), requiredCustomModifiers, optionalCustomModifiers, module);
-            WriteSignatureForType(encoder.Type(), fieldType, module);
+            WriteSignatureForType(encoder.Type(), fieldType, module, requiredCustomModifiers, optionalCustomModifiers);
 
             return fieldSignature;
         }
@@ -85,16 +84,8 @@ namespace System.Reflection.Emit
             new BlobEncoder(methodSignature).MethodSignature(convention, genParamCount, isInstance).
                     Parameters(paramsLength, out ReturnTypeEncoder retEncoder, out ParametersEncoder parEncoder);
 
-            WriteReturnTypeCustomModifiers(retEncoder.CustomModifiers(), returnTypeRequiredModifiers, returnTypeOptionalModifiers, module);
-
-            if (returnType != null && returnType != module.GetTypeFromCoreAssembly(CoreTypeId.Void))
-            {
-                WriteSignatureForType(retEncoder.Type(), returnType, module);
-            }
-            else
-            {
-                retEncoder.Void();
-            }
+            returnType ??= module.GetTypeFromCoreAssembly(CoreTypeId.Void);
+            WriteSignatureForType(retEncoder.Type(), returnType, module, returnTypeRequiredModifiers, returnTypeOptionalModifiers);
 
             WriteParametersSignature(module, parameters, parEncoder, parameterRequiredModifiers, parameterOptionalModifiers);
 
@@ -123,20 +114,6 @@ namespace System.Reflection.Emit
             return parameterTypes;
         }
 
-        private static void WriteReturnTypeCustomModifiers(CustomModifiersEncoder encoder,
-            Type[]? requiredModifiers, Type[]? optionalModifiers, ModuleBuilderImpl module)
-        {
-            if (requiredModifiers != null)
-            {
-                WriteCustomModifiers(encoder, requiredModifiers, isOptional: false, module);
-            }
-
-            if (optionalModifiers != null)
-            {
-                WriteCustomModifiers(encoder, optionalModifiers, isOptional: true, module);
-            }
-        }
-
         private static void WriteCustomModifiers(CustomModifiersEncoder encoder, Type[] customModifiers, bool isOptional, ModuleBuilderImpl module)
         {
             // GetOptionalCustomModifiers and GetRequiredCustomModifiers return modifiers in reverse order
@@ -156,17 +133,10 @@ namespace System.Reflection.Emit
                 {
                     ParameterTypeEncoder encoder = parameterEncoder.AddParameter();
 
-                    if (requiredModifiers != null && requiredModifiers.Length > i && requiredModifiers[i] != null)
-                    {
-                        WriteCustomModifiers(encoder.CustomModifiers(), requiredModifiers[i], isOptional: false, module);
-                    }
+                    Type[]? modreqs = (requiredModifiers != null && requiredModifiers.Length > i) ? requiredModifiers[i] : null;
+                    Type[]? modopts = (optionalModifiers != null && optionalModifiers.Length > i) ? optionalModifiers[i] : null;
 
-                    if (optionalModifiers != null && optionalModifiers.Length > i && optionalModifiers[i] != null)
-                    {
-                        WriteCustomModifiers(encoder.CustomModifiers(), optionalModifiers[i], isOptional: true, module);
-                    }
-
-                    WriteSignatureForType(encoder.Type(), parameters[i], module);
+                    WriteSignatureForType(encoder.Type(), parameters[i], module, modreqs, modopts);
                 }
             }
         }
@@ -179,15 +149,16 @@ namespace System.Reflection.Emit
                 PropertySignature(isInstanceProperty: property.CallingConventions.HasFlag(CallingConventions.HasThis)).
                 Parameters(property.ParameterTypes == null ? 0 : property.ParameterTypes.Length, out ReturnTypeEncoder retType, out ParametersEncoder paramEncoder);
 
-            WriteReturnTypeCustomModifiers(retType.CustomModifiers(), property._returnTypeRequiredCustomModifiers, property._returnTypeOptionalCustomModifiers, module);
-            WriteSignatureForType(retType.Type(), property.PropertyType, module);
+            WriteSignatureForType(retType.Type(), property.PropertyType, module, property._returnTypeRequiredCustomModifiers, property._returnTypeOptionalCustomModifiers);
             WriteParametersSignature(module, property.ParameterTypes, paramEncoder, property._parameterTypeRequiredCustomModifiers, property._parameterTypeOptionalCustomModifiers);
 
             return propertySignature;
         }
 
-        private static void WriteSignatureForType(SignatureTypeEncoder signature, Type type, ModuleBuilderImpl module)
+        private static void WriteSignatureForType(SignatureTypeEncoder signature, Type type, ModuleBuilderImpl module, Type[]? requiredModifiers = null, Type[]? optionalModifiers = null)
         {
+            WriteCustomModifiers(signature.CustomModifiers(), requiredModifiers ?? type.GetRequiredCustomModifiers(), isOptional: false, module);
+            WriteCustomModifiers(signature.CustomModifiers(), optionalModifiers ?? type.GetOptionalCustomModifiers(), isOptional: true, module);
             if (type.IsArray)
             {
                 Type elementType = type.GetElementType()!;
@@ -199,8 +170,8 @@ namespace System.Reflection.Emit
                 else
                 {
                     signature.Array(out SignatureTypeEncoder elTypeSignature, out ArrayShapeEncoder arrayEncoder);
-                    WriteSimpleSignature(elTypeSignature, elementType, module);
-                    arrayEncoder.Shape(type.GetArrayRank(), ImmutableArray.Create<int>(), ImmutableArray.Create<int>(new int[rank]));
+                    WriteSignatureForType(elTypeSignature, elementType, module);
+                    arrayEncoder.Shape(type.GetArrayRank(), [], default);
                 }
             }
             else if (type.IsPointer)
@@ -287,26 +258,11 @@ namespace System.Reflection.Emit
             MethodSignatureEncoder sigEncoder = signature.FunctionPointer(callConv, attribs);
             sigEncoder.Parameters(paramTypes.Length, out ReturnTypeEncoder retTypeEncoder, out ParametersEncoder paramsEncoder);
 
-            CustomModifiersEncoder retModifiersEncoder = retTypeEncoder.CustomModifiers();
-
-            if (returnType.GetOptionalCustomModifiers() is Type[] retModOpts)
-                WriteCustomModifiers(retModifiersEncoder, retModOpts, isOptional: true, module);
-
-            if (returnType.GetRequiredCustomModifiers() is Type[] retModReqs)
-                WriteCustomModifiers(retModifiersEncoder, retModReqs, isOptional: false, module);
-
             WriteSignatureForType(retTypeEncoder.Type(), returnType, module);
 
             foreach (Type paramType in paramTypes)
             {
                 ParameterTypeEncoder paramEncoder = paramsEncoder.AddParameter();
-                CustomModifiersEncoder paramModifiersEncoder = paramEncoder.CustomModifiers();
-
-                if (paramType.GetOptionalCustomModifiers() is Type[] paramModOpts)
-                    WriteCustomModifiers(paramModifiersEncoder, paramModOpts, isOptional: true, module);
-
-                if (paramType.GetRequiredCustomModifiers() is Type[] paramModReqs)
-                    WriteCustomModifiers(paramModifiersEncoder, paramModReqs, isOptional: false, module);
 
                 WriteSignatureForType(paramEncoder.Type(), paramType, module);
             }

src/libraries/System.Reflection.Emit/tests/PersistedAssemblyBuilder/AssemblySaveTypeBuilderTests.cs
@@ -627,6 +627,59 @@ namespace System.Reflection.Emit.Tests
         }
 
         [Fact]
+        public void SaveInterfaceOverrideWithCustomModifier()
+        {
+            using (TempFile file = TempFile.Create())
+            {
+                AssemblyName name = new("TestAssembly");
+                PersistedAssemblyBuilder assemblyBuilder = AssemblySaveTools.PopulateAssemblyBuilder(name);
+                ModuleBuilder mb = assemblyBuilder.DefineDynamicModule("My Module");
+
+                TypeBuilder tb = mb.DefineType("IMethodWithModifiersImpl", TypeAttributes.Class | TypeAttributes.Public);
+                tb.AddInterfaceImplementation(typeof(IMethodWithModifiers));
+                MethodInfo mRun = typeof(IMethodWithModifiers).GetMethod(nameof(IMethodWithModifiers.Run));
+                MethodBuilder m = tb.DefineMethod("IMethodWithModifiers.Run",
+                    MethodAttributes.Private | MethodAttributes.Final | MethodAttributes.HideBySig | MethodAttributes.NewSlot | MethodAttributes.Virtual,
+                    CallingConventions.Standard,
+                    returnType: mRun.ReturnParameter.GetModifiedParameterType(),
+                    returnTypeRequiredCustomModifiers: null,
+                    returnTypeOptionalCustomModifiers: null,
+                    // The first parameter will have modreqs specified from parameterTypeRequiredCustomModifiers, and the second from parameterTypes.
+                    parameterTypes: mRun.GetParameters().Select((x, i) => i == 0 ? x.ParameterType : x.GetModifiedParameterType()).ToArray(),
+                    parameterTypeRequiredCustomModifiers: [[typeof(InAttribute)], null],
+                    parameterTypeOptionalCustomModifiers: null);
+                tb.DefineMethodOverride(m, mRun);
+                ParameterBuilder pb = m.DefineParameter(1, ParameterAttributes.In, "x");
+                pb.SetCustomAttribute(new CustomAttributeBuilder(typeof(IsReadOnlyAttribute).GetConstructor(types: []), []));
+                m.GetILGenerator().Emit(OpCodes.Ret);
+                MethodInfo mRun2 = typeof(IMethodWithModifiers).GetMethod(nameof(IMethodWithModifiers.Run2));
+                MethodBuilder m2 = tb.DefineMethod("IMethodWithModifiers.Run2",
+                    MethodAttributes.Private | MethodAttributes.Final | MethodAttributes.HideBySig | MethodAttributes.NewSlot | MethodAttributes.Virtual,
+                    CallingConventions.Standard,
+                    returnType: mRun2.ReturnParameter.GetModifiedParameterType(),
+                    returnTypeRequiredCustomModifiers: null,
+                    returnTypeOptionalCustomModifiers: null,
+                    parameterTypes: mRun2.GetParameters().Select(x => x.GetModifiedParameterType()).ToArray(),
+                    // Test that passing null gets modreqs from the parameter types.
+                    parameterTypeRequiredCustomModifiers: null,
+                    parameterTypeOptionalCustomModifiers: null);
+                tb.DefineMethodOverride(m2, mRun2);
+                ParameterBuilder pb2 = m2.DefineParameter(1, ParameterAttributes.In, "x");
+                pb2.SetCustomAttribute(new CustomAttributeBuilder(typeof(IsReadOnlyAttribute).GetConstructor(types: []), []));
+                m2.GetILGenerator().Emit(OpCodes.Ret);
+
+                tb.CreateType();
+                assemblyBuilder.Save(file.Path);
+
+                TestAssemblyLoadContext context = new();
+                // Load the assembly and check that loading the type does not throw.
+                Assembly loadedAsm = context.LoadFromAssemblyPath(file.Path);
+                _ = loadedAsm.GetType(tb.Name, throwOnError: true);
+                context.Unload();
+            }
+        }
+
+        [Fact]
         public void SaveMultipleGenericTypeParametersToEnsureSortingWorks()
         {
             using (TempFile file = TempFile.Create())
@@ -870,7 +923,7 @@ namespace System.Reflection.Emit.Tests
             // public unsafe class Container
             // {
             //     public static delegate*<int, int, int> Method;
-            // 
+            //
             //     public static int Add(int a, int b) => a + b;
             //     public static void Init() => Method = &Add;
             // }
@@ -981,6 +1034,12 @@ namespace System.Reflection.Emit.Tests
     public interface IOneMethod
     {
         object Func(string a, short b);
+    }
+
+    public interface IMethodWithModifiers
+    {
+        unsafe void Run(in int x, delegate*<in long, void> f);
+        void Run2(in int x);
     }
 
     public struct EmptyStruct

src/libraries/System.Reflection.Emit/tests/PersistedAssemblyBuilder/AssemblySaveILGeneratorTests.cs
@@ -2638,9 +2638,9 @@ public class MyType
                 TypeBuilder nestedItem = type.DefineNestedType("ItemInfo", TypeAttributes.NestedPublic);
                 GenericTypeParameterBuilder itemParam = nestedItem.DefineGenericParameters(genParams)[0];
                 TypeBuilder nestedSector = type.DefineNestedType("Sector", TypeAttributes.NestedPublic);
-                GenericTypeParameterBuilder nestedParam = nestedSector.DefineGenericParameters(genParams)[0];
+                GenericTypeParameterBuilder sectorParam = nestedSector.DefineGenericParameters(genParams)[0];
 
-                Type nestedOfT = nestedItem.MakeGenericType(nestedParam);
+                Type nestedOfT = nestedItem.MakeGenericType(sectorParam);
                 Type parent = typeof(HashSet<>).MakeGenericType(nestedOfT);
                 nestedSector.SetParent(parent);
 

src/libraries/System.Reflection.Emit/tests/System.Reflection.Emit.Tests.csproj
@@ -135,6 +135,7 @@
   </ItemGroup>
 
   <ItemGroup>
+    <ProjectReference Include="$(LibrariesProjectRoot)System.Reflection.Emit\src\System.Reflection.Emit.csproj" />
     <ProjectReference Include="$(LibrariesProjectRoot)System.Reflection.MetadataLoadContext\src\System.Reflection.MetadataLoadContext.csproj" />
   </ItemGroup>
 

src/mono/System.Private.CoreLib/src/System/Reflection/ModifiedType.Mono.cs
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Diagnostics;
+
 namespace System.Reflection
 {
     internal partial class ModifiedType
@@ -19,11 +21,13 @@ namespace System.Reflection
         ///         volatile delegate* unmanaged[Cdecl]&lt;int&gt; fptrField2;
         ///     NOTE: In scenario 3) the SignatureHolderInfo has higher priority for retrieving field data (like custom modifiers)
         /// </summary>
-        internal struct TypeSignature
+        internal readonly struct TypeSignature
         {
-            internal readonly RuntimeType? SignatureHolderType;
-            internal readonly object? SignatureHolderInfo;
-            internal int ParameterIndex;
+            public readonly RuntimeType? SignatureHolderType;
+            public readonly object? SignatureHolderInfo;
+            // The index of the function pointer (starting from 1, 0 being the return type)
+            // or generic parameter for which we are retrieving modifiers.
+            public readonly int ParameterIndex;
 
             internal TypeSignature(RuntimeType signatureHolderType, int parameterIndex)
             {
@@ -39,8 +43,9 @@ namespace System.Reflection
                 ParameterIndex = parameterIndex;
             }
 
-            internal TypeSignature(RuntimeType signatureHolderType, object signatureHolderInfo, int parameterIndex)
+            internal TypeSignature(RuntimeType? signatureHolderType, object? signatureHolderInfo, int parameterIndex)
             {
+                Debug.Assert(signatureHolderType is not null || signatureHolderInfo is not null);
                 SignatureHolderType = signatureHolderType;
                 SignatureHolderInfo = signatureHolderInfo;
                 ParameterIndex = parameterIndex;
@@ -88,15 +93,15 @@ namespace System.Reflection
             }
         }
 
-        internal static Type Create(Type sourceType, object sourceTypeInfo, int parameterIndex = 0)
+        internal static Type Create(Type sourceType, object sourceTypeInfo)
         {
             var unmodifiedType = (RuntimeType)sourceType;
             TypeSignature typeSignature;
 
             if (unmodifiedType.IsFunctionPointer)
-                typeSignature = new TypeSignature(unmodifiedType, sourceTypeInfo, parameterIndex);
+                typeSignature = new TypeSignature(unmodifiedType, sourceTypeInfo, -1);
             else
-                typeSignature = new TypeSignature(sourceTypeInfo, parameterIndex);
+                typeSignature = new TypeSignature(sourceTypeInfo, -1);
 
             return Create(unmodifiedType, typeSignature);
         }
@@ -118,18 +123,12 @@ namespace System.Reflection
             }
             else
             {
+                var parentSignatureHolderInfo = _typeSignature.SignatureHolderInfo;
                 if (parentUnmodifiedType.IsFunctionPointer)
                 {
-                    var parentSignatureHolderType = _typeSignature.SignatureHolderType ??
-                        throw new Exception($"Parent's {nameof(_typeSignature.SignatureHolderType)} cannot be null");
-                    childTypeSignature = new TypeSignature(parentSignatureHolderType, index);
+                    parentSignatureHolderInfo = null;
                 }
-                else
-                {
-                    var parentSignatureHolderInfo = _typeSignature.SignatureHolderInfo ??
-                        throw new Exception($"Parent's {nameof(_typeSignature.SignatureHolderInfo)} cannot be null");
-                    childTypeSignature = new TypeSignature(parentSignatureHolderInfo, index);
-                }
+                childTypeSignature = new TypeSignature(_typeSignature.SignatureHolderType, parentSignatureHolderInfo, index);
             }
 
             return Create(childUnmodifiedType, childTypeSignature);

src/mono/System.Private.CoreLib/src/System/Reflection/RuntimeParameterInfo.cs
@@ -425,6 +425,6 @@ namespace System.Reflection
 
         internal Type[] GetCustomModifiersFromModifiedType(bool optional, int genericArgumentPosition) => GetTypeModifiers(ParameterType, Member, Position, optional, genericArgumentPosition) ?? Type.EmptyTypes;
 
-        public override Type GetModifiedParameterType() => ModifiedType.Create(ParameterType, this, PositionImpl + 1);
+        public override Type GetModifiedParameterType() => ModifiedType.Create(ParameterType, this);
     }
 }

commit 20c325cb2f62e5e6c9bb00d370095aea0287db63

Fix bug introduced in #123925

src/libraries/System.Reflection.Emit/src/System/Reflection/Emit/SignatureHelper.cs
@@ -258,13 +258,23 @@ namespace System.Reflection.Emit
             MethodSignatureEncoder sigEncoder = signature.FunctionPointer(callConv, attribs);
             sigEncoder.Parameters(paramTypes.Length, out ReturnTypeEncoder retTypeEncoder, out ParametersEncoder paramsEncoder);
 
-            WriteSignatureForType(retTypeEncoder.Type(), returnType, module);
+            WriteSignatureForType(
+                retTypeEncoder.Type(),
+                returnType,
+                module,
+                returnType.GetRequiredCustomModifiers(),
+                returnType.GetOptionalCustomModifiers());
 
             foreach (Type paramType in paramTypes)
             {
                 ParameterTypeEncoder paramEncoder = paramsEncoder.AddParameter();
 
-                WriteSignatureForType(paramEncoder.Type(), paramType, module);
+                WriteSignatureForType(
+                    paramEncoder.Type(),
+                    paramType,
+                    module,
+                    paramType.GetRequiredCustomModifiers(),
+                    paramType.GetOptionalCustomModifiers());
             }
         }
 

src/libraries/System.Reflection.Emit/tests/PersistedAssemblyBuilder/AssemblySaveTypeBuilderTests.cs
@@ -860,17 +860,24 @@ namespace System.Reflection.Emit.Tests
             tb.DefineField("FuncPtr1", funcPtr1, FieldAttributes.Public | FieldAttributes.Static);
 
             // delegate* unmanaged[Cdecl]<int, float, double>
-            Type funcPtr4 = new ModifiedTypeHelpers.FunctionPointer(
+            Type funcPtr2 = new ModifiedTypeHelpers.FunctionPointer(
                 typeof(delegate* unmanaged[Cdecl]<int, float, double>),
                 [typeof(CallConvCdecl)]);
-            tb.DefineField("FuncPtr2", funcPtr4, FieldAttributes.Public | FieldAttributes.Static);
+            tb.DefineField("FuncPtr2", funcPtr2, FieldAttributes.Public | FieldAttributes.Static);
 
             // delegate* unmanaged[Stdcall]<string, in int, void>
-            Type funcPtr5 = new ModifiedTypeHelpers.FunctionPointer(
+            Type funcPtr3 = new ModifiedTypeHelpers.FunctionPointer(
                 typeof(delegate* unmanaged[Stdcall]<string, in int, void>),
                 [typeof(CallConvStdcall)],
                 customParameterTypes: [typeof(string), new ModifiedTypeHelpers.ModifiedType(typeof(int).MakeByRefType(), [typeof(InAttribute)], [])]);
-            tb.DefineField("FuncPtr3", funcPtr5, FieldAttributes.Public | FieldAttributes.Static);
+            tb.DefineField("FuncPtr3", funcPtr3, FieldAttributes.Public | FieldAttributes.Static);
+
+            // delegate* unmanaged[Fastcall, SuppressGCTransition]<void>
+            Type funcPtr4 = new ModifiedTypeHelpers.FunctionPointer(
+                typeof(delegate* unmanaged[Fastcall, SuppressGCTransition]<void>),
+                [typeof(CallConvFastcall), typeof(CallConvSuppressGCTransition)],
+                new ModifiedTypeHelpers.ModifiedType(typeof(void), [], [typeof(CallConvFastcall), typeof(CallConvSuppressGCTransition)]));
+            tb.DefineField("FuncPtr4", funcPtr4, FieldAttributes.Public | FieldAttributes.Static);
 
             tb.CreateType();
             ab.Save(file.Path);
@@ -889,8 +896,8 @@ namespace System.Reflection.Emit.Tests
             Assert.Equal(typeof(int).FullName, field1.FieldType.GetFunctionPointerReturnType().FullName);
 
             FieldInfo field2 = testType.GetField("FuncPtr2");
-            Type field2Type = field2.GetModifiedFieldType();
             Assert.NotNull(field2);
+            Type field2Type = field2.GetModifiedFieldType();
             Assert.True(field2Type.IsFunctionPointer);
             Assert.True(field2Type.IsUnmanagedFunctionPointer);
             Type[] paramTypes2 = field2Type.GetFunctionPointerParameterTypes();
@@ -902,8 +909,8 @@ namespace System.Reflection.Emit.Tests
             Assert.Contains(callingConventions2, t => t.FullName == typeof(CallConvCdecl).FullName);
 
             FieldInfo field3 = testType.GetField("FuncPtr3");
-            Type field3Type = field3.GetModifiedFieldType();
             Assert.NotNull(field3);
+            Type field3Type = field3.GetModifiedFieldType();
             Assert.True(field3Type.IsFunctionPointer);
             Assert.True(field3Type.IsUnmanagedFunctionPointer);
             Type[] paramTypes3 = field3Type.GetFunctionPointerParameterTypes();
@@ -914,6 +921,18 @@ namespace System.Reflection.Emit.Tests
             Assert.Equal(typeof(void).FullName, field3Type.GetFunctionPointerReturnType().FullName);
             Type[] callingConventions3 = field3Type.GetFunctionPointerCallingConventions();
             Assert.Contains(callingConventions3, t => t.FullName == typeof(CallConvStdcall).FullName);
+
+            FieldInfo field4 = testType.GetField("FuncPtr4");
+            Assert.NotNull(field4);
+            Type field4Type = field4.GetModifiedFieldType();
+            Assert.True(field4Type.IsFunctionPointer);
+            Assert.True(field4Type.IsUnmanagedFunctionPointer);
+            Type[] paramTypes4 = field4Type.GetFunctionPointerParameterTypes();
+            Assert.Equal(0, paramTypes4.Length);
+            Assert.Equal(typeof(void).FullName, field4Type.GetFunctionPointerReturnType().FullName);
+            Type[] callingConventions4 = field4Type.GetFunctionPointerCallingConventions();
+            Assert.Contains(callingConventions4, t => t.FullName == typeof(CallConvFastcall).FullName);
+            Assert.Contains(callingConventions4, t => t.FullName == typeof(CallConvSuppressGCTransition).FullName);
         }
 
         [Fact]

src/libraries/System.Reflection.Emit/tests/Utilities.cs
@@ -177,11 +177,11 @@ namespace System.Reflection.Emit.Tests
 
             public FunctionPointer(
                 Type baseFunctionPointerType,
-                Type[] conventions = null,
+                Type[]? conventions = null,
                 Type customReturnType = null,
-                Type[] customParameterTypes = null,
-                Type[] fnPtrRequiredMods = null,
-                Type[] fnPtrOptionalMods = null)
+                Type[]? customParameterTypes = null,
+                Type[]? fnPtrRequiredMods = null,
+                Type[]? fnPtrOptionalMods = null)
                 : base(baseFunctionPointerType)
             {
                 callingConventions = conventions ?? [];
@@ -203,7 +203,7 @@ namespace System.Reflection.Emit.Tests
             private readonly Type[] requiredModifiers;
             private readonly Type[] optionalModifiers;
 
-            public ModifiedType(Type delegatingType, Type[] requiredMods = null, Type[] optionalMods = null)
+            public ModifiedType(Type delegatingType, Type[]? requiredMods = null, Type[]? optionalMods = null)
                 : base(delegatingType)
             {
                 requiredModifiers = requiredMods ?? [];