67293c153e72c36bc209b4200a982c86af68ae0d
Jonas Heiß <loschag@outlook.de>
2026-02-23 18:09:37 +0100

    Remove unused using

--- src/libraries/System.Reflection.Emit/tests/Utilities.cs
+++ src/libraries/System.Reflection.Emit/tests/Utilities.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using Xunit;
 
 namespace System.Reflection.Emit.Tests
