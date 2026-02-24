dotnet/runtime

commit 1fa1745581e26069093a679626e346b675aa534f

Improve software exception handling performance (#108480)
* Improve software exception handling performance

I have recently discovered that large part of the time spent in the EH
while handling software exception is in the RtlLookupFunctionEntry
Windows API. That API is called when unwinding native (non-managed)
frames on stack. The current implementation of the IL_Throw JIT helper
that is used to throw managed exceptions needs three unwinds to get to
the managed caller. Due to the two passes of EH, it means there are six
calls made to that API per throw.
This change reduces that to just a single unwind, which leads to about
12% improvement in single threaded scenarios and about 30% improvement
in multi threaded one for a case when an exception is thrown over 10
managed frames and then caught. It also leads to similar improvements in
async exception handling performance. On Linux, the multi-threaded gain is much higher,
I've measured 3 fold improvement.

An additional benefit of this change is removal of the helper method
frame from IL_Throw and IL_Rethrow, which contributes to the current
effort of getting rid of the helper method frames.

I have made this change for the new EH only.

src/coreclr/vm/excep.cpp
@@ -11553,3 +11553,79 @@ MethodDesc * GetUserMethodForILStub(Thread * pThread, UINT_PTR uStubSP, MethodDe
 #endif // FEATURE_COMINTEROP
     return pUserMD;
 }
+
+
+#ifdef FEATURE_EH_FUNCLETS
+
+void SoftwareExceptionFrame::UpdateRegDisplay(const PREGDISPLAY pRD, bool updateFloats)
+{
+    LIMITED_METHOD_DAC_CONTRACT;
+
+#define CALLEE_SAVED_REGISTER(regname) pRD->pCurrentContext->regname = *dac_cast<PTR_SIZE_T>((TADDR)m_ContextPointers.regname);
+    ENUM_CALLEE_SAVED_REGISTERS();
+#undef CALLEE_SAVED_REGISTER
+
+#define CALLEE_SAVED_REGISTER(regname) pRD->pCurrentContextPointers->regname = m_ContextPointers.regname;
+    ENUM_CALLEE_SAVED_REGISTERS();
+#undef CALLEE_SAVED_REGISTER
+
+#define CALLEE_SAVED_REGISTER(regname) pRD->pCurrentContext->regname = m_Context.regname;
+    ENUM_FP_CALLEE_SAVED_REGISTERS();
+#undef CALLEE_SAVED_REGISTER
+
+    SetIP(pRD->pCurrentContext, ::GetIP(&m_Context));
+    SetSP(pRD->pCurrentContext, ::GetSP(&m_Context));
+
+    pRD->ControlPC = ::GetIP(&m_Context);
+    pRD->SP = ::GetSP(&m_Context);
+
+    pRD->IsCallerContextValid = FALSE;
+    pRD->IsCallerSPValid      = FALSE;        // Don't add usage of this field.  This is only temporary.
+}
+
+#ifndef DACCESS_COMPILE
+//
+// Init a new frame
+//
+void SoftwareExceptionFrame::Init()
+{
+    WRAPPER_NO_CONTRACT;
+
+#define CALLEE_SAVED_REGISTER(regname) m_ContextPointers.regname = NULL;
+    ENUM_CALLEE_SAVED_REGISTERS();
+#undef CALLEE_SAVED_REGISTER
+
+#ifndef TARGET_UNIX
+    Thread::VirtualUnwindCallFrame(&m_Context, &m_ContextPointers);
+#else // !TARGET_UNIX
+    BOOL success = PAL_VirtualUnwind(&m_Context, &m_ContextPointers);
+    if (!success)
+    {
+        _ASSERTE(!"SoftwareExceptionFrame::Init failed");
+        EEPOLICY_HANDLE_FATAL_ERROR(COR_E_EXECUTIONENGINE);
+    }
+#endif // !TARGET_UNIX
+
+#define CALLEE_SAVED_REGISTER(regname) if (m_ContextPointers.regname == NULL) m_ContextPointers.regname = &m_Context.regname;
+    ENUM_CALLEE_SAVED_REGISTERS();
+#undef CALLEE_SAVED_REGISTER
+
+    _ASSERTE(ExecutionManager::IsManagedCode(::GetIP(&m_Context)));
+
+    m_ReturnAddress = ::GetIP(&m_Context);
+}
+
+//
+// Init and Link in a new frame
+//
+void SoftwareExceptionFrame::InitAndLink(Thread *pThread)
+{
+    WRAPPER_NO_CONTRACT;
+
+    Init();
+
+    Push(pThread);
+}
+
+#endif // DACCESS_COMPILE
+#endif // FEATURE_EH_FUNCLETS

src/coreclr/inc/vptr_list.h
@@ -55,6 +55,9 @@ VPTR_CLASS(DebuggerSecurityCodeMarkFrame)
 VPTR_CLASS(DebuggerExitFrame)
 VPTR_CLASS(DebuggerU2MCatchHandlerFrame)
 VPTR_CLASS(FaultingExceptionFrame)
+#ifdef FEATURE_EH_FUNCLETS
+VPTR_CLASS(SoftwareExceptionFrame)
+#endif // FEATURE_EH_FUNCLETS
 VPTR_CLASS(FuncEvalFrame)
 VPTR_CLASS(HelperMethodFrame)
 VPTR_CLASS(HelperMethodFrame_1OBJ)

src/coreclr/vm/frames.h
@@ -190,6 +190,9 @@ FRAME_TYPE_NAME(ResumableFrame)
 FRAME_TYPE_NAME(RedirectedThreadFrame)
 #endif // FEATURE_HIJACK
 FRAME_TYPE_NAME(FaultingExceptionFrame)
+#ifdef FEATURE_EH_FUNCLETS
+FRAME_TYPE_NAME(SoftwareExceptionFrame)
+#endif // FEATURE_EH_FUNCLETS
 #ifdef DEBUGGING_SUPPORTED
 FRAME_TYPE_NAME(FuncEvalFrame)
 #endif // DEBUGGING_SUPPORTED
@@ -1145,6 +1148,69 @@ public:
     // Keep as last entry in class
     DEFINE_VTABLE_GETTER_AND_DTOR(FaultingExceptionFrame)
 };
+
+#ifdef FEATURE_EH_FUNCLETS
+
+class SoftwareExceptionFrame : public Frame
+{
+    TADDR                           m_ReturnAddress;
+    T_CONTEXT                       m_Context;
+    T_KNONVOLATILE_CONTEXT_POINTERS m_ContextPointers;
+
+    VPTR_VTABLE_CLASS(SoftwareExceptionFrame, Frame)
+
+public:
+#ifndef DACCESS_COMPILE
+    SoftwareExceptionFrame() {
+        LIMITED_METHOD_CONTRACT;
+    }
+#endif
+
+    virtual TADDR GetReturnAddressPtr()
+    {
+        LIMITED_METHOD_DAC_CONTRACT;
+        return PTR_HOST_MEMBER_TADDR(SoftwareExceptionFrame, this, m_ReturnAddress);
+    }
+
+    void Init();
+    void InitAndLink(Thread *pThread);
+
+    Interception GetInterception()
+    {
+        LIMITED_METHOD_DAC_CONTRACT;
+        return INTERCEPTION_EXCEPTION;
+    }
+
+    virtual ETransitionType GetTransitionType()
+    {
+        LIMITED_METHOD_DAC_CONTRACT;
+        return TT_InternalCall;
+    }
+
+    unsigned GetFrameAttribs()
+    {
+        LIMITED_METHOD_DAC_CONTRACT;
+        return FRAME_ATTR_EXCEPTION;
+    }
+
+    T_CONTEXT* GetContext()
+    {
+        LIMITED_METHOD_DAC_CONTRACT;
+        return &m_Context;
+    }
+
+    virtual BOOL NeedsUpdateRegDisplay()
+    {
+        return TRUE;
+    }
+
+    virtual void UpdateRegDisplay(const PREGDISPLAY, bool updateFloats = false);
+
+    // Keep as last entry in class
+    DEFINE_VTABLE_GETTER_AND_DTOR(SoftwareExceptionFrame)
+};
+
+#endif // FEATURE_EH_FUNCLETS
 
 //-----------------------------------------------------------------------
 // Frame for debugger function evaluation
@@ -3190,6 +3256,7 @@ public:
     void Poll() { WRAPPER_NO_CONTRACT; m_frame.Poll(); }
     void SetStackPointerPtr(TADDR sp) { WRAPPER_NO_CONTRACT; m_frame.SetStackPointerPtr(sp); }
     void InitAndLink(T_CONTEXT *pContext) { WRAPPER_NO_CONTRACT; m_frame.InitAndLink(pContext); }
+    void InitAndLink(Thread *pThread) { WRAPPER_NO_CONTRACT; m_frame.InitAndLink(pThread); }
     void Init(Thread *pThread, OBJECTREF *pObjRefs, UINT numObjRefs, BOOL maybeInterior)
         { WRAPPER_NO_CONTRACT; m_frame.Init(pThread, pObjRefs, numObjRefs, maybeInterior); }
     ValueClassInfo ** GetValueClassInfoList() { WRAPPER_NO_CONTRACT; return m_frame.GetValueClassInfoList(); }

src/coreclr/vm/jithelpers.cpp
@@ -2675,39 +2675,6 @@ HCIMPLEND
 
 /*************************************************************/
 
-#ifdef FEATURE_EH_FUNCLETS
-void ThrowNew(OBJECTREF oref)
-{
-    if (oref == 0)
-        DispatchManagedException(kNullReferenceException);
-    else
-    if (!IsException(oref->GetMethodTable()))
-    {
-        GCPROTECT_BEGIN(oref);
-
-        WrapNonCompliantException(&oref);
-
-        GCPROTECT_END();
-    }
-    else
-    {   // We know that the object derives from System.Exception
-
-        // If the flag indicating ForeignExceptionRaise has been set,
-        // then do not clear the "_stackTrace" field of the exception object.
-        if (GetThread()->GetExceptionState()->IsRaisingForeignException())
-        {
-            ((EXCEPTIONREF)oref)->SetStackTraceString(NULL);
-        }
-        else
-        {
-            ((EXCEPTIONREF)oref)->ClearStackTracePreservingRemoteStackTrace();
-        }
-    }
-
-    DispatchManagedException(oref);
-}
-#endif // FEATURE_EH_FUNCLETS
-
 HCIMPL1(void, IL_Throw,  Object* obj)
 {
     FCALL_CONTRACT;
@@ -2717,22 +2684,58 @@ HCIMPL1(void, IL_Throw,  Object* obj)
 
     FC_GC_POLL_NOT_NEEDED();    // throws always open up for GC
 
-    HELPER_METHOD_FRAME_BEGIN_ATTRIB_NOPOLL(Frame::FRAME_ATTR_EXCEPTION);    // Set up a frame
-
     OBJECTREF oref = ObjectToOBJECTREF(obj);
+
+#ifdef FEATURE_EH_FUNCLETS
+    if (g_isNewExceptionHandlingEnabled)
+    {
+        Thread *pThread = GetThread();
+
+        FrameWithCookie<SoftwareExceptionFrame> exceptionFrame;
+        *(&exceptionFrame)->GetGSCookiePtr() = GetProcessGSCookie();
+        RtlCaptureContext(exceptionFrame.GetContext());
+        exceptionFrame.InitAndLink(pThread);
+
+        FC_CAN_TRIGGER_GC();
+
+        if (oref == 0)
+            DispatchManagedException(kNullReferenceException);
+        else
+        if (!IsException(oref->GetMethodTable()))
+        {
+            GCPROTECT_BEGIN(oref);
+
+            WrapNonCompliantException(&oref);
+
+            GCPROTECT_END();
+        }
+        else
+        {   // We know that the object derives from System.Exception
+
+            // If the flag indicating ForeignExceptionRaise has been set,
+            // then do not clear the "_stackTrace" field of the exception object.
+            if (pThread->GetExceptionState()->IsRaisingForeignException())
+            {
+                ((EXCEPTIONREF)oref)->SetStackTraceString(NULL);
+            }
+            else
+            {
+                ((EXCEPTIONREF)oref)->ClearStackTracePreservingRemoteStackTrace();
+            }
+        }
+
+        DispatchManagedException(oref, exceptionFrame.GetContext());
+        FC_CAN_TRIGGER_GC_END();
+        UNREACHABLE();
+    }
+#endif // FEATURE_EH_FUNCLETS
+
+    HELPER_METHOD_FRAME_BEGIN_ATTRIB_NOPOLL(Frame::FRAME_ATTR_EXCEPTION);    // Set up a frame
 
 #if defined(_DEBUG) && defined(TARGET_X86)
     __helperframe.InsureInit(NULL);
     g_ExceptionEIP = (LPVOID)__helperframe.GetReturnAddress();
 #endif // defined(_DEBUG) && defined(TARGET_X86)
-
-#ifdef FEATURE_EH_FUNCLETS
-    if (g_isNewExceptionHandlingEnabled)
-    {
-        ThrowNew(oref);
-        UNREACHABLE();
-    }
-#endif
 
     if (oref == 0)
         COMPlusThrow(kNullReferenceException);
@@ -2768,48 +2771,47 @@ HCIMPLEND
 
 /*************************************************************/
 
-#ifdef FEATURE_EH_FUNCLETS
-void RethrowNew()
-{
-    Thread *pThread = GetThread();
-
-    ExInfo *pActiveExInfo = (ExInfo*)pThread->GetExceptionState()->GetCurrentExceptionTracker();
-
-    CONTEXT exceptionContext;
-    RtlCaptureContext(&exceptionContext);
-
-    ExInfo exInfo(pThread, pActiveExInfo->m_ptrs.ExceptionRecord, &exceptionContext, ExKind::None);
-
-    GCPROTECT_BEGIN(exInfo.m_exception);
-    PREPARE_NONVIRTUAL_CALLSITE(METHOD__EH__RH_RETHROW);
-    DECLARE_ARGHOLDER_ARRAY(args, 2);
-
-    args[ARGNUM_0] = PTR_TO_ARGHOLDER(pActiveExInfo);
-    args[ARGNUM_1] = PTR_TO_ARGHOLDER(&exInfo);
-
-    pThread->IncPreventAbort();
-
-    //Ex.RhRethrow(ref ExInfo activeExInfo, ref ExInfo exInfo)
-    CALL_MANAGED_METHOD_NORET(args)
-    GCPROTECT_END();
-}
-#endif // FEATURE_EH_FUNCLETS
-
 HCIMPL0(void, IL_Rethrow)
 {
     FCALL_CONTRACT;
 
     FC_GC_POLL_NOT_NEEDED();    // throws always open up for GC
 
-    HELPER_METHOD_FRAME_BEGIN_ATTRIB_NOPOLL(Frame::FRAME_ATTR_EXCEPTION);    // Set up a frame
-
 #ifdef FEATURE_EH_FUNCLETS
     if (g_isNewExceptionHandlingEnabled)
     {
-        RethrowNew();
+        Thread *pThread = GetThread();
+
+        FrameWithCookie<SoftwareExceptionFrame> exceptionFrame;
+        *(&exceptionFrame)->GetGSCookiePtr() = GetProcessGSCookie();
+        RtlCaptureContext(exceptionFrame.GetContext());
+        exceptionFrame.InitAndLink(pThread);
+
+        ExInfo *pActiveExInfo = (ExInfo*)pThread->GetExceptionState()->GetCurrentExceptionTracker();
+
+        ExInfo exInfo(pThread, pActiveExInfo->m_ptrs.ExceptionRecord, exceptionFrame.GetContext(), ExKind::None);
+
+        FC_CAN_TRIGGER_GC();
+
+        GCPROTECT_BEGIN(exInfo.m_exception);
+        PREPARE_NONVIRTUAL_CALLSITE(METHOD__EH__RH_RETHROW);
+        DECLARE_ARGHOLDER_ARRAY(args, 2);
+
+        args[ARGNUM_0] = PTR_TO_ARGHOLDER(pActiveExInfo);
+        args[ARGNUM_1] = PTR_TO_ARGHOLDER(&exInfo);
+
+        pThread->IncPreventAbort();
+
+        //Ex.RhRethrow(ref ExInfo activeExInfo, ref ExInfo exInfo)
+        CALL_MANAGED_METHOD_NORET(args)
+        GCPROTECT_END();
+
+        FC_CAN_TRIGGER_GC_END();
         UNREACHABLE();
     }
 #endif
+
+    HELPER_METHOD_FRAME_BEGIN_ATTRIB_NOPOLL(Frame::FRAME_ATTR_EXCEPTION);    // Set up a frame
 
     OBJECTREF throwable = GetThread()->GetThrowable();
     if (throwable != NULL)

commit ca778ae603146daa53b0a6abf37bc71c4612261e

Don't crash when walking callstack roots in the debugger (#124402)
Changeset
https://github.com/dotnet/runtime/commit/1fa1745581e26069093a679626e346b675aa534f
introduced a stack walking regression where we will crash trying to
unwind/report registers. The real fix here is in
cgenamd64.cpp/excep.cpp, which will point the context to a local copy
before we try to use it.

I've also added some changes in gcinfodecoder.cpp that aren't strictly
necessary but would have avoided the problem to begin with.

Fixes #124401.

src/coreclr/vm/amd64/cgenamd64.cpp
@@ -218,21 +218,30 @@ void FaultingExceptionFrame::UpdateRegDisplay_Impl(const PREGDISPLAY pRD, bool u
     pRD->SSP = m_SSP;
 #endif
 
-    pRD->pCurrentContextPointers->Rax = &m_ctx.Rax;
-    pRD->pCurrentContextPointers->Rcx = &m_ctx.Rcx;
-    pRD->pCurrentContextPointers->Rdx = &m_ctx.Rdx;
-    pRD->pCurrentContextPointers->Rbx = &m_ctx.Rbx;
-    pRD->pCurrentContextPointers->Rbp = &m_ctx.Rbp;
-    pRD->pCurrentContextPointers->Rsi = &m_ctx.Rsi;
-    pRD->pCurrentContextPointers->Rdi = &m_ctx.Rdi;
-    pRD->pCurrentContextPointers->R8  = &m_ctx.R8;
-    pRD->pCurrentContextPointers->R9  = &m_ctx.R9;
-    pRD->pCurrentContextPointers->R10 = &m_ctx.R10;
-    pRD->pCurrentContextPointers->R11 = &m_ctx.R11;
-    pRD->pCurrentContextPointers->R12 = &m_ctx.R12;
-    pRD->pCurrentContextPointers->R13 = &m_ctx.R13;
-    pRD->pCurrentContextPointers->R14 = &m_ctx.R14;
-    pRD->pCurrentContextPointers->R15 = &m_ctx.R15;
+#ifdef DACCESS_COMPILE
+    // &m_ctx.Xxx resolves through the DAC cache and the entry can be evicted
+    // before context pointers are consumed. Point at the local copy in
+    // pCurrentContext instead (values were already copied above).
+    CONTEXT *pContext = pRD->pCurrentContext;
+#else
+    CONTEXT *pContext = &m_ctx;
+#endif
+
+    pRD->pCurrentContextPointers->Rax = &pContext->Rax;
+    pRD->pCurrentContextPointers->Rcx = &pContext->Rcx;
+    pRD->pCurrentContextPointers->Rdx = &pContext->Rdx;
+    pRD->pCurrentContextPointers->Rbx = &pContext->Rbx;
+    pRD->pCurrentContextPointers->Rbp = &pContext->Rbp;
+    pRD->pCurrentContextPointers->Rsi = &pContext->Rsi;
+    pRD->pCurrentContextPointers->Rdi = &pContext->Rdi;
+    pRD->pCurrentContextPointers->R8  = &pContext->R8;
+    pRD->pCurrentContextPointers->R9  = &pContext->R9;
+    pRD->pCurrentContextPointers->R10 = &pContext->R10;
+    pRD->pCurrentContextPointers->R11 = &pContext->R11;
+    pRD->pCurrentContextPointers->R12 = &pContext->R12;
+    pRD->pCurrentContextPointers->R13 = &pContext->R13;
+    pRD->pCurrentContextPointers->R14 = &pContext->R14;
+    pRD->pCurrentContextPointers->R15 = &pContext->R15;
 
     pRD->IsCallerContextValid = FALSE;
     pRD->IsCallerSPValid      = FALSE;        // Don't add usage of this field.  This is only temporary.

src/coreclr/vm/arm/stubs.cpp
@@ -1287,14 +1287,23 @@ void FaultingExceptionFrame::UpdateRegDisplay_Impl(const PREGDISPLAY pRD, bool u
 
     // Update the integer registers in KNONVOLATILE_CONTEXT_POINTERS from
     // the exception context we have.
-    pRD->pCurrentContextPointers->R4 = (PDWORD)&m_ctx.R4;
-    pRD->pCurrentContextPointers->R5 = (PDWORD)&m_ctx.R5;
-    pRD->pCurrentContextPointers->R6 = (PDWORD)&m_ctx.R6;
-    pRD->pCurrentContextPointers->R7 = (PDWORD)&m_ctx.R7;
-    pRD->pCurrentContextPointers->R8 = (PDWORD)&m_ctx.R8;
-    pRD->pCurrentContextPointers->R9 = (PDWORD)&m_ctx.R9;
-    pRD->pCurrentContextPointers->R10 = (PDWORD)&m_ctx.R10;
-    pRD->pCurrentContextPointers->R11 = (PDWORD)&m_ctx.R11;
+#ifdef DACCESS_COMPILE
+    // &m_ctx.Xxx resolves through the DAC cache and the entry can be evicted
+    // before context pointers are consumed. Point at the local copy in
+    // pCurrentContext instead (values were already copied above).
+    T_CONTEXT *pContext = pRD->pCurrentContext;
+#else
+    T_CONTEXT *pContext = &m_ctx;
+#endif
+
+    pRD->pCurrentContextPointers->R4 = (PDWORD)&pContext->R4;
+    pRD->pCurrentContextPointers->R5 = (PDWORD)&pContext->R5;
+    pRD->pCurrentContextPointers->R6 = (PDWORD)&pContext->R6;
+    pRD->pCurrentContextPointers->R7 = (PDWORD)&pContext->R7;
+    pRD->pCurrentContextPointers->R8 = (PDWORD)&pContext->R8;
+    pRD->pCurrentContextPointers->R9 = (PDWORD)&pContext->R9;
+    pRD->pCurrentContextPointers->R10 = (PDWORD)&pContext->R10;
+    pRD->pCurrentContextPointers->R11 = (PDWORD)&pContext->R11;
     pRD->pCurrentContextPointers->Lr = NULL;
 
     pRD->IsCallerContextValid = FALSE;

src/coreclr/vm/arm64/stubs.cpp
@@ -294,18 +294,27 @@ void FaultingExceptionFrame::UpdateRegDisplay_Impl(const PREGDISPLAY pRD, bool u
 
     // Update the integer registers in KNONVOLATILE_CONTEXT_POINTERS from
     // the exception context we have.
-    pRD->pCurrentContextPointers->X19 = (PDWORD64)&m_ctx.X19;
-    pRD->pCurrentContextPointers->X20 = (PDWORD64)&m_ctx.X20;
-    pRD->pCurrentContextPointers->X21 = (PDWORD64)&m_ctx.X21;
-    pRD->pCurrentContextPointers->X22 = (PDWORD64)&m_ctx.X22;
-    pRD->pCurrentContextPointers->X23 = (PDWORD64)&m_ctx.X23;
-    pRD->pCurrentContextPointers->X24 = (PDWORD64)&m_ctx.X24;
-    pRD->pCurrentContextPointers->X25 = (PDWORD64)&m_ctx.X25;
-    pRD->pCurrentContextPointers->X26 = (PDWORD64)&m_ctx.X26;
-    pRD->pCurrentContextPointers->X27 = (PDWORD64)&m_ctx.X27;
-    pRD->pCurrentContextPointers->X28 = (PDWORD64)&m_ctx.X28;
-    pRD->pCurrentContextPointers->Fp = (PDWORD64)&m_ctx.Fp;
-    pRD->pCurrentContextPointers->Lr = (PDWORD64)&m_ctx.Lr;
+#ifdef DACCESS_COMPILE
+    // &m_ctx.Xxx resolves through the DAC cache and the entry can be evicted
+    // before context pointers are consumed. Point at the local copy in
+    // pCurrentContext instead (values were already copied above).
+    T_CONTEXT *pContext = pRD->pCurrentContext;
+#else
+    T_CONTEXT *pContext = &m_ctx;
+#endif
+
+    pRD->pCurrentContextPointers->X19 = (PDWORD64)&pContext->X19;
+    pRD->pCurrentContextPointers->X20 = (PDWORD64)&pContext->X20;
+    pRD->pCurrentContextPointers->X21 = (PDWORD64)&pContext->X21;
+    pRD->pCurrentContextPointers->X22 = (PDWORD64)&pContext->X22;
+    pRD->pCurrentContextPointers->X23 = (PDWORD64)&pContext->X23;
+    pRD->pCurrentContextPointers->X24 = (PDWORD64)&pContext->X24;
+    pRD->pCurrentContextPointers->X25 = (PDWORD64)&pContext->X25;
+    pRD->pCurrentContextPointers->X26 = (PDWORD64)&pContext->X26;
+    pRD->pCurrentContextPointers->X27 = (PDWORD64)&pContext->X27;
+    pRD->pCurrentContextPointers->X28 = (PDWORD64)&pContext->X28;
+    pRD->pCurrentContextPointers->Fp = (PDWORD64)&pContext->Fp;
+    pRD->pCurrentContextPointers->Lr = (PDWORD64)&pContext->Lr;
 
     ClearRegDisplayArgumentAndScratchRegisters(pRD);
 

src/coreclr/vm/excep.cpp
@@ -10742,9 +10742,9 @@ void SoftwareExceptionFrame::UpdateRegDisplay_Impl(const PREGDISPLAY pRD, bool u
     ENUM_CALLEE_SAVED_REGISTERS();
 #undef CALLEE_SAVED_REGISTER
 
-#if defined(DACCESS_COMPILE) && defined(TARGET_X86)
-// X86 unwinding always works in terms of context pointers, so they need to be in the correct address space when debugging
-// This may work for other architectures as well, but that isn't tested.
+#if defined(DACCESS_COMPILE)
+// Context pointers in m_ContextPointers reference the target process address space and cannot be used directly.
+// Point them at the local context copy instead.
 #define CALLEE_SAVED_REGISTER(regname) pRD->pCurrentContextPointers->regname = &pRD->pCurrentContext->regname;
 #else
 #define CALLEE_SAVED_REGISTER(regname) pRD->pCurrentContextPointers->regname = m_ContextPointers.regname;

src/coreclr/vm/i386/cgenx86.cpp
@@ -220,11 +220,20 @@ void FaultingExceptionFrame::UpdateRegDisplay_Impl(const PREGDISPLAY pRD, bool u
     pRD->SP = m_ctx.Esp;
     pRD->ControlPC = m_ctx.Eip;
 
-#define ARGUMENT_AND_SCRATCH_REGISTER(regname) pRD->pCurrentContextPointers->regname = &m_ctx.regname;
+#ifdef DACCESS_COMPILE
+    // &m_ctx.Xxx resolves through the DAC cache and the entry can be evicted
+    // before context pointers are consumed. Point at the local copy in
+    // pCurrentContext instead (values were already copied above).
+    CONTEXT *pContext = pRD->pCurrentContext;
+#else
+    CONTEXT *pContext = &m_ctx;
+#endif
+
+#define ARGUMENT_AND_SCRATCH_REGISTER(regname) pRD->pCurrentContextPointers->regname = &pContext->regname;
     ENUM_ARGUMENT_AND_SCRATCH_REGISTERS();
 #undef ARGUMENT_AND_SCRATCH_REGISTER
 
-#define CALLEE_SAVED_REGISTER(regname) pRD->pCurrentContextPointers->regname = &m_ctx.regname;
+#define CALLEE_SAVED_REGISTER(regname) pRD->pCurrentContextPointers->regname = &pContext->regname;
     ENUM_CALLEE_SAVED_REGISTERS();
 #undef CALLEE_SAVED_REGISTER
 

src/coreclr/vm/loongarch64/stubs.cpp
@@ -306,17 +306,26 @@ void FaultingExceptionFrame::UpdateRegDisplay_Impl(const PREGDISPLAY pRD, bool u
 
     // Update the integer registers in KNONVOLATILE_CONTEXT_POINTERS from
     // the exception context we have.
-    pRD->pCurrentContextPointers->S0 = (PDWORD64)&m_ctx.S0;
-    pRD->pCurrentContextPointers->S1 = (PDWORD64)&m_ctx.S1;
-    pRD->pCurrentContextPointers->S2 = (PDWORD64)&m_ctx.S2;
-    pRD->pCurrentContextPointers->S3 = (PDWORD64)&m_ctx.S3;
-    pRD->pCurrentContextPointers->S4 = (PDWORD64)&m_ctx.S4;
-    pRD->pCurrentContextPointers->S5 = (PDWORD64)&m_ctx.S5;
-    pRD->pCurrentContextPointers->S6 = (PDWORD64)&m_ctx.S6;
-    pRD->pCurrentContextPointers->S7 = (PDWORD64)&m_ctx.S7;
-    pRD->pCurrentContextPointers->S8 = (PDWORD64)&m_ctx.S8;
-    pRD->pCurrentContextPointers->Fp = (PDWORD64)&m_ctx.Fp;
-    pRD->pCurrentContextPointers->Ra = (PDWORD64)&m_ctx.Ra;
+#ifdef DACCESS_COMPILE
+    // &m_ctx.Xxx resolves through the DAC cache and the entry can be evicted
+    // before context pointers are consumed. Point at the local copy in
+    // pCurrentContext instead (values were already copied above).
+    T_CONTEXT *pContext = pRD->pCurrentContext;
+#else
+    T_CONTEXT *pContext = &m_ctx;
+#endif
+
+    pRD->pCurrentContextPointers->S0 = (PDWORD64)&pContext->S0;
+    pRD->pCurrentContextPointers->S1 = (PDWORD64)&pContext->S1;
+    pRD->pCurrentContextPointers->S2 = (PDWORD64)&pContext->S2;
+    pRD->pCurrentContextPointers->S3 = (PDWORD64)&pContext->S3;
+    pRD->pCurrentContextPointers->S4 = (PDWORD64)&pContext->S4;
+    pRD->pCurrentContextPointers->S5 = (PDWORD64)&pContext->S5;
+    pRD->pCurrentContextPointers->S6 = (PDWORD64)&pContext->S6;
+    pRD->pCurrentContextPointers->S7 = (PDWORD64)&pContext->S7;
+    pRD->pCurrentContextPointers->S8 = (PDWORD64)&pContext->S8;
+    pRD->pCurrentContextPointers->Fp = (PDWORD64)&pContext->Fp;
+    pRD->pCurrentContextPointers->Ra = (PDWORD64)&pContext->Ra;
 
     ClearRegDisplayArgumentAndScratchRegisters(pRD);
 

src/coreclr/vm/riscv64/stubs.cpp
@@ -260,21 +260,30 @@ void FaultingExceptionFrame::UpdateRegDisplay_Impl(const PREGDISPLAY pRD, bool u
 
     // Update the integer registers in KNONVOLATILE_CONTEXT_POINTERS from
     // the exception context we have.
-    pRD->pCurrentContextPointers->S1 = (PDWORD64)&m_ctx.S1;
-    pRD->pCurrentContextPointers->S2 = (PDWORD64)&m_ctx.S2;
-    pRD->pCurrentContextPointers->S3 = (PDWORD64)&m_ctx.S3;
-    pRD->pCurrentContextPointers->S4 = (PDWORD64)&m_ctx.S4;
-    pRD->pCurrentContextPointers->S5 = (PDWORD64)&m_ctx.S5;
-    pRD->pCurrentContextPointers->S6 = (PDWORD64)&m_ctx.S6;
-    pRD->pCurrentContextPointers->S7 = (PDWORD64)&m_ctx.S7;
-    pRD->pCurrentContextPointers->S8 = (PDWORD64)&m_ctx.S8;
-    pRD->pCurrentContextPointers->S9 = (PDWORD64)&m_ctx.S9;
-    pRD->pCurrentContextPointers->S10 = (PDWORD64)&m_ctx.S10;
-    pRD->pCurrentContextPointers->S11 = (PDWORD64)&m_ctx.S11;
-    pRD->pCurrentContextPointers->Fp = (PDWORD64)&m_ctx.Fp;
-    pRD->pCurrentContextPointers->Gp = (PDWORD64)&m_ctx.Gp;
-    pRD->pCurrentContextPointers->Tp = (PDWORD64)&m_ctx.Tp;
-    pRD->pCurrentContextPointers->Ra = (PDWORD64)&m_ctx.Ra;
+#ifdef DACCESS_COMPILE
+    // &m_ctx.Xxx resolves through the DAC cache and the entry can be evicted
+    // before context pointers are consumed. Point at the local copy in
+    // pCurrentContext instead (values were already copied above).
+    T_CONTEXT *pContext = pRD->pCurrentContext;
+#else
+    T_CONTEXT *pContext = &m_ctx;
+#endif
+
+    pRD->pCurrentContextPointers->S1 = (PDWORD64)&pContext->S1;
+    pRD->pCurrentContextPointers->S2 = (PDWORD64)&pContext->S2;
+    pRD->pCurrentContextPointers->S3 = (PDWORD64)&pContext->S3;
+    pRD->pCurrentContextPointers->S4 = (PDWORD64)&pContext->S4;
+    pRD->pCurrentContextPointers->S5 = (PDWORD64)&pContext->S5;
+    pRD->pCurrentContextPointers->S6 = (PDWORD64)&pContext->S6;
+    pRD->pCurrentContextPointers->S7 = (PDWORD64)&pContext->S7;
+    pRD->pCurrentContextPointers->S8 = (PDWORD64)&pContext->S8;
+    pRD->pCurrentContextPointers->S9 = (PDWORD64)&pContext->S9;
+    pRD->pCurrentContextPointers->S10 = (PDWORD64)&pContext->S10;
+    pRD->pCurrentContextPointers->S11 = (PDWORD64)&pContext->S11;
+    pRD->pCurrentContextPointers->Fp = (PDWORD64)&pContext->Fp;
+    pRD->pCurrentContextPointers->Gp = (PDWORD64)&pContext->Gp;
+    pRD->pCurrentContextPointers->Tp = (PDWORD64)&pContext->Tp;
+    pRD->pCurrentContextPointers->Ra = (PDWORD64)&pContext->Ra;
 
     ClearRegDisplayArgumentAndScratchRegisters(pRD);
 