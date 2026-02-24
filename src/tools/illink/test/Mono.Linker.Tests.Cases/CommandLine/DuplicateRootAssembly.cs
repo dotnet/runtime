181ca3f3265a1f7e93ded696e76073af8b316f7d
michaelv <michaelv@unity3d.com>
2026-02-24 13:23:24 -0500

    Ignore `DuplicateRootAssembly` during debug builds.  It will trigger a Debug.Assert leading to a failure.

--- src/tools/illink/test/Mono.Linker.Tests.Cases/CommandLine/DuplicateRootAssembly.cs
+++ src/tools/illink/test/Mono.Linker.Tests.Cases/CommandLine/DuplicateRootAssembly.cs
@@ -1,10 +1,14 @@
 ﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Mono.Linker.Tests.Cases.Expectations.Assertions;
 using Mono.Linker.Tests.Cases.Expectations.Metadata;
 
 namespace Mono.Linker.Tests.Cases.CommandLine;
 
+#if DEBUG
+[IgnoreTestCase("This test will trip a Debug.Assert which means letting the test run would lead to a failure")]
+#endif
 [SetupLinkerArgument("-a", "test.exe", "entrypoint")]
 [SetupLinkerArgument("-a", "../input/test.exe", "entrypoint")]
 public class DuplicateRootAssembly
