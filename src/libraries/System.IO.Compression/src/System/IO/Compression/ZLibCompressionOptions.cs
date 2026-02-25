a1658e72c459c9619bd82135c3cd75efcb9e59aa
iremyux <iremyuksel@microsoft.com>
2026-02-25 13:24:33 +0100

    Use constants for Quality as well

--- src/libraries/System.IO.Compression/src/System/IO/Compression/ZLibCompressionOptions.cs
+++ src/libraries/System.IO.Compression/src/System/IO/Compression/ZLibCompressionOptions.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 namespace System.IO.Compression
@@ -33,9 +33,12 @@ namespace System.IO.Compression
             get => _compressionLevel;
             set
             {
-                ArgumentOutOfRangeException.ThrowIfLessThan(value, -1);
-                ArgumentOutOfRangeException.ThrowIfGreaterThan(value, 9);
-
+                if (value != -1)
+                {
+                    ArgumentOutOfRangeException.ThrowIfLessThan(value, MinQuality);
+                    ArgumentOutOfRangeException.ThrowIfGreaterThan(value, MaxQuality);
+                }
+                
                 _compressionLevel = value;
             }
         }
