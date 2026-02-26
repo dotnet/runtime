4d369f235ff45eeff84ead8bd98575f000c98956
Andy Ayers <andya@microsoft.com>
2026-02-26 14:39:57 -0800

    Revert "Increase number of assertions (GlobalAP) + VN cache (#124132)"
    This reverts commit 92741bebcae5b82cd537979eef962674627e7f89.

--- src/coreclr/jit/compiler.h
+++ src/coreclr/jit/compiler.h
@@ -8432,8 +8432,7 @@ protected:
     JitExpandArray<ASSERT_TP>* optAssertionDep = nullptr; // table that holds dependent assertions (assertions
                                                           // using the value of a local var) for each local var
     AssertionDsc*  optAssertionTabPrivate;                // table that holds info about assertions
-    VNSet*         optAssertionVNsMap = nullptr;
-    AssertionIndex optAssertionCount  = 0; // total number of assertions in the assertion table
+    AssertionIndex optAssertionCount = 0;                 // total number of assertions in the assertion table
     AssertionIndex optMaxAssertionCount;
     bool           optCrossBlockLocalAssertionProp;
     unsigned       optAssertionOverflow;
@@ -8562,7 +8561,6 @@ public:
 
     bool optCreateJumpTableImpliedAssertions(BasicBlock* switchBb);
 
-    bool optAssertionHasAssertionsForVN(ValueNum vn, bool addIfNotFound = false);
 #ifdef DEBUG
     void optPrintAssertion(const AssertionDsc& newAssertion, AssertionIndex assertionIndex = 0);
     void optPrintAssertionIndex(AssertionIndex index);
