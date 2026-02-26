346a77b7dfd56e284880b7efc9c0b1bcc61ff17f
Michal Žůrek <188900745+mrek-msft@users.noreply.github.com>
2026-02-26 14:41:30 +0100

    Reregister in case of Free/Attach/AllocConsole directly used.

--- src/libraries/System.Private.CoreLib/src/System/Runtime/InteropServices/PosixSignalRegistration.Windows.cs
+++ src/libraries/System.Private.CoreLib/src/System/Runtime/InteropServices/PosixSignalRegistration.Windows.cs
@@ -9,7 +9,7 @@ namespace System.Runtime.InteropServices
     public sealed partial class PosixSignalRegistration
     {
         private static readonly Dictionary<int, List<Token>> s_registrations = new();
-        private static bool s_isCtrlHandlerRegisteredOnce;
+        private static bool s_isCtrlHandlerRegistering;
 
         private static unsafe PosixSignalRegistration Register(PosixSignal signal, Action<PosixSignalContext> handler)
         {
@@ -24,15 +24,49 @@ namespace System.Runtime.InteropServices
 
             var token = new Token(signal, signo, handler);
             var registration = new PosixSignalRegistration(token);
+
             bool registerCtrlHandler = false;
+            lock (s_registrations)
+            {
+                if (s_registrations.Count == 0 && !s_isCtrlHandlerRegistering)
+                {
+                    registerCtrlHandler = true;
+                    s_isCtrlHandlerRegistering = true;
+                }
+            }
+
+            if (registerCtrlHandler)
+            {
+                // User may reset registrations externally by direct calls of Free/Attach/AllocConsole.
+                // We do not know if it is currently registered or not. To prevent duplicate
+                // registration, we try unregister existing one first.
+                if (!Interop.Kernel32.SetConsoleCtrlHandler(&HandlerRoutine, Add: false))
+                {
+                    // Returns ERROR_INVALID_PARAMETER if it was not registered. Throw for everything else.
+                    int error = Marshal.GetLastPInvokeError();
+                    if (error != Interop.Errors.ERROR_INVALID_PARAMETER)
+                    {
+                        lock (s_registrations)
+                        {
+                            s_isCtrlHandlerRegistering = false;
+                        }
+                        throw Win32Marshal.GetExceptionForWin32Error(error);
+                    }
+                }
+
+                if (!Interop.Kernel32.SetConsoleCtrlHandler(&HandlerRoutine, Add: true))
+                {
+                    lock (s_registrations)
+                    {
+                        s_isCtrlHandlerRegistering = false;
+                    }
+                    throw Win32Marshal.GetExceptionForLastWin32Error();
+                }
+            }
 
             lock (s_registrations)
             {
-                if (s_registrations.Count == 0 && !s_isCtrlHandlerRegisteredOnce)
-                {
-                    s_isCtrlHandlerRegisteredOnce = true;
-                    registerCtrlHandler = true;
-                }
+                s_isCtrlHandlerRegistering = false;
 
                 if (!s_registrations.TryGetValue(signo, out List<Token>? tokens))
                 {
@@ -40,12 +74,6 @@ namespace System.Runtime.InteropServices
                 }
 
                 tokens.Add(token);
-            }
-
-            if (registerCtrlHandler &&
-                !Interop.Kernel32.SetConsoleCtrlHandler(&HandlerRoutine, Add: true))
-            {
-                throw Win32Marshal.GetExceptionForLastWin32Error();
             }
 
             return registration;
