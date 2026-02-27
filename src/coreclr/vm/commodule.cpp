24e8cfbdbb9280cfa4516597dcdbf4cea1e4c77e
Jan Kotas <jkotas@microsoft.com>
2026-02-27 07:45:42 -0800

    Update src/coreclr/vm/commodule.cpp

--- src/coreclr/vm/commodule.cpp
+++ src/coreclr/vm/commodule.cpp
@@ -764,6 +764,7 @@ extern "C" void QCALLTYPE RuntimeModule_GetTypes(QCall::ModuleHandle pModule, QC
     {
         // We should have filled the array exactly.
         _ASSERTE(curPos == dwNumTypeDefs);
+    }
 
         // Assign the return value to the CLR array
         retTypes.Set(gc.refArrClasses);
