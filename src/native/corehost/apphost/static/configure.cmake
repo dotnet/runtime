9d0de28ab7185eb8972de1fbbf13206d306fa4c8
Adeel Mujahid <3840695+am11@users.noreply.github.com>
2026-03-03 20:48:40 +0200

    Re-enable HeimdalGssApi header check in CMake

--- src/native/corehost/apphost/static/configure.cmake
+++ src/native/corehost/apphost/static/configure.cmake
@@ -5,14 +5,15 @@ check_include_files(
     HAVE_GSSFW_HEADERS)
 
 option(HeimdalGssApi "use heimdal implementation of GssApi" OFF)
-if (HeimdalGssApi)
-   check_include_files(
-       gssapi/gssapi.h
-       HAVE_HEIMDAL_HEADERS)
-endif()
 
 if (CLR_CMAKE_TARGET_OPENBSD)
     set(HeimdalGssApi ON)
     set(CMAKE_REQUIRED_INCLUDES ${CROSS_ROOTFS}/heimdal/include)
     set(CMAKE_PREFIX_PATH ${CROSS_ROOTFS}/heimdal/lib)
+endif()
+
+if (HeimdalGssApi)
+   check_include_files(
+       gssapi/gssapi.h
+       HAVE_HEIMDAL_HEADERS)
 endif()
