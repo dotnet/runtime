83024cc23997c6e7012f05690eeacd10cf8dc007
Adam Sitnik <adam.sitnik@gmail.com>
2026-03-04 20:07:13 +0100

    Apply suggestion from @adamsitnik

--- src/libraries/System.IO.Ports/src/System/IO/Ports/SerialStream.Windows.cs
+++ src/libraries/System.IO.Ports/src/System/IO/Ports/SerialStream.Windows.cs
@@ -4,7 +4,6 @@
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
-using System.IO;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
