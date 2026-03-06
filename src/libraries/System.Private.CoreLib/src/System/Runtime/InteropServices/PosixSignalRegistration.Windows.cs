0bcf834da0069c75f4a23b3c1d1e050682c8ae9c
Michal Žůrek <188900745+mrek-msft@users.noreply.github.com>
2026-03-06 12:57:30 +0100

    Rework locking mechanism to handle possible races

--- src/libraries/System.Private.CoreLib/src/System/Runtime/InteropServices/PosixSignalRegistration.Windows.cs
+++ src/libraries/System.Private.CoreLib/src/System/Runtime/InteropServices/PosixSignalRegistration.Windows.cs
@@ -9,7 +9,16 @@ namespace System.Runtime.InteropServices
     public sealed partial class PosixSignalRegistration
     {
         private static readonly Dictionary<int, List<Token>> s_registrations = new();
-        private static bool s_isCtrlHandlerRegistering;
+
+        /// <summary>
+        /// Lock object for critical section ensurring that only one of register and unregister happen at time.
+        /// </summary>
+        private static readonly object s_oneOfRegisterUnregisterLock = new();
+
+        /// <summary>
+        /// Lock object for critical section ensuring that only one of unregister and callback execution happen at time.
+        /// </summary>
+        private static readonly object s_oneOfUnregisterExecuteLock = new();
 
         private static unsafe PosixSignalRegistration Register(PosixSignal signal, Action<PosixSignalContext> handler)
         {
@@ -25,55 +34,47 @@ namespace System.Runtime.InteropServices
             var token = new Token(signal, signo, handler);
             var registration = new PosixSignalRegistration(token);
 
-            bool registerCtrlHandler = false;
-            lock (s_registrations)
+            lock (s_oneOfRegisterUnregisterLock)
             {
-                if (s_registrations.Count == 0 && !s_isCtrlHandlerRegistering)
+                bool registerCtrlHandler = false;
+                lock (s_registrations)
                 {
-                    registerCtrlHandler = true;
-                    s_isCtrlHandlerRegistering = true;
-                }
-            }
-
-            if (registerCtrlHandler)
-            {
-                // User may reset registrations externally by direct calls of Free/Attach/AllocConsole.
-                // We do not know if it is currently registered or not. To prevent duplicate
-                // registration, we try unregister existing one first.
-                if (!Interop.Kernel32.SetConsoleCtrlHandler(&HandlerRoutine, Add: false))
-                {
-                    // Returns ERROR_INVALID_PARAMETER if it was not registered. Throw for everything else.
-                    int error = Marshal.GetLastPInvokeError();
-                    if (error != Interop.Errors.ERROR_INVALID_PARAMETER)
+                    if (s_registrations.Count == 0)
                     {
-                        lock (s_registrations)
+                        registerCtrlHandler = true;
+                    }
+                }
+
+                if (registerCtrlHandler)
+                {
+                    // User may reset registrations externally by direct calls of Free/Attach/AllocConsole.
+                    // We do not know if it is currently registered or not. To prevent duplicate
+                    // registration, we try unregister existing one first.
+                    if (!Interop.Kernel32.SetConsoleCtrlHandler(&HandlerRoutine, Add: false))
+                    {
+                        // Returns ERROR_INVALID_PARAMETER if it was not registered. Throw for everything else.
+                        int error = Marshal.GetLastPInvokeError();
+                        if (error != Interop.Errors.ERROR_INVALID_PARAMETER)
                         {
-                            s_isCtrlHandlerRegistering = false;
+                            throw Win32Marshal.GetExceptionForWin32Error(error);
                         }
-                        throw Win32Marshal.GetExceptionForWin32Error(error);
                     }
-                }
 
-                if (!Interop.Kernel32.SetConsoleCtrlHandler(&HandlerRoutine, Add: true))
-                {
-                    lock (s_registrations)
+                    if (!Interop.Kernel32.SetConsoleCtrlHandler(&HandlerRoutine, Add: true))
                     {
-                        s_isCtrlHandlerRegistering = false;
+                        throw Win32Marshal.GetExceptionForLastWin32Error();
                     }
-                    throw Win32Marshal.GetExceptionForLastWin32Error();
                 }
-            }
 
-            lock (s_registrations)
-            {
-                s_isCtrlHandlerRegistering = false;
-
-                if (!s_registrations.TryGetValue(signo, out List<Token>? tokens))
+                lock (s_registrations)
                 {
-                    s_registrations[signo] = tokens = new List<Token>();
-                }
+                    if (!s_registrations.TryGetValue(signo, out List<Token>? tokens))
+                    {
+                        s_registrations[signo] = tokens = new List<Token>();
+                    }
 
-                tokens.Add(token);
+                    tokens.Add(token);
+                }
             }
 
             return registration;
@@ -81,18 +82,24 @@ namespace System.Runtime.InteropServices
 
         private unsafe void Unregister()
         {
-            lock (s_registrations)
+            lock (s_oneOfUnregisterExecuteLock)
             {
-                if (_token is Token token)
+                lock (s_oneOfRegisterUnregisterLock)
                 {
-                    _token = null;
-
-                    if (s_registrations.TryGetValue(token.SigNo, out List<Token>? tokens))
+                    lock (s_registrations)
                     {
-                        tokens.Remove(token);
-                        if (tokens.Count == 0)
+                        if (_token is Token token)
                         {
-                            s_registrations.Remove(token.SigNo);
+                            _token = null;
+
+                            if (s_registrations.TryGetValue(token.SigNo, out List<Token>? tokens))
+                            {
+                                tokens.Remove(token);
+                                if (tokens.Count == 0)
+                                {
+                                    s_registrations.Remove(token.SigNo);
+                                }
+                            }
                         }
                     }
                 }
@@ -103,32 +110,34 @@ namespace System.Runtime.InteropServices
         private static Interop.BOOL HandlerRoutine(int dwCtrlType)
         {
             Token[]? tokens = null;
-
-            lock (s_registrations)
+            lock (s_oneOfUnregisterExecuteLock)
             {
-                if (s_registrations.TryGetValue(dwCtrlType, out List<Token>? registrations))
+                lock (s_registrations)
                 {
-                    tokens = new Token[registrations.Count];
-                    registrations.CopyTo(tokens);
+                    if (s_registrations.TryGetValue(dwCtrlType, out List<Token>? registrations))
+                    {
+                        tokens = new Token[registrations.Count];
+                        registrations.CopyTo(tokens);
+                    }
                 }
+
+                if (tokens is null)
+                {
+                    return Interop.BOOL.FALSE;
+                }
+
+                var context = new PosixSignalContext(0);
+
+                // Iterate through the tokens in reverse order to match the order of registration.
+                for (int i = tokens.Length - 1; i >= 0; i--)
+                {
+                    Token token = tokens[i];
+                    context.Signal = token.Signal;
+                    token.Handler(context);
+                }
+
+                return context.Cancel ? Interop.BOOL.TRUE : Interop.BOOL.FALSE;
             }
-
-            if (tokens is null)
-            {
-                return Interop.BOOL.FALSE;
-            }
-
-            var context = new PosixSignalContext(0);
-
-            // Iterate through the tokens in reverse order to match the order of registration.
-            for (int i = tokens.Length - 1; i >= 0; i--)
-            {
-                Token token = tokens[i];
-                context.Signal = token.Signal;
-                token.Handler(context);
-            }
-
-            return context.Cancel ? Interop.BOOL.TRUE : Interop.BOOL.FALSE;
         }
     }
 }
