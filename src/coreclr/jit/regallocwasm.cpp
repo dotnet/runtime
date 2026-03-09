bc68926f746e6cf19b28fe75c750cad866ce99bf
Katelyn Gadd <kg@luminance.org>
2026-03-09 11:27:51 -0700

    Suggested fix from SingleAccretion

--- src/coreclr/jit/regallocwasm.cpp
+++ src/coreclr/jit/regallocwasm.cpp
@@ -523,9 +523,18 @@ void WasmRegAlloc::RewriteLocalStackStore(GenTreeLclVarCommon* lclNode)
     // into the store's address mode. We can utilize a contained LEA, but that will require some liveness work.
 
     var_types storeType = lclNode->TypeGet();
-    // Check that both the storeType AND the value being stored are structs. If there is a mismatch, we will
-    //  crash later on while trying to create a STORE_BLK because the value has no layout.
-    bool         isStruct = (storeType == TYP_STRUCT) && value->TypeIs(TYP_STRUCT);
+    // We can end up with a block copy operation storing a non-STRUCT into a STRUCT due to type erasure.
+    if ((storeType == TYP_STRUCT) && lclNode->OperIsCopyBlkOp())
+    {
+        LclVarDsc* varDsc = m_compiler->lvaGetDesc(lclNode->GetLclNum());
+        var_types lclRegType = varDsc->GetRegisterType(lclNode);
+        if (lclRegType != TYP_UNDEF)
+        {
+            storeType = lclRegType;
+        }
+    }
+
+    bool         isStruct = storeType == TYP_STRUCT;
     uint16_t     offset   = lclNode->GetLclOffs();
     ClassLayout* layout   = isStruct ? lclNode->GetLayout(m_compiler) : nullptr;
     lclNode->SetOper(GT_LCL_ADDR);
@@ -540,8 +549,7 @@ void WasmRegAlloc::RewriteLocalStackStore(GenTreeLclVarCommon* lclNode)
     }
     else
     {
-        store = m_compiler->gtNewStoreIndNode(storeType == TYP_STRUCT ? value->TypeGet() : storeType, lclNode, value,
-                                              indFlags);
+        store = m_compiler->gtNewStoreIndNode(storeType, lclNode, value, indFlags);
     }
     CurrentRange().InsertAfter(lclNode, store);
     CurrentRange().Remove(lclNode);
