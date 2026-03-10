9d455f89cef5773eccda4ebd5dbb55ed8645c65c
Marc Zwart <marcdevin@hotmail.com>
2026-03-10 23:16:58 +0100

    unused import

--- src/mono/wasm/Wasm.Build.Tests/LibraryInitializerTests.cs
+++ src/mono/wasm/Wasm.Build.Tests/LibraryInitializerTests.cs
@@ -9,7 +9,6 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using Microsoft.NET.Sdk.WebAssembly;
 using Xunit.Abstractions;
 using Xunit;
 
