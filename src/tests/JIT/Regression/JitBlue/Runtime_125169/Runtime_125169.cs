9696b407810553a21e95bc5ceae46ebfd04624f6
Jan Vorlicek <janvorli@microsoft.com>
2026-03-10 23:20:48 +0100

    Fix missing call to write barrier in static constructors of ref structs
    The JIT was incorrectly handling initialization of static fields of ref
    structs. It was not adding call to JIT_ByRefWriteBarrier when setting
    these fields even though these statics live in GC heap.
    
    This change fixes it and adds a regression test that causes fatal error
    when run with DOTNET_HeapVerify=1.
    
    Close #125169

---
+++ src/tests/JIT/Regression/JitBlue/Runtime_125169/Runtime_125169.cs
@@ -0,0 +1,38 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Xunit;
+
+public readonly struct ByteArrayWrapper
+{
+    public readonly byte[] _innerArray;
+
+    public ByteArrayWrapper(scoped in byte[] innerArray)
+    {
+        _innerArray = innerArray;
+    }
+}
+
+public readonly ref struct RefWrapper
+{
+   public static readonly ByteArrayWrapper _data = new ByteArrayWrapper(new byte[] {1, 2, 3, 4, 0});
+}
+
+class Program
+{
+    // Regression test for bug in JIT where ref struct static fields of reference type initialized in
+    // its static constructor were missing calls to JIT_ByRefWriteBarrier for each such static field.
+    [Fact]
+    public static void TestEntryPoint()
+    {
+        // Generate enough garbage
+        for (int i = 0; i < 100; i++)
+        {
+            var arr = new byte[100000];
+        }
+
+        ReadOnlySpan<byte> testSpan = new(RefWrapper._data._innerArray);
+        // Without the fix, the following GC.Collect call causes fatal error during heap verification with DOTNET_HeapVerify=1
+        GC.Collect(0);
+    }
+}
