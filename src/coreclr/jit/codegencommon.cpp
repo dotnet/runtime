dotnet/runtime

commit 0bdd76841a9711ae797b9c5b877c8f8b1b87c16b

JIT: Support async contexts for OSR/EnC (#121672)
Now that async contexts are stored around the body of async methods we
need to take special care that these pieces of state get restored on OSR
and EnC transitions.

src/coreclr/jit/codegencommon.cpp
@@ -2102,11 +2102,15 @@ void CodeGen::genEmitMachineCode()
 
 #ifdef TARGET_64BIT
     trackedStackPtrsContig = false;
-#elif defined(TARGET_ARM)
-    // On arm due to prespilling of arguments, tracked stk-ptrs may not be contiguous
-    trackedStackPtrsContig = !compiler->opts.compDbgEnC && !compiler->compIsProfilerHookNeeded();
 #else
-    trackedStackPtrsContig = !compiler->opts.compDbgEnC;
+    // We try to allocate GC pointers contiguously on the stack except for some special cases.
+    trackedStackPtrsContig = !compiler->opts.compDbgEnC && (compiler->lvaAsyncExecutionContextVar == BAD_VAR_NUM) &&
+                             (compiler->lvaAsyncSynchronizationContextVar == BAD_VAR_NUM);
+
+#ifdef TARGET_ARM
+    // On arm due to prespilling of arguments, tracked stk-ptrs may not be contiguous
+    trackedStackPtrsContig &= !compiler->compIsProfilerHookNeeded();
+#endif
 #endif
 
     if (compiler->opts.disAsm && compiler->opts.disTesting)

src/coreclr/jit/compiler.cpp
@@ -5650,6 +5650,22 @@ void Compiler::generatePatchpointInfo()
                 patchpointInfo->MonitorAcquiredOffset());
     }
 
+    if (lvaAsyncExecutionContextVar != BAD_VAR_NUM)
+    {
+        LclVarDsc* const varDsc = lvaGetDesc(lvaAsyncExecutionContextVar);
+        patchpointInfo->SetAsyncExecutionContextOffset(varDsc->GetStackOffset() + offsetAdjust);
+        JITDUMP("--OSR-- async execution context V%02u virtual offset is %d\n", lvaAsyncExecutionContextVar,
+                patchpointInfo->AsyncExecutionContextOffset());
+    }
+
+    if (lvaAsyncSynchronizationContextVar != BAD_VAR_NUM)
+    {
+        LclVarDsc* const varDsc = lvaGetDesc(lvaAsyncSynchronizationContextVar);
+        patchpointInfo->SetAsyncSynchronizationContextOffset(varDsc->GetStackOffset() + offsetAdjust);
+        JITDUMP("--OSR-- async synchronization context V%02u virtual offset is %d\n", lvaAsyncSynchronizationContextVar,
+                patchpointInfo->AsyncSynchronizationContextOffset());
+    }
+
 #if defined(TARGET_AMD64)
     // Record callee save registers.
     // Currently only needed for x64.
@@ -10372,7 +10388,8 @@ bool Compiler::lvaIsOSRLocal(unsigned varNum)
         {
             // Sanity check for promoted fields of OSR locals.
             //
-            if (varNum >= info.compLocalsCount)
+            if ((varNum >= info.compLocalsCount) && (varNum != lvaMonAcquired) &&
+                (varNum != lvaAsyncExecutionContextVar) && (varNum != lvaAsyncSynchronizationContextVar))
             {
                 assert(varDsc->lvIsStructField);
                 assert(varDsc->lvParentLcl < info.compLocalsCount);

src/coreclr/jit/compiler.h
@@ -4066,6 +4066,7 @@ public:
     void lvaAssignVirtualFrameOffsetsToLocals();
     bool lvaParamHasLocalStackSpace(unsigned lclNum);
     int lvaAllocLocalAndSetVirtualOffset(unsigned lclNum, unsigned size, int stkOffs);
+    int lvaAllocAsyncContexts(int stkOffs);
 #ifdef TARGET_AMD64
     // Returns true if compCalleeRegsPushed (including RBP if used as frame pointer) is even.
     bool lvaIsCalleeSavedIntRegCountEven();

src/coreclr/jit/lclvars.cpp
@@ -4097,18 +4097,14 @@ unsigned Compiler::lvaGetMaxSpillTempSize()
  *      |-----------------------| <---- EBP
  *      |Callee saved registers |
  *      |-----------------------|
- *      |   security object     |
+ *      |      MonAcquired      |
  *      |-----------------------|
  *      |     ParamTypeArg      |
-// If funclet support is disabled
  *      |-----------------------|
- *      |  Last-executed-filter |
+ *      |    Async contexts     |
  *      |-----------------------|
- *      |                       |
- *      ~      Shadow SPs       ~
- *      |                       |
+ *      |      GS cookie        |
  *      |-----------------------|
-// Endif funclet support is disabled
  *      |                       |
  *      ~      Variables        ~
  *      |                       |
@@ -4180,9 +4176,13 @@ unsigned Compiler::lvaGetMaxSpillTempSize()
  *      -------------------------
  *      | Callee saved Flt regs |
  *      |-----------------------|
- *      |   security object     |
+ *      |      MonAcquired      |
+ *      |-----------------------|
+ *      |     Async contexts    |
  *      |-----------------------|
  *      |     ParamTypeArg      |
+ *      |-----------------------|
+ *      |      GS cookie        |
  *      |-----------------------|
  *      |                       |
  *      |                       |
@@ -4221,9 +4221,11 @@ unsigned Compiler::lvaGetMaxSpillTempSize()
  *      |-----------------------| <---- Virtual '0'
  *      |Callee saved registers |
  *      |-----------------------|
- *      ~ possible double align ~
+ *      |      MonAcquired      |
  *      |-----------------------|
- *      |   security object     |
+ *      |     Async contexts    |
+ *      |-----------------------|
+ *      ~ possible double align ~
  *      |-----------------------|
  *      |     ParamTypeArg      |
  *      |-----------------------|
@@ -4261,9 +4263,11 @@ unsigned Compiler::lvaGetMaxSpillTempSize()
  *      |-----------------------| <---- Virtual '0'
  *      |Callee saved registers |
  *      |-----------------------|
- *      ~ possible double align ~
+ *      |      MonAcquired      |
  *      |-----------------------|
- *      |   security object     |
+ *      |     Async contexts    |
+ *      |-----------------------|
+ *      ~ possible double align ~
  *      |-----------------------|
  *      |     ParamTypeArg      |
  *      |-----------------------|
@@ -4309,7 +4313,9 @@ unsigned Compiler::lvaGetMaxSpillTempSize()
  *      |Callee saved registers |
  *      |   except fp/lr        |
  *      |-----------------------|
- *      |   security object     |
+ *      |      MonAcquired      |
+ *      |-----------------------|
+ *      |     Async contexts    |
  *      |-----------------------|
  *      |     ParamTypeArg      |
  *      |-----------------------|
@@ -4350,7 +4356,9 @@ unsigned Compiler::lvaGetMaxSpillTempSize()
  *      |Callee saved registers |
  *      |   except fp/lr        |
  *      |-----------------------|
- *      |   security object     |
+ *      |      MonAcquired      |
+ *      |-----------------------|
+ *      |     Async contexts    |
  *      |-----------------------|
  *      |     ParamTypeArg      |
  *      |-----------------------|
@@ -4396,7 +4404,9 @@ unsigned Compiler::lvaGetMaxSpillTempSize()
  *      |-----------------------|
  *      |Callee saved registers |
  *      |-----------------------|
- *      |   security object     |
+ *      |      MonAcquired      |
+ *      |-----------------------|
+ *      |     Async contexts    |
  *      |-----------------------|
  *      |     ParamTypeArg      |
  *      |-----------------------|
@@ -4603,6 +4613,20 @@ void Compiler::lvaFixVirtualFrameOffsets()
             int offset = lvaTable[lvaMonAcquired].GetStackOffset() + delta;
             lvaTable[lvaMonAcquired].SetStackOffset(offset);
             delta += lvaLclStackHomeSize(lvaMonAcquired);
+        }
+
+        if ((lvaAsyncExecutionContextVar != BAD_VAR_NUM) && !opts.IsOSR())
+        {
+            int offset = lvaTable[lvaAsyncExecutionContextVar].GetStackOffset() + delta;
+            lvaTable[lvaAsyncExecutionContextVar].SetStackOffset(offset);
+            delta += lvaLclStackHomeSize(lvaAsyncExecutionContextVar);
+        }
+
+        if ((lvaAsyncSynchronizationContextVar != BAD_VAR_NUM) && !opts.IsOSR())
+        {
+            int offset = lvaTable[lvaAsyncSynchronizationContextVar].GetStackOffset() + delta;
+            lvaTable[lvaAsyncSynchronizationContextVar].SetStackOffset(offset);
+            delta += lvaLclStackHomeSize(lvaAsyncSynchronizationContextVar);
         }
 
         JITDUMP("--- delta bump %d for FP frame\n", delta);
@@ -5125,30 +5149,25 @@ void Compiler::lvaAssignVirtualFrameOffsetsToLocals()
 
 #endif // TARGET_AMD64
 
-    if (lvaMonAcquired != BAD_VAR_NUM)
+    if (!opts.IsOSR())
     {
-        // For OSR we use the flag set up by the original method.
-        //
-        if (opts.IsOSR())
-        {
-            assert(info.compPatchpointInfo->HasMonitorAcquired());
-            int originalOffset = info.compPatchpointInfo->MonitorAcquiredOffset();
-            int offset         = originalFrameStkOffs + originalOffset;
-
-            JITDUMP(
-                "---OSR--- V%02u (on tier0 frame, monitor acquired) tier0 FP-rel offset %d tier0 frame offset %d new "
-                "virt offset %d\n",
-                lvaMonAcquired, originalOffset, originalFrameStkOffs, offset);
-
-            lvaTable[lvaMonAcquired].SetStackOffset(offset);
-        }
-        else
+        if (lvaMonAcquired != BAD_VAR_NUM)
         {
             // This var must go first, in what is called the 'frame header' for EnC so that it is
             // preserved when remapping occurs.  See vm\eetwain.cpp for detailed comment specifying frame
             // layout requirements for EnC to work.
             stkOffs = lvaAllocLocalAndSetVirtualOffset(lvaMonAcquired, lvaLclStackHomeSize(lvaMonAcquired), stkOffs);
         }
+
+#ifndef JIT32_GCENCODER
+        // In this case the async contexts are part of the EnC frame header and the generic context is _not_.
+        // Preserving the generic context is handled as if it was a normal IL local.
+        //
+        // For jit32 the generic context is considered part of the EnC frame header too, and the async contexts
+        // come after it, so we allocate them later.
+        //
+        stkOffs = lvaAllocAsyncContexts(stkOffs);
+#endif
     }
 
     if (mustDoubleAlign)
@@ -5242,6 +5261,11 @@ void Compiler::lvaAssignVirtualFrameOffsetsToLocals()
             lvaCachedGenericContextArgOffs = stkOffs;
         }
     }
+#endif
+
+#ifdef JIT32_GCENCODER
+    assert(!opts.IsOSR());
+    stkOffs = lvaAllocAsyncContexts(stkOffs);
 #endif
 
     if (compGSReorderStackLayout)
@@ -5390,8 +5414,25 @@ void Compiler::lvaAssignVirtualFrameOffsetsToLocals()
                 {
                     // Add frampointer-relative offset of this OSR live local in the original frame
                     // to the offset of original frame in our new frame.
-                    const int originalOffset = info.compPatchpointInfo->Offset(lclNum);
-                    const int offset         = originalFrameStkOffs + originalOffset;
+                    int originalOffset;
+                    if (lclNum == lvaMonAcquired)
+                    {
+                        originalOffset = info.compPatchpointInfo->MonitorAcquiredOffset();
+                    }
+                    else if (lclNum == lvaAsyncExecutionContextVar)
+                    {
+                        originalOffset = info.compPatchpointInfo->AsyncExecutionContextOffset();
+                    }
+                    else if (lclNum == lvaAsyncSynchronizationContextVar)
+                    {
+                        originalOffset = info.compPatchpointInfo->AsyncSynchronizationContextOffset();
+                    }
+                    else
+                    {
+                        assert(lclNum < info.compPatchpointInfo->NumberOfLocals());
+                        originalOffset = info.compPatchpointInfo->Offset(lclNum);
+                    }
+                    const int offset = originalFrameStkOffs + originalOffset;
 
                     JITDUMP(
                         "---OSR--- V%02u (on tier0 frame) tier0 FP-rel offset %d tier0 frame offset %d new virt offset "
@@ -5454,7 +5495,8 @@ void Compiler::lvaAssignVirtualFrameOffsetsToLocals()
                 continue;
             }
 
-            if (lclNum == lvaMonAcquired)
+            if ((lclNum == lvaMonAcquired) || (lclNum == lvaAsyncExecutionContextVar) ||
+                (lclNum == lvaAsyncSynchronizationContextVar))
             {
                 continue;
             }
@@ -5893,6 +5935,43 @@ int Compiler::lvaAllocLocalAndSetVirtualOffset(unsigned lclNum, unsigned size, i
         printf(", size=%d, stkOffs=%c0x%x\n", size, stkOffs < 0 ? '-' : '+', stkOffs < 0 ? -stkOffs : stkOffs);
     }
 #endif
+
+    return stkOffs;
+}
+
+//------------------------------------------------------------------------
+// lvaAllocAsyncContexts:
+//   Allocate stack space for the two async context locals, if present.
+//
+// Arguments:
+//   stkOffs - Current stack offset
+//
+// Return Value:
+//    New stack offset.
+//
+int Compiler::lvaAllocAsyncContexts(int stkOffs)
+{
+    if (lvaAsyncExecutionContextVar != BAD_VAR_NUM)
+    {
+        stkOffs = lvaAllocLocalAndSetVirtualOffset(lvaAsyncExecutionContextVar,
+                                                   lvaLclStackHomeSize(lvaAsyncExecutionContextVar), stkOffs);
+    }
+    else
+    {
+        // For x86 EnC the VM expects that we always allocate stack space
+        // for this local when contexts were saved.
+        assert((info.compMethodInfo->options & CORINFO_ASYNC_SAVE_CONTEXTS) == 0);
+    }
+
+    if (lvaAsyncSynchronizationContextVar != BAD_VAR_NUM)
+    {
+        stkOffs = lvaAllocLocalAndSetVirtualOffset(lvaAsyncSynchronizationContextVar,
+                                                   lvaLclStackHomeSize(lvaAsyncSynchronizationContextVar), stkOffs);
+    }
+    else
+    {
+        assert((info.compMethodInfo->options & CORINFO_ASYNC_SAVE_CONTEXTS) == 0);
+    }
 
     return stkOffs;
 }

src/coreclr/inc/jiteeversionguid.h
@@ -37,11 +37,11 @@
 
 #include <minipal/guid.h>
 
-constexpr GUID JITEEVersionIdentifier = { /* d1188f9b-d81e-487a-b525-cedb10d15c8e */
-    0xd1188f9b,
-    0xd81e,
-    0x487a,
-    {0xb5, 0x25, 0xce, 0xdb, 0x10, 0xd1, 0x5c, 0x8e}
+constexpr GUID JITEEVersionIdentifier = { /* 693c76d4-6555-49b4-bcb4-db73cb87e8d2 */
+    0x693c76d4,
+    0x6555,
+    0x49b4,
+    {0xbc, 0xb4, 0xdb, 0x73, 0xcb, 0x87, 0xe8, 0xd2}
   };
 
 #endif // JIT_EE_VERSIONING_GUID_H

src/coreclr/inc/patchpointinfo.h
@@ -145,6 +145,37 @@ struct PatchpointInfo
         m_monitorAcquiredOffset = offset;
     }
 
+    // Original method FP relative offset for async contexts
+    int32_t AsyncExecutionContextOffset() const
+    {
+        return m_asyncExecutionContextOffset;
+    }
+
+    bool HasAsyncExecutionContextOffset() const
+    {
+        return m_asyncExecutionContextOffset != -1;
+    }
+
+    void SetAsyncExecutionContextOffset(int32_t offset)
+    {
+        m_asyncExecutionContextOffset = offset;
+    }
+
+    int32_t AsyncSynchronizationContextOffset() const
+    {
+        return m_asyncSynchronizationContextOffset;
+    }
+
+    bool HasAsyncSynchronizationContextOffset() const
+    {
+        return m_asyncSynchronizationContextOffset != -1;
+    }
+
+    void SetAsyncSynchronizationContextOffset(int32_t offset)
+    {
+        m_asyncSynchronizationContextOffset = offset;
+    }
+
     // True if this local was address exposed in the original method
     bool IsExposed(uint32_t localNum) const
     {
@@ -200,6 +231,8 @@ private:
     int32_t      m_keptAliveThisOffset;
     int32_t      m_securityCookieOffset;
     int32_t      m_monitorAcquiredOffset;
+    int32_t      m_asyncExecutionContextOffset;
+    int32_t      m_asyncSynchronizationContextOffset;
     int32_t      m_offsetAndExposureData[];
 };
 

src/coreclr/jit/async.cpp
@@ -73,6 +73,12 @@ PhaseStatus Compiler::SaveAsyncContexts()
     lvaAsyncSynchronizationContextVar                     = lvaGrabTemp(false DEBUGARG("Async SynchronizationContext"));
     lvaGetDesc(lvaAsyncSynchronizationContextVar)->lvType = TYP_REF;
 
+    if (opts.IsOSR())
+    {
+        lvaGetDesc(lvaAsyncExecutionContextVar)->lvIsOSRLocal       = true;
+        lvaGetDesc(lvaAsyncSynchronizationContextVar)->lvIsOSRLocal = true;
+    }
+
     // Create try-fault structure. This is actually a try-finally, but we
     // manually insert the restore code in a (merged) return block, so EH wise
     // we only need to restore on fault.
@@ -153,23 +159,28 @@ PhaseStatus Compiler::SaveAsyncContexts()
     CORINFO_ASYNC_INFO* asyncInfo = eeGetAsyncInfo();
 
     // Insert CaptureContexts call before the try (keep it before so the
-    // try/finally can be removed if there is no exception side effects)
-    GenTreeCall* captureCall = gtNewCallNode(CT_USER_FUNC, asyncInfo->captureContextsMethHnd, TYP_VOID);
-    captureCall->gtArgs.PushFront(this, NewCallArg::Primitive(gtNewLclAddrNode(lvaAsyncSynchronizationContextVar, 0)));
-    captureCall->gtArgs.PushFront(this, NewCallArg::Primitive(gtNewLclAddrNode(lvaAsyncExecutionContextVar, 0)));
-    lvaGetDesc(lvaAsyncSynchronizationContextVar)->lvHasLdAddrOp = true;
-    lvaGetDesc(lvaAsyncExecutionContextVar)->lvHasLdAddrOp       = true;
+    // try/finally can be removed if there is no exception side effects).
+    // For OSR, we did this in the tier0 method.
+    if (!opts.IsOSR())
+    {
+        GenTreeCall* captureCall = gtNewCallNode(CT_USER_FUNC, asyncInfo->captureContextsMethHnd, TYP_VOID);
+        captureCall->gtArgs.PushFront(this,
+                                      NewCallArg::Primitive(gtNewLclAddrNode(lvaAsyncSynchronizationContextVar, 0)));
+        captureCall->gtArgs.PushFront(this, NewCallArg::Primitive(gtNewLclAddrNode(lvaAsyncExecutionContextVar, 0)));
+        lvaGetDesc(lvaAsyncSynchronizationContextVar)->lvHasLdAddrOp = true;
+        lvaGetDesc(lvaAsyncExecutionContextVar)->lvHasLdAddrOp       = true;
 
-    CORINFO_CALL_INFO callInfo = {};
-    callInfo.hMethod           = captureCall->gtCallMethHnd;
-    callInfo.methodFlags       = info.compCompHnd->getMethodAttribs(callInfo.hMethod);
-    impMarkInlineCandidate(captureCall, MAKE_METHODCONTEXT(callInfo.hMethod), false, &callInfo, compInlineContext);
+        CORINFO_CALL_INFO callInfo = {};
+        callInfo.hMethod           = captureCall->gtCallMethHnd;
+        callInfo.methodFlags       = info.compCompHnd->getMethodAttribs(callInfo.hMethod);
+        impMarkInlineCandidate(captureCall, MAKE_METHODCONTEXT(callInfo.hMethod), false, &callInfo, compInlineContext);
 
-    Statement* captureStmt = fgNewStmtFromTree(captureCall);
-    fgInsertStmtAtBeg(fgFirstBB, captureStmt);
+        Statement* captureStmt = fgNewStmtFromTree(captureCall);
+        fgInsertStmtAtBeg(fgFirstBB, captureStmt);
 
-    JITDUMP("Inserted capture\n");
-    DISPSTMT(captureStmt);
+        JITDUMP("Inserted capture\n");
+        DISPSTMT(captureStmt);
+    }
 
     // Insert RestoreContexts call in fault (exceptional case)
     // First argument: started = (continuation == null)

src/coreclr/jit/codegenarm.cpp
@@ -2460,6 +2460,14 @@ void CodeGen::genCaptureFuncletPrologEpilogInfo()
         {
             saveSizeWithPSP += TARGET_POINTER_SIZE;
         }
+        if (compiler->lvaAsyncExecutionContextVar != BAD_VAR_NUM)
+        {
+            saveSizeWithPSP += TARGET_POINTER_SIZE;
+        }
+        if (compiler->lvaAsyncSynchronizationContextVar != BAD_VAR_NUM)
+        {
+            saveSizeWithPSP += TARGET_POINTER_SIZE;
+        }
         assert(compiler->lvaOutgoingArgSpaceSize % REGSIZE_BYTES == 0);
         unsigned funcletFrameSize = preSpillRegArgSize + saveSizeWithPSP + compiler->lvaOutgoingArgSpaceSize;
 

commit 2a4ede49738ba56673f5b9fb65f09109a6449d29

JIT: Fix patchpoint info out-of-bounds read for special OSR locals (#125378)
`genEnregisterOSRArgsAndLocals` was not properly handling computing the
stack offset for special OSR locals like `MonitorAcquired` and the async
contexts. It meant that if these ended up enregistered we would hit an
out-of-bounds when trying to obtain their offset.

This was a regression introduced by #121672 which started marking these
3 locals as OSR locals. Before that we only had `MonitorAcquired`, and I
suspect that one was never enregistered, so even though it was not
handled by `genEnregisterOSRArgsAndLocals` that was never a problem.
However, the async contexts can be enregistered in the OSR function so
they do need to be handled here.

src/coreclr/jit/codegencommon.cpp
@@ -4138,7 +4138,7 @@ void CodeGen::genEnregisterOSRArgsAndLocals()
         //
         const var_types lclTyp  = varDsc->GetStackSlotHomeType();
         const emitAttr  size    = emitActualTypeSize(lclTyp);
-        const int       stkOffs = patchpointInfo->Offset(lclNum) + fieldOffset;
+        const int       stkOffs = m_compiler->lvaOSRLocalTier0FrameOffset(lclNum) + fieldOffset;
 
 #if defined(TARGET_AMD64)
 

src/coreclr/jit/compiler.cpp
@@ -10437,7 +10437,7 @@ bool Compiler::killGCRefs(GenTree* tree)
 // Return Value:
 //    true       - this is an OSR compile and this local requires special treatment
 //    false      - not an OSR compile, or not an interesting local for OSR
-
+//
 bool Compiler::lvaIsOSRLocal(unsigned varNum)
 {
     LclVarDsc* const varDsc = lvaGetDesc(varNum);
@@ -10464,6 +10464,37 @@ bool Compiler::lvaIsOSRLocal(unsigned varNum)
 #endif
 
     return varDsc->lvIsOSRLocal;
+}
+
+//------------------------------------------------------------------------
+// lvaOSRLocalTier0FrameOffset:
+//   Get the offset in the tier0 frame for a specified OSR local.
+//
+// Arguments:
+//   varNum - variable of interest
+//
+// Return Value:
+//   The offset into the tier 0 frame.
+//
+int Compiler::lvaOSRLocalTier0FrameOffset(unsigned varNum)
+{
+    assert(lvaIsOSRLocal(varNum));
+
+    if (varNum == lvaMonAcquired)
+    {
+        return info.compPatchpointInfo->MonitorAcquiredOffset();
+    }
+    if (varNum == lvaAsyncExecutionContextVar)
+    {
+        return info.compPatchpointInfo->AsyncExecutionContextOffset();
+    }
+    if (varNum == lvaAsyncSynchronizationContextVar)
+    {
+        return info.compPatchpointInfo->AsyncSynchronizationContextOffset();
+    }
+
+    assert(varNum < info.compPatchpointInfo->NumberOfLocals());
+    return info.compPatchpointInfo->Offset(varNum);
 }
 
 //------------------------------------------------------------------------------

src/coreclr/jit/compiler.h
@@ -4120,6 +4120,8 @@ public:
     // located on the original method stack frame.
     bool lvaIsOSRLocal(unsigned varNum);
 
+    int lvaOSRLocalTier0FrameOffset(unsigned varNum);
+
     //------------------------ For splitting types ----------------------------
 
     void lvaInitTypeRef();

src/coreclr/jit/lclvars.cpp
@@ -5132,7 +5132,7 @@ void Compiler::lvaAssignVirtualFrameOffsetsToLocals()
                 if (varDsc->lvIsStructField)
                 {
                     const unsigned parentLclNum         = varDsc->lvParentLcl;
-                    const int      parentOriginalOffset = info.compPatchpointInfo->Offset(parentLclNum);
+                    const int      parentOriginalOffset = lvaOSRLocalTier0FrameOffset(parentLclNum);
                     const int      offset = originalFrameStkOffs + parentOriginalOffset + varDsc->lvFldOffset;
 
                     JITDUMP("---OSR--- V%02u (promoted field of V%02u; on tier0 frame) tier0 FP-rel offset %d tier0 "
@@ -5147,25 +5147,8 @@ void Compiler::lvaAssignVirtualFrameOffsetsToLocals()
                 {
                     // Add frampointer-relative offset of this OSR live local in the original frame
                     // to the offset of original frame in our new frame.
-                    int originalOffset;
-                    if (lclNum == lvaMonAcquired)
-                    {
-                        originalOffset = info.compPatchpointInfo->MonitorAcquiredOffset();
-                    }
-                    else if (lclNum == lvaAsyncExecutionContextVar)
-                    {
-                        originalOffset = info.compPatchpointInfo->AsyncExecutionContextOffset();
-                    }
-                    else if (lclNum == lvaAsyncSynchronizationContextVar)
-                    {
-                        originalOffset = info.compPatchpointInfo->AsyncSynchronizationContextOffset();
-                    }
-                    else
-                    {
-                        assert(lclNum < info.compPatchpointInfo->NumberOfLocals());
-                        originalOffset = info.compPatchpointInfo->Offset(lclNum);
-                    }
-                    const int offset = originalFrameStkOffs + originalOffset;
+                    int       originalOffset = lvaOSRLocalTier0FrameOffset(lclNum);
+                    const int offset         = originalFrameStkOffs + originalOffset;
 
                     JITDUMP(
                         "---OSR--- V%02u (on tier0 frame) tier0 FP-rel offset %d tier0 frame offset %d new virt offset "