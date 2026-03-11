dotnet/runtime

commit 65b2bc1ee86719dba05c628514d3e460fa8789f6

Emit runtime-async methods in crossgen2 (#124203)
Emit async methods and resumption stubs in crossgen2 ReadyToRun images

Add support for compiling and emitting runtime-async methods, their
async thunks, and resumption stubs into ReadyToRun (R2R) images
produced by crossgen2.

Async method signatures are emitted in the InstanceMethod table with a
new ENCODE_METHOD_SIG_AsyncVariant (0x100) flag. Resumption stubs are
encoded as precode fixups using a new READYTORUN_FIXUP_ResumptionStubEntryPoint
(0x38) fixup kind, where the "signature" is the RVA of the stub code.
At fixup resolution time, a DynamicMethodDesc/ILStub is created to
represent the resumption and provide GC and unwind info. Async methods
that do not await skip resumption stub emission.

Key changes:
- Add ResumptionStubEntryPointSignature node and fixup support
- Implement getAsyncResumptionStub and getFunctionEntryPoint in
  crossgen2's CorInfoImpl to enable the JIT to emit async calls
- Set R2R entrypoints on JIT-synthesized async helper calls
  (context capture/restore) via SetCallEntrypointForR2R
- Extend ReadyToRunILProvider to handle AsyncMethodVariant and
  AsyncResumptionStub IL bodies, including cross-module token
  rewriting through ManifestModuleWrappedMethodIL
- Route async variants and resumption stubs through the instance
  entrypoint table and inlining info emission
- Fix PEObjectWriter ARM64 page-relative relocations to support
  non-zero addends (needed for resumption stub code deltas)
- Fix GC pointer map encoding for types whose size is not
  pointer-aligned (off-by-one in encodedGCRefMap allocation)
- Mark CEE_JMP as unsupported in R2R (incorrect indirection cell setup)
- Disable hot/cold splitting for async methods (runtime limitation)
- Disable async method emission in composite mode and ARM32
  (known issues tracked in #125337 and #125338)
- Extend crossgen2 CI pipeline to arm64 (linux, osx, windows)
- Update r2rdump/ReadyToRunReader to discover resumption stubs

src/coreclr/tools/aot/ILCompiler.ReadyToRun/JitInterface/CorInfoImpl.ReadyToRun.cs
@@ -134,8 +134,7 @@ namespace Internal.JitInterface
         public readonly bool OwningTypeNotDerivedFromToken;
         public readonly TypeDesc OwningType;
 
-
-        public MethodWithToken(MethodDesc method, ModuleToken token, TypeDesc constrainedType, bool unboxing, object context, TypeDesc devirtualizedMethodOwner = null)
+        public MethodWithToken(MethodDesc method, ModuleToken token, TypeDesc constrainedType, bool unboxing, TypeSystemEntity context, TypeDesc devirtualizedMethodOwner = null)
         {
             Debug.Assert(!method.IsUnboxingThunk());
             Method = method;
@@ -143,15 +142,9 @@ namespace Internal.JitInterface
             ConstrainedType = constrainedType;
             Unboxing = unboxing;
             OwningType = GetMethodTokenOwningType(this, constrainedType, context, devirtualizedMethodOwner, out OwningTypeNotDerivedFromToken);
-            if (method.IsAsync && method.IsAsyncVariant() && token.Module is MutableModule)
-            {
-                var ecmaMethod = (EcmaMethod)method.GetTypicalMethodDefinition().GetPrimaryMethodDesc();
-                Token = new (ecmaMethod.Module, ecmaMethod.Handle);
-                OwningTypeNotDerivedFromToken = true;
-            }
         }
 
-        private static TypeDesc GetMethodTokenOwningType(MethodWithToken methodToken, TypeDesc constrainedType, object context, TypeDesc devirtualizedMethodOwner, out bool owningTypeNotDerivedFromToken)
+        private static TypeDesc GetMethodTokenOwningType(MethodWithToken methodToken, TypeDesc constrainedType, TypeSystemEntity context, TypeDesc devirtualizedMethodOwner, out bool owningTypeNotDerivedFromToken)
         {
             ModuleToken moduleToken = methodToken.Token;
             owningTypeNotDerivedFromToken = false;
@@ -186,7 +179,7 @@ namespace Internal.JitInterface
                     return methodToken.Method.OwningType;
             }
 
-            TypeDesc HandleContext(IEcmaModule module, EntityHandle handle, TypeDesc methodTargetOwner, TypeDesc constrainedType, object context, TypeDesc devirtualizedMethodOwner, ref bool owningTypeNotDerivedFromToken)
+            TypeDesc HandleContext(IEcmaModule module, EntityHandle handle, TypeDesc methodTargetOwner, TypeDesc constrainedType, TypeSystemEntity context, TypeDesc devirtualizedMethodOwner, ref bool owningTypeNotDerivedFromToken)
             {
                 var tokenOnlyOwningType = module.GetType(handle);
                 TypeDesc actualOwningType;
@@ -359,6 +352,8 @@ namespace Internal.JitInterface
                 sb.Append("; UNBOXING"u8);
             if (Method.IsAsyncVariant())
                 sb.Append("; ASYNC"u8);
+            if (Method is AsyncResumptionStub)
+                sb.Append("; RESUME"u8);
         }
 
         public override string ToString()
@@ -481,6 +476,7 @@ namespace Internal.JitInterface
         private List<EcmaMethod> _ilBodiesNeeded;
         private Dictionary<TypeDesc, bool> _preInitedTypes = new Dictionary<TypeDesc, bool>();
         private HashSet<MethodDesc> _synthesizedPgoDependencies;
+        private MethodDesc _asyncResumptionStub;
 
         public bool HasColdCode { get; private set; }
 
@@ -494,6 +490,23 @@ namespace Internal.JitInterface
         {
             _precodeFixups = _precodeFixups ?? new List<ISymbolNode>();
             _precodeFixups.Add(node);
+        }
+
+        private void AddResumptionStubFixup(MethodWithGCInfo compiledStubNode)
+        {
+            _methodCodeNode.Fixups.Add(_compilation.SymbolNodeFactory.ResumptionStubEntryPoint(compiledStubNode));
+        }
+
+        private CORINFO_METHOD_STRUCT_* getAsyncResumptionStub(ref void* entryPoint)
+        {
+            if (_asyncResumptionStub is null)
+            {
+                _asyncResumptionStub = new AsyncResumptionStub(MethodBeingCompiled, MethodBeingCompiled.OwningType);
+                AddResumptionStubFixup(_compilation.NodeFactory.CompiledMethodNode(_asyncResumptionStub));
+            }
+
+            entryPoint = (void*)ObjectToHandle(_compilation.NodeFactory.CompiledMethodNode(_asyncResumptionStub));
+            return ObjectToHandle(_asyncResumptionStub);
         }
 
         private static mdToken FindGenericMethodArgTypeSpec(EcmaModule module)
@@ -565,8 +578,7 @@ namespace Internal.JitInterface
 
         public static bool ShouldCodeNotBeCompiledIntoFinalImage(InstructionSetSupport instructionSetSupport, MethodDesc method)
         {
-            EcmaMethod ecmaMethod = method.GetTypicalMethodDefinition().GetPrimaryMethodDesc() as EcmaMethod;
-
+            EcmaMethod ecmaMethod = (EcmaMethod)(method.GetPrimaryMethodDesc().GetTypicalMethodDefinition());
             var metadataReader = ecmaMethod.MetadataReader;
             var stringComparer = metadataReader.StringComparer;
 
@@ -927,8 +939,8 @@ namespace Internal.JitInterface
             MethodDesc targetMethodDesc = HandleToObject(pTargetMethod.hMethod);
             Debug.Assert(!targetMethodDesc.IsUnboxingThunk());
 
-            var typeOrMethodContext = (pTargetMethod.tokenContext == contextFromMethodBeingCompiled()) ?
-                MethodBeingCompiled : HandleToObject((void*)pTargetMethod.tokenContext);
+            TypeSystemEntity typeOrMethodContext = (TypeSystemEntity)((pTargetMethod.tokenContext == contextFromMethodBeingCompiled()) ?
+                MethodBeingCompiled : HandleToObject((void*)pTargetMethod.tokenContext));
 
             TypeDesc constrainedType = null;
             if (targetConstraint != 0)
@@ -1289,7 +1301,19 @@ namespace Internal.JitInterface
 
         private void getFunctionEntryPoint(CORINFO_METHOD_STRUCT_* ftn, ref CORINFO_CONST_LOOKUP pResult, CORINFO_ACCESS_FLAGS accessFlags)
         {
-            throw new RequiresRuntimeJitException(HandleToObject(ftn).ToString());
+            var method = HandleToObject(ftn);
+
+            var entrypoint = _compilation.NodeFactory.MethodEntrypoint(
+                new MethodWithToken(
+                    method,
+                    _compilation.NodeFactory.Resolver.GetModuleTokenForMethod(method, true, true),
+                    null,
+                    false,
+                    MethodBeingCompiled),
+                false,
+                false,
+                false);
+            pResult = CreateConstLookupToSymbol(entrypoint);
         }
 
         private bool canTailCall(CORINFO_METHOD_STRUCT_* callerHnd, CORINFO_METHOD_STRUCT_* declaredCalleeHnd, CORINFO_METHOD_STRUCT_* exactCalleeHnd, bool fIsTailPrefix)
@@ -1336,7 +1360,7 @@ namespace Internal.JitInterface
 
         private MethodWithToken ComputeMethodWithToken(MethodDesc method, ref CORINFO_RESOLVED_TOKEN pResolvedToken, TypeDesc constrainedType, bool unboxing)
         {
-            ModuleToken token = HandleToModuleToken(ref pResolvedToken, method, out object context, ref constrainedType);
+            ModuleToken token = HandleToModuleToken(ref pResolvedToken, method, out TypeSystemEntity context, ref constrainedType);
 
             TypeDesc devirtualizedMethodOwner = null;
             if (pResolvedToken.tokenType == CorInfoTokenKind.CORINFO_TOKENKIND_DevirtualizedMethod)
@@ -1347,7 +1371,7 @@ namespace Internal.JitInterface
             return new MethodWithToken(method, token, constrainedType: constrainedType, unboxing: unboxing, context: context, devirtualizedMethodOwner: devirtualizedMethodOwner);
         }
 
-        private ModuleToken HandleToModuleToken(ref CORINFO_RESOLVED_TOKEN pResolvedToken, MethodDesc methodDesc, out object context, ref TypeDesc constrainedType)
+        private ModuleToken HandleToModuleToken(ref CORINFO_RESOLVED_TOKEN pResolvedToken, MethodDesc methodDesc, out TypeSystemEntity context, ref TypeDesc constrainedType)
         {
             if (methodDesc != null && (_compilation.NodeFactory.CompilationModuleGroup.VersionsWithMethodBody(methodDesc)
                 || (pResolvedToken.tokenType == CorInfoTokenKind.CORINFO_TOKENKIND_DevirtualizedMethod)
@@ -1406,7 +1430,7 @@ namespace Internal.JitInterface
                     // It's okay to strip the instantiation away because we don't need a MethodSpec
                     // token - SignatureBuilder will generate the generic method signature
                     // using instantiation parameters from the MethodDesc entity.
-                    resultMethod = resultMethod.GetTypicalMethodDefinition().GetPrimaryMethodDesc();
+                    resultMethod = resultMethod.GetPrimaryMethodDesc().GetTypicalMethodDefinition();
 
                     if (!_compilation.NodeFactory.CompilationModuleGroup.VersionsWithType(resultMethod.OwningType))
                     {
@@ -1431,24 +1455,7 @@ namespace Internal.JitInterface
                 }
                 else
                 {
-                    if (resultDef is EcmaType ecmaType)
-                    {
-                        if (!_compilation.NodeFactory.CompilationModuleGroup.VersionsWithType(ecmaType))
-                        {
-                            ModuleToken result = _compilation.NodeFactory.Resolver.GetModuleTokenForType(ecmaType, allowDynamicallyCreatedReference: true, throwIfNotFound: true);
-                            return result;
-                        }
-                        token = (mdToken)MetadataTokens.GetToken(ecmaType.Handle);
-                        module = ecmaType.Module;
-                    }
-                    else
-                    {
-                        // To replace !!0, we need to find the token for a !!0 TypeSpec within the image.
-                        Debug.Assert(resultDef is SignatureMethodVariable);
-                        Debug.Assert(((SignatureMethodVariable)resultDef).Index == 0);
-                        module = (EcmaModule)((MetadataType)methodILDef.OwningMethod.OwningType).Module;
-                        token = FindGenericMethodArgTypeSpec((EcmaModule)module);
-                    }
+                    return GetModuleTokenForType((TypeSystemEntity)resultDef);
                 }
             }
             else
@@ -1457,6 +1464,30 @@ namespace Internal.JitInterface
             }
 
             return new ModuleToken(module, token);
+
+            ModuleToken GetModuleTokenForType(TypeSystemEntity resultDef)
+            {
+                switch (resultDef)
+                {
+                    case SignatureMethodVariable sigMethod:
+                        Debug.Assert(sigMethod.Index == 0);
+                        module = (EcmaModule)((MetadataType)methodILDef.OwningMethod.OwningType).Module;
+                        token = FindGenericMethodArgTypeSpec((EcmaModule)module);
+                        return new ModuleToken(module, token);
+                    case ParameterizedType paramType:
+                        return  _compilation.NodeFactory.Resolver.GetModuleTokenForType(paramType, allowDynamicallyCreatedReference: true, throwIfNotFound: true);
+                    case EcmaType ecmaType:
+                        if (!_compilation.NodeFactory.CompilationModuleGroup.VersionsWithType(ecmaType))
+                        {
+                            return  _compilation.NodeFactory.Resolver.GetModuleTokenForType(ecmaType, allowDynamicallyCreatedReference: true, throwIfNotFound: true);
+                        }
+                        token = (mdToken)MetadataTokens.GetToken(ecmaType.Handle);
+                        module = ecmaType.Module;
+                        return new ModuleToken(module, token);
+                    default:
+                        throw new NotImplementedException($"Unsupported token resolution for {resultDef.GetType()}");
+                }
+            }
         }
 
         private InfoAccessType constructStringLiteral(CORINFO_MODULE_STRUCT_* module, mdToken metaTok, ref void* ppValue)
@@ -1497,7 +1528,7 @@ namespace Internal.JitInterface
                 CORINFO_EH_CLAUSE_FLAGS flags = clause.Flags;
                 uint classTokenOrOffset = clause.ClassTokenOrOffset;
                 if (flags == CORINFO_EH_CLAUSE_FLAGS.CORINFO_EH_CLAUSE_NONE
-                    && MethodBeingCompiled.IsAsyncThunk()
+                    && MethodBeingCompiled.IsCompilerGeneratedILBodyForAsync()
                     && ((TypeDesc)ResolveTokenInScope(_compilation.GetMethodIL(MethodBeingCompiled), MethodBeingCompiled, (mdToken)classTokenOrOffset)).IsWellKnownType(WellKnownType.Exception))
                 {
                     flags |= CORINFO_EH_CLAUSE_FLAGS.CORINFO_EH_CLAUSE_R2R_SYSTEM_EXCEPTION;
@@ -1903,7 +1934,7 @@ namespace Internal.JitInterface
                 throw new RequiresRuntimeJitException(callerMethod.ToString() + " -> " + originalMethod.ToString());
             }
 
-            callerModule = ((EcmaMethod)callerMethod.GetTypicalMethodDefinition()).Module;
+            callerModule = ((EcmaMethod)callerMethod.GetPrimaryMethodDesc().GetTypicalMethodDefinition()).Module;
             bool isCallVirt = (flags & CORINFO_CALLINFO_FLAGS.CORINFO_CALLINFO_CALLVIRT) != 0;
             bool isLdftn = (flags & CORINFO_CALLINFO_FLAGS.CORINFO_CALLINFO_LDFTN) != 0;
             bool isStaticVirtual = (originalMethod.Signature.IsStatic && originalMethod.IsVirtual);
@@ -2472,7 +2503,7 @@ namespace Internal.JitInterface
 
                         // If the abi of the method isn't stable, this will cause a usage of the RequiresRuntimeJitSymbol, which will trigger a RequiresRuntimeJitException
                         UpdateConstLookupWithRequiresRuntimeJitSymbolIfNeeded(ref pResult->codePointerOrStubLookup.constLookup, targetMethod);
-                        }
+                    }
                     break;
 
 
@@ -2506,6 +2537,17 @@ namespace Internal.JitInterface
                         if (methodToCall.OwningType.IsArray && methodToCall.IsConstructor)
                         {
                             pResult->codePointerOrStubLookup.constLookup = default;
+                        }
+                        else if (MethodBeingCompiled is AsyncResumptionStub resumptionStub
+                            && nonUnboxingMethod == resumptionStub.TargetMethod)
+                        {
+                            // Async resumption stubs must call the exact code version that created
+                            // the continuation, since the continuation layout is coupled to the
+                            // compilation. Use a direct call to the compiled method body so tiering
+                            // backpatching cannot redirect this call to a different code version.
+                            MethodDesc compilableTarget = nonUnboxingMethod.GetCanonMethodTarget(CanonicalFormKind.Specific);
+                            MethodWithGCInfo targetCodeNode = _compilation.NodeFactory.CompiledMethodNode(compilableTarget);
+                            pResult->codePointerOrStubLookup.constLookup = CreateConstLookupToSymbol(targetCodeNode);
                         }
                         else
                         {
@@ -3263,9 +3305,9 @@ namespace Internal.JitInterface
 
                 var typicalMethod = inlinee.GetTypicalMethodDefinition();
 
-                if ((typicalMethod.IsAsyncVariant() || typicalMethod.IsAsync || typicalMethod.IsAsyncThunk()) && !_compilation.CompilationModuleGroup.VersionsWithMethodBody(typicalMethod))
+                if ((typicalMethod.IsAsyncVariant() || typicalMethod.IsAsync || typicalMethod.IsCompilerGeneratedILBodyForAsync()) && !_compilation.CompilationModuleGroup.VersionsWithMethodBody(typicalMethod))
                 {
-                    // TODO: fix this restriction in runtime async
+                    // TODO: fix this restriction in runtime async. https://github.com/dotnet/runtime/issues/124665
                     // Disable async methods in cross module inlines for now, we need to trigger the CheckILBodyFixupSignature in the right situations, and that hasn't been implemented
                     // yet. Currently, we'll correctly trigger the _ilBodiesNeeded logic below, but we also need to avoid triggering the ILBodyFixupSignature for the async thunks, but we ALSO need to make
                     // sure we generate the CheckILBodyFixupSignature for the actual runtime-async body in which case I think the typicalMethod will be an AsyncVariantMethod, which doesn't appear

eng/pipelines/coreclr/crossgen2.yml
@@ -106,8 +106,11 @@ extends:
           buildConfig: Release
           platforms:
             - linux_x64
+            - linux_arm64
             # - osx_x64 Not included to reduce pressure on osx_x64 machines
+            - osx_arm64
             - windows_x64
+            - windows_arm64
           jobParameters:
             testGroup: innerloop
             buildArgs: -s clr+libs+libs.tests

src/coreclr/inc/corinfo.h
@@ -593,6 +593,7 @@ enum CorInfoHelpFunc
     CORINFO_HELP_VALIDATE_INDIRECT_CALL,    // CFG: Validate function pointer
     CORINFO_HELP_DISPATCH_INDIRECT_CALL,    // CFG: Validate and dispatch to pointer
 
+    // Helpers for runtime async (see System.Runtime.CompilerServices.AsyncHelpers)
     CORINFO_HELP_ALLOC_CONTINUATION,
     CORINFO_HELP_ALLOC_CONTINUATION_METHOD,
     CORINFO_HELP_ALLOC_CONTINUATION_CLASS,

src/coreclr/inc/readytorun.h
@@ -199,6 +199,7 @@ enum ReadyToRunMethodSigFlags
     READYTORUN_METHOD_SIG_Constrained           = 0x20,
     READYTORUN_METHOD_SIG_OwnerType             = 0x40,
     READYTORUN_METHOD_SIG_UpdateContext         = 0x80,
+    READYTORUN_METHOD_SIG_AsyncVariant          = 0x100,
 };
 
 enum ReadyToRunFieldSigFlags
@@ -298,6 +299,7 @@ enum ReadyToRunFixupKind
     READYTORUN_FIXUP_Check_IL_Body              = 0x35, /* Check to see if an IL method is defined the same at runtime as at compile time. A failed match will cause code not to be used. */
     READYTORUN_FIXUP_Verify_IL_Body             = 0x36, /* Verify an IL body is defined the same at compile time and runtime. A failed match will cause a hard runtime failure. */
     READYTORUN_FIXUP_Continuation_Layout        = 0x37, /* Layout of an async method continuation type */
+    READYTORUN_FIXUP_ResumptionStubEntryPoint   = 0x38, /* Entry point of an async method resumption stub */
 
     READYTORUN_FIXUP_ModuleOverride             = 0x80, /* followed by sig-encoded UInt with assemblyref index into either the assemblyref table of the MSIL metadata of the master context module for the signature or */
                                                         /* into the extra assemblyref table in the manifest metadata R2R header table (used in cases inlining brings in references to assemblies not seen in the MSIL). */
@@ -469,9 +471,9 @@ enum ReadyToRunHelper
 
     READYTORUN_HELPER_GetCurrentManagedThreadId = 0x112,
 
-    READYTORUN_HELPER_AllocContinuation = 0x113,
-    READYTORUN_HELPER_AllocContinuationClass = 0x114,
-    READYTORUN_HELPER_AllocContinuationMethod = 0x115,
+    READYTORUN_HELPER_AllocContinuation         = 0x113,
+    READYTORUN_HELPER_AllocContinuationClass    = 0x114,
+    READYTORUN_HELPER_AllocContinuationMethod   = 0x115,
 
     READYTORUN_HELPER_InitClass                 = 0x116,
     READYTORUN_HELPER_InitInstClass             = 0x117,

src/coreclr/inc/readytorunhelpers.h
@@ -20,6 +20,7 @@ HELPER(READYTORUN_HELPER_RngChkFail,                CORINFO_HELP_RNGCHKFAIL,    
 HELPER(READYTORUN_HELPER_FailFast,                  CORINFO_HELP_FAIL_FAST,                         OPTIMIZEFORSIZE)
 HELPER(READYTORUN_HELPER_ThrowNullRef,              CORINFO_HELP_THROWNULLREF,                      OPTIMIZEFORSIZE)
 HELPER(READYTORUN_HELPER_ThrowDivZero,              CORINFO_HELP_THROWDIVZERO,                      OPTIMIZEFORSIZE)
+HELPER(READYTORUN_HELPER_ThrowExact,                CORINFO_HELP_THROWEXACT,                        OPTIMIZEFORSIZE)
 
 HELPER(READYTORUN_HELPER_WriteBarrier,              CORINFO_HELP_ASSIGN_REF,                        )
 HELPER(READYTORUN_HELPER_CheckedWriteBarrier,       CORINFO_HELP_CHECKED_ASSIGN_REF,                )
@@ -128,7 +129,6 @@ HELPER(READYTORUN_HELPER_GetCurrentManagedThreadId, CORINFO_HELP_GETCURRENTMANAG
 HELPER(READYTORUN_HELPER_AllocContinuation,         CORINFO_HELP_ALLOC_CONTINUATION,                )
 HELPER(READYTORUN_HELPER_AllocContinuationClass,    CORINFO_HELP_ALLOC_CONTINUATION_CLASS,          )
 HELPER(READYTORUN_HELPER_AllocContinuationMethod,   CORINFO_HELP_ALLOC_CONTINUATION_METHOD,         )
-
 HELPER(READYTORUN_HELPER_InitClass,                 CORINFO_HELP_INITCLASS,                         )
 HELPER(READYTORUN_HELPER_InitInstClass,             CORINFO_HELP_INITINSTCLASS,                     )
 

src/coreclr/jit/async.cpp
@@ -45,6 +45,19 @@
 #include "jitstd/algorithm.h"
 #include "async.h"
 
+static void SetCallEntrypointForR2R(GenTreeCall* call, Compiler* compiler, CORINFO_METHOD_HANDLE handle)
+{
+#ifdef FEATURE_READYTORUN
+    if (!compiler->IsReadyToRun())
+    {
+        return;
+    }
+    CORINFO_CONST_LOOKUP entryPoint;
+    compiler->info.compCompHnd->getFunctionEntryPoint(handle, &entryPoint);
+    call->setEntryPoint(entryPoint);
+#endif
+}
+
 //------------------------------------------------------------------------
 // Compiler::SaveAsyncContexts:
 //   Insert code in async methods that saves and restores contexts.
@@ -164,6 +177,7 @@ PhaseStatus Compiler::SaveAsyncContexts()
     if (!opts.IsOSR())
     {
         GenTreeCall* captureCall = gtNewCallNode(CT_USER_FUNC, asyncInfo->captureContextsMethHnd, TYP_VOID);
+        SetCallEntrypointForR2R(captureCall, this, asyncInfo->captureContextsMethHnd);
         captureCall->gtArgs.PushFront(this,
                                       NewCallArg::Primitive(gtNewLclAddrNode(lvaAsyncSynchronizationContextVar, 0)));
         captureCall->gtArgs.PushFront(this, NewCallArg::Primitive(gtNewLclAddrNode(lvaAsyncExecutionContextVar, 0)));
@@ -197,6 +211,7 @@ PhaseStatus Compiler::SaveAsyncContexts()
     }
 
     GenTreeCall* restoreCall = gtNewCallNode(CT_USER_FUNC, asyncInfo->restoreContextsMethHnd, TYP_VOID);
+    SetCallEntrypointForR2R(restoreCall, this, asyncInfo->restoreContextsMethHnd);
     restoreCall->gtArgs.PushFront(this,
                                   NewCallArg::Primitive(gtNewLclVarNode(lvaAsyncSynchronizationContextVar, TYP_REF)));
     restoreCall->gtArgs.PushFront(this, NewCallArg::Primitive(gtNewLclVarNode(lvaAsyncExecutionContextVar, TYP_REF)));
@@ -363,6 +378,7 @@ BasicBlock* Compiler::CreateReturnBB(unsigned* mergedReturnLcl)
     }
 
     GenTreeCall* restoreCall = gtNewCallNode(CT_USER_FUNC, asyncInfo->restoreContextsMethHnd, TYP_VOID);
+    SetCallEntrypointForR2R(restoreCall, this, asyncInfo->restoreContextsMethHnd);
     restoreCall->gtArgs.PushFront(this,
                                   NewCallArg::Primitive(gtNewLclVarNode(lvaAsyncSynchronizationContextVar, TYP_REF)));
     restoreCall->gtArgs.PushFront(this, NewCallArg::Primitive(gtNewLclVarNode(lvaAsyncExecutionContextVar, TYP_REF)));
@@ -2117,6 +2133,7 @@ void AsyncTransformation::FillInDataOnSuspension(GenTreeCall*              call,
         GenTree*     flagsPlaceholder              = m_compiler->gtNewZeroConNode(TYP_BYREF);
         GenTreeCall* captureCall =
             m_compiler->gtNewCallNode(CT_USER_FUNC, m_asyncInfo->captureContinuationContextMethHnd, TYP_VOID);
+        SetCallEntrypointForR2R(captureCall, m_compiler, m_asyncInfo->captureContinuationContextMethHnd);
 
         captureCall->gtArgs.PushFront(m_compiler, NewCallArg::Primitive(flagsPlaceholder));
         captureCall->gtArgs.PushFront(m_compiler, NewCallArg::Primitive(contContextElementPlaceholder));
@@ -2161,6 +2178,7 @@ void AsyncTransformation::FillInDataOnSuspension(GenTreeCall*              call,
     {
         GenTreeCall* captureExecContext =
             m_compiler->gtNewCallNode(CT_USER_FUNC, m_asyncInfo->captureExecutionContextMethHnd, TYP_REF);
+        SetCallEntrypointForR2R(captureExecContext, m_compiler, m_asyncInfo->captureExecutionContextMethHnd);
 
         m_compiler->compCurBB = suspendBB;
         m_compiler->fgMorphTree(captureExecContext);
@@ -2203,6 +2221,7 @@ void AsyncTransformation::RestoreContexts(BasicBlock* block, GenTreeCall* call, 
     GenTree*     syncContextPlaceholder = m_compiler->gtNewNull();
     GenTreeCall* restoreCall =
         m_compiler->gtNewCallNode(CT_USER_FUNC, m_asyncInfo->restoreContextsOnSuspensionMethHnd, TYP_VOID);
+    SetCallEntrypointForR2R(restoreCall, m_compiler, m_asyncInfo->restoreContextsOnSuspensionMethHnd);
 
     restoreCall->gtArgs.PushFront(m_compiler, NewCallArg::Primitive(syncContextPlaceholder));
     restoreCall->gtArgs.PushFront(m_compiler, NewCallArg::Primitive(execContextPlaceholder));
@@ -2406,6 +2425,7 @@ void AsyncTransformation::RestoreFromDataOnResumption(const ContinuationLayout& 
         GenTree*     valuePlaceholder = m_compiler->gtNewZeroConNode(TYP_REF);
         GenTreeCall* restoreCall =
             m_compiler->gtNewCallNode(CT_USER_FUNC, m_asyncInfo->restoreExecutionContextMethHnd, TYP_VOID);
+        SetCallEntrypointForR2R(restoreCall, m_compiler, m_asyncInfo->restoreExecutionContextMethHnd);
         restoreCall->gtArgs.PushFront(m_compiler, NewCallArg::Primitive(valuePlaceholder));
 
         m_compiler->compCurBB = resumeBB;

src/coreclr/jit/importer.cpp
@@ -7049,6 +7049,14 @@ void Compiler::impImportBlockCode(BasicBlock* block)
 
                 assert(!compIsForInlining());
 
+                if (IsReadyToRun())
+                {
+                    // jmp is not supported on ReadyToRun
+                    // The call to the delayload method would not be properly set up to put the indirection cell address
+                    // in the correct register. See https://github.com/dotnet/runtime/issues/125252
+                    implReadyToRunUnsupported();
+                }
+
                 if ((info.compFlags & CORINFO_FLG_SYNCH) || block->hasTryIndex() || block->hasHndIndex())
                 {
                     /* CEE_JMP does not make sense in some "protected" regions. */

src/coreclr/tools/Common/Compiler/AsyncMethodVariant.cs
@@ -84,6 +84,11 @@ namespace ILCompiler
             return method.IsAsyncVariant() ^ method.IsAsync;
         }
 
+        public static bool IsCompilerGeneratedILBodyForAsync(this MethodDesc method)
+        {
+            return method.IsAsyncThunk() || method is AsyncResumptionStub;
+        }
+
         public static MethodDesc GetAsyncVariant(this MethodDesc method)
         {
             Debug.Assert(!method.IsAsyncVariant());

commit ee863ea7f9f7b3327b950a1552d9ddf4171076d2

Fix crossgen2 crash on Apple mobile for InstantiatedType token resolution in async methods
Add a TypeDesc case to GetModuleTokenForType to handle InstantiatedType
and other non-EcmaType TypeDesc variants by delegating to the existing
ModuleTokenResolver.GetModuleTokenForType.

Fixes a regression from #124203 where async resumption stubs referencing
generic type instantiations (e.g., CallStruct1M0<T>) crash with
NotImplementedException on Apple simulator CoreCLR RuntimeTests.

Co-authored-by: Copilot <223556219+Copilot@users.noreply.github.com>

src/coreclr/tools/aot/ILCompiler.ReadyToRun/JitInterface/CorInfoImpl.ReadyToRun.cs
@@ -1484,6 +1484,8 @@ namespace Internal.JitInterface
                         token = (mdToken)MetadataTokens.GetToken(ecmaType.Handle);
                         module = ecmaType.Module;
                         return new ModuleToken(module, token);
+                    case TypeDesc typeDesc:
+                        return _compilation.NodeFactory.Resolver.GetModuleTokenForType(typeDesc, allowDynamicallyCreatedReference: true, throwIfNotFound: true);
                     default:
                         throw new NotImplementedException($"Unsupported token resolution for {resultDef.GetType()}");
                 }