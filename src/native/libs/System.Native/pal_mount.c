377c8a803163a543848ac54aec883310c681ff96
Adeel Mujahid <3840695+am11@users.noreply.github.com>
2026-03-14 23:40:44 +0200

    Update pal_mount.c

--- src/native/libs/System.Native/pal_mount.c
+++ src/native/libs/System.Native/pal_mount.c
@@ -100,6 +100,8 @@ int32_t SystemNative_GetAllMountPoints(MountPointFound onFound, void* context)
         // Get actual mount point information
     #if HAVE_GETFSSTAT_SIZE_T
         count = getfsstat(mounts, bufferSize, MNT_NOWAIT);
+    #elif HAVE_GETFSSTAT_LONG
+        count = getfsstat(mounts, (long)bufferSize, MNT_NOWAIT);
     #elif HAVE_GETFSSTAT_INT
         count = getfsstat(mounts, (int)bufferSize, MNT_NOWAIT);
     #else
