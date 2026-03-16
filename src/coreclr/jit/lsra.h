dotnet/runtime

commit 703be0833b619fab3b970175f0d391c097d1e17b

JIT: Handle some def/use conflicts less conservatively in LSRA (#125214)
A def-use conflict happens for SDSU intervals when the def and use share
no common candidate register. For these cases we run through a set of
potential ways to resolve the conflict that make use of the contract
between LSRA and codegen. For example, when the definition requires a
single fixed register, it is still valid for LSRA to assign a different
register. This means that codegen can use the original fixed register,
but that it must insert a copy into the register assigned by LSRA.

In the worst case we cannot use any of the approaches to solve the
conflict and then we will fall back to spilling/copying as necessary.

It appears that several of the cases were overly conservative.
Particularly case 1 and 2, as described by the function header, were
never reachable; we assign `defRegConflict` and `useRegConflict` based
on whether there is a conflict in register constraints, but that is
always true when we get into this function. It appears the original
intention of these variables were whether there is a conflict with a
_different_ use of the registers we could otherwise have resolved the
original conflict with. This PR restores the meaning to that.

src/coreclr/jit/lsrabuild.cpp
@@ -255,8 +255,8 @@ void LinearScan::resolveConflictingDefAndUse(Interval* interval, RefPosition* de
     SingleTypeRegSet useRegAssignment = useRefPosition->registerAssignment;
     regNumber        defReg           = REG_NA;
     regNumber        useReg           = REG_NA;
-    bool             defRegConflict   = ((defRegAssignment & useRegAssignment) == RBM_NONE);
-    bool             useRegConflict   = defRegConflict;
+    bool             defRegConflict   = false;
+    bool             useRegConflict   = false;
 
     // If the useRefPosition is a "delayRegFree", we can't change the registerAssignment
     // on it, or we will fail to ensure that the fixedReg is busy at the time the target
@@ -268,7 +268,7 @@ void LinearScan::resolveConflictingDefAndUse(Interval* interval, RefPosition* de
     {
         INDEBUG(dumpLsraAllocationEvent(LSRA_EVENT_DEFUSE_FIXED_DELAY_USE));
     }
-    if (defRefPosition->isFixedRegRef && !defRegConflict)
+    if (defRefPosition->isFixedRegRef)
     {
         defReg = defRefPosition->assignedReg();
         if (canChangeUseAssignment)
@@ -294,7 +294,7 @@ void LinearScan::resolveConflictingDefAndUse(Interval* interval, RefPosition* de
             }
         }
     }
-    if (useRefPosition->isFixedRegRef && !useRegConflict)
+    if (useRefPosition->isFixedRegRef)
     {
         useReg = useRefPosition->assignedReg();
 
@@ -320,6 +320,15 @@ void LinearScan::resolveConflictingDefAndUse(Interval* interval, RefPosition* de
             }
             if (!useRegConflict)
             {
+                // The use-reg may be busy at this point due to being a
+                // delay-free use from the previous location.
+                if (isRegInUse(useReg, interval->registerType))
+                {
+                    useRegConflict = true;
+                }
+            }
+            if (!useRegConflict)
+            {
                 // This is case #2.  Use the useRegAssignment
                 INDEBUG(dumpLsraAllocationEvent(LSRA_EVENT_DEFUSE_CASE2, interval));
                 defRefPosition->registerAssignment = useRegAssignment;
@@ -336,6 +345,7 @@ void LinearScan::resolveConflictingDefAndUse(Interval* interval, RefPosition* de
         // This is case #3.
         INDEBUG(dumpLsraAllocationEvent(LSRA_EVENT_DEFUSE_CASE3, interval));
         defRefPosition->registerAssignment = useRegAssignment;
+        defRefPosition->isFixedRegRef      = false;
         return;
     }
     if ((useReg != REG_NA) && !defRegConflict && canChangeUseAssignment)
@@ -343,6 +353,7 @@ void LinearScan::resolveConflictingDefAndUse(Interval* interval, RefPosition* de
         // This is case #4.
         INDEBUG(dumpLsraAllocationEvent(LSRA_EVENT_DEFUSE_CASE4, interval));
         useRefPosition->registerAssignment = defRegAssignment;
+        useRefPosition->isFixedRegRef      = false;
         return;
     }
     if ((defReg != REG_NA) && (useReg != REG_NA))

commit f27c573c56e7aa280f519770b0678305a70442ae

JIT: Preference SDSU intervals away from killed registers (#125219)
This revives #81242 on top of #125214. I noticed some regressions with
#125214 because we were missing this logic (specifically case 5 when
resolving DU conflicts). Also, this may have some TP implications that I
want to look at separately from the other change.

Example:
```diff
 G_M40772_IG01:
-       sub      rsp, 40
-       xor      eax, eax
-       mov      qword ptr [rsp+0x20], rax
-						;; size=11 bbWeight=1 PerfScore 1.50
+       push     rbx
+       sub      rsp, 32
+						;; size=5 bbWeight=1 PerfScore 1.25
 G_M40772_IG02:
        call     [CORINFO_HELP_READYTORUN_GCSTATIC_BASE]
-       mov      bword ptr [rsp+0x20], rax
+       mov      rbx, rax
        call     [System.Reflection.Internal.PooledStringBuilder:CreatePool():System.Reflection.Internal.ObjectPool`1[System.Reflection.Internal.PooledStringBuilder]]
-       mov      rcx, bword ptr [rsp+0x20]
+       mov      rcx, rbx
        mov      rdx, rax
        call     [CORINFO_HELP_ASSIGN_REF]
        nop      
-						;; size=32 bbWeight=1 PerfScore 11.50
+						;; size=28 bbWeight=1 PerfScore 10.00
 G_M40772_IG03:
-       add      rsp, 40
+       add      rsp, 32
+       pop      rbx
        ret      
-						;; size=5 bbWeight=1 PerfScore 1.25
+						;; size=6 bbWeight=1 PerfScore 1.75
 
-; Total bytes of code 48, prolog size 11, PerfScore 14.25, instruction count 12, allocated bytes for code 48 (MethodHash=bb6e60bb) for method System.Reflection.Internal.PooledStringBuilder:.cctor() (FullOpts)
+; Total bytes of code 39, prolog size 5, PerfScore 13.00, instruction count 12, allocated bytes for code 39 (MethodHash=bb6e60bb) for method System.Reflection.Internal.PooledStringBuilder:.cctor() (FullOpts)
 ; ============================================================
```

src/coreclr/jit/lsra.h
@@ -1081,6 +1081,7 @@ private:
 
     // Given some tree node add refpositions for all the registers this node kills
     bool buildKillPositionsForNode(GenTree* tree, LsraLocation currentLoc, regMaskTP killMask);
+    void updateIntervalPreferencesForKill(Interval* interval, regMaskTP killMask);
 
     SingleTypeRegSet allRegs(RegisterType rt);
     SingleTypeRegSet allByteRegs();

src/coreclr/jit/lsrabuild.cpp
@@ -1125,7 +1125,7 @@ regMaskTP LinearScan::getKillSetForNode(GenTree* tree)
 //
 //    This method can add kills even if killMask is RBM_NONE, if this tree is one of the
 //    special cases that signals that we can't permit callee save registers to hold GC refs.
-
+//
 bool LinearScan::buildKillPositionsForNode(GenTree* tree, LsraLocation currentLoc, regMaskTP killMask)
 {
     bool insertedKills = false;
@@ -1164,43 +1164,23 @@ bool LinearScan::buildKillPositionsForNode(GenTree* tree, LsraLocation currentLo
                     {
                         continue;
                     }
-                Interval*  interval   = getIntervalForLocalVar(varIndex);
-                const bool isCallKill = ((killMask.getLow() == RBM_INT_CALLEE_TRASH) || (killMask == RBM_CALLEE_TRASH));
-                SingleTypeRegSet regsKillMask = killMask.GetRegSetForType(interval->registerType);
 
-                if (isCallKill)
-                {
-                    interval->preferCalleeSave = true;
-                }
-
-                // We are more conservative about allocating callee-saves registers to write-thru vars, since
-                // a call only requires reloading after (not spilling before). So we record (above) the fact
-                // that we'd prefer a callee-save register, but we don't update the preferences at this point.
-                // See the "heuristics for writeThru intervals" in 'buildIntervals()'.
-                if (!interval->isWriteThru || !isCallKill)
-                {
-                    SingleTypeRegSet newPreferences = allRegs(interval->registerType) & (~regsKillMask);
-
-                    if (newPreferences != RBM_NONE)
-                    {
-                        if (!interval->isWriteThru)
-                        {
-                            // Update the register aversion as long as this is not write-thru vars for
-                            // reason mentioned above.
-                            interval->registerAversion |= regsKillMask;
-                        }
-                        interval->updateRegisterPreferences(newPreferences);
-                    }
-                    else
-                    {
-                        // If there are no callee-saved registers, the call could kill all the registers.
-                        // This is a valid state, so in that case assert should not trigger. The RA will spill in order
-                        // to free a register later.
-                        assert(m_compiler->opts.compDbgEnC || (calleeSaveRegs(varDsc->lvType) == RBM_NONE) ||
-                               varTypeIsStruct(varDsc->lvType));
-                    }
-                }
+                Interval* interval = getIntervalForLocalVar(varIndex);
+                updateIntervalPreferencesForKill(interval, killMask);
             }
+        }
+
+        // Now update preferences of LIR edges to avoid the killed registers.
+        for (RefInfoListNode* cur = defList.Begin(); cur != defList.End(); cur = cur->Next())
+        {
+            Interval* interval = cur->ref->getInterval();
+            if (interval->isLocalVar)
+            {
+                // Handled via liveness above
+                continue;
+            }
+
+            updateIntervalPreferencesForKill(interval, killMask);
         }
 
         insertedKills = true;
@@ -1214,6 +1194,46 @@ bool LinearScan::buildKillPositionsForNode(GenTree* tree, LsraLocation currentLo
     }
 
     return insertedKills;
+}
+
+//------------------------------------------------------------------------
+// updateIntervalPreferencesForKill:
+//   Update the specified interval to take into account that some registers will
+//   be killed while it is live.
+//
+// Arguments:
+//    interval - The interval
+//    killMask - Registers that will be killed during the interval's lifetime
+//
+void LinearScan::updateIntervalPreferencesForKill(Interval* interval, regMaskTP killMask)
+{
+    const bool       isCallKill   = ((killMask.getLow() == RBM_INT_CALLEE_TRASH) || (killMask == RBM_CALLEE_TRASH));
+    SingleTypeRegSet regsKillMask = killMask.GetRegSetForType(interval->registerType);
+
+    if (isCallKill)
+    {
+        interval->preferCalleeSave = true;
+    }
+
+    // We are more conservative about allocating callee-saves registers to write-thru vars, since
+    // a call only requires reloading after (not spilling before). So we record (above) the fact
+    // that we'd prefer a callee-save register, but we don't update the preferences at this point.
+    // See the "heuristics for writeThru intervals" in 'buildIntervals()'.
+    if (!interval->isWriteThru || !isCallKill)
+    {
+        SingleTypeRegSet newPreferences = allRegs(interval->registerType) & (~regsKillMask);
+
+        if (newPreferences != RBM_NONE)
+        {
+            if (!interval->isWriteThru)
+            {
+                // Update the register aversion as long as this is not write-thru vars for
+                // reason mentioned above.
+                interval->registerAversion |= regsKillMask;
+            }
+            interval->updateRegisterPreferences(newPreferences);
+        }
+    }
 }
 
 //------------------------------------------------------------------------