b732fb8f1aa24f610b7b5f3091a1285a93b7d63d
Marie Píchová <11718369+ManickaP@users.noreply.github.com>
2026-03-18 10:49:51 +0100

    [QUIC] Disable Quic tests on AzL3 VM (#125665)
    Contributes to https://github.com/dotnet/runtime/issues/123216

--- src/libraries/System.Net.Quic/tests/FunctionalTests/QuicListenerTests.cs
+++ src/libraries/System.Net.Quic/tests/FunctionalTests/QuicListenerTests.cs
@@ -14,7 +14,7 @@ using Xunit.Abstractions;
 namespace System.Net.Quic.Tests
 {
     [Collection(nameof(QuicTestCollection))]
-    [ConditionalClass(typeof(QuicTestBase), nameof(QuicTestBase.IsSupported), nameof(QuicTestBase.IsNotArm32CoreClrStressTest))]
+    [ConditionalClass(typeof(QuicTestBase), nameof(QuicTestBase.IsSupported), nameof(QuicTestBase.IsNotArm32CoreClrStressTest), nameof(QuicTestBase.IsNotAzureLinux3VM))]
     public sealed class QuicListenerTests : QuicTestBase
     {
         public QuicListenerTests(ITestOutputHelper output) : base(output) { }
