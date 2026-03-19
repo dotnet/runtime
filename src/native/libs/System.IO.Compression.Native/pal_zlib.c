2a0b9479fb6522552218578a332bc109bddb0257
iremyux <iremyuksel@microsoft.com>
2026-03-19 18:51:55 +0100

    Add 'assert' to CompressionNative_CompressBound

--- src/native/libs/System.IO.Compression.Native/pal_zlib.c
+++ src/native/libs/System.IO.Compression.Native/pal_zlib.c
@@ -211,5 +211,7 @@ uint32_t CompressionNative_Crc32(uint32_t crc, uint8_t* buffer, int32_t len)
 
 uint32_t CompressionNative_CompressBound(uint32_t sourceLen)
 {
-    return (uint32_t)compressBound(sourceLen);
+    unsigned long result = compressBound(sourceLen);
+    assert(result <= UINT32_MAX);
+    return (uint32_t)result;
 }
