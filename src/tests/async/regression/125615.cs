29e2bf3437428a194beb8eb350c86f1136760c7e
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2026-03-23 18:22:19 +0100

    Feedback

--- src/tests/async/regression/125615.cs
+++ src/tests/async/regression/125615.cs
@@ -1,11 +1,10 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
 using System.Threading.Tasks;
 using Xunit;
 
-public class MutateImplicitByRefTest
+public class Runtime_125615
 {
     [Fact]
     public static void TestEntryPoint()
@@ -21,7 +20,7 @@ public class MutateImplicitByRefTest
         await Task.Yield();
         return value.A;
     }
-    
+
     private struct ImpByRef
     {
         public long A, B, C, D, E;
