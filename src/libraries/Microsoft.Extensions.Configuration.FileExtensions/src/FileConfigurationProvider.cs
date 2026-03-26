0a76a86b95c14b595bb8df0c81563a481f796950
Michal Žůrek <188900745+mrek-msft@users.noreply.github.com>
2026-03-26 17:58:52 +0100

    Change exception back to rpopagating IOException directly and improve file path formatting logic.

--- src/libraries/Microsoft.Extensions.Configuration.FileExtensions/src/FileConfigurationProvider.cs
+++ src/libraries/Microsoft.Extensions.Configuration.FileExtensions/src/FileConfigurationProvider.cs
@@ -106,13 +106,13 @@ namespace Microsoft.Extensions.Configuration
                         {
                             Data = new Dictionary<string, string?>(StringComparer.OrdinalIgnoreCase);
                         }
-                        throw ex;
+                        string filePath = Source.Path ?? file.Name;
+                        throw new InvalidDataException(SR.Format(SR.Error_FailedToLoad, filePath), ex);
                     }
                 }
                 catch (Exception ex)
                 {
-                    var exception = new InvalidDataException(SR.Format(SR.Error_FailedToLoad, file.PhysicalPath), ex);
-                    HandleException(ExceptionDispatchInfo.Capture(exception));
+                    HandleException(ExceptionDispatchInfo.Capture(ex));
                 }
             }
             // REVIEW: Should we raise this in the base as well / instead?
@@ -129,6 +129,7 @@ namespace Microsoft.Extensions.Configuration
         /// <exception cref="InvalidDataException">An exception was thrown by the concrete implementation of the
         /// <see cref="Load()"/> method. Use the source <see cref="FileConfigurationSource.OnLoadException"/> callback
         /// if you need more control over the exception.</exception>
+        /// <exception cref="IOException">An exception was thrown when opening file.</exception>
         public override void Load()
         {
             Load(reload: false);
