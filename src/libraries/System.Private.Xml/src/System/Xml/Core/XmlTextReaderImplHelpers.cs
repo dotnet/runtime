e53592dd839d5d8a48a0a074b76b5db3aaa861d6
Krzysztof Wicher <kwicher@microsoft.com>
2026-03-26 14:54:15 +0100

    Fix attribute duplicate check performance for XmlTextReaderImpl

--- src/libraries/System.Private.Xml/src/System/Xml/Core/XmlTextReaderImplHelpers.cs
+++ src/libraries/System.Private.Xml/src/System/Xml/Core/XmlTextReaderImplHelpers.cs
@@ -8,6 +8,7 @@ using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.IO;
+using System.Runtime.CompilerServices;
 using System.Runtime.Versioning;
 using System.Security;
 using System.Text;
@@ -703,6 +704,30 @@ namespace System.Xml
                     Debug.Fail("We should never get to this point.");
                     // 'other' is null, 'this' is not null. Always return 1, like "".CompareTo(null).
                     return 1;
+                }
+            }
+
+            internal sealed class AtomizedNameEqualityComparer : IEqualityComparer<NodeData>
+            {
+                internal static readonly AtomizedNameEqualityComparer Instance = new AtomizedNameEqualityComparer();
+
+                public bool Equals(NodeData? x, NodeData? y)
+                {
+                    if (x is null)
+                    {
+                        return y is null;
+                    }
+
+                    return y is not null
+                        && Ref.Equal(x.localName, y.localName)
+                        && Ref.Equal(x.ns, y.ns);
+                }
+
+                public int GetHashCode(NodeData node)
+                {
+                    return HashCode.Combine(
+                        RuntimeHelpers.GetHashCode(node.localName),
+                        RuntimeHelpers.GetHashCode(node.ns));
                 }
             }
         }
