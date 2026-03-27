10daad5ab879635114c73a2498481c2cd520e99c
J-P Nurmi <jpnurmi@gmail.com>
2026-03-27 21:09:28 +0100

    Restore pre-Mono signal handlers instead of resetting to SIG_DFL
    Use mono_runtime_posix_remove_signal_handler to restore whatever
    handlers were installed before Mono, rather than unconditionally
    resetting to SIG_DFL. This correctly handles crash chaining without
    needing a special case.

--- src/mono/mono/mini/mini-exceptions.c
+++ src/mono/mono/mini/mini-exceptions.c
@@ -2955,29 +2955,18 @@ mono_handle_native_crash (const char *signal, MonoContext *mctx, MONO_SIG_HANDLE
 	MonoJitTlsData *jit_tls = mono_tls_get_jit_tls ();
 
 #ifdef MONO_ARCH_USE_SIGACTION
-	// When crash chaining is enabled, keep our signal handlers installed so
-	// that secondary signals (e.g. SIGABRT from FORTIFY on other threads)
-	// don't kill the process with SIG_DFL before we can chain the original
-	// crash to the previous handler.
-	if (!mono_do_crash_chaining) {
-		struct sigaction sa;
-		sa.sa_handler = SIG_DFL;
-		sigemptyset (&sa.sa_mask);
-		sa.sa_flags = 0;
+	/* Remove our SIGABRT handler */
+	mono_runtime_posix_remove_signal_handler (SIGABRT);
 
-		/* Remove our SIGABRT handler */
-		g_assert (sigaction (SIGABRT, &sa, NULL) != -1);
+	/* On some systems we get a SIGILL when calling abort (), because it might
+	 * fail to raise SIGABRT */
+	mono_runtime_posix_remove_signal_handler (SIGILL);
 
-		/* On some systems we get a SIGILL when calling abort (), because it might
-		 * fail to raise SIGABRT */
-		g_assert (sigaction (SIGILL, &sa, NULL) != -1);
+	/* Remove SIGCHLD, it uses the finalizer thread */
+	mono_runtime_posix_remove_signal_handler (SIGCHLD);
 
-		/* Remove SIGCHLD, it uses the finalizer thread */
-		g_assert (sigaction (SIGCHLD, &sa, NULL) != -1);
-
-		/* Remove SIGQUIT, we are already dumping threads */
-		g_assert (sigaction (SIGQUIT, &sa, NULL) != -1);
-	}
+	/* Remove SIGQUIT, we are already dumping threads */
+	mono_runtime_posix_remove_signal_handler (SIGQUIT);
 #endif
 
 	if (mini_debug_options.suspend_on_native_crash) {
