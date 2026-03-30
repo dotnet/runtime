922b086c53f06dbd150f03b48d5cd5bdf82797f2
Egor Bogatov <egorbo@gmail.com>
2026-03-30 18:09:16 +0200

    Update src/libraries/Common/src/Polyfills/CollectionsPolyfills.cs
    Co-authored-by: Jan Kotas <jkotas@microsoft.com>

--- src/libraries/Common/src/Polyfills/CollectionsPolyfills.cs
+++ src/libraries/Common/src/Polyfills/CollectionsPolyfills.cs
@@ -8,7 +8,6 @@ namespace System.Collections.Generic
     /// <summary>Provides downlevel polyfills for <see cref="KeyValuePair{TKey, TValue}"/>.</summary>
     internal static class KeyValuePairPolyfills
     {
-        [EditorBrowsable(EditorBrowsableState.Never)]
         public static void Deconstruct<TKey, TValue>(this KeyValuePair<TKey, TValue> source, out TKey key, out TValue value)
         {
             key = source.Key;
