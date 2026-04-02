3f5c8fa5a68b79d448de0b52fe967dd8a0da8e13
Kevin Jones <kevin@vcsjones.com>
2026-04-02 14:54:52 -0400

    Using var

--- src/libraries/System.Runtime.Numerics/src/System/Numerics/BigInteger.cs
+++ src/libraries/System.Runtime.Numerics/src/System/Numerics/BigInteger.cs
@@ -918,15 +918,13 @@ namespace System.Numerics
             {
                 int size = dividend._bits.Length;
                 Span<nuint> quotient = RentedBuffer.Create(size, out RentedBuffer quotientBuffer);
+                using var _ = quotientBuffer;
 
-                using (quotientBuffer)
-                {
-                    // may throw DivideByZeroException
-                    BigIntegerCalculator.Divide(dividend._bits, NumericsHelpers.Abs(divisor._sign), quotient, out nuint rest);
+                // may throw DivideByZeroException
+                BigIntegerCalculator.Divide(dividend._bits, NumericsHelpers.Abs(divisor._sign), quotient, out nuint rest);
 
-                    remainder = dividend._sign < 0 ? -(long)rest : (long)rest;
-                    return new BigInteger(quotient, (dividend._sign < 0) ^ (divisor._sign < 0));
-                }
+                remainder = dividend._sign < 0 ? -(long)rest : (long)rest;
+                return new BigInteger(quotient, (dividend._sign < 0) ^ (divisor._sign < 0));
             }
 
             Debug.Assert(divisor._bits is not null);
