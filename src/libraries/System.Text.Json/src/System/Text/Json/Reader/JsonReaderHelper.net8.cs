ccee1e8a02177cbc2de58e1e0a3c42297e9a7f27
Egor Bogatov <egorbo@gmail.com>
2026-04-09 16:22:32 +0200

    Optimize bitmask check for ARM64 in JsonReaderHelper

--- src/libraries/System.Text.Json/src/System/Text/Json/Reader/JsonReaderHelper.net8.cs
+++ src/libraries/System.Text.Json/src/System/Text/Json/Reader/JsonReaderHelper.net8.cs
@@ -40,9 +40,11 @@ namespace System.Text.Json
             // but that is not currently optimized in JIT for ARM64, so we do it manually here.
             if (AdvSimd.IsSupported)
             {
-                ulong mask = AdvSimd.ShiftRightLogicalNarrowingLower(cmp.AsUInt16(), 4).AsUInt64().ToScalar();
-                if (mask != 0)
+                if (cmp != Vector128<byte>.Zero)
+                {
+                    ulong mask = AdvSimd.ShiftRightLogicalNarrowingLower(cmp.AsUInt16(), 4).AsUInt64().ToScalar();
                     return BitOperations.TrailingZeroCount(mask) >> 2;
+                }
             }
             else
             {
