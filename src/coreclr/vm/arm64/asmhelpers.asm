cefcd3fc992621f0a02bb4fc3c37d1f36621197c
Swapnil Gaikwad <swapnil.gaikwad@arm.com>
2026-04-10 10:41:31 +0100

    Fix register addressing in asmhelpers

--- src/coreclr/vm/arm64/asmhelpers.asm
+++ src/coreclr/vm/arm64/asmhelpers.asm
@@ -338,7 +338,7 @@ NoFloatingPointRetVal
         mov x17, x0
         mov x16, x1
         DCD 0xD503211F  ; pacia1716 instruction in binary to avoid error while compiling with non-PAC enabled compilers
-        mov x0, $x17
+        mov x0, x17
         ret
     LEAF_END PacSignPtr
 
