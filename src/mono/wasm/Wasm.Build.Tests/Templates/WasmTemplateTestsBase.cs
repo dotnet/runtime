3f93ab379bfbf130a92864e7a11336a3fd9eb886
Marek Fišera <mara@neptuo.com>
2026-04-10 09:36:50 +0000

    Clean up

--- src/mono/wasm/Wasm.Build.Tests/Templates/WasmTemplateTestsBase.cs
+++ src/mono/wasm/Wasm.Build.Tests/Templates/WasmTemplateTestsBase.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Configuration;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
