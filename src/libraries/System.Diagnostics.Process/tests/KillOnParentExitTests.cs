35b273ce8085fa1adf90026ca2a57cba0365934f
Adam Sitnik <adam.sitnik@gmail.com>
2026-04-11 10:31:58 +0200

    try to avoid creating a memory dump

--- src/libraries/System.Diagnostics.Process/tests/KillOnParentExitTests.cs
+++ src/libraries/System.Diagnostics.Process/tests/KillOnParentExitTests.cs
@@ -149,12 +149,18 @@ namespace System.Diagnostics.Tests
         [InlineData(true, true)]
         [InlineData(true, false)]
         [InlineData(false, false)] // (false, true) is tested by ProcessHandleTests
+        [OuterLoop("May create a memory dump")]
         public void KillOnParentExit_KillsTheChild_WhenParentCrashes(bool enabled, bool restrictInheritance)
         {
             RemoteInvokeOptions remoteInvokeOptions = new() { CheckExitCode = false };
             remoteInvokeOptions.StartInfo.RedirectStandardOutput = true;
             remoteInvokeOptions.StartInfo.RedirectStandardError = true;
             remoteInvokeOptions.StartInfo.RedirectStandardInput = true;
+
+            // RemoteExecutor creates a memory dump when the child process does not exit on time.
+            // Until we have a dedicated API to control this behavior,
+            // we can disable dump creation by setting the upload root to null.
+            remoteInvokeOptions.StartInfo.Environment["HELIX_WORKITEM_UPLOAD_ROOT"] = null;
 
             using RemoteInvokeHandle childHandle = RemoteExecutor.Invoke(
                 (enabledStr, limitInheritanceStr) =>
