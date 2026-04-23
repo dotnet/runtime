5f34825a1cd10f570f71f8aee189a80658607370
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2026-04-24 08:22:07 +0900

    Update NullableUnwrappedGenericParameterValue.cs

--- src/tools/illink/src/ILLink.Shared/TrimAnalysis/NullableUnwrappedGenericParameterValue.cs
+++ src/tools/illink/src/ILLink.Shared/TrimAnalysis/NullableUnwrappedGenericParameterValue.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using ILLink.Shared.DataFlow;
 
 // This is needed due to NativeAOT which doesn't enable nullable globally yet
