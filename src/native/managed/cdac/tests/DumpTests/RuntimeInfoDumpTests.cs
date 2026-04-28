06cde5ad0abff8da68493e0890d014651fd32bfb
Rachel Jarvi <rachel.jarvi@gmail.com>
2026-04-27 23:39:14 -0700

    adding a few full dumps

--- src/native/managed/cdac/tests/DumpTests/RuntimeInfoDumpTests.cs
+++ src/native/managed/cdac/tests/DumpTests/RuntimeInfoDumpTests.cs
@@ -14,6 +14,7 @@ namespace Microsoft.Diagnostics.DataContractReader.DumpTests;
 public class RuntimeInfoDumpTests : DumpTestBase
 {
     protected override string DebuggeeName => "BasicThreads";
+    protected override string DumpType => "full";
 
     [ConditionalTheory]
     [MemberData(nameof(TestConfigurations))]
--- src/native/managed/cdac/tests/DumpTests/ThreadDumpTests.cs
+++ src/native/managed/cdac/tests/DumpTests/ThreadDumpTests.cs
@@ -16,6 +16,7 @@ public class ThreadDumpTests : DumpTestBase
     private const int SpawnedThreadCount = 5;
 
     protected override string DebuggeeName => "BasicThreads";
+    protected override string DumpType => "full";
 
     [ConditionalTheory]
     [MemberData(nameof(TestConfigurations))]
