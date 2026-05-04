c20fa677c2d6665ca55169d36592001f16d93416
Radek Doulik <radek.doulik@gmail.com>
2026-05-04 12:54:18 +0200

    Feedback
    Co-authored-by: Jan Kotas <jkotas@microsoft.com>

--- src/coreclr/vm/wasm/callhelpers-interp-to-managed.cpp
+++ src/coreclr/vm/wasm/callhelpers-interp-to-managed.cpp
@@ -9,7 +9,6 @@
 
 #include <callhelpers.hpp>
 #include <minipal/utils.h>
-#include "precode_portable.hpp"
 
 // Arguments are passed on the stack with each argument aligned to INTERP_STACK_SLOT_SIZE.
 #define ARG_ADDR(i) (pArgs + (i * INTERP_STACK_SLOT_SIZE))
