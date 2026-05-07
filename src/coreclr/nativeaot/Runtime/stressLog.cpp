26d858c21af231c7cb88d00a786cf85ed8de0e52
Jan Kotas <jkotas@microsoft.com>
2026-05-06 17:43:51 -0700

    Update src/coreclr/nativeaot/Runtime/stressLog.cpp

--- src/coreclr/nativeaot/Runtime/stressLog.cpp
+++ src/coreclr/nativeaot/Runtime/stressLog.cpp
@@ -95,7 +95,8 @@ void StressLog::Initialize(unsigned facilities,  unsigned level, unsigned maxByt
         return;
     }
 
-    bool success = minipal_mutex_init(&theLog.lock)
+    bool success;
+    success = minipal_mutex_init(&theLog.lock);
     _ASSERTE(success);
 
     g_pStressLog = &theLog;
