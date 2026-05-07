24d44ea6a4ecbf2aba3f455c95bf377e30a657af
Swapnil Gaikwad <swapnil.gaikwad@arm.com>
2026-05-07 19:20:35 +0100

    Fix formatting

--- src/coreclr/jit/emitarm64.cpp
+++ src/coreclr/jit/emitarm64.cpp
@@ -16242,7 +16242,8 @@ emitter::insExecutionCharacteristics emitter::getInsExecutionCharacteristics(ins
             }
             break;
 
-        case IF_PC_0A: // autia1716, autiasp, autib1716, autibsp, autiaz, pacia1716, paciasp, pacib1716, pacibsp, pacibz, paciaz, xpaclri
+        case IF_PC_0A: // autia1716, autiasp, autib1716, autibsp, autiaz, pacia1716, paciasp, pacib1716, pacibsp,
+                       // pacibz, paciaz, xpaclri
         case IF_PC_1A: // autiza, paciza, xpacd, xpaci
         case IF_PC_2A: // autia, pacia
             switch (ins)
