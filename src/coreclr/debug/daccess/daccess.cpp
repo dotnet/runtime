85d16387d1823351bcb0749a6dab239c8b7a5292
Juan Sebastian Hoyos Ayala <19413848+hoyosjs@users.noreply.github.com>
2026-05-10 23:56:07 -0700

    Cache debugger patches to speed up x64 stackwalk epilogue/prologue scanning (#125459)

--- src/coreclr/debug/daccess/daccess.cpp
+++ src/coreclr/debug/daccess/daccess.cpp
@@ -3274,6 +3274,9 @@ ClrDataAccess::Flush(void)
     //
     m_mdImports.Flush();
 
+    // Free cached patch entries for thread unwinding
+    m_patchCache.Flush();
+
     // Free instance memory.
     m_instances.Flush();
 
