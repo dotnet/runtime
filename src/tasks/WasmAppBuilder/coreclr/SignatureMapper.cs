f3f030cd2a58a27a2cc9de35f591b07cc60e9862
Radek Doulik <radek.doulik@gmail.com>
2026-05-15 12:49:10 +0200

    Put the structures into WasmAppBuilderTests namespace

--- src/tasks/WasmAppBuilder/coreclr/SignatureMapper.cs
+++ src/tasks/WasmAppBuilder/coreclr/SignatureMapper.cs
@@ -20,13 +20,14 @@ internal static class SignatureMapper
     // and PInvoke signatures.
     private static readonly Dictionary<string, int> s_knownStructSizes = new()
     {
-        ["PairStruct"] = 8, // Used by WBT tests
-        ["S"] = 8, // Used by WBT tests
         ["System.Runtime.CompilerServices.QCallModule"] = 8,
         ["System.Runtime.CompilerServices.QCallAssembly"] = 8,
         ["System.Runtime.CompilerServices.QCallTypeHandle"] = 8,
         ["System.GC+GCHeapHardLimitInfo"] = 64,
-        ["Test+S"] = 8, // Used by WBT tests
+        // Used by WBT tests
+        ["WasmAppBuilderTests.PairStruct"] = 8,
+        ["WasmAppBuilderTests.S"] = 8,
+        ["WasmAppBuilderTests.Test+S"] = 8,
     };
 
     internal static char? TypeToChar(Type t, LogAdapter log, out bool isByRefStruct, out int structSize, int depth = 0)
