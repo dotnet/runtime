a0661b6334840c3553aa48eca66449322ee5e522
Weihan Li <weihanli@outlook.com>
2026-05-15 16:47:05 +0800

    add extension for nullable support

---
+++ src/libraries/System.Threading.RateLimiting/src/System/Threading/RateLimiting/RateLimiterHelper.cs
@@ -0,0 +1,23 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics;
+
+namespace System.Threading.RateLimiting
+{
+    internal static class RateLimiterHelper
+    {
+        extension(Stopwatch)
+        {
+            public static TimeSpan? GetElapsedTime(long? startTimestamp)
+            {
+                if (startTimestamp is null)
+                {
+                    return null;
+                }
+
+                return Stopwatch.GetElapsedTime(startTimestamp.Value);
+            }
+        }
+    }
+}
