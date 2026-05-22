4d3ab07939f633c02b0d18a84a124140386758e6
Simon Rozsival <simon@rozsival.com>
2026-05-22 18:28:46 +0200

    Remove unnecessary namespace using

--- src/libraries/System.Security.Cryptography/tests/X509Certificates/ChainTests.cs
+++ src/libraries/System.Security.Cryptography/tests/X509Certificates/ChainTests.cs
@@ -4,7 +4,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates.Tests.Common;
 using System.Text;
 using System.Threading;
 using Test.Cryptography;
