93303cf10f9411b303d3cf4a01c049e83a2d5737
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2026-05-25 16:05:45 +0200

    JIT: Tail merge async suspensions
    This adds an optimization that tail merges suspension points when
    predecessors of joins end with async calls.

--- src/coreclr/jit/async.h
+++ src/coreclr/jit/async.h
@@ -119,6 +119,8 @@ public:
         return m_locals;
     }
 
+    static bool Equals(const ContinuationLayoutBuilder& a, const ContinuationLayoutBuilder& b);
+
     struct ContinuationLayout* Create();
 
     static ContinuationLayoutBuilder* CreateSharedLayout(Compiler*                                comp,
@@ -404,6 +406,10 @@ class AsyncTransformation
 
     CallDefinitionInfo CanonicalizeCallDefinition(BasicBlock* block, GenTreeCall* call, AsyncAnalysis* analyses);
 
+    const AsyncState* FindReusableSuspension(BasicBlock* block, GenTreeCall* call, const CallDefinitionInfo& defInfo, ContinuationLayoutBuilder* layoutBuilder, bool resumeReachable, VARSET_VALARG_TP mutatedSinceResumption);
+    bool IsReusableSuspension(const AsyncState* state, BasicBlock* block, GenTreeCall* call, const CallDefinitionInfo& defInfo, ContinuationLayoutBuilder* layoutBuilder, bool resumeReachable, VARSET_VALARG_TP mutatedSinceResumption);
+    void HandleReusedSuspension(BasicBlock* callBlock, GenTreeCall* call);
+
     BasicBlock* CreateSuspensionBlock(BasicBlock* block, unsigned stateNum);
     void        CreateSuspension(BasicBlock*                      callBlock,
                                  GenTreeCall*                     call,
@@ -418,8 +424,7 @@ class AsyncTransformation
                                              GenTree*                  prevContinuation,
                                              const ContinuationLayout& layout);
 
-    void                    FillInDataOnSuspension(GenTreeCall*                     call,
-                                                   const ContinuationLayout&        layout,
+    void                    FillInDataOnSuspension(const ContinuationLayout&        layout,
                                                    const ContinuationLayoutBuilder& subLayout,
                                                    BasicBlock*                      suspendBB,
                                                    VARSET_VALARG_TP                 mutatedSinceResumption,
@@ -443,6 +448,7 @@ class AsyncTransformation
                                                            const CallDefinitionInfo& callDefInfo,
                                                            BasicBlock*               suspendBB,
                                                            BasicBlock**              remainder);
+
     BasicBlock*             CreateResumptionBlock(BasicBlock* remainder, unsigned stateNum);
     void                    CreateResumption(BasicBlock*                      callBlock,
                                              GenTreeCall*                     call,
