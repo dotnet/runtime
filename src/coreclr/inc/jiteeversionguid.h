54cfeb453970e794043c71448a60a39b4fee3a38
Steve <hez2010@outlook.com>
2026-05-28 09:53:58 +0000

    JIT: Unify to instParamLookup and improve GVM test coverage (#126947)
    Extracted from #123323. This PR is the first step in splitting the
    shared GVM devirtualization work.
    
    It does two things:
    
    - expands the generic virtual method test coverage so the later
    enablement PRs have good coverage
    - refactors devirtualization a bit to replace multiple ad-hoc flags in
    `CORINFO_DEVIRTUALIZATION_INFO` with a single `instParamLookup`
    
    Contributes to #112596

--- src/coreclr/inc/jiteeversionguid.h
+++ src/coreclr/inc/jiteeversionguid.h
@@ -37,11 +37,11 @@
 
 #include <minipal/guid.h>
 
-constexpr GUID JITEEVersionIdentifier = { /* ab90f94d-0a8e-4718-b9cb-f7f9bcdf6393 */
-    0xab90f94d,
-    0x0a8e,
-    0x4718,
-    {0xb9, 0xcb, 0xf7, 0xf9, 0xbc, 0xdf, 0x63, 0x93}
+constexpr GUID JITEEVersionIdentifier = { /* fc5f63e7-921b-4091-b920-8df8d7b872c1 */
+    0xfc5f63e7,
+    0x921b,
+    0x4091,
+    {0xb9, 0x20, 0x8d, 0xf8, 0xd7, 0xb8, 0x72, 0xc1}
   };
 
 #endif // JIT_EE_VERSIONING_GUID_H
