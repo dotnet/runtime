2ee60481d24cd6d9ae5e13da0a6686c42840dbf7
SingleAccretion <AccretionMail@yandex.ru>
2026-05-28 20:44:46 +0300

    Revert "[Wasm RyuJIT] Move genHomeRegisterParams' output out of the prolog (#125398)"
    This reverts commit 338e288bd9535217764f4146f2fe89707818ac97.

--- src/coreclr/jit/emit.cpp
+++ src/coreclr/jit/emit.cpp
@@ -9929,6 +9929,7 @@ void emitter::emitInitIG(insGroup* ig)
        sure we act the same in non-DEBUG builds.
     */
 
+    ig->igData   = nullptr;
     ig->igSize   = 0;
     ig->igGCregs = RBM_NONE;
     ig->igInsCnt = 0;
@@ -9946,8 +9947,6 @@ void emitter::emitInitIG(insGroup* ig)
     // Explicitly call init, since IGs don't actually have a constructor.
     ig->igBlocks.jitstd::list<BasicBlock*>::init(m_compiler->getAllocator(CMK_DebugOnly));
 #endif
-
-    ig->igData = nullptr;
 }
 
 /*****************************************************************************
