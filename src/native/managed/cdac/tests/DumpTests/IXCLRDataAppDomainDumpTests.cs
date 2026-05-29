bb472d3b40739183b0d9f68f56fa6ac2270b79a5
rcj1 <rachel.jarvi@gmail.com>
2026-05-28 17:28:09 -0700

    fix tests

--- src/native/managed/cdac/tests/DumpTests/IXCLRDataAppDomainDumpTests.cs
+++ src/native/managed/cdac/tests/DumpTests/IXCLRDataAppDomainDumpTests.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
+using System.Linq;
 using Microsoft.Diagnostics.DataContractReader.Contracts;
 using Microsoft.Diagnostics.DataContractReader.Legacy;
 using Xunit;
