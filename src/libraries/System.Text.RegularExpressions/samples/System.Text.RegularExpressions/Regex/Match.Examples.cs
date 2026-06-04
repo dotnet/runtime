a6fbdd5639ae140769484955fac0cc65620a9637
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2026-06-04 10:32:14 -0700

    move remarks back from conceptual docs repo

---
+++ src/libraries/System.Text.RegularExpressions/samples/System.Text.RegularExpressions/Regex/Match.Examples.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace System.Text.RegularExpressions.Examples
+{
+    public class MatchExample
+    {
+        public static void Run()
+        {
+            #region Match
+            string input = "Zip code: 98052";
+            var regex = new Regex(@"(?<=Zip code: )\d{5}");
+            Match match = regex.Match(input, 5);
+            if (match.Success)
+                Console.WriteLine($"Match found: {match.Value}");
+
+            // This code prints the following output:
+            //
+            // Match found: 98052
+            #endregion
+        }
+    }
+}
