4c6a46c13f5daa19f674aa97cfd2b455d2b7bb4a
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2026-06-05 15:33:05 +0800

    Remove stale comment

--- src/coreclr/pal/src/init/pal.cpp
+++ src/coreclr/pal/src/init/pal.cpp
@@ -611,8 +611,7 @@ Function:
   PAL_InitializeCoreCLR
 
 Abstract:
-  A replacement for PAL_Initialize when loading CoreCLR. Instead of taking a command line (which CoreCLR
-  instances aren't given anyway) the path into which the CoreCLR is installed is supplied instead.
+  A replacement for PAL_Initialize when loading CoreCLR.
 
   This routine also makes sure the psuedo dynamic libraries PALRT and mscorwks have their initialization
   methods called.
