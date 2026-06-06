fb4bc3448b54eed39f13a71debfe3860b8867ad5
Viviana Dueñas Chavez <vivianad@microsoft.com>
2026-06-05 18:09:59 -0700

    Remove deferred DataAnnotations async validation (ValidateDataAnnotationsAsync, DataAnnotationValidateOptionsAsync)

--- src/libraries/Microsoft.Extensions.Options.DataAnnotations/src/OptionsBuilderDataAnnotationsExtensions.cs
+++ src/libraries/Microsoft.Extensions.Options.DataAnnotations/src/OptionsBuilderDataAnnotationsExtensions.cs
@@ -24,25 +24,5 @@ namespace Microsoft.Extensions.DependencyInjection
             optionsBuilder.Services.AddSingleton<IValidateOptions<TOptions>>(new DataAnnotationValidateOptions<TOptions>(optionsBuilder.Name));
             return optionsBuilder;
         }
-
-#if NET11_0_OR_GREATER
-        /// <summary>
-        /// Register this options instance for asynchronous validation of its DataAnnotations.
-        /// </summary>
-        /// <typeparam name="TOptions">The options type to be configured.</typeparam>
-        /// <param name="optionsBuilder">The options builder to add the services to.</param>
-        /// <returns>The <see cref="OptionsBuilder{TOptions}"/> so that additional calls can be chained.</returns>
-        /// <remarks>
-        /// Async validators run only at startup when used with <c>ValidateOnStart</c>.
-        /// <see cref="IOptionsMonitor{TOptions}"/> reload validation uses only synchronous validators.
-        /// </remarks>
-        [RequiresUnreferencedCode("Uses DataAnnotationValidateOptionsAsync which is unsafe given that the options type passed in when calling Validate cannot be statically analyzed so its" +
-            " members may be trimmed.")]
-        public static OptionsBuilder<TOptions> ValidateDataAnnotationsAsync<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicProperties | DynamicallyAccessedMemberTypes.NonPublicProperties)] TOptions>(this OptionsBuilder<TOptions> optionsBuilder) where TOptions : class
-        {
-            optionsBuilder.Services.AddSingleton<IAsyncValidateOptions<TOptions>>(new DataAnnotationValidateOptionsAsync<TOptions>(optionsBuilder.Name));
-            return optionsBuilder;
-        }
-#endif
     }
 }
