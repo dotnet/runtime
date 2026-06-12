f6f137a729658db15f3be6f3ba1eb14ee2ff552a
Swapnil Gaikwad <swapnil.gaikwad@arm.com>
2026-06-12 16:37:20 +0100

    Revert cdac unwinder changes

--- src/native/managed/cdac/Microsoft.Diagnostics.DataContractReader.Contracts/Contracts/StackWalk/Context/ARM64/ARM64Unwinder.cs
+++ src/native/managed/cdac/Microsoft.Diagnostics.DataContractReader.Contracts/Contracts/StackWalk/Context/ARM64/ARM64Unwinder.cs
@@ -763,7 +763,8 @@ internal class ARM64Unwinder(Target target)
                     return false;
                 }
 
-                context.Lr &= 0x0000FFFFFFFFFFFF;
+                // TODO-PAC: Enable processing PAC-enabled return address
+                // context.Lr &= 0x0000FFFFFFFFFFFF;
 
                 //
                 // TODO: Implement support for UnwindFlags RTL_VIRTUAL_UNWIND2_VALIDATE_PAC.
