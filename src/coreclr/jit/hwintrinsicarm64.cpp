35eb3b6ab6b6b2f3a184e525eaf24e704c2c926f
Jonathan Davies <jonathan.davies@arm.com>
2026-06-17 08:41:24 +0000

    arm64: Improve predicate instruction usage

--- src/coreclr/jit/hwintrinsicarm64.cpp
+++ src/coreclr/jit/hwintrinsicarm64.cpp
@@ -770,15 +770,31 @@ GenTree* Compiler::impSpecialIntrinsic(NamedIntrinsic        intrinsic,
             break;
         }
 
-        case NI_AdvSimd_BitwiseClear:
         case NI_Sve_BitwiseClear:
+        {
+            assert(sig->numArgs == 2);
+
+            op2 = impSIMDPopStack();
+            op1 = impSIMDPopStack();
+
+            // We don't want to support creating AND_NOT nodes prior to LIR
+            // as it can break important optimizations. We'll produce this
+            // in lowering instead so decompose into the individual operations
+            // on import
+
+            op2     = gtNewSimdUnOpNode(GT_NOT, retType, op2, simdBaseType, simdSize);
+            retNode = gtNewSimdBinOpNode(GT_AND, retType, op1, op2, simdBaseType, simdSize);
+            break;
+        }
+
+        case NI_AdvSimd_BitwiseClear:
         case NI_Vector64_AndNot:
         case NI_Vector128_AndNot:
         {
             assert(sig->numArgs == 2);
 
             // We don't want to support creating AND_NOT nodes prior to LIR
-            // as it can break important optimizations. We'll produces this
+            // as it can break important optimizations. We'll produce this
             // in lowering instead so decompose into the individual operations
             // on import
 
--- src/coreclr/jit/hwintrinsiccodegenarm64.cpp
+++ src/coreclr/jit/hwintrinsiccodegenarm64.cpp
@@ -2785,6 +2785,14 @@ void CodeGen::genHWIntrinsic(GenTreeHWIntrinsic* node)
                 break;
             }
 
+            case NI_Sve_And_Predicates:
+            case NI_Sve_BitwiseClear_Predicates:
+            {
+                assert(intrin.numOperands == 2);
+                GetEmitter()->emitInsSve_R_R_R_R(ins, emitSize, targetReg, op1Reg, op1Reg, op2Reg, INS_OPTS_SCALABLE_B);
+                break;
+            }
+
             case NI_Sve_CreateBreakAfterPropagateMask:
             case NI_Sve_CreateBreakBeforePropagateMask:
             case NI_Sve_ConditionalSelect_Predicates:
