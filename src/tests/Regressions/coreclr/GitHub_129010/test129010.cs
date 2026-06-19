1936ed2b3cd63423f90d3355aa1014fcc790eded
Jan Vorlicek <janvorli@microsoft.com>
2026-06-19 18:45:29 +0200

    Regression test

---
+++ src/tests/Regressions/coreclr/GitHub_129010/test129010.cs
@@ -0,0 +1,103 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Runtime.CompilerServices;
+using Xunit;
+
+public class Program
+{
+    static Exception ex = new Exception();
+    static byte[] data = null;
+    static int count = 0;
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private static void Alloc()
+    {
+        data = new byte[65536];
+        if (count % 256 == 255)
+        {
+            GC.Collect();
+        }
+        count++;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static void Test()
+    {
+        try
+        {
+            throw ex;
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static object Dummy(object a)
+    {
+        return a;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static void Finally(object a)
+    {
+        // This puts object a into the first nonvolatile register
+        object b = Dummy(a);
+        Alloc();
+        Dummy(b);
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static void Test2(object a)
+    {
+        try
+        {
+            try
+            {
+                Test();
+            }
+            finally
+            {
+                Finally(a);
+            }
+        }
+        catch (Exception)
+        {
+        }
+    }
+
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private static object Foo(int x)
+    {
+        return x.ToString();
+    }
+
+    static int sum = 0;
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static void Verify(object a)
+    {
+        sum += ((string)a).Length;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public static void Test3()
+    {
+        // This puts object a into the first nonvolatile register
+        object a = Foo(5);
+        Test2(a);
+        Verify(a);
+    }
+
+    [Fact]
+    public static void TestEntryPoint()
+    {
+        for (int i = 0; i < 1000000; i++)
+        {
+            Test3();
+        }
+    }
+}
