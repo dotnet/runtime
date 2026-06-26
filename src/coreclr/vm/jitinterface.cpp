546e2502d680af89780f107af368e98cad5aae4d
Giridhar Trivedi <giritrivedi@gmail.com>
2026-06-26 07:05:13 +0000

    List the functions to be jitted

--- src/coreclr/vm/jitinterface.cpp
+++ src/coreclr/vm/jitinterface.cpp
@@ -12401,6 +12401,70 @@ static CorJitResult CompileMethodWithEtwWrapper(EEJitManager *jitMgr,
 }
 #endif // FEATURE_INTERPRETER
 
+#if defined(TARGET_S390X)
+//
+// Helper function to check if a function name should use interpreter fallback
+//
+static bool isJitted(MethodDesc* ftnDesc,const char* ftnName)
+{
+    // List of functions to be jitted and fall back if they fail to jit
+    static const char* JittedFunctions[] = {
+	    "foo",
+            "s390xHw",
+            "oneArg",
+            "twoArgs",
+            "addTwo",
+    };
+
+    // 2D array for class name and function name combinations to exclude from JIT compilation
+    struct JitExclusionEntry
+    {
+        const char* className;
+        const char* functionName;
+    };
+
+    static const JitExclusionEntry jitExclusionList[] = {
+       { "System.Diagnostics.Tracing.EventSource", "Initialize" },
+       { "System.Collections.Generic.Dictionary`2[__Canon,__Canon]", ".ctor"},
+       { "System.Collections.Generic.NonRandomizedStringEqualityComparer", ".ctor"},
+       { "System.Collections.Generic.List`1[__Canon]", ".ctor"},
+       { "System.Runtime.CompilerServices.QCallTypeHandle", ".ctor"},
+       { "System.Resources.ResourceManager", ".ctor"}, //Assertion failed 'Unhandled TARGET in getReturnTypeForStruct'
+       { "RuntimeTypeCache", ".ctor"},
+       { "System.Runtime.CompilerServices.QCallModule", ".ctor"},
+       { "System.Runtime.CompilerServices.ObjectHandleOnStack", ".ctor"},
+       { "System.Reflection.MetadataImport", ".ctor"},
+       { "System.Runtime.CompilerServices.DefaultInterpolatedStringHandler", ".ctor"}, //Assertion failed 'Unhandled TARGET in getReturnTypeForStruct'
+       { "System.Runtime.CompilerServices.ConditionalWeakTable`2[__Canon,__Canon]", ".ctor"},
+       { "Container[__Canon,__Canon]", ".ctor"},
+    };
+
+    const size_t numExclusions = sizeof(jitExclusionList) / sizeof(jitExclusionList[0]);
+
+    // Check if the current class/function combination should be excluded from JIT
+    for (size_t i = 0; i < numExclusions; i++)
+    {
+        if (!strcmp(ftnDesc->m_pszDebugClassName, jitExclusionList[i].className) &&
+            !strcmp(ftnName, jitExclusionList[i].functionName))
+        {
+            return false;
+        }
+    }
+    
+    const size_t numFunctions = sizeof(JittedFunctions) / sizeof(JittedFunctions[0]);
+
+    for (size_t i = 0; i < numFunctions; i++)
+    {
+        if (!strcmp(ftnName, JittedFunctions[i]))
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+#endif 
+
 //
 // Helper function because can't have dtors in BEGIN_SO_TOLERANT_CODE.
 //
@@ -12452,17 +12516,16 @@ CorJitResult invokeCompileMethodHelper(EEJitManager *jitMgr,
     const char* ftnName = ftnDesc->GetName();
 
     forceInterpreter = true;
-    if (!strcmp(ftnName, "foo") ||
-        !strcmp(ftnName, "s390xHw") ||
-        !strcmp(ftnName, "oneArg") ||
-        !strcmp(ftnName, "twoArgs") ||
-        !strcmp(ftnName, "addTwo"))
+    if (isJitted(ftnDesc,ftnName))
     {
-        printf("Function name is %s\n", ftnName);
+        printf("Jitted Function name is %s\n", ftnName);
         interpreterFallback = true;
     }
     else
+    {
+        printf("Interpreted Function name is %s\n", ftnName);
 	interpreterFallback = false;
+    }
 #endif
 
     if (interpreterFallback == false)
