532df3a1d80d08420392611368cce29161d63bc4
Swapnil Gaikwad <swapnil.gaikwad@arm.com>
2026-06-26 16:37:48 +0100

    Disable hardcoding pauth on Windows

--- src/coreclr/vm/cdacplatformmetadata.cpp
+++ src/coreclr/vm/cdacplatformmetadata.cpp
@@ -12,7 +12,7 @@ void CDacPlatformMetadata::Init()
 {
 #if defined(TARGET_ARM)
     (&g_cdacPlatformMetadata)->codePointerFlags = CDacCodePointerFlags::HasArm32ThumbBit;
-#elif defined(TARGET_ARM64)
+#elif defined(TARGET_ARM64) && !defined(TARGET_WINDOWS)
     CLRConfig::ConfigDWORDInfo jitPacEnabledConfig { W("JitPacEnabled"), 1, CLRConfig::LookupOptions::Default };
     (&g_cdacPlatformMetadata)->codePointerFlags =
         CLRConfig::GetConfigValue(jitPacEnabledConfig) != 0
