dotnet/runtime

commit a3dc1337a17a3de6aefe76e465f85ebf5ba6ae5e

Remove helper frames from MetadataImport class FCalls (#100939)
Co-authored-by: Jan Kotas <jkotas@microsoft.com>

src/coreclr/vm/ecalllist.h
@@ -156,31 +156,30 @@ FCFuncStart(gCOMTypeHandleFuncs)
 FCFuncEnd()
 
 FCFuncStart(gMetaDataImport)
-    FCFuncElement("_GetDefaultValue", MetaDataImport::GetDefaultValue)
-    FCFuncElement("_GetName", MetaDataImport::GetName)
-    FCFuncElement("_GetUserString", MetaDataImport::GetUserString)
-    FCFuncElement("_GetScopeProps", MetaDataImport::GetScopeProps)
-    FCFuncElement("_GetClassLayout", MetaDataImport::GetClassLayout)
-    FCFuncElement("_GetSignatureFromToken", MetaDataImport::GetSignatureFromToken)
-    FCFuncElement("_GetNamespace", MetaDataImport::GetNamespace)
-    FCFuncElement("_GetEventProps", MetaDataImport::GetEventProps)
-    FCFuncElement("_GetFieldDefProps", MetaDataImport::GetFieldDefProps)
-    FCFuncElement("_GetPropertyProps", MetaDataImport::GetPropertyProps)
-    FCFuncElement("_GetParentToken", MetaDataImport::GetParentToken)
-    FCFuncElement("_GetParamDefProps", MetaDataImport::GetParamDefProps)
-    FCFuncElement("_GetGenericParamProps", MetaDataImport::GetGenericParamProps)
+    FCFuncElement("GetDefaultValue", MetaDataImport::GetDefaultValue)
+    FCFuncElement("GetName", MetaDataImport::GetName)
+    FCFuncElement("GetUserString", MetaDataImport::GetUserString)
+    FCFuncElement("GetScopeProps", MetaDataImport::GetScopeProps)
+    FCFuncElement("GetClassLayout", MetaDataImport::GetClassLayout)
+    FCFuncElement("GetSignatureFromToken", MetaDataImport::GetSignatureFromToken)
+    FCFuncElement("GetNamespace", MetaDataImport::GetNamespace)
+    FCFuncElement("GetEventProps", MetaDataImport::GetEventProps)
+    FCFuncElement("GetFieldDefProps", MetaDataImport::GetFieldDefProps)
+    FCFuncElement("GetPropertyProps", MetaDataImport::GetPropertyProps)
+    FCFuncElement("GetParentToken", MetaDataImport::GetParentToken)
+    FCFuncElement("GetParamDefProps", MetaDataImport::GetParamDefProps)
+    FCFuncElement("GetGenericParamProps", MetaDataImport::GetGenericParamProps)
 
-    FCFuncElement("_Enum", MetaDataImport::Enum)
-    FCFuncElement("_GetMemberRefProps", MetaDataImport::GetMemberRefProps)
-    FCFuncElement("_GetCustomAttributeProps", MetaDataImport::GetCustomAttributeProps)
-    FCFuncElement("_GetFieldOffset", MetaDataImport::GetFieldOffset)
+    FCFuncElement("GetMemberRefProps", MetaDataImport::GetMemberRefProps)
+    FCFuncElement("GetCustomAttributeProps", MetaDataImport::GetCustomAttributeProps)
+    FCFuncElement("GetFieldOffset", MetaDataImport::GetFieldOffset)
 
-    FCFuncElement("_GetSigOfFieldDef", MetaDataImport::GetSigOfFieldDef)
-    FCFuncElement("_GetSigOfMethodDef", MetaDataImport::GetSigOfMethodDef)
-    FCFuncElement("_GetFieldMarshal", MetaDataImport::GetFieldMarshal)
-    FCFuncElement("_GetPInvokeMap", MetaDataImport::GetPinvokeMap)
-    FCFuncElement("_IsValidToken", MetaDataImport::IsValidToken)
-    FCFuncElement("_GetMarshalAs", MetaDataImport::GetMarshalAs)
+    FCFuncElement("GetSigOfFieldDef", MetaDataImport::GetSigOfFieldDef)
+    FCFuncElement("GetSigOfMethodDef", MetaDataImport::GetSigOfMethodDef)
+    FCFuncElement("GetFieldMarshal", MetaDataImport::GetFieldMarshal)
+    FCFuncElement("GetPInvokeMap", MetaDataImport::GetPInvokeMap)
+    FCFuncElement("IsValidToken", MetaDataImport::IsValidToken)
+    FCFuncElement("GetMarshalAs", MetaDataImport::GetMarshalAs)
 FCFuncEnd()
 
 FCFuncStart(gSignatureNative)

src/coreclr/vm/managedmdimport.hpp
@@ -24,95 +24,56 @@ typedef struct
     void* m_array;
 } ConstArray;
 
-typedef struct
-{
-    I4Array * largeResult;
-    int length;
-    int smallResult[16];
-} MetadataEnumResult;
-
-#define MDDecl0(RET, NAME) static FCDECL1(RET, NAME, IMDInternalImport* pScope)
-#define MDDecl1(RET, NAME, arg0) static FCDECL2(RET, NAME, IMDInternalImport* pScope, arg0)
-#define MDDecl2(RET, NAME, arg0, arg1) static FCDECL3(RET, NAME, IMDInternalImport* pScope, arg0, arg1)
-#define MDDecl3(RET, NAME, arg0, arg1, arg2) static FCDECL4(RET, NAME, IMDInternalImport* pScope, arg0, arg1, arg2)
-#define MDDecl4(RET, NAME, arg0, arg1, arg2, arg3) static FCDECL5(RET, NAME, IMDInternalImport* pScope, arg0, arg1, arg2, arg3)
-#define MDDecl5(RET, NAME, arg0, arg1, arg2, arg3, arg4) static FCDECL6(RET, NAME, IMDInternalImport* pScope, arg0, arg1, arg2, arg3, arg4)
-#define MDDecl6(RET, NAME, arg0, arg1, arg2, arg3, arg4, arg5) static FCDECL7(RET, NAME, IMDInternalImport* pScope, arg0, arg1, arg2, arg3, arg4, arg5)
-#define MDDecl7(RET, NAME, arg0, arg1, arg2, arg3, arg4, arg5, arg6) static FCDECL8(RET, NAME, IMDInternalImport* pScope, arg0, arg1, arg2, arg3, arg4, arg5, arg6)
-#define MDDecl8(RET, NAME, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7) static FCDECL9(RET, NAME, IMDInternalImport* pScope, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7)
-#define MDDecl9(RET, NAME, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8) static FCDECL10(RET, NAME, IMDInternalImport* pScope, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8)
-#define MDDecl10(RET, NAME, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9) static FCDECL11(RET, NAME, IMDInternalImport* pScope, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9)
-
-#define MDImpl0(RET, NAME) FCIMPL1(RET, NAME, IMDInternalImport* pScope)
-#define MDImpl1(RET, NAME, arg0) FCIMPL2(RET, NAME, IMDInternalImport* pScope, arg0)
-#define MDImpl2(RET, NAME, arg0, arg1) FCIMPL3(RET, NAME, IMDInternalImport* pScope, arg0, arg1)
-#define MDImpl3(RET, NAME, arg0, arg1, arg2) FCIMPL4(RET, NAME, IMDInternalImport* pScope, arg0, arg1, arg2)
-#define MDImpl4(RET, NAME, arg0, arg1, arg2, arg3) FCIMPL5(RET, NAME, IMDInternalImport* pScope, arg0, arg1, arg2, arg3)
-#define MDImpl5(RET, NAME, arg0, arg1, arg2, arg3, arg4) FCIMPL6(RET, NAME, IMDInternalImport* pScope, arg0, arg1, arg2, arg3, arg4)
-#define MDImpl6(RET, NAME, arg0, arg1, arg2, arg3, arg4, arg5) FCIMPL7(RET, NAME, IMDInternalImport* pScope, arg0, arg1, arg2, arg3, arg4, arg5)
-#define MDImpl7(RET, NAME, arg0, arg1, arg2, arg3, arg4, arg5, arg6) FCIMPL8(RET, NAME, IMDInternalImport* pScope, arg0, arg1, arg2, arg3, arg4, arg5, arg6)
-#define MDImpl8(RET, NAME, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7) FCIMPL9(RET, NAME, IMDInternalImport* pScope, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7)
-#define MDImpl9(RET, NAME, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8) FCIMPL10(RET, NAME, IMDInternalImport* pScope, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8)
-#define MDImpl10(RET, NAME, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9) FCIMPL11(RET, NAME, IMDInternalImport* pScope, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9)
-
 class MetaDataImport
 {
 public:
-    //
-    // GetXXXProps
-    //
-    MDDecl1(void, GetScopeProps, GUID* pmvid);
-    MDDecl4(void, GetTypeDefProps, mdTypeDef td, STRINGREF* pszTypeDef, DWORD* pdwTypeDefFlags, mdToken* ptkExtends);
-    MDDecl2(void, GetMemberRefProps, mdMemberRef mr, ConstArray* ppvSigBlob);
+    static FCDECL2(HRESULT, GetScopeProps, IMDInternalImport* pScope, GUID* pmvid);
+    static FCDECL3(HRESULT, GetMemberRefProps, IMDInternalImport* pScope, mdMemberRef mr, ConstArray* ppvSigBlob);
 
+    static FCDECL4(HRESULT, GetCustomAttributeProps, IMDInternalImport* pScope, mdCustomAttribute cv, mdToken* ptkType, ConstArray* ppBlob);
 
-    ////
-    //// EnumXXX
-    ////
-    MDDecl3(void, Enum, mdToken type, mdToken tkParent, MetadataEnumResult * pResult);
-    MDDecl3(void, GetCustomAttributeProps, mdCustomAttribute cv, mdToken* ptkType, ConstArray* ppBlob);
+    static FCDECL6(HRESULT, GetDefaultValue, IMDInternalImport* pScope, mdToken tk, INT64* pDefaultValue, LPCWSTR* pStringValue, INT32* pLength, INT32* pCorElementType);
+    static FCDECL3(HRESULT, GetName, IMDInternalImport* pScope, mdToken tk, LPCSTR* pszName);
+    static FCDECL4(HRESULT, GetUserString, IMDInternalImport* pScope, mdToken tk, LPCWSTR* pszName, ULONG* pCount);
+    static FCDECL3(HRESULT, GetNamespace, IMDInternalImport* pScope, mdToken tk, LPCSTR* pszName);
+    static FCDECL3(HRESULT, GetParentToken, IMDInternalImport* pScope, mdToken tk, mdToken* ptk);
+    static FCDECL4(HRESULT, GetParamDefProps, IMDInternalImport* pScope, mdToken tk, INT32* pSequence, INT32* pAttributes);
+    static FCDECL5(HRESULT, GetPInvokeMap, IMDInternalImport* pScope, mdToken tk, DWORD* pMappingFlags, LPCSTR* pszImportName, LPCSTR* pszImportDll);
 
-    ////
-    //// Misc
-    ////
+    static FCDECL4(HRESULT, GetClassLayout, IMDInternalImport* pScope, mdTypeDef td, DWORD* pdwPackSize, ULONG* pulClassSize);
+    static FCDECL5(HRESULT, GetFieldOffset, IMDInternalImport* pScope, mdTypeDef td, mdFieldDef target, DWORD* pdwFieldOffsetGetFieldOffset, CLR_BOOL* found);
 
-    MDDecl4(Object *, GetDefaultValue, mdToken tk, INT64* pDefaultValue, INT32* pLength, INT32* pCorElementType);
-    MDDecl2(void, GetName, mdToken tk, LPCSTR* pszName);
-    MDDecl3(void, GetUserString, mdToken tk, LPCSTR* pszName, ULONG* pCount);
-    MDDecl2(void, GetNamespace, mdToken tk, LPCSTR* pszName);
-    MDDecl2(void, GetParentToken, mdToken tk, mdToken* ptk);
-    MDDecl3(void, GetParamDefProps, mdToken tk, INT32* pSequence, INT32* pAttributes);
-    MDDecl4(void, GetPinvokeMap, mdToken tk, DWORD* pMappingFlags, LPCSTR* pszImportName, LPCSTR* pszImportDll);
+    static FCDECL4(HRESULT, GetEventProps, IMDInternalImport* pScope, mdToken tk, LPCSTR* pszName, INT32 *pdwEventFlags);
+    static FCDECL3(HRESULT, GetGenericParamProps, IMDInternalImport* pScope, mdToken tk, DWORD* pAttributes);
+    static FCDECL3(HRESULT, GetFieldDefProps, IMDInternalImport* pScope, mdToken tk, INT32 *pdwFieldFlags);
+    static FCDECL5(HRESULT, GetPropertyProps, IMDInternalImport* pScope, mdToken tk, LPCSTR* pszName, INT32 *pdwPropertyFlags, ConstArray* ppvSigBlob);
 
-    MDDecl3(void, GetClassLayout, mdTypeDef td, DWORD* pdwPackSize, ULONG* pulClassSize);
-    MDDecl3(FC_BOOL_RET, GetFieldOffset, mdTypeDef td, mdFieldDef target, DWORD* pdwFieldOffset);
+    static FCDECL3(HRESULT, GetSignatureFromToken, IMDInternalImport* pScope, mdToken tk, ConstArray* pSig);
+    static FCDECL3(HRESULT, GetSigOfFieldDef, IMDInternalImport* pScope, mdToken tk, ConstArray* pMarshalInfo);
+    static FCDECL3(HRESULT, GetSigOfMethodDef, IMDInternalImport* pScope, mdToken tk, ConstArray* pMarshalInfo);
+    static FCDECL3(HRESULT, GetFieldMarshal, IMDInternalImport* pScope, mdToken tk, ConstArray* pMarshalInfo);
+    static FCDECL2(FC_BOOL_RET, IsValidToken, IMDInternalImport* pScope, mdToken tk);
 
-    MDDecl3(void, GetEventProps, mdToken tk, LPCSTR* pszName, INT32 *pdwEventFlags);
-    MDDecl2(void, GetGenericParamProps, mdToken tk, DWORD* pAttributes);
-    MDDecl2(void, GetFieldDefProps, mdToken tk, INT32 *pdwFieldFlags);
-    MDDecl4(void, GetPropertyProps, mdToken tk, LPCSTR* pszName, INT32 *pdwPropertyFlags, ConstArray* ppvSigBlob);
-
-    MDDecl2(void, GetSignatureFromToken, mdToken tk, ConstArray* pSig);
-    MDDecl2(void, GetSigOfFieldDef, mdToken tk, ConstArray* pMarshalInfo);
-    MDDecl2(void, GetSigOfMethodDef, mdToken tk, ConstArray* pMarshalInfo);
-    MDDecl2(void, GetFieldMarshal, mdToken tk, ConstArray* pMarshalInfo);
-    MDDecl2(mdParamDef, GetParamForMethodIndex, mdMethodDef md, ULONG ulParamSeq);
-    MDDecl1(FC_BOOL_RET, IsValidToken, mdToken tk);
-    MDDecl1(mdTypeDef, GetNestedClassProps, mdTypeDef tdNestedClass);
-    MDDecl1(ULONG, GetNativeCallConvFromSig, ConstArray sig);
-
-    static FCDECL11(void, GetMarshalAs,
-        BYTE*           pvNativeType,
-        ULONG           cbNativeType,
-        INT32*          unmanagedType,
-        INT32*          safeArraySubType,
-        STRINGREF*      safeArrayUserDefinedSubType,
-        INT32*          arraySubType,
-        INT32*          sizeParamIndex,
-        INT32*          sizeConst,
-        STRINGREF*      marshalType,
-        STRINGREF*      marshalCookie,
-        INT32*          iidParamIndex);
+    static FCDECL11(FC_BOOL_RET, GetMarshalAs,
+        BYTE*   pvNativeType,
+        ULONG   cbNativeType,
+        INT32*  unmanagedType,
+        INT32*  safeArraySubType,
+        LPUTF8* safeArrayUserDefinedSubType,
+        INT32*  arraySubType,
+        INT32*  sizeParamIndex,
+        INT32*  sizeConst,
+        LPUTF8* marshalType,
+        LPUTF8* marshalCookie,
+        INT32*  iidParamIndex);
 };
+
+extern "C" void QCALLTYPE MetadataImport_Enum(
+    IMDInternalImport* pScope,
+    mdToken type,
+    mdToken tkParent,
+    INT32* length,
+    INT32* shortResult,
+    QCall::ObjectHandleOnStack longResult);
 
 #endif

src/coreclr/vm/qcallentrypoints.cpp
@@ -333,6 +333,7 @@ static const Entry s_QCall[] =
     DllImportEntry(Monitor_Pulse)
     DllImportEntry(Monitor_PulseAll)
     DllImportEntry(Monitor_GetLockContentionCount)
+    DllImportEntry(MetadataImport_Enum)
     DllImportEntry(ReflectionInvocation_RunClassConstructor)
     DllImportEntry(ReflectionInvocation_RunModuleConstructor)
     DllImportEntry(ReflectionInvocation_CompileMethod)

commit 62582ba8b5e27dd02ac86af64fa747966aca6319

[release/10.0] Fix TypeLoadException in GetMarshalAs when SafeArray has zero-length user-defined type name (#129899)
Backport of #124408 to release/10.0

## Customer Impact

- [x] Customer reported
- [ ] Found internally

Customer reported in https://github.com/dotnet/runtime/issues/124346.
Getting the `CustomAttributes` of a `ParameterInfo` throws
`TypeLoadException` when consuming tlbimp-generated COM interop
assemblies (e.g. the `Interop.MFilesAPI` NuGet package).

A backport to .NET 10.0 was requested in
https://github.com/dotnet/runtime/issues/129889. This is already fixed
in .NET 11.

`MetadataImport.GetMarshalAs` returned raw byte pointers to managed code
without associated lengths. When a `MarshalAs` blob contains
`NATIVE_TYPE_SAFEARRAY` with a zero-length user-defined type name
(common in tlbimp-generated COM interop assemblies), the managed side
used `CreateReadOnlySpanFromNullTerminated` to read these pointers, but
the strings in the metadata blob are length-prefixed and NOT
null-terminated, so this read garbage memory, producing a garbled string
that failed type resolution with `TypeLoadException`.

The fix passes string byte counts alongside the raw pointers, converts
`GetMarshalAs` from an FCALL to a QCall, removes overly strict asserts
that forbade trailing bytes (which ECMA-335 permits and tlbimp
produces), and adds a regression test.

Fixes https://github.com/dotnet/runtime/issues/124346
Fixes https://github.com/dotnet/runtime/issues/129889

## Regression

- [x] Yes
- [ ] No

Regression from commit a3dc1337 (.NET 9), which switched `GetMarshalAs`
from returning managed strings to returning raw byte pointers.

## Testing

Automated regression test added (`MarshalAsAttributeTests.cs`) that
builds a PE via `PersistedAssemblyBuilder` with a FieldMarshal blob
reproducing the dangling pointer read.

## Risk

Low. Scoped to metadata blob parsing in `GetMarshalAs`; the bounded-span
reads and assert removals only affect the SAFEARRAY/CUSTOMMARSHALER

src/coreclr/System.Private.CoreLib/src/System/Reflection/MdImport.cs
@@ -227,18 +227,22 @@ namespace System.Reflection
         #endregion
 
         #region Static Members
-        [MethodImpl(MethodImplOptions.InternalCall)]
-        private static extern unsafe bool GetMarshalAs(
+        [LibraryImport(RuntimeHelpers.QCall, EntryPoint = "MetadataImport_GetMarshalAs")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static unsafe partial bool GetMarshalAs(
             IntPtr pNativeType,
             int cNativeType,
             out int unmanagedType,
             out int safeArraySubType,
             out byte* safeArrayUserDefinedSubType,
+            out int safeArrayUserDefinedSubTypeLength,
             out int arraySubType,
             out int sizeParamIndex,
             out int sizeConst,
             out byte* marshalType,
+            out int marshalTypeLength,
             out byte* marshalCookie,
+            out int marshalCookieLength,
             out int iidParamIndex);
 
         internal static unsafe MarshalAsAttribute GetMarshalAs(ConstArray nativeType, RuntimeModule scope)
@@ -249,11 +253,14 @@ namespace System.Reflection
                     out int unmanagedTypeRaw,
                     out int safeArraySubTypeRaw,
                     out byte* safeArrayUserDefinedSubTypeRaw,
+                    out int safeArrayUserDefinedSubTypeLength,
                     out int arraySubTypeRaw,
                     out int sizeParamIndex,
                     out int sizeConst,
                     out byte* marshalTypeRaw,
+                    out int marshalTypeLength,
                     out byte* marshalCookieRaw,
+                    out int marshalCookieLength,
                     out int iidParamIndex))
             {
                 throw new BadImageFormatException();
@@ -261,21 +268,32 @@ namespace System.Reflection
 
             string? safeArrayUserDefinedTypeName = safeArrayUserDefinedSubTypeRaw == null
                 ? null
-                : Text.Encoding.UTF8.GetString(MemoryMarshal.CreateReadOnlySpanFromNullTerminated(safeArrayUserDefinedSubTypeRaw));
+                : Text.Encoding.UTF8.GetString(new ReadOnlySpan<byte>(safeArrayUserDefinedSubTypeRaw, safeArrayUserDefinedSubTypeLength));
             string? marshalTypeName = marshalTypeRaw == null
                 ? null
-                : Text.Encoding.UTF8.GetString(MemoryMarshal.CreateReadOnlySpanFromNullTerminated(marshalTypeRaw));
+                : Text.Encoding.UTF8.GetString(new ReadOnlySpan<byte>(marshalTypeRaw, marshalTypeLength));
             string? marshalCookie = marshalCookieRaw == null
                 ? null
-                : Text.Encoding.UTF8.GetString(MemoryMarshal.CreateReadOnlySpanFromNullTerminated(marshalCookieRaw));
+                : Text.Encoding.UTF8.GetString(new ReadOnlySpan<byte>(marshalCookieRaw, marshalCookieLength));
 
-            RuntimeType? safeArrayUserDefinedType = string.IsNullOrEmpty(safeArrayUserDefinedTypeName) ? null :
-                TypeNameResolver.GetTypeReferencedByCustomAttribute(safeArrayUserDefinedTypeName, scope);
+            RuntimeType? safeArrayUserDefinedType = null;
+
+            try
+            {
+                safeArrayUserDefinedType = string.IsNullOrEmpty(safeArrayUserDefinedTypeName) ? null :
+                    TypeNameResolver.GetTypeReferencedByCustomAttribute(safeArrayUserDefinedTypeName, scope);
+            }
+            catch (TypeLoadException)
+            {
+                // The user may have supplied a bad type name string causing this TypeLoadException
+                Debug.Assert(safeArrayUserDefinedTypeName is not null);
+            }
+
             RuntimeType? marshalTypeRef = null;
 
             try
             {
-                marshalTypeRef = marshalTypeName is null ? null : TypeNameResolver.GetTypeReferencedByCustomAttribute(marshalTypeName, scope);
+                marshalTypeRef = string.IsNullOrEmpty(marshalTypeName) ? null : TypeNameResolver.GetTypeReferencedByCustomAttribute(marshalTypeName, scope);
             }
             catch (TypeLoadException)
             {

src/coreclr/vm/ecalllist.h
@@ -123,7 +123,6 @@ FCFuncStart(gMetaDataImport)
     FCFuncElement("GetFieldMarshal", MetaDataImport::GetFieldMarshal)
     FCFuncElement("GetPInvokeMap", MetaDataImport::GetPInvokeMap)
     FCFuncElement("IsValidToken", MetaDataImport::IsValidToken)
-    FCFuncElement("GetMarshalAs", MetaDataImport::GetMarshalAs)
 FCFuncEnd()
 
 FCFuncStart(gSignatureNative)

src/coreclr/vm/managedmdimport.cpp
@@ -12,20 +12,29 @@
 //
 extern BOOL ParseNativeTypeInfo(NativeTypeParamInfo* pInfo, PCCOR_SIGNATURE pvNativeType, ULONG cbNativeType);
 
-FCIMPL11(FC_BOOL_RET, MetaDataImport::GetMarshalAs,
+extern "C" BOOL QCALLTYPE MetadataImport_GetMarshalAs(
     BYTE*   pvNativeType,
     ULONG   cbNativeType,
     INT32*  unmanagedType,
     INT32*  safeArraySubType,
     LPUTF8* safeArrayUserDefinedSubType,
+    INT32*  safeArrayUserDefinedSubTypeLength,
     INT32*  arraySubType,
     INT32*  sizeParamIndex,
     INT32*  sizeConst,
     LPUTF8* marshalType,
+    INT32*  marshalTypeLength,
     LPUTF8* marshalCookie,
+    INT32*  marshalCookieLength,
     INT32*  iidParamIndex)
 {
-    FCALL_CONTRACT;
+    CONTRACTL
+    {
+        NOTHROW;
+        GC_NOTRIGGER;
+        MODE_ANY;
+    }
+    CONTRACTL_END;
 
     NativeTypeParamInfo info{};
 
@@ -37,7 +46,7 @@ FCIMPL11(FC_BOOL_RET, MetaDataImport::GetMarshalAs,
     ZeroMemory(&info, sizeof(info));
     if (!ParseNativeTypeInfo(&info, pvNativeType, cbNativeType))
     {
-        FC_RETURN_BOOL(FALSE);
+        return FALSE;
     }
 
     *unmanagedType = info.m_NativeType;
@@ -51,21 +60,24 @@ FCIMPL11(FC_BOOL_RET, MetaDataImport::GetMarshalAs,
     *safeArraySubType = info.m_SafeArrayElementVT;
 
     *safeArrayUserDefinedSubType = info.m_strSafeArrayUserDefTypeName;
+    *safeArrayUserDefinedSubTypeLength = info.m_cSafeArrayUserDefTypeNameBytes;
 #else
     *iidParamIndex = 0;
 
     *safeArraySubType = VT_EMPTY;
 
     *safeArrayUserDefinedSubType = NULL;
+    *safeArrayUserDefinedSubTypeLength = 0;
 #endif
 
     *marshalType = info.m_strCMMarshalerTypeName;
+    *marshalTypeLength = info.m_cCMMarshalerTypeNameBytes;
 
     *marshalCookie = info.m_strCMCookie;
+    *marshalCookieLength = info.m_cCMCookieStrBytes;
 
-    FC_RETURN_BOOL(TRUE);
+    return TRUE;
 }
-FCIMPLEND
 
 FCIMPL1(IMDInternalImport*, MetaDataImport::GetMetadataImport, ReflectModuleBaseObject * pModuleUNSAFE)
 {

src/coreclr/vm/managedmdimport.hpp
@@ -54,20 +54,23 @@ public:
     static FCDECL3(HRESULT, GetSigOfMethodDef, IMDInternalImport* pScope, mdToken tk, ConstArray* pMarshalInfo);
     static FCDECL3(HRESULT, GetFieldMarshal, IMDInternalImport* pScope, mdToken tk, ConstArray* pMarshalInfo);
     static FCDECL2(FC_BOOL_RET, IsValidToken, IMDInternalImport* pScope, mdToken tk);
-
-    static FCDECL11(FC_BOOL_RET, GetMarshalAs,
-        BYTE*   pvNativeType,
-        ULONG   cbNativeType,
-        INT32*  unmanagedType,
-        INT32*  safeArraySubType,
-        LPUTF8* safeArrayUserDefinedSubType,
-        INT32*  arraySubType,
-        INT32*  sizeParamIndex,
-        INT32*  sizeConst,
-        LPUTF8* marshalType,
-        LPUTF8* marshalCookie,
-        INT32*  iidParamIndex);
 };
+
+extern "C" BOOL QCALLTYPE MetadataImport_GetMarshalAs(
+    BYTE*   pvNativeType,
+    ULONG   cbNativeType,
+    INT32*  unmanagedType,
+    INT32*  safeArraySubType,
+    LPUTF8* safeArrayUserDefinedSubType,
+    INT32*  safeArrayUserDefinedSubTypeLength,
+    INT32*  arraySubType,
+    INT32*  sizeParamIndex,
+    INT32*  sizeConst,
+    LPUTF8* marshalType,
+    INT32*  marshalTypeLength,
+    LPUTF8* marshalCookie,
+    INT32*  marshalCookieLength,
+    INT32*  iidParamIndex);
 
 extern "C" void QCALLTYPE MetadataImport_Enum(
     IMDInternalImport* pScope,

src/coreclr/vm/mlinfo.cpp
@@ -206,7 +206,6 @@ BOOL ParseNativeTypeInfo(NativeTypeParamInfo* pParamInfo,
 
                 pParamInfo->m_strSafeArrayUserDefTypeName = (LPUTF8)pvNativeType;
                 pParamInfo->m_cSafeArrayUserDefTypeNameBytes = strLen;
-                _ASSERTE((ULONG)(pvNativeType + strLen - pvNativeTypeStart) == cbNativeType);
             }
             break;
 
@@ -304,7 +303,6 @@ BOOL ParseNativeTypeInfo(NativeTypeParamInfo* pParamInfo,
 
             pParamInfo->m_strCMCookie = (LPUTF8)pvNativeType;
             pParamInfo->m_cCMCookieStrBytes = strLen;
-            _ASSERTE((ULONG)(pvNativeType + strLen - pvNativeTypeStart) == cbNativeType);
             break;
 
         default:

src/coreclr/vm/qcallentrypoints.cpp
@@ -413,6 +413,7 @@ static const Entry s_QCall[] =
     DllImportEntry(Monitor_TryEnter_Slowpath)
     DllImportEntry(Monitor_Exit_Slowpath)
     DllImportEntry(MetadataImport_Enum)
+    DllImportEntry(MetadataImport_GetMarshalAs)
     DllImportEntry(ReflectionInvocation_RunClassConstructor)
     DllImportEntry(ReflectionInvocation_RunModuleConstructor)
     DllImportEntry(ReflectionInvocation_CompileMethod)

src/libraries/System.Runtime.InteropServices/tests/System.Runtime.InteropServices.UnitTests/System/Runtime/InteropServices/MarshalAsAttributeTests.cs
@@ -1,6 +1,13 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.IO;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
+using System.Reflection.PortableExecutable;
+using System.Runtime.Loader;
 using Xunit;
 
 namespace System.Runtime.InteropServices.Tests
@@ -11,7 +18,7 @@ namespace System.Runtime.InteropServices.Tests
         [InlineData((UnmanagedType)(-1))]
         [InlineData(UnmanagedType.HString)]
         [InlineData((UnmanagedType)int.MaxValue)]
-        public void Ctor_UmanagedTye(UnmanagedType unmanagedType)
+        public void Ctor_UnmanagedType(UnmanagedType unmanagedType)
         {
             var attribute = new MarshalAsAttribute(unmanagedType);
             Assert.Equal(unmanagedType, attribute.Value);
@@ -25,6 +32,93 @@ namespace System.Runtime.InteropServices.Tests
         {
             var attribute = new MarshalAsAttribute(umanagedType);
             Assert.Equal((UnmanagedType)umanagedType, attribute.Value);
+        }
+
+        [ConditionalFact(typeof(PlatformDetection), nameof(PlatformDetection.IsBuiltInComEnabled), nameof(PlatformDetection.IsReflectionEmitSupported))]
+        public void SafeArrayParameter_ZeroLengthUserDefinedSubType_DoesNotThrow()
+        {
+            byte[] peBytes = BuildPEWithSafeArrayMarshalBlob();
+
+            AssemblyLoadContext alc = new(nameof(SafeArrayParameter_ZeroLengthUserDefinedSubType_DoesNotThrow), isCollectible: true);
+            try
+            {
+                using MemoryStream peStream = new(peBytes);
+                Assembly asm = alc.LoadFromStream(peStream);
+                Type iface = asm.GetType("TestInterface")!;
+                MethodInfo method = iface.GetMethod("TestMethod")!;
+                ParameterInfo param = method.GetParameters()[0];
+
+                // Must not throw TypeLoadException.
+                _ = param.GetCustomAttributes(false);
+
+                MarshalAsAttribute? attr = (MarshalAsAttribute?)Attribute.GetCustomAttribute(param, typeof(MarshalAsAttribute));
+                Assert.NotNull(attr);
+                Assert.Equal(UnmanagedType.SafeArray, attr.Value);
+                Assert.Null(attr.SafeArrayUserDefinedSubType);
+            }
+            finally
+            {
+                alc.Unload();
+            }
+        }
+
+        /// <summary>
+        /// Creates a PE whose parameter has a FieldMarshal blob that
+        /// reproduces the native bug: NATIVE_TYPE_SAFEARRAY (0x1D),
+        /// VT_DISPATCH (0x09), zero-length string (0x00), followed by
+        /// poison byte 'X' (0x58) and null terminator (0x00).
+        /// Without the native fix the FCALL returns a dangling pointer
+        /// into the poison byte region causing TypeLoadException.
+        /// </summary>
+        private static byte[] BuildPEWithSafeArrayMarshalBlob()
+        {
+            PersistedAssemblyBuilder ab = new(
+                new AssemblyName("SafeArrayTestAsm"), typeof(object).Assembly);
+            ModuleBuilder mod = ab.DefineDynamicModule("SafeArrayTestAsm.dll");
+            TypeBuilder td = mod.DefineType("TestInterface",
+                TypeAttributes.Interface | TypeAttributes.Abstract | TypeAttributes.Public);
+
+            MethodBuilder md = td.DefineMethod("TestMethod",
+                MethodAttributes.Public | MethodAttributes.Abstract |
+                MethodAttributes.Virtual | MethodAttributes.NewSlot |
+                MethodAttributes.HideBySig,
+                typeof(void), new[] { typeof(object[]) });
+
+            md.DefineParameter(1, ParameterAttributes.HasFieldMarshal, "args");
+            td.CreateType();
+
+            MetadataBuilder mdb = ab.GenerateMetadata(out BlobBuilder ilBlob, out _);
+
+            // This is the only parameter defined on the only method, so it
+            // occupies row 1 in the Param table. PersistedAssemblyBuilder
+            // emits parameters in definition order deterministically.
+            const int paramRowNumber = 1;
+
+            // Blob bytes:
+            //   0x1D  NATIVE_TYPE_SAFEARRAY
+            //   0x09  VT_DISPATCH
+            //   0x00  compressed string length 0
+            //   0x58  'X' poison (not consumed by parser)
+            //   0x00  null terminator
+            BlobBuilder marshalBlob = new();
+            marshalBlob.WriteByte(0x1D);
+            marshalBlob.WriteByte(0x09);
+            marshalBlob.WriteByte(0x00);
+            marshalBlob.WriteByte(0x58);
+            marshalBlob.WriteByte(0x00);
+            mdb.AddMarshallingDescriptor(
+                MetadataTokens.ParameterHandle(paramRowNumber),
+                mdb.GetOrAddBlob(marshalBlob));
+
+            ManagedPEBuilder pe = new(
+                PEHeaderBuilder.CreateLibraryHeader(),
+                new MetadataRootBuilder(mdb),
+                ilBlob,
+                flags: CorFlags.ILOnly);
+
+            BlobBuilder output = new();
+            pe.Serialize(output);
+            return output.ToArray();
         }
     }
 }