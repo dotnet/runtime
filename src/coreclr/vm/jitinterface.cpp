34cdc1d4143a730c05269dd206c373f0094dc22b
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2026-06-29 22:16:36 +0200

    Fix async resumption stub with byref parameters
    It is not valid IL to initialize a byref local with `initobj`. Just load
    the value of these directly.
    
    We had a test for byref parameters, but it did not suspend and did not
    result in creation of a resumption stub. The test case requires
    suspension + NoInlining to guarantee a suspension point is created.

--- src/coreclr/vm/jitinterface.cpp
+++ src/coreclr/vm/jitinterface.cpp
@@ -15148,10 +15148,18 @@ CORINFO_METHOD_HANDLE CEEJitInfo::getAsyncResumptionStub(void** entryPoint)
     while ((ty = msig.NextArg()) != ELEMENT_TYPE_END)
     {
         TypeHandle tyHnd = msig.GetLastTypeHandleThrowing();
-        DWORD loc = pCode->NewLocal(LocalDesc(tyHnd));
-        pCode->EmitLDLOCA(loc);
-        pCode->EmitINITOBJ(pCode->GetToken(tyHnd));
-        pCode->EmitLDLOC(loc);
+        if (tyHnd.GetInternalCorElementType() == ELEMENT_TYPE_BYREF)
+        {
+            pCode->EmitLDC(0);
+            pCode->EmitCONV_U();
+        }
+        else
+        {
+            DWORD loc = pCode->NewLocal(LocalDesc(tyHnd));
+            pCode->EmitLDLOCA(loc);
+            pCode->EmitINITOBJ(pCode->GetToken(tyHnd));
+            pCode->EmitLDLOC(loc);
+        }
         numArgs++;
     }
 
