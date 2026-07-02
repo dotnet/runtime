3e860952f710ec48a979da0d30478deb0b204a31
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2026-07-02 15:24:18 +0900

    Add Xunit test for TestEntryPoint method

--- src/tests/nativeaot/Regressions/Runtime112629/Runtime112629.cs
+++ src/tests/nativeaot/Regressions/Runtime112629/Runtime112629.cs
@@ -1,12 +1,15 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Xunit;
+
 extern alias AssemblyWithDot;
 extern alias AssemblyWithUnderscore;
 
 class Program
 {
-    public static int Main()
+    [Fact]
+    public static int TestEntryPoint()
     {
         if (new AssemblyWithDot::TestNamespace.TestType().Value != "A.B")
             return 101;
