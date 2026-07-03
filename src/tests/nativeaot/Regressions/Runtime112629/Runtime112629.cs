bca9aef3fcaf7691293b0ae3d11eb779432b2e08
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2026-07-03 12:57:21 +0900

    Refactor TestEntryPoint to use assertions

--- src/tests/nativeaot/Regressions/Runtime112629/Runtime112629.cs
+++ src/tests/nativeaot/Regressions/Runtime112629/Runtime112629.cs
@@ -1,22 +1,17 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Xunit;
-
 extern alias AssemblyWithDot;
 extern alias AssemblyWithUnderscore;
+
+using Xunit;
 
 class Program
 {
     [Fact]
-    public static int TestEntryPoint()
+    public static void TestEntryPoint()
     {
-        if (new AssemblyWithDot::TestNamespace.TestType().Value != "A.B")
-            return 101;
-
-        if (new AssemblyWithUnderscore::TestNamespace.TestType().Value != "A_B")
-            return 102;
-
-        return 100;
+        Assert.Equal("A.B", new AssemblyWithDot::TestNamespace.TestType().Value);
+        Assert.Equal("A_B", new AssemblyWithUnderscore::TestNamespace.TestType().Value);
     }
 }
