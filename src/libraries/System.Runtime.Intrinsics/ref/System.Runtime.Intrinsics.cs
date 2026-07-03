41f9cf27121ebc9bbb42275af3e0ebbbc60241ed
Liu An <liuan@loongson.cn>
2026-07-03 11:37:41 +0800

    Delete System.Runtime.Intrinsics.cs changes
    Change-Id: I2c1e8d43106616da47e231670e557f508330e3d8

--- src/libraries/System.Runtime.Intrinsics/ref/System.Runtime.Intrinsics.cs
+++ src/libraries/System.Runtime.Intrinsics/ref/System.Runtime.Intrinsics.cs
@@ -7347,31 +7347,6 @@ namespace System.Runtime.Intrinsics.Arm
         StoreL3NonTemporal = 13
     };
 }
-namespace System.Runtime.Intrinsics.LoongArch
-{
-    [System.CLSCompliantAttribute(false)]
-    public abstract partial class LAM
-    {
-        internal LAM() { }
-        public abstract partial class BH
-        {
-            internal BH() { }
-            public static bool IsSupported { get { throw null; } }
-            public static byte Exchange(ref byte location1, byte value) { throw null; }
-            public static ushort Exchange(ref ushort location1, ushort value) { throw null; }
-        }
-        public abstract partial class CAS
-        {
-            internal CAS() { }
-            public static bool IsSupported { get { throw null; } }
-            public static byte CompareExchange(ref byte location1, byte value, byte comparand) { throw null; }
-            public static ushort CompareExchange(ref ushort location1, ushort value, ushort comparand) { throw null; }
-            public static int CompareExchange(ref int location1, int value, int comparand) { throw null; }
-            public static unsafe int CompareExchange(int* location1, int value, int comparand) { throw null; }
-            public static long CompareExchange(ref long location1, long value, long comparand) { throw null; }
-        }
-    }
-}
 namespace System.Runtime.Intrinsics.X86
 {
     [System.CLSCompliantAttribute(false)]
