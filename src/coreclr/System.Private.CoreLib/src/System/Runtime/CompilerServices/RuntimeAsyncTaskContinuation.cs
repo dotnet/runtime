4c04ba113af5606e17a0538f43cf52c83d067ff7
Jakob Botsch Nielsen <jakob.botsch.nielsen@gmail.com>
2026-07-07 15:33:59 +0200

    Experiment with inlining continuation

--- src/coreclr/System.Private.CoreLib/src/System/Runtime/CompilerServices/RuntimeAsyncTaskContinuation.cs
+++ src/coreclr/System.Private.CoreLib/src/System/Runtime/CompilerServices/RuntimeAsyncTaskContinuation.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Diagnostics;
+using System.Numerics;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -26,28 +27,52 @@ namespace System.Runtime.CompilerServices
 
         internal void Execute(bool canInline)
         {
-            Debug.Assert(RuntimeAsyncTask != null);
+            Debug.Assert(Task != null && RuntimeAsyncTask != null);
 
-            if (((Flags & ContinuationFlags.AllContinuationFlags) == 0) || !QueueIfNecessary(canInline))
+            Task raTask = RuntimeAsyncTask;
+            object? continuationContext = ContinuationContext;
+            ref ContinuationFlags flags = ref Flags;
+
+            if (Task.IsCompletedSuccessfully)
+            {
+                // We are going to inline the completion to avoid the double continuation dispatch.
+                Debug.Assert(Next != null);
+                Continuation nextCont = Next;
+
+                // We have two situations that are both compatible with a single "queue continuation" operation:
+                // - This was a transparent await and the next continuation's continuation context is what matters
+                //   (e.g. await Foo(), where Foo() is an async version)
+                // - This was not a transparent await, e.g. "await AsyncHelpers.Await(someTask)". Next continuation
+                //   does not have a continuation context and would run transparently.
+                Debug.Assert(BitOperations.PopCount((nuint)((Flags | nextCont.Flags) & ContinuationFlags.AllContinuationFlags)) <= 1);
+
+                nextCont.Flags |= Flags & ContinuationFlags.AllContinuationFlags;
+                Flags &= ~ContinuationFlags.AllContinuationFlags;
+                flags = ref nextCont.Flags;
+
+                raTask.m_stateObject = nextCont;
+
+                TaskContinuationResume.ResumeTaskContinuation(this, ref nextCont.GetResultStorageOrNull());
+            }
+
+            if (((flags & ContinuationFlags.AllContinuationFlags) == 0) || !QueueIfNecessary(canInline, raTask, continuationContext, ref flags))
             {
                 if (canInline)
                 {
-                    RuntimeAsyncTask.ExecuteDirectly(null);
+                    raTask.ExecuteDirectly(null);
                 }
                 else
                 {
-                    ThreadPool.UnsafeQueueUserWorkItemInternal(RuntimeAsyncTask, preferLocal: true);
+                    ThreadPool.UnsafeQueueUserWorkItemInternal(raTask, preferLocal: true);
                 }
             }
         }
 
-        private bool QueueIfNecessary(bool canInline)
+        private static bool QueueIfNecessary(bool canInline, Task raTask, object? continuationContext, ref ContinuationFlags flags)
         {
-            Debug.Assert(RuntimeAsyncTask != null);
-
-            if ((Flags & ContinuationFlags.ContinueOnThreadPool) != 0)
+            if ((flags & ContinuationFlags.ContinueOnThreadPool) != 0)
             {
-                Flags &= ~ContinuationFlags.ContinueOnThreadPool;
+                flags &= ~ContinuationFlags.ContinueOnThreadPool;
                 SynchronizationContext? ctx = Thread.CurrentThread._synchronizationContext;
                 if (ctx == null || ctx.GetType() == typeof(SynchronizationContext))
                 {
@@ -59,15 +84,14 @@ namespace System.Runtime.CompilerServices
                     }
                 }
 
-                ThreadPool.UnsafeQueueUserWorkItemInternal(RuntimeAsyncTask, preferLocal: true);
+                ThreadPool.UnsafeQueueUserWorkItemInternal(raTask, preferLocal: true);
                 return true;
             }
 
-            if ((Flags & ContinuationFlags.ContinueOnCapturedSynchronizationContext) != 0)
+            if ((flags & ContinuationFlags.ContinueOnCapturedSynchronizationContext) != 0)
             {
-                Flags &= ~ContinuationFlags.ContinueOnCapturedSynchronizationContext;
+                flags &= ~ContinuationFlags.ContinueOnCapturedSynchronizationContext;
 
-                object? continuationContext = ContinuationContext;
                 Debug.Assert(continuationContext is SynchronizationContext { });
                 SynchronizationContext continuationSyncCtx = (SynchronizationContext)continuationContext;
 
@@ -78,7 +102,7 @@ namespace System.Runtime.CompilerServices
 
                 try
                 {
-                    continuationSyncCtx.Post(TaskContinuationResume.s_postCallback, RuntimeAsyncTask);
+                    continuationSyncCtx.Post(TaskContinuationResume.s_postCallback, raTask);
                 }
                 catch (Exception ex)
                 {
@@ -88,14 +112,13 @@ namespace System.Runtime.CompilerServices
                 return true;
             }
 
-            if ((Flags & ContinuationFlags.ContinueOnCapturedTaskScheduler) != 0)
+            if ((flags & ContinuationFlags.ContinueOnCapturedTaskScheduler) != 0)
             {
-                Flags &= ~ContinuationFlags.ContinueOnCapturedTaskScheduler;
-                object? continuationContext = ContinuationContext;
+                flags &= ~ContinuationFlags.ContinueOnCapturedTaskScheduler;
                 Debug.Assert(continuationContext is TaskScheduler { });
                 TaskScheduler sched = (TaskScheduler)continuationContext;
 
-                TaskSchedulerAwaitTaskContinuation.RunOrScheduleAction((Action)RuntimeAsyncTask.m_action!, sched, capturedContext: null, allowInlining: canInline);
+                TaskSchedulerAwaitTaskContinuation.RunOrScheduleAction((Action)raTask.m_action!, sched, capturedContext: null, allowInlining: canInline);
 
                 return true;
             }
@@ -149,7 +172,7 @@ namespace System.Runtime.CompilerServices
                 Resume = &ResumeTaskContinuation,
             };
 
-            private static Continuation? ResumeTaskContinuation(Continuation cont, ref byte result)
+            internal static Continuation? ResumeTaskContinuation(Continuation cont, ref byte result)
             {
                 var taskCont = (RuntimeAsyncTaskContinuation)cont;
                 taskCont.Next = null;
