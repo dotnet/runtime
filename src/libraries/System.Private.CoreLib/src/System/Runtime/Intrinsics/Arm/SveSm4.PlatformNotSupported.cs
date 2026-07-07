77006d9c294789353615f96eefa0f28cc267d6e0
Alan Hayward <alan.hayward@arm.com>
2026-07-07 13:35:06 +0100

    fix IsSupported

--- src/libraries/System.Private.CoreLib/src/System/Runtime/Intrinsics/Arm/SveSm4.PlatformNotSupported.cs
+++ src/libraries/System.Private.CoreLib/src/System/Runtime/Intrinsics/Arm/SveSm4.PlatformNotSupported.cs
@@ -27,7 +27,7 @@ namespace System.Runtime.Intrinsics.Arm
             /// <summary>Gets a value that indicates whether the APIs in this class are supported.</summary>
             /// <value><see langword="true" /> if the APIs are supported; otherwise, <see langword="false" />.</value>
             /// <remarks>A value of <see langword="false" /> indicates that the APIs will throw <see cref="PlatformNotSupportedException" />.</remarks>
-            public static new bool IsSupported { get => IsSupported; }
+            public static new bool IsSupported { [Intrinsic] get { return false; } }
         }
 
 
