dotnet/runtime

commit e0f8bcb4ba34a7ba40d5f3ea4352eef23127df1e

[release/10.0] Optimize Directory.GetFiles by passing safe patterns to NtQueryDirectoryFile (#127974)
Backport of #122947 and #123695 into release/10.0

## Customer Impact

- [x] Customer reported
- [ ] Found internally

Customers with selective patterns in large directories experience severe
performance degradation. Example: Directory.GetFiles(path,
"A14881*.jpg") in a directory with 140K files but 4 matches currently
enumerates all 140K files. This PR reduces that to ~4-10 entries.

We've received feedback on this several times over the years, and
#122947 implemented a fix that we've been happy with the results from.
Customers have also reported this issue within PowerShell and this
backport will benefit those scenarios through their servicing updates as
well.

## Regression
- [ ] Yes
- [x] No

Not a regression within .NET, but customers experience this as a
regression when moving from .NET Framework to .NET.

## Testing

New unit tests were added when this merged into main on January 26, and
no issues have been encountered with .NET 11 thus far.

## Risk

Low. Thorough reviews and research to validate the changes.

---------

Co-authored-by: Copilot <198982749+Copilot@users.noreply.github.com>
Co-authored-by: Stephen Toub <stoub@microsoft.com>
Co-authored-by: jozkee <16040868+jozkee@users.noreply.github.com>
Co-authored-by: stephentoub <2642209+stephentoub@users.noreply.github.com>
Co-authored-by: Jeremy Kuhne <jeremy.kuhne@microsoft.com>

src/libraries/System.Private.CoreLib/src/System/IO/Enumeration/FileSystemEnumerator.Windows.cs
@@ -5,6 +5,7 @@ using System;
 using System.Buffers;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Runtime.CompilerServices;
 using System.Runtime.ConstrainedExecution;
@@ -25,6 +26,7 @@ namespace System.IO.Enumeration
         private readonly string _originalRootDirectory;
         private readonly string _rootDirectory;
         private readonly EnumerationOptions _options;
+        private readonly string? _expression;
 
         private readonly object _lock = new object();
 
@@ -37,6 +39,50 @@ namespace System.IO.Enumeration
         private string? _currentPath;
         private bool _lastEntryFound;
         private Queue<(IntPtr Handle, string Path, int RemainingDepth)>? _pending;
+        private bool _isFirstGetData = true;
+
+        /// <summary>
+        /// Encapsulates a find operation.
+        /// </summary>
+        /// <param name="directory">The directory to search in.</param>
+        /// <param name="isNormalized">Whether the directory path is already normalized or not.</param>
+        /// <param name="options">Enumeration options to use.</param>
+        /// <param name="expression">The search expression to potentially use for OS-level filtering (Windows only).</param>
+        internal FileSystemEnumerator(string directory, bool isNormalized, EnumerationOptions? options, string? expression) :
+            this(directory, isNormalized, options)
+        {
+            if (_options.RecurseSubdirectories)
+            {
+                // We can never filter when recursing, as we need to find all subdirectories to recurse into them.
+                expression = null;
+            }
+            else if (expression is not null)
+            {
+                // We need to ensure that the expression is prepped to be used as an NtQueryDirectoryFile filter.
+                // If the match type is simple, we need to escape the special DOS wildcard characters. If it is
+                // DOS style, the characters have already been escaped in the FileSystemEnumerableFactory.
+
+                if (expression.Length > 255 || expression is "*" or "." or "..")
+                {
+                    // Don't allow expressions longer than the maximum filename length. This comes back as
+                    // STATUS_INVALID_PARAMETER from NtQueryDirectoryFile. We could catch that there, but
+                    // don't want to mask other bad parameter issues.
+                    //
+                    // Also, an expression of "*" is pointless as a filter- it matches everything. "*.*"
+                    // should have been converted to "*" already in the case of MatchType.Win32.
+                    //
+                    // Finally, "." and ".." are invalid filter names on their own, but we allow you to see
+                    // these entries if you specifically ask via EnumerationOptions.ReturnSpecialDirectories.
+                    expression = null;
+                }
+                else if (_options.MatchType == MatchType.Simple)
+                {
+                    expression = FileSystemName.EscapeExpression(expression);
+                }
+            }
+
+            _expression = expression;
+        }
 
         private void Init()
         {
@@ -83,29 +129,55 @@ namespace System.IO.Enumeration
             Debug.Assert(_directoryHandle != (IntPtr)(-1) && _directoryHandle != IntPtr.Zero && !_lastEntryFound);
 
             Interop.NtDll.IO_STATUS_BLOCK statusBlock;
-            int status = Interop.NtDll.NtQueryDirectoryFile(
-                FileHandle: _directoryHandle,
-                Event: IntPtr.Zero,
-                ApcRoutine: IntPtr.Zero,
-                ApcContext: IntPtr.Zero,
-                IoStatusBlock: &statusBlock,
-                FileInformation: _buffer,
-                Length: (uint)_bufferLength,
-                FileInformationClass: Interop.NtDll.FILE_INFORMATION_CLASS.FileFullDirectoryInformation,
-                ReturnSingleEntry: Interop.BOOLEAN.FALSE,
-                FileName: null,
-                RestartScan: Interop.BOOLEAN.FALSE);
+            int status;
+            fixed (char* pBuffer = _expression)
+            {
+                Interop.UNICODE_STRING* fileNamePtr = null;
+                Interop.UNICODE_STRING fileNameStruct = default;
+
+                // On the first call for each directory, check if we can use the expression as an OS-level filter hint.
+                // All results will still be validated in managed code, as the OS filtering may be looser than what .NET expects.
+                // We cannot use OS filtering when recursing, as the filter would exclude subdirectories that don't match the pattern.
+                if (_isFirstGetData)
+                {
+                    _isFirstGetData = false;
+                    if (_expression is not null)
+                    {
+                        fileNameStruct.Length = fileNameStruct.MaximumLength = (ushort)(_expression.Length * sizeof(char));
+                        fileNameStruct.Buffer = (IntPtr)pBuffer;
+                        fileNamePtr = &fileNameStruct;
+                    }
+                }
+
+                status = Interop.NtDll.NtQueryDirectoryFile(
+                    FileHandle: _directoryHandle,
+                    Event: IntPtr.Zero,
+                    ApcRoutine: IntPtr.Zero,
+                    ApcContext: IntPtr.Zero,
+                    IoStatusBlock: &statusBlock,
+                    FileInformation: _buffer,
+                    Length: (uint)_bufferLength,
+                    FileInformationClass: Interop.NtDll.FILE_INFORMATION_CLASS.FileFullDirectoryInformation,
+                    ReturnSingleEntry: Interop.BOOLEAN.FALSE,
+                    FileName: fileNamePtr,
+                    RestartScan: Interop.BOOLEAN.FALSE);
+            }
 
             switch ((uint)status)
             {
-                case Interop.StatusOptions.STATUS_NO_MORE_FILES:
-                    DirectoryFinished();
-                    return false;
                 case Interop.StatusOptions.STATUS_SUCCESS:
                     Debug.Assert(statusBlock.Information.ToInt64() != 0);
                     return true;
+                case Interop.StatusOptions.STATUS_NO_MORE_FILES:
                 // FILE_NOT_FOUND can occur when there are NO files in a volume root (usually there are hidden system files).
                 case Interop.StatusOptions.STATUS_FILE_NOT_FOUND:
+                // STATUS_OBJECT_NAME_INVALID is returned when the FileName has characters the filesystem doesn't like. They
+                // never would have matched manually filtering the current directory, so we'll return no matches. It is
+                // better to let Windows tell us that we have an invalid name as opposed to guessing. We can't know which
+                // file system driver a given directory is going to be on- could even be a Unix type file system.
+                //
+                // This is also the error given back when trying to filter for "." or "..".
+                case Interop.StatusOptions.STATUS_OBJECT_NAME_INVALID:
                     DirectoryFinished();
                     return false;
                 default:
@@ -287,6 +359,7 @@ namespace System.IO.Enumeration
                 return false;
 
             (_directoryHandle, _currentPath, _remainingRecursionDepth) = _pending.Dequeue();
+            _isFirstGetData = true;
             return true;
         }
 

src/libraries/Common/src/Interop/Windows/NtDll/Interop.NtStatus.cs
@@ -17,6 +17,7 @@ internal static partial class Interop
         internal const uint STATUS_FILE_NOT_FOUND         = 0xC000000F;
         internal const uint STATUS_NO_MEMORY              = 0xC0000017;
         internal const uint STATUS_ACCESS_DENIED          = 0xC0000022;
+        internal const uint STATUS_OBJECT_NAME_INVALID    = 0xC0000033;
         internal const uint STATUS_OBJECT_NAME_NOT_FOUND  = 0xC0000034;
         internal const uint STATUS_QUOTA_EXCEEDED         = 0xC0000044;
         internal const uint STATUS_ACCOUNT_RESTRICTION    = 0xC000006E;

src/libraries/System.Private.CoreLib/src/System/IO/Enumeration/FileSystemEnumerable.cs
@@ -17,13 +17,14 @@ namespace System.IO.Enumeration
         private readonly FindTransform _transform;
         private readonly EnumerationOptions _options;
         private readonly string _directory;
+        private readonly string? _expression;
 
         public FileSystemEnumerable(string directory, FindTransform transform, EnumerationOptions? options = null)
             : this(directory, transform, options, isNormalized: false)
         {
         }
 
-        internal FileSystemEnumerable(string directory, FindTransform transform, EnumerationOptions? options, bool isNormalized)
+        internal FileSystemEnumerable(string directory, FindTransform transform, EnumerationOptions? options, bool isNormalized, string? expression = null)
         {
             ArgumentNullException.ThrowIfNull(directory);
             ArgumentNullException.ThrowIfNull(transform);
@@ -31,6 +32,7 @@ namespace System.IO.Enumeration
             _directory = directory;
             _transform = transform;
             _options = options ?? EnumerationOptions.Default;
+            _expression = expression;
 
             // We need to create the enumerator up front to ensure that we throw I/O exceptions for
             // the root directory on creation of the enumerable.
@@ -62,7 +64,7 @@ namespace System.IO.Enumeration
             private readonly FileSystemEnumerable<TResult> _enumerable;
 
             public DelegateEnumerator(FileSystemEnumerable<TResult> enumerable, bool isNormalized)
-                : base(enumerable._directory, isNormalized, enumerable._options)
+                : base(enumerable._directory, isNormalized, enumerable._options, enumerable._expression)
             {
                 _enumerable = enumerable;
             }

src/libraries/System.Private.CoreLib/src/System/IO/Enumeration/FileSystemEnumerableFactory.cs
@@ -74,15 +74,9 @@ namespace System.IO.Enumeration
                         // is the directory separator and cannot be part of any path segment in Windows). The other three are the
                         // special case wildcards that we'll convert some * and ? into. They're also valid as filenames on Unix,
                         // which is not true in Windows and as such we'll escape any that occur on the input string.
-                        if (Path.DirectorySeparatorChar != '\\' && expression.AsSpan().ContainsAny(@"\""<>"))
+                        if (Path.DirectorySeparatorChar != '\\')
                         {
-                            // Backslash isn't the default separator, need to escape (e.g. Unix)
-                            expression = expression.Replace("\\", "\\\\");
-
-                            // Also need to escape the other special wild characters ('"', '<', and '>')
-                            expression = expression.Replace("\"", "\\\"");
-                            expression = expression.Replace(">", "\\>");
-                            expression = expression.Replace("<", "\\<");
+                            expression = FileSystemName.EscapeExpression(expression);
                         }
 
                         // Need to convert the expression to match Win32 behavior
@@ -118,7 +112,9 @@ namespace System.IO.Enumeration
             return new FileSystemEnumerable<string>(
                 directory,
                 (ref FileSystemEntry entry) => entry.ToSpecifiedFullPath(),
-                options)
+                options,
+                isNormalized: false,
+                expression)
             {
                 ShouldIncludePredicate = (ref FileSystemEntry entry) =>
                     !entry.IsDirectory && MatchesPattern(expression, entry.FileName, options)
@@ -132,7 +128,9 @@ namespace System.IO.Enumeration
             return new FileSystemEnumerable<string>(
                 directory,
                 (ref FileSystemEntry entry) => entry.ToSpecifiedFullPath(),
-                options)
+                options,
+                isNormalized: false,
+                expression)
             {
                 ShouldIncludePredicate = (ref FileSystemEntry entry) =>
                     entry.IsDirectory && MatchesPattern(expression, entry.FileName, options)
@@ -146,7 +144,9 @@ namespace System.IO.Enumeration
             return new FileSystemEnumerable<string>(
                 directory,
                 (ref FileSystemEntry entry) => entry.ToSpecifiedFullPath(),
-                options)
+                options,
+                isNormalized: false,
+                expression)
             {
                 ShouldIncludePredicate = (ref FileSystemEntry entry) =>
                     MatchesPattern(expression, entry.FileName, options)
@@ -163,7 +163,8 @@ namespace System.IO.Enumeration
                directory,
                (ref FileSystemEntry entry) => (FileInfo)entry.ToFileSystemInfo(),
                options,
-               isNormalized)
+               isNormalized,
+               expression)
             {
                 ShouldIncludePredicate = (ref FileSystemEntry entry) =>
                     !entry.IsDirectory && MatchesPattern(expression, entry.FileName, options)
@@ -180,7 +181,8 @@ namespace System.IO.Enumeration
                directory,
                (ref FileSystemEntry entry) => (DirectoryInfo)entry.ToFileSystemInfo(),
                options,
-               isNormalized)
+               isNormalized,
+               expression)
             {
                 ShouldIncludePredicate = (ref FileSystemEntry entry) =>
                     entry.IsDirectory && MatchesPattern(expression, entry.FileName, options)
@@ -197,7 +199,8 @@ namespace System.IO.Enumeration
                directory,
                (ref FileSystemEntry entry) => entry.ToFileSystemInfo(),
                options,
-               isNormalized)
+               isNormalized,
+               expression)
             {
                 ShouldIncludePredicate = (ref FileSystemEntry entry) =>
                     MatchesPattern(expression, entry.FileName, options)

src/libraries/System.Private.CoreLib/src/System/IO/Enumeration/FileSystemEnumerator.Unix.cs
@@ -30,6 +30,19 @@ namespace System.IO.Enumeration
         // Used for creating full paths
         private char[]? _pathBuffer;
 
+        /// <summary>
+        /// Encapsulates a find operation.
+        /// </summary>
+        /// <param name="directory">The directory to search in.</param>
+        /// <param name="isNormalized">Whether the directory path is already normalized or not.</param>
+        /// <param name="options">Enumeration options to use.</param>
+        /// <param name="expression">The search expression to potentially use for OS-level filtering (Windows only).</param>
+        internal FileSystemEnumerator(string directory, bool isNormalized, EnumerationOptions? options, string? expression) :
+            this(directory, isNormalized, options)
+        {
+            _ = expression; // unused
+        }
+
         private void Init()
         {
             // We need to initialize the directory handle up front to ensure

src/libraries/System.Private.CoreLib/src/System/IO/Enumeration/FileSystemName.cs
@@ -413,5 +413,35 @@ namespace System.IO.Enumeration
 
             return currentState == maxState;
         }
+
+        /// <summary>
+        /// Escapes the given expression so that only '*' and '?' are treated as wildcards, and
+        /// '\' isn't treated as an escape character.
+        /// </summary>
+        internal static string EscapeExpression(string expression)
+        {
+            ReadOnlySpan<char> span = expression;
+            ReadOnlySpan<char> charsToEscape = ['\\', '"', '<', '>'];
+
+            int i = span.IndexOfAny(charsToEscape);
+            if (i >= 0)
+            {
+                ValueStringBuilder vsb = new(stackalloc char[256]);
+                do
+                {
+                    vsb.Append(span.Slice(0, i));
+                    vsb.Append('\\');
+                    vsb.Append(span[i]);
+                    span = span.Slice(i + 1);
+                    i = span.IndexOfAny(charsToEscape);
+                }
+                while (i >= 0);
+
+                vsb.Append(span);
+                expression = vsb.ToString();
+            }
+
+            return expression;
+        }
     }
 }

commit 4b1a6517fdbadca430e950a13d1dac7f562d4cd3

Handle STATUS_OBJECT_NAME_NOT_FOUND in Windows directory enumeration (#130196)
Fixes #130134

## Summary

`Directory.GetFiles`/`Directory.EnumerateFiles` (and the
`DirectoryInfo`/`Directory.GetFileSystemEntries` variants) with a search
pattern that matches **zero** files throws
`System.IO.FileNotFoundException` -- naming the directory itself -- on
some read-only / virtual file systems, instead of returning an empty
result. This was reported on an Azure App Service Windows app deployed
with `WEBSITE_RUN_FROM_PACKAGE`, where the content root is a read-only
mounted zip. It is a regression in 10.0.9, introduced by #127974
(backport of #122947 / #123695).

## Root cause

#127974 began passing the search pattern to `NtQueryDirectoryFile` as an
OS-level `FileName` filter. When zero entries match,
`FileSystemEnumerator<T>.GetData()`'s `switch` only handles
`STATUS_NO_SUCH_FILE` (`0xC000000F`). Some file system drivers report
the same "no matching entries" condition as
**`STATUS_OBJECT_NAME_NOT_FOUND` (`0xC0000034`)**, which fell through to
the `default` case → `ERROR_FILE_NOT_FOUND` → `throw`.

## Evidence that the culprit is `STATUS_OBJECT_NAME_NOT_FOUND`

The `default` arm of `GetData()` turns the raw `NTSTATUS` into the
thrown exception in **two hops**:

```csharp
// FileSystemEnumerator.Windows.cs, default case
int error = (int)Interop.NtDll.RtlNtStatusToDosError(status);      // hop 1: NTSTATUS  -> Win32 error
...
throw Win32Marshal.GetExceptionForWin32Error(error, _currentPath); // hop 2: Win32 error -> Exception
```

**Hop 2** — `Win32Marshal.GetExceptionForWin32Error`
(`src/libraries/Common/src/System/IO/Win32Marshal.cs`) selects the
exception type and message purely from the Win32 error code:

| Win32 error | Exception | Message resource / text |
| --- | --- | --- |
| `ERROR_FILE_NOT_FOUND` (2) | `FileNotFoundException` |
`IO_FileNotFound_FileName` = `"Could not find file '{0}'."` |
| `ERROR_PATH_NOT_FOUND` (3) | `DirectoryNotFoundException` |

src/libraries/System.Private.CoreLib/src/System/IO/Enumeration/FileSystemEnumerator.Windows.cs
@@ -171,6 +171,13 @@ namespace System.IO.Enumeration
                 case Interop.StatusOptions.STATUS_NO_MORE_FILES:
                 // FILE_NOT_FOUND can occur when there are NO files in a volume root (usually there are hidden system files).
                 case Interop.StatusOptions.STATUS_FILE_NOT_FOUND:
+                // STATUS_OBJECT_NAME_NOT_FOUND occurs when an OS-level filter (FileName) is passed to NtQueryDirectoryFile
+                // and nothing matches. Most file systems report this as STATUS_NO_SUCH_FILE (handled above as
+                // STATUS_FILE_NOT_FOUND), but some- particularly read-only or virtual file systems- report it as
+                // STATUS_OBJECT_NAME_NOT_FOUND instead. We can't know which file system driver a given directory is on. The
+                // directory handle was already opened successfully, so this can only mean the filter matched nothing; treat
+                // it the same as no matches.
+                case Interop.StatusOptions.STATUS_OBJECT_NAME_NOT_FOUND:
                 // STATUS_OBJECT_NAME_INVALID is returned when the FileName has characters the filesystem doesn't like. They
                 // never would have matched manually filtering the current directory, so we'll return no matches. It is
                 // better to let Windows tell us that we have an invalid name as opposed to guessing. We can't know which