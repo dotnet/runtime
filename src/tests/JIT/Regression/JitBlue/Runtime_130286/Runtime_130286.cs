dotnet/runtime

commit 3cb9cf71803370cfd2a2580c06cf32f2ea16365c

Remove CORINFO_HELP_ASSIGN_BYREF (#128542)
This PR removes `CORINFO_HELP_ASSIGN_BYREF` write-barrier (which is
basically `CORINFO_HELP_CHECKED_ASSIGN_REF` with a custom calling
convention), it is replaced by either `CORINFO_HELP_ASSIGN_REF` or
`CORINFO_HELP_CHECKED_ASSIGN_REF`. As a side effect we no longer use
slow `rep movsq` on x64 for gaps between GC slots and may use SIMD. The
motivation that `CORINFO_HELP_ASSIGN_BYREF` was very useful for large
struct copies, but we now have a bulk write barrier for those.

NOTE: A lot of code (>1000-2000 LOC) can be removed from CoreCLR/NAOT
side as dead as well since `CORINFO_HELP_ASSIGN_BYREF` is unused now,
but that's for another PR and might require R2R bump.

```cs
class MyClass
{
    MyStruct _fld;
    
    void SetA(MyStruct value) => _fld = value;
    
}

record struct MyStruct(long N1, long N2, long N3, long N4, long N5, string A, string B);
```

```diff
-       push     rdi
        push     rsi
+       push     rbx
+       mov      rbx, rdx
-       lea      rdi, bword ptr [rcx+0x08]
-       mov      rsi, rdx
-       call     CORINFO_HELP_ASSIGN_BYREF
-       call     CORINFO_HELP_ASSIGN_BYREF
-       mov      ecx, 5
-       rep movsq
+       lea      rsi, bword ptr [rcx+0x08]
+       mov      rdx, gword ptr [rbx]
+       mov      rcx, rsi
+       call     CORINFO_HELP_ASSIGN_REF
+       mov      rdx, gword ptr [rbx+0x08]
+       lea      rcx, bword ptr [rsi+0x08]
+       call     CORINFO_HELP_ASSIGN_REF
+       vmovdqu  ymm0, ymmword ptr [rbx+0x10]
+       vmovdqu  ymmword ptr [rsi+0x10], ymm0
+       mov      rax, qword ptr [rbx+0x30]
+       mov      qword ptr [rsi+0x30], rax
+       vzeroupper
+       pop      rbx
        pop      rsi
-       pop      rdi
        ret
```

## Benchmark


```cs
using BenchmarkDotNet.Attributes;
using BenchmarkDotNet.Running;
using System.Runtime.CompilerServices;

BenchmarkSwitcher.FromAssembly(typeof(Benchmarks).Assembly).Run(args);

public class Benchmarks
{

src/coreclr/jit/lower.cpp
@@ -10136,37 +10136,25 @@ void Lowering::ContainCheckBitCast(GenTreeUnOp* node)
 }
 
 //------------------------------------------------------------------------
-// TryLowerBlockStoreAsGcBulkCopyCall: Lower a block store node as a CORINFO_HELP_BULK_WRITEBARRIER call
+// LowerBlockStoreAsGcBulkCopyCall: Lower a block store node as a CORINFO_HELP_BULK_WRITEBARRIER call
 //
 // Arguments:
-//    blkNode - The block store node to lower
+//    blk - The block store node to lower
 //
-bool Lowering::TryLowerBlockStoreAsGcBulkCopyCall(GenTreeBlk* blk)
+void Lowering::LowerBlockStoreAsGcBulkCopyCall(GenTreeBlk* blk)
 {
-    if (m_compiler->opts.OptimizationDisabled())
-    {
-        return false;
-    }
-
-    // Replace STORE_BLK (struct copy) with CORINFO_HELP_BULK_WRITEBARRIER which performs
-    // bulk copy for byrefs.
-    const unsigned bulkCopyThreshold = 4;
-    if (!blk->OperIs(GT_STORE_BLK) || blk->OperIsInitBlkOp() || blk->IsVolatile() ||
-        (blk->GetLayout()->GetGCPtrCount() < bulkCopyThreshold))
-    {
-        return false;
-    }
+    assert(blk->OperIs(GT_STORE_BLK));
+    assert(blk->GetLayout()->HasGCPtr());
+    assert(!blk->OperIsInitBlkOp());
+    assert(!blk->IsAddressNotOnHeap(m_compiler));
+    assert(!blk->IsVolatile());
+    assert(!blk->Data()->OperIs(GT_IND) || !blk->Data()->AsIndir()->IsVolatile());
 
     GenTree* dest = blk->Addr();
     GenTree* data = blk->Data();
 
     if (data->OperIs(GT_IND))
     {
-        if (data->AsIndir()->IsVolatile())
-        {
-            return false;
-        }
-
         // Drop GT_IND nodes
         BlockRange().Remove(data);
         data = data->AsIndir()->Addr();
@@ -10242,8 +10230,102 @@ bool Lowering::TryLowerBlockStoreAsGcBulkCopyCall(GenTreeBlk* blk)
     };
     wrapWithNullcheck(dest);
     wrapWithNullcheck(data);
+}
 
-    return true;
+//------------------------------------------------------------------------
+// LowerCopyBlockStore: Shared lowering of a struct-copy
+//
+// Arguments:
+//    blkNode - The block store node to lower. Must be a copy (non-InitBlk).
+//
+void Lowering::LowerCopyBlockStore(GenTreeBlk* blkNode)
+{
+    assert(blkNode->OperIs(GT_STORE_BLK));
+    assert(!blkNode->OperIsInitBlkOp());
+
+    // For struct copies (containing GC slots) we might want to split the big store into a sequence
+    // of smaller stores.
+
+    GenTree*       src     = blkNode->Data();
+    GenTree*       dstAddr = blkNode->Addr();
+    const unsigned size    = blkNode->Size();
+
+    assert(src->OperIs(GT_IND, GT_LCL_VAR, GT_LCL_FLD));
+    src->SetContained();
+
+    if (src->OperIs(GT_LCL_VAR))
+    {
+        // TODO-1stClassStructs: for now we can't work with STORE_BLOCK source in register.
+        const unsigned srcLclNum = src->AsLclVar()->GetLclNum();
+        m_compiler->lvaSetVarDoNotEnregister(srcLclNum DEBUGARG(DoNotEnregisterReason::StoreBlkSrc));
+    }
+
+    ClassLayout* layout    = blkNode->GetLayout();
+    bool         doCpObj   = layout->HasGCPtr();
+    const bool   isNotHeap = blkNode->IsAddressNotOnHeap(m_compiler);
+
+#ifndef TARGET_WASM
+    // x86/x64 has a SIMD-aware Memcpy unroll threshold; SIMD is only usable
+    // when we are not going through the per-slot write-barrier path.
+    const bool     canUseSimd  = !doCpObj || isNotHeap;
+    const unsigned unrollLimit = m_compiler->getUnrollThreshold(Compiler::UnrollKind::Memcpy, canUseSimd);
+#endif
+
+    if (doCpObj && isNotHeap)
+    {
+        // No write barriers are needed if the destination is known to be outside of the GC heap.
+        // If the layout contains a byref, then we know it must live on the stack.
+        doCpObj = false;
+#if !defined(JIT32_GCENCODER) && !defined(TARGET_WASM)
+        if (size <= unrollLimit)
+        {
+            // If the size is small enough to unroll then we need to mark the block as non-interruptible
+            // to actually allow unrolling. The generated code does not report GC references loaded in the
+            // temporary register(s) used for copying. This is not supported for the JIT32_GCENCODER.
+            blkNode->gtBlkOpGcUnsafe = true;
+        }
+#endif
+    }
+
+    if (doCpObj)
+    {
+        // Try to decompose this STORE_BLK into a sequence of independent indirections.
+        // It is faster than the bulk copy for structs with just a few GC slots.
+        if (!TryDecomposeBlockStoreAsIndirs(blkNode))
+        {
+            // Otherwise, use the bulk copy.
+            LowerBlockStoreAsGcBulkCopyCall(blkNode);
+        }
+        return;
+    }
+
+#ifdef TARGET_WASM
+    // Use the wasm `memory.copy` instruction.
+    blkNode->gtBlkOpKind = GenTreeBlk::BlkOpKindNativeOpcode;
+    if (src->OperIs(GT_IND) && ((src->gtFlags & GTF_IND_NONFAULTING) == 0))
+    {
+        SetMultiplyUsed(src->AsIndir()->Addr() DEBUGARG("LowerCopyBlockStore source address (indirection)"));
+    }
+
+    if ((blkNode->gtFlags & GTF_IND_NONFAULTING) == 0)
+    {
+        SetMultiplyUsed(dstAddr DEBUGARG("LowerCopyBlockStore destination address"));
+    }
+#else
+    if (size <= unrollLimit)
+    {
+        blkNode->gtBlkOpKind = GenTreeBlk::BlkOpKindUnroll;
+        if (src->OperIs(GT_IND))
+        {
+            ContainBlockStoreAddress(blkNode, size, src->AsIndir()->Addr(), src->AsIndir());
+        }
+        ContainBlockStoreAddress(blkNode, size, dstAddr, nullptr);
+        return;
+    }
+
+    // Use memcpy
+    LowerBlockStoreAsHelperCall(blkNode);
+#endif // TARGET_WASM
 }
 
 //------------------------------------------------------------------------
@@ -12080,8 +12162,164 @@ void Lowering::LowerBlockStoreCommon(GenTreeBlk* blkNode)
         return;
     }
 
-    LowerBlockStore(blkNode);
+    if (blkNode->OperIsInitBlkOp())
+    {
+        LowerBlockStore(blkNode);
+    }
+    else
+    {
+        LowerCopyBlockStore(blkNode);
+    }
+
     LowerStoreIndirCoalescing(blkNode);
+}
+
+//------------------------------------------------------------------------
+// TryDecomposeBlockStoreAsIndirs: try to lower a struct copy GT_STORE_BLK
+//   that has GC pointers as a sequence of independent indirections.
+//
+// Arguments:
+//   blkNode - the GT_STORE_BLK to potentially decompose.
+//
+// Return value:
+//   true if the node was decomposed (and bashed to a NOP), false otherwise.
+//
+bool Lowering::TryDecomposeBlockStoreAsIndirs(GenTreeBlk* blkNode)
+{
+    assert(blkNode->OperIs(GT_STORE_BLK));
+    assert(!blkNode->OperIsInitBlkOp());
+    assert(blkNode->GetLayout()->HasGCPtr());
+
+    ClassLayout* layout = blkNode->GetLayout();
+    GenTree*     src    = blkNode->Data();
+
+    // Layouts containing GC pointers are required (by the runtime) to have a
+    // pointer-size-multiple total size.
+    assert((layout->GetSize() == (layout->GetSlotCount() * TARGET_POINTER_SIZE)));
+    assert(src->OperIs(GT_IND, GT_LCL_VAR, GT_LCL_FLD));
+
+    // Always decompose for volatile blocks as the bulk helper doesn't support those.
+    if (!(blkNode->IsVolatile() || (src->OperIs(GT_IND) && src->AsIndir()->IsVolatile())))
+    {
+        // More than 3 GC pointers, use the bulk copy helper.
+        // TODO-CQ: find a better heuristic here.
+        if (layout->GetGCPtrCount() >= 4)
+        {
+            return false;
+        }
+
+        // We assume that the bulk helper is also a better option for Tier0/cold blocks.
+        if ((layout->GetGCPtrCount() > 1) &&
+            (!m_compiler->opts.OptimizationEnabled() || ((m_block != nullptr) && (m_block->isRunRarely()))))
+        {
+            return false;
+        }
+    }
+
+    JITDUMP("Decomposing STORE_BLK [%06u] as individual indirections\n", m_compiler->dspTreeID(blkNode));
+
+    // Spill dstAddr (and srcAddr when src is an indirection) into temp locals so
+    // we can compute offset-based addresses multiple times.
+    LIR::Use        dstAddrUse(BlockRange(), &blkNode->Addr(), blkNode);
+    const var_types dstAddrType   = blkNode->Addr()->TypeGet();
+    const unsigned  dstAddrLclNum = dstAddrUse.ReplaceWithLclVar(m_compiler);
+    unsigned        srcAddrLclNum = BAD_VAR_NUM;
+    var_types       srcAddrType   = TYP_UNDEF;
+    unsigned        srcLclNum     = BAD_VAR_NUM;
+    unsigned        srcLclOffs    = 0;
+
+    if (src->OperIs(GT_IND))
+    {
+        LIR::Use srcAddrUse(BlockRange(), &src->AsIndir()->Addr(), src);
+        srcAddrType   = src->AsIndir()->Addr()->TypeGet();
+        srcAddrLclNum = srcAddrUse.ReplaceWithLclVar(m_compiler);
+    }
+    else
+    {
+        srcLclNum  = src->AsLclVarCommon()->GetLclNum();
+        srcLclOffs = src->AsLclVarCommon()->GetLclOffs();
+        m_compiler->lvaSetVarDoNotEnregister(srcLclNum DEBUGARG(DoNotEnregisterReason::BlockOp));
+    }
+
+    auto offsetAddr = [&](unsigned lclNum, var_types lclType, unsigned offset) -> GenTree* {
+        GenTree* addr = m_compiler->gtNewLclvNode(lclNum, lclType);
+        if (offset != 0)
+        {
+            addr = m_compiler->gtNewOperNode(GT_ADD, varTypeIsGC(lclType) ? TYP_BYREF : TYP_I_IMPL, addr,
+                                             m_compiler->gtNewIconNode(static_cast<ssize_t>(offset), TYP_I_IMPL));
+        }
+        return addr;
+    };
+
+    const GenTreeFlags dstIndFlags = blkNode->gtFlags & GTF_IND_FLAGS;
+    const GenTreeFlags srcIndFlags = src->OperIs(GT_IND) ? (src->gtFlags & GTF_IND_FLAGS) : GTF_EMPTY;
+
+    auto emitStore = [&](unsigned offset, var_types scalarType, ClassLayout* runLayout) {
+        const var_types valType = (runLayout != nullptr) ? TYP_STRUCT : scalarType;
+
+        GenTree* srcVal;
+        if (src->OperIs(GT_IND))
+        {
+            srcVal = m_compiler->gtNewLoadValueNode(valType, runLayout, offsetAddr(srcAddrLclNum, srcAddrType, offset),
+                                                    srcIndFlags);
+        }
+        else
+        {
+            srcVal = m_compiler->gtNewLclFldNode(srcLclNum, valType, srcLclOffs + offset, runLayout);
+        }
+
+        GenTree* store =
+            m_compiler->gtNewStoreValueNode(valType, runLayout, offsetAddr(dstAddrLclNum, dstAddrType, offset), srcVal,
+                                            dstIndFlags);
+
+        LIR::Range range = LIR::SeqTree(m_compiler, store);
+        GenTree*   first = range.FirstNode();
+        GenTree*   last  = range.LastNode();
+        BlockRange().InsertBefore(blkNode, std::move(range));
+        LowerRange(first, last);
+    };
+
+    const unsigned slots = layout->GetSlotCount();
+
+    for (unsigned i = 0; i < slots;)
+    {
+        if (layout->IsGCPtr(i))
+        {
+            // One STOREIND<gctype> per GC slot. Lowers to ASSIGN_REF /
+            // CHECKED_ASSIGN_REF via the normal write-barrier lowering.
+            emitStore(i * TARGET_POINTER_SIZE, layout->GetGCPtrType(i), nullptr);
+            i++;
+        }
+        else
+        {
+            // Coalesce a contiguous run of non-GC slots into one store: a
+            // single-slot run becomes STOREIND<TYP_I_IMPL>; a wider run becomes
+            // a STORE_BLK with a non-GC layout so the standard block-copy
+            // lowering can use SIMD as appropriate.
+            const unsigned runStart = i;
+            do
+            {
+                i++;
+            } while ((i < slots) && !layout->IsGCPtr(i));
+
+            const unsigned runSlots = i - runStart;
+            ClassLayout*   runLayout =
+                (runSlots > 1) ? m_compiler->typGetBlkLayout(runSlots * TARGET_POINTER_SIZE) : nullptr;
+
+            emitStore(runStart * TARGET_POINTER_SIZE, TYP_I_IMPL, runLayout);
+        }
+    }
+
+    // Remove the now-orphan operand nodes and bash blkNode to a NOP so the
+    // LowerNode driver continues from blkNode->gtNext.
+    BlockRange().Remove(blkNode->Addr());
+    if (src->OperIs(GT_IND))
+    {
+        BlockRange().Remove(src->AsIndir()->Addr());
+    }
+    BlockRange().Remove(src);
+    blkNode->gtBashToNOP();
+    return true;
 }
 
 //------------------------------------------------------------------------

src/coreclr/jit/codegen.h
@@ -1175,7 +1175,6 @@ protected:
     void genCodeForReturnTrap(GenTreeOp* tree);
     void genCodeForStoreInd(GenTreeStoreInd* tree);
     void genCodeForSwap(GenTreeOp* tree);
-    void genCodeForCpObj(GenTreeBlk* cpObjNode);
     void genCodeForCpBlkRepMovs(GenTreeBlk* cpBlkNode);
     void genCodeForCpBlkUnroll(GenTreeBlk* cpBlkNode);
     void genCodeForPhysReg(GenTreePhysReg* tree);

src/coreclr/jit/codegenarm.cpp
@@ -811,128 +811,6 @@ void CodeGen::genCodeForNegNot(GenTreeOp* tree)
     genProduceReg(tree);
 }
 
-// Generate code for CpObj nodes which copy structs that have interleaved
-// GC pointers.
-// For this case we'll generate a sequence of loads/stores in the case of struct
-// slots that don't contain GC pointers.  The generated code will look like:
-// ldr tempReg, [R13, #8]
-// str tempReg, [R14, #8]
-//
-// In the case of a GC-Pointer we'll call the ByRef write barrier helper
-// who happens to use the same registers as the previous call to maintain
-// the same register requirements and register killsets:
-// bl CORINFO_HELP_ASSIGN_BYREF
-//
-// So finally an example would look like this:
-// ldr tempReg, [R13, #8]
-// str tempReg, [R14, #8]
-// bl CORINFO_HELP_ASSIGN_BYREF
-// ldr tempReg, [R13, #8]
-// str tempReg, [R14, #8]
-// bl CORINFO_HELP_ASSIGN_BYREF
-// ldr tempReg, [R13, #8]
-// str tempReg, [R14, #8]
-void CodeGen::genCodeForCpObj(GenTreeBlk* cpObjNode)
-{
-    GenTree*  dstAddr       = cpObjNode->Addr();
-    GenTree*  source        = cpObjNode->Data();
-    var_types srcAddrType   = TYP_BYREF;
-    bool      sourceIsLocal = false;
-    regNumber dstReg        = REG_NA;
-    regNumber srcReg        = REG_NA;
-
-    assert(source->isContained());
-    if (source->OperIs(GT_IND))
-    {
-        GenTree* srcAddr = source->gtGetOp1();
-        assert(!srcAddr->isContained());
-        srcAddrType = srcAddr->TypeGet();
-    }
-    else
-    {
-        noway_assert(source->IsLocal());
-        sourceIsLocal = true;
-    }
-
-    bool dstOnStack = cpObjNode->IsAddressNotOnHeap(m_compiler);
-
-#ifdef DEBUG
-    assert(!dstAddr->isContained());
-
-    // This GenTree node has data about GC pointers, this means we're dealing
-    // with CpObj.
-    assert(cpObjNode->GetLayout()->HasGCPtr());
-#endif // DEBUG
-
-    // Consume the operands and get them into the right registers.
-    // They may now contain gc pointers (depending on their type; gcMarkRegPtrVal will "do the right thing").
-    genConsumeBlockOp(cpObjNode, REG_WRITE_BARRIER_DST_BYREF, REG_WRITE_BARRIER_SRC_BYREF, REG_NA);
-    gcInfo.gcMarkRegPtrVal(REG_WRITE_BARRIER_SRC_BYREF, srcAddrType);
-    gcInfo.gcMarkRegPtrVal(REG_WRITE_BARRIER_DST_BYREF, dstAddr->TypeGet());
-
-    // Temp register used to perform the sequence of loads and stores.
-    regNumber tmpReg = internalRegisters.Extract(cpObjNode);
-    assert(genIsValidIntReg(tmpReg));
-
-    if (cpObjNode->IsVolatile())
-    {
-        // issue a full memory barrier before & after a volatile CpObj operation
-        instGen_MemoryBarrier();
-    }
-
-    emitter*     emit   = GetEmitter();
-    ClassLayout* layout = cpObjNode->GetLayout();
-    unsigned     slots  = layout->GetSlotCount();
-
-    // If we can prove it's on the stack we don't need to use the write barrier.
-    if (dstOnStack)
-    {
-        for (unsigned i = 0; i < slots; ++i)
-        {
-            emitAttr attr = emitTypeSize(layout->GetGCPtrType(i));
-
-            emit->emitIns_R_R_I(INS_ldr, attr, tmpReg, REG_WRITE_BARRIER_SRC_BYREF, TARGET_POINTER_SIZE,
-                                INS_FLAGS_DONT_CARE, INS_OPTS_LDST_POST_INC);
-            emit->emitIns_R_R_I(INS_str, attr, tmpReg, REG_WRITE_BARRIER_DST_BYREF, TARGET_POINTER_SIZE,
-                                INS_FLAGS_DONT_CARE, INS_OPTS_LDST_POST_INC);
-        }
-    }
-    else
-    {
-        unsigned gcPtrCount = layout->GetGCPtrCount();
-
-        unsigned i = 0;
-        while (i < slots)
-        {
-            if (!layout->IsGCPtr(i))
-            {
-                emit->emitIns_R_R_I(INS_ldr, EA_PTRSIZE, tmpReg, REG_WRITE_BARRIER_SRC_BYREF, TARGET_POINTER_SIZE,
-                                    INS_FLAGS_DONT_CARE, INS_OPTS_LDST_POST_INC);
-                emit->emitIns_R_R_I(INS_str, EA_PTRSIZE, tmpReg, REG_WRITE_BARRIER_DST_BYREF, TARGET_POINTER_SIZE,
-                                    INS_FLAGS_DONT_CARE, INS_OPTS_LDST_POST_INC);
-            }
-            else
-            {
-                genEmitHelperCall(CORINFO_HELP_ASSIGN_BYREF, 0, EA_PTRSIZE);
-                gcPtrCount--;
-            }
-            ++i;
-        }
-        assert(gcPtrCount == 0);
-    }
-
-    if (cpObjNode->IsVolatile())
-    {
-        // issue a full memory barrier before & after a volatile CpObj operation
-        instGen_MemoryBarrier();
-    }
-
-    // Clear the gcInfo for registers of source and dest.
-    // While we normally update GC info prior to the last instruction that uses them,
-    // these actually live into the helper call.
-    gcInfo.gcMarkRegSetNpt(RBM_WRITE_BARRIER_SRC_BYREF | RBM_WRITE_BARRIER_DST_BYREF);
-}
-
 //------------------------------------------------------------------------
 // genCodeForShiftLong: Generates the code sequence for a GenTree node that
 // represents a three operand bit shift or rotate operation (<<Hi, >>Lo).

commit 2df526ff6336c2127591833b21d5c74215c22ead

JIT: fix GC holes in large stack-target struct block ops
LowerCopyBlockStore/LowerInitBlockStore dropped write barriers for a
stack destination containing GC pointers (correct) but then, for sizes
above the unroll threshold, fell back to the managed CORINFO_HELP_MEMCPY
(SpanHelpers.Memmove) / CORINFO_HELP_MEMZERO (SpanHelpers.ClearWithoutReferences)
helpers. Those calls are GC-safe points, so a GC triggered mid-operation
could observe torn GC pointers in the partially-written, GC-reported
stack destination and corrupt the heap.

Only take the "no write barriers" copy shortcut when the copy is actually
unrolled into a single non-interruptible region; otherwise stay on the
GC-aware CpObj path. Route zeroing of any GC-pointer struct (stack or
heap) to the atomic pointer-sized zeroing loop instead of the helper.

Copy regression introduced by #128542.

Fixes #130286

Co-authored-by: Copilot <223556219+Copilot@users.noreply.github.com>

src/coreclr/jit/lower.cpp
@@ -10326,21 +10326,24 @@ void Lowering::LowerCopyBlockStore(GenTreeBlk* blkNode)
 #if !defined(JIT32_GCENCODER)
     if (doCpObj && isNotHeap)
     {
-        // No write barriers are needed if the destination is known to be outside of the GC heap.
-        // If the layout contains a byref, then we know it must live on the stack.
-        doCpObj = false;
 #if !defined(TARGET_WASM)
+        // No write barriers are needed for a stack destination, but only take this shortcut when the
+        // copy is small enough to be unrolled into a single non-interruptible region. Otherwise it is
+        // lowered to CORINFO_HELP_MEMCPY (Memmove), a GC-safe point that could observe torn GC pointers
+        // in the partially-written, GC-reported destination; larger copies stay on the CpObj path.
         if (size <= unrollLimit)
         {
-            // If the size is small enough to unroll then we need to mark the block as non-interruptible
-            // to actually allow unrolling. The generated code does not report GC references loaded in the
-            // temporary register(s) used for copying. This is not supported for the JIT32_GCENCODER, so
-            // on that target we keep doCpObj=true above and stay on the GC-aware CpObj path; the lowering
-            // heuristics in TryDecomposeBlockStoreAsIndirs then choose between per-slot decomposition and
-            // the bulk write-barrier helper.
+            doCpObj = false;
+
+            // Mark the block non-interruptible so it can be unrolled: the copy does not report the GC
+            // references held in its temporary register(s).
             blkNode->gtBlkOpGcUnsafe = true;
         }
-#endif
+#else  // TARGET_WASM
+       // WASM lowers this to a single `memory.copy` opcode (not a GC-safe point), so it is safe for
+       // any size.
+        doCpObj = false;
+#endif // TARGET_WASM
     }
 #endif // !JIT32_GCENCODER
 
@@ -10460,7 +10463,13 @@ void Lowering::LowerInitBlockStore(GenTreeBlk* blkNode)
         return;
     }
 
-    if (blkNode->IsZeroingGcPointersOnHeap())
+    // Zeroing a GC-pointer struct must not use the CORINFO_HELP_MEMSET/MEMZERO helper: it is a
+    // GC-safe point that could observe a torn GC pointer in the partially-written, GC-reported
+    // destination. Use the atomic pointer-sized loop for both stack and heap destinations.
+    const bool isZeroingGcPointers =
+        blkNode->OperIs(GT_STORE_BLK) && src->IsIntegralConst(0) && blkNode->ContainsReferences();
+
+    if (isZeroingGcPointers)
     {
         blkNode->gtBlkOpKind = GenTreeBlk::BlkOpKindLoop;
 #if FEATURE_HAS_ZERO_REG

src/tests/JIT/Regression/JitBlue/Runtime_130286/Runtime_130286.cs
@@ -1,0 +1,116 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+// Regression test for two related GC holes in stack-target struct block ops.
+// When a struct that contains GC pointers lives on the stack and is larger than
+// the SIMD unroll threshold, the JIT used to drop the write barriers (correct for
+// a stack destination) but then fall back to a managed helper call:
+//   * copies were lowered to CORINFO_HELP_MEMCPY (SpanHelpers.Memmove);
+//   * zeroing was lowered to CORINFO_HELP_MEMZERO (SpanHelpers.ClearWithoutReferences).
+// Both helpers are normal managed calls and therefore GC-safe points, so a GC
+// triggered mid-operation could observe torn GC pointers in the partially-written,
+// GC-reported stack destination and corrupt the heap. The fix keeps such copies on
+// the GC-aware CpObj path and such zeroing on the atomic pointer-sized loop.
+//
+// The structs below are intentionally larger than every unroll threshold so the
+// block ops are lowered to the helper calls. Needs GCStress to reliably reproduce;
+// CI pipelines that exercise GCStress will catch a regression.
+
+namespace Runtime_130286;
+
+using System.Runtime.CompilerServices;
+using Xunit;
+
+public struct BigGcStruct
+{
+    // 40 object references => 320 bytes on 64-bit, larger than the largest
+    // (AVX-512) Memcpy unroll threshold of 256 bytes.
+    public object O00, O01, O02, O03, O04, O05, O06, O07, O08, O09;
+    public object O10, O11, O12, O13, O14, O15, O16, O17, O18, O19;
+    public object O20, O21, O22, O23, O24, O25, O26, O27, O28, O29;
+    public object O30, O31, O32, O33, O34, O35, O36, O37, O38, O39;
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public BigGcStruct(object o)
+    {
+        O00 = o; O01 = o; O02 = o; O03 = o; O04 = o; O05 = o; O06 = o; O07 = o; O08 = o; O09 = o;
+        O10 = o; O11 = o; O12 = o; O13 = o; O14 = o; O15 = o; O16 = o; O17 = o; O18 = o; O19 = o;
+        O20 = o; O21 = o; O22 = o; O23 = o; O24 = o; O25 = o; O26 = o; O27 = o; O28 = o; O29 = o;
+        O30 = o; O31 = o; O32 = o; O33 = o; O34 = o; O35 = o; O36 = o; O37 = o; O38 = o; O39 = o;
+    }
+}
+
+public struct HugeGcStruct
+{
+    // 80 object references => 640 bytes on 64-bit, larger than the largest
+    // (AVX-512) Memset unroll threshold of 512 bytes.
+    public object O00, O01, O02, O03, O04, O05, O06, O07, O08, O09;
+    public object O10, O11, O12, O13, O14, O15, O16, O17, O18, O19;
+    public object O20, O21, O22, O23, O24, O25, O26, O27, O28, O29;
+    public object O30, O31, O32, O33, O34, O35, O36, O37, O38, O39;
+    public object O40, O41, O42, O43, O44, O45, O46, O47, O48, O49;
+    public object O50, O51, O52, O53, O54, O55, O56, O57, O58, O59;
+    public object O60, O61, O62, O63, O64, O65, O66, O67, O68, O69;
+    public object O70, O71, O72, O73, O74, O75, O76, O77, O78, O79;
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    public HugeGcStruct(object o)
+    {
+        O00 = o; O09 = o; O79 = o;
+    }
+}
+
+public class Runtime_130286
+{
+    [Fact]
+    public static int TestEntryPoint()
+    {
+        for (int i = 0; i < 200_000; i++)
+        {
+            // Allocate garbage to increase GC pressure/frequency.
+            object o = new byte[8];
+            if (!CopyAndCheck(o) || !ZeroAndCheck(o))
+            {
+                return -1;
+            }
+        }
+
+        return 100;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private static bool CopyAndCheck(object o)
+    {
+        // The ctor builds the struct into a stack temp ...
+        BigGcStruct src = new BigGcStruct(o);
+        // ... which is then copied stack-to-stack. This is the STORE_BLK that used
+        // to be lowered to CORINFO_HELP_MEMCPY for this GC-pointer-containing struct.
+        BigGcStruct dst = src;
+        // Passing 'dst' by reference forces it to be address-exposed and reported to the GC.
+        return Check(in dst);
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private static bool Check(in BigGcStruct s)
+    {
+        // Touch every slot so a torn GC pointer would be observed.
+        return s.O00 != null && s.O09 != null && s.O19 != null && s.O29 != null && s.O39 != null;
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private static bool ZeroAndCheck(object o)
+    {
+        HugeGcStruct s = new HugeGcStruct(o);
+        bool populated = CheckHuge(in s);
+        // Re-zeroing a live, GC-reported stack struct. This is the STORE_BLK that used
+        // to be lowered to CORINFO_HELP_MEMZERO for this GC-pointer-containing struct.
+        s = default;
+        return populated && !CheckHuge(in s);
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private static bool CheckHuge(in HugeGcStruct s)
+    {
+        return s.O00 != null || s.O09 != null || s.O79 != null;
+    }
+}

src/tests/JIT/Regression/Regression_ro_2.csproj
@@ -112,6 +112,7 @@
     <Compile Include="JitBlue\Runtime_130189\Runtime_130189.cs" />
     <Compile Include="JitBlue\Runtime_130216\Runtime_130216.cs" />
     <Compile Include="JitBlue\Runtime_130248\Runtime_130248.cs" />
+    <Compile Include="JitBlue\Runtime_130286\Runtime_130286.cs" />
     <Compile Include="JitBlue\Runtime_10337\Runtime_10337.cs" />
     <Compile Include="JitBlue\Runtime_125160\Runtime_125160.cs" />
   </ItemGroup>