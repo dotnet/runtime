b39a8137df07a3d156d039f76cabe44a18bdb6dc
Michał Petryka <35800402+MichalPetryka@users.noreply.github.com>
2026-07-10 21:47:46 +0200

    Change signature of MulticastDebuggerTraceHelper

--- src/coreclr/vm/corelib.h
+++ src/coreclr/vm/corelib.h
@@ -1127,7 +1127,7 @@ DEFINE_METHOD(STUBHELPERS,          PROFILER_BEGIN_TRANSITION_CALLBACK, Profiler
 DEFINE_METHOD(STUBHELPERS,          PROFILER_END_TRANSITION_CALLBACK,   ProfilerEndTransitionCallback,   SM_PtrVoid_RetVoid)
 #endif
 
-DEFINE_METHOD(STUBHELPERS,          MULTICAST_DEBUGGER_TRACE_HELPER,    MulticastDebuggerTraceHelper,    SM_Obj_Int_RetVoid)
+DEFINE_METHOD(STUBHELPERS,          MULTICAST_DEBUGGER_TRACE_HELPER,    MulticastDebuggerTraceHelper,    NoSig)
 
 DEFINE_CLASS(CLEANUP_WORK_LIST_ELEMENT,     StubHelpers,            CleanupWorkListElement)
 
