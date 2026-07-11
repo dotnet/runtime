be27aa9f4d6012a0a61788b866c438ce49b8a34f
Jan Kotas <jkotas@microsoft.com>
2026-07-11 06:29:32 -0700

    Apply suggestion from @jkotas

--- src/libraries/System.Runtime/tests/System.Runtime.Tests/TestEquivalentTypes/System.TestEquivalentTypes.cs
+++ src/libraries/System.Runtime/tests/System.Runtime.Tests/TestEquivalentTypes/System.TestEquivalentTypes.cs
@@ -5,9 +5,7 @@ using System.Runtime.InteropServices;
 
 namespace System.Tests
 {
-    // This file is compiled into both System.Runtime.Tests.dll and System.TestEquivalentTypes.dll.
-    // The two resulting types are type-equivalent because they have the same name, namespace, and
-    // matching [TypeIdentifier] scope and identifier. See TypeIdentifierAttribute for details.
+    // This file is compiled into both System.Runtime.Tests.dll and System.TestEquivalentTypes.dll to create type-equivalent types.
     [TypeIdentifier("31F8EDB4-A306-4EBB-8C3D-B9F4B28F1DE9", "7B43F12E-AEF2-4987-B01D-B8B6B39E8C41")]
     public delegate void EquivalentDelegate();
 }
