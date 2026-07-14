778f57f0b5d491a9d6d5b65aac8656a2a42f0e51
Pranav Senthilnathan <pranas@microsoft.com>
2026-07-13 21:19:31 -0400

    Address copilot feedback

--- src/libraries/System.Security.Cryptography/tests/RSASignatureFormatterTests.NoKey.cs
+++ src/libraries/System.Security.Cryptography/tests/RSASignatureFormatterTests.NoKey.cs
@@ -1,10 +1,12 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Microsoft.DotNet.XUnitExtensions;
 using Xunit;
 
 namespace System.Security.Cryptography.Tests
 {
+    [SkipOnPlatform(TestPlatforms.Browser, "Not supported on Browser")]
     public static class RSASignatureFormatterTests_NoKey
     {
         [Fact]
