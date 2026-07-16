2f7ae92e105c665581632866fff45e47a073c83f
Vlad Brezae <brezaevlad@gmail.com>
2026-07-17 00:35:24 +0300

    Disable r2r test on Release (#130769)
    Disables recently added
    ILCompiler.ReadyToRun.Tests.TestCases.R2RTestSuites.ArmThumbBitHotColdRuntimeFunctions
    on Release, where it fails. This test passes
    `JitStressProcedureSplitting=1` to the JIT, option which is only valid
    on Debug/Checked builds.
    
    ---------
    
    Co-authored-by: Jackson Schuster <36744439+jtschuster@users.noreply.github.com>

--- src/coreclr/tools/aot/ILCompiler.ReadyToRun.Tests/TestCases/R2RTestSuites.cs
+++ src/coreclr/tools/aot/ILCompiler.ReadyToRun.Tests/TestCases/R2RTestSuites.cs
@@ -189,7 +189,8 @@ public class R2RTestSuites
         }
     }
 
-    [Fact]
+    // JitStressProcedureSplitting is only available in Debug/Checked JIT builds.
+    [ConditionalFact(typeof(TestPaths), nameof(TestPaths.IsNotReleaseCoreCLR))]
     public void ArmThumbBitHotColdRuntimeFunctions()
     {
         var hotColdSplitting = new CompiledAssembly
