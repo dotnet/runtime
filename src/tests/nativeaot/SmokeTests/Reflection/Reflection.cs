dotnet/runtime

commit f41747370bfdd6887c4ae4a27c5bc219f838eedc

Use dispatch cells for generic virtual method dispatch (#129609)
This puts generic virtual method dispatch on the same plan as interface
method dispatch.

Benefits:

* The cost of generic virtual methods went down (still not quite same as
interface calls due to fat pointers, but closer)
  * Monomorpic GVM callsite: Before: ~2.45 ns/call. After: 1.74 ns/call
* Polymorphic GVM callsite: Before: ~2.45 ns/call. After: 2.10 ns/call.
* GVM methods no longer implicitly considered targets of reflection due
to LDTOKEN used as implementation detail.
* The weird `RuntimeMethodHandle` for async method variants that
reflection should never see is gone

This is three commits:

* First just deletes some dead code I noticed.
* Second moves some of the dispatch resolution to CoreLib. We've been
breaking the "interface dispatch lives in Runtime.Base" boundary for a
while. This puts us closer to it living in CoreLib. We can move rest of
the interface resolution later, didn't want to perturb this too much.
The cache itself stays in Runtime.Base because the assembly helper can
only handle one. (We can decide the assembly helper is also owned by
CoreLib I guess, but we don't currently have such layer.) I think
copilot could easily undo this if this is not the split we want.
* Third commit switches the GVM calls to use the dispatch cell. We need
a resolve helper variant because GVM dispatch is a resolver due to fat
calls, not a dispatcher.

---------

Co-authored-by: Copilot Autofix powered by AI <175728472+Copilot@users.noreply.github.com>

src/coreclr/tools/Common/Compiler/DependencyAnalysis/GVMDependenciesNode.cs
@@ -55,6 +55,7 @@ namespace ILCompiler.DependencyAnalysis
             {
                 yield return new DependencyListEntry(factory.TypeGVMEntries(_method.OwningType.GetTypeDefinition()), "Resolution metadata");
             }
+            yield return new DependencyListEntry(factory.AnalysisCharacteristic("GenericVirtualMethodsPresent"), "Runtime GVM resolution needed");
 #endif
         }
 

commit 4750b7fffdb14175df29747481b7b041f7b16539

Track reflectability of delegates pointing to generic virtuals
`Delegate.Method` needs to work since it's not a trim-unsafe API.

I thought I maybe broke this in #129609, but the added test doesn't work in .NET 10 either, so I only broke a small aspect of this (GVMs were implicitly considered targets of reflection because of the `RuntimeMethodHandle` used as implementation detail).

src/coreclr/tools/Common/Compiler/DependencyAnalysis/GVMDependenciesNode.cs
@@ -182,6 +182,7 @@ namespace ILCompiler.DependencyAnalysis
 #if !READYTORUN
                                 TypeSystemEntity origin = (implementingMethodInstantiation.OwningType != potentialOverrideType) ? potentialOverrideType : null;
                                 factory.MetadataManager.NoteOverridingMethod(_method, implementingMethodInstantiation, origin);
+                                factory.MetadataManager.GetDependenciesForOverridingMethod(ref dynamicDependencies, factory, _method, implementingMethodInstantiation);
 #endif
                             }
 
@@ -240,6 +241,7 @@ namespace ILCompiler.DependencyAnalysis
                             dynamicDependencies.Add(new CombinedDependencyListEntry(node, null, "DerivedMethodInstantiation"));
 #if !READYTORUN
                         factory.MetadataManager.NoteOverridingMethod(_method, instantiatedTargetMethod);
+                        factory.MetadataManager.GetDependenciesForOverridingMethod(ref dynamicDependencies, factory, _method, instantiatedTargetMethod);
 
                         foundImpl = true;
 #endif

src/tests/nativeaot/SmokeTests/Reflection/Reflection.cs
@@ -2322,25 +2322,41 @@ internal static class ReflectionTest
         abstract class Base
         {
             public virtual void VirtualMethod() { }
+            public virtual void VirtualMethodShared<T>() { }
+            public virtual void VirtualMethodUnshared<T>() { }
             public abstract void AbstractMethod();
+            public abstract void AbstractMethodShared<T>();
+            public abstract void AbstractMethodUnshared<T>();
         }
 
         class Derived : Base, IBar
         {
             public override void AbstractMethod() { }
+            public override void AbstractMethodShared<T>() { }
+            public override void AbstractMethodUnshared<T>() { }
             public override void VirtualMethod() { }
+            public override void VirtualMethodShared<T>() { }
+            public override void VirtualMethodUnshared<T>() { }
             void IFoo.InterfaceMethod() { }
+            void IFoo.InterfaceMethodShared<T>() { }
+            void IFoo.InterfaceMethodUnshared<T>() { }
         }
 
         interface IFoo
         {
             void InterfaceMethod();
+            void InterfaceMethodShared<T>();
+            void InterfaceMethodUnshared<T>();
             void DefaultInterfaceMethod() { }
+            void DefaultInterfaceMethodShared<T>() { }
+            void DefaultInterfaceMethodUnshared<T>() { }
         }
 
         interface IBar : IFoo
         {
             void IFoo.DefaultInterfaceMethod() { }
+            void IFoo.DefaultInterfaceMethodShared<T>() { }
+            void IFoo.DefaultInterfaceMethodUnshared<T>() { }
         }
 
         static Base s_baseInstance = new Derived();
@@ -2354,16 +2370,48 @@ internal static class ReflectionTest
             if (abstractMethod.GetMethodInfo().Name != nameof(Derived.AbstractMethod))
                 throw new Exception();
 
+            Action abstractMethodShared = s_baseInstance.AbstractMethodShared<object>;
+            if (abstractMethodShared.GetMethodInfo().Name != nameof(Derived.AbstractMethodShared))
+                throw new Exception();
+
+            Action abstractMethodUnshared = s_baseInstance.AbstractMethodUnshared<int>;
+            if (abstractMethodUnshared.GetMethodInfo().Name != nameof(Derived.AbstractMethodUnshared))
+                throw new Exception();
+
             Action virtualMethod = s_baseInstance.VirtualMethod;
             if (virtualMethod.GetMethodInfo().Name != nameof(Derived.VirtualMethod))
+                throw new Exception();
+
+            Action virtualMethodShared = s_baseInstance.VirtualMethodShared<object>;
+            if (virtualMethodShared.GetMethodInfo().Name != nameof(Derived.VirtualMethodShared))
+                throw new Exception();
+
+            Action virtualMethodUnshared = s_baseInstance.VirtualMethodUnshared<int>;
+            if (virtualMethodUnshared.GetMethodInfo().Name != nameof(Derived.VirtualMethodUnshared))
                 throw new Exception();
 
             Action interfaceMethod = s_ifooInstance.InterfaceMethod;
             if (!interfaceMethod.GetMethodInfo().Name.EndsWith("IFoo.InterfaceMethod"))
                 throw new Exception();
 
+            Action interfaceMethodShared = s_ifooInstance.InterfaceMethodShared<object>;
+            if (!interfaceMethodShared.GetMethodInfo().Name.EndsWith("IFoo.InterfaceMethodShared"))
+                throw new Exception();
+
+            Action interfaceMethodUnshared = s_ifooInstance.InterfaceMethodUnshared<int>;
+            if (!interfaceMethodUnshared.GetMethodInfo().Name.EndsWith("IFoo.InterfaceMethodUnshared"))
+                throw new Exception();
+
             Action defaultMethod = s_ifooInstance.DefaultInterfaceMethod;
             if (!defaultMethod.GetMethodInfo().Name.EndsWith("IFoo.DefaultInterfaceMethod"))
+                throw new Exception();
+
+            Action defaultMethodShared = s_ifooInstance.DefaultInterfaceMethodShared<object>;
+            if (!defaultMethodShared.GetMethodInfo().Name.EndsWith("IFoo.DefaultInterfaceMethodShared"))
+                throw new Exception();
+
+            Action defaultMethodUnshared = s_ifooInstance.DefaultInterfaceMethodUnshared<int>;
+            if (!defaultMethodUnshared.GetMethodInfo().Name.EndsWith("IFoo.DefaultInterfaceMethodUnshared"))
                 throw new Exception();
         }
     }