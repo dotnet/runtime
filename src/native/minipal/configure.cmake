dotnet/runtime

commit 3cfd1253bdb946d3ddeb77573aed7a1897cdfdff

Updating the managed baselines to `x86-64-v2` and `armv8-a + lse` (#118101)
* Updating the managed baselines to x86-64-v2 and armv8.1-a

* Fix parens on MacOS

* Fix a check that's necessary for 64-bit

* Don't require FEAT_LSE on Linux

* Remove the legacy encoding for pextrw

* Fix a pre-initialization smoke test to account for Popcnt now being pre-initialized

* Update the R2R baseline to differ from the NAOT baseline

* Have the preinitialization smoke test use an ISA that is opportunistic with the new baseline

* Don't target x86-64-v3 for OSX to avoid negatively impacting Apple Silicon

* Ensure the RCPC2 check is done last so methods aren't marked as using it unnecessarily

* Ensure containment for Ssse3.Shuffle still works

* Ensure AVX only hardware doesn't assert during lowering

* Don't run the JittedMethodsCountingTest if the default target is disabled

src/native/minipal/cpufeatures.c
@@ -238,8 +238,17 @@ int minipal_getcpufeatures(void)
     bool hasAvx2Dependencies = false;
     bool hasAvx10v1Dependencies = false;
 
-    assert((cpuidInfo[CPUID_EDX] & (1 << 25)) != 0);                                                            // SSE
-    assert((cpuidInfo[CPUID_EDX] & (1 << 26)) != 0);                                                            // SSE2
+    if (((cpuidInfo[CPUID_EDX] & (1 << 25)) == 0) ||                                                            // SSE
+        ((cpuidInfo[CPUID_EDX] & (1 << 26)) == 0) ||                                                            // SSE2
+        ((cpuidInfo[CPUID_ECX] & (1 << 0)) == 0) ||                                                             // SSE3
+        ((cpuidInfo[CPUID_ECX] & (1 << 9)) == 0) ||                                                             // SSSE3
+        ((cpuidInfo[CPUID_ECX] & (1 << 19)) == 0) ||                                                            // SSE4.1
+        ((cpuidInfo[CPUID_ECX] & (1 << 20)) == 0) ||                                                            // SSE4.2
+        ((cpuidInfo[CPUID_ECX] & (1 << 23)) == 0))                                                              // POPCNT
+    {
+        // One of the baseline ISAs is not supported
+        result |= IntrinsicConstants_Invalid;
+    }
 
     if (((cpuidInfo[CPUID_ECX] & (1 << 25)) != 0) &&                                                            // AESNI
         ((cpuidInfo[CPUID_ECX] & (1 << 1)) != 0))                                                               // PCLMULQDQ
@@ -247,27 +256,18 @@ int minipal_getcpufeatures(void)
         result |= XArchIntrinsicConstants_Aes;
     }
 
-    if (((cpuidInfo[CPUID_ECX] & (1 << 0)) != 0) &&                                                             // SSE3
-        ((cpuidInfo[CPUID_ECX] & (1 << 9)) != 0) &&                                                             // SSSE3
-        ((cpuidInfo[CPUID_ECX] & (1 << 19)) != 0) &&                                                            // SSE4.1
-        ((cpuidInfo[CPUID_ECX] & (1 << 20)) != 0) &&                                                            // SSE4.2
-        ((cpuidInfo[CPUID_ECX] & (1 << 23)) != 0))                                                              // POPCNT
+    if (((cpuidInfo[CPUID_ECX] & (1 << 27)) != 0) &&                                                            // OSXSAVE
+        ((cpuidInfo[CPUID_ECX] & (1 << 28)) != 0))                                                              // AVX
     {
-        result |= XArchIntrinsicConstants_Sse42;
-
-        if (((cpuidInfo[CPUID_ECX] & (1 << 27)) != 0) &&                                                        // OSXSAVE
-            ((cpuidInfo[CPUID_ECX] & (1 << 28)) != 0))                                                          // AVX
+        if (IsAvxEnabled() && (xmmYmmStateSupport() == 1))                                                      // XGETBV == 11
         {
-            if (IsAvxEnabled() && (xmmYmmStateSupport() == 1))                                                  // XGETBV == 11
-            {
-                result |= XArchIntrinsicConstants_Avx;
+            result |= XArchIntrinsicConstants_Avx;
 
-                if (((cpuidInfo[CPUID_ECX] & (1 << 29)) != 0) &&                                                // F16C
-                    ((cpuidInfo[CPUID_ECX] & (1 << 12)) != 0) &&                                                // FMA
-                    ((cpuidInfo[CPUID_ECX] & (1 << 22)) != 0))                                                  // MOVBE
-                {
-                    hasAvx2Dependencies = true;
-                }
+            if (((cpuidInfo[CPUID_ECX] & (1 << 29)) != 0) &&                                                    // F16C
+                ((cpuidInfo[CPUID_ECX] & (1 << 12)) != 0) &&                                                    // FMA
+                ((cpuidInfo[CPUID_ECX] & (1 << 22)) != 0))                                                      // MOVBE
+            {
+                hasAvx2Dependencies = true;
             }
         }
     }
@@ -455,13 +455,17 @@ int minipal_getcpufeatures(void)
 #if HAVE_AUXV_HWCAP_H
     unsigned long hwCap = getauxval(AT_HWCAP);
 
-    assert(hwCap & HWCAP_ASIMD);
+    if ((hwCap & HWCAP_ASIMD) == 0)
+    {
+        // One of the baseline ISAs is not supported
+        result |= IntrinsicConstants_Invalid;
+    }
+
+    if ((hwCap & HWCAP_ATOMICS) != 0)
+        result |= ARM64IntrinsicConstants_Atomics;
 
     if (hwCap & HWCAP_AES)
         result |= ARM64IntrinsicConstants_Aes;
-
-    if (hwCap & HWCAP_ATOMICS)
-        result |= ARM64IntrinsicConstants_Atomics;
 
     if (hwCap & HWCAP_CRC32)
         result |= ARM64IntrinsicConstants_Crc32;
@@ -498,6 +502,17 @@ int minipal_getcpufeatures(void)
     int64_t valueFromSysctl = 0;
     size_t sz = sizeof(valueFromSysctl);
 
+    if ((sysctlbyname("hw.optional.AdvSIMD", &valueFromSysctl, &sz, NULL, 0) != 0) || (valueFromSysctl == 0) ||
+        (sysctlbyname("hw.optional.arm.FEAT_LSE", &valueFromSysctl, &sz, NULL, 0) != 0) || (valueFromSysctl == 0))
+    {
+        // One of the baseline ISAs is not supported
+        result |= IntrinsicConstants_Invalid;
+    }
+    else
+    {
+        result |= ARM64IntrinsicConstants_Atomics;
+    }
+
     if ((sysctlbyname("hw.optional.arm.FEAT_AES", &valueFromSysctl, &sz, NULL, 0) == 0) && (valueFromSysctl != 0))
         result |= ARM64IntrinsicConstants_Aes;
 
@@ -516,9 +531,6 @@ int minipal_getcpufeatures(void)
     if ((sysctlbyname("hw.optional.arm.FEAT_SHA256", &valueFromSysctl, &sz, NULL, 0) == 0) && (valueFromSysctl != 0))
         result |= ARM64IntrinsicConstants_Sha256;
 
-    if ((sysctlbyname("hw.optional.armv8_1_atomics", &valueFromSysctl, &sz, NULL, 0) == 0) && (valueFromSysctl != 0))
-        result |= ARM64IntrinsicConstants_Atomics;
-
     if ((sysctlbyname("hw.optional.arm.FEAT_LRCPC", &valueFromSysctl, &sz, NULL, 0) == 0) && (valueFromSysctl != 0))
         result |= ARM64IntrinsicConstants_Rcpc;
 
@@ -529,6 +541,17 @@ int minipal_getcpufeatures(void)
 #endif // HOST_UNIX
 
 #if defined(HOST_WINDOWS)
+    if (!IsProcessorFeaturePresent(PF_ARM_V8_INSTRUCTIONS_AVAILABLE) ||
+        !IsProcessorFeaturePresent(PF_ARM_V81_ATOMIC_INSTRUCTIONS_AVAILABLE))
+    {
+        // One of the baseline ISAs is not supported
+        result |= IntrinsicConstants_Invalid;
+    }
+    else
+    {
+        result |= ARM64IntrinsicConstants_Atomics;
+    }
+
     if (IsProcessorFeaturePresent(PF_ARM_V8_CRYPTO_INSTRUCTIONS_AVAILABLE))
     {
         result |= ARM64IntrinsicConstants_Aes;
@@ -539,11 +562,6 @@ int minipal_getcpufeatures(void)
     if (IsProcessorFeaturePresent(PF_ARM_V8_CRC32_INSTRUCTIONS_AVAILABLE))
     {
         result |= ARM64IntrinsicConstants_Crc32;
-    }
-
-    if (IsProcessorFeaturePresent(PF_ARM_V81_ATOMIC_INSTRUCTIONS_AVAILABLE))
-    {
-        result |= ARM64IntrinsicConstants_Atomics;
     }
 
     if (IsProcessorFeaturePresent(PF_ARM_V82_DP_INSTRUCTIONS_AVAILABLE))
@@ -578,7 +596,6 @@ int minipal_getcpufeatures(void)
     {
         result |= ARM64IntrinsicConstants_Sve2;
     }
-
 #endif // HOST_WINDOWS
 
 #endif // HOST_ARM64

eng/pipelines/common/templates/runtimes/run-test-job.yml
@@ -359,74 +359,31 @@ jobs:
           - jitstress_random_2
         ${{ if in(parameters.testGroup, 'jitstress-isas-arm') }}:
           scenarios:
-          - jitstress_isas_incompletehwintrinsic
           - jitstress_isas_nohwintrinsic
-          - jitstress_isas_nohwintrinsic_nosimd
-          - jitstress_isas_nosimd
         ${{ if in(parameters.testGroup, 'jitstress-isas-x86') }}:
           scenarios:
-          - jitstress_isas_incompletehwintrinsic
           - jitstress_isas_nohwintrinsic
-          - jitstress_isas_nohwintrinsic_nosimd
-          - jitstress_isas_nosimd
           - jitstress_isas_x86_evex
-          - jitstress_isas_x86_noaes
           - jitstress_isas_x86_noavx
           - jitstress_isas_x86_noavx2
           - jitstress_isas_x86_noavx512
-          - jitstress_isas_x86_nobmi1
-          - jitstress_isas_x86_nobmi2
-          - jitstress_isas_x86_nofma
-          - jitstress_isas_x86_nohwintrinsic
-          - jitstress_isas_x86_nolzcnt
-          - jitstress_isas_x86_nopclmulqdq
-          - jitstress_isas_x86_nopopcnt
-          - jitstress_isas_x86_nosse
-          - jitstress_isas_x86_nosse2
-          - jitstress_isas_x86_nosse3
-          - jitstress_isas_x86_nosse3_4
-          - jitstress_isas_x86_nosse41
-          - jitstress_isas_x86_nosse42
-          - jitstress_isas_x86_nossse3
           - jitstress_isas_x86_vectort128
           - jitstress_isas_x86_vectort512
           - jitstress_isas_x86_noavx512_vectort128
-          - jitstress_isas_1_x86_noaes
+          - jitstress_isas_1_x86_evex
           - jitstress_isas_1_x86_noavx
           - jitstress_isas_1_x86_noavx2
           - jitstress_isas_1_x86_noavx512
-          - jitstress_isas_1_x86_nobmi1
-          - jitstress_isas_1_x86_nobmi2
-          - jitstress_isas_1_x86_nofma
-          - jitstress_isas_1_x86_nohwintrinsic
-          - jitstress_isas_1_x86_nolzcnt
-          - jitstress_isas_1_x86_nopclmulqdq
-          - jitstress_isas_1_x86_nopopcnt
-          - jitstress_isas_1_x86_nosse
-          - jitstress_isas_1_x86_nosse2
-          - jitstress_isas_1_x86_nosse3
-          - jitstress_isas_1_x86_nosse3_4
-          - jitstress_isas_1_x86_nosse41
-          - jitstress_isas_1_x86_nosse42
-          - jitstress_isas_1_x86_nossse3
-          - jitstress_isas_2_x86_noaes
+          - jitstress_isas_1_x86_vectort128
+          - jitstress_isas_1_x86_vectort512
+          - jitstress_isas_1_x86_noavx512_vectort128
+          - jitstress_isas_2_x86_evex
           - jitstress_isas_2_x86_noavx
           - jitstress_isas_2_x86_noavx2
           - jitstress_isas_2_x86_noavx512
-          - jitstress_isas_2_x86_nobmi1
-          - jitstress_isas_2_x86_nobmi2
-          - jitstress_isas_2_x86_nofma
-          - jitstress_isas_2_x86_nohwintrinsic
-          - jitstress_isas_2_x86_nolzcnt
-          - jitstress_isas_2_x86_nopclmulqdq
-          - jitstress_isas_2_x86_nopopcnt
-          - jitstress_isas_2_x86_nosse
-          - jitstress_isas_2_x86_nosse2
-          - jitstress_isas_2_x86_nosse3
-          - jitstress_isas_2_x86_nosse3_4
-          - jitstress_isas_2_x86_nosse41
-          - jitstress_isas_2_x86_nosse42
-          - jitstress_isas_2_x86_nossse3
+          - jitstress_isas_2_x86_vectort128
+          - jitstress_isas_2_x86_vectort512
+          - jitstress_isas_2_x86_noavx512_vectort128
         ${{ if in(parameters.testGroup, 'jitstress-isas-avx512') }}:
           scenarios:
           - jitstress_isas_x86_evex

src/coreclr/inc/clrconfigvalues.h
@@ -670,7 +670,6 @@ RETAIL_CONFIG_DWORD_INFO(EXTERNAL_EnableHWIntrinsic,            W("EnableHWIntri
 #endif // defined(TARGET_LOONGARCH64)
 
 #if defined(TARGET_AMD64) || defined(TARGET_X86)
-RETAIL_CONFIG_DWORD_INFO(EXTERNAL_EnableSSE42,                  W("EnableSSE42"),               1, "Allows SSE3, SSSE3, SSE4.1, SSE4.2, POPCNT, and dependent hardware intrinsics to be disabled")
 RETAIL_CONFIG_DWORD_INFO(EXTERNAL_EnableAVX,                    W("EnableAVX"),                 1, "Allows AVX and dependent hardware intrinsics to be disabled")
 RETAIL_CONFIG_DWORD_INFO(EXTERNAL_EnableAVX2,                   W("EnableAVX2"),                1, "Allows AVX2, BMI1, BMI2, F16C, FMA, LZCNT, MOVBE and dependent hardware intrinsics to be disabled")
 RETAIL_CONFIG_DWORD_INFO(EXTERNAL_EnableAVX512,                 W("EnableAVX512"),              1, "Allows AVX512 F+BW+CD+DQ+VL and depdendent hardware intrinsics to be disabled")

commit 8cf83faee4e4625d7617e1b9a218f50fe39637fa

Add CPU feature detection using elf_aux_info (#130901)
When running AOT smoke tests on freebsd-arm64, it was failing AOT apps
with:

```sh
[6](https://github.com/am11/CrossRepoCITesting/actions/runs/29426499240/job/87390152366#step:5:336)
  Running /home/runner/work/CrossRepoCITesting/CrossRepoCITesting/runtime/artifacts/tests/coreclr/freebsd.arm64.Checked/nativeaot/SmokeTests/PInvoke/PInvoke/native/PInvoke...
  
  The current CPU is missing one or more of the following instruction sets: AdvSimd
  Abort trap
```

It's a net11.0 regression from
https://github.com/dotnet/runtime/pull/118101; now passing:
https://github.com/am11/CrossRepoCITesting/actions/runs/29519372748/job/87692356810

src/native/minipal/configure.cmake
@@ -10,6 +10,7 @@ check_include_files("sys/resource.h" HAVE_RESOURCE_H)
 check_function_exists(sysctlbyname HAVE_SYSCTLBYNAME)
 check_function_exists(fsync HAVE_FSYNC)
 
+check_symbol_exists(elf_aux_info "sys/auxv.h" HAVE_ELF_AUX_INFO)
 check_symbol_exists(arc4random_buf "stdlib.h" HAVE_ARC4RANDOM_BUF)
 check_symbol_exists(getrandom "sys/random.h" HAVE_GETRANDOM)
 check_symbol_exists(getentropy "unistd.h" HAVE_GETENTROPY)

src/native/minipal/cpufeatures.c
@@ -7,6 +7,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
+#include <sys/types.h>
+
+#include "minipalconfig.h"
+
+#if HAVE_ELF_AUX_INFO
+#include <sys/auxv.h>
+#endif
 
 #include "cpufeatures.h"
 #include "cpuid.h"
@@ -40,8 +47,6 @@
 #endif
 
 #else // HOST_WINDOWS
-
-#include "minipalconfig.h"
 
 #if HAVE_AUXV_HWCAP_H
 
@@ -510,8 +515,17 @@ int minipal_getcpufeatures(void)
 #if defined(HOST_ARM64)
 #if defined(HOST_UNIX)
 
+#if HAVE_AUXV_HWCAP_H || HAVE_ELF_AUX_INFO
 #if HAVE_AUXV_HWCAP_H
     unsigned long hwCap = getauxval(AT_HWCAP);
+    unsigned long hwCap2 = getauxval(AT_HWCAP2);
+#elif HAVE_ELF_AUX_INFO
+    unsigned long hwCap = 0;
+    unsigned long hwCap2 = 0;
+
+    elf_aux_info(AT_HWCAP, &hwCap, sizeof(hwCap));
+    elf_aux_info(AT_HWCAP2, &hwCap2, sizeof(hwCap2));
+#endif
 
     if ((hwCap & HWCAP_ASIMD) == 0)
     {
@@ -555,8 +569,6 @@ int minipal_getcpufeatures(void)
     if (hwCap & HWCAP_SVE)
         result |= ARM64IntrinsicConstants_Sve;
 
-    unsigned long hwCap2 = getauxval(AT_HWCAP2);
-
     if (hwCap2 & HWCAP2_SVE2)
         result |= ARM64IntrinsicConstants_Sve2;
 
@@ -571,10 +583,7 @@ int minipal_getcpufeatures(void)
 
     if (hwCap2 & HWCAP2_CSSC)
         result |= ARM64IntrinsicConstants_Cssc;
-
-#else // !HAVE_AUXV_HWCAP_H
-
-#if HAVE_SYSCTLBYNAME
+#elif HAVE_SYSCTLBYNAME
     int64_t valueFromSysctl = 0;
     size_t sz = sizeof(valueFromSysctl);
 
@@ -646,7 +655,6 @@ int minipal_getcpufeatures(void)
     if ((sysctlbyname("hw.optional.arm.FEAT_CSSC", &valueFromSysctl, &sz, NULL, 0) == 0) && (valueFromSysctl != 0))
         result |= ARM64IntrinsicConstants_Cssc;
 #endif // HAVE_SYSCTLBYNAME
-#endif // HAVE_AUXV_HWCAP_H
 #endif // HOST_UNIX
 
 #if defined(HOST_WINDOWS)

src/native/minipal/minipalconfig.h.in
@@ -9,6 +9,7 @@
 #cmakedefine01 HAVE_RESOURCE_H
 #cmakedefine01 HAVE_O_CLOEXEC
 #cmakedefine01 HAVE_SYSCTLBYNAME
+#cmakedefine01 HAVE_ELF_AUX_INFO
 #cmakedefine01 HAVE_CLOCK_MONOTONIC_COARSE
 #cmakedefine01 HAVE_CLOCK_GETTIME_NSEC_NP
 #cmakedefine01 BIGENDIAN