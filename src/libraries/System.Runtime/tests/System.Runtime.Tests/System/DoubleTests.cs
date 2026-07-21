dotnet/runtime

commit a4407a106f883a6593105a660d44da6f8fa4017c

Use generic math for floating point formatting (#102683)
* Generic DiyFp

* Generic Grisu3

* Generic Dragon4

* Add MaxRoundTripDigits to MaxPrecisionCustomFormat to FormatInfo

* Generic FormatFloat

* Adapt with existing FP types

* Fix ExtractFractionAndBiasedExponent

src/libraries/System.Private.CoreLib/src/System/Number.Dragon4.cs
@@ -10,82 +10,23 @@ namespace System
     // The backing algorithm and the proofs behind it are described in more detail here:  https://www.cs.indiana.edu/~dyb/pubs/FP-Printing-PLDI96.pdf
     internal static partial class Number
     {
-        public static void Dragon4Double(double value, int cutoffNumber, bool isSignificantDigits, ref NumberBuffer number)
+        public static unsafe void Dragon4<TNumber>(TNumber value, int cutoffNumber, bool isSignificantDigits, ref NumberBuffer number)
+            where TNumber : unmanaged, IBinaryFloatParseAndFormatInfo<TNumber>
         {
-            double v = double.IsNegative(value) ? -value : value;
+            TNumber v = TNumber.IsNegative(value) ? -value : value;
 
-            Debug.Assert(v > 0);
-            Debug.Assert(double.IsFinite(v));
+            Debug.Assert(v > TNumber.Zero);
+            Debug.Assert(TNumber.IsFinite(v));
 
             ulong mantissa = ExtractFractionAndBiasedExponent(value, out int exponent);
 
             uint mantissaHighBitIdx;
             bool hasUnequalMargins = false;
 
-            if ((mantissa >> DiyFp.DoubleImplicitBitIndex) != 0)
+            if ((mantissa >> TNumber.DenormalMantissaBits) != 0)
             {
-                mantissaHighBitIdx = DiyFp.DoubleImplicitBitIndex;
-                hasUnequalMargins = (mantissa == (1UL << DiyFp.DoubleImplicitBitIndex));
-            }
-            else
-            {
-                Debug.Assert(mantissa != 0);
-                mantissaHighBitIdx = (uint)BitOperations.Log2(mantissa);
-            }
-
-            int length = (int)(Dragon4(mantissa, exponent, mantissaHighBitIdx, hasUnequalMargins, cutoffNumber, isSignificantDigits, number.Digits, out int decimalExponent));
-
-            number.Scale = decimalExponent + 1;
-            number.Digits[length] = (byte)('\0');
-            number.DigitsCount = length;
-        }
-
-        public static unsafe void Dragon4Half(Half value, int cutoffNumber, bool isSignificantDigits, ref NumberBuffer number)
-        {
-            Half v = Half.IsNegative(value) ? Half.Negate(value) : value;
-
-            Debug.Assert((double)v > 0.0);
-            Debug.Assert(Half.IsFinite(v));
-
-            ushort mantissa = ExtractFractionAndBiasedExponent(value, out int exponent);
-
-            uint mantissaHighBitIdx;
-            bool hasUnequalMargins = false;
-
-            if ((mantissa >> DiyFp.HalfImplicitBitIndex) != 0)
-            {
-                mantissaHighBitIdx = DiyFp.HalfImplicitBitIndex;
-                hasUnequalMargins = (mantissa == (1U << DiyFp.HalfImplicitBitIndex));
-            }
-            else
-            {
-                Debug.Assert(mantissa != 0);
-                mantissaHighBitIdx = (uint)BitOperations.Log2(mantissa);
-            }
-
-            int length = (int)(Dragon4(mantissa, exponent, mantissaHighBitIdx, hasUnequalMargins, cutoffNumber, isSignificantDigits, number.Digits, out int decimalExponent));
-
-            number.Scale = decimalExponent + 1;
-            number.Digits[length] = (byte)('\0');
-            number.DigitsCount = length;
-        }
-
-        public static unsafe void Dragon4Single(float value, int cutoffNumber, bool isSignificantDigits, ref NumberBuffer number)
-        {
-            float v = float.IsNegative(value) ? -value : value;
-
-            Debug.Assert(v > 0);
-            Debug.Assert(float.IsFinite(v));
-
-            uint mantissa = ExtractFractionAndBiasedExponent(value, out int exponent);
-
-            uint mantissaHighBitIdx;
-            bool hasUnequalMargins = false;
-
-            if ((mantissa >> DiyFp.SingleImplicitBitIndex) != 0)
-            {
-                mantissaHighBitIdx = DiyFp.SingleImplicitBitIndex;
-                hasUnequalMargins = (mantissa == (1U << DiyFp.SingleImplicitBitIndex));
+                mantissaHighBitIdx = TNumber.DenormalMantissaBits;
+                hasUnequalMargins = (mantissa == (1U << TNumber.DenormalMantissaBits));
             }
             else
             {

src/libraries/System.Private.CoreLib/src/System/Double.cs
@@ -354,33 +354,33 @@ namespace System
 
         public override string ToString()
         {
-            return Number.FormatDouble(m_value, null, NumberFormatInfo.CurrentInfo);
+            return Number.FormatFloat(m_value, null, NumberFormatInfo.CurrentInfo);
         }
 
         public string ToString([StringSyntax(StringSyntaxAttribute.NumericFormat)] string? format)
         {
-            return Number.FormatDouble(m_value, format, NumberFormatInfo.CurrentInfo);
+            return Number.FormatFloat(m_value, format, NumberFormatInfo.CurrentInfo);
         }
 
         public string ToString(IFormatProvider? provider)
         {
-            return Number.FormatDouble(m_value, null, NumberFormatInfo.GetInstance(provider));
+            return Number.FormatFloat(m_value, null, NumberFormatInfo.GetInstance(provider));
         }
 
         public string ToString([StringSyntax(StringSyntaxAttribute.NumericFormat)] string? format, IFormatProvider? provider)
         {
-            return Number.FormatDouble(m_value, format, NumberFormatInfo.GetInstance(provider));
+            return Number.FormatFloat(m_value, format, NumberFormatInfo.GetInstance(provider));
         }
 
         public bool TryFormat(Span<char> destination, out int charsWritten, [StringSyntax(StringSyntaxAttribute.NumericFormat)] ReadOnlySpan<char> format = default, IFormatProvider? provider = null)
         {
-            return Number.TryFormatDouble(m_value, format, NumberFormatInfo.GetInstance(provider), destination, out charsWritten);
+            return Number.TryFormatFloat(m_value, format, NumberFormatInfo.GetInstance(provider), destination, out charsWritten);
         }
 
         /// <inheritdoc cref="IUtf8SpanFormattable.TryFormat" />
         public bool TryFormat(Span<byte> utf8Destination, out int bytesWritten, [StringSyntax(StringSyntaxAttribute.NumericFormat)] ReadOnlySpan<char> format = default, IFormatProvider? provider = null)
         {
-            return Number.TryFormatDouble(m_value, format, NumberFormatInfo.GetInstance(provider), utf8Destination, out bytesWritten);
+            return Number.TryFormatFloat(m_value, format, NumberFormatInfo.GetInstance(provider), utf8Destination, out bytesWritten);
         }
 
         public static double Parse(string s) => Parse(s, NumberStyles.Float | NumberStyles.AllowThousands, provider: null);
@@ -2334,6 +2334,10 @@ namespace System
         static double IBinaryFloatParseAndFormatInfo<double>.BitsToFloat(ulong bits) => BitConverter.UInt64BitsToDouble(bits);
 
         static ulong IBinaryFloatParseAndFormatInfo<double>.FloatToBits(double value) => BitConverter.DoubleToUInt64Bits(value);
+
+        static int IBinaryFloatParseAndFormatInfo<double>.MaxRoundTripDigits => 17;
+
+        static int IBinaryFloatParseAndFormatInfo<double>.MaxPrecisionCustomFormat => 15;
 
         //
         // Helpers

src/libraries/System.Private.CoreLib/src/System/Half.cs
@@ -505,7 +505,7 @@ namespace System
         /// </summary>
         public override string ToString()
         {
-            return Number.FormatHalf(this, null, NumberFormatInfo.CurrentInfo);
+            return Number.FormatFloat(this, null, NumberFormatInfo.CurrentInfo);
         }
 
         /// <summary>
@@ -513,7 +513,7 @@ namespace System
         /// </summary>
         public string ToString([StringSyntax(StringSyntaxAttribute.NumericFormat)] string? format)
         {
-            return Number.FormatHalf(this, format, NumberFormatInfo.CurrentInfo);
+            return Number.FormatFloat(this, format, NumberFormatInfo.CurrentInfo);
         }
 
         /// <summary>
@@ -521,7 +521,7 @@ namespace System
         /// </summary>
         public string ToString(IFormatProvider? provider)
         {
-            return Number.FormatHalf(this, null, NumberFormatInfo.GetInstance(provider));
+            return Number.FormatFloat(this, null, NumberFormatInfo.GetInstance(provider));
         }
 
         /// <summary>
@@ -529,7 +529,7 @@ namespace System
         /// </summary>
         public string ToString([StringSyntax(StringSyntaxAttribute.NumericFormat)] string? format, IFormatProvider? provider)
         {
-            return Number.FormatHalf(this, format, NumberFormatInfo.GetInstance(provider));
+            return Number.FormatFloat(this, format, NumberFormatInfo.GetInstance(provider));
         }
 
         /// <summary>
@@ -542,13 +542,13 @@ namespace System
         /// <returns></returns>
         public bool TryFormat(Span<char> destination, out int charsWritten, [StringSyntax(StringSyntaxAttribute.NumericFormat)] ReadOnlySpan<char> format = default, IFormatProvider? provider = null)
         {
-            return Number.TryFormatHalf(this, format, NumberFormatInfo.GetInstance(provider), destination, out charsWritten);
+            return Number.TryFormatFloat(this, format, NumberFormatInfo.GetInstance(provider), destination, out charsWritten);
         }
 
         /// <inheritdoc cref="IUtf8SpanFormattable.TryFormat" />
         public bool TryFormat(Span<byte> utf8Destination, out int bytesWritten, [StringSyntax(StringSyntaxAttribute.NumericFormat)] ReadOnlySpan<char> format = default, IFormatProvider? provider = null)
         {
-            return Number.TryFormatHalf(this, format, NumberFormatInfo.GetInstance(provider), utf8Destination, out bytesWritten);
+            return Number.TryFormatFloat(this, format, NumberFormatInfo.GetInstance(provider), utf8Destination, out bytesWritten);
         }
 
         //
@@ -2373,5 +2373,9 @@ namespace System
         static Half IBinaryFloatParseAndFormatInfo<Half>.BitsToFloat(ulong bits) => BitConverter.UInt16BitsToHalf((ushort)(bits));
 
         static ulong IBinaryFloatParseAndFormatInfo<Half>.FloatToBits(Half value) => BitConverter.HalfToUInt16Bits(value);
+
+        static int IBinaryFloatParseAndFormatInfo<Half>.MaxRoundTripDigits => 5;
+
+        static int IBinaryFloatParseAndFormatInfo<Half>.MaxPrecisionCustomFormat => 5;
     }
 }

src/libraries/System.Private.CoreLib/src/System/Number.DiyFp.cs
@@ -17,10 +17,6 @@ namespace System
         // DiyFp are not designed to contain special doubles (NaN and Infinity).
         internal readonly ref struct DiyFp
         {
-            public const int DoubleImplicitBitIndex = 52;
-            public const int SingleImplicitBitIndex = 23;
-            public const int HalfImplicitBitIndex = 10;
-
             public const int SignificandSize = 64;
 
             public readonly ulong f;
@@ -33,60 +29,21 @@ namespace System
             //
             // Precondition:
             //  The value encoded by value must be greater than 0.
-            public static DiyFp CreateAndGetBoundaries(double value, out DiyFp mMinus, out DiyFp mPlus)
+            public static DiyFp CreateAndGetBoundaries<TNumber>(TNumber value, out DiyFp mMinus, out DiyFp mPlus)
+                where TNumber : unmanaged, IBinaryFloatParseAndFormatInfo<TNumber>
             {
-                var result = new DiyFp(value);
-                result.GetBoundaries(DoubleImplicitBitIndex, out mMinus, out mPlus);
+                var result = Create(value);
+                result.GetBoundaries(TNumber.DenormalMantissaBits, out mMinus, out mPlus);
                 return result;
             }
 
-            // Computes the two boundaries of value.
-            //
-            // The bigger boundary (mPlus) is normalized.
-            // The lower boundary has the same exponent as mPlus.
-            //
-            // Precondition:
-            //  The value encoded by value must be greater than 0.
-            public static DiyFp CreateAndGetBoundaries(float value, out DiyFp mMinus, out DiyFp mPlus)
+            public static DiyFp Create<TNumber>(TNumber value)
+                where TNumber : unmanaged, IBinaryFloatParseAndFormatInfo<TNumber>
             {
-                var result = new DiyFp(value);
-                result.GetBoundaries(SingleImplicitBitIndex, out mMinus, out mPlus);
-                return result;
-            }
-
-            // Computes the two boundaries of value.
-            //
-            // The bigger boundary (mPlus) is normalized.
-            // The lower boundary has the same exponent as mPlus.
-            //
-            // Precondition:
-            //  The value encoded by value must be greater than 0.
-            public static DiyFp CreateAndGetBoundaries(Half value, out DiyFp mMinus, out DiyFp mPlus)
-            {
-                var result = new DiyFp(value);
-                result.GetBoundaries(HalfImplicitBitIndex, out mMinus, out mPlus);
-                return result;
-            }
-
-            public DiyFp(double value)
-            {
-                Debug.Assert(double.IsFinite(value));
-                Debug.Assert(value > 0.0);
-                f = ExtractFractionAndBiasedExponent(value, out e);
-            }
-
-            public DiyFp(float value)
-            {
-                Debug.Assert(float.IsFinite(value));
-                Debug.Assert(value > 0.0f);
-                f = ExtractFractionAndBiasedExponent(value, out e);
-            }
-
-            public DiyFp(Half value)
-            {
-                Debug.Assert(Half.IsFinite(value));
-                Debug.Assert((float)value > 0.0f);
-                f = ExtractFractionAndBiasedExponent(value, out e);
+                Debug.Assert(TNumber.IsFinite(value));
+                Debug.Assert(value > TNumber.Zero);
+                ulong f = ExtractFractionAndBiasedExponent(value, out int e);
+                return new DiyFp(f, e);
             }
 
             public DiyFp(ulong f, int e)

src/libraries/System.Private.CoreLib/src/System/Number.Grisu3.cs
@@ -321,12 +321,13 @@ namespace System
                 1000000000, // 10^9
             ];
 
-            public static bool TryRunDouble(double value, int requestedDigits, ref NumberBuffer number)
+            public static bool TryRun<TNumber>(TNumber value, int requestedDigits, ref NumberBuffer number)
+                where TNumber : unmanaged, IBinaryFloatParseAndFormatInfo<TNumber>
             {
-                double v = double.IsNegative(value) ? -value : value;
+                TNumber v = TNumber.IsNegative(value) ? -value : value;
 
-                Debug.Assert(v > 0);
-                Debug.Assert(double.IsFinite(v));
+                Debug.Assert(v > TNumber.Zero);
+                Debug.Assert(TNumber.IsFinite(v));
 
                 int length;
                 int decimalExponent;
@@ -339,75 +340,7 @@ namespace System
                 }
                 else
                 {
-                    DiyFp w = new DiyFp(v).Normalize();
-                    result = TryRunCounted(in w, requestedDigits, number.Digits, out length, out decimalExponent);
-                }
-
-                if (result)
-                {
-                    Debug.Assert((requestedDigits == -1) || (length == requestedDigits));
-
-                    number.Scale = length + decimalExponent;
-                    number.Digits[length] = (byte)('\0');
-                    number.DigitsCount = length;
-                }
-
-                return result;
-            }
-
-            public static bool TryRunHalf(Half value, int requestedDigits, ref NumberBuffer number)
-            {
-                Half v = Half.IsNegative(value) ? Half.Negate(value) : value;
-
-                Debug.Assert((double)v > 0);
-                Debug.Assert(Half.IsFinite(v));
-
-                int length;
-                int decimalExponent;
-                bool result;
-
-                if (requestedDigits == -1)
-                {
-                    DiyFp w = DiyFp.CreateAndGetBoundaries(v, out DiyFp boundaryMinus, out DiyFp boundaryPlus).Normalize();
-                    result = TryRunShortest(in boundaryMinus, in w, in boundaryPlus, number.Digits, out length, out decimalExponent);
-                }
-                else
-                {
-                    DiyFp w = new DiyFp(v).Normalize();
-                    result = TryRunCounted(in w, requestedDigits, number.Digits, out length, out decimalExponent);
-                }
-
-                if (result)
-                {
-                    Debug.Assert((requestedDigits == -1) || (length == requestedDigits));
-
-                    number.Scale = length + decimalExponent;
-                    number.Digits[length] = (byte)('\0');
-                    number.DigitsCount = length;
-                }
-
-                return result;
-            }
-
-            public static bool TryRunSingle(float value, int requestedDigits, ref NumberBuffer number)
-            {
-                float v = float.IsNegative(value) ? -value : value;
-
-                Debug.Assert(v > 0);
-                Debug.Assert(float.IsFinite(v));
-
-                int length;
-                int decimalExponent;
-                bool result;
-
-                if (requestedDigits == -1)
-                {
-                    DiyFp w = DiyFp.CreateAndGetBoundaries(v, out DiyFp boundaryMinus, out DiyFp boundaryPlus).Normalize();
-                    result = TryRunShortest(in boundaryMinus, in w, in boundaryPlus, number.Digits, out length, out decimalExponent);
-                }
-                else
-                {
-                    DiyFp w = new DiyFp(v).Normalize();
+                    DiyFp w = DiyFp.Create(v).Normalize();
                     result = TryRunCounted(in w, requestedDigits, number.Digits, out length, out decimalExponent);
                 }
 

src/libraries/System.Private.CoreLib/src/System/Number.Parsing.cs
@@ -85,6 +85,18 @@ namespace System
         static abstract TSelf BitsToFloat(ulong bits);
 
         static abstract ulong FloatToBits(TSelf value);
+
+        // Maximum number of digits required to guarantee that any given floating point
+        // number can roundtrip. Some numbers may require less, but none will require more.
+        static abstract int MaxRoundTripDigits { get; }
+
+        // SinglePrecisionCustomFormat and DoublePrecisionCustomFormat are used to ensure that
+        // custom format strings return the same string as in previous releases when the format
+        // would return x digits or less (where x is the value of the corresponding constant).
+        // In order to support more digits, we would need to update ParseFormatSpecifier to pre-parse
+        // the format and determine exactly how many digits are being requested and whether they
+        // represent "significant digits" or "digits after the decimal point".
+        static abstract int MaxPrecisionCustomFormat { get; }
     }
 
     internal static partial class Number

src/libraries/System.Private.CoreLib/src/System/Single.cs
@@ -349,33 +349,33 @@ namespace System
 
         public override string ToString()
         {
-            return Number.FormatSingle(m_value, null, NumberFormatInfo.CurrentInfo);
+            return Number.FormatFloat(m_value, null, NumberFormatInfo.CurrentInfo);
         }
 
         public string ToString(IFormatProvider? provider)
         {
-            return Number.FormatSingle(m_value, null, NumberFormatInfo.GetInstance(provider));
+            return Number.FormatFloat(m_value, null, NumberFormatInfo.GetInstance(provider));
         }
 
         public string ToString([StringSyntax(StringSyntaxAttribute.NumericFormat)] string? format)
         {
-            return Number.FormatSingle(m_value, format, NumberFormatInfo.CurrentInfo);
+            return Number.FormatFloat(m_value, format, NumberFormatInfo.CurrentInfo);
         }
 
         public string ToString([StringSyntax(StringSyntaxAttribute.NumericFormat)] string? format, IFormatProvider? provider)
         {
-            return Number.FormatSingle(m_value, format, NumberFormatInfo.GetInstance(provider));
+            return Number.FormatFloat(m_value, format, NumberFormatInfo.GetInstance(provider));
         }
 
         public bool TryFormat(Span<char> destination, out int charsWritten, [StringSyntax(StringSyntaxAttribute.NumericFormat)] ReadOnlySpan<char> format = default, IFormatProvider? provider = null)
         {
-            return Number.TryFormatSingle(m_value, format, NumberFormatInfo.GetInstance(provider), destination, out charsWritten);
+            return Number.TryFormatFloat(m_value, format, NumberFormatInfo.GetInstance(provider), destination, out charsWritten);
         }
 
         /// <inheritdoc cref="IUtf8SpanFormattable.TryFormat" />
         public bool TryFormat(Span<byte> utf8Destination, out int bytesWritten, [StringSyntax(StringSyntaxAttribute.NumericFormat)] ReadOnlySpan<char> format = default, IFormatProvider? provider = null)
         {
-            return Number.TryFormatSingle(m_value, format, NumberFormatInfo.GetInstance(provider), utf8Destination, out bytesWritten);
+            return Number.TryFormatFloat(m_value, format, NumberFormatInfo.GetInstance(provider), utf8Destination, out bytesWritten);
         }
 
         // Parses a float from a String in the given style.  If
@@ -2216,6 +2216,10 @@ namespace System
         static float IBinaryFloatParseAndFormatInfo<float>.BitsToFloat(ulong bits) => BitConverter.UInt32BitsToSingle((uint)(bits));
 
         static ulong IBinaryFloatParseAndFormatInfo<float>.FloatToBits(float value) => BitConverter.SingleToUInt32Bits(value);
+
+        static int IBinaryFloatParseAndFormatInfo<float>.MaxRoundTripDigits => 9;
+
+        static int IBinaryFloatParseAndFormatInfo<float>.MaxPrecisionCustomFormat => 7;
 
         //
         // Helpers

commit 5bf0d87f22617b5d17959da9e34f45a9f185f302

Fix Dragon4 shortest formatting for exact powers of two (#131131)
## Summary

`Number.Dragon4` computed `hasUnequalMargins` by comparing the extracted
mantissa against `1U << TNumber.DenormalMantissaBits`. `1U` is 32-bit,
and C# masks the shift count to 5 bits for a 32-bit operand, so for
`double` (`DenormalMantissaBits == 52`) this evaluates to `1U << (52 &
31)` == `1U << 20`, not `1UL << 52`.

As a result `hasUnequalMargins` was wrongly `false` for every exact
power of two in `double`, so Dragon4 used equal rounding margins and
produced a non-round-trippable shortest string for cases like `2^-25`
and `2^-958`:

| Value | Before | After |
| --- | --- | --- |
| `2^-25` | `2.980232238769531E-08` | `2.9802322387695312E-08` |
| `2^-958` | `4.104536801298376E-289` | `4.1045368012983762E-289` |

The "before" strings parse back to the adjacent lower-magnitude
`double`.

The fix widens the shift to `1UL`, which is correct for all supported
types (`float` 23, `double` 52, `Half` 10, `BFloat16` 7 � all `< 64`).

----------

This is a regression from #102683, which merged the per-type
`double`/`float`/`Half` paths into one generic method. The pre-refactor
`double` path used `1UL << DiyFp.DoubleImplicitBitIndex`; the merge kept
`Half`'s `1U`, which silently truncates for `double`.
`ExtractFractionAndBiasedExponent` and `DiyFp.GetBoundaries` were
unaffected (both already use `1UL`).

## Testing

- Added `DoubleTests.ToString_ExactPowerOfTwo_Roundtrips` covering
�`2^-25` and �`2^-958`. It fails without the fix and passes with it.
- Existing `double`/`float`/`Half`/`BFloat16` `ToString` tests pass
(checked runtime).
- Exhaustive power-of-two sweep (2098 values) and ~10M randomized
`float`/`double` round-trips, plus exhaustive `Half`, all pass.

> [!NOTE]
> GitHub Copilot helped create this PR.

---------

Co-authored-by: Copilot App <223556219+Copilot@users.noreply.github.com>

src/libraries/System.Private.CoreLib/src/System/Number.Dragon4.cs
@@ -33,7 +33,7 @@ namespace System
             if ((mantissa >> TNumber.DenormalMantissaBits) != 0)
             {
                 mantissaHighBitIdx = TNumber.DenormalMantissaBits;
-                hasUnequalMargins = (mantissa == (1U << TNumber.DenormalMantissaBits));
+                hasUnequalMargins = (mantissa == (1UL << TNumber.DenormalMantissaBits));
             }
             else
             {

src/libraries/System.Runtime/tests/System.Runtime.Tests/System/DoubleTests.cs
@@ -1051,6 +1051,23 @@ namespace System.Tests
             Assert.Equal(expected, d.ToString(format, NumberFormatInfo.InvariantInfo));
         }
 
+        [Theory]
+        // These exact powers of two have unequal rounding margins; their shortest round-trippable
+        // string needs all 17 significant digits, and dropping the last one parses back to the
+        // adjacent lower value.
+        [InlineData(0x3E60000000000000, "2.9802322387695312E-08")]   // +2^-25
+        [InlineData(0xBE60000000000000, "-2.9802322387695312E-08")]  // -2^-25
+        [InlineData(0x0410000000000000, "4.1045368012983762E-289")]  // +2^-958
+        [InlineData(0x8410000000000000, "-4.1045368012983762E-289")] // -2^-958
+        public static void ToString_ExactPowerOfTwo_Roundtrips(ulong bits, string expected)
+        {
+            double d = BitConverter.UInt64BitsToDouble(bits);
+
+            Assert.Equal(expected, d.ToString("R", CultureInfo.InvariantCulture));
+            Assert.Equal(expected, d.ToString(CultureInfo.InvariantCulture));
+            Assert.Equal(d, double.Parse(expected, CultureInfo.InvariantCulture));
+        }
+
         [ConditionalFact(typeof(PlatformDetection), nameof(PlatformDetection.Is64BitProcess))] // Requires a lot of memory
         [OuterLoop("Takes a long time, allocates a lot of memory")]
         [SkipOnMono("Frequently throws OOM on Mono")]