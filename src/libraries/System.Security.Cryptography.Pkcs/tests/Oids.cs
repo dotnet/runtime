540e3c411a5d012ffbc3d487adb28165a2839f97
Jeremy Barton <jbarton@microsoft.com>
2026-07-23 15:20:06 -0700

    Also do SLH-DSA

--- src/libraries/System.Security.Cryptography.Pkcs/tests/Oids.cs
+++ src/libraries/System.Security.Cryptography.Pkcs/tests/Oids.cs
@@ -30,6 +30,18 @@ namespace System.Security.Cryptography.Pkcs.Tests
         public const string MLDsa44 = "2.16.840.1.101.3.4.3.17";
         public const string MLDsa65 = "2.16.840.1.101.3.4.3.18";
         public const string MLDsa87 = "2.16.840.1.101.3.4.3.19";
+        public const string SlhDsaSha2_128s = "2.16.840.1.101.3.4.3.20";
+        public const string SlhDsaSha2_128f = "2.16.840.1.101.3.4.3.21";
+        public const string SlhDsaSha2_192s = "2.16.840.1.101.3.4.3.22";
+        public const string SlhDsaSha2_192f = "2.16.840.1.101.3.4.3.23";
+        public const string SlhDsaSha2_256s = "2.16.840.1.101.3.4.3.24";
+        public const string SlhDsaSha2_256f = "2.16.840.1.101.3.4.3.25";
+        public const string SlhDsaShake128s = "2.16.840.1.101.3.4.3.26";
+        public const string SlhDsaShake128f = "2.16.840.1.101.3.4.3.27";
+        public const string SlhDsaShake192s = "2.16.840.1.101.3.4.3.28";
+        public const string SlhDsaShake192f = "2.16.840.1.101.3.4.3.29";
+        public const string SlhDsaShake256s = "2.16.840.1.101.3.4.3.30";
+        public const string SlhDsaShake256f = "2.16.840.1.101.3.4.3.31";
 
         // Cryptographic Attribute Types
         public const string SigningTime = "1.2.840.113549.1.9.5";
