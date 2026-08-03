c1592900dfaa141a8f2b762dc62277aa1e41256f
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2026-08-04 01:43:06 +0200

    JIT: Treat the indicator variable as transition-preserved (#131731)
    In OSR functions we recomputed this from the async continuation argument
    (which itself is transition-preserved). That approach is not sufficient
    for EnC. Instead just preserve the state across EnC/OSR transitions in
    the same way as other state.
    
    To simplify the remapping/local allocation logic this also changes the
    indicator to consistently be a `TYP_I_IMPL` local, with all definitions
    of it writing that size. The arguments passed to the helper calls remain
    `TYP_INT` since they are bools in the managed signatures. We use
    `TYP_INT` typed local nodes for the indicator variable for these
    (allowed in JIT IR and used by `optNarrowTree`/IV widening also).

--- src/coreclr/jit/compiler.h
+++ src/coreclr/jit/compiler.h
@@ -4344,6 +4344,7 @@ public:
     unsigned lvaMonAcquired = BAD_VAR_NUM; // boolean variable introduced into in synchronized methods
                              // that tracks whether the lock has been taken
 
+    unsigned lvaResumedIndicator = BAD_VAR_NUM;               // Variable representing "have we resumed?" for async methods
     unsigned lvaAsyncThreadObjectVar = BAD_VAR_NUM;           // Thread local for async methods
     unsigned lvaAsyncExecutionContextVar = BAD_VAR_NUM;       // ExecutionContext local for async methods
     unsigned lvaAsyncSynchronizationContextVar = BAD_VAR_NUM; // SynchronizationContext local for async methods
@@ -4388,9 +4389,6 @@ public:
 
     // Variable representing async continuation argument passed.
     unsigned lvaAsyncContinuationArg = BAD_VAR_NUM;
-
-    // Variable representing "have we resumed?" for async methods
-    unsigned lvaResumedIndicator = BAD_VAR_NUM;
 
 #if defined(DEBUG) && defined(TARGET_XARCH)
 
