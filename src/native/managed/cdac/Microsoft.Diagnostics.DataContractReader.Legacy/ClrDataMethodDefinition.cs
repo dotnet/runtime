359ae5462807160962a92c956e671f7e946ce3a2
rcj1 <rachel.jarvi@gmail.com>
2026-08-03 15:50:00 -0700

    test

--- src/native/managed/cdac/Microsoft.Diagnostics.DataContractReader.Legacy/ClrDataMethodDefinition.cs
+++ src/native/managed/cdac/Microsoft.Diagnostics.DataContractReader.Legacy/ClrDataMethodDefinition.cs
@@ -242,7 +242,7 @@ public sealed unsafe partial class ClrDataMethodDefinition : IXCLRDataMethodDefi
 #if DEBUG
         if (_legacyImpl is not null)
         {
-            Debug.ValidateHResult(hr, hrLocal);
+            Debug.ValidateHResult(hr, hrLocal, HResultValidationMode.AllowCdacSuccess);
         }
 #endif
 
