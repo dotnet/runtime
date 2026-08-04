dotnet/runtime

commit b738611e500e405a82a60ae443b14eb3373829e1

JIT: Generalize flow edge redirection logic (#116933)
Replace fgRedirect*Edge helpers with an implementation agnostic to the source block's jump kind.

src/coreclr/jit/lower.cpp
@@ -1550,14 +1550,13 @@ bool Lowering::TryLowerSwitchToBitTest(FlowEdge*   jumpTable[],
 #endif
 
     //
-    // Rewire the blocks as needed.
+    // Set successor edge dup counts to 1 each
     //
 
-    comp->fgRemoveAllRefPreds(bbCase1, bbSwitch);
-    comp->fgRemoveAllRefPreds(bbCase0, bbSwitch);
-
-    case0Edge = comp->fgAddRefPred(bbCase0, bbSwitch, case0Edge);
-    case1Edge = comp->fgAddRefPred(bbCase1, bbSwitch, case1Edge);
+    bbCase0->bbRefs -= (case0Edge->getDupCount() - 1);
+    bbCase1->bbRefs -= (case1Edge->getDupCount() - 1);
+    case0Edge->decrementDupCount(case0Edge->getDupCount() - 1);
+    case1Edge->decrementDupCount(case1Edge->getDupCount() - 1);
 
     // If defaultLikelihood is not ~ 1.0
     //   up-scale case likelihoods by 1.0 / (1.0 - defaultLikelihood)

src/coreclr/jit/async.cpp
@@ -1916,11 +1916,8 @@ void AsyncTransformation::CreateResumptionSwitch()
                 checkILOffsetBB->bbNum);
 
         // Redirect newEntryBB -> onContinuationBB into newEntryBB -> checkILOffsetBB -> onContinuationBB
-        m_comp->fgRemoveRefPred(newEntryBB->GetTrueEdge());
-
-        FlowEdge* toCheckILOffsetBB = m_comp->fgAddRefPred(checkILOffsetBB, newEntryBB);
-        newEntryBB->SetTrueEdge(toCheckILOffsetBB);
-        toCheckILOffsetBB->setLikelihood(0);
+        m_comp->fgRedirectEdge(newEntryBB->GetTrueEdgeRef(), checkILOffsetBB);
+        newEntryBB->GetTrueEdge()->setLikelihood(0);
         checkILOffsetBB->inheritWeightPercentage(newEntryBB, 0);
 
         FlowEdge* toOnContinuationBB = m_comp->fgAddRefPred(onContinuationBB, checkILOffsetBB);
@@ -1969,10 +1966,8 @@ void AsyncTransformation::CreateResumptionSwitch()
         BasicBlock* checkILOffsetBB    = m_comp->fgNewBBbefore(BBJ_COND, onContinuationBB, true);
 
         // Switch newEntryBB -> onContinuationBB into newEntryBB -> checkILOffsetBB
-        m_comp->fgRemoveRefPred(newEntryBB->GetTrueEdge());
-        FlowEdge* toCheckILOffset = m_comp->fgAddRefPred(checkILOffsetBB, newEntryBB);
-        newEntryBB->SetTrueEdge(toCheckILOffset);
-        toCheckILOffset->setLikelihood(0);
+        m_comp->fgRedirectEdge(newEntryBB->GetTrueEdgeRef(), checkILOffsetBB);
+        newEntryBB->GetTrueEdge()->setLikelihood(0);
         checkILOffsetBB->inheritWeightPercentage(newEntryBB, 0);
 
         // Make checkILOffsetBB ->(true)  onNoContinuationBB

src/coreclr/jit/block.h
@@ -684,15 +684,15 @@ public:
         return m_dupCount;
     }
 
-    void incrementDupCount()
+    void incrementDupCount(unsigned dupCount = 1)
     {
-        m_dupCount++;
+        m_dupCount += dupCount;
     }
 
-    void decrementDupCount()
+    void decrementDupCount(unsigned dupCount = 1)
     {
-        assert(m_dupCount >= 1);
-        m_dupCount--;
+        assert(m_dupCount >= dupCount);
+        m_dupCount -= dupCount;
     }
 
     bool visited() const
@@ -861,6 +861,14 @@ public:
         return bbTargetEdge;
     }
 
+    FlowEdge*& GetTargetEdgeRef()
+    {
+        assert(HasInitializedTarget());
+        assert(bbTargetEdge->getSourceBlock() == this);
+        assert(bbTargetEdge->getDestinationBlock() != nullptr);
+        return bbTargetEdge;
+    }
+
     void SetTargetEdge(FlowEdge* targetEdge)
     {
         // SetKindAndTarget() nulls target for non-jump kinds,
@@ -880,6 +888,15 @@ public:
     }
 
     FlowEdge* GetTrueEdge() const
+    {
+        assert(KindIs(BBJ_COND));
+        assert(bbTrueEdge != nullptr);
+        assert(bbTrueEdge->getSourceBlock() == this);
+        assert(bbTrueEdge->getDestinationBlock() != nullptr);
+        return bbTrueEdge;
+    }
+
+    FlowEdge*& GetTrueEdgeRef()
     {
         assert(KindIs(BBJ_COND));
         assert(bbTrueEdge != nullptr);
@@ -913,6 +930,15 @@ public:
     }
 
     FlowEdge* GetFalseEdge() const
+    {
+        assert(KindIs(BBJ_COND));
+        assert(bbFalseEdge != nullptr);
+        assert(bbFalseEdge->getSourceBlock() == this);
+        assert(bbFalseEdge->getDestinationBlock() != nullptr);
+        return bbFalseEdge;
+    }
+
+    FlowEdge*& GetFalseEdgeRef()
     {
         assert(KindIs(BBJ_COND));
         assert(bbFalseEdge != nullptr);

src/coreclr/jit/compiler.h
@@ -6101,11 +6101,7 @@ private:
     FlowEdge** fgGetPredInsertPoint(BasicBlock* blockPred, BasicBlock* newTarget);
 
 public:
-    void fgRedirectTargetEdge(BasicBlock* block, BasicBlock* newTarget);
-
-    void fgRedirectTrueEdge(BasicBlock* block, BasicBlock* newTarget);
-
-    void fgRedirectFalseEdge(BasicBlock* block, BasicBlock* newTarget);
+    void fgRedirectEdge(FlowEdge*& edgeRef, BasicBlock* newTarget);
 
     void fgFindBasicBlocks();
 

src/coreclr/jit/fgbasic.cpp
@@ -481,6 +481,7 @@ void Compiler::fgReplaceJumpTarget(BasicBlock* block, BasicBlock* oldTarget, Bas
 {
     assert(block != nullptr);
     assert(fgPredsComputed);
+    assert(fgGetPredForBlock(oldTarget, block) != nullptr);
 
     switch (block->GetKind())
     {
@@ -491,36 +492,21 @@ void Compiler::fgReplaceJumpTarget(BasicBlock* block, BasicBlock* oldTarget, Bas
         case BBJ_EHFILTERRET:
         case BBJ_LEAVE: // This function can be called before import, so we still have BBJ_LEAVE
             assert(block->TargetIs(oldTarget));
-            fgRedirectTargetEdge(block, newTarget);
+            fgRedirectEdge(block->GetTargetEdgeRef(), newTarget);
             break;
 
         case BBJ_COND:
             if (block->TrueTargetIs(oldTarget))
             {
-                if (block->FalseEdgeIs(block->GetTrueEdge()))
-                {
-                    // Branch was degenerate, simplify it first
-                    //
-                    fgRemoveConditionalJump(block);
-                    assert(block->KindIs(BBJ_ALWAYS));
-                    assert(block->TargetIs(oldTarget));
-                    fgRedirectTargetEdge(block, newTarget);
-                }
-                else
-                {
-                    fgRedirectTrueEdge(block, newTarget);
-                }
+                fgRedirectEdge(block->GetTrueEdgeRef(), newTarget);
             }
             else
             {
-                // Already degenerate cases should have taken the true path above
-                //
                 assert(block->FalseTargetIs(oldTarget));
-                assert(!block->TrueEdgeIs(block->GetFalseEdge()));
-                fgRedirectFalseEdge(block, newTarget);
+                fgRedirectEdge(block->GetFalseEdgeRef(), newTarget);
             }
 
-            if (block->KindIs(BBJ_COND) && block->TrueEdgeIs(block->GetFalseEdge()))
+            if (block->TrueEdgeIs(block->GetFalseEdge()))
             {
                 // Block became degenerate, simplify
                 //
@@ -4132,7 +4118,7 @@ void Compiler::fgFixEntryFlowForOSR()
     fgCreateNewInitBB();
     assert(fgFirstBB->KindIs(BBJ_ALWAYS));
 
-    fgRedirectTargetEdge(fgFirstBB, fgOSREntryBB);
+    fgRedirectEdge(fgFirstBB->GetTargetEdgeRef(), fgOSREntryBB);
 
     fgFirstBB->bbWeight = fgCalledCount;
     fgFirstBB->CopyFlags(fgEntryBB, BBF_PROF_WEIGHT);

src/coreclr/jit/fgehopt.cpp
@@ -171,7 +171,7 @@ PhaseStatus Compiler::fgRemoveEmptyFinally()
                     fgRemoveBlock(leaveBlock, /* unreachable */ true);
 
                     // Ref count updates.
-                    fgRedirectTargetEdge(currentBlock, postTryFinallyBlock);
+                    fgRedirectEdge(currentBlock->GetTargetEdgeRef(), postTryFinallyBlock);
                     currentBlock->SetKind(BBJ_ALWAYS);
                     currentBlock->RemoveFlags(BBF_RETLESS_CALL); // no longer a BBJ_CALLFINALLY
 
@@ -1565,7 +1565,7 @@ PhaseStatus Compiler::fgCloneFinally()
                     fgRemoveBlock(leaveBlock, /* unreachable */ true);
 
                     // Ref count updates.
-                    fgRedirectTargetEdge(currentBlock, firstCloneBlock);
+                    fgRedirectEdge(currentBlock->GetTargetEdgeRef(), firstCloneBlock);
 
                     // This call returns to the expected spot, so retarget it to branch to the clone.
                     currentBlock->RemoveFlags(BBF_RETLESS_CALL); // no longer a BBJ_CALLFINALLY
@@ -2197,7 +2197,7 @@ bool Compiler::fgRetargetBranchesToCanonicalCallFinally(BasicBlock*      block,
             canonicalCallFinally->bbNum);
 
     assert(callFinally->bbRefs > 0);
-    fgRedirectTargetEdge(block, canonicalCallFinally);
+    fgRedirectEdge(block->GetTargetEdgeRef(), canonicalCallFinally);
 
     // Update profile counts
     //

src/coreclr/jit/fgflow.cpp
@@ -367,154 +367,41 @@ FlowEdge** Compiler::fgGetPredInsertPoint(BasicBlock* blockPred, BasicBlock* new
 }
 
 //------------------------------------------------------------------------
-// fgRedirectTargetEdge: Sets block->bbTargetEdge's target block to newTarget,
-// updating pred lists as necessary.
+// fgRedirectEdge: Sets the given edge's target block to 'newTarget',
+// updating pred lists as needed.
 //
 // Arguments:
-//    block -- The block we want to make a predecessor of newTarget.
-//             It could be one already, in which case nothing changes.
-//    newTarget -- The new successor of block.
+//    edge -- The edge to update. Note that this is a reference type
+//    to support automatic updates to BasicBlock members (bbTargetEdge et al).
+//    newTarget -- The new successor of the edge.
 //
-void Compiler::fgRedirectTargetEdge(BasicBlock* block, BasicBlock* newTarget)
+void Compiler::fgRedirectEdge(FlowEdge*& edge, BasicBlock* newTarget)
 {
-    assert(block != nullptr);
+    assert(edge != nullptr);
     assert(newTarget != nullptr);
 
-    FlowEdge* edge = block->GetTargetEdge();
-    assert(edge->getDupCount() == 1);
+    BasicBlock* const block    = edge->getSourceBlock();
+    const unsigned    dupCount = edge->getDupCount();
+    fgRemoveAllRefPreds(edge->getDestinationBlock(), block);
 
-    // Update oldTarget's pred list.
-    // We could call fgRemoveRefPred, but since we're removing the one and only ref from block to oldTarget,
-    // fgRemoveAllRefPreds is slightly more efficient (one fewer branch, doesn't update edge's dup count, etc).
-    //
-    BasicBlock* oldTarget = edge->getDestinationBlock();
-    fgRemoveAllRefPreds(oldTarget, block);
-
-    // Splice edge into new target block's pred list
-    //
-    FlowEdge** predListPtr = fgGetPredInsertPoint(block, newTarget);
-    edge->setNextPredEdge(*predListPtr);
-    edge->setDestinationBlock(newTarget);
-    *predListPtr = edge;
-
-    // Pred list of target should (still) be ordered
-    //
-    assert(newTarget->checkPredListOrder());
-
-    // Edge should still have only one ref
-    assert(edge->getDupCount() == 1);
-    newTarget->bbRefs++;
-}
-
-//------------------------------------------------------------------------
-// fgRedirectTrueEdge: Sets block->bbTrueEdge's target block to newTarget,
-// updating pred lists as necessary.
-//
-// Arguments:
-//    block -- The block we want to make a predecessor of newTarget.
-//             It could be one already, in which case nothing changes.
-//    newTarget -- The new successor of block.
-//
-// Notes:
-//    This assumes block's true and false targets are different.
-//    If setting block's true target to its false target,
-//    fgRedirectTrueEdge increments the false edge's dup count,
-//    and ensures block->bbTrueEdge == block->bbFalseEdge.
-//    We don't update newTarget->bbPreds in this case,
-//    as we don't want to have more than one edge from the same predecessor.
-//
-void Compiler::fgRedirectTrueEdge(BasicBlock* block, BasicBlock* newTarget)
-{
-    assert(block != nullptr);
-    assert(newTarget != nullptr);
-    assert(block->KindIs(BBJ_COND));
-    assert(!block->TrueEdgeIs(block->GetFalseEdge()));
-
-    // Update oldTarget's pred list.
-    // We could call fgRemoveRefPred, but since we're removing the one and only ref from block to oldTarget,
-    // fgRemoveAllRefPreds is slightly more efficient (one fewer branch, doesn't update edge's dup count, etc).
-    //
-    FlowEdge*   trueEdge  = block->GetTrueEdge();
-    BasicBlock* oldTarget = trueEdge->getDestinationBlock();
-    fgRemoveAllRefPreds(oldTarget, block);
-
-    // Splice edge into new target block's pred list
-    //
     FlowEdge** predListPtr = fgGetPredInsertPoint(block, newTarget);
     FlowEdge*  predEdge    = *predListPtr;
 
-    if (block->FalseEdgeIs(predEdge))
+    if ((predEdge != nullptr) && (predEdge->getSourceBlock() == block))
     {
-        block->SetTrueEdge(predEdge);
-        predEdge->incrementDupCount();
+        edge = predEdge;
+        edge->incrementDupCount(dupCount);
     }
     else
     {
-        trueEdge->setNextPredEdge(predEdge);
-        trueEdge->setDestinationBlock(newTarget);
-        *predListPtr = trueEdge;
+        edge->setNextPredEdge(predEdge);
+        *predListPtr = edge;
+        edge->setDestinationBlock(newTarget);
     }
 
-    newTarget->bbRefs++;
+    newTarget->bbRefs += dupCount;
 
-    // Pred list of target should (still) be ordered
-    //
-    assert(newTarget->checkPredListOrder());
-}
-
-//------------------------------------------------------------------------
-// fgRedirectFalseEdge: Sets block->bbFalseEdge's target block to newTarget,
-// updating pred lists as necessary.
-//
-// Arguments:
-//    block -- The block we want to make a predecessor of newTarget.
-//             It could be one already, in which case nothing changes.
-//    newTarget -- The new successor of block.
-//
-// Notes:
-//    This assumes block's true and false targets are different.
-//    If setting block's false target to its true target,
-//    fgRedirectFalseEdge increments the true edge's dup count,
-//    and ensures block->bbTrueEdge == block->bbFalseEdge.
-//    We don't update newTarget->bbPreds in this case,
-//    as we don't want to have more than one edge from the same predecessor.
-//
-void Compiler::fgRedirectFalseEdge(BasicBlock* block, BasicBlock* newTarget)
-{
-    assert(block != nullptr);
-    assert(newTarget != nullptr);
-    assert(block->KindIs(BBJ_COND));
-    assert(!block->TrueEdgeIs(block->GetFalseEdge()));
-
-    // Update oldTarget's pred list.
-    // We could call fgRemoveRefPred, but since we're removing the one and only ref from block to oldTarget,
-    // fgRemoveAllRefPreds is slightly more efficient (one fewer branch, doesn't update edge's dup count, etc).
-    //
-    FlowEdge*   falseEdge = block->GetFalseEdge();
-    BasicBlock* oldTarget = falseEdge->getDestinationBlock();
-    fgRemoveAllRefPreds(oldTarget, block);
-
-    // Splice edge into new target block's pred list
-    //
-    FlowEdge** predListPtr = fgGetPredInsertPoint(block, newTarget);
-    FlowEdge*  predEdge    = *predListPtr;
-
-    if (block->TrueEdgeIs(predEdge))
-    {
-        block->SetFalseEdge(predEdge);
-        predEdge->incrementDupCount();
-    }
-    else
-    {
-        falseEdge->setNextPredEdge(predEdge);
-        falseEdge->setDestinationBlock(newTarget);
-        *predListPtr = falseEdge;
-    }
-
-    newTarget->bbRefs++;
-
-    // Pred list of target should (still) be ordered
-    //
+    // Pred list of target should still be ordered
     assert(newTarget->checkPredListOrder());
 }
 

src/coreclr/jit/fginline.cpp
@@ -1852,7 +1852,7 @@ void Compiler::fgInsertInlineeBlocks(InlineInfo* pInlineInfo)
         // Insert inlinee's blocks into inliner's block list.
         assert(topBlock->KindIs(BBJ_ALWAYS));
         assert(topBlock->TargetIs(bottomBlock));
-        fgRedirectTargetEdge(topBlock, InlineeCompiler->fgFirstBB);
+        fgRedirectEdge(topBlock->GetTargetEdgeRef(), InlineeCompiler->fgFirstBB);
 
         topBlock->SetNext(InlineeCompiler->fgFirstBB);
         InlineeCompiler->fgLastBB->SetNext(bottomBlock);

commit 891f0e67104c9fbb3da62b287d5dbd28e5b14f27

JIT: fix bit-test switch lowering when the bit table is inverted (#131736)
Fixes #131716

`TryLowerSwitchToBitTest` inverts the bit table on xarch when its upper
32 bits are all set, so that it still fits in a 32 bit immediate, and
swaps the two targets. The swap only updated `bbCase0`/`bbCase1`, but
everything after it consumes `case0Edge`/`case1Edge` - so `SetCond`
wired the JCC to the wrong successor, and the dup-count fixup
decremented `bbRefs` on the wrong blocks. Regressed by #116933, which
replaced the `fgRemoveAllRefPreds`/`fgAddRefPred` pair (which re-derived
the edges from the swapped blocks) with in-place dup-count adjustment.

Fix: swap the edges and derive the blocks afterwards.

The inversion can only trigger with exactly 64 bit table entries — below
that, bits above `bitCount` are zero, so `~bitTable` exceeds
`UINT32_MAX`. That is why only 64-case switches were affected.

Before (64 cases, indices 1-3 -> 111):
```asm
mov      eax, 14
bt       rax, rcx
jae      SHORT G_M14444_IG06   ; wrong: bit set falls through to 222
```
After:
```asm
mov      eax, 14
bt       rax, rcx
jb       SHORT G_M14444_IG06   ; bit set -> 111
```

No SPMI asm diffs on benchmarks.run, libraries.pmi and
libraries.crossgen2 (~660K contexts).

Co-authored-by: Copilot <223556219+Copilot@users.noreply.github.com>
Copilot-Session: 3c492a8b-6607-48dc-8a2e-98128e3197b0

src/coreclr/jit/lower.cpp
@@ -1429,8 +1429,8 @@ bool Lowering::TryLowerSwitchToBitTest(FlowEdge*   jumpTable[],
 
     //
     // Build a bit table where a bit set to 0 corresponds to bbCase0 and a bit set to 1 corresponds to
-    // bbCase1. Simply use the first block in the jump table as bbCase1, later we can invert the bit
-    // table and/or swap the blocks if it's beneficial.
+    // bbCase1. Simply use the first edge in the jump table as case1Edge, later we can invert the bit
+    // table and/or swap the edges if it's beneficial.
     //
 
     FlowEdge* case0Edge = nullptr;
@@ -1456,11 +1456,6 @@ bool Lowering::TryLowerSwitchToBitTest(FlowEdge*   jumpTable[],
         }
     }
 
-    BasicBlock* bbCase0 = case0Edge->getDestinationBlock();
-    BasicBlock* bbCase1 = case1Edge->getDestinationBlock();
-
-    JITDUMP("Lowering switch " FMT_BB " to bit test\n", bbSwitch->bbNum);
-
 #if defined(TARGET_64BIT) && defined(TARGET_XARCH)
     //
     // See if we can avoid a 8 byte immediate on 64 bit targets. If all upper 32 bits are 1
@@ -1473,9 +1468,14 @@ bool Lowering::TryLowerSwitchToBitTest(FlowEdge*   jumpTable[],
     if (~bitTable <= UINT32_MAX)
     {
         bitTable = ~bitTable;
-        std::swap(bbCase0, bbCase1);
+        std::swap(case0Edge, case1Edge);
     }
 #endif
+
+    BasicBlock* bbCase0 = case0Edge->getDestinationBlock();
+    BasicBlock* bbCase1 = case1Edge->getDestinationBlock();
+
+    JITDUMP("Lowering switch " FMT_BB " to bit test\n", bbSwitch->bbNum);
 
     //
     // Set successor edge dup counts to 1 each

src/tests/JIT/Regression/JitBlue/Runtime_131716/Runtime_131716.cs
@@ -1,0 +1,186 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+// Regression test for https://github.com/dotnet/runtime/issues/131716
+//
+// A switch with exactly 64 cases and two distinct targets is lowered to a bit test.
+// On xarch the JIT inverts the bit table when its upper 32 bits are all set, so that the
+// table still fits in a 32 bit immediate. The inversion has to swap the two successor
+// edges as well; only swapping the target blocks left the bit test branching to the
+// wrong target (and corrupted the block ref counts).
+
+using System.Runtime.CompilerServices;
+using Xunit;
+
+namespace Runtime_131716;
+
+public class Runtime_131716
+{
+    // 64 cases, two targets. Case 0 and cases 4..63 share a target, so the bit table is
+    // 0xFFFFFFFFFFFFFFF1 and gets inverted to 0xE.
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private static int Inverted(uint x)
+    {
+        switch (x)
+        {
+            case 1:
+            case 2:
+            case 3:
+                return 111;
+            case 0:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+            case 13:
+            case 14:
+            case 15:
+            case 16:
+            case 17:
+            case 18:
+            case 19:
+            case 20:
+            case 21:
+            case 22:
+            case 23:
+            case 24:
+            case 25:
+            case 26:
+            case 27:
+            case 28:
+            case 29:
+            case 30:
+            case 31:
+            case 32:
+            case 33:
+            case 34:
+            case 35:
+            case 36:
+            case 37:
+            case 38:
+            case 39:
+            case 40:
+            case 41:
+            case 42:
+            case 43:
+            case 44:
+            case 45:
+            case 46:
+            case 47:
+            case 48:
+            case 49:
+            case 50:
+            case 51:
+            case 52:
+            case 53:
+            case 54:
+            case 55:
+            case 56:
+            case 57:
+            case 58:
+            case 59:
+            case 60:
+            case 61:
+            case 62:
+            case 63:
+                return 222;
+            default:
+                return 333;
+        }
+    }
+
+    // Same shape, but cases 0..3 share a target so the bit table is 0xF and is not inverted.
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private static int NotInverted(uint x)
+    {
+        switch (x)
+        {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+                return 111;
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+            case 13:
+            case 14:
+            case 15:
+            case 16:
+            case 17:
+            case 18:
+            case 19:
+            case 20:
+            case 21:
+            case 22:
+            case 23:
+            case 24:
+            case 25:
+            case 26:
+            case 27:
+            case 28:
+            case 29:
+            case 30:
+            case 31:
+            case 32:
+            case 33:
+            case 34:
+            case 35:
+            case 36:
+            case 37:
+            case 38:
+            case 39:
+            case 40:
+            case 41:
+            case 42:
+            case 43:
+            case 44:
+            case 45:
+            case 46:
+            case 47:
+            case 48:
+            case 49:
+            case 50:
+            case 51:
+            case 52:
+            case 53:
+            case 54:
+            case 55:
+            case 56:
+            case 57:
+            case 58:
+            case 59:
+            case 60:
+            case 61:
+            case 62:
+            case 63:
+                return 222;
+            default:
+                return 333;
+        }
+    }
+
+    [Fact]
+    public static void TestEntryPoint()
+    {
+        for (uint i = 0; i < 128; i++)
+        {
+            Assert.Equal(i > 63 ? 333 : (i is >= 1 and <= 3) ? 111 : 222, Inverted(i));
+            Assert.Equal(i > 63 ? 333 : (i <= 3) ? 111 : 222, NotInverted(i));
+        }
+
+        Assert.Equal(333, Inverted(uint.MaxValue));
+        Assert.Equal(333, NotInverted(uint.MaxValue));
+    }
+}

src/tests/JIT/Regression/Regression_ro_2.csproj
@@ -126,6 +126,7 @@
     <Compile Include="JitBlue\Runtime_125160\Runtime_125160.cs" />
     <Compile Include="JitBlue\GitHub_131472\GitHub_131472.cs" />
     <Compile Include="JitBlue\Runtime_122237\Runtime_122237.cs" />
+    <Compile Include="JitBlue\Runtime_131716\Runtime_131716.cs" />
   </ItemGroup>
   <Import Project="$(TestSourceDir)MergedTestRunner.targets" />
 </Project>