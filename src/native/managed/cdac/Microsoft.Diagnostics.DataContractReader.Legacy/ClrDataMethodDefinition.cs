c36073dd0914509fee27a4f63b1cef56f97e31e9
Rachel Jarvi <rachel.jarvi@gmail.com>
2026-08-04 18:26:36 -0700

    Update ClrDataMethodDefinition.cs

--- src/native/managed/cdac/Microsoft.Diagnostics.DataContractReader.Legacy/ClrDataMethodDefinition.cs
+++ src/native/managed/cdac/Microsoft.Diagnostics.DataContractReader.Legacy/ClrDataMethodDefinition.cs
@@ -242,7 +242,7 @@ public sealed unsafe partial class ClrDataMethodDefinition : IXCLRDataMethodDefi
 #if DEBUG
         if (_legacyImpl is not null)
         {
-            Debug.ValidateHResult(hr, hrLocal, HResultValidationMode.AllowCdacSuccess);
+            Debug.ValidateHResult(hr, hrLocal);
         }
 #endif
 
