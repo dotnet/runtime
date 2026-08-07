e62aaf8f48a0b85b184654c26547a455ac453244
Rachel Jarvi <rachel.jarvi@gmail.com>
2026-08-06 22:14:59 -0700

    Update PalUnix.cpp

--- src/coreclr/nativeaot/Runtime/unix/PalUnix.cpp
+++ src/coreclr/nativeaot/Runtime/unix/PalUnix.cpp
@@ -862,7 +862,7 @@ HANDLE PalGetModuleHandleFromPointer(_In_ void* pointer, bool pinModule)
         {
             // NativeAOT runtime state cannot be safely unloaded.
             // Keep the extra reference for the lifetime of the process.
-            (void)dlopen(info.dli_fname, RTLD_LAZY | RTLD_NOLOAD);
+            dlopen(info.dli_fname, RTLD_LAZY | RTLD_NOLOAD);
         }
 #else
         (void)pinModule;
