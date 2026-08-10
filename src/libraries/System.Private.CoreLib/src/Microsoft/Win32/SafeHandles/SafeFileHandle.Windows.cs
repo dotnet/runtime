9931d09bc433eb126ee975e92c6759c4a46af473
Adam Sitnik <adam.sitnik@gmail.com>
2026-08-10 19:22:59 +0200

    use GetFileType like we did for .NET 10 and before

--- src/libraries/System.Private.CoreLib/src/Microsoft/Win32/SafeHandles/SafeFileHandle.Windows.cs
+++ src/libraries/System.Private.CoreLib/src/Microsoft/Win32/SafeHandles/SafeFileHandle.Windows.cs
@@ -327,7 +327,19 @@ namespace Microsoft.Win32.SafeHandles
             return _fileOptions = result;
         }
 
-        private bool GetCanSeekCore() => Interop.Kernel32.SetFilePointerEx(this, 0, out _, (uint)SeekOrigin.Current);
+        // We need to figure out whether given file is seekable or not.
+        // The options that were considered:
+        // 1. Using SetFilePointerEx to move by 0 from current position:
+        //  - It can succeed for non-seekable files (pipes, sockets) opened for sync IO,
+        //  - It can fail for non-seekable files (pipes, sockets) opened for async IO.
+        // 2. Using Type property which internally uses GetFileType and then GetNamedPipeInfo to distinguish pipes from sockets.
+        // GetNamedPipeInfo requires GENERIC_READ or GENERIC_WRITE and FILE_READ_ATTRIBUTES access.
+        // There are valid scenarios like CreateNamedPipeW(PIPE_ACCESS_OUTBOUND) where GetNamedPipeInfo fails with ERROR_ACCESS_DENIED.
+        // 3. Switching from GetNamedPipeInfo to getsockopt to distinguish pipes from sockets. It would add a dependency on ws2_32.dll, which is not desirable.
+        // That is why we use the cached file type (if available), and if not available, we use GetFileType to determine whether the file is seekable or not.
+        private bool GetCanSeekCore() => _cachedFileType != -1
+            ? (FileHandleType)_cachedFileType == FileHandleType.RegularFile
+            : Interop.Kernel32.GetFileType(this) == Interop.Kernel32.FileTypes.FILE_TYPE_DISK;
 
         internal FileHandleType GetFileTypeCore()
         {
