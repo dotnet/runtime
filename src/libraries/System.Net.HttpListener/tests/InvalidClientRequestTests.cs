fa6df8d83b6a8eae3f7aa48947a184690b83331c
iremyux <iremyuksel@microsoft.com>
2026-08-11 21:39:46 +0200

    Change order of  test data

--- src/libraries/System.Net.HttpListener/tests/InvalidClientRequestTests.cs
+++ src/libraries/System.Net.HttpListener/tests/InvalidClientRequestTests.cs
@@ -82,6 +82,8 @@ namespace System.Net.Tests
 
             yield return new object[] { "GET {path} HTTP/1.1", null, new string[] { "NoValue" }, null, "Bad Request" };
             yield return new object[] { "GET {path} HTTP/1.1", null, new string[] { ":" }, null, "Bad Request" };
+            yield return new object[] { "GET {path} HTTP/1.1", null, new string[] { "\0:value" }, null, "Bad Request" };
+            yield return new object[] { "GET {path} HTTP/1.1", null, new string[] { "value:\0" }, null, "Bad Request" };
             if (Helpers.IsManagedImplementation)
             {
                 yield return new object[] { "GET {path} HTTP/1.1", null, new string[] { " Header: value" }, null, "Bad Request" };
@@ -89,8 +91,6 @@ namespace System.Net.Tests
                 yield return new object[] { "GET {path} HTTP/1.1", null, new string[] { "Header : value" }, null, "Bad Request" };
                 yield return new object[] { "GET {path} HTTP/1.1", null, new string[] { "Header\t: value" }, null, "Bad Request" };
             }
-            yield return new object[] { "GET {path} HTTP/1.1", null, new string[] { "\0:value" }, null, "Bad Request" };
-            yield return new object[] { "GET {path} HTTP/1.1", null, new string[] { "value:\0" }, null, "Bad Request" };
 
             yield return new object[] { "GET {path} HTTP/1.1", "", null, null, "Bad Request" };
             yield return new object[] { "GET {path} HTTP/1.1", "Host: \r\n", null, null, "Bad Request" };
