f168f7cc53a880d2103e7ad94c2a8ace63000eca
Jeremy Koritzinsky <jekoritz@microsoft.com>
2026-08-11 21:45:14 -0700

    Update Program.cs
    Co-authored-by: Jan Kotas <jkotas@microsoft.com>

--- src/tests/Interop/COM/ComWrappers/GcRestrictedCalloutReversePInvoke/Program.cs
+++ src/tests/Interop/COM/ComWrappers/GcRestrictedCalloutReversePInvoke/Program.cs
@@ -11,11 +11,8 @@ using Xunit;
 
 public class Program
 {
-    public static bool IsSupportedConfiguration =>
-        TestLibrary.PlatformDetection.IsMultithreadingSupported &&
-        !TestLibrary.CoreClrConfigurationDetection.IsGCStress;
-
-    [ConditionalFact(typeof(Program), nameof(IsSupportedConfiguration))]
+    [ConditionalFact(typeof(PlatformDetection), nameof(PlatformDetection.IsMultithreadingSupported))]
+    [SkipOnCoreClr("This test is not compatible with GC stress.", RuntimeTestModes.AnyGCStress)]
     public static void TestEntryPoint()
     {
         ComWrappers.RegisterForTrackerSupport(TrackerComWrappers.Instance);
