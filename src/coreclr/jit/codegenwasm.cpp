9d7ac0f0ca8bfdb9279219aff6937e80db6e1ea0
Adam Perlin <adamp@nanosoft.com>
2026-08-12 15:17:08 -0700

    Implement SP/FP delta computation methods on Wasm

--- src/coreclr/jit/codegenwasm.cpp
+++ src/coreclr/jit/codegenwasm.cpp
@@ -4466,20 +4466,18 @@ int CodeGenInterface::genTotalFrameSize() const
 }
 
 //---------------------------------------------------------------------
-// genSPtoFPdelta - return the offset from SP to the frame pointer.
-// This number is going to be positive, since SP must be at the lowest
-// address.
+// genSPtoFPdelta - return the offset from the initial SP to the frame pointer in linear memory.
+// This number should be zero on wasm, since Initial SP = FP.
 //
 // There must be a frame pointer to call this function!
 int CodeGenInterface::genSPtoFPdelta() const
 {
     assert(isFramePointerUsed());
-    NYI_WASM("genSPtoFPdelta");
     return 0;
 }
 
 //---------------------------------------------------------------------
-// genCallerSPtoFPdelta - return the offset from Caller-SP to the frame pointer.
+// genCallerSPtoFPdelta - return the offset from Caller-SP to the frame pointer in linear memory.
 // This number is going to be negative, since the Caller-SP is at a higher
 // address than the frame pointer.
 //
@@ -4487,18 +4485,20 @@ int CodeGenInterface::genSPtoFPdelta() const
 int CodeGenInterface::genCallerSPtoFPdelta() const
 {
     assert(isFramePointerUsed());
-    NYI_WASM("genCallerSPtoFPdelta");
-    return 0;
+    int callerSPToFPDelta = genSPtoFPdelta() + genCallerSPtoInitialSPdelta();
+    assert(callerSPToFPDelta <= 0);
+    return callerSPToFPDelta;
 }
 
 //---------------------------------------------------------------------
-// genCallerSPtoInitialSPdelta - return the offset from Caller-SP to Initial SP.
+// genCallerSPtoInitialSPdelta - return the offset from Caller-SP to Initial SP in linear memory.
 //
 // This number will be negative.
 int CodeGenInterface::genCallerSPtoInitialSPdelta() const
 {
-    NYI_WASM("genCallerSPtoInitialSPdelta");
-    return 0;
+    int callerSPToInitialSPDelta = -genTotalFrameSize();
+    assert(callerSPToInitialSPDelta <= 0);
+    return callerSPToInitialSPDelta;
 }
 
 void RegSet::verifyRegUsed(regNumber reg)
