5d4d049f1f7cc434d5b516e7ac517d70fd8085ea
Eduardo Velarde <evelardepola@microsoft.com>
2026-08-20 15:13:49 -0700

    Abort instead of assert on unixes

--- src/coreclr/vm/ceemain.cpp
+++ src/coreclr/vm/ceemain.cpp
@@ -1877,6 +1877,10 @@ void CheckThreadStateNotDestroyed()
         return;
     }
 
+#if defined(TARGET_UNIX) && !defined(TARGET_WASM)
+    minipal_log_write_error("Attempt to execute managed code after the .NET runtime thread state has been destroyed.\n");
+    abort();
+#else
     // Managed C++ may run managed code in DllMain (e.g. during DLL_PROCESS_DETACH to run global destructors). This is
     // not supported and unreliable. Historically, it happened to work most of the time. For backward compatibility,
     // suppress this assert in release builds if we have encountered any mixed mode binaries.
@@ -1888,6 +1892,7 @@ void CheckThreadStateNotDestroyed()
     {
         _ASSERTE_ALL_BUILDS(!"Attempt to execute managed code after the .NET runtime thread state has been destroyed.");
     }
+#endif
 }
 
 #ifdef DEBUGGING_SUPPORTED
