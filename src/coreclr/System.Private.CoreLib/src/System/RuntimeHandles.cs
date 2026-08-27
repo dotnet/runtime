3eb0f3db4d192b9bdf6f1d37ca8b5f8c027e4ac9
Adam Perlin <adamp@nanosoft.com>
2026-08-27 16:35:37 -0700

    Remove RuntimeTypeHandle.GetRuntimeTypeFromHandleMaybeNull

--- src/coreclr/System.Private.CoreLib/src/System/RuntimeHandles.cs
+++ src/coreclr/System.Private.CoreLib/src/System/RuntimeHandles.cs
@@ -31,7 +31,7 @@ namespace System
         /// <param name="value">An IntPtr handle to a RuntimeType to create a <see cref="RuntimeTypeHandle"/> object from.</param>
         /// <returns>A new <see cref="RuntimeTypeHandle"/> object that corresponds to the value parameter.</returns>
         public static RuntimeTypeHandle FromIntPtr(IntPtr value) =>
-            new RuntimeTypeHandle(GetRuntimeTypeFromHandleMaybeNull(value));
+            new RuntimeTypeHandle(value != IntPtr.Zero ? GetRuntimeTypeFromHandle(value) : null);
 
         [LibraryImport(RuntimeHelpers.QCall, EntryPoint = "RuntimeTypeHandle_GetRuntimeTypeFromHandleSlow")]
         private static partial void GetRuntimeTypeFromHandleSlow(
@@ -53,16 +53,6 @@ namespace System
             return (h.IsTypeDesc
                 ? h.AsTypeDesc()->ExposedClassObject
                 : h.AsMethodTable()->AuxiliaryData->ExposedClassObject) ?? GetRuntimeTypeFromHandleSlow(handle);
-        }
-
-        internal static RuntimeType? GetRuntimeTypeFromHandleMaybeNull(IntPtr handle)
-        {
-            if (handle == IntPtr.Zero)
-            {
-                return null;
-            }
-
-            return GetRuntimeTypeFromHandle(handle);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
