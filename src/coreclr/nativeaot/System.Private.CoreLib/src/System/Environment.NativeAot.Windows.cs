6071cc6a72256a744f68e3a320b72d74f9b2906f
pavelsavara <pavel.savara@gmail.com>
2026-08-27 18:22:18 +0200

    Guard NativeAOT CoreLib callers of Windows-only interop with CA1416 pragmas

--- src/coreclr/nativeaot/System.Private.CoreLib/src/System/Environment.NativeAot.Windows.cs
+++ src/coreclr/nativeaot/System.Private.CoreLib/src/System/Environment.NativeAot.Windows.cs
@@ -1,6 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#pragma warning disable CA1416 // Interop is only supported on Windows; this file is compiled only for Windows.
+
 using System.Diagnostics.CodeAnalysis;
 
 namespace System
