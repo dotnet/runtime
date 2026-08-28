d9bf4d78e23830dcd58485498eb087588a673a7a
Jackson Schuster <36744439+jtschuster@users.noreply.github.com>
2026-08-28 13:30:03 -0700

    Unblock WasmEmitter in NativeAOT

--- src/coreclr/tools/Common/Compiler/DependencyAnalysis/Target_Wasm/WasmEmitter.cs
+++ src/coreclr/tools/Common/Compiler/DependencyAnalysis/Target_Wasm/WasmEmitter.cs
@@ -2,41 +2,19 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
-using System.Diagnostics;
 
-using ILCompiler.ObjectWriter;
-
-// These #ifs will disappear when we enable NativeAOT support for Wasm
-#if READYTORUN
 using ILCompiler.ObjectWriter.WasmInstructions;
-#endif
 
 namespace ILCompiler.DependencyAnalysis.Wasm
 {
-    public struct WasmEmitter
+    public struct WasmEmitter(NodeFactory factory, bool relocsOnly)
     {
-#if READYTORUN
-        public WasmFunctionBody FunctionBody = null;
-#endif
-
-        private readonly NodeFactory _factory;
-        private readonly bool _relocsOnly;
-
-        public WasmEmitter(NodeFactory factory, bool relocsOnly)
-        {
-            _factory = factory;
-            _relocsOnly = relocsOnly;
-#if READYTORUN
-            FunctionBody = null;
-#endif
-        }
-
-        public bool Is64Bit => _factory.Target.PointerSize == 8;
-        public bool RelocsOnly => _relocsOnly;
+        public WasmFunctionBody FunctionBody;
+        public bool Is64Bit => factory.Target.PointerSize == 8;
+        public bool RelocsOnly => relocsOnly;
 
         public ObjectNode.ObjectData Encode(ISymbolDefinitionNode symbolDefinitionNode)
         {
-#if READYTORUN
             byte[] encodedThunk = new byte[FunctionBody.EncodeSize()];
             FunctionBody.Encode(encodedThunk);
 
@@ -44,9 +22,6 @@ namespace ILCompiler.DependencyAnalysis.Wasm
             FunctionBody.EncodeRelocations(relocs.AsSpan());
 
             return new ObjectNode.ObjectData(encodedThunk, relocs, 1, new ISymbolDefinitionNode[] { symbolDefinitionNode });
-#else
-            throw new PlatformNotSupportedException("NativeAOT WebAssembly assembly stubs are not supported.");
-#endif
         }
     }
 }
