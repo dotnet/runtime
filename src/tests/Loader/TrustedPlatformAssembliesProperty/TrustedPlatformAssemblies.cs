55426381f0cfa3845f85d0bb4385f8bd3e012d05
Elinor Fung <elfung@microsoft.com>
2026-08-28 11:48:38 -0700

    Fix tests on macOS

--- src/tests/Loader/TrustedPlatformAssembliesProperty/TrustedPlatformAssemblies.cs
+++ src/tests/Loader/TrustedPlatformAssembliesProperty/TrustedPlatformAssemblies.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
+using System.IO;
 using Xunit;
 
 public class TrustedPlatformAssemblies
@@ -10,6 +11,14 @@ public class TrustedPlatformAssemblies
     public static void IsAvailable()
     {
         string tpa = Assert.IsType<string>(AppContext.GetData("TRUSTED_PLATFORM_ASSEMBLIES"));
-        Assert.Contains(typeof(object).Assembly.Location, tpa, StringComparison.OrdinalIgnoreCase);
+        string coreLibLocation = typeof(object).Assembly.Location;
+
+        // On macOS, /tmp is a symlink to /private/tmp. Assembly.Location resolves the path, while host-provided TPA paths do not.
+        if (OperatingSystem.IsMacOS() && coreLibLocation.StartsWith("/private/tmp/", StringComparison.Ordinal))
+        {
+            coreLibLocation = coreLibLocation["/private".Length..];
+        }
+
+        Assert.Contains(coreLibLocation, tpa.Split(Path.PathSeparator), StringComparer.OrdinalIgnoreCase);
     }
 }
