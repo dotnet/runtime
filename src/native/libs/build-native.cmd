ab84324b6c7802041afd341ac0e16509d6653e4c
pavelsavara <pavel.savara@gmail.com>
2026-08-29 20:18:47 +0200

    fix WASI build on windows

--- src/native/libs/build-native.cmd
+++ src/native/libs/build-native.cmd
@@ -63,6 +63,9 @@ if "%__TargetOS%"=="android" (
 if "%__TargetOS%"=="browser" (
     set __CrossTarget=1
 )
+if "%__TargetOS%"=="wasi" (
+    set __CrossTarget=1
+)
 
 if %__CrossTarget% EQU 0 (
     call "%__repoRoot%\eng\native\version\copy_version_files.cmd"
