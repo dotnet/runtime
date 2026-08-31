fcc2617aad63d8b6bb509849c22be2955b036534
Jackson Schuster <36744439+jtschuster@users.noreply.github.com>
2026-08-31 15:14:44 -0700

    Remove dead code for ReturnCall
    Co-authored-by: Jackson Schuster <36744439+jtschuster@users.noreply.github.com>

--- src/coreclr/tools/Common/Compiler/ObjectWriter/WasmInstructions.cs
+++ src/coreclr/tools/Common/Compiler/ObjectWriter/WasmInstructions.cs
@@ -122,7 +122,6 @@ namespace ILCompiler.ObjectWriter.WasmInstructions
         End = 0x0B,
         Call = 0x10,
         CallIndirect = 0x11,
-        ReturnCall = 0x12,
         LocalGet = 0x20,
         LocalSet = 0x21,
         LocalTee = 0x22,
@@ -880,7 +879,6 @@ namespace ILCompiler.ObjectWriter.WasmInstructions
         public static WasmExpr Unreachable => new WasmUnaryExpr(WasmExprKind.Unreachable);
         public static WasmExpr Call(ISymbolNode target) => new WasmLEBConstantReloc(WasmExprKind.Call, target, RelocType.WASM_FUNCTION_INDEX_LEB);
         public static WasmExpr CallIndirect(ISymbolNode funcType, uint tableIndex) => new WasmIndirectCallInstruction(WasmExprKind.CallIndirect, funcType, tableIndex);
-        public static WasmExpr ReturnCall(ISymbolNode target) => new WasmLEBConstantReloc(WasmExprKind.ReturnCall, target, RelocType.WASM_FUNCTION_INDEX_LEB);
     }
     internal static class Table
     {
