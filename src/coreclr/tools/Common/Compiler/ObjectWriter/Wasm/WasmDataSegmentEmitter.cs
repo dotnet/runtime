0d759c41d4e52a3bf4fd1f72909303c64ef24d79
Jackson Schuster <36744439+jtschuster@users.noreply.github.com>
2026-09-02 16:08:14 -0700

    Revert "Remove GetMemoryAddressOfOffset() from IWasmDataSegment"
    This reverts commit d3523737fb691ae94324961fda12126d7bfabf08.

--- src/coreclr/tools/Common/Compiler/ObjectWriter/Wasm/WasmDataSegmentEmitter.cs
+++ src/coreclr/tools/Common/Compiler/ObjectWriter/Wasm/WasmDataSegmentEmitter.cs
@@ -74,5 +74,11 @@ namespace ILCompiler.ObjectWriter
         {
             return new WasmInstructionGroup([I32.PaddedConst(_memoryOffset)]);
         }
+
+        public int GetMemoryAddressOfOffset(int offsetInSegment)
+        {
+            Debug.Assert(offsetInSegment >= 0 && offsetInSegment <= ContentReadStream.Length);
+            return _memoryOffset + offsetInSegment;
+        }
     }
 }
