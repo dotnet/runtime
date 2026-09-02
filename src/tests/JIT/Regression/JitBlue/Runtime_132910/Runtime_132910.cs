fc9cf768b52020539f60eb7a77a0ff86098aa155
David Hartglass <dhartglass+github@microsoft.com>
2026-09-02 13:41:58 -0700

    copilot comments

--- src/tests/JIT/Regression/JitBlue/Runtime_132910/Runtime_132910.cs
+++ src/tests/JIT/Regression/JitBlue/Runtime_132910/Runtime_132910.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
 using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Runtime.Intrinsics.Arm;
@@ -12,20 +13,22 @@ public class Runtime_132910
     private static Vector<float> Repro() => Sve.CreateTrueMaskSingle(SveMaskPattern.VectorCount5);
 
     [MethodImpl(MethodImplOptions.NoInlining)]
-    private static Vector<long> CreateVectorCount16Mask() => Sve.CreateTrueMaskInt64(SveMaskPattern.VectorCount16);
+    private static Vector<float> CreateVectorCount256Mask() => Sve.CreateTrueMaskSingle(SveMaskPattern.VectorCount256);
 
     [ConditionalFact(typeof(Sve), nameof(Sve.IsSupported))]
     public static void TestEntryPoint()
     {
-        _ = Repro();
+        const int VectorCount5 = 5;
 
-        Vector<long> mask = CreateVectorCount16Mask();
-        const int RequestedCount = 16;
-        int activeElementCount = Vector<long>.Count >= RequestedCount ? RequestedCount : 0;
+        AssertMask(Repro(), Vector<float>.Count >= VectorCount5 ? VectorCount5 : 0);
+        AssertMask(CreateVectorCount256Mask(), 0);
+    }
 
-        for (int i = 0; i < Vector<long>.Count; i++)
+    private static void AssertMask(Vector<float> mask, int activeElementCount)
+    {
+        for (int i = 0; i < Vector<float>.Count; i++)
         {
-            Assert.Equal(i < activeElementCount ? -1L : 0L, mask[i]);
+            Assert.Equal(i < activeElementCount ? -1 : 0, BitConverter.SingleToInt32Bits(mask[i]));
         }
     }
 }
