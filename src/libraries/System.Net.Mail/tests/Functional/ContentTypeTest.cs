15df36cd8a07b1de551fea66b3466219fd26238b
Michal Žůrek <188900745+mrek-msft@users.noreply.github.com>
2026-09-07 13:34:14 +0200

    Add test case

--- src/libraries/System.Net.Mail/tests/Functional/ContentTypeTest.cs
+++ src/libraries/System.Net.Mail/tests/Functional/ContentTypeTest.cs
@@ -177,6 +177,8 @@ namespace System.Net.Mime.Tests
         [InlineData("=?utf-8?B?Zm9v?=\"; filename=\"foo.txt")]
         // A would-be encoded-word containing an unescaped quote in the data section.
         [InlineData("=?utf-8?Q?foo\"; filename=\"foo.txt?=")]
+        // Like the above, but with no whitespace after ';' (still should not be treated as safe pre-encoded input).
+        [InlineData("=?utf-8?Q?foo\";filename=\"foo.txt?=")]
         [InlineData("=?utf-8?Q?foo\"bar?=")]
         // Trailing content after the closing "?=" with no folding whitespace.
         [InlineData("=?utf-8?B?Zm9v?=trailing")]
