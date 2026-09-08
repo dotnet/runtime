93f32427ca18871cde273e7dfdd72284ee267795
Michał Petryka <paprikapolishgamer@gmail.com>
2026-09-08 12:30:14 +0200

    Regen

--- src/coreclr/inc/jiteeversionguid.h
+++ src/coreclr/inc/jiteeversionguid.h
@@ -37,11 +37,11 @@
 
 #include <minipal/guid.h>
 
-constexpr GUID JITEEVersionIdentifier = { /* 5a3e8dc8-83bf-47e5-b032-531dbd307dbe */
-    0x5a3e8dc8,
-    0x83bf,
-    0x47e5,
-    {0xb0, 0x32, 0x53, 0x1d, 0xbd, 0x30, 0x7d, 0xbe}
+constexpr GUID JITEEVersionIdentifier = { /* 9e4d8272-5aa1-4dbc-b6bf-fe7829b1b253 */
+    0x9e4d8272,
+    0x5aa1,
+    0x4dbc,
+    {0xb6, 0xbf, 0xfe, 0x78, 0x29, 0xb1, 0xb2, 0x53}
   };
 
 #endif // JIT_EE_VERSIONING_GUID_H
