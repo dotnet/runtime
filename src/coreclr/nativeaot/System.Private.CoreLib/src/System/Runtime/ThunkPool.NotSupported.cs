9cfab0698b51865e8433257bd7bb0c9f303be6f5
Jackson Schuster <36744439+jtschuster@users.noreply.github.com>
2026-09-17 12:17:43 -0700

    remove extra stuff

--- src/coreclr/nativeaot/System.Private.CoreLib/src/System/Runtime/ThunkPool.NotSupported.cs
+++ src/coreclr/nativeaot/System.Private.CoreLib/src/System/Runtime/ThunkPool.NotSupported.cs
@@ -1,11 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-//
-// WASM does not support dynamic code generation without access to the host.
-// Browser hosts can support it, but NativeAOT does not support the APIs that
-// require thunks there.
-//
 #pragma warning disable CA1822 // Mark members as static
 
 namespace System.Runtime
