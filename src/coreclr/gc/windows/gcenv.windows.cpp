468cc8481adc98401097a079531bdd46b2b31080
Jan Vorlicek <janvorli@microsoft.com>
2026-09-18 15:45:44 +0200

    Reflect PR feedback

--- src/coreclr/gc/windows/gcenv.windows.cpp
+++ src/coreclr/gc/windows/gcenv.windows.cpp
@@ -22,6 +22,8 @@ static bool g_SeLockMemoryPrivilegeAcquired = false;
 // The cached total number of CPUs that can be used in the OS.
 uint32_t g_totalCpuCount = 0;
 
+static uint32_t g_maxProcessorCount = 0;
+
 static AffinitySet g_processAffinitySet;
 
 namespace {
@@ -503,6 +505,14 @@ void GetGroupForProcessor(uint16_t processor_number, uint16_t* group_number, uin
 //  true if it has succeeded, false if it has failed
 bool GCToOSInterface::Initialize()
 {
+    uint16_t maximumProcessorGroupCount = GetMaximumProcessorGroupCount();
+    if (maximumProcessorGroupCount == 0)
+    {
+        return false;
+    }
+
+    g_maxProcessorCount = static_cast<uint32_t>(maximumProcessorGroupCount) * 64;
+
     SYSTEM_INFO systemInfo;
     GetSystemInfo(&systemInfo);
 
@@ -515,7 +525,7 @@ bool GCToOSInterface::Initialize()
     InitNumaNodeInfo();
     InitCPUGroupInfo();
 
-    if (!g_processAffinitySet.Initialize(GCToOSInterface::GetTotalProcessorCount()))
+    if (!g_processAffinitySet.Initialize(GCToOSInterface::GetMaxProcessorCount()))
     {
         return false;
     }
@@ -1087,7 +1097,8 @@ uint32_t GCToOSInterface::GetTotalProcessorCount()
 
 uint32_t GCToOSInterface::GetMaxProcessorCount()
 {
-    return static_cast<uint32_t>(GetMaximumProcessorGroupCount()) * 64;
+    assert(g_maxProcessorCount != 0);
+    return g_maxProcessorCount;
 }
 
 bool GCToOSInterface::CanEnableGCNumaAware()
