3bc3f15b8cff2353d703c85ff8d638d6435ab2bd
Egor Bogatov <egorbo@gmail.com>
2026-09-21 21:08:15 +0200

    Update utils.cpp

--- src/coreclr/jit/utils.cpp
+++ src/coreclr/jit/utils.cpp
@@ -1689,14 +1689,13 @@ void HelperCallProperties::init()
             case CORINFO_HELP_CHECKED_ASSIGN_REF_EDI:
 #endif
             // GC Write barrier support
-            // TODO-ARM64-Bug?: Can these throw or not?
             case CORINFO_HELP_ASSIGN_REF:
             case CORINFO_HELP_CHECKED_ASSIGN_REF:
-                isNoGC = true;
-                FALLTHROUGH;
             case CORINFO_HELP_BULK_WRITEBARRIER_SMALL:
                 isNoGC = true;
-                FALLTHROUGH;
+                mutatesHeap = true;
+                break;
+
             case CORINFO_HELP_BULK_WRITEBARRIER:
                 mutatesHeap = true;
                 break;
