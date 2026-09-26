c29faec90e46262340a1dcd5558fd3dd09dc1c67
Kevin Jones <kevin@vcsjones.com>
2026-09-26 08:22:34 -0400

    Support EC private scalar-only imports on iOS
    On iOS / tvOS / MacCatalyst, importing an EC key with only `d` would fail because it was not implemented. Previously it was not implemented because CryptoKit was not available to the platform. But now it is, so lets implement it.
    
    This matters for HPKE - right now most integration tests for HPKE are failing because HPKE depends on being able to import only the private scalar. This fixes all HPKE tests on mobile Apple platforms.

--- src/libraries/Common/src/System/Security/Cryptography/EccAppleCrypto.iOS.cs
+++ src/libraries/Common/src/System/Security/Cryptography/EccAppleCrypto.iOS.cs
@@ -15,9 +15,36 @@ namespace System.Security.Cryptography
 #pragma warning disable IDE0060
         private static ECParameters ExportParametersFromLegacyKey(SecKeyPair keys, bool includePrivateParameters)
             => throw new CryptographicException();
+#pragma warning restore IDE0060
 
         private static void ExtractPublicKeyFromPrivateKey(ref ECParameters ecParameters)
-            => throw new PlatformNotSupportedException(SR.Cryptography_NotValidPublicOrPrivateKey);
-#pragma warning restore IDE0060
+        {
+            int keySizeInBits = ecParameters.Curve.Oid.Value switch
+            {
+                Oids.secp256r1 => 256,
+                Oids.secp384r1 => 384,
+                Oids.secp521r1 => 521,
+                _ => throw DebugFail(), // Apple only supports NIST curves.
+            };
+
+            byte[] privateKey = ecParameters.D!;
+            int fieldSize = (keySizeInBits + 7) / 8;
+            Debug.Assert(privateKey.Length == fieldSize);
+
+            const int MaxPublicKeySize = 136; // P-521 is 133 bytes, round this off to 136.
+            Span<byte> publicKey = (stackalloc byte[MaxPublicKeySize]).Slice(0, 1 + 2 * fieldSize);
+            Interop.AppleCrypto.EccExportPublicKeyFromPrivateKey(keySizeInBits, privateKey, publicKey);
+            AsymmetricAlgorithmHelpers.DecodeFromUncompressedAnsiX963Key(
+                publicKey,
+                hasPrivateKey: false,
+                out ECParameters publicParameters);
+            ecParameters.Q = publicParameters.Q;
+
+            static Exception DebugFail()
+            {
+                Debug.Fail($"Unexpected curve with OID.");
+                return new CryptographicException();
+            }
+        }
     }
 }
